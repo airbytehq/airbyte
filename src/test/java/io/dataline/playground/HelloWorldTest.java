b8a1da2a32b4d5b313c8217e8e07bd84b303c939
Michel Tricot <michel@dataline.io>
2020-08-03 16:34:33 -0700

    cosmetic

--- src/test/java/io/dataline/playground/HelloWorldTest.java
+++ src/test/java/io/dataline/playground/HelloWorldTest.java
@@ -2,7 +2,7 @@ package io.dataline.playground;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class HelloWorldTest {
     @Test
