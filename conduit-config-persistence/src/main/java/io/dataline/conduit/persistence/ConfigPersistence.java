c76c28b6c13c58694a42d330ef2b715b0fbebef2
cgardens <giardina.charles@gmail.com>
2020-08-10 13:03:53 -0700

    persistence first draft

--- conduit-config-persistence/src/main/java/io/dataline/conduit/persistence/ConfigPersistence.java
+++ conduit-config-persistence/src/main/java/io/dataline/conduit/persistence/ConfigPersistence.java
@@ -1,14 +1,12 @@
 package io.dataline.conduit.persistence;
 
-import com.fasterxml.jackson.databind.JsonNode;
 import java.util.Set;
 
 public interface ConfigPersistence {
-  JsonNode getConfig(ConfigType configType, String configId);
+  <T> T getStandardConfig(PersistenceConfigType persistenceConfigType, String configId, Class<T> clazz);
 
-  Set<JsonNode> getConfigs(ConfigType configType);
+  <T> Set<T> getStandardConfigs(PersistenceConfigType persistenceConfigType, Class<T> clazz);
 
-  void updateConfig(ConfigType configType, String configId, JsonNode config);
-
-  void createConfig(ConfigType configType, String configId, JsonNode config);
+  <T> void writeStandardConfig(
+          PersistenceConfigType persistenceConfigType, String configId, T config, Class<T> clazz);
 }
--- conduit-config/src/main/java/io/dataline/conduit/conduit_config/BlahMah.java
+++
@@ -1,7 +0,0 @@
-package io.dataline.conduit.conduit_config;
-
-public class BlahMah {
-    public static String hello() {
-        return "hello";
-    }
-}
