358a2dc962e034569c4dc2e8ac71e539f59000a2
cgardens <giardina.charles@gmail.com>
2020-08-11 08:43:58 -0700

    why not

--- dataline-config/src/main/java/io/dataline/config/ConfigSchema.java
+++ dataline-config/src/main/java/io/dataline/config/ConfigSchema.java
@@ -1,6 +1,8 @@
 package io.dataline.config;
 
 public enum ConfigSchema {
+  STANDARD_SOURCE("StandardSource.json"),
+  SOURCE_CONNECTION_SPECIFICATION("SourceConnectionSpecification.json"),
   SOURCE_CONNECTION_IMPLEMENTATION("SourceConnectionImplementation.json"),
   STANDARD_CONNECTION_STATUS("StandardConnectionStatus.json"),
   STANDARD_DISCOVERY_OUTPUT("StandardDiscoveryOutput.json"),
@@ -10,9 +12,7 @@ public enum ConfigSchema {
   STANDARD_SYNC_STATE("StandardSyncState.json"),
   STANDARD_SYNC_SCHEDULE("StandardSyncSchedule.json"),
   STATE("State.json"),
-  STANDARD_WORKSPACE_CONFIGURATION("StandardWorkspaceConfiguration.json"),
-  STANDARD_SOURCE("StandardSource.json"),
-  SOURCE_CONNECTION_SPECIFICATION("SourceConnectionSpecification.json");
+  STANDARD_WORKSPACE_CONFIGURATION("StandardWorkspaceConfiguration.json");
 
   private final String schemaFilename;
 
