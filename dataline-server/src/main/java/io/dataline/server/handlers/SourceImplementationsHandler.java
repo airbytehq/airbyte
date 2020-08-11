ffe1a102466dd6c260271a47296f346652ebba02
cgardens <giardina.charles@gmail.com>
2020-08-11 15:27:01 -0700

    first pass

---
+++ dataline-server/src/main/java/io/dataline/server/handlers/SourceImplementationsHandler.java
@@ -0,0 +1,69 @@
+package io.dataline.server.handlers;
+
+import io.dataline.api.model.SourceImplementationCreate;
+import io.dataline.api.model.SourceImplementationRead;
+import io.dataline.config.SourceConnectionImplementation;
+import io.dataline.config.persistence.ConfigNotFoundException;
+import io.dataline.config.persistence.ConfigPersistence;
+import io.dataline.config.persistence.JsonValidationException;
+import io.dataline.config.persistence.PersistenceConfigType;
+import io.dataline.server.validation.IntegrationSchemaValidation;
+import java.util.UUID;
+
+public class SourceImplementationsHandler {
+  private final ConfigPersistence configPersistence;
+
+  public SourceImplementationsHandler(ConfigPersistence configPersistence) {
+    this.configPersistence = configPersistence;
+  }
+
+  public SourceImplementationRead createSourceImplementation(
+      SourceImplementationCreate sourceImplementationCreate)
+      throws JsonValidationException, ConfigNotFoundException {
+
+    // validate configuration
+    final IntegrationSchemaValidation validator =
+        new IntegrationSchemaValidation(configPersistence);
+    validator.validateSourceConnectionConfiguration(
+        sourceImplementationCreate.getSourceSpecificationId(),
+        sourceImplementationCreate.getConnectionConfiguration());
+
+    // persist
+    final UUID sourceImplementationId = UUID.randomUUID();
+    final SourceConnectionImplementation sourceConnectionImplementation =
+        new SourceConnectionImplementation();
+    sourceConnectionImplementation.setSourceSpecificationId(
+        sourceImplementationCreate.getSourceSpecificationId());
+    sourceConnectionImplementation.setSourceImplementationId(sourceImplementationId);
+    sourceConnectionImplementation.setConfiguration(
+        sourceImplementationCreate.getConnectionConfiguration());
+
+    configPersistence.writeConfig(
+        PersistenceConfigType.SOURCE_CONNECTION_IMPLEMENTATION,
+        sourceImplementationId.toString(),
+        sourceConnectionImplementation);
+
+    // read configuration from db
+    final SourceConnectionImplementation sourceConnectionImplementation2 =
+        configPersistence.getConfig(
+            PersistenceConfigType.SOURCE_CONNECTION_IMPLEMENTATION,
+            sourceImplementationId.toString(),
+            SourceConnectionImplementation.class);
+
+    return sourceConnectionImplementationToSourceImplementationRead(
+        sourceConnectionImplementation2);
+  }
+
+  private SourceImplementationRead sourceConnectionImplementationToSourceImplementationRead(
+      SourceConnectionImplementation sourceConnectionImplementation) {
+    final SourceImplementationRead sourceImplementationRead = new SourceImplementationRead();
+    sourceConnectionImplementation.setSourceImplementationId(
+        sourceConnectionImplementation.getSourceImplementationId());
+    sourceConnectionImplementation.setSourceSpecificationId(
+        sourceConnectionImplementation.getSourceSpecificationId());
+    sourceConnectionImplementation.setConfiguration(
+        sourceConnectionImplementation.getConfiguration());
+
+    return sourceImplementationRead;
+  }
+}
