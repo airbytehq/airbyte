b60233610dfdcc54092a72c7e81e53e470c1cec4
Charles <giardina.charles@gmail.com>
2020-08-12 13:44:48 -0700

    Implement Workspace and Source* API endpoints (#42)

--- dataline-server/src/main/java/io/dataline/server/apis/ConfigurationApi.java
+++ dataline-server/src/main/java/io/dataline/server/apis/ConfigurationApi.java
@@ -1,11 +1,103 @@
 package io.dataline.server.apis;
 
 import io.dataline.api.model.*;
+import io.dataline.config.persistence.ConfigPersistence;
+import io.dataline.config.persistence.ConfigPersistenceImpl;
+import io.dataline.server.handlers.SourceImplementationsHandler;
+import io.dataline.server.handlers.SourceSpecificationsHandler;
+import io.dataline.server.handlers.SourcesHandler;
+import io.dataline.server.handlers.WorkspacesHandler;
 import javax.validation.Valid;
 import javax.ws.rs.Path;
 
 @Path("/v1")
 public class ConfigurationApi implements io.dataline.api.V1Api {
+  private final WorkspacesHandler workspacesHandler;
+  private final SourcesHandler sourcesHandler;
+  private final SourceSpecificationsHandler sourceSpecificationsHandler;
+  private final SourceImplementationsHandler sourceImplementationsHandler;
+
+  public ConfigurationApi() {
+    ConfigPersistence configPersistence = new ConfigPersistenceImpl();
+    workspacesHandler = new WorkspacesHandler(configPersistence);
+    sourcesHandler = new SourcesHandler(configPersistence);
+    sourceSpecificationsHandler = new SourceSpecificationsHandler(configPersistence);
+    sourceImplementationsHandler = new SourceImplementationsHandler(configPersistence);
+  }
+
+  // WORKSPACE
+
+  @Override
+  public WorkspaceRead getWorkspace(@Valid WorkspaceIdRequestBody workspaceIdRequestBody) {
+    return workspacesHandler.getWorkspace(workspaceIdRequestBody);
+  }
+
+  @Override
+  public WorkspaceRead getWorkspaceBySlug(@Valid SlugRequestBody slugRequestBody) {
+    return workspacesHandler.getWorkspaceBySlug(slugRequestBody);
+  }
+
+  @Override
+  public WorkspaceRead updateWorkspace(@Valid WorkspaceUpdate workspaceUpdate) {
+    return workspacesHandler.updateWorkspace(workspaceUpdate);
+  }
+
+  // SOURCE
+
+  @Override
+  public SourceReadList listSources() {
+    return sourcesHandler.listSources();
+  }
+
+  @Override
+  public SourceRead getSource(@Valid SourceIdRequestBody sourceIdRequestBody) {
+    return sourcesHandler.getSource(sourceIdRequestBody);
+  }
+
+  // SOURCE SPECIFICATION
+
+  @Override
+  public SourceSpecificationRead getSourceSpecification(
+      @Valid SourceIdRequestBody sourceIdRequestBody) {
+    return sourceSpecificationsHandler.getSourceSpecification(sourceIdRequestBody);
+  }
+
+  // SOURCE IMPLEMENTATION
+  @Override
+  public SourceImplementationRead createSourceImplementation(
+      @Valid SourceImplementationCreate sourceImplementationCreate) {
+    return sourceImplementationsHandler.createSourceImplementation(sourceImplementationCreate);
+  }
+
+  @Override
+  public SourceImplementationDiscoverSchemaRead discoverSchemaForSourceImplementation(
+      @Valid SourceImplementationIdRequestBody sourceImplementationIdRequestBody) {
+    return null;
+  }
+
+  @Override
+  public SourceImplementationRead getSourceImplementation(
+      @Valid SourceImplementationIdRequestBody sourceImplementationIdRequestBody) {
+    return null;
+  }
+
+  @Override
+  public SourceImplementationReadList getSourceImplementationsForWorkspace(
+      @Valid WorkspaceIdRequestBody workspaceIdRequestBody) {
+    return null;
+  }
+
+  @Override
+  public SourceImplementationTestConnectionRead testConnectionToSourceImplementation(
+      @Valid SourceImplementationIdRequestBody sourceImplementationIdRequestBody) {
+    return null;
+  }
+
+  @Override
+  public SourceImplementationRead updateSourceImplementation(
+      @Valid SourceImplementationUpdate sourceImplementationUpdate) {
+    return null;
+  }
 
   @Override
   public ConnectionRead createConnection(@Valid ConnectionCreate connectionCreate) {
@@ -15,18 +107,6 @@ public class ConfigurationApi implements io.dataline.api.V1Api {
   @Override
   public DestinationImplementationRead createDestinationImplementation(
       @Valid DestinationImplementationCreate destinationImplementationCreate) {
-    return null;
-  }
-
-  @Override
-  public SourceImplementationRead createSourceImplementation(
-      @Valid SourceImplementationCreate sourceImplementationCreate) {
-    return null;
-  }
-
-  @Override
-  public SourceImplementationDiscoverSchemaRead discoverSchemaForSourceImplementation(
-      @Valid SourceImplementationIdRequestBody sourceImplementationIdRequestBody) {
     return null;
   }
 
@@ -53,39 +133,6 @@ public class ConfigurationApi implements io.dataline.api.V1Api {
   }
 
   @Override
-  public SourceRead getSource(@Valid SourceIdRequestBody sourceIdRequestBody) {
-    return null;
-  }
-
-  @Override
-  public SourceImplementationRead getSourceImplementation(
-      @Valid SourceImplementationIdRequestBody sourceImplementationIdRequestBody) {
-    return null;
-  }
-
-  @Override
-  public SourceImplementationReadList getSourceImplementationsForWorkspace(
-      @Valid WorkspaceIdRequestBody workspaceIdRequestBody) {
-    return null;
-  }
-
-  @Override
-  public SourceSpecificationRead getSourceSpecification(
-      @Valid SourceIdRequestBody sourceIdRequestBody) {
-    return null;
-  }
-
-  @Override
-  public WorkspaceRead getWorkspace(@Valid WorkspaceIdRequestBody workspaceIdRequestBody) {
-    return null;
-  }
-
-  @Override
-  public WorkspaceRead getWorkspaceBySlug(@Valid SlugRequestBody slugRequestBody) {
-    return null;
-  }
-
-  @Override
   public ConnectionReadList listConnectionsForWorkspace(
       @Valid WorkspaceIdRequestBody workspaceIdRequestBody) {
     return null;
@@ -103,18 +150,7 @@ public class ConfigurationApi implements io.dataline.api.V1Api {
   }
 
   @Override
-  public SourceReadList listSources() {
-    return null;
-  }
-
-  @Override
   public ConnectionSyncRead syncConnection(@Valid ConnectionIdRequestBody connectionIdRequestBody) {
-    return null;
-  }
-
-  @Override
-  public SourceImplementationTestConnectionRead testConnectiontoSourceImplementation(
-      @Valid SourceImplementationIdRequestBody sourceImplementationIdRequestBody) {
     return null;
   }
 
@@ -126,12 +162,6 @@ public class ConfigurationApi implements io.dataline.api.V1Api {
   @Override
   public DestinationImplementationRead updateDestinationImplementation(
       @Valid DestinationImplementationUpdate destinationImplementationUpdate) {
-    return null;
-  }
-
-  @Override
-  public SourceImplementationRead updateSourceImplementation(
-      @Valid SourceImplementationUpdate sourceImplementationUpdate) {
     return null;
   }
 }
