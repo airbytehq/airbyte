ca08f21183789fd1eb1535d21d7e79f220116aee
Sherif Nada <snadalive@gmail.com>
2020-08-11 23:54:47 -0700

    test instance

--- dataline-workers/src/test/java/io/dataline/workers/BaseWorkerTestCase.java
+++ dataline-workers/src/test/java/io/dataline/workers/BaseWorkerTestCase.java
@@ -6,7 +6,9 @@ import java.nio.file.Paths;
 import java.util.UUID;
 import org.apache.commons.io.FileUtils;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.TestInstance;
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public abstract class BaseWorkerTestCase {
   private Path workspaceDirectory;
 
