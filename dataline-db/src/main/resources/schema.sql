3fba8b3d6f7df0a2a558d1953fb9f0fdfa90dda7
Jared Rhizor <jared@dataline.io>
2020-08-13 13:56:49 -0700

    update job schema to allow for oneoff jobs (#50)
    * update job schema to allow for oneoff jobs
    
    * unrelated fix required for build

--- dataline-db/src/main/resources/schema.sql
+++ dataline-db/src/main/resources/schema.sql
@@ -1,2 +1,3 @@
 CREATE TABLE DATALINE_METADATA (id varchar(255), value varchar(255), PRIMARY KEY (id));
 INSERT INTO DATALINE_METADATA VALUES  ('server-uuid', ((lower(hex(randomblob(4))) || '-' || lower(hex(randomblob(2))) || '-4' || substr(lower(hex(randomblob(2))),2) || '-' || substr('89ab',abs(random()) % 4 + 1, 1) || substr(lower(hex(randomblob(2))),2) || '-' || lower(hex(randomblob(6))))));
+CREATE TABLE JOBS (id INTEGER PRIMARY KEY AUTOINCREMENT, scope varchar(255), created_at INTEGER, started_at INTEGER, updated_at INTEGER, status INTEGER, config BLOB, output, BLOB, stdout_path varchar(255), stderr_path varchar(255));
