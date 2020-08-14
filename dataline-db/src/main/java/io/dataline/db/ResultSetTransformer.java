88fc2e534297186fe270175691d29f093502f607
Jared Rhizor <jared@dataline.io>
2020-08-14 11:02:10 -0700

    use postgres / jooq (without code generation) / test containers (#57)
    * add database image to docker compose
    
    * add database setup
    
    * allow connetions from a connect string in env variables
    
    * add a test for the docker case
    
    * add pg init script in docker-compose
    
    * fix env and add server dependency on db
    
    * convert schema to postgres
    
    * use postgres driver
    
    * fix build
    
    * fix edge cases (dependencies between containers, data dirs, etc)
    
    * use docker wait
    
    * remove debugging logging
    
    * add grant
    
    * working test and working docker-compose
    
    * use beforeall
    
    * use key instead of id for k-v metadata
    
    * clean up env names
    
    * fix typo

--- dataline-db/src/main/java/io/dataline/db/ResultSetTransformer.java
+++
@@ -1,9 +0,0 @@
-package io.dataline.db;
-
-import java.sql.ResultSet;
-import java.sql.SQLException;
-
-@FunctionalInterface
-public interface ResultSetTransformer<T> {
-  T apply(ResultSet resultSet) throws SQLException;
-}
