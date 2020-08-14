a070aaf6eb834685499d291dcded16a72f10e1ac
Charles <giardina.charles@gmail.com>
2020-08-14 07:32:21 -0700

    add destinations, destinationSpecifications, and destinationImplementations resources to API (#49)
    * add destinationsHandler
    
    * add destinationSpecificationsHandler
    
    * add destinationImplementationsHandler

--- dataline-server/src/main/java/io/dataline/server/validation/IntegrationSchemaValidation.java
+++ dataline-server/src/main/java/io/dataline/server/validation/IntegrationSchemaValidation.java
@@ -2,6 +2,7 @@ package io.dataline.server.validation;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import io.dataline.config.DestinationConnectionSpecification;
 import io.dataline.config.SourceConnectionSpecification;
 import io.dataline.config.persistence.*;
 import java.util.UUID;
@@ -30,6 +31,22 @@ public class IntegrationSchemaValidation {
 
     final JsonNode schemaJson =
         objectMapper.valueToTree(sourceConnectionSpecification.getSpecification());
+    final JsonNode configJson = objectMapper.valueToTree(configuration);
+
+    jsonSchemaValidation.validateThrow(schemaJson, configJson);
+  }
+
+  public void validateDestinationConnectionConfiguration(
+      UUID destinationConnectionSpecificationId, Object configuration)
+      throws JsonValidationException, ConfigNotFoundException {
+    final DestinationConnectionSpecification destinationConnectionSpecification =
+        configPersistence.getConfig(
+            PersistenceConfigType.DESTINATION_CONNECTION_SPECIFICATION,
+            destinationConnectionSpecificationId.toString(),
+            DestinationConnectionSpecification.class);
+
+    final JsonNode schemaJson =
+        objectMapper.valueToTree(destinationConnectionSpecification.getSpecification());
     final JsonNode configJson = objectMapper.valueToTree(configuration);
 
     jsonSchemaValidation.validateThrow(schemaJson, configJson);
