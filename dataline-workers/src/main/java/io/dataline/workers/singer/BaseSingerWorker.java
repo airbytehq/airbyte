2363eedbd36a4c3ea30835853d6da07cc39dada4
Sherif A. Nada <snadalive@gmail.com>
2020-08-14 16:32:06 -0700

    Add Singer infra (#58)

--- dataline-workers/src/main/java/io/dataline/workers/singer/BaseSingerWorker.java
+++ dataline-workers/src/main/java/io/dataline/workers/singer/BaseSingerWorker.java
@@ -1,6 +1,7 @@
 package io.dataline.workers.singer;
 
 import io.dataline.workers.JobStatus;
+import io.dataline.workers.OutputAndStatus;
 import io.dataline.workers.Worker;
 import java.io.BufferedReader;
 import java.io.FileReader;
@@ -10,6 +11,7 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
+import org.apache.commons.io.FileUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -19,14 +21,31 @@ public abstract class BaseSingerWorker<OutputType> implements Worker<OutputType>
   protected JobStatus jobStatus;
   protected String workerId;
   protected Process workerProcess;
+  protected final Path workspacePath;
 
-  private final String workspaceRoot;
   private final String singerRoot;
 
   protected BaseSingerWorker(String workerId, String workspaceRoot, String singerRoot) {
     this.workerId = workerId;
-    this.workspaceRoot = workspaceRoot;
+    this.workspacePath = Path.of(workspaceRoot, workerId);
     this.singerRoot = singerRoot;
+  }
+
+  @Override
+  public OutputAndStatus<OutputType> run() {
+    createWorkspace();
+    return runInternal();
+  }
+
+  public abstract OutputAndStatus<OutputType> runInternal();
+
+  private void createWorkspace() {
+    try {
+      FileUtils.forceMkdir(workspacePath.toFile());
+    } catch (IOException e) {
+      LOGGER.error("Unable to create workspace for worker {} due to exception {} ", workerId, e);
+      throw new RuntimeException(e);
+    }
   }
 
   @Override
@@ -44,7 +63,7 @@ public abstract class BaseSingerWorker<OutputType> implements Worker<OutputType>
   }
 
   protected Path getWorkspacePath() {
-    return Paths.get(workspaceRoot, workerId);
+    return workspacePath;
   }
 
   protected String readFileFromWorkspace(String fileName) {
---
+++ tools/singer/setup_singer_env.buster.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# Sets up the Singer environment on a Debian (Buster) distro
+
+set -e
+
+. tools/lib/lib.sh
+
+assert_root
+SINGER_ROOT=$1
+[ -z "$SINGER_ROOT" ] && error "singer_root is required"
+
+apt-get clean
+apt-get update
+apt-get -y install libpq-dev=11.7-0+deb10u1 \
+  python3.7=3.7.3-2+deb10u2 \
+  python3-venv=3.7.3-1 \
+  python3-pip=18.1-5
+
+./tools/singer/install_all_connectors.sh "$SINGER_ROOT"
