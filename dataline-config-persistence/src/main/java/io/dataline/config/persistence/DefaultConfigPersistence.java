7390f681a63b19eda8c9b05a50e01107e0332a19
Charles <giardina.charles@gmail.com>
2020-08-17 15:03:27 -0700

    pass in persistence paths as env variables (#60)

--- dataline-config-persistence/src/main/java/io/dataline/config/persistence/DefaultConfigPersistence.java
+++ dataline-config-persistence/src/main/java/io/dataline/config/persistence/DefaultConfigPersistence.java
@@ -26,12 +26,13 @@ package io.dataline.config.persistence;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.common.annotations.VisibleForTesting;
 import io.dataline.config.ConfigSchema;
 import java.io.File;
 import java.io.IOException;
+import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.HashSet;
 import java.util.Optional;
 import java.util.Set;
@@ -43,7 +44,6 @@ import org.apache.commons.io.FileUtils;
 // we force all interaction with disk storage to be effectively single threaded.
 public class DefaultConfigPersistence implements ConfigPersistence {
   private static final Object lock = new Object();
-  private static final String CONFIG_SCHEMA_ROOT = "../dataline-config/src/main/resources/json/";
 
   private final ObjectMapper objectMapper;
   private final JsonSchemaValidation jsonSchemaValidation;
@@ -98,46 +98,53 @@ public class DefaultConfigPersistence implements ConfigPersistence {
   public <T> void writeConfig(
       PersistenceConfigType persistenceConfigType, String configId, T config) {
     synchronized (lock) {
-      final String configPath = getConfigPath(persistenceConfigType, configId);
+      final Path configPath = getConfigPath(persistenceConfigType, configId);
       ensureDirectory(getConfigDirectory(persistenceConfigType));
       try {
-        objectMapper.writeValue(new File(configPath), config);
+        objectMapper.writeValue(configPath.toFile(), config);
       } catch (IOException e) {
         throw new RuntimeException(e);
       }
     }
   }
 
-  private JsonNode getSchema(PersistenceConfigType persistenceConfigType) {
-    String configSchemaFilename =
+  @VisibleForTesting
+  JsonNode getSchema(PersistenceConfigType persistenceConfigType) {
+    final String configSchemaFilename =
         standardConfigTypeToConfigSchema(persistenceConfigType).getSchemaFilename();
-    File schemaFile = new File(String.format("%s/%s", CONFIG_SCHEMA_ROOT, configSchemaFilename));
+    final String filenameWithPrefix = ConfigSchema.getSchemaDirectory() + configSchemaFilename;
+    final URL resource = ConfigSchema.class.getClassLoader().getResource(filenameWithPrefix);
+    if (resource == null) {
+      throw new RuntimeException(
+          String.format("Could not find resource for %s.", persistenceConfigType));
+    }
+
     try {
       // JsonReferenceProcessor follows $ref in json objects. Jackson does not natively support
       // this.
       final JsonReferenceProcessor jsonReferenceProcessor = new JsonReferenceProcessor();
       jsonReferenceProcessor.setMaxDepth(-1); // no max.
-      return jsonReferenceProcessor.process(schemaFile);
+      return jsonReferenceProcessor.process(resource);
     } catch (IOException | JsonReferenceException e) {
       throw new RuntimeException(e);
     }
   }
 
   private Set<Path> getFiles(PersistenceConfigType persistenceConfigType) {
-    String configDirPath = getConfigDirectory(persistenceConfigType);
+    Path configDirPath = getConfigDirectory(persistenceConfigType);
     try {
-      return Files.list(new File(configDirPath).toPath()).collect(Collectors.toSet());
+      return Files.list(configDirPath).collect(Collectors.toSet());
     } catch (IOException e) {
       throw new RuntimeException(e);
     }
   }
 
-  private String getConfigDirectory(PersistenceConfigType persistenceConfigType) {
-    return String.format("%s/%s", storageRoot, persistenceConfigType.toString());
+  private Path getConfigDirectory(PersistenceConfigType persistenceConfigType) {
+    return Path.of(storageRoot).resolve(persistenceConfigType.toString());
   }
 
-  private String getConfigPath(PersistenceConfigType persistenceConfigType, String configId) {
-    return String.format("%s/%s", getConfigDirectory(persistenceConfigType), getFilename(configId));
+  private Path getConfigPath(PersistenceConfigType persistenceConfigType, String configId) {
+    return getConfigDirectory(persistenceConfigType).resolve(getFilename(configId));
   }
 
   private Set<String> getConfigIds(PersistenceConfigType persistenceConfigType) {
@@ -148,7 +155,7 @@ public class DefaultConfigPersistence implements ConfigPersistence {
 
   private Optional<Path> getFile(PersistenceConfigType persistenceConfigType, String id) {
     ensureDirectory(getConfigDirectory(persistenceConfigType));
-    final Path path = Paths.get(getConfigPath(persistenceConfigType, id));
+    final Path path = getConfigPath(persistenceConfigType, id);
     if (Files.exists(path)) {
       return Optional.of(path);
     } else {
@@ -232,9 +239,9 @@ public class DefaultConfigPersistence implements ConfigPersistence {
     }
   }
 
-  private void ensureDirectory(String path) {
+  private void ensureDirectory(Path path) {
     try {
-      FileUtils.forceMkdir(new File(path));
+      FileUtils.forceMkdir(path.toFile());
     } catch (IOException e) {
       throw new RuntimeException(e);
     }
