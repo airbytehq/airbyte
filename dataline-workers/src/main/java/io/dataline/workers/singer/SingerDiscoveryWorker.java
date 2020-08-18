23f02b9b104403ddee9e091b1a26b84602881386
Michel Tricot <michel@dataline.io>
2020-08-17 17:50:19 -0700

    cleanup

--- dataline-workers/src/main/java/io/dataline/workers/singer/SingerDiscoveryWorker.java
+++ dataline-workers/src/main/java/io/dataline/workers/singer/SingerDiscoveryWorker.java
@@ -1,18 +1,18 @@
 /*
  * MIT License
- * 
+ *
  * Copyright (c) 2020 Dataline
- * 
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in all
  * copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -38,9 +38,9 @@ public class SingerDiscoveryWorker extends BaseSingerWorker<DiscoveryOutput> {
 
   // TODO log errors to specified file locations
   private static final Logger LOGGER = LoggerFactory.getLogger(SingerDiscoveryWorker.class);
-  private static String CONFIG_JSON_FILENAME = "config.json";
-  private static String CATALOG_JSON_FILENAME = "catalog.json";
-  private static String ERROR_LOG_FILENAME = "err.log";
+  private static final String CONFIG_JSON_FILENAME = "config.json";
+  private static final String CATALOG_JSON_FILENAME = "catalog.json";
+  private static final String ERROR_LOG_FILENAME = "err.log";
 
   private final String configDotJson;
   private final SingerTap tap;
