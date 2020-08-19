f63a909cdc0d8dbccb40379897d8678162c6b4e1
jrhizor <me@jaredrhizor.com>
2020-08-19 15:16:28 -0700

    remove all building from start script

--- tools/app/build.sh
+++ tools/app/build.sh
@@ -16,6 +16,7 @@ main() {
   # The base image may launch docker containers, so mount the docker socket as a volume to allow that
   docker run -t --rm -v /var/run/docker.sock:/var/run/docker.sock "$BASE_IMAGE_NAME"
   docker build -f "$DIST_IMAGE_DOCKERFILE" . -t "$DIST_IMAGE_NAME"
+  docker-compose -f docker-compose.dev.yaml build --parallel
 }
 
 main "$@"
