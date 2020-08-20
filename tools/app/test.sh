c64d519d7ecc343e0068103ef49f42466c3d6d6a
jrhizor <me@jaredrhizor.com>
2020-08-20 14:34:24 -0700

    final

--- tools/app/test.sh
+++ tools/app/test.sh
@@ -6,8 +6,8 @@ set -e
 
 main() {
   assert_root
-  docker run --rm -v /var/run/docker.sock:/var/run/docker.sock -v ~/.gradle:/home/gradle/.gradle -t dataline/server-base:latest /bin/sh -c ./gradlew test --no-daemon --console rich -g /home/gradle/.gradle
-  docker run --rm -t dataline/webapp-base:latest /bin/sh -c 'npm run test'
+  docker run --rm -v /var/run/docker.sock:/var/run/docker.sock -v ~/.gradle:/home/gradle/.gradle -t dataline/server-base:dev /bin/sh -c ./gradlew test --no-daemon --console rich -g /home/gradle/.gradle
+  docker run --rm -t dataline/webapp-base:dev /bin/sh -c 'npm run test'
 }
 
 main "$@"
