b1e4d835a7b2d979e59683ebc4002dbce2ab1e9c
jrhizor <me@jaredrhizor.com>
2020-08-20 13:30:32 -0700

    make names line up

--- tools/app/build.sh
+++ tools/app/build.sh
@@ -8,10 +8,10 @@ main() {
   assert_root
 
   echo "Building server-base..."
-  docker build -f server_base.Dockerfile . -t server-base:dev
+  docker build -f server_base.Dockerfile . -t dataline/server-base:dev
 
   echo "Building webapp-base..."
-  docker build -f webapp_base.Dockerfile . -t webapp-base:dev
+  docker build -f webapp_base.Dockerfile . -t dataline/webapp-base:dev
 
   echo "Running docker-compose..."
   docker-compose -f docker-compose.dev.yaml build --parallel
