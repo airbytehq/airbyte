63216f6583cf00cbd35db56a4ea424480aa8936c
cgardens <giardina.charles@gmail.com>
2020-08-20 17:28:27 -0700

    wip

---
+++ dataline-workers/src/main/java/io/dataline/workers/singer/SingerCatalogConverters.java
@@ -0,0 +1,133 @@
+package io.dataline.workers.singer;
+
+import com.fasterxml.jackson.databind.annotation.JsonAppend;
+import io.dataline.config.Column;
+import io.dataline.config.DataType;
+import io.dataline.config.Properties;
+import io.dataline.config.PropertiesProperty;
+import io.dataline.config.Schema;
+import io.dataline.config.SingerCatalog;
+import io.dataline.config.SingerSchema;
+import io.dataline.config.SingerType;
+import io.dataline.config.StandardDiscoveryOutput;
+import io.dataline.config.Stream;
+import io.dataline.config.Table;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class SingerCatalogConverters {
+
+  public static SingerCatalog toCatalogBang(SingerCatalog catalog, Schema schema) {
+    Map<String, Table> tableNameToTable =
+        schema.getTables().stream().collect(Collectors.toMap(Table::getName, table -> table));
+
+    final List<Stream> updatedStreams = catalog.getStreams().stream().map(stream -> {
+      if (!tableNameToTable.containsKey(stream.getStream())) {
+        // recourse here is probably to run discovery again and update sync configuration.
+        throw new RuntimeException("could not find table in singer catalog.");
+      }
+      final Table table = tableNameToTable.get(stream.getStream());
+      final Map<String, Column> columnNameToColumn = table.getColumns().stream().collect(Collectors.toMap(Column::getName, column -> column));
+      stream.getSchema().getProperties().getAdditionalProperties().entrySet().stream().map((Map.Entry<String, PropertiesProperty> entry) -> {
+        if (columnNameToColumn.containsKey(entry.getKey())) {
+          // selected
+        } else {
+          // maybe this should happen or if it does, not selected.
+        }
+
+        return entry;
+      });
+
+      // rename this shit.
+      new Properties().setAdditionalProperty();
+      final SingerSchema singerSchema = new SingerSchema();
+      singerSchema.setProperties();
+      singerSchema.setType("???");
+      stream.setSchema();
+
+      return stream;
+    }).collect(Collectors.toList());
+
+    final SingerCatalog outputCatalog = new SingerCatalog();
+    outputCatalog.setStreams(updatedStreams);
+
+    return outputCatalog;
+  }
+
+  public static Schema toDatalineSchema(SingerCatalog catalog) {
+    List<Table> tableStream =
+        catalog.getStreams().stream()
+            .map(
+                stream -> {
+                  final Table table = new Table();
+                  table.setName(
+                      stream.getStream()); // todo (cgardens) - is stream the same as table name?
+                  table.setColumns(
+                      stream
+                          .getSchema()
+                          .getProperties()
+                          .getAdditionalProperties()
+                          .entrySet()
+                          .stream()
+                          .map(
+                              entry -> {
+                                final String columnName = entry.getKey();
+                                final PropertiesProperty columnMetadata = entry.getValue();
+                                final Column column = new Column();
+                                column.setName(columnName);
+                                column.setDataType(singerTypesToDataType(columnMetadata.getType()));
+                                return column;
+                              })
+                          .collect(Collectors.toList()));
+                  return table;
+                })
+            .collect(Collectors.toList());
+
+    final Schema schema = new Schema();
+    schema.setTables(tableStream);
+    return schema;
+  }
+
+  /**
+   * Singer tends to have 2 types for columns one of which is null. The null is pretty irrelevant,
+   * so look at types and find the first non-null one and use that.
+   *
+   * @param singerTypes - list of types discovered by singer.
+   * @return reduce down to one type which best matches the column's data type
+   */
+  private static DataType singerTypesToDataType(List<SingerType> singerTypes) {
+    return singerTypes.stream()
+        .filter(singerType -> !SingerType.NULL.equals(singerType))
+        .map(SingerCatalogConverters::singerTypeToDataType)
+        .findFirst()
+        .orElse(DataType.STRING);
+  }
+
+  /**
+   * Singer doesn't seem to have an official list of the data types that they support, so we will
+   * have to do our best here as we discover them. If it becomes too awful, we can just map types we
+   * don't recognize to string.
+   *
+   * @param singerType - singer's column data type
+   * @return best match for our own data type
+   */
+  private static DataType singerTypeToDataType(SingerType singerType) {
+    switch (singerType) {
+      case STRING:
+        return DataType.STRING;
+      case INTEGER:
+        return DataType.NUMBER;
+      case NULL:
+        //noinspection DuplicateBranchesInSwitch
+        return DataType.STRING; // todo (cgardens) - hackasaurus rex
+      case BOOLEAN:
+        return DataType.BOOLEAN;
+      default:
+        throw new RuntimeException(
+            String.format("could not map SingerType: %s to DataType", singerType));
+    }
+  }
+}
