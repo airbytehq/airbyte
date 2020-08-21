27db54a98f3fd6d7760cc27ec2b0d09ff8de41e9
Jared Rhizor <jared@dataline.io>
2020-08-21 10:35:12 -0700

    add simple job history api interface (#91)
    * add simple job history api
    
    * fix compilation errors

--- dataline-server/src/main/java/io/dataline/server/apis/ConfigurationApi.java
+++ dataline-server/src/main/java/io/dataline/server/apis/ConfigurationApi.java
@@ -40,6 +40,9 @@ import io.dataline.api.model.DestinationImplementationUpdate;
 import io.dataline.api.model.DestinationRead;
 import io.dataline.api.model.DestinationReadList;
 import io.dataline.api.model.DestinationSpecificationRead;
+import io.dataline.api.model.JobIdRequestBody;
+import io.dataline.api.model.JobInfoRead;
+import io.dataline.api.model.JobReadList;
 import io.dataline.api.model.SlugRequestBody;
 import io.dataline.api.model.SourceIdRequestBody;
 import io.dataline.api.model.SourceImplementationCreate;
@@ -71,6 +74,7 @@ import io.dataline.server.validation.IntegrationSchemaValidation;
 import javax.validation.Valid;
 import javax.ws.rs.Path;
 import org.apache.commons.dbcp2.BasicDataSource;
+import org.apache.commons.lang3.NotImplementedException;
 
 @Path("/v1")
 public class ConfigurationApi implements io.dataline.api.V1Api {
@@ -198,6 +202,16 @@ public class ConfigurationApi implements io.dataline.api.V1Api {
   public DestinationSpecificationRead getDestinationSpecification(
       @Valid DestinationIdRequestBody destinationIdRequestBody) {
     return destinationSpecificationsHandler.getDestinationSpecification(destinationIdRequestBody);
+  }
+
+  @Override
+  public JobReadList listJobsFor(@Valid ConnectionIdRequestBody connectionIdRequestBody) {
+    throw new NotImplementedException("listJobsFor not supported yet");
+  }
+
+  @Override
+  public JobInfoRead getJobInfo(@Valid JobIdRequestBody jobIdRequestBody) {
+    throw new NotImplementedException("getJobInfo not supported yet");
   }
 
   // DESTINATION IMPLEMENTATION
