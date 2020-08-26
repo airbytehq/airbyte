fb42d0a37a804764043fa27c5e70cafefccf8ca3
cgardens <giardina.charles@gmail.com>
2020-08-26 11:19:39 -0700

    gradle style

--- dataline-workers/src/main/java/io/dataline/workers/singer/SingerTap.java
+++ dataline-workers/src/main/java/io/dataline/workers/singer/SingerTap.java
@@ -41,7 +41,6 @@ import java.nio.file.Path;
 import java.util.Arrays;
 import java.util.Iterator;
 import java.util.concurrent.TimeUnit;
-
 import org.apache.commons.lang3.ArrayUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
