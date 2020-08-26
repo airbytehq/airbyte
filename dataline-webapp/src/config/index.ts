cdd56130a9ed14d8da7c3e6bb35177efd203a5b8
Artem Astapenko <jamakase54@gmail.com>
2020-08-26 19:25:28 +0300

    Edit api url in config

--- dataline-webapp/src/config/index.ts
+++ dataline-webapp/src/config/index.ts
@@ -1,10 +1,13 @@
-const config = {
+const config: {
+  ui: { helpLink: string; docsLink: string; workspaceId: string };
+  apiUrl: string;
+} = {
   ui: {
     helpLink: "https://dataline.io/",
     docsLink: "https://docs.dataline.io",
     workspaceId: "5ae6b09b-fdec-41af-aaf7-7d94cfc33ef6"
   },
-  apiUrl: "http://localhost:8001/api/v1/"
+  apiUrl: process.env.REACT_APP_API_URL || "http://localhost:8001/api/v1/"
 };
 
 export default config;
