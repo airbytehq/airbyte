f5a2bdee4a587781fd3d53d11ec8c3c6db0ae904
Artem Astapenko <jamakase54@gmail.com>
2020-08-27 15:28:23 +0300

    Add main resources for onboarding, add loading and success request states

---
+++ dataline-webapp/src/core/resources/Destination.ts
@@ -0,0 +1,33 @@
+import { Resource } from "rest-hooks";
+import BaseResource from "./BaseResource";
+
+export interface Destination {
+  destinationId: string;
+  name: string;
+}
+
+export default class DestinationResource extends BaseResource
+  implements Destination {
+  readonly destinationId: string = "";
+  readonly name: string = "";
+
+  pk() {
+    return this.destinationId?.toString();
+  }
+
+  static urlRoot = "destinations";
+
+  static listShape<T extends typeof Resource>(this: T) {
+    return {
+      ...super.listShape(),
+      schema: { destinations: [this.asSchema()] }
+    };
+  }
+
+  static detailShape<T extends typeof Resource>(this: T) {
+    return {
+      ...super.detailShape(),
+      schema: this.asSchema()
+    };
+  }
+}
