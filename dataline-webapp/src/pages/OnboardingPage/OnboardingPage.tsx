4fa5b56220c3901bc929fb670058edf15bfd5737
Artem Astapenko <jamakase54@gmail.com>
2020-08-28 17:54:56 +0300

    Edit onboardin file. Create connection request

--- dataline-webapp/src/pages/OnboardingPage/OnboardingPage.tsx
+++ dataline-webapp/src/pages/OnboardingPage/OnboardingPage.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo, useState } from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import { FormattedMessage } from "react-intl";
 import { useFetcher, useResource } from "rest-hooks";
@@ -21,7 +21,11 @@ import SourceResource from "../../core/resources/Source";
 import DestinationResource from "../../core/resources/Destination";
 import SourceImplementationResource from "../../core/resources/SourceImplementation";
 import DestinationImplementationResource from "../../core/resources/DestinationImplementation";
+import ConnectionResource from "../../core/resources/Connection";
 import config from "../../config";
+import StepsConfig, { StepsTypes } from "./components/StepsConfig";
+import PrepareDropDownLists from "./components/PrepareDropDownLists";
+import FrequencyConfig from "../../data/FrequencyConfig.json";
 
 const Content = styled.div`
   width: 100%;
