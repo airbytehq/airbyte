b68840391969ca9f31b8ac2bfc7ab88dcd9ff7c7
Michel Tricot <michel@dataline.io>
2020-08-30 16:59:34 -0700

    sql prettier

--- dataline-db/src/main/resources/schema.sql
+++ dataline-db/src/main/resources/schema.sql
@@ -1,31 +1,55 @@
 -- database
-CREATE DATABASE dataline;
+ CREATE
+    DATABASE dataline;
+
 \connect dataline;
 
 -- extensions
-CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+ CREATE
+    EXTENSION IF NOT EXISTS "uuid-ossp";
 
 -- types
-CREATE TYPE JOB_STATUS AS ENUM ('pending', 'running', 'failed', 'completed', 'cancelled');
+ CREATE
+    TYPE JOB_STATUS AS ENUM(
+        'pending',
+        'running',
+        'failed',
+        'completed',
+        'cancelled'
+    );
 
 -- tables
-CREATE TABLE DATALINE_METADATA (key VARCHAR (255) PRIMARY KEY, value VARCHAR (255));
+ CREATE
+    TABLE
+        DATALINE_METADATA(
+            KEY VARCHAR(255) PRIMARY KEY,
+            value VARCHAR(255)
+        );
 
-CREATE TABLE JOBS (
-    id BIGSERIAL PRIMARY KEY,
-    scope VARCHAR (255),
-    created_at TIMESTAMPTZ,
-    started_at TIMESTAMPTZ,
-    updated_at TIMESTAMPTZ,
-    status JOB_STATUS,
-    config JSONB,
-    output JSONB,
-    stdout_path VARCHAR (255),
-    stderr_path VARCHAR (255)
-);
+CREATE
+    TABLE
+        JOBS(
+            id BIGSERIAL PRIMARY KEY,
+            SCOPE VARCHAR(255),
+            created_at TIMESTAMPTZ,
+            started_at TIMESTAMPTZ,
+            updated_at TIMESTAMPTZ,
+            status JOB_STATUS,
+            config JSONB,
+            OUTPUT JSONB,
+            stdout_path VARCHAR(255),
+            stderr_path VARCHAR(255)
+        );
 
 -- entries
-INSERT INTO DATALINE_METADATA VALUES  ('server-uuid', uuid_generate_v4());
+ INSERT
+    INTO
+        DATALINE_METADATA
+    VALUES(
+        'server-uuid',
+        uuid_generate_v4()
+    );
 
 -- grants
-GRANT ALL ON DATABASE dataline TO docker;
+ GRANT ALL ON
+DATABASE dataline TO docker;
