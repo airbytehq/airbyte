10f4bdac8aa103c9d773312e25e63c5fe385d5e2
Sherif Nada <snadalive@gmail.com>
2020-08-31 16:32:44 -0700

    add acceptance tests

--- dataline-acceptance-tests/src/test/java/io/dataline/tests/acceptance/AcceptanceTests.java
+++ dataline-acceptance-tests/src/test/java/io/dataline/tests/acceptance/AcceptanceTests.java
@@ -3,23 +3,20 @@
  *
  * Copyright (c) 2020 Dataline
  *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+ * associated documentation files (the "Software"), to deal in the Software without restriction,
+ * including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ * sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
  *
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
+ * The above copyright notice and this permission notice shall be included in all copies or
+ * substantial portions of the Software.
  *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
+ * NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 
 package io.dataline.tests.acceptance;
@@ -82,8 +79,7 @@ public class AcceptanceTests {
             .workspaceId(defaultWorkspaceId)
             .connectionConfiguration(dbConfiguration);
 
-    SourceImplementationRead createResponse =
-        callApi("source_implementations/create", sourceImplementationCreate, SourceImplementationRead.class);
+    SourceImplementationRead createResponse = callApi("source_implementations/create", sourceImplementationCreate, SourceImplementationRead.class);
 
     assertEquals(defaultWorkspaceId, createResponse.getWorkspaceId());
     assertEquals(sourceSpecificationId, createResponse.getSourceSpecificationId());
@@ -99,10 +95,7 @@ public class AcceptanceTests {
         .getSourceId();
   }
 
-  private <Input, Output> Output callApi(
-                                         String relativePath,
-                                         Input requestBody,
-                                         Class<Output> outputClass)
+  private <Input, Output> Output callApi(String relativePath, Input requestBody, Class<Output> outputClass)
       throws IOException {
     RequestBody body = RequestBody.create(Jsons.serialize(requestBody), JSON_CONTENT);
     Request request = new Request.Builder().post(body).url(SERVER_URL + relativePath).build();
