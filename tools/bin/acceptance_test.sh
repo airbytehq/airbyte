7424ac3bbb3b4cc4ac56f28071bbccd8cd5be257
Sherif A. Nada <snadalive@gmail.com>
2020-09-01 14:44:47 -0700

    Add initial acceptance test structure and first test (#150)

---
+++ tools/bin/acceptance_test.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+set -e
+
+. tools/lib/lib.sh
+
+assert_root
+
+echo "Starting app..."
+
+# Detach so we can run subsequent commands
+VERSION=dev docker-compose up -d
+trap "docker-compose down" EXIT
+
+echo "Waiting for services to begin"
+sleep 10 # TODO need a better way to wait
+
+echo "Running e2e tests via gradle"
+./gradlew :dataline-tests:acceptanceTests
