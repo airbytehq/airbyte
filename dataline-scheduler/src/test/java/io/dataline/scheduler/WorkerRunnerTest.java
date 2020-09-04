8f62367344c1ba3041ce16f54b5c3a2da261a3f4
Michel Tricot <michel@dataline.io>
2020-09-04 10:01:50 -0700

    progress

---
+++ dataline-scheduler/src/test/java/io/dataline/scheduler/WorkerRunnerTest.java
@@ -0,0 +1,62 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2020 Dataline
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package io.dataline.scheduler;
+
+import io.dataline.scheduler.persistence.SchedulerPersistence;
+import io.dataline.workers.process.ProcessBuilderFactory;
+import java.io.IOException;
+import java.nio.file.Path;
+import org.apache.commons.dbcp2.BasicDataSource;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+class WorkerRunnerTest {
+
+  @Mock
+  private BasicDataSource connectionPool;
+  @Mock
+  private SchedulerPersistence persistence;
+  @Mock
+  private Path root;
+  @Mock
+  ProcessBuilderFactory pbf;
+  @Mock
+  private WorkerRun.Factory runFactory;
+
+  @BeforeEach
+  void setUp() {}
+
+  @Test
+  void testSimple() throws IOException {
+    WorkerRunner runner = new WorkerRunner(1L, connectionPool, persistence, root, pbf, runFactory);
+
+    runner.voidCall();
+  }
+
+}
