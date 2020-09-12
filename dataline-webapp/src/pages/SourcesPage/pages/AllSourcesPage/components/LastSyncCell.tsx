a1853a52b58a3088b280d89af8d6da3dd9926b8f
Michel Tricot <michel@dataline.io>
2020-09-12 01:53:33 -0700

    revert style

--- dataline-webapp/src/pages/SourcesPage/pages/AllSourcesPage/components/LastSyncCell.tsx
+++ dataline-webapp/src/pages/SourcesPage/pages/AllSourcesPage/components/LastSyncCell.tsx
@@ -5,32 +5,35 @@ import { faCalendarAlt } from "@fortawesome/free-regular-svg-icons";
 import { FormattedRelativeTime } from "react-intl";
 
 const CalendarIcon = styled(FontAwesomeIcon)`
-    color: ${({ theme }) => theme.greyColor40};
-    font-size: 14px;
-    line-height: 14px;
-    margin-right: 5px;
+  color: ${({ theme }) => theme.greyColor40};
+  font-size: 14px;
+  line-height: 14px;
+  margin-right: 5px;
 `;
 
 const Content = styled.div<{ enabled?: boolean }>`
-    color: ${({ theme, enabled }) => (!enabled ? theme.greyColor40 : "inheret")};
+  color: ${({ theme, enabled }) => (!enabled ? theme.greyColor40 : "inheret")};
 `;
 
 type IProps = {
-    valueInSecond: number;
-    enabled?: boolean;
+  valueInSecond: number;
+  enabled?: boolean;
 };
 
 const LastSyncCell: React.FC<IProps> = ({ valueInSecond, enabled }) => {
-    if (!valueInSecond) {
-        return null;
-    }
+  if (!valueInSecond) {
+    return null;
+  }
 
-    return (
-        <Content enabled={enabled}>
-            <CalendarIcon icon={faCalendarAlt} />
-            <FormattedRelativeTime value={valueInSecond - Date.now() / 1000} updateIntervalInSeconds={60} />
-        </Content>
-    );
+  return (
+    <Content enabled={enabled}>
+      <CalendarIcon icon={faCalendarAlt} />
+      <FormattedRelativeTime
+        value={valueInSecond - Date.now() / 1000}
+        updateIntervalInSeconds={60}
+      />
+    </Content>
+  );
 };
 
 export default LastSyncCell;
