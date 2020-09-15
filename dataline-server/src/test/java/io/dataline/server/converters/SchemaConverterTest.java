39a0eb65e909c16e20d8ffd4940462e420a8a665
cgardens <giardina.charles@gmail.com>
2020-09-14 17:25:05 -0700

    add SchemaConverter unit tests

--- dataline-server/src/test/java/io/dataline/server/converters/SchemaConverterTest.java
+++ dataline-server/src/test/java/io/dataline/server/converters/SchemaConverterTest.java
@@ -26,11 +26,45 @@ package io.dataline.server.converters;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import com.google.common.collect.Lists;
+import io.dataline.api.model.SourceSchema;
+import io.dataline.api.model.SourceSchemaColumn;
+import io.dataline.api.model.SourceSchemaTable;
 import io.dataline.commons.enums.Enums;
+import io.dataline.config.Column;
 import io.dataline.config.DataType;
+import io.dataline.config.Schema;
+import io.dataline.config.Table;
 import org.junit.jupiter.api.Test;
 
 class SchemaConverterTest {
+
+  private static final Schema SCHEMA = new Schema()
+      .withTables(Lists.newArrayList(new Table()
+          .withName("users")
+          .withSelected(true)
+          .withColumns(Lists.newArrayList(new Column()
+              .withDataType(DataType.STRING)
+              .withName("id")
+              .withSelected(true)))));
+
+  private static final SourceSchema API_SCHEMA = new SourceSchema()
+      .tables(Lists.newArrayList(new SourceSchemaTable()
+      .name("users")
+      .columns(Lists.newArrayList(new SourceSchemaColumn()
+          .dataType(io.dataline.api.model.DataType.STRING)
+          .name("id")
+          .selected(true)))));
+
+  @Test
+  void convertToPersistenceSchema() {
+    assertEquals(SCHEMA, SchemaConverter.toPersistenceSchema(API_SCHEMA));
+  }
+
+  @Test
+  void convertToApiSchema() {
+    assertEquals(API_SCHEMA, SchemaConverter.toApiSchema(SCHEMA));
+  }
 
   @Test
   void testEnumConversion() {
