0b538eb0267a2332bb6c890be2cc92c744958ff1
Sherif Nada <snadalive@gmail.com>
2020-09-16 14:05:23 -0700

    remove ineffective newlines

--- tools/bin/acceptance_test.sh
+++ tools/bin/acceptance_test.sh
@@ -10,7 +10,7 @@ echo "Starting app..."
 
 # Detach so we can run subsequent commands
 VERSION=dev docker-compose up -d
-trap "echo 'docker-compose logs: \n\n' && docker-compose logs -t --tail 150 && docker-compose down" EXIT
+trap "echo 'docker-compose logs:' && docker-compose logs -t --tail 150 && docker-compose down" EXIT
 
 echo "Waiting for services to begin"
 sleep 10 # TODO need a better way to wait
