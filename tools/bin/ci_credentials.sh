5e148cbd5b3403106ac595e8f73c9c539ef5c83c
jrhizor <me@jaredrhizor.com>
2020-09-17 10:16:01 -0700

    use a single script for writing credentials

---
+++ tools/bin/ci_credentials.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+set -e
+
+mkdir dataline-integrations/singer/bigquery/destination/config
+echo "$BIGQUERY_INTEGRATION_TEST_CREDS" > dataline-integrations/singer/bigquery/destination/config/credentials.json
+
+mkdir dataline-integrations/singer/stripe/source/config
+echo "$STRIPE_INTEGRATION_TEST_CREDS" > dataline-integrations/singer/stripe/source/config/source.json
