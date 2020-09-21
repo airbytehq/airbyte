6b888623a0c1a4c9fbd44e7635dd2234584e3692
Sherif Nada <snadalive@gmail.com>
2020-09-21 15:42:32 -0700

    rename

--- airbyte-integrations/singer/bigquery/destination/bigquery-destination.sh
+++ airbyte-integrations/singer/bigquery/destination/bigquery-destination.sh
@@ -8,9 +8,9 @@ case "$1" in
 esac
 
 # Set default_target_schema to the value of dataset_id
-PRE_PROCESSED_CONFIG="preprocessed_config.json"
-mv "$CONFIG_FILE" "$PRE_PROCESSED_CONFIG"
-jq '.default_target_schema = .dataset_id' "$PRE_PROCESSED_CONFIG" > "$CONFIG_FILE"
+ORIGINAL_CONFIG_FILE="original_config.json"
+mv "$CONFIG_FILE" "$ORIGINAL_CONFIG_FILE"
+jq '.default_target_schema = .dataset_id' "$ORIGINAL_CONFIG_FILE" > "$CONFIG_FILE"
 
 cat "$CONFIG_FILE" | jq '.credentials_json | fromjson' > credentials.json
 GOOGLE_APPLICATION_CREDENTIALS=credentials.json target-bigquery "$@"
