653ca73bd4124b0fb3e151fd73616f8bac154d20
Sherif Nada <snadalive@gmail.com>
2020-09-21 15:31:22 -0700

    remove default_target_schema

--- airbyte-integrations/singer/bigquery/destination/bigquery-destination.sh
+++ airbyte-integrations/singer/bigquery/destination/bigquery-destination.sh
@@ -7,5 +7,10 @@ case "$1" in
   *)                 shift; break             ;;
 esac
 
-cat $CONFIG_FILE | jq '.credentials_json | fromjson' > credentials.json
+# Set default_target_schema to the value of dataset_id
+PRE_PROCESSED_CONFIG="preprocessed_config.json"
+mv "$CONFIG_FILE" "$PRE_PROCESSED_CONFIG"
+jq '.default_target_schema = .dataset_id' "$PRE_PROCESSED_CONFIG" > "$CONFIG_FILE"
+
+cat "$CONFIG_FILE" | jq '.credentials_json | fromjson' > credentials.json
 GOOGLE_APPLICATION_CREDENTIALS=credentials.json target-bigquery "$@"
