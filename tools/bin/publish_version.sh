126850fd9d0c4b34b75e6121087770df06b4ef98
jrhizor <me@jaredrhizor.com>
2020-09-22 22:36:40 -0700

    add deploy script

---
+++ tools/bin/publish_version.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+set -e
+
+. tools/lib/lib.sh
+
+VERSION=$(cat .env | grep VERSION= | cut -d= -f 2)
+[[ -z "$VERSION" ]] && echo "Couldn't find version in env file..." && exit 1
+
+GIT_REVISION=$(git rev-parse HEAD)
+[[ -z "$GIT_REVISION" ]] && echo "Couldn't get the git revision..." && exit 1
+
+[[ ! -z "$(git status --porcelain)" ]] && echo "Cannot tag revision if all changes aren't checked in..." && exit 1
+
+echo "Building and publishing version $VERSION for git revision $GIT_REVISION..."
+GIT_REVISION=$GIT_REVISION docker-compose -f docker-compose.build.yaml -f docker-compose.yaml build
+GIT_REVISION=$GIT_REVISION docker-compose -f docker-compose.build.yaml -f docker-compose.yaml push
+echo "Completed building and publishing..."
+
+echo "Tagging git revision..."
+git tag -a v"$VERSION" -m "Version $VERSION"
+echo "Completed.."
