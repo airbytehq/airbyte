d918cddb2e2b48ce85c6b3f90fbc206b245cd119
jrhizor <me@jaredrhizor.com>
2020-09-29 13:30:37 -0700

    misc

--- airbyte-integrations/singer/exchangeratesapi_io/source/source_implementation.py
+++ airbyte-integrations/singer/exchangeratesapi_io/source/source_implementation.py
@@ -10,3 +10,21 @@ class SourceImplementation(Source):
     def spec(self) -> AirbyteSpec:
         print("custom value")
         pass
+
+    def read_config(self):
+            raise Exception("invalid")
+
+    def check(self, logger, config) -> AirbyteCheckResponse:
+        raise Exception("invalid")
+
+    def discover(self, logger, config) -> AirbyteSchema:
+        raise Exception("invalid")
+
+    # todo: should the config still be the filename or rendered filename or the actual string?
+    # should each read write that to a temp file?
+    # https://docs.python.org/3/library/tempfile.html
+    def read(self, logger, config, state=None):
+        # process=subprocess.Popen([PathToProcess],stdin=subprocess.PIPE,stdout=subprocess.PIPE);
+        # process.stdin.write("\n")
+        raise Exception("invalid")
+
