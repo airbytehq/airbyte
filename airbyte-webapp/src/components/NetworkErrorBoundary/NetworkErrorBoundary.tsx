3cbae61f9621a743414be78dac3db391df8f5682
Artem Astapenko <3767150+Jamakase@users.noreply.github.com>
2020-10-02 11:16:07 +0300

    Indicate server is starting up (#475)
    * Indicate that the server is still starting up
    
    * Check error message

---
+++ airbyte-webapp/src/components/NetworkErrorBoundary/NetworkErrorBoundary.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+import ServerIsStarting from "./components/ServerIsStarting";
+
+class NetworkErrorBoundary extends React.Component<
+  {},
+  { unReachServer: boolean }
+> {
+  constructor(props: Object) {
+    super(props);
+    this.state = { unReachServer: false };
+  }
+
+  static getDerivedStateFromError(error: { message: string; status?: number }) {
+    // Update state so the next render will show the fallback UI.
+    return { unReachServer: error.message === "Failed to fetch" };
+  }
+
+  componentDidCatch() {}
+
+  render() {
+    if (this.state.unReachServer) {
+      return <ServerIsStarting />;
+    }
+
+    return this.props.children;
+  }
+}
+
+export default NetworkErrorBoundary;
