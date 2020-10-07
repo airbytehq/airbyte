f8fb54bc8c6920ccfb76d69dc8c78cde7c5f0d54
Charles <giardina.charles@gmail.com>
2020-10-07 15:57:46 -0700

    use the word catalog instead of schema in javabase (#515)

--- airbyte-integrations/base-java/src/test/java/io/airbyte/integrations/base/IntegrationConfigTest.java
+++ airbyte-integrations/base-java/src/test/java/io/airbyte/integrations/base/IntegrationConfigTest.java
@@ -34,7 +34,7 @@ import org.junit.jupiter.api.Test;
 class IntegrationConfigTest {
 
   private static final Path CONFIG_PATH = Path.of("config.json");
-  private static final Path SCHEMA_PATH = Path.of("schema.json");
+  private static final Path CATALOG_PATH = Path.of("catalog.json");
   private static final Path STATE_PATH = Path.of("state.json");
 
   @Test
@@ -42,7 +42,7 @@ class IntegrationConfigTest {
     final IntegrationConfig config = IntegrationConfig.spec();
     assertEquals(Command.SPEC, config.getCommand());
     assertThrows(IllegalStateException.class, config::getConfigPath);
-    assertThrows(IllegalStateException.class, config::getSchemaPath);
+    assertThrows(IllegalStateException.class, config::getCatalogPath);
     assertThrows(IllegalStateException.class, config::getStatePath);
   }
 
@@ -53,7 +53,7 @@ class IntegrationConfigTest {
     final IntegrationConfig config = IntegrationConfig.check(CONFIG_PATH);
     assertEquals(Command.CHECK, config.getCommand());
     assertEquals(CONFIG_PATH, config.getConfigPath());
-    assertThrows(IllegalStateException.class, config::getSchemaPath);
+    assertThrows(IllegalStateException.class, config::getCatalogPath);
     assertThrows(IllegalStateException.class, config::getStatePath);
   }
 
@@ -64,43 +64,43 @@ class IntegrationConfigTest {
     final IntegrationConfig config = IntegrationConfig.discover(CONFIG_PATH);
     assertEquals(Command.DISCOVER, config.getCommand());
     assertEquals(CONFIG_PATH, config.getConfigPath());
-    assertThrows(IllegalStateException.class, config::getSchemaPath);
+    assertThrows(IllegalStateException.class, config::getCatalogPath);
     assertThrows(IllegalStateException.class, config::getStatePath);
   }
 
   @Test
   void testWrite() {
-    assertThrows(NullPointerException.class, () -> IntegrationConfig.write(null, SCHEMA_PATH));
+    assertThrows(NullPointerException.class, () -> IntegrationConfig.write(null, CATALOG_PATH));
     assertThrows(NullPointerException.class, () -> IntegrationConfig.write(CONFIG_PATH, null));
 
-    final IntegrationConfig config = IntegrationConfig.write(CONFIG_PATH, SCHEMA_PATH);
+    final IntegrationConfig config = IntegrationConfig.write(CONFIG_PATH, CATALOG_PATH);
     assertEquals(Command.WRITE, config.getCommand());
     assertEquals(CONFIG_PATH, config.getConfigPath());
-    assertEquals(SCHEMA_PATH, config.getSchemaPath());
+    assertEquals(CATALOG_PATH, config.getCatalogPath());
     assertThrows(IllegalStateException.class, config::getStatePath);
   }
 
   @Test
   void testReadWithState() {
-    assertThrows(NullPointerException.class, () -> IntegrationConfig.read(null, SCHEMA_PATH, STATE_PATH));
+    assertThrows(NullPointerException.class, () -> IntegrationConfig.read(null, CATALOG_PATH, STATE_PATH));
     assertThrows(NullPointerException.class, () -> IntegrationConfig.read(CONFIG_PATH, null, STATE_PATH));
 
-    final IntegrationConfig config = IntegrationConfig.read(CONFIG_PATH, SCHEMA_PATH, STATE_PATH);
+    final IntegrationConfig config = IntegrationConfig.read(CONFIG_PATH, CATALOG_PATH, STATE_PATH);
     assertEquals(Command.READ, config.getCommand());
     assertEquals(CONFIG_PATH, config.getConfigPath());
-    assertEquals(SCHEMA_PATH, config.getSchemaPath());
+    assertEquals(CATALOG_PATH, config.getCatalogPath());
     assertEquals(Optional.of(STATE_PATH), config.getStatePath());
   }
 
   @Test
   void testReadWithoutState() {
-    assertThrows(NullPointerException.class, () -> IntegrationConfig.read(null, SCHEMA_PATH, null));
+    assertThrows(NullPointerException.class, () -> IntegrationConfig.read(null, CATALOG_PATH, null));
     assertThrows(NullPointerException.class, () -> IntegrationConfig.read(CONFIG_PATH, null, null));
 
-    final IntegrationConfig config = IntegrationConfig.read(CONFIG_PATH, SCHEMA_PATH, null);
+    final IntegrationConfig config = IntegrationConfig.read(CONFIG_PATH, CATALOG_PATH, null);
     assertEquals(Command.READ, config.getCommand());
     assertEquals(CONFIG_PATH, config.getConfigPath());
-    assertEquals(SCHEMA_PATH, config.getSchemaPath());
+    assertEquals(CATALOG_PATH, config.getCatalogPath());
     assertEquals(Optional.empty(), config.getStatePath());
   }
 
