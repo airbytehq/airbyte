52041c5878ae09644dd36cc7dd590fd114db87ce
cgardens <giardina.charles@gmail.com>
2020-10-08 12:01:11 -0700

    destination integration test suite (base)

---
+++ airbyte-integrations/integration-test-lib/src/main/java/io/airbyte/integrations/base/TestDestination.java
@@ -0,0 +1,243 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2020 Airbyte
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package io.airbyte.integrations.base;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.Lists;
+import io.airbyte.commons.json.Jsons;
+import io.airbyte.commons.lang.Exceptions;
+import io.airbyte.commons.resources.MoreResources;
+import io.airbyte.config.DestinationConnectionImplementation;
+import io.airbyte.config.Schema;
+import io.airbyte.config.StandardSync;
+import io.airbyte.config.StandardTargetConfig;
+import io.airbyte.singer.SingerMessage;
+import io.airbyte.singer.SingerMessage.Type;
+import io.airbyte.workers.WorkerException;
+import io.airbyte.workers.process.DockerProcessBuilderFactory;
+import io.airbyte.workers.process.ProcessBuilderFactory;
+import io.airbyte.workers.process.SingerIntegrationLauncher;
+import io.airbyte.workers.protocols.singer.DefaultSingerDestination;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtensionContext;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.ArgumentsProvider;
+import org.junit.jupiter.params.provider.ArgumentsSource;
+
+public abstract class TestDestination {
+
+  private TestDestinationEnv testEnv;
+
+  private Path jobRoot;
+  protected Path localRoot;
+  private ProcessBuilderFactory pbf;
+
+  /**
+   * Name of the docker image that the tests will run against.
+   *
+   * @return docker image name
+   */
+  protected abstract String getImageName();
+
+  /**
+   * Configuration specific to the integration. Will be passed to integration where appropriate in
+   * each test. Should be valid.
+   *
+   * @return integration-specific configuration
+   */
+  protected abstract JsonNode getConfig();
+
+  /**
+   * Configuration specific to the integration. Will be passed to integration where appropriate in
+   * tests that test behavior when configuration is invalid. e.g incorrect password. Should be
+   * invalid.
+   *
+   * @return integration-specific configuration
+   */
+  protected abstract JsonNode getFailCheckConfig();
+
+  /**
+   * Function that returns all of the records in destination as json at the time this method is
+   * invoked. These will be used to check that the data actually written is what should actually be
+   * there.
+   *
+   * @param testEnv - information about the test environment.
+   * @return All of the records in the destination at the time this method is invoked.
+   * @throws Exception - can throw any exception, test framework will handle.
+   */
+  protected abstract List<JsonNode> retrieveRecords(TestDestinationEnv testEnv) throws Exception;
+
+  /**
+   * Function that performs any setup of external resources required for the test. e.g. instantiate a
+   * postgres database. This function will be called before EACH test.
+   *
+   * @param testEnv - information about the test environment.
+   * @throws Exception - can throw any exception, test framework will handle.
+   */
+  protected abstract void setup(TestDestinationEnv testEnv) throws Exception;
+
+  /**
+   * Function that performs any clean up of external resources required for the test. e.g. delete a
+   * postgres database. This function will be called after EACH test. It MUST remove all data in the
+   * destination so that there is no contamination across tests.
+   *
+   * @param testEnv - information about the test environment.
+   * @throws Exception - can throw any exception, test framework will handle.
+   */
+  protected abstract void tearDown(TestDestinationEnv testEnv) throws Exception;
+
+  @BeforeEach
+  void setUpInternal() throws Exception {
+    final Path workspaceRoot = Files.createTempDirectory("test");
+    jobRoot = Files.createDirectories(Path.of(workspaceRoot.toString(), "job"));
+    localRoot = Files.createTempDirectory("output");
+    testEnv = new TestDestinationEnv(localRoot);
+
+    setup(testEnv);
+
+    pbf = new DockerProcessBuilderFactory(workspaceRoot, workspaceRoot.toString(), localRoot.toString(), "host");
+  }
+
+  @AfterEach
+  void tearDownInternal() throws Exception {
+    tearDown(testEnv);
+  }
+
+  /**
+   * Verify that when given valid credentials, that check connection returns a success response.
+   * Assume that the {@link TestDestination#getConfig()} is valid.
+   */
+  @Test
+  void testCheckConnection() {
+    // todo (cgardens) - blocked on worker not calling discover worker.
+  }
+
+  /**
+   * Verify that when given invalid credentials, that check connection returns a failed response.
+   * Assume that the {@link TestDestination#getFailCheckConfig()} is invalid.
+   */
+  @Test
+  void testCheckConnectionInvalidCredentials() {
+    // todo (cgardens) - blocked on worker not calling discover worker.
+  }
+
+  private static class DataArgumentsProvider implements ArgumentsProvider {
+
+    @Override
+    public Stream<? extends Arguments> provideArguments(ExtensionContext context) {
+      return Stream.of(
+          Arguments.of("exchange_rate_messages.txt", "exchange_rate_catalog.json")
+      // todo - need to use the new protocol to capture this.
+      // Arguments.of("stripe_messages.txt", "stripe_schema.json")
+      );
+    }
+
+  }
+
+  /**
+   * Verify that the integration successfully writes records. Tests a wide variety of messages and
+   * schemas (aspirationally, anyway).
+   */
+  @ParameterizedTest
+  @ArgumentsSource(DataArgumentsProvider.class)
+  void testSync(String messagesFilename, String catalogFilename) throws Exception {
+    final Schema catalog = Jsons.deserialize(MoreResources.readResource(catalogFilename), Schema.class);
+    final List<SingerMessage> messages = MoreResources.readResource(messagesFilename).lines()
+        .map(record -> Jsons.deserialize(record, SingerMessage.class)).collect(Collectors.toList());
+    runSync(messages, catalog);
+
+    assertSameMessages(messages, retrieveRecords(testEnv));
+  }
+
+  /**
+   * Verify that the integration overwrites the first sync with the second sync.
+   */
+  @Test
+  void testSecondSync() throws Exception {
+    final Schema catalog = Jsons.deserialize(MoreResources.readResource("exchange_rate_catalog.json"), Schema.class);
+    final List<SingerMessage> firstSyncMessages = MoreResources.readResource("exchange_rate_messages.txt").lines()
+        .map(record -> Jsons.deserialize(record, SingerMessage.class)).collect(Collectors.toList());
+    runSync(firstSyncMessages, catalog);
+
+    List<SingerMessage> secondSyncMessages = Lists.newArrayList(new SingerMessage()
+        .withStream(catalog.getStreams().get(0).getName())
+        .withRecord(Jsons.jsonNode(ImmutableMap.builder()
+            .put("date", "2020-03-31T00:00:00Z")
+            .put("HKD", 10)
+            .put("NZD", 700)
+            .build())));
+    runSync(secondSyncMessages, catalog);
+    assertSameMessages(secondSyncMessages, retrieveRecords(testEnv));
+  }
+
+  private void runSync(List<SingerMessage> messages, Schema catalog) throws IOException, WorkerException {
+    final StandardTargetConfig targetConfig = new StandardTargetConfig()
+        .withDestinationConnectionImplementation(new DestinationConnectionImplementation().withConfiguration(getConfig()))
+        .withStandardSync(new StandardSync().withSchema(catalog));
+
+    new SingerIntegrationLauncher(getImageName(), pbf);
+    final DefaultSingerDestination target = new DefaultSingerDestination(new SingerIntegrationLauncher(getImageName(), pbf));
+
+    target.start(targetConfig, jobRoot);
+    messages.forEach(message -> Exceptions.toRuntime(() -> target.accept(message)));
+    target.notifyEndOfStream();
+    target.close();
+  }
+
+  private void assertSameMessages(List<SingerMessage> expected, List<JsonNode> actual) {
+    final List<JsonNode> expectedJson = expected.stream()
+        .filter(message -> message.getType() == Type.RECORD)
+        .map(SingerMessage::getRecord)
+        .collect(Collectors.toList());
+    assertEquals(expectedJson, actual);
+  }
+
+  public static class TestDestinationEnv {
+
+    private final Path localRoot;
+
+    public TestDestinationEnv(Path localRoot) {
+      this.localRoot = localRoot;
+    }
+
+    public Path getLocalRoot() {
+      return localRoot;
+    }
+
+  }
+
+}
