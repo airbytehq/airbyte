14f50da2210dceb18d08e31747656723657cb667
Sherif Nada <snadalive@gmail.com>
2020-10-12 13:22:23 -0700

    imports

--- airbyte-server/src/main/java/io/airbyte/server/handlers/SourcesHandler.java
+++ airbyte-server/src/main/java/io/airbyte/server/handlers/SourcesHandler.java
@@ -35,19 +35,16 @@ import io.airbyte.config.StandardSource;
 import io.airbyte.config.persistence.ConfigNotFoundException;
 import io.airbyte.config.persistence.ConfigRepository;
 import io.airbyte.server.errors.KnownException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.List;
-import java.util.Optional;
 import java.util.UUID;
 import java.util.function.Supplier;
 import java.util.stream.Collectors;
-import javax.swing.*;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class SourcesHandler {
 
