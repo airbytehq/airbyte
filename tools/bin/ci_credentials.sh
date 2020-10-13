5d3fbccfde2fdebab47a869b20fd8e841e8e579d
cgardens <giardina.charles@gmail.com>
2020-10-13 15:35:22 -0700

    hail mary

--- tools/bin/ci_credentials.sh
+++ tools/bin/ci_credentials.sh
@@ -5,5 +5,8 @@ set -e
 mkdir airbyte-integrations/singer/bigquery/destination/config
 echo "$BIGQUERY_INTEGRATION_TEST_CREDS" > airbyte-integrations/singer/bigquery/destination/config/credentials.json
 
+mkdir config
+echo "$BIGQUERY_INTEGRATION_TEST_CREDS" > config/credentials.json
+
 mkdir airbyte-integrations/singer/stripe/source/config
 echo "$STRIPE_INTEGRATION_TEST_CREDS" > airbyte-integrations/singer/stripe/source/config/config.json
