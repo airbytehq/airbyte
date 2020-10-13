bbe34ad681e8e5dbafa5c1c95c04dc7e123653b8
jrhizor <me@jaredrhizor.com>
2020-10-13 11:28:31 -0700

    partially working mapping (in wrong location)

--- airbyte-integrations/singer/stripe_abprotocol/source/source_stripe_singer/source_stripe_singer.py
+++ airbyte-integrations/singer/stripe_abprotocol/source/source_stripe_singer/source_stripe_singer.py
@@ -7,6 +7,15 @@ import requests
 from typing import Generator
 from base_singer import SingerHelper
 import sys
+import json
+
+
+def is_field_metadata(metadata):
+    if len(metadata.get("breadcrumb")) != 2:
+        return False
+    else:
+        return metadata.get("breadcrumb")[0] != "property"
+
 
 class SourceStripeSinger(Source):
     def __init__(self):
@@ -26,7 +35,45 @@ class SourceStripeSinger(Source):
         return catalogs.airbyte_catalog
 
     def read(self, logger, config_container, catalog_path, state=None) -> Generator[AirbyteMessage, None, None]:
+        masked_airbyte_catalog = self.read_config(catalog_path)
+        discovered_singer_catalog = SingerHelper.get_catalogs(logger, f"tap-stripe --config {config_container.rendered_config_path} --discover").singer_catalog
+
+        # todo: combine discovered singer catalog with the generated airbyte catalog
+
+        combined_catalog_path = "/mount/rendered_catalog.json"
+        masked_singer_streams = []
+
+        stream_to_airbyte_schema = {}
+        for stream in masked_airbyte_catalog["streams"]:
+            stream_to_airbyte_schema[stream.get("name")] = stream
+
+        for singer_stream in discovered_singer_catalog.get("streams"):
+            print("stream", singer_stream.get("stream"))
+            if singer_stream.get("stream") in stream_to_airbyte_schema:
+                new_metadatas = []
+                metadatas = singer_stream.get("metadata")
+                for metadata in metadatas:
+                    new_metadata = metadata
+                    new_metadata["metadata"]["selected"] = True
+                    if not is_field_metadata(new_metadata):
+                        new_metadata["metadata"]["replication-method"] = "FULL_TABLE"
+                    new_metadatas += [new_metadata]
+                singer_stream["metadata"] = new_metadatas
+
+            masked_singer_streams += [singer_stream]
+
+        combined_catalog = { "streams": masked_singer_streams}
+        with open(combined_catalog_path, 'w') as fh:
+            fh.write(json.dumps(combined_catalog))
+
+        # print("discovered_singer")
+        # print(json.dumps(discovered_singer_catalog))
+        print("combined_catalog")
+        print(json.dumps(combined_catalog))
+
+        # todo: figure out how to make this easier to consume for new implementers
+
         if state:
-            return SingerHelper.read(logger, f"tap-stripe --config {config_container.rendered_config_path} --state {state}")
+            return SingerHelper.read(logger, f"tap-stripe --config {config_container.rendered_config_path} --properties {combined_catalog_path} --state {state}")
         else:
-            return SingerHelper.read(logger, f"tap-stripe --config {config_container.rendered_config_path}")
+            return SingerHelper.read(logger, f"tap-stripe --config {config_container.rendered_config_path} --properties {combined_catalog_path}")
