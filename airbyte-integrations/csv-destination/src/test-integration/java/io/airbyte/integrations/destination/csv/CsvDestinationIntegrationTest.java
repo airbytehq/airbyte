3438bb63be1e3b329b8bdefa49542da78ce7bc04
Charles <giardina.charles@gmail.com>
2020-10-13 10:40:34 -0700

    Destination Integration Test Suite (base) (#524)

---
+++ airbyte-integrations/csv-destination/src/test-integration/java/io/airbyte/integrations/destination/csv/CsvDestinationIntegrationTest.java
@@ -0,0 +1,89 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2020 Airbyte
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package io.airbyte.integrations.destination.csv;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.google.common.collect.ImmutableMap;
+import io.airbyte.commons.json.Jsons;
+import io.airbyte.integrations.base.TestDestination;
+import java.io.FileReader;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVRecord;
+
+public class CsvDestinationIntegrationTest extends TestDestination {
+
+  private static final String COLUMN_NAME = "data";
+  private static final Path RELATIVE_PATH = Path.of("integration_test/test");
+
+  @Override
+  protected String getImageName() {
+    return "airbyte/airbyte-csv-destination:dev";
+  }
+
+  @Override
+  protected JsonNode getConfig() {
+    return Jsons.jsonNode(ImmutableMap.of("destination_path", Path.of("/local").resolve(RELATIVE_PATH).toString()));
+  }
+
+  @Override
+  protected JsonNode getFailCheckConfig() {
+    return Jsons.jsonNode(ImmutableMap.of("destination_path", Path.of("/@").resolve(RELATIVE_PATH).toString()));
+  }
+
+  @Override
+  protected List<JsonNode> retrieveRecords(TestDestinationEnv testEnv) throws Exception {
+    final List<Path> list = Files.list(testEnv.getLocalRoot().resolve(RELATIVE_PATH)).collect(Collectors.toList());
+    // todo (cgardens) - this should be here. add a retrieve tables abstract method to verify this.
+    assertEquals(1, list.size());
+
+    final FileReader in = new FileReader(list.get(0).toFile());
+    final Iterable<CSVRecord> records = CSVFormat.DEFAULT
+        .withHeader(COLUMN_NAME)
+        .withFirstRecordAsHeader()
+        .parse(in);
+
+    return StreamSupport.stream(records.spliterator(), false)
+        .map(record -> Jsons.deserialize(record.toMap().get(COLUMN_NAME)))
+        .collect(Collectors.toList());
+  }
+
+  @Override
+  protected void setup(TestDestinationEnv testEnv) throws Exception {
+    // no op
+  }
+
+  @Override
+  protected void tearDown(TestDestinationEnv testEnv) throws Exception {
+    // no op
+  }
+
+}
