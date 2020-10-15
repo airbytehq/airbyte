49ecf2d632469cdc21b7c217207b6d35e6be95f9
cgardens <giardina.charles@gmail.com>
2020-10-15 16:53:37 -0700

    code review feedback

--- airbyte-config/init/scripts/create_mount_directories.sh
+++ airbyte-config/init/scripts/create_mount_directories.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env sh
 
+set -e
+
 # hack: attempt to get local mounts to work properly
 # constraints: ROOT_PARENT better exist on the local filesystem.
 # check that the given directory (ROOT) that we plan to use as a mount
@@ -11,17 +13,11 @@
 # We create MOUNT_ROOT which will look like /local_parent/airbyte_local.
 # Because it is using the mount name, we can create it on the local
 # fileystem from within the container.
-MOUNT=$1
-ROOT_PARENT=$2
-ROOT=$3
-echo mount
-echo $MOUNT
-echo root parent
-echo ${ROOT_PARENT};
-echo root
-echo ${ROOT};
-[[ "${ROOT}"="${ROOT_PARENT}"* ]] || echo \"ROOT ${ROOT} is not a child of ROOT_PARENT ${ROOT_PARENT}.\"
-MOUNT_ROOT=${MOUNT}/$(echo $ROOT | sed -e "s|${ROOT_PARENT}||g");
-echo mount root
-echo ${MOUNT_ROOT};
-mkdir -p ${MOUNT_ROOT};
+MOUNT=$1; echo "MOUNT: $MOUNT"
+ROOT_PARENT=$2; echo "ROOT_PARENT: $ROOT_PARENT"
+ROOT=$3; echo "ROOT: $ROOT"
+
+[[ "${ROOT}"="${ROOT_PARENT}"* ]] || (echo "ROOT ${ROOT} is not a child of ROOT_PARENT ${ROOT_PARENT}." && exit 1)
+MOUNT_ROOT=${MOUNT}/$(echo $ROOT | sed -e "s|${ROOT_PARENT}||g")
+echo "MOUNT_ROOT: ${MOUNT_ROOT}"
+mkdir -p ${MOUNT_ROOT}
