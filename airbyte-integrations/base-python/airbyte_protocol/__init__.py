f9493ba8f2de096e59535119a0050cb2ce70c9ed
Michel Tricot <michel@airbyte.io>
2020-10-15 20:22:11 -0700

    progress

--- airbyte-integrations/base-python/airbyte_protocol/__init__.py
+++ airbyte-integrations/base-python/airbyte_protocol/__init__.py
@@ -1,12 +1,9 @@
 from .entrypoint import AirbyteEntrypoint
-
+from .integration import *
+from .logger import AirbyteLogger
 from .models import AirbyteCatalog
 from .models import AirbyteLogMessage
 from .models import AirbyteMessage
 from .models import AirbyteRecordMessage
 from .models import AirbyteStateMessage
 from .models import AirbyteStream
-
-from .integration import *
-
-from .logger import AirbyteLogger
