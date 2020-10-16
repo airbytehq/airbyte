91375382217b20e77216b7f81103f595d40d4594
jrhizor <me@jaredrhizor.com>
2020-10-16 11:48:29 -0700

    use connection status and fix log method

--- airbyte-integrations/base-python/airbyte_protocol/models/__init__.py
+++ airbyte-integrations/base-python/airbyte_protocol/models/__init__.py
@@ -1,5 +1,6 @@
 from .airbyte_catalog import AirbyteStream
 from .airbyte_catalog import AirbyteCatalog
+from .airbyte_connetion_status import AirbyteConnectionStatus
 from .airbyte_message import AirbyteMessage
 from .airbyte_message import AirbyteLogMessage
 from .airbyte_message import AirbyteRecordMessage
