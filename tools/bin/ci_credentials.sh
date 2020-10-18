5d3090d50060067a06705f182063261f6028f710
Michel Tricot <michel@airbyte.io>
2020-10-17 17:05:35 -0700

    fix creds

--- tools/bin/ci_credentials.sh
+++ tools/bin/ci_credentials.sh
@@ -5,7 +5,7 @@ set -e
 CONNECTOR_BASE=airbyte-integrations/connectors
 
 function install_secret() {
-  local path; path=$1/secrets
+  local path; path=$1
   local filename; filename=$2
   local value; value=$3
 
@@ -14,9 +14,9 @@ function install_secret() {
 }
 
 # BigQuery Credentials
-install_secret $CONNECTOR_BASE/bigquery/_deprecated-destination-singer/ credentials.json "$BIGQUERY_INTEGRATION_TEST_CREDS"
-install_secret $CONNECTOR_BASE/bigquery/destination/ credentials.json "$BIGQUERY_INTEGRATION_TEST_CREDS"
+install_secret $CONNECTOR_BASE/bigquery/_deprecated-destination-singer credentials.json "$BIGQUERY_INTEGRATION_TEST_CREDS"
+install_secret $CONNECTOR_BASE/bigquery/destination credentials.json "$BIGQUERY_INTEGRATION_TEST_CREDS"
 
 # Stripe Credentials
-install_secret $CONNECTOR_BASE/stripe/_deprecated-source-singer/ config.json "$STRIPE_INTEGRATION_TEST_CREDS"
-install_secret $CONNECTOR_BASE/stripe/source-singer/ config.json "$STRIPE_INTEGRATION_TEST_CREDS"
+install_secret $CONNECTOR_BASE/stripe/_deprecated-source-singer config.json "$STRIPE_INTEGRATION_TEST_CREDS"
+install_secret $CONNECTOR_BASE/stripe/source-singer config.json "$STRIPE_INTEGRATION_TEST_CREDS"
