313cadb6200fddbaf74142317999fe791d995a1e
Sherif Nada <snadalive@gmail.com>
2020-10-19 13:39:23 -0700

    coerce ints into numbers'

--- airbyte-config/models/src/main/java/io/airbyte/config/AirbyteProtocolConverters.java
+++ airbyte-config/models/src/main/java/io/airbyte/config/AirbyteProtocolConverters.java
@@ -29,6 +29,7 @@ import com.google.common.collect.ImmutableMap;
 import io.airbyte.commons.json.Jsons;
 import io.airbyte.protocol.models.AirbyteCatalog;
 import io.airbyte.protocol.models.AirbyteStream;
+
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
@@ -87,6 +88,7 @@ public class AirbyteProtocolConverters {
   }
 
   // todo (cgardens) - add more robust handling for jsonschema types.
+
   /**
    * JsonSchema tends to have 2 types for fields one of which is null. The null is pretty irrelevant,
    * so look at types and find the first non-null one and use that.
@@ -96,11 +98,11 @@ public class AirbyteProtocolConverters {
    */
   private static DataType jsonSchemaTypesToDataType(JsonNode node) {
     if (node.isTextual()) {
-      return DataType.valueOf(node.asText().toUpperCase());
+      return DataType.valueOf(convertToNumberIfInteger(node.asText().toUpperCase()));
     } else if (node.isArray()) {
       return StreamSupport.stream(Spliterators.spliteratorUnknownSize(node.elements(), 0), false)
           .filter(typeString -> !typeString.asText().toUpperCase().equals("NULL"))
-          .map(typeString -> DataType.valueOf(typeString.asText().toUpperCase()))
+          .map(typeString -> DataType.valueOf(convertToNumberIfInteger(typeString.asText().toUpperCase())))
           .findFirst()
           // todo (cgardens) - or throw?
           .orElse(DataType.STRING);
@@ -109,4 +111,12 @@ public class AirbyteProtocolConverters {
     }
   }
 
+  // TODO HACK: convert Integer to Number until we have a more solid typing system
+  private static String convertToNumberIfInteger(String type) {
+    if (type.toUpperCase().equals("INTEGER")) {
+      return "NUMBER";
+    } else {
+      return type;
+    }
+  }
 }
