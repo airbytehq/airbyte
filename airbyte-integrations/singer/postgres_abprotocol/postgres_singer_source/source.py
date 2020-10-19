91771e0e51dfa1ed0854cad6baea58260756a596
Sherif Nada <snadalive@gmail.com>
2020-10-18 21:09:23 -0700

    extend singer source

--- airbyte-integrations/singer/postgres_abprotocol/postgres_singer_source/source.py
+++ airbyte-integrations/singer/postgres_abprotocol/postgres_singer_source/source.py
@@ -9,16 +9,16 @@ from airbyte_protocol import Status
 from airbyte_protocol import AirbyteMessage
 from airbyte_protocol import Source
 from airbyte_protocol import ConfigContainer
-from base_singer import SingerHelper
+from base_singer import SingerHelper, SingerSource
+
 
 TAP_CMD = "PGCLIENTENCODING=UTF8 tap-postgres"
-class PostgresSingerSource(Source):
+class PostgresSingerSource(SingerSource):
     def __init__(self):
         pass
 
     def check(self, logger, config_container: ConfigContainer) -> AirbyteConnectionStatus:
         config = config_container.rendered_config
-        print(config)
         try:
             params="dbname='{dbname}' user='{user}' host='{host}' password='{password}' port='{port}'".format(**config)
             psycopg2.connect(params)
@@ -27,18 +27,11 @@ class PostgresSingerSource(Source):
             logger.error(f"Exception while connecting to postgres database: {e}")
             return AirbyteConnectionStatus(status=Status.FAILED, message=str(e))
 
-    def discover(self, logger, config_container) -> AirbyteCatalog:
-        catalogs = SingerHelper.get_catalogs(logger, f"{TAP_CMD} --config {config_container.rendered_config_path} --discover")
-        return catalogs.airbyte_catalog
+    def discover_cmd(self, logger, config_container: ConfigContainer) -> AirbyteCatalog:
+        return f"{TAP_CMD} --config {config_container.rendered_config_path} --discover"
 
-    def read(self, logger, config_container, catalog_path, state=None) -> Generator[AirbyteMessage, None, None]:
-        discover_cmd = f"{TAP_CMD} --config {config_container.rendered_config_path} --discover"
-        discovered_singer_catalog = SingerHelper.get_catalogs(logger, discover_cmd).singer_catalog
-
-        masked_airbyte_catalog = self.read_config(catalog_path)
-        selected_singer_catalog = SingerHelper.create_singer_catalog_with_selection(masked_airbyte_catalog, discovered_singer_catalog)
-
+    def read_cmd(self, logger, config_container: ConfigContainer, catalog_path: str, state=None) -> Generator[AirbyteMessage, None, None]:
         catalog_path = f"--properties {selected_singer_catalog}"
         config_option = f"--config {config_container.rendered_config_path}"
         state_option = f"--state {state}" if state else ""
-        return SingerHelper.read(logger, f"{TAP_CMD} {catalog_path} {config_option} {state_option}")
+        return f"{TAP_CMD} {catalog_path} {config_option} {state_option}"
