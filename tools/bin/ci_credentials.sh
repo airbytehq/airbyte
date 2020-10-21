ddd2616e83fd8e6ed22b026ca5447757fa730d92
Charles <giardina.charles@gmail.com>
2020-10-20 23:04:36 -0700

    remove singer style integrations (#669)

--- tools/bin/ci_credentials.sh
+++ tools/bin/ci_credentials.sh
@@ -2,14 +2,8 @@
 
 set -e
 
-mkdir airbyte-integrations/singer/bigquery/destination/secrets
-echo "$BIGQUERY_INTEGRATION_TEST_CREDS" > airbyte-integrations/singer/bigquery/destination/secrets/credentials.json
-
 mkdir airbyte-integrations/bigquery-destination/secrets
 echo "$BIGQUERY_INTEGRATION_TEST_CREDS" > airbyte-integrations/bigquery-destination/secrets/credentials.json
-
-mkdir airbyte-integrations/singer/stripe/source/secrets
-echo "$STRIPE_INTEGRATION_TEST_CREDS" > airbyte-integrations/singer/stripe/source/secrets/config.json
 
 mkdir airbyte-integrations/singer/stripe_abprotocol/source/secrets
 echo "$STRIPE_INTEGRATION_TEST_CREDS" > airbyte-integrations/singer/stripe_abprotocol/source/secrets/config.json
