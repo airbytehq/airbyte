65bc169cbf398b3849fb426eda76e3516723c422
Charles <giardina.charles@gmail.com>
2020-10-21 13:05:25 -0700

    Integrations Reorganization: Connectors (#676)

--- tools/bin/ci_credentials.sh
+++ tools/bin/ci_credentials.sh
@@ -2,8 +2,8 @@
 
 set -e
 
-mkdir airbyte-integrations/bigquery-destination/secrets
-echo "$BIGQUERY_INTEGRATION_TEST_CREDS" > airbyte-integrations/bigquery-destination/secrets/credentials.json
+mkdir airbyte-integrations/connectors/destination-bigquery/secrets/
+echo "$BIGQUERY_INTEGRATION_TEST_CREDS" > airbyte-integrations/connectors/destination-bigquery/secrets/credentials.json
 
-mkdir airbyte-integrations/singer/stripe_abprotocol/source/secrets
-echo "$STRIPE_INTEGRATION_TEST_CREDS" > airbyte-integrations/singer/stripe_abprotocol/source/secrets/config.json
+mkdir airbyte-integrations/connectors/source-stripe-singer/secrets/
+echo "$STRIPE_INTEGRATION_TEST_CREDS" > airbyte-integrations/connectors/source-stripe-singer/secrets/config.json
