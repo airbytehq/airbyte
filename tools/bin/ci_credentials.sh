9ff0185de32652cc9131cbd4784fcd96f15b212c
cgardens <giardina.charles@gmail.com>
2020-10-22 14:27:58 -0700

    again

--- tools/bin/ci_credentials.sh
+++ tools/bin/ci_credentials.sh
@@ -10,4 +10,4 @@ echo "$STRIPE_INTEGRATION_TEST_CREDS" > airbyte-integrations/connectors/source-s
 
 mkdir airbyte-integrations/connectors/source-github-singer/secrets/
 # pull sample config. add in the access key. write to secrets.
-jq .access_token="$GH_INTEGRATION_TEST_CREDS" airbyte-integrations/connectors/source-github-singer/config.sample.json > airbyte-integrations/connectors/source-github-singer/secrets/config.json
+jq .access_token=$GH_INTEGRATION_TEST_CREDS airbyte-integrations/connectors/source-github-singer/config.sample.json > airbyte-integrations/connectors/source-github-singer/secrets/config.json
