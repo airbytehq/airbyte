9fbc118c689d5093f8af77223eefb08c716c52bc
Liren Tu <tuliren@gmail.com>
2020-10-24 16:06:26 -0700

    Fix tabs

--- airbyte-integrations/plopfile.js
+++ airbyte-integrations/plopfile.js
@@ -1,25 +1,25 @@
 module.exports = function (plop) {
-	plop.setGenerator('java-destination', {
-		description: 'generate java destination',
-		prompts: [
-		  {
+  plop.setGenerator('java-destination', {
+    description: 'generate java destination',
+    prompts: [
+      {
         type: 'input',
-			  name: 'name',
-			  message: 'destination name (without the "destination" suffix; e.g. "my sql")'
-		  }
-		],
-		actions: [
+        name: 'name',
+        message: 'destination name (without the "destination" suffix; e.g. "my sql")'
+      }
+    ],
+    actions: [
       {
         type: 'add',
         path: 'connectors/destination-{{dashCase name}}/build.gradle',
         templateFile: 'connector-templates/java-destination/build.gradle.hbs',
       },
-		  {
-		    type: 'add',
-		    path: 'connectors/destination-{{dashCase name}}/src/main/java/io/airbyte/integrations/destination/{{snakeCase name}}/{{properCase name}}Destination.java',
-		    templateFile: 'connector-templates/java-destination/Destination.java.hbs',
-		  },
-		  {
+      {
+        type: 'add',
+        path: 'connectors/destination-{{dashCase name}}/src/main/java/io/airbyte/integrations/destination/{{snakeCase name}}/{{properCase name}}Destination.java',
+        templateFile: 'connector-templates/java-destination/Destination.java.hbs',
+      },
+      {
         type: 'add',
         path: 'connectors/destination-{{dashCase name}}/Dockerfile',
         templateFile: 'connector-templates/java-destination/Dockerfile.hbs',
@@ -34,6 +34,6 @@ module.exports = function (plop) {
         path: 'connectors/destination-{{dashCase name}}/spec.json',
         templateFile: 'connector-templates/java-destination/spec.json.hbs',
       },
-		],
-	});
+    ],
+  });
 };
