e4bc915d802a99c90b42c287c1eafad02b523872
Chris <christophe.duong@gmail.com>
2020-10-26 18:37:48 +0100

    Handle Exception cases while loading CSVs

--- airbyte-integrations/connectors/source-csv/csv_source/source.py
+++ airbyte-integrations/connectors/source-csv/csv_source/source.py
@@ -1,5 +1,4 @@
-"""
-MIT License
+"""MIT License.
 
 Copyright (c) 2020 Airbyte
 
@@ -39,72 +38,97 @@ from airbyte_protocol import (
 
 
 class CsvSource(Source):
-    """
-        This source aims to provide support for readers described here:
-        https://pandas.pydata.org/pandas-docs/stable/user_guide/io.html#io-excel-reader
+    """This source aims to provide support for readers described here:
+    https://pandas.pydata.org/pandas-docs/stable/user_guide/io.html#io-excel-
+    reader.
 
-        Starting first with the read_csv primitive.
+    Starting first with the read_csv primitive.
     """
 
     def check(self, logger, config_container) -> AirbyteConnectionStatus:
-        """
-            Check involves verifying that the specified csv is reachable with our credentials.
+        """Check involves verifying that the specified csv is reachable with
+        our credentials.
+
         :param logger:
         :param config_container:
         :return:
         """
         config = config_container.rendered_config
-        csv_url = config["csv_url"]
-        logger.info(f"Checking access to {csv_url}...")
-        self.load_dataframe(config)
-        return AirbyteConnectionStatus(status=Status.SUCCEEDED)
+        data_url = config["data_url"]
+        logger.info(f"Checking access to {data_url}...")
+        try:
+            self.load_dataframe(config)
+            return AirbyteConnectionStatus(status=Status.SUCCEEDED)
+        except Exception as err:
+            reason = f"Failed to load {data_url}: {repr(err)}"
+            logger.error(reason)
+            return AirbyteConnectionStatus(status=Status.FAILED, message=reason)
 
     def discover(self, logger, config_container) -> AirbyteCatalog:
+        """
+
+        :param logger:
+        :param config_container:
+        :return:
+        """
         config = config_container.rendered_config
-        csv_url = config["csv_url"]
-        logger.info(f"Discovering {csv_url}...")
+        data_url = config["data_url"]
+        logger.info(f"Discovering {data_url}...")
         streams = []
-        # TODO handle discovery of directories of csv files?
-        df = self.load_dataframe(config)
-        csv_json_schema = {
-            "$schema": "http://json-schema.org/draft-07/schema#",
-            "type": "object",
-            "properties": {field: {"type": self.convert_dtype(df[field].dtype)} for field in df.columns},
-        }
-        streams.append(AirbyteStream(name=csv_url, json_schema=csv_json_schema))
+        try:
+            # TODO handle discovery of directories of csv files?
+            df = self.load_dataframe(config)
+            csv_json_schema = {
+                "$schema": "http://json-schema.org/draft-07/schema#",
+                "type": "object",
+                "properties": {field: {"type": self.convert_dtype(df[field].dtype)} for field in df.columns},
+            }
+            streams.append(AirbyteStream(name=data_url, json_schema=csv_json_schema))
+        except Exception as err:
+            reason = f"Failed to discover schemas of {data_url}: {repr(err)}"
+            logger.error(reason)
         return AirbyteCatalog(streams=streams)
 
     def read(self, logger, config_container, catalog_path, state_path=None) -> Generator[AirbyteMessage, None, None]:
         config = config_container.rendered_config
-        csv_url = config["csv_url"]
-        logger.info(f"Reading ({csv_url}, {catalog_path}, {state_path})...")
-        df = self.load_dataframe(config)
-        # TODO get subset of columns from catalog
-        for data in df.to_dict(orient="records"):
-            yield AirbyteMessage(
-                type=Type.RECORD,
-                record=AirbyteRecordMessage(stream=csv_url, data=data, emitted_at=int(datetime.now().timestamp()) * 1000),
-            )
+        data_url = config["data_url"]
+        logger.info(f"Reading ({data_url}, {catalog_path}, {state_path})...")
+        try:
+            df = self.load_dataframe(config)
+            # TODO get subset of columns from catalog
+            for data in df.to_dict(orient="records"):
+                yield AirbyteMessage(
+                    type=Type.RECORD,
+                    record=AirbyteRecordMessage(stream=data_url, data=data, emitted_at=int(datetime.now().timestamp()) * 1000),
+                )
+        except Exception as err:
+            reason = f"Failed to discover schemas of {data_url}: {repr(err)}"
+            logger.error(reason)
+            raise err
 
     @staticmethod
     def load_dataframe(config) -> pd.DataFrame:
-        """
-            From a Airbyte Configuration file, load and return the pandas dataframe
+        """From a Airbyte Configuration file, load and return the pandas
+        dataframe.
+
         :param config:
         :return:
         """
-        csv_url = config["csv_url"]
-        if "pandas.read_csv" in config:
-            # pandas.read_csv additional arguments can be passed to customize how to parse csv.
-            # see https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.read_csv.html
-            return pd.read_csv(csv_url, **config["pandas.read_csv"])
+        data_url = config["data_url"]
+        if config["reader"] == "read_csv":
+            if "reader_arguments" in config:
+                # pandas.read_csv additional arguments can be passed to customize how to parse csv.
+                # see https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.read_csv.html
+                return pd.read_csv(data_url, **config["reader_arguments"])
+            else:
+                return pd.read_csv(data_url)
         else:
-            return pd.read_csv(csv_url)
+            raise Exception("No valid readers defined in configuration")
 
     @staticmethod
     def convert_dtype(dtype) -> str:
-        """
-            Convert Pandas Dataframe types to Airbyte Types
+        """Convert Pandas Dataframe types to Airbyte Types.
+
         :param dtype: Pandas Dataframe type
         :return: Corresponding Airbyte Type
         """
