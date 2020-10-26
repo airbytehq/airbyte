2dfaa28703bfb61359416230a01ce9fcc7df3f10
Chris <christophe.duong@gmail.com>
2020-10-26 13:13:32 +0100

    Implement CSV Source

--- airbyte-integrations/connectors/source-csv/csv_source/source.py
+++ airbyte-integrations/connectors/source-csv/csv_source/source.py
@@ -22,43 +22,96 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 """
 
-import json
-import pkgutil
-import time
+from datetime import datetime
 from typing import Generator
 
+import pandas as pd
 from airbyte_protocol import (
     AirbyteCatalog,
     AirbyteConnectionStatus,
     AirbyteMessage,
     AirbyteRecordMessage,
-    AirbyteStateMessage,
     AirbyteStream,
     Source,
     Status,
+    Type,
 )
 
 
 class CsvSource(Source):
+    """
+        This source aims to provide support for readers described here:
+        https://pandas.pydata.org/pandas-docs/stable/user_guide/io.html#io-excel-reader
+
+        Starting first with the read_csv primitive.
+    """
+
     def check(self, logger, config_container) -> AirbyteConnectionStatus:
-        logger.info(f"Checking configuration ({config_container.rendered_config_path})...")
+        """
+            Check involves verifying that the specified csv is reachable with our credentials.
+        :param logger:
+        :param config_container:
+        :return:
+        """
+        config = config_container.rendered_config
+        csv_url = config["csv_url"]
+        logger.info(f"Checking access to {csv_url}...")
+        self.load_dataframe(config)
         return AirbyteConnectionStatus(status=Status.SUCCEEDED)
 
     def discover(self, logger, config_container) -> AirbyteCatalog:
-        logger.info(f"Discovering ({config_container.rendered_config_path})...")
-        catalog = pkgutil.get_data(__name__, "catalog.json")
-        if catalog:
-            schema = json.loads(catalog)
-            airbyte_streams = [AirbyteStream(name=__name__, json_schema=schema)]
-        else:
-            airbyte_streams = []
-        return AirbyteCatalog(streams=airbyte_streams)
+        config = config_container.rendered_config
+        csv_url = config["csv_url"]
+        logger.info(f"Discovering {csv_url}...")
+        streams = []
+        # TODO handle discovery of directories of csv files?
+        df = self.load_dataframe(config)
+        csv_json_schema = {
+            "$schema": "http://json-schema.org/draft-07/schema#",
+            "type": "object",
+            "properties": {field: {"type": self.convert_dtype(df[field].dtype)} for field in df.columns},
+        }
+        streams.append(AirbyteStream(name=csv_url, json_schema=csv_json_schema))
+        return AirbyteCatalog(streams=streams)
 
     def read(self, logger, config_container, catalog_path, state_path=None) -> Generator[AirbyteMessage, None, None]:
-        logger.info(f"Reading ({config_container.rendered_config_path}, {catalog_path}, {state_path})...")
+        config = config_container.rendered_config
+        csv_url = config["csv_url"]
+        logger.info(f"Reading ({csv_url}, {catalog_path}, {state_path})...")
+        df = self.load_dataframe(config)
+        # TODO get subset of columns from catalog
+        for data in df.to_dict(orient="records"):
+            yield AirbyteMessage(
+                type=Type.RECORD,
+                record=AirbyteRecordMessage(stream=csv_url, data=data, emitted_at=int(datetime.now().timestamp()) * 1000),
+            )
 
-        message = AirbyteRecordMessage(stream="love_airbyte", data={"love": True}, emitted_at=int(time.time() * 1000))
-        yield AirbyteMessage(type="RECORD", record=message)
+    @staticmethod
+    def load_dataframe(config) -> pd.DataFrame:
+        """
+            From a Airbyte Configuration file, load and return the pandas dataframe
+        :param config:
+        :return:
+        """
+        csv_url = config["csv_url"]
+        if "pandas.read_csv" in config:
+            # pandas.read_csv additional arguments can be passed to customize how to parse csv.
+            # see https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.read_csv.html
+            return pd.read_csv(csv_url, **config["pandas.read_csv"])
+        else:
+            return pd.read_csv(csv_url)
 
-        state = AirbyteStateMessage(data={"love_cursor": "next_version"})
-        yield AirbyteMessage(type="STATE", state=state)
+    @staticmethod
+    def convert_dtype(dtype) -> str:
+        """
+            Convert Pandas Dataframe types to Airbyte Types
+        :param dtype: Pandas Dataframe type
+        :return: Corresponding Airbyte Type
+        """
+        if dtype == object:
+            return "string"
+        elif dtype in ("int64", "float64"):
+            return "number"
+        elif dtype == "bool":
+            return "bool"
+        return "string"
