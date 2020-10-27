d4f663f2d50731e8398f358fa69f122b3c09e63c
Charles <giardina.charles@gmail.com>
2020-10-26 17:16:41 -0700

    only run integration tests for a subset of integrations during PR builds (#710)

---
+++ tools/bin/integration_test_pr.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+set -e
+
+. tools/lib/lib.sh
+
+assert_root
+
+./gradlew --no-daemon --scan -x generateProtocolClassFiles \
+  :airbyte-integrations:connectors:destination-bigquery:integrationTest \
+  :airbyte-integrations:connectors:destination-postgres:integrationTest \
+  :airbyte-integrations:connectors:destination-csv:integrationTest \
+  :airbyte-integrations:connectors:source-postgres-singer:integrationTest \
+  :airbyte-integrations:connectors:source-stripe-singer:integrationTest \
+  :airbyte-integrations:connectors:source-exchangeratesapi-singer:integrationTest
