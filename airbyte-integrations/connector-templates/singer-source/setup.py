f046e2bd98e9b04b03034c4d404b37f197e7bcd1
cgardens <giardina.charles@gmail.com>
2020-10-27 20:20:11 -0700

    more mess

--- airbyte-integrations/connector-templates/singer-source/setup.py
+++ airbyte-integrations/connector-templates/singer-source/setup.py
@@ -34,7 +34,7 @@ setup(
     install_requires=[
         "tap-exchangeratesapi==0.1.1",
         "pydantic==1.6.1",
-        "base_singer",
-        "airbyte_protocol",
+        "base-singer",
+        "airbyte-protocol",
     ],
 )
