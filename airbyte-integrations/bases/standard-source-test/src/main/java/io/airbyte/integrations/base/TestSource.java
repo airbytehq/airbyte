b41ea9882c1dbb456c673a6763055dad3a8e7b21
Sherif Nada <snadalive@gmail.com>
2020-10-28 11:21:40 -0700

    save

--- airbyte-integrations/bases/standard-source-test/src/main/java/io/airbyte/integrations/base/TestSource.java
+++ airbyte-integrations/bases/standard-source-test/src/main/java/io/airbyte/integrations/base/TestSource.java
@@ -61,8 +61,6 @@ import java.util.stream.Collectors;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public abstract class TestSource {
 
