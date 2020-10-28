5814c789172f599542d4185b452217ba00d2bfcf
LiRen Tu <tuliren@gmail.com>
2020-10-27 23:41:53 -0700

    Generate connector with templating (#700)

---
+++ airbyte-integrations/plopfile.js
@@ -0,0 +1,40 @@
+module.exports = function (plop) {
+  plop.setGenerator('java-destination', {
+    description: 'generate java destination',
+    prompts: [
+      {
+        type: 'input',
+        name: 'name',
+        message: 'destination name (without the "destination" suffix; e.g. "my sql")'
+      }
+    ],
+    actions: [
+      {
+        type: 'add',
+        path: 'connectors/destination-{{dashCase name}}/build.gradle',
+        templateFile: 'connector-templates/java-destination/build.gradle.hbs',
+      },
+      {
+        type: 'add',
+        path: 'connectors/destination-{{dashCase name}}/src/main/java/io/airbyte/integrations/destination/{{snakeCase name}}/{{properCase name}}Destination.java',
+        templateFile: 'connector-templates/java-destination/Destination.java.hbs',
+      },
+      {
+        type: 'add',
+        path: 'connectors/destination-{{dashCase name}}/Dockerfile',
+        templateFile: 'connector-templates/java-destination/Dockerfile.hbs',
+      },
+      {
+        type: 'add',
+        path: 'connectors/destination-{{dashCase name}}/.dockerignore',
+        templateFile: 'connector-templates/java-destination/.dockerignore.hbs',
+      },
+      {
+        type: 'add',
+        path: 'connectors/destination-{{dashCase name}}/spec.json',
+        templateFile: 'connector-templates/java-destination/spec.json.hbs',
+      },
+      'Your new connector has been created. Happy coding~~',
+    ],
+  });
+};
