6e14e0dafb47b52b9d1a9eecf4c3d49133aaa00a
Sherif Nada <snadalive@gmail.com>
2020-11-02 16:17:09 -0800

    add postgresql source

---
+++ airbyte-integrations/connectors/source-postgres/src/test-integration/java/io/airbyte/integrations/io/airbyte/integration_tests/sources/PostgresIntegrationTests.java
@@ -0,0 +1,91 @@
+package io.airbyte.integrations.io.airbyte.integration_tests.sources;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.google.common.collect.ImmutableMap;
+import io.airbyte.commons.json.Jsons;
+import io.airbyte.commons.resources.MoreResources;
+import io.airbyte.db.Database;
+import io.airbyte.db.Databases;
+import io.airbyte.integrations.base.TestSource;
+import io.airbyte.protocol.models.AirbyteCatalog;
+import io.airbyte.protocol.models.CatalogHelpers;
+import io.airbyte.protocol.models.ConnectorSpecification;
+import io.airbyte.protocol.models.Field;
+import org.jooq.SQLDialect;
+import org.testcontainers.containers.PostgreSQLContainer;
+
+import java.util.Collections;
+import java.util.List;
+
+public class PostgresIntegrationTests extends TestSource {
+
+  private static final String STREAM_NAME = "id_and_name";
+
+  private PostgreSQLContainer<?> container;
+  private JsonNode config;
+
+  @Override
+  protected void setup(TestDestinationEnv testEnv) throws Exception {
+    container = new PostgreSQLContainer<>("postgres:13-alpine");
+    container.start();
+
+    config = Jsons.jsonNode(ImmutableMap.builder()
+        .put("host", container.getHost())
+        .put("port", container.getFirstMappedPort())
+        .put("database", container.getDatabaseName())
+        .put("username", container.getUsername())
+        .put("password", container.getPassword())
+        .build());
+
+    final Database database = Databases.createDatabase(
+        config.get("username").asText(),
+        config.get("password").asText(),
+        String.format("jdbc:postgresql://%s:%s/%s",
+            config.get("host").asText(),
+            config.get("port").asText(),
+            config.get("database").asText()),
+        "org.postgresql.Driver",
+        SQLDialect.POSTGRES);
+
+    database.query(ctx -> {
+      ctx.fetch("CREATE TABLE id_and_name(id INTEGER, name VARCHAR(200));");
+      ctx.fetch("INSERT INTO id_and_name (id, name) VALUES (1,'arthur'),  (2, 'thomas'), (3, 'finn');");
+      return null;
+    });
+
+    database.close();
+  }
+
+  @Override
+  protected void tearDown(TestDestinationEnv testEnv) {
+    container.close();
+  }
+
+  @Override
+  protected String getImageName() {
+    return "airbyte/source-postgres:dev";
+  }
+
+  @Override
+  protected ConnectorSpecification getSpec() throws Exception {
+    return Jsons.deserialize(MoreResources.readResource("spec.json"), ConnectorSpecification.class);
+  }
+
+  @Override
+  protected JsonNode getConfig() {
+    return config;
+  }
+
+  @Override
+  protected AirbyteCatalog getCatalog() {
+    return CatalogHelpers.createAirbyteCatalog(
+        STREAM_NAME,
+        Field.of("id", Field.JsonSchemaPrimitive.NUMBER),
+        Field.of("name", Field.JsonSchemaPrimitive.STRING));
+  }
+
+  @Override
+  protected List<String> getRegexTests() {
+    return Collections.emptyList();
+  }
+}
---
+++ airbyte-integrations/connectors/source-postgres/src/test/java/io/airbyte/integrations/source/mysql/PostgresSourceTest.java
@@ -0,0 +1,238 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2020 Airbyte
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package io.airbyte.integrations.source.mysql;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.fail;
+import static org.mockito.Mockito.doCallRealMethod;
+import static org.mockito.Mockito.spy;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.Lists;
+import com.google.common.collect.Sets;
+import io.airbyte.commons.json.Jsons;
+import io.airbyte.commons.resources.MoreResources;
+import io.airbyte.db.Database;
+import io.airbyte.db.Databases;
+import io.airbyte.integrations.source.postgres.PostgresSource;
+import io.airbyte.protocol.models.AirbyteCatalog;
+import io.airbyte.protocol.models.AirbyteConnectionStatus;
+import io.airbyte.protocol.models.AirbyteConnectionStatus.Status;
+import io.airbyte.protocol.models.AirbyteMessage;
+import io.airbyte.protocol.models.AirbyteMessage.Type;
+import io.airbyte.protocol.models.AirbyteRecordMessage;
+import io.airbyte.protocol.models.AirbyteStream;
+import io.airbyte.protocol.models.CatalogHelpers;
+import io.airbyte.protocol.models.ConnectorSpecification;
+import io.airbyte.protocol.models.Field;
+import io.airbyte.protocol.models.Field.JsonSchemaPrimitive;
+
+import java.io.BufferedReader;
+import java.io.StringReader;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import io.airbyte.test.utils.PostgreSQLContainerHelper;
+import org.apache.commons.lang3.RandomStringUtils;
+import org.jooq.SQLDialect;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.utility.MountableFile;
+
+class PostgresSourceTest {
+
+  private static final String STREAM_NAME = "id_and_name";
+  private static final AirbyteCatalog CATALOG = CatalogHelpers.createAirbyteCatalog(
+      STREAM_NAME,
+      Field.of("id", JsonSchemaPrimitive.NUMBER),
+      Field.of("name", JsonSchemaPrimitive.STRING));
+  private static final Set<AirbyteMessage> ASCII_MESSAGES = Sets.newHashSet(
+      new AirbyteMessage().withType(Type.RECORD)
+          .withRecord(new AirbyteRecordMessage().withStream(STREAM_NAME).withData(Jsons.jsonNode(ImmutableMap.of("id", 1, "name", "goku")))),
+      new AirbyteMessage().withType(Type.RECORD)
+          .withRecord(new AirbyteRecordMessage().withStream(STREAM_NAME).withData(Jsons.jsonNode(ImmutableMap.of("id", 2, "name", "vegeta")))),
+      new AirbyteMessage().withType(Type.RECORD)
+          .withRecord(new AirbyteRecordMessage().withStream(STREAM_NAME).withData(Jsons.jsonNode(ImmutableMap.of("id", 3, "name", "piccolo")))));
+
+  private static final Set<AirbyteMessage> UTF8_MESSAGES = Sets.newHashSet(
+      new AirbyteMessage().withType(Type.RECORD)
+          .withRecord(
+              new AirbyteRecordMessage().withStream(STREAM_NAME).withData(Jsons.jsonNode(ImmutableMap.of("id", 1, "name", "\u2013 someutfstring")))),
+      new AirbyteMessage().withType(Type.RECORD)
+          .withRecord(new AirbyteRecordMessage().withStream(STREAM_NAME).withData(Jsons.jsonNode(ImmutableMap.of("id", 2, "name", "\u2215")))));
+
+  private static PostgreSQLContainer<?> PSQL_DB;
+
+  private JsonNode config;
+
+  @BeforeAll
+  static void init() {
+    PSQL_DB = new PostgreSQLContainer<>("postgres:13-alpine");
+    PSQL_DB.start();
+
+  }
+
+  @BeforeEach
+  void setup() throws Exception {
+    final String dbName = "db_" + RandomStringUtils.randomAlphabetic(10).toLowerCase();
+
+    config = getConfig(PSQL_DB, dbName);
+
+    final String initScriptName = "init_" + dbName.concat(".sql");
+    MoreResources.writeResource(initScriptName, "CREATE DATABASE " + dbName + ";");
+    PostgreSQLContainerHelper.runSqlScript(MountableFile.forClasspathResource(initScriptName), PSQL_DB);
+
+    final Database database = getDatabaseFromConfig(config);
+    database.query(ctx -> {
+      ctx.fetch("CREATE TABLE id_and_name(id INTEGER, name VARCHAR(200));");
+      ctx.fetch("INSERT INTO id_and_name (id, name) VALUES (1,'goku'),  (2, 'vegeta'), (3, 'piccolo');");
+      return null;
+    });
+    database.close();
+  }
+
+  private Database getDatabaseFromConfig(JsonNode config) {
+    return Databases.createDatabase(
+        config.get("username").asText(),
+        config.get("password").asText(),
+        String.format("jdbc:postgresql://%s:%s/%s",
+            config.get("host").asText(),
+            config.get("port").asText(),
+            config.get("database").asText()),
+        "org.postgresql.Driver",
+        SQLDialect.POSTGRES);
+  }
+
+  private JsonNode getConfig(PostgreSQLContainer<?> psqlDb, String dbName) {
+    return Jsons.jsonNode(ImmutableMap.builder()
+        .put("host", psqlDb.getHost())
+        .put("port", psqlDb.getFirstMappedPort())
+        .put("database", dbName)
+        .put("username", psqlDb.getUsername())
+        .put("password", psqlDb.getPassword())
+        .build());
+  }
+
+  private JsonNode getConfig(PostgreSQLContainer<?> psqlDb) {
+    return getConfig(psqlDb, psqlDb.getDatabaseName());
+  }
+
+  @AfterAll
+  static void cleanUp() {
+    PSQL_DB.close();
+  }
+
+  @Test
+  void testSpec() throws Exception {
+    final ConnectorSpecification actual = new PostgresSource().spec();
+    final String resourceString = MoreResources.readResource("spec.json");
+    final ConnectorSpecification expected = Jsons.deserialize(resourceString, ConnectorSpecification.class);
+
+    assertEquals(expected, actual);
+  }
+
+  @Test
+  void testCheckSuccess() {
+    final AirbyteConnectionStatus actual = new PostgresSource().check(config);
+    final AirbyteConnectionStatus expected = new AirbyteConnectionStatus().withStatus(Status.SUCCEEDED);
+    assertEquals(expected, actual);
+  }
+
+  @Test
+  void testCheckFailure() {
+    ((ObjectNode) config).put("password", "fake");
+    final AirbyteConnectionStatus actual = new PostgresSource().check(config);
+    final AirbyteConnectionStatus expected = new AirbyteConnectionStatus().withStatus(Status.FAILED)
+        .withMessage("Can't connect with provided configuration.");
+    assertEquals(expected, actual);
+  }
+
+  @Test
+  void testDiscover() throws Exception {
+    final AirbyteCatalog actual = new PostgresSource().discover(config);
+    assertEquals(CATALOG, actual);
+  }
+
+  @Test
+  void testReadSuccess() throws Exception {
+    final Set<AirbyteMessage> actualMessages = new PostgresSource().read(config, CATALOG, null).collect(Collectors.toSet());
+
+    actualMessages.forEach(r -> {
+      if (r.getRecord() != null) {
+        r.getRecord().setEmittedAt(null);
+      }
+    });
+
+    assertEquals(ASCII_MESSAGES, actualMessages);
+  }
+
+  @Test
+  public void testCanReadUtf8() throws Exception {
+    // force the db server to start with sql_ascii encoding to verify the tap can read UTF8 even when
+    // default settings are in another encoding
+    try (PostgreSQLContainer<?> db = new PostgreSQLContainer<>("postgres:13-alpine").withCommand("postgres -c client_encoding=sql_ascii")) {
+      db.start();
+      final JsonNode config = getConfig(db);
+      try (final Database database = getDatabaseFromConfig(config)) {
+        database.query(ctx -> {
+          ctx.fetch("CREATE TABLE id_and_name(id INTEGER, name VARCHAR(200));");
+          ctx.fetch("INSERT INTO id_and_name (id, name) VALUES (1,E'\\u2013 someutfstring'),  (2, E'\\u2215');");
+          return null;
+        });
+      }
+
+      Set<AirbyteMessage> actualMessages = new PostgresSource().read(config, CATALOG, null).collect(Collectors.toSet());
+
+      for (AirbyteMessage actualMessage : actualMessages) {
+        if (actualMessage.getRecord() != null) {
+          actualMessage.getRecord().setEmittedAt(null);
+        }
+      }
+
+      assertEquals(UTF8_MESSAGES, actualMessages);
+    }
+  }
+
+  @SuppressWarnings("ResultOfMethodCallIgnored")
+  @Test
+  void testReadFailure() throws Exception {
+    final AirbyteStream spiedAbStream = spy(CATALOG.getStreams().get(0));
+    final AirbyteCatalog catalog = new AirbyteCatalog().withStreams(Lists.newArrayList(spiedAbStream));
+    doCallRealMethod().doCallRealMethod().doThrow(new RuntimeException()).when(spiedAbStream).getName();
+
+    final Stream<AirbyteMessage> stream = new PostgresSource().read(config, catalog, null);
+
+    assertThrows(RuntimeException.class, () -> stream.collect(Collectors.toList()));
+  }
+
+}
