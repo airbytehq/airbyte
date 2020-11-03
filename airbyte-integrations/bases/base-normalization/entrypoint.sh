321f906791dff150dd7a35e3c6ba0e5f914f98a8
cgardens <giardina.charles@gmail.com>
2020-11-02 16:46:19 -0800

    update point parses correct args

--- airbyte-integrations/bases/base-normalization/entrypoint.sh
+++ airbyte-integrations/bases/base-normalization/entrypoint.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+TRANSFORMED_CONFIG=transformed_config.json
+DBT_MODEL=dbt_model.json
 set -e
 
 function echo2() {
@@ -11,59 +13,47 @@ function error() {
   exit 1
 }
 
-function main() {
-  transform-config
-  transform-catalog
-}
-
 ## todo: make it easy to select source or destination and validate based on selection by adding an integration type env variable.
-#function main() {
-#  CMD="$1"
-#  shift 1 || error "command not specified."
-#
-#  ARGS=
-#  while [ $# -ne 0 ]; do
-#    case "$1" in
-#    --config)
-#      CONFIG_FILE="$2"
-#      shift 2
-#      ;;
-#    --catalog)
-#      CATALOG_FILE="$2"
-#      shift 2
-#      ;;
-#    --state)
-#      STATE_FILE="$2"
-#      shift 2
-#      ;;
-#    *)
-#      error "Unknown option: $1"
-#      ;;
-#    esac
-#  done
-#
-#  case "$CMD" in
-#  spec)
-#    eval "$AIRBYTE_SPEC_CMD"
-#    ;;
-#  check)
-#    eval "$AIRBYTE_CHECK_CMD" --config "$CONFIG_FILE"
-#    ;;
-#  discover)
-#    eval "$AIRBYTE_DISCOVER_CMD" --config "$CONFIG_FILE"
-#    ;;
-#  read)
-#    READ_STATEMENT="$AIRBYTE_READ_CMD --config $CONFIG_FILE --catalog $CATALOG_FILE"
-#    if [[ ! -z "$STATE_FILE" ]]; then READ_STATEMENT="$READ_STATEMENT --state $STATE_FILE"; fi
-#    eval "$READ_STATEMENT"
-#    ;;
-#  write)
-#    eval "$AIRBYTE_WRITE_CMD" --config "$CONFIG_FILE" --catalog "$CATALOG_FILE"
-#    ;;
-#  *)
-#    error "Unknown command: $CMD"
-#    ;;
-#  esac
-#}
+function main() {
+  CMD="$1"
+  shift 1 || error "command not specified."
+
+  ARGS=
+  while [ $# -ne 0 ]; do
+    case "$1" in
+    --config)
+      CONFIG_FILE="$2"
+      shift 2
+      ;;
+    --catalog)
+      CATALOG_FILE="$2"
+      shift 2
+      ;;
+    --integration-type)
+      INTEGRATION_TYPE="$2"
+      shift 2
+      ;;
+    *)
+      error "Unknown option: $1"
+      ;;
+    esac
+  done
+
+  case "$CMD" in
+  run)
+    transform-config --config "$CONFIG_FILE" --integration-type "$INTEGRATION_TYPE" --out "$TRANSFORMED_CONFIG"
+#    transform-catalog --catalog "$CATALOG_FILE" --out "$DBT_MODEL"
+    cat $TRANSFORMED_CONFIG
+#    cat $DBT_MODEL
+#    execute-dbt --config "$TRANSFORMED_CONFIG" --model "$DBT_MODEL"
+    ;;
+  dry-run)
+    error "Not Implemented"
+    ;;
+  *)
+    error "Unknown command: $CMD"
+    ;;
+  esac
+}
 
 main "$@"
