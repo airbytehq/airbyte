5e56b7714d19c48183f716a78ba8db0af157e078
Sherif Nada <snadalive@gmail.com>
2020-11-03 23:02:31 -0800

    remove references

--- tools/bin/integration_test_pr.sh
+++ tools/bin/integration_test_pr.sh
@@ -10,7 +10,6 @@ assert_root
   :airbyte-integrations:connectors:destination-bigquery:integrationTest \
   :airbyte-integrations:connectors:destination-postgres:integrationTest \
   :airbyte-integrations:connectors:destination-csv:integrationTest \
-  :airbyte-integrations:connectors:source-postgres-singer:integrationTest \
   :airbyte-integrations:connectors:source-postgres:integrationTest \
   :airbyte-integrations:connectors:source-stripe-singer:integrationTest \
   :airbyte-integrations:connectors:source-exchangeratesapi-singer:integrationTest
