7b88a050ddebd6571cddcae696caefa7ff5d4ce8
Sherif A. Nada <snadalive@gmail.com>
2020-11-05 10:43:36 -0800

    Add Marketo source (#826)

--- tools/bin/standard_test_pr.sh
+++ tools/bin/standard_test_pr.sh
@@ -7,4 +7,5 @@ set -e
 assert_root
 
 ./gradlew --no-daemon --scan -x generateProtocolClassFiles \
-  :airbyte-integrations:connectors:source-github-singer:standardSourceTestPython 
+  :airbyte-integrations:connectors:source-github-singer:standardSourceTestPython \
+  :airbyte-integrations:connectors:source-marketo-singer:standardSourceTestPython
