e156338fcae9001ca21c0cde31c78a9f1e0d807f
Jin <jbjxu@thoughtworks.com>
2020-11-06 15:33:53 +0800

    [hive-source] Implemented the check interface of Hive source

--- airbyte-integrations/connectors/source-hive/source_hive/source.py
+++ airbyte-integrations/connectors/source-hive/source_hive/source.py
@@ -24,8 +24,10 @@ SOFTWARE.
 
 from typing import Generator
 
-from airbyte_protocol import AirbyteCatalog, AirbyteConnectionStatus, AirbyteMessage
-from base_python import AirbyteLogger, Source, ConfigContainer
+from impala.dbapi import connect
+
+from airbyte_protocol import AirbyteCatalog, AirbyteConnectionStatus, AirbyteMessage, Status
+from base_python import AirbyteLogger, ConfigContainer, Source
 
 
 class SourceHive(Source):
@@ -33,7 +35,31 @@ class SourceHive(Source):
         super().__init__()
 
     def check(self, logger: AirbyteLogger, config_container: ConfigContainer) -> AirbyteConnectionStatus:
-        raise Exception("unimplemented")
+        json_config = config_container.rendered_config
+        try:
+            logger.info('Hive Source: Start to connect to Hive server ......')
+            conn = connect(host=json_config['host'],
+                           port=json_config['port'],
+                           database=json_config['database'],
+                           user=json_config['username'],
+                           password=json_config['password'],
+                           auth_mechanism=json_config['authMechanism'],
+                           use_http_transport=True,
+                           http_path='cliservice'
+                           )
+            cur = conn.cursor()
+            cur.execute("SELECT VERSION()")
+            version = cur.fetchone()
+            logger.info('Hive Source: Connect successful! The Hive version is ' + str(version))
+            cur.close()
+            conn.close()
+
+        except Exception as e:
+            logger.error('Hive Source: Connect failed! The reason is ' + str(e))
+            return AirbyteConnectionStatus(status=Status.FAILED)
+
+        return AirbyteConnectionStatus(status=Status.SUCCEEDED)
+
 
     def discover(self, logger: AirbyteLogger, config_container: ConfigContainer) -> AirbyteCatalog:
         # discover the schema with the provided config
