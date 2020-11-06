86768d62f6b1bbc3f0e639625639821e4617efb7
Jin <jbjxu@thoughtworks.com>
2020-11-06 17:00:32 +0800

    [hive-source] Implemented the discovery method of Hive source connector

--- airbyte-integrations/connectors/source-hive/source_hive/source.py
+++ airbyte-integrations/connectors/source-hive/source_hive/source.py
@@ -26,8 +26,22 @@ from typing import Generator
 
 from impala.dbapi import connect
 
-from airbyte_protocol import AirbyteCatalog, AirbyteConnectionStatus, AirbyteMessage, Status
+from airbyte_protocol import AirbyteCatalog, AirbyteConnectionStatus, AirbyteMessage, Status, AirbyteStream
 from base_python import AirbyteLogger, ConfigContainer, Source
+
+
+def connecting(json_config):
+    conn = connect(host=json_config["host"],
+                   port=json_config["port"],
+                   database=json_config["database"],
+                   user=json_config["username"],
+                   password=json_config["password"],
+                   auth_mechanism=json_config["authMechanism"],
+                   use_http_transport=True,
+                   http_path="cliservice"
+                   )
+    cur = conn.cursor()
+    return conn, cur
 
 
 class SourceHive(Source):
@@ -37,33 +51,50 @@ class SourceHive(Source):
     def check(self, logger: AirbyteLogger, config_container: ConfigContainer) -> AirbyteConnectionStatus:
         json_config = config_container.rendered_config
         try:
-            logger.info('Hive Source: Start to connect to Hive server ......')
-            conn = connect(host=json_config['host'],
-                           port=json_config['port'],
-                           database=json_config['database'],
-                           user=json_config['username'],
-                           password=json_config['password'],
-                           auth_mechanism=json_config['authMechanism'],
-                           use_http_transport=True,
-                           http_path='cliservice'
-                           )
-            cur = conn.cursor()
+            logger.info("Hive Source: Start to connect to Hive server ......")
+            conn, cur = connecting(json_config)
             cur.execute("SELECT VERSION()")
             version = cur.fetchone()
-            logger.info('Hive Source: Connect successful! The Hive version is ' + str(version))
+            logger.info("Hive Source: Connect successful! The Hive version is " + str(version))
             cur.close()
             conn.close()
 
         except Exception as e:
-            logger.error('Hive Source: Connect failed! The reason is ' + str(e))
+            logger.error("Hive Source: Connect failed! The reason is " + str(e))
             return AirbyteConnectionStatus(status=Status.FAILED)
 
         return AirbyteConnectionStatus(status=Status.SUCCEEDED)
 
-
     def discover(self, logger: AirbyteLogger, config_container: ConfigContainer) -> AirbyteCatalog:
-        # discover the schema with the provided config
-        raise Exception("unimplemented")
+        json_config = config_container.rendered_config
+        logger.info("Hive source: Start to connect to Hive server ...... ")
+        conn, cur = connecting(json_config)
+        try:
+            logger.info("Hive source: Start to query all tables from Hive server ...... ")
+            cur.execute("show tables")
+            tables = []
+            for name in cur.fetchall():
+                logger.info(f"Hive source: Contain table: {name[0]}")
+                tables.append(name[0])
 
-    def read(self, logger: AirbyteLogger, config_container: ConfigContainer, catalog_path, state=None) -> Generator[AirbyteMessage, None, None]:
-        raise Exception("unimplemented")
+            streams = []
+            for table_name in tables:
+                metadata = []
+                logger.info(f"Hive source: Start to query {table_name}'s metadata from Hive server ...... ")
+                cur.execute(f"describe {table_name}")
+                for column in cur.fetchall():
+                    metadata.append(column)
+
+                json_schema = {
+                    "$schema": "http://json-schema.org/draft-07/schema#",
+                    "type": "object",
+                    "properties": {field[0]: {"type": field[1]} for field in metadata},
+                }
+                streams.append(AirbyteStream(name=table_name, json_schema=json_schema))
+        except Exception as err:
+            reason = f"Hive source: Failed to discover schemas"
+            logger.error(reason)
+            raise err
+        cur.close()
+        conn.close()
+        return AirbyteCatalog(streams=streams)
