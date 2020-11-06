e6cc9b1cf2b7ef14207a8e7add45020b4e64c8ed
Chris <christophe.duong@gmail.com>
2020-11-06 22:36:39 +0100

    Check if items is in dict before accessing it

--- airbyte-integrations/bases/base-normalization/normalization/transform_catalog/transform.py
+++ airbyte-integrations/bases/base-normalization/normalization/transform_catalog/transform.py
@@ -263,7 +263,7 @@ def extract_nested_properties(path: List[str], field: str, properties: dict) -> 
                     result.update(found)
             elif "type" not in properties[key]:
                 pass
-            elif is_array(properties[key]["type"]):
+            elif is_array(properties[key]["type"]) and "items" in properties[key]:
                 combining = find_combining_schema(properties[key]["items"])
                 if combining:
                     # skip combining schemas
