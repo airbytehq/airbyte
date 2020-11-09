e1d7add50a5da77ac01a253cd1e4603ef8ccb99f
Jared Rhizor <jared@dataline.io>
2020-11-09 11:00:11 -0800

    mostly incremental builds (#817)
    * support cached builds for base -> base-java -> snowflake
    
    * use plugin for image building
    
    * fix matching on COPY from
    
    * remove docker.gradle
    
    * tmp commit
    
    * update connectors
    
    * finish rest of build files
    
    * fix ide errors
    
    * more build fixes
    
    * clean up
    
    * clean up for new soruces
    
    * fix spotless
    
    * fix flake problems
    
    * add recommended empty file
    
    * python caching
    
    * fixes upon review
    
    * clean up docker and build test files
    
    * clean up python
    
    * clean up
    
    * fix integration test dependencies
    
    * fix standard tests
    
    * fix
    
    * remove symlink
    
    * re-add requirements to fix normalizatioon build
    
    * fix symlink
    
    * fix dumbest build problem of all
    
    * add missing integration test def
    
    * fix missing dep
    
    * remove class exclusion
    
    * move trim so null source versions are allowed
    
    * rename map
    
    * fix hardcoded value
    
    * remove unnecessary dep
    
    * use dashes for salesforce package name
    
    * fix typo
    
    * DRY and fix test image name
    
    * Fix edit
    
    * assert string is not empty
    
    * build integration test image only for integrationTest
    
    * move code generator to tools and rename docker build tasks
    
    * make source test depend on integration test build, not the other way
    
    * remove guard because the docker build should exist before the integrationtest is applied
    
    * remove comment
    
    * DRY up airbyte-source-test
    
    * fix plugin compilation
    
    * add missing dependency
    
    * rename getTaggedImage to getDevTaggedImage
    
    * fix test vs main docker build bug

--- airbyte-integrations/connectors/source-salesforce-singer/source_salesforce_singer/__init__.py
+++ airbyte-integrations/connectors/source-salesforce-singer/source_salesforce_singer/__init__.py
@@ -22,5 +22,6 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 """
 
-from .source import *
+from .source import SourceSalesforceSinger
 
+__all__ = ["SourceSalesforceSinger"]
