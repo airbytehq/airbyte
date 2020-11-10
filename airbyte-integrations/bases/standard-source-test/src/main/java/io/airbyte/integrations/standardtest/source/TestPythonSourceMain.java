d507f0f95ba0aabb58cb96ba6d235fad11392e6b
Charles <giardina.charles@gmail.com>
2020-11-09 21:43:51 -0800

    Fix names for standard tests (#862)

--- airbyte-integrations/bases/standard-source-test/src/main/java/io/airbyte/integrations/standardtest/source/TestPythonSourceMain.java
+++ airbyte-integrations/bases/standard-source-test/src/main/java/io/airbyte/integrations/standardtest/source/TestPythonSourceMain.java
@@ -22,7 +22,7 @@
  * SOFTWARE.
  */
 
-package io.airbyte.integrations.base;
+package io.airbyte.integrations.standardtest.source;
 
 import net.sourceforge.argparse4j.ArgumentParsers;
 import net.sourceforge.argparse4j.inf.ArgumentParser;
