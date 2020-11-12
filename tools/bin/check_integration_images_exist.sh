c5ecd31ac6542884017e3881931110c849f36cb5
jrhizor <me@jaredrhizor.com>
2020-11-12 10:40:08 -0800

    add script

---
+++ tools/bin/check_integration_images_exist.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+set -e
+
+function docker_tag_exists() {
+    URL=https://hub.docker.com/v2/repositories/"$1"/tags/"$2"
+    printf "\tURL: %s\n" "$URL"
+    curl --silent -f -lSL "$URL" > /dev/null
+}
+
+CONFIG_FILES=$(find airbyte-config/init | grep json | grep -v STANDARD_WORKSPACE | grep -v build)
+[ -z "$CONFIG_FILES" ] && echo "ERROR: Could not find any config files." && exit 1
+
+while IFS= read -r file; do
+    REPO=$(cat "$file" | jq -r .dockerRepository)
+    TAG=$(cat "$file" | jq -r .dockerImageTag)
+    echo "Checking $file..."
+    printf "\tREPO: %s\n" "$REPO"
+    printf "\tTAG: %s\n" "$TAG"
+    if docker_tag_exists "$REPO" "$TAG"; then
+        printf "\tSTATUS: found\n"
+    else
+        printf "\tERROR: not found!\n" && exit 1
+    fi
+done <<< "$CONFIG_FILES"
+
