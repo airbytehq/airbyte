7950062e61a31a100bbbe78bbc10dd36a70377f8
Sherif A. Nada <snadalive@gmail.com>
2020-11-12 20:36:18 -0800

    Update connector READMEs to reflect new build plugin commands (#969)

--- airbyte-integrations/connectors/source-github-singer/main_dev.py
+++ airbyte-integrations/connectors/source-github-singer/main_dev.py
@@ -24,7 +24,7 @@ SOFTWARE.
 
 import sys
 
-from airbyte_protocol.entrypoint import launch
+from base_python.entrypoint import launch
 from source_github_singer import SourceGithubSinger
 
 if __name__ == "__main__":
