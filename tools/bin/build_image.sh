9da86934390be4bd467882c2fded6a67f55d3e85
jrhizor <me@jaredrhizor.com>
2020-11-18 16:23:58 -0800

    try save and load method instead

--- tools/bin/build_image.sh
+++ tools/bin/build_image.sh
@@ -14,21 +14,4 @@ assert_root
 
 cd "$PROJECT_DIR"
 
-if [[ -z "$CI" ]]; then
-  # run standard build locally (not on CI)
-  DOCKER_BUILDKIT=1 docker build \
-    -f "$DOCKERFILE" . \
-    -t "$TAG" \
-    --iidfile "$ID_FILE"
-else
-  # run build with local docker registery for CI
-  docker pull localhost:5000/"$TAG" || true
-  docker build \
-    -f "$DOCKERFILE" . \
-    -t "$TAG" \
-    --iidfile "$ID_FILE" \
-    --cache-from localhost:5000/"$TAG" \
-    --build-arg BUILDKIT_INLINE_CACHE=1
-  docker tag "$TAG" localhost:5000/"$TAG"
-  docker push localhost:5000/"$TAG"
-fi
+DOCKER_BUILDKIT=1 docker build -f "$DOCKERFILE" . -t "$TAG" --iidfile "$ID_FILE"
