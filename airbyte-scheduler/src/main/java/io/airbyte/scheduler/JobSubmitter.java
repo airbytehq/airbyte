fa82a04500d49423fd98d5e6785ec6e03665146b
cgardens <giardina.charles@gmail.com>
2020-11-25 15:33:38 -0800

    skip GET_SPEC

--- airbyte-scheduler/src/main/java/io/airbyte/scheduler/JobSubmitter.java
+++ airbyte-scheduler/src/main/java/io/airbyte/scheduler/JobSubmitter.java
@@ -140,6 +140,11 @@ public class JobSubmitter implements Runnable {
   }
 
   private void track(Map<String, Object> metadata) {
+    // do not track get spec. it is done frequently and not terribly interesting.
+    if(metadata.get("job_type").equals("GET_SPEC")) {
+      return;
+    }
+
     TrackingClientSingleton.get().track("Connector Jobs", metadata);
   }
 
