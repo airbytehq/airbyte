78a83fae0ebdfc024712bb8d7f1e60732e1dcf3a
Sherif A. Nada <snadalive@gmail.com>
2020-11-25 16:05:51 -0800

    Mailchimp incremental fixes (#1094)

--- airbyte-integrations/connectors/source-mailchimp/source_mailchimp/client.py
+++ airbyte-integrations/connectors/source-mailchimp/source_mailchimp/client.py
@@ -25,7 +25,7 @@ SOFTWARE.
 import json
 import pkgutil
 from datetime import datetime
-from typing import DefaultDict, Dict, Generator
+from typing import DefaultDict, Dict, Generator, Tuple
 
 from airbyte_protocol import AirbyteMessage, AirbyteRecordMessage, AirbyteStateMessage, AirbyteStream, Type
 from dateutil import parser
@@ -62,20 +62,20 @@ class Client:
         cursor_field = "date_created"
         stream_name = self._LISTS
         date_created = self._get_cursor_or_none(state, stream_name, cursor_field)
+        default_params, max_date_created = self._get_default_params_and_cursor(cursor_field, date_created)
 
-        default_params = {"since_date_created": date_created, "sort_field": cursor_field, "sort_dir": "ASC"}
         offset = 0
-        max_date_created = date_created
         done = False
         while not done:
-            lists_response = self._client.lists.all(dict(default_params, count=self.PAGINATION, offset=offset))["lists"]
+            params = dict(default_params, count=self.PAGINATION, offset=offset)
+            lists_response = self._client.lists.all(**params)["lists"]
             for mc_list in lists_response:
                 list_created_at = parser.isoparse(mc_list[cursor_field])
                 max_date_created = max(max_date_created, list_created_at) if max_date_created else list_created_at
                 yield self._record(stream=self._LISTS, data=mc_list)
 
             if max_date_created:
-                state[self._LISTS][cursor_field] = max_date_created
+                state[self._LISTS][cursor_field] = self._format_date_as_string(max_date_created)
                 yield self._state(state)
 
             done = len(lists_response) < self.PAGINATION
@@ -85,24 +85,44 @@ class Client:
         cursor_field = "create_time"
         stream_name = self._CAMPAIGNS
         create_time = self._get_cursor_or_none(state, stream_name, cursor_field)
+        default_params, max_create_time = self._get_default_params_and_cursor(cursor_field, create_time)
 
-        default_params = {"since_create_time": create_time, "sort_field": cursor_field, "sort_dir": "ASC"}
         offset = 0
-        max_create_time = create_time
         done = False
         while not done:
-            campaigns_response = self._client.campaigns.all(dict(default_params, count=self.PAGINATION, offset=offset))
+            params = dict(default_params, count=self.PAGINATION, offset=offset)
+            campaigns_response = self._client.campaigns.all(**params)
             for campaign in campaigns_response["campaigns"]:
                 campaign_created_at = parser.isoparse(campaign[cursor_field])
                 max_create_time = max(max_create_time, campaign_created_at) if max_create_time else campaign_created_at
                 yield self._record(stream=stream_name, data=campaign)
 
             if max_create_time:
-                state[stream_name][cursor_field] = max_create_time
+                state[stream_name][cursor_field] = self._format_date_as_string(max_create_time)
                 yield self._state(state)
 
             done = len(campaigns_response) < self.PAGINATION
             offset += self.PAGINATION
+
+    @staticmethod
+    def _get_default_params_and_cursor(cursor_field_name: str, cursor_value: str) -> Tuple[Dict[str, str], datetime]:
+        """
+        :param cursor_field_name:
+        :param cursor_value: assumed to be a date represented as an ISO8601 string
+        :return:
+        """
+        default_params = {"sort_field": cursor_field_name, "sort_dir": "ASC"}
+        if cursor_value:
+            default_params[f"since_{cursor_field_name}"] = cursor_value
+            parsed_date_cursor = parser.isoparse(cursor_value)
+        else:
+            parsed_date_cursor = None
+
+        return default_params, parsed_date_cursor
+
+    @staticmethod
+    def _format_date_as_string(d: datetime) -> str:
+        return d.astimezone().replace(microsecond=0).isoformat()
 
     @staticmethod
     def _get_cursor_or_none(state: DefaultDict[str, any], stream_name: str, cursor_name: str) -> any:
