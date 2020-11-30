72a62467a167335b9a79ef742b1889caab4c6ea3
Christophe Duong <christophe.duong@gmail.com>
2020-11-30 18:01:44 +0100

    Impl of source-redshift (#1009)

---
+++ airbyte-integrations/connectors/source-redshift/src/main/java/io/airbyte/integrations/source/redshift/RedshiftSource.java
@@ -0,0 +1,71 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2020 Airbyte
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package io.airbyte.integrations.source.redshift;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.google.common.collect.ImmutableMap;
+import io.airbyte.commons.json.Jsons;
+import io.airbyte.integrations.base.IntegrationRunner;
+import io.airbyte.integrations.base.Source;
+import io.airbyte.integrations.source.jdbc.AbstractJdbcSource;
+import java.util.List;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class RedshiftSource extends AbstractJdbcSource implements Source {
+
+  private static final Logger LOGGER = LoggerFactory.getLogger(RedshiftSource.class);
+
+  // todo (cgardens) - clean up passing the dialect as null versus explicitly adding the case to the
+  // constructor.
+  public RedshiftSource() {
+    super("com.amazon.redshift.jdbc.Driver", null);
+  }
+
+  @Override
+  public JsonNode toJdbcConfig(JsonNode redshiftConfig) {
+    return Jsons.jsonNode(ImmutableMap.builder()
+        .put("username", redshiftConfig.get("username").asText())
+        .put("password", redshiftConfig.get("password").asText())
+        .put("jdbc_url", String.format("jdbc:redshift://%s:%s/%s",
+            redshiftConfig.get("host").asText(),
+            redshiftConfig.get("port").asText(),
+            redshiftConfig.get("database").asText()))
+        .build());
+  }
+
+  @Override
+  protected List<String> getExcludedInternalSchemas() {
+    return List.of("information_schema", "pg_catalog", "pg_internal", "catalog_history");
+  }
+
+  public static void main(String[] args) throws Exception {
+    final Source source = new RedshiftSource();
+    LOGGER.info("starting source: {}", RedshiftSource.class);
+    new IntegrationRunner(source).run(args);
+    LOGGER.info("completed source: {}", RedshiftSource.class);
+  }
+
+}
