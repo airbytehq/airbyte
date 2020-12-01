dba7dd6a834d53335f0b6c03bb192aeddb7023dd
Sherif A. Nada <snadalive@gmail.com>
2020-12-01 10:51:24 -0800

    Automatically enable incremental syncs for singer sources (#1142)

--- airbyte-integrations/connectors/source-stripe-singer/main_dev.py
+++ airbyte-integrations/connectors/source-stripe-singer/main_dev.py
@@ -24,7 +24,7 @@ SOFTWARE.
 
 import sys
 
-from airbyte_protocol.entrypoint import launch
+from base_python.entrypoint import launch
 from source_stripe_singer import SourceStripeSinger
 
 if __name__ == "__main__":
