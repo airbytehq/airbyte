64071fc5ea48b6d201ab0c1317272c32c21eaab3
jrhizor <me@jaredrhizor.com>
2020-12-01 15:19:23 -0800

    revert to simpler docker times

--- tools/bin/build_image.sh
+++ tools/bin/build_image.sh
@@ -14,21 +14,7 @@ assert_root
 
 cd "$PROJECT_DIR"
 
-if [[ -z "$CI" ]]; then
-  # run standard build locally (not on CI)
-  DOCKER_BUILDKIT=1 docker build \
-    -f "$DOCKERFILE" . \
-    -t "$TAGGED_IMAGE" \
-    --iidfile "$ID_FILE"
-else
-  # run build with local docker registery for CI
-  docker pull localhost:5000/"$TAGGED_IMAGE" || true
-  DOCKER_BUILDKIT=1 docker build \
-    -f "$DOCKERFILE" . \
-    -t "$TAGGED_IMAGE" \
-    --iidfile "$ID_FILE" \
-    --cache-from localhost:5000/"$TAGGED_IMAGE" \
-    --build-arg BUILDKIT_INLINE_CACHE=1
-  docker tag "$TAGGED_IMAGE" localhost:5000/"$TAGGED_IMAGE"
-  docker push localhost:5000/"$TAGGED_IMAGE"
-fi
+DOCKER_BUILDKIT=1 docker build \
+  -f "$DOCKERFILE" . \
+  -t "$TAGGED_IMAGE" \
+  --iidfile "$ID_FILE"
