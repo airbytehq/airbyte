7deab320b733aefe2194906c80a797cab3a4f6e9
ykurochkin <zhenia.kurochkin@gmail.com>
2020-12-01 18:30:43 +0200

    add Slack source

---
+++ airbyte-integrations/connectors/source-slack-singer/source_slack_singer/source.py
@@ -0,0 +1,59 @@
+"""
+MIT License
+
+Copyright (c) 2020 Airbyte
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
+from airbyte_protocol import AirbyteConnectionStatus, Status
+from base_python import AirbyteLogger, ConfigContainer
+from base_singer import SingerSource
+from slack_sdk import WebClient
+from slack_sdk.errors import SlackApiError
+
+TAP_CMD = "tap-slack"
+
+
+class SourceSlackSinger(SingerSource):
+    def transform_config(self, raw_config):
+        return {
+            "token": raw_config["token"],
+            "start_date": raw_config["start_date"],
+            "private_channels": str(raw_config["private_channels"]),
+            "join_public_channels": str(raw_config["join_public_channels"]),
+            "exclude_archived": str(raw_config["exclude_archived"]),
+            "date_window_size": "7",
+        }
+
+    def check(self, logger: AirbyteLogger, config_container: ConfigContainer) -> AirbyteConnectionStatus:
+        try:
+            json_config = config_container.rendered_config
+            client = WebClient(token=json_config["token"])
+            client.conversations_list()
+            return AirbyteConnectionStatus(status=Status.SUCCEEDED)
+        except SlackApiError as e:
+            logger.error(f"Got an error: {e.args[0]}")
+            return AirbyteConnectionStatus(status=Status.FAILED, message=str(e.args[0]))
+
+    def discover_cmd(self, logger: AirbyteLogger, config_path: str) -> str:
+        return f"{TAP_CMD} --config {config_path} --discover"
+
+    def read_cmd(self, logger: AirbyteLogger, config_path: str, catalog_path: str, state_path: str = None) -> str:
+        return f"{TAP_CMD} --config {config_path} --catalog {catalog_path}"
