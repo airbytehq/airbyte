47aea26a5401e420f969687d4c11a782c638fef8
Eugene Kulak <kulak.eugene@gmail.com>
2020-12-02 20:34:28 +0200

    intercom init

---
+++ airbyte-integrations/connectors/source-intercom-singer/source_intercom_singer/source.py
@@ -0,0 +1,47 @@
+"""
+MIT License
+
+Copyright (c) 2020 Airbyte
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
+
+from airbyte_protocol import AirbyteConnectionStatus
+from base_python import AirbyteLogger, ConfigContainer
+from base_singer import SingerSource
+
+
+class SourceIntercomSinger(SingerSource):
+    def __init__(self):
+        super().__init__()
+
+    def check(self, logger: AirbyteLogger, config_container: ConfigContainer) -> AirbyteConnectionStatus:
+        cmd = "tap-intercom - -config config.json - -discover > catalog.json"
+        raise Exception("unimplemented")
+
+    def discover_cmd(self, logger: AirbyteLogger, config_path: str) -> str:
+        cmd = "tap-intercom - -config config.json - -discover > catalog.json"
+
+        # discover the schema with the provided config
+        raise Exception("unimplemented")
+
+    def read_cmd(self, logger: AirbyteLogger, config_path: str, catalog_path: str, state_path: str = None) -> str:
+        cmd = "tap-intercom - -config config.json - -discover > catalog.json"
+        raise Exception("unimplemented")
