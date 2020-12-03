dd657e497d95485a5334b1d959a0478c4b61e6e0
Sherif Nada <snadalive@gmail.com>
2020-12-03 00:10:52 -0800

    fix maindev

--- airbyte-integrations/bases/base-python/main_dev.py
+++ airbyte-integrations/bases/base-python/main_dev.py
@@ -22,7 +22,7 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 """
 
-from airbyte_protocol.entrypoint import main
+from base_python.entrypoint import main
 
 if __name__ == "__main__":
     main()
