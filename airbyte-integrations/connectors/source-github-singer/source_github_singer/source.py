79d812c09c0a20485273c766441e4fc89ea8d848
Sherif Nada <snadalive@gmail.com>
2020-12-03 00:25:03 -0800

    gh incremental

--- airbyte-integrations/connectors/source-github-singer/source_github_singer/source.py
+++ airbyte-integrations/connectors/source-github-singer/source_github_singer/source.py
@@ -23,13 +23,14 @@ SOFTWARE.
 """
 
 import requests
-from airbyte_protocol import AirbyteConnectionStatus, Status
-from base_singer import SingerSource
+from airbyte_protocol import AirbyteCatalog, AirbyteConnectionStatus, Status, SyncMode
+from base_python import AirbyteLogger, ConfigContainer
+from base_singer import SingerHelper, SingerSource
 
 
 class SourceGithubSinger(SingerSource):
     def __init__(self):
-        pass
+        super().__init__()
 
     def check(self, logger, config_container) -> AirbyteConnectionStatus:
         try:
@@ -44,8 +45,35 @@ class SourceGithubSinger(SingerSource):
             logger.error(e)
             return AirbyteConnectionStatus(status=Status.FAILED, message=f"{str(e)}")
 
-    def discover_cmd(self, logger, config_path) -> str:
+    def discover_cmd(self, logger: AirbyteLogger, config_path: str) -> str:
         return f"tap-github --config {config_path} --discover"
+
+    def discover(self, logger: AirbyteLogger, config_container: ConfigContainer) -> AirbyteCatalog:
+        """
+        HACK.
+        The singer tap does not declare any valid cursors for use in incremental sync. This is because its cursor is not a field on any of the objects
+        it returns. But by default the singer tap assumes an incremental sync on all of its output streams. So we alter each steam in the outgoing
+        catalog to:
+            1. Reflect that it only supports incremental syncs
+            2. Add one of the stream's properties (its primary key) as a "bogus" cursor to avoid having an empty cursor_field.
+        """
+        discover_cmd = self.discover_cmd(logger, config_container.rendered_config_path)
+        catalogs = SingerHelper.get_catalogs(logger, discover_cmd)
+        singer_catalog = catalogs.singer_catalog
+        streams_to_modify = {}
+        for stream in singer_catalog["streams"]:
+            if "key_properties" in stream and len(stream["key_properties"]) > 0:
+                fake_cursor = stream["key_properties"][0]
+                streams_to_modify[stream["stream"]] = fake_cursor
+
+        airyte_catalog = catalogs.airbyte_catalog
+        for stream in airyte_catalog.streams:
+            if stream.name in streams_to_modify:
+                stream.supported_sync_modes = [SyncMode.incremental]
+                stream.source_defined_cursor = True
+                stream.default_cursor_field = [streams_to_modify[stream.name]]
+
+        return catalogs.airbyte_catalog
 
     def read_cmd(self, logger, config_path, catalog_path, state_path=None) -> str:
         config_option = f"--config {config_path}"
