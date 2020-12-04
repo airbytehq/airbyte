25173e2d90c2466136b9dc29840e67dc930d4162
Eugene Kulak <kulak.eugene@gmail.com>
2020-12-04 13:17:06 +0200

    mixpanel integration

---
+++ airbyte-integrations/connectors/source-mixpanel-singer/source_mixpanel_singer/source.py
@@ -0,0 +1,89 @@
+"""
+MIT License
+
+Copyright (c) 2020 Airbyte
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
+
+from airbyte_protocol import AirbyteConnectionStatus, Status
+from base_python import AirbyteLogger, ConfigContainer
+from base_singer import SingerSource
+from tap_mixpanel.client import MixpanelClient, MixpanelError
+
+
+class BaseSingerSource(SingerSource):
+    tap_cmd = None
+    tap_name = None
+    api_error = Exception
+
+    def discover_cmd(self, logger: AirbyteLogger, config_path: str) -> str:
+        return f"{self.tap_cmd} --config {config_path} --discover"
+
+    def read_cmd(self, logger: AirbyteLogger, config_path: str, catalog_path: str, state_path: str = None) -> str:
+        args = {"--config": config_path, "--catalog": catalog_path, "--state": state_path}
+        cmd = " ".join([f"{k} {v}" for k, v in args.items() if v is not None])
+
+        return f"{self.tap_cmd} {cmd}"
+
+    def check(self, logger: AirbyteLogger, config_container: ConfigContainer) -> AirbyteConnectionStatus:
+        try:
+            json_config = config_container.rendered_config
+            self.try_connect(logger, json_config)
+        except self.api_error as err:
+            logger.error("Exception while connecting to the %s: %s", self.tap_name, str(err))
+            # this should be in UI
+            error_msg = (
+                f"Unable to connect to the {self.tap_name} with the provided credentials. "
+                "Please make sure the input credentials and environment are correct. "
+                f"Error: {err}"
+            )
+            return AirbyteConnectionStatus(status=Status.FAILED, message=error_msg)
+
+        return AirbyteConnectionStatus(status=Status.SUCCEEDED)
+
+    def try_connect(self, logger: AirbyteLogger, config: dict):
+        raise NotImplementedError
+
+
+class SourceIntercomSinger(BaseSingerSource):
+    tap_cmd = "tap-mixpanel"
+    tap_name = "Mixpanel API"
+    api_error = MixpanelClient
+    client_class = MixpanelError
+
+    def transform_config(self, raw_config):
+        airbyte_config = {
+            "user_agent": "tap-mixpanel contact@airbyte.io",
+            "api_secret": raw_config["api_key"],
+            "start_date": raw_config.get("start_date"),
+            "date_window_size": raw_config.get("date_window_size", 30),
+            "attribution_window": raw_config.get("attribution_window", 5),
+            "project_timezone": raw_config.get("US/Pacific"),
+            "select_properties_by_default": raw_config.get("select_properties_by_default"),
+        }
+        # drop None values as some of them has no default fallback
+        return {k: v for k, v in airbyte_config.items() if v is not None}
+
+    def try_connect(self, logger: AirbyteLogger, config: dict):
+        client = self.client_class(user_agent=config["user_agent"], api_secret=config["api_secret"])
+        ok = client.check_access_token()
+        if not ok:
+            raise self.api_error(f"Got an empty response from {self.tap_name}, check your permissions")
---
+++ docs/integrations/sources/mixpanel.md
@@ -0,0 +1,48 @@
+# Mixpanel
+
+## Overview
+
+The Mixpanel source supports Full Refresh syncs. That is, every time a sync is run, 
+Airbyte will copy all rows in the tables and columns you set up for replication into the destination in a new table.
+
+This Hubspot source wraps the [Singer Mixpanel Tap](https://github.com/singer-io/tap-intercom).
+
+### Output schema
+
+Several output streams are available from this source:
+
+* [Export](https://developer.mixpanel.com/docs/exporting-raw-data#section-export-api-reference)
+* [Engage](https://developer.mixpanel.com/docs/data-export-api#section-engage)
+* [Funnels](https://developer.mixpanel.com/docs/data-export-api#section-funnels)
+* [Revenue](https://developer.mixpanel.com/docs/data-export-api#section-hr-span-style-font-family-courier-revenue-span)
+* [Annotations](https://developer.mixpanel.com/docs/data-export-api#section-annotations)
+* [Cohorts](https://developer.mixpanel.com/docs/cohorts#section-list-cohorts)
+* [Cohort Members](https://developer.mixpanel.com/docs/data-export-api#section-engage)
+
+If there are more endpoints you'd like Airbyte to support, please 
+[create an issue.](https://github.com/airbytehq/airbyte/issues/new/choose)
+
+### Features
+
+| Feature | Supported? |
+| :--- | :--- |
+| Full Refresh Sync | Yes |
+| Incremental Sync | Yes |
+| Replicate Incremental Deletes | No |
+| SSL connection | Yes |
+
+### Performance considerations
+
+The Mixpanel connector should not run into Mixpanel API limitations under normal usage. 
+Please [create an issue](https://github.com/airbytehq/airbyte/issues) if you see any rate 
+limit issues that are not automatically retried successfully.
+
+## Getting started
+
+### Requirements
+
+* Mixpanel API Secret
+
+### Setup guide
+
+Please read [Find API Secret](https://help.mixpanel.com/hc/en-us/articles/115004502806-Find-Project-Token-).
