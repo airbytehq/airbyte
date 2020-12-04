c4fde25039c6f95036c1187f66b0dcb06aee9bb0
Sherif A. Nada <snadalive@gmail.com>
2020-12-04 14:49:41 -0800

    Fix twilio & sendgrid CI (#1206)

--- tools/bin/ci_credentials.sh
+++ tools/bin/ci_credentials.sh
@@ -70,3 +70,7 @@ echo "$FRESHDESK_TEST_CREDS" > "${FRESHDESK_SECRETS_DIR}/config.json"
 TWILIO_SECRETS_DIR=airbyte-integrations/connectors/source-twilio-singer/secrets
 mkdir $TWILIO_SECRETS_DIR
 echo "$TWILIO_TEST_CREDS" > "${TWILIO_SECRETS_DIR}/config.json"
+
+SENDGRID_SECRETS_DIR=airbyte-integrations/connectors/source-sendgrid/secrets
+mkdir $SENDGRID_SECRETS_DIR
+echo "$SENDGRID_INTEGRATION_TEST_CREDS" > "${SENDGRID_SECRETS_DIR}/config.json"
