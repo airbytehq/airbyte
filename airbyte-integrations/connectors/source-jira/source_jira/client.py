5fcc6b0254031c723c95cf43a90ad655b9d00df2
ykurochkin <zhenia.kurochkin@gmail.com>
2020-12-08 21:25:19 +0200

    local test config

--- airbyte-integrations/connectors/source-jira/source_jira/client.py
+++ airbyte-integrations/connectors/source-jira/source_jira/client.py
@@ -23,6 +23,7 @@ SOFTWARE.
 """
 
 import json
+from functools import partial
 from json.decoder import JSONDecodeError
 from typing import Mapping, Tuple
 
@@ -34,32 +35,58 @@ from requests.exceptions import ConnectionError
 
 class Client(BaseClient):
     API_VERSION = 3
-    DEFAULT_ITEMS_PER_PAGE = 1
-    ENTITIES = ["projects", "issues", "issue_comments", "resolutions", "users"]
+    DEFAULT_ITEMS_PER_PAGE = 100
+
+    PARAMS = {"maxResults": DEFAULT_ITEMS_PER_PAGE, "startAt": 0}
+    ENTITIES_MAP = {
+        "projects": {"url": "/project/search", "func": lambda v: v["values"], "params": PARAMS},
+        "issues": {"url": "/search", "func": lambda v: v["issues"], "params": PARAMS},
+        "issue_comments": {"url": "/search", "func": lambda v: v["issues"], "params": {**PARAMS, **{"fields": ["comment"]}}},
+        "users": {"url": "/users/search", "func": lambda v: v, "params": PARAMS},
+        "resolutions": None,
+    }
 
     def __init__(self, api_token, domain, email):
         self.auth = HTTPBasicAuth(email, api_token)
-        self.headers = {"Accept": "application/json"}
         self.base_api_url = f"https://{domain}/rest/api/{self.API_VERSION}"
         super().__init__()
 
+    def lists(self, name, **kwargs):
+        while True:
+            response = requests.get(f"{self.base_api_url}{kwargs['url']}", params=kwargs["params"], auth=self.auth)
+            data = kwargs["func"](response.json())
+            print(data)
+            for d in data:
+                if name == "issue_comments":
+                    for comment in d["fields"]["comment"]["comments"]:
+                        yield comment
+                else:
+                    yield d
+            kwargs["params"]["startAt"] += self.DEFAULT_ITEMS_PER_PAGE
+            if len(data) < self.DEFAULT_ITEMS_PER_PAGE:
+                break
+
     def _enumerate_methods(self) -> Mapping[str, callable]:
-        return {entity: getattr(self, f"get_{entity}") for entity in self.ENTITIES}
+        return {
+            entity: getattr(self, f"get_{entity}", None) or partial(self.lists, name=entity, **value)
+            for entity, value in self.ENTITIES_MAP.items()
+        }
 
     def health_check(self) -> Tuple[bool, str]:
         alive = True
         error_msg = None
 
         try:
-            response = requests.get(
-                f"{self.base_api_url}/users/search", headers=self.headers, params={"maxResults": 1, "startAt": 0}, auth=self.auth
-            )
+            response = requests.get(f"{self.base_api_url}/users/search", params={"maxResults": 1, "startAt": 0}, auth=self.auth)
 
             if response.status_code == 200:
                 try:
                     json.loads(response.text)
                 except JSONDecodeError:
-                    alive, error_msg = False, "Some text"
+                    alive, error_msg = (
+                        False,
+                        "Unable to connect to the Jira API with the provided credentials. Please make sure the input credentials and environment are correct.",
+                    )
             else:
                 alive, error_msg = False, str(response.text)
 
@@ -68,79 +95,8 @@ class Client(BaseClient):
 
         return alive, error_msg
 
-    def get_projects(self):
-        params = {"maxResults": self.DEFAULT_ITEMS_PER_PAGE, "startAt": 0}
-        while True:
-            response = requests.get(
-                f"{self.base_api_url}/project/search", headers=self.headers, params=params, auth=self.auth
-            )
-            projects = response.json()['values']
-            for project in projects:
-                yield project
-            params["startAt"] += self.DEFAULT_ITEMS_PER_PAGE
-            if len(projects) < self.DEFAULT_ITEMS_PER_PAGE:
-                break
-        # response = requests.get(
-        #     f"{self.base_api_url}/project/search", headers=self.headers, params={"maxResults": self.DEFAULT_ITEMS_PER_PAGE, "startAt": 0}, auth=self.auth
-        # )
-        #
-        # data = response.json()
-        # for project in data['values']:
-        #     yield project
-        #
-        # while not data['isLast']:
-        #     response = requests.get(
-        #         data['nextPage'], headers=self.headers, auth=self.auth
-        #     )
-        #
-        #     data = response.json()
-        #     for project in data['values']:
-        #         yield project
-
-    def get_issues(self):
-        params = {"maxResults": self.DEFAULT_ITEMS_PER_PAGE, "startAt": 0}
-        while True:
-            response = requests.get(
-                f"{self.base_api_url}/search", headers=self.headers, params=params, auth=self.auth
-            )
-            issues = response.json()['issues']
-            for issue in issues:
-                yield issue
-            params["startAt"] += self.DEFAULT_ITEMS_PER_PAGE
-            if len(issues) < self.DEFAULT_ITEMS_PER_PAGE:
-                break
-
-    def get_issue_comments(self):
-        params = {"maxResults": self.DEFAULT_ITEMS_PER_PAGE, "startAt": 0, 'fields': ['comment']}
-        while True:
-            response = requests.get(
-                f"{self.base_api_url}/search", headers=self.headers, params=params, auth=self.auth
-            )
-            issues = response.json()['issues']
-            for issue in issues:
-                for comment in issue['fields']['comment']['comments']:
-                    yield comment
-            params["startAt"] += self.DEFAULT_ITEMS_PER_PAGE
-            if len(issues) < self.DEFAULT_ITEMS_PER_PAGE:
-                break
-
     def get_resolutions(self):
-        response = requests.get(
-            f"{self.base_api_url}/resolution", headers=self.headers, auth=self.auth
-        )
+        response = requests.get(f"{self.base_api_url}/resolution", auth=self.auth)
 
         for res in response.json():
             yield res
-
-    def get_users(self):
-        params = {"maxResults": self.DEFAULT_ITEMS_PER_PAGE, "startAt": 0}
-        while True:
-            response = requests.get(
-                f"{self.base_api_url}/users/search", headers=self.headers, params=params, auth=self.auth
-            )
-            users = response.json()
-            for user in users:
-                yield user
-            params["startAt"] += self.DEFAULT_ITEMS_PER_PAGE
-            if len(users) < self.DEFAULT_ITEMS_PER_PAGE:
-                break
