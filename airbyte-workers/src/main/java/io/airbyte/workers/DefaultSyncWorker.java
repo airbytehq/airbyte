c3d27bd21251dbf98e003c5564a9e55c2a8e5389
Charles <giardina.charles@gmail.com>
2020-12-09 10:59:05 -0800

    hotfix: run normalization AFTER source and destination are closed (release v0.7.1-alpha) (#1263)

--- airbyte-workers/src/main/java/io/airbyte/workers/DefaultSyncWorker.java
+++ airbyte-workers/src/main/java/io/airbyte/workers/DefaultSyncWorker.java
@@ -95,17 +95,21 @@ public class DefaultSyncWorker implements SyncWorker {
 
       destination.notifyEndOfStream();
 
-      try (normalizationRunner) {
-        LOGGER.info("Running normalization.");
-        normalizationRunner.start();
-        final Path normalizationRoot = Files.createDirectories(jobRoot.resolve("normalize"));
-        if (!normalizationRunner.normalize(normalizationRoot, syncInput.getDestinationConnection().getConfiguration(), syncInput.getCatalog())) {
-          throw new WorkerException("Normalization Failed.");
-        }
-      }
     } catch (Exception e) {
       LOGGER.error("Sync worker failed.", e);
 
+      return new OutputAndStatus<>(JobStatus.FAILED, null);
+    }
+
+    try (normalizationRunner) {
+      LOGGER.info("Running normalization.");
+      normalizationRunner.start();
+      final Path normalizationRoot = Files.createDirectories(jobRoot.resolve("normalize"));
+      if (!normalizationRunner.normalize(normalizationRoot, syncInput.getDestinationConnection().getConfiguration(), syncInput.getCatalog())) {
+        throw new WorkerException("Normalization Failed.");
+      }
+    } catch (Exception e) {
+      LOGGER.error("Normalization Failed.", e);
       return new OutputAndStatus<>(JobStatus.FAILED, null);
     }
 
