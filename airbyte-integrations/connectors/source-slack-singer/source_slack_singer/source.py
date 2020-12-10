aeda6d76a1ffc52773a92ae97b8cdaa55fe810c2
Sherif Nada <snadalive@gmail.com>
2020-12-10 10:17:35 -0800

    save slack

--- airbyte-integrations/connectors/source-slack-singer/source_slack_singer/source.py
+++ airbyte-integrations/connectors/source-slack-singer/source_slack_singer/source.py
@@ -55,9 +55,6 @@ class SourceSlackSinger(SingerSource):
     def discover_cmd(self, logger: AirbyteLogger, config_path: str) -> str:
         return f"{TAP_CMD} --config {config_path} --discover"
 
-    def discover(self, logger: AirbyteLogger, config_container: ConfigContainer) -> AirbyteCatalog:
-        catalog = super().discover(logger, config_container)
-        return CatalogHelper.coerce_catalog_as_full_refresh(catalog)
-
     def read_cmd(self, logger: AirbyteLogger, config_path: str, catalog_path: str, state_path: str = None) -> str:
-        return f"{TAP_CMD} --config {config_path} --catalog {catalog_path}"
+        state_opt = f"--state {state_path}" if state_path else ""
+        return f"{TAP_CMD} --config {config_path} --catalog {catalog_path} {state_opt}"
