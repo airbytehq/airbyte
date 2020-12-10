db961fd4119a4da1418cdd4026d54c29eecdc1b2
Sherif Nada <snadalive@gmail.com>
2020-12-10 00:03:01 -0800

    save

--- airbyte-integrations/connectors/source-hubspot-singer/source_hubspot_singer/source.py
+++ airbyte-integrations/connectors/source-hubspot-singer/source_hubspot_singer/source.py
@@ -75,17 +75,11 @@ class SourceHubspotSinger(SingerSource):
     def discover_cmd(self, logger, config_path) -> str:
         return f"tap-hubspot --config {config_path} --discover"
 
-    def discover(self, logger: AirbyteLogger, config_container: ConfigContainer) -> AirbyteCatalog:
-        catalog = super().discover(logger, config_container)
-        return CatalogHelper.coerce_catalog_as_full_refresh(catalog)
-
     def read_cmd(self, logger, config_path, catalog_path, state_path=None) -> str:
         config_option = f"--config {config_path}"
         properties_option = f"--properties {catalog_path}"
-        # state_option = f"--state {state_path}" if state_path else ""
-
-        # TODO we don't pass in state to coerce this source to behave in a fullrefresh manner. See #1178 for context.
-        return f"tap-hubspot {config_option} {properties_option} "
+        state_option = f"--state {state_path}" if state_path else ""
+        return f"tap-hubspot {config_option} {properties_option} {state_option}"
 
     def transform_config(self, raw_config):
         rendered_config = dict(raw_config)
