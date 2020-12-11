2227715e2872c15397b83014d9a33368ce679559
jrhizor <me@jaredrhizor.com>
2020-12-11 15:39:26 -0800

    don't allow any retries for requested jobs

--- airbyte-scheduler/bin/kube_runner.sh
+++ airbyte-scheduler/bin/kube_runner.sh
@@ -3,6 +3,9 @@
 set -e
 
 JOB_YAML_PATH=$1
+echo "JOB_YAML_PATH = $JOB_YAML_PATH"
+
+echo "Launch job..."
 kubectl apply -f "$JOB_YAML_PATH"
 
 JOB_NAME=$(grep airbyte-worker- < "$JOB_YAML_PATH" | cut -d " " -f4)
@@ -11,6 +14,11 @@ JOB_UUID=$(kubectl get job "$JOB_NAME" -o "jsonpath={.metadata.labels.controller
 echo "JOB_UUID = $JOB_UUID"
 POD_NAME=$(kubectl get po -l controller-uid="$JOB_UUID" -o name)
 echo "POD_NAME = $POD_NAME"
+
+echo "Waiting for pod to start..."
+kubectl wait --for=condition=Ready --timeout=10m "$POD_NAME"
+
+echo "Emitting pod logs..."
 kubectl logs "$POD_NAME" --follow --pod-running-timeout=1000m
 
 # TODO: do we need to terminate on job closure with:
