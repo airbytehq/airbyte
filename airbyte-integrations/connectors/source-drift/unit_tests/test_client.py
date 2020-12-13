ccc4dba8c206d7513d2240fb47d9c21c8c27de68
Eugene Kulak <kulak.eugene@gmail.com>
2020-12-14 01:49:52 +0200

    add Drift source

---
+++ airbyte-integrations/connectors/source-drift/unit_tests/test_client.py
@@ -0,0 +1,40 @@
+"""
+MIT License
+
+Copyright (c) 2020 Airbyte
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
+import pytest
+from source_drift.client import Client, AuthError
+
+
+def test__heal_check_with_wrong_token():
+    client = Client(access_key="wrong_key")
+    alive, error = client.health_check()
+
+    assert not alive
+    assert error == '401 {"message":"Invalid Basic Auth credentials"}'
+
+
+def test__users_with_wrong_token():
+    client = Client(api_key="wrong_key")
+    with pytest.raises(AuthError, match='401 {"message":"Invalid Basic Auth credentials"}'):
+        next(client.users.list())
