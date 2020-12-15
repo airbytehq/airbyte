333b0396344fa08e737ccc4c155d68668217c508
Samuel Gordalina <samuel.gordalina@gmail.com>
2020-12-14 16:20:12 -0800

    Update discover and read commands

--- airbyte-integrations/connectors/source-google-analytics/lib/commands/discover.ex
+++ airbyte-integrations/connectors/source-google-analytics/lib/commands/discover.ex
@@ -1,18 +1,54 @@
 defmodule Airbyte.Source.GoogleAnalytics.Commands.Discover do
   alias Airbyte.Protocol.{
     AirbyteCatalog,
-    ConnectorSpecification
+    AirbyteStream
   }
 
-  alias Airbyte.Source.GoogleAnalytics.Apis.Api
+  alias Airbyte.Source.GoogleAnalytics.{
+    ConnectionSpecification,
+    GoogleAnalytics
+  }
 
-  def run(%ConnectorSpecification{} = spec) do
-    streams =
-      Api.impl()
-      |> Keyword.values()
-      |> Enum.map(& &1.discover(spec))
-      |> List.flatten()
+  alias GoogleApi.Analytics.V3.Api.{Management, Metadata}
+  alias GoogleApi.Analytics.V3.Connection
+  alias GoogleApi.Analytics.V3.Model.{Column, Columns}
 
-    %AirbyteCatalog{streams: streams}
+  # def run(%ConnectorSpecification{} = spec) do
+  #   streams =
+  #     Api.impl()
+  #     |> Keyword.values()
+  #     |> Enum.map(& &1.discover(spec))
+  #     |> List.flatten()
+
+  #   %AirbyteCatalog{streams: streams}
+  # end
+
+  def run(%ConnectionSpecification{} = spec) do
+    get_standard_dimensions(spec)
+    |> IO.inspect()
+
+    {:ok,
+     %AirbyteCatalog{
+       streams: [
+         %AirbyteStream{
+           name: "analytics",
+           json_schema: %{},
+           supported_sync_modes: ["incremental", "full_refresh"],
+           # not sure
+           source_defined_cursor: true,
+           # not sure
+           default_cursor_field: nil
+         }
+       ]
+     }}
+  end
+
+  defp get_standard_dimensions(%ConnectionSpecification{} = spec) do
+    with {:ok, conn} <- spec |> GoogleAnalytics.connection(),
+         {:ok, columns} <- Metadata.analytics_metadata_columns_list(conn, "ga") do
+      # columns.items
+      # |> Enum.map(fn %Column{} = col -> [col[:id]: col["dataType"] end)
+      columns
+    end
   end
 end
--- airbyte-integrations/connectors/source-google-analytics/lib/commands/read.ex
+++ airbyte-integrations/connectors/source-google-analytics/lib/commands/read.ex
@@ -1,17 +1,26 @@
 defmodule Airbyte.Source.GoogleAnalytics.Commands.Read do
   alias Airbyte.Protocol.{
     AirbyteMessage,
+    AirbyteRecordMessage,
     ConfiguredAirbyteCatalog,
-    ConnectorSpecification
+    ConnectionSpecification
+  }
+
+  alias Airbyte.Source.GoogleAnalytics.{
+    ConnectionSpecification,
+    GoogleAnalytics
   }
 
   def run(
-        config: %ConnectorSpecification{} = config,
+        config: %ConnectionSpecification{} = config,
         catalog: %ConfiguredAirbyteCatalog{} = catalog
       ) do
-    IO.inspect({config, catalog})
-    # spec() |> AirbyteMessage.dispatch()
+    # IO.inspect({config, catalog})
 
-    :ok
+    stream =
+      [AirbyteRecordMessage.new("stream-name", %{users: 1})]
+      |> Stream.map(&Function.identity/1)
+
+    {:ok, stream}
   end
 end
