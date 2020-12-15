fa05475a55407525bd7b2d6ff3ca08c5ffa57aee
Samuel Gordalina <samuel.gordalina@gmail.com>
2020-12-14 16:20:23 -0800

    Add ConnectionSpecification

---
+++ airbyte-integrations/connectors/source-google-analytics/lib/connection_specification.ex
@@ -0,0 +1,14 @@
+defmodule Airbyte.Source.GoogleAnalytics.ConnectionSpecification do
+  @moduledoc """
+  Airbyte Google Analytics Connection Specification
+  """
+  use TypedStruct
+
+  @derive Jason.Encoder
+
+  typedstruct do
+    @typedoc "Airbyte Google Analytics Connection Specification"
+
+    field(:service_account_key, String.t())
+  end
+end
--- airbyte-integrations/connectors/source-google-analytics/lib/helpers.ex
+++ airbyte-integrations/connectors/source-google-analytics/lib/helpers.ex
@@ -20,13 +20,4 @@ defmodule Airbyte.Source.GoogleAnalytics.Helpers do
 
     :error
   end
-
-  def json_to_struct(path, to_struct) do
-    with {:ok, contents} <- path |> File.read(),
-         {:ok, decoded} <- contents |> Jason.decode(keys: :atoms) do
-      struct(to_struct, decoded)
-    else
-      {:error, :enoent} -> raise "Could not find file: #{path}"
-    end
-  end
 end
