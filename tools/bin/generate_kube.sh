1280fe1d9b37ae8384c303538d590008ef34b38e
jrhizor <me@jaredrhizor.com>
2020-12-15 11:17:47 -0800

    delete unused file

--- tools/bin/generate_kube.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-
-RESOLVED_PATH=kube/docker-compose-resolved.yaml
-
-docker-compose config > "$RESOLVED_PATH"
-docker run --rm --name kompose -it -v "$(pwd)":/src femtopixel/kompose convert -f "$RESOLVED_PATH" -o kube/local
