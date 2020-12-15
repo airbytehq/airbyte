eaf6a2b02c0532d5607b50a4ae27a1eadfabe1e6
Samuel Gordalina <samuel.gordalina@gmail.com>
2020-12-14 16:19:10 -0800

    Use only analytics API

--- airbyte-integrations/connectors/source-google-analytics/lib/google_analytics.ex
+++ airbyte-integrations/connectors/source-google-analytics/lib/google_analytics.ex
@@ -1,35 +1,34 @@
-defmodule Airbyte.Source.GoogleAnalytics do
-  @behaviour Airbyte.Source
+defmodule Airbyte.Source.GoogleAnalytics.GoogleAnalytics do
+  alias Airbyte.Source.GoogleAnalytics.ConnectionSpecification
+  alias GoogleApi.Analytics.V3.Connection
 
-  alias Airbyte.Protocol.{
-    AirbyteMessage,
-    ConnectorSpecification
-  }
-
-  alias Airbyte.Source.GoogleAnalytics.Commands.{
-    Check,
-    Discover,
-    Spec,
-    Read
-  }
-
-  @impl Airbyte.Source
-  def check(%ConnectorSpecification{} = spec) do
-    spec
-    |> Check.run()
-    |> AirbyteMessage.dispatch()
+  def connection(%ConnectionSpecification{} = spec) do
+    with {:ok, %Goth.Token{token: token}} <- spec |> get_token() do
+      {:ok, token |> Connection.new()}
+    end
   end
 
-  @impl Airbyte.Source
-  def discover(%ConnectorSpecification{} = spec) do
-    spec
-    |> Discover.run()
-    |> AirbyteMessage.dispatch()
+  def get_token(%ConnectionSpecification{service_account_key: path}) do
+    with {:ok, json} <- path |> File.read!() |> Poison.decode(),
+         account <- json["client_email"],
+         :ok <- Goth.Config.add_config(json) do
+      scope = "https://www.googleapis.com/auth/analytics.readonly"
+      Goth.Token.for_scope({account, scope})
+    end
   end
 
-  @impl Airbyte.Source
-  def read(options), do: Read.run(options)
+  def get_error_message({:error, %Tesla.Env{body: body}}), do: parse_api_error(body)
 
-  @impl Airbyte.Source
-  def spec(), do: Spec.run()
+  def get_error_message({:error, msg}), do: "Connection failed with: #{msg}"
+  def get_error_message(_), do: "Connection failed with: unknown error"
+
+  defp parse_api_error(body) do
+    case Jason.decode(body) do
+      {:ok, body} ->
+        "Connection failed with code #{body.error.code}: #{body.error.message}"
+
+      _ ->
+        "Connection failed with: #{body}"
+    end
+  end
 end
