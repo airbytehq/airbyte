be9b1b5bbcd19529093698543223a3d6f8753429
Vitalii Vdovenko <vitalii.vdovenko@zazmic.com>
2020-12-16 15:44:21 +0200

    Microsoft teams source - add pagination for request Microsoft Graph API

--- airbyte-integrations/connectors/source-microsoft-teams/source_microsoft_teams/client.py
+++ airbyte-integrations/connectors/source-microsoft-teams/source_microsoft_teams/client.py
@@ -27,7 +27,7 @@ import pkgutil
 import msal
 import requests
 
-from typing import Dict, List, Optional, Tuple
+from typing import Dict, Optional, Tuple
 from msal.exceptions import MsalServiceError
 
 from airbyte_protocol import AirbyteStream
@@ -36,24 +36,29 @@ from airbyte_protocol import AirbyteStream
 class Client:
     MICROSOFT_GRAPH_BASE_API_URL = 'https://graph.microsoft.com/'
     MICROSOFT_GRAPH_API_VERSION = 'v1.0'
+    PAGINATION_COUNT = 20
 
     def __init__(self, config: Dict):
         self.ENTITY_MAP = {
-            'users': self.get_users,
-            'groups': self.get_groups,
-            'group_members': self.get_group_members,
-            'group_owners': self.get_group_owners,
-            'channels': self.get_channels,
-            'channel_members': self.get_channel_members,
-            'channel_tabs': self.get_channel_tabs,
-            'conversations': self.get_conversations,
-            'conversation_threads': self.get_conversation_threads,
-            'conversation_posts': self.get_conversation_posts,
-            'team_drives': self.get_team_drives,
-            # 'team_device_usage_report': self.get_team_device_usage_report,
+            # 'users': self.get_users,
+            # 'groups': self.get_groups,
+            # 'group_members': self.get_group_members,
+            # 'group_owners': self.get_group_owners,
+            # 'channels': self.get_channels,
+            # 'channel_members': self.get_channel_members,
+            # 'channel_tabs': self.get_channel_tabs,
+            # 'conversations': self.get_conversations,
+            # 'conversation_threads': self.get_conversation_threads,
+            # 'conversation_posts': self.get_conversation_posts,
+            # 'team_drives': self.get_team_drives,
+            'team_device_usage_report': self.get_team_device_usage_report,
         }
         self.configs = config
         self._group_ids = None
+        self.msal_app = msal.ConfidentialClientApplication(self.configs['client_id'],
+                                                           authority=f"https://login.microsoftonline.com/"
+                                                                     f"{self.configs['tenant_id']}",
+                                                           client_credential=self.configs['client_secret'])
 
     def _get_api_url(self, endpoint: str) -> str:
         api_url = f'{self.MICROSOFT_GRAPH_BASE_API_URL}{self.MICROSOFT_GRAPH_API_VERSION}/{endpoint}/'
@@ -61,28 +66,47 @@ class Client:
 
     def _get_access_token(self) -> Dict[str, str]:
         scope = ['https://graph.microsoft.com/.default']
-        app = msal.ConfidentialClientApplication(self.configs['client_id'],
-                                                 authority=f"https://login.microsoftonline.com/"
-                                                           f"{self.configs['tenant_id']}",
-                                                 client_credential=self.configs['client_secret'])
-        result = app.acquire_token_silent(scope, account=None)
+        result = self.msal_app.acquire_token_silent(scope, account=None)
         if not result:
-            result = app.acquire_token_for_client(scopes=scope)
+            result = self.msal_app.acquire_token_for_client(scopes=scope)
         if 'access_token' in result:
             return result['access_token']
         else:
             raise MsalServiceError(error=result.get('error'), error_description=result.get("error_description"))
 
-    def _make_request(self, endpoint: str, params: Optional[Dict] = None) -> List:
-        api_url = self._get_api_url(endpoint)
+    def _make_request(self, api_url: str, params: Optional[Dict] = None) -> Dict:
         access_token = self._get_access_token()
         headers = {
             'Authorization': f'Bearer {access_token}'
         }
         response = requests.get(api_url, headers=headers, params=params)
         raw_response = response.json()
+        return raw_response
+
+    @staticmethod
+    def _post_process_request(raw_response: Dict):
         value = raw_response['value']
         return value
+
+    def _get_request_params(self, params: Optional[Dict] = None, pagination: bool = True) -> Dict:
+        if self.PAGINATION_COUNT and pagination:
+            params = params if params else {}
+            if '$top' not in params:
+                params['$top'] = self.PAGINATION_COUNT
+        return params
+
+    def _fetch_data(self, endpoint: str, params: Optional[Dict] = None, pagination: bool = True):
+        api_url = self._get_api_url(endpoint)
+        params = self._get_request_params(params, pagination)
+        raw_response = self._make_request(api_url, params)
+        while '@odata.nextLink' in raw_response:
+            api_url = raw_response['@odata.nextLink']
+            raw_response = self._make_request(api_url)
+            value = self._post_process_request(raw_response)
+            yield value
+        else:
+            value = self._post_process_request(raw_response)
+            yield value
 
     def health_check(self) -> Tuple[bool, object]:
         try:
@@ -99,77 +123,102 @@ class Client:
         return streams
 
     def get_users(self):
-        users = self._make_request('users')
-        yield users
+        for users in self._fetch_data('users'):
+            yield users
 
     def get_groups(self):
-        groups = filter(lambda item: 'Team' in item['resourceProvisioningOptions'], self._make_request('groups'))
-        yield groups
+        for groups in self._fetch_data('groups'):
+            yield filter(lambda item: 'Team' in item['resourceProvisioningOptions'], groups)
 
     def _get_group_ids(self):
         if not self._group_ids:
+            api_url = self._get_api_url('groups')
             params = {
                 '$select': 'id,resourceProvisioningOptions'
             }
-            self._group_ids = [item["id"] for item in self._make_request('groups', params=params) if
-                               'Team' in item['resourceProvisioningOptions']]
+            groups = self._post_process_request(self._make_request(api_url, params=params))
+            self._group_ids = [item["id"] for item in groups if 'Team' in item['resourceProvisioningOptions']]
         return self._group_ids
 
     def get_group_members(self):
         for group_id in self._get_group_ids():
-            members = self._make_request(f'groups/{group_id}/members')
-            yield members
+            for members in self._fetch_data(f'groups/{group_id}/members'):
+                yield members
 
     def get_group_owners(self):
         for group_id in self._get_group_ids():
-            owners = self._make_request(f'groups/{group_id}/owners')
-            yield owners
+            for owners in self._fetch_data(f'groups/{group_id}/owners'):
+                yield owners
 
     def get_channels(self):
         for group_id in self._get_group_ids():
-            channels = self._make_request(f'teams/{group_id}/channels')
-            yield channels
+            for channels in self._fetch_data(f'teams/{group_id}/channels', pagination=False):
+                yield channels
+
+    def _get_channel_ids(self, group_id: str):
+        api_url = self._get_api_url(f'teams/{group_id}/channels')
+        params = {
+            '$select': 'id'
+        }
+        channels_ids = self._post_process_request(self._make_request(api_url, params=params))
+        return channels_ids
 
     def get_channel_members(self):
         for group_id in self._get_group_ids():
-            channels = self._make_request(f'teams/{group_id}/channels')
+            channels = self._get_channel_ids(group_id=group_id)
             for channel in channels:
-                members = self._make_request(f'teams/{group_id}/channels/{channel["id"]}/members')
-                yield members
+                for members in self._fetch_data(f'teams/{group_id}/channels/{channel["id"]}/members'):
+                    yield members
 
     def get_channel_tabs(self):
         for group_id in self._get_group_ids():
-            channels = self._make_request(f'teams/{group_id}/channels')
+            channels = self._get_channel_ids(group_id=group_id)
             for channel in channels:
-                tabs = self._make_request(f'teams/{group_id}/channels/{channel["id"]}/tabs')
-                yield tabs
+                for tabs in self._fetch_data(f'teams/{group_id}/channels/{channel["id"]}/tabs', pagination=False):
+                    yield tabs
 
     def get_conversations(self):
         for group_id in self._get_group_ids():
-            conversations = self._make_request(f'groups/{group_id}/conversations')
-            yield conversations
+            for conversations in self._fetch_data(f'groups/{group_id}/conversations'):
+                yield conversations
+
+    def _get_conversation_ids(self, group_id: str):
+        api_url = self._get_api_url(f'groups/{group_id}/conversations')
+        params = {
+            '$select': 'id'
+        }
+        conversation_ids = self._post_process_request(self._make_request(api_url, params=params))
+        return conversation_ids
 
     def get_conversation_threads(self):
         for group_id in self._get_group_ids():
-            conversations = self._make_request(f'groups/{group_id}/conversations')
+            conversations = self._get_conversation_ids(group_id=group_id)
             for conversation in conversations:
-                threads = self._make_request(f'groups/{group_id}/conversations/{conversation["id"]}/threads')
-                yield threads
+                for threads in self._fetch_data(f'groups/{group_id}/conversations/{conversation["id"]}/threads'):
+                    yield threads
+
+    def _get_thread_ids(self, group_id: str, conversation_id: str):
+        api_url = self._get_api_url(f'groups/{group_id}/conversations/{conversation_id}/threads')
+        params = {
+            '$select': 'id'
+        }
+        thread_ids = self._post_process_request(self._make_request(api_url, params=params))
+        return thread_ids
 
     def get_conversation_posts(self):
         for group_id in self._get_group_ids():
-            conversations = self._make_request(f'groups/{group_id}/conversations')
+            conversations = self._get_conversation_ids(group_id=group_id)
             for conversation in conversations:
-                threads = self._make_request(f'groups/{group_id}/conversations/{conversation["id"]}/threads')
+                threads = self._get_thread_ids(group_id, conversation["id"])
                 for thread in threads:
-                    posts = self._make_request(
-                        f'groups/{group_id}/conversations/{conversation["id"]}/threads/{thread["id"]}/posts')
-                    yield posts
+                    for posts in self._fetch_data(
+                            f'groups/{group_id}/conversations/{conversation["id"]}/threads/{thread["id"]}/posts'):
+                        yield posts
 
     def get_team_drives(self):
         for group_id in self._get_group_ids():
-            drives = self._make_request(f'groups/{group_id}/drives')
-            yield drives
+            for drives in self._fetch_data(f'groups/{group_id}/drives'):
+                yield drives
 
     def get_team_device_usage_report(self):
         # start_date = self.configs['start_date']
