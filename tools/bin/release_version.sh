c22d754cc6de5975d4f06fe713423ceda7b49f2f
jrhizor <me@jaredrhizor.com>
2020-12-18 10:12:52 -0800

    use separate bump versions

--- tools/bin/release_version.sh
+++ tools/bin/release_version.sh
@@ -4,14 +4,21 @@ set -e
 
 . tools/lib/lib.sh
 
+BRANCH=$(git rev-parse --abbrev-ref HEAD)
+
+if [[ "$BRANCH" == "master" ]]; then
+  echo 'This script should be run from a branch!';
+  exit 1;
+fi
+
 PART_TO_BUMP=$1
 [[ -z "$PART_TO_BUMP" ]] && echo "Usage ./tools/bin/release_version.sh (major|minor|patch)" && exit 1
 
 # uses .bumpversion.cfg to find files to bump
 # requires no git diffs to run
-# commits the bumped versions code  to your branch
+# commits the bumped versions code to your branch
 pip install bumpversion
-VERSION=$(bumpversion --tag-message "Version {new_version}" "$PART_TO_BUMP" | grep new_version | sed -r s,"^.*=",,)
+VERSION=$(bumpversion "$PART_TO_BUMP" | grep new_version | sed -r s,"^.*=",,)
 
 GIT_REVISION=$(git rev-parse HEAD)
 [[ -z "$GIT_REVISION" ]] && echo "Couldn't get the git revision..." && exit 1
@@ -22,7 +29,6 @@ GIT_REVISION=$GIT_REVISION docker-compose -f docker-compose.build.yaml -f docker
 GIT_REVISION=$GIT_REVISION docker-compose -f docker-compose.build.yaml -f docker-compose.yaml push
 echo "Completed building and publishing..."
 
-echo "Pushing bumped version code and tags..."
-git push # pushes code
-git push origin "v$VERSION" # pushes tag
-echo "Completed..."
+echo "Pushing bumped version code..."
+git push
+echo "Completed. After you merge, remember to switch to master, "
