f614308bae76f46f710b808cc1a28cb6a5e7001e
jrhizor <me@jaredrhizor.com>
2020-12-18 09:57:29 -0800

    add support for bumpversion

--- tools/bin/release_version.sh
+++ tools/bin/release_version.sh
@@ -4,13 +4,17 @@ set -e
 
 . tools/lib/lib.sh
 
-VERSION=$(cat .env | grep VERSION= | cut -d= -f 2)
-[[ -z "$VERSION" ]] && echo "Couldn't find version in env file..." && exit 1
+PART_TO_BUMP=$1
+[[ -z "$PART_TO_BUMP" ]] && echo "Usage ./tools/bin/release_version.sh (major|minor|patch)" && exit 1
+
+# uses .bumpversion.cfg to find files to bump
+# requires no git diffs to run
+# commits the bumped versions code  to your branch
+pip install bumpversion
+VERSION=$(bumpversion --tag-message "Version {new_version}" "$PART_TO_BUMP" | grep new_version | sed -r s,"^.*=",,)
 
 GIT_REVISION=$(git rev-parse HEAD)
 [[ -z "$GIT_REVISION" ]] && echo "Couldn't get the git revision..." && exit 1
-
-[[ ! -z "$(git status --porcelain)" ]] && echo "Cannot tag revision if all changes aren't checked in..." && exit 1
 
 echo "Building and publishing version $VERSION for git revision $GIT_REVISION..."
 ./gradlew clean composeBuild
@@ -18,8 +22,7 @@ GIT_REVISION=$GIT_REVISION docker-compose -f docker-compose.build.yaml -f docker
 GIT_REVISION=$GIT_REVISION docker-compose -f docker-compose.build.yaml -f docker-compose.yaml push
 echo "Completed building and publishing..."
 
-echo "Tagging git revision..."
-TAG_NAME="v$VERSION"
-git tag -a "$TAG_NAME" -m "Version $VERSION"
-git push origin "$TAG_NAME"
+echo "Pushing bumped version code and tags..."
+git push # pushes code
+git push origin "v$VERSION" # pushes tag
 echo "Completed..."
