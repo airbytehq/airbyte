0346bba077a9bbf93331b716287e3011e1f87f93
jrhizor <me@jaredrhizor.com>
2020-12-18 10:16:06 -0800

    re-enable building artifacts

--- tools/bin/release_version.sh
+++ tools/bin/release_version.sh
@@ -23,11 +23,11 @@ bumpversion "$PART_TO_BUMP"
 GIT_REVISION=$(git rev-parse HEAD)
 [[ -z "$GIT_REVISION" ]] && echo "Couldn't get the git revision..." && exit 1
 
-#echo "Building and publishing version $VERSION for git revision $GIT_REVISION..."
-#./gradlew clean composeBuild
-#GIT_REVISION=$GIT_REVISION docker-compose -f docker-compose.build.yaml -f docker-compose.yaml build
-#GIT_REVISION=$GIT_REVISION docker-compose -f docker-compose.build.yaml -f docker-compose.yaml push
-#echo "Completed building and publishing..."
+echo "Building and publishing version $VERSION for git revision $GIT_REVISION..."
+./gradlew clean composeBuild
+GIT_REVISION=$GIT_REVISION docker-compose -f docker-compose.build.yaml -f docker-compose.yaml build
+GIT_REVISION=$GIT_REVISION docker-compose -f docker-compose.build.yaml -f docker-compose.yaml push
+echo "Completed building and publishing..."
 
 echo "Pushing bumped version code..."
 git push
