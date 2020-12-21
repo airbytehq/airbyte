89f00cc12f4b3bcf457349dff80316367a289bc5
Charles <giardina.charles@gmail.com>
2020-12-21 15:55:02 -0800

    remove java destination template (#1405)

--- airbyte-integrations/connector-templates/generator/plopfile.js
+++ airbyte-integrations/connector-templates/generator/plopfile.js
@@ -3,56 +3,15 @@ const fs = require('fs');
 const path = require('path');
 
 module.exports = function (plop) {
-  const javaDestinationInputRoot = '../java-destination';
   const pythonSourceInputRoot = '../source-python';
   const singerSourceInputRoot = '../source-singer';
   const genericSourceInputRoot = '../source-generic';
 
   const basesDir = '../../bases';
   const outputDir = '../../connectors';
-  const javaDestinationOutputRoot = `${outputDir}/destination-{{dashCase name}}`;
   const pythonSourceOutputRoot = `${outputDir}/source-{{dashCase name}}`;
   const singerSourceOutputRoot = `${outputDir}/source-{{dashCase name}}-singer`;
   const genericSourceOutputRoot = `${outputDir}/source-{{dashCase name}}`;
-
-  plop.setGenerator('Java Destination', {
-    description: 'Generate an Airbyte destination written in Java',
-    prompts: [
-      {
-        type: 'input',
-        name: 'name',
-        message: 'destination name (without the "destination" suffix; e.g. "my sql")'
-      }
-    ],
-    actions: [
-      {
-        type: 'add',
-        path: `${javaDestinationOutputRoot}/build.gradle`,
-        templateFile: `${javaDestinationInputRoot}/build.gradle.hbs`,
-      },
-      {
-        type: 'add',
-        path: `${javaDestinationOutputRoot}/src/main/java/io/airbyte/integrations/destination/{{snakeCase name}}/{{properCase name}}Destination.java`,
-        templateFile: `${javaDestinationInputRoot}/Destination.java.hbs`,
-      },
-      {
-        type: 'add',
-        path: `${javaDestinationOutputRoot}/Dockerfile`,
-        templateFile: `${javaDestinationInputRoot}/Dockerfile.hbs`,
-      },
-      {
-        type: 'add',
-        path: `${javaDestinationOutputRoot}/.dockerignore`,
-        templateFile: `${javaDestinationInputRoot}/.dockerignore.hbs`,
-      },
-      {
-        type: 'add',
-        path: `${javaDestinationOutputRoot}/spec.json`,
-        templateFile: `${javaDestinationInputRoot}/spec.json.hbs`,
-      },
-      'Your new connector has been created. Happy coding~~',
-    ],
-  });
 
   plop.setGenerator('Python Source', {
     description: 'Generate an Airbyte Source written in Python',
