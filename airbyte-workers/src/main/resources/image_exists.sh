9ba8410f89fc17000adc90186410969de7af9afa
Sherif A. Nada <snadalive@gmail.com>
2020-12-29 11:14:41 -0800

    Add technical support docs page & better logging when pulling Docker images (#1465)

--- airbyte-workers/src/main/resources/image_exists.sh
+++ airbyte-workers/src/main/resources/image_exists.sh
@@ -18,14 +18,19 @@ main() {
   [[ $# -eq 1 ]] || _usage
   imageName=$1
 
+  echo "Checking if ${imageName} exists..."
   # handle the case where the image exists ONLY on the local machine.
   LOCAL=$(docker_exists_in_local ${imageName})
 
   if [[ $LOCAL -eq 0 ]]; then
+    echo "${imageName} not found locally. Attempting to pull the image..."
     # handle the case where the image exists in the remote and either has never been pulled or has already been pulled
     # and is already up to date.
     RESULT=$(docker pull $imageName 2> /dev/null | awk '/Status: Image is up to date/ || /Status: Downloaded newer image/')
     [ -z "$RESULT" ] && _error "Image does not exist."
+    echo "Pulled ${imageName} from remote."
+  else
+    echo "${imageName} was found locally."
   fi
 
   exit 0
