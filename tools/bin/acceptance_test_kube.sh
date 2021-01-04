ed3f3f640688553d41874ad456b52d3fc964bcb2
jrhizor <me@jaredrhizor.com>
2021-01-04 11:21:23 -0800

    wait BEFORE port forwarding

--- tools/bin/acceptance_test_kube.sh
+++ tools/bin/acceptance_test_kube.sh
@@ -10,9 +10,10 @@ echo "Starting app..."
 
 # Detach so we can run subsequent commands
 kubectl apply -k kube/overlays/dev
-kubectl port-forward svc/airbyte-server-svc 8001:8001 &
 echo "Waiting for services to begin"
 sleep 60
+
+kubectl port-forward svc/airbyte-server-svc 8001:8001 &
 
 echo "Running e2e tests via gradle"
 ./gradlew --no-daemon :airbyte-tests:acceptanceTests --rerun-tasks --scan
