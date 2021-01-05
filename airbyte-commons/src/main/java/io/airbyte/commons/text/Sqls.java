cb1b5da79672b3b0e96ac2e6227ee05b7fef74f3
Michel Tricot <michel@airbyte.io>
2021-01-05 09:50:34 -0800

    add comment

--- airbyte-commons/src/main/java/io/airbyte/commons/text/Sqls.java
+++ airbyte-commons/src/main/java/io/airbyte/commons/text/Sqls.java
@@ -33,6 +33,12 @@ public class Sqls {
     return value.name().toLowerCase();
   }
 
+  /**
+   * Generate a string fragment that can be put in the IN clause of a SQL statement. eg. column IN (value1, value2)
+   * @param values to encode
+   * @param <T> enum type
+   * @return "'value1', 'value2', 'value3'"
+   */
   public static <T extends Enum<T>> String toSqlInFragment(final Iterable<T> values) {
     return StreamSupport.stream(values.spliterator(), false).map(Sqls::toSqlName).map(Names::singleQuote)
         .collect(Collectors.joining(",", "(", ")"));
