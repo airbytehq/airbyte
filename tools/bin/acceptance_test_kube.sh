6baa129643f55e5127f7a83c97366258861144c7
jrhizor <me@jaredrhizor.com>
2021-01-05 14:54:16 -0800

    pare tests down to just acceptance tests

--- tools/bin/acceptance_test_kube.sh
+++ tools/bin/acceptance_test_kube.sh
@@ -13,6 +13,12 @@ echo "Starting app..."
 echo "Applying dev manifests to kubernetes..."
 kubectl apply -k kube/overlays/dev
 
+sleep 30
+
+kubectl get pods
+kubectl describe pods
+kubectl logs svc/airbyte-server-svc
+
 echo "Waiting for server to be ready..."
 kubectl wait --for=condition=Available deployment/airbyte-server --timeout=200s
 
@@ -21,4 +27,4 @@ sleep 30
 kubectl port-forward svc/airbyte-server-svc 8001:8001 &
 
 echo "Running e2e tests via gradle..."
-./gradlew --no-daemon :airbyte-tests:acceptanceTests --rerun-tasks --scan --tests "*AcceptanceTestsKube"
+./gradlew --no-daemon :airbyte-tests:acceptanceTests --scan --tests "*AcceptanceTestsKube"
