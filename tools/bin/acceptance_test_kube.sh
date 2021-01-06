a70a460b3ab4b3e7387b298dc287de64b2ec6b7d
jrhizor <me@jaredrhizor.com>
2021-01-05 18:26:45 -0800

    finish cleaning up script

--- tools/bin/acceptance_test_kube.sh
+++ tools/bin/acceptance_test_kube.sh
@@ -13,29 +13,8 @@ echo "Starting app..."
 echo "Applying dev manifests to kubernetes..."
 kubectl apply -k kube/overlays/dev
 
-sleep 2m
-
-
-echo "Waiting for server to be ready..."
-echo "============"
-echo "============"
-echo "============"
-echo "============"
 kubectl wait --for=condition=Available deployment/airbyte-server --timeout=200s
-kubectl get pods
-kubectl describe pods
-echo "============"
-echo "============"
-echo "============"
-echo "============"
 kubectl wait --for=condition=Available deployment/airbyte-scheduler --timeout=200s
-
-kubectl get pods
-kubectl describe pods
-echo "============"
-echo "============"
-echo "============"
-echo "============"
 
 kubectl port-forward svc/airbyte-server-svc 8001:8001 &
 
