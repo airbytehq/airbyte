6a3b113f723a80cf7561a4fd6db65f91d003d5ba
jrhizor <me@jaredrhizor.com>
2021-01-06 09:46:34 -0800

    wait longer for port to be usable

--- tools/bin/acceptance_test_kube.sh
+++ tools/bin/acceptance_test_kube.sh
@@ -16,6 +16,8 @@ kubectl apply -k kube/overlays/dev
 kubectl wait --for=condition=Available deployment/airbyte-server --timeout=300s || (kubectl describe pods && exit 1)
 kubectl wait --for=condition=Available deployment/airbyte-scheduler --timeout=300s || (kubectl describe pods && exit 1)
 
+sleep 20s
+
 kubectl port-forward svc/airbyte-server-svc 8001:8001 &
 
 echo "Running e2e tests via gradle..."
