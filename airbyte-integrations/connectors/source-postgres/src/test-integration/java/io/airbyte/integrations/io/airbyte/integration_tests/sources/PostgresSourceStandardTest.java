102b432a5b550cc35bfb12c26c62d4f3bddc1633
Charles <giardina.charles@gmail.com>
2021-01-08 14:15:34 -0800

    Migrate Postgres and MySql to use new JdbcSource (#1307)

--- airbyte-integrations/connectors/source-postgres/src/test-integration/java/io/airbyte/integrations/io/airbyte/integration_tests/sources/PostgresSourceStandardTest.java
+++ airbyte-integrations/connectors/source-postgres/src/test-integration/java/io/airbyte/integrations/io/airbyte/integration_tests/sources/PostgresSourceStandardTest.java
@@ -45,7 +45,7 @@ import java.util.List;
 import org.jooq.SQLDialect;
 import org.testcontainers.containers.PostgreSQLContainer;
 
-public class PostgresIntegrationTests extends StandardSourceTest {
+public class PostgresSourceStandardTest extends StandardSourceTest {
 
   private static final String STREAM_NAME = "public.id_and_name";
   private static final String STREAM_NAME2 = "public.starships";
