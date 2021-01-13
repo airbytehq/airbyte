9ab20c7b50eabf56ac6cf84bf675c0fe2313a683
Vitalii Vdovenko <vitalii.vdovenko@zazmic.com>
2021-01-14 01:16:14 +0200

    Github best practices #1563 - installing singer tap from tap-github fork instead PyPi

--- airbyte-integrations/connectors/source-github-singer/setup.py
+++ airbyte-integrations/connectors/source-github-singer/setup.py
@@ -33,7 +33,13 @@ setup(
     package_data={"": ["*.json"]},
     # two sets of dependencies: 1) for main 2) for standard test deps. 2 does not have all of the dependencies of 1, which is we cannot use install_requires.
     extras_require={
-        "main": ["tap-github==1.9.0", "requests", "airbyte-protocol", "base-singer", "base-python"],
+        "main": [
+            "tap-github @ https://github.com/vitaliizazmic/tap-github/tarball/master#egg=tap-github-1.9.1",
+            "requests",
+            "airbyte-protocol",
+            "base-singer",
+            "base-python",
+        ],
         "standardtest": ["airbyte_python_test"],
     },
 )
