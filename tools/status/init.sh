eabf6b841ab86896f1fc49f9aac0ffc6ac6c69a1
jrhizor <me@jaredrhizor.com>
2021-01-15 15:53:32 -0800

    working status reporter for integration tests

--- tools/status/init.sh
+++ tools/status/init.sh
@@ -8,6 +8,11 @@ set -e
 BUCKET=airbyte-status
 PROFILE=prod
 REGION=us-east-2
+S3_DOMAIN="$BUCKET.s3-website.$REGION.amazonaws.com"
+
+export AWS_PAGER=""
+
+echo "This has already been created. Comment out this line if you really want to run this again." && exit 1
 
 echo "Creating bucket..."
 aws s3api create-bucket --bucket "$BUCKET" --region "$REGION"  --create-bucket-configuration LocationConstraint="$REGION" --profile "$PROFILE"
@@ -21,4 +26,13 @@ aws s3 sync "$(pwd)"/tools/status/defaults/ s3://"$BUCKET"/ --profile "$PROFILE"
 echo "Setting bucket as website..."
 aws s3 website s3://"$BUCKET"/ --index-document index.html --error-document error.html --profile "$PROFILE"
 
-echo "Site should be ready at http://$BUCKET.s3-website.$REGION.amazonaws.com"
+
+aws cloudfront create-distribution \
+    --origin-domain-name $S3_DOMAIN \
+    --default-root-object index.html \
+    --profile "$PROFILE"
+
+echo "Site should be ready at http://$S3_DOMAIN"
+echo "1. Add a certificate and cname to the distribution: https://advancedweb.hu/how-to-use-a-custom-domain-on-cloudfront-with-cloudflare-managed-dns/"
+echo "2. Configure a CNAME on Cloudflare for status-api.airbyte.io to point to the bucket!"
+echo "3. Create STATUS_API_AWS_ACCESS_KEY_ID and STATUS_API_AWS_SECRET_ACCESS_KEY Github secrets with access to the bucket."
