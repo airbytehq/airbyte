c1febb2271e058c104280dcd0d608df936cdeb6b
Eugene Kulak <kulak.eugene@gmail.com>
2021-01-19 01:04:41 +0200

    fix merge

--- airbyte-integrations/connectors/source-facebook-marketing/source_facebook_marketing/client/client.py
+++ airbyte-integrations/connectors/source-facebook-marketing/source_facebook_marketing/client/client.py
@@ -22,7 +22,7 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 """
 from abc import ABC, abstractmethod
-from typing import Iterator, Sequence, Tuple, Dict, Any
+from typing import Iterator, Sequence, Tuple, Any, Dict
 
 import backoff
 import pendulum as pendulum
