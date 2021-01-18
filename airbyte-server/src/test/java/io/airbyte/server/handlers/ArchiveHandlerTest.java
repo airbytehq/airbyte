d3fa0ad3fdadbfd1a233b2975e5553f7d06a47d9
cgardens <giardina.charles@gmail.com>
2021-01-18 08:48:40 -0800

    Update WorkspaceRead struct
    * Remove onboardingComplete flag (no longer used).
    
    * Add news, anonymousDataCollection, and securityUpdates to the read struct so that we can continue to do PUT instead of PATH when updating workspaces. workspaces/update was prevously only called once at when Airbyte was started up, but with import / export it can now be called more frequently.

--- airbyte-server/src/test/java/io/airbyte/server/handlers/ArchiveHandlerTest.java
+++ airbyte-server/src/test/java/io/airbyte/server/handlers/ArchiveHandlerTest.java
@@ -96,8 +96,7 @@ public class ArchiveHandlerTest {
         .withEmail("test@airbyte.io")
         .withName("test workspace")
         .withSlug("default")
-        .withInitialSetupComplete(false)
-        .withOnboardingComplete(true);
+        .withInitialSetupComplete(false);
   }
 
   @Test
