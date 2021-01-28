1018c69c4a22e8d271ab115c6f044227ecafb3c1
vitaliizazmic <75620293+vitaliizazmic@users.noreply.github.com>
2021-01-28 05:21:47 +0200

    Looker source #1837  - CI tests failing (#1857)
    Co-authored-by: Sherif Nada <snadalive@gmail.com>

--- airbyte-integrations/connectors/source-looker/setup.py
+++ airbyte-integrations/connectors/source-looker/setup.py
@@ -30,7 +30,7 @@ setup(
     author="Airbyte",
     author_email="contact@airbyte.io",
     packages=find_packages(),
-    install_requires=["airbyte-protocol", "base-python", "requests"],
+    install_requires=["airbyte-protocol", "base-python", "requests", "backoff"],
     package_data={"": ["*.json", "schemas/*.json"]},
     setup_requires=["pytest-runner"],
     tests_require=["pytest"],
