87640555d5b3656841b4318b352c228724c75290
cgardens <giardina.charles@gmail.com>
2021-02-04 15:45:17 -0800

    works

--- airbyte-integrations/connectors/destination-meilisearch/src/main/java/io/airbyte/integrations/destination/meilisearch/MeiliSearchDestination.java
+++ airbyte-integrations/connectors/destination-meilisearch/src/main/java/io/airbyte/integrations/destination/meilisearch/MeiliSearchDestination.java
@@ -25,7 +25,12 @@
 package io.airbyte.integrations.destination.meilisearch;
 
 import com.fasterxml.jackson.databind.JsonNode;
-import io.airbyte.commons.concurrency.VoidCallable;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.google.common.base.Preconditions;
+import com.meilisearch.sdk.Client;
+import com.meilisearch.sdk.Config;
+import com.meilisearch.sdk.Index;
+import io.airbyte.commons.functional.CheckedBiConsumer;
 import io.airbyte.commons.json.Jsons;
 import io.airbyte.commons.text.Names;
 import io.airbyte.integrations.DefaultSpecConnector;
@@ -33,30 +38,31 @@ import io.airbyte.integrations.base.Destination;
 import io.airbyte.integrations.base.DestinationConsumer;
 import io.airbyte.integrations.base.IntegrationRunner;
 import io.airbyte.integrations.destination.NamingConventionTransformer;
-import io.airbyte.integrations.destination.StandardNameTransformer;
-import io.airbyte.integrations.destination.WriteConfig;
 import io.airbyte.integrations.destination.buffered_stream_consumer.BufferedStreamConsumer;
-import io.airbyte.integrations.destination.jdbc.JdbcBufferedConsumerFactory;
 import io.airbyte.protocol.models.AirbyteConnectionStatus;
 import io.airbyte.protocol.models.AirbyteConnectionStatus.Status;
 import io.airbyte.protocol.models.AirbyteMessage;
-import io.airbyte.protocol.models.AirbyteStream;
+import io.airbyte.protocol.models.AirbyteRecordMessage;
+import io.airbyte.protocol.models.CatalogHelpers;
 import io.airbyte.protocol.models.ConfiguredAirbyteCatalog;
 import io.airbyte.protocol.models.ConfiguredAirbyteStream;
-import io.airbyte.protocol.models.ConnectorSpecification;
+import io.airbyte.protocol.models.SyncMode;
+import java.time.Instant;
 import java.util.ArrayList;
-import java.util.List;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import com.meilisearch.sdk.Client;
-import com.meilisearch.sdk.Config;
-import com.meilisearch.sdk.Index;
 
 public class MeiliSearchDestination extends DefaultSpecConnector implements Destination {
 
   private static final Logger LOGGER = LoggerFactory.getLogger(MeiliSearchDestination.class);
+
+  private static final String AB_PK_PREFIX = "_ab_pk_";
 
   // todo (cgardens) - why is this in the Destination interface as opposed to in the JdbcDestination?
   @Override
@@ -67,37 +73,176 @@ public class MeiliSearchDestination extends DefaultSpecConnector implements Dest
   @Override
   public DestinationConsumer<AirbyteMessage> write(JsonNode config, ConfiguredAirbyteCatalog catalog) throws Exception {
     final Client client = getClient(config);
-    createIndices(catalog, client);
+    final Map<String, WriteConfig> indexNameToIndex = createIndices(catalog, client);
 
     return new BufferedStreamConsumer(
         () -> LOGGER.info("Starting write to MeiliSearch."),
-        recordWriterFunction(database, sqlOperations, writeConfigs, catalog),
-        () -> LOGGER.info("Completed writing to MeiliSearch."),
+        recordWriterFunction(indexNameToIndex),
+        (hasFailed) -> LOGGER.info("Completed writing to MeiliSearch. Status: {}", hasFailed ? "FAILED" : "SUCCEEDED"),
         catalog,
-        getStreamNames(catalog);
+        CatalogHelpers.getStreamNames(catalog));
   }
 
-  private static List<Index> createIndices(ConfiguredAirbyteCatalog catalog, Client client) throws Exception {
-    List<Index> list = new ArrayList<>();
-    for (String streamName : getStreamNames(catalog)) {
-      Index index = client.index(streamName);
-      list.add(index);
+  private static Map<String, WriteConfig> createIndices(ConfiguredAirbyteCatalog catalog, Client client) throws Exception {
+    final Map<String, WriteConfig> map = new HashMap<>();
+    for (final ConfiguredAirbyteStream stream : catalog.getStreams()) {
+      final String indexName = getIndexName(stream);
+
+      if (stream.getSyncMode() == SyncMode.FULL_REFRESH && indexExists(client, indexName)) {
+        client.deleteIndex(indexName);
+      }
+
+      final PrimaryKey primaryKey = getPrimaryKey(stream);
+      final Index index = client.getOrCreateIndex(indexName, primaryKey.getArtificialKey());
+      map.put(indexName, new WriteConfig(index, primaryKey));
     }
-    return list;
+    return map;
   }
 
-  private static Set<String> getStreamNames(ConfiguredAirbyteCatalog catalog) {
+  private static class WriteConfig {
+
+    private final Index index;
+    private final PrimaryKey primaryKey;
+
+    public WriteConfig(Index index, PrimaryKey primaryKey) {
+      this.index = index;
+      this.primaryKey = primaryKey;
+    }
+
+    public Index getIndex() {
+      return index;
+    }
+
+    public PrimaryKey getPrimaryKey() {
+      return primaryKey;
+    }
+
+  }
+
+  private static boolean indexExists(Client client, String indexName) throws Exception {
+    return Arrays.stream(client.getIndexList())
+        .map(Index::getUid)
+        .anyMatch(actualIndexName -> actualIndexName.equals(indexName));
+  }
+
+  private static CheckedBiConsumer<String, Stream<AirbyteRecordMessage>, Exception> recordWriterFunction(final Map<String, WriteConfig> indexNameToWriteConfig) {
+    return (streamName, recordStream) -> {
+      final String resolvedIndexName = getIndexName(streamName);
+      if (!indexNameToWriteConfig.containsKey(resolvedIndexName)) {
+        throw new IllegalArgumentException(
+            String.format("Message contained record from a stream that was not in the catalog. \nexpected streams: %s",
+                indexNameToWriteConfig.keySet()));
+      }
+
+      final Index index = indexNameToWriteConfig.get(resolvedIndexName).getIndex();
+      final PrimaryKey primaryKey = indexNameToWriteConfig.get(resolvedIndexName).getPrimaryKey();
+
+      // Only writes the data, not the full AirbyteRecordMessage. This is different from how database
+      // destinations work. There is not really a viable way to "transform" data after it is Meilisearch.
+      // Tools like DBT do not apply. Therefore, we need to try to write data in the most usable format
+      // possible that does not require alteration.
+      final String json = Jsons.serialize(recordStream
+          .map(AirbyteRecordMessage::getData)
+          // meilisearch document ids can only have alphanumeric characters and _ (docs:
+          // https://docs.meilisearch.com/learn/core_concepts/documents.html#primary-field). thus, we create
+          // an "artificial" field where we take the contents of the field being used as the primary key and
+          // make it compatible with the meilisearch document id format.
+          .filter(o -> {
+            if (o.has(primaryKey.getOriginalKey())) {
+              return true;
+            } else {
+              LOGGER.warn("filtering record because it does not contain a primary key. stream: {} primary key: {} record: {}", streamName,
+                  primaryKey.getOriginalKey(), o);
+              return false;
+            }
+          })
+          .peek(o -> {
+            LOGGER.info("key:value, {}:{}", primaryKey.getOriginalKey(), o.get(primaryKey.getOriginalKey()).asText());
+            ((ObjectNode) o).put(primaryKey.getArtificialKey(), Names.toAlphanumericAndUnderscore(o.get(primaryKey.getOriginalKey()).asText()));
+          })
+          .collect(Collectors.toList()));
+      LOGGER.info("records to write {}:", json);
+      final String s = index.addDocuments(json);
+      LOGGER.info("add docs response {}", s);
+      LOGGER.info("waiting for update started{}", Instant.now());
+      try {
+        index.waitForPendingUpdate(Jsons.deserialize(s).get("updateId").asInt());
+      } catch (Exception e) {
+        LOGGER.error("waiting for update failed.", e);
+        LOGGER.error("printing meilisearch update statuses: {}", Arrays.asList(index.getUpdates()));
+        throw e;
+      }
+      LOGGER.info("waiting for update completed {}", Instant.now());
+    };
+  }
+
+  private static class PrimaryKey {
+
+    private final String originalKey;
+    private final String artificialKey;
+
+    public PrimaryKey(String originalKey, String artificialKey) {
+      this.originalKey = originalKey;
+      this.artificialKey = artificialKey;
+    }
+
+    public String getOriginalKey() {
+      return originalKey;
+    }
+
+    public String getArtificialKey() {
+      return artificialKey;
+    }
+
+  }
+
+  private static PrimaryKey getPrimaryKey(ConfiguredAirbyteStream stream) {
+    final ArrayList<String> fieldNames = new ArrayList<>(CatalogHelpers.getTopLevelFieldNames(stream));
+
+    Preconditions.checkState(!fieldNames.isEmpty(), "Cannot infer a primary key from a stream with no fields.");
+
+    // emulating the logic that meilisearch uses to infer ids:
+    // https://github.com/meilisearch/MeiliSearch/blob/master/meilisearch-http/src/routes/document.rs#L199-L206
+    for (String fieldName : fieldNames) {
+      if (fieldName.toLowerCase().contains("id")) {
+        LOGGER.info(
+            "For stream {}, inferred {} as the primary key. Any record for which this field is not present will not be uploaded into MeiliSearch.",
+            stream.getStream().getName(), fieldName);
+        return new PrimaryKey(fieldName, fieldNameToPkFieldName(fieldName));
+      }
+    }
+
+    // if cannot "intelligently" infer the id, just pick the first field.
+    final String fallbackPrimaryKey = fieldNames.get(0);
+    LOGGER.warn(
+        "Could not infer an id field for stream {}. Falling back on {}. Any record for which this field is not present will not be uploaded into MeiliSearch.",
+        stream.getStream().getName(), fallbackPrimaryKey);
+    return new PrimaryKey(fallbackPrimaryKey, fieldNameToPkFieldName(fallbackPrimaryKey));
+  }
+
+  private static String fieldNameToPkFieldName(String fieldName) {
+    return AB_PK_PREFIX + fieldName;
+  }
+
+  private static String getIndexName(String streamName) {
+    return Names.toAlphanumericAndUnderscore(streamName);
+  }
+
+  private static String getIndexName(ConfiguredAirbyteStream stream) {
+    return getIndexName(stream.getStream().getName());
+  }
+
+  private static Set<String> getIndexNames(ConfiguredAirbyteCatalog catalog) {
     return catalog.getStreams()
         .stream()
-        .map(ConfiguredAirbyteStream::getStream)
-        .map(AirbyteStream::getName)
-        .map(Names::toAlphanumericAndUnderscore)
+        .map(MeiliSearchDestination::getIndexName)
         .collect(Collectors.toSet());
   }
 
   @Override
   public AirbyteConnectionStatus check(JsonNode config) throws Exception {
     try {
+      LOGGER.info("config in check {}", config);
       final Client client = getClient(config);
       final Index index = client.index("_airbyte");
       index.addDocuments("[{\"id\": \"_airbyte\" }]");
@@ -105,46 +250,20 @@ public class MeiliSearchDestination extends DefaultSpecConnector implements Dest
       return new AirbyteConnectionStatus().withStatus(Status.SUCCEEDED);
     } catch (Exception e) {
       LOGGER.error("Check connection failed.", e);
-      return new AirbyteConnectionStatus().withStatus(Status.FAILED).withMessage("Check connection failed: "+ e.getMessage());
+      return new AirbyteConnectionStatus().withStatus(Status.FAILED).withMessage("Check connection failed: " + e.getMessage());
     }
   }
 
-  private static Client getClient(JsonNode config) {
-    return new Client(new Config("http://localhost:7700", null));
-//    return new Client(new Config(config.get("host").asText(), config.has("api_key") ? config.get("api_key").asText() : null));
+  static Client getClient(JsonNode config) {
+    // return new Client(new Config("http://localhost:7700", null));
+    return new Client(new Config(config.get("host").asText(), config.has("api_key") ? config.get("api_key").asText() : null));
   }
 
   public static void main(String[] args) throws Exception {
     final Destination destination = new MeiliSearchDestination();
-    LOGGER.info("spec result {}", Jsons.serialize(destination.check(Jsons.emptyObject())));
-//    LOGGER.info("starting destination: {}", MeiliSearchDestination.class);
-//    new IntegrationRunner(destination).run(args);
-//    LOGGER.info("completed destination: {}", MeiliSearchDestination.class);
+    // LOGGER.info("spec result {}", Jsons.serialize(destination.check(Jsons.emptyObject())));
+    LOGGER.info("starting destination: {}", MeiliSearchDestination.class);
+    new IntegrationRunner(destination).run(args);
+    LOGGER.info("completed destination: {}", MeiliSearchDestination.class);
   }
-
-//  public static void main(String[] args) throws Exception {
-//    final String documents = "["
-//        + "{\"book_id\": 123, \"title\": \"Pride and Prejudice\"},"
-//        + "{\"book_id\": 456, \"title\": \"Le Petit Prince\"},"
-//        + "{\"book_id\": 1, \"title\": \"Alice In Wonderland\"},"
-//        + "{\"book_id\": 1344, \"title\": \"The Hobbit\"},"
-//        + "{\"book_id\": 4, \"title\": \"Harry Potter and the Half-Blood Prince\"},"
-//        + "{\"book_id\": 2, \"title\": \"The Hitchhiker\'s Guide to the Galaxy\"}"
-//        + "]";
-//
-//    Client client = new Client(new Config("http://localhost:7700", "masterKey"));
-//
-//    // An index is where the documents are stored.
-//    Index index = client.index("books");
-//
-//    // If the index 'books' does not exist, MeiliSearch creates it when you first add the documents.
-////    index.addDocuments(documents); // => { "updateId": 0 }
-//
-//    System.out.println(index.search("petit"));
-//    System.out.println(index.search("pett"));
-//    System.out.println(index.search("halfblood"));
-//    System.out.println(index.search("halfblood"));
-//    System.out.println(index.search("1344"));
-//  }
-
 }
--- airbyte-integrations/connectors/destination-meilisearch/src/test-integration/java/io/airbyte/integrations/destination/meilisearch/MeiliSearchStandardTest.java
+++ airbyte-integrations/connectors/destination-meilisearch/src/test-integration/java/io/airbyte/integrations/destination/meilisearch/MeiliSearchStandardTest.java
@@ -25,10 +25,43 @@
 package io.airbyte.integrations.destination.meilisearch;
 
 import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
+import com.meilisearch.sdk.Client;
+import com.meilisearch.sdk.Index;
+import io.airbyte.commons.json.Jsons;
+import io.airbyte.commons.stream.MoreStreams;
+import io.airbyte.commons.text.Names;
 import io.airbyte.integrations.standardtest.destination.TestDestination;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.List;
+import java.util.Map.Entry;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtensionContext;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.ArgumentsProvider;
+import org.junit.jupiter.params.provider.ArgumentsSource;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.utility.DockerImageName;
 
 public class MeiliSearchStandardTest extends TestDestination {
+
+  private static final Integer DEFAULT_MEILI_SEARCH_PORT = 7700;
+  private static final Integer EXPOSED_PORT = 7701;
+  // calling localhost from within a docker container on mac.
+  private static final String HOST = "http://host.docker.internal:" + EXPOSED_PORT;
+  // calling localhost.
+  private static final String LOCAL_HOST = "http://localhost:" + EXPOSED_PORT;
+  private static final String API_KEY = "masterKey";
+
+  private Client meiliSearchClient;
+  private GenericContainer<?> genericContainer;
 
   @Override
   protected String getImageName() {
@@ -37,33 +70,79 @@ public class MeiliSearchStandardTest extends TestDestination {
 
   @Override
   protected JsonNode getConfig() {
-    return null;
+    final JsonNode jsonNode = Jsons.jsonNode(ImmutableMap.builder()
+        .put("host", HOST)
+        .put("api_key", API_KEY)
+        .build());
+    System.out.println("jsonNode = " + jsonNode);
+    return jsonNode;
   }
 
   @Override
   protected JsonNode getFailCheckConfig() {
-    return null;
+    final JsonNode invalidConfig = Jsons.clone(getConfig());
+    ((ObjectNode) invalidConfig).put("host", "localhost:7702");
+    return invalidConfig;
   }
 
   @Override
   protected List<JsonNode> retrieveRecords(TestDestinationEnv env, String streamName) throws Exception {
-    return null;
+    final Index index = meiliSearchClient.index(Names.toAlphanumericAndUnderscore(streamName));
+    final String responseString = index.getDocuments();
+    final JsonNode response = Jsons.deserialize(responseString);
+    return MoreStreams.toStream(response.iterator())
+        .peek(r -> MoreStreams.toStream(r.fields()).map(Entry::getKey)
+            .filter(fieldName -> fieldName.startsWith("_ab_pk_"))
+            .findFirst().ifPresent(((ObjectNode) r)::remove))
+        .collect(Collectors.toList());
   }
 
   @Override
-  protected boolean implementsBasicNormalization() {
-    return false;
+  protected void setup(TestDestinationEnv testEnv) throws IOException {
+    final Path meiliSearchDataDir = Files.createTempDirectory(Path.of("/tmp"), "meilisearch-integration-test");
+    meiliSearchDataDir.toFile().deleteOnExit();
+
+    genericContainer = new GenericContainer<>(DockerImageName.parse("getmeili/meilisearch:latest"))
+        .withFileSystemBind(meiliSearchDataDir.toString(), "/data.ms");
+    genericContainer.setPortBindings(ImmutableList.of(EXPOSED_PORT + ":" + DEFAULT_MEILI_SEARCH_PORT));
+    genericContainer.start();
+
+    final JsonNode localHostConfig = Jsons.clone(getConfig());
+    ((ObjectNode) localHostConfig).put("host", LOCAL_HOST);
+    meiliSearchClient = MeiliSearchDestination.getClient(localHostConfig);
   }
 
   @Override
-  protected List<String> resolveIdentifier(String identifier) {
-    return null;
+  protected void tearDown(TestDestinationEnv testEnv) {
+    genericContainer.stop();
   }
 
-  @Override
-  protected void setup(TestDestinationEnv testEnv) {}
+  protected static class MeiliSearchDataArgumentsProvider implements ArgumentsProvider {
 
+    @Override
+    public Stream<? extends Arguments> provideArguments(ExtensionContext context) {
+      return Stream.of(
+          Arguments.of("exchange_rate_messages_for_meilisearch.txt", "exchange_rate_catalog.json"),
+          Arguments.of("edge_case_messages_for_meilisearch.txt", "edge_case_catalog_for_meilisearch.json"));
+    }
+
+  }
+
+  // override base test in TestDestination so that use records and catalogs that work with this
+  // destination. specifically that a primary key is needed.
   @Override
-  protected void tearDown(TestDestinationEnv testEnv) {}
+  @ParameterizedTest
+  @ArgumentsSource(MeiliSearchDataArgumentsProvider.class)
+  public void testSync(String messagesFilename, String catalogFilename) throws Exception {
+    super.testSync(messagesFilename, catalogFilename);
+  }
+
+  // override base test in TestDestination so that use records and catalogs that work with this
+  // destination. specifically that a primary key is needed.
+  @Test
+  @Override
+  public void testIncrementalSync() throws Exception {
+    super.testIncrementalSync("exchange_rate_messages_for_meilisearch.txt", "exchange_rate_catalog_for_meilisearch.json");
+  }
 
 }
