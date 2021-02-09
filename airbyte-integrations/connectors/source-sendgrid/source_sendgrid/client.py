16362f479162b3ad8c8eb3cd9c41ddab39ee8681
ykurochkin <y.kurochkin@zazmic.com>
2021-02-09 15:03:11 +0200

    #1380 Issue: update code after review

--- airbyte-integrations/connectors/source-sendgrid/source_sendgrid/client.py
+++ airbyte-integrations/connectors/source-sendgrid/source_sendgrid/client.py
@@ -22,15 +22,23 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 """
 
+from enum import Enum
 from typing import Generator
 
 import backoff
 import requests
 from base_python import BaseClient
+from base_python.entrypoint import logger
 from requests.models import Response
 from requests.status_codes import codes as status_codes
 
 PAGE_SIZE = 50
+
+
+class Pagination(Enum):
+    Non_pagination = 0
+    Offset = 1
+    Metadata = 2
 
 
 class TooManyRequests(Exception):
@@ -55,76 +63,72 @@ class Client(BaseClient):
     def _request(self, url: str, **kwargs) -> Response:
         response = requests.get(url, headers=self._headers, **kwargs)
         if response.status_code == status_codes.TOO_MANY_REQUESTS:
-            raise TooManyRequests()
+            msg = f"Rate limit error: {response.json()}"
+            logger.error(msg)
+            raise TooManyRequests(msg)
+        elif response.status_code != status_codes.OK:
+            raise Exception(f"Unable to get data, error during request from url: {url}. Error: {response.json()}")
         return response
 
-    def _paginator(self, stream_url: str, pagination_type: str = None, key: str = None) -> Generator[dict, None, None]:
-        if not pagination_type:
+    def _read_data(self, stream_url: str, pagination: Pagination, key: str = None) -> Generator[dict, None, None]:
+        if pagination == Pagination.Non_pagination:
             response = self._request(f"{self.BASE_URL}{stream_url}")
-            if response.status_code == status_codes.OK:
-                yield from response.json()[key] if key else response.json()
-            return
-        elif pagination_type == "offset":
+            yield from response.json()[key] if key else response.json()
+        elif pagination == Pagination.Offset:
             offset = 0
             while True:
                 response = self._request(f"{self.BASE_URL}{stream_url}?limit={PAGE_SIZE}&offset={offset}")
-                if response.status_code != status_codes.OK:
-                    return
                 stream_data = response.json()[key] if key else response.json()
                 yield from stream_data
 
                 if len(stream_data) < PAGE_SIZE:
                     return
                 offset += PAGE_SIZE
-        elif pagination_type == "metadata":
-            response = self._request(f"{self.BASE_URL}{stream_url}", params={"page_size": PAGE_SIZE})
-            if response.status_code != status_codes.OK:
-                return
-            stream_data = (response.json()[key] if key else response.json()) or []
-            yield from stream_data
-
-            while response.json()["_metadata"].get("next", False):
-                response = self._request(response.json()["_metadata"]["next"])
-                if response.status_code != status_codes.OK:
-                    return
+        elif pagination == Pagination.Metadata:
+            next_page = True
+            while next_page:
+                response = self._request(f"{self.BASE_URL}{stream_url}", params={"page_size": PAGE_SIZE})
+                next_page = response.json()["_metadata"].get("next", False)
                 stream_data = (response.json()[key] if key else response.json()) or []
                 yield from stream_data
+        else:
+            raise Exception(f"Unknown pagination type '{pagination.name}'")
 
     def stream__lists(self, fields):
-        yield from self._paginator("marketing/lists", pagination_type="metadata", key="result")
+        yield from self._read_data("marketing/lists", pagination=Pagination.Metadata, key="result")
 
     def stream__campaigns(self, fields):
-        yield from self._paginator("marketing/campaigns", pagination_type="metadata", key="result")
+        yield from self._read_data("marketing/campaigns", pagination=Pagination.Metadata, key="result")
 
     def stream__contacts(self, fields):
-        yield from self._paginator("marketing/contacts", key="result")
+        yield from self._read_data("marketing/contacts", pagination=Pagination.Non_pagination, key="result")
 
     def stream__stats_automations(self, fields):
-        yield from self._paginator("marketing/stats/automations", pagination_type="metadata", key="results")
+        yield from self._read_data("marketing/stats/automations", pagination=Pagination.Metadata, key="results")
 
     def stream__segments(self, fields):
-        yield from self._paginator("marketing/segments", key="results")
+        yield from self._read_data("marketing/segments", pagination=Pagination.Non_pagination, key="results")
 
     def stream__templates(self, fields):
-        yield from self._paginator("templates?generations=legacy,dynamic", pagination_type="metadata", key="result")
+        yield from self._read_data("templates?generations=legacy,dynamic", pagination=Pagination.Metadata, key="result")
 
     def stream__global_suppressions(self, fields):
-        yield from self._paginator("suppression/unsubscribes", pagination_type="offset")
+        yield from self._read_data("suppression/unsubscribes", pagination=Pagination.Offset)
 
     def stream__suppression_groups(self, fields):
-        yield from self._paginator("asm/groups")
+        yield from self._read_data("asm/groups", pagination=Pagination.Non_pagination)
 
     def stream__suppression_group_members(self, fields):
-        yield from self._paginator("asm/suppressions", pagination_type="offset")
+        yield from self._read_data("asm/suppressions", pagination=Pagination.Offset)
 
     def stream__blocks(self, fields):
-        yield from self._paginator("suppression/blocks", pagination_type="offset")
+        yield from self._read_data("suppression/blocks", pagination=Pagination.Offset)
 
     def stream__bounces(self, fields):
-        yield from self._paginator("suppression/bounces")
+        yield from self._read_data("suppression/bounces", pagination=Pagination.Non_pagination)
 
     def stream__invalid_emails(self, fields):
-        yield from self._paginator("suppression/invalid_emails", pagination_type="offset")
+        yield from self._read_data("suppression/invalid_emails", pagination=Pagination.Offset)
 
     def stream__spam_reports(self, fields):
-        yield from self._paginator("suppression/spam_reports", pagination_type="offset")
+        yield from self._read_data("suppression/spam_reports", pagination=Pagination.Offset)
