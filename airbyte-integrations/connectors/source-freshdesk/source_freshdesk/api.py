b03606c1672ee34498da5583ce51f0005f5de7c0
Eugene Kulak <kulak.eugene@gmail.com>
2021-02-12 10:52:40 +0200

    add conversations, replace python-freshdesk with custom client

---
+++ airbyte-integrations/connectors/source-freshdesk/source_freshdesk/api.py
@@ -0,0 +1,219 @@
+"""
+MIT License
+
+Copyright (c) 2020 Airbyte
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+from abc import ABC, abstractmethod
+from functools import partial
+from typing import Callable, Mapping, Sequence, Iterator, Any, MutableMapping
+
+import requests
+from requests import HTTPError
+
+
+class FreshdeskError(HTTPError):
+    """
+    Base error class.
+    Subclassing HTTPError to avoid breaking existing code that expects only HTTPErrors.
+    """
+
+
+class FreshdeskBadRequest(FreshdeskError):
+    """Most 40X and 501 status codes"""
+
+
+class FreshdeskUnauthorized(FreshdeskError):
+    """401 Unauthorized"""
+
+
+class FreshdeskAccessDenied(FreshdeskError):
+    """403 Forbidden"""
+
+
+class FreshdeskNotFound(FreshdeskError):
+    """404"""
+
+
+class FreshdeskRateLimited(FreshdeskError):
+    """429 Rate Limit Reached"""
+
+
+class FreshdeskServerError(FreshdeskError):
+    """50X errors"""
+
+
+class API:
+    def __init__(self, domain: str, api_key: str, verify: bool = True,
+                 proxies: MutableMapping[str, str] = None):
+        """ Basic HTTP interface to read from endpoints
+        """
+        self._api_prefix = f"https://{domain.rstrip('/')}/api/v2/"
+        self._session = requests.Session()
+        self._session.auth = (api_key, "unused_with_api_key")
+        self._session.verify = verify
+        self._session.proxies = proxies
+        self._session.headers = {"Content-Type": "application/json"}
+
+        if domain.find("freshdesk.com") < 0:
+            raise AttributeError(
+                "Freshdesk v2 API works only via Freshdesk domains and not via custom CNAMEs")
+
+    @staticmethod
+    def _parse_and_handle_errors(req):
+        try:
+            j = req.json()
+        except ValueError:
+            j = {}
+
+        error_message = "Freshdesk Request Failed"
+        if "errors" in j:
+            error_message = "{}: {}".format(j.get("description"), j.get("errors"))
+        elif "message" in j:
+            error_message = j["message"]
+
+        if req.status_code == 400:
+            raise FreshdeskBadRequest(error_message)
+        elif req.status_code == 401:
+            raise FreshdeskUnauthorized(error_message)
+        elif req.status_code == 403:
+            raise FreshdeskAccessDenied(error_message)
+        elif req.status_code == 404:
+            raise FreshdeskNotFound(error_message)
+        elif req.status_code == 429:
+            raise FreshdeskRateLimited(
+                "429 Rate Limit Exceeded: API rate-limit has been reached until {} seconds. See "
+                "http://freshdesk.com/api#ratelimit".format(req.headers.get("Retry-After"))
+            )
+        elif 500 < req.status_code < 600:
+            raise FreshdeskServerError("{}: Server Error".format(req.status_code))
+
+        # Catch any other errors
+        try:
+            req.raise_for_status()
+        except HTTPError as e:
+            raise FreshdeskError("{}: {}".format(e, j))
+
+        return j
+
+    def get(self, url: str, params: Mapping = None):
+        """Wrapper around request.get() to use the API prefix. Returns a JSON response."""
+        params = params or {}
+        req = self._session.get(self._api_prefix + url, params=params)
+        return self._parse_and_handle_errors(req)
+
+
+class StreamAPI(ABC):
+    """Basic stream API that allows to iterate over entities"""
+
+    result_return_limit = 100  # maximum value
+
+    def __init__(self, api: API, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._api = api
+
+    @abstractmethod
+    def list(self, fields: Sequence[str] = None) -> Iterator[dict]:
+        """Iterate over entities"""
+
+    def read(self, getter: Callable, params: Mapping[str, Any] = None) -> Iterator:
+        """Read using getter"""
+        params = params or {}
+        pagination_params = dict(per_page=self.result_return_limit, page=1)
+
+        while True:
+            batch = list(getter(params={**params, **pagination_params}))
+            yield from batch
+
+            if len(batch) < self.result_return_limit:
+                break
+
+            pagination_params['page'] += 1
+
+
+class AgentsAPI(StreamAPI):
+    def list(self, fields: Sequence[str] = None) -> Iterator[dict]:
+        """Iterate over entities"""
+        yield from self.read(partial(self._api.get, url="agents"))
+
+
+class CompaniesAPI(StreamAPI):
+    def list(self, fields: Sequence[str] = None) -> Iterator[dict]:
+        """Iterate over entities"""
+        yield from self.read(partial(self._api.get, url="companies"))
+
+
+class ContactsAPI(StreamAPI):
+    def list(self, fields: Sequence[str] = None) -> Iterator[dict]:
+        """Iterate over entities"""
+        yield from self.read(partial(self._api.get, url="contacts"))
+
+
+class GroupsAPI(StreamAPI):
+    """Only users with admin privileges can access the following APIs."""
+
+    def list(self, fields: Sequence[str] = None) -> Iterator[dict]:
+        """Iterate over entities"""
+        yield from self.read(partial(self._api.get, url="groups"))
+
+
+class RolesAPI(StreamAPI):
+    """Only users with admin privileges can access the following APIs."""
+
+    def list(self, fields: Sequence[str] = None) -> Iterator[dict]:
+        """Iterate over entities"""
+        yield from self.read(partial(self._api.get, url="roles"))
+
+
+class SkillsAPI(StreamAPI):
+    """Only users with admin privileges can access the following APIs."""
+
+    def list(self, fields: Sequence[str] = None) -> Iterator[dict]:
+        """Iterate over entities"""
+        yield from self.read(partial(self._api.get, url="skills"))
+
+
+class SurveysAPI(StreamAPI):
+    def list(self, fields: Sequence[str] = None) -> Iterator[dict]:
+        """Iterate over entities"""
+        yield from self.read(partial(self._api.get, url="surveys"))
+
+
+class TicketsAPI(StreamAPI):
+    def list(self, fields: Sequence[str] = None) -> Iterator[dict]:
+        """Iterate over entities"""
+        params = {"include": "description"}
+        yield from self.read(partial(self._api.get, url="tickets"), params=params)
+
+
+class TimeEntriesAPI(StreamAPI):
+    def list(self, fields: Sequence[str] = None) -> Iterator[dict]:
+        """Iterate over entities"""
+        yield from self.read(partial(self._api.get, url="time_entries"))
+
+
+class ConversationsAPI(StreamAPI):
+    """Notes and Replies"""
+
+    def list(self, fields: Sequence[str] = None) -> Iterator[dict]:
+        """Iterate over entities"""
+        for ticket in TicketsAPI(self._api).list():
+            url = f"tickets/{ticket['id']}/conversations"
+            yield from self.read(partial(self._api.get, url=url))
--- airbyte-integrations/connectors/source-freshdesk/source_freshdesk/client.py
+++ airbyte-integrations/connectors/source-freshdesk/source_freshdesk/client.py
@@ -21,50 +21,37 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 """
-
-from functools import partial
-from typing import Callable, Dict, Mapping, Tuple
+from typing import Mapping, Tuple
 
 from base_python import BaseClient
-from freshdesk.api import API
-from freshdesk.v2.errors import FreshdeskError
 
-DEFAULT_ITEMS_PER_PAGE = 100
-
-
-def paginator(request: Callable, params: Dict, page: int = None, per_page: int = DEFAULT_ITEMS_PER_PAGE, **kwargs):
-    """Split requests in multiple batches and return records as generator"""
-    read_all = page is None
-    page = page or 1
-    while True:
-        rows = request(params={**params, "page": page, "per_page": per_page})
-        yield from rows
-
-        if len(rows) < per_page or not read_all:
-            break
-        page += 1
+from .api import API, AgentsAPI, CompaniesAPI, ContactsAPI, ConversationsAPI, GroupsAPI, RolesAPI, \
+    SkillsAPI, SurveysAPI, TicketsAPI, TimeEntriesAPI, FreshdeskError
 
 
 class Client(BaseClient):
-    ENTITIES = ["agents", "contacts", "companies", "groups", "roles", "skills", "surveys", "tickets", "time_entries"]
-
     def __init__(self, domain, api_key):
-        self._client = API(domain=domain, api_key=api_key, version=2)
+        self._api = API(domain=domain, api_key=api_key)
+        self._apis = {
+            "agents": AgentsAPI(self._api),
+            "companies": CompaniesAPI(self._api),
+            "contacts": ContactsAPI(self._api),
+            "conversations": ConversationsAPI(self._api),
+            "groups": GroupsAPI(self._api),
+            "roles": RolesAPI(self._api),
+            "skills": SkillsAPI(self._api),
+            "surveys": SurveysAPI(self._api),  # need subscription
+            "tickets": TicketsAPI(self._api),
+            "time_entries": TimeEntriesAPI(self._api),
+        }
         super().__init__()
 
-    def list(self, name, **kwargs):
-        # for now exact matching
-        url = name
-        request = partial(self._client._get, url=url)
-        yield from paginator(request, params={}, **kwargs)
-
-    def settings(self, **kwargs):
+    def settings(self):
         url = "settings/helpdesk"
-        request = partial(self._client._get, url=url)
-        return list(paginator(request, params={}, **kwargs))
+        return self._api.get(url)
 
     def _enumerate_methods(self) -> Mapping[str, callable]:
-        return {entity: partial(self.list, name=entity) for entity in self.ENTITIES}
+        return {name: api.list for name, api in self._apis.items()}
 
     def health_check(self) -> Tuple[bool, str]:
         alive = True
