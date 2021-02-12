30142e05beac576923e57467ca64593fa731ab78
Eugene Kulak <kulak.eugene@gmail.com>
2021-02-12 11:36:19 +0200

    format

--- airbyte-integrations/connectors/source-freshdesk/source_freshdesk/client.py
+++ airbyte-integrations/connectors/source-freshdesk/source_freshdesk/client.py
@@ -21,12 +21,25 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 """
+
 from typing import Mapping, Tuple
 
 from base_python import BaseClient
 
-from .api import API, AgentsAPI, CompaniesAPI, ContactsAPI, ConversationsAPI, GroupsAPI, RolesAPI, \
-    SkillsAPI, SurveysAPI, TicketsAPI, TimeEntriesAPI, FreshdeskError
+from .api import (
+    API,
+    AgentsAPI,
+    CompaniesAPI,
+    ContactsAPI,
+    ConversationsAPI,
+    GroupsAPI,
+    RolesAPI,
+    SkillsAPI,
+    SurveysAPI,
+    TicketsAPI,
+    TimeEntriesAPI,
+    FreshdeskError,
+)
 
 
 class Client(BaseClient):
