12f2b826027df28c008a180b6608b61fbb88462d
Michel Tricot <michel@airbyte.io>
2021-02-15 22:48:51 -0800

    progress

--- airbyte-integrations/connector-templates/generator/plopfile.js
+++ airbyte-integrations/connector-templates/generator/plopfile.js
@@ -66,8 +66,9 @@ module.exports = function (plop) {
     description: 'Generate an Airbyte Source written on top of a Singer Tap.',
     prompts: [
       {type: 'input', name: 'name', message: 'Source name, without the "source-" prefix e.g: "google-analytics"', filter: function (name) {
-          return name.endsWith('-singer') ? name.replace(/-singer$/, '') : name;
+        return name.endsWith('-singer') ? name.replace(/-singer$/, '') : name;
       }},
+      {type: 'input', name: 'tap_name', message: 'Singer tap package'},
     ],
     actions: [
       {
