9e8bdddfc28a80f685e213b248bb93cba5f671f9
jrhizor <me@jaredrhizor.com>
2021-03-01 10:33:57 -0800

    remove logging

--- airbyte-integrations/bases/base-normalization/unit_tests/test_stream_processor.py
+++ airbyte-integrations/bases/base-normalization/unit_tests/test_stream_processor.py
@@ -113,12 +113,6 @@ def test_stream_processor_tables_naming(integration_type: str, catalog_file: str
         elif DestinationType.REDSHIFT.value == destination_type.value:
             expected_nested = {table.lower() for table in expected_nested}
 
-    # todo: remove after done iterating
-    table_list = list(tables_registry - expected_top_level)
-    table_list.sort()
-    for table in table_list:
-        print(f'"{table}",')
-
     assert (tables_registry - expected_top_level) == expected_nested
 
 
