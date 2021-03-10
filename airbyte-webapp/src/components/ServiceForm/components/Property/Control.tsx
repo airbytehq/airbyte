8cdfddf5cf7d62e61811737fe5ac2343770dfff3
Artem Astapenko <jamakase54@gmail.com>
2021-03-11 01:11:55 +0300

    Fix textarea for secrets

--- airbyte-webapp/src/components/ServiceForm/components/Property/Control.tsx
+++ airbyte-webapp/src/components/ServiceForm/components/Property/Control.tsx
@@ -3,7 +3,7 @@ import { useIntl } from "react-intl";
 import { useField } from "formik";
 
 import { DropDown, Input, TextArea } from "components";
-import ConfirmationInput from "./ConfirmationInput";
+import ConfirmationControl from "./ConfirmationControl";
 import { FormBaseItem } from "core/form/types";
 
 type IProps = {
@@ -58,17 +58,45 @@ const Control: React.FC<IProps> = ({
         value={value}
       />
     );
+  } else if (property.multiline && !property.isSecret) {
+    return (
+      <TextArea
+        {...field}
+        placeholder={placeholder}
+        autoComplete="off"
+        value={value ?? ""}
+        rows={3}
+      />
+    );
   } else if (property.isSecret) {
     const unfinishedSecret = unfinishedSecrets[fieldName];
+    const isEditInProgress = !!unfinishedSecret;
     return (
-      <ConfirmationInput
-        {...field}
-        autoComplete="off"
-        placeholder={placeholder}
-        type="password"
-        value={value ?? ""}
+      <ConfirmationControl
+        component={
+          property.multiline && isEditInProgress ? (
+            <TextArea
+              {...field}
+              placeholder={placeholder}
+              autoComplete="off"
+              value={value ?? ""}
+              disabled={!isEditInProgress}
+              rows={3}
+            />
+          ) : (
+            <Input
+              {...field}
+              autoComplete="off"
+              placeholder={placeholder}
+              type="password"
+              autoFocus={isEditInProgress}
+              disabled={!isEditInProgress}
+              value={value ?? ""}
+            />
+          )
+        }
         showButtons={!!meta.initialValue}
-        isEditInProgress={!!unfinishedSecret}
+        isEditInProgress={isEditInProgress}
         onDone={() => removeUnfinishedSecret(fieldName)}
         onStart={() => {
           addUnfinishedSecret(fieldName, { startValue: field.value });
@@ -83,16 +111,6 @@ const Control: React.FC<IProps> = ({
             form.setValue(unfinishedSecret.startValue);
           }
         }}
-      />
-    );
-  } else if (property.multiline) {
-    return (
-      <TextArea
-        {...field}
-        placeholder={placeholder}
-        autoComplete="off"
-        value={value ?? ""}
-        rows={3}
       />
     );
   } else {
