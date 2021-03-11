53384ec4bbdb0c3ee38a8b0fe0bf12b441701130
jrhizor <me@jaredrhizor.com>
2021-03-11 10:16:09 -0800

    remove double scheduling

--- airbyte-workers/src/main/java/io/airbyte/workers/temporal/CancellationHandler.java
+++ airbyte-workers/src/main/java/io/airbyte/workers/temporal/CancellationHandler.java
@@ -24,15 +24,10 @@
 
 package io.airbyte.workers.temporal;
 
-import com.google.common.base.Stopwatch;
 import io.airbyte.workers.WorkerException;
 import io.temporal.activity.Activity;
 import io.temporal.activity.ActivityExecutionContext;
 import io.temporal.client.ActivityCompletionException;
-import java.time.Duration;
-import java.util.concurrent.TimeUnit;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public interface CancellationHandler {
 
@@ -40,29 +35,19 @@ public interface CancellationHandler {
 
   class TemporalCancellationHandler implements CancellationHandler {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(TemporalCancellationHandler.class);
-    private static final Duration INTERVAL = Duration.ofSeconds(5);
     final ActivityExecutionContext context;
-    final Stopwatch stopwatch;
 
     public TemporalCancellationHandler() {
       context = Activity.getExecutionContext();
-      stopwatch = Stopwatch.createStarted();
     }
 
     @Override
     public void heartbeat(Runnable callback) throws WorkerException {
-      if (stopwatch.elapsed(TimeUnit.SECONDS) > TimeUnit.SECONDS.convert(INTERVAL)) {
-        try {
-          LOGGER.info("heartbeating...");
-          context.heartbeat(null);
-        } catch (ActivityCompletionException e) {
-          LOGGER.info("running callback...");
-          callback.run();
-          throw new WorkerException("Sync cancelled", e);
-        }
-        stopwatch.reset();
-        stopwatch.start();
+      try {
+        context.heartbeat(null);
+      } catch (ActivityCompletionException e) {
+        callback.run();
+        throw new WorkerException("Sync cancelled", e);
       }
     }
 
