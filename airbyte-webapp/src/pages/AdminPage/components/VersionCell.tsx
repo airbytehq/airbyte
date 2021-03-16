9b5f0b74a7f0c804fb94dc399881493fda1423d0
Artem Astapenko <3767150+Jamakase@users.noreply.github.com>
2021-03-17 02:12:44 +0300

    Jamakase/update connector version (#2462)
    * Add indicator
    
    * Add current and latest version to tables
    
    * Add latest note
    
    * Show indicator in sidebar
    
    * Add improvements to useNotification hook

--- airbyte-webapp/src/pages/AdminPage/components/VersionCell.tsx
+++ airbyte-webapp/src/pages/AdminPage/components/VersionCell.tsx
@@ -1,13 +1,14 @@
 import React from "react";
 import { Formik, Form, FieldProps, Field } from "formik";
 import styled from "styled-components";
-import { FormattedMessage } from "react-intl";
+import { FormattedMessage, useIntl } from "react-intl";
 
 import { Input, Button, Spinner } from "components";
 import { FormContent } from "./PageComponents";
 
 type IProps = {
   version: string;
+  currentVersion: string;
   id: string;
   onChange: ({ version, id }: { version: string; id: string }) => void;
   feedback?: "success" | string;
@@ -18,6 +19,25 @@ const VersionInput = styled(Input)`
   margin-right: 19px;
 `;
 
+const InputField = styled.div<{ showNote?: boolean }>`
+  display: inline-block;
+  position: relative;
+  background: ${({ theme }) => theme.whiteColor};
+
+  &:before {
+    position: absolute;
+    display: ${({ showNote }) => (showNote ? "block" : "none")};
+    content: attr(data-before);
+    color: ${({ theme }) => theme.greyColor40};
+    top: 10px;
+    right: 22px;
+    z-index: 3;
+  }
+  &:focus-within:before {
+    display: none;
+  }
+`;
+
 const SuccessMessage = styled.div`
   color: ${({ theme }) => theme.successColor};
   font-size: 12px;
@@ -25,7 +45,7 @@ const SuccessMessage = styled.div`
   position: absolute;
   text-align: right;
   width: 205px;
-  left: -218px;
+  left: -208px;
   height: 100%;
   display: flex;
   align-items: center;
@@ -39,7 +59,15 @@ const ErrorMessage = styled(SuccessMessage)`
   line-height: 14px;
 `;
 
-const VersionCell: React.FC<IProps> = ({ version, id, onChange, feedback }) => {
+const VersionCell: React.FC<IProps> = ({
+  version,
+  id,
+  onChange,
+  feedback,
+  currentVersion,
+}) => {
+  const formatMessage = useIntl().formatMessage;
+
   const renderFeedback = (
     dirty: boolean,
     isSubmitting: boolean,
@@ -84,10 +112,20 @@ const VersionCell: React.FC<IProps> = ({ version, id, onChange, feedback }) => {
             {renderFeedback(dirty, isSubmitting, feedback)}
             <Field name="version">
               {({ field }: FieldProps<string>) => (
-                <VersionInput {...field} type="text" autoComplete="off" />
+                <InputField
+                  showNote={version === field.value}
+                  data-before={formatMessage({
+                    id: "admin.latestNote",
+                  })}
+                >
+                  <VersionInput {...field} type="text" autoComplete="off" />
+                </InputField>
               )}
             </Field>
-            <Button type="submit" disabled={isSubmitting || !dirty}>
+            <Button
+              type="submit"
+              disabled={(isSubmitting || !dirty) && currentVersion === version}
+            >
               <FormattedMessage id="form.change" />
             </Button>
           </Form>
