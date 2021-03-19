5149f99fbcbb52e8b169daef7b894bad07d7e4d2
Christophe Duong <christophe.duong@gmail.com>
2021-03-19 19:08:35 +0100

    Harmonize /test and /publish commands on github

--- tools/bin/ci_integration_test.sh
+++ tools/bin/ci_integration_test.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+. tools/lib/lib.sh
+
 # runs integration tests for an integration name
 
 connector="$1"
@@ -11,11 +13,17 @@ if [[ "$connector" == "all" ]] ; then
   echo "Running: ./gradlew --no-daemon --scan integrationTest"
   ./gradlew --no-daemon --scan integrationTest
 else
-  selected_integration_test=$(echo "$all_integration_tests" | grep "^$connector$" || echo "")
-  integrationTestCommand=":airbyte-integrations:connectors:$connector:integrationTest"
+  if [[ "$connector" == *"connector"* ]] || [[ "$connector" == *"bases"* ]]; then
+    connector_name=$(echo $connector | cut -d / -f 2)
+    selected_integration_test=$(echo "$all_integration_tests" | grep "^$connector_name$" || echo "")
+    integrationTestCommand="$(_to_gradle_path "airbyte-integrations/$connector" integrationTest)"
+  else
+    selected_integration_test=$(echo "$all_integration_tests" | grep "^$connector$" || echo "")
+    integrationTestCommand=":airbyte-integrations:connectors:$connector:integrationTest"
+  fi
   if [ -n "$selected_integration_test" ] ; then
-    echo "Running: ./gradlew --no-daemon --scan $integrationTestCommand"
-    ./gradlew --no-daemon --scan "$integrationTestCommand"
+    echo "Running: ./gradlew --no-daemon --scan --debug $integrationTestCommand"
+    ./gradlew --no-daemon --scan --debug "$integrationTestCommand"
   else
     echo "Connector '$connector' not found..."
     exit 1
