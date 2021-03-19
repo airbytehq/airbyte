1475fe364f6b1d4c55524f88e4b051c231853e54
Michel Tricot <michel@dataline.io>
2021-03-18 20:34:01 -0700

    Fix OOM in migration (#2520)

--- airbyte-migration/src/main/java/io/airbyte/migrate/Migrate.java
+++ airbyte-migration/src/main/java/io/airbyte/migrate/Migrate.java
@@ -32,6 +32,7 @@ import io.airbyte.commons.lang.Exceptions;
 import io.airbyte.commons.map.MoreMaps;
 import io.airbyte.commons.set.MoreSets;
 import io.airbyte.commons.stream.MoreStreams;
+import io.airbyte.commons.util.AutoCloseableIterator;
 import io.airbyte.commons.version.AirbyteVersion;
 import io.airbyte.commons.yaml.Yamls;
 import io.airbyte.validation.json.JsonSchemaValidator;
@@ -46,7 +47,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.function.Consumer;
-import java.util.stream.BaseStream;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 import org.apache.commons.io.FileUtils;
@@ -120,7 +120,18 @@ public class Migrate {
     final Path tmpOutputDir = Files.createDirectories(migrateRoot.resolve(migration.getVersion()));
 
     // create a map of each input resource path to the input stream.
-    final Map<ResourceId, Stream<JsonNode>> inputData = createInputStreams(migration, migrationInputRoot);
+    final Map<ResourceId, AutoCloseableIterator<JsonNode>> inputData = createInputStreams(migration, migrationInputRoot);
+    final Map<ResourceId, Stream<JsonNode>> inputDataStreams = inputData.entrySet().stream()
+        .collect(Collectors.toMap(
+            Map.Entry::getKey,
+            entry -> MoreStreams.toStream(entry.getValue())
+                .peek(r -> {
+                  try {
+                    jsonSchemaValidator.ensure(migration.getInputSchema().get(entry.getKey()), r);
+                  } catch (JsonValidationException e) {
+                    throw new IllegalArgumentException("Input data schema does not match declared input schema.", e);
+                  }
+                })));
 
     final Map<ResourceId, RecordConsumer> outputStreams = createOutputStreams(migration, tmpOutputDir);
     // make the java compiler happy (it can't resolve that RecordConsumer is, in fact, a
@@ -128,19 +139,19 @@ public class Migrate {
     final Map<ResourceId, Consumer<JsonNode>> outputDataWithGenericType = MigrationUtils.mapRecordConsumerToConsumer(outputStreams);
 
     // do the migration.
-    new MigrateWithMetadata(migration).migrate(inputData, outputDataWithGenericType);
+    new MigrateWithMetadata(migration).migrate(inputDataStreams, outputDataWithGenericType);
 
     // clean up.
-    inputData.values().forEach(BaseStream::close);
+    inputData.values().forEach(v -> Exceptions.toRuntime(v::close));
     outputStreams.values().forEach(v -> Exceptions.toRuntime(v::close));
 
     return tmpOutputDir;
   }
 
-  private Map<ResourceId, Stream<JsonNode>> createInputStreams(Migration migration, Path migrationInputRoot) {
-    final Map<ResourceId, Stream<JsonNode>> resourceIdToInputStreams = MoreMaps.merge(
-        createInputStreamsForResourceType(migration, migrationInputRoot, ResourceType.CONFIG),
-        createInputStreamsForResourceType(migration, migrationInputRoot, ResourceType.JOB));
+  private Map<ResourceId, AutoCloseableIterator<JsonNode>> createInputStreams(Migration migration, Path migrationInputRoot) {
+    final Map<ResourceId, AutoCloseableIterator<JsonNode>> resourceIdToInputStreams = MoreMaps.merge(
+        createInputStreamsForResourceType(migrationInputRoot, ResourceType.CONFIG),
+        createInputStreamsForResourceType(migrationInputRoot, ResourceType.JOB));
 
     try {
       MoreSets.assertEqualsVerbose(migration.getInputSchema().keySet(), resourceIdToInputStreams.keySet());
@@ -150,25 +161,16 @@ public class Migrate {
     return resourceIdToInputStreams;
   }
 
-  private Map<ResourceId, Stream<JsonNode>> createInputStreamsForResourceType(Migration migration,
-                                                                              Path migrationInputRoot,
-                                                                              ResourceType resourceType) {
+  private Map<ResourceId, AutoCloseableIterator<JsonNode>> createInputStreamsForResourceType(Path migrationInputRoot, ResourceType resourceType) {
     final List<Path> inputFilePaths = FileUtils.listFiles(migrationInputRoot.resolve(resourceType.getDirectoryName()).toFile(), null, false)
         .stream()
         .map(File::toPath)
         .collect(Collectors.toList());
 
-    final Map<ResourceId, Stream<JsonNode>> inputData = new HashMap<>();
+    final Map<ResourceId, AutoCloseableIterator<JsonNode>> inputData = new HashMap<>();
     for (final Path absolutePath : inputFilePaths) {
       final ResourceId resourceId = ResourceId.fromRecordFilePath(resourceType, absolutePath);
-      final Stream<JsonNode> recordInputStream = MoreStreams.toStream(Yamls.deserialize(IOs.readFile(absolutePath)).elements())
-          .peek(r -> {
-            try {
-              jsonSchemaValidator.ensure(migration.getInputSchema().get(resourceId), r);
-            } catch (JsonValidationException e) {
-              throw new IllegalArgumentException("Input data schema does not match declared input schema.", e);
-            }
-          });
+      AutoCloseableIterator<JsonNode> recordInputStream = Yamls.deserializeArray(IOs.inputStream(absolutePath));
       inputData.put(resourceId, recordInputStream);
     }
 
