90d3d14417235473ceb184e8e1f9b7dc842e6688
Christophe Duong <christophe.duong@gmail.com>
2021-03-22 14:41:08 +0100

    Remove debug flag

--- tools/bin/ci_integration_test.sh
+++ tools/bin/ci_integration_test.sh
@@ -13,7 +13,7 @@ if [[ "$connector" == "all" ]] ; then
   echo "Running: ./gradlew --no-daemon --scan integrationTest"
   ./gradlew --no-daemon --scan integrationTest
 else
-  if [[ "$connector" == *"connector"* ]] || [[ "$connector" == *"bases"* ]]; then
+  if [[ "$connector" == *"connectors"* ]] || [[ "$connector" == *"bases"* ]]; then
     connector_name=$(echo $connector | cut -d / -f 2)
     selected_integration_test=$(echo "$all_integration_tests" | grep "^$connector_name$" || echo "")
     integrationTestCommand="$(_to_gradle_path "airbyte-integrations/$connector" integrationTest)"
@@ -22,8 +22,8 @@ else
     integrationTestCommand=":airbyte-integrations:connectors:$connector:integrationTest"
   fi
   if [ -n "$selected_integration_test" ] ; then
-    echo "Running: ./gradlew --no-daemon --scan --debug $integrationTestCommand"
-    ./gradlew --no-daemon --scan --debug "$integrationTestCommand"
+    echo "Running: ./gradlew --no-daemon --scan $integrationTestCommand"
+    ./gradlew --no-daemon --scan "$integrationTestCommand"
   else
     echo "Connector '$connector' not found..."
     exit 1
