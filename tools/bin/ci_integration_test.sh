310e0641b346e3a557f96e7606b51df1b5a730fd
Christophe Duong <christophe.duong@gmail.com>
2021-03-23 10:47:29 +0100

    Harmonize /test and /publish commands on github + Fix MySQL tests in CI (#2531)
    * Harmonize /test and /publish commands on github
    
    * Make normalization build on clean system without PyICU
    
    see https://getdbt.slack.com/archives/C50NEBJGG/p1615803416053800?thread_ts=1615511262.026400&cid=C50NEBJGG
    
    * Follow example from https://github.com/testcontainers/testcontainers-java/pull/2689 to modify MySQL container as root
    
    * Add docs on new syntax for /test

--- tools/bin/ci_integration_test.sh
+++ tools/bin/ci_integration_test.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+. tools/lib/lib.sh
+
 # runs integration tests for an integration name
 
 connector="$1"
@@ -11,8 +13,14 @@ if [[ "$connector" == "all" ]] ; then
   echo "Running: ./gradlew --no-daemon --scan integrationTest"
   ./gradlew --no-daemon --scan integrationTest
 else
-  selected_integration_test=$(echo "$all_integration_tests" | grep "^$connector$" || echo "")
-  integrationTestCommand=":airbyte-integrations:connectors:$connector:integrationTest"
+  if [[ "$connector" == *"connectors"* ]] || [[ "$connector" == *"bases"* ]]; then
+    connector_name=$(echo $connector | cut -d / -f 2)
+    selected_integration_test=$(echo "$all_integration_tests" | grep "^$connector_name$" || echo "")
+    integrationTestCommand="$(_to_gradle_path "airbyte-integrations/$connector" integrationTest)"
+  else
+    selected_integration_test=$(echo "$all_integration_tests" | grep "^$connector$" || echo "")
+    integrationTestCommand=":airbyte-integrations:connectors:$connector:integrationTest"
+  fi
   if [ -n "$selected_integration_test" ] ; then
     echo "Running: ./gradlew --no-daemon --scan $integrationTestCommand"
     ./gradlew --no-daemon --scan "$integrationTestCommand"
