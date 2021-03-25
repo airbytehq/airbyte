7101ba70efde6e25716deabc82b3cd627e1503f7
jrhizor <me@jaredrhizor.com>
2021-03-24 17:45:30 -0700

    push current

--- airbyte-integrations/connectors/source-postgres2/src/main/java/io/airbyte/integrations/source/postgres/DPostgresSource.java
+++ airbyte-integrations/connectors/source-postgres2/src/main/java/io/airbyte/integrations/source/postgres/DPostgresSource.java
@@ -27,6 +27,9 @@ package io.airbyte.integrations.source.postgres;
 import static java.lang.Thread.sleep;
 
 import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.JsonNodeCreator;
+import com.fasterxml.jackson.databind.node.JsonNodeFactory;
+import com.google.common.collect.ImmutableMap;
 import io.airbyte.commons.json.Jsons;
 import io.airbyte.commons.util.AutoCloseableIterator;
 import io.airbyte.commons.util.AutoCloseableIterators;
@@ -46,6 +49,7 @@ import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicReference;
 
+import org.apache.commons.lang3.RandomStringUtils;
 import org.checkerframework.checker.units.qual.A;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -54,90 +58,99 @@ public class DPostgresSource {
 
   private static final Logger LOGGER = LoggerFactory.getLogger(DPostgresSource.class);
 
-  public static void main(String[] args) throws IOException, InterruptedException {
+//  public static Properties getProps() {
+//    // Define the configuration for the Debezium Engine with MySQL connector...
+//    // final Properties props = config.asProperties();
+//    final Properties props = new Properties();
+//    props.setProperty("name", "engine");
+//    props.setProperty("plugin.name", "pgoutput");
+//    props.setProperty("connector.class", "io.debezium.connector.postgresql.PostgresConnector");
+//    props.setProperty("offset.storage", "org.apache.kafka.connect.storage.FileOffsetBackingStore");
+//    props.setProperty("offset.storage.file.filename", "/tmp/offsets-" + RandomStringUtils.randomAlphabetic(5) + ".dat");
+//    props.setProperty("offset.flush.interval.ms", "60000");
+//    /* begin connector properties */
+//    // .with("database.server.name", "orders")
+//    // .with("database.hostname", "localhost")
+//    // .with("database.port", 5432)
+//    // .with("database.user", "postgres")
+//    // .with("database.password", "postgres")
+//    // .with("database.dbname", "demo")
+//    // .with("table.whitelist", "public.orders")
+//
+//    // https://debezium.io/documentation/reference/configuration/avro.html
+//    props.setProperty("key.converter.schemas.enable", "false");
+//    props.setProperty("value.converter.schemas.enable", "false");
+//
+//    // https://debezium.io/documentation/reference/configuration/event-flattening.html
+//    props.setProperty("delete.handling.mode", "rewrite");
+//    props.setProperty("drop.tombstones", "false");
+//    props.setProperty("transforms.unwrap.type", "io.debezium.transforms.ExtractNewRecordState");
+//
+//    props.setProperty("table.include.list", "public.*");
+//    props.setProperty("name", "orders-postgres-connector");
+//    props.setProperty("include_schema_changes", "true");
+//    props.setProperty("database.server.name", "orders");
+//    props.setProperty("database.hostname", "localhost");
+//    props.setProperty("database.port", "5432");
+//    props.setProperty("database.user", "postgres");
+//    props.setProperty("database.password", "");
+//    props.setProperty("database.dbname", "debezium_test");
+//    props.setProperty("database.history", "io.debezium.relational.history.FileDatabaseHistory"); // todo: any reason not to use in memory version and
+//    // reload from
+//    props.setProperty("database.history.file.filename", "/tmp/debezium/dbhistory-" + RandomStringUtils.randomAlphabetic(5) + ".dat");
+//
+//    props.setProperty("slot.name", "debezium");
+//
+//    // can set value.converter.schemas.enabled to false if we want to save space/effort for records
+//
+//    // https://debezium.io/documentation/reference/1.0/connectors/postgresql.html#discrepance-between-plugins
+//
+//    // https://debezium.io/blog/2020/02/25/lessons-learned-running-debezium-with-postgresql-on-rds/
+//
+//    return props;
+//  }
 
-    // Define the configuration for the Debezium Engine with MySQL connector...
-    // final Properties props = config.asProperties();
-    final Properties props = new Properties();
-    props.setProperty("name", "engine");
-    props.setProperty("plugin.name", "pgoutput");
-    props.setProperty("connector.class", "io.debezium.connector.postgresql.PostgresConnector");
-    props.setProperty("offset.storage", "org.apache.kafka.connect.storage.FileOffsetBackingStore");
-    props.setProperty("offset.storage.file.filename", "/tmp/offsets2.dat");
-    props.setProperty("offset.flush.interval.ms", "60000");
-    /* begin connector properties */
-    // .with("database.server.name", "orders")
-    // .with("database.hostname", "localhost")
-    // .with("database.port", 5432)
-    // .with("database.user", "postgres")
-    // .with("database.password", "postgres")
-    // .with("database.dbname", "demo")
-    // .with("table.whitelist", "public.orders")
-
-    // https://debezium.io/documentation/reference/configuration/avro.html
-    props.setProperty("key.converter.schemas.enable", "false");
-    props.setProperty("value.converter.schemas.enable", "false");
-
-    // https://debezium.io/documentation/reference/configuration/event-flattening.html
-    props.setProperty("delete.handling.mode", "rewrite");
-    props.setProperty("drop.tombstones", "false");
-    props.setProperty("transforms.unwrap.type", "io.debezium.transforms.ExtractNewRecordState");
-
-    props.setProperty("table.include.list", "public.*");
-    props.setProperty("name", "orders-postgres-connector");
-    props.setProperty("include_schema_changes", "true");
-    props.setProperty("database.server.name", "orders");
-    props.setProperty("database.hostname", "localhost");
-    props.setProperty("database.port", "5432");
-    props.setProperty("database.user", "postgres");
-    props.setProperty("database.password", "");
-    props.setProperty("database.dbname", "debezium_test");
-    props.setProperty("database.history", "io.debezium.relational.history.FileDatabaseHistory"); // todo: any reason not to use in memory version and
-                                                                                                 // reload from
-    props.setProperty("database.history.file.filename", "/tmp/debezium/dbhistory.dat");
-
-    props.setProperty("slot.name", "debezium");
-
-    // can set value.converter.schemas.enabled to false if we want to save space/effort for records
-
-    // https://debezium.io/documentation/reference/1.0/connectors/postgresql.html#discrepance-between-plugins
-
-    // https://debezium.io/blog/2020/02/25/lessons-learned-running-debezium-with-postgresql-on-rds/
-
-    BlockingQueue<JsonNode> queue = new LinkedBlockingQueue<>();
-    AtomicReference<Throwable> thrownError = new AtomicReference<>();
-    AtomicBoolean completed = new AtomicBoolean(false);
-    ExecutorService executor = Executors.newSingleThreadExecutor();
-
-    // Create the engine with this configuration ...
-    try(AutoCloseableIterator<JsonNode> iterator = getIterator(props, queue, completed, thrownError, executor)) {
-      while(iterator.hasNext()) {
-        LOGGER.info("iterator.next() = " + iterator.next());
-      }
-    } catch (Exception e) {
-      throw new RuntimeException(e);
-    }
-
-    // Do something else or wait for a signal or an event
-    // Engine is stopped when the main code is finished
-  }
+//  public static void main(String[] args) throws IOException, InterruptedException {
+//
+//    BlockingQueue<JsonNode> queue = new LinkedBlockingQueue<>();
+//    AtomicReference<Throwable> thrownError = new AtomicReference<>();
+//    AtomicBoolean completed = new AtomicBoolean(false);
+//    ExecutorService executor = Executors.newSingleThreadExecutor();
+//
+//    // Create the engine with this configuration ...
+//    try(AutoCloseableIterator<JsonNode> iterator = getIterator(getProps(), queue, completed, thrownError, executor)) {
+//      while(iterator.hasNext()) {
+//        LOGGER.info("iterator.next() = " + iterator.next());
+//      }
+//    } catch (Exception e) {
+//      throw new RuntimeException(e);
+//    }
+//
+//    // Do something else or wait for a signal or an event
+//    // Engine is stopped when the main code is finished
+//  }
 
   // todo: https://debezium.io/documentation/reference/configuration/event-flattening.html
 
-  private static AutoCloseableIterator<JsonNode> getIterator(Properties props, BlockingQueue<JsonNode> queue, AtomicBoolean completed, AtomicReference<Throwable> thrownError, ExecutorService executor) {
+  // todo: REPLICA IDENTITY for 'public.id_and_name' is 'DEFAULT'; UPDATE and DELETE events will contain previous values only for PK columns
+
+  public static AutoCloseableIterator<JsonNode> getIterator(Properties props, BlockingQueue<JsonNode> queue, AtomicBoolean completed, AtomicReference<Throwable> thrownError, ExecutorService executor) {
     DebeziumEngine<ChangeEvent<String, String>> engine = DebeziumEngine.create(Json.class)
             .using(props)
             .notifying(record -> {
               try {
-                queue.add(Jsons.jsonNode(record)); // todo: better transformation function here
+                JsonNode node = Jsons.jsonNode(ImmutableMap.of("key", record.key(), "value", record.value(), "destination", record.destination())); // todo: better transformation function here
+                System.out.println("node = " + node);
+                queue.add(node);
               } catch (Exception e) {
                 thrownError.set(e);
               }
             })
-            .using((success, message, error) -> {
-              completed.set(true);
-              thrownError.set(error);
-            })
+//            .using((success, message, error) -> {
+//              System.out.println("completed!");
+//              completed.set(true);
+//              thrownError.set(error);
+//            })
             .build();
 
     // Run the engine asynchronously ...
@@ -148,6 +161,7 @@ public class DPostgresSource {
     return new AutoCloseableIterator<>() {
       @Override
       public void close() throws Exception {
+        System.out.println("in close()");
         engine.close();
         executor.shutdown();
 
@@ -158,11 +172,27 @@ public class DPostgresSource {
 
       @Override
       public boolean hasNext() {
-        return !completed.get() && delegateIterator.hasNext();
+        while(!completed.get()) {
+          if(delegateIterator.hasNext()) {
+            return true;
+          } else {
+            System.out.println("sleeping...");
+            try {
+              Thread.sleep(1000);
+            } catch (InterruptedException e) {
+              e.printStackTrace();
+            }
+          }
+        }
+
+        return false;
       }
+
+      // todo: how to handle locking?
 
       @Override
       public JsonNode next() {
+        System.out.println("in next()");
         return delegateIterator.next();
       }
     };
