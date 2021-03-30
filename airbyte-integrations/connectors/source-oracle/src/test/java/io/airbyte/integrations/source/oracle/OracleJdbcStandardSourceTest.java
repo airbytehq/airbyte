22875c8938264b76e10424c8d129ca3f9d955311
Marcos Marx <marcosmarxm@gmail.com>
2021-03-30 18:31:24 -0300

    create helper method + change print to logger

--- airbyte-integrations/connectors/source-oracle/src/test/java/io/airbyte/integrations/source/oracle/OracleJdbcStandardSourceTest.java
+++ airbyte-integrations/connectors/source-oracle/src/test/java/io/airbyte/integrations/source/oracle/OracleJdbcStandardSourceTest.java
@@ -25,33 +25,17 @@
 package io.airbyte.integrations.source.oracle;
 
 import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.node.ObjectNode;
-import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.ImmutableSet;
-import com.google.common.collect.Lists;
 import io.airbyte.commons.json.Jsons;
-import io.airbyte.commons.util.MoreIterators;
-import io.airbyte.db.Databases;
-import io.airbyte.db.jdbc.JdbcDatabase;
-import io.airbyte.db.jdbc.JdbcUtils;
 import io.airbyte.integrations.source.jdbc.AbstractJdbcSource;
-import io.airbyte.integrations.source.jdbc.models.JdbcState;
-import io.airbyte.integrations.source.jdbc.models.JdbcStreamState;
 import io.airbyte.integrations.source.jdbc.test.JdbcSourceStandardTest;
-import io.airbyte.protocol.models.*;
+import java.math.BigDecimal;
 import org.junit.jupiter.api.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
 import org.testcontainers.containers.OracleContainer;
-
-import java.math.BigDecimal;
 import java.sql.*;
-import java.util.*;
-import java.util.stream.Collectors;
 
 class OracleJdbcStandardSourceTest extends JdbcSourceStandardTest {
 
@@ -62,7 +46,6 @@ class OracleJdbcStandardSourceTest extends JdbcSourceStandardTest {
   static void init() {
     // Oracle returns uppercase values
 
-
     ORACLE_DB = new OracleContainer("epiclabs/docker-oracle-xe-11g")
         .withEnv("NLS_DATE_FORMAT", "YYYY-MM-DD");
     ORACLE_DB.start();
@@ -71,12 +54,13 @@ class OracleJdbcStandardSourceTest extends JdbcSourceStandardTest {
   @BeforeEach
   public void setup() throws Exception {
 
-    SCHEMA_NAME = "JDBC_INTEGRATION_TEST";
+    SCHEMA_NAME = "JDBC_INTEGRATION_TEST1";
     SCHEMA_NAME2 = "JDBC_INTEGRATION_TEST2";
     SCHEMA_NAME3 = "JDBC_MULTI_TABLE";
     SCHEMA_NAME4 = "JDBC_MULTI_TABLE_INCR";
     SCHEMA_NAME5 = "SCHEMA_WITH_SPACE";
-    TEST_SCHEMAS = ImmutableSet.of(SCHEMA_NAME, SCHEMA_NAME2, SCHEMA_NAME3, SCHEMA_NAME4, SCHEMA_NAME5);
+    TEST_SCHEMAS = ImmutableSet
+        .of(SCHEMA_NAME, SCHEMA_NAME2, SCHEMA_NAME3, SCHEMA_NAME4, SCHEMA_NAME5);
 
     TABLE_NAME = "ID_AND_NAME";
     TABLE_NAME_WITH_SPACES = "ID AND NAME";
@@ -88,6 +72,11 @@ class OracleJdbcStandardSourceTest extends JdbcSourceStandardTest {
     COL_FIRST_NAME = "FIRST_NAME";
     COL_LAST_NAME = "LAST_NAME";
     COL_LAST_NAME_WITH_SPACE = "LAST NAME";
+    ID_VALUE_1 = new BigDecimal(1);
+    ID_VALUE_2 = new BigDecimal(2);
+    ID_VALUE_3 = new BigDecimal(3);
+    ID_VALUE_4 = new BigDecimal(4);
+    ID_VALUE_5 = new BigDecimal(5);
 
     config = Jsons.jsonNode(ImmutableMap.builder()
         .put("host", ORACLE_DB.getHost())
@@ -108,8 +97,10 @@ class OracleJdbcStandardSourceTest extends JdbcSourceStandardTest {
     // https://stackoverflow.com/questions/205160/what-can-cause-intermittent-ora-12519-tns-no-appropriate-handler-found-errors
     // sleep for 1000
     executeOracleStatement(String.format("DROP TABLE %s", getFullyQualifiedTableName(TABLE_NAME)));
-    executeOracleStatement(String.format("DROP TABLE %s", getFullyQualifiedTableName(TABLE_NAME_WITHOUT_PK)));
-    executeOracleStatement(String.format("DROP TABLE %s", getFullyQualifiedTableName(TABLE_NAME_COMPOSITE_PK)));
+    executeOracleStatement(
+        String.format("DROP TABLE %s", getFullyQualifiedTableName(TABLE_NAME_WITHOUT_PK)));
+    executeOracleStatement(
+        String.format("DROP TABLE %s", getFullyQualifiedTableName(TABLE_NAME_COMPOSITE_PK)));
     Thread.sleep(1000);
   }
 
@@ -147,7 +138,9 @@ class OracleJdbcStandardSourceTest extends JdbcSourceStandardTest {
     if (supportsSchemas()) {
       for (String schemaName : TEST_SCHEMAS) {
         executeOracleStatement(
-            String.format("CREATE USER %s IDENTIFIED BY password DEFAULT TABLESPACE USERS QUOTA UNLIMITED ON USERS", schemaName)
+            String.format(
+                "CREATE USER %s IDENTIFIED BY password DEFAULT TABLESPACE USERS QUOTA UNLIMITED ON USERS",
+                schemaName)
         );
       }
     }
@@ -162,22 +155,22 @@ class OracleJdbcStandardSourceTest extends JdbcSourceStandardTest {
     try (Statement stmt = conn.createStatement()) {
       stmt.execute(query);
     } catch (SQLException e) {
-      printSQLException(e);
+      logSQLException(e);
     }
     conn.close();
   }
 
-  public static void printSQLException(SQLException ex) {
+  public static void logSQLException(SQLException ex) {
     for (Throwable e : ex) {
       if (e instanceof SQLException) {
         if (ignoreSQLException(((SQLException) e).getSQLState()) == false) {
           e.printStackTrace(System.err);
-          System.err.println("SQLState: " + ((SQLException) e).getSQLState());
-          System.err.println("Error Code: " + ((SQLException) e).getErrorCode());
-          System.err.println("Message: " + e.getMessage());
+          LOGGER.info("SQLState: " + ((SQLException) e).getSQLState());
+          LOGGER.info("Error Code: " + ((SQLException) e).getErrorCode());
+          LOGGER.info("Message: " + e.getMessage());
           Throwable t = ex.getCause();
           while (t != null) {
-            System.out.println("Cause: " + t);
+            LOGGER.info("Cause: " + t);
             t = t.getCause();
           }
         }
@@ -187,18 +180,21 @@ class OracleJdbcStandardSourceTest extends JdbcSourceStandardTest {
 
   public static boolean ignoreSQLException(String sqlState) {
     if (sqlState == null) {
-      System.out.println("The SQL state is not defined!");
+      LOGGER.info("The SQL state is not defined!");
       return false;
     }
     // X0Y32: Jar file already exists in schema
-    if (sqlState.equalsIgnoreCase("X0Y32"))
+    if (sqlState.equalsIgnoreCase("X0Y32")) {
       return true;
+    }
     // 42Y55: Table already exists in schema
-    if (sqlState.equalsIgnoreCase("42Y55"))
+    if (sqlState.equalsIgnoreCase("42Y55")) {
       return true;
+    }
     // 42000: User name already exists
-    if (sqlState.equalsIgnoreCase("42000"))
+    if (sqlState.equalsIgnoreCase("42000")) {
       return true;
+    }
 
     return false;
   }
