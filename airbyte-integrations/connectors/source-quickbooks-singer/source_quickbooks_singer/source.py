1e2fb403d71925169700c639e6db0484a98f859f
Vitalii Vdovenko <vitalii.vdovenko@zazmic.com>
2021-04-06 13:16:14 +0300

    Google audit logs #2694 - configured catalog

--- airbyte-integrations/connectors/source-quickbooks-singer/source_quickbooks_singer/source.py
+++ airbyte-integrations/connectors/source-quickbooks-singer/source_quickbooks_singer/source.py
@@ -26,43 +26,22 @@ import json
 
 from airbyte_protocol import AirbyteConnectionStatus
 from base_python import AirbyteLogger
-from base_singer import SingerSource, Status
+from base_singer import BaseSingerSource, Status
 
 
-class SourceQuickbooksSinger(SingerSource):
-    TAP_CMD = "tap-quickbooks"
+class SourceQuickbooksSinger(BaseSingerSource):
+    tap_cmd = "tap-quickbooks"
+    tap_name = "Quickbooks API"
+    api_error = None
 
-    def check_config(self, logger: AirbyteLogger, config_path: str, config: json) -> AirbyteConnectionStatus:
-        """
-        Tests if the input configuration can be used to successfully connect to the integration
-            e.g: if a provided Stripe API token can be used to connect to the Stripe API.
+    def try_connect(self, logger: AirbyteLogger, config: json):
+        pass
 
-        :param logger: Logging object to display debug/info/error to the logs
-            (logs will not be accessible via airbyte UI if they are not passed to this logger)
-        :param config_path: Path to the file containing the configuration json config
-        :param config: Json object containing the configuration of this source, content of this json is as specified in
-        the properties of the spec.json file
-
-        :return: AirbyteConnectionStatus indicating a Success or Failure
-        """
-        try:
-            # Not Implemented
-
-            return AirbyteConnectionStatus(status=Status.SUCCEEDED)
-        except Exception as e:
-            return AirbyteConnectionStatus(status=Status.FAILED, message=f"An exception occurred: {str(e)}")
-
-    def discover_cmd(self, logger: AirbyteLogger, config_path: str) -> str:
-        """
-        Return the string commands to invoke the tap with the --discover flag and the right configuration options
-        """
-        return f"{self.TAP_CMD} -c {config_path} --discover"
-
-    def read_cmd(self, logger: AirbyteLogger, config_path: str, catalog_path: str, state_path: str = None) -> str:
-        """
-        Return the string commands to invoke the tap with the right configuration options to read data from the source
-        """
-        config_option = f"--config {config_path}"
-        properties_option = f"--catalog {catalog_path}"
-        state_option = f"--state {state_path}" if state_path else ""
-        return f"{self.TAP_CMD} {config_option} {properties_option} {state_option}"
+    # def read_cmd(self, logger: AirbyteLogger, config_path: str, catalog_path: str, state_path: str = None) -> str:
+    #     """
+    #     Return the string commands to invoke the tap with the right configuration options to read data from the source
+    #     """
+    #     config_option = f"--config {config_path}"
+    #     properties_option = f"--catalog {catalog_path}"
+    #     state_option = f"--state {state_path}" if state_path else ""
+    #     return f"{self.TAP_CMD} {config_option} {properties_option} {state_option}"
