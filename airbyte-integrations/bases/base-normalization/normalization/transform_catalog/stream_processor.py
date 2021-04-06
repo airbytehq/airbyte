0a94ec43ffdd44b7b7c7792d5978751286d8dd46
Christophe Duong <christophe.duong@gmail.com>
2021-04-06 21:34:51 +0200

    Add primary tests to TestDestination

--- airbyte-integrations/bases/base-normalization/normalization/transform_catalog/stream_processor.py
+++ airbyte-integrations/bases/base-normalization/normalization/transform_catalog/stream_processor.py
@@ -233,11 +233,6 @@ class StreamProcessor(object):
         for field in self.properties.keys():
             if not is_airbyte_column(field):
                 fields.append(field)
-            if self.destination_sync_mode.value == DestinationSyncMode.append_dedup.value:
-                # When deduping, some airbyte columns could be used as special cursor or primary key columns
-                if field in self.cursor_field[0] or field in [f[0] for f in self.primary_key if len(f) == 1]:
-                    if field not in fields:
-                        fields.append(field)
         result = {}
         field_names = set()
         for field in fields:
@@ -502,7 +497,11 @@ from {{ from_table }}
 
     def get_cursor_field(self, column_names: Dict[str, Tuple[str, str]]) -> str:
         if self.cursor_field and len(self.cursor_field) == 1:
-            return column_names[self.cursor_field[0]][0]
+            if not is_airbyte_column(self.cursor_field[0]):
+                return column_names[self.cursor_field[0]][0]
+            else:
+                # using an airbyte generated column
+                return self.cursor_field[0]
         else:
             if self.cursor_field:
                 raise ValueError(f"Unsupported nested cursor field {'.'.join(self.cursor_field)} for stream {self.stream_name}")
@@ -517,7 +516,11 @@ from {{ from_table }}
 
     def get_primary_key_from_path(self, column_names: Dict[str, Tuple[str, str]], path: List[str]) -> str:
         if path and len(path) == 1:
-            return column_names[path[0]][0]
+            if not is_airbyte_column(path[0]):
+                return column_names[path[0]][0]
+            else:
+                # using an airbyte generated column
+                return path[0]
         else:
             if path:
                 raise ValueError(f"Unsupported nested path {'.'.join(path)} for stream {self.stream_name}")
