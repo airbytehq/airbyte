d0079527ad896a116ba395bf486d701e123ce137
ykurochkin <y.kurochkin@zazmic.com>
2021-04-13 17:44:26 +0300

    base version of Iterable connector

---
+++ airbyte-integrations/connectors/source-iterable/source_iterable/api.py
@@ -0,0 +1,229 @@
+"""
+MIT License
+
+Copyright (c) 2020 Airbyte
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
+import json
+import urllib.parse as urlparse
+from abc import ABC, abstractmethod
+from datetime import datetime
+from typing import Any, Iterable, Mapping, MutableMapping, Optional
+
+import pendulum
+import requests
+from base_python import HttpStream
+
+
+class IterableStream(HttpStream, ABC):
+    url_base = "https://api.iterable.com/api/"
+
+    # Hardcode the value because it is not returned from the API
+    BACKOFF_TIME_CONSTANT = 10.0
+
+    @property
+    @abstractmethod
+    def data_field(self) -> str:
+        """
+        :return: Default field name to get data from response
+        """
+
+    def backoff_time(self, response: requests.Response) -> Optional[float]:
+        return self.BACKOFF_TIME_CONSTANT
+
+    def next_page_token(self, response: requests.Response) -> Optional[Mapping[str, Any]]:
+        return None
+
+    def request_params(
+        self,
+        stream_state: Mapping[str, Any],
+        request_configuration: Optional[Mapping] = None,
+        next_page_token: Mapping[str, Any] = None,
+        parent_stream_record: Mapping = None,
+    ) -> MutableMapping[str, Any]:
+
+        params = self.authenticator.get_auth_params()
+        return params
+
+    def parse_response(self, response: requests.Response, **kwargs) -> Iterable[Mapping]:
+        response_json = response.json()
+        yield from response_json.get(self.data_field, [])
+
+
+class IterableExportStream(IterableStream, ABC):
+    def __init__(self, start_date, **kwargs):
+        super(IterableExportStream, self).__init__(**kwargs)
+        self._start_date = pendulum.parse(start_date)
+
+    def request_params(
+        self,
+        stream_state: Mapping[str, Any],
+        request_configuration: Optional[Mapping] = None,
+        next_page_token: Mapping[str, Any] = None,
+        parent_stream_record: Mapping = None,
+    ) -> MutableMapping[str, Any]:
+
+        params = super(IterableExportStream, self).request_params(stream_state=stream_state)
+        params.update(
+            {"startDateTime": self._start_date.strftime("%Y-%m-%d %H:%M:%S"), "endDateTime": pendulum.now().strftime("%Y-%m-%d %H:%M:%S")}
+        )
+        return params
+
+    def path(self, **kwargs) -> str:
+        return f"/export/data.json?dataTypeName={self.data_field}"
+
+    def parse_response(self, response: requests.Response, **kwargs) -> Iterable[Mapping]:
+        for obj in response.iter_lines():
+            yield json.loads(obj)
+
+
+class Lists(IterableStream):
+    data_field = "lists"
+
+    def path(self, **kwargs) -> str:
+        return "lists"
+
+
+class ListUsers(IterableStream):
+    data_field = "getUsers"
+    name = "list_users"
+
+    def path(self, parent_stream_record, **kwargs) -> str:
+        return f"lists/{self.data_field}?listId={parent_stream_record['id']}"
+
+    def parse_response(self, response: requests.Response, **kwargs) -> Iterable[Mapping]:
+        list_id = self._get_list_id(response.url)
+        for user in response.iter_lines():
+            yield {"email": user.decode(), "listId": list_id, "updatedAt": datetime.now().strftime("%Y-%m-%d %H:%M:%S")}
+
+    @staticmethod
+    def _get_list_id(url: str) -> int:
+        parsed_url = urlparse.urlparse(url)
+        for q in parsed_url.query.split("&"):
+            key, value = q.split("=")
+            if key == "listId":
+                return int(value)
+
+
+class Campaigns(IterableStream):
+    data_field = "campaigns"
+
+    def path(self, **kwargs) -> str:
+        return "campaigns"
+
+
+class Channels(IterableStream):
+    data_field = "channels"
+
+    def path(self, **kwargs) -> str:
+        return "channels"
+
+
+class EmailBounce(IterableExportStream):
+    name = "email_bounce"
+    data_field = "emailBounce"
+
+
+class EmailClick(IterableExportStream):
+    name = "email_click"
+    data_field = "emailClick"
+
+
+class EmailComplaint(IterableExportStream):
+    name = "email_complaint"
+    data_field = "emailComplaint"
+
+
+class EmailOpen(IterableExportStream):
+    name = "email_open"
+    data_field = "emailOpen"
+
+
+class EmailSend(IterableExportStream):
+    name = "email_send"
+    data_field = "emailSend"
+
+
+class EmailSendSkip(IterableExportStream):
+    name = "email_send_skip"
+    data_field = "emailSendSkip"
+
+
+class EmailSubscribe(IterableExportStream):
+    name = "email_subscribe"
+    data_field = "emailSubscribe"
+
+
+class EmailUnsubscribe(IterableExportStream):
+    name = "email_unsubscribe"
+    data_field = "emailUnsubscribe"
+
+
+class MessageTypes(IterableStream):
+    data_field = "messageTypes"
+    name = "message_types"
+
+    def path(self, **kwargs) -> str:
+        return "messageTypes"
+
+
+class Metadata(IterableStream):
+    data_field = "results"
+
+    def path(self, **kwargs) -> str:
+        return "metadata"
+
+
+class Templates(IterableExportStream):
+    data_field = "templates"
+    template_types = ["Base", "Blast", "Triggered", "Workflow"]
+    message_types = ["Email", "Push", "InApp", "SMS"]
+
+    def path(self, **kwargs) -> str:
+        return "templates?templateType={}&messageMedium={}"
+
+    def _list_records(self, stream_state: Mapping[str, Any], parent_stream_record: Mapping[str, Any] = None) -> Iterable[Mapping[str, Any]]:
+        """
+        :param parent_stream_record If this is a child stream, this is a record from the parent stream. Otherwise, this record is None.
+        :return:
+        """
+
+        for templete in self.template_types:
+            for message in self.message_types:
+                args = {"stream_state": stream_state}
+
+                request = self._create_prepared_request(
+                    path=self.path().format(templete, message),
+                    headers=dict(self.request_headers(**args), **self.authenticator.get_auth_header()),
+                    params=self.request_params(**args),
+                    json=self.request_body_json(**args),
+                )
+
+                response = self._send_request(request)
+                yield from self.parse_response(response, stream_state=stream_state)
+
+    def parse_response(self, response: requests.Response, **kwargs) -> Iterable[Mapping]:
+        response_json = response.json()
+        yield from response_json.get(self.data_field, [])
+
+
+class Users(IterableExportStream):
+    data_field = "user"
---
+++ airbyte-integrations/connectors/source-iterable/source_iterable/source.py
@@ -0,0 +1,80 @@
+"""
+MIT License
+
+Copyright (c) 2020 Airbyte
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
+from typing import Any, List, Mapping, Tuple
+
+from base_python import AbstractSource, Stream
+
+from .api import (
+    Campaigns,
+    Channels,
+    EmailBounce,
+    EmailClick,
+    EmailComplaint,
+    EmailOpen,
+    EmailSend,
+    EmailSendSkip,
+    EmailSubscribe,
+    EmailUnsubscribe,
+    Lists,
+    ListUsers,
+    MessageTypes,
+    Metadata,
+    Templates,
+    Users,
+)
+from .auth import ParamsAuthenticator
+
+
+class SourceIterable(AbstractSource):
+    def check_connection(self, logger, config) -> Tuple[bool, any]:
+        try:
+            authenticator = ParamsAuthenticator(config["api_key"])
+            list(Lists(authenticator=authenticator)._list_records(stream_state={}))
+            return True, None
+        except Exception as e:
+            return False, e
+
+    def streams(self, config: Mapping[str, Any]) -> List[Stream]:
+        authenticator = ParamsAuthenticator(config["api_key"])
+
+        lists = Lists(authenticator=authenticator)
+        return [
+            Campaigns(authenticator=authenticator),
+            Channels(authenticator=authenticator),
+            EmailBounce(start_date=config["start_date"], authenticator=authenticator),
+            EmailClick(start_date=config["start_date"], authenticator=authenticator),
+            EmailComplaint(start_date=config["start_date"], authenticator=authenticator),
+            EmailOpen(start_date=config["start_date"], authenticator=authenticator),
+            EmailSend(start_date=config["start_date"], authenticator=authenticator),
+            EmailSendSkip(start_date=config["start_date"], authenticator=authenticator),
+            EmailSubscribe(start_date=config["start_date"], authenticator=authenticator),
+            EmailUnsubscribe(start_date=config["start_date"], authenticator=authenticator),
+            lists,
+            ListUsers(authenticator=authenticator, parent_stream=lists),
+            MessageTypes(authenticator=authenticator),
+            Metadata(authenticator=authenticator),
+            Templates(start_date=config["start_date"], authenticator=authenticator),
+            Users(start_date=config["start_date"], authenticator=authenticator),
+        ]
