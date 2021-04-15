4880c6fb6281f1debf15fcb2f305de5782577723
Sherif A. Nada <snadalive@gmail.com>
2021-04-15 09:45:47 -0700

    Add Nate as author in setup.yp

--- airbyte-integrations/connectors/source-smartsheets/setup.py
+++ airbyte-integrations/connectors/source-smartsheets/setup.py
@@ -27,7 +27,7 @@ from setuptools import find_packages, setup
 setup(
     name="source_smartsheets",
     description="Source implementation for Smartsheets.",
-    author="Airbyte",
+    author="Nate Nowack",
     author_email="contact@airbyte.io",
     packages=find_packages(),
     install_requires=["airbyte-protocol", "base-python", "pytest==6.1.2", "smartsheet-python-sdk"],
