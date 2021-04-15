0e433aa145b5ba187f0fb4090371fa7e0b725144
Nathan Nowack <31014960+zzstoatzz@users.noreply.github.com>
2021-04-15 10:48:30 -0600

    🎉 New Source Connector: Smartsheets  (#2880)

--- airbyte-integrations/connectors/source-smartsheets/main_dev.py
+++ airbyte-integrations/connectors/source-smartsheets/main_dev.py
@@ -22,5 +22,11 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 """
 
-def test_example_method():
-    assert True
+import sys
+
+from base_python.entrypoint import launch
+from source_smartsheets import SourceSmartsheets
+
+if __name__ == "__main__":
+    source = SourceSmartsheets()
+    launch(source, sys.argv[1:])
