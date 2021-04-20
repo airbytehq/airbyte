241b03e579784e89698d1ed967b080a2b7382812
cgardens <giardina.charles@gmail.com>
2021-04-19 17:50:10 -0700

    there is a new groove in town

--- airbyte-integrations/connectors/source-gitlab-singer/unit_tests/unit_test.py
+++ airbyte-integrations/connectors/source-gitlab-singer/unit_tests/unit_test.py
@@ -20,6 +20,5 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-
 def test_example_method():
     assert True
--- airbyte-integrations/connectors/source-google-directory/source_google_directory/utils.py
+++ airbyte-integrations/connectors/source-google-directory/source_google_directory/utils.py
@@ -20,7 +20,6 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-
 def rate_limit_handling(error):
     retried_cases = [
         (403, "quotaExceeded"),
