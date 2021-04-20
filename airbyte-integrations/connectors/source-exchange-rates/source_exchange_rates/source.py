70a05f04d7f1850c7fe48be5f6a1aaf60c31416d
Davin Chia <davinchia@gmail.com>
2021-04-20 18:47:18 +0800

    Implement incremental.

--- airbyte-integrations/connectors/source-exchange-rates/source_exchange_rates/source.py
+++ airbyte-integrations/connectors/source-exchange-rates/source_exchange_rates/source.py
@@ -31,8 +31,11 @@ from pendulum import DateTime
 
 
 class ExchangeRates(HttpStream):
-    url_base = "https://api.ratesapi.io/"
     date_field_name = "date"
+
+    # HttpStream related fields
+    url_base = "https://api.ratesapi.io/"
+    cursor_field = date_field_name
 
     def __init__(self, base: str, start_date: DateTime):
         super().__init__()
@@ -46,10 +49,6 @@ class ExchangeRates(HttpStream):
     def path(
         self, stream_state: Mapping[str, Any] = None, stream_slice: Mapping[str, Any] = None, next_page_token: Mapping[str, Any] = None
     ) -> str:
-        # how do we set the slices? and state? and pull this out?
-        print("path")
-        print(f"{stream_state}")
-        print(f"{stream_slice}")
         return f"api/{stream_slice[self.date_field_name]}"
 
     def request_params(self, **kwargs) -> MutableMapping[str, Any]:
@@ -61,21 +60,16 @@ class ExchangeRates(HttpStream):
         yield from [response_json]
 
     def stream_slices(self, stream_state: Mapping[str, Any] = None, **kwargs) -> Iterable[Optional[Mapping[str, any]]]:
-        print("stream_slices")
         stream_state = stream_state or {}
         start_date = pendulum.parse(stream_state.get(self.date_field_name, self._start_date))
         return chunk_date_range(start_date)
 
     def get_updated_state(self, current_stream_state: MutableMapping[str, Any], latest_record: Mapping[str, Any]):
         current_stream_state = current_stream_state or {}
-        print("get_updated_state")
-        # how do we compare dates?
-        print(f"{latest_record}")
-        # okay how do we convert this from date to timestamp?
         current_stream_state[self.date_field_name] = max(
-            float(latest_record[self.date_field_name]), current_stream_state.get(self.date_field_name, 0)
+            latest_record[self.date_field_name], current_stream_state.get(self.date_field_name, self._start_date)
         )
-        return {}
+        return current_stream_state
 
 
 def chunk_date_range(start_date: DateTime) -> Iterable[Mapping[str, any]]:
@@ -83,12 +77,12 @@ def chunk_date_range(start_date: DateTime) -> Iterable[Mapping[str, any]]:
     Returns a list of the beginning and ending timetsamps of each day between the start date and now.
     The return value is a list of dicts {'oldest': float, 'latest': float} which can be used directly with the Slack API
     """
-    print("chunk_date_range")
     days = []
     now = pendulum.now()
-    while start_date <= now:
-        days.append({"date": f"{start_date.year}-{start_date.month}-{start_date.day}"})
+    while start_date < now:
+        days.append({"date": f"{start_date.to_date_string()}"})
         start_date = start_date.add(days=1)
+
     return days
 
 
