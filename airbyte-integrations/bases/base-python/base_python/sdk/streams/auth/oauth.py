0ad81d915ff28c93358a6f0a4e835637993513c7
Sherif Nada <snadalive@gmail.com>
2021-04-21 17:50:34 -0700

    oauth client plus okta impl

--- airbyte-integrations/bases/base-python/base_python/sdk/streams/auth/oauth.py
+++ airbyte-integrations/bases/base-python/base_python/sdk/streams/auth/oauth.py
@@ -22,36 +22,67 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 """
 
-from typing import Any, Mapping
+from typing import Any, Mapping, Tuple
 
+import pendulum
 import requests
 from base_python.sdk.streams.auth.core import HttpAuthenticator
 
 
 class Oauth2Authenticator(HttpAuthenticator):
     def __init__(
-        self,
-        client_id: str,
-        client_secret: str,
-        refresh_token: str,
-        access_token: str = None,
+            self,
+            token_refresh_endpoint: str,
+            client_id: str,
+            client_secret: str,
+            refresh_token: str
     ):
-        # set expires at timestamp to something in the past.
-        raise NotImplementedError
+        self.token_refresh_endpoint = token_refresh_endpoint
+        self.client_secret = client_secret
+        self.client_id = client_id
+        self.refresh_token = refresh_token
+
+        self._token_expiry_date = pendulum.now().subtract(days=1)
+        self._access_token = None
 
     def get_auth_header(self) -> Mapping[str, Any]:
-
-        # TODO
-        raise NotImplementedError
+        return {'Authorization': f'Bearer {self.get_access_token()}'}
 
     def get_access_token(self):
         if self.token_has_expired():
-            self.refresh_access_token()
+            t0 = pendulum.now()
+            token, expires_in = self.refresh_access_token()
+            self._access_token = token
+            self._token_expiry_date = t0.add(seconds=expires_in)
 
-    def token_has_expired(self, response: requests.Response = None) -> bool:
-        # time-based? Does it require a request?
-        raise NotImplementedError
+        return self._access_token
 
-    def refresh_access_token(self) -> None:
-        # how do we know where to refresh the access token?
-        raise NotImplementedError
+    def token_has_expired(self) -> bool:
+        return pendulum.now() > self._token_expiry_date
+
+    def refresh_access_token(self) -> Tuple[str, int]:
+        """
+        returns a tuple of (access_token, token_lifespan_in_seconds)
+        """
+        data = {
+            'grant_type': 'refresh_token',
+            'client_id': self.client_id,
+            'client_secret': self.client_secret,
+            'refresh_token': self.refresh_token
+        }
+        try:
+            response = requests.request(method='POST', url=self.token_refresh_endpoint, data=data)
+            response.raise_for_status()
+            response_json = response.json()
+            return response_json['access_token'], response_json['expires_in']
+        except Exception as e:
+            raise Exception(f"Error while refreshing access token: {e}") from e
+
+
+authenticator = Oauth2Authenticator(
+    token_refresh_endpoint=f'https://{okta_domain}/oauth2/default/v1/token',
+    client_id=client_id,
+    client_secret=client_secret,
+    refresh_token=refresh_token
+)
+
