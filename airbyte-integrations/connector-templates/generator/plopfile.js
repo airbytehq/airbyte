f16df5b5f4501a45a10b107978b0338f20787069
Davin Chia <davinchia@gmail.com>
2021-04-24 10:57:49 +0800

    Capitalise Source.

--- airbyte-integrations/connector-templates/generator/plopfile.js
+++ airbyte-integrations/connector-templates/generator/plopfile.js
@@ -38,7 +38,7 @@ module.exports = function (plop) {
   });
 
   plop.setGenerator('Python HTTP API Source', {
-    description: 'Generate a source that pulls data from a synchronous HTTP API.',
+    description: 'Generate a Source that pulls data from a synchronous HTTP API.',
     prompts: [{type: 'input', name: 'name', message: 'Source name e.g: "google-analytics"'}],
     actions: [
       {
