e63ab8490aba0e3fd5aa55445aa640cc3bd1198f
Davin Chia <davinchia@gmail.com>
2021-04-27 13:35:35 +0800

    Make Redshift part size configurable. (#3053)
    The Redshift Copy strategy currently has it's part size set to 10 MB. Since S3 allows a file to be broken into max of 10k parts, this results in a 100GB table limit. A user is trying to sync a table of 115GB and running into this issue.
    
    This makes the part size configurable so users can increase this size if needed.

--- airbyte-integrations/connectors/destination-jdbc/src/main/java/io/airbyte/integrations/destination/jdbc/copy/s3/S3Config.java
+++ airbyte-integrations/connectors/destination-jdbc/src/main/java/io/airbyte/integrations/destination/jdbc/copy/s3/S3Config.java
@@ -24,18 +24,22 @@
 
 package io.airbyte.integrations.destination.jdbc.copy.s3;
 
+import com.fasterxml.jackson.databind.JsonNode;
+
 public class S3Config {
 
   private final String bucketName;
   private final String accessKeyId;
   private final String secretAccessKey;
   private final String region;
+  private final Integer partSize;
 
-  public S3Config(String bucketName, String accessKeyId, String secretAccessKey, String region) {
+  public S3Config(String bucketName, String accessKeyId, String secretAccessKey, String region, Integer partSize) {
     this.bucketName = bucketName;
     this.accessKeyId = accessKeyId;
     this.secretAccessKey = secretAccessKey;
     this.region = region;
+    this.partSize = partSize;
   }
 
   public String getBucketName() {
@@ -52,6 +56,23 @@ public class S3Config {
 
   public String getRegion() {
     return region;
+  }
+
+  public Integer getPartSize() {
+    return partSize;
+  }
+
+  public static S3Config getS3Config(JsonNode config) {
+    var partSize = S3StreamCopier.DEFAULT_PART_SIZE_MB;
+    if (config.get("part_size") != null) {
+      partSize = config.get("part_size").asInt();
+    }
+    return new S3Config(
+        config.get("s3_bucket_name").asText(),
+        config.get("access_key_id").asText(),
+        config.get("secret_access_key").asText(),
+        config.get("s3_bucket_region").asText(),
+        partSize);
   }
 
 }
--- airbyte-integrations/connectors/destination-jdbc/src/main/java/io/airbyte/integrations/destination/jdbc/copy/s3/S3StreamCopier.java
+++ airbyte-integrations/connectors/destination-jdbc/src/main/java/io/airbyte/integrations/destination/jdbc/copy/s3/S3StreamCopier.java
@@ -55,7 +55,7 @@ public abstract class S3StreamCopier implements StreamCopier {
   // The smallest part size is 5MB. An S3 upload can be maximally formed of 10,000 parts. This gives
   // us an upper limit of 10,000 * 10 / 1000 = 100 GB per table with a 10MB part size limit.
   // WARNING: Too large a part size can cause potential OOM errors.
-  private static final int PART_SIZE_MB = 10;
+  public static final int DEFAULT_PART_SIZE_MB = 10;
 
   private final String s3StagingFile;
   private final DestinationSyncMode destSyncMode;
@@ -90,7 +90,7 @@ public abstract class S3StreamCopier implements StreamCopier {
     this.sqlOperations = sqlOperations;
 
     this.s3StagingFile = String.join("/", stagingFolder, schemaName, streamName);
-
+    LOGGER.info("S3 upload part size: {} MB", s3Config.getPartSize());
     this.tmpTableName = nameTransformer.getTmpTableName(streamName);
     // The stream transfer manager lets us greedily stream into S3. The native AWS SDK does not
     // have support for streaming multipart uploads;
@@ -103,7 +103,7 @@ public abstract class S3StreamCopier implements StreamCopier {
         new StreamTransferManager(s3Config.getBucketName(), s3StagingFile, client)
             .numUploadThreads(DEFAULT_UPLOAD_THREADS)
             .queueCapacity(DEFAULT_QUEUE_CAPACITY)
-            .partSize(PART_SIZE_MB);
+            .partSize(s3Config.getPartSize());
     // We only need one output stream as we only have one input stream. This is reasonably performant.
     // See the above comment.
     this.outputStream = multipartUploadManager.getMultiPartOutputStreams().get(0);
