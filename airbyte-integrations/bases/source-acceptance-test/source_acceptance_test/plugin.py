88b77aa9f3ae8ca0e0de8d246e3543a13d9e7d19
Eugene Kulak <widowmakerreborn@gmail.com>
2021-04-28 18:11:48 -0400

    Standard tests: update template (#2937)
    * update template for python sources
    * update templates and their docs
    * update docs, rename folders, add acceptance script
    * adding hooks for custom setup and teardown
    + improve plugin behaviour, enable it explicitly
    + add acceptance.py to run acceptance tests together with integration tests
    + remove helper acceptance-test-python.sh
    + fix gradle command for acceptance tests
    + update docs
    
    Co-authored-by: Eugene Kulak <kulak.eugene@gmail.com>
    Co-authored-by: Sherif A. Nada <snadalive@gmail.com>

--- airbyte-integrations/bases/source-acceptance-test/source_acceptance_test/plugin.py
+++ airbyte-integrations/bases/source-acceptance-test/source_acceptance_test/plugin.py
@@ -23,12 +23,25 @@
 
 
 
+from pathlib import Path
+from typing import List
+
 import pytest
+from _pytest.config import Config
+from _pytest.config.argparsing import Parser
 from source_acceptance_test.utils import diff_dicts, load_config
 
 
+HERE = Path(__file__).parent.absolute()
+
+
+def pytest_load_initial_conftests(early_config: Config, parser: Parser, args: List[str]):
+    """Hook function to add acceptance tests to args"""
+    args.append(str(HERE / "tests"))
+
+
 def pytest_addoption(parser):
-    """Hook function to add CLI option `standard_test_config`"""
+    """Hook function to add CLI option `acceptance-test-config`"""
     parser.addoption(
         "--acceptance-test-config", action="store", default=".", help="Folder with standard test config - acceptance_test_config.yml"
     )
