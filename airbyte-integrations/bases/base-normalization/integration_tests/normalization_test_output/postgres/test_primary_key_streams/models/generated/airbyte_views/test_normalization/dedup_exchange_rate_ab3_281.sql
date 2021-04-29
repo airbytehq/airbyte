90224d25804488f83efc6b59f7dbe03fc665b6a1
Christophe Duong <christophe.duong@gmail.com>
2021-04-29 14:32:59 +0200

    Fix normalization Nesting bug (#3110)
    * New test case for nested streams
    
    * Fix filename naming (collisions and nesting)
    
    * Update generated files from tests with new file naming
    
    * Allow invalid json data in raw tables when normalizing on redshift
    
    * Regenerate final sql files
    
    * Disable unit tests on stream naming (temporarly)
    
    * Fix unnesting bug in postgres
    
    * Reactivate unit tests and change table registry
    
    * Move normalization unit tests to integration tests (too slow)
    
    * Remove heavy catalog.json used in unit_tests (actual catalog from facebook/stripe with thousands of lines)
    
    * Bumpversion of normalization image

---
+++ airbyte-integrations/bases/base-normalization/integration_tests/normalization_test_output/postgres/test_primary_key_streams/models/generated/airbyte_views/test_normalization/dedup_exchange_rate_ab3_281.sql
@@ -0,0 +1,15 @@
+{{ config(alias="dedup_exchange_rate_ab3", schema="_airbyte_test_normalization", tags=["top-level-intermediate"]) }}
+-- SQL model to build a hash column based on the values of this record
+select
+    *,
+    {{ dbt_utils.surrogate_key([
+        adapter.quote('id'),
+        'currency',
+        adapter.quote('date'),
+        'hkd',
+        'nzd',
+        'usd',
+    ]) }} as _airbyte_dedup_exchange_rate_hashid
+from {{ ref('dedup_exchange_rate_ab2_281') }}
+-- dedup_exchange_rate
+
---
+++ airbyte-integrations/bases/base-normalization/integration_tests/normalization_test_output/snowflake/test_primary_key_streams/models/generated/airbyte_tables/TEST_NORMALIZATION/DEDUP_EXCHANGE_RATE_81D.sql
@@ -0,0 +1,15 @@
+{{ config(alias="DEDUP_EXCHANGE_RATE", schema="TEST_NORMALIZATION", tags=["top-level"]) }}
+-- Final base SQL model
+select
+    ID,
+    CURRENCY,
+    DATE,
+    HKD,
+    NZD,
+    USD,
+    _airbyte_emitted_at,
+    _AIRBYTE_DEDUP_EXCHANGE_RATE_HASHID
+from {{ ref('DEDUP_EXCHANGE_RATE_SCD_81D') }}
+-- DEDUP_EXCHANGE_RATE from {{ source('TEST_NORMALIZATION', '_AIRBYTE_RAW_DEDUP_EXCHANGE_RATE') }}
+where _airbyte_active_row = True
+
