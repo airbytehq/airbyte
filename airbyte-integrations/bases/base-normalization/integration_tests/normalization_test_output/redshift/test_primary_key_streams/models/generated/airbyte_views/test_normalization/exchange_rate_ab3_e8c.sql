c251ca2f147dc618fb8db07e263dec79702494c5
Christophe Duong <christophe.duong@gmail.com>
2021-04-29 14:32:59 +0200

    Fix normalization Nesting bug (#3110)
    * New test case for nested streams
    
    * Fix filename naming (collisions and nesting)
    
    * Update generated files from tests with new file naming
    
    * Allow invalid json data in raw tables when normalizing on redshift
    
    * Regenerate final sql files
    
    * Disable unit tests on stream naming (temporarly)
    
    * Fix unnesting bug in postgres
    
    * Reactivate unit tests and change table registry
    
    * Move normalization unit tests to integration tests (too slow)
    
    * Remove heavy catalog.json used in unit_tests (actual catalog from facebook/stripe with thousands of lines)
    
    * Bumpversion of normalization image

---
+++ airbyte-integrations/bases/base-normalization/integration_tests/normalization_test_output/redshift/test_primary_key_streams/models/generated/airbyte_views/test_normalization/exchange_rate_ab3_e8c.sql
@@ -0,0 +1,15 @@
+{{ config(alias="exchange_rate_ab3", schema="_airbyte_test_normalization", tags=["top-level-intermediate"]) }}
+-- SQL model to build a hash column based on the values of this record
+select
+    *,
+    {{ dbt_utils.surrogate_key([
+        'id',
+        'currency',
+        'date',
+        'hkd',
+        'nzd',
+        'usd',
+    ]) }} as _airbyte_exchange_rate_hashid
+from {{ ref('exchange_rate_ab2_e8c') }}
+-- exchange_rate
+
