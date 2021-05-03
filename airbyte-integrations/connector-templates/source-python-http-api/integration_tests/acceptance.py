41ad3a9211d643943eb45441cfed8f707a1bd990
Eugene Kulak <widowmakerreborn@gmail.com>
2021-05-03 13:43:10 -0400

    Standard Tests: fix incremental test and docs (#3161)
    * fix cursor comparision in the tests
    
    + update docs and templates
    
    * format
    
    * format
    
    * fix colors
    
    * update acceptance-test-docker.sh script
    
    * fix colors in docker
    
    * Update airbyte-integrations/connector-templates/source-python-http-api/integration_tests/acceptance.py
    
    Co-authored-by: Sherif A. Nada <snadalive@gmail.com>
    
    * Update airbyte-integrations/bases/source-acceptance-test/source_acceptance_test/utils/json_schema_helper.py
    
    Co-authored-by: Sherif A. Nada <snadalive@gmail.com>
    
    * address comments from @sherifnada
    
    Co-authored-by: Eugene Kulak <kulak.eugene@gmail.com>
    Co-authored-by: Sherif A. Nada <snadalive@gmail.com>

--- airbyte-integrations/connector-templates/source-python-http-api/integration_tests/acceptance.py
+++ airbyte-integrations/connector-templates/source-python-http-api/integration_tests/acceptance.py
@@ -20,11 +20,15 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-import sys
 
-from base_python.entrypoint import launch
-from source_{{snakeCase name}} import Source{{properCase name}}
+import pytest
 
-if __name__ == "__main__":
-    source = Source{{properCase name}}()
-    launch(source, sys.argv[1:])
+pytest_plugins = ("source_acceptance_test.plugin",)
+
+
+@pytest.fixture(scope="session", autouse=True)
+def connector_setup():
+    """ This fixture is a placeholder for external resources that acceptance test might require."""
+    # TODO: setup test dependencies if needed. otherwise remove the TODO comments
+    yield
+    # TODO: clean up test dependencies
