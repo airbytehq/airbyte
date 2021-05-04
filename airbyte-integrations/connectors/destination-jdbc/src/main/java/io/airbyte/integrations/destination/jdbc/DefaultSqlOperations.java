19414eeb6b74f4114f3a18eccbafb3477e386930
jrhizor <me@jaredrhizor.com>
2021-05-03 18:50:58 -0700

    postgres but faster

--- airbyte-integrations/connectors/destination-jdbc/src/main/java/io/airbyte/integrations/destination/jdbc/DefaultSqlOperations.java
+++ airbyte-integrations/connectors/destination-jdbc/src/main/java/io/airbyte/integrations/destination/jdbc/DefaultSqlOperations.java
@@ -24,11 +24,31 @@
 
 package io.airbyte.integrations.destination.jdbc;
 
+import io.airbyte.commons.json.Jsons;
 import io.airbyte.db.jdbc.JdbcDatabase;
 import io.airbyte.integrations.base.JavaBaseConstants;
 import io.airbyte.protocol.models.AirbyteRecordMessage;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.Reader;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.time.Instant;
 import java.util.List;
+import java.util.UUID;
+
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVPrinter;
+import org.postgresql.copy.CopyManager;
+import org.postgresql.core.BaseConnection;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -63,22 +83,53 @@ public class DefaultSqlOperations implements SqlOperations {
 
   @Override
   public void insertRecords(JdbcDatabase database, List<AirbyteRecordMessage> records, String schemaName, String tmpTableName) throws SQLException {
+    if (records.isEmpty()) {
+      return;
+    }
 
-    // todo (cgardens) - move this into a postgres version of this. this syntax is VERY postgres
-    // specific.
-    // postgres query syntax:
-    // INSERT INTO public.users (ab_id, data, emitted_at) VALUES
-    // (?, ?::jsonb, ?),
-    // ...
-    final String insertQueryComponent = String.format(
-        "INSERT INTO %s.%s (%s, %s, %s) VALUES\n",
-        schemaName,
-        tmpTableName,
-        JavaBaseConstants.COLUMN_NAME_AB_ID,
-        JavaBaseConstants.COLUMN_NAME_DATA,
-        JavaBaseConstants.COLUMN_NAME_EMITTED_AT);
-    final String recordQueryComponent = "(?, ?::jsonb, ?),\n";
-    SqlOperationsUtils.insertRawRecordsInSingleQuery(insertQueryComponent, recordQueryComponent, database, records);
+    // todo (cgardens) - move this into a postgres version of this. this syntax is postgres-specific
+    database.execute(connection -> {
+      File tmpFile = null;
+      try {
+        tmpFile = Files.createTempFile(tmpTableName+"-", ".tmp").toFile();
+        writeBatchToFile(tmpFile, records);
+
+        var copyManager = new CopyManager(connection.unwrap(BaseConnection.class));
+        var sql = String.format("COPY %s.%s FROM stdin DELIMITER ',' CSV", schemaName, tmpTableName);
+        var bufferedReader = new BufferedReader(new FileReader(tmpFile));
+        copyManager.copyIn(sql, bufferedReader);
+      } catch (Exception e) {
+        throw new RuntimeException(e);
+      } finally {
+          try {
+            if(tmpFile != null) {
+              Files.delete(tmpFile.toPath());
+            }
+          } catch (IOException e) {
+            throw new RuntimeException(e);
+          }
+      }
+    });
+  }
+
+  private void writeBatchToFile(File tmpFile, List<AirbyteRecordMessage> records) throws Exception {
+    PrintWriter writer = null;
+    try {
+      writer = new PrintWriter(tmpFile, StandardCharsets.UTF_8);
+      var csvPrinter = new CSVPrinter(writer, CSVFormat.DEFAULT);
+
+      for (AirbyteRecordMessage record : records) {
+        var uuid = UUID.randomUUID().toString();
+        var jsonData = Jsons.serialize(record.getData());
+        var emittedAt = Timestamp.from(Instant.ofEpochMilli(record.getEmittedAt()));
+        csvPrinter.printRecord(uuid, jsonData, emittedAt);
+      }
+    } finally {
+      if(writer != null) {
+        writer.close();
+      }
+    }
+
   }
 
   @Override
