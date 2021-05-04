8730d1d0de2826f40051574509a4018569de82d5
Jared Rhizor <jared@dataline.io>
2021-05-04 09:59:32 -0700

    snowflake gcs staging (#3183)
    * spec
    
    * snowflake gcs
    
    * spec comment
    
    * fmt
    
    * remove sout
    
    * add docs
    
    * format
    
    * respond to comments
    
    * fix
    
    * bump to 0.3.3

--- airbyte-integrations/connectors/destination-jdbc/src/main/java/io/airbyte/integrations/destination/jdbc/copy/gcs/GcsConfig.java
+++ airbyte-integrations/connectors/destination-jdbc/src/main/java/io/airbyte/integrations/destination/jdbc/copy/gcs/GcsConfig.java
@@ -22,22 +22,39 @@
  * SOFTWARE.
  */
 
-package io.airbyte.integrations.destination.snowflake;
+package io.airbyte.integrations.destination.jdbc.copy.gcs;
 
 import com.fasterxml.jackson.databind.JsonNode;
-import com.google.common.base.Preconditions;
-import io.airbyte.commons.io.IOs;
-import io.airbyte.commons.json.Jsons;
-import java.nio.file.Path;
 
-public class SnowflakeS3CopyIntegrationTest extends SnowflakeInsertIntegrationTest {
+public class GcsConfig {
 
-  @Override
-  public JsonNode getStaticConfig() {
-    final JsonNode copyConfig = Jsons.deserialize(IOs.readFile(Path.of("secrets/copy_s3_config.json")));
-    Preconditions.checkArgument(SnowflakeDestination.isS3Copy(copyConfig));
-    Preconditions.checkArgument(!SnowflakeDestination.isGcsCopy(copyConfig));
-    return copyConfig;
+  private final String projectId;
+  private final String bucketName;
+  private final String credentialsJson;
+
+  public GcsConfig(String projectId, String bucketName, String credentialsJson) {
+    this.projectId = projectId;
+    this.bucketName = bucketName;
+    this.credentialsJson = credentialsJson;
+  }
+
+  public String getProjectId() {
+    return projectId;
+  }
+
+  public String getBucketName() {
+    return bucketName;
+  }
+
+  public String getCredentialsJson() {
+    return credentialsJson;
+  }
+
+  public static GcsConfig getGcsConfig(JsonNode config) {
+    return new GcsConfig(
+        config.get("loading_method").get("project_id").asText(),
+        config.get("loading_method").get("bucket_name").asText(),
+        config.get("loading_method").get("credentials_json").asText());
   }
 
 }
