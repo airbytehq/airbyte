9d0111c715641181baa928409a78b353710cc183
Sherif Nada <snadalive@gmail.com>
2021-05-04 16:26:20 -0700

    cleanup

--- airbyte-cdk/python/unit_tests/base_python/test_schema_helpers.py
+++ airbyte-cdk/python/unit_tests/base_python/test_schema_helpers.py
@@ -20,10 +20,7 @@ def create_and_teardown_schemas_dir():
     os.mkdir(SCHEMAS_ROOT)
     os.mkdir(SCHEMAS_ROOT / "shared")
     yield
-    print("WE ARE HERE")
-    print(f"DELETING {SCHEMAS_ROOT}")
     shutil.rmtree(SCHEMAS_ROOT)
-    print("DONE")
 
 
 def create_schema(name: str, content: Mapping):
@@ -31,48 +28,54 @@ def create_schema(name: str, content: Mapping):
         f.write(json.dumps(content))
 
 
-# Test that a simple schema is loaded correctly
-def test_inline_schema_resolves():
-    expected_schema = {
-        "type": ["null", "object"],
-        "properties": {
-            "str": {"type": "string"},
-            "int": {"type": "integer"},
-            "obj": {"type": ["null", "object"], "properties": {"k1": {"type": "string"}}}}
-    }
+class TestJsonSchemaResolver:
+    # TODO 
+    pass
 
-    create_schema("simple_schema", expected_schema)
-    resolver = ResourceSchemaLoader(MODULE_NAME)
-    actual_schema = resolver.get_schema("simple_schema")
-    assert actual_schema == expected_schema
-
-
-def test_shared_schemas_resolves():
-    expected_schema = {
-        "type": ["null", "object"],
-        "properties": {
-            "str": {"type": "string"},
-            "int": {"type": "integer"},
-            "obj": {"type": ["null", "object"], "properties": {"k1": {"type": "string"}}}}
-    }
-
-    partial_schema = {
-        "type": ["null", "object"],
-        "properties": {
-            "str": {"type": "string"},
-            "int": {"type": "integer"},
-            "obj": {"$ref": "shared_schema.json"}
+class TestResourceSchemaLoader:
+    # Test that a simple schema is loaded correctly
+    @staticmethod
+    def test_inline_schema_resolves():
+        expected_schema = {
+            "type": ["null", "object"],
+            "properties": {
+                "str": {"type": "string"},
+                "int": {"type": "integer"},
+                "obj": {"type": ["null", "object"], "properties": {"k1": {"type": "string"}}}}
         }
-    }
 
-    referenced_schema = {"type": ["null", "object"], "properties": {"k1": {"type": "string"}}}
+        create_schema("simple_schema", expected_schema)
+        resolver = ResourceSchemaLoader(MODULE_NAME)
+        actual_schema = resolver.get_schema("simple_schema")
+        assert actual_schema == expected_schema
 
-    create_schema("complex_schema", partial_schema)
-    create_schema("shared/shared_schema", referenced_schema)
+    @staticmethod
+    def test_shared_schemas_resolves():
+        expected_schema = {
+            "type": ["null", "object"],
+            "properties": {
+                "str": {"type": "string"},
+                "int": {"type": "integer"},
+                "obj": {"type": ["null", "object"], "properties": {"k1": {"type": "string"}}}}
+        }
 
-    resolver = ResourceSchemaLoader(MODULE_NAME)
+        partial_schema = {
+            "type": ["null", "object"],
+            "properties": {
+                "str": {"type": "string"},
+                "int": {"type": "integer"},
+                "obj": {"$ref": "shared_schema.json"}
+            }
+        }
 
-    actual_schema = resolver.get_schema("complex_schema")
-    assert actual_schema == expected_schema
+        referenced_schema = {"type": ["null", "object"], "properties": {"k1": {"type": "string"}}}
+
+        create_schema("complex_schema", partial_schema)
+        create_schema("shared/shared_schema", referenced_schema)
+
+        resolver = ResourceSchemaLoader(MODULE_NAME)
+
+        actual_schema = resolver.get_schema("complex_schema")
+        assert actual_schema == expected_schema
 
 
