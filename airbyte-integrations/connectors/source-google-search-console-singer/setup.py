d5a7d14b7b25250cf96227c14b3c06532843892b
Vitalii Vdovenko <vitalii.vdovenko@zazmic.com>
2021-05-06 16:06:18 +0300

    Google search console source #2257 - disable performance_report_date stream

--- airbyte-integrations/connectors/source-google-search-console-singer/setup.py
+++ airbyte-integrations/connectors/source-google-search-console-singer/setup.py
@@ -24,7 +24,7 @@
 from setuptools import find_packages, setup
 
 MAIN_REQUIREMENTS = [
-    "tap-google-search-console @ https://github.com/vitaliizazmic/tap-google-search-console/tarball/error_handling",
+    "tap-google-search-console @ https://github.com/vitaliizazmic/tap-google-search-console/tarball/disable_performance_report_date",
     "airbyte-protocol",
     "base-singer",
     "base-python",
