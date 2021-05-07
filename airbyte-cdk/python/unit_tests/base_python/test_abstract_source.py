cb69c3ac3b45a92663730a1d6cfbc3e28e04b917
Sherif Nada <snadalive@gmail.com>
2021-05-07 00:43:18 -0700

    test abstract source

--- airbyte-cdk/python/unit_tests/base_python/test_abstract_source.py
+++ airbyte-cdk/python/unit_tests/base_python/test_abstract_source.py
@@ -1,10 +1,12 @@
-from typing import Mapping, Any, List, Tuple, Optional, Callable, Union, Iterable
-from unittest.mock import Mock, MagicMock
+from collections import defaultdict
+from typing import Mapping, Any, List, Tuple, Optional, Callable, Union, Iterable, Dict
 
 import pytest
 
-from airbyte_cdk import AirbyteConnectionStatus, Status, SyncMode, AirbyteStream, AirbyteCatalog
+from airbyte_cdk import AirbyteConnectionStatus, Status, SyncMode, AirbyteStream, AirbyteCatalog, ConfiguredAirbyteCatalog, ConfiguredAirbyteStream, \
+    AirbyteMessage, Type, AirbyteRecordMessage
 from airbyte_cdk.base_python import AbstractSource, Stream, AirbyteLogger
+from airbyte_cdk.models import DestinationSyncMode, AirbyteStateMessage
 
 
 class MockSource(AbstractSource):
@@ -40,10 +42,27 @@ def test_raising_check():
 
 
 class MockStream(Stream):
+    def __init__(
+            self,
+            inputs_and_mocked_outputs: List[Tuple[Mapping[str, Any], Iterable[Mapping[str, Any]]]] = None,
+            name: str = None
+    ):
+        self._inputs_and_mocked_outputs = inputs_and_mocked_outputs
+        self._name = name
 
-    def read_records(self, sync_mode: SyncMode, cursor_field: List[str] = None, stream_slice: Mapping[str, Any] = None,
-                     stream_state: Mapping[str, Any] = None) -> Iterable[Mapping[str, Any]]:
-        pass
+    @property
+    def name(self):
+        return self._name
+
+    def read_records(self, **kwargs) -> Iterable[Mapping[str, Any]]:
+        # Remove None values
+        kwargs = {k: v for k, v in kwargs.items() if v is not None}
+        if self._inputs_and_mocked_outputs:
+            for _input, output in self._inputs_and_mocked_outputs:
+                if kwargs == _input:
+                    return output
+
+            raise Exception(f"No mocked output supplied for input: {kwargs}. Mocked inputs/outputs: {self._inputs_and_mocked_outputs}")
 
     @property
     def primary_key(self) -> Optional[Union[str, List[str], List[List[str]]]]:
@@ -72,25 +91,159 @@ def test_discover(mocker):
     assert expected == src.discover(logger, {})
 
 
-def test_read_nonexistent_stream_raises_exception():
-    pass
+# TODO not implemented yet
+# def test_read_nonexistent_stream_raises_exception():
+#     pass
+
+GLOBAL_EMITTED_AT = 1
 
 
-def test_valid_fullrefresh_read_no_slices():
-    pass
+def _as_record(stream: str, data: Dict[str, Any]) -> AirbyteMessage:
+    return AirbyteMessage(type=Type.RECORD, record=AirbyteRecordMessage(stream=stream, data=data, emitted_at=GLOBAL_EMITTED_AT))
 
 
-def test_valid_full_refresh_read_with_slices():
-    pass
+def _as_records(stream: str, data: List[Dict[str, Any]]) -> List[AirbyteMessage]:
+    return [_as_record(stream, datum) for datum in data]
 
 
-def test_valid_incremental_read_with_record_interval():
-    pass
+def _configured_stream(stream: Stream, sync_mode: SyncMode):
+    return ConfiguredAirbyteStream(stream=stream.as_airbyte_stream(), sync_mode=sync_mode, destination_sync_mode=DestinationSyncMode.overwrite)
 
 
-def test_valid_incremental_read_with_no_interval():
-    pass
+def _fix_emitted_at(messages: List[AirbyteMessage]) -> List[AirbyteMessage]:
+    for msg in messages:
+        if msg.type == Type.RECORD:
+            msg.record.emitted_at = GLOBAL_EMITTED_AT
+    return messages
 
 
-def test_valid_incremental_read_with_slices():
-    pass
+def test_valid_fullrefresh_read_no_slices(logger, mocker):
+    stream_output = [{'k1': 'v1'}, {'k2': 'v2'}]
+    s1 = MockStream([({'sync_mode': SyncMode.full_refresh}, stream_output)], name='s1')
+    s2 = MockStream([({'sync_mode': SyncMode.full_refresh}, stream_output)], name='s2')
+
+    mocker.patch.object(MockStream, 'get_json_schema', return_value={})
+
+    src = MockSource(streams=[s1, s2])
+    catalog = ConfiguredAirbyteCatalog(streams=[_configured_stream(s1, SyncMode.full_refresh), _configured_stream(s2, SyncMode.full_refresh)])
+
+    expected = _as_records('s1', stream_output) + _as_records('s2', stream_output)
+    messages = _fix_emitted_at(list(src.read(logger, {}, catalog)))
+
+    assert expected == messages
+
+
+def test_valid_full_refresh_read_with_slices(mocker, logger):
+    stream_output = [{'k1': 'v1'}, {'k2': 'v2'}]
+    slices = [{'1': '1'}, {'2': '2'}]
+    s1 = MockStream(
+        [({'sync_mode': SyncMode.full_refresh, 'stream_slice': s}, stream_output) for s in slices],
+        name='s1'
+    )
+    s2 = MockStream(
+        [({'sync_mode': SyncMode.full_refresh, 'stream_slice': s}, stream_output) for s in slices],
+        name='s2'
+    )
+
+    mocker.patch.object(MockStream, 'get_json_schema', return_value={})
+    mocker.patch.object(MockStream, 'stream_slices', return_value=slices)
+
+    src = MockSource(streams=[s1, s2])
+    catalog = ConfiguredAirbyteCatalog(streams=[_configured_stream(s1, SyncMode.full_refresh), _configured_stream(s2, SyncMode.full_refresh)])
+
+    expected = _as_records('s1', stream_output) * 2 + _as_records('s2', stream_output) * 2
+    messages = _fix_emitted_at(list(src.read(logger, {}, catalog)))
+
+    assert expected == messages
+
+
+def _state(state_data: Dict[str, Any]):
+    return AirbyteMessage(type=Type.STATE, state=AirbyteStateMessage(data=state_data))
+
+
+def test_valid_incremental_read_with_checkpoint_interval(mocker, logger):
+    stream_output = [{'k1': 'v1'}, {'k2': 'v2'}]
+    s1 = MockStream([({'sync_mode': SyncMode.incremental, 'stream_state': {}}, stream_output)], name='s1')
+    s2 = MockStream([({'sync_mode': SyncMode.incremental, 'stream_state': {}}, stream_output)], name='s2')
+    state = {'cursor': 'value'}
+    mocker.patch.object(MockStream, 'get_updated_state', return_value=state)
+    mocker.patch.object(MockStream, 'supports_incremental', return_value=True)
+    mocker.patch.object(MockStream, 'get_json_schema', return_value={})
+    # Tell the source to output one state message per record
+    mocker.patch.object(MockStream, 'state_checkpoint_interval', return_value=1)
+
+    src = MockSource(streams=[s1, s2])
+    catalog = ConfiguredAirbyteCatalog(streams=[_configured_stream(s1, SyncMode.incremental), _configured_stream(s2, SyncMode.incremental)])
+
+    expected = [
+        _as_record('s1', stream_output[0]),
+        _state({'s1': state}),
+        _as_record('s1', stream_output[1]),
+        _state({'s1': state}),
+        _as_record('s2', stream_output[0]),
+        _state({'s1': state, 's2': state}),
+        _as_record('s2', stream_output[1]),
+        _state({'s1': state, 's2': state})
+    ]
+    messages = _fix_emitted_at(list(src.read(logger, {}, catalog, state=defaultdict(dict))))
+
+    assert expected == messages
+
+
+def test_valid_incremental_read_with_no_interval(mocker, logger):
+    stream_output = [{'k1': 'v1'}, {'k2': 'v2'}]
+    s1 = MockStream([({'sync_mode': SyncMode.incremental, 'stream_state': {}}, stream_output)], name='s1')
+    s2 = MockStream([({'sync_mode': SyncMode.incremental, 'stream_state': {}}, stream_output)], name='s2')
+    state = {'cursor': 'value'}
+    mocker.patch.object(MockStream, 'get_updated_state', return_value=state)
+    mocker.patch.object(MockStream, 'supports_incremental', return_value=True)
+    mocker.patch.object(MockStream, 'get_json_schema', return_value={})
+
+    src = MockSource(streams=[s1, s2])
+    catalog = ConfiguredAirbyteCatalog(streams=[_configured_stream(s1, SyncMode.incremental), _configured_stream(s2, SyncMode.incremental)])
+
+    expected = [*_as_records('s1', stream_output), _state({'s1': state}), *_as_records('s2', stream_output), _state({'s1': state, 's2': state})]
+
+    messages = _fix_emitted_at(list(src.read(logger, {}, catalog, state=defaultdict(dict))))
+
+    assert expected == messages
+
+
+def test_valid_incremental_read_with_slices(mocker, logger):
+    slices = [{'1': '1'}, {'2': '2'}]
+    stream_output = [{'k1': 'v1'}, {'k2': 'v2'}, {'k3': 'v3'}]
+    s1 = MockStream(
+        [({'sync_mode': SyncMode.incremental, 'stream_slice': s, 'stream_state': mocker.ANY}, stream_output) for s in slices],
+        name='s1'
+    )
+    s2 = MockStream(
+        [({'sync_mode': SyncMode.incremental, 'stream_slice': s, 'stream_state': mocker.ANY}, stream_output) for s in slices],
+        name='s2'
+    )
+    state = {'cursor': 'value'}
+    mocker.patch.object(MockStream, 'get_updated_state', return_value=state)
+    mocker.patch.object(MockStream, 'supports_incremental', return_value=True)
+    mocker.patch.object(MockStream, 'get_json_schema', return_value={})
+    mocker.patch.object(MockStream, 'stream_slices', return_value=slices)
+
+    src = MockSource(streams=[s1, s2])
+    catalog = ConfiguredAirbyteCatalog(streams=[_configured_stream(s1, SyncMode.incremental), _configured_stream(s2, SyncMode.incremental)])
+
+    expected = [
+        # stream 1 slice 1
+        *_as_records('s1', stream_output),
+        _state({'s1': state}),
+        # stream 1 slice 2
+        *_as_records('s1', stream_output),
+        _state({'s1': state}),
+        # stream 2 slice 1
+        *_as_records('s2', stream_output),
+        _state({'s1': state, 's2': state}),
+        # stream 2 slice 2
+        *_as_records('s2', stream_output),
+        _state({'s1': state, 's2': state})
+    ]
+
+    messages = _fix_emitted_at(list(src.read(logger, {}, catalog, state=defaultdict(dict))))
+
+    assert expected == messages
