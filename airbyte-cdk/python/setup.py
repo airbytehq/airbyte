9f1e0baeea2372026ef1d9532556d125d388214a
Sherif Nada <snadalive@gmail.com>
2021-05-09 14:00:58 -0700

    rc4

--- airbyte-cdk/python/setup.py
+++ airbyte-cdk/python/setup.py
@@ -33,7 +33,7 @@ README = (HERE / "README.md").read_text()
 
 setup(
     name="airbyte-cdk",
-    version="0.1.0-rc3",
+    version="0.1.0rc4",
     description="A framework for writing Airbyte Connectors.",
     long_description=README,
     long_description_content_type="text/markdown",
