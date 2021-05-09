a427811c19c643876888f67dc52f03f17f4fa9f5
Sherif Nada <snadalive@gmail.com>
2021-05-09 14:00:14 -0700

    fix circular import issue

--- airbyte-cdk/python/type_check_and_test.sh
+++ airbyte-cdk/python/type_check_and_test.sh
@@ -4,7 +4,7 @@
 # TODO(davin): This should not assume the user has already set up the venv folder.
 
 # Static Type Checking
-echo "Running MyPy to static check base_python directory and test files."
+echo "Running MyPy to static check and test files."
 # Exclude Singer for the time being.
 mypy --exclude airbyte_cdk/sources/singer airbyte_cdk/ unit_tests/
 
