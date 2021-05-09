19777f95261a28a489c5c11e6b7cd764f285c7ec
Sherif Nada <snadalive@gmail.com>
2021-05-09 14:09:55 -0700

    add singer unit test stubs

---
+++ airbyte-cdk/python/unit_tests/singer/test_singer_helpers.py
@@ -0,0 +1,6 @@
+from airbyte_cdk.sources.singer import SingerHelper
+
+
+def test_singer_helper():
+    # TODO write tests. for now this just verifies the file imports correctly.
+    assert SingerHelper is not None
---
+++ airbyte-cdk/python/unit_tests/singer/test_singer_source.py
@@ -0,0 +1,6 @@
+from airbyte_cdk.sources.singer import SingerSource
+
+
+def test_singer_source_loads():
+    # TODO write tests. for now this just verifies the file imports correctly.
+    source = SingerSource()
