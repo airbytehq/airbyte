7e3b0669d3f50dc7e161861dcd5a3c34dbd3c593
Davin Chia <davinchia@gmail.com>
2021-05-10 09:22:44 +0800

    Better MyPy command to not type check singer. Export UserDefinedException as that is used in tests.

--- airbyte-cdk/python/airbyte_cdk/sources/streams/http/__init__.py
+++ airbyte-cdk/python/airbyte_cdk/sources/streams/http/__init__.py
@@ -1,4 +1,5 @@
 # Initialize Streams Package
 from .http import HttpStream
+from .exceptions import UserDefinedBackoffException
 
-__all__ = ["HttpStream"]
+__all__ = ["HttpStream", "UserDefinedBackoffException"]
