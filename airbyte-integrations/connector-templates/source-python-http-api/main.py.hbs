04ce8dcdf70fb8c8ff31891a79b9b7902c8a4152
Davin Chia <davinchia@gmail.com>
2021-05-10 19:58:42 +0800

    Fix formatting errors.

--- airbyte-integrations/connector-templates/source-python-http-api/main.py.hbs
+++ airbyte-integrations/connector-templates/source-python-http-api/main.py.hbs
@@ -20,6 +20,7 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
+
 import sys
 
 from airbyte_cdk.entrypoint import launch
--- airbyte-integrations/connectors/source-scaffold-source-http/unit_tests/unit_test.py
+++ airbyte-integrations/connectors/source-scaffold-source-http/unit_tests/unit_test.py
@@ -20,8 +20,6 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-# format anchor
-
 
 def test_example_method():
     assert True
