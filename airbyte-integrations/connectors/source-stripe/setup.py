6972aaac5dac92606c31a418731e7d254a377e68
Sherif A. Nada <snadalive@gmail.com>
2021-05-11 15:27:44 -0700

    Bump CDK consumers (#3368)

--- airbyte-integrations/connectors/source-stripe/setup.py
+++ airbyte-integrations/connectors/source-stripe/setup.py
@@ -30,5 +30,5 @@ setup(
     author_email="contact@airbyte.io",
     packages=find_packages(),
     package_data={"": ["*.json", "schemas/*.json", "schemas/shared/*.json"]},
-    install_requires=["airbyte-cdk==0.1.1", "stripe"],
+    install_requires=["airbyte-cdk==0.1.2", "stripe"],
 )
