80cb61ca1642fed0db7e56073b128274150046ad
cgardens <giardina.charles@gmail.com>
2021-05-10 20:13:11 -0700

    add test that migration output schema same as source schema

---
+++ airbyte-migration/src/test/java/io/airbyte/migrate/MigrationCurrentSchemaTest.java
@@ -0,0 +1,72 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2020 Airbyte
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package io.airbyte.migrate;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import io.airbyte.commons.enums.Enums;
+import io.airbyte.migrate.migrations.MigrationV0_14_0.ConfigKeys;
+import java.nio.file.Path;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.stream.Collectors;
+import org.junit.jupiter.api.Test;
+
+public class MigrationCurrentSchemaTest {
+
+  // get all of the "current" configs (in other words the one airbyte-config). get all of the configs
+  // from the output schema of the last migration. make sure they match.
+  @Test
+  void testConfigsOfLastMigrationMatchSource() {
+    final Migration lastMigration = Migrations.MIGRATIONS.get(Migrations.MIGRATIONS.size() - 1);
+    System.out.println("lastMigration.getVersion() = " + lastMigration.getVersion());
+    final Map<ResourceId, JsonNode> lastMigrationOutputSchema = lastMigration.getOutputSchema();
+    final Map<ResourceId, JsonNode> currentSchema = MigrationUtils.getNameToSchemasFromResourcePath(
+        Path.of("types"),
+        ResourceType.CONFIG,
+        Enums.valuesAsStrings(ConfigKeys.class));
+
+    final Map<ResourceId, JsonNode> lastMigrationOutputSchemaCleaned = lastMigrationOutputSchema.entrySet()
+        .stream()
+        .filter(entry -> entry.getKey().getType() == ResourceType.CONFIG)
+        .collect(Collectors.toMap(Entry::getKey, Entry::getValue));
+
+    assertEquals(currentSchema.size(), lastMigrationOutputSchemaCleaned.size());
+
+    final List<Entry<ResourceId, JsonNode>> lastMigrationOutputSchemaCleanedSorted = lastMigrationOutputSchemaCleaned.entrySet()
+        .stream()
+        .sorted(Comparator.comparing((v) -> v.getKey().getType()))
+        .collect(Collectors.toList());
+
+    // break out element-wise assertion so it is easier to read any failed tests.
+    for (Map.Entry<ResourceId, JsonNode> lastMigrationEntry : lastMigrationOutputSchemaCleanedSorted) {
+      assertEquals(lastMigrationEntry.getValue(), currentSchema.get(lastMigrationEntry.getKey()));
+    }
+  }
+
+}
