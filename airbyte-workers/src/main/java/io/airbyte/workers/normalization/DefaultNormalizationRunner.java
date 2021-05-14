586150a83f296ad06fce8d06593e09818bdcd2e5
Christophe Duong <christophe.duong@gmail.com>
2021-05-14 18:20:46 +0200

    Bumpversion normalization image

--- airbyte-workers/src/main/java/io/airbyte/workers/normalization/DefaultNormalizationRunner.java
+++ airbyte-workers/src/main/java/io/airbyte/workers/normalization/DefaultNormalizationRunner.java
@@ -44,9 +44,7 @@ public class DefaultNormalizationRunner implements NormalizationRunner {
 
   private static final Logger LOGGER = LoggerFactory.getLogger(DefaultNormalizationRunner.class);
 
-  // TODO chris: swap before merging
-  // public static final String NORMALIZATION_IMAGE_NAME = "airbyte/normalization:0.1.29";
-  public static final String NORMALIZATION_IMAGE_NAME = "airbyte/normalization:dev";
+  public static final String NORMALIZATION_IMAGE_NAME = "airbyte/normalization:0.1.29";
 
   private final DestinationType destinationType;
   private final ProcessBuilderFactory pbf;
