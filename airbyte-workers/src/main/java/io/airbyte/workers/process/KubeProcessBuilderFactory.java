bd3f17fb966e81fa6615972877f1f90030152dce
Davin Chia <davinchia@gmail.com>
2021-05-19 21:41:18 +0800

    Checkpoint: Sync worker pod is able to send output to the destination pod.

--- airbyte-workers/src/main/java/io/airbyte/workers/process/KubeProcessBuilderFactory.java
+++ airbyte-workers/src/main/java/io/airbyte/workers/process/KubeProcessBuilderFactory.java
@@ -32,6 +32,10 @@ import com.google.common.collect.Lists;
 import io.airbyte.commons.json.Jsons;
 import io.airbyte.commons.resources.MoreResources;
 import io.airbyte.workers.WorkerException;
+import io.fabric8.kubernetes.api.model.Pod;
+import io.fabric8.kubernetes.api.model.PodList;
+import io.fabric8.kubernetes.client.DefaultKubernetesClient;
+import io.fabric8.kubernetes.client.KubernetesClient;
 import io.kubernetes.client.openapi.ApiClient;
 import io.kubernetes.client.openapi.ApiException;
 import io.kubernetes.client.openapi.Configuration;
@@ -39,7 +43,11 @@ import io.kubernetes.client.openapi.apis.CoreV1Api;
 import io.kubernetes.client.openapi.models.V1Pod;
 import io.kubernetes.client.openapi.models.V1PodList;
 import io.kubernetes.client.util.Config;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
 import java.nio.file.Path;
 import java.util.Arrays;
 import java.util.List;
@@ -105,14 +113,32 @@ public class KubeProcessBuilderFactory implements ProcessBuilderFactory {
   }
 
   public static void main(String[] args) throws IOException, ApiException {
-    ApiClient client = Config.defaultClient();
-    Configuration.setDefaultApiClient(client);
-
-    CoreV1Api api = new CoreV1Api();
-    V1PodList list = api.listPodForAllNamespaces(null, null, null, null, null, null, null, null, null);
-    for (V1Pod item : list.getItems()) {
-      LOGGER.info(item.getMetadata().getName());
+    var PORT = 9000;
+    String IP = null;
+    KubernetesClient client = new DefaultKubernetesClient();
+    // TODO: Assign labels to pods to narrow the search.
+    PodList pods = client.pods().inNamespace("default").list();
+    for (Pod p : pods.getItems()) {
+      // TODO: filter for the actual pod.
+      // retrieve the ip
+      LOGGER.info(p.getMetadata().getName());
+      LOGGER.info(p.getStatus().getPodIP());
+      if (p.getMetadata().getName().equals("destination-listen-and-echo")) {
+        LOGGER.info("Found IP!");
+        IP = p.getStatus().getPodIP();
+        break;
+      }
     }
+
+    client.close();
+
+    // TODO: create the pod
+    var clientSocket = new Socket(IP, PORT);
+    var out = new PrintWriter(clientSocket.getOutputStream(), true);
+    out.print("Hello!");
+    out.close();
+    // try and connect to this ip and send something
+
   }
 
 }
