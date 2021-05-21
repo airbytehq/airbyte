fc19affce1d93b897438d8a2dfedfe0f72996662
Davin Chia <davinchia@gmail.com>
2021-05-21 23:24:07 +0800

    Dest pod loops back into worker pod. However, the right messages do not seem to be passing in.

--- airbyte-workers/src/main/java/io/airbyte/workers/process/KubeProcessBuilderFactoryPOC.java
+++ airbyte-workers/src/main/java/io/airbyte/workers/process/KubeProcessBuilderFactoryPOC.java
@@ -24,19 +24,28 @@
 
 package io.airbyte.workers.process;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
 import com.google.common.collect.Lists;
 import io.airbyte.commons.io.IOs;
+import io.airbyte.commons.json.Jsons;
+import io.airbyte.commons.resources.MoreResources;
 import io.fabric8.kubernetes.api.model.Pod;
 import io.fabric8.kubernetes.api.model.PodList;
 import io.fabric8.kubernetes.client.DefaultKubernetesClient;
 import io.fabric8.kubernetes.client.KubernetesClient;
 import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.InetAddress;
+import java.net.ServerSocket;
 import java.net.Socket;
 import java.nio.file.Path;
 import java.util.List;
+import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 import org.apache.commons.lang3.RandomStringUtils;
@@ -48,7 +57,7 @@ public class KubeProcessBuilderFactoryPOC {
   private static final Logger LOGGER = LoggerFactory.getLogger(KubeProcessBuilderFactoryPOC.class);
 
   private static final KubernetesClient KUBE_CLIENT = new DefaultKubernetesClient();
-  private static final int PORT = 9000;
+  private static final int PORT = 9001;
 
   // todo: this should really be cached
   private static String getCommandFromImage(String imageName) throws IOException {
@@ -119,18 +128,16 @@ public class KubeProcessBuilderFactoryPOC {
   }
 
   private static String getPodIP(String podName) {
-    // TODO: Why does this not work?
+    // TODO: Why does directly searching for the pod not work?
     // LOGGER.info(destPod.getStatus().getPodIP());
     // destPod = client.resource(destPod).get();
     // LOGGER.info("Status: {}", destPod.getStatus());
     // LOGGER.info("IP: {}", destPod.getStatus().getPodIP());
     // IP = destPod.getStatus().getPodIP();
 
-    // TODO: Assign labels to pods to narrow the search.
+    // TODO: We could assign labels to pods to narrow the search.
     PodList pods = KUBE_CLIENT.pods().inNamespace("default").list();
     for (Pod p : pods.getItems()) {
-      LOGGER.info(p.getMetadata().getName());
-      LOGGER.info(p.getStatus().getPodIP());
       // Filter by pod and retrieve IP.
       if (p.getMetadata().getName().equals(podName)) {
         LOGGER.info("Found IP!");
@@ -141,6 +148,29 @@ public class KubeProcessBuilderFactoryPOC {
     return null;
   }
 
+  // TODO: It might be easier to do this using the same Socat pattern we use in the
+  //  Dockerfile and be reading from a file.
+  private static void startListeningOnPort(int port) {
+    Executors.newSingleThreadExecutor().submit(() -> {
+      try (var serverSocket = new ServerSocket(port)) {
+        LOGGER.info("Created server and waiting for connection..");
+        var socket = serverSocket.accept();
+        LOGGER.info("Accepted connection!");
+        var input = socket.getInputStream();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+        while(!reader.ready())
+        while (true) {
+          final var line = reader.readLine();
+          if (line == null) break;
+
+          LOGGER.info("Destination sent: {}", line);
+        }
+      } catch (IOException e) {
+        LOGGER.error("Error starting socket reader: ",e);
+      }
+    });
+  }
+
   private static void createIfNotExisting(String podName, Pod def) throws InterruptedException {
     LOGGER.info("Checking pod: {}", podName);
     var podSet = KUBE_CLIENT.pods().inNamespace("default").list().getItems().stream()
@@ -148,37 +178,50 @@ public class KubeProcessBuilderFactoryPOC {
     if (podSet.size() == 0) {
       LOGGER.info("Pod {} does not exist", podName);
       Pod destPod = KUBE_CLIENT.pods().create(def);
-      LOGGER.info("Created pod: {}, waiting for it to be ready", destPod);
+      LOGGER.info("Created pod: {}, waiting for it to be ready", destPod.getMetadata().getName());
       KUBE_CLIENT.resource(destPod).waitUntilReady(1, TimeUnit.MINUTES);
       LOGGER.info("Pod {} ready", podName);
     }
   }
 
-  public static void main(String[] args) throws InterruptedException, IOException {
+  private static void runSampleKubeWorker() throws InterruptedException, IOException {
     String myIp = InetAddress.getLocalHost().getHostAddress();
     LOGGER.info("Kube sync worker ip: {}", myIp);
 
-    var destPodName = "destination-listen-and-echo";
+    var destPodName = "kube-destination-sample";
 
-    // Load spec and create the pod.
-    var stream = KubeProcessBuilderFactoryPOC.class.getClassLoader().getResourceAsStream("kube_queue_poc/destination-listen-and-echo.yaml");
-    var destPodDef = KUBE_CLIENT.pods().load(stream).get();
-    LOGGER.info("Loaded spec: {}", destPodDef);
+    // Load spec and swap in worker ip.
+    var template = MoreResources.readResource("kube_queue_poc/kube-destination-sample-pod.yaml");
+    var rendered = template.replaceAll("WORKER_IP", myIp);
+    var renderedStream = new ByteArrayInputStream(rendered.getBytes());
+    var destPodDef = KUBE_CLIENT.pods().load(renderedStream).get();
+    LOGGER.info("Loaded spec");
+
+    // TODO: 1) The container image needs to line up with the actual spec. 2) Why isn't this working?
+    //var containers = destPodDef.getSpec().getContainers();
+    //getCommandFromImage(containers.get(0).getImage());
+
+    // Start a listening server for the Destination to connect to.
+    startListeningOnPort(9001);
 
     createIfNotExisting(destPodName, destPodDef);
     String destPodIp = getPodIP(destPodName);
     LOGGER.info("Dest pod ip: {}", destPodIp);
 
     // Send something!
-    var clientSocket = new Socket(destPodIp, PORT);
-    var out = new PrintWriter(clientSocket.getOutputStream(), true);
-    out.print("Hello!");
-    out.close();
+    var socketToDestStdIo = new Socket(destPodIp, PORT);
+    var toDest = new PrintWriter(socketToDestStdIo.getOutputStream(), true);
+    toDest.print("Hello!");
+    toDest.close();
 
     KUBE_CLIENT.pods().delete(destPodDef);
     // TODO: Why does this wait not work?
     KUBE_CLIENT.resource(destPodDef).waitUntilCondition(pod -> !pod.getStatus().getPhase().equals("Terminating"), 1, TimeUnit.MINUTES);
     KUBE_CLIENT.close();
+  }
+
+  public static void main(String[] args) throws InterruptedException, IOException {
+    runSampleKubeWorker();
   }
 
 }
