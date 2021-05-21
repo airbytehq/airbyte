12f21558d56c9397482bf9172feabd59a112e6c7
vitaliizazmic <75620293+vitaliizazmic@users.noreply.github.com>
2021-05-21 15:07:21 +0300

    Jira Source HTTP CDK
    * Source Jira #3453 - cleaning branch
    
    * Source Jira #3453 - cleaning branch (fix)
    
    * Source Jira #3453 - abstractmethod get_updated_state

--- airbyte-integrations/bases/standard-source-test/src/main/java/io/airbyte/integrations/standardtest/source/StandardSourceTest.java
+++ airbyte-integrations/bases/standard-source-test/src/main/java/io/airbyte/integrations/standardtest/source/StandardSourceTest.java
@@ -118,7 +118,8 @@ public abstract class StandardSourceTest {
       "airbyte/source-gitlab-singer",
       "airbyte/source-google-workspace-admin-reports",
       "airbyte/source-zendesk-talk",
-      "airbyte/source-quickbooks-singer");
+      "airbyte/source-quickbooks-singer",
+      "airbyte/source-jira");
 
   /**
    * FIXME: Some sources can't guarantee that there will be no events between two sequential sync
