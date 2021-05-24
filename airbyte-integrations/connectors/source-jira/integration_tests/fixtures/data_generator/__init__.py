631408f874d10cc51cc99c695729555c8b103c27
Vitalii Vdovenko <vitalii.vdovenko@zazmic.com>
2021-05-24 18:07:30 +0300

    Source Jira #2100 - improving according to comments

--- airbyte-integrations/connectors/source-jira/integration_tests/fixtures/data_generator/__init__.py
+++ airbyte-integrations/connectors/source-jira/integration_tests/fixtures/data_generator/__init__.py
@@ -21,8 +21,3 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 """
-
-from auth import AuthClient
-from base import BaseStream
-
-__all__ = ["AuthClient", "BaseStream"]
