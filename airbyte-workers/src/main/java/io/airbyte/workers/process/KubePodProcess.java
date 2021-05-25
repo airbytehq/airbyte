b447fe8085d5a64e812ff033544addd10a0f5039
Jared Rhizor <jared@dataline.io>
2021-05-24 20:27:19 -0700

    wrap kube pod in process (#3540)
    also clean up kubernetes deploys.

---
+++ airbyte-workers/src/main/java/io/airbyte/workers/process/KubePodProcess.java
@@ -0,0 +1,230 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2020 Airbyte
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package io.airbyte.workers.process;
+
+import com.google.common.base.Preconditions;
+import io.fabric8.kubernetes.api.model.Container;
+import io.fabric8.kubernetes.api.model.ContainerBuilder;
+import io.fabric8.kubernetes.api.model.DeletionPropagation;
+import io.fabric8.kubernetes.api.model.Pod;
+import io.fabric8.kubernetes.api.model.PodBuilder;
+import io.fabric8.kubernetes.api.model.Volume;
+import io.fabric8.kubernetes.api.model.VolumeBuilder;
+import io.fabric8.kubernetes.api.model.VolumeMount;
+import io.fabric8.kubernetes.api.model.VolumeMountBuilder;
+import io.fabric8.kubernetes.client.KubernetesClient;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import org.apache.commons.io.output.NullOutputStream;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class KubePodProcess extends Process {
+
+  private static final Logger LOGGER = LoggerFactory.getLogger(KubePodProcess.class);
+
+  private static final int STDIN_REMOTE_PORT = 9001;
+
+  private final KubernetesClient client;
+  private final Pod podDefinition;
+
+  private final OutputStream stdin;
+  private InputStream stdout;
+  private final ServerSocket stdoutServerSocket;
+  private final ExecutorService executorService;
+
+  public KubePodProcess(KubernetesClient client, String podName, String image, int stdoutLocalPort, boolean usesStdin)
+      throws IOException, InterruptedException {
+    this.client = client;
+
+    // allow reading stdout from pod
+    LOGGER.info("Creating socket server...");
+    this.stdoutServerSocket = new ServerSocket(stdoutLocalPort);
+
+    executorService = Executors.newSingleThreadExecutor();
+    executorService.submit(() -> {
+      try {
+        LOGGER.info("Creating socket from server...");
+        var socket = stdoutServerSocket.accept(); // blocks until connected
+        LOGGER.info("Setting stdout...");
+        this.stdout = socket.getInputStream();
+      } catch (IOException e) {
+        e.printStackTrace(); // todo: propagate exception / join at the end of constructor
+      }
+    });
+
+    // create pod
+    String entrypoint = KubeProcessBuilderFactoryPOC.getCommandFromImage(image);
+
+    Volume volume = new VolumeBuilder()
+        .withName("airbyte-pipes")
+        .withNewEmptyDir()
+        .endEmptyDir()
+        .build();
+
+    VolumeMount volumeMount = new VolumeMountBuilder()
+        .withName("airbyte-pipes")
+        .withMountPath("/pipes")
+        .build();
+
+    Container initContainer = new ContainerBuilder()
+        .withName("init")
+        .withImage("busybox:1.28")
+        .withCommand("sh", "-c", usesStdin ? "mkfifo /pipes/stdin && mkfifo /pipes/stdout" : "mkfifo /pipes/stdout")
+        .withVolumeMounts(volumeMount)
+        .build();
+
+    Container main = new ContainerBuilder()
+        .withName("main")
+        .withImage(image)
+        .withCommand("sh", "-c", usesStdin ? "cat /pipes/stdin | " + entrypoint + " > /pipes/stdout" : entrypoint + " > /pipes/stdout")
+        .withVolumeMounts(volumeMount)
+        .build();
+
+    Container remoteStdin = new ContainerBuilder()
+        .withName("remote-stdin")
+        .withImage("alpine/socat:1.7.4.1-r1")
+        .withCommand("sh", "-c", "socat -d -d -d TCP-L:9001 STDOUT > /pipes/stdin")
+        .withVolumeMounts(volumeMount)
+        .build();
+
+    Container relayStdout = new ContainerBuilder()
+        .withName("relay-stdout")
+        .withImage("alpine/socat:1.7.4.1-r1")
+        .withCommand("sh", "-c", "cat /pipes/stdout | socat -d -d -d - TCP:" + InetAddress.getLocalHost().getHostAddress() + ":" + stdoutLocalPort)
+        .withVolumeMounts(volumeMount)
+        .build();
+
+    List<Container> containers = usesStdin ? List.of(main, remoteStdin, relayStdout) : List.of(main, relayStdout);
+
+    Pod pod = new PodBuilder()
+        .withApiVersion("v1")
+        .withNewMetadata()
+        .withName(podName)
+        .endMetadata()
+        .withNewSpec()
+        .withRestartPolicy("Never")
+        .withInitContainers(initContainer)
+        .withContainers(containers)
+        .withVolumes(volume)
+        .endSpec()
+        .build();
+
+    LOGGER.info("Creating pod...");
+    this.podDefinition = client.pods().inNamespace("default").createOrReplace(pod);
+
+    LOGGER.info("Waiting until pod is ready...");
+    client.resource(podDefinition).waitUntilReady(5, TimeUnit.MINUTES);
+
+    // allow writing stdin to pod
+    LOGGER.info("Reading pod IP...");
+    var podIp = KubeProcessBuilderFactoryPOC.getPodIP(podName);
+    LOGGER.info("Pod IP: {}", podIp);
+
+    if (usesStdin) {
+      LOGGER.info("Creating stdin socket...");
+      var socketToDestStdIo = new Socket(podIp, STDIN_REMOTE_PORT);
+      this.stdin = socketToDestStdIo.getOutputStream();
+    } else {
+      LOGGER.info("Using null stdin output stream...");
+      this.stdin = NullOutputStream.NULL_OUTPUT_STREAM;
+    }
+  }
+
+  @Override
+  public OutputStream getOutputStream() {
+    return this.stdin;
+  }
+
+  @Override
+  public InputStream getInputStream() {
+    return this.stdout;
+  }
+
+  @Override
+  public InputStream getErrorStream() {
+    // there is no error stream equivalent for Kube-based processes so we use a null stream here
+    return InputStream.nullInputStream();
+  }
+
+  @Override
+  public int waitFor() throws InterruptedException {
+    client.resource(podDefinition).waitUntilCondition(this::isTerminal, 10, TimeUnit.DAYS);
+    return exitValue();
+  }
+
+  private boolean isTerminal(Pod pod) {
+    if (pod.getStatus() != null) {
+      return pod.getStatus()
+          .getContainerStatuses()
+          .stream()
+          .anyMatch(e -> e.getState() != null && e.getState().getTerminated() != null);
+    } else {
+      return false;
+    }
+  }
+
+  private int getReturnCode(Pod pod) {
+    Pod refreshedPod = client.pods().withName(pod.getMetadata().getName()).get(); // todo: use more robust version here
+    Preconditions.checkArgument(isTerminal(refreshedPod));
+
+    return refreshedPod.getStatus().getContainerStatuses()
+        .stream()
+        .filter(containerStatus -> containerStatus.getState() != null && containerStatus.getState().getTerminated() != null)
+        .map(containerStatus -> {
+          int statusCode = containerStatus.getState().getTerminated().getExitCode();
+          LOGGER.info("Termination status for container " + containerStatus.getName() + " is " + statusCode);
+          return statusCode;
+        })
+        .reduce(Integer::sum)
+        .orElseThrow();
+  }
+
+  @Override
+  public int exitValue() {
+    return getReturnCode(podDefinition);
+  }
+
+  @Override
+  public void destroy() {
+    try {
+      stdoutServerSocket.close();
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    } finally {
+      executorService.shutdown();
+      client.resource(podDefinition).withPropagationPolicy(DeletionPropagation.FOREGROUND).delete();
+    }
+  }
+
+}
--- airbyte-workers/src/main/java/io/airbyte/workers/process/KubeProcessBuilderFactoryPOC.java
+++ airbyte-workers/src/main/java/io/airbyte/workers/process/KubeProcessBuilderFactoryPOC.java
@@ -24,30 +24,20 @@
 
 package io.airbyte.workers.process;
 
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
 import com.google.common.collect.Lists;
 import io.airbyte.commons.io.IOs;
-import io.airbyte.commons.json.Jsons;
-import io.airbyte.commons.resources.MoreResources;
 import io.fabric8.kubernetes.api.model.Pod;
 import io.fabric8.kubernetes.api.model.PodList;
 import io.fabric8.kubernetes.client.DefaultKubernetesClient;
 import io.fabric8.kubernetes.client.KubernetesClient;
 import java.io.BufferedReader;
-import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
-import java.net.InetAddress;
-import java.net.ServerSocket;
-import java.net.Socket;
 import java.nio.file.Path;
 import java.util.List;
+import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
-import java.util.stream.Collectors;
 import org.apache.commons.lang3.RandomStringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -57,10 +47,9 @@ public class KubeProcessBuilderFactoryPOC {
   private static final Logger LOGGER = LoggerFactory.getLogger(KubeProcessBuilderFactoryPOC.class);
 
   private static final KubernetesClient KUBE_CLIENT = new DefaultKubernetesClient();
-  private static final int PORT = 9001;
 
   // todo: this should really be cached
-  private static String getCommandFromImage(String imageName) throws IOException {
+  public static String getCommandFromImage(String imageName) throws IOException {
     final String suffix = RandomStringUtils.randomAlphabetic(5).toLowerCase();
 
     final String podName = "airbyte-command-fetcher-" + suffix;
@@ -70,7 +59,7 @@ public class KubeProcessBuilderFactoryPOC {
             "kubectl",
             "run",
             "--generator=run-pod/v1",
-            "--rm",
+            // "--rm",
             "-i",
             "--pod-running-timeout=24h",
             "--image=" + imageName,
@@ -102,8 +91,6 @@ public class KubeProcessBuilderFactoryPOC {
     }
   }
 
-  private static void createPodAndWaitTillReady(String imageId) {}
-
   private static void saveJaredWork() {
     try {
       // todo: test this with args that are used by the process
@@ -127,7 +114,7 @@ public class KubeProcessBuilderFactoryPOC {
     }
   }
 
-  private static String getPodIP(String podName) {
+  public static String getPodIP(String podName) {
     // TODO: Why does directly searching for the pod not work?
     // LOGGER.info(destPod.getStatus().getPodIP());
     // destPod = client.resource(destPod).get();
@@ -148,85 +135,48 @@ public class KubeProcessBuilderFactoryPOC {
     return null;
   }
 
-  // TODO: It might be easier to do this using the same Socat pattern we use in the
-  //  Dockerfile and be reading from a file.
-  private static void startListeningOnPort(int port) {
-    Executors.newSingleThreadExecutor().submit(() -> {
-      try (var serverSocket = new ServerSocket(port)) {
-        LOGGER.info("Created server and waiting for connection..");
-        var socket = serverSocket.accept();
-        LOGGER.info("Accepted connection!");
-        var input = socket.getInputStream();
-        BufferedReader reader = new BufferedReader(new InputStreamReader(input));
-//        while(!reader.ready())
-        while (true) {
-          final var line = reader.readLine();
-          if (line == null) break;
+  public static void main(String[] args) throws InterruptedException, IOException {
+    LOGGER.info("Launching source process...");
+    Process src = new KubePodProcess(KUBE_CLIENT, "src", "np_source:dev", 9002, false);
 
-          LOGGER.info("Destination sent: {}", line);
+    LOGGER.info("Launching destination process...");
+    Process dest = new KubePodProcess(KUBE_CLIENT, "dest", "np_dest:dev", 9003, true);
+
+    LOGGER.info("Launching background thread to read destination lines...");
+    ExecutorService executor = Executors.newSingleThreadExecutor();
+    executor.submit(() -> {
+      BufferedReader reader = new BufferedReader(new InputStreamReader(dest.getInputStream()));
+
+      while (true) {
+        try {
+          String line;
+          if ((line = reader.readLine()) != null) {
+            LOGGER.info("Destination sent: {}", line);
+          }
+        } catch (IOException e) {
+          e.printStackTrace();
         }
-      } catch (IOException e) {
-        LOGGER.error("Error starting socket reader: ",e);
       }
     });
-  }
 
-  private static void createIfNotExisting(String podName, Pod def) throws InterruptedException {
-    LOGGER.info("Checking pod: {}", podName);
-    var podSet = KUBE_CLIENT.pods().inNamespace("default").list().getItems().stream()
-        .filter(pod -> pod.getMetadata().getName().equals(podName)).collect(Collectors.toSet());
-    if (podSet.size() == 0) {
-      LOGGER.info("Pod {} does not exist", podName);
-      Pod destPod = KUBE_CLIENT.pods().create(def);
-      LOGGER.info("Created pod: {}, waiting for it to be ready", destPod.getMetadata().getName());
-      KUBE_CLIENT.resource(destPod).waitUntilReady(1, TimeUnit.MINUTES);
-      LOGGER.info("Pod {} ready", podName);
+    LOGGER.info("Copying source stdout to destination stdin...");
+
+    BufferedReader reader = IOs.newBufferedReader(src.getInputStream());
+    PrintWriter writer = new PrintWriter(dest.getOutputStream(), true);
+
+    String line;
+    while ((line = reader.readLine()) != null) {
+      writer.println(line);
     }
-  }
+    writer.close();
 
-  private static void runSampleKubeWorker() throws InterruptedException, IOException {
-    String myIp = InetAddress.getLocalHost().getHostAddress();
-    LOGGER.info("Kube sync worker ip: {}", myIp);
+    LOGGER.info("Waiting for source...");
+    src.waitFor();
+    LOGGER.info("Waiting for destination...");
+    dest.waitFor();
+    LOGGER.info("Done!");
 
-    var destPodName = "kube-destination-sample";
-
-    // Load spec and swap in worker ip.
-    var template = MoreResources.readResource("kube_queue_poc/kube-destination-sample-pod.yaml");
-    var rendered = template.replaceAll("WORKER_IP", myIp);
-    var renderedStream = new ByteArrayInputStream(rendered.getBytes());
-    var destPodDef = KUBE_CLIENT.pods().load(renderedStream).get();
-    LOGGER.info("Loaded spec");
-
-    // TODO: 1) The container image needs to line up with the actual spec. 2) Why isn't this working?
-//    var containers = destPodDef.getSpec().getContainers();
-//    System.out.println(containers.get(0));
-//    getCommandFromImage(containers.get(0).getImage());
-
-    // Start a listening server for the Destination to connect to.
-    startListeningOnPort(9001);
-
-    createIfNotExisting(destPodName, destPodDef);
-    String destPodIp = getPodIP(destPodName);
-    LOGGER.info("Dest pod ip: {}", destPodIp);
-
-    // Send something!
-    var socketToDestStdIo = new Socket(destPodIp, PORT);
-    var toDest = new PrintWriter(socketToDestStdIo.getOutputStream(), true);
-    toDest.println("Hello!");
-    toDest.println("a!");
-    toDest.println("b!");
-    toDest.println("c!");
-    toDest.println("d!");
-    toDest.close();
-
-    KUBE_CLIENT.pods().delete(destPodDef);
-    // TODO: Why does this wait not work?
-    KUBE_CLIENT.resource(destPodDef).waitUntilCondition(pod -> !pod.getStatus().getPhase().equals("Terminating"), 1, TimeUnit.MINUTES);
-    KUBE_CLIENT.close();
-  }
-
-  public static void main(String[] args) throws InterruptedException, IOException {
-    runSampleKubeWorker();
+    System.exit(0); // todo: handle executors so we don't need to kill the JVM
   }
 
 }
