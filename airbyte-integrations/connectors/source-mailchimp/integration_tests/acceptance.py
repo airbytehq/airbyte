52623c057bc122d52d37927ec3e1ca8a9abdc54f
vovavovavovavova <39351371+vovavovavovavova@users.noreply.github.com>
2021-05-27 13:37:16 +0300

    Source Mailchimp: Get Member activities (#3415)
    * add logic for new stream: email activities (campaign dependent)
    
    * de-duplicate pagination logic
    
    * MailChimp: CDK version (before acceptance test)
    
    * add test files
    
    * applying requested changes + incremental for email reports (requested)
    
    * no-formatted test modification
    
    * save changes for test
    
    * upd tests + build
    
    * update accordinately to requested changes + readme
    
    * restore original READMe & update mailchimps
    
    * upd requested changes
    
    * rm metadata _links from schemas
    
    * rm links from configured_catalog
    
    * fixup unittest
    
    * requested changes from Sherif
    
    * upd: describe why acceptance tests are commented for incremental
    
    * bump version

--- airbyte-integrations/connectors/source-mailchimp/integration_tests/acceptance.py
+++ airbyte-integrations/connectors/source-mailchimp/integration_tests/acceptance.py
@@ -23,34 +23,14 @@
 #
 
 
-import json
-
 import pytest
-from source_mailchimp.client import Client
+
+pytest_plugins = ("source_acceptance_test.plugin",)
 
 
-class TestSourceMailchimp:
-    mailchimp_config: str = "../secrets/config.json"
-
-    @pytest.fixture()
-    def get_client(self):
-        with open(self.mailchimp_config) as json_file:
-            config = json.load(json_file)
-        client = Client(username=config["username"], apikey=config["apikey"])
-        return client
-
-    def test_client_right_credentials(self, get_client):
-        status, error = get_client.health_check()
-        assert status
-
-    def test_client_getting_streams(self, get_client):
-        streams = get_client.get_streams()
-        assert streams
-
-    def test_client_read_lists(self, get_client):
-        lists = list(get_client.lists())
-        assert isinstance(lists, list)
-
-    def test_client_read_campaigns(self, get_client):
-        campaigns = list(get_client.campaigns())
-        assert isinstance(campaigns, list)
+@pytest.fixture(scope="session", autouse=True)
+def connector_setup():
+    """This fixture is a placeholder for external resources that acceptance test might require."""
+    # TODO: setup test dependencies
+    yield
+    # TODO: clean up test dependencies
