aec3b9d79a258e158311ccb1e32501cb20c184d5
Vadym Ratniuk <midavadim@yahoo.com>
2021-06-01 00:51:37 +0300

    Initialization

--- airbyte-integrations/connectors/source-paypal-transaction/source_paypal_transaction/source.py
+++ airbyte-integrations/connectors/source-paypal-transaction/source_paypal_transaction/source.py
@@ -22,18 +22,16 @@
 # SOFTWARE.
 #
 
-import logging
+
 from abc import ABC
-from datetime import datetime, timedelta
 from typing import Any, Iterable, List, Mapping, MutableMapping, Optional, Tuple
 
 import requests
 from airbyte_cdk.sources import AbstractSource
 from airbyte_cdk.sources.streams import Stream
 from airbyte_cdk.sources.streams.http import HttpStream
-from airbyte_cdk.sources.streams.http.auth import Oauth2Authenticator
+from airbyte_cdk.sources.streams.http.auth import TokenAuthenticator
 
-logging.basicConfig(level=logging.DEBUG)
 """
 TODO: Most comments in this class are instructive and should be deleted after the source is implemented.
 
@@ -77,8 +75,8 @@ class PaypalTransactionStream(HttpStream, ABC):
     See the reference docs for the full list of configurable options.
     """
 
-    url_base = "https://api-m.sandbox.paypal.com/v1/reporting/"
-    # url_base = "https://api-m.paypal.com/v1/reporting/"
+    # TODO: Fill in the url base. Required.
+    url_base = "https://example-api.com/v1/"
 
     def next_page_token(self, response: requests.Response) -> Optional[Mapping[str, Any]]:
         """
@@ -95,13 +93,7 @@ class PaypalTransactionStream(HttpStream, ABC):
         :return If there is another page in the result, a mapping (e.g: dict) containing information needed to query the next page in the response.
                 If there are no more pages in the result, return None.
         """
-        decoded_response = response.json()
-        total_pages = decoded_response.get("total_pages")
-        page_number = decoded_response.get("page")
-        if page_number >= total_pages:
-            return None
-        else:
-            return {"page": page_number + 1}
+        return None
 
     def request_params(
         self, stream_state: Mapping[str, Any], stream_slice: Mapping[str, any] = None, next_page_token: Mapping[str, Any] = None
@@ -110,149 +102,110 @@ class PaypalTransactionStream(HttpStream, ABC):
         TODO: Override this method to define any query parameters to be set. Remove this method if you don't need to define request params.
         Usually contains common params e.g. pagination size etc.
         """
-        page_number = 1
-        if next_page_token:
-            page_number = next_page_token.get("page")
-
-        start_date = stream_slice["date"]
-        end_date_dt = datetime.fromisoformat(start_date) + timedelta(days=self.stream_size_in_days)
-
-        date_time_now = datetime.now().astimezone()
-        if end_date_dt > date_time_now:
-            end_date_dt = date_time_now
-
-        end_date = end_date_dt.isoformat()
-        return {"start_date": start_date, "end_date": end_date, "fields": "all", "page_size": "1", "page": page_number}
+        return {}
 
     def parse_response(self, response: requests.Response, **kwargs) -> Iterable[Mapping]:
         """
         TODO: Override this method to define how a response is parsed.
         :return an iterable containing each record in the response
         """
-        json_response = response.json()
-        records = json_response.get(self.data_field, []) if self.data_field is not None else json_response
-        yield from records
-
-    @staticmethod
-    def get_field(record: Mapping[str, Any], field_path: List[str]):
-
-        data = record
-        for attr in field_path:
-            if data:
-                data = data.get(attr)
-            else:
-                break
-
-        return data
+        yield {}
 
 
-class Transactions(PaypalTransactionStream):
+class Customers(PaypalTransactionStream):
     """
-    Stream for Transactions /v1/reporting/transactions
+    TODO: Change class name to match the table/data source this stream corresponds to.
     """
-
-    data_field = "transaction_details"
-    primary_key = "transaction_id"
-    cursor_field = ["transaction_info", "transaction_initiation_date"]
-    stream_size_in_days = 1
-
-    def __init__(self, start_date: datetime, **kwargs):
-        super().__init__(**kwargs)
-        self.start_date = start_date
-
-    def path(
-        self, stream_state: Mapping[str, Any] = None, stream_slice: Mapping[str, Any] = None, next_page_token: Mapping[str, Any] = None
-    ) -> str:
-        return "transactions"
-
-    def get_updated_state(self, current_stream_state: MutableMapping[str, Any], latest_record: Mapping[str, Any]) -> Mapping[str, any]:
-        # This method is called once for each record returned from the API to compare the cursor field value in that record with the current state
-        # we then return an updated state object. If this is the first time we run a sync or no state was passed, current_stream_state will be None.
-        latest_record_date_str = self.get_field(latest_record, self.cursor_field)
-
-        if current_stream_state and "date" in current_stream_state and latest_record_date_str:
-            if len(latest_record_date_str) == 24:
-                # Add ':' to timezone part to match iso format, example:
-                # python iso format:  2021-06-04T00:00:00+03:00
-                # format from record: 2021-06-04T00:00:00+0300
-                latest_record_date_str = ":".join([latest_record_date_str[:22], latest_record_date_str[22:]])
-
-            latest_record_date = datetime.fromisoformat(latest_record_date_str)
-            current_parsed_date = datetime.fromisoformat(current_stream_state["date"])
-
-            return {"date": max(current_parsed_date, latest_record_date).isoformat()}
-        else:
-            return {"date": self.start_date.isoformat()}
-
-    def _chunk_date_range(self, start_date: datetime) -> List[Mapping[str, any]]:
-        """
-        Returns a list of each day between the start date and now.
-        The return value is a list of dicts {'date': date_string}.
-        """
-        dates = []
-        while start_date < datetime.now().astimezone() - timedelta(days=2):
-            dates.append({"date": start_date.isoformat()})
-            start_date += timedelta(days=self.stream_size_in_days)
-        return dates
-
-    def stream_slices(
-        self, sync_mode, cursor_field: List[str] = None, stream_state: Mapping[str, Any] = None
-    ) -> Iterable[Optional[Mapping[str, any]]]:
-
-        start_date = self.start_date
-        if stream_state and "date" in stream_state:
-            start_date = datetime.fromisoformat(stream_state["date"])
-
-        return self._chunk_date_range(start_date)
-
-
-class Balances(PaypalTransactionStream):
-    """
-    Stream for Balances /v1/reporting/balances
-    """
-
-    data_field = "transaction_details"
 
     # TODO: Fill in the primary key. Required. This is usually a unique field in the stream, like an ID or a timestamp.
-    primary_key = "as_of_time"
+    primary_key = "customer_id"
 
     def path(
         self, stream_state: Mapping[str, Any] = None, stream_slice: Mapping[str, Any] = None, next_page_token: Mapping[str, Any] = None
     ) -> str:
-        return "balances"
-
-
-class PayPalOauth2Authenticator(Oauth2Authenticator):
-    """
-    curl -v POST https://api-m.sandbox.paypal.com/v1/oauth2/token \
-      -H "Accept: application/json" \
-      -H "Accept-Language: en_US" \
-      -u "CLIENT_ID:SECRET" \
-      -d "grant_type=client_credentials"
-    """
-
-    def get_refresh_request_body(self) -> Mapping[str, Any]:
-        """ Override to define additional parameters """
-        payload: MutableMapping[str, Any] = {"grant_type": "client_credentials"}
-        return payload
-
-    def refresh_access_token(self) -> Tuple[str, int]:
         """
-        returns a tuple of (access_token, token_lifespan_in_seconds)
+        TODO: Override this method to define the path this stream corresponds to. E.g. if the url is https://example-api.com/v1/customers then this
+        should return "customers". Required.
         """
-        try:
-            data = "grant_type=client_credentials"
-            headers = {"Accept": "application/json", "Accept-Language": "en_US"}
-            auth = (self.client_id, self.client_secret)
-            response = requests.request(method="POST", url=self.token_refresh_endpoint, data=data, headers=headers, auth=auth)
-            response.raise_for_status()
-            response_json = response.json()
-            return response_json["access_token"], response_json["expires_in"]
-        except Exception as e:
-            raise Exception(f"Error while refreshing access token: {e}") from e
+        return "customers"
 
 
+# Basic incremental stream
+class IncrementalPaypalTransactionStream(PaypalTransactionStream, ABC):
+    """
+    TODO fill in details of this class to implement functionality related to incremental syncs for your connector.
+         if you do not need to implement incremental sync for any streams, remove this class.
+    """
+
+    # TODO: Fill in to checkpoint stream reads after N records. This prevents re-reading of data if the stream fails for any reason.
+    state_checkpoint_interval = None
+
+    @property
+    def cursor_field(self) -> str:
+        """
+        TODO
+        Override to return the cursor field used by this stream e.g: an API entity might always use created_at as the cursor field. This is
+        usually id or date based. This field's presence tells the framework this in an incremental stream. Required for incremental.
+
+        :return str: The name of the cursor field.
+        """
+        return []
+
+    def get_updated_state(self, current_stream_state: MutableMapping[str, Any], latest_record: Mapping[str, Any]) -> Mapping[str, Any]:
+        """
+        Override to determine the latest state after reading the latest record. This typically compared the cursor_field from the latest record and
+        the current state and picks the 'most' recent cursor. This is how a stream's state is determined. Required for incremental.
+        """
+        return {}
+
+
+class Employees(IncrementalPaypalTransactionStream):
+    """
+    TODO: Change class name to match the table/data source this stream corresponds to.
+    """
+
+    # TODO: Fill in the cursor_field. Required.
+    cursor_field = "start_date"
+
+    # TODO: Fill in the primary key. Required. This is usually a unique field in the stream, like an ID or a timestamp.
+    primary_key = "employee_id"
+
+    def path(self, **kwargs) -> str:
+        """
+        TODO: Override this method to define the path this stream corresponds to. E.g. if the url is https://example-api.com/v1/employees then this should
+        return "single". Required.
+        """
+        return "employees"
+
+    def stream_slices(self, stream_state: Mapping[str, Any] = None, **kwargs) -> Iterable[Optional[Mapping[str, any]]]:
+        """
+        TODO: Optionally override this method to define this stream's slices. If slicing is not needed, delete this method.
+
+        Slices control when state is saved. Specifically, state is saved after a slice has been fully read.
+        This is useful if the API offers reads by groups or filters, and can be paired with the state object to make reads efficient. See the "concepts"
+        section of the docs for more information.
+
+        The function is called before reading any records in a stream. It returns an Iterable of dicts, each containing the
+        necessary data to craft a request for a slice. The stream state is usually referenced to determine what slices need to be created.
+        This means that data in a slice is usually closely related to a stream's cursor_field and stream_state.
+
+        An HTTP request is made for each returned slice. The same slice can be accessed in the path, request_params and request_header functions to help
+        craft that specific request.
+
+        For example, if https://example-api.com/v1/employees offers a date query params that returns data for that particular day, one way to implement
+        this would be to consult the stream state object for the last synced date, then return a slice containing each date from the last synced date
+        till now. The request_params function would then grab the date from the stream_slice and make it part of the request by injecting it into
+        the date query param.
+        """
+        raise NotImplementedError("Implement stream slices or delete this method!")
+
+
+# Source
 class SourcePaypalTransaction(AbstractSource):
+
+    url_base = "https://api-m.sandbox.paypal.com"
+    # url_base = "https://api-m.paypal.com"
+
     def check_connection(self, logger, config) -> Tuple[bool, any]:
         """
         TODO: Implement a connection check to validate that the user-provided config can be used to connect to the underlying API
@@ -264,29 +217,14 @@ class SourcePaypalTransaction(AbstractSource):
         :param logger:  logger object
         :return Tuple[bool, any]: (True, None) if the input config can be used to connect to the API successfully, (False, error) otherwise.
         """
-        token = PayPalOauth2Authenticator(
-            token_refresh_endpoint="https://api-m.sandbox.paypal.com/v1/oauth2/token",
-            client_id=config["client_id"],
-            client_secret=config["secret"],
-            refresh_token="",
-        ).get_access_token()
-        if not token:
-            return False, "Unable to fetch Paypal API token due to incorrect client_id or secret"
-
         return True, None
 
     def streams(self, config: Mapping[str, Any]) -> List[Stream]:
-        """58
+        """
         TODO: Replace the streams below with your own streams.
 
         :param config: A Mapping of the user input configuration as defined in the connector spec.
         """
-        authenticator = PayPalOauth2Authenticator(
-            token_refresh_endpoint="https://api-m.sandbox.paypal.com/v1/oauth2/token",
-            client_id=config["client_id"],
-            client_secret=config["secret"],
-            refresh_token="",
-        )
-        start_date = datetime.strptime(config["start_date"], "%Y-%m-%d").astimezone()
-        # return [Transactions(authenticator=auth), Balances(authenticator=auth)]
-        return [Transactions(authenticator=authenticator, start_date=start_date)]
+        # TODO remove the authenticator if not required.
+        auth = TokenAuthenticator(token="api_key")  # Oauth2Authenticator is also available if you need oauth support
+        return [Customers(authenticator=auth), Employees(authenticator=auth)]
