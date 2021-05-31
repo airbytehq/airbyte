f582c85ad704fd284fda4924613bff4cc50119e4
TymoshokDmytro <tymoshok.dmytro@gmail.com>
2021-05-31 16:17:07 +0300

    Make generator works from both  npm run generate or from generate.sh (docker)

--- airbyte-integrations/connector-templates/generator/generate.sh
+++ airbyte-integrations/connector-templates/generator/generate.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env sh
 # Remove container if already exist
 docker container rm -f airbyte-connector-bootstrap > /dev/null 2>&1
-# Stop container if accidentally it is running
+# Build image for container from Dockerfile
 docker build . -t airbyte/connector-bootstrap
+# Run the container
 docker run -it  --name airbyte-connector-bootstrap -v $(pwd)/..:/sources airbyte/connector-bootstrap
-docker cp airbyte-connector-bootstrap:/output/. ../../connectors/.
+# Copy generated template to connectors folder
+docker cp airbyte-connector-bootstrap:/connectors/. ../../connectors/.
 # Remove container after coping files
 docker container rm -f airbyte-connector-bootstrap > /dev/null 2>&1
