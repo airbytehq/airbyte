cb5676512796c6771fc0f46c9022deef46eeb3f9
Harshith Mullapudi <harshithmullapudi@gmail.com>
2021-06-01 10:18:28 +0530

    🎉 New source: Amazon Seller Partner (SP) API  (#3691)

---
+++ airbyte-integrations/connectors/source-amazon-seller-partner/source_amazon_seller_partner/amazon.py
@@ -0,0 +1,76 @@
+from sp_api.api import Reports, Orders
+from sp_api.base import Marketplaces
+
+
+class AmazonClient:
+    PAGECOUNT = 100
+
+    MARKETPLACES_TO_ID = {
+        "Australia": Marketplaces.AU,
+        "Brazil": Marketplaces.BR,
+        "Canada": Marketplaces.CA,
+        "Egypt": Marketplaces.EG,
+        "France": Marketplaces.FR,
+        "Germany": Marketplaces.DE,
+        "India": Marketplaces.IN,
+        "Italy": Marketplaces.IT,
+        "Japan": Marketplaces.JP,
+        "Mexico": Marketplaces.MX,
+        "Netherlands": Marketplaces.NL,
+        "Poland": Marketplaces.PL,
+        "Singapore": Marketplaces.SG,
+        "Spain": Marketplaces.ES,
+        "Sweden": Marketplaces.ES,
+        "Turkey": Marketplaces.TR,
+        "UAE": Marketplaces.AE,
+        "UK": Marketplaces.UK,
+        "USA": Marketplaces.US
+    }
+
+    GET_FLAT_FILE_ALL_ORDERS_DATA_BY_ORDER_DATE_GENERAL = "GET_FLAT_FILE_ALL_ORDERS_DATA_BY_ORDER_DATE_GENERAL"
+    ORDERS = "Orders"
+    CURSORS = {
+        GET_FLAT_FILE_ALL_ORDERS_DATA_BY_ORDER_DATE_GENERAL: "purchase-date",
+        ORDERS: "LastUpdateDate"
+    }
+
+    _REPORT_ENTITIES = [GET_FLAT_FILE_ALL_ORDERS_DATA_BY_ORDER_DATE_GENERAL]
+    _OTHER_ENTITIES = [ORDERS]
+    _ENTITIES = _REPORT_ENTITIES + _OTHER_ENTITIES
+
+    def __init__(self, credentials: dict, marketplace: str):
+        self.credentials = credentials
+        self.marketplace = self.MARKETPLACES_TO_ID[marketplace]
+
+    def get_entities(self):
+        return self._ENTITIES
+
+    def is_report(self, stream_name: str) -> str:
+        if stream_name in self._REPORT_ENTITIES:
+            return True
+        return False
+
+    def get_cursor_for_stream(self, stream_name: str) -> str:
+        return self.CURSORS[stream_name]
+
+    def fetch_orders(self, updated_after: str, page_size: int, next_token: str) -> any:
+        page_count = page_size or self.PAGECOUNT
+        response = Orders(credentials=self.credentials, marketplace=self.marketplace).get_orders(
+            LastUpdatedAfter=updated_after, MaxResultsPerPage=page_count, NextToken=next_token)
+        return response.payload
+
+    def request_report(self, report_type: str, data_start_time: str, data_end_time: str) -> any:
+        response = Reports(credentials=self.credentials, marketplace=self.marketplace).create_report(
+            reportType=report_type, dataStartTime=data_start_time, dataEndTime=data_end_time)
+
+        return response.payload
+
+    def get_report(self, report_id: str):
+        response = Reports(credentials=self.credentials,
+                           marketplace=Marketplaces.IN).get_report(report_id=report_id)
+        return response.payload
+
+    def get_report_document(self, report_document_id: str):
+        response = Reports(credentials=self.credentials, marketplace=Marketplaces.IN).get_report_document(
+            report_document_id, decrypt=True)
+        return response.payload
---
+++ airbyte-integrations/connectors/source-amazon-seller-partner/unit_tests/test_client.py
@@ -0,0 +1,116 @@
+from datetime import datetime
+from dateutil.relativedelta import *
+from typing import Mapping
+
+from source_amazon_seller_partner.client import BaseClient
+
+SP_CREDENTIALS = {
+    "refresh_token": "ABC",
+    "lwa_app_id": "lwa_app_id",
+    "lwa_client_secret": "lwa_client_secret",
+    "aws_access_key": "aws_access_key",
+    "aws_secret_key": "aws_secret_key",
+    "role_arn": "role_arn",
+    "start_date": "start_date",
+    "marketplace": "USA"
+}
+
+GET_FLAT_FILE_ALL_ORDERS_DATA_BY_ORDER_DATE_GENERAL = "GET_FLAT_FILE_ALL_ORDERS_DATA_BY_ORDER_DATE_GENERAL"
+ORDERS = "Orders"
+_ENTITIES = [GET_FLAT_FILE_ALL_ORDERS_DATA_BY_ORDER_DATE_GENERAL, ORDERS]
+
+MARKETPLACE = "India"
+ORDERS_RESPONSE = [{"orderId": 1}]
+
+
+class MockAmazonClient:
+    def __init__(self, credentials, marketplace):
+        self.credentials = credentials
+        self.marketplace = marketplace
+
+    def fetch_orders(updated_after, page_count, next_token=None):
+        return ORDERS_RESPONSE
+
+
+def get_base_client(config: Mapping):
+    return BaseClient(**config)
+
+
+def test_check_connection(mocker):
+    mocker.patch("source_amazon_seller_partner.client.AmazonClient",
+                 return_value=MockAmazonClient)
+    base_client = get_base_client(SP_CREDENTIALS)
+
+    assert ORDERS_RESPONSE == base_client.check_connection()
+
+
+def test_get_cursor_state():
+    cursor_value = "2021-05-02"
+    end_date = "2021-06-02"
+
+    base_client = get_base_client(SP_CREDENTIALS)
+
+    assert "2021-05-03" == base_client._get_cursor_state(
+        cursor_value=cursor_value, end_date=end_date)
+
+    cursor_value = "2021-02-28"
+    end_date = "2021-03-02"
+
+    assert "2021-03-03" == base_client._get_cursor_state(
+        cursor_value=cursor_value, end_date=end_date)
+
+
+def test_get_records():
+    data = {"document": "name\ttest\nairbyte\t1"}
+    base_client = get_base_client(SP_CREDENTIALS)
+
+    assert [{"name": "airbyte", "test": "1"}] == base_client._get_records(data)
+
+
+def test_apply_conversion_window():
+    current_date = "2021-03-04"
+    base_client = get_base_client(SP_CREDENTIALS)
+
+    assert "2021-02-18" == base_client._apply_conversion_window(current_date)
+
+
+def test_convert_array_into_dict():
+    headers = ["name", "test"]
+    records = ["airbyte\t1"]
+    assert [{"name": "airbyte", "test": "1"}
+            ] == BaseClient._convert_array_into_dict(headers, records)
+
+
+def test_increase_date_by_month():
+    current_date = "2021-03-04"
+    assert "2021-04-04" == BaseClient._increase_date_by_month(current_date)
+
+
+def fmt_date(date):
+    return datetime.strftime(date, '%Y-%m-%d')
+
+
+def test_get_date_parameters():
+    # If the start date is more than one month ago then we expect a full 30 day increment
+    now = datetime.today()
+    two_months_ago = fmt_date(now - relativedelta(months=2))
+    one_month_ago = fmt_date(now - relativedelta(months=1))
+    assert (two_months_ago, one_month_ago) == BaseClient._get_date_parameters(
+        two_months_ago)
+
+    # If the start date is less than one month ago we expect to advance no later than today
+    one_week_ago = fmt_date(now - relativedelta(weeks=1))
+    yesterday = fmt_date(now - relativedelta(days=1))
+    assert (one_week_ago, yesterday) == BaseClient._get_date_parameters(
+        one_week_ago)
+
+
+def test_get_cursor_or_none():
+    state = {"stream_name": {"update-date": "2021-03-04"}}
+    stream_name = "stream_name"
+    cursor_name = "update-date"
+    assert "2021-03-04" == BaseClient._get_cursor_or_none(
+        state, stream_name, cursor_name)
+    state = {"stream_name-2": {"update-date": "2021-03-04"}}
+    assert None == BaseClient._get_cursor_or_none(
+        state, stream_name, cursor_name)
