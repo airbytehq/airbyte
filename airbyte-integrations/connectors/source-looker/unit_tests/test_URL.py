7d58efa049c06a327828f5bcea2b9d93190143b0
root <root@DESKTOP-SS2MC07.localdomain>
2021-06-03 19:41:49 +0530

    writing unitests using pytest

--- airbyte-integrations/connectors/source-looker/unit_tests/test_URL.py
+++ airbyte-integrations/connectors/source-looker/unit_tests/test_URL.py
@@ -1,5 +1,4 @@
 import re
-import unittest
 
 def valid_URL(test_str):
     pattern="(^[a-zA-Z0-9._-]*\.looker\.com$)|(^(?:[a-zA-Z0-9._-]*)?[a-zA-Z0-9._-]*\.[a-zA-Z0-9._-]*(?::\d{1,5})?$)|(^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}(?::\d{1,5})?$)"
@@ -9,36 +8,37 @@ def valid_URL(test_str):
 
 
 
-class TestURLPattern(unittest.TestCase):
+#Defining tests below
+def test_valid_URL_1():
+    url="airbyte.looker.com"
+    assert(valid_URL("airbyte.looker.com")==url)
 
-    def test_valid_URL_1(self):
-        url="airbyte.looker.com"
-        self.assertEqual(valid_URL("airbyte.looker.com"),url)
+def test_valid_URL_2():
+    url="looker.airbyte.com"
+    assert(valid_URL("looker.airbyte.com")==url)
 
-    def test_valid_URL_2(self):
-        url="looker.airbyte.com"
-        self.assertEqual(valid_URL("looker.airbyte.com"),url)
+def test_valid_URL_3():
+    url="125.125.125.126:8009"
+    assert(valid_URL("125.125.125.126:8009")==url)
 
-    def test_valid_URL_3(self):
-        url="125.125.125.126:8009"
-        self.assertEqual(valid_URL("125.125.125.126:8009"),url)
+def test_valid_URL_4():
+    url="cooradial.sample.co.in"
+    assert(valid_URL("cooradial.sample.co.in")==url)
 
-    def test_valid_URL_4(self):
-        url="cooradial.sample.co.in"
-        self.assertEqual(valid_URL("cooradial.sample.co.in"),url)
-    
-    def test_valid_URL_5(self):
-        url="my.shiny.website.com:1234"
-        self.assertEqual(valid_URL("my.shiny.website.com:1234"),url)
+def test_valid_URL_5():
+    url="my.shiny.website.com:1234"
+    assert(valid_URL("my.shiny.website.com:1234")==url)
 
-    def test_valid_URL_6(self):
-        url="aeneas.mit.edu"
-        self.assertEqual(valid_URL("aeneas.mit.edu"),url)
+def test_valid_URL_6():
+    url="aeneas.mit.edu"
+    assert(valid_URL("aeneas.mit.edu")==url)
 
-    def test_valid_URL_7(self):
-        self.assertEqual(valid_URL("suj.ki@fg"),None)     
+def test_valid_URL_7():
+    assert(valid_URL("suj.ki@fg")==None)     
 
-    def test_valid_URL_8(self):
-        self.assertEqual(valid_URL("www$google.com"),None)     
+def test_valid_URL_8():
+    assert(valid_URL("www$google.com")==None)     
 
-        
+
+
+
