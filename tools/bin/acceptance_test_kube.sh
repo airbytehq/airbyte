978350a6973f77f45c4662cebc9371165407d06d
jrhizor <me@jaredrhizor.com>
2021-06-04 11:19:05 -0700

    print logs after run

--- tools/bin/acceptance_test_kube.sh
+++ tools/bin/acceptance_test_kube.sh
@@ -14,7 +14,13 @@ kubectl apply -k kube/overlays/dev
 kubectl wait --for=condition=Available deployment/airbyte-server --timeout=300s || (kubectl describe pods && exit 1)
 kubectl wait --for=condition=Available deployment/airbyte-scheduler --timeout=300s || (kubectl describe pods && exit 1)
 
-sleep 20s
+sleep 120s
+
+server_logs () { echo "server logs:" && kubectl logs deployment.apps/airbyte-server; }
+scheduler_logs () { echo "scheduler logs:" && kubectl logs deployment.apps/airbyte-scheduler; }
+print_all_logs () { server_logs; scheduler_logs; }
+
+trap "echo 'kube logs:' && print_all_logs" EXIT
 
 kubectl port-forward svc/airbyte-server-svc 8001:8001 &
 
