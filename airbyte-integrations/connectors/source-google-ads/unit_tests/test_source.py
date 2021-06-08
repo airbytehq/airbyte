1fb924051af57a2641d8883d16d67bb5b9f1e519
harshithmullapudi <harshithmullapudi@gmail.com>
2021-06-08 10:29:31 +0530

    uncomment test cases as we have config

--- airbyte-integrations/connectors/source-google-ads/unit_tests/test_source.py
+++ airbyte-integrations/connectors/source-google-ads/unit_tests/test_source.py
@@ -12,29 +12,27 @@ def test_chunk_date_range():
             {'date': '2021-04-18'}] == response
 
 
-"""
-This won't work until we get sample credentials 
-"""
-
-# SAMPLE_CONFIG = {
-#   "developer_token": "developer_token",
-#   "client_id": "client_id",
-#   "client_secret": "client_secret",
-#   "refresh_token": "refresh_token",
-#   "start_date": "start_date",
-#   "customer_id": "customer_id"
-# }
+SAMPLE_CONFIG = {
+    "developer_token": "developer_token",
+    "client_id": "client_id",
+    "client_secret": "client_secret",
+    "refresh_token": "refresh_token",
+    "start_date": "start_date",
+    "customer_id": "customer_id"
+}
 
 
-# def test_get_updated_state():
-#     client = AdGroupAdReport(SAMPLE_CONFIG)
-#     current_state_stream = {}
-#     latest_record = {"segments.date": "2020-01-01"}
+def test_get_updated_state():
+    client = AdGroupAdReport(SAMPLE_CONFIG)
+    current_state_stream = {}
+    latest_record = {"segments.date": "2020-01-01"}
 
-#     new_stream_state = client.get_updated_state(current_state_stream, latest_record)
-#     assert new_stream_state == {'segments.date': '2020-01-01'}
+    new_stream_state = client.get_updated_state(
+        current_state_stream, latest_record)
+    assert new_stream_state == {'segments.date': '2020-01-01'}
 
-#     current_state_stream = {'segments.date': '2020-01-01'}
-#     latest_record = {"segments.date": "2020-02-01"}
-#     new_stream_state = client.get_updated_state(current_state_stream, latest_record)
-#     assert new_stream_state == {'segments.date': '2020-02-01'}
+    current_state_stream = {'segments.date': '2020-01-01'}
+    latest_record = {"segments.date": "2020-02-01"}
+    new_stream_state = client.get_updated_state(
+        current_state_stream, latest_record)
+    assert new_stream_state == {'segments.date': '2020-02-01'}
