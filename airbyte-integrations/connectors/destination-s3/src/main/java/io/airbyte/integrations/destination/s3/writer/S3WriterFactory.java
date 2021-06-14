cc4e1d3a1cedbf86dda93fed935b1373c5dcfad5
LiRen Tu <tuliren.git@outlook.com>
2021-06-14 16:49:27 -0700

    🎉 Destination S3: parquet output (#3908)
    * Add skeleton code for parquet writer
    
    * Refactor s3 destination code
    
    * Add parquet to spec
    
    * Complete parquet writer
    
    * Change testing data from int to double
    
    * Add acceptance test for parquet writer
    
    * Handle special schema field names
    
    * Format code
    
    * Add parquet config
    
    * Add documentation
    
    * Add unit tests
    
    * Fix typo
    
    * Update document
    
    * Bump version
    
    * Fix date format
    
    * Fix credential filename
    
    * Update doc
    
    * Update test and publish commands
    
    * Refactor s3 format config
    
    * Append compression codec file extension
    
    * Update doc
    
    * Remove compression codec file extension
    
    * Add comments
    
    * Add README, CHANGELOG, and sample configs
    
    * Move changelog
    
    * Use switch statement
    
    * Move filename helper method to base writer
    
    * Rename converter
    
    * Separate test cases
    
    * Drop union type length restriction
    
    * Support array with multiple types
    
    * Move comments to connector doc
    
    * Share config between acceptance tests
    
    * Add doc about additional properties
    
    * Move shared code out of if branch
    
    * Add doc about adding a new format
    
    * Format code
    
    * Bump version to 0.1.4
    
    * Fix default max padding size

--- airbyte-integrations/connectors/destination-s3/src/main/java/io/airbyte/integrations/destination/s3/writer/S3WriterFactory.java
+++ airbyte-integrations/connectors/destination-s3/src/main/java/io/airbyte/integrations/destination/s3/writer/S3WriterFactory.java
@@ -22,35 +22,22 @@
  * SOFTWARE.
  */
 
-package io.airbyte.integrations.destination.s3;
+package io.airbyte.integrations.destination.s3.writer;
 
-import com.fasterxml.jackson.databind.JsonNode;
-import io.airbyte.commons.json.Jsons;
-import io.airbyte.integrations.destination.s3.csv.S3CsvFormatConfig;
-import io.airbyte.integrations.destination.s3.parquet.S3ParquetFormatConfig;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import com.amazonaws.services.s3.AmazonS3;
+import io.airbyte.integrations.destination.s3.S3DestinationConfig;
+import io.airbyte.protocol.models.ConfiguredAirbyteStream;
+import java.sql.Timestamp;
 
-public class S3FormatConfigs {
+/**
+ * Create different {@link S3Writer} based on {@link S3DestinationConfig}.
+ */
+public interface S3WriterFactory {
 
-  protected static final Logger LOGGER = LoggerFactory.getLogger(S3FormatConfigs.class);
-
-  public static S3FormatConfig getS3FormatConfig(JsonNode config) {
-    JsonNode formatConfig = config.get("format");
-    LOGGER.info("S3 format config: {}", formatConfig.toString());
-    S3Format formatType = S3Format.valueOf(formatConfig.get("format_type").asText().toUpperCase());
-
-    switch (formatType) {
-      case CSV -> {
-        return new S3CsvFormatConfig(formatConfig);
-      }
-      case PARQUET -> {
-        return new S3ParquetFormatConfig(formatConfig);
-      }
-      default -> {
-        throw new RuntimeException("Unexpected output format: " + Jsons.serialize(config));
-      }
-    }
-  }
+  S3Writer create(S3DestinationConfig config,
+                  AmazonS3 s3Client,
+                  ConfiguredAirbyteStream configuredStream,
+                  Timestamp uploadTimestamp)
+      throws Exception;
 
 }
