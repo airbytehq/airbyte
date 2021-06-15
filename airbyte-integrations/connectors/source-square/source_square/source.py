49044f795d8f7d6b86a84b7394bae86d47d2ba0c
TymoshokDmytro <tymoshok.dmytro@gmail.com>
2021-06-16 00:05:02 +0300

    Refactor. Add incremental sync to items, categories, discounts and taxes

--- airbyte-integrations/connectors/source-square/source_square/source.py
+++ airbyte-integrations/connectors/source-square/source_square/source.py
@@ -24,6 +24,7 @@
 from abc import ABC
 from typing import Any, Iterable, List, Mapping, MutableMapping, Optional, Tuple
 
+import pendulum
 import requests
 from airbyte_cdk.sources import AbstractSource
 from airbyte_cdk.sources.streams import Stream
@@ -44,9 +45,52 @@ The approach here is not authoritative, and devs are free to use their own judge
 There are additional required TODOs in the files within the integration_tests folder and the spec.json file.
 """
 
+import logging
 
-# Basic full refresh stream
+logging.basicConfig(level=logging.DEBUG)
+
+
 class SquareStream(HttpStream, ABC):
+
+    def __init__(self, is_sandbox: bool, api_version: str, start_date: str, **kwargs):
+        super().__init__(**kwargs)
+        self.is_sandbox = is_sandbox
+        self.api_version = api_version
+        # Converting users ISO 8601 format (YYYY-MM-DD) to RFC 3339 (2021-06-14T13:47:56.799Z)
+        # Because this standard is used in 'updated_at' records field
+        self.start_date = '{}'.format(pendulum.parse(start_date))
+
+    data_field = None
+    primary_key = 'id'
+
+    @property
+    def url_base(self) -> str:
+        return "https://connect.squareup{}.com/v2/".format('sandbox' if self.is_sandbox else '')
+
+    def next_page_token(self, response: requests.Response) -> Optional[Mapping[str, Any]]:
+        next_page_cursor = response.json().get("cursor", False)
+        if next_page_cursor:
+            return {"cursor": next_page_cursor}
+
+    def request_headers(
+            self, stream_state: Mapping[str, Any], stream_slice: Mapping[str, Any] = None,
+            next_page_token: Mapping[str, Any] = None
+    ) -> Mapping[str, Any]:
+        return {'Square-Version': self.api_version, "Content-Type": "application/json"}
+
+    def request_params(
+            self, stream_state: Mapping[str, Any], stream_slice: Mapping[str, any] = None,
+            next_page_token: Mapping[str, Any] = None
+    ) -> MutableMapping[str, Any]:
+        return next_page_token if next_page_token else {}
+
+    def parse_response(self, response: requests.Response, **kwargs) -> Iterable[Mapping]:
+        json_response = response.json()
+        records = json_response.get(self.data_field, []) if self.data_field is not None else json_response
+        yield from records
+
+
+class SquareCatalogObjectsStream(SquareStream):
     """
     TODO remove this comment
 
@@ -73,140 +117,44 @@ class SquareStream(HttpStream, ABC):
     See the reference docs for the full list of configurable options.
     """
 
-    data_field = None
-    # TODO Change this to the prod version https://connect.squareup.com/v2/
-    url_base = "https://connect.squareupsandbox.com/v2/"
+    data_field = 'objects'
+    http_method = 'POST'
+    limit = 1000
 
-    def next_page_token(self, response: requests.Response) -> Optional[Mapping[str, Any]]:
-        next_page_cursor = response.json().get("cursor", False)
-        if next_page_cursor:
-            return {"cursor": next_page_cursor}
-
-        return None
+    def path(
+            self, stream_state: Mapping[str, Any] = None, stream_slice: Mapping[str, Any] = None,
+            next_page_token: Mapping[str, Any] = None
+    ) -> str:
+        return "catalog/search"
 
     def request_params(
             self, stream_state: Mapping[str, Any], stream_slice: Mapping[str, any] = None,
             next_page_token: Mapping[str, Any] = None
     ) -> MutableMapping[str, Any]:
-        if next_page_token:
-            return next_page_token
-
         return {}
 
-    def parse_response(self, response: requests.Response, **kwargs) -> Iterable[Mapping]:
-        """
-        :return an iterable containing each record in the response
-        """
-        json_response = response.json()
-        records = json_response.get(self.data_field, []) if self.data_field is not None else json_response
-        yield from records
+    def request_body_json(
+            self,
+            stream_state: Mapping[str, Any],
+            stream_slice: Mapping[str, Any] = None,
+            next_page_token: Mapping[str, Any] = None,
+    ) -> Optional[Mapping]:
+        json_payload = {
+            "include_deleted_objects": True,
+            "include_related_objects": False,
+            "limit": self.limit
+        }
+
+        if next_page_token:
+            json_payload.update({'cursor': next_page_token['cursor']})
+
+        return json_payload
 
 
-class Items(SquareStream):
-    """
-    """
-    data_field = "objects"
+class IncrementalSquareCatalogObjectsStream(SquareCatalogObjectsStream, ABC):
+    state_checkpoint_interval = SquareCatalogObjectsStream.limit
 
-    primary_key = "id"
-
-    def path(
-            self, stream_state: Mapping[str, Any] = None, stream_slice: Mapping[str, Any] = None,
-            next_page_token: Mapping[str, Any] = None
-    ) -> str:
-        """
-        should return "catalog/list?types=ITEM". Required.
-        """
-        return "catalog/list?types=ITEM"
-
-
-class Discounts(SquareStream):
-    """
-    """
-    data_field = "objects"
-
-    primary_key = "id"
-
-    def path(
-            self, stream_state: Mapping[str, Any] = None, stream_slice: Mapping[str, Any] = None,
-            next_page_token: Mapping[str, Any] = None
-    ) -> str:
-        """
-        should return "catalog/list?types=ITEM". Required.
-        """
-        return "catalog/list?types=DISCOUNT"
-
-
-class Categories(SquareStream):
-    """
-    """
-    data_field = "objects"
-
-    primary_key = "id"
-
-    def path(
-            self, stream_state: Mapping[str, Any] = None, stream_slice: Mapping[str, Any] = None,
-            next_page_token: Mapping[str, Any] = None
-    ) -> str:
-        """
-        should return "catalog/list?types=CATEGORY". Required.
-        """
-        return "catalog/list?types=CATEGORY"
-
-
-class Taxes(SquareStream):
-    """
-    """
-    data_field = "objects"
-
-    primary_key = "id"
-
-    def path(
-            self, stream_state: Mapping[str, Any] = None, stream_slice: Mapping[str, Any] = None,
-            next_page_token: Mapping[str, Any] = None
-    ) -> str:
-        """
-        should return "catalog/list?types=CATEGORY". Required.
-        """
-        return "catalog/list?types=Tax"
-
-
-class Locations(SquareStream):
-    """
-    """
-    data_field = "locations"
-
-    primary_key = "id"
-
-    def path(
-            self, stream_state: Mapping[str, Any] = None, stream_slice: Mapping[str, Any] = None,
-            next_page_token: Mapping[str, Any] = None
-    ) -> str:
-        """
-        should return "catalog/list?types=CATEGORY". Required.
-        """
-        return "locations"
-
-
-# Basic incremental stream
-class IncrementalSquareStream(SquareStream, ABC):
-    """
-    TODO fill in details of this class to implement functionality related to incremental syncs for your connector.
-         if you do not need to implement incremental sync for any streams, remove this class.
-    """
-
-    # TODO: Fill in to checkpoint stream reads after N records. This prevents re-reading of data if the stream fails for any reason.
-    state_checkpoint_interval = None
-
-    @property
-    def cursor_field(self) -> str:
-        """
-        TODO
-        Override to return the cursor field used by this stream e.g: an API entity might always use created_at as the cursor field. This is
-        usually id or date based. This field's presence tells the framework this in an incremental stream. Required for incremental.
-
-        :return str: The name of the cursor field.
-        """
-        return []
+    cursor_field = "updated_at"
 
     def get_updated_state(self, current_stream_state: MutableMapping[str, Any], latest_record: Mapping[str, Any]) -> \
             Mapping[str, Any]:
@@ -214,52 +162,46 @@ class IncrementalSquareStream(SquareStream, ABC):
         Override to determine the latest state after reading the latest record. This typically compared the cursor_field from the latest record and
         the current state and picks the 'most' recent cursor. This is how a stream's state is determined. Required for incremental.
         """
-        return {}
+        if current_stream_state is not None and self.cursor_field in current_stream_state:
+            return {self.cursor_field: max(current_stream_state[self.cursor_field], latest_record[self.cursor_field])}
+        else:
+            return {self.cursor_field: self.start_date}
 
 
-class Employees(IncrementalSquareStream):
-    """
-    TODO: Change class name to match the table/data source this stream corresponds to.
-    """
+class Items(IncrementalSquareCatalogObjectsStream):
+    def request_body_json(self, **kwargs) -> Optional[Mapping]:
+        return {**super(Items, self).request_body_json(**kwargs), "object_types": ["ITEM"]}
 
-    # TODO: Fill in the cursor_field. Required.
-    cursor_field = "start_date"
 
-    # TODO: Fill in the primary key. Required. This is usually a unique field in the stream, like an ID or a timestamp.
-    primary_key = "employee_id"
+class Categories(IncrementalSquareCatalogObjectsStream):
+    def request_body_json(self, **kwargs) -> Optional[Mapping]:
+        return {**super(Categories, self).request_body_json(**kwargs), "object_types": ["CATEGORY"]}
 
-    def path(self, **kwargs) -> str:
-        """
-        TODO: Override this method to define the path this stream corresponds to. E.g. if the url is https://example-api.com/v1/employees then this should
-        return "single". Required.
-        """
-        return "employees"
 
-    def stream_slices(self, stream_state: Mapping[str, Any] = None, **kwargs) -> Iterable[Optional[Mapping[str, any]]]:
-        """
-        TODO: Optionally override this method to define this stream's slices. If slicing is not needed, delete this method.
+class Discounts(IncrementalSquareCatalogObjectsStream):
+    def request_body_json(self, **kwargs) -> Optional[Mapping]:
+        return {**super(Discounts, self).request_body_json(**kwargs), "object_types": ["DISCOUNT"]}
 
-        Slices control when state is saved. Specifically, state is saved after a slice has been fully read.
-        This is useful if the API offers reads by groups or filters, and can be paired with the state object to make reads efficient. See the "concepts"
-        section of the docs for more information.
 
-        The function is called before reading any records in a stream. It returns an Iterable of dicts, each containing the
-        necessary data to craft a request for a slice. The stream state is usually referenced to determine what slices need to be created.
-        This means that data in a slice is usually closely related to a stream's cursor_field and stream_state.
+class Taxes(IncrementalSquareCatalogObjectsStream):
+    def request_body_json(self, **kwargs) -> Optional[Mapping]:
+        return {**super(Taxes, self).request_body_json(**kwargs), "object_types": ["TAX"]}
 
-        An HTTP request is made for each returned slice. The same slice can be accessed in the path, request_params and request_header functions to help
-        craft that specific request.
 
-        For example, if https://example-api.com/v1/employees offers a date query params that returns data for that particular day, one way to implement
-        this would be to consult the stream state object for the last synced date, then return a slice containing each date from the last synced date
-        till now. The request_params function would then grab the date from the stream_slice and make it part of the request by injecting it into
-        the date query param.
-        """
-        raise NotImplementedError("Implement stream slices or delete this method!")
+class Locations(SquareStream):
+    data_field = "locations"
+
+    def path(
+            self, stream_state: Mapping[str, Any] = None, stream_slice: Mapping[str, Any] = None,
+            next_page_token: Mapping[str, Any] = None
+    ) -> str:
+        return "locations"
 
 
 # Source
 class SourceSquare(AbstractSource):
+    api_version = "2021-05-13"  # Latest Stable Release
+
     def check_connection(self, logger, config) -> Tuple[bool, any]:
         """
         See https://github.com/airbytehq/airbyte/blob/master/airbyte-integrations/connectors/source-stripe/source_stripe/source.py#L232
@@ -270,16 +212,12 @@ class SourceSquare(AbstractSource):
         :return Tuple[bool, any]: (True, None) if the input config can be used to connect to the API successfully, (False, error) otherwise.
         """
 
-        api_key = config["api_key"]
-        api_version = "2021-05-13"  # Latest Stable Release
-
         headers = {
-            "Square-Version": api_version,
-            "Authorization": "Bearer {}".format(api_key),
+            "Square-Version": self.api_version,
+            "Authorization": "Bearer {}".format(config["api_key"]),
             "Content-Type": "application/json"
         }
-
-        url = f"https://connect.squareupsandbox.com/v2/catalog/info"
+        url = "https://connect.squareup{}.com/v2/catalog/info".format('sandbox' if config['is_sandbox'] else '')
 
         try:
             session = requests.get(url, headers=headers)
@@ -293,10 +231,14 @@ class SourceSquare(AbstractSource):
         :param config: A Mapping of the user input configuration as defined in the connector spec.
         """
         auth = TokenAuthenticator(token=config["api_key"])
+        args = {'authenticator': auth,
+                'is_sandbox': config['is_sandbox'],
+                'api_version': self.api_version,
+                'start_date': config['start_date']}
         return [
-            Items(authenticator=auth),
-            Categories(authenticator=auth),
-            Discounts(authenticator=auth),
-            Taxes(authenticator=auth),
-            Locations(authenticator=auth)
+            Items(**args),
+            Categories(**args),
+            Discounts(**args),
+            Taxes(**args),
+            Locations(**args)
         ]
