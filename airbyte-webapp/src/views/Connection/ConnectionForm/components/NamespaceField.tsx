836fc37f2db0458956db8a866db9cecec8d8b48d
Artem Astapenko <jamakase54@gmail.com>
2021-06-15 23:53:32 +0300

    Update styling for namespace

--- airbyte-webapp/src/views/Connection/ConnectionForm/components/NamespaceField.tsx
+++ airbyte-webapp/src/views/Connection/ConnectionForm/components/NamespaceField.tsx
@@ -1,40 +1,77 @@
-import React from "react";
+import React, { useMemo } from "react";
 import { Field, FieldProps } from "formik";
+import { FormattedMessage } from "react-intl";
+import styled from "styled-components";
 
-import { DropDown, Input } from "components";
+import { ControlLabels, DropDown, Input } from "components";
 import { ConnectionNamespaceDefinition } from "core/domain/connection";
 
+const NamespaceConfigurationLabel = styled(ControlLabels)`
+  width: 360px;
+`;
+
+const NamespaceFormatLabel = styled(ControlLabels)`
+  margin-left: 21px;
+`;
+
+const Row = styled.div`
+  display: flex;
+  margin-bottom: 26px;
+`;
+
 const NamespaceField: React.FC<{}> = ({}) => {
+  const definitions = useMemo(
+    () => [
+      {
+        value: ConnectionNamespaceDefinition.Source,
+        text: "source",
+      },
+      {
+        value: ConnectionNamespaceDefinition.Destination,
+        text: "destination",
+      },
+      {
+        value: ConnectionNamespaceDefinition.CustomFormat,
+        text: "custom",
+      },
+    ],
+    []
+  );
   return (
-    <>
+    <Row>
       <Field name="namespaceDefinition">
         {({ field, form }: FieldProps<string>) => (
           <>
-            <DropDown
-              data={[
-                {
-                  value: ConnectionNamespaceDefinition.Source,
-                  text: "source",
-                },
-                {
-                  value: ConnectionNamespaceDefinition.Destination,
-                  text: "destination",
-                },
-                {
-                  value: ConnectionNamespaceDefinition.CustomFormat,
-                  text: "custom",
-                },
-              ]}
-              value={field.value}
-              onChange={({ value }) => form.setFieldValue(field.name, value)}
-            />
+            <NamespaceConfigurationLabel
+              label={
+                <FormattedMessage id="connectionForm.namespaceDefinition.title" />
+              }
+              message={
+                <FormattedMessage id="connectionForm.namespaceDefinition.subtitle" />
+              }
+            >
+              <DropDown
+                data={definitions}
+                value={field.value}
+                onChange={({ value }) => form.setFieldValue(field.name, value)}
+              />
+            </NamespaceConfigurationLabel>
             {field.value === ConnectionNamespaceDefinition.CustomFormat && (
-              <Field name="namespaceFormat" component={Input} />
+              <NamespaceFormatLabel
+                label={
+                  <FormattedMessage id="connectionForm.namespaceFormat.title" />
+                }
+                message={
+                  <FormattedMessage id="connectionForm.namespaceFormat.subtitle" />
+                }
+              >
+                <Field name="namespaceFormat" component={Input} />
+              </NamespaceFormatLabel>
             )}
           </>
         )}
       </Field>
-    </>
+    </Row>
   );
 };
 
