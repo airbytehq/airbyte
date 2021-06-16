746a5938dc21ff87676b3e3739b65e303067322d
Davin Chia <davinchia@gmail.com>
2021-06-16 23:36:43 +0800

    Strip quotes from jq output.

--- tools/bin/logging_test.sh
+++ tools/bin/logging_test.sh
@@ -3,8 +3,8 @@
 set -e
 
 echo "Writing cloud storage credentials.."
-export AWS_ACCESS_KEY_ID="$(echo "$AWS_S3_INTEGRATION_TEST_CREDS" | jq .aws_access_key_id)"
-export AWS_SECRET_ACCESS_KEY="$(echo "$AWS_S3_INTEGRATION_TEST_CREDS" | jq .aws_secret_access_key)"
+export AWS_ACCESS_KEY_ID="$(echo "$AWS_S3_INTEGRATION_TEST_CREDS" | jq -r .aws_access_key_id)"
+export AWS_SECRET_ACCESS_KEY="$(echo "$AWS_S3_INTEGRATION_TEST_CREDS" | jq -r .aws_secret_access_key)"
 export S3_LOG_BUCKET=airbytetestbucket
 export S3_LOG_BUCKET_REGION=eu-west-3
 
