a507f30a9c290042681a18a018a539f563f529ef
Yevhenii <34103125+yevhenii-ldv@users.noreply.github.com>
2021-06-17 15:58:43 +0300

    Google Ads Connector: Release community-contributed  (#4135)
    * add public docs to Summary.md, add new connector to YAML, little update of code
    
    * update code, delete utils and util_tests
    
    * remove ad_group_ad.ad.call_only_ad.phone_number, it is 'Unrecognized field in the query'
    
    * update spec
    
    Co-authored-by: ykurochkin <y.kurochkin@zazmic.com>

--- airbyte-integrations/connectors/source-google-ads/unit_tests/test_google_ads.py
+++ airbyte-integrations/connectors/source-google-ads/unit_tests/test_google_ads.py
@@ -75,10 +75,12 @@ class MockGoogleAdsClient:
 
 
 SAMPLE_CONFIG = {
-    "developer_token": "developer_token",
-    "client_id": "client_id",
-    "client_secret": "client_secret",
-    "refresh_token": "refresh_token",
+    "credentials": {
+        "developer_token": "developer_token",
+        "client_id": "client_id",
+        "client_secret": "client_secret",
+        "refresh_token": "refresh_token",
+    },
     "customer_id": "customer_id",
 }
 
