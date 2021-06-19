a479d8b9b7faad7048955e77519fa095adbab80b
Jared Rhizor <jared@dataline.io>
2021-06-18 18:04:45 -0700

    make entrypoints posix-compatible (#4220)

--- airbyte-workers/src/main/resources/entrypoints/check.sh
+++ airbyte-workers/src/main/resources/entrypoints/check.sh
@@ -1,11 +1,11 @@
 trap "touch TERMINATION_FILE_CHECK" EXIT
 (set -e; while true; do curl -s HEARTBEAT_URL &> /dev/null; sleep 1; done) &
 CHILD_PID=$!
-(while true; do if [[ -f TERMINATION_FILE_MAIN ]]; then kill $CHILD_PID; exit 0; fi; sleep 1; done) &
+(while true; do if [ -f TERMINATION_FILE_MAIN ]; then kill $CHILD_PID; exit 0; fi; sleep 1; done) &
 wait $CHILD_PID
 EXIT_CODE=$?
 
-if [[ -f TERMINATION_FILE_MAIN ]]
+if [ -f TERMINATION_FILE_MAIN ]
 then
   exit 0
 else
