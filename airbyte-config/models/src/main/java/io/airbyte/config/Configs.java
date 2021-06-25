e416aca59ccc40cdb72987494aa7b327a081a6c3
Christophe Duong <christophe.duong@gmail.com>
2021-06-25 02:18:11 +0200

    Specify kube namespace (#4317)
    * Specify kube namespace
    
    * remove work that turned out to be unnecessary and add debugging statement
    
    * make it possible to run without configuring s3
    
    * cleanup
    
    * use default as default namespace
    
    Co-authored-by: jrhizor <me@jaredrhizor.com>

--- airbyte-config/models/src/main/java/io/airbyte/config/Configs.java
+++ airbyte-config/models/src/main/java/io/airbyte/config/Configs.java
@@ -65,6 +65,8 @@ public interface Configs {
 
   Set<Integer> getTemporalWorkerPorts();
 
+  String getKubeNamespace();
+
   // The following methods retrieve logging related information.
   String getS3LogBucket();
 
