6a631ff42573a16c5412eeda3f07b6d0cd7ceb86
Sherif A. Nada <snadalive@gmail.com>
2021-06-30 14:18:54 -0700

    fix formatting, remove slack singer, add okta connectors (#4448)

--- airbyte-integrations/connectors/source-chargebee/source_chargebee/source.py
+++ airbyte-integrations/connectors/source-chargebee/source_chargebee/source.py
@@ -1,22 +1,43 @@
-from abc import ABC, abstractmethod
-from typing import Any, Iterable, List, Mapping, MutableMapping, Optional, Tuple
+#
+# MIT License
+#
+# Copyright (c) 2020 Airbyte
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+#
 
-import pendulum
-import requests
+from typing import Any, Iterable, List, Mapping, MutableMapping, Tuple
+
 import backoff
 
 # Chargebee
 import chargebee
-from chargebee.main import ChargeBee
-from chargebee.list_result import ListResult  # stores next_offset
-from chargebee.models import Subscription, Customer, Invoice, Order, Plan, Addon
-from chargebee.api_error import OperationFailedError
+import pendulum
+from airbyte_cdk.logger import AirbyteLogger
+from airbyte_cdk.models import SyncMode
 
 # Airbyte
 from airbyte_cdk.sources import AbstractSource
 from airbyte_cdk.sources.streams import Stream
-from airbyte_cdk.models import AirbyteStream, SyncMode
-from airbyte_cdk.logger import AirbyteLogger
+from chargebee.api_error import OperationFailedError
+from chargebee.list_result import ListResult  # stores next_offset
+from chargebee.models import Addon, Customer, Invoice, Order, Plan, Subscription
 
 # Backoff params below
 # according to Chargebee's guidance on rate limit
