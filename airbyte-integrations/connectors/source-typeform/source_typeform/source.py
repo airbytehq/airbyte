dc59cb85607d231c19eb6126543b34bc5e00b067
yaroslav-dudar <flayingfog@gmail.com>
2021-06-30 12:24:02 +0300

    responses stream: implemented incremental sync, upd form schema

--- airbyte-integrations/connectors/source-typeform/source_typeform/source.py
+++ airbyte-integrations/connectors/source-typeform/source_typeform/source.py
@@ -1,6 +1,7 @@
+#
 # MIT License
 #
-# Copyright (c) 2021 Airbyte
+# Copyright (c) 2020 Airbyte
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
@@ -19,26 +20,26 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
+#
 
 
-from abc import ABC
+import logging
 import urllib.parse as urlparse
-from urllib.parse import parse_qs
+from abc import ABC, abstractmethod
 from typing import Any, Iterable, List, Mapping, MutableMapping, Optional, Tuple
+from urllib.parse import parse_qs
 
-import requests
 import pendulum
-
+import requests
+from airbyte_cdk.models import SyncMode
 from airbyte_cdk.sources import AbstractSource
 from airbyte_cdk.sources.streams import Stream
-from airbyte_cdk.models import SyncMode
 from airbyte_cdk.sources.streams.http import HttpStream
 from airbyte_cdk.sources.streams.http.auth import TokenAuthenticator
-import logging
 
-#logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(level=logging.DEBUG)
 
-# Basic full refresh stream
+
 class TypeformStream(HttpStream, ABC):
     url_base = "https://api.typeform.com"
 
@@ -46,14 +47,29 @@ class TypeformStream(HttpStream, ABC):
 
     date_format: str = "YYYY-MM-DDTHH:mm:ss[Z]"
 
-    def next_page_token(
-        self, response: requests.Response
-    ) -> Optional[Mapping[str, Any]]:
+    def next_page_token(self, response: requests.Response) -> Optional[Mapping[str, Any]]:
+        return None
+
+    def parse_response(self, response: requests.Response, **kwargs) -> Iterable[Mapping]:
+        for item in response.json()["items"]:
+            yield item
+
+
+class TrimForms(TypeformStream):
+    primary_key = "id"
+
+    def path(
+        self,
+        stream_state: Mapping[str, Any] = None,
+        stream_slice: Mapping[str, Any] = None,
+        next_page_token: Mapping[str, Any] = None,
+    ) -> str:
+        return "/forms"
+
+    def next_page_token(self, response: requests.Response) -> Optional[Mapping[str, Any]]:
         page = self.get_current_page_token(response.url)
-        # if page field not found - finish pagination
-        next_page = (
-            None if page is None or response.json()["page_count"] <= page else page + 1
-        )
+        # stop pagination if current page equals to total pages
+        next_page = None if response.json()["page_count"] <= page else page + 1
         return next_page
 
     def get_current_page_token(self, url: str) -> Optional[int]:
@@ -68,22 +84,11 @@ class TypeformStream(HttpStream, ABC):
         next_page_token: Mapping[str, Any] = None,
     ) -> MutableMapping[str, Any]:
         params = {"page_size": self.limit}
-        if next_page_token:
-            params["page"] = next_page_token
-        else:
-            params["page"] = 1
-
+        params["page"] = next_page_token if next_page_token else 1
         return params
-
-    def parse_response(
-        self, response: requests.Response, **kwargs
-    ) -> Iterable[Mapping]:
-        for item in response.json()["items"]:
-            yield item
 
 
 class Forms(TypeformStream):
-    # TODO: Fill in the primary key. Required. This is usually a unique field in the stream, like an ID or a timestamp.
     primary_key = "id"
 
     def path(
@@ -92,10 +97,25 @@ class Forms(TypeformStream):
         stream_slice: Mapping[str, Any] = None,
         next_page_token: Mapping[str, Any] = None,
     ) -> str:
-        return "/forms"
+        return f"/forms/{stream_slice['form_id']}"
+
+    def stream_slices(self, **kwargs) -> Iterable[Optional[Mapping[str, any]]]:
+        forms = TrimForms(authenticator=self.authenticator)
+        for item in forms.read_records(sync_mode=SyncMode.full_refresh):
+            yield {"form_id": item["id"]}
+
+    def request_params(
+        self,
+        stream_state: Mapping[str, Any],
+        stream_slice: Mapping[str, any] = None,
+        next_page_token: Mapping[str, Any] = None,
+    ) -> MutableMapping[str, Any]:
+        return {}
+
+    def parse_response(self, response: requests.Response, **kwargs) -> Iterable[Mapping]:
+        yield response.json()
 
 
-# Basic incremental stream
 class IncrementalTypeformStream(TypeformStream, ABC):
     cursor_field: str = "submitted_at"
 
@@ -107,48 +127,57 @@ class IncrementalTypeformStream(TypeformStream, ABC):
 
     state_checkpoint_interval = limit
 
-    min_cursor: str = "1970-01-01T00:00:00Z"
-
+    @abstractmethod
     def get_updated_state(
         self,
         current_stream_state: MutableMapping[str, Any],
         latest_record: Mapping[str, Any],
     ) -> Mapping[str, Any]:
-        return {
-            self.cursor_field: max(
-                pendulum.from_format(
-                    latest_record.get(self.cursor_field, self.min_cursor),
-                    self.date_format,
-                ),
-                pendulum.from_format(
-                    current_stream_state.get(self.cursor_field, self.min_cursor),
-                    self.date_format,
-                ),
-            )
-        }
+        pass
 
-    def next_page_token(
-        self, response: requests.Response
-    ) -> Optional[Mapping[str, Any]]:
+    def next_page_token(self, response: requests.Response) -> Optional[Mapping[str, Any]]:
         items = response.json()["items"]
-        breakpoint()
         if items and len(items) == self.limit:
             return items[-1][self.token_field]
         return None
 
 
 class Responses(IncrementalTypeformStream):
-
-    # TODO: Fill in the primary key. Required. This is usually a unique field in the stream, like an ID or a timestamp.
     primary_key = "response_id"
 
     def path(self, stream_slice: Optional[Mapping[str, Any]] = None, **kwargs) -> str:
         return f"/forms/{stream_slice['form_id']}/responses"
 
     def stream_slices(self, **kwargs) -> Iterable[Optional[Mapping[str, any]]]:
-        forms = Forms(authenticator=self.authenticator)
+        forms = TrimForms(authenticator=self.authenticator)
         for item in forms.read_records(sync_mode=SyncMode.full_refresh):
             yield {"form_id": item["id"]}
+
+    def get_form_id(self, record: Mapping[str, Any]) -> Optional[str]:
+        """
+        Fetches form id to which current record belongs
+        """
+        referer = record.get("metadata", {}).get("referer")
+        return referer.rsplit("/")[-1] if referer else None
+
+    def get_updated_state(
+        self,
+        current_stream_state: MutableMapping[str, Any],
+        latest_record: Mapping[str, Any],
+    ) -> Mapping[str, Any]:
+        form_id = self.get_form_id(latest_record)
+        if not form_id:
+            return current_stream_state
+
+        last_record_cursor = latest_record.get(self.cursor_field)
+        current_stream_state[form_id] = current_stream_state.get(form_id, {})
+
+        record_state = max(
+            pendulum.from_format(last_record_cursor, self.date_format).int_timestamp if last_record_cursor else 1,
+            current_stream_state[form_id].get(self.cursor_field, 1),
+        )
+        current_stream_state[form_id][self.cursor_field] = record_state
+        return current_stream_state
 
     def request_params(
         self,
@@ -156,21 +185,20 @@ class Responses(IncrementalTypeformStream):
         stream_slice: Mapping[str, any] = None,
         next_page_token: Mapping[str, Any] = None,
     ) -> MutableMapping[str, Any]:
-        params = super().request_params(
-            stream_state=stream_state,
-            stream_slice=stream_slice,
-            next_page_token=next_page_token,
-        )
+        params = {"page_size": self.limit}
         stream_state = stream_state or {}
-        if stream_state and not next_page_token:
+        since = stream_state.get(stream_slice["form_id"], {}).get(self.cursor_field)
+
+        if not next_page_token:
             # use state for first request in incremental sync
             params["sort"] = "submitted_at,asc"
-            params["since"] = stream_state.get(self.cursor_field)
-        elif next_page_token:
+            if since:
+                params["since"] = pendulum.from_timestamp(since).format(self.date_format)
+        else:
             # use response token for pagination after first request
+            # this approach allow to avoid data duplication whithin single sync
             params["after"] = next_page_token
 
-        del params["page"]
         return params
 
 
