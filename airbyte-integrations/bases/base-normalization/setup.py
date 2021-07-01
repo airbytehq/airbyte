cd6c514dc7de5636ba79ad045984b958b867b9aa
Christophe Duong <christophe.duong@gmail.com>
2021-07-01 14:25:12 +0200

    Move to Dockerfile

--- airbyte-integrations/bases/base-normalization/setup.py
+++ airbyte-integrations/bases/base-normalization/setup.py
@@ -36,8 +36,6 @@ setuptools.setup(
         "airbyte-protocol",
         "pyyaml",
         "jinja2",
-        "dbt==0.19.0",
-        "dbt-mysql @ git+https://github.com/dbeatty10/dbt-mysql@96655ea9f7fca7be90c9112ce8ffbb5aac1d3716#egg=dbt-mysql",
     ],
     package_data={"": ["*.yml"]},
     setup_requires=["pytest-runner"],
