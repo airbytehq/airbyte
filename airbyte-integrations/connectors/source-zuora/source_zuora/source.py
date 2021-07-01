7ea8da540d53e17591d98a0e58467ea70a3dad75
Oleksandr Bazarnov <oleksandr.bazarnov@globallogic.com>
2021-07-01 13:12:23 +0300

    updates to source.py

--- airbyte-integrations/connectors/source-zuora/source_zuora/source.py
+++ airbyte-integrations/connectors/source-zuora/source_zuora/source.py
@@ -22,11 +22,12 @@
 
 
 from abc import ABC
-from typing import Any, Iterable, List, Mapping, MutableMapping, Optional, Tuple
+from datetime import datetime
+from typing import Any, Callable, Iterable, List, Mapping, MutableMapping, Optional, Tuple
 
 import json as j
-import time
-from itertools import chain
+# import time
+# from itertools import chain
 from math import ceil
 import requests
 import pendulum
@@ -34,60 +35,22 @@ from airbyte_cdk import AirbyteLogger
 from airbyte_cdk.models import SyncMode
 from airbyte_cdk.sources import AbstractSource
 from airbyte_cdk.sources.streams import Stream
-# from airbyte_cdk.sources.streams.http import HttpStream
-from airbyte_cdk.sources.streams.http.auth import HttpAuthenticator
-
-
-class ZuoraAuthenticator(HttpAuthenticator):
-
-    """
-    Zuora Authenticator, based on generateToken()
-    """
-
-    def __init__(self, client_id: str, client_secret: str, is_sandbox: bool = True):
-        self.client_id = client_id
-        self.client_secret = client_secret
-        self.is_sandbox = is_sandbox
-
-    # Define Endpoint from is_sandbox arg
-    @property
-    def endpoint(self) -> str:
-        if self.is_sandbox == True:
-            return "https://rest.apisandbox.zuora.com"
-        else: 
-            return "https://rest.zuora.com"
-
-    def generateToken(self):
-        endpoint = f"{self.endpoint}/oauth/token"
-        header = {"Content-Type": "application/x-www-form-urlencoded"}
-        data = {"client_id": f"{self.client_id}", 
-                "client_secret" : f"{self.client_secret}",
-                "grant_type": "client_credentials"}
-        try:
-            session = requests.post(endpoint, headers=header, data=data)
-            session.raise_for_status()
-            token = session.json().get("access_token")
-            session.close()
-            self.token = token
-            return {"status": session.status_code, "token": self.token}
-        except requests.exceptions.HTTPError as e:
-            return {"status": e}
-
-    def get_auth_header(self) -> Mapping[str, Any]:
-        return {"Authorization": f"Bearer {self.token}", "Content-Type":"application/json", "X-Zuora-WSDL-Version": "107"}
+from source_zuora.zuora_auth import ZuoraAuthenticator
 
 
 # Basic full refresh stream
 class ZuoraStream(Stream, ABC):
     
-    def __init__(self, auth_header: dict, start_date: str, client_id: str, client_secret: str, is_sandbox: bool, **kwargs):
+    def __init__(self, authenticator: str, auth_header: dict, start_date: str, client_id: str, client_secret: str, is_sandbox: bool, **kwargs):
         self.auth_header = auth_header
         self.start_date = start_date
-        self.client_id = client_id
-        self.client_secret = client_secret
+        # self.client_id = client_id
+        # self.client_secret = client_secret
         self.is_sandbox = is_sandbox
+        self.token = authenticator
+        # self.new_token = refreshed_token
 
-    # Define URL_BASE
+    # Define base url from is_sandbox arg.
     @property
     def url_base(self) -> str:
         if self.is_sandbox == True:
@@ -95,58 +58,177 @@ class ZuoraStream(Stream, ABC):
         else: 
             return "https://rest.zuora.com"
 
+    # Define limit in days for the fetch
+    limit_days = 200
 
+    # Define general primary key
+    primary_key = "id"
+
+    # SUBMIT: data_query_job
+    def _submit_dq_job(self, z_obj: str, start_date: str, end_date: str = None, token_retry_max: int = 2) -> str:
+        self.start_date = start_date
+        self.end_date = pendulum.now().to_datetime_string() if end_date is None else end_date
+        self.token_retry_max = token_retry_max
+
+        endpoint = f"{self.url_base}/query/jobs"
+        header = self.auth_header
+        query = {
+            "compression": "NONE",
+            "output": {"target": "S3"},
+            "outputFormat": "JSON",
+            "query": f"select * from {z_obj} where UpdatedDate >= TIMESTAMP '{self.start_date}' and UpdatedDate <= TIMESTAMP '{self.end_date}'"
+        }
+
+        # make initial submition
+        token_retry = 0
+        while token_retry <= self.token_retry_max:
+            try: 
+                submit_job = requests.post(url=endpoint, headers=header, json=query)
+                submit_job.raise_for_status()
+                submit_job = submit_job.json()["data"]["id"]
+                # print({"submit job_id": submit_job})
+                return submit_job
+            except requests.exceptions.HTTPError as e:
+                # if we got 401 HTTPError: Unauthorised, because of invalid or expired token
+                # we refresh it and replace with the new token
+                token_retry += 1
+                if token_retry <= self.token_retry_max:
+                    print(f"Refreshing Token...")
+                    self.generateToken()
+                    header["Authorization"] = f"Bearer {self.token}"
+                    continue
+                raise Exception(e)
+
+    # CHECK: the submited data_query_job status 
+    def _check_dq_job_status(self, dq_job_id: str, check_interval: int = 1, token_retry_max: int = 2) -> requests.Response:
+        self.dq_job_id = dq_job_id
+        self.token_retry_max = token_retry_max
+
+        endpoint = f"{self.url_base}/query/jobs/{self.dq_job_id}"
+        header = self.auth_header
+
+        status = None
+        token_retry = 0
+        
+        while status != "completed":
+            try:
+                # time.sleep(check_interval)
+                res = requests.get(url=endpoint, headers=header)
+                res.raise_for_status()
+                res = res.json()
+                status = res["data"]["queryStatus"]
+                # print({"status job_id": self.data_query_job_id, "status": status})
+                    
+                if status == "failed":
+                    print({"errorMessage": res["data"]["errorMessage"]})
+                    return iter(())
+                    # Wait interval before next check
+                
+            except requests.exceptions.HTTPError as e:
+                # if we got 401 HTTPError: Unauthorised, because of invalid or expired token
+                # we refresh it and replace with the new token
+                token_retry += 1
+                if token_retry <= self.token_retry_max:
+                    print(f"Refreshing Token...")
+                    # self.token = self.new_token
+                    header["Authorization"] = f"Bearer {self.token}"
+                    continue
+                else:
+                    raise Exception(e)
+        return res
+
+    # GET: data_query_job result
+    def _get_data_from_dq_job(self, response: object) -> List:
+
+        # print({"data job_id": response["data"]["id"]})
+        get_dq_job_data = requests.get(response["data"]["dataFile"])
+        get_dq_job_data.raise_for_status()
+        data = [j.loads(l) for l in get_dq_job_data.text.splitlines()]
+        return data or []
+
+    @staticmethod
+    def _check_end_date(slice_end_date, global_end_date: str) -> str:
+        if pendulum.parse(slice_end_date) > global_end_date:
+            slice_end_date = global_end_date.to_datetime_string()
+        return slice_end_date
+
+    @staticmethod
+    def _get_n_date_slices(start_date: datetime, end_date: datetime, window_days: int):
+        d = pendulum.period(start_date,end_date).in_days()
+        # case when we have the 1 sec more for end_date would produce n = 0, so put 1 instead
+        n = 1 if ceil(d/window_days) <= 0 else ceil(d/window_days)
+        return n
+
+    @staticmethod
+    def _next_slice_end_date(end_date: str, window_days: int):
+        return pendulum.parse(end_date).add(days=window_days).to_datetime_string()
+
+    # Warpper function for `Submit > Check > Get`
+    def _get_data(self, z_obj: str, start_date: str, end_date: str = None) -> Iterable[Mapping]:
+        submit = self._submit_dq_job(z_obj=z_obj, start_date=start_date, end_date=end_date)
+        check = self._check_dq_job_status(submit)
+        get = self._get_data_from_dq_job(check)
+        yield from get
+
+    # Warper for _get_data using date-slices as pages
+    def _get_data_with_date_slice(self, z_obj: str, start_date: str, end_date: str = None, window_days: int = 10) -> Iterable[Mapping]:
+
+        # Parsing input dates
+        s = pendulum.parse(start_date)
+        # If there is no `end_date` as input - use now()
+        e = pendulum.parse(pendulum.now().to_datetime_string()) if end_date is None else pendulum.parse(end_date)
+
+        # check end_date bigger than start_date
+        if s >= e: 
+            return {"Error": "'End Date' should be bigger than 'Start Date'!"}
+        else:
+            # Get n of date-slices
+            n = self._get_n_date_slices(s, e, window_days)
+            # initiating intermediate `start` and `end` for date slice
+            iter_s = s.to_datetime_string()
+            # if iter_e is bigger than the input end_date, switch to the end_date
+            iter_e = self._check_end_date(s.add(days=window_days).to_datetime_string(), e)
+
+            # If we have more than 1 date-slice to fetch
+            if n > 1:
+                # print(f"\nGetting data with {n} Chunks\n")
+                while n > 0:
+                    # print(f"\nDate-Slice: {iter_s} -- {iter_e}\n")
+                    yield from self._get_data(z_obj, iter_s, iter_e)
+
+                    # make next date-slice
+                    iter_s = iter_e
+                    iter_e = self._next_slice_end_date(iter_e, window_days)
+
+                    # if iter_e is bigger than the input end_date, we switch to the end_date
+                    iter_e = self._check_end_date(iter_e, e)
+
+                    # Modify iterator
+                    n -= 1
+            else:
+                # if we have just only 1 date-slice to fetch
+                print(f"\nDate Range Chunk: {iter_s} -- {iter_e}\n")
+                yield from self._get_data(z_obj, iter_s, iter_e)
     
-
-
-
-
-
-
-
-    def next_page_token(self, response: requests.Response) -> Optional[Mapping[str, Any]]:
-        pass
-
-    def request_params(
-        self, stream_state: Mapping[str, Any], stream_slice: Mapping[str, any] = None, next_page_token: Mapping[str, Any] = None
-    ) -> MutableMapping[str, Any]:
-        pass
-
-
-    def parse_response(self, response: requests.Response, **kwargs) -> Iterable[Mapping]:
-        """
-        TODO: Override this method to define how a response is parsed.
-        :return an iterable containing each record in the response
-        """
-        yield {}
+    def read_records(
+        self, 
+        # sync_mode: SyncMode, 
+        # cursor_field: List[str] = None, 
+        # stream_slice: Mapping[str, Any] = None, 
+        # stream_state: Mapping[str, Any] = None,
+        **kwargs
+    ) -> Iterable[Mapping]:
+        yield from self._get_data_with_date_slice(z_obj=self.name, start_date=self.start_date, window_days=self.limit_days)
 
 
 class Account(ZuoraStream):
+    cursor_field = "updated_at"
 
-    obj = "Account"
-    primary_key = "id"
-
-    """ def some_func(self, obj: str):
-        return print(self.submit_data_query_job(zuora_object=obj, start_date=self.start_date)) """
-
-    def read_records(
-        self,
-        sync_mode: SyncMode,
-        cursor_field: List[str] = None,
-        stream_slice: Mapping[str, Any] = None,
-        stream_state: Mapping[str, Any] = None,
-        zuora_object: str = None,
-    ) -> Iterable[Mapping[str, Any]]:
-        """
-        This method should be overridden by subclasses to read records based on the inputs
-        """
-        return print(self.submit_data_query_job(zuora_object=self.obj, start_date=self.start_date))
+class Order(ZuoraStream):
+    cursor_field = "updated_at"
 
 
-    def path(
-        self, stream_state: Mapping[str, Any] = None, stream_slice: Mapping[str, Any] = None, next_page_token: Mapping[str, Any] = None
-    ) -> str:
-        return "account"
+
 
 
 
@@ -256,7 +338,8 @@ class SourceZuora(AbstractSource):
             "start_date": config["start_date"],
             "client_id": config["client_id"],
             "client_secret": config["client_secret"],
-            "is_sandbox": config["is_sandbox"]
+            "is_sandbox": config["is_sandbox"],
+            #  "refreshed_token": inst.generateToken()
         }
         return [
             Account(**args)
