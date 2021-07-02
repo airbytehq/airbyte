8c159a4b2c202e21b2231050adc47eb7c6302613
Sherif Nada <snadalive@gmail.com>
2021-07-02 12:59:18 -0700

    disablecache

--- tools/integrations/manage.sh
+++ tools/integrations/manage.sh
@@ -32,14 +32,14 @@ cmd_build() {
   local run_tests=$1; shift || run_tests=true
 
   echo "Building $path"
-  ./gradlew --no-daemon "$(_to_gradle_path "$path" clean)"
-  ./gradlew --no-daemon "$(_to_gradle_path "$path" build)"
+  ./gradlew --no-daemon -no-build-cache "$(_to_gradle_path "$path" clean)"
+  ./gradlew --no-daemon -no-build-cache "$(_to_gradle_path "$path" build)"
 
   if [ "$run_tests" = false ] ; then
     echo "Skipping integration tests..."
   else
     echo "Running integration tests..."
-    ./gradlew "$(_to_gradle_path "$path" integrationTest)"
+    ./gradlew --no-daemon -no-build-cache "$(_to_gradle_path "$path" integrationTest)"
   fi
 }
 
