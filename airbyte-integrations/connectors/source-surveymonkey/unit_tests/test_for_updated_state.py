5b196cd07a491ca9565655131b827ded86075ef6
Vova <vovchik4@bigmir.net>
2021-07-02 15:23:58 +0300

    pytest mark parametrized use

--- airbyte-integrations/connectors/source-surveymonkey/unit_tests/test_for_updated_state.py
+++ airbyte-integrations/connectors/source-surveymonkey/unit_tests/test_for_updated_state.py
@@ -22,9 +22,8 @@
 # SOFTWARE.
 #
 
-import json
-
 import pendulum
+import pytest
 from airbyte_cdk.sources.streams.http.auth import TokenAuthenticator
 from source_surveymonkey.streams import Surveys
 
@@ -35,29 +34,35 @@ test_parameters = {
 }
 
 
-def test_get_updated_state_unit():
-    config = {"start_date": "2021-01-01T00:00:00", "access_token": "something"}
-    authenticator = TokenAuthenticator(token=config["access_token"])
-    start_date = pendulum.parse(config["start_date"])
-    stream = Surveys(authenticator=authenticator, start_date=start_date)
+class TestFacebookMarketingSource:
+    @pytest.mark.parametrize(
+        ("base_date", "lesser_date", "bigger_date"),
+        [
+            ("2021-06-10T11:02:01", "2021-06-09T11:02:01", "2021-06-10T11:02:02"),
+            ("2021-06-10T11:02:01", "2021-05-10T11:02:01", "2025-06-10T11:02:01"),
+            ("2021-06-10T11:02:01", "2020-06-10T11:02:01", "2021-08-10T11:02:01"),
+        ],
+    )
+    def test_get_updated_state_unit(self, base_date, lesser_date, bigger_date):
+        config = {"start_date": "2021-01-01T00:00:00", "access_token": "something"}
+        authenticator = TokenAuthenticator(token=config["access_token"])
+        start_date = pendulum.parse(config["start_date"])
+        stream = Surveys(authenticator=authenticator, start_date=start_date)
 
-    record = {"title": "Market Research - Product Testing Template", "date_modified": "2021-06-08T18:09:00", "id": "306079584"}
+        current_state = {"date_modified": base_date}
 
-    current_state = {"date_modified": test_parameters["base_date"]}
-
-    # test lesser current state
-    expected_state = current_state
-    for lesser_date in test_parameters["lesser_dates"]:
+        # test lesser current state
+        expected_state = current_state
         record = {"title": "test", "id": 100500, "date_modified": lesser_date}
         assert stream.get_updated_state(current_state, record) == expected_state
 
-    # test bigger current state
-    for bigger_date in test_parameters["bigger_dates"]:
+        # test bigger current state
+
         record = {"title": "test", "id": 100500, "date_modified": bigger_date}
         expected_state = {stream.cursor_field: bigger_date}
         assert stream.get_updated_state(current_state, record) == expected_state
 
-    # test zero current state
-    record_with_some_date = {"title": "test", "date_modified": "2000-06-15T18:09:00", "id": 1}
-    expected_state = {stream.cursor_field: record_with_some_date[stream.cursor_field]}
-    assert stream.get_updated_state({}, record_with_some_date) == expected_state
+        # test zero current state
+        record_with_some_date = {"title": "test", "date_modified": "2000-06-15T18:09:00", "id": 1}
+        expected_state = {stream.cursor_field: record_with_some_date[stream.cursor_field]}
+        assert stream.get_updated_state({}, record_with_some_date) == expected_state
