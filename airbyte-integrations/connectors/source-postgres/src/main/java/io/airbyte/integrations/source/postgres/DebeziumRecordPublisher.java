03ac830746e77d54f378b7ea520ab71f7b836889
Rodrigo Menezes <r@seedfi.com>
2021-07-06 10:53:52 -0400

    Fix comment so it points to psql docs instead

--- airbyte-integrations/connectors/source-postgres/src/main/java/io/airbyte/integrations/source/postgres/DebeziumRecordPublisher.java
+++ airbyte-integrations/connectors/source-postgres/src/main/java/io/airbyte/integrations/source/postgres/DebeziumRecordPublisher.java
@@ -159,7 +159,7 @@ public class DebeziumRecordPublisher implements AutoCloseable {
     // By default "decimal.handing.mode=precise" which's caused returning this value as a binary.
     // The "double" type may cause a loss of precision, so set Debezium's config to store it as a String
     // explicitly in its Kafka messages for more details see:
-    // https://debezium.io/documentation/reference/connectors/mysql.html#mysql-decimal-types
+    // https://debezium.io/documentation/reference/1.4/connectors/postgresql.html#postgresql-decimal-types
     // https://debezium.io/documentation/faq/#how_to_retrieve_decimal_field_from_binary_representation
     props.setProperty("decimal.handling.mode", "string");
 
