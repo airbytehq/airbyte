dffbf1bb7c5f18596d20ae15b7c22a68e9a092c7
Davin Chia <davinchia@gmail.com>
2021-07-07 18:56:53 +0800

    Add test and fix bad logic.

--- airbyte-config/models/src/main/java/io/airbyte/config/helpers/CloudLogs.java
+++ airbyte-config/models/src/main/java/io/airbyte/config/helpers/CloudLogs.java
@@ -27,6 +27,8 @@ package io.airbyte.config.helpers;
 import java.io.File;
 import java.io.IOException;
 import java.util.List;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Interface for various Cloud Storage clients supporting Cloud log retrieval.
@@ -36,6 +38,8 @@ import java.util.List;
  * was written from it's name.
  */
 public interface CloudLogs {
+
+  Logger LOGGER = LoggerFactory.getLogger(CloudLogs.class);
 
   /**
    * Retrieve all objects at the given path in lexicographical order, and return their contents as one
@@ -59,10 +63,12 @@ public interface CloudLogs {
   static CloudLogs createCloudLogClient(LogConfigs configs) {
     // check if the configs exists, and pick a client.
     if (hasS3Configuration(configs)) {
+      LOGGER.info("Creating AWS Log Client");
       return new S3Logs();
     }
 
     if (hasGcpConfiguration(configs)) {
+      LOGGER.info("Creating GCS Log Client");
       return new GcsLogs();
     }
 
@@ -70,15 +76,15 @@ public interface CloudLogs {
   }
 
   private static boolean hasS3Configuration(LogConfigs configs) {
-    return configs.getAwsAccessKey().isBlank() ||
-        configs.getAwsSecretAccessKey().isBlank() ||
-        configs.getS3LogBucketRegion().isBlank() ||
-        configs.getS3LogBucket().isBlank();
+    return !configs.getAwsAccessKey().isBlank() &&
+        !configs.getAwsSecretAccessKey().isBlank() &&
+        !configs.getS3LogBucketRegion().isBlank() &&
+        !configs.getS3LogBucket().isBlank();
   }
 
   private static boolean hasGcpConfiguration(LogConfigs configs) {
-    return configs.getGcpStorageBucket().isBlank() ||
-        configs.getGoogleApplicationCredentials().isBlank();
+    return !configs.getGcpStorageBucket().isBlank() &&
+        !configs.getGoogleApplicationCredentials().isBlank();
   }
 
 }
