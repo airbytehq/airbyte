ae5f05968020a9ec4c0e2af216e249c4b3af6cc7
Andrii Leonets <aleonets@gmail.com>
2021-07-07 16:50:46 +0300

    fix table info retrieving

--- airbyte-db/src/main/java/io/airbyte/db/bigquery/BigQueryDatabase.java
+++ airbyte-db/src/main/java/io/airbyte/db/bigquery/BigQueryDatabase.java
@@ -143,7 +143,8 @@ public class BigQueryDatabase extends SqlDatabase {
         .iterateAll()
         .forEach(dataset -> bigQuery.listTables(dataset.getDatasetId())
             .iterateAll()
-            .forEach(tableList::add));
+            .forEach(table -> tableList.add(bigQuery.getTable(table.getTableId())))
+        );
     return tableList;
   }
 
