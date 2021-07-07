0b7581bb8104ad95b1d91cf7e268cd8edef65730
subodh <subodh1810@gmail.com>
2021-07-07 23:57:53 +0530

    rename class + add missing property

--- airbyte-integrations/connectors/source-debezium/src/main/java/io/airbyte/integrations/source/debezium/DebeziumRecordPublisher.java
+++ airbyte-integrations/connectors/source-debezium/src/main/java/io/airbyte/integrations/source/debezium/DebeziumRecordPublisher.java
@@ -180,6 +180,13 @@ public class DebeziumRecordPublisher implements AutoCloseable {
       props.setProperty("database.password", config.get("password").asText());
     }
 
+    // By default "decimal.handing.mode=precise" which's caused returning this value as a binary.
+    // The "double" type may cause a loss of precision, so set Debezium's config to store it as a String
+    // explicitly in its Kafka messages for more details see:
+    // https://debezium.io/documentation/reference/1.4/connectors/postgresql.html#postgresql-decimal-types
+    // https://debezium.io/documentation/faq/#how_to_retrieve_decimal_field_from_binary_representation
+    props.setProperty("decimal.handling.mode", "string");
+
     // table selection
     final String tableWhitelist = getTableWhitelist(catalog);
     props.setProperty("table.include.list", tableWhitelist);
