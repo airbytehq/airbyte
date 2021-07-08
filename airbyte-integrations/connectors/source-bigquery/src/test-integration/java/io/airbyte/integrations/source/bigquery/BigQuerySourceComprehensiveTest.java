094fa827131d0c9ad9fe0250e11921fb6a496cbc
Andrii Leonets <aleonets@gmail.com>
2021-07-08 16:10:12 +0300

    Add working comprehensive test without data cases

--- airbyte-integrations/connectors/source-bigquery/src/test-integration/java/io/airbyte/integrations/source/bigquery/BigQuerySourceComprehensiveTest.java
+++ airbyte-integrations/connectors/source-bigquery/src/test-integration/java/io/airbyte/integrations/source/bigquery/BigQuerySourceComprehensiveTest.java
@@ -1,5 +1,10 @@
 package io.airbyte.integrations.source.bigquery;
 
+import static io.airbyte.integrations.source.bigquery.BigQuerySource.CONFIG_CREDS;
+import static io.airbyte.integrations.source.bigquery.BigQuerySource.CONFIG_DATASET_ID;
+import static io.airbyte.integrations.source.bigquery.BigQuerySource.CONFIG_DATASET_LOCATION;
+import static io.airbyte.integrations.source.bigquery.BigQuerySource.CONFIG_PROJECT_ID;
+
 import com.fasterxml.jackson.databind.JsonNode;
 import com.google.cloud.bigquery.Dataset;
 import com.google.cloud.bigquery.DatasetInfo;
@@ -12,19 +17,17 @@ import io.airbyte.integrations.standardtest.source.SourceComprehensiveTest;
 import io.airbyte.integrations.standardtest.source.TestDestinationEnv;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
 
+@TestInstance(Lifecycle.PER_CLASS)
 public class BigQuerySourceComprehensiveTest extends SourceComprehensiveTest {
 
   private static final Path CREDENTIALS_PATH = Path.of("secrets/credentials.json");
 
-  private static final String CONFIG_DATASET_ID = "dataset_id";
-  private static final String CONFIG_PROJECT_ID = "project_id";
-  private static final String CONFIG_DATASET_LOCATION = "dataset_location";
-  private static final String CONFIG_CREDS = "credentials_json";
-
   private TempBigQueryJoolDatabaseImpl database;
   private Dataset dataset;
-  private boolean tornDown;
   private JsonNode config;
 
   @Override
@@ -56,7 +59,7 @@ public class BigQuerySourceComprehensiveTest extends SourceComprehensiveTest {
     final String projectId = credentialsJson.get(CONFIG_PROJECT_ID).asText();
     final String datasetLocation = "US";
 
-    final String datasetId = Strings.addRandomSuffix("airbyte_tests", "_", 8);
+    final String datasetId = Strings.addRandomSuffix("airbyte_tests_compr", "_", 8);
 
     config = Jsons.jsonNode(ImmutableMap.builder()
         .put(CONFIG_PROJECT_ID, projectId)
@@ -70,6 +73,7 @@ public class BigQuerySourceComprehensiveTest extends SourceComprehensiveTest {
     final DatasetInfo datasetInfo =
         DatasetInfo.newBuilder(config.get(CONFIG_DATASET_ID).asText()).setLocation(config.get(CONFIG_DATASET_LOCATION).asText()).build();
     dataset = database.getRealDatabase().getBigQuery().create(datasetInfo);
+
     return database;
   }
 
@@ -80,6 +84,11 @@ public class BigQuerySourceComprehensiveTest extends SourceComprehensiveTest {
 
   @Override
   protected String getNameSpace() {
-    return dataset.getFriendlyName();
+    return dataset.getDatasetId().getDataset();
+  }
+
+  @AfterAll
+  public void cleanTestInstance() {
+    database.getRealDatabase().cleanDataSet(getNameSpace());
   }
 }
