ba47b8bee9890b1dfdc8e34fbf256f53fe096a0a
Abhi Vaidyanatha <abhivaidyanatha@Abhis-MacBook-Pro.local>
2021-07-08 16:29:19 -0700

    Airflow demo: Script cleanup

--- resources/examples/airflow/down.sh
+++ resources/examples/airflow/down.sh
@@ -2,5 +2,5 @@
 cd ../../..
 docker-compose down -v
 cd resources/examples/airflow || exit
-docker-compose -f docker-compose-superset.yaml down -v
-docker-compose -f docker-compose.yaml down -v
+docker-compose -f docker-compose-superset.yaml down -v --remove-orphans
+docker-compose -f docker-compose.yaml down -v --remove-orphans
