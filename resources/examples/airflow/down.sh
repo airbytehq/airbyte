8ffbc85d66b44c28e97547464b03bed543caa451
Abhi Vaidyanatha <abhivaidyanatha@Abhis-MacBook-Pro.local>
2021-07-08 16:51:44 -0700

    Clean up airbyte destination

--- resources/examples/airflow/down.sh
+++ resources/examples/airflow/down.sh
@@ -4,3 +4,4 @@ docker-compose down -v
 cd resources/examples/airflow || exit
 docker-compose -f docker-compose-airflow.yaml down -v
 docker-compose -f superset/docker-compose-superset.yaml down -v
+docker stop airbyte-destination
