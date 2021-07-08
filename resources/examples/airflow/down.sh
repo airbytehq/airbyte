eb7ebf64025c8aabb88ee9e9bf7c74324821e9f7
Abhi Vaidyanatha <abhivaidyanatha@Abhis-MacBook-Pro.local>
2021-07-08 16:30:25 -0700

    Correct docker compose name for airflow file

--- resources/examples/airflow/down.sh
+++ resources/examples/airflow/down.sh
@@ -3,4 +3,4 @@ cd ../../..
 docker-compose down -v
 cd resources/examples/airflow || exit
 docker-compose -f docker-compose-superset.yaml down -v --remove-orphans
-docker-compose -f docker-compose.yaml down -v --remove-orphans
+docker-compose -f docker-compose-airflow.yaml down -v --remove-orphans
