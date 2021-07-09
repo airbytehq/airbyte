b94f2181bfcb4277bac006d9fef068bda5d071c0
Jenny Brown <jenny@airbyte.io>
2021-07-09 14:11:24 -0500

    Whitespace and formatting per standard tooling.

--- airbyte-scheduler/persistence/src/main/resources/job_history_purge.sql
+++ airbyte-scheduler/persistence/src/main/resources/job_history_purge.sql
@@ -1,33 +1,100 @@
-delete from jobs where jobs.id in (
-    select jobs.id  
-    from jobs 
-    left join (
-           select scope, count(jobs.id) as jobCount from jobs group by scope
-    ) counts on jobs.scope = counts.scope 
-    where 
-          -- job must be at least MINIMUM_AGE_IN_DAYS old or connection has more than EXCESSIVE_NUMBER_OF_JOBS 
-      (jobs.created_at < (TO_TIMESTAMP(?, 'YYYY-MM-DD') - interval '%d' day) or counts.jobCount >  ?) 
-    and jobs.id not in (
-          -- cannot be the most recent job with saved state 
-          select job_id as latest_job_id_with_state from (
-              select jobs.scope, jobs.id as job_id, jobs.config_type, jobs.created_at, jobs.status, 
-                bool_or(attempts."output" -> 'sync' -> 'state' -> 'state' is not null) as outputStateExists,
-                row_number() OVER (PARTITION BY scope ORDER BY jobs.created_at desc, jobs.id desc) as stateRecency
-              from jobs 
-              left join attempts on jobs.id = attempts.job_id 
-              group by scope, jobs.id
-              having bool_or(attempts."output" -> 'sync' -> 'state' -> 'state' is not null) = true
-              order by scope, jobs.created_at desc, jobs.id desc
-           ) jobs_with_state where stateRecency=1
-    ) and jobs.id not in (
-          -- cannot be one of the last MINIMUM_RECENCY jobs for that connection/scope
-          select id from (
-                select jobs.scope, jobs.id, jobs.created_at,
-                row_number() OVER (PARTITION BY scope ORDER BY jobs.created_at desc, jobs.id desc) as recency
-                from jobs
-                group by scope, jobs.id
-                order by scope, jobs.created_at desc, jobs.id desc
-          ) jobs_by_recency 
-          where recency <= ?
+DELETE
+FROM
+    jobs
+WHERE
+    jobs.id IN(
+        SELECT
+            jobs.id
+        FROM
+            jobs
+        LEFT JOIN(
+                SELECT
+                    SCOPE,
+                    COUNT( jobs.id ) AS jobCount
+                FROM
+                    jobs
+                GROUP BY
+                    SCOPE
+            ) counts ON
+            jobs.scope = counts.scope
+        WHERE
+            -- job must be at least MINIMUM_AGE_IN_DAYS old or connection has more than EXCESSIVE_NUMBER_OF_JOBS 
+(
+                jobs.created_at <(
+                    TO_TIMESTAMP(
+                        ?,
+                        'YYYY-MM-DD'
+                    )- INTERVAL '%d' DAY
+                )
+                OR counts.jobCount >?
+            )
+            AND jobs.id NOT IN(
+                -- cannot be the most recent job with saved state 
+ SELECT
+                    job_id AS latest_job_id_with_state
+                FROM
+                    (
+                        SELECT
+                            jobs.scope,
+                            jobs.id AS job_id,
+                            jobs.config_type,
+                            jobs.created_at,
+                            jobs.status,
+                            bool_or(
+                                attempts."output" -> 'sync' -> 'state' -> 'state' IS NOT NULL
+                            ) AS outputStateExists,
+                            ROW_NUMBER() OVER(
+                                PARTITION BY SCOPE
+                            ORDER BY
+                                jobs.created_at DESC,
+                                jobs.id DESC
+                            ) AS stateRecency
+                        FROM
+                            jobs
+                        LEFT JOIN attempts ON
+                            jobs.id = attempts.job_id
+                        GROUP BY
+                            SCOPE,
+                            jobs.id
+                        HAVING
+                            bool_or(
+                                attempts."output" -> 'sync' -> 'state' -> 'state' IS NOT NULL
+                            )= TRUE
+                        ORDER BY
+                            SCOPE,
+                            jobs.created_at DESC,
+                            jobs.id DESC
+                    ) jobs_with_state
+                WHERE
+                    stateRecency = 1
+            )
+            AND jobs.id NOT IN(
+                -- cannot be one of the last MINIMUM_RECENCY jobs for that connection/scope
+ SELECT
+                    id
+                FROM
+                    (
+                        SELECT
+                            jobs.scope,
+                            jobs.id,
+                            jobs.created_at,
+                            ROW_NUMBER() OVER(
+                                PARTITION BY SCOPE
+                            ORDER BY
+                                jobs.created_at DESC,
+                                jobs.id DESC
+                            ) AS recency
+                        FROM
+                            jobs
+                        GROUP BY
+                            SCOPE,
+                            jobs.id
+                        ORDER BY
+                            SCOPE,
+                            jobs.created_at DESC,
+                            jobs.id DESC
+                    ) jobs_by_recency
+                WHERE
+                    recency <=?
+            )
     )
-)
