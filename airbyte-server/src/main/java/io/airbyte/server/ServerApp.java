9e1575a6778e710d5fbf5c2a817c946d4db51d2b
Jared Rhizor <jared@dataline.io>
2021-07-12 11:22:05 -0700

    allow injecting filters for server (#4677)
    * allow injecting filters
    
    * fmt

--- airbyte-server/src/main/java/io/airbyte/server/ServerApp.java
+++ airbyte-server/src/main/java/io/airbyte/server/ServerApp.java
@@ -60,10 +60,14 @@ import io.airbyte.workers.temporal.TemporalUtils;
 import io.temporal.serviceclient.WorkflowServiceStubs;
 import java.io.IOException;
 import java.nio.file.Path;
+import java.util.Collections;
 import java.util.Map;
 import java.util.Optional;
+import java.util.Set;
 import java.util.UUID;
 import java.util.concurrent.TimeUnit;
+import javax.ws.rs.container.ContainerRequestFilter;
+import javax.ws.rs.container.ContainerResponseFilter;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
@@ -89,13 +93,19 @@ public class ServerApp {
   private final ConfigRepository configRepository;
   private final JobPersistence jobPersistence;
   private final Configs configs;
+  private final Set<ContainerRequestFilter> requestFilters;
+  private final Set<ContainerResponseFilter> responseFilters;
 
   public ServerApp(final ConfigRepository configRepository,
                    final JobPersistence jobPersistence,
-                   final Configs configs) {
+                   final Configs configs,
+                   final Set<ContainerRequestFilter> requestFilters,
+                   final Set<ContainerResponseFilter> responseFilters) {
     this.configRepository = configRepository;
     this.jobPersistence = jobPersistence;
     this.configs = configs;
+    this.requestFilters = requestFilters;
+    this.responseFilters = responseFilters;
   }
 
   public void start() throws Exception {
@@ -123,9 +133,9 @@ public class ServerApp {
 
     ResourceConfig rc =
         new ResourceConfig()
-            // todo (cgardens) - the CORs settings are wide open. will need to revisit when we add auth.
-            // cors
-            .register(new CorsFilter())
+            // add filters
+            .registerInstances(requestFilters)
+            .registerInstances(responseFilters)
             // request logging
             .register(new RequestLogger(mdc))
             // api
@@ -183,7 +193,9 @@ public class ServerApp {
     }
   }
 
-  public static void main(String[] args) throws Exception {
+  public static void runServer(final Set<ContainerRequestFilter> requestFilters,
+                               final Set<ContainerResponseFilter> responseFilters)
+      throws Exception {
     final Configs configs = new EnvConfigs();
 
     MDC.put(LogClientSingleton.WORKSPACE_MDC_KEY, LogClientSingleton.getServerLogsRoot(configs).toString());
@@ -233,11 +245,15 @@ public class ServerApp {
 
     if (airbyteDatabaseVersion.isPresent() && AirbyteVersion.isCompatible(airbyteVersion, airbyteDatabaseVersion.get())) {
       LOGGER.info("Starting server...");
-      new ServerApp(configRepository, jobPersistence, configs).start();
+      new ServerApp(configRepository, jobPersistence, configs, requestFilters, responseFilters).start();
     } else {
       LOGGER.info("Start serving version mismatch errors. Automatic migration either failed or didn't run");
       new VersionMismatchServer(airbyteVersion, airbyteDatabaseVersion.get(), PORT).start();
     }
+  }
+
+  public static void main(String[] args) throws Exception {
+    runServer(Collections.emptySet(), Set.of(new CorsFilter()));
   }
 
   /**
