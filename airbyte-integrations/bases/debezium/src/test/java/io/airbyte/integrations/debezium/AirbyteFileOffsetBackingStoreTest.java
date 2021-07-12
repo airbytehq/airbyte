4863ea1a93e10aac6067e2b6ec30cec44f3a1168
Subodh Kant Chaturvedi <subodh1810@gmail.com>
2021-07-12 22:39:38 +0530

    introduce common abstraction for CDC via debezium (#4580)
    * wip
    
    * add file
    
    * final structure
    
    * few more updates
    
    * undo unwanted changes
    
    * add abstract test + more refinement
    
    * remove CDC metadata to debezium
    
    * rename class + add missing property
    
    * move debezium to bases + upgrade debezium version + review comments
    
    * downgrade version + minor fixes
    
    * reset to minutes
    
    * fix build
    
    * address review comments
    
    * should return Optional
    
    * use common abstraction for CDC via debezium for mysql (#4604)
    
    * use new cdc abstraction for mysql
    
    * undo wanted change
    
    * pull in latest changes
    
    * use renamed class + move constants to MySqlSource
    
    * bring in latest changes from cdc abstraction
    
    * format
    
    * bring in latest changes
    
    * pull in latest changes
    
    * use common abstraction for CDC via debezium for postgres (#4607)
    
    * use cdc abstraction for postgres
    
    * add files
    
    * ready
    
    * use renamed class + move constants to PostgresSource
    
    * bring in the latest changes
    
    * bring in latest changes
    
    * pull in latest changes

--- airbyte-integrations/bases/debezium/src/test/java/io/airbyte/integrations/debezium/AirbyteFileOffsetBackingStoreTest.java
+++ airbyte-integrations/bases/debezium/src/test/java/io/airbyte/integrations/debezium/AirbyteFileOffsetBackingStoreTest.java
@@ -22,17 +22,20 @@
  * SOFTWARE.
  */
 
-package io.airbyte.integrations.source.postgres;
+package io.airbyte.integrations.debezium;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import io.airbyte.commons.io.IOs;
+import io.airbyte.commons.json.Jsons;
 import io.airbyte.commons.resources.MoreResources;
-import io.airbyte.integrations.source.relationaldb.models.CdcState;
+import io.airbyte.integrations.debezium.internals.AirbyteFileOffsetBackingStore;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Map;
 import org.junit.jupiter.api.Test;
 
 class AirbyteFileOffsetBackingStoreTest {
@@ -49,15 +52,17 @@ class AirbyteFileOffsetBackingStoreTest {
     final Path writeFilePath = testRoot.resolve("offset.dat");
 
     final AirbyteFileOffsetBackingStore offsetStore = new AirbyteFileOffsetBackingStore(templateFilePath);
-    final CdcState stateFromTemplateFile = offsetStore.read();
+    Map<String, String> offset = offsetStore.read();
+
+    final JsonNode asJson = Jsons.jsonNode(offset);
 
     final AirbyteFileOffsetBackingStore offsetStore2 = new AirbyteFileOffsetBackingStore(writeFilePath);
-    offsetStore2.persist(stateFromTemplateFile);
+    offsetStore2.persist(asJson);
 
-    final CdcState stateFromOffsetStoreRoundTrip = offsetStore2.read();
+    final Map<String, String> stateFromOffsetStoreRoundTrip = offsetStore2.read();
 
     // verify that, after a round trip through the offset store, we get back the same data.
-    assertEquals(stateFromTemplateFile, stateFromOffsetStoreRoundTrip);
+    assertEquals(offset, stateFromOffsetStoreRoundTrip);
     // verify that the file written by the offset store is identical to the template file.
     assertTrue(com.google.common.io.Files.equal(templateFilePath.toFile(), writeFilePath.toFile()));
   }
