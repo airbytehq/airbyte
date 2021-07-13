7737ae3ac95a88b5822597a4606fc0a3d68ac1db
Sherif A. Nada <snadalive@gmail.com>
2021-07-13 16:18:08 -0700

    CDK: Add initial Destination abstraction and tests (#4719)
    Co-authored-by: Eugene Kulak <widowmakerreborn@gmail.com>

--- airbyte-cdk/python/setup.py
+++ airbyte-cdk/python/setup.py
@@ -35,7 +35,7 @@ README = (HERE / "README.md").read_text()
 
 setup(
     name="airbyte-cdk",
-    version="0.1.5",
+    version="0.1.6-rc1",
     description="A framework for writing Airbyte Connectors.",
     long_description=README,
     long_description_content_type="text/markdown",
