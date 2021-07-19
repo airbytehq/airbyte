8ebf75db63f201460ec79ab3921cc392747124fc
Sherif Nada <snadalive@gmail.com>
2021-07-19 14:50:59 -0700

    save

--- tools/status/init.sh
+++ tools/status/init.sh
@@ -5,14 +5,14 @@ set -e
 # This script should only be used to set up the status site for the first time or to make your own version for testing.
 # Prod refers to the prod environment used for prior Airbyte projects.
 
-BUCKET=airbyte-status
-PROFILE=prod
+BUCKET=airbyte-connector-build-status
+PROFILE=dev
 REGION=us-east-2
 S3_DOMAIN="$BUCKET.s3-website.$REGION.amazonaws.com"
 
 export AWS_PAGER=""
 
-echo "This has already been created. Comment out this line if you really want to run this again." && exit 1
+#echo "This has already been created. Comment out this line if you really want to run this again." && exit 1
 
 echo "Creating bucket..."
 aws s3api create-bucket --bucket "$BUCKET" --region "$REGION"  --create-bucket-configuration LocationConstraint="$REGION" --profile "$PROFILE"
