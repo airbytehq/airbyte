ede7822366b3fbebebd5f91bfa8b09a1dea2a1b4
Liren Tu <tuliren.git@outlook.com>
2021-07-19 21:29:11 -0700

    Prune imports

--- airbyte-config/persistence/src/main/java/io/airbyte/config/persistence/ConfigPersistenceBuilder.java
+++ airbyte-config/persistence/src/main/java/io/airbyte/config/persistence/ConfigPersistenceBuilder.java
@@ -31,8 +31,6 @@ import io.airbyte.config.Configs;
 import io.airbyte.db.Database;
 import io.airbyte.db.Databases;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
