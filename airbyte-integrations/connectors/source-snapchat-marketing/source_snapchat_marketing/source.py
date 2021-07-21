0ddabdeeacbf5ddb82f28c17870803b9e5bdbd8e
TymoshokDmytro <tymoshok.dmytro@gmail.com>
2021-07-21 17:00:28 +0300

    Fix schemas and do some refactoring

--- airbyte-integrations/connectors/source-snapchat-marketing/source_snapchat_marketing/source.py
+++ airbyte-integrations/connectors/source-snapchat-marketing/source_snapchat_marketing/source.py
@@ -33,7 +33,7 @@ from airbyte_cdk.models import SyncMode
 from airbyte_cdk.sources import AbstractSource
 from airbyte_cdk.sources.streams import Stream
 from airbyte_cdk.sources.streams.http import HttpStream
-from airbyte_cdk.sources.streams.http.auth import TokenAuthenticator, Oauth2Authenticator
+from airbyte_cdk.sources.streams.http.auth import Oauth2Authenticator
 
 """
 TODO: Most comments in this class are instructive and should be deleted after the source is implemented.
@@ -58,7 +58,7 @@ class SnapchatMarketingException(Exception):
 class SnapchatMarketingStream(HttpStream, ABC):
     data_field = None
     url_base = "https://adsapi.snapchat.com/v1/"
-    primary_key = 'id'
+    primary_key = "id"
     items_per_page_limit = 1000
 
     def __init__(self, start_date, **kwargs):
@@ -68,11 +68,10 @@ class SnapchatMarketingStream(HttpStream, ABC):
     def next_page_token(self, response: requests.Response) -> Optional[Mapping[str, Any]]:
         next_page_cursor = response.json().get("paging", False)
         if next_page_cursor:
-            return {"cursor": dict(parse_qsl(urlparse(next_page_cursor['next_link']).query))['cursor']}
+            return {"cursor": dict(parse_qsl(urlparse(next_page_cursor["next_link"]).query))["cursor"]}
 
     def request_params(
-            self, stream_state: Mapping[str, Any], stream_slice: Mapping[str, any] = None,
-            next_page_token: Mapping[str, Any] = None
+        self, stream_state: Mapping[str, Any], stream_slice: Mapping[str, any] = None, next_page_token: Mapping[str, Any] = None
     ) -> MutableMapping[str, Any]:
         params_payload = {}
         if next_page_token:
@@ -89,34 +88,44 @@ class SnapchatMarketingStream(HttpStream, ABC):
             for resp in json_response:
                 yield resp.get(self.data_field[1])
         else:
-            error_text = f'Cannot parse response. Data field unrecognized: {self.data_field}'
+            error_text = f"Cannot parse response. Data field unrecognized: {self.data_field}"
             self.logger.error(error_text)
             raise SnapchatMarketingException(error_text)
 
 
-class IncrementalSnapchatMarketingtream(SnapchatMarketingStream, ABC):
+class IncrementalSnapchatMarketingStream(SnapchatMarketingStream, ABC):
     state_checkpoint_interval = SnapchatMarketingStream.items_per_page_limit
+    cursor_field = "updated_at"
 
-    def get_updated_state(self, current_stream_state: MutableMapping[str, Any], latest_record: Mapping[str, Any]) -> \
-            Mapping[str, Any]:
+    def get_updated_state(self, current_stream_state: MutableMapping[str, Any], latest_record: Mapping[str, Any]) -> Mapping[str, Any]:
         if current_stream_state is not None and self.cursor_field in current_stream_state:
             return {self.cursor_field: max(current_stream_state[self.cursor_field], latest_record[self.cursor_field])}
         else:
             return {self.cursor_field: self.start_date}
 
+    def read_records(self, stream_state: Mapping[str, Any] = None, **kwargs) -> Iterable[Mapping[str, Any]]:
+        records = super().read_records(**kwargs)
+        if stream_state:
+            for record in records:
+                if record[self.cursor_field] >= stream_state.get(self.cursor_field):
+                    yield record
+        else:
+            yield from records
 
-class Organizations(IncrementalSnapchatMarketingtream):
+
+class Organizations(SnapchatMarketingStream):
     """ Docs: https://marketingapi.snapchat.com/docs/#organizations """
-    data_field = ['organizations', 'organization']
+
+    data_field = ["organizations", "organization"]
 
     def path(self, **kwargs) -> str:
         return "me/organizations"
 
 
-class AdAccounts(IncrementalSnapchatMarketingtream):
+class AdAccounts(IncrementalSnapchatMarketingStream):
     """ Docs: https://marketingapi.snapchat.com/docs/#get-all-ad-accounts """
-    data_field = ['adaccounts', 'adaccount']
-    cursor_field = 'updated_at'
+
+    data_field = ["adaccounts", "adaccount"]
 
     def stream_slices(self, **kwargs) -> Iterable[Optional[Mapping[str, Any]]]:
         organizations_stream = Organizations(authenticator=self.authenticator, start_date=self.start_date)
@@ -134,16 +143,22 @@ class AdAccounts(IncrementalSnapchatMarketingtream):
             yield {"organization_id": organization_id}
 
     def path(self, stream_slice: Mapping[str, Any] = None, **kwargs) -> str:
-        return "organizations/{}/adaccounts".format(stream_slice['organization_id'])
+        return "organizations/{}/adaccounts".format(stream_slice["organization_id"])
 
 
-class Creatives(IncrementalSnapchatMarketingtream):
+class Creatives(IncrementalSnapchatMarketingStream):
     """ Docs: https://marketingapi.snapchat.com/docs/#get-all-creatives """
 
-    data_field = ['creatives', 'creative']
-    cursor_field = 'updated_at'
+    data_field = ["creatives", "creative"]
 
     def stream_slices(self, **kwargs) -> Iterable[Optional[Mapping[str, Any]]]:
+        # The trick with this code is that all 3 streams are chained:
+        # Organizations -> AdAccounts -> Creatives.
+        # AdAccounts need the organization_id as path variable
+        # Creatives need the ad_account_id as path variable
+        # So first we must get the AdAccounts, then do the slicing for them
+        # and then call the read_records for each slice
+
         ad_accounts_stream = AdAccounts(authenticator=self.authenticator, start_date=self.start_date)
         ad_accounts_slices = ad_accounts_stream.stream_slices(**kwargs)
         ad_account_ids = []
@@ -162,7 +177,7 @@ class Creatives(IncrementalSnapchatMarketingtream):
             yield {"ad_account_id": ad_account_id}
 
     def path(self, stream_slice: Mapping[str, Any] = None, **kwargs) -> str:
-        return "adaccounts/{}/creatives".format(stream_slice['ad_account_id'])
+        return "adaccounts/{}/creatives".format(stream_slice["ad_account_id"])
 
 
 class SnapchatAdsOauth2Authenticator(Oauth2Authenticator):
@@ -176,34 +191,34 @@ class SnapchatAdsOauth2Authenticator(Oauth2Authenticator):
 
     def __init__(self, config):
         super().__init__(
-            token_refresh_endpoint='https://accounts.snapchat.com/login/oauth2/access_token',
+            token_refresh_endpoint="https://accounts.snapchat.com/login/oauth2/access_token",
             client_id=config["client_id"],
-            client_secret=config["client_secret"],
+            client_secret=config["secret"],
             refresh_token=config["refresh_token"],
         )
 
-    def refresh_access_token(self) -> Tuple[str, int]:
-        """
-        returns a tuple of (access_token, token_lifespan_in_seconds)
-        """
-        response_json = None
-        try:
-            response = requests.request(method="POST", url=self.token_refresh_endpoint,
-                                        data=self.get_refresh_request_body())
-            response_json = response.json()
-            response.raise_for_status()
-            return response_json["access_token"], response_json["expires_in"]
-        except requests.exceptions.RequestException as e:
-            if response_json and 'error' in response_json:
-                raise Exception("Error refreshing access token. Error: {}; Error details: {}; Exception: {}".format(
-                    response_json['error'], response_json['error_description'], e)) from e
-
-            raise Exception(f"Error refreshing access token: {e}") from e
+    # def refresh_access_token(self) -> Tuple[str, int]:
+    #     """
+    #     returns a tuple of (access_token, token_lifespan_in_seconds)
+    #     """
+    #     response_json = None
+    #     try:
+    #         response = requests.request(method="POST", url=self.token_refresh_endpoint, data=self.get_refresh_request_body())
+    #         response_json = response.json()
+    #         response.raise_for_status()
+    #         return response_json["access_token"], response_json["expires_in"]
+    #     except requests.exceptions.RequestException as e:
+    #         # if response_json and "error" in response_json:
+    #         #     raise Exception(
+    #         #         "Error refreshing access token. Error: {}; Error details: {}; Exception: {}".format(
+    #         #             response_json["error"], response_json["error_description"], e
+    #         #         )
+    #         #     ) from e
+    #         raise Exception(f"Error refreshing access token: {e}") from e
 
 
 # Source
 class SourceSnapchatMarketing(AbstractSource):
-
     def check_connection(self, logger, config) -> Tuple[bool, any]:
 
         try:
@@ -211,7 +226,7 @@ class SourceSnapchatMarketing(AbstractSource):
 
             token = auth.get_access_token()
 
-            url = 'https://adsapi.snapchat.com/v1/me'
+            url = "https://adsapi.snapchat.com/v1/me"
 
             session = requests.get(url, headers={"Authorization": "Bearer {}".format(token)})
             session.raise_for_status()
@@ -222,12 +237,5 @@ class SourceSnapchatMarketing(AbstractSource):
 
     def streams(self, config: Mapping[str, Any]) -> List[Stream]:
         auth = SnapchatAdsOauth2Authenticator(config)
-        kwargs = {
-            'authenticator': auth,
-            'start_date': config['start_date']
-        }
-        return [
-            Organizations(**kwargs),
-            AdAccounts(**kwargs),
-            Creatives(**kwargs)
-        ]
+        kwargs = {"authenticator": auth, "start_date": config["start_date"]}
+        return [Organizations(**kwargs), AdAccounts(**kwargs), Creatives(**kwargs)]
