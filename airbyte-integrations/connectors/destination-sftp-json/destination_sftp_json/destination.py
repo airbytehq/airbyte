fad0b9ee03aa4248f4141b2b8a55606e387cf650
Madison Swain-Bowden <bowdenm@spu.edu>
2021-07-21 12:31:11 -0700

    Add destination logic for write, check

--- airbyte-integrations/connectors/destination-sftp-json/destination_sftp_json/destination.py
+++ airbyte-integrations/connectors/destination-sftp-json/destination_sftp_json/destination.py
@@ -19,25 +19,32 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
-
-
+import traceback
+import uuid
 from typing import Mapping, Any, Iterable
 
 from airbyte_cdk import AirbyteLogger
 from airbyte_cdk.destinations import Destination
-from airbyte_cdk.models import AirbyteConnectionStatus, ConfiguredAirbyteCatalog, AirbyteMessage, Type
+from airbyte_cdk.models import (
+    AirbyteConnectionStatus,
+    ConfiguredAirbyteCatalog,
+    AirbyteMessage,
+    Status,
+    DestinationSyncMode,
+    Type,
+)
+from destination_sftp_json.client import SftpClient
 
 
 class DestinationSftpJson(Destination):
     def write(
-            self,
-            config: Mapping[str, Any],
-            configured_catalog: ConfiguredAirbyteCatalog,
-            input_messages: Iterable[AirbyteMessage]
+        self,
+        config: Mapping[str, Any],
+        configured_catalog: ConfiguredAirbyteCatalog,
+        input_messages: Iterable[AirbyteMessage],
     ) -> Iterable[AirbyteMessage]:
 
         """
-        TODO
         Reads the input stream of messages, config, and catalog to write data to the destination.
 
         This method returns an iterable (typically a generator of AirbyteMessages via yield) containing state messages received
@@ -51,10 +58,33 @@ class DestinationSftpJson(Destination):
         :param input_messages: The stream of input messages received from the source
         :return: Iterable of AirbyteStateMessages wrapped in AirbyteMessage structs
         """
+        # Apply default filename if one is not defined
+        config = dict(config)
+        filename = config.get("filename") or configured_catalog.streams[0].stream.name
+        config["filename"] = filename
+        writer = SftpClient(**config)
 
-        pass
+        for configured_stream in configured_catalog.streams:
+            if configured_stream.destination_sync_mode == DestinationSyncMode.overwrite:
+                writer.delete()
 
-    def check(self, logger: AirbyteLogger, config: Mapping[str, Any]) -> AirbyteConnectionStatus:
+        with writer:
+            for message in input_messages:
+                if message.type == Type.STATE:
+                    # Emitting a state message indicates that all records which came
+                    # before it have been written to the destination. We don't need to
+                    # do anything specific to save the data so we just re-emit these
+                    yield message
+                elif message.type == Type.RECORD:
+                    record = message.record
+                    writer.write(record.data)
+                else:
+                    # ignore other message types for now
+                    continue
+
+    def check(
+        self, logger: AirbyteLogger, config: Mapping[str, Any]
+    ) -> AirbyteConnectionStatus:
         """
         Tests if the input configuration can be used to successfully connect to the destination with the needed permissions
             e.g: if a provided API token or password can be used to connect and write to the destination.
@@ -67,11 +97,18 @@ class DestinationSftpJson(Destination):
         :return: AirbyteConnectionStatus indicating a Success or Failure
         """
         try:
-            # TODO
-
+            # Verify write access by attempting to write then delete
+            filename = str(uuid.uuid4())
+            destination_path = "/tmp"
+            writer = SftpClient(
+                **{**config, "destination_path": destination_path, "filename": filename}
+            )
+            writer.write({"value": "_airbyte_connection_check"})
+            writer.delete()
             return AirbyteConnectionStatus(status=Status.SUCCEEDED)
         except Exception as e:
-            return AirbyteConnectionStatus(status=Status.FAILED, message=f"An exception occurred: {repr(e)}")
-
-
-
+            traceback.print_exc()
+            return AirbyteConnectionStatus(
+                status=Status.FAILED,
+                message=f"An exception occurred: {e}. \nStacktrace: \n{traceback.format_exc()}",
+            )
