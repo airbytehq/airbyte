05313d388422a663630b7046a3ffaf92ac6dce07
Madison Swain-Bowden <bowdenm@spu.edu>
2021-07-23 09:29:29 -0700

    Define port as a string

--- airbyte-integrations/connectors/source-file/source_file/client.py
+++ airbyte-integrations/connectors/source-file/source_file/client.py
@@ -119,7 +119,9 @@ class URLFile:
         elif storage in ("ssh://", "scp://", "sftp://"):
             user = self._provider["user"]
             host = self._provider["host"]
-            port = self._provider.get("port", 22)
+            # TODO: Remove int casting when https://github.com/airbytehq/airbyte/issues/4952 is addressed
+            # TODO: The "port" field in spec.json must also be changed
+            port = int(self._provider.get("port", 22))
             # Explicitly turn off ssh keys stored in ~/.ssh
             transport_params = {"connect_kwargs": {"look_for_keys": False}}
             if "password" in self._provider:
