fdead7beba5c8e56a1fef5da39ab6ee98309cb0f
Jenny Brown <jenny@airbyte.io>
2021-07-23 13:32:12 -0500

    Creating a bastion host

---
+++ terraform/aws/ssh_tunnel/ec2.tf
@@ -0,0 +1,53 @@
+# Create a bastion host with a user account we can ssh in as, for using an ssh tunnel.
+
+data "aws_ami" "amazon-linux-2" {
+  owners = [137112412989]
+
+  filter {
+    name   = "owner-alias"
+    values = ["amazon"]
+  }
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-2.0.20210701.0-x86_64-gp2"]
+  }
+}
+
+resource "aws_key_pair" "key" {
+  key_name   = "dbtunnel-bastion-ec2-user-ssh-key"
+  public_key = file("dbtunnel-bastion-ec2-user_rsa.pub")
+}
+
+resource "aws_instance" "dbtunnel-bastion" {
+  ami           = data.aws_ami.amazon-linux-2.id
+  instance_type = "t3.small"
+
+  subnet_id = aws_subnet.main-subnet-public-dbtunnel.id
+  vpc_security_group_ids = [aws_security_group.ssh-and-egress-allowed.id]
+  key_name = aws_key_pair.key.key_name
+  user_data = file("userdata.sh")
+
+  tags = {
+    Name = "dbtunnel-bastion"
+  }
+}
+
+resource "aws_eip" "dbtunnel-eip" {
+  vpc = true
+  instance = aws_instance.dbtunnel-bastion.id
+}
+
+data "aws_route53_zone" "selected" {
+  name         = "dev.dataline.io."
+}
+
+resource "aws_route53_record" "proxy_cname" {
+  name    = "dbtunnel-bastion.${data.aws_route53_zone.selected.name}"
+  type    = "A"
+  zone_id = data.aws_route53_zone.selected.zone_id
+  ttl     = "300"
+  records = [aws_instance.dbtunnel-bastion.public_ip]
+}
+
+
---
+++ terraform/aws/ssh_tunnel/userdata.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+# Nothing to do yet.
