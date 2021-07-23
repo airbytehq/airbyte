0c5cf907e06739e863d888cb8dad8666207c07d9
vovavovavovavova <39351371+vovavovavovavova@users.noreply.github.com>
2021-07-23 11:50:27 +0300

    New Source Cart: Implementation (#4785)
    * save the setup
    
    * preparing the streams::in progress
    
    * adjust new source streams.py
    
    * upd spec
    
    * save schemas without nullable
    
    * fixing with schemas
    
    * save before test
    
    * successful check
    
    * fix discover
    
    * rename wrong name
    
    * read+
    
    * upd configured_catalog
    
    * format
    
    * fixing schemas (doc mismatch a bit with responses also with fields presense) + fix abnormal json
    
    * modify configured_catalog
    
    * gradle jsons
    
    * requested changes
    
    * upd wording
    
    * increase timeouts
    
    * if Cart store name is incorrect: improve user hint && upd failure timeout to be able to check wrong stor name (not only wrong token which is fast)
    
    * cleanup spec.json
    
    * latest upd
    
    * New Source Cart (#4588): Documentation (#4859)
    
    * save docs
    
    * forgotten file name
    
    * finish docs?
    
    * Update docs/integrations/sources/cart.md
    
    Co-authored-by: Eugene Kulak <widowmakerreborn@gmail.com>
    
    * Update docs/integrations/sources/cart.md
    
    Co-authored-by: Eugene Kulak <widowmakerreborn@gmail.com>
    
    * Update docs/integrations/sources/cart.md
    
    Co-authored-by: Eugene Kulak <widowmakerreborn@gmail.com>
    
    * Update docs/integrations/sources/cart.md
    
    Co-authored-by: Eugene Kulak <widowmakerreborn@gmail.com>
    
    * merge && resolve conflict
    
    Co-authored-by: Eugene Kulak <widowmakerreborn@gmail.com>
    
    * apply add abs_property
    
    Co-authored-by: Eugene Kulak <widowmakerreborn@gmail.com>

--- airbyte-integrations/connectors/source-cart/integration_tests/acceptance.py
+++ airbyte-integrations/connectors/source-cart/integration_tests/acceptance.py
@@ -1,3 +1,4 @@
+#
 # MIT License
 #
 # Copyright (c) 2020 Airbyte
@@ -19,6 +20,7 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
+#
 
 
 import pytest
@@ -29,6 +31,4 @@ pytest_plugins = ("source_acceptance_test.plugin",)
 @pytest.fixture(scope="session", autouse=True)
 def connector_setup():
     """ This fixture is a placeholder for external resources that acceptance test might require."""
-    # TODO: setup test dependencies if needed. otherwise remove the TODO comments
     yield
-    # TODO: clean up test dependencies
