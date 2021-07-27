ef6ab983d96329e3e579e98307bb51eda99466e9
Christophe Duong <christophe.duong@gmail.com>
2021-07-27 10:48:36 +0200

    Fix normalization integration tests in CI (#4910)

--- tools/bin/ci_integration_test.sh
+++ tools/bin/ci_integration_test.sh
@@ -13,7 +13,17 @@ if [[ "$connector" == "all" ]] ; then
   echo "Running: ./gradlew --no-daemon --scan integrationTest"
   ./gradlew --no-daemon --scan integrationTest
 else
-  if [[ "$connector" == *"connectors"* ]] || [[ "$connector" == *"bases"* ]]; then
+  if [[ "$connector" == *"base-normalization"* ]]; then
+    selected_integration_test="base-normalization"
+    integrationTestCommand="$(_to_gradle_path "airbyte-integrations/bases/base-normalization" integrationTest)"
+    export SUB_BUILD="CONNECTORS_BASE"
+    ./gradlew --no-daemon --scan airbyteDocker
+  elif [[ "$connector" == *"bases"* ]]; then
+    connector_name=$(echo $connector | cut -d / -f 2)
+    selected_integration_test=$(echo "$all_integration_tests" | grep "^$connector_name$" || echo "")
+    integrationTestCommand="$(_to_gradle_path "airbyte-integrations/$connector" integrationTest)"
+    export SUB_BUILD="CONNECTORS_BASE"
+  elif [[ "$connector" == *"connectors"* ]]; then
     connector_name=$(echo $connector | cut -d / -f 2)
     selected_integration_test=$(echo "$all_integration_tests" | grep "^$connector_name$" || echo "")
     integrationTestCommand="$(_to_gradle_path "airbyte-integrations/$connector" integrationTest)"
