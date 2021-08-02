64e8e37432ae1cf89c5c5bd06c2a0f9ba34d69b1
Madison Swain-Bowden <bowdenm@spu.edu>
2021-08-01 20:28:15 -0700

    Add unit tests!

--- airbyte-integrations/connectors/destination-hdfs/unit_tests/unit_test.py
+++ airbyte-integrations/connectors/destination-hdfs/unit_tests/unit_test.py
@@ -20,6 +20,82 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
+from pathlib import Path
+from unittest import mock
 
-def test_example_method():
-    assert True
+import pytest
+
+from destination_hdfs.client import HDFSClient
+from hdfs import InsecureClient
+
+
+@pytest.mark.parametrize(
+    "config, expected_client",
+    [
+        # Using defaults
+        (
+            {
+                "host": "hdfs-host",
+                "user": "me",
+            },
+            InsecureClient("http://hdfs-host:50070", user="me"),
+        ),
+        # Different port
+        (
+            {
+                "host": "hdfs-host",
+                "user": "me",
+                "port": 33,
+            },
+            InsecureClient("http://hdfs-host:33", user="me"),
+        ),
+    ],
+)
+def test_get_uri(config, expected_client):
+    hdfs_client = HDFSClient(
+        **config, destination_path="/foo", stream="bar"
+    ).get_client()
+    assert hdfs_client.url == expected_client.url
+    assert (
+        hdfs_client._session.params["user.name"]
+        == expected_client._session.params["user.name"]
+    )
+
+
+def test_path():
+    hdfs_client = HDFSClient(
+        host="hdfs-host", user="me", destination_path="/path/to/hdfs", stream="mystream"
+    )
+    assert hdfs_client.path == "/path/to/hdfs/airbyte_json_mystream.jsonl"
+
+
+@pytest.mark.parametrize("overwrite", [True, False], ids=["overwrite", "append"])
+@pytest.mark.parametrize(
+    "exists", [True, False], ids=["exists-already", "does-not-exist"]
+)
+def test_write(overwrite, exists):
+    hdfs_client = HDFSClient(
+        host="hdfs-host", user="me", destination_path="/foo", stream="bar"
+    )
+
+    with mock.patch("destination_hdfs.client.InsecureClient") as InsecureClientMock:
+        hdfs_mock = InsecureClientMock.return_value
+        hdfs_mock.status.return_value = exists
+        hdfs_client.write("sample-data", overwrite)
+
+        # Check that the path is unconditionally made
+        hdfs_mock.makedirs.assert_called_with(hdfs_client.destination_path)
+        # Check that the file was deleted if it was supposed to exist, but only if the
+        # mode was set to overwrite
+        if overwrite and exists:
+            assert hdfs_mock.delete.called
+        else:
+            hdfs_mock.delete.assert_not_called()
+        # Check that a new empty file was made if it didn't exist, or if overwrite was
+        # specified
+        if overwrite or not exists:
+            hdfs_mock.write.assert_any_call(hdfs_client.path, encoding="utf-8", data="")
+        # Lastly, check that a write of the passed data was made!
+        hdfs_mock.write.assert_called_with(
+            hdfs_client.path, encoding="utf-8", data="sample-data", append=True
+        )
