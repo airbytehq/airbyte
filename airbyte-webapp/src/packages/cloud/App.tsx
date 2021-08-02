9c20f65790dac3881879ca239a891904439f6830
Artem Astapenko <jamakase54@gmail.com>
2021-08-02 21:54:01 +0300

    Add cloud locales

--- airbyte-webapp/src/packages/cloud/App.tsx
+++ airbyte-webapp/src/packages/cloud/App.tsx
@@ -4,6 +4,7 @@ import { IntlProvider } from "react-intl";
 import { CacheProvider } from "rest-hooks";
 
 import en from "locales/en.json";
+import cloudLocales from "./locales/en.json";
 import GlobalStyle from "global-styles";
 import { theme } from "theme";
 
@@ -16,12 +17,14 @@ import NotificationService from "components/hooks/services/Notification";
 import { AnalyticsInitializer } from "views/common/AnalyticsInitializer";
 import { AuthenticationProvider } from "./services/auth/AuthService";
 
+const messages = Object.assign({}, en, cloudLocales);
+
 const App: React.FC = () => {
   return (
     <React.StrictMode>
       <ThemeProvider theme={theme}>
         <GlobalStyle />
-        <IntlProvider locale="en" messages={en}>
+        <IntlProvider locale="en" messages={messages}>
           <CacheProvider>
             <Suspense fallback={<LoadingPage />}>
               <ApiErrorBoundary>
