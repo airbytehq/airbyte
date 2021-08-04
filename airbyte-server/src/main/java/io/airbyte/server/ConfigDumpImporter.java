67ccf19f300d4d364eeb9fc137972a44172a6548
Liren Tu <tuliren.git@outlook.com>
2021-08-04 16:52:35 -0700

    Simplify code

--- airbyte-server/src/main/java/io/airbyte/server/ConfigDumpImporter.java
+++ airbyte-server/src/main/java/io/airbyte/server/ConfigDumpImporter.java
@@ -73,7 +73,6 @@ import org.apache.commons.io.FileUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-@SuppressWarnings("OptionalUsedAsFieldOrParameterType")
 public class ConfigDumpImporter {
 
   private static final Logger LOGGER = LoggerFactory.getLogger(ConfigDumpImporter.class);
@@ -96,11 +95,6 @@ public class ConfigDumpImporter {
   }
 
   public ImportRead importDataWithSeed(String targetVersion, File archive, Path seedPath) {
-    return importDataInternal(targetVersion, archive, Optional.of(seedPath));
-  }
-
-  // seedPath - if present, merge with the import. otherwise just use the data in the import.
-  private ImportRead importDataInternal(String targetVersion, File archive, Optional<Path> seedPath) {
     Preconditions.checkNotNull(seedPath);
 
     ImportRead result;
@@ -146,7 +140,7 @@ public class ConfigDumpImporter {
     return result;
   }
 
-  private void checkImport(String targetVersion, Path tempFolder, Optional<Path> seed) throws IOException, JsonValidationException {
+  private void checkImport(String targetVersion, Path tempFolder, Path seed) throws IOException, JsonValidationException {
     final Path versionFile = tempFolder.resolve(VERSION_FILE_NAME);
     final String importVersion = Files.readString(versionFile, Charset.defaultCharset())
         .replace("\n", "").strip();
@@ -168,7 +162,7 @@ public class ConfigDumpImporter {
     }
   }
 
-  private <T> void importConfigsFromArchive(final Path sourceRoot, Optional<Path> seedPath, final boolean dryRun)
+  private <T> void importConfigsFromArchive(final Path sourceRoot, Path seedPath, final boolean dryRun)
       throws IOException, JsonValidationException {
     final List<String> sourceDefinitionsToMigrate = new ArrayList<>();
     final List<String> destinationDefinitionsToMigrate = new ArrayList<>();
@@ -181,12 +175,8 @@ public class ConfigDumpImporter {
     Collections.sort(directories);
     final Map<AirbyteConfig, Stream<T>> data = new LinkedHashMap<>();
 
-    final Map<ConfigSchema, Map<String, T>> seed;
-    if (seedPath.isPresent()) {
-      seed = getSeed(seedPath.get());
-    } else {
-      seed = new HashMap<>();
-    }
+    final Map<ConfigSchema, Map<String, T>> seed = getSeed(seedPath);
+
     for (final String directory : directories) {
       final Optional<ConfigSchema> configSchemaOptional = Enums.toEnum(directory.replace(".yaml", ""), ConfigSchema.class);
 
