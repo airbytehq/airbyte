fdd9f0fe5c5b11eaba71e4d550ad87363412c45d
Sherif Nada <snadalive@gmail.com>
2021-08-04 22:53:26 -0700

    fmt

--- airbyte-integrations/connectors/source-kustomer-singer/main.py
+++ airbyte-integrations/connectors/source-kustomer-singer/main.py
@@ -1,3 +1,4 @@
+#
 # MIT License
 #
 # Copyright (c) 2020 Airbyte
@@ -19,11 +20,11 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
+#
 
 import sys
 
 from airbyte_cdk.entrypoint import launch
-
 from source_kustomer_singer import SourceKustomerSinger
 
 if __name__ == "__main__":
