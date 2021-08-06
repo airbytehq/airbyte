407ee1faf888abde441fb8443d386f91678d00a9
Sherif Nada <snadalive@gmail.com>
2021-08-06 11:14:16 -0700

    fmt

--- airbyte-integrations/connectors/source-hubspot/source_hubspot/api.py
+++ airbyte-integrations/connectors/source-hubspot/source_hubspot/api.py
@@ -279,7 +279,7 @@ class Stream(ABC):
 
         try:
             casted_value = target_type(field_value)
-        except ValueError as e:
+        except ValueError:
             logger.exception(f"Could not cast {field_value} to {target_type}")
             return field_value
 
