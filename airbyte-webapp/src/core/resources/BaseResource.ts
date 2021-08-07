5104d18ad54e636630302af895b9885cc167caff
Pawan <pawan@blotout.io>
2021-08-07 15:12:19 +0530

    token fix

--- airbyte-webapp/src/core/resources/BaseResource.ts
+++ airbyte-webapp/src/core/resources/BaseResource.ts
@@ -19,7 +19,7 @@ export default abstract class BaseResource extends Resource {
     url: string,
     body?: Readonly<Record<string, unknown> | Array<unknown> | string>
   ): Promise<Response> {
-    const token = localStorage.getItem("token");
+    const token = localStorage.getItem("bo_token");
     let options: RequestInit = {
       method: "POST",
       headers: {
