52e1b0abae4421aa9efd1c604e8558410440123f
Oleksandr Bazarnov <oleksandr.bazarnov@globallogic.com>
2021-08-09 23:00:59 +0300

    Implemented CHECK method

---
+++ airbyte-integrations/connectors/source-linkedin-ads/source_linkedin_ads/source.py
@@ -0,0 +1,145 @@
+#
+# MIT License
+#
+# Copyright (c) 2020 Airbyte
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+#
+
+
+from abc import ABC
+from typing import Any, Iterable, List, Dict, Mapping, MutableMapping, Optional, Tuple
+
+import requests
+from airbyte_cdk import AirbyteLogger
+from airbyte_cdk.sources import AbstractSource
+from airbyte_cdk.sources.streams import Stream
+from airbyte_cdk.sources.streams.http import HttpStream
+from airbyte_cdk.sources.streams.http.auth import TokenAuthenticator
+
+
+class LinkedinAdsStream(HttpStream, ABC):
+
+    url_base = "https://api.linkedin.com/v2/"
+
+    def  __init__(self, config: Dict):
+        super().__init__(authenticator=config["authenticator"])
+        self.config = config
+
+    def next_page_token(self, response: requests.Response) -> Optional[Mapping[str, Any]]:
+        return None
+
+    def request_params(
+        self, stream_state: Mapping[str, Any], stream_slice: Mapping[str, any] = None, next_page_token: Mapping[str, Any] = None
+    ) -> MutableMapping[str, Any]:
+        """
+        TODO: Override this method to define any query parameters to be set. Remove this method if you don't need to define request params.
+        Usually contains common params e.g. pagination size etc.
+        """
+        return {}
+
+    def parse_response(self, response: requests.Response, **kwargs) -> Iterable[Mapping]:
+        """
+        TODO: Override this method to define how a response is parsed.
+        :return an iterable containing each record in the response
+        """
+        yield {}
+
+
+class Accounts(LinkedinAdsStream):
+
+    primary_key = "id"
+
+    def path(
+        self, stream_state: Mapping[str, Any] = None, stream_slice: Mapping[str, Any] = None, next_page_token: Mapping[str, Any] = None
+    ) -> str:
+        
+        return "/adAccountsV2/"
+
+
+class IncrementalLinkedinAdsStream(LinkedinAdsStream, ABC):
+
+    # TODO: Fill in to checkpoint stream reads after N records. This prevents re-reading of data if the stream fails for any reason.
+    state_checkpoint_interval = None
+
+    @property
+    def cursor_field(self) -> str:
+        """
+        TODO
+        Override to return the cursor field used by this stream e.g: an API entity might always use created_at as the cursor field. This is
+        usually id or date based. This field's presence tells the framework this in an incremental stream. Required for incremental.
+
+        :return str: The name of the cursor field.
+        """
+        return []
+
+    def get_updated_state(self, current_stream_state: MutableMapping[str, Any], latest_record: Mapping[str, Any]) -> Mapping[str, Any]:
+        """
+        Override to determine the latest state after reading the latest record. This typically compared the cursor_field from the latest record and
+        the current state and picks the 'most' recent cursor. This is how a stream's state is determined. Required for incremental.
+        """
+        return {}
+
+
+class Employees(IncrementalLinkedinAdsStream):
+
+    cursor_field = "start_date"
+
+    # TODO: Fill in the primary key. Required. This is usually a unique field in the stream, like an ID or a timestamp.
+    primary_key = "employee_id"
+
+    def path(self, **kwargs) -> str:
+        
+        return "employees"
+
+    def stream_slices(self, stream_state: Mapping[str, Any] = None, **kwargs) -> Iterable[Optional[Mapping[str, any]]]:
+        
+        raise NotImplementedError("Implement stream slices or delete this method!")
+
+
+class SourceLinkedinAds(AbstractSource):
+
+    def check_connection(self, logger: AirbyteLogger, config: Mapping[str, Any]) -> Tuple[bool, any]:
+        """
+        Testing connection availability for the connector.
+        :: for this check method the Customer must have the "r_liteprofile" scope enabled.
+        :: more info: https://docs.microsoft.com/linkedin/consumer/integrations/self-serve/sign-in-with-linkedin
+        """
+
+        header = TokenAuthenticator(token=config["access_token"]).get_auth_header()
+        profile_url = "https://api.linkedin.com/v2/me"
+
+        try:
+            response = requests.get(url=profile_url, headers=header)
+            response.raise_for_status()
+            return True, None
+        except requests.exceptions.HTTPError as e:
+            return False, f"{e}, {response.json().get('message')}"
+
+    def streams(self, config: Mapping[str, Any]) -> List[Stream]:
+        """
+        Mapping a input config of the user input configuration as defined in the connector spec.
+        Passing config to the streams.
+        """
+
+        config["authenticator"] = TokenAuthenticator(token=config["access_token"])
+
+        return [
+            Accounts(config), 
+            ]
