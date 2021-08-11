fd52290f151f0b19eba37e95d0d41d3948fa6f12
midavadim <midavadim@yahoo.com>
2021-08-11 21:25:19 +0300

    Source google spreadsheets: fix error during listing sheets with diagram only (#5233)
    * Fix error during listing sheets with diagram only
    
    * added PR number
    
    * fixed formatting
    
    * add dummy test
    
    * added filter for grid sheets, added SAT tests.
    
    * added new GOOGLE_SHEETS_TESTS_CREDS env
    
    * fixed formatting

--- airbyte-integrations/connectors/source-google-sheets/integration_tests/acceptance.py
+++ airbyte-integrations/connectors/source-google-sheets/integration_tests/acceptance.py
@@ -23,31 +23,12 @@
 #
 
 
-from setuptools import find_packages, setup
+import pytest
 
-TEST_REQUIREMENTS = [
-    "pytest~=6.1",
-    "source-acceptance-test",
-]
+pytest_plugins = ("source_acceptance_test.plugin",)
 
-setup(
-    name="google_sheets_source",
-    description="Source implementation for Google Sheets.",
-    author="Airbyte",
-    author_email="contact@airbyte.io",
-    packages=find_packages(),
-    install_requires=[
-        "airbyte-protocol",
-        "base-python",
-        "backoff",
-        "requests",
-        "google-auth-httplib2",
-        "google-api-python-client",
-        "PyYAML==5.4",
-        "pydantic==1.6.2",
-    ],
-    package_data={"": ["*.json", "schemas/*.json", "schemas/shared/*.json"]},
-    extras_require={
-        "tests": TEST_REQUIREMENTS,
-    },
-)
+
+@pytest.fixture(scope="session", autouse=True)
+def connector_setup():
+    """ This fixture is a placeholder for external resources that acceptance test might require."""
+    yield
