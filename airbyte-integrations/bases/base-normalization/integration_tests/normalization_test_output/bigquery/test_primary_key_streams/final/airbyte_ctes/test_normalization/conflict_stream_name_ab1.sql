e4fe62f739b6526adf10176632fdb44aeb85ccee
Marcos Marx <marcosmarxm@users.noreply.github.com>
2021-08-11 20:18:45 -0300

    Normalization: solve conflict when stream and field have same name (#4557)
    * solve conflict when stream and field have same name
    
    * add logic to handle conflict
    
    * change files
    
    * change json_extract functions
    
    * json_operations
    
    * add normalization files
    
    * test integration mysql
    
    * remove table_alias
    
    * mysql run
    
    * json ops
    
    * solve conflict with master
    
    * solve mysql circle dependency dbt
    
    * add tests for scalar and arrays
    
    * add sql files
    
    * bump normalization version
    
    * format

---
+++ airbyte-integrations/bases/base-normalization/integration_tests/normalization_test_output/bigquery/test_primary_key_streams/final/airbyte_ctes/test_normalization/conflict_stream_name_ab1.sql
@@ -0,0 +1,13 @@
+
+
+  create or replace view `dataline-integration-testing`._airbyte_test_normalization.`conflict_stream_name_ab1`
+  OPTIONS()
+  as 
+-- SQL model to parse JSON blob stored in a single column and extract into separated field columns as described by the JSON Schema
+select
+    json_extract_scalar(_airbyte_data, "$['id']") as id,
+    json_extract(table_alias._airbyte_data, "$['conflict_stream_name']") as conflict_stream_name,
+    _airbyte_emitted_at
+from `dataline-integration-testing`.test_normalization._airbyte_raw_conflict_stream_name as table_alias
+-- conflict_stream_name;
+
