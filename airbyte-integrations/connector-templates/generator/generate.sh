db6daed9220fdf4be21cbae471712bf796c2d222
Dmytro <avida.d3@gmail.com>
2021-08-12 10:33:50 +0300

    Add autogenerated uittests for python http source (#5245)
    Co-authored-by: Dmytro Rezchykov <dmitry.rezchykov@zazmic.com>

--- airbyte-integrations/connector-templates/generator/generate.sh
+++ airbyte-integrations/connector-templates/generator/generate.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env sh
 
+UID=$(id -u)
+GID=$(id -g)
 # Remove container if already exist
 echo "Removing previous generator if it exists..."
 docker container rm -f airbyte-connector-bootstrap >/dev/null 2>&1
@@ -8,7 +10,7 @@ docker container rm -f airbyte-connector-bootstrap >/dev/null 2>&1
 # Specify the host system user UID and GID to chown the generated files to host system user.
 # This is done because all generated files in container with mounted folders has root owner
 echo "Building generator docker image..."
-docker build --build-arg UID="$(id -u)" --build-arg GID="$(id -g)" . -t airbyte/connector-bootstrap
+docker build --build-arg UID="$UID" --build-arg GID="$GID" . -t airbyte/connector-bootstrap
 
 # Run the container and mount the airbyte folder
 if [ $# -eq 2 ]; then
@@ -16,7 +18,7 @@ if [ $# -eq 2 ]; then
   docker run --name airbyte-connector-bootstrap -e package_desc="$1" -e package_name="$2" -v "$(pwd)/../../../.":/airbyte airbyte/connector-bootstrap
 else
   echo "Running generator..."
-  docker run --rm -it --name airbyte-connector-bootstrap -v "$(pwd)/../../../.":/airbyte airbyte/connector-bootstrap
+  docker run --rm -it --name airbyte-connector-bootstrap --user $UID:$GID -v "$(pwd)/../../../.":/airbyte airbyte/connector-bootstrap
 fi
 
 echo "Finished running generator"
