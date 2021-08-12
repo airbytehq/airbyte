bb2e301fe5b0f1bf173c2d4826ed6a35aaba98f0
Artem Astapenko <jamakase54@gmail.com>
2021-08-12 22:34:37 +0300

    Remove redundant logs

--- airbyte-webapp/src/core/analytics/AnalyticsService.ts
+++ airbyte-webapp/src/core/analytics/AnalyticsService.ts
@@ -12,16 +12,13 @@ export class AnalyticsService {
 
   reset = (): void => this.getSegmentAnalytics()?.reset?.();
 
-  track = (name: string, properties: Record<string, unknown>): void => {
-    console.log("track", properties);
-    console.log(this.getSegmentAnalytics());
+  track = (name: string, properties: Record<string, unknown>): void =>
     this.getSegmentAnalytics()?.track?.(name, {
       user_id: this.userId,
       ...properties,
       airbyte_version: this.version,
       environment: this.version === "dev" ? "dev" : "prod",
     });
-  };
 
   identify = (userId: string, traits: Record<string, unknown> = {}): void => {
     this.getSegmentAnalytics()?.identify?.(userId, traits);
