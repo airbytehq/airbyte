e8d726dcd345670ccd93523b927fa13399e3fdba
Oleksandr Bazarnov <oleksandr.bazarnov@globallogic.com>
2021-08-12 15:39:35 +0300

    refactorred code, edited unit-tests, migrated to new CI, edited acceptance-tests, bumped version

--- airbyte-integrations/connectors/source-dixa/source_dixa/utils.py
+++ airbyte-integrations/connectors/source-dixa/source_dixa/utils.py
@@ -23,11 +23,28 @@
 #
 
 
-import pytest
-
-pytest_plugins = ("source_acceptance_test.plugin",)
+from datetime import datetime, timedelta, timezone
 
 
-@pytest.fixture(scope="session", autouse=True)
-def connector_setup():
-    yield
+def validate_ms_timestamp(milliseconds: int) -> int:
+    if not type(milliseconds) == int or not len(str(milliseconds)) == 13:
+        raise ValueError(f"Not a millisecond-precision timestamp: {milliseconds}")
+    return milliseconds
+
+
+def ms_timestamp_to_datetime(milliseconds: int) -> datetime:
+    """
+    Converts a millisecond-precision timestamp to a datetime object.
+    """
+    return datetime.fromtimestamp(validate_ms_timestamp(milliseconds) / 1000, tz=timezone.utc)
+
+
+def datetime_to_ms_timestamp(dt: datetime) -> int:
+    """
+    Converts a datetime object to a millisecond-precision timestamp.
+    """
+    return int(dt.timestamp() * 1000)
+
+
+def add_days_to_ms_timestamp(days: int, milliseconds: int) -> int:
+    return datetime_to_ms_timestamp(ms_timestamp_to_datetime(validate_ms_timestamp(milliseconds)) + timedelta(days=days))
