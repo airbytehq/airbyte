9467014e485038de20ab1c766a93e3824a117962
Dmytro <avida.d3@gmail.com>
2021-08-13 14:58:24 +0300

    Validate oneOf JSON Schema rules as part of connector Acceptance Test  (#5242)
    * 3962: Add oneOf schema rules validation for acceptance test
    
    * Add spec acceptance test for mssql and postgres connectors
    
    
    Co-authored-by: Dmytro Rezchykov <dmitry.rezchykov@zazmic.com>

--- airbyte-integrations/bases/source-acceptance-test/unit_tests/conftest.py
+++ airbyte-integrations/bases/source-acceptance-test/unit_tests/conftest.py
@@ -22,30 +22,18 @@
 # SOFTWARE.
 #
 
+import json
 
-import setuptools
+import pytest
 
-MAIN_REQUIREMENTS = [
-    "airbyte-cdk~=0.1",
-    "docker~=4.4",
-    "PyYAML~=5.4",
-    "icdiff~=1.9",
-    "inflection~=0.5",
-    "pdbpp~=0.10",
-    "pydantic~=1.6",
-    "pytest~=6.1",
-    "pytest-sugar~=0.9",
-    "pytest-timeout~=1.4",
-    "pprintpp~=0.4",
-    "dpath~=2.0.1",
-]
 
-setuptools.setup(
-    name="source-acceptance-test",
-    description="Contains acceptance tests for source connectors.",
-    author="Airbyte",
-    author_email="contact@airbyte.io",
-    url="https://github.com/airbytehq/airbyte",
-    packages=setuptools.find_packages(),
-    install_requires=MAIN_REQUIREMENTS,
-)
+@pytest.fixture
+def mssql_spec_schema():
+    with open("unit_tests/data/mssql_spec.json") as f:
+        return json.load(f).get("connectionSpecification")
+
+
+@pytest.fixture
+def postgres_source_spec_schema():
+    with open("unit_tests/data/postgres_spec.json") as f:
+        return json.load(f).get("connectionSpecification")
