e057bc301ba37ea86902fe54516ee54f70950c44
Jenny Brown <jenny@airbyte.io>
2021-08-16 15:04:16 -0500

    Fixed for ssh tunneling

--- airbyte-integrations/infrastructure/ssh_tunnel/module/sql/postgresql-02-user-create.sql
+++ airbyte-integrations/infrastructure/ssh_tunnel/module/sql/postgresql-02-user-create.sql
@@ -2,6 +2,10 @@
  CREATE
     USER testcaseuser WITH password 'ThisIsNotTheRealPassword.PleaseSetThisByHand';
 
-GRANT CONNECT ON DATABASE test TO testcaseuser;
-GRANT USAGE ON SCHEMA public TO testcaseuser;
+GRANT CONNECT ON
+DATABASE test TO testcaseuser;
+
+GRANT USAGE ON
+SCHEMA public TO testcaseuser;
+
 GRANT integrationtest_rw TO testcaseuser;
