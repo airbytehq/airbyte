cb59dd8382ccb478cfa6f613e4d7537729726a28
Baz <bazarnov@me.com>
2021-08-18 21:59:33 +0300

    📝 🎉 Source Dixa: Migrated to CI Sandbox, refactored code structure for future support (#5367)

--- airbyte-integrations/connectors/source-dixa/unit_tests/unit_test.py
+++ airbyte-integrations/connectors/source-dixa/unit_tests/unit_test.py
@@ -25,37 +25,40 @@
 from datetime import datetime, timezone
 
 import pytest
+from source_dixa import utils
 from source_dixa.source import ConversationExport
+
+config = {"authenticator": "", "start_date": "2021-07-01", "api_token": "TOKEN", "batch_size": 1}
 
 
 @pytest.fixture
 def conversation_export():
-    return ConversationExport(start_date=datetime(year=2021, month=7, day=1, hour=12, tzinfo=timezone.utc), batch_size=1, logger=None)
+    return ConversationExport(config)
 
 
 def test_validate_ms_timestamp_with_valid_input():
-    assert ConversationExport._validate_ms_timestamp(1234567890123) == 1234567890123
+    assert utils.validate_ms_timestamp(1234567890123) == 1234567890123
 
 
 def test_validate_ms_timestamp_with_invalid_input_type():
     with pytest.raises(ValueError):
-        assert ConversationExport._validate_ms_timestamp(1.2)
+        assert utils.validate_ms_timestamp(1.2)
 
 
 def test_validate_ms_timestamp_with_invalid_input_length():
     with pytest.raises(ValueError):
-        assert ConversationExport._validate_ms_timestamp(1)
+        assert utils.validate_ms_timestamp(1)
 
 
 def test_ms_timestamp_to_datetime():
-    assert ConversationExport.ms_timestamp_to_datetime(1625312980123) == datetime(
+    assert utils.ms_timestamp_to_datetime(1625312980123) == datetime(
         year=2021, month=7, day=3, hour=11, minute=49, second=40, microsecond=123000, tzinfo=timezone.utc
     )
 
 
 def test_datetime_to_ms_timestamp():
     assert (
-        ConversationExport.datetime_to_ms_timestamp(
+        utils.datetime_to_ms_timestamp(
             datetime(year=2021, month=7, day=3, hour=11, minute=49, second=40, microsecond=123000, tzinfo=timezone.utc)
         )
         == 1625312980123
@@ -63,62 +66,72 @@ def test_datetime_to_ms_timestamp():
 
 
 def test_add_days_to_ms_timestamp():
-    assert ConversationExport.add_days_to_ms_timestamp(days=1, milliseconds=1625312980123) == 1625399380123
+    assert utils.add_days_to_ms_timestamp(days=1, ms_timestamp=1625312980123) == 1625399380123
 
 
 def test_stream_slices_without_state(conversation_export):
-    conversation_export.end_timestamp = 1625270400001  # 2021-07-03 00:00:00 + 1 ms
+    conversation_export.end_timestamp = 1625259600000  # 2021-07-03 00:00:00 + 1 ms
+
     expected_slices = [
-        {"updated_after": 1625140800000, "updated_before": 1625227200000},  # 2021-07-01 12:00:00  # 2021-07-02 12:00:00
-        {"updated_after": 1625227200000, "updated_before": 1625270400001},
+        {"updated_after": 1625097600000, "updated_before": 1625184000000},
+        {"updated_after": 1625184000000, "updated_before": 1625259600000},
     ]
+
     actual_slices = conversation_export.stream_slices()
     assert actual_slices == expected_slices
 
 
 def test_stream_slices_without_state_large_batch():
-    conversation_export = ConversationExport(
-        start_date=datetime(year=2021, month=7, day=1, hour=12, tzinfo=timezone.utc), batch_size=31, logger=None
-    )
-    conversation_export.end_timestamp = 1625270400001  # 2021-07-03 00:00:00 + 1 ms
-    expected_slices = [{"updated_after": 1625140800000, "updated_before": 1625270400001}]  # 2021-07-01 12:00:00
+
+    updated_config = config
+    updated_config["batch_size"] = 31
+
+    conversation_export = ConversationExport(updated_config)
+    conversation_export.end_timestamp = 1625259600000  # 2021-07-03 00:00:00 + 1 ms
+    expected_slices = [{"updated_after": 1625097600000, "updated_before": 1625259600000}]  # 2021-07-01 12:00:00 """
     actual_slices = conversation_export.stream_slices()
     assert actual_slices == expected_slices
 
 
 def test_stream_slices_with_state(conversation_export):
-    conversation_export.end_timestamp = 1625263200001  # 2021-07-03 00:00:00 + 1 ms
-    expected_slices = [{"updated_after": 1625220000000, "updated_before": 1625263200001}]  # 2021-07-01 12:00:00
+    conversation_export.end_timestamp = 1625259600001  # 2021-07-03 00:00:00 + 1 ms
+    expected_slices = [{"updated_after": 1625220000000, "updated_before": 1625259600001}]  # 2021-07-01 12:00:00
     actual_slices = conversation_export.stream_slices(stream_state={"updated_at": 1625220000000})  # # 2021-07-02 12:00:00
     assert actual_slices == expected_slices
 
 
 def test_stream_slices_with_start_timestamp_larger_than_state():
-    """
-    Test that if start_timestamp is larger than state, then start at start_timestamp.
-    """
-    conversation_export = ConversationExport(
-        start_date=datetime(year=2021, month=12, day=1, tzinfo=timezone.utc), batch_size=31, logger=None
-    )
-    conversation_export.end_timestamp = 1638360000001  # 2021-12-01 12:00:00 + 1 ms
-    expected_slices = [{"updated_after": 1638316800000, "updated_before": 1638360000001}]  # 2021-07-01 12:00:00
-    actual_slices = conversation_export.stream_slices(stream_state={"updated_at": 1625220000000})  # # 2021-07-02 12:00:00
+    #
+    # Test that if start_timestamp is larger than state, then start at start_timestamp.
+    #
+    updated_config = config
+    updated_config["start_date"] = "2021-12-01"
+    updated_config["batch_size"] = 31
+
+    conversation_export = ConversationExport(updated_config)
+    conversation_export.end_timestamp = 1638352800001  # 2021-12-01 12:00:00 + 1 ms
+    expected_slices = [{"updated_after": 1638316800000, "updated_before": 1638352800001}]  # 2021-07-01 12:00:00 """
+    actual_slices = conversation_export.stream_slices(stream_state={"updated_at": 1625216400000})  # # 2021-07-02 12:00:00
     assert actual_slices == expected_slices
 
 
 def test_get_updated_state_without_state(conversation_export):
-    assert conversation_export.get_updated_state(current_stream_state=None, latest_record={"updated_at": 1625263200000}) == {
-        "updated_at": 1625140800000
-    }
+    expected = {"updated_at": 1638316800000}
+    actual = conversation_export.get_updated_state(current_stream_state=None, latest_record={"updated_at": 1625259600001})
+    assert actual == expected
 
 
 def test_get_updated_state_with_bigger_state(conversation_export):
-    assert conversation_export.get_updated_state(
+    expected = {"updated_at": 1625263200000}
+    actual = conversation_export.get_updated_state(
         current_stream_state={"updated_at": 1625263200000}, latest_record={"updated_at": 1625220000000}
-    ) == {"updated_at": 1625263200000}
+    )
+    assert actual == expected
 
 
 def test_get_updated_state_with_smaller_state(conversation_export):
-    assert conversation_export.get_updated_state(
+    expected = {"updated_at": 1625263200000}
+    actual = conversation_export.get_updated_state(
         current_stream_state={"updated_at": 1625220000000}, latest_record={"updated_at": 1625263200000}
-    ) == {"updated_at": 1625263200000}
+    )
+    assert actual == expected
--- docs/integrations/sources/dixa.md
+++ docs/integrations/sources/dixa.md
@@ -54,4 +54,5 @@ decrease the number of requests sent to the API, but increase the response and p
 ## Changelog
 | Version | Date       | Pull Request | Subject |
 | :------ | :--------  | :-----       | :------ |
+| 0.1.1   | 2021-08-12 | [5367](https://github.com/airbytehq/airbyte/pull/5367) | Migrated to CI Sandbox, refactorred code structure for future support |
 | 0.1.0   | 2021-07-07 | [4358](https://github.com/airbytehq/airbyte/pull/4358) | New source |
