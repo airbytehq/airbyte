3eb21179d6e006ec4c2751ba3be324b98f784a7e
Marcos Marx <marcosmarxm@gmail.com>
2021-08-18 15:09:53 -0300

    bump version

--- airbyte-integrations/connectors/source-google-ads/source_google_ads/streams.py
+++ airbyte-integrations/connectors/source-google-ads/source_google_ads/streams.py
@@ -193,6 +193,7 @@ class ShoppingPerformanceReport(IncrementalGoogleAdsStream):
     Google Ads API field mapping: https://developers.google.com/google-ads/api/docs/migration/mapping#shopping_performance
     """
 
+
 class UserLocationReport(IncrementalGoogleAdsStream):
     """
     UserLocationReport stream: https://developers.google.com/google-ads/api/fields/v8/user_location_view
