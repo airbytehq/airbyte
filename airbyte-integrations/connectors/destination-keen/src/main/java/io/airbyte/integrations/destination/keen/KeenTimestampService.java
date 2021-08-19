fa96ad4122691d51f537091a1689c6672b8c3922
Marcos Marx <marcosmarxm@users.noreply.github.com>
2021-08-19 00:16:07 -0300

    Source Google Ads: enable additional params spec.json (#5509)
    * enable additioanl params spec.json
    
    * removed duplicated tests
    
    * add docs
    
    * format keen

--- airbyte-integrations/connectors/destination-keen/src/main/java/io/airbyte/integrations/destination/keen/KeenTimestampService.java
+++ airbyte-integrations/connectors/destination-keen/src/main/java/io/airbyte/integrations/destination/keen/KeenTimestampService.java
@@ -1,3 +1,27 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2020 Airbyte
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 package io.airbyte.integrations.destination.keen;
 
 import com.fasterxml.jackson.databind.JsonNode;
@@ -19,13 +43,17 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
- * This class is used for timestamp inference. Keen leverages use of time-related data for it's analytics, so it's
- * important to have timestamp values for historical data if possible. If stream contains cursor field, then its value is
- * used as a timestamp, if parsing it is possible.
+ * This class is used for timestamp inference. Keen leverages use of time-related data for it's
+ * analytics, so it's important to have timestamp values for historical data if possible. If stream
+ * contains cursor field, then its value is used as a timestamp, if parsing it is possible.
  */
 public class KeenTimestampService {
 
-  public enum CursorType {STRING, NUMBER, UNRECOGNIZED}
+  public enum CursorType {
+    STRING,
+    NUMBER,
+    UNRECOGNIZED
+  }
 
   private static final Logger LOGGER = LoggerFactory.getLogger(KeenRecordsConsumer.class);
 
@@ -56,11 +84,13 @@ public class KeenTimestampService {
   }
 
   /**
-   * Tries to inject keen.timestamp field to the given message data. If the stream contains
-   * cursor fields of types NUMBER or STRING, this value is tried to be parsed to a timestamp.
-   * If this procedure fails, stream is removed from timestamp-parsable stream map, so parsing is not tried
-   * for future messages in the same stream. If parsing succeeds, keen.timestamp field is put as a JSON node
-   * to the message data and whole data is returned. Otherwise, keen.timestamp is set to emittedAt value
+   * Tries to inject keen.timestamp field to the given message data. If the stream contains cursor
+   * fields of types NUMBER or STRING, this value is tried to be parsed to a timestamp. If this
+   * procedure fails, stream is removed from timestamp-parsable stream map, so parsing is not tried
+   * for future messages in the same stream. If parsing succeeds, keen.timestamp field is put as a
+   * JSON node to the message data and whole data is returned. Otherwise, keen.timestamp is set to
+   * emittedAt value
+   *
    * @param message AirbyteRecordMessage containing record data
    * @return Record data together with keen.timestamp field
    */
@@ -79,8 +109,7 @@ public class KeenTimestampService {
         streamCursorFields.remove(streamName);
         injectTimestamp(data, Instant.ofEpochMilli(message.getEmittedAt()).toString());
       }
-    }
-    else {
+    } else {
       injectTimestamp(data, Instant.ofEpochMilli(message.getEmittedAt()).toString());
     }
     return data;
@@ -95,7 +124,7 @@ public class KeenTimestampService {
     return switch (cursorField.type) {
       case NUMBER -> dateFromNumber(
           getNestedNode(data, cursorField.path)
-          .asLong());
+              .asLong());
       case STRING -> parser
           .parse(getNestedNode(data, cursorField.path)
               .asText())
@@ -123,12 +152,10 @@ public class KeenTimestampService {
 
     private static final Set<String> STRING_TYPES = Set.of(
         "STRING", "CHAR", "NCHAR", "NVARCHAR", "VARCHAR", "LONGVARCHAR", "DATE",
-        "TIME", "TIMESTAMP"
-    );
+        "TIME", "TIMESTAMP");
     private static final Set<String> NUMBER_TYPES = Set.of(
         "NUMBER", "TINYINT", "SMALLINT", "INT", "INTEGER", "BIGINT", "FLOAT", "DOUBLE",
-        "REAL", "NUMERIC", "DECIMAL"
-    );
+        "REAL", "NUMERIC", "DECIMAL");
 
     private final List<String> path;
     private final CursorType type;
@@ -171,8 +198,10 @@ public class KeenTimestampService {
 
     @Override
     public boolean equals(Object o) {
-      if (this == o) return true;
-      if (o == null || getClass() != o.getClass()) return false;
+      if (this == o)
+        return true;
+      if (o == null || getClass() != o.getClass())
+        return false;
       CursorField that = (CursorField) o;
       return Objects.equals(path, that.path) && type == that.type;
     }
