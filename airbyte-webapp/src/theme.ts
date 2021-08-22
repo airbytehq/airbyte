690149f844fafbfdc04ff8595db6f3db10b35719
Artem Astapenko <jamakase54@gmail.com>
2021-08-23 00:39:56 +0300

    Edit tables styles

--- airbyte-webapp/src/theme.ts
+++ airbyte-webapp/src/theme.ts
@@ -11,11 +11,11 @@ export const theme = {
 
   brightColor: "#F7F6FF",
 
-  dangerColor: "#F74D58",
+  dangerColor: "#FF5E7B",
   lightDangerColor: "#FEEDEE",
   dangerTransparentColor: "rgba(247, 77, 88, 0.1)",
   attentionColor: "#FFBD2E",
-  successColor: "#45E698",
+  successColor: "#67DAE1",
   successColor10: "rgba(69, 230, 152, .1)",
   successColor20: "rgba(69, 230, 152, .2)",
   backgroundColor: "#FEF9F4",
@@ -23,6 +23,7 @@ export const theme = {
   cardShadowColor: "rgba(26, 25, 77, 0.12)",
 
   textColor: "#1A194D",
+  lightTextColor: "#AFAFC1",
   textColor90: "rgba(26,26,33, .9)",
   greyColor80: "#353542",
   greyColor70: "#555461",
