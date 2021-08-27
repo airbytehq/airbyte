82ccb7e3f9df6f6acf8011a68446b5490650240b
Christophe Duong <christophe.duong@gmail.com>
2021-08-27 18:44:29 +0200

    format code

--- airbyte-server/src/main/java/io/airbyte/server/ConfigDumpImporter.java
+++ airbyte-server/src/main/java/io/airbyte/server/ConfigDumpImporter.java
@@ -466,7 +466,8 @@ public class ConfigDumpImporter {
     final List<String> directories = listDirectories(sourceRoot);
     // We sort the directories because we want to process SOURCE_CONNECTION after
     // STANDARD_SOURCE_DEFINITION and DESTINATION_CONNECTION after STANDARD_DESTINATION_DEFINITION
-    // so that we can identify which connectors should not be imported because the definitions are not existing
+    // so that we can identify which connectors should not be imported because the definitions are not
+    // existing
     directories.sort(Comparator.reverseOrder());
     Stream<T> standardSyncs = null;
 
