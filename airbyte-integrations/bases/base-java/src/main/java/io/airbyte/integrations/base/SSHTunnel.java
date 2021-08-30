b5aa5f4b0098a9f87fc2c2b6831e4057821b62f8
cgardens <giardina.charles@gmail.com>
2021-08-30 12:50:04 -0700

    clean up

--- airbyte-integrations/bases/base-java/src/main/java/io/airbyte/integrations/base/SSHTunnel.java
+++ airbyte-integrations/bases/base-java/src/main/java/io/airbyte/integrations/base/SSHTunnel.java
@@ -25,13 +25,13 @@
 package io.airbyte.integrations.base;
 
 import com.fasterxml.jackson.databind.JsonNode;
+import com.google.common.base.Preconditions;
 import io.airbyte.commons.concurrency.VoidCallable;
 import io.airbyte.commons.functional.CheckedSupplier;
 import io.airbyte.commons.json.Jsons;
 import java.io.IOException;
 import java.io.StringReader;
 import java.security.KeyPair;
-import java.util.Objects;
 import org.apache.sshd.client.SshClient;
 import org.apache.sshd.client.keyverifier.AcceptAllServerKeyVerifier;
 import org.apache.sshd.client.session.ClientSession;
@@ -54,8 +54,14 @@ public class SSHTunnel implements AutoCloseable {
 
   private static final Logger LOGGER = LoggerFactory.getLogger(SSHTunnel.class);
 
+  public enum Method {
+    NO_TUNNEL,
+    SSH_PASSWORD_AUTH,
+    SSH_KEY_AUTH
+  }
+
   public static final int TIMEOUT_MILLIS = 15000; // 15 seconds
-  private final String method;
+  private final Method method;
   private final String host;
   private final String tunnelSshPort;
   private final String user;
@@ -68,7 +74,7 @@ public class SSHTunnel implements AutoCloseable {
   private SshClient sshclient;
   private ClientSession tunnelSession;
 
-  public SSHTunnel(final String method,
+  public SSHTunnel(final Method method,
                    final String host,
                    final String tunnelSshPort,
                    final String user,
@@ -77,24 +83,50 @@ public class SSHTunnel implements AutoCloseable {
                    final String remoteDatabaseHost,
                    final String remoteDatabasePort,
                    final String tunnelDatabasePort) {
-    this.method = Objects.requireNonNullElse(method, "NO_TUNNEL");
-    this.host = host;
-    this.tunnelSshPort = tunnelSshPort;
-    this.user = user;
-    this.sshkey = sshKey;
-    this.password = password;
-    this.remoteDatabaseHost = remoteDatabaseHost;
-    this.remoteDatabasePort = remoteDatabasePort;
-    this.tunnelDatabasePort = tunnelDatabasePort;
 
-    this.sshclient = createClient();
-    this.tunnelSession = openTunnel(sshclient);
+    Preconditions.checkNotNull(method);
+    this.method = method;
+
+    if (method.equals(Method.NO_TUNNEL)) {
+      this.host = null;
+      this.tunnelSshPort = null;
+      this.user = null;
+      this.sshkey = null;
+      this.password = null;
+      this.remoteDatabaseHost = null;
+      this.remoteDatabasePort = null;
+      this.tunnelDatabasePort = null;
+    } else {
+      Preconditions.checkNotNull(host);
+      Preconditions.checkNotNull(tunnelSshPort);
+      Preconditions.checkNotNull(user);
+      Preconditions.checkArgument(sshKey != null || password != null,
+          "SSH Tunnel was requested to be opened while it was already open.  This is a coding error.");
+      Preconditions.checkNotNull(remoteDatabaseHost);
+      Preconditions.checkNotNull(remoteDatabasePort);
+      Preconditions.checkNotNull(tunnelDatabasePort);
+
+      this.host = host;
+      this.tunnelSshPort = tunnelSshPort;
+      this.user = user;
+      this.sshkey = sshKey;
+      this.password = password;
+      this.remoteDatabaseHost = remoteDatabaseHost;
+      this.remoteDatabasePort = remoteDatabasePort;
+      this.tunnelDatabasePort = tunnelDatabasePort;
+
+      this.sshclient = createClient();
+      this.tunnelSession = openTunnel(sshclient);
+    }
   }
 
   public static SSHTunnel getInstance(final JsonNode config) {
     final JsonNode sshConfig = config.get("tunnel_method");
+    final Method tunnelMethod = Method.valueOf(Jsons.getStringOrNull(sshConfig, "tunnel_method").trim());
+    LOGGER.info("Starting connection with method: {}", tunnelMethod);
+
     return new SSHTunnel(
-        Jsons.getStringOrNull(sshConfig, "tunnel_method").trim(),
+        tunnelMethod,
         Jsons.getStringOrNull(sshConfig, "tunnel_host").trim(),
         Jsons.getStringOrNull(sshConfig, "tunnel_ssh_port").trim(),
         Jsons.getStringOrNull(sshConfig, "tunnel_username").trim(),
@@ -127,12 +159,12 @@ public class SSHTunnel implements AutoCloseable {
       if (shouldTunnel()) {
         if (tunnelSession != null) {
           tunnelSession.close();
+          tunnelSession = null;
         }
         if (sshclient != null) {
           sshclient.stop();
+          sshclient = null;
         }
-        tunnelSession = null;
-        sshclient = null;
       }
     } catch (final Throwable t) {
       throw new RuntimeException(t);
@@ -170,9 +202,7 @@ public class SSHTunnel implements AutoCloseable {
   }
 
   private void validate() {
-    if (host == null) {
-      throw new RuntimeException("SSH Tunnel host is null - verify configuration before starting tunnel!");
-    }
+    Preconditions.checkNotNull(host, "SSH Tunnel host is null - verify configuration before starting tunnel!");
   }
 
   /**
