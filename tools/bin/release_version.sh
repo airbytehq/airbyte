16c19240cc7be99826957b0e6498fe85c5af7575
Subodh Kant Chaturvedi <subodh@airbyte.io>
2021-09-01 00:11:20 +0530

    introduce a github action for open source release (#5732)
    * introduce a github action for open source release
    
    * add more-secrets environment
    
    * PR body should contain the changelog
    
    * update script
    
    * add new lines

--- tools/bin/release_version.sh
+++ tools/bin/release_version.sh
@@ -4,13 +4,6 @@ set -e
 
 . tools/lib/lib.sh
 
-BRANCH=$(git rev-parse --abbrev-ref HEAD)
-
-if [[ "$BRANCH" == "master" ]]; then
-  echo 'This script should be run from a branch!';
-  exit 1;
-fi
-
 if [[ -z "${CLOUDREPO_USER}" ]]; then
   echo 'CLOUDREPO_USER env var not set. Please retrieve the user email from the CloudRepo lastpass secret and run export CLOUDREPO_USER=<user_from_secret>.';
   exit 1;
@@ -23,7 +16,6 @@ fi
 
 PREV_VERSION=$(grep VERSION .env | cut -d"=" -f2)
 
-PART_TO_BUMP=$1
 [[ -z "$PART_TO_BUMP" ]] && echo "Usage ./tools/bin/release_version.sh (major|minor|patch)" && exit 1
 
 # uses .bumpversion.cfg to find files to bump
@@ -44,15 +36,3 @@ SUB_BUILD=PLATFORM ./gradlew publish
 VERSION=$NEW_VERSION GIT_REVISION=$GIT_REVISION docker-compose -f docker-compose.build.yaml build
 VERSION=$NEW_VERSION GIT_REVISION=$GIT_REVISION docker-compose -f docker-compose.build.yaml push
 echo "Completed building and publishing..."
-
-echo
-echo "Changelog:"
-PAGER=cat git log v${PREV_VERSION}..${GIT_REVISION} --oneline --decorate=no
-
-echo
-echo "Final Steps:"
-echo "1. Push your changes"
-echo "2. Merge your PR"
-echo "3. Switch to master"
-echo "4. Run ./tools/bin/tag_version.sh"
-echo "5. Create a GitHub release with the changelog"
