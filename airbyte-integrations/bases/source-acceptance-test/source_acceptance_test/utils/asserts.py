6b36352236d08f7c80672408993e0d829284da7f
Oleksandr Bazarnov <oleksandr.bazarnov@globallogic.com>
2021-09-01 14:37:49 +0300

    formated

--- airbyte-integrations/bases/source-acceptance-test/source_acceptance_test/utils/asserts.py
+++ airbyte-integrations/bases/source-acceptance-test/source_acceptance_test/utils/asserts.py
@@ -24,22 +24,17 @@
 
 import logging
 import re
-import pendulum
 from collections import defaultdict
 from typing import List, Mapping
 
+import pendulum
 from airbyte_cdk.models import AirbyteRecordMessage, ConfiguredAirbyteCatalog
-from jsonschema import Draft7Validator, FormatChecker, ValidationError, FormatError
+from jsonschema import Draft7Validator, FormatChecker, FormatError, ValidationError
 
-timestamp_regex = re.compile((r"^\d{4}-\d?\d-\d?\d" # date
-                              r"(\s|T)" # separator
-                              r"\d?\d:\d?\d:\d?\d(.\d+)?" # time
-                              r".*$" #timezone
-                             ))
+timestamp_regex = re.compile((r"^\d{4}-\d?\d-\d?\d" r"(\s|T)" r"\d?\d:\d?\d:\d?\d(.\d+)?" r".*$"))  # date  # separator  # time  # timezone
 
 
 class CustomFormatChecker(FormatChecker):
-
     @staticmethod
     def check_datetime(value: str) -> bool:
         valid_format = timestamp_regex.match(value)
@@ -57,7 +52,6 @@ class CustomFormatChecker(FormatChecker):
                 raise FormatError(f"{instance} has invalid datetime format")
         else:
             return super().check(instance, format)
-
 
 
 def verify_records_schema(
