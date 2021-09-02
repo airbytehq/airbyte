577933351b67a7cee130074fc7efb50280dfba4b
George Claireaux <george@claireaux.co.uk>
2021-09-02 22:22:30 +0100

    Update spec and configs (#5792)
    * changes toward creating database/tables through jdbc
    
    * Delete DatabricksSqlOperations.java
    
    * revert sqlops
    
    * minor changes

--- airbyte-integrations/connectors/destination-databricks/src/main/java/io/airbyte/integrations/destination/databricks/DatabricksStreamCopier.java
+++ airbyte-integrations/connectors/destination-databricks/src/main/java/io/airbyte/integrations/destination/databricks/DatabricksStreamCopier.java
@@ -14,6 +14,7 @@ import io.airbyte.integrations.destination.s3.writer.S3WriterFactory;
 import io.airbyte.protocol.models.AirbyteRecordMessage;
 import io.airbyte.protocol.models.AirbyteStream;
 import io.airbyte.protocol.models.ConfiguredAirbyteStream;
+import io.airbyte.protocol.models.DestinationSyncMode;
 import java.sql.Timestamp;
 import java.util.UUID;
 import org.slf4j.Logger;
@@ -31,15 +32,20 @@ public class DatabricksStreamCopier implements StreamCopier {
   private final AmazonS3 s3Client;
   private final S3Config s3Config;
   private final String tmpTableName;
+  private final DestinationSyncMode syncMode;
   private final AirbyteStream stream;
   private final JdbcDatabase db;
+  private final String database;
+  private final String streamName;
   private final ExtendedNameTransformer nameTransformer;
-  private final SqlOperations sqlOperations;
+  private final DatabricksSqlOperations sqlOperations;
   private final S3ParquetWriter parquetWriter;
 
   public DatabricksStreamCopier(String stagingFolder,
+                                DestinationSyncMode syncMode,
                                 String schema,
                                 ConfiguredAirbyteStream configuredStream,
+                                String streamName,
                                 AmazonS3 s3Client,
                                 JdbcDatabase db,
                                 S3Config s3Config,
@@ -48,14 +54,18 @@ public class DatabricksStreamCopier implements StreamCopier {
                                 S3WriterFactory writerFactory,
                                 Timestamp uploadTime) throws Exception {
     this.stream = configuredStream.getStream();
+    this.syncMode = syncMode;
     this.db = db;
+    this.database = schema;
+    this.streamName = streamName;
     this.nameTransformer = nameTransformer;
-    this.sqlOperations = sqlOperations;
-    this.tmpTableName = nameTransformer.getTmpTableName(stream.getName());
+    this.sqlOperations = (DatabricksSqlOperations) sqlOperations;
+    this.tmpTableName = nameTransformer.getTmpTableName(streamName);
     this.s3Client = s3Client;
     this.s3Config = s3Config;
     this.parquetWriter = (S3ParquetWriter) writerFactory
         .create(getS3DestinationConfig(s3Config, stagingFolder), s3Client, configuredStream, uploadTime);
+    LOGGER.info(parquetWriter.parquetSchema.toString());
   }
 
   @Override
@@ -69,28 +79,46 @@ public class DatabricksStreamCopier implements StreamCopier {
   }
 
   @Override
-  public void createTemporaryTable() throws Exception {
+  public void createDestinationSchema() throws Exception {
+    LOGGER.info("Creating database in destination if it doesn't exist: {}", database);
+    sqlOperations.createSchemaIfNotExists(db, database);
+  }
 
+  @Override
+  public void createTemporaryTable() throws Exception {
+    LOGGER.info("Preparing tmp table in destination for stream: {}, database: {}, tmp table name: {}.", streamName, database, tmpTableName);
+    LOGGER.info(parquetWriter.parquetSchema.toString());
+    sqlOperations.createTableIfNotExists(db, database, tmpTableName);
   }
 
   @Override
   public void copyStagingFileToTemporaryTable() throws Exception {
-
+    LOGGER.info("Starting copy to tmp table: {} in destination for stream: {}, database: {}, .", tmpTableName, streamName, database);
+    // TODO: load data sql operation 
+    LOGGER.info("Copy to tmp table {} in destination for stream {} complete.", tmpTableName, streamName);
   }
 
-  @Override
-  public void createDestinationSchema() throws Exception {
-
-  }
 
   @Override
   public String createDestinationTable() throws Exception {
-    return null;
+    var destTableName = nameTransformer.getRawTableName(streamName);
+    LOGGER.info("Preparing table {} in destination.", destTableName);
+    sqlOperations.createTableIfNotExists(db, database, destTableName);
+    LOGGER.info("Table {} in destination prepared.", tmpTableName);
+
+    return destTableName;
   }
 
   @Override
-  public String generateMergeStatement(String destTableName) throws Exception {
-    return null;
+  public String generateMergeStatement(String destTableName) {
+    LOGGER.info("Preparing to merge tmp table {} to dest table: {}, database: {}, in destination.", tmpTableName, destTableName, database);
+    var queries = new StringBuilder();
+    if (syncMode.equals(DestinationSyncMode.OVERWRITE)) {
+      queries.append(sqlOperations.truncateTableQuery(db, database, destTableName));
+      LOGGER.info("Destination OVERWRITE mode detected. Dest table: {}, database: {}, truncated.", destTableName, database);
+    }
+    queries.append(sqlOperations.copyTableQuery(db, database, tmpTableName, destTableName));
+    return queries.toString();
   }
 
   @Override
