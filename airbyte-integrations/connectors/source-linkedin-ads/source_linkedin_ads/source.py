3bb4c2e813530f1b8eab2e761b64798f63d22a9a
Oleksandr Bazarnov <oleksandr.bazarnov@globallogic.com>
2021-09-06 12:43:22 +0300

    formated

--- airbyte-integrations/connectors/source-linkedin-ads/source_linkedin_ads/source.py
+++ airbyte-integrations/connectors/source-linkedin-ads/source_linkedin_ads/source.py
@@ -319,12 +319,12 @@ class AdCreativeAnalytics(AnalyticsStreamMixin):
 
 
 class SourceLinkedinAds(AbstractSource):
-    """ 
-    Abstract Source inheritance, 
-    provides: 
+    """
+    Abstract Source inheritance, provides:
     - implementation for `check` connector's connectivity
     - implementation to call each stream with it's input parameters.
     """
+
     def check_connection(self, logger: AirbyteLogger, config: Mapping[str, Any]) -> Tuple[bool, any]:
         """
         Testing connection availability for the connector.
