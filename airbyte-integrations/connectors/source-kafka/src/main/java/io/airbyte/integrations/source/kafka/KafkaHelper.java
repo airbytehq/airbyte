2cd2bfd8cf1dc454c9260971cb67cdde6de0c3a0
heade <danildubinin2@gmail.com>
2021-09-08 11:16:39 +0300

    new source: kafka

---
+++ airbyte-integrations/connectors/source-kafka/src/main/java/io/airbyte/integrations/source/kafka/KafkaHelper.java
@@ -0,0 +1,45 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2020 Airbyte
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package io.airbyte.integrations.source.kafka;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class KafkaHelper {
+
+  public static Set<String> getTopicsFromPartitionInfo(JsonNode topicPartitionInfo) {
+    String topicPartitions = topicPartitionInfo.get("topic_partitions").asText();
+    String[] topicPartitionsStr = topicPartitions.replaceAll("\\s+", "").split(",");
+    return Arrays.stream(topicPartitionsStr).map(topicPartition -> topicPartition.split(":")[0]).collect(Collectors.toSet());
+  }
+
+  public static Set<String> getTopicsFromTopicPattern(JsonNode topicInfo) {
+    return Collections.singleton(topicInfo.get("topic_pattern").asText());
+  }
+
+}
---
+++ airbyte-integrations/connectors/source-kafka/src/main/java/io/airbyte/integrations/source/kafka/KafkaSourceConfig.java
@@ -0,0 +1,127 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2020 Airbyte
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package io.airbyte.integrations.source.kafka;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.google.common.collect.ImmutableMap;
+import io.airbyte.commons.json.Jsons;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+import org.apache.kafka.clients.CommonClientConfigs;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.TopicPartition;
+import org.apache.kafka.common.config.SaslConfigs;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.connect.json.JsonDeserializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class KafkaSourceConfig {
+
+  protected static final Logger LOGGER = LoggerFactory.getLogger(KafkaSourceConfig.class);
+  private final JsonNode config;
+
+  private KafkaSourceConfig(JsonNode config) {
+    this.config = config;
+  }
+
+  public static KafkaSourceConfig getKafkaSourceConfig(JsonNode config) {
+    return new KafkaSourceConfig(config);
+  }
+
+  private KafkaConsumer<String, JsonNode> buildKafkaConsumer(JsonNode config) {
+    final Map<String, Object> props = new HashMap<>();
+    props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, config.get("bootstrap_servers").asText());
+    props.put(ConsumerConfig.GROUP_ID_CONFIG, config.get("group_id").asText());
+    props.put(ConsumerConfig.MAX_POLL_RECORDS_CONFIG, config.get("max_poll_records").intValue());
+    props.putAll(propertiesByProtocol(config));
+    props.put(ConsumerConfig.CLIENT_ID_CONFIG,
+        config.has("client_id") ? config.get("client_id").asText() : null);
+    props.put(ConsumerConfig.CLIENT_DNS_LOOKUP_CONFIG, config.get("client_dns_lookup").asText());
+    props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, config.get("enable_auto_commit").booleanValue());
+    props.put(ConsumerConfig.AUTO_COMMIT_INTERVAL_MS_CONFIG, config.get("auto_commit_interval_ms").intValue());
+    props.put(ConsumerConfig.RETRY_BACKOFF_MS_CONFIG, config.get("retry_backoff_ms").intValue());
+    props.put(ConsumerConfig.REQUEST_TIMEOUT_MS_CONFIG, config.get("request_timeout_ms").intValue());
+    props.put(ConsumerConfig.RECEIVE_BUFFER_CONFIG, config.get("receive_buffer_bytes").intValue());
+    props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+    props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class.getName());
+
+    final Map<String, Object> filteredProps = props.entrySet().stream()
+        .filter(entry -> entry.getValue() != null && !entry.getValue().toString().isBlank())
+        .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+
+    return new KafkaConsumer<>(filteredProps);
+  }
+
+  private Map<String, Object> propertiesByProtocol(JsonNode config) {
+    JsonNode protocolConfig = config.get("protocol");
+    LOGGER.info("Kafka protocol config: {}", protocolConfig.toString());
+    final KafkaProtocol protocol = KafkaProtocol.valueOf(protocolConfig.get("security_protocol").asText().toUpperCase());
+    final ImmutableMap.Builder<String, Object> builder = ImmutableMap.<String, Object>builder()
+        .put(CommonClientConfigs.SECURITY_PROTOCOL_CONFIG, protocol.toString());
+
+    switch (protocol) {
+      case PLAINTEXT -> {}
+      case SASL_SSL, SASL_PLAINTEXT -> {
+        builder.put(SaslConfigs.SASL_JAAS_CONFIG, config.get("sasl_jaas_config").asText());
+        builder.put(SaslConfigs.SASL_MECHANISM, config.get("sasl_mechanism").asText());
+      }
+      default -> throw new RuntimeException("Unexpected Kafka protocol: " + Jsons.serialize(protocol));
+    }
+
+    return builder.build();
+  }
+
+  public KafkaConsumer<String, JsonNode> getConsumer() {
+    KafkaConsumer<String, JsonNode> consumer = buildKafkaConsumer(config);
+
+    JsonNode subscription = config.get("subscription");
+    LOGGER.info("Kafka subscribe method: {}", subscription.toString());
+    switch (subscription.get("subscription_type").asText()) {
+      case "subscribe" -> consumer.subscribe(Pattern.compile(subscription.get("topic_pattern").asText()));
+      case "assign" -> {
+        String topicPartitions = subscription.get("topic_partitions").asText();
+        String[] topicPartitionsStr = topicPartitions.replaceAll("\\s+", "").split(",");
+        List<TopicPartition> topicPartitionList = Arrays.stream(topicPartitionsStr).map(topicPartition -> {
+          String[] pair = topicPartition.split(":");
+          return new TopicPartition(pair[0], Integer.parseInt(pair[1]));
+        }).collect(Collectors.toList());
+        LOGGER.info("Topic-partition list: {}", topicPartitionList);
+        consumer.assign(topicPartitionList);
+      }
+    }
+    return consumer;
+  }
+
+  public KafkaConsumer<String, JsonNode> getTestConsumer() {
+    return buildKafkaConsumer(config);
+  }
+
+}
