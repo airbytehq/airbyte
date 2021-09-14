d0065eac9f5b1a5afeb2e1fbf0c065aef2138645
Christophe Duong <christophe.duong@gmail.com>
2021-09-14 13:47:51 +0200

    Regenerate and check in integration tests files for normalization (#6050)

--- airbyte-integrations/bases/base-normalization/integration_tests/normalization_test_output/postgres/test_primary_key_streams/final/airbyte_tables/test_normalization/unnest_alias_children_owner.sql
+++ airbyte-integrations/bases/base-normalization/integration_tests/normalization_test_output/postgres/test_primary_key_streams/final/airbyte_tables/test_normalization/unnest_alias_children_owner.sql
@@ -3,12 +3,51 @@
   create  table "postgres".test_normalization."unnest_alias_children_owner__dbt_tmp"
   as (
     
--- Final base SQL model
+with __dbt__CTE__unnest_alias_children_owner_ab1 as (
+
+-- SQL model to parse JSON blob stored in a single column and extract into separated field columns as described by the JSON Schema
+select
+    _airbyte_children_hashid,
+    jsonb_extract_path_text("owner", 'owner_id') as owner_id,
+    _airbyte_emitted_at
+from "postgres".test_normalization."unnest_alias_children" as table_alias
+where "owner" is not null
+-- owner at unnest_alias/children/owner
+),  __dbt__CTE__unnest_alias_children_owner_ab2 as (
+
+-- SQL model to cast each column to its adequate SQL type converted from the JSON schema type
+select
+    _airbyte_children_hashid,
+    cast(owner_id as 
+    bigint
+) as owner_id,
+    _airbyte_emitted_at
+from __dbt__CTE__unnest_alias_children_owner_ab1
+-- owner at unnest_alias/children/owner
+),  __dbt__CTE__unnest_alias_children_owner_ab3 as (
+
+-- SQL model to build a hash column based on the values of this record
+select
+    md5(cast(
+    
+    coalesce(cast(_airbyte_children_hashid as 
+    varchar
+), '') || '-' || coalesce(cast(owner_id as 
+    varchar
+), '')
+
+ as 
+    varchar
+)) as _airbyte_owner_hashid,
+    tmp.*
+from __dbt__CTE__unnest_alias_children_owner_ab2 tmp
+-- owner at unnest_alias/children/owner
+)-- Final base SQL model
 select
     _airbyte_children_hashid,
     owner_id,
     _airbyte_emitted_at,
     _airbyte_owner_hashid
-from "postgres"._airbyte_test_normalization."unnest_alias_children_owner_ab3"
+from __dbt__CTE__unnest_alias_children_owner_ab3
 -- owner at unnest_alias/children/owner from "postgres".test_normalization."unnest_alias_children"
   );
