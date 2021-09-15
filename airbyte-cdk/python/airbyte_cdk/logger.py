c51d7b2e0937b639a2c7bbe93cf70f945f719275
Vitalii Vdovenko <vitalii.vdovenko@zazmic.com>
2021-09-15 16:36:42 +0300

    CDK - using native logger(fix exception and add trace)

--- airbyte-cdk/python/airbyte_cdk/logger.py
+++ airbyte-cdk/python/airbyte_cdk/logger.py
@@ -29,10 +29,14 @@ import traceback
 from airbyte_cdk.models import AirbyteLogMessage, AirbyteMessage
 
 
+TRACE_LEVEL_NUM = 5
+
+
 def get_logger():
     logging.setLoggerClass(AirbyteNativeLogger)
+    logging.addLevelName(TRACE_LEVEL_NUM, "TRACE")
     logger = logging.getLogger('airbyte.source.zabbix')
-    logger.setLevel(logging.DEBUG)
+    logger.setLevel(TRACE_LEVEL_NUM)
     handler = logging.StreamHandler(stream=sys.stdout)
     handler.setFormatter(AirbyteLogFormatter())
     logger.addHandler(handler)
@@ -49,18 +53,26 @@ class AirbyteLogFormatter(logging.Formatter):
 class AirbyteNativeLogger(logging.Logger):
     def __init__(self, name):
         logging.Logger.__init__(self, name)
-        self.valid_log_types = ["FATAL", "ERROR", "WARN", "INFO", "DEBUG", "TRACE"]
+        self.valid_log_types = {"FATAL": 50, "ERROR": 40, "WARN": 30, "INFO": 20, "DEBUG": 10, "TRACE": 5}
 
-    def log_by_prefix(self, message, default_level):
-        split_line = message.split()
+    def log_by_prefix(self, msg, default_level):
+        split_line = msg.split()
         first_word = next(iter(split_line), None)
         if first_word in self.valid_log_types:
-            log_level = first_word
+            log_level = self.valid_log_types.get(first_word)
             rendered_message = " ".join(split_line[1:])
         else:
-            log_level = default_level
-            rendered_message = message
+            log_level = self.valid_log_types.get(default_level, 20)
+            rendered_message = msg
         self.log(log_level, rendered_message)
+
+    def exception(self, msg, *args, exc_info=True, **kwargs):
+        msg = f"{msg}\n{traceback.format_exc()}"
+        self._log(logging.ERROR, msg, args, **kwargs)
+
+    def trace(self, msg, *args, **kwargs):
+        if self.isEnabledFor(TRACE_LEVEL_NUM):
+            self._log(TRACE_LEVEL_NUM, msg, args, **kwargs)
 
 
 class AirbyteLogger:
