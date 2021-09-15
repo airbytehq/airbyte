1b83934af76f7ab8bc0daddfdd8efe40a49f1291
Brian Muita <bgm@69mb.org>
2021-09-15 20:58:15 +0300

    Source BigQuery: Fixed formatting
    * Fixes from `./gradlew format` run

--- airbyte-integrations/connectors/source-bigquery/src/test/java/io/airbyte/integrations/source/bigquery/BigQuerySourceTest.java
+++ airbyte-integrations/connectors/source-bigquery/src/test/java/io/airbyte/integrations/source/bigquery/BigQuerySourceTest.java
@@ -24,22 +24,22 @@
 
 package io.airbyte.integrations.source.bigquery;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import java.io.IOException;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import io.airbyte.commons.json.Jsons;
 import io.airbyte.commons.resources.MoreResources;
+import java.io.IOException;
 import org.junit.jupiter.api.Test;
 
 class BigQuerySourceTest {
 
   @Test
   public void testEmptyDatasetIdInConfig() throws IOException {
-      JsonNode configJson = Jsons.deserialize(MoreResources.readResource("test_config_empty_datasetid.json"));
-      JsonNode dbConfig = new BigQuerySource().toDatabaseConfig(configJson);
-      assertTrue(dbConfig.get(BigQuerySource.CONFIG_DATASET_ID).isEmpty());
+    JsonNode configJson = Jsons.deserialize(MoreResources.readResource("test_config_empty_datasetid.json"));
+    JsonNode dbConfig = new BigQuerySource().toDatabaseConfig(configJson);
+    assertTrue(dbConfig.get(BigQuerySource.CONFIG_DATASET_ID).isEmpty());
   }
 
   @Test
