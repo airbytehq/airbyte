ed2ba72e8b778058b344b327ccf76aac71adafde
Vadym <vege1wgw@gmail.com>
2021-09-15 19:23:31 +0300

    🎉 CDK: Add requests native authenticator support (#5731)
    * Add requests native auth class
    
    * Update init file.
    Update type annotations.
    Bump version.
    
    * Update TokenAuthenticator implementation.
    Update Oauth2Authenticator implemetation.
    Add CHANGELOG.md record.
    
    * Update Oauth2Authenticator default value setting.
    Update CHANGELOG.md
    
    * Add requests native authenticator tests
    
    * Add CDK requests native __call__ method tests.
    Update CHANGELOG.md
    
    * Add outdated auth deprication messages
    
    * Update requests native auth __call__ method tests
    
    * Bump CDK version to 0.1.20

--- airbyte-cdk/python/airbyte_cdk/sources/streams/http/requests_native_auth/token.py
+++ airbyte-cdk/python/airbyte_cdk/sources/streams/http/requests_native_auth/token.py
@@ -22,33 +22,38 @@
 # SOFTWARE.
 #
 
-
 from itertools import cycle
 from typing import Any, List, Mapping
 
-from deprecated import deprecated
-
-from .core import HttpAuthenticator
+from requests.auth import AuthBase
 
 
-@deprecated(version="0.1.20", reason="Use airbyte_cdk.sources.streams.http.requests_native_auth.TokenAuthenticator instead")
-class TokenAuthenticator(HttpAuthenticator):
-    def __init__(self, token: str, auth_method: str = "Bearer", auth_header: str = "Authorization"):
-        self.auth_method = auth_method
-        self.auth_header = auth_header
-        self._token = token
+class MultipleTokenAuthenticator(AuthBase):
+    """
+    Builds auth header, based on the list of tokens provided.
+    Auth header is changed per each `get_auth_header` call, using each token in cycle.
+    The token is attached to each request via the `auth_header` header.
+    """
 
-    def get_auth_header(self) -> Mapping[str, Any]:
-        return {self.auth_header: f"{self.auth_method} {self._token}"}
-
-
-@deprecated(version="0.1.20", reason="Use airbyte_cdk.sources.streams.http.requests_native_auth.MultipleTokenAuthenticator instead")
-class MultipleTokenAuthenticator(HttpAuthenticator):
     def __init__(self, tokens: List[str], auth_method: str = "Bearer", auth_header: str = "Authorization"):
         self.auth_method = auth_method
         self.auth_header = auth_header
         self._tokens = tokens
         self._tokens_iter = cycle(self._tokens)
 
+    def __call__(self, request):
+        request.headers.update(self.get_auth_header())
+        return request
+
     def get_auth_header(self) -> Mapping[str, Any]:
         return {self.auth_header: f"{self.auth_method} {next(self._tokens_iter)}"}
+
+
+class TokenAuthenticator(MultipleTokenAuthenticator):
+    """
+    Builds auth header, based on the token provided.
+    The token is attached to each request via the `auth_header` header.
+    """
+
+    def __init__(self, token: str, auth_method: str = "Bearer", auth_header: str = "Authorization"):
+        super().__init__([token], auth_method, auth_header)
