95cc15783aabcd05b8e30ef08000d276e6265cdb
jrhizor <me@jaredrhizor.com>
2021-09-17 13:04:12 -0700

    add separate test cases

---
+++ airbyte-config/persistence/src/test/java/io/airbyte/config/persistence/split_secrets/test_cases/SimpleTestCase.java
@@ -0,0 +1,36 @@
+package io.airbyte.config.persistence.split_secrets.test_cases;
+
+import io.airbyte.config.persistence.split_secrets.SecretCoordinate;
+import io.airbyte.config.persistence.split_secrets.SecretPersistence;
+import io.airbyte.config.persistence.split_secrets.SecretsHelpersTest;
+import io.airbyte.config.persistence.split_secrets.SecretsTestCase;
+
+import java.util.Map;
+import java.util.function.Consumer;
+
+public class SimpleTestCase implements SecretsTestCase {
+
+    @Override
+    public String getName() {
+        return "simple";
+    }
+
+    @Override
+    public Map<SecretCoordinate, String> getFirstSecretMap() {
+        return Map.of(
+                new SecretCoordinate("workspace_" + SecretsHelpersTest.WORKSPACE_ID + "_secret_" + SecretsHelpersTest.UUIDS.get(0), 1), "hunter1");
+    }
+
+    @Override
+    public Map<SecretCoordinate, String> getSecondSecretMap() {
+        return Map.of(
+                new SecretCoordinate("workspace_" + SecretsHelpersTest.WORKSPACE_ID + "_secret_" + SecretsHelpersTest.UUIDS.get(0), 2), "hunter1");
+    }
+
+    @Override
+    public Consumer<SecretPersistence> getPersistenceUpdater() {
+        return secretPersistence -> {
+            secretPersistence.write(new SecretCoordinate("workspace_" + SecretsHelpersTest.WORKSPACE_ID + "_secret_" + SecretsHelpersTest.UUIDS.get(0), 1), "hunter1");
+        };
+    }
+}
