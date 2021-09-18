51fadf2b9b74f375b0c52e6b6896a0c548cb44b9
Davin Chia <davinchia@gmail.com>
2021-09-18 10:21:04 +0800

    🐛 Airbyte Kube will now error on start up if incorrect logs configuration is passed in. Fix scheduler counting bug. (#6188)
    * Move the cloud client create call so this errors out on app start up instead of when they the log file is retrieved.
    
    * Add tests.
    
    * Fix code that was never being called.

--- airbyte-config/models/src/test/java/io/airbyte/config/helpers/CloudLogsClientTest.java
+++ airbyte-config/models/src/test/java/io/airbyte/config/helpers/CloudLogsClientTest.java
@@ -25,11 +25,59 @@
 package io.airbyte.config.helpers;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
-public class CloudLogsTest {
+public class CloudLogsClientTest {
+
+  @Nested
+  class CloudLogClientMissingConfiguration {
+
+    @Test
+    public void testMinio() {
+      var configs = Mockito.mock(LogConfigs.class);
+      // Mising bucket.
+      Mockito.when(configs.getS3MinioEndpoint()).thenReturn("minio-endpoint");
+      Mockito.when(configs.getAwsAccessKey()).thenReturn("access-key");
+      Mockito.when(configs.getAwsSecretAccessKey()).thenReturn("access-key-secret");
+      Mockito.when(configs.getS3LogBucket()).thenReturn("");
+      Mockito.when(configs.getS3LogBucketRegion()).thenReturn("");
+
+      assertThrows(RuntimeException.class, () -> CloudLogs.createCloudLogClient(configs));
+    }
+
+    @Test
+    public void testAws() {
+      var configs = Mockito.mock(LogConfigs.class);
+      // Missing bucket and access key.
+      Mockito.when(configs.getS3MinioEndpoint()).thenReturn("");
+      Mockito.when(configs.getAwsAccessKey()).thenReturn("");
+      Mockito.when(configs.getAwsSecretAccessKey()).thenReturn("access-key-secret");
+      Mockito.when(configs.getS3LogBucket()).thenReturn("");
+      Mockito.when(configs.getS3LogBucketRegion()).thenReturn("");
+
+      assertThrows(RuntimeException.class, () -> CloudLogs.createCloudLogClient(configs));
+    }
+
+    @Test
+    public void testGcs() {
+      var configs = Mockito.mock(LogConfigs.class);
+      Mockito.when(configs.getAwsAccessKey()).thenReturn("");
+      Mockito.when(configs.getAwsSecretAccessKey()).thenReturn("");
+      Mockito.when(configs.getS3LogBucket()).thenReturn("");
+      Mockito.when(configs.getS3LogBucketRegion()).thenReturn("");
+
+      // Missing bucket.
+      Mockito.when(configs.getGcpStorageBucket()).thenReturn("");
+      Mockito.when(configs.getGoogleApplicationCredentials()).thenReturn("path/to/google/secret");
+
+      assertThrows(RuntimeException.class, () -> CloudLogs.createCloudLogClient(configs));
+    }
+
+  }
 
   @Test
   public void createCloudLogClientTestMinio() {
