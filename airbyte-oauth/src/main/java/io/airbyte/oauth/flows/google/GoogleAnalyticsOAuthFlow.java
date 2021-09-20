7222214fa2fa40e3a61f7d2aeecae2b6a16fd356
Christophe Duong <christophe.duong@gmail.com>
2021-09-20 18:55:36 +0200

    Change oauth implementation with nested auth_mechanism for Google Analytics v4

--- airbyte-oauth/src/main/java/io/airbyte/oauth/flows/google/GoogleAnalyticsOAuthFlow.java
+++ airbyte-oauth/src/main/java/io/airbyte/oauth/flows/google/GoogleAnalyticsOAuthFlow.java
@@ -24,9 +24,13 @@
 
 package io.airbyte.oauth.flows.google;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import com.google.common.annotations.VisibleForTesting;
+import com.google.common.base.Preconditions;
 import io.airbyte.config.persistence.ConfigRepository;
+import java.io.IOException;
 import java.net.http.HttpClient;
+import java.util.Map;
 import java.util.function.Supplier;
 
 public class GoogleAnalyticsOAuthFlow extends GoogleOAuthFlow {
@@ -47,4 +51,23 @@ public class GoogleAnalyticsOAuthFlow extends GoogleOAuthFlow {
     return SCOPE_URL;
   }
 
+  @Override
+  protected String getClientIdUnsafe(JsonNode config) {
+    // the config object containing client ID and secret is nested inside the "credentials" object
+    Preconditions.checkArgument(config.hasNonNull("auth_mechanism"));
+    return super.getClientIdUnsafe(config.get("auth_mechanism"));
+  }
+
+  @Override
+  protected String getClientSecretUnsafe(JsonNode config) {
+    // the config object containing client ID and secret is nested inside the "credentials" object
+    Preconditions.checkArgument(config.hasNonNull("auth_mechanism"));
+    return super.getClientSecretUnsafe(config.get("auth_mechanism"));
+  }
+
+  @Override
+  protected Map<String, Object> extractRefreshToken(JsonNode data) throws IOException {
+    // the config object containing refresh token is nested inside the "auth_mechanism" object
+    return Map.of("auth_mechanism", super.extractRefreshToken(data));
+  }
 }
