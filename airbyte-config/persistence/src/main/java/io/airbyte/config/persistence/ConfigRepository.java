3c54d88e058393d3df0b345b3c140bee63499a0d
Christophe Duong <christophe.duong@gmail.com>
2021-09-20 20:11:29 +0200

    Update instance wide oauth parameters (#6309)
    * Update instance wide oauth parameters
    
    * Add tests

--- airbyte-config/persistence/src/main/java/io/airbyte/config/persistence/ConfigRepository.java
+++ airbyte-config/persistence/src/main/java/io/airbyte/config/persistence/ConfigRepository.java
@@ -44,6 +44,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import java.util.Optional;
 import java.util.UUID;
 import java.util.stream.Stream;
@@ -230,6 +231,17 @@ public class ConfigRepository {
     return persistence.getConfig(ConfigSchema.SOURCE_OAUTH_PARAM, SourceOAuthParameterId.toString(), SourceOAuthParameter.class);
   }
 
+  public Optional<SourceOAuthParameter> getSourceOAuthParamByDefinitionIdOptional(final UUID workspaceId, final UUID sourceDefinitionId)
+      throws JsonValidationException, IOException {
+    for (final SourceOAuthParameter oAuthParameter : listSourceOAuthParam()) {
+      if (sourceDefinitionId.equals(oAuthParameter.getSourceDefinitionId()) &&
+          Objects.equals(workspaceId, oAuthParameter.getWorkspaceId())) {
+        return Optional.of(oAuthParameter);
+      }
+    }
+    return Optional.empty();
+  }
+
   public void writeSourceOAuthParam(final SourceOAuthParameter SourceOAuthParameter) throws JsonValidationException, IOException {
     persistence.writeConfig(ConfigSchema.SOURCE_OAUTH_PARAM, SourceOAuthParameter.getOauthParameterId().toString(), SourceOAuthParameter);
   }
@@ -241,6 +253,18 @@ public class ConfigRepository {
   public DestinationOAuthParameter getDestinationOAuthParams(final UUID destinationOAuthParameterId)
       throws JsonValidationException, IOException, ConfigNotFoundException {
     return persistence.getConfig(ConfigSchema.DESTINATION_OAUTH_PARAM, destinationOAuthParameterId.toString(), DestinationOAuthParameter.class);
+  }
+
+  public Optional<DestinationOAuthParameter> getDestinationOAuthParamByDefinitionIdOptional(final UUID workspaceId,
+                                                                                            final UUID destinationDefinitionId)
+      throws JsonValidationException, IOException {
+    for (final DestinationOAuthParameter oAuthParameter : listDestinationOAuthParam()) {
+      if (destinationDefinitionId.equals(oAuthParameter.getDestinationDefinitionId()) &&
+          Objects.equals(workspaceId, oAuthParameter.getWorkspaceId())) {
+        return Optional.of(oAuthParameter);
+      }
+    }
+    return Optional.empty();
   }
 
   public void writeDestinationOAuthParam(final DestinationOAuthParameter destinationOAuthParameter) throws JsonValidationException, IOException {
