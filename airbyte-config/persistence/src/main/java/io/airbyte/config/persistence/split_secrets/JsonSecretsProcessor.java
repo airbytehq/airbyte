1a26fc3a9d216f53cbf0ec89bdeacc7fcb412c8d
jrhizor <me@jaredrhizor.com>
2021-09-21 09:59:25 -0700

    remove comment typo

--- airbyte-config/persistence/src/main/java/io/airbyte/config/persistence/split_secrets/JsonSecretsProcessor.java
+++ airbyte-config/persistence/src/main/java/io/airbyte/config/persistence/split_secrets/JsonSecretsProcessor.java
@@ -49,7 +49,7 @@ public class JsonSecretsProcessor {
    * Returns a copy of the input object wherein any fields annotated with "airbyte_secret" in the
    * input schema are masked.
    * <p>
-   * f This method masks secrets both at the top level of the configuration object and in nested
+   * This method masks secrets both at the top level of the configuration object and in nested
    * properties in a oneOf.
    *
    * @param schema Schema containing secret annotations
