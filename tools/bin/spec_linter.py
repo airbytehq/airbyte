a1e33d96b5ed511c74702f0b037dfaa80e31eba2
yaroslav-dudar <flayingfog@gmail.com>
2021-09-21 20:55:14 +0300

    fixe oneof paring; upd errors logging

--- tools/bin/spec_linter.py
+++ tools/bin/spec_linter.py
@@ -1,9 +1,26 @@
+"""
+This script is responsible for connectors spec.json file validation.
+
+Input:
+List of spec files
+
+Output:
+exit code 0 - check is success
+exit code 1 - check failed for at least one spec file
+
+How spec file validation works:
+1. read spec file and serialize it as python dict object
+2. get properties field from spec object
+3. check if all fields from FIELDS_TO_CHECK exist in each property
+4. if field has oneOf attribute - fetch all subobjects and for each of them goto 2. step
+"""
+
 import logging
 import sys
 import os
 import json
 from collections import defaultdict
-from typing import Any, List, Mapping, Union
+from typing import Any, List, Mapping
 
 # errors counter
 ERRORS = defaultdict(int)
@@ -13,47 +30,65 @@ FIELDS_TO_CHECK = {"title", "description"}
 logging.basicConfig(format="%(message)s")
 
 
-def parse_spec(spec_path: str):
+def read_spec_file(spec_path: str):
     with open(spec_path) as json_file:
         try:
-            properties = json.load(json_file)['connectionSpecification']['properties']
+            root_schema = json.load(json_file)['connectionSpecification']['properties']
         except KeyError:
             logging.error(f"\033[1m{spec_path}\033[0m: Couldn't find properties in connector spec.json")
             ERRORS[spec_path] += 1
             return
 
-    return validate_spec_field(spec_path, properties)
+    validate_schema(spec_path, root_schema)
 
 
-def validate_spec_field(
+def validate_schema(
     spec_path: str,
-    spec_object: Union[Mapping[str, Any], List],
+    schema: Mapping[str, Any],
     parent_fields: List[str] = None
 ):
     parent_fields = parent_fields if parent_fields else []
-    if isinstance(spec_object, dict):
-        for field_name, field_object in spec_object.items():
-            if not FIELDS_TO_CHECK.issubset(field_object):
-                full_field_name = get_full_field_name(field_name, parent_fields)
-                logging.error(f"\033[1m{spec_path}\033[0m: Check failed for field \x1b[31;1m{full_field_name}\033[0m")
-                ERRORS[spec_path] += 1
-                validate_spec_field()
-    elif isinstance(spec_object, list)
+    for field_name, field_schema in schema.items():
+        if not FIELDS_TO_CHECK.issubset(field_schema):
+            full_field_name = get_full_field_name(field_name, parent_fields)
+            logging.error(
+                f"\033[1m{spec_path}\033[0m: Check failed for field \x1b[31;1m{full_field_name}\033[0m"
+            )
+            ERRORS[spec_path] += 1
+
+        for index, oneof_schema in enumerate(fetch_oneof_schemas(field_schema)):
+            validate_schema(
+                spec_path,
+                oneof_schema['properties'],
+                parent_fields + [field_name, str(index)]
+            )
+
+
+def fetch_oneof_schemas(schema: Mapping[str, Any]) -> List[Mapping[str, Any]]:
+    """
+    Finds sub-objects in oneOf field
+    """
+    if schema.get("oneOf") and schema["type"] == "object":
+        return [spec for spec in schema["oneOf"] if spec.get('properties')]
+    return []
 
 
 def get_full_field_name(field_name: str, parent_fields: List[str] = None):
+    """
+    Returns full path to field.
+    e.g. root.middle.child, root.oneof.1.attr
+    """
     return '.'.join(parent_fields + [field_name]) if parent_fields else field_name
 
 
 if __name__ == "__main__":
-    # read list of spec.json files
     spec_files = sys.argv[1:]
-    print(f'Spec files found: {len(spec_files)}')
+    logging.warning(f'Spec files found: {len(spec_files)}')
 
     project_root_dir = os.path.abspath(os.curdir)
     spec_files = [os.path.join(project_root_dir, f) for f in spec_files]
     for file_path in spec_files:
-        parse_spec(file_path)
+        read_spec_file(file_path)
 
     if ERRORS:
         exit(1)
