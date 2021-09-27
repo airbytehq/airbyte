dfd957f2b7a8975d23475c0ce8d5bbb651629e74
Sherif Nada <snadalive@gmail.com>
2021-09-26 23:45:04 -0700

    add tests

--- airbyte-server/src/main/java/io/airbyte/server/converters/OauthModelConverter.java
+++ airbyte-server/src/main/java/io/airbyte/server/converters/OauthModelConverter.java
@@ -27,6 +27,7 @@ package io.airbyte.server.converters;
 import io.airbyte.api.model.AuthSpecification;
 import io.airbyte.api.model.OAuth2Specification;
 import io.airbyte.protocol.models.ConnectorSpecification;
+
 import java.util.Optional;
 
 public class OauthModelConverter {
@@ -41,7 +42,10 @@ public class OauthModelConverter {
     if (incomingAuthSpec.getAuthType() == io.airbyte.protocol.models.AuthSpecification.AuthType.OAUTH_2_0) {
       authSpecification.authType(AuthSpecification.AuthTypeEnum.OAUTH2_0)
           .oauth2Specification(new OAuth2Specification()
-              .oauthFlowInitParameters(incomingAuthSpec.getOauth2Specification().getOauthFlowInitParameters()));
+              .rootObject(incomingAuthSpec.getOauth2Specification().getRootObject())
+              .oauthFlowInitParameters(incomingAuthSpec.getOauth2Specification().getOauthFlowInitParameters())
+              .oauthFlowOutputParameters(incomingAuthSpec.getOauth2Specification().getOauthFlowOutputParameters())
+          );
     }
 
     return Optional.ofNullable(authSpecification);
