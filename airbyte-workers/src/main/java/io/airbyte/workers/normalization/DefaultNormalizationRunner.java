97fce1b56a8e85d04d797fb2bb79430c87962667
cgardens <giardina.charles@gmail.com>
2021-09-27 19:45:02 -0700

    default to using ssl in postgres destinatoin

--- airbyte-workers/src/main/java/io/airbyte/workers/normalization/DefaultNormalizationRunner.java
+++ airbyte-workers/src/main/java/io/airbyte/workers/normalization/DefaultNormalizationRunner.java
@@ -102,6 +102,7 @@ public class DefaultNormalizationRunner implements NormalizationRunner {
                              final String... args)
       throws Exception {
     try {
+      LOGGER.info("Running with normalization version: {}", NORMALIZATION_IMAGE_NAME);
       process = processFactory.create(jobId, attempt, jobRoot, NORMALIZATION_IMAGE_NAME, false, files, null, resourceRequirements, args);
 
       LineGobbler.gobble(process.getInputStream(), LOGGER::info);
