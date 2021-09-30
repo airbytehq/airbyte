343c072c8f7f11136767b2f46ce6624a9225893e
Artem Astapenko <3767150+Jamakase@users.noreply.github.com>
2021-09-30 16:09:32 +0300

    implement oauth in UI (#6385)
    * Implement auth
    
    * Implement auth
    
    * Fix wrong changes
    
    * Place authenticate button in the proper place
    
    * Finish oauth implementation
    
    * Temporary ignore test
    
    * Fix stories
    
    * Fix react-widgets UI
    
    * Remove unnecessary mock
    
    * Add support for oneOf case
    
    * Temporary disable test
    
    * Fix path
    
    * Fix lock file
    
    * Do not flush values on auth
    
    * Apply last auth changes
    
    * Move button within section
    
    * Leave comment about dirty tricks
    
    * Call WebBackend endpoints for create instead of core one

--- airbyte-webapp/src/views/Connector/ServiceForm/index.stories.tsx
+++ airbyte-webapp/src/views/Connector/ServiceForm/index.stories.tsx
@@ -3,20 +3,46 @@ import { ComponentMeta, ComponentStory } from "@storybook/react";
 import ServiceForm from "./ServiceForm";
 import { ContentCard } from "components";
 
+const TempConnector = {
+  name: "Service",
+  documentationUrl: "http://service.com",
+  sourceDefinitionId: "serviceId",
+  dockerRepository: "",
+  dockerImageTag: "",
+  latestDockerImageTag: "",
+  icon: "",
+};
+
 export default {
   title: "Views/ServiceForm",
   component: ServiceForm,
+  parameters: { actions: { argTypesRegex: "^on.*" } },
+  args: {
+    formType: "source",
+    availableServices: [TempConnector],
+  },
 } as ComponentMeta<typeof ServiceForm>;
 
-const Template: ComponentStory<typeof ServiceForm> = (args) => (
-  <ContentCard title="Test">
-    <ServiceForm {...args} />
-  </ContentCard>
-);
+const Template: ComponentStory<typeof ServiceForm> = (args) => {
+  // Hack to allow devs to not specify sourceDefinitionId
+  if (
+    args.selectedConnector &&
+    !(args.selectedConnector as any).sourceDefinitionId
+  ) {
+    (args.selectedConnector as any).sourceDefinitionId = "";
+  }
+  return (
+    <ContentCard title="Test">
+      <ServiceForm {...args} />
+    </ContentCard>
+  );
+};
 
 export const Common = Template.bind({});
 Common.args = {
-  specifications: JSON.parse(`{
+  // @ts-ignore
+  selectedConnector: {
+    connectionSpecification: JSON.parse(`{
     "$schema": "http://json-schema.org/draft-07/schema#",
     "title": "BigQuery Destination Spec",
     "type": "object",
@@ -87,13 +113,15 @@ Common.args = {
       }
     }
   }`),
-  formType: "source",
-  availableServices: [],
+  },
 };
 
 export const Oneof = Template.bind({});
 Oneof.args = {
-  specifications: JSON.parse(`{
+  selectedConnector: {
+    sourceDefinitionId: "",
+    documentationUrl: "",
+    connectionSpecification: JSON.parse(`{
     "$schema": "http://json-schema.org/draft-07/schema#",
     "title": "MSSQL Source Spec",
     "type": "object",
@@ -157,6 +185,5 @@ Oneof.args = {
       }
     }
   }`),
-  formType: "source",
-  availableServices: [],
+  },
 };
