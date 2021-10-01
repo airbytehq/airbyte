d5819414521c11dfb94fcc76a8e2fc5734695115
Oleksandr Bazarnov <oleksandr.bazarnov@globallogic.com>
2021-10-01 15:21:22 +0300

    updated after review, added SQL files for MSSQL [final, models]

--- airbyte-integrations/bases/base-normalization/dbt-project-template/macros/cross_db_utils/surrogate_key.sql
+++ airbyte-integrations/bases/base-normalization/dbt-project-template/macros/cross_db_utils/surrogate_key.sql
@@ -11,12 +11,3 @@
         {%- endfor %}
     )
 {%- endmacro %}
-
-{# MSSQL surrogate_key #}
-{%- macro sqlserver__surrogate_key(field_list) -%}
-    {% set fields = [] %}
-    {%- for field in field_list -%}
-        {% set _ = fields.append("coalesce(cast(" ~ field ~ " as " ~ dbt_utils.type_string() ~ "), '')") %}
-    {%- endfor -%}
-    {{dbt_utils.hash(dbt_utils.concat(fields))}}
-{%- endmacro -%}
