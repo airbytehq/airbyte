f034ea1be17a1996713b871ff14100fe0b866c75
monai <monai@cure.lt>
2021-10-05 16:58:07 +0300

    Implement Items stream

--- airbyte-integrations/connectors/source-tplcentral/source_tplcentral/source.py
+++ airbyte-integrations/connectors/source-tplcentral/source_tplcentral/source.py
@@ -52,15 +52,18 @@ class TplcentralStream(HttpStream, ABC):
         self.facility_id = config.get('facility_id', None)
         self.start_date = config.get('start_date', None)
 
+        self.total_results_field = "TotalResults"
+
     @property
     def page_size(self):
         None
 
     def next_page_token(self, response: requests.Response, **kwargs) -> Optional[Mapping[str, Any]]:
+        print("next_page_token")
         data = response.json()
-        total = data['TotalResults']
+        total = data[self.total_results_field]
 
-        pgsiz = self.page_size or len(data['Summaries'])
+        pgsiz = self.page_size or len(data[self.collection_field])
 
         url = urlparse(response.request.url)
         qs = dict(parse_qsl(url.query))
@@ -82,88 +85,66 @@ class TplcentralStream(HttpStream, ABC):
         return next_page_token
 
     def parse_response(self, response: requests.Response, **kwargs) -> Iterable[Mapping]:
-        return [deep_snake_keys(v) for v in response.json()['Summaries']]
+        return response.json()
 
 
 class StockSummaries(TplcentralStream):
     primary_key = ["facility_id", ["item_identifier", "id"]]
     page_size = 500
+    collection_field = "Summaries"
 
     def path(
         self, stream_state: Mapping[str, Any] = None, stream_slice: Mapping[str, Any] = None, next_page_token: Mapping[str, Any] = None
     ) -> str:
         return "inventory/stocksummaries"
 
+    def parse_response(self, response: requests.Response, **kwargs) -> Iterable[Mapping]:
+        return [deep_snake_keys(v) for v in response.json()['Summaries']]
 
-# Basic incremental stream
+
 class IncrementalTplcentralStream(TplcentralStream, ABC):
-    """
-    TODO fill in details of this class to implement functionality related to incremental syncs for your connector.
-         if you do not need to implement incremental sync for any streams, remove this class.
-    """
-
     # TODO: Fill in to checkpoint stream reads after N records. This prevents re-reading of data if the stream fails for any reason.
-    state_checkpoint_interval = None
+    state_checkpoint_interval = 10
 
     @property
     def cursor_field(self) -> str:
-        """
-        TODO
-        Override to return the cursor field used by this stream e.g: an API entity might always use created_at as the cursor field. This is
-        usually id or date based. This field's presence tells the framework this in an incremental stream. Required for incremental.
-
-        :return str: The name of the cursor field.
-        """
         return []
 
     def get_updated_state(self, current_stream_state: MutableMapping[str, Any], latest_record: Mapping[str, Any]) -> Mapping[str, Any]:
-        """
-        Override to determine the latest state after reading the latest record. This typically compared the cursor_field from the latest record and
-        the current state and picks the 'most' recent cursor. This is how a stream's state is determined. Required for incremental.
-        """
-        return {}
+        return {
+            "last_modified_date": latest_record.get('ReadOnly', {}).get('LastModifiedDate', None)
+        }
 
 
-class Employees(IncrementalTplcentralStream):
-    """
-    TODO: Change class name to match the table/data source this stream corresponds to.
-    """
-
-    # TODO: Fill in the cursor_field. Required.
-    cursor_field = "start_date"
-
-    # TODO: Fill in the primary key. Required. This is usually a unique field in the stream, like an ID or a timestamp.
-    primary_key = "employee_id"
+class Items(IncrementalTplcentralStream):
+    cursor_field = "last_modified_date"
+    primary_key = "id"
+    collection_field = "ResourceList"
 
     def path(self, **kwargs) -> str:
-        """
-        TODO: Override this method to define the path this stream corresponds to. E.g. if the url is https://example-api.com/v1/employees then this should
-        return "single". Required.
-        """
-        return "employees"
+        return f"customers/{self.customer_id}/items"
 
     def stream_slices(self, stream_state: Mapping[str, Any] = None, **kwargs) -> Iterable[Optional[Mapping[str, any]]]:
-        """
-        TODO: Optionally override this method to define this stream's slices. If slicing is not needed, delete this method.
+        return [{
+            "last_modified_date": "2021-08-01"
+        }]
 
-        Slices control when state is saved. Specifically, state is saved after a slice has been fully read.
-        This is useful if the API offers reads by groups or filters, and can be paired with the state object to make reads efficient. See the "concepts"
-        section of the docs for more information.
+    def request_params(
+        self, stream_state: Mapping[str, Any], stream_slice: Mapping[str, any] = None, next_page_token: Mapping[str, Any] = None
+    ) -> MutableMapping[str, Any]:
+        params = next_page_token.copy() if isinstance(next_page_token, dict) else {}
 
-        The function is called before reading any records in a stream. It returns an Iterable of dicts, each containing the
-        necessary data to craft a request for a slice. The stream state is usually referenced to determine what slices need to be created.
-        This means that data in a slice is usually closely related to a stream's cursor_field and stream_state.
+        if self.cursor_field:
+            cursor = stream_slice.get(self.cursor_field, None)
+            if cursor:
+                params.update({
+                    "rql": f"ReadOnly.lastModifiedDate=ge={cursor}",
+                })
 
-        An HTTP request is made for each returned slice. The same slice can be accessed in the path, request_params and request_header functions to help
-        craft that specific request.
+        return params
 
-        For example, if https://example-api.com/v1/employees offers a date query params that returns data for that particular day, one way to implement
-        this would be to consult the stream state object for the last synced date, then return a slice containing each date from the last synced date
-        till now. The request_params function would then grab the date from the stream_slice and make it part of the request by injecting it into
-        the date query param.
-        """
-        # raise NotImplementedError("Implement stream slices or delete this method!")
-        return {}
+    def parse_response(self, response: requests.Response, **kwargs) -> Iterable[Mapping]:
+        return response.json()['ResourceList']
 
 
 class TplcentralAuthenticator(Oauth2Authenticator):
@@ -215,7 +196,6 @@ class TplcentralAuthenticator(Oauth2Authenticator):
             raise Exception(f"Error while refreshing access token: {e}") from e
 
 
-# Source
 class SourceTplcentral(AbstractSource):
     def _auth(self, config):
         return TplcentralAuthenticator(
@@ -239,5 +219,5 @@ class SourceTplcentral(AbstractSource):
 
         return [
             StockSummaries(config),
-            Employees(config),
+            Items(config),
         ]
