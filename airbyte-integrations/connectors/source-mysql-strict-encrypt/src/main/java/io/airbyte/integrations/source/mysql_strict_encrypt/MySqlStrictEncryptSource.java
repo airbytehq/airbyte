b31da802cdb91ca0ecc95269433a9a7b7fcc441d
andriikorotkov <88329385+andriikorotkov@users.noreply.github.com>
2021-10-05 09:47:10 +0300

    Exposing SSL-only version of MySQL Source (#6605)
    * Source MySQL - added option to connect using ssl
    
    * Source MySQL - updated changelog
    
    * dummy commit
    
    * Updated PR as per comment
    
    * added source mysql strict encrypt
    
    * updated code style for MySQL source
    
    * updated code style for MySQL source
    
    * fixed remarks
    
    * fixed code style
    
    * fixed remarks
    
    Co-authored-by: ievgeniit <ievgeniit>
    Co-authored-by: ievgeniit <etsybaev@gmail.com>

---
+++ airbyte-integrations/connectors/source-mysql-strict-encrypt/src/main/java/io/airbyte/integrations/source/mysql_strict_encrypt/MySqlStrictEncryptSource.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright (c) 2021 Airbyte, Inc., all rights reserved.
+ */
+
+package io.airbyte.integrations.source.mysql_strict_encrypt;
+
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import io.airbyte.commons.json.Jsons;
+import io.airbyte.integrations.base.IntegrationRunner;
+import io.airbyte.integrations.base.Source;
+import io.airbyte.integrations.base.spec_modification.SpecModifyingSource;
+import io.airbyte.integrations.source.mysql.MySqlSource;
+import io.airbyte.protocol.models.ConnectorSpecification;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Secure-only version of MySQL source that can be used in the Airbyte cloud.
+ * This connector inherently prevent certain insecure connections such as connecting to
+ * a database over the public internet without encryption.
+ */
+public class MySqlStrictEncryptSource extends SpecModifyingSource implements Source {
+
+  private static final Logger LOGGER = LoggerFactory.getLogger(MySqlStrictEncryptSource.class);
+
+  MySqlStrictEncryptSource() {
+    super(MySqlSource.sshWrappedSource());
+  }
+
+  @Override
+  public ConnectorSpecification modifySpec(final ConnectorSpecification originalSpec) {
+    final ConnectorSpecification spec = Jsons.clone(originalSpec);
+    // SSL property should be enabled by default for secure versions of connectors
+    // that can be used in the Airbyte cloud. User should not be able to change this property.
+    ((ObjectNode) spec.getConnectionSpecification().get("properties")).remove("ssl");
+    return spec;
+  }
+
+  public static void main(String[] args) throws Exception {
+    final Source source = new MySqlStrictEncryptSource();
+    LOGGER.info("starting source: {}", MySqlStrictEncryptSource.class);
+    new IntegrationRunner(source).run(args);
+    LOGGER.info("completed source: {}", MySqlStrictEncryptSource.class);
+  }
+
+}
