d1f31a5773df02c6f1db6d0d0f75d7715a91c09b
jrhizor <me@jaredrhizor.com>
2021-10-06 18:16:16 -0700

    add dsn to window type

--- airbyte-webapp/src/config/types.ts
+++ airbyte-webapp/src/config/types.ts
@@ -17,6 +17,7 @@ declare global {
     FIREBASE_API_KEY?: string;
     FIREBASE_AUTH_DOMAIN?: string;
     CLOUD_API_URL?: string;
+    REACT_APP_SENTRY_DNS?: string;
 
     analytics: SegmentAnalytics;
     _API_URL: string;
