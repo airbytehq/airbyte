3bf381089d5fb813f0c140bf98922f9bd7deffa9
Jared Rhizor <jared@dataline.io>
2021-10-08 12:56:31 -0700

    add rest of sentry config (#6902)
    * add rest of sentry config
    
    * fall back on dev for the tag

--- airbyte-webapp/src/config/types.ts
+++ airbyte-webapp/src/config/types.ts
@@ -18,6 +18,7 @@ declare global {
     FIREBASE_AUTH_DOMAIN?: string;
     CLOUD_API_URL?: string;
     REACT_APP_SENTRY_DSN?: string;
+    REACT_APP_WEBAPP_TAG?: string;
     REACT_APP_INTERCOM_APP_ID?: string;
 
     analytics: SegmentAnalytics;
