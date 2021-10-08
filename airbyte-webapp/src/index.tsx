8c44a765c2c299ff5c4e93dd3521272a703122fd
Jared Rhizor <jared@dataline.io>
2021-10-08 08:53:20 -0700

    use window vars for sentry and correct env name (#6802)
    * fall back to window var for sentry initialization
    
    * correct naming
    
    * add dsn to window type
    
    * fix naming

--- airbyte-webapp/src/index.tsx
+++ airbyte-webapp/src/index.tsx
@@ -4,7 +4,9 @@ import * as Sentry from "@sentry/react";
 
 // We do not follow default config approach that we follow as we want to init
 // sentry asap
-Sentry.init({ dsn: process.env.REACT_APP_SENTRY_DNS });
+Sentry.init({
+  dsn: process.env.REACT_APP_SENTRY_DSN || window.REACT_APP_SENTRY_DSN,
+});
 
 const CloudApp = lazy(() => import(`packages/cloud/App`));
 const App = lazy(() => import(`./App`));
