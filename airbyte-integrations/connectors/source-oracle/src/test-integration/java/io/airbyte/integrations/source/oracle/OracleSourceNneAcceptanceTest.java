2c1f9a0ede275307aaeb1393e50ca0e5f818adb4
VitaliiMaltsev <39538064+VitaliiMaltsev@users.noreply.github.com>
2021-10-08 11:38:19 +0300

    🎉 Oracle Source: Add encryption options (#6616)
    * OracleSource add encryption
    
    * add tests
    
    * add tests
    
    * remove debug option from Dockerfile
    
    * add docs and fix unit tests
    
    * remove System.out.println
    
    * fix checkstyle | remove 'encryption' from required fields
    
    * update timeout to 30 seconds
    
    * bump version to 0.3.6
    
    * bump version to 0.3.7
    
    Co-authored-by: vmaltsev <vitalii.maltsev@globallogic.com>

---
+++ airbyte-integrations/connectors/source-oracle/src/test-integration/java/io/airbyte/integrations/source/oracle/OracleSourceNneAcceptanceTest.java
@@ -0,0 +1,98 @@
+/*
+ * Copyright (c) 2021 Airbyte, Inc., all rights reserved.
+ */
+
+package io.airbyte.integrations.source.oracle;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.google.common.collect.ImmutableMap;
+import io.airbyte.commons.json.Jsons;
+import io.airbyte.db.Databases;
+import io.airbyte.db.jdbc.JdbcDatabase;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.stream.Collectors;
+import org.junit.jupiter.api.Test;
+
+public class OracleSourceNneAcceptanceTest extends OracleSourceAcceptanceTest {
+
+  @Test
+  public void testEncrytion() throws SQLException {
+    final JsonNode clone = Jsons.clone(getConfig());
+    ((ObjectNode) clone).put("encryption", Jsons.jsonNode(ImmutableMap.builder()
+        .put("encryption_method", "client_nne")
+        .put("encryption_algorithm", "3DES168")
+        .build()));
+
+    String algorithm = clone.get("encryption")
+        .get("encryption_algorithm").asText();
+
+    JdbcDatabase database = Databases.createJdbcDatabase(clone.get("username").asText(),
+        clone.get("password").asText(),
+        String.format("jdbc:oracle:thin:@//%s:%s/%s",
+            clone.get("host").asText(),
+            clone.get("port").asText(),
+            clone.get("sid").asText()),
+        "oracle.jdbc.driver.OracleDriver",
+        "oracle.net.encryption_client=REQUIRED;" +
+            "oracle.net.encryption_types_client=( "
+            + algorithm + " )");
+
+    String network_service_banner = "select network_service_banner from v$session_connect_info where sid in (select distinct sid from v$mystat)";
+    List<JsonNode> collect = database.query(network_service_banner).collect(Collectors.toList());
+
+    assertTrue(collect.get(2).get("NETWORK_SERVICE_BANNER").asText()
+        .contains("Oracle Advanced Security: " + algorithm + " encryption"));
+  }
+
+  @Test
+  public void testNoneEncrytion() throws SQLException {
+
+    JdbcDatabase database = Databases.createJdbcDatabase(config.get("username").asText(),
+        config.get("password").asText(),
+        String.format("jdbc:oracle:thin:@//%s:%s/%s",
+            config.get("host").asText(),
+            config.get("port").asText(),
+            config.get("sid").asText()),
+        "oracle.jdbc.driver.OracleDriver");
+
+    String network_service_banner = "select network_service_banner from v$session_connect_info where sid in (select distinct sid from v$mystat)";
+    List<JsonNode> collect = database.query(network_service_banner).collect(Collectors.toList());
+
+    assertTrue(collect.get(1).get("NETWORK_SERVICE_BANNER").asText()
+        .contains("Oracle Advanced Security: encryption"));
+  }
+
+  @Test
+  public void testCheckProtocol() throws SQLException {
+    final JsonNode clone = Jsons.clone(getConfig());
+    ((ObjectNode) clone).put("encryption", Jsons.jsonNode(ImmutableMap.builder()
+        .put("encryption_method", "client_nne")
+        .put("encryption_algorithm", "AES256")
+        .build()));
+
+    String algorithm = clone.get("encryption")
+        .get("encryption_algorithm").asText();
+
+    JdbcDatabase database = Databases.createJdbcDatabase(clone.get("username").asText(),
+        clone.get("password").asText(),
+        String.format("jdbc:oracle:thin:@//%s:%s/%s",
+            clone.get("host").asText(),
+            clone.get("port").asText(),
+            clone.get("sid").asText()),
+        "oracle.jdbc.driver.OracleDriver",
+        "oracle.net.encryption_client=REQUIRED;" +
+            "oracle.net.encryption_types_client=( "
+            + algorithm + " )");
+
+    String network_service_banner = "SELECT sys_context('USERENV', 'NETWORK_PROTOCOL') as network_protocol FROM dual";
+    List<JsonNode> collect = database.query(network_service_banner).collect(Collectors.toList());
+
+    assertEquals("tcp", collect.get(0).get("NETWORK_PROTOCOL").asText());
+  }
+
+}
