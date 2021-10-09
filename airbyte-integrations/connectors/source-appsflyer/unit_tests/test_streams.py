fae5813724dc784ad9ecb105b4c712ab79602434
asyarif93 <asyarif93@gmail.com>
2021-10-09 19:32:50 +0700

    test: refactor should retry test

--- airbyte-integrations/connectors/source-appsflyer/unit_tests/test_streams.py
+++ airbyte-integrations/connectors/source-appsflyer/unit_tests/test_streams.py
@@ -45,6 +45,7 @@ def test_parse_response_return_ok(mocker, patch_base_class):
     expected_parsed_object = {"a": "c", "b": "d"}
     assert next(stream.parse_response(**inputs)) == expected_parsed_object
 
+
 def test_parse_response_return_empty_row(mocker, patch_base_class):
     mocker.patch.object(AppsflyerStream, "main_fields", ("a"))
     stream = AppsflyerStream()
@@ -54,39 +55,31 @@ def test_parse_response_return_empty_row(mocker, patch_base_class):
     expected_parsed_object = []
     assert list(stream.parse_response(**inputs)) == expected_parsed_object
 
+
 def test_http_method(patch_base_class):
     stream = AppsflyerStream()
     expected_method = "GET"
     assert stream.http_method == expected_method
 
+
 @pytest.mark.parametrize(
-    ("http_status", "should_retry"),
+    ("http_status", "response_text", "should_retry"),
     [
-        (HTTPStatus.OK, False),
-        (HTTPStatus.BAD_REQUEST, False),
-        (HTTPStatus.TOO_MANY_REQUESTS, True),
-        (HTTPStatus.INTERNAL_SERVER_ERROR, True),
+        (HTTPStatus.OK, "", False),
+        (HTTPStatus.BAD_REQUEST, "", False),
+        (HTTPStatus.TOO_MANY_REQUESTS, "", True),
+        (HTTPStatus.INTERNAL_SERVER_ERROR, "", True),
+        (HTTPStatus.BAD_REQUEST, "Your API calls limit has been reached for report type", True),
+        (HTTPStatus.FORBIDDEN, "Limit reached for ", True),
     ],
 )
-def test_should_retry(patch_base_class, http_status, should_retry):
+def test_should_retry(patch_base_class, http_status, response_text, should_retry):
     response_mock = MagicMock()
     response_mock.status_code = http_status
+    response_mock.text = response_text
     stream = AppsflyerStream()
     assert stream.should_retry(response_mock) == should_retry
 
-def test_should_retry_raw_data(patch_base_class):
-    response_mock = MagicMock()
-    response_mock.status_code = HTTPStatus.BAD_REQUEST
-    response_mock.text = "Your API calls limit has been reached for report type"
-    stream = AppsflyerStream()
-    assert stream.should_retry(response_mock) == True
-
-def test_should_retry_aggregate_data(patch_base_class):
-    response_mock = MagicMock()
-    response_mock.status_code = HTTPStatus.FORBIDDEN
-    response_mock.text = "Limit reached for "
-    stream = AppsflyerStream()
-    assert stream.should_retry(response_mock) == True
 
 @pytest.mark.parametrize(
     ("http_status", "response_text", "expected_backoff_time"),
