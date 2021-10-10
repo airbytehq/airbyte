e02aeccac8f5539d083b3bc4bb5c505638c0182a
Foluso Ogunlana <foluso_ogunlana@stearsng.com>
2021-10-10 19:24:23 +0100

    feat(67): add support for discover and read customer stream

---
+++ airbyte-integrations/connectors/source-paystack/source_paystack/constants.py
@@ -0,0 +1,3 @@
+PAYSTACK_API_BASE_URL = "https://api.paystack.co/"
+AUTH_KEY_FIELD = "secret_key"
+PAYSTACK_CREATED_AT = "createdAt"
--- airbyte-integrations/connectors/source-paystack/source_paystack/source.py
+++ airbyte-integrations/connectors/source-paystack/source_paystack/source.py
@@ -1,18 +1,15 @@
 #
 # Copyright (c) 2021 Airbyte, Inc., all rights reserved.
 #
-from abc import ABC
-from typing import Any, Iterable, List, Mapping, MutableMapping, Optional, Tuple
+from typing import Any, List, Mapping, Tuple
 
 import requests
 from airbyte_cdk.sources import AbstractSource
 from airbyte_cdk.sources.streams import Stream
-from airbyte_cdk.sources.streams.http import HttpStream
 from airbyte_cdk.sources.streams.http.auth import TokenAuthenticator
+from source_paystack.constants import PAYSTACK_API_BASE_URL, AUTH_KEY_FIELD
+from source_paystack.streams import Customers
 
-PAYSTACK_API_BASE_URL = "https://api.paystack.co/"
-
-# Source
 class SourcePaystack(AbstractSource):
     def check_connection(self, logger, config) -> Tuple[bool, any]:
         """
@@ -26,27 +23,31 @@ class SourcePaystack(AbstractSource):
         try:
             response = requests.get(
                 f"{PAYSTACK_API_BASE_URL}/customer?page=1&perPage=1", 
-                headers={"Authorization": f"Bearer {config['secret_key']}"},
+                headers={"Authorization": f"Bearer {config[AUTH_KEY_FIELD]}"},
                 verify=True
             )
             response.raise_for_status()
         except Exception as e:
             msg = e
             if e.response.status_code == 401:
-                msg = 'Connection to Paystack could not be authorized. Please check your secret key.'
-
+                msg = 'Connection to Paystack was not authorized. Please check that your secret key is correct.'
             return False, msg
 
         response_json = response.json()
         if response_json['status'] == False:
             return False, 'Connection test failed due to error: ' + response_json['message']
-
         return True, None
 
     def streams(self, config: Mapping[str, Any]) -> List[Stream]:
         """
-        TODO: Replace the streams below with your own streams.
+        Returns list of streams output by the Paystack source connector
 
         :param config: A Mapping of the user input configuration as defined in the connector spec.
         """
-        return []
+        authenticator = TokenAuthenticator(config[AUTH_KEY_FIELD])
+        args = {"authenticator": authenticator, "start_date": config["start_date"]}
+        incremental_args = {**args, "lookback_window_days": config.get("lookback_window_days")}
+        return [
+            Customers(**incremental_args)
+        ]
+
