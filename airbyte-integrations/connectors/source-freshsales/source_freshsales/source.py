bfc4306c9efa826869c2d588b89957108214655b
Tuan Nguyen <anhtuan.nguyen@me.com>
2021-10-11 22:34:27 +0700

    finish implementing full_refresh

--- airbyte-integrations/connectors/source-freshsales/source_freshsales/source.py
+++ airbyte-integrations/connectors/source-freshsales/source_freshsales/source.py
@@ -5,6 +5,7 @@
 
 from abc import ABC
 from typing import Any, Iterable, List, Mapping, MutableMapping, Optional, Tuple
+from urllib.parse import parse_qsl, urlparse
 
 import requests
 from airbyte_cdk.sources import AbstractSource
@@ -13,132 +14,89 @@ from airbyte_cdk.sources.streams.http import HttpStream
 from airbyte_cdk.sources.streams.http.auth import TokenAuthenticator
 
 
-"""
-TODO: Most comments in this class are instructive and should be deleted after the source is implemented.
-
-This file provides a stubbed example of how to use the Airbyte CDK to develop both a source connector which supports full refresh or and an
-incremental syncs from an HTTP API.
-
-The various TODOs are both implementation hints and steps - fulfilling all the TODOs should be sufficient to implement one basic and one incremental
-stream from a source. This pattern is the same one used by Airbyte internally to implement connectors.
-
-The approach here is not authoritative, and devs are free to use their own judgement.
-
-There are additional required TODOs in the files within the integration_tests folder and the spec.json file.
-"""
-
-
 # Basic full refresh stream
 class FreshsalesStream(HttpStream, ABC):
-    """
-    TODO remove this comment
-
-    This class represents a stream output by the connector.
-    This is an abstract base class meant to contain all the common functionality at the API level e.g: the API base URL, pagination strategy,
-    parsing responses etc..
-
-    Each stream should extend this class (or another abstract subclass of it) to specify behavior unique to that stream.
-
-    Typically for REST APIs each stream corresponds to a resource in the API. For example if the API
-    contains the endpoints
-        - GET v1/customers
-        - GET v1/employees
-
-    then you should have three classes:
-    `class FreshsalesStream(HttpStream, ABC)` which is the current class
-    `class Customers(FreshsalesStream)` contains behavior to pull data for customers using v1/customers
-    `class Employees(FreshsalesStream)` contains behavior to pull data for employees using v1/employees
-
-    If some streams implement incremental sync, it is typical to create another class
-    `class IncrementalFreshsalesStream((FreshsalesStream), ABC)` then have concrete stream implementations extend it. An example
-    is provided below.
-
-    See the reference docs for the full list of configurable options.
-    """
     url_base = "https://{}/crm/sales/api/"
     primary_key = "id"
+    order_field = "updated_at"
     def __init__(self, domain_name: str, **kwargs):
         super().__init__(**kwargs)
         self.url_base = self.url_base.format(domain_name)
+        self.domain_name = domain_name
+        self.page = 1
+        self.get_view_id()
         
-
     def next_page_token(self, response: requests.Response) -> Optional[Mapping[str, Any]]:
-        """
-        TODO: Override this method to define a pagination strategy. If you will not be using pagination, no action is required - just return None.
-
-        This method should return a Mapping (e.g: dict) containing whatever information required to make paginated requests. This dict is passed
-        to most other methods in this class to help you form headers, request bodies, query params, etc..
-
-        For example, if the API accepts a 'page' parameter to determine which page of the result to return, and a response from the API contains a
-        'page' number, then this method should probably return a dict {'page': response.json()['page'] + 1} to increment the page count by 1.
-        The request_params method should then read the input next_page_token and set the 'page' param to next_page_token['page'].
-
-        :param response: the most recent response from the API
-        :return If there is another page in the result, a mapping (e.g: dict) containing information needed to query the next page in the response.
-                If there are no more pages in the result, return None.
-        """
-        return None
+        list_result = response.json().get(self.object_name, [])
+        if list_result:
+            self.page += 1
+            return self.page
+        else:
+            return None
 
     def request_params(
         self, stream_state: Mapping[str, Any], stream_slice: Mapping[str, any] = None, next_page_token: Mapping[str, Any] = None
     ) -> MutableMapping[str, Any]:
-        """
-        TODO: Override this method to define any query parameters to be set. Remove this method if you don't need to define request params.
-        Usually contains common params e.g. pagination size etc.
-        """
-        return {}
+        params = {"page": self.page, "sort": self.order_field, "sort_type": "asc"}  
+        return params
 
     def parse_response(self, response: requests.Response, **kwargs) -> Iterable[Mapping]:
+        json_response = response.json()
+        records = json_response.get(self.object_name, []) if self.object_name is not None else json_response
+        yield from records
+    
+    def get_filters(self):
+        filters_url = f"https://{self.domain_name}/crm/sales/api/{self.object_name}/filters"
+        auth = self.authenticator.get_auth_header()
+        try:
+            r = requests.get(filters_url, headers=auth)
+            r.raise_for_status()
+            return r.json().get('filters')
+        except requests.exceptions.RequestException as e:
+            return False, e
+
+    def get_view_id(self):
+        if hasattr(self, "filter_name"):
+            filters = self.get_filters()
+            return next(filter['id'] for filter in filters if filter['name'] == self.filter_name)
+        else:
+            return
+
+# Basic incremental stream
+class IncrementalFreshsalesStream(FreshsalesStream, ABC):
+    cursor_field = "updated_at"
+
+    def get_updated_state(self, current_stream_state: MutableMapping[str, Any], latest_record: Mapping[str, Any]) -> Mapping[str, Any]:
         """
-        TODO: Override this method to define how a response is parsed.
-        :return an iterable containing each record in the response
+        Override to determine the latest state after reading the latest record. This typically compared the cursor_field from the latest record and
+        the current state and picks the 'most' recent cursor. This is how a stream's state is determined. Required for incremental.
         """
-        return [response.json()]
+        return {self.cursor_field: max(latest_record.get(self.cursor_field, ""), current_stream_state.get(self.cursor_field, ""))}
+
+    def request_params(self, stream_state: Mapping[str, Any], stream_slice: Mapping[str, any] = None, next_page_token: Mapping[str, Any] = None) -> MutableMapping[str, Any]:
+        params = super().request_params(stream_state, stream_slice=stream_slice, next_page_token=next_page_token)
+        if stream_state:
+            params[self.filter_field] = stream_state.get(self.cursor_field)
+
+    def filter_records_newer_than_state(self, stream_state: Mapping[str, Any] = None, records_slice: Mapping[str, Any] = None) -> Iterable:
+        # Getting records >= state
+        if stream_state:
+            for record in records_slice:
+                if record.get(self.cursor_field) >= stream_state.get(self.cursor_field):
+                    yield record
+        else:
+            yield from records_slice
 
 
-class Contacts(FreshsalesStream):
-    """
-    TODO: Change class name to match the table/data source this stream corresponds to.
-    """
-
-    # TODO: Fill in the primary key. Required. This is usually a unique field in the stream, like an ID or a timestamp.
+class Contacts(IncrementalFreshsalesStream):
+    object_name = "contacts"
+    filter_name = "All Contacts"
     def path(
         self, stream_state: Mapping[str, Any] = None, stream_slice: Mapping[str, Any] = None, next_page_token: Mapping[str, Any] = None
     ) -> str:
-        """
-        TODO: Override this method to define the path this stream corresponds to. E.g. if the url is https://example-api.com/v1/customers then this
-        should return "customers". Required.
-        """
-        return "contacts/filters"
+        view_id = self.get_view_id()
+        return f"{self.object_name}/view/{view_id}"
 
-
-# # Basic incremental stream
-# class IncrementalFreshsalesStream(FreshsalesStream, ABC):
-#     """
-#     TODO fill in details of this class to implement functionality related to incremental syncs for your connector.
-#          if you do not need to implement incremental sync for any streams, remove this class.
-#     """
-
-#     # TODO: Fill in to checkpoint stream reads after N records. This prevents re-reading of data if the stream fails for any reason.
-#     state_checkpoint_interval = None
-
-#     @property
-#     def cursor_field(self) -> str:
-#         """
-#         TODO
-#         Override to return the cursor field used by this stream e.g: an API entity might always use created_at as the cursor field. This is
-#         usually id or date based. This field's presence tells the framework this in an incremental stream. Required for incremental.
-
-#         :return str: The name of the cursor field.
-#         """
-#         return []
-
-#     def get_updated_state(self, current_stream_state: MutableMapping[str, Any], latest_record: Mapping[str, Any]) -> Mapping[str, Any]:
-#         """
-#         Override to determine the latest state after reading the latest record. This typically compared the cursor_field from the latest record and
-#         the current state and picks the 'most' recent cursor. This is how a stream's state is determined. Required for incremental.
-#         """
-#         return {}
 
 
 # class Employees(IncrementalFreshsalesStream):
