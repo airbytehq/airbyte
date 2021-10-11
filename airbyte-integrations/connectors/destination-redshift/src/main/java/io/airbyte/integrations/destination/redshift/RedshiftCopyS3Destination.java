e18f259638782674e0642095ec27eb23e843d8a6
vmaltsev <vitalii.maltsev@globallogic.com>
2021-10-11 15:36:35 +0300

    Redshift Destination add ssl field to spec

--- airbyte-integrations/connectors/destination-redshift/src/main/java/io/airbyte/integrations/destination/redshift/RedshiftCopyS3Destination.java
+++ airbyte-integrations/connectors/destination-redshift/src/main/java/io/airbyte/integrations/destination/redshift/RedshiftCopyS3Destination.java
@@ -5,7 +5,6 @@
 package io.airbyte.integrations.destination.redshift;
 
 import com.fasterxml.jackson.databind.JsonNode;
-import io.airbyte.db.Databases;
 import io.airbyte.db.jdbc.JdbcDatabase;
 import io.airbyte.integrations.base.AirbyteMessageConsumer;
 import io.airbyte.integrations.destination.ExtendedNameTransformer;
@@ -16,14 +15,17 @@ import io.airbyte.integrations.destination.jdbc.copy.s3.S3Config;
 import io.airbyte.integrations.destination.jdbc.copy.s3.S3StreamCopier;
 import io.airbyte.protocol.models.AirbyteMessage;
 import io.airbyte.protocol.models.ConfiguredAirbyteCatalog;
+
 import java.util.function.Consumer;
+
+import static io.airbyte.integrations.destination.redshift.RedshiftInsertDestination.getJdbcDatabase;
 
 /**
  * A more efficient Redshift Destination than the sql-based {@link RedshiftDestination}. Instead of
  * inserting data as batched SQL INSERTs, we follow Redshift best practices and, 1) Stream the data
  * to S3. One compressed file is created per table. 2) Copy the S3 file to Redshift. See
  * https://docs.aws.amazon.com/redshift/latest/dg/c_best-practices-use-copy.html for more info.
- *
+ * <p>
  * Although Redshift recommends splitting the file for more efficient copying, this introduces
  * complexity around file partitioning that should be handled by a file destination connector. The
  * single file approach is orders of magnitude faster than batch inserting and 'good-enough' for
@@ -31,53 +33,48 @@ import java.util.function.Consumer;
  */
 public class RedshiftCopyS3Destination extends CopyDestination {
 
-  @Override
-  public AirbyteMessageConsumer getConsumer(JsonNode config,
-                                            ConfiguredAirbyteCatalog catalog,
-                                            Consumer<AirbyteMessage> outputRecordCollector)
-      throws Exception {
-    return CopyConsumerFactory.create(
-        outputRecordCollector,
-        getDatabase(config),
-        getSqlOperations(),
-        getNameTransformer(),
-        getS3Config(config),
-        catalog,
-        new RedshiftStreamCopierFactory(),
-        getConfiguredSchema(config));
-  }
+    @Override
+    public AirbyteMessageConsumer getConsumer(JsonNode config,
+                                              ConfiguredAirbyteCatalog catalog,
+                                              Consumer<AirbyteMessage> outputRecordCollector)
+            throws Exception {
+        return CopyConsumerFactory.create(
+                outputRecordCollector,
+                getDatabase(config),
+                getSqlOperations(),
+                getNameTransformer(),
+                getS3Config(config),
+                catalog,
+                new RedshiftStreamCopierFactory(),
+                getConfiguredSchema(config));
+    }
 
-  @Override
-  public void checkPersistence(JsonNode config) throws Exception {
-    S3StreamCopier.attemptS3WriteAndDelete(getS3Config(config));
-  }
+    @Override
+    public void checkPersistence(JsonNode config) {
+        S3StreamCopier.attemptS3WriteAndDelete(getS3Config(config));
+    }
 
-  @Override
-  public ExtendedNameTransformer getNameTransformer() {
-    return new RedshiftSQLNameTransformer();
-  }
+    @Override
+    public ExtendedNameTransformer getNameTransformer() {
+        return new RedshiftSQLNameTransformer();
+    }
 
-  @Override
-  public JdbcDatabase getDatabase(JsonNode config) throws Exception {
-    var jdbcConfig = RedshiftInsertDestination.getJdbcConfig(config);
-    return Databases.createJdbcDatabase(
-        jdbcConfig.get("username").asText(),
-        jdbcConfig.has("password") ? jdbcConfig.get("password").asText() : null,
-        jdbcConfig.get("jdbc_url").asText(),
-        RedshiftInsertDestination.DRIVER_CLASS);
-  }
+    @Override
+    public JdbcDatabase getDatabase(JsonNode config) {
+        return getJdbcDatabase(config);
+    }
 
-  @Override
-  public SqlOperations getSqlOperations() {
-    return new RedshiftSqlOperations();
-  }
+    @Override
+    public SqlOperations getSqlOperations() {
+        return new RedshiftSqlOperations();
+    }
 
-  private String getConfiguredSchema(JsonNode config) {
-    return config.get("schema").asText();
-  }
+    private String getConfiguredSchema(JsonNode config) {
+        return config.get("schema").asText();
+    }
 
-  private S3Config getS3Config(JsonNode config) {
-    return S3Config.getS3Config(config);
-  }
+    private S3Config getS3Config(JsonNode config) {
+        return S3Config.getS3Config(config);
+    }
 
 }
--- airbyte-integrations/connectors/destination-redshift/src/main/java/io/airbyte/integrations/destination/redshift/RedshiftInsertDestination.java
+++ airbyte-integrations/connectors/destination-redshift/src/main/java/io/airbyte/integrations/destination/redshift/RedshiftInsertDestination.java
@@ -7,39 +7,71 @@ package io.airbyte.integrations.destination.redshift;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.google.common.collect.ImmutableMap;
 import io.airbyte.commons.json.Jsons;
+import io.airbyte.db.Databases;
+import io.airbyte.db.jdbc.JdbcDatabase;
 import io.airbyte.integrations.base.Destination;
 import io.airbyte.integrations.destination.jdbc.AbstractJdbcDestination;
-import java.util.Optional;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 public class RedshiftInsertDestination extends AbstractJdbcDestination implements Destination {
 
-  private static final Logger LOGGER = LoggerFactory.getLogger(RedshiftDestination.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(RedshiftDestination.class);
 
-  public static final String DRIVER_CLASS = "com.amazon.redshift.jdbc.Driver";
+    public static final String DRIVER_CLASS = "com.amazon.redshift.jdbc.Driver";
 
-  public RedshiftInsertDestination() {
-    super(DRIVER_CLASS, new RedshiftSQLNameTransformer(), new RedshiftSqlOperations());
-  }
+    public RedshiftInsertDestination() {
+        super(DRIVER_CLASS, new RedshiftSQLNameTransformer(), new RedshiftSqlOperations());
+    }
 
-  @Override
-  public JsonNode toJdbcConfig(JsonNode redshiftConfig) {
-    return getJdbcConfig(redshiftConfig);
-  }
+    @Override
+    public JsonNode toJdbcConfig(JsonNode redshiftConfig) {
+        return getJdbcConfig(redshiftConfig);
+    }
 
-  public static JsonNode getJdbcConfig(JsonNode redshiftConfig) {
-    final String schema = Optional.ofNullable(redshiftConfig.get("schema")).map(JsonNode::asText).orElse("public");
+    @Override
+    public JdbcDatabase getDatabase(JsonNode config) {
+        return getJdbcDatabase(config);
+    }
 
-    return Jsons.jsonNode(ImmutableMap.builder()
-        .put("username", redshiftConfig.get("username").asText())
-        .put("password", redshiftConfig.get("password").asText())
-        .put("jdbc_url", String.format("jdbc:redshift://%s:%s/%s",
-            redshiftConfig.get("host").asText(),
-            redshiftConfig.get("port").asText(),
-            redshiftConfig.get("database").asText()))
-        .put("schema", schema)
-        .build());
-  }
+    private static void readSsl(JsonNode redshiftConfig, List<String> additionalProperties) {
+        boolean tls = redshiftConfig.has("tls") && redshiftConfig.get("tls").asBoolean(); // for backward compatibility
+        if (!tls) {
+            additionalProperties.add("ssl=false");
+        } else {
+            additionalProperties.add("ssl=true");
+            additionalProperties.add("sslfactory=com.amazon.redshift.ssl.NonValidatingFactory");
+        }
+    }
+
+    public static JdbcDatabase getJdbcDatabase(JsonNode config) {
+        List<String> additionalProperties = new ArrayList<>();
+        var jdbcConfig = RedshiftInsertDestination.getJdbcConfig(config);
+        readSsl(config, additionalProperties);
+        return Databases.createJdbcDatabase(
+                jdbcConfig.get("username").asText(),
+                jdbcConfig.has("password") ? jdbcConfig.get("password").asText() : null,
+                jdbcConfig.get("jdbc_url").asText(),
+                RedshiftInsertDestination.DRIVER_CLASS,
+                additionalProperties.isEmpty() ? "" : String.join(";", additionalProperties));
+    }
+
+    public static JsonNode getJdbcConfig(JsonNode redshiftConfig) {
+        final String schema = Optional.ofNullable(redshiftConfig.get("schema")).map(JsonNode::asText).orElse("public");
+
+        return Jsons.jsonNode(ImmutableMap.builder()
+                .put("username", redshiftConfig.get("username").asText())
+                .put("password", redshiftConfig.get("password").asText())
+                .put("jdbc_url", String.format("jdbc:redshift://%s:%s/%s",
+                        redshiftConfig.get("host").asText(),
+                        redshiftConfig.get("port").asText(),
+                        redshiftConfig.get("database").asText()))
+                .put("schema", schema)
+                .build());
+    }
 
 }
