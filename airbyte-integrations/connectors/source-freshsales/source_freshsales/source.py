fd5680d2cb760c8e2f0816086e6b1c8d7450a829
Tuan Nguyen <anhtuan.nguyen@me.com>
2021-10-12 07:16:50 +0700

    add other tables

--- airbyte-integrations/connectors/source-freshsales/source_freshsales/source.py
+++ airbyte-integrations/connectors/source-freshsales/source_freshsales/source.py
@@ -61,34 +61,7 @@ class FreshsalesStream(HttpStream, ABC):
             return next(filter['id'] for filter in filters if filter['name'] == self.filter_name)
         else:
             return
-
-# Basic incremental stream
-class IncrementalFreshsalesStream(FreshsalesStream, ABC):
-    cursor_field = "updated_at"
-
-    def get_updated_state(self, current_stream_state: MutableMapping[str, Any], latest_record: Mapping[str, Any]) -> Mapping[str, Any]:
-        """
-        Override to determine the latest state after reading the latest record. This typically compared the cursor_field from the latest record and
-        the current state and picks the 'most' recent cursor. This is how a stream's state is determined. Required for incremental.
-        """
-        return {self.cursor_field: max(latest_record.get(self.cursor_field, ""), current_stream_state.get(self.cursor_field, ""))}
-
-    def request_params(self, stream_state: Mapping[str, Any], stream_slice: Mapping[str, any] = None, next_page_token: Mapping[str, Any] = None) -> MutableMapping[str, Any]:
-        params = super().request_params(stream_state, stream_slice=stream_slice, next_page_token=next_page_token)
-        if stream_state:
-            params[self.filter_field] = stream_state.get(self.cursor_field)
-
-    def filter_records_newer_than_state(self, stream_state: Mapping[str, Any] = None, records_slice: Mapping[str, Any] = None) -> Iterable:
-        # Getting records >= state
-        if stream_state:
-            for record in records_slice:
-                if record.get(self.cursor_field) >= stream_state.get(self.cursor_field):
-                    yield record
-        else:
-            yield from records_slice
-
-
-class Contacts(IncrementalFreshsalesStream):
+class Contacts(FreshsalesStream):
     object_name = "contacts"
     filter_name = "All Contacts"
     def path(
@@ -97,53 +70,104 @@ class Contacts(IncrementalFreshsalesStream):
         view_id = self.get_view_id()
         return f"{self.object_name}/view/{view_id}"
 
+class Accounts(FreshsalesStream):
+    object_name = "sales_accounts"
+    filter_name = "All Accounts"
+    def path(
+        self, stream_state: Mapping[str, Any] = None, stream_slice: Mapping[str, Any] = None, next_page_token: Mapping[str, Any] = None
+    ) -> str:
+        view_id = self.get_view_id()
+        return f"{self.object_name}/view/{view_id}"
 
+class OpenDeals(FreshsalesStream):
+    object_name = "deals"
+    filter_name = "Open Deals"
+    def path(
+        self, stream_state: Mapping[str, Any] = None, stream_slice: Mapping[str, Any] = None, next_page_token: Mapping[str, Any] = None
+    ) -> str:
+        view_id = self.get_view_id()
+        return f"{self.object_name}/view/{view_id}"
 
-# class Employees(IncrementalFreshsalesStream):
-#     """
-#     TODO: Change class name to match the table/data source this stream corresponds to.
-#     """
+class WonDeals(FreshsalesStream):
+    object_name = "deals"
+    filter_name = "Won Deals"
+    def path(
+        self, stream_state: Mapping[str, Any] = None, stream_slice: Mapping[str, Any] = None, next_page_token: Mapping[str, Any] = None
+    ) -> str:
+        view_id = self.get_view_id()
+        return f"{self.object_name}/view/{view_id}"
 
-#     # TODO: Fill in the cursor_field. Required.
-#     cursor_field = "start_date"
+class LostDeals(FreshsalesStream):
+    object_name = "deals"
+    filter_name = "Lost Deals"
+    def path(
+        self, stream_state: Mapping[str, Any] = None, stream_slice: Mapping[str, Any] = None, next_page_token: Mapping[str, Any] = None
+    ) -> str:
+        view_id = self.get_view_id()
+        return f"{self.object_name}/view/{view_id}"
 
-#     # TODO: Fill in the primary key. Required. This is usually a unique field in the stream, like an ID or a timestamp.
-#     primary_key = "employee_id"
+class OpenTasks(FreshsalesStream):
+    object_name = "tasks"
+    filter_value = "open"
+    def path(
+        self, stream_state: Mapping[str, Any] = None, stream_slice: Mapping[str, Any] = None, next_page_token: Mapping[str, Any] = None
+    ) -> str:
+        return f"{self.object_name}"
 
-#     def path(self, **kwargs) -> str:
-#         """
-#         TODO: Override this method to define the path this stream corresponds to. E.g. if the url is https://example-api.com/v1/employees then this should
-#         return "single". Required.
-#         """
-#         return "employees"
+    def request_params(self, stream_state: Mapping[str, Any], stream_slice: Mapping[str, any] = None, next_page_token: Mapping[str, Any] = None) -> MutableMapping[str, Any]:
+        params = super().request_params(stream_state, stream_slice=stream_slice, next_page_token=next_page_token)
+        params['filter'] = self.filter_value
+        return params
 
-#     def stream_slices(self, stream_state: Mapping[str, Any] = None, **kwargs) -> Iterable[Optional[Mapping[str, any]]]:
-#         """
-#         TODO: Optionally override this method to define this stream's slices. If slicing is not needed, delete this method.
+class CompletedTasks(FreshsalesStream):
+    object_name = "tasks"
+    filter_value = "completed"
+    def path(
+        self, stream_state: Mapping[str, Any] = None, stream_slice: Mapping[str, Any] = None, next_page_token: Mapping[str, Any] = None
+    ) -> str:
+        return f"{self.object_name}"
 
-#         Slices control when state is saved. Specifically, state is saved after a slice has been fully read.
-#         This is useful if the API offers reads by groups or filters, and can be paired with the state object to make reads efficient. See the "concepts"
-#         section of the docs for more information.
+    def request_params(self, stream_state: Mapping[str, Any], stream_slice: Mapping[str, any] = None, next_page_token: Mapping[str, Any] = None) -> MutableMapping[str, Any]:
+        params = super().request_params(stream_state, stream_slice=stream_slice, next_page_token=next_page_token)
+        params['filter'] = self.filter_value
+        return params
 
-#         The function is called before reading any records in a stream. It returns an Iterable of dicts, each containing the
-#         necessary data to craft a request for a slice. The stream state is usually referenced to determine what slices need to be created.
-#         This means that data in a slice is usually closely related to a stream's cursor_field and stream_state.
+class PastAppointments(FreshsalesStream):
+    object_name = "appointments"
+    filter_value = "past"
+    def path(
+        self, stream_state: Mapping[str, Any] = None, stream_slice: Mapping[str, Any] = None, next_page_token: Mapping[str, Any] = None
+    ) -> str:
+        return f"{self.object_name}"
 
-#         An HTTP request is made for each returned slice. The same slice can be accessed in the path, request_params and request_header functions to help
-#         craft that specific request.
+    def request_params(self, stream_state: Mapping[str, Any], stream_slice: Mapping[str, any] = None, next_page_token: Mapping[str, Any] = None) -> MutableMapping[str, Any]:
+        params = super().request_params(stream_state, stream_slice=stream_slice, next_page_token=next_page_token)
+        params['filter'] = self.filter_value
+        return params
 
-#         For example, if https://example-api.com/v1/employees offers a date query params that returns data for that particular day, one way to implement
-#         this would be to consult the stream state object for the last synced date, then return a slice containing each date from the last synced date
-#         till now. The request_params function would then grab the date from the stream_slice and make it part of the request by injecting it into
-#         the date query param.
-#         """
-#         raise NotImplementedError("Implement stream slices or delete this method!")
+class UpcomingAppointments(FreshsalesStream):
+    object_name = "appointments"
+    filter_value = "upcoming"
+    def path(
+        self, stream_state: Mapping[str, Any] = None, stream_slice: Mapping[str, Any] = None, next_page_token: Mapping[str, Any] = None
+    ) -> str:
+        return f"{self.object_name}"
 
+    def request_params(self, stream_state: Mapping[str, Any], stream_slice: Mapping[str, any] = None, next_page_token: Mapping[str, Any] = None) -> MutableMapping[str, Any]:
+        params = super().request_params(stream_state, stream_slice=stream_slice, next_page_token=next_page_token)
+        params['filter'] = self.filter_value
+        return params
+class SalesActivities(FreshsalesStream):
+    object_name = "sales_activities"
+    def path(
+        self, stream_state: Mapping[str, Any] = None, stream_slice: Mapping[str, Any] = None, next_page_token: Mapping[str, Any] = None
+    ) -> str:
+        return f"{self.object_name}"
+
+# Source
 class FreshsalesAuthenticator(TokenAuthenticator):
     def get_auth_header(self) -> Mapping[str, Any]:
         return {"Authorization": f"Token token={self._token}"}
-
-# Source
 class SourceFreshsales(AbstractSource):
     def check_connection(self, logger, config) -> Tuple[bool, any]:
         auth = FreshsalesAuthenticator(token=config["api_key"]).get_auth_header()
@@ -158,4 +182,15 @@ class SourceFreshsales(AbstractSource):
     def streams(self, config: Mapping[str, Any]) -> List[Stream]:
         auth = FreshsalesAuthenticator(token=config["api_key"])
         args = {"authenticator": auth, "domain_name": config["domain_name"]}
-        return [Contacts(**args)]
+        return [
+            Contacts(**args),
+            Accounts(**args),
+            OpenDeals(**args),
+            WonDeals(**args),
+            LostDeals(**args),
+            OpenTasks(**args),
+            CompletedTasks(**args),
+            PastAppointments(**args),
+            UpcomingAppointments(**args),
+            SalesActivities(**args)
+        ]
