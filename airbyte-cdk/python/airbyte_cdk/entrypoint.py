9230672cdc2d763b5df0e94a59115676427e50be
Vadym Hevlich <vege1wgw@gmail.com>
2021-10-12 17:22:42 +0300

    Add connector check_config_against_spec parameter

--- airbyte-cdk/python/airbyte_cdk/entrypoint.py
+++ airbyte-cdk/python/airbyte_cdk/entrypoint.py
@@ -73,7 +73,8 @@ class AirbyteEntrypoint(object):
                 # Remove internal flags from config before validating so
                 # jsonschema's additionalProperties flag wont fail the validation
                 config, internal_config = split_config(config)
-                check_config_against_spec_or_exit(config, source_spec, logger)
+                if self.source.check_config_against_spec:
+                    check_config_against_spec_or_exit(config, source_spec, logger)
                 # Put internal flags back to config dict
                 config.update(internal_config.dict())
 
