84c8652ccc449bd02b23204b602d0bebef715423
Benoit Moriceau <benoit@airbyte.io>
2021-10-13 09:46:47 -0700

    Fix the content type retrieval

--- airbyte-server/src/main/java/io/airbyte/server/RequestLogger.java
+++ airbyte-server/src/main/java/io/airbyte/server/RequestLogger.java
@@ -64,7 +64,11 @@ public class RequestLogger implements ContainerRequestFilter, ContainerResponseF
     String method = servletRequest.getMethod();
     String url = servletRequest.getRequestURI();
     boolean isContentTypeGzip =
-        servletRequest.getHeader("Content-Type") != null && servletRequest.getHeader("Content-Type").toLowerCase().contains("application/x-gzip");
+        servletRequest.getHeader("Content-Type") != null && (
+          // the exhaustive media type can be found here https://www.iana.org/assignments/media-types/media-types.xhtml if some more need to be added
+          servletRequest.getHeader("Content-Type").toLowerCase().contains("application/gzip") ||
+          servletRequest.getHeader("Content-Type").toLowerCase().contains("application/tlsprt+gzip")
+        );
     int status = responseContext.getStatus();
 
     StringBuilder logBuilder = new StringBuilder()
