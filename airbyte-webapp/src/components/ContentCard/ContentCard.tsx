f2d83b6b69a4de9506f8cbb3d21130d695589573
Iakov Salikov <salikovpro@gmail.com>
2021-10-14 12:36:08 +0100

    feat: s/d docs popup

--- airbyte-webapp/src/components/ContentCard/ContentCard.tsx
+++ airbyte-webapp/src/components/ContentCard/ContentCard.tsx
@@ -1,29 +1,44 @@
 import React from "react";
 import styled from "styled-components";
 
-import { H5, Card } from "components";
+import { H5, Card, Button } from "components";
 
 type IProps = {
   title?: string | React.ReactNode;
+  onButtonClick?: () => void;
   className?: string;
   onClick?: () => void;
   full?: boolean;
   $light?: boolean;
 };
 
-const Title = styled(H5)<{ $light?: boolean }>`
+const Title = styled(H5)`
+  font-weight: 600;
+  letter-spacing: 0.008em;
+`;
+
+const Header = styled.div<{ $light?: boolean }>`
   padding: ${({ $light }) => ($light ? "19px 20px 20px" : "25px 25px 22px")};
   color: ${({ theme }) => theme.darkPrimaryColor};
   box-shadow: ${({ $light, theme }) =>
     $light ? "none" : `0 1px 2px ${theme.shadowColor}`};
-  font-weight: 600;
-  letter-spacing: 0.008em;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
   border-radius: 10px 10px 0 0;
 `;
 
 const ContentCard: React.FC<IProps> = (props) => (
   <Card className={props.className} onClick={props.onClick} full={props.full}>
-    {props.title ? <Title $light={props.$light}>{props.title}</Title> : null}
+    <Header $light={props.$light}>
+      {props.title ? <Title>{props.title}</Title> : null}
+      <Button
+        secondary
+        onClick={() => props.onButtonClick && props.onButtonClick()}
+      >
+        Setup Guide
+      </Button>
+    </Header>
     {props.children}
   </Card>
 );
