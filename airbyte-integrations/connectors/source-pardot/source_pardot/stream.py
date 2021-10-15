eb00892444ba4adae3273e25b8563997f0e9bb50
Tuan Nguyen <anhtuan.nguyen@me.com>
2021-10-15 13:57:04 +0700

    first streams with incremental

--- airbyte-integrations/connectors/source-pardot/source_pardot/stream.py
+++ airbyte-integrations/connectors/source-pardot/source_pardot/stream.py
@@ -1,22 +1,27 @@
 from abc import ABC
-from typing import Any, Iterable, Mapping, MutableMapping, Optional, Dict
+from typing import Any, Iterable, Mapping, MutableMapping, Optional, Dict, List
 
+import pendulum
 import requests
+from airbyte_cdk.models import SyncMode
 from airbyte_cdk.sources.streams.http import HttpStream
 
 
 # Basic full refresh stream
 class PardotStream(HttpStream, ABC):
     url_base = "https://pi.pardot.com/api/"
-    primary_key = "id"
     api_version = "4"
+    time_filter_template = "%Y-%m-%dT%H:%M:%SZ"
+    primary_key = None
 
     def __init__(self, config: Dict, **kwargs):
         super().__init__(**kwargs)
         self.config = config
 
     def next_page_token(self, response: requests.Response) -> Optional[Mapping[str, Any]]:
-        return None
+        records = response.json().get('result', [])
+        if records.get(self.data_key, []):
+            return {self.filter_param: records[self.data_key][-1][self.cursor_field]}
 
     def request_headers(
         self, stream_state: Mapping[str, Any], stream_slice: Mapping[str, Any] = None, next_page_token: Mapping[str, Any] = None
@@ -32,94 +37,75 @@ class PardotStream(HttpStream, ABC):
         params = {
             "format": "json",
         }
+        start_date = self.config.get('start_date', None)
+        if start_date:
+            params.update({'created_after': pendulum.parse(start_date, strict=False).strftime(self.time_filter_template)})
+        if next_page_token:
+            params.update(**next_page_token)
         return params
 
     def parse_response(self, response: requests.Response, **kwargs) -> Iterable[Mapping]:
-        json_response = response.json()['result']
-        records = json_response.get(self.object_name, []) if self.object_name is not None else json_response
+        print(response.text)
+        json_response = response.json().get('result', [])
+        records = json_response.get(self.data_key, []) if self.data_key is not None else json_response
         yield from records
-        
-class EmailClicks(PardotStream):
-    object_name = "emailClick"
+    
     def path(
         self, stream_state: Mapping[str, Any] = None, stream_slice: Mapping[str, Any] = None, next_page_token: Mapping[str, Any] = None
     ) -> str:
         return f"{self.object_name}/version/{self.api_version}/do/query"
 
+    def get_updated_state(self, current_stream_state: MutableMapping[str, Any], latest_record: Mapping[str, Any]) -> Mapping[str, Any]:
+        return {self.cursor_field: max(latest_record.get(self.cursor_field, ""), current_stream_state.get(self.cursor_field, ""))}
+
+    def filter_records_newer_than_state(self, stream_state: Mapping[str, Any] = None, records_slice: Mapping[str, Any] = None) -> Iterable:
+        if stream_state:
+            for record in records_slice:
+                if record[self.cursor_field] >= stream_state.get(self.cursor_field):
+                    yield record
+        else:
+            yield from records_slice
+        
+class PardotIdReplicationStream(PardotStream):
+    primary_key = "id"
+    cursor_field = "id"
+    filter_param = "id_greater_than"
+
+class EmailClicks(PardotIdReplicationStream):
+    object_name = "emailClick"
+    data_key = "emailClick"
+
+class VisitorActivities(PardotIdReplicationStream):
+    object_name = "visitorActivity"
+    data_key = "visitor_activity"
+
 class Campaigns(PardotStream):
     object_name = "campaign"
-    def path(
-        self, stream_state: Mapping[str, Any] = None, stream_slice: Mapping[str, Any] = None, next_page_token: Mapping[str, Any] = None
-    ) -> str:
-        return f"{self.object_name}/version/{self.api_version}/do/query"
     
 class ListMemberships(PardotStream):
     object_name = "listMembership"
-    def path(
-        self, stream_state: Mapping[str, Any] = None, stream_slice: Mapping[str, Any] = None, next_page_token: Mapping[str, Any] = None
-    ) -> str:
-        return f"{self.object_name}/version/{self.api_version}/do/query"
     
 class Lists(PardotStream):
     object_name = "list"
-    def path(
-        self, stream_state: Mapping[str, Any] = None, stream_slice: Mapping[str, Any] = None, next_page_token: Mapping[str, Any] = None
-    ) -> str:
-        return f"{self.object_name}/version/{self.api_version}/do/query"
     
 class Opportunities(PardotStream):
     object_name = "opportunity"
-    def path(
-        self, stream_state: Mapping[str, Any] = None, stream_slice: Mapping[str, Any] = None, next_page_token: Mapping[str, Any] = None
-    ) -> str:
-        return f"{self.object_name}/version/{self.api_version}/do/query"
     
 class ProspectAccounts(PardotStream):
     object_name = "prospectAccount"
-    def path(
-        self, stream_state: Mapping[str, Any] = None, stream_slice: Mapping[str, Any] = None, next_page_token: Mapping[str, Any] = None
-    ) -> str:
-        return f"{self.object_name}/version/{self.api_version}/do/query"
     
 class Prospects(PardotStream):
     object_name = "prospect"
-    def path(
-        self, stream_state: Mapping[str, Any] = None, stream_slice: Mapping[str, Any] = None, next_page_token: Mapping[str, Any] = None
-    ) -> str:
-        return f"{self.object_name}/version/{self.api_version}/do/query"
     
 class Users(PardotStream):
     object_name = "user"
-    def path(
-        self, stream_state: Mapping[str, Any] = None, stream_slice: Mapping[str, Any] = None, next_page_token: Mapping[str, Any] = None
-    ) -> str:
-        return f"{self.object_name}/version/{self.api_version}/do/query"
     
 class Visits(PardotStream):
     object_name = "visit"
-    def path(
-        self, stream_state: Mapping[str, Any] = None, stream_slice: Mapping[str, Any] = None, next_page_token: Mapping[str, Any] = None
-    ) -> str:
-        return f"{self.object_name}/version/{self.api_version}/do/query"
     
 class Visitors(PardotStream):
     object_name = "visitor"
-    def path(
-        self, stream_state: Mapping[str, Any] = None, stream_slice: Mapping[str, Any] = None, next_page_token: Mapping[str, Any] = None
-    ) -> str:
-        return f"{self.object_name}/version/{self.api_version}/do/query"
-    
-class VisitorActivities(PardotStream):
-    object_name = "visitorActivity"
-    def path(
-        self, stream_state: Mapping[str, Any] = None, stream_slice: Mapping[str, Any] = None, next_page_token: Mapping[str, Any] = None
-    ) -> str:
-        return f"{self.object_name}/version/{self.api_version}/do/query"
     
 class Visitors(PardotStream):
     object_name = "visitor"
-    def path(
-        self, stream_state: Mapping[str, Any] = None, stream_slice: Mapping[str, Any] = None, next_page_token: Mapping[str, Any] = None
-    ) -> str:
-        return f"{self.object_name}/version/{self.api_version}/do/query"
     
