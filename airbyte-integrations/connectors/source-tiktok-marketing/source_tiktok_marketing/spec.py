713fe61efd6fddf85d6230f5383b450a0a781542
Maksym Pavlenok <antixar@gmail.com>
2021-10-15 19:24:14 +0300

    🎉 New Source: TikTok Marketing (#5887)
    * create a new connector folder
    
    * add base classes
    
    * add schemas
    
    * add ads/ad_groups streams
    
    * update tests and docs
    
    * add a bootstrap file
    
    * update the base domain name for API
    
    * update the base domain name for API
    
    * update Dockerfile libs
    
    * remove unused comments
    
    * Update airbyte-integrations/connectors/source-tiktok-marketing/bootstrap.md
    
    Co-authored-by: George Claireaux <george@claireaux.co.uk>
    
    * Update airbyte-integrations/connectors/source-tiktok-marketing/bootstrap.md
    
    Co-authored-by: George Claireaux <george@claireaux.co.uk>
    
    * fix UI error with input parameters
    
    * fix problem with updated state
    
    * Update airbyte-integrations/connectors/source-tiktok-marketing/source_tiktok_marketing/spec.py
    
    Co-authored-by: Davin Chia <davinchia@gmail.com>
    
    * add an unit test
    
    * update README.md
    
    * bump version
    
    Co-authored-by: Maksym Pavlenok <maksym.pavlenok@globallogic.com>
    Co-authored-by: George Claireaux <george@claireaux.co.uk>
    Co-authored-by: Davin Chia <davinchia@gmail.com>

---
+++ airbyte-integrations/connectors/source-tiktok-marketing/source_tiktok_marketing/spec.py
@@ -0,0 +1,78 @@
+#
+# Copyright (c) 2021 Airbyte, Inc., all rights reserved.
+#
+
+
+import json
+import re
+from copy import deepcopy
+from typing import Union
+
+from jsonschema import RefResolver
+from pydantic import BaseModel, Field
+
+# TikTok Initial release date is September 2016
+DEFAULT_START_DATE = "01-09-2016"
+
+
+class SandboxEnvSpec(BaseModel):
+    class Config:
+        title = "Sandbox"
+
+    environment: str = Field("sandbox", const=True)
+
+    # it is string because UI has the bug https://github.com/airbytehq/airbyte/issues/6875
+    advertiser_id: str = Field(
+        description="The Advertiser ID  which generated for the developer's Sandbox application.",
+    )
+
+
+class ProductionEnvSpec(BaseModel):
+    class Config:
+        title = "Production"
+
+    environment: str = Field("prod", const=True)
+
+    # it is float because UI has the bug https://github.com/airbytehq/airbyte/issues/6875
+    app_id: str = Field(
+        description="The App id applied by the developer.",
+    )
+    secret: str = Field(description="The private key of the developer's application.", airbyte_secret=True)
+
+
+class SourceTiktokMarketingSpec(BaseModel):
+    class Config:
+        title = "TikTok Marketing Source Spec"
+
+    environment: Union[ProductionEnvSpec, SandboxEnvSpec] = Field(default=ProductionEnvSpec.Config.title)
+
+    access_token: str = Field(description="Long-term Authorized Access Token.", airbyte_secret=True)
+
+    start_date: str = Field(
+        description="Start Date in format: YYYY-MM-DD.", default=DEFAULT_START_DATE, pattern="^[0-9]{4}-[0-9]{2}-[0-9]{2}$"
+    )
+
+    @staticmethod
+    def change_format_to_oneOf(schema: dict, field_name: str) -> dict:
+        schema["properties"][field_name]["oneOf"] = deepcopy(schema["properties"][field_name]["anyOf"])
+        schema["properties"][field_name]["type"] = "object"
+        del schema["properties"][field_name]["anyOf"]
+        return schema
+
+    @staticmethod
+    def resolve_refs(schema: dict) -> dict:
+        json_schema_ref_resolver = RefResolver.from_schema(schema)
+        str_schema = json.dumps(schema)
+        for ref_block in re.findall(r'{"\$ref": "#\/definitions\/.+?(?="})"}', str_schema):
+            ref = json.loads(ref_block)["$ref"]
+            str_schema = str_schema.replace(ref_block, json.dumps(json_schema_ref_resolver.resolve(ref)[1]))
+        pyschema = json.loads(str_schema)
+        del pyschema["definitions"]
+        return pyschema
+
+    @classmethod
+    def schema(cls) -> dict:
+        """ we're overriding the schema classmethod to enable some post-processing """
+        schema = super().schema()
+        schema = cls.change_format_to_oneOf(schema, "environment")
+        return cls.resolve_refs(schema)
