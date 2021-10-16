16bac8adbb76adf434bb3d37cf271f3ba77a0910
Charles <giardina.charles@gmail.com>
2021-10-15 17:26:08 -0700

    move basic normalization to connectors_base build (#7068)

--- airbyte-workers/src/test/java/io/airbyte/workers/normalization/NormalizationRunnerFactoryTest.java
+++ airbyte-workers/src/test/java/io/airbyte/workers/normalization/NormalizationRunnerFactoryTest.java
@@ -29,10 +29,10 @@ class NormalizationRunnerFactoryTest {
     for (final Entry<String, ImmutablePair<String, DestinationType>> entry : NormalizationRunnerFactory.NORMALIZATION_MAPPING.entrySet()) {
       assertEquals(entry.getValue().getValue(),
           ((DefaultNormalizationRunner) NormalizationRunnerFactory.create(
-              String.format("%s:0.1.0", entry.getKey()), processFactory, "test")).getDestinationType());
+              String.format("%s:0.1.0", entry.getKey()), processFactory)).getDestinationType());
     }
     assertThrows(IllegalStateException.class,
-        () -> NormalizationRunnerFactory.create("airbyte/destination-csv:0.1.0", processFactory, "test"));
+        () -> NormalizationRunnerFactory.create("airbyte/destination-csv:0.1.0", processFactory));
   }
 
 }
