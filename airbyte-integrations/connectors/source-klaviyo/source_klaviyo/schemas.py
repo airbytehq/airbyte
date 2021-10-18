87ac5790d41aac2de3993db6eef5df3dd98e8783
Arthur Galuza <art@galuza.io>
2021-10-18 18:37:30 +0300

    Upd comment

--- airbyte-integrations/connectors/source-klaviyo/source_klaviyo/schemas.py
+++ airbyte-integrations/connectors/source-klaviyo/source_klaviyo/schemas.py
@@ -15,12 +15,12 @@ class BaseSchemaModel(BaseModel):
 
         @staticmethod
         def schema_extra(schema: MutableMapping[str, Any], model) -> None:
+            # Pydantic adds a title to each attribute, that is not needed, so we manually drop them.
+            # Also pydantic does not add a "null" option to a field marked as optional,
+            # so we add this functionality manually. Same for "$ref"
             schema.pop("title", None)
-            schema.pop("description", None)
             for name, prop in schema.get("properties", {}).items():
-                # Pydantic adds title to every attribute, this is too much, so we manually drop them
                 prop.pop("title", None)
-                prop.pop("description", None)
                 allow_none = model.__fields__[name].allow_none
                 if allow_none:
                     if "type" in prop:
