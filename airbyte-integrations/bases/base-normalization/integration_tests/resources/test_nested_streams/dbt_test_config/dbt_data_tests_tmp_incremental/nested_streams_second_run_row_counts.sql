1be482e3706180aa6a677949d2b212c70124e8b8
Christophe Duong <christophe.duong@gmail.com>
2021-10-19 11:17:11 +0200

    Modify incremental test case for nested streams

--- airbyte-integrations/bases/base-normalization/integration_tests/resources/test_nested_streams/dbt_test_config/dbt_data_tests_tmp_incremental/nested_streams_second_run_row_counts.sql
+++ airbyte-integrations/bases/base-normalization/integration_tests/resources/test_nested_streams/dbt_test_config/dbt_data_tests_tmp_incremental/nested_streams_second_run_row_counts.sql
@@ -1,14 +1,14 @@
 with table_row_counts as (
-    select distinct count(*) as row_count, 2 as expected_count
+    select distinct '_airbyte_raw_nested_stream_with_complex_columns_resulting_into_long_names' as label, count(*) as row_count, 3 as expected_count
     from {{ source('test_normalization', '_airbyte_raw_nested_stream_with_complex_columns_resulting_into_long_names') }}
 union all
-    select distinct count(*) as row_count, 2 as expected_count
+    select distinct 'nested_stream_with_complex_columns_resulting_into_long_names' as label, count(*) as row_count, 3 as expected_count
     from {{ ref('nested_stream_with_complex_columns_resulting_into_long_names') }}
 union all
-    select distinct count(*) as row_count, 2 as expected_count
+    select distinct 'nested_stream_with_complex_columns_resulting_into_long_names_partition' as label, count(*) as row_count, 3 as expected_count
     from {{ ref('nested_stream_with_complex_columns_resulting_into_long_names_partition') }}
 union all
-    select count(distinct currency) as row_count, 1 as expected_count
+    select 'nested_stream_with_complex_columns_resulting_into_long_names_partition_DATA' as label, count(distinct currency) as row_count, 1 as expected_count
     from {{ ref('nested_stream_with_complex_columns_resulting_into_long_names_partition_DATA') }}
 -- union all
 --    select count(distinct id) as row_count, 3 as expected_count
