c2e96e113292b7007c9dc4c0eff5dc82bd08f0d0
Achmad Syarif Hidayatullah <30652154+asyarif93@users.noreply.github.com>
2021-10-19 13:40:30 +0700

    🎉 New Source: Appsflyer (#6497)

---
+++ airbyte-integrations/connectors/source-appsflyer/source_appsflyer/fields/retargeting_daily_report.py
@@ -0,0 +1,16 @@
+# This order matters
+main_fields = (
+    "date",
+    "af_prt",
+    "media_source",
+    "campaign",
+    "clicks",
+    "conversions",
+    "conversion_type",
+    "conversion_rate",
+    "sessions",
+    "loyal_users",
+    "loyal_users_rate",
+    "total_cost",
+    "average_ecpi"
+)
