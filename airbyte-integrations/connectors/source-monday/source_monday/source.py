2d2b01223cc5739fd5a68cce5f00f7b527eb67b0
Tuan Nguyen <anhtuan.nguyen@me.com>
2021-10-20 09:18:28 +0700

    lint

--- airbyte-integrations/connectors/source-monday/source_monday/source.py
+++ airbyte-integrations/connectors/source-monday/source_monday/source.py
@@ -3,9 +3,9 @@
 #
 
 
-from abc import ABC
-import os
 import json
+import os
+from abc import ABC
 from typing import Any, Iterable, List, Mapping, MutableMapping, Optional, Tuple
 
 import requests
@@ -21,31 +21,30 @@ class MondayStream(HttpStream, ABC):
     primary_key = "id"
     page = 1
 
-
     def next_page_token(self, response: requests.Response) -> Optional[Mapping[str, Any]]:
-        json_response = response.json().get('data', {})
+        json_response = response.json().get("data", {})
         records = json_response.get(self.name.lower(), [])
         self.page += 1
         if records:
             return {"page": self.page}
-    
+
     def load_schema(self):
-        '''
+        """
         Load schema from file and make a GraphQL query
-        '''
+        """
         script_dir = os.path.dirname(__file__)
-        schema_path = os.path.join(script_dir, f'schemas/{self.name.lower()}.json')
+        schema_path = os.path.join(script_dir, f"schemas/{self.name.lower()}.json")
         with open(schema_path) as f:
             schema_dict = json.load(f)
-            schema = schema_dict['properties']
+            schema = schema_dict["properties"]
             graphql_schema = []
             for col in schema:
-                if 'properties' in schema[col]:
-                    nested_ids = ','.join(schema[col]['properties'])
-                    graphql_schema.append(f'{col}{{{nested_ids}}}')
+                if "properties" in schema[col]:
+                    nested_ids = ",".join(schema[col]["properties"])
+                    graphql_schema.append(f"{col}{{{nested_ids}}}")
                 else:
                     graphql_schema.append(col)
-        return ','.join(graphql_schema)
+        return ",".join(graphql_schema)
 
     def request_params(
         self, stream_state: Mapping[str, Any], stream_slice: Mapping[str, any] = None, next_page_token: Mapping[str, Any] = None
@@ -54,52 +53,59 @@ class MondayStream(HttpStream, ABC):
         if next_page_token:
             graphql_params.update(next_page_token)
 
-        graphql_query = ','.join([f'{k}:{v}' for k,v in graphql_params.items()])
+        graphql_query = ",".join([f"{k}:{v}" for k, v in graphql_params.items()])
 
         # Monday uses a query string to pass in environments
-        params = {
-            'query': f"query {{ {self.name.lower()} ({graphql_query}) {{ {self.load_schema()} }} }}"
-        }
+        params = {"query": f"query {{ {self.name.lower()} ({graphql_query}) {{ {self.load_schema()} }} }}"}
         return params
 
     def parse_response(self, response: requests.Response, **kwargs) -> Iterable[Mapping]:
-        json_response = response.json().get('data', {})
+        json_response = response.json().get("data", {})
         records = json_response.get(self.name.lower(), [])
         yield from records
-    
-    def path(self, stream_state: Mapping[str, Any] = None, stream_slice: Mapping[str, Any] = None, next_page_token: Mapping[str, Any] = None) -> str:
-        return ''
+
+    def path(
+        self, stream_state: Mapping[str, Any] = None, stream_slice: Mapping[str, Any] = None, next_page_token: Mapping[str, Any] = None
+    ) -> str:
+        return ""
+
 
 class Items(MondayStream):
     """
     API Documentation: https://api.developer.monday.com/docs/items-queries
     """
 
+
 class Boards(MondayStream):
     """
     API Documentation: https://api.developer.monday.com/docs/groups-queries#groups-queries
     """
+
+
 class Teams(MondayStream):
     """
     API Documentation: https://api.developer.monday.com/docs/teams-queries
     """
+
 
 class Updates(MondayStream):
     """
     API Documentation: https://api.developer.monday.com/docs/updates-queries
     """
 
+
 class Users(MondayStream):
     """
     API Documentation: https://api.developer.monday.com/docs/users-queries-1
     """
 
+
 # Source
 class SourceMonday(AbstractSource):
     def check_connection(self, logger, config) -> Tuple[bool, any]:
-        url = 'https://api.monday.com/v2'
+        url = "https://api.monday.com/v2"
         params = {"query": "{boards(limit:1){id name}}"}
-        auth = TokenAuthenticator(config['api_token']).get_auth_header()
+        auth = TokenAuthenticator(config["api_token"]).get_auth_header()
         try:
             response = requests.post(url, params=params, headers=auth)
             response.raise_for_status()
@@ -107,9 +113,8 @@ class SourceMonday(AbstractSource):
         except requests.exceptions.RequestException as e:
             return False, e
 
-
     def streams(self, config: Mapping[str, Any]) -> List[Stream]:
-        auth = TokenAuthenticator(token=config['api_token'])
+        auth = TokenAuthenticator(token=config["api_token"])
         return [
             Items(authenticator=auth),
             Boards(authenticator=auth),
