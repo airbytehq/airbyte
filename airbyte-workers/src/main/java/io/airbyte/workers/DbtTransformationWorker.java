0b27e0221aa0b793e162353da8a197f8acb8259e
Benoit Moriceau <benoit@airbyte.io>
2021-10-21 16:13:29 -0700

    Tmp

--- airbyte-workers/src/main/java/io/airbyte/workers/DbtTransformationWorker.java
+++ airbyte-workers/src/main/java/io/airbyte/workers/DbtTransformationWorker.java
@@ -4,11 +4,16 @@
 
 package io.airbyte.workers;
 
+import io.airbyte.commons.logging.LoggingHelper;
+import io.airbyte.commons.logging.LoggingHelper.Color;
+import io.airbyte.commons.logging.ScopedMDCChange;
 import io.airbyte.config.OperatorDbtInput;
 import io.airbyte.config.ResourceRequirements;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.time.Duration;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.atomic.AtomicBoolean;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -21,6 +26,7 @@ public class DbtTransformationWorker implements Worker<OperatorDbtInput, Void>, 
   private final int attempt;
   private final DbtTransformationRunner dbtTransformationRunner;
   private final ResourceRequirements resourceRequirements;
+  private final Map<String, String> newMDCValue;
 
   private final AtomicBoolean cancelled;
 
@@ -34,44 +40,56 @@ public class DbtTransformationWorker implements Worker<OperatorDbtInput, Void>, 
     this.resourceRequirements = resourceRequirements;
 
     this.cancelled = new AtomicBoolean(false);
+
+    this.newMDCValue = new HashMap<>() {{
+      put(LoggingHelper.LOG_SOURCE_MDC_KEY, LoggingHelper.applyColor(Color.GREEN, getApplicationName()));
+    }};
   }
 
   @Override
   public Void run(final OperatorDbtInput operatorDbtInput, final Path jobRoot) throws WorkerException {
-    final long startTime = System.currentTimeMillis();
+    try (final ScopedMDCChange scopedMDCChange = new ScopedMDCChange(newMDCValue)) {
+      final long startTime = System.currentTimeMillis();
 
-    try (dbtTransformationRunner) {
-      LOGGER.info("Running dbt transformation.");
-      dbtTransformationRunner.start();
-      final Path transformRoot = Files.createDirectories(jobRoot.resolve("transform"));
-      if (!dbtTransformationRunner.run(
-          jobId,
-          attempt,
-          transformRoot,
-          operatorDbtInput.getDestinationConfiguration(),
-          resourceRequirements,
-          operatorDbtInput.getOperatorDbt())) {
-        throw new WorkerException("DBT Transformation Failed.");
+      try (dbtTransformationRunner) {
+        LOGGER.info("Running dbt transformation.");
+        dbtTransformationRunner.start();
+        final Path transformRoot = Files.createDirectories(jobRoot.resolve("transform"));
+        if (!dbtTransformationRunner.run(
+            jobId,
+            attempt,
+            transformRoot,
+            operatorDbtInput.getDestinationConfiguration(),
+            resourceRequirements,
+            operatorDbtInput.getOperatorDbt())) {
+          throw new WorkerException("DBT Transformation Failed.");
+        }
+      } catch (final Exception e) {
+        throw new WorkerException("Dbt Transformation Failed.", e);
       }
+      if (cancelled.get()) {
+        LOGGER.info("Dbt Transformation was cancelled.");
+      }
+
+      final Duration duration = Duration.ofMillis(System.currentTimeMillis() - startTime);
+      LOGGER.info("Dbt Transformation executed in {}.", duration.toMinutesPart());
+
+      return null;
     } catch (final Exception e) {
-      throw new WorkerException("Dbt Transformation Failed.", e);
+      throw new WorkerException(getApplicationName() + " failed", e);
     }
-    if (cancelled.get()) {
-      LOGGER.info("Dbt Transformation was cancelled.");
-    }
-
-    final Duration duration = Duration.ofMillis(System.currentTimeMillis() - startTime);
-    LOGGER.info("Dbt Transformation executed in {}.", duration.toMinutesPart());
-
-    return null;
   }
 
   @Override
   public void cancel() {
-    LOGGER.info("Cancelling Dbt Transformation runner...");
-    try {
-      cancelled.set(true);
-      dbtTransformationRunner.close();
+    try (final ScopedMDCChange scopedMDCChange = new ScopedMDCChange(newMDCValue)) {
+      LOGGER.info("Cancelling Dbt Transformation runner...");
+      try {
+        cancelled.set(true);
+        dbtTransformationRunner.close();
+      } catch (final Exception e) {
+        e.printStackTrace();
+      }
     } catch (final Exception e) {
       e.printStackTrace();
     }
