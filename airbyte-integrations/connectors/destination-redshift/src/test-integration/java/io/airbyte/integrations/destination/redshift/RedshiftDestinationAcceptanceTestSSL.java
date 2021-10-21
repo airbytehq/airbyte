3b5f5126806b0341b34276f8ed2270c3e21daa02
vmaltsev <vitalii.maltsev@globallogic.com>
2021-10-21 10:39:51 +0300

    Redshift Source and Restination set SSL as default option

--- airbyte-integrations/connectors/destination-redshift/src/test-integration/java/io/airbyte/integrations/destination/redshift/RedshiftDestinationAcceptanceTestSSL.java
+++ airbyte-integrations/connectors/destination-redshift/src/test-integration/java/io/airbyte/integrations/destination/redshift/RedshiftDestinationAcceptanceTestSSL.java
@@ -4,22 +4,10 @@
 
 package io.airbyte.integrations.destination.redshift;
 
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.node.ObjectNode;
-import io.airbyte.commons.json.Jsons;
-import io.airbyte.commons.string.Strings;
 import io.airbyte.db.Database;
 import io.airbyte.db.Databases;
 
 public class RedshiftDestinationAcceptanceTestSSL extends RedshiftCopyDestinationAcceptanceTest {
-
-  @Override
-  protected JsonNode getConfig() {
-    final JsonNode baseConfig = Jsons.clone(getStaticConfig());
-    ((ObjectNode) baseConfig).put("tls", true);
-    ((ObjectNode) baseConfig).put("schema", Strings.addRandomSuffix("integration_test", "_", 5));
-    return baseConfig;
-  }
 
   @Override
   protected Database getDatabase() {
