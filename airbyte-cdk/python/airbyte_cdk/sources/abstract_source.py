5eb2af6c083d77cbeb211ef8cd7d4d888d13b77b
Yevhenii <34103125+yevhenii-ldv@users.noreply.github.com>
2021-10-22 20:47:48 +0300

    CDK: Autogenerate reference docs  (#4759)
    * CDK Autogenerated reference docs: base version
    
    * Update docs config.
    Add .readthedocs.yaml file.
    Update build html files.
    
    * Update .gitignore.
    Remove sphinx build files.
    
    * Add newline at the end of .gitignore
    
    * Update setup.py requirements.
    Update .readthedocs.yaml config.
    
    * Update rst files.
    Add Makefile rst config.
    
    * Update CDK docstring format.
    Change rst layout.
    Update sphinx config.
    
    * Add Sphinx docs.
    Update index.rst.
    Update abstract_source.py docstrings.
    
    * Override master_doc and package templates.
    Add docs schema enerator script.
    Update sphinx docs.
    
    * Add `Publishing to Read the Docs` section to sphinx-docs.md".
    Replace sphinx-docs.md to `airbyte-cdk` module.
    
    * Update sphinx-docs.md section name
    
    * Bump airbyte-cdk version.
    Update CHANGELOG.md.
    
    Co-authored-by: ykurochkin <y.kurochkin@zazmic.com>
    Co-authored-by: Vadym Hevlich <vege1wgw@gmail.com>

--- airbyte-cdk/python/airbyte_cdk/sources/abstract_source.py
+++ airbyte-cdk/python/airbyte_cdk/sources/abstract_source.py
@@ -39,11 +39,13 @@ class AbstractSource(Source, ABC):
     @abstractmethod
     def check_connection(self, logger: AirbyteLogger, config: Mapping[str, Any]) -> Tuple[bool, Optional[Any]]:
         """
-        :param config: The user-provided configuration as specified by the source's spec. This usually contains information required to check connection e.g. tokens, secrets and keys etc.
-        :return: A tuple of (boolean, error). If boolean is true, then the connection check is successful and we can connect to the underlying data
-        source using the provided configuration.
-        Otherwise, the input config cannot be used to connect to the underlying data source, and the "error" object should describe what went wrong.
-        The error object will be cast to string to display the problem to the user.
+        :param config: The user-provided configuration as specified by the source's spec.
+          This usually contains information required to check connection e.g. tokens, secrets and keys etc.
+        :return: A tuple of (boolean, error). If boolean is true, then the connection check is successful
+          and we can connect to the underlying data source using the provided configuration.
+          Otherwise, the input config cannot be used to connect to the underlying data source,
+          and the "error" object should describe what went wrong.
+          The error object will be cast to string to display the problem to the user.
         """
 
     @abstractmethod
