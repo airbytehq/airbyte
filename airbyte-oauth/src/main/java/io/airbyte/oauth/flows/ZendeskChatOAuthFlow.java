dac667e8e1cb50d0392b4e405afce63d5e232161
Oleksandr Bazarnov <oleksandr.bazarnov@globallogic.com>
2021-10-23 16:44:07 +0300

    corrected oauthFlow.java

--- airbyte-oauth/src/main/java/io/airbyte/oauth/flows/ZendeskChatOAuthFlow.java
+++ airbyte-oauth/src/main/java/io/airbyte/oauth/flows/ZendeskChatOAuthFlow.java
@@ -45,7 +45,8 @@ public class ZendeskChatOAuthFlow extends BaseOAuthFlow {
         .addParameter("redirect_uri", redirectUrl)
         .addParameter("state", getState())
         .addParameter("response_type", "code")
-        .addParameter("scope", "read");
+        .addParameter("scope", "read")
+        .addParameter("subdomain", "d3v-airbyte"); // fixme: need to set the subdomain dynamicaly from user's input
     try {
       return builder.build().toString();
     } catch (final URISyntaxException e) {
