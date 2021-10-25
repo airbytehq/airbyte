c689e17e80101b81735483a5c5985a027296d409
Benoit Moriceau <benoit@airbyte.io>
2021-10-25 13:29:10 -0700

    refactor the catalog discovery workflow (#7350)
    * refactor the catalog discovery workflow
    
    * Format

--- airbyte-workers/src/main/java/io/airbyte/workers/temporal/TemporalClient.java
+++ airbyte-workers/src/main/java/io/airbyte/workers/temporal/TemporalClient.java
@@ -20,6 +20,7 @@ import io.airbyte.scheduler.models.IntegrationLauncherConfig;
 import io.airbyte.scheduler.models.JobRunConfig;
 import io.airbyte.workers.WorkerUtils;
 import io.airbyte.workers.temporal.check.connection.CheckConnectionWorkflow;
+import io.airbyte.workers.temporal.discover.catalog.DiscoverCatalogWorkflow;
 import io.temporal.client.WorkflowClient;
 import java.nio.file.Path;
 import java.util.UUID;
