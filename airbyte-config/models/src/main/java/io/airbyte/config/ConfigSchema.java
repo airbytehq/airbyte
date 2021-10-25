31ee0a22eda8abbc15f6bf2175e3b148a467c6ad
Liren Tu <tuliren.git@outlook.com>
2021-10-25 04:39:33 -0700

    Add standard sync state

--- airbyte-config/models/src/main/java/io/airbyte/config/ConfigSchema.java
+++ airbyte-config/models/src/main/java/io/airbyte/config/ConfigSchema.java
@@ -46,6 +46,10 @@ public enum ConfigSchema implements AirbyteConfig {
       StandardSyncOperation.class,
       standardSyncOperation -> standardSyncOperation.getOperationId().toString(),
       "operationId"),
+  STANDARD_SYNC_STATE("StandardSyncState.yaml",
+      StandardSyncState.class,
+      standardSyncState -> standardSyncState.getConnectionId().toString(),
+      "connectionId"),
 
   SOURCE_OAUTH_PARAM("SourceOAuthParameter.yaml", SourceOAuthParameter.class,
       sourceOAuthParameter -> sourceOAuthParameter.getOauthParameterId().toString(),
