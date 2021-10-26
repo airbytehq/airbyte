2b7d33dd9f81b6be6066df83e90b585654a60ef2
LiRen Tu <tuliren@gmail.com>
2021-10-25 17:08:08 -0700

    Copy job attempt state to configs database (#7219)
    * Add migration to create latest state table
    
    * Log migration name
    
    * Expose db variables to airbyte-db
    
    * Implement migration
    
    * Fix migration test
    
    * temp
    
    * Rebase on master
    
    * Save state in temporal (#7253)
    
    * Copy state to airbyte_configs table
    
    * Add standard sync state
    
    * Move state methods to config repository
    
    * Add unit tests
    
    * Fix unit tests
    
    * Register standard sync state in migration
    
    * Add comment
    
    * Use config model instead of json node
    
    * Add comments
    
    * Remove unnecessary method
    
    * Fix migration query
    
    * Remove unused config database
    
    * Move persist statement and log the call
    
    * Update dev doc
    
    * Add unit tests for sync workflow
    
    Co-authored-by: Charles <giardina.charles@gmail.com>

--- airbyte-config/persistence/src/test/java/io/airbyte/config/persistence/ConfigRepositoryTest.java
+++ airbyte-config/persistence/src/test/java/io/airbyte/config/persistence/ConfigRepositoryTest.java
@@ -6,16 +6,23 @@ package io.airbyte.config.persistence;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.reset;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+import io.airbyte.commons.json.Jsons;
 import io.airbyte.config.ConfigSchema;
+import io.airbyte.config.StandardSyncState;
 import io.airbyte.config.StandardWorkspace;
+import io.airbyte.config.State;
 import io.airbyte.config.persistence.split_secrets.MemorySecretPersistence;
 import io.airbyte.config.persistence.split_secrets.NoOpSecretsHydrator;
 import io.airbyte.validation.json.JsonValidationException;
 import java.io.IOException;
 import java.util.Optional;
 import java.util.UUID;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -32,6 +39,11 @@ class ConfigRepositoryTest {
     final var secretPersistence = new MemorySecretPersistence();
     configRepository =
         new ConfigRepository(configPersistence, new NoOpSecretsHydrator(), Optional.of(secretPersistence), Optional.of(secretPersistence));
+  }
+
+  @AfterEach
+  void cleanUp() {
+    reset(configPersistence);
   }
 
   @Test
@@ -53,6 +65,36 @@ class ConfigRepositoryTest {
     when(configPersistence.getConfig(ConfigSchema.STANDARD_WORKSPACE, WORKSPACE_ID.toString(), StandardWorkspace.class)).thenReturn(workspace);
 
     assertEquals(workspace, configRepository.getStandardWorkspace(WORKSPACE_ID, true));
+  }
+
+  @Test
+  void testGetConnectionState() throws Exception {
+    final UUID connectionId = UUID.randomUUID();
+    final State state = new State().withState(Jsons.deserialize("{ \"cursor\": 1000 }"));
+    final StandardSyncState connectionState = new StandardSyncState().withConnectionId(connectionId).withState(state);
+
+    when(configPersistence.getConfig(ConfigSchema.STANDARD_SYNC_STATE, connectionId.toString(), StandardSyncState.class))
+        .thenThrow(new ConfigNotFoundException(ConfigSchema.STANDARD_SYNC_STATE, connectionId));
+    assertEquals(Optional.empty(), configRepository.getConnectionState(connectionId));
+
+    reset(configPersistence);
+    when(configPersistence.getConfig(ConfigSchema.STANDARD_SYNC_STATE, connectionId.toString(), StandardSyncState.class))
+        .thenReturn(connectionState);
+    assertEquals(Optional.of(state), configRepository.getConnectionState(connectionId));
+  }
+
+  @Test
+  void testUpdateConnectionState() throws Exception {
+    final UUID connectionId = UUID.randomUUID();
+    final State state1 = new State().withState(Jsons.deserialize("{ \"cursor\": 1 }"));
+    final StandardSyncState connectionState1 = new StandardSyncState().withConnectionId(connectionId).withState(state1);
+    final State state2 = new State().withState(Jsons.deserialize("{ \"cursor\": 2 }"));
+    final StandardSyncState connectionState2 = new StandardSyncState().withConnectionId(connectionId).withState(state2);
+
+    configRepository.updateConnectionState(connectionId, state1);
+    verify(configPersistence, times(1)).writeConfig(ConfigSchema.STANDARD_SYNC_STATE, connectionId.toString(), connectionState1);
+    configRepository.updateConnectionState(connectionId, state2);
+    verify(configPersistence, times(1)).writeConfig(ConfigSchema.STANDARD_SYNC_STATE, connectionId.toString(), connectionState2);
   }
 
 }
