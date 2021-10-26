0b0f2ddaa5e8f8fc78bb3f056946fafd393b5fdf
lmossman <lake@airbyte.io>
2021-10-26 09:53:36 -0700

    run gw format

--- airbyte-server/src/main/java/io/airbyte/server/handlers/DestinationDefinitionsHandler.java
+++ airbyte-server/src/main/java/io/airbyte/server/handlers/DestinationDefinitionsHandler.java
@@ -140,7 +140,8 @@ public class DestinationDefinitionsHandler {
         destinationDefinitionUpdate.getDockerImageTag());
 
     final boolean imageTagHasChanged = !currentDestination.getDockerImageTag().equals(destinationDefinitionUpdate.getDockerImageTag());
-    // TODO (lmossman): remove null spec condition when the spec field becomes required on the definition struct
+    // TODO (lmossman): remove null spec condition when the spec field becomes required on the
+    // definition struct
     final ConnectorSpecification spec = (imageTagHasChanged || currentDestination.getSpec() == null)
         ? getSpecForImage(currentDestination.getDockerRepository(), destinationDefinitionUpdate.getDockerImageTag())
         : currentDestination.getSpec();
