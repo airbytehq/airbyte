c3e5141709a04c98a873bf43d5546efbd3ae908d
Terence Cho <terencecho12@gmail.com>
2021-10-25 21:08:39 -0700

    Fix formatting

--- airbyte-integrations/connectors/source-strava/source_strava/source.py
+++ airbyte-integrations/connectors/source-strava/source_strava/source.py
@@ -1,13 +1,13 @@
 #
 # Copyright (c) 2021 Airbyte, Inc., all rights reserved.
 #
+
 from typing import Any, List, Mapping, Tuple
 
 from airbyte_cdk.sources import AbstractSource
 from airbyte_cdk.sources.streams import Stream
 from airbyte_cdk.sources.streams.http.requests_native_auth import Oauth2Authenticator
-
-from source_strava.streams import AthleteStats, Activities
+from source_strava.streams import Activities, AthleteStats
 
 
 # Source
@@ -19,7 +19,7 @@ class SourceStrava(AbstractSource):
         :return Tuple[bool, any]: (True, None) if the input config can be used to connect to the API successfully, (False, error) otherwise.
         """
         try:
-            auth = self.getOauth(config)
+            auth = self.get_oauth(config)
             _ = auth.get_auth_header()
             return True, None
         except Exception as e:
@@ -29,9 +29,11 @@ class SourceStrava(AbstractSource):
         """
         :param config: A Mapping of the user input configuration as defined in the connector spec.
         """
-        auth = self.getOauth(config)
-        return [AthleteStats(authenticator=auth, athlete_id=config["athlete_id"]),
-                Activities(authenticator=auth, after=config["start_date"])]
+        auth = self.get_oauth(config)
+        return [
+            AthleteStats(authenticator=auth, athlete_id=config["athlete_id"]),
+            Activities(authenticator=auth, after=config["start_date"]),
+        ]
 
     def get_oauth(self, config):
         return Oauth2Authenticator(
@@ -39,4 +41,5 @@ class SourceStrava(AbstractSource):
             client_id=config["client_id"],
             client_secret=config["client_secret"],
             refresh_token=config["refresh_token"],
-            scopes=["read_all", "activity:read_all"])
+            scopes=["read_all", "activity:read_all"],
+        )
