d133c57c91ad9bf09a72125dd0a3823ea4ba310c
Jenny Brown <jenny@airbyte.io>
2021-11-02 15:22:50 -0500

    Improved error handling

--- airbyte-json-validation/src/main/java/io/airbyte/validation/json/JsonSchemaValidator.java
+++ airbyte-json-validation/src/main/java/io/airbyte/validation/json/JsonSchemaValidator.java
@@ -65,11 +65,11 @@ public class JsonSchemaValidator {
       return;
     }
 
+    // Be careful not to echo the request data back at the user as it may contain secrets data.
     throw new JsonValidationException(String.format(
-        "json schema validation failed. \nerrors: %s \nschema: \n%s \nobject: \n%s",
+        "json schema validation failed when comparing the data to the json schema. \nErrors: %s \nSchema: \n%s",
         Strings.join(validationMessages, ", "),
-        schemaJson.toPrettyString(),
-        objectJson.toPrettyString()));
+        schemaJson.toPrettyString()));
   }
 
   public void ensureAsRuntime(final JsonNode schemaJson, final JsonNode objectJson) {
