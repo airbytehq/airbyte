09a635b1c5aac510065eb57485c0af6bcdce6c5a
monai <monai@cure.lt>
2021-11-04 17:10:55 +0200

    Add test_source

--- airbyte-integrations/connectors/source-linnworks/unit_tests/test_source.py
+++ airbyte-integrations/connectors/source-linnworks/unit_tests/test_source.py
@@ -4,19 +4,37 @@
 
 from unittest.mock import MagicMock
 
+from pytest import fixture
 from source_linnworks.source import SourceLinnworks
 
 
-def test_check_connection(mocker):
-    source = SourceLinnworks()
-    logger_mock, config_mock = MagicMock(), MagicMock()
-    assert source.check_connection(logger_mock, config_mock) == (True, None)
+@fixture
+def config():
+    return {
+        "config": {
+            "application_id": "xxx",
+            "application_secret": "yyy",
+            "token": "zzz",
+            "start_date": "2021-11-01"
+        }
+    }
 
+
+def test_check_connection(mocker, requests_mock, config):
+    source = SourceLinnworks()
+    logger_mock = MagicMock()
+    requests_mock.post(
+        "https://api.linnworks.net/api/Auth/AuthorizeByApplication",
+        json={
+            "Token": "00000000-0000-0000-0000-000000000000",
+            "Server": "https://xx-ext.linnworks.net",
+        },
+    )
+    assert source.check_connection(logger_mock, **config) == (True, None)
 
 def test_streams(mocker):
     source = SourceLinnworks()
     config_mock = MagicMock()
     streams = source.streams(config_mock)
-    # TODO: replace this with your streams number
-    expected_streams_number = 2
+    expected_streams_number = 3
     assert len(streams) == expected_streams_number
