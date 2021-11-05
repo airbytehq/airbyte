0504fd2249bd76b86f41f3d1fd0f35b2e9a07b9d
harshithmullapudi <harshithmullapudi@gmail.com>
2021-11-05 15:16:42 +0530

    fix: format

--- tools/bin/ci_credentials.sh
+++ tools/bin/ci_credentials.sh
@@ -163,4 +163,3 @@ write_standard_creds source-zendesk-support "$ZENDESK_SUPPORT_OAUTH_TEST_CREDS" 
 write_standard_creds source-zendesk-talk "$ZENDESK_TALK_TEST_CREDS"
 write_standard_creds source-zoom-singer "$ZOOM_INTEGRATION_TEST_CREDS"
 write_standard_creds source-zuora "$SOURCE_ZUORA_TEST_CREDS"
-
