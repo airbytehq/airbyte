30e6d3648d6db59f716d62eaf01f9ba4d98df6d7
Christophe Duong <christophe.duong@gmail.com>
2021-11-05 12:48:08 +0100

    Regen SQL

--- airbyte-integrations/bases/base-normalization/integration_tests/normalization_test_output/mssql/test_nested_streams/first_output/airbyte_views/test_normalization/nested_stream_with_co__lting_into_long_names_ab3.sql
+++ airbyte-integrations/bases/base-normalization/integration_tests/normalization_test_output/mssql/test_nested_streams/first_output/airbyte_views/test_normalization/nested_stream_with_co__lting_into_long_names_ab3.sql
@@ -1,7 +1,37 @@
 USE [test_normalization];
     execute('create view _airbyte_test_normalization."nested_stream_with_co__lting_into_long_names_ab3__dbt_tmp" as
     
--- SQL model to build a hash column based on the values of this record
+with __dbt__CTE__nested_stream_with_co__lting_into_long_names_ab1 as (
+
+-- SQL model to parse JSON blob stored in a single column and extract into separated field columns as described by the JSON Schema
+select
+    json_value(_airbyte_data, ''$."id"'') as id,
+    json_value(_airbyte_data, ''$."date"'') as "date",
+    json_query(_airbyte_data, ''$."partition"'') as "partition",
+    _airbyte_ab_id,
+    _airbyte_emitted_at,
+    SYSDATETIME() as _airbyte_normalized_at
+from "test_normalization".test_normalization._airbyte_raw_nested_stream_with_complex_columns_resulting_into_long_names as table_alias
+-- nested_stream_with_co__lting_into_long_names
+where 1 = 1
+
+),  __dbt__CTE__nested_stream_with_co__lting_into_long_names_ab2 as (
+
+-- SQL model to cast each column to its adequate SQL type converted from the JSON schema type
+select
+    cast(id as 
+    VARCHAR(max)) as id,
+    cast("date" as 
+    VARCHAR(max)) as "date",
+    cast("partition" as VARCHAR(max)) as "partition",
+    _airbyte_ab_id,
+    _airbyte_emitted_at,
+    SYSDATETIME() as _airbyte_normalized_at
+from __dbt__CTE__nested_stream_with_co__lting_into_long_names_ab1
+-- nested_stream_with_co__lting_into_long_names
+where 1 = 1
+
+)-- SQL model to build a hash column based on the values of this record
 select
     convert(varchar(32), HashBytes(''md5'',  coalesce(cast(
     
@@ -13,7 +43,7 @@ select
     VARCHAR(max)), ''''),''''), '''') as 
     VARCHAR(max)), '''')), 2) as _airbyte_nested_strea__nto_long_names_hashid,
     tmp.*
-from "test_normalization"._airbyte_test_normalization."nested_stream_with_co__lting_into_long_names_ab2" tmp
+from __dbt__CTE__nested_stream_with_co__lting_into_long_names_ab2 tmp
 -- nested_stream_with_co__lting_into_long_names
 where 1 = 1
 
--- airbyte-integrations/bases/base-normalization/integration_tests/normalization_test_output/postgres/test_nested_streams/first_output/airbyte_tables/test_normalization/conflict_stream_scalar.sql
+++ airbyte-integrations/bases/base-normalization/integration_tests/normalization_test_output/postgres/test_nested_streams/first_output/airbyte_tables/test_normalization/conflict_stream_scalar.sql
@@ -3,7 +3,50 @@
   create  table "postgres".test_normalization."conflict_stream_scalar__dbt_tmp"
   as (
     
--- Final base SQL model
+with __dbt__cte__conflict_stream_scalar_ab1 as (
+
+-- SQL model to parse JSON blob stored in a single column and extract into separated field columns as described by the JSON Schema
+select
+    jsonb_extract_path_text(_airbyte_data, 'id') as "id",
+    jsonb_extract_path_text(_airbyte_data, 'conflict_stream_scalar') as conflict_stream_scalar,
+    _airbyte_ab_id,
+    _airbyte_emitted_at,
+    now() as _airbyte_normalized_at
+from "postgres".test_normalization._airbyte_raw_conflict_stream_scalar as table_alias
+-- conflict_stream_scalar
+where 1 = 1
+),  __dbt__cte__conflict_stream_scalar_ab2 as (
+
+-- SQL model to cast each column to its adequate SQL type converted from the JSON schema type
+select
+    cast("id" as 
+    varchar
+) as "id",
+    cast(conflict_stream_scalar as 
+    bigint
+) as conflict_stream_scalar,
+    _airbyte_ab_id,
+    _airbyte_emitted_at,
+    now() as _airbyte_normalized_at
+from __dbt__cte__conflict_stream_scalar_ab1
+-- conflict_stream_scalar
+where 1 = 1
+),  __dbt__cte__conflict_stream_scalar_ab3 as (
+
+-- SQL model to build a hash column based on the values of this record
+select
+    md5(cast(coalesce(cast("id" as 
+    varchar
+), '') || '-' || coalesce(cast(conflict_stream_scalar as 
+    varchar
+), '') as 
+    varchar
+)) as _airbyte_conflict_stream_scalar_hashid,
+    tmp.*
+from __dbt__cte__conflict_stream_scalar_ab2 tmp
+-- conflict_stream_scalar
+where 1 = 1
+)-- Final base SQL model
 select
     "id",
     conflict_stream_scalar,
@@ -11,7 +54,7 @@ select
     _airbyte_emitted_at,
     now() as _airbyte_normalized_at,
     _airbyte_conflict_stream_scalar_hashid
-from "postgres"._airbyte_test_normalization."conflict_stream_scalar_ab3"
+from __dbt__cte__conflict_stream_scalar_ab3
 -- conflict_stream_scalar from "postgres".test_normalization._airbyte_raw_conflict_stream_scalar
 where 1 = 1
   );
