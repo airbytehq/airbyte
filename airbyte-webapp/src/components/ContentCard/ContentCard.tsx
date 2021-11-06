c728aacff02099f082a51f3c2d650691d525db32
Artem Astapenko <jamakase54@gmail.com>
2021-11-07 00:13:08 +0300

    Fix ContentCard

--- airbyte-webapp/src/components/ContentCard/ContentCard.tsx
+++ airbyte-webapp/src/components/ContentCard/ContentCard.tsx
@@ -11,28 +11,19 @@ type IProps = {
   $light?: boolean;
 };
 
-const Title = styled(H5)`
-  font-weight: 600;
-  letter-spacing: 0.008em;
-`;
-
-const Header = styled.div<{ $light?: boolean }>`
+const Title = styled(H5)<{ $light?: boolean }>`
   padding: ${({ $light }) => ($light ? "19px 20px 20px" : "25px 25px 22px")};
   color: ${({ theme }) => theme.darkPrimaryColor};
   box-shadow: ${({ $light, theme }) =>
     $light ? "none" : `0 1px 2px ${theme.shadowColor}`};
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
+  font-weight: 600;
+  letter-spacing: 0.008em;
   border-radius: 10px 10px 0 0;
-  height: 80px;
 `;
 
 const ContentCard: React.FC<IProps> = (props) => (
   <Card className={props.className} onClick={props.onClick} full={props.full}>
-    <Header $light={props.$light}>
-      {props.title ? <Title>{props.title}</Title> : null}
-    </Header>
+    {props.title ? <Title $light={props.$light}>{props.title}</Title> : null}
     {props.children}
   </Card>
 );
