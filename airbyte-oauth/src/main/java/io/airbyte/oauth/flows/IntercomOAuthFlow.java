04f6178de9ed951eb0c577c05ff95b734ababd3f
Vadym Ratniuk <midavadim@yahoo.com>
2021-11-09 20:23:33 +0200

    updated IntercomOAuthFlow to match base class changes

--- airbyte-oauth/src/main/java/io/airbyte/oauth/flows/IntercomOAuthFlow.java
+++ airbyte-oauth/src/main/java/io/airbyte/oauth/flows/IntercomOAuthFlow.java
@@ -6,17 +6,19 @@ package io.airbyte.oauth.flows;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.google.common.annotations.VisibleForTesting;
+import com.google.common.base.Preconditions;
 import io.airbyte.config.persistence.ConfigRepository;
-import io.airbyte.oauth.BaseOAuthFlow;
+import io.airbyte.oauth.BaseOAuth2Flow;
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.net.http.HttpClient;
+import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 import java.util.function.Supplier;
 import org.apache.http.client.utils.URIBuilder;
 
-public class IntercomOAuthFlow extends BaseOAuthFlow {
+public class IntercomOAuthFlow extends BaseOAuth2Flow {
 
   private static final String AUTHORIZE_URL = "https://app.intercom.com/a/oauth/connect";
   private static final String ACCESS_TOKEN_URL = "https://api.intercom.io/auth/eagle/token";
@@ -50,14 +52,16 @@ public class IntercomOAuthFlow extends BaseOAuthFlow {
   }
 
   @Override
-  protected Map<String, Object> extractRefreshToken(final JsonNode data, String accessTokenUrl) throws IOException {
+  protected Map<String, Object> extractOAuthOutput(final JsonNode data, final String accessTokenUrl) {
     // Intercom does not have refresh token but calls it "long lived access token" instead:
     // see https://developers.intercom.com/building-apps/docs/setting-up-oauth
-    if (data.has("access_token")) {
-      return Map.of("access_token", data.get("access_token").asText());
-    } else {
-      throw new IOException(String.format("Missing 'access_token' in query params from %s", ACCESS_TOKEN_URL));
-    }
+    Preconditions.checkArgument(data.has("access_token"), "Missing 'access_token' in query params from %s", ACCESS_TOKEN_URL);
+    return Map.of("access_token", data.get("access_token").asText());
+  }
+
+  @Override
+  protected List<String> getDefaultOAuthOutputPath() {
+    return List.of();
   }
 
 }
