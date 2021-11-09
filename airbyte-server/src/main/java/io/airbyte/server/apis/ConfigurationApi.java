723717acfb0895397791f235980fffb805c085b1
Christophe Duong <christophe.duong@gmail.com>
2021-11-09 19:41:18 +0100

    Change OAuth API

--- airbyte-server/src/main/java/io/airbyte/server/apis/ConfigurationApi.java
+++ airbyte-server/src/main/java/io/airbyte/server/apis/ConfigurationApi.java
@@ -8,7 +8,9 @@ import io.airbyte.analytics.TrackingClient;
 import io.airbyte.api.model.CheckConnectionRead;
 import io.airbyte.api.model.CheckOperationRead;
 import io.airbyte.api.model.CompleteDestinationOAuthRequest;
+import io.airbyte.api.model.CompleteDestinationOauthResponse;
 import io.airbyte.api.model.CompleteSourceOauthRequest;
+import io.airbyte.api.model.CompleteSourceOauthResponse;
 import io.airbyte.api.model.ConnectionCreate;
 import io.airbyte.api.model.ConnectionIdRequestBody;
 import io.airbyte.api.model.ConnectionRead;
@@ -122,7 +124,6 @@ import java.io.File;
 import java.io.IOException;
 import java.net.http.HttpClient;
 import java.nio.file.Path;
-import java.util.Map;
 
 @javax.ws.rs.Path("/v1")
 public class ConfigurationApi implements io.airbyte.api.V1Api {
@@ -300,7 +301,7 @@ public class ConfigurationApi implements io.airbyte.api.V1Api {
   }
 
   @Override
-  public Map<String, Object> completeSourceOAuth(final CompleteSourceOauthRequest completeSourceOauthRequest) {
+  public CompleteSourceOauthResponse completeSourceOAuth(final CompleteSourceOauthRequest completeSourceOauthRequest) {
     return execute(() -> oAuthHandler.completeSourceOAuth(completeSourceOauthRequest));
   }
 
@@ -310,7 +311,7 @@ public class ConfigurationApi implements io.airbyte.api.V1Api {
   }
 
   @Override
-  public Map<String, Object> completeDestinationOAuth(final CompleteDestinationOAuthRequest requestBody) {
+  public CompleteDestinationOauthResponse completeDestinationOAuth(final CompleteDestinationOAuthRequest requestBody) {
     return execute(() -> oAuthHandler.completeDestinationOAuth(requestBody));
   }
 
