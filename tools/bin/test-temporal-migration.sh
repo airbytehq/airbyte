ae45d3c158abaef1af51ad81456a046ddca0a461
Benoit Moriceau <benoit@airbyte.io>
2021-11-10 13:22:36 -0800

    Fix compose down

--- tools/bin/test-temporal-migration.sh
+++ tools/bin/test-temporal-migration.sh
@@ -17,6 +17,7 @@ git pull --no-rebase
 
 SUB_BUILD=PLATFORM "$SCRIPT_DIR"/../../gradlew -p "$SCRIPT_DIR"/../.. generate-docker
 
+cd "$SCRIPT_DIR"/../..
 VERSION=dev docker-compose -f "$SCRIPT_DIR"/../../docker-compose.yaml up &
 
 sleep 75
