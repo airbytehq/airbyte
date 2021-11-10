7c4d55f2ba1cd05dc2f63dfc0d745d466edc089b
charity1475 <charitymbisi@gmail.com>
2021-11-10 20:43:25 +0300

    fix: destination

--- airbyte-integrations/connectors/destination-corebos/destination_corebos/destination.py
+++ airbyte-integrations/connectors/destination-corebos/destination_corebos/destination.py
@@ -26,6 +26,9 @@ from typing import Mapping, Any, Iterable
 from airbyte_cdk import AirbyteLogger
 from airbyte_cdk.destinations import Destination
 from airbyte_cdk.models import AirbyteConnectionStatus, ConfiguredAirbyteCatalog, AirbyteMessage, Status
+from airbyte_protocol.models.airbyte_protocol import DestinationSyncMode, Type
+from base_python import logger
+from destination_corebos.libs.WSClient import *
 
 
 class DestinationCorebos(Destination):
@@ -36,42 +39,39 @@ class DestinationCorebos(Destination):
             input_messages: Iterable[AirbyteMessage]
     ) -> Iterable[AirbyteMessage]:
 
-        """
-        TODO
-        Reads the input stream of messages, config, and catalog to write data to the destination.
+        url = config["url"]
+        client = WSClient(url)
+        #TODO: change the query to Module name
+        module = config["query"]
 
-        This method returns an iterable (typically a generator of AirbyteMessages via yield) containing state messages received
-        in the input message stream. Outputting a state message means that every AirbyteRecordMessage which came before it has been
-        successfully persisted to the destination. This is used to ensure fault tolerance in the case that a sync fails before fully completing,
-        then the source is given the last state message output from this method as the starting point of the next sync.
+        for configured_stream in configured_catalog.streams:
+            if configured_stream.destination_sync_mode == DestinationSyncMode.overwrite:
+                logger.info("details in stream %s will be overrriden", configured_stream)
 
-        :param config: dict of JSON configuration matching the configuration declared in spec.json
-        :param configured_catalog: The Configured Catalog describing the schema of the data being received and how it should be persisted in the
-                                    destination
-        :param input_messages: The stream of input messages received from the source
-        :return: Iterable of AirbyteStateMessages wrapped in AirbyteMessage structs
-        """
-
-        pass
+        for message in input_messages:
+            if message.type == Type.STATE:
+                yield message
+            elif message.type == Type.RECORD:
+                record = message.record
+                recordIfo = client.do_create(module, record)
+                logger.info(":: created %s", client.do_retrieve(recordIfo['id']))
+            else:
+                continue
 
     def check(self, logger: AirbyteLogger, config: Mapping[str, Any]) -> AirbyteConnectionStatus:
-        """
-        Tests if the input configuration can be used to successfully connect to the destination with the needed permissions
-            e.g: if a provided API token or password can be used to connect and write to the destination.
-
-        :param logger: Logging object to display debug/info/error to the logs
-            (logs will not be accessible via airbyte UI if they are not passed to this logger)
-        :param config: Json object containing the configuration of this destination, content of this json is as specified in
-        the properties of the spec.json file
-
-        :return: AirbyteConnectionStatus indicating a Success or Failure
-        """
-        try:
-            # TODO
-
+        url = config["url"]
+        client = WSClient(url)
+        username = config["username"]
+        key = config["access_token"]
+        login = client.do_login(username,key,withpassword=False)
+        logger.info(login)
+        if login:
             return AirbyteConnectionStatus(status=Status.SUCCEEDED)
-        except Exception as e:
-            return AirbyteConnectionStatus(status=Status.FAILED, message=f"An exception occurred: {repr(e)}")
+        else:
+            return AirbyteConnectionStatus(
+                    status=Status.FAILED,
+                    message='An exception occurred, content: {}'.format(login),
+                )
 
 
 
--- airbyte-integrations/connectors/destination-corebos/integration_tests/integration_test.py
+++ airbyte-integrations/connectors/destination-corebos/integration_tests/integration_test.py
@@ -20,6 +20,44 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
+from os import name
+from typing import Any, Mapping
+import pytest,json
+from airbyte_cdk import AirbyteLogger
+from airbyte_cdk.models import (
+    AirbyteMessage,
+    AirbyteRecordMessage,
+    AirbyteStateMessage,
+    AirbyteStream,
+    ConfiguredAirbyteCatalog,
+    ConfiguredAirbyteStream,
+    DestinationSyncMode,
+    Status,
+    SyncMode,
+    Type,
+)
+
+
+@pytest.fixture(name="config")
+def config_fixture()->Mapping[str,Any]:
+    with open("secrets/config.json","r") as f:
+        return json.loads(f.read())
+
+@pytest.fixture(name="configured_catalog")
+def configured_catalog_fixture() -> ConfiguredAirbyteCatalog:
+    stream_schema = {"type": "object", "properties": {"string_col": {"type": "str"}, "int_col": {"type": "integer"}}}
+    append_stream = ConfiguredAirbyteStream(
+        stream = AirbyteStream(name="append_stream", json_schem=stream_schema),
+        sync_mode = SyncMode.incremental,
+        destination_sync_mode = DestinationSyncMode.append
+    )
+    overwrite_stream = ConfiguredAirbyteStream(
+        stream = AirbyteStream(name="overwrite_stream", json_schem=stream_schema),
+        sync_mode = SyncMode.incremental,
+        destination_sync_mode = DestinationSyncMode.overwrite
+    )
+    return ConfiguredAirbyteCatalog(streams=[append_stream,overwrite_stream])
+
 def integration_test():
     # TODO write integration tests
     pass
