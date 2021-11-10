11adb758be868160e485bb065c3090224f88b687
Benoit Moriceau <benoit@airbyte.io>
2021-11-09 17:01:06 -0800

    Tmp

--- tools/bin/test-temporal-migration.sh
+++ tools/bin/test-temporal-migration.sh
@@ -17,11 +17,11 @@ SUB_BUILD=PLATFORM "$SCRIPT_DIR"/../../gradlew -p "$SCRIPT_DIR"/../.. assemble
 
 curl https://raw.githubusercontent.com/airbytehq/airbyte/master/docker-compose.yaml > /tmp/old-docker-compose/docker-compose.yaml
 
-docker-compose -f /tmp/old-docker-compose/docker-compose.debug.yaml up &
+VERSION=dev docker-compose -f /tmp/old-docker-compose/docker-compose.yaml up &
 
 sleep 2m
 
 git checkout $NEW_HASH
 SUB_BUILD=PLATFORM "$SCRIPT_DIR"/../../gradlew -p "$SCRIPT_DIR"/../.. assemble
 
-docker-compose -f "$SCRIPT_DIR"/../../docker-compose.yaml up
+VERSION=dev docker-compose -f "$SCRIPT_DIR"/../../docker-compose.yaml up
