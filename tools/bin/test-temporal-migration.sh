686f24803b1d6a10fa259359087cd55f359ce58c
Benoit Moriceau <benoit@airbyte.io>
2021-11-10 13:06:34 -0800

    get rid of timeout

--- tools/bin/test-temporal-migration.sh
+++ tools/bin/test-temporal-migration.sh
@@ -7,7 +7,7 @@ SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 if ! command -v timeout &> /dev/null
 then
     echo "timeout could not be found, installing it"
-    brew install timeout
+    brew install coreutils
 fi
 
 NEW_HASH="$( git rev-parse HEAD )"
@@ -17,9 +17,10 @@ git pull --no-rebase
 
 SUB_BUILD=PLATFORM "$SCRIPT_DIR"/../../gradlew -p "$SCRIPT_DIR"/../.. generate-docker
 
-VERSION=dev timeout -k 120s 60s docker-compose -f "$SCRIPT_DIR"/../../docker-compose.yaml up &
+VERSION=dev docker-compose -f "$SCRIPT_DIR"/../../docker-compose.yaml up &
 pid=$!
-wait pid
+sleep 75
+kill -9 $pid
 
 git stash
 git checkout $NEW_HASH
