c218c231744180652cbac12314433c33972405db
Benoit Moriceau <benoit@airbyte.io>
2021-11-09 16:46:45 -0800

    Tmp

--- tools/bin/test-temporal-migration.sh
+++ tools/bin/test-temporal-migration.sh
@@ -4,16 +4,24 @@ set -ex
 
 SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
-while getopts o:t: params
-do
-  case "${params}" in
-    o) originalVersion=${OPTARG};;
-    t) targetVersion=${OPTARG};;
-  esac
-done
-
+rm -rf /tmp/old-docker-compose
 mkdir /tmp/old-docker-compose
 
-"SUB_BUILD=PLATFORM $SCRIPT_DIR/../../gradlew assemble"
+NEW_HASH="$( git rev-parse HEAD )"
 
-curl https://raw.githubusercontent.com/airbytehq/airbyte/master/docker-compose.debug.yaml > /tmp/old-docker-compose.yaml
+git checkout master
+git pull --no-rebase
+
+# TODO: Change to docker-image build when merge
+SUB_BUILD=PLATFORM "$SCRIPT_DIR"/../../gradlew -p "$SCRIPT_DIR"/../.. assemble
+
+curl https://raw.githubusercontent.com/airbytehq/airbyte/master/docker-compose.yaml > /tmp/old-docker-compose/docker-compose.yaml
+
+docker-compose -f /tmp/old-docker-compose/docker-compose.debug.yaml up &
+
+sleep 2m
+
+git checkout $NEW_HASH
+SUB_BUILD=PLATFORM "$SCRIPT_DIR"/../../gradlew -p "$SCRIPT_DIR"/../.. assemble
+
+docker-compose -f "$SCRIPT_DIR"/../../docker-compose.yaml up
