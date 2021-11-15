d7d74439f49b32d2447d4b432a03357db1e48a01
monai <monai@cure.lt>
2021-11-15 15:32:02 +0200

    Cleanup

--- airbyte-integrations/connectors/source-linnworks/source_linnworks/streams.py
+++ airbyte-integrations/connectors/source-linnworks/source_linnworks/streams.py
@@ -78,7 +78,6 @@ class StockLocations(LinnworksStream):
     def path(
         self, stream_state: Mapping[str, Any] = None, stream_slice: Mapping[str, Any] = None, next_page_token: Mapping[str, Any] = None
     ) -> str:
-        print("--- alio")
         return "/api/Inventory/GetStockLocations"
 
 
