e4e30cb06d628bd009dab082aaf80c77a31575e4
Sergei Solonitcyn <11441558+sergei-solonitcyn@users.noreply.github.com>
2021-11-19 00:16:35 +0200

    Update source.py

--- airbyte-integrations/connectors/source-mailgun/source_mailgun/source.py
+++ airbyte-integrations/connectors/source-mailgun/source_mailgun/source.py
@@ -13,20 +13,6 @@ from airbyte_cdk.sources import AbstractSource
 from airbyte_cdk.sources.streams import Stream
 from airbyte_cdk.sources.streams.http import HttpStream
 
-"""
-TODO: Most comments in this class are instructive and should be deleted after the source is implemented.
-
-This file provides a stubbed example of how to use the Airbyte CDK to develop both a source connector which supports full refresh or and an
-incremental syncs from an HTTP API.
-
-The various TODOs are both implementation hints and steps - fulfilling all the TODOs should be sufficient to implement one basic and one incremental
-stream from a source. This pattern is the same one used by Airbyte internally to implement connectors.
-
-The approach here is not authoritative, and devs are free to use their own judgement.
-
-There are additional required TODOs in the files within the integration_tests folder and the spec.json file.
-"""
-
 
 # Basic full refresh stream
 class MailgunStream(HttpStream, ABC):
