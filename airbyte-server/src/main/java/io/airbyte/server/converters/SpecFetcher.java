f36c308499bd59ea31b7b71f078d7991c44c245c
lmossman <lake@airbyte.io>
2021-11-22 15:11:24 -0800

    run gw format

--- airbyte-server/src/main/java/io/airbyte/server/converters/SpecFetcher.java
+++ airbyte-server/src/main/java/io/airbyte/server/converters/SpecFetcher.java
@@ -5,23 +5,16 @@
 package io.airbyte.server.converters;
 
 import com.google.common.base.Preconditions;
-import io.airbyte.commons.docker.DockerUtils;
-import io.airbyte.config.JobConfig.ConfigType;
-import io.airbyte.config.StandardDestinationDefinition;
-import io.airbyte.config.StandardSourceDefinition;
 import io.airbyte.protocol.models.ConnectorSpecification;
-import io.airbyte.scheduler.client.SynchronousJobMetadata;
 import io.airbyte.scheduler.client.SynchronousResponse;
-import io.airbyte.scheduler.client.SynchronousSchedulerClient;
-import java.io.IOException;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class SpecFetcher {
+
   public static ConnectorSpecification getSpecFromJob(final SynchronousResponse<ConnectorSpecification> response) {
     Preconditions.checkState(response.isSuccess(), "Get Spec job failed.");
     Preconditions.checkNotNull(response.getOutput(), "Get Spec job return null spec");
 
     return response.getOutput();
   }
+
 }
