a1e046a4da3dc1a0801d5d2028b1cfc1d782ef6b
Marcos Eliziario Santos <marcos@coremarcos.com>
2021-11-23 13:40:02 -0300

    Secure logger implementation and unit tests

--- airbyte-cdk/python/airbyte_cdk/secure_logger.py
+++ airbyte-cdk/python/airbyte_cdk/secure_logger.py
@@ -9,7 +9,7 @@ from typing import Mapping, Optional
 
 from airbyte_cdk.sources import Source
 
-from .logger import LOGGING_CONFIG, TRACE_LEVEL_NUM, AirbyteLogFormatter, AirbyteNativeLogger
+from .logger import LOGGING_CONFIG, TRACE_LEVEL_NUM, AirbyteNativeLogger
 
 
 def init_secure_logger(name: str = None, source: Optional[Source] = None, config: Optional[Mapping] = None):
@@ -17,36 +17,33 @@ def init_secure_logger(name: str = None, source: Optional[Source] = None, config
     logging.setLoggerClass(AirbyteNativeLogger)
     logging.addLevelName(TRACE_LEVEL_NUM, "TRACE")
     logger = logging.getLogger(name)
+    logger.propagate = True
 
     logger.setLevel(TRACE_LEVEL_NUM)
     logging.config.dictConfig(LOGGING_CONFIG)
-    for handler in logger.handlers:
-        handler.setFormatter(AirbyteSecureLogFormatter(source, config))
-
+    logger.addFilter(AirbyteLogFilter(source, config))
     return logger
 
 
-class AirbyteSecureLogFormatter(AirbyteLogFormatter):
-    def __init__(
-        self,
-        fmt: Optional[str] = ...,
-        datefmt: Optional[str] = ...,
-        style: str = ...,
-        validate: bool = ...,
-        source: Source = None,
-        config: Mapping = None,
-    ) -> None:
-        super().__init__(fmt, datefmt, style, validate)
+class AirbyteLogFilter(logging.Filter):
+    def __init__(self, source: Optional[Source] = None, config: Optional[Mapping] = None):
+        super().__init__()
         if source and config:
             self.secrets_strings = self.get_secrets(source, config)
         else:
             self.secrets_strings = []
 
-    def format(self, record: logging.LogRecord) -> str:
-        reduce(lambda log_msg, secret: log_msg.replace(secret, "****"), self.secrets_strings, record.msg)
-        return super(AirbyteSecureLogFormatter, self).format(record)
+    def filter(self, record: logging.LogRecord) -> str:
+        record.msg = reduce(
+            lambda log_msg, secret: log_msg.replace(secret, "****"),
+            self.secrets_strings,
+            record.msg,
+        )
+        return True
 
     @staticmethod
     def get_secrets(source, config):
-        secret_key_names = [k for k, v in source.spec().connectionSpecification["properties"].items() if v.get("airbyte_secret", False)]
-        return [config.get(k) for k in secret_key_names if config.get(k)]
+        secret_key_names = [
+            k for k, v in source.spec().connectionSpecification.get("properties", {}).items() if v.get("airbyte_secret", False)
+        ]
+        return [str(config.get(k)) for k in secret_key_names if config.get(k)]
