297f4e838cce5e8085b9f66934bed04165e86110
Benoit Moriceau <benoit@airbyte.io>
2021-11-23 14:52:31 -0800

    small update

--- airbyte-workers/src/main/java/io/airbyte/workers/temporal/scheduling/ConnectionUpdaterWorkflowImpl.java
+++ airbyte-workers/src/main/java/io/airbyte/workers/temporal/scheduling/ConnectionUpdaterWorkflowImpl.java
@@ -4,7 +4,11 @@
 
 package io.airbyte.workers.temporal.scheduling;
 
+import io.airbyte.workers.temporal.sync.SyncWorkflow;
+import io.temporal.workflow.ChildWorkflowCancellationType;
+import io.temporal.workflow.ChildWorkflowOptions;
 import io.temporal.workflow.Workflow;
+import java.time.Duration;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
@@ -13,16 +17,32 @@ public class ConnectionUpdaterWorkflowImpl implements ConnectionUpdaterWorkflow 
   private boolean canStart = false;
   private boolean isRunning = false;
   private boolean isDeleted = false;
+  private final boolean skipScheduling = false;
 
   @Override
   public SyncResult run() {
-    Workflow.await(() -> canStart);
+    Workflow.await(() -> canStart());
+
+    // TODO: bmoric get time to wait through an activity
+    final Duration timeToWait = Duration.ofSeconds(5);
+
+    Workflow.await(timeToWait, () -> skipScheduling());
+
+    // TODO: Fetch config (maybe store it in GCS)
+    log.info("Starting child WF");
+    Workflow.newChildWorkflowStub(SyncWorkflow.class,
+        ChildWorkflowOptions.newBuilder()
+            // This will cancel the child workflow when the parent is terminated
+            .setCancellationType(ChildWorkflowCancellationType.TRY_CANCEL)
+            .build()
+    );
 
     if (isDeleted) {
       return new SyncResult(true);
     } else {
       Workflow.continueAsNew();
     }
+    // This should not be reachable
     return null;
   }
 
@@ -52,6 +72,10 @@ public class ConnectionUpdaterWorkflowImpl implements ConnectionUpdaterWorkflow 
 
   private Boolean canStart() {
     return canStart;
+  }
+
+  private Boolean skipScheduling() {
+    return skipScheduling;
   }
 
 }
---
+++ airbyte-workers/src/main/java/io/airbyte/workers/temporal/sync/EmptySyncWorkflow.java
@@ -0,0 +1,19 @@
+package io.airbyte.workers.temporal.sync;
+
+import io.airbyte.config.StandardSyncInput;
+import io.airbyte.config.StandardSyncOutput;
+import io.airbyte.scheduler.models.IntegrationLauncherConfig;
+import io.airbyte.scheduler.models.JobRunConfig;
+import java.util.UUID;
+
+public class EmptySyncWorkflow implements SyncWorkflow {
+
+  @Override
+  public StandardSyncOutput run(final JobRunConfig jobRunConfig,
+                                final IntegrationLauncherConfig sourceLauncherConfig,
+                                final IntegrationLauncherConfig destinationLauncherConfig,
+                                final StandardSyncInput syncInput,
+                                final UUID connectionId) {
+    return null;
+  }
+}
