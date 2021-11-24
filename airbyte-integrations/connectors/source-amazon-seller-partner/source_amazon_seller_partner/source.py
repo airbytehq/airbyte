87892f173f8328dd681f30d9bbb08460a99b076f
lizdeika <lizdeika@gmail.com>
2021-11-24 14:11:35 +0200

    Source Amazon Seller Partner: cleanup spec duplicates and order UI fields

--- airbyte-integrations/connectors/source-amazon-seller-partner/source_amazon_seller_partner/source.py
+++ airbyte-integrations/connectors/source-amazon-seller-partner/source_amazon_seller_partner/source.py
@@ -7,7 +7,7 @@ from typing import Any, List, Mapping, Tuple
 import boto3
 import requests
 from airbyte_cdk.logger import AirbyteLogger
-from airbyte_cdk.models import ConnectorSpecification, SyncMode
+from airbyte_cdk.models import SyncMode
 from airbyte_cdk.sources import AbstractSource
 from airbyte_cdk.sources.streams import Stream
 from pydantic import Field
@@ -28,35 +28,12 @@ from source_amazon_seller_partner.streams import (
 )
 
 
-class ConnectorConfig(BaseModel):
-    class Config:
-        title = "Amazon Seller Partner Spec"
-
-    replication_start_date: str = Field(
-        description="UTC date and time in the format 2017-01-25T00:00:00Z. Any data before this date will not be replicated.",
-        pattern="^[0-9]{4}-[0-9]{2}-[0-9]{2}T[0-9]{2}:[0-9]{2}:[0-9]{2}Z$",
-        examples=["2017-01-25T00:00:00Z"],
-    )
-    refresh_token: str = Field(
-        description="The refresh token used obtained via authorization (can be passed to the client instead)", airbyte_secret=True
-    )
-    lwa_app_id: str = Field(description="Your login with amazon app id", airbyte_secret=True)
-    lwa_client_secret: str = Field(description="Your login with amazon client secret", airbyte_secret=True)
-    aws_access_key: str = Field(description="AWS user access key", airbyte_secret=True)
-    aws_secret_key: str = Field(description="AWS user secret key", airbyte_secret=True)
-    role_arn: str = Field(description="The role's arn (needs permission to 'Assume Role' STS)", airbyte_secret=True)
-    aws_environment: AWSEnvironment = Field(
-        description="Affects the AWS base url to be used",
-    )
-    region: AWSRegion = Field(description="Region to pull data from")
-
-
 class SourceAmazonSellerPartner(AbstractSource):
-    def _get_stream_kwargs(self, config: ConnectorConfig) -> Mapping[str, Any]:
-        endpoint, marketplace_id, region = get_marketplaces(config.aws_environment)[config.region]
+    def _get_stream_kwargs(self, config: Mapping[str, Any]) -> Mapping[str, Any]:
+        endpoint, marketplace_id, region = get_marketplaces(config["aws_environment"])[config["region"]]
 
-        boto3_client = boto3.client("sts", aws_access_key_id=config.aws_access_key, aws_secret_access_key=config.aws_secret_key)
-        role = boto3_client.assume_role(RoleArn=config.role_arn, RoleSessionName="guid")
+        boto3_client = boto3.client("sts", aws_access_key_id=config["aws_access_key"], aws_secret_access_key=config["aws_secret_key"])
+        role = boto3_client.assume_role(RoleArn=config["role_arn"], RoleSessionName="guid")
         role_creds = role["Credentials"]
         aws_signature = AWSSignature(
             service="execute-api",
@@ -67,16 +44,16 @@ class SourceAmazonSellerPartner(AbstractSource):
         )
         auth = AWSAuthenticator(
             token_refresh_endpoint="https://api.amazon.com/auth/o2/token",
-            client_secret=config.lwa_client_secret,
-            client_id=config.lwa_app_id,
-            refresh_token=config.refresh_token,
+            client_secret=config["lwa_client_secret"],
+            client_id=config["lwa_app_id"],
+            refresh_token=config["refresh_token"],
             host=endpoint.replace("https://", ""),
         )
         stream_kwargs = {
             "url_base": endpoint,
             "authenticator": auth,
             "aws_signature": aws_signature,
-            "replication_start_date": config.replication_start_date,
+            "replication_start_date": config["replication_start_date"],
             "marketplace_ids": [marketplace_id],
         }
         return stream_kwargs
@@ -88,7 +65,6 @@ class SourceAmazonSellerPartner(AbstractSource):
         Show error message in case of request exception or unexpected response.
         """
         try:
-            config = ConnectorConfig.parse_obj(config)  # FIXME: this will be not need after we fix CDK
             stream_kwargs = self._get_stream_kwargs(config)
             orders_stream = Orders(**stream_kwargs)
             next(orders_stream.read_records(sync_mode=SyncMode.full_refresh))
@@ -101,7 +77,6 @@ class SourceAmazonSellerPartner(AbstractSource):
         """
         :param config: A Mapping of the user input configuration as defined in the connector spec.
         """
-        config = ConnectorConfig.parse_obj(config)  # FIXME: this will be not need after we fix CDK
         stream_kwargs = self._get_stream_kwargs(config)
 
         return [
@@ -116,19 +91,3 @@ class SourceAmazonSellerPartner(AbstractSource):
             VendorInventoryHealthReports(**stream_kwargs),
             Orders(**stream_kwargs),
         ]
-
-    def spec(self, *args, **kwargs) -> ConnectorSpecification:
-        """
-        Returns the spec for this integration. The spec is a JSON-Schema object describing the required
-        configurations (e.g: username and password) required to run this integration.
-        """
-        # FIXME: airbyte-cdk does not parse pydantic $ref correctly. This override won't be needed after the fix
-        schema = ConnectorConfig.schema()
-        schema["properties"]["aws_environment"] = schema["definitions"]["AWSEnvironment"]
-        schema["properties"]["region"] = schema["definitions"]["AWSRegion"]
-
-        return ConnectorSpecification(
-            documentationUrl="https://docs.airbyte.io/integrations/sources/amazon-seller-partner",
-            changelogUrl="https://docs.airbyte.io/integrations/sources/amazon-seller-partner",
-            connectionSpecification=schema,
-        )
