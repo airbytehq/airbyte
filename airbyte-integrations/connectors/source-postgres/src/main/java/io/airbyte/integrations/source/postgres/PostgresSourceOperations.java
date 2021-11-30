769e2d5cf1e580fa027a65670c608002f08cf41b
mkhokh <mariia.khokh@globallogic.com>
2021-11-30 15:20:39 +0200

    Revert "Source-MySql: fix formatting"
    This reverts commit 5a94474a52ac935b6d9b41fb22998ba338f365f3.

--- airbyte-integrations/connectors/source-postgres/src/main/java/io/airbyte/integrations/source/postgres/PostgresSourceOperations.java
+++ airbyte-integrations/connectors/source-postgres/src/main/java/io/airbyte/integrations/source/postgres/PostgresSourceOperations.java
@@ -1,7 +1,3 @@
-/*
- * Copyright (c) 2021 Airbyte, Inc., all rights reserved.
- */
-
 package io.airbyte.integrations.source.postgres;
 
 import com.fasterxml.jackson.databind.JsonNode;
--- airbyte-integrations/connectors/source-postgres/src/test/java/io/airbyte/integrations/source/postgres/PostgresSourceOperationsTest.java
+++ airbyte-integrations/connectors/source-postgres/src/test/java/io/airbyte/integrations/source/postgres/PostgresSourceOperationsTest.java
@@ -1,7 +1,3 @@
-/*
- * Copyright (c) 2021 Airbyte, Inc., all rights reserved.
- */
-
 package io.airbyte.integrations.source.postgres;
 
 import static org.junit.jupiter.api.Assertions.*;
