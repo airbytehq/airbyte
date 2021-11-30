bfd4736356f0fa92b9399a3e875ed7e52a498190
Sergey Chvalyuk <grubberr@gmail.com>
2021-11-30 20:23:23 +0200

    test_boards_stream added
    Signed-off-by: Sergey Chvalyuk <grubberr@gmail.com>

--- airbyte-integrations/connectors/source-trello/unit_tests/test_streams.py
+++ airbyte-integrations/connectors/source-trello/unit_tests/test_streams.py
@@ -5,8 +5,9 @@
 
 from unittest.mock import MagicMock
 
+from airbyte_cdk.sources.streams.http.auth.core import NoAuth
 from pytest import fixture
-from source_trello.source import TrelloStream
+from source_trello.source import Boards, TrelloStream
 
 
 @fixture
@@ -29,3 +30,32 @@ def test_next_page_token(patch_base_class, config):
     inputs = {"response": MagicMock()}
     expected_token = None
     assert stream.next_page_token(**inputs) == expected_token
+
+
+def test_boards_stream(requests_mock):
+    headers = {
+        "x-rate-limit-api-token-max": "1",
+        "x-rate-limit-api-token-remaining": "1",
+        "x-rate-limit-api-key-max": "1",
+        "x-rate-limit-api-key-remaining": "1",
+    }
+
+    mock_boards_request = requests_mock.get(
+        "https://api.trello.com/1/members/me/boards",
+        headers=headers,
+        json=[{"name": "board_1", "id": "611aa0ef37acd675af67dc9b"}, {"name": "board_2", "id": "611aa586ef5f2c8e1deec8b6"}],
+    )
+
+    config = {"authenticator": NoAuth(), "start_date": "2021-02-11T08:35:49.540Z"}
+    stream1 = Boards(config=config)
+    records = list(stream1.read_records(sync_mode=None))
+    assert [
+        {"id": "611aa0ef37acd675af67dc9b", "name": "board_1"},
+        {"id": "611aa586ef5f2c8e1deec8b6", "name": "board_2"},
+    ] == records
+
+    stream2 = Boards(config={**config, "board_ids": ["611aa586ef5f2c8e1deec8b6"]})
+    records = list(stream2.read_records(sync_mode=None))
+    assert [{"id": "611aa586ef5f2c8e1deec8b6", "name": "board_2"}] == records
+
+    assert mock_boards_request.call_count == 2
