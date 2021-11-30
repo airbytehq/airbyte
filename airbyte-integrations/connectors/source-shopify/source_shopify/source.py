a55ef51f48a6fde49825a5f2d048b8388c56a8c8
Baz <oleksandr.bazarnov@globallogic.com>
2021-11-30 18:02:06 +0200

    Source Shopify: fix for missing `properties` & `items` in schemas (#7783)

--- airbyte-integrations/connectors/source-shopify/source_shopify/source.py
+++ airbyte-integrations/connectors/source-shopify/source_shopify/source.py
@@ -72,7 +72,6 @@ class ShopifyStream(HttpStream, ABC):
         """The name of the field in the response which contains the data"""
 
 
-# Basic incremental stream
 class IncrementalShopifyStream(ShopifyStream, ABC):
 
     # Setting the check point interval to the limit of the records output
@@ -103,7 +102,7 @@ class IncrementalShopifyStream(ShopifyStream, ABC):
         # Getting records >= state
         if stream_state:
             for record in records_slice:
-                if record.get(self.cursor_field) >= stream_state.get(self.cursor_field):
+                if record.get(self.cursor_field, "") >= stream_state.get(self.cursor_field):
                     yield record
         else:
             yield from records_slice
@@ -343,7 +342,6 @@ class Locations(ShopifyStream):
 class InventoryLevels(ChildSubstream):
     parent_stream_class: object = Locations
     slice_key = "location_id"
-    cursor_field = "updated_at"
 
     data_field = "inventory_levels"
 
@@ -359,10 +357,7 @@ class InventoryLevels(ChildSubstream):
             return record
 
         # associate the surrogate key
-        yield from map(
-            generate_key,
-            records_stream,
-        )
+        yield from map(generate_key, records_stream)
 
 
 class InventoryItems(ChildSubstream):
