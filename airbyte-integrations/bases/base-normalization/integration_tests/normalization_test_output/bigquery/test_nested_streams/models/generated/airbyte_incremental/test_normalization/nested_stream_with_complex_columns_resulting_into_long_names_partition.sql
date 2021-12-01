74b51b10cb768084c91a45a3c469dd73923d10fa
Christophe Duong <christophe.duong@gmail.com>
2021-12-01 17:13:04 +0100

    Fix normalization when un-nesting (#8378)
    * Remove unique key on exploded nested tables
    
    * un-nest hint
    
    * Regen SQL

--- airbyte-integrations/bases/base-normalization/integration_tests/normalization_test_output/bigquery/test_nested_streams/models/generated/airbyte_incremental/test_normalization/nested_stream_with_complex_columns_resulting_into_long_names_partition.sql
+++ airbyte-integrations/bases/base-normalization/integration_tests/normalization_test_output/bigquery/test_nested_streams/models/generated/airbyte_incremental/test_normalization/nested_stream_with_complex_columns_resulting_into_long_names_partition.sql
@@ -1,7 +1,6 @@
 {{ config(
     cluster_by = "_airbyte_emitted_at",
     partition_by = {"field": "_airbyte_emitted_at", "data_type": "timestamp", "granularity": "day"},
-    unique_key = '_airbyte_ab_id',
     schema = "test_normalization",
     tags = [ "nested" ]
 ) }}
