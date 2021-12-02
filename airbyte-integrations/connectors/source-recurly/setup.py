cc4efbc0b3ae2b6a7e13be9095e1fa99c0623d1e
Vitalii Vdovenko <vitalii.vdovenko@zazmic.com>
2021-12-02 20:06:31 +0200

    Source Microsoft Teams #8315 - reformat

--- airbyte-integrations/connectors/source-recurly/setup.py
+++ airbyte-integrations/connectors/source-recurly/setup.py
@@ -5,11 +5,7 @@
 
 from setuptools import find_packages, setup
 
-MAIN_REQUIREMENTS = [
-    "airbyte-cdk~=0.1",
-    "recurly==3.14.0",
-    "requests"
-]
+MAIN_REQUIREMENTS = ["airbyte-cdk~=0.1", "recurly==3.14.0", "requests"]
 
 TEST_REQUIREMENTS = [
     "pytest~=6.1",
