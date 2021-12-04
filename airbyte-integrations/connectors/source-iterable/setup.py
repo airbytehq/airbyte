954c8e392283b0db120d4bae9de81f64f629f440
Vadym Hevlich <vege1wgw@gmail.com>
2021-12-04 16:56:52 +0200

    Fix docs iterable.md. Update setup.py requirements.

--- airbyte-integrations/connectors/source-iterable/setup.py
+++ airbyte-integrations/connectors/source-iterable/setup.py
@@ -11,7 +11,7 @@ MAIN_REQUIREMENTS = [
     "requests~=2.25",
 ]
 
-TEST_REQUIREMENTS = ["pytest~=6.1", "responses==0.13.3", "freezegun==1.1.0", "pytest-mock~=3.6"]
+TEST_REQUIREMENTS = ["pytest~=6.1", "responses==0.13.3", "freezegun==1.1.0"]
 
 
 setup(
