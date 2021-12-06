b19388564f024e0c61449f2835acc46c86631a09
antixar <antixar@gmail.com>
2021-12-07 01:14:48 +0200

    fix pyenv installation

--- tools/ci_common_utils/ci_common_utils/google_api.py
+++ tools/ci_common_utils/ci_common_utils/google_api.py
@@ -12,6 +12,9 @@ TOKEN_TTL = 3600
 
 @dataclass
 class GoogleApi:
+    """
+    Simple Google API client
+    """
     logger: ClassVar[Logger] = Logger()
 
     config: Mapping[str, Any]
@@ -86,64 +89,3 @@ class GoogleApi:
             "grant_type": "urn:ietf:params:oauth:grant-type:jwt-bearer",
         })
         return resp.json()["access_token"], None
-
-# "${config_file}" "$scopes"
-# `
-# local
-# token_uri =$(_parse_token_uri "${config_file}")
-# local
-# data =$(curl - s - X POST ${token_uri} \
-#     --data-urlencode "assertion=${jwt}" \
-#     --data-urlencode 'grant_type=urn:ietf:params:oauth:grant-type:jwt-bearer'
-#         )
-# echo $data | jq - r.access_token
-# }
-
-# !/usr/bin/env bash
-# Test script to access/generate secrets in Secret Manager
-
-# PROJECT="engineering-devops"
-# SCOPE="https://www.googleapis.com/auth/cloud-platform"
-# SERVICE_ACCOUNT_FILE=secret-manager.json
-# SECRET=my-secret
-# TOKEN_TTL = 3600
-#
-# _var2base64()
-# {
-#     printf
-# "$1" | _urlencode_base64
-# }
-#
-# _urlencode_base64()
-# {
-#     base64 | tr
-# '/+' '_-' | tr - d
-# '=\n'
-# }
-#
-# function
-# _parse_token_uri()
-# {
-#     local
-# config_file =$1
-# local
-# token_uri =$(jq - r.token_uri ${config_file})
-# echo
-# "${token_uri}"
-# }
-#
-# function
-
-#
-# function
-# parse_project_id()
-# {
-#     # find a project_id into config file
-#     local
-# config_file =$1
-# local
-# project_id =$(jq - r.project_id ${config_file})
-# echo
-# "${project_id}"
-# }
-#
