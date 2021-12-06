049fbe21844419cae5f142c7f1c5814f11975549
vitaliizazmic <75620293+vitaliizazmic@users.noreply.github.com>
2021-12-06 19:04:49 +0200

    🎉 Source Microsoft Teams: migrate to the CDK
    * Source Microsoft Teams #8315 - migrate to the CDK
    
    * Source Microsoft Teams #8315 - acceptance tests
    
    * Source Google Sheets #8313 - removing configured_catalog for SAT according to PR review
    
    * Source Google Sheets #8313 - bump version and update docs
    
    * Source Microsoft Teams #8315 - update source spec

--- airbyte-integrations/connectors/source-microsoft-teams/main.py
+++ airbyte-integrations/connectors/source-microsoft-teams/main.py
@@ -5,7 +5,7 @@
 
 import sys
 
-from base_python.entrypoint import launch
+from airbyte_cdk.entrypoint import launch
 from source_microsoft_teams import SourceMicrosoftTeams
 
 if __name__ == "__main__":
