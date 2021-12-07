9472d98b3fe2f5a6bbc4f78312ee3312978d4c11
Steven Baldwin <sbaldwin@rockset.com>
2021-12-07 13:17:52 -0800

    PR Comments

--- airbyte-integrations/connectors/destination-rockset/src/main/java/io/airbyte/integrations/destination/rockset/RocksetDestination.java
+++ airbyte-integrations/connectors/destination-rockset/src/main/java/io/airbyte/integrations/destination/rockset/RocksetDestination.java
@@ -1,38 +1,16 @@
 /*
- * MIT License
- *
- * Copyright (c) 2020 Airbyte
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
+ * Copyright (c) 2021 Airbyte, Inc., all rights reserved.
  */
 
 package io.airbyte.integrations.destination.rockset;
 
-import static io.airbyte.integrations.destination.rockset.RocksetUtils.WORKSPACE_ID;
+import static io.airbyte.integrations.destination.rockset.RocksetUtils.ROCKSET_WORKSPACE_ID;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.common.collect.ImmutableMap;
 import com.rockset.client.ApiClient;
-import com.rockset.client.RocksetClient;
-import com.rockset.client.api.CollectionsApi;
 import com.rockset.client.api.DocumentsApi;
 import com.rockset.client.model.AddDocumentsRequest;
 import io.airbyte.integrations.BaseConnector;
@@ -66,12 +44,12 @@ public class RocksetDestination extends BaseConnector implements Destination {
         ApiClient client = null;
         String workspace = null;
         // Create a temporary table
-        final String cname = "tmp_test_collection_" + RandomStringUtils.randomAlphabetic(7).toLowerCase();
+        final String cname = "tmp_test_airbyte_collection_" + RandomStringUtils.randomAlphabetic(7).toLowerCase();
 
         try {
             client = RocksetUtils.apiClientFromConfig(config);
 
-            workspace = config.get(WORKSPACE_ID).asText();
+            workspace = config.get(ROCKSET_WORKSPACE_ID).asText();
             RocksetUtils.createWorkspaceIfNotExists(client, workspace);
 
 
