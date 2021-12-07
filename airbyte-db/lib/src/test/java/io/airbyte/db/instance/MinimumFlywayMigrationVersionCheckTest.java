bc1a5adea0f8fec9769628c85d52d5bedbff26f8
Davin Chia <davinchia@gmail.com>
2021-12-08 01:33:59 +0800

    Checkpoint: Add test class.

---
+++ airbyte-db/lib/src/test/java/io/airbyte/db/instance/MinimumFlywayMigrationVersionCheckTest.java
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) 2021 Airbyte, Inc., all rights reserved.
+ */
+
+package io.airbyte.db.instance;
+
+import org.junit.jupiter.api.Test;
+
+public class MinimumFlywayMigrationVersionCheckTest {
+
+  @Test
+  void testFulfilled() {
+
+  }
+
+  @Test
+  void testFulfilledAfter() {
+
+  }
+
+  @Test
+  void testTimeout() {
+
+  }
+
+}
