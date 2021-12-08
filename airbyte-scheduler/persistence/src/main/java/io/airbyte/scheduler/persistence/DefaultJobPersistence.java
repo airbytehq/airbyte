fa69a14b956cdb645a334b2f867c6b5ec7f3d346
Lake Mossman <lake@airbyte.io>
2021-12-08 15:26:40 -0800

    Prevent deadlock when importing database (#8648)
    * lock all tables at beginning of transaction to avoid deadlocks
    
    * fix lock statement format

--- airbyte-scheduler/persistence/src/main/java/io/airbyte/scheduler/persistence/DefaultJobPersistence.java
+++ airbyte-scheduler/persistence/src/main/java/io/airbyte/scheduler/persistence/DefaultJobPersistence.java
@@ -640,6 +640,10 @@ public class DefaultJobPersistence implements JobPersistence {
     if (!data.isEmpty()) {
       createSchema(BACKUP_SCHEMA);
       jobDatabase.transaction(ctx -> {
+        // obtain locks on all tables first, to prevent deadlocks
+        for (final JobsDatabaseSchema tableType : data.keySet()) {
+          ctx.execute(String.format("LOCK TABLE %s IN ACCESS EXCLUSIVE MODE", tableType.name()));
+        }
         for (final JobsDatabaseSchema tableType : data.keySet()) {
           if (!incrementalImport) {
             truncateTable(ctx, targetSchema, tableType.name(), BACKUP_SCHEMA);
