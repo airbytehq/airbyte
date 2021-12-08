0e772a78f6df85958092e489b37a91328ca44adc
alvova <annalvova05@gmail.com>
2021-12-08 16:47:37 +0200

    add window_in_days in check

--- airbyte-integrations/connectors/source-google-analytics-v4/source_google_analytics_v4/source.py
+++ airbyte-integrations/connectors/source-google-analytics-v4/source_google_analytics_v4/source.py
@@ -522,6 +522,8 @@ class SourceGoogleAnalyticsV4(AbstractSource):
         config["authenticator"] = authenticator
         config["metrics"] = ["ga:14dayUsers"]
         config["dimensions"] = ["ga:date"]
+        if "window_in_days" not in config:
+            config["window_in_days"] = 90
 
         try:
             # test the eligibility of custom_reports input
