fc7412379c11bfa73bafd383d623ee626a48a364
Eugene Kulak <kulak.eugene@gmail.com>
2021-12-10 02:53:16 +0200

    fix tests

--- airbyte-integrations/connectors/source-klaviyo/unit_tests/test_streams.py
+++ airbyte-integrations/connectors/source-klaviyo/unit_tests/test_streams.py
@@ -20,12 +20,20 @@ class SomeStream(KlaviyoStream):
         return "sub_path"
 
 
-class SomeIncrementalStream(SomeStream):
+class SomeIncrementalStream(IncrementalKlaviyoStream):
+    schema = mock.Mock(spec=BaseModel)
     cursor_field = "updated_at"
+
+    def path(self, **kwargs) -> str:
+        return "sub_path"
 
 
 class SomeReverseIncrementalStream(ReverseIncrementalKlaviyoStream):
+    schema = mock.Mock(spec=BaseModel)
     cursor_field = "updated_at"
+
+    def path(self, **kwargs) -> str:
+        return "sub_path"
 
 
 @pytest.fixture(name="response")
@@ -35,7 +43,7 @@ def response_fixture(mocker):
 
 class TestKlaviyoStream:
     def test_schema_is_required_property(self):
-        with pytest.raises(NotImplementedError):
+        with pytest.raises(TypeError, match="Can't instantiate abstract class KlaviyoStream with abstract methods path, schema"):
             KlaviyoStream(api_key="some_key")
 
     def test_get_json_schema(self):
@@ -47,7 +55,7 @@ class TestKlaviyoStream:
     @pytest.mark.parametrize(
         ["response_json", "next_page_token"],
         [
-            ({"end": 108, "total": 0, "page": 9}, {"page": 10}),  # first page
+            ({"end": 108, "total": 110, "page": 0}, {"page": 1}),  # first page
             ({"end": 108, "total": 110, "page": 9}, {"page": 10}),  # has next page
             ({"end": 109, "total": 110, "page": 9}, None),  # last page
         ],
@@ -82,7 +90,7 @@ class TestKlaviyoStream:
 
 class TestIncrementalKlaviyoStream:
     def test_cursor_field_is_required(self):
-        with pytest.raises(NotImplementedError):
+        with pytest.raises(TypeError, match="Can't instantiate abstract class IncrementalKlaviyoStream with abstract methods cursor_field, path, schema"):
             IncrementalKlaviyoStream(api_key="some_key", start_date=START_DATE.isoformat())
 
     @pytest.mark.parametrize(
@@ -102,7 +110,7 @@ class TestIncrementalKlaviyoStream:
             (
                 {"since": 123},
                 {"updated_at": START_DATE.int_timestamp - 1},
-                {"api_key": "some_key", "count": 100, "sort": "asc", "since": START_DATE.int_timestamp},
+                {"api_key": "some_key", "count": 100, "sort": "asc", "since": 123},
             ),
             # and again
             (
@@ -111,7 +119,7 @@ class TestIncrementalKlaviyoStream:
                 {"api_key": "some_key", "count": 100, "sort": "asc", "since": 123},
             ),
             # finally state > start_date and can be used
-            (None, {"updated_at": START_DATE.int_timestamp + 1}, {"api_key": "some_key", "count": 100, "sort": "asc", "since": 123}),
+            (None, {"updated_at": START_DATE.int_timestamp + 1}, {"api_key": "some_key", "count": 100, "sort": "asc", "since": START_DATE.int_timestamp + 1}),
         ],
     )
     def test_request_params(self, next_page_token, stream_state, expected_params):
@@ -124,7 +132,7 @@ class TestIncrementalKlaviyoStream:
         ["current_state", "latest_record", "expected_state"],
         [
             ({}, {"updated_at": 10, "some_field": 100}, {"updated_at": 10}),
-            ({"updated_at": 11}, {"updated_at": 10, "some_field": 100}, {"updated_at": 10}),
+            ({"updated_at": 11}, {"updated_at": 10, "some_field": 100}, {"updated_at": 11}),
             ({"updated_at": 11}, {"updated_at": 12, "some_field": 100}, {"updated_at": 12}),
         ],
     )
@@ -151,7 +159,7 @@ class TestIncrementalKlaviyoStream:
 
 class TestReverseIncrementalKlaviyoStream:
     def test_cursor_field_is_required(self):
-        with pytest.raises(NotImplementedError):
+        with pytest.raises(TypeError, match="Can't instantiate abstract class ReverseIncrementalKlaviyoStream with abstract methods cursor_field, path, schema"):
             ReverseIncrementalKlaviyoStream(api_key="some_key", start_date=START_DATE.isoformat())
 
     def test_state_checkpoint_interval(self):
@@ -159,7 +167,7 @@ class TestReverseIncrementalKlaviyoStream:
 
         assert stream.state_checkpoint_interval == stream.page_size, "reversed stream on the first read commit state for each page"
 
-        stream.request_params(stream_state={"updated_at": 1000})
+        stream.request_params(stream_state={"updated_at": START_DATE.isoformat()})
         assert stream.state_checkpoint_interval is None, "reversed stream should commit state only in the end"
 
     @pytest.mark.parametrize(
@@ -167,8 +175,8 @@ class TestReverseIncrementalKlaviyoStream:
         [
             (None, {}, {"api_key": "some_key", "count": 100, "sort": "asc"}),
             ({"page": 10}, {}, {"api_key": "some_key", "count": 100, "sort": "asc", "page": 10}),
-            (None, {"updated_at": 1000}, {"api_key": "some_key", "count": 100, "sort": "asc"}),
-            ({"page": 10}, {"updated_at": 1000}, {"api_key": "some_key", "count": 100, "sort": "asc", "page": 10}),
+            (None, {"updated_at": START_DATE.isoformat()}, {"api_key": "some_key", "count": 100, "sort": "desc"}),
+            ({"page": 10}, {"updated_at": START_DATE.isoformat()}, {"api_key": "some_key", "count": 100, "sort": "desc", "page": 10}),
         ],
     )
     def test_request_params(self, next_page_token, stream_state, expected_params):
@@ -181,7 +189,7 @@ class TestReverseIncrementalKlaviyoStream:
         ["current_state", "latest_record", "expected_state"],
         [
             ({}, {"updated_at": 10, "some_field": 100}, {"updated_at": 10}),
-            ({"updated_at": 11}, {"updated_at": 10, "some_field": 100}, {"updated_at": 10}),
+            ({"updated_at": 11}, {"updated_at": 10, "some_field": 100}, {"updated_at": 11}),
             ({"updated_at": 11}, {"updated_at": 12, "some_field": 100}, {"updated_at": 12}),
         ],
     )
