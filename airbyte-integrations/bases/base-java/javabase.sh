b6945d85c8db6ecf29147bc229b2083a78d1f8a2
ievgeniit <etsybaev@gmail.com>
2021-12-15 16:41:33 +0200

    Updated entrypoint script for java-based connector to capture a HEAP dump when connector fails with outOfMemory error

--- airbyte-integrations/bases/base-java/javabase.sh
+++ airbyte-integrations/bases/base-java/javabase.sh
@@ -2,6 +2,20 @@
 
 set -e
 
+# if port exists - then we're in cloud and no need to add anything
+if [[ -z $KUBERNETES_PORT ]]; then
+
+  arrayOfSupportedConnectors=("source-postgres" "source-mssql" "source-mysql" )
+
+  # The heap dump would be captured only in case when java-based connector fails with OutOfMemory error
+  if [[ " ${arrayOfSupportedConnectors[*]} " =~ " $APPLICATION " ]]; then
+      JAVA_OPTS=$JAVA_OPTS" -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=/data/dump.hprof"
+      export JAVA_OPTS
+      echo "Added JAVA_OPTS=$JAVA_OPTS"
+      echo "APPLICATION=$APPLICATION"
+  fi
+fi
+
 # Wrap run script in a script so that we can lazy evaluate the value of APPLICATION. APPLICATION is
 # set by the dockerfile that inherits base-java, so it cannot be evaluated when base-java is built.
 # We also need to make sure that stdin of the script is piped to the stdin of the java application.
