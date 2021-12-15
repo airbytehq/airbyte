1791b6c27be15600128744fef57cafebfe893c98
antixar <antixar@gmail.com>
2021-12-15 17:35:22 +0200

    fix docker build

--- airbyte-integrations/connectors/source-intercom/acceptance-test-docker.sh
+++ airbyte-integrations/connectors/source-intercom/acceptance-test-docker.sh
@@ -11,6 +11,6 @@ docker run --rm -it \
     -v /var/run/docker.sock:/var/run/docker.sock \
     -v /tmp:/tmp \
     -v $(pwd):/test_input \
-    airbyte/source-acceptance-test:dev \
+    airbyte/source-acceptance-test \
     --acceptance-test-config /test_input
 
