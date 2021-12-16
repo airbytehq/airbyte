4246b1915fbe48949b2e5941f02f2aa7e514e0f2
VitaliiMaltsev <39538064+VitaliiMaltsev@users.noreply.github.com>
2021-12-16 12:01:21 +0200

    Destination Snowflake: update check method to verify permissions for staging (#8781)
    * Destination Snowflake update check method to verify permission for stages
    
    * fix for jdk 17
    
    * fix for jdk 17
    
    * fix with ci secrets
    
    * fix with ci secrets
    
    * removed snowflake secrets from ci_credentials.sh
    
    * bump version
    
    Co-authored-by: vmaltsev <vitalii.maltsev@globallogic.com>

--- airbyte-integrations/connectors/destination-snowflake/src/main/java/io/airbyte/integrations/destination/snowflake/SnowflakeInternalStagingDestination.java
+++ airbyte-integrations/connectors/destination-snowflake/src/main/java/io/airbyte/integrations/destination/snowflake/SnowflakeInternalStagingDestination.java
@@ -10,14 +10,47 @@ import io.airbyte.db.jdbc.JdbcDatabase;
 import io.airbyte.integrations.base.AirbyteMessageConsumer;
 import io.airbyte.integrations.base.Destination;
 import io.airbyte.integrations.destination.jdbc.AbstractJdbcDestination;
+import io.airbyte.protocol.models.AirbyteConnectionStatus;
 import io.airbyte.protocol.models.AirbyteMessage;
 import io.airbyte.protocol.models.ConfiguredAirbyteCatalog;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.UUID;
 import java.util.function.Consumer;
 
 public class SnowflakeInternalStagingDestination extends AbstractJdbcDestination implements Destination {
 
+  private static final Logger LOGGER = LoggerFactory.getLogger(SnowflakeInternalStagingDestination.class);
+
   public SnowflakeInternalStagingDestination() {
     super("", new SnowflakeSQLNameTransformer(), new SnowflakeStagingSqlOperations());
+  }
+
+  @Override
+  public AirbyteConnectionStatus check(JsonNode config) {
+    SnowflakeSQLNameTransformer nameTransformer = new SnowflakeSQLNameTransformer();
+    SnowflakeStagingSqlOperations snowflakeStagingSqlOperations = new SnowflakeStagingSqlOperations();
+    try (final JdbcDatabase database = getDatabase(config)) {
+      final String outputSchema = super.getNamingResolver().getIdentifier(config.get("schema").asText());
+      attemptSQLCreateAndDropTableOperations(outputSchema, database, nameTransformer, snowflakeStagingSqlOperations);
+      attemptSQLCreateAndDropStages(outputSchema, database, nameTransformer, snowflakeStagingSqlOperations);
+      return new AirbyteConnectionStatus().withStatus(AirbyteConnectionStatus.Status.SUCCEEDED);
+    } catch (final Exception e) {
+      LOGGER.error("Exception while checking connection: ", e);
+      return new AirbyteConnectionStatus()
+              .withStatus(AirbyteConnectionStatus.Status.FAILED)
+              .withMessage("Could not connect with provided configuration. \n" + e.getMessage());
+    }
+  }
+
+  private static void attemptSQLCreateAndDropStages(String outputSchema, JdbcDatabase database, SnowflakeSQLNameTransformer namingResolver, SnowflakeStagingSqlOperations sqlOperations) throws Exception {
+
+      // verify we have permissions to create/drop stage
+      final String outputTableName = namingResolver.getIdentifier("_airbyte_connection_test_" + UUID.randomUUID().toString().replaceAll("-", ""));
+      String stageName = namingResolver.getStageName(outputSchema, outputTableName);;
+      sqlOperations.createStageIfNotExists(database, stageName);
+      sqlOperations.dropStageIfExists(database,stageName);
   }
 
   @Override
