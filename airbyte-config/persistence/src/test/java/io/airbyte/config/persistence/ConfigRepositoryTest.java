a373c80ba76040fabafd99d2794410959f8b4480
pmossman <parker@airbyte.io>
2021-12-15 17:10:36 -0800

    remove accidental import

--- airbyte-config/persistence/src/test/java/io/airbyte/config/persistence/ConfigRepositoryTest.java
+++ airbyte-config/persistence/src/test/java/io/airbyte/config/persistence/ConfigRepositoryTest.java
@@ -36,7 +36,6 @@ import java.util.Optional;
 import java.util.UUID;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayNameGenerator.Standard;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
