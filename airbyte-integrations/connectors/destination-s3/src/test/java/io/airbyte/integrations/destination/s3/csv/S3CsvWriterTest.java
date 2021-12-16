192d6a3fd69644b003148fd88229b6ab3fb85233
Edward Gao <edward.gao@airbyte.io>
2021-12-16 07:26:19 -0800

    add comment

--- airbyte-integrations/connectors/destination-s3/src/test/java/io/airbyte/integrations/destination/s3/csv/S3CsvWriterTest.java
+++ airbyte-integrations/connectors/destination-s3/src/test/java/io/airbyte/integrations/destination/s3/csv/S3CsvWriterTest.java
@@ -294,6 +294,20 @@ class S3CsvWriterTest {
         outputStreams.get(0).toString(StandardCharsets.UTF_8));
   }
 
+  /**
+   * This test really just wants to validate that:
+   * <ul>
+   * <li>we're dumping into the correct directory (fake-bucketPath/fake_namespace/fake_stream) and
+   * that the filename contains the upload time</li>
+   * <li>each S3CsvWriter generates a unique filename suffix (the UUID) so that they don't overwrite
+   * each other</li>
+   * <li>we generate a .csv extension</li>
+   * </ul>
+   * So the UUID check isn't strictly necessary.
+   * <p>
+   * Eventually the output path generator should probably be injected into the S3CsvWriter (and we
+   * would test the generator directly + test that the writer calls the generator)
+   */
   private static void checkObjectName(final String objectName) {
     final String errorMessage = "Object was actually " + objectName;
 
