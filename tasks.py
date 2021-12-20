3dbd717695d34a423cd91f6c1437fbb425b077a4
Sergei Solonitcyn <sergei.solonitcyn@zazmic.com>
2021-12-20 02:15:41 +0200

    airbyte-8278 All-in-one static code checker to be run locally as well as during CI pipelines
    updated to use normal invoke context instead of `subprocess.Pool()`
    add: coverage
    
    Signed-off-by: Sergei Solonitcyn <sergei.solonitcyn@zazmic.com>

--- tasks.py
+++ tasks.py
@@ -1,14 +1,12 @@
 import os
 import shutil
-import sys
 import tempfile
 from glob import glob
-from itertools import zip_longest
 from multiprocessing import Pool
-from subprocess import PIPE, Popen
-from typing import Dict, List, Sequence, Set, Tuple
+from typing import Dict, Iterable, List, Set
 
 import invoke
+import virtualenv
 from invoke import task
 
 ROOT_PROJECT_DIR: str = os.path.abspath(os.path.curdir)
@@ -17,10 +15,11 @@ CONFIG_FILE: str = os.path.join(ROOT_PROJECT_DIR, "pyproject.toml")
 
 CONNECTORS_DIR: str = os.path.join(ROOT_PROJECT_DIR, "airbyte-integrations", "connectors")
 
-# TODO: Get it from a single place with `pre-commit` (or make pre-commit use these tasks)
+# TODO: Get it from a single place with `pre-commit` (or make pre-commit to use these tasks)
 TOOLS_VERSIONS: Dict[str, str] = {
     "black": "21.12b0",
     "colorama": "0.4.4",
+    "coverage": "6.2",
     "flake": "0.0.1a2",
     "isort": "5.10.1",
     "mccabe": "0.6.1",
@@ -31,26 +30,34 @@ TOOLS_VERSIONS: Dict[str, str] = {
 TASK_COMMANDS: Dict[str, List[str]] = {
     "black": [
         f"pip install black~={TOOLS_VERSIONS['black']}",
-        f"black -v {{check_option}} {{source_dir}}/.",
+        f"XDG_CACHE_HOME={os.devnull} black -v {{check_option}} {{source_path}}/.",
+    ],
+    "coverage": [
+        "pip install .",
+        f"pip install coverage[toml]~={TOOLS_VERSIONS['coverage']}",
+        f"coverage report --rcfile={CONFIG_FILE}",
     ],
     "flake": [
         f"pip install mccabe~={TOOLS_VERSIONS['mccabe']}",
         f"pip install pyproject-flake8~={TOOLS_VERSIONS['flake']}",
-        f"pflake8 -v {{source_dir}}",
+        f"pflake8 -v {{source_path}}",
     ],
     "isort": [
         f"pip install colorama~={TOOLS_VERSIONS['colorama']}",
         f"pip install isort~={TOOLS_VERSIONS['isort']}",
-        f"isort -v {{check_option}} {{source_dir}}/.",
+        f"isort -v {{check_option}} {{source_path}}/.",
     ],
     "mypy": [
+        "pip install .",
         f"pip install mypy~={TOOLS_VERSIONS['mypy']}",
-        f"mypy {{source_dir}} --config-file={CONFIG_FILE}",
+        f"mypy {{source_path}} --config-file={CONFIG_FILE}",
     ],
     "test": [
+        "pip install .",
+        f"pip install {os.path.join(CONNECTORS_DIR, os.pardir, 'bases', 'source-acceptance-test')}",
         "pip install .[tests]",
         "pip install pytest-cov",
-        "pytest -v --cov={source_dir} unit_tests",
+        f"pytest -v --cov={{source_path}} unit_tests",
     ],
 }
 
@@ -58,6 +65,7 @@ TASK_COMMANDS: Dict[str, List[str]] = {
 ###########################################################################################################################################
 # HELPER FUNCTIONS
 ###########################################################################################################################################
+
 
 def get_connectors_names() -> Set[str]:
     cur_dir = os.path.abspath(os.curdir)
@@ -75,64 +83,67 @@ def get_connectors_names() -> Set[str]:
 CONNECTORS_NAMES = get_connectors_names()
 
 
-def _run_task(args: Sequence[str], multi_envs=True, **kwargs) -> Tuple[int, str, str]:
-    # TODO: smartly separate for multi calls. `subprocess.run()` could be ok (and easier) for that case.
-    connector_string, task_name = args
-    if multi_envs:
-        source_dir = f"source_{connector_string.replace('-', '_')}"
-        os.chdir(os.path.join(CONNECTORS_DIR, f"source-{connector_string}"))
-    else:
-        source_dir = connector_string
+def _run_single_connector_task(args: Iterable) -> int:
+    """
+    Wrapper for unpack task arguments.
+    """
+    return _run_task(*args)
 
-    runner = Popen("bash", shell=True, stdin=PIPE, stdout=PIPE, stderr=PIPE)
+
+def _run_task(ctx: invoke.Context, connector_string: str, task_name: str, multi_envs=True, **kwargs) -> int:
+    """
+    Run task for
+    """
+    if multi_envs:
+        source_path = f"source_{connector_string.replace('-', '_')}"
+        os.chdir(os.path.join(CONNECTORS_DIR, f"source-{connector_string}"))
+
+    else:
+        source_path = connector_string
 
     venv_name = tempfile.mkdtemp(dir=os.curdir)
-    commands = [
-        f"virtualenv {venv_name}",
-        f"source {os.path.join(venv_name, 'bin', 'activate')}",
-    ]
-    if multi_envs:
-        commands.extend([
-            "pip install -r requirements.txt",
-        ])
+    virtualenv.cli_run([venv_name])
+    activator = os.path.join(os.path.abspath(venv_name), "bin", "activate")
 
-    commands.extend([cmd.format(source_dir=source_dir, **kwargs) for cmd in TASK_COMMANDS[task_name]])
+    commands = []
 
-    runner.stdin.writelines(f"{cmd}\n".encode() for cmd in commands)
-    runner.stdin.flush()
-    out, err = runner.communicate()
+    commands.extend([cmd.format(source_path=source_path, **kwargs) for cmd in TASK_COMMANDS[task_name]])
+
+    exit_code = 0
+    with ctx.prefix(f"source {activator}"):
+        for command in commands:
+            if ctx.run(command).return_code:
+                exit_code = 1
+                break
+
     shutil.rmtree(venv_name)
-    return runner.returncode, out.decode(), err.decode()
+    return exit_code
 
 
-def apply_task_for_connectors(connectors_names: str, task_name: str, multi_envs=False, **kwargs):
+def apply_task_for_connectors(ctx: invoke.Context, connectors_names: str, task_name: str, multi_envs=False, **kwargs):
+    """
+    Run task commands for every connector or for once for a set of connectors, depending on task needs (`multi_envs` param).
+    If `multi_envs == True` task for every connector runs in its own subprocess.
+    """
+    # TODO: Separate outputs to avoid a mess.
+
     connectors = connectors_names.split(",") if connectors_names else CONNECTORS_NAMES
 
     connectors = set(connectors) & CONNECTORS_NAMES
 
-    exit_code: int = 0
-
     if multi_envs:
+
+        task_args = [(ctx, connector, task_name) for connector in connectors]
+        exit_code: int = 0
         with Pool() as pool:
-            for result in pool.imap_unordered(_run_task, zip_longest(connectors, [], fillvalue=task_name)):
-                print(result[1], file=sys.stdout)
-                print()
-                print(result[2], file=sys.stderr)
-                print()
-
-                if result[0]:
+            for result in pool.imap_unordered(_run_single_connector_task, task_args):
+                if result:
                     exit_code = 1
-    else:
-        source_dir = " ".join(
-            [f"{os.path.join(CONNECTORS_DIR, f'source-{connector}')}" for connector in connectors]
-        )
-        result = _run_task([source_dir, task_name], multi_envs=False, **kwargs)
-        print(result[1], file=sys.stdout)
-        print()
-        print(result[2], file=sys.stderr)
-        exit_code = result[0]
 
-    raise invoke.Exit(code=exit_code)
+        invoke.Exit(code=exit_code)
+    else:
+        source_path = " ".join([f"{os.path.join(CONNECTORS_DIR, f'source-{connector}')}" for connector in connectors])
+        _run_task(ctx, source_path, task_name, multi_envs=False, **kwargs)
 
 
 ###########################################################################################################################################
@@ -145,50 +156,76 @@ _arg_help_connectors = (
 )
 
 
+@task(help={"connectors": _arg_help_connectors})
+def all_checks(ctx, connectors=None):
+    """
+    Run following checks one by one with default parameters: black, flake, isort, mypy, test, coverage.
+    Zero exit code indicates about successful passing of all checks.
+    Terminate on the first non-zero exit code.
+    """
+    black(ctx, connectors=connectors)
+    flake(ctx, connectors=connectors)
+    isort(ctx, connectors=connectors)
+    mypy(ctx, connectors=connectors)
+    coverage(ctx, connectors=connectors)
+
+
 @task(help={"connectors": _arg_help_connectors, "write": "Write changes into the files (runs 'black' without '--check' option)"})
-def black(ctx, connectors=None, write=False):  # type: ignore[no-untyped-def]
+def black(ctx, connectors=None, write=False):
     """
     Run 'black' checks for one or more given connector(s) code.
     Zero exit code indicates about successful passing of all checks.
     """
     check_option: str = "" if write else " --check"
-    apply_task_for_connectors(connectors, "black", check_option=check_option)
+    apply_task_for_connectors(ctx, connectors, "black", check_option=check_option)
 
 
 @task(help={"connectors": _arg_help_connectors})
-def flake(ctx, connectors=None):  # type: ignore[no-untyped-def]
+def flake(ctx, connectors=None):
     """
     Run 'flake8' checks for one or more given connector(s) code.
     Zero exit code indicates about successful passing of all checks.
     """
-    apply_task_for_connectors(connectors, "flake")
+    apply_task_for_connectors(ctx, connectors, "flake")
 
 
 @task(help={"connectors": _arg_help_connectors, "write": "Write changes into the files (runs 'black' without '--check' option)"})
-def isort(ctx, connectors=None, write=False):  # type: ignore[no-untyped-def]
+def isort(ctx, connectors=None, write=False):
     """
     Run 'isort' checks for one or more given connector(s) code.
     Zero exit code indicates about successful passing of all checks.
     """
     check_option: str = "" if write else " --check"
-    apply_task_for_connectors(connectors, "isort", check_option=check_option)
+    apply_task_for_connectors(ctx, connectors, "isort", check_option=check_option)
 
 
 @task(help={"connectors": _arg_help_connectors})
-def mypy(ctx, connectors=None):  # type: ignore[no-untyped-def]
+def mypy(ctx, connectors=None):
     """
     Run MyPy checks for one or more given connector(s) code.
     A virtual environment is being created for every one.
     Zero exit code indicates about successful passing of all checks.
     """
-    apply_task_for_connectors(connectors, "mypy", multi_envs=True)
+    apply_task_for_connectors(ctx, connectors, "mypy", multi_envs=True)
 
 
 @task(help={"connectors": _arg_help_connectors})
-def test(ctx, connectors=None):  # type: ignore[no-untyped-def]
+def test(ctx, connectors=None):
     """
     Run unittests for one or more given connector(s).
     A virtual environment is being created for every one.
     Zero exit code indicates about successful passing of all tests.
     """
-    apply_task_for_connectors(connectors, "test", multi_envs=True)
+    apply_task_for_connectors(ctx, connectors, "test", multi_envs=True)
+
+
+@task(help={"connectors": _arg_help_connectors})
+def coverage(ctx, connectors=None):
+    """
+    Check test coverage of code for one or more given connector(s).
+    A virtual environment is being created for every one.
+    "test" command is being run before this one.
+    Zero exit code indicates about enough coverage level.
+    """
+    test(ctx, connectors=connectors)
+    apply_task_for_connectors(ctx, connectors, "coverage", multi_envs=True)
