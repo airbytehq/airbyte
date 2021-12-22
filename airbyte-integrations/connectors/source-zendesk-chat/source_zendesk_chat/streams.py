c24187c43062a03b92a19ff2e61146c71fdbb24f
Oleksandr Bazarnov <oleksandr.bazarnov@globallogic.com>
2021-12-22 20:40:27 +0200

    merged with PR https://github.com/airbytehq/airbyte/pull/9040

--- airbyte-integrations/connectors/source-zendesk-chat/source_zendesk_chat/streams.py
+++ airbyte-integrations/connectors/source-zendesk-chat/source_zendesk_chat/streams.py
@@ -238,7 +238,7 @@ class Bans(IdIncrementalStream):
 
     def get_stream_data(self, response_data) -> List[dict]:
         bans = response_data["ip_address"] + response_data["visitor"]
-        bans = sorted(bans, key=lambda x: pendulum.parse(x["created_at"]))
+        bans = sorted(bans, key=lambda x: pendulum.parse(x["created_at"]) if x["created_at"] else pendulum.min)
         return bans
 
 
