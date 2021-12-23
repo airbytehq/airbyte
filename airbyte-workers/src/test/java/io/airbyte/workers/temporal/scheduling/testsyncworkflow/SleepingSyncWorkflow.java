8cb619aaab2ed08d7c24aff81da7fa5341be5702
Benoit Moriceau <benoit@airbyte.io>
2021-12-23 20:15:38 +0100

    Feature/new temporal scheduler (#8352)
    This getting rid of scheduling with the scheduler app and the job submitter. It is replaced by a temporal workflow which will be responsible to schedule the syncs on time.

---
+++ airbyte-workers/src/test/java/io/airbyte/workers/temporal/scheduling/testsyncworkflow/SleepingSyncWorkflow.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright (c) 2021 Airbyte, Inc., all rights reserved.
+ */
+
+package io.airbyte.workers.temporal.scheduling.testsyncworkflow;
+
+import io.airbyte.config.StandardSyncInput;
+import io.airbyte.config.StandardSyncOutput;
+import io.airbyte.scheduler.models.IntegrationLauncherConfig;
+import io.airbyte.scheduler.models.JobRunConfig;
+import io.airbyte.workers.temporal.sync.SyncWorkflow;
+import io.temporal.workflow.Workflow;
+import java.time.Duration;
+import java.util.UUID;
+
+public class SleepingSyncWorkflow implements SyncWorkflow {
+
+  @Override
+  public StandardSyncOutput run(final JobRunConfig jobRunConfig,
+                                final IntegrationLauncherConfig sourceLauncherConfig,
+                                final IntegrationLauncherConfig destinationLauncherConfig,
+                                final StandardSyncInput syncInput,
+                                final UUID connectionId) {
+
+    Workflow.sleep(Duration.ofSeconds(2));
+
+    return new StandardSyncOutput();
+  }
+
+}
