be09211bda618f583f074833aedd24e80a5d469d
Edward Gao <edward.gao@airbyte.io>
2021-12-23 15:29:56 -0800

    handle m1 error

--- airbyte-integrations/bases/base-normalization/integration_tests/dbt_integration_test.py
+++ airbyte-integrations/bases/base-normalization/integration_tests/dbt_integration_test.py
@@ -447,6 +447,8 @@ class DbtIntegrationTest(object):
                         "Configuration paths exist in your dbt_project.yml",  # When no cte / view are generated
                         "Error loading config file: .dockercfg: $HOME is not defined",  # ignore warning
                         "depends on a node named 'disabled_test' which was not found",  # Tests throwing warning because it is disabled
+                        "The requested image's platform (linux/amd64) does not match the detected host platform "
+                        + "(linux/arm64/v8) and no specific platform was requested",  # temporary patch until we publish images for arm64
                     ]:
                         if except_clause in str_line:
                             is_exception = True
