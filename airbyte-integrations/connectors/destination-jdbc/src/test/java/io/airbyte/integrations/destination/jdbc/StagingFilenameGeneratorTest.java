3db46ba49649f8b89784846e6a646637ffa52f52
Alexander Tsukanov <alexander.tsukanovvv@gmail.com>
2021-12-24 12:32:10 +0200

    🎉 Destination Snowflake and RedShift: Implement the Byte-buffered logic (#8869)
    * airbyte-8336: Byte based approach.
    
    * test-commit
    
    * airbyte-8336: Split file by cnhunks.
    
    * airbyte-8336: Renamed variable.
    
    * airbyte-8336: make snowflake DEFAULT_MAX_BATCH_SIZE_BYTES_SNOWFLAKE constant.
    
    * airbyte-8336: make snowflake DEFAULT_MAX_BATCH_SIZE_BYTES_SNOWFLAKE constant.
    
    * airbyte-8336: make snowflake DEFAULT_MAX_BATCH_SIZE_BYTES_SNOWFLAKE constant.
    
    * airbyte-8336: fix of unit tests
    
    * airbyte-8336: Changed to default buffer size in SnowFlake.
    
    * airbyte-8336: Changed 15 GB to 1 GB for max size.
    
    * airbyte-8336: Changed to default buffer size in SnowFlake.
    
    * airbyte-8336: Bumped connector version.
    
    * airbyte-8336: Bumped connector version.
    
    * airbyte-8336: Bumped connector version.

--- airbyte-integrations/connectors/destination-jdbc/src/test/java/io/airbyte/integrations/destination/jdbc/StagingFilenameGeneratorTest.java
+++ airbyte-integrations/connectors/destination-jdbc/src/test/java/io/airbyte/integrations/destination/jdbc/StagingFilenameGeneratorTest.java
@@ -6,26 +6,31 @@ package io.airbyte.integrations.destination.jdbc;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import io.aesy.datasize.ByteUnit.IEC;
+import io.aesy.datasize.DataSize;
+import io.airbyte.integrations.destination.jdbc.constants.GlobalDataSizeConstants;
 import org.apache.commons.lang3.RandomStringUtils;
 import org.junit.jupiter.api.Test;
 
 class StagingFilenameGeneratorTest {
 
   private static final String STREAM_NAME = RandomStringUtils.randomAlphabetic(5).toLowerCase();
-  private static final int MAX_PARTS_PER_FILE = 3;
+  // Equal to GlobalDataSizeConstants.MAX_BYTE_PARTS_PER_FILE / GlobalDataSizeConstants.DEFAULT_MAX_BATCH_SIZE_BYTES
+  // because <insert explanation here>
+  private static final int EXPECTED_ITERATIONS_WITH_STANDARD_BYTE_BUFFER = 4;
   private static final StagingFilenameGenerator FILENAME_GENERATOR =
-      new StagingFilenameGenerator(STREAM_NAME, MAX_PARTS_PER_FILE);
+      new StagingFilenameGenerator(STREAM_NAME, GlobalDataSizeConstants.DEFAULT_MAX_BATCH_SIZE_BYTES);
 
   @Test
   public void testGetStagingFilename() {
     // the file suffix increments after every MAX_PARTS_PER_FILE method calls
     for (int suffix = 0; suffix < 10; ++suffix) {
-      for (int part = 0; part < MAX_PARTS_PER_FILE; ++part) {
+      for (long part = 0; part < EXPECTED_ITERATIONS_WITH_STANDARD_BYTE_BUFFER; ++part) {
         assertEquals(STREAM_NAME + "_0000" + suffix, FILENAME_GENERATOR.getStagingFilename());
       }
     }
     for (int suffix = 10; suffix < 20; ++suffix) {
-      for (int part = 0; part < MAX_PARTS_PER_FILE; ++part) {
+      for (int part = 0; part < EXPECTED_ITERATIONS_WITH_STANDARD_BYTE_BUFFER; ++part) {
         assertEquals(STREAM_NAME + "_000" + suffix, FILENAME_GENERATOR.getStagingFilename());
       }
     }
