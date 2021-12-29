0c553991dbcc1115a8b0e76d7398a74f2c069a54
Edward Gao <edward.gao@airbyte.io>
2021-12-29 14:21:10 -0800

    regenerate test output

--- airbyte-integrations/bases/base-normalization/integration_tests/normalization_test_output/snowflake/test_nested_streams/first_output/airbyte_incremental/scd/TEST_NORMALIZATION/NESTED_STREAM_WITH_COMPLEX_COLUMNS_RESULTING_INTO_LONG_NAMES_SCD.sql
+++ airbyte-integrations/bases/base-normalization/integration_tests/normalization_test_output/snowflake/test_nested_streams/first_output/airbyte_incremental/scd/TEST_NORMALIZATION/NESTED_STREAM_WITH_COMPLEX_COLUMNS_RESULTING_INTO_LONG_NAMES_SCD.sql
@@ -12,17 +12,6 @@ input_data as (
     -- NESTED_STREAM_WITH_COMPLEX_COLUMNS_RESULTING_INTO_LONG_NAMES from "AIRBYTE_DATABASE".TEST_NORMALIZATION._AIRBYTE_RAW_NESTED_STREAM_WITH_COMPLEX_COLUMNS_RESULTING_INTO_LONG_NAMES
 ),
 
-input_data_with_active_row_num as (
-    select *,
-      row_number() over (
-        partition by ID
-        order by
-            DATE is null asc,
-            DATE desc,
-            _AIRBYTE_EMITTED_AT desc
-      ) as _airbyte_active_row_num
-    from input_data
-),
 scd_data as (
     -- SQL model to build a Type 2 Slowly Changing Dimension (SCD) table for each record identified by their primary key
     select
@@ -35,18 +24,24 @@ scd_data as (
         DATE,
         PARTITION,
       DATE as _AIRBYTE_START_AT,
-      lag(DATE) over (
+        lag(DATE) over (
         partition by ID
         order by
             DATE is null asc,
             DATE desc,
             _AIRBYTE_EMITTED_AT desc
       ) as _AIRBYTE_END_AT,
-      case when _airbyte_active_row_num = 1 then 1 else 0 end as _AIRBYTE_ACTIVE_ROW,
+        case when row_number() over (
+        partition by ID
+        order by
+            DATE is null asc,
+            DATE desc,
+            _AIRBYTE_EMITTED_AT desc
+      ) = 1 then 1 else 0 end as _AIRBYTE_ACTIVE_ROW,
       _AIRBYTE_AB_ID,
       _AIRBYTE_EMITTED_AT,
       _AIRBYTE_NESTED_STREAM_WITH_COMPLEX_COLUMNS_RESULTING_INTO_LONG_NAMES_HASHID
-    from input_data_with_active_row_num
+    from input_data
 ),
 dedup_data as (
     select
--- airbyte-integrations/bases/base-normalization/integration_tests/normalization_test_output/snowflake/test_simple_streams/models/generated/airbyte_incremental/scd/TEST_NORMALIZATION/DEDUP_EXCHANGE_RATE_SCD.sql
+++ airbyte-integrations/bases/base-normalization/integration_tests/normalization_test_output/snowflake/test_simple_streams/models/generated/airbyte_incremental/scd/TEST_NORMALIZATION/DEDUP_EXCHANGE_RATE_SCD.sql
@@ -54,6 +54,7 @@ input_data as (
     -- DEDUP_EXCHANGE_RATE from {{ source('TEST_NORMALIZATION', '_AIRBYTE_RAW_DEDUP_EXCHANGE_RATE') }}
 ),
 {% endif %}
+{%- if var("destination") == "clickhouse" %}
 input_data_with_active_row_num as (
     select *,
       row_number() over (
@@ -65,6 +66,7 @@ input_data_with_active_row_num as (
       ) as _airbyte_active_row_num
     from input_data
 ),
+{%- endif %}
 scd_data as (
     -- SQL model to build a Type 2 Slowly Changing Dimension (SCD) table for each record identified by their primary key
     select
@@ -82,18 +84,35 @@ scd_data as (
         NZD,
         USD,
       DATE as _AIRBYTE_START_AT,
-      lag(DATE) over (
+      {%- if var("destination") == "clickhouse" %}
+        case when _airbyte_active_row_num = 1 then 1 else 0 end as _AIRBYTE_ACTIVE_ROW,
+        lag(DATE) over (
         partition by ID, CURRENCY, cast(NZD as {{ dbt_utils.type_string() }})
         order by
             DATE is null asc,
             DATE desc,
             _AIRBYTE_EMITTED_AT desc
       ) as _AIRBYTE_END_AT,
-      case when _airbyte_active_row_num = 1 then 1 else 0 end as _AIRBYTE_ACTIVE_ROW,
+      {%- else %}
+        lag(DATE) over (
+        partition by ID, CURRENCY, cast(NZD as {{ dbt_utils.type_string() }})
+        order by
+            DATE is null asc,
+            DATE desc,
+            _AIRBYTE_EMITTED_AT desc
+      ) as _AIRBYTE_END_AT,
+        case when row_number() over (
+        partition by ID, CURRENCY, cast(NZD as {{ dbt_utils.type_string() }})
+        order by
+            DATE is null asc,
+            DATE desc,
+            _AIRBYTE_EMITTED_AT desc
+      ) = 1 then 1 else 0 end as _AIRBYTE_ACTIVE_ROW,
+      {%- endif %}
       _AIRBYTE_AB_ID,
       _AIRBYTE_EMITTED_AT,
       _AIRBYTE_DEDUP_EXCHANGE_RATE_HASHID
-    from input_data_with_active_row_num
+    from input_data
 ),
 dedup_data as (
     select
