9ab63541e12107f9acc39918d7d9edfa398bf415
Oleksandr Bazarnov <oleksandr.bazarnov@globallogic.com>
2021-12-29 21:30:01 +0200

    try 100501

--- airbyte-integrations/connectors/source-quickbooks-singer/setup.py
+++ airbyte-integrations/connectors/source-quickbooks-singer/setup.py
@@ -13,7 +13,7 @@ from setuptools.command.develop import develop
 from setuptools.command.egg_info import egg_info
 from setuptools.command.install import install
 
-TMP_DIR = "/tmp/singer-python"
+TMP_DIR = "/tmp/singer-python11"
 
 
 def check_singer():
@@ -21,7 +21,8 @@ def check_singer():
         check_call(f"git clone -b v5.12.1 --depth 1 https://github.com/singer-io/singer-python.git {TMP_DIR}".split())
     setup_py = Path(TMP_DIR) / "setup.py"
     setup_py.write_text(setup_py.read_text().replace("jsonschema==2.6.0", "jsonschema==3.2.0"))
-    setup_py.write_text(setup_py.read_text().replace("backoff==", "backoff>="))
+    setup_py.write_text(setup_py.read_text().replace("backoff==1.8.0", "backof==1.11.1"))
+    setup_py.write_text(setup_py.read_text().replace("requests==2.23.0", "requests==2.26.0"))
 
 
 class CustomInstallCommand(install):
@@ -53,10 +54,7 @@ MAIN_REQUIREMENTS = [
     "tap-quickbooks @ https://github.com/airbytehq//tap-quickbooks/tarball/v1.0.5-airbyte",
 ]
 
-TEST_REQUIREMENTS = [
-    "pytest~=6.1",
-    # "source-acceptance-test",
-]
+TEST_REQUIREMENTS = ["pytest~=6.1"]
 
 setup(
     name="source_quickbooks_singer",
