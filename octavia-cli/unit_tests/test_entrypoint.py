2afe6c2bb0c9269a39af803cd09aa19fb8ef0c9b
alafanechere <augustin.lafanechere@gmail.com>
2021-12-29 01:12:11 +0100

    test octavia command group

--- octavia-cli/unit_tests/test_entrypoint.py
+++ octavia-cli/unit_tests/test_entrypoint.py
@@ -1,17 +1,37 @@
 #
 # Copyright (c) 2021 Airbyte, Inc., all rights reserved.
 #
+from unittest import mock
 
+import click
 import pytest
 from click.testing import CliRunner
 from octavia_cli import entrypoint
 
 
-def test_octavia():
+@click.command()
+@click.pass_context
+def dumb(ctx):
+    pass
+
+
+@mock.patch("octavia_cli.entrypoint.workspace_api")
+@mock.patch("octavia_cli.entrypoint.airbyte_api_client")
+def test_octavia(mock_airbyte_api_client: mock.Mock, mock_workspace_api: mock.Mock):
+    context_object = {}
+    mock_api_instance = mock_workspace_api.WorkspaceApi.return_value
+    mock_api_instance.list_workspaces.return_value = mock.MagicMock(workspaces=[mock.MagicMock(workspace_id="expected_workspace_id")])
+
+    entrypoint.octavia.add_command(dumb)
     runner = CliRunner()
-    result = runner.invoke(entrypoint.octavia)
+    result = runner.invoke(entrypoint.octavia, ["--airbyte-url", "test-airbyte-url", "dumb"], obj=context_object)
+    mock_airbyte_api_client.Configuration.assert_called_with(host="test-airbyte-url/api")
+    mock_airbyte_api_client.ApiClient.assert_called_with(mock_airbyte_api_client.Configuration.return_value)
+    mock_workspace_api.WorkspaceApi.assert_called_with(mock_airbyte_api_client.ApiClient.return_value)
+    mock_api_instance.list_workspaces.assert_called_once()
+    assert context_object["API_CLIENT"] == mock_airbyte_api_client.ApiClient.return_value
+    assert context_object["WORKSPACE_ID"] == "expected_workspace_id"
     assert result.exit_code == 0
-    assert result.output.startswith("Usage: octavia [OPTIONS] COMMAND [ARGS]...")
 
 
 @pytest.mark.parametrize(
