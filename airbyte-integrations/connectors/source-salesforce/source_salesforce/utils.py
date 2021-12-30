8d66f9b551628c318e7ee4568bdf6acc0ac42976
ykurochkin <zhenia.kurochkin@gmail.com>
2021-12-30 16:15:39 +0200

    update tests, add functions in utils

--- airbyte-integrations/connectors/source-salesforce/source_salesforce/utils.py
+++ airbyte-integrations/connectors/source-salesforce/source_salesforce/utils.py
@@ -25,18 +25,25 @@ def filter_streams(streams_list: list, search_word: str, search_criteria: str):
 def filter_streams_for_test(streams_list: list, search_word: str, search_criteria: str):
     if search_word.isupper():
         search_word = search_word.lower()
-    criteria_mapping = {
-        "starts with": lambda stream_name: stream_name.startswith(search_word),
-        "starts not with": lambda stream_name: not stream_name.startswith(search_word),
-        "ends with": lambda stream_name: stream_name.endswith(search_word),
-        "ends not with": lambda stream_name: not stream_name.endswith(search_word),
-        "contains": lambda stream_name: search_word in stream_name,
-        "not contains": lambda stream_name: search_word not in stream_name,
-        "exacts": lambda stream_name: search_word == stream_name,
-        "not exacts": lambda stream_name: search_word != stream_name,
-    }
+
     new_streams_list = []
     for stream in streams_list:
-        if criteria_mapping[search_criteria](stream.lower()):
+        stream = stream.lower()
+        if search_criteria == "starts with" and stream.startswith(search_word):
             new_streams_list.append(stream)
+        elif search_criteria == "starts not with" and not stream.startswith(search_word):
+            new_streams_list.append(stream)
+        elif search_criteria == "ends with" and stream.endswith(search_word):
+            new_streams_list.append(stream)
+        elif search_criteria == "ends not with" and not stream.endswith(search_word):
+            new_streams_list.append(stream)
+        elif search_criteria == "contains" and search_word in stream:
+            new_streams_list.append(stream)
+        elif search_criteria == "not contains" and search_word not in stream:
+            new_streams_list.append(stream)
+        elif search_criteria == "exacts" and search_word == stream:
+            new_streams_list.append(stream)
+        elif search_criteria == "not exacts" and search_word != stream:
+            new_streams_list.append(stream)
+
     return new_streams_list
