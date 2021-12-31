e41c1bfc4b7d6ad888ddca21a36bf03063df50e6
Noah Kawasaki <noahkawasaki@google.com>
2021-12-31 14:51:54 +0000

    Couple of small improvements

--- tools/bin/check_requirements.sh
+++ tools/bin/check_requirements.sh
@@ -5,14 +5,6 @@ cd "$(dirname "${0}")" || exit 1
 
 echo "Checking Airbyte development tools and versions.."
 
-printf "Docker ";
-# Currently no pinned docker version: https://github.com/airbytehq/airbyte/issues/9229
-if [[ $(which docker) && $(docker --version) ]]; then
-    printf "is installed"
-  else
-    printf "needs to be installed"
-fi;
-
 printf "\n";
 DESIRED_JAVA_VERSION="$(cat ../../.java-version)"
 printf "Java ";
@@ -22,7 +14,7 @@ if [[ "$(which java)" && "$(java --version)" ]];
         # Returns like "openjdk 11.0.12 2021-11-24"
         IFS=' ' read -ra array <<< "${str}"
         java_version="${array[1]}"
-        printf "${java_version} installed"
+        printf "${java_version} is installed"
         if [[ "${java_version}" == "${DESIRED_JAVA_VERSION}" ]];
             then
                 printf " and matches version ${DESIRED_JAVA_VERSION}"
@@ -42,7 +34,7 @@ if [[ "$(which pip)" && "$(pip --version)" ]];
         # Returns like "pip 20.X.Y from.."
         IFS=' ' read -ra array <<< "${str}"
         pip_version="${array[1]}"
-        printf "${pip_version} installed"
+        printf "${pip_version} is installed"
         if [[ "${pip_version}" == "${DESIRED_PIP_VERSION}" ]];
             then
                 printf " and matches version ${DESIRED_PIP_VERSION}"
@@ -62,7 +54,7 @@ if [[ -n "$(which python3)" && -n "$(python3 --version)" ]];
         # Returns like "Python 3.9.9"
         IFS=' ' read -ra array <<< "${str}"
         python_version="${array[1]}"
-        printf "${python_version} installed"
+        printf "${python_version} is installed"
         if [[ "${python_version}" == "${DESIRED_PYTHON_VERSION}" ]];
             then
                 printf " and matches version ${DESIRED_PYTHON_VERSION}"
@@ -80,7 +72,7 @@ if [[ "$(which node)" && "$(node --version)" ]];
     then
         node_version="$(node --version | tr -d v)"
         # Returns like v16.13.0, strip the "v"
-        printf "${node_version} installed"
+        printf "${node_version} is installed"
         if [[ "${node_version}" == "${DESIRED_NODE_VERSION}" ]];
             then
                 printf " and matches version ${DESIRED_NODE_VERSION}"
@@ -89,6 +81,24 @@ if [[ "$(which node)" && "$(node --version)" ]];
         fi
     else
         printf "not installed, please install Node ${DESIRED_NODE_VERSION}"
+fi;
+
+printf "\n"
+printf "Docker ";
+# Currently no pinned docker version: https://github.com/airbytehq/airbyte/issues/9229
+if [[ $(which docker) && $(docker --version) ]]; then
+    printf "is installed"
+  else
+    printf "needs to be installed"
+fi;
+
+printf "\n";
+printf "Docker Compose ";
+# Currently no pinned docker-compose version: https://github.com/airbytehq/airbyte/issues/9229
+if [[ $(which docker-compose) && $(docker-compose --version) ]]; then
+    printf "is installed"
+  else
+    printf "needs to be installed"
 fi;
 
 printf "\n";
