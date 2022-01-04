24b3b5c4b01651c7faff3c91c5df3b767575aeb0
antixar <antixar@gmail.com>
2022-01-04 02:34:24 +0200

    test coverage 4

--- tools/ci_static_check_reports/ci_build_static_checkers_reports/main.py
+++ tools/ci_static_check_reports/ci_build_static_checkers_reports/main.py
@@ -7,47 +7,56 @@ import json
 import os
 import shutil
 import sys
+from pathlib import Path
 from typing import Dict, List
 
 from invoke import Context
 
-sys.path.insert(0, "airbyte-integrations/connectors")
-from tasks import CONFIG_FILE, TOOLS_VERSIONS, _run_task  # noqa
 from ci_common_utils import Logger
 
 LOGGER = Logger()
+
+ROOT_DIR = Path(os.getcwd())
+while str(ROOT_DIR) != "/" and not (ROOT_DIR / "pyproject.toml").is_file():
+    ROOT_DIR = ROOT_DIR.parent
+if str(ROOT_DIR) == "/":
+    LOGGER.critical("this script must be executed into the Airbite repo only")
+
+sys.path.insert(0, str(ROOT_DIR / "airbyte-integrations/connectors"))
+from tasks import CONFIG_FILE, TOOLS_VERSIONS, _run_task  # noqa
+
 TASK_COMMANDS: Dict[str, List[str]] = {
-    # "black": [
-    #     f"pip install black~={TOOLS_VERSIONS['black']}",
-    #     f"XDG_CACHE_HOME={os.devnull} black -v {{check_option}} --diff {{source_path}}/. > {{reports_path}}/black.txt",
-    # ],
+    "black": [
+        f"pip install black~={TOOLS_VERSIONS['black']}",
+        f"XDG_CACHE_HOME={os.devnull} black -v {{check_option}} --diff {{source_path}}/. > {{reports_path}}/black.txt",
+    ],
     "coverage": [
         "pip install .",
         f"pip install coverage[toml]~={TOOLS_VERSIONS['coverage']}",
-        "coverage xml --rcfile={toml_config_file} -o {reports_path}/coverage.xml",
+        "coverage xml --fail-under 0 --rcfile={toml_config_file} -o {reports_path}/coverage.xml",
     ],
-    # "flake": [
-    #     f"pip install mccabe~={TOOLS_VERSIONS['mccabe']}",
-    #     f"pip install pyproject-flake8~={TOOLS_VERSIONS['flake']}",
-    #     f"pip install flake8-junit-report~={TOOLS_VERSIONS['flake_junit']}",
-    #     "pflake8 -v {source_path} --output-file={reports_path}/flake.txt --bug-report",
-    #     "flake8_junit {reports_path}/flake.txt {reports_path}/flake.xml",
-    #     "rm -f {reports_path}/flake.txt",
-    # ],
-    # "isort": [
-    #     f"pip install colorama~={TOOLS_VERSIONS['colorama']}",
-    #     f"pip install isort~={TOOLS_VERSIONS['isort']}",
-    #     "isort -v {check_option} {source_path}/. > {reports_path}/isort.txt",
-    # ],
-    # "mypy": [
-    #     "pip install .",
-    #     f"pip install lxml~={TOOLS_VERSIONS['lxml']}",
-    #     f"pip install mypy~={TOOLS_VERSIONS['mypy']}",
-    #     "mypy {source_path} --config-file={toml_config_file} --cobertura-xml-report={reports_path}",
-    # ],
+    "flake": [
+        f"pip install mccabe~={TOOLS_VERSIONS['mccabe']}",
+        f"pip install pyproject-flake8~={TOOLS_VERSIONS['flake']}",
+        f"pip install flake8-junit-report~={TOOLS_VERSIONS['flake_junit']}",
+        "pflake8 -v {source_path} --output-file={reports_path}/flake.txt --bug-report",
+        "flake8_junit {reports_path}/flake.txt {reports_path}/flake.xml",
+        "rm -f {reports_path}/flake.txt",
+    ],
+    "isort": [
+        f"pip install colorama~={TOOLS_VERSIONS['colorama']}",
+        f"pip install isort~={TOOLS_VERSIONS['isort']}",
+        "isort -v {check_option} {source_path}/. > {reports_path}/isort.txt",
+    ],
+    "mypy": [
+        "pip install .",
+        f"pip install lxml~={TOOLS_VERSIONS['lxml']}",
+        f"pip install mypy~={TOOLS_VERSIONS['mypy']}",
+        "mypy {source_path} --config-file={toml_config_file} --cobertura-xml-report={reports_path}",
+    ],
     "test": [
         "mkdir {venv}/source-acceptance-test",
-        "cp -f $(git ls-tree -r HEAD --name-only {source_acceptance_test_path} | tr '\n' ' ') {venv}/source-acceptance-test",
+        "git ls-tree -r HEAD --name-only {source_acceptance_test_path} | while read src; do cp -f $src {venv}/source-acceptance-test; done",
         "pip install build",
         f"python -m build {os.path.join('{venv}', 'source-acceptance-test')}",
         f"pip install {os.path.join('{venv}', 'source-acceptance-test', 'dist', 'source_acceptance_test-*.whl')}",
@@ -55,15 +64,30 @@ TASK_COMMANDS: Dict[str, List[str]] = {
         "pip install .",
         "pip install .[tests]",
         "pip install pytest-cov",
-        "echo '111111111111 pytest -v --cov={source_path} --cov-report xml:{reports_path}/pytest.xml {source_path}/unit_tests'",
         "pytest -v --cov={source_path} --cov-report xml:{reports_path}/pytest.xml {source_path}/unit_tests",
     ],
 }
 
 
+def print_commands(folder: str, output_folder: str, venv_folder: str, test_name: str) -> int:
+    """Generates a suite of commands only"""
+    if test_name not in TASK_COMMANDS:
+        return LOGGER.error(f"not found the test suite '{test_name}', Available values: {TASK_COMMANDS.keys()}")
+
+    toml_config_file = str(ROOT_DIR / "pyproject.toml")
+    for cmd in TASK_COMMANDS[test_name]:
+        rendered_cmd = cmd.replace(
+            "{source_path}", folder).replace(
+            "{reports_path}", output_folder).replace(
+            "{venv}", venv_folder).replace("{toml_config_file}", toml_config_file).replace(
+            "{source_acceptance_test_path}", str(ROOT_DIR / "airbyte-integrations/bases/source-acceptance-test"))
+        print(rendered_cmd, file=sys.stdout)
+    return 0
+
+
 def build_py_static_checkers_reports(folder: str, output_folder: str) -> int:
     ctx = Context()
-    toml_config_file = os.path.join(os.getcwd(), "pyproject.toml")
+    toml_config_file = str(ROOT_DIR / "pyproject.toml")
 
     if os.path.exists(output_folder):
         shutil.rmtree(output_folder)
@@ -82,7 +106,7 @@ def build_py_static_checkers_reports(folder: str, output_folder: str) -> int:
             task_commands=TASK_COMMANDS,
             toml_config_file=toml_config_file,
             reports_path=output_folder,
-            source_acceptance_test_path=os.path.join(os.getcwd(), "airbyte-integrations/bases/source-acceptance-test"),
+            source_acceptance_test_path=str(ROOT_DIR / "airbyte-integrations/bases/source-acceptance-test"),
         )
         LOGGER.info(f"stop the test '{checker} => {output_folder}'...")
     return 0
@@ -93,28 +117,43 @@ def main() -> int:
     parser.add_argument("changed_modules", nargs="*")
     parser.add_argument("--output_folder", help="Folder where all reports will be saved", required=False, type=str,
                         default=f"{os.getcwd()}/static_checker_reports")
-
+    parser.add_argument("--print_commands", help="print of all test commands for a needed command only", required=False, type=str)
+    parser.add_argument("--venv_folder", help="path to virtual env if needed", required=False, type=str, default=".")
     args = parser.parse_args()
     modules = [json.loads(m) for m in args.changed_modules]
     modules = {m['folder']: m for m in itertools.chain.from_iterable([m if isinstance(m, list) else [m] for m in modules])}
+
     for module in modules.values():
         output_folder = args.output_folder
         if len(modules) > 1:
             output_folder += "/" + module["module"].replace("/", "_")
+        if not os.path.exists(module["folder"]):
+            folder = ROOT_DIR / module["folder"]
+            if folder.is_dir():
+                LOGGER.info(f"corrected {module['folder']} => {folder}")
+                module["folder"] = str(folder)
+            else:
+                return LOGGER.error(f"Not found the folders: {module['folder']} or {folder}")
+        if args.print_commands:
+            for suite in args.print_commands.split(","):
+                if print_commands(
+                        test_name=suite, venv_folder=args.venv_folder,
+                        folder=module["folder"], output_folder=output_folder,
+                ):
+                    return 1
+            return 0
 
         LOGGER.info(f"Found the module {module['module']}, lang: {module['lang']} => {output_folder}")
         if module["lang"] != "py":
             LOGGER.warning(f"Skipped the module: {module} because its tests are not supported now")
             continue
-        elif not os.path.exists(module["folder"]):
-            return LOGGER.error(f"Not found the folder: {module['folder']}")
         elif "setup.py" not in os.listdir(module["folder"]):
             return LOGGER.error(f"Not found the setup.py file in the {module['folder']}")
+
         elif build_py_static_checkers_reports(folder=module["folder"], output_folder=output_folder):
             return 1
-    LOGGER.info("all tests were finished...")
-    return 0
+        LOGGER.info("all tests were finished...")
+        return 0
 
-
-if __name__ == "__main__":
-    sys.exit(main())
+    if __name__ == "__main__":
+        sys.exit(main())
