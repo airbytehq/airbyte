d863236335fb2968eb6dcf5d83c3fff33fb14f23
Liren Tu <tuliren.git@outlook.com>
2022-01-04 15:33:37 -0800

    Download jdbc driver from databricks directly

--- tools/lib/databricks.sh
+++ tools/lib/databricks.sh
@@ -2,18 +2,20 @@
 
 . tools/lib/lib.sh
 
-# Download Spark JDBC driver for the Databricks connector.
-# The original driver file was downloaded from
-# https://databricks.com/spark/jdbc-drivers-download
-# with the acceptance of the following terms & conditions:
+# Whoever runs this script must accept the following terms & conditions:
 # https://databricks.com/jdbc-odbc-driver-license
 _get_databricks_jdbc_driver() {
+  local driver_zip="SimbaSparkJDBC42-2.6.21.1039.zip"
+  local driver_url="https://databricks-bi-artifacts.s3.us-east-2.amazonaws.com/simbaspark-drivers/jdbc/2.6.21/${driver_zip}"
   local connector_path="airbyte-integrations/connectors/destination-databricks"
 
-  if [[ -f "$connector_path"/lib/SparkJDBC42.jar ]] ; then
+  if [[ -f "${connector_path}/lib/SparkJDBC42.jar" ]] ; then
     echo "[Databricks] Spark JDBC driver already exists"
   else
     echo "[Databricks] Downloading Spark JDBC driver..."
-    gsutil cp gs://io-airbyte-build-dependencies/SparkJDBC42.jar "$connector_path"/lib
+    curl -o "${connector_path}/lib/SimbaSparkJDBC42-2.6.21.1039.zip" "${driver_url}"
+    tar -xf "${connector_path}/lib/SimbaSparkJDBC42-2.6.21.1039.zip" SparkJDBC42.jar
+    mv SparkJDBC42.jar "${connector_path}/lib/"
+    rm "${connector_path}/lib/SimbaSparkJDBC42-2.6.21.1039.zip"
   fi
 }
