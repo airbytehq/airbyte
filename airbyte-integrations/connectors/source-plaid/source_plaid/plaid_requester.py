48f02d62d2ae2af3d3946fc40f7f0315fa09b4bb
alafanechere <augustin.lafanechere@gmail.com>
2022-01-05 11:03:51 +0100

    refacto

--- airbyte-integrations/connectors/source-plaid/source_plaid/plaid_requester.py
+++ airbyte-integrations/connectors/source-plaid/source_plaid/plaid_requester.py
@@ -3,11 +3,13 @@
 #
 
 import datetime
+import json
 from typing import Any, Dict, Iterator, Optional
 
 import plaid
 from plaid.api import plaid_api
 from plaid.model.accounts_balance_get_request import AccountsBalanceGetRequest
+from plaid.model.item_public_token_exchange_request import ItemPublicTokenExchangeRequest
 from plaid.model.transactions_get_request import TransactionsGetRequest
 
 BALANCE_ACCOUNT_KEY = "accounts"
@@ -21,24 +23,24 @@ SPEC_ENV_TO_PLAID_ENV = {
 
 
 class PlaidRequester:
-    def __init__(self, api: plaid_api.PlaidApi, access_token: str):
-        self.api_requester = api
-        self.access_token = access_token
-
-    @classmethod
-    def from_config(cls, plaid_env, client_id, api_key, access_token):
+    def __init__(self, plaid_env: str, client_id: str, api_key: str, access_token: str):
         plaid_config = plaid.Configuration(host=SPEC_ENV_TO_PLAID_ENV[plaid_env], api_key={"clientId": client_id, "secret": api_key})
         api_client = plaid.ApiClient(plaid_config)
-        api_requester = plaid_api.PlaidApi(api_client)
-
-        return cls(api_requester, access_token)
+        self.client = plaid_api.PlaidApi(api_client)
+        self.access_token = access_token
 
     def test_connection(self) -> None:
         """
         Test that you can connect to plaid and create a query with the given credentials.
         Raises an error on failure.
         """
-        self.api_requester.accounts_balance_get(AccountsBalanceGetRequest(access_token=self.access_token))
+        try:
+            request = AccountsBalanceGetRequest(access_token=self.access_token)
+            self.client.accounts_balance_get(request)
+            return True, None
+        except plaid.ApiException as e:
+            response = json.loads(e.body)
+            return False, response
 
     def transaction_generator(self, date: Optional[str] = None) -> Iterator[Dict[str, Any]]:
         if not date:
@@ -49,14 +51,14 @@ class PlaidRequester:
         if date >= datetime.datetime.utcnow().date():
             return
 
-        transaction_response = self.api_requester.transactions_get(
+        transaction_response = self.client.transactions_get(
             TransactionsGetRequest(access_token=self.access_token, start_date=date, end_date=datetime.datetime.utcnow().date())
         )
 
         yield from map(lambda x: x.to_dict(), sorted(transaction_response[TRANSACTION_KEY], key=lambda t: t["date"]))
 
     def balance_generator(self) -> Iterator[Dict[str, Any]]:
-        balance_response = self.api_requester.accounts_balance_get(AccountsBalanceGetRequest(access_token=self.access_token))
+        balance_response = self.client.accounts_balance_get(AccountsBalanceGetRequest(access_token=self.access_token))
 
         for balance in balance_response[BALANCE_ACCOUNT_KEY]:
             message_dict = balance["balances"].to_dict()
--- airbyte-integrations/connectors/source-plaid/source_plaid/source.py
+++ airbyte-integrations/connectors/source-plaid/source_plaid/source.py
@@ -2,45 +2,48 @@
 # Copyright (c) 2021 Airbyte, Inc., all rights reserved.
 #
 
+import datetime
+import json
 import pathlib
 from abc import abstractmethod
 from typing import Any, Iterable, List, Mapping, MutableMapping, Optional, Tuple, Union
 
+import plaid
 from airbyte_cdk.logger import AirbyteLogger
 from airbyte_cdk.models import SyncMode
 from airbyte_cdk.sources import AbstractSource
 from airbyte_cdk.sources.streams import Stream
+from plaid.api import plaid_api
+from plaid.model.accounts_balance_get_request import AccountsBalanceGetRequest
+from plaid.model.transactions_get_request import TransactionsGetRequest
 
 from .plaid_requester import PlaidRequester
 
-CATALOG_PATH = pathlib.Path(__file__).parent / "catalog.json"
-CURSOR_FIELD = "date"
-
-
-class SourcePlaid(AbstractSource):
-    def check_connection(self, logger: AirbyteLogger, config: Mapping[str, Any]) -> Tuple[bool, Optional[Any]]:
-        try:
-            api_requester = PlaidRequester.from_config(**config)
-            api_requester.test_connection()
-            return True, None
-        except Exception as error:
-            return False, error
-
-    def streams(self, config: Mapping[str, Any]) -> List[Stream]:
-        plaid_requester = PlaidRequester.from_config(**config)
-        return [BalanceStream(plaid_requester), IncrementalTransactionStream(plaid_requester)]
+SPEC_ENV_TO_PLAID_ENV = {
+    "production": plaid.Environment.Production,
+    "development": plaid.Environment.Development,
+    "sandbox": plaid.Environment.Sandbox,
+}
 
 
 class PlaidStream(Stream):
-    def __init__(self, plaid_requester: PlaidRequester):
-        self.plaid_requester = plaid_requester
+    def __init__(self, config: Mapping[str, Any]):
+        plaid_config = plaid.Configuration(
+            host=SPEC_ENV_TO_PLAID_ENV[config["plaid_env"]], api_key={"clientId": config["client_id"], "secret": config["api_key"]}
+        )
+        api_client = plaid.ApiClient(plaid_config)
+        self.client = plaid_api.PlaidApi(api_client)
+        self.access_token = config["access_token"]
+
+
+class BalanceStream(PlaidStream):
+    @property
+    def name(self):
+        return "balance"
 
     @property
-    def source_defined_cursor(self) -> bool:
-        """
-        Return False if the cursor can be configured by the user.
-        """
-        return True
+    def primary_key(self) -> Optional[Union[str, List[str], List[List[str]]]]:
+        return "account_id"
 
     def read_records(
         self,
@@ -49,45 +52,11 @@ class PlaidStream(Stream):
         stream_slice: Mapping[str, Any] = None,
         stream_state: Mapping[str, Any] = None,
     ) -> Iterable[Mapping[str, Any]]:
-        if sync_mode == SyncMode.incremental:
-            yield from self.stream_generator_function(**stream_state)
-        else:
-            yield from self.stream_generator_function()
-
-    @abstractmethod
-    def stream_generator_function(self, **kwargs):
-        pass
-
-
-class BalanceStream(PlaidStream):
-    def stream_generator_function(self, **_kwargs):
-        yield from self.plaid_requester.balance_generator()
-
-    @property
-    def name(self):
-        return "balance"
-
-    def get_json_schema(self) -> Mapping[str, Any]:
-        return {
-            "type": "object",
-            "required": ["account_id", "current"],
-            "properties": {
-                "account_id": {"type": "string"},
-                "available": {"type": ["number", "null"]},
-                "current": {"type": "number"},
-                "iso_currency_code": {"type": ["string", "null"]},
-                "limit": {"type": ["number", "null"]},
-                "unofficial_currency_code": {"type": ["string", "null"]},
-            },
-        }
-
-    @property
-    def primary_key(self) -> Optional[Union[str, List[str], List[List[str]]]]:
-        return "account_id"
-
-    @property
-    def cursor_field(self) -> Union[str, List[str]]:
-        return []
+        balance_response = self.client.accounts_balance_get(AccountsBalanceGetRequest(access_token=self.access_token))
+        for balance in balance_response["accounts"]:
+            message_dict = balance["balances"].to_dict()
+            message_dict["account_id"] = balance["account_id"]
+            yield message_dict
 
 
 class IncrementalTransactionStream(PlaidStream):
@@ -99,70 +68,60 @@ class IncrementalTransactionStream(PlaidStream):
     def name(self):
         return "transaction"
 
-    def get_updated_state(self, current_stream_state: MutableMapping[str, Any], latest_record: Mapping[str, Any]):
-        return {CURSOR_FIELD: latest_record[CURSOR_FIELD]}
-
-    def stream_generator_function(self, **kwargs):
-        try:
-            yield from self.plaid_requester.transaction_generator(**kwargs)
-        except ValueError:
-            pass
-
-    def get_json_schema(self) -> Mapping[str, Any]:
-        return {
-            "type": "object",
-            "required": ["account_id", "amount", "iso_currency_code", "name", "transaction_id", "category", "date", "transaction_type"],
-            "properties": {
-                "account_id": {"type": "string"},
-                "amount": {"type": "number"},
-                "category": {"type": "array", "items": {"type": "string"}},
-                "category_id": {"type": ["string", "null"]},
-                "date": {"type": "string"},
-                "iso_currency_code": {"type": "string"},
-                "name": {"type": "string"},
-                "payment_channel": {"type": ["string", "null"]},
-                "pending": {"type": ["boolean", "null"]},
-                "transaction_id": {"type": "string"},
-                "transaction_type": {"type": "string"},
-                "location": {
-                    "type": ["object", "null"],
-                    "properties": {
-                        "address": {"type": ["string", "null"]},
-                        "city": {"type": ["string", "null"]},
-                        "country": {"type": ["string", "null"]},
-                        "lat": {"type": ["string", "null"]},
-                        "lon": {"type": ["string", "null"]},
-                        "postal_code": {"type": ["string", "null"]},
-                        "region": {"type": ["string", "null"]},
-                        "store_number": {"type": ["string", "null"]},
-                    },
-                },
-                "payment_meta": {
-                    "type": ["object", "null"],
-                    "properties": {
-                        "by_order_of": {"type": ["string", "null"]},
-                        "payee": {"type": ["string", "null"]},
-                        "payer": {"type": ["string", "null"]},
-                        "payment_method": {"type": ["string", "null"]},
-                        "payment_processor": {"type": ["string", "null"]},
-                        "ppd_id": {"type": ["string", "null"]},
-                        "reason": {"type": ["string", "null"]},
-                        "reference_number": {"type": ["string", "null"]},
-                    },
-                },
-                "account_owner": {"type": ["string", "null"]},
-                "authorized_date": {"type": ["string", "null"]},
-                "authorized_datetime": {"type": ["string", "null"]},
-                "check_number": {"type": ["string", "null"]},
-                "datetime": {"type": ["string", "null"]},
-                "merchant_name": {"type": ["string", "null"]},
-                "pending_transaction_id": {"type": ["string", "null"]},
-                "personal_finance_category": {"type": ["string", "null"]},
-                "transaction_code": {"type": ["string", "null"]},
-                "unofficial_currency_code": {"type": ["string", "null"]},
-            },
-        }
+    @property
+    def source_defined_cursor(self) -> bool:
+        """
+        Return False if the cursor can be configured by the user.
+        """
+        return True
 
     @property
     def cursor_field(self) -> Union[str, List[str]]:
-        return CURSOR_FIELD
+        return "date"
+
+    def get_updated_state(self, current_stream_state: MutableMapping[str, Any], latest_record: Mapping[str, Any]):
+        return {"date": latest_record.get("date")}
+
+    def read_records(
+        self,
+        sync_mode: SyncMode,
+        cursor_field: List[str] = None,
+        stream_slice: Mapping[str, Any] = None,
+        stream_state: Mapping[str, Any] = None,
+    ) -> Iterable[Mapping[str, Any]]:
+        stream_state = stream_state or {}
+        date = stream_state.get("date")
+        if not date:
+            date = datetime.date.fromtimestamp(0)
+        else:
+            date = datetime.date.fromisoformat(date)
+        if date >= datetime.datetime.utcnow().date():
+            return
+
+        transaction_response = self.client.transactions_get(
+            TransactionsGetRequest(access_token=self.access_token, start_date=date, end_date=datetime.datetime.utcnow().date())
+        )
+
+        yield from map(lambda x: x.to_dict(), sorted(transaction_response["transactions"], key=lambda t: t["date"]))
+
+
+class SourcePlaid(AbstractSource):
+    def check_connection(self, logger: AirbyteLogger, config: Mapping[str, Any]) -> Tuple[bool, Optional[Any]]:
+        try:
+            plaid_config = plaid.Configuration(
+                host=SPEC_ENV_TO_PLAID_ENV[config["plaid_env"]], api_key={"clientId": config["client_id"], "secret": config["api_key"]}
+            )
+            api_client = plaid.ApiClient(plaid_config)
+            client = plaid_api.PlaidApi(api_client)
+            try:
+                request = AccountsBalanceGetRequest(access_token=config["access_token"])
+                client.accounts_balance_get(request)
+                return True, None
+            except plaid.ApiException as e:
+                response = json.loads(e.body)
+                return False, response
+        except Exception as error:
+            return False, error
+
+    def streams(self, config: Mapping[str, Any]) -> List[Stream]:
+        return [BalanceStream(config), IncrementalTransactionStream(config)]
