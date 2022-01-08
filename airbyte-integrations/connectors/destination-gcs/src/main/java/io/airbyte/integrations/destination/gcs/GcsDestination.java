275a3ef79162ffcba2ed4b5c8259267b56ae32fb
ievgeniit <etsybaev@gmail.com>
2022-01-08 18:00:14 +0200

    Fixed check method

--- airbyte-integrations/connectors/destination-gcs/src/main/java/io/airbyte/integrations/destination/gcs/GcsDestination.java
+++ airbyte-integrations/connectors/destination-gcs/src/main/java/io/airbyte/integrations/destination/gcs/GcsDestination.java
@@ -4,6 +4,10 @@
 
 package io.airbyte.integrations.destination.gcs;
 
+import static io.airbyte.integrations.destination.gcs.util.Consts.CHECK_ACTIONS_TMP_FILE_NAME;
+import static io.airbyte.integrations.destination.gcs.util.Consts.DUMMY_MIDDLE_SIZE_TEXT;
+import static io.airbyte.integrations.destination.gcs.util.Consts.EXPECTED_ROLES;
+
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.transfer.TransferManager;
 import com.amazonaws.services.s3.transfer.TransferManagerBuilder;
@@ -21,6 +25,7 @@ import io.airbyte.protocol.models.AirbyteMessage;
 import io.airbyte.protocol.models.ConfiguredAirbyteCatalog;
 import java.io.File;
 import java.io.FileWriter;
+import java.io.IOException;
 import java.util.function.Consumer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -36,25 +41,21 @@ public class GcsDestination extends BaseConnector implements Destination {
   @Override
   public AirbyteConnectionStatus check(final JsonNode config) {
     try {
-      final String fileName = "test";
-      final String testContent = "check-content";
       final GcsDestinationConfig destinationConfig = GcsDestinationConfig
           .getGcsDestinationConfig(config);
       final AmazonS3 s3Client = GcsS3Helper.getGcsS3Client(destinationConfig);
-      LOGGER.info("Started testing if all required credentials assigned to user");
-      s3Client.putObject(destinationConfig.getBucketName(), fileName, testContent);
-      s3Client.deleteObject(destinationConfig.getBucketName(), fileName);
-      LOGGER.info("Finished checking for normal upload, started checking multipart uploading...");
+
+      // Test single Upload (for small files) permissions
+      testSingleUpload(s3Client, destinationConfig);
 
       // Test Multipart Upload permissions
-      final TransferManager tm = performTestMultipartUpload(s3Client,
-          destinationConfig.getBucketName(), fileName, testContent);
-      deleteTestObjectAndShutdownClient(s3Client, destinationConfig.getBucketName(), fileName, tm);
+      testMultipartUpload(s3Client, destinationConfig);
 
       return new AirbyteConnectionStatus().withStatus(Status.SUCCEEDED);
     } catch (final Exception e) {
       LOGGER.error("Exception attempting to access the Gcs bucket: {}", e.getMessage());
-      LOGGER.error("Please make sure you account has both \"storage.objects.create\" and \"storage.multipartUploads.create\" roles");
+      LOGGER.error("Please make sure you account has all of these roles: " + EXPECTED_ROLES);
+
       return new AirbyteConnectionStatus()
           .withStatus(AirbyteConnectionStatus.Status.FAILED)
           .withMessage("Could not connect to the Gcs bucket with the provided configuration. \n" + e
@@ -62,35 +63,43 @@ public class GcsDestination extends BaseConnector implements Destination {
     }
   }
 
-  private TransferManager performTestMultipartUpload(final AmazonS3 s3Client,
-                                                     final String bucketName,
-                                                     final String fileName,
-                                                     final String testContent)
-      throws Exception {
-    final TransferManager tm = TransferManagerBuilder.standard()
-        .withS3Client(s3Client)
-        .build();
-    // TransferManager processes all transfers asynchronously,
-    // so this call returns immediately.
-    final File tmpFile = File.createTempFile(fileName, ".tmp");
-    try (final FileWriter writer = new FileWriter(tmpFile)) {
-      writer.write(testContent);
-    }
-    // TransferManager processes all transfers asynchronously,
-    // so this call returns immediately.
-    final Upload upload = tm.upload(bucketName, fileName, tmpFile);
-    upload.waitForCompletion();
-    LOGGER.info("Object upload complete");
-    return tm;
+  private void testSingleUpload(final AmazonS3 s3Client, final GcsDestinationConfig destinationConfig){
+    LOGGER.info("Started testing if all required credentials assigned to user for single file uploading");
+    s3Client.putObject(destinationConfig.getBucketName(), CHECK_ACTIONS_TMP_FILE_NAME, DUMMY_MIDDLE_SIZE_TEXT);
+    s3Client.deleteObject(destinationConfig.getBucketName(), CHECK_ACTIONS_TMP_FILE_NAME);
+    LOGGER.info("Finished checking for normal upload mode");
   }
 
-  private void deleteTestObjectAndShutdownClient(final AmazonS3 s3Client,
-                                                 final String bucketName,
-                                                 final String fileName,
-                                                 final TransferManager tm) {
-    s3Client.deleteObject(bucketName, fileName);
-    LOGGER.info("All checks have passed.");
-    tm.shutdownNow(true);
+  private void testMultipartUpload(final AmazonS3 s3Client, final GcsDestinationConfig destinationConfig)
+      throws Exception {
+
+    LOGGER.info("Started testing if all required credentials assigned to user for Multipart upload");
+    final TransferManager tm = TransferManagerBuilder.standard()
+        .withS3Client(s3Client)
+        // Sets the size threshold, in bytes, for when to use multipart uploads. Uploads over this size will
+        // automatically use a multipart upload strategy, while uploads smaller than this threshold will use
+        // a single connection to upload the whole object. So we need to set it as small for testing connection
+        .withMultipartUploadThreshold(1024L) // set 1KB as part size
+        .build();
+
+    try{
+      // TransferManager processes all transfers asynchronously,
+      // so this call returns immediately.
+      final Upload upload = tm.upload(destinationConfig.getBucketName(), CHECK_ACTIONS_TMP_FILE_NAME, getTmpFileToUpload());
+      upload.waitForCompletion();
+      s3Client.deleteObject(destinationConfig.getBucketName(), CHECK_ACTIONS_TMP_FILE_NAME);
+    } finally {
+      tm.shutdownNow(true);
+    }
+    LOGGER.info("Finished verification for multipart upload mode");
+  }
+
+  private File getTmpFileToUpload() throws IOException {
+    final File tmpFile = File.createTempFile(CHECK_ACTIONS_TMP_FILE_NAME, ".tmp");
+    try (final FileWriter writer = new FileWriter(tmpFile)) {
+      writer.write(DUMMY_MIDDLE_SIZE_TEXT);
+    }
+    return tmpFile;
   }
 
   @Override
