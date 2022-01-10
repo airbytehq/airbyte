52037091f1cb081c33b3189c275388a0adf07e91
vmaltsev <vitalii.maltsev@globallogic.com>
2022-01-10 12:26:35 +0200

    replace concat with +

--- airbyte-integrations/connectors/destination-snowflake/src/main/java/io/airbyte/integrations/destination/snowflake/SnowflakeSQLNameTransformer.java
+++ airbyte-integrations/connectors/destination-snowflake/src/main/java/io/airbyte/integrations/destination/snowflake/SnowflakeSQLNameTransformer.java
@@ -18,6 +18,6 @@ public class SnowflakeSQLNameTransformer extends ExtendedNameTransformer {
   }
 
   public String getStagingPath(String schemaName, String tableName, String currentSyncPath) {
-    return getStageName(schemaName,tableName).concat("/staged/").concat(currentSyncPath).toUpperCase();
+    return (getStageName(schemaName,tableName)+"/staged/"+currentSyncPath).toUpperCase();
   }
 }
