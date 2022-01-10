a911f54f17accfaa477a84fdf61f50da80364281
alafanechere <augustin.lafanechere@gmail.com>
2022-01-10 10:58:21 +0100

    implement and test definitions

--- octavia-cli/octavia_cli/list/definitions.py
+++ octavia-cli/octavia_cli/list/definitions.py
@@ -3,7 +3,7 @@
 #
 import abc
 from enum import Enum
-from typing import Union
+from typing import List, Union
 
 import airbyte_api_client
 from airbyte_api_client.api import destination_definition_api, source_definition_api
@@ -16,8 +16,9 @@ class DefinitionType(Enum):
 
 class Definitions(abc.ABC):
     @property
+    @abc.abstractmethod
     def api(self) -> Union[source_definition_api.SourceDefinitionApi, destination_definition_api.DestinationDefinitionApi]:
-        raise NotImplementedError
+        pass
 
     def __init__(self, definition_type: DefinitionType, api_client: airbyte_api_client.ApiClient):
         self.definition_type = definition_type
@@ -45,10 +46,36 @@ class Definitions(abc.ABC):
         ]
         return definitions
 
+    # TODO alafanechere: declare in a specific formatting module because it will probably be reused
+    @staticmethod
+    def _compute_col_width(data: List[List[str]], padding: int = 2) -> int:
+        """Compute column width for display purposes:
+        Find largest column size, add a padding of two characters.
+        Returns:
+            data (List[List[str]]): Tabular data containing rows and columns.
+            padding (int): Number of character to adds to create space between columns.
+        Returns:
+            col_width (int): The computed column width according to input data.
+        """
+        col_width = max(len(col) for row in data for col in row) + padding
+        return col_width
+
+    # TODO alafanechere: declare in a specific formatting module because it will probably be reused
+    @staticmethod
+    def _display_as_table(data: List[List[str]]) -> str:
+        """Formats tabular input data into a displayable table with columns.
+        Args:
+            data (List[List[str]]): Tabular data containing rows and columns.
+        Returns:
+            table (str): String representation of input tabular data.
+        """
+        col_width = Definitions._compute_col_width(data)
+        table = "\n".join(["".join(col.ljust(col_width) for col in row) for row in data])
+        return table
+
     def __repr__(self):
         definitions = [[f.upper() for f in self.fields_to_display]] + self.latest_definitions
-        col_width = max(len(col) for row in definitions for col in row) + 2  # padding
-        return "\n".join(["".join(col.ljust(col_width) for col in row) for row in definitions])
+        return self._display_as_table(definitions)
 
 
 class SourceDefinitions(Definitions):
@@ -57,8 +84,8 @@ class SourceDefinitions(Definitions):
     def __init__(self, api_client: airbyte_api_client.ApiClient):
         super().__init__(DefinitionType.SOURCE, api_client)
 
-    def list_latest_definitions(self, api_instance):
-        api_response = source_definition_api.SourceDefinitionApi.list_latest_source_definitions(api_instance)
+    def list_latest_definitions(self):
+        api_response = self.api.list_latest_source_definitions(self.api_instance)
         return self._parse_response(api_response)
 
 
@@ -68,6 +95,6 @@ class DestinationDefinitions(Definitions):
     def __init__(self, api_client: airbyte_api_client.ApiClient):
         super().__init__(DefinitionType.DESTINATION, api_client)
 
-    def list_latest_definitions(self, api_instance):
-        api_response = destination_definition_api.DestinationDefinitionApi.list_latest_destination_definitions(api_instance)
+    def list_latest_definitions(self):
+        api_response = self.api.list_latest_destination_definitions(self.api_instance)
         return self._parse_response(api_response)
--- octavia-cli/unit_tests/test_list/test_definitions.py
+++ octavia-cli/unit_tests/test_list/test_definitions.py
@@ -2,7 +2,13 @@
 # Copyright (c) 2021 Airbyte, Inc., all rights reserved.
 #
 import pytest
-from octavia_cli.list.definitions import Definitions, DefinitionType
+from airbyte_api_client.api import destination_definition_api, source_definition_api
+from octavia_cli.list.definitions import (
+    Definitions,
+    DefinitionType,
+    DestinationDefinitions,
+    SourceDefinitions,
+)
 
 
 def test_definition_type():
@@ -34,6 +40,11 @@ class TestDefinitions:
         mock_api.assert_called_with(mock_api_client)
         assert definitions.api_instance == mock_api.return_value
 
+    def test_abstract_methods(self, mock_definition_type, mock_api_client):
+        assert Definitions.__abstractmethods__ == {"api", "list_latest_definitions"}
+        with pytest.raises(TypeError):
+            Definitions(mock_definition_type, mock_api_client)
+
     def test_fields_to_display(self, patch_base_class, mock_definition_type, mock_api_client):
         definitions = Definitions(mock_definition_type, mock_api_client)
         expected_field_to_display = ["name", "docker_repository", "docker_image_tag", "my_definition_type_definition_id"]
@@ -45,12 +56,11 @@ class TestDefinitions:
         assert definitions.response_definition_list_field == expected_response_definition_list_field
 
     def test_latest_definitions(self, patch_base_class, mock_definition_type, mock_api_client, mocker):
-        mock_list_latest_definitions = mocker.Mock()
-        mocker.patch.object(Definitions, "list_latest_definitions", mock_list_latest_definitions)
+        mocker.patch.object(Definitions, "list_latest_definitions", mocker.Mock())
         definitions = Definitions(mock_definition_type, mock_api_client)
         latest_definitions = definitions.latest_definitions
-        mock_list_latest_definitions.assert_called_with(definitions.api_instance)
-        assert latest_definitions == mock_list_latest_definitions.return_value
+        definitions.list_latest_definitions.assert_called_with(definitions.api_instance)
+        assert latest_definitions == definitions.list_latest_definitions.return_value
 
     def test_parse_response(self, patch_base_class, mock_definition_type, mock_api_client):
         definitions = Definitions(mock_definition_type, mock_api_client)
@@ -62,5 +72,85 @@ class TestDefinitions:
         parsed_definitions = definitions._parse_response(api_response)
         assert len(parsed_definitions) == 5
         for i in range(5):
-            assert parsed_definitions[i] == {field: f"{field}_value_{i}" for field in definitions.fields_to_display}
+            assert parsed_definitions[i] == [f"{field}_value_{i}" for field in definitions.fields_to_display]
             assert "discarded_value" not in parsed_definitions[i]
+
+    def test_repr(self, patch_base_class, mocker, mock_definition_type, mock_api_client):
+        headers = ["field_a", "field_b", "field_c"]
+        latest_definitions = [["a", "b", "c"]]
+        mocker.patch.object(Definitions, "fields_to_display", headers)
+        mocker.patch.object(Definitions, "latest_definitions", latest_definitions)
+        mocker.patch.object(Definitions, "_display_as_table", mocker.Mock())
+        definitions = Definitions(mock_definition_type, mock_api_client)
+        representation = definitions.__repr__()
+        definitions._display_as_table.assert_called_with([[f.upper() for f in headers]] + latest_definitions)
+        assert representation == definitions._display_as_table.return_value
+
+    @pytest.mark.parametrize(
+        "test_data,padding,expected_col_width",
+        [([["a", "___10chars"], ["e", "f"]], 2, 2 + 10), ([["a", "___10chars"], ["e", "____11chars"]], 2, 2 + 11), ([[""]], 2, 2)],
+    )
+    def test_compute_col_width(self, test_data, padding, expected_col_width):
+        col_width = Definitions._compute_col_width(test_data, padding)
+        assert col_width == expected_col_width
+
+    @pytest.mark.parametrize(
+        "test_data,col_width,expected_output",
+        [
+            ([["a", "___10chars"], ["e", "____11chars"]], 13, "a            ___10chars   \ne            ____11chars  "),
+        ],
+    )
+    def test_display_as_table(self, mocker, test_data, col_width, expected_output, mock_definition_type, mock_api_client):
+        mocker.patch.object(Definitions, "_compute_col_width", mocker.Mock(return_value=col_width))
+        assert Definitions._display_as_table(test_data) == expected_output
+
+
+class TestSourceDefinitions:
+    @pytest.fixture
+    def mock_api_client(self, mocker):
+        return mocker.Mock()
+
+    def test_init(self, mocker, mock_api_client):
+        mocker.patch.object(Definitions, "__init__", mocker.Mock())
+        SourceDefinitions(mock_api_client)
+        Definitions.__init__.assert_called_with(DefinitionType.SOURCE, mock_api_client)
+
+    def test_class_attributes(self):
+        assert SourceDefinitions.api == source_definition_api.SourceDefinitionApi
+
+    def test_list_latest_definitions(self, mocker, mock_api_client):
+        mocker.patch.object(SourceDefinitions, "api", mocker.Mock())
+        mocker.patch.object(SourceDefinitions, "_parse_response", mocker.Mock())
+
+        source_definitions = SourceDefinitions(mock_api_client)
+        source_definitions.api_instance = mocker.Mock()
+
+        latest_definitions = source_definitions.list_latest_definitions()
+        assert latest_definitions == source_definitions._parse_response.return_value
+        source_definitions.api.list_latest_source_definitions.assert_called_with(source_definitions.api_instance)
+
+
+class TestDestinationDefinitions:
+    @pytest.fixture
+    def mock_api_client(self, mocker):
+        return mocker.Mock()
+
+    def test_init(self, mocker, mock_api_client):
+        definitions_init = mocker.Mock()
+        mocker.patch.object(Definitions, "__init__", definitions_init)
+        DestinationDefinitions(mock_api_client)
+        definitions_init.assert_called_with(DefinitionType.DESTINATION, mock_api_client)
+
+    def test_class_attributes(self):
+        assert DestinationDefinitions.api == destination_definition_api.DestinationDefinitionApi
+
+    def test_list_latest_definitions(self, mocker, mock_api_client):
+        mocker.patch.object(DestinationDefinitions, "api", mocker.Mock())
+        mocker.patch.object(DestinationDefinitions, "_parse_response", mocker.Mock())
+
+        destination_definitions = DestinationDefinitions(mock_api_client)
+        destination_definitions.api_instance = mocker.Mock()
+
+        latest_definitions = destination_definitions.list_latest_definitions()
+        assert latest_definitions == destination_definitions._parse_response.return_value
+        destination_definitions.api.list_latest_destination_definitions.assert_called_with(destination_definitions.api_instance)
