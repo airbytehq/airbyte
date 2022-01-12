8ccafccec3b9b30534ddae294634145c62f79095
Wadii <wadii@lazylantern.com>
2022-01-12 16:59:30 +0100

    Read Users data from persistiq

--- airbyte-integrations/connectors/source-persistiq/source_persistiq/source.py
+++ airbyte-integrations/connectors/source-persistiq/source_persistiq/source.py
@@ -10,7 +10,7 @@ import requests
 from airbyte_cdk.sources import AbstractSource
 from airbyte_cdk.sources.streams import Stream
 from airbyte_cdk.sources.streams.http import HttpStream
-from airbyte_cdk.sources.streams.http.auth import TokenAuthenticator
+from airbyte_cdk.sources.streams.http.auth import NoAuth
 
 """
 TODO: Most comments in this class are instructive and should be deleted after the source is implemented.
@@ -29,67 +29,23 @@ There are additional required TODOs in the files within the integration_tests fo
 
 # Basic full refresh stream
 class PersistiqStream(HttpStream, ABC):
-    """
-    TODO remove this comment
-
-    This class represents a stream output by the connector.
-    This is an abstract base class meant to contain all the common functionality at the API level e.g: the API base URL, pagination strategy,
-    parsing responses etc..
-
-    Each stream should extend this class (or another abstract subclass of it) to specify behavior unique to that stream.
-
-    Typically for REST APIs each stream corresponds to a resource in the API. For example if the API
-    contains the endpoints
-        - GET v1/customers
-        - GET v1/employees
-
-    then you should have three classes:
-    `class PersistiqStream(HttpStream, ABC)` which is the current class
-    `class Customers(PersistiqStream)` contains behavior to pull data for customers using v1/customers
-    `class Employees(PersistiqStream)` contains behavior to pull data for employees using v1/employees
-
-    If some streams implement incremental sync, it is typical to create another class
-    `class IncrementalPersistiqStream((PersistiqStream), ABC)` then have concrete stream implementations extend it. An example
-    is provided below.
-
-    See the reference docs for the full list of configurable options.
-    """
+    def __init__(
+        self, api_key: str, **kwargs
+    ):
+        super().__init__(**kwargs)
+        self.api_key = api_key
 
     url_base = "https://api.persistiq.com/v1/"
 
     def next_page_token(self, response: requests.Response) -> Optional[Mapping[str, Any]]:
-        """
-        TODO: Override this method to define a pagination strategy. If you will not be using pagination, no action is required - just return None.
-
-        This method should return a Mapping (e.g: dict) containing whatever information required to make paginated requests. This dict is passed
-        to most other methods in this class to help you form headers, request bodies, query params, etc..
-
-        For example, if the API accepts a 'page' parameter to determine which page of the result to return, and a response from the API contains a
-        'page' number, then this method should probably return a dict {'page': response.json()['page'] + 1} to increment the page count by 1.
-        The request_params method should then read the input next_page_token and set the 'page' param to next_page_token['page'].
-
-        :param response: the most recent response from the API
-        :return If there is another page in the result, a mapping (e.g: dict) containing information needed to query the next page in the response.
-                If there are no more pages in the result, return None.
-        """
         return None
 
     def request_params(
         self, stream_state: Mapping[str, Any], stream_slice: Mapping[str, any] = None, next_page_token: Mapping[str, Any] = None
     ) -> MutableMapping[str, Any]:
-        """
-        TODO: Override this method to define any query parameters to be set. Remove this method if you don't need to define request params.
-        Usually contains common params e.g. pagination size etc.
-        """
         return {}
 
     def parse_response(self, response: requests.Response, **kwargs) -> Iterable[Mapping]:
-        """
-        TODO: Override this method to define how a response is parsed.
-        :return an iterable containing each record in the response
-        """
-        print('parsing')
-        print(response)
         yield {}
 
 
@@ -100,6 +56,15 @@ class Users(PersistiqStream):
         self, stream_state: Mapping[str, Any] = None, stream_slice: Mapping[str, Any] = None, next_page_token: Mapping[str, Any] = None
     ) -> str:
         return "users"
+
+    def request_headers(
+            self,
+            **kwargs
+    ) -> MutableMapping[str, Any]:
+        return {'x-api-key': self.api_key}
+
+    def parse_response(self, response: requests.Response, **kwargs) -> Iterable[Mapping]:
+        yield response.json()
 
 
 # Basic incremental stream
@@ -131,46 +96,46 @@ class IncrementalPersistiqStream(PersistiqStream, ABC):
         return {}
 
 
-class Employees(IncrementalPersistiqStream):
-    """
-    TODO: Change class name to match the table/data source this stream corresponds to.
-    """
+# class Employees(IncrementalPersistiqStream):
+#     """
+#     TODO: Change class name to match the table/data source this stream corresponds to.
+#     """
 
-    # TODO: Fill in the cursor_field. Required.
-    cursor_field = "start_date"
+#     # TODO: Fill in the cursor_field. Required.
+#     cursor_field = "start_date"
 
-    # TODO: Fill in the primary key. Required. This is usually a unique field in the stream, like an ID or a timestamp.
-    primary_key = "employee_id"
+#     # TODO: Fill in the primary key. Required. This is usually a unique field in the stream, like an ID or a timestamp.
+#     primary_key = "employee_id"
 
-    def path(self, **kwargs) -> str:
-        """
-        TODO: Override this method to define the path this stream corresponds to. E.g. if the url is https://example-api.com/v1/employees then this should
-        return "single". Required.
-        """
-        return "employees"
+#     def path(self, **kwargs) -> str:
+#         """
+#         TODO: Override this method to define the path this stream corresponds to. E.g. if the url is https://example-api.com/v1/employees then this should
+#         return "single". Required.
+#         """
+#         return "employees"
 
-    def stream_slices(self, stream_state: Mapping[str, Any] = None, **kwargs) -> Iterable[Optional[Mapping[str, any]]]:
-        """
-        TODO: Optionally override this method to define this stream's slices. If slicing is not needed, delete this method.
+#     def stream_slices(self, stream_state: Mapping[str, Any] = None, **kwargs) -> Iterable[Optional[Mapping[str, any]]]:
+#         """
+#         TODO: Optionally override this method to define this stream's slices. If slicing is not needed, delete this method.
 
-        Slices control when state is saved. Specifically, state is saved after a slice has been fully read.
-        This is useful if the API offers reads by groups or filters, and can be paired with the state object to make reads efficient. See the "concepts"
-        section of the docs for more information.
+#         Slices control when state is saved. Specifically, state is saved after a slice has been fully read.
+#         This is useful if the API offers reads by groups or filters, and can be paired with the state object to make reads efficient. See the "concepts"
+#         section of the docs for more information.
 
-        The function is called before reading any records in a stream. It returns an Iterable of dicts, each containing the
-        necessary data to craft a request for a slice. The stream state is usually referenced to determine what slices need to be created.
-        This means that data in a slice is usually closely related to a stream's cursor_field and stream_state.
+#         The function is called before reading any records in a stream. It returns an Iterable of dicts, each containing the
+#         necessary data to craft a request for a slice. The stream state is usually referenced to determine what slices need to be created.
+#         This means that data in a slice is usually closely related to a stream's cursor_field and stream_state.
 
-        An HTTP request is made for each returned slice. The same slice can be accessed in the path, request_params and request_header functions to help
-        craft that specific request.
+#         An HTTP request is made for each returned slice. The same slice can be accessed in the path, request_params and request_header functions to help
+#         craft that specific request.
 
-        For example, if https://example-api.com/v1/employees offers a date query params that returns data for that particular day, one way to implement
-        this would be to consult the stream state object for the last synced date, then return a slice containing each date from the last synced date
-        till now. The request_params function would then grab the date from the stream_slice and make it part of the request by injecting it into
-        the date query param.
-        """
-        raise NotImplementedError(
-            "Implement stream slices or delete this method!")
+#         For example, if https://example-api.com/v1/employees offers a date query params that returns data for that particular day, one way to implement
+#         this would be to consult the stream state object for the last synced date, then return a slice containing each date from the last synced date
+#         till now. The request_params function would then grab the date from the stream_slice and make it part of the request by injecting it into
+#         the date query param.
+#         """
+#         raise NotImplementedError(
+#             "Implement stream slices or delete this method!")
 
 
 # Source
@@ -193,6 +158,7 @@ class SourcePersistiq(AbstractSource):
 
         :param config: A Mapping of the user input configuration as defined in the connector spec.
         """
-        # TODO remove the authenticator if not required.
-        auth = TokenAuthenticator(token="api_key")  # Oauth2Authenticator is also available if you need oauth support
-        return [Customers(authenticator=auth), Employees(authenticator=auth)]
+
+        # auth = {"x-api-key": config['api_key']}
+        auth = NoAuth()
+        return [Users(authenticator=auth, api_key=config['api_key'])]
