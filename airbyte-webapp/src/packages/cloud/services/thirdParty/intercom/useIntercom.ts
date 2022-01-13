18a3fb18b69b8be2216f4b34100bc48eb44876d9
Artem Astapenko <jamakase54@gmail.com>
2022-01-13 23:14:18 +0300

    Add workspaceId to intercom custom attributes

--- airbyte-webapp/src/packages/cloud/services/thirdParty/intercom/useIntercom.ts
+++ airbyte-webapp/src/packages/cloud/services/thirdParty/intercom/useIntercom.ts
@@ -1,20 +1,42 @@
 import { useEffect } from "react";
-import { useIntercom as useIntercomProvider } from "react-use-intercom";
+import {
+  useIntercom as useIntercomProvider,
+  IntercomContextValues,
+} from "react-use-intercom";
 
 import { useCurrentUser } from "packages/cloud/services/auth/AuthService";
+import { useCurrentWorkspace } from "hooks/services/useWorkspace";
 
-export const useIntercom = (): void => {
+export const useIntercom = (): IntercomContextValues => {
+  const intercomContextValues = useIntercomProvider();
+
+  // It may be better to get these values from analytics context
+  // in case it will be possible to init intercom for non authorized users
   const user = useCurrentUser();
-  const { boot, shutdown } = useIntercomProvider();
+  const workspace = useCurrentWorkspace();
 
   useEffect(() => {
-    boot({
+    intercomContextValues.boot({
       email: user.email,
       name: user.name,
       userId: user.userId,
       userHash: user.intercomHash,
+
+      customAttributes: {
+        workspaceId: workspace?.workspaceId,
+      },
     });
 
-    return () => shutdown();
+    return () => intercomContextValues.shutdown();
   }, [user]);
+
+  useEffect(() => {
+    intercomContextValues.update({
+      customAttributes: {
+        workspaceId: workspace?.workspaceId,
+      },
+    });
+  }, [workspace.workspaceId]);
+
+  return intercomContextValues;
 };
