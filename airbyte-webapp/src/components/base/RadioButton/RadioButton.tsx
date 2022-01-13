c5e4103f183623e62fc8bcce8d335d9f2fe00c9d
Artem Astapenko <jamakase54@gmail.com>
2022-01-14 00:56:37 +0300

    Edit radiobutton styles

--- airbyte-webapp/src/components/base/RadioButton/RadioButton.tsx
+++ airbyte-webapp/src/components/base/RadioButton/RadioButton.tsx
@@ -8,18 +8,23 @@ const RadioButtonInput = styled.input`
   margin: 0;
 `;
 
-const Check = styled.div`
+const Check = styled.div<{ checked?: boolean }>`
   height: 100%;
   width: 100%;
   border-radius: 50%;
-  background: ${({ theme }) => theme.primaryColor};
+  background: ${({ theme, checked }) =>
+    checked ? theme.whiteColor : theme.greyColor20};
 `;
 
-const RadioButtonContainer = styled.label`
-  height: 16px;
-  width: 16px;
-  background: ${({ theme }) => theme.greyColor20};
-  color: ${({ theme }) => theme.primaryColor};
+const RadioButtonContainer = styled.label<{ checked?: boolean }>`
+  height: 18px;
+  width: 18px;
+  background: ${({ theme, checked }) =>
+    checked ? theme.primaryColor : theme.whiteColor};
+  border: 1px solid
+    ${({ theme, checked }) =>
+      checked ? theme.primaryColor : theme.greyColor20};
+  color: ${({ theme }) => theme.whiteColor};
   text-align: center;
   border-radius: 50%;
   display: inline-block;
@@ -34,8 +39,9 @@ const RadioButton: React.FC<React.InputHTMLAttributes<HTMLInputElement>> = (
     <RadioButtonContainer
       className={props.className}
       onClick={(event: React.SyntheticEvent) => event.stopPropagation()}
+      checked={props.checked}
     >
-      {props.checked && <Check />}
+      <Check checked={props.checked} />
       <RadioButtonInput {...props} type="radio" />
     </RadioButtonContainer>
   );
