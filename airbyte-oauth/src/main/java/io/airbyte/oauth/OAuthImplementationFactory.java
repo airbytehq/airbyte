6b502d8c326bcde791128f5711a21cc03bde19f3
midavadim <midavadim@yahoo.com>
2022-01-18 09:11:27 +0200

    🎉 square: added oauth support (#6842)
    * fixed test which check incorrect cred config
    
    * Added oauth2 authentication
    
    * Added oauth creds
    
    * fixed formatting
    
    * added oauth2 spec section, added missing type hints
    
    * Added java part of Square OAuth
    
    * fixed checkstyle
    
    * removed commented code
    
    * added support for old format of spec.json files, updated change logs docs
    
    * renamed spec property 'authentication' to default 'credentials'. fixed changes in java part
    
    * recovered empty files
    
    * updated OAuthImplementationFactory.java
    
    * fixed issue with autheticator for sub streams, added config catalog with all streams, updated docs
    
    * use advanced_auth
    
    * added advanced_auth
    
    * moved scopes to private property
    
    * updated source version
    
    * Revert "updated source version"
    
    This reverts commit ce3d06165c4bbbe1592e22203d6b6c545deec9a9.
    
    * updated source version
    
    * added new version for airbyte index
    
    Co-authored-by: ievgeniit <etsybaev@gmail.com>

--- airbyte-oauth/src/main/java/io/airbyte/oauth/OAuthImplementationFactory.java
+++ airbyte-oauth/src/main/java/io/airbyte/oauth/OAuthImplementationFactory.java
@@ -47,6 +47,7 @@ public class OAuthImplementationFactory {
         .put("airbyte/source-salesforce", new SalesforceOAuthFlow(configRepository, httpClient))
         .put("airbyte/source-slack", new SlackOAuthFlow(configRepository, httpClient))
         .put("airbyte/source-snapchat-marketing", new SnapchatMarketingOAuthFlow(configRepository, httpClient))
+        .put("airbyte/source-square", new SquareOAuthFlow(configRepository, httpClient))
         .put("airbyte/source-strava", new StravaOAuthFlow(configRepository, httpClient))
         .put("airbyte/source-surveymonkey", new SurveymonkeyOAuthFlow(configRepository, httpClient))
         .put("airbyte/source-trello", new TrelloOAuthFlow(configRepository, httpClient))
