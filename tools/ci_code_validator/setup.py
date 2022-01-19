1832c69c269e1c9f15c3eb36f7a1bd59a861ab73
h-chlor <43164320+h-chlor@users.noreply.github.com>
2022-01-19 18:25:43 +0200

    Add  SonarQube to CI (#8362)

--- tools/ci_code_validator/setup.py
+++ tools/ci_code_validator/setup.py
@@ -5,13 +5,25 @@
 
 from setuptools import find_packages, setup
 
-MAIN_REQUIREMENTS = ["requests", "ci_common_utils"]
+MAIN_REQUIREMENTS = [
+    "requests",
+    "ci_common_utils",
+    "unidiff",
+    "mdutils~=1.3.1"
+]
 
-TEST_REQUIREMENTS = ["requests-mock"]
+TEST_REQUIREMENTS = [
+    "requests-mock",
+    "pytest",
+    "black",
+    "mypy",
+    "lxml",
+    "isort"
+]
 
 setup(
     version="0.0.0",
-    name="ci_credentials",
+    name="ci_code_validator",
     description="Load and extract CI secrets for test suites",
     author="Airbyte",
     author_email="contact@airbyte.io",
@@ -20,10 +32,12 @@ setup(
     python_requires='>=3.7',
     extras_require={
         "tests": TEST_REQUIREMENTS,
+
     },
     entry_points={
         'console_scripts': [
-            'ci_credentials = ci_credentials.main:main',
+            'ci_sonar_qube = ci_sonar_qube.main:main',
+            'ci_changes_detection = ci_changes_detection.main:main',
         ],
     },
 )
