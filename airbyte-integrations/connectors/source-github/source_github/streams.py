a5090e98fa59f052c156f0d8aae1b5aa30bc71a1
ykurochkin <zhenia.kurochkin@gmail.com>
2022-01-21 00:56:05 +0200

    pass

--- airbyte-integrations/connectors/source-github/source_github/streams.py
+++ airbyte-integrations/connectors/source-github/source_github/streams.py
@@ -802,3 +802,12 @@ class PullRequestCommentReactions(ReactionStream):
     """
 
     parent_entity = ReviewComments
+
+
+def create_test_task(a=12):
+    if a == 12:
+        return 13
+    elif a == 22:
+        return 23
+    else:
+        return 44
