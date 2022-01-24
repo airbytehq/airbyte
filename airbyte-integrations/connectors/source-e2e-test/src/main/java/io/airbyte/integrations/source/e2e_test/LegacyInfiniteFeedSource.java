c5de7f4913aba5cf4a859a1ddeb08c309aee6c78
LiRen Tu <tuliren@gmail.com>
2022-01-23 21:09:03 -0800

    🎉 Source e2e test: support custom catalog (#9720)
    * Add continuous feed mode to source e2e-test
    
    * Update connector catalog doc
    
    * Fix sonar qube issues
    
    * Add cloud variant
    
    * Format code
    
    * Add testing source connector to seed

--- airbyte-integrations/connectors/source-e2e-test/src/main/java/io/airbyte/integrations/source/e2e_test/LegacyInfiniteFeedSource.java
+++ airbyte-integrations/connectors/source-e2e-test/src/main/java/io/airbyte/integrations/source/e2e_test/LegacyInfiniteFeedSource.java
@@ -20,24 +20,17 @@ import io.airbyte.protocol.models.AirbyteConnectionStatus.Status;
 import io.airbyte.protocol.models.AirbyteMessage;
 import io.airbyte.protocol.models.AirbyteMessage.Type;
 import io.airbyte.protocol.models.AirbyteRecordMessage;
-import io.airbyte.protocol.models.CatalogHelpers;
 import io.airbyte.protocol.models.ConfiguredAirbyteCatalog;
-import io.airbyte.protocol.models.Field;
-import io.airbyte.protocol.models.JsonSchemaPrimitive;
 import java.time.Instant;
 import java.util.Optional;
 import java.util.concurrent.atomic.AtomicLong;
-import java.util.function.Predicate;
+import java.util.function.LongPredicate;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class InfiniteFeedSource extends BaseConnector implements Source {
+public class LegacyInfiniteFeedSource extends BaseConnector implements Source {
 
-  private static final Logger LOGGER = LoggerFactory.getLogger(InfiniteFeedSource.class);
-
-  public static final AirbyteCatalog CATALOG = CatalogHelpers.createAirbyteCatalog(
-      "data",
-      Field.of("column1", JsonSchemaPrimitive.STRING));
+  private static final Logger LOGGER = LoggerFactory.getLogger(LegacyInfiniteFeedSource.class);
 
   @Override
   public AirbyteConnectionStatus check(final JsonNode config) {
@@ -46,13 +39,14 @@ public class InfiniteFeedSource extends BaseConnector implements Source {
 
   @Override
   public AirbyteCatalog discover(final JsonNode config) {
-    return Jsons.clone(CATALOG);
+    return Jsons.clone(LegacyConstants.DEFAULT_CATALOG);
   }
 
   @Override
   public AutoCloseableIterator<AirbyteMessage> read(final JsonNode config, final ConfiguredAirbyteCatalog catalog, final JsonNode state) {
-    final Predicate<Long> anotherRecordPredicate =
-        config.has("max_records") ? recordNumber -> recordNumber < config.get("max_records").asLong() : recordNumber -> true;
+    final LongPredicate anotherRecordPredicate = config.has("max_records")
+        ? recordNumber -> recordNumber < config.get("max_records").asLong()
+        : recordNumber -> true;
 
     final Optional<Long> sleepTime = Optional.ofNullable(config.get("message_interval")).map(JsonNode::asLong);
 
@@ -62,28 +56,27 @@ public class InfiniteFeedSource extends BaseConnector implements Source {
 
       @Override
       protected AirbyteMessage computeNext() {
-        if (anotherRecordPredicate.test(i.get())) {
-          if (i.get() != 0) {
-            if (sleepTime.isPresent()) {
-              try {
-                LOGGER.info("sleeping for {} ms", sleepTime.get());
-                sleep(sleepTime.get());
-              } catch (final InterruptedException e) {
-                throw new RuntimeException(e);
-              }
-            }
-          }
-          i.incrementAndGet();
-          LOGGER.info("source emitting record {}:", i.get());
-          return new AirbyteMessage()
-              .withType(Type.RECORD)
-              .withRecord(new AirbyteRecordMessage()
-                  .withStream("data")
-                  .withEmittedAt(Instant.now().toEpochMilli())
-                  .withData(Jsons.jsonNode(ImmutableMap.of("column1", i))));
-        } else {
+        if (!anotherRecordPredicate.test(i.get())) {
           return endOfData();
         }
+
+        if (sleepTime.isPresent() && i.get() != 0) {
+          try {
+            LOGGER.info("sleeping for {} ms", sleepTime.get());
+            sleep(sleepTime.get());
+          } catch (final InterruptedException e) {
+            throw new RuntimeException(e);
+          }
+        }
+
+        i.incrementAndGet();
+        LOGGER.info("source emitting record {}:", i.get());
+        return new AirbyteMessage()
+            .withType(Type.RECORD)
+            .withRecord(new AirbyteRecordMessage()
+                .withStream("data")
+                .withEmittedAt(Instant.now().toEpochMilli())
+                .withData(Jsons.jsonNode(ImmutableMap.of("column1", i))));
       }
 
     });
