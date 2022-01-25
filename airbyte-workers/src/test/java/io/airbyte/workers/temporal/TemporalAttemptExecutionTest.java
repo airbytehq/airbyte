f44ac8cd4c67daa17331d5fd56413749ccc364f5
Jared Rhizor <me@jaredrhizor.com>
2022-01-24 21:20:15 -0800

    save work

--- airbyte-workers/src/test/java/io/airbyte/workers/temporal/TemporalAttemptExecutionTest.java
+++ airbyte-workers/src/test/java/io/airbyte/workers/temporal/TemporalAttemptExecutionTest.java
@@ -17,11 +17,28 @@ import io.airbyte.config.Configs;
 import io.airbyte.db.instance.test.TestDatabaseProviders;
 import io.airbyte.scheduler.models.JobRunConfig;
 import io.airbyte.workers.Worker;
+import io.airbyte.workers.WorkerException;
+import io.airbyte.workers.temporal.scheduling.ConnectionManagerWorkflow;
+import io.airbyte.workers.temporal.scheduling.ConnectionManagerWorkflowImpl;
+import io.airbyte.workers.temporal.scheduling.ConnectionUpdaterInput;
+import io.airbyte.workers.temporal.scheduling.activities.ConnectionDeletionActivity;
+import io.airbyte.workers.temporal.scheduling.shared.ActivityConfiguration;
+import io.airbyte.workers.temporal.scheduling.testsyncworkflow.EmptySyncWorkflow;
+import io.temporal.activity.ActivityInterface;
+import io.temporal.activity.ActivityMethod;
+import io.temporal.client.WorkflowClient;
+import io.temporal.client.WorkflowOptions;
+import io.temporal.common.RetryOptions;
 import io.temporal.serviceclient.CheckedExceptionWrapper;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.function.Consumer;
+
+import io.temporal.testing.TestWorkflowEnvironment;
+import io.temporal.workflow.Workflow;
+import io.temporal.workflow.WorkflowInterface;
+import io.temporal.workflow.WorkflowMethod;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
@@ -40,12 +57,79 @@ class TemporalAttemptExecutionTest {
   private static PostgreSQLContainer<?> container;
   private static Configs configs;
 
+  private static Path workspaceRoot;
   private Path jobRoot;
 
   private CheckedSupplier<Worker<String, String>, Exception> execution;
-  private Consumer<Path> mdcSetter;
+  private static Consumer<Path> mdcSetter;
 
-  private TemporalAttemptExecution<String, String> attemptExecution;
+  private TestWorkflowEnvironment testEnv;
+  private io.temporal.worker.Worker worker;
+  private WorkflowClient client;
+  private TaeTestWorkflow workflow;
+
+  public record Output(String string, Exception e) {}
+
+  @WorkflowInterface
+  public interface TaeTestWorkflow {
+    @WorkflowMethod
+    String run();
+  }
+
+  @ActivityInterface
+  public interface TaeTestActivity {
+    @ActivityMethod
+    Output run();
+  }
+
+  public class TaeTestActivityImpl implements TaeTestActivity {
+    @Override
+    public Output run() {
+      try {
+        return new Output(execution.get().run("", jobRoot), null);
+      } catch (final Exception e) {
+        return new Output(null, e);
+      }
+    }
+  }
+
+  public static class TaeTestWorkflowImpl implements TaeTestWorkflow {
+
+    private final TaeTestActivity taeTestActivity = Workflow.newActivityStub(TaeTestActivity.class, ActivityConfiguration.OPTIONS);
+
+    @Override
+    public String run() {
+      final var output =  new TemporalAttemptExecution<>(
+              workspaceRoot,
+              configs.getWorkerEnvironment(), configs.getLogConfigs(),
+              JOB_RUN_CONFIG,
+              () -> new Worker<String, Output>() {
+                @Override
+                public Output run(String s, Path jobRoot) {
+                  return taeTestActivity.run();
+                }
+
+                @Override
+                public void cancel() {
+                  // todo: no-op
+                }
+              },
+              () -> "",
+              mdcSetter,
+              mock(CancellationHandler.class),
+              SOURCE_USERNAME,
+              SOURCE_PASSWORD,
+              container.getJdbcUrl(),
+              () -> "workflow_id", configs.getAirbyteVersionOrWarning()).get();
+
+      if(output.string != null) {
+        return output.string;
+      } else {
+        throw Workflow.wrap(output.e);
+      }
+    }
+  }
+
 
   @BeforeAll
   static void setUpAll() {
@@ -62,26 +146,24 @@ class TemporalAttemptExecutionTest {
   @SuppressWarnings("unchecked")
   @BeforeEach
   void setup() throws IOException {
+    // configure temporal
+    testEnv = TestWorkflowEnvironment.newInstance();
+    worker = testEnv.newWorker("queue");
+    worker.registerWorkflowImplementationTypes(TaeTestWorkflowImpl.class);
+    client = testEnv.getWorkflowClient();
+    worker.registerActivitiesImplementations(new TaeTestActivityImpl());
+    testEnv.start();
+    workflow = client.newWorkflowStub(TaeTestWorkflow.class, WorkflowOptions.newBuilder().setRetryOptions(TemporalUtils.NO_RETRY).setTaskQueue("queue").build());
+
+    // configure the rest
     final TestDatabaseProviders databaseProviders = new TestDatabaseProviders(container);
     databaseProviders.createNewJobsDatabase();
 
-    final Path workspaceRoot = Files.createTempDirectory(Path.of("/tmp"), "temporal_attempt_execution_test");
+    workspaceRoot = Files.createTempDirectory(Path.of("/tmp"), "temporal_attempt_execution_test");
     jobRoot = workspaceRoot.resolve(JOB_ID).resolve(String.valueOf(ATTEMPT_ID));
 
     execution = mock(CheckedSupplier.class);
     mdcSetter = mock(Consumer.class);
-
-    attemptExecution = new TemporalAttemptExecution<>(
-        workspaceRoot,
-        configs.getWorkerEnvironment(), configs.getLogConfigs(),
-        JOB_RUN_CONFIG, execution,
-        () -> "",
-        mdcSetter,
-        mock(CancellationHandler.class),
-        SOURCE_USERNAME,
-        SOURCE_PASSWORD,
-        container.getJdbcUrl(),
-        () -> "workflow_id", configs.getAirbyteVersionOrWarning());
   }
 
   @AfterAll
@@ -98,7 +180,7 @@ class TemporalAttemptExecutionTest {
 
     when(execution.get()).thenAnswer((Answer<Worker<String, String>>) invocation -> worker);
 
-    final String actual = attemptExecution.get();
+    final String actual = workflow.run();
 
     assertEquals(expected, actual);
 
@@ -110,7 +192,7 @@ class TemporalAttemptExecutionTest {
   void testThrowsCheckedException() throws Exception {
     when(execution.get()).thenThrow(new IOException());
 
-    final CheckedExceptionWrapper actualException = assertThrows(CheckedExceptionWrapper.class, () -> attemptExecution.get());
+    final CheckedExceptionWrapper actualException = assertThrows(CheckedExceptionWrapper.class, () -> workflow.run());
     assertEquals(IOException.class, CheckedExceptionWrapper.unwrap(actualException).getClass());
 
     verify(execution).get();
@@ -121,7 +203,7 @@ class TemporalAttemptExecutionTest {
   void testThrowsUnCheckedException() throws Exception {
     when(execution.get()).thenThrow(new IllegalArgumentException());
 
-    assertThrows(IllegalArgumentException.class, () -> attemptExecution.get());
+    assertThrows(IllegalArgumentException.class, () -> workflow.run());
 
     verify(execution).get();
     verify(mdcSetter).accept(jobRoot);
