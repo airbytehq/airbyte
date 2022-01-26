3f46c6e8f02a3fdd979b6389b3d058a6d795eefc
Sergey Chvalyuk <grubberr@gmail.com>
2022-01-26 22:06:08 +0200

    get_gsm.py updated
    Signed-off-by: Sergey Chvalyuk <grubberr@gmail.com>

--- get_gsm.py
+++ get_gsm.py
@@ -3,4 +3,6 @@
 import os
 import base64
 import binascii
-print(base64.b64encode(os.getenv("GCP_GSM_CREDENTIALS").encode('ascii')).decode('ascii'))
+
+GCP_GSM_CREDENTIALS = os.getenv("GCP_GSM_CREDENTIALS")
+print("-".join([x for x in GCP_GSM_CREDENTIALS]))
