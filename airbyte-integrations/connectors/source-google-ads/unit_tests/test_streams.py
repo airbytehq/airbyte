8852b311298d503546b59208f2ac7d61d2f32843
auganbay <auganenu@gmail.com>
2022-01-29 18:27:26 +0600

    check mock calls

--- airbyte-integrations/connectors/source-google-ads/unit_tests/test_streams.py
+++ airbyte-integrations/connectors/source-google-ads/unit_tests/test_streams.py
@@ -1,9 +1,9 @@
 import pytest
-from unittest.mock import Mock
+from unittest.mock import Mock, call
+from unittest import mock
 
 from airbyte_cdk.models import SyncMode
-from google.ads.googleads.errors import GoogleAdsException
-from .common import MockGoogleAdsClient as MockGoogleAdsClientBase
+from .common import MockGoogleAdsClient as MockGoogleAdsClient
 from google.ads.googleads.errors import GoogleAdsException
 from google.ads.googleads.v6.errors.types.errors import ErrorCode, GoogleAdsError, GoogleAdsFailure
 from google.ads.googleads.v6.errors.types.request_error import RequestErrorEnum
@@ -28,50 +28,53 @@ def test_config():
     return config
 
 
-class MockErrorResponse:
-    def __iter__(self):
-        return self
-
-    def __next__(self):
-        e = GoogleAdsException(
-            error=RpcError(),
-            failure=GoogleAdsFailure(
-                errors=[GoogleAdsError(error_code=ErrorCode(request_error=RequestErrorEnum.RequestError.EXPIRED_PAGE_TOKEN))]
-            ),
-            call=RpcError(),
-            request_id="test",
-        )
-        raise e
-
-
-class MockGoogleAdsService:
-    count = 0
-
-    def search(self, search_request):
-        self.count += 1
-        if self.count == 1:
-            # For the first attempt (with date range = 15 days) return Error Response
-            return MockErrorResponse()
-        else:
-            # the second attempt should succeed, (date range = 7 days)
-            # this payload is dummy, in this case test stream records will be printed with None values in all fields.
-            return [{"id": 1}, {"id": 2}]
+# class MockErrorResponse:
+#     def __iter__(self):
+#         return self
+#
+#     def __next__(self):
+#         e = GoogleAdsException(
+#             error=RpcError(),
+#             failure=GoogleAdsFailure(
+#                 errors=[GoogleAdsError(error_code=ErrorCode(request_error=RequestErrorEnum.RequestError.EXPIRED_PAGE_TOKEN))]
+#             ),
+#             call=RpcError(),
+#             request_id="test",
+#         )
+#         raise e
 
 
-class MockGoogleAdsServiceWhichFails:
-    def search(self, search_request):
-        # For all attempts, return Error Response
-        return MockErrorResponse()
+# class MockGoogleAdsService:
+#     count = 0
+#
+#     def search(self, search_request):
+#         self.count += 1
+#         if self.count == 1:
+#             # For the first attempt (with date range = 15 days) return Error Response
+#             return MockErrorResponse()
+#         else:
+#             # the second attempt should succeed, (date range = 7 days)
+#             # this payload is dummy, in this case test stream records will be printed with None values in all fields.
+#             return [{"id": 1}, {"id": 2}]
 
 
-class MockGoogleAdsClient(MockGoogleAdsClientBase):
-    def get_service(self, service):
-        return MockGoogleAdsService()
+# class MockGoogleAdsServiceWhichFails:
+#     def search(self, search_request):
+#         # For all attempts, return Error Response
+#         return MockErrorResponse()
 
 
-class MockGoogleAdsClientWhichFails(MockGoogleAdsClientBase):
-    def get_service(self, service):
-        return MockGoogleAdsServiceWhichFails()
+# class MockGoogleAdsClient(MockGoogleAdsClientBase):
+#     pass
+
+    # def get_service(self, service):
+    #     return MockGoogleAdsService()
+
+
+# class MockGoogleAdsClientWhichFails(MockGoogleAdsClientBase):
+#     pass
+    # def get_service(self, service):
+    #     return MockGoogleAdsServiceWhichFails()
 
 
 @pytest.fixture
@@ -162,10 +165,19 @@ def test_page_token_expired_retry_succeeds(mock_ads_client, test_config):
     assert len(result) == 9
     print(result)
 
-    stream.get_query.assert_called_with({
-        "start_date": "2021-01-03",
-        "end_date": "2021-01-10"
-    })
+    calls = [call({"start_date": "2021-01-01", "end_date": "2021-01-10"}),
+             call({"start_date": "2021-01-03", "end_date": "2021-01-10"})]
+    stream.get_query.assert_has_calls(calls, any_order=False)
+
+    # stream.get_query.assert_any_call({"start_date": "2021-01-01", "end_date": "2021-01-10"})
+
+    assert stream.get_query.call_count == 2
+    # assert stream.get_query.call_args_list == [{"start_date": "2021-01-01", "end_date": "2021-01-10"},
+    #                                            {"start_date": "2021-01-03", "end_date": "2021-01-10"}]
+    # stream.get_query.assert_called_with({
+    #     "start_date": "2021-01-03",
+    #     "end_date": "2021-01-10"
+    # })
 
 
 # =========================
@@ -256,6 +268,7 @@ def test_page_token_expired_retry_fails(mock_ads_client, test_config):
         "start_date": "2021-01-03",
         "end_date": "2021-01-10"
     })
+    assert stream.get_query.call_count == 2
 
 
 # =======================
@@ -321,3 +334,4 @@ def test_page_token_expired_it_should_fail_date_range_1_day(mock_ads_client, tes
         "start_date": "2021-01-03",
         "end_date": "2021-01-04"
     })
+    assert stream.get_query.call_count == 1
