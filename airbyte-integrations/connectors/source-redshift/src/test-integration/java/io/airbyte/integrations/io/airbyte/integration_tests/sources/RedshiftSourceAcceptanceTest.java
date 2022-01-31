1f57a0dbae1d53121e905cbea9a6c5151d211cfb
vmaltsev <vitalii.maltsev@globallogic.com>
2022-01-31 16:21:06 +0200

    fix Redshift tests

--- airbyte-integrations/connectors/source-redshift/src/test-integration/java/io/airbyte/integrations/io/airbyte/integration_tests/sources/RedshiftSourceAcceptanceTest.java
+++ airbyte-integrations/connectors/source-redshift/src/test-integration/java/io/airbyte/integrations/io/airbyte/integration_tests/sources/RedshiftSourceAcceptanceTest.java
@@ -18,13 +18,8 @@ import io.airbyte.db.jdbc.JdbcUtils;
 import io.airbyte.integrations.source.redshift.RedshiftSource;
 import io.airbyte.integrations.standardtest.source.SourceAcceptanceTest;
 import io.airbyte.integrations.standardtest.source.TestDestinationEnv;
-import io.airbyte.protocol.models.AirbyteCatalog;
-import io.airbyte.protocol.models.AirbyteStream;
-import io.airbyte.protocol.models.CatalogHelpers;
-import io.airbyte.protocol.models.ConfiguredAirbyteCatalog;
-import io.airbyte.protocol.models.ConnectorSpecification;
-import io.airbyte.protocol.models.Field;
-import io.airbyte.protocol.models.JsonSchemaPrimitive;
+import io.airbyte.protocol.models.*;
+
 import java.nio.file.Path;
 import java.sql.SQLException;
 import java.util.HashMap;
@@ -33,9 +28,9 @@ import java.util.List;
 public class RedshiftSourceAcceptanceTest extends SourceAcceptanceTest {
 
   protected static final List<Field> FIELDS = List.of(
-      Field.of("c_custkey", JsonSchemaPrimitive.NUMBER),
-      Field.of("c_name", JsonSchemaPrimitive.STRING),
-      Field.of("c_nation", JsonSchemaPrimitive.STRING));
+      Field.of("c_custkey", JsonSchemaType.NUMBER),
+      Field.of("c_name", JsonSchemaType.STRING),
+      Field.of("c_nation", JsonSchemaType.STRING));
 
   // This test case expects an active redshift cluster that is useable from outside of vpc
   protected ObjectNode config;
