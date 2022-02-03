d1bdaf4c7870e186d6593738c932b7a3a1be04ae
Benoit Moriceau <benoit@airbyte.io>
2022-02-02 17:18:16 -0800

    Add concurrent list for test

--- airbyte-workers/src/main/java/io/airbyte/workers/temporal/scheduling/state/listener/TestStateListener.java
+++ airbyte-workers/src/main/java/io/airbyte/workers/temporal/scheduling/state/listener/TestStateListener.java
@@ -9,6 +9,7 @@ import java.util.Optional;
 import java.util.Queue;
 import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentLinkedQueue;
 
 public class TestStateListener implements WorkflowStateChangedListener {
 
@@ -26,7 +27,7 @@ public class TestStateListener implements WorkflowStateChangedListener {
   @Override
   public void addEvent(final UUID testId, final ChangedStateEvent event) {
     Optional.ofNullable(events.get(testId))
-        .or(() -> Optional.of(new LinkedList<>()))
+        .or(() -> Optional.of(new ConcurrentLinkedQueue<>()))
         .stream()
         .forEach((eventQueue) -> {
           eventQueue.add(event);
