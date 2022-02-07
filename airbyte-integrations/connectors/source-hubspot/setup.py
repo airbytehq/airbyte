07ff9acdaae08813c4985bfbd9d85a47b70490f2
auganbay <auganenu@gmail.com>
2022-02-07 14:13:07 +0600

    added __init__.py to unit_tests

--- airbyte-integrations/connectors/source-hubspot/setup.py
+++ airbyte-integrations/connectors/source-hubspot/setup.py
@@ -6,7 +6,7 @@
 from setuptools import find_packages, setup
 
 MAIN_REQUIREMENTS = [
-    "airbyte-cdk",
+    "airbyte-cdk~=0.1",
     "backoff==1.11.1",
     "pendulum==2.1.2",
     "requests==2.26.0",
