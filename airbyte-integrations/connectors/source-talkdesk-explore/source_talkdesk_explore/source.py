292989fe6c51b05c056b098dfdd64376a254cf8a
alexmago <alexandre.martins@saltpay.co>
2022-02-07 22:09:11 +0000

    add source files

---
+++ airbyte-integrations/connectors/source-talkdesk-explore/source_talkdesk_explore/source.py
@@ -0,0 +1,44 @@
+from typing import List, Mapping, Any, Tuple
+
+from airbyte_cdk.logger import AirbyteLogger
+from airbyte_cdk.sources import AbstractSource
+from airbyte_cdk.sources.streams import Stream
+from airbyte_cdk.sources.streams.http.auth import TokenAuthenticator
+
+
+from .talkdesk_auth import TalkdeskAuth
+from .streams import Calls, UserStatus, StudioFlowExecution, Contacts, RingAttempts
+
+
+class SourceTalkdeskExplore(AbstractSource):
+    def check_connection(self, logger: AirbyteLogger, config: Mapping[str, Any]) -> Tuple[bool, Any]:
+        talkdesk_auth = TalkdeskAuth(config)
+        token_request = talkdesk_auth.request_bearer_token()
+
+        # Check for valid token and scope
+        if 'access_token' not in token_request.keys():
+            return False, "Unable to retrieve access token. Check your credentials."
+        elif 'data-reports:read' and 'data-reports:write' not in token_request['scope']:
+            return False, "Provided credential does not have necessary privileges to read data. Required scope: data-reports:read AND data-reports:write"
+        else:
+            return True, None
+
+    def streams(self, config: Mapping[str, Any]) -> List[Stream]:
+        talkdesk_auth = TalkdeskAuth(config)
+        token_request = talkdesk_auth.request_bearer_token()
+        talkdesk_auth_token = token_request.get('access_token', None)
+
+        authenticator = TokenAuthenticator(token=talkdesk_auth_token)
+
+        start_date = config.get("START_DATE", None)
+        timezone = config.get("TIMEZONE", None)
+
+        streams_ = [
+            Calls(start_date=start_date, timezone=timezone, authenticator=authenticator),
+            UserStatus(start_date=start_date, timezone=timezone, authenticator=authenticator),
+            StudioFlowExecution(start_date=start_date, timezone=timezone, authenticator=authenticator),
+            Contacts(start_date=start_date, timezone=timezone, authenticator=authenticator),
+            RingAttempts(start_date=start_date, timezone=timezone, authenticator=authenticator)
+        ]
+
+        return streams_
---
+++ airbyte-integrations/connectors/source-talkdesk-explore/source_talkdesk_explore/talkdesk_auth.py
@@ -0,0 +1,40 @@
+import requests
+import base64
+from typing import Dict, Mapping, Any
+
+
+class TalkdeskAuth:
+    """Main class for handling Talkdesk Authentication.
+    Only 'client_credentials' auth method supported at the moment.
+
+    # TODO: Implement 'Signed JWT' and 'Authorization Code' auth methods.
+
+    Docs: https://docs.talkdesk.com/docs/authentication
+    
+    """
+    def __init__(self, config: Mapping[str, Any]):
+        self.api_key = config.get("API_KEY", None)
+        self.auth_url = config.get("AUTH_URL", None)
+    
+    def _encode_key(self, key: str) -> bytes:
+        """Encode 'str' API key to bytes"""
+        base64_bytes = base64.b64encode(
+            key.encode("ascii")
+        )
+        return base64_bytes.decode("ascii")
+    
+    def request_bearer_token(self) -> Dict:
+        headers = {
+            "Authorization": f"Basic {self._encode_key(self.api_key)}",
+            "Content-Type": "application/x-www-form-urlencoded",
+        }
+        try:
+            response = requests.request(
+                "POST",
+                url=self.auth_url,
+                headers=headers,
+            )
+        except Exception as exc:
+            raise exc
+
+        return response.json()
