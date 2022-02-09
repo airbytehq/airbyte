7a443d01763c39220c94494a65da1fb1da858212
Lake Mossman <lake@airbyte.io>
2022-02-09 14:51:12 -0800

    fix partial success check (#10233)

--- airbyte-webapp/src/components/JobItem/JobItem.tsx
+++ airbyte-webapp/src/components/JobItem/JobItem.tsx
@@ -46,7 +46,10 @@ const JobCurrentLogs: React.FC<{
 };
 
 const isPartialSuccessCheck = (attempts: Attempt[]) => {
-  if (attempts[attempts.length - 1].status === Status.FAILED) {
+  if (
+    attempts.length > 0 &&
+    attempts[attempts.length - 1].status === Status.FAILED
+  ) {
     return attempts.some(
       (attempt) =>
         attempt.failureSummary && attempt.failureSummary.partialSuccess
