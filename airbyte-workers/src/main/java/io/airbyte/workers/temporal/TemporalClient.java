4ef4ca066f713424384b0e490a29e61d0b5b3065
Benoit Moriceau <benoit@airbyte.io>
2022-02-08 16:57:44 -0800

    Do not wait for the workflow to be potentially destroy

--- airbyte-workers/src/main/java/io/airbyte/workers/temporal/TemporalClient.java
+++ airbyte-workers/src/main/java/io/airbyte/workers/temporal/TemporalClient.java
@@ -335,7 +335,7 @@ public class TemporalClient {
 
     connectionManagerWorkflow.cancelJob();
 
-    do {
+    while (connectionManagerWorkflow.getState().isRunning()) {
       try {
         Thread.sleep(DELAY_BETWEEN_QUERY_MS);
       } catch (final InterruptedException e) {
@@ -343,7 +343,7 @@ public class TemporalClient {
             Optional.of("Didn't manage cancel a sync for: " + connectionId),
             Optional.empty());
       }
-    } while (connectionManagerWorkflow.getState().isRunning());
+    }
 
     log.info("end of manual cancellation");
 
