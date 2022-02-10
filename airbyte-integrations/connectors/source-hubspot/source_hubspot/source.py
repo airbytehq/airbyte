dc32e265bd700b9bf1b98c3c847f5d754bcc2121
auganbay <auganenu@gmail.com>
2022-02-10 21:09:58 +0600

    remove comments

--- airbyte-integrations/connectors/source-hubspot/source_hubspot/source.py
+++ airbyte-integrations/connectors/source-hubspot/source_hubspot/source.py
@@ -184,19 +184,3 @@ class SourceHubspot(AbstractSource):
             yield self._checkpoint_state(stream_name, stream_state, connector_state, logger)
             if self._limit_reached(internal_config, total_records_counter):
                 return
-
-
-# class SourceHubspot(BaseSource):
-#     client_class = Client
-#
-#     def _read_stream(
-#         self, logger: logging.Logger, client: BaseClient, configured_stream: ConfiguredAirbyteStream, state: MutableMapping[str, Any]
-#     ):
-#         """
-#         This method is overridden to check if the stream exists in the client.
-#         """
-#         stream_name = configured_stream.stream.name
-#         if not client._apis.get(stream_name):
-#             logger.warning(f"Stream {stream_name} does not exist in the client.")
-#             return
-#         yield from super()._read_stream(logger=logger, client=client, configured_stream=configured_stream, state=state)
