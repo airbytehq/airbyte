e186660edc817557ff682a368f23fbe05765161e
Davin Chia <davinchia@gmail.com>
2022-02-10 19:29:15 +0800

    Rename the current metrics singleton by adding a Prometheus prefix.

--- airbyte-metrics/src/main/java/io/airbyte/metrics/DogstatsdMetricSingleton.java
+++ airbyte-metrics/src/main/java/io/airbyte/metrics/DogstatsdMetricSingleton.java
@@ -6,14 +6,16 @@ package io.airbyte.metrics;
 
 import com.timgroup.statsd.NonBlockingStatsDClientBuilder;
 import com.timgroup.statsd.StatsDClient;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 public class DogstatsdMetricSingleton {
 
   public static void main(final String[] args) throws InterruptedException {
     final StatsDClient client = new NonBlockingStatsDClientBuilder()
-        .prefix("statsd")
-        .hostname("localhost")
-        .port(8125)
+        .prefix("test_a")
+        .hostname(System.getenv("DD_AGENT_HOST"))
+        .port(Integer.parseInt(System.getenv("DD_DOGSTATSD_PORT")))
         .build();
 
     // count
@@ -23,6 +25,7 @@ public class DogstatsdMetricSingleton {
     while (true) {
       System.out.println("publishing metric!");
       client.gauge("davin_test_1", 1);
+      client.count("davin_test_1_counter", 1, "test_1:dev");
       Thread.sleep(1000);
     }
   }
