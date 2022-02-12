ca123d82a331c09ca663c01c87cef21addecb152
Liren Tu <tuliren.git@outlook.com>
2022-02-12 12:39:29 -0800

    Refactor code

--- airbyte-integrations/connectors/destination-gcs/src/main/java/io/airbyte/integrations/destination/gcs/GcsDestination.java
+++ airbyte-integrations/connectors/destination-gcs/src/main/java/io/airbyte/integrations/destination/gcs/GcsDestination.java
@@ -22,7 +22,6 @@ import io.airbyte.protocol.models.ConfiguredAirbyteCatalog;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.nio.charset.StandardCharsets;
-import java.util.UUID;
 import java.util.function.Consumer;
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVPrinter;
@@ -34,8 +33,6 @@ public class GcsDestination extends BaseConnector implements Destination {
   private static final Logger LOGGER = LoggerFactory.getLogger(GcsDestination.class);
   public static final String EXPECTED_ROLES = "storage.multipartUploads.abort, storage.multipartUploads.create, "
       + "storage.objects.create, storage.objects.delete, storage.objects.get, storage.objects.list";
-
-  public static final String CHECK_ACTIONS_TMP_FILE_NAME = "test-" + UUID.randomUUID();
 
   public static void main(final String[] args) throws Exception {
     new IntegrationRunner(new GcsDestination()).run(args);
@@ -68,27 +65,31 @@ public class GcsDestination extends BaseConnector implements Destination {
 
   private void testSingleUpload(final AmazonS3 s3Client, final GcsDestinationConfig destinationConfig) {
     LOGGER.info("Started testing if all required credentials assigned to user for single file uploading");
-    s3Client.putObject(destinationConfig.getBucketName(), CHECK_ACTIONS_TMP_FILE_NAME, "this is a test file");
-    s3Client.deleteObject(destinationConfig.getBucketName(), CHECK_ACTIONS_TMP_FILE_NAME);
+    final String testFile = "test_" + System.currentTimeMillis();
+    s3Client.putObject(destinationConfig.getBucketName(), testFile, "this is a test file");
+    s3Client.deleteObject(destinationConfig.getBucketName(), testFile);
     LOGGER.info("Finished checking for normal upload mode");
   }
 
   private void testMultipartUpload(final AmazonS3 s3Client, final GcsDestinationConfig destinationConfig) throws IOException {
+    final String testFile = "test_" + System.currentTimeMillis();
     final StreamTransferManager manager = S3StreamTransferManagerHelper.getDefault(
         destinationConfig.getBucketName(),
-        CHECK_ACTIONS_TMP_FILE_NAME,
+        testFile,
         s3Client,
         (long) S3StreamTransferManagerHelper.DEFAULT_PART_SIZE_MB);
+
     try (final MultiPartOutputStream outputStream = manager.getMultiPartOutputStreams().get(0);
         final CSVPrinter csvPrinter = new CSVPrinter(new PrintWriter(outputStream, true, StandardCharsets.UTF_8), CSVFormat.DEFAULT)) {
       final String oneMegaByteString = "a".repeat(500_000);
-      // write a ~7 MB file, which is larger than the 5 MB default part size, to make sure it is a multipart upload
+      // write a file larger than the 5 MB, which is the default part size, to make sure it is a multipart upload
       for (int i = 0; i < 7; ++i) {
         csvPrinter.printRecord(System.currentTimeMillis(), oneMegaByteString);
       }
     }
+
     manager.complete();
-    s3Client.deleteObject(destinationConfig.getBucketName(), CHECK_ACTIONS_TMP_FILE_NAME);
+    s3Client.deleteObject(destinationConfig.getBucketName(), testFile);
   }
 
   @Override
