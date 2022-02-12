3e03b5652a6982f8678b62c07d91eb733c9ffb42
Sergey Chvalyuk <grubberr@gmail.com>
2022-02-12 12:00:14 +0200

    waitstatus_to_exitcode added for pty_spawn.py
    Signed-off-by: Sergey Chvalyuk <grubberr@gmail.com>

--- tools/bin/pty_spawn.py
+++ tools/bin/pty_spawn.py
@@ -4,4 +4,26 @@ import os
 import sys
 import pty
 
-sys.exit(os.WEXITSTATUS(pty.spawn(sys.argv[1:])))
+
+def waitstatus_to_exitcode(status):
+    """
+    https://github.com/python/cpython/blob/e0bc8ee945af96f9395659bbe3cc30b082e7a361/Modules/posixmodule.c#L14658-L14748
+    """
+
+    if os.WIFEXITED(status):
+        exitcode = os.WEXITSTATUS(status)
+        if exitcode < 0: raise ValueError("invalid WEXITSTATUS: %i" % exitcode)
+        return exitcode
+    elif os.WIFSIGNALED(status):
+        signum = os.WTERMSIG(status)
+        if signum <= 0: raise ValueError("invalid WTERMSIG: %i" % signum)
+        return -signum
+    elif os.WIFSTOPPED(status):
+        raise ValueError("process stopped by delivery of signal %i" % os.WSTOPSIG(status))
+    else:
+        raise ValueError("invalid wait status: %i" % status)
+
+
+status = pty.spawn(sys.argv[1:])
+exitcode = waitstatus_to_exitcode(status)
+sys.exit(exitcode)
