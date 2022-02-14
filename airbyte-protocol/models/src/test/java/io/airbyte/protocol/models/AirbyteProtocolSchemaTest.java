064d3dafd3408a3ee448b86b91e58454963b8da4
Liren Tu <tuliren.git@outlook.com>
2022-02-13 22:21:21 -0800

    Remove unused variables in test

--- airbyte-protocol/models/src/test/java/io/airbyte/protocol/models/AirbyteProtocolSchemaTest.java
+++ airbyte-protocol/models/src/test/java/io/airbyte/protocol/models/AirbyteProtocolSchemaTest.java
@@ -4,8 +4,6 @@
 
 package io.airbyte.protocol.models;
 
-import static io.airbyte.protocol.models.JsonSchemaType.DATE_TIME;
-import static io.airbyte.protocol.models.JsonSchemaType.TIMESTAMP_WITH_TIMEZONE;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.io.IOException;
@@ -30,20 +28,6 @@ class AirbyteProtocolSchemaTest {
 
   @Test
   void testJsonSchemaType() {
-    JsonSchemaType build = new JsonSchemaType.Builder()
-        .withType(JsonSchemaPrimitive.STRING)
-        .withFormat(DATE_TIME)
-        .build();
-    JsonSchemaType build1 = new JsonSchemaType.Builder()
-        .withType(JsonSchemaPrimitive.STRING)
-        .withFormat(DATE_TIME)
-        .build();
-    JsonSchemaType build2 = new JsonSchemaType.Builder()
-        .withType(JsonSchemaPrimitive.STRING)
-        .withFormat(TIMESTAMP_WITH_TIMEZONE)
-        .build();
-    JsonSchemaType build3 = JsonSchemaType.STRING;
-    JsonSchemaType build4 = JsonSchemaType.STRING;;
     for (final AirbyteProtocolSchema value : AirbyteProtocolSchema.values()) {
       assertTrue(Files.exists(value.getFile().toPath()));
     }
