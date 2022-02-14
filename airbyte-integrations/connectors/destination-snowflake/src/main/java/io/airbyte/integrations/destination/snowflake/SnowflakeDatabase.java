042cc6830ff68cbe73b3b7bd1c014f05098b3054
ievgeniit <etsybaev@gmail.com>
2022-02-14 15:20:10 +0200

    added basic logic for token refresh

--- airbyte-integrations/connectors/destination-snowflake/src/main/java/io/airbyte/integrations/destination/snowflake/SnowflakeDatabase.java
+++ airbyte-integrations/connectors/destination-snowflake/src/main/java/io/airbyte/integrations/destination/snowflake/SnowflakeDatabase.java
@@ -4,14 +4,29 @@
 
 package io.airbyte.integrations.destination.snowflake;
 
+import static java.util.stream.Collectors.joining;
+
 import com.fasterxml.jackson.databind.JsonNode;
+import io.airbyte.commons.json.Jsons;
 import io.airbyte.db.jdbc.DefaultJdbcDatabase;
 import io.airbyte.db.jdbc.DefaultJdbcDatabase.CloseableConnectionSupplier;
 import io.airbyte.db.jdbc.JdbcDatabase;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URLEncoder;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpRequest.BodyPublisher;
+import java.net.http.HttpRequest.BodyPublishers;
+import java.net.http.HttpResponse;
+import java.nio.charset.StandardCharsets;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.time.Duration;
+import java.util.Base64;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Properties;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -26,6 +41,12 @@ public class SnowflakeDatabase {
   private static final SnowflakeSQLNameTransformer nameTransformer = new SnowflakeSQLNameTransformer();
   private static final Logger LOGGER = LoggerFactory.getLogger(SnowflakeDatabase.class);
 
+  private static final String REFRESH_TOKEN_URL = "https://%s/oauth/token-request";
+  private static final HttpClient httpClient = HttpClient.newBuilder()
+      .version(HttpClient.Version.HTTP_2)
+      .connectTimeout(Duration.ofSeconds(10))
+      .build();
+
   public static Connection getConnection(final JsonNode config) throws SQLException {
 
     final StringBuilder jdbcUrl = new StringBuilder(String.format("jdbc:snowflake://%s/?",
@@ -34,11 +55,20 @@ public class SnowflakeDatabase {
     final Properties properties = new Properties();
 
     final JsonNode credentials = config.get("credentials");
-    if (credentials.has("auth_type") && "Client".equals(credentials.get("auth_type").asText())){
+    if (credentials.has("auth_type") && "Client".equals(credentials.get("auth_type").asText())) {
       // OAuth login option is selected on UI
+      final String accessToken;
+      try {
+        // accessToken is only valid for 10 minutes. So we need to get a new one before processing new
+        // stream
+        accessToken = getAccessTokenUsingRefreshToken(config.get("host").asText(), credentials.get("client_id").asText(),
+            credentials.get("client_secret").asText(), credentials.get("refresh_token").asText());
+      } catch (IOException e) {
+        throw new RuntimeException(e);
+      }
       properties.put("authenticator", "oauth");
-      properties.put("token", credentials.get("access_token").asText());
-    }else {
+      properties.put("token", accessToken);
+    } else {
       // Username and pass login option is selected on UI
       properties.put("user", credentials.get("username").asText());
       properties.put("password", credentials.get("password").asText());
@@ -69,6 +99,44 @@ public class SnowflakeDatabase {
     LOGGER.info(jdbcUrl.toString());
 
     return DriverManager.getConnection(jdbcUrl.toString(), properties);
+  }
+
+  private static String getAccessTokenUsingRefreshToken(final String hostName,
+                                                        final String clientId,
+                                                        final String clientSecret,
+                                                        final String refreshCode)
+      throws IOException {
+    final var refreshTokenUri = String.format(REFRESH_TOKEN_URL, hostName);
+    final Map<String, String> requestBody = new HashMap<>();
+    requestBody.put("grant_type", "refresh_token");
+    requestBody.put("refresh_token", refreshCode);
+
+    try {
+      BodyPublisher bodyPublisher = BodyPublishers.ofString(requestBody.keySet().stream()
+          .map(key -> key + "=" + URLEncoder.encode(requestBody.get(key), StandardCharsets.UTF_8))
+          .collect(joining("&")));
+
+      final HttpRequest request = HttpRequest.newBuilder()
+          .POST(bodyPublisher)
+          .uri(URI.create(refreshTokenUri))
+          .header("Content-Type", "application/x-www-form-urlencoded")
+          .header("Accept", "application/json")
+          .header("Authorization", "Basic " + new String(
+              Base64.getEncoder().encode((clientId + ":" + clientSecret).getBytes())))
+          .build();
+
+      final HttpResponse<String> response = httpClient.send(request,
+          HttpResponse.BodyHandlers.ofString());
+
+      final JsonNode jsonResponse = Jsons.deserialize(response.body());
+      if (jsonResponse.has("access_token")) {
+        return jsonResponse.get("access_token").asText();
+      } else {
+        throw new RuntimeException("Failed to obtain accessToken using refresh token. " + jsonResponse);
+      }
+    } catch (final InterruptedException e) {
+      throw new IOException("Failed to refreshToken", e);
+    }
   }
 
   public static JdbcDatabase getDatabase(final JsonNode config) {
