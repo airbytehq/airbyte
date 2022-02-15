3ec6837cf5247ef4f31087f7052e1e032edcf453
Sergey Chvalyuk <grubberr@gmail.com>
2022-02-15 21:20:16 +0200

    implement queue in queue to keep record order
    Signed-off-by: Sergey Chvalyuk <grubberr@gmail.com>

--- airbyte-cdk/python/airbyte_cdk/utils/concurrent.py
+++ airbyte-cdk/python/airbyte_cdk/utils/concurrent.py
@@ -3,12 +3,11 @@
 #
 
 
-import math
 import queue
 import threading
-import time
-from dataclasses import dataclass, field
-from typing import Callable, Dict, Optional, Union
+from dataclasses import dataclass
+from functools import total_ordering
+from typing import Callable, ClassVar, Dict
 
 from airbyte_cdk.models import ConfiguredAirbyteStream, SyncMode
 from airbyte_cdk.sources.streams import Stream
@@ -18,34 +17,38 @@ class StopException(Exception):
     pass
 
 
-@dataclass(order=True, frozen=True)
-class Cursor:
-    slice_number: Union[float, int]
-    record_number: Union[float, int]
+@total_ordering
+class Message:
+    prio = 0
+
+    def __eq__(self, other):
+        if not isinstance(other, Message):
+            return TypeError
+        return self.prio == other.prio
+
+    def __lt__(self, other):
+        if not isinstance(other, Message):
+            return TypeError
+        return self.prio < other.prio
 
 
-@dataclass(order=True, frozen=True)
-class QueueMessage(Cursor):
-    record: Optional[Dict] = field(compare=False, default=None)
-
-    @property
-    def cursor(self) -> Cursor:
-        return Cursor(self.slice_number, self.record_number)
-
-    def next_cursor(self) -> Cursor:
-        if self.record is None:
-            return Cursor(self.slice_number + 1, 1)
-        return Cursor(self.slice_number, self.record_number + 1)
+@dataclass(eq=False)
+class MessageDone(Message):
+    pass
 
 
-messageDone = QueueMessage(math.inf, math.inf)
-messageFail = QueueMessage(0, 0)
+@dataclass(eq=False)
+class MessageFail(Message):
+    prio: ClassVar[int] = 1
+    exception: Exception
+
+
+@dataclass(eq=False)
+class MessageData(Message):
+    record: Dict
 
 
 class ConcurrentStreamReader:
-
-    DELAY = 0.1
-
     def __init__(self, stream_instance: Stream, configured_stream: ConfiguredAirbyteStream):
         self.stream_instance = stream_instance
         self.max_workers = stream_instance.max_workers
@@ -53,12 +56,12 @@ class ConcurrentStreamReader:
         self.configured_stream = configured_stream
 
         self.stop_all = threading.Event()
-        self.to_workers: queue.Queue[QueueMessage] = queue.Queue(maxsize=self.max_workers * 100)
-        self.to_consumer: queue.Queue[QueueMessage] = queue.PriorityQueue()
-        self.cursor = Cursor(1, 1)
-        self.delay: float = self.DELAY
+        self.to_workers: queue.Queue[Message] = queue.Queue(maxsize=self.max_workers * 100)
+        self.to_consumer: queue.Queue[Message] = queue.Queue()
+        self.to_iterator: queue.Queue[Message] = queue.PriorityQueue()
 
     def __enter__(self):
+        threading.Thread(target=self._safe_thread, args=(self.consumer,), daemon=True).start()
         for _ in range(self.max_workers):
             threading.Thread(target=self._safe_thread, args=(self.worker,), daemon=True).start()
         threading.Thread(target=self._safe_thread, args=(self.producer,), daemon=True).start()
@@ -74,7 +77,7 @@ class ConcurrentStreamReader:
             pass
         except Exception as e:
             self.logger.exception(e)
-            self.to_consumer.put(messageFail)
+            self.to_iterator.put(MessageFail(exception=e))
 
     def _q_put(self, Q, message):
         while True:
@@ -98,51 +101,46 @@ class ConcurrentStreamReader:
 
     def producer(self):
         slices = self.stream_instance.stream_slices(sync_mode=SyncMode.full_refresh, cursor_field=self.configured_stream.cursor_field)
-        for slice_number, slice in enumerate(slices, 1):
-            q_message = QueueMessage(slice_number, 0, slice)
-            self._q_put(self.to_workers, q_message)
+        for slice in slices:
+            Q = queue.Queue()
+            message = MessageData(record={"queue": Q, "slice": slice})
+            self._q_put(self.to_workers, message)
+            self._q_put(self.to_consumer, message)
         for _ in range(self.max_workers):
-            self._q_put(self.to_workers, messageDone)
+            self._q_put(self.to_workers, MessageDone())
+        self._q_put(self.to_consumer, MessageDone())
 
     def worker(self):
         while True:
-            item = self._q_get(self.to_workers)
-            if item == messageDone:
-                self.to_consumer.put(messageDone)
+            message = self._q_get(self.to_workers)
+            if isinstance(message, MessageDone):
                 break
-
-            slice = item.record
+            queue, slice = message.record["queue"], message.record["slice"]
             records = self.stream_instance.read_records(
                 stream_slice=slice, sync_mode=SyncMode.full_refresh, cursor_field=self.configured_stream.cursor_field
             )
-            for record_number, record in enumerate(records, 1):
-                q_message = QueueMessage(item.slice_number, record_number, record)
-                self._q_put(self.to_consumer, q_message)
-            q_message = QueueMessage(item.slice_number, record_number + 1)
-            self._q_put(self.to_consumer, q_message)
+            for record in records:
+                self._q_put(queue, MessageData(record=record))
+            self._q_put(queue, MessageDone())
+
+    def consumer(self):
+        while True:
+            message = self._q_get(self.to_consumer)
+            if isinstance(message, MessageDone):
+                self._q_put(self.to_iterator, message)
+                break
+            queue = message.record["queue"]
+            while True:
+                message = self._q_get(queue)
+                if isinstance(message, MessageDone):
+                    break
+                self._q_put(self.to_iterator, message)
 
     def __iter__(self):
-        stopped_workers = 0
         while True:
-            item = self.to_consumer.get(timeout=300)
-            if item == messageDone:
-                stopped_workers += 1
-                if stopped_workers >= self.max_workers:
-                    return
-            elif item == messageFail:
-                return
-            else:
-                if not self.is_next(item):
-                    self.to_consumer.put(item)
-                elif item.record:
-                    yield item.record
-
-    def is_next(self, q_message: QueueMessage) -> bool:
-        if q_message.cursor == self.cursor:
-            self.delay = self.DELAY
-            self.cursor = q_message.next_cursor()
-            return True
-
-        self.delay = self.delay * 2
-        time.sleep(self.delay)
-        return False
+            message = self.to_iterator.get(timeout=300)
+            if isinstance(message, MessageDone):
+                break
+            if isinstance(message, MessageFail):
+                raise message.exception
+            yield message.record
