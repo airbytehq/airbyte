abdc3db10107cfee1c9c7a9c7586f5d8c30174b2
Malik Diarra <malik@airbyte.io>
2022-02-14 18:59:08 -0800

    Update airbyte-db/lib/src/main/java/io/airbyte/db/instance/configs/migrations/V0_35_28_001__AddActorCatalogMetadataColumns.java
    Co-authored-by: LiRen Tu <tuliren.git@outlook.com>

--- airbyte-db/lib/src/main/java/io/airbyte/db/instance/configs/migrations/V0_35_28_001__AddActorCatalogMetadataColumns.java
+++ airbyte-db/lib/src/main/java/io/airbyte/db/instance/configs/migrations/V0_35_28_001__AddActorCatalogMetadataColumns.java
@@ -16,7 +16,6 @@ import org.jooq.impl.SQLDataType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-// TODO: update migration description in the class name
 public class V0_35_28_001__AddActorCatalogMetadataColumns extends BaseJavaMigration {
 
   private static final Logger LOGGER = LoggerFactory.getLogger(
