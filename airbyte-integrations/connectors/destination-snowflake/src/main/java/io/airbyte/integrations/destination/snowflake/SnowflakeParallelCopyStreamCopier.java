ef3bb56c7a104c8dd5ce9ac95817ec560cd34907
vmaltsev <vitalii.maltsev@globallogic.com>
2022-02-16 20:40:20 +0200

    update parallel copy with CompletableFuture

--- airbyte-integrations/connectors/destination-snowflake/src/main/java/io/airbyte/integrations/destination/snowflake/SnowflakeParallelCopyStreamCopier.java
+++ airbyte-integrations/connectors/destination-snowflake/src/main/java/io/airbyte/integrations/destination/snowflake/SnowflakeParallelCopyStreamCopier.java
@@ -4,9 +4,12 @@
 
 package io.airbyte.integrations.destination.snowflake;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.StringJoiner;
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutorService;
+import java.util.stream.IntStream;
 
 interface SnowflakeParallelCopyStreamCopier {
 
@@ -17,15 +20,20 @@ interface SnowflakeParallelCopyStreamCopier {
   }
 
   default void copyFilesInParallel(List<List<String>> partitions, ExecutorService executorService) {
-    partitions.forEach(files -> {
-      try {
-        executorService.execute(() -> copyIntoStage(files));
+
+    CompletableFuture[] futures = new CompletableFuture[partitions.size()];
+    IntStream.range(0, partitions.size()).forEach(i -> {
+      List<String> partition = partitions.get(i);
+      futures[i] = CompletableFuture.runAsync(() -> copyIntoStage(partition), executorService);
+    });
+    try {
+        // This will wait until all futures ready.
+        CompletableFuture.allOf(futures).join();
       } catch (Exception e) {
         throw new RuntimeException("Failed to copy files from stage to tmp table {}" + e);
       } finally {
         executorService.shutdown();
       }
-    });
   }
 
   void copyIntoStage(List<String> files);
