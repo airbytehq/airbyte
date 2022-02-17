843bb0e04ffef5314f27daa0807415088975dd02
Sergey Chvalyuk <grubberr@gmail.com>
2022-02-18 00:32:15 +0200

    ProjectColumns added
    Signed-off-by: Sergey Chvalyuk <grubberr@gmail.com>

--- airbyte-integrations/connectors/source-github/source_github/streams.py
+++ airbyte-integrations/connectors/source-github/source_github/streams.py
@@ -180,11 +180,15 @@ class GithubStream(HttpStream, ABC):
         for record in response.json():  # GitHub puts records in an array.
             yield self.transform(record=record, repository=stream_slice["repository"])
 
-    def transform(self, record: MutableMapping[str, Any], repository: str = None, organization: str = None) -> MutableMapping[str, Any]:
+    def transform(
+        self, record: MutableMapping[str, Any], repository: str = None, organization: str = None, project_id: str = None
+    ) -> MutableMapping[str, Any]:
         if repository:
             record["repository"] = repository
         if organization:
             record["organization"] = organization
+        if project_id:
+            record["project_id"] = project_id
 
         return record
 
@@ -837,3 +841,70 @@ class Deployments(SemiIncrementalGithubStream):
 
     def path(self, stream_slice: Mapping[str, Any] = None, **kwargs) -> str:
         return f"repos/{stream_slice['repository']}/deployments"
+
+
+class ProjectColumns(GithubStream):
+
+    cursor_field = "updated_at"
+
+    def __init__(self, parent: HttpStream, start_date: str, **kwargs):
+        super().__init__(**kwargs)
+        self.parent = parent
+        self._start_date = start_date
+
+    def path(self, stream_slice: Mapping[str, Any] = None, **kwargs) -> str:
+        return f"projects/{stream_slice['project_id']}/columns"
+
+    def stream_slices(
+        self, sync_mode: SyncMode, cursor_field: List[str] = None, stream_state: Mapping[str, Any] = None
+    ) -> Iterable[Optional[Mapping[str, Any]]]:
+        parent_stream_slices = self.parent.stream_slices(sync_mode=sync_mode, cursor_field=cursor_field, stream_state=stream_state)
+        for stream_slice in parent_stream_slices:
+            parent_records = self.parent.read_records(
+                sync_mode=sync_mode, cursor_field=cursor_field, stream_slice=stream_slice, stream_state=stream_state
+            )
+            for record in parent_records:
+                yield {"repository": record["repository"], "project_id": str(record["id"])}
+
+    def read_records(
+        self,
+        sync_mode: SyncMode,
+        cursor_field: List[str] = None,
+        stream_slice: Mapping[str, Any] = None,
+        stream_state: Mapping[str, Any] = None,
+    ) -> Iterable[Mapping[str, Any]]:
+        starting_point = self.get_starting_point(stream_state=stream_state, stream_slice=stream_slice)
+        for record in super().read_records(
+            sync_mode=sync_mode, cursor_field=cursor_field, stream_slice=stream_slice, stream_state=stream_state
+        ):
+            if record[self.cursor_field] > starting_point:
+                yield record
+
+    def get_starting_point(self, stream_state: Mapping[str, Any], stream_slice: Mapping[str, Any]) -> str:
+        repository = stream_slice["repository"]
+        project_id = stream_slice["project_id"]
+        stream_state_value = stream_state.get(repository, {}).get(project_id, {}).get(self.cursor_field)
+        if stream_state_value:
+            return max(self._start_date, stream_state_value)
+        return self._start_date
+
+    def get_updated_state(self, current_stream_state: MutableMapping[str, Any], latest_record: Mapping[str, Any]):
+        repository = latest_record["repository"]
+        project_id = latest_record["project_id"]
+        updated_state = latest_record[self.cursor_field]
+        stream_state_value = current_stream_state.get(repository, {}).get(project_id, {}).get(self.cursor_field)
+        if stream_state_value:
+            updated_state = max(updated_state, stream_state_value)
+        current_stream_state.setdefault(repository, {}).setdefault(project_id, {})[self.cursor_field] = updated_state
+        return current_stream_state
+
+    def parse_response(
+        self,
+        response: requests.Response,
+        *,
+        stream_state: Mapping[str, Any],
+        stream_slice: Mapping[str, Any] = None,
+        next_page_token: Mapping[str, Any] = None,
+    ) -> Iterable[Mapping]:
+        for record in response.json():
+            yield self.transform(record=record, repository=stream_slice["repository"], project_id=stream_slice["project_id"])
