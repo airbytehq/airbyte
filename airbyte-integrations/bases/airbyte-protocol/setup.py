a3cc1432c1e8b564ad382f71d661e94597bf2d8c
antixar <antixar@gmail.com>
2022-02-18 13:34:56 +0200

    fix markupsafe dep

--- airbyte-integrations/bases/airbyte-protocol/setup.py
+++ airbyte-integrations/bases/airbyte-protocol/setup.py
@@ -2,8 +2,16 @@
 # Copyright (c) 2021 Airbyte, Inc., all rights reserved.
 #
 
-
 import setuptools
+
+MAIN_REQUIREMENTS = (
+    "PyYAML==5.4",
+    "pydantic==1.6.*",
+    # the new version (>= 2.1.0) of package markupsafe removed the funcion `soft_unicode`. And it broke other dependences
+    # https://github.com/pallets/markupsafe/blob/main/CHANGES.rst
+    # thus this version is pinned
+    "markupsafe==2.0.1",
+)
 
 setuptools.setup(
     name="airbyte-protocol",
@@ -13,5 +21,5 @@ setuptools.setup(
     url="https://github.com/airbytehq/airbyte",
     packages=setuptools.find_packages(),
     package_data={"": ["models/yaml/*.yaml"]},
-    install_requires=["PyYAML==5.4", "pydantic==1.6.*", "markupsafe~=2.1.0"],
+    install_requires=MAIN_REQUIREMENTS,
 )
