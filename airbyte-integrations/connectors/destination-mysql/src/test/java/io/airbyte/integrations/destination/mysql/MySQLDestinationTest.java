e090e7a2c5effcfecf4003c1221e395ceabcca7d
Alexandre Girard <alexandre@airbyte.io>
2022-02-18 09:59:47 -0800

    fix test

--- airbyte-integrations/connectors/destination-mysql/src/test/java/io/airbyte/integrations/destination/mysql/MySQLDestinationTest.java
+++ airbyte-integrations/connectors/destination-mysql/src/test/java/io/airbyte/integrations/destination/mysql/MySQLDestinationTest.java
@@ -7,7 +7,6 @@ import com.google.common.collect.ImmutableMap;
 import io.airbyte.commons.json.Jsons;
 import io.airbyte.commons.map.MoreMaps;
 import java.util.Map;
-import org.junit.Before;
 import org.junit.jupiter.api.Test;
 
 public class MySQLDestinationTest {
@@ -18,7 +17,6 @@ public class MySQLDestinationTest {
   );
 
   public static final String JDBC_URL = "jdbc:mysql://localhost:1337/db";
-  private MySQLDestination destination;
 
   private JsonNode buildConfigNoJdbcParameters() {
     return Jsons.jsonNode(ImmutableMap.of(
@@ -49,40 +47,35 @@ public class MySQLDestinationTest {
     ));
   }
 
-  @Before
-  public void setUp() {
-    destination = new MySQLDestination();
-  }
-
   @Test
   void testNoExtraParams() {
     final JsonNode config = buildConfigNoJdbcParameters();
-    final JsonNode jdbcConfig = destination.toJdbcConfig(config);
+    final JsonNode jdbcConfig = new MySQLDestination().toJdbcConfig(config);
     assertEquals(JDBC_URL, jdbcConfig.get("jdbc_url").asText());
   }
 
   @Test
   void testEmptyExtraParams() {
-    final JsonNode jdbcConfig = destination.toJdbcConfig(buildConfigWithExtraJdbcParameters(""));
+    final JsonNode jdbcConfig = new MySQLDestination().toJdbcConfig(buildConfigWithExtraJdbcParameters(""));
     assertEquals(JDBC_URL, jdbcConfig.get("jdbc_url").asText());
   }
 
   @Test
   void testExtraParams() {
     final String extraParam = "key1=value1&key2=value2&key3=value3";
-    final JsonNode jdbcConfig = destination.toJdbcConfig(buildConfigWithExtraJdbcParameters(extraParam));
+    final JsonNode jdbcConfig = new MySQLDestination().toJdbcConfig(buildConfigWithExtraJdbcParameters(extraParam));
     assertEquals(JDBC_URL, jdbcConfig.get("jdbc_url").asText());
   }
 
   @Test
   void testDefaultParamsNoSSL() {
-    final Map<String, String> defaultProperties = destination.getDefaultConnectionProperties(buildConfigNoExtraJdbcParametersWithoutSsl());
+    final Map<String, String> defaultProperties = new MySQLDestination().getDefaultConnectionProperties(buildConfigNoExtraJdbcParametersWithoutSsl());
     assertEquals(MySQLDestination.DEFAULT_JDBC_PARAMETERS, defaultProperties);
   }
 
   @Test
   void testDefaultParamsWithSSL() {
-    final Map<String, String> defaultProperties = destination.getDefaultConnectionProperties(buildConfigNoJdbcParameters());
+    final Map<String, String> defaultProperties = new MySQLDestination().getDefaultConnectionProperties(buildConfigNoJdbcParameters());
     assertEquals(DEFAULT_PARAMETERS_WITH_SSL, defaultProperties);
   }
 }
