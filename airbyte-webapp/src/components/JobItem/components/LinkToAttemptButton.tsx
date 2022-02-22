93b34e28d310be3b24fe3f7c74253fda61730ba9
Tim Roes <tim@airbyte.io>
2022-02-22 20:28:07 +0100

    Add Copy link to log output button (#10506)
    * Add link to log button
    
    * Extract hook into utils file

---
+++ airbyte-webapp/src/components/JobItem/components/LinkToAttemptButton.tsx
@@ -0,0 +1,52 @@
+import React, { useState } from "react";
+import { useDebounce } from "react-use";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faLink } from "@fortawesome/free-solid-svg-icons";
+
+import { Button } from "components";
+import ToolTip from "components/ToolTip";
+import { copyToClipboard } from "utils/clipboard";
+import { buildAttemptLink } from "../attemptLinkUtils";
+import { FormattedMessage, useIntl } from "react-intl";
+
+interface Props {
+  jobId: string | number;
+  attemptId?: number;
+}
+
+export const LinkToAttemptButton: React.FC<Props> = ({ jobId, attemptId }) => {
+  const { formatMessage } = useIntl();
+
+  const [showCopyTooltip, setShowCopyTooltip] = useState(false);
+  const [hideTooltip] = useDebounce(() => setShowCopyTooltip(false), 3000, [
+    showCopyTooltip,
+  ]);
+
+  const onCopyLink = async () => {
+    // Get the current URL and replace (or add) hash to current log
+    const url = new URL(window.location.href);
+    url.hash = buildAttemptLink(jobId, attemptId);
+    await copyToClipboard(url.href);
+    // Show and hide tooltip with a delay again
+    setShowCopyTooltip(true);
+    hideTooltip();
+  };
+
+  return (
+    <ToolTip
+      disabled={!showCopyTooltip}
+      control={
+        <Button
+          secondary
+          onClick={onCopyLink}
+          title={formatMessage({ id: "connection.copyLogLink" })}
+          aria-label={formatMessage({ id: "connection.copyLogLink" })}
+        >
+          <FontAwesomeIcon icon={faLink} />
+        </Button>
+      }
+    >
+      <FormattedMessage id="connection.linkCopied" />
+    </ToolTip>
+  );
+};
