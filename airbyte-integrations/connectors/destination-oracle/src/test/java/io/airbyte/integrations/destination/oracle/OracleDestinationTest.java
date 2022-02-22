c3a3dca32fe4e4782c2052f1c073ecd1ab215e07
Alexandre Girard <alexandre@airbyte.io>
2022-02-22 10:59:11 -0800

    fix oracle destination test

---
+++ airbyte-integrations/connectors/destination-oracle/src/test/java/io/airbyte/integrations/destination/oracle/OracleDestinationTest.java
@@ -0,0 +1,14 @@
+package io.airbyte.integrations.destination.oracle;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class OracleDestinationTest {
+
+  @Test
+  void test() {
+    assertTrue(true);
+  }
+
+}
