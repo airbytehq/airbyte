6fe86f9403a6a2ddfc88c6d33efa0388e0f362e0
CremaLuca <lc.crema@hotmail.com>
2022-02-22 17:36:18 +0100

    feat: ran generate.sh to create yahoo-finance HTTP source

---
+++ airbyte-integrations/connectors/source-yahoo-finance/main.py
@@ -0,0 +1,13 @@
+#
+# Copyright (c) 2021 Airbyte, Inc., all rights reserved.
+#
+
+
+import sys
+
+from airbyte_cdk.entrypoint import launch
+from source_yahoo_finance import SourceYahooFinance
+
+if __name__ == "__main__":
+    source = SourceYahooFinance()
+    launch(source, sys.argv[1:])
---
+++ airbyte-integrations/connectors/source-yahoo-finance/source_yahoo_finance/__init__.py
@@ -0,0 +1,8 @@
+#
+# Copyright (c) 2021 Airbyte, Inc., all rights reserved.
+#
+
+
+from .source import SourceYahooFinance
+
+__all__ = ["SourceYahooFinance"]
