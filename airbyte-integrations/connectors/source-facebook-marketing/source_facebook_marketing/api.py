55541c5db20987b96ed6d43001a9b9855717eb22
Eugene Kulak <widowmakerreborn@gmail.com>
2022-02-24 01:01:44 +0400

    Update airbyte-integrations/connectors/source-facebook-marketing/source_facebook_marketing/api.py
    Co-authored-by: Sherif A. Nada <snadalive@gmail.com>

--- airbyte-integrations/connectors/source-facebook-marketing/source_facebook_marketing/api.py
+++ airbyte-integrations/connectors/source-facebook-marketing/source_facebook_marketing/api.py
@@ -93,6 +93,9 @@ class MyFacebookAdsApi(FacebookAdsApi):
             max_pause_interval = self.pause_interval_minimum
 
             for record in response.json():
+                # there are two types of failures: 
+                # 1. no response (we execute batch until all inner requests has response)
+                # 2. response with error (we crash loudly)
                 # in case it is failed inner request the headers might not be present
                 if "headers" not in record:
                     continue
