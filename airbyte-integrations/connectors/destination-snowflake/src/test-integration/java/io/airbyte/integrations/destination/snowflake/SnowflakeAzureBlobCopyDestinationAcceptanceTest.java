9c5529eb08b426e62ed917601db097f628e42dd5
vmaltsev <vitalii.maltsev@globallogic.com>
2022-02-24 15:41:59 +0200

    add tests

---
+++ airbyte-integrations/connectors/destination-snowflake/src/test-integration/java/io/airbyte/integrations/destination/snowflake/SnowflakeAzureBlobCopyDestinationAcceptanceTest.java
@@ -0,0 +1,18 @@
+package io.airbyte.integrations.destination.snowflake;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.google.api.client.util.Preconditions;
+import io.airbyte.commons.io.IOs;
+import io.airbyte.commons.json.Jsons;
+import java.nio.file.Path;
+
+public class SnowflakeAzureBlobCopyDestinationAcceptanceTest extends SnowflakeInsertDestinationAcceptanceTest{
+  @Override
+  public JsonNode getStaticConfig() {
+    final JsonNode copyConfig = Jsons.deserialize(IOs.readFile(Path.of("secrets/copy_azure_blob_config.json")));
+    Preconditions.checkArgument(SnowflakeDestination.isAzureBlobCopy(copyConfig));
+    Preconditions.checkArgument(!SnowflakeDestination.isS3Copy(copyConfig));
+    Preconditions.checkArgument(!SnowflakeDestination.isGcsCopy(copyConfig));
+    return copyConfig;
+  }
+}
