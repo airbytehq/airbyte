353b2456e562931a23cd763fc1c7fc9628983faf
Yurii Bidiuk <yura.bidyuk@gmail.com>
2022-03-03 00:37:11 +0200

    fix comments

--- airbyte-integrations/bases/standard-destination-test/src/main/java/io/airbyte/integrations/standardtest/destination/DateTimeUtils.java
+++ airbyte-integrations/bases/standard-destination-test/src/main/java/io/airbyte/integrations/standardtest/destination/DateTimeUtils.java
@@ -4,8 +4,6 @@
 
 package io.airbyte.integrations.standardtest.destination;
 
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
 import java.time.Instant;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -15,32 +13,29 @@ import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
 import java.time.temporal.ChronoField;
-import java.util.Date;
 import java.util.function.Function;
 import java.util.regex.Pattern;
-import org.apache.commons.lang3.StringUtils;
 
+/**
+ * Used for operations on date/date-time strings to convert it to the specific connector's format
+ * Only for test purposes.
+ */
 public class DateTimeUtils {
 
-  public static final String DATE_TIME_FORMAT_PATTERN = "yyyy-MM-dd'T'HH:mm:ss'Z'";
-  public static final String DATE_FORMAT_PATTERN = "yyyy-MM-dd";
-  public static final DateFormat DATE_TIME_FORMAT = new SimpleDateFormat(DATE_TIME_FORMAT_PATTERN);
-  public static final DateFormat DATE_FORMAT = new SimpleDateFormat(DATE_FORMAT_PATTERN);
+  public static final String DATE_TIME_FORMAT_PATTERN = "yyyy-MM-dd'T'HH:mm:ss.SSSSSS'Z'";
 
   public static final String DATE_TIME = "date-time";
   public static final String DATE = "date";
 
   public static final Pattern MILLISECONDS_PATTERN = Pattern.compile("\\.\\d*");
 
-  private static final String T_SEPARATOR = "T";
-  private static final String ZERO_TIMEZONE = "Z";
   private static final DateTimeFormatter FORMATTER =
       DateTimeFormatter.ofPattern(
           "[yyyy][yy]['-']['/']['.'][' '][MMM][MM][M]['-']['/']['.'][' '][dd][d]" +
               "[[' ']['T']HH:mm[':'ss[.][SSSSSS][SSSSS][SSSS][SSS][' '][z][zzz][Z][O][x][XXX][XX][X]]]");
 
   /**
-   * Parse the Json date-time logical type to an Avro long value.
+   * Parse the Json date-time logical type to long value of epoch microseconds.
    *
    * @return the number of microseconds from the unix epoch, 1 January 1970 00:00:00.000000 UTC.
    */
@@ -49,7 +44,7 @@ public class DateTimeUtils {
   }
 
   /**
-   * Parse the Json date logical type to an Avro int.
+   * Parse the Json date logical type to int value of epoch day.
    *
    * @return the number of days from the unix epoch, 1 January 1970 (ISO calendar).
    */
@@ -65,6 +60,7 @@ public class DateTimeUtils {
   }
 
   /**
+   * Parse the Json date-time type to bigquery-denormalized specific format.
    *
    * @param data "2021-01-03T01:01:01.544+01:00"
    * @return converted data "2021-01-03T01:01:01.544000"
@@ -86,6 +82,7 @@ public class DateTimeUtils {
   }
 
   /**
+   * Parse the Json date-time type to snowflake specific format
    *
    * @param jsonDateTime e.g. "2021-01-03T01:01:01.544+01:00"
    * @return converted data e.g. "2021-01-03T00:01:02Z"
@@ -96,11 +93,12 @@ public class DateTimeUtils {
       ZonedDateTime zdt = ZonedDateTime.parse(jsonDateTime, FORMATTER);
       instant = zdt.toLocalDateTime().atZone(ZoneId.systemDefault()).toLocalDateTime()
           .toInstant(ZoneOffset.of(zdt.getOffset().toString()));
-      return DATE_TIME_FORMAT.format(new Date(instant.toEpochMilli() + instant.get(ChronoField.MILLI_OF_SECOND)));
+      return DateTimeFormatter.ofPattern(DATE_TIME_FORMAT_PATTERN).withZone(ZoneOffset.UTC).format(instant);
     } catch (DateTimeParseException e) {
       try {
         LocalDateTime dt = LocalDateTime.parse(jsonDateTime, FORMATTER);
-        instant = dt.minusNanos(dt.getNano()).toInstant(ZoneOffset.ofHours(-8));
+        instant = dt.toInstant(ZoneOffset.ofHours(-8));
+        return DateTimeFormatter.ofPattern(DATE_TIME_FORMAT_PATTERN).withZone(ZoneOffset.UTC).format(instant);
       } catch (DateTimeParseException ex) {
         // no logging since it may generate too much noise
       }
@@ -109,6 +107,7 @@ public class DateTimeUtils {
   }
 
   /**
+   * Parse the Json date-time type to Redshift specific format
    *
    * @param jsonDateTime e.g. "2021-01-03T01:01:01.544+01:00"
    * @return converted data e.g. "2021-01-03 00:01:01.544000+00"
@@ -118,6 +117,7 @@ public class DateTimeUtils {
   }
 
   /**
+   * Parse the Json date-time type to postgres specific format
    *
    * @param jsonDateTime e.g. "2021-01-03T01:01:01.544+01:00"
    * @return converted data e.g. "2021-01-03T00:01:01.544Z"
@@ -127,6 +127,7 @@ public class DateTimeUtils {
   }
 
   /**
+   * Parse the Json date-time type to databricks specific format
    *
    * @param jsonDateTime e.g. "2021-01-03T01:01:01.544+01:00"
    * @return converted data "{\"member0\":2021-01-03 00:01:01.544,\"member1\":null}"
@@ -136,6 +137,7 @@ public class DateTimeUtils {
   }
 
   /**
+   * Parse the Json date-time type to MSSQL specific format
    *
    * @param jsonDateTime e.g. "2021-01-03T01:01:01.544+01:00"
    * @return converted data "2021-01-03 00:01:01.544"
@@ -145,23 +147,25 @@ public class DateTimeUtils {
   }
 
   /**
+   * Parse the Json date type to date-time format with zero values for time
    *
    * @param jsonDate e.g. "2021-01-01"
    * @return converted data "2021-01-01T00:00:00Z"
    */
   public static String convertToDateFormatWithZeroTime(String jsonDate) {
-    return convertDate(jsonDate, date -> DATE_TIME_FORMAT.format(
-        Date.from(date.atStartOfDay().atZone(ZoneId.systemDefault()).toInstant())));
+    return convertDate(jsonDate, date -> DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss'Z'")
+        .format(date.atStartOfDay().atZone(ZoneId.systemDefault())));
   }
 
   /**
+   * Parse the Json date type to general ISO date format
    *
    * @param jsonDate e.g. "2021-1-1"
    * @return converted data "2021-01-01"
    */
   public static String convertToDateFormat(String jsonDate) {
-    return convertDate(jsonDate, date -> DATE_FORMAT.format(
-        Date.from(date.atStartOfDay().atZone(ZoneId.systemDefault()).toInstant())));
+    return convertDate(jsonDate, date -> DateTimeFormatter.ISO_LOCAL_DATE.format(
+        date.atStartOfDay().atZone(ZoneId.systemDefault())));
   }
 
   private static <T> T convertDateTime(String jsonDateTime, Function<Instant, T> dateTimeFormatter) {
@@ -192,26 +196,28 @@ public class DateTimeUtils {
   }
 
   private static String toMSSQLDateFormat(Instant instant) {
-    String str = MILLISECONDS_PATTERN.matcher(instant.toString().replace(T_SEPARATOR, StringUtils.SPACE)
-        .replace(ZERO_TIMEZONE, StringUtils.EMPTY))
-        .replaceAll("." + (instant.get(ChronoField.MILLI_OF_SECOND) == 0 ? "0"
-            : (instant.get(ChronoField.MICRO_OF_SECOND) + 500) / 1000));
-    return instant.get(ChronoField.MILLI_OF_SECOND) > 0 ? str : str + ".0";
+    return DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS").withZone(ZoneOffset.UTC).format(instant);
   }
 
   private static String toRedshiftDateFormat(Instant instant) {
-    return MILLISECONDS_PATTERN.matcher(instant.toString().replace(T_SEPARATOR, StringUtils.SPACE).replace(ZERO_TIMEZONE, "+00"))
-        .replaceAll("." + instant.get(ChronoField.MICRO_OF_SECOND));
+    if (instant.get(ChronoField.MILLI_OF_SECOND) == 0) {
+      return DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss'+00'").withZone(ZoneOffset.UTC).format(instant);
+    } else {
+      return DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSSSSS'+00'").withZone(ZoneOffset.UTC).format(instant);
+    }
   }
 
   private static String toBigqueryDenormalizedDateFormat(Instant instant) {
-    return MILLISECONDS_PATTERN.matcher(instant.toString().replace(ZERO_TIMEZONE, StringUtils.EMPTY))
-        .replaceAll("." + (instant.getNano() / 1000000) * 1000);
+    if (instant.get(ChronoField.MILLI_OF_SECOND) == 0) {
+      return DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss").withZone(ZoneOffset.UTC).format(instant);
+    } else {
+      String formattedDateTime = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSSSSS").withZone(ZoneOffset.UTC).format(instant);
+      return MILLISECONDS_PATTERN.matcher(formattedDateTime).replaceAll("." + (instant.getNano() / 1000000) * 1000);
+    }
   }
 
   private static String toDatabricksDateFormat(Instant instant) {
-    return String.format("***\"member0\":%s,\"member1\":null***", MILLISECONDS_PATTERN.matcher(instant.toString().replace('T', ' ').replace("Z", ""))
-        .replaceAll("." + instant.get(ChronoField.MICRO_OF_SECOND) / 1000));
+    return DateTimeFormatter.ofPattern("'***\"member0\":'yyyy-MM-dd HH:mm:ss.SSS',\"member1\":null***'").withZone(ZoneOffset.UTC).format(instant);
   }
 
 }
