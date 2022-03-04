0cb5dbe2a09a7e18e726fcaf3359f7a5da9aa453
Alexandre Girard <alexandre@airbyte.io>
2022-03-04 11:29:24 -0800

    gradlew format

--- airbyte-integrations/connectors/destination-scaffold-destination-python/destination_scaffold_destination_python/destination.py
+++ airbyte-integrations/connectors/destination-scaffold-destination-python/destination_scaffold_destination_python/destination.py
@@ -12,10 +12,7 @@ from airbyte_cdk.models import AirbyteConnectionStatus, AirbyteMessage, Configur
 
 class DestinationScaffoldDestinationPython(Destination):
     def write(
-        self,
-        config: Mapping[str, Any],
-        configured_catalog: ConfiguredAirbyteCatalog,
-        input_messages: Iterable[AirbyteMessage]
+        self, config: Mapping[str, Any], configured_catalog: ConfiguredAirbyteCatalog, input_messages: Iterable[AirbyteMessage]
     ) -> Iterable[AirbyteMessage]:
 
         """
