aa86b12faabcab09cad7f3931acc4c53ce4a4ffb
alafanechere <augustin.lafanechere@gmail.com>
2022-03-05 18:03:14 +0100

    fix exclusion of test in packaging

--- octavia-cli/setup.py
+++ octavia-cli/setup.py
@@ -40,7 +40,7 @@ setup(
         "Source": "https://github.com/airbytehq/airbyte",
         "Tracker": "https://github.com/airbytehq/airbyte/issues",
     },
-    packages=find_packages(exclude=("unit_tests", "docs")),
+    packages=find_packages(exclude=("tests", "docs")),
     package_data={"octavia_cli.generate": ["templates/*.j2"]},
     install_requires=[
         "click~=8.0.3",
