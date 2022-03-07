3f7bd1d6c3e522b58b8c38fc305173b898709795
Oleksandr Bazarnov <oleksandr.bazarnov@globallogic.com>
2022-03-07 12:25:13 +0200

    edited unit_test

--- airbyte-integrations/connectors/source-shopify/unit_tests/test_cached_stream_state.py
+++ airbyte-integrations/connectors/source-shopify/unit_tests/test_cached_stream_state.py
@@ -8,16 +8,16 @@ from source_shopify.source import OrderRefunds, Orders
 from source_shopify.utils import EagerlyCachedStreamState as stream_state_cache
 
 # Define the Stream instances for the tests
-PARENT_STREAM = Orders(config={"authenticator": None})
-CHILD_STREAM = OrderRefunds(config={"authenticator": None})
+SHOPIFY_STREAM = Orders(config={"authenticator": None})
+SHOPIFY_SUB_STREAM = OrderRefunds(config={"authenticator": None})
 
 
 @pytest.mark.parametrize(
     "stream, cur_stream_state, state_object, expected_output",
     [
         # When Full-Refresh: state_object: empty.
-        (PARENT_STREAM, {PARENT_STREAM.cursor_field: ""}, {}, {PARENT_STREAM.name: {PARENT_STREAM.cursor_field: ""}}),
-        (CHILD_STREAM, {CHILD_STREAM.cursor_field: ""}, {}, {CHILD_STREAM.name: {CHILD_STREAM.cursor_field: ""}}),
+        (SHOPIFY_STREAM, {SHOPIFY_STREAM.cursor_field: ""}, {}, {SHOPIFY_STREAM.name: {SHOPIFY_STREAM.cursor_field: ""}}),
+        (SHOPIFY_SUB_STREAM, {SHOPIFY_SUB_STREAM.cursor_field: ""}, {}, {SHOPIFY_SUB_STREAM.name: {SHOPIFY_SUB_STREAM.cursor_field: ""}}),
     ],
     ids=["Sync Started. Parent.", "Sync Started. Child."],
 )
@@ -37,29 +37,29 @@ def test_full_refresh(stream, cur_stream_state, state_object, expected_output):
     [
         # When start the incremental refresh, assuming we have the state of STREAM.
         (
-            PARENT_STREAM,
-            {PARENT_STREAM.cursor_field: "2021-01-01T01-01-01"},
+            SHOPIFY_STREAM,
+            {SHOPIFY_STREAM.cursor_field: "2021-01-01T01-01-01"},
             {},
-            {PARENT_STREAM.name: {PARENT_STREAM.cursor_field: "2021-01-01T01-01-01"}},
+            {SHOPIFY_STREAM.name: {SHOPIFY_STREAM.cursor_field: "2021-01-01T01-01-01"}},
         ),
         (
-            CHILD_STREAM,
-            {CHILD_STREAM.cursor_field: "2021-01-01T01-01-01"},
+            SHOPIFY_SUB_STREAM,
+            {SHOPIFY_SUB_STREAM.cursor_field: "2021-01-01T01-01-01"},
             {},
-            {CHILD_STREAM.name: {CHILD_STREAM.cursor_field: "2021-01-01T01-01-01"}},
+            {SHOPIFY_SUB_STREAM.name: {SHOPIFY_SUB_STREAM.cursor_field: "2021-01-01T01-01-01"}},
         ),
         # While doing the incremental refresh, we keeping the original state, even if the state is updated during the sync.
         (
-            PARENT_STREAM,
-            {PARENT_STREAM.cursor_field: "2021-01-05T02-02-02"},
+            SHOPIFY_STREAM,
+            {SHOPIFY_STREAM.cursor_field: "2021-01-05T02-02-02"},
             {},
-            {PARENT_STREAM.name: {PARENT_STREAM.cursor_field: "2021-01-05T02-02-02"}},
+            {SHOPIFY_STREAM.name: {SHOPIFY_STREAM.cursor_field: "2021-01-05T02-02-02"}},
         ),
         (
-            CHILD_STREAM,
-            {CHILD_STREAM.cursor_field: "2021-01-05T02-02-02"},
+            SHOPIFY_SUB_STREAM,
+            {SHOPIFY_SUB_STREAM.cursor_field: "2021-01-05T02-02-02"},
             {},
-            {CHILD_STREAM.name: {CHILD_STREAM.cursor_field: "2021-01-05T02-02-02"}},
+            {SHOPIFY_SUB_STREAM.name: {SHOPIFY_SUB_STREAM.cursor_field: "2021-01-05T02-02-02"}},
         ),
     ],
     ids=["Sync Started. Parent", "Sync Started. Child", "Sync in progress. Parent", "Sync in progress. Child"],
