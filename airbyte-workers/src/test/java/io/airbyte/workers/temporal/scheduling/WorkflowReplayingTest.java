cc8c0d2e36c4d0e0fb6e33169fed7d5eb7461651
Benoit Moriceau <benoit@airbyte.io>
2022-03-08 15:10:23 -0800

    Add workflow replayer

---
+++ airbyte-workers/src/test/java/io/airbyte/workers/temporal/scheduling/WorkflowReplayingTest.java
@@ -0,0 +1,19 @@
+package io.airbyte.workers.temporal.scheduling;
+
+import io.temporal.testing.WorkflowReplayer;
+import java.io.File;
+import java.net.URL;
+import org.junit.jupiter.api.Test;
+
+// TODO: Auto generation of the input and more scenario coverage
+public class WorkflowReplayingTest {
+
+  @Test
+  public void replaySimpleSuccessfulWorkflow() throws Exception {
+    final URL historyPath = getClass().getClassLoader().getResource("workflowHistory.json");
+
+    final File historyFile = new File(historyPath.toURI());
+
+    WorkflowReplayer.replayWorkflowExecution(historyFile, ConnectionManagerWorkflowImpl.class);
+  }
+}
