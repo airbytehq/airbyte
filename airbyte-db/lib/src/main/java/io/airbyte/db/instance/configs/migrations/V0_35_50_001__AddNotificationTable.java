94c142a3134b23a35f09c3f85d585b11e6d62b71
marcosmarxm <marcosmarxm@gmail.com>
2022-03-09 18:55:10 -0300

    add notification_connection table in migration file

--- airbyte-db/lib/src/main/java/io/airbyte/db/instance/configs/migrations/V0_35_50_001__AddNotificationTable.java
+++ airbyte-db/lib/src/main/java/io/airbyte/db/instance/configs/migrations/V0_35_50_001__AddNotificationTable.java
@@ -4,7 +4,13 @@
 
 package io.airbyte.db.instance.configs.migrations;
 
+import java.util.List;
 import java.util.UUID;
+
+import com.google.common.annotations.VisibleForTesting;
+import io.airbyte.config.ConfigSchema;
+import io.airbyte.config.ConfigWithMetadata;
+import io.airbyte.config.StandardWorkspace;
 import org.flywaydb.core.api.migration.BaseJavaMigration;
 import org.flywaydb.core.api.migration.Context;
 import org.jooq.DSLContext;
@@ -20,16 +26,40 @@ public class V0_35_50_001__AddNotificationTable extends BaseJavaMigration {
 
   @Override
   public void migrate(final Context context) throws Exception {
-    LOGGER.info("Running migration: {}", this.getClass().getSimpleName());
-
     final DSLContext ctx = DSL.using(context.getConnection());
+    migrate(ctx);
+  }
 
-    final Field<UUID> id = DSL.field("id", SQLDataType.UUID.nullable(true));
-    final Field<String> name = DSL.field("name", SQLDataType.VARCHAR(256).nullable(true));
+  @VisibleForTesting
+  public static void migrate(final DSLContext ctx) {
+    createNotificationConfigTable(ctx);
+    createNotificationConnectionTable(ctx);
+  }
+
+  public static void createNotificationConfigTable(final DSLContext ctx) {
+
+
+    final Field<UUID> id = DSL.field("id", SQLDataType.UUID.nullable(false));
+    final Field<String> name = DSL.field("name", SQLDataType.VARCHAR(256).nullable(false));
+    final Field<String> webhook = DSL.field("webhook", SQLDataType.VARCHAR(516).nullable(false));
 
     ctx.createTableIfNotExists("notification_config")
-        .columns(id, name)
+        .columns(id, name, webhook)
         .execute();
+    LOGGER.info("workspace table created");
+  }
+
+  public static void createNotificationConnectionTable(final DSLContext ctx) {
+
+    final Field<UUID> connectionId = DSL.field("connection_id", SQLDataType.UUID.nullable(true));
+    final Field<UUID> notificationId = DSL.field("notification_id", SQLDataType.UUID.nullable(true));
+    final Field<Boolean> onSuccess = DSL.field("on_success", SQLDataType.BOOLEAN.nullable(false));
+    final Field<Boolean> onFailure = DSL.field("on_failure", SQLDataType.BOOLEAN.nullable(false));
+
+
+    ctx.createTableIfNotExists("notification_connection")
+            .columns(connectionId, notificationId, onSuccess, onFailure)
+            .execute();
   }
 
 }
