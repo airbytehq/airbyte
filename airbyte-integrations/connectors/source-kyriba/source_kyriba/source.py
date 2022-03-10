13fd01e27285af158076579a46e740a97677473e
Will Watkinson <wjwatkinson@gmail.com>
2022-03-10 17:31:19 -0500

    added account balances

--- airbyte-integrations/connectors/source-kyriba/source_kyriba/source.py
+++ airbyte-integrations/connectors/source-kyriba/source_kyriba/source.py
@@ -6,10 +6,13 @@ from abc import ABC
 from typing import Any, Iterable, List, Mapping, MutableMapping, Optional, Tuple
 
 import requests
+import uuid
+from datetime import timedelta, date
 from airbyte_cdk.sources import AbstractSource
 from airbyte_cdk.sources.streams import Stream
-from airbyte_cdk.sources.streams.http import HttpStream
+from airbyte_cdk.sources.streams.http import HttpStream, HttpSubStream
 from airbyte_cdk.sources.streams.http.auth import TokenAuthenticator
+from airbyte_cdk.models import SyncMode
 
 class KyribaClient:
     def __init__(self, username: str, password: str, gateway_url: str):
@@ -90,7 +93,7 @@ class IncrementalKyribaStream(KyribaStream, ABC):
         params = { "sort": self.cursor_field }
         latest_cursor = stream_state.get(self.cursor_field) or self.start_date + " 00:00:00"
         if latest_cursor:
-            # the Kyriba datetime output contains T and Z, but the input has a space and no time zone
+            # the Kyriba atetime output contains T and Z, but the input has a space and no time zone
             fmt_cursor = latest_cursor.replace("T", " ").replace("Z", "")
             filter = f"{self.cursor_field}=gt='{fmt_cursor}'"
             params["filter"] = filter
@@ -102,6 +105,74 @@ class IncrementalKyribaStream(KyribaStream, ABC):
 class Accounts(KyribaStream):
     def path(self, **kwargs) -> str:
         return "accounts"
+
+
+#class AccountSubStream(HttpSubStream, IncrementalKyribaStream):
+#    def __init__(self, **kwargs):
+#        super().__init__(Accounts, **kwargs)
+#
+#    def stream_slices(self, **kwargs) -> Iterable[Optional[Mapping[str, Any]]]:
+#        accounts = self.parent.read_records()
+#        [{"uuid": a["uuid"]} for a in accounts]
+
+
+class CashBalances(HttpSubStream, IncrementalKyribaStream):
+    def __init__(self, **kwargs):
+        super().__init__(Accounts, **kwargs)
+        self.parent = Accounts(**kwargs)
+
+    cursor_field = "date"
+
+    # Checkpoint stream reads after N records. This prevents re-reading of data if the stream fails for any reason.
+    @property
+    def state_checkpoint_interval(self) -> int:
+        return 100
+
+    def get_updated_state(self, current_stream_state: MutableMapping[str, Any], latest_record: Mapping[str, Any]) -> Mapping[str, Any]:
+        latest_cursor = latest_record["cashBalance"][-1]["balanceDate"].get(self.cursor_field) or ""
+        current_cursor = current_stream_state.get(self.cursor_field) or ""
+        return {self.cursor_field: max(current_cursor, latest_cursor)}
+
+    def stream_slices(self, stream_state: Mapping[str, Any], **kwargs) -> Iterable[Optional[Mapping[str, Any]]]:
+        slices = []
+        account_uuids = [a["uuid"] for a in self.parent.read_records(sync_mode = SyncMode.full_refresh)]
+        # we can query a max of 31 days at a time
+        days_inc = 31
+        start_str = stream_state.get("date") or self.start_date
+        start_date = date.fromisoformat(start_str)
+        while start_date <= date.today():
+            end_date = start_date + timedelta(days=days_inc)
+            date_params = {
+                "startDate": start_date.isoformat(),
+                "endDate": end_date.isoformat(),
+            }
+            slices.extend([{"uuid": uuid, **date_params} for uuid in account_uuids])
+            start_date = end_date + timedelta(days=1)
+            #raise BaseException(start_date <= date.today())
+        return slices
+
+    def path(self, stream_slice: Mapping[str, Any], **kwargs) -> str:
+        account_uuid = stream_slice['uuid']
+        return f"cash-balances/accounts/{account_uuid}/balances"
+
+    def request_params(
+        self, stream_state: Mapping[str, Any], stream_slice: Mapping[str, any] = None, next_page_token: Mapping[str, Any] = None
+    ) -> MutableMapping[str, Any]:
+        return {
+            "endDate": stream_slice["endDate"],
+            "startDate": stream_slice["startDate"],
+            "intraday": True,
+            "actual": True,
+            "estimatedForecasts": True,
+            "confirmedForecasts": True,
+            "dateType": "VALUE",
+        }
+
+    def next_page_token(self, response: requests.Response):
+        pass
+
+    def parse_response(self, response: requests.Response, **kwargs) -> Iterable[Mapping]:
+        return [response.json()]
 
 
 class CashFlows(IncrementalKyribaStream):
@@ -140,4 +211,4 @@ class SourceKyriba(AbstractSource):
             "client": client,
             "start_date": config.get("start_date"),
         }
-        return [Accounts(**kwargs), CashFlows(**kwargs)]
+        return [Accounts(**kwargs), CashFlows(**kwargs), CashBalances(**kwargs)]
