d19754c169f5188c3926492d1ddf7b595c6d1624
Serhii Chvaliuk <grubberr@gmail.com>
2022-03-10 18:02:51 +0200

    🐛 Source Github: Fix error handling for 404 streams (#10878)
    * Fix, no error if "Projects" feature is disabled
    * improve requests.codes.NOT_FOUND
    
    Signed-off-by: Sergey Chvalyuk <grubberr@gmail.com>

--- airbyte-integrations/connectors/source-github/unit_tests/test_stream.py
+++ airbyte-integrations/connectors/source-github/unit_tests/test_stream.py
@@ -6,9 +6,12 @@ from http import HTTPStatus
 from unittest.mock import MagicMock, patch
 
 import pytest
+import requests
 import responses
 from airbyte_cdk.sources.streams.http.exceptions import BaseBackoffException
-from source_github.streams import PullRequestCommentReactions
+from source_github.streams import Projects, PullRequestCommentReactions, Repositories, Teams
+
+from .utils import read_full_refresh
 
 DEFAULT_BACKOFF_DELAYS = [5, 10, 20, 40, 80]
 
@@ -47,3 +50,54 @@ def test_backoff_time(http_status, response_text, expected_backoff_time):
     args = {"authenticator": None, "repositories": ["test_repo"], "start_date": "start_date", "page_size_for_large_streams": 30}
     stream = PullRequestCommentReactions(**args)
     assert stream.backoff_time(response_mock) == expected_backoff_time
+
+
+@responses.activate
+def test_stream_teams_404():
+    kwargs = {"organizations": ["org_name"]}
+    stream = Teams(**kwargs)
+
+    responses.add(
+        "GET",
+        "https://api.github.com/orgs/org_name/teams",
+        status=requests.codes.NOT_FOUND,
+        json={"message": "Not Found", "documentation_url": "https://docs.github.com/rest/reference/teams#list-teams"},
+    )
+
+    assert read_full_refresh(stream) == []
+    assert len(responses.calls) == 1
+    assert responses.calls[0].request.url == "https://api.github.com/orgs/org_name/teams?per_page=100"
+
+
+@responses.activate
+def test_stream_repositories_404():
+    kwargs = {"organizations": ["org_name"]}
+    stream = Repositories(**kwargs)
+
+    responses.add(
+        "GET",
+        "https://api.github.com/orgs/org_name/repos",
+        status=requests.codes.NOT_FOUND,
+        json={"message": "Not Found", "documentation_url": "https://docs.github.com/rest/reference/repos#list-organization-repositories"},
+    )
+
+    assert read_full_refresh(stream) == []
+    assert len(responses.calls) == 1
+    assert responses.calls[0].request.url == "https://api.github.com/orgs/org_name/repos?per_page=100"
+
+
+@responses.activate
+def test_stream_projects_disabled():
+    kwargs = {"start_date": "start_date", "page_size_for_large_streams": 30, "repositories": ["test_repo"]}
+    stream = Projects(**kwargs)
+
+    responses.add(
+        "GET",
+        "https://api.github.com/repos/test_repo/projects",
+        status=requests.codes.GONE,
+        json={"message": "Projects are disabled for this repository", "documentation_url": "https://docs.github.com/v3/projects"},
+    )
+
+    assert read_full_refresh(stream) == []
+    assert len(responses.calls) == 1
+    assert responses.calls[0].request.url == "https://api.github.com/repos/test_repo/projects?per_page=100&state=all"
