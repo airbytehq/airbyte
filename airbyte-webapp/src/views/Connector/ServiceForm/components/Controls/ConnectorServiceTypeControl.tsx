7ee88e0240bc6d66a1b85c91c5686aee1be4b210
Alexandre Girard <alexandre@airbyte.io>
2022-03-11 10:50:33 -0800

    let

--- airbyte-webapp/src/views/Connector/ServiceForm/components/Controls/ConnectorServiceTypeControl.tsx
+++ airbyte-webapp/src/views/Connector/ServiceForm/components/Controls/ConnectorServiceTypeControl.tsx
@@ -176,9 +176,9 @@ const ConnectorServiceTypeControl: React.FC<{
   // This way, they will not be available for usage in new connections, but they will be available for users
   // already leveraging them.
   // TODO End hack
-  var disallowedOauthConnectors =
+  let disallowedOauthConnectors =
     // I would prefer to use windowConfigProvider.cloud but that function is async
-    window.CLOUD === "true"
+    window.CLOUD !== "false"
       ? [
           "200330b2-ea62-4d11-ac6d-cfe3e3f8ab2b", // Snapchat
           "2470e835-feaf-4db6-96f3-70fd645acc77", // Salesforce Singer
