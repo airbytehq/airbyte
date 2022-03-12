e173b1c6fc0d7a4bc4f261db4043a3225c5658f8
Sergey Chvalyuk <grubberr@gmail.com>
2022-03-12 08:28:50 +0200

    test_get_branches_data added
    Signed-off-by: Sergey Chvalyuk <grubberr@gmail.com>

--- airbyte-integrations/connectors/source-github/unit_tests/test_source.py
+++ airbyte-integrations/connectors/source-github/unit_tests/test_source.py
@@ -52,3 +52,42 @@ def test_check_connection_org_only():
     assert status.status == Status.SUCCEEDED
     # One request to check organization
     assert len(responses.calls) == 1
+
+
+@responses.activate
+def test_get_branches_data():
+
+    repository_args = {"repositories": ["airbytehq/integration-test"], "page_size_for_large_streams": 10}
+
+    source = SourceGithub()
+
+    responses.add(
+        "GET",
+        "https://api.github.com/repos/airbytehq/integration-test",
+        json={"full_name": "airbytehq/integration-test", "default_branch": "master"},
+    )
+
+    responses.add(
+        "GET",
+        "https://api.github.com/repos/airbytehq/integration-test/branches",
+        json=[
+            {"repository": "airbytehq/integration-test", "name": "feature/branch_0"},
+            {"repository": "airbytehq/integration-test", "name": "feature/branch_1"},
+            {"repository": "airbytehq/integration-test", "name": "feature/branch_2"},
+            {"repository": "airbytehq/integration-test", "name": "master"},
+        ],
+    )
+
+    default_branches, branches_to_pull = source._get_branches_data("", repository_args)
+    assert default_branches == {"airbytehq/integration-test": "master"}
+    assert branches_to_pull == {"airbytehq/integration-test": ["master"]}
+
+    default_branches, branches_to_pull = source._get_branches_data(
+        "airbytehq/integration-test/feature/branch_0 airbytehq/integration-test/feature/branch_1 airbytehq/integration-test/feature/branch_3",
+        repository_args,
+    )
+
+    assert default_branches == {"airbytehq/integration-test": "master"}
+    assert len(branches_to_pull["airbytehq/integration-test"]) == 2
+    assert "feature/branch_0" in branches_to_pull["airbytehq/integration-test"]
+    assert "feature/branch_1" in branches_to_pull["airbytehq/integration-test"]
