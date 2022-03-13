ff80d2d99006ad621a6b40bbe9617cf4d8d99f1a
Davin Chia <davinchia@gmail.com>
2022-03-13 11:48:56 +0800

    Reporter App Monitoring. (#11074)
    Add a metric to monitor the monitoring app's rate of publishing metrics. Though this isn't perfect, it gives us some insight into whether metric publishing is okay or running into issues.

--- airbyte-metrics/reporter/src/main/java/io/airbyte/metrics/reporter/ToEmit.java
+++ airbyte-metrics/reporter/src/main/java/io/airbyte/metrics/reporter/ToEmit.java
@@ -4,7 +4,7 @@
 
 package io.airbyte.metrics.reporter;
 
-import io.airbyte.commons.lang.Exceptions;
+import io.airbyte.commons.lang.Exceptions.Procedure;
 import io.airbyte.db.instance.jobs.jooq.enums.JobStatus;
 import io.airbyte.metrics.lib.DogStatsDMetricSingleton;
 import io.airbyte.metrics.lib.MetricQueries;
@@ -12,37 +12,39 @@ import io.airbyte.metrics.lib.MetricTags;
 import io.airbyte.metrics.lib.MetricsRegistry;
 import java.util.concurrent.TimeUnit;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.tuple.Pair;
 
 /**
  * This class contains all metrics emitted by the {@link ReporterApp}.
  */
+@Slf4j
 @AllArgsConstructor
 public enum ToEmit {
 
-  NUM_PENDING_JOBS(Exceptions.toSwallowExceptionRunnable(() -> {
+  NUM_PENDING_JOBS(countMetricEmission(() -> {
     final var pendingJobs = ReporterApp.configDatabase.query(MetricQueries::numberOfPendingJobs);
     DogStatsDMetricSingleton.gauge(MetricsRegistry.NUM_PENDING_JOBS, pendingJobs);
   })),
-  NUM_RUNNING_JOBS(Exceptions.toSwallowExceptionRunnable(() -> {
+  NUM_RUNNING_JOBS(countMetricEmission(() -> {
     final var runningJobs = ReporterApp.configDatabase.query(MetricQueries::numberOfRunningJobs);
     DogStatsDMetricSingleton.gauge(MetricsRegistry.NUM_RUNNING_JOBS, runningJobs);
   })),
-  OLDEST_RUNNING_JOB_AGE_SECS(Exceptions.toSwallowExceptionRunnable(() -> {
+  OLDEST_RUNNING_JOB_AGE_SECS(countMetricEmission(() -> {
     final var age = ReporterApp.configDatabase.query(MetricQueries::oldestRunningJobAgeSecs);
     DogStatsDMetricSingleton.gauge(MetricsRegistry.OLDEST_RUNNING_JOB_AGE_SECS, age);
   })),
-  OLDEST_PENDING_JOB_AGE_SECS(Exceptions.toSwallowExceptionRunnable(() -> {
+  OLDEST_PENDING_JOB_AGE_SECS(countMetricEmission(() -> {
     final var age = ReporterApp.configDatabase.query(MetricQueries::oldestPendingJobAgeSecs);
     DogStatsDMetricSingleton.gauge(MetricsRegistry.OLDEST_PENDING_JOB_AGE_SECS, age);
   })),
-  NUM_ACTIVE_CONN_PER_WORKSPACE(Exceptions.toSwallowExceptionRunnable(() -> {
+  NUM_ACTIVE_CONN_PER_WORKSPACE(countMetricEmission(() -> {
     final var age = ReporterApp.configDatabase.query(MetricQueries::numberOfActiveConnPerWorkspace);
     for (long count : age) {
       DogStatsDMetricSingleton.percentile(MetricsRegistry.NUM_ACTIVE_CONN_PER_WORKSPACE, count);
     }
   })),
-  OVERALL_JOB_RUNTIME_IN_LAST_HOUR_BY_TERMINAL_STATE_SECS(Exceptions.toSwallowExceptionRunnable(() -> {
+  OVERALL_JOB_RUNTIME_IN_LAST_HOUR_BY_TERMINAL_STATE_SECS(countMetricEmission(() -> {
     final var times = ReporterApp.configDatabase.query(MetricQueries::overallJobRuntimeForTerminalJobsInLastHour);
     for (Pair<JobStatus, Double> pair : times) {
       DogStatsDMetricSingleton.recordTimeGlobal(
@@ -57,6 +59,24 @@ public enum ToEmit {
 
   ToEmit(Runnable toEmit) {
     this(toEmit, 15, TimeUnit.SECONDS);
+  }
+
+  /**
+   * Wrapper callable to handle 1) query exception logging and 2) counting metric emissions so
+   * reporter app can be monitored too.
+   *
+   * @param metricQuery
+   * @return
+   */
+  private static Runnable countMetricEmission(Procedure metricQuery) {
+    return () -> {
+      try {
+        metricQuery.call();
+        DogStatsDMetricSingleton.count(MetricsRegistry.EST_NUM_METRICS_EMITTED_BY_REPORTER, 1);
+      } catch (Exception e) {
+        log.error("Exception querying database for metric: ", e);
+      }
+    };
   }
 
 }
