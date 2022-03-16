671051c151bed1b9fbeeaa4d9e8fc08deaa0fa1a
Alexandre Girard <alexandre@airbyte.io>
2022-03-16 08:52:32 -0700

    use my workspace id

--- airbyte-webapp/src/views/Connector/ServiceForm/components/Controls/ConnectorServiceTypeControl.tsx
+++ airbyte-webapp/src/views/Connector/ServiceForm/components/Controls/ConnectorServiceTypeControl.tsx
@@ -190,7 +190,7 @@ const ConnectorServiceTypeControl: React.FC<{
       ? [
           "200330b2-ea62-4d11-ac6d-cfe3e3f8ab2b", // Snapchat
           "2470e835-feaf-4db6-96f3-70fd645acc77", // Salesforce Singer
-          ...(workspace.workspaceId !== "54135667-ce73-4820-a93c-29fe1510d348"
+          ...(workspace.workspaceId !== "8fda1978-22bc-466c-a9d5-eaf18bb705a9"
             ? ["9da77001-af33-4bcd-be46-6252bf9342b9"]
             : []), // Shopify
         ]
