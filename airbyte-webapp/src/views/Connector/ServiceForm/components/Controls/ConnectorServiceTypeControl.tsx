1c53886256f026ec059e1e182526e214c14bd444
Alexandre Girard <alexandre@airbyte.io>
2022-03-16 08:51:03 -0700

    Simpler approach

--- airbyte-webapp/src/views/Connector/ServiceForm/components/Controls/ConnectorServiceTypeControl.tsx
+++ airbyte-webapp/src/views/Connector/ServiceForm/components/Controls/ConnectorServiceTypeControl.tsx
@@ -185,19 +185,16 @@ const ConnectorServiceTypeControl: React.FC<{
   // already leveraging them.
   // TODO End hack
   const workspace = useCurrentWorkspace();
-  const disallowedOauthConnectors = ([] as string[]).concat(
-    ...// I would prefer to use windowConfigProvider.cloud but that function is async
-    (window.CLOUD === "true"
+  const disallowedOauthConnectors =
+    window.CLOUD === "true"
       ? [
-          ["200330b2-ea62-4d11-ac6d-cfe3e3f8ab2b"], // Snapchat
-          ["2470e835-feaf-4db6-96f3-70fd645acc77"], // Salesforce Singer
-          // TODO Issue 10817: we need to enable shopify ONLY for this workspace so until Shopify gives us approval to use oauth
-          workspace.workspaceId !== "54135667-ce73-4820-a93c-29fe1510d348" // Shopify workspace
-            ? ["9da77001-af33-4bcd-be46-6252bf9342b9"] // Shopify
-            : [],
+          "200330b2-ea62-4d11-ac6d-cfe3e3f8ab2b", // Snapchat
+          "2470e835-feaf-4db6-96f3-70fd645acc77", // Salesforce Singer
+          ...(workspace.workspaceId !== "54135667-ce73-4820-a93c-29fe1510d348"
+            ? ["9da77001-af33-4bcd-be46-6252bf9342b9"]
+            : []), // Shopify
         ]
-      : [])
-  );
+      : [];
   const sortedDropDownData = useMemo(
     () =>
       availableServices
