38ec1d54820dca256818480323a777745840f4dc
Alexandre Girard <alexandre@airbyte.io>
2022-03-16 16:54:45 -0700

    comments

--- airbyte-webapp/src/views/Connector/ServiceForm/components/Controls/ConnectorServiceTypeControl.tsx
+++ airbyte-webapp/src/views/Connector/ServiceForm/components/Controls/ConnectorServiceTypeControl.tsx
@@ -191,9 +191,9 @@ const ConnectorServiceTypeControl: React.FC<{
       ? [
           "200330b2-ea62-4d11-ac6d-cfe3e3f8ab2b", // Snapchat
           "2470e835-feaf-4db6-96f3-70fd645acc77", // Salesforce Singer
-          ...(workspace.workspaceId !== "54135667-ce73-4820-a93c-29fe1510d348"
-            ? ["9da77001-af33-4bcd-be46-6252bf9342b9"]
-            : []), // Shopify
+          ...(workspace.workspaceId !== "54135667-ce73-4820-a93c-29fe1510d348" // Shopify workspace for review
+            ? ["9da77001-af33-4bcd-be46-6252bf9342b9"] // Shopify
+            : []),
         ]
       : [];
   const sortedDropDownData = useMemo(
