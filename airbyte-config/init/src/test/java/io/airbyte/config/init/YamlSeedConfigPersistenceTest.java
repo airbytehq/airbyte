1b02cee242b5e9474ab3ba7198b95b137be38e48
git-phu <peter@airbyte.io>
2022-03-16 19:42:05 -0700

    seed yaml definitions are always public, never custom

--- airbyte-config/init/src/test/java/io/airbyte/config/init/YamlSeedConfigPersistenceTest.java
+++ airbyte-config/init/src/test/java/io/airbyte/config/init/YamlSeedConfigPersistenceTest.java
@@ -44,6 +44,7 @@ public class YamlSeedConfigPersistenceTest {
     assertEquals("https://docs.airbyte.io/integrations/sources/mysql", mysqlSource.getDocumentationUrl());
     assertEquals("mysql.svg", mysqlSource.getIcon());
     assertEquals(URI.create("https://docs.airbyte.io/integrations/sources/mysql"), mysqlSource.getSpec().getDocumentationUrl());
+    assertEquals(true, mysqlSource.getPublic());
 
     // destination
     final String s3DestinationId = "4816b78f-1489-44c1-9060-4b19d5fa9362";
@@ -54,6 +55,7 @@ public class YamlSeedConfigPersistenceTest {
     assertEquals("airbyte/destination-s3", s3Destination.getDockerRepository());
     assertEquals("https://docs.airbyte.io/integrations/destinations/s3", s3Destination.getDocumentationUrl());
     assertEquals(URI.create("https://docs.airbyte.io/integrations/destinations/s3"), s3Destination.getSpec().getDocumentationUrl());
+    assertEquals(true, s3Destination.getPublic());
   }
 
   @Test
