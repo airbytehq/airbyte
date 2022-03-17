907fa07e48a69f5b9028383defc5fdcaadd8cb5f
Christophe Duong <christophe.duong@gmail.com>
2022-03-17 12:23:07 +0100

    Format code

--- airbyte-integrations/bases/base-java/src/main/java/io/airbyte/integrations/destination/record_buffer/BufferingStrategy.java
+++ airbyte-integrations/bases/base-java/src/main/java/io/airbyte/integrations/destination/record_buffer/BufferingStrategy.java
@@ -12,9 +12,10 @@ import io.airbyte.protocol.models.AirbyteMessage;
  * High-level interface used by
  * {@link io.airbyte.integrations.destination.buffered_stream_consumer.BufferedStreamConsumer}
  *
- * A Record buffer strategy relies on the capacity available of underlying
+ * A Record buffering strategy relies on the capacity available of underlying
  * {@link SerializableBuffer} to determine what to do when consuming a new
- * {@link AirbyteMessage} into the buffer. And when to
+ * {@link AirbyteMessage} into the buffer. It also defines when to flush
+ * such buffers and how to empty them once they fill up.
  *
  */
 public interface BufferingStrategy extends AutoCloseable {
