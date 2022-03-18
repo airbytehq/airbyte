ca8022f0407ec60b6417b403785338aa9046b85c
Augustin <augustin.lafanechere@gmail.com>
2022-03-18 12:17:57 +0100

    🐛 octavia-cli: cross-platform builds (arm64 + amd64) (#11226)

--- octavia-cli/publish.sh
+++ octavia-cli/publish.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 
-set -eux
+set -ux
 VERSION=$1
-docker tag airbyte/octavia-cli:dev airbyte/octavia-cli:${VERSION}
-docker push airbyte/octavia-cli:${VERSION}
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+docker buildx create --name octavia_builder > /dev/null 2>&1
+set -e
+docker buildx use octavia_builder
+docker buildx inspect --bootstrap
+docker buildx build --push --tag airbyte/octavia-cli:${VERSION} --platform=linux/arm64,linux/amd64 ${SCRIPT_DIR}
