387d6d05453eab73968cbb6c8170d7d8d0dd25bc
Octavia Squidington III <90398440+octavia-squidington-iii@users.noreply.github.com>
2022-03-21 20:47:39 +0100

    Bump Airbyte version from 0.35.58-alpha to 0.35.59-alpha (#11298)
    Co-authored-by: pmossman <pmossman@users.noreply.github.com>

--- airbyte-server/src/test/java/io/airbyte/server/handlers/OperationsHandlerTest.java
+++ airbyte-server/src/test/java/io/airbyte/server/handlers/OperationsHandlerTest.java
@@ -30,7 +30,6 @@ import io.airbyte.config.persistence.ConfigNotFoundException;
 import io.airbyte.config.persistence.ConfigRepository;
 import io.airbyte.validation.json.JsonValidationException;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.UUID;
