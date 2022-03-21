b8236f2b2d122b79a33853d02f20acb5fff6439c
Tim Roes <tim@airbyte.io>
2022-03-21 15:00:31 +0100

    Use <Navigate> instead of selectWorkspace

--- airbyte-webapp/src/packages/cloud/views/DefaultView.tsx
+++ airbyte-webapp/src/packages/cloud/views/DefaultView.tsx
@@ -1,22 +1,20 @@
-import { LoadingPage } from "components";
+import { RoutePaths } from "pages/routes";
 import { Navigate } from "react-router-dom";
 import { CloudRoutes } from "../cloudRoutes";
-import {
-  useListCloudWorkspaces,
-  useWorkspaceService,
-} from "../services/workspaces/WorkspacesService";
+import { useListCloudWorkspaces } from "../services/workspaces/WorkspacesService";
 
 export const DefaultView: React.FC = () => {
   const workspaces = useListCloudWorkspaces();
-  const { selectWorkspace } = useWorkspaceService();
 
   // Only show the workspace creation list if there is more than one workspace
   if (workspaces.length > 1) {
     return <Navigate to={`/${CloudRoutes.SelectWorkspace}`} replace />;
   }
 
-  // Otherwise directly switch to the single workspace
-  selectWorkspace(workspaces[0].workspaceId);
-
-  return <LoadingPage />;
+  return (
+    <Navigate
+      to={`/${RoutePaths.Workspaces}/${workspaces[0].workspaceId}`}
+      replace
+    />
+  );
 };
