11cfa8358da51f490bad4fcc4d30dfedb22f7d2f
Luis Gomez <lgomezm90@gmail.com>
2022-03-26 21:21:48 -0500

    Added unit tests

--- airbyte-integrations/connectors/source-notion/unit_tests/test_streams.py
+++ airbyte-integrations/connectors/source-notion/unit_tests/test_streams.py
@@ -7,7 +7,8 @@ from unittest.mock import MagicMock
 
 import pytest
 import requests
-from source_notion.streams import NotionStream
+from airbyte_cdk.models import SyncMode
+from source_notion.streams import NotionStream, Users
 
 
 @pytest.fixture
@@ -76,3 +77,61 @@ def test_backoff_time(patch_base_class):
     stream = NotionStream(config=MagicMock())
     expected_backoff_time = None
     assert stream.backoff_time(response_mock) == expected_backoff_time
+
+
+def test_users_request_params(patch_base_class):
+    stream = Users(config=MagicMock())
+
+    # No next_page_token. First pull
+    inputs = {"stream_slice": None, "stream_state": None, "next_page_token": None}
+    expected_params = {"page_size": 100}
+    assert stream.request_params(**inputs) == expected_params
+
+    # When getting pages after the first pull.
+    inputs = {"stream_slice": None, "stream_state": None, "next_page_token": {"next_cursor": "123"}}
+    expected_params = {"start_cursor": "123", "page_size": 100}
+    assert stream.request_params(**inputs) == expected_params
+
+
+def test_user_stream_handles_pagination_correclty(requests_mock):
+    """
+    Test shows that Users stream uses pagination as per Notion API docs.
+    """
+
+    response_body = {
+        "object": "list",
+        "results": [{"id": f"{x}"} for x in range(100)],
+        "next_cursor": "test_cursor_1",
+        "has_more": True,
+        "type": "user"
+    }
+    url = "https://api.notion.com/v1/users?page_size=100"
+    requests_mock.get(url, json=response_body)
+
+    response_body = {
+        "object": "list",
+        "results": [{"id": f"{x}"} for x in range(100, 200)],
+        "next_cursor": "test_cursor_2",
+        "has_more": True,
+        "type": "user"
+    }
+    url = "https://api.notion.com/v1/users?page_size=100&start_cursor=test_cursor_1"
+    requests_mock.get(url, json=response_body)
+
+    response_body = {
+        "object": "list",
+        "results": [{"id": f"{x}"} for x in range(200, 220)],
+        "next_cursor": None,
+        "has_more": False,
+        "type": "user"
+    }
+    url = "https://api.notion.com/v1/users?page_size=100&start_cursor=test_cursor_2"
+    requests_mock.get(url, json=response_body)
+
+    stream = Users(config=MagicMock())
+
+    user_ids = set()
+    for record in stream.read_records(sync_mode=SyncMode.full_refresh):
+        user_ids.add(record["id"])
+
+    assert len(user_ids) == 220
