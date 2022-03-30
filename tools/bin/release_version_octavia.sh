633d80a3e713d586aba6405c7afc2b23a2f9dfb9
alafanechere <augustin.lafanechere@gmail.com>
2022-03-30 16:31:10 +0200

    fix

--- tools/bin/release_version_octavia.sh
+++ tools/bin/release_version_octavia.sh
@@ -11,13 +11,6 @@ fi
 
 docker login -u airbytebot -p "${DOCKER_PASSWORD}"
 
-
-# uses .bumpversion.cfg to find files to bump
-# requires no git diffs to run
-# commits the bumped versions code to your branch
-pip install bumpversion
-bumpversion "$PART_TO_BUMP"
-
 ./tools/bin/bump_version.sh
 
 # TESTING: DISABLE BUILD AND PUBLISH FOR ITERATIONS ON CI
