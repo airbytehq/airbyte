2ebc4e3fa3ec0ce758f142a6b1b60d0249e8c722
alafanechere <augustin.lafanechere@gmail.com>
2022-03-30 10:26:15 +0200

    separte job for octavia release

--- tools/bin/release_version_octavia.sh
+++ tools/bin/release_version_octavia.sh
@@ -4,6 +4,16 @@ set -e
 
 . tools/lib/lib.sh
 
+if [[ -z "${CLOUDREPO_USER}" ]]; then
+  echo 'CLOUDREPO_USER env var not set. Please retrieve the user email from the CloudRepo lastpass secret and run export CLOUDREPO_USER=<user_from_secret>.';
+  exit 1;
+fi
+
+if [[ -z "${CLOUDREPO_PASSWORD}" ]]; then
+  echo 'CLOUDREPO_PASSWORD env var not set. Please retrieve the user email from the CloudRepo lastpass secret and run export CLOUDREPO_PASSWORD=<password_from_secret>.';
+  exit 1;
+fi
+
 if [[ -z "${DOCKER_PASSWORD}" ]]; then
   echo 'DOCKER_PASSWORD for airbytebot not set.';
   exit 1;
@@ -11,12 +21,23 @@ fi
 
 docker login -u airbytebot -p "${DOCKER_PASSWORD}"
 
+PREV_VERSION=$(grep -w VERSION .env | cut -d"=" -f2)
+
+[[ -z "$PART_TO_BUMP" ]] && echo "Usage ./tools/bin/release_version.sh (major|minor|patch)" && exit 1
+
+# uses .bumpversion.cfg to find files to bump
+# requires no git diffs to run
+# commits the bumped versions code to your branch
+pip install bumpversion
+bumpversion "$PART_TO_BUMP" octavia-cli/install.sh
+bumpversion "$PART_TO_BUMP" octavia-cli/README.md
+bumpversion "$PART_TO_BUMP" octavia-cli/Dockerfile
+
 NEW_VERSION=$(grep -w VERSION .env | cut -d"=" -f2)
 GIT_REVISION=$(git rev-parse HEAD)
-
 [[ -z "$GIT_REVISION" ]] && echo "Couldn't get the git revision..." && exit 1
+echo "Bumped version from ${PREV_VERSION} to ${NEW_VERSION}"
 
-rm -rf .venv # Removing the virtualenv that might already exists from a previous build. 
 echo "Building and publishing OCTAVIA version $NEW_VERSION for git revision $GIT_REVISION..."
 VERSION=$NEW_VERSION SUB_BUILD=OCTAVIA_CLI ./gradlew clean build
 ./octavia-cli/publish.sh ${NEW_VERSION} ${GIT_REVISION}
