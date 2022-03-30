128382168545f1bdd65d4e5d9d2c4da6f86a34a3
alafanechere <augustin.lafanechere@gmail.com>
2022-03-30 12:08:58 +0200

    re-enable platform build

--- tools/bin/release_version.sh
+++ tools/bin/release_version.sh
@@ -36,9 +36,8 @@ GIT_REVISION=$(git rev-parse HEAD)
 [[ -z "$GIT_REVISION" ]] && echo "Couldn't get the git revision..." && exit 1
 echo "Bumped version from ${PREV_VERSION} to ${NEW_VERSION}"
 
-# Disabling build and  push for testing:
-# echo "Building and publishing PLATFORM version $NEW_VERSION for git revision $GIT_REVISION..."
-# VERSION=$NEW_VERSION SUB_BUILD=PLATFORM ./gradlew clean build
-# SUB_BUILD=PLATFORM ./gradlew publish
-# VERSION=$NEW_VERSION GIT_REVISION=$GIT_REVISION docker-compose -f docker-compose.build.yaml push
-# echo "Completed building and publishing PLATFORM..."
+echo "Building and publishing PLATFORM version $NEW_VERSION for git revision $GIT_REVISION..."
+VERSION=$NEW_VERSION SUB_BUILD=PLATFORM ./gradlew clean build
+SUB_BUILD=PLATFORM ./gradlew publish
+VERSION=$NEW_VERSION GIT_REVISION=$GIT_REVISION docker-compose -f docker-compose.build.yaml push
+echo "Completed building and publishing PLATFORM..."
