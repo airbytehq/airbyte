0d8ebb2c2ba46c991cefc536feca50d31bbe8ba0
Yurii Bidiuk <yura.bidyuk@gmail.com>
2022-03-30 16:06:59 +0300

    add comments

--- airbyte-integrations/bases/standard-destination-test/src/main/java/io/airbyte/integrations/standardtest/destination/DateTimeConverter.java
+++ airbyte-integrations/bases/standard-destination-test/src/main/java/io/airbyte/integrations/standardtest/destination/DateTimeConverter.java
@@ -14,6 +14,14 @@ import java.util.Map;
 
 public interface DateTimeConverter {
 
+  /**
+   * Search dateTimeFieldNames inside data from @messages and converts to connector-specific date or
+   * date-time format
+   *
+   * @param messages list with AirbyteMessage
+   * @param dateTimeFieldNames map where key - path to the date/date-time field (e.g.
+   *        /parentField/field), value - "date" or "date-time" depends on the format from catalog.
+   */
   default void convertDateTimeFields(List<AirbyteMessage> messages, Map<String, String> dateTimeFieldNames) {
     for (AirbyteMessage message : messages) {
       if (message.getType() == Type.RECORD) {
@@ -22,16 +30,46 @@ public interface DateTimeConverter {
     }
   }
 
+  /**
+   * Search dateTimeFieldNames inside @data and converts to connector-specific date or date-time
+   * format
+   *
+   * @param data from message record
+   * @param dateTimeFieldNames map where key - path to the date/date-time field (e.g.
+   *        /parentField/field), value - "date" or "date-time" depends on the format from catalog.
+   */
   default void convertDateTime(ObjectNode data, Map<String, String> dateTimeFieldNames) {}
 
+  /**
+   * Override this method and return 'true' if destination connector requires conversion for
+   * date/date-time fields for testSyncWithNormalization() method. Then override convertDateTime(..)
+   * method to convert data to specific date-time format
+   *
+   * @return true - if destination connector requires conversion for date/date-time fields, false - in
+   *         the other case.
+   */
   default boolean requiresDateTimeConversionForNormalizedSync() {
     return false;
   }
 
+  /**
+   * Override this method and return 'true' if destination connector requires conversion for
+   * date/date-time fields for testSync() method. Then override convertDateTime(..) method to convert
+   * data to specific date-time format
+   *
+   * @return true - if destination connector requires conversion for date/date-time fields, false - in
+   *         the other case.
+   */
   default boolean requiresDateTimeConversionForSync() {
     return false;
   }
 
+  /**
+   *
+   * @param path path to field e.g /field1/field2
+   * @param key e.g field key
+   * @return true if the path consists of only one @key
+   */
   default boolean isKeyInPath(String path, String key) {
     var pathFields = new ArrayList<>(Arrays.asList(path.split("/")));
     pathFields.remove(0); // first element always empty string
