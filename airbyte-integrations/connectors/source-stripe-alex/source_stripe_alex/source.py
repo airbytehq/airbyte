24d6de36d5cf052bd19c84641788db189cc49f36
Alexandre Girard <alexandre@airbyte.io>
2022-03-30 13:10:43 -0700

    update

--- airbyte-integrations/connectors/source-stripe-alex/source_stripe_alex/source.py
+++ airbyte-integrations/connectors/source-stripe-alex/source_stripe_alex/source.py
@@ -3,77 +3,37 @@
 #
 
 
-import logging
 from typing import Any, List, Mapping, Tuple
 
 import pendulum
-import requests
+import stripe
+from airbyte_cdk import AirbyteLogger
 from airbyte_cdk.sources import AbstractSource
 from airbyte_cdk.sources.streams import Stream
-from airbyte_cdk.sources.streams.http.auth import NoAuth
-from source_stripe_alex.streams import Invoices
-
-logging.basicConfig(level=logging.DEBUG)
-requests_log = logging.getLogger("requests.packages.urllib3")
-requests_log.setLevel(logging.DEBUG)
-requests_log.propagate = True
-
-"""
-TODO: Most comments in this class are instructive and should be deleted after the source is implemented.
-
-This file provides a stubbed example of how to use the Airbyte CDK to develop both a source connector which supports full refresh or and an
-incremental syncs from an HTTP API.
-
-The various TODOs are both implementation hints and steps - fulfilling all the TODOs should be sufficient to implement one basic and one incremental
-stream from a source. This pattern is the same one used by Airbyte internally to implement connectors.
-
-The approach here is not authoritative, and devs are free to use their own judgement.
-
-There are additional required TODOs in the files within the integration_tests folder and the spec.json file.
-"""
+from airbyte_cdk.sources.streams.http.auth import TokenAuthenticator
+from source_stripe_alex.streams import (
+    InvoiceItems,
+    InvoiceLineItems,
+    Invoices,
+)
 
 
-# Source
 class SourceStripeAlex(AbstractSource):
-    def check_connection(self, logger, config) -> Tuple[bool, any]:
+    def check_connection(self, logger: AirbyteLogger, config: Mapping[str, Any]) -> Tuple[bool, Any]:
         try:
-            logger.info(f"config: {config}")
-            headers = self._get_headers(config)
-
-            res = self._call_api(config["url_base"], config["check_endpoint"], headers, logger)
-            connected = res.ok
-            return connected, None
+            stripe.api_key = config["client_secret"]
+            stripe.Account.retrieve(config["account_id"])
+            return True, None
         except Exception as e:
             return False, e
 
     def streams(self, config: Mapping[str, Any]) -> List[Stream]:
-        url_base = config["url_base"]
-        primary_key = config["primary_key"]
-        retry_factor = config["retry_factor"]
-        max_retries = config["max_retries"]
-        headers = self._get_headers(config)
-        response_key = config["response_key"]
-        request_parameters = config["request_parameters"]
+        authenticator = TokenAuthenticator(config["client_secret"])
         start_date = pendulum.parse(config["start_date"]).int_timestamp
-        cursor_field = config["cursor_field"]
+        args = {"authenticator": authenticator, "account_id": config["account_id"], "start_date": start_date}
+        incremental_args = {**args, "lookback_window_days": config.get("lookback_window_days")}
         return [
-            Invoices(
-                url_base=url_base,
-                primary_key=primary_key,
-                retry_factor=retry_factor,
-                max_retries=max_retries,
-                headers=headers,
-                response_key=response_key,
-                request_parameters=request_parameters,
-                start_date=start_date,
-                cursor_field=cursor_field,
-                authenticator=NoAuth()
-            )
+            InvoiceItems(**incremental_args),
+            InvoiceLineItems(**args),
+            Invoices(**incremental_args)
         ]
-
-    def _get_headers(self, config):
-        return {k: v.format(**config) for k, v in config["headers"].items()}
-
-    def _call_api(self, url_base, endpoint, headers, logger):
-        url = f"{url_base}{endpoint}"
-        return requests.get(url, headers=headers)
--- airbyte-integrations/connectors/source-stripe-alex/source_stripe_alex/streams.py
+++ airbyte-integrations/connectors/source-stripe-alex/source_stripe_alex/streams.py
@@ -2,121 +2,242 @@
 # Copyright (c) 2021 Airbyte, Inc., all rights reserved.
 #
 
-from abc import ABC
-from typing import Any, Iterable, List, Mapping, MutableMapping, Optional, Union
+import math
+from abc import ABC, abstractmethod
+from itertools import chain
+from typing import Any, Iterable, Mapping, MutableMapping, Optional
 
+import pendulum
 import requests
+from airbyte_cdk.models import SyncMode
 from airbyte_cdk.sources.streams.http import HttpStream
-from airbyte_cdk.sources.streams.http.auth import HttpAuthenticator
 
 
-# Basic full refresh stream
-class StripeAlexStream(HttpStream, ABC):
-    def __init__(
-            self,
-            *,
-            path: str,
-            url_base: str,
-            primary_key: str,
-            retry_factor: float,
-            max_retries: int,
-            headers: Mapping[str, str],
-            response_key: str,
-            request_parameters: Mapping[str, Any],
-            start_date: int,
-            cursor_field: str,
-            authenticator: HttpAuthenticator,
-            **kwargs
-    ):
-        super(StripeAlexStream, self).__init__(authenticator, **kwargs)
-        self._path = path
-        self._primary_key = primary_key
-        self.url_base = url_base
-        self._retry_factor = retry_factor
-        self._max_retries = max_retries
-        self._headers = headers
-        self._response_key = response_key
-        self._request_parameters = request_parameters
-        self._start_date = start_date
-        self._cursor_field = cursor_field
-        self._cursor_value = None
-        self._parent = self
-        self._filter = None
-        self._add_parent_id = False
+class StripeStream(HttpStream, ABC):
+    url_base = "https://api.stripe.com/v1/"
+    primary_key = "id"
+
+    def __init__(self, start_date: int, account_id: str, **kwargs):
+        super().__init__(**kwargs)
+        self.account_id = account_id
+        self.start_date = start_date
 
     def next_page_token(self, response: requests.Response) -> Optional[Mapping[str, Any]]:
-        return None
-
-    def request_headers(self, **kwargs) -> Mapping[str, Any]:
-        return self._headers
+        decoded_response = response.json()
+        if bool(decoded_response.get("has_more", "False")) and decoded_response.get("data", []):
+            last_object_id = decoded_response["data"][-1]["id"]
+            return {"starting_after": last_object_id}
 
     def request_params(
-            self, stream_state: Mapping[str, Any], stream_slice: Mapping[str, any] = None, next_page_token: Mapping[str, Any] = None
+            self,
+            stream_state: Mapping[str, Any],
+            stream_slice: Mapping[str, Any] = None,
+            next_page_token: Mapping[str, Any] = None,
     ) -> MutableMapping[str, Any]:
-        params = self._request_parameters
-        # FIXME would be great to extract this too!
+
+        # Stripe default pagination is 10, max is 100
+        params = {"limit": 100}
+
         # Handle pagination by inserting the next page's token in the request parameters
         if next_page_token:
             params.update(next_page_token)
+
         return params
+
+    def request_headers(self, **kwargs) -> Mapping[str, Any]:
+        if self.account_id:
+            return {"Stripe-Account": self.account_id}
+
+        return {}
 
     def parse_response(self, response: requests.Response, **kwargs) -> Iterable[Mapping]:
         response_json = response.json()
-        yield from response_json.get(self._response_key, [])  # Stripe puts records in a container array "data"
+        yield from response_json.get("data", [])  # Stripe puts records in a container array "data"
 
-    def url_base(self) -> str:
-        return self._url_base
 
-    def path(
-            self,
-            *,
-            stream_state: Mapping[str, Any] = None,
-            stream_slice: Mapping[str, Any] = None,
-            next_page_token: Mapping[str, Any] = None,
-    ) -> str:
-        return self._path
+class IncrementalStripeAlexStream(StripeStream, ABC):
+    # Stripe returns most recently created objects first, so we don't want to persist state until the entire stream has been read
+    state_checkpoint_interval = math.inf
+
+    def __init__(self, lookback_window_days: int = 0, **kwargs):
+        super().__init__(**kwargs)
+        self.lookback_window_days = lookback_window_days
 
     @property
-    def primary_key(self) -> Optional[Union[str, List[str], List[List[str]]]]:
-        return self._primary_key
-
-    @primary_key.setter
-    def set_primary_key(self, value):
-        self._primary_key = value
-
-    @property
-    def retry_factor(self) -> float:
-        return self._retry_factor
-
-    @retry_factor.setter
-    def set_retry_factor(self, value):
-        self._retry_factor = value
-
-    @property
-    def max_retries(self) -> Union[int, None]:
-        return self._max_retries
-
-    @max_retries.setter
-    def set_max_retries(self, value):
-        self._max_retries = value
-
-    @property
-    def parent(self) -> Union[int, None]:
-        return self._parent
-
-    @parent.setter
-    def set_parent(self, value):
-        self._parent = value
-
-    @property
+    @abstractmethod
     def cursor_field(self) -> str:
         """
         Defining a cursor field indicates that a stream is incremental, so any incremental stream must extend this class
         and define a cursor field.
         """
-        return self._cursor_field
+        pass
+
+    def get_updated_state(self, current_stream_state: MutableMapping[str, Any], latest_record: Mapping[str, Any]) -> Mapping[str, Any]:
+        """
+        Return the latest state by comparing the cursor value in the latest record with the stream's most recent state object
+        and returning an updated state object.
+        """
+        return {self.cursor_field: max(latest_record.get(self.cursor_field), current_stream_state.get(self.cursor_field, 0))}
+
+    def request_params(self, stream_state: Mapping[str, Any] = None, **kwargs):
+        stream_state = stream_state or {}
+        params = super().request_params(stream_state=stream_state, **kwargs)
+
+        start_timestamp = self.get_start_timestamp(stream_state)
+        if start_timestamp:
+            params["created[gte]"] = start_timestamp
+        return params
+
+    def get_start_timestamp(self, stream_state) -> int:
+        start_point = self.start_date
+        if stream_state and self.cursor_field in stream_state:
+            start_point = max(start_point, stream_state[self.cursor_field])
+
+        if start_point and self.lookback_window_days:
+            self.logger.info(f"Applying lookback window of {self.lookback_window_days} days to stream {self.name}")
+            start_point = int(pendulum.from_timestamp(start_point).subtract(days=abs(self.lookback_window_days)).timestamp())
+
+        return start_point
 
 
-class Invoices(StripeAlexStream):
-    def __init__(self, **kwargs):
-        super().__init__(path="v1/invoices", **kwargs)
+class StripeSubStream(StripeStream, ABC):
+    """
+    Research shows that records related to SubStream can be extracted from Parent streams which already
+    contain 1st page of needed items. Thus, it significantly decreases a number of requests needed to get
+    all item in parent stream, since parent stream returns 100 items per request.
+    Note, in major cases, pagination requests are not performed because sub items are fully reported in parent streams
+
+    For example:
+    Line items are part of each 'invoice' record, so use Invoices stream because
+    it allows bulk extraction:
+        0.1.28 and below - 1 request extracts line items for 1 invoice (+ pagination reqs)
+        0.1.29 and above - 1 request extracts line items for 100 invoices (+ pagination reqs)
+
+    if line items object has indication for next pages ('has_more' attr)
+    then use current stream to extract next pages. In major cases pagination requests
+    are not performed because line items are fully reported in 'invoice' record
+
+    Example for InvoiceLineItems and parent Invoice streams, record from Invoice stream:
+        {
+          "created": 1641038947,    <--- 'Invoice' record
+          "customer": "cus_HezytZRkaQJC8W",
+          "id": "in_1KD6OVIEn5WyEQxn9xuASHsD",    <---- value for 'parent_id' attribute
+          "object": "invoice",
+          "total": 0,
+          ...
+          "lines": {    <---- sub_items_attr
+            "data": [
+              {
+                "id": "il_1KD6OVIEn5WyEQxnm5bzJzuA",    <---- 'Invoice' line item record
+                "object": "line_item",
+                ...
+              },
+              {...}
+            ],
+            "has_more": false,    <---- next pages from 'InvoiceLineItemsPaginated' stream
+            "object": "list",
+            "total_count": 2,
+            "url": "/v1/invoices/in_1KD6OVIEn5WyEQxn9xuASHsD/lines"
+          }
+        }
+    """
+
+    filter: Optional[Mapping[str, Any]] = None
+    add_parent_id: bool = False
+
+    @property
+    @abstractmethod
+    def parent(self) -> StripeStream:
+        """
+        :return: parent stream which contains needed records in <sub_items_attr>
+        """
+
+    @property
+    @abstractmethod
+    def parent_id(self) -> str:
+        """
+        :return: string with attribute name
+        """
+
+    @property
+    @abstractmethod
+    def sub_items_attr(self) -> str:
+        """
+        :return: string if single primary key, list of strings if composite primary key, list of list of strings if composite primary key consisting of nested fields.
+          If the stream has no primary keys, return None.
+        """
+
+    def request_params(self, stream_slice: Mapping[str, Any] = None, **kwargs):
+        params = super().request_params(stream_slice=stream_slice, **kwargs)
+
+        # add 'starting_after' param
+        if not params.get("starting_after") and stream_slice and stream_slice.get("starting_after"):
+            params["starting_after"] = stream_slice["starting_after"]
+
+        return params
+
+    def read_records(self, stream_slice: Optional[Mapping[str, Any]] = None, **kwargs) -> Iterable[Mapping[str, Any]]:
+
+        parent_stream = self.parent(authenticator=self.authenticator, account_id=self.account_id, start_date=self.start_date)
+        for record in parent_stream.read_records(sync_mode=SyncMode.full_refresh):
+
+            items_obj = record.get(self.sub_items_attr, {})
+            if not items_obj:
+                continue
+
+            items = items_obj.get("data", [])
+
+            # non-generic filter, mainly for BankAccounts stream only
+            if self.filter:
+                items = [i for i in items if i.get(self.filter["attr"]) == self.filter["value"]]
+
+            # get next pages
+            items_next_pages = []
+            if items_obj.get("has_more") and items:
+                stream_slice = {self.parent_id: record["id"], "starting_after": items[-1]["id"]}
+                items_next_pages = super().read_records(sync_mode=SyncMode.full_refresh, stream_slice=stream_slice, **kwargs)
+
+            for item in chain(items, items_next_pages):
+                if self.add_parent_id:
+                    # add reference to parent object when item doesn't have it already
+                    item[self.parent_id] = record["id"]
+                yield item
+
+
+class Invoices(IncrementalStripeAlexStream):
+    """
+    API docs: https://stripe.com/docs/api/invoices/list
+    """
+
+    cursor_field = "created"
+
+    def path(self, **kwargs):
+        return "invoices"
+
+
+class InvoiceLineItems(StripeSubStream):
+    """
+    API docs: https://stripe.com/docs/api/invoices/invoice_lines
+    """
+
+    name = "invoice_line_items"
+
+    parent = Invoices
+    parent_id: str = "invoice_id"
+    sub_items_attr = "lines"
+    add_parent_id = True
+
+    def path(self, stream_slice: Mapping[str, Any] = None, **kwargs):
+        return f"invoices/{stream_slice[self.parent_id]}/lines"
+
+
+class InvoiceItems(IncrementalStripeAlexStream):
+    """
+    API docs: https://stripe.com/docs/api/invoiceitems/list
+    """
+
+    cursor_field = "date"
+    name = "invoice_items"
+
+    def path(self, **kwargs):
+        return "invoiceitems"
