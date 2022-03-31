57a6353668b4628ff81157a14ad1949693ead5f8
Artem Astapenko <3767150+Jamakase@users.noreply.github.com>
2022-03-31 22:35:58 +0300

    Fix cloud login/signup loading state (#11625)
    * Fix cloud loading state
    
    * Use useInitAuthService for deps
    
    * Fix use init service to not reinit on every render

--- airbyte-webapp/src/services/useInitService.tsx
+++ airbyte-webapp/src/services/useInitService.tsx
@@ -5,12 +5,18 @@ export function useInitService<T extends new (...args: unknown[]) => any>(
   f: () => InstanceType<T>,
   deps: ConstructorParameters<T>
 ): InstanceType<T> {
-  const service = useRef<InstanceType<T>>(f());
+  const service = useRef<InstanceType<T> | null>(null);
 
   useEffect(() => {
-    service.current = f();
+    if (service.current !== null) {
+      service.current = f();
+    }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, deps);
 
-  return service.current;
+  if (service.current === null) {
+    service.current = f();
+  }
+
+  return (service.current as unknown) as InstanceType<T>;
 }
