b38941945b9869ac055d577986ec7a8097a9173a
Artem Astapenko <jamakase54@gmail.com>
2022-03-31 15:55:41 +0300

    Fix import paths

--- airbyte-webapp/src/components/ApiErrorBoundary/ApiErrorBoundary.tsx
+++ airbyte-webapp/src/components/ApiErrorBoundary/ApiErrorBoundary.tsx
@@ -3,8 +3,8 @@ import React from "react";
 import { FormattedMessage } from "react-intl";
 import { isVersionError } from "core/request/VersionError";
 import { ErrorOccurredView } from "views/common/ErrorOccurredView";
-import { StartOverErrorView } from "../../views/common/StartOverErrorView";
-import { ResourceNotFoundErrorBoundary } from "../../views/common/ResorceNotFoundErrorBoundary";
+import { StartOverErrorView } from "views/common/StartOverErrorView";
+import { ResourceNotFoundErrorBoundary } from "views/common/ResorceNotFoundErrorBoundary";
 
 type BoundaryState = { errorId?: string; message?: string };
 
