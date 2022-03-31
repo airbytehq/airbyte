15b0eeb5d50540d860f659b3124ec912c3992220
annalvova <annalvova05@gmail.com>
2022-03-31 20:13:51 +0300

    add tests

--- airbyte-integrations/connectors/source-mailchimp/unit_tests/test_streams.py
+++ airbyte-integrations/connectors/source-mailchimp/unit_tests/test_streams.py
@@ -1,6 +1,7 @@
 from http import HTTPStatus
 from unittest.mock import MagicMock
 
+import json
 import pytest
 from source_mailchimp.streams import (
     Lists,
@@ -51,3 +52,50 @@ def test_stream_read(requests_mock, auth, stream, endpoint):
     records = read_full_refresh(stream)
 
     assert records
+
+
+def test_next_page_token(auth):
+    args = {"authenticator": auth}
+    stream = Lists(**args)
+    inputs = {"response": MagicMock()}
+    expected_token = None
+    assert stream.next_page_token(**inputs) == expected_token
+
+    resp = json.loads(json.dumps({"lists": [{"id": i} for i in range(101)]}))
+    inputs = {"response": MagicMock(json=MagicMock(return_value=resp))}
+    expected_token = {"offset": 100}
+    assert stream.next_page_token(**inputs) == expected_token
+
+
+def test_request_params(auth):
+    args = {"authenticator": auth}
+    stream = Lists(**args)
+    inputs = {"stream_slice": None, "stream_state": None, "next_page_token": None}
+    expected_params = {"count": 100, "sort_dir": "ASC", "sort_field": "date_created"}
+    assert stream.request_params(**inputs) == expected_params
+
+    inputs = {"stream_slice": None, "stream_state": None, "next_page_token": {"offset": 100}}
+    expected_params = {"count": 100, "sort_dir": "ASC", "sort_field": "date_created", "offset": 100}
+    assert stream.request_params(**inputs) == expected_params
+
+
+def test_get_updated_state(auth):
+    args = {"authenticator": auth}
+    stream = Lists(**args)
+
+    current_state_stream = {}
+    latest_record = {"date_created": "2020-01-01"}
+    new_stream_state = stream.get_updated_state(current_state_stream, latest_record)
+    assert new_stream_state == {"date_created": "2020-01-01"}
+
+    current_state_stream = {"date_created": "2020-01-01"}
+    latest_record = {"date_created": "2021-01-01"}
+    new_stream_state = stream.get_updated_state(current_state_stream, latest_record)
+    assert new_stream_state == {"date_created": "2021-01-01"}
+
+    current_state_stream = {"date_created": "2021-01-01"}
+    latest_record = {"date_created": "2022-01-01"}
+    new_stream_state = stream.get_updated_state(current_state_stream, latest_record)
+    assert new_stream_state == {"date_created": "2022-01-01"}
+
+
