3d27949dcd7151190a4a891c6088c375928b721b
Alexandre Girard <alexandre@airbyte.io>
2022-04-04 12:45:56 -0700

    new line

--- tools/bin/setup_connector_venv.sh
+++ tools/bin/setup_connector_venv.sh
@@ -5,7 +5,8 @@ set -e
 CONNECTOR=$1
 
 cd airbyte-integrations/connectors/$CONNECTOR
-python -m venv .venv
+echo $(pwd)
+python3 -m venv .venv
 source .venv/bin/activate
 pip install -r requirements.txt
 pip install '.[tests]'
