2cc3acd55b90cb607efaddf16706a01d5f78297f
Alexandre Girard <alexandre@airbyte.io>
2022-04-04 12:47:20 -0700

    echo

--- tools/bin/setup_connector_venv.sh
+++ tools/bin/setup_connector_venv.sh
@@ -3,9 +3,9 @@
 set -e
 
 CONNECTOR=$1
+echo Installing requirements for $CONNECTOR
 
 cd airbyte-integrations/connectors/$CONNECTOR
-echo $(pwd)
 python3 -m venv .venv
 source .venv/bin/activate
 pip install -r requirements.txt
