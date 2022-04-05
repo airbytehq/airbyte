2c167489ea8bd3ee2350feeb30fbd90e08121bfa
andriikorotkov <andrii.h.korotkov@globallogic.com>
2022-04-05 14:08:41 +0300

    updated tests

--- airbyte-integrations/connectors/destination-bigquery-denormalized/src/test-integration/java/io/airbyte/integrations/destination/bigquery/util/BigQueryDenormalizedTestDataUtils.java
+++ airbyte-integrations/connectors/destination-bigquery-denormalized/src/test-integration/java/io/airbyte/integrations/destination/bigquery/util/BigQueryDenormalizedTestDataUtils.java
@@ -19,120 +19,7 @@ public class BigQueryDenormalizedTestDataUtils {
   }
 
   public static JsonNode getAnyOfSchema() {
-    return Jsons.deserialize("""
-                             {
-                                "type":"object",
-                                "properties":{
-                                    "id":{
-                                        "type":[
-                                            "null",
-                                            "string"
-                                        ]
-                                    },
-                                    "name":{
-                                        "type":[
-                                            "null",
-                                            "string"
-                                        ]
-                                    },
-                                    "type":{
-                                        "type":[
-                                            "null",
-                                            "string"
-                                        ]
-                                    },
-                                    "email":{
-                                        "type":[
-                                            "null",
-                                            "string"
-                                        ]
-                                    },
-                                    "avatar":{
-                                        "type":[
-                                            "null",
-                                            "object"
-                                        ],
-                                        "properties":{
-                                            "image_url":{
-                                                "type":[
-                                                    "null",
-                                                    "string"
-                                                ]
-                                            }
-                                        },
-                                        "additionalProperties":false
-                                    },
-                                    "team_ids":{
-                                        "anyOf":[
-                                            {
-                                                "type":"array",
-                                                "items":{
-                                                    "type":"integer"
-                                                }
-                                            },
-                                            {
-                                                "type":"null"
-                                            }
-                                        ]
-                                    },
-                                    "admin_ids":{
-                                        "anyOf":[
-                                            {
-                                                "type":"array",
-                                                "items":{
-                                                    "type":"integer"
-                                                }
-                                            },
-                                            {
-                                                "type":"null"
-                                            }
-                                        ]
-                                    },
-                                    "all_of_field":{
-                                        "allOf":[
-                                            {
-                                                "type":"array",
-                                                "items":{
-                                                    "type":"integer"
-                                                }
-                                            },
-                                            {
-                                                "type":"string"
-                                            },
-                                            {
-                                                "type":"integer"
-                                            }
-                                        ]
-                                    },
-                                    "job_title":{
-                                        "type":[
-                                            "null",
-                                            "string"
-                                        ]
-                                    },
-                                    "has_inbox_seat":{
-                                        "type":[
-                                            "null",
-                                            "boolean"
-                                        ]
-                                    },
-                                    "away_mode_enabled":{
-                                        "type":[
-                                            "null",
-                                            "boolean"
-                                        ]
-                                    },
-                                    "away_mode_reassign":{
-                                        "type":[
-                                            "null",
-                                            "boolean"
-                                        ]
-                                    }
-
-                                },
-                                "additionalProperties":false
-                             }
-                             """);
+    return getTestDataFromResourceJson("schemaAnyOfAllOf.json");
   }
 
   public static JsonNode getSchemaWithFormats() {
@@ -156,59 +43,15 @@ public class BigQueryDenormalizedTestDataUtils {
   }
 
   public static JsonNode getAnyOfFormats() {
-    return Jsons.deserialize("""
-                             {
-                                 "id": "ID",
-                                 "name": "Andrii",
-                                 "type": "some_type",
-                                 "email": "email@email.com",
-                                 "avatar": {
-                                    "image_url": "url_to_avatar.jpg"
-                                 },
-                                 "team_ids": {
-                                     "big_query_array": [1, 2, 3],
-                                     "big_query_null": null
-                                 },
-                                 "admin_ids": {
-                                     "big_query_array": [],
-                                     "big_query_null": null
-                                 },
-                                 "all_of_field": {
-                                     "big_query_array": [4, 5, 6],
-                                     "big_query_string": "Some text",
-                                     "big_query_integer": 42
-                                 },
-                                 "job_title": "title",
-                                 "has_inbox_seat": true,
-                                 "away_mode_enabled": false,
-                                 "away_mode_reassign": false
-                             }
-                             """);
+    return getTestDataFromResourceJson("dataAnyOfFormats.json");
   }
 
   public static JsonNode getAnyOfFormatsWithNull() {
-    return Jsons.deserialize("""
-                             {
-                                 "name": "Mukola",
-                                 "team_ids": null,
-                                 "all_of_field": null,
-                                 "avatar": null
-                             }
-                             """);
+    return getTestDataFromResourceJson("dataAnyOfFormatsWithNull.json");
   }
 
   public static JsonNode getAnyOfFormatsWithEmptyList() {
-    return Jsons.deserialize("""
-                             {
-                                 "name": "Sergii",
-                                 "team_ids": [],
-                                 "all_of_field": {
-                                     "big_query_array": [4, 5, 6],
-                                     "big_query_string": "Some text",
-                                     "big_query_integer": 42
-                                 }
-                             }
-                             """);
+    return getTestDataFromResourceJson("dataAnyOfFormatsWithEmptyList.json");
   }
 
   public static JsonNode getDataWithJSONDateTimeFormats() {
