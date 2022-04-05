2f850b98ac1ba11aef00974642dbffd03fbf8ce8
Yurii Bidiuk <35812734+yurii-bidiuk@users.noreply.github.com>
2022-04-05 14:04:02 +0300

    DAT: verify that a destination is able to write any ISO8601-compliant date string (#9816)
    * added format_reference.txt, edited edge_case_messages.txt, edge_case_catalog.json
    
    * updated due to comments
    
    * fix message order for destiantion acceptance tests
    
    * remove unused import
    
    * handle date-time messages for destiantion acceptance tests
    
    * refactoring
    
    * format code
    
    * remove non iso8601 date
    
    * add comments
    
    * fixed pubsub tests
    
    * add test date with microseconds
    
    * fix comments
    
    * fixed PR remarks
    
    * fix assertion for nested objects
    
    * format code
    
    * fix remarks
    
    * format code
    
    * add additional check to convert only datetime values
    
    * changed key structure for saved date-time fieds
    
    * format code
    
    * add comments
    
    * refactored iteration
    
    * format code
    
    * pubsub bump version
    
    Co-authored-by: Oleksandr Bazarnov <oleksandr.bazarnov@globallogic.com>

--- airbyte-integrations/connectors/destination-gcs/src/test-integration/java/io/airbyte/integrations/destination/gcs/GcsAvroDestinationAcceptanceTest.java
+++ airbyte-integrations/connectors/destination-gcs/src/test-integration/java/io/airbyte/integrations/destination/gcs/GcsAvroDestinationAcceptanceTest.java
@@ -4,17 +4,25 @@
 
 package io.airbyte.integrations.destination.gcs;
 
+import static io.airbyte.integrations.standardtest.destination.DateTimeUtils.DATE;
+import static io.airbyte.integrations.standardtest.destination.DateTimeUtils.DATE_TIME;
+
 import com.amazonaws.services.s3.model.S3Object;
 import com.amazonaws.services.s3.model.S3ObjectSummary;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectReader;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import io.airbyte.commons.json.Jsons;
 import io.airbyte.integrations.destination.s3.S3Format;
 import io.airbyte.integrations.destination.s3.avro.AvroConstants;
 import io.airbyte.integrations.destination.s3.avro.JsonFieldNameUpdater;
 import io.airbyte.integrations.destination.s3.util.AvroRecordHelper;
+import io.airbyte.integrations.standardtest.destination.DateTimeUtils;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 import org.apache.avro.file.DataFileReader;
 import org.apache.avro.file.SeekableByteArrayInput;
 import org.apache.avro.generic.GenericData;
@@ -63,6 +71,47 @@ public class GcsAvroDestinationAcceptanceTest extends GcsDestinationAcceptanceTe
     }
 
     return jsonRecords;
+  }
+
+  @Override
+  public boolean requiresDateTimeConversionForSync() {
+    return true;
+  }
+
+  @Override
+  public void convertDateTime(ObjectNode data, Map<String, String> dateTimeFieldNames) {
+    for (String path : dateTimeFieldNames.keySet()) {
+      if (!data.at(path).isMissingNode() && DateTimeUtils.isDateTimeValue(data.at(path).asText())) {
+        var pathFields = new ArrayList<>(Arrays.asList(path.split("/")));
+        pathFields.remove(0); // first element always empty string
+        // if pathFields.size() == 1 -> /field else /field/nestedField..
+        var pathWithoutLastField = pathFields.size() == 1 ? "/" + pathFields.get(0)
+            : "/" + String.join("/", pathFields.subList(0, pathFields.size() - 1));
+        switch (dateTimeFieldNames.get(path)) {
+          case DATE_TIME -> {
+            if (pathFields.size() == 1) {
+              data.put(pathFields.get(0).toLowerCase(),
+                  (DateTimeUtils.getEpochMicros(data.get(pathFields.get(0)).asText()) / 1000)
+                      * 1000);
+            } else {
+              ((ObjectNode) data.at(pathWithoutLastField)).put(
+                  pathFields.get(pathFields.size() - 1),
+                  (DateTimeUtils.getEpochMicros(data.at(path).asText()) / 1000) * 1000);
+            }
+          }
+          case DATE -> {
+            if (pathFields.size() == 1) {
+              data.put(pathFields.get(0).toLowerCase(),
+                  DateTimeUtils.getEpochDay(data.get(pathFields.get(0)).asText()));
+            } else {
+              ((ObjectNode) data.at(pathWithoutLastField)).put(
+                  pathFields.get(pathFields.size() - 1),
+                  DateTimeUtils.getEpochDay((data.at(path).asText())));
+            }
+          }
+        }
+      }
+    }
   }
 
 }
--- airbyte-integrations/connectors/destination-s3/src/test-integration/java/io/airbyte/integrations/destination/s3/S3ParquetDestinationAcceptanceTest.java
+++ airbyte-integrations/connectors/destination-s3/src/test-integration/java/io/airbyte/integrations/destination/s3/S3ParquetDestinationAcceptanceTest.java
@@ -4,22 +4,35 @@
 
 package io.airbyte.integrations.destination.s3;
 
+import static io.airbyte.integrations.standardtest.destination.DateTimeUtils.DATE;
+import static io.airbyte.integrations.standardtest.destination.DateTimeUtils.DATE_TIME;
+
 import com.amazonaws.services.s3.model.S3Object;
 import com.amazonaws.services.s3.model.S3ObjectSummary;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectReader;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import io.airbyte.commons.json.Jsons;
 import io.airbyte.integrations.destination.s3.avro.AvroConstants;
 import io.airbyte.integrations.destination.s3.avro.JsonFieldNameUpdater;
 import io.airbyte.integrations.destination.s3.parquet.S3ParquetWriter;
 import io.airbyte.integrations.destination.s3.util.AvroRecordHelper;
+import io.airbyte.integrations.standardtest.destination.DateTimeUtils;
+import io.airbyte.protocol.models.AirbyteMessage;
+import io.airbyte.protocol.models.AirbyteMessage.Type;
+import io.airbyte.protocol.models.AirbyteRecordMessage;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import org.apache.avro.generic.GenericData;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.parquet.avro.AvroReadSupport;
 import org.apache.parquet.hadoop.ParquetReader;
@@ -69,6 +82,70 @@ public class S3ParquetDestinationAcceptanceTest extends S3DestinationAcceptanceT
     }
 
     return jsonRecords;
+  }
+
+  @Override
+  public boolean requiresDateTimeConversionForSync() {
+    return true;
+  }
+
+  @Override
+  public void convertDateTime(ObjectNode data, Map<String, String> dateTimeFieldNames) {
+    for (String path : dateTimeFieldNames.keySet()) {
+      if (!data.at(path).isMissingNode() && DateTimeUtils.isDateTimeValue(data.at(path).asText())) {
+        var pathFields = new ArrayList<>(Arrays.asList(path.split("/")));
+        pathFields.remove(0); // first element always empty string
+        // if pathFields.size() == 1 -> /field else /field/nestedField..
+        var pathWithoutLastField = pathFields.size() == 1 ? "/" + pathFields.get(0)
+            : "/" + String.join("/", pathFields.subList(0, pathFields.size() - 1));
+        switch (dateTimeFieldNames.get(path)) {
+          case DATE_TIME -> {
+            if (pathFields.size() == 1) {
+              data.put(pathFields.get(0).toLowerCase(),
+                  (DateTimeUtils.getEpochMicros(data.get(pathFields.get(0)).asText()) / 1000)
+                      * 1000);
+            } else {
+              ((ObjectNode) data.at(pathWithoutLastField)).put(
+                  pathFields.get(pathFields.size() - 1),
+                  (DateTimeUtils.getEpochMicros(data.at(path).asText()) / 1000) * 1000);
+            }
+          }
+          case DATE -> {
+            if (pathFields.size() == 1) {
+              data.put(pathFields.get(0).toLowerCase(),
+                  DateTimeUtils.getEpochDay(data.get(pathFields.get(0)).asText()));
+            } else {
+              ((ObjectNode) data.at(pathWithoutLastField)).put(pathFields.get(pathFields.size() - 1),
+                  DateTimeUtils.getEpochDay((data.at(path).asText())));
+            }
+          }
+        }
+      }
+    }
+  }
+
+  @Override
+  protected void deserializeNestedObjects(List<AirbyteMessage> messages, List<AirbyteRecordMessage> actualMessages) {
+    for (AirbyteMessage message : messages) {
+      if (message.getType() == Type.RECORD) {
+        var iterator = message.getRecord().getData().fieldNames();
+        while (iterator.hasNext()) {
+          var fieldName = iterator.next();
+          if (message.getRecord().getData().get(fieldName).isContainerNode()) {
+            message.getRecord().getData().get(fieldName).fieldNames().forEachRemaining(f -> {
+              var data = message.getRecord().getData().get(fieldName).get(f);
+              var wrappedData = String.format("{\"%s\":%s,\"_airbyte_additional_properties\":null}", f,
+                  dateTimeFieldNames.containsKey(f) || !data.isTextual() ? data.asText() : StringUtils.wrap(data.asText(), "\""));
+              try {
+                ((ObjectNode) message.getRecord().getData()).set(fieldName, new ObjectMapper().readTree(wrappedData));
+              } catch (JsonProcessingException e) {
+                e.printStackTrace();
+              }
+            });
+          }
+        }
+      }
+    }
   }
 
 }
