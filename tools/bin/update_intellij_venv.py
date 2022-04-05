e858aae9a9c63c393083d57095bd1d011113e8af
Alexandre Girard <alexandre@airbyte.io>
2022-04-05 11:00:19 -0700

    Update

--- tools/bin/update_intellij_venv.py
+++ tools/bin/update_intellij_venv.py
@@ -3,14 +3,14 @@ import os
 import xml.etree.ElementTree as ET
 
 
-def add_venv_to_xml_root(module: str, xml_root):
+def add_venv_to_xml_root(module: str, module_full_path: str, python_version: str, xml_root):
     table = xml_root.find("component")
     jdk_node = ET.SubElement(table, 'jdk', {"version": "2"})
-    ET.SubElement(jdk_node, "name", {"value": f"Python ({module})"})
+    ET.SubElement(jdk_node, "name", {"value": f"{python_version} ({module})"})
     ET.SubElement(jdk_node, "type", {"value": "Python SDK"})
-    ET.SubElement(jdk_node, "version", {"value": "Python"})
+    ET.SubElement(jdk_node, "version", {"value": f"{python_version}"})
     ET.SubElement(jdk_node, "homePath",
-                  {"value": f"$USER_HOME$/code/airbyte/airbyte-integrations/connectors/{module}/.venv/bin/python"})
+                  {"value": f"{module_full_path}/.venv/bin/python"})
 
     roots = ET.SubElement(jdk_node, "roots")
     annotationsPath = ET.SubElement(roots, "annotationsPath")
@@ -20,7 +20,7 @@ def add_venv_to_xml_root(module: str, xml_root):
     classPathRoot = ET.SubElement(classPath, "root", {"type": "composite"})
 
     ET.SubElement(classPathRoot, "root", {"url":
-                                              f'file://$USER_HOME$/code/airbyte/airbyte-integrations/connectors/{module}/.venv/lib/python3.8/site-packages',
+                                              f'file://{module_full_path}/.venv/lib/{python_version}/site-packages',
                                           "type": "simple"
                                           })
 
@@ -29,10 +29,11 @@ def create_parser():
     parser = argparse.ArgumentParser(description='Process some integers.')
     group = parser.add_mutually_exclusive_group(required=True)
     group.add_argument('-modules', nargs='?', help='Modules to add')
-    group.add_argument('--all-modules', nargs='?')
+    group.add_argument('--all-modules', action='store_true')
 
-    parser.add_argument("-input", help="Path to input jdk table")
+    parser.add_argument("-input", required=True, help="Path to input jdk table")
     parser.add_argument("-output", help="Path to output jdk table")
+    parser.add_argument("-python", required=True, help="Python version")
 
     parser.add_argument('-airbyte', help='Path to Airbyte root directory')
     return parser
@@ -42,23 +43,28 @@ if __name__ == "__main__":
     parser = create_parser()
     args = parser.parse_args()
 
-    path_to_modules = f"{args.airbyte}/airbyte-integrations/connectors/"
+    path_to_connectors = f"{args.airbyte}/airbyte-integrations/connectors/"
+
+    output_path = args.output
+    if output_path is None:
+        output_path = args.input
 
     if args.all_modules:
-        print(path_to_modules)
-        modules = next(os.walk(path_to_modules))[1]
+        print(path_to_connectors)
+        modules = next(os.walk(path_to_connectors))[1]
     else:
         modules = args.modules.split(",")
     with open(args.input, 'r') as f:
         root = ET.fromstring(f.read())
 
         for module in modules:
-            path_to_requirements_file = f"{path_to_modules}{module}/requirements.txt"
+            path_to_module = f"{path_to_connectors}{module}"
+            path_to_requirements_file = f"{path_to_module}/requirements.txt"
             requirements_file_exists = os.path.exists(path_to_requirements_file)
             if requirements_file_exists:
                 print(f"Adding {module} to jdk table")
-                add_venv_to_xml_root(module, root)
+                add_venv_to_xml_root(module, path_to_module, args.python, root)
             else:
                 print(f"Skipping {module}")
-        with open(args.output, 'w') as fout:
+        with open(output_path, 'w') as fout:
             fout.write(ET.tostring(root, encoding="unicode"))
