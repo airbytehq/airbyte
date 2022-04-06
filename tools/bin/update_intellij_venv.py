47db4bb66f72ebb690ec2ceb309dd4eddbbf6c21
Alexandre Girard <alexandre@airbyte.io>
2022-04-06 13:15:39 -0700

    add install venv option flag

--- tools/bin/update_intellij_venv.py
+++ tools/bin/update_intellij_venv.py
@@ -36,7 +36,7 @@ def get_output_path(input_path, output_path):
 
 
 def create_parser():
-    parser = argparse.ArgumentParser(description='')
+    parser = argparse.ArgumentParser(description="TODO")
     parser.add_argument("-python", required=True, help="Python version")
     group = parser.add_mutually_exclusive_group(required=True)
     group.add_argument('-modules', nargs='?', help='Modules to add')
@@ -46,6 +46,8 @@ def create_parser():
     parser.add_argument("-intellij-version", help="Intellij version to use")
     parser.add_argument("-output", help="Path to output jdk table")
     parser.add_argument(INTELLIJ_VERSION_FLAG, help="Instance of IntelliJ to update")
+    parser.add_argument("--install-venv", action="store_true", help="TODO")
+    parser.add_argument("--update-intellij", action="store_true", help="TODO")
 
     parser.add_argument('-airbyte', default=f"{os.path.dirname(__file__)}/../..", help='Path to Airbyte root directory')
     return parser
@@ -91,20 +93,28 @@ if __name__ == "__main__":
         modules = next(os.walk(path_to_connectors))[1]
     else:
         modules = args.modules.split(",")
-    with open(input_path, 'r') as f:
-        root = ET.fromstring(f.read())
 
+    if args.install_venv:
         for module in modules:
-            path_to_module = f"{path_to_connectors}{module}"
-            path_to_requirements_file = f"{path_to_module}/requirements.txt"
-            requirements_file_exists = os.path.exists(path_to_requirements_file)
-            if requirements_file_exists:
-                print(f"Adding {module} to jdk table")
-                add_venv_to_xml_root(module, path_to_module, args.python, root)
-            else:
-                print(f"Skipping {module}")
-        with open(output_path, 'w') as fout:
-            fout.write(ET.tostring(root, encoding="unicode"))
+            pass
+            # rc = subprocess.call("sleep.sh")
+            # TODO
+    if args.update_intellij:
+        with open(input_path, 'r') as f:
+            root = ET.fromstring(f.read())
+
+            for module in modules:
+                path_to_module = f"{path_to_connectors}{module}"
+                path_to_requirements_file = f"{path_to_module}/requirements.txt"
+                requirements_file_exists = os.path.exists(path_to_requirements_file)
+                if requirements_file_exists:
+                    print(f"Adding {module} to jdk table")
+                    add_venv_to_xml_root(module, path_to_module, args.python, root)
+                else:
+                    print(f"Skipping {module}")
+            with open(output_path, 'w') as fout:
+                fout.write(ET.tostring(root, encoding="unicode"))
+    print("Done running")
 
 
 # --- tests ---
