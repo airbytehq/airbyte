a3cf720e58869730f967d5ba7f0e01bc0c1af62e
airbyte <leon@brigad.co>
2022-04-07 14:58:05 +0000

    fix: import

--- airbyte-integrations/connectors/source-breezy/source_breezy/source.py
+++ airbyte-integrations/connectors/source-breezy/source_breezy/source.py
@@ -23,7 +23,7 @@ from airbyte_cdk.models import (
 )
 import requests
 from airbyte_cdk.sources import AbstractSource
-from airbyte_cdk.sources.streams import Stream
+from airbyte_cdk.sources.streams import Stream, IncrementalMixin
 from airbyte_cdk.sources.streams.http import HttpStream
 from airbyte_cdk.sources.streams.http.auth import TokenAuthenticator
 
