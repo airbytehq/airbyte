42c0c3c1ded0f310162b63c348bc50d99689c421
Serhii Chvaliuk <grubberr@gmail.com>
2022-04-07 10:07:24 +0300

    Update airbyte-integrations/bases/source-acceptance-test/source_acceptance_test/tests/test_core.py
    Co-authored-by: Sherif A. Nada <snadalive@gmail.com>

--- airbyte-integrations/bases/source-acceptance-test/source_acceptance_test/tests/test_core.py
+++ airbyte-integrations/bases/source-acceptance-test/source_acceptance_test/tests/test_core.py
@@ -102,7 +102,7 @@ class TestSpec(BaseTest):
             const_common_prop = const_common_props.pop()
             for n, variant in enumerate(variants):
                 prop_obj = variant["properties"][const_common_prop]
-                assert "default" not in prop_obj, f"There should not be 'default' keyword in common property {oneof_path}[{n}].{const_common_prop}. {docs_msg}"
+                assert "default" not in prop_obj, f"There should not be 'default' keyword in common property {oneof_path}[{n}].{const_common_prop}. Use `const` instead. {docs_msg}."
                 assert "enum" not in prop_obj, f"There should not be 'enum' keyword in common property {oneof_path}[{n}].{const_common_prop}. {docs_msg}"
 
     def test_required(self):
