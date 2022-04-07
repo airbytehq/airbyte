158668515143a941c3ca46198856ae8f2ab13283
LiRen Tu <tuliren.git@outlook.com>
2022-04-07 16:59:19 -0700

    🎉 BigQuery destination: use serialized buffer for gcs staging (#11776)
    * Rebase bigquery changes to master
    
    * Add comments
    
    * Uncomment test code
    
    * Format code
    
    * Bump versions
    
    * Fix denormalized destination target table name
    
    * Fix avro schema for denormalized destination
    
    * Remove unnecessary params from consumer factory
    
    * Add back previous version
    
    * Add warning about standard mode
    
    * auto-bump connector version
    
    * Bump version for bigquery in seed
    
    Co-authored-by: Octavia Squidington III <octavia-squidington-iii@users.noreply.github.com>

--- airbyte-integrations/connectors/destination-bigquery/src/main/java/io/airbyte/integrations/destination/bigquery/uploader/BigQueryDirectUploader.java
+++ airbyte-integrations/connectors/destination-bigquery/src/main/java/io/airbyte/integrations/destination/bigquery/uploader/BigQueryDirectUploader.java
@@ -10,24 +10,20 @@ import io.airbyte.integrations.destination.bigquery.formatter.BigQueryRecordForm
 import io.airbyte.integrations.destination.bigquery.writer.BigQueryTableWriter;
 import io.airbyte.protocol.models.AirbyteMessage;
 import java.util.function.Consumer;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class BigQueryDirectUploader extends AbstractBigQueryUploader<BigQueryTableWriter> {
 
-  private static final Logger LOGGER = LoggerFactory.getLogger(BigQueryDirectUploader.class);
-
-  public BigQueryDirectUploader(TableId table,
-                                TableId tmpTable,
-                                BigQueryTableWriter writer,
-                                JobInfo.WriteDisposition syncMode,
-                                BigQuery bigQuery,
-                                BigQueryRecordFormatter recordFormatter) {
+  public BigQueryDirectUploader(final TableId table,
+                                final TableId tmpTable,
+                                final BigQueryTableWriter writer,
+                                final JobInfo.WriteDisposition syncMode,
+                                final BigQuery bigQuery,
+                                final BigQueryRecordFormatter recordFormatter) {
     super(table, tmpTable, writer, syncMode, bigQuery, recordFormatter);
   }
 
   @Override
-  protected void uploadData(Consumer<AirbyteMessage> outputRecordCollector, AirbyteMessage lastStateMessage) throws Exception {
+  protected void uploadData(final Consumer<AirbyteMessage> outputRecordCollector, final AirbyteMessage lastStateMessage) throws Exception {
     BigQueryUtils.waitForJobFinish(writer.getWriteChannel().getJob());
     super.uploadData(outputRecordCollector, lastStateMessage);
   }
