4977024eae59f6804b8c6b3a21b22ac98c240183
swyx <shawnthe1@gmail.com>
2022-04-11 11:05:06 -0700

    [docs] fix edit link url

--- docusaurus/docusaurus.config.js
+++ docusaurus/docusaurus.config.js
@@ -32,7 +32,7 @@ const config = {
                 docs: {
                     routeBasePath: '/',
                     sidebarPath: require.resolve('./sidebars.js'),
-                    editUrl: 'https://github.com/airbytehq/airbyte/docs',
+                    editUrl: 'https://github.com/airbytehq/airbyte/blob/master/docs',
                     path: '../docs'
                 },
                 blog: false,
