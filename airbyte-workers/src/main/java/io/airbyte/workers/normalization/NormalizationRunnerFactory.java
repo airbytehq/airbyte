b23a7b74541cc4c55ed09615575fae4bb10476a7
Edward Gao <edward.gao@airbyte.io>
2022-04-14 12:20:39 -0700

    downgrade redshift (#12040)

--- airbyte-workers/src/main/java/io/airbyte/workers/normalization/NormalizationRunnerFactory.java
+++ airbyte-workers/src/main/java/io/airbyte/workers/normalization/NormalizationRunnerFactory.java
@@ -14,7 +14,8 @@ import org.apache.commons.lang3.tuple.ImmutablePair;
 public class NormalizationRunnerFactory {
 
   public static final String BASE_NORMALIZATION_IMAGE_NAME = "airbyte/normalization";
-  public static final String NORMALIZATION_VERSION = "0.1.76";
+  // pin to an older version to mitigate a bug in destination-redshift
+  public static final String NORMALIZATION_VERSION = "0.1.75";
 
   static final Map<String, ImmutablePair<String, DefaultNormalizationRunner.DestinationType>> NORMALIZATION_MAPPING =
       ImmutableMap.<String, ImmutablePair<String, DefaultNormalizationRunner.DestinationType>>builder()
