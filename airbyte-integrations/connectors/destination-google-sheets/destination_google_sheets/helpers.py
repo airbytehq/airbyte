73956251965bbca625efdee8f85ab850bd39e9e9
Oleksandr Bazarnov <oleksandr.bazarnov@globallogic.com>
2022-04-17 21:50:56 +0300

    updated docstrings

--- airbyte-integrations/connectors/destination-google-sheets/destination_google_sheets/helpers.py
+++ airbyte-integrations/connectors/destination-google-sheets/destination_google_sheets/helpers.py
@@ -4,13 +4,10 @@
 
 
 import re
-from typing import Mapping
+from typing import List
 from pygsheets import client, Spreadsheet, Worksheet
 from pygsheets.exceptions import WorksheetNotFound
-from requests import codes as status_codes
     
-def error_handler(error) -> bool:
-    return error.resp.status != status_codes.TOO_MANY_REQUESTS
 
 def get_spreadsheet_id(id_or_url: str) -> str:
     if re.match(r"(http://)|(https://)", id_or_url):
@@ -19,18 +16,12 @@ def get_spreadsheet_id(id_or_url: str) -> str:
             return m.group(2)
     else:
         return id_or_url
-    
-def values_to_str(values: list):
-    return [str(values[i]) for i in range(len(values))]
-    
-def get_record_values(record: Mapping):
-    return values_to_str(list(record.values()))
 
 def connection_test_write(client: client) -> bool:
     wks_name: str = "_airbyte_conn_test"
-    test_data = ['conn_test', 'success']
+    test_data: List[str] = ['conn_test', 'success']
     
-    def add_test_wks(client: Spreadsheet, name: str = wks_name) -> Spreadsheet:
+    def add_test_wks(client: Spreadsheet, name: str = wks_name) -> Worksheet:
         client.add_worksheet(name, rows=2, cols=1)
         return client.worksheet_by_title(name)
     
@@ -42,7 +33,7 @@ def connection_test_write(client: client) -> bool:
         value = wks.get_value('A2')
         return True if value == test_data[1] else False
     
-    def remove_test_wks(client: Spreadsheet, name: str = wks_name) -> None:
+    def remove_test_wks(client: Spreadsheet, name: str = wks_name):
         wks = client.worksheet_by_title(name)
         client.del_worksheet(wks)
         
--- airbyte-integrations/connectors/destination-google-sheets/destination_google_sheets/writer.py
+++ airbyte-integrations/connectors/destination-google-sheets/destination_google_sheets/writer.py
@@ -3,89 +3,64 @@
 #
 
 
-from typing import Mapping
-from airbyte_cdk import AirbyteLogger
-from airbyte_cdk.models import AirbyteStream
+from typing import List
+from .buffer import WriteBuffer
 from .client import GoogleSpreadsheetsClient
-from .helpers import get_record_values
 
 
-class GoogleSpreadsheetsWriter:
+class GoogleSpreadsheetsWriter(WriteBuffer):
     
-    logger = AirbyteLogger()
-
-    buffered_streams = []
-    write_buffer = []
-    _headers = []
-    flush_interval = 1000
-
     def __init__(self, client: GoogleSpreadsheetsClient):
         self.client = client
+        
+    def delete_stream_entries(self, stream_name: str):
+        """
+        Deletes all the records belonging to the input stream.
+        """
+        self.client.clean_worksheet(f"{stream_name}")
+        
+    def set_headers(self, stream_name: str, headers_list: List[str]):
+        """
+        Sets headers belonging to the input stream
+        """
+        wks = self.client.open_worksheet(f"{stream_name}")
+        wks.update_row(1, headers_list)
     
-    def buffer_stream(self, configured_stream: AirbyteStream):
-        if configured_stream:
-            self.write_buffer.append({configured_stream.stream.name: []})
-            self._headers.append(
-                {
-                    configured_stream.stream.name: sorted(list(configured_stream.stream.json_schema.get("properties").keys())),
-                    "is_set": False
-                }
-            )
-    
-    def add_to_buffer(self, stream_name: str, record: Mapping):   
-        for stream in self.write_buffer:
-            if stream_name in stream:
-                stream[stream_name].append(get_record_values(self.normalize_record(stream_name, record)))
-    
-    def buffer_has_more_records(self) -> bool:
-        for stream in self.write_buffer:
-            stream_name = list(stream.keys())[0]
-            if stream_name in stream:
-                result = True if len(stream[stream_name]) > 0 else False
-        return result
-    
-    def flush_buffer(self, stream_name: str):
-        for stream in self.write_buffer:
-            if stream_name in stream:
-                stream[stream_name].clear()
-    
-    def check_headers(self, stream_name: str):     
-        for stream in self._headers:
+    def check_headers(self, stream_name: str):
+        """
+        Checks whether data headers belonging to the input stream are set.
+        """     
+        for stream in self.stream_info:
             if stream_name in stream:
                 if not stream["is_set"]:
                     self.set_headers(stream_name, stream[stream_name])
                     stream["is_set"] = True
            
-    def delete_stream_entries(self, stream_name: str):
-        """Deletes all the records belonging to the input stream"""
-        self.client.clean_worksheet(f"{stream_name}")
-        
-    def set_headers(self, stream_name: str, headers_list: list):
-        wks = self.client.open_worksheet(f"{stream_name}")
-        wks.update_row(1, headers_list)
-
-    def normalize_record(self, stream_name: str, record: Mapping):        
-        for stream in self._headers:
-            if stream_name in stream:
-                for key in stream[stream_name]:
-                    if key not in record.keys():
-                        record.update({key: None})
-                for key in record.copy().keys():
-                    if key not in stream[stream_name]:
-                        record.pop(key)
-        return dict(sorted(record.items(), key=lambda x: x[0]))
-             
     def queue_write_operation(self, stream_name: str):
-        for stream in self.write_buffer:
+        """
+        Mimics `batch_write` operation using records_buffer.
+        
+        1) gets data from the records_buffer
+        2) writes it to the target worksheet
+        3) cleans-up the records_buffer belonging to input stream
+        """
+        for stream in self.records_buffer:
             if stream_name in stream:
                 if len(stream[stream_name]) == self.flush_interval:
                     self.write_from_queue(stream_name)
                     self.flush_buffer(stream_name)   
     
     def write_from_queue(self, stream_name: str):
+        """
+        Writes data from records_buffer for belonging to the input stream.
+        
+        1) checks the headers are set
+        2) gets the values from the records_buffer
+        3) if there are records to write - writes them to the target worksheet
+        """
         values: list = []
         self.check_headers(stream_name)
-        for stream in self.write_buffer:
+        for stream in self.records_buffer:
             if stream_name in stream:
                 values = stream[stream_name]
         if len(values) > 0:
@@ -96,7 +71,11 @@ class GoogleSpreadsheetsWriter:
             self.logger.info(f"Skipping empty stream: {stream_name}")
         
     def write_whats_left(self):
-        for stream in self.write_buffer:
+        """
+        Stands for writing records that are still left to be written,
+        but don't match the condition for `queue_write_operation`.
+        """
+        for stream in self.records_buffer:
             stream_name = list(stream.keys())[0]
             if stream_name in stream:
                 self.write_from_queue(stream_name)
