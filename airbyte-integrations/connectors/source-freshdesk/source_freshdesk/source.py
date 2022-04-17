9b8f1523bddd633cfd50b4ac765330361c322081
lgomezm <luis@calixa.io>
2022-04-17 15:23:08 -0500

    Migrated tickets and conversations

--- airbyte-integrations/connectors/source-freshdesk/source_freshdesk/source.py
+++ airbyte-integrations/connectors/source-freshdesk/source_freshdesk/source.py
@@ -2,47 +2,51 @@
 # Copyright (c) 2021 Airbyte, Inc., all rights reserved.
 #
 
-import copy
 import logging
-from typing import Any, Iterator, List, Mapping, MutableMapping, Optional, Tuple
+import requests
+from typing import Any, List, Mapping, Optional, Tuple
+from requests.auth import AuthBase, HTTPBasicAuth
 
-from airbyte_cdk.models import AirbyteMessage, ConfiguredAirbyteCatalog
 from airbyte_cdk.sources import AbstractSource
-from airbyte_cdk.sources.deprecated.base_source import ConfiguredAirbyteStream
 from airbyte_cdk.sources.streams import Stream
-from airbyte_cdk.sources.utils.schema_helpers import InternalConfig, split_config
-from airbyte_cdk.utils.event_timing import create_timer
-from requests import HTTPError
-from source_freshdesk.api import API
-from source_freshdesk.errors import FreshdeskError, FreshdeskNotFound, FreshdeskUnauthorized
-from source_freshdesk.streams import Agents, Companies, Contacts, Groups, Roles, SatisfactionRatings, Skills, TimeEntries
+from source_freshdesk.streams import Agents, Companies, Contacts, Conversations, Groups, Roles, SatisfactionRatings, Skills, Tickets, TimeEntries
 
 
 class SourceFreshdesk(AbstractSource):
+
+    def _create_authenticator(self, api_key: str) -> AuthBase:
+        return HTTPBasicAuth(username=api_key, password="unused_with_api_key")
 
     def check_connection(self, logger: logging.Logger, config: Mapping[str, Any]) -> Tuple[bool, Optional[Any]]:
         alive = True
         error_msg = None
         try:
-            api = API(domain=config['domain'], api_key=config['api_key'])
-            api.get("settings/helpdesk")
-        except (FreshdeskUnauthorized, FreshdeskNotFound):
-            alive = False
-            error_msg = "Invalid credentials"
-        except FreshdeskError as error:
+            url = f"https://{config['domain'].rstrip('/')}/api/v2/settings/helpdesk"
+            r = requests.get(url=url, auth=self._create_authenticator(config["api_key"]))
+            if not r.ok:
+                alive = False
+                try:
+                    body = r.json()
+                    error_msg = f"{body.get('code')}: {body['message']}"
+                except ValueError:
+                    error_msg = "Invalid credentials"
+        except Exception as error:
             alive = False
             error_msg = repr(error)
 
         return alive, error_msg
     
     def streams(self, config: Mapping[str, Any]) -> List[Stream]:
+        authenticator = self._create_authenticator(config["api_key"])
         return [
-            Agents(config=config),
-            Companies(config=config),
-            Contacts(config=config),
-            Groups(config=config),
-            Roles(config=config),
-            Skills(config=config),
-            TimeEntries(config=config),
-            SatisfactionRatings(config=config)
+            Agents(authenticator=authenticator, config=config),
+            Companies(authenticator=authenticator, config=config),
+            Contacts(authenticator=authenticator, config=config),
+            Conversations(authenticator=authenticator, config=config),
+            Groups(authenticator=authenticator, config=config),
+            Roles(authenticator=authenticator, config=config),
+            Skills(authenticator=authenticator, config=config),
+            TimeEntries(authenticator=authenticator, config=config),
+            Tickets(authenticator=authenticator, config=config),
+            SatisfactionRatings(authenticator=authenticator, config=config)
         ]
