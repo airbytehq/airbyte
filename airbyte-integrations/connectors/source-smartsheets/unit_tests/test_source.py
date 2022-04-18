93f58664703dc99298a46a1d8ea460d033c8e20d
Denys Davydov <denys.i.davydov@globallogic.com>
2022-04-18 16:59:08 +0300

    #5520 source smartsheets: add two unit tests

--- airbyte-integrations/connectors/source-smartsheets/unit_tests/test_source.py
+++ airbyte-integrations/connectors/source-smartsheets/unit_tests/test_source.py
@@ -22,24 +22,25 @@ def configured_catalog():
     return Mock(streams=[Mock(stream=stream_mock)])
 
 
-columns = [
+_columns = [
     {"id": "1101932201830276", "title": "id", "type": "TEXT_NUMBER"},
     {"id": "5605531829200772", "title": "first_name", "type": "TEXT_NUMBER"},
     {"id": "3353732015515524", "title": "last_name", "type": "TEXT_NUMBER"},
 ]
 
 
-cells = [
+_cells = [
     {"columnId": "1101932201830276", "value": "11"},
     {"columnId": "5605531829200772", "value": "Leonardo"},
     {"columnId": "3353732015515524", "value": "Dicaprio"},
 ]
 
 
-@pytest.mark.parametrize("row", permutations(cells))
-def test_different_cell_order_produces_one_result(mocker, row, config, catalog):
-    sheet = json.dumps({"name": "test", "totalRowCount": 3, "columns": columns, "rows": [{"cells": row}]})
+@pytest.mark.parametrize(("row", "columns"), (*((perm, _columns) for perm in permutations(_cells)), ([], _columns), ([], [])))
+def test_different_cell_order_produces_one_result(mocker, config, catalog, row, columns):
+    sheet = json.dumps({"name": "test", "totalRowCount": 3, "columns": columns, "rows": [{"cells": row}] if row else []})
     mocker.patch("smartsheet.Smartsheet", Mock(return_value=Mock(Sheets=Mock(get_sheet=Mock(return_value=sheet)))))
     source = SourceSmartsheets()
     records = [message.record.data for message in source.read(logger=Mock(), config=config, catalog=catalog, state={})]
-    assert list(records) == [{"id": "11", "first_name": "Leonardo", "last_name": "Dicaprio"}]
+    expected_records = [] if not row else [{"id": "11", "first_name": "Leonardo", "last_name": "Dicaprio"}]
+    assert list(records) == expected_records
