539c62ea2db8b2bec78b1cce3250059b39766c39
Sergey Chvalyuk <grubberr@gmail.com>
2022-04-21 01:04:42 +0300

    improve labels
    Signed-off-by: Sergey Chvalyuk <grubberr@gmail.com>

--- airbyte-integrations/connectors/source-amazon-ads/source_amazon_ads/spec.py
+++ airbyte-integrations/connectors/source-amazon-ads/source_amazon_ads/spec.py
@@ -5,6 +5,7 @@
 from typing import List
 
 from airbyte_cdk.models import AdvancedAuth, AuthFlowType, OAuthConfigSpecification
+from airbyte_cdk.sources.utils.schema_helpers import expand_refs
 from pydantic import BaseModel, Extra, Field
 from source_amazon_ads.constants import AmazonAdsRegion
 
@@ -21,47 +22,45 @@ class AmazonAdsConfig(BaseModel):
     auth_type: str = Field(default="oauth2.0", const=True, order=0)
 
     client_id: str = Field(
-        name="Client ID",
-        description=("The Client ID of your Amazon developer application."),
+        title="Client ID",
+        description="The Client ID of your Amazon Ads developer application.",
     )
     client_secret: str = Field(
-        name="Client secret",
-        description=("The Client Secret of your Amazon developer application."),
+        title="Client Secret",
+        description="The Client Secret of your Amazon Ads developer application.",
         airbyte_secret=True,
     )
 
     refresh_token: str = Field(
-        name="Oauth refresh token",
-        description=(
-            'Amazon  Ads Refresh Token. See the <a href="https://docs.airbyte.io/integrations/sources/amazon-ads">docs</a> for more information on how to obtain this token.'
-        ),
+        title="Refresh Token",
+        description='Amazon Ads Refresh Token. See the <a href="https://docs.airbyte.com/integrations/sources/amazon-ads">docs</a> for more information on how to obtain this token.',
         airbyte_secret=True,
     )
 
     start_date: str = Field(
         None,
-        name="Start date",
-        description="Start date for collecting reports, should not be more than 60 days in past. In YYYY-MM-DD format",
+        title="Start Date",
+        description="The Start date for collecting reports, should not be more than 60 days in the past. In YYYY-MM-DD format",
         examples=["2022-10-10", "2022-10-22"],
     )
 
-    region: AmazonAdsRegion = Field(name="Region", description="Region to pull data from (EU/NA/FE/SANDBOX)", default=AmazonAdsRegion.NA)
+    region: AmazonAdsRegion = Field(title="Region", description="Region to pull data from (EU/NA/FE/SANDBOX)", default=AmazonAdsRegion.NA)
 
     profiles: List[int] = Field(
         None,
-        name="Profile Ids",
+        title="Profile IDs",
         description="Profile IDs you want to fetch data for.",
     )
 
     report_wait_timeout: int = Field(
-        name="Report Wait Timeout",
+        title="Report Wait Timeout",
         description="Timeout duration in minutes for Reports. Eg. 30",
         default=30,
         examples=[30, 120],
     )
 
     report_generation_max_retries: int = Field(
-        name="Report Geration Maximum Retries",
+        title="Report Generation Maximum Retries",
         description="Maximum retries Airbyte will attempt for fetching Report Data. Eg. 5",
         default=5,
         examples=[5, 10, 15],
@@ -70,11 +69,10 @@ class AmazonAdsConfig(BaseModel):
     @classmethod
     def schema(cls, **kwargs):
         schema = super().schema(**kwargs)
+        expand_refs(schema)
         # Transform pydantic generated enum for region
-        definitions = schema.pop("definitions", None)
-        if definitions:
-            schema["properties"]["region"].update(definitions["AmazonAdsRegion"])
-            schema["properties"]["region"].pop("allOf", None)
+        schema["properties"]["region"] = {**schema["properties"]["region"]["allOf"][0], **schema["properties"]["region"]}
+        schema["properties"]["region"].pop("allOf")
         return schema
 
 
