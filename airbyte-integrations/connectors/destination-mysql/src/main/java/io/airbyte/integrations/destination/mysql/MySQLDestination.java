7ea9d10aaa0642366d764c7f24be3d6c399045ab
noahkawasaki-airbyte <noah@airbyte.io>
2022-04-20 10:56:39 -0700

    Simplify PR, reduce scope of string constants

--- airbyte-integrations/connectors/destination-mysql/src/main/java/io/airbyte/integrations/destination/mysql/MySQLDestination.java
+++ airbyte-integrations/connectors/destination-mysql/src/main/java/io/airbyte/integrations/destination/mysql/MySQLDestination.java
@@ -25,6 +25,15 @@ public class MySQLDestination extends AbstractJdbcDestination implements Destina
 
   private static final Logger LOGGER = LoggerFactory.getLogger(MySQLDestination.class);
 
+  public static final String DATABASE_KEY = "database";
+  public static final String HOST_KEY = "host";
+  public static final String JDBC_URL_KEY = "jdbc_url";
+  public static final String JDBC_URL_PARAMS_KEY = "jdbc_url_params";
+  public static final String PASSWORD_KEY = "password";
+  public static final String PORT_KEY = "port";
+  public static final String SSL_KEY = "ssl";
+  public static final String USERNAME_KEY = "username";
+
   public static final String DRIVER_CLASS = "com.mysql.cj.jdbc.Driver";
 
   static final Map<String, String> DEFAULT_JDBC_PARAMETERS = ImmutableMap.of(
@@ -104,7 +113,6 @@ public class MySQLDestination extends AbstractJdbcDestination implements Destina
     if (config.has(PASSWORD_KEY)) {
       configBuilder.put(PASSWORD_KEY, config.get(PASSWORD_KEY).asText());
     }
-
     if (config.has(JDBC_URL_PARAMS_KEY)) {
       configBuilder.put(JDBC_URL_PARAMS_KEY, config.get(JDBC_URL_PARAMS_KEY));
     }
