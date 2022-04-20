9983f1843aaecc84f15b9b4d6b5e8fe67d53ed7b
Yurii Bidiuk <35812734+yurii-bidiuk@users.noreply.github.com>
2022-04-20 16:57:15 +0300

    🐛 Destination Redshift: fix switching mode (#12085)
    * fix switching mode for redshift
    
    * bump version
    
    * format code
    
    * update spec

--- airbyte-integrations/connectors/destination-redshift/src/test-integration/java/io/airbyte/integrations/destination/redshift/RedshiftS3StagingInsertDestinationAcceptanceTest.java
+++ airbyte-integrations/connectors/destination-redshift/src/test-integration/java/io/airbyte/integrations/destination/redshift/RedshiftS3StagingInsertDestinationAcceptanceTest.java
@@ -31,8 +31,6 @@ import java.util.List;
 import org.apache.commons.lang3.RandomStringUtils;
 import org.jooq.Record;
 import org.jooq.Result;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class RedshiftS3StagingInsertDestinationAcceptanceTest extends RedshiftCopyDestinationAcceptanceTest {
@@ -45,33 +43,33 @@ public class RedshiftS3StagingInsertDestinationAcceptanceTest extends RedshiftCo
   private static final String BOOKS_STREAM_NAME = "books_" + RandomStringUtils.randomAlphabetic(5);
 
   private static final AirbyteMessage MESSAGE_BOOKS1 = new AirbyteMessage().withType(AirbyteMessage.Type.RECORD)
-          .withRecord(new AirbyteRecordMessage().withStream(BOOKS_STREAM_NAME)
-                  .withData(Jsons.jsonNode(ImmutableMap.builder().put("name", "Harry Potter").put("id", "10").build()))
-                  .withEmittedAt(NOW.toEpochMilli()));
+      .withRecord(new AirbyteRecordMessage().withStream(BOOKS_STREAM_NAME)
+          .withData(Jsons.jsonNode(ImmutableMap.builder().put("name", "Harry Potter").put("id", "10").build()))
+          .withEmittedAt(NOW.toEpochMilli()));
   private static final AirbyteMessage MESSAGE_BOOKS2 = new AirbyteMessage().withType(AirbyteMessage.Type.RECORD)
-          .withRecord(new AirbyteRecordMessage().withStream(BOOKS_STREAM_NAME)
-                  .withData(Jsons.jsonNode(ImmutableMap.builder().put("name", "The Great Gatsby").put("id", "30").build()))
-                  .withEmittedAt(NOW.toEpochMilli()));
+      .withRecord(new AirbyteRecordMessage().withStream(BOOKS_STREAM_NAME)
+          .withData(Jsons.jsonNode(ImmutableMap.builder().put("name", "The Great Gatsby").put("id", "30").build()))
+          .withEmittedAt(NOW.toEpochMilli()));
   private static final AirbyteMessage BOOKS_IN_THE_DB = new AirbyteMessage().withType(AirbyteMessage.Type.RECORD)
-          .withRecord(new AirbyteRecordMessage().withStream(BOOKS_STREAM_NAME)
-                  .withData(Jsons.jsonNode(ImmutableMap.builder().put("name", "Brave New World").put("id", "1").build()))
-                  .withEmittedAt(NOW.toEpochMilli()));
+      .withRecord(new AirbyteRecordMessage().withStream(BOOKS_STREAM_NAME)
+          .withData(Jsons.jsonNode(ImmutableMap.builder().put("name", "Brave New World").put("id", "1").build()))
+          .withEmittedAt(NOW.toEpochMilli()));
 
   private static final AirbyteMessage MESSAGE_STATE = new AirbyteMessage().withType(AirbyteMessage.Type.STATE)
-          .withState(new AirbyteStateMessage().withData(Jsons.jsonNode(ImmutableMap.builder().put("checkpoint", "now!").build())));
+      .withState(new AirbyteStateMessage().withData(Jsons.jsonNode(ImmutableMap.builder().put("checkpoint", "now!").build())));
 
   public JsonNode getStaticConfig() {
     return Jsons.deserialize(IOs.readFile(Path.of("secrets/config.json")));
   }
 
-  void setup(){
+  void setup() {
     MESSAGE_BOOKS1.getRecord().setNamespace(DATASET_ID);
     MESSAGE_BOOKS2.getRecord().setNamespace(DATASET_ID);
     catalog = new ConfiguredAirbyteCatalog().withStreams(Lists.newArrayList(
-            CatalogHelpers.createConfiguredAirbyteStream(BOOKS_STREAM_NAME, DATASET_ID,
-                            io.airbyte.protocol.models.Field.of("name", JsonSchemaType.STRING),
-                            io.airbyte.protocol.models.Field.of("id", JsonSchemaType.STRING))
-                    .withDestinationSyncMode(DestinationSyncMode.APPEND)));
+        CatalogHelpers.createConfiguredAirbyteStream(BOOKS_STREAM_NAME, DATASET_ID,
+            io.airbyte.protocol.models.Field.of("name", JsonSchemaType.STRING),
+            io.airbyte.protocol.models.Field.of("id", JsonSchemaType.STRING))
+            .withDestinationSyncMode(DestinationSyncMode.APPEND)));
   }
 
   @Test
@@ -95,9 +93,9 @@ public class RedshiftS3StagingInsertDestinationAcceptanceTest extends RedshiftCo
 
     final List<JsonNode> booksActual = retrieveRecords(testDestinationEnv, BOOKS_STREAM_NAME, DATASET_ID, config);
     final List<JsonNode> expectedUsersJson = Lists.newArrayList(
-            MESSAGE_BOOKS1.getRecord().getData(),
-            MESSAGE_BOOKS2.getRecord().getData(),
-            BOOKS_IN_THE_DB.getRecord().getData());
+        MESSAGE_BOOKS1.getRecord().getData(),
+        MESSAGE_BOOKS2.getRecord().getData(),
+        BOOKS_IN_THE_DB.getRecord().getData());
     assertEquals(expectedUsersJson.size(), booksActual.size());
     assertTrue(expectedUsersJson.containsAll(booksActual) && booksActual.containsAll(expectedUsersJson));
   }
@@ -106,18 +104,19 @@ public class RedshiftS3StagingInsertDestinationAcceptanceTest extends RedshiftCo
     // As we don't care about the previous data we just simulate the flow when previous table exists.
     database.query(q -> {
       q.fetch(String.format("CREATE SCHEMA IF NOT EXISTS %s", DATASET_ID));
-      q.fetch(String.format("CREATE TABLE IF NOT EXISTS %s.%s (%s VARCHAR PRIMARY KEY, %s VARCHAR, %s TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP)",
-              DATASET_ID,
-              streamName,
-              JavaBaseConstants.COLUMN_NAME_AB_ID,
-              JavaBaseConstants.COLUMN_NAME_DATA,
-              JavaBaseConstants.COLUMN_NAME_EMITTED_AT));
+      q.fetch(String.format(
+          "CREATE TABLE IF NOT EXISTS %s.%s (%s VARCHAR PRIMARY KEY, %s VARCHAR, %s TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP)",
+          DATASET_ID,
+          streamName,
+          JavaBaseConstants.COLUMN_NAME_AB_ID,
+          JavaBaseConstants.COLUMN_NAME_DATA,
+          JavaBaseConstants.COLUMN_NAME_EMITTED_AT));
       // Simulate existing record
       q.fetch(String.format("""
-      insert into %s.%s (_airbyte_ab_id, _airbyte_data, _airbyte_emitted_at) values  
-      ('9', '{\"id\":\"1\",\"name\":\"Brave New World\"}', '2022-02-09 12:02:13.322000 +00:00')""",
-              DATASET_ID,
-              streamName));
+                            insert into %s.%s (_airbyte_ab_id, _airbyte_data, _airbyte_emitted_at) values
+                            ('9', '{\"id\":\"1\",\"name\":\"Brave New World\"}', '2022-02-09 12:02:13.322000 +00:00')""",
+          DATASET_ID,
+          streamName));
       return null;
     });
   }
@@ -129,23 +128,26 @@ public class RedshiftS3StagingInsertDestinationAcceptanceTest extends RedshiftCo
    * @param expectedType - data type of _airbyte_data to expect
    * @return if current datatype of _airbyte_data column is expectedType.
    *
-   * PG_TABLE_DEF table Stores information about table columns.
-   * PG_TABLE_DEF only returns information about tables that are visible to the user.
+   *         PG_TABLE_DEF table Stores information about table columns. PG_TABLE_DEF only returns
+   *         information about tables that are visible to the user.
    *
-   * <a href="https://docs.aws.amazon.com/redshift/latest/dg/r_PG_TABLE_DEF.html">PG_TABLE_DEF</a>
+   *         <a href=
+   *         "https://docs.aws.amazon.com/redshift/latest/dg/r_PG_TABLE_DEF.html">PG_TABLE_DEF</a>
    *
    * @throws SQLException
    */
   private boolean isTmpTableDataColumnInExpectedType(final Database database,
-                                          final String dataSet,
-                                          final String streamName,
-                                          final String expectedType) throws SQLException {
+                                                     final String dataSet,
+                                                     final String streamName,
+                                                     final String expectedType)
+      throws SQLException {
     Result<Record> query = database.query(q -> {
       return q.fetch(String.format("""
-              set search_path to %s;
-              select type from pg_table_def where tablename = \'%s\' and "column" = \'%s\'""",
-              dataSet, streamName, JavaBaseConstants.COLUMN_NAME_DATA));
+                                   set search_path to %s;
+                                   select type from pg_table_def where tablename = \'%s\' and "column" = \'%s\'""",
+          dataSet, streamName, JavaBaseConstants.COLUMN_NAME_DATA));
     });
     return query.get(0).getValue(TYPE).toString().trim().contains(expectedType);
   }
+
 }
