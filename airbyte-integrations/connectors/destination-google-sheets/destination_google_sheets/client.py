b501815034f29987f1e943016af4816bc152b6e7
Oleksandr Bazarnov <oleksandr.bazarnov@globallogic.com>
2022-04-21 14:08:20 +0300

    increased max number of retries on HttpErrors

--- airbyte-integrations/connectors/destination-google-sheets/destination_google_sheets/client.py
+++ airbyte-integrations/connectors/destination-google-sheets/destination_google_sheets/client.py
@@ -14,8 +14,16 @@ from .auth import SCOPES, GoogleSheetsAuth
 class GoogleSheetsClient:
     def __init__(self, config: Dict):
         self.config = config
+        # max number of backoff retries
+        self.retries = 20
 
     def authorize(self) -> pygsheet_client:
         input_creds = GoogleSheetsAuth.get_credentials(self.config)
         authenticated_creds = GoogleSheetsAuth.get_authenticated_google_credentials(input_creds)
-        return pygsheets.authorize(custom_credentials=authenticated_creds, scopes=SCOPES)
+        client = pygsheets.authorize(custom_credentials=authenticated_creds, scopes=SCOPES)
+
+        # Increase max number of retries if Rate Limit is reached. Error: <HttpError 429>
+        client.drive.retries = self.retries
+        client.sheet.retries = self.retries
+
+        return client
