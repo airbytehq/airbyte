91781ea54ffecece93f6cc6ccda2d8dff5aeb513
ahmed-buksh <ahmed.buksh26@gmail.com>
2022-04-21 11:40:22 +0500

    :wrench: docker and test files added

---
+++ airbyte-integrations/connectors/source-plaid-heron/setup.py
@@ -0,0 +1,29 @@
+#
+# Copyright (c) 2021 Airbyte, Inc., all rights reserved.
+#
+
+
+from setuptools import find_packages, setup
+
+MAIN_REQUIREMENTS = [
+    "airbyte-cdk~=0.1",
+    "plaid-python"
+]
+
+TEST_REQUIREMENTS = [
+    "pytest~=6.1",
+    "source-acceptance-test",
+]
+
+setup(
+    name="source_plaid_heron",
+    description="Source implementation for Plaid Heron.",
+    author="Airbyte",
+    author_email="contact@airbyte.io",
+    packages=find_packages(),
+    install_requires=MAIN_REQUIREMENTS,
+    package_data={"": ["*.json"]},
+    extras_require={
+        "tests": TEST_REQUIREMENTS,
+    },
+)
