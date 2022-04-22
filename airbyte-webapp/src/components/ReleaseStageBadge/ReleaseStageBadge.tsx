eebaf11ad08adeabee2eb61953650c310bc4e9f5
Edmundo Ruiz Ghanem <168664+edmundito@users.noreply.github.com>
2022-04-22 09:56:07 -0400

    Show GA icon next to source/destination type (#12252)
    * Show GA icon next to source/destination type
    * Add GA icon svg
    * Consolidate values in locales related to connector stage
    * Wrap one line if statement in curly braces
    
    Co-authored-by: Krishna Glick <krishna@airbyte.io>

--- airbyte-webapp/src/components/ReleaseStageBadge/ReleaseStageBadge.tsx
+++ airbyte-webapp/src/components/ReleaseStageBadge/ReleaseStageBadge.tsx
@@ -1,6 +1,7 @@
 import { FormattedMessage } from "react-intl";
 import styled from "styled-components";
 
+import { GAIcon } from "components/icons/GAIcon";
 import ToolTip from "components/ToolTip";
 
 import { ReleaseStage } from "core/domain/connector";
@@ -26,19 +27,22 @@ interface Props {
 }
 
 export const ReleaseStageBadge: React.FC<Props> = ({ stage, small, tooltip = true }) => {
-  if (!stage || stage === ReleaseStage.GENERALLY_AVAILABLE || stage === ReleaseStage.CUSTOM) {
+  if (!stage || stage === ReleaseStage.CUSTOM) {
     return null;
   }
 
-  const badge = (
-    <Stage $small={!!small}>
-      <FormattedMessage id={`component.releaseStageBadge.${stage}.title`} />
-    </Stage>
-  );
+  const badge =
+    stage === ReleaseStage.GENERALLY_AVAILABLE ? (
+      <GAIcon />
+    ) : (
+      <Stage $small={!!small}>
+        <FormattedMessage id={`connector.releaseStage.${stage}`} />
+      </Stage>
+    );
 
   return tooltip ? (
     <ToolTip control={badge} cursor="help">
-      <FormattedMessage id={`component.releaseStageBadge.${stage}.tooltip`} />
+      <FormattedMessage id={`connector.releaseStage.${stage}.description`} />
     </ToolTip>
   ) : (
     badge
