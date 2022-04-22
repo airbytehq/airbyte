8cda7e348b06fd23d6734f6a7758f896c26dce40
Oleksandr Bazarnov <oleksandr.bazarnov@globallogic.com>
2022-04-22 16:36:02 +0300

    added some custom integration tests

--- airbyte-integrations/connectors/destination-google-sheets/integration_tests/integration_test.py
+++ airbyte-integrations/connectors/destination-google-sheets/integration_tests/integration_test.py
@@ -3,6 +3,19 @@
 #
 
 
-def integration_test():
-    # TODO write integration tests
-    pass
+import json
+import pytest
+from typing import Any, Mapping
+from airbyte_cdk import AirbyteLogger
+from airbyte_cdk.models import Status
+from destination_google_sheets.destination import DestinationGoogleSheets
+
+
+@pytest.fixture(name="config")
+def config_fixture() -> Mapping[str, Any]:
+    with open("secrets/config_oauth.json", "r") as f:
+        return json.loads(f.read())
+    
+def test_check_valid_config(config: Mapping):
+    outcome = DestinationGoogleSheets().check(AirbyteLogger(), config)
+    assert outcome.status == Status.SUCCEEDED
