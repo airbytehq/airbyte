a238214e9c63d2a3356c9314f75e185374502407
Davin Chia <davinchia@gmail.com>
2022-04-25 17:26:06 +0800

    Add dataplane column to connections table.

---
+++ airbyte-db/lib/src/main/java/io/airbyte/db/instance/configs/migrations/V0_36_2_001__AddDataPlaneColumnToConnectionTable.java
@@ -0,0 +1,27 @@
+package io.airbyte.db.instance.configs.migrations;
+
+import org.flywaydb.core.api.migration.BaseJavaMigration;
+import org.flywaydb.core.api.migration.Context;
+import org.jooq.DSLContext;
+import org.jooq.impl.DSL;
+import org.jooq.impl.SQLDataType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class V0_36_2_001__AddDataPlaneColumnToConnectionTable extends BaseJavaMigration {
+
+  private static final Logger LOGGER = LoggerFactory.getLogger(V0_36_2_001__AddDataPlaneColumnToConnectionTable.class);
+
+  @Override
+  public void migrate(final Context context) throws Exception {
+    LOGGER.info("Running migration: {}", this.getClass().getSimpleName());
+
+    // Warning: please do not use any jOOQ generated code to write a migration.
+    // As database schema changes, the generated jOOQ code can be deprecated. So
+    // old migration may not compile if there is any generated code.
+    final DSLContext ctx = DSL.using(context.getConnection());
+    ctx.alterTable("connection").addColumn(DSL.field("dataplane_label", SQLDataType.VARCHAR(256).nullable(true)))
+      .execute();
+  }
+
+}
