bad26d4838a51edd0aa2c92ad723286c13f69fa8
Edmundo Ruiz Ghanem <168664+edmundito@users.noreply.github.com>
2022-04-25 14:34:25 -0400

    Update connection replication page fields layout (#12301)
    Updates fields to all start in a new row with a max-width of 810px

--- airbyte-webapp/src/views/Connection/ConnectionForm/ConnectionForm.tsx
+++ airbyte-webapp/src/views/Connection/ConnectionForm/ConnectionForm.tsx
@@ -51,8 +51,15 @@ export const FlexRow = styled.div`
   flex-direction: row;
   justify-content: flex-start;
   align-items: flex-start;
-
   gap: 10px;
+`;
+
+export const LeftFieldCol = styled.div`
+  width: 470px;
+`;
+
+export const RightFieldCol = styled.div`
+  width: 300px;
 `;
 
 const StyledSection = styled.div`
@@ -179,45 +186,78 @@ const ConnectionForm: React.FC<ConnectionFormProps> = ({
           <Section title={<FormattedMessage id="connection.transfer" />}>
             <Field name="schedule">
               {({ field, meta }: FieldProps<ScheduleProperties>) => (
-                <ConnectorLabel
-                  nextLine
-                  error={!!meta.error && meta.touched}
-                  label={formatMessage({
-                    id: "form.frequency",
-                  })}
-                  message={formatMessage({
-                    id: "form.frequency.message",
-                  })}
-                >
-                  <DropDown
-                    {...field}
-                    error={!!meta.error && meta.touched}
-                    options={frequencies}
-                    onChange={(item) => {
-                      if (onDropDownSelect) {
-                        onDropDownSelect(item);
-                      }
-                      setFieldValue(field.name, item.value);
-                    }}
-                  />
-                </ConnectorLabel>
+                <FlexRow>
+                  <LeftFieldCol>
+                    <ConnectorLabel
+                      nextLine
+                      error={!!meta.error && meta.touched}
+                      label={formatMessage({
+                        id: "form.frequency",
+                      })}
+                      message={formatMessage({
+                        id: "form.frequency.message",
+                      })}
+                    />
+                  </LeftFieldCol>
+                  <RightFieldCol>
+                    <DropDown
+                      {...field}
+                      error={!!meta.error && meta.touched}
+                      options={frequencies}
+                      onChange={(item) => {
+                        if (onDropDownSelect) {
+                          onDropDownSelect(item);
+                        }
+                        setFieldValue(field.name, item.value);
+                      }}
+                    />
+                  </RightFieldCol>
+                </FlexRow>
               )}
             </Field>
           </Section>
           <Section title={<FormattedMessage id="connection.streams" />}>
-            <FlexRow>
-              <Field name="namespaceDefinition" component={NamespaceDefinitionField} />
-              <Field name="prefix">
-                {({ field }: FieldProps<string>) => (
-                  <ControlLabels
-                    nextLine
-                    label={formatMessage({
-                      id: "form.prefix",
-                    })}
-                    message={formatMessage({
-                      id: "form.prefix.message",
-                    })}
-                  >
+            <Field name="namespaceDefinition" component={NamespaceDefinitionField} />
+            {values.namespaceDefinition === ConnectionNamespaceDefinition.CustomFormat && (
+              <Field name="namespaceFormat">
+                {({ field, meta }: FieldProps<string>) => (
+                  <FlexRow>
+                    <LeftFieldCol>
+                      <NamespaceFormatLabel
+                        nextLine
+                        error={!!meta.error}
+                        label={<FormattedMessage id="connectionForm.namespaceFormat.title" />}
+                        message={<FormattedMessage id="connectionForm.namespaceFormat.subtitle" />}
+                      />
+                    </LeftFieldCol>
+                    <RightFieldCol>
+                      <Input
+                        {...field}
+                        error={!!meta.error}
+                        placeholder={formatMessage({
+                          id: "connectionForm.namespaceFormat.placeholder",
+                        })}
+                      />
+                    </RightFieldCol>
+                  </FlexRow>
+                )}
+              </Field>
+            )}
+            <Field name="prefix">
+              {({ field }: FieldProps<string>) => (
+                <FlexRow>
+                  <LeftFieldCol>
+                    <ControlLabels
+                      nextLine
+                      label={formatMessage({
+                        id: "form.prefix",
+                      })}
+                      message={formatMessage({
+                        id: "form.prefix.message",
+                      })}
+                    />
+                  </LeftFieldCol>
+                  <RightFieldCol>
                     <Input
                       {...field}
                       type="text"
@@ -225,30 +265,10 @@ const ConnectionForm: React.FC<ConnectionFormProps> = ({
                         id: `form.prefix.placeholder`,
                       })}
                     />
-                  </ControlLabels>
-                )}
-              </Field>
-            </FlexRow>
-            {values.namespaceDefinition === ConnectionNamespaceDefinition.CustomFormat && (
-              <Field name="namespaceFormat">
-                {({ field, meta }: FieldProps<string>) => (
-                  <NamespaceFormatLabel
-                    nextLine
-                    error={!!meta.error}
-                    label={<FormattedMessage id="connectionForm.namespaceFormat.title" />}
-                    message={<FormattedMessage id="connectionForm.namespaceFormat.subtitle" />}
-                  >
-                    <Input
-                      {...field}
-                      error={!!meta.error}
-                      placeholder={formatMessage({
-                        id: "connectionForm.namespaceFormat.placeholder",
-                      })}
-                    />
-                  </NamespaceFormatLabel>
-                )}
-              </Field>
-            )}
+                  </RightFieldCol>
+                </FlexRow>
+              )}
+            </Field>
             <Field
               name="syncCatalog.streams"
               destinationSupportedSyncModes={destDefinition.supportedDestinationSyncModes}
