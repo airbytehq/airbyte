e31f51b977f1d0daf02b43f1b32f3fc9157751c3
lgomezm <luis@calixa.io>
2022-04-24 23:49:06 -0500

    Updated test_300_page.py file

--- airbyte-integrations/connectors/source-freshdesk/unit_tests/test_300_page.py
+++ airbyte-integrations/connectors/source-freshdesk/unit_tests/test_300_page.py
@@ -3,9 +3,20 @@
 #
 
 import pytest
+from requests.auth import HTTPBasicAuth
 
 from airbyte_cdk.models import SyncMode
 from source_freshdesk.streams import Tickets
+
+
+@pytest.fixture(name="config")
+def config_fixture():
+    return {"domain": "test.freshdesk.com", "api_key": "secret_api_key", "requests_per_minute": 50, "start_date": "2002-02-10T22:21:44Z"}
+
+
+@pytest.fixture(name="authenticator")
+def authenticator_fixture(config):
+    return HTTPBasicAuth(username=config["api_key"], password="unused_with_api_key")
 
 
 @pytest.fixture(name="responses")
