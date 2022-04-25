1698709330af4e8abdcdf5ca418c87f8ca461881
Anushree Agrawal <anushree@withorb.com>
2022-04-25 14:05:21 -0700

    Format using gradlew format

--- airbyte-integrations/connectors/source-orb/unit_tests/test_incremental_streams.py
+++ airbyte-integrations/connectors/source-orb/unit_tests/test_incremental_streams.py
@@ -178,20 +178,17 @@ def test_credits_ledger_entries_transform_record(mocker):
         "customer": {
             "id": "foo-customer-id",
         },
-        "credit_block": {
-            "expiry_date": "2023-01-25T12:00:00+00:00",
-            "per_unit_cost_basis": "2.50"
-        }
+        "credit_block": {"expiry_date": "2023-01-25T12:00:00+00:00", "per_unit_cost_basis": "2.50"},
     }
 
     # Validate that calling transform record unwraps nested customer and credit block fields.
     assert stream.transform_record(ledger_entry_record) == {
-            "event_id": "foo-event-id",
-            "entry_type": "decrement",
-            "customer_id": "foo-customer-id",
-            "block_expiry_date": "2023-01-25T12:00:00+00:00",
-            "credit_block_per_unit_cost_basis": "2.50"
-        }
+        "event_id": "foo-event-id",
+        "entry_type": "decrement",
+        "customer_id": "foo-customer-id",
+        "block_expiry_date": "2023-01-25T12:00:00+00:00",
+        "credit_block_per_unit_cost_basis": "2.50",
+    }
 
 
 @responses.activate
