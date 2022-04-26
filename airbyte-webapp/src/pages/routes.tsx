1acf555ca0f4696f933945c06a72029e1d7fa2ca
Tim Roes <tim@airbyte.io>
2022-04-26 11:10:30 +0200

    Refactor tracking component to hook

--- airbyte-webapp/src/pages/routes.tsx
+++ airbyte-webapp/src/pages/routes.tsx
@@ -6,7 +6,7 @@ import { useEffectOnce } from "react-use";
 import { useConfig } from "config";
 import { Workspace } from "core/domain/workspace/Workspace";
 import { useAnalyticsIdentifyUser, useAnalyticsRegisterValues } from "hooks/services/Analytics";
-import { TrackPageAnalytics } from "hooks/services/Analytics/TrackPageAnalytics";
+import { useTrackPageAnalytics } from "hooks/services/Analytics/useTrackPageAnalytics";
 import { useApiHealthPoll } from "hooks/services/Health";
 import { useNotificationService } from "hooks/services/Notification";
 import { OnboardingServiceProvider } from "hooks/services/Onboarding";
@@ -56,7 +56,6 @@ const useAddAnalyticsContextForWorkspace = (workspace: Workspace): void => {
 const MainViewRoutes: React.FC<{ workspace: Workspace }> = ({ workspace }) => {
   return (
     <MainView>
-      <TrackPageAnalytics />
       <Routes>
         <Route path={`${RoutePaths.Destination}/*`} element={<DestinationPage />} />
         <Route path={`${RoutePaths.Source}/*`} element={<SourcesPage />} />
@@ -97,6 +96,7 @@ export const AutoSelectFirstWorkspace: React.FC<{ includePath?: boolean }> = ({ 
 const RoutingWithWorkspace: React.FC = () => {
   const workspace = useCurrentWorkspace();
   useAddAnalyticsContextForWorkspace(workspace);
+  useTrackPageAnalytics();
   useApiHealthPoll();
   useDemo();
 
