b1eba9eacf2176af4d2589c7baf4dad4c30bc20a
Subodh Kant Chaturvedi <subodh1810@gmail.com>
2022-04-26 19:18:30 +0530

    introduce persistence code for service account table (#11944)
    * implement persistence code for workspace_service_account table
    
    * update yaml
    
    * implement secret handling for workspace_service_account table (#11946)
    
    * implement secret handling for workspace_service_account table
    
    * add new line to the mock json
    
    * get rid of file
    
    * address review comments
    
    * update method name and add comment

--- airbyte-config/persistence/src/test/java/io/airbyte/config/persistence/BaseDatabaseConfigPersistenceTest.java
+++ airbyte-config/persistence/src/test/java/io/airbyte/config/persistence/BaseDatabaseConfigPersistenceTest.java
@@ -63,7 +63,7 @@ public abstract class BaseDatabaseConfigPersistenceTest {
   protected static void truncateAllTables() throws SQLException {
     database.query(ctx -> ctx
         .execute(
-            "TRUNCATE TABLE state, actor_catalog, actor_catalog_fetch_event, connection_operation, connection, operation, actor_oauth_parameter, "
+            "TRUNCATE TABLE workspace_service_account, state, actor_catalog, actor_catalog_fetch_event, connection_operation, connection, operation, actor_oauth_parameter, "
                 + "actor, actor_definition, actor_definition_workspace_grant, workspace"));
   }
 
