12fdca9a17f53f86106d2dc20a59caa7b304979f
Denis Davydov <denys.i.davydov@globallogic.com>
2022-04-27 20:41:43 +0300

    Source Smartsheets: incremental read and tests (#12077)
    * #5520 fix scrambled columns bug
    
    * #5520 source smartsheets: add changelog item
    
    * #5520 move pytest to optional setup requirements
    
    * #12003 source smartsheets: implement incremental read + tests
    
    * #12003 source smartsheet: add changelog
    
    * #12003 source smartsheets: fix merge conflict on unit tests
    
    * #12003 source smartsheets: fix startdate in spec
    
    * #12003 source smartsheets: add default start dt to spec
    
    * #12003 source smartsheets: add default start dt to spec
    
    * auto-bump connector version
    
    Co-authored-by: Octavia Squidington III <octavia-squidington-iii@users.noreply.github.com>

--- airbyte-integrations/connectors/source-smartsheets/unit_tests/test_source.py
+++ airbyte-integrations/connectors/source-smartsheets/unit_tests/test_source.py
@@ -2,45 +2,23 @@
 # Copyright (c) 2021 Airbyte, Inc., all rights reserved.
 #
 
-import json
-from itertools import permutations
+import logging
 from unittest.mock import Mock
 
 import pytest
 from source_smartsheets.source import SourceSmartsheets
+from source_smartsheets.streams import SmartsheetStream
 
 
-@pytest.fixture
-def config():
-    return {"access_token": "token", "spreadsheet_id": "id"}
-
-
-@pytest.fixture(name="catalog")
-def configured_catalog():
-    stream_mock = Mock()
-    stream_mock.name = "test"  # cannot be used in __init__
-    return Mock(streams=[Mock(stream=stream_mock)])
-
-
-_columns = [
-    {"id": "1101932201830276", "title": "id", "type": "TEXT_NUMBER"},
-    {"id": "5605531829200772", "title": "first_name", "type": "TEXT_NUMBER"},
-    {"id": "3353732015515524", "title": "last_name", "type": "TEXT_NUMBER"},
-]
-
-
-_cells = [
-    {"columnId": "1101932201830276", "value": "11"},
-    {"columnId": "5605531829200772", "value": "Leonardo"},
-    {"columnId": "3353732015515524", "value": "Dicaprio"},
-]
-
-
-@pytest.mark.parametrize(("row", "columns"), (*((perm, _columns) for perm in permutations(_cells)), ([], _columns), ([], [])))
-def test_different_cell_order_produces_one_result(mocker, config, catalog, row, columns):
-    sheet = json.dumps({"name": "test", "totalRowCount": 3, "columns": columns, "rows": [{"cells": row}] if row else []})
-    mocker.patch("smartsheet.Smartsheet", Mock(return_value=Mock(Sheets=Mock(get_sheet=Mock(return_value=sheet)))))
+@pytest.mark.parametrize("connection_status", ((True, None), (False, "Internal Server Error")))
+def test_check_connection(mocker, config, connection_status):
+    mocker.patch("source_smartsheets.source.SmartSheetAPIWrapper.check_connection", Mock(return_value=connection_status))
     source = SourceSmartsheets()
-    records = [message.record.data for message in source.read(logger=Mock(), config=config, catalog=catalog, state={})]
-    expected_records = [] if not row else [{"id": "11", "first_name": "Leonardo", "last_name": "Dicaprio"}]
-    assert list(records) == expected_records
+    assert source.check_connection(logger=logging.getLogger(), config=config) == connection_status
+
+
+def test_streams(config):
+    source = SourceSmartsheets()
+    streams_iter = iter(source.streams(config))
+    assert type(next(streams_iter)) == SmartsheetStream
+    assert next(streams_iter, None) is None
