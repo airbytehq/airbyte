9481f263fc9e31b82c13e1fac9ab86f47161691e
Edmundo Ruiz Ghanem <edmundo@airbyte.io>
2022-04-27 09:19:50 -0400

    Update Input password component visibilty button * Update layout to prevent password manager buttons from overlapping with visiblity button * Apply consistent layout to both password and non-password inputs

--- airbyte-webapp/src/components/base/Input/Input.tsx
+++ airbyte-webapp/src/components/base/Input/Input.tsx
@@ -1,6 +1,7 @@
 import { faEye, faEyeSlash } from "@fortawesome/free-regular-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React, { useState } from "react";
+import { useToggle } from "react-use";
 import styled from "styled-components";
 import { Theme } from "theme";
 
@@ -23,31 +24,38 @@ export type InputProps = {
   light?: boolean;
 } & React.InputHTMLAttributes<HTMLInputElement>;
 
-const InputComponent = styled.input<InputProps>`
-  outline: none;
+const InputContainer = styled.div<InputProps>`
   width: 100%;
-  padding: 7px 18px 7px 8px;
-  border-radius: 4px;
-  font-size: 14px;
-  line-height: 20px;
-  font-weight: normal;
-  border: 1px solid ${(props) => (props.error ? props.theme.dangerColor : props.theme.greyColor0)};
+  position: relative;
   background: ${(props) => getBackgroundColor(props)};
-  color: ${({ theme }) => theme.textColor};
-  caret-color: ${({ theme }) => theme.primaryColor};
-
-  &::placeholder {
-    color: ${({ theme }) => theme.greyColor40};
-  }
+  border: 1px solid ${(props) => (props.error ? props.theme.dangerColor : props.theme.greyColor0)};
+  border-radius: 4px;
 
   &:hover {
     background: ${({ theme, light }) => (light ? theme.whiteColor : theme.greyColor20)};
     border-color: ${(props) => (props.error ? props.theme.dangerColor : props.theme.greyColor20)};
   }
 
-  &:focus {
+  &.input-container--focused {
     background: ${({ theme, light }) => (light ? theme.whiteColor : theme.primaryColor12)};
     border-color: ${({ theme }) => theme.primaryColor};
+  }
+`;
+
+const InputComponent = styled.input<InputProps & { isPassword?: boolean }>`
+  outline: none;
+  width: ${({ isPassword }) => (isPassword ? "calc(100% - 22px)" : "100%")};
+  padding: 7px 8px 7px 8px;
+  font-size: 14px;
+  line-height: 20px;
+  font-weight: normal;
+  border: none;
+  background: none;
+  color: ${({ theme }) => theme.textColor};
+  caret-color: ${({ theme }) => theme.primaryColor};
+
+  &::placeholder {
+    color: ${({ theme }) => theme.greyColor40};
   }
 
   &:disabled {
@@ -56,34 +64,42 @@ const InputComponent = styled.input<InputProps>`
   }
 `;
 
-const Container = styled.div`
-  width: 100%;
-  position: relative;
-`;
-
 const VisibilityButton = styled(Button)`
   position: absolute;
-  right: 2px;
-  top: 7px;
+  right: 0px;
+  top: 0;
+  display: flex;
+  height: 100%;
+  width: 30px;
+  align-items: center;
+  justify-content: center;
+  border: none;
 `;
 
 const Input: React.FC<InputProps> = (props) => {
   const [isContentVisible, setIsContentVisible] = useState(false);
+  const [focused, toggleFocused] = useToggle(false);
 
-  if (props.type === "password") {
-    return (
-      <Container>
-        <InputComponent {...props} type={isContentVisible ? "text" : "password"} />
-        {props.disabled ? null : (
-          <VisibilityButton iconOnly onClick={() => setIsContentVisible(!isContentVisible)} type="button">
-            <FontAwesomeIcon icon={isContentVisible ? faEyeSlash : faEye} />
-          </VisibilityButton>
-        )}
-      </Container>
-    );
-  }
+  const isPassword = props.type === "password";
+  const isVisibilityButtonVisible = isPassword && !props.disabled;
+  const onInputFocusChange = () => toggleFocused();
 
-  return <InputComponent {...props} />;
+  return (
+    <InputContainer {...props} className={focused ? "input-container--focused" : undefined}>
+      <InputComponent
+        {...props}
+        type={!isPassword || isContentVisible ? "text" : "password"}
+        isPassword={isPassword}
+        onFocus={onInputFocusChange}
+        onBlur={onInputFocusChange}
+      />
+      {isVisibilityButtonVisible ? (
+        <VisibilityButton iconOnly onClick={() => setIsContentVisible(!isContentVisible)} type="button">
+          <FontAwesomeIcon icon={isContentVisible ? faEyeSlash : faEye} fixedWidth />
+        </VisibilityButton>
+      ) : null}
+    </InputContainer>
+  );
 };
 
 export default Input;
