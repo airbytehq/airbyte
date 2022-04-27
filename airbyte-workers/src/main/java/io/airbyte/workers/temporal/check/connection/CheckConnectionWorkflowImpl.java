8075b2c7ef48bdd51da52f704f497d8fdde39284
evantahler <evan@airbyte.io>
2022-04-27 15:25:26 -0700

    WIP scafolding for WorkerRun

--- airbyte-workers/src/main/java/io/airbyte/workers/temporal/check/connection/CheckConnectionWorkflowImpl.java
+++ airbyte-workers/src/main/java/io/airbyte/workers/temporal/check/connection/CheckConnectionWorkflowImpl.java
@@ -8,18 +8,11 @@ import io.airbyte.config.StandardCheckConnectionInput;
 import io.airbyte.config.StandardCheckConnectionOutput;
 import io.airbyte.scheduler.models.IntegrationLauncherConfig;
 import io.airbyte.scheduler.models.JobRunConfig;
-import io.airbyte.workers.temporal.TemporalUtils;
-import io.temporal.activity.ActivityOptions;
+import io.airbyte.workers.temporal.scheduling.shared.ActivityConfiguration;
 import io.temporal.workflow.Workflow;
-import java.time.Duration;
 
 public class CheckConnectionWorkflowImpl implements CheckConnectionWorkflow {
-
-  final ActivityOptions options = ActivityOptions.newBuilder()
-      .setScheduleToCloseTimeout(Duration.ofHours(1))
-      .setRetryOptions(TemporalUtils.NO_RETRY)
-      .build();
-  private final CheckConnectionActivity activity = Workflow.newActivityStub(CheckConnectionActivity.class, options);
+  private final CheckConnectionActivity activity = Workflow.newActivityStub(CheckConnectionActivity.class, ActivityConfiguration.CHECK_ACTIVITY_OPTIONS);
 
   @Override
   public StandardCheckConnectionOutput run(final JobRunConfig jobRunConfig,
