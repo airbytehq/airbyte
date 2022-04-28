dd3742eaa416b87bdd54957e0f17ebed1476d2e6
Sergey Chvalyuk <grubberr@gmail.com>
2022-04-28 22:13:05 +0300

    source-all-types added
    Signed-off-by: Sergey Chvalyuk <grubberr@gmail.com>

---
+++ airbyte-integrations/connectors/source-all-types/setup.py
@@ -0,0 +1,28 @@
+#
+# Copyright (c) 2021 Airbyte, Inc., all rights reserved.
+#
+
+
+from setuptools import find_packages, setup
+
+MAIN_REQUIREMENTS = [
+    "airbyte-cdk~=0.1",
+]
+
+TEST_REQUIREMENTS = [
+    "pytest~=6.1",
+    "source-acceptance-test",
+]
+
+setup(
+    name="source_all_types",
+    description="Source implementation for All Types.",
+    author="Airbyte",
+    author_email="contact@airbyte.io",
+    packages=find_packages(),
+    install_requires=MAIN_REQUIREMENTS,
+    package_data={"": ["*.json", "*.yaml"]},
+    extras_require={
+        "tests": TEST_REQUIREMENTS,
+    },
+)
