03f89b6d87bc52124c2110552cde47008cd8b3b6
lgomezm <luis@calixa.io>
2022-04-28 08:37:44 -0500

    Moved order of test methods

--- airbyte-integrations/connectors/source-intercom/unit_tests/unit_test.py
+++ airbyte-integrations/connectors/source-intercom/unit_tests/unit_test.py
@@ -194,6 +194,96 @@ def test_read(stream, endpoint, response, expected, requests_mock):
     assert records == expected
 
 
+def build_conversations_response_body(conversations, next_url = None):
+    return {
+        "type": "conversation.list",
+        "pages": {"next": next_url} if next_url else {},
+        "conversations": conversations
+    }
+
+
+def build_conversation_response_body(conversation_id, conversation_parts):
+    return {
+        "type": "conversation",
+        "id": conversation_id,
+        "conversation_parts": {
+            "type": "conversation_part.list",
+            "conversation_parts": conversation_parts,
+            "total_count": len(conversation_parts),
+        },
+    }
+
+
+@pytest.fixture
+def single_conversation_response():
+    return {
+        "type": "conversation",
+        "id": "151272900024304",
+        "created_at": 1647365706,
+        "updated_at": 1647366443,
+        "conversation_parts": {
+            "type": "conversation_part.list",
+            "conversation_parts": [
+                {"type": "conversation_part", "id": "13740311965"},
+                {"type": "conversation_part", "id": "13740312024"},
+            ],
+            "total_count": 2,
+        },
+    }
+
+
+@pytest.fixture
+def conversation_parts_responses():
+    return [
+        (
+            "https://api.intercom.io/conversations", 
+            build_conversations_response_body(
+                conversations=[
+                    {"id":"151272900026677","updated_at":1650988600},
+                    {"id":"151272900026666","updated_at":1650988500}
+                ],
+                next_url="https://api.intercom.io/conversations?per_page=2&page=2"
+            )
+        ),
+        (
+            "https://api.intercom.io/conversations?per_page=2&page=2",
+            build_conversations_response_body(
+                conversations=[
+                    {"id":"151272900026466","updated_at":1650988450},
+                    {"id":"151272900026680","updated_at":1650988100}, # Older than state, won't be processed
+                ]
+            )
+        ),
+        (
+            "https://api.intercom.io/conversations/151272900026677",
+            build_conversation_response_body(
+                conversation_id="151272900026677",
+                conversation_parts=[
+                    {"id": "13740311961","updated_at":1650988300},
+                    {"id": "13740311962","updated_at":1650988450}
+                ]
+            )
+        ),
+        (
+            "https://api.intercom.io/conversations/151272900026666",
+            build_conversation_response_body(
+                conversation_id="151272900026666",
+                conversation_parts=[
+                    {"id": "13740311955","updated_at":1650988150},
+                    {"id": "13740312056","updated_at":1650988500}
+                ]
+            )
+        ),
+        (
+            "https://api.intercom.io/conversations/151272900026466",
+            build_conversation_response_body(
+                conversation_id="151272900026466",
+                conversation_parts=[{"id": "13740311970","updated_at":1650988600}]
+            )
+        )
+    ]
+
+
 def test_conversation_part_has_conversation_id(requests_mock, single_conversation_response):
     """
     Test shows that conversation_part records include the `conversation_id` field.
@@ -230,92 +320,3 @@ def test_conversation_part_filtering_based_on_conversation(requests_mock, conver
         records.extend(list(conversation_parts.read_records(sync_mode=SyncMode.incremental, stream_slice=slice, stream_state=state)))
 
     assert expected_record_ids == {r["id"] for r in records}
-
-
-@pytest.fixture
-def single_conversation_response():
-    return {
-        "type": "conversation",
-        "id": "151272900024304",
-        "created_at": 1647365706,
-        "updated_at": 1647366443,
-        "conversation_parts": {
-            "type": "conversation_part.list",
-            "conversation_parts": [
-                {"type": "conversation_part", "id": "13740311965"},
-                {"type": "conversation_part", "id": "13740312024"},
-            ],
-            "total_count": 2,
-        },
-    }
-
-@pytest.fixture
-def conversation_parts_responses():
-    return [
-        (
-            "https://api.intercom.io/conversations", 
-            _conversations_response(
-                conversations=[
-                    {"id":"151272900026677","updated_at":1650988600},
-                    {"id":"151272900026666","updated_at":1650988500}
-                ],
-                next_url="https://api.intercom.io/conversations?per_page=2&page=2"
-            )
-        ),
-        (
-            "https://api.intercom.io/conversations?per_page=2&page=2",
-            _conversations_response(
-                conversations=[
-                    {"id":"151272900026466","updated_at":1650988450},
-                    {"id":"151272900026680","updated_at":1650988100}, # Older than state, won't be processed
-                ]
-            )
-        ),
-        (
-            "https://api.intercom.io/conversations/151272900026677",
-            _conversation_response(
-                conversation_id="151272900026677",
-                conversation_parts=[
-                    {"id": "13740311961","updated_at":1650988300},
-                    {"id": "13740311962","updated_at":1650988450}
-                ]
-            )
-        ),
-        (
-            "https://api.intercom.io/conversations/151272900026666",
-            _conversation_response(
-                conversation_id="151272900026666",
-                conversation_parts=[
-                    {"id": "13740311955","updated_at":1650988150},
-                    {"id": "13740312056","updated_at":1650988500}
-                ]
-            )
-        ),
-        (
-            "https://api.intercom.io/conversations/151272900026466",
-            _conversation_response(
-                conversation_id="151272900026466",
-                conversation_parts=[{"id": "13740311970","updated_at":1650988600}]
-            )
-        )
-    ]
-
-
-def  _conversations_response(conversations, next_url = None):
-    return {
-        "type": "conversation.list",
-        "pages": {"next": next_url} if next_url else {},
-        "conversations": conversations
-    }
-
-
-def _conversation_response(conversation_id, conversation_parts):
-    return {
-        "type": "conversation",
-        "id": conversation_id,
-        "conversation_parts": {
-            "type": "conversation_part.list",
-            "conversation_parts": conversation_parts,
-            "total_count": len(conversation_parts),
-        },
-    }
