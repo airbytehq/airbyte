4916f35a2a6ef7ffdfc3540f187efea5da09b72d
Liren Tu <tuliren.git@outlook.com>
2022-04-28 17:45:25 -0700

    Add comment by buffer size

--- airbyte-db/lib/src/main/java/io/airbyte/db/jdbc/streaming/FetchSizeConstants.java
+++ airbyte-db/lib/src/main/java/io/airbyte/db/jdbc/streaming/FetchSizeConstants.java
@@ -6,7 +6,10 @@ package io.airbyte.db.jdbc.streaming;
 
 public final class FetchSizeConstants {
 
-  // desired buffer size in memory to store the fetched rows
+  // The desired buffer size in memory to store the fetched rows.
+  // This size is not enforced. It is only used to calculate a proper
+  // fetch size. The max row size the connector can handle is actually
+  // limited by the heap size.
   public static final long TARGET_BUFFER_BYTE_SIZE = 200L * 1024L * 1024L; // 200 MB
   // sample size for making the first estimation of the row size
   public static final int INITIAL_SAMPLE_SIZE = 10;
