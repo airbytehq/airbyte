c559f2fa832e674f497a6a0e7df40083b2a9dabc
noahkawasaki-airbyte <noah@airbyte.io>
2022-04-29 07:08:06 -0700

    Initial commit

--- buildSrc/src/main/groovy/DockerHelpers.groovy
+++ buildSrc/src/main/groovy/DockerHelpers.groovy
@@ -18,6 +18,13 @@ class DockerHelpers {
     }
 
     static String getDevTaggedImage(projectDir, dockerfileName) {
-        return "${extractImageName(Paths.get(projectDir.absolutePath, dockerfileName).toString())}:dev"
+        def imageName = extractImageName(Paths.get(projectDir.absolutePath, dockerfileName).toString())
+        def imageVersion = extractImageVersion(Paths.get(projectDir.absolutePath, dockerfileName).toString())
+
+        if (System.getenv("BUILD_CANDIDATE_IMAGE") == "true" && System.getenv("PR_NUMBER") != null) {
+            return "${imageName}:${imageVersion}-candidate-${System.getenv("PR_NUMBER")}"
+        } else {
+            return "${imageName}:dev"
+        }
     }
 }
