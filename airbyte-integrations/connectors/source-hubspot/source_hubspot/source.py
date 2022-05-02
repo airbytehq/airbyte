86db8ff850ab615793494b20649445a2e67fe983
Alexandre Girard <alexandre@airbyte.io>
2022-05-02 10:02:00 -0700

    read workflows

--- airbyte-integrations/connectors/source-hubspot/source_hubspot/source.py
+++ airbyte-integrations/connectors/source-hubspot/source_hubspot/source.py
@@ -52,7 +52,7 @@ class SourceHubspot(AbstractSource):
         error_msg = None
         common_params = self.get_common_params(config=config)
         try:
-            contacts = Contacts(**common_params)
+            contacts = Workflows(**common_params)
             _ = contacts.properties
         except HTTPError as error:
             alive = False
