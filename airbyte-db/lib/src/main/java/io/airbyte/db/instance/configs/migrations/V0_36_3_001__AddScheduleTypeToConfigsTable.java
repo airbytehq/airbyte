773436eaf34294d253f421ee4061fa324af462b7
Topher Lubaway <asimplechris@gmail.com>
2022-05-03 14:18:58 -0500

    WIP

--- airbyte-db/lib/src/main/java/io/airbyte/db/instance/configs/migrations/V0_36_3_001__AddScheduleTypeToConfigsTable.java
+++ airbyte-db/lib/src/main/java/io/airbyte/db/instance/configs/migrations/V0_36_3_001__AddScheduleTypeToConfigsTable.java
@@ -6,15 +6,52 @@ package io.airbyte.db.instance.configs.migrations;
 
 import org.flywaydb.core.api.migration.BaseJavaMigration;
 import org.flywaydb.core.api.migration.Context;
+import org.jooq.Catalog;
 import org.jooq.DSLContext;
 import org.jooq.impl.DSL;
 import org.jooq.impl.SQLDataType;
+import org.jooq.impl.SchemaImpl;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class V0_36_3_001__AddScheduleTypeToConfigsTable extends BaseJavaMigration {
 
   private static final Logger LOGGER = LoggerFactory.getLogger(V0_36_3_001__AddScheduleTypeToConfigsTable.class);
+
+  public enum ScheduleType implements EnumType {
+
+    manual("manual"),
+    basicSchedule("basicSchedule"),
+    cron("cron"),;
+
+    private final String literal;
+
+    ScheduleType(final String literal) {
+      this.literal = literal;
+    }
+
+    @Override
+    public Catalog getCatalog() {
+      return getSchema() == null ? null : getSchema().getCatalog();
+    }
+
+    @Override
+    public Schema getSchema() {
+      return new SchemaImpl(DSL.name("public"), null);
+
+    }
+
+    @Override
+    public String getName() {
+      return "schedule_type";
+    }
+
+    @Override
+    public String getLiteral() {
+      return literal;
+    }
+
+  }
 
   @Override
   public void migrate(final Context context) throws Exception {
@@ -30,6 +67,9 @@ public class V0_36_3_001__AddScheduleTypeToConfigsTable extends BaseJavaMigratio
   public static void addPublicColumn(final DSLContext ctx) {
     ctx.alterTable("connection")
         .addColumnIfNotExists(DSL.field("scheduleType", SQLDataType.VARCHAR(256).nullable(false)))
+    ctx.createType("schedule_type").asEnum("manual", "basicSchedule", "cron").execute();
+    ctx.alterTable("connection")
+        .addColumnIfNotExists(DSL.field("schedule_type", SQLDataType.VARCHAR.asEnumDataType(ScheduleType.class).nullable(true)))
         .execute();
   }
 
