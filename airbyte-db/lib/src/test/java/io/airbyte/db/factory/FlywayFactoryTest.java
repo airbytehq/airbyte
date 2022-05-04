b286ab44090e643a181e031aca898e5e1d69f426
jdpgrailsdev <jpearlin1@gmail.com>
2022-05-04 13:35:42 -0400

    Add method for cloud

--- airbyte-db/lib/src/test/java/io/airbyte/db/factory/FlywayFactoryTest.java
+++ airbyte-db/lib/src/test/java/io/airbyte/db/factory/FlywayFactoryTest.java
@@ -22,6 +22,26 @@ public class FlywayFactoryTest extends AbstractFactoryTest {
   void testCreatingAFlywayInstance() {
     final String installedBy = "test";
     final String dbIdentifier = "test";
+    final String baselineVersion = "1.2.3";
+    final String baselineDescription = "A test baseline description";
+    final boolean baselineOnMigrate = true;
+    final String migrationFileLocation = "classpath:io/airbyte/db/instance/toys/migrations";
+    final DataSource dataSource = DatabaseConnectionHelper.createDataSource(container);
+    final Flyway flyway = FlywayFactory.create(dataSource, installedBy, dbIdentifier, baselineVersion, baselineDescription, baselineOnMigrate, migrationFileLocation);
+    assertNotNull(flyway);
+    assertTrue(flyway.getConfiguration().isBaselineOnMigrate());
+    assertEquals(baselineDescription, flyway.getConfiguration().getBaselineDescription());
+    assertEquals(baselineVersion, flyway.getConfiguration().getBaselineVersion().getVersion());
+    assertEquals(baselineOnMigrate, flyway.getConfiguration().isBaselineOnMigrate());
+    assertEquals(installedBy, flyway.getConfiguration().getInstalledBy());
+    assertEquals(String.format(FlywayFactory.MIGRATION_TABLE_FORMAT, dbIdentifier), flyway.getConfiguration().getTable());
+    assertEquals(migrationFileLocation, flyway.getConfiguration().getLocations()[0].getDescriptor());
+  }
+
+  @Test
+  void testCreatingAFlywayInstanceWithDefaults() {
+    final String installedBy = "test";
+    final String dbIdentifier = "test";
     final String migrationFileLocation = "classpath:io/airbyte/db/instance/toys/migrations";
     final DataSource dataSource = DatabaseConnectionHelper.createDataSource(container);
     final Flyway flyway = FlywayFactory.create(dataSource, installedBy, dbIdentifier, migrationFileLocation);
@@ -29,6 +49,7 @@ public class FlywayFactoryTest extends AbstractFactoryTest {
     assertTrue(flyway.getConfiguration().isBaselineOnMigrate());
     assertEquals(FlywayFactory.BASELINE_DESCRIPTION, flyway.getConfiguration().getBaselineDescription());
     assertEquals(FlywayFactory.BASELINE_VERSION, flyway.getConfiguration().getBaselineVersion().getVersion());
+    assertEquals(FlywayFactory.BASELINE_ON_MIGRATION, flyway.getConfiguration().isBaselineOnMigrate());
     assertEquals(installedBy, flyway.getConfiguration().getInstalledBy());
     assertEquals(String.format(FlywayFactory.MIGRATION_TABLE_FORMAT, dbIdentifier), flyway.getConfiguration().getTable());
     assertEquals(migrationFileLocation, flyway.getConfiguration().getLocations()[0].getDescriptor());
