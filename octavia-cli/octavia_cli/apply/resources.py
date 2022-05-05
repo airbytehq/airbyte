13364cebb09739c689c0dce408352e8483d8b560
Augustin <augustin.lafanechere@gmail.com>
2022-05-05 09:23:03 +0200

    Update octavia-cli/octavia_cli/apply/resources.py
    Co-authored-by: Lake Mossman <lake@airbyte.io>

--- octavia-cli/octavia_cli/apply/resources.py
+++ octavia-cli/octavia_cli/apply/resources.py
@@ -564,7 +564,7 @@ class Connection(BaseResource):
             "connection_id",
             "operation_ids",
             "source_catalog_id",
-        ]  # We do not allow local edition of these keys
+        ]  # We do not allow local editing of these keys
         return {k: v for k, v in self.remote_resource.to_dict().items() if k not in keys_to_filter_out}
 
 
