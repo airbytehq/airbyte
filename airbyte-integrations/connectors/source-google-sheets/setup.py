eb4976883a2f7f5560eda9960d97651a1ff627cb
Pedro Lopez <pedroslopez@me.com>
2022-05-06 18:05:47 -0400

    bump GA connectors to latest version of python cdk

--- airbyte-integrations/connectors/source-google-sheets/setup.py
+++ airbyte-integrations/connectors/source-google-sheets/setup.py
@@ -6,7 +6,7 @@
 from setuptools import find_packages, setup
 
 MAIN_REQUIREMENTS = [
-    "airbyte-cdk~=0.1",
+    "airbyte-cdk~=0.1.56",
     "backoff",
     "requests",
     "google-auth-httplib2",
