c80a505bff800395d434c281124d99bb31925c87
Augustin <augustin.lafanechere@gmail.com>
2022-05-06 18:55:22 +0200

    🐙 octavia-cli: use model serialization (#12133)

--- octavia-cli/integration_tests/test_apply/test_resources.py
+++ octavia-cli/integration_tests/test_apply/test_resources.py
@@ -2,6 +2,7 @@
 # Copyright (c) 2021 Airbyte, Inc., all rights reserved.
 #
 
+
 import pytest
 
 pytestmark = pytest.mark.integration
@@ -10,23 +11,25 @@ pytestmark = pytest.mark.integration
 def test_source_lifecycle(source):
     assert not source.was_created
     source.create()
-    source.state = source._get_state_from_file()
+    source.state = source._get_state_from_file(source.configuration_path)
     assert source.was_created
     assert not source.get_diff_with_remote_resource()
-    source.local_configuration["configuration"]["pokemon_name"] = "snorlax"
+    source.raw_configuration["configuration"]["pokemon_name"] = "snorlax"
+    source.configuration = source._deserialize_raw_configuration()
     assert 'changed from "ditto" to "snorlax"' in source.get_diff_with_remote_resource()
     source.update()
     assert not source.get_diff_with_remote_resource()
-    assert source.catalog["streams"][0]["config"]["aliasName"] == "pokemon"
+    assert source.catalog["streams"][0]["config"]["alias_name"] == "pokemon"
 
 
 def test_destination_lifecycle(destination):
     assert not destination.was_created
     destination.create()
-    destination.state = destination._get_state_from_file()
+    destination.state = destination._get_state_from_file(destination.configuration_path)
     assert destination.was_created
     assert not destination.get_diff_with_remote_resource()
-    destination.local_configuration["configuration"]["host"] = "foo"
+    destination.raw_configuration["configuration"]["host"] = "foo"
+    destination.configuration = destination._deserialize_raw_configuration()
     assert 'changed from "localhost" to "foo"' in destination.get_diff_with_remote_resource()
     destination.update()
     assert not destination.get_diff_with_remote_resource()
@@ -37,10 +40,11 @@ def test_connection_lifecycle(source, destination, connection):
     assert destination.was_created
     assert not connection.was_created
     connection.create()
-    connection.state = connection._get_state_from_file()
+    connection.state = connection._get_state_from_file(connection.configuration_path)
     assert connection.was_created
     assert not connection.get_diff_with_remote_resource()
-    connection.local_configuration["configuration"]["status"] = "inactive"
+    connection.raw_configuration["configuration"]["status"] = "inactive"
+    connection.configuration = connection._deserialize_raw_configuration()
     assert 'changed from "active" to "inactive"' in connection.get_diff_with_remote_resource()
     connection.update()
     assert not connection.get_diff_with_remote_resource()
