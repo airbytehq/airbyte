a994106bac736c3e4a1cf3c2743c02d8b5b339a4
Volodymyr Petrov <volodymyr.s.petrov@globallogic.com>
2022-05-06 15:42:29 +0300

    replace functions call in jsx with components

--- airbyte-webapp/src/components/ResetDataModal/ResetDataModal.tsx
+++ airbyte-webapp/src/components/ResetDataModal/ResetDataModal.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { FC } from "react";
 import { FormattedMessage } from "react-intl";
 import styled from "styled-components";
 
@@ -28,56 +28,56 @@ const ButtonWithMargin = styled(LoadingButton)`
   margin-right: 9px;
 `;
 
+interface IModalTextProps {
+  modalType: ModalTypes | undefined;
+}
+const ModalText: FC<IModalTextProps> = ({ modalType }) => {
+  if (modalType === ModalTypes.RESET_CHANGED_COLUMN) {
+    return <FormattedMessage id="form.changedColumns" />;
+  }
+
+  if (modalType === ModalTypes.UPDATE_SCHEMA) {
+    return <FormattedMessage id="connection.updateSchemaText" />;
+  }
+
+  return <FormattedMessage id="form.resetDataText" />;
+};
+
 const ResetDataModal: React.FC<IProps> = ({ onClose, onSubmit, modalType }) => {
   const { isLoading, startAction } = useLoadingState();
   const onSubmitBtnClick = () => startAction({ action: () => onSubmit() });
 
-  const modalText = () => {
-    if (modalType === ModalTypes.RESET_CHANGED_COLUMN) {
-      return <FormattedMessage id="form.changedColumns" />;
-    }
+  const modalTitle =
+    modalType === ModalTypes.UPDATE_SCHEMA ? (
+      <FormattedMessage id="connection.updateSchema" />
+    ) : (
+      <FormattedMessage id="form.resetData" />
+    );
 
-    if (modalType === ModalTypes.UPDATE_SCHEMA) {
-      return <FormattedMessage id="connection.updateSchemaText" />;
-    }
+  const modalCancelButtonText =
+    modalType === ModalTypes.UPDATE_SCHEMA ? (
+      <FormattedMessage id="form.cancel" />
+    ) : (
+      <FormattedMessage id="form.noNeed" />
+    );
 
-    return <FormattedMessage id="form.resetDataText" />;
-  };
-
-  const modalTitle = () => {
-    if (modalType === ModalTypes.UPDATE_SCHEMA) {
-      return <FormattedMessage id="connection.updateSchema" />;
-    }
-
-    return <FormattedMessage id="form.resetData" />;
-  };
-
-  const modalCancelButtonText = () => {
-    if (modalType === ModalTypes.UPDATE_SCHEMA) {
-      return <FormattedMessage id="form.cancel" />;
-    }
-
-    return <FormattedMessage id="form.noNeed" />;
-  };
-
-  const modalSubmitButtonText = () => {
-    if (modalType === ModalTypes.UPDATE_SCHEMA) {
-      return <FormattedMessage id="connection.updateSchema" />;
-    }
-
-    return <FormattedMessage id="form.reset" />;
-  };
+  const modalSubmitButtonText =
+    modalType === ModalTypes.UPDATE_SCHEMA ? (
+      <FormattedMessage id="connection.updateSchema" />
+    ) : (
+      <FormattedMessage id="form.reset" />
+    );
 
   return (
-    <Modal onClose={onClose} title={modalTitle()}>
+    <Modal onClose={onClose} title={modalTitle}>
       <Content>
-        {modalText()}
+        <ModalText modalType={modalType} />
         <ButtonContent>
           <ButtonWithMargin onClick={onClose} secondary disabled={isLoading}>
-            {modalCancelButtonText()}
+            {modalCancelButtonText}
           </ButtonWithMargin>
           <LoadingButton onClick={onSubmitBtnClick} isLoading={isLoading} disabled={isLoading}>
-            {modalSubmitButtonText()}
+            {modalSubmitButtonText}
           </LoadingButton>
         </ButtonContent>
       </Content>
