2b2436327fbf11efa3d03474c1e89d8d99165f91
Alexandre Girard <alexandre@airbyte.io>
2022-05-09 09:42:14 -0700

    spec test passes

--- airbyte-cdk/python/airbyte_cdk/sources/cac/configurable_source.py
+++ airbyte-cdk/python/airbyte_cdk/sources/cac/configurable_source.py
@@ -18,14 +18,14 @@ class ConfigurableSource(AbstractSource):
             path_to_spec = f"/airbyte/integration_code/{path_to_spec[1:]}"
         self._path_to_spec = path_to_spec
         self._factory = LowCodeComponentFactory()
-        self._spec = self._load_spec()
-        self._source_config = self._spec["source"]
+        self._config = self._load_config()
+        self._source_config = self._config["source"]
         self._options = self._source_config["options"]
         self._vars = self._source_config.get("vars", {})
-        self._top_level_vars = self._spec.get("vars", {})
+        self._top_level_vars = self._config.get("vars", {})
         self._all_vars = self._merge_dicts(self._vars, self._top_level_vars)
 
-    def _load_spec(self):
+    def _load_config(self):
         # TODO is it better to do package loading?
         print(f"path: {self._path_to_spec}")
         print(f"path: {os.path}")
@@ -34,7 +34,8 @@ class ConfigurableSource(AbstractSource):
             return yaml.load(f.read(), Loader=yaml.SafeLoader)
 
     def get_spec_obj(self):
-        return self._spec
+        print(f"_config: {self._config}")
+        return self._config["spec"]
 
     def check_connection(self, logger, config) -> Tuple[bool, any]:
         print("entering check_connection")
