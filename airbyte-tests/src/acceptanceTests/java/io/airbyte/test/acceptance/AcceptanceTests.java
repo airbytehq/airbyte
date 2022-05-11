0b37c504545f84187f9eed93f9a14bc79cf630aa
Evan Tahler <evan@airbyte.io>
2022-05-11 21:06:52 +0000

    Use `host.docker.internal` for acceptance testing

--- airbyte-tests/src/acceptanceTests/java/io/airbyte/test/acceptance/AcceptanceTests.java
+++ airbyte-tests/src/acceptanceTests/java/io/airbyte/test/acceptance/AcceptanceTests.java
@@ -1549,7 +1549,8 @@ public class AcceptanceTests {
         dbConfig.put("host", "host.docker.internal");
       }
     } else {
-      dbConfig.put("host", "localhost");
+      // Updated from localhost to host.docker.internal (appears to be needed on newer docker desktop versions + M1 macs)
+      dbConfig.put("host", "host.docker.internal");
     }
 
     if (hiddenPassword) {
