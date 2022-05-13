997cd22c0af23c24ef423260e20137038df8fdbc
Alexandre Girard <alexandre@airbyte.io>
2022-05-12 17:19:58 -0700

    check_stream unit tests

--- airbyte-cdk/python/airbyte_cdk/sources/cac/checks/check_stream.py
+++ airbyte-cdk/python/airbyte_cdk/sources/cac/checks/check_stream.py
@@ -1,13 +1,10 @@
 #
 # Copyright (c) 2021 Airbyte, Inc., all rights reserved.
 #
-from typing import TYPE_CHECKING, Tuple
+from typing import Tuple
 
 from airbyte_cdk.models.airbyte_protocol import SyncMode
 from airbyte_cdk.sources.cac.checks.connection_checker import ConnectionChecker
-
-if TYPE_CHECKING:
-    pass
 
 
 class CheckStream(ConnectionChecker):
@@ -16,8 +13,10 @@ class CheckStream(ConnectionChecker):
 
     def check_connection(self, logger, config) -> Tuple[bool, any]:
         stream = self._source.streams(config)[0]
+        print(f"stream: {stream}")
         try:
             records = stream.read_records(sync_mode=SyncMode.full_refresh)
+            print(f"records: {records}")
             next(records)
             return True, None
         except Exception as error:
---
+++ airbyte-cdk/python/unit_tests/sources/cac/checks/test_check_stream.py
@@ -0,0 +1,38 @@
+#
+# Copyright (c) 2021 Airbyte, Inc., all rights reserved.
+#
+from unittest.mock import MagicMock
+
+from airbyte_cdk.sources.cac.checks.check_stream import CheckStream
+
+logger = None
+config = dict()
+
+
+def test_check_stream():
+    record = MagicMock()
+    stream = MagicMock()
+    stream.return_value.read_records.return_value = [record]
+
+    source = MagicMock()
+    source.return_value.streams.return_value = [stream]
+
+    check_stream = CheckStream(source)
+
+    connected, error = check_stream.check_connection(logger, config)
+    assert connected
+    assert error is None
+
+
+def test_check_stream_no_records():
+    stream = MagicMock()
+    stream.read_records.return_value = []
+
+    source = MagicMock()
+    source.streams.return_value = [stream]
+
+    check_stream = CheckStream(source)
+
+    connected, error = check_stream.check_connection(logger, config)
+    assert not connected
+    assert error is not None
