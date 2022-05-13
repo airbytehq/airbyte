cc3cadf669f4fc58f6ee50b30381ee402da8738e
Alexandre Girard <alexandre@airbyte.io>
2022-05-12 17:22:20 -0700

    typing

--- airbyte-cdk/python/airbyte_cdk/sources/cac/checks/check_stream.py
+++ airbyte-cdk/python/airbyte_cdk/sources/cac/checks/check_stream.py
@@ -1,17 +1,19 @@
 #
 # Copyright (c) 2021 Airbyte, Inc., all rights reserved.
 #
-from typing import Tuple
+import logging
+from typing import Any, Mapping, Tuple
 
 from airbyte_cdk.models.airbyte_protocol import SyncMode
+from airbyte_cdk.sources.abstract_source import AbstractSource
 from airbyte_cdk.sources.cac.checks.connection_checker import ConnectionChecker
 
 
 class CheckStream(ConnectionChecker):
-    def __init__(self, source):
+    def __init__(self, source: AbstractSource):
         self._source = source
 
-    def check_connection(self, logger, config) -> Tuple[bool, any]:
+    def check_connection(self, logger: logging.Logger, config: Mapping[str, Any]) -> Tuple[bool, any]:
         stream = self._source.streams(config)[0]
         try:
             records = stream.read_records(sync_mode=SyncMode.full_refresh)
