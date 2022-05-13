9a3e5a9f73804ad56b5888cf021e55c48a9fa9a6
Alexandre Girard <alexandre@airbyte.io>
2022-05-12 19:23:52 -0700

    remove unused method

--- airbyte-cdk/python/airbyte_cdk/sources/cac/retrievers/retriever.py
+++ airbyte-cdk/python/airbyte_cdk/sources/cac/retrievers/retriever.py
@@ -23,21 +23,6 @@ class Retriever(ABC):
     def stream_slices(self, *, sync_mode: SyncMode, stream_state: Mapping[str, Any] = None) -> Iterable[Optional[Mapping[str, Any]]]:
         pass
 
-    # FIXME: this method is only needed for the adapter
-    @property
-    def state_checkpoint_interval(self) -> Optional[int]:
-        """
-        Decides how often to checkpoint state (i.e: emit a STATE message). E.g: if this returns a value of 100, then state is persisted after reading
-        100 records, then 200, 300, etc.. A good default value is 1000 although your mileage may vary depending on the underlying data source.
-
-        Checkpointing a stream avoids re-reading records in the case a sync is failed or cancelled.
-
-        return None if state should not be checkpointed e.g: because records returned from the underlying data source are not returned in
-        ascending order with respect to the cursor field. This can happen if the source does not support reading records in ascending order of
-        created_at date (or whatever the cursor is). In those cases, state must only be saved once the full stream has been read.
-        """
-        return None
-
     @abstractmethod
     def get_state(self) -> MutableMapping[str, Any]:
         """State getter, should return state in form that can serialized to a string and send to the output
