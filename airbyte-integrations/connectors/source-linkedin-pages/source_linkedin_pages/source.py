0c70a5c4aceee02404d316a23f9ae0c3d93dbbc2
Jordan Scott <jscottpolevault@gmail.com>
2022-05-13 14:02:30 -0500

    next attempt

--- airbyte-integrations/connectors/source-linkedin-pages/source_linkedin_pages/source.py
+++ airbyte-integrations/connectors/source-linkedin-pages/source_linkedin_pages/source.py
@@ -21,9 +21,10 @@ class LinkedinPagesStream(HttpStream, ABC):
     url_base = "https://api.linkedin.com/v2/"
     primary_key = None
 
-    def __init__(self, config: Dict):
+    def __init__(self, config):
         super().__init__(authenticator=config.get("authenticator"))
         self.config = config
+        
 
     @property
     def org(self):
@@ -48,6 +49,18 @@ class LinkedinPagesStream(HttpStream, ABC):
 class OrganizationLookup(LinkedinPagesStream):
 
     endpoint = "organizations/35571209"
+
+    # def request_params(self, stream_state: Mapping[str, Any], **kwargs) -> MutableMapping[str, Any]:
+    #     """
+    #     Override request_params() to have the ability to accept the specific account_ids from user's configuration.
+    #     If we have list of account_ids, we need to make sure that the request_params are encoded correctly,
+    #     We will get HTTP Error 500, if we use standard requests.urlencode methods to parse parameters,
+    #     so the urlencode(..., safe=":(),") is used instead, to keep the values as they are.
+    #     """
+    #     params = super().request_params(stream_state=stream_state, **kwargs)
+    #     if self.org:
+    #         params[self.config["org_ids"]]
+    #     return params
 
 class FollowerStatistics(LinkedinPagesStream):
 
@@ -105,6 +118,8 @@ class SourceLinkedinPages(AbstractSource):
         raise Exception("incorrect input parameters")
 
     def check_connection(self, logger: AirbyteLogger, config: Mapping[str, Any]) -> Tuple[bool, any]:
+        # RUN $ python main.py check --config secrets/config.json
+
         """
         Testing connection availability for the connector.
         :: for this check method the Customer must have the "r_liteprofile" scope enabled.
@@ -119,7 +134,8 @@ class SourceLinkedinPages(AbstractSource):
             return True, None
         except Exception as e:
             return False, e
-
+        
+        # RUN: $ python main.py read --config secrets/config.json --catalog integration_tests/configured_catalog.json
 
     def streams(self, config: Mapping[str, Any]) -> List[Stream]:
         config["authenticator"] = self.get_authenticator(config)
@@ -132,3 +148,4 @@ class SourceLinkedinPages(AbstractSource):
             TotalFollowerCount(config),
             UgcPosts(config)
         ]
+        
