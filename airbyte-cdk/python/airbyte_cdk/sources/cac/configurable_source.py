2e422da02e2d694051da14a4eaef3e29cc92d2d1
Alexandre Girard <alexandre@airbyte.io>
2022-05-13 09:06:25 -0700

    delete dead code

--- airbyte-cdk/python/airbyte_cdk/sources/cac/configurable_source.py
+++ airbyte-cdk/python/airbyte_cdk/sources/cac/configurable_source.py
@@ -1,7 +1,6 @@
 #
 # Copyright (c) 2021 Airbyte, Inc., all rights reserved.
 #
-import json
 from abc import abstractmethod
 from typing import Tuple
 
@@ -15,16 +14,3 @@ class ConfigurableSource(AbstractSource):
 
     def check_connection(self, logger, config) -> Tuple[bool, any]:
         return self.connection_checker().check_connection(logger, config)
-
-    def _load_config(self):
-        # TODO is it better to do package loading?
-        with open(self._path_to_spec, "r") as f:
-            return json.loads(f.read())
-
-    def get_spec_obj(self):
-        try:
-            with open("/airbyte/integration_code/source_sendgrid/spec.json", "r") as f:
-                return json.loads(f.read())
-        except FileNotFoundError:
-            with open("./source_sendgrid/spec.json", "r") as f:
-                return json.loads(f.read())
