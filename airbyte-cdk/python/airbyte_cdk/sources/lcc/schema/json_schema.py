86f633144966106b5af2a91f56942259a9753b32
Alexandre Girard <alexandre@airbyte.io>
2022-05-16 14:43:33 -0700

    remove dead code

--- airbyte-cdk/python/airbyte_cdk/sources/lcc/schema/json_schema.py
+++ airbyte-cdk/python/airbyte_cdk/sources/lcc/schema/json_schema.py
@@ -2,26 +2,14 @@
 # Copyright (c) 2021 Airbyte, Inc., all rights reserved.
 #
 import json
-import os
 from typing import Any, Mapping
 
 from airbyte_cdk.sources.lcc.schema.schema_loader import SchemaLoader
 
 
 class JsonSchema(SchemaLoader):
-    def __init__(self, file_path, vars=None, config=None):
-        if vars is None:
-            vars = dict()
-        if config is None:
-            config = dict()
-        # Small hack until i figure out the docker issue
-        if not os.path.exists(file_path):
-            relative = "/".join(file_path.split("/")[-3:])
-            file_path = f"/airbyte/integration_code/{relative}"
-
+    def __init__(self, file_path):
         self._file_path = file_path
-        self._vars = vars
-        self._config = config
 
     def get_json_schema(self) -> Mapping[str, Any]:
         with open(self._file_path, "r") as f:
