ba46db5064a28b26efcb12bcf526a9741ed4ad13
andrii.leonets <aleonets@gmail.com>
2022-05-16 13:45:30 +0300

    add MySqlTestDataComparator

---
+++ airbyte-integrations/connectors/destination-mysql/src/test-integration/java/io/airbyte/integrations/destination/mysql/MySqlTestDataComparator.java
@@ -0,0 +1,24 @@
+package io.airbyte.integrations.destination.mysql;
+
+import io.airbyte.integrations.destination.ExtendedNameTransformer;
+import io.airbyte.integrations.standardtest.destination.comparator.AdvancedTestDataComparator;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MySqlTestDataComparator extends AdvancedTestDataComparator {
+
+    private final ExtendedNameTransformer namingResolver = new MySQLNameTransformer();
+
+    @Override
+    protected List<String> resolveIdentifier(final String identifier) {
+        final List<String> result = new ArrayList<>();
+        final String resolved = namingResolver.getIdentifier(identifier);
+        result.add(identifier);
+        result.add(resolved);
+        if (!resolved.startsWith("\"")) {
+            result.add(resolved.toLowerCase());
+        }
+        return result;
+    }
+}
