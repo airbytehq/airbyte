8368d2fbca560b1b359dd96d58bbd40b3bd15c52
alovew <anne@airbyte.io>
2022-05-16 13:39:23 -0700

    formatting

--- airbyte-workers/src/main/java/io/airbyte/workers/RecordSchemaValidator.java
+++ airbyte-workers/src/main/java/io/airbyte/workers/RecordSchemaValidator.java
@@ -49,7 +49,8 @@ public class RecordSchemaValidator {
     try {
       validator.ensure(matchingSchema, messageData);
     } catch (final JsonValidationException e) {
-      throw new RecordSchemaValidationException(String.format("Record schema validation failed for %s. Expected schema: %s", messageStream, matchingSchema));
+      throw new RecordSchemaValidationException(
+          String.format("Record schema validation failed for %s. Expected schema: %s", messageStream, matchingSchema));
     }
   }
 
