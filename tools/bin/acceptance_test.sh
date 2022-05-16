de570bc4a0797849128feadb187e79a7cff14a71
lmossman <lake@airbyte.io>
2022-05-16 16:35:23 -0700

    escape inner command to make it be executed at the right time

--- tools/bin/acceptance_test.sh
+++ tools/bin/acceptance_test.sh
@@ -12,7 +12,7 @@ echo "Starting app..."
 VERSION=dev TRACKING_STRATEGY=logging docker-compose up -d
 
 # Sometimes source/dest containers using airbyte volumes survive shutdown, which need to be killed in order to shut down properly.
-shutdown_cmd="docker-compose down -v || docker kill $(docker ps -a -f volume=airbyte_workspace -f volume=airbyte_data -f volume=airbyte_db -q) && docker-compose down -v"
+shutdown_cmd="docker-compose down -v || docker kill \$(docker ps -a -f volume=airbyte_workspace -f volume=airbyte_data -f volume=airbyte_db -q) && docker-compose down -v"
 trap "echo 'docker-compose logs:' && docker-compose logs -t --tail 1000 && $shutdown_cmd" EXIT
 
 echo "Waiting for services to begin"
