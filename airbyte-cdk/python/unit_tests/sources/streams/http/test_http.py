a70df3c1969d39cec5adde612c4ab35f9f4b9fd5
Pedro Lopez <pedroslopez@me.com>
2022-05-17 19:16:04 -0400

    add parse_response_error_message tests

--- airbyte-cdk/python/unit_tests/sources/streams/http/test_http.py
+++ airbyte-cdk/python/unit_tests/sources/streams/http/test_http.py
@@ -442,3 +442,34 @@ def test_send_raise_on_http_errors_logs(mocker, status_code):
         response = stream._send_request(req, {})
         stream.logger.error.assert_called_with(response.text)
         assert response.status_code == status_code
+
+
+@pytest.mark.parametrize("api_response, expected_message", [
+    ({"error": "something broke"}, "something broke"),
+    ({"error": {"message": "something broke"}}, "something broke"),
+    ({"error": "err-001", "message": "something broke"}, "something broke"),
+    ({"errors": ["one", "two", "three"]}, "one, two, three"),
+    ({"messages": ["one", "two", "three"]}, "one, two, three"),
+    ({"errors": [{"message": "one"}, {"message": "two"}, {"message": "three"}]}, "one, two, three"),
+    ({"errors": [{"error": "one"}, {"error": "two"}, {"error": "three"}]}, "one, two, three"),
+    (["one", "two", "three"], "one, two, three"),
+    ({"error": True}, None),
+    ({"something_else": "hi"}, None),
+    ({}, None)
+])
+def test_default_parse_response_error_message(api_response: dict, expected_message: Optional[str]):
+    stream = StubBasicReadHttpStream()
+    response = MagicMock()
+    response.json.return_value = api_response
+
+    message = stream.parse_response_error_message(response)
+    assert message == expected_message
+
+
+def test_default_parse_response_error_message_not_json():
+    stream = StubBasicReadHttpStream()
+    response = MagicMock()
+    response.json.side_effect = requests.exceptions.JSONDecodeError()
+
+    message = stream.parse_response_error_message(response)
+    assert message is None
