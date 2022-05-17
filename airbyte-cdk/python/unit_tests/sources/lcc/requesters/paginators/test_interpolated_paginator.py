f8361891b5838dbf32dcb06792a033b4bd97a8e1
Alexandre Girard <alexandre@airbyte.io>
2022-05-17 13:02:14 -0700

    decoder

--- airbyte-cdk/python/unit_tests/sources/lcc/requesters/paginators/test_interpolated_paginator.py
+++ airbyte-cdk/python/unit_tests/sources/lcc/requesters/paginators/test_interpolated_paginator.py
@@ -5,6 +5,7 @@
 import json
 
 import requests
+from airbyte_cdk.sources.lcc.decoders.json_decoder import JsonDecoder
 from airbyte_cdk.sources.lcc.requesters.paginators.interpolated_paginator import InterpolatedPaginator
 
 config = {"option": "OPTION"}
@@ -14,11 +15,12 @@ response.headers = {"A_HEADER": "HEADER_VALUE"}
 response_body = {"next_page_cursor": "12345"}
 response._content = json.dumps(response_body).encode("utf-8")
 last_responses = [{"id": 0}]
+decoder = JsonDecoder()
 
 
 def test_value_is_static():
     next_page_tokens = {"cursor": "a_static_value"}
-    paginator = InterpolatedPaginator(next_page_tokens, config)
+    paginator = InterpolatedPaginator(next_page_tokens, decoder, config)
 
     next_page_token = paginator.next_page_token(response, last_responses)
 
@@ -27,7 +29,7 @@ def test_value_is_static():
 
 def test_value_depends_response_body():
     next_page_tokens = {"cursor": "{{ decoded_response['next_page_cursor'] }}"}
-    paginator = InterpolatedPaginator(next_page_tokens, config)
+    paginator = InterpolatedPaginator(next_page_tokens, decoder, config)
 
     next_page_token = paginator.next_page_token(response, last_responses)
 
@@ -36,7 +38,7 @@ def test_value_depends_response_body():
 
 def test_value_depends_response_header():
     next_page_tokens = {"cursor": "{{ headers['A_HEADER'] }}"}
-    paginator = InterpolatedPaginator(next_page_tokens, config)
+    paginator = InterpolatedPaginator(next_page_tokens, decoder, config)
 
     next_page_token = paginator.next_page_token(response, last_responses)
 
@@ -45,7 +47,7 @@ def test_value_depends_response_header():
 
 def test_value_depends_on_last_responses():
     next_page_tokens = {"cursor": "{{ last_records[-1]['id'] }}"}
-    paginator = InterpolatedPaginator(next_page_tokens, config)
+    paginator = InterpolatedPaginator(next_page_tokens, decoder, config)
 
     next_page_token = paginator.next_page_token(response, last_responses)
 
@@ -54,7 +56,7 @@ def test_value_depends_on_last_responses():
 
 def test_name_is_interpolated():
     next_page_tokens = {"{{ decoded_response['next_page_cursor'] }}": "a_static_value"}
-    paginator = InterpolatedPaginator(next_page_tokens, config)
+    paginator = InterpolatedPaginator(next_page_tokens, decoder, config)
 
     next_page_token = paginator.next_page_token(response, last_responses)
 
@@ -63,7 +65,7 @@ def test_name_is_interpolated():
 
 def test_token_is_none_if_field_not_found():
     next_page_tokens = {"cursor": "{{ decoded_response['next_page_cursor'] }}"}
-    paginator = InterpolatedPaginator(next_page_tokens, config)
+    paginator = InterpolatedPaginator(next_page_tokens, decoder, config)
 
     r = requests.Response()
     r._content = json.dumps({"not_next_page_cursor": "12345"}).encode("utf-8")
