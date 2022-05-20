4e8d59728859b91f097c311fedad24661250c656
Topher Lubaway <asimplechris@gmail.com>
2022-05-20 07:40:41 -0500

    formatting

--- airbyte-db/lib/src/main/java/io/airbyte/db/instance/configs/migrations/V0_38_4_001__AddScheduleDataToConfigsTable.java
+++ airbyte-db/lib/src/main/java/io/airbyte/db/instance/configs/migrations/V0_38_4_001__AddScheduleDataToConfigsTable.java
@@ -1,13 +1,16 @@
+/*
+ * Copyright (c) 2021 Airbyte, Inc., all rights reserved.
+ */
+
 package io.airbyte.db.instance.configs.migrations;
 
 import org.flywaydb.core.api.migration.BaseJavaMigration;
 import org.flywaydb.core.api.migration.Context;
 import org.jooq.DSLContext;
-import org.jooq.impl.SQLDataType;
 import org.jooq.impl.DSL;
+import org.jooq.impl.SQLDataType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 
 public class V0_38_4_001__AddScheduleDataToConfigsTable extends BaseJavaMigration {
 
@@ -20,7 +23,6 @@ public class V0_38_4_001__AddScheduleDataToConfigsTable extends BaseJavaMigratio
     addPublicColumn(ctx);
   }
 
-
   private static void addPublicColumn(final DSLContext ctx) {
     ctx.alterTable("connection")
         .addColumnIfNotExists(DSL.field(
@@ -28,4 +30,5 @@ public class V0_38_4_001__AddScheduleDataToConfigsTable extends BaseJavaMigratio
             SQLDataType.JSONB.nullable(true)))
         .execute();
   }
+
 }
