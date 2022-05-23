996a9e0d79e565beb7f570643ceba70de34a8873
Alexandre Girard <alexandre@airbyte.io>
2022-05-23 13:40:04 -0700

    pass parameters through kwargs

--- airbyte-cdk/python/airbyte_cdk/sources/configurable/states/state.py
+++ airbyte-cdk/python/airbyte_cdk/sources/configurable/states/state.py
@@ -4,17 +4,11 @@
 
 import copy
 from abc import ABC, abstractmethod
-from typing import Any, Mapping
-
-import requests
-from airbyte_cdk.sources.configurable.types import Record
 
 
 class State(ABC):
     @abstractmethod
-    def update_state(
-        self, *stream_slice: Mapping[str, Any], stream_state: Mapping[str, Any], last_response: requests.Response, last_record: Record
-    ):
+    def update_state(self, **kwargs):
         pass
 
     @abstractmethod
