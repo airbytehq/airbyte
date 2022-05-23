9e33134191f9d38db60e2394c0099e6ca2ee9ce7
Brian Leonard <brian@bleonard.com>
2022-05-23 13:48:48 -0700

    Debug file contents

--- tools/bin/ci_integration_test.sh
+++ tools/bin/ci_integration_test.sh
@@ -67,7 +67,10 @@ show_java_run_details() {
 }
 
 write_results_summary() {
-  echo "write_results_summary"
+  file_contents=`tail -n 1000 build.out`
+  echo "write_results_summary: $file_contents"
+  echo "write_results_summary_end"
+  
   success="$1"
   python_info=`sed -n '/=* short test summary info =*/,/========/p' build.out`
   java_info=`sed -n '/tests completed,/p' build.out`
