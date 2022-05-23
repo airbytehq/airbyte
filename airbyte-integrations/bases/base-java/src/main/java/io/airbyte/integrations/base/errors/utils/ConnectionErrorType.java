27ff2be49f2c985e7e2e90a665d79d0719839577
mkhokh <mariia.khokh@globallogic.com>
2022-05-23 17:14:13 +0300

    change ConnectionErrorType

--- airbyte-integrations/bases/base-java/src/main/java/io/airbyte/integrations/base/errors/utils/ConnectionErrorType.java
+++ airbyte-integrations/bases/base-java/src/main/java/io/airbyte/integrations/base/errors/utils/ConnectionErrorType.java
@@ -11,6 +11,7 @@ public enum ConnectionErrorType {
   INCORRECT_BUCKET_NAME("Incorrect bucket name"),
   INCORRECT_HOST_OR_PORT("Incorrect host or port"),
   INCORRECT_USERNAME_OR_HOST("Incorrect username or host"),
+  INCORRECT_USERNAME_OR_PASSWORD_OR_DATABASE("Incorrect username or host or password"),
   INCORRECT_USERNAME_OR_PASSWORD_OR_DATABASE_OR_USER_ACCESS_DENIED("Some of provided parameters are incorrect: username, password, database name or user access denied"),
   INCORRECT_DB_NAME("Incorrect data base name"),
   INCORRECT_DB_NAME_OR_USER_ACCESS_DENIED("Incorrect data base name or user access denied"),
