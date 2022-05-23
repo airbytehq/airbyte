8dc2d1ff9e7c9ee41780b0e20ba1396794626982
Alexandre Girard <alexandre@airbyte.io>
2022-05-23 08:50:33 -0700

    update comments

--- airbyte-cdk/python/airbyte_cdk/sources/lcc/requesters/requester.py
+++ airbyte-cdk/python/airbyte_cdk/sources/lcc/requesters/requester.py
@@ -17,19 +17,28 @@ class HttpMethod(Enum):
 class Requester(ABC):
     @abstractmethod
     def get_authenticator(self) -> HttpAuthenticator:
+        """
+        Specifies the authenticator to use when submitting requests
+        """
         pass
 
     @abstractmethod
     def get_url_base(self) -> str:
-        pass
+        """
+        :return: URL base for the  API endpoint e.g: if you wanted to hit https://myapi.com/v1/some_entity then this should return "https://myapi.com/v1/"
+        """
 
     @abstractmethod
     def get_path(self, *, stream_state: Mapping[str, Any], stream_slice: Mapping[str, Any], next_page_token: Mapping[str, Any]) -> str:
-        pass
+        """
+        Returns the URL path for the API endpoint e.g: if you wanted to hit https://myapi.com/v1/some_entity then this should return "some_entity"
+        """
 
     @abstractmethod
     def get_method(self) -> HttpMethod:
-        pass
+        """
+        Specifies the HTTP method to use
+        """
 
     @abstractmethod
     def request_params(
@@ -38,36 +47,37 @@ class Requester(ABC):
         stream_slice: Mapping[str, Any] = None,
         next_page_token: Mapping[str, Any] = None,
     ) -> MutableMapping[str, Any]:
-        pass
+        """
+        Specifies the query parameters that should be set on an outgoing HTTP request given the inputs.
+
+        E.g: you might want to define query parameters for paging if next_page_token is not None.
+        """
 
     @property
     @abstractmethod
     def raise_on_http_errors(self) -> bool:
         """
-        Override if needed. If set to False, allows opting-out of raising HTTP code exception.
+        If set to False, allows opting-out of raising HTTP code exception.
         """
-        pass
 
     @property
     @abstractmethod
     def max_retries(self) -> Union[int, None]:
         """
-        Override if needed. Specifies maximum amount of retries for backoff policy. Return None for no limit.
+        Specifies maximum amount of retries for backoff policy. Return None for no limit.
         """
-        pass
 
     @property
     @abstractmethod
     def retry_factor(self) -> float:
         """
-        Override if needed. Specifies factor for backoff policy.
+        Specifies factor for backoff policy.
         """
-        pass
 
     @abstractmethod
     def should_retry(self, response: requests.Response) -> bool:
         """
-        Override to set different conditions for backoff based on the response from the server.
+        Specifies conditions for backoff based on the response from the server.
 
         By default, back off on the following HTTP response statuses:
          - 429 (Too Many Requests) indicating rate limiting
@@ -75,12 +85,11 @@ class Requester(ABC):
 
         Unexpected but transient exceptions (connection timeout, DNS resolution failed, etc..) are retried by default.
         """
-        pass
 
     @abstractmethod
     def backoff_time(self, response: requests.Response) -> Optional[float]:
         """
-        Override this method to dynamically determine backoff time e.g: by reading the X-Retry-After header.
+        Dynamically determine backoff time e.g: by reading the X-Retry-After header.
 
         This method is called only if should_backoff() returns True for the input request.
 
@@ -88,16 +97,14 @@ class Requester(ABC):
         :return how long to backoff in seconds. The return value may be a floating point number for subsecond precision. Returning None defers backoff
         to the default backoff behavior (e.g using an exponential algorithm).
         """
-        pass
 
     @abstractmethod
     def request_headers(
         self, stream_state: Mapping[str, Any], stream_slice: Mapping[str, Any] = None, next_page_token: Mapping[str, Any] = None
     ) -> Mapping[str, Any]:
         """
-        Override to return any non-auth headers. Authentication headers will overwrite any overlapping headers returned from this method.
+        Return any non-auth headers. Authentication headers will overwrite any overlapping headers returned from this method.
         """
-        pass
 
     @abstractmethod
     def request_body_data(
@@ -107,7 +114,7 @@ class Requester(ABC):
         next_page_token: Mapping[str, Any] = None,
     ) -> Optional[Union[Mapping, str]]:
         """
-        Override when creating POST/PUT/PATCH requests to populate the body of the request with a non-JSON payload.
+        Specifies how to populate the body of the request with a non-JSON payload.
 
         If returns a ready text that it will be sent as is.
         If returns a dict that it will be converted to a urlencoded form.
@@ -115,7 +122,6 @@ class Requester(ABC):
 
         At the same time only one of the 'request_body_data' and 'request_body_json' functions can be overridden.
         """
-        pass
 
     @abstractmethod
     def request_body_json(
@@ -125,11 +131,10 @@ class Requester(ABC):
         next_page_token: Mapping[str, Any] = None,
     ) -> Optional[Mapping]:
         """
-        Override when creating POST/PUT/PATCH requests to populate the body of the request with a JSON payload.
+        Specifies how to populate the body of the request with a JSON payload.
 
         At the same time only one of the 'request_body_data' and 'request_body_json' functions can be overridden.
         """
-        pass
 
     @abstractmethod
     def request_kwargs(
@@ -139,24 +144,21 @@ class Requester(ABC):
         next_page_token: Mapping[str, Any] = None,
     ) -> Mapping[str, Any]:
         """
-        Override to return a mapping of keyword arguments to be used when creating the HTTP request.
+        Returns a mapping of keyword arguments to be used when creating the HTTP request.
         Any option listed in https://docs.python-requests.org/en/latest/api/#requests.adapters.BaseAdapter.send for can be returned from
         this method. Note that these options do not conflict with request-level options such as headers, request params, etc..
         """
-        pass
 
     @property
     @abstractmethod
     def cache_filename(self) -> str:
         """
-        Override if needed. Return the name of cache file
+        Return the name of cache file
         """
-        pass
 
     @property
     @abstractmethod
     def use_cache(self) -> bool:
         """
-        Override if needed. If True, all records will be cached.
+        If True, all records will be cached.
         """
-        pass
