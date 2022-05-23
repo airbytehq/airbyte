b18de086317e9fdee6120895f93ebc1b31a4bd67
Jonathan Pearlin <jpearlin1@gmail.com>
2022-05-23 12:23:34 -0400

    Refactor database initialization logic (#12961)
    * Refactor database initialization logic
    
    * Formatting
    
    * Move database constants
    
    * PR feedback
    
    * PR feedback (use custom exceptions)
    
    * PR feedback
    
    * Formatting

--- airbyte-db/lib/src/main/java/io/airbyte/db/instance/DatabaseInstance.java
+++ airbyte-db/lib/src/main/java/io/airbyte/db/instance/DatabaseInstance.java
@@ -11,17 +11,29 @@ public interface DatabaseInstance {
 
   /**
    * Check is a database has been initialized.
+   *
+   * @deprecated Will be removed in future versions that separate the initialization from
+   *             creation/retrieval of an instance.
    */
+  @Deprecated(forRemoval = true)
   boolean isInitialized() throws IOException;
 
   /**
    * Get a database that has been initialized and is ready to use.
+   *
+   * @deprecated Will be replaced in future versions that separate the initialization from
+   *             creation/retrieval of an instance.
    */
+  @Deprecated(forRemoval = true)
   Database getInitialized();
 
   /**
    * Get an empty database and initialize it.
+   *
+   * @deprecated Will be replaced in future versions that separate the initialization from
+   *             creation/retrieval of an instance.
    */
+  @Deprecated(forRemoval = true)
   Database getAndInitialize() throws IOException;
 
 }
