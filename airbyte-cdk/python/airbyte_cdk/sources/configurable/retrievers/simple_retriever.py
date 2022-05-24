eaa6ad111a709e8c5dc0f01d26e36d819afc3695
Alexandre Girard <alexandre@airbyte.io>
2022-05-23 17:35:20 -0700

    leverage generator

--- airbyte-cdk/python/airbyte_cdk/sources/configurable/retrievers/simple_retriever.py
+++ airbyte-cdk/python/airbyte_cdk/sources/configurable/retrievers/simple_retriever.py
@@ -215,11 +215,10 @@ class SimpleRetriever(Retriever, HttpStream):
         stream_slice: Mapping[str, Any] = None,
         stream_state: Mapping[str, Any] = None,
     ) -> Iterable[Mapping[str, Any]]:
-        records = [r for r in HttpStream.read_records(self, sync_mode, cursor_field, stream_slice, stream_state)]
-        for r in records:
+        records_generator = HttpStream.read_records(self, sync_mode, cursor_field, stream_slice, stream_state)
+        for r in records_generator:
             self._state.update_state(stream_slice=stream_slice, stream_state=stream_state, last_response=self._last_response, last_record=r)
-        if records:
-            yield from records
+            yield r
         else:
             self._state.update_state(
                 stream_slice=stream_slice, stream_state=stream_state, last_reponse=self._last_response, last_record=None
