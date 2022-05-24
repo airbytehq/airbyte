b719c2defd5c3948e1f7e9a32759ca306a544bd8
Yevhen Sukhomud <suhomud@gmail.com>
2022-05-24 14:09:07 +0700

    11797 New SFTP source

---
+++ airbyte-integrations/connectors/source-sftp/src/main/java/io/airbyte/integrations/source/sftp/parsers/SftpFileParserFactory.java
@@ -0,0 +1,19 @@
+package io.airbyte.integrations.source.sftp.parsers;
+
+import io.airbyte.integrations.source.sftp.enums.SupportedFileExtension;
+
+public class SftpFileParserFactory {
+
+    public static SftpFileParser createInstance(SupportedFileExtension fileExtension) {
+        switch (fileExtension) {
+            case JSON -> {
+             return new JsonFileParser();
+            }
+            case CSV -> {
+                return new CsvFileParser();
+            }
+            default -> throw new RuntimeException("Unsupported file type");
+        }
+    }
+
+}
