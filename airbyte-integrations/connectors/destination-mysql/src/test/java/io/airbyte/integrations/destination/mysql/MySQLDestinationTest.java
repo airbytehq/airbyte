53ef8ddde3cba9686d5abdfbc18d68ac4a608fe4
mkhokh <mariia.khokh@globallogic.com>
2022-05-24 16:35:28 +0300

    added test for mysql destination

--- airbyte-integrations/connectors/destination-mysql/src/test/java/io/airbyte/integrations/destination/mysql/MySQLDestinationTest.java
+++ airbyte-integrations/connectors/destination-mysql/src/test/java/io/airbyte/integrations/destination/mysql/MySQLDestinationTest.java
@@ -4,19 +4,12 @@
 
 package io.airbyte.integrations.destination.mysql;
 
-import static io.airbyte.integrations.base.errors.utils.ConnectionErrorType.INCORRECT_BUCKET_NAME;
-import static io.airbyte.integrations.base.errors.utils.ConnectionErrorType.INCORRECT_DB_NAME;
-import static io.airbyte.integrations.base.errors.utils.ConnectionErrorType.INCORRECT_HOST_OR_PORT;
-import static io.airbyte.integrations.base.errors.utils.ConnectionErrorType.INCORRECT_USERNAME_OR_PASSWORD;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.google.common.collect.ImmutableMap;
 import io.airbyte.commons.json.Jsons;
 import java.util.Map;
-
-import io.airbyte.protocol.models.AirbyteConnectionStatus;
 import org.junit.jupiter.api.Test;
 
 public class MySQLDestinationTest {
@@ -81,49 +74,4 @@ public class MySQLDestinationTest {
     assertEquals(MySQLDestination.DEFAULT_SSL_JDBC_PARAMETERS, defaultProperties);
   }
 
-  @Test
-  void testCheckIncorrectPasswordFailure() {
-    var config = buildConfigNoJdbcParameters();
-    ((ObjectNode) config).put("password", "fake");
-    var destination = new MySQLDestination();
-    var actual = destination.check(config);
-    assertEquals(AirbyteConnectionStatus.Status.FAILED, actual.getStatus(), INCORRECT_USERNAME_OR_PASSWORD.getValue());
-  }
-
-  @Test
-  public void testCheckIncorrectUsernameFailure() {
-    var config = buildConfigNoJdbcParameters();
-    ((ObjectNode) config).put("username", "");
-    var destination = new MySQLDestination();
-    final AirbyteConnectionStatus actual = destination.check(config);
-    assertEquals(AirbyteConnectionStatus.Status.FAILED, actual.getStatus(), INCORRECT_USERNAME_OR_PASSWORD.getValue());
-  }
-
-  @Test
-  public void testCheckIncorrectHostFailure() {
-    var config = buildConfigNoJdbcParameters();
-    ((ObjectNode) config).put("host", "localhost2");
-    var destination = new MySQLDestination();
-    final AirbyteConnectionStatus actual = destination.check(config);
-    assertEquals(AirbyteConnectionStatus.Status.FAILED, actual.getStatus(), INCORRECT_BUCKET_NAME.getValue());
-  }
-
-  @Test
-  public void testCheckIncorrectPortFailure() {
-    var config = buildConfigNoJdbcParameters();
-    ((ObjectNode) config).put("port", "0000");
-    var destination = new MySQLDestination();
-    final AirbyteConnectionStatus actual = destination.check(config);
-    assertEquals(AirbyteConnectionStatus.Status.FAILED, actual.getStatus(), INCORRECT_HOST_OR_PORT.getValue());
-  }
-
-  @Test
-  public void testCheckIncorrectDataBaseFailure() {
-    var config = buildConfigNoJdbcParameters();
-    ((ObjectNode) config).put("database", "wrongdatabase");
-    var destination = new MySQLDestination();
-    final AirbyteConnectionStatus actual = destination.check(config);
-    assertEquals(AirbyteConnectionStatus.Status.FAILED, actual.getStatus(), INCORRECT_DB_NAME.getValue());
-  }
-
-}
+ }
