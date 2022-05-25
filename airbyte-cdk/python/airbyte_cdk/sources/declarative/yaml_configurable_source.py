31a72b1c82afb35ef2d02a0fca5a5e8eab71ee91
Alexandre Girard <alexandre@airbyte.io>
2022-05-25 09:59:01 -0700

    rename method

--- airbyte-cdk/python/airbyte_cdk/sources/declarative/yaml_configurable_source.py
+++ airbyte-cdk/python/airbyte_cdk/sources/declarative/yaml_configurable_source.py
@@ -13,7 +13,7 @@ class YamldeclarativeSource(DeclarativeSource):
     def __init__(self, path_to_yaml):
         self._factory = LowCodeComponentFactory()
         self._parser = YamlParser()
-        self._source_config = self._read_config(path_to_yaml)
+        self._source_config = self._read_and_parse_yaml_file(path_to_yaml)
 
     @property
     def connection_checker(self):
@@ -22,7 +22,7 @@ class YamldeclarativeSource(DeclarativeSource):
     def streams(self, config: Mapping[str, Any]) -> List[Stream]:
         return [self._factory.create_component(stream_config, config)() for stream_config in self._source_config["streams"]]
 
-    def _read_config(self, path_to_yaml_file):
+    def _read_and_parse_yaml_file(self, path_to_yaml_file):
         with open(path_to_yaml_file, "r") as f:
             config_content = f.read()
             return self._parser.parse(config_content)
