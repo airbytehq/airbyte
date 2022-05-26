8bca4be1df34ae2d5e1928cb1573a946e799bbe4
alovew <anne@airbyte.io>
2022-05-25 17:32:10 -0700

    change metric emitting app

--- airbyte-metrics/lib/src/main/java/io/airbyte/metrics/lib/OssMetricsRegistry.java
+++ airbyte-metrics/lib/src/main/java/io/airbyte/metrics/lib/OssMetricsRegistry.java
@@ -92,7 +92,7 @@ public enum OssMetricsRegistry implements MetricsRegistry {
   OVERALL_JOB_RUNTIME_IN_LAST_HOUR_BY_TERMINAL_STATE_SECS(MetricEmittingApps.METRICS_REPORTER,
       "overall_job_runtime_in_last_hour_by_terminal_state_secs",
       "overall job runtime - scheduling and execution for all attempts - for jobs that reach terminal states in the last hour. tagged by terminal states."),
-  NUM_RECORD_SCHEMA_VALIDATION_ERRORS(MetricEmittingApps.METRICS_REPORTER,
+  NUM_RECORD_SCHEMA_VALIDATION_ERRORS(MetricEmittingApps.WORKER,
       "record_schema_validation_error",
       "number of record schema validation errors");
 
