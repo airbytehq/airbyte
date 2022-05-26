dca2256a7ccc0108eb076e2849a476c969b958e2
Marcos Marx <marcosmarxm@users.noreply.github.com>
2022-05-26 14:00:42 -0400

    Bump 2022 license version (#13233)
    * Bump year in license short to 2022
    
    * remove protocol from cdk

--- airbyte-cdk/python/unit_tests/sources/declarative/interpolation/test_interpolated_string.py
+++ airbyte-cdk/python/unit_tests/sources/declarative/interpolation/test_interpolated_string.py
@@ -1,5 +1,5 @@
 #
-# Copyright (c) 2021 Airbyte, Inc., all rights reserved.
+# Copyright (c) 2022 Airbyte, Inc., all rights reserved.
 #
 
 from airbyte_cdk.sources.declarative.interpolation.interpolated_string import InterpolatedString
