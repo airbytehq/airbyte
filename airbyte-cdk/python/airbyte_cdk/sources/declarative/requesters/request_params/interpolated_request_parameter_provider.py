c4b0ac0cf33e84b3d60c5c14e31b4ffc710059a6
Brian Lai <51336873+brianjlai@users.noreply.github.com>
2022-05-25 22:15:19 -0400

    [Low Code CDK] add support for string interpolation for request headers (#13215)
    * add support for string interpolation for request headers + small refactor
    
    * fix formatting
    
    * move parameter provider to folder for consistency

--- airbyte-cdk/python/airbyte_cdk/sources/declarative/requesters/request_params/interpolated_request_parameter_provider.py
+++ airbyte-cdk/python/airbyte_cdk/sources/declarative/requesters/request_params/interpolated_request_parameter_provider.py
@@ -4,22 +4,15 @@
 
 from typing import Any, Mapping, MutableMapping
 
-from airbyte_cdk.sources.declarative.interpolation.interpolated_mapping import InterpolatedMapping
-from airbyte_cdk.sources.declarative.interpolation.jinja import JinjaInterpolation
+from airbyte_cdk.sources.declarative.requesters.interpolated_request_input_provider import InterpolatedRequestInputProvider
 from airbyte_cdk.sources.declarative.requesters.request_params.request_parameters_provider import RequestParameterProvider
 
 
 class InterpolatedRequestParameterProvider(RequestParameterProvider):
-    def __init__(self, *, config, request_parameters=None):
-        if request_parameters is None:
-            request_parameters = dict()
-        self._interpolation = InterpolatedMapping(request_parameters, JinjaInterpolation())
-        self._config = config
+    def __init__(self, *, config, request_parameters):
+        self._interpolator = InterpolatedRequestInputProvider(config=config, request_inputs=request_parameters)
 
     def request_params(
         self, stream_state: Mapping[str, Any], stream_slice: Mapping[str, Any] = None, next_page_token: Mapping[str, Any] = None
     ) -> MutableMapping[str, Any]:
-        kwargs = {"stream_state": stream_state, "stream_slice": stream_slice, "next_page_token": next_page_token}
-        interpolated_values = self._interpolation.eval(self._config, **kwargs)
-        non_null_tokens = {k: v for k, v in interpolated_values.items() if v}
-        return non_null_tokens
+        return self._interpolator.request_inputs(stream_state, stream_slice, next_page_token)
