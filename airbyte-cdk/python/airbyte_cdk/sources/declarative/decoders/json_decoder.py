bee1274b5a8a2fa227f468a73cf1c2b4eb610e42
Alexandre Girard <alexandre@airbyte.io>
2022-05-27 09:14:55 -0700

    Update

--- airbyte-cdk/python/airbyte_cdk/sources/declarative/decoders/json_decoder.py
+++ airbyte-cdk/python/airbyte_cdk/sources/declarative/decoders/json_decoder.py
@@ -2,16 +2,11 @@
 # Copyright (c) 2021 Airbyte, Inc., all rights reserved.
 #
 
-from typing import Mapping, Union
-
 import requests
 from airbyte_cdk.sources.declarative.decoders.decoder import Decoder
 from airbyte_cdk.sources.declarative.response import Response
 
 
 class JsonDecoder(Decoder):
-    def decode(self, response: Union[Mapping[str, str], requests.Response]) -> Response:
-        if isinstance(response, requests.Response):
-            return Response(response.json())
-        else:
-            return Response(response)
+    def decode(self, response: requests.Response) -> Response:
+        return Response(response.json())
