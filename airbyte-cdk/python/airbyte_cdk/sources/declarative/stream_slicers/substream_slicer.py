fde825a1d78990f180841abfb2227268a340ae56
Alexandre Girard <alexandre@airbyte.io>
2022-05-27 10:14:56 -0700

    substream slicer

---
+++ airbyte-cdk/python/airbyte_cdk/sources/declarative/stream_slicers/substream_slicer.py
@@ -0,0 +1,36 @@
+#
+# Copyright (c) 2022 Airbyte, Inc., all rights reserved.
+#
+from typing import Any, Iterable, Mapping
+
+from airbyte_cdk.models import SyncMode
+from airbyte_cdk.sources.declarative.interpolation.jinja import JinjaInterpolation
+from airbyte_cdk.sources.declarative.states.dict_state import DictState
+from airbyte_cdk.sources.declarative.stream_slicers.stream_slicer import StreamSlicer
+
+
+class SubstreamSlicer(StreamSlicer):
+    def __init__(self, parent_stream, state: DictState, slice_definition: Mapping[str, Any], **kwargs):
+        self._parent_stream = parent_stream
+        self._state = state
+        self._interpolation = JinjaInterpolation()
+        self._slice_definition = slice_definition
+
+    def stream_slices(self, sync_mode: SyncMode, stream_state: Mapping[str, Any]) -> Iterable[Mapping[str, Any]]:
+        for parent_stream_slice in self._parent_stream.stream_slices(sync_mode=sync_mode, cursor_field=None, stream_state=stream_state):
+            self._state.update_state(parent_stream_slice=parent_stream_slice)
+            self._state.update_state(parent_record=None)
+            empty = True
+
+            for parent_record in self._parent_stream.read_records(
+                sync_mode=SyncMode.full_refresh, cursor_field=None, stream_slice=parent_stream_slice, stream_state=None
+            ):
+                print(f"parent_record: {parent_record}")
+                empty = False
+                slice_definition = {
+                    k: self._interpolation.eval(v, None, None, parent_record=parent_record) for k, v in self._slice_definition.items()
+                }
+                self._state.update_state(parent_record=parent_record)
+                yield {**parent_stream_slice, **slice_definition}
+            if empty:
+                yield {**parent_stream_slice, **{k: None for k, v in self._slice_definition.items()}}
---
+++ airbyte-cdk/python/unit_tests/sources/declarative/stream_slicers/test_substream_slicer.py
@@ -0,0 +1,74 @@
+#
+# Copyright (c) 2022 Airbyte, Inc., all rights reserved.
+#
+from typing import Any, Iterable, List, Mapping, Optional, Union
+
+import pytest as pytest
+from airbyte_cdk.models import SyncMode
+from airbyte_cdk.sources.declarative.states.dict_state import DictState
+from airbyte_cdk.sources.declarative.stream_slicers.substream_slicer import SubstreamSlicer
+from airbyte_cdk.sources.streams.core import Stream
+
+parent_records = [{"id": 1, "data": "data1"}, {"id": 2, "data": "data2"}]
+
+data_first_parent_slice = [{"id": 0, "slice": "first", "data": "A"}, {"id": 1, "slice": "first", "data": "B"}]
+data_second_parent_slice = [{"id": 2, "slice": "second", "data": "C"}]
+data_third_parent_slice = []
+all_parent_data = data_first_parent_slice + data_second_parent_slice + data_third_parent_slice
+parent_slices = [{"slice": "first"}, {"slice": "second"}, {"slice": "third"}]
+
+slice_definition = {"parent_id": "{{ parent_record['id'] }}"}
+
+
+@pytest.mark.parametrize(
+    "test_name, parent_slices, parent_records, slice_definition, expected_slices",
+    [
+        ("test_single_parent_slices_no_records", [{}], [], slice_definition, [{"parent_id": None}]),
+        ("test_single_parent_slices_with_records", [{}], parent_records, slice_definition, [{"parent_id": "1"}, {"parent_id": "2"}]),
+        (
+            "test_with_parent_slices_and_records",
+            parent_slices,
+            all_parent_data,
+            slice_definition,
+            [
+                {"slice": "first", "parent_id": "0"},
+                {"slice": "first", "parent_id": "1"},
+                {"slice": "second", "parent_id": "2"},
+                {"slice": "third", "parent_id": None},
+            ],
+        ),
+    ],
+)
+def test_substream_slicer(test_name, parent_slices, parent_records, slice_definition, expected_slices):
+    parent_stream = MockStream(parent_slices, parent_records)
+    state = DictState()
+    slicer = SubstreamSlicer(parent_stream, state, slice_definition)
+    slices = [s for s in slicer.stream_slices(SyncMode.incremental, stream_state=None)]
+    assert slices == expected_slices
+
+
+class MockStream(Stream):
+    def __init__(self, slices, records):
+        self._slices = slices
+        self._records = records
+
+    @property
+    def primary_key(self) -> Optional[Union[str, List[str], List[List[str]]]]:
+        return "id"
+
+    def stream_slices(
+        self, *, sync_mode: SyncMode, cursor_field: List[str] = None, stream_state: Mapping[str, Any] = None
+    ) -> Iterable[Optional[Mapping[str, Any]]]:
+        yield from self._slices
+
+    def read_records(
+        self,
+        sync_mode: SyncMode,
+        cursor_field: List[str] = None,
+        stream_slice: Mapping[str, Any] = None,
+        stream_state: Mapping[str, Any] = None,
+    ) -> Iterable[Mapping[str, Any]]:
+        if not stream_slice:
+            yield from self._records
+        else:
+            yield from [r for r in self._records if r["slice"] == stream_slice["slice"]]
