3b1f4866c1696560480e17e1c1564fb879d5f232
Volodymyr Petrov <volodymyr.s.petrov@globallogic.com>
2022-05-27 17:17:25 +0300

    Add padding to bottom and convert styles to scss

--- airbyte-webapp/src/components/MainPageWithScroll/MainPageWithScroll.tsx
+++ airbyte-webapp/src/components/MainPageWithScroll/MainPageWithScroll.tsx
@@ -1,44 +1,29 @@
+import classnames from "classnames";
 import React from "react";
-import styled from "styled-components";
 
-const Content = styled.div`
-  overflow-y: auto;
-  padding-top: 17px;
-  height: 100%;
-`;
-
-const Header = styled.div<{ hasError?: boolean }>`
-  padding-top: ${({ hasError }) => (hasError ? 25 : 0)}px;
-`;
-
-const Page = styled.div`
-  overflow-y: hidden;
-  height: 100%;
-  display: flex;
-  flex-direction: column;
-`;
+import styles from "./MainPageWithScroll.module.scss";
 
 /**
  * @param headTitle the title shown in the browser toolbar
  * @param pageTitle the title shown on the page
  */
-interface IProps {
+interface MainPageWithScrollProps {
   error?: React.ReactNode;
   headTitle?: React.ReactNode;
   pageTitle?: React.ReactNode;
   children?: React.ReactNode;
 }
 
-const MainPageWithScroll: React.FC<IProps> = ({ error, headTitle, pageTitle, children }) => {
+const MainPageWithScroll: React.FC<MainPageWithScrollProps> = ({ error, headTitle, pageTitle, children }) => {
   return (
-    <Page>
+    <div className={styles.page}>
       {error}
-      <Header hasError={!!error}>
+      <div className={classnames({ [styles.headerError]: !!error })}>
         {headTitle}
         {pageTitle}
-      </Header>
-      <Content>{children}</Content>
-    </Page>
+      </div>
+      <div className={styles.content}>{children}</div>
+    </div>
   );
 };
 
