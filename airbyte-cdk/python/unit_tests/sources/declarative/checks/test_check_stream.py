855abf87e52e6a70710edafa36ae303a5685e88d
Alexandre Girard <alexandre@airbyte.io>
2022-05-26 17:39:09 -0700

    raise error if streams to check are not in the catalog

--- airbyte-cdk/python/unit_tests/sources/declarative/checks/test_check_stream.py
+++ airbyte-cdk/python/unit_tests/sources/declarative/checks/test_check_stream.py
@@ -3,36 +3,37 @@
 #
 from unittest.mock import MagicMock
 
+import pytest
 from airbyte_cdk.sources.declarative.checks.check_stream import CheckStream
 
 logger = None
 config = dict()
 
+stream_names = ["s1"]
+record = MagicMock()
 
-def test_check_stream():
-    record = MagicMock()
+
+@pytest.mark.parametrize(
+    "test_name, record, streams_to_check, expectation",
+    [
+        ("test success check", record, stream_names, (True, None)),
+        ("test fail check", None, stream_names, (True, None)),
+        ("test try to check invalid stream", record, ["invalid_stream_name"], None),
+    ],
+)
+def test_check_stream(test_name, record, streams_to_check, expectation):
     stream = MagicMock()
+    stream.name = "s1"
     stream.read_records.return_value = iter([record])
 
     source = MagicMock()
     source.streams.return_value = [stream]
 
-    check_stream = CheckStream(source)
+    check_stream = CheckStream(streams_to_check)
 
-    connected, error = check_stream.check_connection(logger, config)
-    assert connected
-    assert error is None
-
-
-def test_check_stream_no_records():
-    stream = MagicMock()
-    stream.read_records.return_value = iter([])
-
-    source = MagicMock()
-    source.streams.return_value = [stream]
-
-    check_stream = CheckStream(source)
-
-    connected, error = check_stream.check_connection(logger, config)
-    assert not connected
-    assert error is not None
+    if expectation:
+        actual = check_stream.check_connection(source, logger, config)
+        assert actual == expectation
+    else:
+        with pytest.raises(ValueError):
+            check_stream.check_connection(source, logger, config)
