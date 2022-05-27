e3cb566f793c42a70b7bfffca756448f055942ed
Evan Tahler <evan@airbyte.io>
2022-05-27 10:36:05 -0700

    Bump faker version to test M1 Connector builds (#13235)
    * Bump faker version to test M1 Connector builds
    
    * add PR link to changelog
    
    * Remove birhtday, it  doesn't obey seed
    
    * bump readme
    
    * more birthday to remove
    
    * remove image specificity in publish-command
    
    * Set AMI for testing.
    
    * test with including buildx emulators
    
    * auto-bump connector version
    
    * add TODO comment
    
    Co-authored-by: Davin Chia <davinchia@gmail.com>
    Co-authored-by: Octavia Squidington III <octavia-squidington-iii@users.noreply.github.com>

--- tools/integrations/manage.sh
+++ tools/integrations/manage.sh
@@ -202,11 +202,15 @@ cmd_publish() {
   local versioned_image=$image_name:$image_version
   local latest_image="$image_name" # don't include ":latest", that's assumed here
   local build_arch="linux/amd64,linux/arm64"
-  
+
   # learn about this version of Docker
   echo "--- docker info ---"
   docker --version
   docker buildx version
+
+  # Install docker emulators
+  # TODO: Don't run this command on M1 macs locally (it won't work and isn't needed)
+  docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
 
   # log into docker
   DOCKER_USERNAME=${DOCKER_USERNAME:-airbytebot}
