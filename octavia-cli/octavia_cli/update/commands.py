58303ea625fe6279856ed7da4d9917731b9f21fd
danieldiamond <danieldiamond1@gmail.com>
2022-05-29 22:12:05 +1000

    Add "update sources" and "update destinations" to create yaml files for all existing sources and destinations

--- octavia-cli/octavia_cli/update/commands.py
+++ octavia-cli/octavia_cli/update/commands.py
@@ -7,8 +7,10 @@ from typing import List
 import click
 from octavia_cli.base_commands import OctaviaCommand
 from octavia_cli.generate import definitions
+from octavia_cli.generate.commands import generate_source_or_destination
 from octavia_cli.generate.renderers import ConnectorSpecificationRenderer
 from octavia_cli.get.resources import Connection, Destination, Source
+from octavia_cli.list.listings import Destinations, Sources
 
 
 @click.group("update", help="Update configurations in a YAML spec for a source, destination or a connection.")
@@ -17,12 +19,24 @@ def update(ctx: click.Context):
     pass
 
 
+def list_resource(resource_type, api_client, workspace_id):
+    resources = resource_type(api_client, workspace_id)
+    return {resource_id: resource_name for resource_name, _, resource_id in resources.get_listing()}
+
+
+def get_resource_definition_id(resource_type, api_client, workspace_id, resource_id):
+    resource = resource_type(api_client, workspace_id, resource_id)
+    config = resource.get_config()
+    return config[f"{resource_type.name}_definition_id"]
+
+
 def update_resource(resource_type, api_client, workspace_id, resource_id):
     resource = resource_type(api_client, workspace_id, resource_id)
     definition_type = resource_type.name
     config = resource.get_config()
     definition_id = config[f"{resource_type.name}_definition_id"]
-    resource_name = config[f"{resource_type.name}_name"]
+    # Schema varies across resources, e.g. pull "name" else default to "source_name"
+    resource_name = config.get("name") or config[f"{resource_type.name}_name"]
 
     definition = definitions.factory(definition_type, api_client, workspace_id, definition_id)
     renderer = ConnectorSpecificationRenderer(resource_name, definition)
@@ -37,6 +51,36 @@ def update_resource(resource_type, api_client, workspace_id, resource_id):
 @click.pass_context
 def source(ctx: click.Context, resource_id: str):
     update_resource(Source, ctx.obj["API_CLIENT"], ctx.obj["WORKSPACE_ID"], resource_id)
+
+
+@update.command(cls=OctaviaCommand, name="sources", help="Get YAML for a source")
+@click.pass_context
+def sources(ctx: click.Context):
+    api_client = ctx.obj["API_CLIENT"]
+    workspace_id = ctx.obj["WORKSPACE_ID"]
+    sources = list_resource(Sources, api_client, workspace_id)
+
+    for source_id, source_name in sources.items():
+        definition_id = get_resource_definition_id(Source, api_client, workspace_id, source_id)
+
+        generate_source_or_destination("source", api_client, workspace_id, definition_id, source_name)
+
+        update_resource(Source, api_client, workspace_id, source_id)
+
+
+@update.command(cls=OctaviaCommand, name="destinations", help="Get YAML for a destination")
+@click.pass_context
+def destinations(ctx: click.Context):
+    api_client = ctx.obj["API_CLIENT"]
+    workspace_id = ctx.obj["WORKSPACE_ID"]
+    destinations = list_resource(Destinations, api_client, workspace_id)
+
+    for destination_id, destination_name in destinations.items():
+        definition_id = get_resource_definition_id(Destination, api_client, workspace_id, destination_id)
+
+        generate_source_or_destination("destination", api_client, workspace_id, definition_id, destination_name)
+
+        update_resource(Destination, api_client, workspace_id, destination_id)
 
 
 @update.command(cls=OctaviaCommand, name="destination", help="Get YAML for a destination")
