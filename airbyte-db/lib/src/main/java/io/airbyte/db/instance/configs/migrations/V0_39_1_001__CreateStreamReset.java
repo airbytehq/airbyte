372c87ea368f93698900b6c62ff571e3b6a9f9e5
alovew <anne@airbyte.io>
2022-05-31 09:53:43 -0700

    add namespace column

--- airbyte-db/lib/src/main/java/io/airbyte/db/instance/configs/migrations/V0_39_1_001__CreateStreamReset.java
+++ airbyte-db/lib/src/main/java/io/airbyte/db/instance/configs/migrations/V0_39_1_001__CreateStreamReset.java
@@ -5,6 +5,7 @@
 package io.airbyte.db.instance.configs.migrations;
 
 import static org.jooq.impl.DSL.currentOffsetDateTime;
+import static org.jooq.impl.DSL.unique;
 
 import java.time.OffsetDateTime;
 import java.util.UUID;
@@ -18,9 +19,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 // TODO: update migration description in the class name
-public class V0_39_1_001__CreateStreamResets extends BaseJavaMigration {
+public class V0_39_1_001__CreateStreamReset extends BaseJavaMigration {
 
-  private static final Logger LOGGER = LoggerFactory.getLogger(V0_39_1_001__CreateStreamResets.class);
+  private static final Logger LOGGER = LoggerFactory.getLogger(V0_39_1_001__CreateStreamReset.class);
 
   @Override
   public void migrate(final Context context) throws Exception {
@@ -30,12 +31,13 @@ public class V0_39_1_001__CreateStreamResets extends BaseJavaMigration {
     // As database schema changes, the generated jOOQ code can be deprecated. So
     // old migration may not compile if there is any generated code.
     final DSLContext ctx = DSL.using(context.getConnection());
-    createResetsTable(ctx);
+    createStreamResetTable(ctx);
   }
 
-  private static void createResetsTable(final DSLContext ctx) {
+  private static void createStreamResetTable(final DSLContext ctx) {
     final Field<UUID> id = DSL.field("id", SQLDataType.UUID.nullable(false));
     final Field<UUID> connectionId = DSL.field("connection_id", SQLDataType.UUID.nullable(false));
+    final Field<String> streamNamespace = DSL.field("stream_namespace", SQLDataType.CLOB.nullable(false));
     final Field<String> streamName = DSL.field("stream_name", SQLDataType.CLOB.nullable(false));
     final Field<OffsetDateTime> createdAt =
         DSL.field("created_at", SQLDataType.TIMESTAMPWITHTIMEZONE.nullable(false).defaultValue(currentOffsetDateTime()));
@@ -43,8 +45,12 @@ public class V0_39_1_001__CreateStreamResets extends BaseJavaMigration {
         DSL.field("updated_at", SQLDataType.TIMESTAMPWITHTIMEZONE.nullable(false).defaultValue(currentOffsetDateTime()));
 
     ctx.createTableIfNotExists("stream_resets")
-        .columns(id, connectionId, streamName, createdAt, updatedAt).execute();
-    ctx.createIndex("connection_id_stream_name_idx").on("stream_resets", "connection_id", "stream_name").execute();
+        .columns(id, connectionId, streamNamespace, streamName, createdAt, updatedAt)
+        .constraints(
+          unique(connectionId, streamNamespace, streamName))
+        .execute();
+
+        ctx.createIndex("connection_id_stream_name_namespace_idx").on("stream_resets", "connection_id", "stream_namespace", "stream_name").execute();
   }
 
 }
