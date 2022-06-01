637c9f3f030e0e8bd396f2d975b71f6f888f6a40
Alexandre Girard <alexandre@airbyte.io>
2022-05-31 18:42:24 -0700

    format

--- airbyte-cdk/python/airbyte_cdk/sources/declarative/create_partial.py
+++ airbyte-cdk/python/airbyte_cdk/sources/declarative/create_partial.py
@@ -1,6 +1,7 @@
 #
 # Copyright (c) 2022 Airbyte, Inc., all rights reserved.
 #
+
 import inspect
 
 from airbyte_cdk.sources.declarative.interpolation.interpolated_mapping import InterpolatedMapping
--- airbyte-cdk/python/airbyte_cdk/sources/declarative/parsers/yaml_parser.py
+++ airbyte-cdk/python/airbyte_cdk/sources/declarative/parsers/yaml_parser.py
@@ -1,6 +1,7 @@
 #
 # Copyright (c) 2022 Airbyte, Inc., all rights reserved.
 #
+
 from copy import deepcopy
 from typing import Any, Mapping
 
