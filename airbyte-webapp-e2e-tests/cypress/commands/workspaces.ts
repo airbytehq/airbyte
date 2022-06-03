609541d9ae0d4d4e44fc20bea5dc208d2742d282
Edmundo Ruiz Ghanem <168664+edmundito@users.noreply.github.com>
2022-06-03 13:00:58 -0400

    Migrate cypress tests and commands to TypeScript (#13091)
    * Migrate cypress tests and commands to TypeScript
    
    * Cypress added plugins to tsconfig
    
    * Update cypress plugins file to .ts

--- airbyte-webapp-e2e-tests/cypress/commands/workspaces.ts
+++ airbyte-webapp-e2e-tests/cypress/commands/workspaces.ts
@@ -1,4 +1,4 @@
-Cypress.Commands.add("initialSetupCompleted", (completed = true) => {
+export const initialSetupCompleted = (completed = true) => {
   // Modify the workspaces/list response to mark every workspace as "initialSetupComplete" to ensure we're not showing
   // the setup/preference page for any workspace if this method got called.
   cy.intercept("POST", "/api/v1/workspaces/get", (req) => {
@@ -7,4 +7,4 @@ Cypress.Commands.add("initialSetupCompleted", (completed = true) => {
       res.send(res.body);
     });
   });
-});
+};
