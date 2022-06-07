043b3434f922213320cbc72d9bd65fa0fa43b7d3
Alexandre Girard <alexandre@airbyte.io>
2022-06-06 19:12:12 -0700

    use jello

--- airbyte-cdk/python/airbyte_cdk/sources/declarative/extractors/jq.py
+++ airbyte-cdk/python/airbyte_cdk/sources/declarative/extractors/jq.py
@@ -4,11 +4,11 @@
 
 from typing import Any, List, Mapping, Union
 
-import pyjq
 from airbyte_cdk.sources.declarative.extractors.http_extractor import HttpExtractor
 from airbyte_cdk.sources.declarative.interpolation.jinja import JinjaInterpolation
 from airbyte_cdk.sources.declarative.response import Response
 from airbyte_cdk.sources.declarative.types import Record
+from jello import lib as jello_lib
 
 
 class JqExtractor(HttpExtractor):
@@ -24,7 +24,4 @@ class JqExtractor(HttpExtractor):
 
     def extract_records(self, response: Response) -> Union[List[Record], Mapping[str, Any]]:
         script = self._interpolator.eval(self._transform, self._config, default=self.default_transform, **{"kwargs": self._kwargs})
-        if script.endswith("[]"):
-            return pyjq.all(script, response.body)
-        else:
-            return pyjq.first(script, response.body)
+        return jello_lib.pyquery(response.body, script)
