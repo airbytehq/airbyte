c67acea7a59b7a18cf3db7967e8e1bf50aa5570d
Douglas Martins <douglaspmartins0@gmail.com>
2022-06-06 21:38:27 -0300

    create package for schema_generator

--- tools/schema_generator/schema_generator/infer_schemas.py
+++ tools/schema_generator/schema_generator/infer_schemas.py
@@ -1,27 +1,3 @@
-#
-# MIT License
-#
-# Copyright (c) 2020 Airbyte
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in all
-# copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
-#
-
 import json
 import os
 import sys
@@ -36,6 +12,7 @@ class NoRequiredObj(Object):
     This class has Object behaviour, but it does not generate "required[]" fields
     every time it parses object. So we dont add unnecessary extra field.
     """
+
     def to_schema(self):
         schema = super(Object, self).to_schema()
         schema["type"] = "object"
@@ -50,7 +27,7 @@ class NoRequiredSchemaBuilder(SchemaBuilder):
     EXTRA_STRATEGIES = (NoRequiredObj,)
 
 
-def main():
+def infer_schemas():
     default_folder = os.path.join(os.getcwd(), "schemas")
     if not os.path.exists(default_folder):
         os.mkdir(default_folder)
@@ -71,7 +48,3 @@ def main():
         output_file_name = os.path.join(default_folder, stream_name + ".json")
         with open(output_file_name, "w") as outfile:
             json.dump(schema, outfile, indent=2, sort_keys=True)
-
-
-if __name__ == "__main__":
-    main()
