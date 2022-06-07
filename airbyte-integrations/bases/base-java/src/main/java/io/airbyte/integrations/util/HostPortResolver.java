0e06496d61ded011efa0af49a71c3081826210e7
terencecho <terence@airbyte.io>
2022-06-07 10:57:38 -0400

    Fix build: run gradlew format (#13556)

--- airbyte-integrations/bases/base-java/src/main/java/io/airbyte/integrations/util/HostPortResolver.java
+++ airbyte-integrations/bases/base-java/src/main/java/io/airbyte/integrations/util/HostPortResolver.java
@@ -1,3 +1,7 @@
+/*
+ * Copyright (c) 2022 Airbyte, Inc., all rights reserved.
+ */
+
 package io.airbyte.integrations.util;
 
 import java.util.Objects;
@@ -6,19 +10,18 @@ import org.testcontainers.containers.GenericContainer;
 public class HostPortResolver {
 
   public static String resolveHost(GenericContainer container) {
-    return System.getProperty("os.name").toLowerCase().startsWith("mac") ?
-        Objects.requireNonNull(container.getContainerInfo()
-            .getNetworkSettings()
-            .getNetworks()
-            .entrySet().stream()
-            .findFirst()
-            .get().getValue().getIpAddress())
+    return System.getProperty("os.name").toLowerCase().startsWith("mac") ? Objects.requireNonNull(container.getContainerInfo()
+        .getNetworkSettings()
+        .getNetworks()
+        .entrySet().stream()
+        .findFirst()
+        .get().getValue().getIpAddress())
         : container.getHost();
   }
 
   public static int resolvePort(GenericContainer container) {
-    return System.getProperty("os.name").toLowerCase().startsWith("mac") ? (Integer)
-        container.getExposedPorts().get(0) : container.getFirstMappedPort();
+    return System.getProperty("os.name").toLowerCase().startsWith("mac") ? (Integer) container.getExposedPorts().get(0)
+        : container.getFirstMappedPort();
   }
 
 }
