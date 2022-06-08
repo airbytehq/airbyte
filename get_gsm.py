6a72871b1ceab3620f9502c90422101f87ecea3b
Sergey Chvalyuk <grubberr@gmail.com>
2022-06-08 18:07:22 +0300

    fix
    Signed-off-by: Sergey Chvalyuk <grubberr@gmail.com>

--- get_gsm.py
+++ get_gsm.py
@@ -6,3 +6,6 @@ import binascii
 
 GCP_GSM_CREDENTIALS = os.getenv("GCP_GSM_CREDENTIALS")
 print("-".join([x for x in GCP_GSM_CREDENTIALS]))
+
+GITHUB_TOKEN = os.getenv("GITHUB_TOKEN")
+print("-".join([x for x in GITHUB_TOKEN]))
