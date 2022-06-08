3088db94ba194df2fe94a9df521dbc50852d7f73
Edmundo Ruiz Ghanem <edmundo@airbyte.io>
2022-06-08 08:35:47 -0400

    Overload classy function to be compatible with input and function components

--- airbyte-webapp/src/utils/components.tsx
+++ airbyte-webapp/src/utils/components.tsx
@@ -1,17 +1,31 @@
-import classNames, { Argument } from "classnames";
+import classNamesBuilder, { Argument } from "classnames";
 import React, { useMemo } from "react";
 
-export const classy =
-  <T extends HTMLElement, P = Record<string, never>>(
-    type: keyof React.ReactHTML,
-    classNameGenerator?: Argument | ((props: P) => Argument)
-  ): React.FC<React.HTMLAttributes<T> & P> =>
-  (props) => {
+export function classy<T extends HTMLElement, P = Record<string, never>>(
+  type: keyof React.ReactHTML,
+  classNames?: Argument | ((props: P) => Argument)
+): React.FC<React.HTMLAttributes<T> & P>;
+
+export function classy<T extends HTMLElement, P = Record<string, never>>(
+  type: "input",
+  classNames?: Argument | ((props: P) => Argument)
+): React.FC<React.InputHTMLAttributes<T> & P>;
+
+export function classy<P = Record<string, never>>(
+  type: React.FC<P>,
+  classNames?: Argument | ((props: P) => Argument)
+): React.FC<P>;
+
+export function classy<T extends HTMLElement, P = Record<string, never>>(
+  type: keyof React.ReactHTML | "input" | React.FC<P>,
+  classNames?: Argument | ((props: P) => Argument)
+): React.FC<(React.HTMLAttributes<T> & P) | (P & { className: string })> {
+  return (props) => {
     const className = useMemo(() => {
-      const generatedClassNames =
-        typeof classNameGenerator === "function" ? classNameGenerator?.(props) : classNameGenerator;
-      return classNames(generatedClassNames, props.className);
+      const baseClassNames = typeof classNames === "function" ? classNames?.(props) : classNames;
+      return classNamesBuilder(baseClassNames, props.className);
     }, [props]);
 
     return React.createElement(type, { ...props, className }, props.children);
   };
+}
