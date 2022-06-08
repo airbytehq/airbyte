4bacad707bd300d6787a3c5f2ca41042379871cc
Edmundo Ruiz Ghanem <edmundo@airbyte.io>
2022-06-08 14:44:46 -0400

    Update frequency cell in connection table to return Manual when manual sync

--- airbyte-webapp/src/components/EntityTable/components/FrequencyCell.tsx
+++ airbyte-webapp/src/components/EntityTable/components/FrequencyCell.tsx
@@ -1,12 +1,11 @@
-import React from "react";
+import React, { useMemo } from "react";
 import styled from "styled-components";
 
 import FrequencyConfig from "config/FrequencyConfig.json";
+import { ConnectionSchedule } from "core/request/AirbyteClient";
 import { equal } from "utils/objects";
 
-import { ConnectionSchedule } from "../../../core/request/AirbyteClient";
-
-interface IProps {
+interface FrequencyCellProps {
   value: ConnectionSchedule;
   enabled?: boolean;
 }
@@ -15,9 +14,13 @@ const Content = styled.div<{ enabled?: boolean }>`
   color: ${({ theme, enabled }) => (!enabled ? theme.greyColor40 : "inherit")};
 `;
 
-const FrequencyCell: React.FC<IProps> = ({ value, enabled }) => {
-  const cellText = FrequencyConfig.find((item) => equal(item.config, value));
-  return <Content enabled={enabled}>{cellText?.text || ""}</Content>;
+const FrequencyCell: React.FC<FrequencyCellProps> = ({ value, enabled }) => {
+  const text = useMemo<string>(() => {
+    const cellText = FrequencyConfig.find((item) => equal(item.config, value ?? null));
+    return cellText?.text ?? "";
+  }, [value]);
+
+  return <Content enabled={enabled}>{text}</Content>;
 };
 
 export default FrequencyCell;
