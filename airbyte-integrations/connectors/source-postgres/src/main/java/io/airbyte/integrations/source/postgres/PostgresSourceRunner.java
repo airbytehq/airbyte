c8cad84aad54e8d98f5f07dd313377041cb6113b
Liren Tu <tuliren.git@outlook.com>
2022-06-07 21:38:33 -0700

    Migrate postgres source to use adaptive runner

---
+++ airbyte-integrations/connectors/source-postgres/src/main/java/io/airbyte/integrations/source/postgres/PostgresSourceRunner.java
@@ -0,0 +1,14 @@
+package io.airbyte.integrations.source.postgres;
+
+import io.airbyte.integrations.base.adaptive.AdaptiveSourceRunner;
+
+public class PostgresSourceRunner {
+
+  public static void main(final String[] args) throws Exception {
+    AdaptiveSourceRunner.baseOnEnv()
+        .withOssSource(PostgresSource::sshWrappedSource)
+        .withCloudSource(PostgresSourceStrictEncrypt::new)
+        .run(args);
+  }
+
+}
