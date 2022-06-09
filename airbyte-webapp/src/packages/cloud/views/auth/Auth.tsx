7cca379480d5c7d66b5001b2642a31c8a17dc2f5
Volodymyr Petrov <volodymyr.s.petrov@globallogic.com>
2022-06-09 18:51:20 +0300

    convert styled components in Auth component to scss

--- airbyte-webapp/src/packages/cloud/views/auth/Auth.tsx
+++ airbyte-webapp/src/packages/cloud/views/auth/Auth.tsx
@@ -1,6 +1,5 @@
 import React, { Suspense } from "react";
 import { Navigate, Route, Routes } from "react-router-dom";
-import styled from "styled-components";
 
 import { LoadingPage } from "components";
 
@@ -9,42 +8,20 @@ import { CloudRoutes } from "packages/cloud/cloudRoutes";
 import { useAuthService } from "packages/cloud/services/auth/AuthService";
 import { ResetPasswordAction } from "packages/cloud/views/FirebaseActionRoute";
 
+import styles from "./Auth.module.scss";
 import FormContent from "./components/FormContent";
 import News from "./components/News";
 import { LoginPage } from "./LoginPage";
 import { ResetPasswordPage } from "./ResetPasswordPage";
 import { SignupPage } from "./SignupPage";
 
-const Content = styled.div`
-  width: 100%;
-  height: 100%;
-  overflow: hidden;
-  display: flex;
-  flex-direction: row;
-  background: ${({ theme }) => theme.whiteColor};
-`;
-
-const Part = styled.div`
-  flex: 1 0 0;
-  padding: 20px 36px 39px 46px;
-  height: 100%;
-`;
-
-const NewsPart = styled(Part)`
-  background: ${({ theme }) => theme.beigeColor};
-  padding: 36px 97px 39px 64px;
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-`;
-
 const Auth: React.FC = () => {
   const { pathname, location } = useRouter();
   const { loggedOut } = useAuthService();
 
   return (
-    <Content>
-      <Part>
+    <div className={styles.container}>
+      <div className={styles.leftSide}>
         <FormContent toLogin={pathname === `${CloudRoutes.Signup}`}>
           <Suspense fallback={<LoadingPage />}>
             <Routes>
@@ -59,11 +36,11 @@ const Auth: React.FC = () => {
             </Routes>
           </Suspense>
         </FormContent>
-      </Part>
-      <NewsPart>
+      </div>
+      <div className={styles.rightSide}>
         <News />
-      </NewsPart>
-    </Content>
+      </div>
+    </div>
   );
 };
 
