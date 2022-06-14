e0c4850f71fa9bdf28b0e274bf1ea7922dab6f3f
alafanechere <augustin.lafanechere@gmail.com>
2022-06-14 22:52:32 +0200

    refacto api_headers and tests

--- octavia-cli/octavia_cli/api_headers.py
+++ octavia-cli/octavia_cli/api_headers.py
@@ -1,9 +1,8 @@
 #
-# Copyright (c) 2021 Airbyte, Inc., all rights reserved.
+# Copyright (c) 2022 Airbyte, Inc., all rights reserved.
 #
 from dataclasses import dataclass
-from itertools import groupby
-from typing import Optional, List, Tuple, NoReturn
+from typing import List, Optional, Tuple
 
 import airbyte_api_client
 import click
@@ -13,120 +12,98 @@ from .apply.yaml_loaders import EnvVarLoader
 
 API_HEADERS_YAML_FILE_INSTRUCTION = """
 headers:
-  - name: Authorization
-    value: Basic dXNlcjpwYXNzd29yZA==
-  - name: Content-Type
-    value: application/json
+  "Authorization": "Basic dXNlcjpwYXNzd29yZA=="
+  "Content-Type": "application/json"
 """.strip()
 
 
+class InvalidApiHttpHeadersFileError(click.exceptions.ClickException):
+    pass
+
+
 @dataclass
-class ApplicationHeader:
-    name: Optional[str]
-    value: Optional[str]
+class ApiHttpHeader:
+    name: str
+    value: str
 
     def __post_init__(self):
         try:
             assert isinstance(self.name, str) and self.name
             assert isinstance(self.value, str) and self.value
         except AssertionError:
-            raise AttributeError("Header name and value must be non empty string")
+            raise AttributeError("Header name and value must be non empty string.")
         self.name = self.name.strip()
         self.value = self.value.strip()
 
-    def clean(self):
-        return ApplicationHeader(
-            name=self.name.strip(),
-            value=self.value.strip()
-        )
 
+def deserialize_file_based_headers(header_configuration_path: str) -> List[ApiHttpHeader]:
+    """Parse API HTTP headers declared in a YAML file to a list of ApiHttpHeaders
 
-def deduplicate_api_headers(application_headers: List[ApplicationHeader]) -> List[ApplicationHeader]:
-    sorted_headers = sorted(application_headers, key=lambda header: header.name)
-    groups = groupby(sorted_headers, key=lambda header: header.name)
-    unique_headers = [
-        ApplicationHeader(
-            name=header_name,
-            value=sorted([header.value for header in group_headers])[0]
-        ) for header_name, group_headers in groups]
-    return unique_headers
+    Args:
+        header_configuration_path (str): Path to the YAML file where API HTTP headers are declared.
 
+    Raises:
+        InvalidApiHttpHeadersFileError: Raised if the YAML structure is not valid.
 
-class InvalidHeaderConfigurationFile(Exception):
-    pass
-
-
-def deserialize_file_based_headers(header_configuration_path: str) -> List[ApplicationHeader]:
+    Returns:
+        List[ApiHttpHeader]: List of HTTP headers parsed from the YAML file.
+    """
     with open(header_configuration_path) as file:
-        content = yaml.load(file, EnvVarLoader)
+        try:
+            content = yaml.load(file, EnvVarLoader)
+            headers = content["headers"]
+        except (TypeError, KeyError, yaml.scanner.ScannerError):
+            raise InvalidApiHttpHeadersFileError(
+                f"Please provide valid yaml file to declare API HTTP headers, like below:\n{API_HEADERS_YAML_FILE_INSTRUCTION}"
+            )
 
-    headers = content.get("headers", None)
-
-    if not headers:
-        raise AttributeError("""
-        Please provide valid configuration file, like below
-        headers:
-          - name: Authorization
-            value: Basic dXNlcjpwYXNzd29yZA==
-          - name: Content-Type
-            value: application/json
-        """)
-
-    try:
-        application_headers = [ApplicationHeader(header["name"], header["value"]) for header in headers]
-    except KeyError:
-        raise InvalidHeaderConfigurationFile("Headers must have name and value keys")
-
-    return deduplicate_api_headers(application_headers)
+    return [ApiHttpHeader(name, value) for name, value in headers.items()]
 
 
-def deseriliaze_opiton_based_headers(api_headers: List[Tuple[str, str]]) -> List[ApplicationHeader]:
-    return deduplicate_api_headers([
-        ApplicationHeader(header_name, header_value) for header_name, header_value in api_headers
-    ])
+def deserialize_option_based_headers(api_http_headers: List[Tuple[str, str]]) -> List[ApiHttpHeader]:
+    """Parse API HTTP headers declared in CLI options to a list of ApiHttpHeaders
+
+    Args:
+        api_http_headers (List[Tuple[str, str]]): Raw list of api headers tuples retrieved from CLI options.
+
+    Returns:
+        List[ApiHttpHeader]: List of HTTP headers parsed from the CLI options.
+    """
+    return list({header_name: ApiHttpHeader(header_name, header_value) for header_name, header_value in api_http_headers}.values())
 
 
 def merge_api_headers(
-        option_based_headers: List[ApplicationHeader],
-        file_based_headers: List[ApplicationHeader]) -> List[ApplicationHeader]:
+    option_based_raw_headers: Optional[List[Tuple[str, str]]], api_http_headers_file_path: Optional[str]
+) -> List[ApiHttpHeader]:
+    """Deserialize headers from options and files into ApiHttpHeader and merge options based headers with file based headers.
+
+    Args:
+        option_based_raw_headers (Optional[List[Tuple[str, str]]]): Option based headers.
+        api_http_headers_file_path (Optional[str]): Path to the YAML file with http headers.
+
+    Returns:
+        List[ApiHttpHeader]: Lit of unique ApiHttpHeaders
     """
-    This function merges the HTTP headers parsed from the yam file and from the CLI option. Headers from CLI options override those defined
-    in th file.
-    :param option_based_headers:  headers which came from CLI options --api-headers
-    :param file_based_headers: all application headers which were parsed from file --api-headers-file
-    :return: the merged list of unique Http headers
-    """
+    if option_based_raw_headers and api_http_headers_file_path:
+        click.echo(
+            "ℹ️ - You passed API HTTP headers in a file and in options at the same time. Option based headers will override file based headers."
+        )
+
+    option_based_headers = deserialize_option_based_headers(option_based_raw_headers) if option_based_raw_headers is not None else []
+    file_based_headers = deserialize_file_based_headers(api_http_headers_file_path) if api_http_headers_file_path else []
 
     merged_headers = {header.name: header for header in file_based_headers}
     for header in option_based_headers:
         merged_headers[header.name] = header
-
     return list(merged_headers.values())
 
 
-def deserialize_api_headers(api_headers: Optional[List[Tuple[str, str]]], api_headers_file: Optional[str]) -> List[ApplicationHeader]:
-    if api_headers and api_headers_file:
-        click.echo("Passed api headers file and api headers at the same time, Overriding policy applied when priority of file is higher")
+def set_api_headers_on_api_client(api_client: airbyte_api_client.ApiClient, api_headers: List[ApiHttpHeader]) -> None:
+    """Set the API headers on the API client
 
-    argument_based_headers = []
-    file_based_application_headers = []
-
-    if api_headers:
-        argument_based_headers = deseriliaze_opiton_based_headers(api_headers)
-
-    if api_headers_file:
-        file_based_application_headers = deserialize_file_based_headers(api_headers_file)
-
-    api_headers_result = merge_api_headers(
-        argument_based_headers, file_based_application_headers
-    )
-
-    return api_headers_result
-
-
-def set_api_headers(api_client: airbyte_api_client.ApiClient, api_headers: List[ApplicationHeader]) -> NoReturn:
+    Args:
+        api_client (airbyte_api_client.ApiClient): The API client on which headers will be set.
+        api_headers (List[ApiHttpHeader]): Headers to set on the API client.
+    """
     for api_header in api_headers:
-        try:
-            api_client.set_default_header(api_header.name, api_header.value)
-        except Exception as e:
-            click.echo(f"Skipping applying configuration due to exception {e}")
+        api_client.set_default_header(api_header.name, api_header.value)
--- octavia-cli/unit_tests/test_api_headers.py
+++ octavia-cli/unit_tests/test_api_headers.py
@@ -1,312 +1,194 @@
 #
-# Copyright (c) 2021 Airbyte, Inc., all rights reserved.
+# Copyright (c) 2022 Airbyte, Inc., all rights reserved.
 #
 
 import os
-import uuid
-from typing import Dict, Any, List, Tuple
 
 import pytest
-import yaml
-
-from octavia_cli.api_headers import deserialize_file_based_headers, ApplicationHeader, deseriliaze_opiton_based_headers, deserialize_api_headers, \
-    deduplicate_api_headers, InvalidHeaderConfigurationFile
-
-os.environ["EXAMPLE_ENV_VARIABLE_VALUE"] = "test_header_value"
-os.environ["EXAMPLE_ENV_VARIABLE_NAME"] = "test_header_name"
-
-INVALID_FIELDS_YAML_STRING = """
-header:
-    - name: some_name
-      value: some_value
-""".strip()
-
-NON_PARSABLE_YAML_FILE = """
-some random words
-- some dashes:
-  - and_next
-""".strip()
-
-EMPTY_YAML_FILE = ""
+from octavia_cli import api_headers
 
 
-class TestHeadersAssignment:
-
+class TestApiHttpHeader:
     @pytest.mark.parametrize(
-        "data,expected_count,expected_api_headers", [
-            ({
-                 "headers": [
-                     {"name": "name1", "value": "value1"},
-                     {"name": "name2", "value": "value2"},
-                     {"name": "name3", "value": "value3"}
-                 ]
-             },
-             3,
-             [
-                 ApplicationHeader(name="name1", value="value1"),
-                 ApplicationHeader(name="name2", value="value2"),
-                 ApplicationHeader(name="name3", value="value3")
-             ]
-            ),
-            ({
-                 "headers": [
-                     {"name": "${EXAMPLE_ENV_VARIABLE_NAME}", "value": "${EXAMPLE_ENV_VARIABLE_VALUE}"},
-                 ]
-             },
-             1,
-             [
-                 ApplicationHeader(name="test_header_name", value="test_header_value")
-             ]
-            ),
-            ({
-                 "headers": [
-                     {"name": "name1 ", "value": "value1 "},
-                 ]
-             },
-             1,
-             [
-                 ApplicationHeader(name="name1", value="value1")
-             ]
-            ),
-            ({
-                 "headers": [
-                     {"name": "name1", "value": "value1"},
-                     {"name": "name1", "value": "value1"},
-                     {"name": "name2", "value": "value1"}
-                 ]
-             },
-             2,
-             [
-                 ApplicationHeader(name="name1", value="value1"),
-                 ApplicationHeader(name="name2", value="value1")
-             ]
-            ),
-            ({
-                 "headers": [
-                     {"name": "name1 ", "value": "value1"},
-                     {"name": "name1 ", "value": "value1"},
-                     {"name": "name2", "value": "value1"}
-                 ]
-             },
-             2,
-             [
-                 ApplicationHeader(name="name1", value="value1"),
-                 ApplicationHeader(name="name2", value="value1")
-             ]
-            )
-        ]
+        "header_name, header_value, expected_error, expected_name, expected_value",
+        [
+            ("foo", "bar", None, "foo", "bar"),
+            (" foo ", " bar ", None, "foo", "bar"),
+            ("", "bar", AttributeError, None, None),
+            ("foo", "", AttributeError, None, None),
+        ],
     )
-    def test_read_configuration_yaml_file(self, data: Dict[str, Any], expected_count: int, expected_api_headers: List[ApplicationHeader]):
-        # given yaml file
-        yaml_file_path = self._produce_yaml_file(data)
+    def test_init(self, header_name, header_value, expected_error, expected_name, expected_value):
+        if expected_error is None:
+            api_http_header = api_headers.ApiHttpHeader(header_name, header_value)
+            assert api_http_header.name == expected_name and api_http_header.value == expected_value
+        else:
+            with pytest.raises(expected_error):
+                api_headers.ApiHttpHeader(header_name, header_value)
 
-        # when loading the configuration file
-        api_headers = deserialize_file_based_headers(
-            yaml_file_path
+
+@pytest.fixture
+def api_http_header_env_var():
+    os.environ["API_HTTP_HEADER_IN_ENV_VAR"] = "bar"
+    yield "bar"
+    del os.environ["API_HTTP_HEADER_IN_ENV_VAR"]
+
+
+@pytest.mark.parametrize(
+    "yaml_document, expected_api_http_headers, expected_error",
+    [
+        (
+            """
+    headers:
+      Content-Type: ${API_HTTP_HEADER_IN_ENV_VAR}
+    """,
+            [api_headers.ApiHttpHeader("Content-Type", "bar")],
+            None,
+        ),
+        (
+            """
+    headers:
+      Content-Type: application/json
+    """,
+            [api_headers.ApiHttpHeader("Content-Type", "application/json")],
+            None,
+        ),
+        (
+            """
+    headers:
+      Content-Type: application/csv
+      Content-Type: application/json
+    """,
+            [api_headers.ApiHttpHeader("Content-Type", "application/json")],
+            None,
+        ),
+        (
+            """
+    headers:
+      Content-Type: application/json
+      Authorization: Bearer XXX
+    """,
+            [api_headers.ApiHttpHeader("Content-Type", "application/json"), api_headers.ApiHttpHeader("Authorization", "Bearer XXX")],
+            None,
+        ),
+        ("no_headers: foo", None, api_headers.InvalidApiHttpHeadersFileError),
+        ("", None, api_headers.InvalidApiHttpHeadersFileError),
+        (
+            """
+     some random words
+     - some dashes:
+      - and_next
+     """.strip(),
+            None,
+            api_headers.InvalidApiHttpHeadersFileError,
+        ),
+    ],
+)
+def test_deserialize_file_based_headers(api_http_header_env_var, tmp_path, yaml_document, expected_api_http_headers, expected_error):
+    yaml_file_path = tmp_path / "api_http_headers.yaml"
+    yaml_file_path.write_text(yaml_document)
+    if expected_error is None:
+        file_based_headers = api_headers.deserialize_file_based_headers(yaml_file_path)
+        assert file_based_headers == expected_api_http_headers
+    else:
+        with pytest.raises(expected_error):
+            api_headers.deserialize_file_based_headers(yaml_file_path)
+
+
+@pytest.mark.parametrize(
+    "option_based_headers, expected_option_based_headers",
+    [
+        ([("Content-Type", "application/json")], [api_headers.ApiHttpHeader("Content-Type", "application/json")]),
+        (
+            [("Content-Type", "application/yaml"), ("Content-Type", "application/json")],
+            [api_headers.ApiHttpHeader("Content-Type", "application/json")],
+        ),
+        (
+            [("Content-Type", "application/json"), ("Authorization", "Bearer XXX")],
+            [api_headers.ApiHttpHeader("Content-Type", "application/json"), api_headers.ApiHttpHeader("Authorization", "Bearer XXX")],
+        ),
+        ([], []),
+    ],
+)
+def test_deserialize_option_based_headers(option_based_headers, expected_option_based_headers):
+    assert api_headers.deserialize_option_based_headers(option_based_headers) == expected_option_based_headers
+
+
+@pytest.mark.parametrize(
+    "yaml_document, option_based_raw_headers, expected_merged_headers",
+    [
+        (
+            """
+    headers:
+      Content-Type: application/csv
+    """,
+            [("Content-Type", "application/json")],
+            [api_headers.ApiHttpHeader("Content-Type", "application/json")],
+        ),
+        (
+            None,
+            [("Content-Type", "application/json")],
+            [api_headers.ApiHttpHeader("Content-Type", "application/json")],
+        ),
+        (
+            """
+    headers:
+      Content-Type: application/json
+    """,
+            [],
+            [api_headers.ApiHttpHeader("Content-Type", "application/json")],
+        ),
+        (
+            """
+    headers:
+      Content-Type: application/json
+    """,
+            None,
+            [api_headers.ApiHttpHeader("Content-Type", "application/json")],
+        ),
+        (
+            """
+    headers:
+      Content-Type: application/json
+    """,
+            [("Authorization", "Bearer XXX")],
+            [api_headers.ApiHttpHeader("Content-Type", "application/json"), api_headers.ApiHttpHeader("Authorization", "Bearer XXX")],
+        ),
+        (
+            """
+    headers:
+      Content-Type: application/json
+      Foo: Bar
+    """,
+            [("Authorization", "Bearer XXX")],
+            [
+                api_headers.ApiHttpHeader("Content-Type", "application/json"),
+                api_headers.ApiHttpHeader("Foo", "Bar"),
+                api_headers.ApiHttpHeader("Authorization", "Bearer XXX"),
+            ],
+        ),
+    ],
+)
+def test_merge_api_headers(tmp_path, mocker, yaml_document, option_based_raw_headers, expected_merged_headers):
+    mocker.patch.object(api_headers.click, "echo")
+    if yaml_document is not None:
+        yaml_file_path = tmp_path / "api_http_headers.yaml"
+        yaml_file_path.write_text(yaml_document)
+    else:
+        yaml_file_path = None
+    assert api_headers.merge_api_headers(option_based_raw_headers, yaml_file_path) == expected_merged_headers
+    if option_based_raw_headers and yaml_file_path:
+        api_headers.click.echo.assert_called_with(
+            "ℹ️ - You passed API HTTP headers in a file and in options at the same time. Option based headers will override file based headers."
         )
 
-        # then headers should match to expected ones
 
-        assert len(api_headers) == expected_count
-        assert api_headers == expected_api_headers
-
-        self._remove_temp_yaml_file(yaml_file_path)
-
-    def test_should_not_assign_headers_when_file_is_invalid(self):
-        # given not parsable yaml file
-        yaml_file_path = self._produce_invalid_yaml_configuration_file(NON_PARSABLE_YAML_FILE)
-
-        # while loading Attribute error should be raised
-        with pytest.raises(yaml.scanner.ScannerError):
-            deserialize_file_based_headers(yaml_file_path)
-
-        self._remove_temp_yaml_file(yaml_file_path)
-
-    def test_should_raise_an_exception_when_structure_of_file_is_invalid(self):
-        # given invalid yaml file data
-        yaml_file_path = self._produce_invalid_yaml_configuration_file(INVALID_FIELDS_YAML_STRING)
-        empty_yaml_file_path = self._produce_invalid_yaml_configuration_file(EMPTY_YAML_FILE)
-
-        # while loading Attribute error should be raised
-        with pytest.raises(AttributeError):
-            deserialize_file_based_headers(yaml_file_path)
-            deserialize_file_based_headers(empty_yaml_file_path)
-
-        self._remove_temp_yaml_file(yaml_file_path)
-        self._remove_temp_yaml_file(empty_yaml_file_path)
-
-    @pytest.mark.parametrize(
-        "header_pairs,expected_count,expected_elements",
+def test_set_api_headers_on_api_client(mocker, mock_api_client):
+    headers = [api_headers.ApiHttpHeader("foo", "bar"), api_headers.ApiHttpHeader("bar", "foo")]
+    api_headers.set_api_headers_on_api_client(mock_api_client, headers)
+    mock_api_client.set_default_header.assert_has_calls(
         [
-            (
-                    [("name1", "value2"), ("name2", "value4"), ("name3", "value3")],
-                    3,
-                    [
-                        ApplicationHeader(name="name1", value="value2"),
-                        ApplicationHeader(name="name2", value="value4"),
-                        ApplicationHeader(name="name3", value="value3")
-                    ]
-            ),
-            (
-                    [("name1", "value2"), ("name1", "value2"), ("name3", "value3")],
-                    2,
-                    [
-                        ApplicationHeader(name="name1", value="value2"),
-                        ApplicationHeader(name="name3", value="value3")
-                    ]
-            ),
-            (
-                    [("name2", "value2"), ("name2", "value3")],
-                    1,
-                    [
-                        ApplicationHeader(name="name2", value="value2")
-                    ]
-            ),
+            mocker.call(headers[0].name, headers[0].value),
+            mocker.call(headers[1].name, headers[1].value),
         ]
     )
-    def test_should_properly_assign_value_from_arguments_based_approach(self, header_pairs: List[Tuple[str, str]], expected_count: int,
-                                                                        expected_elements: List[ApplicationHeader]):
-        # when trying to parse header
-        parsed_headers = deseriliaze_opiton_based_headers(header_pairs)
-
-        # then count should be as expected
-        assert len(parsed_headers) == expected_count
-
-        # and content should also match
-        assert parsed_headers == expected_elements
-
-    def test_should_return_api_headers_attributes_when_no_file_provided(self):
-        # given api header attributes
-        api_header_attributes = [("name", "value1")]
-
-        # when validating api_headers
-        parsed_attributes = deserialize_api_headers(api_header_attributes, None)
-
-        # then count should match to expected
-        assert len(parsed_attributes) == 1
-
-        # and result configuration should match to expected
-        assert parsed_attributes == [ApplicationHeader(name="name", value="value1")]
-
-    def test_should_return_file_based_headers_when_no_attributes_provided(self):
-        # given yaml file
-        yaml_file_path = self._produce_yaml_file(
-            {
-                "headers": [
-                    {"name": "name1", "value": "value1"}
-                ]
-            }
-        )
-
-        # when validating api headers
-        parsed_attributes = deserialize_api_headers(None, yaml_file_path)
-
-        # then result count should be from file based headers
-        assert len(parsed_attributes) == 1
-
-        # and content should match
-        assert parsed_attributes == [ApplicationHeader(name="name1", value="value1")]
-
-        self._remove_temp_yaml_file(yaml_file_path)
-
-    @pytest.mark.parametrize(
-        "file_based_api_headers,argument_based_api_headers,expected_headers",
-        [
-            (
-                    {
-                        "headers": [
-                            {"name": "name1", "value": "value1"}
-                        ]
-                    },
-                    [],
-                    [ApplicationHeader(name="name1", value="value1")]
-            ),
-            (
-                    {
-                        "headers": [
-                            {"name": "name1", "value": "value1"},
-                            {"name": "name2", "value": "value1"},
-                        ]
-                    },
-                    [("name1", "value2"), ("name3", "value4")],
-                    [
-                        ApplicationHeader(name="name1", value="value2"),
-                        ApplicationHeader(name="name2", value="value1"),
-                        ApplicationHeader(name="name3", value="value4")
-                    ]
-            ),
-        ]
-    )
-    def test_should_merge_headers_from_file_and_arguments_when_both_specified(self, file_based_api_headers: Dict[str, Any],
-                                                                              argument_based_api_headers: List[Tuple[str, str]],
-                                                                              expected_headers: List[ApplicationHeader]):
-        # given yaml file with configuration
-        yaml_file_path = self._produce_yaml_file(file_based_api_headers)
-
-        # when merging the configuration
-        parsed_attributes = deserialize_api_headers(argument_based_api_headers, yaml_file_path)
-
-        # then headers from file should have higher priority
-        assert expected_headers == parsed_attributes
-
-        self._remove_temp_yaml_file(yaml_file_path)
-
-    def test_should_raise_exception_when_configuration_file_does_not_exists(self):
-        with pytest.raises(FileNotFoundError):
-            deserialize_api_headers(None, "/some/path")
-
-    @pytest.mark.parametrize(
-        "headers,expected_headers", [
-            (
-                    [],
-                    []
-            ),
-            (
-                    [ApplicationHeader("name1", "value1"), ApplicationHeader("name2", "value2")],
-                    [ApplicationHeader("name1", "value1"), ApplicationHeader("name2", "value2")]
-            ),
-            (
-                    [ApplicationHeader("name1", "value1"), ApplicationHeader("name1", "value1"), ApplicationHeader("name2", "value2")],
-                    [ApplicationHeader("name1", "value1"), ApplicationHeader("name2", "value2")]
-            )
-        ]
-    )
-    def test_deduplicating_headers(self, headers: List[ApplicationHeader], expected_headers: List[ApplicationHeader]):
-        # when deduplicating header list
-        deduplicated_headers = deduplicate_api_headers(headers)
-
-        # then list should contain unique headers names
-        assert expected_headers == deduplicated_headers
-
-    def test_should_raise_invalid_file_exception_when_file_has_invalid_keys(self):
-        # given api headers configuration file
-        yaml_file_path = self._produce_yaml_file({
-            "headers": [
-                {"not_name": "name1", "value": "value1"}
-            ]
-        })
-
-        # when yaml header configuration from file, then exception should be raised
-
-        with pytest.raises(InvalidHeaderConfigurationFile):
-            deserialize_file_based_headers(yaml_file_path)
-
-    @staticmethod
-    def _produce_yaml_file(data: Dict[str, Any]) -> str:
-        random_file_name = f"{uuid.uuid4()}.yaml"
-
-        with open(random_file_name, "w") as file:
-            yaml.dump(data, file)
-        return random_file_name
-
-    @staticmethod
-    def _remove_temp_yaml_file(yaml_file_path: str):
-        if os.path.isfile(yaml_file_path):
-            os.remove(yaml_file_path)
-
-    def _produce_invalid_yaml_configuration_file(self, data: str):
-        random_file_name = f"{uuid.uuid4()}.yaml"
-
-        with open(random_file_name, "w") as file:
-            file.write(data)
-        return random_file_name
