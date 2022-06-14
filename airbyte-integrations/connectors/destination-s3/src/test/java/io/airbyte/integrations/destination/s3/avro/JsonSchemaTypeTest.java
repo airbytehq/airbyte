6710a41af60305e2a27745dca043792e432b8571
oneshcheret <sashkaliife@gmail.com>
2022-06-14 12:36:46 +0300

    S3 destination: update unit tests.

--- airbyte-integrations/connectors/destination-s3/src/test/java/io/airbyte/integrations/destination/s3/avro/JsonSchemaTypeTest.java
+++ airbyte-integrations/connectors/destination-s3/src/test/java/io/airbyte/integrations/destination/s3/avro/JsonSchemaTypeTest.java
@@ -6,7 +6,11 @@ package io.airbyte.integrations.destination.s3.avro;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.stream.Stream;
+import org.junit.jupiter.api.extension.ExtensionContext;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.ArgumentsProvider;
 import org.junit.jupiter.params.provider.ArgumentsSource;
 
 public class JsonSchemaTypeTest {
@@ -17,6 +21,26 @@ public class JsonSchemaTypeTest {
     assertEquals(
         expectedJsonSchemaType,
         JsonSchemaType.fromJsonSchemaType(type, airbyteType));
+  }
+
+  public static class JsonSchemaTypeProvider implements ArgumentsProvider {
+
+    @Override
+    public Stream<? extends Arguments> provideArguments(ExtensionContext context) throws Exception {
+      return Stream.of(
+          Arguments.of("number", "integer", JsonSchemaType.NUMBER_INT),
+          Arguments.of("number", "big_integer", JsonSchemaType.NUMBER_LONG),
+          Arguments.of("number", "float", JsonSchemaType.NUMBER_FLOAT),
+          Arguments.of("number", null, JsonSchemaType.NUMBER),
+          Arguments.of("string", null, JsonSchemaType.STRING),
+          Arguments.of("integer", null, JsonSchemaType.INTEGER),
+          Arguments.of("boolean", null, JsonSchemaType.BOOLEAN),
+          Arguments.of("null", null, JsonSchemaType.NULL),
+          Arguments.of("object", null, JsonSchemaType.OBJECT),
+          Arguments.of("array", null, JsonSchemaType.ARRAY),
+          Arguments.of("combined", null, JsonSchemaType.COMBINED));
+    }
+
   }
 
 }
