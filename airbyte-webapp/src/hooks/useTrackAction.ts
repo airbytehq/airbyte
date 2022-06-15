572d17c6d785579c8646130c74999e92bd68306d
Teal Larson <LARSON.TEAL@GMAIL.COM>
2022-06-15 16:53:57 -0400

    wip

--- airbyte-webapp/src/hooks/useTrackAction.ts
+++ airbyte-webapp/src/hooks/useTrackAction.ts
@@ -14,12 +14,34 @@ interface TrackActionProperties {
   connector_destination_definition_id?: string;
 }
 
+interface NewTrackActionProperties {
+  connector_source?: string;
+  connector_source_definition_id?: string;
+  connector_destination?: string;
+  connector_destination_definition_id?: string;
+}
+
 export const useTrackAction = (type: TrackActionType) => {
   const analyticsService = useAnalyticsService();
 
   return useCallback(
     (action: string, properties: TrackActionProperties) => {
       analyticsService.track(`${type} - Action`, { action, ...properties });
+    },
+    [analyticsService, type]
+  );
+};
+
+export const useNewTrackAction = (type: TrackActionType) => {
+  const analyticsService = useAnalyticsService();
+
+  return useCallback(
+    (action: string, properties: NewTrackActionProperties) => {
+      analyticsService.track(`AIRBYTE.UI.${namespace}.${action_type}`, {
+        action,
+        ...properties,
+        legacy_event_name: `${type} - Action`,
+      });
     },
     [analyticsService, type]
   );
