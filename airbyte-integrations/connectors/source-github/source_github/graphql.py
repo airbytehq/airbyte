624ba57c0e257293d1096b3f6cd5bffa9c30ea2f
Sergey Chvalyuk <grubberr@gmail.com>
2022-06-15 21:51:21 +0300

    github_schema.py added
    Signed-off-by: Sergey Chvalyuk <grubberr@gmail.com>

--- airbyte-integrations/connectors/source-github/source_github/graphql.py
+++ airbyte-integrations/connectors/source-github/source_github/graphql.py
@@ -3,70 +3,16 @@
 #
 
 
-from sgqlc.operation import Operation
-from sgqlc.types import Field, Type, datetime, list_of
-from sgqlc.types.relay import Connection, connection_args
+import sgqlc.operation
 
+from . import github_schema
 
-class Actor(Type):
-    login = str
-
-
-class IssueCommentConnection(Type):
-    totalCount = int
-
-
-class PullRequestCommitConnection(Type):
-    totalCount = int
-
-
-class PullRequestReviewCommentConnection(Type):
-    totalCount = int
-
-
-class PullRequestReview(Type):
-    comments = Field(PullRequestReviewCommentConnection)
-
-
-class PullRequestReviewConnection(Connection):
-    totalCount = int
-    nodes = list_of(PullRequestReview)
-
-
-class PullRequest(Type):
-    id = str
-    database_id = int
-    number = int
-    repository = Field("Repository")
-    comments = Field(IssueCommentConnection)
-    commits = Field(PullRequestCommitConnection)
-    reviews = Field(PullRequestReviewConnection, args=connection_args())
-    updated_at = datetime.DateTime
-    changedFiles = int
-    deletions = int
-    additions = int
-    merged = bool
-    mergedBy = Actor
-    canBeRebased = bool
-    maintainerCanModify = bool
-    mergeStateStatus = str
-
-
-class PullRequestConnection(Connection):
-    nodes = list_of(PullRequest)
-
-
-class Repository(Type):
-    name = str
-    pull_requests = Field(PullRequestConnection, args=connection_args())
-
-
-class Query(Type):
-    repository = Field(Repository, args={"owner": str, "name": str})
+_schema = github_schema
+_schema_root = _schema.github_schema
 
 
 def get_query(owner, name, page_size, next_page_token):
-    op = Operation(Query)
+    op = sgqlc.operation.Operation(_schema_root.query_type)
 
     kwargs = {"first": page_size}
     if next_page_token:
@@ -77,20 +23,24 @@ def get_query(owner, name, page_size, next_page_token):
     pull_requests.nodes.database_id()
     pull_requests.nodes.number()
     pull_requests.nodes.updated_at()
-    pull_requests.nodes.repository()
-    pull_requests.nodes.comments()
-    pull_requests.nodes.commits()
+    repository = pull_requests.nodes.repository()
+    repository.name()
+    comments = pull_requests.nodes.comments()
+    comments.total_count()
+    commits = pull_requests.nodes.commits()
+    commits.total_count()
     reviews = pull_requests.nodes.reviews(first=100)
-    reviews.totalCount()
-    reviews.nodes.comments()
-    pull_requests.nodes.changedFiles()
+    reviews.total_count()
+    reviews_comments = reviews.nodes.comments()
+    reviews_comments.total_count()
+    pull_requests.nodes.changed_files()
     pull_requests.nodes.deletions()
     pull_requests.nodes.additions()
     pull_requests.nodes.merged()
-    pull_requests.nodes.mergedBy()
-    pull_requests.nodes.canBeRebased()
-    pull_requests.nodes.maintainerCanModify()
-    pull_requests.nodes.mergeStateStatus()
+    pull_requests.nodes.merged_by()
+    pull_requests.nodes.can_be_rebased()
+    pull_requests.nodes.maintainer_can_modify()
+    pull_requests.nodes.merge_state_status()
     pull_requests.page_info.__fields__("has_next_page")
     pull_requests.page_info.__fields__(end_cursor=True)
     return str(op)
