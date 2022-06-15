a182aca5e61fb45ee21f59ed2a9bceaddd90b141
Augustin <augustin.lafanechere@gmail.com>
2022-06-15 20:24:32 +0200

    octavia-cli: per workspace state (#13070)

--- octavia-cli/integration_tests/test_apply/test_resources.py
+++ octavia-cli/integration_tests/test_apply/test_resources.py
@@ -8,10 +8,10 @@ import pytest
 pytestmark = pytest.mark.integration
 
 
-def test_source_lifecycle(source):
+def test_source_lifecycle(source, workspace_id):
     assert not source.was_created
     source.create()
-    source.state = source._get_state_from_file(source.configuration_path)
+    source.state = source._get_state_from_file(source.configuration_path, workspace_id)
     assert source.was_created
     assert not source.get_diff_with_remote_resource()
     source.raw_configuration["configuration"]["pokemon_name"] = "snorlax"
@@ -22,10 +22,10 @@ def test_source_lifecycle(source):
     assert source.catalog["streams"][0]["config"]["alias_name"] == "pokemon"
 
 
-def test_destination_lifecycle(destination):
+def test_destination_lifecycle(destination, workspace_id):
     assert not destination.was_created
     destination.create()
-    destination.state = destination._get_state_from_file(destination.configuration_path)
+    destination.state = destination._get_state_from_file(destination.configuration_path, workspace_id)
     assert destination.was_created
     assert not destination.get_diff_with_remote_resource()
     destination.raw_configuration["configuration"]["host"] = "foo"
@@ -35,12 +35,12 @@ def test_destination_lifecycle(destination):
     assert not destination.get_diff_with_remote_resource()
 
 
-def test_connection_lifecycle(source, destination, connection):
+def test_connection_lifecycle(source, destination, connection, workspace_id):
     assert source.was_created
     assert destination.was_created
     assert not connection.was_created
     connection.create()
-    connection.state = connection._get_state_from_file(connection.configuration_path)
+    connection.state = connection._get_state_from_file(connection.configuration_path, workspace_id)
     assert connection.was_created
     assert not connection.get_diff_with_remote_resource()
     connection.raw_configuration["configuration"]["status"] = "inactive"
@@ -50,13 +50,13 @@ def test_connection_lifecycle(source, destination, connection):
     assert not connection.get_diff_with_remote_resource()
 
 
-def test_connection_lifecycle_with_normalization(source, destination, connection_with_normalization):
+def test_connection_lifecycle_with_normalization(source, destination, connection_with_normalization, workspace_id):
     assert source.was_created
     assert destination.was_created
     assert not connection_with_normalization.was_created
     connection_with_normalization.create()
     connection_with_normalization.state = connection_with_normalization._get_state_from_file(
-        connection_with_normalization.configuration_path
+        connection_with_normalization.configuration_path, workspace_id
     )
     assert connection_with_normalization.was_created
     assert connection_with_normalization.remote_resource["operations"][0]["operation_id"] is not None
