92eb5ca328ba0447a58d1dcf06c43b1e12f5e663
alafanechere <augustin.lafanechere@gmail.com>
2022-06-15 13:09:27 +0200

    revert changes on ../tools/bin/integration_tests_octavia.sh

--- tools/bin/integration_tests_octavia.sh
+++ tools/bin/integration_tests_octavia.sh
@@ -9,7 +9,7 @@ assert_root
 echo "Starting app..."
 
 # Detach so we can run subsequent commands
-VERSION=dev TRACKING_STRATEGY=logging docker-compose -f docker-compose.yaml
+VERSION=dev TRACKING_STRATEGY=logging docker-compose up -d
 
 # Sometimes source/dest containers using airbyte volumes survive shutdown, which need to be killed in order to shut down properly.
 shutdown_cmd="docker-compose down -v || docker kill \$(docker ps -a -f volume=airbyte_workspace -f volume=airbyte_data -f volume=airbyte_db -q) && docker-compose down -v"
@@ -19,5 +19,4 @@ echo "Waiting for services to begin"
 while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' localhost:8000/api/v1/health)" != "200" ]]; do echo "Waiting for docker deployment.."; sleep 5; done
 
 echo "Running integration tests via gradle"
-
 SUB_BUILD=OCTAVIA_CLI ./gradlew :octavia-cli:integrationTest --rerun-tasks --scan
