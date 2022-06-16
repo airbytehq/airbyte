3beeab6a519ac76ab47fcb4ca5a63cdd2de6a95f
Pedro Lopez <pedroslopez@me.com>
2022-06-16 19:48:27 -0400

    send stack traces from python connectors to sentry

--- airbyte-scheduler/scheduler-persistence/src/main/java/io/airbyte/scheduler/persistence/job_error_reporter/SentryErrorReportingClient.java
+++ airbyte-scheduler/scheduler-persistence/src/main/java/io/airbyte/scheduler/persistence/job_error_reporter/SentryErrorReportingClient.java
@@ -98,8 +98,56 @@ public class SentryErrorReportingClient implements ErrorReportingClient {
   }
 
   private List<SentryException> buildPythonSentryExceptions(final String stacktrace) {
-    // TODO
-    return null;
+    final List<SentryException> sentryExceptions = new ArrayList<>();
+    final String[] exceptions = stacktrace.split("\n\n"); // chained exceptions are separated by two new lines
+
+    for (final String exceptionStr : exceptions) {
+      final SentryStackTrace stackTrace = new SentryStackTrace();
+      final List<SentryStackFrame> stackFrames = new ArrayList<>();
+
+      // Use a regex to grab stack trace frame information
+      final Pattern framePattern = Pattern.compile("File \"(?<absPath>.+)\", line (?<lineno>[0-9]+), in (?<function>.+)\\n {4}(?<contextLine>.+)\\n");
+      final Matcher matcher = framePattern.matcher(exceptionStr);
+      int lastMatchIdx = -1;
+
+      while (matcher.find()) {
+        final String absPath = matcher.group("absPath");
+        final String lineno = matcher.group("lineno");
+        final String function = matcher.group("function");
+        final String contextLine = matcher.group("contextLine");
+
+        final SentryStackFrame stackFrame = new SentryStackFrame();
+        stackFrame.setAbsPath(absPath);
+        stackFrame.setLineno(Integer.valueOf(lineno));
+        stackFrame.setFunction(function);
+        stackFrame.setContextLine(contextLine);
+        stackFrames.add(stackFrame);
+
+        lastMatchIdx = matcher.end();
+      }
+
+      if (stackFrames.size() > 0) {
+        stackTrace.setFrames(stackFrames);
+
+        final SentryException sentryException = new SentryException();
+        sentryException.setStacktrace(stackTrace);
+
+        // The final part of our stack trace has the exception type and (optionally) a value (e.g. "RuntimeError: This is the value")
+        final String remaining = exceptionStr.substring(lastMatchIdx);
+        final String[] parts = remaining.split(":", 2);
+
+        if (parts.length > 0) {
+          sentryException.setType(parts[0]);
+          if (parts.length == 2) {
+            sentryException.setValue(parts[1]);
+          }
+
+          sentryExceptions.add(sentryException);
+        }
+      }
+    }
+
+    return sentryExceptions;
   }
 
   private List<SentryException> buildJavaSentryExceptions(final String stacktrace) {
