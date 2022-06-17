5984d43b000dee52c90b3a9a25fb101f65c03e1d
Volodymyr Petrov <volodymyr.s.petrov@globallogic.com>
2022-06-17 17:02:35 +0300

    create new PersonQuoteCover component

---
+++ airbyte-webapp/src/packages/cloud/views/auth/components/PersonQuoteCover.tsx
@@ -0,0 +1,9 @@
+import styles from "./PersonQuoteCover.module.scss";
+
+export const PersonQuoteCover = () => {
+  return (
+    <div className={styles.overlay}>
+      <div className={styles.container}></div>
+    </div>
+  );
+};
