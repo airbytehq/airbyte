99882f35dacfc045bbbabdf0d146489c8682d4fc
sajarin <sajarindider@gmail.com>
2022-06-17 10:54:25 -0400

    format: add double quotes and remove unused import

--- airbyte-integrations/connectors/source-webflow/unit_tests/test_source.py
+++ airbyte-integrations/connectors/source-webflow/unit_tests/test_source.py
@@ -5,13 +5,13 @@
 from unittest import TestCase
 from unittest.mock import MagicMock, patch
 
-from source_webflow.source import SourceWebflow, CollectionsList
+from source_webflow.source import SourceWebflow
 
 
 def test_check_connection(mocker):
     source = SourceWebflow()
     fake_info_record = {"collection": "is_mocked"}
-    with patch('source_webflow.source.CollectionsList.read_records', MagicMock(return_value=iter([fake_info_record]))):
+    with patch("source_webflow.source.CollectionsList.read_records", MagicMock(return_value=iter([fake_info_record]))):
         logger_mock, config_mock = MagicMock(), MagicMock()
         assert source.check_connection(logger_mock, config_mock) == (True, None)
         logger_mock.info.assert_called_once()
@@ -21,9 +21,8 @@ def test_check_connection(mocker):
 
 def test_streams(mocker):
     # use the "with" to prevent the patch from impacting other tests
-    with patch('source_webflow.source.SourceWebflow.generate_streams', MagicMock(return_value=["This would be a stream"])):
+    with patch("source_webflow.source.SourceWebflow.generate_streams", MagicMock(return_value=["This would be a stream"])):
         source = SourceWebflow()
         config_mock = MagicMock()
         streams = source.streams(config_mock)
         assert len(streams) == 1
-
