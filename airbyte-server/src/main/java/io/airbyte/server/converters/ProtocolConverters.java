12e4b8107ea289ba9085ea1d37039a6e1a7bf22d
Charles <charles@airbyte.io>
2022-06-22 10:52:54 -0700

    add streams to reset to job info (#13919)

--- airbyte-server/src/main/java/io/airbyte/server/converters/ProtocolConverters.java
+++ airbyte-server/src/main/java/io/airbyte/server/converters/ProtocolConverters.java
@@ -11,6 +11,10 @@ import io.airbyte.api.model.generated.StreamDescriptor;
  */
 public class ProtocolConverters {
 
+  public static StreamDescriptor streamDescriptorToApi(final io.airbyte.config.StreamDescriptor protocolStreamDescriptor) {
+    return new StreamDescriptor().name(protocolStreamDescriptor.getName()).namespace(protocolStreamDescriptor.getNamespace());
+  }
+
   public static StreamDescriptor streamDescriptorToApi(final io.airbyte.protocol.models.StreamDescriptor protocolStreamDescriptor) {
     return new StreamDescriptor().name(protocolStreamDescriptor.getName()).namespace(protocolStreamDescriptor.getNamespace());
   }
