1252989ff1bb2a27514ababc5f2739f8b605f769
Benoit Moriceau <benoit@airbyte.io>
2022-06-24 13:14:51 -0700

    Update test

--- airbyte-config/config-models/src/test/java/io/airbyte/config/helpers/StateMessageHelperTest.java
+++ airbyte-config/config-models/src/test/java/io/airbyte/config/helpers/StateMessageHelperTest.java
@@ -4,6 +4,7 @@
 
 package io.airbyte.config.helpers;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import com.google.common.collect.Lists;
 import io.airbyte.commons.json.Jsons;
 import io.airbyte.config.StateType;
@@ -60,6 +61,24 @@ public class StateMessageHelperTest {
   }
 
   @Test
+  public void testGlobalForceLegacy() {
+    final JsonNode legacyState = Jsons.jsonNode(1);
+    final AirbyteStateMessage stateMessage = new AirbyteStateMessage()
+        .withType(AirbyteStateType.GLOBAL)
+        .withGlobal(
+            new AirbyteGlobalState()
+                .withSharedState(Jsons.emptyObject())
+                .withStreamStates(Lists.newArrayList(
+                    new AirbyteStreamState().withStreamDescriptor(new StreamDescriptor().withName("a")).withStreamState(Jsons.emptyObject()),
+                    new AirbyteStreamState().withStreamDescriptor(new StreamDescriptor().withName("b")).withStreamState(Jsons.emptyObject()))))
+        .withData(legacyState);
+    final Optional<StateWrapper> stateWrapper = StateMessageHelper.getTypedState(Jsons.jsonNode(Lists.newArrayList(stateMessage)), false);
+    Assertions.assertThat(stateWrapper).isNotEmpty();
+    Assertions.assertThat(stateWrapper.get().getStateType()).isEqualTo(StateType.LEGACY);
+    Assertions.assertThat(stateWrapper.get().getLegacyState()).isEqualTo(legacyState);
+  }
+
+  @Test
   public void testStream() {
     final AirbyteStateMessage stateMessage1 = new AirbyteStateMessage()
         .withType(AirbyteStateType.STREAM)
@@ -74,6 +93,27 @@ public class StateMessageHelperTest {
     Assertions.assertThat(stateWrapper).isNotEmpty();
     Assertions.assertThat(stateWrapper.get().getStateType()).isEqualTo(StateType.STREAM);
     Assertions.assertThat(stateWrapper.get().getStateMessages()).containsExactlyInAnyOrder(stateMessage1, stateMessage2);
+  }
+
+  @Test
+  public void testStreamForceLegacy() {
+    final JsonNode firstEmittedLegacyState = Jsons.jsonNode(1);
+    final AirbyteStateMessage stateMessage1 = new AirbyteStateMessage()
+        .withType(AirbyteStateType.STREAM)
+        .withStream(
+            new AirbyteStreamState().withStreamDescriptor(new StreamDescriptor().withName("a")).withStreamState(Jsons.emptyObject()))
+        .withData(firstEmittedLegacyState);
+    final JsonNode secondEmittedLegacyState = Jsons.jsonNode(2);
+    final AirbyteStateMessage stateMessage2 = new AirbyteStateMessage()
+        .withType(AirbyteStateType.STREAM)
+        .withStream(
+            new AirbyteStreamState().withStreamDescriptor(new StreamDescriptor().withName("b")).withStreamState(Jsons.emptyObject()))
+        .withData(secondEmittedLegacyState);
+    final Optional<StateWrapper> stateWrapper =
+        StateMessageHelper.getTypedState(Jsons.jsonNode(Lists.newArrayList(stateMessage1, stateMessage2)), false);
+    Assertions.assertThat(stateWrapper).isNotEmpty();
+    Assertions.assertThat(stateWrapper.get().getStateType()).isEqualTo(StateType.LEGACY);
+    Assertions.assertThat(stateWrapper.get().getLegacyState()).isEqualTo(secondEmittedLegacyState);
   }
 
   @Test
