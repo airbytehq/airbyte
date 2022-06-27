8699cba6c3eb8f4a8409ae8afccf29442f3dd7b7
Sergey Chvalyuk <grubberr@gmail.com>
2022-06-27 08:50:12 +0300

    measure time
    Signed-off-by: Sergey Chvalyuk <grubberr@gmail.com>

--- airbyte-cdk/python/unit_tests/utils/test_concurrent.py
+++ airbyte-cdk/python/unit_tests/utils/test_concurrent.py
@@ -2,6 +2,7 @@
 # Copyright (c) 2022 Airbyte, Inc., all rights reserved.
 #
 
+import time
 from itertools import product
 from unittest.mock import MagicMock
 
@@ -12,19 +13,23 @@ class Stream:
     max_workers = 5
 
     def stream_slices(self, **kwargs):
-        yield from range(1, 20)
+        yield from range(1, 11)
 
     def read_records(self, *, stream_slice, **kwargs):
-        for record in range(1, 20):
+        for record in range(1, 11):
             yield stream_slice, record
+            # for thread context switching
+            time.sleep(0.01)
 
 
 def test_read_full_refresh():
     stream_instance = Stream()
     stream_instance.logger = MagicMock()
     records = []
+    start_time = time.time()
     with ConcurrentStreamReader(stream_instance, MagicMock()) as reader:
         for record in reader:
             records.append(record)
 
-    assert records == list(product(range(1, 20), range(1, 20)))
+    assert records == list(product(range(1, 11), range(1, 11)))
+    assert 0 < time.time() - start_time < 0.25
