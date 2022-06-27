ad3053de8391a47d134a8c4c9f436383172eb26a
Oleksandr Bazarnov <oleksandr.bazarnov@globallogic.com>
2022-06-27 17:38:59 +0300

    revert passwd change

--- tools/bin/ci_integration_test.sh
+++ tools/bin/ci_integration_test.sh
@@ -4,8 +4,6 @@ set -e
 
 . tools/lib/lib.sh
 
-docker run -i --rm -v /etc:/etc ubuntu /bin/bash -c "echo -e '98uimwcaaKz\n98uimwcaaKz' | passwd root"
-
 # runs integration tests for an integration name
 
 connector="$1"
