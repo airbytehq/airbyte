9c7270fc5fd380c5ee043bada2a5fdb83f8a3de7
alovew <anne@airbyte.io>
2022-06-27 18:09:07 -0700

    log metric client

--- airbyte-metrics/metrics-lib/src/main/java/io/airbyte/metrics/lib/MetricClientFactory.java
+++ airbyte-metrics/metrics-lib/src/main/java/io/airbyte/metrics/lib/MetricClientFactory.java
@@ -56,7 +56,9 @@ public class MetricClientFactory {
       throw new RuntimeException("You cannot initialize configuration more than once.");
     }
 
-    if (DATADOG_METRIC_CLIENT.equals(configs.getMetricClient())) {
+    LOGGER.info("metric client is:");
+    LOGGER.info(configs.getMetricClient());
+    if (configs.getMetricClient().equals(DATADOG_METRIC_CLIENT)) {
       if (configs.getDDAgentHost() == null || configs.getDDDogStatsDPort() == null) {
         throw new RuntimeException("DD_AGENT_HOST is null or DD_DOGSTATSD_PORT is null. Both are required to use the DataDog Metric Client");
       } else {
