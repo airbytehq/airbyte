a8a9d7656119fd8760e6335868350438f453ed8e
lmossman <lake@airbyte.io>
2022-06-27 18:54:28 -0700

    add acceptance test for resetting while sync is running

--- airbyte-tests/src/acceptanceTests/java/io/airbyte/test/acceptance/BasicAcceptanceTests.java
+++ airbyte-tests/src/acceptanceTests/java/io/airbyte/test/acceptance/BasicAcceptanceTests.java
@@ -671,6 +671,46 @@ public class BasicAcceptanceTests {
     assertEquals(JobConfigType.RESET_CONNECTION, jobInfoRead.getJob().getConfigType());
   }
 
+  @Test
+  @Order(17)
+  public void testResetCancelsRunningSync() throws Exception {
+    final SourceDefinitionRead sourceDefinition = createE2eSourceDefinition();
+
+    final SourceRead source = createSource(
+        "E2E Test Source -" + UUID.randomUUID(),
+        workspaceId,
+        sourceDefinition.getSourceDefinitionId(),
+        Jsons.jsonNode(ImmutableMap.builder()
+            .put("type", "INFINITE_FEED")
+            .put("message_interval", 1000)
+            .put("max_records", Duration.ofMinutes(5).toSeconds())
+            .build()));
+
+    final String connectionName = "test-connection";
+    final UUID sourceId = source.getSourceId();
+    final UUID destinationId = createDestination().getDestinationId();
+    final UUID operationId = createOperation().getOperationId();
+    final AirbyteCatalog catalog = discoverSourceSchema(sourceId);
+    final SyncMode syncMode = SyncMode.FULL_REFRESH;
+    final DestinationSyncMode destinationSyncMode = DestinationSyncMode.OVERWRITE;
+    catalog.getStreams().forEach(s -> s.getConfig().syncMode(syncMode).destinationSyncMode(destinationSyncMode));
+    final UUID connectionId =
+        createConnection(connectionName, sourceId, destinationId, List.of(operationId), catalog, null).getConnectionId();
+    final JobInfoRead connectionSyncRead = apiClient.getConnectionApi().syncConnection(new ConnectionIdRequestBody().connectionId(connectionId));
+
+    // wait to get out of PENDING
+    final JobRead jobRead = waitWhileJobHasStatus(apiClient.getJobsApi(), connectionSyncRead.getJob(), Set.of(JobStatus.PENDING));
+    assertEquals(JobStatus.RUNNING, jobRead.getStatus());
+
+    // send reset request while sync is still running
+    final JobInfoRead jobInfoRead = apiClient.getConnectionApi().resetConnection(new ConnectionIdRequestBody().connectionId(connectionId));
+    assertEquals(JobConfigType.RESET_CONNECTION, jobInfoRead.getJob().getConfigType());
+
+    // verify that sync job was cancelled
+    final JobRead connectionSyncReadAfterReset = apiClient.getJobsApi().getJobInfo(new JobIdRequestBody().id(connectionSyncRead.getJob().getId())).getJob();
+    assertEquals(JobStatus.CANCELLED, connectionSyncReadAfterReset.getStatus());
+  }
+
   // This test is disabled because it takes a couple minutes to run, as it is testing timeouts.
   // It should be re-enabled when the @SlowIntegrationTest can be applied to it.
   // See relevant issue: https://github.com/airbytehq/airbyte/issues/8397
