cbe4e578e099d3a070884ab7396955a2a9587dd6
Alexandre Girard <alexandre@airbyte.io>
2022-06-29 13:52:07 -0700

    fix

--- airbyte-cdk/python/airbyte_cdk/sources/declarative/retrievers/simple_retriever.py
+++ airbyte-cdk/python/airbyte_cdk/sources/declarative/retrievers/simple_retriever.py
@@ -106,7 +106,7 @@ class SimpleRetriever(Retriever, HttpStream):
         Specifies request headers.
         Authentication headers will overwrite any overlapping headers returned from this method.
         """
-        return self._requester.request_headers(stream_state, stream_slice, next_page_token)
+        return self._requester.request_headers(self.state, stream_slice, next_page_token)
 
     def request_body_data(
         self,
@@ -123,7 +123,7 @@ class SimpleRetriever(Retriever, HttpStream):
 
         At the same time only one of the 'request_body_data' and 'request_body_json' functions can be overridden.
         """
-        return self._requester.request_body_data(stream_state, stream_slice, next_page_token)
+        return self._requester.request_body_data(self.state, stream_slice, next_page_token)
 
     def request_body_json(
         self,
@@ -136,7 +136,7 @@ class SimpleRetriever(Retriever, HttpStream):
 
         At the same time only one of the 'request_body_data' and 'request_body_json' functions can be overridden.
         """
-        return self._requester.request_body_json(stream_state, stream_slice, next_page_token)
+        return self._requester.request_body_json(self.state, stream_slice, next_page_token)
 
     def request_kwargs(
         self,
@@ -149,12 +149,12 @@ class SimpleRetriever(Retriever, HttpStream):
         Any option listed in https://docs.python-requests.org/en/latest/api/#requests.adapters.BaseAdapter.send for can be returned from
         this method. Note that these options do not conflict with request-level options such as headers, request params, etc..
         """
-        return self._requester.request_kwargs(stream_state, stream_slice, next_page_token)
+        return self._requester.request_kwargs(self.state, stream_slice, next_page_token)
 
     def path(
         self, *, stream_state: Mapping[str, Any] = None, stream_slice: Mapping[str, Any] = None, next_page_token: Mapping[str, Any] = None
     ) -> str:
-        return self._requester.get_path(stream_state=stream_state, stream_slice=stream_slice, next_page_token=next_page_token)
+        return self._requester.get_path(stream_state=self.state, stream_slice=stream_slice, next_page_token=next_page_token)
 
     def request_params(
         self,
@@ -167,7 +167,7 @@ class SimpleRetriever(Retriever, HttpStream):
 
         E.g: you might want to define query parameters for paging if next_page_token is not None.
         """
-        return self._requester.request_params(stream_state, stream_slice, next_page_token)
+        return self._requester.request_params(self.state, stream_slice, next_page_token)
 
     @property
     def cache_filename(self):
@@ -193,7 +193,7 @@ class SimpleRetriever(Retriever, HttpStream):
     ) -> Iterable[Mapping]:
         self._last_response = response
         records = self._record_selector.select_records(
-            response=response, stream_state=stream_state, stream_slice=stream_slice, next_page_token=next_page_token
+            response=response, stream_state=self.state, stream_slice=stream_slice, next_page_token=next_page_token
         )
         self._last_records = records
         return records
@@ -219,14 +219,13 @@ class SimpleRetriever(Retriever, HttpStream):
         stream_slice: Mapping[str, Any] = None,
         stream_state: Mapping[str, Any] = None,
     ) -> Iterable[Mapping[str, Any]]:
+        stream_state = self.state
         records_generator = HttpStream.read_records(self, sync_mode, cursor_field, stream_slice, stream_state)
         for r in records_generator:
             self._state.update_state(stream_slice=stream_slice, stream_state=stream_state, last_response=self._last_response, last_record=r)
             yield r
         else:
-            self._state.update_state(
-                stream_slice=stream_slice, stream_state=stream_state, last_reponse=self._last_response, last_record=None
-            )
+            self._state.update_state(stream_slice=stream_slice, stream_state=stream_state, last_reponse=self._last_response)
             yield from []
 
     def stream_slices(
@@ -240,8 +239,7 @@ class SimpleRetriever(Retriever, HttpStream):
         :param stream_state:
         :return:
         """
-        # FIXME: this is not passing the cursor field because it is always known at init time
-        return self._iterator.stream_slices(sync_mode, stream_state)
+        return self._iterator.stream_slices(sync_mode, self.state)
 
     @property
     def state(self) -> MutableMapping[str, Any]:
