be6c88113a1bd0c1c473c693fc9a006563c24431
Yashkumar Makwana <64660381+makyash@users.noreply.github.com>
2022-06-29 15:39:54 -0500

    Added Pagination

--- airbyte-integrations/connectors/source-timely/source_timely_integration/source.py
+++ airbyte-integrations/connectors/source-timely/source_timely_integration/source.py
@@ -1,32 +1,37 @@
 from abc import ABC
-from typing import Any, Iterable, List, Mapping, MutableMapping, Optional, Tuple
-
-import requests
 from datetime import datetime
+from typing import Any, Iterable, List, Mapping, MutableMapping, Optional, Tuple
+from urllib.parse import parse_qs, urlparse
+import requests
 from airbyte_cdk.sources import AbstractSource
 from airbyte_cdk.sources.streams import Stream
 from airbyte_cdk.sources.streams.http import HttpStream
-from airbyte_cdk.sources.streams.http.auth import TokenAuthenticator
-# import ndjson
-import json
+
 
 # Basic full refresh stream
 class TimelyIntegrationStream(HttpStream, ABC):
+    FIRST_PAGE = 1
 
     url_base = "https://api.timelyapp.com/1.1/"
 
-    def __init__(self, account_id:str, start_date:str, bearer_token:str,**kwargs):
+    def __init__(self, account_id: str, start_date: str, bearer_token: str, **kwargs):
         super().__init__(**kwargs)
         self.account_id = account_id
         self.start_date = start_date
         self.bearer_token = bearer_token
 
-    def request_params(self, 
-                    stream_state: Mapping[str, any], 
-                    stream_slice: Mapping[str, Any]= None,
-                    next_page_token: Mapping[str, Any]= None) -> MutableMapping[str, Any]:
-        return {"account_id":self.account_id}
-    
+    def request_params(
+        self, stream_state: Mapping[str, any], stream_slice: Mapping[str, Any] = None, next_page_token: Mapping[str, Any] = None
+    ) -> MutableMapping[str, Any]:
+
+        if next_page_token is None:
+            return {"page": self.FIRST_PAGE, 
+                    "per_page":"1000",
+                    "account_id": self.account_id}
+
+        return next_page_token
+
+
     def request_headers(self, **kwargs) -> Mapping[str, Any]:
         bearer_token = self.bearer_token
         event_headers = {"Authorization": f"Bearer {bearer_token}", "Content-Type": "application/json"}
@@ -35,39 +40,45 @@ class TimelyIntegrationStream(HttpStream, ABC):
     def parse_response(
         self,
         response: requests.Response,
-        stream_state: Mapping[str, Any], 
-        stream_slice: Mapping[str, Any] = None, 
-        next_page_token: Mapping[str, Any] = None
+        stream_state: Mapping[str, Any],
+        stream_slice: Mapping[str, Any] = None,
+        next_page_token: Mapping[str, Any] = None,
     ) -> Iterable[Mapping]:
-        # input = [response.json()]
-        # data = json.loads(input)
-        # output = ndjson.dumps(data)
-        # return output
+
         return response.json()
 
     def next_page_token(self, response: requests.Response) -> Optional[Mapping[str, Any]]:
-        return None
 
-#Incremental
+        results = response.json()
+
+        if results:
+            if len(results) > 0:
+                url_query = urlparse(response.url).query
+                query_params = parse_qs(url_query)
+
+                new_params = {param_name: param_value[0] for param_name, param_value in query_params.items()}
+                if "page" in new_params:
+                    new_params["page"] = int(new_params["page"]) + 1
+                return new_params
 
 class events(TimelyIntegrationStream):
-    
+
     primary_key = "id"
-    
+
     def path(self, **kwargs) -> str:
         account_id = self.account_id
         start_date = self.start_date
-        upto = datetime.today().strftime('%Y-%m-%d')
+        upto = datetime.today().strftime("%Y-%m-%d")
         return f"{account_id}/events?since={start_date}&upto={upto}"
 
-class SourceTimelyIntegration(AbstractSource):
 
+class SourceTimelyIntegration(AbstractSource):
     def check_connection(self, logger, config) -> Tuple[bool, any]:
         account_id = config["account_id"]
         start_date = config["start_date"]
         bearer_token = config["bearer_token"]
 
-        headers = {"Authorization":f"Bearer {bearer_token}","Content-Type":"application/json"}
+        headers = {"Authorization": f"Bearer {bearer_token}", "Content-Type": "application/json"}
         url = f"https://api.timelyapp.com/1.1/{account_id}/events?since={start_date}&upto=2022-05-01"
 
         try:
@@ -79,11 +90,5 @@ class SourceTimelyIntegration(AbstractSource):
 
     def streams(self, config: Mapping[str, Any]) -> List[Stream]:
 
-        #authroization = TokenAuthenticator(config["bearer_token"])
-
-        args={
-            "bearer_token": config["bearer_token"],
-            "account_id":config["account_id"],
-            "start_date":config["start_date"]
-        }
+        args = {"bearer_token": config["bearer_token"], "account_id": config["account_id"], "start_date": config["start_date"]}
         return [events(**args)]
