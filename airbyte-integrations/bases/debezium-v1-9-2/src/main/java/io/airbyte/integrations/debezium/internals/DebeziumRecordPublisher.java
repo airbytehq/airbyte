95e2e850e3f7736443ae366c3ef2bd01dd240fcb
Liren Tu <tuliren.git@outlook.com>
2022-06-29 02:06:24 -0700

    Update logging

--- airbyte-integrations/bases/debezium-v1-9-2/src/main/java/io/airbyte/integrations/debezium/internals/DebeziumRecordPublisher.java
+++ airbyte-integrations/bases/debezium-v1-9-2/src/main/java/io/airbyte/integrations/debezium/internals/DebeziumRecordPublisher.java
@@ -79,7 +79,7 @@ public class DebeziumRecordPublisher implements AutoCloseable {
           public void taskStarted() {
             ConnectorCallback.super.taskStarted();
             debeziumTracker.markAsStarted();
-            LOGGER.info("Debezium source task has started...");
+            LOGGER.info("Debezium connector task started");
           }
 
         })
