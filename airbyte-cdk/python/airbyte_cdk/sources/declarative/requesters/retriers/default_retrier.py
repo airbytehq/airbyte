eef3a2be8f700f42df7851b5ec8f9bddc49c7668
Alexandre Girard <alexandre@airbyte.io>
2022-06-30 14:11:00 -0700

    return response status

--- airbyte-cdk/python/airbyte_cdk/sources/declarative/requesters/retriers/default_retrier.py
+++ airbyte-cdk/python/airbyte_cdk/sources/declarative/requesters/retriers/default_retrier.py
@@ -2,6 +2,8 @@
 # Copyright (c) 2022 Airbyte, Inc., all rights reserved.
 #
 
+from dataclasses import dataclass
+from enum import Enum
 from typing import List, Optional, Union
 
 import requests
@@ -13,8 +15,8 @@ class HttpResponseFilter:
     TOO_MANY_REQUESTS_ERRORS = [429]
     DEFAULT_RETRIABLE_ERRORS = [x for x in range(500, 600)] + TOO_MANY_REQUESTS_ERRORS
 
-    def __init__(self, http_codes: List[int] = DEFAULT_RETRIABLE_ERRORS, predicate: str = ""):
-        self._http_codes = http_codes
+    def __init__(self, http_codes: List[int] = None, predicate: str = ""):
+        self._http_codes = http_codes or HttpResponseFilter.DEFAULT_RETRIABLE_ERRORS
         self._predicate = InterpolatedBoolean(predicate)
 
     def matches(self, response: requests.Response) -> bool:
@@ -23,11 +25,32 @@ class HttpResponseFilter:
         )
 
 
+class NonRetriableResponseStatus(Enum):
+    Ok = ("OK",)
+    FAIL = ("FAIL",)
+    IGNORE = ("IGNORE",)
+
+
+@dataclass
+class RetryResponseStatus:
+    retry_in: Optional[float]
+
+
+ResponseStatus = Union[NonRetriableResponseStatus, RetryResponseStatus]
+
+
 class DefaultRetrier(Retrier):
-    def __init__(self, response_filter: HttpResponseFilter = None, max_retries: Optional[int] = 5, retry_factor: float = 5):
+    def __init__(
+        self,
+        retry_response_filter: HttpResponseFilter = None,
+        ignore_response_filter: HttpResponseFilter = None,
+        max_retries: Optional[int] = 5,
+        retry_factor: float = 5,
+    ):
         self._max_retries = max_retries
         self._retry_factor = retry_factor
-        self._http_response_filter = response_filter or HttpResponseFilter()
+        self._retry_response_filter = retry_response_filter or HttpResponseFilter()
+        self._ignore_response_filter = ignore_response_filter or HttpResponseFilter([])
 
     @property
     def max_retries(self) -> Union[int, None]:
@@ -37,8 +60,15 @@ class DefaultRetrier(Retrier):
     def retry_factor(self) -> float:
         return self._retry_factor
 
-    def should_retry(self, response: requests.Response) -> bool:
-        return self._http_response_filter.matches(response)
+    def should_retry(self, response: requests.Response) -> ResponseStatus:
+        if self._retry_response_filter.matches(response):
+            return RetryResponseStatus(self.backoff_time(response))
+        elif self._ignore_response_filter.matches(response):
+            return NonRetriableResponseStatus.IGNORE
+        elif response.ok:
+            return NonRetriableResponseStatus.Ok
+        else:
+            return NonRetriableResponseStatus.FAIL
 
     def backoff_time(self, response: requests.Response) -> Optional[float]:
         return None
--- airbyte-cdk/python/unit_tests/sources/declarative/requesters/retriers/test_retrier.py
+++ airbyte-cdk/python/unit_tests/sources/declarative/requesters/retriers/test_retrier.py
@@ -5,7 +5,12 @@ from http import HTTPStatus
 from unittest.mock import MagicMock, patch
 
 import pytest
-from airbyte_cdk.sources.declarative.requesters.retriers.default_retrier import DefaultRetrier, HttpResponseFilter
+from airbyte_cdk.sources.declarative.requesters.retriers.default_retrier import (
+    DefaultRetrier,
+    HttpResponseFilter,
+    NonRetriableResponseStatus,
+    RetryResponseStatus,
+)
 
 
 @pytest.mark.parametrize(
@@ -27,29 +32,52 @@ def test_something(time_mock, test_name, http_code, response_headers, should_ret
 
 
 @pytest.mark.parametrize(
-    "test_name, http_code, response_filter, response_headers, should_retry, backoff_time",
+    "test_name, http_code, retry_response_filter, ignore_response_filter, response_headers, should_retry, backoff_time",
     [
-        ("test_bad_gateway", HTTPStatus.BAD_GATEWAY, None, {}, True, None),
-        ("test_403", HTTPStatus.FORBIDDEN, None, {}, False, None),
-        ("test_429", HTTPStatus.TOO_MANY_REQUESTS, None, {}, True, None),
-        ("test_403_with_predicate", HTTPStatus.FORBIDDEN, HttpResponseFilter(predicate="{{ 'code' in decoded_response }}"), {}, True, None),
+        ("test_bad_gateway", HTTPStatus.BAD_GATEWAY, None, None, {}, RetryResponseStatus(None), None),
+        ("test_200", HTTPStatus.OK, None, None, {}, NonRetriableResponseStatus.Ok, None),
+        ("test_3XX", HTTPStatus.PERMANENT_REDIRECT, None, None, {}, NonRetriableResponseStatus.Ok, None),
+        ("test_403", HTTPStatus.FORBIDDEN, None, None, {}, NonRetriableResponseStatus.FAIL, None),
+        ("test_429", HTTPStatus.TOO_MANY_REQUESTS, None, None, {}, RetryResponseStatus(None), None),
+        (
+            "test_ignore_403",
+            HTTPStatus.FORBIDDEN,
+            None,
+            HttpResponseFilter(http_codes=[HTTPStatus.FORBIDDEN]),
+            {},
+            NonRetriableResponseStatus.IGNORE,
+            None,
+        ),
+        (
+            "test_403_with_predicate",
+            HTTPStatus.FORBIDDEN,
+            HttpResponseFilter(predicate="{{ 'code' in decoded_response }}"),
+            None,
+            {},
+            RetryResponseStatus(None),
+            None,
+        ),
         (
             "test_403_with_predicate",
             HTTPStatus.FORBIDDEN,
             HttpResponseFilter(predicate="{{ 'some_absent_field' in decoded_response }}"),
+            None,
             {},
-            False,
+            NonRetriableResponseStatus.FAIL,
             None,
         ),
-        ("test_retry_403", HTTPStatus.FORBIDDEN, HttpResponseFilter([HTTPStatus.FORBIDDEN]), {}, True, None),
+        ("test_retry_403", HTTPStatus.FORBIDDEN, HttpResponseFilter([HTTPStatus.FORBIDDEN]), None, {}, RetryResponseStatus(None), None),
     ],
 )
 @patch("time.time", return_value=1655804424.0)
-def test_default_retrier(time_mock, test_name, http_code, response_filter, response_headers, should_retry, backoff_time):
+def test_default_retrier(
+    time_mock, test_name, http_code, retry_response_filter, ignore_response_filter, response_headers, should_retry, backoff_time
+):
     response_mock = MagicMock()
     response_mock.status_code = http_code
     response_mock.headers = response_headers
     response_mock.json.return_value = {"code": "1000"}
-    retrier = DefaultRetrier(response_filter)
+    response_mock.ok = http_code < 400
+    retrier = DefaultRetrier(retry_response_filter=retry_response_filter, ignore_response_filter=ignore_response_filter)
     assert retrier.should_retry(response_mock) == should_retry
     assert retrier.backoff_time(response_mock) == backoff_time
