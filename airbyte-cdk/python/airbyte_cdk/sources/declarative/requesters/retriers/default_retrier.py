22a22966a9a547026b3e40181eab2f00414ac74b
Alexandre Girard <alexandre@airbyte.io>
2022-06-30 13:20:25 -0700

    start working on retrier

--- airbyte-cdk/python/airbyte_cdk/sources/declarative/requesters/retriers/default_retrier.py
+++ airbyte-cdk/python/airbyte_cdk/sources/declarative/requesters/retriers/default_retrier.py
@@ -2,16 +2,28 @@
 # Copyright (c) 2022 Airbyte, Inc., all rights reserved.
 #
 
-from typing import Optional, Union
+from typing import List, Optional, Union
 
 import requests
 from airbyte_cdk.sources.declarative.requesters.retriers.retrier import Retrier
 
 
+class HttpResponseFilter:
+    TOO_MANY_REQUESTS_ERRORS = [429]
+    DEFAULT_RETRIABLE_ERRORS = [x for x in range(500, 600)] + TOO_MANY_REQUESTS_ERRORS
+
+    def __init__(self, http_codes: List[int]):
+        self._http_codes = http_codes
+
+    def matches(self, response: requests.Response) -> bool:
+        return response.status_code in self._http_codes
+
+
 class DefaultRetrier(Retrier):
-    def __init__(self, max_retries: Optional[int] = 5, retry_factor: float = 5):
+    def __init__(self, response_filter: HttpResponseFilter = None, max_retries: Optional[int] = 5, retry_factor: float = 5):
         self._max_retries = max_retries
         self._retry_factor = retry_factor
+        self._http_response_filter = response_filter or HttpResponseFilter(HttpResponseFilter.DEFAULT_RETRIABLE_ERRORS)
 
     @property
     def max_retries(self) -> Union[int, None]:
@@ -22,7 +34,7 @@ class DefaultRetrier(Retrier):
         return self._retry_factor
 
     def should_retry(self, response: requests.Response) -> bool:
-        return response.status_code == 429 or 500 <= response.status_code < 600
+        return self._http_response_filter.matches(response)
 
     def backoff_time(self, response: requests.Response) -> Optional[float]:
         return None
---
+++ airbyte-cdk/python/unit_tests/sources/declarative/requesters/retriers/test_retrier.py
@@ -0,0 +1,45 @@
+#
+# Copyright (c) 2022 Airbyte, Inc., all rights reserved.
+#
+from http import HTTPStatus
+from unittest.mock import MagicMock, patch
+
+import pytest
+from airbyte_cdk.sources.declarative.requesters.retriers.default_retrier import DefaultRetrier, HttpResponseFilter
+
+
+@pytest.mark.parametrize(
+    "test_name, http_code, response_headers, should_retry, backoff_time",
+    [
+        ("test_bad_gateway", HTTPStatus.BAD_GATEWAY, {}, True, 60),
+        ("test_retry_after", HTTPStatus.BAD_GATEWAY, {"Retry-After": 120}, True, 120),
+        ("test_bad_gateway", HTTPStatus.BAD_GATEWAY, {"X-RateLimit-Reset": 1655804724}, True, 300.0),
+    ],
+)
+@patch("time.time", return_value=1655804424.0)
+def test_something(time_mock, test_name, http_code, response_headers, should_retry, backoff_time):
+    response_mock = MagicMock()
+    response_mock.status_code = http_code
+    response_mock.headers = response_headers
+    retrier = DefaultRetrier()
+    assert retrier.should_retry(response_mock) == should_retry
+    assert retrier.backoff_time(response_mock) == backoff_time
+
+
+@pytest.mark.parametrize(
+    "test_name, http_code, response_filter, response_headers, should_retry, backoff_time",
+    [
+        ("test_bad_gateway", HTTPStatus.BAD_GATEWAY, None, {}, True, None),
+        ("test_403", HTTPStatus.FORBIDDEN, None, {}, False, None),
+        ("test_429", HTTPStatus.TOO_MANY_REQUESTS, None, {}, True, None),
+        ("test_retry_403", HTTPStatus.FORBIDDEN, HttpResponseFilter([HTTPStatus.FORBIDDEN]), {}, True, None),
+    ],
+)
+@patch("time.time", return_value=1655804424.0)
+def test_default_retrier(time_mock, test_name, http_code, response_filter, response_headers, should_retry, backoff_time):
+    response_mock = MagicMock()
+    response_mock.status_code = http_code
+    response_mock.headers = response_headers
+    retrier = DefaultRetrier(response_filter)
+    assert retrier.should_retry(response_mock) == should_retry
+    assert retrier.backoff_time(response_mock) == backoff_time
