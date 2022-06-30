5da17a484bb64d69d57b6fa94cf8c8ee61bd4e06
Arsen Losenko <arsen.losenko@globallogic.com>
2022-06-30 16:33:44 +0300

    Specify integer for dimension ga:dateHourMinute

--- airbyte-integrations/connectors/source-google-analytics-v4/unit_tests/unit_test.py
+++ airbyte-integrations/connectors/source-google-analytics-v4/unit_tests/unit_test.py
@@ -147,7 +147,7 @@ def test_metrics_dimensions_type_list(mock_metrics_dimensions_type_list_link):
 def get_metrics_dimensions_mapping():
     test_metrics_dimensions_map = {
         "metric": [("ga:users", "integer"), ("ga:newUsers", "integer")],
-        "dimension": [("ga:dimension", "string")],
+        "dimension": [("ga:dimension", "string"), ("ga:dateHourMinute", "integer")],
     }
     for field_type, attribute_expected_pairs in test_metrics_dimensions_map.items():
         for attribute_expected_pair in attribute_expected_pairs:
