ecbfcf3593d9576dc13769fc847f441269cab996
Alexandre Girard <alexandre@airbyte.io>
2022-07-01 15:41:12 -0700

    add declarative stream to registry

--- airbyte-cdk/python/airbyte_cdk/sources/declarative/parsers/default_implementation_registry.py
+++ airbyte-cdk/python/airbyte_cdk/sources/declarative/parsers/default_implementation_registry.py
@@ -6,6 +6,7 @@ from typing import Mapping, Type
 
 from airbyte_cdk.sources.declarative.checks.check_stream import CheckStream
 from airbyte_cdk.sources.declarative.checks.connection_checker import ConnectionChecker
+from airbyte_cdk.sources.declarative.declarative_stream import DeclarativeStream
 from airbyte_cdk.sources.declarative.decoders.decoder import Decoder
 from airbyte_cdk.sources.declarative.decoders.json_decoder import JsonDecoder
 from airbyte_cdk.sources.declarative.extractors.http_selector import HttpSelector
@@ -26,6 +27,7 @@ from airbyte_cdk.sources.declarative.states.dict_state import DictState
 from airbyte_cdk.sources.declarative.states.state import State
 from airbyte_cdk.sources.declarative.stream_slicers.single_slice import SingleSlice
 from airbyte_cdk.sources.declarative.stream_slicers.stream_slicer import StreamSlicer
+from airbyte_cdk.sources.streams.core import Stream
 
 DEFAULT_IMPLEMENTATIONS_REGISTRY: Mapping[Type, Type] = {
     Requester: HttpRequester,
@@ -40,4 +42,5 @@ DEFAULT_IMPLEMENTATIONS_REGISTRY: Mapping[Type, Type] = {
     StreamSlicer: SingleSlice,
     Paginator: NoPagination,
     HttpResponseFilter: HttpResponseFilter,
+    Stream: DeclarativeStream,
 }
