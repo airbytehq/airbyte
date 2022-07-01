0f80b12348d004eb9055489d78e28afbe6a445d7
Alexandre Girard <alexandre@airbyte.io>
2022-07-01 08:39:19 -0700

    Alex/declarative stream incremental fix (#14268)
    * checkout files from test branch
    
    * read_incremental works
    
    * reset to master
    
    * remove dead code
    
    * comment
    
    * fix
    
    * Add test
    
    * comments
    
    * utc
    
    * format
    
    * small fix
    
    * Add test with rfc3339
    
    * remove unused param
    
    * fix test

--- airbyte-cdk/python/airbyte_cdk/sources/declarative/interpolation/jinja.py
+++ airbyte-cdk/python/airbyte_cdk/sources/declarative/interpolation/jinja.py
@@ -2,9 +2,12 @@
 # Copyright (c) 2022 Airbyte, Inc., all rights reserved.
 #
 
+import ast
 import datetime
+import numbers
 
 from airbyte_cdk.sources.declarative.interpolation.interpolation import Interpolation
+from dateutil import parser
 from jinja2 import Environment
 from jinja2.exceptions import UndefinedError
 
@@ -17,6 +20,12 @@ class JinjaInterpolation(Interpolation):
         self._environment.globals["now_local"] = datetime.datetime.now
         self._environment.globals["now_utc"] = lambda: datetime.datetime.now(datetime.timezone.utc)
         self._environment.globals["today_utc"] = lambda: datetime.datetime.now(datetime.timezone.utc).date()
+        self._environment.globals["timestamp"] = (
+            lambda dt: int(dt)
+            if isinstance(dt, numbers.Number)
+            else int(parser.parse(dt).replace(tzinfo=datetime.timezone.utc).timestamp())
+        )
+        self._environment.globals["max"] = lambda a, b: max(a, b)
 
     def eval(self, input_str: str, config, default=None, **kwargs):
         context = {"config": config, **kwargs}
@@ -24,14 +33,20 @@ class JinjaInterpolation(Interpolation):
             if isinstance(input_str, str):
                 result = self._eval(input_str, context)
                 if result:
-                    return result
+                    return self._literal_eval(result)
             else:
                 # If input is not a string, return it as is
                 raise Exception(f"Expected a string. got {input_str}")
         except UndefinedError:
             pass
         # If result is empty or resulted in an undefined error, evaluate and return the default string
-        return self._eval(default, context)
+        return self._literal_eval(self._eval(default, context))
+
+    def _literal_eval(self, result):
+        try:
+            return ast.literal_eval(result)
+        except (ValueError, SyntaxError):
+            return result
 
     def _eval(self, s: str, context):
         try:
