994d1252578ae4545ee53a1763fa68de8d493aca
Alexandre Girard <alexandre@airbyte.io>
2022-07-01 12:12:11 -0700

    comment

--- airbyte-cdk/python/airbyte_cdk/sources/declarative/requesters/retriers/backoff_strategies/exponential_backoff_strategy.py
+++ airbyte-cdk/python/airbyte_cdk/sources/declarative/requesters/retriers/backoff_strategies/exponential_backoff_strategy.py
@@ -10,4 +10,5 @@ from airbyte_cdk.sources.declarative.requesters.retriers.backoff_strategy import
 
 class ExponentialBackoffStrategy(BackoffStrategy):
     def backoff(self, response: requests.Response) -> Optional[float]:
+        # Returning None backoff time makes the HttpStream use exponential backoff
         return None
