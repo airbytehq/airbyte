041edd0547a1747682982ba19b22cf3c28ad31db
Pedro S. Lopez <pedroslopez@me.com>
2022-07-04 18:17:09 -0400

    Add more metadata to the JobErrorReporter (#14395)
    * add workspace_id and connector_repository as tags
    
    * add tag for connection url
    
    * fix urls for job notifier
    
    * format
    
    * fix failing test

--- airbyte-scheduler/scheduler-persistence/src/main/java/io/airbyte/scheduler/persistence/job_error_reporter/JobErrorReporter.java
+++ airbyte-scheduler/scheduler-persistence/src/main/java/io/airbyte/scheduler/persistence/job_error_reporter/JobErrorReporter.java
@@ -13,6 +13,7 @@ import io.airbyte.config.StandardDestinationDefinition;
 import io.airbyte.config.StandardSourceDefinition;
 import io.airbyte.config.StandardWorkspace;
 import io.airbyte.config.persistence.ConfigRepository;
+import io.airbyte.scheduler.persistence.WebUrlHelper;
 import java.util.HashMap;
 import java.util.List;
 import java.util.UUID;
@@ -28,24 +29,30 @@ public class JobErrorReporter {
   private static final String AIRBYTE_VERSION_META_KEY = "airbyte_version";
   private static final String FAILURE_ORIGIN_META_KEY = "failure_origin";
   private static final String FAILURE_TYPE_META_KEY = "failure_type";
+  private static final String WORKSPACE_ID_META_KEY = "workspace_id";
   private static final String CONNECTION_ID_META_KEY = "connection_id";
+  private static final String CONNECTION_URL_META_KEY = "connection_url";
   private static final String CONNECTOR_NAME_META_KEY = "connector_name";
+  private static final String CONNECTOR_REPOSITORY_META_KEY = "connector_repository";
   private static final String CONNECTOR_DEFINITION_ID_META_KEY = "connector_definition_id";
   private static final String CONNECTOR_RELEASE_STAGE_META_KEY = "connector_release_stage";
 
   private final ConfigRepository configRepository;
   private final DeploymentMode deploymentMode;
   private final String airbyteVersion;
+  private final WebUrlHelper webUrlHelper;
   private final JobErrorReportingClient jobErrorReportingClient;
 
   public JobErrorReporter(final ConfigRepository configRepository,
                           final DeploymentMode deploymentMode,
                           final String airbyteVersion,
+                          final WebUrlHelper webUrlHelper,
                           final JobErrorReportingClient jobErrorReportingClient) {
 
     this.configRepository = configRepository;
     this.deploymentMode = deploymentMode;
     this.airbyteVersion = airbyteVersion;
+    this.webUrlHelper = webUrlHelper;
     this.jobErrorReportingClient = jobErrorReportingClient;
   }
 
@@ -62,12 +69,15 @@ public class JobErrorReporter {
         .toList();
 
     final StandardWorkspace workspace = configRepository.getStandardWorkspaceFromConnection(connectionId, true);
+    final String connectionUrl = webUrlHelper.getConnectionUrl(workspace.getWorkspaceId(), connectionId);
 
     for (final FailureReason failureReason : traceMessageFailures) {
       final FailureOrigin failureOrigin = failureReason.getFailureOrigin();
 
       final HashMap<String, String> metadata = new HashMap<>();
+      metadata.put(WORKSPACE_ID_META_KEY, workspace.getWorkspaceId().toString());
       metadata.put(CONNECTION_ID_META_KEY, connectionId.toString());
+      metadata.put(CONNECTION_URL_META_KEY, connectionUrl);
       metadata.put(AIRBYTE_VERSION_META_KEY, airbyteVersion);
       metadata.put(DEPLOYMENT_MODE_META_KEY, deploymentMode.name());
       metadata.put(FAILURE_ORIGIN_META_KEY, failureOrigin.value());
@@ -80,6 +90,7 @@ public class JobErrorReporter {
 
           metadata.put(CONNECTOR_DEFINITION_ID_META_KEY, sourceDefinition.getSourceDefinitionId().toString());
           metadata.put(CONNECTOR_NAME_META_KEY, sourceDefinition.getName());
+          metadata.put(CONNECTOR_REPOSITORY_META_KEY, sourceDefinition.getDockerRepository());
           metadata.put(CONNECTOR_RELEASE_STAGE_META_KEY, sourceDefinition.getReleaseStage().value());
 
           jobErrorReportingClient.reportJobFailureReason(workspace, failureReason, dockerImage, metadata);
@@ -89,6 +100,7 @@ public class JobErrorReporter {
 
           metadata.put(CONNECTOR_DEFINITION_ID_META_KEY, destinationDefinition.getDestinationDefinitionId().toString());
           metadata.put(CONNECTOR_NAME_META_KEY, destinationDefinition.getName());
+          metadata.put(CONNECTOR_REPOSITORY_META_KEY, destinationDefinition.getDockerRepository());
           metadata.put(CONNECTOR_RELEASE_STAGE_META_KEY, destinationDefinition.getReleaseStage().value());
 
           jobErrorReportingClient.reportJobFailureReason(workspace, failureReason, dockerImage, metadata);
