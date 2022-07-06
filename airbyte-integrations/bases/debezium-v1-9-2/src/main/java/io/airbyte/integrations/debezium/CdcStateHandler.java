6a5056ca530200d458e3bb3697584534c7850b0d
Subodh Kant Chaturvedi <subodh1810@gmail.com>
2022-07-06 17:24:38 +0530

    implement logic to identify new streams to snapshot in postgres cdc (#14399)
    * implement logic to identify new streams to snapshot in postgres cdc
    
    * improve assertions around state message
    
    * return all the streams in LegacyStateManager
    
    * add test

--- airbyte-integrations/bases/debezium-v1-9-2/src/main/java/io/airbyte/integrations/debezium/CdcStateHandler.java
+++ airbyte-integrations/bases/debezium-v1-9-2/src/main/java/io/airbyte/integrations/debezium/CdcStateHandler.java
@@ -11,9 +11,10 @@ import java.util.Map;
  * This interface is used to allow connectors to save the offset and schema history in the manner
  * which suits them
  */
-@FunctionalInterface
 public interface CdcStateHandler {
 
   AirbyteMessage saveState(Map<String, String> offset, String dbHistory);
+
+  AirbyteMessage saveStateAfterCompletionOfSnapshotOfNewStreams();
 
 }
