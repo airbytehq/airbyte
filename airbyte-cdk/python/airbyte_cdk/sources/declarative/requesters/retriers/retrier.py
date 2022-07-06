d03c80943f2aa63c5bfeaec6469e06d4e34c20a1
Alexandre Girard <alexandre@airbyte.io>
2022-07-06 11:54:38 -0700

    Add docstrings

--- airbyte-cdk/python/airbyte_cdk/sources/declarative/requesters/retriers/retrier.py
+++ airbyte-cdk/python/airbyte_cdk/sources/declarative/requesters/retriers/retrier.py
@@ -28,13 +28,24 @@ class Retrier(ABC):
     @property
     @abstractmethod
     def max_retries(self) -> Union[int, None]:
+        """
+        Specifies maximum amount of retries for backoff policy. Return None for no limit.
+        """
         pass
 
     @property
     @abstractmethod
     def retry_factor(self) -> float:
+        """
+        Override if needed. Specifies factor for exponential backoff policy. Not used for other backoff strategies.
+        """
         pass
 
     @abstractmethod
     def should_retry(self, response: requests.Response) -> ResponseStatus:
+        """
+        Evaluate response status describing whether a failing request should be retried or ignored.
+        :param response: response to evaluate
+        :return: response status
+        """
         pass
