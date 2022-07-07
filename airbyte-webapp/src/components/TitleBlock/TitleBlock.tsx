f443f83f8fbce0f38ef05f7a2c59b5e84e9e9be5
Harshith Mullapudi <harshithmullapudi@gmail.com>
2022-07-07 16:35:40 +0530

    fix: move styling to sass based

--- airbyte-webapp/src/components/TitleBlock/TitleBlock.tsx
+++ airbyte-webapp/src/components/TitleBlock/TitleBlock.tsx
@@ -1,38 +1,23 @@
 import React from "react";
-import styled from "styled-components";
 
 import { H5 } from "components";
 import ContentCard from "components/ContentCard";
+
+import styles from "./TitleBlock.module.scss";
 
 interface IProps {
   title: React.ReactElement;
   actions: React.ReactElement;
 }
 
-const TitleBlockComponent = styled(ContentCard)`
-  margin-bottom: 12px;
-  padding: 19px 20px 20px;
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-`;
-
-const Text = styled.div`
-  margin-left: 20px;
-  font-size: 11px;
-  line-height: 13px;
-  color: ${({ theme }) => theme.greyColor40};
-  white-space: pre-line;
-`;
-
 const TitleBlock: React.FC<IProps> = ({ title, actions }) => {
   return (
-    <TitleBlockComponent>
-      <Text>
+    <ContentCard className={styles.contentCard}>
+      <div className={styles.titleContainer}>
         <H5 bold>{title}</H5>
-      </Text>
+      </div>
       <div>{actions}</div>
-    </TitleBlockComponent>
+    </ContentCard>
   );
 };
 
