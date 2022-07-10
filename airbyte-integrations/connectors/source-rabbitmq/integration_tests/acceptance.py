9562f56354d597f3647465bb30cdcaec3b1f7c1b
Amit Zafran <aamita00@gmail.com>
2022-07-10 14:42:01 +0300

    add test container to setup rabbitMQ  before accptence

--- airbyte-integrations/connectors/source-rabbitmq/integration_tests/acceptance.py
+++ airbyte-integrations/connectors/source-rabbitmq/integration_tests/acceptance.py
@@ -4,13 +4,20 @@
 
 
 import pytest
+from testcontainers.rabbitmq import RabbitMqContainer
 
 pytest_plugins = ("source_acceptance_test.plugin",)
 
+
+def create_environment():
+    with RabbitMqContainer('rabbitmq:management-alpine') as rabbitmq:
+        while not rabbitmq.readiness_probe:
+            print('RabbitMQ is not ready')
 
 @pytest.fixture(scope="session", autouse=True)
 def connector_setup():
     """This fixture is a placeholder for external resources that acceptance test might require."""
     # TODO: setup test dependencies
+    create_environment()
     yield
     # TODO: clean up test dependencies
--- airbyte-integrations/connectors/source-rabbitmq/setup.py
+++ airbyte-integrations/connectors/source-rabbitmq/setup.py
@@ -13,6 +13,7 @@ MAIN_REQUIREMENTS = [
 TEST_REQUIREMENTS = [
     "pytest~=6.1",
     "source-acceptance-test",
+    "testcontainers[rabbitmq]~=3.6.0"
 ]
 
 setup(
