549aba9999cd97dcbb34a4891355a5b71bdefdd6
Amit Zafran <aamita00@gmail.com>
2022-07-10 16:18:07 +0300

    add setup and teardown to acceptance test

--- airbyte-integrations/connectors/source-rabbitmq/integration_tests/acceptance.py
+++ airbyte-integrations/connectors/source-rabbitmq/integration_tests/acceptance.py
@@ -3,21 +3,42 @@
 #
 
 
+from typing import Any
 import pytest
+from pika import BlockingConnection, ConnectionParameters, PlainCredentials
 from testcontainers.rabbitmq import RabbitMqContainer
 
 pytest_plugins = ("source_acceptance_test.plugin",)
+rabbit: RabbitMqContainer
 
+def rabbitmq_setup() -> RabbitMqContainer:
+    rabbit_container = RabbitMqContainer('rabbitmq:management-alpine')
+    rabbit_container.start()
+    while not rabbit_container.readiness_probe:
+        print('RabbitMQ is not ready')
+    return rabbit_container
 
-def create_environment():
-    with RabbitMqContainer('rabbitmq:management-alpine') as rabbitmq:
-        while not rabbitmq.readiness_probe:
-            print('RabbitMQ is not ready')
+def _get_rabbitmq() -> RabbitMqContainer:
+    if rabbit in None:
+        return rabbitmq_setup()
+    else: 
+        return rabbit
+
+def generate_messages(connection_params: ConnectionParameters, exhcange: str):
+    with BlockingConnection(connection_params) as conn:
+        with conn.channel() as channel:
+            channel.basic_publish(exchange=exhcange, body='sample')
+        
 
 @pytest.fixture(scope="session", autouse=True)
 def connector_setup():
     """This fixture is a placeholder for external resources that acceptance test might require."""
     # TODO: setup test dependencies
-    create_environment()
+    rabbitmq_container = rabbitmq_setup()
+    generate_messages(rabbitmq_container.get_connection_params(), 'AIRBYTE_ACCEPTENCE')
     yield
     # TODO: clean up test dependencies
+
+def pytest_sessionfinish(session: Any, exitstatus: Any) -> None:
+    rabbit_container = _get_rabbitmq()
+    rabbit_container.stop(force=True, delete_volume=True)
