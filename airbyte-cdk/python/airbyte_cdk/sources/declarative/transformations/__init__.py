245ee082173b0baf614037266296d8f28e023a4e
Sherif Nada <snadalive@gmail.com>
2022-07-10 22:27:22 -0700

    i win isort

--- airbyte-cdk/python/airbyte_cdk/sources/declarative/transformations/__init__.py
+++ airbyte-cdk/python/airbyte_cdk/sources/declarative/transformations/__init__.py
@@ -3,9 +3,11 @@
 #
 
 
-from .remove_fields import RemoveFields
-
-# The order of imports matters
+# The order of imports matters, so we add the split directive below to tell isort to sort imports while keeping RecordTransformation
+# as the first import
 from .transformation import RecordTransformation
+
+# isort: split
+from .remove_fields import RemoveFields
 
 __all__ = ["RecordTransformation", "RemoveFields"]
