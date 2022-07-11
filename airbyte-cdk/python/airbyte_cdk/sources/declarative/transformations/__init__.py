199c590694da3735d069ae2cc05f5aaa93442634
Sherif Nada <snadalive@gmail.com>
2022-07-10 19:14:49 -0700

    add factory tests

--- airbyte-cdk/python/airbyte_cdk/sources/declarative/transformations/__init__.py
+++ airbyte-cdk/python/airbyte_cdk/sources/declarative/transformations/__init__.py
@@ -1,7 +1,11 @@
 #
 # Copyright (c) 2022 Airbyte, Inc., all rights reserved.
 #
+
+
 from .remove_fields import RemoveFields
+
+# The order of imports matters
 from .transformation import RecordTransformation
 
 __all__ = ["RecordTransformation", "RemoveFields"]
