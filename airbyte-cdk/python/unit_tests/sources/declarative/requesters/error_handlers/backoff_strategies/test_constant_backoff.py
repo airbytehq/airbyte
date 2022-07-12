18e562a167c84374b6f2382548880a51f7c5bfc8
Alexandre Girard <alexandre@airbyte.io>
2022-07-12 11:19:21 -0700

    missing unit test

--- airbyte-cdk/python/unit_tests/sources/declarative/requesters/error_handlers/backoff_strategies/test_constant_backoff.py
+++ airbyte-cdk/python/unit_tests/sources/declarative/requesters/error_handlers/backoff_strategies/test_constant_backoff.py
@@ -5,20 +5,20 @@
 from unittest.mock import MagicMock
 
 import pytest
-from airbyte_cdk.sources.declarative.requesters.error_handlers.backoff_strategies.exponential_backoff_strategy import (
-    ExponentialBackoffStrategy,
-)
+from airbyte_cdk.sources.declarative.requesters.error_handlers.backoff_strategies.constant_backoff_strategy import ConstantBackoffStrategy
+
+BACKOFF_TIME = 10
 
 
 @pytest.mark.parametrize(
     "test_name, attempt_count, expected_backoff_time",
     [
-        ("test_exponential_backoff", 1, 10),
-        ("test_exponential_backoff", 2, 20),
+        ("test_exponential_backoff", 1, BACKOFF_TIME),
+        ("test_exponential_backoff", 2, BACKOFF_TIME),
     ],
 )
 def test_exponential_backoff(test_name, attempt_count, expected_backoff_time):
     response_mock = MagicMock()
-    backoff_strategy = ExponentialBackoffStrategy(factor=5)
+    backoff_strategy = ConstantBackoffStrategy(backoff_time_in_seconds=BACKOFF_TIME)
     backoff = backoff_strategy.backoff(response_mock, attempt_count)
     assert backoff == expected_backoff_time
