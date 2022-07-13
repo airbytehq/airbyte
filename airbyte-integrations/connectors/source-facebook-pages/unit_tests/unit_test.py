c1044efc8d0359194fa7708f04f914f27734cd44
Luis Gomez <lgomezm90@gmail.com>
2022-07-12 22:29:12 -0500

    Added tests

--- airbyte-integrations/connectors/source-facebook-pages/unit_tests/unit_test.py
+++ airbyte-integrations/connectors/source-facebook-pages/unit_tests/unit_test.py
@@ -3,9 +3,14 @@
 #
 
 import json
+from airbyte_cdk.logger import AirbyteLogger
 
 import pytest
+import requests_mock
+from source_facebook_pages.source import SourceFacebookPages
 from source_facebook_pages.streams import Post
+
+logger = AirbyteLogger()
 
 
 class MockResponse:
@@ -14,6 +19,49 @@ class MockResponse:
 
     def json(self):
         return json.loads(self.value)
+
+
+@pytest.fixture(name="config")
+def config_fixture():
+    config = {
+        "access_token": "TOKEN",
+        "page_id": "105762628872670",
+    }
+
+    return config
+
+
+def test_check_connection_ok(config):
+    with requests_mock.Mocker() as m:
+        m.get(f"https://graph.facebook.com/{config['page_id']}", json={"access_token": "PAGE_TOKEN"})
+        m.get(f"https://graph.facebook.com/v14.0/{config['page_id']}?access_token=PAGE_TOKEN", json={})
+    
+        ok, error_msg = SourceFacebookPages().check_connection(logger, config=config)
+
+        assert ok
+        assert not error_msg
+
+
+def test_check_connection_empty_config():
+    ok, error_msg = SourceFacebookPages().check_connection(logger, config={})
+
+    assert not ok
+    assert error_msg
+
+
+def test_check_connection_invalid_config(config):
+    config.pop("page_id")
+    ok, error_msg = SourceFacebookPages().check_connection(logger, config=config)
+
+    assert not ok
+    assert error_msg
+
+
+def test_check_connection_exception(config):
+    ok, error_msg = SourceFacebookPages().check_connection(logger, config=config)
+
+    assert not ok
+    assert error_msg
 
 
 @pytest.mark.parametrize(
