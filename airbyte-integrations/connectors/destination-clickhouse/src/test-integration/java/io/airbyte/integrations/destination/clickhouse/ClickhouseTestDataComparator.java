270e5ce7a01e0d0dd23112ab3a05a411947af3f5
ievgeniit <etsybaev@gmail.com>
2022-07-14 11:24:27 +0200

    added comparator

--- airbyte-integrations/connectors/destination-clickhouse/src/test-integration/java/io/airbyte/integrations/destination/clickhouse/ClickhouseTestDataComparator.java
+++ airbyte-integrations/connectors/destination-clickhouse/src/test-integration/java/io/airbyte/integrations/destination/clickhouse/ClickhouseTestDataComparator.java
@@ -8,9 +8,12 @@ import io.airbyte.integrations.destination.ExtendedNameTransformer;
 import io.airbyte.integrations.standardtest.destination.comparator.AdvancedTestDataComparator;
 import java.util.ArrayList;
 import java.util.List;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class ClickhouseTestDataComparator extends AdvancedTestDataComparator {
 
+  private static final Logger LOGGER = LoggerFactory.getLogger(ClickhouseTestDataComparator.class);
   private final ExtendedNameTransformer namingResolver = new ExtendedNameTransformer();
 
   @Override
@@ -24,6 +27,30 @@ public class ClickhouseTestDataComparator extends AdvancedTestDataComparator {
       result.add(resolved.toUpperCase());
     }
     return result;
+  }
+
+
+  @Override
+  protected boolean compareNumericValues(final String firstNumericValue, final String secondNumericValue) {
+    // clickhouse stores double 1.14 as 1.1400000000000001
+    //https://clickhouse.com/docs/en/sql-reference/data-types/float/
+    double epsilon = 0.000000000000001d;
+
+    double firstValue = Double.parseDouble(firstNumericValue);
+    double secondValue = Double.parseDouble(secondNumericValue);
+
+    return Math.abs(firstValue - secondValue) < epsilon;
+  }
+
+  @Override
+  protected boolean compareBooleanValues(final String firstValue, final String secondValue) {
+    final boolean firstBooleanValue = Boolean.parseBoolean(firstValue);
+
+    // Type bool is stored as UInt8. Possible values true (1), false (0).
+    //https://clickhouse.com/docs/en/sql-reference/data-types/boolean
+    final boolean secondBooleanValue = Integer.parseInt(secondValue) > 0;
+
+    return firstBooleanValue == secondBooleanValue;
   }
 
 }
