8293d6d54b279a3c4958223ab1b02c0240912ca0
Oleksandr Bazarnov <oleksandr.bazarnov@globallogic.com>
2022-07-14 21:21:49 +0300

    fixed unit_tests

--- airbyte-integrations/connectors/source-notion/source_notion/source.py
+++ airbyte-integrations/connectors/source-notion/source_notion/source.py
@@ -26,10 +26,7 @@ class NotionAuthenticator:
             auth_type = credentials.get("auth_type")
             if auth_type == "OAuth2.0":
                 return TokenAuthenticator(credentials.get("access_token"))
-            elif auth_type == "token":
-                return TokenAuthenticator(credentials.get("token"))
-            else:
-                raise NotImplementedError("This authentication method is not implemented.")
+            return TokenAuthenticator(credentials.get("token"))
         
         # support the old config
         if "access_token" in self.config:
