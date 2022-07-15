92bc4ec73dce07cd007128b362e88237350c05be
siakon89 <siaterliskonsta@gmail.com>
2022-07-15 14:40:27 +0300

    feat: implement export dimensions from adaptive [ODP-20]

--- airbyte-integrations/connectors/source-adaptive-insights/source_adaptive_insights/source.py
+++ airbyte-integrations/connectors/source-adaptive-insights/source_adaptive_insights/source.py
@@ -4,175 +4,143 @@
 
 
 from abc import ABC
-from typing import Any, Iterable, List, Mapping, MutableMapping, Optional, Tuple
-
+from typing import (
+    Any,
+    Iterable,
+    List,
+    Mapping,
+    MutableMapping,
+    Optional,
+    Tuple,
+    Union
+)
 import requests
+import xmltodict
 from airbyte_cdk.sources import AbstractSource
 from airbyte_cdk.sources.streams import Stream
 from airbyte_cdk.sources.streams.http import HttpStream
-from airbyte_cdk.sources.streams.http.auth import TokenAuthenticator
-
-"""
-TODO: Most comments in this class are instructive and should be deleted after the source is implemented.
-
-This file provides a stubbed example of how to use the Airbyte CDK to develop both a source connector which supports full refresh or and an
-incremental syncs from an HTTP API.
-
-The various TODOs are both implementation hints and steps - fulfilling all the TODOs should be sufficient to implement one basic and one incremental
-stream from a source. This pattern is the same one used by Airbyte internally to implement connectors.
-
-The approach here is not authoritative, and devs are free to use their own judgement.
-
-There are additional required TODOs in the files within the integration_tests folder and the spec.yaml file.
-"""
+from .helper import handle_export_dimensions
 
 
 # Basic full refresh stream
 class AdaptiveInsightsStream(HttpStream, ABC):
-    """
-    TODO remove this comment
-
-    This class represents a stream output by the connector.
-    This is an abstract base class meant to contain all the common functionality at the API level e.g: the API base URL, pagination strategy,
-    parsing responses etc..
-
-    Each stream should extend this class (or another abstract subclass of it) to specify behavior unique to that stream.
-
-    Typically for REST APIs each stream corresponds to a resource in the API. For example if the API
-    contains the endpoints
-        - GET v1/customers
-        - GET v1/employees
-
-    then you should have three classes:
-    `class AdaptiveInsightsStream(HttpStream, ABC)` which is the current class
-    `class Customers(AdaptiveInsightsStream)` contains behavior to pull data for customers using v1/customers
-    `class Employees(AdaptiveInsightsStream)` contains behavior to pull data for employees using v1/employees
-
-    If some streams implement incremental sync, it is typical to create another class
-    `class IncrementalAdaptiveInsightsStream((AdaptiveInsightsStream), ABC)` then have concrete stream implementations extend it. An example
-    is provided below.
-
-    See the reference docs for the full list of configurable options.
-    """
 
     # TODO: Fill in the url base. Required.
-    url_base = "https://reqres.in/api/"
+    url_base = "https://api.adaptiveinsights.com/api/"
+    method = None
 
-    def next_page_token(self, response: requests.Response) -> Optional[Mapping[str, Any]]:
-
-        json_response = response.json()
-        total_pages = json_response.get("total_pages", None)
-        curr_page = int(json_response.get("page", 1))
-
-        if total_pages and curr_page < total_pages:
-            return {"page": curr_page + 1}
-
-        return None
-
-    def request_params(
-        self, 
-        stream_state: Mapping[str, Any], 
-        stream_slice: Mapping[str, any] = None, 
-        next_page_token: Mapping[str, Any] = None
-    ) -> MutableMapping[str, Any]:
-        params = {
-            "per_page": 2
-        }
-
-        if next_page_token:
-            params.update(next_page_token)
-
-        return params
+    def __init__(self, username: str, password: str, **kwargs):
+        super().__init__(**kwargs)
+        self.username = username
+        self.password = password
 
     def request_headers(self, **kwargs) -> Mapping[str, Any]:
 
-        return {"accept": "application/json"}
+        return {'Content-Type': 'text/xml; charset=UTF-8'}
+
+    def request_body_data(
+        self,
+        stream_state: Mapping[str, Any],
+        stream_slice: Mapping[str, Any] = None,
+        next_page_token: Mapping[str, Any] = None,
+    ) -> Optional[Union[Mapping, str]]:
+
+        body = f"""<?xml version='1.0' encoding='UTF-8'?>
+        <call method="{self.method}" callerName="Airbyte - auto">
+        <credentials login="{self.username}" password="{self.password}"/>
+        <include versionName="Current LBE" dimensionValues="true"/>
+        </call>
+        """.encode("utf-8")
+
+        return body
 
     def parse_response(self, response: requests.Response, **kwargs) -> Iterable[Mapping]:
-        json_response = response.json()
+        json_response = xmltodict.parse(response.contenct)
         
-        yield from json_response.get("data", [])
+        yield from handle_export_dimensions(json_response)
 
 
-class Users(AdaptiveInsightsStream):
+class ExportDimensions(AdaptiveInsightsStream):
 
     primary_key = "id"
+    http_method = "POST"
+    method = "exportDimension"
 
     def path(
         self, stream_state: Mapping[str, Any] = None, stream_slice: Mapping[str, Any] = None, next_page_token: Mapping[str, Any] = None
     ) -> str:
 
-        return "users"
+        return "v32"
 
 
 # Basic incremental stream
-class IncrementalAdaptiveInsightsStream(AdaptiveInsightsStream, ABC):
-    """
-    TODO fill in details of this class to implement functionality related to incremental syncs for your connector.
-         if you do not need to implement incremental sync for any streams, remove this class.
-    """
+# class IncrementalAdaptiveInsightsStream(AdaptiveInsightsStream, ABC):
+#     """
+#     TODO fill in details of this class to implement functionality related to incremental syncs for your connector.
+#          if you do not need to implement incremental sync for any streams, remove this class.
+#     """
 
-    # TODO: Fill in to checkpoint stream reads after N records. This prevents re-reading of data if the stream fails for any reason.
-    state_checkpoint_interval = None
+#     # TODO: Fill in to checkpoint stream reads after N records. This prevents re-reading of data if the stream fails for any reason.
+#     state_checkpoint_interval = None
 
-    @property
-    def cursor_field(self) -> str:
-        """
-        TODO
-        Override to return the cursor field used by this stream e.g: an API entity might always use created_at as the cursor field. This is
-        usually id or date based. This field's presence tells the framework this in an incremental stream. Required for incremental.
+#     @property
+#     def cursor_field(self) -> str:
+#         """
+#         TODO
+#         Override to return the cursor field used by this stream e.g: an API entity might always use created_at as the cursor field. This is
+#         usually id or date based. This field's presence tells the framework this in an incremental stream. Required for incremental.
 
-        :return str: The name of the cursor field.
-        """
-        return []
+#         :return str: The name of the cursor field.
+#         """
+#         return []
 
-    def get_updated_state(self, current_stream_state: MutableMapping[str, Any], latest_record: Mapping[str, Any]) -> Mapping[str, Any]:
-        """
-        Override to determine the latest state after reading the latest record. This typically compared the cursor_field from the latest record and
-        the current state and picks the 'most' recent cursor. This is how a stream's state is determined. Required for incremental.
-        """
-        return {}
+#     def get_updated_state(self, current_stream_state: MutableMapping[str, Any], latest_record: Mapping[str, Any]) -> Mapping[str, Any]:
+#         """
+#         Override to determine the latest state after reading the latest record. This typically compared the cursor_field from the latest record and
+#         the current state and picks the 'most' recent cursor. This is how a stream's state is determined. Required for incremental.
+#         """
+#         return {}
 
 
-class Employees(IncrementalAdaptiveInsightsStream):
-    """
-    TODO: Change class name to match the table/data source this stream corresponds to.
-    """
+# class Employees(IncrementalAdaptiveInsightsStream):
+#     """
+#     TODO: Change class name to match the table/data source this stream corresponds to.
+#     """
 
-    # TODO: Fill in the cursor_field. Required.
-    cursor_field = "start_date"
+#     # TODO: Fill in the cursor_field. Required.
+#     cursor_field = "start_date"
 
-    # TODO: Fill in the primary key. Required. This is usually a unique field in the stream, like an ID or a timestamp.
-    primary_key = "employee_id"
+#     # TODO: Fill in the primary key. Required. This is usually a unique field in the stream, like an ID or a timestamp.
+#     primary_key = "employee_id"
 
-    def path(self, **kwargs) -> str:
-        """
-        TODO: Override this method to define the path this stream corresponds to. E.g. if the url is https://example-api.com/v1/employees then this should
-        return "single". Required.
-        """
-        return "employees"
+#     def path(self, **kwargs) -> str:
+#         """
+#         TODO: Override this method to define the path this stream corresponds to. E.g. if the url is https://example-api.com/v1/employees then this should
+#         return "single". Required.
+#         """
+#         return "employees"
 
-    def stream_slices(self, stream_state: Mapping[str, Any] = None, **kwargs) -> Iterable[Optional[Mapping[str, any]]]:
-        """
-        TODO: Optionally override this method to define this stream's slices. If slicing is not needed, delete this method.
+#     def stream_slices(self, stream_state: Mapping[str, Any] = None, **kwargs) -> Iterable[Optional[Mapping[str, any]]]:
+#         """
+#         TODO: Optionally override this method to define this stream's slices. If slicing is not needed, delete this method.
 
-        Slices control when state is saved. Specifically, state is saved after a slice has been fully read.
-        This is useful if the API offers reads by groups or filters, and can be paired with the state object to make reads efficient. See the "concepts"
-        section of the docs for more information.
+#         Slices control when state is saved. Specifically, state is saved after a slice has been fully read.
+#         This is useful if the API offers reads by groups or filters, and can be paired with the state object to make reads efficient. See the "concepts"
+#         section of the docs for more information.
 
-        The function is called before reading any records in a stream. It returns an Iterable of dicts, each containing the
-        necessary data to craft a request for a slice. The stream state is usually referenced to determine what slices need to be created.
-        This means that data in a slice is usually closely related to a stream's cursor_field and stream_state.
+#         The function is called before reading any records in a stream. It returns an Iterable of dicts, each containing the
+#         necessary data to craft a request for a slice. The stream state is usually referenced to determine what slices need to be created.
+#         This means that data in a slice is usually closely related to a stream's cursor_field and stream_state.
 
-        An HTTP request is made for each returned slice. The same slice can be accessed in the path, request_params and request_header functions to help
-        craft that specific request.
+#         An HTTP request is made for each returned slice. The same slice can be accessed in the path, request_params and request_header functions to help
+#         craft that specific request.
 
-        For example, if https://example-api.com/v1/employees offers a date query params that returns data for that particular day, one way to implement
-        this would be to consult the stream state object for the last synced date, then return a slice containing each date from the last synced date
-        till now. The request_params function would then grab the date from the stream_slice and make it part of the request by injecting it into
-        the date query param.
-        """
-        raise NotImplementedError("Implement stream slices or delete this method!")
+#         For example, if https://example-api.com/v1/employees offers a date query params that returns data for that particular day, one way to implement
+#         this would be to consult the stream state object for the last synced date, then return a slice containing each date from the last synced date
+#         till now. The request_params function would then grab the date from the stream_slice and make it part of the request by injecting it into
+#         the date query param.
+#         """
+#         raise NotImplementedError("Implement stream slices or delete this method!")
 
 
 # Source
@@ -191,11 +159,14 @@ class SourceAdaptiveInsights(AbstractSource):
         return True, None
 
     def streams(self, config: Mapping[str, Any]) -> List[Stream]:
-        """
-        TODO: Replace the streams below with your own streams.
+        username = config["username"]
+        password = config["password"]
+        args = {
+            "username": username,
+            "password": password
+        }
 
-        :param config: A Mapping of the user input configuration as defined in the connector spec.
-        """
-        # TODO remove the authenticator if not required.
-        # auth = TokenAuthenticator(token="api_key")  # Oauth2Authenticator is also available if you need oauth support
-        return [Customers(authenticator=auth), Employees(authenticator=auth)]
+        return [
+            ExportDimensions(**args)
+            # Employees(authenticator=auth)
+        ]
