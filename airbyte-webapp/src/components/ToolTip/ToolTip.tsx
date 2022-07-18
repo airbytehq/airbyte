c043fad1b13f6a5a6789e2511872586bb626a1ba
Edmundo Ruiz Ghanem <edmundo@airbyte.io>
2022-07-18 14:22:35 -0700

    Update ToolTip to show from any side perfectly centered
    * Add react-tether dependency
    * Update ToolTip to use react-tether
    * Add align property to ToolTip

--- airbyte-webapp/src/components/ToolTip/ToolTip.tsx
+++ airbyte-webapp/src/components/ToolTip/ToolTip.tsx
@@ -1,23 +1,94 @@
 import classNames from "classnames";
-import React from "react";
+import React, { useState, useEffect } from "react";
+import TetherComponent from "react-tether";
 
 import styles from "./ToolTip.module.scss";
+
+type ToolTipCursor = "pointer" | "help" | "not-allowed" | "initial";
+type ToolTipMode = "dark" | "light";
+type ToolTipAlignment = "top" | "right" | "bottom" | "left";
 
 interface ToolTipProps {
   control: React.ReactNode;
   className?: string;
   disabled?: boolean;
-  cursor?: "pointer" | "help" | "not-allowed" | "initial";
-  mode?: "dark" | "light";
+  cursor?: ToolTipCursor;
+  mode?: ToolTipMode;
+  align?: ToolTipAlignment;
 }
 
-export const ToolTip: React.FC<ToolTipProps> = ({ children, control, className, disabled, mode = "dark", cursor }) => (
-  <div className={styles.container} style={disabled ? undefined : { cursor }}>
-    {control}
-    <div
-      className={classNames(styles.toolTip, mode === "light" && styles.light, disabled && styles.disabled, className)}
-    >
-      {children}
-    </div>
-  </div>
-);
+const MOUSE_OUT_TIMEOUT_MS: Readonly<number> = 250;
+
+const TETHER_ATTACHMENT_BY_ALIGNMENT: Readonly<Record<ToolTipAlignment, string>> = {
+  top: "bottom center",
+  right: "middle left",
+  bottom: "top center",
+  left: "middle right",
+};
+
+export const ToolTip: React.FC<ToolTipProps> = ({
+  children,
+  control,
+  className,
+  disabled,
+  cursor,
+  mode = "dark",
+  align = "bottom",
+}) => {
+  const [isMouseOver, setIsMouseOver] = useState(false);
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    if (isMouseOver) {
+      setIsVisible(true);
+      return;
+    }
+
+    const timeout = window.setTimeout(() => {
+      setIsVisible(false);
+    }, MOUSE_OUT_TIMEOUT_MS);
+
+    return () => {
+      window.clearTimeout(timeout);
+    };
+  }, [isMouseOver]);
+
+  const canShowTooltip = isVisible && !disabled;
+
+  const onMouseOver = () => {
+    setIsMouseOver(true);
+  };
+
+  const onMouseOut = () => {
+    setIsMouseOver(false);
+  };
+
+  return (
+    <TetherComponent
+      attachment={TETHER_ATTACHMENT_BY_ALIGNMENT[align]}
+      renderTarget={(ref) => (
+        <div
+          ref={ref as React.LegacyRef<HTMLDivElement>}
+          className={styles.container}
+          style={disabled ? undefined : { cursor }}
+          onMouseOver={onMouseOver}
+          onMouseOut={onMouseOut}
+        >
+          {control}
+        </div>
+      )}
+      renderElement={(ref) =>
+        canShowTooltip && (
+          <div
+            ref={ref as React.LegacyRef<HTMLDivElement>}
+            className={classNames(styles.toolTip, mode === "light" && styles.light, className)}
+            onMouseOver={onMouseOver}
+            onMouseOut={onMouseOut}
+          >
+            {children}
+          </div>
+        )
+      }
+    />
+  );
+};
