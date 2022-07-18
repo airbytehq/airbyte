2ca69f10791071c442820ca97cddf6b1d30ea615
Alexandre Girard <alexandre@airbyte.io>
2022-07-18 07:47:37 -0700

    add test

--- airbyte-cdk/python/unit_tests/sources/declarative/requesters/paginators/test_conditional_paginator.py
+++ airbyte-cdk/python/unit_tests/sources/declarative/requesters/paginators/test_conditional_paginator.py
@@ -17,29 +17,78 @@ from airbyte_cdk.sources.declarative.requesters.request_options.interpolated_req
 
 
 @pytest.mark.parametrize(
-    "test_name, stop_condition_template, expected_next_page_token",
+    "test_name, stop_condition_template, pass_by, expected_next_page_token",
     [
-        ("test_stop_pagination_from_response_body", "{{ not decoded_response['accounts'] }}", None),
-        ("test_stop_pagination_from_config", "{{ config['response_override'] == decoded_response['_metadata']['content'] }}", None),
+        ("test_stop_pagination_from_response_body", "{{ not decoded_response['accounts'] }}", RequestOptionType.body_json, None),
+        (
+            "test_stop_pagination_from_config",
+            "{{ config['response_override'] == decoded_response['_metadata']['content'] }}",
+            RequestOptionType.body_json,
+            None,
+        ),
         (
             "test_continue_pagination_from_response_body",
             "{{ decoded_response['end'] == decoded_response['total'] - 1 }}",
+            RequestOptionType.body_json,
             {"next_page_token": 99},
         ),
-        ("test_continue_pagination_from_response_headers", "{{ decoded_response['headers']['has_more'] }}", {"next_page_token": 99}),
-        ("test_continue_pagination_from_last_records", "{{ last_records[-1]['more_records'] == False }}", {"next_page_token": 99}),
-        ("test_continue_pagination_for_empty_dict_evaluates_false", "{{ decoded_response['characters'] }}", {"next_page_token": 99}),
+        (
+            "test_continue_pagination_from_response_headers",
+            "{{ decoded_response['headers']['has_more'] }}",
+            RequestOptionType.body_json,
+            {"next_page_token": 99},
+        ),
+        (
+            "test_continue_pagination_from_last_records_body_json",
+            "{{ last_records[-1]['more_records'] == False }}",
+            RequestOptionType.body_json,
+            {"next_page_token": 99},
+        ),
+        (
+            "test_continue_pagination_from_last_records_body_data",
+            "{{ last_records[-1]['more_records'] == False }}",
+            RequestOptionType.body_data,
+            {"next_page_token": 99},
+        ),
+        (
+            "test_continue_pagination_from_last_records_params",
+            "{{ last_records[-1]['more_records'] == False }}",
+            RequestOptionType.request_parameter,
+            {"next_page_token": 99},
+        ),
+        (
+            "test_continue_pagination_from_last_records_header",
+            "{{ last_records[-1]['more_records'] == False }}",
+            RequestOptionType.header,
+            {"next_page_token": 99},
+        ),
+        (
+            "test_continue_pagination_from_last_records_path",
+            "{{ last_records[-1]['more_records'] == False }}",
+            RequestOptionType.path,
+            {"next_page_token": 99},
+        ),
+        (
+            "test_continue_pagination_for_empty_dict_evaluates_false",
+            "{{ decoded_response['characters'] }}",
+            RequestOptionType.body_json,
+            {"next_page_token": 99},
+        ),
     ],
 )
-def test_interpolated_conditional_paginator(test_name, stop_condition_template, expected_next_page_token):
+def test_interpolated_conditional_paginator(test_name, stop_condition_template, pass_by, expected_next_page_token):
     decoder = JsonDecoder()
     config = {"response_override": "stop_if_you_see_me"}
 
     cursor_value = "{{ decoded_response.end }}"
     strategy = CursorPaginationStrategy(cursor_value, decoder=decoder, config=config)
+    url_base = "https://airbyte.io"
 
     request_options_provider = InterpolatedRequestOptionsProvider(config=config)
-    page_token = RequestOption(pass_by=RequestOptionType.body_json, field_name="from")
+    if pass_by == RequestOptionType.path:
+        page_token = RequestOption(pass_by)
+    else:
+        page_token = RequestOption(pass_by=pass_by, field_name="from")
     response = requests.Response()
     response.headers = {"has_more": True}
     response_body = {"_metadata": {"content": "stop_if_you_see_me"}, "accounts": [], "end": 99, "total": 200, "characters": {}}
@@ -47,8 +96,27 @@ def test_interpolated_conditional_paginator(test_name, stop_condition_template, 
     last_records = [{"id": 0, "more_records": True}, {"id": 1, "more_records": True}]
 
     paginator = InterpolatedConditionalPaginator(
-        InterpolatedBoolean(stop_condition_template), request_options_provider, page_token, strategy, config, None, decoder
+        InterpolatedBoolean(stop_condition_template), request_options_provider, page_token, strategy, config, url_base, decoder
     )
     next_page_token = paginator.next_page_token(response, last_records)
 
+    pass_by_to_mapping = {
+        RequestOptionType.request_parameter: paginator.request_params(),
+        RequestOptionType.header: paginator.request_headers(),
+        RequestOptionType.path: paginator.path(),
+        RequestOptionType.body_json: paginator.request_body_json(),
+        RequestOptionType.body_data: paginator.request_body_data(),
+    }
+
     assert next_page_token == expected_next_page_token
+    for option_type, mapping in pass_by_to_mapping.items():
+        if option_type == pass_by and expected_next_page_token:
+            if option_type == RequestOptionType.path:
+                assert mapping == "99"
+            else:
+                assert mapping == {"from": 99}
+        else:
+            if option_type == RequestOptionType.path:
+                assert mapping is None
+            else:
+                assert mapping == {}
