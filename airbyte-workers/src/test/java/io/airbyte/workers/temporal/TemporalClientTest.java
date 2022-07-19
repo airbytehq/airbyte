198e580f349b244d7ac05e481e9d7118fbb333fc
Pedro S. Lopez <pedroslopez@me.com>
2022-07-19 17:19:35 -0400

    Build failure reasons for synchronous jobs (check/spec/discover) (#14715)
    * demo for surfacing synchronous job failures
    
    * add missing changes for StandardDiscoverCatalogOutput impl
    
    * extract trace message failure reason for discover job
    
    * move to using a single pojo to represent synchronous job outputs
    
    * format
    
    * handle new output type in check before sync
    
    * re-genericize DefaultSynchronousSchedulerClient.execute
    
    * fix failing tests
    
    * fix failing scheduler client tests
    
    * get spec returns failure reason from trace message
    
    * build failure reason from trace message for check job
    
    * type safety
    
    * only consider error-type trace messages
    
    * add more tests
    
    * just use nulls
    
    * this was removed but incorrectly re-added when merging master into the branch
    
    * check output version for workflow replay support
    
    * refactor trace message finding to util method
    
    * additionalProperties: true
    
    * add versioning for CheckConnectionWorkflow
    
    * update comment

--- airbyte-workers/src/test/java/io/airbyte/workers/temporal/TemporalClientTest.java
+++ airbyte-workers/src/test/java/io/airbyte/workers/temporal/TemporalClientTest.java
@@ -22,6 +22,8 @@ import static org.mockito.Mockito.when;
 
 import com.google.common.collect.Sets;
 import io.airbyte.commons.json.Jsons;
+import io.airbyte.config.ConnectorJobOutput;
+import io.airbyte.config.FailureReason;
 import io.airbyte.config.JobCheckConnectionConfig;
 import io.airbyte.config.JobDiscoverCatalogConfig;
 import io.airbyte.config.JobGetSpecConfig;
@@ -140,6 +142,23 @@ class TemporalClientTest {
 
       assertNotNull(response);
       assertFalse(response.getOutput().isPresent());
+      assertFalse(response.getMetadata().isSucceeded());
+      assertEquals(logPath, response.getMetadata().getLogPath());
+    }
+
+    @Test
+    void testExecuteWithConnectorJobFailure() {
+      final Supplier<ConnectorJobOutput> supplier = mock(Supplier.class);
+      final FailureReason mockFailureReason = mock(FailureReason.class);
+      final ConnectorJobOutput connectorJobOutput = new ConnectorJobOutput()
+          .withFailureReason(mockFailureReason);
+      when(supplier.get()).thenReturn(connectorJobOutput);
+
+      final TemporalResponse<ConnectorJobOutput> response = temporalClient.execute(JOB_RUN_CONFIG, supplier);
+
+      assertNotNull(response);
+      assertTrue(response.getOutput().isPresent());
+      assertEquals(connectorJobOutput, response.getOutput().get());
       assertFalse(response.getMetadata().isSucceeded());
       assertEquals(logPath, response.getMetadata().getLogPath());
     }
