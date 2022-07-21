be432c3fe6fceda327c017ac54cb0613e1a07806
Alexandre Girard <alexandre@airbyte.io>
2022-07-21 07:06:55 -0700

    refactor test

--- airbyte-cdk/python/unit_tests/sources/declarative/interpolation/test_interpolated_string.py
+++ airbyte-cdk/python/unit_tests/sources/declarative/interpolation/test_interpolated_string.py
@@ -1,33 +1,23 @@
 #
 # Copyright (c) 2022 Airbyte, Inc., all rights reserved.
 #
-
+import pytest
 from airbyte_cdk.sources.declarative.interpolation.interpolated_string import InterpolatedString
 
 config = {"field": "value"}
+options = {"hello": "world"}
+kwargs = {"c": "airbyte"}
 
 
-def test_static_value():
-    static_value = "HELLO WORLD"
-    s = InterpolatedString.create(static_value, options={})
-    assert s.eval(config) == "HELLO WORLD"
-
-
-def test_eval_from_config():
-    string = "{{ config['field'] }}"
-    s = InterpolatedString.create(string, options={})
-    assert s.eval(config) == "value"
-
-
-def test_eval_from_kwargs():
-    string = "{{ kwargs['c'] }}"
-    kwargs = {"c": "airbyte"}
-    s = InterpolatedString.create(string, options={})
-    assert s.eval(config, **{"kwargs": kwargs}) == "airbyte"
-
-
-def test_eval_from_options():
-    string = "{{ options['hello'] }}"
-    kwargs = {"c": "airbyte"}
-    s = InterpolatedString.create(string, options={"hello": "world"})
-    assert s.eval(config, **{"kwargs": kwargs}) == "world"
+@pytest.mark.parametrize(
+    "test_name, input_string, expected_value",
+    [
+        ("test_static_value", "HELLO WORLD", "HELLO WORLD"),
+        ("test_eval_from_options", "{{ options['hello'] }}", "world"),
+        ("test_eval_from_config", "{{ config['field'] }}", "value"),
+        ("test_eval_from_kwargs", "{{ kwargs['c'] }}", "airbyte"),
+    ],
+)
+def test_interpolated_string(test_name, input_string, expected_value):
+    s = InterpolatedString.create(input_string, options=options)
+    assert s.eval(config, **{"kwargs": kwargs}) == expected_value
