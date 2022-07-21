157fd1e5853f96abb9355ac51d647230684f0454
Brian Lai <51336873+brianjlai@users.noreply.github.com>
2022-07-21 07:35:22 -0400

    [#14361] Adding new generator for configuration based source template (#14887)
    * [#14361] Adding new generator for configuration based source template
    
    * remove unit tests and update a few doc files generated by the templates that aren't relevant to config based connectors
    
    * use 0.1.65 as the latest available CDK version we have

--- airbyte-cdk/python/airbyte_cdk/sources/declarative/parsers/class_types_registry.py
+++ airbyte-cdk/python/airbyte_cdk/sources/declarative/parsers/class_types_registry.py
@@ -7,6 +7,7 @@ from typing import Mapping, Type
 from airbyte_cdk.sources.declarative.datetime.min_max_datetime import MinMaxDatetime
 from airbyte_cdk.sources.declarative.declarative_stream import DeclarativeStream
 from airbyte_cdk.sources.declarative.extractors.jello import JelloExtractor
+from airbyte_cdk.sources.declarative.extractors.record_selector import RecordSelector
 from airbyte_cdk.sources.declarative.requesters.error_handlers.backoff_strategies.constant_backoff_strategy import ConstantBackoffStrategy
 from airbyte_cdk.sources.declarative.requesters.error_handlers.backoff_strategies.exponential_backoff_strategy import (
     ExponentialBackoffStrategy,
@@ -15,8 +16,11 @@ from airbyte_cdk.sources.declarative.requesters.error_handlers.composite_error_h
 from airbyte_cdk.sources.declarative.requesters.error_handlers.default_error_handler import DefaultErrorHandler
 from airbyte_cdk.sources.declarative.requesters.http_requester import HttpRequester
 from airbyte_cdk.sources.declarative.requesters.paginators.limit_paginator import LimitPaginator
+from airbyte_cdk.sources.declarative.requesters.paginators.no_pagination import NoPagination
 from airbyte_cdk.sources.declarative.requesters.paginators.strategies.cursor_pagination_strategy import CursorPaginationStrategy
 from airbyte_cdk.sources.declarative.requesters.paginators.strategies.offset_increment import OffsetIncrement
+from airbyte_cdk.sources.declarative.retrievers.simple_retriever import SimpleRetriever
+from airbyte_cdk.sources.declarative.schema.json_schema import JsonSchema
 from airbyte_cdk.sources.declarative.stream_slicers.cartesian_product_stream_slicer import CartesianProductStreamSlicer
 from airbyte_cdk.sources.declarative.stream_slicers.datetime_stream_slicer import DatetimeStreamSlicer
 from airbyte_cdk.sources.declarative.stream_slicers.list_stream_slicer import ListStreamSlicer
@@ -36,10 +40,14 @@ CLASS_TYPES_REGISTRY: Mapping[str, Type] = {
     "ExponentialBackoffStrategy": ExponentialBackoffStrategy,
     "HttpRequester": HttpRequester,
     "JelloExtractor": JelloExtractor,
+    "JsonSchema": JsonSchema,
     "LimitPaginator": LimitPaginator,
     "ListStreamSlicer": ListStreamSlicer,
     "MinMaxDatetime": MinMaxDatetime,
+    "NoPagination": NoPagination,
     "OffsetIncrement": OffsetIncrement,
+    "RecordSelector": RecordSelector,
     "RemoveFields": RemoveFields,
+    "SimpleRetriever": SimpleRetriever,
     "TokenAuthenticator": TokenAuthenticator,
 }
