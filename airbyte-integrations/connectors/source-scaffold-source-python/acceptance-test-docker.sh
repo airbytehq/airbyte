ab5f9b52dbc93b079f29e15c551225d02b4d168d
alafanechere <augustin.lafanechere@gmail.com>
2022-07-22 14:17:59 +0200

    edit acceptance-test-docker.sh to optionally bypass the spec backward compatibilty tests

--- airbyte-integrations/connectors/source-scaffold-source-python/acceptance-test-docker.sh
+++ airbyte-integrations/connectors/source-scaffold-source-python/acceptance-test-docker.sh
@@ -1,10 +1,20 @@
 #!/usr/bin/env sh
 
+# Set this variable to false to bypass spec backward compatibility checks
+RUN_SPEC_BACKWARD_COMPATIBILITY_TEST=true
+
 # Build latest connector image
 docker build . -t $(cat acceptance-test-config.yml | grep "connector_image" | head -n 1 | cut -d: -f2-)
 
-# Pull latest acctest image
+# Pull latest source-acceptance test image
 docker pull airbyte/source-acceptance-test:latest
+
+TEST_OPTIONS="--acceptance-test-config /test_input"
+if [ "$RUN_SPEC_BACKWARD_COMPATIBILITY_TEST" = false ] ; then
+    echo "Won't run spec backward compatibility tests"
+    # Add a -m flag to pytest options to not run tested with spec_backward_compatibility marker
+    TEST_OPTIONS+='-m "not spec_backward_compatibility"'
+fi
 
 # Run
 docker run --rm -it \
@@ -12,5 +22,5 @@ docker run --rm -it \
     -v /tmp:/tmp \
     -v $(pwd):/test_input \
     airbyte/source-acceptance-test \
-    --acceptance-test-config /test_input
+    ${TEST_OPTIONS}
 
