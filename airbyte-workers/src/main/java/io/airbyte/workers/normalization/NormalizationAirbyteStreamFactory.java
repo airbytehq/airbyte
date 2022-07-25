16b588d9ead8120dcf02b69f544d21dc21387f7e
George Claireaux <george@claireaux.co.uk>
2022-07-25 12:29:47 +0100

    Benoit comments

--- airbyte-workers/src/main/java/io/airbyte/workers/normalization/NormalizationAirbyteStreamFactory.java
+++ airbyte-workers/src/main/java/io/airbyte/workers/normalization/NormalizationAirbyteStreamFactory.java
@@ -5,6 +5,7 @@
 package io.airbyte.workers.normalization;
 
 import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.JsonNodeType;
 import io.airbyte.commons.json.Jsons;
 import io.airbyte.commons.logging.MdcScope;
 import io.airbyte.protocol.models.AirbyteLogMessage;
@@ -49,44 +50,9 @@ public class NormalizationAirbyteStreamFactory implements AirbyteStreamFactory {
   public Stream<AirbyteMessage> create(final BufferedReader bufferedReader) {
     return bufferedReader
         .lines()
-        .flatMap(line -> {
-          final Optional<JsonNode> jsonLine = Jsons.tryDeserialize(line);
-          if (jsonLine.isEmpty()) {
-            // we log as info all the lines that are not valid json.
-            try (final var mdcScope = containerLogMdcBuilder.build()) {
-              logger.info(line);
-              // this is really hacky and vulnerable to picking up lines we don't want,
-              // however it is only for destinations that are using dbt version < 1.0.
-              // For v1 + we switch on JSON logging and parse those in the next block.
-              if (line.contains("[error]")) {
-                dbtErrors.add(line);
-              }
-            }
-          }
-          return jsonLine.stream();
-        })
-        .flatMap(jsonLine -> {
-          final Optional<AirbyteMessage> m = Jsons.tryObject(jsonLine, AirbyteMessage.class);
-          if (m.isEmpty()) {
-            // valid JSON but not an AirbyteMessage, so we assume this is a dbt json log
-            try {
-              final String logLevel = jsonLine.get("level").isNull() ? "" : jsonLine.get("level").asText();
-              final String logMsg = jsonLine.get("msg").isNull() ? "" : jsonLine.get("msg").asText();
-              try (final var mdcScope = containerLogMdcBuilder.build()) {
-                switch (logLevel) {
-                  case "debug" -> logger.debug(logMsg);
-                  case "info" -> logger.info(logMsg);
-                  case "warn" -> logger.warn(logMsg);
-                  case "error" -> dbtErrors.add(logMsg);
-                }
-              }
-            } catch (final Exception e) {
-              logger.info(jsonLine.asText());
-            }
-          }
-          return m.stream();
-        })
-        // filter logs
+        .flatMap(this::filterOutAndHandleNonJsonLines)
+        .flatMap(this::filterOutAndHandleNonAirbyteMessageLines)
+        // so now we are just left with AirbyteMessages
         .filter(airbyteMessage -> {
           final boolean isLog = airbyteMessage.getType() == AirbyteMessage.Type.LOG;
           if (isLog) {
@@ -96,6 +62,47 @@ public class NormalizationAirbyteStreamFactory implements AirbyteStreamFactory {
           }
           return !isLog;
         });
+  }
+
+  private Stream<JsonNode> filterOutAndHandleNonJsonLines(String line) {
+    final Optional<JsonNode> jsonLine = Jsons.tryDeserialize(line);
+    if (jsonLine.isEmpty()) {
+      // we log as info all the lines that are not valid json.
+      try (final var mdcScope = containerLogMdcBuilder.build()) {
+        logger.info(line);
+        // this is really hacky and vulnerable to picking up lines we don't want,
+        // however it is only for destinations that are using dbt version < 1.0.
+        // For v1 + we switch on JSON logging and parse those in the next block.
+        if (line.contains("[error]")) {
+          dbtErrors.add(line);
+        }
+      }
+    }
+    return jsonLine.stream();
+  }
+
+  private Stream<AirbyteMessage> filterOutAndHandleNonAirbyteMessageLines(JsonNode jsonLine) {
+    final Optional<AirbyteMessage> m = Jsons.tryObject(jsonLine, AirbyteMessage.class);
+    if (m.isEmpty()) {
+      // valid JSON but not an AirbyteMessage, so we assume this is a dbt json log
+      try {
+        final String logLevel = (jsonLine.getNodeType() == JsonNodeType.NULL || jsonLine.get("level").isNull())
+            ? "" : jsonLine.get("level").asText();
+        final String logMsg = jsonLine.get("msg").isNull() ? "" : jsonLine.get("msg").asText();
+        try (final var mdcScope = containerLogMdcBuilder.build()) {
+          switch (logLevel) {
+            case "debug" -> logger.debug(logMsg);
+            case "info" -> logger.info(logMsg);
+            case "warn" -> logger.warn(logMsg);
+            case "error" -> dbtErrors.add(logMsg);
+            case "" -> logger.info(jsonLine.asText());  // this shouldn't happen but logging it to avoid hiding unexpected lines.
+          }
+        }
+      } catch (final Exception e) {
+        logger.info(jsonLine.asText());
+      }
+    }
+    return m.stream();
   }
 
   public List<String> getDbtErrors() {
