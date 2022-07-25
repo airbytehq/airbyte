52e8018cf49d91b6ee4893b7daabf0344adf8f0c
Serhii <serglazebny@gmail.com>
2022-07-25 20:50:12 +0300

    Fix date validation and schema issues

--- airbyte-integrations/connectors/source-facebook-marketing/source_facebook_marketing/utils.py
+++ airbyte-integrations/connectors/source-facebook-marketing/source_facebook_marketing/utils.py
@@ -3,12 +3,19 @@
 #
 
 from datetime import datetime
+from dateutil.relativedelta import relativedelta
 
 
-class FutureDateException(Exception):
-    def __init__(self, field_name, *args, **kwargs):
-        self.field_name = field_name
-        self.message = f"{self.field_name} cannot be in the future. Please set today's date or later"
+# Facebook store metrics maximum of 37 months old. Any time range that
+# older that 37 months from current date would result in 400 Bad request
+# HTTP response.
+# https://developers.facebook.com/docs/marketing-api/reference/ad-account/insights/#overview
+DATA_RETENTION_PERIOD = 37
+
+
+class ValidationDateException(Exception):
+    def __init__(self, message, *args, **kwargs):
+        self.message = message
         super().__init__(self.message, *args, **kwargs)
 
     def __str__(self):
@@ -18,7 +25,11 @@ class FutureDateException(Exception):
         return self.__str__()
 
 
-def validate_date_field(field, date):
+def validate_date_field(field_name, date):
     if date.timestamp() > datetime.now().timestamp():
-        raise FutureDateException(field)
+        message = f"{field_name} cannot be in the future. Please set today's date or later."
+        raise ValidationDateException(message)
+    elif date.timestamp() < (datetime.now() + relativedelta(months=-DATA_RETENTION_PERIOD)).timestamp():
+        message = f"{field_name} cannot be beyond {DATA_RETENTION_PERIOD} months from the current date."
+        raise ValidationDateException(message)
     return date
