c28b34cb2a7c7750f8d2ab9a40bc74cedc642965
Alexandre Girard <alexandre@airbyte.io>
2022-07-25 18:10:32 -0700

    Alex/lowcode referencedocs (#14973)
    * Add docstrings for auth package
    
    * docstrings for the check package
    
    * docstrings for the datetime package
    
    * docstrings for the decoder package
    
    * docstrings for extractors package and fix tests
    
    * interpolation docstrings
    
    * ref ->  and parser docstrings
    
    * docstrings for parsers package
    
    * error handler docstrings
    
    * requester docstrings
    
    * more docstrings
    
    * docstrings
    
    * docstrings
    
    * docstrings
    
    * Use defined type annotations
    
    * update
    
    * update docstrings
    
    * Update docstrings
    
    * update docstrings
    
    * update docstrings
    
    * update template
    
    * Revert "update template"
    
    This reverts commit eb4a11858b2ffcd86cda430a78fc4215590f84f0.
    
    * update template
    
    * update
    
    * move to interpolated_string
    
    * update docstring
    
    * update
    
    * fix tests
    
    * format
    
    * return type can also be an array
    
    * Update airbyte-cdk/python/airbyte_cdk/sources/declarative/interpolation/interpolated_boolean.py
    
    Co-authored-by: Sherif A. Nada <snadalive@gmail.com>
    
    * Update airbyte-cdk/python/airbyte_cdk/sources/declarative/interpolation/interpolation.py
    
    Co-authored-by: Sherif A. Nada <snadalive@gmail.com>
    
    * Update airbyte-cdk/python/airbyte_cdk/sources/declarative/interpolation/jinja.py
    
    Co-authored-by: Sherif A. Nada <snadalive@gmail.com>
    
    * Update airbyte-cdk/python/airbyte_cdk/sources/declarative/interpolation/interpolated_boolean.py
    
    Co-authored-by: Sherif A. Nada <snadalive@gmail.com>
    
    * Update airbyte-cdk/python/airbyte_cdk/sources/declarative/requesters/error_handlers/backoff_strategy.py
    
    * Update as per comments
    
    Co-authored-by: Sherif A. Nada <snadalive@gmail.com>

--- airbyte-cdk/python/airbyte_cdk/sources/declarative/interpolation/interpolated_boolean.py
+++ airbyte-cdk/python/airbyte_cdk/sources/declarative/interpolation/interpolated_boolean.py
@@ -2,23 +2,41 @@
 # Copyright (c) 2022 Airbyte, Inc., all rights reserved.
 #
 
-from airbyte_cdk.sources.declarative.interpolation.jinja import JinjaInterpolation
+from typing import Any, Final, List
 
-false_values = ["False", "false", "{}", "[]", "()", "", "0", "0.0", "False", "false", {}, False, [], (), set()]
+from airbyte_cdk.sources.declarative.interpolation.jinja import JinjaInterpolation
+from airbyte_cdk.sources.declarative.types import Config
+
+FALSE_VALUES: Final[List[Any]] = ["False", "false", "{}", "[]", "()", "", "0", "0.0", "False", "false", {}, False, [], (), set()]
 
 
 class InterpolatedBoolean:
-    def __init__(self, condition):
+    f"""
+    Wrapper around a string to be evaluated to a boolean value.
+    The string will be evaluated as False if it interpolates to a value in {FALSE_VALUES}
+    """
+
+    def __init__(self, condition: str):
+        """
+        :param condition: The string representing the condition to evaluate to a boolean
+        """
         self._condition = condition
         self._default = "False"
         self._interpolation = JinjaInterpolation()
 
-    def eval(self, config, **kwargs):
+    def eval(self, config: Config, **kwargs):
+        """
+        Interpolates the predicate condition string using the config and other optional arguments passed as parameter.
+
+        :param config: The user-provided configuration as specified by the source's spec
+        :param kwargs: Optional parameters used for interpolation
+        :return: The interpolated string
+        """
         if isinstance(self._condition, bool):
             return self._condition
         else:
             evaluated = self._interpolation.eval(self._condition, config, self._default, **kwargs)
-            if evaluated in false_values:
+            if evaluated in FALSE_VALUES:
                 return False
             # The presence of a value is generally regarded as truthy, so we treat it as such
             return True
--- airbyte-cdk/python/airbyte_cdk/sources/declarative/interpolation/interpolated_string.py
+++ airbyte-cdk/python/airbyte_cdk/sources/declarative/interpolation/interpolated_string.py
@@ -2,21 +2,54 @@
 # Copyright (c) 2022 Airbyte, Inc., all rights reserved.
 #
 
-from typing import Optional
+from typing import Optional, Union
 
 from airbyte_cdk.sources.declarative.interpolation.jinja import JinjaInterpolation
+from airbyte_cdk.sources.declarative.types import Config
 
 
 class InterpolatedString:
+    """
+    Wrapper around a raw string to be interpolated with the Jinja2 templating engine
+    """
+
     def __init__(self, string: str, default: Optional[str] = None):
+        """
+        :param string: The string to evalute
+        :param default: The default value to return if the evaluation returns an empty string
+        """
         self._string = string
         self._default = default or string
         self._interpolation = JinjaInterpolation()
 
-    def eval(self, config, **kwargs):
+    def eval(self, config: Config, **kwargs):
+        """
+        Interpolates the input string using the config and other optional arguments passed as parameter.
+
+        :param config: The user-provided configuration as specified by the source's spec
+        :param kwargs: Optional parameters used for interpolation
+        :return: The interpolated string
+        """
         return self._interpolation.eval(self._string, config, self._default, **kwargs)
 
     def __eq__(self, other):
         if not isinstance(other, InterpolatedString):
             return False
         return self._string == other._string and self._default == other._default
+
+    @classmethod
+    def create(
+        cls,
+        string_or_interpolated: Union["InterpolatedString", str],
+    ):
+        """
+        Helper function to obtain an InterpolatedString from either a raw string or an InterpolatedString.
+
+        :param string_or_interpolated: Either a raw string or an InterpolatedString.
+        :param default: The default value to return if the evaluation returns an empty string
+        :return: InterpolatedString representing the input string.
+        """
+        if isinstance(string_or_interpolated, str):
+            return InterpolatedString(string_or_interpolated)
+        else:
+            return string_or_interpolated
