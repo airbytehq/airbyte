7129f3a1dc993ed62b53396ffcf6690ec14922b8
Alexandre Girard <alexandre@airbyte.io>
2022-07-25 18:15:02 -0700

    Missing docstrings

--- airbyte-cdk/python/airbyte_cdk/sources/streams/http/requests_native_auth/abtract_token.py
+++ airbyte-cdk/python/airbyte_cdk/sources/streams/http/requests_native_auth/abtract_token.py
@@ -9,19 +9,25 @@ from requests.auth import AuthBase
 
 
 class AbstractHeaderAuthenticator(AuthBase):
+    """
+    Abstract class for header-based authenticators that set a key-value pair in outgoing HTTP headers
+    """
+
     def __call__(self, request):
+        """Attach the HTTP headers required to authenticate on the HTTP request"""
         request.headers.update(self.get_auth_header())
         return request
 
     def get_auth_header(self) -> Mapping[str, Any]:
+        """HTTP header to set on the requests"""
         return {self.auth_header: self.token}
 
     @property
     @abstractmethod
     def auth_header(self) -> str:
-        pass
+        """HTTP header to set on the requests"""
 
     @property
     @abstractmethod
     def token(self) -> str:
-        pass
+        """Value of the HTTP header to set on the requests"""
