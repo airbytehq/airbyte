2e45b51d4efb1dfd0e218ed7a162b19bc99a62fe
Oleksandr Tsukanov <alexander.tsukanovvv@gmail.com>
2022-07-27 13:10:23 +0300

    Initial commit

--- airbyte-integrations/bases/base-java/javabase.sh
+++ airbyte-integrations/bases/base-java/javabase.sh
@@ -16,6 +16,7 @@ if [[ $IS_CAPTURE_HEAP_DUMP_ON_ERROR = true ]]; then
   fi
 fi
 
+export JAVA_OPTS=$JAVA_OPTS" -javaagent:/airbyte/newrelic.jar"
 # Wrap run script in a script so that we can lazy evaluate the value of APPLICATION. APPLICATION is
 # set by the dockerfile that inherits base-java, so it cannot be evaluated when base-java is built.
 # We also need to make sure that stdin of the script is piped to the stdin of the java application.
