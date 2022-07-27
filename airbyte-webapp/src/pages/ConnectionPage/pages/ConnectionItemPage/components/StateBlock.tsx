791c64ed311cd7fe901ecdc48ae33044174580ab
evantahler <evan@airbyte.io>
2022-07-27 08:34:04 -0700

    nits picked

--- airbyte-webapp/src/pages/ConnectionPage/pages/ConnectionItemPage/components/StateBlock.tsx
+++ airbyte-webapp/src/pages/ConnectionPage/pages/ConnectionItemPage/components/StateBlock.tsx
@@ -6,6 +6,19 @@ import { H5, Card } from "components";
 import { ConnectionState } from "core/request/AirbyteClient";
 import { useGetConnectionState } from "hooks/services/useConnectionHook";
 
+function formatState(
+  legacyState: ConnectionState["state"],
+  globalState: ConnectionState["globalState"],
+  streamState: ConnectionState["streamState"],
+  formatMessage: ReturnType<typeof useIntl>["formatMessage"]
+) {
+  // This hierarchy assumes that for those connections which have both global and per-stream state, the global state contains a meaningful copy of any state which would also be saved per-stream
+  const formattedState = legacyState ?? globalState ?? streamState ?? null;
+  return formattedState
+    ? JSON.stringify(formattedState, null, 2)
+    : formatMessage({ id: "tables.connectionState.noConnectionState" });
+}
+
 interface StateBlockProps {
   connectionId: string;
 }
@@ -15,7 +28,7 @@ export const StateBlock: React.FC<StateBlockProps> = ({ connectionId }) => {
   const state = useGetConnectionState(connectionId);
 
   const stateString = useMemo(
-    () => displayState(state.state, state.globalState, state.streamState, formatMessage),
+    () => formatState(state.state, state.globalState, state.streamState, formatMessage),
     [formatMessage, state.state, state.globalState, state.streamState]
   );
 
@@ -28,16 +41,3 @@ export const StateBlock: React.FC<StateBlockProps> = ({ connectionId }) => {
     </Card>
   );
 };
-
-function displayState(
-  legacyState: ConnectionState["state"],
-  globalState: ConnectionState["globalState"],
-  streamState: ConnectionState["streamState"],
-  formatMessage: ReturnType<typeof useIntl>["formatMessage"]
-) {
-  // This hierarchy assumes that for those connections which have both global and per-stream state, the global state contains a meaningful copy of any state which would also be saved per-stream
-  const displayState = legacyState ?? globalState ?? streamState ?? null;
-  return displayState
-    ? JSON.stringify(displayState, null, 2)
-    : formatMessage({ id: "tables.connectionState.noConnectionState" });
-}
