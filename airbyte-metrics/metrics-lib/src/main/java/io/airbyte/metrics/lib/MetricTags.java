833355dae90624c6d1544c1fd66ee022ebccf877
jdpgrailsdev <jpearlin1@gmail.com>
2022-08-01 09:49:24 -0400

    PR feedback

--- airbyte-metrics/metrics-lib/src/main/java/io/airbyte/metrics/lib/MetricTags.java
+++ airbyte-metrics/metrics-lib/src/main/java/io/airbyte/metrics/lib/MetricTags.java
@@ -13,12 +13,12 @@ import io.airbyte.db.instance.jobs.jooq.generated.enums.JobStatus;
  */
 public class MetricTags {
 
-  public static final String RELEASE_STAGE = "release_stage";
-  public static final String FAILURE_ORIGIN = "failure_origin";
-  public static final String JOB_STATUS = "job_status";
-  public static final String RESET_WORKFLOW_FAILURE_CAUSE = "failure_cause";
   public static final String CONNECTION_ID = "connection_id";
+  public static final String FAILURE_ORIGIN = "failure_origin";
   public static final String JOB_ID = "job_id";
+  public static final String JOB_STATUS = "job_status";
+  public static final String RELEASE_STAGE = "release_stage";
+  public static final String RESET_WORKFLOW_FAILURE_CAUSE = "failure_cause";
   public static final String WORKFLOW_TYPE = "workflow_type";
 
   public static String getReleaseStage(final ReleaseStage stage) {
