fcd979926810d88dd977188da14fb6d55b0b6977
Mark Berger <m19.berger@gmail.com>
2022-08-02 01:37:43 +0300

    Refactored to match new design

--- airbyte-webapp/src/pages/ConnectionPage/pages/ConnectionItemPage/components/StatusView.tsx
+++ airbyte-webapp/src/pages/ConnectionPage/pages/ConnectionItemPage/components/StatusView.tsx
@@ -1,10 +1,9 @@
-import { faRedoAlt } from "@fortawesome/free-solid-svg-icons";
+import { faRedoAlt, faXmark } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React, { useState } from "react";
 import { FormattedMessage } from "react-intl";
-import styled from "styled-components";
 
-import { Button, ContentCard, LoadingButton } from "components";
+import { Button, ContentCard } from "components";
 import EmptyResource from "components/EmptyResourceBlock";
 import ToolTip from "components/ToolTip";
 
@@ -13,11 +12,10 @@ import Status from "core/statuses";
 import { useConfirmationModalService } from "hooks/services/ConfirmationModal";
 import { FeatureItem, useFeature } from "hooks/services/Feature";
 import { useResetConnection, useSyncConnection } from "hooks/services/useConnectionHook";
-import useLoadingState from "hooks/useLoadingState";
-import { useListJobs } from "services/job/JobService";
+import { useCancelJob, useListJobs } from "services/job/JobService";
 
-import JobCancelButton from "./JobCancelButton";
 import JobsList from "./JobsList";
+import styles from "./StatusView.module.scss";
 
 interface StatusViewProps {
   connection: WebBackendConnectionRead;
@@ -25,43 +23,11 @@ interface StatusViewProps {
 }
 
 enum CurrentActionType {
-  RESET = "reset",
+  RESET = "reset_connection",
   SYNC = "sync",
 }
 
-const Content = styled.div`
-  margin: 0 10px;
-`;
-
-const StyledContentCard = styled(ContentCard)`
-  margin-bottom: 20px;
-`;
-
-const Title = styled.div`
-  display: flex;
-  justify-content: space-between;
-  flex-direction: row;
-  align-items: center;
-`;
-
-const TryArrow = styled(FontAwesomeIcon)`
-  margin: 0 10px -1px 0;
-  font-size: 14px;
-`;
-
-const SyncButton = styled(LoadingButton)`
-  padding: 5px 8px;
-  margin: -5px 0 -5px 9px;
-  min-width: 101px;
-  min-height: 28px;
-`;
-
-const StatusView: React.FC<StatusViewProps> = ({ connection, isStatusUpdating }) => {
-  const [currentAction, setCurrentAction] = useState<CurrentActionType>();
-  const { openConfirmationModal, closeConfirmationModal } = useConfirmationModalService();
-  const { isLoading, showFeedback, startAction } = useLoadingState();
-  const allowSync = useFeature(FeatureItem.AllowSync);
-
+const StatusView: React.FC<StatusViewProps> = ({ connection }) => {
   const jobs = useListJobs({
     configId: connection.connectionId,
     configTypes: ["sync", "reset_connection"],
@@ -71,6 +37,16 @@ const StatusView: React.FC<StatusViewProps> = ({ connection, isStatusUpdating })
     const jobStatus = jobWithAttempts?.job?.status;
     return jobStatus === Status.PENDING || jobStatus === Status.RUNNING || jobStatus === Status.INCOMPLETE;
   });
+
+  const [jobCancelling, setJobCancelling] = useState<boolean>();
+  const [currentAction, setCurrentAction] = useState<CurrentActionType | null>(
+    (jobRunningOrPending?.job?.configType || null) as CurrentActionType
+  );
+
+  const { openConfirmationModal, closeConfirmationModal } = useConfirmationModalService();
+
+  const allowSync = useFeature(FeatureItem.AllowSync);
+  const cancelJob = useCancelJob();
 
   const { mutateAsync: resetConnection } = useResetConnection();
   const { mutateAsync: syncConnection } = useSyncConnection();
@@ -95,38 +71,45 @@ const StatusView: React.FC<StatusViewProps> = ({ connection, isStatusUpdating })
 
   const onSyncNowButtonClick = () => {
     setCurrentAction(CurrentActionType.SYNC);
-    return startAction({ action: onSync });
+    return onSync();
   };
 
+  const onCancelJob = async () => {
+    if (!jobRunningOrPending?.job?.id) {
+      return;
+    }
+    setJobCancelling(true);
+    await cancelJob(jobRunningOrPending?.job?.id);
+    setJobCancelling(false);
+  };
+
+  const cancelJobBtn = (
+    <Button className={styles.cancelButton} disabled={jobCancelling} onClick={onCancelJob}>
+      <FontAwesomeIcon className={styles.iconXmark} icon={faXmark} />
+      {currentAction === CurrentActionType.RESET && <FormattedMessage id="connection.cancelReset" />}
+      {currentAction === CurrentActionType.SYNC && <FormattedMessage id="connection.cancelSync" />}
+    </Button>
+  );
+
   const resetDataBtn = (
-    <Button disabled={isStatusUpdating} onClick={onResetDataButtonClick}>
+    <Button className={styles.resetButton} secondary onClick={onResetDataButtonClick}>
       <FormattedMessage id="connection.resetData" />
     </Button>
   );
 
   const syncNowBtn = (
-    <SyncButton
-      disabled={!allowSync || isStatusUpdating}
-      isLoading={isLoading}
-      wasActive={showFeedback}
-      onClick={onSyncNowButtonClick}
-    >
-      {showFeedback ? (
-        <FormattedMessage id="sources.syncingNow" />
-      ) : (
-        <>
-          <TryArrow icon={faRedoAlt} />
-          <FormattedMessage id="sources.syncNow" />
-        </>
-      )}
-    </SyncButton>
+    <Button className={styles.syncButton} disabled={!allowSync} onClick={onSyncNowButtonClick}>
+      <FontAwesomeIcon className={styles.iconRedoAlt} icon={faRedoAlt} />
+      <FormattedMessage id="sources.syncNow" />
+    </Button>
   );
 
   return (
-    <Content>
-      <StyledContentCard
+    <div className={styles.statusView}>
+      <ContentCard
+        className={styles.contentCard}
         title={
-          <Title>
+          <div className={styles.title}>
             <FormattedMessage id="sources.syncHistory" />
             {connection.status === ConnectionStatus.active && (
               <div>
@@ -141,19 +124,18 @@ const StatusView: React.FC<StatusViewProps> = ({ connection, isStatusUpdating })
                   </>
                 )}
                 {jobRunningOrPending && (
-                  <JobCancelButton jobId={jobRunningOrPending.job?.id}>
-                    {currentAction === CurrentActionType.RESET && <FormattedMessage id="connection.cancelReset" />}
-                    {currentAction === CurrentActionType.SYNC && <FormattedMessage id="connection.cancelSync" />}
-                  </JobCancelButton>
+                  <ToolTip control={cancelJobBtn} cursor="not-allowed">
+                    <FormattedMessage id="connection.pendingSync" />
+                  </ToolTip>
                 )}
               </div>
             )}
-          </Title>
+          </div>
         }
       >
         {jobs.length ? <JobsList jobs={jobs} /> : <EmptyResource text={<FormattedMessage id="sources.noSync" />} />}
-      </StyledContentCard>
-    </Content>
+      </ContentCard>
+    </div>
   );
 };
 
