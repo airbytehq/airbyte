5fd0d50e7b212923228437af37d48eb25157be9a
marcosmarxm <marcosmarxm@gmail.com>
2022-08-03 14:46:54 -0300

    remove public class from test class

--- airbyte-metrics/metrics-lib/src/test/java/io/airbyte/metrics/lib/DogStatsDMetricClientTest.java
+++ airbyte-metrics/metrics-lib/src/test/java/io/airbyte/metrics/lib/DogStatsDMetricClientTest.java
@@ -11,7 +11,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-public class DogStatsDMetricClientTest {
+class DogStatsDMetricClientTest {
 
   DogStatsDMetricClient dogStatsDMetricClient;
 
--- airbyte-metrics/metrics-lib/src/test/java/io/airbyte/metrics/lib/DogStatsDMetricSingletonTest.java
+++ airbyte-metrics/metrics-lib/src/test/java/io/airbyte/metrics/lib/DogStatsDMetricSingletonTest.java
@@ -10,7 +10,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-public class DogStatsDMetricSingletonTest {
+class DogStatsDMetricSingletonTest {
 
   @AfterEach
   void tearDown() {
