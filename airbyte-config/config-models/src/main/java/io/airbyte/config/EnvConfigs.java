e9d561c3e67bb7ee4efd03b4fc42a214631afd85
Marcos Marx <marcosmarxm@users.noreply.github.com>
2022-08-03 15:38:52 -0400

    Kubernetes: Datadog Constant Tags (#15213)
    * adding option for constant tags to be sent to datadog for split environments
    
    * removing unnecessary space
    
    * Adding tests on envconfig
    
    * clean test
    
    * adding empty string test for constant tags
    
    * format files
    
    * remove public class from test class
    
    Co-authored-by: Guy Feldman <gfeldman@86labs.com>

--- airbyte-config/config-models/src/main/java/io/airbyte/config/EnvConfigs.java
+++ airbyte-config/config-models/src/main/java/io/airbyte/config/EnvConfigs.java
@@ -99,7 +99,7 @@ public class EnvConfigs implements Configs {
   private static final String CONTAINER_ORCHESTRATOR_IMAGE = "CONTAINER_ORCHESTRATOR_IMAGE";
   private static final String DD_AGENT_HOST = "DD_AGENT_HOST";
   private static final String DD_DOGSTATSD_PORT = "DD_DOGSTATSD_PORT";
-
+  public static final String DD_CONSTANT_TAGS = "DD_CONSTANT_TAGS";
   public static final String STATE_STORAGE_S3_BUCKET_NAME = "STATE_STORAGE_S3_BUCKET_NAME";
   public static final String STATE_STORAGE_S3_REGION = "STATE_STORAGE_S3_REGION";
   public static final String STATE_STORAGE_S3_ACCESS_KEY = "STATE_STORAGE_S3_ACCESS_KEY";
@@ -794,6 +794,15 @@ public class EnvConfigs implements Configs {
   @Override
   public String getDDDogStatsDPort() {
     return getEnvOrDefault(DD_DOGSTATSD_PORT, "");
+  }
+
+  @Override
+  public List<String> getDDConstantTags() {
+    String tagsString = getEnvOrDefault(DD_CONSTANT_TAGS, "");
+    return Splitter.on(",")
+        .splitToStream(tagsString)
+        .filter(s -> !s.trim().isBlank())
+        .collect(Collectors.toList());
   }
 
   @Override
