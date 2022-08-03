b683e5c4bad33fa544ebb751f380827a1b429cad
Neil Macneale <neil.macneale.v@gmail.com>
2022-08-03 14:25:50 -0700

    Add fauna source

---
+++ airbyte-integrations/connectors/source-fauna/unit_tests/test_util.py
@@ -0,0 +1,109 @@
+from unittest.mock import Mock, MagicMock
+from faunadb.objects import Ref
+from faunadb import query as q
+from typing import List
+
+from typing import List
+
+def ref(id: int, collection="foo") -> Ref:
+    return Ref(str(id), cls=Ref(collection, cls=Ref("collections")))
+
+def mock_logger():
+    def mock_log(level: str):
+        def perform_mock_log(msg: str):
+            print(f"[{level}]: {msg}")
+        return Mock(side_effect=perform_mock_log)
+
+    logger = Mock()
+    logger.info = mock_log("info")
+    logger.error = mock_log("error")
+    return logger
+
+class DeletionsConfig:
+    def __init__(self, mode: str, column=""):
+        self.mode = mode
+        self.column = column
+    @staticmethod
+    def ignore() -> 'DeletionsConfig':
+        return DeletionsConfig(mode="ignore")
+    @staticmethod
+    def deleted_field(column: str) -> 'DeletionsConfig':
+        return DeletionsConfig(mode="deleted_field", column=column)
+class Column:
+    def __init__(self, name: str, path: List[str], type: str, required: bool, format="", airbyte_type=""):
+        self.name = name
+        self.path = path
+        self.type = type
+        self.required = required
+        self.format = format
+        self.airbyte_type = airbyte_type
+class CollectionConfig:
+    def __init__(
+        self,
+        index="",
+        deletions=DeletionsConfig.ignore(),
+        name="",
+        data_column=True,
+        additional_columns=[],
+        page_size=64,
+    ):
+        self.name = name
+        self.data_column = data_column
+        self.additional_columns = additional_columns
+        self.page_size = page_size
+        self.index = index
+        self.deletions = deletions
+class DiscoverConfig:
+    """
+    A limited version of FullConfig, storing only the values needed for discover()
+    """
+    def __init__(self, collection: CollectionConfig):
+        self.collection = collection
+class FullConfig:
+    def __init__(self, domain: str, port: int, scheme: str, secret: str, collection=CollectionConfig()):
+        self.domain = domain
+        self.port = port
+        self.scheme = scheme
+        self.secret = secret
+        self.collection = collection
+    @staticmethod
+    def localhost(collection=CollectionConfig()) -> 'FullConfig':
+        # 9000 is our testing db, that we spawn in database_test.py
+        return FullConfig(domain="127.0.0.1", port=9000, scheme="http", secret="secret", collection=collection)
+
+def partial_overwrite(obj: dict, new: dict) -> dict:
+    """
+    Recursively replaces the values in obj with the values in new.
+    """
+    for k, v in new.items():
+        if type(v) is dict:
+            partial_overwrite(obj[k], v)
+        else:
+            obj[k] = v
+    return obj
+
+def config(extra: dict[str, any]) -> dict[str, any]:
+    obj = {
+        "domain": "127.0.0.1",
+        "port": 8443,
+        "scheme": "http",
+        "secret": "secret",
+        "collection": {
+            "name": "foo",
+            "data_column": True,
+            "page_size": 64,
+            "index": "ts",
+            "deletions": { "deletion_mode": "ignore" },
+        }
+    }
+    return partial_overwrite(obj, extra)
+
+def expand_columns_query(ref):
+    doc = q.var("document")
+    return q.let({
+        "document": q.get(ref),
+    }, {
+        "ref": q.select(["ref", "id"], doc),
+        "ts": q.select("ts", doc),
+        "data": q.select("data", doc),
+    })
---
+++ docs/integrations/sources/fauna.md
@@ -0,0 +1,91 @@
+# Fauna
+
+This page guides you through setting up a [Fauna](https://fauna.com/) source.
+
+# Overview
+
+The Fauna source supports the following sync modes:
+
+- **Full Sync** - export all the data from a Fauna collection.
+- **Incremental Sync** - export data incrementally from a Fauna collection.
+
+You will need to create a seperate source per collection you want to export.
+
+## Preliminary setup
+
+1. Choose the Fauna collection you want to export and enter that in the "Collection" field on the left.
+2. Enter the domain of the collection's database you are exporting. The url can be found on [the docs](https://docs.fauna.com/fauna/current/learn/understanding/region_groups#how-to-use-region-groups).
+
+## Full Sync
+
+Follow these steps if you want full sync for this connection.
+
+1. Create a role that can read the collection you are exporting. You can create the role with the in your dashboard or the [fauna shell](https://github.com/fauna/fauna-shell) with the following code:
+```javascript
+CreateRole({
+  name: "airbyte-readonly",
+  privileges: [{
+    resource: Collection("COLLECTION_NAME"),
+    actions: { read: true }
+  }],
+})
+```
+2. Create a key with that role. You can create a key using this code:
+```javascript
+CreateKey({
+  name: "airbyte-readonly",
+  role: Role("airbyte-readonly"),
+})
+```
+3. Copy the `secret` output by the `CreateKey` command and enter that as the "Fauna Secret" on the left.
+
+## Incremental Sync
+
+Follow these steps if you want incremental sync for this connection.
+
+1. Create the "Incremental Sync Index" which contains the `ts` and `ref` of the document as the first two values and no terms. This will enable the Fauna source to perform incremental syncs. You can create the index with the [fauna shell](https://github.com/fauna/fauna-shell) or in your dashboard with the following code:
+```javascript
+CreateIndex({
+  name: "INDEX_NAME",
+  source: Collection("COLLECTION_NAME"),
+  terms: [],
+  values: [
+    { "field": "ts" },
+    { "field": "ref" }
+  ]
+})
+```
+2. Create a role that can read the collection, the index, and the metadata of all indexes. It needs access to index metadata in order to validate the index settings. You can create the role with this code:
+```javascript
+CreateRole({
+  name: "airbyte-readonly",
+  privileges: [
+    {
+      resource: Collection("COLLECTION_NAME"),
+      actions: { read: true }
+    },
+    {
+      resource: Index("INDEX_NAME"),
+      actions: { read: true }
+    },
+    {
+      resource: Indexes(),
+      actions: { read: true }
+    }
+  ],
+})
+```
+3. Create a key with that role. You can create a key using this code:
+```javascript
+CreateKey({
+  name: "airbyte-readonly",
+  role: Role("airbyte-readonly"),
+})
+```
+4. Copy the `secret` output by the `CreateKey` command and enter that as the "Fauna Secret" on the left.
+
+## Changelog
+
+| Version | Date       | Pull Request                                     | Subject          |
+| ------- | ---------- | ------------------------------------------------ | ---------------- |
+| 0.1.0   | 2022-08-03 | [0](https://github.com/airbytehq/airbyte/pull/0) | Add Fauna Source |
