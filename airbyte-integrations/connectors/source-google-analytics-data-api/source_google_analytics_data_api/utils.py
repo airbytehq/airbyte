7c02f2a09030df9bb528c7c577c3721c944d4255
Roman Yermilov <roman.yermilov@globallogic.com>
2022-08-03 12:16:51 +0400

    Source: google analytics data api refactoring

---
+++ airbyte-integrations/connectors/source-google-analytics-data-api/source_google_analytics_data_api/utils.py
@@ -0,0 +1,14 @@
+import calendar
+import datetime
+
+
+def datetime_to_secs(dt: datetime.datetime) -> int:
+    return calendar.timegm(dt.utctimetuple())
+
+
+def string_to_date(d: str, f: str = "%Y-%m-%d") -> datetime.date:
+    return datetime.datetime.strptime(d, f).date()
+
+
+def date_to_string(d: datetime.date, f: str = "%Y-%m-%d") -> str:
+    return d.strftime(f)
