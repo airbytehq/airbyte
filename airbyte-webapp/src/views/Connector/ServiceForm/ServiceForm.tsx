6b5e4fb6e6975d833f7c9054fc67e81ec1d53f8f
Edmundo Ruiz Ghanem <168664+edmundito@users.noreply.github.com>
2022-08-04 10:30:46 -0700

    🪟 🎉 Improved rendering of variable input fields in source/destination and update custom transformations form. (#14514)
    * Show field names instead of index in ArraySection items
    
    * Update ArraySection to show description
    
    * Update EditorRow to show description as tooltip
    * Update ArrayOfObjectsEditor with render name and description props, fix base item interface
    
    * Update ToolTip so that cursor only changes if specified, make inline
    
    * Update form types from type to interface
    
    * Update EditorRow style to match new design
    
    * Update Add/Edit mode in ArrayOfObjectsEditor to use Modal
    
    * Add bottom margin to content card title to ensure drop shadow does not overlap bottom content
    
    * Cleanup spacing in Modal scss
    
    * Add width and height settings to ArrayOfObjectEditor
    Ensure that Transformation field edit modal and ArraySection edit modal have the right width and height
    
    * Move buttons outside of ArrayOfObjects editor and let TransformationField and ArraySection handle it
    
    * Move DBT command reference docs to links config
    
    * Move ArrayOfObjectsEditor to css module
    
    * Split variable input fields form into component, make editable via hidden field
    
    * Removed unfinished flow usage from ArraySection
    
    * Add button type to EditorRow buttons to prevent Formik warning
    
    * Fix path prop in ArraySection to not include "hidden"
    
    * Add validation to VariableInputFieldForm
    * Move default field from FromGroup to FormItemBase
    * Add Form validationSchema to Service Form Context
    * Update path in ArraySection to use correct path
    * Update VariableInputFieldForm to use form validatonSchema to determine if the data is valid or not
    * Add default values to VariableInputFieldForm item
    
    * Move EditorRow styles to scss and fix wrappig on small width
    
    * Add styles for tooltip in ArraySection
    
    * Update ArrayOfObjectsEditor component to match design
    * Update edit and close button icons
    * Fix spacing
    * Show item count
    * Show add / edit string on modal depending on mode
    
    * Update 0 items to No items in en file
    
    * Fix serviceForm 'should fill right values in array of objects field' test
    * Add testId field to Modal
    * Add testId to ArrayOfObjectsEditor modal
    * Cleanup test
    
    * Fix 'should fill all fields by right values' in serviceForm tests
    * Update addPriceListItem to a utility
    * Only use document.body for modal query
    
    * Cleanup mocks in serviceForm test
    * Update naming in DocumentationPanelContext
    
    * Fix stylings in EditorRow and ContentCard
    
    * Update EditorRow to always show a border between items regardless of having a description or not
    
    * Update VariableInputField field name such that it can be removed from formik values when done or cancelled
    
    * Update temp field name in variable input fields form and explain the reasons for it
    
    * Update ArrayOfObjectEditor to render form as prop instead of children
    
    Co-authored-by: Tim Roes <tim@airbyte.io>

--- airbyte-webapp/src/views/Connector/ServiceForm/ServiceForm.tsx
+++ airbyte-webapp/src/views/Connector/ServiceForm/ServiceForm.tsx
@@ -265,6 +265,7 @@ const ServiceForm: React.FC<ServiceFormProps> = (props) => {
           availableServices={props.availableServices}
           isEditMode={props.isEditMode}
           isLoadingSchema={props.isLoading}
+          validationSchema={validationSchema}
         >
           {!props.isEditMode && <SetDefaultName />}
           <RevalidateOnValidationSchemaChange validationSchema={validationSchema} />
