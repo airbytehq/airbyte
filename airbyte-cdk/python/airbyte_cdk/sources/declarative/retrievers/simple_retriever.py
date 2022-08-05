0ccf5cb0d077d6169ae9e63b72575067cf124bfb
Alexandre Girard <alexandre@airbyte.io>
2022-08-05 08:00:01 -0700

    Update airbyte-cdk/python/airbyte_cdk/sources/declarative/retrievers/simple_retriever.py
    Co-authored-by: Sherif A. Nada <snadalive@gmail.com>

--- airbyte-cdk/python/airbyte_cdk/sources/declarative/retrievers/simple_retriever.py
+++ airbyte-cdk/python/airbyte_cdk/sources/declarative/retrievers/simple_retriever.py
@@ -299,7 +299,6 @@ class SimpleRetriever(Retriever, HttpStream):
         # Warning: use self.state instead of the stream_state passed as argument!
         stream_slice = stream_slice or {}  # None-check
         self._paginator.reset()
-        print("after")
         records_generator = HttpStream.read_records(self, sync_mode, cursor_field, stream_slice, self.state)
         for r in records_generator:
             self._stream_slicer.update_cursor(stream_slice, last_record=r)
