f0aee6a16767d1781e0c7c8227a1a4548cf486f1
sivankumar86 <sivankumar86@gmail.com>
2022-08-05 10:14:24 +1000

    information schema included

--- airbyte-integrations/connectors/source-mssql/src/main/java/io/airbyte/integrations/source/mssql/MssqlSource.java
+++ airbyte-integrations/connectors/source-mssql/src/main/java/io/airbyte/integrations/source/mssql/MssqlSource.java
@@ -205,7 +205,6 @@ public class MssqlSource extends AbstractJdbcSource<JDBCType> implements Source 
   @Override
   public Set<String> getExcludedInternalNameSpaces() {
     return Set.of(
-        "INFORMATION_SCHEMA",
         "sys",
         "spt_fallback_db",
         "spt_monitor",
