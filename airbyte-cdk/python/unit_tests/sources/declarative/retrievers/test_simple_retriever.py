7807d1ee87cc20bc5620afb8b9b800fb64dd637c
Alexandre Girard <alexandre@airbyte.io>
2022-08-06 10:10:23 -0700

    low-code connectors: convert request headers to string before submitting them (#15336)
    * convert values to strings
    
    * introduce variable
    
    * kwargs
    
    * bump version

--- airbyte-cdk/python/unit_tests/sources/declarative/retrievers/test_simple_retriever.py
+++ airbyte-cdk/python/unit_tests/sources/declarative/retrievers/test_simple_retriever.py
@@ -193,21 +193,19 @@ def test_backoff_time(test_name, response_action, retry_in, expected_backoff_tim
     ],
 )
 def test_get_request_options_from_pagination(test_name, paginator_mapping, stream_slicer_mapping, expected_mapping):
+    # This test does not test request headers because they must be strings
     paginator = MagicMock()
-    paginator.get_request_headers.return_value = paginator_mapping
     paginator.get_request_params.return_value = paginator_mapping
     paginator.get_request_body_data.return_value = paginator_mapping
     paginator.get_request_body_json.return_value = paginator_mapping
 
     stream_slicer = MagicMock()
-    stream_slicer.get_request_headers.return_value = stream_slicer_mapping
     stream_slicer.get_request_params.return_value = stream_slicer_mapping
     stream_slicer.get_request_body_data.return_value = stream_slicer_mapping
     stream_slicer.get_request_body_json.return_value = stream_slicer_mapping
 
     base_mapping = {"key": "value"}
     requester = MagicMock()
-    requester.get_request_headers.return_value = base_mapping
     requester.get_request_params.return_value = base_mapping
     requester.get_request_body_data.return_value = base_mapping
     requester.get_request_body_json.return_value = base_mapping
@@ -224,10 +222,47 @@ def test_get_request_options_from_pagination(test_name, paginator_mapping, strea
     )
 
     request_option_type_to_method = {
-        RequestOptionType.header: retriever.request_headers,
         RequestOptionType.request_parameter: retriever.request_params,
         RequestOptionType.body_data: retriever.request_body_data,
         RequestOptionType.body_json: retriever.request_body_json,
+    }
+
+    for _, method in request_option_type_to_method.items():
+        if expected_mapping:
+            actual_mapping = method(None, None, None)
+            assert expected_mapping == actual_mapping
+        else:
+            try:
+                method(None, None, None)
+                assert False
+            except ValueError:
+                pass
+
+
+@pytest.mark.parametrize(
+    "test_name, paginator_mapping, expected_mapping",
+    [
+        ("test_only_base_headers", {}, {"key": "value"}),
+        ("test_header_from_pagination", {"offset": 1000}, {"key": "value", "offset": "1000"}),
+        ("test_duplicate_header", {"key": 1000}, None),
+    ],
+)
+def test_get_request_headers(test_name, paginator_mapping, expected_mapping):
+    # This test is separate from the other request options because request headers must be strings
+    paginator = MagicMock()
+    paginator.get_request_headers.return_value = paginator_mapping
+    requester = MagicMock()
+
+    base_mapping = {"key": "value"}
+    requester.get_request_headers.return_value = base_mapping
+
+    record_selector = MagicMock()
+    retriever = SimpleRetriever(
+        name="stream_name", primary_key=primary_key, requester=requester, record_selector=record_selector, paginator=paginator, options={}
+    )
+
+    request_option_type_to_method = {
+        RequestOptionType.header: retriever.request_headers,
     }
 
     for _, method in request_option_type_to_method.items():
