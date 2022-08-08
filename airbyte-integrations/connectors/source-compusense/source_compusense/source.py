6560b19553109909a108d0bcea3fc121d5014b0f
Kyle Bochanski <kbochanski@plenty.ag>
2022-08-08 15:32:31 -0600

    pass spec check and discover

--- airbyte-integrations/connectors/source-compusense/source_compusense/source.py
+++ airbyte-integrations/connectors/source-compusense/source_compusense/source.py
@@ -10,68 +10,19 @@ import requests
 from airbyte_cdk.sources import AbstractSource
 from airbyte_cdk.sources.streams import Stream
 from airbyte_cdk.sources.streams.http import HttpStream
-from airbyte_cdk.sources.streams.http.auth import TokenAuthenticator
-
-"""
-TODO: Most comments in this class are instructive and should be deleted after the source is implemented.
-
-This file provides a stubbed example of how to use the Airbyte CDK to develop both a source connector which supports full refresh or and an
-incremental syncs from an HTTP API.
-
-The various TODOs are both implementation hints and steps - fulfilling all the TODOs should be sufficient to implement one basic and one incremental
-stream from a source. This pattern is the same one used by Airbyte internally to implement connectors.
-
-The approach here is not authoritative, and devs are free to use their own judgement.
-
-There are additional required TODOs in the files within the integration_tests folder and the spec.yaml file.
-"""
-
+from airbyte_cdk.sources.streams.http.auth import TokenAuthenticator,HttpAuthenticator
+from airbyte_cdk.sources.streams.http.requests_native_auth.token import BasicHttpAuthenticator
+from airbyte_cdk.sources.streams import IncrementalMixin
 
 # Basic full refresh stream
 class CompusenseStream(HttpStream, ABC):
     """
-    TODO remove this comment
-
-    This class represents a stream output by the connector.
-    This is an abstract base class meant to contain all the common functionality at the API level e.g: the API base URL, pagination strategy,
-    parsing responses etc..
-
-    Each stream should extend this class (or another abstract subclass of it) to specify behavior unique to that stream.
-
-    Typically for REST APIs each stream corresponds to a resource in the API. For example if the API
-    contains the endpoints
-        - GET v1/customers
-        - GET v1/employees
-
-    then you should have three classes:
-    `class CompusenseStream(HttpStream, ABC)` which is the current class
-    `class Customers(CompusenseStream)` contains behavior to pull data for customers using v1/customers
-    `class Employees(CompusenseStream)` contains behavior to pull data for employees using v1/employees
-
-    If some streams implement incremental sync, it is typical to create another class
-    `class IncrementalCompusenseStream((CompusenseStream), ABC)` then have concrete stream implementations extend it. An example
-    is provided below.
-
-    See the reference docs for the full list of configurable options.
     """
 
-    # TODO: Fill in the url base. Required.
-    url_base = "https://example-api.com/v1/"
+    url_base = "https://plentytestdataapi.compusensecloud.com/jf6testresultsapife/api/"
 
     def next_page_token(self, response: requests.Response) -> Optional[Mapping[str, Any]]:
         """
-        TODO: Override this method to define a pagination strategy. If you will not be using pagination, no action is required - just return None.
-
-        This method should return a Mapping (e.g: dict) containing whatever information required to make paginated requests. This dict is passed
-        to most other methods in this class to help you form headers, request bodies, query params, etc..
-
-        For example, if the API accepts a 'page' parameter to determine which page of the result to return, and a response from the API contains a
-        'page' number, then this method should probably return a dict {'page': response.json()['page'] + 1} to increment the page count by 1.
-        The request_params method should then read the input next_page_token and set the 'page' param to next_page_token['page'].
-
-        :param response: the most recent response from the API
-        :return If there is another page in the result, a mapping (e.g: dict) containing information needed to query the next page in the response.
-                If there are no more pages in the result, return None.
         """
         return None
 
@@ -79,35 +30,44 @@ class CompusenseStream(HttpStream, ABC):
         self, stream_state: Mapping[str, Any], stream_slice: Mapping[str, any] = None, next_page_token: Mapping[str, Any] = None
     ) -> MutableMapping[str, Any]:
         """
-        TODO: Override this method to define any query parameters to be set. Remove this method if you don't need to define request params.
-        Usually contains common params e.g. pagination size etc.
         """
-        return {}
+        # TODO (kbochanski@plenty.ag): These should be dynamic
+        return {"start":"20220101","end":"20220710"}
+    
+    def request_headers(
+        self, stream_state: Mapping[str, Any], stream_slice: Mapping[str, Any] = None, next_page_token: Mapping[str, Any] = None
+    ) -> Mapping[str, Any]:
+        """
+        Override to return any non-auth headers. Authentication headers will overwrite any overlapping headers returned from this method.
+        """
+        # TODO (kbochanski@plenty.ag): Determine why exactyl this is required for Compusense API
+        return {'user-agent': "airbyte-client"}
 
     def parse_response(self, response: requests.Response, **kwargs) -> Iterable[Mapping]:
         """
-        TODO: Override this method to define how a response is parsed.
         :return an iterable containing each record in the response
         """
-        yield {}
+        # Since Compusense data is of the type array, return type does not need to be wrapped with []
+        return response.json()
 
 
-class Customers(CompusenseStream):
+class SurveyMetadata(CompusenseStream):
     """
-    TODO: Change class name to match the table/data source this stream corresponds to.
     """
 
-    # TODO: Fill in the primary key. Required. This is usually a unique field in the stream, like an ID or a timestamp.
-    primary_key = "customer_id"
+    primary_key = "testId"
+
+    def __init__(self, **kwargs):
+        # Pass authenticator to parent class
+        super().__init__(**kwargs)
 
     def path(
         self, stream_state: Mapping[str, Any] = None, stream_slice: Mapping[str, Any] = None, next_page_token: Mapping[str, Any] = None
     ) -> str:
         """
-        TODO: Override this method to define the path this stream corresponds to. E.g. if the url is https://example-api.com/v1/customers then this
-        should return "customers". Required.
         """
-        return "customers"
+        # Appends return value to base url
+        return "testmetadata"
 
 
 # Basic incremental stream
@@ -193,7 +153,28 @@ class SourceCompusense(AbstractSource):
         :param logger:  logger object
         :return Tuple[bool, any]: (True, None) if the input config can be used to connect to the API successfully, (False, error) otherwise.
         """
-        return True, None
+        # TODO (kbochanski@plenty.ag): May want to use TokenAuthenticator?
+        auth = requests.auth.HTTPBasicAuth(config["client_id"],config["client_secret"])
+        url = "https://plentytestdataapi.compusensecloud.com/jf6testresultsapife/api/testmetadata"
+        querystring = {"start":"20220101","end":"20220710"}
+        headers = {
+            'user-agent': "airbyte-client",
+            }
+        try:
+            
+            logger.info('Checking Compusense Connection...')
+            # TODO (kbochanski@plenty.ag): Is a health endpoint to hit instead?
+            response = requests.get(url, auth = auth, headers=headers, params=querystring)
+
+            # Check that data is returned
+            assert response.status_code == 200
+            body = response.json()
+            assert len(body) > 0
+            
+            return True, None
+        except requests.exceptions.RequestException as e:
+            logger(e)
+            return False, e
 
     def streams(self, config: Mapping[str, Any]) -> List[Stream]:
         """
@@ -201,6 +182,12 @@ class SourceCompusense(AbstractSource):
 
         :param config: A Mapping of the user input configuration as defined in the connector spec.
         """
-        # TODO remove the authenticator if not required.
-        auth = TokenAuthenticator(token="api_key")  # Oauth2Authenticator is also available if you need oauth support
-        return [Customers(authenticator=auth), Employees(authenticator=auth)]
+        client_id = config["client_id"]
+        client_secret = config["client_secret"]
+        # See https://github.com/airbytehq/airbyte/blob/94abef3acc085320a6493a557d6bcdd31252f94f/airbyte-cdk/python/airbyte_cdk/sources/streams/http/requests_native_auth/token.py
+        auth = BasicHttpAuthenticator(client_id,client_secret)
+
+        return [SurveyMetadata(authenticator=auth),]
+
+# TODO (kbochanski@plenty.ag): Need to finish https://docs.airbyte.com/connector-development/tutorials/cdk-tutorial-python-http/read-data
+# TODO (kbochanski@plenty.ag): Will want to use an incremental loader BUT query only takes date inputs and data returned does not have a single date field (would need to combine day,month,year)
