c0b526c1a6703a18a462f3bf2e6de6afc4880c63
pmossman <parker@airbyte.io>
2022-08-08 09:02:18 -0700

    Squashed commit of the following:
    commit 677b4771926b2521a830b0dc0fa24a5b3ba61389
    Author: pmossman <parker@airbyte.io>
    Date:   Fri Aug 5 15:24:10 2022 -0700
    
        format
    
    commit 679b602a41752f8673544152810e01f7119b7af0
    Author: pmossman <parker@airbyte.io>
    Date:   Fri Aug 5 15:18:03 2022 -0700
    
        move converters to module that worker can access, convert statePersistence calls to API calls, convert statePersistence helper to local private method
    
    commit 628931d42679ad9c0b6f610c59c09b0a9f0aa57a
    Author: pmossman <parker@airbyte.io>
    Date:   Fri Aug 5 10:37:00 2022 -0700
    
        add createOrUpdateState API endpoint
    
    commit bcf424dc1e5008153711dbb6d46fc92ce572c051
    Author: pmossman <parker@airbyte.io>
    Date:   Fri Aug 5 13:42:16 2022 -0700
    
        add AirbyteApiClient to WorkerApp for data plane workers to use

--- airbyte-config/config-models/src/main/java/io/airbyte/config/helpers/ProtocolConverters.java
+++ airbyte-config/config-models/src/main/java/io/airbyte/config/helpers/ProtocolConverters.java
@@ -2,17 +2,23 @@
  * Copyright (c) 2022 Airbyte, Inc., all rights reserved.
  */
 
-package io.airbyte.server.converters;
+package io.airbyte.config.helpers;
 
 import io.airbyte.api.model.generated.StreamDescriptor;
 
 /**
- * Utilities that convert protocol types into API representations of the protocol type.
+ * Utilities that convert protocol types into API or client representations of the protocol type.
  */
 public class ProtocolConverters {
 
   public static StreamDescriptor streamDescriptorToApi(final io.airbyte.protocol.models.StreamDescriptor protocolStreamDescriptor) {
     return new StreamDescriptor().name(protocolStreamDescriptor.getName()).namespace(protocolStreamDescriptor.getNamespace());
+  }
+
+  public static io.airbyte.api.client.model.generated.StreamDescriptor streamDescriptorToClient(final io.airbyte.protocol.models.StreamDescriptor protocolStreamDescriptor) {
+    return new io.airbyte.api.client.model.generated.StreamDescriptor()
+        .name(protocolStreamDescriptor.getName())
+        .namespace(protocolStreamDescriptor.getNamespace());
   }
 
   public static io.airbyte.protocol.models.StreamDescriptor streamDescriptorToProtocol(final StreamDescriptor apiStreamDescriptor) {
--- airbyte-workers/src/main/java/io/airbyte/workers/WorkerApp.java
+++ airbyte-workers/src/main/java/io/airbyte/workers/WorkerApp.java
@@ -7,6 +7,7 @@ package io.airbyte.workers;
 import io.airbyte.analytics.Deployment;
 import io.airbyte.analytics.TrackingClient;
 import io.airbyte.analytics.TrackingClientSingleton;
+import io.airbyte.api.client.AirbyteApiClient;
 import io.airbyte.commons.features.EnvVariableFeatureFlags;
 import io.airbyte.commons.features.FeatureFlags;
 import io.airbyte.commons.lang.CloseableShutdownHook;
@@ -147,6 +148,7 @@ public class WorkerApp {
   private final FeatureFlags featureFlags;
   private final JobCreator jobCreator;
   private final StatePersistence statePersistence;
+  private final AirbyteApiClient airbyteApiClient;
 
   public void start() {
     final Map<String, String> mdc = MDC.getCopyOfContextMap();
@@ -239,7 +241,7 @@ public class WorkerApp {
       final DbtTransformationActivityImpl dbtTransformationActivity = getDbtActivityImpl(
           defaultWorkerConfigs,
           defaultProcessFactory);
-      final PersistStateActivityImpl persistStateActivity = new PersistStateActivityImpl(statePersistence, featureFlags);
+      final PersistStateActivityImpl persistStateActivity = new PersistStateActivityImpl(airbyteApiClient, featureFlags);
 
       for (final String taskQueue : configs.getSyncDataPlaneTaskQueues()) {
         // TODO (parker) consider separating out maxSyncActivityWorkers and maxSyncWorkflowWorkers
@@ -498,6 +500,20 @@ public class WorkerApp {
             webUrlHelper,
             jobErrorReportingClient);
 
+    LOGGER.info("Creating Airbyte Config Api Client with Host: {}, Port: {}, Auth-Header Name: {}, Auth-Header Value: {}",
+        configs.getAirbyteApiHost(), configs.getAirbyteApiPort(), configs.getAirbyteApiAuthHeaderName(), configs.getAirbyteApiAuthHeaderValue());
+
+    final AirbyteApiClient airbyteApiClient = new AirbyteApiClient(
+        new io.airbyte.api.client.invoker.generated.ApiClient()
+            .setScheme("http")
+            .setHost(configs.getAirbyteApiHost())
+            .setPort(configs.getAirbyteApiPort())
+            .setBasePath("/api")
+            .setRequestInterceptor(builder -> {
+              builder.setHeader(configs.getAirbyteApiAuthHeaderName(), configs.getAirbyteApiAuthHeaderValue());
+              builder.setHeader("User-Agent", "WorkerApp");
+            }));
+
     new WorkerApp(
         workspaceRoot,
         defaultProcessFactory,
@@ -529,7 +545,8 @@ public class WorkerApp {
         streamResetPersistence,
         featureFlags,
         jobCreator,
-        statePersistence).start();
+        statePersistence,
+        airbyteApiClient).start();
   }
 
   public static void main(final String[] args) {
