779bc7b8b476420b322e531e2b6a0ce1285ea042
Arsen Losenko <arsen.losenko@globallogic.com>
2022-08-09 15:20:27 +0300

    Move logging line, specify another label

--- airbyte-integrations/connectors/source-instagram/source_instagram/source.py
+++ airbyte-integrations/connectors/source-instagram/source_instagram/source.py
@@ -47,10 +47,10 @@ class SourceInstagram(AbstractSource):
         """
         ok = False
         error_msg = None
-        logger.info(f"access token received after OAuth: {config.access_token}")
 
         try:
             config = ConnectorConfig.parse_obj(config)  # FIXME: this will be not need after we fix CDK
+            logger.info(f"access token received after OAuth: {config.access_token}")
             api = InstagramAPI(access_token=config.access_token)
             logger.info(f"Available accounts: {api.accounts}")
             ok = True
