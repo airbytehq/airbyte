4b34b71eba07abcda72f75882f9957a8966e8e70
Davin Chia <davinchia@gmail.com>
2022-08-10 16:49:43 -0700

    Add google endpoints authentication code to worker.

--- airbyte-config/config-models/src/main/java/io/airbyte/config/EnvConfigs.java
+++ airbyte-config/config-models/src/main/java/io/airbyte/config/EnvConfigs.java
@@ -119,6 +119,11 @@ public class EnvConfigs implements Configs {
   private static final String TEMPORAL_CLOUD_CLIENT_CERT = "TEMPORAL_CLOUD_CLIENT_CERT";
   private static final String TEMPORAL_CLOUD_CLIENT_KEY = "TEMPORAL_CLOUD_CLIENT_KEY";
 
+  private static final String DATA_PLANE_SERVICE_ACCOUNT_CREDENTIALS_PATH = "DATA_PLANE_SERVICE_ACCOUNT_CREDENTIALS_PATH";
+  private static final String DATA_PLANE_SERVICE_ACCOUNT_EMAIL = "DATA_PLANE_SERVICE_ACCOUNT_EMAIL";
+  private static final String CONTROL_PLANE_GOOGLE_ENDPOINT = "CONTROL_PLANE_GOOGLE_ENDPOINT";
+
+
   public static final String ACTIVITY_MAX_TIMEOUT_SECOND = "ACTIVITY_MAX_TIMEOUT_SECOND";
   public static final String ACTIVITY_MAX_ATTEMPT = "ACTIVITY_MAX_ATTEMPT";
   public static final String ACTIVITY_INITIAL_DELAY_BETWEEN_ATTEMPTS_SECONDS = "ACTIVITY_INITIAL_DELAY_BETWEEN_ATTEMPTS_SECONDS";
@@ -443,6 +448,21 @@ public class EnvConfigs implements Configs {
   @Override
   public String getTemporalCloudClientKey() {
     return getEnvOrDefault(TEMPORAL_CLOUD_CLIENT_KEY, "");
+  }
+
+  @Override
+  public String getDataPlaneServiceAccountCredentialsPath() {
+    return getEnvOrDefault(DATA_PLANE_SERVICE_ACCOUNT_CREDENTIALS_PATH, "");
+  }
+
+  @Override
+  public String getDataPlaneServiceAccountEmail() {
+    return getEnvOrDefault(DATA_PLANE_SERVICE_ACCOUNT_EMAIL, "");
+  }
+
+  @Override
+  public String getControlPlaneGoogleEndpoint() {
+    return getEnvOrDefault(CONTROL_PLANE_GOOGLE_ENDPOINT, "");
   }
 
   // Airbyte Services
@@ -810,7 +830,7 @@ public class EnvConfigs implements Configs {
 
   @Override
   public List<String> getDDConstantTags() {
-    String tagsString = getEnvOrDefault(DD_CONSTANT_TAGS, "");
+    final String tagsString = getEnvOrDefault(DD_CONSTANT_TAGS, "");
     return Splitter.on(",")
         .splitToStream(tagsString)
         .filter(s -> !s.trim().isBlank())
