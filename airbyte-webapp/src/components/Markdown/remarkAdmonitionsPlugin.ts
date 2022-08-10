7d5825190df0a2c622fe57d7a898c9df3f43ac15
Edmundo Ruiz Ghanem <edmundo@airbyte.io>
2022-08-10 12:37:18 -0400

    Support docusaurus admonitions in markdown docs
    * Narrow down docs folders in vscod workspace
    * Add mdast and remark-directive dependencies
    * Add remark-directive to Markdown component plugins
    * Add admonitions plugin to support docusaurus admonitions
    * Add styles for admonitions in markdown files

---
+++ airbyte-webapp/src/components/Markdown/remarkAdmonitionsPlugin.ts
@@ -0,0 +1,22 @@
+import { Root } from "mdast";
+import { Plugin } from "unified";
+import { Node } from "unist";
+import { visit } from "unist-util-visit";
+
+const SUPPORTED_ADMONITION_NAMES: Readonly<string[]> = ["note", "tip", "info", "caution", "warning", "danger"];
+const SUPPORTED_NODE_TYPES: Readonly<string[]> = ["textDirective", "leafDirective", "containerDirective"];
+
+export const remarkAdmonitionsPlugin: Plugin<[], Root> = () => (tree) => {
+  visit<Node & { name?: string }>(tree, (node) => {
+    if (!node.name || !SUPPORTED_ADMONITION_NAMES.includes(node.name) || !SUPPORTED_NODE_TYPES.includes(node.type)) {
+      return;
+    }
+
+    const data = node.data ?? (node.data = {});
+    const tagName = node.type === "textDirective" ? "span" : "div";
+    const className = `admonition admonition--${node.name}`;
+
+    data.hName = tagName;
+    data.hProperties = { class: className };
+  });
+};
