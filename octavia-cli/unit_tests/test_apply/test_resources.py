fe8ed8eb418a162186e4f5acce22f45e5c10d274
alafanechere <augustin.lafanechere@gmail.com>
2022-08-10 12:34:39 +0200

    handle new schedule related fields in generate / apply / import

--- octavia-cli/unit_tests/test_apply/test_resources.py
+++ octavia-cli/unit_tests/test_apply/test_resources.py
@@ -8,7 +8,8 @@ from unittest.mock import mock_open, patch
 import pytest
 from airbyte_api_client import ApiException
 from airbyte_api_client.model.airbyte_catalog import AirbyteCatalog
-from airbyte_api_client.model.connection_schedule import ConnectionSchedule
+from airbyte_api_client.model.connection_schedule_data_basic_schedule import ConnectionScheduleDataBasicSchedule
+from airbyte_api_client.model.connection_schedule_type import ConnectionScheduleType
 from airbyte_api_client.model.connection_status import ConnectionStatus
 from airbyte_api_client.model.destination_definition_id_with_workspace_id import DestinationDefinitionIdWithWorkspaceId
 from airbyte_api_client.model.namespace_definition_type import NamespaceDefinitionType
@@ -468,7 +469,8 @@ class TestConnection:
                         }
                     ]
                 },
-                "schedule": {"units": 1, "time_unit": "days"},
+                "schedule_type": "basic",
+                "schedule_data": {"units": 1, "time_unit": "days"},
                 "status": "active",
                 "resource_requirements": {"cpu_request": "foo", "cpu_limit": "foo", "memory_request": "foo", "memory_limit": "foo"},
             },
@@ -551,6 +553,28 @@ class TestConnection:
                                     "aliasName": "alias_name_example",
                                     "selected": True,
                                 },
+                            }
+                        ]
+                    },
+                    "schedule": {"units": 1, "time_unit": "days"},
+                    "status": "active",
+                    "resource_requirements": {"cpu_request": "foo", "cpu_limit": "foo", "memory_request": "foo", "memory_limit": "foo"},
+                },
+            },
+            {
+                "definition_type": "connection",
+                "resource_name": "my_connection",
+                "source_id": "my_source",
+                "destination_id": "my_destination",
+                "configuration": {
+                    "namespace_definition": "customformat",
+                    "namespace_format": "foo",
+                    "prefix": "foo",
+                    "sync_catalog": {
+                        "streams": [
+                            {
+                                "stream": {},
+                                "config": {},
                             }
                         ]
                     },
@@ -776,7 +800,11 @@ class TestConnection:
         assert configuration["namespace_definition"] == NamespaceDefinitionType(
             connection_configuration["configuration"]["namespace_definition"]
         )
-        assert configuration["schedule"] == ConnectionSchedule(**connection_configuration["configuration"]["schedule"])
+        assert configuration["schedule_type"] == ConnectionScheduleType(connection_configuration["configuration"]["schedule_type"])
+        assert (
+            configuration["schedule_data"].to_dict()
+            == ConnectionScheduleDataBasicSchedule(**connection_configuration["configuration"]["schedule_data"]).to_dict()
+        )
         assert configuration["resource_requirements"] == ResourceRequirements(
             **connection_configuration["configuration"]["resource_requirements"]
         )
@@ -786,7 +814,8 @@ class TestConnection:
             "namespace_format",
             "prefix",
             "sync_catalog",
-            "schedule",
+            "schedule_type",
+            "schedule_data",
             "status",
             "resource_requirements",
         ]
