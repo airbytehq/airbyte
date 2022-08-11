3c908c9ce874d31a665ac6c545c961b46ca2f207
Andrii Korotkov <andrii.h.korotkov@globallogic.com>
2022-08-11 12:04:48 +0300

    fixed minor remarks

--- airbyte-integrations/connectors/source-mysql-strict-encrypt/src/main/java/io/airbyte/integrations/source/mysql_strict_encrypt/MySqlStrictEncryptSource.java
+++ airbyte-integrations/connectors/source-mysql-strict-encrypt/src/main/java/io/airbyte/integrations/source/mysql_strict_encrypt/MySqlStrictEncryptSource.java
@@ -24,6 +24,10 @@ import org.slf4j.LoggerFactory;
 public class MySqlStrictEncryptSource extends SpecModifyingSource implements Source {
 
   private static final Logger LOGGER = LoggerFactory.getLogger(MySqlStrictEncryptSource.class);
+  private static final String SSL_MODE_DESCRIPTION = "SSL connection modes. " +
+          "<li><b>required</b> - Always connect with SSL. If the MySQL server doesn’t support SSL, the connection will not be established. Certificate Authority (CA) and Hostname are not verified.</li>" +
+          "<li><b>verify-ca</b> - Always connect with SSL. Verifies CA, but allows connection even if Hostname does not match.</li>" +
+          "<li><b>Verify Identity</b> - Always connect with SSL. Verify both CA and Hostname.</li></ul>Read more <a href=\"https://dev.mysql.com/doc/connector-j/8.0/en/connector-j-reference-using-ssl.html\"> in the docs</a>.";
 
   MySqlStrictEncryptSource() {
     super(MySqlSource.sshWrappedSource());
@@ -36,8 +40,14 @@ public class MySqlStrictEncryptSource extends SpecModifyingSource implements Sou
     // that can be used in the Airbyte cloud. User should not be able to change this property.
     ((ObjectNode) spec.getConnectionSpecification().get("properties")).remove(JdbcUtils.SSL_KEY);
     final ArrayNode modifiedSslModes = spec.getConnectionSpecification().get("properties").get("ssl_mode").get("oneOf").deepCopy();
-    // Assume that the first items is the "disable" and "preferred" options; remove these options
-    modifiedSslModes.remove(1);
+    // update description for ssl_mode property
+    ((ObjectNode) spec.getConnectionSpecification().get("properties").get("ssl_mode")).remove("description");
+    ((ObjectNode) spec.getConnectionSpecification().get("properties").get("ssl_mode")).remove("type");
+    ((ObjectNode) spec.getConnectionSpecification().get("properties").get("ssl_mode")).remove("order");
+    ((ObjectNode) spec.getConnectionSpecification().get("properties").get("ssl_mode")).put("description", SSL_MODE_DESCRIPTION);
+    ((ObjectNode) spec.getConnectionSpecification().get("properties").get("ssl_mode")).put("type", "object");
+    ((ObjectNode) spec.getConnectionSpecification().get("properties").get("ssl_mode")).put("order", 7);
+    // Assume that the first items is the "preferred" option; remove this option
     modifiedSslModes.remove(0);
     ((ObjectNode) spec.getConnectionSpecification().get("properties").get("ssl_mode")).remove("oneOf");
     ((ObjectNode) spec.getConnectionSpecification().get("properties").get("ssl_mode")).put("oneOf", modifiedSslModes);
