b3a81be23f367defd2a1c6b3e079012fe0465588
kananindzya <gonindzya@gmail.com>
2022-08-12 16:31:19 +0600

    mark test as disabled

--- airbyte-integrations/connectors/destination-r2/src/test-integration/java/io/airbyte/integrations/destination/r2/R2ParquetDestinationAcceptanceTest.java
+++ airbyte-integrations/connectors/destination-r2/src/test-integration/java/io/airbyte/integrations/destination/r2/R2ParquetDestinationAcceptanceTest.java
@@ -2,7 +2,12 @@ package io.airbyte.integrations.destination.r2;
 
 import io.airbyte.integrations.destination.s3.S3BaseParquetDestinationAcceptanceTest;
 import io.airbyte.integrations.destination.s3.StorageProvider;
+import org.junit.jupiter.api.Disabled;
 
+/**
+ * s3a client not supported by cloudflare R2
+ */
+@Disabled
 public class R2ParquetDestinationAcceptanceTest extends S3BaseParquetDestinationAcceptanceTest {
 
   @Override
