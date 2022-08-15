243153fa5a9f513c5144ff832e497f1f824e0692
Denys Davydov <davydov.den18@gmail.com>
2022-08-15 12:52:09 +0300

    #402 source tiktok: more unit test fixes

--- airbyte-integrations/connectors/source-tiktok-marketing/unit_tests/streams_test.py
+++ airbyte-integrations/connectors/source-tiktok-marketing/unit_tests/streams_test.py
@@ -126,8 +126,8 @@ def test_stream_slices_report(advertiser_ids, granularity, slices_expected, pend
 @pytest.mark.parametrize(
     "stream, metrics_number",
     [
-        (AdsReports, 54),
-        (AdGroupsReports, 51),
+        (AdsReports, 53),
+        (AdGroupsReports, 50),
         (AdvertisersReports, 29),
         (CampaignsReports, 28),
         (AdvertisersAudienceReports, 6),
@@ -142,8 +142,8 @@ def test_basic_reports_get_metrics_day(stream, metrics_number):
 @pytest.mark.parametrize(
     "stream, metrics_number",
     [
-        (AdsReports, 54),
-        (AdGroupsReports, 51),
+        (AdsReports, 53),
+        (AdGroupsReports, 50),
         (AdvertisersReports, 27),
         (CampaignsReports, 28),
         (AdvertisersAudienceReports, 6),
