7307790b6cb5c58cdaf3c51780e6ec4479258a49
Edmundo Ruiz Ghanem <edmundo@airbyte.io>
2022-08-15 16:55:13 -0400

    Split types between Text and Heading

--- airbyte-webapp/src/components/base/Text/Text.tsx
+++ airbyte-webapp/src/components/base/Text/Text.tsx
@@ -1,31 +1,40 @@
 import classNames from "classnames";
-import React, { useMemo } from "react";
+import React from "react";
 
 import headingStyles from "./heading.module.scss";
 import textStyles from "./text.module.scss";
 
 type TextSize = "sm" | "md" | "lg" | "xl";
-type TextElementType = "h1" | "h2" | "h3" | "h4" | "h5" | "h6" | "p" | "span" | "div";
+type HeadingSize = TextSize | "xl";
+type TextElementType = "p" | "span" | "div";
+type HeadingElementType = "h1" | "h2" | "h3" | "h4" | "h5" | "h6";
 
-interface TextProps {
-  as?: TextElementType;
-  size?: TextSize;
+interface BaseProps {
   className?: string;
-  bold?: boolean;
   centered?: boolean;
+}
+
+interface TextProps extends BaseProps {
+  as?: HeadingElementType;
+  size?: TextSize;
+  bold?: boolean;
+}
+
+interface HeadingProps extends BaseProps {
+  as?: TextElementType;
+  size?: HeadingSize;
 }
 
 const getTextClassNames = ({ size, centered, bold }: Required<Pick<TextProps, "size" | "centered" | "bold">>) => {
   const sizes: Partial<Record<TextSize, string>> = {
     sm: textStyles.sm,
     lg: textStyles.lg,
-    xl: textStyles.xl,
   };
 
   return classNames(textStyles.text, sizes[size], centered && textStyles.centered, bold && textStyles.bold);
 };
 
-const getHeadingClassNames = ({ size, centered }: Required<Pick<TextProps, "size" | "centered">>) => {
+const getHeadingClassNames = ({ size, centered }: Required<Pick<HeadingProps, "size" | "centered">>) => {
   const sizes: Partial<Record<TextSize, string>> = {
     sm: headingStyles.sm,
     lg: headingStyles.lg,
@@ -35,24 +44,23 @@ const getHeadingClassNames = ({ size, centered }: Required<Pick<TextProps, "size
   return classNames(headingStyles.heading, sizes[size], centered && headingStyles.centered);
 };
 
-export const Text: React.FC<TextProps> = ({
-  as = "p",
-  size = "md",
-  bold = false,
-  centered = false,
-  className: customClassName,
-  children,
-}) => {
-  const className = useMemo(() => {
-    const isHeading = /^h[0-6]$/.test(as);
-    return classNames(
-      isHeading ? getHeadingClassNames({ size, centered }) : getTextClassNames({ size, centered, bold }),
-      customClassName
-    );
-  }, [as, bold, centered, customClassName, size]);
+const isHeadingType = (props: TextProps | HeadingProps): props is HeadingProps => {
+  return props.as ? /^h[0-6]$/.test(props.as) : false;
+};
+
+export const Text: React.FC<TextProps | HeadingProps> = React.memo((props) => {
+  const isHeading = isHeadingType(props);
+  const { as = isHeading ? "h1" : "p", size = "md", centered = false, children } = props;
+
+  const className = classNames(
+    isHeadingType(props)
+      ? getHeadingClassNames({ size, centered })
+      : getTextClassNames({ size, centered, bold: props.bold ?? false }),
+    props.className
+  );
 
   return React.createElement(as, {
     className,
     children,
   });
-};
+});
