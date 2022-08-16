02cd7ed885428319faf7ba014baad7b5a2271be2
Mahdi Dibaiee <mdibaiee@pm.me>
2022-08-16 15:01:07 +0100

    local-build.sh

--- local-build-push.sh
+++
@@ -1,1 +0,0 @@
-DOCKER_BUILD_PLATFORM=linux/amd64 ALPINE_IMAGE=amd64/alpine:3.14 DOCKER_BUILD_ARCH=amd64 ./gradlew :airbyte-integrations:connectors:$1:build && docker tag docker.io/airbyte/$1:dev ghcr.io/estuary/airbyte-$1:dev && docker push ghcr.io/estuary/airbyte-$1:dev
---
+++ local-build.sh
@@ -0,0 +1,1 @@
+DOCKER_BUILD_PLATFORM=linux/amd64 ALPINE_IMAGE=amd64/alpine:3.14 DOCKER_BUILD_ARCH=amd64 ./gradlew :airbyte-integrations:connectors:$1:build && docker tag docker.io/airbyte/$1:dev ghcr.io/estuary/$1:local
