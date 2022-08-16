46fc263b698aa64d3600764c109a67285bcac85a
kananindzya <gonindzya@gmail.com>
2022-08-16 18:07:48 +0600

    added tests for S3DEstinationConfig

--- airbyte-integrations/bases/base-java-s3/src/test/java/io/airbyte/integrations/destination/s3/S3DestinationConfigTest.java
+++ airbyte-integrations/bases/base-java-s3/src/test/java/io/airbyte/integrations/destination/s3/S3DestinationConfigTest.java
@@ -4,9 +4,13 @@
 
 package io.airbyte.integrations.destination.s3;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
+import com.amazonaws.auth.AWSCredentials;
+import com.fasterxml.jackson.databind.JsonNode;
+import io.airbyte.commons.json.Jsons;
 import io.airbyte.integrations.destination.s3.credential.S3AccessKeyCredentialConfig;
 import org.junit.jupiter.api.Test;
 
@@ -50,4 +54,49 @@ class S3DestinationConfigTest {
     assertEquals(newSecret, credentialConfig.getSecretAccessKey());
   }
 
+  @Test
+  public void testGetS3DestinationConfigAWS_S3Provider() {
+    final JsonNode s3config = Jsons.deserialize("{\n"
+        + "  \"s3_bucket_name\": \"paste-bucket-name-here\",\n"
+        + "  \"s3_bucket_path\": \"integration-test\",\n"
+        + "  \"s3_bucket_region\": \"paste-bucket-region-here\",\n"
+        + "  \"access_key_id\": \"paste-access-key-id-here\",\n"
+        + "  \"secret_access_key\": \"paste-secret-access-key-here\"\n"
+        + "}");
+
+    final S3DestinationConfig result = S3DestinationConfig.getS3DestinationConfig(s3config, StorageProvider.AWS_S3);
+
+    assertThat(result.getEndpoint()).isEmpty();
+    assertThat(result.getBucketName()).isEqualTo("paste-bucket-name-here");
+    assertThat(result.getBucketPath()).isEqualTo("integration-test");
+    assertThat(result.getBucketRegion()).isEqualTo("paste-bucket-region-here");
+    assertThat(result.getPathFormat()).isEqualTo("${NAMESPACE}/${STREAM_NAME}/${YEAR}_${MONTH}_${DAY}_${EPOCH}_");
+    final AWSCredentials awsCredentials = result.getS3CredentialConfig().getS3CredentialsProvider().getCredentials();
+    assertThat(awsCredentials.getAWSAccessKeyId()).isEqualTo("paste-access-key-id-here");
+    assertThat(awsCredentials.getAWSSecretKey()).isEqualTo("paste-secret-access-key-here");
+    assertThat(result.isCheckIntegrity()).isEqualTo(true);
+  }
+
+  @Test
+  public void testGetS3DestinationConfigCF_R2Provider() {
+    final JsonNode s3config = Jsons.deserialize("{\n"
+        + "  \"s3_bucket_name\": \"paste-bucket-name-here\",\n"
+        + "  \"s3_bucket_path\": \"integration-test\",\n"
+        + "  \"account_id\": \"paster-account-id-here\",\n"
+        + "  \"access_key_id\": \"paste-access-key-id-here\",\n"
+        + "  \"secret_access_key\": \"paste-secret-access-key-here\"\n"
+        + "}\n");
+
+    final S3DestinationConfig result = S3DestinationConfig.getS3DestinationConfig(s3config, StorageProvider.CF_R2);
+
+    assertThat(result.getEndpoint()).isEqualTo("https://paster-account-id-here.r2.cloudflarestorage.com");
+    assertThat(result.getBucketName()).isEqualTo("paste-bucket-name-here");
+    assertThat(result.getBucketPath()).isEqualTo("integration-test");
+    assertThat(result.getBucketRegion()).isNull();
+    assertThat(result.getPathFormat()).isEqualTo("${NAMESPACE}/${STREAM_NAME}/${YEAR}_${MONTH}_${DAY}_${EPOCH}_");
+    final AWSCredentials awsCredentials = result.getS3CredentialConfig().getS3CredentialsProvider().getCredentials();
+    assertThat(awsCredentials.getAWSAccessKeyId()).isEqualTo("paste-access-key-id-here");
+    assertThat(awsCredentials.getAWSSecretKey()).isEqualTo("paste-secret-access-key-here");
+    assertThat(result.isCheckIntegrity()).isEqualTo(false);
+  }
 }
