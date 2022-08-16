77119de72d119240cfee9258a8133137a3c9ff82
oneshcheret <sashkaliife@gmail.com>
2022-08-16 17:50:08 +0300

    Snowflake source: handle case with empty role fields

--- airbyte-oauth/src/main/java/io/airbyte/oauth/flows/SourceSnowflakeOAuthFlow.java
+++ airbyte-oauth/src/main/java/io/airbyte/oauth/flows/SourceSnowflakeOAuthFlow.java
@@ -45,16 +45,25 @@ public class SourceSnowflakeOAuthFlow extends BaseOAuth2Flow {
                                     JsonNode inputOAuthConfiguration)
       throws IOException {
     try {
-      return new URIBuilder(String.format(AUTHORIZE_URL, extractUrl(inputOAuthConfiguration)))
+      String consentUrl = new URIBuilder(String.format(AUTHORIZE_URL, extractUrl(inputOAuthConfiguration)))
           .addParameter("client_id", clientId)
           .addParameter("redirect_uri", redirectUrl)
           .addParameter("response_type", "code")
           .addParameter("state", getState())
-          .addParameter("scope", "session:role:" + extractRole(inputOAuthConfiguration))
           .build().toString();
+      String providedRole = extractRole(inputOAuthConfiguration);
+      return providedRole.isEmpty()
+          ? consentUrl
+          : getConsentUrlWithScopeRole(consentUrl, providedRole);
     } catch (final URISyntaxException e) {
       throw new IOException("Failed to format Consent URL for OAuth flow", e);
     }
+  }
+
+  private static String getConsentUrlWithScopeRole(String consentUrl, String providedRole) throws URISyntaxException {
+    return new URIBuilder(consentUrl)
+        .addParameter("scope", "session:role:" + providedRole)
+        .build().toString();
   }
 
   @Override
