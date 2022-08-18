fedb644288852feb698874c7c87047ee55879fdf
Brian Lai <51336873+brianjlai@users.noreply.github.com>
2022-08-18 15:29:26 -0400

    [low code connectors] perform schema validation of the input config against the declarative language schema (#15543)
    * draft: first pass at complete schema language generation and factory validator
    
    * actually a working validator and fixes to the schema that went uncaught
    
    * remove extra spike file
    
    * fix formatting file
    
    * pr feedback and a little bit of refactoring
    
    * fix some types that were erroneously marked as invalid schema
    
    * some comments
    
    * add jsonschemamixin to interfaces
    
    * update changelog
    
    * bump version

--- airbyte-cdk/python/setup.py
+++ airbyte-cdk/python/setup.py
@@ -15,7 +15,7 @@ README = (HERE / "README.md").read_text()
 
 setup(
     name="airbyte-cdk",
-    version="0.1.76",
+    version="0.1.77",
     description="A framework for writing Airbyte Connectors.",
     long_description=README,
     long_description_content_type="text/markdown",
