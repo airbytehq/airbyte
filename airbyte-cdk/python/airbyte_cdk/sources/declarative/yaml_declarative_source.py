ca80d3782a93c2cac13e33484b3537fe4b1058e1
Brian Lai <51336873+brianjlai@users.noreply.github.com>
2022-08-18 15:29:26 -0400

    [low code connectors] perform schema validation of the input config against the declarative language schema (#15543)
    * draft: first pass at complete schema language generation and factory validator
    
    * actually a working validator and fixes to the schema that went uncaught
    
    * remove extra spike file
    
    * fix formatting file
    
    * pr feedback and a little bit of refactoring
    
    * fix some types that were erroneously marked as invalid schema
    
    * some comments
    
    * add jsonschemamixin to interfaces
    
    * update changelog
    
    * bump version

--- airbyte-cdk/python/airbyte_cdk/sources/declarative/yaml_declarative_source.py
+++ airbyte-cdk/python/airbyte_cdk/sources/declarative/yaml_declarative_source.py
@@ -4,14 +4,26 @@
 
 import json
 import logging
+from dataclasses import dataclass
 from typing import Any, List, Mapping
 
+from airbyte_cdk.sources.declarative.checks import CheckStream
 from airbyte_cdk.sources.declarative.checks.connection_checker import ConnectionChecker
 from airbyte_cdk.sources.declarative.declarative_source import DeclarativeSource
+from airbyte_cdk.sources.declarative.declarative_stream import DeclarativeStream
 from airbyte_cdk.sources.declarative.exceptions import InvalidConnectorDefinitionException
 from airbyte_cdk.sources.declarative.parsers.factory import DeclarativeComponentFactory
 from airbyte_cdk.sources.declarative.parsers.yaml_parser import YamlParser
-from airbyte_cdk.sources.streams import Stream
+from airbyte_cdk.sources.streams.core import Stream
+from dataclasses_jsonschema import JsonSchemaMixin
+from jsonschema.validators import validate
+
+
+@dataclass
+class ConcreteDeclarativeSource(JsonSchemaMixin):
+    version: str
+    checker: CheckStream
+    streams: List[DeclarativeStream]
 
 
 class YamlDeclarativeSource(DeclarativeSource):
@@ -27,6 +39,8 @@ class YamlDeclarativeSource(DeclarativeSource):
         self._factory = DeclarativeComponentFactory()
         self._path_to_yaml = path_to_yaml
         self._source_config = self._read_and_parse_yaml_file(path_to_yaml)
+
+        self._validate_source()
 
         # Stopgap to protect the top-level namespace until it's validated through the schema
         unknown_fields = [key for key in self._source_config.keys() if key not in self.VALID_TOP_LEVEL_FIELDS]
@@ -45,14 +59,28 @@ class YamlDeclarativeSource(DeclarativeSource):
             "parsed YAML into declarative source",
             extra={"path_to_yaml_file": self._path_to_yaml, "source_name": self.name, "parsed_config": json.dumps(self._source_config)},
         )
-
-        stream_configs = self._source_config["streams"]
-        for s in stream_configs:
-            if "class_name" not in s:
-                s["class_name"] = "airbyte_cdk.sources.declarative.declarative_stream.DeclarativeStream"
-        return [self._factory.create_component(stream_config, config)() for stream_config in self._source_config["streams"]]
+        return [self._factory.create_component(stream_config, config, True)() for stream_config in self._stream_configs()]
 
     def _read_and_parse_yaml_file(self, path_to_yaml_file):
         with open(path_to_yaml_file, "r") as f:
             config_content = f.read()
             return YamlParser().parse(config_content)
+
+    def _validate_source(self):
+        full_config = {}
+        if "version" in self._source_config:
+            full_config["version"] = self._source_config["version"]
+        if "check" in self._source_config:
+            full_config["checker"] = self._source_config["check"]
+        streams = [self._factory.create_component(stream_config, {}, False)() for stream_config in self._stream_configs()]
+        if len(streams) > 0:
+            full_config["streams"] = streams
+        declarative_source_schema = ConcreteDeclarativeSource.json_schema()
+        validate(full_config, declarative_source_schema)
+
+    def _stream_configs(self):
+        stream_configs = self._source_config.get("streams", [])
+        for s in stream_configs:
+            if "class_name" not in s:
+                s["class_name"] = "airbyte_cdk.sources.declarative.declarative_stream.DeclarativeStream"
+        return stream_configs
