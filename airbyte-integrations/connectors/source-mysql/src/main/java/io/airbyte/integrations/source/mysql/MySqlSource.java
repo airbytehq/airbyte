213fc7eeb1e1483fa64cdc03fb512e10c8fa8230
mkhokh-33 <mariia.khokh@globallogic.com>
2022-08-18 19:48:22 +0300

    Mkhokh/8858 cursor fields for incremental update (#14356)
    * add logic to skip incremental mode for tables which do not have a field types that could not be used as cursor
    
    * added tests for cursor fields incremental updates
    
    * remove not used methods
    
    * fix tests
    
    * fix formatting
    
    * revert RedshiftInsertDestinationAcceptanceTest
    
    * fix ssl clickhouse test
    
    * fix codereview comments
    
    * fix clickhouse strict encrypt test
    
    * fix postgres source after merge from master
    
    * optimize ssl clickhouse test logic
    
    * fix db2 test
    
    * added check for cursor field should be not nullable, fix formatting
    
    * fixed tests, formatting, issue after merge from master
    
    * fix merge issues
    
    * fix test for source-jdbc and remove some code used for local testing
    
    * added description for cockroach db tests
    
    * test changes
    
    * refactoring for cursor field tests after code review comments
    
    * refactoring for cursor field tests after code review comments
    
    * fix code review comments
    
    * remove unused imports
    
    * fix formatting
    
    * fix compilation error
    
    * fix postgres and postgress ssl test
    
    * Fixed bucket naming for S3
    
    * remove CHAR, NCHAR from possible cursor types
    
    * remove SYMBOL from possible cursor types
    
    * removed redundant configs
    
    * fixed mysql-strict-encrypt tests
    
    * fixed mongodb-v2 tests
    
    * minor format changes
    
    * bump version
    
    * auto-bump connector version [ci skip]
    
    * auto-bump connector version [ci skip]
    
    * auto-bump connector version [ci skip]
    
    * auto-bump connector version [ci skip]
    
    * auto-bump connector version [ci skip]
    
    * auto-bump connector version [ci skip]
    
    * auto-bump connector version [ci skip]
    
    * auto-bump connector version [ci skip]
    
    * auto-bump connector version [ci skip]
    
    * auto-bump connector version [ci skip]
    
    Co-authored-by: vmaltsev <ihbectop@gmail.com>
    Co-authored-by: subodh <subodh1810@gmail.com>
    Co-authored-by: Octavia Squidington III <octavia-squidington-iii@users.noreply.github.com>

--- airbyte-integrations/connectors/source-mysql/src/main/java/io/airbyte/integrations/source/mysql/MySqlSource.java
+++ airbyte-integrations/connectors/source-mysql/src/main/java/io/airbyte/integrations/source/mysql/MySqlSource.java
@@ -12,6 +12,7 @@ import static java.util.stream.Collectors.toList;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.Lists;
 import com.mysql.cj.MysqlType;
 import io.airbyte.commons.functional.CheckedConsumer;
 import io.airbyte.commons.json.Jsons;
@@ -70,6 +71,10 @@ public class MySqlSource extends AbstractJdbcSource<MysqlType> implements Source
     super(DRIVER_CLASS, AdaptiveStreamingQueryConfig::new, new MySqlSourceOperations());
   }
 
+  private static AirbyteStream overrideSyncModes(final AirbyteStream stream) {
+    return stream.withSupportedSyncModes(Lists.newArrayList(SyncMode.FULL_REFRESH, SyncMode.INCREMENTAL));
+  }
+
   private static AirbyteStream removeIncrementalWithoutPk(final AirbyteStream stream) {
     if (stream.getSourceDefinedPrimaryKey().isEmpty()) {
       stream.getSupportedSyncModes().remove(SyncMode.INCREMENTAL);
@@ -117,6 +122,7 @@ public class MySqlSource extends AbstractJdbcSource<MysqlType> implements Source
 
     if (isCdc(config)) {
       final List<AirbyteStream> streams = catalog.getStreams().stream()
+          .map(MySqlSource::overrideSyncModes)
           .map(MySqlSource::removeIncrementalWithoutPk)
           .map(MySqlSource::setIncrementalToSourceDefined)
           .map(MySqlSource::addCdcMetadataColumns)
