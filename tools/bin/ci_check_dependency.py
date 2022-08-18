9d81560f6109e6a6d355344a546703af619cfafa
Yevhen Sukhomud <suhomud@gmail.com>
2022-08-18 22:08:17 +0700

    11815 test

--- tools/bin/ci_check_dependency.py
+++ tools/bin/ci_check_dependency.py
@@ -1,45 +1,48 @@
 import sys
 import os
 import os.path
-import logging
 
-LOGGING_FORMAT = "%(asctime)-15s %(levelname)s %(message)s"
 CONNECTOR_FOLDER = "airbyte-integrations/connectors/"
 CONNECTOR_PATH = "./airbyte-integrations/connectors/"
+WHITE_LIST = ["/src/main/", "build.gradle"]
 
 
 def main():
-    # home_directory = os.getenv("HOME")
-    # logging.info(f"HOME directory: {home_directory}")
     git_diff_file_path = ' '.join(sys.argv[1:])
-    # logging.info(f"git diff files path {git_diff_file_path}")
 
+    # Get changed connectors files
+    connectors_changed_files = get_connectors_changed_files(git_diff_file_path)
     # Get changed connectors
-    changed_connectors_files = get_changed_connectors(git_diff_file_path)
-    # logging.info(f"Changed connectors  {changed_connectors_files}")
+    changed_connectors = get_changed_connectors(connectors_changed_files)
 
-    # Get all connectors
+    # Get all existing connectors
     all_connectors = get_all_connectors()
-    # logging.info(f"Found {len(all_connectors)} connectors to check")
 
-    # getting all build.gradle file
+    # Getting all build.gradle file
     all_build_gradle_files = get_connectors_gradle_files(all_connectors)
-    # logging.info(f"Found {len(all_build_gradle_files)} build.gradle files")
 
-    # Checking build.gradle filse
-    depended_connectors = list(set(get_depended_connectors(changed_connectors_files, all_build_gradle_files)))
-    # logging.info(f"Make sure to run corresponding integration tests: {depended_connectors}")
+    # Try to find dependency in build.gradle file
+    depended_connectors = list(set(get_depended_connectors(changed_connectors, all_build_gradle_files)))
 
     write_report(depended_connectors)
 
 
-def get_changed_connectors(path):
+def get_connectors_changed_files(path):
     changed_connectors_files = []
     with open(path) as file:
         for line in file:
             if CONNECTOR_FOLDER in line:
-                changed_connectors_files.append(line.split("/")[2])
+                changed_connectors_files.append(line)
     return list(set(changed_connectors_files))
+
+
+def get_changed_connectors(changed_files):
+    changed_connectors = []
+    for changed_file in changed_files:
+        # Check if this file should be ignored
+        if any(ignor in changed_file for ignor in WHITE_LIST):
+            changed_connectors.append(changed_file.split("/")[2])
+    return list(set(changed_connectors))
 
 
 def get_all_connectors():
@@ -51,25 +54,23 @@ def get_connectors_gradle_files(all_connectors):
     all_build_gradle_files = {}
     for connector in all_connectors:
         build_gradle_path = CONNECTOR_PATH + connector + "/"
-        build_gradle_file = findFile("build.gradle", build_gradle_path)
+        build_gradle_file = find_file("build.gradle", build_gradle_path)
         all_build_gradle_files[connector] = build_gradle_file
     return all_build_gradle_files
 
 
-def findFile(name, path):
+def find_file(name, path):
     for root, dirs, files in os.walk(path):
         if name in files:
             return os.path.join(root, name)
 
 
-def get_depended_connectors(changed_connectors_files, all_build_gradle_files):
+def get_depended_connectors(changed_connectors, all_build_gradle_files):
     depended_connectors = []
-    for changed_connector in changed_connectors_files:
-        # logging.info(f"Trying to find dependency for: {changed_connector}")
+    for changed_connector in changed_connectors:
         for connector, gradle_file in all_build_gradle_files.items():
             with open(gradle_file) as f:
                 if changed_connector in f.read():
-                    # logging.info(f"Dependency found in: {connector}")
                     depended_connectors.append(connector)
     return depended_connectors
 
@@ -80,5 +81,4 @@ def write_report(depended_connectors):
 
 
 if __name__ == "__main__":
-    logging.basicConfig(format=LOGGING_FORMAT, level=logging.INFO)
     main()
