0151b1d4a9aaf0d53a7e30d968bc6030654d4c5f
Edmundo Ruiz Ghanem <edmundo@airbyte.io>
2022-08-19 09:37:31 -0400

    Update InfoTooltip to SCSS, ensure that it's centered aligned

--- airbyte-webapp/src/components/base/Tooltip/InfoTooltip.tsx
+++ airbyte-webapp/src/components/base/Tooltip/InfoTooltip.tsx
@@ -1,24 +1,19 @@
 import React from "react";
-import styled from "styled-components";
 
 import { InfoIcon } from "components/icons/InfoIcon";
 
+import styles from "./InfoTooltip.module.scss";
 import { Tooltip } from "./Tooltip";
-
-const Info = styled.div`
-  display: inline-block;
-  vertical-align: text-top;
-  margin-left: 5px;
-  color: ${({ theme }) => theme.lightTextColor};
-`;
 
 export const InfoTooltip: React.FC = ({ children }) => {
   return (
     <Tooltip
       control={
-        <Info>
-          <InfoIcon />
-        </Info>
+        <div className={styles.container}>
+          <div className={styles.icon}>
+            <InfoIcon />
+          </div>
+        </div>
       }
     >
       {children}
