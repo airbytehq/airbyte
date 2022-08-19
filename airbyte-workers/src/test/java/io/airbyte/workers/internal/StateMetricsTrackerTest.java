718a0a35202f0d4dd00e164e72016cbb08f0ed4f
alovew <anne@airbyte.io>
2022-08-19 13:40:09 -0700

    more complex per stream test

--- airbyte-workers/src/test/java/io/airbyte/workers/internal/StateMetricsTrackerTest.java
+++ airbyte-workers/src/test/java/io/airbyte/workers/internal/StateMetricsTrackerTest.java
@@ -18,6 +18,7 @@ public class StateMetricsTrackerTest {
 
   private StateMetricsTracker stateMetricsTracker;
   private static final String STREAM_1 = "stream1";
+  private static final String STREAM_2 = "stream2";
 
   @BeforeEach
   void setup() {
@@ -35,26 +36,35 @@ public class StateMetricsTrackerTest {
 
   @Test
   void testStreamMaxandMeanSecondsBetweenStateMessageEmittedandCommitted() throws StateMetricsTrackerException {
-    final AirbyteStateMessage s1 = AirbyteMessageUtils.createStreamStateMessage(STREAM_1, 1);
-    final AirbyteStateMessage s2 = AirbyteMessageUtils.createStreamStateMessage(STREAM_1, 2);
-    final AirbyteStateMessage s3 = AirbyteMessageUtils.createStreamStateMessage(STREAM_1, 3);
+    final AirbyteStateMessage s1s1 = AirbyteMessageUtils.createStreamStateMessage(STREAM_1, 1);
+    final AirbyteStateMessage s1s2 = AirbyteMessageUtils.createStreamStateMessage(STREAM_1, 2);
+    final AirbyteStateMessage s1s3 = AirbyteMessageUtils.createStreamStateMessage(STREAM_1, 3);
+    final AirbyteStateMessage s2s1 = AirbyteMessageUtils.createStreamStateMessage(STREAM_2, 1);
+    final AirbyteStateMessage s2s2 = AirbyteMessageUtils.createStreamStateMessage(STREAM_2, 2);
 
-    // 3 per-stream state messages emitted for the same stream
     final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-    stateMetricsTracker.addState(s1, 0, LocalDateTime.parse("2022-01-01 12:00:00", formatter));
-    stateMetricsTracker.addState(s2, 1, LocalDateTime.parse("2022-01-01 12:00:01", formatter));
-    stateMetricsTracker.addState(s3, 2, LocalDateTime.parse("2022-01-01 12:00:02", formatter));
+    stateMetricsTracker.addState(s1s1, 0, LocalDateTime.parse("2022-01-01 12:00:00", formatter)); // stream 1 state
+    stateMetricsTracker.addState(s1s2, 1, LocalDateTime.parse("2022-01-01 12:00:01", formatter)); // stream 1 state
+    stateMetricsTracker.addState(s2s1, 0, LocalDateTime.parse("2022-01-01 12:00:02", formatter)); // stream 2 state
+    stateMetricsTracker.addState(s1s3, 2, LocalDateTime.parse("2022-01-01 12:00:03", formatter)); // stream 1 state
 
-    // Committed up to 2nd state message - time to commit is 5 seconds (second 00 to second 05)
+    // Committed up to 2nd state message in stream 1 - time to commit is 5 seconds (second 00 to second 05)
     stateMetricsTracker.incrementTotalDestinationEmittedStateMessages();
-    stateMetricsTracker.updateStates(s2, 1, LocalDateTime.parse("2022-01-01 12:00:05", formatter));
+    stateMetricsTracker.updateStates(s1s2, 1, LocalDateTime.parse("2022-01-01 12:00:05", formatter));
 
-    // Committed final state message - time to commit is 7 seconds (second 02 to second 09)
+    // Committed final state message for stream 1 - time to commit is 7 seconds (second 03 to second 10)
     stateMetricsTracker.incrementTotalDestinationEmittedStateMessages();
-    stateMetricsTracker.updateStates(s3, 2, LocalDateTime.parse("2022-01-01 12:00:09", formatter));
+    stateMetricsTracker.updateStates(s1s3, 2, LocalDateTime.parse("2022-01-01 12:00:10", formatter));
 
-    assertEquals(7L, stateMetricsTracker.getMaxSecondsBetweenStateMessageEmittedAndCommitted());
-    assertEquals(6L, stateMetricsTracker.getMeanSecondsBetweenStateMessageEmittedAndCommitted());
+    stateMetricsTracker.addState(s2s2, 2, LocalDateTime.parse("2022-01-01 12:00:11", formatter));
+
+    // Commit final state message for stream 2 - time to commit is 12 seconds (second 14 - second 02)
+    stateMetricsTracker.incrementTotalDestinationEmittedStateMessages();
+    stateMetricsTracker.updateStates(s2s2, 2, LocalDateTime.parse("2022-01-01 12:00:14", formatter));
+
+    // max time across both streams was 12, mean time across all streams was (5 + 7 + 12)/3 == 24/3 == 8
+    assertEquals(12L, stateMetricsTracker.getMaxSecondsBetweenStateMessageEmittedAndCommitted());
+    assertEquals(8L, stateMetricsTracker.getMeanSecondsBetweenStateMessageEmittedAndCommitted());
   }
 
   @Test
