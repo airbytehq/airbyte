69b5f91e370be864b46dfaad4cf6b50b90a8e619
Edmundo Ruiz Ghanem <edmundo@airbyte.io>
2022-08-19 09:55:38 -0400

    Ensure that Tooltips auto update on scroll

--- airbyte-webapp/src/components/base/Tooltip/Tooltip.tsx
+++ airbyte-webapp/src/components/base/Tooltip/Tooltip.tsx
@@ -1,4 +1,4 @@
-import { flip, offset, shift, useFloating } from "@floating-ui/react-dom";
+import { autoUpdate, flip, offset, shift, useFloating, UseFloatingProps } from "@floating-ui/react-dom";
 import classNames from "classnames";
 import React, { useState, useEffect } from "react";
 import { createPortal } from "react-dom";
@@ -9,6 +9,15 @@ import { TooltipProps } from "./types";
 
 const MOUSE_OUT_TIMEOUT_MS = 50;
 
+const FLOATING_OPTIONS: UseFloatingProps = {
+  middleware: [
+    offset(5), // $spacing-sm
+    flip(),
+    shift(),
+  ],
+  whileElementsMounted: autoUpdate,
+};
+
 export const Tooltip: React.FC<TooltipProps> = (props) => {
   const { children, control, className, disabled, cursor, theme = "dark", placement = "bottom" } = props;
 
@@ -16,12 +25,8 @@ export const Tooltip: React.FC<TooltipProps> = (props) => {
   const [isVisible, setIsVisible] = useState(false);
 
   const { x, y, reference, floating, strategy } = useFloating({
+    ...FLOATING_OPTIONS,
     placement,
-    middleware: [
-      offset(5), // $spacing-sm
-      flip(),
-      shift(),
-    ],
   });
 
   useEffect(() => {
