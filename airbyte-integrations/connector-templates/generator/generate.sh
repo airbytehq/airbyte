b355d9bb52fcfb5c420af6ef7fe292ab99da317c
Michel Tricot <michel@airbyte.io>
2022-08-21 23:29:53 -0700

    works

--- airbyte-integrations/connector-templates/generator/generate.sh
+++ airbyte-integrations/connector-templates/generator/generate.sh
@@ -1,41 +1,27 @@
 #!/usr/bin/env bash
 
-error_handler() {
-  echo "While trying to generate a connector, an error occurred on line $1 of generate.sh and the process aborted early.  This is probably a bug."
+set -eo pipefail
+
+IMAGE_NAME=connector-generator
+CONTAINER_NAME=airbyte-connector-generator-${RANDOM}
+
+_ensure_environment() {
+  # Make sure docker is running before trying
+  docker ps || { echo "docker is not running, please start up the docker daemon"; exit 1; }
+
+  # Ensure script always runs from this directory because that's how docker build contexts work
+  cd "$(dirname "${0}")" || exit 1
 }
-trap 'error_handler $LINENO' ERR
 
-set -e
+main() {
+  _ensure_environment
 
-# Ensure script always runs from this directory because thats how docker build contexts work
-cd "$(dirname "${0}")" || exit 1
+  docker build . -t "${IMAGE_NAME}"
 
-# Make sure docker is running before trying
-if ! docker ps; then
-  echo "docker is not running, this script requires docker to be up"
-  echo "please start up the docker daemon!"
-  exit
-fi
+  echo "Running generator (${CONTAINER_NAME})"
+  docker run --rm -it \
+    -v "$(pwd)/../../../.":/airbyte \
+    --name "$CONTAINER_NAME" "${IMAGE_NAME}"
+}
 
-_UID=$(id -u)
-_GID=$(id -g)
-# Remove container if already exist
-echo "Removing previous generator if it exists..."
-docker container rm -f airbyte-connector-bootstrap >/dev/null 2>&1
-
-# Build image for container from Dockerfile
-# Specify the host system user UID and GID to chown the generated files to host system user.
-# This is done because all generated files in container with mounted folders has root owner
-echo "Building generator docker image..."
-docker build --build-arg UID="$_UID" --build-arg GID="$_GID" . -t airbyte/connector-bootstrap
-
-# Run the container and mount the airbyte folder
-if [ $# -eq 2 ]; then
-  echo "2 arguments supplied: 1=$1 2=$2"
-  docker run --name airbyte-connector-bootstrap --user "$_UID:$_GID" -e HOME=/tmp -e package_desc="$1" -e package_name="$2" -v "$(pwd)/../../../.":/airbyte airbyte/connector-bootstrap
-else
-  echo "Running generator..."
-  docker run --rm -it --name airbyte-connector-bootstrap --user "$_UID:$_GID" -e HOME=/tmp -v "$(pwd)/../../../.":/airbyte airbyte/connector-bootstrap
-fi
-
-echo "Finished running generator"
+main "$@"
