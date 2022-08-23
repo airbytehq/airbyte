d4f10f4f7e6a960a1fd5d8c60ce97a21beb289ba
Yevhen Sukhomud <suhomud@gmail.com>
2022-08-23 16:59:29 +0700

    11815 Updated git diff, Updated python script

--- tools/bin/ci_check_dependency.py
+++ tools/bin/ci_check_dependency.py
@@ -4,16 +4,25 @@ import os.path
 
 CONNECTOR_FOLDER = "airbyte-integrations/connectors/"
 CONNECTOR_PATH = "./airbyte-integrations/connectors/"
-WHITE_LIST = ["/src/main/", "build.gradle"]
+IGNORE_LIST = [
+    # Java
+    "/src/test/","/src/test-integration/", "/src/testFixtures/",
+    # Python
+    "/integration_tests/", "/unit_tests/",
+    # Common
+    "acceptance-test-config.yml", "acceptance-test-docker.sh", ".md", ".dockerignore", ".gitignore", "requirements.txt"]
 
 
 def main():
+    # Used git diff checks airbyte-integrations/ folder only
+    # See .github/workflows/report-connectors-dependency.yml file
     git_diff_file_path = ' '.join(sys.argv[1:])
 
-    # Get changed connectors files
-    connectors_changed_files = get_connectors_changed_files(git_diff_file_path)
-    # Get changed connectors
-    changed_connectors = get_changed_connectors(connectors_changed_files)
+    # Get changed files
+    changed_files = get_changed_files(git_diff_file_path)
+    # Get changed modules. e.g. source-acceptance-test from airbyte-integrations/bases/
+    # or destination-mysql from airbyte-integrations/connectors/
+    changed_modules = get_changed_modules(changed_files)
 
     # Get all existing connectors
     all_connectors = get_all_connectors()
@@ -22,27 +31,26 @@ def main():
     all_build_gradle_files = get_connectors_gradle_files(all_connectors)
 
     # Try to find dependency in build.gradle file
-    depended_connectors = list(set(get_depended_connectors(changed_connectors, all_build_gradle_files)))
-
+    depended_connectors = list(set(get_depended_connectors(changed_modules, all_build_gradle_files)))
     write_report(depended_connectors)
 
 
-def get_connectors_changed_files(path):
+def get_changed_files(path):
     changed_connectors_files = []
     with open(path) as file:
         for line in file:
-            if CONNECTOR_FOLDER in line:
-                changed_connectors_files.append(line)
-    return list(set(changed_connectors_files))
+            changed_connectors_files.append(line)
+    return changed_connectors_files
 
 
-def get_changed_connectors(changed_files):
-    changed_connectors = []
+def get_changed_modules(changed_files):
+    changed_modules = []
     for changed_file in changed_files:
         # Check if this file should be ignored
-        if any(ignor in changed_file for ignor in WHITE_LIST):
-            changed_connectors.append(changed_file.split("/")[2])
-    return list(set(changed_connectors))
+        if not any(ignor in changed_file for ignor in IGNORE_LIST):
+            split_changed_file = changed_file.split("/")
+            changed_modules.append(split_changed_file[1] + ":" + split_changed_file[2])
+    return list(set(changed_modules))
 
 
 def get_all_connectors():
@@ -69,8 +77,8 @@ def get_depended_connectors(changed_connectors, all_build_gradle_files):
     depended_connectors = []
     for changed_connector in changed_connectors:
         for connector, gradle_file in all_build_gradle_files.items():
-            with open(gradle_file) as f:
-                if changed_connector in f.read():
+            with open(gradle_file) as file:
+                if changed_connector in file.read():
                     depended_connectors.append(connector)
     return depended_connectors
 
