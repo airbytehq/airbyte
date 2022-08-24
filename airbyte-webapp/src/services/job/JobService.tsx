5d96907dd21169b7420ac7b676506d71d0465d75
lmossman <lake@airbyte.io>
2022-08-24 16:28:06 -0700

    add pagination to query key

--- airbyte-webapp/src/services/job/JobService.tsx
+++ airbyte-webapp/src/services/job/JobService.tsx
@@ -5,13 +5,13 @@ import { JobsService } from "core/domain/job/JobsService";
 import { useDefaultRequestMiddlewares } from "services/useDefaultRequestMiddlewares";
 import { useInitService } from "services/useInitService";
 
-import { JobDebugInfoRead, JobInfoRead, JobListRequestBody } from "../../core/request/AirbyteClient";
+import { JobDebugInfoRead, JobInfoRead, JobListRequestBody, Pagination } from "../../core/request/AirbyteClient";
 import { useSuspenseQuery } from "../connector/useSuspenseQuery";
 
 export const jobsKeys = {
   all: ["jobs"] as const,
   lists: () => [...jobsKeys.all, "list"] as const,
-  list: (filters: string) => [...jobsKeys.lists(), { filters }] as const,
+  list: (filters: string, pagination?: Pagination) => [...jobsKeys.lists(), { filters, pagination }] as const,
   detail: (jobId: number) => [...jobsKeys.all, "details", jobId] as const,
   getDebugInfo: (jobId: number) => [...jobsKeys.all, "getDebugInfo", jobId] as const,
   cancel: (jobId: string) => [...jobsKeys.all, "cancel", jobId] as const,
@@ -25,7 +25,7 @@ function useGetJobService() {
 
 export const useListJobs = (listParams: JobListRequestBody) => {
   const service = useGetJobService();
-  return useSuspenseQuery(jobsKeys.list(listParams.configId), () => service.list(listParams), {
+  return useSuspenseQuery(jobsKeys.list(listParams.configId, listParams.pagination), () => service.list(listParams), {
     refetchInterval: 2500, // every 2,5 seconds,
   }).jobs;
 };
