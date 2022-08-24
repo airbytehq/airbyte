8b54ef1e71f53226ed6a0df2d8b7924693482216
lmossman <lake@airbyte.io>
2022-08-24 16:22:02 -0700

    add load more jobs button

--- airbyte-webapp/src/pages/ConnectionPage/pages/ConnectionItemPage/components/StatusView.tsx
+++ airbyte-webapp/src/pages/ConnectionPage/pages/ConnectionItemPage/components/StatusView.tsx
@@ -8,8 +8,11 @@ import { Tooltip } from "components/base/Tooltip";
 import EmptyResource from "components/EmptyResourceBlock";
 import { RotateIcon } from "components/icons/RotateIcon";
 
+import { getFrequencyType } from "config/utils";
+import { Action, Namespace } from "core/analytics";
 import { ConnectionStatus, JobWithAttemptsRead, WebBackendConnectionRead } from "core/request/AirbyteClient";
 import Status from "core/statuses";
+import { useAnalyticsService } from "hooks/services/Analytics";
 import { useConfirmationModalService } from "hooks/services/ConfirmationModal";
 import { FeatureItem, useFeature } from "hooks/services/Feature";
 import { useResetConnection, useSyncConnection } from "hooks/services/useConnectionHook";
@@ -17,6 +20,8 @@ import { useCancelJob, useListJobs } from "services/job/JobService";
 
 import JobsList from "./JobsList";
 import styles from "./StatusView.module.scss";
+
+const JOB_PAGE_SIZE_INCREMENT = 25;
 
 enum ActionType {
   RESET = "reset_connection",
@@ -43,10 +48,15 @@ const getJobRunningOrPending = (jobs: JobWithAttemptsRead[]) => {
 
 const StatusView: React.FC<StatusViewProps> = ({ connection }) => {
   const [activeJob, setActiveJob] = useState<ActiveJob>();
+  const [jobPageSize, setJobPageSize] = useState(JOB_PAGE_SIZE_INCREMENT);
+  const analyticsService = useAnalyticsService();
 
   const jobs = useListJobs({
     configId: connection.connectionId,
     configTypes: ["sync", "reset_connection"],
+    pagination: {
+      pageSize: jobPageSize,
+    },
   });
 
   useEffect(() => {
@@ -102,6 +112,21 @@ const StatusView: React.FC<StatusViewProps> = ({ connection }) => {
     return cancelJob(activeJob.id);
   };
 
+  const onLoadMoreJobs = () => {
+    setJobPageSize((prevJobPageSize) => prevJobPageSize + JOB_PAGE_SIZE_INCREMENT);
+
+    analyticsService.track(Namespace.CONNECTION, Action.LOAD_MORE_JOBS, {
+      actionDescription: "Load more jobs button was clicked",
+      connection_id: connection.connectionId,
+      connector_source: connection.source?.sourceName,
+      connector_source_definition_id: connection.source?.sourceDefinitionId,
+      connector_destination: connection.destination?.destinationName,
+      connector_destination_definition_id: connection.destination?.destinationDefinitionId,
+      frequency: getFrequencyType(connection.schedule),
+      job_page_size: jobPageSize,
+    });
+  };
+
   const cancelJobBtn = (
     <Button className={styles.cancelButton} disabled={!activeJob?.id || activeJob.isCanceling} onClick={onCancelJob}>
       <FontAwesomeIcon className={styles.iconXmark} icon={faXmark} />
@@ -145,6 +170,11 @@ const StatusView: React.FC<StatusViewProps> = ({ connection }) => {
       >
         {jobs.length ? <JobsList jobs={jobs} /> : <EmptyResource text={<FormattedMessage id="sources.noSync" />} />}
       </ContentCard>
+      <footer className={styles.footer}>
+        <Button onClick={onLoadMoreJobs} disabled={jobs.length < jobPageSize}>
+          <FormattedMessage id="connection.loadMoreJobs" />
+        </Button>
+      </footer>
     </div>
   );
 };
