706f5bb15485a5720aa4b8bc4ec3eb2feb6aeb2e
pmossman <parker@airbyte.io>
2022-08-25 15:32:16 -0700

    add a RouterService class that is injected into RouteToTaskQueueActivityImpl

--- airbyte-workers/src/main/java/io/airbyte/workers/temporal/sync/RouteToTaskQueueActivityImpl.java
+++ airbyte-workers/src/main/java/io/airbyte/workers/temporal/sync/RouteToTaskQueueActivityImpl.java
@@ -4,27 +4,19 @@
 
 package io.airbyte.workers.temporal.sync;
 
-import io.airbyte.config.Configs;
-import io.airbyte.workers.temporal.TemporalJobType;
 import java.util.UUID;
 
 public class RouteToTaskQueueActivityImpl implements RouteToTaskQueueActivity {
 
-  // To be replaced by proper Routing Service. For now, our MVP supports a single external Data Plane
-  private static final String MVP_DATA_PLANE_TASK_QUEUE = "MVP_DATA_PLANE";
+  private final RouterService routerService;
 
-  private final Configs configs;
-
-  public RouteToTaskQueueActivityImpl(final Configs configs) {
-    this.configs = configs;
+  public RouteToTaskQueueActivityImpl(final RouterService routerService) {
+    this.routerService = routerService;
   }
 
   @Override
   public String routeToTaskQueue(final UUID connectionId) {
-    if (configs.connectionIdsForDataPlane().contains(connectionId.toString())) {
-      return MVP_DATA_PLANE_TASK_QUEUE;
-    }
-    return TemporalJobType.SYNC.name();
+    return routerService.getTaskQueue(connectionId);
   }
 
 }
