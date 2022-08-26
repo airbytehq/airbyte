0c2d7a33c62d18cf616ba8340f4443d652c9214f
Andrii Korotkov <andrii.h.korotkov@globallogic.com>
2022-08-26 14:04:50 +0300

    fixed remarks

--- airbyte-integrations/bases/base-java/src/main/java/io/airbyte/integrations/base/errors/messages/ErrorMessage.java
+++ airbyte-integrations/bases/base-java/src/main/java/io/airbyte/integrations/base/errors/messages/ErrorMessage.java
@@ -23,7 +23,7 @@ public class ErrorMessage {
   private static String configMessage(String stateCode, int errorCode, String message) {
     String stateCodePart = Objects.isNull(stateCode) ? "" : String.format("State code: %s; ", stateCode);
     String errorCodePart = errorCode == 0 ? "" : String.format("Error code: %s; ", errorCode);
-    return String.format("%s %s Message: %s", stateCodePart, errorCodePart, message);
+    return String.format("%s%sMessage: %s", stateCodePart, errorCodePart, message);
   }
 
 }
