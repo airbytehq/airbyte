3d944f48f895740ee94e729ce8eb1136a614fb52
dolphinxyz <robimalco@gmail.com>
2022-08-29 19:17:04 +0200

    working connector

--- airbyte-integrations/connectors/source-graffle/source_graffle/stream_events.py
+++ airbyte-integrations/connectors/source-graffle/source_graffle/stream_events.py
@@ -1,4 +1,4 @@
-import time
+import json
 import requests
 from datetime import datetime
 from typing import Any, Mapping, Iterable, Optional, MutableMapping
@@ -10,6 +10,7 @@ class EventsStream(HttpStream):
     primary_key = "id"
     cursor_field = "eventDate"
     url_base = "https://prod-main-net-dashboard-api.azurewebsites.net"
+    purchase_event =  "A.30cf5dcf6ea8d379.AeraPack.Purchased"
     
     def __init__(self, config: Mapping[str, Any], **_):
         super().__init__()
@@ -21,20 +22,22 @@ class EventsStream(HttpStream):
 
     def request_params(self, stream_state: Mapping[str, Any], **_) -> MutableMapping[str, Any]:
         if stream_state:
-            return {"startDate": self._cursor_value}
-        return {"startDate": self.latest_stream_timestamp}
+            return {"startDate": self._cursor_value, "eventType": self.purchase_event}
+        return {"startDate": self.latest_stream_timestamp, "eventType": self.purchase_event}
 
     def parse_response(self, response: requests.Response, **_) -> Iterable[Mapping]:
-        if response.text:
-            self._cursor_value = response.json()[0]["eventDate"][:26] # [:26] to get rid of +00:00
-        yield from response.json()
+        self._cursor_value = response.json()[0]["eventDate"][:26] # [:26] to get rid of +00:00
+        lower_response = json.loads(json.dumps(response.json()).lower()) # transform json to lowercase
+        yield from lower_response
 
     def next_page_token(self, _: requests.Response) -> Optional[Mapping[str, Any]]:
         return None
 
+    """
     def read_records(self, stream_state: Mapping[str, Any] = None, *args, **kwargs) -> Iterable[Mapping[str, Any]]:
         for record in super().read_records(*args, **kwargs):
             yield  {k.lower(): v for k, v in record.items()} # convert keys to lower case because redshift problem
+    """
 
     @property
     def state(self) -> Mapping[str, Any]:
