910d58efc5bea0e77897198230596abc30f74b86
dolphinxyz <robimalco@gmail.com>
2022-09-01 10:19:28 +0200

    adding a few print to help debugging

--- airbyte-integrations/connectors/source-graffle/source_graffle/stream_events.py
+++ airbyte-integrations/connectors/source-graffle/source_graffle/stream_events.py
@@ -18,16 +18,21 @@ class EventsStream(HttpStream):
         self.latest_stream_timestamp = config["start_datetime"]
 
     def request_headers(self, **_) -> Mapping[str, Any]:
+        print("############request_headers", "NO REQUEST HEADERS")
         return {}
 
     def request_params(self, stream_state: Mapping[str, Any], **_) -> MutableMapping[str, Any]:
         if stream_state:
+            print("############request_params", {"startDate": self._cursor_value, "eventType": self.event_id})
             return {"startDate": self._cursor_value, "eventType": self.event_id}
+        print("############request_params", {"startDate": self._cursor_value, "eventType": self.event_id})
         return {"startDate": self.latest_stream_timestamp, "eventType": self.event_id}
 
     def parse_response(self, response: requests.Response, **_) -> Iterable[Mapping]:
+        print("############parse_response", "PARSING RESPONSE")
         self._cursor_value = response.json()[0]["eventDate"][:26] # [:26] to get rid of +00:00
         lower_response = json.loads(json.dumps(response.json()).lower()) # transform json to lowercase
+        print("############parse_response", self._cursor_value)
         yield from lower_response
 
     def next_page_token(self, _: requests.Response) -> Optional[Mapping[str, Any]]:
