d49c3063e6445ad388a64ff8fc56576dd2410dbb
letiescanciano <45267095+letiescanciano@users.noreply.github.com>
2022-09-05 12:23:20 +0200

    Add email to identify users analytics call (#16327)

--- airbyte-webapp/src/packages/cloud/cloudRoutes.tsx
+++ airbyte-webapp/src/packages/cloud/cloudRoutes.tsx
@@ -152,7 +152,7 @@ export const Routing: React.FC = () => {
     [user]
   );
   useAnalyticsRegisterValues(analyticsContext);
-  useAnalyticsIdentifyUser(user?.userId, { providers });
+  useAnalyticsIdentifyUser(user?.userId, { providers, email: user?.email });
   useTrackPageAnalytics();
 
   if (!inited) {
