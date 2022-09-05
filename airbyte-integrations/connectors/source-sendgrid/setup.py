279c69a35e6ae7b12645c18602e7e117c0d3b632
Serhii Lazebnyi <serhii.lazebnyi@globallogic.com>
2022-09-05 17:28:42 +0200

    Updated setup

--- airbyte-integrations/connectors/source-sendgrid/setup.py
+++ airbyte-integrations/connectors/source-sendgrid/setup.py
@@ -5,12 +5,28 @@
 
 from setuptools import find_packages, setup
 
+MAIN_REQUIREMENTS = [
+    "airbyte-cdk~=0.1",
+    "backoff",
+    "requests",
+]
+
+TEST_REQUIREMENTS = [
+    "pytest~=6.1",
+    "source-acceptance-test",
+    "pytest-mock~=3.6",
+    "requests_mock~=1.8"
+]
+
 setup(
     name="source_sendgrid",
     description="Source implementation for Sendgrid.",
     author="Airbyte",
     author_email="contact@airbyte.io",
     packages=find_packages(),
-    install_requires=["airbyte-cdk~=0.1", "backoff", "requests", "pytest==6.1.2", "pytest-mock"],
-    package_data={"": ["*.json", "schemas/*.json"]},
+    install_requires=MAIN_REQUIREMENTS,
+    package_data={"": ["*.json", "schemas/*.json", "schemas/shared/*.json"]},
+    extras_require={
+        "tests": TEST_REQUIREMENTS,
+    },
 )
