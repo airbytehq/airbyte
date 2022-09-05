a7688c57fb89c6fd5c839e92c234803f8b52f272
Volodymyr Petrov <volodymyr.s.petrov@globallogic.com>
2022-09-05 11:20:19 +0300

    add SlickSlider component

---
+++ airbyte-webapp/src/views/Connector/ServiceForm/components/Slider/SlickSlider.tsx
@@ -0,0 +1,68 @@
+import { faChevronLeft, faChevronRight } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import classnames from "classnames";
+import React, { useMemo } from "react";
+import Slider, { CustomArrowProps, Settings as SliderProps } from "react-slick";
+
+import { Text } from "components/base/Text";
+
+import styles from "./SlickSlider.module.scss";
+
+import "./slider.css";
+
+interface SlickSliderProps {
+  title?: string;
+  sliderSettingsOverride?: SliderProps;
+}
+
+export const SlickSlider: React.FC<SlickSliderProps> = ({ title, sliderSettingsOverride, children }) => {
+  const PrevArrow = ({ slideCount, currentSlide, className, onClick, ...restProps }: CustomArrowProps) => (
+    <div
+      className={classnames(styles.leftArrow, {
+        [styles.arrowDisabled]: onClick === null,
+      })}
+      onClick={onClick}
+      role="button"
+      tabIndex={0}
+      aria-label="prev slide"
+    >
+      <FontAwesomeIcon icon={faChevronLeft} className={classnames(className)} {...restProps} />
+    </div>
+  );
+  const NextArrow = ({ slideCount, currentSlide, className, onClick, ...restProps }: CustomArrowProps) => (
+    <div
+      className={classnames(styles.rightArrow, {
+        [styles.arrowDisabled]: onClick === null,
+      })}
+      onClick={onClick}
+      role="button"
+      tabIndex={0}
+      aria-label="next slide"
+    >
+      <FontAwesomeIcon icon={faChevronRight} className={classnames(className)} {...restProps} />
+    </div>
+  );
+
+  const settings: SliderProps = useMemo(
+    () => ({
+      arrows: true,
+      accessibility: true,
+      infinite: false,
+      swipeToSlide: true,
+      speed: 200,
+      slidesToShow: 2,
+      slidesToScroll: 2,
+      prevArrow: <PrevArrow />,
+      nextArrow: <NextArrow />,
+      ...sliderSettingsOverride,
+    }),
+    [sliderSettingsOverride]
+  );
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.titleContainer}>{title && <Text size="sm">{title}</Text>}</div>
+      <Slider {...settings}>{children}</Slider>
+    </div>
+  );
+};
