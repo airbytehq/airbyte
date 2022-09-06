884d4777c09dba39ad68327ce34542235265d889
jdpgrailsdev <jpearlin1@gmail.com>
2022-09-06 15:27:21 -0400

    Revert temporal proxy changes

--- airbyte-workers/src/main/java/io/airbyte/workers/temporal/support/TemporalActivityStubInterceptor.java
+++ airbyte-workers/src/main/java/io/airbyte/workers/temporal/support/TemporalActivityStubInterceptor.java
@@ -2,21 +2,18 @@
  * Copyright (c) 2022 Airbyte, Inc., all rights reserved.
  */
 
-package io.airbyte.workers.temporal;
+package io.airbyte.workers.temporal.support;
 
 import com.google.common.annotations.VisibleForTesting;
 import io.airbyte.workers.temporal.annotations.TemporalActivityStub;
-import io.airbyte.workers.temporal.support.TemporalActivityStubGenerationOptions;
-import io.airbyte.workers.temporal.support.TemporalActivityStubGeneratorFunction;
 import io.micronaut.context.BeanRegistration;
 import io.temporal.activity.ActivityOptions;
+import io.temporal.workflow.Workflow;
 import java.lang.reflect.Field;
-import java.lang.reflect.InvocationTargetException;
 import java.util.Collection;
 import java.util.Optional;
 import java.util.concurrent.Callable;
 import lombok.extern.slf4j.Slf4j;
-import net.bytebuddy.implementation.bind.annotation.AllArguments;
 import net.bytebuddy.implementation.bind.annotation.RuntimeType;
 import net.bytebuddy.implementation.bind.annotation.SuperCall;
 import net.bytebuddy.implementation.bind.annotation.This;
@@ -34,15 +31,16 @@ import org.springframework.util.ReflectionUtils;
 public class TemporalActivityStubInterceptor<T> {
 
   /**
+   * Function that generates Temporal activity stubs.
+   *
+   * Replace this value for unit testing.
+   */
+  private TemporalActivityStubGeneratorFunction<Class<?>, ActivityOptions, Object> activityStubGenerator = Workflow::newActivityStub;
+
+  /**
    * The collection of configured {@link ActivityOptions} beans provided by the application framework.
    */
   private final Collection<BeanRegistration<ActivityOptions>> availableActivityOptions;
-
-  /**
-   * Collection of configured {@link TemporalActivityStubGeneratorFunction} beans provided by the
-   * application framework.
-   */
-  private final Collection<BeanRegistration<TemporalActivityStubGeneratorFunction>> availableGeneratorFunctions;
 
   /**
    * The type of the workflow implementation to be proxied.
@@ -55,15 +53,11 @@ public class TemporalActivityStubInterceptor<T> {
    * @param workflowImplClass The Temporal workflow implementation class that will be intercepted.
    * @param availableActivityOptions The collection of configured {@link ActivityOptions} beans
    *        provided by the application framework.
-   * @param availableGeneratorFunctions The collection of configured
-   *        {@link TemporalActivityStubGeneratorFunction} beans provided by the application framework.
    */
   public TemporalActivityStubInterceptor(final Class<T> workflowImplClass,
-                                         final Collection<BeanRegistration<ActivityOptions>> availableActivityOptions,
-                                         final Collection<BeanRegistration<TemporalActivityStubGeneratorFunction>> availableGeneratorFunctions) {
+                                         final Collection<BeanRegistration<ActivityOptions>> availableActivityOptions) {
     this.workflowImplClass = workflowImplClass;
     this.availableActivityOptions = availableActivityOptions;
-    this.availableGeneratorFunctions = availableGeneratorFunctions;
   }
 
   /**
@@ -72,17 +66,16 @@ public class TemporalActivityStubInterceptor<T> {
    * @param workflowImplInstance The actual workflow implementation object invoked on the proxy
    *        Temporal workflow instance.
    * @param call A {@link Callable} used to invoke the proxied method.
-   * @param args The arguments passed to the proxy invoked method.
    * @return The result of the proxied method execution.
    * @throws Exception if the proxied method throws a checked exception
    * @throws IllegalStateException if the Temporal activity stubs associated with the workflow cannot
    *         be initialized.
    */
   @RuntimeType
-  public Object execute(@This final T workflowImplInstance, @SuperCall final Callable<Object> call, @AllArguments final Object... args)
+  public Object execute(@This final T workflowImplInstance, @SuperCall final Callable<Object> call)
       throws Exception {
     // Initialize the activity stubs, if not already done, before execution of the workflow method
-    initializeActivityStubs(workflowImplClass, workflowImplInstance, args);
+    initializeActivityStubs(workflowImplClass, workflowImplInstance);
     return call.call();
   }
 
@@ -92,15 +85,12 @@ public class TemporalActivityStubInterceptor<T> {
    *
    * @param workflowImplClass The target class of the proxy.
    * @param workflowInstance The workflow instance that may contain Temporal activity stub fields.
-   * @param methodArguments The collection of {@link ActivityOptions} beans configured in the
-   *        application context.
    */
   private void initializeActivityStubs(final Class<T> workflowImplClass,
-                                       final T workflowInstance,
-                                       final Object... methodArguments) {
+                                       final T workflowInstance) {
     for (final Field field : workflowImplClass.getDeclaredFields()) {
       if (field.isAnnotationPresent(TemporalActivityStub.class)) {
-        initializeActivityStub(workflowInstance, field, methodArguments);
+        initializeActivityStub(workflowInstance, field);
       }
     }
   }
@@ -112,20 +102,16 @@ public class TemporalActivityStubInterceptor<T> {
    * @param workflowInstance The Temporal workflow instance that contains the Temporal activity stub
    *        field.
    * @param activityStubField The field that represents the Temporal activity stub.
-   * @param methodArguments The collection of {@link ActivityOptions} beans configured in the
-   *        application context.
    */
   private void initializeActivityStub(final T workflowInstance,
-                                      final Field activityStubField,
-                                      final Object... methodArguments) {
+                                      final Field activityStubField) {
     try {
       log.debug("Attempting to initialize Temporal activity stub for activity '{}' on workflow '{}'...", activityStubField.getType(),
           workflowInstance.getClass().getName());
       ReflectionUtils.makeAccessible(activityStubField);
       if (activityStubField.get(workflowInstance) == null) {
-        final TemporalActivityStubGenerationOptions generationOptions = getGenerationOptions(activityStubField, methodArguments);
-        final TemporalActivityStubGeneratorFunction generatorFunction = getGeneratorFunction(activityStubField);
-        final Object activityStub = generatorFunction.apply(generationOptions);
+        final ActivityOptions activityOptions = getActivityOptions(activityStubField);
+        final Object activityStub = generateActivityStub(activityStubField, activityOptions);
         activityStubField.set(workflowInstance, activityStub);
         log.debug("Initialized Temporal activity stub for activity '{}' for workflow '{}'.", activityStubField.getType(),
             workflowInstance.getClass().getName());
@@ -173,65 +159,12 @@ public class TemporalActivityStubInterceptor<T> {
    * @throws IllegalStateException if the referenced {@link TemporalActivityStubGeneratorFunction}
    *         bean cannot be located.
    */
-  private TemporalActivityStubGeneratorFunction getGeneratorFunction(final Field activityStubField) {
-    final TemporalActivityStub annotation = activityStubField.getAnnotation(TemporalActivityStub.class);
-    final String generatorFunctionBeanName = annotation.activityGeneratorBeanName();
-    final Optional<TemporalActivityStubGeneratorFunction> selectedGeneratorFunction =
-        availableGeneratorFunctions.stream().filter(b -> b.getIdentifier().getName().equalsIgnoreCase(generatorFunctionBeanName))
-            .map(b -> b.getBean()).findFirst();
-    if (selectedGeneratorFunction.isPresent()) {
-      return selectedGeneratorFunction.get();
-    } else {
-      throw new IllegalStateException("No activity generator function beans of name '" + generatorFunctionBeanName + "' exists.");
-    }
+  private Object generateActivityStub(final Field activityStubField, final ActivityOptions activityOptions) {
+    return activityStubGenerator.apply(activityStubField.getType(), activityOptions);
   }
 
-  /**
-   * Builds the {@link TemporalActivityStubGenerationOptions} used to generate the Temporal activity
-   * stub.
-   *
-   * @param activityStubField The field that represents the Temporal activity stub.
-   * @param methodArguments The arguments passed to the proxy invoked method.
-   * @return The {@link TemporalActivityStubGenerationOptions} to be used to generate a Temporal
-   *         activity stub.
-   */
-  private TemporalActivityStubGenerationOptions getGenerationOptions(final Field activityStubField, final Object... methodArguments) {
-    final ActivityOptions activityOptions = getActivityOptions(activityStubField);
-    final Class<?> activityStubClass = activityStubField.getType();
-    final String workflowVersionChangedId = getWorkflowVersionChangeId(activityStubField);
-    return new TemporalActivityStubGenerationOptions(activityOptions, activityStubClass, methodArguments, workflowVersionChangedId);
-  }
-
-  /**
-   * Retrieves the Temporal workflow version change ID associated with the Temporal activity stub.
-   *
-   * @param activityStubField The field that represents the Temporal activity stub.
-   * @return The configured Temporal workflow version change ID associated with the Temporal activity
-   *         stub.
-   */
-  private String getWorkflowVersionChangeId(final Field activityStubField) {
-    final TemporalActivityStub annotation = activityStubField.getAnnotation(TemporalActivityStub.class);
-    return annotation.workflowVersionChangeId();
-  }
-
-  /**
-   * Handle the given {@link InvocationTargetException}. Throws the underlying
-   * {@link RuntimeException} or {@link Error} in case of such a root cause. Otherwise, the
-   * {@link Exception} is thrown.
-   *
-   * @param e The {@link InvocationTargetException}.
-   * @throws Exception the underlying cause exception extracted from the
-   *         {@link InvocationTargetException}.
-   */
   @VisibleForTesting
-  static void handleInvocationTargetException(final InvocationTargetException e) throws Exception {
-    if (e.getTargetException() instanceof Error) {
-      throw (Error) e.getTargetException();
-    } else if (e.getTargetException() instanceof RuntimeException) {
-      throw (RuntimeException) e.getTargetException();
-    } else {
-      throw (Exception) e.getTargetException();
-    }
+  void setActivityStubGenerator(final TemporalActivityStubGeneratorFunction<Class<?>, ActivityOptions, Object> activityStubGenerator) {
+    this.activityStubGenerator = activityStubGenerator;
   }
-
 }
