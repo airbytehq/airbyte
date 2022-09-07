6db5452a4ca938ce409fd40ced6ac555c0e81079
Sergey Chvalyuk <grubberr@gmail.com>
2022-09-07 12:21:30 +0300

    scan_all_spec_airbyte_secret.py destination added
    Signed-off-by: Sergey Chvalyuk <grubberr@gmail.com>

--- tools/bin/scan_all_spec_airbyte_secret.py
+++ tools/bin/scan_all_spec_airbyte_secret.py
@@ -17,6 +17,7 @@ import docker
 import yaml
 
 SOURCE_DEFINITIONS = "airbyte-config/init/src/main/resources/seed/source_definitions.yaml"
+DESTINATION_DEFINITIONS = "airbyte-config/init/src/main/resources/seed/destination_definitions.yaml"
 SPECS_DIR = "specs"
 SPEC_FILE = "spec.json"
 PATTERNS = [
@@ -42,12 +43,12 @@ def git_toplevel():
     return process.stdout.strip()
 
 
-def get_source_connectors():
+def get_connectors(filename):
     toplevel = git_toplevel()
-    with open(os.path.join(toplevel, SOURCE_DEFINITIONS)) as fp:
-        source_definitions = yaml.safe_load(fp)
+    with open(os.path.join(toplevel, filename)) as fp:
+        definitions = yaml.safe_load(fp)
     res = {}
-    for item in source_definitions:
+    for item in definitions:
         connector_name = item["dockerRepository"][len("airbyte/") :]
         docker_image = item["dockerRepository"] + ":" + item["dockerImageTag"]
         res[connector_name] = docker_image
@@ -75,8 +76,8 @@ def get_spec(output):
 
 def generate_all_specs():
     client = docker.from_env()
-    source_connectors = get_source_connectors()
-    for connector_name, docker_image in source_connectors.items():
+    connectors = get_connectors(SOURCE_DEFINITIONS) | get_connectors(DESTINATION_DEFINITIONS)
+    for connector_name, docker_image in connectors.items():
         logging.info(f"docker run -ti --rm {docker_image} spec")
         output = docker_run(client, docker_image)
         if output:
@@ -107,16 +108,17 @@ def find_properties(properties, path=None):
         path = []
 
     for prop_name, prop_obj in properties.items():
-        if prop_obj.get("type") == "object":
-            if "properties" in prop_obj:
-                yield from find_properties(prop_obj["properties"], path=path + [prop_name])
-            elif "oneOf" in prop_obj:
-                for n, oneof in enumerate(prop_obj["oneOf"]):
-                    yield from find_properties(oneof["properties"], path=path + [prop_name, f"[{n}]"])
-        elif prop_obj.get("type", "string") == "array" and prop_obj["items"].get("type") == "object":
-            yield from find_properties(prop_obj["items"]["properties"], path=path + [prop_name])
-        else:
-            yield path, prop_name, prop_obj
+        if isinstance(prop_obj, dict):
+            if prop_obj.get("type") == "object":
+                if "properties" in prop_obj:
+                    yield from find_properties(prop_obj["properties"], path=path + [prop_name])
+                elif "oneOf" in prop_obj:
+                    for n, oneof in enumerate(prop_obj["oneOf"]):
+                        yield from find_properties(oneof["properties"], path=path + [prop_name, f"[{n}]"])
+            elif prop_obj.get("type", "string") == "array" and prop_obj["items"].get("type") == "object":
+                yield from find_properties(prop_obj["items"]["properties"], path=path + [prop_name])
+            else:
+                yield path, prop_name, prop_obj
 
 
 def main():
@@ -126,8 +128,8 @@ def main():
     PATTERN = re.compile("|".join(PATTERNS), re.I)
 
     for filename, obj in iter_all_specs(SPECS_DIR):
-        properties = obj["spec"]["connectionSpecification"]["properties"]
-        for prop_path, prop_name, prop_obj in find_properties(properties):
+        spec = obj["spec"]
+        for prop_path, prop_name, prop_obj in find_properties(spec):
             if prop_obj.get("type") != "boolean" and not prop_obj.get("airbyte_secret") and PATTERN.search(prop_name):
                 print(filename, ".".join(prop_path + [prop_name]))
 
