8a71d8106499cdb177de5192ff2e29731d74714b
nataly <nataly@airbyte.io>
2022-09-07 09:35:11 -0400

    Converts worspace item into a button and adds some styling.

--- airbyte-webapp/src/packages/cloud/views/workspaces/WorkspacesPage/components/WorkspaceItem.tsx
+++ airbyte-webapp/src/packages/cloud/views/workspaces/WorkspacesPage/components/WorkspaceItem.tsx
@@ -3,26 +3,28 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 import styled from "styled-components";
 
-import { H5, ContentCard } from "components";
+import { H5 } from "components";
 
-const Item = styled(ContentCard)`
-  padding: 20px 28px 20px 20px;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  margin-bottom: 10px;
-  cursor: pointer;
-`;
+import styles from "./WorkspaceItem.module.scss";
+
+// const Item = styled(ContentCard)`
+//   padding: 20px 28px 20px 20px;
+//   display: flex;
+//   justify-content: space-between;
+//   align-items: center;
+//   margin-bottom: 10px;
+//   cursor: pointer;
+// `;
 
 const Arrow = styled(FontAwesomeIcon)`
   color: ${({ theme }) => theme.primaryColor};
 `;
 
 const WorkspaceItem: React.FC<{ onClick: (id: string) => void; id: string }> = (props) => (
-  <Item onClick={() => props.onClick(props.id)}>
+  <button className={styles.item} onClick={() => props.onClick(props.id)}>
     <H5 bold>{props.children}</H5>
     <Arrow icon={faChevronRight} />
-  </Item>
+  </button>
 );
 
 export default WorkspaceItem;
