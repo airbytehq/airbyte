fe13a952cee90bb9b86183c53bf34b6f92d44957
Benoit Moriceau <benoit@airbyte.io>
2022-09-08 10:31:53 -0700

    Remove PMD warning and fix a code style

--- airbyte-workers/src/main/java/io/airbyte/workers/temporal/scheduling/ConnectionManagerWorkflowImpl.java
+++ airbyte-workers/src/main/java/io/airbyte/workers/temporal/scheduling/ConnectionManagerWorkflowImpl.java
@@ -179,6 +179,7 @@ public class ConnectionManagerWorkflowImpl implements ConnectionManagerWorkflow 
     }
   }
 
+  @SuppressWarnings("PMD.EmptyIfStmt")
   private CancellationScope generateSyncWorkflowRunnable(final ConnectionUpdaterInput connectionUpdaterInput) {
     return Workflow.newCancellationScope(() -> {
       connectionId = connectionUpdaterInput.getConnectionId();
@@ -247,7 +248,7 @@ public class ConnectionManagerWorkflowImpl implements ConnectionManagerWorkflow 
         if (childWorkflowFailure.getCause() instanceof CanceledFailure) {
           // do nothing, cancellation handled by cancellationScope
           recordWorkflowFailureCountMetric(connectionUpdaterInput, FailureCause.CANCELED);
-        } else if (childWorkflowFailure.getCause()instanceof final ActivityFailure af) {
+        } else if (childWorkflowFailure.getCause() instanceof final ActivityFailure af) {
           // Allows us to classify unhandled failures from the sync workflow. e.g. If the normalization
           // activity throws an exception, for
           // example, this lets us set the failureOrigin to normalization.
