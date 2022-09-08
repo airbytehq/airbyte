50cf5ef4c086490f93c5f8c1292e860e49d62ae7
Edmundo Ruiz Ghanem <168664+edmundito@users.noreply.github.com>
2022-09-08 14:28:50 -0400

    🪟 🎨 🧹 Migrate `Input` and `TextArea` to SCSS  (#16378)
    * Migrate TextArea component to SCSS and add Storybook
    
    * Move Input styles to SCSS, add Storybook
    
    * Fix Input stylelint issues
    
    * Fix hover selector on Input container to avoid hovering on focus
    
    * Fix Input focus test by using style file
    
    * Add missing & to Textarea style
    
    * Fix styleint inssue in Input
    
    * Move input testid before props

--- airbyte-webapp/src/components/base/TextArea/TextArea.tsx
+++ airbyte-webapp/src/components/base/TextArea/TextArea.tsx
@@ -1,44 +1,25 @@
+import classNames from "classnames";
 import React from "react";
-import styled from "styled-components";
 
-type TextAreaProps = {
+import styles from "./TextArea.module.scss";
+
+interface TextAreaProps extends React.TextareaHTMLAttributes<HTMLTextAreaElement> {
   error?: boolean;
   light?: boolean;
-} & React.TextareaHTMLAttributes<HTMLTextAreaElement>;
+}
 
-const TextArea = styled.textarea<TextAreaProps>`
-  outline: none;
-  resize: none;
-  width: 100%;
-  padding: 7px 8px;
-  border-radius: 4px;
-  font-size: 14px;
-  line-height: 20px;
-  font-weight: normal;
-  border: 1px solid ${(props) => (props.error ? props.theme.dangerColor : props.theme.greyColor0)};
-  background: ${({ theme }) => theme.greyColor0};
-  color: ${({ theme }) => theme.textColor};
-  caret-color: ${({ theme }) => theme.primaryColor};
-
-  &::placeholder {
-    color: ${({ theme }) => theme.greyColor40};
-  }
-
-  &:hover {
-    background: ${({ theme, light }) => (light ? theme.whiteColor : theme.greyColor20)};
-    border-color: ${(props) => (props.error ? props.theme.dangerColor : props.theme.greyColor20)};
-  }
-
-  &:focus {
-    background: ${({ theme, light }) => (light ? theme.whiteColor : theme.primaryColor12)};
-    border-color: ${({ theme }) => theme.primaryColor};
-  }
-
-  &:disabled {
-    pointer-events: none;
-    color: ${({ theme }) => theme.greyColor55};
-  }
-`;
-
-export { TextArea };
-export type { TextAreaProps };
+export const TextArea: React.FC<TextAreaProps> = ({ error, light, children, className, ...textAreaProps }) => (
+  <textarea
+    {...textAreaProps}
+    className={classNames(
+      styles.textarea,
+      {
+        [styles.error]: error,
+        [styles.light]: light,
+      },
+      className
+    )}
+  >
+    {children}
+  </textarea>
+);
