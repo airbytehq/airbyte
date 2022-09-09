3176b341d17cfcf7f0bc4e7c9eefff343303e45b
lmossman <lake@airbyte.io>
2022-09-09 11:25:47 -0700

    move examples into its own component

--- airbyte-webapp/src/views/Connector/ServiceForm/components/Property/LabelInfo.tsx
+++ airbyte-webapp/src/views/Connector/ServiceForm/components/Property/LabelInfo.tsx
@@ -5,38 +5,38 @@ import { TextWithHTML } from "components/TextWithHTML";
 
 import styles from "./LabelInfo.module.scss";
 
-interface Props {
+interface LabelInfoProps {
   label: React.ReactNode;
   examples?: JSONSchema7Type;
   description?: string;
 }
 
-export const LabelInfo: React.FC<Props> = ({ label, examples, description }) => {
-  const constructExamples = () => {
-    if (!examples) {
-      return null;
-    }
+const Examples: React.FC<Pick<LabelInfoProps, "examples">> = ({ examples }) => {
+  if (!examples) {
+    return null;
+  }
 
-    const examplesArray = Array.isArray(examples) ? examples : [examples];
+  const examplesArray = Array.isArray(examples) ? examples : [examples];
 
-    return (
-      <>
-        {/* don't use <Text as=h4> here, because we want the default tooltip text styling for this header */}
-        <h4 className={styles.exampleHeader}>{`Example value${examplesArray.length > 1 ? "s" : ""}`}</h4>
-        <div className={styles.exampleContainer}>
-          {examplesArray.map((example) => (
-            <span className={styles.exampleItem}>{example}</span>
-          ))}
-        </div>
-      </>
-    );
-  };
+  return (
+    <>
+      {/* don't use <Text as=h4> here, because we want the default parent styling for this header */}
+      <h4 className={styles.exampleHeader}>{`Example value${examplesArray.length > 1 ? "s" : ""}`}</h4>
+      <div className={styles.exampleContainer}>
+        {examplesArray.map((example) => (
+          <span className={styles.exampleItem}>{example}</span>
+        ))}
+      </div>
+    </>
+  );
+};
 
+export const LabelInfo: React.FC<LabelInfoProps> = ({ label, examples, description }) => {
   return (
     <>
       <h4 className={styles.descriptionHeader}>{label}</h4>
       {description && <TextWithHTML className={styles.description} text={description} />}
-      {constructExamples()}
+      <Examples examples={examples} />
     </>
   );
 };
