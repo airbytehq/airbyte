e5df0e21efb1326445dcf25cb3c0127676e07269
Pedro Lopez <pedroslopez@me.com>
2022-09-09 01:42:13 -0400

    fix remote definitions provider: add tombstones

--- airbyte-config/init/src/main/java/io/airbyte/config/init/RemoteDefinitionsProvider.java
+++ airbyte-config/init/src/main/java/io/airbyte/config/init/RemoteDefinitionsProvider.java
@@ -50,10 +50,10 @@ final public class RemoteDefinitionsProvider implements DefinitionsProvider {
     final CombinedConnectorCatalog catalog = getRemoteDefinitionCatalog(this.remoteDefinitionCatalogUrl, this.timeout);
     this.sourceDefinitions = catalog.getSources().stream().collect(Collectors.toMap(
         StandardSourceDefinition::getSourceDefinitionId,
-        source -> source));
+        source -> source.withTombstone(source.getTombstone() != null && source.getTombstone())));
     this.destinationDefinitions = catalog.getDestinations().stream().collect(Collectors.toMap(
         StandardDestinationDefinition::getDestinationDefinitionId,
-        source -> source));
+        destination -> destination.withTombstone(destination.getTombstone() != null && destination.getTombstone())));
   }
 
   @Override
