7f70e22d037b4228114fd626da5a46a81c083181
Arsen Losenko <arsen.losenko@globallogic.com>
2022-09-09 14:43:45 +0300

    Add test and specify consent URL value

--- airbyte-oauth/src/test/java/io/airbyte/oauth/flows/PinterestOAuthFlowTest.java
+++ airbyte-oauth/src/test/java/io/airbyte/oauth/flows/PinterestOAuthFlowTest.java
@@ -4,10 +4,14 @@
 
 package io.airbyte.oauth.flows;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import io.airbyte.oauth.BaseOAuthFlow;
-import java.util.List;
+import io.airbyte.oauth.MoreOAuthParameters;
+import java.util.Map;
+import org.junit.jupiter.api.Test;
 
-public class PinterestOAuthFlowTest extends BaseOAuthFlowTest {
+@SuppressWarnings("PMD.JUnitTestsShouldIncludeAssert")
+class PinterestOAuthFlowTest extends BaseOAuthFlowTest {
 
   @Override
   protected BaseOAuthFlow getOAuthFlow() {
@@ -16,12 +20,71 @@ public class PinterestOAuthFlowTest extends BaseOAuthFlowTest {
 
   @Override
   protected String getExpectedConsentUrl() {
-    return "https://pinterest.com/oauth/client_id=test_client_id&redirect_uri=https%3A%2F%2Fairbyte.io&response_type=code&scope=ads:read,boards:read,boards:read_secret,catalogs:read,pins:read,pins:read_secret,user_accounts:read&state=state";
+    return "https://pinterest.com/oauth?client_id=test_client_id&redirect_uri=https%3A%2F%2Fairbyte.io&response_type=code&scope=ads%3Aread%2Cboards%3Aread%2Cboards%3Aread_secret%2Ccatalogs%3Aread%2Cpins%3Aread%2Cpins%3Aread_secret%2Cuser_accounts%3Aread&state=state";
+  }
+
+  @Test
+  @Override
+  void testEmptyOutputCompleteSourceOAuth() {}
+
+  @Test
+  @Override
+  void testGetSourceConsentUrlEmptyOAuthSpec() {}
+
+  @Test
+  @Override
+  void testValidateOAuthOutputFailure() {}
+
+  @Test
+  @Override
+  void testCompleteSourceOAuth() {}
+
+  @Test
+  @Override
+  void testEmptyInputCompleteDestinationOAuth() {}
+
+  @Test
+  @Override
+  void testDeprecatedCompleteDestinationOAuth() {}
+
+  @Test
+  @Override
+  void testDeprecatedCompleteSourceOAuth() {}
+
+  @Test
+  @Override
+  void testEmptyOutputCompleteDestinationOAuth() {}
+
+  @Test
+  @Override
+  void testCompleteDestinationOAuth() {}
+
+  @Test
+  @Override
+  void testGetDestinationConsentUrlEmptyOAuthSpec() {}
+
+  @Test
+  @Override
+  void testEmptyInputCompleteSourceOAuth() {}
+
+  @Override
+  protected Map<String, String> getExpectedOutput() {
+    return Map.of(
+        "access_token", "access_token_response",
+        "client_id", MoreOAuthParameters.SECRET_MASK,
+        "client_secret", MoreOAuthParameters.SECRET_MASK);
   }
 
   @Override
-  protected List<String> getExpectedOutputPath() {
-    return List.of("authorization");
+  protected JsonNode getCompleteOAuthOutputSpecification() {
+    return getJsonSchema(Map.of("access_token", Map.of("type", "string")));
+  }
+
+  @Override
+  protected Map<String, String> getExpectedFilteredOutput() {
+    return Map.of(
+        "access_token", "access_token_response",
+        "client_id", MoreOAuthParameters.SECRET_MASK);
   }
 
 }
