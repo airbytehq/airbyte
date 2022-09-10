d50498770af524f3daedf35a13ae2e75979438e8
Sergey Chvalyuk <grubberr@gmail.com>
2022-09-10 21:18:26 +0300

    fix tests
    Signed-off-by: Sergey Chvalyuk <grubberr@gmail.com>

--- airbyte-integrations/connectors/source-mixpanel/unit_tests/conftest.py
+++ airbyte-integrations/connectors/source-mixpanel/unit_tests/conftest.py
@@ -18,9 +18,18 @@ def config(start_date):
     return {
         "api_secret": "unexisting-secret",
         "attribution_window": 5,
-        "project_timezone": "UTC",
+        "project_timezone": pendulum.timezone("UTC"),
         "select_properties_by_default": True,
         "start_date": start_date,
         "end_date": start_date + timedelta(days=31),
         "region": "US",
     }
+
+
+@pytest.fixture
+def config_raw(config):
+    config = {**config}
+    config["project_timezone"] = config["project_timezone"].name
+    config["start_date"] = "2017-01-25T00:00:00Z"
+    config["end_date"] = "2017-02-25T00:00:00Z"
+    return config
