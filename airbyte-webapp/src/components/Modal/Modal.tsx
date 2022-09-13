02e2bbc58917a38cb5cf5de3854e6fe19a0b3b18
Teal Larson <LARSON.TEAL@GMAIL.COM>
2022-09-13 10:54:06 -0400

    🪟 🧹 Clean up modal and onboarding video components (#16466)
    * remove unneeded closeOnBackground property
    
    * rename variable for clarity
    
    * here too

--- airbyte-webapp/src/components/Modal/Modal.tsx
+++ airbyte-webapp/src/components/Modal/Modal.tsx
@@ -9,8 +9,7 @@ import styles from "./Modal.module.scss";
 export interface ModalProps {
   title?: string | React.ReactNode;
   onClose?: () => void;
-  clear?: boolean;
-  closeOnBackground?: boolean;
+  cardless?: boolean;
   size?: "sm" | "md" | "lg" | "xl";
   testId?: string;
 }
@@ -22,7 +21,7 @@ const cardStyleBySize = {
   xl: styles.xl,
 };
 
-const Modal: React.FC<ModalProps> = ({ children, title, size, onClose, clear, testId }) => {
+const Modal: React.FC<ModalProps> = ({ children, title, size, onClose, cardless, testId }) => {
   const [isOpen, setIsOpen] = useState(true);
 
   const onModalClose = () => {
@@ -35,7 +34,7 @@ const Modal: React.FC<ModalProps> = ({ children, title, size, onClose, clear, te
       <div className={styles.backdrop} />
       <div className={styles.modalContainer}>
         <Dialog.Panel className={styles.modalPanel}>
-          {clear ? (
+          {cardless ? (
             children
           ) : (
             <ContentCard title={title} className={classNames(styles.card, size ? cardStyleBySize[size] : undefined)}>
