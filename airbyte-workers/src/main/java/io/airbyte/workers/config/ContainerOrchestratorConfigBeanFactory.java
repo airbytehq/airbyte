474642868cd8532318c9bbf7a3d394ca6c7f4669
Jonathan Pearlin <jonathan@airbyte.io>
2022-09-14 13:10:23 -0400

    Convert airbyte-workers to Micronaut (#16434)
    * WIP Convert airbyte-workers to Micronaut framework
    
    * Rebase cleanup
    
    * Fix broken tests
    
    * Simplify code
    
    * Support control vs data plane configuration
    
    * make WORFKLOW_PROXY_CACHE non-static to avoid cacheing mocks in unit tests
    
    * Formatting
    
    * Pairing on Worker Micronaut (#16364)
    
    * add RouteToSyncTaskQueue activity
    
    * use new route activity in connection manager workflow
    
    * format
    
    * call router service for task queue
    
    * Revert temporal proxy changes
    
    * Formatting
    
    * Fix default value
    
    * register new route activity in test
    
    * fix SyncWorkflowTest now that it isn't doing any routing
    
    * Update dependencies
    
    * More dependency updates
    
    * Update dependencies
    
    * Improve conditional bean check
    
    * Match existing Optional functionality
    
    * Add notEquals check
    
    * Add missing env var to Helm chart
    
    * Fix typo
    
    * Mark LogConfigs as Singleton
    
    * Env vars for log/state storage type
    
    * Remove use of Optional in bean declarations
    
    * Fix typo in config property name
    
    * Support Temporal Cloud namespace
    
    * Change to @Value
    
    * Use correct value for conditional check
    
    * Upgrade Micronaut
    
    * Fix merge conflict
    
    * Formatting
    
    * Add missing env var
    
    * Use sync task queue environment variable
    
    * Handle sync task queue as set
    
    * format and force http
    
    * Handle case where sync task queue is empty
    
    * Add correct path to config property
    
    * Remove unused import
    
    * Remove unused parameter
    
    * Formatting
    
    * Use pattern for condition process factory beans
    
    * Cleanup
    
    * PR feedback
    
    * Revert hack for testing
    
    Co-authored-by: pmossman <parker@airbyte.io>

---
+++ airbyte-workers/src/main/java/io/airbyte/workers/config/ContainerOrchestratorConfigBeanFactory.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright (c) 2022 Airbyte, Inc., all rights reserved.
+ */
+
+package io.airbyte.workers.config;
+
+import io.airbyte.config.storage.CloudStorageConfigs;
+import io.airbyte.workers.ContainerOrchestratorConfig;
+import io.airbyte.workers.general.DocumentStoreClient;
+import io.airbyte.workers.storage.StateClients;
+import io.fabric8.kubernetes.client.DefaultKubernetesClient;
+import io.micronaut.context.annotation.Factory;
+import io.micronaut.context.annotation.Requires;
+import io.micronaut.context.annotation.Value;
+import io.micronaut.core.util.StringUtils;
+import java.nio.file.Path;
+import java.util.Optional;
+import javax.inject.Named;
+import javax.inject.Singleton;
+
+/**
+ * Micronaut bean factory for container orchestrator configuration-related singletons.
+ */
+@Factory
+public class ContainerOrchestratorConfigBeanFactory {
+
+  // IMPORTANT: Changing the storage location will orphan already existing kube pods when the new
+  // version is deployed!
+  private static final Path STATE_STORAGE_PREFIX = Path.of("/state");
+
+  @Singleton
+  @Requires(property = "airbyte.container.orchestrator.enabled",
+            value = "true")
+  @Named("containerOrchestratorConfig")
+  public ContainerOrchestratorConfig kubernetesContainerOrchestratorConfig(
+                                                                           @Named("stateStorageConfigs") final Optional<CloudStorageConfigs> cloudStateStorageConfiguration,
+                                                                           @Value("${airbyte.version}") final String airbyteVersion,
+                                                                           @Value("${airbyte.container.orchestrator.image}") final String containerOrchestratorImage,
+                                                                           @Value("${airbyte.worker.job.kube.main.container.image-pull-policy}") final String containerOrchestratorImagePullPolicy,
+                                                                           @Value("${airbyte.container.orchestrator.secret-mount-path}") final String containerOrchestratorSecretMountPath,
+                                                                           @Value("${airbyte.container.orchestrator.secret-name}") final String containerOrchestratorSecretName,
+                                                                           @Value("${google.application.credentials}") final String googleApplicationCredentials,
+                                                                           @Value("${airbyte.worker.job.kube.namespace}") final String namespace) {
+    final var kubernetesClient = new DefaultKubernetesClient();
+
+    final DocumentStoreClient documentStoreClient = StateClients.create(
+        cloudStateStorageConfiguration.orElse(null),
+        STATE_STORAGE_PREFIX);
+
+    return new ContainerOrchestratorConfig(
+        namespace,
+        documentStoreClient,
+        kubernetesClient,
+        containerOrchestratorSecretName,
+        containerOrchestratorSecretMountPath,
+        StringUtils.isNotEmpty(containerOrchestratorImage) ? containerOrchestratorImage : "airbyte/container-orchestrator:" + airbyteVersion,
+        containerOrchestratorImagePullPolicy,
+        googleApplicationCredentials);
+  }
+
+}
