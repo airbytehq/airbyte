e3b06ab3ecd61e52e90c47d4681c50b2ec8db4a5
Oleksandr Bazarnov <oleksandr.bazarnov@globallogic.com>
2022-09-14 23:41:30 +0300

    refactored to use REST API

--- airbyte-integrations/connectors/source-netsuite/source_netsuite/source.py
+++ airbyte-integrations/connectors/source-netsuite/source_netsuite/source.py
@@ -1,85 +1,113 @@
 #
-# Copyright (c) 2022 Airbyte, Inc., all rights reserved.
+# Copyright (c) 2021 Airbyte, Inc., all rights reserved.
 #
 
+
+from collections import Counter
 from typing import Any, List, Mapping, Tuple
 
+import requests
 from airbyte_cdk.sources import AbstractSource
 from airbyte_cdk.sources.streams import Stream
-from netsuitesdk import NetSuiteConnection
-from netsuitesdk.internal.exceptions import NetSuiteLoginError, NetSuiteRequestError
-
+from requests_oauthlib import OAuth1
 from source_netsuite.streams import (
-    Accounts,
-    BillingAccounts,
-    Classifications,
-    Currencies,
-    Customers,
-    Departments,
-    Employees,
-    ExpenseCategories,
-    ExpenseReports,
-    Files,
-    Folders,
-    JournalEntries,
-    Locations,
-    Projects,
-    Subsidiaries,
-    TaxGroups,
-    TaxItems,
-    Terms,
-    VendorBills,
-    VendorPayments,
-    Vendors,
+    NetsuiteStream, 
+    IncrementalNetsuiteStream, 
+    CustomIncrementalNetsuiteStream,
+    RECORD_PATH,
+    META_PATH,
+    SCHEMA_HEADERS,
 )
 
 
 class SourceNetsuite(AbstractSource):
+    def auth(self, config: Mapping[str, Any]) -> OAuth1:
+        return OAuth1(
+            client_key=config["consumer_key"],
+            client_secret=config["consumer_secret"],
+            resource_owner_key=config["token_key"],
+            resource_owner_secret=config["token_secret"],
+            realm=config["realm"],
+            signature_method="HMAC-SHA256",
+        )
 
-    def check_connection(self, logger, config) -> Tuple[bool, any]:
-        netsuite_con_config_map = {
-            "account": config["realm"],
-            "consumer_key": config["consumer_key"],
-            "consumer_secret": config["consumer_secret"],
-            "token_key": config["token_key"],
-            "token_secret": config["token_secret"],
-        }
-        try:
-            netsuite_con = NetSuiteConnection(**netsuite_con_config_map)
-            if len(netsuite_con.accounts.get_all()) > 0:
+    def base_url(self, config: Mapping[str, Any]) -> str:
+        realm = config["realm"]
+        subdomain = realm.lower().replace("_", "-")
+        return f"https://{subdomain}.suitetalk.api.netsuite.com"
+
+    def get_session(self, auth: OAuth1) -> requests.Session:
+        session = requests.Session()
+        session.auth = auth
+        return session
+
+    def check_connection(self, logger, config: Mapping[str, Any]) -> Tuple[bool, any]:
+        auth = self.auth(config)
+        object_types = config.get("object_types")
+        base_url = self.base_url(config)
+        session = self.get_session(auth)
+        # if record types are specified make sure they are valid
+        if object_types:
+            # ensure there are no duplicate record types as this will break Airbyte
+            duplicates = [k for k, v in Counter(object_types).items() if v > 1]
+            if duplicates:
+                return False, f'Duplicate record type: {", ".join(duplicates)}'
+            # check connectivity to all provided `object_types`
+            url = base_url + RECORD_PATH
+            for object in object_types:
+                try:
+                    response = session.get(url=url+object, params={"limit": 1})
+                    response.raise_for_status()
+                    return True, None
+                except requests.exceptions.HTTPError as e:
+                    return False, e 
+        else:
+            # if `object_types` are not provided, use `Contact` stream
+            # there should be at least 1 contact available in every NetSuite account by default.
+            url = base_url + RECORD_PATH + "contact"
+            try:
+                response = session.get(url=url, params={"limit": 1})
+                response.raise_for_status()
                 return True, None
-        except NetSuiteLoginError or NetSuiteRequestError as e:
-            return False, e
+            except requests.exceptions.HTTPError as e:
+                return False, e
 
     def streams(self, config: Mapping[str, Any]) -> List[Stream]:
-        netsuite_con_config_map = {
-            "account": config["realm"],
-            "consumer_key": config["consumer_key"],
-            "consumer_secret": config["consumer_secret"],
-            "token_key": config["token_key"],
-            "token_secret": config["token_secret"],
-        }
-        config["netsuite_con"] = NetSuiteConnection(**netsuite_con_config_map)
-        return [
-            Accounts(config),
-            BillingAccounts(config),
-            Classifications(config),
-            Currencies(config),
-            Customers(config),
-            Departments(config),
-            Employees(config),
-            ExpenseReports(config),
-            ExpenseCategories(config),
-            Files(config),
-            Folders(config),
-            JournalEntries(config),
-            Locations(config),
-            Projects(config),
-            Subsidiaries(config),
-            TaxGroups(config),
-            TaxItems(config),
-            Terms(config),
-            VendorBills(config),
-            VendorPayments(config),
-            Vendors(config),
+        base_url = self.base_url(config)
+        auth = self.auth(config)
+        start_datetime = config["start_datetime"]
+        window_in_days = config["window_in_days"]
+        session = self.get_session(auth)
+        metadata_url = base_url + META_PATH
+        object_names = config.get("object_types", [])
+        
+        if not object_names:
+            # retrieve all record types
+            metadata = session.get(metadata_url).json().get("items")
+            object_names = [r["name"] for r in metadata]
+
+        # fetch schemas
+        schemas = {n: session.get(metadata_url + n, headers=SCHEMA_HEADERS).json() for n in object_names}
+        # get incremental object names
+        # incremental streams must have a `lastModifiedDate` property
+        incremental_object_names = [n for n in object_names if schemas[n]["properties"].get("lastModifiedDate")]
+        # get custom incremental object names
+        # custom incremental streams must have a `lastmodified` property
+        custom_incremental_object_names = [n for n in object_names if schemas[n]["properties"].get("lastmodified")]
+        # get full-refresh object names
+        standard_object_names = [n for n in object_names if n not in incremental_object_names]
+
+        incremental_streams = [
+            IncrementalNetsuiteStream(auth, name, base_url, start_datetime, window_in_days)
+            for name in incremental_object_names
         ]
+        custom_incremental_streams = [
+            CustomIncrementalNetsuiteStream(auth, name, base_url, start_datetime, window_in_days)
+            for name in custom_incremental_object_names
+        ]
+        streams = [
+            NetsuiteStream(auth, name, base_url, start_datetime, window_in_days)
+            for name in standard_object_names
+        ]
+
+        return streams + incremental_streams + custom_incremental_streams
--- airbyte-integrations/connectors/source-netsuite/source_netsuite/streams.py
+++ airbyte-integrations/connectors/source-netsuite/source_netsuite/streams.py
@@ -1,159 +1,246 @@
 #
-# Copyright (c) 2022 Airbyte, Inc., all rights reserved.
+# Copyright (c) 2021 Airbyte, Inc., all rights reserved.
 #
 
 
-from typing import Any, Iterable, Mapping, Optional, Union
+from abc import ABC
+from datetime import date, datetime, timedelta
+from typing import Any, Iterable, Mapping, MutableMapping, Optional, Union
 
-import zeep
 import requests
-from airbyte_cdk.models import SyncMode
 from airbyte_cdk.sources.streams.http import HttpStream
+from requests_oauthlib import OAuth1
 
+REST_PATH: str = "/services/rest/"
+RECORD_PATH: str = REST_PATH + "record/v1/"
+META_PATH: str = RECORD_PATH + "metadata-catalog/"
+SCHEMA_HEADERS: dict = {"Accept": "application/schema+json"}
 
-class NetsuiteStream(HttpStream):
-
-    def __init__(self, config: dict):
-        self.netsuite_con = config["netsuite_con"]
-
-    primary_key = "internalId"
+class NetsuiteStream(HttpStream, ABC):
     
+    def __init__(
+        self, 
+        auth: OAuth1, 
+        obj_name: str, 
+        base_url: str, 
+        start_datetime: str, 
+        window_in_days: int,
+    ):
+        self.obj_name = obj_name
+        self.base_url = base_url
+        self.start_datetime = start_datetime
+        self.window_in_days = window_in_days
+        self.schemas = {}  # store subschemas to reduce API calls
+        super().__init__(authenticator=auth)
+
+    primary_key = "id"
+    raise_on_http_errors = True
+    output_datetime_format = "%Y-%m-%dT%H:%M:%SZ"
+    input_datetime_format = "%m/%d/%Y"
+
+    @property
+    def name(self) -> str:
+        return self.obj_name
+
     @property
     def url_base(self) -> str:
-        return "/"
-    
-    @property
-    def data_structure(self) -> str:
-        return "list"
+        return self.base_url
 
     def path(self, **kwargs) -> str:
-        return self.name
-    
-    def next_page_token(self, response: requests.Response) -> Optional[Mapping[str, Any]]:
-        return None
+        return RECORD_PATH + self.obj_name
 
-    def parse_response(self, response: requests.Response, **kwargs) -> Iterable[Mapping]:
-        yield {}
-    
-    @staticmethod
-    def as_serialized(record) -> Iterable[Mapping[str, Any]]:
-        return [zeep.helpers.serialize_object(_) for _ in record]
-    
-    def get_stream(self, stream_name: str) -> Union[object, None]:
-        try:
-            # get stream object reference from NetsuiteConnection
-            return getattr(self.netsuite_con, stream_name)
-        except AttributeError:
-            self.logger.info(f"Stream `{stream_name}` is not available for NetsuiteConnection. Skipping...")
-            return None
-    
-    def fetch_records(self) -> Union[Iterable[Mapping[str, Any]], None]:
-        stream = self.get_stream(self.name)
-        if stream:
-            # prepare build-in generator for data fetch
-            records_producer = stream.get_all_generator()
-            # some streams return data as OrderedDict
-            if self.data_structure == "dict":
-                yield records_producer
+    def ref_schema(self) -> Mapping[str, str]:
+        return {
+            "type": ["null", "object"],
+            "properties": {
+                "id": {"title": "Internal identifier", "type": ["null", "string"]},
+                "refName": {"title": "Reference Name", "type": ["null", "string"]},
+                "externalId": {"title": "External identifier", "type": ["null", "string"]},
+                "links": {
+                    "title": "Links",
+                    "type": "array",
+                    "readOnly": True,
+                    "items": self.get_schema("/services/rest/record/v1/metadata-catalog/nsLink"),
+                },
+            },
+        }
+
+    def get_schema(self, ref: str) -> Union[Mapping[str, Any], str]:
+        # try to retrieve the schema from the cache
+        schema = self.schemas.get(ref)
+        if not schema:
+            url = self.url_base + ref
+            resp = requests.get(url, headers=SCHEMA_HEADERS, auth=self._session.auth)
+            # some schemas, like transaction, do not exist because they refer to multiple
+            # record types, e.g. sales order/invoice ... in this case we can't retrieve
+            # the correct schema, so we just put the json in a string
+            if resp.status_code == 404:
+                schema = {"title": ref, "type": "string"}
             else:
-                yield from records_producer
+                resp.raise_for_status
+                schema = resp.json()
+            self.schemas[ref] = schema
+        return schema
+
+    def build_schema(self, record: Any) -> Mapping[str, Any]:
+        # these parts of the schema is not used by Airybte
+        remove_from_schema: list = [
+            "enum", 
+            "x-ns-filterable", 
+            "x-ns-custom-field", 
+            "nullable",
+        ]
+        # recursively build a schema with subschemas
+        if isinstance(record, dict):
+            # Netsuite schemas do not specify if fields can be null, or not
+            # as Airbyte expects, so we have to allow every field to be null
+            property_type = record.get("type")
+            property_type_list = property_type if isinstance(property_type, list) else [property_type]
+            # ensure there is a type, type is the json schema type and not a property
+            # and null has not already been added
+            if property_type and not isinstance(property_type, dict) and "null" not in property_type_list:
+                record["type"] = ["null"] + property_type_list
+            # removing non-functional elements from schema
+            for element in remove_from_schema:
+                if record.get(element):
+                    record.pop(element)
+             
+            ref = record.get("$ref")
+            if ref:
+                ns_link = ref == "/services/rest/record/v1/metadata-catalog/nsLink"
+                return self.get_schema(ref) if ns_link else self.ref_schema()
+            else:
+                return {k: self.build_schema(v) for k, v in record.items()}
+        else:
+            return record
+
+    def get_json_schema(self, **kwargs) -> dict:
+        schema = self.get_schema(META_PATH + self.name)
+        return self.build_schema(schema)
+
+    def next_page_token(self, response: requests.Response) -> Optional[Mapping[str, Any]]:
+        resp = response.json()
+        has_more = resp.get("hasMore")
+        if has_more:
+            return {"offset": resp["offset"] + resp["count"]}
         return None
 
-    def read_records(self, sync_mode: SyncMode, **kwargs) -> Iterable[Mapping[str, Any]]:
-        for record in self.fetch_records():
-            yield from self.as_serialized(record)
+    def format_date(self, last_modified_date: str) -> str:
+        # the date format returned is differnet than what we need to use in the query
+        lmd_datetime = datetime.strptime(last_modified_date, self.output_datetime_format)
+        return lmd_datetime.strftime(self.input_datetime_format)
 
+    def request_params(self, next_page_token: Mapping[str, Any] = None, **kwargs) -> MutableMapping[str, Any]:
+        params = {}
+        if next_page_token:
+            params.update(**next_page_token)
+        return params
 
-class Accounts(NetsuiteStream):
-    """https://docs.oracle.com/en/cloud/saas/netsuite/ns-online-help/section_N3739999.html"""
+    def fetch_record(self, record: Mapping[str, Any], request_kwargs: Mapping[str, Any]) -> Iterable[Mapping[str, Any]]:
+        url = record["links"][0]["href"]
+        args = {"method": "GET", "url": url, "params": {"expandSubResources": True}}
+        prep_req = self._session.prepare_request(requests.Request(**args))
+        response = self._send_request(prep_req, request_kwargs)
+        yield response.json() or {}
+
+    def parse_response(
+        self,
+        response: requests.Response,
+        stream_state: Mapping[str, Any],
+        stream_slice: Mapping[str, Any] = None,
+        next_page_token: Mapping[str, Any] = None,
+        **kwargs,
+    ) -> Iterable[Mapping]:
+        records = response.json().get("items")
+        request_kwargs = self.request_kwargs(stream_slice, next_page_token)
+        if records:
+            for record in records:
+                # make sub-requests for each record fetched
+                yield from self.fetch_record(record, request_kwargs)
     
+    def stream_slices(
+        self, stream_state: Mapping[str, Any] = None, **kwargs: Optional[Mapping[str, Any]],
+    ) -> Iterable[Optional[Mapping[str, Any]]]:
+        # Netsuite cannot order records returned by the API, so we need stream slices
+        # to maintain state properly https://docs.airbyte.com/connector-development/cdk-python/incremental-stream/#streamstream_slices
+        ranges = []
+        start_str = stream_state.get(self.cursor_field) if stream_state else self.start_datetime
+        start = datetime.strptime(start_str, self.output_datetime_format).date()
+        # handle abnormal state values
+        if start > date.today():
+            return [None]
+        else:
+            while start <= date.today():
+                next_day = start + timedelta(days=self.window_in_days)
+                ranges.append(
+                    {
+                        "start": start.strftime(self.input_datetime_format),
+                        "end": next_day.strftime(self.input_datetime_format),
+                    }
+                )
+                start = next_day
+        return ranges
+                
+    def should_retry(self, response: requests.Response) -> bool:
+        if response.status_code == 400:
+            message = response.json().get("o:errorDetails")
+            if isinstance(message, list):
+                if f"Record '{self.name}' was not found." in message[0].get("detail"):
+                    # when stream declared but not available for fetch
+                    setattr(self, "raise_on_http_errors", False)
+                    self.logger.warn(f"Stream `{self.name}` is not available. Skipping...")
+                elif message[0].get("o:errorCode") == "USER_ERROR":
+                    # when user restricted using non-admin credentials
+                    setattr(self, "raise_on_http_errors", False)
+                    self.logger.warn(f"Stream `{self.name}`: reading an Admin record allowed for Admin Only.")
+                else:
+                    return super().should_retry(response)
+        return super().should_retry(response)   
 
-class BillingAccounts(NetsuiteStream):
-    """https://docs.oracle.com/en/cloud/saas/netsuite/ns-online-help/section_4779334000.html"""
+class IncrementalNetsuiteStream(NetsuiteStream, ABC):
     
-
-class Classifications(NetsuiteStream):
-    """https://docs.oracle.com/en/cloud/saas/netsuite/ns-online-help/section_N3741163.html"""
-
-
-class Departments(NetsuiteStream):
-    """https://docs.oracle.com/en/cloud/saas/netsuite/ns-online-help/section_N3742370.html"""
-
-
-class Currencies(NetsuiteStream):
-    """https://docs.oracle.com/en/cloud/saas/netsuite/ns-online-help/section_N3741577.html"""
+    @property
+    def cursor_field(self) -> str:
+        return "lastModifiedDate"
     
-    data_structure = "dict"
+    def filter_records_newer_than_state(
+        self, 
+        stream_state: Mapping[str, Any] = None, 
+        records: Mapping[str, Any] = None,
+    ) -> Iterable[Mapping[str, Any]]:
+        """Parse the records with respect to `stream_state` for `incremental` sync."""
+        if stream_state:
+            for record in records:
+                if record.get(self.cursor_field, "") >= stream_state.get(self.cursor_field):
+                    yield record
+        else:
+            yield from records
+
+    def parse_response(
+        self,
+        response: requests.Response,
+        stream_state: Mapping[str, Any],
+        stream_slice: Mapping[str, Any] = None,
+        next_page_token: Mapping[str, Any] = None,
+        **kwargs,
+    ) -> Iterable[Mapping]:
+        records = super().parse_response(response, stream_state, stream_slice, next_page_token)
+        yield from self.filter_records_newer_than_state(stream_state, records)
     
+    def get_updated_state(self, current_stream_state: MutableMapping[str, Any], latest_record: Mapping[str, Any]) -> Mapping[str, Any]:
+        latest_cursor = latest_record.get(self.cursor_field, "")
+        current_cursor = current_stream_state.get(self.cursor_field, "")
+        return {self.cursor_field: max(latest_cursor, current_cursor)}
 
-class Locations(NetsuiteStream):
-    """https://docs.oracle.com/en/cloud/saas/netsuite/ns-online-help/section_N3747661.html"""
-
-
-class VendorBills(NetsuiteStream):
-    """https://docs.oracle.com/en/cloud/saas/netsuite/ns-online-help/section_N3694535.html"""
-    
-    data_structure = "dict"
-    
-
-class Vendors(NetsuiteStream):
-    """https://docs.oracle.com/en/cloud/saas/netsuite/ns-online-help/section_N3767893.html"""
+    def request_params(self, stream_slice: Mapping[str, Any] = None, next_page_token: Mapping[str, Any] = None, **kwargs) -> MutableMapping[str, Any]:
+        params = {**(next_page_token or {})}
+        if stream_slice:
+            params.update(**{"q": f'{self.cursor_field} AFTER "{stream_slice["start"]}" AND {self.cursor_field} BEFORE "{stream_slice["end"]}"'})
+        return params
 
 
-class VendorPayments(NetsuiteStream):
-    """https://docs.oracle.com/en/cloud/saas/netsuite/ns-online-help/section_N3695867.html"""
-    
-    data_structure = "dict"
-    
+class CustomIncrementalNetsuiteStream(IncrementalNetsuiteStream):
 
-class Subsidiaries(NetsuiteStream):
-    """https://docs.oracle.com/en/cloud/saas/netsuite/ns-online-help/section_N3754626.html"""
-
-
-class JournalEntries(NetsuiteStream):
-    """https://docs.oracle.com/en/cloud/saas/netsuite/ns-online-help/section_N3683608.html"""
-    
-    data_structure = "dict"
-    
-
-class Employees(NetsuiteStream):
-    """https://docs.oracle.com/en/cloud/saas/netsuite/ns-online-help/preface_3714104131.html"""
-
-
-class ExpenseCategories(NetsuiteStream):
-    """https://docs.oracle.com/en/cloud/saas/netsuite/ns-online-help/section_N913978.html"""
-
-
-class ExpenseReports(NetsuiteStream):
-    """https://docs.oracle.com/en/cloud/saas/netsuite/ns-online-help/section_N908140.html"""
-    
-    data_structure = "dict"
-    
-
-class Folders(NetsuiteStream):
-    """https://docs.oracle.com/en/cloud/saas/netsuite/ns-online-help/section_N3784601.html"""
-
-
-class Files(NetsuiteStream):
-    """https://docs.oracle.com/en/cloud/saas/netsuite/ns-online-help/section_N3784054.html"""
-
-
-class Customers(NetsuiteStream):
-    """https://docs.oracle.com/en/cloud/saas/netsuite/ns-online-help/section_N3639940.html"""
-
-
-class Projects(NetsuiteStream):
-    """https://docs.oracle.com/en/cloud/saas/netsuite/ns-online-help/preface_3714107248.html"""
-
-
-class Terms(NetsuiteStream):
-    """https://docs.oracle.com/en/cloud/saas/netsuite/ns-online-help/section_N3767518.html"""
-
-
-class TaxItems(NetsuiteStream):
-    """https://docs.oracle.com/en/cloud/saas/netsuite/ns-online-help/section_N3755370.html"""
-
-
-class TaxGroups(NetsuiteStream):
-    """https://docs.oracle.com/en/cloud/saas/netsuite/ns-online-help/section_N3755021.html"""
-
+    @property
+    def cursor_field(self) -> str:
+        return "lastmodified"
