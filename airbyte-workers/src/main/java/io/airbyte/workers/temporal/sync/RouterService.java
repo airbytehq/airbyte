b36799916acc7244d659bc15643ae99a8c6c864f
Jonathan Pearlin <jonathan@airbyte.io>
2022-09-14 13:10:23 -0400

    Convert airbyte-workers to Micronaut (#16434)
    * WIP Convert airbyte-workers to Micronaut framework
    
    * Rebase cleanup
    
    * Fix broken tests
    
    * Simplify code
    
    * Support control vs data plane configuration
    
    * make WORFKLOW_PROXY_CACHE non-static to avoid cacheing mocks in unit tests
    
    * Formatting
    
    * Pairing on Worker Micronaut (#16364)
    
    * add RouteToSyncTaskQueue activity
    
    * use new route activity in connection manager workflow
    
    * format
    
    * call router service for task queue
    
    * Revert temporal proxy changes
    
    * Formatting
    
    * Fix default value
    
    * register new route activity in test
    
    * fix SyncWorkflowTest now that it isn't doing any routing
    
    * Update dependencies
    
    * More dependency updates
    
    * Update dependencies
    
    * Improve conditional bean check
    
    * Match existing Optional functionality
    
    * Add notEquals check
    
    * Add missing env var to Helm chart
    
    * Fix typo
    
    * Mark LogConfigs as Singleton
    
    * Env vars for log/state storage type
    
    * Remove use of Optional in bean declarations
    
    * Fix typo in config property name
    
    * Support Temporal Cloud namespace
    
    * Change to @Value
    
    * Use correct value for conditional check
    
    * Upgrade Micronaut
    
    * Fix merge conflict
    
    * Formatting
    
    * Add missing env var
    
    * Use sync task queue environment variable
    
    * Handle sync task queue as set
    
    * format and force http
    
    * Handle case where sync task queue is empty
    
    * Add correct path to config property
    
    * Remove unused import
    
    * Remove unused parameter
    
    * Formatting
    
    * Use pattern for condition process factory beans
    
    * Cleanup
    
    * PR feedback
    
    * Revert hack for testing
    
    Co-authored-by: pmossman <parker@airbyte.io>

--- airbyte-workers/src/main/java/io/airbyte/workers/temporal/sync/RouterService.java
+++ airbyte-workers/src/main/java/io/airbyte/workers/temporal/sync/RouterService.java
@@ -4,27 +4,45 @@
 
 package io.airbyte.workers.temporal.sync;
 
-import io.airbyte.config.Configs;
+import com.google.common.annotations.VisibleForTesting;
 import io.airbyte.workers.temporal.TemporalJobType;
+import io.micronaut.context.annotation.Value;
+import io.micronaut.core.util.StringUtils;
+import java.util.Arrays;
+import java.util.Set;
 import java.util.UUID;
-import lombok.AllArgsConstructor;
+import java.util.stream.Collectors;
+import javax.inject.Singleton;
 
-@AllArgsConstructor
+@Singleton
 public class RouterService {
 
-  private static final String MVP_DATA_PLANE_TASK_QUEUE = "MVP_DATA_PLANE";
+  static final String MVP_DATA_PLANE_TASK_QUEUE = "MVP_DATA_PLANE";
 
-  private final Configs configs;
+  @Value("${airbyte.data.plane.connection-ids-mvp}")
+  private String connectionIdsForMvpDataPlane;
 
   /**
    * For now, returns a Task Queue by checking to see if the connectionId is on the env var list for
    * usage in the MVP Data Plane. This will be replaced by a proper Router Service in the future.
    */
   public String getTaskQueue(final UUID connectionId) {
-    if (configs.connectionIdsForMvpDataPlane().contains(connectionId.toString())) {
-      return MVP_DATA_PLANE_TASK_QUEUE;
+    if (connectionId != null) {
+      if (getConnectionIdsForMvpDataPlane().contains(connectionId.toString())) {
+        return MVP_DATA_PLANE_TASK_QUEUE;
+      }
     }
     return TemporalJobType.SYNC.name();
+  }
+
+  private Set<String> getConnectionIdsForMvpDataPlane() {
+    return StringUtils.isNotEmpty(connectionIdsForMvpDataPlane) ? Arrays.stream(connectionIdsForMvpDataPlane.split(",")).collect(Collectors.toSet())
+        : Set.of();
+  }
+
+  @VisibleForTesting
+  void setConnectionIdsForMvpDataPlane(final String connectionIdsForMvpDataPlane) {
+    this.connectionIdsForMvpDataPlane = connectionIdsForMvpDataPlane;
   }
 
 }
