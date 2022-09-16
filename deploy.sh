bf1335451db8218d010e52ea0fca35058e18ff9a
Jeenu Thomas <jeenuthomas@Hogwarts.local>
2022-09-16 09:59:03 +0530

    Updating deploy script to include commcare

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,10 @@
+#!/usr/bin/env bash
+set -e
+
 # Build SurveyCTO connector image
 cd airbyte-integrations/connectors/source-surveycto && docker build . -t airbyte/source-surveycto:dev
 
 # Build Commcare connector image
-cd airbyte-integrations/connectors/source-commcare && docker build . -t airbyte/source-commcare:dev
+cd ../../../airbyte-integrations/connectors/source-commcare && docker build . -t airbyte/source-commcare:dev
 
-docker compose up -d
+cd ../../../ && docker compose up -d
