2b348f5da3f5423091a8c29a8a57c0470223c06a
Debanjan Ganguly <debanjan.ganguly.98@gmail.com>
2022-09-21 12:06:35 +0530

    fix(sendgrid): adds email as a part of the primary key for suppression group members (#22)

--- airbyte-integrations/connectors/source-sendgrid/source_sendgrid/streams.py
+++ airbyte-integrations/connectors/source-sendgrid/source_sendgrid/streams.py
@@ -243,7 +243,7 @@ class SuppressionGroups(SendgridStream):
 
 
 class SuppressionGroupMembers(SendgridStreamOffsetPagination):
-    primary_key = "group_id"
+    primary_key = ["group_id", "email"]
 
     def path(self, **kwargs) -> str:
         return "asm/suppressions"
