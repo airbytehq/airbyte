deb98f4a780ffc5c31a5caea1e2d09a3b21a17cb
Volodymyr Petrov <volodymyr.s.petrov@globallogic.com>
2022-09-23 00:01:49 +0300

    refactored WarningMessage component

--- airbyte-webapp/src/views/Connector/ServiceForm/components/WarningMessage.tsx
+++ airbyte-webapp/src/views/Connector/ServiceForm/components/WarningMessage.tsx
@@ -1,49 +1,34 @@
 import React from "react";
 import { FormattedMessage } from "react-intl";
-import styled from "styled-components";
+
+import { Text } from "components/base/Text";
 
 import { useConfig } from "config";
 import { ReleaseStage } from "core/request/AirbyteClient";
 
-const Content = styled.div`
-  padding: 13px 16px;
-  background: ${({ theme }) => theme.warningBackgroundColor};
-  border-radius: 8px;
-  font-size: 12px;
-  white-space: break-spaces;
-  margin-top: 16px;
-`;
-
-const Link = styled.a`
-  color: ${({ theme }) => theme.darkPrimaryColor};
-
-  &:hover,
-  &:focus {
-    color: ${({ theme }) => theme.darkPrimaryColor60};
-  }
-`;
+import styles from "./WarningMessage.module.scss";
 
 interface WarningMessageProps {
   stage: typeof ReleaseStage.alpha | typeof ReleaseStage.beta;
 }
 
-const WarningMessage: React.FC<WarningMessageProps> = ({ stage }) => {
+export const WarningMessage: React.FC<WarningMessageProps> = ({ stage }) => {
   const config = useConfig();
   return (
-    <Content>
-      <FormattedMessage id={`connector.releaseStage.${stage}.description`} />{" "}
-      <FormattedMessage
-        id="connector.connectorsInDevelopment.docLink"
-        values={{
-          lnk: (node: React.ReactNode) => (
-            <Link href={config.links.productReleaseStages} target="_blank" rel="noreferrer">
-              {node}
-            </Link>
-          ),
-        }}
-      />
-    </Content>
+    <div className={styles.container}>
+      <Text size="sm">
+        <FormattedMessage id={`connector.releaseStage.${stage}.description`} />{" "}
+        <FormattedMessage
+          id="connector.connectorsInDevelopment.docLink"
+          values={{
+            lnk: (node: React.ReactNode) => (
+              <a className={styles.link} href={config.links.productReleaseStages} target="_blank" rel="noreferrer">
+                {node}
+              </a>
+            ),
+          }}
+        />
+      </Text>
+    </div>
   );
 };
-
-export { WarningMessage };
