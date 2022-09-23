0d3091e7ff888a92bb9b660889447a9ffd18d5ae
David Yaffe <davidyaffe@davids-mbp.lan>
2022-09-23 17:05:44 -0400

    updating oauth for source: notion to fix airbyte bug

--- airbyte-integrations/connectors/source-notion/source_notion/source.py
+++ airbyte-integrations/connectors/source-notion/source_notion/source.py
@@ -25,11 +25,13 @@ class NotionAuthenticator:
             auth_type = credentials.get("auth_type")
             if auth_type == "OAuth2.0":
                 return TokenAuthenticator(credentials.get("access_token"))
-            return TokenAuthenticator(credentials.get("token"))
+            else:
+                return TokenAuthenticator(self.config.get("access_token"))
+ #           return TokenAuthenticator(credentials.get("token"))
 
         # support the old config
-        if "access_token" in self.config:
-            return TokenAuthenticator(self.config.get("access_token"))
+#       if "access_token" in self.config:
+#            return TokenAuthenticator(self.config.get("access_token"))
 
 
 class SourceNotion(AbstractSource):
