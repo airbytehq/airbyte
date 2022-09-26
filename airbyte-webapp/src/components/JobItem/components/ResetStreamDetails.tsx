9ff249a336873016aa30ec9a82b8322e3075f0ab
yatsukbogdan <yatsukbogdan@gmail.com>
2022-09-26 12:03:03 +0300

    Fixes ResetStreamDetails names prop

--- airbyte-webapp/src/components/JobItem/components/ResetStreamDetails.tsx
+++ airbyte-webapp/src/components/JobItem/components/ResetStreamDetails.tsx
@@ -6,11 +6,11 @@ import { Text } from "components/base/Text";
 import styles from "./ResetStreamDetails.module.scss";
 
 interface ResetStreamsDetailsProps {
-  names: string[];
+  names?: string[];
   isOpen?: boolean;
 }
 
-export const ResetStreamsDetails: React.FC<ResetStreamsDetailsProps> = ({ names, isOpen }) => (
+export const ResetStreamsDetails: React.FC<ResetStreamsDetailsProps> = ({ names = [], isOpen }) => (
   <Text as="p" size="sm" className={classNames(styles.textContainer, { [styles.open]: isOpen })}>
     {names.map((name) => (
       <span key={name} className={styles.text}>
