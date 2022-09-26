f0dcdef5622e87a42ac3f09e562919eade6c72f7
satish_chinthanippu <satish.chinthanippu@teradata.com>
2022-09-26 02:33:14 -0700

    Review comments addressed

--- airbyte-integrations/connectors/destination-teradata/src/main/java/io/airbyte/integrations/destination/teradata/TeradataSqlOperations.java
+++ airbyte-integrations/connectors/destination-teradata/src/main/java/io/airbyte/integrations/destination/teradata/TeradataSqlOperations.java
@@ -15,10 +15,14 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.sql.SQLException;
 import java.util.List;
-
+import io.airbyte.integrations.base.JavaBaseConstants;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class TeradataSqlOperations extends JdbcSqlOperations {
-    public TeradataSqlOperations() {
+private static final Logger LOGGER = LoggerFactory.getLogger(TeradataSqlOperations.class);   
+       
+	public TeradataSqlOperations() {
         super(new TeradataDataAdapter());
       }
 
@@ -51,6 +55,48 @@ public class TeradataSqlOperations extends JdbcSqlOperations {
         }
       }
     });
+  }
+
+
+   @Override
+  public void createSchemaIfNotExists(final JdbcDatabase database, final String schemaName) throws Exception {
+   try {
+       	  database.execute(String.format("CREATE DATABASE %s AS PERM = 1e9 SKEW = 10 PERCENT;", schemaName));
+   }  catch (SQLException e) {
+
+
+      if(e.getMessage().contains("already exists")) {
+        LOGGER.warn("Database " + schemaName + " already exists.");
+      }
+  }
+
+  
+  
+   }
+
+  @Override
+  public void createTableIfNotExists(final JdbcDatabase database, final String schemaName, final String tableName) throws SQLException {
+    database.execute(createTableQuery(database, schemaName, tableName));
+  }
+
+  @Override
+  public String createTableQuery(final JdbcDatabase database, final String schemaName, final String tableName) {
+    return String.format(
+	"CREATE SET TABLE %s.%s, FALLBACK ( \n"
+	+ "%s VARCHAR(25), \n"
+     	+ "%s VARCHAR(100), \n"
+	+ "%s DATE \n"
+	+ ");\n", 
+	schemaName, tableName, JavaBaseConstants.COLUMN_NAME_AB_ID, JavaBaseConstants.COLUMN_NAME_DATA, JavaBaseConstants.COLUMN_NAME_EMITTED_AT);	
+  }
+
+@Override
+  public void dropTableIfExists(final JdbcDatabase database, final String schemaName, final String tableName) throws SQLException {
+    database.execute(dropTableIfExistsQuery(schemaName, tableName));
+  }
+
+  private String dropTableIfExistsQuery(final String schemaName, final String tableName) {
+    return String.format("DROP TABLE  %s.%s;\n", schemaName, tableName);
   }
     
 }
