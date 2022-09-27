86cd3764876d0bdf5a9419cc2cf777922697d496
Edmundo Ruiz Ghanem <edmundo@airbyte.io>
2022-09-27 13:22:10 -0400

    Update Modal storybook with more content, use UI component Buttons

--- airbyte-webapp/src/components/ui/Modal/index.stories.tsx
+++ airbyte-webapp/src/components/ui/Modal/index.stories.tsx
@@ -1,5 +1,7 @@
 import { ComponentStory, ComponentMeta } from "@storybook/react";
 
+import { Button } from "components/base/Button";
+
 import { Modal, ModalBody, ModalFooter } from ".";
 
 export default {
@@ -19,8 +21,8 @@ const Template: ComponentStory<typeof Modal> = (args) => {
     <Modal {...args}>
       <ModalBody>{args.children}</ModalBody>
       <ModalFooter>
-        <button>Button 1</button>
-        <button>Button 2</button>
+        <Button>Lorem</Button>
+        <Button>Ipsum</Button>
       </ModalFooter>
     </Modal>
   );
@@ -28,6 +30,19 @@ const Template: ComponentStory<typeof Modal> = (args) => {
 
 export const Primary = Template.bind({});
 Primary.args = {
-  children: "Modal children go here.",
+  size: "md",
+  children: (
+    <div>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+        magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+        consequat.
+      </p>
+      <p>
+        Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur
+        sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      </p>
+    </div>
+  ),
   title: "Modal Title",
 };
