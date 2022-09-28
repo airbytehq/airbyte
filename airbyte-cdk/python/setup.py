f09c20d89f20da417f14350980c8c7208a2ed7b9
Pedro Lopez <pedroslopez@me.com>
2022-09-28 00:32:09 -0400

    bump version / update changelog

--- airbyte-cdk/python/setup.py
+++ airbyte-cdk/python/setup.py
@@ -15,7 +15,7 @@ README = (HERE / "README.md").read_text()
 
 setup(
     name="airbyte-cdk",
-    version="0.1.88",
+    version="0.1.89",
     description="A framework for writing Airbyte Connectors.",
     long_description=README,
     long_description_content_type="text/markdown",
