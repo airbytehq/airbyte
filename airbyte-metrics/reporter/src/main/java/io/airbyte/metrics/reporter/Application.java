f6ebcbd155a4275115e364f7202ff3599b63cfe0
Cole Snodgrass <cole@airbyte.io>
2022-09-29 11:03:16 -0700

    move event-listeners to EventListeners class

--- airbyte-metrics/reporter/src/main/java/io/airbyte/metrics/reporter/Application.java
+++ airbyte-metrics/reporter/src/main/java/io/airbyte/metrics/reporter/Application.java
@@ -4,48 +4,16 @@
 
 package io.airbyte.metrics.reporter;
 
-import io.micronaut.context.event.ApplicationEventListener;
 import io.micronaut.runtime.Micronaut;
-import io.micronaut.runtime.server.event.ServerStartupEvent;
-import jakarta.inject.Singleton;
-import java.lang.invoke.MethodHandles;
-import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 /**
  * Metric Reporter application.
  * <p>
  * Responsible for emitting metric information on a periodic basis.
  */
-@Singleton
-public class Application implements ApplicationEventListener<ServerStartupEvent> {
-
-  private static final Logger log = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
-  private final Emitter[] emitters;
-
-  public Application(final Emitter[] emitters) {
-    this.emitters = emitters;
-  }
+public class Application {
 
   public static void main(final String[] args) {
     Micronaut.run(Application.class, args);
-  }
-
-  /**
-   * Manually registers all the emitters to run.
-   *
-   * @param event the event to respond to, not utilized but required per the method signature
-   *              <p>
-   *              See https://guides.micronaut.io/latest/micronaut-scheduled-gradle-java.html for documentation.
-   */
-  @Override
-  public void onApplicationEvent(final ServerStartupEvent event) {
-    final var pollers = Executors.newScheduledThreadPool(emitters.length);
-    for (final var emitter : emitters) {
-      pollers.scheduleAtFixedRate(emitter::Emit, 0, emitter.getDuration().getSeconds(), TimeUnit.SECONDS);
-    }
-    log.info("registered {} emitters", emitters.length);
   }
 }
---
+++ airbyte-metrics/reporter/src/main/java/io/airbyte/metrics/reporter/EventListeners.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright (c) 2022 Airbyte, Inc., all rights reserved.
+ */
+
+package io.airbyte.metrics.reporter;
+
+import io.micronaut.runtime.event.ApplicationShutdownEvent;
+import io.micronaut.runtime.event.ApplicationStartupEvent;
+import io.micronaut.runtime.event.annotation.EventListener;
+import jakarta.inject.Singleton;
+import java.lang.invoke.MethodHandles;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * EventListeners registers event listeners for the startup and shutdown events from Micronaut.
+ */
+@Singleton
+public class EventListeners {
+
+  private static final Logger log = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+  private final Emitter[] emitters;
+  private final ScheduledExecutorService executor;
+
+  EventListeners(final Emitter[] emitters) {
+    this.emitters = emitters;
+    this.executor = Executors.newScheduledThreadPool(emitters.length);
+  }
+
+  /**
+   * Manually registers all the emitters to run on startup.
+   *
+   * @param event unused but required in order to listen to the startup event.
+   */
+  @EventListener
+  public void startEmitters(final ApplicationStartupEvent event) {
+    for (final var emitter : emitters) {
+      executor.scheduleAtFixedRate(emitter::Emit, 0, emitter.getDuration().getSeconds(), TimeUnit.SECONDS);
+    }
+    log.info("registered {} emitters", emitters.length);
+  }
+
+  /**
+   * Attempts to cleanly shutdown the running emitters
+   *
+   * @param event unused but required in order to listen to the shutdown event.
+   */
+  @EventListener
+  public void stopEmitters(final ApplicationShutdownEvent event) {
+    log.info("shutting down emitters");
+    executor.shutdown();
+  }
+
+}
