256901fbdf0ee048b564c9e3b530d3efcc09b3e6
sage-watterworth <watter.sage@gmail.com>
2022-09-30 16:21:51 -0400

    amazon ad status filter

--- airbyte-integrations/connectors/source-amazon-ads/unit_tests/test_report_streams.py
+++ airbyte-integrations/connectors/source-amazon-ads/unit_tests/test_report_streams.py
@@ -19,9 +19,12 @@ from requests.exceptions import ConnectionError
 from source_amazon_ads.schemas.profile import AccountInfo, Profile
 from source_amazon_ads.source import CONFIG_DATE_FORMAT
 from source_amazon_ads.streams import (
+    SponsoredBrandsCampaigns,
     SponsoredBrandsReportStream,
     SponsoredBrandsVideoReportStream,
+    SponsoredDisplayCampaigns,
     SponsoredDisplayReportStream,
+    SponsoredProductCampaigns,
     SponsoredProductsReportStream,
 )
 from source_amazon_ads.streams.report_streams.report_streams import ReportGenerationFailure, ReportGenerationInProgress, TooManyRequests
@@ -556,3 +559,42 @@ def test_read_incremental_with_records_start_date(config):
         records = list(read_incremental(stream, state))
         assert state == {"1": {"reportDate": "20210104"}}
         assert {r["reportDate"] for r in records} == {"20210103", "20210104", "20210105", "20210106"}
+
+
+@pytest.mark.parametrize("state_filter", [["enabled", "archived", "paused"], ["enabled"], None])
+def test_display_report_stream_campaign_state_filer(mocker, config, state_filter):
+    profiles = make_profiles()
+    mocker.patch.object(SponsoredDisplayCampaigns, "state_filter", new_callable=mocker.PropertyMock, return_value=state_filter)
+
+    stream = SponsoredDisplayCampaigns(config, profiles)
+    params = stream.request_params(stream_state=None, stream_slice=None, next_page_token=None)
+    if "stateFilter" in params:
+        assert params["stateFilter"] == ",".join(state_filter)
+    else:
+        assert state_filter is None
+
+
+@pytest.mark.parametrize("state_filter", [["enabled", "archived", "paused"], ["enabled"], None])
+def test_display_report_stream_product_campaign_state_filer(mocker, config, state_filter):
+    profiles = make_profiles()
+    mocker.patch.object(SponsoredProductCampaigns, "state_filter", new_callable=mocker.PropertyMock, return_value=state_filter)
+
+    stream = SponsoredProductCampaigns(config, profiles)
+    params = stream.request_params(stream_state=None, stream_slice=None, next_page_token=None)
+    if "stateFilter" in params:
+        assert params["stateFilter"] == ",".join(state_filter)
+    else:
+        assert state_filter is None
+
+
+@pytest.mark.parametrize("state_filter", [["enabled", "archived", "paused"], ["enabled"], None])
+def test_display_report_stream_brand_campaign_state_filer(mocker, config, state_filter):
+    profiles = make_profiles()
+    mocker.patch.object(SponsoredBrandsCampaigns, "state_filter", new_callable=mocker.PropertyMock, return_value=state_filter)
+
+    stream = SponsoredBrandsCampaigns(config, profiles)
+    params = stream.request_params(stream_state=None, stream_slice=None, next_page_token=None)
+    if "stateFilter" in params:
+        assert params["stateFilter"] == ",".join(state_filter)
+    else:
+        assert state_filter is None
