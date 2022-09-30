1b0f6d9e743d2fa697311354c012fa679910882f
Jimmy Ma <jimmy@airbyte.io>
2022-09-30 15:52:39 -0700

    Add versioned migrator

--- airbyte-commons-protocol/src/main/java/io/airbyte/commons/protocol/AirbyteMessageVersionedMigrator.java
+++ airbyte-commons-protocol/src/main/java/io/airbyte/commons/protocol/AirbyteMessageVersionedMigrator.java
@@ -1,2 +1,28 @@
-package io.airbyte.commons.protocol;public class AirbyteMessageVersionedMigrator {
+/*
+ * Copyright (c) 2022 Airbyte, Inc., all rights reserved.
+ */
+
+package io.airbyte.commons.protocol;
+
+import io.airbyte.commons.version.Version;
+import io.airbyte.protocol.models.v0.AirbyteMessage;
+import lombok.AllArgsConstructor;
+
+/**
+ * Wraps message migration from a fixed version to the most recent version
+ */
+@AllArgsConstructor
+public class AirbyteMessageVersionedMigrator<OriginalMessageType> {
+
+  private final AirbyteMessageMigrator migrator;
+  private final Version version;
+
+  public OriginalMessageType downgrade(final AirbyteMessage message) {
+    return migrator.downgrade(message, version);
+  }
+
+  public AirbyteMessage upgrade(final OriginalMessageType message) {
+    return migrator.upgrade(message, version);
+  }
+
 }
