90806ec416ddf4ac64981f4d547d77c8566700a7
Cole Snodgrass <cole@airbyte.io>
2022-09-30 13:55:07 -0700

    remove unused singleton

--- airbyte-metrics/reporter/src/main/java/io/airbyte/metrics/reporter/ReporterFactory.java
+++ airbyte-metrics/reporter/src/main/java/io/airbyte/metrics/reporter/ReporterFactory.java
@@ -4,17 +4,13 @@
 
 package io.airbyte.metrics.reporter;
 
-import io.airbyte.db.Database;
 import io.airbyte.metrics.lib.MetricClient;
 import io.airbyte.metrics.lib.MetricClientFactory;
 import io.micronaut.context.annotation.Factory;
-import jakarta.inject.Named;
 import jakarta.inject.Singleton;
-import org.jooq.DSLContext;
 
 /**
- * Micronaut factory for creating the appropriate singletons utilized by the metric reporter
- * service.
+ * Micronaut factory for creating the appropriate singletons utilized by the metric reporter service.
  */
 @Factory
 class ReporterFactory {
@@ -23,10 +19,5 @@ class ReporterFactory {
   public MetricClient metricClient() {
     return MetricClientFactory.getMetricClient();
   }
-
-  @Singleton
-  public Database database(@Named("config") final DSLContext context) {
-    return new Database(context);
-  }
-
+  
 }
