a975a8f7aa0dc4e58add756efd81a700fec15df3
Edward Gao <edward.gao@airbyte.io>
2022-09-30 11:04:04 -0700

    cloud should remove insecure protocols

--- airbyte-integrations/connectors/destination-kafka/src/main/java/io/airbyte/integrations/destination/kafka/KafkaStrictEncryptDestination.java
+++ airbyte-integrations/connectors/destination-kafka/src/main/java/io/airbyte/integrations/destination/kafka/KafkaStrictEncryptDestination.java
@@ -1,23 +1,68 @@
 package io.airbyte.integrations.destination.kafka;
 
 import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import io.airbyte.commons.json.Jsons;
+import io.airbyte.integrations.base.spec_modification.SpecModifyingDestination;
 import io.airbyte.protocol.models.AirbyteConnectionStatus;
 import io.airbyte.protocol.models.AirbyteConnectionStatus.Status;
+import io.airbyte.protocol.models.ConnectorSpecification;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Set;
 
-public class KafkaStrictEncryptDestination extends KafkaDestination {
+public class KafkaStrictEncryptDestination extends SpecModifyingDestination {
 
-  private static final Set<String> SECURE_PROTOCOLS = Set.of(KafkaProtocol.SASL_SSL.name());
+  private static final ObjectMapper MAPPER = new ObjectMapper();
+  private static final Set<String> SECURE_KAFKA_PROTOCOLS = Set.of(KafkaProtocol.SASL_SSL.name());
+  private static final Set<String> SECURE_PROTOCOLS = Set.of("SSL");
+
+  public KafkaStrictEncryptDestination() {
+    super(new KafkaDestination());
+  }
 
   @Override
-  public AirbyteConnectionStatus check(final JsonNode config) {
-    // TODO after implementing https://github.com/airbytehq/airbyte/issues/17356, check that either SSH tunnel is enabled OR SASL SSL is enabled
-    if (!SECURE_PROTOCOLS.contains(config.get("protocol").get("security_protocol").asText())) {
-      // Fail immediately if connection is unsecured. We don't run the super.check() because we don't want to send any traffic at all in this case.
+  public ConnectorSpecification modifySpec(ConnectorSpecification originalSpec) {
+    final ConnectorSpecification spec = Jsons.clone(originalSpec);
+    ObjectNode protocolConfigNode = (ObjectNode) spec.getConnectionSpecification().get("properties").get("protocol");
+
+    ArrayNode protocols = (ArrayNode) protocolConfigNode.get("oneOf");
+    ArrayNode filteredProtocols = MAPPER.createArrayNode();
+    for (JsonNode protocol : protocols) {
+      if (SECURE_KAFKA_PROTOCOLS.contains(protocol.get("properties").get("security_protocol").get("default").asText())) {
+        filteredProtocols.add(protocol);
+      }
+    }
+
+    protocolConfigNode.set("oneOf", filteredProtocols);
+    return spec;
+  }
+
+  @Override
+  public AirbyteConnectionStatus check(final JsonNode config) throws Exception {
+    // Bootstrap servers may specify a protocol. For example, these are both valid configs:
+    //   foo.com:1234,bar.com:1234,baz.com:1234
+    //   PLAINTEXT://foo.com:1234,SSL://bar.com:1234,baz.com:1234
+    // If the connection protocol is specified, we should require it to be SSL.
+    String[] bootstrapServers = config.get("bootstrap_servers").asText().split(",");
+    List<String> unsecuredServers = new ArrayList<>();
+    for (String bootstrapServer : bootstrapServers) {
+      int protocolSeparatorIndex = bootstrapServer.indexOf("://");
+      if (protocolSeparatorIndex != -1) {
+        String protocol = bootstrapServer.substring(0, protocolSeparatorIndex);
+        if (!SECURE_PROTOCOLS.contains(protocol)) {
+          unsecuredServers.add(bootstrapServer);
+        }
+      }
+    }
+    if (!unsecuredServers.isEmpty()) {
       return new AirbyteConnectionStatus()
           .withStatus(Status.FAILED)
-          .withMessage("Unsecured connection not allowed.");
+          .withMessage("Unsecured connection to bootstrap servers is not allowed. These servers specify an insecure connection protocol: " + unsecuredServers);
     }
+
     return super.check(config);
   }
 }
--- airbyte-integrations/connectors/destination-kafka/src/test/java/io/airbyte/integrations/destination/kafka/KafkaStrictEncryptDestinationTest.java
+++ airbyte-integrations/connectors/destination-kafka/src/test/java/io/airbyte/integrations/destination/kafka/KafkaStrictEncryptDestinationTest.java
@@ -3,8 +3,11 @@ package io.airbyte.integrations.destination.kafka;
 import static org.junit.jupiter.api.Assertions.*;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import io.airbyte.commons.json.Jsons;
+import io.airbyte.commons.resources.MoreResources;
 import io.airbyte.protocol.models.AirbyteConnectionStatus;
 import io.airbyte.protocol.models.AirbyteConnectionStatus.Status;
+import io.airbyte.protocol.models.ConnectorSpecification;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -14,20 +17,25 @@ public class KafkaStrictEncryptDestinationTest {
   private static final Logger LOGGER = LoggerFactory.getLogger(KafkaStrictEncryptDestinationTest.class);
 
   @Test
+  void testGetSpec() throws Exception {
+    LOGGER.info("spec was {}", new KafkaStrictEncryptDestination().spec().getConnectionSpecification());
+    assertEquals(Jsons.deserialize(MoreResources.readResource("expected_strict_encrypt_spec.json"), ConnectorSpecification.class),
+        new KafkaStrictEncryptDestination().spec());
+  }
+
+  @Test
   void check_should_failOnInsecureConfig() throws Exception {
     ObjectMapper mapper = new ObjectMapper();
     AirbyteConnectionStatus checkResult = new KafkaStrictEncryptDestination().check(mapper.readTree(
         """
             {
-              "protocol": {
-                "security_protocol": "SASL_PLAINTEXT"
-              }
+              "bootstrap_servers": "PLAINTEXT://localhost:56789,SSL://localhost:67890"
             }
             """
     ));
     LOGGER.info("Check result was {}", checkResult);
     assertEquals(Status.FAILED, checkResult.getStatus());
-    assertEquals("Unsecured connection not allowed.", checkResult.getMessage());
+    assertEquals("Unsecured connection to bootstrap servers is not allowed. These servers specify an insecure connection protocol: [PLAINTEXT://localhost:56789]", checkResult.getMessage());
   }
 
   @Test
@@ -38,9 +46,7 @@ public class KafkaStrictEncryptDestinationTest {
     AirbyteConnectionStatus checkResult = new KafkaStrictEncryptDestination().check(mapper.readTree(
         """
             {
-              "protocol": {
-                "security_protocol": "SASL_SSL"
-              }
+              "bootstrap_servers": "SSL://localhost:67890,localhost:78901"
             }
             """
     ));
