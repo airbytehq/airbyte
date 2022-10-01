25739798fab1b9fedc82efe09b2abd14e8c4c639
root <root@sdl63613.labs.teradata.com>
2022-09-30 21:12:37 -0700

    connection to teradata fialing. fixed the issue

--- airbyte-integrations/connectors/destination-teradata/src/main/java/io/airbyte/integrations/destination/teradata/TeradataDestination.java
+++ airbyte-integrations/connectors/destination-teradata/src/main/java/io/airbyte/integrations/destination/teradata/TeradataDestination.java
@@ -1,15 +1,11 @@
 /*
- * Copyright (c) 2022 Airbyte, Inc., all rights reserved.
+ * Copyright (c) 2021 Airbyte, Inc., all rights reserved.
  */
 
 package io.airbyte.integrations.destination.teradata;
 
 import com.fasterxml.jackson.databind.JsonNode;
-import com.google.common.collect.ImmutableMap;
-import io.airbyte.commons.json.Jsons;
-import io.airbyte.db.factory.DatabaseDriver;
-import io.airbyte.db.jdbc.JdbcUtils;
-import io.airbyte.integrations.BaseConnector;
+import io.airbyte.integrations.destination.jdbc.AbstractJdbcDestination;
 import io.airbyte.integrations.base.AirbyteMessageConsumer;
 import io.airbyte.integrations.base.Destination;
 import io.airbyte.integrations.base.IntegrationRunner;
@@ -17,24 +13,32 @@ import io.airbyte.protocol.models.AirbyteConnectionStatus;
 import io.airbyte.protocol.models.AirbyteMessage;
 import io.airbyte.protocol.models.ConfiguredAirbyteCatalog;
 import java.util.function.Consumer;
-import io.airbyte.integrations.destination.jdbc.AbstractJdbcDestination;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Optional;
-import java.util.Collections;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import io.airbyte.integrations.destination.ExtendedNameTransformer;
+import java.util.Map;
+import java.util.Optional;
+import java.util.List;
+import java.util.Collections;
+import io.airbyte.commons.json.Jsons;
+import com.google.common.collect.ImmutableMap;
+import io.airbyte.db.factory.DatabaseDriver;
 
 public class TeradataDestination extends AbstractJdbcDestination  implements Destination {
 
   private static final Logger LOGGER = LoggerFactory.getLogger(TeradataDestination.class);
-  public static final String DRIVER_CLASS = DatabaseDriver.TERADATA.getDriverClassName();
-  public static final String PUBLIC_KEY = "public";
+  public static final String DRIVER_CLASS = "com.teradata.jdbc.TeraDriver";
+  public static final String DATABASE_KEY = "database";
+  public static final String JDBC_URL_KEY = "jdbc_url";
+  public static final String JDBC_URL_PARAMS_KEY = "jdbc_url_params";
+  public static final String PASSWORD_KEY = "password";
+  public static final String USERNAME_KEY = "username";
+  public static final String SCHEMA_KEY = "schema";
 
 
   public static void main(String[] args) throws Exception {
-    new IntegrationRunner(new TeradataDestination()).run(args);
+   	System.out.println("Satish : main args" + args);
+	  new IntegrationRunner(new TeradataDestination()).run(args);
   }
 
   public TeradataDestination() {
@@ -47,26 +51,29 @@ public class TeradataDestination extends AbstractJdbcDestination  implements Des
   }
   @Override
   public JsonNode toJdbcConfig(final JsonNode config) {
-    final String schema = Optional.ofNullable(config.get(JdbcUtils.SCHEMA_KEY)).map(JsonNode::asText).orElse(PUBLIC_KEY);
+    final String schema = Optional.ofNullable(config.get("schema")).map(JsonNode::asText).orElse("public");
+    System.out.println("Satish: schema" + schema);
+    System.out.println("Satish: config" + config);
 
-    final String jdbcUrl = String.format("jdbc:teradata://%s?",
-        config.get(JdbcUtils.HOST_KEY).asText());
+    final String jdbcUrl = String.format("jdbc:teradata://%s/",
+        config.get("host").asText());
 
     final ImmutableMap.Builder<Object, Object> configBuilder = ImmutableMap.builder()
-        .put(JdbcUtils.USERNAME_KEY, config.get(JdbcUtils.USERNAME_KEY).asText())
-        .put(JdbcUtils.JDBC_URL_KEY, jdbcUrl)
-        .put(JdbcUtils.SCHEMA_KEY, schema);
+        .put(USERNAME_KEY, config.get(USERNAME_KEY).asText())
+        .put(JDBC_URL_KEY, jdbcUrl)
+        .put(SCHEMA_KEY, schema);
 
-    if (config.has(JdbcUtils.PASSWORD_KEY)) {
-      configBuilder.put(JdbcUtils.PASSWORD_KEY, config.get(JdbcUtils.PASSWORD_KEY).asText());
+    if (config.has(PASSWORD_KEY)) {
+      configBuilder.put(PASSWORD_KEY, config.get(PASSWORD_KEY).asText());
     }
 
-    if (config.has(JdbcUtils.JDBC_URL_PARAMS_KEY)) {
-      configBuilder.put(JdbcUtils.JDBC_URL_PARAMS_KEY, config.get(JdbcUtils.JDBC_URL_PARAMS_KEY).asText());
+    if (config.has(JDBC_URL_PARAMS_KEY)) {
+      configBuilder.put(JDBC_URL_PARAMS_KEY, config.get(JDBC_URL_PARAMS_KEY).asText());
     }
-
+     System.out.println("JDBC Config: " + configBuilder.build());
     return Jsons.jsonNode(configBuilder.build());
   }
-  
+
 
 }
+
