c20fb7c7eacd5d257eea43f1e4988d83e5bf7751
Volodymyr Petrov <volodymyr.s.petrov@globallogic.com>
2022-10-03 18:02:17 +0300

    - refactor FrequentlyUsedDestinations component - update tests - updates storybook

--- airbyte-webapp/src/views/Connector/ServiceForm/components/FrequentlyUsedDestinations/FrequentlyUsedDestinations.tsx
+++ airbyte-webapp/src/views/Connector/ServiceForm/components/FrequentlyUsedDestinations/FrequentlyUsedDestinations.tsx
@@ -1,52 +1,50 @@
-import { useField } from "formik";
-import React from "react";
-import { useIntl } from "react-intl";
+import React, { useMemo } from "react";
 
-import { ConnectorCard } from "components";
-import { SlickSlider } from "components/ui/SlickSlider";
-import { Spinner } from "components/ui/Spinner";
+import { ConnectorDefinition } from "core/domain/connector";
+import { isDestinationDefinition } from "core/domain/connector/destination";
+import { useExperiment } from "hooks/services/Experiment";
+import { DestinationDefinitionReadWithLatestTag } from "services/connector/DestinationDefinitionService";
 
 import { DestinationConnectorCard } from "../../types";
-import styles from "./FrequentlyUsedDestinations.module.scss";
+import { FrequentlyUsedDestinationsCard } from "./FrequentlyUsedDestinationsCard";
 
-export interface FrequentlyUsedDestinationsProps {
-  propertyPath: string;
-  destinations: DestinationConnectorCard[];
-  onDestinationSelect?: (id: string) => void;
+interface FrequentlyUsedDestinationsProps {
+  availableServices: ConnectorDefinition[];
+  onServiceSelect: ((id: string) => void) | undefined;
   isLoading?: boolean;
 }
 
 export const FrequentlyUsedDestinations: React.FC<FrequentlyUsedDestinationsProps> = ({
-  propertyPath,
-  destinations,
-  onDestinationSelect,
+  availableServices,
+  onServiceSelect,
   isLoading,
 }) => {
-  const [, , { setValue }] = useField(propertyPath);
-  const { formatMessage } = useIntl();
+  const frequentlyUsedDestinationIds = useExperiment("connector.frequentlyUsedDestinationIds", [
+    "22f6c74f-5699-40ff-833c-4a879ea40133",
+    "424892c4-daac-4491-b35d-c6688ba547ba",
+  ]);
 
-  if (!destinations?.length) {
-    return null;
-  }
-  const onSlideClick = (id: string) => {
-    setValue(id);
-    onDestinationSelect?.(id);
-  };
+  const frequentlyUsedDestinations: DestinationConnectorCard[] = useMemo(
+    () =>
+      availableServices
+        .filter(
+          (service): service is DestinationDefinitionReadWithLatestTag =>
+            isDestinationDefinition(service) && frequentlyUsedDestinationIds.includes(service.destinationDefinitionId)
+        )
+        .map(({ destinationDefinitionId, name, icon, releaseStage }) => ({
+          destinationDefinitionId,
+          name,
+          icon,
+          releaseStage,
+        })),
+    [availableServices, frequentlyUsedDestinationIds]
+  );
+
   return (
-    <div className={styles.container}>
-      {isLoading ? (
-        <div className={styles.spinnerContainer}>
-          <Spinner small />
-        </div>
-      ) : (
-        <SlickSlider title={formatMessage({ id: "destinations.frequentlyUsed" })}>
-          {destinations.map(({ destinationDefinitionId, name, icon, releaseStage }, index) => (
-            <button key={index} className={styles.card} onClick={() => onSlideClick(destinationDefinitionId)}>
-              <ConnectorCard connectionName={name} icon={icon} releaseStage={releaseStage} fullWidth />
-            </button>
-          ))}
-        </SlickSlider>
-      )}
-    </div>
+    <FrequentlyUsedDestinationsCard
+      destinations={frequentlyUsedDestinations}
+      onDestinationSelect={onServiceSelect}
+      isLoading={isLoading}
+    />
   );
 };
