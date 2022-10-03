11fa81d041e05bb548daff4881d3caba36f97855
Tim Roes <tim@airbyte.io>
2022-10-03 17:43:14 +0200

    :window: :wrench: Add additional eslint rules (#17348)
    * Add additional eslint rules
    
    * Add comment
    
    * Restore async await act
    
    * Fix broken gradle reference

--- airbyte-webapp/src/pages/DestinationPage/pages/CreateDestinationPage/components/DestinationForm.tsx
+++ airbyte-webapp/src/pages/DestinationPage/pages/CreateDestinationPage/components/DestinationForm.tsx
@@ -73,7 +73,7 @@ export const DestinationForm: React.FC<DestinationFormProps> = ({
   };
 
   const onSubmitForm = async (values: { name: string; serviceType: string }) => {
-    await onSubmit({
+    onSubmit({
       ...values,
       destinationDefinitionId: destinationDefinitionSpecification?.destinationDefinitionId,
     });
