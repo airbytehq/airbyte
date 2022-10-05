a78da1294af600ed16817eab6818ac155a9d2cfd
Vincent Koc <koconder@users.noreply.github.com>
2022-10-05 23:50:47 +1100

    finalise to full refresh for now

--- airbyte-integrations/connectors/source-genesys/source_genesys/source.py
+++ airbyte-integrations/connectors/source-genesys/source_genesys/source.py
@@ -27,8 +27,7 @@ class GenesysStream(HttpStream, ABC):
 
         if response_json.get("nextUri"):
             next_query_string = urllib.parse.urlsplit(response_json.get("nextUri")).query
-            params = dict(urllib.parse.parse_qsl(next_query_string))
-            return params
+            return dict(urllib.parse.parse_qsl(next_query_string))
 
     def request_params(
         self,
@@ -218,33 +217,7 @@ class UserGroups(GenesysStream):
     # next: pageNumber
     def path(self, **kwargs) -> str:
         return "groups"
-
-# class IncrementalGenesysStream(GenesysStream, ABC):
-#     """
-#     TODO fill in details of this class to implement functionality related to incremental syncs for your connector.
-#          if you do not need to implement incremental sync for any streams, remove this class.
-#     """
-
-#     # TODO: Fill in to checkpoint stream reads after N records. This prevents re-reading of data if the stream fails for any reason.
-#     state_checkpoint_interval = None
-
-#     @property
-#     def cursor_field(self) -> str:
-#         """
-#         TODO
-#         Override to return the cursor field used by this stream e.g: an API entity might always use created_at as the cursor field. This is
-#         usually id or date based. This field's presence tells the framework this in an incremental stream. Required for incremental.
-
-#         :return str: The name of the cursor field.
-#         """
-#         return []
-
-#     def get_updated_state(self, current_stream_state: MutableMapping[str, Any], latest_record: Mapping[str, Any]) -> Mapping[str, Any]:
-#         """
-#         Override to determine the latest state after reading the latest record. This typically compared the cursor_field from the latest record and
-#         the current state and picks the 'most' recent cursor. This is how a stream's state is determined. Required for incremental.
-#         """
-#         return {}
+        
 class SourceGenesys(AbstractSource): 
 
     @staticmethod
