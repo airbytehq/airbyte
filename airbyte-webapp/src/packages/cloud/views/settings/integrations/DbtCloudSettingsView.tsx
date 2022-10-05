578113b320456eddebd3d225e9bc5713232f5412
Alex Birdsall <ambirdsall@gmail.com>
2022-10-05 13:02:50 -0700

    Add CollapsablePanel around url input, MVP styling
    To get the styling to work, I needed to edit `LabeledInput` to accept
    a `className` prop, so I could give it contextually-specific styling.

--- airbyte-webapp/src/packages/cloud/views/settings/integrations/DbtCloudSettingsView.tsx
+++ airbyte-webapp/src/packages/cloud/views/settings/integrations/DbtCloudSettingsView.tsx
@@ -1,11 +1,23 @@
-import { Field, FieldProps, Form, Formik } from "formik";
+import { Field, FieldInputProps, FieldProps, Form, Formik } from "formik";
 import React from "react";
 import { FormattedMessage } from "react-intl";
 
 import { LabeledInput } from "components/LabeledInput";
 import { Button } from "components/ui/Button";
+import { CollapsablePanel } from "components/ui/CollapsablePanel";
 
 import { Content, SettingsCard } from "pages/SettingsPage/pages/SettingsComponents";
+
+import styles from "./DbtCloudSettingsView.module.scss";
+
+const singleTenantUrlInput = (fieldProps: FieldInputProps<string>) => (
+  <LabeledInput
+    {...fieldProps}
+    className={styles.singleTenantUrlInput}
+    label={<FormattedMessage id="settings.integrationSettings.dbtCloudSettings.form.singleTenantUrl" />}
+    type="text"
+  />
+);
 
 export const DbtCloudSettingsView: React.FC = () => (
   <SettingsCard title={<FormattedMessage id="settings.integrationSettings.dbtCloudSettings" />}>
@@ -29,19 +41,24 @@ export const DbtCloudSettingsView: React.FC = () => (
           </Field>
           <Field name="singleTenantUrl">
             {({ field }: FieldProps<string>) => (
-              <LabeledInput
-                {...field}
-                label={<FormattedMessage id="settings.integrationSettings.dbtCloudSettings.form.singleTenantUrl" />}
-                type="text"
-              />
+              <CollapsablePanel
+                className={styles.advancedOptions}
+                closedClassName={styles.advancedOptionsClosed}
+                openClassName={styles.advancedOptionsOpen}
+                drawer={singleTenantUrlInput(field)}
+              >
+                <FormattedMessage id="settings.integrationSettings.dbtCloudSettings.form.advancedOptions" />
+              </CollapsablePanel>
             )}
           </Field>
-          <Button variant="secondary">
-            <FormattedMessage id="settings.integrationSettings.dbtCloudSettings.form.testConnection" />
-          </Button>
-          <Button variant="primary" type="submit">
-            <FormattedMessage id="settings.integrationSettings.dbtCloudSettings.form.submit" />
-          </Button>
+          <div className={styles.controlGroup}>
+            <Button variant="secondary" className={styles.button}>
+              <FormattedMessage id="settings.integrationSettings.dbtCloudSettings.form.testConnection" />
+            </Button>
+            <Button variant="primary" type="submit" className={styles.button}>
+              <FormattedMessage id="settings.integrationSettings.dbtCloudSettings.form.submit" />
+            </Button>
+          </div>
         </Form>
       </Formik>
     </Content>
