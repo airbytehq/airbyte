62c68690a14c9c0a547a286893e29019b6074113
Arsen Losenko <20901439+arsenlosenko@users.noreply.github.com>
2022-10-05 20:26:53 +0300

    Source Recharge: skip stream if 403 received (#17608)
    * Source Recharge: skip stream if 403 received
    
    * Refactor should_retry method
    
    * Add logging message for clarity
    
    * Update changelog
    
    * auto-bump connector version [ci skip]
    
    Co-authored-by: Octavia Squidington III <octavia-squidington-iii@users.noreply.github.com>

--- airbyte-integrations/connectors/source-recharge/source_recharge/api.py
+++ airbyte-integrations/connectors/source-recharge/source_recharge/api.py
@@ -19,6 +19,7 @@ class RechargeStream(HttpStream, ABC):
 
     limit = 250
     page_num = 1
+    raise_on_http_errors = True
 
     # regestring the default schema transformation
     transformer: TypeTransformer = TypeTransformer(TransformConfig.DefaultSchemaNormalization)
@@ -61,13 +62,18 @@ class RechargeStream(HttpStream, ABC):
             return [response_data]
 
     def should_retry(self, response: requests.Response) -> bool:
-        res = super().should_retry(response)
-        if res:
-            return res
-
-        # For some reason, successful responses contains incomplete data
         content_length = int(response.headers.get("Content-Length", 0))
-        return response.status_code == 200 and content_length > len(response.content)
+        incomplete_data_response = response.status_code == 200 and content_length > len(response.content)
+        forbidden_error = isinstance(response.json(), dict) and response.status_code == requests.codes.FORBIDDEN
+
+        if incomplete_data_response:
+            return True
+        elif forbidden_error:
+            setattr(self, "raise_on_http_errors", False)
+            self.logger.error(f"Skiping stream {self.name} because of a 403 error.")
+            return False
+
+        return super().should_retry(response)
 
 
 class IncrementalRechargeStream(RechargeStream, ABC):
