318c0a0c06d378e53422455ef231f2e4c9968af7
Volodymyr Petrov <volodymyr.s.petrov@globallogic.com>
2022-10-05 12:02:26 +0300

    - refactor ConnectorCard component

--- airbyte-webapp/src/views/Connector/ConnectorCard/ConnectorCard.tsx
+++ airbyte-webapp/src/views/Connector/ConnectorCard/ConnectorCard.tsx
@@ -5,7 +5,7 @@ import { JobItem } from "components/JobItem/JobItem";
 import { Card } from "components/ui/Card";
 
 import { Action, Namespace } from "core/analytics";
-import { Connector, ConnectorT } from "core/domain/connector";
+import { Connector, ConnectorSpecification, ConnectorT } from "core/domain/connector";
 import { SynchronousJobRead } from "core/request/AirbyteClient";
 import { LogsRequestError } from "core/request/LogsRequestError";
 import { useAnalyticsService } from "hooks/services/Analytics";
@@ -13,6 +13,8 @@ import { useAdvancedModeSetting } from "hooks/services/useAdvancedModeSetting";
 import { generateMessageFromError } from "utils/errorStatusMessage";
 import { ServiceForm, ServiceFormProps, ServiceFormValues } from "views/Connector/ServiceForm";
 
+import { ConnectorServiceTypeControl } from "../ServiceForm/components/Controls/ConnectorServiceTypeControl";
+import styles from "./ConnectorCard.module.scss";
 import { useTestConnector } from "./useTestConnector";
 
 type ConnectorCardProvidedProps = Omit<
@@ -24,6 +26,8 @@ interface ConnectorCardBaseProps extends ConnectorCardProvidedProps {
   title?: React.ReactNode;
   full?: boolean;
   jobInfo?: SynchronousJobRead | null;
+  additionalDropdownComponent?: React.ReactNode;
+  intermediateComponent?: React.ReactNode;
 }
 
 interface ConnectorCardCreateProps extends ConnectorCardBaseProps {
@@ -40,6 +44,8 @@ export const ConnectorCard: React.FC<ConnectorCardCreateProps | ConnectorCardEdi
   full,
   jobInfo,
   onSubmit,
+  additionalDropdownComponent,
+  intermediateComponent,
   ...props
 }) => {
   const [saved, setSaved] = useState(false);
@@ -98,21 +104,40 @@ export const ConnectorCard: React.FC<ConnectorCardCreateProps | ConnectorCardEdi
 
   const job = jobInfo || LogsRequestError.extractJobInfo(errorStatusRequest);
 
+  const { selectedConnectorDefinitionSpecification, onServiceSelect, availableServices, isEditMode } = props;
+  const selectedConnectorDefinitionSpecificationId =
+    selectedConnectorDefinitionSpecification && ConnectorSpecification.id(selectedConnectorDefinitionSpecification);
+
   return (
-    <Card title={title} fullWidth={full}>
-      <ServiceForm
-        {...props}
-        errorMessage={props.errorMessage || (error && generateMessageFromError(error))}
-        isTestConnectionInProgress={isTestConnectionInProgress}
-        onStopTesting={onStopTesting}
-        testConnector={testConnector}
-        onSubmit={onHandleSubmit}
-        successMessage={
-          props.successMessage || (saved && props.isEditMode && <FormattedMessage id="form.changesSaved" />)
-        }
-      />
-      {/* Show the job log only if advanced mode is turned on or the actual job failed (not the check inside the job) */}
-      {job && (advancedMode || !job.succeeded) && <JobItem job={job} />}
-    </Card>
+    <>
+      <Card title={title} fullWidth={full} className={styles.serviceTypeSelectCard}>
+        <div className={styles.dropdownContainer}>
+          <ConnectorServiceTypeControl
+            formType={props.formType}
+            onChangeServiceType={onServiceSelect}
+            availableServices={availableServices}
+            isEditMode={isEditMode}
+            selectedServiceId={selectedConnectorDefinitionSpecificationId}
+          />
+          {additionalDropdownComponent}
+        </div>
+      </Card>
+      {intermediateComponent}
+      <Card>
+        <ServiceForm
+          {...props}
+          errorMessage={props.errorMessage || (error && generateMessageFromError(error))}
+          isTestConnectionInProgress={isTestConnectionInProgress}
+          onStopTesting={onStopTesting}
+          testConnector={testConnector}
+          onSubmit={onHandleSubmit}
+          successMessage={
+            props.successMessage || (saved && props.isEditMode && <FormattedMessage id="form.changesSaved" />)
+          }
+        />
+          {/* Show the job log only if advanced mode is turned on or the actual job failed (not the check inside the job) */}
+          {job && (advancedMode || !job.succeeded) && <JobItem job={job} />}
+      </Card>
+    </>
   );
 };
