d77dfb3d064574e675bd855b4a407ba4ae88096b
Alexandre Girard <alexandre@airbyte.io>
2022-10-06 15:01:58 -0700

    🐛 : Add missing component types to the types registry (#17700)
    * Add page increment to type registry
    
    * Add page increment to type registry

--- airbyte-cdk/python/setup.py
+++ airbyte-cdk/python/setup.py
@@ -15,7 +15,7 @@ README = (HERE / "README.md").read_text()
 
 setup(
     name="airbyte-cdk",
-    version="0.1.94",
+    version="0.1.95",
     description="A framework for writing Airbyte Connectors.",
     long_description=README,
     long_description_content_type="text/markdown",
