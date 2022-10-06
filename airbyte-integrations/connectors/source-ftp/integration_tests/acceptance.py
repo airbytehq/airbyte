c9b9b4631267844965bcce853ad033b97a19db43
Henri Blancke <blanckehenri@gmail.com>
2022-10-06 15:21:20 -0400

    [UPD] clean up crew
    Signed-off-by: Henri Blancke <blanckehenri@gmail.com>

--- airbyte-integrations/connectors/source-ftp/integration_tests/acceptance.py
+++ airbyte-integrations/connectors/source-ftp/integration_tests/acceptance.py
@@ -10,20 +10,9 @@ def connector_setup():
     docker_client = docker.from_env()
     dir_path = os.getcwd() + "/integration_tests"
 
-    config = {
-        "host": "0.0.0.0",
-        "port": 22,
-        "username": "foo",
-        "password": "pass",
-        "file_type": "json",
-        "start_date": "2021-01-01T00:00:00Z",
-        "folder_path": "/files",
-        "stream_name": "overwrite_stream",
-    }
-
     container = docker_client.containers.run(
         "atmoz/sftp",
-        f"{config['username']}:{config['password']}",
+        f"foo:pass",
         name="mysftpacceptance",
         ports={22: 22},
         volumes={
