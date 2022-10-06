42bab80a92ab8a1a31033c73738fd57e9d016316
letiescanciano <leti@airbyte.io>
2022-10-06 18:08:05 +0200

    improve formatted message

--- airbyte-webapp/src/components/experiments/SpeedyConnection/SpeedyConnectionBanner/SpeedyConnectionBanner.tsx
+++ airbyte-webapp/src/components/experiments/SpeedyConnection/SpeedyConnectionBanner/SpeedyConnectionBanner.tsx
@@ -25,38 +25,31 @@ export const SpeedyConnectionBanner = () => {
       <div className={styles.innerContainer}>
         <img src={credits} alt="" />
 
-        <Link
-          className={classNames(styles.linkCta, {
-            [styles.textDecorationNone]: location.pathname.includes("onboarding"),
-          })}
-          to={RoutePaths.Onboarding}
-          state={{
-            step: StepType.CREATE_SOURCE,
+        <FormattedMessage
+          id="experiment.speedyConnection"
+          defaultMessage="<link>Set up your first connection</link> in the next {count} and get <b>100 additonal credits </b>for your trial"
+          values={{
+            link: (link: React.ReactNode[]) => (
+              <Link
+                className={classNames(styles.linkCta, {
+                  [styles.textDecorationNone]: location.pathname.includes("onboarding"),
+                })}
+                to={RoutePaths.Onboarding}
+                state={{
+                  step: StepType.CREATE_SOURCE,
+                }}
+                onClick={() =>
+                  analyticsService.track(Namespace.ONBOARDING, Action.START_EXP_SPEEDY_CONNECTION, {
+                    actionDescription: "Start Onboarding speedy connection experiment",
+                  })
+                }
+              >
+                <Text bold>{link}</Text>
+              </Link>
+            ),
+            count: (_) => <CountDownTimer expiredOfferDate={expiredOfferDate} />,
           }}
-          onClick={() =>
-            analyticsService.track(Namespace.ONBOARDING, Action.START_EXP_SPEEDY_CONNECTION, {
-              actionDescription: "Start Onboarding speedy connection experiment",
-            })
-          }
-        >
-          <Text>
-            <FormattedMessage
-              id="experiment.speedyConnection.firstConnection"
-              defaultMessage="Set up your first connection "
-            />{" "}
-          </Text>
-        </Link>
-        <Text>
-          <FormattedMessage id="experiment.speedyConnection.next" defaultMessage="in the next" />{" "}
-        </Text>
-
-        <CountDownTimer expiredOfferDate={expiredOfferDate} />
-        <Text>
-          <FormattedMessage
-            id="experiment.speedyConnection.offer"
-            defaultMessage=" and get <b>100 additonal credits </b>for your trial"
-          />
-        </Text>
+        />
         <img src={credits} alt="" />
       </div>
     </div>
