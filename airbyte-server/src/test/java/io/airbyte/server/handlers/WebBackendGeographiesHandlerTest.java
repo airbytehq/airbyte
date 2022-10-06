718bb01c52be7a6ada01d5d0e830222abd9a3c1d
pmossman <parker@airbyte.io>
2022-10-06 11:11:05 -0700

    fix pmd

--- airbyte-server/src/test/java/io/airbyte/server/handlers/WebBackendGeographiesHandlerTest.java
+++ airbyte-server/src/test/java/io/airbyte/server/handlers/WebBackendGeographiesHandlerTest.java
@@ -11,7 +11,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-public class WebBackendGeographiesHandlerTest {
+class WebBackendGeographiesHandlerTest {
 
   private WebBackendGeographiesHandler webBackendGeographiesHandler;
 
