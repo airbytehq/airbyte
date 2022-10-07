714fd60e1b75d56c686aeef432ec450af4af6b2b
Volodymyr Petrov <volodymyr.s.petrov@globallogic.com>
2022-10-08 00:50:07 +0300

    wrap analytics track functions with useCallback

--- airbyte-webapp/src/views/Connector/ServiceForm/components/Controls/ConnectorServiceTypeControl/useAnalyticsTrackFunctions.tsx
+++ airbyte-webapp/src/views/Connector/ServiceForm/components/Controls/ConnectorServiceTypeControl/useAnalyticsTrackFunctions.tsx
@@ -1,4 +1,5 @@
 import { capitalize } from "lodash";
+import { useCallback } from "react";
 
 import { Action, Namespace } from "core/analytics";
 import { useAnalyticsService } from "hooks/services/Analytics";
@@ -8,26 +9,32 @@ export const useAnalyticsTrackFunctions = (formType: "source" | "destination") =
 
   const namespaceType = formType === "source" ? Namespace.SOURCE : Namespace.DESTINATION;
 
-  const trackMenuOpen = () => {
+  const trackMenuOpen = useCallback(() => {
     analytics.track(namespaceType, Action.SELECTION_OPENED, {
       actionDescription: "Opened connector type selection",
     });
-  };
+  }, [analytics, namespaceType]);
 
-  const trackNoOptionMessage = (inputValue: string) => {
-    analytics.track(namespaceType, Action.NO_MATCHING_CONNECTOR, {
-      actionDescription: "Connector query without results",
-      query: inputValue,
-    });
-  };
+  const trackNoOptionMessage = useCallback(
+    (inputValue: string) => {
+      analytics.track(namespaceType, Action.NO_MATCHING_CONNECTOR, {
+        actionDescription: "Connector query without results",
+        query: inputValue,
+      });
+    },
+    [analytics, namespaceType]
+  );
 
-  const trackConnectorSelection = (connectorId: string, connectorName: string) => {
-    analytics.track(namespaceType, Action.SELECT, {
-      actionDescription: `${capitalize(formType)} connector type selected`,
-      [`connector_${formType}`]: connectorName,
-      [`connector_${formType}_definition_id`]: connectorId,
-    });
-  };
+  const trackConnectorSelection = useCallback(
+    (connectorId: string, connectorName: string) => {
+      analytics.track(namespaceType, Action.SELECT, {
+        actionDescription: `${capitalize(formType)} connector type selected`,
+        [`connector_${formType}`]: connectorName,
+        [`connector_${formType}_definition_id`]: connectorId,
+      });
+    },
+    [analytics, formType, namespaceType]
+  );
 
   return { trackMenuOpen, trackNoOptionMessage, trackConnectorSelection };
 };
