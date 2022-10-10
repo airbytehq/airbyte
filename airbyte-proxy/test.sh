71b0a91c89cf0d14f20a49649bdb2a6e8ed850a6
evantahler <evan@airbyte.io>
2022-10-10 13:07:59 -0700

    change test host on CI

--- airbyte-proxy/test.sh
+++ airbyte-proxy/test.sh
@@ -5,15 +5,23 @@ PORT=18000
 BASIC_AUTH_USERNAME=airbyte
 BASIC_AUTH_PASSWORD=password
 BASIC_AUTH_UPDATED_PASSWORD=pa55w0rd
+TEST_HOST=localhost
+
+# On a dev machine, the test is run outside of docker.  On github actions, it's already dockerized
+if [[ -z "${CI}" ]]; then
+  SAMPLE_HOST=host.docker.internal
+else
+  SAMPLE_HOST=localhost
+fi
 
 function start_container () {
   docker run -d -p $PORT:8000 --env BASIC_AUTH_USERNAME=$1 --env BASIC_AUTH_PASSWORD=$2 --name $NAME airbyte/proxy:dev
-  docker run --rm jwilder/dockerize -wait tcp://host.docker.internal:$PORT -timeout 30s
+  docker run --rm jwilder/dockerize -wait tcp://$SAMPLE_HOST:$PORT -timeout 30s
 }
 
 function start_container_with_proxy () {
   docker run -d -p $PORT:8000 --env PROXY_PASS_WEB=$3 --name $NAME airbyte/proxy:dev
-  docker run --rm jwilder/dockerize -wait tcp://host.docker.internal:$PORT -timeout 30s
+  docker run --rm jwilder/dockerize -wait tcp://$SAMPLE_HOST:$PORT -timeout 30s
 }
 
 function stop_container () {
@@ -30,7 +38,7 @@ echo "Starting $NAME"
 start_container $BASIC_AUTH_USERNAME $BASIC_AUTH_PASSWORD
 
 echo "Testing access without auth"
-RESPONSE=`curl "http://localhost:$PORT" -i --silent`
+RESPONSE=`curl "http://$TEST_HOST:$PORT" -i --silent`
 if [[ $RESPONSE == *"401 Unauthorized"* ]]; then
   echo "✔️  access without auth blocked"
 else
@@ -40,7 +48,7 @@ else
 fi
 
 echo "Testing access with auth"
-RESPONSE=`curl "http://$BASIC_AUTH_USERNAME:$BASIC_AUTH_PASSWORD@localhost:$PORT" -i --silent`
+RESPONSE=`curl "http://$BASIC_AUTH_USERNAME:$BASIC_AUTH_PASSWORD@$TEST_HOST:$PORT" -i --silent`
 if [[ $RESPONSE == *"200 OK"* ]]; then
   echo "✔️  access with auth worked"
 else
@@ -55,7 +63,7 @@ echo "Starting $NAME with updated password"
 start_container $BASIC_AUTH_USERNAME $BASIC_AUTH_UPDATED_PASSWORD
 
 echo "Testing access with orignial paassword"
-RESPONSE=`curl "http://$BASIC_AUTH_USERNAME:$BASIC_AUTH_PASSWORD@localhost:$PORT" -i --silent`
+RESPONSE=`curl "http://$BASIC_AUTH_USERNAME:$BASIC_AUTH_PASSWORD@$TEST_HOST:$PORT" -i --silent`
 if [[ $RESPONSE == *"401 Unauthorized"* ]]; then
   echo "✔️  access with original auth blocked"
 else
@@ -65,7 +73,7 @@ else
 fi
 
 echo "Testing access updated auth"
-RESPONSE=`curl "http://$BASIC_AUTH_USERNAME:$BASIC_AUTH_UPDATED_PASSWORD@localhost:$PORT" -i --silent`
+RESPONSE=`curl "http://$BASIC_AUTH_USERNAME:$BASIC_AUTH_UPDATED_PASSWORD@$TEST_HOST:$PORT" -i --silent`
 if [[ $RESPONSE == *"200 OK"* ]]; then
   echo "✔️  access with updated auth worked"
 else
@@ -80,7 +88,7 @@ echo "Starting $NAME with no password"
 start_container "" ""
 
 echo "Testing access without auth"
-RESPONSE=`curl "http://localhost:$PORT" -i --silent`
+RESPONSE=`curl "http://$TEST_HOST:$PORT" -i --silent`
 if [[ $RESPONSE == *"200 OK"* ]]; then
   echo "✔️  access without auth allowed when configured"
 else
@@ -94,7 +102,7 @@ stop_container;
 echo "Testing that PROXY_PASS can be used to change the backend"
 start_container_with_proxy $BASIC_AUTH_USERNAME $BASIC_AUTH_UPDATED_PASSWORD "https://www.google.com"
 
-RESPONSE=`curl "http://localhost:$PORT" -i --silent`
+RESPONSE=`curl "http://$TEST_HOST:$PORT" -i --silent`
 if [[ $RESPONSE == *"google.com"* ]]; then
   echo "✔️  proxy backends can be changed"
 else
