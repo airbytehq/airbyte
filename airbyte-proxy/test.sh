44e6a23858d9edf48576f55ddc32729a1f3b4696
Evan Tahler <evan@airbyte.io>
2022-10-10 22:32:15 +0000

    simpler test for uptime

--- airbyte-proxy/test.sh
+++ airbyte-proxy/test.sh
@@ -7,27 +7,28 @@ BASIC_AUTH_PASSWORD=password
 BASIC_AUTH_UPDATED_PASSWORD=pa55w0rd
 TEST_HOST=localhost
 
-# On a dev machine, the test is run outside of docker.  On github actions, it's already dockerized
-if [[ -z "${CI}" ]]; then
-  SAMPLE_HOST=host.docker.internal
-else
-  SAMPLE_HOST=localhost
-fi
 
 function start_container () {
-  docker run -d -p $PORT:8000 --env BASIC_AUTH_USERNAME=$1 --env BASIC_AUTH_PASSWORD=$2 --name $NAME airbyte/proxy:dev
-  docker run --rm jwilder/dockerize -wait tcp://$SAMPLE_HOST:$PORT -timeout 15s
+  docker run -d -p $PORT:8000 -p 8000:8000 --env BASIC_AUTH_USERNAME=$1 --env BASIC_AUTH_PASSWORD=$2 --name $NAME airbyte/proxy:dev
+  wait_for_docker;
 }
 
 function start_container_with_proxy () {
-  docker run -d -p $PORT:8000 --env PROXY_PASS_WEB=$1 --env PROXY_PASS_RESOLVER=$2 --name $NAME airbyte/proxy:dev
-  docker run --rm jwilder/dockerize -wait tcp://$SAMPLE_HOST:$PORT -timeout 15s
+  docker run -d -p $PORT:8000 -p 8000:8000 --env PROXY_PASS_WEB=$1 --env PROXY_PASS_RESOLVER=$2 --name $NAME airbyte/proxy:dev
+  wait_for_docker;
 }
 
 function stop_container () {
   echo "Stopping $NAME"
   docker kill $NAME
   docker rm $NAME
+}
+
+function wait_for_docker() {
+  until [ "`docker inspect -f {{.State.Running}} $NAME`"=="true" ]; do
+    sleep 1;
+  done;
+  sleep 1;
 }
 
 echo "Testing airbyte proxy..."
