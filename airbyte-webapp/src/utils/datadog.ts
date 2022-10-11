b2976f96f512d23d8ebed93384c759ca3353816d
Edmundo Ruiz Ghanem <edmundo@airbyte.io>
2022-10-11 17:05:51 +0200

    Switch process.env with window for sentry and datadog init

--- airbyte-webapp/src/utils/datadog.ts
+++ airbyte-webapp/src/utils/datadog.ts
@@ -1,15 +1,15 @@
 export const loadDatadog = async (): Promise<void> => {
-  const applicationId = process.env.REACT_APP_DATADOG_APPLICATION_ID ?? window.REACT_APP_DATADOG_APPLICATION_ID;
+  const applicationId = window.REACT_APP_DATADOG_APPLICATION_ID ?? process.env.REACT_APP_DATADOG_APPLICATION_ID;
   if (!applicationId) {
     return;
   }
 
   const { datadogRum } = await import("@datadog/browser-rum");
 
-  const clientToken = process.env.REACT_APP_DATADOG_CLIENT_TOKEN ?? window.REACT_APP_DATADOG_CLIENT_TOKEN;
-  const site = process.env.REACT_APP_DATADOG_SITE ?? window.REACT_APP_DATADOG_SITE;
-  const service = process.env.REACT_APP_DATADOG_SERVICE ?? window.REACT_APP_DATADOG_SERVICE;
-  const version = process.env.REACT_APP_WEBAPP_TAG ?? window.REACT_APP_WEBAPP_TAG ?? "dev";
+  const clientToken = window.REACT_APP_DATADOG_CLIENT_TOKEN ?? process.env.REACT_APP_DATADOG_CLIENT_TOKEN;
+  const site = window.REACT_APP_DATADOG_SITE ?? process.env.REACT_APP_DATADOG_SITE;
+  const service = window.REACT_APP_DATADOG_SERVICE ?? process.env.REACT_APP_DATADOG_SERVICE;
+  const version = window.REACT_APP_WEBAPP_TAG ?? process.env.REACT_APP_WEBAPP_TAG ?? "dev";
 
   datadogRum.init({
     applicationId,
--- airbyte-webapp/src/utils/sentry.ts
+++ airbyte-webapp/src/utils/sentry.ts
@@ -1,12 +1,12 @@
 export const loadSentry = async (): Promise<void> => {
-  const dsn = process.env.REACT_APP_SENTRY_DSN ?? window.REACT_APP_SENTRY_DSN;
+  const dsn = window.REACT_APP_SENTRY_DSN ?? process.env.REACT_APP_SENTRY_DSN;
   if (!dsn) {
     return;
   }
 
   const [Sentry, { Integrations }] = await Promise.all([import("@sentry/react"), import("@sentry/tracing")]);
 
-  const release = process.env.REACT_APP_WEBAPP_TAG ?? window.REACT_APP_WEBAPP_TAG ?? "dev";
+  const release = window.REACT_APP_WEBAPP_TAG ?? process.env.REACT_APP_WEBAPP_TAG ?? "dev";
   const integrations = [new Integrations.BrowserTracing()];
 
   Sentry.init({
