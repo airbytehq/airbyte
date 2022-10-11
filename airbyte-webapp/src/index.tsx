1be2e8b461700b245d91a88fec336ace00673444
Edmundo Ruiz Ghanem <edmundo@airbyte.io>
2022-10-11 10:55:01 +0200

    Move sentry init to its own util

--- airbyte-webapp/src/index.tsx
+++ airbyte-webapp/src/index.tsx
@@ -1,5 +1,3 @@
-import * as Sentry from "@sentry/react";
-import { Integrations } from "@sentry/tracing";
 import { lazy, Suspense } from "react";
 import ReactDOM from "react-dom";
 
@@ -7,17 +5,11 @@ import "react-reflex/styles.css";
 import { isCloudApp } from "utils/app";
 import { loadDatadogRum } from "utils/datadog";
 import { loadOsano } from "utils/dataPrivacy";
+import { initSentry } from "utils/sentry";
 
 import "./globals";
 
-// We do not follow default config approach since we want to init sentry asap
-Sentry.init({
-  dsn: process.env.REACT_APP_SENTRY_DSN || window.REACT_APP_SENTRY_DSN,
-  release: process.env.REACT_APP_WEBAPP_TAG || window.REACT_APP_WEBAPP_TAG || "dev",
-  integrations: [new Integrations.BrowserTracing()],
-  tracesSampleRate: 1.0, // may need to adjust this in the future
-});
-
+initSentry();
 loadDatadogRum();
 
 // In Cloud load the Osano script (GDPR consent tool before anything else)
