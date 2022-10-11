60dba7772af6f94483c54b46058bc42cc44c66b4
Sergey Chvalyuk <grubberr@gmail.com>
2022-10-11 13:52:14 +0300

    revert-me
    Signed-off-by: Sergey Chvalyuk <grubberr@gmail.com>

--- tools/integrations/manage.sh
+++ tools/integrations/manage.sh
@@ -220,7 +220,8 @@ cmd_publish() {
 
   # Install docker emulators
   # TODO: Don't run this command on M1 macs locally (it won't work and isn't needed)
-  docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
+  # docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
+  apt -y install qemu-user-static
 
   # log into docker
   if test -z "${DOCKER_HUB_USERNAME}"; then
