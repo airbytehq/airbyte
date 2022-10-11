ca2605db95059131c8f327fba53126e4b862966f
Anne <102554163+alovew@users.noreply.github.com>
2022-10-10 18:04:34 -0700

    Add breakingChange to catalogDiff (#17588)
    * Add breaking field to FieldTransform on catalogDiff

--- airbyte-webapp/src/views/Connection/CatalogDiffModal/index.stories.tsx
+++ airbyte-webapp/src/views/Connection/CatalogDiffModal/index.stories.tsx
@@ -33,12 +33,13 @@ Primary.args = {
         transformType: "update_stream",
         streamDescriptor: { namespace: "apple", name: "harissa_paste" },
         updateStream: [
-          { transformType: "add_field", fieldName: ["users", "phone"] },
-          { transformType: "add_field", fieldName: ["users", "email"] },
-          { transformType: "remove_field", fieldName: ["users", "lastName"] },
+          { transformType: "add_field", fieldName: ["users", "phone"], breaking: false },
+          { transformType: "add_field", fieldName: ["users", "email"], breaking: false },
+          { transformType: "remove_field", fieldName: ["users", "lastName"], breaking: false },
 
           {
             transformType: "update_field_schema",
+            breaking: false,
             fieldName: ["users", "address"],
             updateFieldSchema: { oldSchema: { type: "number" }, newSchema: { type: "string" } },
           },
