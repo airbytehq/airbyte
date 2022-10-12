b705a5f2bc28d585ece39ad6b299263c4959d1aa
josephkmh <joseph@airbyte.io>
2022-10-12 14:32:52 +0200

    remove validation package in favor of regex

--- airbyte-webapp/src/views/Connection/ConnectionForm/ScheduleField/validationSchema.ts
+++ airbyte-webapp/src/views/Connection/ConnectionForm/ScheduleField/validationSchema.ts
@@ -1,35 +1,62 @@
-import * as cronValidator from "cron-expression-validator";
 import * as yup from "yup";
 
 import { ConnectionScheduleType } from "core/request/AirbyteClient";
 
-export const scheduleFieldValidationSchema = yup.object({
-  scheduleData: yup.mixed().when("scheduleType", (scheduleType) => {
-    if (scheduleType === ConnectionScheduleType.basic) {
-      return yup.object({
-        basicSchedule: yup
-          .object({
-            units: yup.number().required("form.empty.error"),
-            timeUnit: yup.string().required("form.empty.error"),
-          })
-          .defined("form.empty.error"),
-      });
-    } else if (scheduleType === ConnectionScheduleType.manual) {
-      return yup.mixed().notRequired();
+const regexMap = [
+  /^((\d|\*)+)/, // seconds
+  /^((\d|\*)+)/, // minutes
+  /^((\d|\*)+)/, // hours
+  /^((\d|\*|\?|L|W)+)/, // day of month
+  /^((\d|\*|JAN|FEB|MAR|APR|JUN|JUL|AUG|SEP|OCT|NOV|DEC)+)/, // month
+  /^((\d|\*|\?|L|#|SUN|MON|TUE|WED|THU|FRI|SAT|SUN)+)/, // day of week
+  /^((\d|\*)+)?/, // year
+];
+
+export function validateCronExpression(expression: string): boolean {
+  try {
+    const cronFields = expression.trim().split(" ");
+
+    if (cronFields.length < 6) {
+      throw new Error(`Cron expression "${expression}" is not long enough ${cronFields.length}`);
     }
 
+    cronFields.forEach((field, index) => {
+      if (!regexMap[index].test(field)) {
+        throw new Error(`${field} did not match regex index ${index}`);
+      }
+    });
+  } catch (e) {
+    return false;
+  }
+
+  return true;
+}
+
+export const scheduleFieldValidationSchema = yup.mixed().when("scheduleType", (scheduleType) => {
+  if (scheduleType === ConnectionScheduleType.basic) {
     return yup.object({
-      cron: yup
+      basicSchedule: yup
         .object({
-          cronExpression: yup
-            .string()
-            .required("form.empty.error")
-            .test("validCron", "form.cronExpression.error", (expression) =>
-              !expression ? false : cronValidator.isValidCronExpression(expression)
-            ),
-          cronTimeZone: yup.string().required("form.empty.error"),
+          units: yup.number().required("form.empty.error"),
+          timeUnit: yup.string().required("form.empty.error"),
         })
         .defined("form.empty.error"),
     });
-  }),
+  } else if (scheduleType === ConnectionScheduleType.manual) {
+    return yup.mixed().notRequired();
+  }
+
+  return yup.object({
+    cron: yup
+      .object({
+        cronExpression: yup
+          .string()
+          .required("form.empty.error")
+          .test("validCron", "form.cronExpression.error", (expression) =>
+            !expression ? false : validateCronExpression(expression)
+          ),
+        cronTimeZone: yup.string().required("form.empty.error"),
+      })
+      .defined("form.empty.error"),
+  });
 });
