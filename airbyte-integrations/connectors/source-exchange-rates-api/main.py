f8cefe37d3e04d44e3d0fb42fabe080afaf216b1
chaucodes <justinchau31@gmail.com>
2022-10-12 13:44:12 -0500

    added new connector

---
+++ airbyte-integrations/connectors/source-exchange-rates-api/main.py
@@ -0,0 +1,13 @@
+#
+# Copyright (c) 2022 Airbyte, Inc., all rights reserved.
+#
+
+
+import sys
+
+from airbyte_cdk.entrypoint import launch
+from source_exchange_rates_api import SourceExchangeRatesApi
+
+if __name__ == "__main__":
+    source = SourceExchangeRatesApi()
+    launch(source, sys.argv[1:])
