8ca704d797f00bffd4ad3165cd4d59afee641438
lmossman <lake@airbyte.io>
2022-10-14 16:11:58 -0700

    replace react-select dropdown with headless-ui listbox

--- airbyte-webapp/src/components/ConnectorTestingPanel/StreamSelector.tsx
+++ airbyte-webapp/src/components/ConnectorTestingPanel/StreamSelector.tsx
@@ -1,7 +1,11 @@
-import { DropDown, DropDownOptionDataItem } from "components/ui/DropDown";
+import { faSortDown } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Listbox } from "@headlessui/react";
+import classNames from "classnames";
+
+import { Text } from "components/ui/Text";
 
 import styles from "./StreamSelector.module.scss";
-
 interface StreamSelectorProps {
   streams: string[];
   selectedStream: string;
@@ -9,20 +13,25 @@ interface StreamSelectorProps {
 }
 
 export const StreamSelector: React.FC<StreamSelectorProps> = ({ streams, selectedStream, onSelect }) => {
-  const streamOptions = streams.map((streamName) => {
-    return { label: streamName, value: streamName };
-  });
-
-  const handleSelection = (selection: DropDownOptionDataItem) => {
-    onSelect(selection.value);
-  };
-
   return (
-    <DropDown
-      className={styles.streamSelector}
-      value={selectedStream}
-      options={streamOptions}
-      onChange={handleSelection}
-    />
+    <Listbox value={selectedStream} onChange={onSelect}>
+      <Listbox.Button className={classNames(styles.button, styles.centered)}>
+        <Text as="h1" size="sm">
+          {selectedStream}
+        </Text>
+        <FontAwesomeIcon className={styles.arrow} icon={faSortDown} />
+      </Listbox.Button>
+      <Listbox.Options className={classNames(styles.optionsMenu, styles.centered)}>
+        {streams.map((stream) => (
+          <Listbox.Option key={stream} value={stream} className={styles.option}>
+            {({ active }) => (
+              <div className={classNames(styles.optionValue, { [styles.active]: active })}>
+                <Text size="lg">{stream}</Text>
+              </div>
+            )}
+          </Listbox.Option>
+        ))}
+      </Listbox.Options>
+    </Listbox>
   );
 };
