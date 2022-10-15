d5df5d8e23648b7593bd8855113c05a5bebf933b
evantahler <evan@airbyte.io>
2022-10-14 17:21:13 -0700

    guard against small ranges

--- airbyte-integrations/connectors/source-faker/source_faker/source.py
+++ airbyte-integrations/connectors/source-faker/source_faker/source.py
@@ -287,6 +287,8 @@ def read_json(filepath):
 def random_date_in_range(start_date: datetime.datetime, end_date: datetime.datetime = datetime.datetime.now()) -> datetime.datetime:
     time_between_dates = end_date - start_date
     days_between_dates = time_between_dates.days
+    if days_between_dates < 2:
+        days_between_dates = 2
     random_number_of_days = random.randrange(days_between_dates)
     random_date = start_date + datetime.timedelta(days=random_number_of_days)
     return random_date
