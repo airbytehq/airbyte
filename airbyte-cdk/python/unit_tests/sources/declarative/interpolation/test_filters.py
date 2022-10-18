8b5fb646ea8a83684e40eda91eae93e62b61e7ef
Alexandre Girard <alexandre@airbyte.io>
2022-10-17 17:41:56 -0700

    format

--- airbyte-cdk/python/unit_tests/sources/declarative/interpolation/test_filters.py
+++ airbyte-cdk/python/unit_tests/sources/declarative/interpolation/test_filters.py
@@ -1,12 +1,16 @@
+#
+# Copyright (c) 2022 Airbyte, Inc., all rights reserved.
+#
 import hashlib
+
 from airbyte_cdk.sources.declarative.interpolation.jinja import JinjaInterpolation
 
 interpolation = JinjaInterpolation()
 
 
 def test_hash_md5_no_salt():
-    input_string = 'abcd'
-    s = "{{ '%s' | hash('md5') }}"% input_string
+    input_string = "abcd"
+    s = "{{ '%s' | hash('md5') }}" % input_string
     filter_hash = interpolation.eval(s, config={})
 
     # compute expected hash calling hashlib directly
@@ -15,6 +19,7 @@ def test_hash_md5_no_salt():
     hashlib_computed_hash = hash_obj.hexdigest()
 
     assert filter_hash == hashlib_computed_hash
+
 
 def test_hash_md5_on_numeric_value():
     input_value = 123.456
@@ -28,9 +33,10 @@ def test_hash_md5_on_numeric_value():
 
     assert filter_hash == hashlib_computed_hash
 
+
 def test_hash_md5_with_salt():
-    input_string = 'test_input_string'
-    input_salt = 'test_input_salt'
+    input_string = "test_input_string"
+    input_salt = "test_input_salt"
 
     s = "{{ '%s' | hash('md5', '%s' ) }}" % (input_string, input_salt)
     filter_hash = interpolation.eval(s, config={})
