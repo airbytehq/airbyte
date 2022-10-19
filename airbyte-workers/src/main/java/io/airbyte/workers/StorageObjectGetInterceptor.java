dfe9a5fd1f7ce0f797a067456c3ba549443bec07
Cole Snodgrass <cole@airbyte.io>
2022-10-19 15:47:03 -0700

    remove debugging; add test

--- airbyte-workers/src/main/java/io/airbyte/workers/StorageObjectGetInterceptor.java
+++ airbyte-workers/src/main/java/io/airbyte/workers/StorageObjectGetInterceptor.java
@@ -9,7 +9,6 @@ import datadog.trace.api.interceptor.TraceInterceptor;
 import java.util.ArrayList;
 import java.util.Collection;
 
-@SuppressWarnings("PMD")
 public class StorageObjectGetInterceptor implements TraceInterceptor {
 
   @Override
@@ -17,29 +16,26 @@ public class StorageObjectGetInterceptor implements TraceInterceptor {
                                                            final Collection<? extends MutableSpan> trace) {
     final var filtered = new ArrayList<MutableSpan>();
     trace.forEach(s -> {
-      System.out.printf("span name: %s (%s); isError: %s; tags: %s%n",
-          s.getResourceName(),
-          s.getOperationName(), s.isError(), s.getTags());
       final var tags = s.getTags();
       // if no tags, then keep the span and move on to the next one
-      if (tags == null) {
+      if (tags == null || tags.isEmpty()) {
         filtered.add(s);
         return;
       }
 
       // There are two different errors spans that we want to ignore, both of which are specific to
-      // "storage.googleapis.com". One returns an http status code of 404 and the other has an error
+      // "storage.googleapis.com". One returns a http status code of 404 and the other has an error
       // message
       // that begins with "404 Not Found"
       final var is404 = tags.getOrDefault("http.status_code", "").equals(404) ||
           ((String) tags.getOrDefault("error.msg", "")).startsWith("404 Not Found");
       if (s.isError() && tags.getOrDefault("peer.hostname", "").equals("storage.googleapis.com")
           && is404) {
+        // Mark these spans as non-errors as this is expected behavior based on our
+        // current google storage usage.
         s.setError(false);
       }
-      System.out.printf("span name: %s (%s); isError: %s; tags: %s%n",
-          s.getResourceName(),
-          s.getOperationName(), s.isError(), s.getTags());
+
       filtered.add(s);
     });
 
