20d9069b895ba54d86257e5b08166edba64d3605
evantahler <evan@airbyte.io>
2022-10-20 09:18:24 -0700

    Disable auth for frontend acceptance tests

--- tools/bin/e2e_test.sh
+++ tools/bin/e2e_test.sh
@@ -28,4 +28,4 @@ until $(curl --output /dev/null --fail --silent --max-time 5 --head localhost:80
 done
 
 echo "Running e2e tests via gradle"
-SUB_BUILD=PLATFORM ./gradlew --no-daemon :airbyte-webapp-e2e-tests:e2etest -PcypressWebappKey=$CYPRESS_WEBAPP_KEY
+SUB_BUILD=PLATFORM BASIC_AUTH_USERNAME="" BASIC_AUTH_PASSWORD="" ./gradlew --no-daemon :airbyte-webapp-e2e-tests:e2etest -PcypressWebappKey=$CYPRESS_WEBAPP_KEY
