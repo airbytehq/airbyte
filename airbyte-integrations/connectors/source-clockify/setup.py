5b053687658535578a03c60d9a908559918a1972
nataly <nataly@airbyte.io>
2022-10-20 15:20:18 -0400

    Updates airbyte-cdk dependency.

--- airbyte-integrations/connectors/source-clockify/setup.py
+++ airbyte-integrations/connectors/source-clockify/setup.py
@@ -6,7 +6,7 @@
 from setuptools import find_packages, setup
 
 MAIN_REQUIREMENTS = [
-    "airbyte-cdk~=0.1.56",
+    "airbyte-cdk~=0.2.0",
 ]
 
 TEST_REQUIREMENTS = ["pytest~=6.1", "pytest-mock~=3.6.1", "source-acceptance-test", "responses"]
