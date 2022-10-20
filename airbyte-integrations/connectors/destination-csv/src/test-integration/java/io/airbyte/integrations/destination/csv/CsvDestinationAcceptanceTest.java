c3e22b93eaef5bc177b1aef9eb5a7fb98b90d642
nataly <nataly@airbyte.io>
2022-10-20 12:50:49 -0400

    Creates CSV file with delimiters and prints it as stirng.

--- airbyte-integrations/connectors/destination-csv/src/test-integration/java/io/airbyte/integrations/destination/csv/CsvDestinationAcceptanceTest.java
+++ airbyte-integrations/connectors/destination-csv/src/test-integration/java/io/airbyte/integrations/destination/csv/CsvDestinationAcceptanceTest.java
@@ -12,20 +12,31 @@ import io.airbyte.commons.json.Jsons;
 import io.airbyte.integrations.base.JavaBaseConstants;
 import io.airbyte.integrations.destination.StandardNameTransformer;
 import io.airbyte.integrations.standardtest.destination.DestinationAcceptanceTest;
+
+import java.io.BufferedWriter;
 import java.io.FileReader;
+import java.io.IOException;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
 import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVPrinter;
+import org.apache.commons.io.FileUtils;
 import org.apache.commons.csv.CSVRecord;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class CsvDestinationAcceptanceTest extends DestinationAcceptanceTest {
 
   private static final Path RELATIVE_PATH = Path.of("integration_test/test");
+  private static final String SAMPLE_CSV_FILE = "./sample.csv";
 
   @Override
   protected String getImageName() {
@@ -36,6 +47,58 @@ public class CsvDestinationAcceptanceTest extends DestinationAcceptanceTest {
   protected JsonNode getConfig() {
     return Jsons.jsonNode(ImmutableMap.of("destination_path", Path.of("/local").resolve(RELATIVE_PATH).toString()));
   }
+
+  @ParameterizedTest
+  @ValueSource(chars = {',',';','.',' '})
+  void detectDelimiter(char input) throws IOException {
+    // Create a CSV file with specified delimiter
+
+    final Path path = Paths.get(SAMPLE_CSV_FILE);
+    try (
+            BufferedWriter writer = Files.newBufferedWriter(path);
+
+            CSVPrinter csvPrinter = new CSVPrinter(writer, CSVFormat.DEFAULT
+                    .withDelimiter(input)
+                    .withHeader("ID", "Instrument", "Type", "Melodic"));
+    ) {
+      csvPrinter.printRecord("1", "Violin", "String", "True");
+
+      csvPrinter.flush();
+
+      String content = Files.readString(path);
+
+      System.out.println(content);
+    }
+
+//    CSVParser csvParser = new CSVParser(new FileReader(SAMPLE_CSV_FILE), CSVFormat.DEFAULT.withDelimiter(input));
+//    for (CSVRecord record : csvParser) {
+//      for (int i = 0; i < record.size(); i++) {
+//        System.out.println("At " + i + ": " + record.get(i));
+//      }
+//    }
+
+//    try (CSVReader reader = new CSVReader(new FileReader(SAMPLE_CSV_FILE))) {
+//      List<String[]> r = reader.readAll();
+//      r.forEach(x -> System.out.println(Arrays.toString(x)));
+//    }
+  }
+//    FileWriter out = new FileWriter("book.csv");
+//    try {
+//      (CSVPrinter printer = new CSVPrinter(out, CSVFormat.DEFAULT
+//              .withDelimiter(input)
+//              .withHeader(HEADERS))){
+//        AUTHOR_BOOK_MAP.forEach((author, title) -> {
+//          printer.printRecord(author, title);
+//        });
+//        return out;
+//      }
+//    }
+//    Reader in = new FileReader(out);
+//    Iterable<CSVRecord> records = CSVFormat.DEFAULT
+//      .withHeader(HEADERS)
+//      .withFirstRecordAsHeader()
+//      .parse(in);
+
 
   // todo (cgardens) - it would be great if we could find a configuration here that failed. the
   // commented out one fails in mac but not on the linux box that the github action runs in. instead
@@ -89,7 +152,5 @@ public class CsvDestinationAcceptanceTest extends DestinationAcceptanceTest {
   protected void tearDown(final TestDestinationEnv testEnv) {
     // no op
   }
-
-  // TODO: add a test to check for delimeter
 
 }
