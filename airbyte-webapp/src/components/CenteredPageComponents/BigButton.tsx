106926e3a71fc4fbe18f62a8fb7c4b26848fb7b4
MuhammadAkif <akifmalhi786@gmail.com>
2022-10-20 14:17:33 +0500

    CU-34vy9qb - Implement webapp landing page UI

--- airbyte-webapp/src/components/CenteredPageComponents/BigButton.tsx
+++ airbyte-webapp/src/components/CenteredPageComponents/BigButton.tsx
@@ -2,11 +2,13 @@ import styled from "styled-components";
 
 import { Button } from "components";
 const BigButton = styled(Button)<{ shadow?: boolean }>`
+  background-color: #4f46e5;
+  border-radius: 25px;
   font-size: 16px;
   line-height: 19px;
-  padding: 10px 27px;
+  padding: 15px 30px;
   font-weight: 500;
-  box-shadow: ${({ shadow }) => (shadow ? "0 8px 5px -5px rgba(0, 0, 0, 0.2)" : "none")};
+  //box-shadow: ${({ shadow }) => (shadow ? "0 8px 5px -5px rgba(0, 0, 0, 0.2)" : "none")};
 `;
 
 export default BigButton;
