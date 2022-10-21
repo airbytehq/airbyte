e14273cfcdd298b4d1f08c37835deeda7463fc00
Alexandre Girard <alexandre@airbyte.io>
2022-10-21 14:46:52 -0700

    sleep before returning

--- airbyte-connector-builder/connector_builder/entrypoint.py
+++ airbyte-connector-builder/connector_builder/entrypoint.py
@@ -1,6 +1,9 @@
 #
 # Copyright (c) 2022 Airbyte, Inc., all rights reserved.
 #
+
+import time
+
 from flask import Flask  # Importing the class flask
 
 # app is the object or instance of Flask
@@ -12,6 +15,14 @@ app = Flask(__name__)
 # Creating a function named success
 def hello():
     return "hello flask"
+
+
+# app.route informs Flask about the URL to be used by function
+@app.route("/wait/<wait_time>")
+# Creating a function named success
+def wait(wait_time):
+    time.sleep(int(wait_time))
+    return f"waited {wait_time} before returning"
 
 
 # Programs executes from here in a development server (locally on your system)
