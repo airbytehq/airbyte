6d5bf54d701b4fcdbd6839d682e25dbf2d08f35e
Volodymyr Petrov <volodymyr.s.petrov@globallogic.com>
2022-10-21 17:00:29 +0300

    add regex matcher for custom transformation GitHub url

--- airbyte-webapp/src/views/Connection/TransformationForm/TransformationForm.tsx
+++ airbyte-webapp/src/views/Connection/TransformationForm/TransformationForm.tsx
@@ -32,7 +32,10 @@ const validationSchema = yup.object({
   name: yup.string().required("form.empty.error"),
   operatorConfiguration: yup.object({
     dbt: yup.object({
-      gitRepoUrl: yup.string().required("form.empty.error"),
+      gitRepoUrl: yup
+        .string()
+        .required("form.empty.error")
+        .matches(/((http(s)?)|(git@[\w.]+))(:(\/\/)?)([\w.@:/\-~]+)(\.git)$/, "form.repositoryUrl.invalidUrl"),
       dockerImage: yup.string().required("form.empty.error"),
       dbtArguments: yup.string().required("form.empty.error"),
       gitRepoBranch: yup.string().nullable(),
