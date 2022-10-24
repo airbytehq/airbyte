789d72e0a15ed402cbed9421e030c93de28ce42e
Edmundo Ruiz Ghanem <edmundo@airbyte.io>
2022-10-24 16:15:16 -0400

    Remove outline and tint from PillButton until it's resolved with design

--- airbyte-webapp/src/components/ui/PillButton/PillButton.tsx
+++ airbyte-webapp/src/components/ui/PillButton/PillButton.tsx
@@ -5,28 +5,21 @@ import classNames from "classnames";
 import { Text } from "../Text";
 import styles from "./PillButton.module.scss";
 
-type PillButtonTint = "grey" | "light-grey" | "red" | "light-red" | "blue" | "green";
-
 interface PillButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   asDropdown?: boolean;
   active?: boolean;
-  outline?: boolean;
-  tint?: PillButtonTint;
 }
 
 export const PillButton: React.FC<React.PropsWithChildren<PillButtonProps>> = ({
   children,
   active,
   asDropdown,
-  outline,
-  tint = "grey",
   ...buttonProps
 }) => {
   const buttonClassName = classNames(
     styles.button,
     {
       [styles.active]: active,
-      [styles.light]: outline,
       // TODO: Implement tint styles
     },
     buttonProps.className
