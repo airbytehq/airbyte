f361d37caed4a30126e2a3655afe1a24a3f2905c
Artur Siepietowski <artursiepietowski@gmail.com>
2022-10-24 19:10:51 +0200

    Adding transactions for klarna source integration

---
+++ airbyte-integrations/connectors/source-klarna/source_klarna/source.py
@@ -0,0 +1,219 @@
+#
+# Copyright (c) 2022 Airbyte, Inc., all rights reserved.
+#
+
+
+from abc import ABC, abstractmethod
+from typing import Any, Iterable, List, Mapping, MutableMapping, Optional, Tuple
+from urllib.parse import urlparse, parse_qs
+
+import requests
+from airbyte_cdk.sources import AbstractSource
+from airbyte_cdk.sources.streams import Stream
+from airbyte_cdk.sources.streams.http import HttpStream
+from airbyte_cdk.sources.streams.http.auth import TokenAuthenticator
+from requests.auth import HTTPBasicAuth
+"""
+TODO: Most comments in this class are instructive and should be deleted after the source is implemented.
+
+This file provides a stubbed example of how to use the Airbyte CDK to develop both a source connector which supports full refresh or and an
+incremental syncs from an HTTP API.
+
+The various TODOs are both implementation hints and steps - fulfilling all the TODOs should be sufficient to implement one basic and one incremental
+stream from a source. This pattern is the same one used by Airbyte internally to implement connectors.
+
+The approach here is not authoritative, and devs are free to use their own judgement.
+
+There are additional required TODOs in the files within the integration_tests folder and the spec.yaml file.
+"""
+
+
+# Basic full refresh stream
+class KlarnaStream(HttpStream, ABC):
+    """
+    TODO remove this comment
+
+    This class represents a stream output by the connector.
+    This is an abstract base class meant to contain all the common functionality at the API level e.g: the API base URL, pagination strategy,
+    parsing responses etc..
+
+    Each stream should extend this class (or another abstract subclass of it) to specify behavior unique to that stream.
+
+    Typically for REST APIs each stream corresponds to a resource in the API. For example if the API
+    contains the endpoints
+        - GET v1/customers
+        - GET v1/employees
+
+    then you should have three classes:
+    `class KlarnaStream(HttpStream, ABC)` which is the current class
+    `class Customers(KlarnaStream)` contains behavior to pull data for customers using v1/customers
+    `class Employees(KlarnaStream)` contains behavior to pull data for employees using v1/employees
+
+    If some streams implement incremental sync, it is typical to create another class
+    `class IncrementalKlarnaStream((KlarnaStream), ABC)` then have concrete stream implementations extend it. An example
+    is provided below.
+
+    See the reference docs for the full list of configurable options.
+    """
+    def __init__(self, region: str,  playground: bool, authenticator: HTTPBasicAuth, **kwargs):
+        self.region = region
+        self.playground = playground
+        self.kwargs = kwargs
+        super().__init__(authenticator=authenticator)
+
+    page_size = 500
+
+    @property
+    def url_base(self) -> str:
+        playground_path = 'playground.' if self.playground else ''
+        if self.region == 'eu':
+            endpoint = f"https://api.{playground_path}klarna.com/"
+        else:
+            endpoint = f"https://api-{self.region}.{playground_path}klarna.com/"
+        return endpoint
+
+    def next_page_token(self, response: requests.Response) -> Optional[Mapping[str, Any]]:
+        response_json = response.json()
+        if 'pagination' in response_json:
+            if 'next' in response_json['pagination']:
+                parsed_url = urlparse(response_json['pagination']['next'])
+                query_params = parse_qs(parsed_url.query)
+                # noinspection PyTypeChecker
+                return query_params
+        else:
+            return None
+
+    def request_params(
+        self, stream_state: Mapping[str, Any], stream_slice: Mapping[str, any] = None, next_page_token: Mapping[str, Any] = None
+    ) -> MutableMapping[str, Any]:
+        if next_page_token:
+            return dict(next_page_token)
+        else:
+            return {"offset": 0, "size": self.page_size}
+
+
+    @abstractmethod
+    def parse_response(self, response: requests.Response, **kwargs) -> Iterable[Mapping]:
+        """
+        TODO: Override this method to define how a response is parsed.
+        :return an iterable containing each record in the response
+        """
+        yield {}
+
+
+class Transactions(KlarnaStream):
+    """
+    TODO: Change class name to match the table/data source this stream corresponds to.
+    """
+
+    # TODO: Fill in the primary key. Required. This is usually a unique field in the stream, like an ID or a timestamp.
+    primary_key = "customer_id"
+
+    def path(
+            self, stream_state: Mapping[str, Any] = None, stream_slice: Mapping[str, Any] = None,
+            next_page_token: Mapping[str, Any] = None
+    ) -> str:
+        return "/settlements/v1/transactions"
+
+    def parse_response(self, response: requests.Response, **kwargs) -> Iterable[Mapping]:
+        """
+        TODO: Override this method to define how a response is parsed.
+        :return an iterable containing each record in the response
+        """
+        transactions = response.json().get("transactions", [])
+        yield from transactions
+
+# Basic incremental stream
+class IncrementalKlarnaStream(KlarnaStream, ABC):
+    """
+    TODO fill in details of this class to implement functionality related to incremental syncs for your connector.
+         if you do not need to implement incremental sync for any streams, remove this class.
+    """
+
+    # TODO: Fill in to checkpoint stream reads after N records. This prevents re-reading of data if the stream fails for any reason.
+    state_checkpoint_interval = None
+
+    @property
+    def cursor_field(self) -> str:
+        """
+        TODO
+        Override to return the cursor field used by this stream e.g: an API entity might always use created_at as the cursor field. This is
+        usually id or date based. This field's presence tells the framework this in an incremental stream. Required for incremental.
+
+        :return str: The name of the cursor field.
+        """
+        return []
+
+    def get_updated_state(self, current_stream_state: MutableMapping[str, Any], latest_record: Mapping[str, Any]) -> Mapping[str, Any]:
+        """
+        Override to determine the latest state after reading the latest record. This typically compared the cursor_field from the latest record and
+        the current state and picks the 'most' recent cursor. This is how a stream's state is determined. Required for incremental.
+        """
+        return {}
+
+
+class Employees(IncrementalKlarnaStream):
+    """
+    TODO: Change class name to match the table/data source this stream corresponds to.
+    """
+
+    # TODO: Fill in the cursor_field. Required.
+    cursor_field = "start_date"
+
+    # TODO: Fill in the primary key. Required. This is usually a unique field in the stream, like an ID or a timestamp.
+    primary_key = "employee_id"
+
+    def path(self, **kwargs) -> str:
+        """
+        TODO: Override this method to define the path this stream corresponds to. E.g. if the url is https://example-api.com/v1/employees then this should
+        return "single". Required.
+        """
+        return "employees"
+
+    def stream_slices(self, stream_state: Mapping[str, Any] = None, **kwargs) -> Iterable[Optional[Mapping[str, any]]]:
+        """
+        TODO: Optionally override this method to define this stream's slices. If slicing is not needed, delete this method.
+
+        Slices control when state is saved. Specifically, state is saved after a slice has been fully read.
+        This is useful if the API offers reads by groups or filters, and can be paired with the state object to make reads efficient. See the "concepts"
+        section of the docs for more information.
+
+        The function is called before reading any records in a stream. It returns an Iterable of dicts, each containing the
+        necessary data to craft a request for a slice. The stream state is usually referenced to determine what slices need to be created.
+        This means that data in a slice is usually closely related to a stream's cursor_field and stream_state.
+
+        An HTTP request is made for each returned slice. The same slice can be accessed in the path, request_params and request_header functions to help
+        craft that specific request.
+
+        For example, if https://example-api.com/v1/employees offers a date query params that returns data for that particular day, one way to implement
+        this would be to consult the stream state object for the last synced date, then return a slice containing each date from the last synced date
+        till now. The request_params function would then grab the date from the stream_slice and make it part of the request by injecting it into
+        the date query param.
+        """
+        raise NotImplementedError("Implement stream slices or delete this method!")
+
+
+# Source
+class SourceKlarna(AbstractSource):
+    def check_connection(self, logger, config) -> Tuple[bool, any]:
+        """
+        TODO: Implement a connection check to validate that the user-provided config can be used to connect to the underlying API
+
+        See https://github.com/airbytehq/airbyte/blob/master/airbyte-integrations/connectors/source-stripe/source_stripe/source.py#L232
+        for an example.
+
+        :param config:  the user-input config object conforming to the connector's spec.yaml
+        :param logger:  logger object
+        :return Tuple[bool, any]: (True, None) if the input config can be used to connect to the API successfully, (False, error) otherwise.
+        """
+        return True, None
+
+    def streams(self, config: Mapping[str, Any]) -> List[Stream]:
+        """
+        TODO: Replace the streams below with your own streams.
+
+        :param config: A Mapping of the user input configuration as defined in the connector spec.
+        """
+        # TODO remove the authenticator if not required.
+        auth = HTTPBasicAuth(username=config['username'], password=config['password'])
+        return [Transactions(authenticator=auth, **config)]
