a070278b72ec00bdaac24a7606f9831b06252e38
Cirdes Henrique <cirdes@gmail.com>
2022-10-23 22:39:17 -0300

    🎉 New Destination: Typesense

--- airbyte-integrations/connectors/destination-typesense/destination_typesense/destination.py
+++ airbyte-integrations/connectors/destination-typesense/destination_typesense/destination.py
@@ -3,64 +3,59 @@
 #
 
 
+from logging import Logger
 from typing import Any, Iterable, Mapping
 
-from airbyte_cdk import AirbyteLogger
 from airbyte_cdk.destinations import Destination
-from airbyte_cdk.models import AirbyteConnectionStatus, AirbyteMessage, ConfiguredAirbyteCatalog, Status
+from airbyte_cdk.models import AirbyteConnectionStatus, AirbyteMessage, ConfiguredAirbyteCatalog, DestinationSyncMode, Status, Type
+from destination_typesense.writer import TypesenseWriter
 from typesense import Client
+
 
 def get_client(config: Mapping[str, Any]) -> Client:
     api_key = config.get("api_key")
     host = config.get("host")
-    port = config.get("port")
-    protocol = config.get("protocol")
+    port = config.get("port") or "8108"
+    protocol = config.get("protocol") or "https"
 
-    client = typesense.Client({
-    'api_key': api_key,
-    'nodes': [{
-        'host': host,
-        'port': port,
-        'protocol': protocol
-    }],
-    'connection_timeout_seconds': 2
-    })
+    client = Client({"api_key": api_key, "nodes": [{"host": host, "port": port, "protocol": protocol}], "connection_timeout_seconds": 2})
 
     return client
+
 
 class DestinationTypesense(Destination):
     def write(
         self, config: Mapping[str, Any], configured_catalog: ConfiguredAirbyteCatalog, input_messages: Iterable[AirbyteMessage]
     ) -> Iterable[AirbyteMessage]:
+        client = get_client(config=config)
 
-        """
-        TODO
-        Reads the input stream of messages, config, and catalog to write data to the destination.
+        for configured_stream in configured_catalog.streams:
+            steam_name = configured_stream.stream.name
+            if configured_stream.destination_sync_mode == DestinationSyncMode.overwrite:
+                try:
+                    client.collections[steam_name].delete()
+                except Exception:
+                    pass
+                client.collections.create({"name": steam_name, "fields": [{"name": ".*", "type": "auto"}]})
 
-        This method returns an iterable (typically a generator of AirbyteMessages via yield) containing state messages received
-        in the input message stream. Outputting a state message means that every AirbyteRecordMessage which came before it has been
-        successfully persisted to the destination. This is used to ensure fault tolerance in the case that a sync fails before fully completing,
-        then the source is given the last state message output from this method as the starting point of the next sync.
+            writer = TypesenseWriter(client, steam_name, config.get("batch_size"))
+            for message in input_messages:
+                if message.type == Type.STATE:
+                    writer.flush()
+                    yield message
+                elif message.type == Type.RECORD:
+                    writer.queue_write_operation(message.record.data)
+                else:
+                    continue
+            writer.flush()
 
-        :param config: dict of JSON configuration matching the configuration declared in spec.json
-        :param configured_catalog: The Configured Catalog describing the schema of the data being received and how it should be persisted in the
-                                    destination
-        :param input_messages: The stream of input messages received from the source
-        :return: Iterable of AirbyteStateMessages wrapped in AirbyteMessage structs
-        """
-
-        pass
-
-    def check(self, logger: AirbyteLogger, config: Mapping[str, Any]) -> AirbyteConnectionStatus:
+    def check(self, logger: Logger, config: Mapping[str, Any]) -> AirbyteConnectionStatus:
         try:
             client = get_client(config=config)
-            create_collection = client.collections.create({
-                "name": "_airbyte",
-                "fields": [{"name": "title", "type": "string"}]
-            })
-            client.collections['_airbyte'].documents.create({'id': '1', 'title': 'The Hunger Games'})
-            client.collections['_airbyte'].documents['1'].retrieve()
-            client.collections['_airbyte'].delete()
+            client.collections.create({"name": "_airbyte", "fields": [{"name": "title", "type": "string"}]})
+            client.collections["_airbyte"].documents.create({"id": "1", "title": "The Hunger Games"})
+            client.collections["_airbyte"].documents["1"].retrieve()
+            client.collections["_airbyte"].delete()
             return AirbyteConnectionStatus(status=Status.SUCCEEDED)
         except Exception as e:
             return AirbyteConnectionStatus(status=Status.FAILED, message=f"An exception occurred: {repr(e)}")
--- airbyte-integrations/connectors/destination-typesense/unit_tests/unit_test.py
+++ airbyte-integrations/connectors/destination-typesense/unit_tests/unit_test.py
@@ -2,6 +2,21 @@
 # Copyright (c) 2022 Airbyte, Inc., all rights reserved.
 #
 
+from unittest.mock import patch
 
-def test_example_method():
-    assert True
+from destination_typesense.writer import TypesenseWriter
+
+
+@patch("typesense.Client")
+def test_queue_write_operation(client):
+    writer = TypesenseWriter(client, "steam_name")
+    writer.queue_write_operation({"a": "a"})
+    assert len(writer.write_buffer) == 1
+
+
+@patch("typesense.Client")
+def test_flush(client):
+    writer = TypesenseWriter(client, "steam_name")
+    writer.queue_write_operation({"a": "a"})
+    writer.flush()
+    client.collections.__getitem__.assert_called_once_with("steam_name")
