ae313a0cc76cf05d8332c10d6933bb60bd00e07e
espoir murhabazi <espoir.mur@gmail.com>
2022-10-25 12:45:32 +0100

    fix the issue with the authentication

--- airbyte-integrations/connectors/source-google-ad-manager/source_google_ad_manager/authenticator.py
+++ airbyte-integrations/connectors/source-google-ad-manager/source_google_ad_manager/authenticator.py
@@ -1,35 +1,46 @@
 import json
 from googleads import ad_manager
 from googleads import oauth2
+from googleads.common import ProxyConfig
 from googleads.errors import GoogleAdsValueError
-from typing import Any, Iterator, List, Mapping, MutableMapping
-from pathlib import Path
+from typing import Any, List, Mapping, MutableMapping
+from google.oauth2.service_account import Credentials
 
 
 API_VERSION = 'v202208'
 
+
+class CustomServiceAccountClient(oauth2.GoogleServiceAccountClient):
+    """this is a custom service account client that implement from dict file instead of file path
+
+    Args:
+        oauth2 (_type_): _description_
+
+    Raises:
+        Exception: _description_
+
+    Returns:
+        _type_: _description_
+    """
+    def __init__(self, credentials_dict: Mapping[str, str], scope: str, sub=None, proxy_config=None):
+        try:
+            self.creds = (Credentials.from_service_account_info(credentials_dict, scopes=[scope], subject=sub))
+        except KeyError as exc:
+            raise GoogleAdsValueError(f"make sure the credentials dictionary has the following keys: {exc.args[0]}")
+        self.proxy_config = (proxy_config if proxy_config else ProxyConfig())
+        self.Refresh()
+    
 
 class GoogleAdManagerAuthenticator:
     """responsible for generating the google_ad_manager authenticator object
     """
 
     def __init__(self, config: MutableMapping[str, Any], application_name: str):
-        service_key_path = self.generate_json_file_from_credentials(config)
         try:
-            self.oauth2_client = oauth2.GoogleServiceAccountClient(service_key_path, oauth2.GetAPIScope('ad_manager'))
+            self.oauth2_client = CustomServiceAccountClient(config, oauth2.GetAPIScope('ad_manager'))
         except GoogleAdsValueError as e:
             raise Exception(f"Error: {e}")
         self.ad_manager_client = ad_manager.AdManagerClient(self.oauth2_client, application_name)
-
-    def generate_json_file_from_credentials(self, credentials: MutableMapping[str, Any]) -> str:
-        """generates a json file from the credentials
-        """
-        # @todo: check if this does not have any security issues
-        credentials["private_key"] = credentials["private_key"].replace("\\n", "\n")  # hack to fix the private key issue
-        service_key_path = Path('/tmp/service_key.json')
-        with open(service_key_path, 'w') as f:
-            json.dump(credentials, f)
-        return service_key_path
 
     def generate_report_downloader(self) -> ad_manager.DataDownloader:
         report_downloader = self.ad_manager_client.GetDataDownloader(version='v202208')
