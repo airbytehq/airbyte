ccdc8d8968b0685c0625ee6558de1222a1ec37c4
Lake Mossman <lake@airbyte.io>
2022-10-24 17:33:05 -0700

    :window: :wrench: Generify connector documentation layout into resizable panels component (#18272)
    * generify connector documentation layout into resizable panels component
    
    * add intl string for connector builder expand message
    
    * update name of formatted download yaml message
    
    * fix stylelint
    
    * Update airbyte-webapp/src/components/ui/ResizablePanels/ResizablePanels.module.scss
    
    Co-authored-by: Joey Marshment-Howell <josephkmh@users.noreply.github.com>
    
    * move splitter styles into scss module
    
    * simplify ResizablePanels component
    
    * reorder
    
    * use variables
    
    * remove units on 0
    
    * Update airbyte-webapp/src/views/Connector/ConnectorDocumentationLayout/ConnectorDocumentationLayout.module.scss
    
    Co-authored-by: Joey Marshment-Howell <josephkmh@users.noreply.github.com>
    
    * Update airbyte-webapp/src/components/ui/ResizablePanels/ResizablePanels.module.scss
    
    Co-authored-by: Joey Marshment-Howell <josephkmh@users.noreply.github.com>
    
    * Update airbyte-webapp/src/components/ui/ResizablePanels/ResizablePanels.module.scss
    
    Co-authored-by: Joey Marshment-Howell <josephkmh@users.noreply.github.com>
    
    Co-authored-by: Joey Marshment-Howell <josephkmh@users.noreply.github.com>

--- airbyte-webapp/src/pages/ConnectorBuilderPage/ConnectorBuilderPage.tsx
+++ airbyte-webapp/src/pages/ConnectorBuilderPage/ConnectorBuilderPage.tsx
@@ -1,5 +1,28 @@
+import { ResizablePanels } from "components/ui/ResizablePanels";
 import { YamlEditor } from "components/YamlEditor";
 
+import styles from "./ConnectorBuilderPage.module.scss";
+
 export const ConnectorBuilderPage: React.FC = () => {
-  return <YamlEditor />;
+  return (
+    <ResizablePanels
+      className={styles.container}
+      leftPanel={{
+        children: <YamlEditor />,
+        className: styles.leftPanel,
+        minWidth: 400,
+      }}
+      rightPanel={{
+        children: <div>Testing panel</div>,
+        className: styles.rightPanel,
+        startingFlex: 0.33,
+        minWidth: 60,
+        overlay: {
+          displayThreshold: 300,
+          header: "Stream Name",
+          rotation: "counter-clockwise",
+        },
+      }}
+    />
+  );
 };
