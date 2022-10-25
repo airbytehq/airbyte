a753ae2c112d5e10153ddebad945f88078cd4b88
Alexandre Girard <alexandre@airbyte.io>
2022-10-25 15:50:17 -0700

    fix versioning

--- airbyte-integrations/connectors/source-google-sheets/setup.py
+++ airbyte-integrations/connectors/source-google-sheets/setup.py
@@ -6,7 +6,7 @@
 from setuptools import find_packages, setup
 
 MAIN_REQUIREMENTS = [
-    "airbyte-cdk>=0.2.3",
+    "airbyte-cdk~=0.2",
     "backoff",
     "requests",
     "google-auth-httplib2",
