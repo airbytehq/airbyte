a113c5889295a635c4af97e2765c31d630787639
alafanechere <augustin.lafanechere@gmail.com>
2022-10-27 11:55:25 +0200

    fix typo

--- airbyte-integrations/bases/base-normalization/normalization/transform_catalog/catalog_processor.py
+++ airbyte-integrations/bases/base-normalization/normalization/transform_catalog/catalog_processor.py
@@ -9,7 +9,8 @@ import re
 from typing import Any, Dict, List, Set
 
 import yaml
-from airbyte_cdk.models.airbyte_protocol import DestinationSyncMode, DestinationType, SyncMode
+from airbyte_cdk.models.airbyte_protocol import DestinationSyncMode, SyncMode
+from normalization.destination_type import DestinationType
 from normalization.transform_catalog import dbt_macro
 from normalization.transform_catalog.destination_name_transformer import DestinationNameTransformer
 from normalization.transform_catalog.stream_processor import StreamProcessor
