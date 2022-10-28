eb7c4be398f83f4446d72e383d01092f4c12d34a
Octavia Squidington III <octavia-squidington-iii@users.noreply.github.com>
2022-10-28 14:37:47 +0000

    auto-bump connector version

--- airbyte-integrations/connectors/destination-typesense/setup.py
+++ airbyte-integrations/connectors/destination-typesense/setup.py
@@ -5,10 +5,7 @@
 
 from setuptools import find_packages, setup
 
-MAIN_REQUIREMENTS = [
-    "airbyte-cdk",
-    "typesense>=0.14.0"
-]
+MAIN_REQUIREMENTS = ["airbyte-cdk", "typesense>=0.14.0"]
 
 TEST_REQUIREMENTS = ["pytest~=6.1", "typesense>=0.14.0"]
 
