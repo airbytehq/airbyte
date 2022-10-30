ea39075b5744301be055194d0566aa9df1599064
ievgeniit <etsybaev@gmail.com>
2022-10-30 21:11:22 +0100

    [16283] Destination-dynamodb: enforce ssl connection

--- airbyte-integrations/connectors/destination-dynamodb/src/main/java/io/airbyte/integrations/destination/dynamodb/DynamodbDestination.java
+++ airbyte-integrations/connectors/destination-dynamodb/src/main/java/io/airbyte/integrations/destination/dynamodb/DynamodbDestination.java
@@ -4,6 +4,7 @@
 
 package io.airbyte.integrations.destination.dynamodb;
 
+import com.amazonaws.Protocol;
 import com.fasterxml.jackson.databind.JsonNode;
 import io.airbyte.integrations.BaseConnector;
 import io.airbyte.integrations.base.AirbyteMessageConsumer;
@@ -12,12 +13,14 @@ import io.airbyte.integrations.base.IntegrationRunner;
 import io.airbyte.protocol.models.AirbyteConnectionStatus;
 import io.airbyte.protocol.models.AirbyteMessage;
 import io.airbyte.protocol.models.ConfiguredAirbyteCatalog;
+import java.net.URL;
 import java.util.function.Consumer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class DynamodbDestination extends BaseConnector implements Destination {
 
+  private static final String NON_SECURE_URL_ERR_MSG = "Server Endpoint requires HTTPS";
   private static final Logger LOGGER = LoggerFactory.getLogger(DynamodbDestination.class);
 
   public static void main(final String[] args) throws Exception {
@@ -27,7 +30,19 @@ public class DynamodbDestination extends BaseConnector implements Destination {
   @Override
   public AirbyteConnectionStatus check(final JsonNode config) {
     try {
-      DynamodbChecker.attemptDynamodbWriteAndDelete(DynamodbDestinationConfig.getDynamodbDestinationConfig(config));
+      final DynamodbDestinationConfig dynamodbDestinationConfig =
+          DynamodbDestinationConfig.getDynamodbDestinationConfig(config);
+
+      // enforce ssl connection
+      if (!dynamodbDestinationConfig.getEndpoint().isBlank()) {
+        if (new URL(dynamodbDestinationConfig.getEndpoint()).getProtocol().equals(Protocol.HTTP.toString())) {
+          return new AirbyteConnectionStatus()
+              .withStatus(AirbyteConnectionStatus.Status.FAILED)
+              .withMessage(NON_SECURE_URL_ERR_MSG);
+        }
+      }
+
+      DynamodbChecker.attemptDynamodbWriteAndDelete(dynamodbDestinationConfig);
       return new AirbyteConnectionStatus().withStatus(AirbyteConnectionStatus.Status.SUCCEEDED);
     } catch (final Exception e) {
       LOGGER.error("Exception attempting to access the DynamoDB table: ", e);
