0fe9c5fc80e66b92d9e96b55b0384f10a844c70d
cgardens <charles@airbyte.io>
2022-10-31 14:46:57 -0700

    add migration

---
+++ airbyte-db/db-lib/src/main/java/io/airbyte/db/instance/configs/migrations/V0_40_17_001__AddAuditLog.java
@@ -0,0 +1,81 @@
+/*
+ * Copyright (c) 2022 Airbyte, Inc., all rights reserved.
+ */
+
+package io.airbyte.db.instance.configs.migrations;
+
+import static org.jooq.impl.DSL.currentOffsetDateTime;
+import static org.jooq.impl.DSL.primaryKey;
+
+import io.airbyte.commons.resources.MoreResources;
+import java.io.IOException;
+import java.time.OffsetDateTime;
+import java.util.Set;
+import java.util.UUID;
+import org.flywaydb.core.api.migration.BaseJavaMigration;
+import org.flywaydb.core.api.migration.Context;
+import org.jooq.Catalog;
+import org.jooq.DSLContext;
+import org.jooq.EnumType;
+import org.jooq.Field;
+import org.jooq.JSONB;
+import org.jooq.Schema;
+import org.jooq.impl.DSL;
+import org.jooq.impl.SQLDataType;
+import org.jooq.impl.SchemaImpl;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class V0_40_17_001__AddAuditLog extends BaseJavaMigration {
+  private static final UUID PLACEHOLDER_CREATED_BY = UUID.fromString("00000000-0000-0000-0000-000000000000");
+  private static final Field<UUID> DEFAULT_CREATED_BY_COLUMN = DSL.field("created_by", SQLDataType.UUID.nullable(false).defaultValue(PLACEHOLDER_CREATED_BY));
+
+  private static final Set<String> TABLE_TO_TRACK = Set.of(
+      "actor_definition",
+      "actor",
+      "actor_oauth_parameter",
+      "connection",
+      "connection_operation",
+      "operation",
+      "stream_reset",
+      "workspace",
+      "workspace_service_account"
+//      "state" todo (cgardens) should we do this one too?
+  );
+
+  private static final Logger LOGGER = LoggerFactory.getLogger(V0_40_17_001__AddAuditLog.class);
+
+  @Override
+  public void migrate(final Context context) throws Exception {
+    LOGGER.info("Running migration: {}", getClass().getSimpleName());
+
+    // Warning: please do not use any jOOQ generated code to write a migration.
+    // As database schema changes, the generated jOOQ code can be deprecated. So
+    // old migration may not compile if there is any generated code.
+    final DSLContext ctx = DSL.using(context.getConnection());
+
+    // set up functions
+    createConfigEventTableAndFunctions(ctx);
+
+    // add created_by to tables
+    for(final String tableName : TABLE_TO_TRACK) {
+      addCreatedByToTable(ctx, tableName);
+      trackUpdatesToTable(ctx, tableName);
+    }
+  }
+
+  private static void createConfigEventTableAndFunctions(final DSLContext ctx) throws IOException {
+    ctx.execute(MoreResources.readResource("sql_functions_and_triggers/config_events.sql"));
+    LOGGER.info("created config_event table and config event functions and triggers");
+  }
+
+  private static void addCreatedByToTable(final DSLContext ctx, final String tableName) {
+    ctx.alterTable(tableName).addColumn(DEFAULT_CREATED_BY_COLUMN);
+    LOGGER.info(String.format("created created_by column for %s table.", tableName));
+  }
+
+  private static void trackUpdatesToTable(final DSLContext ctx, final String tableName) {
+     ctx.execute(String.format("SELECT create_config_event_trigger('%s');", tableName));
+    LOGGER.info(String.format("added event trigger for %s table.", tableName));
+  }
+}
--- airbyte-db/db-lib/src/main/resources/sql_functions_and_triggers/config_events.sql
+++ airbyte-db/db-lib/src/main/resources/sql_functions_and_triggers/config_events.sql
@@ -12,16 +12,21 @@ usage: to add this tracking ability to a table:
 -- #1 - create config_event table
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
+CREATE TYPE operation_type AS ENUM ('insert', 'update', 'delete');
+
 CREATE TABLE IF NOT EXISTS config_event
 (
     "id"             uuid PRIMARY KEY         DEFAULT uuid_generate_v4(),
     "operation_type" operation_type  NOT NULL,
-    "config_type"    config_type     NOT NULL,
+    "config_type"    varchar(256)    NOT NULL,
     "config_id"      uuid            NOT NULL,
     "result"         jsonb           NULL,
-    "created_at"     timestamptz(35) NOT NULL DEFAULT NULL,
+    "created_at"     timestamptz(35) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "created_by"     uuid            NULL
 );
+
+CREATE INDEX config_id_idx ON config_event (config_id);
+CREATE INDEX config_type_and_id_idx ON config_event (config_type, config_id);
 
 -- #2 - declare function that saves copy of the record after it is modified.
 -- WARNING: target table must define the following 2 columns: id (uuid, non-nullable) and updated_by (uuid, nullable)
@@ -29,7 +34,7 @@ CREATE OR REPLACE FUNCTION process_config_event() RETURNS TRIGGER AS
 $$
 DECLARE
     -- functions used in triggers cannot take arguments in the traditional way. they can be passed in via TG_ARGS though.
-    configtype config_type;
+    configtype varchar(256);
 BEGIN
     configtype := TG_ARGV[0];
     /*
@@ -38,15 +43,15 @@ BEGIN
     IF (TG_OP = 'DELETE') THEN
         -- note: for deletes, we cannot track how did the delete.
         INSERT INTO config_event (operation_type, config_type, config_id, result, created_at, created_by)
-        VALUES ('delete', configtype, OLD.id, null, now(), null);
+        VALUES ('delete', configtype, OLD.id, NULL, NOW(), NULL);
         RETURN OLD;
     ELSIF (TG_OP = 'UPDATE') THEN
         INSERT INTO config_event (operation_type, config_type, config_id, result, created_at, created_by)
-        VALUES ('update', configtype, NEW.id, to_jsonb(NEW), now(), NEW.updated_by);
+        VALUES ('update', configtype, NEW.id, to_jsonb(NEW), NOW(), NEW.updated_by);
         RETURN NEW;
     ELSIF (TG_OP = 'INSERT') THEN
         INSERT INTO config_event (operation_type, config_type, config_id, result, created_at, created_by)
-        VALUES ('insert', configtype, NEW.id, to_jsonb(NEW), now(), NEW.updated_by);
+        VALUES ('insert', configtype, NEW.id, to_jsonb(NEW), NOW(), NEW.updated_by);
         RETURN NEW;
     END IF;
     RETURN NULL; -- result is ignored since this is an AFTER trigger
