5a1c0b7c8b1a2884c0a049a8dc6f580e5a96e778
evantahler <evan@airbyte.io>
2022-10-31 11:37:58 -0700

    UI Basics

--- airbyte-webapp/src/components/ProgressBar/ProgressBar.tsx
+++ airbyte-webapp/src/components/ProgressBar/ProgressBar.tsx
@@ -1,17 +1,50 @@
+import classNames from "classnames";
 import { Line } from "rc-progress";
+import { useIntl } from "react-intl";
 
 import { getJobStatus } from "components/JobItem/JobItem";
 
-import { SynchronousJobRead } from "core/request/AirbyteClient";
+import { AttemptRead, SynchronousJobRead } from "core/request/AirbyteClient";
+import Status from "core/statuses";
 import { JobsWithJobs } from "pages/ConnectionPage/pages/ConnectionItemPage/JobsList";
 
-export var ProgressBar = ({ percent, job }: { percent?: number; job: JobsWithJobs | SynchronousJobRead }) => {
-  if (!percent) {
-    percent = Math.random() * 100;
-  }
+import styles from "./ProgressBar.module.scss";
+
+function isJobsWithJobs(job: JobsWithJobs | SynchronousJobRead): job is JobsWithJobs {
+  return (job as JobsWithJobs).attempts !== undefined;
+}
+
+export var ProgressBar = ({ job }: { job: JobsWithJobs | SynchronousJobRead }) => {
+  const { formatMessage } = useIntl();
+
+  let numerator = 0;
+  let denominator = 0;
+  let totalPercent = -1;
+  let latestAttempt: AttemptRead | undefined;
+
   const jobStatus = getJobStatus(job);
 
-  console.log(job);
+  if (isJobsWithJobs(job)) {
+    if (job.attempts) {
+      latestAttempt = job.attempts[job.attempts?.length - 1];
+      if (latestAttempt && latestAttempt.totalStats) {
+        const totalStats = latestAttempt.totalStats;
+        if (totalStats?.recordsEmitted && totalStats?.estimatedRecords) {
+          numerator = totalStats.recordsEmitted;
+          denominator = totalStats.estimatedRecords;
+        }
+      } else if (latestAttempt && !latestAttempt.totalStats && latestAttempt.streamStats) {
+        for (const stream of latestAttempt.streamStats) {
+          numerator += stream.stats.recordsEmitted ?? 0;
+          denominator += stream.stats.estimatedRecords ?? 0;
+        }
+      }
+    }
+  } else {
+    // TODO... maybe
+  }
+
+  totalPercent = (numerator / denominator) * 100;
 
   // colors from `_colors.scss` TODO: Use the SCSS variables maybe?
   let color = "white";
@@ -30,12 +63,42 @@ export var ProgressBar = ({ percent, job }: { percent?: number; job: JobsWithJob
       break;
     case "succeeded":
       color = "#67dae1";
-      percent = 100; // just to be safe
+      totalPercent = 100; // just to be safe
       break;
     case "cancelled":
-      percent = 0; // just to be safe
+      totalPercent = 0; // just to be safe
       break;
   }
 
-  return <Line percent={percent} strokeColor={[color]} />;
+  return (
+    <div className={classNames(styles.container)}>
+      <Line percent={totalPercent} strokeColor={[color]} />
+      {latestAttempt?.status === Status.RUNNING && latestAttempt.streamStats && (
+        <>
+          <div>
+            {numerator} of {denominator} {formatMessage({ id: "estimate.syncedThusFar" })}
+          </div>
+          <div>
+            <br />
+            <div className={classNames(styles.container)}>Stream Stats:</div>
+            {latestAttempt.streamStats?.map((stream) => {
+              const localNumerator = stream.stats.recordsEmitted;
+              const localDenominator = stream.stats.estimatedRecords;
+
+              return (
+                <div className={classNames(styles.container)}>
+                  <strong>{stream.streamName}</strong> -{" "}
+                  {localNumerator && localDenominator
+                    ? `${Math.round(
+                        (localNumerator * 100) / localDenominator
+                      )}% complete (${localNumerator} / ${localDenominator} records moved)`
+                    : `${localNumerator} records moved so far`}
+                </div>
+              );
+            })}
+          </div>
+        </>
+      )}
+    </div>
+  );
 };
