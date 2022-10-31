713c362ec92e108d0a708484f7a44036c98ffc6e
nataly <nataly@airbyte.io>
2022-10-30 22:43:12 -0400

    Corrects storybook component versions.

--- airbyte-webapp/src/components/GeographyDropdown/index.stories.tsx
+++ airbyte-webapp/src/components/GeographyDropdown/index.stories.tsx
@@ -2,21 +2,6 @@ import { ComponentStory, ComponentMeta } from "@storybook/react";
 
 import { GeographyDropdown } from "./GeographyDropdown";
 
-export const defaultOptions = [
-  {
-    value: "us",
-    label: "us",
-  },
-  {
-    value: "auto",
-    label: "auto",
-  },
-  {
-    value: "eu",
-    label: "eu",
-  },
-];
-
 export default {
   title: "Ui/GeographyDropdown",
   component: GeographyDropdown,
@@ -24,7 +9,23 @@ export default {
 
 const Template: ComponentStory<typeof GeographyDropdown> = (args) => <GeographyDropdown {...args} />;
 
+export const Empty = Template.bind({});
+Empty.args = {};
+
 export const Primary = Template.bind({});
 Primary.args = {
-  options: defaultOptions,
+  options: [
+    {
+      value: "us",
+      label: "us",
+    },
+    {
+      value: "auto",
+      label: "auto",
+    },
+    {
+      value: "eu",
+      label: "eu",
+    },
+  ],
 };
