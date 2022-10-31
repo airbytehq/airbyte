c68e01db5799642c0704cd7ef81660447eb4200c
Alexandre Girard <alexandre@airbyte.io>
2022-10-31 09:34:38 -0700

    script to run acceptance tests with local cdk

---
+++ airbyte-integrations/connectors/source-greenhouse/run-acceptance-tests-local-cdk.sh
@@ -0,0 +1,9 @@
+CURRENT_DIR="$PWD"
+DIR=$(basename "$PWD")
+echo $DIR
+cd $CURRENT_DIR
+docker build --no-cache . -t airbyte/$DIR:dev
+cd ~/code/airbyte
+docker build --no-cache . -t airbyte/$DIR:test -f airbyte-integrations/connectors/$DIR/test/Dockerfile
+cd $CURRENT_DIR
+python3 -m pytest integration_tests -p integration_tests.acceptance
