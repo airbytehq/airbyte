ae7b7fbc8078f205f876e294b080373e9097ae13
Alexandre Girard <alexandre@airbyte.io>
2022-10-31 17:48:37 -0700

    reset to master

--- airbyte-integrations/bases/source-acceptance-test/source_acceptance_test/conftest.py
+++ airbyte-integrations/bases/source-acceptance-test/source_acceptance_test/conftest.py
@@ -97,8 +97,7 @@ def configured_catalog_fixture(configured_catalog_path, discovered_catalog) -> C
 
 @pytest.fixture(name="image_tag")
 def image_tag_fixture(acceptance_test_config) -> str:
-    connector_image = acceptance_test_config.connector_image
-    return connector_image
+    return acceptance_test_config.connector_image
 
 
 @pytest.fixture(name="connector_config")
@@ -131,13 +130,7 @@ def connector_spec_fixture(connector_spec_path) -> ConnectorSpecification:
 
 @pytest.fixture(name="docker_runner")
 def docker_runner_fixture(image_tag, tmp_path) -> ConnectorRunner:
-    image_name = get_docker_image_tag(image_tag)
-    return ConnectorRunner(image_name, volume=tmp_path)
-
-
-def get_docker_image_tag(image_tag):
-    # return image_tag.split(":")[0] + ":test"
-    return image_tag
+    return ConnectorRunner(image_tag, volume=tmp_path)
 
 
 @pytest.fixture(name="previous_connector_image_name")
@@ -167,7 +160,7 @@ def previous_connector_docker_runner_fixture(previous_connector_image_name, tmp_
 @pytest.fixture(scope="session", autouse=True)
 def pull_docker_image(acceptance_test_config) -> None:
     """Startup fixture to pull docker image"""
-    image_name = get_docker_image_tag(acceptance_test_config.connector_image)
+    image_name = acceptance_test_config.connector_image
     config_filename = "acceptance-test-config.yml"
     try:
         ConnectorRunner(image_name=image_name, volume=Path("."))
