36f90be8a1d915aec244ae827c46f5822f236a81
neelshah2409 <shahneel2409@gmail.com>
2022-11-01 23:00:55 +0530

    Added Jokes API

--- airbyte-integrations/bases/base-normalization/integration_tests/normalization_test_output/postgres/test_nested_streams/second_output/airbyte_incremental/test_normalization/nested_stream_with_c__ion_double_array_data.sql
+++
@@ -1,9 +0,0 @@
-
-      
-
-    insert into "postgres".test_normalization."nested_stream_with_c__ion_double_array_data" ("_airbyte_partition_hashid", "id", "_airbyte_ab_id", "_airbyte_emitted_at", "_airbyte_normalized_at", "_airbyte_double_array_data_hashid")
-    (
-        select "_airbyte_partition_hashid", "id", "_airbyte_ab_id", "_airbyte_emitted_at", "_airbyte_normalized_at", "_airbyte_double_array_data_hashid"
-        from "nested_stream_with_c__ion_double_array_dat__dbt_tmp"
-    )
-  
