0188ffd2c114944e032ae6f3b1f139d1a3a765a8
Joey Marshment-Howell <josephkmh@users.noreply.github.com>
2022-11-02 09:40:44 +0100

    🪟🔧 Remove styled components (round 1) (#18766)
    * refactor EditorHeader (untested)
    
    * refactor BaseClearView
    
    * delete unused Subtitle
    
    * refactor ConfirmationModal
    
    * refactor Arrow
    
    * refactor BulkHeader
    
    * refactor CatalogTreeSearch
    
    * refactor StreamFieldTable
    
    * refactor StreamHeader
    
    * refactor ConnectorIcon
    
    * refactor TreeRowWrapper
    
    * refactor DeleteBlock
    
    * refactor EmptyResourceBlock
    
    * revert unintended element change

--- airbyte-webapp/src/components/common/ConfirmationModal/ConfirmationModal.tsx
+++ airbyte-webapp/src/components/common/ConfirmationModal/ConfirmationModal.tsx
@@ -1,25 +1,11 @@
 import React from "react";
 import { FormattedMessage } from "react-intl";
-import styled from "styled-components";
 
 import { Button } from "components/ui/Button";
 import { Modal } from "components/ui/Modal";
 
 import useLoadingState from "../../../hooks/useLoadingState";
 import styles from "./ConfirmationModal.module.scss";
-
-const Content = styled.div`
-  width: 585px;
-  font-size: 14px;
-  padding: 25px;
-  white-space: pre-line;
-`;
-
-const ButtonContent = styled.div`
-  margin-top: 26px;
-  display: flex;
-  justify-content: flex-end;
-`;
 
 export interface ConfirmationModalProps {
   onClose: () => void;
@@ -45,9 +31,9 @@ export const ConfirmationModal: React.FC<ConfirmationModalProps> = ({
 
   return (
     <Modal onClose={onClose} title={<FormattedMessage id={title} />}>
-      <Content>
+      <div className={styles.content}>
         <FormattedMessage id={text} />
-        <ButtonContent>
+        <div className={styles.buttonContent}>
           <Button
             className={styles.buttonWithMargin}
             onClick={onClose}
@@ -60,8 +46,8 @@ export const ConfirmationModal: React.FC<ConfirmationModalProps> = ({
           <Button variant="danger" onClick={onSubmitBtnClick} data-id={submitButtonDataId} isLoading={isLoading}>
             <FormattedMessage id={submitButtonText} />
           </Button>
-        </ButtonContent>
-      </Content>
+        </div>
+      </div>
     </Modal>
   );
 };
