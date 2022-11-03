2d91e182862013ef2668068d280608dd78b8e9a7
Alexandre Girard <alexandre@airbyte.io>
2022-11-03 12:51:22 -0700

    reset FE components to master

--- airbyte-webapp/src/components/YamlEditor/YamlEditor.tsx
+++ airbyte-webapp/src/components/YamlEditor/YamlEditor.tsx
@@ -3,44 +3,15 @@ import { CodeEditor } from "components/ui/CodeEditor";
 import { useConnectorBuilderState } from "services/connectorBuilder/ConnectorBuilderStateService";
 
 import { DownloadYamlButton } from "./DownloadYamlButton";
-import { TestBuilderServer } from "./TestBuilderServer";
 import styles from "./YamlEditor.module.scss";
 
 export const YamlEditor: React.FC = () => {
-  const [locallyStoredEditorValue, setLocallyStoredEditorValue] = useLocalStorage<string>(
-    "connectorBuilderEditorContent",
-    template
-  );
-  const [editorValue, setEditorValue] = useState(locallyStoredEditorValue ?? "");
-  useDebounce(() => setLocallyStoredEditorValue(editorValue), 500, [editorValue]);
-
-  const handleEditorChange = (value: string | undefined) => {
-    setEditorValue(value ?? "");
-  };
-
-  const setEditorTheme = (monaco: Monaco) => {
-    monaco.editor.defineTheme("airbyte", {
-      base: "vs-dark",
-      inherit: true,
-      rules: [
-        // add logic to convert these two six character
-        { token: "string", foreground: "#ffffff" },
-        { token: "type", foreground: styles.tokenType },
-        { token: "number", foreground: styles.tokenNumber },
-        { token: "delimiter", foreground: styles.tokenDelimiter },
-        { token: "keyword", foreground: styles.tokenKeyword },
-      ],
-      colors: {
-        "editor.background": "#00000000", // transparent, so that parent background is shown instead
-      },
-    });
-
-    monaco.editor.setTheme("airbyte");
-  };
+  const { yamlManifest, setYamlManifest } = useConnectorBuilderState();
 
   return (
     <div className={styles.container}>
       <div className={styles.control}>
+        <DownloadYamlButton yaml={yamlManifest} />
       </div>
       <div className={styles.editorContainer}>
         <CodeEditor
