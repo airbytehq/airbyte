042520e4e22d1e5c6833eb24eea6b9697f58b001
Edward Gao <edward.gao@airbyte.io>
2022-11-03 13:46:34 -0700

    maybe do certs better?

--- airbyte-integrations/connectors/destination-kafka/src/test-integration/resources/kafka_ssl/create_certs.sh
+++ airbyte-integrations/connectors/destination-kafka/src/test-integration/resources/kafka_ssl/create_certs.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 set -o xtrace
+set -e
 
 # This is mostly based on https://github.com/confluentinc/cp-docker-images/blob/5.3.3-post/examples/kafka-cluster-ssl/secrets/create-certs.sh
 # Differences:
@@ -11,7 +12,7 @@ set -o xtrace
 # Create a certificate authority
 openssl req -new -x509 -keyout snakeoil-ca-1.key -out snakeoil-ca-1.crt -days 365 -subj '/CN=ca1.test.confluent.io/OU=TEST/O=CONFLUENT/L=PaloAlto/S=Ca/C=US' -passin pass:confluent -passout pass:confluent
 
-broker_hostname=$(hostname)
+broker_hostname=broker1.kafka.destinations.test.airbyte.com
 
 ## Create certificate + creds for server (i.e. broker)
 # Create keystores
@@ -23,10 +24,9 @@ keytool -genkey -noprompt \
         -storepass confluent \
         -keypass confluent
 
-# Create CSR, sign the key and import back into keystore
-keytool -keystore kafka.broker1.keystore.jks -alias broker1 -certreq -file broker1.csr -storepass confluent -keypass confluent
-# TODO
-echo << EOF
+# We need to add the broker's IP as a SAN because the test client connects via IP
+local_ip=$(awk '/\|--/ && !/\.0$|\.255$/ {print $2}' /proc/net/fib_trie | grep -v 127.0.0.1 | uniq | head -n 1)
+cat <<EOF > san.cnf
 [req]
 default_bits  = 2048
 distinguished_name = req_distinguished_name
@@ -39,7 +39,7 @@ countryName = US
 stateOrProvinceName = Ca
 localityName = PaloAlto
 organizationName = CONFLUENT
-commonName = 120.0.0.1: Self-signed certificate
+commonName = ${broker_hostname}
 
 [req_ext]
 subjectAltName = @alt_names
@@ -48,38 +48,28 @@ subjectAltName = @alt_names
 subjectAltName = @alt_names
 
 [alt_names]
-IP.1 = 172.17.0.3
-EOF > san.cnf
-# echo 'IP.1 = '$(awk '/\|--/ && !/\.0$|\.255$/ {print $2}' /proc/net/fib_trie | grep -v 127.0.0.1 | uniq | head -n 1) > san.cnf
-openssl req -x509 -nodes -days 730 -newkey rsa:2048 -keyout key.pem -out cert.pem -config san.cnf
-# openssl x509 -req -CA snakeoil-ca-1.crt -CAkey snakeoil-ca-1.key -in broker1.csr -out broker1-ca1-signed.crt -days 9999 -CAcreateserial -passin pass:confluent
+IP.1 = ${local_ip}
+EOF
+# get the broker's private key from keystore so that we can generate the crt using the correct key pair
+keytool -importkeystore \
+    -srckeystore kafka.broker1.keystore.jks \
+    -destkeystore broker1.p12 \
+    -deststoretype PKCS12 \
+    -srcalias broker1 \
+    -srcstorepass confluent \
+    -deststorepass confluent \
+    -destkeypass confluent
+openssl pkcs12 -in broker1.p12  -nodes -nocerts -out broker1.pem -passin pass:confluent
+# Generate and sign the certificate
+openssl req -x509 -nodes -days 730 -new -key broker1.pem -passin pass:confluent -out broker1-ca1-signed.crt -config san.cnf
+# import the cert chain into the broker's keystore
 keytool -keystore kafka.broker1.keystore.jks -alias CARoot -import -file snakeoil-ca-1.crt -storepass confluent -keypass confluent -noprompt
 keytool -keystore kafka.broker1.keystore.jks -alias broker1 -import -file broker1-ca1-signed.crt -storepass confluent -keypass confluent
 
-# Create truststore and import the CA cert.
-keytool -keystore kafka.broker1.truststore.jks -alias CARoot -import -file snakeoil-ca-1.crt -storepass confluent -keypass confluent -noprompt
-
 echo "confluent" > broker1_sslkey_creds
 echo "confluent" > broker1_keystore_creds
-echo "confluent" > broker1_truststore_creds
 
-## Create truststore for client (i.e. producer)
-# # Create keystores
-# keytool -genkey -noprompt \
-#         -alias producer \
-#         -dname "CN=producer.test.confluent.io, OU=TEST, O=CONFLUENT, L=PaloAlto, S=Ca, C=US" \
-#         -keystore kafka.producer.keystore.jks \
-#         -keyalg RSA \
-#         -storepass confluent \
-#         -keypass confluent
-
-# # Create CSR, sign the key and import back into keystore
-# keytool -keystore kafka.producer.keystore.jks -alias producer -certreq -file producer.csr -storepass confluent -keypass confluent
-# openssl x509 -req -CA snakeoil-ca-1.crt -CAkey snakeoil-ca-1.key -in producer.csr -out producer-ca1-signed.crt -days 9999 -CAcreateserial -passin pass:confluent
-# keytool -keystore kafka.producer.keystore.jks -alias CARoot -import -file snakeoil-ca-1.crt -storepass confluent -keypass confluent
-# keytool -keystore kafka.producer.keystore.jks -alias producer -import -file producer-ca1-signed.crt -storepass confluent -keypass confluent
-
-# Create truststore, import the CA cert, and convert JKS -> PKCS12 -> PEM
+# Create truststore for the producer, import the CA cert, and convert JKS -> PKCS12 -> PEM
 keytool -keystore kafka.producer.truststore.jks -alias CARoot -import -file snakeoil-ca-1.crt -storepass confluent -keypass confluent -noprompt
 keytool -importkeystore -srckeystore kafka.producer.truststore.jks -destkeystore kafka.producer.truststore.p12 -srcstoretype jks -deststoretype pkcs12 -noprompt -srcstorepass confluent -deststorepass confluent
 openssl pkcs12 -in kafka.producer.truststore.p12 -out kafka.producer.truststore.pem -passin pass:confluent
