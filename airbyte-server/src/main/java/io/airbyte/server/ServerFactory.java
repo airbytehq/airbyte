1a6f52109a4fb47e6e5955f0158dab315f02574a
Benoit Moriceau <benoit@airbyte.io>
2022-11-03 14:46:52 -0700

    Extract Operation API (#18928)
    * Extract Operation API
    
    * Format

--- airbyte-server/src/main/java/io/airbyte/server/ServerFactory.java
+++ airbyte-server/src/main/java/io/airbyte/server/ServerFactory.java
@@ -27,6 +27,7 @@ import io.airbyte.server.apis.JobsApiController;
 import io.airbyte.server.apis.LogsApiController;
 import io.airbyte.server.apis.NotificationsApiController;
 import io.airbyte.server.apis.OpenapiApiController;
+import io.airbyte.server.apis.OperationApiController;
 import io.airbyte.server.apis.binders.AttemptApiBinder;
 import io.airbyte.server.apis.binders.ConnectionApiBinder;
 import io.airbyte.server.apis.binders.DbMigrationBinder;
@@ -39,6 +40,7 @@ import io.airbyte.server.apis.binders.JobsApiBinder;
 import io.airbyte.server.apis.binders.LogsApiBinder;
 import io.airbyte.server.apis.binders.NotificationApiBinder;
 import io.airbyte.server.apis.binders.OpenapiApiBinder;
+import io.airbyte.server.apis.binders.OperationApiBinder;
 import io.airbyte.server.apis.binders.SourceOauthApiBinder;
 import io.airbyte.server.apis.factories.AttemptApiFactory;
 import io.airbyte.server.apis.factories.ConnectionApiFactory;
@@ -52,6 +54,7 @@ import io.airbyte.server.apis.factories.JobsApiFactory;
 import io.airbyte.server.apis.factories.LogsApiFactory;
 import io.airbyte.server.apis.factories.NotificationsApiFactory;
 import io.airbyte.server.apis.factories.OpenapiApiFactory;
+import io.airbyte.server.apis.factories.OperationApiFactory;
 import io.airbyte.server.apis.factories.SourceOauthApiFactory;
 import io.airbyte.server.handlers.AttemptHandler;
 import io.airbyte.server.handlers.ConnectionsHandler;
@@ -190,6 +193,8 @@ public interface ServerFactory {
 
       NotificationsApiFactory.setValues(workspacesHandler);
 
+      OperationApiFactory.setValues(operationsHandler);
+
       OpenapiApiFactory.setValues(openApiConfigHandler);
 
       // server configurations
@@ -207,6 +212,7 @@ public interface ServerFactory {
           LogsApiController.class,
           NotificationsApiController.class,
           OpenapiApiController.class,
+          OperationApiController.class,
           SourceOauthApiFactory.class);
 
       final Set<Object> components = Set.of(
@@ -224,6 +230,7 @@ public interface ServerFactory {
           new LogsApiBinder(),
           new NotificationApiBinder(),
           new OpenapiApiBinder(),
+          new OperationApiBinder(),
           new SourceOauthApiBinder());
 
       // construct server
