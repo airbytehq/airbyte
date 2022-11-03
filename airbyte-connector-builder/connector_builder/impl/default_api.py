601b156ef15098ef968f97fe982484b0af59a311
sherifnada <snadalive@gmail.com>
2022-11-03 13:49:12 -0700

    rename gradle task

--- airbyte-connector-builder/connector_builder/impl/default_api.py
+++ airbyte-connector-builder/connector_builder/impl/default_api.py
@@ -9,11 +9,12 @@ from connector_builder.generated.models.streams_list_request_body import Streams
 
 
 class DefaultApiImpl(DefaultApi):
-    async def list_streams(self, streams_list_request_body: StreamsListRequestBody = Body(None, description="")) -> StreamsListRead:
-        raise Exception("not yet implemented")
-
-    async def read_stream(self, stream_read_request_body: StreamReadRequestBody = Body(None, description="")) -> StreamRead:
-        raise Exception("not yet implemented")
 
     async def get_manifest_template(self) -> str:
-        return "Hello world!"
+        return "Hello World"
+
+    async def list_streams(self, streams_list_request_body: StreamsListRequestBody = Body(None, description="")) -> StreamsListRead:
+        pass
+
+    async def read_stream(self, stream_read_request_body: StreamReadRequestBody = Body(None, description="")) -> StreamRead:
+        pass
