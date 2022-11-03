1965e26362cfc39fbb6f812d0940ea7be0723e38
Leibniz.Hu <leibnizhu@gmail.com>
2022-11-03 19:06:35 +0800

    test: Add HadoopCatalog integration tests

---
+++ airbyte-integrations/connectors/destination-iceberg/src/test-integration/java/io/airbyte/integrations/destination/iceberg/IcebergHadoopCatalogS3AvroIntegrationTest.java
@@ -0,0 +1,14 @@
+package io.airbyte.integrations.destination.iceberg;
+
+import io.airbyte.integrations.destination.iceberg.config.format.DataFileFormat;
+
+/**
+ * @author Leibniz on 2022/11/3.
+ */
+public class IcebergHadoopCatalogS3AvroIntegrationTest extends AbstractIcebergHadoopCatalogS3IntegrationTest {
+
+    @Override
+    DataFileFormat fileFormat() {
+        return DataFileFormat.AVRO;
+    }
+}
