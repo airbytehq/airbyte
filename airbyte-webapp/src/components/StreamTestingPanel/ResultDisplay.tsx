22837e06a47a85b6f5675d82a24905cd39b4fe7b
lmossman <lake@airbyte.io>
2022-11-04 15:21:58 -0700

    pull LogsDisplay out into its own component to simplify ResultDisplay

--- airbyte-webapp/src/components/StreamTestingPanel/ResultDisplay.tsx
+++ airbyte-webapp/src/components/StreamTestingPanel/ResultDisplay.tsx
@@ -1,8 +1,6 @@
 import classNames from "classnames";
-import { FormattedMessage } from "react-intl";
 
 import { Paginator } from "components/ui/Paginator";
-import { ResizablePanels } from "components/ui/ResizablePanels";
 import { Text } from "components/ui/Text";
 
 import { StreamRead } from "core/request/ConnectorBuilderClient";
@@ -25,52 +23,22 @@ export const ResultDisplay: React.FC<ResultDisplayProps> = ({ streamRead, classN
   const page = slice.pages[selectedPage];
 
   return (
-    <ResizablePanels
-      className={classNames(className, styles.container)}
-      orientation="horizontal"
-      firstPanel={{
-        className: styles.resultContainer,
-        children: (
-          <>
-            {streamRead.slices.length > 1 && (
-              <SliceSelector
-                className={styles.sliceSelector}
-                slices={streamRead.slices}
-                selectedSliceIndex={selectedSlice}
-                onSelect={setSelectedSlice}
-              />
-            )}
-            <PageDisplay className={styles.pageDisplay} page={page} />
-            {slice.pages.length > 1 && (
-              <div className={styles.paginator}>
-                <Text className={styles.pageLabel}>Page:</Text>
-                <Paginator numPages={numPages} onPageChange={setSelectedPage} selectedPage={selectedPage} />
-              </div>
-            )}
-          </>
-        ),
-        minWidth: 120,
-      }}
-      secondPanel={{
-        className: styles.logsContainer,
-        children: (
-          <>
-            <div className={styles.logsHeader}>
-              <Text size="sm" bold>
-                <FormattedMessage id="connectorBuilder.connectorLogs" />
-              </Text>
-              <Text className={styles.numLogsDisplay} size="xs" bold>
-                {streamRead.logs.length}
-              </Text>
-            </div>
-            <div className={styles.logsDisplay}>
-              <pre>{JSON.stringify(streamRead.logs, null, 2)}</pre>
-            </div>
-          </>
-        ),
-        minWidth: 30,
-        flex: 0,
-      }}
-    />
+    <div className={classNames(className, styles.container)}>
+      {streamRead.slices.length > 1 && (
+        <SliceSelector
+          className={styles.sliceSelector}
+          slices={streamRead.slices}
+          selectedSliceIndex={selectedSlice}
+          onSelect={setSelectedSlice}
+        />
+      )}
+      <PageDisplay className={styles.pageDisplay} page={page} />
+      {slice.pages.length > 1 && (
+        <div className={styles.paginator}>
+          <Text className={styles.pageLabel}>Page:</Text>
+          <Paginator numPages={numPages} onPageChange={setSelectedPage} selectedPage={selectedPage} />
+        </div>
+      )}
+    </div>
   );
 };
