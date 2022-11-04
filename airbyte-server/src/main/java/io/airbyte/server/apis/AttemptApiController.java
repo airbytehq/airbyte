5833acaf8019727fc11b1bfc023c03d100d7bfd4
Davin Chia <davinchia@gmail.com>
2022-11-03 22:29:15 -0700

    Add the save_stats route. Add estimatedBytes/Records to the existing objects.

--- airbyte-server/src/main/java/io/airbyte/server/apis/AttemptApiController.java
+++ airbyte-server/src/main/java/io/airbyte/server/apis/AttemptApiController.java
@@ -6,17 +6,23 @@ package io.airbyte.server.apis;
 
 import io.airbyte.api.generated.AttemptApi;
 import io.airbyte.api.model.generated.InternalOperationResult;
+import io.airbyte.api.model.generated.SaveStatsRequestBody;
 import io.airbyte.api.model.generated.SetWorkflowInAttemptRequestBody;
 import io.airbyte.server.handlers.AttemptHandler;
 import javax.ws.rs.Path;
 
-@Path("/v1/attempt/set_workflow_in_attempt")
+@Path("/v1/attempt/")
 public class AttemptApiController implements AttemptApi {
 
   private final AttemptHandler attemptHandler;
 
   public AttemptApiController(final AttemptHandler attemptHandler) {
     this.attemptHandler = attemptHandler;
+  }
+
+  @Override
+  public InternalOperationResult saveStats(final SaveStatsRequestBody saveStatsRequestBody) {
+    throw new UnsupportedOperationException();
   }
 
   @Override
