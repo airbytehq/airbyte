05e1cb9034d8844e8e8c1b94b17ba5f787f67266
Sofiia Zaitseva <108751383+SofiiaZaitseva@users.noreply.github.com>
2022-11-07 19:05:57 +0200

    add tests

--- airbyte-webapp-e2e-tests/cypress/integration/source.spec.ts
+++ airbyte-webapp-e2e-tests/cypress/integration/source.spec.ts
@@ -1,6 +1,12 @@
-import { appendRandomString } from "commands/common";
+import { appendRandomString, submitButtonClick } from "commands/common";
 import { createPostgresSource, deleteSource, updateSource } from "commands/source";
 import { initialSetupCompleted } from "commands/workspaces";
+import { goToSourcePage, openNewSourceForm } from "pages/sourcePage";
+import { openHomepage } from "pages/sidebar";
+import { selectServiceType } from "pages/createConnectorPage";
+import { fillPokeAPIForm } from "commands/connector";
+import { should } from "chai";
+
 
 describe("Source main actions", () => {
   beforeEach(() => {
@@ -8,9 +14,13 @@ describe("Source main actions", () => {
   });
 
   it("Create new source", () => {
+    cy.intercept("/api/v1/sources/create").as("createSource");
     createPostgresSource("Test source cypress");
 
-    cy.url().should("include", `/source/`);
+    cy.wait("@createSource", {timeout: 30000}).then((interception) => {
+      assert("include", `/source/` + interception.response?.body.Id)});
+
+    //cy.url().should("include", `/source/`);
   });
 
   //TODO: add update source on some other connector or create 1 more user for pg
@@ -30,5 +40,62 @@ describe("Source main actions", () => {
 
     cy.visit("/");
     cy.get("div").contains(sourceName).should("not.exist");
+  });
+});
+
+describe("Unsaved changes modal", () => {
+  beforeEach(() => {
+    initialSetupCompleted();
+  });
+
+  it("Check leaving Source page without any changes", () => {
+    goToSourcePage();
+    openNewSourceForm();
+
+    openHomepage();
+
+    cy.url().should("include", `/onboarding`);
+  });
+
+  it("Check leaving Source page without any changes after selection type", () => {
+    goToSourcePage();
+    openNewSourceForm();
+    selectServiceType("PokeAPI");
+
+    openHomepage();
+
+    cy.url().should("include", `/onboarding`);
+  });
+
+  it("Check leaving Source page without any changes", () => {
+    goToSourcePage();
+    openNewSourceForm();
+    fillPokeAPIForm("testName", "ditto");
+
+    openHomepage();
+
+    cy.get("#headlessui-portal-root h5").should("exist");
+    cy.get("#headlessui-portal-root h5").should("have.text", "Discard changes");
+    cy.get("#headlessui-portal-root [class*='ConfirmationModal_content']")
+      .should("have.text", "There are unsaved changes. Are you sure you want to discard your changes?CancelDiscard changes");
+  });
+
+  //https://github.com/airbytehq/airbyte/issues/18246
+  it.skip("Check leaving Source page after failing testing", () => {
+    cy.intercept("/api/v1/scheduler/sources/check_connection").as("checkSourceUpdateConnection");
+
+    goToSourcePage();
+    openNewSourceForm();
+    fillPokeAPIForm("testName", "name");
+    submitButtonClick();
+
+    cy.wait("@checkSourceUpdateConnection", {timeout: 5000});
+
+    openHomepage();
+
+    cy.get("#headlessui-portal-root h5").should("exist");
+    cy.get("#headlessui-portal-root h5").should("have.text", "Discard changes");
+    cy.get("#headlessui-portal-root [class*='ConfirmationModal_content']")
+      .should("have.text", "There are unsaved changes. Are you sure you want to discard your changes?CancelDiscard changes");
   });
 });
