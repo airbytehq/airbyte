75f525b710484e2fe7251d88e0cf1fefb8706186
Alexandre Girard <alexandre@airbyte.io>
2022-11-07 08:33:31 -0800

    symlink

--- airbyte-integrations/connectors/source-sendgrid/setup.py
+++ airbyte-integrations/connectors/source-sendgrid/setup.py
@@ -6,7 +6,7 @@
 from setuptools import find_packages, setup
 
 MAIN_REQUIREMENTS = [
-    "airbyte-cdk",
+    "airbyte-cdk @ file://./airbyte-cdk#egg=airbyte_cdk",
     "backoff",
     "requests",
 ]
