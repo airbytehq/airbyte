60f467fb47d16d691987e3d891b6123172127978
andrii.leonets <aleonets@gmail.com>
2022-11-07 17:42:53 +0200

    manual .sh files format

--- airbyte-integrations/connectors/source-clickhouse/acceptance-test-docker.sh
+++ airbyte-integrations/connectors/source-clickhouse/acceptance-test-docker.sh
@@ -8,8 +8,8 @@ docker pull airbyte/source-acceptance-test:latest
 
 # Run
 docker run --rm -it \
-    -v /var/run/docker.sock:/var/run/docker.sock \
-    -v /tmp:/tmp \
-    -v $(pwd):/test_input \
-    airbyte/source-acceptance-test \
-    --acceptance-test-config /test_input
+  -v /var/run/docker.sock:/var/run/docker.sock \
+  -v /tmp:/tmp \
+  -v $(pwd):/test_input \
+  airbyte/source-acceptance-test \
+  --acceptance-test-config /test_input
