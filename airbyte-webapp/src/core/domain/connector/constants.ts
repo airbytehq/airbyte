5f1fe4ab998aeaccdbacf231d83b23c9063064fb
Ryan Fu <ryan.fu@airbyte.io>
2022-11-08 13:21:15 -0800

    Adds TLS check to mongodb destination and migrates util constants (#18892)
    * Adds TLS check to mongodb destination and migrates util constants
    
    * Migrates MongodbSourceUitls to general purprose Utils file
    
    * Updates expected_spec.json to include SSH tunnel
    
    * Bumps connector version and removes connector from being hidden in UI
    
    * auto-bump connector version
    
    Co-authored-by: Octavia Squidington III <octavia-squidington-iii@users.noreply.github.com>

--- airbyte-webapp/src/core/domain/connector/constants.ts
+++ airbyte-webapp/src/core/domain/connector/constants.ts
@@ -21,7 +21,6 @@ export const getExcludedConnectorIds = (workspaceId: string) =>
         "707456df-6f4f-4ced-b5c6-03f73bcad1c5", // hide Cassandra Destination https://github.com/airbytehq/airbyte-cloud/issues/2606
         "9f760101-60ae-462f-9ee6-b7a9dafd454d", // hide Kafka Destination https://github.com/airbytehq/airbyte-cloud/issues/2610
         "294a4790-429b-40ae-9516-49826b9702e1", // hide MariaDB Destination https://github.com/airbytehq/airbyte-cloud/issues/2611
-        "8b746512-8c2e-6ac1-4adc-b59faafd473c", // hide MongoDB Destination https://github.com/airbytehq/airbyte-cloud/issues/2612
         "f3802bc4-5406-4752-9e8d-01e504ca8194", // hide MQTT Destination https://github.com/airbytehq/airbyte-cloud/issues/2613
         "2340cbba-358e-11ec-8d3d-0242ac130203", // hide Pular Destination https://github.com/airbytehq/airbyte-cloud/issues/2614
         "2c9d93a7-9a17-4789-9de9-f46f0097eb70", // hide Rockset Destination https://github.com/airbytehq/airbyte-cloud/issues/2615
