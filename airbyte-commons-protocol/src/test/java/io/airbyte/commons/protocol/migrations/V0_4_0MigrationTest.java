b9275bd3fdb841967b7ec549b1ae9ba8dd5df37b
Edward Gao <edward.gao@airbyte.io>
2022-11-08 15:41:33 -0800

    [wip] add failing test for other primitive types

--- airbyte-commons-protocol/src/test/java/io/airbyte/commons/protocol/migrations/V0_4_0MigrationTest.java
+++ airbyte-commons-protocol/src/test/java/io/airbyte/commons/protocol/migrations/V0_4_0MigrationTest.java
@@ -16,7 +16,6 @@ import org.junit.jupiter.api.Test;
 
 public class V0_4_0MigrationTest {
 
-  private static final ObjectMapper MAPPER = new ObjectMapper();
   private static final String STREAM_NAME = "TEST_STREAM";
 
   private AirbyteMessageMigrationV0_4_0 migration;
@@ -33,8 +32,10 @@ public class V0_4_0MigrationTest {
   }
 
   @Test
-  public void testBasicUpgrade() throws JsonProcessingException {
-    JsonNode oldSchema = MAPPER.readTree("""
+  public void testBasicUpgrade() {
+    // This isn't actually a valid stream schema (since it's not an object)
+    // but
+    JsonNode oldSchema = Jsons.deserialize("""
         {
           "type": "string"
         }
@@ -62,6 +63,103 @@ public class V0_4_0MigrationTest {
     assertEquals(
         expectedMessage,
         upgradedMessage
+    );
+  }
+
+  @Test
+  public void testUpgradeAllPrimitives() {
+    JsonNode oldSchema = Jsons.deserialize("""
+        {
+          "type": "object",
+          "properties": {
+            "example_string": {
+              "type": "string"
+            },
+            "example_number": {
+              "type": "number"
+            },
+            "example_integer": {
+              "type": "integer"
+            },
+            "example_airbyte_integer": {
+              "type": "number",
+              "airbyte_type": "integer"
+            },
+            "example_boolean": {
+              "type": "boolean"
+            },
+            "example_timestamptz": {
+              "type": "string",
+              "format": "date-time",
+              "airbyte_type": "timestamp_with_timezone"
+            },
+            "example_timestamp_without_tz": {
+              "type": "string",
+              "format": "date-time",
+              "airbyte_type": "timestamp_without_timezone"
+            },
+            "example_timez": {
+              "type": "string",
+              "format": "time",
+              "airbyte_type": "time_with_timezone"
+            },
+            "example_time_without_tz": {
+              "type": "string",
+              "format": "time",
+              "airbyte_type": "time_without_timezone"
+            },
+            "example_date": {
+              "type": "string",
+              "format": "date"
+            }
+          }
+        }
+        """);
+
+    io.airbyte.protocol.models.v0.AirbyteMessage upgradedMessage = migration.upgrade(createCatalogMessage(oldSchema));
+
+    JsonNode expectedSchema = Jsons.deserialize(
+        """
+            {
+              "type": "object",
+              "properties": {
+                "example_string": {
+                  "$ref": "WellKnownTypes.json#definitions/String"
+                },
+                "example_number": {
+                  "$ref": "WellKnownTypes.json#definitions/Number"
+                },
+                "example_integer": {
+                  "$ref": "WellKnownTypes.json#definitions/Integer"
+                },
+                "example_airbyte_integer": {
+                  "$ref": "WellKnownTypes.json#definitions/Integer"
+                },
+                "example_boolean": {
+                  "$ref": "WellKnownTypes.json#definitions/Boolean"
+                },
+                "example_timestamptz": {
+                  "$ref": "WellKnownTypes.json#definitions/TimestampWithTimezone"
+                },
+                "example_timestamp_without_tz": {
+                  "$ref": "WellKnownTypes.json#definitions/TimestampWithoutTimezone"
+                },
+                "example_timez": {
+                  "$ref": "WellKnownTypes.json#definitions/TimeWithTimezone"
+                },
+                "example_time_without_tz": {
+                  "$ref": "WellKnownTypes.json#definitions/TimeWithoutTimezone"
+                },
+                "example_date": {
+                  "$ref": "WellKnownTypes.json#definitions/Date"
+                }
+              }
+            }
+            """
+    );
+    assertEquals(
+        expectedSchema,
+        upgradedMessage.getCatalog().getStreams().get(0).getJsonSchema()
     );
   }
 
