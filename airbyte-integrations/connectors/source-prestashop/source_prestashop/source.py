f67316cdd0caba4f500de826da65c8096a57037a
Sergey Chvalyuk <grubberr@gmail.com>
2022-11-08 11:17:54 +0200

    _validate_and_transform added
    Signed-off-by: Sergey Chvalyuk <grubberr@gmail.com>

--- airbyte-integrations/connectors/source-prestashop/source_prestashop/source.py
+++ airbyte-integrations/connectors/source-prestashop/source_prestashop/source.py
@@ -2,7 +2,6 @@
 # Copyright (c) 2022 Airbyte, Inc., all rights reserved.
 #
 
-from airbyte_cdk.sources.declarative.yaml_declarative_source import YamlDeclarativeSource
 
 """
 This file provides the necessary constructs to interpret a provided declarative YAML configuration file into
@@ -11,8 +10,39 @@ source connector.
 WARNING: Do not modify this file.
 """
 
+import logging
+import re
+from typing import Any, Iterator, List, Mapping, MutableMapping, Union
+
+from airbyte_cdk.models import AirbyteCatalog, AirbyteConnectionStatus, AirbyteMessage, AirbyteStateMessage, ConfiguredAirbyteCatalog
+from airbyte_cdk.sources.declarative.yaml_declarative_source import YamlDeclarativeSource
+
 
 # Declarative Source
 class SourcePrestashop(YamlDeclarativeSource):
     def __init__(self):
         super().__init__(**{"path_to_yaml": "prestashop.yaml"})
+
+    def _validate_and_transform(self, config: Mapping[str, Any]):
+        if not config.get("_allow_http"):
+            if re.match(r"^http://", config["url"], re.I):
+                raise Exception(f"Invalid url: {config['url']}, only https scheme is allowed")
+        return config
+
+    def discover(self, logger: logging.Logger, config: Mapping[str, Any]) -> AirbyteCatalog:
+        config = self._validate_and_transform(config)
+        return super().discover(logger, config)
+
+    def check(self, logger: logging.Logger, config: Mapping[str, Any]) -> AirbyteConnectionStatus:
+        config = self._validate_and_transform(config)
+        return super().check(logger, config)
+
+    def read(
+        self,
+        logger: logging.Logger,
+        config: Mapping[str, Any],
+        catalog: ConfiguredAirbyteCatalog,
+        state: Union[List[AirbyteStateMessage], MutableMapping[str, Any]] = None,
+    ) -> Iterator[AirbyteMessage]:
+        config = self._validate_and_transform(config)
+        return super().read(logger, config, catalog, state)
