64b50adfdd847cd610b058074620f93e42dbef36
Alexandre Girard <alexandre@airbyte.io>
2022-11-09 13:07:26 -0800

    Connector-builder server stub (#18410)
    * init
    
    * bad copy/paste
    
    * move to top level
    
    * Revert "move to top level"
    
    This reverts commit aca3534d389ba27c6a53bb92d13a6147909bd6e8.
    
    * attempt to wire up connector builder frontend to server
    
    * copy from octaviacli
    
    * fix connection to builder server
    
    * update
    
    * delete
    
    * Update
    
    * delete python-version
    
    * Revert "delete python-version"
    
    This reverts commit f9258a77550d094d2b6fad4701afda19897fdd0b.
    
    * setup python
    
    * install python
    
    * rename
    
    * kube stuff
    
    * Install python
    
    * missing kube file
    
    * rename
    
    * Update files
    
    * Update bumpversion
    
    * install python
    
    * try with different entrypoint
    
    * rename container
    
    * point to docker-compose.yaml file
    
    * derp
    
    * copy acceptance_test.sh
    
    * copy from acceptance tests
    
    * delete cruft
    
    * update
    
    * remove application env
    
    * reset
    
    * reset to master
    
    * update
    
    * skip comprehensive incremental tests
    
    * Revert "skip comprehensive incremental tests"
    
    This reverts commit 9cee657596663ff6093e5a4bae5de35f16c71433.
    
    * reset to master
    
    * remove cruft
    
    * delete superfluous steps
    
    * update port to 8003
    
    * reset to master
    
    * Update publish docker
    
    * move openapi spec to airbyte-connector-builder
    
    * point to openapi spec
    
    * dont expose the connector builder to localhost
    
    * reset FE components to master
    
    * Don't deploy the connector-builder
    
    * Revert "Don't deploy the connector-builder"
    
    This reverts commit 3d157494cf576a2b84c2318cb0074978d3241ab9.
    
    * Revert "Revert "Don't deploy the connector-builder""
    
    This reverts commit beac3d48f06efa2496fa3983511920ea919d4159.
    
    * comment out more things related to connector builder server
    
    * more attempts at removing the connector builder
    
    * comment out more things
    
    * Apply suggestions from code review
    
    Co-authored-by: Lake Mossman <lake@airbyte.io>
    
    * Update airbyte-webapp/src/config/configProviders.ts
    
    Co-authored-by: Lake Mossman <lake@airbyte.io>
    
    * update
    
    * rename
    
    * indent
    
    * Revert "move openapi spec to airbyte-connector-builder"
    
    This reverts commit 57dda047235d386e5a5a781288541699db1ab73a.
    
    * Revert "rename"
    
    This reverts commit b2d802b8fa80d2f4441485e1760f488c1a74612f.
    
    * Revert "Revert "rename""
    
    This reverts commit 91db24fd4a5a3f0ffbad35270a0f4bff7397951a.
    
    * point to wrong file in case it fixes the build
    
    * point to right openapi file
    
    * Revert "Revert "move openapi spec to airbyte-connector-builder""
    
    This reverts commit e46a837454e3ad5cc78d5cb9dcd1f6c2df4f5345.
    
    * point to moved file
    
    * fix path
    
    * Update from master
    
    * newline
    
    * Add failing test
    
    * Revert "Add failing test"
    
    This reverts commit ed9fea09b5c7c114d79ffb8f071cb2f073e63fd1.
    
    * comment
    
    * update commented requires
    
    * Add a comment
    
    * 2022
    
    * rename to connector-builder-server
    
    * typo
    
    Co-authored-by: lmossman <lake@airbyte.io>

---
+++ airbyte-connector-builder-server/connector_builder/entrypoint.py
@@ -0,0 +1,30 @@
+#
+# Copyright (c) 2022 Airbyte, Inc., all rights reserved.
+#
+
+from typing import Union
+
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "fastapi"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
