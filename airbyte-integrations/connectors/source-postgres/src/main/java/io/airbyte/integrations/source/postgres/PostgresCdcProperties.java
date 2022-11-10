74b3a198b1d18bf2b6aabaa34cc36fb2ddcc8017
Rodi Reich Zilberman <867491+rodireich@users.noreply.github.com>
2022-11-09 18:46:53 -0800

    sanity

--- airbyte-integrations/connectors/source-postgres/src/main/java/io/airbyte/integrations/source/postgres/PostgresCdcProperties.java
+++ airbyte-integrations/connectors/source-postgres/src/main/java/io/airbyte/integrations/source/postgres/PostgresCdcProperties.java
@@ -56,8 +56,6 @@ public class PostgresCdcProperties {
     props.setProperty("converters", "datetime");
     props.setProperty("datetime.type", PostgresConverter.class.getName());
     props.setProperty("include.unknown.datatypes", "true");
-
-
     props.setProperty("heartbeat.interval.ms", Long.toString(Duration.ofSeconds(HEARTBEAT_FREQUENCY_SEC).toMillis()));
 
     // Check params for SSL connection in config and add properties for CDC SSL connection
