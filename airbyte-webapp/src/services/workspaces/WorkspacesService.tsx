7e3795a86fae58ca78836112d9d34e6b1582fbf9
Tim Roes <tim@airbyte.io>
2022-11-10 22:01:42 +0100

    Make useCurrentWorkspaceId work globally

--- airbyte-webapp/src/services/workspaces/WorkspacesService.tsx
+++ airbyte-webapp/src/services/workspaces/WorkspacesService.tsx
@@ -1,6 +1,6 @@
 import React, { useCallback, useContext, useMemo } from "react";
 import { useMutation, useQueryClient } from "react-query";
-import { useNavigate, useParams } from "react-router-dom";
+import { matchPath, useLocation, useNavigate } from "react-router-dom";
 
 import { Workspace, WorkspaceService } from "core/domain/workspace";
 import { RoutePaths } from "pages/routePaths";
@@ -76,9 +76,8 @@ function useWorkspaceApiService() {
 }
 
 export const useCurrentWorkspaceId = () => {
-  const params = useParams<{ workspaceId: string }>();
-
-  return params.workspaceId as string;
+  const location = useLocation();
+  return matchPath({ path: `/${RoutePaths.Workspaces}/:workspaceId/*` }, location.pathname)?.params.workspaceId ?? "";
 };
 
 export const useCurrentWorkspace = () => {
