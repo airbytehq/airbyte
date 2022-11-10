4749c2042b01f07746bbb6f47d28209d6f8b41d1
Lake Mossman <lake@airbyte.io>
2022-11-10 08:52:11 -0800

    Update airbyte-webapp/src/components/StreamTestingPanel/utils.ts
    Co-authored-by: Tim Roes <tim@airbyte.io>

--- airbyte-webapp/src/components/StreamTestingPanel/utils.ts
+++ airbyte-webapp/src/components/StreamTestingPanel/utils.ts
@@ -1,5 +1,4 @@
 // Take in any, because that is what JSON.stringify() takes in
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-export function formatJson(json: any): string {
+export function formatJson(json: unknown): string {
   return JSON.stringify(json, null, 2);
 }
