b3720940f2d1fe49c62366b3f4e02ed41559eb07
Edward Gao <edward.gao@airbyte.io>
2022-11-10 12:34:54 -0800

    switch to v1

--- airbyte-commons-protocol/src/main/java/io/airbyte/commons/protocol/migrations/AirbyteMessageMigrationV1.java
+++ airbyte-commons-protocol/src/main/java/io/airbyte/commons/protocol/migrations/AirbyteMessageMigrationV1.java
@@ -1,29 +1,232 @@
-/*
- * Copyright (c) 2022 Airbyte, Inc., all rights reserved.
- */
-
 package io.airbyte.commons.protocol.migrations;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.ImmutableSet;
 import io.airbyte.commons.json.Jsons;
-import io.airbyte.commons.version.AirbyteProtocolVersion;
 import io.airbyte.commons.version.Version;
-import io.airbyte.protocol.models.v0.AirbyteMessage;
-import jakarta.inject.Singleton;
+import io.airbyte.protocol.models.AirbyteMessage;
+import io.airbyte.protocol.models.AirbyteMessage.Type;
+import io.airbyte.protocol.models.v0.AirbyteStream;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.stream.StreamSupport;
 
-/**
- * Placeholder AirbyteMessage Migration from v0 to v1
- */
-@Singleton
-public class AirbyteMessageMigrationV1 implements AirbyteMessageMigration<AirbyteMessage, io.airbyte.protocol.models.AirbyteMessage> {
+public class AirbyteMessageMigrationV1 implements AirbyteMessageMigration<AirbyteMessage, io.airbyte.protocol.models.v0.AirbyteMessage> {
+
+  private static final Set<String> PRIMITIVE_TYPES = ImmutableSet.of(
+    "string",
+    "number",
+    "integer",
+    "boolean"
+  );
+  private static final Map<String, String> AIRBYTE_TYPE_TO_REFERENCE_TYPE = ImmutableMap.of(
+      "timestamp_with_timezone", "WellKnownTypes.json#definitions/TimestampWithTimezone",
+      "timestamp_without_timezone", "WellKnownTypes.json#definitions/TimestampWithoutTimezone",
+      "time_with_timezone", "WellKnownTypes.json#definitions/TimeWithTimezone",
+      "time_without_timezone", "WellKnownTypes.json#definitions/TimeWithoutTimezone",
+      "integer", "WellKnownTypes.json#definitions/Integer",
+      // these types never actually use airbyte_type, but including them for consistency
+      "string", "WellKnownTypes.json#definitions/String",
+      "number", "WellKnownTypes.json#definitions/Number",
+      "boolean", "WellKnownTypes.json#definitions/Boolean",
+      "date", "WellKnownTypes.json#definitions/Date"
+  );
 
   @Override
-  public AirbyteMessage downgrade(io.airbyte.protocol.models.AirbyteMessage message) {
-    return Jsons.object(Jsons.jsonNode(message), AirbyteMessage.class);
+  public AirbyteMessage downgrade(io.airbyte.protocol.models.v0.AirbyteMessage oldMessage) {
+    return null;
   }
 
   @Override
-  public io.airbyte.protocol.models.AirbyteMessage upgrade(AirbyteMessage message) {
-    return Jsons.object(Jsons.jsonNode(message), io.airbyte.protocol.models.AirbyteMessage.class);
+  public io.airbyte.protocol.models.v0.AirbyteMessage upgrade(AirbyteMessage oldMessage) {
+    // We're not introducing any changes to the structure of the record/catalog
+    // so just clone a new message object, which we can edit in-place
+    io.airbyte.protocol.models.v0.AirbyteMessage newMessage = Jsons.object(
+        Jsons.jsonNode(oldMessage),
+        io.airbyte.protocol.models.v0.AirbyteMessage.class);
+    if (oldMessage.getType() == Type.CATALOG) {
+      for (AirbyteStream stream : newMessage.getCatalog().getStreams()) {
+        JsonNode schema = stream.getJsonSchema();
+        mutateSchemas(
+            this::isPrimitiveTypeDeclaration,
+            this::upgradeTypeDeclaration,
+            schema);
+      }
+    } else if (oldMessage.getType() == Type.RECORD) {
+      // TODO upgrade record
+      JsonNode data = newMessage.getRecord().getData();
+    }
+    return newMessage;
+  }
+
+  /**
+   * Detects any schema that looks like a primitive type declaration, e.g.:
+   * { "type": "string" }
+   * or
+   * { "type": ["string", "object"] }
+   */
+  private boolean isPrimitiveTypeDeclaration(JsonNode schema) {
+    if (!schema.isObject() || !schema.hasNonNull("type")) {
+      return false;
+    }
+    JsonNode typeNode = schema.get("type");
+    if (typeNode.isArray()) {
+      return StreamSupport.stream(typeNode.spliterator(), false)
+          .anyMatch(n -> PRIMITIVE_TYPES.contains(n.asText()));
+    } else {
+      return PRIMITIVE_TYPES.contains(typeNode.asText());
+    }
+  }
+
+  /**
+   * Modifies the schema in-place to upgrade from the old-style type declaration to the new-style $ref declaration.
+   * @param schema An ObjectNode representing a primitive type declaration
+   */
+  private void upgradeTypeDeclaration(JsonNode schema) {
+    ObjectNode schemaNode = (ObjectNode)schema;
+
+    if (schemaNode.hasNonNull("airbyte_type")) {
+      // If airbyte_type is defined, always respect it
+      String referenceType = AIRBYTE_TYPE_TO_REFERENCE_TYPE.get(schemaNode.get("airbyte_type").asText());
+      schemaNode.removeAll();
+      schemaNode.put("$ref", referenceType);
+    } else {
+      // Otherwise, fall back to type/format
+      // TODO handle when schema["type"] is a list
+      String type = schemaNode.get("type").asText();
+      String referenceType = getReferenceType(type, schemaNode);
+      schemaNode.removeAll();
+      schemaNode.put("$ref", referenceType);
+    }
+  }
+
+  /**
+   * Given a primitive (string/int/num/bool) type declaration _without_ an airbyte_type,
+   * get the appropriate $ref type.
+   */
+  private String getReferenceType(String type, ObjectNode schemaNode) {
+    return switch (type) {
+      case "string" -> {
+        if (schemaNode.hasNonNull("format")) {
+          yield switch (schemaNode.get("format").asText()) {
+            case "date" -> "WellKnownTypes.json#definitions/Date";
+            // In these two cases, we default to the "with timezone" type, rather than "without timezone".
+            // This matches existing behavior in normalization.
+            case "date-time" -> "WellKnownTypes.json#definitions/TimestampWithTimezone";
+            case "time" -> "WellKnownTypes.json#definitions/TimeWithTimezone";
+            // If we don't recognize the format, just use a plain string
+            default -> "WellKnownTypes.json#definitions/String";
+          };
+        } else {
+          yield "WellKnownTypes.json#definitions/String";
+        }
+      }
+      case "integer" -> "WellKnownTypes.json#definitions/Integer";
+      case "number" -> "WellKnownTypes.json#definitions/Number";
+      case "boolean" -> "WellKnownTypes.json#definitions/Boolean";
+      // This is impossible, because we'll only call this method on string/integer/number/boolean
+      default -> "WellKnownTypes.json#definitions/String";
+    };
+  }
+
+  /**
+   * Recurses through all type declarations in the schema. For each type declaration that are accepted by matcher,
+   * mutate them using transformer. For all other type declarations, recurse into them.
+   *
+   * @param schema The JsonSchema node to walk down
+   * @param matcher A function which returns true on any schema node that needs to be transformed
+   * @param transformer A function which mutates a schema node
+   */
+  private static void mutateSchemas(Function<JsonNode, Boolean> matcher, Consumer<JsonNode> transformer, JsonNode schema) {
+    if (schema.isBoolean()) {
+      // We never want to modify a schema of `true` or `false` (e.g. additionalProperties: true)
+      // so just return immediately
+      return;
+    }
+    if (matcher.apply(schema)) {
+      // Base case: If this schema has a primitive type, then we need to mutate it
+      transformer.accept(schema);
+    } else {
+      // Otherwise, we need to find all the subschemas and mutate them.
+      // technically, it might be more correct to do something like:
+      //   if schema["type"] == "array": find subschemas for items, additionalItems, contains
+      //   else if schema["type"] == "object": find subschemas for properties, patternProperties, additionalProperties
+      //   else if oneof, allof, etc
+      // but that sounds really verbose for no real benefit
+      List<JsonNode> subschemas = new ArrayList<>();
+
+      // array schemas
+      findSubschemas(subschemas, schema, "items");
+      findSubschemas(subschemas, schema, "additionalItems");
+      findSubschemas(subschemas, schema, "contains");
+
+      // object schemas
+      if (schema.hasNonNull("properties")) {
+        ObjectNode propertiesNode = (ObjectNode)schema.get("properties");
+        Iterator<Entry<String, JsonNode>> propertiesIterator = propertiesNode.fields();
+        while (propertiesIterator.hasNext()) {
+          Entry<String, JsonNode> property = propertiesIterator.next();
+          subschemas.add(property.getValue());
+        }
+      }
+      if (schema.hasNonNull("patternProperties")) {
+        ObjectNode propertiesNode = (ObjectNode)schema.get("patternProperties");
+        Iterator<Entry<String, JsonNode>> propertiesIterator = propertiesNode.fields();
+        while (propertiesIterator.hasNext()) {
+          Entry<String, JsonNode> property = propertiesIterator.next();
+          subschemas.add(property.getValue());
+        }
+      }
+      findSubschemas(subschemas, schema, "additionalProperties");
+
+      // combining restrictions - destinations have limited support for these, but we should handle the schemas correctly anyway
+      findSubschemas(subschemas, schema, "allOf");
+      findSubschemas(subschemas, schema, "oneOf");
+      findSubschemas(subschemas, schema, "anyOf");
+      findSubschemas(subschemas, schema, "not");
+
+      // recurse into each subschema
+      for (JsonNode subschema : subschemas) {
+        mutateSchemas(matcher, transformer, subschema);
+      }
+    }
+  }
+
+  /**
+   * If schema contains key, then grab the subschema(s) at schema[key] and add them to the subschemas list.
+   *
+   * For example:
+   * schema = {"items": [{"type": "string}]}
+   * key = "items"
+   * -> add {"type": "string"} to subschemas
+   *
+   * schema = {"items": {"type": "string"}}
+   * key = "items"
+   * -> add {"type": "string"} to subschemas
+   *
+   * schema = {"additionalProperties": true}
+   * key = "additionalProperties"
+   * -> add nothing to subschemas (technically `true` is a valid JsonSchema, but we don't want to modify it)
+   */
+  private static void findSubschemas(List<JsonNode> subschemas, JsonNode schema, String key) {
+    if (schema.hasNonNull(key)) {
+      JsonNode subschemaNode = schema.get(key);
+      if (subschemaNode.isArray()) {
+        for (JsonNode subschema : subschemaNode) {
+          subschemas.add(subschema);
+        }
+      } else if (subschemaNode.isObject()) {
+        subschemas.add(subschemaNode);
+      }
+    }
   }
 
   @Override
