7c115be1c3ab0e7b50ad72d13afa665c774734ad
Satish Ch <satish.chinthanippu@teradata.com>
2022-11-10 12:40:56 +0530

    Removed unncessary comments

--- airbyte-integrations/connectors/destination-teradata/src/test-integration/java/io/airbyte/integrations/destination/teradata/TeradataDestinationAcceptanceTest.java
+++ airbyte-integrations/connectors/destination-teradata/src/test-integration/java/io/airbyte/integrations/destination/teradata/TeradataDestinationAcceptanceTest.java
@@ -30,116 +30,110 @@ import org.slf4j.LoggerFactory;
 
 public class TeradataDestinationAcceptanceTest extends DestinationAcceptanceTest {
 
-  private static final Logger LOGGER = LoggerFactory.getLogger(TeradataDestinationAcceptanceTest.class);
+	private static final Logger LOGGER = LoggerFactory.getLogger(TeradataDestinationAcceptanceTest.class);
 
-  private JsonNode configJson;
-  private JdbcDatabase database;
-  private DataSource dataSource;
-  private TeradataDestination destination = new TeradataDestination();
+	private JsonNode configJson;
+	private JdbcDatabase database;
+	private DataSource dataSource;
+	private TeradataDestination destination = new TeradataDestination();
 
-  @Override
-  protected String getImageName() {
-    return "airbyte/destination-teradata:dev";
-  }
+	@Override
+	protected String getImageName() {
+		return "airbyte/destination-teradata:dev";
+	}
 
-  @Override
-  protected JsonNode getConfig() {
-    return configJson;
-  }
+	@Override
+	protected JsonNode getConfig() {
+		return configJson;
+	}
 
-  public JsonNode getStaticConfig() throws Exception {
-    final JsonNode config = Jsons.deserialize(Files.readString(Paths.get("secrets/config.json")));
-    return config;
-  }
+	public JsonNode getStaticConfig() throws Exception {
+		final JsonNode config = Jsons.deserialize(Files.readString(Paths.get("secrets/config.json")));
+		return config;
+	}
 
-  @Override
-  protected JsonNode getFailCheckConfig() throws Exception {
-    final JsonNode credentialsJsonString = Jsons
-        .deserialize(Files.readString(Paths.get("secrets/failureconfig.json")));
-    final AirbyteConnectionStatus check = new TeradataDestination().check(credentialsJsonString);
-    assertEquals(AirbyteConnectionStatus.Status.FAILED, check.getStatus());
-    return credentialsJsonString;
-  }
+	@Override
+	protected JsonNode getFailCheckConfig() throws Exception {
+		final JsonNode credentialsJsonString = Jsons
+				.deserialize(Files.readString(Paths.get("secrets/failureconfig.json")));
+		final AirbyteConnectionStatus check = new TeradataDestination().check(credentialsJsonString);
+		assertEquals(AirbyteConnectionStatus.Status.FAILED, check.getStatus());
+		return credentialsJsonString;
+	}
 
-  @Override
-  protected List<JsonNode> retrieveRecords(final TestDestinationEnv testEnv,
-                                           final String streamName,
-                                           final String namespace,
-                                           final JsonNode streamSchema)
-      throws Exception {
-    /*
-     * return retrieveRecordsFromTable(namingResolver.getRawTableName(streamName), namespace).stream()
-     * .map(r -> r.get(JavaBaseConstants.COLUMN_NAME_DATA)).collect(Collectors.toList());
-     */
-    return null;
-  }
+	@Override
+	protected List<JsonNode> retrieveRecords(final TestDestinationEnv testEnv, final String streamName,
+			final String namespace, final JsonNode streamSchema) throws Exception {
 
-  /*
-   * private List<JsonNode> retrieveRecordsFromTable(final String tableName, final String schemaName)
-   * throws SQLException {
-   *
-   * final List<JsonNode> actual = database.bufferedResultSetQuery( connection ->
-   * connection.createStatement().executeQuery("SELECT * FROM id_and_name;"),
-   * sourceOperations::rowToJson); return actual; }
-   */
+		return retrieveRecordsFromTable(namingResolver.getRawTableName(streamName), namespace).stream()
+				.map(r -> r.get(JavaBaseConstants.COLUMN_NAME_DATA)).collect(Collectors.toList());
 
-  @Override
-  protected void setup(TestDestinationEnv testEnv) {
-    final String schemaName = Strings.addRandomSuffix("integration_test", "_", 5);
-    LOGGER.info("Satish TeradataDestinationAcceptanceTest setup : schemaName - " + schemaName);
-    final String createSchemaQuery = String
-        .format(String.format("CREATE DATABASE %s AS PERM = 1e9 SKEW = 10 PERCENT;", schemaName));
-    LOGGER.info("Satish TeradataDestinationAcceptanceTest setup : createSchemaQuery - " + createSchemaQuery);
+		return null;
+	}
 
-    try {
-      this.configJson = Jsons.clone(getStaticConfig());
-      LOGGER.info("Satish TeradataDestinationAcceptanceTest setup : configJson - " + configJson);
-      ((ObjectNode) configJson).put("schema", schemaName);
+	private List<JsonNode> retrieveRecordsFromTable(final String tableName, final String schemaName)
+			throws SQLException {
 
-      dataSource = getDataSource(configJson);
-      LOGGER.info("Satish TeradataDestinationAcceptanceTest : setup dataSource - " + dataSource);
-      database = getDatabase(dataSource);
-      LOGGER.info("Satish TeradataDestinationAcceptanceTest : setup database - " + database);
-      database.execute(createSchemaQuery);
-      LOGGER.info("Satish TeradataDestinationAcceptanceTest setup : execute success ");
-    } catch (Exception e) {
-      LOGGER.info("Satish TeradataDestinationAcceptanceTest setup : failed - " + e.getMessage());
-      AirbyteTraceMessageUtility.emitSystemErrorTrace(e, "setup failed");
-    }
-  }
+		final List<JsonNode> actual = database.bufferedResultSetQuery(
+				connection -> connection.createStatement().executeQuery("SELECT * FROM id_and_name;"),
+				sourceOperations::rowToJson);
+		return actual;
+	}
 
-  @Override
-  protected void tearDown(TestDestinationEnv testEnv) {}
+	@Override
+	protected void setup(TestDestinationEnv testEnv) {
+		final String schemaName = Strings.addRandomSuffix("integration_test", "_", 5);
+		final String createSchemaQuery = String
+				.format(String.format("CREATE DATABASE %s AS PERM = 1e9 SKEW = 10 PERCENT;", schemaName));
 
-  protected DataSource getDataSource(final JsonNode config) {
-    final JsonNode jdbcConfig = destination.toJdbcConfig(config);
-    return DataSourceFactory.create(jdbcConfig.get(JdbcUtils.USERNAME_KEY).asText(),
-        jdbcConfig.has(JdbcUtils.PASSWORD_KEY) ? jdbcConfig.get(JdbcUtils.PASSWORD_KEY).asText() : null,
-        TeradataDestination.DRIVER_CLASS, jdbcConfig.get(JdbcUtils.JDBC_URL_KEY).asText(), getConnectionProperties(config));
-  }
+		try {
+			this.configJson = Jsons.clone(getStaticConfig());
+			((ObjectNode) configJson).put("schema", schemaName);
 
-  protected JdbcDatabase getDatabase(final DataSource dataSource) {
-    return new DefaultJdbcDatabase(dataSource);
-  }
+			dataSource = getDataSource(configJson);
+			database = getDatabase(dataSource);
+			database.execute(createSchemaQuery);
+		} catch (Exception e) {
+			AirbyteTraceMessageUtility.emitSystemErrorTrace(e, "setup failed");
+		}
+	}
 
-  protected Map<String, String> getConnectionProperties(final JsonNode config) {
-    final Map<String, String> customProperties = JdbcUtils.parseJdbcParameters(config, JdbcUtils.JDBC_URL_PARAMS_KEY);
-    final Map<String, String> defaultProperties = getDefaultConnectionProperties(config);
-    assertCustomParametersDontOverwriteDefaultParameters(customProperties, defaultProperties);
-    return MoreMaps.merge(customProperties, defaultProperties);
-  }
+	@Override
+	protected void tearDown(TestDestinationEnv testEnv) {
+	}
 
-  protected Map<String, String> getDefaultConnectionProperties(final JsonNode config) {
-    return Collections.emptyMap();
-  }
+	protected DataSource getDataSource(final JsonNode config) {
+		final JsonNode jdbcConfig = destination.toJdbcConfig(config);
+		return DataSourceFactory.create(jdbcConfig.get(JdbcUtils.USERNAME_KEY).asText(),
+				jdbcConfig.has(JdbcUtils.PASSWORD_KEY) ? jdbcConfig.get(JdbcUtils.PASSWORD_KEY).asText() : null,
+				TeradataDestination.DRIVER_CLASS, jdbcConfig.get(JdbcUtils.JDBC_URL_KEY).asText(),
+				getConnectionProperties(config));
+	}
 
-  private void assertCustomParametersDontOverwriteDefaultParameters(final Map<String, String> customParameters,
-                                                                    final Map<String, String> defaultParameters) {
-    for (final String key : defaultParameters.keySet()) {
-      if (customParameters.containsKey(key) && !Objects.equals(customParameters.get(key), defaultParameters.get(key))) {
-        throw new IllegalArgumentException("Cannot overwrite default JDBC parameter " + key);
-      }
-    }
-  }
+	protected JdbcDatabase getDatabase(final DataSource dataSource) {
+		return new DefaultJdbcDatabase(dataSource);
+	}
+
+	protected Map<String, String> getConnectionProperties(final JsonNode config) {
+		final Map<String, String> customProperties = JdbcUtils.parseJdbcParameters(config,
+				JdbcUtils.JDBC_URL_PARAMS_KEY);
+		final Map<String, String> defaultProperties = getDefaultConnectionProperties(config);
+		assertCustomParametersDontOverwriteDefaultParameters(customProperties, defaultProperties);
+		return MoreMaps.merge(customProperties, defaultProperties);
+	}
+
+	protected Map<String, String> getDefaultConnectionProperties(final JsonNode config) {
+		return Collections.emptyMap();
+	}
+
+	private void assertCustomParametersDontOverwriteDefaultParameters(final Map<String, String> customParameters,
+			final Map<String, String> defaultParameters) {
+		for (final String key : defaultParameters.keySet()) {
+			if (customParameters.containsKey(key)
+					&& !Objects.equals(customParameters.get(key), defaultParameters.get(key))) {
+				throw new IllegalArgumentException("Cannot overwrite default JDBC parameter " + key);
+			}
+		}
+	}
 
 }
