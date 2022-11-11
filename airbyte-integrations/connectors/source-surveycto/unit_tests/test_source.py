6d566a9cc64f6d347c2872395b75c807d06bcc01
siddhant <ssiddhant3030@gmail.com>
2022-11-11 14:09:28 +0530

    unit test added

--- airbyte-integrations/connectors/source-surveycto/unit_tests/test_source.py
+++ airbyte-integrations/connectors/source-surveycto/unit_tests/test_source.py
@@ -1,22 +1,30 @@
 # #
 # # Copyright (c) 2022 Airbyte, Inc., all rights reserved.
 # #
+import pytest
+from unittest.mock import Mock, MagicMock
 
-# from unittest.mock import MagicMock
+from source_surveycto.source import SourceSurveycto
+from source_surveycto.helpers import Helpers
 
-# from source_surveycto.source import SourceSurveycto
+@pytest.fixture(name='config')
+def config_fixture():
+    return {'server_name': 'server_name', 'form_id': 'form_id', 'start_date': 'Jan 09, 2022 00:00:00 AM', 'password': 'password', 'username': 'username'}
 
+def test_check_connection(config):
+    source = SourceSurveycto()
+    logger_mock=Mock()
+    assert source.check_connection(logger_mock, config=config) == (True, None)
 
-# def test_check_connection(mocker):
-#     source = SourceSurveycto()
-#     logger_mock, config_mock = MagicMock(), MagicMock()
-#     assert source.check_connection(logger_mock, config_mock) == (True, None)
+def test_check_connection_fail(config):
+    source = SourceSurveycto()
+    logger_mock =MagicMock()
+    assert source.check_connection(logger_mock, config={}) == (False, None)
 
+def test_streams(config):
+    source = SourceSurveycto()
+    Helpers.call_survey_cto = MagicMock()
+    streams = source.streams(config)
 
-# def test_streams(mocker):
-#     source = SourceSurveycto()
-#     config_mock = MagicMock()
-#     streams = source.streams(config_mock)
-#     # TODO: replace this with your streams number
-#     expected_streams_number = 2
-#     assert len(streams) == expected_streams_number
+    expected_streams_number = 7
+    assert len(streams) == expected_streams_number
