f9926736819d98dbaa78d15fe6fb514cdf9e941e
siddhant <ssiddhant3030@gmail.com>
2022-11-12 00:34:26 +0530

    survey cto connector

---
+++ airbyte-integrations/connectors/source-surveycto/source_surveycto/helpers.py
@@ -0,0 +1,93 @@
+
+from typing import Any, Dict
+
+import requests
+import base64
+from airbyte_cdk.models import AirbyteStream
+from airbyte_cdk.models.airbyte_protocol import DestinationSyncMode, SyncMode
+from airbyte_cdk.sources.streams.http.requests_native_auth import TokenAuthenticator
+from bigquery_schema_generator.generate_schema import SchemaGenerator
+from gbqschema_converter.gbqschema_to_jsonschema import json_representation as converter
+from requests.adapters import HTTPAdapter
+from requests.packages.urllib3.util.retry import Retry
+from datetime import datetime
+
+
+class Helpers(object):
+
+    @staticmethod
+    def _base64_encode(string:str) -> str:
+        return base64.b64encode(string.encode("ascii")).decode("ascii")
+
+    @staticmethod
+    def call_survey_cto(config, form_id):
+        server_name = config['server_name']
+        start_date = config['start_date']
+        user_name_password = f"{config['username']}:{config['password']}"
+        auth_token = Helpers._base64_encode(user_name_password)
+        
+        url = f"https://{server_name}.surveycto.com/api/v2/forms/data/wide/json/{form_id}?date={start_date}"
+
+        retry_strategy = Retry(
+            total=3,
+            status_forcelist=[429, 409],
+            method_whitelist=["HEAD", "GET", "OPTIONS"]
+        )
+        adapter = HTTPAdapter(max_retries=retry_strategy)
+        http = requests.Session()
+        http.mount("https://", adapter)
+        http.mount("http://", adapter)
+
+        response = http.get(url, headers = {'Authorization': 'Basic ' + auth_token })
+        response_json = response.json()
+
+        for data in response_json:
+            try:
+                key = data["KEY"]
+                o = key.replace('uuid:', '')
+                data["KEY"] = o
+
+                starttime = data["starttime"]
+                a = datetime.strptime(starttime,'%b %d, %Y %I:%M:%S %p').strftime('%Y-%m-%dT%H:%M:%S+00:00')
+                data["starttime"] = a
+
+                endtime = data["endtime"]
+                a = datetime.strptime(endtime,'%b %d, %Y %I:%M:%S %p').strftime('%Y-%m-%dT%H:%M:%S+00:00')
+                data["endtime"] = a
+
+                completiondate = data["CompletionDate"]
+                a = datetime.strptime(completiondate,'%b %d, %Y %I:%M:%S %p').strftime('%Y-%m-%dT%H:%M:%S+00:00')
+                data["CompletionDate"] = a
+
+                submissiondate = data["SubmissionDate"]
+                a = datetime.strptime(submissiondate,'%b %d, %Y %I:%M:%S %p').strftime('%Y-%m-%dT%H:%M:%S+00:00')
+                data["SubmissionDate"] = a
+
+                
+                yield data
+            except Exception as e:
+                raise e
+
+        return data
+
+
+    
+    @staticmethod
+    def get_filter_data(data):
+        generator = SchemaGenerator(input_format='dict', infer_mode='NULLABLE',preserve_input_sort_order='true')
+
+        schema_map, error_logs = generator.deduce_schema(input_data=data)
+        schema = generator.flatten_schema(schema_map)
+        schema_json = converter(schema)
+        schema=schema_json['definitions']['element']['properties']   
+        return schema
+
+    @staticmethod
+    def get_json_schema(schema):
+
+        json_schema = {
+            "$schema": "http://json-schema.org/draft-07/schema#",
+            "type": "object",
+            "properties": schema,
+        }
+        return json_schema
---
+++ airbyte-integrations/connectors/source-surveycto/source_surveycto/source.py
@@ -0,0 +1,179 @@
+#
+# Copyright (c) 2022 Airbyte, Inc., all rights reserved.
+#
+from bigquery_schema_generator.generate_schema import SchemaGenerator
+from gbqschema_converter.gbqschema_to_jsonschema import json_representation as converter
+import json
+import sys
+
+
+from abc import ABC
+from imaplib import _Authenticator
+from pydoc import doc
+from typing import Any, Iterable, List, Mapping, MutableMapping, Optional, Tuple
+
+from airbyte_cdk.models import SyncMode
+from datetime import datetime, timedelta 
+from  dateutil.parser import parse
+
+import requests
+import asyncio
+import base64
+import time
+from airbyte_cdk.sources import AbstractSource
+from airbyte_cdk.logger import AirbyteLogger
+from airbyte_cdk.models import AirbyteCatalog
+from airbyte_cdk.sources.streams import Stream, IncrementalMixin
+from airbyte_cdk.sources.streams.http import HttpStream
+from airbyte_cdk.sources.streams.http.auth import TokenAuthenticator, NoAuth
+from airbyte_cdk.sources.utils.transform import TransformConfig, TypeTransformer
+from airbyte_cdk.sources.streams.core import Stream
+
+from .helpers import Helpers
+
+
+
+class SurveyStream(HttpStream, ABC):
+    transformer: TypeTransformer = TypeTransformer(TransformConfig.DefaultSchemaNormalization)
+
+    def __init__(self, config: Mapping[str, Any], form_id, schema, **kwargs):
+        super().__init__()
+
+        self.config = config
+        self.schema = schema
+        self.server_name = config['server_name']
+        self.form_id = form_id
+        self.start_date = config['start_date']
+        #base64 encode username and password as auth token
+        user_name_password = f"{config['username']}:{config['password']}"
+        self.auth_token = self._base64_encode(user_name_password)
+
+    @property
+    def url_base(self) -> str:
+         return f"https://{self.server_name}.surveycto.com/api/v2/forms/data/wide/json/"
+
+    def _base64_encode(self,string:str) -> str:
+        return base64.b64encode(string.encode("ascii")).decode("ascii")
+
+    def request_params(
+        self, stream_state: Mapping[str, Any], stream_slice: Mapping[str, any] = None, next_page_token: Mapping[str, Any] = None
+    ) -> MutableMapping[str, Any]:
+        
+        return {}
+
+
+class SurveyctoStream(SurveyStream, IncrementalMixin):
+    primary_key = 'KEY'
+    date_format_scto = '%b %d, %Y %H:%M:%S %p'
+    dateformat =  '%Y-%m-%dT%H:%M:%S+00:00'
+    cursor_field = 'CompletionDate'
+    _cursor_value = None
+
+
+    @property
+    def state(self) -> Mapping[str, Any]:
+        initial_date = datetime.strptime(self.start_date, self.date_format_scto)
+        if self._cursor_value:
+            return {self.cursor_field: self._cursor_value}
+        else:
+            return {self.cursor_field: initial_date}
+
+    @state.setter
+    def state(self, value: Mapping[str, Any]):
+        self._cursor_value = datetime.strptime(value[self.cursor_field], self.dateformat)
+        
+    @property
+    def name(self) -> str:
+        return self.form_id
+
+    def next_page_token(self, response: requests.Response) -> Optional[Mapping[str, Any]]:
+        return None
+
+    def get_json_schema(self):  
+        return self.schema
+
+    def request_params(
+        self, stream_state: Mapping[str, Any], stream_slice: Mapping[str, any] = None, next_page_token: Mapping[str, Any] = None
+    ) -> MutableMapping[str, Any]:
+        ix = self.state[self.cursor_field] 
+        
+        return {'date': ix.strftime(self.date_format_scto)}
+
+    def request_headers(
+        self, stream_state: Mapping[str, Any], stream_slice: Mapping[str, Any] = None, next_page_token: Mapping[str, Any] = None
+    ) -> Mapping[str, Any]:
+        return {'Authorization': 'Basic ' + self.auth_token }
+
+    def path(self, stream_slice: Mapping[str, Any] = None, **kwargs) -> str:
+         return self.form_id
+
+    def parse_response(
+        self,
+        response: requests.Response,
+        stream_state: Mapping[str, Any],
+        stream_slice: Mapping[str, Any] = None,
+        next_page_token: Mapping[str, Any] = None,
+    ) -> Iterable[Mapping]:
+        self.response_json = response.json()
+    
+        for data in self.response_json:
+            try:
+
+                key = data["KEY"]
+                o = key.replace('uuid:', '')
+                data["KEY"] = o
+ 
+                starttime = data["starttime"]
+                a = datetime.strptime(starttime,'%b %d, %Y %I:%M:%S %p').strftime('%Y-%m-%dT%H:%M:%S+00:00')
+                data["starttime"] = a
+
+                endtime = data["endtime"]
+                a = datetime.strptime(endtime,'%b %d, %Y %I:%M:%S %p').strftime('%Y-%m-%dT%H:%M:%S+00:00')
+                data["endtime"] = a
+
+                completiondate = data["CompletionDate"]
+                a = datetime.strptime(completiondate,'%b %d, %Y %I:%M:%S %p').strftime('%Y-%m-%dT%H:%M:%S+00:00')
+                data["CompletionDate"] = a
+
+                submissiondate = data["SubmissionDate"]
+                a = datetime.strptime(submissiondate,'%b %d, %Y %I:%M:%S %p').strftime('%Y-%m-%dT%H:%M:%S+00:00')
+                data["SubmissionDate"] = a
+
+                
+                yield data
+            except Exception as e:
+                msg = f"""Encountered an exception parsing schema"""
+                self.logger.exception(msg)
+                raise e
+                
+    def read_records(self, *args, **kwargs) -> Iterable[Mapping[str, Any]]:
+        for record in super().read_records(*args, **kwargs):
+            self._cursor_value = record[self.cursor_field]
+            yield record
+
+
+# Source
+class SourceSurveycto(AbstractSource):
+    def check_connection(self, logger, config) -> Tuple[bool, any]:
+        return True, None
+
+    def no_auth(self):
+        return NoAuth()  
+
+    def generate_streams(self, config: str) -> List[Stream]:
+        forms = config.get("form_id", [])
+        streams = []
+      
+        for form_id in forms:
+            schema = Helpers.call_survey_cto(config, form_id)
+            filter_data = Helpers.get_filter_data(schema)
+            schema_res = Helpers.get_json_schema(filter_data)
+            stream = SurveyctoStream(config=config,form_id=form_id,schema=schema_res)
+            streams.append(stream)
+        return streams
+
+    def streams(self, config: Mapping[str, Any]) -> List[Stream]:    
+        streams = self.generate_streams(config=config)
+        return streams
+
+
