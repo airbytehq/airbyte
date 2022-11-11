4ecc7e2cebe018a9771d73a9975471e05d2355d3
Satish Ch <satish.chinthanippu@teradata.com>
2022-11-11 19:20:15 +0530

    more info logs added

--- airbyte-integrations/connectors/destination-teradata/src/test/java/io/airbyte/integrations/destination/teradata/TeradataDestinationTest.java
+++ airbyte-integrations/connectors/destination-teradata/src/test/java/io/airbyte/integrations/destination/teradata/TeradataDestinationTest.java
@@ -19,109 +19,139 @@ import org.slf4j.LoggerFactory;
 
 public class TeradataDestinationTest {
 
-  private static final Logger LOGGER = LoggerFactory.getLogger(TeradataDestinationTest.class);
+	private static final Logger LOGGER = LoggerFactory.getLogger(TeradataDestinationTest.class);
 
-  private JsonNode config;
-  final TeradataDestination destination = new TeradataDestination();
+	private JsonNode config;
+	final TeradataDestination destination = new TeradataDestination();
 
-  private static final String EXPECTED_JDBC_URL = "jdbc:teradata://localhost/";
+	private static String EXPECTED_JDBC_URL = "jdbc:teradata://localhost/";
+	private static String EXPECTED_JDBC_ESCAPED_URL = "jdbc:teradata://localhost/";
 
-  private JsonNode buildConfigNoJdbcParameters() {
-    return Jsons.jsonNode(ImmutableMap.of(JdbcUtils.HOST_KEY, "localhost", JdbcUtils.USERNAME_KEY, "user",
-        JdbcUtils.DATABASE_KEY, "db"));
-  }
+	private String getUserName() {
+		return config.get(JdbcUtils.USERNAME_KEY).asText();
+	}
 
-  private static final String EXPECTED_JDBC_ESCAPED_URL = "jdbc:teradata://localhost/";
+	private String getPassword() {
+		return config.get(JdbcUtils.PASSWORD_KEY).asText();
+	}
 
-  private JsonNode buildConfigEscapingNeeded() {
-    return Jsons.jsonNode(ImmutableMap.of(JdbcUtils.HOST_KEY, "localhost", JdbcUtils.USERNAME_KEY, "user",
-        JdbcUtils.DATABASE_KEY, "db/foo"));
-  }
+	private String getHostName() {
+		return config.get(JdbcUtils.HOST_KEY).asText();
+	}
 
-  private JsonNode buildConfigWithExtraJdbcParameters(final String extraParam) {
-    return Jsons.jsonNode(ImmutableMap.of(JdbcUtils.HOST_KEY, "localhost", JdbcUtils.USERNAME_KEY, "user",
-        JdbcUtils.DATABASE_KEY, "db", JdbcUtils.JDBC_URL_PARAMS_KEY, extraParam));
-  }
+	private JsonNode buildConfigNoJdbcParameters() {
+		return Jsons.jsonNode(ImmutableMap.of(JdbcUtils.HOST_KEY, config.get(JdbcUtils.HOST_KEY).asText(),
+				JdbcUtils.USERNAME_KEY, config.get(JdbcUtils.USERNAME_KEY).asText(), JdbcUtils.DATABASE_KEY,
+				config.get(JdbcUtils.PASSWORD_KEY).asText()));
+	}
 
-  @Test
-  void testJdbcUrlAndConfigNoExtraParams() {
-    final JsonNode jdbcConfig = new TeradataDestination().toJdbcConfig(buildConfigNoJdbcParameters());
-    LOGGER.info(" JDBC URL of JdbcUrl With No Extra Params --  " + jdbcConfig.get(JdbcUtils.JDBC_URL_KEY).asText());
-    assertEquals(EXPECTED_JDBC_URL, jdbcConfig.get(JdbcUtils.JDBC_URL_KEY).asText());
-  }
+	@Override
+	protected void setup(TestDestinationEnv testEnv) {
 
-  @Test
-  void testJdbcUrlWithEscapedDatabaseName() {
-    final JsonNode jdbcConfig = new TeradataDestination().toJdbcConfig(buildConfigEscapingNeeded());
-    LOGGER.info(" JDBC URL of JdbcUrl With Escaped DatabaseName test --  " + jdbcConfig.get(JdbcUtils.JDBC_URL_KEY).asText());
-    assertEquals(EXPECTED_JDBC_ESCAPED_URL, jdbcConfig.get(JdbcUtils.JDBC_URL_KEY).asText());
-  }
+		try {
+			this.config = Jsons.clone(Jsons.deserialize(Files.readString(Paths.get("secrets/config.json"))));
+			LOGGER.info("TeradataDestinationTest - this.config : " + this.config);
+			System.out.println("TeradataDestinationTest - this.config : " + this.config);
+			this.EXPECTED_JDBC_URL = String.format("jdbc:teradata://%s/", config.get(JdbcUtils.HOST_KEY).asText());
+			this.EXPECTED_JDBC_ESCAPED_URL = String.format("jdbc:teradata://%s/",
+					config.get(JdbcUtils.HOST_KEY).asText());
+			LOGGER.info("TeradataDestinationTest - EXPECTED_JDBC_URL : " + this.EXPECTED_JDBC_URL);
+			System.out.println("TeradataDestinationTest - this.EXPECTED_JDBC_ESCAPED_URL : " + this.EXPECTED_JDBC_ESCAPED_URL);
+		} catch (Exception e) {
+			AirbyteTraceMessageUtility.emitSystemErrorTrace(e, "setup failed");
+		}
+	}
 
-  @Test
-  void testJdbcUrlEmptyExtraParams() {
-    final JsonNode jdbcConfig = new TeradataDestination().toJdbcConfig(buildConfigWithExtraJdbcParameters(""));
-    LOGGER.info(" JDBC URL of JdbcUrl With Empty params --  " + jdbcConfig.get(JdbcUtils.JDBC_URL_KEY).asText());
-    assertEquals(EXPECTED_JDBC_URL, jdbcConfig.get(JdbcUtils.JDBC_URL_KEY).asText());
-  }
+	private JsonNode buildConfigEscapingNeeded() {
+		return Jsons.jsonNode(ImmutableMap.of(JdbcUtils.HOST_KEY, getHostName(), JdbcUtils.USERNAME_KEY, getUserName(),
+				JdbcUtils.DATABASE_KEY, "db/foo"));
+	}
 
-  @Test
-  void testJdbcUrlExtraParams() {
-    final String extraParam = "key1=value1&key2=value2&key3=value3";
-    final JsonNode jdbcConfig = new TeradataDestination()
-        .toJdbcConfig(buildConfigWithExtraJdbcParameters(extraParam));
-    LOGGER.info(" jdbcConfig in testJdbcUrlExtraParams -   " + jdbcConfig);
-    LOGGER
-        .info(" JDBC URL of JdbcUrl With Escaped DatabaseName test in testJdbcUrlExtraParams -   " + jdbcConfig.get(JdbcUtils.JDBC_URL_KEY).asText());
-    assertEquals(EXPECTED_JDBC_URL, jdbcConfig.get(JdbcUtils.JDBC_URL_KEY).asText());
-  }
+	private JsonNode buildConfigWithExtraJdbcParameters(final String extraParam) {
+		return Jsons.jsonNode(ImmutableMap.of(JdbcUtils.HOST_KEY, getHostName(), JdbcUtils.USERNAME_KEY, getUserName(),
+				JdbcUtils.DATABASE_KEY, "db", JdbcUtils.JDBC_URL_PARAMS_KEY, extraParam));
+	}
 
-  @Test
-  void testCheckIncorrectPasswordFailure() {
-    final var config = buildConfigNoJdbcParameters();
-    LOGGER.info(" config in testCheckIncorrectPasswordFailure -   " + config);
-    ((ObjectNode) config).put(JdbcUtils.PASSWORD_KEY, "fake");
-    ((ObjectNode) config).put(JdbcUtils.SCHEMA_KEY, "public");
-    final TeradataDestination destination = new TeradataDestination();
-    final var actual = destination.check(config);
-    LOGGER.info(" Status msg of testCheckIncorrectPasswordFailure -   " + actual.getMessage());
-    assertTrue(actual.getMessage().contains("SQLState 08S01"));
-  }
+	@Test
+	void testJdbcUrlAndConfigNoExtraParams() {
+		final JsonNode jdbcConfig = new TeradataDestination().toJdbcConfig(buildConfigNoJdbcParameters());
+		LOGGER.info(" JDBC URL of JdbcUrl With No Extra Params --  " + jdbcConfig.get(JdbcUtils.JDBC_URL_KEY).asText());
+		assertEquals(EXPECTED_JDBC_URL, jdbcConfig.get(JdbcUtils.JDBC_URL_KEY).asText());
+	}
 
-  @Test
-  public void testCheckIncorrectUsernameFailure() {
-    final var config = buildConfigNoJdbcParameters();
-    LOGGER.info(" config in testCheckIncorrectUsernameFailure -   " + config);
-    ((ObjectNode) config).put(JdbcUtils.USERNAME_KEY, "");
-    ((ObjectNode) config).put(JdbcUtils.SCHEMA_KEY, "public");
-    final TeradataDestination destination = new TeradataDestination();
-    final AirbyteConnectionStatus status = destination.check(config);
-    LOGGER.info(" Status msg of testCheckIncorrectUsernameFailure -   " + status.getMessage());
-    assertTrue(status.getMessage().contains("SQLState 08S01"));
-  }
+	@Test
+	void testJdbcUrlWithEscapedDatabaseName() {
+		final JsonNode jdbcConfig = new TeradataDestination().toJdbcConfig(buildConfigEscapingNeeded());
+		LOGGER.info(" JDBC URL of JdbcUrl With Escaped DatabaseName test --  "
+				+ jdbcConfig.get(JdbcUtils.JDBC_URL_KEY).asText());
+		assertEquals(EXPECTED_JDBC_ESCAPED_URL, jdbcConfig.get(JdbcUtils.JDBC_URL_KEY).asText());
+	}
 
-  @Test
-  public void testCheckIncorrectHostFailure() {
-    final var config = buildConfigNoJdbcParameters();
-    LOGGER.info(" config in testCheckIncorrectHostFailure -   " + config);
-    ((ObjectNode) config).put(JdbcUtils.HOST_KEY, "localhost2");
-    ((ObjectNode) config).put(JdbcUtils.SCHEMA_KEY, "public");
-    final TeradataDestination destination = new TeradataDestination();
-    final AirbyteConnectionStatus status = destination.check(config);
-    LOGGER.info(" Status msg of testCheckIncorrectHostFailure -   " + status.getMessage());
-    assertTrue(status.getMessage().contains("SQLState 08S01"));
-  }
+	@Test
+	void testJdbcUrlEmptyExtraParams() {
+		final JsonNode jdbcConfig = new TeradataDestination().toJdbcConfig(buildConfigWithExtraJdbcParameters(""));
+		LOGGER.info(" JDBC URL of JdbcUrl With Empty params --  " + jdbcConfig.get(JdbcUtils.JDBC_URL_KEY).asText());
+		assertEquals(EXPECTED_JDBC_URL, jdbcConfig.get(JdbcUtils.JDBC_URL_KEY).asText());
+	}
 
-  @Test
-  public void testCheckIncorrectDataBaseFailure() {
-    final var config = buildConfigNoJdbcParameters();
-    LOGGER.info(" config in testCheckIncorrectDataBaseFailure -   " + config);
-    ((ObjectNode) config).put(JdbcUtils.DATABASE_KEY, "wrongdatabase");
-    ((ObjectNode) config).put(JdbcUtils.SCHEMA_KEY, "public");
-    final TeradataDestination destination = new TeradataDestination();
-    final AirbyteConnectionStatus status = destination.check(config);
-    LOGGER.info(" Status msg of testCheckIncorrectDataBaseFailure -   " + status.getMessage());
-    System.out.println(" Status msg of testCheckIncorrectDataBaseFailure -   " + status.getMessage());
-    assertTrue(status.getMessage().contains("SQLState 08S01"));
-  }
+	@Test
+	void testJdbcUrlExtraParams() {
+		final String extraParam = "key1=value1&key2=value2&key3=value3";
+		final JsonNode jdbcConfig = new TeradataDestination()
+				.toJdbcConfig(buildConfigWithExtraJdbcParameters(extraParam));
+		LOGGER.info(" jdbcConfig in testJdbcUrlExtraParams -   " + jdbcConfig);
+		LOGGER.info(" JDBC URL of JdbcUrl With Escaped DatabaseName test in testJdbcUrlExtraParams -   "
+				+ jdbcConfig.get(JdbcUtils.JDBC_URL_KEY).asText());
+		assertEquals(EXPECTED_JDBC_URL, jdbcConfig.get(JdbcUtils.JDBC_URL_KEY).asText());
+	}
+
+	@Test
+	void testCheckIncorrectPasswordFailure() {
+		final var config = buildConfigNoJdbcParameters();
+		LOGGER.info(" config in testCheckIncorrectPasswordFailure -   " + config);
+		((ObjectNode) config).put(JdbcUtils.PASSWORD_KEY, "fake");
+		((ObjectNode) config).put(JdbcUtils.SCHEMA_KEY, "public");
+		final TeradataDestination destination = new TeradataDestination();
+		final var actual = destination.check(config);
+		LOGGER.info(" Status msg of testCheckIncorrectPasswordFailure -   " + actual.getMessage());
+		assertTrue(actual.getMessage().contains("SQLState 08S01"));
+	}
+
+	@Test
+	public void testCheckIncorrectUsernameFailure() {
+		final var config = buildConfigNoJdbcParameters();
+		LOGGER.info(" config in testCheckIncorrectUsernameFailure -   " + config);
+		((ObjectNode) config).put(JdbcUtils.USERNAME_KEY, "");
+		((ObjectNode) config).put(JdbcUtils.SCHEMA_KEY, "public");
+		final TeradataDestination destination = new TeradataDestination();
+		final AirbyteConnectionStatus status = destination.check(config);
+		LOGGER.info(" Status msg of testCheckIncorrectUsernameFailure -   " + status.getMessage());
+		assertTrue(status.getMessage().contains("SQLState 08S01"));
+	}
+
+	@Test
+	public void testCheckIncorrectHostFailure() {
+		final var config = buildConfigNoJdbcParameters();
+		LOGGER.info(" config in testCheckIncorrectHostFailure -   " + config);
+		((ObjectNode) config).put(JdbcUtils.HOST_KEY, "localhost2");
+		((ObjectNode) config).put(JdbcUtils.SCHEMA_KEY, "public");
+		final TeradataDestination destination = new TeradataDestination();
+		final AirbyteConnectionStatus status = destination.check(config);
+		LOGGER.info(" Status msg of testCheckIncorrectHostFailure -   " + status.getMessage());
+		assertTrue(status.getMessage().contains("SQLState 08S01"));
+	}
+
+	@Test
+	public void testCheckIncorrectDataBaseFailure() {
+		final var config = buildConfigNoJdbcParameters();
+		LOGGER.info(" config in testCheckIncorrectDataBaseFailure -   " + config);
+		((ObjectNode) config).put(JdbcUtils.DATABASE_KEY, "wrongdatabase");
+		((ObjectNode) config).put(JdbcUtils.SCHEMA_KEY, "public");
+		final TeradataDestination destination = new TeradataDestination();
+		final AirbyteConnectionStatus status = destination.check(config);
+		LOGGER.info(" Status msg of testCheckIncorrectDataBaseFailure -   " + status.getMessage());
+		System.out.println(" Status msg of testCheckIncorrectDataBaseFailure -   " + status.getMessage());
+		assertTrue(status.getMessage().contains("SQLState 08S01"));
+	}
 
 }
