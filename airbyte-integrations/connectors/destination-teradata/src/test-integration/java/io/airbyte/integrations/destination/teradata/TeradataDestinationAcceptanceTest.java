bdcb0d95b1a395785845a5c20fefba7e08d85888
Satish Ch <satish.chinthanippu@teradata.com>
2022-11-14 11:13:00 +0530

    Acceptance tests added

--- airbyte-integrations/connectors/destination-teradata/src/test-integration/java/io/airbyte/integrations/destination/teradata/TeradataDestinationAcceptanceTest.java
+++ airbyte-integrations/connectors/destination-teradata/src/test-integration/java/io/airbyte/integrations/destination/teradata/TeradataDestinationAcceptanceTest.java
@@ -17,6 +17,7 @@ import io.airbyte.db.jdbc.JdbcDatabase;
 import io.airbyte.db.jdbc.JdbcUtils;
 import io.airbyte.integrations.base.AirbyteTraceMessageUtility;
 import io.airbyte.integrations.standardtest.destination.DestinationAcceptanceTest;
+import io.airbyte.integrations.standardtest.destination.JdbcDestinationAcceptanceTest;
 import io.airbyte.protocol.models.AirbyteConnectionStatus;
 import java.nio.file.Files;
 import java.nio.file.Paths;
