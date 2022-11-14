3c5fdb94724a2d56ead6e7d551e94d885c84ae85
Ganpat Agarwal <gagarwal@artica.com>
2022-11-14 22:33:03 +0530

    add tests for sponsored products report

--- airbyte-integrations/connectors/source-amazon-ads/unit_tests/test_report_streams.py
+++ airbyte-integrations/connectors/source-amazon-ads/unit_tests/test_report_streams.py
@@ -649,3 +649,58 @@ def test_display_report_stream_with_custom_record_types(config_gen, custom_recor
         if record['recordType'] not in custom_record_types:
             if flag_match_error:
                 assert False
+
+
+@responses.activate
+@pytest.mark.parametrize(
+    "custom_record_types, expected_record_types, flag_match_error",
+    [
+        (
+                ["campaigns"],
+                ["campaigns"],
+                True
+        ),
+        (
+                ["asins_keywords"],
+                ["asins"],
+                True
+        ),
+        (
+                ["asins_targets"],
+                ["asins"],
+                True
+        ),
+        (
+                ["campaigns", "adGroups"],
+                ["campaigns", "adGroups"],
+                True
+        ),
+        (
+                [],
+                [],
+                False
+        ),
+        (
+                ["invalid_record_type"],
+                [],
+                True
+        )
+    ]
+)
+def test_products_report_stream_with_custom_record_types(config_gen, custom_record_types, expected_record_types, flag_match_error):
+    setup_responses(
+        init_response_products=REPORT_INIT_RESPONSE,
+        status_response=REPORT_STATUS_RESPONSE,
+        metric_response=METRIC_RESPONSE,
+    )
+
+    profiles = make_profiles(profile_type="vendor")
+
+    stream = SponsoredProductsReportStream(config_gen(report_record_types=custom_record_types), profiles, authenticator=mock.MagicMock())
+    stream_slice = {"profile": profiles[0], "reportDate": "20210725", "retry_count": 3}
+    records = list(stream.read_records(SyncMode.incremental, stream_slice=stream_slice))
+    for record in records:
+        print(record)
+        if record['recordType'] not in expected_record_types:
+            if flag_match_error:
+                assert False
