a2ec790eecfa81756bcc412fe53d3077f61a763f
Edmundo Ruiz Ghanem <edmundo@airbyte.io>
2022-11-14 19:55:19 -0500

    Add padding bottoom to DiffAccordion Update CatalogDiff stories to include different use cases

--- airbyte-webapp/src/views/Connection/CatalogDiffModal/index.stories.tsx
+++ airbyte-webapp/src/views/Connection/CatalogDiffModal/index.stories.tsx
@@ -3,6 +3,7 @@ import { FormattedMessage } from "react-intl";
 
 import { Modal } from "components/ui/Modal";
 
+import { CatalogDiff } from "core/request/AirbyteClient";
 import { ModalServiceProvider } from "hooks/services/Modal";
 
 import { CatalogDiffModal } from "./CatalogDiffModal";
@@ -22,52 +23,112 @@ const Template: ComponentStory<typeof CatalogDiffModal> = (args) => {
   );
 };
 
+const oneStreamAddCatalogDiff: CatalogDiff = {
+  transforms: [
+    {
+      transformType: "add_stream",
+      streamDescriptor: { namespace: "apple", name: "banana" },
+    },
+  ],
+};
+
+const oneFieldAddCatalogDiff: CatalogDiff = {
+  transforms: [
+    {
+      transformType: "update_stream",
+      streamDescriptor: { namespace: "apple", name: "harissa_paste" },
+      updateStream: [{ transformType: "add_field", fieldName: ["users", "phone"], breaking: false }],
+    },
+  ],
+};
+
+const oneFieldUpdateCatalogDiff: CatalogDiff = {
+  transforms: [
+    {
+      transformType: "update_stream",
+      streamDescriptor: { namespace: "apple", name: "harissa_paste" },
+      updateStream: [
+        {
+          transformType: "update_field_schema",
+          breaking: false,
+          fieldName: ["users", "address"],
+          updateFieldSchema: { oldSchema: { type: "number" }, newSchema: { type: "string" } },
+        },
+      ],
+    },
+  ],
+};
+
+const fullCatalogDiff: CatalogDiff = {
+  transforms: [
+    {
+      transformType: "update_stream",
+      streamDescriptor: { namespace: "apple", name: "harissa_paste" },
+      updateStream: [
+        { transformType: "add_field", fieldName: ["users", "phone"], breaking: false },
+        { transformType: "add_field", fieldName: ["users", "email"], breaking: false },
+        { transformType: "remove_field", fieldName: ["users", "lastName"], breaking: false },
+        {
+          transformType: "update_field_schema",
+          breaking: false,
+          fieldName: ["users", "address"],
+          updateFieldSchema: { oldSchema: { type: "number" }, newSchema: { type: "string" } },
+        },
+        {
+          transformType: "update_field_schema",
+          breaking: false,
+          fieldName: ["users", "updated_at"],
+          updateFieldSchema: { oldSchema: { type: "string" }, newSchema: { type: "DateTime" } },
+        },
+      ],
+    },
+    {
+      transformType: "add_stream",
+      streamDescriptor: { namespace: "apple", name: "banana" },
+    },
+    {
+      transformType: "add_stream",
+      streamDescriptor: { namespace: "apple", name: "carrot" },
+    },
+    {
+      transformType: "remove_stream",
+      streamDescriptor: { namespace: "apple", name: "dragonfruit" },
+    },
+    {
+      transformType: "remove_stream",
+      streamDescriptor: { namespace: "apple", name: "eclair" },
+    },
+    {
+      transformType: "remove_stream",
+      streamDescriptor: { namespace: "apple", name: "fishcake" },
+    },
+    {
+      transformType: "remove_stream",
+      streamDescriptor: { namespace: "apple", name: "gelatin_mold" },
+    },
+  ],
+};
+
 export const Primary = Template.bind({});
-
 Primary.args = {
-  catalogDiff: {
-    transforms: [
-      {
-        transformType: "update_stream",
-        streamDescriptor: { namespace: "apple", name: "harissa_paste" },
-        updateStream: [
-          { transformType: "add_field", fieldName: ["users", "phone"], breaking: false },
-          { transformType: "add_field", fieldName: ["users", "email"], breaking: false },
-          { transformType: "remove_field", fieldName: ["users", "lastName"], breaking: false },
+  catalogDiff: fullCatalogDiff,
+  catalog: { streams: [] },
+};
 
-          {
-            transformType: "update_field_schema",
-            breaking: false,
-            fieldName: ["users", "address"],
-            updateFieldSchema: { oldSchema: { type: "number" }, newSchema: { type: "string" } },
-          },
-        ],
-      },
-      {
-        transformType: "add_stream",
-        streamDescriptor: { namespace: "apple", name: "banana" },
-      },
-      {
-        transformType: "add_stream",
-        streamDescriptor: { namespace: "apple", name: "carrot" },
-      },
-      {
-        transformType: "remove_stream",
-        streamDescriptor: { namespace: "apple", name: "dragonfruit" },
-      },
-      {
-        transformType: "remove_stream",
-        streamDescriptor: { namespace: "apple", name: "eclair" },
-      },
-      {
-        transformType: "remove_stream",
-        streamDescriptor: { namespace: "apple", name: "fishcake" },
-      },
-      {
-        transformType: "remove_stream",
-        streamDescriptor: { namespace: "apple", name: "gelatin_mold" },
-      },
-    ],
-  },
+export const OneStreamAdd = Template.bind({});
+OneStreamAdd.args = {
+  catalogDiff: oneStreamAddCatalogDiff,
+  catalog: { streams: [] },
+};
+
+export const OneFieldAdd = Template.bind({});
+OneFieldAdd.args = {
+  catalogDiff: oneFieldAddCatalogDiff,
+  catalog: { streams: [] },
+};
+
+export const OneFieldUpdate = Template.bind({});
+OneFieldUpdate.args = {
+  catalogDiff: oneFieldUpdateCatalogDiff,
   catalog: { streams: [] },
 };
