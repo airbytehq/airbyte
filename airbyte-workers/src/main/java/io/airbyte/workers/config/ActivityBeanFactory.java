2ce2df378380476ebfdc2392d22886eaa2140e45
Benoit Moriceau <benoit@airbyte.io>
2022-11-15 13:47:44 -0800

    Make sure that long running activity are not retried

--- airbyte-workers/src/main/java/io/airbyte/workers/config/ActivityBeanFactory.java
+++ airbyte-workers/src/main/java/io/airbyte/workers/config/ActivityBeanFactory.java
@@ -170,6 +170,7 @@ public class ActivityBeanFactory {
   public RetryOptions containerOrchestratorRetryOptions() {
     return RetryOptions.newBuilder()
         .setDoNotRetry(RuntimeException.class.getName(), WorkerException.class.getName())
+        .setMaximumAttempts(1)
         .build();
   }
 
