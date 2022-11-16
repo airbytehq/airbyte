0c7d98a4fb8c72562fbb2285699fd6fe4ab97bfc
Alexandre Girard <alexandre@airbyte.io>
2022-11-16 13:12:33 -0800

    Run integration tests

--- airbyte-connector-builder-server/run.sh
+++ airbyte-connector-builder-server/run.sh
@@ -2,5 +2,5 @@ cd /tmp
 pip install -e .
 pip install -e '.[main]'
 pip install -e '.[tests]'
-#python -m pytest unit_tests
 python -m coverage run -m pytest unit_tests -c pytest.ini
+python -m coverage run -m pytest integration_tests -c pytest.ini
