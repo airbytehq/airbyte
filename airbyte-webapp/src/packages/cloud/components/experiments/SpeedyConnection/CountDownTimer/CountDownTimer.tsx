3856ae695fcde65bf8bceaadd019902e91f57b9c
letiescanciano <45267095+letiescanciano@users.noreply.github.com>
2022-11-17 08:13:00 +0100

    🪟 🧪 [Experiment] Show speedy connection banner only to corporate emails (#19354)
    * 🪟 🧪 [Experiment] Show speedy connection banner only to corporate emails
    
    * PR comments
    
    * update banner styles to match design. Remove seconds
    
    Co-authored-by: Tim Roes <tim@airbyte.io>

--- airbyte-webapp/src/packages/cloud/components/experiments/SpeedyConnection/CountDownTimer/CountDownTimer.tsx
+++ airbyte-webapp/src/packages/cloud/components/experiments/SpeedyConnection/CountDownTimer/CountDownTimer.tsx
@@ -3,13 +3,12 @@ import { Text } from "components/ui/Text";
 import styles from "./CountDownTimer.module.scss";
 import { useCountdown } from "./useCountdown";
 export const CountDownTimer: React.FC<{ expiredOfferDate: string }> = ({ expiredOfferDate }) => {
-  const [hours, minutes, seconds] = useCountdown(expiredOfferDate);
+  const [hours, minutes] = useCountdown(expiredOfferDate);
 
   return (
     <div className={styles.countDownTimerContainer}>
       <Text className={styles.countDownTimerItem}>{hours.toString().padStart(2, "0")}h</Text>
       <Text className={styles.countDownTimerItem}>{minutes.toString().padStart(2, "0")}m</Text>
-      <Text className={styles.countDownTimerItem}>{seconds.toString().padStart(2, "0")}s</Text>
     </div>
   );
 };
