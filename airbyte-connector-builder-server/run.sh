5ff630ea4aab14092ad1a8ed5686282385d62d46
Alexandre Girard <alexandre@airbyte.io>
2022-11-16 18:18:36 -0800

    update

--- airbyte-connector-builder-server/run.sh
+++ airbyte-connector-builder-server/run.sh
@@ -1,10 +1,15 @@
 cd $1
 
+# Install dependencies
 pip install -e .
 pip install -e '.[main]'
 pip install -e '.[tests]'
+
+# Run the tests
 python -m coverage run -m pytest unit_tests -c pytest.ini
 python -m coverage run -m pytest integration_tests -c pytest.ini
+
+# Pasted from
 pip install 'mccabe==0.6.1'
 pip install 'flake8==4.0.1'
 pip install 'pyproject-flake8==0.0.1a2'
@@ -13,11 +18,12 @@ pip install 'mypy==0.930'
 pip install 'isort==5.6.4'
 pip install 'pytest==6.1.2'
 pip install 'coverage[toml]==6.3.1'
-apt update
-apt install -y git
+
+# Format and static analysis
 python -m isort --settings-file=pyproject.toml ./
 python -m isort --settings-file=pyproject.toml --diff --quiet ./
 python -m black --config pyproject.toml ./
 python -m black --config pyproject.toml ./ --diff --quiet
 python -m pflake8 --config pyproject.toml ./
 python -m pflake8 --config pyproject.toml ./ --diff --quiet
+python -m mypy --config pyproject.toml ./
