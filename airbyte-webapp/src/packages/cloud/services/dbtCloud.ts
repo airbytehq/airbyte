6f18f68a927127f8426fcd551f5945ea9dff211b
Alex Birdsall <ambirdsall@gmail.com>
2022-11-18 10:16:16 -0800

    Clean up type definitions
    Also removes the "Error" from the `{setV,v}alidationErrorMessage`
    variables to pave the way for success feedback.

--- airbyte-webapp/src/packages/cloud/services/dbtCloud.ts
+++ airbyte-webapp/src/packages/cloud/services/dbtCloud.ts
@@ -64,6 +64,8 @@ export const isSameJob = (remoteJob: DbtCloudJobInfo, savedJob: DbtCloudJob): bo
 
 type ServiceToken = string;
 
+type ServiceToken = string;
+
 export const useSubmitDbtCloudIntegrationConfig = () => {
   const { workspaceId } = useCurrentWorkspace();
   const { mutateAsync: updateWorkspace } = useUpdateWorkspace();
