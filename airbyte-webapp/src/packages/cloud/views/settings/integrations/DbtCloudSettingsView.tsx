b05389d47fc40cdf8162dae4c2ae93e03933f0bf
Alex Birdsall <ambirdsall@gmail.com>
2022-11-17 23:08:44 -0800

    Display service token validation errors in the UI

--- airbyte-webapp/src/packages/cloud/views/settings/integrations/DbtCloudSettingsView.tsx
+++ airbyte-webapp/src/packages/cloud/views/settings/integrations/DbtCloudSettingsView.tsx
@@ -1,5 +1,5 @@
 import { Field, FieldProps, Form, Formik } from "formik";
-import React from "react";
+import React, { useState } from "react";
 import { FormattedMessage } from "react-intl";
 
 import { LabeledInput } from "components/LabeledInput";
@@ -12,6 +12,8 @@ import styles from "./DbtCloudSettingsView.module.scss";
 
 export const DbtCloudSettingsView: React.FC = () => {
   const { mutate: submitDbtCloudIntegrationConfig, isLoading } = useSubmitDbtCloudIntegrationConfig();
+  const [hasValidationError, setHasValidationError] = useState<boolean>(false);
+  const [validationErrorMessage, setValidationErrorMessage] = useState<string | null>(null);
   return (
     <SettingsCard title={<FormattedMessage id="settings.integrationSettings.dbtCloudSettings" />}>
       <Content>
@@ -19,7 +21,21 @@ export const DbtCloudSettingsView: React.FC = () => {
           initialValues={{
             serviceToken: "",
           }}
-          onSubmit={({ serviceToken }) => submitDbtCloudIntegrationConfig(serviceToken)}
+          onSubmit={({ serviceToken }) => {
+            setHasValidationError(false);
+            return submitDbtCloudIntegrationConfig(serviceToken, {
+              onError: (e) => {
+                setHasValidationError(true);
+
+                // I honestly don't know why I had to do this; for some reason, when I
+                // gave a `TError` type argument to the upstream `useMutation`, I start
+                // getting spurious "no matching overloads" errors.
+                // eslint-disable-next-line
+                setValidationErrorMessage((e as any)?.message?.replace("Internal Server Error: ", ""));
+              },
+              onSuccess: () => setValidationErrorMessage(null),
+            });
+          }}
         >
           <Form>
             <Field name="serviceToken">
@@ -27,6 +43,8 @@ export const DbtCloudSettingsView: React.FC = () => {
                 <LabeledInput
                   {...field}
                   label={<FormattedMessage id="settings.integrationSettings.dbtCloudSettings.form.serviceToken" />}
+                  error={hasValidationError}
+                  message={validationErrorMessage}
                   type="text"
                 />
               )}
