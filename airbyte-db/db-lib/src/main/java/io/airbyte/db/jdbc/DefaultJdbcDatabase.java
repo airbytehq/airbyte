f4df336b2e9eec25fc3d5b9d35586b61b2d131e1
vmaltsev <ihbectop@gmail.com>
2022-11-22 15:55:55 +0200

    JDBC Sources: Wrap SQLTransientException with ConfigErrorException

--- airbyte-db/db-lib/src/main/java/io/airbyte/db/jdbc/DefaultJdbcDatabase.java
+++ airbyte-db/db-lib/src/main/java/io/airbyte/db/jdbc/DefaultJdbcDatabase.java
@@ -5,15 +5,18 @@
 package io.airbyte.db.jdbc;
 
 import com.google.errorprone.annotations.MustBeClosed;
+import io.airbyte.commons.exceptions.ConfigErrorException;
 import io.airbyte.commons.exceptions.ConnectionErrorException;
 import io.airbyte.commons.functional.CheckedConsumer;
 import io.airbyte.commons.functional.CheckedFunction;
 import io.airbyte.db.JdbcCompatibleSourceOperations;
+
 import java.sql.Connection;
 import java.sql.DatabaseMetaData;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.SQLTransientException;
 import java.util.List;
 import java.util.Objects;
 import java.util.stream.Collectors;
@@ -79,6 +82,12 @@ public class DefaultJdbcDatabase extends JdbcDatabase {
       final DatabaseMetaData metaData = connection.getMetaData();
       return metaData;
     } catch (final SQLException e) {
+      if (e instanceof SQLTransientException){
+        final String message = e.getMessage();
+        if (message.contains("request timed out")){
+          throw new ConfigErrorException("Connection timed out. Unable to contact server.");
+        }
+      }
       // Some databases like Redshift will have null cause
       if (Objects.isNull(e.getCause()) || !(e.getCause() instanceof SQLException)) {
         throw new ConnectionErrorException(e.getSQLState(), e.getErrorCode(), e.getMessage(), e);
