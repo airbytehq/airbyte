c360475eef2c5514097f8410566956e3c6d9c4d7
lmossman <lake@airbyte.io>
2022-11-22 11:28:18 -0800

    add comments to craco config

--- airbyte-webapp/craco.config.js
+++ airbyte-webapp/craco.config.js
@@ -7,7 +7,9 @@ module.exports = {
   webpack: {
     configure: (webpackConfig) => {
       const overriddenConfig = merge({}, webpackConfig, {
+        // suppress warnings of libraries missing source maps
         ignoreWarnings: [/Failed to parse source map/],
+        // add required polyfills for some libraries
         resolve: {
           fallback: {
             https: false,
@@ -31,12 +33,15 @@ module.exports = {
           filename: "static/css/[name].[contenthash:8].css",
           chunkFilename: "static/css/[name].[contenthash:8].chunk.css",
         }),
+        // polyfill global nodeJS variables required by some libraries
         new webpack.ProvidePlugin({
           process: "process/browser",
           Buffer: ["buffer", "Buffer"],
         }),
       ];
 
+      // required for process/browser polyfill to work
+      // https://github.com/microsoft/PowerBI-visuals-tools/issues/365#issuecomment-1099716186
       overriddenConfig.module.rules.push({
         test: /\.m?js/,
         resolve: {
