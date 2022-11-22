5b396585745e744c5cf200bfbb4881e060f2fc93
Mahdi Dibaiee <mdibaiee@pm.me>
2022-11-22 14:35:21 +0000

    base_venv: fix activate

--- tools/bin/setup_base_venv.sh
+++ tools/bin/setup_base_venv.sh
@@ -11,7 +11,7 @@ python get-pip.py
 for base in base-normalization base-python base-python-test source-acceptance-test base-singer airbyte-protocol
 do
   $PYTHON_EXECUTABLE -m venv --without-pip airbyte-integrations/bases/$base/.venv
-  source airbyte-integrations/bases/$base/.venv
+  source airbyte-integrations/bases/$base/.venv/bin/activate
   python get-pip.py
 done
 cd airbyte-integrations/connectors/$CONNECTOR
