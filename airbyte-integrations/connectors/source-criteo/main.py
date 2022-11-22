b4a5bf4c8b315c641effb99fb8023e9f1dde8b8f
carlonuccio <carlonuccio91@gmail.com>
2022-11-22 15:16:47 +0100

    added source-criteo folder

---
+++ airbyte-integrations/connectors/source-criteo/main.py
@@ -0,0 +1,13 @@
+#
+# Copyright (c) 2022 Airbyte, Inc., all rights reserved.
+#
+
+
+import sys
+
+from airbyte_cdk.entrypoint import launch
+from source_criteo import SourceCriteo
+
+if __name__ == "__main__":
+    source = SourceCriteo()
+    launch(source, sys.argv[1:])
