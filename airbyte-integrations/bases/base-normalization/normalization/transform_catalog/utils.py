b5ab1369293269dd55918e456db634ff263f8b2a
ievgeniit <etsybaev@gmail.com>
2022-11-22 17:54:46 +0100

    Updated normalization simple stream processing to handle new datatypes

--- airbyte-integrations/bases/base-normalization/normalization/transform_catalog/utils.py
+++ airbyte-integrations/bases/base-normalization/normalization/transform_catalog/utils.py
@@ -6,6 +6,7 @@
 from typing import Set, Union
 
 from normalization.transform_catalog import dbt_macro
+from normalization import data_type
 
 
 def jinja_call(command: Union[str, dbt_macro.Macro]) -> str:
@@ -17,75 +18,92 @@ def remove_jinja(command: str) -> str:
 
 
 def is_string(property_type) -> bool:
-    return property_type == "string" or "string" in property_type
+    return property_type == data_type.STRING_TYPE or data_type.STRING_TYPE in property_type
 
 
 def is_datetime(definition: dict) -> bool:
-    return (
-        is_string(definition["type"])
-        and ("format" in definition.keys())
-        and (definition["format"] == "date-time" or "date-time" in definition["format"])
-    )
+    return is_datetime_with_timezone(definition) or is_datetime_without_timezone(definition)
+    # return (
+    #     is_string(definition[data_type.REF_TYPE_VAR_NAME])
+    #     and ("format" in definition.keys())
+    #     and (definition["format"] == "date-time" or "date-time" in definition["format"])
+    # )
 
 
 def is_datetime_without_timezone(definition: dict) -> bool:
-    return is_datetime(definition) and definition.get("airbyte_type") == "timestamp_without_timezone"
+    property_type = definition[data_type.REF_TYPE_VAR_NAME]
+    return property_type == data_type.TIMESTAMP_WITHOUT_TIMEZONE_TYPE or data_type.TIMESTAMP_WITHOUT_TIMEZONE_TYPE in property_type
+    # return is_datetime(definition) and definition.get("airbyte_type") == "timestamp_without_timezone"
 
 
 def is_datetime_with_timezone(definition: dict) -> bool:
-    return is_datetime(definition) and (not definition.get("airbyte_type") or definition.get("airbyte_type") == "timestamp_with_timezone")
+    property_type = definition[data_type.REF_TYPE_VAR_NAME]
+    return property_type == data_type.TIMESTAMP_WITH_TIMEZONE_TYPE or data_type.TIMESTAMP_WITH_TIMEZONE_TYPE in property_type
+    # return is_datetime(definition) and (not definition.get("airbyte_type") or definition.get("airbyte_type") == "timestamp_with_timezone")
 
 
 def is_date(definition: dict) -> bool:
-    return (
-        is_string(definition["type"])
-        and ("format" in definition.keys())
-        and (definition["format"] == "date" or "date" in definition["format"])
-    )
+    property_type = definition[data_type.REF_TYPE_VAR_NAME]
+    return property_type == data_type.DATE_TYPE or data_type.DATE_TYPE in property_type
+    # return (
+    #     is_string(definition[data_type.TYPE_VAR_NAME])
+    #     and ("format" in definition.keys())
+    #     and (definition["format"] == "date" or "date" in definition["format"])
+    # )
 
 
 def is_time(definition: dict) -> bool:
-    return is_string(definition["type"]) and definition.get("format") == "time"
+    return is_time_with_timezone(definition) or is_time_without_timezone(definition)
+    # return is_string(definition[data_type.REF_TYPE_VAR_NAME]) and definition.get("format") == "time"
 
 
 def is_time_with_timezone(definition: dict) -> bool:
-    return is_time(definition) and definition.get("airbyte_type") == "time_with_timezone"
+    property_type = definition[data_type.REF_TYPE_VAR_NAME]
+    return property_type == data_type.TIME_WITH_TIME_ZONE_TYPE or data_type.TIME_WITH_TIME_ZONE_TYPE in property_type
+    # return is_time(definition) and definition.get("airbyte_type") == "time_with_timezone"
 
 
 def is_time_without_timezone(definition: dict) -> bool:
-    return is_time(definition) and definition.get("airbyte_type") == "time_without_timezone"
+    property_type = definition[data_type.REF_TYPE_VAR_NAME]
+    return property_type == data_type.TIME_WITHOUT_TIME_ZONE_TYPE or data_type.TIME_WITHOUT_TIME_ZONE_TYPE in property_type
+    # return is_time(definition) and definition.get("airbyte_type") == "time_without_timezone"
 
 
 def is_number(property_type) -> bool:
     if is_string(property_type):
         # Handle union type, give priority to wider scope types
         return False
-    return property_type == "number" or "number" in property_type
+    return property_type == data_type.NUMBER_TYPE or data_type.NUMBER_TYPE in property_type
 
 
+#TODO drop it !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 def is_big_integer(definition: dict) -> bool:
-    return "airbyte_type" in definition and definition["airbyte_type"] == "big_integer"
+    property_type = definition[data_type.REF_TYPE_VAR_NAME]
+    return property_type == data_type.INTEGER_TYPE or data_type.INTEGER_TYPE in property_type
+    # return "airbyte_type" in definition and definition["airbyte_type"] == "big_integer"
 
 
 def is_long(property_type, definition: dict) -> bool:
-    # Check specifically for {type: number, airbyte_type: integer}
-    if (
-        (property_type == "number" or "number" in property_type)
-        and "airbyte_type" in definition
-        and definition["airbyte_type"] == "integer"
-    ):
-        return True
-    if is_string(property_type) or is_number(property_type):
-        # Handle union type, give priority to wider scope types
-        return False
-    return property_type == "integer" or "integer" in property_type
+    return property_type == data_type.INTEGER_TYPE or data_type.INTEGER_TYPE in property_type
+    # # Check specifically for {type: number, airbyte_type: integer}
+    # if (
+    #     (property_type == data_type.NUMBER_TYPE or data_type.NUMBER_TYPE in property_type)
+    #     and "airbyte_type" in definition
+    #     and definition["airbyte_type"] == "integer"
+    # ):
+    #     return True
+    # if is_string(property_type) or is_number(property_type):
+    #     # Handle union type, give priority to wider scope types
+    #     return False
+    # return property_type == "integer" or "integer" in property_type
 
 
 def is_boolean(property_type, definition: dict) -> bool:
+    # TODO do we need it now?
     if is_string(property_type) or is_number(property_type) or is_big_integer(definition) or is_long(property_type, definition):
         # Handle union type, give priority to wider scope types
         return False
-    return property_type == "boolean" or "boolean" in property_type
+    return property_type == data_type.BOOLEAN_TYPE or data_type.BOOLEAN_TYPE in property_type
 
 
 def is_array(property_type) -> bool:
@@ -101,10 +119,10 @@ def is_airbyte_column(name: str) -> bool:
 
 
 def is_simple_property(definition: dict) -> bool:
-    if "type" not in definition:
+    if data_type.REF_TYPE_VAR_NAME not in definition:
         property_type = "object"
     else:
-        property_type = definition["type"]
+        property_type = definition[data_type.REF_TYPE_VAR_NAME]
     return (
         is_string(property_type)
         or is_big_integer(definition)
