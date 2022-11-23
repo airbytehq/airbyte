3daa9ad4e06f5fbe7af2def6481d687bcc79154a
Jimmy Ma <jimmy@airbyte.io>
2022-11-23 11:12:40 -0800

    Update tools to use docker compose v2

--- tools/bin/acceptance_test.sh
+++ tools/bin/acceptance_test.sh
@@ -24,9 +24,9 @@ echo "Starting app..."
 VERSION=dev TRACKING_STRATEGY=logging USE_STREAM_CAPABLE_STATE=true BASIC_AUTH_USERNAME="" BASIC_AUTH_PASSWORD="" docker compose -f docker-compose.yaml -f docker-compose.acceptance-test.yaml up -d
 
 # Sometimes source/dest containers using airbyte volumes survive shutdown, which need to be killed in order to shut down properly.
-shutdown_cmd="docker compose down -v || docker kill \$(docker ps -a -f volume=airbyte_workspace -f volume=airbyte_data -f volume=airbyte_db -q) && docker-compose down -v"
+shutdown_cmd="docker compose down -v || docker kill \$(docker ps -a -f volume=airbyte_workspace -f volume=airbyte_data -f volume=airbyte_db -q) && docker compose down -v"
 # Uncomment for debugging. Warning, this is verbose.
-# trap "echo 'docker-compose logs:' && docker-compose logs -t --tail 1000 && $shutdown_cmd" EXIT
+# trap "echo 'docker compose logs:' && docker compose logs -t --tail 1000 && $shutdown_cmd" EXIT
 
 echo "Waiting for services to begin"
 starttime=`get_epoch_time`
