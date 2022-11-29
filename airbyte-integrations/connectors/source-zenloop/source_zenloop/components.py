8d90fede14e3100ec694ab956fa3881d77beea9d
Serhii Lazebnyi <serhii.lazebnyi@globallogic.com>
2022-11-29 12:53:43 +0100

    Updated to review

--- airbyte-integrations/connectors/source-zenloop/source_zenloop/components.py
+++ airbyte-integrations/connectors/source-zenloop/source_zenloop/components.py
@@ -25,7 +25,7 @@ class ZenloopDatetimeStreamSlicer(DatetimeStreamSlicer):
         next_page_token: Optional[Mapping[str, Any]] = None,
     ) -> Mapping[str, Any]:
         params = super().get_request_params(stream_state=stream_state, stream_slice=stream_slice, next_page_token=next_page_token)
-        date_shortcut = {"date_shortcut": "custom"}
+        date_shortcut = {"date_shortcut": "custom"}  # required request param when specified date_from and date_to params
         params.update(date_shortcut)
         return params
 
@@ -33,6 +33,14 @@ class ZenloopDatetimeStreamSlicer(DatetimeStreamSlicer):
 @dataclass
 class ZenloopSubstreamSlicer(SubstreamSlicer):
     def stream_slices(self, sync_mode: SyncMode, stream_state: StreamState) -> Iterable[StreamSlice]:
+        """
+
+        config_parent_field : parent field name in config
+
+        Use parent id's as stream state value if it specified in config or
+        create stream_slices according SubstreamSlicer logic.
+
+        """
         config = self._options.get("config")
         parent_field = self._options.get("config_parent_field")
         custom_stream_state_value = config.get(parent_field)
@@ -43,31 +51,3 @@ class ZenloopSubstreamSlicer(SubstreamSlicer):
             for parent_stream_config in self.parent_stream_configs:
                 stream_state_field = parent_stream_config.stream_slice_field or None
                 yield {stream_state_field: custom_stream_state_value, "parent_slice": {}}
-
-
-@dataclass
-class ZenloopPageIncrement(PageIncrement):
-    """
-    Starts page from 1 instead of the default value that is 0. Stops Pagination when next page not exist.
-    """
-
-    def next_page_token(self, response: requests.Response, last_records: List[Mapping[str, Any]]) -> Optional[Any]:
-        decoded_response = response.json()
-
-        current_page = decoded_response["meta"]["page"]
-        per_page = decoded_response["meta"]["per_page"]
-        total = decoded_response["meta"]["total"]
-
-        next_page_exist = current_page < math.ceil(total / per_page)
-
-        if next_page_exist:
-            self._page += 1
-            return self._page
-        else:
-            return None
-
-    def __post_init__(self, options: Mapping[str, Any]):
-        self._page = 1
-
-    def reset(self):
-        self._page = 1
