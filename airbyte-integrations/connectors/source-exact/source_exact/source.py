795db6fd1f3c9ec4887c7f68f01f1450501d1d1b
Joël Luijmes <me@joell.dev>
2022-11-29 15:32:08 +0100

    feat(exact): sync incremental

--- airbyte-integrations/connectors/source-exact/source_exact/source.py
+++ airbyte-integrations/connectors/source-exact/source_exact/source.py
@@ -2,27 +2,71 @@
 # Copyright (c) 2022 Airbyte, Inc., all rights reserved.
 #
 
-
-from abc import ABC, abstractmethod
-import logging
 from typing import Any, Iterable, List, Mapping, MutableMapping, Optional, Tuple
 
 import requests
 import pendulum
 import re
 
-from datetime import datetime, timezone
-from airbyte_cdk.models import AirbyteCatalog
 from airbyte_cdk.sources import AbstractSource
-from airbyte_cdk.sources.streams import Stream
+from airbyte_cdk.sources.streams import Stream, IncrementalMixin
+from airbyte_cdk.sources.streams.core import StreamData
 from airbyte_cdk.sources.streams.http import HttpStream
 from airbyte_cdk.sources.streams.http.requests_native_auth import Oauth2Authenticator
 
 
-# Basic full refresh stream
-class ExactStream(HttpStream, ABC):
+class SingleRefreshOauth2Authenticator(Oauth2Authenticator):
+    def refresh_access_token(self) -> Tuple[str, int]:
+        """
+        Returns the refresh token and its lifespan in seconds
+
+        :return: a tuple of (access_token, token_lifespan_in_seconds)
+        """
+        try:
+            response = requests.post(
+                url=self.get_token_refresh_endpoint(),
+                data=self.build_refresh_request_body(),
+
+            )
+            response_json = response.json()
+            print(response_json)
+            response.raise_for_status()
+
+            self._refresh_token = response_json["refresh_token"]
+
+            return response_json[self.get_access_token_name()], response_json[self.get_expires_in_name()]
+        except Exception as e:
+            raise Exception(f"Error while refreshing access token: {e}") from e
+
+
+class ExactStream(HttpStream, IncrementalMixin):
     url_base = "https://start.exactonline.nl/api/v1/3361923/"
-    
+    cursor_field = "Timestamp"
+    state_checkpoint_interval = 1000
+
+    _cursor_value = None
+
+    @property
+    def state(self) -> MutableMapping[str, Any]:
+        if self._cursor_value:
+            return {self.cursor_field: self._cursor_value}
+        else:
+            return {}
+
+    @state.setter
+    def state(self, value: MutableMapping[str, Any]):
+        if value:
+            self._cursor_value = value[self.cursor_field]
+
+    def read_records(self, *args, **kwargs) -> Iterable[StreamData]:
+        for record in super().read_records(*args, **kwargs):
+            # Track the largest timestamp value
+            timestamp = record[self.cursor_field]
+            self._cursor_value = max(timestamp, self._cursor_value) if self._cursor_value else timestamp
+
+            yield record
+
+
     def next_page_token(self, response: requests.Response) -> Optional[Mapping[str, Any]]:
         """
         If response contains the __next property, there are more pages. This property contains the full url to 
@@ -31,10 +75,9 @@ class ExactStream(HttpStream, ABC):
 
         response_json = response.json()
         next_url = response_json.get("d", {}).get("__next")
-        return {
-            "next_url": next_url
-        }
 
+        return { "next_url": next_url } if next_url else None
+        
 
     def request_headers(self, **kwargs) -> MutableMapping[str, Any]:
         """
@@ -50,15 +93,20 @@ class ExactStream(HttpStream, ABC):
         The sync endpoints requires selection of fields to return. We use the configured catalog to make selection
         of fields we want to have.
         """
-        
+
         # Contains the full next page, so don't append new query params
         if next_page_token:
             return {}
 
         configured_properties = list(self.get_json_schema()["properties"].keys())
-        return {
-            "$select": ",".join(configured_properties)
+        params = {
+            "$select": ",".join(configured_properties),
         }
+
+        if self._cursor_value:
+            params["$filter"] = f"Timestamp gt {self._cursor_value}L"
+
+        return params
 
 
     def parse_response(self, response: requests.Response, **kwargs) -> Iterable[Mapping]:
@@ -84,7 +132,7 @@ class ExactStream(HttpStream, ABC):
 
     def _parse_timestamps(self, obj: dict):
         """
-        Exact returns timestamps in following format: /Date(1672531200000)/
+        Exact returns timestamps in following format: /Date(1672531200000)/ (OData date format).
         The value is in seconds since Epoch (UNIX time). Note, the time is in CET and not in GMT/UTC.
         https://support.exactonline.com/community/s/knowledge-base#All-All-DNO-Content-faq-rest-api
         """
@@ -114,77 +162,8 @@ class ExactStream(HttpStream, ABC):
 
 class Subscriptions(ExactStream):
     primary_key = "EntryID"
-    endpoint = "subscription/Subscriptions"
+    endpoint = "sync/subscription/Subscriptions"
 
-
-# # Basic incremental stream
-# class IncrementalExactStream(ExactStream, ABC):
-#     """
-#     TODO fill in details of this class to implement functionality related to incremental syncs for your connector.
-#          if you do not need to implement incremental sync for any streams, remove this class.
-#     """
-
-#     # TODO: Fill in to checkpoint stream reads after N records. This prevents re-reading of data if the stream fails for any reason.
-#     state_checkpoint_interval = None
-
-#     @property
-#     def cursor_field(self) -> str:
-#         """
-#         TODO
-#         Override to return the cursor field used by this stream e.g: an API entity might always use created_at as the cursor field. This is
-#         usually id or date based. This field's presence tells the framework this in an incremental stream. Required for incremental.
-
-#         :return str: The name of the cursor field.
-#         """
-#         return []
-
-#     def get_updated_state(self, current_stream_state: MutableMapping[str, Any], latest_record: Mapping[str, Any]) -> Mapping[str, Any]:
-#         """
-#         Override to determine the latest state after reading the latest record. This typically compared the cursor_field from the latest record and
-#         the current state and picks the 'most' recent cursor. This is how a stream's state is determined. Required for incremental.
-#         """
-#         return {}
-
-
-# class Employees(IncrementalExactStream):
-#     """
-#     TODO: Change class name to match the table/data source this stream corresponds to.
-#     """
-
-#     # TODO: Fill in the cursor_field. Required.
-#     cursor_field = "start_date"
-
-#     # TODO: Fill in the primary key. Required. This is usually a unique field in the stream, like an ID or a timestamp.
-#     primary_key = "employee_id"
-
-#     def path(self, **kwargs) -> str:
-#         """
-#         TODO: Override this method to define the path this stream corresponds to. E.g. if the url is https://example-api.com/v1/employees then this should
-#         return "single". Required.
-#         """
-#         return "employees"
-
-#     def stream_slices(self, stream_state: Mapping[str, Any] = None, **kwargs) -> Iterable[Optional[Mapping[str, any]]]:
-#         """
-#         TODO: Optionally override this method to define this stream's slices. If slicing is not needed, delete this method.
-
-#         Slices control when state is saved. Specifically, state is saved after a slice has been fully read.
-#         This is useful if the API offers reads by groups or filters, and can be paired with the state object to make reads efficient. See the "concepts"
-#         section of the docs for more information.
-
-#         The function is called before reading any records in a stream. It returns an Iterable of dicts, each containing the
-#         necessary data to craft a request for a slice. The stream state is usually referenced to determine what slices need to be created.
-#         This means that data in a slice is usually closely related to a stream's cursor_field and stream_state.
-
-#         An HTTP request is made for each returned slice. The same slice can be accessed in the path, request_params and request_header functions to help
-#         craft that specific request.
-
-#         For example, if https://example-api.com/v1/employees offers a date query params that returns data for that particular day, one way to implement
-#         this would be to consult the stream state object for the last synced date, then return a slice containing each date from the last synced date
-#         till now. The request_params function would then grab the date from the stream_slice and make it part of the request by injecting it into
-#         the date query param.
-#         """
-#         raise NotImplementedError("Implement stream slices or delete this method!")
 
 
 # Source
@@ -246,26 +225,3 @@ class SourceExact(AbstractSource):
         return [
             Subscriptions(authenticator=auth),
         ]
-
-class SingleRefreshOauth2Authenticator(Oauth2Authenticator):
-    def refresh_access_token(self) -> Tuple[str, int]:
-        """
-        Returns the refresh token and its lifespan in seconds
-
-        :return: a tuple of (access_token, token_lifespan_in_seconds)
-        """
-        try:
-            response = requests.post(
-                url=self.get_token_refresh_endpoint(),
-                data=self.build_refresh_request_body(),
-
-            )
-            response_json = response.json()
-            print(response_json)
-            response.raise_for_status()
-
-            self._refresh_token = response_json["refresh_token"]
-
-            return response_json[self.get_access_token_name()], response_json[self.get_expires_in_name()]
-        except Exception as e:
-            raise Exception(f"Error while refreshing access token: {e}") from e
