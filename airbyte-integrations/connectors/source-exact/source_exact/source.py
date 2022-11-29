bbc51d31d4e6be5a1949679f06bd97f22f02da9c
Joël Luijmes <me@joell.dev>
2022-11-29 12:21:26 +0100

    feat(exact): initial subscription sync

--- airbyte-integrations/connectors/source-exact/source_exact/source.py
+++ airbyte-integrations/connectors/source-exact/source_exact/source.py
@@ -3,190 +3,155 @@
 #
 
 
-from abc import ABC
+from abc import ABC, abstractmethod
+import logging
 from typing import Any, Iterable, List, Mapping, MutableMapping, Optional, Tuple
 
 import requests
 import pendulum
 
+from airbyte_cdk.models import AirbyteCatalog
 from airbyte_cdk.sources import AbstractSource
 from airbyte_cdk.sources.streams import Stream
 from airbyte_cdk.sources.streams.http import HttpStream
 from airbyte_cdk.sources.streams.http.requests_native_auth import Oauth2Authenticator
 
-"""
-TODO: Most comments in this class are instructive and should be deleted after the source is implemented.
-
-This file provides a stubbed example of how to use the Airbyte CDK to develop both a source connector which supports full refresh or and an
-incremental syncs from an HTTP API.
-
-The various TODOs are both implementation hints and steps - fulfilling all the TODOs should be sufficient to implement one basic and one incremental
-stream from a source. This pattern is the same one used by Airbyte internally to implement connectors.
-
-The approach here is not authoritative, and devs are free to use their own judgement.
-
-There are additional required TODOs in the files within the integration_tests folder and the spec.yaml file.
-"""
-
 
 # Basic full refresh stream
 class ExactStream(HttpStream, ABC):
-    """
-    TODO remove this comment
-
-    This class represents a stream output by the connector.
-    This is an abstract base class meant to contain all the common functionality at the API level e.g: the API base URL, pagination strategy,
-    parsing responses etc..
-
-    Each stream should extend this class (or another abstract subclass of it) to specify behavior unique to that stream.
-
-    Typically for REST APIs each stream corresponds to a resource in the API. For example if the API
-    contains the endpoints
-        - GET v1/customers
-        - GET v1/employees
-
-    then you should have three classes:
-    `class ExactStream(HttpStream, ABC)` which is the current class
-    `class Customers(ExactStream)` contains behavior to pull data for customers using v1/customers
-    `class Employees(ExactStream)` contains behavior to pull data for employees using v1/employees
-
-    If some streams implement incremental sync, it is typical to create another class
-    `class IncrementalExactStream((ExactStream), ABC)` then have concrete stream implementations extend it. An example
-    is provided below.
-
-    See the reference docs for the full list of configurable options.
-    """
-
-    url_base = "https://start.exactonline.nl/api"
+    url_base = "https://start.exactonline.nl/api/v1/3361923/"
     
     def next_page_token(self, response: requests.Response) -> Optional[Mapping[str, Any]]:
         """
-        TODO: Override this method to define a pagination strategy. If you will not be using pagination, no action is required - just return None.
-
-        This method should return a Mapping (e.g: dict) containing whatever information required to make paginated requests. This dict is passed
-        to most other methods in this class to help you form headers, request bodies, query params, etc..
-
-        For example, if the API accepts a 'page' parameter to determine which page of the result to return, and a response from the API contains a
-        'page' number, then this method should probably return a dict {'page': response.json()['page'] + 1} to increment the page count by 1.
-        The request_params method should then read the input next_page_token and set the 'page' param to next_page_token['page'].
-
-        :param response: the most recent response from the API
-        :return If there is another page in the result, a mapping (e.g: dict) containing information needed to query the next page in the response.
-                If there are no more pages in the result, return None.
+        If response contains the __next property, there are more pages. This property contains the full url to 
+        call next including endpoint and all query parameters.
         """
-        return None
 
-    def request_params(
-        self, stream_state: Mapping[str, Any], stream_slice: Mapping[str, any] = None, next_page_token: Mapping[str, Any] = None
-    ) -> MutableMapping[str, Any]:
+        response_json = response.json()
+        next_url = response_json.get("d", {}).get("__next")
+        return {
+            "next_url": next_url
+        }
+
+
+    def request_headers(self, **kwargs) -> MutableMapping[str, Any]:
         """
-        TODO: Override this method to define any query parameters to be set. Remove this method if you don't need to define request params.
-        Usually contains common params e.g. pagination size etc.
+        Default response type is XML, this is overriden to return JSON.
         """
-        return {}
+
+        return {
+            "Accept": "application/json"
+        }
 
     def parse_response(self, response: requests.Response, **kwargs) -> Iterable[Mapping]:
+        # Parse the results array from returned object
+        response_json = response.json()
+        return response_json.get("d", {}).get("results")
+
+    
+    def path(self, next_page_token: Mapping[str, Any] = None, **kwargs) -> str:
         """
-        TODO: Override this method to define how a response is parsed.
-        :return an iterable containing each record in the response
+        Returns the URL to call. On first call uses the property `endpoint` of subclass. For subsequent
+        pages, `next_page_token` is used.
         """
-        yield {}
+
+        if not self.endpoint:
+            raise RuntimeError("Subclass is missing endpoint")
+
+        if next_page_token:
+            return next_page_token["next_url"]
+
+        return self.endpoint
 
 
-class Customers(ExactStream):
-    """
-    TODO: Change class name to match the table/data source this stream corresponds to.
-    """
-
-    # TODO: Fill in the primary key. Required. This is usually a unique field in the stream, like an ID or a timestamp.
-    primary_key = "customer_id"
-
-    def path(
-        self, stream_state: Mapping[str, Any] = None, stream_slice: Mapping[str, Any] = None, next_page_token: Mapping[str, Any] = None
-    ) -> str:
-        """
-        TODO: Override this method to define the path this stream corresponds to. E.g. if the url is https://example-api.com/v1/customers then this
-        should return "customers". Required.
-        """
-        return "customers"
+class Subscriptions(ExactStream):
+    primary_key = "EntryID"
+    endpoint = "subscription/Subscriptions"
 
 
-# Basic incremental stream
-class IncrementalExactStream(ExactStream, ABC):
-    """
-    TODO fill in details of this class to implement functionality related to incremental syncs for your connector.
-         if you do not need to implement incremental sync for any streams, remove this class.
-    """
+# # Basic incremental stream
+# class IncrementalExactStream(ExactStream, ABC):
+#     """
+#     TODO fill in details of this class to implement functionality related to incremental syncs for your connector.
+#          if you do not need to implement incremental sync for any streams, remove this class.
+#     """
 
-    # TODO: Fill in to checkpoint stream reads after N records. This prevents re-reading of data if the stream fails for any reason.
-    state_checkpoint_interval = None
+#     # TODO: Fill in to checkpoint stream reads after N records. This prevents re-reading of data if the stream fails for any reason.
+#     state_checkpoint_interval = None
 
-    @property
-    def cursor_field(self) -> str:
-        """
-        TODO
-        Override to return the cursor field used by this stream e.g: an API entity might always use created_at as the cursor field. This is
-        usually id or date based. This field's presence tells the framework this in an incremental stream. Required for incremental.
+#     @property
+#     def cursor_field(self) -> str:
+#         """
+#         TODO
+#         Override to return the cursor field used by this stream e.g: an API entity might always use created_at as the cursor field. This is
+#         usually id or date based. This field's presence tells the framework this in an incremental stream. Required for incremental.
 
-        :return str: The name of the cursor field.
-        """
-        return []
+#         :return str: The name of the cursor field.
+#         """
+#         return []
 
-    def get_updated_state(self, current_stream_state: MutableMapping[str, Any], latest_record: Mapping[str, Any]) -> Mapping[str, Any]:
-        """
-        Override to determine the latest state after reading the latest record. This typically compared the cursor_field from the latest record and
-        the current state and picks the 'most' recent cursor. This is how a stream's state is determined. Required for incremental.
-        """
-        return {}
+#     def get_updated_state(self, current_stream_state: MutableMapping[str, Any], latest_record: Mapping[str, Any]) -> Mapping[str, Any]:
+#         """
+#         Override to determine the latest state after reading the latest record. This typically compared the cursor_field from the latest record and
+#         the current state and picks the 'most' recent cursor. This is how a stream's state is determined. Required for incremental.
+#         """
+#         return {}
 
 
-class Employees(IncrementalExactStream):
-    """
-    TODO: Change class name to match the table/data source this stream corresponds to.
-    """
+# class Employees(IncrementalExactStream):
+#     """
+#     TODO: Change class name to match the table/data source this stream corresponds to.
+#     """
 
-    # TODO: Fill in the cursor_field. Required.
-    cursor_field = "start_date"
+#     # TODO: Fill in the cursor_field. Required.
+#     cursor_field = "start_date"
 
-    # TODO: Fill in the primary key. Required. This is usually a unique field in the stream, like an ID or a timestamp.
-    primary_key = "employee_id"
+#     # TODO: Fill in the primary key. Required. This is usually a unique field in the stream, like an ID or a timestamp.
+#     primary_key = "employee_id"
 
-    def path(self, **kwargs) -> str:
-        """
-        TODO: Override this method to define the path this stream corresponds to. E.g. if the url is https://example-api.com/v1/employees then this should
-        return "single". Required.
-        """
-        return "employees"
+#     def path(self, **kwargs) -> str:
+#         """
+#         TODO: Override this method to define the path this stream corresponds to. E.g. if the url is https://example-api.com/v1/employees then this should
+#         return "single". Required.
+#         """
+#         return "employees"
 
-    def stream_slices(self, stream_state: Mapping[str, Any] = None, **kwargs) -> Iterable[Optional[Mapping[str, any]]]:
-        """
-        TODO: Optionally override this method to define this stream's slices. If slicing is not needed, delete this method.
+#     def stream_slices(self, stream_state: Mapping[str, Any] = None, **kwargs) -> Iterable[Optional[Mapping[str, any]]]:
+#         """
+#         TODO: Optionally override this method to define this stream's slices. If slicing is not needed, delete this method.
 
-        Slices control when state is saved. Specifically, state is saved after a slice has been fully read.
-        This is useful if the API offers reads by groups or filters, and can be paired with the state object to make reads efficient. See the "concepts"
-        section of the docs for more information.
+#         Slices control when state is saved. Specifically, state is saved after a slice has been fully read.
+#         This is useful if the API offers reads by groups or filters, and can be paired with the state object to make reads efficient. See the "concepts"
+#         section of the docs for more information.
 
-        The function is called before reading any records in a stream. It returns an Iterable of dicts, each containing the
-        necessary data to craft a request for a slice. The stream state is usually referenced to determine what slices need to be created.
-        This means that data in a slice is usually closely related to a stream's cursor_field and stream_state.
+#         The function is called before reading any records in a stream. It returns an Iterable of dicts, each containing the
+#         necessary data to craft a request for a slice. The stream state is usually referenced to determine what slices need to be created.
+#         This means that data in a slice is usually closely related to a stream's cursor_field and stream_state.
 
-        An HTTP request is made for each returned slice. The same slice can be accessed in the path, request_params and request_header functions to help
-        craft that specific request.
+#         An HTTP request is made for each returned slice. The same slice can be accessed in the path, request_params and request_header functions to help
+#         craft that specific request.
 
-        For example, if https://example-api.com/v1/employees offers a date query params that returns data for that particular day, one way to implement
-        this would be to consult the stream state object for the last synced date, then return a slice containing each date from the last synced date
-        till now. The request_params function would then grab the date from the stream_slice and make it part of the request by injecting it into
-        the date query param.
-        """
-        raise NotImplementedError("Implement stream slices or delete this method!")
+#         For example, if https://example-api.com/v1/employees offers a date query params that returns data for that particular day, one way to implement
+#         this would be to consult the stream state object for the last synced date, then return a slice containing each date from the last synced date
+#         till now. The request_params function would then grab the date from the stream_slice and make it part of the request by injecting it into
+#         the date query param.
+#         """
+#         raise NotImplementedError("Implement stream slices or delete this method!")
 
 
 # Source
 class SourceExact(AbstractSource):
     def check_connection(self, logger, config) -> Tuple[bool, any]:
+        return True, None
+
+    def streams(self, config: Mapping[str, Any]) -> List[Stream]:
         token_endpoint = "https://start.exactonline.nl/api/oauth2/token"
 
-        try:
+        print(config)
+
+        access_token = config.get('access_token')
+        refresh_token = config.get('refresh_token')
+        if not access_token or not refresh_token:
             resp = requests.post(
                 token_endpoint,
                 data={
@@ -208,39 +173,31 @@ class SourceExact(AbstractSource):
                 error_description = response_json.get("error_description")
                 if error_description:
                     if 'The message expired at' in response_json.get("error_description"):
-                        return False, "The code is expired. Create a new code by initiating the OAuth flow manually."
+                        raise RuntimeError("Discovery failed: the code is expired. Create a new code by initiating the OAuth flow manually.")
                     if 'This message has already been processed' in response_json.get("error_description"):
-                        return False, "The code is already used. Create a new code by initiating the OAuth flow manually."
+                        raise RuntimeError("Discovery failed: the code is already used. Create a new code by initiating the OAuth flow manually.")
                     
-                    return False, response_json.get("error_description")
+                    raise RuntimeError(f"Discovery failed: {response_json.get('error_description')}")
 
-                return False, f"Failed to retrieve token\n{response_json}"
+                raise RuntimeError(f"Discovery failed: failed to retrieve token\n{response_json}")
 
-            auth = SingleRefreshOauth2Authenticator(
-                token_refresh_endpoint=token_endpoint,
-                client_id=config["client_id"],
-                client_secret=config["client_secret"],
-                refresh_token=response_json["refresh_token"],
-                # Hardcoded: access tokens are valid for 10 minutes (from the documentation). Even the first one
-                # is subject to rate limit.
-                token_expiry_date=pendulum.now().add(minutes=10)
-            )
-            auth.access_token = response_json["access_token"]
+            access_token = response_json["access_token"]
+            refresh_token = response_json["refresh_token"]
 
-            return True, None
-        except Exception as e:
-            return False, e
+        auth = SingleRefreshOauth2Authenticator(
+            token_refresh_endpoint=token_endpoint,
+            client_id=config["client_id"],
+            client_secret=config["client_secret"],
+            refresh_token=refresh_token,
+            # Hardcoded: access tokens are valid for 10 minutes (from the documentation). Even the first one
+            # is subject to rate limit.
+            token_expiry_date=pendulum.now().add(minutes=10)
+        )
+        auth.access_token = access_token
 
-    def streams(self, config: Mapping[str, Any]) -> List[Stream]:
-        """
-        TODO: Replace the streams below with your own streams.
-
-        :param config: A Mapping of the user input configuration as defined in the connector spec.
-        """
-        # TODO remove the authenticator if not required.
-        auth = SingleRefreshOauth2Authenticator(token="api_key")  # Oauth2Authenticator is also available if you need oauth support
-        return [Customers(authenticator=auth), Employees(authenticator=auth)]
-
+        return [
+            Subscriptions(authenticator=auth),
+        ]
 
 class SingleRefreshOauth2Authenticator(Oauth2Authenticator):
     def refresh_access_token(self) -> Tuple[str, int]:
