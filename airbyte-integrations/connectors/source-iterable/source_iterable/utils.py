33e05c4a18fa3228e4a1f84aba150b6e6aa71deb
Sergey Chvalyuk <grubberr@gmail.com>
2022-11-30 11:56:19 +0000

    dateutil_parse added
    Signed-off-by: Sergey Chvalyuk <grubberr@gmail.com>

---
+++ airbyte-integrations/connectors/source-iterable/source_iterable/utils.py
@@ -0,0 +1,20 @@
+#
+# Copyright (c) 2022 Airbyte, Inc., all rights reserved.
+#
+
+import dateutil
+import pendulum
+
+
+def dateutil_parse(text):
+    dt = dateutil.parser.parse(text)
+    return pendulum.datetime(
+        dt.year,
+        dt.month,
+        dt.day,
+        dt.hour,
+        dt.minute,
+        dt.second,
+        dt.microsecond,
+        tz=dt.tzinfo or pendulum.tz.UTC,
+    )
