2aea57301de789731dce4eba71016f1d137b38a0
yatsukbogdan <yatsukbogdan@gmail.com>
2022-11-30 17:06:09 +0200

    Removes StatusCellControl component and puts all logic inside StatusCell; Adds feature flag isSchemaChangesFeatureEnabled

--- airbyte-webapp/src/components/EntityTable/components/StatusCell.tsx
+++ airbyte-webapp/src/components/EntityTable/components/StatusCell.tsx
@@ -1,10 +1,16 @@
 import React from "react";
+import { FormattedMessage } from "react-intl";
+import { useAsyncFn } from "react-use";
+import styled from "styled-components";
+
+import { Button } from "components/ui/Button";
+import { Switch } from "components/ui/Switch";
 
 import { SchemaChange } from "core/request/AirbyteClient";
+import { useEnableConnection } from "hooks/services/useConnectionHook";
 
 import { ChangesStatusIcon } from "./ChangesStatusIcon";
 import styles from "./StatusCell.module.scss";
-import { StatusCellControl } from "./StatusCellControl";
 
 interface IProps {
   allowSync?: boolean;
@@ -16,18 +22,59 @@ interface IProps {
   schemaChange: SchemaChange;
 }
 
+const ProgressMessage = styled.div`
+  padding: 7px 0;
+`;
+
 export const StatusCell: React.FC<IProps> = ({ enabled, isManual, id, isSyncing, onSync, allowSync, schemaChange }) => {
+  const { mutateAsync: enableConnection, isLoading } = useEnableConnection();
+  const isSchemaChangesFeatureEnabled = process.env.REACT_APP_AUTO_DETECT_SCHEMA_CHANGES_FEATURE_ENABLED === "true";
+  const [{ loading }, OnLaunch] = useAsyncFn(
+    async (event: React.SyntheticEvent) => {
+      event.stopPropagation();
+      onSync(id);
+    },
+    [id]
+  );
+  let ControlComponent;
+
+  if (!isManual) {
+    const onSwitchChange = async (event: React.SyntheticEvent) => {
+      event.stopPropagation();
+      await enableConnection({
+        connectionId: id,
+        enable: !enabled,
+      });
+    };
+
+    ControlComponent = (
+      // this is so we can stop event propagation so the row doesn't receive the click and redirect
+      // eslint-disable-next-line jsx-a11y/no-static-element-interactions
+      <div
+        onClick={(event: React.SyntheticEvent) => event.stopPropagation()}
+        onKeyPress={(event: React.SyntheticEvent) => event.stopPropagation()}
+      >
+        <Switch checked={enabled} onChange={onSwitchChange} disabled={!allowSync} loading={isLoading} />
+      </div>
+    );
+  } else if (isSyncing) {
+    ControlComponent = (
+      <ProgressMessage>
+        <FormattedMessage id="tables.progress" />
+      </ProgressMessage>
+    );
+  } else {
+    ControlComponent = (
+      <Button size="xs" onClick={OnLaunch} isLoading={loading} disabled={!allowSync || !enabled}>
+        <FormattedMessage id="tables.launch" />
+      </Button>
+    );
+  }
+
   return (
     <div className={styles.container}>
-      <StatusCellControl
-        enabled={enabled}
-        id={id}
-        isSyncing={isSyncing}
-        isManual={isManual}
-        onSync={onSync}
-        allowSync={allowSync}
-      />
-      <ChangesStatusIcon schemaChange={schemaChange} />
+      {ControlComponent}
+      {isSchemaChangesFeatureEnabled && <ChangesStatusIcon schemaChange={schemaChange} />}
     </div>
   );
 };
