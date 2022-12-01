a2308a1431233e1eb4077526efb73e59259bd5a8
nataly <nataly@airbyte.io>
2022-12-01 13:11:51 -0500

    Adds basic story file.

---
+++ airbyte-webapp/src/components/ui/ListBox/index.stories.tsx
@@ -0,0 +1,33 @@
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+
+import { ListBox } from "./ListBox";
+
+const listOptions = [
+  {
+    label: "one",
+    value: "value",
+  },
+  {
+    label: "two",
+    value: "value",
+  },
+  {
+    label: "three",
+    value: "value",
+  },
+];
+
+export default {
+  title: "Ui/ListBox",
+  component: ListBox,
+  argTypes: {
+    options: listOptions,
+  },
+} as ComponentMeta<typeof ListBox>;
+
+const Template: ComponentStory<typeof ListBox> = (args) => <ListBox {...args} />;
+
+export const Primary = Template.bind({});
+Primary.args = {
+  options: listOptions,
+};
