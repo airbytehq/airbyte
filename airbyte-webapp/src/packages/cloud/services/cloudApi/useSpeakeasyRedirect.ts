8a9c95553ec47390ef8096b9d813e73f13510db7
josephkmh <joseph@airbyte.io>
2022-12-01 19:00:14 +0000

    create hook for calling cloud api endpoint

---
+++ airbyte-webapp/src/packages/cloud/services/cloudApi/useSpeakeasyRedirect.ts
@@ -0,0 +1,13 @@
+import { getSpeakeasyCallbackUrl } from "packages/cloud/lib/domain/cloudApi";
+import { useSuspenseQuery } from "services/connector/useSuspenseQuery";
+import { useDefaultRequestMiddlewares } from "services/useDefaultRequestMiddlewares";
+
+const SPEAKEASY_QUERY_KEY = "speakeasy-redirect";
+
+export const useSpeakeasyRedirect = () => {
+  const config = { apiUrl: "/cloud_api" };
+  const middlewares = useDefaultRequestMiddlewares();
+  const requestOptions = { config, middlewares };
+
+  return useSuspenseQuery([SPEAKEASY_QUERY_KEY], () => getSpeakeasyCallbackUrl(requestOptions));
+};
