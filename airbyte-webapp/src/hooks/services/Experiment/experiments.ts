121fff016d2f8b65c43e8eb1f72cbc7cd6acb2a0
Edmundo Ruiz Ghanem <edmundo@airbyte.io>
2022-12-02 15:53:29 -0500

    Add auto-detect schema changes feature flag

--- airbyte-webapp/src/hooks/services/Experiment/experiments.ts
+++ airbyte-webapp/src/hooks/services/Experiment/experiments.ts
@@ -23,4 +23,5 @@ export interface Experiments {
   "authPage.oauth.position": "top" | "bottom";
   "connection.onboarding.sources": string;
   "connection.onboarding.destinations": string;
+  "connection.autoDetectSchemaChanges": boolean;
 }
