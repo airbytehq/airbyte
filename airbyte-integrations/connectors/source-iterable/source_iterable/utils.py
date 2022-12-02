8acad7f339b97c2efe3879d7cc819f7f82c941ea
Sergey Chvalyuk <grubberr@gmail.com>
2022-12-02 09:18:34 +0000

    docs added for dateutil_parse
    Signed-off-by: Sergey Chvalyuk <grubberr@gmail.com>

--- airbyte-integrations/connectors/source-iterable/source_iterable/utils.py
+++ airbyte-integrations/connectors/source-iterable/source_iterable/utils.py
@@ -7,6 +7,10 @@ import pendulum
 
 
 def dateutil_parse(text):
+    """
+    The custom function `dateutil_parse` replace `pendulum.parse(text, strict=False)` to avoid memory leak.
+    More details https://github.com/airbytehq/airbyte/pull/19913
+    """
     dt = dateutil.parser.parse(text)
     return pendulum.datetime(
         dt.year,
