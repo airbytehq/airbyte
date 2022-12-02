b7d26812a8eafc7dc9e0cf11131ed14cc449d6df
Kris Sikora <kris@calixa.io>
2022-12-02 15:13:15 -0800

    Destination:PubSub Add Batching and Ordering configuration (#15705)
    * Add Batching and Ordering configuration
    
    * Add changelog
    
    * fix config for tests
    
    * format files
    
    * auto-bump connector version
    
    Co-authored-by: marcosmarxm <marcosmarxm@gmail.com>
    Co-authored-by: Marcos Marx <marcosmarxm@users.noreply.github.com>
    Co-authored-by: Octavia Squidington III <octavia-squidington-iii@users.noreply.github.com>

--- airbyte-integrations/connectors/destination-pubsub/src/test/java/io/airbyte/integration/destination/pubsub/PubsubConsumerTest.java
+++ airbyte-integrations/connectors/destination-pubsub/src/test/java/io/airbyte/integration/destination/pubsub/PubsubConsumerTest.java
@@ -4,9 +4,9 @@
 
 package io.airbyte.integration.destination.pubsub;
 
-import com.fasterxml.jackson.databind.JsonNode;
 import io.airbyte.integrations.base.FailureTrackingAirbyteMessageConsumer;
 import io.airbyte.integrations.destination.pubsub.PubsubConsumer;
+import io.airbyte.integrations.destination.pubsub.PubsubDestinationConfig;
 import io.airbyte.integrations.standardtest.destination.PerStreamStateMessageTest;
 import io.airbyte.protocol.models.AirbyteMessage;
 import io.airbyte.protocol.models.ConfiguredAirbyteCatalog;
@@ -25,7 +25,7 @@ public class PubsubConsumerTest extends PerStreamStateMessageTest {
   private PubsubConsumer consumer;
 
   @Mock
-  private JsonNode config;
+  private PubsubDestinationConfig config;
   @Mock
   private ConfiguredAirbyteCatalog catalog;
 
