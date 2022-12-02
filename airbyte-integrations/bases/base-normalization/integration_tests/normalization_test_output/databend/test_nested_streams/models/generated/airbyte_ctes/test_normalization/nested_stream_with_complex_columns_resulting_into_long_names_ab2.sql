de613684bbadb61d41b38ce0f5bef1f0b52270c8
hantmac <hantmac@outlook.com>
2022-12-02 15:29:28 +0800

    update

---
+++ airbyte-integrations/bases/base-normalization/integration_tests/normalization_test_output/databend/test_nested_streams/models/generated/airbyte_ctes/test_normalization/nested_stream_with_complex_columns_resulting_into_long_names_ab2.sql
@@ -0,0 +1,19 @@
+{{ config(
+    unique_key = '_airbyte_ab_id',
+    schema = "_airbyte_test_normalization",
+    tags = [ "top-level-intermediate" ]
+) }}
+-- SQL model to cast each column to its adequate SQL type converted from the JSON schema type
+-- depends_on: {{ ref('nested_stream_with_complex_columns_resulting_into_long_names_ab1') }}
+select
+    cast(id as {{ dbt_utils.type_string() }}) as id,
+    cast(date as {{ dbt_utils.type_string() }}) as date,
+    cast(partition as {{ type_json() }}) as partition,
+    _airbyte_ab_id,
+    _airbyte_emitted_at,
+    {{ current_timestamp() }} as _airbyte_normalized_at
+from {{ ref('nested_stream_with_complex_columns_resulting_into_long_names_ab1') }}
+-- nested_stream_with_complex_columns_resulting_into_long_names
+where 1 = 1
+{{ incremental_clause('_airbyte_emitted_at', this) }}
+
