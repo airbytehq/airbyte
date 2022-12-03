a05adc2b686eac72ba3ca046c11469883c5a1f2b
Sergey Chvalyuk <grubberr@gmail.com>
2022-12-03 10:21:18 +0000

    hard-code test_ads_insights_breakdowns
    Signed-off-by: Sergey Chvalyuk <grubberr@gmail.com>

--- airbyte-integrations/connectors/source-facebook-marketing/unit_tests/test_streams.py
+++ airbyte-integrations/connectors/source-facebook-marketing/unit_tests/test_streams.py
@@ -64,12 +64,23 @@ def test_parse_call_rate_header():
     assert MyFacebookAdsApi._parse_call_rate_header(headers) == (1, duration(minutes=1))
 
 
-def test_ads_insights_breakdowns():
+@pytest.mark.parametrize(
+    "class_name, breakdowns, action_breakdowns",
+    [
+        [AdsInsights, [], ["action_type", "action_target_id", "action_destination"]],
+        [AdsInsightsActionType, [], ["action_type"]],
+        [AdsInsightsAgeAndGender, ["age", "gender"], ["action_type", "action_target_id", "action_destination"]],
+        [AdsInsightsCountry, ["country"], ["action_type", "action_target_id", "action_destination"]],
+        [AdsInsightsDma, ["dma"], ["action_type", "action_target_id", "action_destination"]],
+        [AdsInsightsPlatformAndDevice, ["publisher_platform", "platform_position", "impression_device"], ["action_type"]],
+        [AdsInsightsRegion, ["region"], ["action_type", "action_target_id", "action_destination"]],
+    ],
+)
+def test_ads_insights_breakdowns(class_name, breakdowns, action_breakdowns):
     kwargs = {"api": None, "start_date": pendulum.now(), "end_date": pendulum.now(), "insights_lookback_window": 1}
-    for stream_class in [AdsInsights, AdsInsightsActionType, AdsInsightsAgeAndGender, AdsInsightsCountry, AdsInsightsDma, AdsInsightsPlatformAndDevice, AdsInsightsRegion]:
-        stream = stream_class(**kwargs)
-        assert stream.breakdowns == stream_class.breakdowns
-        assert stream.action_breakdowns == stream_class.action_breakdowns
+    stream = class_name(**kwargs)
+    assert stream.breakdowns == breakdowns
+    assert stream.action_breakdowns == action_breakdowns
 
 
 def test_custom_ads_insights_breakdowns():
