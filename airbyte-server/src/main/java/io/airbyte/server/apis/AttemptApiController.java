212dd8695a8d1d04fcd413942d3e438f1fe690ff
Benoit Moriceau <benoit@airbyte.io>
2022-12-05 14:21:53 -0800

    Tmp

--- airbyte-server/src/main/java/io/airbyte/server/apis/AttemptApiController.java
+++ airbyte-server/src/main/java/io/airbyte/server/apis/AttemptApiController.java
@@ -11,14 +11,9 @@ import io.airbyte.server.handlers.AttemptHandler;
 import io.micronaut.http.MediaType;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Post;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiResponse;
-import io.swagger.annotations.ApiResponses;
 import lombok.extern.slf4j.Slf4j;
 
 @Controller("/v1/attempt/")
-@Api(description = "the Attempt API")
 @Slf4j
 public class AttemptApiController {
 
@@ -31,13 +26,6 @@ public class AttemptApiController {
   @Post(uri = "/save_stats",
         produces = MediaType.APPLICATION_JSON,
         consumes = MediaType.APPLICATION_JSON)
-  @ApiOperation(value = "For worker to set sync stats of a running attempt.",
-                notes = "",
-                tags = {"attempt", "internal"})
-  @ApiResponses(value = {
-    @ApiResponse(code = 200,
-                 message = "Successful Operation",
-                 response = InternalOperationResult.class)})
   public InternalOperationResult saveStats(final SaveStatsRequestBody saveStatsRequestBody) {
     log.error("Tessssttttttttt");
 
@@ -47,13 +35,6 @@ public class AttemptApiController {
   @Post(uri = "/set_workflow_in_attempt",
         produces = MediaType.APPLICATION_JSON,
         consumes = MediaType.APPLICATION_JSON)
-  @ApiOperation(value = "For worker to register the workflow id in attempt.",
-                notes = "",
-                tags = {"attempt", "internal"})
-  @ApiResponses(value = {
-    @ApiResponse(code = 200,
-                 message = "Successful Operation",
-                 response = InternalOperationResult.class)})
   public InternalOperationResult setWorkflowInAttempt(final SetWorkflowInAttemptRequestBody requestBody) {
     return ApiHelper.execute(() -> attemptHandler.setWorkflowInAttempt(requestBody));
   }
