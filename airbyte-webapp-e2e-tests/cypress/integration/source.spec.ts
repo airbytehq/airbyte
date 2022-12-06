7ba70ad3c91d776e7bd38698c70369a3a96e92ca
Sofiia Zaitseva <108751383+SofiiaZaitseva@users.noreply.github.com>
2022-12-06 22:29:42 +0200

    enable test due to bug fixing

--- airbyte-webapp-e2e-tests/cypress/integration/source.spec.ts
+++ airbyte-webapp-e2e-tests/cypress/integration/source.spec.ts
@@ -74,8 +74,7 @@ describe("Unsaved changes modal", () => {
       .contains("There are unsaved changes. Are you sure you want to discard your changes?");
   });
 
-  //BUG - https://github.com/airbytehq/airbyte/issues/18246
-  it.skip("Check leaving Source page after failing testing", () => {
+  it("Check leaving Source page after failing testing", () => {
     cy.intercept("/api/v1/scheduler/sources/check_connection").as("checkSourceUpdateConnection");
 
     goToSourcePage();
