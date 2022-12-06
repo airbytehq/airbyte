5116d11be5f42bdd5dfb96e57e9ad00795190a1f
Alexandre Girard <alexandre@airbyte.io>
2022-12-06 11:44:58 -0800

    update

--- airbyte-webapp/src/core/domain/connector/constants.ts
+++ airbyte-webapp/src/core/domain/connector/constants.ts
@@ -13,5 +13,4 @@ export const DEV_IMAGE_TAG = "dev";
  * @param {string} workspaceId The workspace Id
  * @returns {array} List of connectorIds that should be filtered out
  */
-export const getExcludedConnectorIds = (_workspaceId?: string): string[] =>
-  [];
+export const getExcludedConnectorIds = (_workspaceId?: string): string[] => [];
