96581517ad1133f95a5e1a6781a0c76232601d12
Sergey Chvalyuk <grubberr@gmail.com>
2022-12-07 16:15:37 +0000

    fix test_offset_increment_paginator_strategy_rises
    Signed-off-by: Sergey Chvalyuk <grubberr@gmail.com>

--- airbyte-cdk/python/unit_tests/sources/declarative/requesters/paginators/test_offset_increment.py
+++ airbyte-cdk/python/unit_tests/sources/declarative/requesters/paginators/test_offset_increment.py
@@ -39,8 +39,8 @@ def test_offset_increment_paginator_strategy(test_name, page_size, expected_next
 
 def test_offset_increment_paginator_strategy_rises():
     paginator_strategy = OffsetIncrement(
-        page_size=InterpolatedString(string="{{ options['page_size'] }}", options={"page_size": "invalid value"}),
-        options={},
+        page_size="{{ options['page_size'] }}",
+        options={"page_size": "invalid value"},
         config={}
     )
     with pytest.raises(Exception) as exc:
