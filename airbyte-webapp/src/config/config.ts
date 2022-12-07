5fcc0f59cf1c32da48fed5a91829f42e3b1723ef
josephkmh <joseph@airbyte.io>
2022-12-07 12:15:32 +0100

    merge oss config providers in static object

--- airbyte-webapp/src/config/config.ts
+++ airbyte-webapp/src/config/config.ts
@@ -2,22 +2,25 @@ import { Config } from "./types";
 
 export const newStaticConfig: Config = {
   segment: {
-    token: process.env.REACT_APP_SEGMENT_TOKEN || window.SEGMENT_TOKEN,
+    token: process.env.REACT_APP_SEGMENT_TOKEN ?? window.SEGMENT_TOKEN,
     enabled: window.TRACKING_STRATEGY === "segment",
   },
-  apiUrl: "string",
+  apiUrl:
+    window.API_URL ??
+    process.env.REACT_APP_API_URL ??
+    `${window.location.protocol}//${window.location.hostname}:8001/api`,
   connectorBuilderApiUrl: "string",
   oauthRedirectUrl: "string",
-  healthCheckInterval: 1,
-  version: "string",
+  healthCheckInterval: 20000,
+  version: window.AIRBYTE_VERSION ?? "dev",
   integrationUrl: "string",
   launchDarkly: "string",
   // Cloud-only values:
-  cloudApiUrl: process.env.REACT_APP_CLOUD_API_URL || window.CLOUD_API_URL,
+  cloudApiUrl: process.env.REACT_APP_CLOUD_API_URL ?? window.CLOUD_API_URL,
   firebase: {
-    apiKey: process.env.REACT_APP_FIREBASE_API_KEY || window.FIREBASE_API_KEY,
-    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN || window.FIREBASE_AUTH_DOMAIN,
-    authEmulatorHost: process.env.REACT_APP_FIREBASE_AUTH_EMULATOR_HOST || window.FIREBASE_AUTH_EMULATOR_HOST,
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY ?? window.FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN ?? window.FIREBASE_AUTH_DOMAIN,
+    authEmulatorHost: process.env.REACT_APP_FIREBASE_AUTH_EMULATOR_HOST ?? window.FIREBASE_AUTH_EMULATOR_HOST,
   },
   intercom: {
     appId: process.env.REACT_APP_INTERCOM_APP_ID,
@@ -25,3 +28,41 @@ export const newStaticConfig: Config = {
 };
 
 console.log(newStaticConfig);
+
+// OSS config providers (from top to bottom, each level overwriting the previous)
+//
+// const defaultConfig = {
+//   segment: { enabled: true, token: "" },
+//   healthCheckInterval: 20000,
+//   version: "dev",
+//   apiUrl: `${window.location.protocol}//${window.location.hostname}:8001/api`,
+//   connectorBuilderApiUrl: `${window.location.protocol}//${window.location.hostname}:8003`,
+//   integrationUrl: "/docs",
+//   oauthRedirectUrl: `${window.location.protocol}//${window.location.host}`,
+// };
+
+// const envConfigProvider: ConfigProvider = async () => {
+//   return {
+//     apiUrl: process.env.REACT_APP_API_URL,
+//     integrationUrl: process.env.REACT_APP_INTEGRATION_DOCS_URLS,
+//     segment: {
+//       token: process.env.REACT_APP_SEGMENT_TOKEN,
+//     },
+//   };
+// };
+
+// const windowConfigProvider: ConfigProvider = async () => {
+//   return {
+//     segment: {
+//       enabled: isDefined(window.TRACKING_STRATEGY) ? window.TRACKING_STRATEGY === "segment" : undefined,
+//       token: window.SEGMENT_TOKEN,
+//     },
+//     apiUrl: window.API_URL,
+//     connectorBuilderApiUrl: window.CONNECTOR_BUILDER_API_URL,
+//     version: window.AIRBYTE_VERSION,
+//     // cloud only start
+//     // TODO: remove when infra team supports proper webapp building
+//     cloud: window.CLOUD === "true",
+//     // cloud only end
+//   };
+// };
