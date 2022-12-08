606e0b45c946048972f4cc3cea890e7fc89ec98c
Alexandre Girard <alexandre@airbyte.io>
2022-12-07 21:34:09 -0800

    Revert "Don't import the connector manifest schema"
    This reverts commit 84589dcf074ea02f9d5112506fc3b89cd76f9d72.

--- airbyte-connector-builder-server/connector_builder/generated/models/stream_read_request_body.py
+++ airbyte-connector-builder-server/connector_builder/generated/models/stream_read_request_body.py
@@ -6,7 +6,8 @@ from datetime import date, datetime  # noqa: F401
 import re  # noqa: F401
 from typing import Any, Dict, List, Optional  # noqa: F401
 
-from pydantic import AnyUrl, BaseModel, EmailStr, Field, validator  # noqa: F401
+from pydantic import AnyUrl, BaseModel, EmailStr, validator  # noqa: F401
+from connector_builder.generated.models.connector_manifest import ConnectorManifest
 
 
 class StreamReadRequestBody(BaseModel):
@@ -22,9 +23,9 @@ class StreamReadRequestBody(BaseModel):
         state: The state of this StreamReadRequestBody [Optional].
     """
 
-    manifest: Dict[str, Any] = Field(alias="manifest")
-    stream: str = Field(alias="stream")
-    config: Dict[str, Any] = Field(alias="config")
-    state: Optional[Dict[str, Any]] = Field(alias="state", default=None)
+    manifest: ConnectorManifest
+    stream: str
+    config: Dict[str, Any]
+    state: Optional[Dict[str, Any]] = None
 
 StreamReadRequestBody.update_forward_refs()
