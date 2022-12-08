402d9d09457e4d5be8b24d405e69060993d796c3
Alexandre Girard <alexandre@airbyte.io>
2022-12-08 13:56:52 -0800

    Don't import the connector manifest schema (#20218)
    * Don't import the connector manifest schema
    
    * Revert "Don't import the connector manifest schema"
    
    This reverts commit 84589dcf074ea02f9d5112506fc3b89cd76f9d72.
    
    * revert
    
    * revert
    
    * reset

--- airbyte-connector-builder-server/connector_builder/generated/models/exponential_backoff_strategy.py
+++ airbyte-connector-builder-server/connector_builder/generated/models/exponential_backoff_strategy.py
@@ -6,7 +6,7 @@ from datetime import date, datetime  # noqa: F401
 import re  # noqa: F401
 from typing import Any, Dict, List, Optional  # noqa: F401
 
-from pydantic import AnyUrl, BaseModel, EmailStr, validator  # noqa: F401
+from pydantic import AnyUrl, BaseModel, EmailStr, Field, validator  # noqa: F401
 from connector_builder.generated.models.any_of_interpolated_stringnumberstring import AnyOfInterpolatedStringnumberstring
 from connector_builder.generated.models.exponential_backoff_strategy_all_of import ExponentialBackoffStrategyAllOf
 
@@ -22,7 +22,7 @@ class ExponentialBackoffStrategy(BaseModel):
         factor: The factor of this ExponentialBackoffStrategy [Optional].
     """
 
-    config: Dict[str, Any]
-    factor: Optional[AnyOfInterpolatedStringnumberstring] = None
+    config: Dict[str, Any] = Field(alias="config")
+    factor: Optional[AnyOfInterpolatedStringnumberstring] = Field(alias="factor", default=None)
 
 ExponentialBackoffStrategy.update_forward_refs()
