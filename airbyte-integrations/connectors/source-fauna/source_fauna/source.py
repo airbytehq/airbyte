25f45ccc5230402921e37f6df5187ddbb3817819
Neil Macneale V <neil.macneale.v@gmail.com>
2022-12-08 15:52:00 -0800

    Cleanup conditional, and allow an index without terms as well as an index with no terms

--- airbyte-integrations/connectors/source-fauna/source_fauna/source.py
+++ airbyte-integrations/connectors/source-fauna/source_fauna/source.py
@@ -239,12 +239,9 @@ class SourceFauna(Source):
                     if (
                         type(source) is Ref
                         and source.collection() == Ref("collections")
-                        # If either of these are missing, the index has no terms or values
-                        and "terms" in index
-                        and "values" in index
                         # Index must have 2 values and no terms
-                        and len(index["values"]) == 2
-                        and len(index["terms"]) == 0
+                        and ("values" in index and len(index["values"]) == 2)
+                        and (("terms" in index and len(index["terms"]) == 0) or "terms" not in index)
                         # Index values must be ts and ref
                         and index["values"][0] == {"field": "ts"}
                         and index["values"][1] == {"field": "ref"}
