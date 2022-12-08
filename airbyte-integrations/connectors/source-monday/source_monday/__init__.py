6e2711d9863eb272e0f9bf8bd8699c61d1db91c7
Roman Yermilov <roman.yermilov@globallogic.com>
2022-12-08 20:53:58 +0400

    Source Monday: use default pagination strategy instead of custom component

--- airbyte-integrations/connectors/source-monday/source_monday/__init__.py
+++ airbyte-integrations/connectors/source-monday/source_monday/__init__.py
@@ -6,7 +6,6 @@
 # from .source import SourceMonday
 from .source_lc import SourceMonday
 from .graphql_request_options_provider import GraphQLRequestOptionsProvider
-from .pagination_workaround import PageIncrementWorkaround
 from .dpath_string_extractor import DpathStringExtractor
 
-__all__ = ["SourceMonday", "GraphQLRequestOptionsProvider", "PageIncrementWorkaround", "DpathStringExtractor"]
+__all__ = ["SourceMonday", "GraphQLRequestOptionsProvider", "DpathStringExtractor"]
