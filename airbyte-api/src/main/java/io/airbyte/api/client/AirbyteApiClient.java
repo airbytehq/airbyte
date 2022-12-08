b486f6d2174cf603ede953f16b3b1598583af5d7
Pedro Lopez <pedroslopez@me.com>
2022-12-08 15:27:54 -0400

    print stacktrace for debugging

--- airbyte-api/src/main/java/io/airbyte/api/client/AirbyteApiClient.java
+++ airbyte-api/src/main/java/io/airbyte/api/client/AirbyteApiClient.java
@@ -182,6 +182,7 @@ public class AirbyteApiClient {
         keepTrying = false;
       } catch (final Exception e) {
         LOGGER.info("Attempt {} to {} error: {}", currRetries, desc, e);
+        LOGGER.error("Error", e);
         currRetries++;
 
         // Sleep anywhere from 1 to jitterMaxIntervalSecs seconds.
