556e7d3f41a27f6babd065d73237c6631ad3b477
Benoit Moriceau <benoit@airbyte.io>
2022-12-09 09:02:38 -0800

    Format

--- airbyte-server/src/main/java/io/airbyte/server/apis/ConnectionApiController.java
+++ airbyte-server/src/main/java/io/airbyte/server/apis/ConnectionApiController.java
@@ -17,7 +17,6 @@ import io.airbyte.server.handlers.ConnectionsHandler;
 import io.airbyte.server.handlers.OperationsHandler;
 import io.airbyte.server.handlers.SchedulerHandler;
 import io.micronaut.context.annotation.Context;
-import io.micronaut.http.MediaType;
 import io.micronaut.http.annotation.Body;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Post;
