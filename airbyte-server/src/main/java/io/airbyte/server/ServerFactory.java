d35f939b86d7287c0995705e421baae77c2d5715
Cole Snodgrass <cole@airbyte.io>
2022-12-09 13:16:14 -0800

    convert StateApiController to Micronaut (#20329)
    * convert to micronaut
    
    * nginx updates
    
    * format

--- airbyte-server/src/main/java/io/airbyte/server/ServerFactory.java
+++ airbyte-server/src/main/java/io/airbyte/server/ServerFactory.java
@@ -25,7 +25,6 @@ import io.airbyte.server.apis.SourceApiController;
 import io.airbyte.server.apis.SourceDefinitionApiController;
 import io.airbyte.server.apis.SourceDefinitionSpecificationApiController;
 import io.airbyte.server.apis.SourceOauthApiController;
-import io.airbyte.server.apis.StateApiController;
 import io.airbyte.server.apis.WebBackendApiController;
 import io.airbyte.server.apis.WorkspaceApiController;
 import io.airbyte.server.apis.binders.DestinationDefinitionSpecificationApiBinder;
@@ -38,7 +37,6 @@ import io.airbyte.server.apis.binders.SourceApiBinder;
 import io.airbyte.server.apis.binders.SourceDefinitionApiBinder;
 import io.airbyte.server.apis.binders.SourceDefinitionSpecificationApiBinder;
 import io.airbyte.server.apis.binders.SourceOauthApiBinder;
-import io.airbyte.server.apis.binders.StateApiBinder;
 import io.airbyte.server.apis.binders.WebBackendApiBinder;
 import io.airbyte.server.apis.binders.WorkspaceApiBinder;
 import io.airbyte.server.apis.factories.DestinationDefinitionSpecificationApiFactory;
@@ -51,7 +49,6 @@ import io.airbyte.server.apis.factories.SourceApiFactory;
 import io.airbyte.server.apis.factories.SourceDefinitionApiFactory;
 import io.airbyte.server.apis.factories.SourceDefinitionSpecificationApiFactory;
 import io.airbyte.server.apis.factories.SourceOauthApiFactory;
-import io.airbyte.server.apis.factories.StateApiFactory;
 import io.airbyte.server.apis.factories.WebBackendApiFactory;
 import io.airbyte.server.apis.factories.WorkspaceApiFactory;
 import io.airbyte.server.handlers.AttemptHandler;
@@ -66,7 +63,6 @@ import io.airbyte.server.handlers.OperationsHandler;
 import io.airbyte.server.handlers.SchedulerHandler;
 import io.airbyte.server.handlers.SourceDefinitionsHandler;
 import io.airbyte.server.handlers.SourceHandler;
-import io.airbyte.server.handlers.StateHandler;
 import io.airbyte.server.handlers.WebBackendCheckUpdatesHandler;
 import io.airbyte.server.handlers.WebBackendConnectionsHandler;
 import io.airbyte.server.handlers.WebBackendGeographiesHandler;
@@ -110,7 +106,6 @@ public interface ServerFactory {
                         final SchedulerHandler schedulerHandler,
                         final SourceHandler sourceHandler,
                         final SourceDefinitionsHandler sourceDefinitionsHandler,
-                        final StateHandler stateHandler,
                         final WorkspacesHandler workspacesHandler,
                         final WebBackendConnectionsHandler webBackendConnectionsHandler,
                         final WebBackendGeographiesHandler webBackendGeographiesHandler,
@@ -147,7 +142,6 @@ public interface ServerFactory {
                                  final SchedulerHandler schedulerHandler,
                                  final SourceHandler sourceHandler,
                                  final SourceDefinitionsHandler sourceDefinitionsHandler,
-                                 final StateHandler stateHandler,
                                  final WorkspacesHandler workspacesHandler,
                                  final WebBackendConnectionsHandler webBackendConnectionsHandler,
                                  final WebBackendGeographiesHandler webBackendGeographiesHandler,
@@ -174,8 +168,6 @@ public interface ServerFactory {
 
       SourceDefinitionSpecificationApiFactory.setValues(schedulerHandler);
 
-      StateApiFactory.setValues(stateHandler);
-
       WebBackendApiFactory.setValues(webBackendConnectionsHandler, webBackendGeographiesHandler, webBackendCheckUpdatesHandler);
 
       WorkspaceApiFactory.setValues(workspacesHandler);
@@ -194,7 +186,6 @@ public interface ServerFactory {
           SourceDefinitionApiController.class,
           SourceDefinitionSpecificationApiController.class,
           SourceOauthApiController.class,
-          StateApiController.class,
           WebBackendApiController.class,
           WorkspaceApiController.class);
 
@@ -209,7 +200,6 @@ public interface ServerFactory {
           new SourceDefinitionApiBinder(),
           new SourceDefinitionSpecificationApiBinder(),
           new SourceOauthApiBinder(),
-          new StateApiBinder(),
           new WebBackendApiBinder(),
           new WorkspaceApiBinder());
 
