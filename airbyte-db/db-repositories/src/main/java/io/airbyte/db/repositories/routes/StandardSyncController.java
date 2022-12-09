dba5c5c1caafb51d1a220a3ba0c3f76cc9c5f979
jonsspaghetti <jon@airbyte.io>
2022-12-09 16:19:03 -0600

    Add some deps, endpoints, model translation

---
+++ airbyte-db/db-repositories/src/main/java/io/airbyte/db/repositories/routes/StandardSyncController.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright (c) 2022 Airbyte, Inc., all rights reserved.
+ */
+
+package io.airbyte.db.repositories.routes;
+
+import io.airbyte.config.StandardSync;
+import io.airbyte.config.persistence.ConfigNotFoundException;
+import io.airbyte.db.repositories.persistence.StandardSyncPersistence;
+import io.micronaut.http.annotation.Controller;
+import io.micronaut.http.annotation.Get;
+import java.io.IOException;
+import java.util.List;
+import java.util.UUID;
+import lombok.extern.slf4j.Slf4j;
+
+@Controller("/standardSync")
+@Slf4j
+public class StandardSyncController {
+
+  protected final StandardSyncPersistence standardSyncPersistence;
+
+  public StandardSyncController(StandardSyncPersistence standardSyncPersistence) {
+    this.standardSyncPersistence = standardSyncPersistence;
+  }
+
+  @Get("/{id}")
+  public StandardSync byId(UUID connectionId) throws ConfigNotFoundException, IOException {
+    return standardSyncPersistence.getStandardSync(connectionId);
+  }
+
+  @Get("/")
+  public List<StandardSync> findAll() throws ConfigNotFoundException, IOException {
+    return standardSyncPersistence.getStandardSyncs();
+  }
+
+}
