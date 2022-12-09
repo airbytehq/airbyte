84e958e75f96df8b8b5a60b5d42e88e73fb60f35
ievgeniit <etsybaev@gmail.com>
2022-12-09 09:28:35 +0100

    minor refactor

--- airbyte-integrations/bases/base-normalization/normalization/transform_catalog/utils.py
+++ airbyte-integrations/bases/base-normalization/normalization/transform_catalog/utils.py
@@ -27,19 +27,14 @@ def get_plain_list_from_one_of_array(one_of_definition) -> list:
 
 
 def is_string(property_type) -> bool:
-    if data_type.ONE_OF_VAR_NAME in property_type and data_type.ONE_OF_VAR_NAME in property_type:
-        plain_datatypes_list = get_plain_list_from_one_of_array(property_type)
-        return plain_datatypes_list == data_type.STRING_TYPE or data_type.STRING_TYPE in plain_datatypes_list
+    if data_type.ONE_OF_VAR_NAME in property_type:
+        return any(option[data_type.REF_TYPE_VAR_NAME] == data_type.STRING_TYPE for option in property_type[data_type.ONE_OF_VAR_NAME])
     else:
         return property_type == data_type.STRING_TYPE or data_type.STRING_TYPE in property_type
 
 
 def is_binary_datatype(property_type) -> bool:
-    if data_type.ONE_OF_VAR_NAME in property_type and data_type.ONE_OF_VAR_NAME in property_type:
-        plain_datatypes_list = get_plain_list_from_one_of_array(property_type)
-        return plain_datatypes_list == data_type.BINARY_DATA_TYPE or data_type.BINARY_DATA_TYPE in plain_datatypes_list
-    else:
-        return property_type == data_type.BINARY_DATA_TYPE or data_type.BINARY_DATA_TYPE in property_type
+    return property_type == data_type.BINARY_DATA_TYPE or data_type.BINARY_DATA_TYPE in property_type
 
 
 def is_datetime(definition: dict) -> bool:
@@ -47,32 +42,26 @@ def is_datetime(definition: dict) -> bool:
 
 
 def is_datetime_without_timezone(definition: dict) -> bool:
-    if data_type.ONE_OF_VAR_NAME in definition and data_type.ONE_OF_VAR_NAME in definition:
-        plain_datatypes_list = get_plain_list_from_one_of_array(definition)
-        return (
-            plain_datatypes_list == data_type.TIMESTAMP_WITHOUT_TIMEZONE_TYPE
-            or data_type.TIMESTAMP_WITHOUT_TIMEZONE_TYPE in plain_datatypes_list
-        )
+    if data_type.ONE_OF_VAR_NAME in definition:
+        return any(option[data_type.REF_TYPE_VAR_NAME] == data_type.TIMESTAMP_WITHOUT_TIMEZONE_TYPE
+                   for option in definition[data_type.ONE_OF_VAR_NAME])
     else:
         property_type = definition[data_type.REF_TYPE_VAR_NAME]
         return property_type == data_type.TIMESTAMP_WITHOUT_TIMEZONE_TYPE or data_type.TIMESTAMP_WITHOUT_TIMEZONE_TYPE in property_type
 
 
 def is_datetime_with_timezone(definition: dict) -> bool:
-    if data_type.ONE_OF_VAR_NAME in definition and data_type.ONE_OF_VAR_NAME in definition:
-        plain_datatypes_list = get_plain_list_from_one_of_array(definition)
-        return (
-            plain_datatypes_list == data_type.TIMESTAMP_WITH_TIMEZONE_TYPE or data_type.TIMESTAMP_WITH_TIMEZONE_TYPE in plain_datatypes_list
-        )
+    if data_type.ONE_OF_VAR_NAME in definition:
+        return any(option[data_type.REF_TYPE_VAR_NAME] == data_type.TIMESTAMP_WITH_TIMEZONE_TYPE
+                   for option in definition[data_type.ONE_OF_VAR_NAME])
     else:
         property_type = definition[data_type.REF_TYPE_VAR_NAME]
         return property_type == data_type.TIMESTAMP_WITH_TIMEZONE_TYPE or data_type.TIMESTAMP_WITH_TIMEZONE_TYPE in property_type
 
 
 def is_date(definition: dict) -> bool:
-    if data_type.ONE_OF_VAR_NAME in definition and data_type.ONE_OF_VAR_NAME in definition:
-        plain_datatypes_list = get_plain_list_from_one_of_array(definition)
-        return plain_datatypes_list == data_type.DATE_TYPE or data_type.DATE_TYPE in plain_datatypes_list
+    if data_type.ONE_OF_VAR_NAME in definition:
+        return any(option[data_type.REF_TYPE_VAR_NAME] == data_type.DATE_TYPE for option in definition[data_type.ONE_OF_VAR_NAME])
     else:
         property_type = definition[data_type.REF_TYPE_VAR_NAME]
         return property_type == data_type.DATE_TYPE or data_type.DATE_TYPE in property_type
@@ -83,20 +72,18 @@ def is_time(definition: dict) -> bool:
 
 
 def is_time_with_timezone(definition: dict) -> bool:
-    if data_type.ONE_OF_VAR_NAME in definition and data_type.ONE_OF_VAR_NAME in definition:
-        plain_datatypes_list = get_plain_list_from_one_of_array(definition)
-        return plain_datatypes_list == data_type.TIME_WITH_TIME_ZONE_TYPE or data_type.TIME_WITH_TIME_ZONE_TYPE in plain_datatypes_list
+    if data_type.ONE_OF_VAR_NAME in definition:
+        return any(option[data_type.REF_TYPE_VAR_NAME] == data_type.TIME_WITH_TIME_ZONE_TYPE
+                   for option in definition[data_type.ONE_OF_VAR_NAME])
     else:
         property_type = definition[data_type.REF_TYPE_VAR_NAME]
         return property_type == data_type.TIME_WITH_TIME_ZONE_TYPE or data_type.TIME_WITH_TIME_ZONE_TYPE in property_type
 
 
 def is_time_without_timezone(definition: dict) -> bool:
-    if data_type.ONE_OF_VAR_NAME in definition and data_type.ONE_OF_VAR_NAME in definition:
-        plain_datatypes_list = get_plain_list_from_one_of_array(definition)
-        return (
-            plain_datatypes_list == data_type.TIME_WITHOUT_TIME_ZONE_TYPE or data_type.TIME_WITHOUT_TIME_ZONE_TYPE in plain_datatypes_list
-        )
+    if data_type.ONE_OF_VAR_NAME in definition:
+        return any(option[data_type.REF_TYPE_VAR_NAME] == data_type.TIME_WITHOUT_TIME_ZONE_TYPE
+                   for option in definition[data_type.ONE_OF_VAR_NAME])
     else:
         property_type = definition[data_type.REF_TYPE_VAR_NAME]
         return property_type == data_type.TIME_WITHOUT_TIME_ZONE_TYPE or data_type.TIME_WITHOUT_TIME_ZONE_TYPE in property_type
@@ -106,9 +93,8 @@ def is_number(property_type) -> bool:
     if is_string(property_type):
         # Handle union type, give priority to wider scope types
         return False
-    if data_type.ONE_OF_VAR_NAME in property_type and data_type.ONE_OF_VAR_NAME in property_type:
-        plain_datatypes_list = get_plain_list_from_one_of_array(property_type)
-        return plain_datatypes_list == data_type.NUMBER_TYPE or data_type.NUMBER_TYPE in plain_datatypes_list
+    if data_type.ONE_OF_VAR_NAME in property_type:
+        return any(option[data_type.REF_TYPE_VAR_NAME] == data_type.NUMBER_TYPE for option in property_type[data_type.ONE_OF_VAR_NAME])
     else:
         return property_type == data_type.NUMBER_TYPE or data_type.NUMBER_TYPE in property_type
 
@@ -119,21 +105,18 @@ def is_big_integer(definition: dict) -> bool:
 
 
 def is_long(property_type, definition: dict) -> bool:
-    if data_type.ONE_OF_VAR_NAME in definition and data_type.ONE_OF_VAR_NAME in definition:
-        plain_datatypes_list = get_plain_list_from_one_of_array(definition)
-        return plain_datatypes_list == data_type.INTEGER_TYPE or data_type.INTEGER_TYPE in plain_datatypes_list
+    if data_type.ONE_OF_VAR_NAME in definition:
+        return any(option[data_type.REF_TYPE_VAR_NAME] == data_type.INTEGER_TYPE for option in property_type[data_type.ONE_OF_VAR_NAME])
     else:
         return property_type == data_type.INTEGER_TYPE or data_type.INTEGER_TYPE in property_type
 
 
 def is_boolean(property_type, definition: dict) -> bool:
-    # TODO do we need it now?
     if is_string(property_type) or is_number(property_type) or is_big_integer(definition) or is_long(property_type, definition):
         # Handle union type, give priority to wider scope types
         return False
-    if data_type.ONE_OF_VAR_NAME in definition and data_type.ONE_OF_VAR_NAME in definition:
-        plain_datatypes_list = get_plain_list_from_one_of_array(definition)
-        return plain_datatypes_list == data_type.BOOLEAN_TYPE or data_type.BOOLEAN_TYPE in plain_datatypes_list
+    if data_type.ONE_OF_VAR_NAME in definition:
+        return any(option[data_type.REF_TYPE_VAR_NAME] == data_type.BOOLEAN_TYPE for option in property_type[data_type.ONE_OF_VAR_NAME])
     else:
         return property_type == data_type.BOOLEAN_TYPE or data_type.BOOLEAN_TYPE in property_type
 
