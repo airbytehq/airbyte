49a90518501a97f302ff14cfdf4cb3e9afc7cc12
vmaltsev <ihbectop@gmail.com>
2022-12-11 17:40:41 +0200

    refactoring

--- airbyte-integrations/connectors/source-snowflake/src/main/java/io.airbyte.integrations.source.snowflake/SnowflakeSourceOperations.java
+++ airbyte-integrations/connectors/source-snowflake/src/main/java/io.airbyte.integrations.source.snowflake/SnowflakeSourceOperations.java
@@ -24,7 +24,6 @@ import java.sql.SQLException;
 import java.sql.Timestamp;
 import java.time.LocalDate;
 import java.time.LocalTime;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -126,19 +125,25 @@ public class SnowflakeSourceOperations extends JdbcSourceOperations {
     node.put(columnName, DateTimeConverter.convertToTimestamp(timestamp));
   }
 
-   @Override
-   protected void putDate(final ObjectNode node, final String columnName, final ResultSet resultSet,
-   final int index) throws SQLException {
-   final Date date = resultSet.getDate(index);
-   node.put(columnName, DateTimeConverter.convertToDate(date));
-   }
+  @Override
+  protected void putDate(final ObjectNode node,
+                         final String columnName,
+                         final ResultSet resultSet,
+                         final int index)
+      throws SQLException {
+    final Date date = resultSet.getDate(index);
+    node.put(columnName, DateTimeConverter.convertToDate(date));
+  }
 
-   @Override
-   protected void putTime(final ObjectNode node, final String columnName, final ResultSet resultSet,
-   final int index) throws SQLException {
-   // resultSet.getTime() will lose nanoseconds precision
-   final LocalTime localTime = resultSet.getTimestamp(index).toLocalDateTime().toLocalTime();
-   node.put(columnName, DateTimeConverter.convertToTime(localTime));
-   }
+  @Override
+  protected void putTime(final ObjectNode node,
+                         final String columnName,
+                         final ResultSet resultSet,
+                         final int index)
+      throws SQLException {
+    // resultSet.getTime() will lose nanoseconds precision
+    final LocalTime localTime = resultSet.getTimestamp(index).toLocalDateTime().toLocalTime();
+    node.put(columnName, DateTimeConverter.convertToTime(localTime));
+  }
 
 }
