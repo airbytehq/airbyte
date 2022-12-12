4d35d35fc909333499268cb8f3456369ca05b2ec
Sergey Chvalyuk <grubberr@gmail.com>
2022-12-12 16:34:33 +0000

    IssueRemoteLinks - refactored
    Signed-off-by: Sergey Chvalyuk <grubberr@gmail.com>

--- airbyte-integrations/connectors/source-jira/source_jira/streams.py
+++ airbyte-integrations/connectors/source-jira/source_jira/streams.py
@@ -519,18 +519,20 @@ class IssueRemoteLinks(StartDateJiraStream):
     https://developer.atlassian.com/cloud/jira/platform/rest/v3/api-group-issue-remote-links/#api-rest-api-3-issue-issueidorkey-remotelink-get
     """
 
-    def path(self, stream_slice: Mapping[str, Any], **kwargs) -> str:
-        key = stream_slice["key"]
-        return f"issue/{key}/remotelink"
-
-    def read_records(self, stream_slice: Optional[Mapping[str, Any]] = None, **kwargs) -> Iterable[Mapping[str, Any]]:
-        issues_stream = Issues(
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.issues_stream = Issues(
             authenticator=self.authenticator,
             domain=self._domain,
             projects=self._projects,
             start_date=self._start_date,
         )
-        for issue in issues_stream.read_records(sync_mode=SyncMode.full_refresh):
+
+    def path(self, stream_slice: Mapping[str, Any], **kwargs) -> str:
+        return f"issue/{stream_slice['key']}/remotelink"
+
+    def read_records(self, stream_slice: Optional[Mapping[str, Any]] = None, **kwargs) -> Iterable[Mapping[str, Any]]:
+        for issue in read_full_refresh(self.issues_stream):
             yield from super().read_records(stream_slice={"key": issue["key"]}, **kwargs)
 
 
