b069277fb70b1d0402f46b40a7cc04c83f41d080
Christoph Pirkl <christoph.pirkl@exasol.com>
2022-12-12 16:06:51 +0100

    Use Exasol container for integration tests

--- airbyte-integrations/connectors/destination-exasol/src/test-integration/java/io/airbyte/integrations/destination/exasol/ExasolDestinationAcceptanceTest.java
+++ airbyte-integrations/connectors/destination-exasol/src/test-integration/java/io/airbyte/integrations/destination/exasol/ExasolDestinationAcceptanceTest.java
@@ -4,6 +4,7 @@
 
 package io.airbyte.integrations.destination.exasol;
 
+import com.exasol.containers.ExasolContainer;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.google.common.collect.ImmutableMap;
@@ -12,26 +13,40 @@ import io.airbyte.db.factory.DataSourceFactory;
 import io.airbyte.db.factory.DatabaseDriver;
 import io.airbyte.db.jdbc.DefaultJdbcDatabase;
 import io.airbyte.db.jdbc.JdbcDatabase;
-import io.airbyte.db.jdbc.JdbcSourceOperations;
 import io.airbyte.db.jdbc.JdbcUtils;
 import io.airbyte.integrations.base.JavaBaseConstants;
 import io.airbyte.integrations.destination.ExtendedNameTransformer;
 import io.airbyte.integrations.standardtest.destination.DestinationAcceptanceTest;
-import java.io.IOException;
-import java.sql.SQLException;
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
-
-import io.airbyte.integrations.util.HostPortResolver;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class ExasolDestinationAcceptanceTest extends DestinationAcceptanceTest {
 
   private static final Logger LOGGER = LoggerFactory.getLogger(ExasolDestinationAcceptanceTest.class);
 
+  private static final ExasolContainer<? extends ExasolContainer> EXASOL = new ExasolContainer<>().withReuse(true);
+
   private final ExtendedNameTransformer namingResolver = new ExtendedNameTransformer();
+
+  @BeforeAll
+  static void startExasolContainer() throws IOException {
+    Files.createDirectories(Path.of("target"));
+    EXASOL.start();
+  }
+
+  @AfterAll
+  static void stopExasolContainer() {
+    EXASOL.stop();
+  }
 
   @Override
   protected String getImageName() {
@@ -42,9 +57,9 @@ public class ExasolDestinationAcceptanceTest extends DestinationAcceptanceTest {
   protected JsonNode getConfig() {
 
     return Jsons.jsonNode(ImmutableMap.builder()
-            .put("connectionstring","172.17.0.2/60E5A89BE78537903378635DD996657676920AA87B4F6D99ABCA11748739C216:8563")
-            .put(JdbcUtils.USERNAME_KEY, "sys")
-            .put(JdbcUtils.PASSWORD_KEY, "exasol")
+            .put("connectionstring",EXASOL.getHost()+"/"+EXASOL.getTlsCertificateFingerprint().get()+":"+EXASOL.getFirstMappedDatabasePort())
+            .put(JdbcUtils.USERNAME_KEY, EXASOL.getUsername())
+            .put(JdbcUtils.PASSWORD_KEY, EXASOL.getPassword())
             .put(JdbcUtils.SCHEMA_KEY, "TEST")
             .build());
   }
