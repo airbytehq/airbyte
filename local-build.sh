2034c0da7b96058d29db2f5d86387f7376ce44f1
Mahdi Dibaiee <mdibaiee@pm.me>
2022-12-12 17:59:54 +0000

    faster builds

--- local-build.sh
+++ local-build.sh
@@ -2,4 +2,4 @@ export DOCKER_BUILD_PLATFORM=linux/amd64
 export ALPINE_IMAGE=amd64/alpine:3.14
 export DOCKER_BUILD_ARCH=amd64
 ./tools/bin/setup_connector_venv.sh $1 python3.9
-./gradlew :airbyte-integrations:connectors:$1:build && docker tag docker.io/airbyte/$1:dev ghcr.io/estuary/$1:local
+./gradlew :airbyte-integrations:connectors:$1:airbyteDocker && docker tag docker.io/airbyte/$1:dev ghcr.io/estuary/$1:local
--- tools/bin/setup_connector_venv.sh
+++ tools/bin/setup_connector_venv.sh
@@ -7,9 +7,13 @@ PYTHON_EXECUTABLE=${2-python}
 echo Installing requirements for $CONNECTOR
 
 cd airbyte-integrations/connectors/$CONNECTOR
-$PYTHON_EXECUTABLE -m venv --without-pip .venv
-source .venv/bin/activate
-python ../../../get-pip.py
-pip install -r requirements.txt
-pip install '.[tests]'
-pip install pytest-cov
+if [[ ! -d .venv ]]; then
+    $PYTHON_EXECUTABLE -m venv --without-pip .venv
+    source .venv/bin/activate
+    python ../../../get-pip.py
+    pip install -r requirements.txt
+    pip install '.[tests]'
+    pip install pytest-cov
+else
+    echo "directory .venv exists, skipping"
+fi
