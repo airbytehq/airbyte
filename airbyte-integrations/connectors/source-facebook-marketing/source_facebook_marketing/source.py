366c6b4b33a0c34849cbca784dd4bffeb95a1512
Artem Inzhyyants <artem.inzhyyants@gmail.com>
2022-12-13 00:22:17 +0100

    Source Facebook Marketing: remove unsupported fields

--- airbyte-integrations/connectors/source-facebook-marketing/source_facebook_marketing/source.py
+++ airbyte-integrations/connectors/source-facebook-marketing/source_facebook_marketing/source.py
@@ -35,6 +35,7 @@ from source_facebook_marketing.streams import (
 from .utils import validate_end_date, validate_start_date
 
 logger = logging.getLogger("airbyte")
+UNSUPPORTED_FIELDS = {"unique_conversions", "unique_ctr", "unique_clicks"}
 
 
 class SourceFacebookMarketing(AbstractSource):
@@ -183,6 +184,12 @@ class SourceFacebookMarketing(AbstractSource):
         """return custom insights streams"""
         streams = []
         for insight in config.custom_insights or []:
+            insight_fields = set(insight.fields)
+            if insight_fields.intersection(UNSUPPORTED_FIELDS):
+                # https://github.com/airbytehq/oncall/issues/1137
+                logger.info(f"Following fields were removed from the Custom{insight.name} fields list due to error"
+                            f" on Facebook side: {insight_fields.intersection(UNSUPPORTED_FIELDS)}")
+                insight_fields -= UNSUPPORTED_FIELDS
             stream = AdsInsights(
                 api=api,
                 name=f"Custom{insight.name}",
