823b07257d688260d13910c28f0bc046afbb710d
Benoit Moriceau <benoit@airbyte.io>
2022-12-12 10:52:54 -0800

    Convert jobs to micronaut

--- airbyte-server/src/main/java/io/airbyte/server/ServerFactory.java
+++ airbyte-server/src/main/java/io/airbyte/server/ServerFactory.java
@@ -13,25 +13,19 @@ import io.airbyte.config.persistence.SecretsRepositoryReader;
 import io.airbyte.config.persistence.SecretsRepositoryWriter;
 import io.airbyte.db.Database;
 import io.airbyte.persistence.job.JobPersistence;
-import io.airbyte.server.apis.JobsApiController;
 import io.airbyte.server.apis.LogsApiController;
 import io.airbyte.server.apis.NotificationsApiController;
-import io.airbyte.server.apis.SourceDefinitionSpecificationApiController;
 import io.airbyte.server.apis.SourceOauthApiController;
 import io.airbyte.server.apis.StateApiController;
 import io.airbyte.server.apis.WebBackendApiController;
 import io.airbyte.server.apis.WorkspaceApiController;
-import io.airbyte.server.apis.binders.JobsApiBinder;
 import io.airbyte.server.apis.binders.LogsApiBinder;
 import io.airbyte.server.apis.binders.NotificationApiBinder;
-import io.airbyte.server.apis.binders.SourceDefinitionSpecificationApiBinder;
 import io.airbyte.server.apis.binders.SourceOauthApiBinder;
 import io.airbyte.server.apis.binders.WebBackendApiBinder;
 import io.airbyte.server.apis.binders.WorkspaceApiBinder;
-import io.airbyte.server.apis.factories.JobsApiFactory;
 import io.airbyte.server.apis.factories.LogsApiFactory;
 import io.airbyte.server.apis.factories.NotificationsApiFactory;
-import io.airbyte.server.apis.factories.SourceDefinitionSpecificationApiFactory;
 import io.airbyte.server.apis.factories.SourceOauthApiFactory;
 import io.airbyte.server.apis.factories.WebBackendApiFactory;
 import io.airbyte.server.apis.factories.WorkspaceApiFactory;
@@ -131,13 +125,9 @@ public interface ServerFactory {
 
       SourceOauthApiFactory.setValues(oAuthHandler);
 
-      JobsApiFactory.setValues(jobHistoryHandler, schedulerHandler);
-
       LogsApiFactory.setValues(logsHandler);
 
       NotificationsApiFactory.setValues(workspacesHandler);
-
-      SourceDefinitionSpecificationApiFactory.setValues(schedulerHandler);
 
       WebBackendApiFactory.setValues(webBackendConnectionsHandler, webBackendGeographiesHandler, webBackendCheckUpdatesHandler);
 
@@ -145,20 +135,16 @@ public interface ServerFactory {
 
       // server configuration
       final Set<Class<?>> componentClasses = Set.of(
-          JobsApiController.class,
           LogsApiController.class,
           NotificationsApiController.class,
-          SourceDefinitionSpecificationApiController.class,
           SourceOauthApiController.class,
           StateApiController.class,
           WebBackendApiController.class,
           WorkspaceApiController.class);
 
       final Set<Object> components = Set.of(
-          new JobsApiBinder(),
           new LogsApiBinder(),
           new NotificationApiBinder(),
-          new SourceDefinitionSpecificationApiBinder(),
           new SourceOauthApiBinder(),
           new WebBackendApiBinder(),
           new WorkspaceApiBinder());
--- airbyte-server/src/main/java/io/airbyte/server/apis/JobsApiController.java
+++ airbyte-server/src/main/java/io/airbyte/server/apis/JobsApiController.java
@@ -14,41 +14,49 @@ import io.airbyte.api.model.generated.JobListRequestBody;
 import io.airbyte.api.model.generated.JobReadList;
 import io.airbyte.server.handlers.JobHistoryHandler;
 import io.airbyte.server.handlers.SchedulerHandler;
+import io.micronaut.http.annotation.Controller;
+import io.micronaut.http.annotation.Post;
 import javax.ws.rs.Path;
 import lombok.AllArgsConstructor;
 
-@Path("/v1/jobs")
+@Controller("/api/v1/jobs")
 @AllArgsConstructor
 public class JobsApiController implements JobsApi {
 
   private final JobHistoryHandler jobHistoryHandler;
   private final SchedulerHandler schedulerHandler;
 
+  @Post("/cancel")
   @Override
   public JobInfoRead cancelJob(final JobIdRequestBody jobIdRequestBody) {
     return ApiHelper.execute(() -> schedulerHandler.cancelJob(jobIdRequestBody));
   }
 
+  @Post("/get_normalization_status")
   @Override
   public AttemptNormalizationStatusReadList getAttemptNormalizationStatusesForJob(final JobIdRequestBody jobIdRequestBody) {
     return ApiHelper.execute(() -> jobHistoryHandler.getAttemptNormalizationStatuses(jobIdRequestBody));
   }
 
+  @Post("/get_debug_info")
   @Override
   public JobDebugInfoRead getJobDebugInfo(final JobIdRequestBody jobIdRequestBody) {
     return ApiHelper.execute(() -> jobHistoryHandler.getJobDebugInfo(jobIdRequestBody));
   }
 
+  @Post("/get")
   @Override
   public JobInfoRead getJobInfo(final JobIdRequestBody jobIdRequestBody) {
     return ApiHelper.execute(() -> jobHistoryHandler.getJobInfo(jobIdRequestBody));
   }
 
+  @Post("/get_light")
   @Override
   public JobInfoLightRead getJobInfoLight(final JobIdRequestBody jobIdRequestBody) {
     return ApiHelper.execute(() -> jobHistoryHandler.getJobInfoLight(jobIdRequestBody));
   }
 
+  @Post("/list")
   @Override
   public JobReadList listJobsFor(final JobListRequestBody jobListRequestBody) {
     return ApiHelper.execute(() -> jobHistoryHandler.listJobsFor(jobListRequestBody));
