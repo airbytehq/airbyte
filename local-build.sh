57418e14b6d25c17f3d2e5174bc3fd47619efbce
Mahdi Dibaiee <mdibaiee@pm.me>
2022-12-13 11:43:13 +0000

    local-build: option to push docker image

--- local-build.sh
+++ local-build.sh
@@ -1,5 +1,32 @@
+_push=0
+while getopts ":hp" option; do
+   case $option in
+      h) # display Help
+         echo "Build a connector image locally."
+         echo "local-build.sh [OPTIONS] <connector-name>"
+         echo ""
+         echo "Options:"
+         echo "-p   publish the image to ghcr, namespace with your username or codespace name"
+         exit;;
+      p)
+        _push=1
+   esac
+done
+
+connector=${@:$OPTIND:1}
+
 export DOCKER_BUILD_PLATFORM=linux/amd64
 export ALPINE_IMAGE=amd64/alpine:3.14
 export DOCKER_BUILD_ARCH=amd64
-./tools/bin/setup_connector_venv.sh $1 python3.9
-./gradlew :airbyte-integrations:connectors:$1:airbyteDocker && docker tag docker.io/airbyte/$1:dev ghcr.io/estuary/$1:local
+./tools/bin/setup_connector_venv.sh $connector python3.9
+./gradlew :airbyte-integrations:connectors:$connector:airbyteDocker
+docker tag docker.io/airbyte/$connector:dev ghcr.io/estuary/$connector:local
+
+if [[ "$_push" -eq "1" ]]; then
+    tag=$CODESPACE_NAME
+    if [[ -z "$tag" ]]; then
+        tag="$USER-airbyte"
+    fi
+    docker tag docker.io/airbyte/$connector:dev ghcr.io/estuary/$connector:$tag
+    docker push ghcr.io/estuary/$connector:$tag
+fi
--- tools/bin/setup_connector_venv.sh
+++ tools/bin/setup_connector_venv.sh
@@ -4,10 +4,10 @@ set -e
 
 CONNECTOR=$1
 PYTHON_EXECUTABLE=${2-python}
-echo Installing requirements for $CONNECTOR
 
 cd airbyte-integrations/connectors/$CONNECTOR
 if [[ ! -d .venv ]]; then
+    echo Installing requirements for $CONNECTOR
     $PYTHON_EXECUTABLE -m venv --without-pip .venv
     source .venv/bin/activate
     python ../../../get-pip.py
