8022b4737e916f2c9cba827481096041507b4515
Sergey Chvalyuk <grubberr@gmail.com>
2022-12-13 09:34:03 +0000

    ScreenTabFields refactored
    Signed-off-by: Sergey Chvalyuk <grubberr@gmail.com>

--- airbyte-integrations/connectors/source-jira/source_jira/streams.py
+++ airbyte-integrations/connectors/source-jira/source_jira/streams.py
@@ -960,8 +960,7 @@ class ScreenTabs(JiraStream):
         self.screens_stream = Screens(authenticator=self.authenticator, domain=self._domain, projects=self._projects)
 
     def path(self, stream_slice: Mapping[str, Any], **kwargs) -> str:
-        screen_id = stream_slice["screen_id"]
-        return f"screens/{screen_id}/tabs"
+        return f"screens/{stream_slice['screen_id']}/tabs"
 
     def read_records(self, stream_slice: Optional[Mapping[str, Any]] = None, **kwargs) -> Iterable[Mapping[str, Any]]:
         for screen in read_full_refresh(self.screens_stream):
@@ -977,16 +976,17 @@ class ScreenTabFields(JiraStream):
     https://developer.atlassian.com/cloud/jira/platform/rest/v3/api-group-screen-tab-fields/#api-rest-api-3-screens-screenid-tabs-tabid-fields-get
     """
 
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.screens_stream = Screens(authenticator=self.authenticator, domain=self._domain, projects=self._projects)
+        self.screen_tabs_stream = ScreenTabs(authenticator=self.authenticator, domain=self._domain, projects=self._projects)
+
     def path(self, stream_slice: Mapping[str, Any], **kwargs) -> str:
-        screen_id = stream_slice["screen_id"]
-        tab_id = stream_slice["tab_id"]
-        return f"screens/{screen_id}/tabs/{tab_id}/fields"
+        return f"screens/{stream_slice['screen_id']}/tabs/{stream_slice['tab_id']}/fields"
 
     def read_records(self, stream_slice: Optional[Mapping[str, Any]] = None, **kwargs) -> Iterable[Mapping[str, Any]]:
-        screens_stream = Screens(authenticator=self.authenticator, domain=self._domain, projects=self._projects)
-        screen_tabs_stream = ScreenTabs(authenticator=self.authenticator, domain=self._domain, projects=self._projects)
-        for screen in screens_stream.read_records(sync_mode=SyncMode.full_refresh):
-            for tab in screen_tabs_stream.read_tab_records(stream_slice={"screen_id": screen["id"]}, **kwargs):
+        for screen in read_full_refresh(self.screens_stream):
+            for tab in self.screen_tabs_stream.read_tab_records(stream_slice={"screen_id": screen["id"]}, **kwargs):
                 if id in tab:  # Check for proper tab record since the ScreenTabs stream doesn't throw http errors
                     yield from super().read_records(stream_slice={"screen_id": screen["id"], "tab_id": tab["id"]}, **kwargs)
 
