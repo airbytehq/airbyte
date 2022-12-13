9cb7fc57eac453dbf093c27579d061b6d13a3d2d
Sergey Chvalyuk <grubberr@gmail.com>
2022-12-13 07:20:11 +0000

    ProjectComponents refactored
    Signed-off-by: Sergey Chvalyuk <grubberr@gmail.com>

--- airbyte-integrations/connectors/source-jira/source_jira/streams.py
+++ airbyte-integrations/connectors/source-jira/source_jira/streams.py
@@ -777,13 +777,15 @@ class ProjectComponents(JiraStream):
 
     extract_field = "values"
 
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.projects_stream = Projects(authenticator=self.authenticator, domain=self._domain, projects=self._projects)
+
     def path(self, stream_slice: Mapping[str, Any], **kwargs) -> str:
-        key = stream_slice["key"]
-        return f"project/{key}/component"
+        return f"project/{stream_slice['key']}/component"
 
     def read_records(self, stream_slice: Optional[Mapping[str, Any]] = None, **kwargs) -> Iterable[Mapping[str, Any]]:
-        projects_stream = Projects(authenticator=self.authenticator, domain=self._domain, projects=self._projects)
-        for project in projects_stream.read_records(sync_mode=SyncMode.full_refresh):
+        for project in read_full_refresh(self.projects_stream):
             yield from super().read_records(stream_slice={"key": project["key"]}, **kwargs)
 
 
