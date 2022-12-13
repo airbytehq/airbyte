d25d6e21d9d3b245ec6c59d89853d82b9b6f50f3
Sergey Chvalyuk <grubberr@gmail.com>
2022-12-13 07:28:28 +0000

    ProjectEmail refactored
    Signed-off-by: Sergey Chvalyuk <grubberr@gmail.com>

--- airbyte-integrations/connectors/source-jira/source_jira/streams.py
+++ airbyte-integrations/connectors/source-jira/source_jira/streams.py
@@ -794,16 +794,22 @@ class ProjectEmail(JiraStream):
     https://developer.atlassian.com/cloud/jira/platform/rest/v3/api-group-project-email/#api-rest-api-3-project-projectid-email-get
     """
 
-    primary_key = None
+    primary_key = "projectId"
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.projects_stream = Projects(authenticator=self.authenticator, domain=self._domain, projects=self._projects)
 
     def path(self, stream_slice: Mapping[str, Any], **kwargs) -> str:
-        project_id = stream_slice["project_id"]
-        return f"project/{project_id}/email"
+        return f"project/{stream_slice['project_id']}/email"
 
     def read_records(self, stream_slice: Optional[Mapping[str, Any]] = None, **kwargs) -> Iterable[Mapping[str, Any]]:
-        projects_stream = Projects(authenticator=self.authenticator, domain=self._domain, projects=self._projects)
-        for project in projects_stream.read_records(sync_mode=SyncMode.full_refresh):
+        for project in read_full_refresh(self.projects_stream):
             yield from super().read_records(stream_slice={"project_id": project["id"]}, **kwargs)
+
+    def transform(self, record: MutableMapping[str, Any], stream_slice: Mapping[str, Any], **kwargs) -> MutableMapping[str, Any]:
+        record["projectId"] = stream_slice["project_id"]
+        return record
 
 
 class ProjectPermissionSchemes(JiraStream):
