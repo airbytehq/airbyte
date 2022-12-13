4264b028c8a12dca165dfe4f4c78c420f573f26b
lmossman <lake@airbyte.io>
2022-12-13 15:59:54 -0800

    style view titles

--- airbyte-webapp/src/components/connectorBuilder/Builder/GlobalConfigView.tsx
+++ airbyte-webapp/src/components/connectorBuilder/Builder/GlobalConfigView.tsx
@@ -1,21 +1,23 @@
+import { FormattedMessage } from "react-intl";
+
+import { Heading } from "components/ui/Heading";
+
 import { BuilderCard } from "./BuilderCard";
 import { BuilderField } from "./BuilderField";
+import { BuilderTitle } from "./BuilderTitle";
+import styles from "./GlobalConfigView.module.scss";
 
 export const GlobalConfigView: React.FC = () => {
   return (
-    <>
+    <div className={styles.container}>
+      <Heading className={styles.heading} as="h1" size="sm">
+        <FormattedMessage id="connectorBuilder.globalConfiguration" />
+      </Heading>
       {/* Not using intl for the labels and tooltips in this component in order to keep maintainence simple */}
-      <BuilderCard>
-        <BuilderField
-          type="text"
-          path="global.connectorName"
-          label="Connector Name"
-          tooltip="Name of the connector being built"
-        />
-      </BuilderCard>
+      <BuilderTitle path="global.connectorName" label="Connector Name" size="lg" />
       <BuilderCard>
         <BuilderField type="text" path="global.urlBase" label="API URL" tooltip="Base URL of the source API" />
       </BuilderCard>
-    </>
+    </div>
   );
 };
--- airbyte-webapp/src/components/connectorBuilder/Builder/StreamConfigView.tsx
+++ airbyte-webapp/src/components/connectorBuilder/Builder/StreamConfigView.tsx
@@ -1,5 +1,11 @@
+import { FormattedMessage } from "react-intl";
+
+import { Heading } from "components/ui/Heading";
+
 import { BuilderCard } from "./BuilderCard";
 import { BuilderField } from "./BuilderField";
+import { BuilderTitle } from "./BuilderTitle";
+import styles from "./StreamConfigView.module.scss";
 
 interface StreamConfigViewProps {
   streamNum: number;
@@ -9,28 +15,33 @@ export const StreamConfigView: React.FC<StreamConfigViewProps> = ({ streamNum })
   const streamPath = (path: string) => `streams[${streamNum}].${path}`;
 
   return (
-    <BuilderCard>
+    <div className={styles.container}>
+      <Heading className={styles.heading} as="h1" size="sm">
+        <FormattedMessage id="connectorBuilder.stream" />
+      </Heading>
       {/* Not using intl for the labels and tooltips in this component in order to keep maintainence simple */}
-      <BuilderField type="text" path={streamPath("name")} label="Stream Name" tooltip="Name of the stream" />
-      <BuilderField
-        type="text"
-        path={streamPath("urlPath")}
-        label="Path URL"
-        tooltip="Path of the endpoint that this stream represents."
-      />
-      <BuilderField
-        type="array"
-        path={streamPath("fieldPointer")}
-        label="Field Pointer"
-        tooltip="Pointer into the response that should be extracted as the final record"
-      />
-      <BuilderField
-        type="enum"
-        path={streamPath("httpMethod")}
-        options={["GET", "POST"]}
-        label="HTTP Method"
-        tooltip="HTTP method to use for requests sent to the API"
-      />
-    </BuilderCard>
+      <BuilderTitle path={streamPath("name")} label="Stream Name" size="md" />
+      <BuilderCard>
+        <BuilderField
+          type="text"
+          path={streamPath("urlPath")}
+          label="Path URL"
+          tooltip="Path of the endpoint that this stream represents."
+        />
+        <BuilderField
+          type="array"
+          path={streamPath("fieldPointer")}
+          label="Field Pointer"
+          tooltip="Pointer into the response that should be extracted as the final record"
+        />
+        <BuilderField
+          type="enum"
+          path={streamPath("httpMethod")}
+          options={["GET", "POST"]}
+          label="HTTP Method"
+          tooltip="HTTP method to use for requests sent to the API"
+        />
+      </BuilderCard>
+    </div>
   );
 };
