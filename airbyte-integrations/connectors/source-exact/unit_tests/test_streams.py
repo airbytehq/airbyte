62be17d370bccec4ae87185e8a186590e9e5c8ae
Joël Luijmes <me@joell.dev>
2022-12-14 15:37:38 +0100

    chore(exact): rename test objects to mitigate warning

--- airbyte-integrations/connectors/source-exact/unit_tests/test_streams.py
+++ airbyte-integrations/connectors/source-exact/unit_tests/test_streams.py
@@ -9,7 +9,7 @@ import requests
 from source_exact.streams import ExactOtherStream, ExactSyncStream
 
 
-class TestExactSyncStream(ExactSyncStream):
+class MyTestExactSyncStream(ExactSyncStream):
     endpoint = "sync/mocked"
 
     @property
@@ -28,7 +28,7 @@ class TestExactSyncStream(ExactSyncStream):
         }
 
 
-class TestExactOtherStream(ExactOtherStream):
+class MyTestExactOtherStream(ExactOtherStream):
     endpoint = "other/mocked"
 
     @property
@@ -44,12 +44,12 @@ class TestExactOtherStream(ExactOtherStream):
         }
 
 
-class TestRequestException(requests.RequestException):
+class MyTestRequestException(requests.RequestException):
     pass
 
 
 def test_path__failure(config_oauth):
-    stream = TestExactSyncStream(config_oauth)
+    stream = MyTestExactSyncStream(config_oauth)
     stream.endpoint = None
 
     with pytest.raises(RuntimeError, match="Subclass is missing endpoint"):
@@ -57,20 +57,20 @@ def test_path__failure(config_oauth):
 
 
 def test_path__next_page(config_oauth):
-    assert TestExactSyncStream(config_oauth).path({"next_url": "example.com/next"}) == "example.com/next"
+    assert MyTestExactSyncStream(config_oauth).path({"next_url": "example.com/next"}) == "example.com/next"
 
 
 def test_path__initial(config_oauth):
-    stream = TestExactSyncStream(config_oauth)
+    stream = MyTestExactSyncStream(config_oauth)
     assert stream.path(None) == stream.endpoint
 
 
 def test_request_headers(config_oauth: dict):
-    assert TestExactSyncStream(config_oauth).request_headers() == {"Accept": "application/json"}
+    assert MyTestExactSyncStream(config_oauth).request_headers() == {"Accept": "application/json"}
 
 
 def test_request_params_with_next_page(config_oauth: dict):
-    assert TestExactSyncStream(config_oauth).request_params(next_page_token="someting") == {}
+    assert MyTestExactSyncStream(config_oauth).request_params(next_page_token="someting") == {}
 
 
 @pytest.mark.parametrize(
@@ -83,7 +83,7 @@ def test_request_params_with_next_page(config_oauth: dict):
     ],
 )
 def test_request_params_with_sync_stream(config_oauth: dict, next_page_token, cursor_value, expected):
-    stream = TestExactSyncStream(config_oauth)
+    stream = MyTestExactSyncStream(config_oauth)
     stream._cursor_value = cursor_value
 
     assert stream.request_params(next_page_token) == expected
@@ -103,7 +103,7 @@ def test_request_params_with_sync_stream(config_oauth: dict, next_page_token, cu
     ],
 )
 def test_request_params_with_other_stream(config_oauth: dict, next_page_token, cursor_value, expected):
-    stream = TestExactOtherStream(config_oauth)
+    stream = MyTestExactOtherStream(config_oauth)
     stream._cursor_value = cursor_value
 
     assert stream.request_params(next_page_token) == expected
@@ -121,7 +121,7 @@ def test_next_page_token(config_oauth: dict, body: dict, expected: dict):
     response_mock = MagicMock()
     response_mock.json.return_value = body
 
-    assert TestExactSyncStream(config_oauth).next_page_token(response_mock) == expected
+    assert MyTestExactSyncStream(config_oauth).next_page_token(response_mock) == expected
 
 
 @pytest.mark.parametrize(
@@ -136,14 +136,14 @@ def test_parse_response(config_oauth: dict, body, expected):
     response_mock = MagicMock()
     response_mock.json.return_value = body
 
-    assert TestExactSyncStream(config_oauth).parse_response(response_mock) == expected
+    assert MyTestExactSyncStream(config_oauth).parse_response(response_mock) == expected
 
 
 def test_is_token_expired__not_expired(config_oauth):
     response_mock = MagicMock()
     response_mock.status_code = 200
 
-    assert not TestExactSyncStream(config_oauth)._is_token_expired(response_mock)
+    assert not MyTestExactSyncStream(config_oauth)._is_token_expired(response_mock)
 
 
 def test_is_token_expired__expired(config_oauth):
@@ -151,7 +151,7 @@ def test_is_token_expired__expired(config_oauth):
     response_mock.status_code = 401
     response_mock.headers = {"WWW-Authenticate": "access token expired"}
 
-    assert TestExactSyncStream(config_oauth)._is_token_expired(response_mock)
+    assert MyTestExactSyncStream(config_oauth)._is_token_expired(response_mock)
 
 
 def test_is_token_expired__failure(config_oauth):
@@ -160,7 +160,7 @@ def test_is_token_expired__failure(config_oauth):
     response_mock.headers = {"WWW-Authenticate": "can't access this resource"}
 
     with pytest.raises(RuntimeError, match="Unexpected forbidden error"):
-        TestExactSyncStream(config_oauth)._is_token_expired(response_mock)
+        MyTestExactSyncStream(config_oauth)._is_token_expired(response_mock)
 
 
 def test_parse_item__nested_timestamps(config_oauth: dict):
@@ -175,7 +175,7 @@ def test_parse_item__nested_timestamps(config_oauth: dict):
         "array": ["/Date(1640995200000)/"],
         "array_with_nested": [{"date": "/Date(1640995200000)/"}],
     }
-    assert TestExactSyncStream(config_oauth)._parse_item(obj) == {
+    assert MyTestExactSyncStream(config_oauth)._parse_item(obj) == {
         "date": "2022-01-01T00:00:00+00:00",
         "nested": {"date": "2022-01-01T00:00:00+00:00"},
         "array": ["2022-01-01T00:00:00+00:00"],
@@ -193,7 +193,7 @@ def test_parse_item__type_casting(config_oauth: dict):
         "FloatField": "7.89",  # explicit str as that also is how Exact can return the data
         "BoolField": "true",  # explicit str as that also is how Exact can return the data
     }
-    assert TestExactSyncStream(config_oauth)._parse_item(obj) == {
+    assert MyTestExactSyncStream(config_oauth)._parse_item(obj) == {
         "ID": "entity-100",
         "Timestamp": 123,
         "IntField": 456,
@@ -205,7 +205,7 @@ def test_parse_item__type_casting(config_oauth: dict):
 def test_send_request__reraise_if_on_non_request_exception(config_oauth):
     request_mock, request_kwargs = MagicMock(), MagicMock()
 
-    stream = TestExactSyncStream(config_oauth)
+    stream = MyTestExactSyncStream(config_oauth)
     stream._send = MagicMock(side_effect=RuntimeError("Random Error"))
 
     with pytest.raises(RuntimeError, match="Random Error"):
@@ -215,10 +215,10 @@ def test_send_request__reraise_if_on_non_request_exception(config_oauth):
 def test_send_request__reraise_if_response_missing_on_exception(config_oauth):
     request_mock, request_kwargs = MagicMock(), MagicMock()
 
-    stream = TestExactSyncStream(config_oauth)
-    stream._send = MagicMock(side_effect=TestRequestException("Missing Response"))
+    stream = MyTestExactSyncStream(config_oauth)
+    stream._send = MagicMock(side_effect=MyTestRequestException("Missing Response"))
 
-    with pytest.raises(TestRequestException, match="Missing Response"):
+    with pytest.raises(MyTestRequestException, match="Missing Response"):
         stream._send_request(request_mock, request_kwargs)
 
 
@@ -226,10 +226,10 @@ def test_send_request__reraise_if_not_retryable_exception(config_oauth):
     request_mock, request_kwargs, response_mock = MagicMock(), MagicMock(), MagicMock()
     response_mock.status_code = 400
 
-    stream = TestExactSyncStream(config_oauth)
-    stream._send = MagicMock(side_effect=TestRequestException("BadRequest", response=response_mock))
+    stream = MyTestExactSyncStream(config_oauth)
+    stream._send = MagicMock(side_effect=MyTestRequestException("BadRequest", response=response_mock))
 
-    with pytest.raises(TestRequestException, match="BadRequest"):
+    with pytest.raises(MyTestRequestException, match="BadRequest"):
         stream._send_request(request_mock, request_kwargs)
 
 
@@ -237,8 +237,8 @@ def test_send_request__retries_on_server_error(config_oauth):
     request_mock, request_kwargs, response_mock = MagicMock(), MagicMock(), MagicMock()
     response_mock.status_code = 500
 
-    stream = TestExactSyncStream(config_oauth)
-    stream._send = MagicMock(side_effect=TestRequestException("ServerError", response=response_mock))
+    stream = MyTestExactSyncStream(config_oauth)
+    stream._send = MagicMock(side_effect=MyTestRequestException("ServerError", response=response_mock))
 
     stream._send_request(request_mock, request_kwargs)
 
@@ -251,8 +251,8 @@ def test_send_request__retries_on_expired_token(config_oauth):
     response_mock.status_code = 401
     response_mock.headers = {"WWW-Authenticate": "access token expired"}
 
-    stream = TestExactSyncStream(config_oauth)
-    stream._send = MagicMock(side_effect=TestRequestException("Forbidden", response=response_mock))
+    stream = MyTestExactSyncStream(config_oauth)
+    stream._send = MagicMock(side_effect=MyTestRequestException("Forbidden", response=response_mock))
     stream._is_token_expired = MagicMock(return_value=True)
 
     stream._send_request(request_mock, request_kwargs)
