94513acdee3d83909e4ed764574621c0b79526cd
erohmensing <erohmensing@gmail.com>
2022-12-14 09:42:15 -0500

    Cleanup: Address review comments

--- airbyte-integrations/connectors/source-github/source_github/availability_strategies.py
+++ airbyte-integrations/connectors/source-github/source_github/availability_strategies.py
@@ -22,14 +22,14 @@ class OrganizationBasedAvailabilityStrategy(HttpAvailabilityStrategy):
         self, stream: Stream, logger: logging.Logger, source: Optional["Source"], error: HTTPError
     ) -> Dict[int, str]:
         stream_slice = StreamHelper().get_stream_slice(stream)
-        organisation = stream_slice.get("organization", "")
+        organization = stream_slice["organization"]
         response_error_msg = str(error.response.json().get("message"))
 
         reasons_for_codes = {
-            requests.codes.NOT_FOUND: f"`{stream.__class__.__name__}` stream isn't available for organization `{stream_slice['organization']}`.",
+            requests.codes.NOT_FOUND: f"`{stream.__class__.__name__}` stream isn't available for organization `{organization}`.",
             # When `403` for the stream, that has no access to the organization's teams, based on OAuth Apps Restrictions:
             # https://docs.github.com/en/organizations/restricting-access-to-your-organizations-data/enabling-oauth-app-access-restrictions-for-your-organization
-            requests.codes.FORBIDDEN: f"`{stream.name}` stream isn't available for organization `{organisation}`. Full error message: {response_error_msg}",
+            requests.codes.FORBIDDEN: f"`{stream.name}` stream isn't available for organization `{organization}`. Full error message: {response_error_msg}",
         }
         return reasons_for_codes
 
@@ -43,13 +43,13 @@ class RepositoryBasedAvailabilityStrategy(HttpAvailabilityStrategy):
         self, stream: Stream, logger: logging.Logger, source: Optional["Source"], error: HTTPError
     ) -> Dict[int, str]:
         stream_slice = StreamHelper().get_stream_slice(stream)
-        repository = stream_slice.get("repository", "")
+        repository = stream_slice["repository"]
         error_msg = str(error.response.json().get("message"))
 
         reasons_for_codes = {
-            requests.codes.NOT_FOUND: f"`{stream.__class__.__name__}` stream isn't available for repository `{stream_slice['repository']}`.",
+            requests.codes.NOT_FOUND: f"`{stream.name}` stream isn't available for repository `{repository}`.",
             requests.codes.FORBIDDEN: f"`{stream.name}` stream isn't available for repository `{repository}`. Full error message: {error_msg}",
-            requests.codes.CONFLICT: f"`{stream.name}` stream isn't available for repository `{stream_slice['repository']}`, it seems like this repository is empty.",
+            requests.codes.CONFLICT: f"`{stream.name}` stream isn't available for repository `{repository}`, it seems like this repository is empty.",
         }
         return reasons_for_codes
 
@@ -63,10 +63,10 @@ class WorkflowRunsAvailabilityStrategy(RepositoryBasedAvailabilityStrategy):
         self, stream: Stream, logger: logging.Logger, source: Optional["Source"], error: HTTPError
     ) -> Dict[int, str]:
         stream_slice = StreamHelper().get_stream_slice(stream)
-        workflow_runs_reasons_for_codes = super().reasons_for_unavailable_status_codes(stream, logger, source, error).copy()
+        reasons_for_codes = super().reasons_for_unavailable_status_codes(stream, logger, source, error).copy()
         server_error_msg = f"Syncing `{stream.name}` stream isn't available for repository `{stream_slice['repository']}`."
-        workflow_runs_reasons_for_codes[requests.codes.SERVER_ERROR] = server_error_msg
-        return workflow_runs_reasons_for_codes
+        reasons_for_codes[requests.codes.SERVER_ERROR] = server_error_msg
+        return reasons_for_codes
 
 
 class ProjectsAvailabilityStrategy(RepositoryBasedAvailabilityStrategy):
@@ -78,11 +78,12 @@ class ProjectsAvailabilityStrategy(RepositoryBasedAvailabilityStrategy):
         self, stream: Stream, logger: logging.Logger, source: Optional["Source"], error: HTTPError
     ) -> Dict[int, str]:
         stream_slice = StreamHelper().get_stream_slice(stream)
-        projects_reasons_for_codes = super().reasons_for_unavailable_status_codes(stream, logger, source, error).copy()
+        repository = stream_slice["repository"]
+        reasons_for_codes = super().reasons_for_unavailable_status_codes(stream, logger, source, error).copy()
 
         # Some repos don't have projects enabled and we we get "410 Client Error: Gone for
         # url: https://api.github.com/repos/xyz/projects?per_page=100" error.
-        gone_error_msg = f"`Projects` stream isn't available for repository `{stream_slice['repository']}`."
-        projects_reasons_for_codes[requests.codes.GONE] = gone_error_msg
+        gone_error_msg = f"`Projects` stream isn't available for repository `{repository}`."
+        reasons_for_codes[requests.codes.GONE] = gone_error_msg
 
-        return projects_reasons_for_codes
+        return reasons_for_codes
