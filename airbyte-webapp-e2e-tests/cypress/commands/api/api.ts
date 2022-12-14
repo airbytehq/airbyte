4758486458bafb3bf27178c16a55ee0a9d00aa96
Edmundo Ruiz Ghanem <edmundo@airbyte.io>
2022-12-14 17:15:35 -0500

    Add the ability to create and delete a connection

---
+++ airbyte-webapp-e2e-tests/cypress/commands/api/api.ts
@@ -0,0 +1,76 @@
+import { toPromise } from "../utils/promise";
+import {
+  Connection,
+  ConnectionCreateRequestBody,
+  ConnectionsList,
+  Destination,
+  DestinationsList,
+  Source,
+  SourceDiscoverSchema,
+  SourcesList,
+} from "./types";
+import { getWorkspaceId, setWorkspaceId } from "./workspace";
+
+const getApiUrl = (path: string): string => `http://localhost:8001/api/v1${path}`;
+
+export const requestWorkspaceId = () => {
+  if (!getWorkspaceId()) {
+    return toPromise(
+      cy.request("POST", getApiUrl("/workspaces/list")).then((response) => {
+        expect(response.status).to.eq(200);
+        const {
+          workspaces: [{ workspaceId }],
+        } = response.body;
+
+        setWorkspaceId(workspaceId as string);
+      })
+    );
+  }
+
+  return Promise.resolve();
+};
+
+const apiRequest = <T = void>(
+  method: Cypress.HttpMethod,
+  path: string,
+  payload?: Cypress.RequestBody,
+  expectedStatus = 200
+): Promise<T> =>
+  toPromise<T>(
+    cy.request(method, getApiUrl(path), payload).then((response) => {
+      expect(response.status).to.eq(expectedStatus, "response status");
+      return response.body;
+    })
+  );
+
+export const requestConnectionsList = () =>
+  apiRequest<ConnectionsList>("POST", "/connections/list", { workspaceId: getWorkspaceId() });
+
+export const requestCreateConnection = (body: ConnectionCreateRequestBody) =>
+  apiRequest<Connection>("POST", "/web_backend/connections/create", body);
+
+export const requestUpdateConnection = (body: Record<string, unknown>) =>
+  apiRequest<Connection>("POST", "/web_backend/connections/update", body);
+
+export const requestDeleteConnection = (connectionId: string) =>
+  apiRequest("POST", "/connections/delete", { connectionId }, 204);
+
+export const requestSourcesList = () =>
+  apiRequest<SourcesList>("POST", "/sources/list", { workspaceId: getWorkspaceId() });
+
+export const requestSourceDiscoverSchema = (sourceId: string) =>
+  apiRequest<SourceDiscoverSchema>("POST", "/sources/discover_schema", { sourceId, disable_cache: true });
+
+export const requestCreateSource = (body: Record<string, unknown>) =>
+  apiRequest<Source>("POST", "/sources/create", body);
+
+export const requestDeleteSource = (sourceId: string) => apiRequest("POST", "/sources/delete", { sourceId }, 204);
+
+export const requestDestinationsList = () =>
+  apiRequest<DestinationsList>("POST", "/destinations/list", { workspaceId: getWorkspaceId() });
+
+export const requestCreateDestination = (body: Record<string, unknown>) =>
+  apiRequest<Destination>("POST", "/destinations/create", body);
+
+export const requestDeleteDestination = (destinationId: string) =>
+  apiRequest("POST", "/destinations/delete", { destinationId }, 204);
--- airbyte-webapp-e2e-tests/cypress/integration/autoDetectSchema.spec.ts
+++ airbyte-webapp-e2e-tests/cypress/integration/autoDetectSchema.spec.ts
@@ -1,23 +1,59 @@
 import {
+  getConnectionCreateRequest,
+  getPostgresCreateDestinationBody,
   getPostgresCreateSourceBody,
+  requestCreateConnection,
+  requestCreateDestination,
   requestCreateSource,
+  requestDeleteConnection,
+  requestDeleteDestination,
   requestDeleteSource,
+  requestSourceDiscoverSchema,
   requestWorkspaceId,
 } from "commands/api";
+import { Connection, Destination, Source } from "commands/api/types";
 import { initialSetupCompleted } from "commands/workspaces";
 
 describe("Auto-detect schema changes", () => {
-  beforeEach(() => {
+  let source: Source;
+  let destination: Destination;
+  let connection: Connection;
+
+  beforeEach(async () => {
     initialSetupCompleted();
-    requestWorkspaceId();
+    await requestWorkspaceId();
+
+    const sourceRequestBody = getPostgresCreateSourceBody("Auto-detect schema Source");
+    const destinationRequestBody = getPostgresCreateDestinationBody("Auto-detect schema Destination");
+
+    source = await requestCreateSource(sourceRequestBody);
+    destination = await requestCreateDestination(destinationRequestBody);
+    const { catalog, catalogId } = await requestSourceDiscoverSchema(source.sourceId);
+
+    const connectionRequestBody = await getConnectionCreateRequest({
+      name: "Auto-detect schema test connection",
+      sourceId: source.sourceId,
+      destinationId: destination.destinationId,
+      syncCatalog: catalog,
+      sourceCatalogId: catalogId,
+    });
+
+    connection = await requestCreateConnection(connectionRequestBody);
+  });
+
+  afterEach(async () => {
+    if (connection) {
+      await requestDeleteConnection(connection.connectionId);
+    }
+    if (source) {
+      await requestDeleteSource(source.sourceId);
+    }
+    if (destination) {
+      await requestDeleteDestination(destination.destinationId);
+    }
   });
 
   it("creates a source", async () => {
-    const payload = getPostgresCreateSourceBody("Test source");
-
-    const { sourceId } = await requestCreateSource("Test source", payload);
-
-    expect(sourceId).to.be.a("string");
-    requestDeleteSource(sourceId);
+    expect(connection.connectionId).to.be.a("string");
   });
 });
