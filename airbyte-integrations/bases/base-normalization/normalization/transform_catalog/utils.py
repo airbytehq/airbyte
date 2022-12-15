6705501d4a26a2c957a4e0fc2b4aafb5decaa781
ievgeniit <etsybaev@gmail.com>
2022-12-15 10:18:40 +0100

    Updated utils.py to use Callable functions and get rid of property_type in is_number and is_bool functions

--- airbyte-integrations/bases/base-normalization/normalization/transform_catalog/utils.py
+++ airbyte-integrations/bases/base-normalization/normalization/transform_catalog/utils.py
@@ -1,8 +1,8 @@
 #
 # Copyright (c) 2022 Airbyte, Inc., all rights reserved.
 #
-import string
-from typing import Set, Union, Callable
+
+from typing import Callable, Set, Union
 
 from normalization import data_type
 from normalization.transform_catalog import dbt_macro
@@ -18,32 +18,24 @@ def remove_jinja(command: str) -> str:
 
 def is_type_included(definition: dict, is_type: Callable[[dict], bool]) -> bool:
     if data_type.ONE_OF_VAR_NAME in definition:
-        return bool(
-            any(
-                is_type(option)
-                for option in definition[data_type.ONE_OF_VAR_NAME]
-            )
-        )
+        return bool(any(is_type(option) for option in definition[data_type.ONE_OF_VAR_NAME]))
     else:
         return is_type(definition)
 
-def get_reftype_function(type: string) -> Callable[[dict], bool]:
+
+def get_reftype_function(type: str) -> Callable[[dict], bool]:
     def is_reftype(definition: dict) -> bool:
         return data_type.REF_TYPE_VAR_NAME in definition and type == definition[data_type.REF_TYPE_VAR_NAME]
+
     return is_reftype
 
 
 def is_string(definition: dict) -> bool:
     return is_type_included(definition, get_reftype_function(data_type.STRING_TYPE))
-    # if data_type.ONE_OF_VAR_NAME in definition:
-    #     return bool(any(option[data_type.REF_TYPE_VAR_NAME] == data_type.STRING_TYPE for option in definition[data_type.ONE_OF_VAR_NAME]))
-    # else:
-    #     return data_type.REF_TYPE_VAR_NAME in definition and data_type.STRING_TYPE == definition[data_type.REF_TYPE_VAR_NAME]
 
 
 def is_binary_datatype(definition: dict) -> bool:
     return is_type_included(definition, get_reftype_function(data_type.BINARY_DATA_TYPE))
-#     return data_type.REF_TYPE_VAR_NAME in definition and data_type.BINARY_DATA_TYPE == definition[data_type.REF_TYPE_VAR_NAME]
 
 
 def is_datetime(definition: dict) -> bool:
@@ -52,36 +44,14 @@ def is_datetime(definition: dict) -> bool:
 
 def is_datetime_without_timezone(definition: dict) -> bool:
     return is_type_included(definition, get_reftype_function(data_type.TIMESTAMP_WITHOUT_TIMEZONE_TYPE))
-    # if data_type.ONE_OF_VAR_NAME in definition:
-    #     return bool(
-    #         any(
-    #             option[data_type.REF_TYPE_VAR_NAME] == data_type.TIMESTAMP_WITHOUT_TIMEZONE_TYPE
-    #             for option in definition[data_type.ONE_OF_VAR_NAME]
-    #         )
-    #     )
-    # else:
-    #     return bool(data_type.TIMESTAMP_WITHOUT_TIMEZONE_TYPE == definition[data_type.REF_TYPE_VAR_NAME])
 
 
 def is_datetime_with_timezone(definition: dict) -> bool:
     return is_type_included(definition, get_reftype_function(data_type.TIMESTAMP_WITH_TIMEZONE_TYPE))
-    # if data_type.ONE_OF_VAR_NAME in definition:
-    #     return bool(
-    #         any(
-    #             option[data_type.REF_TYPE_VAR_NAME] == data_type.TIMESTAMP_WITH_TIMEZONE_TYPE
-    #             for option in definition[data_type.ONE_OF_VAR_NAME]
-    #         )
-    #     )
-    # else:
-    #     return bool(data_type.TIMESTAMP_WITH_TIMEZONE_TYPE == definition[data_type.REF_TYPE_VAR_NAME])
 
 
 def is_date(definition: dict) -> bool:
     return is_type_included(definition, get_reftype_function(data_type.DATE_TYPE))
-    # if data_type.ONE_OF_VAR_NAME in definition:
-    #     return bool(any(option[data_type.REF_TYPE_VAR_NAME] == data_type.DATE_TYPE for option in definition[data_type.ONE_OF_VAR_NAME]))
-    # else:
-    #     return bool(data_type.DATE_TYPE == definition[data_type.REF_TYPE_VAR_NAME])
 
 
 def is_time(definition: dict) -> bool:
@@ -90,28 +60,10 @@ def is_time(definition: dict) -> bool:
 
 def is_time_with_timezone(definition: dict) -> bool:
     return is_type_included(definition, get_reftype_function(data_type.TIME_WITH_TIME_ZONE_TYPE))
-    # if data_type.ONE_OF_VAR_NAME in definition:
-    #     return bool(
-    #         any(
-    #             option[data_type.REF_TYPE_VAR_NAME] == data_type.TIME_WITH_TIME_ZONE_TYPE
-    #             for option in definition[data_type.ONE_OF_VAR_NAME]
-    #         )
-    #     )
-    # else:
-    #     return bool(data_type.TIME_WITH_TIME_ZONE_TYPE == definition[data_type.REF_TYPE_VAR_NAME])
 
 
 def is_time_without_timezone(definition: dict) -> bool:
     return is_type_included(definition, get_reftype_function(data_type.TIME_WITHOUT_TIME_ZONE_TYPE))
-    # if data_type.ONE_OF_VAR_NAME in definition:
-    #     return bool(
-    #         any(
-    #             option[data_type.REF_TYPE_VAR_NAME] == data_type.TIME_WITHOUT_TIME_ZONE_TYPE
-    #             for option in definition[data_type.ONE_OF_VAR_NAME]
-    #         )
-    #     )
-    # else:
-    #     return bool(data_type.TIME_WITHOUT_TIME_ZONE_TYPE == definition[data_type.REF_TYPE_VAR_NAME])
 
 
 def is_number(definition: dict) -> bool:
@@ -119,10 +71,6 @@ def is_number(definition: dict) -> bool:
         # Handle union type, give priority to wider scope types
         return False
     return is_type_included(definition, get_reftype_function(data_type.NUMBER_TYPE))
-    # if data_type.ONE_OF_VAR_NAME in definition:
-    #     return bool(any(option[data_type.REF_TYPE_VAR_NAME] == data_type.NUMBER_TYPE for option in definition[data_type.ONE_OF_VAR_NAME]))
-    # else:
-    #     return data_type.REF_TYPE_VAR_NAME in definition and data_type.NUMBER_TYPE == definition[data_type.REF_TYPE_VAR_NAME]
 
 
 # this is obsolete type that will not be used in new datatypes
@@ -130,27 +78,15 @@ def is_big_integer(definition: dict) -> bool:
     return False
 
 
-def is_long(property_type, definition: dict) -> bool:
+def is_long(definition: dict) -> bool:
     return is_type_included(definition, get_reftype_function(data_type.INTEGER_TYPE))
-    # if data_type.ONE_OF_VAR_NAME in definition:
-    #     return bool(
-    #         any(option[data_type.REF_TYPE_VAR_NAME] == data_type.INTEGER_TYPE for option in property_type[data_type.ONE_OF_VAR_NAME])
-    #     )
-    # else:
-    #     return property_type == data_type.INTEGER_TYPE or data_type.INTEGER_TYPE in property_type
 
 
-def is_boolean(property_type, definition: dict) -> bool:
-    if is_string(definition) or is_number(definition) or is_big_integer(definition) or is_long(property_type, definition):
+def is_boolean(definition: dict) -> bool:
+    if is_string(definition) or is_number(definition) or is_big_integer(definition) or is_long(definition):
         # Handle union type, give priority to wider scope types
         return False
     return is_type_included(definition, get_reftype_function(data_type.BOOLEAN_TYPE))
-    # if data_type.ONE_OF_VAR_NAME in definition:
-    #     return bool(
-    #         any(option[data_type.REF_TYPE_VAR_NAME] == data_type.BOOLEAN_TYPE for option in property_type[data_type.ONE_OF_VAR_NAME])
-    #     )
-    # else:
-    #     return property_type == data_type.BOOLEAN_TYPE or data_type.BOOLEAN_TYPE in property_type
 
 
 def is_array(property_type) -> bool:
@@ -166,19 +102,13 @@ def is_airbyte_column(name: str) -> bool:
 
 
 def is_simple_property(definition: dict) -> bool:
-    if data_type.REF_TYPE_VAR_NAME not in definition and data_type.ONE_OF_VAR_NAME not in definition:
-        property_type = "object"
-    elif data_type.ONE_OF_VAR_NAME in definition and data_type.ONE_OF_VAR_NAME in definition:
-        property_type = data_type.ONE_OF_VAR_NAME
-    else:
-        property_type = definition[data_type.REF_TYPE_VAR_NAME]
     return (
         is_string(definition)
         or is_binary_datatype(definition)
         or is_big_integer(definition)
-        or is_long(property_type, definition)
+        or is_long(definition)
         or is_number(definition)
-        or is_boolean(property_type, definition)
+        or is_boolean(definition)
     )
 
 
