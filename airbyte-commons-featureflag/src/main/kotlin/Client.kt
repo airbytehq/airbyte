2be5cea1f7dbb93ff17ad654d2b7e322c6f50f8e
Cole Snodgrass <cole@airbyte.io>
2022-12-14 19:35:19 -0800

    feature-flag; wip

--- airbyte-commons-featureflag/src/main/kotlin/Client.kt
+++ airbyte-commons-featureflag/src/main/kotlin/Client.kt
@@ -4,38 +4,66 @@
 
 package io.airbyte.commons.featureflag
 
-import com.launchdarkly.sdk.*;
-import com.launchdarkly.sdk.server.*;
+import com.launchdarkly.sdk.ContextKind
+import com.launchdarkly.sdk.LDContext
+import com.launchdarkly.sdk.LDUser
+import com.launchdarkly.sdk.server.LDClient
+import java.nio.file.Path
 
-data class User(override val key: String) : Context {
-    override val kind: String = "user";
+sealed class Flag(internal val key: String) {
+    object FeatureOne : Flag("feature-one")
+    object FeatureTwo : Flag("feature-two")
 }
 
-interface Context {
+data class Workspace(override val key: String) : Context {
+    override val kind = "workspace"
+}
+
+data class User(override val key: String) : Context {
+    override val kind = "user"
+}
+
+sealed interface Context {
     val kind: String
     val key: String
 }
 
 sealed interface Client {
-    fun bool(key: String, ctx: Context, default: Boolean): Boolean
-    fun int(key: String, ctx: Context, default: Int): Int
-    fun string(key: String, ctx: Context, default: String): String
+    fun bool(key: Flag, ctx: Context, default: Boolean): Boolean
 }
 
-class LD(private val key: String) : Client {
-    private val ldClient = LDClient(key)
+/**
+ * LaunchDarkly v5 version
+ *
+ * LaunchDarkly v6 introduces LDContext which replaces the LDUser class,
+ * however this is only available to early-access users accounts currently.
+ *
+ * Once v6 is GA, this method would be removed and replaced with toLDContext.
+ */
+private fun Context.toLDUser(): LDUser {
+    return LDUser(key)
+}
 
-    override
+/**
+ * LaunchDarkly v6 version
+ *
+ * Replaces toLDUser once LaunchDarkly v6 is GA.
+ */
+private fun Context.toLDContext(): LDContext {
+    return LDContext.create(ContextKind.of(kind), key)
+}
 
-    fun bool(key: String, ctx: Context, default: Boolean): Boolean {
+class LD(sdkKey: String) : Client {
+    private val ldClient: LDClient = LDClient(sdkKey)
+
+    override fun bool(flag: Flag, ctx: Context, default: Boolean): Boolean {
+        return ldClient.boolVariation(flag.key, ctx.toLDUser(), default)
+    }
+}
+
+class OSS(config: Path) : Client {
+    override fun bool(key: Flag, ctx: Context, default: Boolean): Boolean {
         TODO("Not yet implemented")
     }
 
-    override fun int(key: String, ctx: Context, default: Int): Int {
-        TODO("Not yet implemented")
-    }
-
-    override fun string(key: String, ctx: Context, default: String): String {
-        TODO("Not yet implemented")
-    }
 }
