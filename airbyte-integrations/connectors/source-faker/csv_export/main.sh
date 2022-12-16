333eae077c282be3544cc320c8ffcb0200324793
evantahler <evan@airbyte.io>
2022-12-16 08:40:13 -0800

    don't do a final pipe to jq or you will run out or ram

--- airbyte-integrations/connectors/source-faker/csv_export/main.sh
+++ airbyte-integrations/connectors/source-faker/csv_export/main.sh
@@ -6,5 +6,4 @@ cd ".."
 mkdir -p /tmp/csv
 
 python main.py read --config secrets/config.json --catalog integration_tests/configured_catalog.json --state secrets/state.json \
-  | tee >(./csv_export/purchases.sh) >(./csv_export/products.sh) >(./csv_export/users.sh) \
-  | jq -c
+  | tee >(./csv_export/purchases.sh) >(./csv_export/products.sh) >(./csv_export/users.sh)
