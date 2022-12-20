abf9b49a2ad22fcbf3f4d1be5f2636285f744f4c
evantahler <evan@airbyte.io>
2022-12-20 15:50:30 -0800

    Faker is 250% faster

--- airbyte-integrations/connectors/source-faker/source_faker/streams.py
+++ airbyte-integrations/connectors/source-faker/source_faker/streams.py
@@ -4,21 +4,38 @@
 
 import datetime
 import os
-from typing import Any, Dict, Iterable, Mapping, Optional
 
-from airbyte_cdk.models import AirbyteEstimateTraceMessage, AirbyteTraceMessage, EstimateType, TraceType
+from multiprocessing import Pool, current_process
+# from multiprocessing.pool import ThreadPool
+
+from typing import Any, Dict, Iterable, Mapping, Optional, List
+
+from airbyte_cdk.models import AirbyteMessage, AirbyteEstimateTraceMessage, Type,  AirbyteTraceMessage, AirbyteRecordMessage, EstimateType, TraceType
 from airbyte_cdk.sources.streams import IncrementalMixin, Stream
 from mimesis import Datetime, Numeric, Person
 from mimesis.locales import Locale
 
 from .utils import format_airbyte_time, read_json
 
+class FakerMultithreaded:
+    def worker_init(self):
+        """For the workers, we want a unique instance of the faker packages with their own seeds to differentiate the generated responses"""
+        global person
+        global dt
+        global numeric
+        seed_with_offset = self.seed
+        if self.seed != None:
+            seed_with_offset = self.seed + current_process()._identity[0]
+        person = Person(locale=Locale.EN, seed=seed_with_offset)
+        dt = Datetime(seed=seed_with_offset)
+        numeric = Numeric(seed=seed_with_offset)
+
 
 class Products(Stream, IncrementalMixin):
     primary_key = None
     cursor_field = "id"
 
-    def __init__(self, count: int, seed: int, records_per_sync: int, records_per_slice: int, **kwargs):
+    def __init__(self, count: int, seed: int, threads:int, records_per_sync: int, records_per_slice: int, **kwargs):
         super().__init__(**kwargs)
         self.seed = seed
         self.records_per_sync = records_per_sync
@@ -60,18 +77,17 @@ class Products(Stream, IncrementalMixin):
         self.state = {self.cursor_field: total_records, "seed": self.seed}
 
 
-class Users(Stream, IncrementalMixin):
+class Users(Stream, IncrementalMixin, FakerMultithreaded):
     primary_key = None
     cursor_field = "id"
 
-    def __init__(self, count: int, seed: int, records_per_sync: int, records_per_slice: int, **kwargs):
+    def __init__(self, count: int, seed: int, threads:int, records_per_sync: int, records_per_slice: int, **kwargs):
         super().__init__(**kwargs)
         self.count = count
         self.seed = seed
         self.records_per_sync = records_per_sync
         self.records_per_slice = records_per_slice
-        self.person = Person(locale=Locale.EN, seed=self.seed)
-        self.dt = Datetime(seed=self.seed)
+        self.threads = threads
 
     @property
     def state_checkpoint_interval(self) -> Optional[int]:
@@ -89,66 +105,68 @@ class Users(Stream, IncrementalMixin):
         self._state = value
 
     def generate_user(self, user_id: int):
-        time_a = self.dt.datetime()
-        time_b = self.dt.datetime()
+        time_a = dt.datetime()
+        time_b = dt.datetime()
 
         profile = {
             "id": user_id + 1,
             "created_at": format_airbyte_time(time_a if time_a <= time_b else time_b),
             "updated_at": format_airbyte_time(time_a if time_a > time_b else time_b),
-            "name": self.person.name(),
-            "title": self.person.title(),
-            "age": self.person.age(),
-            "email": self.person.email(),
-            "telephone": self.person.telephone(),
-            "gender": self.person.gender(),
-            "language": self.person.language(),
-            "academic_degree": self.person.academic_degree(),
-            "nationality": self.person.nationality(),
-            "occupation": self.person.occupation(),
-            "height": self.person.height(),
-            "blood_type": self.person.blood_type(),
-            "weight": self.person.weight(),
+            "name": person.name(),
+            "title": person.title(),
+            "age": person.age(),
+            "email": person.email(),
+            "telephone": person.telephone(),
+            "gender": person.gender(),
+            "language": person.language(),
+            "academic_degree": person.academic_degree(),
+            "nationality": person.nationality(),
+            "occupation": person.occupation(),
+            "height": person.height(),
+            "blood_type": person.blood_type(),
+            "weight": person.weight(),
         }
 
         while not profile["created_at"]:
-            profile["created_at"] = format_airbyte_time(self.dt.datetime())
+            profile["created_at"] = format_airbyte_time(dt.datetime())
 
         if not profile["updated_at"]:
             profile["updated_at"] = profile["created_at"] + 1
 
-        return profile
+        record = AirbyteRecordMessage(stream=self.name, data=profile, emitted_at=now_millis())
+        return AirbyteMessageWithCachedJSON(type=Type.RECORD, record=record)
 
     def read_records(self, **kwargs) -> Iterable[Mapping[str, Any]]:
         total_records = self.state[self.cursor_field] if self.cursor_field in self.state else 0
         records_in_sync = 0
-        records_in_slice = 0
 
         median_record_byte_size = 450
         yield generate_estimate(self.name, self.count - total_records, median_record_byte_size)
 
-        for i in range(total_records, self.count):
-            user = self.generate_user(i)
-            yield user
-            total_records += 1
-            records_in_sync += 1
-            records_in_slice += 1
+        running = True
+        with Pool(initializer=self.worker_init, processes=self.threads) as pool:
+            while running and records_in_sync < self.count:
+                records_remaining_this_loop = min(self.records_per_slice, (self.count - total_records))
+                if records_remaining_this_loop == 0:
+                    break
+                users = pool.map(self.generate_user, range(total_records, total_records + records_remaining_this_loop))
+                for user in users:
+                    total_records += 1
+                    records_in_sync += 1
+                    yield user
 
-            if records_in_slice >= self.records_per_slice:
+                    if records_in_sync == self.records_per_sync:
+                        running = False
+                        break
+
                 self.state = {self.cursor_field: total_records, "seed": self.seed}
-                records_in_slice = 0
-
-            if records_in_sync == self.records_per_sync:
-                break
-
-        self.state = {self.cursor_field: total_records, "seed": self.seed}
 
 
-class Purchases(Stream, IncrementalMixin):
+class Purchases(Stream, IncrementalMixin, FakerMultithreaded):
     primary_key = None
     cursor_field = "id"
 
-    def __init__(self, count: int, seed: int, records_per_sync: int, records_per_slice: int, **kwargs):
+    def __init__(self, count: int, seed: int, threads:int, records_per_sync: int, records_per_slice: int, **kwargs):
         super().__init__(**kwargs)
         self.count = count
         self.seed = seed
@@ -156,6 +174,7 @@ class Purchases(Stream, IncrementalMixin):
         self.records_per_slice = records_per_slice
         self.dt = Datetime(seed=self.seed)
         self.numeric = Numeric(seed=self.seed)
+        self.threads = threads
 
     @property
     def state_checkpoint_interval(self) -> Optional[int]:
@@ -183,72 +202,100 @@ class Purchases(Stream, IncrementalMixin):
         random_date = start_date + datetime.timedelta(days=random_number_of_days)
         return random_date
 
-    def generate_purchases(self, user_id: int, purchases_count: int) -> list[Dict]:
+    def generate_purchases(self, user_id: int) -> list[Dict]:
+        """Because we are doing this work in parallel processes, we need a deterministic way to know what a purchase's ID should be given on the input of a user_id"""
+        """tldr; Every 10 user_ids produce 10 purchases.  User ID x5 has no purchases, User ID mod x7 has 2, and everyone else has 1"""
+
         purchases: list[Dict] = []
-        purchase_percent_remaining = 70  # ~30% of people will have no purchases
+        last_user_id_digit = int(repr(user_id)[-1])
+        purchase_count = 1
+        id_offset = 0
+        if last_user_id_digit - 1 == 5:
+            purchase_count = 0
+        elif last_user_id_digit - 1 == 6:
+            id_offset = 1
+        elif last_user_id_digit - 1 == 7:
+            id_offset = 1
+            purchase_count = 2
+
         total_products = 100
-        purchase_percent_remaining = purchase_percent_remaining - self.numeric.integer_number(1, 100)
         i = 0
 
-        time_a = self.dt.datetime()
-        time_b = self.dt.datetime()
-        created_at = time_a if time_a <= time_b else time_b
-
-        while purchase_percent_remaining > 0:
-            id = purchases_count + i + 1
-            product_id = self.numeric.integer_number(1, total_products)
+        while purchase_count > 0:
+            id = user_id + i + 1 - id_offset
+            time_a = dt.datetime()
+            time_b = dt.datetime()
+            created_at = time_a if time_a <= time_b else time_b
+            product_id = numeric.integer_number(1, total_products)
             added_to_cart_at = self.random_date_in_range(created_at)
             purchased_at = (
                 self.random_date_in_range(added_to_cart_at)
-                if added_to_cart_at is not None and self.numeric.integer_number(1, 100) <= 70
+                if added_to_cart_at is not None and numeric.integer_number(1, 100) <= 70
                 else None
             )  # 70% likely to purchase the item in the cart
             returned_at = (
-                self.random_date_in_range(purchased_at) if purchased_at is not None and self.numeric.integer_number(1, 100) <= 15 else None
+                self.random_date_in_range(purchased_at) if purchased_at is not None and numeric.integer_number(1, 100) <= 15 else None
             )  # 15% likely to return the item
 
             purchase = {
                 "id": id,
                 "product_id": product_id,
-                "user_id": user_id,
+                "user_id": user_id + 1,
                 "added_to_cart_at": format_airbyte_time(added_to_cart_at) if added_to_cart_at is not None else None,
                 "purchased_at": format_airbyte_time(purchased_at) if purchased_at is not None else None,
                 "returned_at": format_airbyte_time(returned_at) if returned_at is not None else None,
             }
-            purchases.append(purchase)
 
-            purchase_percent_remaining = purchase_percent_remaining - self.numeric.integer_number(1, 100)
+            record = AirbyteRecordMessage(stream=self.name, data=purchase, emitted_at=now_millis())
+            message = AirbyteMessageWithCachedJSON(type=Type.RECORD, record=record)
+            purchases.append(message)
+
+            purchase_count = purchase_count - 1
             i += 1
+
         return purchases
 
     def read_records(self, **kwargs) -> Iterable[Mapping[str, Any]]:
         total_purchase_records = self.state[self.cursor_field] if self.cursor_field in self.state else 0
         total_user_records = self.state["user_id"] if "user_id" in self.state else 0
         user_records_in_sync = 0
-        user_records_in_slice = 0
 
         median_record_byte_size = 230
         yield generate_estimate(
             self.name, (self.count - total_user_records) * 1.3, median_record_byte_size
         )  # a fuzzy guess, some users have purchases, some don't
 
-        for i in range(total_user_records, self.count):
-            purchases = self.generate_purchases(i + 1, total_purchase_records)
-            for purchase in purchases:
-                total_purchase_records += 1
-                yield purchase
-            total_user_records += 1
-            user_records_in_sync += 1
-            user_records_in_slice += 1
+        running = True
+        with Pool(initializer=self.worker_init, processes=self.threads) as pool:
+            while running and total_user_records < self.count:
+                records_remaining_this_loop = min(self.records_per_slice, (self.count - user_records_in_sync))
+                if records_remaining_this_loop == 0:
+                    break
+                carts = pool.map(self.generate_purchases, range(total_user_records, total_user_records + records_remaining_this_loop))
+                for purchases in carts:
+                    for purchase in purchases:
+                        yield purchase
 
-            if user_records_in_slice >= self.records_per_slice:
+                    total_user_records += 1
+                    user_records_in_sync += 1
+
+                    if user_records_in_sync == self.records_per_sync:
+                        running = False
+                        break
+
                 self.state = {self.cursor_field: total_purchase_records, "user_id": total_user_records, "seed": self.seed}
-                user_records_in_slice = 0
 
-            if user_records_in_sync == self.records_per_sync:
-                break
+def now_millis():
+    return int(datetime.datetime.now().timestamp() * 1000)
 
-        self.state = {self.cursor_field: total_purchase_records, "user_id": total_user_records, "seed": self.seed}
+class AirbyteMessageWithCachedJSON(AirbyteMessage):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self._json = self.json(exclude_unset=True)
+        self.json = self.get_json
+
+    def get_json(self, **kwargs):
+        return self._json
 
 
 def generate_estimate(stream_name: str, total: int, bytes_per_row: int):
