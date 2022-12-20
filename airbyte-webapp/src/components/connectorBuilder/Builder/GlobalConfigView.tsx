a17ab05a2a4fb5a6a61408878b0ad1c33ac2ef3b
Joe Reuter <joe@airbyte.io>
2022-12-20 14:58:11 +0100

    session token and oauth authentication

--- airbyte-webapp/src/components/connectorBuilder/Builder/GlobalConfigView.tsx
+++ airbyte-webapp/src/components/connectorBuilder/Builder/GlobalConfigView.tsx
@@ -1,12 +1,11 @@
 import { useIntl } from "react-intl";
 
+import { AuthenticationSection } from "./AuthenticationSection";
 import { BuilderCard } from "./BuilderCard";
 import { BuilderConfigView } from "./BuilderConfigView";
 import { BuilderField } from "./BuilderField";
-import { BuilderOneOf } from "./BuilderOneOf";
 import { BuilderTitle } from "./BuilderTitle";
 import styles from "./GlobalConfigView.module.scss";
-import { UserInputField } from "./UserInputField";
 
 export const GlobalConfigView: React.FC = () => {
   const { formatMessage } = useIntl();
@@ -18,70 +17,7 @@ export const GlobalConfigView: React.FC = () => {
       <BuilderCard className={styles.content}>
         <BuilderField type="string" path="global.urlBase" label="API URL" tooltip="Base URL of the source API" />
       </BuilderCard>
-      <BuilderCard>
-        <BuilderOneOf
-          path="global.authenticator"
-          label="Authentication"
-          tooltip="Authentication method to use for requests send to the API"
-          options={[
-            { label: "No Auth", typeValue: "NoAuth" },
-            {
-              label: "API Key",
-              typeValue: "ApiKeyAuthenticator",
-              default: {
-                api_token: "{{ config['api_key'] }}",
-              },
-              children: (
-                <>
-                  <BuilderField
-                    type="string"
-                    path="global.authenticator.header"
-                    label="Header"
-                    tooltip="HTTP header which should be set to the API Key"
-                  />
-                  <UserInputField
-                    label="API Key"
-                    tooltip="The API key issued by the service. Fill it in in the user inputs"
-                  />
-                </>
-              ),
-            },
-            {
-              label: "Bearer",
-              typeValue: "BearerAuthenticator",
-              default: {
-                api_token: "{{ config['api_key'] }}",
-              },
-              children: (
-                <UserInputField
-                  label="API Key"
-                  tooltip="The API key issued by the service. Fill it in in the user inputs"
-                />
-              ),
-            },
-            {
-              label: "Basic HTTP",
-              typeValue: "BasicHttpAuthenticator",
-              default: {
-                username: "{{ config['username'] }}",
-                password: "{{ config['password'] }}",
-              },
-              children: (
-                <>
-                  <UserInputField
-                    label="Username"
-                    tooltip="The username for the login. Fill it in in the user inputs"
-                  />
-                  <UserInputField
-                    label="Password"
-                    tooltip="The password for the login. Fill it in in the user inputs"
-                  />
-                </>
-              ),
-            },
-          ]}
-        />
-      </BuilderCard>
+      <AuthenticationSection />
     </BuilderConfigView>
   );
 };
