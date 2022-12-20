c71e3be49836137788752282a8a1b6216e9818f5
Sam Stoelinga <sammiestoel@gmail.com>
2022-12-20 09:30:15 -0800

    change to MutableMapping since we use del

--- airbyte-integrations/connectors/destination-weaviate/destination_weaviate/client.py
+++ airbyte-integrations/connectors/destination-weaviate/destination_weaviate/client.py
@@ -5,7 +5,7 @@
 import uuid
 import logging
 import json
-from typing import Any, Mapping
+from typing import Any, Mapping, MutableMapping
 
 import weaviate
 from .utils import generate_id, parse_id_schema, parse_vectors, stream_to_class_name
@@ -20,7 +20,7 @@ class Client:
         self.vectors = parse_vectors(config.get("vectors"))
         self.id_schema = parse_id_schema(config.get("id_schema"))
 
-    def queue_write_operation(self, stream_name: str, record: Mapping):
+    def queue_write_operation(self, stream_name: str, record: MutableMapping):
         # TODO need to handle case where original DB ID is not a UUID
         if self.id_schema.get(stream_name, "") in record:
             id_field_name = self.id_schema.get(stream_name, "")
