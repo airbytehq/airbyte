806b7a7ccc2fd261448d53e8d7bb0e5c6107dcb5
Tim Roes <tim@airbyte.io>
2022-12-22 21:41:06 +0100

    Allow feature overwriting via .features.dev

--- airbyte-webapp/scripts/dev-overwrites.js
+++ airbyte-webapp/scripts/dev-overwrites.js
@@ -20,7 +20,7 @@ function parseFromString(value) {
 const EXPERIMENTS_FILE = path.resolve(__dirname, "../.experiments.dev");
 
 if (fs.existsSync(EXPERIMENTS_FILE)) {
-  console.log("Overwriting experiments from .experiments.dev ...");
+  console.log("\nOverwriting experiments from .experiments.dev ...");
   const rawOverwrites = dotenv.parse(fs.readFileSync(EXPERIMENTS_FILE));
   const overwrites = Object.fromEntries(
     Object.entries(rawOverwrites).map(([key, value]) => {
@@ -32,4 +32,15 @@ if (fs.existsSync(EXPERIMENTS_FILE)) {
     console.log(`Overwriting experiments with the following values:\n\n${JSON.stringify(overwrites, null, 2)}`);
     process.env.REACT_APP_EXPERIMENT_OVERWRITES = JSON.stringify(overwrites);
   }
+}
+
+const FEATURES_FILE = path.resolve(__dirname, "../.features.dev");
+
+if (fs.existsSync(FEATURES_FILE)) {
+  console.log("\nOverwriting feature states from .features.dev ...");
+  const overwrites = dotenv.parse(fs.readFileSync(FEATURES_FILE));
+  Object.entries(overwrites).forEach(([key, value]) => {
+    console.log(`Overwrite feature ${key} with value ${value}`);
+    process.env[`REACT_APP_FEATURE_${key}`] = value;
+  });
 }
