e3a003795ce0434272c78c6c5cf1eae97aab0c05
Sergey Chvalyuk <grubberr@gmail.com>
2022-12-23 17:35:30 +0000

    fix next_page_token
    Signed-off-by: Sergey Chvalyuk <grubberr@gmail.com>

--- airbyte-integrations/connectors/source-jira/source_jira/streams.py
+++ airbyte-integrations/connectors/source-jira/source_jira/streams.py
@@ -48,9 +48,10 @@ class JiraStream(HttpStream, ABC):
             startAt = response_json.get("startAt")
             if startAt is not None:
                 startAt += response_json["maxResults"]
-                if "total" in response_json and startAt < response_json["total"]:
-                    return {"startAt": startAt}
-                if "values" in response_json and len(response_json["values"]) == self.page_size:
+                if "total" in response_json:
+                    if startAt < response_json["total"]:
+                        return {"startAt": startAt}
+                elif len(response_json["values"]) == self.page_size:
                     return {"startAt": startAt}
         elif isinstance(response_json, list):
             if len(response_json) == self.page_size:
