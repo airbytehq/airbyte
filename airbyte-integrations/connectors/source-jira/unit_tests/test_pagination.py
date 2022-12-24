85bac3ded2b01b9edc848a28dce2bd773b609f97
Sergey Chvalyuk <grubberr@gmail.com>
2022-12-24 08:59:29 +0000

    test_pagination_issues added
    Signed-off-by: Sergey Chvalyuk <grubberr@gmail.com>

--- airbyte-integrations/connectors/source-jira/unit_tests/test_pagination.py
+++ airbyte-integrations/connectors/source-jira/unit_tests/test_pagination.py
@@ -7,7 +7,7 @@ import json
 from http import HTTPStatus
 
 import responses
-from source_jira.streams import Projects
+from source_jira.streams import Issues, Projects
 from source_jira.utils import read_full_refresh
 
 
@@ -15,9 +15,9 @@ from source_jira.utils import read_full_refresh
 def test_pagination_projects():
     domain = "domain.com"
     responses_json = [
-        (HTTPStatus.OK, {}, json.dumps({"startAt": 0, "maxResults": 2, "total": 6, "isLast": False, "values": [{"self": "p1"}, {"self": "p2"}]})),
-        (HTTPStatus.OK, {}, json.dumps({"startAt": 2, "maxResults": 2, "total": 6, "isLast": False, "values": [{"self": "p3"}, {"self": "p4"}]})),
-        (HTTPStatus.OK, {}, json.dumps({"startAt": 4, "maxResults": 2, "total": 6, "isLast": True, "values": [{"self": "p5"}, {"self": "p6"}]})),
+        (HTTPStatus.OK, {}, json.dumps({"startAt": 0, "maxResults": 2, "total": 6, "isLast": False, "values": [{"id": "1"}, {"id": "2"}]})),
+        (HTTPStatus.OK, {}, json.dumps({"startAt": 2, "maxResults": 2, "total": 6, "isLast": False, "values": [{"id": "3"}, {"id": "4"}]})),
+        (HTTPStatus.OK, {}, json.dumps({"startAt": 4, "maxResults": 2, "total": 6, "isLast": True, "values": [{"id": "5"}, {"id": "6"}]})),
     ]
 
     responses.add_callback(
@@ -29,4 +29,33 @@ def test_pagination_projects():
 
     stream = Projects(authenticator=None, domain=domain, projects=[])
     records = list(read_full_refresh(stream))
-    assert records == [{"self": "p1"}, {"self": "p2"}, {"self": "p3"}, {"self": "p4"}, {"self": "p5"}, {"self": "p6"}]
+    assert records == [{"id": "1"}, {"id": "2"}, {"id": "3"}, {"id": "4"}, {"id": "5"}, {"id": "6"}]
+
+
+@responses.activate
+def test_pagination_issues():
+    domain = "domain.com"
+    responses_json = [
+        (HTTPStatus.OK, {}, json.dumps({"startAt": 0, "maxResults": 2, "total": 6, "issues": [{"id": "1", "updated": "2022-01-01"}, {"id": "2", "updated": "2022-01-01"}]})),
+        (HTTPStatus.OK, {}, json.dumps({"startAt": 2, "maxResults": 2, "total": 6, "issues": [{"id": "3", "updated": "2022-01-01"}, {"id": "4", "updated": "2022-01-01"}]})),
+        (HTTPStatus.OK, {}, json.dumps({"startAt": 4, "maxResults": 2, "total": 6, "issues": [{"id": "5", "updated": "2022-01-01"}, {"id": "6", "updated": "2022-01-01"}]})),
+    ]
+
+    responses.add_callback(
+        responses.GET,
+        f"https://{domain}/rest/api/3/search",
+        callback=lambda request: responses_json.pop(0),
+        content_type="application/json",
+    )
+
+    stream = Issues(authenticator=None, domain=domain, projects=[])
+    stream.transform = lambda record, **kwargs: record
+    records = list(read_full_refresh(stream))
+    assert records == [
+        {"id": "1", "updated": "2022-01-01"},
+        {"id": "2", "updated": "2022-01-01"},
+        {"id": "3", "updated": "2022-01-01"},
+        {"id": "4", "updated": "2022-01-01"},
+        {"id": "5", "updated": "2022-01-01"},
+        {"id": "6", "updated": "2022-01-01"}
+    ]
