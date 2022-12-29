4105cf767e9266e3621f9bacdef4c3eb2906d644
Oleksandr Bazarnov <oleksandr.bazarnov@globallogic.com>
2022-12-29 17:36:36 +0200

    added oauth for connector side

--- airbyte-integrations/connectors/source-airtable/source_airtable/source.py
+++ airbyte-integrations/connectors/source-airtable/source_airtable/source.py
@@ -10,8 +10,8 @@ from airbyte_cdk.logger import AirbyteLogger
 from airbyte_cdk.models import AirbyteCatalog
 from airbyte_cdk.sources import AbstractSource
 from airbyte_cdk.sources.streams import Stream
-from airbyte_cdk.sources.streams.http.requests_native_auth import TokenAuthenticator
 
+from .auth import AirtableAuth
 from .helpers import Helpers
 from .streams import AirtableBases, AirtableStream, AirtableTables
 
@@ -20,15 +20,16 @@ class SourceAirtable(AbstractSource):
 
     logger: logging.Logger = logging.getLogger("airbyte")
     streams_catalog: Iterable[Mapping[str, Any]] = []
+    _auth: AirtableAuth = None
 
     def check_connection(self, logger: AirbyteLogger, config: Mapping[str, Any]) -> Tuple[bool, Any]:
-        auth = TokenAuthenticator(token=config["api_key"])
+        auth = AirtableAuth(config)
         try:
             # try reading first table from each base, to check the connectivity,
             for base in AirtableBases(authenticator=auth).read_records(sync_mode=None):
                 base_id = base.get("id")
                 base_name = base.get("name")
-                self.logger.info(f"Reading first table info for base: {base_name}")
+                logger.info(f"Reading first table info for base: {base_name}")
                 next(AirtableTables(base_id=base_id, authenticator=auth).read_records(sync_mode=None))
             return True, None
         except Exception as e:
@@ -54,7 +55,9 @@ class SourceAirtable(AbstractSource):
 
         Retrieve: Bases, Tables from each Base, generate JSON Schema for each table.
         """
-        auth = TokenAuthenticator(token=config["api_key"])
+        # re-use the authenticator while generating streams instances
+        # or create new auth instance otherwise
+        auth = self._auth if self._auth else AirtableAuth(config)
 
         # list all bases available for authenticated account
         for base in AirtableBases(authenticator=auth).read_records(sync_mode=None):
@@ -64,16 +67,17 @@ class SourceAirtable(AbstractSource):
                 base_name=Helpers.clean_name(base.get("name")),
                 base_tables=list(AirtableTables(base_id=base.get("id"), authenticator=auth).read_records(sync_mode=None)),
             )
+
         return AirbyteCatalog(streams=[stream["stream"] for stream in self.streams_catalog])
 
     def streams(self, config: Mapping[str, Any]) -> List[Stream]:
+        self._auth = AirtableAuth(config)
         if not self.streams_catalog:
-            self.discover(None, config)
-
+            self.discover(logger=None, config=config)
         for stream in self.streams_catalog:
             yield AirtableStream(
                 stream_path=stream["stream_path"],
                 stream_name=stream["stream"].name,
                 stream_schema=stream["stream"].json_schema,
-                authenticator=TokenAuthenticator(token=config["api_key"]),
+                authenticator=self._auth,
             )
