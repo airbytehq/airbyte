15689adcdb5ee13000127a42614829e3a56dda88
Pedro S. Lopez <pedroslopez@me.com>
2023-01-03 09:25:44 -0400

    fix(oauth): dont convert config values to string when building consent url (#20932)
    * fix: don't convert config values to strings
    
    * tests
    
    * add note

--- airbyte-server/src/test/java/io/airbyte/server/handlers/OAuthHandlerTest.java
+++ airbyte-server/src/test/java/io/airbyte/server/handlers/OAuthHandlerTest.java
@@ -206,7 +206,8 @@ class OAuthHandlerTest {
         """
         {
           "testMask": "**********",
-          "testNotMask": "this"
+          "testNotMask": "this",
+          "testOtherType": true
         }
         """);
 
@@ -214,7 +215,8 @@ class OAuthHandlerTest {
         """
         {
           "testMask": "mask",
-          "testNotMask": "notThis"
+          "testNotMask": "notThis",
+          "testOtherType": true
         }
         """);
 
@@ -222,7 +224,8 @@ class OAuthHandlerTest {
         """
         {
           "testMask": "mask",
-          "testNotMask": "this"
+          "testNotMask": "this",
+          "testOtherType": true
         }
         """);
 
