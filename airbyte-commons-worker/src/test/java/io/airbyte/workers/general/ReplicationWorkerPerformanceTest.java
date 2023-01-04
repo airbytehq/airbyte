0004daad207266d38924714634e49f81801c9dd8
Davin Chia <davinchia@gmail.com>
2023-01-04 14:06:42 -0800

    Update comments.

--- airbyte-commons-worker/src/test/java/io/airbyte/workers/general/ReplicationWorkerPerformanceTest.java
+++ airbyte-commons-worker/src/test/java/io/airbyte/workers/general/ReplicationWorkerPerformanceTest.java
@@ -4,6 +4,7 @@
 
 package io.airbyte.workers.general;
 
+import io.airbyte.config.JobSyncConfig.NamespaceDefinitionType;
 import io.airbyte.config.ReplicationOutput;
 import io.airbyte.config.StandardSyncInput;
 import io.airbyte.metrics.lib.NotImplementedMetricClient;
@@ -17,6 +18,7 @@ import io.airbyte.protocol.models.SyncMode;
 import io.airbyte.workers.RecordSchemaValidator;
 import io.airbyte.workers.WorkerMetricReporter;
 import io.airbyte.workers.exception.WorkerException;
+import io.airbyte.workers.internal.NamespacingMapper;
 import io.airbyte.workers.internal.book_keeping.AirbyteMessageTracker;
 import java.io.IOException;
 import java.nio.file.Path;
@@ -25,31 +27,57 @@ import java.util.Map;
 import java.util.concurrent.atomic.AtomicReference;
 import lombok.extern.slf4j.Slf4j;
 import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Fork;
+import org.openjdk.jmh.annotations.Measurement;
+import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.Warmup;
 
 @Slf4j
 public class ReplicationWorkerPerformanceTest {
 
-  // write better comment
-  @Benchmark @AirbyteBenchmark
-  public void benchmarkOrchestrator() throws InterruptedException {
-    var perSource = new LimitedAirbyteSource();
-    var perDestination = new EmptyAirbyteDestination();
-    var messageTracker = new AirbyteMessageTracker();
-    var metricReporter = new WorkerMetricReporter(new NotImplementedMetricClient(), "test-image:0.01");
-    var mapper = new StubAirbyteMapper();
-    var validator = new RecordSchemaValidator(Map.of(
+  /**
+   * Hook up the DefaultReplicationWorker to a test harness with an insanely quick Source
+   * {@link LimitedAirbyteSource} and Destination {@link EmptyAirbyteDestination}.
+   * <p>
+   * Harness uses Java Micro Benchmark to run the E2E sync a configured number of times. It then
+   * reports a time distribution for the time taken to run the E2E sync.
+   * <p>
+   * Because the reported time does not explicitly include throughput numbers, throughput logging has
+   * been added. This class is intended to help devs understand the impact of changes on throughput.
+   * <p>
+   * To run this, simply run the main method and run the logs.
+   */
+  @Benchmark
+  // SampleTime = the time taken to run the benchmarked method. Use this because we only care about
+  // the time taken to sync the entire dataset.
+  @BenchmarkMode(Mode.SampleTime)
+  // Warming up the JVM stabilises results however takes longer. Skip this for now since we don't need
+  // that fine a result.
+  @Warmup(iterations = 0)
+  // How many runs to do.
+  @Fork(value = 1)
+  // Within each run, how many iterations to do.
+  @Measurement(iterations = 2)
+  public void executeOneSync() throws InterruptedException {
+    final var perSource = new LimitedAirbyteSource();
+    final var perDestination = new EmptyAirbyteDestination();
+    final var messageTracker = new AirbyteMessageTracker();
+    final var metricReporter = new WorkerMetricReporter(new NotImplementedMetricClient(), "test-image:0.01");
+    final var dstNamespaceMapper = new NamespacingMapper(NamespaceDefinitionType.DESTINATION, "", "");
+    final var validator = new RecordSchemaValidator(Map.of(
         new AirbyteStreamNameNamespacePair("s1", null),
         CatalogHelpers.fieldsToJsonSchema(io.airbyte.protocol.models.Field.of("data", JsonSchemaType.STRING))));
 
-    var worker = new DefaultReplicationWorker("1", 0,
+    final var worker = new DefaultReplicationWorker("1", 0,
         perSource,
-        mapper,
+        dstNamespaceMapper,
         perDestination,
         messageTracker,
         validator,
         metricReporter,
         false);
-    AtomicReference<ReplicationOutput> output = new AtomicReference<>();
+    final AtomicReference<ReplicationOutput> output = new AtomicReference<>();
     final Thread workerThread = new Thread(() -> {
       try {
         output.set(worker.run(new StandardSyncInput().withCatalog(new ConfiguredAirbyteCatalog()
@@ -62,13 +90,14 @@ public class ReplicationWorkerPerformanceTest {
 
     workerThread.start();
     workerThread.join();
-    var summary = output.get().getReplicationAttemptSummary();
-    var mbRead = summary.getBytesSynced() / 1_000_000;
-    var timeTakenSec = (summary.getEndTime() - summary.getStartTime()) / 1000.0;
+    final var summary = output.get().getReplicationAttemptSummary();
+    final var mbRead = summary.getBytesSynced() / 1_000_000;
+    final var timeTakenSec = (summary.getEndTime() - summary.getStartTime()) / 1000.0;
     log.info("MBs read: {}, Time taken sec: {}, MB/s: {}", mbRead, timeTakenSec, mbRead / timeTakenSec);
   }
 
   public static void main(String[] args) throws IOException {
+    // Run this main class to start benchmarking.
     org.openjdk.jmh.Main.main(args);
   }
 
--- airbyte-commons-worker/src/test/java/io/airbyte/workers/general/StubAirbyteMapper.java
+++ airbyte-commons-worker/src/test/java/io/airbyte/workers/general/StubAirbyteMapper.java
@@ -8,6 +8,9 @@ import io.airbyte.protocol.models.AirbyteMessage;
 import io.airbyte.protocol.models.ConfiguredAirbyteCatalog;
 import io.airbyte.workers.internal.AirbyteMapper;
 
+/**
+ * Stub mapper testing what happens without any mapping.
+ */
 public class StubAirbyteMapper implements AirbyteMapper {
 
   @Override
