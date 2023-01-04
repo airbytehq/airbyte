c89cc109cf63f9374f6e1d5001d839f89ada8a4f
Joe Reuter <joe@airbyte.io>
2023-01-04 10:43:22 +0100

    review comments

--- airbyte-cdk/python/unit_tests/utils/test_schema_inferrer.py
+++ airbyte-cdk/python/unit_tests/utils/test_schema_inferrer.py
@@ -24,6 +24,13 @@ def test_deriving_schema_refine():
     assert json.dumps(inferrer.get_inferred_schemas()["my_stream"]) == '{"$schema": "http://json-schema.org/schema#", "type": "object", "properties": {"field_A": {"type": ["number", "string"]}}}'
 
 
+def test_deriving_schema_nested_structures():
+    inferrer = SchemaInferrer()
+    inferrer.accumulate(AirbyteRecordMessage(stream="my_stream", data={"obj": { "data": [1.0, 2.0, 3.0]}}, emitted_at=NOW))
+    inferrer.accumulate(AirbyteRecordMessage(stream="my_stream", data={"obj": { "other_key": "xyz" }}, emitted_at=NOW))
+    assert json.dumps(inferrer.get_inferred_schemas()["my_stream"]) == '{"$schema": "http://json-schema.org/schema#", "type": "object", "properties": {"obj": {"type": "object", "properties": {"data": {"type": "array", "items": {"type": "number"}}, "other_key": {"type": "string"}}}}}'
+
+
 def test_deriving_schema_multiple_streams():
     inferrer = SchemaInferrer()
     inferrer.accumulate(AirbyteRecordMessage(stream="my_stream", data={"field_A": 1.0}, emitted_at=NOW))
