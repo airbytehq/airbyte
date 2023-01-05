09c68cc93a4e62761abd50371e96a11a03986291
alovew <anne@airbyte.io>
2023-01-05 14:45:21 -0800

    version

--- airbyte-workers/src/main/java/io/airbyte/workers/temporal/scheduling/ConnectionNotificationWorkflowImpl.java
+++ airbyte-workers/src/main/java/io/airbyte/workers/temporal/scheduling/ConnectionNotificationWorkflowImpl.java
@@ -16,6 +16,7 @@ import io.airbyte.workers.temporal.annotations.TemporalActivityStub;
 import io.airbyte.workers.temporal.scheduling.activities.ConfigFetchActivity;
 import io.airbyte.workers.temporal.scheduling.activities.NotifySchemaChangeActivity;
 import io.airbyte.workers.temporal.scheduling.activities.SlackConfigActivity;
+import io.temporal.workflow.Workflow;
 import java.io.IOException;
 import java.util.Optional;
 import java.util.UUID;
@@ -23,6 +24,9 @@ import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class ConnectionNotificationWorkflowImpl implements ConnectionNotificationWorkflow {
+
+  private static final String GET_BREAKING_CHANGE_TAG = "get_breaking_change";
+  private static final int GET_BREAKING_CHANGE_VERSION = 1;
 
   @TemporalActivityStub(activityOptionsBeanName = "shortActivityOptions")
   private NotifySchemaChangeActivity notifySchemaChangeActivity;
@@ -34,14 +38,18 @@ public class ConnectionNotificationWorkflowImpl implements ConnectionNotificatio
   @Override
   public boolean sendSchemaChangeNotification(final UUID connectionId)
       throws IOException, InterruptedException, ApiException, ConfigNotFoundException, JsonValidationException {
-    final Optional<Boolean> breakingChange = configFetchActivity.getBreakingChange(connectionId);
-    final Optional<SlackNotificationConfiguration> slackConfig = slackConfigActivity.fetchSlackConfiguration(connectionId);
-    if (slackConfig.isPresent() && breakingChange.isPresent()) {
-      final Notification notification =
-          new Notification().withNotificationType(NotificationType.SLACK).withSendOnFailure(false).withSendOnSuccess(false)
-              .withSlackConfiguration(slackConfig.get());
-      final SlackNotificationClient notificationClient = new SlackNotificationClient(notification);
-      return notifySchemaChangeActivity.notifySchemaChange(notificationClient, connectionId, breakingChange.get());
+    final int getBreakingChangeVersion =
+        Workflow.getVersion(GET_BREAKING_CHANGE_TAG, Workflow.DEFAULT_VERSION, GET_BREAKING_CHANGE_VERSION);
+    if (getBreakingChangeVersion >= GET_BREAKING_CHANGE_VERSION) {
+      final Optional<Boolean> breakingChange = configFetchActivity.getBreakingChange(connectionId);
+      final Optional<SlackNotificationConfiguration> slackConfig = slackConfigActivity.fetchSlackConfiguration(connectionId);
+      if (slackConfig.isPresent() && breakingChange.isPresent()) {
+        final Notification notification =
+            new Notification().withNotificationType(NotificationType.SLACK).withSendOnFailure(false).withSendOnSuccess(false)
+                .withSlackConfiguration(slackConfig.get());
+        final SlackNotificationClient notificationClient = new SlackNotificationClient(notification);
+        return notifySchemaChangeActivity.notifySchemaChange(notificationClient, connectionId, breakingChange.get());
+      }
     } else {
       return false;
     }
