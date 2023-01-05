a1640acaf3a23301d9055a31546ac7e2bc5bc900
Rodi Reich Zilberman <867491+rodireich@users.noreply.github.com>
2023-01-05 12:32:49 +0530

    Better escape the select query for a case a schema name starts with a numebr.

--- airbyte-integrations/connectors/source-postgres/src/main/java/io/airbyte/integrations/source/postgres/PostgresSource.java
+++ airbyte-integrations/connectors/source-postgres/src/main/java/io/airbyte/integrations/source/postgres/PostgresSource.java
@@ -98,7 +98,7 @@ public class PostgresSource extends AbstractJdbcSource<PostgresType> implements 
         SELECT
           (EXISTS (SELECT FROM information_schema.columns WHERE table_schema = '%s' AND table_name = '%s' AND is_nullable = 'YES' AND column_name = '%s'))
         AND
-          (EXISTS (SELECT from %s.\"%s\" where \"%s\" IS NULL LIMIT 1)) AS %s
+          (EXISTS (SELECT from \"%s\".\"%s\" where \"%s\" IS NULL LIMIT 1)) AS %s
       """;
   public static final String NULL_CURSOR_VALUE_NO_SCHEMA =
       """
