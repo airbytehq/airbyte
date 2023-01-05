410cafe73e3dc3ca60f6ed65c89db41f6c14e273
Mark Berger <mark.berger@globallogic.com>
2023-01-06 01:42:19 +0200

    Update form field labels in connection form to match design
    - Replaced ControlLabels message property with infoTooltipContent
    - Replaced property "flex-start" to "center" for FlexContainer (New Connection)

--- airbyte-webapp/src/views/Connection/ConnectionForm/components/NonBreakingChangesPreferenceField.tsx
+++ airbyte-webapp/src/views/Connection/ConnectionForm/components/NonBreakingChangesPreferenceField.tsx
@@ -28,7 +28,7 @@ export const NonBreakingChangesPreferenceField: React.FC<FieldProps<string>> = (
   const { mode } = useConnectionFormService();
 
   return (
-    <FlexContainer alignItems="flex-start">
+    <FlexContainer alignItems="center">
       <div className={styles.leftFieldCol}>
         <ControlLabels
           nextLine
