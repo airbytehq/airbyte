0819c8b9a4f65e8145fba689343fee3c2fa2117b
Rodi Reich Zilberman <867491+rodireich@users.noreply.github.com>
2023-01-06 13:12:45 +0530

    fix format

--- airbyte-integrations/bases/standard-source-test/src/main/java/io/airbyte/integrations/standardtest/source/AbstractSourceDatabaseTypeTest.java
+++ airbyte-integrations/bases/standard-source-test/src/main/java/io/airbyte/integrations/standardtest/source/AbstractSourceDatabaseTypeTest.java
@@ -112,8 +112,7 @@ public abstract class AbstractSourceDatabaseTypeTest extends AbstractSourceConne
     final UUID catalogId = runDiscover();
     final Map<String, AirbyteStream> streams = getLastPersistedCatalog().getStreams().stream()
         .collect(Collectors.toMap(
-            s ->
-                "%s.%s".formatted(s.getNamespace(), s.getName()),
+            s -> "%s.%s".formatted(s.getNamespace(), s.getName()),
             s -> s));
     final List<AirbyteMessage> recordMessages = allMessages.stream().filter(m -> m.getType() == Type.RECORD).toList();
     final Map<String, List<String>> expectedValues = new HashMap<>();
