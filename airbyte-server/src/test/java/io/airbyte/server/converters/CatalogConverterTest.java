1d288b6cc9acf4fa6452e1eac4d49ccce402602c
Michael Siega <109092231+mfsiega-airbyte@users.noreply.github.com>
2023-01-09 19:03:41 +0100

    allow the cursors and primary keys to be deselected when the sync mode does not require it (#21117)

--- airbyte-server/src/test/java/io/airbyte/server/converters/CatalogConverterTest.java
+++ airbyte-server/src/test/java/io/airbyte/server/converters/CatalogConverterTest.java
@@ -5,18 +5,23 @@
 package io.airbyte.server.converters;
 
 import static io.airbyte.server.helpers.ConnectionHelpers.FIELD_NAME;
+import static io.airbyte.server.helpers.ConnectionHelpers.SECOND_FIELD_NAME;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import com.fasterxml.jackson.databind.node.ObjectNode;
+import io.airbyte.api.model.generated.DestinationSyncMode;
 import io.airbyte.api.model.generated.SelectedFieldInfo;
+import io.airbyte.api.model.generated.SyncMode;
 import io.airbyte.commons.enums.Enums;
 import io.airbyte.config.DataType;
 import io.airbyte.config.FieldSelectionData;
 import io.airbyte.server.handlers.helpers.CatalogConverter;
 import io.airbyte.server.helpers.ConnectionHelpers;
 import io.airbyte.validation.json.JsonValidationException;
+import java.util.List;
 import org.junit.jupiter.api.Test;
 
 class CatalogConverterTest {
@@ -74,6 +79,42 @@ class CatalogConverterTest {
       // SelectedFieldInfo with empty path.
       final var catalog = ConnectionHelpers.generateBasicApiCatalog();
       catalog.getStreams().get(0).getConfig().fieldSelectionEnabled(true).addSelectedFieldsItem(new SelectedFieldInfo().addFieldPathItem("foo"));
+      CatalogConverter.toProtocol(catalog);
+    });
+
+    assertThrows(JsonValidationException.class, () -> {
+      final var catalog = ConnectionHelpers.generateApiCatalogWithTwoFields();
+      // Only FIELD_NAME is selected.
+      catalog.getStreams().get(0).getConfig().fieldSelectionEnabled(true).addSelectedFieldsItem(new SelectedFieldInfo().addFieldPathItem(FIELD_NAME));
+      // The sync mode is INCREMENTAL and SECOND_FIELD_NAME is a cursor field.
+      catalog.getStreams().get(0).getConfig().syncMode(SyncMode.INCREMENTAL).cursorField(List.of(SECOND_FIELD_NAME));
+      CatalogConverter.toProtocol(catalog);
+    });
+
+    assertDoesNotThrow(() -> {
+      final var catalog = ConnectionHelpers.generateApiCatalogWithTwoFields();
+      // Only FIELD_NAME is selected.
+      catalog.getStreams().get(0).getConfig().fieldSelectionEnabled(true).addSelectedFieldsItem(new SelectedFieldInfo().addFieldPathItem(FIELD_NAME));
+      // The cursor field is not selected, but it's okay because it's FULL_REFRESH so it doesn't throw.
+      catalog.getStreams().get(0).getConfig().syncMode(SyncMode.FULL_REFRESH).cursorField(List.of(SECOND_FIELD_NAME));
+      CatalogConverter.toProtocol(catalog);
+    });
+
+    assertThrows(JsonValidationException.class, () -> {
+      final var catalog = ConnectionHelpers.generateApiCatalogWithTwoFields();
+      // Only FIELD_NAME is selected.
+      catalog.getStreams().get(0).getConfig().fieldSelectionEnabled(true).addSelectedFieldsItem(new SelectedFieldInfo().addFieldPathItem(FIELD_NAME));
+      // The destination sync mode is DEDUP and SECOND_FIELD_NAME is a primary key.
+      catalog.getStreams().get(0).getConfig().destinationSyncMode(DestinationSyncMode.APPEND_DEDUP).primaryKey(List.of(List.of(SECOND_FIELD_NAME)));
+      CatalogConverter.toProtocol(catalog);
+    });
+
+    assertDoesNotThrow(() -> {
+      final var catalog = ConnectionHelpers.generateApiCatalogWithTwoFields();
+      // Only FIELD_NAME is selected.
+      catalog.getStreams().get(0).getConfig().fieldSelectionEnabled(true).addSelectedFieldsItem(new SelectedFieldInfo().addFieldPathItem(FIELD_NAME));
+      // The primary key is not selected but that's okay because the destination sync mode is OVERWRITE.
+      catalog.getStreams().get(0).getConfig().destinationSyncMode(DestinationSyncMode.OVERWRITE).primaryKey(List.of(List.of(SECOND_FIELD_NAME)));
       CatalogConverter.toProtocol(catalog);
     });
   }
