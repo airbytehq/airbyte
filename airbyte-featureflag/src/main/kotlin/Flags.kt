53e9da6b8d27ee32484f357739e1a2caedf31bc8
Cole Snodgrass <cole@airbyte.io>
2023-01-10 13:13:28 -0800

    replace Team idea with attributes map

--- airbyte-featureflag/src/main/kotlin/Flags.kt
+++ airbyte-featureflag/src/main/kotlin/Flags.kt
@@ -9,23 +9,12 @@ package io.airbyte.featureflag
  *
  * This is a permanent flag and would implement the [Flag] type once converted from an environment-variable.
  */
-object LogConnectorMessages : EnvVar(envVar = "LOG_CONNECTOR_MESSAGES", team = Team.PLATFORM_MOVE)
+object LogConnectorMessages : EnvVar(envVar = "LOG_CONNECTOR_MESSAGES")
 
 object StreamCapableState : EnvVar(envVar = "USE_STREAM_CAPABLE_STATE")
-object AutoDetectSchema : EnvVar(envVar = "AUTO_DETECT_SCHEMA", team = Team.PLATFORM_MOVE)
+object AutoDetectSchema : EnvVar(envVar = "AUTO_DETECT_SCHEMA")
 object NeedStateValidation : EnvVar(envVar = "NEED_STATE_VALIDATION")
 object ApplyFieldSelection : EnvVar(envVar = "APPLY_FIELD_SELECTION")
-
-/**
- * The team that created and is responsible for individual feature-flags.
- * TODO: finalize the options here!
- */
-enum class Team {
-    UNKNOWN,
-    PLATFORM_MOVE,
-    PLATFORM_COMPOSE,
-    FRONTEND,
-}
 
 /**
  * Flag is a sealed class that all feature-flags must inherit from.
@@ -35,12 +24,12 @@ enum class Team {
  *
  * @param [key] is the globally unique identifier for identifying this specific feature-flag.
  * @param [default] is the default value of the flag.
- * @param [team] is the team that is responsible for the feature-flag, defaults to Unknown.
+ * @param [attrs] optional attributes associated with this flag
  */
 sealed class Flag(
     internal val key: String,
     internal val default: Boolean = false,
-    internal val team: Team = Team.UNKNOWN,
+    internal val attrs: Map<String, String> = mapOf(),
 )
 
 /**
@@ -49,15 +38,15 @@ sealed class Flag(
  * A Temporary feature-flag is any feature-flag that is not intended to exist forever.
  * Most feature-flags should be considered temporary.
  *
- * @param [team] is the team that is responsible for the feature-flag, defaults to Unknown.
  * @param [key] is the globally unique identifier for identifying this specific feature-flag.
  * @param [default] is the default value of the flag.
+ * @param [attrs] attributes associated with this flag
  */
 open class Temporary @JvmOverloads constructor(
     key: String,
     default: Boolean = false,
-    team: Team = Team.UNKNOWN,
-) : Flag(key = key, default = default, team = team)
+    attrs: Map<String, String> = mapOf(),
+) : Flag(key = key, default = default, attrs = attrs)
 
 /**
  * Environment Variable based feature-flag.
@@ -65,32 +54,32 @@ open class Temporary @JvmOverloads constructor(
  * Intended only to be used in a transitory manner as the platform migrates to an official feature-flag solution.
  * Every instance of this class should be migrated over to the Temporary class.
  *
- * @param [team] the team that controls this flag
  * @param [envVar] the environment variable to check for the status of this flag
  * @param [default] the default value of this flag, if the environment variable is not defined
+ * @param [attrs] attributes associated with this flag
  * @param [fetcher] the function used to retrieve the environment-variable, overrideable for testing purposes only
  * @constructor an internal constructor for testing purposes, users must be the public constructor
  */
 open class EnvVar internal constructor(
     envVar: String,
     default: Boolean = false,
-    team: Team = Team.UNKNOWN,
+    attrs: Map<String, String> = mapOf(),
     private val fetcher: (String) -> String?,
-) : Flag(key = envVar, default = default, team = team) {
+) : Flag(key = envVar, default = default, attrs = attrs) {
 
     /**
      * Constructs an EnvVar flag
      *
-     * @param [team] the team that controls this flag
      * @param [envVar] the environment variable to check for the status of this flag
      * @param [default] the default value of this flag, if the environment variable is not defined
+     * @param [attrs] attributes associated with this flag
      */
     @JvmOverloads
     constructor(
         envVar: String,
         default: Boolean = false,
-        team: Team = Team.UNKNOWN,
-    ) : this(envVar, default, team, { s -> System.getenv(s) })
+        attrs: Map<String, String> = mapOf(),
+    ) : this(envVar, default, attrs, { s -> System.getenv(s) })
 
     /**
      * Returns true if, and only if, the environment-variable is defined and evaluates to "true".  Otherwise, returns false.
