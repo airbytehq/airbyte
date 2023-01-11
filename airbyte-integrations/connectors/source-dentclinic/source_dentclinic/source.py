29813050cb00b8bdf76af5b875d0a7f99f88cef3
Sebastian Sundet Flaen <sebastianflaen@gmail.com>
2023-01-11 13:57:10 +0100

    Feature: clinic stream

--- airbyte-integrations/connectors/source-dentclinic/source_dentclinic/source.py
+++ airbyte-integrations/connectors/source-dentclinic/source_dentclinic/source.py
@@ -14,6 +14,62 @@ from airbyte_cdk.sources.streams import Stream
 from airbyte_cdk.sources.streams.http import HttpStream
 
 
+
+class DentclinicClinicsStream(HttpStream, ABC):
+    """Base stream"""
+    primary_key = None
+    state_checkpoint_interval = 1
+
+    url_base = "https://dcm-nhn.dentclinicmanager.com/API/DCMConnect.asmx?WSDL"
+
+    def __init__(self, config: Mapping[str, Any], **kwargs):
+        super().__init__()
+        self.api_key = config.get("api_key")
+
+    def request_headers(
+            self, stream_state: Mapping[str, Any], stream_slice: Mapping[str, Any] = None, next_page_token: Mapping[str, Any] = None
+    ) -> Mapping[str, Any]:
+        return {'Content-Type': 'application/soap+xml; charset=utf-8'}
+
+
+    def next_page_token(self, response: requests.Response) -> Optional[Mapping[str, Any]]:
+        return None
+
+    def request_body_data(
+            self,
+            stream_state: Mapping[str, Any],
+            stream_slice: Mapping[str, Any] = None,
+            next_page_token: Mapping[str, Any] = None,
+    ) -> Optional[Mapping]:
+        return f"""<?xml version="1.0" encoding="utf-8"?>
+            <soap12:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap12="http://www.w3.org/2003/05/soap-envelope">
+                <soap12:Body>
+                    <GetClinics xmlns="http://tempuri.org/">
+                        <key>{self.api_key}</key>
+                    </GetClinics>
+                </soap12:Body>
+            </soap12:Envelope>
+        """
+
+    def parse_response(self, response: requests.Response, **kwargs) -> Iterable[Mapping]:
+        """
+        :return an iterable containing each record in the response
+        """
+
+        path = ['soap:Envelope', 'soap:Body', 'GetClinicsResponse', 'GetClinicsResult', 'ClinicModel']
+        data = xmltodict.parse(response.text).copy()
+        for key in path:
+            data = data.get(key)
+            if data is None:
+                data = []
+                break
+
+        if type(data) == dict:
+            data = [data]
+
+        yield from data
+
+
 # Basic full refresh stream
 class DentclinicStream(HttpStream, ABC):
     primary_key = None
@@ -126,6 +182,21 @@ class Bookings(DentclinicStream):
     def http_method(self) -> str:
         return "POST"
 
+class Clinics(DentclinicClinicsStream):
+    primary_key = "Id"
+
+    def path(
+            self, stream_state: Mapping[str, Any] = None, stream_slice: Mapping[str, Any] = None, next_page_token: Mapping[str, Any] = None
+    ) -> str:
+        """
+        should return "clinics". Required.
+        """
+        return ""
+
+    @property
+    def http_method(self) -> str:
+        return "POST"
+
 
 # Source
 class SourceDentclinic(AbstractSource):
@@ -144,4 +215,4 @@ class SourceDentclinic(AbstractSource):
         """
         :param config: A Mapping of the user input configuration as defined in the connector spec.
         """
-        return [Bookings(config=config)]
+        return [Bookings(config=config), Clinics(config=config)]
