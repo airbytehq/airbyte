6dacd90ce68260c0ecfe98feda504bafca2ef60c
Christoph Pirkl <christoph.pirkl@exasol.com>
2023-01-11 11:46:58 +0100

    Fix Exasol integration tests
    * Pass certificate fingerprint when creating a DB connection
    * Use the build directory for temp Exasol credentials

--- airbyte-integrations/connectors/destination-exasol/src/test-integration/java/io/airbyte/integrations/destination/exasol/ExasolDestinationAcceptanceTest.java
+++ airbyte-integrations/connectors/destination-exasol/src/test-integration/java/io/airbyte/integrations/destination/exasol/ExasolDestinationAcceptanceTest.java
@@ -9,6 +9,8 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.google.common.collect.ImmutableMap;
 import io.airbyte.commons.json.Jsons;
+import io.airbyte.db.Database;
+import io.airbyte.db.factory.DSLContextFactory;
 import io.airbyte.db.factory.DataSourceFactory;
 import io.airbyte.db.factory.DatabaseDriver;
 import io.airbyte.db.jdbc.DefaultJdbcDatabase;
@@ -17,6 +19,7 @@ import io.airbyte.db.jdbc.JdbcUtils;
 import io.airbyte.integrations.base.JavaBaseConstants;
 import io.airbyte.integrations.destination.NamingConventionTransformer;
 import io.airbyte.integrations.standardtest.destination.DestinationAcceptanceTest;
+import org.jooq.DSLContext;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.slf4j.Logger;
@@ -26,21 +29,37 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 public class ExasolDestinationAcceptanceTest extends DestinationAcceptanceTest {
 
   private static final Logger LOGGER = LoggerFactory.getLogger(ExasolDestinationAcceptanceTest.class);
 
-  private static final ExasolContainer<? extends ExasolContainer<?>> EXASOL = new ExasolContainer<>().withReuse(true);
+  private static final ExasolContainer<? extends ExasolContainer<?>> EXASOL = new ExasolContainer<>()
+          .withTemporaryCredentialsDirectory(Path.of("build/"))
+          .withReuse(true);
 
   private final NamingConventionTransformer namingResolver = new ExasolSQLNameTransformer();
+  private static JsonNode config;
 
   @BeforeAll
-  static void startExasolContainer() throws IOException {
-    Files.createDirectories(Path.of("target"));
+  static void startExasolContainer() {
     EXASOL.start();
+    config = createExasolConfig(EXASOL);
+  }
+
+  private static JsonNode createExasolConfig(ExasolContainer<? extends ExasolContainer<?>> exasol) {
+    return Jsons.jsonNode(ImmutableMap.builder()
+            .put(JdbcUtils.HOST_KEY, exasol.getHost())
+            .put(JdbcUtils.PORT_KEY, exasol.getFirstMappedDatabasePort())
+            .put("certificateFingerprint", exasol.getTlsCertificateFingerprint().orElseThrow())
+            .put(JdbcUtils.USERNAME_KEY, exasol.getUsername())
+            .put(JdbcUtils.PASSWORD_KEY, exasol.getPassword())
+            .put(JdbcUtils.SCHEMA_KEY, "TEST")
+            .build());
   }
 
   @AfterAll
@@ -55,14 +74,7 @@ public class ExasolDestinationAcceptanceTest extends DestinationAcceptanceTest {
 
   @Override
   protected JsonNode getConfig() {
-    return Jsons.jsonNode(ImmutableMap.builder()
-            .put(JdbcUtils.HOST_KEY, EXASOL.getHost())
-            .put(JdbcUtils.PORT_KEY, EXASOL.getFirstMappedDatabasePort())
-            .put("certificateFingerprint", EXASOL.getTlsCertificateFingerprint().orElseThrow())
-            .put(JdbcUtils.USERNAME_KEY, EXASOL.getUsername())
-            .put(JdbcUtils.PASSWORD_KEY, EXASOL.getPassword())
-            .put(JdbcUtils.SCHEMA_KEY, "TEST")
-            .build());
+    return Jsons.clone(config);
   }
 
   @Override
@@ -85,20 +97,29 @@ public class ExasolDestinationAcceptanceTest extends DestinationAcceptanceTest {
   }
 
   private List<JsonNode> retrieveRecordsFromTable(final String tableName, final String schemaName) throws SQLException {
-    final JdbcDatabase jdbcDB = getDatabase(getConfig());
-    final String query = String.format("SELECT * FROM %s.%s ORDER BY %s ASC", schemaName, tableName, ExasolSqlOperations.COLUMN_NAME_EMITTED_AT);
-    return jdbcDB.queryJsons(query);
+    try (final DSLContext dslContext = getDSLContext(config)) {
+      final List<org.jooq.Record> result = new Database(dslContext)
+              .query(ctx -> new ArrayList<>(ctx.fetch(
+                      String.format("SELECT * FROM %s.%s ORDER BY %s ASC", schemaName, tableName,
+                              ExasolSqlOperations.COLUMN_NAME_EMITTED_AT))));
+      return result
+              .stream()
+              .map(r -> r.formatJSON(JdbcUtils.getDefaultJSONFormat()))
+              .map(Jsons::deserialize)
+              .collect(Collectors.toList());
+    }
   }
 
-  private static JdbcDatabase getDatabase(final JsonNode config) {
+  private static DSLContext getDSLContext(final JsonNode config) {
     String jdbcUrl = String.format(DatabaseDriver.EXASOL.getUrlFormatString(), config.get(JdbcUtils.HOST_KEY).asText(), config.get(JdbcUtils.PORT_KEY).asInt());
-    return new DefaultJdbcDatabase(
-            DataSourceFactory.create(
-                    config.get(JdbcUtils.USERNAME_KEY).asText(),
-                    config.has(JdbcUtils.PASSWORD_KEY) ? config.get(JdbcUtils.PASSWORD_KEY).asText() : null,
-                    ExasolDestination.DRIVER_CLASS,
-                    jdbcUrl)
-    );
+    Map<String, String> jdbcConnectionProperties = Map.of("fingerprint", config.get("certificateFingerprint").asText());
+    return DSLContextFactory.create(
+            config.get(JdbcUtils.USERNAME_KEY).asText(),
+            config.get(JdbcUtils.PASSWORD_KEY).asText(),
+            DatabaseDriver.EXASOL.getDriverClassName(),
+            jdbcUrl,
+            null,
+            jdbcConnectionProperties);
   }
 
   protected boolean implementsNamespaces() {
