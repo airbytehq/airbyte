8c2025590db288dacfc1ee9c916c589ac1c0aa13
Catherine Noll <noll.catherine@gmail.com>
2023-01-11 22:50:35 +0000

    Handle `#/` ref format

--- airbyte-cdk/python/airbyte_cdk/sources/declarative/parsers/manifest_reference_resolver.py
+++ airbyte-cdk/python/airbyte_cdk/sources/declarative/parsers/manifest_reference_resolver.py
@@ -13,10 +13,10 @@ class ManifestReferenceResolver:
     An incoming manifest can contain references to values previously defined.
     This parser will dereference these values to produce a complete ConnectionDefinition.
 
-    References can be defined using a *ref(<arg>) string.
+    References can be defined using a #/<arg> string.
     ```
     key: 1234
-    reference: "*ref(key)"
+    reference: "#/key"
     ```
     will produce the following definition:
     ```
@@ -28,7 +28,7 @@ class ManifestReferenceResolver:
     key_value_pairs:
       k1: v1
       k2: v2
-    same_key_value_pairs: "*ref(key_value_pairs)"
+    same_key_value_pairs: "#/key_value_pairs"
     ```
     will produce the following definition:
     ```
@@ -46,7 +46,7 @@ class ManifestReferenceResolver:
       k1: v1
       k2: v2
     same_key_value_pairs:
-      $ref: "*ref(key_value_pairs)"
+      $ref: "#/key_value_pairs"
       k3: v3
     ```
     will produce the following definition:
@@ -66,7 +66,7 @@ class ManifestReferenceResolver:
     ```
     dict:
         limit: 50
-    limit_ref: "*ref(dict.limit)"
+    limit_ref: "#/dict/limit"
     ```
     will produce the following definition:
     ```
@@ -75,18 +75,18 @@ class ManifestReferenceResolver:
     limit-ref: 50
     ```
 
-    whereas here we want to access the `nested.path` value.
+    whereas here we want to access the `nested/path` value.
     ```
     nested:
         path: "first one"
-    nested.path: "uh oh"
-    value: "ref(nested.path)
+    nested/path: "uh oh"
+    value: "#/nested/path
     ```
     will produce the following definition:
     ```
     nested:
         path: "first one"
-    nested.path: "uh oh"
+    nested/path: "uh oh"
     value: "uh oh"
     ```
 
@@ -118,7 +118,7 @@ class ManifestReferenceResolver:
                 return evaluated_dict
         elif isinstance(node, list):
             return [self._evaluate_node(v, manifest, visited) for v in node]
-        elif isinstance(node, str) and node.startswith("*ref("):
+        elif self._is_ref(node):
             if node in visited:
                 raise CircularReferenceException(node)
             visited.add(node)
@@ -132,36 +132,40 @@ class ManifestReferenceResolver:
         return key == self.ref_tag
 
     def _lookup_reference_value(self, reference: str, manifest: Mapping[str, Any]) -> Any:
-        path = re.match("\\*ref\\(([^)]+)\\)", reference).groups()[0]
+        path = re.match(r"#/(.*)", reference).groups()[0]
         if not path:
             raise UndefinedReferenceException(path, reference)
         try:
             return self._read_reference_value(path, manifest)
-        except (KeyError, IndexError):
+        except (AttributeError, KeyError, IndexError):
             raise UndefinedReferenceException(path, reference)
+
+    @staticmethod
+    def _is_ref(node: Any) -> bool:
+        return isinstance(node, str) and node.startswith("#/")
 
     @staticmethod
     def _read_reference_value(ref: str, manifest_node: Mapping[str, Any]) -> Any:
         """
         Read the value at the referenced location of the manifest.
 
-        References are ambiguous because one could define a key containing `.`
+        References are ambiguous because one could define a key containing `/`
         In this example, we want to refer to the `limit` key in the `dict` object:
             dict:
                 limit: 50
-            limit_ref: "*ref(dict.limit)"
+            limit_ref: "#/dict/limit"
 
-        Whereas here we want to access the `nested.path` value.
+        Whereas here we want to access the `nested/path` value.
           nested:
             path: "first one"
-          nested.path: "uh oh"
-          value: "ref(nested.path)
+          nested/path: "uh oh"
+          value: "#/nested/path"
 
         To resolve the ambiguity, we try looking for the reference key at the top level, and then traverse the structs downward
         until we find a key with the given path, or until there is nothing to traverse.
 
-        Consider the path foo.bar.baz. To resolve the ambiguity, we first try 'foo.bar.baz' in its entirety as a top-level key. If this
-        fails, we try 'foo' as the top-level key, and if this succeeds, pass 'bar.baz' on as the key to be tried at the next level.
+        Consider the path foo/bar/baz. To resolve the ambiguity, we first try 'foo/bar/baz' in its entirety as a top-level key. If this
+        fails, we try 'foo' as the top-level key, and if this succeeds, pass 'bar/baz' on as the key to be tried at the next level.
         """
         while ref:
             try:
@@ -178,18 +182,20 @@ def _parse_path(ref: str) -> Tuple[Union[str, int], str]:
 
     A path component may be a string key, or an int index.
 
-    >>> _parse_path("foo.bar")
+    >>> _parse_path("foo/bar")
     "foo", "bar"
-    >>> _parse_path("foo[7][8].bar")
-    "foo", "[7][8].bar"
-    >>> _parse_path("[7][8].bar")
-    7, "[8].bar"
-    >>> _parse_path("[8].bar")
+    >>> _parse_path("foo/7/8/bar")
+    "foo", "7/8/bar"
+    >>> _parse_path("7/8/bar")
+    7, "8/bar"
+    >>> _parse_path("8/bar")
     8, "bar"
+    >>> _parse_path("8foo/bar")
+    "8foo", "bar"
     """
-    if match := re.match(r"^\[([0-9]+)\]\.?(.*)", ref):
-        idx, rest = match.groups()
-        result = int(idx), rest
-    else:
-        result = re.match(r"([^[.]*)\.?(.*)", ref).groups()
-    return result
+    match = re.match(r"([^/]*)/?(.*)", ref)
+    first, rest = match.groups()
+    try:
+        return int(first), rest
+    except ValueError:
+        return first, rest
