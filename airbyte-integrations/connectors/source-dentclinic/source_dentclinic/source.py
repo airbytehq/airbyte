635abd80658c79ca1a8c37afb8cf3f2dffb077f5
Sebastian Sundet Flaen <sebastianflaen@gmail.com>
2023-01-11 17:12:37 +0100

    Feature: stream resources. TODO: Rename source and make stream logic reusable

--- airbyte-integrations/connectors/source-dentclinic/source_dentclinic/source.py
+++ airbyte-integrations/connectors/source-dentclinic/source_dentclinic/source.py
@@ -70,6 +70,95 @@ class DentclinicStaticStream(HttpStream, ABC):
 
         yield from data
 
+# Basic full refresh stream
+
+
+class DentclinicVisidIdStream(HttpStream, ABC):
+    primary_key = None
+    state_checkpoint_interval = 1
+
+    url_base = "https://dcm-nhn.dentclinicmanager.com/API/DCMConnect.asmx?WSDL"
+
+    def __init__(self, config: Mapping[str, Any], **kwargs):
+        super().__init__()
+        self.api_key = config.get("api_key")
+        self.clinic_ids = self.get_clinic_ids()
+        self.clinic_id = next(self.clinic_ids)
+
+    def request_headers(
+            self, stream_state: Mapping[str, Any], stream_slice: Mapping[str, Any] = None, next_page_token: Mapping[str, Any] = None
+    ) -> Mapping[str, Any]:
+        return {'Content-Type': 'application/soap+xml; charset=utf-8'}
+
+    def get_clinic_ids(self) -> Iterator[str]:
+        payload_clinics = f"""<?xml version="1.0" encoding="utf-8"?>
+            <soap12:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap12="http://www.w3.org/2003/05/soap-envelope">
+                <soap12:Body>
+                    <GetClinics xmlns="http://tempuri.org/">
+                        <key>{self.api_key}</key>
+                    </GetClinics>
+                </soap12:Body>
+            </soap12:Envelope>
+        """
+        headers = {'Content-Type': 'application/soap+xml; charset=utf-8'}
+        response = requests.post(
+            f"{self.url_base}", data=payload_clinics, headers=headers)
+
+        clinics = xmltodict.parse(response.text)[
+            'soap:Envelope']['soap:Body']['GetClinicsResponse']['GetClinicsResult']['ClinicModel']
+        clinic_ids = [x.get('Id') for x in clinics]
+        return iter(clinic_ids)
+
+    def next_page_token(self, response: requests.Response) -> Optional[Mapping[str, Any]]:
+        """
+        :param response: the most recent response from the API
+        :return If there is another page in the result, a mapping (e.g: dict) containing information needed to query the next page in the response.
+                If there are no more pages in the result, return None.
+        """
+
+        self.clinic_id = next(self.clinic_ids, None)
+        if self.clinic_id is None:
+            return None
+
+        
+        return {"clinic_id": self.clinic_id}
+
+    def request_body_data(
+            self,
+            stream_state: Mapping[str, Any],
+            stream_slice: Mapping[str, Any] = None,
+            next_page_token: Mapping[str, Any] = None,
+    ) -> Optional[Mapping]:
+        return f'''<?xml version="1.0" encoding="utf-8"?>
+<soap12:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap12="http://www.w3.org/2003/05/soap-envelope">
+  <soap12:Body>
+    <GetResources xmlns="http://tempuri.org/">
+      <key>{self.api_key}</key>
+      <clinicId>{self.clinic_id}</clinicId>
+    </GetResources>
+  </soap12:Body>
+</soap12:Envelope>'''
+
+    def parse_response(self, response: requests.Response, **kwargs) -> Iterable[Mapping]:
+        """
+        :return an iterable containing each record in the response
+        """
+
+        path = ['soap:Envelope', 'soap:Body', 'GetResourcesResponse',
+                'GetResourcesResult', 'ResourceModel']
+
+        data = xmltodict.parse(response.text).copy()
+        for key in path:
+            data = data.get(key)
+            if data is None:
+                data = []
+                break
+
+        if type(data) == dict:
+            data = [data]
+
+        yield from data
+
 
 # Basic full refresh stream
 class DentclinicStream(HttpStream, ABC):
@@ -82,9 +171,12 @@ class DentclinicStream(HttpStream, ABC):
         super().__init__()
         self.api_key = config.get("api_key")
         self.start_date = pendulum.parse(config.get("start_date"))
-        self.stop_date = pendulum.today().add(months=2)
+        self.months_forward = config.get("months_forward")
+        self.stop_date = pendulum.today().add(months=config.get("months_forward"))
         self.cursor_start_date = self.start_date
-        self.cursor_end_date = self.start_date.add(months=1)
+        self.fetch_interval_days = config.get("fetch_interval_days")
+        self.cursor_end_date = self.start_date.add(
+            days=config.get("fetch_interval_days"))
 
         self.clinic_ids = self.get_clinic_ids()
         self.clinic_id = next(self.clinic_ids)
@@ -126,10 +218,13 @@ class DentclinicStream(HttpStream, ABC):
                 return None
 
             self.cursor_start_date = self.start_date
-            self.cursor_end_date = self.start_date.add(months=1)
+            self.cursor_end_date = self.start_date.add(
+                months=self.fetch_interval_days)
         else:
-            self.cursor_start_date = self.cursor_start_date.add(months=1)
-            self.cursor_end_date = self.cursor_end_date.add(months=1)
+            self.cursor_start_date = self.cursor_start_date.add(
+                months=self.fetch_interval_days)
+            self.cursor_end_date = self.cursor_end_date.add(
+                months=self.fetch_interval_days)
 
         return {"start_date": self.cursor_start_date, "end_date": self.cursor_end_date}
 
@@ -179,6 +274,22 @@ class Bookings(DentclinicStream):
     ) -> str:
         """
         should return "bookings". Required.
+        """
+        return ""
+
+    @property
+    def http_method(self) -> str:
+        return "POST"
+
+
+class Resources(DentclinicVisidIdStream):
+    primary_key = "Id"
+
+    def path(
+            self, stream_state: Mapping[str, Any] = None, stream_slice: Mapping[str, Any] = None, next_page_token: Mapping[str, Any] = None
+    ) -> str:
+        """
+        should return "resources". Required.
         """
         return ""
 
@@ -242,4 +353,4 @@ class SourceDentclinic(AbstractSource):
         """
         :param config: A Mapping of the user input configuration as defined in the connector spec.
         """
-        return [Bookings(config=config), Clinics(config=config), Services(config=config)]
+        return [Bookings(config=config), Clinics(config=config), Services(config=config), Resources(config=config)]
