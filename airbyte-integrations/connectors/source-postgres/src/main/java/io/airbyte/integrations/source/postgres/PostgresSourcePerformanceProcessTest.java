00fa8baa4f58ceaf6dcae578ab4832ff4c285409
Davin Chia <davinchia@gmail.com>
2023-01-10 22:38:35 -0800

    Set up another process only testing.

---
+++ airbyte-integrations/connectors/source-postgres/src/main/java/io/airbyte/integrations/source/postgres/PostgresSourcePerformanceProcessTest.java
@@ -0,0 +1,127 @@
+package io.airbyte.integrations.source.postgres;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.google.common.collect.ImmutableMap;
+import io.airbyte.commons.io.IOs;
+import io.airbyte.commons.json.Jsons;
+import io.airbyte.config.EnvConfigs;
+import io.airbyte.config.ResourceRequirements;
+import io.airbyte.config.State;
+import io.airbyte.config.WorkerSourceConfig;
+import io.airbyte.db.jdbc.JdbcUtils;
+import io.airbyte.protocol.models.AirbyteMessage;
+import io.airbyte.protocol.models.v0.ConfiguredAirbyteCatalog;
+import io.airbyte.workers.WorkerConfigs;
+import io.airbyte.workers.internal.DefaultAirbyteSource;
+import io.airbyte.workers.process.AirbyteIntegrationLauncher;
+import io.airbyte.workers.process.DockerProcessFactory;
+import io.airbyte.workers.process.KubePortManagerSingleton;
+import io.airbyte.workers.process.KubeProcessFactory;
+import io.fabric8.kubernetes.client.DefaultKubernetesClient;
+import io.fabric8.kubernetes.client.KubernetesClient;
+import java.net.InetAddress;
+import java.net.URL;
+import java.net.UnknownHostException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class PostgresSourcePerformanceProcessTest {
+
+  public static final int PORT1 = 9877;
+  public static final int PORT2 = 9878;
+  public static final int PORT3 = 9879;
+  public static final int PORT4 = 9880;
+
+  public static final Set<Integer> PORTS = Set.of(PORT1, PORT2, PORT3, PORT4);
+  private static final String PERFORMANCE_SECRET_CREDS = "secrets/performance-config.json";
+
+  final JsonNode plainConfig = Jsons.deserialize(IOs.readFile(Path.of(PERFORMANCE_SECRET_CREDS)));
+
+  final JsonNode replicationMethod = Jsons.jsonNode(ImmutableMap.builder()
+      .put("method", "Standard")
+      .build());
+
+  public static void main(String[] args) throws Exception {
+    var creds = PostgresSourcePerformanceProcessTest.class.getClassLoader().getResource(PERFORMANCE_SECRET_CREDS).getPath();
+    final JsonNode plainConfig = Jsons.deserialize(IOs.readFile(Path.of(creds)));
+
+    var config = Jsons.jsonNode(ImmutableMap.builder()
+      .put(JdbcUtils.HOST_KEY, plainConfig.get(JdbcUtils.HOST_KEY))
+      .put(JdbcUtils.PORT_KEY, plainConfig.get(JdbcUtils.PORT_KEY))
+      .put(JdbcUtils.DATABASE_KEY, "postgres")
+        .put(JdbcUtils.SCHEMAS_KEY, List.of("public"))
+      .put(JdbcUtils.USERNAME_KEY, plainConfig.get(JdbcUtils.USERNAME_KEY))
+      .put(JdbcUtils.PASSWORD_KEY, plainConfig.get(JdbcUtils.PASSWORD_KEY))
+      .put(JdbcUtils.SSL_KEY, true)
+        .put("replication_method", "standard")
+        .build());
+
+    KubePortManagerSingleton.init(PORTS);
+    final KubernetesClient fabricClient = new DefaultKubernetesClient();
+    final String localIp = InetAddress.getLocalHost().getHostAddress();
+    final String kubeHeartbeatUrl = localIp + ":" + 9000;
+    var workerConfigs = new WorkerConfigs(new EnvConfigs());
+    var processFactory = new KubeProcessFactory(workerConfigs, "default", fabricClient, kubeHeartbeatUrl, false);
+
+//    final Path testDir = Path.of("/tmp/airbyte_tests/");
+//    final Path workspaceRoot = Files.createTempDirectory(testDir, "test");
+//    var localRoot = Files.createTempDirectory(testDir, "output");
+//    var processFactory = new DockerProcessFactory(
+//        workerConfigs,
+//        workspaceRoot,
+//        workspaceRoot.toString(),
+//        localRoot.toString(),
+//        "host");
+
+    ResourceRequirements resourceReqs = null;
+
+    var integrationLauncher =
+          new AirbyteIntegrationLauncher("1", 0, "airbyte/source-postgres:dev", processFactory, resourceReqs, false);
+    var source = new DefaultAirbyteSource(integrationLauncher);
+    var jobRoot = "/";
+
+    final ConfiguredAirbyteCatalog catalog = Jsons.deserialize(
+        "{\"streams\":[{\"stream\":{\"name\":\"towns\",\"namespace\":\"public\",\"json_schema\":{\"type\":\"object\",\"properties\":{\"id\":{\"type\":\"number\",\"airbyte_type\":\"integer\"},\"code\":{\"type\":\"string\"},\"name\":{\"type\":\"string\"},\"article\":{\"type\":\"string\"}}},\"default_cursor_field\":[],\"supported_sync_modes\":[\"full_refresh\",\"incremental\"],\"source_defined_primary_key\":[]},\"sync_mode\":\"full_refresh\",\"primary_key\":[],\"cursor_field\":[],\"destination_sync_mode\":\"overwrite\"}]}",
+        ConfiguredAirbyteCatalog.class);
+
+    final WorkerSourceConfig sourceConfig = new WorkerSourceConfig()
+        .withSourceConnectionConfiguration(config)
+        .withState(null)
+        .withCatalog(convertProtocolObject(catalog, io.airbyte.protocol.models.ConfiguredAirbyteCatalog.class));
+
+    source.start(sourceConfig, Path.of(jobRoot));
+    var totalBytes = 0;
+    var counter = 0;
+    var start = System.currentTimeMillis();
+    while (!source.isFinished()) {
+      final Optional<AirbyteMessage> airbyteMessageOptional = source.attemptRead();
+
+      final io.airbyte.protocol.models.AirbyteMessage airbyteMessage = airbyteMessageOptional.get();
+      if (airbyteMessage.getRecord() != null) {
+        totalBytes += airbyteMessage.getRecord().getData().toString().getBytes().length;
+        counter++;
+      }
+
+      if (counter % 1_000_000 == 0) {
+        break;
+      }
+
+    }
+
+    var end = System.currentTimeMillis();
+    var totalMB = totalBytes / 1_000_000.0;
+    var totalTimeSecs = (end - start) / 1000.0;
+    log.info("total secs: {}. total MB read: {}, throughput: {}", totalTimeSecs, totalMB, totalMB / totalTimeSecs);
+    source.close();
+  }
+
+  private static <V0, V1> V0 convertProtocolObject(final V1 v1, final Class<V0> klass) {
+    return Jsons.object(Jsons.jsonNode(v1), klass);
+  }
+
+}
