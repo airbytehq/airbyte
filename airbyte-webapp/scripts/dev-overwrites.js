5a1ce1a772fb2c2ba707b106bd49c68849ea6ea3
Tim Roes <tim@airbyte.io>
2023-01-12 12:08:59 +0100

    Adjust to discussion

--- airbyte-webapp/scripts/dev-overwrites.js
+++ airbyte-webapp/scripts/dev-overwrites.js
@@ -1,46 +1,14 @@
 const fs = require("fs");
 const path = require("path");
 
-const dotenv = require("dotenv");
-
-function parseFromString(value) {
-  if (value === "true") {
-    return true;
-  }
-  if (value === "false") {
-    return false;
-  }
-  if (value.startsWith("[") || value.startsWith("{")) {
-    return JSON.parse(value);
-  }
-
-  return value;
-}
-
-const EXPERIMENTS_FILE = path.resolve(__dirname, "../.experiments.dev");
+const EXPERIMENTS_FILE = path.resolve(__dirname, "../.experiments.json");
 
 if (fs.existsSync(EXPERIMENTS_FILE)) {
-  console.log("\nOverwriting experiments from .experiments.dev ...");
-  const rawOverwrites = dotenv.parse(fs.readFileSync(EXPERIMENTS_FILE));
-  const overwrites = Object.fromEntries(
-    Object.entries(rawOverwrites).map(([key, value]) => {
-      return [key, parseFromString(value)];
-    })
-  );
+  console.log("\nOverwriting experiments from .experiments.json ...");
+  const overwrites = require(EXPERIMENTS_FILE);
 
   if (Object.keys(overwrites).length) {
     console.log(`Overwriting experiments with the following values:\n\n${JSON.stringify(overwrites, null, 2)}`);
     process.env.REACT_APP_EXPERIMENT_OVERWRITES = JSON.stringify(overwrites);
   }
-}
-
-const FEATURES_FILE = path.resolve(__dirname, "../.features.dev");
-
-if (fs.existsSync(FEATURES_FILE)) {
-  console.log("\nOverwriting feature states from .features.dev ...");
-  const overwrites = dotenv.parse(fs.readFileSync(FEATURES_FILE));
-  Object.entries(overwrites).forEach(([key, value]) => {
-    console.log(`Overwrite feature ${key} with value ${value}`);
-    process.env[`REACT_APP_FEATURE_${key}`] = value;
-  });
 }
