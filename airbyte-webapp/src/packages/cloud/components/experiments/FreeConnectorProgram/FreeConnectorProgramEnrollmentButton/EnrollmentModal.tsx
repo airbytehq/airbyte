070bbedee9bfa9c4c8e9c3ba49563aae2add448a
josephkmh <joseph@airbyte.io>
2023-01-13 16:10:34 +0100

    add enrollment modal logic

---
+++ airbyte-webapp/src/packages/cloud/components/experiments/FreeConnectorProgram/FreeConnectorProgramEnrollmentButton/EnrollmentModal.tsx
@@ -0,0 +1,134 @@
+import React, { useState } from "react";
+import { FormattedMessage } from "react-intl";
+
+import { Button, ButtonVariant } from "components/ui/Button";
+import { FlexContainer, FlexItem } from "components/ui/Flex";
+import { Heading } from "components/ui/Heading";
+import { ModalFooter } from "components/ui/Modal/ModalFooter";
+
+import { useModalService } from "hooks/services/Modal";
+import { StripeCheckoutSessionCreate, StripeCheckoutSessionRead } from "packages/cloud/lib/domain/stripe";
+import { useStripeCheckout } from "packages/cloud/services/stripe/StripeService";
+import { useCurrentWorkspaceId } from "services/workspaces/WorkspacesService";
+
+import { CardSVG } from "./CardsSvg";
+import { ConnectorGridSvg } from "./ConnectorGridSvg";
+import styles from "./EnrollmentModal.module.scss";
+import { FreeSVG } from "./FreeSvg";
+import { MailSVG } from "./MailSvg";
+
+const STRIPE_SUCCESS_QUERY = "stripeCheckoutSuccess";
+
+interface EnrollmentModalContentProps {
+  closeModal: () => void;
+  createCheckout: (p: StripeCheckoutSessionCreate) => Promise<StripeCheckoutSessionRead>;
+
+  workspaceId: string;
+}
+
+const EnrollmentModalContent: React.FC<EnrollmentModalContentProps> = ({ closeModal, createCheckout, workspaceId }) => {
+  const [isLoading, setIsLoading] = useState(false);
+
+  const startStripeCheckout = async () => {
+    setIsLoading(true);
+    // Use the current URL as a success URL but attach the STRIPE_SUCCESS_QUERY to it
+    const successUrl = new URL(window.location.href);
+    successUrl.searchParams.set(STRIPE_SUCCESS_QUERY, "true");
+    const { stripeUrl } = await createCheckout({
+      workspaceId,
+      successUrl: successUrl.href,
+      cancelUrl: window.location.href,
+      stripeMode: "setup",
+    });
+
+    // Forward to stripe as soon as we created a checkout session successfully
+    window.location.assign(stripeUrl);
+  };
+
+  return (
+    <>
+      <FlexContainer alignItems="center" justifyContent="center" className={styles.header}>
+        <div className={styles.headerBackgroundImageContainer}>
+          <ConnectorGridSvg />
+        </div>
+        <div className={styles.pill}>Pill #1</div>
+        <div className={styles.pill}>Pill #2</div>
+      </FlexContainer>
+      <div className={styles.contentWrapper}>
+        <Heading size="lg" as="h2" className={styles.contentHeader}>
+          <FormattedMessage id="freeConnectorProgram.enrollmentModal.title" />
+        </Heading>
+        <FlexContainer direction="column" gap="xl">
+          <FlexContainer>
+            <FlexContainer justifyContent="center" className={styles.iconContainer}>
+              <FreeSVG />
+            </FlexContainer>
+            <FormattedMessage id="freeConnectorProgram.enrollmentModal.free" />
+          </FlexContainer>
+          <FlexContainer>
+            <FlexContainer justifyContent="center" className={styles.iconContainer}>
+              <MailSVG />
+            </FlexContainer>
+            <FormattedMessage id="freeConnectorProgram.enrollmentModal.emailNotification" />
+          </FlexContainer>
+          <FlexContainer>
+            <FlexContainer justifyContent="center" className={styles.iconContainer}>
+              <CardSVG />
+            </FlexContainer>
+            <FormattedMessage id="freeConnectorProgram.enrollmentModal.cardOnFile" />
+          </FlexContainer>
+        </FlexContainer>
+      </div>
+
+      <ModalFooter>
+        <FlexContainer justifyContent="flex-end" gap="lg">
+          <FlexItem>
+            <Button variant="secondary" onClick={() => closeModal()}>
+              <FormattedMessage id="freeConnectorProgram.enrollmentModal.cancelButtonText" />
+            </Button>
+          </FlexItem>
+          <FlexItem>
+            <Button isLoading={isLoading} onClick={() => startStripeCheckout()}>
+              <FormattedMessage id="freeConnectorProgram.enrollmentModal.enrollButtonText" />
+            </Button>
+          </FlexItem>
+        </FlexContainer>
+      </ModalFooter>
+    </>
+  );
+};
+
+interface FreeConnectorProgramEnrollmentButtonProps {
+  buttonTextKey?: string;
+  variant?: ButtonVariant;
+}
+
+export const FreeConnectorProgramEnrollmentButton: React.FC<FreeConnectorProgramEnrollmentButtonProps> = ({
+  buttonTextKey = "freeConnectorProgram.enrollmentModal.enrollButtonText",
+  variant = "primary",
+}) => {
+  const { openModal, closeModal } = useModalService();
+  const { error, mutateAsync: createCheckout } = useStripeCheckout();
+  const workspaceId = useCurrentWorkspaceId();
+
+  console.log(error);
+
+  const showEnrollmentModal = () => {
+    openModal({
+      title: null,
+      content: () => (
+        <EnrollmentModalContent workspaceId={workspaceId} createCheckout={createCheckout} closeModal={closeModal} />
+      ),
+    });
+  };
+
+  return (
+    <FlexContainer justifyContent="flex-end">
+      <FlexItem>
+        <Button onClick={() => showEnrollmentModal()} variant={variant}>
+          <FormattedMessage id={buttonTextKey} />
+        </Button>
+      </FlexItem>
+    </FlexContainer>
+  );
+};
