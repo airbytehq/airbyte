f1f0f03ea5b1383f1740566e5c3a29410dcb7c5f
Benoit Moriceau <benoit@airbyte.io>
2023-01-13 12:03:06 -0800

    Allow the OSS server only on OSS (#21416)

--- airbyte-server/src/main/java/io/airbyte/server/apis/HealthApiController.java
+++ airbyte-server/src/main/java/io/airbyte/server/apis/HealthApiController.java
@@ -7,11 +7,14 @@ package io.airbyte.server.apis;
 import io.airbyte.api.generated.HealthApi;
 import io.airbyte.api.model.generated.HealthCheckRead;
 import io.airbyte.server.handlers.HealthCheckHandler;
+import io.micronaut.context.annotation.Requires;
 import io.micronaut.http.MediaType;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
 
 @Controller("/api/v1/health")
+@Requires(property = "airbyte.deployment-mode",
+          value = "OSS")
 public class HealthApiController implements HealthApi {
 
   private final HealthCheckHandler healthCheckHandler;
