955885d80447975bd059cb2cfcc7ccd8ad52f493
tealjulia <larson.teal@gmail.com>
2023-01-13 11:21:38 -0500

    use experiment hook

--- airbyte-webapp/src/packages/cloud/components/experiments/FreeConnectorProgram/hooks/useFreeConnectorProgram.tsx
+++ airbyte-webapp/src/packages/cloud/components/experiments/FreeConnectorProgram/hooks/useFreeConnectorProgram.tsx
@@ -1,14 +1,15 @@
 import { ReleaseStage, WebBackendConnectionRead } from "core/request/AirbyteClient";
+import { useExperiment } from "hooks/services/Experiment";
 import { useDestinationDefinitionList } from "services/connector/DestinationDefinitionService";
 import { useSourceDefinitionList } from "services/connector/SourceDefinitionService";
 
 export const useFreeConnectorProgram = () => {
-  // const isFreeConnectorProgramEnabled = useExperiment("workspace.freeConnectorsProgram.visible", false);
-  const isFreeConnectorProgramEnabled = true;
+  const isFreeConnectorProgramEnabled = useExperiment("workspace.freeConnectorsProgram.visible", false);
+
   const { sourceDefinitions } = useSourceDefinitionList();
   const { destinationDefinitions } = useDestinationDefinitionList();
 
-  // todo: implement actual call once merged with issue #4006
+  // todo: implement actual call once it is merged with issue #4006
   // for now, we'll just default to false for enrolled
   const enrolledInFreeConnectorProgram = false;
 
