60d8bc4613587552859b0095f6fcd7622656938c
erohmensing <erohmensing@gmail.com>
2023-01-13 15:08:00 -0600

    Clarify what it means when StopIteration is returned by helper methods

--- airbyte-cdk/python/airbyte_cdk/sources/streams/utils/stream_helper.py
+++ airbyte-cdk/python/airbyte_cdk/sources/streams/utils/stream_helper.py
@@ -12,7 +12,8 @@ def get_first_stream_slice(stream) -> Optional[Mapping[str, Any]]:
     """
     Gets the first stream_slice from a given stream's stream_slices.
     :param stream: stream
-    :return: First stream slice from 'stream_slices' generator
+    :raises StopIteration: if there is no first slice to return (the stream_slices generator is empty)
+    :return: first stream slice from 'stream_slices' generator (`None` is a valid stream slice)
     """
     # We wrap the return output of stream_slices() because some implementations return types that are iterable,
     # but not iterators such as lists or tuples
@@ -30,7 +31,8 @@ def get_first_record_for_slice(stream: Stream, stream_slice: Optional[Mapping[st
     Gets the first record for a stream_slice of a stream.
     :param stream: stream
     :param stream_slice: stream_slice
-    :return: StreamData containing the first record in the slice.
+    :raises StopIteration: if there is no first record to return (the read_records generator is empty)
+    :return: StreamData containing the first record in the slice
     """
     records_for_slice = stream.read_records(sync_mode=SyncMode.full_refresh, stream_slice=stream_slice)
     return next(records_for_slice)
