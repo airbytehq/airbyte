0f4d870f3f2373fda4a71855f76afb0fa5aac23b
josephkmh <joseph@airbyte.io>
2023-01-17 08:48:33 +0000

    fallback connector builder api url

--- airbyte-webapp/src/config/config.ts
+++ airbyte-webapp/src/config/config.ts
@@ -9,7 +9,8 @@ export const newStaticConfig: AirbyteWebappConfig = {
     window.API_URL ??
     process.env.REACT_APP_API_URL ??
     `${window.location.protocol}//${window.location.hostname}:8001/api`,
-  connectorBuilderApiUrl: process.env.REACT_APP_CONNECTOR_BUILDER_API_URL ?? "",
+  connectorBuilderApiUrl:
+    process.env.REACT_APP_CONNECTOR_BUILDER_API_URL ?? `${window.location.protocol}//${window.location.hostname}:8003`,
   healthCheckInterval: 20000,
   version: window.AIRBYTE_VERSION ?? "dev",
   integrationUrl: process.env.REACT_APP_INTEGRATION_DOCS_URLS ?? "/docs",
