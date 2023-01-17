d680628d666e8a6e8ad5c02a5d084f8ce8d80897
josephkmh <joseph@airbyte.io>
2023-01-17 14:31:20 +0000

    expand default config object

--- airbyte-webapp/src/config/config.ts
+++ airbyte-webapp/src/config/config.ts
@@ -2,7 +2,7 @@ import { AirbyteWebappConfig } from "./types";
 
 export const newStaticConfig: AirbyteWebappConfig = {
   segment: {
-    token: window.SEGMENT_TOKEN ?? process.env.REACT_APP_SEGMENT_TOKEN ?? "",
+    token: window.SEGMENT_TOKEN ?? process.env.REACT_APP_SEGMENT_TOKEN,
     enabled: window.TRACKING_STRATEGY === "segment",
   },
   apiUrl:
@@ -14,4 +14,23 @@ export const newStaticConfig: AirbyteWebappConfig = {
   healthCheckInterval: 20000,
   version: window.AIRBYTE_VERSION ?? "dev",
   integrationUrl: process.env.REACT_APP_INTEGRATION_DOCS_URLS ?? "/docs",
+  oauthRedirectUrl: `${window.location.protocol}//${window.location.host}`,
+  cloudApiUrl: window.CLOUD_API_URL ?? process.env.REACT_APP_CLOUD_API_URL,
+  cloudPublicApiUrl: process.env.REACT_APP_CLOUD_PUBLIC_API_URL,
+  firebase: {
+    apiKey: window.FIREBASE_API_KEY ?? process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: window.FIREBASE_AUTH_DOMAIN ?? process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    authEmulatorHost: window.FIREBASE_AUTH_EMULATOR_HOST ?? process.env.REACT_APP_FIREBASE_AUTH_EMULATOR_HOST,
+  },
+  intercom: {
+    appId: process.env.REACT_APP_INTERCOM_APP_ID,
+  },
+  launchDarkly: window.LAUNCHDARKLY_KEY ?? process.env.REACT_APP_LAUNCHDARKLY_KEY,
 };
+
+export class MissingConfigError extends Error {
+  constructor(message: string) {
+    super(message);
+    this.name = "MissingConfigError";
+  }
+}
