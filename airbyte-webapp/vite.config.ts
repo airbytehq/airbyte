df8f6417d50db88d53cdb489cef5c36f509583f2
Tim Roes <tim@airbyte.io>
2023-01-17 12:47:22 +0100

    create vite-plugins package

--- airbyte-webapp/vite.config.ts
+++ airbyte-webapp/vite.config.ts
@@ -1,37 +1,14 @@
-import fs from "fs";
 import path from "path";
 
 import basicSsl from "@vitejs/plugin-basic-ssl";
 import react from "@vitejs/plugin-react";
-import { loadEnv, Plugin, UserConfig } from "vite";
+import { loadEnv, UserConfig } from "vite";
 import { defineConfig } from "vite";
 import checker from "vite-plugin-checker";
 import svgrPlugin from "vite-plugin-svgr";
 import viteTsconfigPaths from "vite-tsconfig-paths";
 
-// https://github.com/bvaughn/react-virtualized/issues/1632
-const WRONG_CODE = `import { bpfrpt_proptype_WindowScroller } from "../WindowScroller.js";`;
-export function patchReactVirtualized(): Plugin {
-  return {
-    name: "flat:react-virtualized",
-    // Note: we cannot use the `transform` hook here
-    //       because libraries are pre-bundled in vite directly,
-    //       plugins aren't able to hack that step currently.
-    //       so instead we manually edit the file in node_modules.
-    //       all we need is to find the timing before pre-bundling.
-    configResolved() {
-      const file = require
-        .resolve("react-lazylog/node_modules/react-virtualized")
-        .replace(
-          path.join("dist", "commonjs", "index.js"),
-          path.join("dist", "es", "WindowScroller", "utils", "onScroll.js")
-        );
-      const code = fs.readFileSync(file, "utf-8");
-      const modified = code.replace(WRONG_CODE, "");
-      fs.writeFileSync(file, modified);
-    },
-  };
-}
+import { docMiddleware, patchReactVirtualized } from "./packages/vite-plugins";
 
 export default defineConfig(({ mode }) => {
   // Load variables from all .env files
@@ -75,6 +52,7 @@ export default defineConfig(({ mode }) => {
         typescript: true,
       }),
       patchReactVirtualized(),
+      docMiddleware(),
     ],
     // Use `REACT_APP_` as a prefix for environment variables that should be accessible from within FE code.
     envPrefix: ["REACT_APP_"],
