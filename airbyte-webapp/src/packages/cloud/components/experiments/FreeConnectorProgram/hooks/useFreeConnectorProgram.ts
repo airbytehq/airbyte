77848b97c14ab235e7975fd4049102ebc39f55dd
Alex Birdsall <ambirdsall@gmail.com>
2023-01-18 00:13:48 -0800

    Clean up useFreeConnectorProgram
    - rename the function to match its filename
    - check the experiment within the hook, not at every call site

--- airbyte-webapp/src/packages/cloud/components/experiments/FreeConnectorProgram/hooks/useFreeConnectorProgram.ts
+++ airbyte-webapp/src/packages/cloud/components/experiments/FreeConnectorProgram/hooks/useFreeConnectorProgram.ts
@@ -1,27 +1,26 @@
 import { useQuery } from "react-query";
 
-import { useAuthService } from "packages/cloud/services/auth/AuthService";
+import { useExperiment } from "hooks/services/Experiment";
 import { useConfig } from "packages/cloud/services/config";
 import { useDefaultRequestMiddlewares } from "services/useDefaultRequestMiddlewares";
 import { useCurrentWorkspaceId } from "services/workspaces/WorkspacesService";
 
 import { webBackendGetFreeConnectorProgramInfoForWorkspace } from "../lib/api";
 
-export const useFreeConnectorProgramInfo = () => {
+export const useFreeConnectorProgram = () => {
   const workspaceId = useCurrentWorkspaceId();
   const { cloudApiUrl } = useConfig();
   const config = { apiUrl: cloudApiUrl };
   const middlewares = useDefaultRequestMiddlewares();
   const requestOptions = { config, middlewares };
-  const { emailVerified } = useAuthService();
+  const freeConnectorProgramEnabled = useExperiment("workspace.freeConnectorsProgram.visible", false);
 
   return useQuery(["freeConnectorProgramInfo", workspaceId], () =>
     webBackendGetFreeConnectorProgramInfoForWorkspace({ workspaceId }, requestOptions).then(
       ({ hasEligibleConnector, hasPaymentAccountSaved }) => {
-        const showEnrollmentUi = !hasPaymentAccountSaved && hasEligibleConnector;
-        const needsEmailVerification: boolean = emailVerified;
+        const userIsEligibleToEnroll = !hasPaymentAccountSaved && hasEligibleConnector;
 
-        return { showEnrollmentUi, needsEmailVerification };
+        return freeConnectorProgramEnabled && userIsEligibleToEnroll;
       }
     )
   );
