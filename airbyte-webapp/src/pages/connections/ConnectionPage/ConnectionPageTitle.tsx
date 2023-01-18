b66b9bbbd14648adb13ec96ab400ce9f70e453bb
Teal Larson <LARSON.TEAL@GMAIL.COM>
2023-01-18 12:45:05 -0500

    🪟 🎉 Free Connectors Program inline enrollment callout (#21315)
    * genericize alert component
    
    * add className prop to alert
    
    * rename infobox "callout", swap in place for scss module
    
    * fix imports
    
    * use callout in place of alert
    
    * add blue variant, remove icon prop
    
    * remove single use prop, use a classname instead
    
    * WIP
    
    * WIP: i18n is mad
    
    * add render logic
    
    * make i18n happy
    
    * create a hook instead
    
    * make structure more like other experiments
    
    * fix import order
    
    * cleanup from rebase
    
    * cleanup
    
    * one more cleanup bit
    
    * use new variant name
    
    * pointer
    
    * use experiment hook
    
    * use button instead of span for enroll link
    
    * remove unneeded important
    
    * remove even more css
    
    * hook up enrollment modal
    
    * don't show during connector setup (yet)
    
    * implement api data

--- airbyte-webapp/src/pages/connections/ConnectionPage/ConnectionPageTitle.tsx
+++ airbyte-webapp/src/pages/connections/ConnectionPage/ConnectionPageTitle.tsx
@@ -7,11 +7,15 @@ import { useNavigate, useParams } from "react-router-dom";
 import { ConnectionInfoCard } from "components/connection/ConnectionInfoCard";
 import { ConnectionName } from "components/connection/ConnectionName";
 import { Callout } from "components/ui/Callout";
+import { FlexContainer } from "components/ui/Flex";
 import { StepsMenu } from "components/ui/StepsMenu";
 import { Text } from "components/ui/Text";
 
 import { ConnectionStatus } from "core/request/AirbyteClient";
 import { useConnectionEditService } from "hooks/services/ConnectionEdit/ConnectionEditService";
+import { useExperiment } from "hooks/services/Experiment";
+import { useFreeConnectorProgramInfo } from "packages/cloud/components/experiments/FreeConnectorProgram/hooks/useFreeConnectorProgram";
+import { InlineEnrollmentCallout } from "packages/cloud/components/experiments/FreeConnectorProgram/InlineEnrollmentCallout";
 
 import { ConnectionRoutePaths } from "../types";
 import styles from "./ConnectionPageTitle.module.scss";
@@ -22,6 +26,11 @@ export const ConnectionPageTitle: React.FC = () => {
   const currentStep = params["*"] || ConnectionRoutePaths.Status;
 
   const { connection } = useConnectionEditService();
+
+  const { data: freeConnectorProgramInfo } = useFreeConnectorProgramInfo();
+  const freeConnectorProgramEnabled = useExperiment("workspace.freeConnectorsProgram.visible", false);
+
+  const displayEnrollmentCallout = freeConnectorProgramEnabled && freeConnectorProgramInfo?.showEnrollmentUi;
 
   const steps = useMemo(() => {
     const steps = [
@@ -72,7 +81,10 @@ export const ConnectionPageTitle: React.FC = () => {
       </Text>
       <ConnectionName />
       <div className={styles.statusContainer}>
-        <ConnectionInfoCard />
+        <FlexContainer direction="column" gap="none">
+          <ConnectionInfoCard />
+          {displayEnrollmentCallout && <InlineEnrollmentCallout />}
+        </FlexContainer>
       </div>
       <StepsMenu lightMode data={steps} onSelect={onSelectStep} activeStep={currentStep} />
     </div>
