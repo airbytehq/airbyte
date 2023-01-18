f826281d93c74ff4666eb14a2cfb5f8d587344a0
Sebastian Sundet Flaen <sebastianflaen@gmail.com>
2023-01-18 19:50:00 +0100

    Utilization stream part 1

--- airbyte-integrations/connectors/source-dentclinic/source_dentclinic/source.py
+++ airbyte-integrations/connectors/source-dentclinic/source_dentclinic/source.py
@@ -13,7 +13,7 @@ from airbyte_cdk.sources import AbstractSource
 from airbyte_cdk.sources.streams import Stream
 from airbyte_cdk.sources.streams.http import HttpStream
 
-from .streams import DentclinicBookingStream, DentclinicClinicIdsStream, DentclinicStaticStream
+from .streams import DentclinicBookingStream, DentclinicClinicIdsStream, DentclinicStaticStream, DentclinicUtilizationStream
 
 
 class BookingsFr(DentclinicBookingStream):
@@ -35,6 +35,42 @@ class BookingsFr(DentclinicBookingStream):
 class Bookings(DentclinicBookingStream):
     state_checkpoint_interval = 1
     primary_key = "Id"
+
+    def path(
+            self, stream_state: Mapping[str, Any] = None, stream_slice: Mapping[str, Any] = None, next_page_token: Mapping[str, Any] = None
+    ) -> str:
+        """should return "bookings". Required."""
+        return ""
+
+    @property
+    def http_method(self) -> str:
+        return "POST"
+
+    @property
+    def cursor_field(self) -> str:
+        """
+        This field's presence tells the framework this in an incremental stream. Required for incremental.
+
+        :return str: The name of the cursor field.
+        """
+        return "Time"
+
+    def get_updated_state(self, current_stream_state: MutableMapping[str, Any], latest_record: Mapping[str, Any]) -> Mapping[str, Any]:
+        """
+        Override to determine the latest state after reading the latest record. This typically compared the cursor_field from the latest record and
+        the current state and picks the 'most' recent cursor. This is how a stream's state is determined. Required for incremental.
+        """
+        state_mapping = current_stream_state.get(self.cursor_field, {})
+
+        last_record_value = latest_record.get(self.cursor_field)
+        if last_record_value:
+            state_mapping.update({self.clinic_id: last_record_value})
+
+        return {self.cursor_field: state_mapping}
+
+class UtilizationReport(DentclinicUtilizationStream):
+    state_checkpoint_interval = 1
+    primary_key = "ProfessionalId"
 
     def path(
             self, stream_state: Mapping[str, Any] = None, stream_slice: Mapping[str, Any] = None, next_page_token: Mapping[str, Any] = None
@@ -156,6 +192,6 @@ class SourceDentclinic(AbstractSource):
         :param config: A Mapping of the user input configuration as defined in the connector spec.
         """
         return [BookingsFr(config=config, enable_days_back_limit=True), Bookings(config=config), Clinics(config=config),
-                Services(config=config), Resources(config=config), PatientsReport(config=config)]
+                Services(config=config), Resources(config=config), PatientsReport(config=config), UtilizationReport(config=config)]
 
 
--- airbyte-integrations/connectors/source-dentclinic/source_dentclinic/streams.py
+++ airbyte-integrations/connectors/source-dentclinic/source_dentclinic/streams.py
@@ -151,6 +151,149 @@ class DentclinicBookingStream(HttpStream, ABC):
         yield from data
 
 
+
+class DentclinicUtilizationStream(HttpStream, ABC):
+    primary_key = None
+    state_checkpoint_interval = 1
+
+    url_base = "https://dcm-nhn.dentclinicmanager.com/API/DCMConnect.asmx?WSDL"
+
+    def __init__(self, config: Mapping[str, Any], enable_days_back_limit: bool = False, **kwargs):
+        super().__init__()
+        self.fetch_interval_days = int(config.get('fetch_interval_days', 1))
+
+        self.api_key = config.get("api_key")
+        self.days_forward = int(config.get("days_forward"))
+
+        self.start_date = pendulum.parse(config.get("start_date"))
+        if enable_days_back_limit:
+            self.start_date = max(self.start_date, pendulum.today().add(days = -int(config.get('days_back'))))
+        self.stop_date = pendulum.today().add(days=self.days_forward)
+
+        self.cursor_start_date = self.start_date
+        self.cursor_end_date = self.start_date.add(days=self.fetch_interval_days)
+
+        self.clinic_ids = self.get_clinic_ids()
+        self.clinic_id = next(self.clinic_ids)
+
+    def request_headers(
+            self, stream_state: Mapping[str, Any], stream_slice: Mapping[str, Any] = None, next_page_token: Mapping[str, Any] = None
+    ) -> Mapping[str, Any]:
+        return {'Content-Type': 'application/soap+xml; charset=utf-8'}
+
+    @property
+    def cursor_field(self) -> str:
+        """
+        :return str: The name of the cursor field.
+        """
+        return "stop_date"
+
+    @property
+    def raise_on_http_errors(self) -> bool:
+        """
+        Override if needed. If set to False, allows opting-out of raising HTTP code exception.
+        """
+        return False
+
+    def should_retry(self, response: requests.Response) -> bool:
+        """
+        Override to set different conditions for backoff based on the response from the server.
+
+        By default, back off on the following HTTP response statuses:
+         - 429 (Too Many Requests) indicating rate limiting
+         - 500s to handle transient server errors
+
+        Unexpected but transient exceptions (connection timeout, DNS resolution failed, etc..) are retried by default.
+        """
+        return response.status_code == 429 or 501 <= response.status_code < 600
+
+    def get_clinic_ids(self) -> Iterator[str]:
+        payload_clinics = f"""<?xml version="1.0" encoding="utf-8"?>
+            <soap12:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap12="http://www.w3.org/2003/05/soap-envelope">
+                <soap12:Body>
+                    <GetClinics xmlns="http://tempuri.org/">
+                        <key>{self.api_key}</key>
+                    </GetClinics>
+                </soap12:Body>
+            </soap12:Envelope>
+        """
+        headers = {'Content-Type': 'application/soap+xml; charset=utf-8'}
+        response = requests.post(f"{self.url_base}", data=payload_clinics, headers=headers)
+
+        clinics = xmltodict.parse(response.text)['soap:Envelope']['soap:Body']['GetClinicsResponse']['GetClinicsResult']['ClinicModel']
+        clinic_ids = [x.get('Id') for x in clinics]
+        return iter(clinic_ids)
+
+    def next_page_token(self, response: requests.Response) -> Optional[Mapping[str, Any]]:
+        """
+        :param response: the most recent response from the API
+        :return If there is another page in the result, a mapping (e.g: dict) containing information needed to query the next page in the response.
+                If there are no more pages in the result, return None.
+        """
+
+        print("*" * 100)
+        print({"clinic_id": self.clinic_id, "start_date": self.cursor_start_date, "end_date": self.cursor_end_date})
+        print("*" * 100)
+
+        if self.clinic_id is None:
+            return None
+
+        return {"start_date": self.cursor_start_date, "end_date": self.cursor_end_date}
+
+    def request_body_data(self, stream_state: Mapping[str, Any], stream_slice: Mapping[str, Any] = None,
+                          next_page_token: Mapping[str, Any] = None, ) -> Optional[Mapping]:
+
+        if self.cursor_end_date >= self.stop_date:
+            self.clinic_id = next(self.clinic_ids, None)
+            if self.clinic_id is None:
+                return ""
+
+            self.cursor_start_date = self.start_date
+            self.cursor_end_date = self.start_date.add(days=self.fetch_interval_days)
+        else:
+            self.cursor_start_date = self.cursor_start_date.add(days=self.fetch_interval_days)
+            self.cursor_end_date = self.cursor_end_date.add(days=self.fetch_interval_days)
+
+        start_ts = self.cursor_start_date
+        end_ts = self.cursor_end_date
+
+        clinic_state = stream_state.get(self.cursor_field, {}).get(self.clinic_id)
+        if clinic_state:
+            state_ts = pendulum.parse(clinic_state)
+            start_ts = max(start_ts, state_ts)
+            end_ts = start_ts.add(days=self.fetch_interval_days)
+
+        return f"""<?xml version="1.0" encoding="utf-8"?>
+        <soap12:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap12="http://www.w3.org/2003/05/soap-envelope">
+          <soap12:Body>
+            <GetUtilizationReport xmlns="http://tempuri.org/">
+              <key>{self.api_key}</key>
+              <clinicId>{self.clinic_id}</clinicId>
+              <dateFrom>{start_ts}</dateFrom>
+              <dateUntil>{end_ts}</dateUntil>
+            </GetUtilizationReport>
+          </soap12:Body>
+        </soap12:Envelope>"""
+
+    def parse_response(self, response: requests.Response, **kwargs) -> Iterable[Mapping]:
+        """
+        :return an iterable containing each record in the response
+        """
+
+        path = ['soap:Envelope', 'soap:Body', 'GetUtilizationReportResponse', 'GetUtilizationReportResult','ProfessionalsAndShifts','ProfessionalShifts']
+        data = xmltodict.parse(response.text).copy()
+        for key in path:
+            data = data.get(key)
+            if data is None:
+                data = []
+                break
+
+        if type(data) == dict:
+            data = [data]
+
+        yield from data
+
+
 class DentclinicStaticStream(HttpStream, ABC):
     """Base stream"""
     primary_key = None
