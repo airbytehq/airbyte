3f9f5a02c99cf4db4933390d213db937c0b53ee1
Alex Birdsall <ambirdsall@gmail.com>
2023-01-18 00:13:48 -0800

    Clean up useFreeConnectorProgram
    - rename the function to match its filename
    - check the experiment within the hook, not at every call site

--- airbyte-webapp/src/packages/cloud/components/experiments/FreeConnectorProgram/hooks/useFreeConnectorProgram.ts
+++ airbyte-webapp/src/packages/cloud/components/experiments/FreeConnectorProgram/hooks/useFreeConnectorProgram.ts
@@ -1,5 +1,6 @@
 import { useQuery } from "react-query";
 
+import { useExperiment } from "hooks/services/Experiment";
 import { useConfig } from "packages/cloud/services/config";
 import { useDefaultRequestMiddlewares } from "services/useDefaultRequestMiddlewares";
 import { useCurrentWorkspaceId } from "services/workspaces/WorkspacesService";
@@ -12,13 +13,14 @@ export const useFreeConnectorProgram = () => {
   const config = { apiUrl: cloudApiUrl };
   const middlewares = useDefaultRequestMiddlewares();
   const requestOptions = { config, middlewares };
+  const freeConnectorProgramEnabled = useExperiment("workspace.freeConnectorsProgram.visible", false);
 
   return useQuery(["freeConnectorProgramInfo", workspaceId], () =>
     webBackendGetFreeConnectorProgramInfoForWorkspace({ workspaceId }, requestOptions).then(
       ({ hasEligibleConnector, hasPaymentAccountSaved }) => {
         const userIsEligibleToEnroll = !hasPaymentAccountSaved && hasEligibleConnector;
 
-        return userIsEligibleToEnroll;
+        return freeConnectorProgramEnabled && userIsEligibleToEnroll;
       }
     )
   );
