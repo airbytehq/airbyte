e11c17b1068cb0ae351a6a6ffc6209eb8970dad3
Xiaohan Song <xiaohan@airbyte.io>
2023-01-17 17:10:58 -0800

    worker change 1

--- airbyte-commons-worker/src/main/java/io/airbyte/workers/general/DefaultDiscoverCatalogWorker.java
+++ airbyte-commons-worker/src/main/java/io/airbyte/workers/general/DefaultDiscoverCatalogWorker.java
@@ -10,6 +10,8 @@ import static io.airbyte.metrics.lib.ApmTraceConstants.Tags.SOURCE_ID_KEY;
 import static io.airbyte.metrics.lib.ApmTraceConstants.WORKER_OPERATION_NAME;
 
 import datadog.trace.api.Trace;
+import io.airbyte.api.client.AirbyteApiClient;
+import io.airbyte.api.client.model.generated.SourceDiscoverSchemaWriteRequestBody;
 import io.airbyte.commons.io.LineGobbler;
 import io.airbyte.commons.json.Jsons;
 import io.airbyte.config.ConnectorJobOutput;
@@ -43,28 +45,26 @@ public class DefaultDiscoverCatalogWorker implements DiscoverCatalogWorker {
 
   private static final Logger LOGGER = LoggerFactory.getLogger(DefaultDiscoverCatalogWorker.class);
 
-  private final ConfigRepository configRepository;
-
   private final IntegrationLauncher integrationLauncher;
   private final AirbyteStreamFactory streamFactory;
   private final ConnectorConfigUpdater connectorConfigUpdater;
-
+  private final AirbyteApiClient airbyteApiClient;
   private volatile Process process;
 
-  public DefaultDiscoverCatalogWorker(final ConfigRepository configRepository,
+  public DefaultDiscoverCatalogWorker(final AirbyteApiClient airbyteApiClient,
                                       final IntegrationLauncher integrationLauncher,
                                       final ConnectorConfigUpdater connectorConfigUpdater,
                                       final AirbyteStreamFactory streamFactory) {
-    this.configRepository = configRepository;
+    this.airbyteApiClient = airbyteApiClient;
     this.integrationLauncher = integrationLauncher;
     this.streamFactory = streamFactory;
     this.connectorConfigUpdater = connectorConfigUpdater;
   }
 
-  public DefaultDiscoverCatalogWorker(final ConfigRepository configRepository,
+  public DefaultDiscoverCatalogWorker(final AirbyteApiClient airbyteApiClient,
                                       final IntegrationLauncher integrationLauncher,
                                       final ConnectorConfigUpdater connectorConfigUpdater) {
-    this(configRepository, integrationLauncher, connectorConfigUpdater, new DefaultAirbyteStreamFactory());
+    this(airbyteApiClient, integrationLauncher, connectorConfigUpdater, new DefaultAirbyteStreamFactory());
   }
 
   @Trace(operationName = WORKER_OPERATION_NAME)
@@ -103,12 +103,12 @@ public class DefaultDiscoverCatalogWorker implements DiscoverCatalogWorker {
 
       if (catalog.isPresent()) {
         final UUID catalogId =
-            configRepository.writeActorCatalogFetchEvent(catalog.get(),
+            airbyteApiClient.getSourceApi().writeDiscoverFetchEvent(new SourceDiscoverSchemaWriteRequestBody().catalog(CatalogConverter.toApi(catalog.get())).sourceId(
                 // NOTE: sourceId is marked required in the OpenAPI config but the code generator doesn't enforce
                 // it, so we check again here.
-                discoverSchemaInput.getSourceId() == null ? null : UUID.fromString(discoverSchemaInput.getSourceId()),
-                discoverSchemaInput.getConnectorVersion(),
-                discoverSchemaInput.getConfigHash());
+                discoverSchemaInput.getSourceId() == null ? null : UUID.fromString(discoverSchemaInput.getSourceId())).connectorVersion(
+                discoverSchemaInput.getConnectorVersion()).configurationHash(
+                discoverSchemaInput.getConfigHash()));
         jobOutput.setDiscoverCatalogId(catalogId);
       } else if (failureReason.isEmpty()) {
         WorkerUtils.throwWorkerException("Integration failed to output a catalog struct and did not output a failure reason", process);
