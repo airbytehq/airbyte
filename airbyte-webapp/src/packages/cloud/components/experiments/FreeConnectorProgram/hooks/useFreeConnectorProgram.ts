ca894d3cf2fbfe27cadbc3e693dc1ced24d43fc5
Alex Birdsall <ambirdsall@gmail.com>
2023-01-17 16:49:49 -0800

    check email verification status for real

--- airbyte-webapp/src/packages/cloud/components/experiments/FreeConnectorProgram/hooks/useFreeConnectorProgram.ts
+++ airbyte-webapp/src/packages/cloud/components/experiments/FreeConnectorProgram/hooks/useFreeConnectorProgram.ts
@@ -1,6 +1,6 @@
 import { useQuery } from "react-query";
 
-import { useExperiment } from "hooks/services/Experiment";
+import { useAuthService } from "packages/cloud/services/auth/AuthService";
 import { useConfig } from "packages/cloud/services/config";
 import { useDefaultRequestMiddlewares } from "services/useDefaultRequestMiddlewares";
 import { useCurrentWorkspaceId } from "services/workspaces/WorkspacesService";
@@ -13,14 +13,13 @@ export const useFreeConnectorProgramInfo = () => {
   const config = { apiUrl: cloudApiUrl };
   const middlewares = useDefaultRequestMiddlewares();
   const requestOptions = { config, middlewares };
-  const freeConnectorProgramEnabled = useExperiment("workspace.freeConnectorsProgram.visible", false);
+  const { emailVerified } = useAuthService();
 
   return useQuery(["freeConnectorProgramInfo", workspaceId], () =>
     webBackendGetFreeConnectorProgramInfoForWorkspace({ workspaceId }, requestOptions).then(
       ({ hasEligibleConnector, hasPaymentAccountSaved }) => {
-        const showEnrollmentUi = !hasPaymentAccountSaved && hasEligibleConnector && freeConnectorProgramEnabled;
-        // TODO hardcoding this value to allow testing while data source gets sorted out
-        const needsEmailVerification = false;
+        const showEnrollmentUi = !hasPaymentAccountSaved && hasEligibleConnector;
+        const needsEmailVerification: boolean = emailVerified;
 
         return { showEnrollmentUi, needsEmailVerification };
       }
