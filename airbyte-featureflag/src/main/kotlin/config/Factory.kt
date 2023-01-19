14b76766c3b98c62b66601ff94b397b00c22fca4
Cole Snodgrass <cole@airbyte.io>
2023-01-18 16:13:43 -0800

    change factory to always return one of the implementations

--- airbyte-featureflag/src/main/kotlin/config/Factory.kt
+++ airbyte-featureflag/src/main/kotlin/config/Factory.kt
@@ -10,7 +10,6 @@ import io.airbyte.featureflag.FeatureFlagClient
 import io.airbyte.featureflag.LaunchDarklyClient
 import io.micronaut.context.annotation.Factory
 import io.micronaut.context.annotation.Property
-import io.micronaut.context.annotation.Requirements
 import io.micronaut.context.annotation.Requires
 import jakarta.inject.Singleton
 import java.nio.file.Path
@@ -20,22 +19,22 @@ internal const val CONFIG_OSS_KEY = "airbyte.feature-flag.path"
 
 @Factory
 class Factory {
-    @Requirements(
-        Requires(property = CONFIG_LD_KEY),
-        Requires(missingProperty = CONFIG_OSS_KEY),
-    )
+
+    @Requires(property = CONFIG_LD_KEY)
     @Singleton
-    fun Cloud(@Property(name = CONFIG_LD_KEY) apiKey: String): FeatureFlagClient {
+    fun LaunchDarklyClient(@Property(name = CONFIG_LD_KEY) apiKey: String): FeatureFlagClient {
         val client = LDClient(apiKey)
         return LaunchDarklyClient(client)
     }
 
-    @Requirements(
-        Requires(property = CONFIG_OSS_KEY),
-        Requires(missingProperty = CONFIG_LD_KEY),
-    )
-    fun Platform(@Property(name = CONFIG_OSS_KEY) configPath: String): FeatureFlagClient {
-        val path = Path.of(configPath)
+    @Requires(missingProperty = CONFIG_LD_KEY)
+    @Singleton
+    fun ConfigFileClient(@Property(name = CONFIG_OSS_KEY) configPath: String): FeatureFlagClient {
+        val path: Path? = if (configPath.isNotBlank()) {
+            Path.of(configPath)
+        } else {
+            null
+        }
         return ConfigFileClient(path)
     }
 }
