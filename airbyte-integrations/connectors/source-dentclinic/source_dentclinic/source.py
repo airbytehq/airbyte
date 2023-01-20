5bf0f3c5fc40ac92c599ef5f6c9184e25a3c2c3e
Sebastian Sundet Flaen <sebastianflaen@gmail.com>
2023-01-20 12:11:24 +0100

    Feature: treatments report

--- airbyte-integrations/connectors/source-dentclinic/source_dentclinic/source.py
+++ airbyte-integrations/connectors/source-dentclinic/source_dentclinic/source.py
@@ -23,6 +23,7 @@ class BookingsFr(DentclinicIncrementalStream):
     static_endpoint = "GetBookings"
     date_from_field = "dateTimeStart"
     date_until_field = "dateTimeEnd"
+
     def path(
             self, stream_state: Mapping[str, Any] = None, stream_slice: Mapping[str, Any] = None, next_page_token: Mapping[str, Any] = None
     ) -> str:
@@ -142,6 +143,70 @@ class UtilizationReportFr(DentclinicIncrementalStream):
         return "POST"
 
 
+class TreatmentsReport(DentclinicIncrementalStream):
+    state_checkpoint_interval = 1
+    primary_key = "Id"
+    endpoint_data_path = ['soap:Envelope', 'soap:Body',
+                          'GetTreatmentsReportResponse', 'GetTreatmentsReportResult', 'ExecutedTreatmentModel']
+    static_endpoint = "GetTreatmentsReport"
+    date_from_field = "dateFrom"
+    date_until_field = "dateUntil"
+
+    def path(
+            self, stream_state: Mapping[str, Any] = None, stream_slice: Mapping[str, Any] = None, next_page_token: Mapping[str, Any] = None
+    ) -> str:
+        """should return "bookings". Required."""
+        return ""
+
+    @property
+    def http_method(self) -> str:
+        return "POST"
+
+    @property
+    def cursor_field(self) -> str:
+        """
+        This field's presence tells the framework this in an incremental stream. Required for incremental.
+
+        :return str: The name of the cursor field.
+        """
+        return "Date"
+
+    def get_updated_state(self, current_stream_state: MutableMapping[str, Any], latest_record: Mapping[str, Any]) -> Mapping[str, Any]:
+        """
+        Override to determine the latest state after reading the latest record. This typically compared the cursor_field from the latest record and
+        the current state and picks the 'most' recent cursor. This is how a stream's state is determined. Required for incremental.
+        """
+        state_mapping = current_stream_state.get(self.cursor_field, {})
+
+        last_record_value = latest_record.get(self.cursor_field)
+        if last_record_value:
+            state_mapping.update({self.clinic_id: last_record_value})
+
+        return {self.cursor_field: state_mapping}
+
+
+class TreatmentsReportFr(DentclinicIncrementalStream):
+    # state_checkpoint_interval = 1
+    primary_key = "Id"
+    endpoint_data_path = ['soap:Envelope', 'soap:Body',
+                          'GetTreatmentsReportResponse', 'GetTreatmentsReportResult', 'ExecutedTreatmentModel']
+    static_endpoint = "GetTreatmentsReport"
+    date_from_field = "dateFrom"
+    date_until_field = "dateUntil"
+
+    def path(
+            self, stream_state: Mapping[str, Any] = None, stream_slice: Mapping[str, Any] = None, next_page_token: Mapping[str, Any] = None
+    ) -> str:
+        """
+        should return "bookings". Required.
+        """
+        return ""
+
+    @property
+    def http_method(self) -> str:
+        return "POST"
+
+
 class Resources(DentclinicClinicIdsStream):
     primary_key = "Id"
 
@@ -232,5 +297,7 @@ class SourceDentclinic(AbstractSource):
         """
         :param config: A Mapping of the user input configuration as defined in the connector spec.
         """
-        return [BookingsFr(config=config, enable_days_back_limit=True), Bookings(config=config,enable_days_back_limit=False), Clinics(config=config),
-                Services(config=config), Resources(config=config), PatientsReport(config=config), UtilizationReport(config=config, enable_days_back_limit=False), UtilizationReportFr(config=config, enable_days_back_limit=True)] 
+        return [BookingsFr(config=config, enable_days_back_limit=True), Bookings(config=config, enable_days_back_limit=False), Clinics(config=config),
+                Services(config=config), Resources(config=config), PatientsReport(config=config), UtilizationReport(
+                    config=config, enable_days_back_limit=False), UtilizationReportFr(config=config, enable_days_back_limit=True),
+                TreatmentsReportFr(config=config, enable_days_back_limit=True), TreatmentsReport(config=config, enable_days_back_limit=False)]
