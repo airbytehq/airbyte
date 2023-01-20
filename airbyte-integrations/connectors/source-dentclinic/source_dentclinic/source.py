51f18af5830ecf6fe81402d4ec044a1733fdab15
Sebastian Sundet Flaen <sebastianflaen@gmail.com>
2023-01-20 11:42:10 +0100

    Removed old booking streams logic

--- airbyte-integrations/connectors/source-dentclinic/source_dentclinic/source.py
+++ airbyte-integrations/connectors/source-dentclinic/source_dentclinic/source.py
@@ -13,63 +13,10 @@ from airbyte_cdk.sources import AbstractSource
 from airbyte_cdk.sources.streams import Stream
 from airbyte_cdk.sources.streams.http import HttpStream
 
-from .streams import DentclinicBookingStream, DentclinicClinicIdsStream, DentclinicStaticStream, DentclinicIncrementalStream
+from .streams import DentclinicClinicIdsStream, DentclinicStaticStream, DentclinicIncrementalStream
 
 
-class BookingsFr(DentclinicBookingStream):
-    primary_key = "Id"
-
-    def path(
-            self, stream_state: Mapping[str, Any] = None, stream_slice: Mapping[str, Any] = None, next_page_token: Mapping[str, Any] = None
-    ) -> str:
-        """
-        should return "bookings". Required.
-        """
-        return ""
-
-    @property
-    def http_method(self) -> str:
-        return "POST"
-
-
-class Bookings(DentclinicBookingStream):
-    state_checkpoint_interval = 1
-    primary_key = "Id"
-
-    def path(
-            self, stream_state: Mapping[str, Any] = None, stream_slice: Mapping[str, Any] = None, next_page_token: Mapping[str, Any] = None
-    ) -> str:
-        """should return "bookings". Required."""
-        return ""
-
-    @property
-    def http_method(self) -> str:
-        return "POST"
-
-    @property
-    def cursor_field(self) -> str:
-        """
-        This field's presence tells the framework this in an incremental stream. Required for incremental.
-
-        :return str: The name of the cursor field.
-        """
-        return "Time"
-
-    def get_updated_state(self, current_stream_state: MutableMapping[str, Any], latest_record: Mapping[str, Any]) -> Mapping[str, Any]:
-        """
-        Override to determine the latest state after reading the latest record. This typically compared the cursor_field from the latest record and
-        the current state and picks the 'most' recent cursor. This is how a stream's state is determined. Required for incremental.
-        """
-        state_mapping = current_stream_state.get(self.cursor_field, {})
-
-        last_record_value = latest_record.get(self.cursor_field)
-        if last_record_value:
-            state_mapping.update({self.clinic_id: last_record_value})
-
-        return {self.cursor_field: state_mapping}
-
-
-class BookingsTestFr(DentclinicIncrementalStream):
+class BookingsFr(DentclinicIncrementalStream):
     primary_key = "Id"
     endpoint_data_path = ['soap:Envelope', 'soap:Body', 'GetBookingsResponse',
                           'GetBookingsResult', 'BookingModel']
@@ -89,7 +36,7 @@ class BookingsTestFr(DentclinicIncrementalStream):
         return "POST"
 
 
-class BookingsTest(DentclinicIncrementalStream):
+class Bookings(DentclinicIncrementalStream):
     state_checkpoint_interval = 1
     primary_key = "Id"
     endpoint_data_path = ['soap:Envelope', 'soap:Body', 'GetBookingsResponse',
@@ -131,7 +78,7 @@ class BookingsTest(DentclinicIncrementalStream):
         return {self.cursor_field: state_mapping}
 
 
-class UtilizationReportTest(DentclinicIncrementalStream):
+class UtilizationReport(DentclinicIncrementalStream):
     state_checkpoint_interval = 1
     primary_key = "Id"
     endpoint_data_path = ['soap:Envelope', 'soap:Body',
@@ -173,7 +120,7 @@ class UtilizationReportTest(DentclinicIncrementalStream):
         return {self.cursor_field: state_mapping}
 
 
-class UtilizationReportTestFr(DentclinicIncrementalStream):
+class UtilizationReportFr(DentclinicIncrementalStream):
     # state_checkpoint_interval = 1
     primary_key = "Id"
     endpoint_data_path = ['soap:Envelope', 'soap:Body',
@@ -285,5 +232,5 @@ class SourceDentclinic(AbstractSource):
         """
         :param config: A Mapping of the user input configuration as defined in the connector spec.
         """
-        return [BookingsFr(config=config, enable_days_back_limit=True), Bookings(config=config), Clinics(config=config),
-                Services(config=config), Resources(config=config), PatientsReport(config=config), BookingsTest(config=config, enable_days_back_limit=False), BookingsTestFr(config=config, enable_days_back_limit=True), UtilizationReportTest(config=config, enable_days_back_limit=False), UtilizationReportTestFr(config=config, enable_days_back_limit=True)] 
+        return [BookingsFr(config=config, enable_days_back_limit=True), Bookings(config=config,enable_days_back_limit=False), Clinics(config=config),
+                Services(config=config), Resources(config=config), PatientsReport(config=config), UtilizationReport(config=config, enable_days_back_limit=False), UtilizationReportFr(config=config, enable_days_back_limit=True)] 
