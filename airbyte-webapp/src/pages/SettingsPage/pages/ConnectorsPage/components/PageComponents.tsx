84552675ac90c7caf9cdec8eb04d03f1cc450ff9
Edmundo Ruiz Ghanem <edmundo@airbyte.io>
2023-01-20 11:20:50 -0500

    Remove unused components

--- airbyte-webapp/src/pages/SettingsPage/pages/ConnectorsPage/components/PageComponents.tsx
+++ airbyte-webapp/src/pages/SettingsPage/pages/ConnectorsPage/components/PageComponents.tsx
@@ -1,19 +1,5 @@
 import styled from "styled-components";
 
-import { H5 } from "components/base/Titles";
-
-export const Title = styled(H5)`
-  color: ${({ theme }) => theme.darkPrimaryColor};
-  margin-bottom: 19px;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-`;
-
-export const Block = styled.div`
-  margin-bottom: 56px;
-`;
-
 export const FormContent = styled.div`
   width: 253px;
   margin: -10px 0 -10px 200px;
