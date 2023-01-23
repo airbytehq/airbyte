70b786959c25b0245c9100de2d1d78a45f2657da
Sergio Ropero <sergio@airbyte.io>
2023-01-23 19:35:12 +0100

    Reformat code

--- airbyte-integrations/bases/debezium-v1-9-6/src/main/java/io/airbyte/integrations/debezium/internals/DebeziumStateDecoratingIterator.java
+++ airbyte-integrations/bases/debezium-v1-9-6/src/main/java/io/airbyte/integrations/debezium/internals/DebeziumStateDecoratingIterator.java
@@ -24,117 +24,117 @@ import java.util.Optional;
  */
 public class DebeziumStateDecoratingIterator extends AbstractIterator<AirbyteMessage> implements Iterator<AirbyteMessage> {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(DebeziumStateDecoratingIterator.class);
+  private static final Logger LOGGER = LoggerFactory.getLogger(DebeziumStateDecoratingIterator.class);
 
-    private final Iterator<AirbyteMessage> messageIterator;
-    private final CdcStateHandler cdcStateHandler;
-    private final AirbyteFileOffsetBackingStore offsetManager;
-    private final boolean trackSchemaHistory;
-    private final Optional<AirbyteSchemaHistoryStorage> schemaHistoryManager;
+  private final Iterator<AirbyteMessage> messageIterator;
+  private final CdcStateHandler cdcStateHandler;
+  private final AirbyteFileOffsetBackingStore offsetManager;
+  private final boolean trackSchemaHistory;
+  private final Optional<AirbyteSchemaHistoryStorage> schemaHistoryManager;
 
-    private boolean isSyncFinished;
+  private boolean isSyncFinished;
 
-    /**
-     * These parameters control when a checkpoint message has to be sent in a CDC integration. We can emit a checkpoint
-     * when any of the following two conditions are met.
-     * <p/>
-     * 1. The amount of records in the current loop ({@code SYNC_CHECKPOINT_RECORDS}) is higher than a threshold defined
-     * by {@code SYNC_CHECKPOINT_RECORDS}.
-     * <p/>
-     * 2. Time between checkpoints ({@code dateTimeLastSync}) is higher than a {@code Duration} defined at
-     * {@code SYNC_CHECKPOINT_SECONDS}.
-     * <p/>
-     */
-    private final static Duration SYNC_CHECKPOINT_SECONDS = Duration.ofMinutes(10);
-    private final static int SYNC_CHECKPOINT_RECORDS = 1000;
+  /**
+   * These parameters control when a checkpoint message has to be sent in a CDC integration. We can emit a checkpoint
+   * when any of the following two conditions are met.
+   * <p/>
+   * 1. The amount of records in the current loop ({@code SYNC_CHECKPOINT_RECORDS}) is higher than a threshold defined
+   * by {@code SYNC_CHECKPOINT_RECORDS}.
+   * <p/>
+   * 2. Time between checkpoints ({@code dateTimeLastSync}) is higher than a {@code Duration} defined at
+   * {@code SYNC_CHECKPOINT_SECONDS}.
+   * <p/>
+   */
+  private final static Duration SYNC_CHECKPOINT_SECONDS = Duration.ofMinutes(10);
+  private final static int SYNC_CHECKPOINT_RECORDS = 1000;
 
-    //Properties used as checkpoint
-    private OffsetDateTime dateTimeLastSync;
-    private Integer recordsLastSync;
+  //Properties used as checkpoint
+  private OffsetDateTime dateTimeLastSync;
+  private Integer recordsLastSync;
 
-    /**
-     * @param messageIterator      Base iterator that we want to enrich with checkpoint messages
-     * @param cdcStateHandler      Handler to save the offset and schema history
-     * @param offsetManager        Handler to read and write debezium offset file
-     * @param trackSchemaHistory   Set true if the schema needs to be tracked
-     * @param schemaHistoryManager Handler to write schema. Needs to be initialized if trackSchemaHistory is set to true
-     */
-    public DebeziumStateDecoratingIterator(
-            final Iterator<AirbyteMessage> messageIterator,
-            final CdcStateHandler cdcStateHandler,
-            final AirbyteFileOffsetBackingStore offsetManager,
-            final boolean trackSchemaHistory,
-            final Optional<AirbyteSchemaHistoryStorage> schemaHistoryManager) {
-        this.messageIterator = messageIterator;
-        this.cdcStateHandler = cdcStateHandler;
-        this.offsetManager = offsetManager;
-        this.trackSchemaHistory = trackSchemaHistory;
-        this.schemaHistoryManager = schemaHistoryManager;
+  /**
+   * @param messageIterator      Base iterator that we want to enrich with checkpoint messages
+   * @param cdcStateHandler      Handler to save the offset and schema history
+   * @param offsetManager        Handler to read and write debezium offset file
+   * @param trackSchemaHistory   Set true if the schema needs to be tracked
+   * @param schemaHistoryManager Handler to write schema. Needs to be initialized if trackSchemaHistory is set to true
+   */
+  public DebeziumStateDecoratingIterator(
+    final Iterator<AirbyteMessage> messageIterator,
+    final CdcStateHandler cdcStateHandler,
+    final AirbyteFileOffsetBackingStore offsetManager,
+    final boolean trackSchemaHistory,
+    final Optional<AirbyteSchemaHistoryStorage> schemaHistoryManager) {
+    this.messageIterator = messageIterator;
+    this.cdcStateHandler = cdcStateHandler;
+    this.offsetManager = offsetManager;
+    this.trackSchemaHistory = trackSchemaHistory;
+    this.schemaHistoryManager = schemaHistoryManager;
 
-        this.isSyncFinished = false;
+    this.isSyncFinished = false;
+    initializeCheckpointValues();
+  }
+
+  /**
+   * Computes the next record retrieved from Source stream. Emits state messages as checkpoints based on number of
+   * records or time lapsed.
+   *
+   * <p>
+   * If this method throws an exception, it will propagate outward to the {@code hasNext} or
+   * {@code next} invocation that invoked this method. Any further attempts to use the iterator will
+   * result in an {@link IllegalStateException}.
+   * </p>
+   *
+   * @return {@link AirbyteStateMessage} containing CDC data or state checkpoint message.
+   */
+  @Override
+  protected AirbyteMessage computeNext() {
+    if (isSyncFinished) {
+      return endOfData();
+    }
+
+    if (messageIterator.hasNext()) {
+      if (recordsLastSync >= SYNC_CHECKPOINT_RECORDS ||
+        Duration.between(dateTimeLastSync, OffsetDateTime.now()).compareTo(SYNC_CHECKPOINT_SECONDS) > 0) {
+        AirbyteMessage stateMessage = createStateMessage();
         initializeCheckpointValues();
+        return stateMessage;
+      }
+
+      recordsLastSync++;
+      // Use try-catch to catch Exception that could occur when connection to the database fails
+      try {
+        return messageIterator.next();
+      } catch (final Exception e) {
+        LOGGER.error("Message iterator failed to read next record. {}", e.getMessage());
+        //TODO: Check that the record that fails is not missed on next execution!
+        isSyncFinished = true;
+        return createStateMessage();
+      }
+    } else {
+      isSyncFinished = true;
+      return createStateMessage();
     }
+  }
 
-    /**
-     * Computes the next record retrieved from Source stream. Emits state messages as checkpoints based on number of
-     * records or time lapsed.
-     *
-     * <p>
-     * If this method throws an exception, it will propagate outward to the {@code hasNext} or
-     * {@code next} invocation that invoked this method. Any further attempts to use the iterator will
-     * result in an {@link IllegalStateException}.
-     * </p>
-     *
-     * @return {@link AirbyteStateMessage} containing CDC data or state checkpoint message.
-     */
-    @Override
-    protected AirbyteMessage computeNext() {
-        if (isSyncFinished) {
-            return endOfData();
-        }
+  /**
+   * Initialize or reset the checkpoint variables.
+   */
+  private void initializeCheckpointValues() {
+    this.recordsLastSync = 0;
+    this.dateTimeLastSync = OffsetDateTime.now();
+  }
 
-        if (messageIterator.hasNext()) {
-            if (recordsLastSync >= SYNC_CHECKPOINT_RECORDS ||
-                    Duration.between(dateTimeLastSync, OffsetDateTime.now()).compareTo(SYNC_CHECKPOINT_SECONDS) > 0) {
-                AirbyteMessage stateMessage = createStateMessage();
-                initializeCheckpointValues();
-                return stateMessage;
-            }
+  /**
+   * Creates {@link AirbyteStateMessage} while updating CDC data, used to checkpoint the state of the process.
+   *
+   * @return {@link AirbyteStateMessage} which includes offset and schema history if used.
+   */
+  private AirbyteMessage createStateMessage() {
+    final Map<String, String> offset = offsetManager.read();
+    final String dbHistory = trackSchemaHistory ? schemaHistoryManager
+      .orElseThrow(() -> new RuntimeException("Schema History Tracking is true but manager is not initialised")).read() : null;
 
-            recordsLastSync++;
-            // Use try-catch to catch Exception that could occur when connection to the database fails
-            try {
-                return messageIterator.next();
-            } catch (final Exception e) {
-                LOGGER.error("Message iterator failed to read next record. {}", e.getMessage());
-                //TODO: Check that the record that fails is not missed on next execution!
-                isSyncFinished = true;
-                return createStateMessage();
-            }
-        } else {
-            isSyncFinished = true;
-            return createStateMessage();
-        }
-    }
-
-    /**
-     * Initialize or reset the checkpoint variables.
-     */
-    private void initializeCheckpointValues() {
-        this.recordsLastSync = 0;
-        this.dateTimeLastSync = OffsetDateTime.now();
-    }
-
-    /**
-     * Creates {@link AirbyteStateMessage} while updating CDC data, used to checkpoint the state of the process.
-     *
-     * @return {@link AirbyteStateMessage} which includes offset and schema history if used.
-     */
-    private AirbyteMessage createStateMessage() {
-        final Map<String, String> offset = offsetManager.read();
-        final String dbHistory = trackSchemaHistory ? schemaHistoryManager
-                .orElseThrow(() -> new RuntimeException("Schema History Tracking is true but manager is not initialised")).read() : null;
-
-        return cdcStateHandler.saveState(offset, dbHistory);
-    }
+    return cdcStateHandler.saveState(offset, dbHistory);
+  }
 }
