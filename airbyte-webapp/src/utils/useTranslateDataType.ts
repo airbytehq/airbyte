fe45017713319f0cc2180e67d2c813ea9d7a36af
Jimmy Ma <jimmy@airbyte.io>
2023-01-25 10:26:50 -0800

    Move wellknown types mapping to the utility function

--- airbyte-webapp/src/utils/useTranslateDataType.ts
+++ airbyte-webapp/src/utils/useTranslateDataType.ts
@@ -10,6 +10,20 @@ export interface AirbyteConnectorData {
   oneOf?: unknown[];
 }
 
+const wellKnownTypesPrefix = "WellKnownTypes.json#/definitions/";
+const wellKnownTypeToDataTypeKey = {
+  [`${wellKnownTypesPrefix}String`]: "string",
+  [`${wellKnownTypesPrefix}BinaryData`]: "binary_data",
+  [`${wellKnownTypesPrefix}Number`]: "number",
+  [`${wellKnownTypesPrefix}Integer`]: "integer",
+  [`${wellKnownTypesPrefix}Boolean`]: "boolean",
+  [`${wellKnownTypesPrefix}Date`]: "date",
+  [`${wellKnownTypesPrefix}TimestampWithTimezone`]: "timestamp_with_timezone",
+  [`${wellKnownTypesPrefix}TimestampWithoutTimezone`]: "timestamp_without_timezone",
+  [`${wellKnownTypesPrefix}TimeWithTimezone`]: "time_with_timezone",
+  [`${wellKnownTypesPrefix}TimeWithoutTimezone`]: "time_without_timezone",
+};
+
 const getType = (data: AirbyteConnectorData): string => {
   if (data.oneOf || data.anyOf) {
     return "union";
@@ -17,7 +31,10 @@ const getType = (data: AirbyteConnectorData): string => {
   if (!data.anyOf && !data.oneOf && !data.airbyte_type && !data.format && !data.type && !data.$ref) {
     return "unknown";
   }
-  return data.$ref ?? data.airbyte_type ?? data.format ?? data.type;
+  if (data.$ref) {
+    return wellKnownTypeToDataTypeKey[data.$ref] ?? "unknown";
+  }
+  return data.airbyte_type ?? data.format ?? data.type;
 };
 
 export const useTranslateDataType = (props: AirbyteConnectorData) => {
