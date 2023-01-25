b8195dcf1ea15d11ee6f4f9f711d0ba5fd2c8027
erohmensing <erohmensing@gmail.com>
2023-01-25 17:18:40 -0600

    Turn off default for source-zendesk-talk (GA)

--- airbyte-integrations/connectors/source-zendesk-talk/source_zendesk_talk/streams.py
+++ airbyte-integrations/connectors/source-zendesk-talk/source_zendesk_talk/streams.py
@@ -11,6 +11,8 @@ from urllib.parse import parse_qs, urlparse
 import pendulum as pendulum
 import requests
 from airbyte_cdk.sources.streams.http import HttpStream
+from airbyte_cdk.sources.streams import AvailabilityStrategy
+
 
 
 class ZendeskTalkStream(HttpStream, ABC):
@@ -32,6 +34,10 @@ class ZendeskTalkStream(HttpStream, ABC):
     def url_base(self) -> str:
         """API base url based on configured subdomain"""
         return f"https://{self._subdomain}.zendesk.com/api/v2/channels/voice/"
+
+    @property
+    def availability_strategy(self) -> Optional["AvailabilityStrategy"]:
+        return None
 
     def backoff_time(self, response: requests.Response) -> Optional[float]:
         """
