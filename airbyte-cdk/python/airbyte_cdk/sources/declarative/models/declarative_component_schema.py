856484d351f7c9c52775b2441afd9844da9f055a
Catherine Noll <noll.catherine@gmail.com>
2023-01-26 22:37:46 +0000

    Formatting updates

--- airbyte-cdk/python/airbyte_cdk/sources/declarative/models/declarative_component_schema.py
+++ airbyte-cdk/python/airbyte_cdk/sources/declarative/models/declarative_component_schema.py
@@ -398,8 +398,8 @@ class CompositeErrorHandler(BaseModel):
 
 class HttpRequester(BaseModel):
     type: Literal["HttpRequester"]
-    path: str
-    url_base: str
+    path: str = Field(..., description="The specific endpoint to fetch data from for a resource.")
+    url_base: str = Field(..., description="The root of the API source.")
     authenticator: Optional[
         Union[
             ApiKeyAuthenticator,
@@ -410,9 +410,17 @@ class HttpRequester(BaseModel):
             NoAuth,
             SessionTokenAuthenticator,
         ]
-    ] = None
-    error_handler: Optional[Union[DefaultErrorHandler, CustomErrorHandler, CompositeErrorHandler]] = None
-    http_method: Optional[Union[str, HttpMethodEnum]] = "GET"
+    ] = Field(
+        None,
+        description="Authenticator component that defines how to authenticate to the source.",
+    )
+    error_handler: Optional[Union[DefaultErrorHandler, CustomErrorHandler, CompositeErrorHandler]] = Field(
+        None, description="Error handler component that defines how to handle errors."
+    )
+    http_method: Optional[Union[str, HttpMethodEnum]] = Field(
+        "GET",
+        description="The HTTP method used to fetch data from the source (can be GET or POST).",
+    )
     request_body_data: Optional[Union[str, Dict[str, str]]] = Field(
         None,
         description="Specifies how to populate the body of the request with a non-JSON payload. If returns a ready text that it will be sent as is. If returns a dict that it will be converted to a urlencoded form.",
@@ -483,9 +491,18 @@ class ParentStreamConfig(BaseModel):
 
 class SimpleRetriever(BaseModel):
     type: Literal["SimpleRetriever"]
-    record_selector: RecordSelector
-    requester: Union[CustomRequester, HttpRequester]
-    paginator: Optional[Union[DefaultPaginator, NoPagination]] = None
+    record_selector: RecordSelector = Field(
+        ...,
+        description="Component that describes how to extract records from a HTTP response.",
+    )
+    requester: Union[CustomRequester, HttpRequester] = Field(
+        ...,
+        description="Requester component that describes how to prepare HTTP requests to send to the source API.",
+    )
+    paginator: Optional[Union[DefaultPaginator, NoPagination]] = Field(
+        None,
+        description="Paginator component that describes how to navigate through the API's pages.",
+    )
     stream_slicer: Optional[
         Union[
             CartesianProductStreamSlicer,
@@ -495,7 +512,10 @@ class SimpleRetriever(BaseModel):
             SingleSlice,
             SubstreamSlicer,
         ]
-    ] = None
+    ] = Field(
+        None,
+        description="StreamSlicer component that describes how to partition the stream, enabling incremental syncs and checkpointing.",
+    )
     parameters: Optional[Dict[str, Any]] = Field(None, alias="$parameters")
 
 
