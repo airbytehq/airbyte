ec0f25c954386fc8c13146d0bea8112cb8134b2e
maxi297 <maxime@airbyte.io>
2023-01-26 10:23:15 -0500

    Update migration scripts according to manifest file rename

--- airbyte-cdk/python/bin/migrate_options_to_parameters.sh
+++ airbyte-cdk/python/bin/migrate_options_to_parameters.sh
@@ -9,11 +9,11 @@ CDK_DIR=$ROOT_DIR/airbyte-cdk/python/
 
 for directory in $CONNECTORS_DIR/source-* ; do
   MANIFEST_DIRECTORY=$(basename $directory | tr - _)
-  SOURCE_NAME=${MANIFEST_DIRECTORY#source_}
-  FILEPATH=$directory/source_$SOURCE_NAME/$SOURCE_NAME.yaml
+  FILEPATH=$directory/$MANIFEST_DIRECTORY/manifest.yaml
 
-  echo "Migrating manifest located at $FILEPATH"
   if test -f $FILEPATH; then
+    echo "Migrating manifest located at $FILEPATH"
+
     # In place replacement of $options to $parameters
     sed -i '' -E 's/\$options/\$parameters/' $FILEPATH
 
