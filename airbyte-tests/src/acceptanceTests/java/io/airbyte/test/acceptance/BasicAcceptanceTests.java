3c16024594c8b7a652da281777f21e67a74ca661
Evan Tahler <evan@airbyte.io>
2023-01-27 10:07:01 -0800

    Suggested Streams via Actor Definition (#21577)
    * Suggested Streams in in the Actor Definition
    
    * Fix steam addition
    
    * fix tests
    
    * enable faker streams
    
    * test resilaince.
    
    * lint
    
    * fig configLookup
    
    * fixup definition load in webBackend
    
    * fix build/tests
    
    * Include 'suggested' in discover API response
    
    * fix test
    
    * Update airbyte-api/src/main/openapi/config.yaml
    
    Co-authored-by: Pedro S. Lopez <pedroslopez@me.com>
    
    * update build with typo
    
    * fix test
    
    * remove comment
    
    * add more context
    
    ---------
    
    Co-authored-by: Pedro S. Lopez <pedroslopez@me.com>

--- airbyte-tests/src/acceptanceTests/java/io/airbyte/test/acceptance/BasicAcceptanceTests.java
+++ airbyte-tests/src/acceptanceTests/java/io/airbyte/test/acceptance/BasicAcceptanceTests.java
@@ -337,7 +337,8 @@ class BasicAcceptanceTests {
         .destinationSyncMode(DestinationSyncMode.APPEND)
         .primaryKey(Collections.emptyList())
         .aliasName(STREAM_NAME.replace(".", "_"))
-        .selected(true);
+        .selected(true)
+        .suggested(true);
     final AirbyteCatalog expected = new AirbyteCatalog()
         .streams(Lists.newArrayList(new AirbyteStreamAndConfiguration()
             .stream(stream)
@@ -359,6 +360,7 @@ class BasicAcceptanceTests {
     catalog.getStreams().forEach(s -> s.getConfig().syncMode(syncMode).destinationSyncMode(destinationSyncMode).setFieldSelectionEnabled(false));
     final ConnectionRead createdConnection =
         testHarness.createConnection(name, sourceId, destinationId, List.of(operationId), catalog, ConnectionScheduleType.BASIC, BASIC_SCHEDULE_DATA);
+    createdConnection.getSyncCatalog().getStreams().forEach(s -> s.getConfig().setSuggested(true));
 
     assertEquals(sourceId, createdConnection.getSourceId());
     assertEquals(destinationId, createdConnection.getDestinationId());
