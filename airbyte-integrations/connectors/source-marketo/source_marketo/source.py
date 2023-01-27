56944a3c6a4e80113f2f74a52dc55bd30daad2b4
Ella Rohm-Ensing <erohmensing@gmail.com>
2023-01-27 17:24:02 -0500

    Update source.py

--- airbyte-integrations/connectors/source-marketo/source_marketo/source.py
+++ airbyte-integrations/connectors/source-marketo/source_marketo/source.py
@@ -14,9 +14,9 @@ import requests
 from airbyte_cdk.models import SyncMode
 from airbyte_cdk.sources import AbstractSource
 from airbyte_cdk.sources.streams import Stream
+from airbyte_cdk.sources.streams.availability_strategy import AvailabilityStrategy
 from airbyte_cdk.sources.streams.http import HttpStream
 from airbyte_cdk.sources.streams.http.auth import Oauth2Authenticator
-from airbyte_cdk.sources.streams import AvailabilityStrategy
 
 from .utils import STRING_TYPES, clean_string, format_value, to_datetime_str
 
