58809bd9d637b3d63998bbdd834ff72f6f95e3c0
Akash Kulkarni <akash@airbyte.io>
2023-01-26 19:07:20 -0800

    Update JdbcStreamingQueryConfig.java

--- airbyte-db/db-lib/src/main/java/io/airbyte/db/jdbc/streaming/JdbcStreamingQueryConfig.java
+++ airbyte-db/db-lib/src/main/java/io/airbyte/db/jdbc/streaming/JdbcStreamingQueryConfig.java
@@ -11,11 +11,11 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 /*
-Interface that defines how to stream results from a Jdbc database. This involves determining updating what the fetch size should be based on
-the size of the existing rows.
-  1. The config initializes the fetch size and sets up the estimator.
-  2. The config then accepts each row and feeds it to the estimator. If the estimator has a new estimate, it updates the fetch size.
-*/
+ * Interface that defines how to stream results from a Jdbc database. This involves determining
+ * updating what the fetch size should be based on the size of the existing rows. 1. The config
+ * initializes the fetch size and sets up the estimator. 2. The config then accepts each row and
+ * feeds it to the estimator. If the estimator has a new estimate, it updates the fetch size.
+ */
 
 public interface JdbcStreamingQueryConfig extends CheckedBiConsumer<ResultSet, Object, SQLException> {
 
