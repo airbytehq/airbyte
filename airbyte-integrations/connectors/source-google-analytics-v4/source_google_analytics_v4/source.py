b4f14842720bce662f9e4dc58a91e90e2d42e5dc
Ella Rohm-Ensing <erohmensing@gmail.com>
2023-01-27 16:38:05 -0500

    Update source.py

--- airbyte-integrations/connectors/source-google-analytics-v4/source_google_analytics_v4/source.py
+++ airbyte-integrations/connectors/source-google-analytics-v4/source_google_analytics_v4/source.py
@@ -17,9 +17,9 @@ import requests
 from airbyte_cdk.models import SyncMode
 from airbyte_cdk.sources import AbstractSource
 from airbyte_cdk.sources.streams import Stream
+from airbyte_cdk.sources.streams.availability_strategy import AvailabilityStrategy
 from airbyte_cdk.sources.streams.http import HttpStream
 from airbyte_cdk.sources.streams.http.auth import Oauth2Authenticator
-from airbyte_cdk.sources.streams import AvailabilityStrategy
 
 from .custom_reports_validator import CustomReportsValidator
 
