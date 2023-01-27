18daf25c6b6b93fbbed499a8debb4a95882bfb53
Ella Rohm-Ensing <erohmensing@gmail.com>
2023-01-27 17:46:50 -0500

    Update streams.py

--- airbyte-integrations/connectors/source-twilio/source_twilio/streams.py
+++ airbyte-integrations/connectors/source-twilio/source_twilio/streams.py
@@ -15,7 +15,6 @@ from airbyte_cdk.sources.streams import IncrementalMixin
 from airbyte_cdk.sources.streams.availability_strategy import AvailabilityStrategy
 from airbyte_cdk.sources.streams.http import HttpStream
 from airbyte_cdk.sources.streams.http.auth.core import HttpAuthenticator
-
 from airbyte_cdk.sources.utils.transform import TransformConfig, TypeTransformer
 from pendulum.datetime import DateTime
 from requests.auth import AuthBase
