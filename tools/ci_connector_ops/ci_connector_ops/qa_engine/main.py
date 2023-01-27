80d5f691090f5002ea90b5b0dd937e4761d21dbd
alafanechere <augustin.lafanechere@gmail.com>
2023-01-27 11:55:05 +0100

    revert

--- tools/ci_connector_ops/ci_connector_ops/qa_engine/main.py
+++ tools/ci_connector_ops/ci_connector_ops/qa_engine/main.py
@@ -3,14 +3,13 @@
 #
 
 
-from .cloud_availability_updater import deploy_eligible_connectors
+from .constants import GCS_QA_REPORT_PATH
 from .enrichments import get_enriched_catalog
 from .inputs import CLOUD_CATALOG, OSS_CATALOG
-from .validations import get_qa_report, get_connectors_eligible_for_cloud
+from .validations import get_qa_report
+
 
 def main():
     enriched_catalog = get_enriched_catalog(OSS_CATALOG, CLOUD_CATALOG)
     qa_report = get_qa_report(enriched_catalog)
-    connectors_eligible_for_cloud = get_connectors_eligible_for_cloud(qa_report)
-    deploy_eligible_connectors(connectors_eligible_for_cloud)
-    #qa_report.to_json(GCS_QA_REPORT_PATH, orient="records")
+    qa_report.to_json(GCS_QA_REPORT_PATH, orient="records")
