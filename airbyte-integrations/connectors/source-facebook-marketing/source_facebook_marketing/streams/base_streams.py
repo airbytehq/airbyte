6baf1f57c24e79dabf360e20008ca231200ae980
Ella Rohm-Ensing <erohmensing@gmail.com>
2023-01-27 16:26:27 -0500

    remove import

--- airbyte-integrations/connectors/source-facebook-marketing/source_facebook_marketing/streams/base_streams.py
+++ airbyte-integrations/connectors/source-facebook-marketing/source_facebook_marketing/streams/base_streams.py
@@ -16,7 +16,6 @@ from airbyte_cdk.sources.utils.transform import TransformConfig, TypeTransformer
 from cached_property import cached_property
 from facebook_business.adobjects.abstractobject import AbstractObject
 from facebook_business.api import FacebookAdsApiBatch, FacebookRequest, FacebookResponse
-from airbyte_cdk.sources.streams import AvailabilityStrategy
 
 from .common import deep_merge
 
