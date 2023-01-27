047cee0447b8ff404ca330a7e4834eeca6b9b23b
Sergey Chvalyuk <grubberr@gmail.com>
2023-01-27 09:58:24 +0000

    calls_stream added
    Signed-off-by: Sergey Chvalyuk <grubberr@gmail.com>

---
+++ airbyte-integrations/connectors/source-twilio/source_twilio/components.py
@@ -0,0 +1,38 @@
+#
+# Copyright (c) 2022 Airbyte, Inc., all rights reserved.
+#
+
+from dataclasses import dataclass
+from typing import Any, Iterable, Mapping, Optional
+
+from airbyte_cdk.models import SyncMode
+from airbyte_cdk.sources.declarative.stream_slicers import CartesianProductStreamSlicer
+from airbyte_cdk.sources.declarative.types import Record, StreamSlice
+
+
+@dataclass
+class TwilioCartesianProductStreamSlicer(CartesianProductStreamSlicer):
+    def __post_init__(self, options: Mapping[str, Any]):
+        self._cursor = {}
+        self._options = options
+
+    def get_stream_state(self) -> Mapping[str, Any]:
+        return self._cursor
+
+    def stream_slices(self, sync_mode: SyncMode, stream_state: Mapping[str, Any]) -> Iterable[Mapping[str, Any]]:
+        accounts_slicer, datetime_slicer = self.stream_slicers
+        for account_slice in accounts_slicer.stream_slices(sync_mode, stream_state):
+            account_id = account_slice["record"]["sid"]
+            path = account_slice["record"]["subresource_uris"][self._options["name"]]
+            datetime_slicer._cursor = None
+            for datetime_slice in datetime_slicer.stream_slices(sync_mode, stream_state.get(account_id)):
+                yield {"account_id": account_id, "path": path} | datetime_slice
+
+    def update_cursor(self, stream_slice: StreamSlice, last_record: Optional[Record] = None):
+        datetime_slicer = self.stream_slicers[1]
+        datetime_slicer.update_cursor(stream_slice, last_record)
+        if last_record:
+            account_id = stream_slice["account_id"]
+            self._cursor.setdefault(account_id, {}).update(datetime_slicer.get_stream_state())
+        else:
+            self._cursor = stream_slice
