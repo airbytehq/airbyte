5f90275866eb58e4ebf956d549f5a4c9374952aa
lmossman <lake@airbyte.io>
2023-01-27 14:54:22 -0800

    save working version of minimal landing page

--- airbyte-webapp/src/pages/ConnectorBuilderPage/ConnectorBuilderPage.tsx
+++ airbyte-webapp/src/pages/ConnectorBuilderPage/ConnectorBuilderPage.tsx
@@ -1,15 +1,25 @@
 import classnames from "classnames";
-import { Formik } from "formik";
-import React, { useCallback, useEffect, useMemo, useRef } from "react";
+import { Formik, useFormikContext } from "formik";
+import { load, YAMLException } from "js-yaml";
+import isEqual from "lodash/isEqual";
+import React, { useCallback, useEffect, useMemo, useRef, useState } from "react";
 import { useIntl } from "react-intl";
 
 import { Builder } from "components/connectorBuilder/Builder/Builder";
 import { StreamTestingPanel } from "components/connectorBuilder/StreamTestingPanel";
-import { builderFormValidationSchema, BuilderFormValues } from "components/connectorBuilder/types";
+import {
+  builderFormValidationSchema,
+  BuilderFormValues,
+  DEFAULT_BUILDER_FORM_VALUES,
+} from "components/connectorBuilder/types";
+import { useManifestToBuilderForm } from "components/connectorBuilder/useManifestToBuilderForm";
 import { YamlEditor } from "components/connectorBuilder/YamlEditor";
+import { Button } from "components/ui/Button";
 import { ResizablePanels } from "components/ui/ResizablePanels";
+import { Text } from "components/ui/Text";
 
 import { Action, Namespace } from "core/analytics";
+import { ConnectorManifest } from "core/request/ConnectorManifest";
 import { useAnalyticsService } from "hooks/services/Analytics";
 import {
   ConnectorBuilderTestStateProvider,
@@ -35,9 +45,11 @@ const ConnectorBuilderPageInner: React.FC = React.memo(() => {
   const switchToUI = useCallback(() => setEditorView("ui"), [setEditorView]);
   const switchToYaml = useCallback(() => setEditorView("yaml"), [setEditorView]);
 
+  const [showLandingPage, setShowLandingPage] = useState(isEqual(builderFormValues, DEFAULT_BUILDER_FORM_VALUES));
+
   const initialFormValues = useRef(builderFormValues);
-  return useMemo(
-    () => (
+  return useMemo(() => {
+    return (
       <Formik
         initialValues={initialFormValues.current}
         validateOnBlur
@@ -47,6 +59,15 @@ const ConnectorBuilderPageInner: React.FC = React.memo(() => {
         validationSchema={builderFormValidationSchema}
       >
         {(props) => {
+          if (showLandingPage) {
+            return (
+              <LandingPage
+                setShowLandingPage={setShowLandingPage}
+                switchToUI={switchToUI}
+                switchToYaml={switchToYaml}
+              />
+            );
+          }
           return (
             <Panels
               editorView={editorView}
@@ -58,9 +79,8 @@ const ConnectorBuilderPageInner: React.FC = React.memo(() => {
           );
         }}
       </Formik>
-    ),
-    [editorView, switchToUI, switchToYaml]
-  );
+    );
+  }, [editorView, showLandingPage, switchToUI, switchToYaml]);
 });
 
 export const ConnectorBuilderPage: React.FC = () => (
@@ -114,6 +134,100 @@ const Panels = React.memo(
           },
         }}
       />
+    );
+  }
+);
+
+const LandingPage = React.memo(
+  ({
+    setShowLandingPage,
+    switchToUI,
+    switchToYaml,
+  }: {
+    setShowLandingPage: (value: boolean) => void;
+    switchToUI: () => void;
+    switchToYaml: () => void;
+  }) => {
+    const fileInputRef = useRef<HTMLInputElement>(null);
+    const { setJsonManifest, setYamlIsValid } = useConnectorBuilderFormState();
+    const { convertToBuilderFormValues } = useManifestToBuilderForm();
+    const { setValues } = useFormikContext<BuilderFormValues>();
+    const [importLoading, setImportLoading] = useState(false);
+
+    const handleYamlUpload = useCallback(
+      async (yaml: string) => {
+        try {
+          let json;
+          try {
+            json = load(yaml) as ConnectorManifest;
+          } catch (e) {
+            if (e instanceof YAMLException) {
+              console.log(`The YAML you provided is invalid! \nError: ${e.reason}\nLine number: ${e.mark.line}`);
+            }
+            return;
+          }
+          setYamlIsValid(true);
+
+          let convertedFormValues;
+          try {
+            convertedFormValues = await convertToBuilderFormValues(json, DEFAULT_BUILDER_FORM_VALUES);
+          } catch (e) {
+            switchToYaml();
+            setJsonManifest(json);
+            setShowLandingPage(false);
+            return;
+          }
+
+          switchToUI();
+          setValues(convertedFormValues);
+          setShowLandingPage(false);
+        } finally {
+          setImportLoading(false);
+        }
+      },
+      [
+        convertToBuilderFormValues,
+        setJsonManifest,
+        setShowLandingPage,
+        setValues,
+        setYamlIsValid,
+        switchToUI,
+        switchToYaml,
+      ]
+    );
+
+    return (
+      <div>
+        <Text>Landing Page</Text>
+        <input
+          type="file"
+          accept=".yml,.yaml"
+          ref={fileInputRef}
+          style={{ display: "none" }}
+          onChange={(uploadEvent) => {
+            setImportLoading(true);
+            const file = uploadEvent.target.files?.[0];
+            const reader = new FileReader();
+            reader.onload = (readerEvent) => {
+              handleYamlUpload(readerEvent.target?.result as string);
+            };
+            if (file) {
+              reader.readAsText(file);
+            }
+          }}
+        />
+        <Button onClick={() => fileInputRef.current?.click()} isLoading={importLoading}>
+          Import YAML
+        </Button>
+        <Button
+          onClick={() => {
+            switchToUI();
+            setShowLandingPage(false);
+          }}
+        >
+          Start from scratch
+        </Button>
+      </div>
     );
   }
 );
