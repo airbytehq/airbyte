fefa7a6b21497e1df20ced6f2c1dc24eee45ce46
Hervé Commowick <herve.commowick@sundayapp.com>
2023-01-27 15:31:17 +0100

    Upgrade temporal to latest v1.13.x

--- airbyte-temporal/scripts/build-temporal.sh
+++ airbyte-temporal/scripts/build-temporal.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-TEMPORAL_VERSION=1.13.0
+TEMPORAL_VERSION=1.13.4
 
 curl -OL https://github.com/temporalio/temporal/archive/refs/tags/v"$TEMPORAL_VERSION".tar.gz
 tar -xvf v"$TEMPORAL_VERSION".tar.gz
