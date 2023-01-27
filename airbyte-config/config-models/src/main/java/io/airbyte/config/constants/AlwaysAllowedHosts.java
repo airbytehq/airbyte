c3b0212961cc16d1a50c1c0a37f41e3dc8a8f53b
evantahler <evan@airbyte.io>
2023-01-27 10:11:53 -0800

    `AlwaysAllowedHosts`

---
+++ airbyte-config/config-models/src/main/java/io/airbyte/config/constants/AlwaysAllowedHosts.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright (c) 2022 Airbyte, Inc., all rights reserved.
+ */
+
+package io.airbyte.config.constants;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AlwaysAllowedHosts {
+
+  private final List<String> hosts = new ArrayList<>();
+
+  public AlwaysAllowedHosts() {
+    // DataDog. See https://docs.datadoghq.com/agent/proxy/?tab=linux and change the location tabs
+    hosts.add("*.datadoghq.com");
+    hosts.add("*.datadoghq.eu");
+
+    // Sentry.  See https://docs.sentry.io/api/ for more information
+    hosts.add("*.sentry.io");
+  }
+
+  public List<String> getHosts() {
+    return hosts;
+  }
+
+}
