90c0a8cd0b41595382b4fc0bd4e22105fc259d9b
lmossman <lake@airbyte.io>
2023-01-27 17:48:36 -0800

    save styling progress

--- airbyte-webapp/src/pages/ConnectorBuilderPage/LandingPage.tsx
+++ airbyte-webapp/src/pages/ConnectorBuilderPage/LandingPage.tsx
@@ -5,13 +5,18 @@ import { FormattedMessage } from "react-intl";
 
 import { BuilderFormValues, DEFAULT_BUILDER_FORM_VALUES } from "components/connectorBuilder/types";
 import { useManifestToBuilderForm } from "components/connectorBuilder/useManifestToBuilderForm";
-import { Button } from "components/ui/Button";
+import { Button, ButtonProps } from "components/ui/Button";
+import { FlexContainer } from "components/ui/Flex";
+import { Heading } from "components/ui/Heading";
 import { Text } from "components/ui/Text";
 import { ToastType } from "components/ui/Toast";
 
 import { ConnectorManifest } from "core/request/ConnectorManifest";
 import { useNotificationService } from "hooks/services/Notification";
 import { useConnectorBuilderFormState } from "services/connectorBuilder/ConnectorBuilderStateService";
+
+import styles from "./LandingPage.module.scss";
+import { ReactComponent as AirbyteLogo } from "../../../public/images/airbyte/logo.svg";
 
 const YAML_UPLOAD_ERROR_ID = "connectorBuilder.yamlUpload.error";
 
@@ -30,7 +35,7 @@ export const LandingPage = React.memo(
     const { setJsonManifest, setYamlIsValid } = useConnectorBuilderFormState();
     const { convertToBuilderFormValues } = useManifestToBuilderForm();
     const { setValues } = useFormikContext<BuilderFormValues>();
-    const [importLoading, setImportLoading] = useState(false);
+    const [importYamlLoading, setImportYamlLoading] = useState(false);
 
     const handleYamlUpload = useCallback(
       async (yaml: string) => {
@@ -75,7 +80,7 @@ export const LandingPage = React.memo(
           if (fileInputRef.current) {
             fileInputRef.current.value = "";
           }
-          setImportLoading(false);
+          setImportYamlLoading(false);
         }
       },
       [
@@ -91,43 +96,82 @@ export const LandingPage = React.memo(
     );
 
     return (
-      <div>
-        <Text>Landing Page</Text>
-        <input
-          type="file"
-          accept=".yml,.yaml"
-          ref={fileInputRef}
-          style={{ display: "none" }}
-          onChange={(uploadEvent) => {
-            setImportLoading(true);
-            const file = uploadEvent.target.files?.[0];
-            const reader = new FileReader();
-            reader.onload = (readerEvent) => {
-              handleYamlUpload(readerEvent.target?.result as string);
-            };
-            if (file) {
-              reader.readAsText(file);
-            }
-          }}
-        />
-        <Button
-          onClick={() => {
-            unregisterNotificationById(YAML_UPLOAD_ERROR_ID);
-            fileInputRef.current?.click();
-          }}
-          isLoading={importLoading}
-        >
-          Import YAML
-        </Button>
-        <Button
-          onClick={() => {
-            switchToUI();
-            setShowLandingPage(false);
-          }}
-        >
-          Start from scratch
-        </Button>
-      </div>
+      <FlexContainer direction="column" alignItems="center" gap="2xl">
+        <FlexContainer direction="column" gap="md" alignItems="center" className={styles.titleContainer}>
+          <AirbyteLogo />
+          <Heading as="h1" size="lg" className={styles.title}>
+            <FormattedMessage id="connectorBuilder.landingPage.title" />
+          </Heading>
+        </FlexContainer>
+        <Heading as="h1" size="lg">
+          <FormattedMessage id="connectorBuilder.landingPage.prompt" />
+        </Heading>
+        <FlexContainer direction="row">
+          <input
+            type="file"
+            accept=".yml,.yaml"
+            ref={fileInputRef}
+            style={{ display: "none" }}
+            onChange={(uploadEvent) => {
+              setImportYamlLoading(true);
+              const file = uploadEvent.target.files?.[0];
+              const reader = new FileReader();
+              reader.onload = (readerEvent) => {
+                handleYamlUpload(readerEvent.target?.result as string);
+              };
+              if (file) {
+                reader.readAsText(file);
+              }
+            }}
+          />
+          <Tile
+            image={<AirbyteLogo />}
+            title="connectorBuilder.landingPage.importYaml.title"
+            description="connectorBuilder.landingPage.importYaml.description"
+            buttonText="connectorBuilder.landingPage.importYaml.button"
+            buttonProps={{ isLoading: importYamlLoading }}
+            onClick={() => {
+              unregisterNotificationById(YAML_UPLOAD_ERROR_ID);
+              fileInputRef.current?.click();
+            }}
+          />
+
+          <Button
+            onClick={() => {
+              switchToUI();
+              setShowLandingPage(false);
+            }}
+          >
+            Start from scratch
+          </Button>
+        </FlexContainer>
+      </FlexContainer>
     );
   }
 );
+
+interface TileProps {
+  image: React.ReactNode;
+  title: string;
+  description: string;
+  buttonText: string;
+  buttonProps?: Partial<ButtonProps>;
+  onClick: () => void;
+}
+
+const Tile: React.FC<TileProps> = ({ image, title, description, buttonText, buttonProps, onClick }) => {
+  return (
+    <FlexContainer direction="column" gap="lg" className={styles.tile}>
+      {image}
+      <Text>
+        <FormattedMessage id={title} />
+      </Text>
+      <Text>
+        <FormattedMessage id={description} />
+      </Text>
+      <Button onClick={onClick} {...buttonProps}>
+        <FormattedMessage id={buttonText} />
+      </Button>
+    </FlexContainer>
+  );
+};
