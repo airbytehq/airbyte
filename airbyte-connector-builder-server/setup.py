91c1a9adb5836fa8e06abea73acaa235dc94d657
Octavia Squidington III <90398440+octavia-squidington-iii@users.noreply.github.com>
2023-01-30 21:05:22 +0100

    Bump Airbyte version from 0.40.30 to 0.40.31 (#22085)
    Co-authored-by: lmossman <lmossman@users.noreply.github.com>

--- airbyte-connector-builder-server/setup.py
+++ airbyte-connector-builder-server/setup.py
@@ -14,7 +14,7 @@ README = (HERE / "README.md").read_text()
 
 setup(
     name="connector-builder-server",
-    version="0.40.30",
+    version="0.40.31",
     description="",
     long_description=README,
     author="Airbyte",
