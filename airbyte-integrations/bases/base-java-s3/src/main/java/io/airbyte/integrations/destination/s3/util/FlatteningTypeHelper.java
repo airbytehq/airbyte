4196bfa36f45e7eca9cb5dcc2cecc3a814e49cab
nataly <nataly@airbyte.io>
2023-02-02 17:59:50 -0500

    Adds flattening type helper to check the flattening value.

---
+++ airbyte-integrations/bases/base-java-s3/src/main/java/io/airbyte/integrations/destination/s3/util/FlatteningTypeHelper.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright (c) 2022 Airbyte, Inc., all rights reserved.
+ */
+
+package io.airbyte.integrations.destination.s3.util;
+
+import static io.airbyte.integrations.destination.s3.S3DestinationConstants.FLATTENING_TYPE_ARG_NAME;
+import static io.airbyte.integrations.destination.s3.S3DestinationConstants.DEFAULT_FLATTENING_TYPE;
+
+import com.fasterxml.jackson.databind.JsonNode;
+
+public class FlatteningTypeHelper {
+
+    private FlatteningTypeHelper() {}
+
+    /**
+     * Sample expected input: { "compression_type": "No Compression" }
+     */
+    public static FlatteningType parseFlatteningType(final JsonNode flatteningConfig) {
+        if (flatteningConfig == null || flatteningConfig.isNull()) {
+            return DEFAULT_FLATTENING_TYPE;
+        }
+        final String flatteningType = flatteningConfig.get(FLATTENING_TYPE_ARG_NAME).asText();
+        if (flatteningType.equals(FlatteningType.ROOT_LEVEL.name())) {
+            return FlatteningType.ROOT_LEVEL;
+        } else {
+            return FlatteningType.NO;
+        }
+    }
+}
