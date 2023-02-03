26b4ecfe3446fdce52342609fdf0ea14cd29dc44
cpdeethree <conor@airbyte.io>
2023-02-03 14:06:50 -0600

    format

--- airbyte-config/init/src/main/java/io/airbyte/config/init/CombinedConnectorCatalogDownloader.java
+++ airbyte-config/init/src/main/java/io/airbyte/config/init/CombinedConnectorCatalogDownloader.java
@@ -1,22 +1,24 @@
+/*
+ * Copyright (c) 2022 Airbyte, Inc., all rights reserved.
+ */
+
 package io.airbyte.config.init;
 
 import io.airbyte.commons.io.IOs;
 import io.airbyte.commons.json.Jsons;
 import io.airbyte.config.CombinedConnectorCatalog;
-
-
 import java.nio.file.Path;
 
 public class CombinedConnectorCatalogDownloader {
 
-        public static void main(final String[] args) throws Exception {
-            final Path outputRoot = Path.of(args[0]);
-            final String outputFileName = args[1];
+  public static void main(final String[] args) throws Exception {
+    final Path outputRoot = Path.of(args[0]);
+    final String outputFileName = args[1];
 
-            final RemoteDefinitionsProvider remoteDefinitionsProvider = new RemoteDefinitionsProvider("https://storage.googleapis.com/prod-airbyte-cloud-connector-metadata-service/oss_catalog.json", 10000);
-            final CombinedConnectorCatalog combinedCatalog = remoteDefinitionsProvider.getRemoteDefinitionCatalog();
-            IOs.writeFile(outputRoot.resolve(outputFileName), Jsons.toPrettyString(Jsons.jsonNode(combinedCatalog)));
-        }
-    }
+    final RemoteDefinitionsProvider remoteDefinitionsProvider =
+        new RemoteDefinitionsProvider("https://storage.googleapis.com/prod-airbyte-cloud-connector-metadata-service/oss_catalog.json", 10000);
+    final CombinedConnectorCatalog combinedCatalog = remoteDefinitionsProvider.getRemoteDefinitionCatalog();
+    IOs.writeFile(outputRoot.resolve(outputFileName), Jsons.toPrettyString(Jsons.jsonNode(combinedCatalog)));
+  }
 
-
+}
