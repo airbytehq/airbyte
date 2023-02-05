5e2bc9aef6c7d2e921d8f2c8c9a0df573de803c4
Joe Reuter <joe@airbyte.io>
2023-02-05 15:17:24 -0800

    cache versioned schemas

--- airbyte-webapp/scripts/load-declarative-schema.sh
+++ airbyte-webapp/scripts/load-declarative-schema.sh
@@ -1,16 +1,25 @@
 set -e
 mkdir -p build
 
+# Make sure this is aligned with the CDK version of the connector builder server
+DEFAULT_CDK_VERSION="0.26.0"
+
 if [ -z "$CDK_VERSION" ]
 then
-    version="0.25.0"
+    CDK_VERSION=$DEFAULT_CDK_VERSION
 fi
 
 
 if [ -z "$CDK_MANIFEST_PATH" ]
 then
-    echo "Downloading CDK manifest $version from pypi"
-    curl -L https://pypi.python.org/packages/source/a/airbyte-cdk/airbyte-cdk-${version}.tar.gz | tar -xzO airbyte-cdk-${version}/airbyte_cdk/sources/declarative/declarative_component_schema.yaml > build/declarative_component_schema.yaml
+    TARGET_FILE="build/declarative_component_schema-${CDK_VERSION}.yaml"
+    if [ ! -f "$TARGET_FILE" ]; then
+        echo "Downloading CDK manifest schema $CDK_VERSION from pypi"
+        curl -L https://pypi.python.org/packages/source/a/airbyte-cdk/airbyte-cdk-${CDK_VERSION}.tar.gz | tar -xzO airbyte-cdk-${CDK_VERSION}/airbyte_cdk/sources/declarative/declarative_component_schema.yaml > ${TARGET_FILE}
+    else
+        echo "Found cached CDK manifest schema $CDK_VERSION"
+    fi
+    cp ${TARGET_FILE} build/declarative_component_schema.yaml
 else
     echo "Copying local CDK manifest version from $CDK_MANIFEST_PATH"
     cp ${CDK_MANIFEST_PATH} build/declarative_component_schema.yaml
