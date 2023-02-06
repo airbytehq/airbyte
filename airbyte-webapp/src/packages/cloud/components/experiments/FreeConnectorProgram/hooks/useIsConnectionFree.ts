c407215ba993ae2951a15102d11b7c343e9f1439
josephkmh <joseph@airbyte.io>
2023-02-06 10:26:53 +0100

    add comment

--- airbyte-webapp/src/packages/cloud/components/experiments/FreeConnectorProgram/hooks/useIsConnectionFree.ts
+++ airbyte-webapp/src/packages/cloud/components/experiments/FreeConnectorProgram/hooks/useIsConnectionFree.ts
@@ -5,6 +5,11 @@ import { useSourceDefinition } from "services/connector/SourceDefinitionService"
 import { useFreeConnectorProgram } from "./useFreeConnectorProgram";
 import { freeReleaseStages } from "../lib/model";
 
+/**
+ * Detects whether a given conneciton is free according to the terms of the free connector program.
+ * A connection might be disabled in the UI due to a negative credit balance, but we still want to enable it
+ * if the user is enrolled in the FCP and the connection is eligible.
+ */
 export const useIsConnectionFree = (connection: WebBackendConnectionRead | WebBackendConnectionListItem): boolean => {
   const { userDidEnroll } = useFreeConnectorProgram();
   const sourceReleaseStage = useSourceDefinition(connection.source.sourceDefinitionId).releaseStage;
