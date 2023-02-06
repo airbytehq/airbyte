494eb716057694eedb8cf4025e8cdb5a4641eed1
Lake Mossman <lake@airbyte.io>
2023-02-06 09:19:18 -0800

    🪟 🎉  Connector Builder Landing Page (#22122)
    * save working version of minimal landing page
    
    * move landing page to own component and show Toast on invalid yaml
    
    * add default Untitled connector name
    
    * save styling progress
    
    * style the landing page
    
    * use card component
    
    * reword descriptions
    
    * undo unnecessary syntax change
    
    * use file name as connector name
    
    * move showLandingPage to global state and set to true on reset
    
    * fix e2e test
    
    * move local storage hooks into a separate service/context
    
    * move landing page to its own page
    
    * update doc
    
    * move full file input handling logic into callback
    
    * use hidden
    
    * clear out notification on unmount
    
    * use url if filename is 'manifest'
    
    * add comment
    
    ---------
    
    Co-authored-by: Joe Reuter <joe@airbyte.io>

--- airbyte-webapp/src/components/connectorBuilder/types.ts
+++ airbyte-webapp/src/components/connectorBuilder/types.ts
@@ -25,6 +25,8 @@ import {
   CursorPagination,
 } from "core/request/ConnectorManifest";
 
+export type EditorView = "ui" | "yaml";
+
 export interface BuilderFormInput {
   key: string;
   required: boolean;
@@ -99,7 +101,7 @@ export interface BuilderStream {
 
 export const DEFAULT_BUILDER_FORM_VALUES: BuilderFormValues = {
   global: {
-    connectorName: "",
+    connectorName: "Untitled",
     urlBase: "",
     authenticator: { type: "NoAuth" },
   },
@@ -656,3 +658,5 @@ export const convertToManifest = (values: BuilderFormValues): ConnectorManifest 
     spec,
   });
 };
+
+export const DEFAULT_JSON_MANIFEST_VALUES: ConnectorManifest = convertToManifest(DEFAULT_BUILDER_FORM_VALUES);
