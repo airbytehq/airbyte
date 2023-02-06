761895d728e87561f7e401d8f08e9fe70d8c382d
Volodymyr Petrov <volodymyr.s.petrov@globallogic.com>
2023-02-06 16:10:38 +0200

    add more interceptors

--- airbyte-webapp-e2e-tests/cypress/commands/interceptors.ts
+++ airbyte-webapp-e2e-tests/cypress/commands/interceptors.ts
@@ -1,6 +1,18 @@
-export const interceptGetConnectionRequest = () => cy.intercept("/api/v1/web_backend/connections/get").as("getConnection")
+export const interceptGetConnectionRequest = () =>
+  cy.intercept("/api/v1/web_backend/connections/get").as("getConnection");
 export const waitForGetConnectionRequest = () => cy.wait("@getConnection");
 
 export const interceptUpdateConnectionRequest = () =>
   cy.intercept("/api/v1/web_backend/connections/update").as("updateConnection");
 export const waitForUpdateConnectionRequest = () => cy.wait("@updateConnection", { timeout: 10000 });
+
+export const interceptDiscoverSchemaRequest = () =>
+  cy.intercept("/api/v1/sources/discover_schema").as("discoverSchema");
+export const waitForDiscoverSchemaRequest = () => cy.wait("@discoverSchema");
+
+export const interceptCreateConnectionRequest = () =>
+  cy.intercept("/api/v1/web_backend/connections/create").as("createConnection");
+export const waitForCreateConnectionRequest = () => cy.wait("@createConnection");
+
+export const interceptGetSourcesListRequest = () => cy.intercept("/api/v1/sources/list").as("getSourcesList");
+export const waitForGetSourcesListRequest = () => cy.wait("@getSourcesList");
