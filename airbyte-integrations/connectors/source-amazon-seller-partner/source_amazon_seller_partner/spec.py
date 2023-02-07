d6122cb80c7d91df51ddabb7ad39559bfbff10e1
dandpz <daniele.dapuzzo92@gmail.com>
2023-02-07 17:38:22 +0100

    add implementation with test

--- airbyte-integrations/connectors/source-amazon-seller-partner/source_amazon_seller_partner/spec.py
+++ airbyte-integrations/connectors/source-amazon-seller-partner/source_amazon_seller_partner/spec.py
@@ -106,9 +106,9 @@ class AmazonSellerPartnerConfig(BaseModel):
         order=12,
     )
 
-    # This field has been introduced because some users experienced a delay on report data update, because
-    # for certain reports the data availability SLA is not explicited this field allows to customize per user this value. Using this sort of
-    # options make possible to extend the usage adding more customization per report.
+    # This field has been introduced because some users experienced a delay on report data update,
+    # for certain reports the data availability SLA is not explicited this field allows to customize this value. Using this sort of
+    # options make possible to extend the usage adding more customization per stream.
     advanced_stream_options: str = Field(
         None,
         description="Additional information to configure report options. This varies by report type, not every report implement this kind of feature. Must be a valid json string.",
