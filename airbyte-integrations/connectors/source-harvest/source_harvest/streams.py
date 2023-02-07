1c050987c8bf5754953e059da7f8cab1ce04c2dd
Sergey Chvalyuk <grubberr@gmail.com>
2023-02-07 12:42:12 +0000

    none AvailabilityStrategy removed
    Signed-off-by: Sergey Chvalyuk <grubberr@gmail.com>

--- airbyte-integrations/connectors/source-harvest/source_harvest/streams.py
+++ airbyte-integrations/connectors/source-harvest/source_harvest/streams.py
@@ -9,7 +9,6 @@ from urllib.parse import parse_qsl, urlparse
 import pendulum
 import requests
 from airbyte_cdk.models import SyncMode
-from airbyte_cdk.sources.streams.availability_strategy import AvailabilityStrategy
 from airbyte_cdk.sources.streams.http import HttpStream
 
 
@@ -25,10 +24,6 @@ class HarvestStream(HttpStream, ABC):
         :return: Default field name to get data from response
         """
         return self.name
-
-    @property
-    def availability_strategy(self) -> Optional["AvailabilityStrategy"]:
-        return None
 
     def backoff_time(self, response: requests.Response):
         if "Retry-After" in response.headers:
