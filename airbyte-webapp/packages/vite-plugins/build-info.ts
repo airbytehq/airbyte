2bae0381489289c595383559dd5fb13bb2606a6e
Tim Roes <tim@airbyte.io>
2023-02-07 17:26:35 +0100

    Add more documentation

--- airbyte-webapp/packages/vite-plugins/build-info.ts
+++ airbyte-webapp/packages/vite-plugins/build-info.ts
@@ -7,6 +7,10 @@ import { v4 as uuidV4 } from "uuid";
 
 const buildHash = uuidV4();
 
+/**
+ * A Vite plugin that will generate on every build a new random UUID and write that to the `/buildInfo.json`
+ * file as well as make it available as `process.env.BUILD_HASH` in code.
+ */
 export function buildInfo(): Plugin {
   return {
     name: "airbyte/build-info",
