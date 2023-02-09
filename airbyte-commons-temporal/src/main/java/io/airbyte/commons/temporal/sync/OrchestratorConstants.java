b3d3bc415cae8df80c497e7e5599fbeeddfcef2c
Benoit Moriceau <benoit@airbyte.io>
2023-02-08 16:43:11 -0800

    Revert "Revert "Add normalization strict incremental feature flag (#22514)" (#22612)" (#22619)
    This reverts commit ba978d3f2ff962261810931d1eccf07b4d784de9.

--- airbyte-commons-temporal/src/main/java/io/airbyte/commons/temporal/sync/OrchestratorConstants.java
+++ airbyte-commons-temporal/src/main/java/io/airbyte/commons/temporal/sync/OrchestratorConstants.java
@@ -73,6 +73,8 @@ public class OrchestratorConstants {
           EnvVariableFeatureFlags.AUTO_DETECT_SCHEMA,
           EnvVariableFeatureFlags.APPLY_FIELD_SELECTION,
           EnvVariableFeatureFlags.FIELD_SELECTION_WORKSPACES,
+          EnvVariableFeatureFlags.STRICT_COMPARISON_NORMALIZATION_WORKSPACES,
+          EnvVariableFeatureFlags.STRICT_COMPARISON_NORMALIZATION_TAG,
           FEATURE_FLAG_CLIENT,
           FEATURE_FLAG_PATH,
           EnvConfigs.LAUNCHDARKLY_KEY,
