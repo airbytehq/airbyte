c6867c8b0506738263de96648104c8226c55b33d
Alex Birdsall <ambirdsall@gmail.com>
2023-02-09 17:56:40 -0800

    Sync sourceId between location.state and query param

--- airbyte-webapp/src/pages/connections/CreateConnectionPage/CreateConnectionPage.tsx
+++ airbyte-webapp/src/pages/connections/CreateConnectionPage/CreateConnectionPage.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { FormattedMessage, useIntl } from "react-intl";
 import { useLocation, useNavigate, useSearchParams } from "react-router-dom";
 
@@ -60,14 +60,44 @@ function usePreloadData(): {
   destinationDefinition?: DestinationDefinitionRead;
 } {
   const location = useLocation();
+  const navigate = useNavigate();
   const [searchParams, setSearchParams] = useSearchParams();
 
-  const sourceId = hasSourceId(location.state) ? location.state.sourceId : searchParams.get("sourceId");
-  const source = useGetSource(sourceId);
+  const sourceIdFromLocationState = hasSourceId(location.state) && location.state.sourceId;
+  const sourceIdFromSearchParams = searchParams.get("sourceId");
+  const sourceId = sourceIdFromLocationState || sourceIdFromSearchParams;
 
-  if (source && source.sourceId !== searchParams.get("sourceId")) {
-    setSearchParams({ sourceId: source.sourceId });
-  }
+  /**
+   * There are two places we may find a sourceId, depending on the scenario. This effect
+   * keeps them in sync according to the following logic:
+   *   0) if `location.state.sourceId` and the `sourceId` query param are both unset or
+   *      are both set to the same value, then we don't need to take any action.
+   *   1) else if `location.state.sourceId` exists, we arrived at this page via the legacy
+   *      internal "routing" system, meaning there has been a user interaction
+   *      specifically selecting that source. This is the highest precedence source of
+   *      truth, so if it exists we explicitly set the sourceId query param to match it.
+   *   2) else if there's a `sourceId` query param, we arrived at this page via a fresh
+   *      page load. This logic was added to support the airbyte-cloud's free connector
+   *      program enrollment flow: it involves a round-trip visit to a Stripe domain,
+   *      which wipes location.state. We explicitly navigate to the current path
+   *      (including the current query string) with `location.state.sourceId` set.
+   */
+  useEffect(() => {
+    if (sourceIdFromLocationState && sourceIdFromSearchParams === sourceIdFromLocationState) {
+      // sourceId is set and everything is in sync, no further action needed
+    } else if (sourceIdFromLocationState) {
+      sourceId && setSearchParams({ sourceId });
+    } else if (sourceIdFromSearchParams) {
+      // we have to simultaneously set both the query string and location.state to avoid
+      // an infinite mutually recursive rerender loop:
+      //   A: set location.state and rerender; GOTO B
+      //   B: set query param and rerender; GOTO A
+      navigate(`${location.search}`, { state: { sourceId }, replace: true });
+    } else {
+      // sourceId is unset and everything is in sync, no further action needed
+    }
+  }, [sourceIdFromLocationState, sourceIdFromSearchParams, setSearchParams, navigate, sourceId, location.search]);
+  const source = useGetSource(sourceId);
 
   const sourceDefinition = useSourceDefinition(source?.sourceDefinitionId);
 
@@ -85,7 +115,7 @@ export const CreateConnectionPage: React.FC = () => {
   const navigate = useNavigate();
   const { clearAllFormChanges } = useFormChangeTrackerService();
 
-  // TODO: Probably there is a better way to figure it out instead of just checking third elem
+  // TODO: select UI and behavior based on the route using, you know, the router
   const locationType = location.pathname.split("/")[3];
 
   const type: EntityStepsTypes =
