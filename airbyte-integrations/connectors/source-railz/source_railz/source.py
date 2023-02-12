f5cbf542a0eecf81f7738b39e215828f228fa0e9
Sergey Chvalyuk <grubberr@gmail.com>
2023-02-12 07:45:34 +0000

    railz.yaml -> manifest.yaml
    Signed-off-by: Sergey Chvalyuk <grubberr@gmail.com>

--- airbyte-integrations/connectors/source-railz/source_railz/source.py
+++ airbyte-integrations/connectors/source-railz/source_railz/source.py
@@ -15,4 +15,4 @@ WARNING: Do not modify this file.
 # Declarative Source
 class SourceRailz(YamlDeclarativeSource):
     def __init__(self):
-        super().__init__(**{"path_to_yaml": "railz.yaml"})
+        super().__init__(**{"path_to_yaml": "manifest.yaml"})
