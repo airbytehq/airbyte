4063ed2e7e6aaefd227b2c96d80dd72eaa99af5d
Edmundo Ruiz Ghanem <edmundo@airbyte.io>
2023-02-13 14:49:08 -0500

    Update StickEditControlsContainer to have a blurry top instead of a simulated card

--- airbyte-webapp/src/pages/connections/ConnectionReplicationPage/StickyEditControlsContainer.tsx
+++ airbyte-webapp/src/pages/connections/ConnectionReplicationPage/StickyEditControlsContainer.tsx
@@ -1,12 +1,7 @@
 import React from "react";
 
-import { Card } from "components/ui/Card";
-
 import styles from "./StickyEditControlsContainer.module.scss";
 
 export const StickyEditControlsContainer: React.FC<React.PropsWithChildren<unknown>> = ({ children }) => (
-  <div className={styles.container}>
-    <Card className={styles.halfCard} />
-    {children}
-  </div>
+  <div className={styles.container}>{children}</div>
 );
