e8d5c3a3ff1fa42ad2927166fbe9137cf44fe9e7
Roman Yermilov [GL] <86300758+roman-yermilov-gl@users.noreply.github.com>
2023-02-14 14:44:56 +0400

    Source Zendesk Support: fix tests (#22483)
    * Source Zendesk Support: fix tests
    
    * Source Zendesk Support: bump version
    
    * Source Zendesk Support: update changelog
    
    * Source Zendesk Support: update expected records
    
    * Source Zendesk Support: update expected records

--- airbyte-integrations/connectors/source-zendesk-support/source_zendesk_support/streams.py
+++ airbyte-integrations/connectors/source-zendesk-support/source_zendesk_support/streams.py
@@ -12,6 +12,8 @@ from collections import deque
 from concurrent.futures import Future, ProcessPoolExecutor
 from datetime import datetime, timedelta
 from functools import partial
+
+from airbyte_cdk.sources.streams.core import StreamData
 from math import ceil
 from pickle import PickleError, dumps
 from typing import Any, Iterable, List, Mapping, MutableMapping, Optional, Union
@@ -27,6 +29,7 @@ from airbyte_cdk.sources.streams.http.auth.core import HttpAuthenticator
 from airbyte_cdk.sources.streams.http.exceptions import DefaultBackoffException
 from airbyte_cdk.sources.streams.http.rate_limiting import TRANSIENT_EXCEPTIONS
 from airbyte_cdk.sources.utils.transform import TransformConfig, TypeTransformer
+from requests import HTTPError
 from requests.auth import AuthBase
 from requests_futures.sessions import PICKLE_ERROR, FuturesSession
 
@@ -534,6 +537,20 @@ class Tickets(SourceZendeskIncrementalExportStream):
 
     response_list_name: str = "tickets"
     transformer: TypeTransformer = TypeTransformer(TransformConfig.DefaultSchemaNormalization)
+
+    def read_records(
+        self,
+        sync_mode: SyncMode,
+        cursor_field: List[str] = None,
+        stream_slice: Mapping[str, Any] = None,
+        stream_state: Mapping[str, Any] = None,
+    ) -> Iterable[StreamData]:
+        try:
+            yield from super(Tickets, self).read_records(sync_mode, cursor_field, stream_slice, stream_state)
+        except HTTPError as e:
+            if e.response.status_code == 400 and e.response.json().get('error', '') == 'StartTimeTooRecent':
+                return []
+            raise e
 
 
 class TicketComments(SourceZendeskSupportTicketEventsExportStream):
