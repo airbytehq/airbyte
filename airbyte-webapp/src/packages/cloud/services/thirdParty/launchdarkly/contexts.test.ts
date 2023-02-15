d958f9197459948cdf321116d53afae8e725cfd1
josephkmh <joseph@airbyte.io>
2023-02-15 14:26:42 +0100

    add context creation methods and tests

---
+++ airbyte-webapp/src/packages/cloud/services/thirdParty/launchdarkly/contexts.test.ts
@@ -0,0 +1,66 @@
+import { v4 as uuidV4 } from "uuid";
+
+import { User } from "packages/cloud/lib/domain/users";
+
+import { createMultiContext, createUserContext, createWorkspaceContext } from "./contexts";
+
+const mockLocale = "en";
+
+describe(`${createUserContext.name}`, () => {
+  it("creates an anonymous user context", () => {
+    const context = createUserContext(null, mockLocale);
+    expect(context).toEqual({
+      kind: "user",
+      anonymous: true,
+      locale: mockLocale,
+    });
+  });
+
+  it("creates an identified user context", () => {
+    const mockUser: User = {
+      userId: uuidV4(),
+      name: "John Doe",
+      email: "john.doe@airbyte.io",
+      intercomHash: "intercome_hash_string",
+      authUserId: "auth_user_id_string",
+    };
+    const context = createUserContext(mockUser, mockLocale);
+    expect(context).toEqual({
+      kind: "user",
+      anonymous: false,
+      key: mockUser.userId,
+      email: mockUser.email,
+      name: mockUser.name,
+      intercomHash: mockUser.intercomHash,
+      locale: mockLocale,
+    });
+  });
+});
+
+describe(`${createWorkspaceContext.name}`, () => {
+  it("creates a workspace context", () => {
+    const mockWorkspaceId = uuidV4();
+    const context = createWorkspaceContext(mockWorkspaceId);
+    expect(context).toEqual({
+      kind: "workspace",
+      key: mockWorkspaceId,
+    });
+  });
+});
+
+describe(`${createMultiContext.name}`, () => {
+  it("creates a workspace context", () => {
+    const mockWorkspaceId = uuidV4();
+    const userContext = createUserContext(null, mockLocale);
+    const workspaceContext = createWorkspaceContext(mockWorkspaceId);
+    const multiContext = createMultiContext([userContext, workspaceContext]);
+    const { kind: userKind, ...userContextWithoutKind } = userContext;
+    const { kind: workspaceKind, ...workspaceContextWithoutKind } = workspaceContext;
+
+    expect(multiContext).toEqual({
+      kind: "multi",
+      user: userContextWithoutKind,
+      workspace: workspaceContextWithoutKind,
+    });
+  });
+});
---
+++ airbyte-webapp/src/packages/cloud/services/thirdParty/launchdarkly/contexts.ts
@@ -0,0 +1,46 @@
+import { LDMultiKindContext, LDSingleKindContext } from "launchdarkly-js-client-sdk";
+
+import { User } from "packages/cloud/lib/domain/users/types";
+
+export function createUserContext(user: User | null, locale: string): LDSingleKindContext {
+  const kind = "user";
+
+  if (!user) {
+    return {
+      kind,
+      anonymous: true,
+      locale,
+      // @ts-expect-error LaunchDarkly's typescript types specify that key must be defined, but actually they handle a null or undefined key. It's even recommended in their docs to omit the key for anonymous users https://docs.launchdarkly.com/sdk/client-side/javascript/migration-2-to-3#understanding-changes-to-anonymous-users
+      key: undefined,
+    };
+  }
+
+  return {
+    kind,
+    anonymous: false,
+    key: user.userId,
+    email: user.email,
+    name: user.name,
+    intercomHash: user.intercomHash,
+    locale,
+  };
+}
+
+export function createWorkspaceContext(workspaceId: string): LDSingleKindContext {
+  return {
+    kind: "workspace",
+    key: workspaceId,
+  };
+}
+
+export function createMultiContext(contexts: LDSingleKindContext[]): LDMultiKindContext {
+  const multiContext: LDMultiKindContext = {
+    kind: "multi",
+  };
+
+  contexts.forEach(({ kind, ...context }) => {
+    multiContext[kind] = { ...context };
+  });
+
+  return multiContext;
+}
