86fc09787252a832d8813c17a93b94f46d4de75b
Ben Church <ben@airbyte.io>
2023-02-15 12:56:13 -0800

    Add LOCAL_CONNECTOR_CATALOG_PATH env var override

--- airbyte-config/config-models/src/main/java/io/airbyte/config/CatalogDefinitionsConfig.java
+++ airbyte-config/config-models/src/main/java/io/airbyte/config/CatalogDefinitionsConfig.java
@@ -5,25 +5,37 @@
 package io.airbyte.config;
 
 import java.nio.file.Path;
+import java.util.Optional;
 
 public class CatalogDefinitionsConfig {
 
   private static final String RESOURCE_DIRECTORY_PATH = "src/main/resources/";
-  private static final String LOCAL_CONNECTOR_CATALOG_FILE_NAME = "oss_catalog.json";
   private static final String SEED_SUBDIRECTORY = "seed/";
-
   private static final String ICON_SUBDIRECTORY = "icons/";
+  private static final String LOCAL_CONNECTOR_CATALOG_FILE_NAME = "oss_catalog.json";
+  private static final String DEFAULT_LOCAL_CONNECTOR_CATALOG_PATH =
+      SEED_SUBDIRECTORY + LOCAL_CONNECTOR_CATALOG_FILE_NAME;
+
 
   private static final String REMOTE_OSS_CATALOG_URL =
       "https://storage.googleapis.com/prod-airbyte-cloud-connector-metadata-service/oss_catalog.json";
 
   public static String getLocalConnectorCatalogPath() {
-    return SEED_SUBDIRECTORY + LOCAL_CONNECTOR_CATALOG_FILE_NAME;
+    Optional<String> customCatalogPath = new EnvConfigs().getLocalCatalogPath();
+    if (customCatalogPath.isPresent()) {
+      return customCatalogPath.get();
+    }
+
+    return DEFAULT_LOCAL_CONNECTOR_CATALOG_PATH;
+
   }
 
   public static Path getLocalCatalogWritePath() {
     final Path resourcesRoot = Path.of(RESOURCE_DIRECTORY_PATH);
-    final Path writePath = resourcesRoot.resolve(getLocalConnectorCatalogPath());
+
+    // We always want to write to the default path
+    // This is to prevent overwriting the catalog file in the event we are using a custom catalog path.
+    final Path writePath = resourcesRoot.resolve(DEFAULT_LOCAL_CONNECTOR_CATALOG_PATH);
     return writePath;
   }
 
