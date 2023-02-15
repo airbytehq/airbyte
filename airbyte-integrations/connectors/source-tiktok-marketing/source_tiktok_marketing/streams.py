b2659ba5d0ad03a05bb315e3ff02a7b66d40383b
Daryna Ishchenko <darina.ishchenko17@gmail.com>
2023-02-15 18:34:18 +0200

    added handling of 504 error

--- airbyte-integrations/connectors/source-tiktok-marketing/source_tiktok_marketing/streams.py
+++ airbyte-integrations/connectors/source-tiktok-marketing/source_tiktok_marketing/streams.py
@@ -204,10 +204,14 @@ class TiktokStream(HttpStream, ABC):
         Once the rate limit is met, the server returns "code": 40100
         Docs: https://business-api.tiktok.com/marketing_api/docs?id=1701890997610497
         Retry 50002 as well - it's a server error.
+        Retry when 504 error: response doesn't consist json, so we need to handle response status code to retry.
         """
         try:
             data = response.json()
         except Exception:
+            if response.status_code == 504:
+                self.logger.error(f"Gateway Timeout: The proxy server did not receive a timely response from the upstream server.")
+                return super().should_retry(response)
             self.logger.error(f"Incorrect JSON response: {response.text}")
             raise
         if data["code"] in (40100, 50002):
