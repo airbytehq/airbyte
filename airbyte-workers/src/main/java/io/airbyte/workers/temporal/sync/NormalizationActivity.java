3fff49eb21bb750ac08074b3bdd11578858d8b8f
Benoit Moriceau <benoit@airbyte.io>
2023-02-15 14:22:16 +0900

    Reduce input generation payload (#22840)
    * Reduce input geenration payload
    
    * PR comments
    
    * format
    
    ---------
    
    Co-authored-by: Davin Chia <davinchia@gmail.com>

--- airbyte-workers/src/main/java/io/airbyte/workers/temporal/sync/NormalizationActivity.java
+++ airbyte-workers/src/main/java/io/airbyte/workers/temporal/sync/NormalizationActivity.java
@@ -4,14 +4,17 @@
 
 package io.airbyte.workers.temporal.sync;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import io.airbyte.config.NormalizationInput;
 import io.airbyte.config.NormalizationSummary;
 import io.airbyte.config.StandardSyncInput;
 import io.airbyte.config.StandardSyncOutput;
 import io.airbyte.persistence.job.models.IntegrationLauncherConfig;
 import io.airbyte.persistence.job.models.JobRunConfig;
+import io.airbyte.protocol.models.ConfiguredAirbyteCatalog;
 import io.temporal.activity.ActivityInterface;
 import io.temporal.activity.ActivityMethod;
+import java.util.UUID;
 
 @ActivityInterface
 public interface NormalizationActivity {
@@ -23,5 +26,10 @@ public interface NormalizationActivity {
 
   @ActivityMethod
   NormalizationInput generateNormalizationInput(final StandardSyncInput syncInput, final StandardSyncOutput syncOutput);
+
+  @ActivityMethod
+  NormalizationInput generateNormalizationInputWithMinimumPayload(final JsonNode destinationConfiguration,
+                                                                  final ConfiguredAirbyteCatalog airbyteCatalog,
+                                                                  final UUID workspaceId);
 
 }
