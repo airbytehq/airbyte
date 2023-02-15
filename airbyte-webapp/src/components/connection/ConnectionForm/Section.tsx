a441537b87eaf732c983d377686f3d1139446f70
Volodymyr Petrov <volodymyr.s.petrov@globallogic.com>
2023-02-15 02:55:39 +0200

    extend Section component to support collapsed state with preview info

--- airbyte-webapp/src/components/connection/ConnectionForm/Section.tsx
+++ airbyte-webapp/src/components/connection/ConnectionForm/Section.tsx
@@ -1,26 +1,52 @@
+import { faChevronRight } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import classNames from "classnames";
+import React, { useState } from "react";
 
 import { Card } from "components/ui/Card";
 import { Heading } from "components/ui/Heading";
 
 import styles from "./Section.module.scss";
 
-interface SectionProps {
+export interface SectionProps {
   title?: React.ReactNode;
   flush?: boolean;
   className?: string;
+  collapsible?: boolean;
+  collapsedPreviewInfo?: React.ReactNode;
+  collapsedInitially?: boolean;
 }
 
-export const Section: React.FC<React.PropsWithChildren<SectionProps>> = ({ title, flush, children, className }) => {
+export const Section: React.FC<React.PropsWithChildren<SectionProps>> = ({
+  title,
+  flush,
+  children,
+  className,
+  collapsible = false,
+  collapsedPreviewInfo,
+  collapsedInitially = false,
+}) => {
+  const [isCollapsed, setIsCollapsed] = useState(collapsedInitially);
+
   return (
     <Card>
       <div className={classNames(styles.section, { [styles.flush]: flush }, className)}>
-        {title && (
-          <Heading as="h2" size="sm">
-            {title}
-          </Heading>
-        )}
-        {children}
+        <div className={styles.header}>
+          {title && (
+            <Heading as="h2" size="sm">
+              {title}
+            </Heading>
+          )}
+          {collapsible && (
+            <FontAwesomeIcon
+              className={classNames(styles.arrow, { [styles.collapsed]: isCollapsed })}
+              icon={faChevronRight}
+              onClick={() => setIsCollapsed((prevState) => !prevState)}
+            />
+          )}
+        </div>
+        {isCollapsed && collapsedPreviewInfo}
+        {collapsible ? !isCollapsed && children : children}
       </div>
     </Card>
   );
