23ffed5f3d6a0fa3ea40a6bbc6f9c1479f967d71
pmossman <parker@airbyte.io>
2023-02-16 09:30:45 -0800

    start adding cron

---
+++ airbyte-cron/src/main/java/io/airbyte/cron/workspace_failure_monitor/WorkspaceFailureMonitor.java
@@ -0,0 +1,10 @@
+package io.airbyte.cron.workspace_failure_monitor;
+
+/**
+ * WorkspaceFailureMonitor
+ *
+ * Emits failure metrics for workspace IDs that are configured for examination.
+ */
+public class WorkspaceFailureMonitor {
+
+}
