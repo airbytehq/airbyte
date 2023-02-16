5e0349fba8420daf77a21532cf2d3c343cd54e44
Oleksandr Bazarnov <oleksandr.bazarnov@globallogic.com>
2023-02-16 23:58:45 +0200

    updated the  ratelimiter  and added custom requester

--- airbyte-integrations/connectors/source-intercom/source_intercom/components.py
+++ airbyte-integrations/connectors/source-intercom/source_intercom/components.py
@@ -2,21 +2,29 @@
 # Copyright (c) 2023 Airbyte, Inc., all rights reserved.
 #
 
-import re
+import os
 from dataclasses import InitVar, dataclass, field
-from typing import Any, Iterable, List, Mapping, Optional, Union
+from functools import lru_cache, wraps
+from time import sleep
+from typing import Any, Iterable, List, Mapping, MutableMapping, Optional, Union
 
 import dpath.util
 import requests
 from airbyte_cdk.models import AirbyteMessage, SyncMode, Type
+from airbyte_cdk.sources.declarative.auth.declarative_authenticator import DeclarativeAuthenticator, NoAuth
 from airbyte_cdk.sources.declarative.interpolation.interpolated_string import InterpolatedString
 from airbyte_cdk.sources.declarative.partition_routers.substream_partition_router import ParentStreamConfig
-from airbyte_cdk.sources.declarative.requesters.error_handlers.backoff_strategies.header_helper import get_numeric_value_from_header
-from airbyte_cdk.sources.declarative.requesters.error_handlers.backoff_strategy import BackoffStrategy
+from airbyte_cdk.sources.declarative.requesters.error_handlers.default_error_handler import DefaultErrorHandler
+from airbyte_cdk.sources.declarative.requesters.error_handlers.error_handler import ErrorHandler
+from airbyte_cdk.sources.declarative.requesters.error_handlers.response_status import ResponseStatus
 from airbyte_cdk.sources.declarative.requesters.request_option import RequestOptionType
+from airbyte_cdk.sources.declarative.requesters.request_options.interpolated_request_input_provider import InterpolatedRequestInputProvider
+from airbyte_cdk.sources.declarative.requesters.requester import HttpMethod, Requester
 from airbyte_cdk.sources.declarative.stream_slicers.stream_slicer import StreamSlicer
 from airbyte_cdk.sources.declarative.types import Config, Record, StreamSlice, StreamState
 from airbyte_cdk.sources.streams.core import Stream
+
+RequestInput = Union[str, Mapping[str, str]]
 
 
 @dataclass
@@ -350,49 +358,30 @@ class IncrementalSubstreamSlicer(StreamSlicer):
 
 
 @dataclass
-class LoadBasedWaitTimeFromHeaderBackoffStrategy(BackoffStrategy):
+class IntercomRateLimiter:
     """
-    To avoid reaching Intercom API Rate Limits, use the 'X-RateLimit-Limit','X-RateLimit-Remaining' header values,
-    to determine the current rate limits and load and handle wait_time based on load %.
-    Recomended wait_time between each request is 1 sec, we would handle this dynamicaly.
-
-    :: current_rate_header - responce header item, contains information with max rate_limits available (max)
-    :: regex_current_rate_header - same as `current_rate_header`, but uses `RegExp` to fetch the header value
-    :: total_rate_header - responce header item, contains information with how many requests are still available (current)
-    :: regex_total_rate_header - same as `total_rate_header`, but uses `RegExp` to fetch the header value
-    :: threshold - is the % cutoff for the rate_limits % load, if this cutoff is crossed,
-        the connector waits `x` amount of time, default value = 0.1 (10% left from max capacity)
-
-    Header example:
-    {
-        X-RateLimit-Limit: 100
-        X-RateLimit-Remaining: 51
-        X-RateLimit-Reset: 1487332510
-    },
-        where: 51 - requests remains and goes down, 100 - max requests capacity.
-
-    More information: https://developers.intercom.com/intercom-api-reference/reference/rate-limiting
+    Define timings for RateLimits. Adjust timings if needed.
+    :: on_unknown_load = 1.0 sec - Intercom recommended time to hold between each API call.
+    :: on_low_load = 0.01 sec (10 miliseconds) - ideal ratio between hold time and api call, also the standard hold time between each API call.
+    :: on_mid_load = 1.5 sec - great timing to retrieve another 15% of request capacity while having mid_load.
+    :: on_high_load = 8.0 sec - ideally we should wait 5.0 sec while having high_load, but we hold 8 sec to retrieve up to 80% of request capacity.
     """
 
-    config: Config
-    parameters: InitVar[Mapping[str, Any]]
-    current_rate_header: Union[InterpolatedString, str]
-    total_rate_header: Union[InterpolatedString, str]
-    threshold: float = field(default=float)
-    wait_on_load: Mapping[str, float] = field(default_factory=dict)
-    regex_current_rate_header: Optional[str] = None
-    regex_total_rate_header: Optional[str] = None
+    threshold: float = 0.1
+    on_unknown_load: float = 1.0
+    on_low_load: float = 0.01
+    on_mid_load: float = 1.5
+    on_high_load: float = 8.0  # max time
 
-    def __post_init__(self, parameters: Mapping[str, Any]):
-        self.current_rate_header = InterpolatedString.create(self.current_rate_header, parameters=parameters)
-        self.total_rate_header = InterpolatedString.create(self.total_rate_header, parameters=parameters)
-        self.regex_current_rate_header = re.compile(self.regex_current_rate_header) if self.regex_current_rate_header else None
-        self.regex_total_rate_header = re.compile(self.regex_total_rate_header) if self.regex_total_rate_header else None
+    @staticmethod
+    def backoff_time(backoff_time: float):
+        return sleep(backoff_time)
 
+    @staticmethod
     def _define_values_from_headers(
-        self,
         current_rate_header_value: Optional[float],
         total_rate_header_value: Optional[float],
+        threshold: float = threshold,
     ) -> tuple[float, Union[float, str]]:
         # define current load and cutoff from rate_limits
         if current_rate_header_value and total_rate_header_value:
@@ -400,26 +389,239 @@ class LoadBasedWaitTimeFromHeaderBackoffStrategy(BackoffStrategy):
             load: float = current_rate_header_value / total_rate_header_value
         else:
             # to guarantee cutoff value to be exactly 1 sec, based on threshold, if headers are not available
-            cutoff: float = self.threshold * (1 / self.threshold)
+            cutoff: float = threshold * (1 / threshold)
             load = None
         return cutoff, load
 
-    def _convert_load_to_wait_time(self, cutoff: float, load: Optional[float] = None) -> float:
-        # define wait_time based on load conditions
+    @staticmethod
+    def _convert_load_to_backoff_time(
+        cutoff: float,
+        load: Optional[float] = None,
+        threshold: float = threshold,
+    ) -> float:
+        # define backoff_time based on load conditions
         if not load:
-            wait_time = self.wait_on_load.get("unknown")
-        elif load <= self.threshold:
-            wait_time = self.wait_on_load.get("max")
+            backoff_time = IntercomRateLimiter.on_unknown_load
+        elif load <= threshold:
+            backoff_time = IntercomRateLimiter.on_high_load
         elif load <= cutoff:
-            wait_time = self.wait_on_load.get("avg")
+            backoff_time = IntercomRateLimiter.on_mid_load
         elif load > cutoff:
-            wait_time = self.wait_on_load.get("min")
-        return wait_time
+            backoff_time = IntercomRateLimiter.on_low_load
+        return backoff_time
 
-    def backoff(self, response: requests.Response, attempt_count: int) -> Optional[float]:
-        current_rate_header: str = self.current_rate_header.eval(config=self.config)
-        total_rate_header: str = self.total_rate_header.eval(config=self.config)
-        current_rate_header_value: float = get_numeric_value_from_header(response, current_rate_header, self.regex_current_rate_header)
-        total_rate_header_value: float = get_numeric_value_from_header(response, total_rate_header, self.regex_total_rate_header)
-        cutoff, load = self._define_values_from_headers(current_rate_header_value, total_rate_header_value)
-        return self._convert_load_to_wait_time(cutoff, load)
+    @staticmethod
+    def get_backoff_time(
+        *args,
+        threshold: float = threshold,
+        rate_limit_header: str = "X-RateLimit-Limit",
+        rate_limit_remain_header: str = "X-RateLimit-Remaining",
+    ):
+        """
+        To avoid reaching Intercom API Rate Limits, use the 'X-RateLimit-Limit','X-RateLimit-Remaining' header values,
+        to determine the current rate limits and load and handle backoff_time based on load %.
+        Recomended backoff_time between each request is 1 sec, we would handle this dynamicaly.
+        :: threshold - is the % cutoff for the rate_limits % load, if this cutoff is crossed,
+                        the connector waits `sleep_on_high_load` amount of time, default value = 0.1 (10% left from max capacity)
+        :: backoff_time - time between each request = 200 miliseconds
+        :: rate_limit_header - responce header item, contains information with max rate_limits available (max)
+        :: rate_limit_remain_header - responce header item, contains information with how many requests are still available (current)
+        Header example:
+        {
+            X-RateLimit-Limit: 100
+            X-RateLimit-Remaining: 51
+            X-RateLimit-Reset: 1487332510
+        },
+            where: 51 - requests remains and goes down, 100 - max requests capacity.
+        More information: https://developers.intercom.com/intercom-api-reference/reference/rate-limiting
+        """
+
+        # find the requests.Response inside args list
+        for arg in args:
+            if isinstance(arg, requests.models.Response):
+                headers = arg.headers or {}
+
+        # Get the rate_limits from response
+        total_rate = int(headers.get(rate_limit_header, 0)) if headers else None
+        current_rate = int(headers.get(rate_limit_remain_header, 0)) if headers else None
+        cutoff, load = IntercomRateLimiter._define_values_from_headers(
+            current_rate_header_value=current_rate,
+            total_rate_header_value=total_rate,
+            threshold=threshold,
+        )
+
+        backoff_time = IntercomRateLimiter._convert_load_to_backoff_time(cutoff=cutoff, load=load, threshold=threshold)
+        return backoff_time
+
+    @staticmethod
+    def balance_rate_limit(
+        threshold: float = threshold,
+        rate_limit_header: str = "X-RateLimit-Limit",
+        rate_limit_remain_header: str = "X-RateLimit-Remaining",
+    ):
+        """
+        The decorator function.
+        Adjust `threshold`,`rate_limit_header`,`rate_limit_remain_header` if needed.
+        """
+
+        def decorator(func):
+            @wraps(func)
+            def wrapper_balance_rate_limit(*args, **kwargs):
+                IntercomRateLimiter.backoff_time(
+                    IntercomRateLimiter.get_backoff_time(
+                        *args, threshold=threshold, rate_limit_header=rate_limit_header, rate_limit_remain_header=rate_limit_remain_header
+                    )
+                )
+                return func(*args, **kwargs)
+
+            return wrapper_balance_rate_limit
+
+        return decorator
+
+
+@dataclass
+class HttpRequesterWithRateLimiter(Requester):
+    """
+    The difference between the built-in `HttpRequester` and this one is the custom decorator,
+    applied on top of `interpret_response_status` to preserve the api calls for a defined amount of time,
+    calculated using the rate limit headers and not use the custom backoff strategy,
+    since we deal with Response.status_code == 200,
+    the default requester's logic doesn't allow to handle the status of 200 with `should_retry()`.
+
+    Attributes:
+        name (str): Name of the stream. Only used for request/response caching
+        url_base (Union[InterpolatedString, str]): Base url to send requests to
+        path (Union[InterpolatedString, str]): Path to send requests to
+        http_method (Union[str, HttpMethod]): HTTP method to use when sending requests
+        request_options_provider (Optional[InterpolatedRequestOptionsProvider]): request option provider defining the options to set on outgoing requests
+        authenticator (DeclarativeAuthenticator): Authenticator defining how to authenticate to the source
+        error_handler (Optional[ErrorHandler]): Error handler defining how to detect and handle errors
+        config (Config): The user-provided configuration as specified by the source's spec
+    """
+
+    name: str
+    url_base: Union[InterpolatedString, str]
+    path: Union[InterpolatedString, str]
+    config: Config
+    parameters: InitVar[Mapping[str, Any]]
+    http_method: Union[str, HttpMethod] = HttpMethod.GET
+    request_parameters: Optional[RequestInput] = None
+    request_headers: Optional[RequestInput] = None
+    request_body_data: Optional[RequestInput] = None
+    request_body_json: Optional[RequestInput] = None
+    authenticator: DeclarativeAuthenticator = None
+    error_handler: Optional[ErrorHandler] = None
+
+    def __post_init__(self, parameters: Mapping[str, Any]):
+        self.url_base = InterpolatedString.create(self.url_base, parameters=parameters)
+        self.path = InterpolatedString.create(self.path, parameters=parameters)
+
+        self.authenticator = self.authenticator or NoAuth(parameters=parameters)
+        if type(self.http_method) == str:
+            self.http_method = HttpMethod[self.http_method]
+        self._method = self.http_method
+        self.error_handler = self.error_handler or DefaultErrorHandler(parameters=parameters, config=self.config)
+        self._parameters = parameters
+
+        self.request_parameters = self.request_parameters if self.request_parameters else {}
+        self.request_headers = self.request_headers if self.request_headers else {}
+        self.request_body_data = self.request_body_data if self.request_body_data else {}
+        self.request_body_json = self.request_body_json if self.request_body_json else {}
+
+        if self.request_body_json and self.request_body_data:
+            raise ValueError("RequestOptionsProvider should only contain either 'request_body_data' or 'request_body_json' not both")
+
+        self._parameter_interpolator = InterpolatedRequestInputProvider(
+            config=self.config, request_inputs=self.request_parameters, parameters=parameters
+        )
+        self._headers_interpolator = InterpolatedRequestInputProvider(
+            config=self.config, request_inputs=self.request_headers, parameters=parameters
+        )
+        self._body_data_interpolator = InterpolatedRequestInputProvider(
+            config=self.config, request_inputs=self.request_body_data, parameters=parameters
+        )
+        self._body_json_interpolator = InterpolatedRequestInputProvider(
+            config=self.config, request_inputs=self.request_body_json, parameters=parameters
+        )
+
+    @property
+    def cache_filename(self) -> str:
+        return f"{self.name}.yml"
+
+    @property
+    def use_cache(self) -> bool:
+        return False
+
+    def __hash__(self):
+        return hash(tuple(self.__dict__))
+
+    def get_authenticator(self):
+        return self.authenticator
+
+    def get_url_base(self):
+        return os.path.join(self.url_base.eval(self.config), "")
+
+    def get_path(
+        self, *, stream_state: Optional[StreamState], stream_slice: Optional[StreamSlice], next_page_token: Optional[Mapping[str, Any]]
+    ) -> str:
+        kwargs = {"stream_state": stream_state, "stream_slice": stream_slice, "next_page_token": next_page_token}
+        path = self.path.eval(self.config, **kwargs)
+        return path.strip("/")
+
+    def get_method(self):
+        return self._method
+
+    # The RateLimiter is applied to balance the api requests.
+    @lru_cache(maxsize=10)
+    @IntercomRateLimiter.balance_rate_limit()
+    def interpret_response_status(self, response: requests.Response) -> ResponseStatus:
+        # Check for response.headers to define the backoff time before the next api call
+        return self.error_handler.interpret_response(response)
+
+    def get_request_params(
+        self,
+        *,
+        stream_state: Optional[StreamState] = None,
+        stream_slice: Optional[StreamSlice] = None,
+        next_page_token: Optional[Mapping[str, Any]] = None,
+    ) -> MutableMapping[str, Any]:
+        interpolated_value = self._parameter_interpolator.eval_request_inputs(stream_state, stream_slice, next_page_token)
+        if isinstance(interpolated_value, dict):
+            return interpolated_value
+        return {}
+
+    def get_request_headers(
+        self,
+        *,
+        stream_state: Optional[StreamState] = None,
+        stream_slice: Optional[StreamSlice] = None,
+        next_page_token: Optional[Mapping[str, Any]] = None,
+    ) -> Mapping[str, Any]:
+        return self._headers_interpolator.eval_request_inputs(stream_state, stream_slice, next_page_token)
+
+    def get_request_body_data(
+        self,
+        *,
+        stream_state: Optional[StreamState] = None,
+        stream_slice: Optional[StreamSlice] = None,
+        next_page_token: Optional[Mapping[str, Any]] = None,
+    ) -> Optional[Union[Mapping, str]]:
+        return self._body_data_interpolator.eval_request_inputs(stream_state, stream_slice, next_page_token)
+
+    def get_request_body_json(
+        self,
+        *,
+        stream_state: Optional[StreamState] = None,
+        stream_slice: Optional[StreamSlice] = None,
+        next_page_token: Optional[Mapping[str, Any]] = None,
+    ) -> Optional[Mapping]:
+        return self._body_json_interpolator.eval_request_inputs(stream_state, stream_slice, next_page_token)
+
+    def request_kwargs(
+        self,
+        *,
+        stream_state: Optional[StreamState] = None,
+        stream_slice: Optional[StreamSlice] = None,
+        next_page_token: Optional[Mapping[str, Any]] = None,
+    ) -> Mapping[str, Any]:
+        return {}
