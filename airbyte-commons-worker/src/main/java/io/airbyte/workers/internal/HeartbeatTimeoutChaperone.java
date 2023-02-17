08b594a2e844e4de0c4ba161820dd11d36d43882
benmoriceau <benoit@airbyte.io>
2023-02-17 14:44:08 -0800

    DONT REVIEW

--- airbyte-commons-worker/src/main/java/io/airbyte/workers/internal/HeartbeatTimeoutChaperone.java
+++ airbyte-commons-worker/src/main/java/io/airbyte/workers/internal/HeartbeatTimeoutChaperone.java
@@ -39,7 +39,7 @@ public class HeartbeatTimeoutChaperone {
 
   private static final Logger LOGGER = LoggerFactory.getLogger(HeartbeatTimeoutChaperone.class);
 
-  public static final Duration DEFAULT_TIMEOUT_CHECK_DURATION = Duration.ofMinutes(1);
+  public static final Duration DEFAULT_TIMEOUT_CHECK_DURATION = Duration.ofSeconds(1);
 
   private final HeartbeatMonitor heartbeatMonitor;
   private final Duration timeoutCheckDuration;
