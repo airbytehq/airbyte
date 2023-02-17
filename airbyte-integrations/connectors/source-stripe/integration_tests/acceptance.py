31d9c9b22131a0e1415e5e57b00c48ebc7cfc2ab
Joe Reuter <joe@airbyte.io>
2023-02-17 12:27:17 +0100

    negative test

--- airbyte-integrations/connectors/source-stripe/integration_tests/acceptance.py
+++ airbyte-integrations/connectors/source-stripe/integration_tests/acceptance.py
@@ -10,5 +10,6 @@ pytest_plugins = ("connector_acceptance_test.plugin",)
 
 @pytest.fixture(scope="session", autouse=True)
 def connector_setup():
+    raise RuntimeError("this should happen if everything works fine")
     """This fixture is a placeholder for external resources that acceptance test might require."""
     yield
