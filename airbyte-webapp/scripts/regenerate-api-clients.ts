e99efbd925a424a4ea81d3ee09c07502f4347b47
Tim Roes <tim@airbyte.io>
2023-02-17 13:14:37 +0100

    Add output if spec is up to date

--- airbyte-webapp/scripts/regenerate-api-clients.ts
+++ airbyte-webapp/scripts/regenerate-api-clients.ts
@@ -1,13 +1,17 @@
 import { statSync, existsSync } from "fs";
-import { resolve } from "path";
+import { resolve, basename } from "path";
+
+import chalk from "chalk";
 
 import conf from "../orval.config";
 
 const lastTouched = (filepath: string) => statSync(resolve(filepath)).mtime;
 
-for (const [, api] of Object.entries(conf)) {
+for (const api of Object.values(conf)) {
   if (!existsSync(api.output.target) || lastTouched(api.input) > lastTouched(api.output.target)) {
     // no need to read and parse the orval lib if all files are up to date
     require("orval").generate(api);
+  } else {
+    console.log(`🎉 ${chalk.green(basename(api.output.target))} is up-to-date`);
   }
 }
