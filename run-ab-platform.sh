94245dced7e01519b9fc2dfc61b4d1dc87a419df
Topher Lubaway <asimplechris@gmail.com>
2023-02-18 11:11:15 -0600

    Made flags cleaner and more cleanup
    i've never used this flag pattern, its much
    easier than my usual way

--- run-ab-platform.sh
+++ run-ab-platform.sh
@@ -17,15 +17,27 @@ PS4="$blue_text""${BASH_SOURCE}:${LINENO}: ""$default_text"
 Help()
 {
    # Display Help
-   echo "Add description of the script functions here."
+   echo "This Script will download the necessary files for running docker compose"
+   echo "It will also run docker compose up"
    echo
-   echo "Syntax: run-ab-platform.sh [-d|x|h]"
+   # $0 is the currently running program
+   echo "Syntax: $0"
    echo "options:"
-   echo "d     Only download files - don't run docker compose"
-   echo "h     Print this Help."
-   echo "x     Verbose mode."
+   echo "-d --download    Only download files - don't run docker compose"
+   echo "-r --refresh     ${red_text}DELETE${default_text} existing assets and re-download new ones"
+   echo "-h --help        Print this Help."
+   echo "-x --debug       Verbose mode."
    echo
 }
+
+########## Declare assets care about ##########
+docker_compose_yaml="docker-compose.yaml"
+            dot_env=".env"
+        dot_env_dev=".env.dev"
+              flags="flags.yml"
+# any string is an array to POSIX shell. Space seperates values
+all_files="$docker_compose_yaml $dot_env $dot_env_dev $flags"
+base_github_url="https://raw.githubusercontent.com/airbytehq/airbyte-platform/main/"
 
 ############################################################
 # Download                                                 #
@@ -33,22 +45,15 @@ Help()
 Download()
 {
   ########## Check if we already have the assets we are looking for ##########
-  docker_compose_yaml="docker-compose.yaml"
-              dot_env=".env"
-          dot_env_dev=".env.dev"
-                flags="flags.yml"
-  base_github_url="https://raw.githubusercontent.com/airbytehq/airbyte-platform/main/"
-  # url_to_docker_compose_yaml="${base_github_url}${docker_compose_yaml}"
-  # url_to_dot_env="${base_github_url}${dot_env}"
-  # url_to_dot_env_dev="${base_github_url}${dot_env_dev}"
-
-  for file in $docker_compose_yaml $dot_env $dot_env_dev $flags; do
-    if test $(find $file -type f -mtime +15 > /dev/null); then
-      echo "$red_text""Warning your $file may be stale!""$default_text"
-      echo "$red_text""rm $file to refresh!""$default_text"
-    fi
+  for file in $all_files; do
     if test -f $file; then
-      echo "$blue_text""found $file locally!""$default_text"
+      # Check if the assets are old.  A possibly sharp corner
+      if test $(find $file -type f -mtime +60 > /dev/null); then
+        echo "$red_text""Warning your $file may be stale!""$default_text"
+        echo "$red_text""rm $file to refresh!""$default_text"
+      else
+        echo "$blue_text""found $file locally!""$default_text"
+      fi
     else
       echo "$blue_text""Downloading $file""$default_text"
       curl --location\
@@ -60,6 +65,18 @@ Download()
   done
 }
 
+DeleteLocalAssets()
+{
+  for file in $all_files; do
+    echo "$blue_text""Attempting to delete $file!""$default_text"
+    if test -f $file; then
+      rm $file && echo "It's gone!"
+    else
+      echo "$file not found locally.  Nothing to delete."
+    fi
+  done
+}
+
 
 # $0 is the currently running program (this file)
 this_file_directory=$(dirname $0)
@@ -67,22 +84,30 @@ this_file_directory=$(dirname $0)
 cd ${this_file_directory}
 
 
-
-while getopts dhx flag
-do
-    case "${flag}" in
-        d) 
-          Download
-          exit
-          ;;
-        h)       
-          Help
-          exit
-          ;;
-        x) 
-          set -o xtrace  # -x display every line before execution; enables PS4
-          ;;
-    esac
+for argument in $@; do
+  case $argument in
+    -d | --download)
+      Download
+      exit
+      ;;
+    -r | --refresh)
+      DeleteLocalAssets
+      exit
+      ;;
+    -h | --help)
+      Help
+      exit
+      ;;
+    -x | --debug)
+      set -o xtrace  # -x display every line before execution; enables PS4
+      ;;
+    --)
+      echo "$argument is not a known command."
+      echo
+      Help
+      break;;
+  esac
+  shift
 done
 
 
@@ -125,12 +150,13 @@ echo "$blue_text""Starting Docker Compose""$default_text"
 
 docker-compose up
 
+# $? is the exit code of the last command. So here: docker-compose up
 if test $? -ne 0; then
   echo "$red_text""Docker compose failed.  If you are seeing container conflicts""$default_text"
   echo "$red_text""please consider removing old containers""$default_text"
 fi
 
 ########## Ending Docker ##########
-
 docker-compose down
 
+echo "$blue_text""Starting Docker Compose""$default_text"
