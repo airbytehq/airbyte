a42777c1f2289e8885409499ef81a22d8326928b
Volodymyr Petrov <volodymyr.s.petrov@globallogic.com>
2023-02-18 04:34:56 +0200

    extend connection configuration preview

--- airbyte-webapp/src/components/connection/ConnectionForm/ConnectionConfigurationFormPreview.tsx
+++ airbyte-webapp/src/components/connection/ConnectionForm/ConnectionConfigurationFormPreview.tsx
@@ -7,19 +7,26 @@ import { Text } from "components/ui/Text";
 import {
   ConnectionScheduleData,
   ConnectionScheduleType,
+  NamespaceDefinitionType,
   NonBreakingChangesPreference,
 } from "core/request/AirbyteClient";
 import { FeatureItem, useFeature } from "hooks/services/Feature";
 
 import styles from "./ConnectionConfigurationFormPreview.module.scss";
+import { FormikConnectionFormValues } from "./formConfig";
+import { namespaceDefinitionOptions } from "./types";
+import { StreamNameDefinitionValueType } from "../DestinationStreamNamesModal/DestinationStreamNamesModal";
 
 export const ConnectionConfigurationFormPreview: React.FC = () => {
-  const { getFieldMeta } = useFormikContext();
+  const { getFieldMeta } = useFormikContext<FormikConnectionFormValues>();
   const allowAutoDetectSchema = useFeature(FeatureItem.AllowAutoDetectSchema);
 
   const scheduleType = getFieldMeta<ConnectionScheduleType>("scheduleType").value;
   const scheduleData = getFieldMeta<ConnectionScheduleData>("scheduleData").value;
   const nonBreakingChangesPreference = getFieldMeta<NonBreakingChangesPreference>("nonBreakingChangesPreference").value;
+  const destNamespaceDef = getFieldMeta("namespaceDefinition").value as NamespaceDefinitionType;
+  const destNamespaceFormat = getFieldMeta("namespaceFormat").value;
+  const destPrefix = getFieldMeta("prefix").value;
 
   const frequency = (
     <div>
@@ -44,6 +51,38 @@ export const ConnectionConfigurationFormPreview: React.FC = () => {
     </div>
   );
 
+  const destinationNamespace = (
+    <div>
+      <Text size="xs" color="grey">
+        <FormattedMessage id="connectionForm.namespaceDefinition.title" />:
+      </Text>
+      <Text size="md" color="grey">
+        <FormattedMessage id={`connectionForm.${namespaceDefinitionOptions[destNamespaceDef]}`} />
+        {namespaceDefinitionOptions[destNamespaceDef] === namespaceDefinitionOptions.customformat && (
+          <>
+            {" - "}
+            {destNamespaceFormat}
+          </>
+        )}
+      </Text>
+    </div>
+  );
+
+  const destinationPrefix = (
+    <div>
+      <Text size="xs" color="grey">
+        <FormattedMessage id="form.prefix" />:
+      </Text>
+      <Text size="md" color="grey">
+        {destPrefix === StreamNameDefinitionValueType.Prefix || destPrefix === "" ? ( // is prefix is reserved word? we cant use it as a value
+          <FormattedMessage id="connectionForm.modal.destinationStreamNames.radioButton.mirror" />
+        ) : (
+          destPrefix
+        )}
+      </Text>
+    </div>
+  );
+
   const nonBreakingChanges = allowAutoDetectSchema && (
     <div>
       <Text size="xs" color="grey">
@@ -58,6 +97,8 @@ export const ConnectionConfigurationFormPreview: React.FC = () => {
   return (
     <FlexContainer className={styles.container}>
       {frequency}
+      {destinationNamespace}
+      {destinationPrefix}
       {nonBreakingChanges}
     </FlexContainer>
   );
