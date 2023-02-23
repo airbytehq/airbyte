8c53c955ea60bf84aaf4d7aa36c74601e3c5c036
erohmensing <erohmensing@gmail.com>
2023-02-23 16:50:14 -0600

    Pull out default methods for overriding

--- airbyte-integrations/connectors/source-breaker/source_breaker/source.py
+++ airbyte-integrations/connectors/source-breaker/source_breaker/source.py
@@ -2,9 +2,14 @@
 # Copyright (c) 2023 Airbyte, Inc., all rights reserved.
 #
 
+import json
+import logging
 from typing import Any, List, Mapping, Tuple
 
+import yaml
+from airbyte_cdk.connector import load_optional_package_file
 from airbyte_cdk.logger import AirbyteLogger
+from airbyte_cdk.models import AirbyteCatalog, AirbyteConnectionStatus, ConnectorSpecification, Status
 from airbyte_cdk.sources import AbstractSource
 from airbyte_cdk.sources.streams import Stream
 
@@ -12,11 +17,57 @@ from .streams import Products, Purchases, Users
 
 
 class SourceBreaker(AbstractSource):
+    def spec(self, logger: logging.Logger) -> ConnectorSpecification:
+        """
+        Returns the spec for this integration. The spec is a JSON-Schema object describing the required configurations (e.g: username and password)
+        required to run this integration. By default, this will be loaded from a "spec.yaml" or a "spec.json" in the package root.
+        """
+
+        package = self.__class__.__module__.split(".")[0]
+
+        yaml_spec = load_optional_package_file(package, "spec.yaml")
+        json_spec = load_optional_package_file(package, "spec.json")
+
+        if yaml_spec and json_spec:
+            raise RuntimeError("Found multiple spec files in the package. Only one of spec.yaml or spec.json should be provided.")
+
+        if yaml_spec:
+            spec_obj = yaml.load(yaml_spec, Loader=yaml.SafeLoader)
+        elif json_spec:
+            try:
+                spec_obj = json.loads(json_spec)
+            except json.JSONDecodeError as error:
+                raise ValueError(f"Could not read json spec file: {error}. Please ensure that it is a valid JSON.")
+        else:
+            raise FileNotFoundError("Unable to find spec.yaml or spec.json in the package.")
+
+        return ConnectorSpecification.parse_obj(spec_obj)
+
+    def check(self, logger: logging.Logger, config: Mapping[str, Any]) -> AirbyteConnectionStatus:
+        """Implements the Check Connection operation from the Airbyte Specification.
+        See https://docs.airbyte.com/understanding-airbyte/airbyte-protocol/#check.
+        """
+        try:
+            check_succeeded, error = self.check_connection(logger, config)
+            if not check_succeeded:
+                return AirbyteConnectionStatus(status=Status.FAILED, message=repr(error))
+        except Exception as e:
+            return AirbyteConnectionStatus(status=Status.FAILED, message=repr(e))
+
+        return AirbyteConnectionStatus(status=Status.SUCCEEDED)
+
     def check_connection(self, logger: AirbyteLogger, config: Mapping[str, Any]) -> Tuple[bool, Any]:
         if type(config["count"]) == int or type(config["count"]) == float:
             return True, None
         else:
             return False, "Count option is missing"
+
+    def discover(self, logger: logging.Logger, config: Mapping[str, Any]) -> AirbyteCatalog:
+        """Implements the Discover operation from the Airbyte Specification.
+        See https://docs.airbyte.com/understanding-airbyte/airbyte-protocol/#discover.
+        """
+        streams = [stream.as_airbyte_stream() for stream in self.streams(config=config)]
+        return AirbyteCatalog(streams=streams)
 
     def streams(self, config: Mapping[str, Any]) -> List[Stream]:
         count: int = config["count"] if "count" in config else 0
