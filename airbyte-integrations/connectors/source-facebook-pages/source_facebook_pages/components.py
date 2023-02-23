30b12571874bc911f345727093bcc088fe297c7d
Artem Inzhyyants <artem.inzhyyants@gmail.com>
2023-02-23 19:24:00 +0100

    Source Facebook Pages: Transform 'date-time' items to RFC3339 format

--- airbyte-integrations/connectors/source-facebook-pages/source_facebook_pages/components.py
+++ airbyte-integrations/connectors/source-facebook-pages/source_facebook_pages/components.py
@@ -2,16 +2,19 @@
 # Copyright (c) 2023 Airbyte, Inc., all rights reserved.
 #
 
-from dataclasses import dataclass
+from dataclasses import dataclass, InitVar
 from http import HTTPStatus
-from typing import Any, Mapping, Union
-
+from typing import Any, Mapping, Union, Optional, MutableMapping
+import dpath.util
+import pendulum
 import requests
 from requests import HTTPError
 
 from airbyte_cdk.sources.declarative.auth.declarative_authenticator import NoAuth
 from airbyte_cdk.sources.declarative.interpolation.interpolated_string import InterpolatedString
-from airbyte_cdk.sources.declarative.types import Config
+from airbyte_cdk.sources.declarative.types import Config, Record, StreamState, StreamSlice
+from airbyte_cdk.sources.declarative.transformations import RecordTransformation
+from airbyte_cdk.sources.declarative.schema import JsonFileSchemaLoader
 
 
 @dataclass
@@ -46,3 +49,50 @@ class AuthenticatorFacebookPageAccessToken(NoAuth):
             return r.json().get("access_token")
         except Exception as e:
             raise Exception(f"Error while generating page access token: {e}") from e
+
+
+@dataclass
+class CustomFieldTransformation(RecordTransformation):
+    config: Config
+    parameters: InitVar[Mapping[str, Any]]
+
+    def __post_init__(self, parameters: Mapping[str, Any]):
+        self.name = parameters.get("name")
+
+    def _get_schema_root_properties(self):
+        schema_loader = JsonFileSchemaLoader(config=self.config, parameters={"name": self.name})
+        schema = schema_loader.get_json_schema()
+        return schema["properties"]
+
+    def _get_date_time_dpath_from_schema(self):
+        """
+        Get all dpath in format 'a/b/*/c' from schema with format: 'date-time'
+        """
+        schema = self._get_schema_root_properties()
+        all_results = dpath.util.search(schema, '**', yielded=True, afilter=lambda x: True if "date-time" in str(x) else False)
+        full_dpath = [x[0] for x in all_results if isinstance(x[1], dict) and x[1].get('format') == 'date-time']
+        return [path.replace('/properties', '').replace('items', '*') for path in full_dpath]
+
+    def _date_time_to_rfc3339(self, record: MutableMapping[str, Any]) -> MutableMapping[str, Any]:
+        """
+        Transform 'date-time' items to RFC3339 format
+        """
+        date_time_paths = self._get_date_time_dpath_from_schema()
+        for path in date_time_paths:
+            if '*' not in path:
+                if field_value := dpath.util.get(record, path, default=None):
+                    dpath.util.set(record, path, pendulum.parse(field_value).to_rfc3339_string())
+            else:
+                if field_values := dpath.util.values(record, path):
+                    for i, date_time_value in enumerate(field_values):
+                        dpath.util.set(record, path.replace('*', str(i)), pendulum.parse(date_time_value).to_rfc3339_string())
+        return record
+
+    def transform(
+        self,
+        record: Record,
+        config: Optional[Config] = None,
+        stream_state: Optional[StreamState] = None,
+        stream_slice: Optional[StreamSlice] = None,
+    ) -> Record:
+        return self._date_time_to_rfc3339(record)
