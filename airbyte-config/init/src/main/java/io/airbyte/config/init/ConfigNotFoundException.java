82f6ad719758589b514cd16f9e037460bdc04d59
cgardens <charles@airbyte.io>
2023-02-23 22:36:58 -0800

    remove unneeded config import

--- airbyte-config/init/src/main/java/io/airbyte/config/init/ConfigNotFoundException.java
+++ airbyte-config/init/src/main/java/io/airbyte/config/init/ConfigNotFoundException.java
@@ -4,9 +4,6 @@
 
 package io.airbyte.config.init;
 
-import io.airbyte.config.AirbyteConfig;
-import java.util.UUID;
-
 public class ConfigNotFoundException extends Exception {
 
   private static final long serialVersionUID = 836273627;
@@ -17,14 +14,6 @@ public class ConfigNotFoundException extends Exception {
     super(String.format("config type: %s id: %s", type, configId));
     this.type = type;
     this.configId = configId;
-  }
-
-  public ConfigNotFoundException(final AirbyteConfig type, final String configId) {
-    this(type.toString(), configId);
-  }
-
-  public ConfigNotFoundException(final AirbyteConfig type, final UUID uuid) {
-    this(type.toString(), uuid.toString());
   }
 
   public String getType() {
