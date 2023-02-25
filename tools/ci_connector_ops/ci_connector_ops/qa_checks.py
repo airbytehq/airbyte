e59959dbb4665382749e5958362363daf3916b6b
evantahler <evan@airbyte.io>
2023-02-24 16:49:14 -0800

    endswith seems better

--- tools/ci_connector_ops/ci_connector_ops/qa_checks.py
+++ tools/ci_connector_ops/ci_connector_ops/qa_checks.py
@@ -198,7 +198,7 @@ def run_qa_checks():
     if not connector_technical_name.startswith("source-") and not connector_technical_name.startswith("destination-"):
         print("No QA check to run as this is not a connector.")
         sys.exit(0)
-    if connector_technical_name.__contains__("-strict-encrypt"):
+    if connector_technical_name.endswith("-strict-encrypt"):
         connector_technical_name = connector_technical_name.replace("-strict-encrypt", "")
         print("Checking connector " + connector_technical_name + " due to strict-encrypt")
     connector = Connector(connector_technical_name)
