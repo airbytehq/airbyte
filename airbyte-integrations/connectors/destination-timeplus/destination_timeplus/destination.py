812ee9794290a4f05e08aeec64d452b3956a782c
Jove Zhong <jove@timeplus.io>
2023-02-28 17:47:35 -0800

    format code, following the PEP-8

--- airbyte-integrations/connectors/destination-timeplus/destination_timeplus/destination.py
+++ airbyte-integrations/connectors/destination-timeplus/destination_timeplus/destination.py
@@ -12,6 +12,7 @@ from airbyte_cdk.models import AirbyteConnectionStatus, AirbyteMessage, Configur
 import json
 from timeplus import Stream, Environment
 
+
 class DestinationTimeplus(Destination):
     def write(
         self, config: Mapping[str, Any], configured_catalog: ConfiguredAirbyteCatalog, input_messages: Iterable[AirbyteMessage]
@@ -30,10 +31,10 @@ class DestinationTimeplus(Destination):
         :param input_messages: The stream of input messages received from the source
         :return: Iterable of AirbyteStateMessages wrapped in AirbyteMessage structs
         """
-        endpoint=config["endpoint"]
-        apikey=config["apikey"]
-        if endpoint[-1]=='/':
-            endpoint=endpoint[0:len(endpoint)-1]
+        endpoint = config["endpoint"]
+        apikey = config["apikey"]
+        if endpoint[-1] == '/':
+            endpoint = endpoint[0:len(endpoint)-1]
         env = Environment().address(endpoint).apikey(apikey)
         stream_list = Stream(env=env).list()
         all_streams = {s.name for s in stream_list}
@@ -47,11 +48,11 @@ class DestinationTimeplus(Destination):
             if is_overwrite:
                 if stream_exists:
                     # delete all data in the existing stream and recreate the stream.
-                    need_delete_stream=True
-                    need_create_stream=True
+                    need_delete_stream = True
+                    need_create_stream = True
                 else:
                     # only need to create the stream
-                    need_create_stream=True
+                    need_create_stream = True
             else:
                 if stream_exists:
                     # for append mode, just add more data to the existing stream. No need to do anything.
@@ -59,14 +60,16 @@ class DestinationTimeplus(Destination):
                 else:
                     # for append mode, create the stream and append data to it.
                     need_create_stream = True
-            
+
             if need_delete_stream:
                 # delete the existing stream
-                Stream(env=env).name(configured_stream.stream.name).get().delete()
-            if need_create_stream:    
+                Stream(env=env).name(
+                    configured_stream.stream.name).get().delete()
+            if need_create_stream:
                 # create a new stream
-                DestinationTimeplus.create_stream(env, configured_stream.stream)
-                
+                DestinationTimeplus.create_stream(
+                    env, configured_stream.stream)
+
         for message in input_messages:
             if message.type == Type.STATE:
                 # Emitting a state message indicates that all records which came before it have been written to the destination. So we flush
@@ -74,41 +77,48 @@ class DestinationTimeplus(Destination):
                 yield message
             elif message.type == Type.RECORD:
                 record = message.record
-                
+
                 # this code is to send data to a single-column stream
                 # Stream(env=env).name(record.stream).column("raw", "string").ingest(payload=record.data)
 
-                Stream(env=env).name(record.stream).ingest(payload=record.data, format="streaming")
+                Stream(env=env).name(record.stream).ingest(
+                    payload=record.data, format="streaming")
             else:
                 # ignore other message types for now
                 continue
 
     @staticmethod
-    def create_stream(env,stream: AirbyteStream):
+    def create_stream(env, stream: AirbyteStream):
         # singlel-column stream
         # Stream(env=env).name(stream.name).column('raw','string').create()
-        
-        tp_stream=Stream(env=env).name(stream.name.strip())
-        for name,v in stream.json_schema['properties'].items():
-            tp_stream.column(name.strip(),DestinationTimeplus.type_mapping(v))
+
+        tp_stream = Stream(env=env).name(stream.name.strip())
+        for name, v in stream.json_schema['properties'].items():
+            tp_stream.column(name.strip(), DestinationTimeplus.type_mapping(v))
         tp_stream.create()
 
     @staticmethod
     def type_mapping(v) -> str:
-        airbyte_type=v['type']
+        airbyte_type = v['type']
         if type(airbyte_type) is list:
             for t in list(airbyte_type):
-                if t!='null': 
-                    type_def={'type':t}
-                    if t=='array': type_def['items']=v['items']
+                if t != 'null':
+                    type_def = {'type': t}
+                    if t == 'array':
+                        type_def['items'] = v['items']
                     return DestinationTimeplus.type_mapping(type_def)
-        if airbyte_type=='number':return 'float'
-        elif airbyte_type=='integer':return 'integer'
-        elif airbyte_type=='boolean':return 'bool'
-        elif airbyte_type=='object':return 'string'
-        elif airbyte_type=='array':
+        if airbyte_type == 'number':
+            return 'float'
+        elif airbyte_type == 'integer':
+            return 'integer'
+        elif airbyte_type == 'boolean':
+            return 'bool'
+        elif airbyte_type == 'object':
+            return 'string'
+        elif airbyte_type == 'array':
             return f"array({DestinationTimeplus.type_mapping(v['items'])})"
-        else:return 'string'
+        else:
+            return 'string'
 
     def check(self, logger: AirbyteLogger, config: Mapping[str, Any]) -> AirbyteConnectionStatus:
         """
@@ -123,14 +133,14 @@ class DestinationTimeplus(Destination):
         :return: AirbyteConnectionStatus indicating a Success or Failure
         """
         try:
-            endpoint=config["endpoint"]
-            apikey=config["apikey"]
+            endpoint = config["endpoint"]
+            apikey = config["apikey"]
             if not endpoint.startswith("http"):
                 return AirbyteConnectionStatus(status=Status.FAILED, message="Endpoint must start with http or https")
             if len(apikey) != 60:
                 return AirbyteConnectionStatus(status=Status.FAILED, message="API Key must be 60 characters")
-            if endpoint[-1]=='/':
-                endpoint=endpoint[0:len(endpoint)-1]
+            if endpoint[-1] == '/':
+                endpoint = endpoint[0:len(endpoint)-1]
             env = Environment().address(endpoint).apikey(apikey)
             Stream(env=env).list()
             logger.info("Successfully connected to "+endpoint)
