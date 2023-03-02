f531f768a97ddd0fd599f0933880a7d0cb3312f7
Alexandre Girard <alexandre@airbyte.io>
2023-03-01 18:23:39 -0800

    Revert "Merge branch 'master' into alex/support_3.8"
    This reverts commit 5c7581518ef74285a64670a5c38b28c5a8f20f5b, reversing
    changes made to 5cb47f8c70f2818a260fbe7a7f695610b6e4673d.

--- airbyte-cdk/python/CHANGELOG.md
+++ airbyte-cdk/python/CHANGELOG.md
@@ -1,8 +1,5 @@
 # Changelog
 
-## 0.29.2
-minor fixes to support python3.8
-
 ## 0.29.1
 Publishing Docker image for source-declarative-manifest
 
--- airbyte-cdk/python/setup.py
+++ airbyte-cdk/python/setup.py
@@ -15,7 +15,7 @@ README = (HERE / "README.md").read_text()
 
 setup(
     name="airbyte-cdk",
-    version="0.29.2",
+    version="0.29.1",
     description="A framework for writing Airbyte Connectors.",
     long_description=README,
     long_description_content_type="text/markdown",
