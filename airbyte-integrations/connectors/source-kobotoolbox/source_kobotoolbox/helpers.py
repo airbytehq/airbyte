f7bb4a1eae02176724e7edfb638df2407ab8b42b
siddhant <ssiddhant3030@gmail.com>
2023-03-03 16:14:28 +0530

    refactor

--- airbyte-integrations/connectors/source-kobotoolbox/source_kobotoolbox/helpers.py
+++ airbyte-integrations/connectors/source-kobotoolbox/source_kobotoolbox/helpers.py
@@ -2,15 +2,6 @@
 # Copyright (c) 2023 Airbyte, Inc., all rights reserved.
 #
 
-import base64
-from datetime import datetime
-
-import requests
-from bigquery_schema_generator.generate_schema import SchemaGenerator
-from gbqschema_converter.gbqschema_to_jsonschema import json_representation as converter
-from requests.adapters import HTTPAdapter
-from requests.packages.urllib3.util.retry import Retry
-
 
 class Helpers(object):
     @staticmethod
