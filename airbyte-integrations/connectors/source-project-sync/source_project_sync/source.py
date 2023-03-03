d1e43bf73e0aef11f9306eaa3bb5631d3dd7b081
Ali Hassan <alihassan01264@gmail.com>
2023-03-03 11:33:09 +0500

    minor change

--- airbyte-integrations/connectors/source-project-sync/source_project_sync/source.py
+++ airbyte-integrations/connectors/source-project-sync/source_project_sync/source.py
@@ -1,6 +1,7 @@
 from typing import Any, List, Mapping, Tuple
 
 import requests
+from http import HTTPStatus
 from airbyte_cdk.sources import AbstractSource
 from airbyte_cdk.sources.streams import Stream
 from airbyte_cdk.sources.streams.http.auth import TokenAuthenticator, NoAuth
@@ -11,7 +12,7 @@ from .streams import *
 class SourceProjectSync(AbstractSource):
     def check_connection(self, logger, config) -> Tuple[bool, any]:
         response = requests.get('https://www.xledger.net/')
-        if response.status_code == 200:
+        if response.status_code == HTTPStatus.OK:
             return True, None
         else:
             return False, 'Failed to connect to https://www.xledger.net/.'
