141b78c2e78c7ccb1ca16f70354c243d472c77d5
Julien Fouilhé <julien.fouilhe@gmail.com>
2023-03-03 07:32:50 +0100

    Add feature to discover cursor fields

--- airbyte-integrations/connectors/source-firestore/source_firestore/source.py
+++ airbyte-integrations/connectors/source-firestore/source_firestore/source.py
@@ -44,7 +44,7 @@ class Helpers(object):
 # Basic full refresh stream
 class FirestoreStream(HttpStream, ABC):
     _cursor_value: Optional[datetime]
-    cursor_field: Union[str, List[str]] = "updatedAt"
+    cursor_field: Union[str, List[str], None] = None
     @property
     def cursor_key(self):
         if isinstance(self.cursor_field, list):
@@ -54,10 +54,10 @@ class FirestoreStream(HttpStream, ABC):
         return self.cursor_field
 
 
-    url_base = Helpers.url_base
-    _primary_key = "name"
-    page_size = 100
-    http_method = "POST"
+    url_base: str = Helpers.url_base
+    _primary_key: str = "name"
+    page_size: int = 100
+    http_method: str = "POST"
     collection_name: str
 
     @property
@@ -89,20 +89,21 @@ class FirestoreStream(HttpStream, ABC):
         return { "timestampValue": documents[len(documents) - 1][self.cursor_key] }
 
     def request_params(
-        self, stream_state: Mapping[str, Any], stream_slice: Mapping[str, any] = None, next_page_token: Mapping[str, Any] = None
+        self, stream_state: Mapping[str, Any], stream_slice: Mapping[str, Any] = {}, next_page_token: Mapping[str, Any] = {}
     ) -> MutableMapping[str, Any]:
         return {}
 
     def request_body_json(
         self,
         stream_state: Mapping[str, Any],
-        stream_slice: Mapping[str, Any] = None,
-        next_page_token: Mapping[str, Any] = None,
+        stream_slice: Mapping[str, Any] = {},
+        next_page_token: Mapping[str, Any] = {},
     ) -> Optional[Mapping]:
-        timestamp_state: Optional[datetime] = stream_state.get(self.cursor_key)
+        page_size = stream_state.get("page_size", self.page_size)
+        timestamp_state: Optional[datetime] = stream_state.get(self.cursor_key) if self.cursor_key else None
         timestamp_value = Helpers.parse_date(timestamp_state).isoformat() if timestamp_state else None
 
-        self.logger.info(f"Requesting body JSON with cursor {self.cursor_key} value {next_page_token}")
+        self.logger.info(f"Requesting body JSON with cursor {self.cursor_key} value {next_page_token} and page_size {page_size}")
 
         return {
             "structuredQuery": {
@@ -116,7 +117,7 @@ class FirestoreStream(HttpStream, ABC):
                         },
                     }
                 } if timestamp_value else None,
-                "limit": self.page_size,
+                "limit": page_size,
                 "orderBy": [{"field": {"fieldPath": self.cursor_key}, "direction": "ASCENDING"}] if self.cursor_key else None,
                 "startAt": {"values": [next_page_token], "before": False} if next_page_token else None,
             }
@@ -157,20 +158,34 @@ class FirestoreStream(HttpStream, ABC):
 class IncrementalFirestoreStream(FirestoreStream, IncrementalMixin):
     start_date: Optional[datetime]
 
-    def __init__(self, authenticator: TokenAuthenticator, collection_name: str):
+    def __init__(self, authenticator: TokenAuthenticator, collection_name: str, cursor_field_possibilities: List[str] = []):
         super().__init__(authenticator=authenticator, collection_name=collection_name)
+        self.cursor_field = self.guess_cursor_field(cursor_field_possibilities)
+        print(self.cursor_field)
+
+    def guess_cursor_field(self, cursor_field_possibilities: List[str]) -> Union[str, None]:
+        records = list(self.read_records(sync_mode=SyncMode.full_refresh, stream_state={"page_size": 1}))
+        if len(records) == 0:
+            return None
+        # try to guess default field from first record
+        first_record = records[0]["json_data"]
+
+        for cursor_field in cursor_field_possibilities:
+            if cursor_field in first_record:
+                return cursor_field
+        return "name"
 
     @property
-    def state(self) -> MutableMapping[str, Any]:
-        return { self.cursor_key: self._cursor_value.isoformat() } if self._cursor_value else {}
+    def state(self) -> dict[str, Any]:
+        return { self.cursor_key: self._cursor_value.isoformat() } if self._cursor_value and self.cursor_key else {}
 
     @state.setter
     def state(self, value: MutableMapping[str, Any]):
-        new_cursor_value = value.get(self.cursor_key, self.start_date)
+        new_cursor_value = value.get(self.cursor_key, self.start_date) if self.cursor_key else None
         self._cursor_value = Helpers.parse_date(new_cursor_value) if isinstance(new_cursor_value, str) else new_cursor_value
 
     def request_params(
-        self, stream_state: Mapping[str, Any], stream_slice: Mapping[str, any] = None, next_page_token: Mapping[str, Any] = None
+        self, stream_state: Mapping[str, Any], stream_slice: Mapping[str, Any] = {}, next_page_token: Mapping[str, Any] = {}
     ) -> MutableMapping[str, Any]:
         params = super().request_params(stream_state=stream_state, stream_slice=stream_slice, next_page_token=next_page_token)
         return params
@@ -178,9 +193,9 @@ class IncrementalFirestoreStream(FirestoreStream, IncrementalMixin):
     def read_records(
         self,
         sync_mode: SyncMode,
-        cursor_field: Union[str, List[str]] = None,
-        stream_slice: Mapping[str, Any] = None,
-        stream_state: Mapping[str, Any] = None,
+        cursor_field: List[str] = [],
+        stream_slice: Mapping[str, Any] = {},
+        stream_state: Mapping[str, Any] = {},
     ) -> Iterable[Mapping[str, Any]]:
         self.logger.info(f"Stream {self.name}: Reading in {sync_mode} (cursor field {cursor_field}). Current cursor value: {self._cursor_value}")
         self.cursor_field = cursor_field
@@ -189,20 +204,21 @@ class IncrementalFirestoreStream(FirestoreStream, IncrementalMixin):
         ):
             yield record
             record_date = Helpers.parse_date(record[self.cursor_key]) if self.cursor_key else None
-            self._cursor_value = max(record_date, self._cursor_value) if self._cursor_value else record_date
+            if record_date:
+                self._cursor_value = max(record_date, self._cursor_value) if self._cursor_value else record_date
 
 class Collection(IncrementalFirestoreStream):
     project_id: str
     collection_name: str
 
     def __init__(self, authenticator: TokenAuthenticator, collection_name: str, config: Mapping[str, Any]):
-        super().__init__(authenticator, collection_name=collection_name)
-        self.collection_name = collection_name
         self.project_id = config["project_id"]
+        self.collection_name = collection_name
+        super().__init__(authenticator, collection_name=collection_name, cursor_field_possibilities=config["cursor_field_possibilities"])
         self.start_date = Helpers.parse_date(config["start_date"]) if "start_date" in config else None
 
     def path(
-        self, stream_state: Mapping[str, Any] = None, stream_slice: Mapping[str, Any] = None, next_page_token: Mapping[str, Any] = None
+        self, stream_state: Mapping[str, Any] = {}, stream_slice: Mapping[str, Any] = {}, next_page_token: Mapping[str, Any] = {}
     ) -> str:
         return Helpers.get_collection_path(self.project_id, self.collection_name)
 
@@ -213,9 +229,10 @@ class SourceFirestore(AbstractSource):
         scopes = ['https://www.googleapis.com/auth/datastore']
         credentials = service_account.Credentials.from_service_account_info(json.loads(config["google_application_credentials"]), scopes=scopes)        
         credentials.refresh(google.auth.transport.requests.Request())
-        return TokenAuthenticator(token=credentials.token)
+        token: Any = credentials.token
+        return TokenAuthenticator(token=token)
 
-    def check_connection(self, logger, config: Mapping[str, Any]) -> Tuple[bool, any]:
+    def check_connection(self, logger, config: Mapping[str, Any]) -> Tuple[bool, Any]:
         auth = self.get_auth(config=config)
         project_id = config["project_id"]
         url = Helpers.get_collections_list_url(project_id)
@@ -233,7 +250,7 @@ class SourceFirestore(AbstractSource):
         json = response.json()
         return json["collectionIds"]
 
-    def streams(self, config: Mapping[str, Any]) -> List[Stream]:
+    def streams(self, config: Mapping[str, Any]):
         auth = self.get_auth(config=config)
         project_id = config["project_id"]
         collections = self.discover_collections(project_id, auth)
