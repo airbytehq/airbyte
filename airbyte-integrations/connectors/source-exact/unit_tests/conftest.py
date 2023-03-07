56de77ced5c3c76c670a6f42843ce93083cdcdd8
nicklubbers <nicklubbers@outlook.com>
2023-03-07 17:22:11 +0100

    fix(exact): test config_oauth: multiple divisions

--- airbyte-integrations/connectors/source-exact/unit_tests/conftest.py
+++ airbyte-integrations/connectors/source-exact/unit_tests/conftest.py
@@ -10,12 +10,13 @@ replication_start_date = "2017-01-25T00:00:00Z"
 @pytest.fixture(name="config_oauth")
 def config_oauth():
     return {
-        "division": 123,
+        "divisions": [123],
         "credentials": {
             "client_id": "client_id",
             "client_secret": "client_secret",
             "access_token": "access_token",
             "refresh_token": "refresh_token",
+            "token_expiry_date": "2345-03-07T14:14:49.775368+00:00",
         },
     }
 
