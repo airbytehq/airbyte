0f1905fdc6fef8d534e5031f1a066cc38c0a819d
Artem Inzhyyants <artem.inzhyyants@gmail.com>
2023-03-09 17:59:38 +0100

    Source Google Analytics: fix unit tests

--- airbyte-integrations/connectors/source-google-analytics-data-api/unit_tests/test_streams.py
+++ airbyte-integrations/connectors/source-google-analytics-data-api/unit_tests/test_streams.py
@@ -278,7 +278,7 @@ def test_backoff_time(patch_base_class):
 
 @freeze_time("2023-01-01 00:00:00")
 def test_stream_slices():
-    config = {"date_ranges_start_date": datetime.date(2022, 12, 29), "window_in_days": 1}
+    config = {"date_ranges_start_date": datetime.date(2022, 12, 29), "window_in_days": 1, "dimensions": ["date"]}
     stream = GoogleAnalyticsDataApiBaseStream(authenticator=None, config=config)
     slices = list(stream.stream_slices(sync_mode=None))
     assert slices == [
@@ -288,7 +288,7 @@ def test_stream_slices():
         {"startDate": "2023-01-01", "endDate": "2023-01-01"},
     ]
 
-    config = {"date_ranges_start_date": datetime.date(2022, 12, 28), "window_in_days": 2}
+    config = {"date_ranges_start_date": datetime.date(2022, 12, 28), "window_in_days": 2, "dimensions": ["date"]}
     stream = GoogleAnalyticsDataApiBaseStream(authenticator=None, config=config)
     slices = list(stream.stream_slices(sync_mode=None))
     assert slices == [
@@ -297,7 +297,7 @@ def test_stream_slices():
         {"startDate": "2023-01-01", "endDate": "2023-01-01"},
     ]
 
-    config = {"date_ranges_start_date": datetime.date(2022, 12, 20), "window_in_days": 5}
+    config = {"date_ranges_start_date": datetime.date(2022, 12, 20), "window_in_days": 5, "dimensions": ["date"]}
     stream = GoogleAnalyticsDataApiBaseStream(authenticator=None, config=config)
     slices = list(stream.stream_slices(sync_mode=None))
     assert slices == [
