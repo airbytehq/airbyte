bd71e91d2e373f740b88882d855be3339f83c273
Alexandre Girard <alexandre@airbyte.io>
2023-03-09 14:36:38 -0800

    read records

--- airbyte-cdk/python/source_declarative_manifest/main.py
+++ airbyte-cdk/python/source_declarative_manifest/main.py
@@ -12,6 +12,8 @@ from airbyte_cdk.entrypoint import AirbyteEntrypoint, launch
 from airbyte_cdk.sources.declarative.declarative_source import DeclarativeSource
 from airbyte_cdk.sources.declarative.manifest_declarative_source import ManifestDeclarativeSource
 from connector_builder import connector_builder_handler
+from airbyte_protocol.models.airbyte_protocol import ConfiguredAirbyteCatalog, ConfiguredAirbyteStream, SyncMode, DestinationSyncMode
+import logging
 
 
 def create_source(config: Mapping[str, Any]) -> DeclarativeSource:
@@ -34,19 +36,46 @@ def get_config_from_args(args: List[str]) -> Mapping[str, Any]:
     return config
 
 
-def preparse(args: List[str]) -> Tuple[str, str]:
+def preparse(args: List[str]) -> Tuple[str, str, str]:
     parser = argparse.ArgumentParser()
     parser.add_argument("command", type=str, help="Airbyte Protocol command")
     parser.add_argument("--config", type=str, required=True, help="path to the json configuration file")
     parsed, _ = parser.parse_known_args(args)
     return parsed.command, parsed.config
 
+def create_configure_catalog(stream_name: str) -> ConfiguredAirbyteCatalog:
+    return ConfiguredAirbyteCatalog.parse_obj(
+        {
+            "streams": [
+                {
+                    "stream": {
+                        "name": stream_name,
+                        "json_schema": {},
+                        "supported_sync_modes": ["full_refresh", "incremental"],
+                    },
+                    "sync_mode": "full_refresh",
+                    "destination_sync_mode": "overwrite",
+                }
+            ]
+        }
+    )
 
-def handle_connector_builder_request(source: DeclarativeSource, config: Mapping[str, Any]):
+def execute_command(source: DeclarativeSource, config: Mapping[str, Any]):
     command = config.get("__command")
+    command_config = config.get("__command_config")
     if command == "resolve_manifest":
         return connector_builder_handler.resolve_manifest(source)
+    elif command == "read":
+        stream_name = command_config["stream_name"]
+        configured_catalog = create_configure_catalog(stream_name)
+        logger = logging.getLogger(f"airbyte.{source.name}")
+        return source.read(logger, config, configured_catalog, None)
     raise ValueError(f"Unrecognized command {command}.")
+
+def handle_connector_builder_request(source: DeclarativeSource, config: Mapping[str, Any]):
+    messages = execute_command(source, config)
+    for message in messages:
+        print(message.json(exclude_unset=True))
 
 
 def handle_connector_request(source: DeclarativeSource, args: List[str]):
