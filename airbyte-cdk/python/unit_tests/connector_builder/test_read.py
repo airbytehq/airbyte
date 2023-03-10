c8631f198e66b5444c3fba1b54da08d8268f0174
Alexandre Girard <alexandre@airbyte.io>
2023-03-09 16:47:05 -0800

    test

--- airbyte-cdk/python/unit_tests/connector_builder/test_read.py
+++ airbyte-cdk/python/unit_tests/connector_builder/test_read.py
@@ -470,6 +470,20 @@ def test_read_stream_given_maximum_number_of_slices_then_test_read_limit_reached
 
     assert stream_read.test_read_limit_reached
 
+def test_read_stream_given_maximum_number_of_pages_then_test_read_limit_reached():
+    maximum_number_of_pages_per_slice = 5
+    request = {}
+    response = {"status_code": 200}
+    mock_source = make_mock_source(
+        iter([slice_message()] + [request_log_message(request), response_log_message(response)] * maximum_number_of_pages_per_slice)
+    )
+
+    api = ConnectorBuilderHandler(MAX_PAGES_PER_SLICE, MAX_SLICES)
+
+    stream_read: StreamRead = api.read_stream(source=mock_source, config=CONFIG, stream="hashiras")
+
+    assert stream_read.test_read_limit_reached
+
 def make_mock_source(return_value: Iterator) -> MagicMock:
     mock_source = MagicMock()
     mock_source.read.return_value = return_value
