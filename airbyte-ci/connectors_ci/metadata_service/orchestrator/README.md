56ac7a8cbed0bd17567dc7e4f10cf2a86d05069c
Ben Church <ben@airbyte.io>
2023-03-10 16:47:25 -0800

    Add multiple dataframes

--- airbyte-ci/connectors_ci/metadata_service/orchestrator/README.md
+++ airbyte-ci/connectors_ci/metadata_service/orchestrator/README.md
@@ -24,3 +24,6 @@ https://github.com/slopp/dagster-dynamic-partitions/blob/cf312590bb3a2d95caee670
 https://github.com/mitodl/ol-data-platform/blob/59b785ab6bd5d73a05d598c9b39bfc8fa4eec65c/src/ol_orchestrate/repositories/edx_gcs_courses.py
 https://github.com/dagster-io/dagster/tree/c607767076da21de66ac364f1501c4ed49c20b49/examples/project_fully_featured
 https://github.com/DataBiosphere/hca-ingest/blob/61601a7830e224ac5f30c13bf0d605768f97d418/orchestration/hca_orchestration/repositories/common.py
+
+good way to restructure this
+https://www.youtube.com/watch?v=ZmUjf3gL1VU
--- airbyte-ci/connectors_ci/metadata_service/orchestrator/jobs/catalog_to_metadata_backfill.py
+++ airbyte-ci/connectors_ci/metadata_service/orchestrator/jobs/catalog_to_metadata_backfill.py
@@ -17,38 +17,69 @@ logger = get_dagster_logger()
 BUCKET_NAME = "ben-ab-test-bucket"
 CATALOG_FOLDER = "catalogs"
 
-@op
-def succeeds():
-    return 1
+# ------ Assets ------ #
 
-@asset(required_resource_keys={"latest_oss_catalog"})
-def oss_destinations_dataframe(context):
-    # todo move the string to its own resource
-    oss_catalog_file = context.resources.latest_oss_catalog
-    json_string = oss_catalog_file.download_as_string().decode('utf-8')
-    oss_catalog_dict = json.loads(json_string)
-    destinations = oss_catalog_dict["destinations"]
+
+
+@asset
+def cloud_sources_dataframe(latest_cloud_catalog_dict):
+    sources = latest_cloud_catalog_dict["sources"]
+    return pd.DataFrame(sources)
+
+@asset
+def oss_sources_dataframe(latest_oss_catalog_dict):
+    sources = latest_oss_catalog_dict["sources"]
+    return pd.DataFrame(sources)
+
+@asset
+def cloud_destinations_dataframe(latest_cloud_catalog_dict):
+    destinations = latest_cloud_catalog_dict["destinations"]
     return pd.DataFrame(destinations)
 
-# @job
-# def generate_catalog_markdown():
-#     logger.info("generate_catalog_markdown")
+@asset
+def oss_destinations_dataframe(latest_oss_catalog_dict):
+    destinations = latest_oss_catalog_dict["destinations"]
+    return pd.DataFrame(destinations)
 
-#     oss_df = oss_catalog_dataframe()
-#     logger.info(f"oss_catalog_dataframe: {dir(oss_df)}")
+@asset(required_resource_keys={"latest_cloud_catalog_gcs_file"})
+def latest_cloud_catalog_dict(context):
+    oss_catalog_file = context.resources.latest_cloud_catalog_gcs_gcs_file
+    json_string = oss_catalog_file.download_as_string().decode('utf-8')
+    oss_catalog_dict = json.loads(json_string)
+    return oss_catalog_dict
 
-#         # recorded metadata can be customized
-#     metadata = {
-#         # "num_records": len(oss_df),
-#         "preview": oss_df,
-#     }
-#     return Output(value=oss_df, metadata=metadata)
+# TODO add partitions
+@asset(required_resource_keys={"latest_oss_catalog_gcs_file"})
+def latest_oss_catalog_dict(context):
+    oss_catalog_file = context.resources.latest_oss_catalog_gcs_file
+    json_string = oss_catalog_file.download_as_string().decode('utf-8')
+    oss_catalog_dict = json.loads(json_string)
+    return oss_catalog_dict
 
-generate_catalog_markdown = define_asset_job(name="generate_catalog_markdown", selection="oss_destinations_dataframe")
 
-# TODO add types
-# TODO move this to config so its available in resource_context
+# ------ Resources ------ #
 
+@resource(required_resource_keys={"gcp_gcs_metadata_bucket"})
+def latest_oss_catalog_gcs_file(resource_context: InitResourceContext):
+    resource_context.log.info("retrieving latest_oss_catalog_gcs_file")
+    bucket = resource_context.resources.gcp_gcs_metadata_bucket
+    oss_catalog_path = f"{CATALOG_FOLDER}/oss_catalog.json"
+    oss_catalog_file = bucket.get_blob(oss_catalog_path)
+    if not oss_catalog_file.exists():
+        raise Exception(f"OSS catalog file does not exist in GCS bucket: {BUCKET_NAME} at path: {oss_catalog_path}")
+
+    return oss_catalog_file;
+
+@resource(required_resource_keys={"gcp_gcs_metadata_bucket"})
+def latest_cloud_catalog_gcs_file(resource_context: InitResourceContext):
+    resource_context.log.info("retrieving latest_cloud_catalog_gcs_file")
+    bucket = resource_context.resources.gcp_gcs_metadata_bucket
+    cloud_catalog_path = f"{CATALOG_FOLDER}/cloud_catalog.json"
+    cloud_catalog_file = bucket.get_blob(cloud_catalog_path);
+    if not cloud_catalog_file.exists():
+        raise Exception(f"Cloud catalog file does not exist in GCS bucket: {BUCKET_NAME} at path: {cloud_catalog_path}")
+
+    return cloud_catalog_file;
 
 
 @resource()
@@ -89,27 +120,12 @@ def gcp_gcs_metadata_bucket(resource_context: InitResourceContext):
     return storage_client.get_bucket(BUCKET_NAME)
 
 
-@resource(required_resource_keys={"gcp_gcs_metadata_bucket"})
-def latest_oss_catalog(resource_context: InitResourceContext):
-    resource_context.log.info("retrieving latest_oss_catalog")
-    bucket = resource_context.resources.gcp_gcs_metadata_bucket
-    oss_catalog_path = f"{CATALOG_FOLDER}/oss_catalog.json"
-    oss_catalog_file = bucket.get_blob(oss_catalog_path)
-    if not oss_catalog_file.exists():
-        raise Exception(f"OSS catalog file does not exist in GCS bucket: {BUCKET_NAME} at path: {oss_catalog_path}")
+# ------ Jobs ------ #
 
-    return oss_catalog_file;
+# todo kick off the final asset creation
+generate_catalog_markdown = define_asset_job(name="generate_catalog_markdown", selection="oss_destinations_dataframe")
 
-@resource(required_resource_keys={"gcp_gcs_metadata_bucket"})
-def latest_cloud_catalog(resource_context: InitResourceContext):
-    resource_context.log.info("retrieving latest_cloud_catalog")
-    bucket = resource_context.resources.gcp_gcs_metadata_bucket
-    cloud_catalog_path = f"{CATALOG_FOLDER}/cloud_catalog.json"
-    cloud_catalog_file = bucket.get_blob(cloud_catalog_path);
-    if not cloud_catalog_file.exists():
-        raise Exception(f"Cloud catalog file does not exist in GCS bucket: {BUCKET_NAME} at path: {cloud_catalog_path}")
-
-    return cloud_catalog_file;
+# ------ Sensors ------ #
 
 def generate_composite_etag_cursor(etags: List[str]):
     return ":".join(etags)
@@ -118,7 +134,7 @@ def generate_composite_etag_cursor(etags: List[str]):
     name="gcs_catalog_updated_sensor",
     job=generate_catalog_markdown,
     minimum_interval_seconds=30, # Todo have a dev and prod version of this
-    default_status=DefaultSensorStatus.RUNNING,
+    default_status=DefaultSensorStatus.STOPPED,
     )
 def gcs_catalog_updated_sensor(context: SensorEvaluationContext):
     # TODO parse which catalog(s) we're watching
@@ -128,8 +144,8 @@ def gcs_catalog_updated_sensor(context: SensorEvaluationContext):
         "gcp_gcs_client": gcp_gcs_client,
         "gcp_gsm_credentials": gcp_gsm_credentials,
         "gcp_gcs_metadata_bucket": gcp_gcs_metadata_bucket,
-        "latest_oss_catalog": latest_oss_catalog,
-        "latest_cloud_catalog": latest_cloud_catalog
+        "latest_oss_catalog_gcs_file": latest_oss_catalog_gcs_file,
+        "latest_cloud_catalog_gcs_file": latest_cloud_catalog_gcs_file
     }
     ) as resources:
         context.log.info("Got resources for gcs_catalog_updated_sensor")
@@ -137,7 +153,7 @@ def gcs_catalog_updated_sensor(context: SensorEvaluationContext):
         etag_cursor = context.cursor or None
         context.log.info(f"Old etag cursor: {etag_cursor}")
 
-        new_etag_cursor = generate_composite_etag_cursor([resources.latest_oss_catalog.etag, resources.latest_cloud_catalog.etag])
+        new_etag_cursor = generate_composite_etag_cursor([resources.latest_oss_catalog_gcs_file.etag, resources.latest_cloud_catalog_gcs_file.etag])
         context.log.info(f"New etag cursor: {new_etag_cursor}")
 
         # Note: ETAGs are GCS's way of providing a version number for a file
@@ -150,82 +166,27 @@ def gcs_catalog_updated_sensor(context: SensorEvaluationContext):
         context.log.info("New catalogs in GCS bucket")
         return RunRequest(run_key="updated_catalogs")
 
+# ------ Definitions ------ #
+
 # TODO move to a generic location
+# todo turn into a repository representation
 defn = Definitions(
-    assets=[oss_destinations_dataframe],
+    assets=[
+        oss_destinations_dataframe,
+        cloud_destinations_dataframe,
+        oss_sources_dataframe,
+        cloud_sources_dataframe,
+        latest_oss_catalog_dict,
+        latest_cloud_catalog_dict,
+    ],
     jobs=[generate_catalog_markdown],
     resources={
         "gcp_gsm_credentials": gcp_gsm_credentials,
         "gcp_gcs_client": gcp_gcs_client,
         "gcp_gcs_metadata_bucket": gcp_gcs_metadata_bucket,
-        "latest_oss_catalog": latest_oss_catalog,
-        "latest_cloud_catalog": latest_cloud_catalog
+        "latest_oss_catalog_gcs_file": latest_oss_catalog_gcs_file,
+        "latest_cloud_catalog_gcs_file": latest_cloud_catalog_gcs_file
     },
     schedules=[],
     sensors=[gcs_catalog_updated_sensor], # todo allow us to watch both the cloud and oss catalog
 )
-
-# from: https://github.com/mitodl/ol-data-platform/blob/59b785ab6bd5d73a05d598c9b39bfc8fa4eec65c/src/ol_orchestrate/sensors/sync_gcs_to_s3.py#L7
-# def check_new_gcs_assets_sensor(context: SensorEvaluationContext):
-#     gcs_config = load_yaml_config("/etc/dagster/edxorg_gcp.yaml")
-#     with build_resources(
-#         resources={"gcp_gcs": gcp_gcs_resource}, resource_config=gcs_config["resources"]
-#     ) as resources:
-#         storage_client = resources.gcp_gcs
-#         bucket = storage_client.get_bucket("simeon-mitx-course-tarballs")
-#         new_files = storage_client.list_blobs(bucket)
-#         if new_files:
-#             context.update_cursor(str(new_files))
-#             yield RunRequest(
-#                 run_key="new_gcs_file",
-#                 run_config=gcs_config,
-#             )
-#         else:
-#             yield SkipReason("No new files in GCS bucket")
-
-
-# edx_gcs_courses = Definitions(
-#     sensors=[
-#         SensorDefinition(
-#             evaluation_fn=check_new_gcs_assets_sensor,
-#             minimum_interval_seconds=86400,
-#             job=gcs_sync_job,
-#             default_status=DefaultSensorStatus.RUNNING,
-#         )
-#     ],
-#     jobs=[gcs_sync_job],
-# )
-
-
-# @asset
-# def hackernews_top_story_ids():
-#     """
-#     Get top stories from the HackerNews top stories endpoint.
-#     API Docs: https://github.com/HackerNews/API#new-top-and-best-stories
-#     """
-#     top_story_ids = requests.get(
-#         "https://hacker-news.firebaseio.com/v0/topstories.json"
-#     ).json()
-#     return top_story_ids[:10]
-
-
-# # asset dependencies can be inferred from parameter names
-# @asset
-# def hackernews_top_stories(hackernews_top_story_ids):
-#     """Get items based on story ids from the HackerNews items endpoint"""
-#     results = []
-#     for item_id in hackernews_top_story_ids:
-#         item = requests.get(
-#             f"https://hacker-news.firebaseio.com/v0/item/{item_id}.json"
-#         ).json()
-#         results.append(item)
-
-#     df = pd.DataFrame(results)
-
-#     # recorded metadata can be customized
-#     metadata = {
-#         "num_records": len(df),
-#         "preview": MetadataValue.md(df[["title", "by", "url"]].to_markdown()),
-#     }
-
-#     return Output(value=df, metadata=metadata)
