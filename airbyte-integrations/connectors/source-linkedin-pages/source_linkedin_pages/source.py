ddddf4baf5991f9c9faf0bcba605edad6869722a
marcosmarxm <marcosmarxm@gmail.com>
2023-03-13 13:58:08 -0300

    fix tests

--- airbyte-integrations/connectors/source-linkedin-pages/source_linkedin_pages/source.py
+++ airbyte-integrations/connectors/source-linkedin-pages/source_linkedin_pages/source.py
@@ -144,4 +144,11 @@ class SourceLinkedinPages(AbstractSource):
 
     def streams(self, config: Mapping[str, Any]) -> List[Stream]:
         config["authenticator"] = self.get_authenticator(config)
-        return [OrganizationLookup(config), FollowerStatistics(config), ShareStatistics(config), TotalFollowerCount(config)]
+        return [
+            OrganizationLookup(config),
+            FollowerStatistics(config),
+            ShareStatistics(config),
+            TotalFollowerCount(config),
+            ShareStatistics(config),
+            TotalFollowerCount(config),
+        ]
