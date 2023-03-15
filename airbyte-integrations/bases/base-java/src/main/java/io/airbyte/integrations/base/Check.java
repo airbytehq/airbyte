050c8fc877a040a4f78a888eeab5323c62b856be
jbfbell <jbfbell@users.noreply.github.com>
2023-03-15 22:16:34 +0000

    Automated Commit - Formatting Changes

--- airbyte-integrations/bases/base-java/src/main/java/io/airbyte/integrations/base/Check.java
+++ airbyte-integrations/bases/base-java/src/main/java/io/airbyte/integrations/base/Check.java
@@ -1,3 +1,7 @@
+/*
+ * Copyright (c) 2023 Airbyte, Inc., all rights reserved.
+ */
+
 package io.airbyte.integrations.base;
 
 import com.fasterxml.jackson.databind.JsonNode;
@@ -6,7 +10,8 @@ import io.airbyte.protocol.models.v0.ConfiguredAirbyteCatalog;
 
 /**
  * The Check method of the Airbyte Protocol is used by Airbyte Actors to verify proper connectivity.
- * This can include verifying hosts are reachable, proper authentication, ability to read or write, etc.
+ * This can include verifying hosts are reachable, proper authentication, ability to read or write,
+ * etc.
  */
 @FunctionalInterface
 public interface Check {
