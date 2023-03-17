9bffecfe2e2af8e82cb29c47e73c6e5cf19b2cb9
Philip Krauss <philip@philipkrauss.at>
2023-03-17 13:47:52 +0100

    Add Xata.io Destination Connector

---
+++ airbyte-integrations/connectors/destination-xata/integration_tests/integration_test.py
@@ -0,0 +1,61 @@
+#
+# Copyright (c) 2023 Airbyte, Inc., all rights reserved.
+#
+
+import json
+from typing import Any, Dict, List, Mapping
+
+from destination_xata import DestinationXata
+
+import pytest
+from airbyte_cdk import AirbyteLogger
+from airbyte_cdk.models import (
+    AirbyteMessage,
+    AirbyteRecordMessage,
+    AirbyteStateMessage,
+    AirbyteStream,
+    ConfiguredAirbyteCatalog,
+    ConfiguredAirbyteStream,
+    DestinationSyncMode,
+    Status,
+    SyncMode,
+    Type,
+)
+
+@pytest.fixture(name="config")
+def config_fixture() -> Mapping[str, Any]:
+    with open("secrets/config.json", "r") as f:
+        return json.loads(f.read())
+
+@pytest.fixture(name="configured_catalog")
+def configured_catalog_fixture() -> ConfiguredAirbyteCatalog:
+    stream_schema = {"type": "object", "properties": {"string_col": {"type": "str"}, "int_col": {"type": "integer"}}}
+
+    append_stream = ConfiguredAirbyteStream(
+        stream=AirbyteStream(name="append_stream", json_schema=stream_schema, supported_sync_modes=[SyncMode.incremental]),
+        sync_mode=SyncMode.incremental,
+        destination_sync_mode=DestinationSyncMode.append,
+    )
+    # TODO implement overwrite
+    """
+    overwrite_stream = ConfiguredAirbyteStream(
+        stream=AirbyteStream(name="overwrite_stream", json_schema=stream_schema, supported_sync_modes=[SyncMode.incremental]),
+        sync_mode=SyncMode.incremental,
+        destination_sync_mode=DestinationSyncMode.overwrite,
+    )
+    """
+    return ConfiguredAirbyteCatalog(streams=[append_stream,]) # overwrite_stream])
+
+def test_check_valid_config(config: Mapping):
+    outcome = DestinationXata().check(AirbyteLogger(), config)
+    assert outcome.status == Status.SUCCEEDED
+
+
+def test_check_invalid_config():
+    outcome = DestinationXata().check(AirbyteLogger(), {"api_key": "airbyte-tests-no-key-provided"})
+    assert outcome.status == Status.FAILED
+
+
+def integration_test():
+    # TODO write integration tests 
+    pass
---
+++ airbyte-integrations/connectors/destination-xata/setup.py
@@ -0,0 +1,26 @@
+#
+# Copyright (c) 2023 Airbyte, Inc., all rights reserved.
+#
+
+
+from setuptools import find_packages, setup
+
+MAIN_REQUIREMENTS = [
+    "airbyte-cdk",
+    "xata==0.8.0",
+]
+
+TEST_REQUIREMENTS = ["pytest~=6.2"]
+
+setup(
+    name="destination_xata",
+    description="Destination implementation for Xata.io",
+    author="Philip Krauss <philip@xata.io>",
+    author_email="support@xata.io",
+    packages=find_packages(),
+    install_requires=MAIN_REQUIREMENTS,
+    package_data={"": ["*.json"]},
+    extras_require={
+        "tests": TEST_REQUIREMENTS,
+    },
+)
