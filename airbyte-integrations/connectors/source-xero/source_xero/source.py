592682e3ac2091ffc036f9a800a650b27e75e4ad
Sergey Chvalyuk <grubberr@gmail.com>
2023-03-18 08:15:22 +0000

    _validate_and_transform added
    Signed-off-by: Sergey Chvalyuk <grubberr@gmail.com>

--- airbyte-integrations/connectors/source-xero/source_xero/source.py
+++ airbyte-integrations/connectors/source-xero/source_xero/source.py
@@ -36,18 +36,24 @@ from .streams import (
 
 
 class SourceXero(AbstractSource):
+    def _validate_and_transform(self, config: Mapping[str, Any]):
+        config["start_date"] = pendulum.parse(config["start_date"])
+        return config
+
     def check_connection(self, logger, config) -> Tuple[bool, any]:
+        config = self._validate_and_transform(config)
         stream = Organisations(authenticator=self.get_authenticator(config), tenant_id=config["tenant_id"])
         records = stream.read_records(sync_mode=SyncMode.full_refresh)
         record = next(records)
         return record["OrganisationID"] == config["tenant_id"], None
 
     def streams(self, config: Mapping[str, Any]) -> List[Stream]:
+        config = self._validate_and_transform(config)
         stream_kwargs = {
             "authenticator": self.get_authenticator(config),
             "tenant_id": config["tenant_id"],
         }
-        incremental_kwargs = {**stream_kwargs, "start_date": pendulum.parse(config.get("start_date"))}
+        incremental_kwargs = {**stream_kwargs, "start_date": config["start_date"]}
         streams = [
             BankTransactions(**incremental_kwargs),
             Contacts(**incremental_kwargs),
