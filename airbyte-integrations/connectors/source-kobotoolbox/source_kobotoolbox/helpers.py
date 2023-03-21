20bf345b3195f62506e863aa5ea4c97eecf3c80a
siddhant <ssiddhant3030@gmail.com>
2023-03-21 13:17:23 +0530

    fixed issue

--- airbyte-integrations/connectors/source-kobotoolbox/source_kobotoolbox/helpers.py
+++ airbyte-integrations/connectors/source-kobotoolbox/source_kobotoolbox/helpers.py
@@ -6,5 +6,9 @@
 class Helpers(object):
     @staticmethod
     def get_json_schema():
-        json_schema = {}
+        json_schema = {
+            "$schema": "http://json-schema.org/draft-07/schema#",
+            "type": "object",
+            "properties": {"_id": {"type": "string"}, "indexed_on": {"type": "string", "format": "date-time"}},
+        }
         return json_schema
