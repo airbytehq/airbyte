63dad6b8b962b521b968d04f11c8ee7026c950b5
marcosmarxm <marcosmarxm@gmail.com>
2023-03-21 16:29:58 -0300

    remove incremental from conversations

--- airbyte-integrations/connectors/source-twilio/source_twilio/streams.py
+++ airbyte-integrations/connectors/source-twilio/source_twilio/streams.py
@@ -22,6 +22,7 @@ from requests.auth import AuthBase
 TWILIO_API_URL_BASE = "https://api.twilio.com"
 TWILIO_API_URL_BASE_VERSIONED = f"{TWILIO_API_URL_BASE}/2010-04-01/"
 TWILIO_MONITOR_URL_BASE = "https://monitor.twilio.com/v1/"
+TWILIO_CONVERSATIONS_URL_BASE = "https://conversations.twilio.com/v1/"
 
 
 class TwilioStream(HttpStream, ABC):
@@ -504,14 +505,11 @@ class Alerts(IncrementalTwilioStream):
         return self.name.title()
 
 
-class Conversations(IncrementalTwilioStream):
+class Conversations(TwilioStream):
     """https://www.twilio.com/docs/conversations/api/conversation-resource#read-multiple-conversation-resources"""
 
-    url_base = "https://conversations.twilio.com/v1/"
-    lower_boundary_filter_field = "StartDate="
-    upper_boundary_filter_field = "EndDate="
+    url_base = TWILIO_CONVERSATIONS_URL_BASE
     data_field = "conversations"
-    cursor_field = "date_updated"
 
     def path(self, **kwargs):
         return self.name.title()
