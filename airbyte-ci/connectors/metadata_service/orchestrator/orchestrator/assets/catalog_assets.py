8001cbce8137afa426bb06cd78c26b789b9bdba5
Ben Church <ben@airbyte.io>
2023-03-20 21:49:50 -0700

    Filter out null metadata values

--- airbyte-ci/connectors/metadata_service/orchestrator/orchestrator/assets/catalog_assets.py
+++ airbyte-ci/connectors/metadata_service/orchestrator/orchestrator/assets/catalog_assets.py
@@ -61,40 +61,69 @@ def compute_catalog_overrides(merged_df):
     if cloud_only or oss_only:
         return catalogs
 
-    # get all columns ending with _oss
-    all_oss_columns = [col for col in merged_df.index if col.endswith(OSS_SUFFIX)]
-
+    allowed_overrides = [
+        "name",
+        "dockerRepository",
+        "dockerImageTag",
+        "supportsDbt",
+        "supportsNormalization",
+        "license",
+        "supportUrl",
+        "sourceType",
+        "allowedHosts",
+        "normalizationConfig",
+        "suggestedStreams",
+        "resourceRequirements",
+    ]
 
     # check if the columns are the same
     # TODO refactor this to handle cloud only
-    for oss_col in all_oss_columns:
-        equivalent_cloud_col = oss_col.replace(OSS_SUFFIX, CLOUD_SUFFIX)
-        if merged_df.get(oss_col) != merged_df.get(equivalent_cloud_col):
-            col_name = oss_col.replace(OSS_SUFFIX, "")
-            catalogs["cloud"][col_name] = merged_df.get(equivalent_cloud_col)
+    for override_col in allowed_overrides:
+        oss_col = override_col + OSS_SUFFIX
+        cloud_col = override_col + CLOUD_SUFFIX
+
+        cloud_value = merged_df.get(cloud_col)
+        oss_value = merged_df.get(oss_col)
+
+        # if the columns are different, add the cloud value to the overrides
+        # TODO do a deep comparison
+        if cloud_value and oss_value != cloud_value:
+            catalogs["cloud"][override_col] = merged_df.get(cloud_col)
 
     return catalogs;
 
 
 def merge_into_metadata_definitions(id_field, connector_type, oss_connector_df, cloud_connector_df) -> pd.Series:
     merged_connectors = pd.merge(oss_connector_df, cloud_connector_df, on=id_field, how='outer', suffixes=(OSS_SUFFIX, CLOUD_SUFFIX), indicator=True)
-
+    sanitized_connectors = merged_connectors.where(pd.notnull(merged_connectors), None)
     def build_metadata(merged_df):
+        raw_data = {
+            "name": get_field_with_fallback(merged_df, "name"),
+            "definitionId": merged_df[id_field],
+            "connectorType": connector_type,
+            "dockerRepository": get_field_with_fallback(merged_df, "dockerRepository"),
+            "githubIssueLabel": get_field_with_fallback(merged_df, "dockerRepository").replace("airbyte/", ""),
+            "dockerImageTag": get_field_with_fallback(merged_df, "dockerImageTag"),
+            "icon": get_field_with_fallback(merged_df, "icon"),
+            "supportUrl": get_field_with_fallback(merged_df, "documentationUrl"),
+            "sourceType": get_field_with_fallback(merged_df, "sourceType"),
+            "releaseStage": get_field_with_fallback(merged_df, "releaseStage"),
+            "license": "MIT",
+
+            "supportsDbt": get_field_with_fallback(merged_df, "supportsDbt"),
+            "supportsNormalization": get_field_with_fallback(merged_df, "supportsNormalization"),
+            "allowedHosts": get_field_with_fallback(merged_df, "allowedHosts"),
+            "normalizationConfig": get_field_with_fallback(merged_df, "normalizationConfig"),
+            "suggestedStreams": get_field_with_fallback(merged_df, "suggestedStreams"),
+            "resourceRequirements": get_field_with_fallback(merged_df, "resourceRequirements"),
+        }
+
+        # remove none values
+        data = {k: v for k, v in raw_data.items() if v is not None}
+
         metadata = {
             "metadataSpecVersion": "1.0",
-            "data": {
-                "name": get_field_with_fallback(merged_df, "name"),
-                "definitionId": merged_df[id_field],
-                "connectorType": connector_type,
-                "dockerRepository": get_field_with_fallback(merged_df, "dockerRepository"),
-                "githubIssueLabel": get_field_with_fallback(merged_df, "dockerRepository").replace("airbyte/", ""),
-                "dockerImageTag": get_field_with_fallback(merged_df, "dockerImageTag"),
-                "icon": get_field_with_fallback(merged_df, "icon"),
-                "supportUrl": get_field_with_fallback(merged_df, "documentationUrl"),
-                "sourceType": get_field_with_fallback(merged_df, "sourceType"),
-                "releaseStage": get_field_with_fallback(merged_df, "releaseStage"),
-                "license": "MIT",
-            }
+            "data": data
         }
 
         catalogs = compute_catalog_overrides(merged_df)
@@ -102,7 +131,7 @@ def merge_into_metadata_definitions(id_field, connector_type, oss_connector_df, 
 
         return metadata
 
-    metadata_list = [build_metadata(merged_df) for _, merged_df in merged_connectors.iterrows()]
+    metadata_list = [build_metadata(merged_df) for _, merged_df in sanitized_connectors.iterrows()]
 
     return metadata_list
 
