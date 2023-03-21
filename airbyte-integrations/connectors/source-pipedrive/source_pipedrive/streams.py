9cbfb03cc7369283390c30116bed2891a2df6bce
Sergey Chvalyuk <grubberr@gmail.com>
2023-03-21 12:06:44 +0000

    improve get_updated_state
    Signed-off-by: Sergey Chvalyuk <grubberr@gmail.com>

--- airbyte-integrations/connectors/source-pipedrive/source_pipedrive/streams.py
+++ airbyte-integrations/connectors/source-pipedrive/source_pipedrive/streams.py
@@ -90,10 +90,13 @@ class PipedriveStream(HttpStream, ABC):
         Return the latest state by comparing the cursor value in the latest record with the stream's most recent state object
         and returning an updated state object.
         """
-        latest_benchmark = latest_record[self.cursor_field]
-        if current_stream_state.get(self.cursor_field):
-            return {self.cursor_field: max(latest_benchmark, current_stream_state[self.cursor_field])}
-        return {self.cursor_field: latest_benchmark}
+        updated_state = latest_record.get(self.cursor_field)
+        if updated_state:
+            stream_state_value = current_stream_state.get(self.cursor_field)
+            if stream_state_value:
+                updated_state = max(updated_state, stream_state_value)
+            current_stream_state[self.cursor_field] = updated_state
+        return current_stream_state
 
 
 class Deals(PipedriveStream):
@@ -224,6 +227,7 @@ class Users(PipedriveStream):
     """
 
     cursor_field = "modified"
+    page_size = 500
 
     def parse_response(self, response: requests.Response, **kwargs) -> Iterable[Mapping]:
         record_gen = super().parse_response(response=response, **kwargs)
