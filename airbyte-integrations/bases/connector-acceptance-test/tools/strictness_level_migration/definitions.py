adffde9e5daf322483cec211c66d4ac374061645
erohmensing <erohmensing@gmail.com>
2023-03-22 17:02:33 -0500

    Default to testing only beta and GA connectors

--- airbyte-integrations/bases/connector-acceptance-test/tools/strictness_level_migration/definitions.py
+++ airbyte-integrations/bases/connector-acceptance-test/tools/strictness_level_migration/definitions.py
@@ -41,3 +41,4 @@ def is_airbyte_connector(connector_definition):
 
 ALL_DEFINITIONS = read_source_definitions()
 GA_DEFINITIONS = find_by_release_stage(ALL_DEFINITIONS, "generally_available")
+BETA_DEFINITIONS = find_by_release_stage(ALL_DEFINITIONS, "beta")
