5264ad772f3f328a0054f08208be69c2886e31f8
Frans Krojegård <frans.krojegard@bonniernews.se>
2023-03-24 13:45:18 +0100

    Add unit test (red) for expected format on ZuoraObjectsBase.to_datetime_str

--- airbyte-integrations/connectors/source-zuora/unit_tests/unit_test.py
+++ airbyte-integrations/connectors/source-zuora/unit_tests/unit_test.py
@@ -2,7 +2,16 @@
 # Copyright (c) 2023 Airbyte, Inc., all rights reserved.
 #
 
+import unittest
+from datetime import datetime
+from source_zuora.source import ZuoraObjectsBase
 
-def test_example():
-    """Example of unit test"""
-    pass
+class TestZuoraObjectsBase(unittest.TestCase):
+    def test_to_datetime_str(self):
+        original_str = '2021-07-15 07:45:55 -07:00'
+        date = datetime.fromisoformat(original_str)
+        generated_str = ZuoraObjectsBase.to_datetime_str(date)
+        self.assertEqual(original_str, generated_str)
+
+if __name__ == '__main__':
+    unittest.main()
