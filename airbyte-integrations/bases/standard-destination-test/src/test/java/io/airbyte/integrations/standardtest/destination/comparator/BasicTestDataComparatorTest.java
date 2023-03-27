5738c16236b2bba44a128dafe56997d8f1a4e263
Joe Bell <joseph.bell@airbyte.io>
2023-03-27 14:26:09 -0700

    add basic comparator test

---
+++ airbyte-integrations/bases/standard-destination-test/src/test/java/io/airbyte/integrations/standardtest/destination/comparator/BasicTestDataComparatorTest.java
@@ -0,0 +1,25 @@
+package io.airbyte.integrations.standardtest.destination.comparator;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import io.airbyte.integrations.standardtest.destination.comparator.parameters.AdvancedTestDataComparatorTestParameters.AssertSameDataArgumentProvider;
+import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ArgumentsSource;
+
+public class BasicTestDataComparatorTest {
+
+  private BasicTestDataComparator comparator;
+
+  @BeforeEach
+  void init() {
+    this.comparator = new BasicTestDataComparator(List::of);
+  }
+
+  @ParameterizedTest
+  @ArgumentsSource(AssertSameDataArgumentProvider.class)
+  public void testAssertSameData(final List<JsonNode> first, final List<JsonNode> second) {
+    comparator.assertSameData(first, second);
+  }
+
+}
