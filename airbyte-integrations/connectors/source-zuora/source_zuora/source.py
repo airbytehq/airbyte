a71aa6d3a37b26677bd76ea52002a92917855c47
Frans Krojegård <frans.krojegard@bonniernews.se>
2023-03-27 15:20:07 +0200

    Test generating str with pendulum instead.

--- airbyte-integrations/connectors/source-zuora/source_zuora/source.py
+++ airbyte-integrations/connectors/source-zuora/source_zuora/source.py
@@ -175,9 +175,9 @@ class ZuoraObjectsBase(ZuoraBase):
         string with 6 decimals for fractions of a second, the updateddate filter is ignored, at least for input rows.
         So if the table has > 10m rows, you hit the 10m row limit.
         Returns the formatted datetime string in a way the Zuora Data Query API endpoint recognises it as timestamp.
-        :: Output example: '2021-07-15 07:45:55.000-07:00'
+        :: Output example: '2021-07-15 07:45:55.000 -07:00'
         """
-        return date.isoformat(sep=' ', timespec='milliseconds')
+        return pendulum.instance(date).format('YYYY-MM-DD HH:mm:ss.SSS Z')
 
     def get_cursor_from_schema(self, schema: Dict) -> str:
         """
--- airbyte-integrations/connectors/source-zuora/unit_tests/unit_test.py
+++ airbyte-integrations/connectors/source-zuora/unit_tests/unit_test.py
@@ -5,10 +5,19 @@
 import unittest
 from datetime import datetime
 from source_zuora.source import ZuoraObjectsBase
+import pendulum
 
 class TestZuoraObjectsBase(unittest.TestCase):
     def test_to_datetime_str(self):
         original_str = '2021-07-15 07:45:55.000-07:00'
-        date = datetime.fromisoformat(original_str)
+        expected_str = '2021-07-15 07:45:55.000 -07:00'
+        date = pendulum.parse(original_str)
         generated_str = ZuoraObjectsBase.to_datetime_str(date)
-        self.assertEqual(original_str, generated_str)
+        self.assertEqual(expected_str, generated_str)
+
+    def test_to_datetime_str_without_tz(self):
+        original_str = '2021-07-15 07:45:55.000'
+        expected_str = '2021-07-15 07:45:55.000 +00:00'
+        date = pendulum.parse(original_str)
+        generated_str = ZuoraObjectsBase.to_datetime_str(date)
+        self.assertEqual(expected_str, generated_str)
