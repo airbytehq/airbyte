73cc08eda15b1cb3e3765a9167e8f52d125e8255
erohmensing <erohmensing@gmail.com>
2023-03-27 19:28:16 -0500

    Clean up run_tests.py

--- airbyte-integrations/bases/connector-acceptance-test/tools/acceptance_test_config_migration/run_tests.py
+++ airbyte-integrations/bases/connector-acceptance-test/tools/acceptance_test_config_migration/run_tests.py
@@ -4,25 +4,25 @@
 
 import argparse
 import asyncio
+import logging
 import os
 from pathlib import Path
 
 import utils
-from definitions import BETA_DEFINITIONS, GA_DEFINITIONS, find_by_name, get_airbyte_connector_name_from_definition, is_airbyte_connector
+from definitions import get_airbyte_connector_name_from_definition, is_airbyte_connector
 
 MODULE_NAME = "fail_on_extra_columns"
 
 parser = argparse.ArgumentParser(description="Run tests for a list of connectors.")
 parser.add_argument("--connectors", nargs="*")
-parser.add_argument("--file")
 parser.add_argument("--max_concurrency", type=int, default=10)
 
 
 async def run_tests(connector_name):
-    path_to_acceptance_test_runner = Path(utils.CONNECTORS_DIRECTORY) / connector_name / "acceptance-test-docker.sh"
+    path_to_acceptance_test_runner = utils.acceptance_test_docker_sh_path(connector_name)
     path_to_acceptance_test_config = utils.acceptance_test_config_path(connector_name)
 
-    print(f"Start running tests for {connector_name}.")
+    logging.info(f"Start running tests for {connector_name}.")
     process = await asyncio.create_subprocess_exec(
         "sh",
         path_to_acceptance_test_runner,
@@ -32,56 +32,45 @@ async def run_tests(connector_name):
     )
     return_code = await process.wait()
 
-    dir_path = Path(f"results/{return_code}")
-    dir_path.mkdir(parents=True, exist_ok=True)
+    output_path = Path(f"templates/{MODULE_NAME}/results/{return_code}")  # TODO put this in the module
+    output_path.mkdir(parents=True, exist_ok=True)
 
     contents = await process.stdout.read()
-    with open(f"{dir_path}/{connector_name}.txt", "wb") as f:
+    with open(f"{output_path}/{connector_name}", "wb") as f:
         f.write(contents)
 
     if return_code == 0:
-        print(f"{connector_name} succeeded.")
+        logging.info(f"{connector_name} succeeded.")
     else:
-        print(f"{connector_name} tests failed with exit code {return_code}.")
+        logging.info(f"{connector_name} tests failed with exit code {return_code}.")
 
 
 async def semaphore_gather(coroutines, num_semaphores):
     # Limit the amount of connectors we want to test at once
     # To avoid crashing our docker by spinning up too many containers
-    # Or using too much CPU. How many you can run will depend on the
-    # specs you've allocated to Docker
+    # Or using too much CPU. How many you can run at once effectively
+    # will depend on the specs you've allocated to Docker
     semaphore = asyncio.Semaphore(num_semaphores)
 
-    async def _wrap_coro(coroutine):
+    async def _wrap_coroutine(coroutine):
         async with semaphore:
             return await coroutine
 
-    return await asyncio.gather(*(_wrap_coro(coroutine) for coroutine in coroutines), return_exceptions=False)
+    return await asyncio.gather(*(_wrap_coroutine(coroutine) for coroutine in coroutines), return_exceptions=False)
 
 
 async def main(definitions, num_semaphores):
     tasks = []
-
     for definition in definitions:
         if is_airbyte_connector(definition):
             connector_name = get_airbyte_connector_name_from_definition(definition)
             tasks.append(run_tests(connector_name))
+        else:
+            logging.warning(f"Won't run tests for non-airbyte connector {get_airbyte_connector_name_from_definition(definition)}")
     await asyncio.gather(semaphore_gather(tasks, num_semaphores=num_semaphores))
 
 
 if __name__ == "__main__":
     args = parser.parse_args()
-
-    definitions = []
-    if args.connectors:
-        definitions = find_by_name(args.connectors)
-    elif args.file:
-        with open(f"templates/{MODULE_NAME}/{args.file}", "r") as f:
-            connector_names = [line.strip() for line in f]
-        definitions = find_by_name(connector_names)
-    else:
-        definitions = GA_DEFINITIONS + BETA_DEFINITIONS
-
-    num_semaphores = args.max_concurrency
-
-    asyncio.run(main(definitions=definitions, num_semaphores=num_semaphores))
+    definitions = utils.get_definitions_from_args(args)
+    asyncio.run(main(definitions=definitions, num_semaphores=args.max_concurrency))
--- airbyte-integrations/bases/connector-acceptance-test/tools/acceptance_test_config_migration/utils.py
+++ airbyte-integrations/bases/connector-acceptance-test/tools/acceptance_test_config_migration/utils.py
@@ -5,7 +5,6 @@ import argparse
 from pathlib import Path
 
 from definitions import find_by_name, GA_DEFINITIONS
-from create_issues import MODULE_NAME
 
 CONNECTORS_DIRECTORY = "../../../../connectors"
 
@@ -14,8 +13,14 @@ def acceptance_test_config_path(connector_name):
     """Returns the path to a given connector's acceptance-test-config.yml file."""
     return Path(CONNECTORS_DIRECTORY) / connector_name / "acceptance-test-config.yml"
 
+
+def acceptance_test_docker_sh_path(connector_name):
+    return Path(CONNECTORS_DIRECTORY) / connector_name / "acceptance-test-docker.sh"
+
+
 def add_connectors_param(parser: argparse.ArgumentParser):
     parser.add_argument()
+
 
 def get_definitions_from_args(args):
     if args.connectors:
