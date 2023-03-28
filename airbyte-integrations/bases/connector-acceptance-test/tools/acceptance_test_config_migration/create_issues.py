c603e9ff7c08e7258c3b95a36752707ccfb236d6
erohmensing <erohmensing@gmail.com>
2023-03-27 19:18:36 -0500

    Refactor create_issues.py

--- airbyte-integrations/bases/connector-acceptance-test/tools/acceptance_test_config_migration/create_issues.py
+++ airbyte-integrations/bases/connector-acceptance-test/tools/acceptance_test_config_migration/create_issues.py
@@ -44,19 +44,24 @@ def get_issue_content(source_definition):
 
     issue_body = template.render(
         connector_name=source_definition["name"], release_stage=source_definition["releaseStage"], test_failure_logs=test_failure_logs
-    )
+    )  # TODO: how to make whatever needs to be rendered configurable
     file_definition, issue_body_path = tempfile.mkstemp()
 
     with os.fdopen(file_definition, "w") as tmp:
         tmp.write(issue_body)
 
-    return {"title": issue_title, "body_file": issue_body_path, "labels": COMMON_ISSUE_LABELS}
+    return {"title": issue_title, "body_file": issue_body_path, "labels": COMMON_ISSUE_LABELS, "project": GITHUB_PROJECT_NAME}
 
 
-def create_issue(source_definition, dry_run=True):
-    issue_content = get_issue_content(source_definition)
+def existing_issues(issue_content):
     list_command_arguments = ["gh", "issue", "list", "--state", "open", "--search", f"'{issue_content['title']}'", "--json", "url"]
+    list_existing_issue_process = subprocess.Popen(list_command_arguments, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    stdout, stderr = list_existing_issue_process.communicate()
+    existing_issues = json.loads(stdout.decode())
+    return existing_issues
 
+
+def create_command(issue_content):
     create_command_arguments = [
         "gh",
         "issue",
@@ -67,19 +72,20 @@ def create_issue(source_definition, dry_run=True):
         issue_content["body_file"],
     ]
     if GITHUB_PROJECT_NAME:
-        create_command_arguments += ["--project", GITHUB_PROJECT_NAME]
+        create_command_arguments += ["--project", issue_content["project"]]
     for label in issue_content["labels"]:
         create_command_arguments += ["--label", label]
+    return create_command
 
-    list_existing_issue_process = subprocess.Popen(list_command_arguments, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    stdout, stderr = list_existing_issue_process.communicate()
-    existing_issues = json.loads(stdout.decode())
-    already_created = len(existing_issues) > 0
-    if already_created:
+
+def create_issue(source_definition, dry_run=True):
+    issue_content = get_issue_content(source_definition)
+
+    if len(existing_issues(issue_content)) > 0:
         logging.warning(f"An issue was already created for this definition: {existing_issues[0]}")
-    if not already_created:
+    else:
         if not dry_run:
-            process = subprocess.Popen(create_command_arguments, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            process = subprocess.Popen(create_command(issue_content), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
             stdout, stderr = process.communicate()
             if stderr:
                 logging.error(stderr.decode())
@@ -87,14 +93,12 @@ def create_issue(source_definition, dry_run=True):
                 created_issue_url = stdout.decode()
                 logging.info(f"Created issue for {source_definition['name']}: {created_issue_url}")
         else:
-            logging.info(f"[DRY RUN]: {' '.join(create_command_arguments)}")
+            logging.info(f"[DRY RUN]: {' '.join(create_command(issue_content))}")
     os.remove(issue_content["body_file"])
 
 
 if __name__ == "__main__":
     args = parser.parse_args()
-
-    definitions = []
     definitions = utils.get_definitions_from_args(args)
 
     for definition in definitions:
@@ -103,5 +107,4 @@ if __name__ == "__main__":
         if not args.allow_alpha and definition not in BETA_DEFINITIONS + GA_DEFINITIONS:
             logging.info(f"Won't create an issue for non beta/GA connector {get_airbyte_connector_name_from_definition(definition)}")
         else:
-            logging.info(f"got dry: {args.dry}")
             create_issue(definition, dry_run=args.dry)
