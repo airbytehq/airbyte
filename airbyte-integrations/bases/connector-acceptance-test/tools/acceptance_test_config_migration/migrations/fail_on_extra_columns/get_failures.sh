be8b8e2339a1c8dd6fe5f6192d1abf6aac726401
erohmensing <erohmensing@gmail.com>
2023-03-29 18:13:10 -0500

    Fix bash script

--- airbyte-integrations/bases/connector-acceptance-test/tools/acceptance_test_config_migration/migrations/fail_on_extra_columns/get_failures.sh
+++ airbyte-integrations/bases/connector-acceptance-test/tools/acceptance_test_config_migration/migrations/fail_on_extra_columns/get_failures.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
+
 FULL_OUTPUT_DIR="./output/1"  # TODO: make this work even when called from another dir
 
-grep -rnw "$FULL_OUTPUT_DIR" -e "Additional properties are not allowed" -B 1 > "/tmp/columns.txt"
+tmp_columns_file=$(mktemp)
+grep -rnw "$FULL_OUTPUT_DIR" -e "Additional properties are not allowed" -B 1 > "$tmp_columns_file"
 mkdir -p "./test_failure_logs"
 
 for f in $(ls $FULL_OUTPUT_DIR); do
-  grep "/tmp/columns.txt" -e "$f" > "./test_failure_logs/$f"
+  results=$(mktemp)
+  grep "$tmp_columns_file" -e "$f" > "$results"
+  if [ -s "$results" ]; then
+      cat "$results" > "./test_failure_logs/$f"
+  fi
 done
-
-rm /tmp/columns.txt
