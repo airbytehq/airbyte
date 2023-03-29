7379950914fa1f0961dcb49c309f137a4c01440e
Mohamad Hallak <16711801+mrhallak@users.noreply.github.com>
2023-03-29 17:54:49 +0200

    Bug fix

--- airbyte-integrations/connectors/source-stripe/source_stripe/streams.py
+++ airbyte-integrations/connectors/source-stripe/source_stripe/streams.py
@@ -218,7 +218,7 @@ class CreditNotes(IncrementalStripeStream):
     cursor_field = "created"
 
     def path(self, **kwargs):
-        return "transfers"
+        return "credit_notes"
 
 
 class Coupons(IncrementalStripeStream):
