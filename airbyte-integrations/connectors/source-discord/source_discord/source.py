e2ebcd3ab531bf3300ab236e784470212cde9092
Jakin-Liu <65338514+Jakin-Liu@users.noreply.github.com>
2023-03-30 17:20:28 +0800

    feat: 调整guild name (#11)
    * feat: 调整guild name
    
    * feat: fixed name
    
    ---------
    
    Co-authored-by: Jakin <iOSLiuYong2012@163.com>

--- airbyte-integrations/connectors/source-discord/source_discord/source.py
+++ airbyte-integrations/connectors/source-discord/source_discord/source.py
@@ -3,7 +3,7 @@
 #
 
 from typing import Any, List, Mapping, Tuple
-
+import json
 import datetime
 import requests
 from airbyte_cdk.sources import AbstractSource
@@ -13,13 +13,22 @@ from .stream_channels import Channels
 from .stream_members import Members
 from .stream_messages import Messages
 from .stream_roles import Roles
-from .stream_serverPreview import ServerPreview
+from .stream_server_preview import ServerPreview
 
 
 class SourceDiscord(AbstractSource):
+
+    def get_guild_name(self, guild_id: str, headers):
+        guild_result = requests.get(f'https://discord.com/api/v10/guilds/{guild_id}/preview', headers=headers)
+        guild_result = json.loads(guild_result.text)
+        guild_name = guild_result['name']
+        return guild_name
+
     def check_connection(self, _, config) -> Tuple[bool, str]:
-        url = "https://discord.com/api/users/@me"
+
         headers = {"Authorization": f"Bot {config['server_token']}"}
+
+        url = "https://discord.com/api/users/@me"
         response = requests.get(url, headers=headers)
         j_response = response.json()
         if "id" not in j_response:
@@ -29,9 +38,14 @@ class SourceDiscord(AbstractSource):
         return True, "accepted"
 
     def streams(self, config: Mapping[str, Any]) -> List[Stream]:
+        headers = {"Authorization": f"Bot {config['server_token']}"}
+        guild_name = config['guild_name'] if config['guild_name'] else self.get_guild_name(guild_id=config['guild_id'], headers=headers)
+
+        config['guild_name'] = guild_name
         config["job_time"] = datetime.datetime.now()
 
         server_preview_stream = ServerPreview(config)
+        print('config --- start -----', config)
         channel_stream = Channels(config)
         message_stream = Messages(config)
         member_stream = Members(config)
