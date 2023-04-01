03d7072b91825ca6abf096888d76e13a7a6129e3
Marcos Marx <marcosmarxm@users.noreply.github.com>
2023-04-01 10:00:27 -0300

    remove extra lines

--- airbyte-integrations/connectors/source-twilio/source_twilio/streams.py
+++ airbyte-integrations/connectors/source-twilio/source_twilio/streams.py
@@ -527,5 +527,3 @@ class ConversationParticipants(TwilioNestedStream):
 
     def parent_record_to_stream_slice(self, record: Mapping[str, Any]) -> Mapping[str, Any]:
         return {"conversation_sid": record["sid"]}
-
-    
