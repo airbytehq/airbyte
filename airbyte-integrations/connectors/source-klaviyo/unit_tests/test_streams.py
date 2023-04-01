be2e269f16f9daddf79baed37cbd844d2b865e06
Ganesh R <geekwhocodes@outlook.com>
2023-04-01 07:36:02 +0530

    Source Klaviyo: adds stream Templates (#23236)
    * Source Klaviyo: adds stream Templates
    
    * Source Klaviyo: adds unit tests
    
    * fix formatting
    
    * bump version and update docs
    
    * Update Dockerfile
    
    * Update email_templates.json
    
    * add expected record and fix schema
    
    * rever abnormal state file
    
    * fix integration tests files
    
    * update connector image version
    
    * Update Dockerfile
    
    * add eof
    
    * update docs
    
    * update base class for Email Tempaltes
    
    * auto-bump connector version
    
    ---------
    
    Co-authored-by: marcosmarxm <marcosmarxm@gmail.com>
    Co-authored-by: Marcos Marx <marcosmarxm@users.noreply.github.com>
    Co-authored-by: Octavia Squidington III <octavia-squidington-iii@users.noreply.github.com>

--- airbyte-integrations/connectors/source-klaviyo/unit_tests/test_streams.py
+++ airbyte-integrations/connectors/source-klaviyo/unit_tests/test_streams.py
@@ -9,7 +9,7 @@ import pendulum
 import pytest
 import requests
 from pydantic import BaseModel
-from source_klaviyo.streams import Events, IncrementalKlaviyoStreamV1, KlaviyoStreamV1, ReverseIncrementalKlaviyoStreamV1
+from source_klaviyo.streams import EmailTemplates, Events, IncrementalKlaviyoStreamV1, KlaviyoStreamV1, ReverseIncrementalKlaviyoStreamV1
 
 START_DATE = pendulum.datetime(2020, 10, 10)
 
@@ -290,4 +290,19 @@ class TestEventsStream:
                 "flow_id": "advanced",
                 "flow_message_id": "nice to meet you",
             },
+        ]
+
+
+class TestEmailTemplatesStream:
+    def test_parse_response(self, mocker):
+        stream = EmailTemplates(api_key="some_key")
+        json = {
+            "data": [
+               {"object": "email-template", "id": "id", "name": "Newsletter #1", "html": "<!DOCTYPE html></html>", "is_writeable": "true", "created": "2023-02-18T11:18:22+00:00", "updated": "2023-02-18T12:01:12+00:00"},
+            ]
+        }
+        records = list(stream.parse_response(mocker.Mock(json=mocker.Mock(return_value=json))))
+
+        assert records == [
+            {"object": "email-template", "id": "id", "name": "Newsletter #1", "html": "<!DOCTYPE html></html>", "is_writeable": "true", "created": "2023-02-18T11:18:22+00:00", "updated": "2023-02-18T12:01:12+00:00"}
         ]
