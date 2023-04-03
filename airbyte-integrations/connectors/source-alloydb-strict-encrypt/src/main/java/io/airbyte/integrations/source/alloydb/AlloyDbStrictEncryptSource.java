41816329d668e88fd97b3039300492e0afde2683
Duy Nguyen <duy@airbyte.io>
2023-04-03 14:42:29 -0700

    Move PostgresStrictEncrypt back to its original location (#24609)
    * Disallow "disable" option in SSL Mode dropdown
    * Separate PostgresStrictEncrypt
    
    ---------
    
    Co-authored-by: Octavia Squidington III <octavia-squidington-iii@users.noreply.github.com>

--- airbyte-integrations/connectors/source-alloydb-strict-encrypt/src/main/java/io/airbyte/integrations/source/alloydb/AlloyDbStrictEncryptSource.java
+++ airbyte-integrations/connectors/source-alloydb-strict-encrypt/src/main/java/io/airbyte/integrations/source/alloydb/AlloyDbStrictEncryptSource.java
@@ -8,7 +8,7 @@ import static io.airbyte.integrations.source.relationaldb.state.StateManager.LOG
 
 import io.airbyte.integrations.base.IntegrationRunner;
 import io.airbyte.integrations.base.Source;
-import io.airbyte.integrations.source.postgres.PostgresSourceStrictEncrypt;
+import io.airbyte.integrations.source.postgres_strict_encrypt.PostgresSourceStrictEncrypt;
 
 public class AlloyDbStrictEncryptSource {
 
