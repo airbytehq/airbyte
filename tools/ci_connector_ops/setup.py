ba5583f08a6a07a5c13c036179f819df04ea9efa
Ben Church <ben@airbyte.io>
2023-04-03 14:16:37 -0700

    Increase connector ops version

--- tools/ci_connector_ops/setup.py
+++ tools/ci_connector_ops/setup.py
@@ -49,7 +49,7 @@ PIPELINES_REQUIREMENTS = [
 ]
 
 setup(
-    version="0.1.18",
+    version="0.2.0",
     name="ci_connector_ops",
     description="Packaged maintained by the connector operations team to perform CI for connectors",
     author="Airbyte",
