864a5170d75cf47ec494395fdab02c254b6c5cfa
Edward Gao <edward.gao@airbyte.io>
2023-04-06 16:49:57 -0700

    minor tweaks

--- airbyte-integrations/connectors/destination-bigquery/src/main/java/io/airbyte/integrations/destination/bigquery/materialization/BigQueryMaterializationOperations.java
+++ airbyte-integrations/connectors/destination-bigquery/src/main/java/io/airbyte/integrations/destination/bigquery/materialization/BigQueryMaterializationOperations.java
@@ -10,6 +10,7 @@ import com.google.cloud.bigquery.Schema;
 import com.google.cloud.bigquery.Table;
 import io.airbyte.protocol.models.v0.ConfiguredAirbyteStream;
 
+// name is debatable :P but "BigQueryTypingAndDedupingOperations" is a really sad name
 public class BigQueryMaterializationOperations {
 
   private final BigQuery bigquery;
@@ -47,11 +48,14 @@ public class BigQueryMaterializationOperations {
    * and casting their types, etc.
    *
    * The stuff around explicitly writing a merge / insert is new; dbt handles that for us.
+   *
+   * The DELETE thing is new, and an improvement over normalization (where we need a separate DELETE call)
    */
   public void mergeFromRawTable(String dataset, String rawTable, String finalTable, Schema schema) {
     // TODO generate a SQL query >.>
     // TODO handle non dedup sync modes (i.e. no PK) - probably needs to be an INSERT instead of MERGE
-    // TODO handle CDC (i.e. multiple new raw records for a single PK)
+    // TODO handle CDC (multiple new raw records for a single PK)
+    // TODO we should save this generated SQL. Just log it for now, but thats going to be super unergonomic
     /*
     WITH new_raw_records AS (
       SELECT * FROM dataset.rawTable
@@ -60,6 +64,7 @@ public class BigQueryMaterializationOperations {
       SELECT
         json_extract(_airbyte_data, field1) AS field1,
         ...additional extract calls for each top-level field...
+        _airbyte_emitted_at, other metadata columns...
       FROM new_raw_records
     ), typed_raw_records AS (
       CAST(field1 AS type1) AS field1,
@@ -69,6 +74,9 @@ public class BigQueryMaterializationOperations {
     MERGE dataset.finalTable T
     USING dataset.typed_raw_records S
     ON T.primary_key = S.primary_key
+    -- only generate this clause if stream contains the column + is incremenaldedup
+    WHEN MATCHED AND _airbyte_cdc_deleted_at IS NOT NULL THEN
+      DELETE
     WHEN MATCHED THEN
       UPDATE SET field1 = S.field1, ...
     WHEN NOT MATCHED THEN
