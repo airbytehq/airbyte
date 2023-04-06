c47a7df03aee8df439efaea9da272c271c2abfeb
Marcelo Guimaraes <marcelofelippe.mfg@gmail.com>
2023-04-05 21:27:26 -0300

    .cloudbuild/

--- airbyte-integrations/connectors/source-meli-full/source_meli_full/source.py
+++ airbyte-integrations/connectors/source-meli-full/source_meli_full/source.py
@@ -60,10 +60,11 @@ class MeliInvoices(HttpStream):
 
         # Getting ACCESS_KEY_ID
         resource_name = f"projects/{self.google_project_id}/secrets/{self.google_secret_aws_credstash_credentials}/versions/latest"
-        response = client.access_secret_version(resource_name)
+        response = client.access_secret_version(name=resource_name)
         aws_credentials = response.payload.data.decode('UTF-8')
         print(aws_credentials)
         logger.info(aws_credentials)
+
 
         # Getting the credentials from Secrets Manager
         aws_access_key_id = self.aws_access_key_id
