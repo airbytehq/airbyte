d49e33d7b6f923292fee105ffff084c61bce8c45
jdpgrailsdev <jdpgrailsdev@users.noreply.github.com>
2023-04-07 18:52:18 +0000

    Automated Commit - Formatting Changes

--- airbyte-cdk/python/airbyte_cdk/utils/stream_status_utils.py
+++ airbyte-cdk/python/airbyte_cdk/utils/stream_status_utils.py
@@ -1,13 +1,13 @@
+#
+# Copyright (c) 2023 Airbyte, Inc., all rights reserved.
+#
+
 
 from datetime import datetime
 
-from airbyte_cdk.models import (
-    AirbyteStreamStatusTraceMessage,
-    AirbyteMessage,
-    AirbyteTraceMessage,
-    TraceType,
-)
+from airbyte_cdk.models import AirbyteMessage, AirbyteStreamStatusTraceMessage, AirbyteTraceMessage, TraceType
 from airbyte_cdk.models import Type as MessageType
+
 
 def as_airbyte_message(self, stream, current_status, successful) -> AirbyteMessage:
     """
@@ -19,11 +19,7 @@ def as_airbyte_message(self, stream, current_status, successful) -> AirbyteMessa
     trace_message = AirbyteTraceMessage(
         type=TraceType.STREAM_STATUS,
         emitted_at=now_millis,
-        stream_status=AirbyteStreamStatusTraceMessage(
-            stream_descriptor=stream,
-            status=current_status,
-            success=successful
-        ),
+        stream_status=AirbyteStreamStatusTraceMessage(stream_descriptor=stream, status=current_status, success=successful),
     )
 
     return AirbyteMessage(type=MessageType.TRACE, trace=trace_message)
