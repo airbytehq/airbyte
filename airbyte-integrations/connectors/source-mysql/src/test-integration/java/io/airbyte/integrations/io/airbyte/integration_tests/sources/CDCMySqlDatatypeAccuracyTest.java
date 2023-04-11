c5aa330740f762710716109e292c6e60c5614fa9
Rodi Reich Zilberman <867491+rodireich@users.noreply.github.com>
2023-04-11 14:54:45 -0700

    Test all datatypes translation (#25029)
    * Test all datatypes translation
    
    * sanity
    
    * Automated Change
    
    * Adding CDC datatype accuracy test
    
    * Revert "Automated Change"
    
    This reverts commit 78938bc8a8ad803948475c4c9324c6e2d42b8ef7.
    
    * Add missing change to oss_catalog.json
    
    ---------
    
    Co-authored-by: rodireich <rodireich@users.noreply.github.com>

---
+++ airbyte-integrations/connectors/source-mysql/src/test-integration/java/io/airbyte/integrations/io/airbyte/integration_tests/sources/CDCMySqlDatatypeAccuracyTest.java
@@ -0,0 +1,94 @@
+package io.airbyte.integrations.io.airbyte.integration_tests.sources;
+
+import static io.airbyte.integrations.io.airbyte.integration_tests.sources.utils.TestConstants.INITIAL_CDC_WAITING_SECONDS;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.google.common.collect.ImmutableMap;
+import io.airbyte.commons.json.Jsons;
+import io.airbyte.db.Database;
+import io.airbyte.db.factory.DSLContextFactory;
+import io.airbyte.db.factory.DatabaseDriver;
+import io.airbyte.db.jdbc.JdbcUtils;
+import io.airbyte.integrations.standardtest.source.TestDestinationEnv;
+import org.jooq.DSLContext;
+import org.jooq.SQLDialect;
+import org.testcontainers.containers.MySQLContainer;
+
+public class CDCMySqlDatatypeAccuracyTest extends MySqlDatatypeAccuracyTest {
+  private DSLContext dslContext;
+
+  @Override
+  protected void tearDown(final TestDestinationEnv testEnv) {
+    dslContext.close();
+    super.tearDown(testEnv);
+  }
+
+  @Override
+  protected Database setupDatabase() throws Exception {
+    container = new MySQLContainer<>("mysql:8.0");
+    container.start();
+    final JsonNode replicationMethod = Jsons.jsonNode(ImmutableMap.builder()
+        .put("method", "CDC")
+        .put("initial_waiting_seconds", INITIAL_CDC_WAITING_SECONDS)
+        .build());
+    config = Jsons.jsonNode(ImmutableMap.builder()
+        .put(JdbcUtils.HOST_KEY, container.getHost())
+        .put(JdbcUtils.PORT_KEY, container.getFirstMappedPort())
+        .put(JdbcUtils.DATABASE_KEY, container.getDatabaseName())
+        .put(JdbcUtils.USERNAME_KEY, container.getUsername())
+        .put(JdbcUtils.PASSWORD_KEY, container.getPassword())
+        .put("replication_method", replicationMethod)
+        .put("snapshot_mode", "initial_only")
+        .put("is_test", true)
+        .build());
+
+    dslContext = DSLContextFactory.create(
+        config.get(JdbcUtils.USERNAME_KEY).asText(),
+        config.get(JdbcUtils.PASSWORD_KEY).asText(),
+        DatabaseDriver.MYSQL.getDriverClassName(),
+        String.format(DatabaseDriver.MYSQL.getUrlFormatString(),
+            config.get(JdbcUtils.HOST_KEY).asText(),
+            config.get(JdbcUtils.PORT_KEY).asInt(),
+            config.get(JdbcUtils.DATABASE_KEY).asText()),
+        SQLDialect.MYSQL);
+    final Database database = new Database(dslContext);
+
+    // It disable strict mode in the DB and allows to insert specific values.
+    // For example, it's possible to insert date with zero values "2021-00-00"
+    database.query(ctx -> ctx.fetch("SET @@sql_mode=''"));
+
+    revokeAllPermissions();
+    grantCorrectPermissions();
+
+    return database;
+  }
+
+  private void revokeAllPermissions() {
+    executeQuery("REVOKE ALL PRIVILEGES, GRANT OPTION FROM " + container.getUsername() + "@'%';");
+  }
+
+  private void grantCorrectPermissions() {
+    executeQuery(
+        "GRANT SELECT, RELOAD, SHOW DATABASES, REPLICATION SLAVE, REPLICATION CLIENT ON *.* TO "
+            + container.getUsername() + "@'%';");
+  }
+
+  private void executeQuery(final String query) {
+    try (final DSLContext dslContext = DSLContextFactory.create(
+        "root",
+        "test",
+        DatabaseDriver.MYSQL.getDriverClassName(),
+        String.format(DatabaseDriver.MYSQL.getUrlFormatString(),
+            container.getHost(),
+            container.getFirstMappedPort(),
+            container.getDatabaseName()),
+        SQLDialect.MYSQL)) {
+      final Database database = new Database(dslContext);
+      database.query(
+          ctx -> ctx
+              .execute(query));
+    } catch (final Exception e) {
+      throw new RuntimeException(e);
+    }
+  }
+}
