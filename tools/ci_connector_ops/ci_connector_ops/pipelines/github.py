74268b434e02c7d0e13efc4520be3899a2a67763
Augustin <augustin@airbyte.io>
2023-04-12 10:03:27 +0200

    connectors-ci: test java connectors (#24225)

--- tools/ci_connector_ops/ci_connector_ops/pipelines/github.py
+++ tools/ci_connector_ops/ci_connector_ops/pipelines/github.py
@@ -1,6 +1,9 @@
 #
 # Copyright (c) 2023 Airbyte, Inc., all rights reserved.
 #
+
+"""Module grouping functions interacting with the GitHub API."""
+
 from __future__ import annotations
 
 import os
@@ -17,6 +20,7 @@ AIRBYTE_GITHUB_REPO = "airbytehq/airbyte"
 
 
 def safe_log(logger: Optional[Logger], message: str, level: str = "info") -> None:
+    """Log a message to a logger if one is available, otherwise print to the console."""
     if logger:
         log_method = getattr(logger, level.lower())
         log_method(message)
@@ -27,11 +31,21 @@ def safe_log(logger: Optional[Logger], message: str, level: str = "info") -> Non
 def update_commit_status_check(
     sha: str, state: str, target_url: str, description: str, context: str, should_send=True, logger: Logger = None
 ):
-    safe_log(logger, f"Attempting to create {state} status for commit {sha} on Github in {context} context.")
+    """Call the GitHub API to create commit status check.
 
+    Args:
+        sha (str): Hash of the commit for which you want to create a status check.
+        state (str): The check state (success, failure, pendint)
+        target_url (str): The URL to attach to the commit check for details.
+        description (str): Description of the check that is run.
+        context (str): Name of the Check context e.g: source-pokeapi tests
+        should_send (bool, optional): Whether the commit check should actually be sent to GitHub API. Defaults to True.
+        logger (Logger, optional): A logger to log info about updates. Defaults to None.
+    """
     if not should_send:
         return
 
+    safe_log(logger, f"Attempting to create {state} status for commit {sha} on Github in {context} context.")
     try:
         github_client = Github(os.environ["CI_GITHUB_ACCESS_TOKEN"])
         airbyte_repo = github_client.get_repo(AIRBYTE_GITHUB_REPO)
