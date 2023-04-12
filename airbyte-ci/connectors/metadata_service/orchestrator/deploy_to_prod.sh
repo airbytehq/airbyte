9e48569df2bd220ae1e12283130b034623958405
Ben Church <ben@airbyte.io>
2023-04-12 14:24:04 -0700

    Update readme

--- airbyte-ci/connectors/metadata_service/orchestrator/deploy_to_prod.sh
+++ airbyte-ci/connectors/metadata_service/orchestrator/deploy_to_prod.sh
@@ -1,3 +1,11 @@
+# Generate a temporary setup.py file from our pyproject.toml
 poetry2setup >> setup.py
-dagster-cloud serverless deploy-python-executable --location-name metadata_service_orchestrator --location-file dagster_cloud.yaml --python-version 3.9
+
+# Deploy the orchestrator to the dagster-cloud serverless platform
+dagster-cloud serverless deploy-python-executable \
+--location-name metadata_service_orchestrator \
+--location-file dagster_cloud.yaml \
+--python-version 3.9
+
+# Remove the temporary setup.py file
 rm setup.py
