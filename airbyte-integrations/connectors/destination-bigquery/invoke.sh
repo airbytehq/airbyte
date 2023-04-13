c8e592c7c6101e08cb82a8830b72a437dcc2c174
Edward Gao <edward.gao@airbyte.io>
2023-04-13 16:43:09 -0700

    add start+end logs

--- airbyte-integrations/connectors/destination-bigquery/invoke.sh
+++ airbyte-integrations/connectors/destination-bigquery/invoke.sh
@@ -4,6 +4,8 @@ set -e
 
 if test $1 = 'write' # && test $NORMALIZATION_TECHNIQUE = 'LEGACY'
 then
+  echo '{"type": "LOG","log":{"level":"INFO","message":"Starting in-connector normalization"}}'
   # TODO parse args maybe? entrypoint.sh will reject unrecognized args... but `write` currently only accepts config and catalog anyway
   /airbyte/entrypoint.sh run ${@:2} --integration-type bigquery | java -cp "/airbyte/lib/*" io.airbyte.integrations.destination.bigquery.NormalizationLogParser
+  echo '{"type": "LOG","log":{"level":"INFO","message":"Completed in-connector normalization"}}'
 fi
