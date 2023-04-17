192ca60506d20c49f95e182fa6420fe2ce426776
Anet <932848996@qq.com>
2023-04-17 17:46:17 +0800

    CU-865c4t3aq - Click Back on the card number information filling page, the returned page is incorrect

--- airbyte-webapp/src/pages/PaymentErrorPage/PaymentErrorPage.tsx
+++ airbyte-webapp/src/pages/PaymentErrorPage/PaymentErrorPage.tsx
@@ -1,41 +1,43 @@
 import React from "react";
-import { FormattedMessage } from "react-intl";
-import styled from "styled-components";
+// import { FormattedMessage } from "react-intl";
+// import styled from "styled-components";
 
-import { Button } from "components";
-import { Separator } from "components/Separator";
+// import { Button } from "components";
+// import { Separator } from "components/Separator";
 
 import useRouter from "hooks/useRouter";
 import { RoutePaths } from "pages/routePaths";
 import { SettingsRoute } from "pages/SettingsPage/SettingsPage";
 
-const Container = styled.div`
-  width: 100%;
-  min-height: 100vh;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  font-weight: 500;
-  font-size: 28px;
-`;
+// const Container = styled.div`
+//   width: 100%;
+//   min-height: 100vh;
+//   display: flex;
+//   flex-direction: column;
+//   align-items: center;
+//   justify-content: center;
+//   font-weight: 500;
+//   font-size: 28px;
+// `;
 
 const PaymentErrorPage: React.FC = () => {
   const { push } = useRouter();
-  return (
-    <Container>
-      <FormattedMessage id="payment.error.text" />
-      <Separator />
-      <Button
-        size="lg"
-        onClick={() => {
-          push(`/${RoutePaths.Settings}/${SettingsRoute.PlanAndBilling}`);
-        }}
-      >
-        <FormattedMessage id="payment.errorPage.btnText" />
-      </Button>
-    </Container>
-  );
+  push(`/${RoutePaths.Settings}/${SettingsRoute.PlanAndBilling}`);
+  return null;
+  // return (
+  //   <Container>
+  //     <FormattedMessage id="payment.error.text" />
+  //     <Separator />
+  //     <Button
+  //       size="lg"
+  //       onClick={() => {
+  //         push(`/${RoutePaths.Settings}/${SettingsRoute.PlanAndBilling}`);
+  //       }}
+  //     >
+  //       <FormattedMessage id="payment.errorPage.btnText" />
+  //     </Button>
+  //   </Container>
+  // );
 };
 
 export default PaymentErrorPage;
