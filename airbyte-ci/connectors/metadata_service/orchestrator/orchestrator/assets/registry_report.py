5563179782402b70fca703edb67eccb2f84c580b
Ben Church <ben@airbyte.io>
2023-04-18 13:15:11 -0700

    Dagster: rename catalog to registry (#25254)
    * rename catalog to registry in metadata service
    
    * rename catalog to registry in metadata files
    
    * Run generate models
    
    * Fix missed renames
    
    * Add github personal access token
    
    * Run black
    
    * Automated Change
    
    ---------
    
    Co-authored-by: bnchrch <bnchrch@users.noreply.github.com>

--- airbyte-ci/connectors/metadata_service/orchestrator/orchestrator/assets/registry_report.py
+++ airbyte-ci/connectors/metadata_service/orchestrator/orchestrator/assets/registry_report.py
@@ -1,9 +1,9 @@
 import pandas as pd
 from dagster import MetadataValue, Output, asset
 
-from orchestrator.templates.render import render_connector_catalog_locations_html, render_connector_catalog_locations_markdown
+from orchestrator.templates.render import render_connector_registry_locations_html, render_connector_registry_locations_markdown
 
-GROUP_NAME = "catalog_reports"
+GROUP_NAME = "registry_reports"
 
 # HELPERS
 
@@ -13,25 +13,25 @@ def augment_and_normalize_connector_dataframes(
 ):
     """
     Normalize the cloud and oss connector dataframes and merge them into a single dataframe.
-    Augment the dataframe with additional columns that indicate if the connector is in the cloud catalog, oss catalog, and if the metadata is valid.
+    Augment the dataframe with additional columns that indicate if the connector is in the cloud registry, oss registry, and if the metadata is valid.
     """
 
-    # Add a column 'is_cloud' to indicate if an image/version pair is in the cloud catalog
+    # Add a column 'is_cloud' to indicate if an image/version pair is in the cloud registry
     cloud_df["is_cloud"] = True
 
-    # Add a column 'is_oss' to indicate if an image/version pair is in the oss catalog
+    # Add a column 'is_oss' to indicate if an image/version pair is in the oss registry
     oss_df["is_oss"] = True
 
     composite_key = [primaryKey, "dockerRepository", "dockerImageTag"]
 
-    # Merge the two catalogs on the 'image' and 'version' columns, keeping only the unique pairs
-    total_catalog = pd.merge(cloud_df, oss_df, how="outer", on=composite_key).drop_duplicates(subset=composite_key)
+    # Merge the two registries on the 'image' and 'version' columns, keeping only the unique pairs
+    total_registry = pd.merge(cloud_df, oss_df, how="outer", on=composite_key).drop_duplicates(subset=composite_key)
 
     # Replace NaN values in the 'is_cloud' and 'is_oss' columns with False
-    total_catalog[["is_cloud", "is_oss"]] = total_catalog[["is_cloud", "is_oss"]].fillna(False)
+    total_registry[["is_cloud", "is_oss"]] = total_registry[["is_cloud", "is_oss"]].fillna(False)
 
-    catalog_with_metadata = pd.merge(
-        total_catalog,
+    registry_with_metadata = pd.merge(
+        total_registry,
         valid_metadata_report_dataframe[["definitionId", "is_metadata_valid"]],
         left_on=primaryKey,
         right_on="definitionId",
@@ -40,8 +40,8 @@ def augment_and_normalize_connector_dataframes(
 
     # merge with cached_specs on dockerRepository and dockerImageTag
     cached_specs["is_spec_cached"] = True
-    merged_catalog = pd.merge(
-        catalog_with_metadata,
+    merged_registry = pd.merge(
+        registry_with_metadata,
         cached_specs,
         left_on=["dockerRepository", "dockerImageTag"],
         right_on=["docker_repository", "docker_image_tag"],
@@ -49,21 +49,21 @@ def augment_and_normalize_connector_dataframes(
     )
 
     # Set connectorType to 'source' or 'destination'
-    merged_catalog["connector_type"] = connector_type
-    merged_catalog["is_source_controlled"] = merged_catalog["dockerRepository"].apply(
+    merged_registry["connector_type"] = connector_type
+    merged_registry["is_source_controlled"] = merged_registry["dockerRepository"].apply(
         lambda x: x.lstrip("airbyte/") in github_connector_folders
     )
 
-    return merged_catalog
+    return merged_registry
 
 
 # ASSETS
 
 
-@asset(required_resource_keys={"catalog_report_directory_manager"}, group_name=GROUP_NAME)
-def connector_catalog_location_html(context, all_destinations_dataframe, all_sources_dataframe):
+@asset(required_resource_keys={"registry_report_directory_manager"}, group_name=GROUP_NAME)
+def connector_registry_location_html(context, all_destinations_dataframe, all_sources_dataframe):
     """
-    Generate an HTML report of the connector catalog locations.
+    Generate an HTML report of the connector registry locations.
     """
 
     columns_to_show = [
@@ -80,13 +80,13 @@ def connector_catalog_location_html(context, all_destinations_dataframe, all_sou
     all_sources_dataframe.replace({True: "✅", False: "❌"}, inplace=True)
     all_destinations_dataframe.replace({True: "✅", False: "❌"}, inplace=True)
 
-    html = render_connector_catalog_locations_html(
+    html = render_connector_registry_locations_html(
         destinations_table=all_destinations_dataframe[columns_to_show].to_html(),
         sources_table=all_sources_dataframe[columns_to_show].to_html(),
     )
 
-    catalog_report_directory_manager = context.resources.catalog_report_directory_manager
-    file_handle = catalog_report_directory_manager.write_data(html.encode(), ext="html", key="connector_catalog_locations")
+    registry_report_directory_manager = context.resources.registry_report_directory_manager
+    file_handle = registry_report_directory_manager.write_data(html.encode(), ext="html", key="connector_registry_locations")
 
     metadata = {
         "preview": html,
@@ -96,10 +96,10 @@ def connector_catalog_location_html(context, all_destinations_dataframe, all_sou
     return Output(metadata=metadata, value=file_handle)
 
 
-@asset(required_resource_keys={"catalog_report_directory_manager"}, group_name=GROUP_NAME)
-def connector_catalog_location_markdown(context, all_destinations_dataframe, all_sources_dataframe):
+@asset(required_resource_keys={"registry_report_directory_manager"}, group_name=GROUP_NAME)
+def connector_registry_location_markdown(context, all_destinations_dataframe, all_sources_dataframe):
     """
-    Generate a markdown report of the connector catalog locations.
+    Generate a markdown report of the connector registry locations.
     """
 
     columns_to_show = [
@@ -116,13 +116,13 @@ def connector_catalog_location_markdown(context, all_destinations_dataframe, all
     all_sources_dataframe.replace({True: "✅", False: "❌"}, inplace=True)
     all_destinations_dataframe.replace({True: "✅", False: "❌"}, inplace=True)
 
-    markdown = render_connector_catalog_locations_markdown(
+    markdown = render_connector_registry_locations_markdown(
         destinations_markdown=all_destinations_dataframe[columns_to_show].to_markdown(),
         sources_markdown=all_sources_dataframe[columns_to_show].to_markdown(),
     )
 
-    catalog_report_directory_manager = context.resources.catalog_report_directory_manager
-    file_handle = catalog_report_directory_manager.write_data(markdown.encode(), ext="md", key="connector_catalog_locations")
+    registry_report_directory_manager = context.resources.registry_report_directory_manager
+    file_handle = registry_report_directory_manager.write_data(markdown.encode(), ext="md", key="connector_registry_locations")
 
     metadata = {
         "preview": MetadataValue.md(markdown),
@@ -136,7 +136,7 @@ def all_sources_dataframe(
     cloud_sources_dataframe, oss_sources_dataframe, github_connector_folders, valid_metadata_report_dataframe, cached_specs
 ) -> pd.DataFrame:
     """
-    Merge the cloud and oss sources catalogs into a single dataframe.
+    Merge the cloud and oss sources registries into a single dataframe.
     """
 
     return augment_and_normalize_connector_dataframes(
@@ -155,7 +155,7 @@ def all_destinations_dataframe(
     cloud_destinations_dataframe, oss_destinations_dataframe, github_connector_folders, valid_metadata_report_dataframe, cached_specs
 ) -> pd.DataFrame:
     """
-    Merge the cloud and oss destinations catalogs into a single dataframe.
+    Merge the cloud and oss destinations registries into a single dataframe.
     """
 
     return augment_and_normalize_connector_dataframes(
