e91bf4cc066de5e0ae47d5ed1d044e4cff679dbe
Edward Gao <edward.gao@airbyte.io>
2023-04-18 16:06:40 -0700

    fix behavior when env var not set

--- airbyte-integrations/connectors/destination-bigquery/invoke.sh
+++ airbyte-integrations/connectors/destination-bigquery/invoke.sh
@@ -3,11 +3,13 @@ set -o pipefail
 
 /airbyte/base.sh $@
 
-if test $1 = 'write' && test $NORMALIZATION_TECHNIQUE = 'LEGACY'
+if test "$1" = 'write' && test "$NORMALIZATION_TECHNIQUE" = 'LEGACY'
 then
   echo '{"type": "LOG","log":{"level":"INFO","message":"Starting in-connector normalization"}}'
   # the args in a write command are `write --catalog foo.json --config bar.json`
   # so if we remove the `write`, we can just pass the rest directly into normalization
   /airbyte/entrypoint.sh run ${@:2} --integration-type bigquery | java -cp "/airbyte/lib/*" io.airbyte.integrations.destination.normalization.NormalizationLogParser
   echo '{"type": "LOG","log":{"level":"INFO","message":"Completed in-connector normalization"}}'
+else
+  echo '{"type": "LOG","log":{"level":"INFO","message":"Skipping in-connector normalization"}}'
 fi
