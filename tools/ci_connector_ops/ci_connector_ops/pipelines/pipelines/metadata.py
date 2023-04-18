b3334b262ca047af8e6ee1f2ad12171bc9c43465
Ben Church <ben@airbyte.io>
2023-04-18 09:07:37 -0700

    Add Dagster Deployment Action skeleton (#24929)
    * Add code skeleton
    
    * Remove pr deployments
    
    * remove connectors change
    
    * Add action git ignore
    
    * Update YAML to deploy properly
    
    * Add newline at eof
    
    * Setup python-executable deploy
    
    * Add temporary script
    
    * Update readme
    
    * Remove github workflow
    
    * Add dagger pipeline for deploy
    
    * Add github action
    
    * Update readme
    
    * Remove comment
    
    * Move script into dagger pipeline
    
    * Add run test helper and skip test logic
    
    * Have the dagster orchestrator properly generate the setup.py file
    
    * Remove deploy script
    
    * Address PR comments
    
    * Update lock
    
    * Automated Commit - Formatting Changes
    
    ---------
    
    Co-authored-by: bnchrch <bnchrch@users.noreply.github.com>

--- tools/ci_connector_ops/ci_connector_ops/pipelines/pipelines/metadata.py
+++ tools/ci_connector_ops/ci_connector_ops/pipelines/pipelines/metadata.py
@@ -3,17 +3,59 @@
 #
 import uuid
 from pathlib import Path
-from typing import Optional, Set
+from typing import Optional, Set, List
 
+import asyncer
 import dagger
-from ci_connector_ops.pipelines.actions.environments import with_poetry_module
-from ci_connector_ops.pipelines.bases import Step, StepStatus, TestReport
+
+from ci_connector_ops.pipelines.actions.environments import with_poetry_module, with_python_base, with_pip_packages
+from ci_connector_ops.pipelines.bases import Step, StepStatus, TestReport, StepResult
 from ci_connector_ops.pipelines.contexts import PipelineContext
 from ci_connector_ops.pipelines.utils import DAGGER_CONFIG, METADATA_FILE_NAME, execute_concurrently
 
 METADATA_DIR = "airbyte-ci/connectors/metadata_service"
 METADATA_LIB_MODULE_PATH = "lib"
 METADATA_ORCHESTRATOR_MODULE_PATH = "orchestrator"
+
+# HELPERS
+
+
+async def run_steps(steps: List[Step | List[Step]], results: List[StepResult] = []) -> List[StepResult]:
+    # If there are no steps to run, return the results
+    if not steps:
+        return results
+
+    # If any of the previous steps failed, skip the remaining steps
+    if any(result.status == StepStatus.FAILURE for result in results):
+        skipped_results = [step.skip() for step in steps]
+        return results + skipped_results
+
+    # Pop the next step to run
+    steps_to_run, remaining_steps = steps[0], steps[1:]
+
+    # wrap the step in a list if it is not already (allows for parallel steps)
+    if not isinstance(steps_to_run, list):
+        steps_to_run = [steps_to_run]
+
+    async with asyncer.create_task_group() as task_group:
+        tasks = [task_group.soonify(step_to_run.run)() for step_to_run in steps_to_run]
+
+    new_results = [task.value for task in tasks]
+    return await run_steps(remaining_steps, results + new_results)
+
+
+def get_metadata_file_from_path(context: PipelineContext, metadata_path: Path) -> dagger.File:
+    if metadata_path.is_file() and metadata_path.name != METADATA_FILE_NAME:
+        breakpoint()
+        raise ValueError(f"The metadata file name is not {METADATA_FILE_NAME}, it is {metadata_path.name} .")
+    if metadata_path.is_dir():
+        metadata_path = metadata_path / METADATA_FILE_NAME
+    if not metadata_path.exists():
+        raise FileNotFoundError(f"{str(metadata_path)} does not exist.")
+    return context.get_repo_dir(str(metadata_path.parent), include=[METADATA_FILE_NAME]).file(METADATA_FILE_NAME)
+
+
+# STEPS
 
 
 class PoetryRun(Step):
@@ -29,17 +71,6 @@ class PoetryRun(Step):
         return await self.get_step_result(poetry_run_exec)
 
 
-def get_metadata_file_from_path(context: PipelineContext, metadata_path: Path) -> dagger.File:
-    if metadata_path.is_file() and metadata_path.name != METADATA_FILE_NAME:
-        breakpoint()
-        raise ValueError(f"The metadata file name is not {METADATA_FILE_NAME}, it is {metadata_path.name} .")
-    if metadata_path.is_dir():
-        metadata_path = metadata_path / METADATA_FILE_NAME
-    if not metadata_path.exists():
-        raise FileNotFoundError(f"{str(metadata_path)} does not exist.")
-    return context.get_repo_dir(str(metadata_path.parent), include=[METADATA_FILE_NAME]).file(METADATA_FILE_NAME)
-
-
 class MetadataValidation(PoetryRun):
     def __init__(self, context: PipelineContext, metadata_path: Path):
         title = f"Validate {metadata_path}"
@@ -53,7 +84,6 @@ class MetadataValidation(PoetryRun):
 
 
 class MetadataUpload(PoetryRun):
-
     GCS_CREDENTIALS_CONTAINER_PATH = "gcs_credentials.json"
 
     def __init__(self, context: PipelineContext, metadata_path: Path, gcs_bucket_name: str, gcs_credentials: str):
@@ -79,6 +109,58 @@ class MetadataUpload(PoetryRun):
                 self.GCS_CREDENTIALS_CONTAINER_PATH,
             ]
         )
+
+
+class DeployOrchestrator(Step):
+    title = "Deploy Metadata Orchestrator to Dagster Cloud"
+    deploy_dagster_command = [
+        "dagster-cloud",
+        "serverless",
+        "deploy-python-executable",
+        "--location-name",
+        "metadata_service_orchestrator",
+        "--location-file",
+        "dagster_cloud.yaml",
+        "--organization",
+        "airbyte-connectors",
+        "--deployment",
+        "prod",
+        "--python-version",
+        "3.9",
+    ]
+
+    async def _run(self) -> StepStatus:
+        parent_dir = self.context.get_repo_dir(METADATA_DIR)
+        python_base = with_python_base(self.context)
+        python_with_dependencies = with_pip_packages(python_base, ["dagster-cloud==1.2.6", "poetry2setup==1.1.0"])
+        dagster_cloud_api_token_secret: dagger.Secret = (
+            self.context.dagger_client.host().env_variable("DAGSTER_CLOUD_METADATA_API_TOKEN").secret()
+        )
+
+        container_to_run = (
+            python_with_dependencies.with_mounted_directory("/src", parent_dir)
+            .with_secret_variable("DAGSTER_CLOUD_API_TOKEN", dagster_cloud_api_token_secret)
+            .with_workdir(f"/src/{METADATA_ORCHESTRATOR_MODULE_PATH}")
+            .with_exec(["/bin/sh", "-c", "poetry2setup >> setup.py"])
+            .with_exec(self.deploy_dagster_command)
+        )
+        return await self.get_step_result(container_to_run)
+
+
+class TestOrchestrator(PoetryRun):
+    def __init__(self, context: PipelineContext):
+        super().__init__(
+            context=context,
+            title="Test Metadata Orchestrator",
+            parent_dir_path=METADATA_DIR,
+            module_path=METADATA_ORCHESTRATOR_MODULE_PATH,
+        )
+
+    async def _run(self) -> StepStatus:
+        return await super()._run(["pytest"])
+
+
+# PIPELINES
 
 
 async def run_metadata_validation_pipeline(
@@ -165,13 +247,8 @@ async def run_metadata_orchestrator_test_pipeline(
     async with dagger.Connection(DAGGER_CONFIG) as dagger_client:
         metadata_pipeline_context.dagger_client = dagger_client.pipeline(metadata_pipeline_context.pipeline_name)
         async with metadata_pipeline_context:
-            test_orch_step = PoetryRun(
-                context=metadata_pipeline_context,
-                title="Test Metadata Service Orchestrator",
-                parent_dir_path=METADATA_DIR,
-                module_path=METADATA_ORCHESTRATOR_MODULE_PATH,
-            )
-            result = await test_orch_step.run(["pytest"])
+            test_orch_step = TestOrchestrator(context=metadata_pipeline_context)
+            result = await test_orch_step.run()
             metadata_pipeline_context.test_report = TestReport(pipeline_context=metadata_pipeline_context, steps_results=[result])
 
     return metadata_pipeline_context.test_report.success
@@ -202,7 +279,6 @@ async def run_metadata_upload_pipeline(
         pipeline_context.dagger_client = dagger_client.pipeline(pipeline_context.pipeline_name)
 
         async with pipeline_context:
-
             results = await execute_concurrently(
                 [
                     MetadataUpload(pipeline_context, metadata_path, gcs_bucket_name, gcs_credentials).run
@@ -212,3 +288,31 @@ async def run_metadata_upload_pipeline(
             pipeline_context.test_report = TestReport(pipeline_context, results)
 
         return pipeline_context.test_report.success
+
+
+async def run_metadata_orchestrator_deploy_pipeline(
+    is_local: bool,
+    git_branch: str,
+    git_revision: str,
+    gha_workflow_run_url: Optional[str],
+    pipeline_start_timestamp: Optional[int],
+    ci_context: Optional[str],
+) -> bool:
+    metadata_pipeline_context = PipelineContext(
+        pipeline_name="Metadata Service Orchestrator Unit Test Pipeline",
+        is_local=is_local,
+        git_branch=git_branch,
+        git_revision=git_revision,
+        gha_workflow_run_url=gha_workflow_run_url,
+        pipeline_start_timestamp=pipeline_start_timestamp,
+        ci_context=ci_context,
+    )
+
+    async with dagger.Connection(DAGGER_CONFIG) as dagger_client:
+        metadata_pipeline_context.dagger_client = dagger_client.pipeline(metadata_pipeline_context.pipeline_name)
+
+        async with metadata_pipeline_context:
+            steps = [TestOrchestrator(context=metadata_pipeline_context), DeployOrchestrator(context=metadata_pipeline_context)]
+            steps_results = await run_steps(steps)
+            metadata_pipeline_context.test_report = TestReport(pipeline_context=metadata_pipeline_context, steps_results=steps_results)
+    return metadata_pipeline_context.test_report.success
