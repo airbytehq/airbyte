52588a3b462e2eb87f4cf97a267257419e14a50b
Ben Church <ben@airbyte.io>
2023-04-18 12:36:40 -0700

    Revert changes to catalog fixture

--- airbyte-ci/connectors/metadata_service/orchestrator/orchestrator/jobs/registry.py
+++ airbyte-ci/connectors/metadata_service/orchestrator/orchestrator/jobs/registry.py
@@ -6,6 +6,6 @@ registry_reports_inclusive = AssetSelection.keys("connector_registry_location_ht
 generate_registry = define_asset_job(name="generate_registry", selection=metadata_definitions_inclusive)
 generate_registry_markdown = define_asset_job(name="generate_registry_markdown", selection=registry_reports_inclusive)
 
-# TODO come and address
+# TODO Change to a Asset selection
 generate_local_metadata_files = define_asset_job(name="generate_local_metadata_files", selection=["persist_metadata_definitions"])
 generate_specs_secrets_mask_file = define_asset_job(name="generate_specs_secrets_mask_file", selection=["persist_specs_secrets_mask"])
