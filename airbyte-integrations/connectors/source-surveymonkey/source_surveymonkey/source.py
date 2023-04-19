7af780d1e256cea2ab94209feec6afffccf03bb1
marcosmarxm <marcosmarxm@gmail.com>
2023-04-19 11:46:54 -0300

    fix stream and add expected records

--- airbyte-integrations/connectors/source-surveymonkey/source_surveymonkey/source.py
+++ airbyte-integrations/connectors/source-surveymonkey/source_surveymonkey/source.py
@@ -13,7 +13,7 @@ from airbyte_cdk.sources import AbstractSource
 from airbyte_cdk.sources.streams import Stream
 from airbyte_cdk.sources.streams.http.auth import TokenAuthenticator
 
-from .streams import SurveyPages, SurveyQuestions, SurveyResponses, SurveyCollectors, Surveys
+from .streams import Collectors, SurveyCollectors, SurveyPages, SurveyQuestions, SurveyResponses, Surveys
 
 
 class SourceSurveymonkey(AbstractSource):
@@ -74,7 +74,7 @@ class SourceSurveymonkey(AbstractSource):
         start_date = pendulum.parse(config["start_date"])
         survey_ids = config.get("survey_ids", [])
         args = {"authenticator": authenticator, "start_date": start_date, "survey_ids": survey_ids}
-        return [Surveys(**args), SurveyPages(**args), SurveyQuestions(**args), SurveyResponses(**args), SurveyCollectors(**args)]
+        return [Collectors(**args), SurveyCollectors(**args), Surveys(**args), SurveyPages(**args), SurveyQuestions(**args), SurveyResponses(**args), SurveyCollectors(**args)]
 
     @staticmethod
     def get_authenticator(config: Mapping[str, Any]):
