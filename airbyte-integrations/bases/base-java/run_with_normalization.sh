5fe367423e5177cbc5d45a7e232958690921b9ab
Edward Gao <edward.gao@airbyte.io>
2023-04-20 09:49:47 -0700

    move to base-java

--- airbyte-integrations/bases/base-java/run_with_normalization.sh
+++ airbyte-integrations/bases/base-java/run_with_normalization.sh
@@ -12,7 +12,7 @@ then
   echo '{"type": "LOG","log":{"level":"INFO","message":"Starting in-connector normalization"}}'
   # the args in a write command are `write --catalog foo.json --config bar.json`
   # so if we remove the `write`, we can just pass the rest directly into normalization
-  /airbyte/entrypoint.sh run ${@:2} --integration-type bigquery | java -cp "/airbyte/lib/*" io.airbyte.integrations.destination.normalization.NormalizationLogParser
+  /airbyte/entrypoint.sh run ${@:2} --integration-type $AIRBYTE_NORMALIZATION_INTEGRATION | java -cp "/airbyte/lib/*" io.airbyte.integrations.destination.normalization.NormalizationLogParser
   normalization_exit_code=$?
   echo '{"type": "LOG","log":{"level":"INFO","message":"Completed in-connector normalization"}}'
 else
