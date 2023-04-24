d972231ce650e6f1327a595b998d791aa64c68dc
marcosmarxm <marcosmarxm@gmail.com>
2023-04-24 11:35:25 -0300

    fix unit test

--- airbyte-integrations/connectors/source-tiktok-marketing/unit_tests/streams_test.py
+++ airbyte-integrations/connectors/source-tiktok-marketing/unit_tests/streams_test.py
@@ -132,7 +132,7 @@ def test_stream_slices_report(advertiser_ids, granularity, slices_expected, pend
         (AdvertisersReports, 29),
         (CampaignsReports, 28),
         (AdvertisersAudienceReports, 6),
-        (AdsAudienceReports, 36),
+        (AdsAudienceReports, 30),
     ],
 )
 def test_basic_reports_get_metrics_day(stream, metrics_number):
