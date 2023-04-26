03fb139889aa1fd2e13ad3b76f40b0f15c71c0c0
Ben Church <ben@airbyte.io>
2023-04-26 00:49:52 -0700

    Update local definition provider

--- airbyte-config-oss/config-models-oss/src/main/java/io/airbyte/configoss/CatalogDefinitionsConfig.java
+++ airbyte-config-oss/config-models-oss/src/main/java/io/airbyte/configoss/CatalogDefinitionsConfig.java
@@ -10,10 +10,11 @@ public class CatalogDefinitionsConfig {
 
   private static final String SEED_SUBDIRECTORY = "seed/";
   private static final String ICON_SUBDIRECTORY = "icons/";
-  private static final String LOCAL_CONNECTOR_CATALOG_FILE_NAME = "oss_catalog.json";
+  private static final String LOCAL_CONNECTOR_CATALOG_FILE_NAME = "oss_registry.json";
   private static final String DEFAULT_LOCAL_CONNECTOR_CATALOG_PATH =
       SEED_SUBDIRECTORY + LOCAL_CONNECTOR_CATALOG_FILE_NAME;
 
+  // TODO ben hey! use this in the ConnectorRegistryDownloader
   public static String getLocalConnectorCatalogPath() {
     final Optional<String> customCatalogPath = new EnvConfigs().getLocalCatalogPath();
     if (customCatalogPath.isPresent()) {
