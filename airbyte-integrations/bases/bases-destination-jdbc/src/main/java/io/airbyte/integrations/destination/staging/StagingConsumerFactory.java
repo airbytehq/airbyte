be78db3b93bec4c64c0020c1bd0e0d5b18a827d7
Jeff Cowan (Airbyte) <4992320+jcowanpdx@users.noreply.github.com>
2023-04-27 08:47:02 -0700

    Clean up destination bases (#25346)
    Changes in this refactor PR
    * Use the proper interface name for the OnStartFunction
    * Use the proper interface name for the OnCloseFunction
    * Create and use a proper interface name for the FlushBufferFunction
    * Create and use a proper interface name for the BufferCreateFunction
    * Mostly naming consistency changes. These are things caught in static, compile time checks so should be low risk.
    
    ---------
    
    Co-authored-by: jcowanpdx <jcowanpdx@users.noreply.github.com>
    Co-authored-by: Octavia Squidington III <octavia-squidington-iii@users.noreply.github.com>

--- airbyte-integrations/bases/bases-destination-jdbc/src/main/java/io/airbyte/integrations/destination/staging/StagingConsumerFactory.java
+++ airbyte-integrations/bases/bases-destination-jdbc/src/main/java/io/airbyte/integrations/destination/staging/StagingConsumerFactory.java
@@ -11,8 +11,6 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.google.common.annotations.VisibleForTesting;
 import com.google.common.base.Preconditions;
 import io.airbyte.commons.exceptions.ConfigErrorException;
-import io.airbyte.commons.functional.CheckedBiConsumer;
-import io.airbyte.commons.functional.CheckedBiFunction;
 import io.airbyte.commons.json.Jsons;
 import io.airbyte.db.jdbc.JdbcDatabase;
 import io.airbyte.integrations.base.AirbyteMessageConsumer;
@@ -21,7 +19,8 @@ import io.airbyte.integrations.destination.buffered_stream_consumer.BufferedStre
 import io.airbyte.integrations.destination.buffered_stream_consumer.OnCloseFunction;
 import io.airbyte.integrations.destination.buffered_stream_consumer.OnStartFunction;
 import io.airbyte.integrations.destination.jdbc.WriteConfig;
-import io.airbyte.integrations.destination.record_buffer.SerializableBuffer;
+import io.airbyte.integrations.destination.record_buffer.BufferCreateFunction;
+import io.airbyte.integrations.destination.record_buffer.FlushBufferFunction;
 import io.airbyte.integrations.destination.record_buffer.SerializedBufferingStrategy;
 import io.airbyte.protocol.models.v0.AirbyteMessage;
 import io.airbyte.protocol.models.v0.AirbyteStream;
@@ -67,7 +66,7 @@ public class StagingConsumerFactory {
                                        final JdbcDatabase database,
                                        final StagingOperations stagingOperations,
                                        final NamingConventionTransformer namingResolver,
-                                       final CheckedBiFunction<AirbyteStreamNameNamespacePair, ConfiguredAirbyteCatalog, SerializableBuffer, Exception> onCreateBuffer,
+                                       final BufferCreateFunction onCreateBuffer,
                                        final JsonNode config,
                                        final ConfiguredAirbyteCatalog catalog,
                                        final boolean purgeStagingData) {
@@ -183,11 +182,11 @@ public class StagingConsumerFactory {
    * @return
    */
   @VisibleForTesting
-  CheckedBiConsumer<AirbyteStreamNameNamespacePair, SerializableBuffer, Exception> flushBufferFunction(
-                                                                                                       final JdbcDatabase database,
-                                                                                                       final StagingOperations stagingOperations,
-                                                                                                       final List<WriteConfig> writeConfigs,
-                                                                                                       final ConfiguredAirbyteCatalog catalog) {
+  FlushBufferFunction flushBufferFunction(
+                                          final JdbcDatabase database,
+                                          final StagingOperations stagingOperations,
+                                          final List<WriteConfig> writeConfigs,
+                                          final ConfiguredAirbyteCatalog catalog) {
     // TODO: (ryankfu) move this block of code that executes before the lambda to #onStartFunction
     final Set<WriteConfig> conflictingStreams = new HashSet<>();
     final Map<AirbyteStreamNameNamespacePair, WriteConfig> pairToWriteConfig = new HashMap<>();
