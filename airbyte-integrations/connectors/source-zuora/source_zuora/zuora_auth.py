9601171b18cacc3f488547810c93ebd14d6c36c7
Frans Krojegård <frans.krojegard@bonniernews.se>
2023-04-27 16:24:14 +0200

    As suggested by @jetvp, this fixes the issue with OAuth.

--- airbyte-integrations/connectors/source-zuora/source_zuora/zuora_auth.py
+++ airbyte-integrations/connectors/source-zuora/source_zuora/zuora_auth.py
@@ -14,12 +14,10 @@ class OAuth(Oauth2Authenticator):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
-    def get_refresh_request_body(self) -> Mapping[str, Any]:
-        payload = super().get_refresh_request_body()
-        payload["grant_type"] = "client_credentials"
+    def build_refresh_request_body(self) -> Mapping[str, Any]:
+        payload = super().build_refresh_request_body()
         payload.pop("refresh_token")  # Zuora doesn't have Refresh Token parameter
         return payload
-
 
 class ZuoraAuthenticator:
     def __init__(self, config: Dict):
@@ -34,5 +32,6 @@ class ZuoraAuthenticator:
             token_refresh_endpoint=f"{self.url_base}/oauth/token",
             client_id=self.config["client_id"],
             client_secret=self.config["client_secret"],
-            refresh_token=None,  # Zuora doesn't have Refresh Token parameter
+            refresh_token='refresh_token',
+            grant_type='client_credentials'
         )
