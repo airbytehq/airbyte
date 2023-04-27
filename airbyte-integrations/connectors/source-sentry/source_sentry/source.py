50283cd846baf75e73f5ab14b01f95901a47659c
Arsen Losenko <20901439+arsenlosenko@users.noreply.github.com>
2023-04-27 14:50:09 +0300

    Source Sentry: Add validation for project's and organization's name during connector setup

--- airbyte-integrations/connectors/source-sentry/source_sentry/source.py
+++ airbyte-integrations/connectors/source-sentry/source_sentry/source.py
@@ -9,6 +9,7 @@ from airbyte_cdk.models import SyncMode
 from airbyte_cdk.sources import AbstractSource
 from airbyte_cdk.sources.streams import Stream
 from airbyte_cdk.sources.streams.http.auth import TokenAuthenticator
+from requests.exceptions import HTTPError
 
 from .streams import Events, Issues, ProjectDetail, Projects, Releases
 
@@ -17,14 +18,19 @@ from .streams import Events, Issues, ProjectDetail, Projects, Releases
 class SourceSentry(AbstractSource):
     def check_connection(self, logger, config) -> Tuple[bool, Any]:
         try:
-            projects_stream = Projects(
+            events_stream = Events(
                 authenticator=TokenAuthenticator(token=config["auth_token"]),
                 hostname=config.get("hostname"),
+                organization=config.get("organization"),
+                project=config.get("project")
             )
-            next(projects_stream.read_records(sync_mode=SyncMode.full_refresh))
+            next(events_stream.read_records(sync_mode=SyncMode.full_refresh))
             return True, None
+        except HTTPError as e:
+            return False, "Check failed: either project's or organization's name is invalid"
         except Exception as e:
             return False, e
+
 
     def streams(self, config: Mapping[str, Any]) -> List[Stream]:
         stream_args = {
