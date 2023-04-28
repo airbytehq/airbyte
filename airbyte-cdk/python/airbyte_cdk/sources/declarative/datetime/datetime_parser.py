4ac44ab8600cbb623489dc4dc976ade97fc45a69
maxi297 <maxime@airbyte.io>
2023-04-28 08:59:55 -0400

    [ISSUE #25646] removing timezone for DatetimeParser interface

--- airbyte-cdk/python/airbyte_cdk/sources/declarative/datetime/datetime_parser.py
+++ airbyte-cdk/python/airbyte_cdk/sources/declarative/datetime/datetime_parser.py
@@ -16,7 +16,7 @@ class DatetimeParser:
     Instead of using the directive directly, we can use datetime.fromtimestamp and dt.timestamp()
     """
 
-    def parse(self, date: Union[str, int], format: str, timezone):
+    def parse(self, date: Union[str, int], format: str):
         # "%s" is a valid (but unreliable) directive for formatting, but not for parsing
         # It is defined as
         # The number of seconds since the Epoch, 1970-01-01 00:00:00+0000 (UTC). https://man7.org/linux/man-pages/man3/strptime.3.html
@@ -24,11 +24,11 @@ class DatetimeParser:
         # The recommended way to parse a date from its timestamp representation is to use datetime.fromtimestamp
         # See https://stackoverflow.com/a/4974930
         if format == "%s":
-            return datetime.datetime.fromtimestamp(int(date), tz=timezone)
+            return datetime.datetime.fromtimestamp(int(date), tz=datetime.timezone.utc)
 
         parsed_datetime = datetime.datetime.strptime(str(date), format)
         if self._is_naive(parsed_datetime):
-            return parsed_datetime.replace(tzinfo=timezone)
+            return parsed_datetime.replace(tzinfo=datetime.timezone.utc)
         return parsed_datetime
 
     def format(self, dt: datetime.datetime, format: str) -> str:
