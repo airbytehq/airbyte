eec26870676ef179607bbce4caa3fe7a52b87c89
Sergey Chvalyuk <grubberr@gmail.com>
2023-05-07 14:02:50 +0000

    recover utils.py
    Signed-off-by: Sergey Chvalyuk <grubberr@gmail.com>

--- airbyte-integrations/connectors/source-github/source_github/utils.py
+++ airbyte-integrations/connectors/source-github/source_github/utils.py
@@ -2,7 +2,6 @@
 # Copyright (c) 2023 Airbyte, Inc., all rights reserved.
 #
 
-from time import time
 
 from airbyte_cdk.models import SyncMode
 from airbyte_cdk.sources.streams import Stream
@@ -25,41 +24,3 @@ def read_full_refresh(stream_instance: Stream):
         records = stream_instance.read_records(stream_slice=_slice, sync_mode=SyncMode.full_refresh)
         for record in records:
             yield record
-
-
-class TokenBucket:
-    """An implementation of the token bucket algorithm.
-
-    >>> bucket = TokenBucket(80, 0.5)
-    >>> print(bucket.consume(10))
-    True
-    >>> print(bucket.consume(90))
-    False
-    """
-
-    def __init__(self, tokens, fill_rate):
-        """tokens is the total tokens in the bucket. fill_rate is the
-        rate in tokens/second that the bucket will be refilled."""
-        self.capacity = float(tokens)
-        self._tokens = float(tokens)
-        self.fill_rate = float(fill_rate)
-        self.timestamp = time()
-
-    def consume(self, tokens):
-        """Consume tokens from the bucket. Returns True if there were
-        sufficient tokens otherwise False."""
-        if tokens <= self.tokens:
-            self._tokens -= tokens
-        else:
-            return False
-        return True
-
-    def get_tokens(self):
-        if self._tokens < self.capacity:
-            now = time()
-            delta = self.fill_rate * (now - self.timestamp)
-            self._tokens = min(self.capacity, self._tokens + delta)
-            self.timestamp = now
-        return self._tokens
-
-    tokens = property(get_tokens)
