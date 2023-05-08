703860f3f6da180f85558a5f6d49f14e005a2eac
Sergey Chvalyuk <grubberr@gmail.com>
2023-05-08 15:55:54 +0000

    class AttributionReport re-implement using slices
    Signed-off-by: Sergey Chvalyuk <grubberr@gmail.com>

--- airbyte-integrations/connectors/source-amazon-ads/unit_tests/test_attribution_report.py
+++ airbyte-integrations/connectors/source-amazon-ads/unit_tests/test_attribution_report.py
@@ -11,6 +11,8 @@ from airbyte_cdk.models import SyncMode
 from jsonschema import validate
 from source_amazon_ads import SourceAmazonAds
 
+from .utils import read_full_refresh
+
 
 def setup_responses(
     profiles_response=None,
@@ -33,11 +35,6 @@ def setup_responses(
             "https://advertising-api.amazon.com/attribution/report",
             body=attribution_report_response,
         )
-
-
-def get_all_stream_records(stream):
-    records = stream.read_records(SyncMode.full_refresh)
-    return [r for r in records]
 
 
 def get_stream_by_name(streams, stream_name):
@@ -70,8 +67,8 @@ def test_attribution_report_schema(config, profiles_response, attribution_report
     attribution_report_stream = get_stream_by_name(streams, stream_name)
     schema = attribution_report_stream.get_json_schema()
 
-    profile_records = get_all_stream_records(profile_stream)
-    attribution_records = get_all_stream_records(attribution_report_stream)
+    profile_records = list(read_full_refresh(profile_stream))
+    attribution_records = list(read_full_refresh(attribution_report_stream))
     assert len(attribution_records) == len(profile_records) * len(json.loads(attribution_report_response(report_type)).get("reports"))
 
     for record in attribution_records:
@@ -118,31 +115,7 @@ def test_attribution_report_with_pagination(mocker, config, profiles_response, a
         callback=_callback,
     )
 
-    attribution_records = get_all_stream_records(attribution_report_stream)
+    attribution_records = list(read_full_refresh(attribution_report_stream))
 
     # request should be called 2 times for a single profile
     assert len(attribution_records) == 2 * len(attribution_data.get("reports"))
-
-
-@pytest.mark.parametrize(
-    ("stream_name", "report_type"),
-    [
-        ("attribution_report_products", "PRODUCTS"),
-        ("attribution_report_performance_adgroup", "PERFORMANCE_ADGROUP"),
-        ("attribution_report_performance_campaign", "PERFORMANCE_CAMPAIGN"),
-        ("attribution_report_performance_creative", "PERFORMANCE_CREATIVE"),
-    ],
-)
-@responses.activate
-def test_attribution_report_no_data(config, profiles_response, attribution_report_bad_response, stream_name, report_type):
-    # custom start date
-    config["start_date"] = "2022-09-03"
-
-    setup_responses(profiles_response=profiles_response, attribution_report_response=attribution_report_bad_response)
-
-    source = SourceAmazonAds()
-    streams = source.streams(config)
-
-    attribution_report_stream = get_stream_by_name(streams, stream_name)
-    attribution_records = get_all_stream_records(attribution_report_stream)
-    assert len(attribution_records) == 0
