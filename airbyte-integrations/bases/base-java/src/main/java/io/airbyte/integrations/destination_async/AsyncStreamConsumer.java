1800d98fe821996c9a7b6b3759b54593d38110c5
cgardens <charles@airbyte.io>
2023-05-15 15:56:11 -0700

    crib what we need from buffereed stream consumer, excluding state manager

--- airbyte-integrations/bases/base-java/src/main/java/io/airbyte/integrations/destination_async/AsyncStreamConsumer.java
+++ airbyte-integrations/bases/base-java/src/main/java/io/airbyte/integrations/destination_async/AsyncStreamConsumer.java
@@ -4,16 +4,24 @@
 
 package io.airbyte.integrations.destination_async;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.google.common.base.Preconditions;
+import io.airbyte.commons.functional.CheckedFunction;
+import io.airbyte.commons.json.Jsons;
 import io.airbyte.integrations.base.AirbyteMessageConsumer;
+import io.airbyte.integrations.destination.buffered_stream_consumer.OnStartFunction;
 import io.airbyte.integrations.destination.buffered_stream_consumer.RecordSizeEstimator;
 import io.airbyte.protocol.models.v0.AirbyteMessage;
 import io.airbyte.protocol.models.v0.AirbyteMessage.Type;
 import io.airbyte.protocol.models.v0.AirbyteStateMessage.AirbyteStateType;
+import io.airbyte.protocol.models.v0.ConfiguredAirbyteCatalog;
 import io.airbyte.protocol.models.v0.StreamDescriptor;
 import java.time.Instant;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
+import java.util.Set;
+import java.util.function.Consumer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -22,21 +30,53 @@ public class AsyncStreamConsumer implements AirbyteMessageConsumer {
   private static final Logger LOGGER = LoggerFactory.getLogger(AsyncStreamConsumer.class);
 
   private static final String NON_STREAM_STATE_IDENTIFIER = "GLOBAL";
+  private final Consumer<AirbyteMessage> outputRecordCollector;
+  private final OnStartFunction onStart;
+  private final OnCloseFunction2 onClose;
+  private final ConfiguredAirbyteCatalog catalog;
+  private final CheckedFunction<JsonNode, Boolean, Exception> isValidRecord;
   private final BufferManagerEnqueue bufferManagerEnqueue;
   private final UploadWorkers uploadWorkers;
+  private final Set<StreamDescriptor> streamNames;
+  private final IgnoredRecordsTracker ignoredRecordsTracker;
 
-  public AsyncStreamConsumer(final BufferManager bufferManager) {
+  private boolean hasStarted;
+  private boolean hasClosed;
+
+  public AsyncStreamConsumer(final Consumer<AirbyteMessage> outputRecordCollector,
+                             final OnStartFunction onStart,
+                             final OnCloseFunction2 onClose,
+                             final ConfiguredAirbyteCatalog catalog,
+                             final CheckedFunction<JsonNode, Boolean, Exception> isValidRecord,
+                             final BufferManager bufferManager) {
+    hasStarted = false;
+    hasClosed = false;
+
+    this.outputRecordCollector = outputRecordCollector;
+    this.onStart = onStart;
+    this.onClose = onClose;
+    this.catalog = catalog;
+    this.isValidRecord = isValidRecord;
     bufferManagerEnqueue = bufferManager.getBufferManagerEnqueue();
     uploadWorkers = new UploadWorkers(bufferManager.getBufferManagerDequeue());
+    streamNames = StreamDescriptorUtils.fromConfiguredCatalog(catalog);
+    ignoredRecordsTracker = new IgnoredRecordsTracker();
   }
 
   @Override
   public void start() throws Exception {
+    Preconditions.checkState(!hasStarted, "Consumer has already been started.");
+    hasStarted = true;
+
     uploadWorkers.start();
+
+    LOGGER.info("{} started.", AsyncStreamConsumer.class);
+    onStart.call();
   }
 
   @Override
   public void accept(final AirbyteMessage message) throws Exception {
+    Preconditions.checkState(hasStarted, "Cannot accept records until consumer has started");
     /*
      * intentionally putting extractStream outside the buffer manager so that if in the future we want
      * to try to use a threadpool to partial deserialize to get record type and stream name, we can do
@@ -48,8 +88,15 @@ public class AsyncStreamConsumer implements AirbyteMessageConsumer {
 
   @Override
   public void close() throws Exception {
+    Preconditions.checkState(hasStarted, "Cannot close; has not started.");
+    Preconditions.checkState(!hasClosed, "Has already closed.");
+    hasClosed = true;
+
     // assume the closing upload workers will flush all accepted records.
+    onClose.call();
     uploadWorkers.close();
+    ignoredRecordsTracker.report();
+    LOGGER.info("{} closed.", AsyncStreamConsumer.class);
   }
 
   // todo (cgardens) - handle global state.
@@ -61,9 +108,15 @@ public class AsyncStreamConsumer implements AirbyteMessageConsumer {
    * @return stream descriptor if the message is a record or state, otherwise empty. In the case of
    *         global state messages the stream descriptor is hardcoded
    */
-  private static Optional<StreamDescriptor> extractStream(final AirbyteMessage message) {
+  private Optional<StreamDescriptor> extractStream(final AirbyteMessage message) {
     if (message.getType() == Type.RECORD) {
-      return Optional.of(new StreamDescriptor().withNamespace(message.getRecord().getNamespace()).withName(message.getRecord().getStream()));
+      final StreamDescriptor streamDescriptor = new StreamDescriptor()
+          .withNamespace(message.getRecord().getNamespace())
+          .withName(message.getRecord().getStream());
+
+      validateRecord(message, streamDescriptor);
+
+      return Optional.of(streamDescriptor);
     } else if (message.getType() == Type.STATE) {
       if (message.getState().getType() == AirbyteStateType.STREAM) {
         return Optional.of(message.getState().getStream().getStreamDescriptor());
@@ -75,7 +128,35 @@ public class AsyncStreamConsumer implements AirbyteMessageConsumer {
     }
   }
 
-  static class BufferManager {
+  private void validateRecord(final AirbyteMessage message, final StreamDescriptor streamDescriptor) {
+    // if stream is not part of list of streams to sync to then throw invalid stream exception
+    if (!streamNames.contains(streamDescriptor)) {
+      throwUnrecognizedStream(catalog, message);
+    }
+
+    trackerIsValidRecord(message, streamDescriptor);
+  }
+
+  private void trackerIsValidRecord(final AirbyteMessage message, final StreamDescriptor streamDescriptor) {
+    // todo (cgardens) - is valid should also move inside the tracker, but don't want to blow up more
+    // constructors right now.
+    try {
+
+      if (!isValidRecord.apply(message.getRecord().getData())) {
+        ignoredRecordsTracker.addRecord(streamDescriptor, message);
+      }
+    } catch (final Exception e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  private static void throwUnrecognizedStream(final ConfiguredAirbyteCatalog catalog, final AirbyteMessage message) {
+    throw new IllegalArgumentException(
+        String.format("Message contained record from a stream that was not in the catalog. \ncatalog: %s , \nmessage: %s",
+            Jsons.serialize(catalog), Jsons.serialize(message)));
+  }
+
+  public static class BufferManager {
 
     Map<StreamDescriptor, MemoryBoundedLinkedBlockingQueue<AirbyteMessage>> buffers;
 
