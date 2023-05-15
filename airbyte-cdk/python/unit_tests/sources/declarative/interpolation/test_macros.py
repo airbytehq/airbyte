b2541e6ba0c98ee29409e5cbcd1f04cc203b2eae
Alexandre Girard <alexandre@airbyte.io>
2023-05-15 15:05:52 -0700

    Update test

--- airbyte-cdk/python/unit_tests/sources/declarative/interpolation/test_macros.py
+++ airbyte-cdk/python/unit_tests/sources/declarative/interpolation/test_macros.py
@@ -36,7 +36,7 @@ def test_macros_export(test_name, fn_name, found_in_macros):
     ("test_datetime_string_with_tz_to_datetime", "2022-01-01T01:01:01-0800", "%Y-%m-%dT%H:%M:%SZ", "2022-01-01T09:01:01Z"),
     ("test_datetime_object_tz_to_date", datetime.datetime(2022, 1, 1, 1, 1, 1), "%Y-%m-%d", "2022-01-01"),
     ("test_datetime_object_tz_to_datetime", datetime.datetime(2022, 1, 1, 1, 1, 1), "%Y-%m-%dT%H:%M:%SZ", "2022-01-01T01:01:01Z"),
-    ("test_format_to_timestamp", datetime.datetime(2022, 1, 1, 1, 1, 1), "%s", "1641027661"),
+    ("test_format_to_timestamp", "2022-01-01T01:01:01Z", "%s", "1641027661"),
 ])
 def test_format_datetime(test_name, input_value, format, expected_output):
     format_datetime = macros["format_datetime"]
