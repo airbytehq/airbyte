b5f5367f3616c4dbf9ec6d29f591df427d8970f3
Daryna Ishchenko <darina.ishchenko17@gmail.com>
2023-05-15 18:43:38 +0300

    added AirbyteTracedException

--- airbyte-integrations/connectors/source-github/source_github/source.py
+++ airbyte-integrations/connectors/source-github/source_github/source.py
@@ -102,12 +102,14 @@ class SourceGithub(AbstractSource):
         """
         config_repositories = SourceGithub._get_and_prepare_repositories_config(config)
         if not SourceGithub._is_repositories_config_valid(config_repositories):
-            raise Exception(
-                f"You provided invalid format of repositories config: {' ' .join(config_repositories)}."
+            user_message = (
+                f"You provided invalid format of repositories config: {' '.join(config_repositories)}."
                 "Repositories with the wrong name, or repositories that do not exist, or have the wrong name format, "
                 "or do not belong to your account are not allowed."
                 f" Valid examples: airbytehq/airbyte airbytehq/another-repo airbytehq/* airbytehq/airbyte"
             )
+            message = "You provided invalid format of repositories config: {' '.join(config_repositories)}."
+            raise AirbyteTracedException(internal_message=message, message=user_message, failure_type=FailureType.config_error)
 
         if not config_repositories:
             raise Exception("Field `repository` required to be provided for connect to Github API")
@@ -236,7 +238,15 @@ class SourceGithub(AbstractSource):
                 raise e
 
         if not any((organizations, repositories)):
-            raise Exception("No streams available. Please check permissions")
+            user_message = (
+                "No streams available. Looks like your config for repositories or organizations is not valid."
+                " Please, check your permissions, names of repositories and organizations."
+            )
+            raise AirbyteTracedException(
+                internal_message="No streams available. Please check permissions",
+                message=user_message,
+                failure_type=FailureType.config_error,
+            )
 
         page_size = config.get("page_size_for_large_streams", DEFAULT_PAGE_SIZE_FOR_LARGE_STREAM)
 
