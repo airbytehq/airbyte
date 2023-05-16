7def9f5764373b0f5ed99ab23b9342de4d43c12b
Rodi Reich Zilberman <867491+rodireich@users.noreply.github.com>
2023-05-15 23:37:00 -0700

    destination harness (#25030)
    * destination harness
    
    * Automated Commit - Formatting Changes
    
    * refactor to support multiple connectors
    
    * better byte counting
    
    * Automated Change
    
    * fix main cp
    
    * harness type
    
    * harness type
    
    * harness type
    
    * harness type
    
    * harness type
    
    * harness type
    
    * harness type
    
    * speed up harness build
    
    * harness type
    
    * harness type
    
    * harness type
    
    * harness type
    
    * 1m catalog
    
    * 1m catalog
    
    * Automated Commit - Formatting Changes
    
    * 1m catalog
    
    * Automated Commit - Formatting Changes
    
    * 1m catalog
    
    * Automated Commit - Formatting Changes
    
    * 1m catalog
    
    * 1m catalog
    
    * Automated Commit - Formatting Changes
    
    * 1m catalog
    
    * Automated Commit - Formatting Changes
    
    * Automated Change
    
    * add some additional logging for debugging
    
    * test
    
    * test
    
    * debugging code
    
    * Automated Commit - Formatting Changes
    
    * debugging code
    
    * debugging code
    
    * debugging code
    
    * Automated Commit - Formatting Changes
    
    * Checkpoint: This is working!.
    
    * Automated Commit - Formatting Changes
    
    * Updates plumbing for Snowflake with static records - Working
    
    * Automated Commit - Formatting Changes
    
    * working 10m
    
    * Automated Commit - Formatting Changes
    
    * add destination datasource
    
    * Automated Commit - Formatting Changes
    
    * sanity
    
    * sanity
    
    * Delete 1m_catalog.json
    
    * sanity
    
    * Update airbyte-integrations/connectors-performance/destination-harness/README.md
    
    Co-authored-by: Davin Chia <davinchia@gmail.com>
    
    * sanity
    
    * Update airbyte-integrations/connectors/destination-e2e-test/src/main/java/io/airbyte/integrations/destination/e2e_test/TestingDestinations.java
    
    Co-authored-by: Davin Chia <davinchia@gmail.com>
    
    * Splits performance harness into separated methods
    
    * Automated Commit - Formatting Changes
    
    * Updates test description to force Github builds
    
    ---------
    
    Co-authored-by: rodireich <rodireich@users.noreply.github.com>
    Co-authored-by: Jeff Cowan (Airbyte) <4992320+jcowanpdx@users.noreply.github.com>
    Co-authored-by: Davin Chia <davinchia@gmail.com>
    Co-authored-by: davinchia <davinchia@users.noreply.github.com>
    Co-authored-by: ryankfu <ryan.fu@airbyte.io>
    Co-authored-by: ryankfu <ryankfu@users.noreply.github.com>

--- airbyte-integrations/connectors-performance/source-harness/src/main/java/io/airbyte/integrations/source_performance/Main.java
+++ airbyte-integrations/connectors-performance/source-harness/src/main/java/io/airbyte/integrations/source_performance/Main.java
@@ -2,7 +2,7 @@
  * Copyright (c) 2023 Airbyte, Inc., all rights reserved.
  */
 
-package io.airbyte.integrations.performance;
+package io.airbyte.integrations.source_performance;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
