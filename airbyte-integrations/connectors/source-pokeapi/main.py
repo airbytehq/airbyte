d994ba52a7bd2350ea7f20a0c4bb4ff79bcc5f36
Ben Church <ben@airbyte.io>
2023-05-16 11:45:08 -0700

    Add comment

--- airbyte-integrations/connectors/source-pokeapi/main.py
+++ airbyte-integrations/connectors/source-pokeapi/main.py
@@ -7,6 +7,7 @@ import sys
 from airbyte_cdk.entrypoint import launch
 from source_pokeapi import SourcePokeapi
 
+#  A comment that should not be merged
 if __name__ == "__main__":
     source = SourcePokeapi()
     launch(source, sys.argv[1:])
