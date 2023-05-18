4b217e7678b0d900539f8416473691ecb84dfa83
cgardens <charles@airbyte.io>
2023-05-17 17:51:04 -0700

    use set instead of list for message type look ups

--- airbyte-connector-test-harnesses/acceptance-test-harness/src/main/java/io/airbyte/workers/internal/DefaultAirbyteDestination.java
+++ airbyte-connector-test-harnesses/acceptance-test-harness/src/main/java/io/airbyte/workers/internal/DefaultAirbyteDestination.java
@@ -92,7 +92,7 @@ public class DefaultAirbyteDestination implements AirbyteDestination {
 
     writer = messageWriterFactory.createWriter(new BufferedWriter(new OutputStreamWriter(destinationProcess.getOutputStream(), Charsets.UTF_8)));
 
-    final List<Type> acceptedMessageTypes = List.of(Type.STATE, Type.TRACE, Type.CONTROL);
+    final Set<Type> acceptedMessageTypes = Set.of(Type.STATE, Type.TRACE, Type.CONTROL);
     messageIterator = streamFactory.create(IOs.newBufferedReader(destinationProcess.getInputStream()))
         .filter(message -> acceptedMessageTypes.contains(message.getType()))
         .iterator();
