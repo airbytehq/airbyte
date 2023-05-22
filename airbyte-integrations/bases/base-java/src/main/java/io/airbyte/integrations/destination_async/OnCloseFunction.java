cf3f7d72a57a7e203405684c9c5ba9035658ca9b
Davin Chia <davinchia@gmail.com>
2023-05-22 13:07:07 -0700

    Better javadocs.

--- airbyte-integrations/bases/base-java/src/main/java/io/airbyte/integrations/destination_async/OnCloseFunction.java
+++ airbyte-integrations/bases/base-java/src/main/java/io/airbyte/integrations/destination_async/OnCloseFunction.java
@@ -6,4 +6,9 @@ package io.airbyte.integrations.destination_async;
 
 import io.airbyte.commons.concurrency.VoidCallable;
 
+/**
+ * Async version of
+ * {@link io.airbyte.integrations.destination.buffered_stream_consumer.OnCloseFunction}. Separately
+ * out for easier versioning.
+ */
 public interface OnCloseFunction extends VoidCallable {}
