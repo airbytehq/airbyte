08db1ab49c43848f1067bf3945a506d8358c7c8b
Davin Chia <davinchia@gmail.com>
2023-05-22 13:03:58 -0700

    Format.

---
+++ airbyte-integrations/bases/base-java/src/main/java/io/airbyte/integrations/destination_async/OnCloseFunction.java
@@ -0,0 +1,9 @@
+/*
+ * Copyright (c) 2023 Airbyte, Inc., all rights reserved.
+ */
+
+package io.airbyte.integrations.destination_async;
+
+import io.airbyte.commons.concurrency.VoidCallable;
+
+public interface OnCloseFunction extends VoidCallable {}
