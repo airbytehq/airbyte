69bd869df338adf3336945f854ef5fa8f049be1f
Ryan Fu <ryan.fu@airbyte.io>
2023-05-22 12:13:38 -0700

    Performance Harness: Support for pseudo-parallel streams (#26219)
    * Adds support for pseudo-parallel datasets
    
    * Ran ./gradlew :spotlessJavaApply
    
    * Automated Change
    
    * Fixes issue with parallel datasets credentials
    
    * Fixes filter for parallel credentials
    
    * Adds a new configurable property  to build a pseudo-parallel catalog
    
    * Fixes Github Actions variable to be processed properly with the K8s harness yaml
    
    * Adds unit test for random streams and generating streams within the same configured catalog
    
    * Ran ./gradlew :spotlessJavaApply
    
    * Added additional description for GitHub Actions
    
    * Update connector-performance-command.yml
    
    Moved text up to connect with other argument discussion
    
    * Fixes spotBugs issue
    
    * Automated Commit - Formatting Changes
    
    * Update GitHub Action description
    
    ---------
    
    Co-authored-by: ryankfu <ryankfu@users.noreply.github.com>
    Co-authored-by: Rodi Reich Zilberman <867491+rodireich@users.noreply.github.com>

--- airbyte-integrations/connectors-performance/destination-harness/src/main/java/io/airbyte/integrations/destination_performance/PerformanceHarness.java
+++ airbyte-integrations/connectors-performance/destination-harness/src/main/java/io/airbyte/integrations/destination_performance/PerformanceHarness.java
@@ -9,6 +9,7 @@ import static java.lang.Thread.sleep;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.common.annotations.VisibleForTesting;
 import io.airbyte.commons.features.EnvVariableFeatureFlags;
 import io.airbyte.commons.json.Jsons;
 import io.airbyte.config.AllowedHosts;
@@ -44,6 +45,7 @@ import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Optional;
+import java.util.Random;
 import java.util.Set;
 import java.util.concurrent.CompletableFuture;
 import java.util.regex.Pattern;
@@ -56,7 +58,7 @@ import lombok.extern.slf4j.Slf4j;
  * sending AirbyteRecordMessages the same way platform pipes data into the destination
  */
 @Slf4j
-public class PerformanceTest {
+public class PerformanceHarness {
 
   public static final int PORT1 = 9877;
   public static final int PORT2 = 9878;
@@ -73,7 +75,7 @@ public class PerformanceTest {
 
   private DefaultAirbyteDestination destination;
 
-  PerformanceTest(final String imageName, final String config, final String catalog, final String datasource) throws JsonProcessingException {
+  PerformanceHarness(final String imageName, final String config, final String catalog, final String datasource) throws JsonProcessingException {
     final ObjectMapper mapper = new ObjectMapper();
     this.imageName = imageName;
     this.config = mapper.readTree(config);
@@ -95,6 +97,8 @@ public class PerformanceTest {
    * @throws Exception
    */
   void runTest() throws Exception {
+    final List<String> streamNames = catalog.getStreams().stream().map(stream -> stream.getStream().getName()).toList();
+    final Random random = new Random();
     final AirbyteIntegrationLauncher dstIntegtationLauncher = getAirbyteIntegrationLauncher();
     final WorkerDestinationConfig dstConfig = new WorkerDestinationConfig()
         .withDestinationConnectionConfiguration(this.config)
@@ -149,7 +153,7 @@ public class PerformanceTest {
         final AirbyteMessage airbyteMessage = new AirbyteMessage()
             .withType(Type.RECORD)
             .withRecord(new AirbyteRecordMessage()
-                .withStream(catalog.getStreams().get(0).getStream().getName())
+                .withStream(getStreamName(streamNames, random))
                 .withNamespace(catalog.getStreams().get(0).getStream().getNamespace())
                 .withData(Jsons.deserialize(recordString)));
         airbyteMessage.getRecord().setEmittedAt(start);
@@ -172,6 +176,13 @@ public class PerformanceTest {
     logListener.cancel(true);
     log.info("Test ended successfully");
     computeThroughput(totalBytes, counter, start);
+    // TODO: (ryankfu) when incremental syncs are supported, add a tearDown method to clear table
+  }
+
+  // TODO: (ryankfu) get less hacky way to generate multiple streams
+  @VisibleForTesting
+  static String getStreamName(final List<String> listOfStreamNames, final Random random) {
+    return listOfStreamNames.get(random.nextInt(listOfStreamNames.size()));
   }
 
   private void computeThroughput(final double totalBytes, final long counter, final long start) {
