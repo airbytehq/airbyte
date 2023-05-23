45263dd8216b51e52ec186cdc9bbd408732263ce
Davin Chia <davinchia@gmail.com>
2023-05-23 15:35:41 -0700

    Inject new async consumer into Snowflake staging.

--- airbyte-integrations/bases/base-java/src/main/java/io/airbyte/integrations/destination_async/OnCloseFunction.java
+++ airbyte-integrations/bases/base-java/src/main/java/io/airbyte/integrations/destination_async/OnCloseFunction.java
@@ -4,11 +4,14 @@
 
 package io.airbyte.integrations.destination_async;
 
-import io.airbyte.commons.concurrency.VoidCallable;
+import io.airbyte.commons.functional.CheckedConsumer;
 
 /**
  * Async version of
  * {@link io.airbyte.integrations.destination.buffered_stream_consumer.OnCloseFunction}. Separately
  * out for easier versioning.
  */
-public interface OnCloseFunction extends VoidCallable {}
+public interface OnCloseFunction extends CheckedConsumer<Boolean, Exception> {
+    @Override
+    void accept(Boolean hasFailed) throws Exception;
+}
