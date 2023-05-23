46861ce8631ce2afa731d059f28bb912324baa50
cgardens <charles@airbyte.io>
2023-05-23 13:36:33 -0700

    add tests

--- airbyte-integrations/bases/base-java/src/test/java/io/airbyte/integrations/destination_async/state/AsyncStateManagerTest.java
+++ airbyte-integrations/bases/base-java/src/test/java/io/airbyte/integrations/destination_async/state/AsyncStateManagerTest.java
@@ -83,23 +83,51 @@ class AsyncStateManagerTest {
     }
 
     stateManager.claim(STREAM2_DESC, 1500);
-    // todo (cgardens) - the fact that i have to do this shows something is leaking.
-    for (int i = 0; i < 500; i++) {
-      buffers.get(STREAM2_DESC).take();
-    }
+    drainNRecords(buffers, STREAM2_DESC, 500);
     assertEquals(Optional.empty(), stateManager.fulfill(STREAM2_DESC, 1500));
 
     stateManager.claim(STREAM1_DESC, 900);
-    for (int i = 0; i < 900; i++) {
-      buffers.get(STREAM1_DESC).take();
-    }
+    drainNRecords(buffers, STREAM1_DESC, 900);
     assertEquals(Optional.empty(), stateManager.fulfill(STREAM1_DESC, 900));
 
     stateManager.claim(STREAM1_DESC, 1000);
-    for (int i = 0; i < 100; i++) {
-      buffers.get(STREAM1_DESC).take();
-    }
+    drainNRecords(buffers, STREAM1_DESC, 100);
     assertEquals(Optional.of(GLOBAL_MESSAGE1), stateManager.fulfill(STREAM1_DESC, 1000));
+  }
+
+  @Test
+  void testOutOfOrderFulfills() {
+    final ConcurrentMap<StreamDescriptor, StreamAwareQueue> buffers = new ConcurrentHashMap<>();
+    buffers.put(STREAM1_DESC, new StreamAwareQueue(BufferManager.TOTAL_QUEUES_MAX_SIZE_LIMIT_BYTES));
+    final AsyncStateManager stateManager = new AsyncStateManager(buffers);
+
+    final List<AirbyteMessage> stream1Records = generateRecords(1000, STREAM_NAME);
+    long count = 0;
+    for (final AirbyteMessage r : stream1Records) {
+      if (count > 0 && count % 950 == 0) {
+        stateManager.trackState(GLOBAL_MESSAGE1, count++);
+      }
+      buffers.get(STREAM1_DESC).offer(r, count++, 160);
+    }
+
+    // worker 1
+    stateManager.claim(STREAM1_DESC, 900);
+    drainNRecords(buffers, STREAM1_DESC, 900);
+    // worker 2
+    stateManager.claim(STREAM1_DESC, 1001);
+    drainNRecords(buffers, STREAM1_DESC, 100);
+
+    assertEquals(Optional.empty(), stateManager.fulfill(STREAM1_DESC, 1001));
+    assertEquals(Optional.of(GLOBAL_MESSAGE1), stateManager.fulfill(STREAM1_DESC, 900));
+  }
+
+  void drainNRecords(final ConcurrentMap<StreamDescriptor, StreamAwareQueue> buffers,
+                     final StreamDescriptor streamDescriptor,
+                     final int numRecord) {
+
+    for (int i = 0; i < numRecord; i++) {
+      buffers.get(streamDescriptor).poll();
+    }
   }
 
   private static List<AirbyteMessage> generateRecords(final long numRecords, final String streamName) {
