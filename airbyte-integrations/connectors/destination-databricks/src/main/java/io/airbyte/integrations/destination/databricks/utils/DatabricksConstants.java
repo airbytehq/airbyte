df6f5fd9112a132c173e2d5e1cff999abe47b251
Gaetan Podevijn <gaetan.podevijn@prima.it>
2023-05-24 15:53:57 +0200

    https://prima-assicurazioni-spa.myjetbrains.com/youtrack/issue/SSDP-165/Enable-schema-evolution-in-the-Airbyte-Databricks-connector

--- airbyte-integrations/connectors/destination-databricks/src/main/java/io/airbyte/integrations/destination/databricks/utils/DatabricksConstants.java
+++ airbyte-integrations/connectors/destination-databricks/src/main/java/io/airbyte/integrations/destination/databricks/utils/DatabricksConstants.java
@@ -16,6 +16,7 @@ public class DatabricksConstants {
   public static final String DATABRICKS_PORT_KEY = "databricks_port";
   public static final String DATABRICKS_CATALOG_KEY = "database";
   public static final String DATABRICKS_SCHEMA_KEY = "schema";
+  public static final String DATABRICKS_ENABLE_SCHEMA_EVOLUTION_KEY = "enable_schema_evolution";
   public static final String DATABRICKS_CATALOG_JDBC_KEY = "ConnCatalog";
   public static final String DATABRICKS_SCHEMA_JDBC_KEY = "ConnSchema";
   public static final String DATABRICKS_PURGE_STAGING_DATA_KEY = "purge_staging_data";
