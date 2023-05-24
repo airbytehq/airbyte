7eb8c95d392d745691f9ac7c3d8cc3e0751df4fc
alafanechere <augustin.lafanechere@gmail.com>
2023-05-24 15:35:39 +0200

    handle finalize_build.py scripts

--- tools/ci_connector_ops/ci_connector_ops/pipelines/actions/environments.py
+++ tools/ci_connector_ops/ci_connector_ops/pipelines/actions/environments.py
@@ -6,6 +6,7 @@
 
 from __future__ import annotations
 
+import importlib.util
 import os
 import uuid
 from typing import TYPE_CHECKING, List, Optional, Tuple
@@ -692,18 +693,42 @@ async def with_airbyte_python_connector(context: ConnectorContext, build_platfor
 
 
 async def finalize_build(context: ConnectorContext, connector_container: Container) -> Container:
-    connector_dir_with_finalize_script = context.get_connector_dir(include=["finalize_build.sh"])
-    has_finalize_script = "finalize_build.sh" in await connector_dir_with_finalize_script.entries()
-    if has_finalize_script:
-        original_entrypoint = await connector_container.entrypoint()
-        context.logger.info(f"{context.connector.technical_name} has finalize_build.sh script, running it")
+    """Finalize build by running finalize_build.sh or finalize_build.py if present in the connector directory."""
+    connector_dir_with_finalize_script = context.get_connector_dir(include=["finalize_build.sh", "finalize_build.py"])
+    finalize_scripts = await connector_dir_with_finalize_script.entries()
+    if not finalize_scripts:
+        return connector_container
+
+    # We don't want finalize scripts to override the entrypoint so we keep it in memory to reset it after finalization
+    original_entrypoint = await connector_container.entrypoint()
+
+    has_finalize_bash_script = "finalize_build.sh" in finalize_scripts
+    has_finalize_python_script = "finalize_build.py" in finalize_scripts
+    if has_finalize_python_script and has_finalize_bash_script:
+        raise Exception("Connector has both finalize_build.sh and finalize_build.py, please remove one of them")
+
+    if has_finalize_python_script:
+        context.logger.info(f"{context.connector.technical_name} has a finalize_build.py script, running it to finalize build...")
+        module_path = context.connector.code_directory / "finalize_build.py"
+        connector_finalize_module_spec = importlib.util.spec_from_file_location(
+            f"{context.connector.code_directory.name}_finalize", module_path
+        )
+        connector_finalize_module = importlib.util.module_from_spec(connector_finalize_module_spec)
+        connector_finalize_module_spec.loader.exec_module(connector_finalize_module)
+        try:
+            connector_container = await connector_finalize_module.finalize_build(connector_container)
+        except AttributeError:
+            raise Exception("Connector has a finalize_build.py script but it doesn't have a finalize_build function.")
+
+    if has_finalize_bash_script:
+        context.logger.info(f"{context.connector.technical_name} has finalize_build.sh script, running it to finalize build...")
         connector_container = (
             connector_container.with_file("/tmp/finalize_build.sh", connector_dir_with_finalize_script.file("finalize_build.sh"))
             .with_entrypoint("sh")
             .with_exec(["/tmp/finalize_build.sh"])
-            .with_entrypoint(original_entrypoint)
         )
-    return connector_container
+
+    return connector_container.with_entrypoint(original_entrypoint)
 
 
 def with_airbyte_python_connector_full_dagger(context: ConnectorContext, build_platform: Platform) -> Container:
