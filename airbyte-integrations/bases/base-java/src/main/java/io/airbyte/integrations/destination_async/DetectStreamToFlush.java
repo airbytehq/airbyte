36da88fc876abb9ab70d75ce3a3693494a102f9b
cgardens <charles@airbyte.io>
2023-05-25 22:23:42 -0700

    add javadocs

--- airbyte-integrations/bases/base-java/src/main/java/io/airbyte/integrations/destination_async/DetectStreamToFlush.java
+++ airbyte-integrations/bases/base-java/src/main/java/io/airbyte/integrations/destination_async/DetectStreamToFlush.java
@@ -17,6 +17,9 @@ import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.stream.Collectors;
 import lombok.extern.slf4j.Slf4j;
 
+/**
+ * This classes finds the best, next stream to flush.
+ */
 @Slf4j
 public class DetectStreamToFlush {
 
@@ -37,6 +40,22 @@ public class DetectStreamToFlush {
     return getNextStreamToFlush(computeQueueThreshold());
   }
 
+  /**
+   * We have a minimum threshold for the size of a queue before we will flush it. The threshold helps
+   * us avoid uploading small amounts of data at a time, which is really resource inefficient.
+   * Depending on certain conditions, we dynamically adjust this threshold.
+   * <p>
+   * Rules:
+   * <li>default - By default the, the threshold is a set at a constant:
+   * QUEUE_FLUSH_THRESHOLD_BYTES.</li>
+   * <li>memory pressure - If we are getting close to maxing out available memory, we reduce it to
+   * zero. This helps in the case where there are a lot of streams, so total memory usage is high, but
+   * each individual queue isn't that large.</li>
+   * <li>closing - If the Flush Worker is closing, we reduce it to zero. We close when all records
+   * have been added to the queue, at which point, our goal is to flush out any non-empty queues.</li>
+   *
+   * @return based on the conditions, the threshold in bytes.
+   */
   @VisibleForTesting
   long computeQueueThreshold() {
     final boolean isBuffer90Full =
@@ -45,6 +64,31 @@ public class DetectStreamToFlush {
     return isClosing.get() || isBuffer90Full ? 0 : QUEUE_FLUSH_THRESHOLD_BYTES;
   }
 
+  // todo (cgardens) - improve prioritization by getting a better estimate of how much data running
+  // workers will process. we have access to their batch sizes after all!
+  /**
+   * Iterates over streams until it finds one that is ready to flush. Streams are ordered by priority.
+   * If no stream is ready, it returns an empty optional.
+   * <p>
+   * A stream is ready to flush if it either meets a size threshold or a time threshold. For the size
+   * threshold, the size of the data in the queue is compared to the threshold that is passed into
+   * this method.
+   * <p>
+   * One caveat, is that if that stream already has a worker running, we "penalize" its size. We do
+   * this by computing what the size of the queue would be after the running workers for that queue
+   * complete. This is based on a dumb estimate of how much data a worker can process. There is an
+   * opportunity for optimization here, by being smarter about predicting how much data a running
+   * worker is likely to process.
+   * <p>
+   * Finally, the time trigger is based on the last time a record was added to the queue. We don't
+   * want records to sit forever, even if the queue is not that full (bad for time to value for
+   * users). Also, the more time passes since a record was added, the less likely another record is
+   * coming (caveat is CDC where it's random).
+   *
+   * @param queueSizeThresholdBytes - the size threshold to use for determining if a stream is ready
+   *        to flush.
+   * @return the next stream to flush. if no stream is ready to flush, empty.
+   */
   @VisibleForTesting
   Optional<StreamDescriptor> getNextStreamToFlush(final long queueSizeThresholdBytes) {
     for (final StreamDescriptor stream : orderStreamsByPriority(bufferDequeue.getBufferedStreams())) {
@@ -78,9 +122,26 @@ public class DetectStreamToFlush {
     return Optional.empty();
   }
 
+  /**
+   * Sort stream descriptors in order of priority with which we would want to flush them.
+   * <p>
+   * Priority is in the following order:
+   * <li>1. size in queue (descending)</li>
+   * <li>2. time since last record (ascending)</li>
+   * <li>3. alphabetical by namespace + stream name.</li>
+   * <p>
+   * In other words, move the biggest queues first, because they are most likely to use available
+   * resources optimally. Then get rid of old stuff (time to value for the user and, generally, as the
+   * age of the last record grows, the likelihood of getting any more records from that stream
+   * decreases, so by flushing them, we can totally complete that stream. Finally, tertiary sort by
+   * name so the order is deterministic.
+   *
+   * @param streams streams to sort.
+   * @return streams sorted by priority.
+   */
   @VisibleForTesting
-  List<StreamDescriptor> orderStreamsByPriority(final Set<StreamDescriptor> stream) {
-    return stream.stream()
+  List<StreamDescriptor> orderStreamsByPriority(final Set<StreamDescriptor> streams) {
+    return streams.stream()
         .sorted(Comparator.comparing((StreamDescriptor s) -> bufferDequeue.getQueueSizeBytes(s).orElseThrow(), Comparator.reverseOrder())
             .thenComparing(s -> bufferDequeue.getTimeOfLastRecord(s).orElseThrow())
             .thenComparing(s -> s.getNamespace() + s.getName()))
