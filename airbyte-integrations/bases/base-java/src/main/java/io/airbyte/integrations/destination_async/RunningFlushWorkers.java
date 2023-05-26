148a4a920e2321c1f29a78ee0b4ffe72ccf3faff
cgardens <charles@airbyte.io>
2023-05-25 22:32:38 -0700

    RunningFlushWorkers javadocs

--- airbyte-integrations/bases/base-java/src/main/java/io/airbyte/integrations/destination_async/RunningFlushWorkers.java
+++ airbyte-integrations/bases/base-java/src/main/java/io/airbyte/integrations/destination_async/RunningFlushWorkers.java
@@ -9,6 +9,9 @@ import io.airbyte.protocol.models.v0.StreamDescriptor;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicInteger;
 
+/**
+ * Track the number of flush workers that are currently running for a given stream.
+ */
 public class RunningFlushWorkers {
 
   private final ConcurrentHashMap<StreamDescriptor, AtomicInteger> streamToInProgressWorkers;
@@ -17,15 +20,31 @@ public class RunningFlushWorkers {
     streamToInProgressWorkers = new ConcurrentHashMap<>();
   }
 
+  /**
+   * Call this when a worker starts flushing a stream.
+   *
+   * @param stream the stream that is being flushed
+   */
   public void trackFlushWorker(final StreamDescriptor stream) {
     streamToInProgressWorkers.computeIfAbsent(stream, ignored -> new AtomicInteger(0)).incrementAndGet();
   }
 
+  /**
+   * Call this when a worker completes flushing a stream.
+   *
+   * @param stream the stream that was flushed
+   */
   public void completeFlushWorker(final StreamDescriptor stream) {
     Preconditions.checkState(streamToInProgressWorkers.containsKey(stream), "Cannot complete flush worker for stream that has not started.");
     streamToInProgressWorkers.get(stream).decrementAndGet();
   }
 
+  /**
+   * Get the number of flush workers that are currently running for a given stream.
+   *
+   * @param stream the stream to check
+   * @return the number of flush workers that are currently running for the given stream
+   */
   public int getNumFlushWorkers(final StreamDescriptor stream) {
     return streamToInProgressWorkers.getOrDefault(stream, new AtomicInteger(0)).get();
   }
