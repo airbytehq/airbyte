9f0811d50a1559ccb32bb0b3b782eb419f282f39
Mal Hancock <mallory@archangelic.space>
2023-05-26 14:32:21 -0700

    add unit test for custom_reports

--- airbyte-integrations/connectors/source-google-analytics-v4/unit_tests/test_custom_reports_validator.py
+++ airbyte-integrations/connectors/source-google-analytics-v4/unit_tests/test_custom_reports_validator.py
@@ -14,11 +14,12 @@ from source_google_analytics_v4.custom_reports_validator import CustomReportsVal
         ([{"name": [], "dimensions": ["ga:test"], "metrics": ["ga:test"]}], "errors: type errors"),
         ([{"name": "test", "dimensions": ["ga:test"], "metrics": ["ga:test"], "added_field": "test"}], "errors: fields not permitted"),
         ([{"missing_name": "test", "dimensions": ["ga:test"], "metrics": ["ga:test"]}], "errors: fields required"),
+        ([{"name": "missing_segment_dimension", "dimensions": ["ga:test"], "segments": ["another_segment"], "metrics": ["ga:test"]}], "ga:segment is required")
     ),
-    ids=["incorrrect field reference", "type_error", "not_permitted", "missing"],
+    ids=["incorrrect field reference", "type_error", "not_permitted", "missing", "missing_segment_dimension"],
 )
 def test_custom_reports_validator(custom_reports, expected):
     try:
         CustomReportsValidator(custom_reports).validate()
-    except AirbyteTracedException as e:
+    except (AirbyteTracedException, ValueError) as e:
         assert expected in str(e)
