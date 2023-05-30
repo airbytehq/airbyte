caf7f5783b6c5eaf25b3bdb70aca7d76473165b6
cgardens <charles@airbyte.io>
2023-05-30 09:21:16 -0700

    turn on async snowflake

--- airbyte-integrations/connectors/destination-snowflake/src/main/java/io/airbyte/integrations/destination/snowflake/SnowflakeDestination.java
+++ airbyte-integrations/connectors/destination-snowflake/src/main/java/io/airbyte/integrations/destination/snowflake/SnowflakeDestination.java
@@ -39,10 +39,10 @@ public class SnowflakeDestination extends SwitchingDestination<SnowflakeDestinat
       throws Exception {
     log.info("destination class: {}", getClass());
     // this is how we toggle async snowflake on.
-    final boolean useAsyncSnowflake = false;
-    // final boolean useAsyncSnowflake = config.has("loading_method")
-    // && config.get("loading_method").has("method")
-    // && config.get("loading_method").get("method").asText().equals("Internal Staging");
+    // final boolean useAsyncSnowflake = false;
+    final boolean useAsyncSnowflake = config.has("loading_method")
+        && config.get("loading_method").has("method")
+        && config.get("loading_method").get("method").asText().equals("Internal Staging");
 
     log.info("using async snowflake: {}", useAsyncSnowflake);
     if (useAsyncSnowflake) {
