7e61912c108c92b1fe928abd95f528f254abbe3e
Alexandre Girard <alexandre@airbyte.io>
2023-05-30 12:28:14 -0700

    Enable debug

--- airbyte-integrations/connectors/source-intercom/source_intercom/source.py
+++ airbyte-integrations/connectors/source-intercom/source_intercom/source.py
@@ -2,7 +2,11 @@
 # Copyright (c) 2023 Airbyte, Inc., all rights reserved.
 #
 
+
+import logging
+
 from airbyte_cdk.sources.declarative.yaml_declarative_source import YamlDeclarativeSource
+from airbyte_cdk.sources.declarative.manifest_declarative_source import ManifestDeclarativeSource
 
 """
 This file provides the necessary constructs to interpret a provided declarative YAML configuration file into
@@ -16,3 +20,10 @@ WARNING: Do not modify this file.
 class SourceIntercom(YamlDeclarativeSource):
     def __init__(self):
         super().__init__(**{"path_to_yaml": "manifest.yaml"})
+
+
+    def _configure_logger_level(self, logger: logging.Logger):
+        """
+        Set the log level to logging.DEBUG if debug mode
+        """
+        logger.setLevel(logging.DEBUG)
