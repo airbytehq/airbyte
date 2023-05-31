8c4132eb7b653813d17e0a34e40d2410bbfd6f1d
Sherif A. Nada <snadalive@gmail.com>
2023-05-31 09:24:40 -0700

    lowcode: Better describe what the cursor field is for (#26815)

--- airbyte-cdk/python/airbyte_cdk/sources/declarative/models/declarative_component_schema.py
+++ airbyte-cdk/python/airbyte_cdk/sources/declarative/models/declarative_component_schema.py
@@ -847,9 +847,9 @@ class DatetimeBasedCursor(BaseModel):
     )
     datetime_format: str = Field(
         ...,
-        description="The format of the datetime value.",
+        description="The datetime format of the Cursor Field.",
         examples=["%Y-%m-%dT%H:%M:%S.%f%z"],
-        title="Datetime Format",
+        title="Cursor Field Datetime Format",
     )
     cursor_granularity: str = Field(
         ...,
