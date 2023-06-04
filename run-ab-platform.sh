ca26690cef784299f64258f0f6abf908e9484bc3
cpdeethree <conor@airbyte.io>
2023-06-04 17:05:20 -0500

    bump to 0.44.11 to catch up to airbyte-platform and fix bumpversion script

--- run-ab-platform.sh
+++ run-ab-platform.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-VERSION=0.44.10
+VERSION=0.44.11
 # Run away from anything even a little scary
 set -o nounset # -u exit if a variable is not set
 set -o errexit # -f exit for any command failure"
