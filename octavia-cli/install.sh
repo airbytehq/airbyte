30992bb9165692d108d5676a38c2b9ef917975de
cpdeethree <cpdeethree@users.noreply.github.com>
2023-06-05 01:25:58 +0000

    Bump Airbyte version from 0.44.11 to 0.44.12

--- octavia-cli/install.sh
+++ octavia-cli/install.sh
@@ -3,7 +3,7 @@
 # This install scripts currently only works for ZSH and Bash profiles.
 # It creates an octavia alias in your profile bound to a docker run command and your current user.
 
-VERSION=0.44.11
+VERSION=0.44.12
 OCTAVIA_ENV_FILE=${HOME}/.octavia
 
 detect_profile() {
