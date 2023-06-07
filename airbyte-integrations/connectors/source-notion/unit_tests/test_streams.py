7b83ca3b9ea5baddf76dd520be9610db68787383
Daryna Ishchenko <80129833+darynaishchenko@users.noreply.github.com>
2023-06-07 18:25:08 +0300

    :bug:Source Notion: added handing empty results in response (#27073)
    * added handing empty results in response
    
    * added changelog

--- airbyte-integrations/connectors/source-notion/unit_tests/test_streams.py
+++ airbyte-integrations/connectors/source-notion/unit_tests/test_streams.py
@@ -98,6 +98,18 @@ def test_should_not_retry_with_not_found_block(requests_mock):
     assert not stream.should_retry(test_response)
 
 
+def test_empty_blocks_results(requests_mock):
+    stream = Blocks(parent=None, config=MagicMock())
+    requests_mock.get(
+        "https://api.notion.com/v1/blocks/aaa/children",
+        json={
+            "next_cursor": None,
+        },
+    )
+    stream.block_id_stack = ["aaa"]
+    assert list(stream.read_records(sync_mode=SyncMode.incremental, stream_slice=[])) == []
+
+
 def test_backoff_time(patch_base_class):
     response_mock = MagicMock(headers={"retry-after": "10"})
     stream = NotionStream(config=MagicMock())
