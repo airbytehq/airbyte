ea273dce383a10650182bb035dee350e059047c7
Octavia Squidington III <octavia-squidington-iii@sers.noreply.github.com>
2023-06-07 09:15:00 -0700

    TODO

--- airbyte-ci/connectors/metadata_service/orchestrator/orchestrator/assets/connector_nightly_report.py
+++ airbyte-ci/connectors/metadata_service/orchestrator/orchestrator/assets/connector_nightly_report.py
@@ -9,6 +9,14 @@ GROUP_NAME = "connector_nightly_report"
 @asset(required_resource_keys={"latest_nightly_file_blobs"}, group_name=GROUP_NAME)
 def generate_nightly_report(context: OpExecutionContext) -> OutputDataFrame:
     """
+    TODO
+    0. Get recent 10 nightly runs
+    2. Get the github action url
+    1. Parse all connectors into a pass fail, not present
+    2. Filter to Failed last build, and more than last build
+    3. Write the report to a markdown file
+    5. Message slack with report contents
+    6. Look at updating badge
     """
     latest_nightly_file_blobs = context.resources.latest_nightly_file_blobs
     file_paths = [blob.name for blob in latest_nightly_file_blobs]
