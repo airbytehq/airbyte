1315f5a7684d83504f424de0c6d7e00c05d46cf0
Ben Church <ben@airbyte.io>
2023-06-08 10:45:13 -0700

    Nightly Reports: Generate markdown slack report from new nightlies (#26990)
    * Add github asset
    
    * reduce columns in asset
    
    * Add sensor
    
    * remove sensor calls
    
    * Remove bucket manager
    
    * Get nightly resources
    
    * Add nightly report data
    
    * TODO
    
    * Generate usable data frame
    
    * Add template
    
    * Add all data to report
    
    * Generalize gcs blob sensor
    
    * Remove old metadata sensor
    
    * Send report to slack
    
    * Complete report
    
    * Remove overly verbose comments
    
    * Add generics
    
    ---------
    
    Co-authored-by: Octavia Squidington III <octavia-squidington-iii@sers.noreply.github.com>

--- airbyte-ci/connectors/metadata_service/orchestrator/tests/test_debug.py
+++ airbyte-ci/connectors/metadata_service/orchestrator/tests/test_debug.py
@@ -1,31 +1,29 @@
 from dagster import build_op_context
 
-from orchestrator.resources.gcp import gcp_gcs_client, gcs_bucket_manager, gcs_file_manager, gcs_directory_blobs
+from orchestrator.resources.gcp import gcp_gcs_client, gcs_directory_blobs
 
-from orchestrator.assets.registry import (
-    persist_oss_registry_from_metadata,
+from orchestrator.assets.connector_nightly_report import (
+    generate_nightly_report,
 )
 
-from orchestrator.assets.metadata import (
-    metadata_definitions,
-)
 
-from metadata_service.constants import METADATA_FILE_NAME, METADATA_FOLDER
-from orchestrator.config import REGISTRIES_FOLDER
+from orchestrator.config import NIGHTLY_INDIVIDUAL_TEST_REPORT_FILE_NAME, NIGHTLY_FOLDER, NIGHTLY_COMPLETE_REPORT_FILE_NAME
 
 
-def debug_registry_generation():
+def test_debug_nightly_report():
     resources = {
         "gcp_gcs_client": gcp_gcs_client.configured(
             {
                 "gcp_gcs_cred_string": {"env": "GCS_CREDENTIALS"},
             }
         ),
-        "gcs_bucket_manager": gcs_bucket_manager.configured({"gcs_bucket": {"env": "METADATA_BUCKET"}}),
-        "registry_directory_manager": gcs_file_manager.configured({"gcs_bucket": {"env": "METADATA_BUCKET"}, "prefix": REGISTRIES_FOLDER}),
-        "latest_metadata_file_blobs": gcs_directory_blobs.configured({"prefix": METADATA_FOLDER, "suffix": METADATA_FILE_NAME}),
+        "latest_nightly_complete_file_blobs": gcs_directory_blobs.configured(
+            {"gcs_bucket": {"env": "CI_REPORT_BUCKET"}, "prefix": NIGHTLY_FOLDER, "suffix": NIGHTLY_COMPLETE_REPORT_FILE_NAME}
+        ),
+        "latest_nightly_test_output_file_blobs": gcs_directory_blobs.configured(
+            {"gcs_bucket": {"env": "CI_REPORT_BUCKET"}, "prefix": NIGHTLY_FOLDER, "suffix": NIGHTLY_INDIVIDUAL_TEST_REPORT_FILE_NAME}
+        ),
     }
 
     context = build_op_context(resources=resources)
-    metadata_definitions_asset = metadata_definitions(context)
-    persist_oss_registry_from_metadata(context, metadata_definitions_asset).value
+    generate_nightly_report(context).value
