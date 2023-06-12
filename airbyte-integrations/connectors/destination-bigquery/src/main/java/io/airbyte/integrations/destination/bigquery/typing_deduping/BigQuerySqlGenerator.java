6911e386400692a74dd6bd7dfb4bc5476558954e
Edward Gao <edward.gao@airbyte.io>
2023-06-12 14:22:57 -0700

    wip generate update/insert statement

--- airbyte-integrations/connectors/destination-bigquery/src/main/java/io/airbyte/integrations/destination/bigquery/typing_deduping/BigQuerySqlGenerator.java
+++ airbyte-integrations/connectors/destination-bigquery/src/main/java/io/airbyte/integrations/destination/bigquery/typing_deduping/BigQuerySqlGenerator.java
@@ -135,47 +135,125 @@ public class BigQuerySqlGenerator implements SqlGenerator<TableDefinition, Stand
     return null;
   }
 
-  // We need the configuredairbytestream for the sync mode + cursor name
   @Override
   public String updateTable(final String finalSuffix, final StreamConfig<StandardSQLTypeName> stream) {
     // TODO this method needs to handle all the sync modes
-    // do the stuff that evan figured out how to do https://github.com/airbytehq/typing-and-deduping-sql/blob/main/one-table.postgres.sql#L153
-    // TODO use a better string templating thing
+    // e.g. this pk null check should only happen for incremental dedup
+    String pkNullChecks = stream.primaryKey().stream().map(
+        pk -> "AND SAFE_CAST(JSON_VALUE(`_airbyte_data`, '$." + pk.originalName() + "') as " + stream.columns().get(pk).name() + ") IS NULL"
+    ).collect(joining("\n"));
+    // TODO - this needs the original AirbyteType so that we can recognize which columns need JSON_QUERY vs JSON_VALUE
+    String columnCasts = stream.columns().entrySet().stream().map(
+        col -> "SAFE_CAST(JSON_VALUE(`_airbyte_data`, '$." + col.getKey().originalName() + "') as " + col.getValue().name() + ") as " + col.getKey()
+            .name() + ","
+    ).collect(joining("\n"));
+    String columnErrors = stream.columns().entrySet().stream().map(
+        col -> new StringSubstitutor(Map.of(
+          "raw_col_name", col.getKey().originalName(),
+            "col_type", col.getValue().name()
+        )).replace(
+            """
+                CASE
+                  WHEN (JSON_VALUE(`_airbyte_data`, '$.${raw_col_name}') IS NOT NULL) AND (SAFE_CAST(JSON_VALUE(`_airbyte_data`, '$.${raw_col_name}') as ${col_type}) IS NULL) THEN ["Problem with `${raw_col_name}`"]
+                  ELSE []
+                END"""
+        )
+    ).collect(joining(",\n"));
+    String pkList = stream.primaryKey().stream().map(QuotedColumnId::name).collect(joining(","));
+    // TODO - this needs the original AirbyteType so that we can recognize which columns need JSON_QUERY vs JSON_VALUE
+    String pkEqualityChecks = stream.columns().entrySet().stream()
+        .filter(e -> stream.primaryKey().contains(e.getKey()))
+        .map(
+        col -> new StringSubstitutor(Map.of(
+            "raw_col_name", col.getKey().originalName(),
+            "col_type", col.getValue().name()
+        )).replace("""
+            `id` IN (
+              SELECT
+                SAFE_CAST(JSON_VALUE(`_airbyte_data`, '$.${raw_col_name}') as ${col_type}) as id
+              FROM ${raw_table_id}
+              WHERE JSON_VALUE(`_airbyte_data`, '$._ab_cdc_deleted_at') IS NOT NULL
+            )""")
+    ).collect(joining("\nAND "));
 
     return new StringSubstitutor(Map.of(
-        "final_namespace", stream.id().finalNamespace(),
-        // TODO this is wrong, we can't just do "`foo`.`bar`" + "_tmp"
-        "final_table_name", finalSuffix.length() > 0 ? stream.id().finalName() + "_" + finalSuffix : stream.id().finalName(),
         "raw_table_id", stream.id().rawTableId(),
-        "airbyte_raw_id", RAW_ID
-    )).replace("""
-        BEGIN;
-
-        INSERT INTO ${final_namespace}.${final_table_name}
-         SELECT
-           TODO....
-         FROM ${raw_table_id};
-
-        DELETE FROM ${final_namespace}.${final_table_name}
-         WHERE ${airbyte_raw_id} IN (
-          SELECT ${airbyte_raw_id} FROM (
-            SELECT ${airbyte_raw_id}, row_number() OVER (
-              PARTITION BY `id` ORDER BY `updated_at` DESC, `_airbyte_extracted_at` DESC
-            ) as row_number FROM ${final_namespace}.${final_table_name}
-          )
-          WHERE row_number != 1
-         )
-         OR
-         -- Delete rows that have been CDC deleted
-         `id` IN (
-          SELECT
-            SAFE_CAST(JSON_EXTRACT_SCALAR(`_airbyte_data`, '$.id') as INT64) as id -- based on the PK which we know from the connector catalog
-          FROM ${raw_table_id}
-          WHERE JSON_EXTRACT_SCALAR(`_airbyte_data`, '$._ab_cdc_deleted_at') IS NOT NULL
-         )
-
-        COMMIT;
-        """);
+        // TODO handle final suffix
+        "final_table_id", stream.id().finalTableId(),
+        "pk_null_checks", pkNullChecks,
+        "column_casts", columnCasts,
+        "column_errors", columnErrors,
+        "pk_list", pkList,
+        "pk_equality_checks", pkEqualityChecks
+    )).replace(
+        """
+            DECLARE missing_pk_count INT64;
+                        
+            BEGIN TRANSACTION;
+              -- Step 1: Validate the incoming data
+              SET missing_pk_count = (
+                SELECT COUNT(1)
+                FROM ${raw_table_id}
+                WHERE
+                  `_airbyte_loaded_at` IS NULL
+                  ${pk_null_checks}
+                );
+                      
+              IF missing_pk_count > 0 THEN
+                RAISE USING message = FORMAT("Raw table has %s rows missing a primary key", CAST(missing_pk_count AS STRING));
+              END IF;
+                      
+              -- Step 2: Move the new data to the typed table
+              INSERT INTO ${final_table_id}
+              SELECT
+            ${column_casts}
+            to_json(struct(array_concat(
+            ${column_errors}
+            ) as errors)) as _airbyte_meta,
+                _airbyte_raw_id,
+                _airbyte_extracted_at
+              FROM ${raw_table_id}
+              WHERE
+                _airbyte_loaded_at IS NULL
+                AND JSON_EXTRACT(`_airbyte_data`, '$._ab_cdc_deleted_at') IS NULL
+              ;
+                      
+              -- Step 3: Dedupe and clean the typed table
+              DELETE FROM testing_evan_2052.users
+              WHERE
+                `_airbyte_raw_id` IN (
+                  SELECT `_airbyte_raw_id` FROM (
+                    SELECT `_airbyte_raw_id`, row_number() OVER (
+                      PARTITION BY ${pk_list} ORDER BY `updated_at` DESC, `_airbyte_extracted_at` DESC
+                    ) as row_number FROM ${final_table_id}
+                  )
+                  WHERE row_number != 1
+                )
+                OR (
+            ${pk_equality_checks}
+                )
+              ;
+                      
+              -- Step 4: Remove old entries from Raw table
+              DELETE FROM
+                ${raw_table_id}
+              WHERE
+                `_airbyte_raw_id` NOT IN (
+                  SELECT `_airbyte_raw_id` FROM ${final_table_id}
+                )
+                AND
+                JSON_VALUE(`_airbyte_data`, '$._ab_cdc_deleted_at') IS NULL
+              ;
+                      
+              -- Step 5: Apply typed_at timestamp where needed
+              UPDATE ${raw_table_id}
+              SET `_airbyte_loaded_at` = CURRENT_TIMESTAMP()
+              WHERE `_airbyte_loaded_at` IS NULL
+              ;
+                      
+            COMMIT TRANSACTION;
+            """
+    );
   }
 
   @Override
--- airbyte-integrations/connectors/destination-bigquery/src/test/java/io/airbyte/integrations/destination/bigquery/typing_deduping/BigQuerySqlGeneratorTest.java
+++ airbyte-integrations/connectors/destination-bigquery/src/test/java/io/airbyte/integrations/destination/bigquery/typing_deduping/BigQuerySqlGeneratorTest.java
@@ -52,4 +52,111 @@ class BigQuerySqlGeneratorTest {
         sql
     );
   }
+
+  @Test
+  public void basicIncrementalDedupUpdateTable() {
+    LinkedHashMap<QuotedColumnId, StandardSQLTypeName> columns = new LinkedHashMap<>();
+    columns.put(generator.quoteColumnId("id"), StandardSQLTypeName.INT64);
+    columns.put(generator.quoteColumnId("updated_at"), StandardSQLTypeName.TIMESTAMP);
+    columns.put(generator.quoteColumnId("name"), StandardSQLTypeName.STRING);
+    StreamConfig<StandardSQLTypeName> stream = new StreamConfig<>(
+        new SqlGenerator.QuotedStreamId("public", "users", "airbyte", "public_users", "public", "users"),
+        SyncMode.INCREMENTAL,
+        DestinationSyncMode.APPEND_DEDUP,
+        List.of(generator.quoteColumnId("id")),
+        Optional.of(generator.quoteColumnId("updated_at")),
+        columns
+    );
+
+    final String sql = generator.updateTable("", stream);
+
+    assertEquals(
+        """
+            DECLARE missing_pk_count INT64;
+                        
+            BEGIN TRANSACTION;
+              -- Step 1: Validate the incoming data
+              SET missing_pk_count = (
+                SELECT COUNT(1)
+                FROM airbyte.public_users
+                WHERE
+                  `_airbyte_loaded_at` IS NULL
+                  AND SAFE_CAST(JSON_VALUE(`_airbyte_data`, '$.id') as INT64) IS NULL
+                );
+                        
+              IF missing_pk_count > 0 THEN
+                RAISE USING message = FORMAT("Raw table has %s rows missing a primary key", CAST(missing_pk_count AS STRING));
+              END IF;
+                        
+              -- Step 2: Move the new data to the typed table
+              INSERT INTO public.users
+              SELECT
+            SAFE_CAST(JSON_VALUE(`_airbyte_data`, '$.id') as INT64) as id,
+            SAFE_CAST(JSON_VALUE(`_airbyte_data`, '$.updated_at') as TIMESTAMP) as updated_at,
+            SAFE_CAST(JSON_VALUE(`_airbyte_data`, '$.name') as STRING) as name,
+            to_json(struct(array_concat(
+            CASE
+              WHEN (JSON_VALUE(`_airbyte_data`, '$.id') IS NOT NULL) AND (SAFE_CAST(JSON_VALUE(`_airbyte_data`, '$.id') as INT64) IS NULL) THEN ["Problem with `id`"]
+              ELSE []
+            END,
+            CASE
+              WHEN (JSON_VALUE(`_airbyte_data`, '$.updated_at') IS NOT NULL) AND (SAFE_CAST(JSON_VALUE(`_airbyte_data`, '$.updated_at') as TIMESTAMP) IS NULL) THEN ["Problem with `updated_at`"]
+              ELSE []
+            END,
+            CASE
+              WHEN (JSON_VALUE(`_airbyte_data`, '$.name') IS NOT NULL) AND (SAFE_CAST(JSON_VALUE(`_airbyte_data`, '$.name') as STRING) IS NULL) THEN ["Problem with `name`"]
+              ELSE []
+            END
+            ) as errors)) as _airbyte_meta,
+                _airbyte_raw_id,
+                _airbyte_extracted_at
+              FROM airbyte.public_users
+              WHERE
+                _airbyte_loaded_at IS NULL
+                AND JSON_EXTRACT(`_airbyte_data`, '$._ab_cdc_deleted_at') IS NULL
+              ;
+                      
+              -- Step 3: Dedupe and clean the typed table
+              DELETE FROM testing_evan_2052.users
+              WHERE
+                `_airbyte_raw_id` IN (
+                  SELECT `_airbyte_raw_id` FROM (
+                    SELECT `_airbyte_raw_id`, row_number() OVER (
+                      PARTITION BY id ORDER BY `updated_at` DESC, `_airbyte_extracted_at` DESC
+                    ) as row_number FROM public.users
+                  )
+                  WHERE row_number != 1
+                )
+                OR (
+            `id` IN (
+              SELECT
+                SAFE_CAST(JSON_VALUE(`_airbyte_data`, '$.id') as INT64) as id
+              FROM airbyte.public_users
+              WHERE JSON_VALUE(`_airbyte_data`, '$._ab_cdc_deleted_at') IS NOT NULL
+            )
+                )
+              ;
+                      
+              -- Step 4: Remove old entries from Raw table
+              DELETE FROM
+                airbyte.public_users
+              WHERE
+                `_airbyte_raw_id` NOT IN (
+                  SELECT `_airbyte_raw_id` FROM public.users
+                )
+                AND
+                JSON_VALUE(`_airbyte_data`, '$._ab_cdc_deleted_at') IS NULL
+              ;
+                      
+              -- Step 5: Apply typed_at timestamp where needed
+              UPDATE airbyte.public_users
+              SET `_airbyte_loaded_at` = CURRENT_TIMESTAMP()
+              WHERE `_airbyte_loaded_at` IS NULL
+              ;
+                      
+            COMMIT TRANSACTION;
+            """,
+        sql
+    );
+  }
 }
