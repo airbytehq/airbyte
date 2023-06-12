e32f8a2b2e4695bd0b7202d01a45a86bbca229a4
Ben Church <ben@airbyte.io>
2023-06-12 15:54:10 -0700

    Metadata: Add Breaking Changes entry (#27178)
    * Add breaking changes metadata
    
    * Add valid breaking change
    
    * Add invalid cases
    
    * Update comment
    
    * Add invalid deadline check
    
    * Convert to releases
    
    * Update to releases
    
    * Test for migration doc url
    
    * Add registry override application
    
    * Add even more tests
    
    * Add default tests
    
    * Apply format
    
    * Add releases to registry model
    
    * Add new test cases
    
    * Set additional properties to false
    
    ---------
    
    Co-authored-by: Octavia Squidington III <octavia-squidington-iii@sers.noreply.github.com>

--- airbyte-ci/connectors/metadata_service/lib/metadata_service/models/generated/ConnectorMetadataDefinitionV0.py
+++ airbyte-ci/connectors/metadata_service/lib/metadata_service/models/generated/ConnectorMetadataDefinitionV0.py
@@ -4,10 +4,10 @@
 from __future__ import annotations
 
 from datetime import date
-from typing import List, Optional
+from typing import Dict, List, Optional
 from uuid import UUID
 
-from pydantic import AnyUrl, BaseModel, Extra, Field
+from pydantic import AnyUrl, BaseModel, Extra, Field, constr
 from typing_extensions import Literal
 
 
@@ -75,12 +75,39 @@ class JobType(BaseModel):
     )
 
 
+class VersionBreakingChange(BaseModel):
+    class Config:
+        extra = Extra.forbid
+
+    upgradeDeadline: date = Field(
+        ...,
+        description="The deadline by which to upgrade before the breaking change takes effect.",
+    )
+    message: str = Field(
+        ..., description="Descriptive message detailing the breaking change."
+    )
+    migrationDocumentationUrl: Optional[AnyUrl] = Field(
+        None,
+        description="URL to documentation on how to migrate to the current version. Defaults to ${documentationUrl}/migration_guide#${version}",
+    )
+
+
 class JobTypeResourceLimit(BaseModel):
     class Config:
         extra = Extra.forbid
 
     jobType: JobType
     resourceRequirements: ResourceRequirements
+
+
+class ConnectorBreakingChanges(BaseModel):
+    class Config:
+        extra = Extra.forbid
+
+    __root__: Dict[constr(regex=r"^\d+\.\d+\.\d+$"), VersionBreakingChange] = Field(
+        ...,
+        description="Each entry denotes a breaking change in a specific version of a connector that requires user action to upgrade.",
+    )
 
 
 class ActorDefinitionResourceRequirements(BaseModel):
@@ -92,6 +119,17 @@ class ActorDefinitionResourceRequirements(BaseModel):
         description="if set, these are the requirements that should be set for ALL jobs run for this actor definition.",
     )
     jobSpecific: Optional[List[JobTypeResourceLimit]] = None
+
+
+class ConnectorReleases(BaseModel):
+    class Config:
+        extra = Extra.forbid
+
+    breakingChanges: ConnectorBreakingChanges
+    migrationDocumentationUrl: Optional[AnyUrl] = Field(
+        None,
+        description="URL to documentation on how to migrate from the previous version to the current version. Defaults to ${documentationUrl}/migration_guide",
+    )
 
 
 class RegistryOverrides(BaseModel):
@@ -154,6 +192,7 @@ class Data(BaseModel):
     )
     registries: Optional[Registry] = None
     allowedHosts: Optional[AllowedHosts] = None
+    releases: Optional[ConnectorReleases] = None
     normalizationConfig: Optional[NormalizationDestinationDefinitionConfig] = None
     suggestedStreams: Optional[SuggestedStreams] = None
     resourceRequirements: Optional[ActorDefinitionResourceRequirements] = None
