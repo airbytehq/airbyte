07ad798866ff99aca8e02e42e3439494a36a2df2
Lee Danilek <lee@convex.dev>
2023-06-12 18:22:48 -0700

    cache json_schemas to pass typecheck

--- airbyte-integrations/connectors/source-convex/source_convex/source.py
+++ airbyte-integrations/connectors/source-convex/source_convex/source.py
@@ -4,6 +4,7 @@
 
 
 from datetime import datetime
+from functools import lru_cache
 from json import JSONDecodeError
 from typing import Any, Dict, Iterable, Iterator, List, Mapping, MutableMapping, Optional, Tuple, TypedDict
 
@@ -106,6 +107,7 @@ class ConvexStream(HttpStream, IncrementalMixin):
     def url_base(self) -> str:
         return self.deployment_url
 
+    @lru_cache(maxsize=None)
     def get_json_schema(self) -> Mapping[str, Any]:
         return self.json_schema
 
