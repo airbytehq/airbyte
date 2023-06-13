ed3bb0f4c56a1c76f789e716d485ecdd00716593
Anas El Mhamdi <anas.elmhamdi@gmail.com>
2023-06-13 13:01:28 +0200

    updated tests and spec

--- airbyte-integrations/connectors/source-rippling/unit_tests/test_source.py
+++ airbyte-integrations/connectors/source-rippling/unit_tests/test_source.py
@@ -4,19 +4,27 @@
 
 from unittest.mock import MagicMock
 
+from pytest import fixture
 from source_rippling.source import SourceRippling
 
 
-def test_check_connection(mocker):
+@fixture()
+def config(request):
+    args = {"api_key": "TOKEN"}
+    return args
+
+
+def test_check_connection(mocker, config):
     source = SourceRippling()
-    logger_mock, config_mock = MagicMock(), MagicMock()
-    assert source.check_connection(logger_mock, config_mock) == (True, None)
+    logger_mock, config_mock = MagicMock(), config
+    expected_status = False
+    (connection_status, _) = source.check_connection(logger_mock, config_mock)
+    assert connection_status == expected_status
 
 
 def test_streams(mocker):
     source = SourceRippling()
     config_mock = MagicMock()
     streams = source.streams(config_mock)
-    # TODO: replace this with your streams number
-    expected_streams_number = 2
+    expected_streams_number = 4
     assert len(streams) == expected_streams_number
