8d00ee808fc065853f1a528c975db5ca76cab495
Roman Yermilov <roman.yermilov@globallogic.com>
2023-06-14 15:06:08 +0400

    Source Stripe: disable slicing

--- airbyte-integrations/connectors/source-stripe/source_stripe/streams.py
+++ airbyte-integrations/connectors/source-stripe/source_stripe/streams.py
@@ -766,3 +766,8 @@ class CreditNotes(StripeStream):
 
     def request_params(self, stream_state: Mapping[str, Any], stream_slice: Mapping[str, Any] = None, next_page_token: Mapping[str, Any] = None) -> MutableMapping[str, Any]:
         return next_page_token or {}
+    
+    def stream_slices(
+        self, *, sync_mode: SyncMode, cursor_field: List[str] = None, stream_state: Mapping[str, Any] = None
+    ) -> Iterable[Optional[Mapping[str, Any]]]:
+        yield from [{}]
