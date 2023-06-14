68c0d6dbbcc99e24073d7a5c58968e690cae2967
Ben Church <ben@airbyte.io>
2023-06-14 11:12:47 -0700

    Connector CI: update badges test summary (#27215)
    * Rename to connector test report
    
    * Get all test outputs
    
    * Get last 10 output for all connectors
    
    * Get runs by connector
    
    * Crudely write badge and summary
    
    * Add render functions
    
    * Update connector report badge and link
    
    * Fix url link
    
    * Fix qa report tests
    
    * update qa-engine tests
    
    * Move regex to resource
    
    * Add cron
    
    * Run format
    
    * New line
    
    * Make badge green if last is successful
    
    * Fix test
    
    * Remove report.sh and build_report.py
    
    ---------
    
    Co-authored-by: Octavia Squidington III <octavia-squidington-iii@sers.noreply.github.com>

--- airbyte-ci/connectors/metadata_service/orchestrator/orchestrator/assets/connector_test_report.py
+++ airbyte-ci/connectors/metadata_service/orchestrator/orchestrator/assets/connector_test_report.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import json
 import os
+import re
+from datetime import datetime
 
 from dagster import Output, asset, OpExecutionContext, MetadataValue
 from google.cloud import storage
@@ -10,14 +12,19 @@ from orchestrator.ops.slack import send_slack_webhook
 from orchestrator.models.ci_report import ConnectorNightlyReport, ConnectorPipelineReport
 from orchestrator.config import (
     NIGHTLY_COMPLETE_REPORT_FILE_NAME,
+    CONNECTOR_TEST_SUMMARY_FOLDER,
 )
 from orchestrator.templates.render import (
     render_connector_nightly_report_md,
+    render_connector_test_summary_html,
+    render_connector_test_badge,
 )
+from orchestrator.utils.dagster_helpers import OutputDataFrame, output_dataframe
+
 
 T = TypeVar("T")
 
-GROUP_NAME = "connector_nightly_report"
+GROUP_NAME = "connector_test_report"
 
 # HELPERS
 
@@ -145,4 +152,88 @@ def generate_nightly_report(context: OpExecutionContext) -> Output[pd.DataFrame]
     return Output(
         nightly_report_connector_matrix_df,
         metadata={"count": len(nightly_report_connector_matrix_df), "preview": MetadataValue.md(nightly_report_complete_md)},
+    )
+
+
+@asset(required_resource_keys={"all_connector_test_output_file_blobs"}, group_name=GROUP_NAME)
+def last_10_connector_test_results(context: OpExecutionContext) -> OutputDataFrame:
+    gcs_file_blobs = context.resources.all_connector_test_output_file_blobs
+
+    # This is an example path a blob that match the regex:
+    #   airbyte-ci/connectors/test/nightly_builds/master/1686249301/61a6f8ed68cbeaa8b8b1ff4e64496d989826fd5e/source-strava/0.1.4/output.json
+    # We want to extract the following information from the path
+    # 1. connector name (source-strava)
+    # 2. connector version (0.1.4)
+    # 3. timestamp (1686249301)
+    report_status = [
+        {
+            "connector_name": blob.name.split("/")[-3],
+            "connector_version": blob.name.split("/")[-2],
+            "timestamp": blob.name.split("/")[-5],
+            "blob": blob,
+        }
+        for blob in gcs_file_blobs
+    ]
+
+    # group by connector name and only keep the latest 10 timestamps
+    report_status = (
+        pd.DataFrame(report_status)
+        .groupby("connector_name")
+        .apply(lambda x: x.sort_values("timestamp", ascending=False).head(10))
+        .reset_index(drop=True)
+    )
+
+    # add an entry for each connectors timestamp that is whether the test passed or failed
+    report_status["model"] = report_status["blob"].apply(lambda blob: json_blob_to_model(blob, ConnectorPipelineReport))
+    report_status["success"] = report_status["model"].apply(lambda model: model.success)
+    report_status["gha_workflow_run_url"] = report_status["model"].apply(lambda model: model.gha_workflow_run_url)
+
+    # Drop the blob column
+    report_status = report_status.drop(columns=["blob", "model"])
+
+    return output_dataframe(report_status)
+
+
+@asset(required_resource_keys={"registry_report_directory_manager"}, group_name=GROUP_NAME)
+def persist_connectors_test_summary_files(context: OpExecutionContext, last_10_connector_test_results: OutputDataFrame) -> OutputDataFrame:
+    registry_report_directory_manager = context.resources.registry_report_directory_manager
+
+    metadata = {}
+    all_connector_names = last_10_connector_test_results["connector_name"].unique()
+    for connector_name in all_connector_names:
+        all_connector_test_results = last_10_connector_test_results[last_10_connector_test_results["connector_name"] == connector_name]
+        connector_test_summary = all_connector_test_results[["timestamp", "connector_version", "success", "gha_workflow_run_url"]]
+
+        # Order by timestamp descending
+        connector_test_summary = connector_test_summary.sort_values("timestamp", ascending=False)
+
+        # convert unix timestamp to iso date
+        connector_test_summary["date"] = connector_test_summary["timestamp"].apply(
+            lambda timestamp: datetime.fromtimestamp(int(timestamp)).isoformat()
+        )
+
+        # drop the timestamp column
+        connector_test_summary = connector_test_summary.drop(columns=["timestamp"])
+
+        report_file_name = f"{CONNECTOR_TEST_SUMMARY_FOLDER}/{connector_name}"
+
+        badge_json = render_connector_test_badge(connector_test_summary)
+        summary_html = render_connector_test_summary_html(connector_name, connector_test_summary)
+        summary_json = connector_test_summary.to_json(orient="records")
+
+        summary_file_key = f"{report_file_name}/index"
+        badge_file_key = f"{report_file_name}/badge"
+
+        summary_html_file_handle = registry_report_directory_manager.write_data(summary_html.encode(), ext="html", key=summary_file_key)
+        summary_json_file_handle = registry_report_directory_manager.write_data(summary_json.encode(), ext="json", key=summary_file_key)
+        badge_json_file_handle = registry_report_directory_manager.write_data(badge_json.encode(), ext="json", key=badge_file_key)
+
+        # add to metadata
+        metadata[summary_file_key] = MetadataValue.url(summary_html_file_handle.public_url)
+        metadata[badge_file_key] = MetadataValue.url(badge_json_file_handle.public_url)
+        metadata[f"{summary_file_key}/json"] = MetadataValue.url(summary_json_file_handle.public_url)
+
+    return Output(
+        value=last_10_connector_test_results,
+        metadata=metadata,
     )
