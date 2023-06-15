b283aa2872ae7a9fc78f5283a3429fc515ec9670
Mahdi Dibaiee <mdibaiee@pm.me>
2023-06-15 12:43:32 +0000

    add new connectors
    amazon-sqs
    bigquery
    dynamodb
    redshift
    woocommerce
    oracle

--- new-connector.sh
+++ new-connector.sh
@@ -4,5 +4,6 @@ export VERSION=$2
 connectors="airbyte-integrations/connectors"
 
 cp -R $connectors/source-template $connectors/source-$NAME
+export DOCUMENTATION_URL="https://go.estuary.dev/source-$NAME"
 export AIRBYTE_TO_FLOW_TAG='$AIRBYTE_TO_FLOW_TAG'
 cat $connectors/source-template/Dockerfile | envsubst > $connectors/source-$NAME/Dockerfile
