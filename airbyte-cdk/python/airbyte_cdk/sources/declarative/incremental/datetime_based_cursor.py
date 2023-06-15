f49f4f0a92081805515a2e0c6cae6897da3f4e93
maxi297 <maxime@airbyte.io>
2023-06-15 15:13:18 -0400

    DatetimeBasedCursor should not update state based on slice (for now at least since it wasn't doing this before)

--- airbyte-cdk/python/airbyte_cdk/sources/declarative/incremental/datetime_based_cursor.py
+++ airbyte-cdk/python/airbyte_cdk/sources/declarative/incremental/datetime_based_cursor.py
@@ -113,10 +113,9 @@ class DatetimeBasedCursor(Cursor):
         :param last_record: last record read
         :return: None
         """
-        stream_slice_value = stream_slice.get(self.partition_field_start.eval(self.config))
         last_record_value = last_record.get(self.cursor_field.eval(self.config)) if last_record else None
 
-        possible_cursor_values = list(filter(lambda item: item, [stream_slice_value, last_record_value, self._cursor]))
+        possible_cursor_values = list(filter(lambda item: item, [last_record_value, self._cursor]))
         self._cursor = max(possible_cursor_values) if possible_cursor_values else None
 
     def stream_slices(self, sync_mode: SyncMode) -> Iterable[Mapping[str, Any]]:
