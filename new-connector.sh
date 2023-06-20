e21ac598e24bac6a859d8663657016810e014166
Mahdi Dibaiee <mdibaiee@pm.me>
2023-06-20 12:47:58 +0000

    source-pinterest

--- new-connector.sh
+++ new-connector.sh
@@ -7,3 +7,4 @@ cp -R $connectors/source-template $connectors/source-$NAME
 export DOCUMENTATION_URL="https://go.estuary.dev/source-$NAME"
 export AIRBYTE_TO_FLOW_TAG='$AIRBYTE_TO_FLOW_TAG'
 cat $connectors/source-template/Dockerfile | envsubst > $connectors/source-$NAME/Dockerfile
+cat $connectors/source-template/documentation_url.patch.json | envsubst > $connectors/source-$NAME/documentation_url.patch.json
