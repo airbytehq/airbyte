bdfd9b87f448629f4f4bd12f08dfef818a556471
Alexandre Girard <alexandre@airbyte.io>
2023-06-20 12:46:52 -0700

    remove dead code

--- airbyte-cdk/python/airbyte_cdk/sources/file_based/stream/file_based_state.py
+++ airbyte-cdk/python/airbyte_cdk/sources/file_based/stream/file_based_state.py
@@ -13,7 +13,6 @@ class FileBasedState:
     def __init__(self, max_history_size: int, time_window_if_history_is_full: timedelta):
         self._file_to_datetime_history: Mapping[str:datetime] = {}
         self._max_history_size = max_history_size
-        logging.warning(f"max history size: {self._max_history_size}")
         self._time_window_if_history_is_full = time_window_if_history_is_full
         self._history_is_complete = True
 
@@ -22,11 +21,9 @@ class FileBasedState:
         self._history_is_complete = not value.get("incomplete_history", False)
 
     def add_file(self, file: RemoteFile):
-        logging.warning(f"adding file to history: {file.uri}")
         self._file_to_datetime_history[file.uri] = file.last_modified.strftime("%Y-%m-%dT%H:%M:%S.%fZ")
         if len(self._file_to_datetime_history) > self._max_history_size:
             oldest_file = min(self._file_to_datetime_history, key=self._file_to_datetime_history.get)
-            logging.warning(f"Removing {oldest_file} from history")
             del self._file_to_datetime_history[oldest_file]
 
     def to_dict(self):
@@ -42,8 +39,6 @@ class FileBasedState:
 
     def get_files_to_sync(self, all_files: List[RemoteFile]):
         start_time = self.compute_start_time()
-        logging.warning(f"history_is_complete: {self.is_history_complete()}")
-        logging.warning(f"all_files: {all_files}")
         files_to_sync = [
             f
             for f in all_files
@@ -52,10 +47,8 @@ class FileBasedState:
                 and (not self._file_to_datetime_history or not self.is_history_complete() or f.uri not in self._file_to_datetime_history)
             )
         ]
-        logging.warning(f"files to sync: {files_to_sync}")
         # If len(files_to_sync), the next sync will not be able to use the history
         if len(files_to_sync) > self._max_history_size:
-            logging.warning(f"History will be too large")
             self._history_is_complete = False
         else:
             self._history_is_complete = True
@@ -68,8 +61,6 @@ class FileBasedState:
             earliest = min(self._file_to_datetime_history.values())
             earliest_dt = datetime.strptime(earliest, "%Y-%m-%dT%H:%M:%S.%fZ")
             if not self.is_history_complete():
-                logging.warning(f"History is incomplete")
                 time_window = datetime.now() - self._time_window_if_history_is_full
                 earliest_dt = min(earliest_dt, time_window)
-            logging.warning(f"earliest_dt: {earliest_dt}")
             return earliest_dt
