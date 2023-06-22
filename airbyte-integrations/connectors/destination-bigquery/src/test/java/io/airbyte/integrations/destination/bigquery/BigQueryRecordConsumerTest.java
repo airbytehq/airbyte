4dd2e84d6c2327addf4f2ada084403ff4c5bf697
Edward Gao <edward.gao@airbyte.io>
2023-06-22 15:39:32 -0700

    fix test

--- airbyte-integrations/connectors/destination-bigquery/src/test/java/io/airbyte/integrations/destination/bigquery/BigQueryRecordConsumerTest.java
+++ airbyte-integrations/connectors/destination-bigquery/src/test/java/io/airbyte/integrations/destination/bigquery/BigQueryRecordConsumerTest.java
@@ -4,28 +4,53 @@
 
 package io.airbyte.integrations.destination.bigquery;
 
+import com.google.cloud.bigquery.BigQuery;
 import io.airbyte.integrations.base.FailureTrackingAirbyteMessageConsumer;
+import io.airbyte.integrations.base.destination.typing_deduping.CatalogParser;
+import io.airbyte.integrations.destination.bigquery.typing_deduping.BigQueryDestinationHandler;
+import io.airbyte.integrations.destination.bigquery.typing_deduping.BigQuerySqlGenerator;
 import io.airbyte.integrations.destination.bigquery.uploader.AbstractBigQueryUploader;
 import io.airbyte.integrations.standardtest.destination.PerStreamStateMessageTest;
 import io.airbyte.protocol.models.v0.AirbyteMessage;
 import io.airbyte.protocol.models.v0.AirbyteStreamNameNamespacePair;
+
+import java.util.Collections;
 import java.util.Map;
 import java.util.function.Consumer;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import static org.mockito.Mockito.mock;
+
 @ExtendWith(MockitoExtension.class)
 public class BigQueryRecordConsumerTest extends PerStreamStateMessageTest {
 
   @Mock
-  private Map<AirbyteStreamNameNamespacePair, AbstractBigQueryUploader<?>> uploaderMap;
+  private Map<BigQueryRecordConsumer.StreamWriteTargets, AbstractBigQueryUploader<?>> uploaderMap;
   @Mock
   private Consumer<AirbyteMessage> outputRecordCollector;
 
-  @InjectMocks
   private BigQueryRecordConsumer bigQueryRecordConsumer;
+
+  @BeforeEach
+  public void setup() {
+    bigQueryRecordConsumer = new BigQueryRecordConsumer(
+        mock(BigQuery.class),
+        uploaderMap,
+        outputRecordCollector,
+        "test-dataset-id",
+        mock(BigQuerySqlGenerator.class),
+        mock(BigQueryDestinationHandler.class),
+        new CatalogParser.ParsedCatalog(Collections.emptyList()),
+        false,
+        "test-namespace-override"
+    );
+  }
 
   @Override
   protected Consumer<AirbyteMessage> getMockedConsumer() {
