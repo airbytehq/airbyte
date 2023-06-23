beb0ed726a5a7dc12dbb8ce31a67f24d4371eb9a
Ella Rohm-Ensing <erohmensing@gmail.com>
2023-06-23 16:00:15 -0500

    Validate that images referenced in breakingChanges exist on dockerhub (#27451)
    * Add check that docker images in breakingChanges exist
    
    * Add invalid test
    
    * pull changes back together
    
    * put em together
    
    * Assert that the ValueError is a validation error
    
    * make way too many changes at once lol
    
    * fix typo
    
    * update method description
    
    * Run tests on all valid upload files, fix jsonschema issues
    
    * Change mock logic
    
    * don't check base:base image
    
    * fix existing images and add new test cases
    
    * reset mocks per test case
    
    * attempt to improve renaming diff
    
    * explicity check the base combos
    
    * move override files - they're invalid if the base image doesn't exist
    
    * make it clearer when a tag won't exist
    
    Co-authored-by: Ben Church <ben@airbyte.io>
    
    * named constant for versions that don't exist
    
    ---------
    
    Co-authored-by: Ben Church <ben@airbyte.io>

--- airbyte-ci/connectors/metadata_service/lib/tests/test_gcs_upload.py
+++ airbyte-ci/connectors/metadata_service/lib/tests/test_gcs_upload.py
@@ -12,9 +12,12 @@ from metadata_service import gcs_upload
 from metadata_service.models.generated.ConnectorMetadataDefinitionV0 import ConnectorMetadataDefinitionV0
 from metadata_service.constants import METADATA_FILE_NAME
 
+# Version exists by default, but "666" is bad! (6.0.0 too since breaking changes regex tho)
+MOCK_VERSIONS_THAT_DO_NOT_EXIST = ["6.6.6", "6.0.0"]
+
 
 def stub_is_image_on_docker_hub(image_name: str, version: str) -> bool:
-    return "exists" in image_name and "exists" in version
+    return "exists" in image_name and version not in MOCK_VERSIONS_THAT_DO_NOT_EXIST
 
 
 def setup_upload_mocks(mocker, version_blob_md5_hash, latest_blob_md5_hash, local_file_md5_hash):
@@ -78,47 +81,48 @@ def setup_upload_mocks(mocker, version_blob_md5_hash, latest_blob_md5_hash, loca
     ],
 )
 def test_upload_metadata_to_gcs_valid_metadata(
-    mocker, valid_metadata_yaml_files, version_blob_md5_hash, latest_blob_md5_hash, local_file_md5_hash
+    mocker, valid_metadata_upload_files, version_blob_md5_hash, latest_blob_md5_hash, local_file_md5_hash
 ):
-    mocks = setup_upload_mocks(mocker, version_blob_md5_hash, latest_blob_md5_hash, local_file_md5_hash)
+    for valid_metadata_upload_file in valid_metadata_upload_files:
+        mocks = setup_upload_mocks(mocker, version_blob_md5_hash, latest_blob_md5_hash, local_file_md5_hash)
 
-    metadata_file_path = pathlib.Path(valid_metadata_yaml_files[0])
-    metadata = ConnectorMetadataDefinitionV0.parse_obj(yaml.safe_load(metadata_file_path.read_text()))
-    expected_version_key = f"metadata/{metadata.data.dockerRepository}/{metadata.data.dockerImageTag}/{METADATA_FILE_NAME}"
-    expected_latest_key = f"metadata/{metadata.data.dockerRepository}/latest/{METADATA_FILE_NAME}"
+        metadata_file_path = pathlib.Path(valid_metadata_upload_file)
+        metadata = ConnectorMetadataDefinitionV0.parse_obj(yaml.safe_load(metadata_file_path.read_text()))
+        expected_version_key = f"metadata/{metadata.data.dockerRepository}/{metadata.data.dockerImageTag}/{METADATA_FILE_NAME}"
+        expected_latest_key = f"metadata/{metadata.data.dockerRepository}/latest/{METADATA_FILE_NAME}"
 
-    latest_blob_exists = latest_blob_md5_hash is not None
-    version_blob_exists = version_blob_md5_hash is not None
+        latest_blob_exists = latest_blob_md5_hash is not None
+        version_blob_exists = version_blob_md5_hash is not None
 
-    # Call function under tests
+        # Call function under tests
 
-    uploaded, blob_id = gcs_upload.upload_metadata_to_gcs(
-        "my_bucket",
-        metadata_file_path,
-    )
+        uploaded, blob_id = gcs_upload.upload_metadata_to_gcs(
+            "my_bucket",
+            metadata_file_path,
+        )
 
-    # Assertions
+        # Assertions
 
-    gcs_upload.service_account.Credentials.from_service_account_info.assert_called_with(json.loads(mocks["service_account_json"]))
-    mocks["mock_storage_client"].bucket.assert_called_with("my_bucket")
-    mocks["mock_bucket"].blob.assert_has_calls([mocker.call(expected_version_key), mocker.call(expected_latest_key)])
-    assert blob_id == mocks["mock_version_blob"].id
+        gcs_upload.service_account.Credentials.from_service_account_info.assert_called_with(json.loads(mocks["service_account_json"]))
+        mocks["mock_storage_client"].bucket.assert_called_with("my_bucket")
+        mocks["mock_bucket"].blob.assert_has_calls([mocker.call(expected_version_key), mocker.call(expected_latest_key)])
+        assert blob_id == mocks["mock_version_blob"].id
 
-    if not version_blob_exists:
-        mocks["mock_version_blob"].upload_from_filename.assert_called_with(metadata_file_path)
-        assert uploaded
+        if not version_blob_exists:
+            mocks["mock_version_blob"].upload_from_filename.assert_called_with(metadata_file_path)
+            assert uploaded
 
-    if not latest_blob_exists:
-        mocks["mock_latest_blob"].upload_from_filename.assert_called_with(metadata_file_path)
-        assert uploaded
+        if not latest_blob_exists:
+            mocks["mock_latest_blob"].upload_from_filename.assert_called_with(metadata_file_path)
+            assert uploaded
 
-    if version_blob_md5_hash != local_file_md5_hash:
-        mocks["mock_version_blob"].upload_from_filename.assert_called_with(metadata_file_path)
-        assert uploaded
+        if version_blob_md5_hash != local_file_md5_hash:
+            mocks["mock_version_blob"].upload_from_filename.assert_called_with(metadata_file_path)
+            assert uploaded
 
-    if latest_blob_md5_hash != local_file_md5_hash:
-        mocks["mock_latest_blob"].upload_from_filename.assert_called_with(metadata_file_path)
-        assert uploaded
+        if latest_blob_md5_hash != local_file_md5_hash:
+            mocks["mock_latest_blob"].upload_from_filename.assert_called_with(metadata_file_path)
+            assert uploaded
 
 
 def test_upload_metadata_to_gcs_non_existent_metadata_file():
