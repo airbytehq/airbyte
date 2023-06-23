43c9427893eb101660e3b91a00ea77702621dc0b
Denys Davydov <davydov.den18@gmail.com>
2023-06-23 17:48:06 +0300

    #2294 Source Hubspot: use fully qualified name to access custom objects

--- airbyte-integrations/connectors/source-hubspot/unit_tests/test_streams.py
+++ airbyte-integrations/connectors/source-hubspot/unit_tests/test_streams.py
@@ -348,21 +348,10 @@ def expected_custom_object_json_schema():
 def test_custom_object_stream_doesnt_call_hubspot_to_get_json_schema_if_available(
     requests_mock, custom_object_schema, expected_custom_object_json_schema, common_params
 ):
-    stream = CustomObject(entity="animals", schema=expected_custom_object_json_schema, **common_params)
+    stream = CustomObject(entity="animals", schema=expected_custom_object_json_schema, fully_qualified_name="p123_animals", **common_params)
 
     adapter = requests_mock.register_uri("GET", "/crm/v3/schemas", [{"json": {"results": [custom_object_schema]}}])
     json_schema = stream.get_json_schema()
 
     assert json_schema == expected_custom_object_json_schema
     assert not adapter.called
-
-
-def test_custom_object_stream_calls_hubspot_to_get_json_schema(
-    requests_mock, custom_object_schema, expected_custom_object_json_schema, common_params
-):
-    stream = CustomObject(entity="animals", schema=None, **common_params)
-
-    adapter = requests_mock.register_uri("GET", "/crm/v3/schemas", [{"json": {"results": [custom_object_schema]}}])
-    json_schema = stream.get_json_schema()
-    assert json_schema == expected_custom_object_json_schema
-    assert adapter.called
