d3c33a6321f8e6d1617ccfbd7a7bc762dacf9e1e
Augustin <augustin@airbyte.io>
2023-06-24 11:17:46 +0200

    connector-ci: better and lighter logging (#27606)

--- tools/ci_connector_ops/ci_connector_ops/pipelines/tests/__init__.py
+++ tools/ci_connector_ops/ci_connector_ops/pipelines/tests/__init__.py
@@ -37,7 +37,6 @@ async def run_metadata_validation(context: ConnectorContext) -> List[StepResult]
     Returns:
         List[StepResult]: The results of the metadata validation steps.
     """
-    context.logger.info("Run metadata validation.")
     return [await MetadataValidation(context, context.connector.code_directory / METADATA_FILE_NAME).run()]
 
 
@@ -50,7 +49,6 @@ async def run_version_checks(context: ConnectorContext) -> List[StepResult]:
     Returns:
         List[StepResult]: The results of the version checks steps.
     """
-    context.logger.info("Run version checks.")
     return [await VersionFollowsSemverCheck(context).run(), await VersionIncrementCheck(context).run()]
 
 
@@ -63,7 +61,6 @@ async def run_qa_checks(context: ConnectorContext) -> List[StepResult]:
     Returns:
         List[StepResult]: The results of the QA checks steps.
     """
-    context.logger.info("Run QA checks.")
     return [await QaChecks(context).run()]
 
 
@@ -77,7 +74,6 @@ async def run_code_format_checks(context: ConnectorContext) -> List[StepResult]:
         List[StepResult]: The results of the code format checks steps.
     """
     if _run_code_format_checks := LANGUAGE_MAPPING["run_code_format_checks"].get(context.connector.language):
-        context.logger.info("Run code format checks.")
         return await _run_code_format_checks(context)
     else:
         context.logger.warning(f"No code format checks defined for connector language {context.connector.language}!")
