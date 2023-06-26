f6f5515f9dc643128b45312bbf16562c7fa31ee5
jdpgrailsdev <jpearlin1@gmail.com>
2023-06-26 10:11:14 -0400

    Use ASCII and flush periodically for parallel streams

--- airbyte-integrations/bases/base-java/src/main/java/io/airbyte/integrations/util/concurrent/ConcurrentMessageConsumer.java
+++ airbyte-integrations/bases/base-java/src/main/java/io/airbyte/integrations/util/concurrent/ConcurrentMessageConsumer.java
@@ -11,10 +11,12 @@ import java.io.FileDescriptor;
 import java.io.FileOutputStream;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
+import java.nio.charset.StandardCharsets;
+import java.util.concurrent.atomic.AtomicLong;
 import java.util.function.Consumer;
 
 /**
- * Custom {@link Consumer} used to serialze messages to standard out.
+ * Custom {@link Consumer} used to serialize messages to standard out.
  * <p>
  * Using {@code System.out.println} in a multi-threaded environment comes with a large performance
  * penalty, as the {@code println} method is synchronized. Instead, use a {@link BufferedWriter} to
@@ -23,18 +25,26 @@ import java.util.function.Consumer;
  */
 public class ConcurrentMessageConsumer implements Consumer<AirbyteMessage>, AutoCloseable {
 
+  private final AtomicLong flushCounter;
+
   private final PrintWriter out;
 
   public ConcurrentMessageConsumer() {
+    flushCounter = new AtomicLong(0);
     out = new PrintWriter(
-        new BufferedWriter(new OutputStreamWriter(new FileOutputStream(FileDescriptor.out))));
+        new BufferedWriter(
+            new OutputStreamWriter(
+                new FileOutputStream(FileDescriptor.out), StandardCharsets.US_ASCII)));
   }
 
   @Override
   public void accept(final AirbyteMessage airbyteMessage) {
     out.write(Jsons.serialize(airbyteMessage));
     out.write(System.lineSeparator());
-    out.flush();
+
+    if (flushCounter.incrementAndGet() % 1000 == 0) {
+      out.flush();
+    }
   }
 
   @Override
