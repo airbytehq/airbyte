4273dc8f8e4886377c805aff960f35d56886e5f8
Octavia Squidington III <octavia-squidington-iii@sers.noreply.github.com>
2023-06-26 17:52:46 -0600

    Get metadata entry

--- airbyte-ci/connectors/metadata_service/orchestrator/orchestrator/assets/registry_entry.py
+++ airbyte-ci/connectors/metadata_service/orchestrator/orchestrator/assets/registry_entry.py
@@ -1,14 +1,26 @@
-from dagster import (
-    DynamicPartitionsDefinition,
-    asset,
-)
+import pandas as pd
+import numpy as np
+import os
+from typing import List
+from dagster import DynamicPartitionsDefinition, asset, OpExecutionContext
+import yaml
+
+from metadata_service.models.generated.ConnectorMetadataDefinitionV0 import ConnectorMetadataDefinitionV0
+from metadata_service.constants import METADATA_FILE_NAME, ICON_FILE_NAME
+
+from orchestrator.utils.object_helpers import are_values_equal, merge_values
+from orchestrator.models.metadata import PartialMetadataDefinition, MetadataDefinition, LatestMetadataEntry
+from orchestrator.config import get_public_url_for_gcs_file
+
+from orchestrator.utils.dagster_helpers import OutputDataFrame, output_dataframe
+
 
 GROUP_NAME = "registry_entry"
 
 metadata_partitions_def = DynamicPartitionsDefinition(name="metadata")
 
 @asset(required_resource_keys={"latest_metadata_file_blobs"}, group_name=GROUP_NAME, partitions_def=metadata_partitions_def)
-def registry_entry(context):
+def metadata_entry(context: OpExecutionContext) -> LatestMetadataEntry:
     etag = context.partition_key
     latest_metadata_file_blobs = context.resources.latest_metadata_file_blobs
 
@@ -20,6 +32,32 @@ def registry_entry(context):
     if not matching_blob:
         raise Exception(f"Could not find blob with etag {etag}")
 
-    # read the blob
-    filename = matching_blob.name
-    return filename
+    # read the matching_blob
+    yaml_string = matching_blob.download_as_string().decode("utf-8")
+    metadata_dict = yaml.safe_load(yaml_string)
+    metadata_def = MetadataDefinition.parse_obj(metadata_dict)
+
+    metadata_file_path = matching_blob.name
+    icon_file_path = metadata_file_path.replace(METADATA_FILE_NAME, ICON_FILE_NAME)
+    icon_blob = matching_blob.bucket.blob(icon_file_path)
+
+    icon_url = (
+        get_public_url_for_gcs_file(icon_blob.bucket.name, icon_blob.name, os.getenv("METADATA_CDN_BASE_URL"))
+        if icon_blob.exists()
+        else None
+    )
+
+    metadata_entry = LatestMetadataEntry(
+        metadata_definition=metadata_def,
+        icon_url=icon_url,
+        bucket_name=matching_blob.bucket.name,
+        file_path=metadata_file_path,
+    )
+    # import pdb; pdb.set_trace()
+    return metadata_entry
+
+@asset(group_name=GROUP_NAME, partitions_def=metadata_partitions_def)
+def registry_entry(context: OpExecutionContext, metadata_entry: LatestMetadataEntry) -> OutputDataFrame:
+    metadata_entry_df = pd.DataFrame(metadata_entry.dict())
+    return output_dataframe(metadata_entry_df)
+
--- airbyte-ci/connectors/metadata_service/orchestrator/tests/test_debug.py
+++ airbyte-ci/connectors/metadata_service/orchestrator/tests/test_debug.py
@@ -2,8 +2,10 @@ from dagster import build_op_context
 
 from orchestrator.resources.gcp import gcp_gcs_client, gcs_directory_blobs, gcs_file_manager
 from orchestrator.assets.connector_test_report import generate_nightly_report, persist_connectors_test_summary_files
+from orchestrator.assets.registry_entry import registry_entry, metadata_entry
 from orchestrator.config import NIGHTLY_INDIVIDUAL_TEST_REPORT_FILE_NAME, NIGHTLY_FOLDER, NIGHTLY_COMPLETE_REPORT_FILE_NAME, REPORT_FOLDER
 
+from metadata_service.constants import METADATA_FILE_NAME, METADATA_FOLDER
 
 def debug_nightly_report():
     resources = {
@@ -38,3 +40,21 @@ def debug_badges():
 
     context = build_op_context(resources=resources)
     persist_connectors_test_summary_files(context).value
+
+def test_debug_registry_entry():
+    resources = {
+        "gcp_gcs_client": gcp_gcs_client.configured(
+            {
+                "gcp_gcs_cred_string": {"env": "GCS_CREDENTIALS"},
+            }
+        ),
+        "latest_metadata_file_blobs": gcs_directory_blobs.configured(
+            {"gcs_bucket": {"env": "METADATA_BUCKET"}, "prefix": METADATA_FOLDER, "match_regex": f".*latest/{METADATA_FILE_NAME}$"}
+        )
+    }
+
+    part_key = "CPuD29SE4v8CEAE="
+
+    context = build_op_context(resources=resources , partition_key=part_key)
+    metadata_entry_val = metadata_entry(context)
+
