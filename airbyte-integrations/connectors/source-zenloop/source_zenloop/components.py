0bc8d4af623bfe05b9704d54b66434adda988075
maxi297 <maxime@airbyte.io>
2023-06-27 11:17:35 -0400

    Update zenloop stream_slices

--- airbyte-integrations/connectors/source-zenloop/source_zenloop/components.py
+++ airbyte-integrations/connectors/source-zenloop/source_zenloop/components.py
@@ -16,7 +16,7 @@ class ZenloopPartitionRouter(SubstreamPartitionRouter):
 
     config: Config
 
-    def stream_slices(self, sync_mode: SyncMode, stream_state: StreamState) -> Iterable[StreamSlice]:
+    def stream_slices(self) -> Iterable[StreamSlice]:
         """
 
         config_parent_field : parent field name in config
@@ -29,7 +29,7 @@ class ZenloopPartitionRouter(SubstreamPartitionRouter):
         custom_stream_state_value = self.config.get(parent_field)
 
         if not custom_stream_state_value:
-            yield from super().stream_slices(sync_mode, stream_state)
+            yield from super().stream_slices()
         else:
             for parent_stream_config in self.parent_stream_configs:
                 stream_state_field = parent_stream_config.partition_field.eval(self.config)
