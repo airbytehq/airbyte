34d2f48619d832e2e8f5025b8e6a469abb1ee331
Mal Hancock <mallory@archangelic.space>
2023-06-27 11:02:10 -0700

    fix merge errors

--- airbyte-integrations/connectors/source-twilio/unit_tests/test_source.py
+++ airbyte-integrations/connectors/source-twilio/unit_tests/test_source.py
@@ -102,7 +102,10 @@ def test_check_connection_handles_exceptions(mocker, config, exception, expected
         (UsageRecords),
         (UsageTriggers),
         (Conversations),
-        (ConversationParticipants)
+        (ConversationParticipants),
+        (Users),
+        (UserConversations),
+        (VerifyServices),
     ],
 )
 def test_streams(stream_cls, config):
