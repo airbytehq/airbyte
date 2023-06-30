c96424ff4733d301e1505b6a4d434753424bc1e3
Rodi Reich Zilberman <867491+rodireich@users.noreply.github.com>
2023-06-30 13:57:45 -0700

    fix test

--- airbyte-integrations/connectors/source-postgres/src/test/java/io/airbyte/integrations/source/postgres/ctid/PostgresCtidHandlerTest.java
+++ airbyte-integrations/connectors/source-postgres/src/test/java/io/airbyte/integrations/source/postgres/ctid/PostgresCtidHandlerTest.java
@@ -12,38 +12,37 @@ public class PostgresCtidHandlerTest {
   void testCtidQueryBounds() {
     var chunks = PostgresCtidHandler.ctidQueryPlan(new Ctid(0,0),380545032192L, 8192L, 50);
     var expected = List.of(
-        Pair.of(Ctid.of(0,0), Ctid.of(6400000, 0)),
-        Pair.of(Ctid.of(6400000,0), Ctid.of(12800000, 0)),
-        Pair.of(Ctid.of(12800000,0), Ctid.of(19200000, 0)),
-        Pair.of(Ctid.of(19200000,0), Ctid.of(25600000, 0)),
-        Pair.of(Ctid.of(25600000,0), Ctid.of(32000000, 0)),
-        Pair.of(Ctid.of(32000000,0), Ctid.of(38400000, 0)),
-        Pair.of(Ctid.of(38400000,0), Ctid.of(44800000, 0)),
-        Pair.of(Ctid.of(44800000,0), null));
-    assertEquals(chunks, expected);
+        Pair.of(Ctid.of(0,0), Ctid.of(6553600, 0)),
+        Pair.of(Ctid.of(6553600,0), Ctid.of(13107200, 0)),
+        Pair.of(Ctid.of(13107200,0), Ctid.of(19660800, 0)),
+        Pair.of(Ctid.of(19660800,0), Ctid.of(26214400, 0)),
+        Pair.of(Ctid.of(26214400,0), Ctid.of(32768000, 0)),
+        Pair.of(Ctid.of(32768000,0), Ctid.of(39321600, 0)),
+        Pair.of(Ctid.of(39321600,0), Ctid.of(45875200, 0)),
+        Pair.of(Ctid.of(45875200,0), null));
+    assertEquals(expected, chunks);
 
     chunks = PostgresCtidHandler.ctidQueryPlan(new Ctid("(23000000,123)"),380545032192L, 8192L, 45);
     expected = List.of(
-        Pair.of(Ctid.of("(23000000,123)"), Ctid.of(28760000, 0)),
-        Pair.of(Ctid.of(28760000,0), Ctid.of("(34520000,0)")),
-        Pair.of(Ctid.of("(34520000,0)"), Ctid.of(40280000,0)),
-        Pair.of(Ctid.of(40280000,0), Ctid.of("(46040000,0)")),
-        Pair.of(Ctid.of(46040000,0), null));
-    assertEquals(chunks, expected);
+        Pair.of(Ctid.of("(23000000,123)"), Ctid.of(28898240, 0)),
+        Pair.of(Ctid.of(28898240,0), Ctid.of("(34796480,0)")),
+        Pair.of(Ctid.of("(34796480,0)"), Ctid.of(40694720,0)),
+        Pair.of(Ctid.of(40694720,0),null));
+    assertEquals(expected, chunks);
 
     chunks = PostgresCtidHandler.ctidQueryPlan(Ctid.of(0,0),380545L, 8192L, 45);
     expected = List.of(
         Pair.of(Ctid.of(0, 0), null));
-    assertEquals(chunks, expected);
+    assertEquals(expected, chunks);
 
     chunks = PostgresCtidHandler.ctidQueryPlan(Ctid.of(9876,5432),380545L, 8192L, 45);
     expected = List.of(
         Pair.of(Ctid.of(9876, 5432), null));
-    assertEquals(chunks, expected);
+    assertEquals(expected, chunks);
 
     chunks = PostgresCtidHandler.ctidQueryPlan(Ctid.of(0,0),4096L, 8192L, 45);
     expected = List.of(
         Pair.of(Ctid.of(0, 0), null));
-    assertEquals(chunks, expected);
+    assertEquals(expected, chunks);
   }
 }
