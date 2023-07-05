5f1186c1edb58772533bc3b945f261e8ad731ed4
Satish Ch <satishchinthanippu@gmail.com>
2023-07-05 10:06:34 -0700

    Integrated clearscape for teradata instance

--- airbyte-integrations/connectors/destination-teradata/src/test-integration/java/io/airbyte/integrations/destination/teradata/TeradataDestinationAcceptanceTest.java
+++ airbyte-integrations/connectors/destination-teradata/src/test-integration/java/io/airbyte/integrations/destination/teradata/TeradataDestinationAcceptanceTest.java
@@ -4,8 +4,6 @@
 
 package io.airbyte.integrations.destination.teradata;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import io.airbyte.commons.json.Jsons;
@@ -19,8 +17,19 @@ import io.airbyte.db.jdbc.JdbcUtils;
 import io.airbyte.integrations.base.AirbyteTraceMessageUtility;
 import io.airbyte.integrations.base.JavaBaseConstants;
 import io.airbyte.integrations.destination.StandardNameTransformer;
+import io.airbyte.integrations.destination.teradata.envclient.TeradataHttpClient;
+import io.airbyte.integrations.destination.teradata.envclient.dto.CreateEnvironmentRequest;
+import io.airbyte.integrations.destination.teradata.envclient.dto.DeleteEnvironmentRequest;
 import io.airbyte.integrations.standardtest.destination.JdbcDestinationAcceptanceTest;
 import io.airbyte.protocol.models.v0.AirbyteConnectionStatus;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.sql.DataSource;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.sql.SQLException;
@@ -28,15 +37,24 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
-import javax.sql.DataSource;
-import org.junit.jupiter.api.Test;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import java.util.concurrent.ExecutionException;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class TeradataDestinationAcceptanceTest extends JdbcDestinationAcceptanceTest {
 
   private static final Logger LOGGER = LoggerFactory.getLogger(TeradataDestinationAcceptanceTest.class);
   private final StandardNameTransformer namingResolver = new StandardNameTransformer();
+
+ private static final String SCHEMA_NAME = Strings.addRandomSuffix("acc_test", "_", 5);
+
+  private static final String CREATE_DATABASE = "CREATE DATABASE \"%s\" AS PERMANENT = 60e6, SPOOL = 60e6 SKEW = 10 PERCENT";
+
+  private static final String DELETE_DATABASE = "DELETE DATABASE \"%s\"";
+
+  private static final String DROP_DATABASE = "DROP DATABASE \"%s\"";
 
   private JsonNode configJson;
   private JdbcDatabase database;
@@ -54,9 +72,28 @@ public class TeradataDestinationAcceptanceTest extends JdbcDestinationAcceptance
     return configJson;
   }
 
+  @BeforeAll
+  void initEnvironment() throws Exception {
+    this.configJson = Jsons.clone(getStaticConfig());
+    TeradataHttpClient teradataHttpClient = new TeradataHttpClient(configJson.get("env_url").asText());
+    var request = new CreateEnvironmentRequest(
+            configJson.get("env_name").asText(),
+            configJson.get("env_region").asText(),
+            configJson.get("env_password").asText());
+    var response = teradataHttpClient.createEnvironment(request, configJson.get("env_token").asText()).get();
+    ((ObjectNode) configJson).put("host", response.ip());
+
+  }
+
+   @AfterAll
+  void cleanupEnvironment() throws ExecutionException, InterruptedException {
+    TeradataHttpClient teradataHttpClient = new TeradataHttpClient(configJson.get("env_url").asText());
+    var request = new DeleteEnvironmentRequest(configJson.get("env_name").asText());
+    teradataHttpClient.deleteEnvironment(request, configJson.get("env_token").asText()).get();
+  }
+
   public JsonNode getStaticConfig() throws Exception {
-    final JsonNode config = Jsons.deserialize(Files.readString(Paths.get("secrets/config.json")));
-    return config;
+  	 return Jsons.deserialize(Files.readString(Paths.get("secrets/config.json")));
   }
 
   @Override
@@ -109,6 +146,19 @@ public class TeradataDestinationAcceptanceTest extends JdbcDestinationAcceptance
   }
 
   @Override
+  protected void tearDown(TestDestinationEnv testEnv) {
+    final String deleteQuery = String.format(String.format(DELETE_DATABASE, SCHEMA_NAME));
+    final String dropQuery = String.format(String.format(DROP_DATABASE, SCHEMA_NAME));
+    try {
+      database.execute(deleteQuery);
+      database.execute(dropQuery);
+    } catch (Exception e) {
+      AirbyteTraceMessageUtility.emitSystemErrorTrace(e, "Database " + SCHEMA_NAME + " delete got failed.");
+    }
+  }
+
+
+  @Override
   @Test
   public void testLineBreakCharacters() {
     // overrides test in coming releases
@@ -119,9 +169,6 @@ public class TeradataDestinationAcceptanceTest extends JdbcDestinationAcceptance
   public void testSecondSync() {
     // overrides test in coming releases
   }
-
-  @Override
-  protected void tearDown(TestDestinationEnv testEnv) {}
 
   protected DataSource getDataSource(final JsonNode config) {
     final JsonNode jdbcConfig = destination.toJdbcConfig(config);
--- airbyte-integrations/connectors/destination-teradata/src/test-integration/java/io/airbyte/integrations/destination/teradata/TeradataDestinationSSLAcceptanceTest.java
+++ airbyte-integrations/connectors/destination-teradata/src/test-integration/java/io/airbyte/integrations/destination/teradata/TeradataDestinationSSLAcceptanceTest.java
@@ -4,15 +4,24 @@
 
 package io.airbyte.integrations.destination.teradata;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.google.common.collect.ImmutableMap;
+import io.airbyte.commons.json.Jsons;
+import org.junit.jupiter.api.TestInstance;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class TeradataDestinationSSLAcceptanceTest extends TeradataDestinationAcceptanceTest {
 
   private static final Logger LOGGER = LoggerFactory.getLogger(TeradataDestinationSSLAcceptanceTest.class);
 
-  protected String getConfigFileName() {
-    return "secrets/sslconfig.json";
+ public JsonNode getStaticConfig() throws Exception {
+    return Jsons.deserialize(Files.readString(Paths.get("secrets/sslconfig.json")));
   }
 
 }
