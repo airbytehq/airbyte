51929ffd7d65173238ef0dac08b5b07891afc8b9
Alexandre Girard <alexandre@airbyte.io>
2023-07-06 10:40:26 -0700

    format

--- airbyte-cdk/python/airbyte_cdk/sources/file_based/config/file_based_stream_config.py
+++ airbyte-cdk/python/airbyte_cdk/sources/file_based/config/file_based_stream_config.py
@@ -7,7 +7,6 @@ from enum import Enum
 from typing import Any, Dict, List, Mapping, Optional, Union
 
 from airbyte_cdk.models import ConfiguredAirbyteCatalog
-from airbyte_cdk.sources.file_based.schema_validation_policies import AbstractSchemaValidationPolicy
 from pydantic import BaseModel, root_validator, validator
 
 PrimaryKeyType = Optional[Union[str, List[str], List[List[str]]]]
