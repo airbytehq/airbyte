fe0720df184d55aa2cceb8bc3d1610ca670e3d79
Alexandre Girard <alexandre@airbyte.io>
2023-07-06 12:03:34 -0700

    basic inferrence works

--- airbyte-cdk/python/airbyte_cdk/sources/file_based/file_types/parquet_parser.py
+++ airbyte-cdk/python/airbyte_cdk/sources/file_based/file_types/parquet_parser.py
@@ -8,6 +8,7 @@ from airbyte_cdk.sources.file_based.config.file_based_stream_config import FileB
 from airbyte_cdk.sources.file_based.file_based_stream_reader import AbstractFileBasedStreamReader
 from airbyte_cdk.sources.file_based.file_types.file_type_parser import FileTypeParser
 from airbyte_cdk.sources.file_based.remote_file import RemoteFile
+import pyarrow as pa
 import pyarrow.parquet as pq
 
 
@@ -15,15 +16,19 @@ class ParquetParser(FileTypeParser):
     async def infer_schema(
         self, config: FileBasedStreamConfig, file: RemoteFile, stream_reader: AbstractFileBasedStreamReader
     ) -> Dict[str, Any]:
-        raise NotImplementedError()
+        table = self._read_file(file, stream_reader)
+        schema = {field.name: {"type": str(field.type)} for field in table.schema}
+        return schema
 
     def parse_records(
         self, config: FileBasedStreamConfig, file: RemoteFile, stream_reader: AbstractFileBasedStreamReader
     ) -> Iterable[Dict[str, Any]]:
-
-        table = pq.read_table(stream_reader.open_file(file))
-
+        table = self._read_file(file, stream_reader)
         for batch in table.to_batches():
             for i in range(batch.num_rows):
                 row_dict = {column: batch.column(column)[i].as_py() for column in table.column_names}
                 yield row_dict
+
+    @staticmethod
+    def _read_file(file: RemoteFile, stream_reader: AbstractFileBasedStreamReader) -> pa.Table:
+        return pq.read_table(stream_reader.open_file(file))
