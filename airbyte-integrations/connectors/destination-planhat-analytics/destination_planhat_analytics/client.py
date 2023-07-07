c88a819bbed4bc119ad4cb44b7f54b72f5bb54cd
KimPlv <kim.plavonil@sendinblue.com>
2023-07-07 13:50:56 +0200

    spec + check

---
+++ airbyte-integrations/connectors/destination-planhat-analytics/destination_planhat_analytics/client.py
@@ -0,0 +1,29 @@
+from typing import Any, Mapping, List
+
+import requests
+import json
+
+class PlanHatClient:
+
+    def __init__(self, tenant_uuid: str, batch_size: str, api_method: Mapping[str, Any]) -> None:
+        self.tenant_uuid = tenant_uuid
+        self.batch_size = batch_size
+        self.endpoint = api_method.get('url')
+        self.api_token = api_method.get('api_token', None)
+
+    def write(self, request_body: List[Mapping]):
+        return self._request("POST", data=request_body) 
+    
+    def _get_base_url(self) -> str:
+        return f"{self.endpoint}/{self.tenant_uuid}"
+
+    def _get_auth_headers(self) -> Mapping[str, Any]:
+        return {"'Content-Type'": "application/json", "Authorization": f"Bearer {self.api_token}"} if self.api_token else {"'Content-Type'": "application/json"}
+    
+    def _request(
+        self, http_method: str, data: List[Mapping] = None
+    ) -> requests.Response:
+        url = self._get_base_url()
+        headers = self._get_auth_headers()
+        response = requests.request(method=http_method, url=url, headers=headers, data=json.dumps(data))
+        return response.status_code
--- airbyte-integrations/connectors/destination-planhat-analytics/destination_planhat_analytics/destination.py
+++ airbyte-integrations/connectors/destination-planhat-analytics/destination_planhat_analytics/destination.py
@@ -9,6 +9,7 @@ from airbyte_cdk import AirbyteLogger
 from airbyte_cdk.destinations import Destination
 from airbyte_cdk.models import AirbyteConnectionStatus, AirbyteMessage, ConfiguredAirbyteCatalog, Status
 
+from .client import PlanHatClient
 
 class DestinationPlanhatAnalytics(Destination):
     def write(
@@ -34,20 +35,9 @@ class DestinationPlanhatAnalytics(Destination):
         pass
 
     def check(self, logger: AirbyteLogger, config: Mapping[str, Any]) -> AirbyteConnectionStatus:
-        """
-        Tests if the input configuration can be used to successfully connect to the destination with the needed permissions
-            e.g: if a provided API token or password can be used to connect and write to the destination.
-
-        :param logger: Logging object to display debug/info/error to the logs
-            (logs will not be accessible via airbyte UI if they are not passed to this logger)
-        :param config: Json object containing the configuration of this destination, content of this json is as specified in
-        the properties of the spec.json file
-
-        :return: AirbyteConnectionStatus indicating a Success or Failure
-        """
         try:
-            # TODO
-
+            client = PlanHatClient(**config)
+            client.write([])
             return AirbyteConnectionStatus(status=Status.SUCCEEDED)
         except Exception as e:
             return AirbyteConnectionStatus(status=Status.FAILED, message=f"An exception occurred: {repr(e)}")
