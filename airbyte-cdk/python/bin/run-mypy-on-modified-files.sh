076b8583eadf3ec44d19de9383efe30a6b872b96
Alexandre Girard <alexandre@airbyte.io>
2023-07-10 15:34:10 -0700

    try without origin

--- airbyte-cdk/python/bin/run-mypy-on-modified-files.sh
+++ airbyte-cdk/python/bin/run-mypy-on-modified-files.sh
@@ -1,2 +1,2 @@
 set -e
-git diff --name-only --relative --diff-filter=d origin/master -- . | grep -E '\.py$' | xargs mypy --config-file mypy.ini --install-types --non-interactive
+git diff --name-only --relative --diff-filter=d master -- . | grep -E '\.py$' | xargs mypy --config-file mypy.ini --install-types --non-interactive
