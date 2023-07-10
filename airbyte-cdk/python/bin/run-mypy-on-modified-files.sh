525e8abf76ebe616591799d6ea4478d34d4033ff
Alexandre Girard <alexandre@airbyte.io>
2023-07-10 15:54:54 -0700

    ls the branches

--- airbyte-cdk/python/bin/run-mypy-on-modified-files.sh
+++ airbyte-cdk/python/bin/run-mypy-on-modified-files.sh
@@ -1,3 +1,5 @@
 set -e
+git ls-remote --heads origin master
 git fetch origin master
+git branch -a
 git diff --name-only --relative --diff-filter=d master -- . | grep -E '\.py$' | xargs mypy --config-file mypy.ini --install-types --non-interactive
