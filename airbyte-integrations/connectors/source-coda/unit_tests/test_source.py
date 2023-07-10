79beae8a07cb1d5816dc7d37f56789bfe4db4798
Himanshu <himichhabra14@gmail.com>
2023-07-11 03:02:35 +0530

    ✨ Source Coda: Add rows to codaio source (#27797)
    * feat: Add rows to codaio source
    
    * fix formatting, iterate version, update docs
    
    ---------
    
    Co-authored-by: Mal Hancock <mallory@archangelic.space>

--- airbyte-integrations/connectors/source-coda/unit_tests/test_source.py
+++ airbyte-integrations/connectors/source-coda/unit_tests/test_source.py
@@ -48,5 +48,5 @@ def test_streams(mocker):
     source = SourceCoda()
     config_mock = MagicMock()
     streams = source.streams(config_mock)
-    expected_streams_number = 7
+    expected_streams_number = 8
     assert len(streams) == expected_streams_number
