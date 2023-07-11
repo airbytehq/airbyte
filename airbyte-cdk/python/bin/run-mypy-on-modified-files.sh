3f1dfe64eb8ef3ca1ec84a5f0586ba1462fd106a
Alexandre Girard <alexandre@airbyte.io>
2023-07-10 21:21:45 -0700

    do not use the github action

--- airbyte-cdk/python/bin/run-mypy-on-modified-files.sh
+++ airbyte-cdk/python/bin/run-mypy-on-modified-files.sh
@@ -1,10 +1,3 @@
 set -e
-
-if [ $# -gt 0 ]; then
-  filesToDiff=("$@")
-else
-  filesToDiff=$(git diff --name-only --relative --diff-filter=d remotes/origin/master -- . | grep -E '\.py$')
-fi
-
-# .venv/bin is automatically created by gradle build
-.venv/bin/python -m mypy --config-file mypy.ini --install-types --non-interactive $filesToDiff
+git fetch origin master
+git diff --name-only --relative --diff-filter=d remotes/origin/master -- . | grep -E '\.py$' | xargs mypy --config-file mypy.ini --install-types --non-interactive
