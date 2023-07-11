8c3ff8e2caa39d2fbdefab6238d01a9c7ec92f77
Daryna Ishchenko <darina.ishchenko17@gmail.com>
2023-07-11 11:27:08 +0300

    fixed tests

--- airbyte-integrations/connectors/source-zendesk-support/unit_tests/unit_test.py
+++ airbyte-integrations/connectors/source-zendesk-support/unit_tests/unit_test.py
@@ -144,11 +144,11 @@ def test_check(response, start_date, check_passed):
 @pytest.mark.parametrize(
     "ticket_forms_response, status_code, expected_n_streams, expected_warnings",
     [
-        ({"ticket_forms": [{"id": 1, "updated_at": "2021-07-08T00:05:45Z"}]}, 200, 27, []),
+        ({"ticket_forms": [{"id": 1, "updated_at": "2021-07-08T00:05:45Z"}]}, 200, 28, []),
         (
                 {"error": "Not sufficient permissions"},
                 403,
-                24,
+                25,
                 ["Skipping stream ticket_forms: Check permissions, error message: Not sufficient permissions."],
         ),
     ],
