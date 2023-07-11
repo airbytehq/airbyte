3701afcffed54fbeb1e0ed899d37bfca0fc291ad
Alexandre Girard <alexandre@airbyte.io>
2023-07-10 21:39:19 -0700

    remove extra fetch

--- airbyte-cdk/python/bin/run-mypy-on-modified-files.sh
+++ airbyte-cdk/python/bin/run-mypy-on-modified-files.sh
@@ -1,3 +1,2 @@
 set -e
-git fetch origin master
 git diff --name-only --relative --diff-filter=d remotes/origin/master -- . | grep -E '\.py$' | xargs mypy --config-file mypy.ini --install-types --non-interactive
