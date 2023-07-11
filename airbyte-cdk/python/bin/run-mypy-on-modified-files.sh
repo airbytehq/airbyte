2fc65ee70ea75ee9d6e517f037c3f10080e3eb56
Alexandre Girard <alexandre@airbyte.io>
2023-07-10 20:59:38 -0700

    Revert "try resetting"
    This reverts commit 3a54c424de5b441c21d845aa75241a2cc9630333.

--- airbyte-cdk/python/bin/run-mypy-on-modified-files.sh
+++ airbyte-cdk/python/bin/run-mypy-on-modified-files.sh
@@ -1,4 +1,10 @@
 set -e
-git fetch origin master
+
+if [ $# -gt 0 ]; then
+  filesToDiff=("$@")
+else
+  filesToDiff=$(git diff --name-only --relative --diff-filter=d remotes/origin/master -- . | grep -E '\.py$')
+fi
+
 # .venv/bin is automatically created by gradle build
-git diff --name-only --relative --diff-filter=d remotes/origin/master -- . | grep -E '\.py$' | xargs .venv/bin/python -m mypy --config-file mypy.ini --install-types --non-interactive
+.venv/bin/python -m mypy --config-file mypy.ini --install-types --non-interactive $filesToDiff
