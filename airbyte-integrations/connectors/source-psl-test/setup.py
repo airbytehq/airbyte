a5ebe59eded908248aa731f08386cfdbcc46fc43
Pedro S. Lopez <pedroslopez@me.com>
2023-07-14 13:56:25 -0400

    add new connector

---
+++ airbyte-integrations/connectors/source-psl-test/setup.py
@@ -0,0 +1,23 @@
+#
+# Copyright (c) 2023 Airbyte, Inc., all rights reserved.
+#
+
+
+from setuptools import find_packages, setup
+
+MAIN_REQUIREMENTS = ["airbyte-cdk~=0.1"]
+
+TEST_REQUIREMENTS = ["pytest~=6.1", "connector-acceptance-test"]
+
+setup(
+    name="source_psl_test",
+    description="Source implementation for psl test.",
+    author="Airbyte",
+    author_email="contact@airbyte.io",
+    packages=find_packages(),
+    install_requires=MAIN_REQUIREMENTS,
+    package_data={"": ["*.json", "schemas/*.json", "schemas/shared/*.json"]},
+    extras_require={
+        "tests": TEST_REQUIREMENTS,
+    },
+)
---
+++ airbyte-integrations/connectors/source-psl-test/source_pokeapi/__init__.py
@@ -0,0 +1,7 @@
+#
+# Copyright (c) 2021 Airbyte, Inc., all rights reserved.
+#
+
+from .source import SourcePokeapi
+
+__all__ = ["SourcePokeapi"]
