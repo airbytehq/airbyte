6827421647ec3f340449c43f13fa46674f273bcb
Alexandre Girard <alexandre@airbyte.io>
2023-07-16 12:02:00 -0700

    fix typo

--- airbyte-cdk/python/airbyte_cdk/sources/file_based/config/jsonl_format.py
+++ airbyte-cdk/python/airbyte_cdk/sources/file_based/config/jsonl_format.py
@@ -7,5 +7,4 @@ from typing_extensions import Literal
 
 
 class JsonlFormat(BaseModel):
-    class ParquetFormat(BaseModel):
-        filetype: Literal["jsonl"] = "jsonl"
+    filetype: Literal["jsonl"] = "jsonl"
