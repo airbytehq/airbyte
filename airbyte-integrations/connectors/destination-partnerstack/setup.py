f68bf535fe6dfcf85e376c00c213a6171a4e1ebd
KimPlv <kim.plavonil@sendinblue.com>
2023-07-18 18:03:18 +0200

    add destination

---
+++ airbyte-integrations/connectors/destination-partnerstack/setup.py
@@ -0,0 +1,23 @@
+#
+# Copyright (c) 2023 Airbyte, Inc., all rights reserved.
+#
+
+
+from setuptools import find_packages, setup
+
+MAIN_REQUIREMENTS = ["airbyte-cdk", "requests"]
+
+TEST_REQUIREMENTS = ["pytest~=6.2"]
+
+setup(
+    name="destination_partnerstack",
+    description="Destination implementation for Partnerstack.",
+    author="Airbyte",
+    author_email="contact@airbyte.io",
+    packages=find_packages(),
+    install_requires=MAIN_REQUIREMENTS,
+    package_data={"": ["*.json"]},
+    extras_require={
+        "tests": TEST_REQUIREMENTS,
+    },
+)
