ed34f9d95d0f10a5ec6124a7d2cb8405cfa921df
Alexandre Girard <alexandre@airbyte.io>
2023-07-18 18:18:53 -0700

    fix test_create_custom_components

--- airbyte-cdk/python/unit_tests/sources/declarative/parsers/test_model_to_component_factory.py
+++ airbyte-cdk/python/unit_tests/sources/declarative/parsers/test_model_to_component_factory.py
@@ -1069,14 +1069,14 @@ def test_create_default_paginator():
                 "class_name": "unit_tests.sources.declarative.parsers.testing_components.TestingSomeComponent",
                 "paginator": {
                     "type": "DefaultPaginator",
-                    "pagination_strategy": {"type": "OffsetIncrement", "page_size": 10},
+                    "pagination_strategy": {"type": "OffsetIncrement", "page_size": "10"},
                     "$parameters": {"url_base": "https://physical_100.com"},
                 },
             },
             "paginator",
             DefaultPaginator(
                 pagination_strategy=OffsetIncrement(
-                    page_size=10, config={"apikey": "verysecrettoken", "repos": ["airbyte", "airbyte-cloud"]}, parameters={}
+                    page_size="10", config={"apikey": "verysecrettoken", "repos": ["airbyte", "airbyte-cloud"]}, parameters={}
                 ),
                 url_base="https://physical_100.com",
                 config={"apikey": "verysecrettoken", "repos": ["airbyte", "airbyte-cloud"]},
