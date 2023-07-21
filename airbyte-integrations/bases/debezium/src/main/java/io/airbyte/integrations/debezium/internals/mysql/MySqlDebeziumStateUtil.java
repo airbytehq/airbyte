8e2f02144a00b17ee492503be85d631f0c662dd1
subodh <subodh1810@gmail.com>
2023-07-21 19:14:28 +0530

    wip 2

--- airbyte-integrations/bases/debezium/src/main/java/io/airbyte/integrations/debezium/internals/mysql/MySqlDebeziumStateUtil.java
+++ airbyte-integrations/bases/debezium/src/main/java/io/airbyte/integrations/debezium/internals/mysql/MySqlDebeziumStateUtil.java
@@ -4,25 +4,77 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.google.common.annotations.VisibleForTesting;
 import io.airbyte.commons.json.Jsons;
 import io.airbyte.db.jdbc.JdbcDatabase;
-import io.debezium.time.Conversions;
-import java.sql.SQLException;
+import io.airbyte.db.jdbc.JdbcUtils;
+import io.airbyte.integrations.debezium.internals.AirbyteFileOffsetBackingStore;
+import io.airbyte.integrations.debezium.internals.AirbyteSchemaHistoryStorage;
+import io.airbyte.integrations.debezium.internals.DebeziumRecordPublisher;
+import io.airbyte.protocol.models.v0.ConfiguredAirbyteCatalog;
+import io.debezium.engine.ChangeEvent;
 import java.time.Instant;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
-import java.util.stream.Stream;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.Properties;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.TimeUnit;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class MySqlDebeziumStateUtil {
 
   private static final Logger LOGGER = LoggerFactory.getLogger(MySqlDebeziumStateUtil.class);
+  public static final String MYSQL_CDC_OFFSET = "mysql_cdc_offset";
+  public static final String MYSQL_DB_HISTORY = "mysql_db_history";
+
+  public JsonNode constructInitialDebeziumState(final Properties properties,
+      final ConfiguredAirbyteCatalog catalog,
+      final JdbcDatabase database) {
+    // https://debezium.io/documentation/reference/2.2/connectors/mysql.html#mysql-property-snapshot-mode
+    properties.setProperty("snapshot.mode", "schema_only_recovery");
+    final AirbyteFileOffsetBackingStore offsetManager = AirbyteFileOffsetBackingStore.initializeState(
+        constructBinlogOffset(database, database.getSourceConfig().get(JdbcUtils.DATABASE_KEY).asText()),
+        Optional.empty());
+    final AirbyteSchemaHistoryStorage schemaHistoryStorage = AirbyteSchemaHistoryStorage.initializeDBHistory(Optional.empty());
+    final LinkedBlockingQueue<ChangeEvent<String, String>> queue = new LinkedBlockingQueue<>();
+    try (final DebeziumRecordPublisher publisher = new DebeziumRecordPublisher(properties, database.getSourceConfig(), catalog, offsetManager,
+        Optional.of(
+            schemaHistoryStorage))) {
+      publisher.start(queue);
+      while (!publisher.hasClosed()) {
+        final ChangeEvent<String, String> event = queue.poll(10, TimeUnit.SECONDS);
+        if (event == null) {
+          continue;
+        }
+        LOGGER.info("Have seen a record, closing the debezium engine since this means initial state is constructed");
+        publisher.close();
+        break;
+      }
+    } catch (Exception e) {
+      throw new RuntimeException(e);
+    }
+
+    final Map<String, String> offset = offsetManager.read();
+    final String dbHistory = schemaHistoryStorage.read();
+
+    assert !offset.isEmpty();
+    assert Objects.nonNull(dbHistory);
+
+    final Map<String, Object> state = new HashMap<>();
+    state.put(MYSQL_CDC_OFFSET, offset);
+    state.put(MYSQL_DB_HISTORY, dbHistory);
+
+    final JsonNode asJson = Jsons.jsonNode(state);
+    LOGGER.info("Initial Debezium state constructed: {}", asJson);
+
+    return asJson;
+  }
 
   /**
    * Method to construct initial Debezium state which can be passed onto Debezium engine to make it process binlogs from a specific file and position
    * and skip snapshot phase
    */
-  public JsonNode constructInitialDebeziumState(final JdbcDatabase database, final String dbName) {
+  private JsonNode constructBinlogOffset(final JdbcDatabase database, final String dbName) {
 
 //    final String showMasterStmt = "SHOW MASTER STATUS";
 //    connection.query(showMasterStmt, rs -> {
@@ -37,35 +89,15 @@ public class MySqlDebeziumStateUtil {
 //          LOGGER.info("\t using binlog '{}' at position '{}' and gtid '{}'", binlogFilename, binlogPosition,
 //              gtidSet);
 //        }
+    //TODO : Add gtid info as well
     return format(MySqlCdcTargetPosition.targetPosition(database).getTargetPosition(), dbName, Instant.now());
-  }
-
-  public static MySqlCdcTargetPosition targetPosition(final JdbcDatabase database) {
-    try (final Stream<MySqlCdcTargetPosition> stream = database.unsafeResultSetQuery(
-        connection -> connection.createStatement().executeQuery("SHOW MASTER STATUS"),
-        resultSet -> {
-          final String file = resultSet.getString("File");
-          final long position = resultSet.getLong("Position");
-          if (file == null || position == 0) {
-            return new MySqlCdcTargetPosition(null, null);
-          }
-          return new MySqlCdcTargetPosition(file, position);
-        })) {
-      final List<MySqlCdcTargetPosition> masterStatus = stream.toList();
-      final MySqlCdcTargetPosition targetPosition = masterStatus.get(0);
-      LOGGER.info("Target File position : " + targetPosition);
-      return targetPosition;
-    } catch (final SQLException e) {
-      throw new RuntimeException(e);
-    }
-
   }
 
   @VisibleForTesting
   public JsonNode format(final MySqlCdcPosition position, final String dbName, final Instant time) {
     final String key = "[\"" + dbName + "\",{\"server\":\"" + dbName + "\"}]";
     final String value =
-        "{\"transaction_id\":null,\"ts_sec\":" + time.getEpochSecond() + ",\"file\":" + position.fileName + ",\"pos\":" + position.position
+        "{\"transaction_id\":null,\"ts_sec\":" + time.getEpochSecond() + ",\"file\":\"" + position.fileName + "\",\"pos\":" + position.position
             + "}";
 
     final Map<String, String> result = new HashMap<>();
