06c764c799a0b1adadae174a4ee262cfae2dc6bf
Edward Gao <edward.gao@airbyte.io>
2023-07-21 17:00:42 -0700

    add skeletons

---
+++ airbyte-integrations/connectors/destination-snowflake/src/main/java/io/airbyte/integrations/destination/snowflake/typing_deduping/SnowflakeSqlGenerator.java
@@ -0,0 +1,45 @@
+package io.airbyte.integrations.destination.snowflake.typing_deduping;
+
+import io.airbyte.integrations.base.destination.typing_deduping.ColumnId;
+import io.airbyte.integrations.base.destination.typing_deduping.SqlGenerator;
+import io.airbyte.integrations.base.destination.typing_deduping.StreamConfig;
+import io.airbyte.integrations.base.destination.typing_deduping.StreamId;
+import io.airbyte.integrations.base.destination.typing_deduping.TableNotMigratedException;
+import java.util.Optional;
+
+public class SnowflakeSqlGenerator implements SqlGenerator<SnowflakeTableDefinition> {
+  @Override
+  public StreamId buildStreamId(String namespace, String name, String rawNamespaceOverride) {
+    return null;
+  }
+
+  @Override
+  public ColumnId buildColumnId(String name) {
+    return null;
+  }
+
+  @Override
+  public String createTable(StreamConfig stream, String suffix) {
+    return null;
+  }
+
+  @Override
+  public boolean existingSchemaMatchesStreamConfig(StreamConfig stream, SnowflakeTableDefinition existingTable) throws TableNotMigratedException {
+    return false;
+  }
+
+  @Override
+  public String softReset(StreamConfig stream) {
+    return null;
+  }
+
+  @Override
+  public String updateTable(StreamConfig stream, String finalSuffix) {
+    return null;
+  }
+
+  @Override
+  public String overwriteFinalTable(StreamId stream, String finalSuffix) {
+    return null;
+  }
+}
