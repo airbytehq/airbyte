47afc4f52df2037d6b36f7d8df16170bc48263ab
Edward Gao <edward.gao@airbyte.io>
2023-07-24 15:15:02 -0700

    fix unit tests

--- airbyte-integrations/connectors/destination-snowflake/src/test/java/io/airbyte/integrations/destination/snowflake/SnowflakeS3StagingSqlOperationsTest.java
+++ airbyte-integrations/connectors/destination-snowflake/src/test/java/io/airbyte/integrations/destination/snowflake/SnowflakeS3StagingSqlOperationsTest.java
@@ -9,10 +9,13 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 import com.amazonaws.services.s3.AmazonS3;
+import io.airbyte.commons.json.Jsons;
+import io.airbyte.integrations.base.DestinationConfig;
 import io.airbyte.integrations.destination.s3.NoEncryption;
 import io.airbyte.integrations.destination.s3.S3DestinationConfig;
 import io.airbyte.integrations.destination.s3.credential.S3AccessKeyCredentialConfig;
 import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class SnowflakeS3StagingSqlOperationsTest {
@@ -26,14 +29,22 @@ class SnowflakeS3StagingSqlOperationsTest {
   private final S3DestinationConfig s3Config = mock(S3DestinationConfig.class);
   private final S3AccessKeyCredentialConfig credentialConfig = mock(S3AccessKeyCredentialConfig.class);
 
-  private final SnowflakeS3StagingSqlOperations snowflakeStagingSqlOperations =
-      new SnowflakeS3StagingSqlOperations(new SnowflakeSQLNameTransformer(), s3Client, s3Config, new NoEncryption());
+  private SnowflakeS3StagingSqlOperations snowflakeStagingSqlOperations;
+
+  @BeforeEach
+  public void setup() {
+    DestinationConfig.initialize(Jsons.emptyObject());
+    snowflakeStagingSqlOperations =
+        new SnowflakeS3StagingSqlOperations(new SnowflakeSQLNameTransformer(), s3Client, s3Config, new NoEncryption());
+  }
 
   @Test
   void copyIntoTmpTableFromStage() {
-    final String expectedQuery = "COPY INTO " + SCHEMA_NAME + "." + TABLE_NAME + " FROM 's3://" + BUCKET_NAME + "/" + STAGE_PATH + "' " +
-        "CREDENTIALS=(aws_key_id='aws_access_key_id' aws_secret_key='aws_secret_access_key') file_format = (type = csv compression = auto " +
-        "field_delimiter = ',' skip_header = 0 FIELD_OPTIONALLY_ENCLOSED_BY = '\"') files = ('filename1','filename2');";
+    final String expectedQuery = """
+        COPY INTO schemaName.tableName FROM 's3://bucket_name/stagePath/2022/'
+        CREDENTIALS=(aws_key_id='aws_access_key_id' aws_secret_key='aws_secret_access_key')
+        file_format = (type = csv compression = auto field_delimiter = ',' skip_header = 0 FIELD_OPTIONALLY_ENCLOSED_BY = '"' NULL_IF=('') )
+         files = ('filename1','filename2');""";
     when(s3Config.getBucketName()).thenReturn(BUCKET_NAME);
     when(s3Config.getS3CredentialConfig()).thenReturn(credentialConfig);
     when(credentialConfig.getAccessKeyId()).thenReturn("aws_access_key_id");
