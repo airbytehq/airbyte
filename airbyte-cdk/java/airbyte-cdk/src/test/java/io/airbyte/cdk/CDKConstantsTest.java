17455a72afb806911df284ebc24e9531a73fa833
aaronsteers <aaronsteers@users.noreply.github.com>
2023-07-25 19:09:03 +0000

    Automated Commit - Formatting Changes

--- airbyte-cdk/java/airbyte-cdk/src/test/java/io/airbyte/cdk/CDKConstantsTest.java
+++ airbyte-cdk/java/airbyte-cdk/src/test/java/io/airbyte/cdk/CDKConstantsTest.java
@@ -1,29 +1,33 @@
+/*
+ * Copyright (c) 2023 Airbyte, Inc., all rights reserved.
+ */
+
 package io.airbyte.cdk;
 
-import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Disabled;
-
+import org.junit.jupiter.api.Test;
 
 class CDKConstantsTest {
 
-    /* TODO: Remove these three canary tests once real tests are in place. */
-    @Test
-    void getVersion() {
-        System.out.println("Running getVersion test");
-        assertEquals("0.0.1-SNAPSHOT", CDKConstants.VERSION);
-    }
+  /* TODO: Remove these three canary tests once real tests are in place. */
+  @Test
+  void getVersion() {
+    System.out.println("Running getVersion test");
+    assertEquals("0.0.1-SNAPSHOT", CDKConstants.VERSION);
+  }
 
-    @Test
-    void mustPass() {
-        assertTrue(true, "This should always pass.");
-    }
+  @Test
+  void mustPass() {
+    assertTrue(true, "This should always pass.");
+  }
 
-    @Test
-    // Comment out this line to force failure:
-    @Disabled("This is an intentionally failing test (skipped).")
-    void mustFail() {
-        assertTrue(false, "This is an intentionally failing test.");
-    }
+  @Test
+  // Comment out this line to force failure:
+  @Disabled("This is an intentionally failing test (skipped).")
+  void mustFail() {
+    assertTrue(false, "This is an intentionally failing test.");
+  }
+
 }
