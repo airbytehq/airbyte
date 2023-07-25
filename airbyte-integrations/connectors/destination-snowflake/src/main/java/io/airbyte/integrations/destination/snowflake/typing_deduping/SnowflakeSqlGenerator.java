bce00b9419e70415898b7610a8bb004a6c9159e3
Edward Gao <edward.gao@airbyte.io>
2023-07-25 09:38:40 -0700

    add toDialectType

--- airbyte-integrations/connectors/destination-snowflake/src/main/java/io/airbyte/integrations/destination/snowflake/typing_deduping/SnowflakeSqlGenerator.java
+++ airbyte-integrations/connectors/destination-snowflake/src/main/java/io/airbyte/integrations/destination/snowflake/typing_deduping/SnowflakeSqlGenerator.java
@@ -1,10 +1,16 @@
 package io.airbyte.integrations.destination.snowflake.typing_deduping;
 
+import io.airbyte.integrations.base.destination.typing_deduping.AirbyteProtocolType;
+import io.airbyte.integrations.base.destination.typing_deduping.AirbyteType;
+import io.airbyte.integrations.base.destination.typing_deduping.Array;
 import io.airbyte.integrations.base.destination.typing_deduping.ColumnId;
 import io.airbyte.integrations.base.destination.typing_deduping.SqlGenerator;
 import io.airbyte.integrations.base.destination.typing_deduping.StreamConfig;
 import io.airbyte.integrations.base.destination.typing_deduping.StreamId;
+import io.airbyte.integrations.base.destination.typing_deduping.Struct;
 import io.airbyte.integrations.base.destination.typing_deduping.TableNotMigratedException;
+import io.airbyte.integrations.base.destination.typing_deduping.Union;
+import io.airbyte.integrations.base.destination.typing_deduping.UnsupportedOneOf;
 
 public class SnowflakeSqlGenerator implements SqlGenerator<SnowflakeTableDefinition> {
   @Override
@@ -17,6 +23,42 @@ public class SnowflakeSqlGenerator implements SqlGenerator<SnowflakeTableDefinit
   public ColumnId buildColumnId(String name) {
     // TODO
     return new ColumnId(name, name, name);
+  }
+
+  public String toDialectType(final AirbyteType type) {
+    if (type instanceof final AirbyteProtocolType p) {
+      return toDialectType(p);
+    } else if (type instanceof Struct) {
+      // TODO should this+array just be VARIANT?
+      return "OBJECT";
+    } else if (type instanceof Array) {
+      return "ARRAY";
+    } else if (type instanceof UnsupportedOneOf) {
+      return "VARIANT";
+    } else if (type instanceof final Union u) {
+      final AirbyteType typeWithPrecedence = u.chooseType();
+      // typeWithPrecedence is never a Union, so this recursion is safe.
+      return toDialectType(typeWithPrecedence);
+    }
+
+    // Literally impossible; AirbyteType is a sealed interface.
+    throw new IllegalArgumentException("Unsupported AirbyteType: " + type);
+  }
+
+  public String toDialectType(final AirbyteProtocolType airbyteProtocolType) {
+    // TODO verify these types against normalization
+    return switch (airbyteProtocolType) {
+      case STRING -> "TEXT";
+      case NUMBER -> "NUMBER";
+      case INTEGER -> "INTEGER";
+      case BOOLEAN -> "BOOLEAN";
+      case TIMESTAMP_WITH_TIMEZONE -> "TIMESTAMP_TZ";
+      case TIMESTAMP_WITHOUT_TIMEZONE -> "TIMESTAMP_NTZ";
+      case TIME_WITH_TIMEZONE -> "STRING";
+      case TIME_WITHOUT_TIMEZONE -> "TIME";
+      case DATE -> "DATE";
+      case UNKNOWN -> "VARIANT";
+    };
   }
 
   @Override
