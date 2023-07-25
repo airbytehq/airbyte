ad30bae0be53731d01ec8541800b4c9bc9cfa43e
Edward Gao <edward.gao@airbyte.io>
2023-07-25 09:57:27 -0700

    basic createTable impl

--- airbyte-integrations/connectors/destination-snowflake/src/main/java/io/airbyte/integrations/destination/snowflake/typing_deduping/SnowflakeSqlGenerator.java
+++ airbyte-integrations/connectors/destination-snowflake/src/main/java/io/airbyte/integrations/destination/snowflake/typing_deduping/SnowflakeSqlGenerator.java
@@ -1,5 +1,7 @@
 package io.airbyte.integrations.destination.snowflake.typing_deduping;
 
+import static java.util.stream.Collectors.joining;
+
 import io.airbyte.integrations.base.destination.typing_deduping.AirbyteProtocolType;
 import io.airbyte.integrations.base.destination.typing_deduping.AirbyteType;
 import io.airbyte.integrations.base.destination.typing_deduping.Array;
@@ -11,8 +13,13 @@ import io.airbyte.integrations.base.destination.typing_deduping.Struct;
 import io.airbyte.integrations.base.destination.typing_deduping.TableNotMigratedException;
 import io.airbyte.integrations.base.destination.typing_deduping.Union;
 import io.airbyte.integrations.base.destination.typing_deduping.UnsupportedOneOf;
+import java.util.Map;
+import org.apache.commons.text.StringSubstitutor;
 
 public class SnowflakeSqlGenerator implements SqlGenerator<SnowflakeTableDefinition> {
+
+  public static final String QUOTE = "\"";
+
   @Override
   public StreamId buildStreamId(String namespace, String name, String rawNamespaceOverride) {
     // TODO
@@ -63,7 +70,24 @@ public class SnowflakeSqlGenerator implements SqlGenerator<SnowflakeTableDefinit
 
   @Override
   public String createTable(StreamConfig stream, String suffix) {
-    return null;
+    final String columnDeclarations = stream.columns().entrySet().stream()
+        .map(column -> column.getKey().name(QUOTE) + " " + toDialectType(column.getValue()))
+        .collect(joining(",\n"));
+    // TODO indexes and stuff
+    return new StringSubstitutor(Map.of(
+        "final_namespace", stream.id().finalNamespace(QUOTE),
+        "final_table_id", stream.id().finalTableId(suffix, QUOTE),
+        "column_declarations", columnDeclarations)).replace(
+        """
+        CREATE SCHEMA IF NOT EXISTS ${final_namespace};
+
+        CREATE TABLE ${final_table_id} (
+          _airbyte_raw_id STRING NOT NULL,
+          _airbyte_extracted_at TIMESTAMP NOT NULL,
+          _airbyte_meta JSON NOT NULL,
+          ${column_declarations}
+        );
+        """);
   }
 
   @Override
