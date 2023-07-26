d26498d414b896e08d905b0ea33d11b2ec029de8
Ben Church <ben@airbyte.io>
2023-07-26 09:49:59 -0600

    Move tools/ci_* projects to airbyte-ci, update to use Poetry, bump to python 3.10 (#27957)
    * Move ci_connector_ops
    
    * Move ci_credentials
    
    * Move tools/ci_common_utils
    
    * Rename tools to airbyte-ci
    
    * Move to ci
    
    * Convert ci_credentials
    
    * Convert ci_common_utls
    
    * Convert ci_connector_ops
    
    * Get pipelines running
    
    * Move pipelines to own poetry project
    
    * Update readme
    
    * Delete
    
    * Add ci_code_validator
    
    * Use pipx to install gha deps
    
    * Fix'
    
    * Ensure every thing is running
    
    * Automated Commit - Formatting Changes
    
    * Gitignore miss
    
    * Add pipx installer
    
    * Get local pipx dependencies
    
    * Fix paths
    
    * Install pipx
    
    * ceremonial source-faker change
    
    * Add installation step for ci_code_validator
    
    * Add comment
    
    * remove ci_code_validator
    
    * Address code review comments
    
    * add pipx install to acceptance-test-docker.sh
    
    * Run formater
    
    * Revert "ceremonial source-faker change"
    
    This reverts commit 26884cd0dbe194727b5982e7b67c96e7b60573a6.
    
    * gitignore lecacy pipeline report path
    
    * update poetry.lock
    
    * skip upload if logs do not exist
    
    ---------
    
    Co-authored-by: bnchrch <bnchrch@users.noreply.github.com>
    Co-authored-by: alafanechere <augustin.lafanechere@gmail.com>

---
+++ airbyte-ci/connectors/pipelines/pipelines/consts.py
@@ -0,0 +1,36 @@
+#
+# Copyright (c) 2023 Airbyte, Inc., all rights reserved.
+#
+
+import platform
+from pathlib import Path
+
+from dagger import Platform
+
+PYPROJECT_TOML_FILE_PATH = "pyproject.toml"
+LICENSE_SHORT_FILE_PATH = "LICENSE_SHORT"
+CONNECTOR_TESTING_REQUIREMENTS = [
+    "pip==21.3.1",
+    "mccabe==0.6.1",
+    "flake8==4.0.1",
+    "pyproject-flake8==0.0.1a2",
+    "black==22.3.0",
+    "isort==5.6.4",
+    "pytest==6.2.5",
+    "coverage[toml]==6.3.1",
+    "pytest-custom_exit_code",
+    "licenseheaders==0.8.8",
+]
+
+CI_CREDENTIALS_SOURCE_PATH = "airbyte-ci/connectors/ci_credentials"
+CONNECTOR_OPS_SOURCE_PATHSOURCE_PATH = "airbyte-ci/connectors/connector_ops"
+BUILD_PLATFORMS = [Platform("linux/amd64"), Platform("linux/arm64")]
+LOCAL_BUILD_PLATFORM = Platform(f"linux/{platform.machine()}")
+DOCKER_VERSION = "24.0.2"
+DOCKER_DIND_IMAGE = "docker:24-dind"
+DOCKER_CLI_IMAGE = "docker:24-cli"
+GRADLE_CACHE_PATH = "/root/.gradle/caches"
+GRADLE_BUILD_CACHE_PATH = f"{GRADLE_CACHE_PATH}/build-cache-1"
+GRADLE_READ_ONLY_DEPENDENCY_CACHE_PATH = "/root/gradle_dependency_cache"
+LOCAL_REPORTS_PATH_ROOT = "airbyte-ci/connectors/pipelines/pipeline_reports/"
+GCS_PUBLIC_DOMAIN = "https://storage.cloud.google.com"
