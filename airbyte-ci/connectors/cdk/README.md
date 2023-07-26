554ca1f8086d5fd874d7cddf11b7026022d308e3
cpdeethree <conor@airbyte.io>
2023-07-25 20:29:21 -0500

    build airbyte java cdk with dagger

---
+++ airbyte-ci/connectors/cdk/README.md
@@ -0,0 +1,13 @@
+# CDK CI
+
+## Installation
+
+```bash
+aircmd plugin install cdk --local .
+```
+
+## Running
+
+```bash
+aircmd cdk java ci
+```
---
+++ airbyte-ci/connectors/cdk/cdk/java_cdk/settings.py
@@ -0,0 +1,11 @@
+
+
+from aircmd.models.base import GlobalSettings
+from pydantic import Field
+
+
+class JavaCDKSettings(GlobalSettings):   
+     version: str = Field("dev", env="VERSION")     
+     dagger: bool = Field(True, env="DAGGER")  
+     sub_build: str = Field("CONNECTORS_BASE", env="SUB_BUILD")
+     
