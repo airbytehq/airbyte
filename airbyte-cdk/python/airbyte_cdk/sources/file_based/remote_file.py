370eb54160e94851f9bcc1dcbba9de09dee4314d
Catherine Noll <noll.catherine@gmail.com>
2023-07-27 11:37:09 -0400

    Source S3: basic structure using file-based CDK

--- airbyte-cdk/python/airbyte_cdk/sources/file_based/remote_file.py
+++ airbyte-cdk/python/airbyte_cdk/sources/file_based/remote_file.py
@@ -3,9 +3,15 @@
 #
 
 from datetime import datetime
+from enum import Enum
 from typing import Optional
 
 from pydantic import BaseModel
+
+
+class FileReadMode(Enum):
+    READ = "r"
+    READ_BINARY = "rb"
 
 
 class RemoteFile(BaseModel):
