d1fa79aeb4b712470a0267c1532a52c2f3119965
maxi297 <maxime@airbyte.io>
2023-07-27 09:50:07 -0400

    Catch files that are not yet commited as well

--- airbyte-cdk/python/bin/run-mypy-on-modified-files.sh
+++ airbyte-cdk/python/bin/run-mypy-on-modified-files.sh
@@ -1,3 +1,3 @@
 set -e
 # TODO change this to include unit_tests as well once it's in a good state
-git diff --name-only master... ':(exclude)unit_tests' | xargs -I '{}' realpath --relative-to=. $(git rev-parse --show-toplevel)/'{}' | grep -E '\.py$' | xargs .venv/bin/python -m mypy --config-file mypy.ini --install-types --non-interactive
+{ git diff --name-only --relative ':(exclude)unit_tests'; git diff --name-only master... --relative ':(exclude)unit_tests'; } | grep -E '\.py$' | xargs .venv/bin/python -m mypy --config-file mypy.ini --install-types --non-interactive
