f2d963862b6b5322e3f9c2c8c9ffcfa27bce6956
Pedro S. Lopez <pedroslopez@me.com>
2023-07-27 10:28:12 -0400

    format

--- airbyte-ci/connectors/pipelines/pipelines/__init__.py
+++ airbyte-ci/connectors/pipelines/pipelines/__init__.py
@@ -9,6 +9,7 @@ import os
 from rich.logging import RichHandler
 
 from . import sentry_utils
+
 sentry_utils.initialize()
 
 logging.getLogger("requests").setLevel(logging.WARNING)
@@ -19,6 +20,11 @@ if "CI" in os.environ:
     # RichHandler does not work great in the CI
     logging_handlers = [logging.StreamHandler()]
 
-logging.basicConfig(level=logging.INFO, format="%(name)s: %(message)s", datefmt="[%X]", handlers=logging_handlers)
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(name)s: %(message)s",
+    datefmt="[%X]",
+    handlers=logging_handlers,
+)
 
 main_logger = logging.getLogger(__name__)
