a5ad6bc18087f8c9056e386253f02aca4ad57275
Shashank Vishwakarma <shashank@datazip.io>
2023-07-28 21:53:41 +0530

    fix: sync records after specified start_date

--- airbyte-integrations/connectors/source-zoho-books/source_dz_zoho_books/source.py
+++ airbyte-integrations/connectors/source-zoho-books/source_dz_zoho_books/source.py
@@ -4,13 +4,15 @@
 
 
 from abc import ABC
-from datetime import datetime
+from datetime import datetime, timedelta
 from types import MappingProxyType
 from typing import Any, Iterable, List, Mapping, MutableMapping, Optional, Tuple
-
+from airbyte_cdk.models import SyncMode
+from airbyte_cdk.sources.streams import IncrementalMixin
+from airbyte_cdk.sources.streams.core import StreamData
 import requests
 from airbyte_cdk.sources import AbstractSource
-from airbyte_cdk.sources.streams import IncrementalMixin, Stream
+from airbyte_cdk.sources.streams import Stream
 from airbyte_cdk.sources.streams.http import HttpStream
 from source_dz_zoho_books.auth import ZohoBooksAuthenticator
 
@@ -19,7 +21,7 @@ from .api import ZohoBooksAPI
 
 # Basic full refresh stream
 class DzZohoBooksStream(HttpStream, ABC):
-    def __init__(self, start_date, base_url, **kwargs):
+    def __init__(self, start_date: datetime, base_url, **kwargs):
         super().__init__(**kwargs)
         self._start_date = start_date
         self.base_url = base_url
@@ -32,7 +34,7 @@ class DzZohoBooksStream(HttpStream, ABC):
         next_page = response.json().get("page_context")
         if not next_page:
             return None
-        elif not next_page["has_more_page"]:
+        elif next_page["has_more_page"] == False:
             return None
         return {"page": next_page["page"] + 1, "per_page": next_page["per_page"]}
 
@@ -42,7 +44,10 @@ class DzZohoBooksStream(HttpStream, ABC):
         if next_page_token is None:
             return {}
         else:
-            return {"per_page": next_page_token["per_page"], **(next_page_token["page"] or {})}
+            return {
+                "per_page": next_page_token["per_page"],
+                **(next_page_token["page"] or {})
+            }
 
     def parse_response(self, response: requests.Response, **kwargs) -> Iterable[Mapping]:
         """
@@ -51,9 +56,13 @@ class DzZohoBooksStream(HttpStream, ABC):
         data = response.json().get(self.name)
         if isinstance(data, list):
             for record in data:
-                yield self.transform(record=record, **kwargs)
+                date = datetime.strptime(record["created_time"], '%Y-%m-%dT%H:%M:%S%z')
+                if date >= self._start_date:
+                    yield self.transform(record=record, **kwargs)
         else:
-            yield self.transform(record=data, **kwargs)
+            date = datetime.strptime(data["created_time"], '%Y-%m-%dT%H:%M:%S%z')
+            if date >= self._start_date:
+                yield self.transform(record=data, **kwargs)
 
     def transform(self, record: MutableMapping[str, Any], stream_slice: Mapping[str, Any], **kwargs) -> MutableMapping[str, Any]:
         return record
@@ -70,60 +79,60 @@ class IncrementalDzZohoBooksStream(DzZohoBooksStream, IncrementalMixin):
     @property
     def state(self) -> Mapping[str, Any]:
         if self._cursor_value:
-            return {self.cursor_field: self._cursor_value}
-        else:
-            time = self.start_date.strftime("%Y-%m-%dT%H:%M:%S%z")
-            return {self.cursor_field: datetime.strptime(time, "%Y-%m-%dT%H:%M:%S%z")}
+            return  {self.cursor_field: self._cursor_value}
+        else :
+            self._cursor_value = datetime.strptime(self.start_date.strftime('%Y-%m-%dT%H:%M:%S%z'), '%Y-%m-%dT%H:%M:%S%z')
+            return  {self.cursor_field: self._cursor_value}
 
     @state.setter
     def state(self, value: Mapping[str, Any]):
         self._cursor_value = datetime.strptime(str(value[self.cursor_field]), "%Y-%m-%dT%H:%M:%S%z")
 
     def find_index(self, records, last_modified_time) -> int:
-        low = 0
-        high = len(records) - 1
-
+        low_index = 0
+        high_index = len(records) - 1
+        
         required_index = -1
-        while low <= high:
-            mid = (low + high) // 2
+        while low_index <= high_index:
+            mid_index = (low_index + high_index) // 2
 
-            time = records[mid][self.cursor_field]
+            time = records[mid_index][self.cursor_field]
 
             if time == last_modified_time:
-                required_index = mid + 1
+                required_index = mid_index + 1
                 break
             elif time > last_modified_time:
-                required_index = mid
-                high = mid - 1
+                required_index = mid_index
+                high_index = mid_index - 1
             else:
-                low = mid + 1
+                low_index = mid_index + 1
 
         return required_index
 
-    def read_records(self, *args, **kwargs) -> Iterable[Mapping[str, Any]]:
-        records = list(super().read_records(*args, **kwargs))
+    def read_records(self, sync_mode: SyncMode, cursor_field: List[str] = None, stream_slice: Mapping[str, Any] = None, stream_state: Mapping[str, Any] = None) -> Iterable[StreamData]:
+        records = list(super().read_records(sync_mode, cursor_field, stream_slice, stream_state))
+
+        latest_cursor_value : datetime = None
         if not self._cursor_value:
             for record in records:
-                current_cursor_value = (
-                    self._cursor_value if self._cursor_value else datetime.strptime(record[self.cursor_field], "%Y-%m-%dT%H:%M:%S%z")
-                )
-                latest_cursor_value = datetime.strptime(record[self.cursor_field], "%Y-%m-%dT%H:%M:%S%z")
-                self._cursor_value = max(current_cursor_value, latest_cursor_value)
+                current_record_cursor_value = datetime.strptime(record[self.cursor_field], '%Y-%m-%dT%H:%M:%S%z')
+                latest_cursor_value = max(current_record_cursor_value, latest_cursor_value) if latest_cursor_value else current_record_cursor_value
                 yield record
         else:
-            target_time = self._cursor_value.strftime("%Y-%m-%dT%H:%M:%S%z")
+            target_time = self._cursor_value.strftime('%Y-%m-%dT%H:%M:%S%z')
             index = self.find_index(records, target_time)
-
+            
             if index == -1:
                 return None
 
             while index < len(records):
-                current_cursor_value = self._cursor_value
-                latest_cursor_value = datetime.strptime(records[index][self.cursor_field], "%Y-%m-%dT%H:%M:%S%z")
-                self._cursor_value = max(current_cursor_value, latest_cursor_value)
+                current_record_cursor_value = datetime.strptime(records[index][self.cursor_field], '%Y-%m-%dT%H:%M:%S%z')
+                latest_cursor_value = max(current_record_cursor_value, latest_cursor_value) if latest_cursor_value else current_record_cursor_value
                 yield records[index]
                 index = index + 1
 
+        self._cursor_value = latest_cursor_value
+        yield from []
 
 class Contacts(IncrementalDzZohoBooksStream):
     primary_key = "contact_id"
@@ -258,6 +267,17 @@ class Bankaccounts(DzZohoBooksStream):
         self, stream_state: Mapping[str, Any] = None, stream_slice: Mapping[str, Any] = None, next_page_token: Mapping[str, Any] = None
     ) -> str:
         return "v3/bankaccounts"
+    
+    def parse_response(self, response: requests.Response, **kwargs) -> Iterable[Mapping]:
+        """
+        :return an iterable containing each record in the response
+        """
+        data = response.json().get(self.name)
+        if isinstance(data, list):
+            for record in data:
+                yield super().transform(record=record, **kwargs)
+        else:
+            yield super().transform(record=data, **kwargs)
 
 
 class Banktransactions(DzZohoBooksStream):
@@ -267,6 +287,22 @@ class Banktransactions(DzZohoBooksStream):
         self, stream_state: Mapping[str, Any] = None, stream_slice: Mapping[str, Any] = None, next_page_token: Mapping[str, Any] = None
     ) -> str:
         return "v3/banktransactions"
+    
+    def parse_response(self, response: requests.Response, **kwargs) -> Iterable[Mapping]:
+        """
+        :return an iterable containing each record in the response
+        """
+        data = response.json().get(self.name)
+        start_date = datetime.strptime(self._start_date.strftime('%Y-%m-%d'), '%Y-%m-%d')
+        if isinstance(data, list):
+            for record in data:
+                date = datetime.strptime(record["date"], '%Y-%m-%d')
+                if date >= start_date:
+                    yield super().transform(record=record, **kwargs)
+        else:
+            date = datetime.strptime(data["date"], '%Y-%m-%d')
+            if date >= start_date:
+                yield super().transform(record=data, **kwargs)
 
 
 class Chartofaccounts(DzZohoBooksStream):
@@ -285,6 +321,22 @@ class Journals(DzZohoBooksStream):
         self, stream_state: Mapping[str, Any] = None, stream_slice: Mapping[str, Any] = None, next_page_token: Mapping[str, Any] = None
     ) -> str:
         return "v3/journals"
+
+    def parse_response(self, response: requests.Response, **kwargs) -> Iterable[Mapping]:
+        """
+        :return an iterable containing each record in the response
+        """
+        data = response.json().get(self.name)
+        start_date = datetime.strptime(self._start_date.strftime('%Y-%m-%d'), '%Y-%m-%d')
+        if isinstance(data, list):
+            for record in data:
+                date = datetime.strptime(record["journal_date"], '%Y-%m-%d')
+                if date >= start_date:
+                    yield super().transform(record=record, **kwargs)
+        else:
+            date = datetime.strptime(data["journal_date"], '%Y-%m-%d')
+            if date >= start_date:
+                yield super().transform(record=data, **kwargs)
 
 
 class Projects(IncrementalDzZohoBooksStream):
@@ -321,6 +373,17 @@ class Users(DzZohoBooksStream):
         self, stream_state: Mapping[str, Any] = None, stream_slice: Mapping[str, Any] = None, next_page_token: Mapping[str, Any] = None
     ) -> str:
         return "v3/users"
+    
+    def parse_response(self, response: requests.Response, **kwargs) -> Iterable[Mapping]:
+        """
+        :return an iterable containing each record in the response
+        """
+        data = response.json().get(self.name)
+        if isinstance(data, list):
+            for record in data:
+                yield super().transform(record=record, **kwargs)
+        else:
+            yield super().transform(record=data, **kwargs)
 
 
 class Currencies(DzZohoBooksStream):
@@ -330,6 +393,17 @@ class Currencies(DzZohoBooksStream):
         self, stream_state: Mapping[str, Any] = None, stream_slice: Mapping[str, Any] = None, next_page_token: Mapping[str, Any] = None
     ) -> str:
         return "v3/settings/currencies"
+    
+    def parse_response(self, response: requests.Response, **kwargs) -> Iterable[Mapping]:
+        """
+        :return an iterable containing each record in the response
+        """
+        data = response.json().get(self.name)
+        if isinstance(data, list):
+            for record in data:
+                yield super().transform(record=record, **kwargs)
+        else:
+            yield super().transform(record=data, **kwargs)
 
 
 # Source
@@ -347,24 +421,45 @@ class SourceDzZohoBooks(AbstractSource):
         """
         :param config: A Mapping of the user input configuration as defined in the connector spec.
         """
+
+        def utc_to_ist(utc_time: str) -> str:
+            """
+            Converts a UTC time to IST.
+
+            Args:
+                utc_time: The UTC time in the format "YYYY-MM-DDTHH:MM:SSZ"
+            Returns:
+                str: The IST time in the format "YYYY-MM-DDTHH:MM:SS+0530".
+            """
+            ist_offset =timedelta(hours=5, minutes=30)
+            ist_time = datetime.strptime(utc_time, "%Y-%m-%dT%H:%M:%SZ") + ist_offset
+            return ist_time.strftime("%Y-%m-%dT%H:%M:%S+05:30")
+        
         auth = ZohoBooksAuthenticator(
             token_refresh_endpoint="https://accounts.zoho.in/oauth/v2/token",
             client_id=config["client_id"],
             client_secret=config["client_secret"],
-            refresh_token=config["refresh_token"],
+            refresh_token=config["refresh_token"]
         )
 
-        start_date = datetime.strptime(config["start_date"], "%Y-%m-%dT%H:%M:%S%z")
+        # Config provides start_date in UTC timezone.
+        # Convert it to IST to easily compare as API provides last_modified_time in IST format. 
+        start_date = datetime.strptime(utc_to_ist(config["start_date"]), "%Y-%m-%dT%H:%M:%S%z")
+        
         _DC_REGION_TO_API_URL = MappingProxyType(
             {
                 "US": "https://www.zohoapis.com/books/",
                 "AU": "https://www.zohoapis.com.au/books/",
                 "EU": "https://www.zohoapis.eu/books/",
                 "IN": "https://www.zohoapis.in/books/",
-                "JP": "https://www.zohoapis.jp/books/",
+                "JP": "https://www.zohoapis.jp/books/"
             }
         )
-        init_params = {"authenticator": auth, "start_date": start_date, "base_url": _DC_REGION_TO_API_URL[config["dc_region"].upper()]}
+        init_params = {
+            "authenticator": auth,
+            "start_date": start_date,
+            "base_url": _DC_REGION_TO_API_URL[config['dc_region'].upper()]
+        }
 
         return [
             Contacts(**init_params),
