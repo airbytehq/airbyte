0ae6b96a71503d9fd810f05b80b4cba577039ba6
benmoriceau <benoit@airbyte.io>
2023-07-31 14:36:43 -0700

    Use a buffer of 10 Mb

--- airbyte-integrations/connectors/destination-redshift/src/main/java/io/airbyte/integrations/destination/redshift/RedshiftStagingS3Destination.java
+++ airbyte-integrations/connectors/destination-redshift/src/main/java/io/airbyte/integrations/destination/redshift/RedshiftStagingS3Destination.java
@@ -172,7 +172,7 @@ public class RedshiftStagingS3Destination extends AbstractJdbcDestination implem
             new NoopTyperDeduper(),
             // The parsedcatalog is only used in v2 mode, so just pass null for now
             null,
-            100 * 1024 * 1024);
+            10 * 1024 * 1024);
   }
 
   /**
