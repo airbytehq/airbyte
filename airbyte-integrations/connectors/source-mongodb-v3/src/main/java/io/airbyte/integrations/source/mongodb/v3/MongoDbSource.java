03395ebd45d8704a8da478919c21890849110f63
jdpgrailsdev <jpearlin1@gmail.com>
2023-07-31 16:16:16 -0400

    Add missing main method

--- airbyte-integrations/connectors/source-mongodb-v3/src/main/java/io/airbyte/integrations/source/mongodb/v3/MongoDbSource.java
+++ airbyte-integrations/connectors/source-mongodb-v3/src/main/java/io/airbyte/integrations/source/mongodb/v3/MongoDbSource.java
@@ -6,14 +6,27 @@ package io.airbyte.integrations.source.mongodb.v3;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import io.airbyte.commons.util.AutoCloseableIterator;
+import io.airbyte.integrations.base.IntegrationRunner;
 import io.airbyte.integrations.base.Source;
 import io.airbyte.protocol.models.v0.AirbyteCatalog;
 import io.airbyte.protocol.models.v0.AirbyteConnectionStatus;
 import io.airbyte.protocol.models.v0.AirbyteMessage;
 import io.airbyte.protocol.models.v0.ConfiguredAirbyteCatalog;
 import io.airbyte.protocol.models.v0.ConnectorSpecification;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class MongoDbSource implements Source, AutoCloseable {
+
+  private static final Logger LOGGER = LoggerFactory.getLogger(MongoDbSource.class);
+
+  public static void main(final String[] args) throws Exception {
+    final Source source = new MongoDbSource();
+    LOGGER.info("starting source: {}", MongoDbSource.class);
+    new IntegrationRunner(source).run(args);
+    LOGGER.info("completed source: {}", MongoDbSource.class);
+  }
+
 
   @Override
   public AirbyteConnectionStatus check(JsonNode config) throws Exception {
