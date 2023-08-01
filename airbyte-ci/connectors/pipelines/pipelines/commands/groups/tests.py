787916c3b13a4301422b8b98fa5db8d0488c432a
Augustin <augustin@airbyte.io>
2023-08-01 13:04:49 +0200

    connectors-ci: fix airbyte-ci test run (#28907)
    * enable workflow dispatch on test workflow
    
    * install curl
    
    * exit with 1 status code if exec error
    
    * use click Abort
    
    * fix wildcard
    
    * remove superfluous fixtures
    
    * skip failing publish tests
    
    * share the docker socket with the testing container for dagger-in-dagger
    
    * bump version
    
    * bump version
    
    * set workflow concurrency

--- airbyte-ci/connectors/pipelines/pipelines/commands/groups/tests.py
+++ airbyte-ci/connectors/pipelines/pipelines/commands/groups/tests.py
@@ -7,6 +7,7 @@ Module exposing the tests command to test airbyte-ci projects.
 """
 
 import logging
+import os
 import sys
 
 import anyio
@@ -24,24 +25,29 @@ def tests(
     Args:
         airbyte_ci_package_path (str): Path to the airbyte-ci package to test, relative to airbyte-ci directory
     """
-    anyio.run(run_test, airbyte_ci_package_path)
+    success = anyio.run(run_test, airbyte_ci_package_path)
+    if not success:
+        click.Abort()
 
 
-async def run_test(airbyte_ci_package_path: str):
+async def run_test(airbyte_ci_package_path: str) -> bool:
     """Runs the tests for the given airbyte-ci package in a Dagger container.
 
     Args:
         airbyte_ci_package_path (str): Path to the airbyte-ci package to test, relative to airbyte-ci directory.
+    Returns:
+        bool: True if the tests passed, False otherwise.
     """
     logger = logging.getLogger(f"{airbyte_ci_package_path}.tests")
     logger.info(f"Running tests for {airbyte_ci_package_path}")
     async with dagger.Connection(dagger.Config(log_output=sys.stderr)) as dagger_client:
         try:
-            pytest_stdout = await (
+            docker_host_socket = dagger_client.host().unix_socket("/var/run/buildkit/buildkitd.sock")
+            pytest_container = await (
                 dagger_client.container()
-                .from_("python:3.10-slim")
+                .from_("python:3.10.12")
                 .with_exec(["apt-get", "update"])
-                .with_exec(["apt-get", "install", "-y", "bash", "git"])
+                .with_exec(["apt-get", "install", "-y", "bash", "git", "curl"])
                 .with_env_variable("VERSION", "24.0.2")
                 .with_exec(["sh", "-c", "curl -fsSL https://get.docker.com | sh"])
                 .with_exec(["pip", "install", "pipx"])
@@ -53,11 +59,18 @@ async def run_test(airbyte_ci_package_path: str):
                 )
                 .with_workdir(f"/airbyte-ci/{airbyte_ci_package_path}")
                 .with_exec(["poetry", "install"])
+                .with_unix_socket("/var/run/docker.sock", dagger_client.host().unix_socket("/var/run/docker.sock"))
                 .with_exec(["poetry", "run", "pytest", "tests"])
-            ).stdout()
-            logger.info("Successfully ran tests")
-            logger.info(pytest_stdout)
+            )
+            if "_EXPERIMENTAL_DAGGER_RUNNER_HOST" in os.environ:
+                logger.info("Using experimental dagger runner host to run CAT with dagger-in-dagger")
+                pytest_container = pytest_container.with_env_variable(
+                    "_EXPERIMENTAL_DAGGER_RUNNER_HOST", "unix:///var/run/buildkit/buildkitd.sock"
+                ).with_unix_socket("/var/run/buildkit/buildkitd.sock", docker_host_socket)
+
+            await pytest_container
+            return True
         except dagger.ExecError as e:
             logger.error("Tests failed")
-            logger.error(e.stdout)
             logger.error(e.stderr)
+            return False
