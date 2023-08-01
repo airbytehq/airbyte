b7ab109bcab4304a6ba7c3a1938a708e11828f49
Dimosthenis Schizas <dimos@hackthebox.eu>
2023-08-01 19:38:34 +0300

    Add support for start date in Quaderno integration

--- airbyte-integrations/connectors/source-quaderno/source_quaderno/streams.py
+++ airbyte-integrations/connectors/source-quaderno/source_quaderno/streams.py
@@ -2,16 +2,17 @@
 # Copyright (c) 2023 Airbyte, Inc., all rights reserved.
 #
 
-import math
-from typing import Any, Iterable, List, Mapping, MutableMapping, Optional, Union
+from typing import Any, Iterable, List, Mapping, MutableMapping, Optional, Tuple, Union
 from urllib.parse import parse_qsl, urlparse
 
+import pendulum
 import requests
+from airbyte_cdk.models import SyncMode
 from airbyte_cdk.sources.streams.http import HttpStream
+from pendulum.date import Date
 
 DEFAULT_PRIMARY_KEY = "id"
-DEFAULT_CURSOR = "id"
-DEFAULT_SORT_KEY = "created_at"
+DEFAULT_CURSOR = "issue_date"
 DEFAULT_LIMIT = 100
 
 
@@ -43,9 +44,7 @@ class QuadernoStream(HttpStream):
     def __init__(self, config: dict, **kwargs):
         super().__init__(**kwargs)
         self._url_base = f"https://{config['account_name']}.quadernoapp.com/api/"
-        self._start_date = None
-        if config.get("start_date"):
-            self._start_date = config["start_date"]
+        self._start_date = pendulum.parse(config.get("start_date")).date()
 
     @property
     def url_base(self) -> str:
@@ -54,8 +53,7 @@ class QuadernoStream(HttpStream):
     @property
     def primary_key(self) -> Optional[Union[str, List[str], List[List[str]]]]:
         """
-        The Quaderno resource primary key. Most of the Quaderno resources have `id` as a primary ID. Other Quaderno
-        resources have different primary key or a composite key can override this method.
+        The Quaderno resource primary key. Most of the Quaderno resources have `id` as a primary ID.
 
         :return: The Quaderno resource primary key(s)
         :rtype: Either `str`, list(str) or list(list(str))
@@ -86,7 +84,7 @@ class QuadernoStream(HttpStream):
         next_page_token: Mapping[str, Any] = None
     ) -> MutableMapping[str, Any]:
         """Define any query parameters to be set."""
-        params = {"limit": 100}
+        params = {"limit": self.limit}
         if next_page_token:
             params.update(next_page_token)
         return params
@@ -99,9 +97,49 @@ class QuadernoStream(HttpStream):
         yield from response_json
 
 
-class IncrementalQuadernoStream(QuadernoStream):
+class BasePaginationQuadernoStream(QuadernoStream):
+    def request_params(
+        self,
+        stream_state: Mapping[str, Any],
+        stream_slice: Mapping[str, Any] = None,
+        next_page_token: Mapping[str, Any] = None,
+    ) -> MutableMapping[str, Any]:
+        params = super().request_params(stream_state, stream_slice, next_page_token)
+        if 'date' in stream_slice:
+            params['date'] = stream_slice['date']
+        return params
 
-    state_checkpoint_interval = 10
+    def chunk_dates(self, start_date: Date) -> Iterable[Tuple[Date, Date]]:
+        today = pendulum.today().date()
+        step = pendulum.duration(days=1)
+        after_date = start_date
+        while after_date < today:
+            before_date = min(today, after_date + step)
+            yield after_date, before_date
+            after_date = before_date + step
+
+    def stream_slices(
+        self, *, sync_mode: SyncMode, cursor_field: List[str] = None, stream_state: Mapping[str, Any] = None
+    ) -> Iterable[Optional[Mapping[str, Any]]]:
+        for start, end in self.chunk_dates(self._start_date):
+            yield {"date": f"{start.isoformat(), end.isoformat()}"}
+
+    def read_records(
+        self,
+        sync_mode: SyncMode,
+        cursor_field: List[str] = None,
+        stream_slice: Mapping[str, Any] = None,
+        stream_state: Mapping[str, Any] = None,
+    ) -> Iterable[Mapping[str, Any]]:
+        if stream_slice is None:
+            return []
+
+        yield from super().read_records(sync_mode, cursor_field, stream_slice, stream_state)
+
+
+class IncrementalQuadernoStream(BasePaginationQuadernoStream):
+
+    state_checkpoint_interval = None
 
     @property
     def cursor_field(self) -> Union[str, List[str]]:
@@ -123,10 +161,29 @@ class IncrementalQuadernoStream(QuadernoStream):
         and the current state and picks the 'most' recent cursor. This is how a stream's state is determined.
         Required for incremental.
         """
-        current_id = (current_stream_state or {}).get(self.cursor_field, math.inf)
-        latest_record_id = latest_record[self.cursor_field]
+        current_issue_date = pendulum.parse(
+            (current_stream_state or {}).get(self.cursor_field, pendulum.from_timestamp(0).isoformat())
+        )
+        latest_record_issue_date = pendulum.parse(latest_record[self.cursor_field])
 
-        return {self.cursor_field: min(current_id, latest_record_id)}
+        return {self.cursor_field: max(current_issue_date, latest_record_issue_date).to_date_string()}
+
+    def stream_slices(
+        self, *, sync_mode: SyncMode, cursor_field: List[str] = None, stream_state: Mapping[str, Any] = None
+    ) -> Iterable[Optional[Mapping[str, Any]]]:
+        start_date = self.get_start_date(stream_state)
+        if start_date >= pendulum.today().date():
+            # if the state is in the future - this will produce a state message but not make an API request
+            yield None
+        else:
+            for start, end in self.chunk_dates(start_date):
+                yield {"date": f"{start.isoformat(), end.isoformat()}"}
+
+    def get_start_date(self, stream_state) -> Date:
+        start_point = self._start_date
+        if stream_state and self.cursor_field in stream_state:
+            start_point = max(start_point, pendulum.parse(stream_state[self.cursor_field]).date())
+        return start_point
 
 
 class Credits(IncrementalQuadernoStream):
