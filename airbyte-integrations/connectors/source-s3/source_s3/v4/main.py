58326287c9579e7bcf799db6f9886be49598f594
Alexandre Girard <alexandre@airbyte.io>
2023-08-02 16:50:49 -0700

    format

--- airbyte-integrations/connectors/source-s3/source_s3/v4/main.py
+++ airbyte-integrations/connectors/source-s3/source_s3/v4/main.py
@@ -9,13 +9,12 @@ from typing import List, Any, Mapping, Tuple, Optional
 
 from airbyte_cdk.entrypoint import AirbyteEntrypoint, launch
 from airbyte_cdk.models import AirbyteErrorTraceMessage, AirbyteMessage, AirbyteTraceMessage, TraceType, Type
-from airbyte_cdk.sources.file_based.file_based_source import FileBasedSource
 from airbyte_cdk.models import ConnectorSpecification
-
-from airbyte_cdk.sources.streams import Stream
-from source_s3.source import SourceS3Spec, SourceS3
-from source_s3.v4 import Config, SourceS3StreamReader
 from airbyte_cdk.sources import AbstractSource
+from airbyte_cdk.sources.file_based.file_based_source import FileBasedSource
+from airbyte_cdk.sources.streams import Stream
+
+from source_s3.v4 import Config, SourceS3StreamReader
 
 
 class AdaptedSourceS3(AbstractSource):
@@ -48,7 +47,7 @@ def get_source(args: List[str]):
                     type=TraceType.ERROR,
                     emitted_at=int(datetime.now().timestamp() * 1000),
                     error=AirbyteErrorTraceMessage(
-                        message=f"Error starting the sync. This could be due to an invalid configuration or catalog. Please contact Support for assistance.",
+                        message="Error starting the sync. This could be due to an invalid configuration or catalog. Please contact Support for assistance.",
                         stack_trace=traceback.format_exc(),
                     ),
                 )
