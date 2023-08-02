39014b9e2627160607696f33ffe94485bb662b4e
Catherine Noll <noll.catherine@gmail.com>
2023-08-02 10:41:41 -0400

    Source S3: use v4 if specified by the config

--- airbyte-integrations/connectors/source-s3/main.py
+++ airbyte-integrations/connectors/source-s3/main.py
@@ -5,9 +5,17 @@
 
 import sys
 
-from airbyte_cdk.entrypoint import launch
+from airbyte_cdk.entrypoint import AirbyteEntrypoint, launch
 from source_s3 import SourceS3
+from source_s3.v4.main import get_source
 
 if __name__ == "__main__":
+    args = sys.argv[1:]
+    config_path = AirbyteEntrypoint.extract_config(args)
     source = SourceS3()
+    if config_path:
+        config = source.read_config(config_path)
+        if config.get("config_version"):
+            source = get_source(args)
+
     launch(source, sys.argv[1:])
