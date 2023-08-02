146680abd9222ab5aa3329f839190cfce6f00be5
Alexandre Girard <alexandre@airbyte.io>
2023-08-02 08:06:32 -0700

    Update

--- airbyte-cdk/python/airbyte_cdk/sources/file_based/file_types/csv_parser.py
+++ airbyte-cdk/python/airbyte_cdk/sources/file_based/file_types/csv_parser.py
@@ -88,12 +88,10 @@ class CsvParser(FileTypeParser):
                 #  sources will likely require one. Rather than modify the interface now we can wait until the real use case
                 self._skip_rows_before_header(fp, config_format.skip_rows_before_header)
                 field_names = self._auto_generate_headers(fp, config_format) if config_format.autogenerate_column_names else None
-                logger.info(f"Using field names: {field_names}")
                 reader = csv.DictReader(fp, dialect=dialect_name, fieldnames=field_names)  # type: ignore
                 yield from self._read_and_cast_types(reader, schema, config_format, logger)
         else:
             with stream_reader.open_file(file, self.file_read_mode, logger) as fp:
-                # self._skip_rows_before_header(fp, config_format.skip_rows_before_header)
                 reader = csv.DictReader(fp)  # type: ignore
                 yield from self._read_and_cast_types(reader, schema, CsvFormat(), logger)
 
