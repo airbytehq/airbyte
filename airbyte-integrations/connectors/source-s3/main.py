292f28054a566679e733f607e87eb0dec333b2af
Alexandre Girard <alexandre@airbyte.io>
2023-08-02 16:38:13 -0700

    tests should pass

--- airbyte-integrations/connectors/source-s3/main.py
+++ airbyte-integrations/connectors/source-s3/main.py
@@ -9,17 +9,17 @@ from airbyte_cdk.entrypoint import AirbyteEntrypoint, launch
 from source_s3 import SourceS3
 from source_s3.v4.main import get_source
 
-def _use_legacy_source(args, config_path):
-    return not config_path and args[0] == "read"
+def _use_file_based(args, config):
+    return config.get("config_version") or args[0] == "spec"
 
 if __name__ == "__main__":
     args = sys.argv[1:]
     config_path = AirbyteEntrypoint.extract_config(args)
     # FIXME: I think we can move a lot of this logic inside AdaptedSourceS3
     source = SourceS3()
-    if not _use_legacy_source(args, config_path):
-        config = source.read_config(config_path) if config_path else {}
-        if config.get("config_version") or args[0] == "spec":
-            source = get_source(args)
+
+    config = source.read_config(config_path) if config_path else {}
+    if _use_file_based(args, config):
+        source = get_source(args)
 
     launch(source, sys.argv[1:])
