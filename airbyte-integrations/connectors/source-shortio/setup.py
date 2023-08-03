b42b7b1d1d23d5053bfc6e14210a88ff0a17490e
btkcodedev <btk.codedev@gmail.com>
2023-08-03 22:03:17 +0530

    ✨Source Shortio: Migrate Python CDK to Low-code CDK (#28950)
    * Migrate Shortio to Low-Code
    
    * Update abnormal state
    
    * Format
    
    * Update Docs
    
    * Fix metadata.yaml
    
    * Add pagination
    
    * Add incremental sync
    
    * add incremental parameters
    
    * update metadata
    
    * rollback update version
    
    * release date
    
    ---------
    
    Co-authored-by: marcosmarxm <marcosmarxm@gmail.com>

--- airbyte-integrations/connectors/source-shortio/setup.py
+++ airbyte-integrations/connectors/source-shortio/setup.py
@@ -5,10 +5,13 @@
 
 from setuptools import find_packages, setup
 
-MAIN_REQUIREMENTS = ["airbyte-cdk~=0.1"]
+MAIN_REQUIREMENTS = [
+    "airbyte-cdk~=0.1",
+]
 
 TEST_REQUIREMENTS = [
-    "pytest~=6.2.5",
+    "pytest~=6.2",
+    "pytest-mock~=3.6.1",
     "connector-acceptance-test",
 ]
 
@@ -19,7 +22,7 @@ setup(
     author_email="contact@airbyte.io",
     packages=find_packages(),
     install_requires=MAIN_REQUIREMENTS,
-    package_data={"": ["*.json", "schemas/*.json", "schemas/shared/*.json"]},
+    package_data={"": ["*.json", "*.yaml", "schemas/*.json", "schemas/shared/*.json"]},
     extras_require={
         "tests": TEST_REQUIREMENTS,
     },
