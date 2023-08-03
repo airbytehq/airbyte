c6ded3f9ece6c2ce77c01946a55770a0d84b8a43
Joe Reuter <joe@airbyte.io>
2023-08-03 12:30:59 +0200

    🚨🚨 Low code CDK: Decouple SimpleRetriever and HttpStream (#28657)
    * fix tests
    
    * format
    
    * review comments
    
    * Automated Commit - Formatting Changes
    
    * review comments
    
    * review comments
    
    * review comments
    
    * log all messages
    
    * log all message
    
    * review comments
    
    * review comments
    
    * Automated Commit - Formatting Changes
    
    * add comment
    
    ---------
    
    Co-authored-by: flash1293 <flash1293@users.noreply.github.com>

--- airbyte-cdk/python/airbyte_cdk/sources/declarative/parsers/model_to_component_factory.py
+++ airbyte-cdk/python/airbyte_cdk/sources/declarative/parsers/model_to_component_factory.py
@@ -696,7 +696,9 @@ class ModelToComponentFactory:
             http_method=model_http_method,
             request_options_provider=request_options_provider,
             config=config,
+            disable_retries=self._disable_retries,
             parameters=model.parameters or {},
+            message_repository=self._message_repository,
         )
 
     @staticmethod
@@ -913,8 +915,6 @@ class ModelToComponentFactory:
                 config=config,
                 maximum_number_of_slices=self._limit_slices_fetched or 5,
                 parameters=model.parameters or {},
-                disable_retries=self._disable_retries,
-                message_repository=self._message_repository,
             )
         return SimpleRetriever(
             name=name,
@@ -926,8 +926,6 @@ class ModelToComponentFactory:
             cursor=cursor,
             config=config,
             parameters=model.parameters or {},
-            disable_retries=self._disable_retries,
-            message_repository=self._message_repository,
         )
 
     @staticmethod
