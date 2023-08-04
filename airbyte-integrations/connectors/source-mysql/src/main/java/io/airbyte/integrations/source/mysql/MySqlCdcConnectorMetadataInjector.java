807f1044147edd71cb6750c3744bdfac95c03328
Akash Kulkarni <113392464+akashkulk@users.noreply.github.com>
2023-08-04 11:57:51 -0700

    source-mysql : CDC PK snapshotting (#28757)
    * Initial Commit of CDC PK snapshotting
    
    * Added unit tests
    
    * Flip to true
    
    * Refactor flags
    
    * Re-add flag
    
    * Comment cleanup
    
    * Code review comments
    
    * Automated Commit - Format and Process Resources Changes
    
    * minor refactor
    
    * Address comments
    
    ---------
    
    Co-authored-by: akashkulk <akashkulk@users.noreply.github.com>
    Co-authored-by: subodh <subodh1810@gmail.com>

--- airbyte-integrations/connectors/source-mysql/src/main/java/io/airbyte/integrations/source/mysql/MySqlCdcConnectorMetadataInjector.java
+++ airbyte-integrations/connectors/source-mysql/src/main/java/io/airbyte/integrations/source/mysql/MySqlCdcConnectorMetadataInjector.java
@@ -4,19 +4,31 @@
 
 package io.airbyte.integrations.source.mysql;
 
+import static io.airbyte.integrations.debezium.internals.DebeziumEventUtils.CDC_DELETED_AT;
+import static io.airbyte.integrations.debezium.internals.DebeziumEventUtils.CDC_UPDATED_AT;
 import static io.airbyte.integrations.source.mysql.MySqlSource.CDC_LOG_FILE;
 import static io.airbyte.integrations.source.mysql.MySqlSource.CDC_LOG_POS;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import io.airbyte.integrations.debezium.CdcMetadataInjector;
+import io.airbyte.integrations.debezium.internals.mysql.MySqlDebeziumStateUtil.MysqlDebeziumStateAttributes;
 
-public class MySqlCdcConnectorMetadataInjector implements CdcMetadataInjector {
+public class MySqlCdcConnectorMetadataInjector implements CdcMetadataInjector<MysqlDebeziumStateAttributes> {
 
   @Override
   public void addMetaData(final ObjectNode event, final JsonNode source) {
     event.put(CDC_LOG_FILE, source.get("file").asText());
     event.put(CDC_LOG_POS, source.get("pos").asLong());
+  }
+
+  @Override
+  public void addMetaDataToRowsFetchedOutsideDebezium(final ObjectNode record, final String transactionTimestamp,
+      final MysqlDebeziumStateAttributes debeziumStateAttributes) {
+    record.put(CDC_UPDATED_AT, transactionTimestamp);
+    record.put(CDC_LOG_FILE, debeziumStateAttributes.binlogFilename());
+    record.put(CDC_LOG_POS, debeziumStateAttributes.binlogPosition());
+    record.put(CDC_DELETED_AT, (String) null);
   }
 
   @Override
