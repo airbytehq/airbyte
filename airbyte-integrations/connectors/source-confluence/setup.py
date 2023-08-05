e587bc4811b9fa4ce896038d849cc4c229a1c05f
Tope Folorunso <folorunsotemitope10@gmail.com>
2023-08-06 00:25:09 +0100

    migrate confluence to low code

--- airbyte-integrations/connectors/source-confluence/setup.py
+++ airbyte-integrations/connectors/source-confluence/setup.py
@@ -6,24 +6,23 @@
 from setuptools import find_packages, setup
 
 MAIN_REQUIREMENTS = [
-    "airbyte-cdk",
+    "airbyte-cdk~=0.1",
 ]
 
 TEST_REQUIREMENTS = [
-    "pytest~=6.1",
+    "pytest~=6.2",
     "pytest-mock~=3.6.1",
     "connector-acceptance-test",
-    "responses~=0.13.3",
 ]
 
 setup(
     name="source_confluence",
     description="Source implementation for Confluence.",
-    author="Tuan Nguyen",
-    author_email="anhtuan.nguyen@me.com",
+    author="Airbyte",
+    author_email="contact@airbyte.io",
     packages=find_packages(),
     install_requires=MAIN_REQUIREMENTS,
-    package_data={"": ["*.json", "schemas/*.json", "schemas/shared/*.json"]},
+    package_data={"": ["*.json", "*.yaml", "schemas/*.json", "schemas/shared/*.json"]},
     extras_require={
         "tests": TEST_REQUIREMENTS,
     },
