cfb9071e7f390c7186bf26ab7fc1de0135c6e193
sajarin <sajarindider@gmail.com>
2023-08-07 11:31:17 -0400

    fix: format files and fix backward compatibility version

--- airbyte-integrations/connectors/source-retently/source_retently/components.py
+++ airbyte-integrations/connectors/source-retently/source_retently/components.py
@@ -4,6 +4,7 @@
 
 from dataclasses import dataclass
 from typing import Any, Mapping
+
 from airbyte_cdk.sources.declarative.auth import DeclarativeOauth2Authenticator
 from airbyte_cdk.sources.declarative.auth.declarative_authenticator import DeclarativeAuthenticator
 from airbyte_cdk.sources.declarative.auth.token import ApiKeyAuthenticator
@@ -16,7 +17,7 @@ class AuthenticatorRetently(DeclarativeAuthenticator):
     oauth: DeclarativeOauth2Authenticator
 
     def __new__(cls, api_auth, oauth, config, *args, **kwargs):
-        if config['credentials']['api_key']:
+        if config["credentials"]["api_key"]:
             return api_auth
         else:
             return oauth
