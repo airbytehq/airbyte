9914166643da111713544b24b16002d5f9153986
Catherine Noll <noll.catherine@gmail.com>
2023-08-07 11:29:28 -0400

    DO NOT MERGE: minimal example with `format` in an array not working

--- airbyte-cdk/python/airbyte_cdk/sources/file_based/config/abstract_file_based_spec.py
+++ airbyte-cdk/python/airbyte_cdk/sources/file_based/config/abstract_file_based_spec.py
@@ -40,72 +40,82 @@ class AbstractFileBasedSpec(BaseModel):
         """
         Generates the mapping comprised of the config fields
         """
-        schema = super().schema(*args, **kwargs)
-        transformed_schema = copy.deepcopy(schema)
-        schema_helpers.expand_refs(transformed_schema)
-        cls.replace_enum_allOf_and_anyOf(transformed_schema)
+        # schema = super().schema(*args, **kwargs)
+        # transformed_schema = copy.deepcopy(schema)
+        # schema_helpers.expand_refs(transformed_schema)
+        # cls.replace_enum_allOf_and_anyOf(transformed_schema)
         transformed_schema = {
             'title': 'Config',
             'description': 'Used during spec; allows the developer to configure the cloud provider specific options\nthat are needed when users configure a file-based source.',
             'type': 'object',
             'properties': {
-                'name': {
-                    'title': 'Name',
-                    'description': 'The name of the stream.',
-                    'type': 'string'
-                },
-                'format': {
-                    'title': 'File Format',
-                    'description': 'This is the <b>description</b> for the file format field',
-                    'default': 'csv',
-                    'type': 'object',
-                    'oneOf': [
-                        {
-                            'title': 'Avro',
-                            'type': 'object',
-                            'properties': {
-                                'filetype': {
-                                    'title': 'Filetype',
-                                    'const': 'avro',
-                                    'type': 'string'
-                                },
-                                'decimal_as_float': {
-                                    'title': 'Convert Decimal Fields to Floats',
-                                    'description': 'Whether to convert decimal fields to floats. There is a loss of precision when converting decimals to floats, so this is not recommended.',
-                                    'default': False,
-                                    'type': 'boolean'
-                                }
-                            }
-                        }, {
-                            'title': 'CSV',
-                            'description': 'This is the description for the CSV option',
-                            'type': 'object',
-                            'properties': {
-                                'filetype': {
-                                    'title': 'Filetype',
-                                    'const': 'csv',
-                                    'type': 'string'
-                                },
-                                'delimiter': {
-                                    'title': 'Delimiter',
-                                    'description': "The character delimiting individual cells in the CSV data. This may only be a 1-character string. For tab-delimited data enter '\\t'.",
-                                    'default': ',',
-                                    'type': 'string'
-                                }
-                            }
-                        }
-                    ]
-                },
-                'globs': {
-                    'title': 'Globs',
-                    'description': 'The pattern used to specify which files should be selected from the file system. For more information on glob pattern matching look <a href="https://en.wikipedia.org/wiki/Glob_(programming)">here</a>.',
+                'streams': {
+                    'title': 'The list of streams to sync',
+                    'description': 'Each instance of this configuration defines a <a href="https://docs.airbyte.com/cloud/core-concepts#stream">stream</a>. Use this to define which files belong in the stream, their format, and how they should be parsed and validated. When sending data to warehouse destination such as Snowflake or BigQuery, each stream is a separate table.',
+                    'order': 10,
                     'type': 'array',
-                    'items': {'type': 'string'}
-                },
-                'validation_policy': {
-                    'title': 'Validation Policy',
-                    'description': 'The name of the validation policy that dictates sync behavior when a record does not adhere to the stream schema.',
-                    'enum': ['Emit Record', 'Skip Record','Wait for Discover']
+                    'items': {
+                        'title': 'FileBasedStreamConfig',
+                        'type': 'object',
+                        'properties': {
+                            'name': {
+                                'title': 'Name',
+                                'description': 'The name of the stream.',
+                                'type': 'string'
+                            },
+                            'format': {
+                                'title': 'Format',
+                                'type': 'object',
+                                'oneOf': [
+                                    {
+                                        'title': 'Avro Format',
+                                        'type': 'object',
+                                        'properties': {
+                                            'filetype': {
+                                                'title': 'Filetype',
+                                                'const': 'avro',
+                                                'type': 'string'
+                                            },
+                                            'decimal_as_float': {
+                                                'title': 'Convert Decimal Fields to Floats',
+                                                'description': 'Whether to convert decimal fields to floats. There is a loss of precision when converting decimals to floats, so this is not recommended.',
+                                                'default': False,
+                                                'type': 'boolean'
+                                            }
+                                        }
+                                    }, {
+                                        'title': 'CSV Format',
+                                        'type': 'object',
+                                        'properties': {
+                                            'filetype': {
+                                                'title': 'Filetype',
+                                                'const': 'csv',
+                                                'type': 'string'
+                                            },
+                                            'delimiter': {
+                                                'title': 'Delimiter',
+                                                'description': "The character delimiting individual cells in the CSV data. This may only be a 1-character string. For tab-delimited data enter '\\t'.",
+                                                'default': ',',
+                                                'type': 'string'
+                                            }
+                                        }
+                                    }
+                                ]
+                            },
+                            'globs': {
+                                'title': 'Globs',
+                                'description': 'The pattern used to specify which files should be selected from the file system. For more information on glob pattern matching look <a href="https://en.wikipedia.org/wiki/Glob_(programming)">here</a>.',
+                                'type': 'array',
+                                'items': {'type': 'string'}
+                            },
+                            'validation_policy': {
+                                'title': 'Validation Policy',
+                                'description': 'The name of the validation policy that dictates sync behavior when a record does not adhere to the stream schema.',
+                                'enum': ['Emit Record', 'Skip Record','Wait for Discover']
+                            }
+                        },
+                        'required': ['name', 'format', 'validation_policy']
+                    }
                 },
                 'bucket': {
                     'title': 'Bucket',
@@ -127,15 +137,18 @@ class AbstractFileBasedSpec(BaseModel):
         anyOfs are also not supported by the UI, so we replace them with the similar oneOf, with the
         additional validation that an incoming config only matches exactly one of a field's types.
         """
-        objects_to_check = schema["properties"]["streams"]["items"]["properties"]["format"]
+        objects_to_check = schema["properties"]["streams"]["items"]["properties"]["file_type"]
         objects_to_check["type"] = "object"
-        objects_to_check["oneOf"] = objects_to_check.pop("anyOf", [])
-        for format in objects_to_check["oneOf"]:
-            for key in format["properties"]:
-                object_property = format["properties"][key]
-                if "allOf" in object_property and "enum" in object_property["allOf"][0]:
-                    object_property["enum"] = object_property["allOf"][0]["enum"]
-                    object_property.pop("allOf")
+        [all_of] = objects_to_check.pop("allOf")
+        objects_to_check["properties"] = all_of["properties"]
+        objects_to_check["properties"]["format"]["oneOf"] = objects_to_check["properties"]["format"].pop("anyOf")
+
+        # for format in objects_to_check["oneOf"]:
+        #     for key in format["properties"]:
+        #         object_property = format["properties"][key]
+        #         if "allOf" in object_property and "enum" in object_property["allOf"][0]:
+        #             object_property["enum"] = object_property["allOf"][0]["enum"]
+        #             object_property.pop("allOf")
 
         properties_to_change = ["validation_policy"]
         for property_to_change in properties_to_change:
@@ -146,4 +159,24 @@ class AbstractFileBasedSpec(BaseModel):
             if "allOf" in property_object and "enum" in property_object["allOf"][0]:
                 property_object["enum"] = property_object["allOf"][0]["enum"]
                 property_object.pop("allOf")
+
+        # properties_to_change = ["file_type"]
+        # for property_to_change in properties_to_change:
+        #     property_object = schema["properties"]["streams"]["items"]["properties"][property_to_change]
+        #     if "anyOf" in property_object:
+        #         schema["properties"]["streams"]["items"]["properties"][property_to_change]["type"] = "object"
+        #         schema["properties"]["streams"]["items"]["properties"][property_to_change]["oneOf"] = property_object.pop("anyOf")
+        #     if "allOf" in property_object and "enum" in property_object["allOf"][0]:
+        #         property_object["oneOf"] = property_object.pop("allOf")
+        #
+        #
+        # properties_to_change = ["format"]
+        # for property_to_change in properties_to_change:
+        #     for k, item in schema["properties"]["streams"]["items"]["properties"]["file_type"].items():
+        #         if "anyOf" in item:
+        #             schema["properties"]["streams"]["items"]["properties"][property_to_change]["type"] = "object"
+        #             schema["properties"]["streams"]["items"]["properties"][property_to_change]["oneOf"] = item.pop("anyOf")
+        #         if "allOf" in item:
+        #             item["enum"] = item.pop("allOf")
+
         return schema
