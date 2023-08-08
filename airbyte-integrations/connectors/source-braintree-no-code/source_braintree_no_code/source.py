f0eb96c4ac704ac67115df058f94840c9d15daf5
Mikhail Masyagin <mikhail.masyagin@anec.app>
2023-08-08 10:49:14 +0300

    anecdote-api: small fixes

--- airbyte-integrations/connectors/source-braintree-no-code/source_braintree_no_code/source.py
+++ airbyte-integrations/connectors/source-braintree-no-code/source_braintree_no_code/source.py
@@ -1,7 +1,17 @@
 #
 # Copyright (c) 2023 Airbyte, Inc., all rights reserved.
 #
+import json
+from dataclasses import dataclass
+from typing import List, Union
 
+from braintree.attribute_getter import AttributeGetter
+from braintree.merchant_account.merchant_account import MerchantAccount
+from braintree.util.xml_util import XmlUtil
+
+import requests
+from airbyte_cdk.sources.declarative.extractors.record_extractor import RecordExtractor
+from airbyte_cdk.sources.declarative.types import Record
 from airbyte_cdk.sources.declarative.yaml_declarative_source import YamlDeclarativeSource
 
 """
@@ -10,6 +20,37 @@ source connector.
 
 WARNING: Do not modify this file.
 """
+
+
+def get_json_from_resource(resource_obj: Union[AttributeGetter, List[AttributeGetter]]):
+    if isinstance(resource_obj, list):
+        return [obj if not isinstance(obj, AttributeGetter) else get_json_from_resource(obj) for obj in resource_obj]
+    obj_dict = resource_obj.__dict__
+    result = dict()
+    for attr in obj_dict:
+        if not attr.startswith("_"):
+            result[attr] = (
+                get_json_from_resource(obj_dict[attr])
+                if isinstance(obj_dict[attr], (AttributeGetter, list))
+                else obj_dict[attr]
+            )
+    return result
+
+
+@dataclass
+class MerchantAccountExtractor(RecordExtractor):
+    """
+    Extractor for Merchant Accounts stream.
+    It parses output XML and finds all `Merchant Account` occurrences in it.
+    """
+
+    def extract_records(self, response: requests.Response,
+                        ) -> List[Record]:
+        merchant_accounts = XmlUtil.dict_from_xml(response.text)['merchant_accounts']['merchant_account']
+        merchant_accounts = [MerchantAccount(None, merchant_account) for merchant_account in merchant_accounts]
+        merchant_accounts = [get_json_from_resource(merchant_account) for merchant_account in merchant_accounts]
+        print(merchant_accounts)
+        return []
 
 
 # Declarative Source
