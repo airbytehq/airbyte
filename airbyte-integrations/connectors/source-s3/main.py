2ce42832d90a7c344653dff9c98b07837bed2632
Alexandre Girard <alexandre@airbyte.io>
2023-08-09 13:47:45 -0700

    Keep v3 for spec

--- airbyte-integrations/connectors/source-s3/main.py
+++ airbyte-integrations/connectors/source-s3/main.py
@@ -6,10 +6,15 @@
 import sys
 
 from airbyte_cdk.entrypoint import AirbyteEntrypoint, launch
-from source_s3.v4 import Config, SourceS3, SourceS3StreamReader
+from source_s3.v4 import Config, SourceS3StreamReader
+from source_s3.v4 import SourceS3 as SourceS3V4
+from source_s3 import SourceS3 as SourceS3V3
 
 if __name__ == "__main__":
     args = sys.argv[1:]
-    catalog_path = AirbyteEntrypoint.extract_catalog(args)
-    source = SourceS3(SourceS3StreamReader(), Config, catalog_path)
+    if args[0] == "spec":
+        source = SourceS3V3()
+    else:
+        catalog_path = AirbyteEntrypoint.extract_catalog(args)
+        source = SourceS3V4(SourceS3StreamReader(), Config, catalog_path)
     launch(source, sys.argv[1:])
