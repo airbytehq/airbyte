aec002cf13d2e459474154451316598c13fcecae
Mikhail Masyagin <mikhail.masyagin@anec.app>
2023-08-10 00:15:40 +0300

    airbyte: update braintree

--- airbyte-integrations/connectors/source-braintree-no-code/source_braintree_no_code/source.py
+++ airbyte-integrations/connectors/source-braintree-no-code/source_braintree_no_code/source.py
@@ -10,14 +10,22 @@ from airbyte_cdk.sources.declarative.extractors.record_extractor import RecordEx
 from airbyte_cdk.sources.declarative.types import Record
 from airbyte_cdk.sources.declarative.yaml_declarative_source import YamlDeclarativeSource
 from braintree.attribute_getter import AttributeGetter
-from braintree.customer import Customer
-from braintree.discount import Discount
-from braintree.dispute import Dispute
-from braintree.merchant_account.merchant_account import MerchantAccount
-from braintree.plan import Plan
-from braintree.subscription import Subscription
-from braintree.transaction import Transaction
+from braintree.customer import Customer as BCustomer
+from braintree.discount import Discount as BDiscount
+from braintree.dispute import Dispute as BDispute
+from braintree.merchant_account.merchant_account import MerchantAccount as BMerchantAccount
+from braintree.plan import Plan as BPlan
+from braintree.subscription import Subscription as BSubscription
+from braintree.transaction import Transaction as BTransaction
 from braintree.util.xml_util import XmlUtil
+
+from source_braintree_no_code.schemas.customer import Customer
+from source_braintree_no_code.schemas.merchant_account import MerchantAccount
+from source_braintree_no_code.schemas.discount import Discount
+from source_braintree_no_code.schemas.subscription import Subscription
+from source_braintree_no_code.schemas.transaction import Transaction
+from source_braintree_no_code.schemas.plan import Plan
+from source_braintree_no_code.schemas.dispute import Dispute
 
 """
 This file provides the necessary constructs to interpret a provided declarative YAML configuration file into
@@ -55,8 +63,7 @@ class BraintreeExtractor(RecordExtractor):
                 result[attr] = (
                     self._get_json_from_resource(obj_dict[attr])
                     if isinstance(obj_dict[attr], (AttributeGetter, list))
-                    else obj_dict[attr].strftime('%Y-%m-%dT%H:%M:%SZ')
-                    if isinstance(obj_dict[attr], datetime.datetime) else obj_dict[attr]
+                    else obj_dict[attr]
                 )
         return result
 
@@ -72,7 +79,8 @@ class MerchantAccountExtractor(BraintreeExtractor):
                         ) -> List[Record]:
         data = XmlUtil.dict_from_xml(response.text)['merchant_accounts']
         merchant_accounts = self._extract_as_array(data, 'merchant_account')
-        return [self._get_json_from_resource(MerchantAccount(None, merchant_account)) for merchant_account in merchant_accounts]
+        return [MerchantAccount(**self._get_json_from_resource(BMerchantAccount(None, merchant_account))).dict(exclude_unset=True)
+                for merchant_account in merchant_accounts]
 
 
 @dataclass
@@ -86,7 +94,8 @@ class CustomerExtractor(BraintreeExtractor):
                         ) -> List[Record]:
         data = XmlUtil.dict_from_xml(response.text)['customers']
         customers = self._extract_as_array(data, 'customer')
-        return [self._get_json_from_resource(Customer(None, customer)) for customer in customers]
+        return [Customer(**self._get_json_from_resource(BCustomer(None, customer))).dict(exclude_unset=True)
+                for customer in customers]
 
 
 @dataclass
@@ -100,7 +109,8 @@ class DiscountExtractor(BraintreeExtractor):
                         ) -> List[Record]:
         data = XmlUtil.dict_from_xml(response.text)
         discounts = self._extract_as_array(data, 'discounts')
-        return [self._get_json_from_resource(Discount(None, discount)) for discount in discounts]
+        return [Discount(**self._get_json_from_resource(BDiscount(None, discount))).dict(exclude_unset=True)
+                for discount in discounts]
 
 
 @dataclass
@@ -114,7 +124,8 @@ class TransactionExtractor(BraintreeExtractor):
                         ) -> List[Record]:
         data = XmlUtil.dict_from_xml(response.text)['credit_card_transactions']
         transactions = self._extract_as_array(data, 'transaction')
-        return [self._get_json_from_resource(Transaction(None, transaction)) for transaction in transactions]
+        return [Transaction(**self._get_json_from_resource(BTransaction(None, transaction))).dict(exclude_unset=True)
+                for transaction in transactions]
 
 
 @dataclass
@@ -128,7 +139,8 @@ class SubscriptionExtractor(BraintreeExtractor):
                         ) -> List[Record]:
         data = XmlUtil.dict_from_xml(response.text)['subscriptions']
         subscriptions = self._extract_as_array(data, 'subscription')
-        return [self._get_json_from_resource(Subscription(None, subscription)) for subscription in subscriptions]
+        return [Subscription(**self._get_json_from_resource(BSubscription(None, subscription))).dict(exclude_unset=True)
+                for subscription in subscriptions]
 
 
 @dataclass
@@ -142,7 +154,8 @@ class PlanExtractor(BraintreeExtractor):
                         ) -> List[Record]:
         data = XmlUtil.dict_from_xml(response.text)
         plans = self._extract_as_array(data, 'plans')
-        return [self._get_json_from_resource(Plan(None, plan)) for plan in plans]
+        return [Plan(**self._get_json_from_resource(BPlan(None, plan))).dict(exclude_unset=True)
+                for plan in plans]
 
 
 @dataclass
@@ -156,7 +169,8 @@ class DisputeExtractor(BraintreeExtractor):
                         ) -> List[Record]:
         data = XmlUtil.dict_from_xml(response.text)['disputes']
         disputes = self._extract_as_array(data, 'dispute')
-        return [self._get_json_from_resource(Dispute(None, dispute)) for dispute in disputes]
+        return [Dispute(**self._get_json_from_resource(BDispute(dispute))).dict(exclude_unset=True)
+                for dispute in disputes]
 
 
 # Declarative Source
