2b36c852ae921da74836d42315c6dd26c799b502
Alexandre Cuoci <alex.cuoci@airbyte.io>
2023-08-09 13:51:29 -0400

    remove extra postgres entry

--- docusaurus/sidebars.js
+++ docusaurus/sidebars.js
@@ -10,7 +10,7 @@ function getFilenamesInDir(prefix, dir, excludes) {
     .readdirSync(dir)
     .filter(
       (fileName) =>
-        !(fileName.endsWith(".inapp.md") || fileName.endsWith("-migrations.md"))
+        !(fileName.endsWith(".inapp.md") || fileName.endsWith("-migrations.md") || fileName.endsWith("postgres.md"))
     )
     .map((fileName) => fileName.replace(".md", ""))
     .filter((fileName) => excludes.indexOf(fileName.toLowerCase()) === -1)
