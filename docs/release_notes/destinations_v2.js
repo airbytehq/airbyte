f11af17de5da4310e37400d28ce02596f7309c78
Edward Gao <edward.gao@airbyte.io>
2023-08-09 16:07:54 -0700

    refactor

--- docs/release_notes/destinations_v2.js
+++ docs/release_notes/destinations_v2.js
@@ -16,16 +16,6 @@ function concatenateRawTableName(namespace, name) {
   return namespace + "_raw" + "_".repeat(longestUnderscoreRun + 1) + "stream_" + name;
 }
 
-function setSql(namespace, name, destination, sql, defaultMessage) {
-  var output;
-  if (namespace != "" && name != "") {
-    output = sql;
-  } else {
-    output = defaultMessage;
-  }
-  document.getElementById("sql_output_block_" + destination).innerHTML = output;
-}
-
 const SqlOutput = ({ destination, defaultMessage }) => {
   return (
     <pre id={ "sql_output_block_" + destination }>
@@ -35,17 +25,10 @@ const SqlOutput = ({ destination, defaultMessage }) => {
 };
 
 export const BigQueryMigrationGenerator = () => {
-  const defaultMessage =
-`Enter your stream's name and namespace to generate a SQL statement.
-If your stream has no namespace, take the default value from the destination connector's settings.
-You may need to tweak the sql statement if your stream name/namespace contain unusual characters.`;
-  function updateSql(event) {
-    let namespace = document.getElementById("stream_namespace_bigquery").value;
-    let name = document.getElementById("stream_name_bigquery").value;
+  function generateSql(namespace, name) {
     let v2RawTableName = '`' + concatenateRawTableName(namespace, name) + '`';
     let v1RawTableName = '`' + namespace + '`.`_airbyte_raw_' + name + '`';
-    let sql =
-`CREATE OR REPLACE TABLE \`airbyte_internal\`.${v2RawTableName} (
+    return `CREATE OR REPLACE TABLE \`airbyte_internal\`.${v2RawTableName} (
   _airbyte_raw_id STRING NOT NULL,
   _airbyte_data JSON NOT NULL,
   _airbyte_extracted_at TIMESTAMP NOT NULL,
@@ -60,31 +43,17 @@ AS (
         CAST(NULL AS TIMESTAMP) AS _airbyte_loaded_at
     FROM ${v1RawTableName}
 )`;
-    setSql(namespace, name, "bigquery", sql, defaultMessage);
   }
   return (
-    <div>
-      <label>Stream namespace </label>
-      <input type="text" id="stream_namespace_bigquery" name="stream_namespace" onChange={ updateSql }/><br/>
-      <label>Stream name </label>
-      <input type="text" id="stream_name_bigquery" name="stream_name" onChange={ updateSql }/><br/>
-      <SqlOutput destination="bigquery" defaultMessage={ defaultMessage }/>
-    </div>
-  )
+    <MigrationGenerator destination="bigquery" generateSql={generateSql}/>
+  );
 }
 
 export const SnowflakeMigrationGenerator = () => {
-  const defaultMessage =
-`Enter your stream's name and namespace to see the SQL output.
-If your stream has no namespace, take the default value from the destination connector's settings.
-You may need to tweak the sql statement if your stream name/namespace contain unusual characters.`;
-  function updateSql(event) {
-    let namespace = document.getElementById("stream_namespace_snowflake").value;
-    let name = document.getElementById("stream_name_snowflake").value;
+  function generateSql(namespace, name) {
     let v2RawTableName = '"' + concatenateRawTableName(namespace, name) + '"';
     let v1RawTableName = namespace + '._airbyte_raw_' + name;
-    let sql =
-`CREATE OR REPLACE TABLE "airbyte_internal".${v2RawTableName} (
+    return `CREATE OR REPLACE TABLE "airbyte_internal".${v2RawTableName} (
   "_airbyte_raw_id" STRING NOT NULL,
   "_airbyte_data" JSON NOT NULL,
   "_airbyte_extracted_at" TIMESTAMP NOT NULL,
@@ -94,20 +63,40 @@ CLUSTER BY "_airbyte_extracted_at"
 AS (
     SELECT
         _airbyte_ab_id AS "_airbyte_raw_id",
-        PARSE_JSON(_airbyte_data) AS "_airbyte_data",
+        _airbyte_data AS "_airbyte_data",
         _airbyte_emitted_at AS "_airbyte_extracted_at",
         CAST(NULL AS TIMESTAMP) AS "_airbyte_loaded_at"
     FROM ${v1RawTableName}
-)`
-    setSql(namespace, name, "snowflake", sql, defaultMessage)
+)`;
+  }
+  return (
+    <MigrationGenerator destination="snowflake" generateSql={generateSql}/>
+  );
+}
+
+export const MigrationGenerator = ({destination, generateSql}) => {
+  const defaultMessage =
+`Enter your stream's name and namespace to see the SQL output.
+If your stream has no namespace, take the default value from the destination connector's settings.`;
+  function updateSql(event) {
+    let namespace = document.getElementById("stream_namespace_" + destination).value;
+    let name = document.getElementById("stream_name_" + destination).value;
+    let sql = generateSql(namespace, name);
+    var output;
+    if (namespace != "" && name != "") {
+      output = sql;
+    } else {
+      output = defaultMessage;
+    }
+    document.getElementById("sql_output_block_" + destination).innerHTML = output;
   }
   return (
     <div>
       <label>Stream namespace </label>
-      <input type="text" id="stream_namespace_snowflake" name="stream_namespace" onChange={ updateSql }/><br/>
+      <input type="text" id={"stream_namespace_" + destination} onChange={ updateSql }/><br/>
       <label>Stream name </label>
-      <input type="text" id="stream_name_snowflake" name="stream_name" onChange={ updateSql }/><br/>
-      <SqlOutput destination="snowflake" defaultMessage={ defaultMessage }/>
+      <input type="text" id={"stream_name_" + destination} onChange={ updateSql }/><br/>
+      <SqlOutput destination={destination} defaultMessage={ defaultMessage }/>
     </div>
-  )
+  );
 }
