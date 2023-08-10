ae381f347532376a83154f9d3efb7c2172a69f89
tomasspangelo <tomas@spangelo.com>
2023-08-10 14:05:04 +0200

    users

--- airbyte-integrations/connectors/source-provet/source_provet/source.py
+++ airbyte-integrations/connectors/source-provet/source_provet/source.py
@@ -244,7 +244,7 @@ class Users(IncrementalProvetStream):
     def path(
         self, stream_state: Mapping[str, Any] = None, stream_slice: Mapping[str, Any] = None, next_page_token: Mapping[str, Any] = None
     ) -> str:
-        return "invoicepayment"
+        return "user"
 
 
 # Source
