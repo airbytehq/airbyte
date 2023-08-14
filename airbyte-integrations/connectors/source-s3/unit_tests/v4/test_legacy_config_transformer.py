3859201f41c05a740460e1fc7eb05c4eae8ac4f3
Brian Lai <51336873+brianjlai@users.noreply.github.com>
2023-08-14 18:47:08 -0400

    ✨ [file-based cdk] S3 file format adapter (#29353)
    * [ISSUE #28893] infer csv schema
    
    * [ISSUE #28893] align with pyarrow
    
    * Automated Commit - Formatting Changes
    
    * [ISSUE #28893] legacy inference and infer only when needed
    
    * [ISSUE #28893] fix scenario tests
    
    * [ISSUE #28893] using discovered schema as part of read
    
    * [ISSUE #28893] self-review + cleanup
    
    * [ISSUE #28893] fix test
    
    * [ISSUE #28893] code review part #1
    
    * [ISSUE #28893] code review part #2
    
    * Fix test
    
    * formatcdk
    
    * [ISSUE #28893] code review
    
    * FIX test log level
    
    * Re-adding failing tests
    
    * [ISSUE #28893] improve inferrence to consider multiple types per value
    
    * Automated Commit - Formatting Changes
    
    * add file adapters for avro, csv, jsonl, and parquet
    
    * fix try catch
    
    * pr feedback with a few additional default options set
    
    * fix things from the rebase of master
    
    ---------
    
    Co-authored-by: maxi297 <maxime@airbyte.io>
    Co-authored-by: maxi297 <maxi297@users.noreply.github.com>

--- airbyte-integrations/connectors/source-s3/unit_tests/v4/test_legacy_config_transformer.py
+++ airbyte-integrations/connectors/source-s3/unit_tests/v4/test_legacy_config_transformer.py
@@ -24,14 +24,9 @@ from source_s3.v4.legacy_config_transformer import LegacyConfigTransformer
 
                 },
                 "format": {
-                    "filetype": "csv",
-                    "delimiter": "^",
-                    "quote_char": "|",
-                    "escape_char": "!",
-                    "double_quote": True,
-                    "quoting_behavior": "Quote All"
+                    "filetype": "avro",
                 },
-                "path_pattern": "**/*.csv",
+                "path_pattern": "**/*.avro",
                 "schema": '{"col1": "string", "col2": "integer"}'
             },
             {
@@ -43,10 +38,13 @@ from source_s3.v4.legacy_config_transformer import LegacyConfigTransformer
                 "streams": [
                     {
                         "name": "test_data",
-                        "file_type": "csv",
-                        "globs": ["a_folder/**/*.csv"],
+                        "file_type": "avro",
+                        "globs": ["a_folder/**/*.avro"],
                         "validation_policy": "Emit Record",
                         "input_schema": '{"col1": "string", "col2": "integer"}',
+                        "format": {
+                            "filetype": "avro"
+                        }
                     }
                 ]
             }
@@ -62,7 +60,7 @@ from source_s3.v4.legacy_config_transformer import LegacyConfigTransformer
                 "format": {
                     "filetype": "avro",
                 },
-                "path_pattern": "**/*.csv",
+                "path_pattern": "**/*.avro",
             },
             {
                 "bucket": "test_bucket",
@@ -70,8 +68,11 @@ from source_s3.v4.legacy_config_transformer import LegacyConfigTransformer
                     {
                         "name": "test_data",
                         "file_type": "avro",
-                        "globs": ["**/*.csv"],
+                        "globs": ["**/*.avro"],
                         "validation_policy": "Emit Record",
+                        "format": {
+                            "filetype": "avro"
+                        }
                     }
                 ]
             }
@@ -84,3 +85,172 @@ def test_convert_legacy_config(legacy_config, expected_config):
     actual_config = LegacyConfigTransformer.convert(parsed_legacy_config)
 
     assert actual_config == expected_config
+
+
+@pytest.mark.parametrize(
+    "file_type,legacy_format_config,expected_format_config, expected_error",
+    [
+        pytest.param(
+            "csv",
+            {
+                "filetype": "csv",
+                "delimiter": "&",
+                "infer_datatypes": False,
+                "quote_char": "^",
+                "escape_char": "$",
+                "encoding": "ansi",
+                "double_quote": False,
+                "newlines_in_values": True,
+                "additional_reader_options": "{\"strings_can_be_null\": true}",
+                "advanced_options": "{\"skip_rows\": 3, \"skip_rows_after_names\": 5, \"autogenerate_column_names\": true}",
+                "blocksize": 20000,
+            },
+            {
+                "filetype": "csv",
+                "delimiter": "&",
+                "quote_char": "^",
+                "escape_char": "$",
+                "encoding": "ansi",
+                "double_quote": False,
+                "null_values": ["", "null", "NULL", "N/A", "NA", "NaN", "None"],
+                "true_values": ["y", "yes", "t", "true", "on", "1"],
+                "false_values": ["n", "no", "f", "false", "off", "0"],
+                "inference_type": "None",
+                "strings_can_be_null": True,
+                "skip_rows_before_header": 3,
+                "skip_rows_after_header": 5,
+                "autogenerate_column_names": True,
+            },
+            None,
+            id="test_csv_all_legacy_options_set"),
+        pytest.param(
+            "csv",
+            {
+                "filetype": "csv",
+                "delimiter": "&",
+                "quote_char": "^",
+                "double_quote": True,
+                "newlines_in_values": False,
+            },
+            {
+                "filetype": "csv",
+                "delimiter": "&",
+                "quote_char": "^",
+                "encoding": "utf8",
+                "double_quote": True,
+                "null_values": ["", "null", "NULL", "N/A", "NA", "NaN", "None"],
+                "true_values": ["y", "yes", "t", "true", "on", "1"],
+                "false_values": ["n", "no", "f", "false", "off", "0"],
+                "inference_type": "Primitive Types Only",
+                "strings_can_be_null": False,
+            },
+            None,
+            id="test_csv_only_required_options"),
+        pytest.param(
+            "csv",
+            {},
+            {
+                "filetype": "csv",
+                "delimiter": ",",
+                "quote_char": "\"",
+                "encoding": "utf8",
+                "double_quote": True,
+                "null_values": ["", "null", "NULL", "N/A", "NA", "NaN", "None"],
+                "true_values": ["y", "yes", "t", "true", "on", "1"],
+                "false_values": ["n", "no", "f", "false", "off", "0"],
+                "inference_type": "Primitive Types Only",
+                "strings_can_be_null": False,
+            },
+            None,
+            id="test_csv_empty_format"),
+        pytest.param(
+            "csv",
+            {
+                "additional_reader_options": "{\"not_valid\": \"at all}",
+            },
+            None,
+            ValueError,
+            id="test_malformed_additional_reader_options"),
+        pytest.param(
+            "csv",
+            {
+                "advanced_options": "{\"not_valid\": \"at all}",
+            },
+            None,
+            ValueError,
+            id="test_malformed_advanced_options"),
+        pytest.param(
+            "jsonl",
+            {
+                "filetype": "jsonl",
+                "newlines_in_values": True,
+                "unexpected_field_behavior": "ignore",
+                "block_size": 0,
+            },
+            {
+                "filetype": "jsonl"
+            },
+            None,
+            id="test_jsonl_format"),
+        pytest.param(
+            "parquet",
+            {
+                "filetype": "parquet",
+                "columns": ["test"],
+                "batch_size": 65536,
+                "buffer_size": 100,
+            },
+            {
+                "filetype": "parquet"
+            },
+            None,
+            id="test_parquet_format"),
+        pytest.param(
+            "avro",
+            {
+                "filetype": "avro",
+            },
+            {
+                "filetype": "avro"
+            },
+            None,
+            id="test_avro_format"),
+    ]
+)
+def test_convert_file_format(file_type, legacy_format_config, expected_format_config, expected_error):
+    legacy_config = {
+        "dataset": "test_data",
+        "provider": {
+            "storage": "S3",
+            "bucket": "test_bucket",
+            "aws_access_key_id": "some_access_key",
+            "aws_secret_access_key": "some_secret",
+
+        },
+        "format": legacy_format_config,
+        "path_pattern": f"**/*.{file_type}",
+    }
+
+    expected_config = {
+        "bucket": "test_bucket",
+        "aws_access_key_id": "some_access_key",
+        "aws_secret_access_key": "some_secret",
+        "streams": [
+            {
+                "name": "test_data",
+                "file_type": file_type,
+                "globs": [f"**/*.{file_type}"],
+                "validation_policy": "Emit Record",
+                "format": expected_format_config
+            }
+        ]
+    }
+
+    parsed_legacy_config = SourceS3Spec(**legacy_config)
+
+    if expected_error:
+        with pytest.raises(expected_error):
+            LegacyConfigTransformer.convert(parsed_legacy_config)
+    else:
+        actual_config = LegacyConfigTransformer.convert(parsed_legacy_config)
+        assert actual_config == expected_config
