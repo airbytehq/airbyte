b1d3e8839ca5bec38daf85b9901f6140cb286994
Artem Inzhyyants <36314070+artem1205@users.noreply.github.com>
2023-08-14 09:46:42 +0200

    Source SalesForce: move to next releaseStage (#28781)
    * Source SalesForce: handle bulk API errors
    
    * Source SalesForce: update docs
    
    * Source SalesForce: bump CDK version
    
    * Source SalesForce: raise config error
    
    * Source SalesForce: add stream slice test
    
    * Source SalesForce: add bulk pagination tests + fixes
    
    * Update Dockerfile
    
    * Source SalesForce: fix start_date in BULK request params
    
    * Source SalesForce: add pattern descriptor
    
    * Source SalesForce: update description in spec
    
    * Source SalesForce: fix stream_state < stream_slice
    
    * Source SalesForce: update comments
    
    * Source Salesforce: update docs + spec description
    
    * Source Salesforce: add unittest
    
    * Source Salesforce: add typing
    
    * Source Salesforce: fix import
    
    * Source Salesforce: fix formatting
    
    * Source Salesforce: remove extra comment
    
    * Apply suggestions from code review
    
    * Source Salesforce: fix unit tests
    
    * Source Salesforce: refactor
    
    ---------
    
    Co-authored-by: Sherif A. Nada <snadalive@gmail.com>
    Co-authored-by: Artem Inzhyyants <artem.inzhyyants@globallogic.com>

--- airbyte-integrations/connectors/source-salesforce/setup.py
+++ airbyte-integrations/connectors/source-salesforce/setup.py
@@ -5,9 +5,9 @@
 
 from setuptools import find_packages, setup
 
-MAIN_REQUIREMENTS = ["airbyte-cdk~=0.2", "vcrpy==4.1.1", "pandas"]
+MAIN_REQUIREMENTS = ["airbyte-cdk~=0.50", "pandas"]
 
-TEST_REQUIREMENTS = ["requests-mock~=1.9.3", "pytest~=6.1", "pytest-mock~=3.6", "requests_mock", "pytest-timeout"]
+TEST_REQUIREMENTS = ["freezegun", "pytest~=6.1", "pytest-mock~=3.6", "requests-mock~=1.9.3", "pytest-timeout"]
 
 setup(
     name="source_salesforce",
