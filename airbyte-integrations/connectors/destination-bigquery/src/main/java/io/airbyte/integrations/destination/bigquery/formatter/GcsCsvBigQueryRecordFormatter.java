b29f83b5665d6af650d0bc1344561aeeae951ddc
tryangul <ryan.broughan@gmail.com>
2023-08-15 15:42:16 -0700

    Re-order fields for CSV formatter.

--- airbyte-integrations/connectors/destination-bigquery/src/main/java/io/airbyte/integrations/destination/bigquery/formatter/GcsCsvBigQueryRecordFormatter.java
+++ airbyte-integrations/connectors/destination-bigquery/src/main/java/io/airbyte/integrations/destination/bigquery/formatter/GcsCsvBigQueryRecordFormatter.java
@@ -5,6 +5,11 @@
 package io.airbyte.integrations.destination.bigquery.formatter;
 
 import com.fasterxml.jackson.databind.JsonNode;
+import com.google.cloud.bigquery.Field;
+import com.google.cloud.bigquery.Schema;
+import com.google.cloud.bigquery.StandardSQLTypeName;
+import io.airbyte.integrations.base.JavaBaseConstants;
+import io.airbyte.integrations.base.TypingAndDedupingFlag;
 import io.airbyte.integrations.destination.StandardNameTransformer;
 
 /**
@@ -13,8 +18,23 @@ import io.airbyte.integrations.destination.StandardNameTransformer;
  */
 public class GcsCsvBigQueryRecordFormatter extends DefaultBigQueryRecordFormatter {
 
+  public static final com.google.cloud.bigquery.Schema CSV_SCHEMA = com.google.cloud.bigquery.Schema.of(
+    Field.of(JavaBaseConstants.COLUMN_NAME_AB_ID, StandardSQLTypeName.STRING),
+    Field.of(JavaBaseConstants.COLUMN_NAME_DATA, StandardSQLTypeName.STRING),
+    Field.of(JavaBaseConstants.COLUMN_NAME_EMITTED_AT, StandardSQLTypeName.TIMESTAMP));
+
   public GcsCsvBigQueryRecordFormatter(JsonNode jsonSchema, StandardNameTransformer namingResolver) {
     super(jsonSchema, namingResolver);
+  }
+
+  @Override
+  public Schema getBigQuerySchema(JsonNode jsonSchema) {
+    if (TypingAndDedupingFlag.isDestinationV2()) {
+      // not sure if this needs overridden :|
+      return SCHEMA_V2;
+    } else {
+      return CSV_SCHEMA;
+    }
   }
 
 }
