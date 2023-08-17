49d706b0cbd88f8c7e6a831887099bd5c78453b3
Aaron Steers <aj@airbyte.io>
2023-08-17 05:14:53 -0700

    chore: updated script

--- airbyte-cdk/java/airbyte-cdk/_temp_migration_script.sh
+++ airbyte-cdk/java/airbyte-cdk/_temp_migration_script.sh
@@ -6,18 +6,62 @@
 #   MIGRATE_SH=./airbyte-cdk/java/airbyte-cdk/_temp_migration_script.sh
 #   $MIGRATE_SH <OLD_PACKAGE_ROOT> <SUBPACKAGE_PATH>
 #
-# Examples:
+# Core capabilities:
 #   $MIGRATE_SH airbyte-db/db-lib db
 #   $MIGRATE_SH airbyte-integrations/bases/base-java
 #   $MIGRATE_SH airbyte-integrations/bases/base-java-s3
 #   $MIGRATE_SH airbyte-integrations/bases/base-typing-deduping
-#   $MIGRATE_SH airbyte-integrations/bases/base-typing-deduping-test
-#   $MIGRATE_SH airbyte-integrations/bases/bases-destination-jdbc
 #   $MIGRATE_SH airbyte-integrations/bases/debezium
+#
+# Abstract DB connectors:
+#   $MIGRATE_SH airbyte-integrations/connectors/source-jdbc
+#   $MIGRATE_SH airbyte-integrations/connectors/source-relational-db
+#   $MIGRATE_SH airbyte-integrations/bases/bases-destination-jdbc
+#
+# Test capabilities:
+#   $MIGRATE_SH airbyte-integrations/bases/base-typing-deduping-test
 #   $MIGRATE_SH airbyte-integrations/bases/s3-destination-base-integration-test
 #   $MIGRATE_SH airbyte-integrations/bases/standard-destination-test
 #   $MIGRATE_SH airbyte-integrations/bases/standard-source-test
-#   $MIGRATE_SH airbyte-integrations/connectors/source-jdbc
+#   $MIGRATE_SH airbyte-test-utils
+#
+# Post-run cleanup stesps:
+# - Find and delete references to the moved packages.
+# - Add the new airbyte-java-connector plugin to connectors' build.gradle files.
+# - Search-and-replace:
+#   - Within /airbyte-cdk/java/airbyte-cdk:
+#     - Find:         `package io.airbyte`
+#     - Replace with: `package io.airbyte.cdk`
+#   - Within /airbyte-cdk/java/airbyte-cdk (because the above is not idempotent):
+#     - Find:         `package io.airbyte.cdk.cdk`
+#     - Replace with: `package io.airbyte.cdk`
+#   - Find all the packages that were moved, and name them in a way that is specific to the CDK inclusions:
+#     - Find (regex): `io.airbyte...` # Everything that now shows up with a search for `package io.airbyte.cdk`
+#                      io.airbyte.db
+#                      io.airbyte.integrations.base
+#                      io.airbyte.integrations.debezium
+#                      io.airbyte.integrations.destination.NamingConventionTransformer
+#                      io.airbyte.integrations.destination.StandardNameTransformer
+#                      io.airbyte.integrations.destination.jdbc
+#                      io.airbyte.integrations.destination.normalization
+#                      io.airbyte.integrations.destination.record_buffer
+#                      io.airbyte.integrations.destination.s3
+#                      io.airbyte.integrations.destination.buffered_stream_consumer
+#                      io.airbyte.integrations.destination.dest_state_lifecycle_manager
+#                      io.airbyte.integrations.destination.staging
+#                      io.airbyte.integrations.destination_async
+#                      io.airbyte.integrations.standardtest
+#                      io.airbyte.integrations.source.jdbc
+#                      io.airbyte.integrations.source.relationaldb
+#                      io.airbyte.integrations.util
+#                      io.airbyte.integrations.BaseConnector
+#                      io.airbyte.test.utils
+#   - Within rest of repo:
+#     - Find (regex) based on the above list:
+#         io\.airbyte\.(db|integrations\.base|integrations\.debezium|integrations\.destination\.NamingConventionTransformer|integrations\.destination\.StandardNameTransformer|integrations\.destination\.jdbc|integrations\.destination\.normalization|integrations\.destination\.record_buffer|integrations\.destination\.s3|integrations\.destination\.buffered_stream_consumer|integrations\.destination\.dest_state_lifecycle_manager|integrations\.destination\.staging|integrations\.destination_async|integrations\.standardtest|integrations\.source\.jdbc|integrations\.source\.relationaldb|integrations\.util|integrations\.BaseConnector|test\.utils)
+#     - Replace with: `io.airbyte.cdk.$1`
+#     - Exclude files: _temp_migration_script.sh,*.html,build,bin
+# - Replace references to the moved packages with the new package names.
 
 # Other notes:
 # - This script is idempotent. It should become a no-op if run to completion.
