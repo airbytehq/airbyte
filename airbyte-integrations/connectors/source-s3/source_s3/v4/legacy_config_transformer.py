435ae061a4aa5ef6f28ff9f14ea9185aa40d2cea
maxi297 <maxime@airbyte.io>
2023-08-17 18:51:29 -0400

    consider additional_reader_options

--- airbyte-integrations/connectors/source-s3/source_s3/v4/legacy_config_transformer.py
+++ airbyte-integrations/connectors/source-s3/source_s3/v4/legacy_config_transformer.py
@@ -80,9 +80,9 @@ class LegacyConfigTransformer:
                 "delimiter": format_options.delimiter,
                 "quote_char": format_options.quote_char,
                 "double_quote": format_options.double_quote,
-                "null_values": ["", "#N/A", "#N/A N/A", "#NA", "-1.#IND", "-1.#QNAN", "-NaN", "-nan", "1.#IND", "1.#QNAN", "N/A", "NA",  "NULL", "NaN",  "n/a", "nan", "null"],
-                "true_values": ["1", "True", "TRUE", "true"],
-                "false_values": ["0", "False", "FALSE", "false"],
+                "null_values": additional_reader_options["null_values"] if "null_values" in additional_reader_options else ["", "#N/A", "#N/A N/A", "#NA", "-1.#IND", "-1.#QNAN", "-NaN", "-nan", "1.#IND", "1.#QNAN", "N/A", "NA",  "NULL", "NaN",  "n/a", "nan", "null"],
+                "true_values": additional_reader_options["true_values"] if "true_values" in additional_reader_options else ["1", "True", "TRUE", "true"],
+                "false_values": additional_reader_options["false_values"] if "false_values" in additional_reader_options else ["0", "False", "FALSE", "false"],
                 "inference_type": "Primitive Types Only" if format_options.infer_datatypes else "None",
                 "strings_can_be_null": additional_reader_options.get("strings_can_be_null", False),
             }
