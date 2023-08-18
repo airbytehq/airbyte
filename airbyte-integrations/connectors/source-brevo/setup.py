c786fad11a8f39481785507abd0c2c6b89fc996a
Mickaël Andrieu <mickael.andrieu@article-1.eu>
2023-08-18 21:22:25 +0200

    [Source] Added Brevo

---
+++ airbyte-integrations/connectors/source-brevo/setup.py
@@ -0,0 +1,29 @@
+#
+# Copyright (c) 2023 Airbyte, Inc., all rights reserved.
+#
+
+
+from setuptools import find_packages, setup
+
+MAIN_REQUIREMENTS = [
+    "airbyte-cdk~=0.1",
+]
+
+TEST_REQUIREMENTS = [
+    "requests-mock~=1.9.3",
+    "pytest~=6.1",
+    "pytest-mock~=3.6.1",
+]
+
+setup(
+    name="source_brevo",
+    description="Source implementation for Brevo.",
+    author="Mickaël Andrieu",
+    author_email="mickael.andrieu@article-1.eu",
+    packages=find_packages(),
+    install_requires=MAIN_REQUIREMENTS,
+    package_data={"": ["*.json", "*.yaml", "schemas/*.json", "schemas/shared/*.json"]},
+    extras_require={
+        "tests": TEST_REQUIREMENTS,
+    },
+)
