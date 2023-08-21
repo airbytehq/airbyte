70aa9c4766cd96861f3565d5be53f323e03833ae
jdpgrailsdev <jpearlin1@gmail.com>
2023-08-21 13:41:34 -0400

    Construct initial Debezium state from resume token

---
+++ airbyte-integrations/bases/debezium/src/main/java/io/airbyte/integrations/debezium/internals/mongodb/MongodbDebeziumConstants.java
@@ -0,0 +1,24 @@
+package io.airbyte.integrations.debezium.internals.mongodb;
+
+/**
+ * A collection of constants for use with the Debezium MongoDB Connector.
+ */
+public class MongodbDebeziumConstants {
+
+    /**
+     * Constants for Debezium Offset State storage.
+     */
+    public static class OffsetState {
+        public static final String KEY_REPLICA_SET = "rs";
+
+        public static final String KEY_SERVER_ID = "server_id";
+
+        public static final String VALUE_INCREMENT = "ord";
+
+        public static final String VALUE_RESUME_TOKEN = "resume_token";
+
+        public static final String VALUE_SECONDS = "sec";
+
+        public static final String VALUE_TRANSACTION_ID = "transaction_id";
+    }
+}
