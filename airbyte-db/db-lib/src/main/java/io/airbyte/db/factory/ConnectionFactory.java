011bceaa1b7dcfce9df65521a481e37f82e652ff
benmoriceau <benoit@airbyte.io>
2023-08-21 16:31:31 -0700

    Add comment in the connection factory

--- airbyte-db/db-lib/src/main/java/io/airbyte/db/factory/ConnectionFactory.java
+++ airbyte-db/db-lib/src/main/java/io/airbyte/db/factory/ConnectionFactory.java
@@ -7,6 +7,15 @@ import java.util.Map;
 import java.util.Properties;
 
 public class ConnectionFactory {
+
+    /**
+     * Construct a new {@link Connection} instance using the provided configuration.
+     * @param username The username of the database user.
+     * @param password The password of the database user.
+     * @param connectionProperties The extra properties to add to the connection.
+     * @param jdbcConnectionString The JDBC connection string.
+     * @return The configured {@link Connection}
+     */
     public static Connection create(final String username,
                                     final String password,
                                     final Map<String, String> connectionProperties,
