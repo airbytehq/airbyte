9d05fe27f2966215cf17155d90c6edc417a417a6
jdpgrailsdev <jpearlin1@gmail.com>
2023-08-21 13:42:46 -0400

    Formatting

--- airbyte-integrations/bases/debezium/src/main/java/io/airbyte/integrations/debezium/internals/mongodb/MongodbDebeziumStateUtil.java
+++ airbyte-integrations/bases/debezium/src/main/java/io/airbyte/integrations/debezium/internals/mongodb/MongodbDebeziumStateUtil.java
@@ -1,3 +1,7 @@
+/*
+ * Copyright (c) 2023 Airbyte, Inc., all rights reserved.
+ */
+
 package io.airbyte.integrations.debezium.internals.mongodb;
 
 import com.fasterxml.jackson.databind.JsonNode;
@@ -7,67 +11,67 @@ import com.mongodb.client.MongoClient;
 import com.mongodb.client.model.changestream.ChangeStreamDocument;
 import io.airbyte.commons.json.Jsons;
 import io.debezium.connector.mongodb.ResumeTokens;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import org.bson.BsonDocument;
 import org.bson.BsonString;
 import org.bson.BsonTimestamp;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 /**
  * Collection of utility methods related to the Debezium offset state.
  */
 public class MongodbDebeziumStateUtil {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(MongodbDebeziumStateUtil.class);
+  private static final Logger LOGGER = LoggerFactory.getLogger(MongodbDebeziumStateUtil.class);
 
-    /**
-     * Constructs the initial Debezium offset state that will be used by the incremental CDC snapshot after an
-     * initial snapshot sync.
-     *
-     * @param mongoClient The {@link MongoClient} used to query the MongoDB server.
-     * @param database The database associated with the sync.
-     * @param replicaSet The replication set associated with the sync.
-     * @return The initial Debezium offset state storage document as a {@link JsonNode}.
-     */
-    public JsonNode constructInitialDebeziumState(final MongoClient mongoClient, final String database, final String replicaSet) {
-        final BsonDocument resumeToken = getResumeToken(mongoClient);
-        final String resumeTokenData = ((BsonString) ResumeTokens.getData(resumeToken)).getValue();
-        final BsonTimestamp timestamp = ResumeTokens.getTimestamp(resumeToken);
+  /**
+   * Constructs the initial Debezium offset state that will be used by the incremental CDC snapshot
+   * after an initial snapshot sync.
+   *
+   * @param mongoClient The {@link MongoClient} used to query the MongoDB server.
+   * @param database The database associated with the sync.
+   * @param replicaSet The replication set associated with the sync.
+   * @return The initial Debezium offset state storage document as a {@link JsonNode}.
+   */
+  public JsonNode constructInitialDebeziumState(final MongoClient mongoClient, final String database, final String replicaSet) {
+    final BsonDocument resumeToken = getResumeToken(mongoClient);
+    final String resumeTokenData = ((BsonString) ResumeTokens.getData(resumeToken)).getValue();
+    final BsonTimestamp timestamp = ResumeTokens.getTimestamp(resumeToken);
 
-        final List<Map<String,Object>> key = List.of(
-                Map.of(MongodbDebeziumConstants.OffsetState.KEY_REPLICA_SET, replicaSet,
-                        MongodbDebeziumConstants.OffsetState.KEY_SERVER_ID, database));
+    final List<Map<String, Object>> key = List.of(
+        Map.of(MongodbDebeziumConstants.OffsetState.KEY_REPLICA_SET, replicaSet,
+            MongodbDebeziumConstants.OffsetState.KEY_SERVER_ID, database));
 
-        final Map<String,Object> value = new HashMap<>();
-        value.put(MongodbDebeziumConstants.OffsetState.VALUE_SECONDS, timestamp.getTime());
-        value.put(MongodbDebeziumConstants.OffsetState.VALUE_INCREMENT, timestamp.getInc());
-        value.put(MongodbDebeziumConstants.OffsetState.VALUE_TRANSACTION_ID, null);
-        value.put(MongodbDebeziumConstants.OffsetState.VALUE_RESUME_TOKEN, resumeTokenData);
+    final Map<String, Object> value = new HashMap<>();
+    value.put(MongodbDebeziumConstants.OffsetState.VALUE_SECONDS, timestamp.getTime());
+    value.put(MongodbDebeziumConstants.OffsetState.VALUE_INCREMENT, timestamp.getInc());
+    value.put(MongodbDebeziumConstants.OffsetState.VALUE_TRANSACTION_ID, null);
+    value.put(MongodbDebeziumConstants.OffsetState.VALUE_RESUME_TOKEN, resumeTokenData);
 
-        final JsonNode state = Jsons.jsonNode(Map.of(key, value));
-        LOGGER.info("Initial Debezium state constructed: {}", state);
-        return state;
+    final JsonNode state = Jsons.jsonNode(Map.of(key, value));
+    LOGGER.info("Initial Debezium state constructed: {}", state);
+    return state;
+  }
+
+  /**
+   * Retrieves the most recent resume token from MongoDB server.
+   *
+   * @param mongoClient The {@link MongoClient} used to query the MongoDB server.
+   * @return The most recent resume token value.
+   */
+  private BsonDocument getResumeToken(final MongoClient mongoClient) {
+    final ChangeStreamIterable<BsonDocument> eventStream = mongoClient.watch(BsonDocument.class);
+    try (final MongoChangeStreamCursor<ChangeStreamDocument<BsonDocument>> eventStreamCursor = eventStream.cursor()) {
+      /*
+       * Must call tryNext before attempting to get the resume token from the cursor directly. Otherwise,
+       * the call to getResumeToken() will return null!
+       */
+      eventStreamCursor.tryNext();
+      return eventStreamCursor.getResumeToken();
     }
+  }
 
-    /**
-     * Retrieves the most recent resume token from MongoDB server.
-     *
-     * @param mongoClient The {@link MongoClient} used to query the MongoDB server.
-     * @return The most recent resume token value.
-     */
-    private BsonDocument getResumeToken(final MongoClient mongoClient) {
-        final ChangeStreamIterable<BsonDocument> eventStream = mongoClient.watch(BsonDocument.class);
-        try (final MongoChangeStreamCursor<ChangeStreamDocument<BsonDocument>> eventStreamCursor = eventStream.cursor()) {
-            /*
-             * Must call tryNext before attempting to get the resume token from the cursor directly.
-             * Otherwise, the call to getResumeToken() will return null!
-             */
-            eventStreamCursor.tryNext();
-            return eventStreamCursor.getResumeToken();
-        }
-    }
 }
--- airbyte-integrations/bases/debezium/src/test/java/io/airbyte/integrations/debezium/internals/mongodb/MongodbDebeziumStateUtilTest.java
+++ airbyte-integrations/bases/debezium/src/test/java/io/airbyte/integrations/debezium/internals/mongodb/MongodbDebeziumStateUtilTest.java
@@ -1,3 +1,7 @@
+/*
+ * Copyright (c) 2023 Airbyte, Inc., all rights reserved.
+ */
+
 package io.airbyte.integrations.debezium.internals.mongodb;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -19,40 +23,41 @@ import org.junit.jupiter.api.Test;
 
 class MongodbDebeziumStateUtilTest {
 
-    private MongodbDebeziumStateUtil mongodbDebeziumStateUtil;
+  private MongodbDebeziumStateUtil mongodbDebeziumStateUtil;
 
-    @BeforeEach
-    void setup() {
-        mongodbDebeziumStateUtil = new MongodbDebeziumStateUtil();
-    }
+  @BeforeEach
+  void setup() {
+    mongodbDebeziumStateUtil = new MongodbDebeziumStateUtil();
+  }
 
-    @Test
-    void testConstructInitialDebeziumState() {
-        final String database = "test";
-        final String replicaSet = "test_rs";
-        final String resumeToken = "8264BEB9F3000000012B0229296E04";
-        final BsonDocument resumeTokenDocument = mock(BsonDocument.class);
-        final ChangeStreamIterable changeStreamIterable = mock(ChangeStreamIterable.class);
-        final MongoChangeStreamCursor<ChangeStreamDocument<BsonDocument>> mongoChangeStreamCursor =
-                mock(MongoChangeStreamCursor.class);
-        final MongoClient mongoClient = mock(MongoClient.class);
+  @Test
+  void testConstructInitialDebeziumState() {
+    final String database = "test";
+    final String replicaSet = "test_rs";
+    final String resumeToken = "8264BEB9F3000000012B0229296E04";
+    final BsonDocument resumeTokenDocument = mock(BsonDocument.class);
+    final ChangeStreamIterable changeStreamIterable = mock(ChangeStreamIterable.class);
+    final MongoChangeStreamCursor<ChangeStreamDocument<BsonDocument>> mongoChangeStreamCursor =
+        mock(MongoChangeStreamCursor.class);
+    final MongoClient mongoClient = mock(MongoClient.class);
 
-        when(resumeTokenDocument.containsKey("_data")).thenReturn(true);
-        when(resumeTokenDocument.get("_data")).thenReturn(new BsonString(resumeToken));
-        when(mongoChangeStreamCursor.getResumeToken()).thenReturn(resumeTokenDocument);
-        when(changeStreamIterable.cursor()).thenReturn(mongoChangeStreamCursor);
-        when(mongoClient.watch(BsonDocument.class)).thenReturn(changeStreamIterable);
+    when(resumeTokenDocument.containsKey("_data")).thenReturn(true);
+    when(resumeTokenDocument.get("_data")).thenReturn(new BsonString(resumeToken));
+    when(mongoChangeStreamCursor.getResumeToken()).thenReturn(resumeTokenDocument);
+    when(changeStreamIterable.cursor()).thenReturn(mongoChangeStreamCursor);
+    when(mongoClient.watch(BsonDocument.class)).thenReturn(changeStreamIterable);
 
-        final JsonNode initialState = mongodbDebeziumStateUtil.constructInitialDebeziumState(mongoClient,
-                database, replicaSet);
+    final JsonNode initialState = mongodbDebeziumStateUtil.constructInitialDebeziumState(mongoClient,
+        database, replicaSet);
 
-        assertNotNull(initialState);
-        assertEquals(1, initialState.size());
-        final BsonTimestamp timestamp = ResumeTokens.getTimestamp(resumeTokenDocument);
-        final JsonNode offsetState = initialState.fields().next().getValue();
-        assertEquals(resumeToken, offsetState.get(MongodbDebeziumConstants.OffsetState.VALUE_RESUME_TOKEN).asText());
-        assertEquals(timestamp.getTime(), offsetState.get(MongodbDebeziumConstants.OffsetState.VALUE_SECONDS).asInt());
-        assertEquals(timestamp.getInc(), offsetState.get(MongodbDebeziumConstants.OffsetState.VALUE_INCREMENT).asInt());
-        assertEquals("null", offsetState.get(MongodbDebeziumConstants.OffsetState.VALUE_TRANSACTION_ID).asText());
-    }
+    assertNotNull(initialState);
+    assertEquals(1, initialState.size());
+    final BsonTimestamp timestamp = ResumeTokens.getTimestamp(resumeTokenDocument);
+    final JsonNode offsetState = initialState.fields().next().getValue();
+    assertEquals(resumeToken, offsetState.get(MongodbDebeziumConstants.OffsetState.VALUE_RESUME_TOKEN).asText());
+    assertEquals(timestamp.getTime(), offsetState.get(MongodbDebeziumConstants.OffsetState.VALUE_SECONDS).asInt());
+    assertEquals(timestamp.getInc(), offsetState.get(MongodbDebeziumConstants.OffsetState.VALUE_INCREMENT).asInt());
+    assertEquals("null", offsetState.get(MongodbDebeziumConstants.OffsetState.VALUE_TRANSACTION_ID).asText());
+  }
+
 }
