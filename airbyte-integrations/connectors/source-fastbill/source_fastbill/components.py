af3801e369da1cd156d7ec0cc1e066360dc4566a
Aviraj Gour <asgour2021@gmail.com>
2023-08-23 21:24:07 +0530

    suggested change

--- airbyte-integrations/connectors/source-fastbill/source_fastbill/components.py
+++ airbyte-integrations/connectors/source-fastbill/source_fastbill/components.py
@@ -1,18 +1,18 @@
 from dataclasses import dataclass
 import base64 
 from airbyte_cdk.sources.declarative.auth.token import BasicHttpAuthenticator
-from airbyte_cdk.sources.declarative.types import Config
+from dataclasses import dataclass
 
 
 @dataclass
 class CustomAuthenticator(BasicHttpAuthenticator):
 
-    config: Config
+
     @property
     def token(self) -> str:
-
-        username = self.config['username'].encode("latin1")
-        password = self.config['api_key'].encode("latin1")
+        
+        username = self._username.eval(self.config).encode("latin1")
+        password = self._password.eval(self.config).encode("latin1")
         encoded_credentials = base64.b64encode(b":".join((username, password))).strip()
         token = "Basic " + encoded_credentials.decode("utf-8")
         return token
--- airbyte-integrations/connectors/source-fastbill/unit_tests/test_components.py
+++ airbyte-integrations/connectors/source-fastbill/unit_tests/test_components.py
@@ -8,7 +8,7 @@ from source_fastbill.components import CustomAuthenticator
 def test_token_generation():
 
     config= { "username": "example@gmail.com", "api_key": "api_key" }
-    authenticator = CustomAuthenticator(config=config,username="",parameters="")
+    authenticator = CustomAuthenticator(config=config,username="example@gmail.com",password="api_key",parameters="")
     token = authenticator.token
     expected_token = "Basic ZXhhbXBsZUBnbWFpbC5jb206YXBpX2tleQ=="
     assert expected_token == token
