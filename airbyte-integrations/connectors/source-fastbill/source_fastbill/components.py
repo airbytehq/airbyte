19f2677c318d488a9ff39b76f53d84e9d9e545f4
marcosmarxm <marcosmarxm@gmail.com>
2023-08-23 13:56:36 -0300

    fixes

--- airbyte-integrations/connectors/source-fastbill/source_fastbill/components.py
+++ airbyte-integrations/connectors/source-fastbill/source_fastbill/components.py
@@ -1,4 +1,3 @@
-from dataclasses import dataclass
 import base64 
 from airbyte_cdk.sources.declarative.auth.token import BasicHttpAuthenticator
 from dataclasses import dataclass
@@ -7,10 +6,9 @@ from dataclasses import dataclass
 @dataclass
 class CustomAuthenticator(BasicHttpAuthenticator):
 
-
     @property
     def token(self) -> str:
-        
+
         username = self._username.eval(self.config).encode("latin1")
         password = self._password.eval(self.config).encode("latin1")
         encoded_credentials = base64.b64encode(b":".join((username, password))).strip()
