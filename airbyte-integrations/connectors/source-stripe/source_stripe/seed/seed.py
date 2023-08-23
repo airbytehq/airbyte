8dc3ab8cdd9cd3f04b61eefcb90071a682e1b443
Alexandre Girard <alexandre@airbyte.io>
2023-08-23 16:06:26 -0700

    refactor

--- airbyte-integrations/connectors/source-stripe/source_stripe/seed/seed.py
+++ airbyte-integrations/connectors/source-stripe/source_stripe/seed/seed.py
@@ -1,13 +1,11 @@
 #
 # Copyright (c) 2023 Airbyte, Inc., all rights reserved.
 #
-import asyncio
+
 import concurrent
 import json
-import urllib
-from typing import Literal, Optional, Dict, Any
+from typing import Any, Dict, Optional
 
-import aiohttp
 import click
 import requests
 from pydantic import BaseModel, Field
@@ -37,33 +35,64 @@ class HttpRequest(BaseModel):
     headers: Dict[str, str]
     body: Optional[Dict[str, Any]]
 
-def prepare_create_customer(headers: Dict[str, str], customer: Customer):
-    url = "https://api.stripe.com/v1/customers"
-    customer_data = customer.dict(exclude_none=True)
-    customer_data.pop("bank_account")
-    return HttpRequest(method="POST", url=url, headers=headers, body=customer_data)
 
-def prepare_create_bank_account(headers: Dict[str, str], customer: Customer):
-    if customer.id is None:
-        raise ValueError(f"Cannot create bank account for uninitialized customer {customer}")
-    if customer.bank_account is None:
-        raise ValueError(f"Cannot create bank account for customer without bank account information: {customer}")
+class Requester:
+    def submit(self, request: HttpRequest):
+        method = requests.post if request.method == "POST" else requests.get
+        if request.body:
+            payload = recursive_url_encode(request.body)
+            response = method(request.url, headers=request.headers, data=payload)
+        else:
+            response = method(request.url, headers=request.headers)
+        return response.json()
 
-    url = f"https://api.stripe.com/v1/customers/{customer.id}/sources"
-    bank_account_data = {"source": customer.bank_account.dict(exclude_none=True)}
-    bank_account_data["source"]["account_holder_name"] = customer.name
-    return HttpRequest(method="POST", url=url, headers=headers, body=bank_account_data)
 
-def prepare_search_customer(headers: Dict[str, str], customer_name: str) -> HttpRequest:
-    url = f"https://api.stripe.com/v1/customers/search"
-    data = {"query": f"name: '{customer_name}'"}
-    return HttpRequest(method="GET", url=url, headers=headers, body=data)
+class StripeAPI:
+    def __init__(self, secret_key: str, requester: Requester):
+        self._authentication_header = {"Authorization": f"Bearer {secret_key}"}
+        self._requester = requester
+
+    def create_customer(self, customer):
+        request = self.prepare_create_customer(customer)
+        return self._requester.submit(request)
+
+    def create_bank_account(self, customer: Customer):
+        if customer.id is None:
+            raise ValueError(f"Cannot create bank account for uninitialized customer {customer}")
+        request = self.prepare_create_bank_account(customer)
+        return self._requester.submit(request)
+
+    def prepare_create_customer(self, customer: Customer):
+        url = "https://api.stripe.com/v1/customers"
+        customer_data = customer.dict(exclude_none=True)
+        customer_data.pop("bank_account")
+        return HttpRequest(method="POST", url=url, headers=self._authentication_header, body=customer_data)
+
+    def search_customer(self, customer_name):
+        request = self.prepare_search_customer(customer_name)
+        response = self._requester.submit(request)
+        return response.get("data", [])
+
+    def prepare_create_bank_account(self, customer: Customer):
+        if customer.id is None:
+            raise ValueError(f"Cannot create bank account for uninitialized customer {customer}")
+        if customer.bank_account is None:
+            raise ValueError(f"Cannot create bank account for customer without bank account information: {customer}")
+
+        url = f"https://api.stripe.com/v1/customers/{customer.id}/sources"
+        bank_account_data = {"source": customer.bank_account.dict(exclude_none=True)}
+        bank_account_data["source"]["account_holder_name"] = customer.name
+        return HttpRequest(method="POST", url=url, headers=self._authentication_header, body=bank_account_data)
+
+    def prepare_search_customer(self, customer_name: str) -> HttpRequest:
+        url = "https://api.stripe.com/v1/customers/search"
+        data = {"query": f"name: '{customer_name}'"}
+        return HttpRequest(method="GET", url=url, headers=self._authentication_header, body=data)
 
 
 @click.group()
 def _main():
     pass
-
 
 
 def kv_translation(d, line, final_str):
@@ -92,57 +121,19 @@ def recursive_url_encode(data, parent_key=None):
     return items
 
 
-def fetch(url, headers, json_payload, method=requests.post):
-    print(f"headers:{headers}")
-    if json_payload:
-        encoded_data = recursive_url_encode(json_payload)
-        print(f"encoded data: {encoded_data}")
-        return method(url, headers=headers, data=encoded_data).json()
-    else:
-        return method(url, headers=headers).json()
-
-def submit(request: HttpRequest):
-    method = requests.post if request.method == "POST" else requests.get
-    if request.body:
-        payload = recursive_url_encode(request.body)
-        response = method(request.url, headers=request.headers, data=payload)
-    else:
-        response = method(request.url, headers=request.headers)
-    return response.json()
-
-
-
-def create_customer(headers, customer):
-    request = prepare_create_customer(headers, customer)
-    return submit(request)
-
-
-def create_bank_account(headers, customer: Customer):
-    if customer.id is None:
-        raise ValueError(f"Cannot create bank account for uninitialized customer {customer}")
-    request = prepare_create_bank_account(headers, customer)
-    return submit(request)
-
-
-def create_customer_and_bank_account(headers, customer: Customer):
-    customer_output = create_customer(headers, customer)
+def create_customer_and_bank_account(customer: Customer, stripe_api: StripeAPI):
+    customer_output = stripe_api.create_customer(customer)
     if "id" in customer_output:
         customer.id = customer_output["id"]
     else:
         raise RuntimeError(f"Failed to created customer {customer}. Response: {customer_output}")
     print(f"created customer: {customer}")
     if customer.bank_account:
-        bank_account = create_bank_account(headers, customer)
+        bank_account = stripe_api.create_bank_account(customer)
         print(f"created bank account {bank_account}")
     else:
         bank_account = None
     return customer, bank_account
-
-
-def search_customer(customer_name, headers):
-    request = prepare_search_customer(headers, customer_name)
-    response = submit(request)
-    return response.get("data", [])
 
 
 def _load_config(path_to_config):
@@ -157,17 +148,18 @@ def generate_records(path_to_config, path_to_data):
     config = _load_config(path_to_config)
     records = _load_config(path_to_data)
     CONCURRENCY = 1
-    headers = {"Authorization": f"Bearer {config['client_secret']}"}
     futures = []
+    requester = Requester()
+    stripe_api = StripeAPI(config["client_secret"], requester)
     with concurrent.futures.ThreadPoolExecutor(max_workers=CONCURRENCY) as executor:
         for record in records:
             customer_data = record
             customer = Customer.parse_obj(customer_data)
-            customer_exists = is_customer_already_created(customer.name, headers)
+            customer_exists = is_customer_already_created(customer.name, stripe_api)
             if customer_exists:
                 print(f"Customer {customer.name} already exists. Skipping...")
             else:
-                f = executor.submit(create_customer_and_bank_account, headers, customer)
+                f = executor.submit(create_customer_and_bank_account, customer, stripe_api)
                 futures.append(f)
     concurrent.futures.wait(futures)
 
@@ -175,8 +167,8 @@ def generate_records(path_to_config, path_to_data):
         print(f"f: {f.result()}")
 
 
-def is_customer_already_created(customer_name, headers):
-    customer = search_customer(customer_name, headers)
+def is_customer_already_created(customer_name: str, stripe_api: StripeAPI):
+    customer = stripe_api.search_customer(customer_name)
     return len(customer) > 0
 
 
--- airbyte-integrations/connectors/source-stripe/unit_tests/test_seed.py
+++ airbyte-integrations/connectors/source-stripe/unit_tests/test_seed.py
@@ -1,131 +1,160 @@
-from source_stripe.seed.seed import Customer, BankAccount, prepare_create_customer, HttpRequest, prepare_create_bank_account, \
-    prepare_search_customer
+#
+# Copyright (c) 2023 Airbyte, Inc., all rights reserved.
+#
+from unittest import TestCase
+from unittest.mock import Mock
 
 import pytest
+from source_stripe.seed.seed import Customer, HttpRequest, StripeAPI
 
-def test_prepare_create_customer_request():
-    record = {
-        "description": "A fake customer",
-        "email": "fake@airbyte.io",
-        "name": "First Last",
-        "balance": "900001",
-        "bank_account": {
-            "object": "bank_account",
-            "country": "US",
-            "currency": "usd",
-            "account_holder_type": "individual",
-            "routing_number": "110000000",
-            "account_number": "000123456789"
+_MOCK_RESPONSE = {"key": "value"}
+_BEARER_TOKEN = "secret"
+_AUTHENTICATION_HEADER = {"Authorization": "Bearer secret"}
+
+
+class StripeApiTestCase(TestCase):
+    def setUp(self) -> None:
+        self._requester = Mock()
+        self._requester.submit.return_value = _MOCK_RESPONSE
+        self._stripe_api = StripeAPI("secret", self._requester)
+
+    def test_prepare_create_customer_request(self):
+        record = {
+            "description": "A fake customer",
+            "email": "fake@airbyte.io",
+            "name": "First Last",
+            "balance": "900001",
+            "bank_account": {
+                "object": "bank_account",
+                "country": "US",
+                "currency": "usd",
+                "account_holder_type": "individual",
+                "routing_number": "110000000",
+                "account_number": "000123456789",
+            },
         }
-    }
 
-    customer = Customer.parse_obj(record)
-    headers = {"header_key": "header_value"}
-    http_request = prepare_create_customer(headers, customer)
+        customer = Customer.parse_obj(record)
 
-    expected_request = HttpRequest(
-        method="POST",
-        url="https://api.stripe.com/v1/customers",
-        headers=headers,
-        body={
+        expected_request = HttpRequest(
+            method="POST",
+            url="https://api.stripe.com/v1/customers",
+            headers=_AUTHENTICATION_HEADER,
+            body={
+                "description": "A fake customer",
+                "email": "fake@airbyte.io",
+                "name": "First Last",
+                "balance": "900001",
+            },
+        )
+
+        response = self._stripe_api.create_customer(customer)
+
+        self._requester.submit.assert_called_with(expected_request)
+        assert response == _MOCK_RESPONSE
+
+    def test_prepare_create_bank_account_request(self):
+        record = {
+            "id": "12345",
+            "description": "A fake customer",
+            "email": "fake@airbyte.io",
+            "name": "First Last",
+            "balance": "900001",
+            "bank_account": {
+                "object": "bank_account",
+                "country": "US",
+                "currency": "usd",
+                "account_holder_type": "individual",
+                "routing_number": "110000000",
+                "account_number": "000123456789",
+            },
+        }
+        expected_request = HttpRequest(
+            method="POST",
+            url="https://api.stripe.com/v1/customers/12345/sources",
+            headers=_AUTHENTICATION_HEADER,
+            body={
+                "source": {
+                    "object": "bank_account",
+                    "country": "US",
+                    "currency": "usd",
+                    "account_holder_type": "individual",
+                    "account_holder_name": "First Last",
+                    "routing_number": "110000000",
+                    "account_number": "000123456789",
+                }
+            },
+        )
+
+        customer = Customer.parse_obj(record)
+        response = self._stripe_api.create_bank_account(customer)
+
+        self._requester.submit.assert_called_with(expected_request)
+        assert response == _MOCK_RESPONSE
+
+    def test_prepare_create_bank_account_request_fails_if_customer_has_no_bank_account(self):
+        record = {
+            "id": "12345",
             "description": "A fake customer",
             "email": "fake@airbyte.io",
             "name": "First Last",
             "balance": "900001",
         }
-    )
 
-    assert http_request == expected_request
+        customer = Customer.parse_obj(record)
+        with pytest.raises(ValueError):
+            self._stripe_api.create_bank_account(customer)
 
-def test_prepare_create_bank_account_request():
-    record = {
-        "id": "12345",
-        "description": "A fake customer",
-        "email": "fake@airbyte.io",
-        "name": "First Last",
-        "balance": "900001",
-        "bank_account": {
-            "object": "bank_account",
-            "country": "US",
-            "currency": "usd",
-            "account_holder_type": "individual",
-            "routing_number": "110000000",
-            "account_number": "000123456789"
-        }
-    }
-
-    customer = Customer.parse_obj(record)
-    headers = {"header_key": "header_value"}
-    http_request = prepare_create_bank_account(headers, customer)
-
-    expected_request = HttpRequest(
-        method="POST",
-        url="https://api.stripe.com/v1/customers/12345/sources",
-        headers=headers,
-        body={
-            "source": {
+    def test_prepare_create_bank_account_request_fails_if_user_has_no_id(self):
+        record = {
+            "description": "A fake customer",
+            "email": "fake@airbyte.io",
+            "name": "First Last",
+            "balance": "900001",
+            "bank_account": {
                 "object": "bank_account",
                 "country": "US",
                 "currency": "usd",
                 "account_holder_type": "individual",
-                "account_holder_name": "First Last",
                 "routing_number": "110000000",
-                "account_number": "000123456789"
-            }
+                "account_number": "000123456789",
+            },
         }
-    )
 
-    assert http_request == expected_request
+        customer = Customer.parse_obj(record)
+        with pytest.raises(ValueError):
+            self._stripe_api.create_bank_account(customer)
 
-def test_prepare_create_bank_account_request_fails_if_customer_has_no_bank_account():
-    record = {
-        "id": "12345",
-        "description": "A fake customer",
-        "email": "fake@airbyte.io",
-        "name": "First Last",
-        "balance": "900001",
-    }
+    def test_prepare_search_customer_request_no_customer_found(self):
+        customer_name = "First Last"
 
-    customer = Customer.parse_obj(record)
-    headers = {"header_key": "header_value"}
-    with pytest.raises(ValueError):
-        prepare_create_bank_account(headers, customer)
+        expected_request = HttpRequest(
+            method="GET",
+            url="https://api.stripe.com/v1/customers/search",
+            headers=_AUTHENTICATION_HEADER,
+            body={"query": "name: 'First Last'"},
+        )
 
-def test_prepare_create_bank_account_request_fails_if_user_has_no_id():
-    record = {
-        "description": "A fake customer",
-        "email": "fake@airbyte.io",
-        "name": "First Last",
-        "balance": "900001",
-        "bank_account": {
-            "object": "bank_account",
-            "country": "US",
-            "currency": "usd",
-            "account_holder_type": "individual",
-            "routing_number": "110000000",
-            "account_number": "000123456789"
-        }
-    }
+        self._requester.submit.return_value = {"data": []}
 
-    customer = Customer.parse_obj(record)
-    headers = {"header_key": "header_value"}
-    with pytest.raises(ValueError):
-        prepare_create_bank_account(headers, customer)
+        response = self._stripe_api.search_customer(customer_name)
 
-def test_prepare_search_customer_request():
-    customer_name = "First Last"
+        self._requester.submit.assert_called_with(expected_request)
+        assert response == []
 
-    headers = {"header_key": "header_value"}
+    def test_prepare_search_customer_request_found_a_customer(self):
+        customer_name = "First Last"
 
-    expected_request = HttpRequest(
-        method="GET",
-        url="https://api.stripe.com/v1/customers/search",
-        headers=headers,
-        body={
-            "query": "name: 'First Last'"
-        }
-    )
+        expected_request = HttpRequest(
+            method="GET",
+            url="https://api.stripe.com/v1/customers/search",
+            headers=_AUTHENTICATION_HEADER,
+            body={"query": "name: 'First Last'"},
+        )
 
-    http_request = prepare_search_customer(headers, customer_name)
+        self._requester.submit.return_value = {"data": [{"id": "123"}]}
 
-    assert http_request == expected_request
+        response = self._stripe_api.search_customer(customer_name)
+
+        self._requester.submit.assert_called_with(expected_request)
+        assert response == [{"id": "123"}]
