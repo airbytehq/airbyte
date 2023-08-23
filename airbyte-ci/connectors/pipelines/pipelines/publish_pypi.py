77083a21095831d60fdb0e9e29f29e2be743d19f
Pedro S. Lopez <pedroslopez@me.com>
2023-08-22 20:59:42 -0400

    simplify error handling

--- airbyte-ci/connectors/pipelines/pipelines/publish_pypi.py
+++ airbyte-ci/connectors/pipelines/pipelines/publish_pypi.py
@@ -7,7 +7,6 @@ from typing import List
 
 import anyio
 from connector_ops.utils import ConnectorLanguage
-from dagger import ExecError
 from pipelines.bases import ConnectorReport, Step, StepResult, StepStatus
 from pipelines.contexts import PublishConnectorContext
 from pipelines.pipelines import metadata
@@ -20,43 +19,36 @@ class PublishPyPIConnector(Step):
         if self.context.connector.language not in [ConnectorLanguage.PYTHON, ConnectorLanguage.LOW_CODE]:
             return self.skip("Only Python connectors can be published to PyPI.")
 
-        try:
-            setup_cfg = dedent(
-                f"""
-                [metadata]
-                name = airbyte-{self.context.connector.technical_name}
-                version = {self.context.connector.version}
-                author = Airbyte
-                author_email = contact@airbyte.io
-            """
-            )
+        setup_cfg = dedent(
+            f"""
+            [metadata]
+            name = airbyte-{self.context.connector.technical_name}
+            version = {self.context.connector.version}
+            author = Airbyte
+            author_email = contact@airbyte.io
+        """
+        )
 
-            twine_username = self.context.dagger_client.set_secret("twine_username", self.context.pypi_username)
-            twine_password = self.context.dagger_client.set_secret("twine_password", self.context.pypi_password)
+        twine_username = self.context.dagger_client.set_secret("twine_username", self.context.pypi_username)
+        twine_password = self.context.dagger_client.set_secret("twine_password", self.context.pypi_password)
 
-            pypi_stdout = await (
-                self.context.dagger_client.container()
-                .from_("python:3.10-slim")
-                .with_exec(["apt-get", "update"])
-                .with_exec(["apt-get", "install", "-y", "twine"])
-                .with_directory("connector", (await self.context.get_connector_dir(exclude=["dist"])))
-                .with_workdir("connector")
-                .with_exec(["sed", "-i", "/name=/d; /author=/d; /author_email=/d; /version=/d", "setup.py"])
-                .with_new_file("setup.cfg", setup_cfg)
-                .with_exec(["pip", "install", "--upgrade", "setuptools", "wheel"])
-                .with_exec(["python", "setup.py", "sdist", "bdist_wheel"])
-                .with_secret_variable("TWINE_USERNAME", twine_username)
-                .with_secret_variable("TWINE_PASSWROD", twine_password)
-                .with_exec(["twine", "upload", "--verbose", "--repository", self.context.pypi_repository, "dist/*"])
-            ).stdout()
+        twine_upload = await (
+            self.context.dagger_client.container()
+            .from_("python:3.10-slim")
+            .with_exec(["apt-get", "update"])
+            .with_exec(["apt-get", "install", "-y", "twine"])
+            .with_directory("connector", (await self.context.get_connector_dir(exclude=["dist"])))
+            .with_workdir("connector")
+            .with_exec(["sed", "-i", "/name=/d; /author=/d; /author_email=/d; /version=/d", "setup.py"])
+            .with_new_file("setup.cfg", setup_cfg)
+            .with_exec(["pip", "install", "--upgrade", "setuptools", "wheel"])
+            .with_exec(["python", "setup.py", "sdist", "bdist_wheel"])
+            .with_secret_variable("TWINE_USERNAME", twine_username)
+            .with_secret_variable("TWINE_PASSWROD", twine_password)
+            .with_exec(["twine", "upload", "--verbose", "--repository", self.context.pypi_repository, "dist/*"])
+        )
 
-            return StepResult(
-                self,
-                status=StepStatus.SUCCESS,
-                stdout=pypi_stdout,
-            )
-        except ExecError as e:
-            return StepResult(self, status=StepStatus.FAILURE, stderr=str(e))
+        return self.get_step_result(twine_upload)
 
 
 async def run_connector_pypi_publish_pipeline(context: PublishConnectorContext, semaphore: anyio.Semaphore) -> ConnectorReport:
