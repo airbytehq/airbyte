a8ee2b520485e381d826e3c620dcb204e8ef9b80
George Manning <gmanning@live.co.uk>
2023-08-25 00:21:14 +0100

    fix new unit tests from rebase

--- airbyte-integrations/connectors/destination-typesense/unit_tests/unit_test.py
+++ airbyte-integrations/connectors/destination-typesense/unit_tests/unit_test.py
@@ -9,19 +9,19 @@ from destination_typesense.writer import TypesenseWriter
 
 @patch("typesense.Client")
 def test_default_batch_size(client):
-    writer = TypesenseWriter(client, "steam_name")
+    writer = TypesenseWriter(client)
     assert writer.batch_size == 10000
 
 
 @patch("typesense.Client")
 def test_empty_batch_size(client):
-    writer = TypesenseWriter(client, "steam_name", "")
+    writer = TypesenseWriter(client, "")
     assert writer.batch_size == 10000
 
 
 @patch("typesense.Client")
 def test_custom_batch_size(client):
-    writer = TypesenseWriter(client, "steam_name", 9000)
+    writer = TypesenseWriter(client, 9000)
     assert writer.batch_size == 9000
 
 
