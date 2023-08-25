e6755444acb7261b23de1f0d8928b55c3cb6ab20
tomasspangelo <tomas@spangelo.com>
2023-08-25 14:58:10 +0200

    Added MonthlyStatus stream

--- airbyte-integrations/connectors/source-tripletex-api/source_tripletex_api/source.py
+++ airbyte-integrations/connectors/source-tripletex-api/source_tripletex_api/source.py
@@ -11,7 +11,8 @@ import pendulum
 import requests
 from airbyte_cdk.sources import AbstractSource
 from airbyte_cdk.sources.streams import Stream
-from airbyte_cdk.sources.streams.http import HttpStream
+from airbyte_cdk.sources.streams.http import HttpStream, HttpSubStream
+from airbyte_cdk.models import SyncMode
 
 
 class TripletexApiStream(HttpStream, ABC):
@@ -196,6 +197,10 @@ class SalaryType(TripletexPaginationStream):
 class Employee(TripletexApiStream):
     primary_key = "id"
 
+    @property
+    def use_cache(self) -> bool:
+        return True
+
     def next_page_token(self, response: requests.Response) -> Optional[Mapping[str, Any]]:
         return None
 
@@ -214,6 +219,51 @@ class BalanceSheet(DateRequiredStream):
         return "balanceSheet"
 
 
+class MonthlyStatus(TripletexPaginationStream):
+    primary_key = "id"
+
+    def __init__(self, parent: Employee, config: Mapping[str, Any]):
+        super().__init__(config=config)
+        self.parent = parent
+        self.lookback = config.get("lookback_monthly_status", 0)
+
+    def path(
+        self, stream_state: Mapping[str, Any] = None, stream_slice: Mapping[str, Any] = None, next_page_token: Mapping[str, Any] = None
+    ) -> str:
+        return "timesheet/month/byMonthNumber"
+
+    def request_params(
+        self, stream_state: Mapping[str, Any], stream_slice: Mapping[str, any] = None, next_page_token: Mapping[str, Any] = None
+    ) -> MutableMapping[str, Any]:
+        params = super().request_params(stream_state, stream_slice, next_page_token)
+        params.update({"monthYear": stream_slice.get("monthYear"),
+                       "employeeIds": stream_slice.get("parent", {}).get("id")})
+        return params
+
+    def _month_slices(self):
+        current_month = pendulum.today().start_of("month")
+        return [current_month.subtract(months=i).format("YYYY-MM") for i in range(self.lookback+1)]
+
+    def stream_slices(
+        self, *, sync_mode: SyncMode, cursor_field: List[str] = None, stream_state: Mapping[str, Any] = None
+    ) -> Iterable[Optional[Mapping[str, Any]]]:
+        parent_stream_slices = self.parent.stream_slices(
+            sync_mode=SyncMode.full_refresh, cursor_field=cursor_field, stream_state=stream_state
+        )
+
+        # iterate over all parent stream_slices
+        for stream_slice in parent_stream_slices:
+            parent_records = self.parent.read_records(
+                sync_mode=SyncMode.full_refresh, cursor_field=cursor_field, stream_slice=stream_slice, stream_state=stream_state
+            )
+
+            # iterate over all parent records with current stream_slice
+            months = self._month_slices()
+            for record in parent_records:
+                for month in months:
+                    yield {"parent": record, "monthYear": month}
+
+
 class SourceTripletexApi(AbstractSource):
     def check_connection(self, logger, config) -> Tuple[bool, any]:
         """
@@ -228,5 +278,8 @@ class SourceTripletexApi(AbstractSource):
         :param config: A Mapping of the user input configuration as defined in the connector spec.
         """
 
+        employee_stream = Employee(config=config)
         return [Postings(config=config), Departments(config=config), Accounts(config=config), BalanceSheet(config=config),
-                Employee(config=config), Invoice(config=config), Payslip(config=config), SalaryType(config=config)]
+                employee_stream, Invoice(config=config), Payslip(
+                    config=config), SalaryType(config=config),
+                MonthlyStatus(parent=employee_stream, config=config)]
