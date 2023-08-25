a0b4a2a2bd13708928f59db91b37060425b8e249
Aaron Steers <aj@airbyte.io>
2023-08-25 23:31:06 +0000

    chore: mark setup.py for future removal

--- airbyte-integrations/connectors/destination-duckdb/setup.py
+++ airbyte-integrations/connectors/destination-duckdb/setup.py
@@ -2,6 +2,7 @@
 # Copyright (c) 2023 Airbyte, Inc., all rights reserved.
 #
 
+# TODO: Deprecate this setup.py in favor of the pyproject.toml file config (Poetry).
 
 from setuptools import find_packages, setup
 
@@ -12,7 +13,7 @@ TEST_REQUIREMENTS = ["pytest~=6.1"]
 setup(
     name="destination_duckdb",
     description="Destination implementation for Duckdb.",
-    author="Simon Späti",
+    author="Simon Späti, Airbyte",
     author_email="contact@airbyte.io",
     packages=find_packages(),
     install_requires=MAIN_REQUIREMENTS,
