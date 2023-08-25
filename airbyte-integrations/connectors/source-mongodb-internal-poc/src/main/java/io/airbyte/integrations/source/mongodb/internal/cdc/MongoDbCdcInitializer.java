952c2bd56fd104cf8d5e8798b1a1fcd0af958f39
jdpgrailsdev <jpearlin1@gmail.com>
2023-08-25 15:46:14 -0400

    WIP add fetcher

--- airbyte-integrations/connectors/source-mongodb-internal-poc/src/main/java/io/airbyte/integrations/source/mongodb/internal/cdc/MongoDbCdcInitializer.java
+++ airbyte-integrations/connectors/source-mongodb-internal-poc/src/main/java/io/airbyte/integrations/source/mongodb/internal/cdc/MongoDbCdcInitializer.java
@@ -4,16 +4,28 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.mongodb.client.MongoClient;
 import io.airbyte.commons.json.Jsons;
 import io.airbyte.commons.util.AutoCloseableIterator;
+import io.airbyte.integrations.debezium.AirbyteDebeziumHandler;
+import io.airbyte.integrations.debezium.CdcMetadataInjector;
+import io.airbyte.integrations.debezium.CdcSavedInfoFetcher;
+import io.airbyte.integrations.debezium.CdcStateHandler;
+import io.airbyte.integrations.debezium.internals.mongodb.MongoDbCdcTargetPosition;
 import io.airbyte.integrations.debezium.internals.mongodb.MongoDbDebeziumStateUtil;
+import io.airbyte.integrations.debezium.internals.postgres.PostgresCdcTargetPosition;
 import io.airbyte.integrations.source.mongodb.internal.state.MongoDbStateManager;
 import io.airbyte.protocol.models.v0.AirbyteMessage;
 import io.airbyte.protocol.models.v0.ConfiguredAirbyteCatalog;
+import org.bson.BsonTimestamp;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.time.Duration;
 import java.time.Instant;
+import java.util.Collections;
 import java.util.List;
+import java.util.OptionalInt;
 import java.util.OptionalLong;
+import java.util.Properties;
+import java.util.function.Supplier;
 
 import static io.airbyte.integrations.source.mongodb.internal.MongoConstants.DATABASE_CONFIGURATION_KEY;
 import static io.airbyte.integrations.source.mongodb.internal.MongoConstants.REPLICA_SET_CONFIGURATION_KEY;
@@ -36,10 +48,18 @@ public class MongoDbCdcInitializer {
             final JsonNode config
             ) {
 
+        final Properties defaultDebeziumProperties = MongoDbCdcProperties.getDebeziumDefaultProperties();
         final String databaseName = config.get(DATABASE_CONFIGURATION_KEY).asText();
         final String replicaSet = config.get(REPLICA_SET_CONFIGURATION_KEY).asText();
-        final MongoDbCdcState cdcState = getCdcState(mongoClient, stateManager, databaseName, replicaSet);
-        final OptionalLong savedOffset = mongoDbDebeziumStateUtil.savedOffset(MongoDbCdcProperties.getDebeziumProperties(), catalog, Jsons.jsonNode(cdcState), config);
+        final JsonNode initialDebeziumState = mongoDbDebeziumStateUtil.constructInitialDebeziumState(mongoClient, databaseName, replicaSet);
+        final JsonNode cdcState = (stateManager.getCdcState() == null || stateManager.getCdcState().state() == null) ? initialDebeziumState :
+                Jsons.clone(stateManager.getCdcState().state());
+        final OptionalLong savedOffset = mongoDbDebeziumStateUtil.savedOffset(
+                Jsons.clone(defaultDebeziumProperties),
+                catalog,
+                cdcState,
+                config
+        );
 
         // We should always be able to extract offset out of state if it's not null
         if (cdcState != null && savedOffset.isEmpty()) {
@@ -53,11 +73,36 @@ public class MongoDbCdcInitializer {
             LOGGER.warn("Saved offset is before most recent resume token. Airbyte will trigger a full refresh.");
         }
 
-        final MongoDbCdcState stateToBeUsed = (!savedOffsetAfterResumeToken || stateManager.getCdcState() == null) ? cdcState : stateManager.getCdcState();
+        final MongoDbCdcState stateToBeUsed = (!savedOffsetAfterResumeToken || stateManager.getCdcState() == null || stateManager.getCdcState().state() == null) ? new MongoDbCdcState(initialDebeziumState) : stateManager.getCdcState();
+
+        // TODO get iterators for streams
+
+        final Duration firstRecordWaitTime = Duration.ofMinutes(5); // TODO get from connector config?
+        final OptionalInt queueSize = OptionalInt.empty(); // TODO get from connector config?
+        final AirbyteDebeziumHandler<BsonTimestamp> handler = new AirbyteDebeziumHandler<>(config,
+                MongoDbCdcTargetPosition.targetPosition(mongoClient), false, firstRecordWaitTime, queueSize);
+        final MongoDbCdcStateHandler mongoDbCdcStateHandler = new MongoDbCdcStateHandler(stateManager);
+        final MongoDbCdcConnectorMetadataInjector cdcMetadataInjector = new MongoDbCdcConnectorMetadataInjector();
+        final MongoDbCdcSavedInfoFetcher cdcSavedInfoFetcher = new MongoDbCdcSavedInfoFetcher(stateToBeUsed);
+
+        final Supplier<AutoCloseableIterator<AirbyteMessage>> incrementalIteratorSupplier = () -> handler.getIncrementalIterators(catalog,
+                cdcSavedInfoFetcher,
+                mongoDbCdcStateHandler,
+                cdcMetadataInjector,
+                defaultDebeziumProperties,
+                emittedAt,
+                false);
+
+
+        return null;
     }
 
     private MongoDbCdcState getCdcState(final MongoClient mongoClient, final MongoDbStateManager stateManager, final String databaseName, final String replicaSet) {
-        final JsonNode initialDebeziumState = mongoDbDebeziumStateUtil.constructInitialDebeziumState(mongoClient, databaseName, replicaSet);
-        return (stateManager.getCdcState() == null) ? Jsons.object(initialDebeziumState, MongoDbCdcState.class) : stateManager.getCdcState();
+        if (stateManager.getCdcState() != null) {
+            return stateManager.getCdcState();
+        } else {
+            final JsonNode initialDebeziumState = mongoDbDebeziumStateUtil.constructInitialDebeziumState(mongoClient, databaseName, replicaSet);
+            return Jsons.object(initialDebeziumState, MongoDbCdcState.class);
+        }
     }
 }
