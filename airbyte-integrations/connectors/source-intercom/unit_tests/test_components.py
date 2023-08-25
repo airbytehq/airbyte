2d736a3a6704c82c358c6d8037022991a3b4f9a1
Arsen Losenko <20901439+arsenlosenko@users.noreply.github.com>
2023-08-25 16:51:32 +0300

    Update tests

--- airbyte-integrations/connectors/source-intercom/unit_tests/test_components.py
+++ airbyte-integrations/connectors/source-intercom/unit_tests/test_components.py
@@ -10,7 +10,6 @@ from airbyte_cdk.sources.streams import Stream
 from source_intercom.components import IncrementalSingleSliceCursor, IncrementalSubstreamSlicerCursor
 
 
-
 def test_slicer():
     date_time_dict = {"updated_at": 1662459010}
     slicer = IncrementalSingleSliceCursor(config={}, parameters={}, cursor_field="updated_at")
@@ -25,32 +24,32 @@ def test_slicer():
     "last_record, expected, records",
     [
         (
-                {"first_stream_cursor": 1662459010},
-                {'parent_stream_name': {'parent_cursor_field': 1662459010}, 'first_stream_cursor': 1662459010},
-                [{"first_stream_cursor": 1662459010}],
-        ),
-        (None, {}, []),
+            {"first_stream_cursor": 1662459010},
+            {'first_stream_cursor': 1662459010, 'prior_state': {'first_stream_cursor': 1662459010, 'parent_stream_name': {'parent_cursor_field': 1662459010}}, 'parent_stream_name': {'parent_cursor_field': 1662459010}},
+            [{"first_stream_cursor": 1662459010}],
+        )
     ],
 )
 def test_sub_slicer(last_record, expected, records):
     parent_stream = Mock(spec=Stream)
-    parent_stream.state = {}
     parent_stream.name = "parent_stream_name"
     parent_stream.cursor_field = "parent_cursor_field"
+    parent_stream.state = {"parent_stream_name": {"parent_cursor_field": 1662459010}}
     parent_stream.stream_slices.return_value = [{"a slice": "value"}]
     parent_stream.read_records = MagicMock(return_value=records)
 
     parent_config = ParentStreamConfig(
         stream=parent_stream,
-        parent_key="id",
+        parent_key="first_stream_cursor",
         partition_field="first_stream_id",
         parameters={},
         config={},
     )
 
     slicer = IncrementalSubstreamSlicerCursor(
-        config={}, parameters={}, cursor_field="first_stream_cursor", parent_stream_configs=[parent_config]
+        config={}, parameters={}, cursor_field="first_stream_cursor", parent_stream_configs=[parent_config], parent_complete_fetch=True
     )
+    slicer.set_initial_state(expected)
     stream_slice = next(slicer.stream_slices()) if records else {}
     slicer.close_slice(stream_slice, last_record)
     assert slicer.get_stream_state() == expected
