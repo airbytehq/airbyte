b47596cd57c5b871a871240c473af6dcd6369c10
Ben Church <ben@airbyte.io>
2023-08-25 10:28:00 -0700

    Airbyte-ci: do not fail pipeline on slack outage (#29822)

--- airbyte-ci/connectors/pipelines/pipelines/slack.py
+++ airbyte-ci/connectors/pipelines/pipelines/slack.py
@@ -3,12 +3,17 @@
 #
 
 import json
-
 import requests
+
+from pipelines import main_logger
 
 
 def send_message_to_webhook(message: str, channel: str, webhook: str) -> dict:
     payload = {"channel": f"#{channel}", "username": "Connectors CI/CD Bot", "text": message}
     response = requests.post(webhook, data={"payload": json.dumps(payload)})
-    response.raise_for_status()
+
+    # log if the request failed, but don't fail the pipeline
+    if not response.ok:
+        main_logger.error(f"Failed to send message to slack webhook: {response.text}")
+
     return response
