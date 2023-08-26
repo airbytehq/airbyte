1529d099a8389c1e7fee46258c620133c7da22d1
prateekmukhedkar <prateekmukhedkar@users.noreply.github.com>
2023-08-26 00:47:01 +0000

    Automated Commit - Formatting Changes

--- airbyte-integrations/connectors/source-mysql/src/test-integration/java/io/airbyte/integrations/io/airbyte/integration_tests/sources/MySqlDatatypeAccuracyTest.java
+++ airbyte-integrations/connectors/source-mysql/src/test-integration/java/io/airbyte/integrations/io/airbyte/integration_tests/sources/MySqlDatatypeAccuracyTest.java
@@ -19,7 +19,6 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
-import java.util.Random;
 import org.jooq.SQLDialect;
 import org.testcontainers.containers.MySQLContainer;
 
