257b83d5736f0dddf2715b7c7ff404adb0e87183
Artem Inzhyyants <artem.inzhyyants@gmail.com>
2023-08-28 12:59:18 +0200

    Zendesk Support: fix unit tests

--- airbyte-integrations/connectors/source-zendesk-support/unit_tests/unit_test.py
+++ airbyte-integrations/connectors/source-zendesk-support/unit_tests/unit_test.py
@@ -144,18 +144,18 @@ def test_check(response, start_date, check_passed):
 @pytest.mark.parametrize(
     "ticket_forms_response, status_code, expected_n_streams, expected_warnings, reason",
     [
-        ('{"ticket_forms": [{"id": 1, "updated_at": "2021-07-08T00:05:45Z"}]}', 200, 28, [], None),
+        ('{"ticket_forms": [{"id": 1, "updated_at": "2021-07-08T00:05:45Z"}]}', 200, 29, [], None),
         (
                 '{"error": "Not sufficient permissions"}',
                 403,
-                25,
+                26,
                 ["Skipping stream ticket_forms: Check permissions, error message: Not sufficient permissions."],
                 None
         ),
         (
                 '',
                 404,
-                25,
+                26,
                 ["Skipping stream ticket_forms: Check permissions, error message: {'title': 'Not Found', 'message': 'Received empty JSON response'}."],
                 'Not Found'
         ),
@@ -574,7 +574,7 @@ class TestSourceZendeskSupportFullRefreshStream:
             (Brands, {"page[size]": 100}),
             (CustomRoles, {}),
             (Schedules, {"page[size]": 100}),
-            (UserSettingsStream, {}),
+            (UserSettingsStream, {"page[size]": 100}),
             (AccountAttributes, {}),
             (AttributeDefinitions, {}),
         ],
