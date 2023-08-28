13ef239cccb40202a24455f306a455ec155e650c
Jonathan Pearlin <jonathan@airbyte.io>
2023-08-28 15:01:32 -0400

    ✨ Source MongoDB Internal POC: CDC Metadata Injector (#29730)
    Co-authored-by: jdpgrailsdev <jdpgrailsdev@users.noreply.github.com>
    Co-authored-by: Jose Pefaur <jose.pefaur@gmail.com>
    Co-authored-by: subodh <subodh1810@gmail.com>

--- airbyte-integrations/connectors/source-mongodb-internal-poc/src/main/java/io/airbyte/integrations/source/mongodb/internal/state/MongoDbStreamState.java
+++ airbyte-integrations/connectors/source-mongodb-internal-poc/src/main/java/io/airbyte/integrations/source/mongodb/internal/state/MongoDbStreamState.java
@@ -2,11 +2,11 @@
  * Copyright (c) 2023 Airbyte, Inc., all rights reserved.
  */
 
-package io.airbyte.integrations.source.mongodb.internal;
+package io.airbyte.integrations.source.mongodb.internal.state;
 
 /*
  * TODO replace `isObjectId` with _id enum (ObjectId, String, etc.)
  */
-public record MongodbStreamState(String id) { // , boolean isObjectId) {
+public record MongoDbStreamState(String id) { // , boolean isObjectId) {
 
 }
