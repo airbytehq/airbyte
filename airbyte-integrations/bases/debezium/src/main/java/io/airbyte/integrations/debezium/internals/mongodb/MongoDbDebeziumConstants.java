0724132a1abe56e49e40faa39e73d1adbbec69ba
Jonathan Pearlin <jonathan@airbyte.io>
2023-08-28 09:44:39 -0400

    ✨ Source MongoDB Internal POC: Debezium Configuration Properties (#29727)

--- airbyte-integrations/bases/debezium/src/main/java/io/airbyte/integrations/debezium/internals/mongodb/MongoDbDebeziumConstants.java
+++ airbyte-integrations/bases/debezium/src/main/java/io/airbyte/integrations/debezium/internals/mongodb/MongoDbDebeziumConstants.java
@@ -17,14 +17,25 @@ public class MongoDbDebeziumConstants {
   public static class ChangeEvent {
 
     public static final String SOURCE = "source";
-
     public static final String SOURCE_ORDER = SourceInfo.ORDER;
-
     public static final String SOURCE_RESUME_TOKEN = "resume_token";
-
     public static final String SOURCE_SECONDS = SourceInfo.TIMESTAMP;
-
     public static final String SOURCE_TIMESTAMP_MS = "ts_ms";
+
+  }
+
+  /**
+   * Constants for the configuration of the MongoDB connector. These constants represent the
+   * configuration values that are to be mapped to the Debezium configuration.
+   */
+  public static class Configuration {
+
+    public static final String AUTH_SOURCE_CONFIGURATION_KEY = "auth_source";
+    public static final String CONNECTION_STRING_CONFIGURATION_KEY = "connection_string";
+    public static final String DATABASE_CONFIGURATION_KEY = "database";
+    public static final String PASSWORD_CONFIGURATION_KEY = "password";
+    public static final String REPLICA_SET_CONFIGURATION_KEY = "replica_set";
+    public static final String USER_CONFIGURATION_KEY = "user";
 
   }
 
@@ -34,17 +45,14 @@ public class MongoDbDebeziumConstants {
   public static class OffsetState {
 
     public static final String KEY_REPLICA_SET = SourceInfo.REPLICA_SET_NAME;
-
     public static final String KEY_SERVER_ID = SourceInfo.SERVER_ID_KEY;
-
     public static final String VALUE_INCREMENT = SourceInfo.ORDER;
-
     public static final String VALUE_RESUME_TOKEN = "resume_token";
-
     public static final String VALUE_SECONDS = SourceInfo.TIMESTAMP;
-
     public static final String VALUE_TRANSACTION_ID = "transaction_id";
 
   }
+
+  private MongoDbDebeziumConstants() {}
 
 }
