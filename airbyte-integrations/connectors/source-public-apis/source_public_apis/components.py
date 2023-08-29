210fd9bec92da78c57fd688f63c7d88ccab78919
marcosmarxm <marcosmarxm@gmail.com>
2023-08-29 11:56:15 -0300

    fix public apis

--- airbyte-integrations/connectors/source-public-apis/source_public_apis/components.py
+++ airbyte-integrations/connectors/source-public-apis/source_public_apis/components.py
@@ -1,12 +1,14 @@
-from typing import Any, Iterable, Mapping
-from airbyte_cdk.sources.streams.http import HttpStream
-from airbyte_cdk.sources.declarative.extractors.record_extractor import RecordExtractor
-from typing import Any, List
+#
+# Copyright (c) 2023 Airbyte, Inc., all rights reserved.
+#
+
+from typing import Any, List, Mapping
+
 import requests
+from airbyte_cdk.sources.declarative.extractors.record_extractor import RecordExtractor
 
 
 class CustomExtractor(RecordExtractor):
-    
     def extract_records(self, response: requests.Response, **kwargs) -> List[Mapping[str, Any]]:
-        
+
         return [{"name": cat} for cat in response.json()["categories"]]
