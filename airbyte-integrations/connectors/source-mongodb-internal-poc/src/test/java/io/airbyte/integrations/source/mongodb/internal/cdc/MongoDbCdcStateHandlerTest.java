ce678607789b36f58a17f00e16d7c99c2f7a836e
jdpgrailsdev <jpearlin1@gmail.com>
2023-08-29 10:08:59 -0400

    More tests

--- airbyte-integrations/connectors/source-mongodb-internal-poc/src/test/java/io/airbyte/integrations/source/mongodb/internal/cdc/MongoDbCdcStateHandlerTest.java
+++ airbyte-integrations/connectors/source-mongodb-internal-poc/src/test/java/io/airbyte/integrations/source/mongodb/internal/cdc/MongoDbCdcStateHandlerTest.java
@@ -2,17 +2,17 @@
  * Copyright (c) 2023 Airbyte, Inc., all rights reserved.
  */
 
-package io.airbyte.integrations.source.mongodb.internal.state;
+package io.airbyte.integrations.source.mongodb.internal.cdc;
 
-import static io.airbyte.integrations.debezium.internals.mongodb.MongoDbDebeziumConstants.ChangeEvent.SOURCE_ORDER;
-import static io.airbyte.integrations.debezium.internals.mongodb.MongoDbDebeziumConstants.ChangeEvent.SOURCE_RESUME_TOKEN;
-import static io.airbyte.integrations.debezium.internals.mongodb.MongoDbDebeziumConstants.ChangeEvent.SOURCE_SECONDS;
 import static io.airbyte.protocol.models.v0.AirbyteStateMessage.AirbyteStateType.GLOBAL;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import io.airbyte.integrations.debezium.internals.mongodb.MongoDbDebeziumStateUtil;
 import io.airbyte.integrations.source.mongodb.internal.cdc.MongoDbCdcState;
 import io.airbyte.integrations.source.mongodb.internal.cdc.MongoDbCdcStateHandler;
+import io.airbyte.integrations.source.mongodb.internal.state.MongoDbStateManager;
 import io.airbyte.protocol.models.Jsons;
 import io.airbyte.protocol.models.v0.AirbyteMessage;
 import java.util.Map;
@@ -21,6 +21,8 @@ import org.junit.jupiter.api.Test;
 
 class MongoDbCdcStateHandlerTest {
 
+  private static final String DATABASE = "test-database";
+  private static final String REPLICA_SET = "test-replica-set";
   private static final String RESUME_TOKEN = "8264BEB9F3000000012B0229296E04";
 
   private MongoDbStateManager mongoDbStateManager;
@@ -35,12 +37,8 @@ class MongoDbCdcStateHandlerTest {
 
   @Test
   void testSavingState() {
-    final int seconds = 1234567;
-    final int order = 1;
-    final Map<String, String> offset = Map.of(SOURCE_SECONDS, String.valueOf(seconds),
-        SOURCE_ORDER, String.valueOf(order),
-        SOURCE_RESUME_TOKEN, RESUME_TOKEN);
-
+    final Map<String, String> offset =
+        Jsons.object(MongoDbDebeziumStateUtil.formatState(DATABASE, REPLICA_SET, RESUME_TOKEN), new TypeReference<>() {});
     final AirbyteMessage airbyteMessage = mongoDbCdcStateHandler.saveState(offset, "");
 
     assertNotNull(airbyteMessage);
