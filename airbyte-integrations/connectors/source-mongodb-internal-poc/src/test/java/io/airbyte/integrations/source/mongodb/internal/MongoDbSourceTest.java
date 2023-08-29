2a0d363c28053b7dae2f1172a2c250c1d839d514
Jose Pefaur <jose.pefaur@gmail.com>
2023-08-28 20:45:23 -0500

    move existing tests into nested class in preparation for more tests to be added

--- airbyte-integrations/connectors/source-mongodb-internal-poc/src/test/java/io/airbyte/integrations/source/mongodb/internal/MongoDbSourceTest.java
+++ airbyte-integrations/connectors/source-mongodb-internal-poc/src/test/java/io/airbyte/integrations/source/mongodb/internal/MongoDbSourceTest.java
@@ -38,171 +38,171 @@ import java.util.Optional;
 import java.util.stream.Collectors;
 import org.bson.Document;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 class MongoDbSourceTest {
 
   private static final String DB_NAME = "airbyte_test";
 
-  private JsonNode airbyteSourceConfig;
-  private MongoClient mongoClient;
-  private MongoDbSource source;
+  @Nested
+  @DisplayName("Tests with mocked database")
+  class MockedDatabaseTests {
 
-  @BeforeEach
-  void setup() {
-    airbyteSourceConfig = createConfiguration(Optional.empty(), Optional.empty());
-    mongoClient = mock(MongoClient.class);
-    source = spy(new MongoDbSource());
-    doReturn(mongoClient).when(source).createMongoClient(airbyteSourceConfig);
+    private JsonNode airbyteSourceConfig;
+    private MongoClient mongoClient;
+    private MongoDbSource source;
+
+    @BeforeEach
+    void setup() {
+      airbyteSourceConfig = createConfiguration("mongodb://localhost:27017/", Optional.of("replica-set"), Optional.empty(), Optional.empty());
+      mongoClient = mock(MongoClient.class);
+      source = spy(new MongoDbSource());
+      doReturn(mongoClient).when(source).createMongoClient(airbyteSourceConfig);
+    }
+
+    @Test
+    void testCheckOperation() throws IOException {
+      final ClusterDescription clusterDescription = mock(ClusterDescription.class);
+      final Document response = Document.parse(MoreResources.readResource("authorized_collections_response.json"));
+      final MongoDatabase mongoDatabase = mock(MongoDatabase.class);
+
+      when(clusterDescription.getType()).thenReturn(ClusterType.REPLICA_SET);
+      when(mongoDatabase.runCommand(any())).thenReturn(response);
+      when(mongoClient.getDatabase(any())).thenReturn(mongoDatabase);
+      when(mongoClient.getClusterDescription()).thenReturn(clusterDescription);
+
+      final AirbyteConnectionStatus airbyteConnectionStatus = source.check(airbyteSourceConfig);
+      assertNotNull(airbyteConnectionStatus);
+      assertEquals(AirbyteConnectionStatus.Status.SUCCEEDED, airbyteConnectionStatus.getStatus());
+    }
+
+    @Test
+    void testCheckOperationNoAuthorizedCollections() throws IOException {
+      final ClusterDescription clusterDescription = mock(ClusterDescription.class);
+      final Document response = Document.parse(MoreResources.readResource("no_authorized_collections_response.json"));
+      final MongoDatabase mongoDatabase = mock(MongoDatabase.class);
+
+      when(clusterDescription.getType()).thenReturn(ClusterType.REPLICA_SET);
+      when(mongoDatabase.runCommand(any())).thenReturn(response);
+      when(mongoClient.getDatabase(any())).thenReturn(mongoDatabase);
+      when(mongoClient.getClusterDescription()).thenReturn(clusterDescription);
+
+      final AirbyteConnectionStatus airbyteConnectionStatus = source.check(airbyteSourceConfig);
+      assertNotNull(airbyteConnectionStatus);
+      assertEquals(AirbyteConnectionStatus.Status.FAILED, airbyteConnectionStatus.getStatus());
+      assertEquals("Target MongoDB database does not contain any authorized collections.", airbyteConnectionStatus.getMessage());
+    }
+
+    @Test
+    void testCheckOperationInvalidClusterType() throws IOException {
+      final ClusterDescription clusterDescription = mock(ClusterDescription.class);
+      final Document response = Document.parse(MoreResources.readResource("authorized_collections_response.json"));
+      final MongoDatabase mongoDatabase = mock(MongoDatabase.class);
+
+      when(clusterDescription.getType()).thenReturn(ClusterType.STANDALONE);
+      when(mongoDatabase.runCommand(any())).thenReturn(response);
+      when(mongoClient.getDatabase(any())).thenReturn(mongoDatabase);
+      when(mongoClient.getClusterDescription()).thenReturn(clusterDescription);
+
+      final AirbyteConnectionStatus airbyteConnectionStatus = source.check(airbyteSourceConfig);
+      assertNotNull(airbyteConnectionStatus);
+      assertEquals(AirbyteConnectionStatus.Status.FAILED, airbyteConnectionStatus.getStatus());
+      assertEquals("Target MongoDB instance is not a replica set cluster.", airbyteConnectionStatus.getMessage());
+    }
+
+    @Test
+    void testCheckOperationUnexpectedException() {
+      final String expectedMessage = "This is just a test failure.";
+      when(mongoClient.getDatabase(any())).thenThrow(new IllegalArgumentException(expectedMessage));
+
+      final AirbyteConnectionStatus airbyteConnectionStatus = source.check(airbyteSourceConfig);
+      assertNotNull(airbyteConnectionStatus);
+      assertEquals(AirbyteConnectionStatus.Status.FAILED, airbyteConnectionStatus.getStatus());
+      assertEquals(expectedMessage, airbyteConnectionStatus.getMessage());
+    }
+
+    @Test
+    void testDiscoverOperation() throws IOException {
+      final AggregateIterable<Document> aggregateIterable = mock(AggregateIterable.class);
+      final List<Map<String, Object>> schemaDiscoveryJsonResponses =
+          Jsons.deserialize(MoreResources.readResource("schema_discovery_response.json"), new TypeReference<>() {});
+      final List<Document> schemaDiscoveryResponses = schemaDiscoveryJsonResponses.stream().map(s -> new Document(s)).collect(Collectors.toList());
+      final Document authorizedCollectionsResponse = Document.parse(MoreResources.readResource("authorized_collections_response.json"));
+      final MongoCollection mongoCollection = mock(MongoCollection.class);
+      final MongoCursor<Document> cursor = mock(MongoCursor.class);
+      final MongoDatabase mongoDatabase = mock(MongoDatabase.class);
+
+      when(cursor.hasNext()).thenReturn(true, true, false);
+      when(cursor.next()).thenReturn(schemaDiscoveryResponses.get(0), schemaDiscoveryResponses.get(1));
+      when(aggregateIterable.cursor()).thenReturn(cursor);
+      when(mongoCollection.aggregate(any())).thenReturn(aggregateIterable);
+      when(mongoDatabase.getCollection(any())).thenReturn(mongoCollection);
+      when(mongoDatabase.runCommand(any())).thenReturn(authorizedCollectionsResponse);
+      when(mongoClient.getDatabase(any())).thenReturn(mongoDatabase);
+
+      final AirbyteCatalog airbyteCatalog = source.discover(airbyteSourceConfig);
+
+      assertNotNull(airbyteCatalog);
+      assertEquals(1, airbyteCatalog.getStreams().size());
+
+      final Optional<AirbyteStream> stream = airbyteCatalog.getStreams().stream().findFirst();
+      assertTrue(stream.isPresent());
+      assertEquals(DB_NAME, stream.get().getNamespace());
+      assertEquals("testCollection", stream.get().getName());
+      assertEquals(JsonSchemaType.STRING.getJsonSchemaTypeMap().get("type"),
+          stream.get().getJsonSchema().get("properties").get("_id").get("type").asText());
+      assertEquals(JsonSchemaType.STRING.getJsonSchemaTypeMap().get("type"),
+          stream.get().getJsonSchema().get("properties").get("name").get("type").asText());
+      assertEquals(JsonSchemaType.STRING.getJsonSchemaTypeMap().get("type"),
+          stream.get().getJsonSchema().get("properties").get("last_updated").get("type").asText());
+      assertEquals(JsonSchemaType.NUMBER.getJsonSchemaTypeMap().get("type"),
+          stream.get().getJsonSchema().get("properties").get("total").get("type").asText());
+      assertEquals(JsonSchemaType.NUMBER.getJsonSchemaTypeMap().get("type"),
+          stream.get().getJsonSchema().get("properties").get("price").get("type").asText());
+      assertEquals(JsonSchemaType.ARRAY.getJsonSchemaTypeMap().get("type"),
+          stream.get().getJsonSchema().get("properties").get("items").get("type").asText());
+      assertEquals(JsonSchemaType.OBJECT.getJsonSchemaTypeMap().get("type"),
+          stream.get().getJsonSchema().get("properties").get("owners").get("type").asText());
+      assertEquals(JsonSchemaType.STRING.getJsonSchemaTypeMap().get("type"),
+          stream.get().getJsonSchema().get("properties").get("other").get("type").asText());
+      assertEquals(JsonSchemaType.NUMBER.getJsonSchemaTypeMap().get("type"),
+          stream.get().getJsonSchema().get("properties").get(DebeziumEventUtils.CDC_LSN).get("type").asText());
+      assertEquals(JsonSchemaType.STRING.getJsonSchemaTypeMap().get("type"),
+          stream.get().getJsonSchema().get("properties").get(DebeziumEventUtils.CDC_DELETED_AT).get("type").asText());
+      assertEquals(JsonSchemaType.STRING.getJsonSchemaTypeMap().get("type"),
+          stream.get().getJsonSchema().get("properties").get(DebeziumEventUtils.CDC_UPDATED_AT).get("type").asText());
+      assertEquals(true, stream.get().getSourceDefinedCursor());
+      assertEquals(List.of(MongoCatalogHelper.DEFAULT_CURSOR_FIELD), stream.get().getDefaultCursorField());
+      assertEquals(List.of(List.of(MongoCatalogHelper.DEFAULT_CURSOR_FIELD)), stream.get().getSourceDefinedPrimaryKey());
+      assertEquals(MongoCatalogHelper.SUPPORTED_SYNC_MODES, stream.get().getSupportedSyncModes());
+    }
+
+    @Test
+    void testDiscoverOperationWithUnexpectedFailure() {
+      final String expectedMessage = "This is just a test failure.";
+      when(mongoClient.getDatabase(any())).thenThrow(new IllegalArgumentException(expectedMessage));
+
+      assertThrows(IllegalArgumentException.class, () -> source.discover(airbyteSourceConfig));
+    }
   }
 
-  @Test
-  void testCheckOperation() throws IOException {
-    final ClusterDescription clusterDescription = mock(ClusterDescription.class);
-    final Document response = Document.parse(MoreResources.readResource("authorized_collections_response.json"));
-    final MongoDatabase mongoDatabase = mock(MongoDatabase.class);
-
-    when(clusterDescription.getType()).thenReturn(ClusterType.REPLICA_SET);
-    when(mongoDatabase.runCommand(any())).thenReturn(response);
-    when(mongoClient.getDatabase(any())).thenReturn(mongoDatabase);
-    when(mongoClient.getClusterDescription()).thenReturn(clusterDescription);
-
-    final AirbyteConnectionStatus airbyteConnectionStatus = source.check(airbyteSourceConfig);
-    assertNotNull(airbyteConnectionStatus);
-    assertEquals(AirbyteConnectionStatus.Status.SUCCEEDED, airbyteConnectionStatus.getStatus());
-  }
-
-  @Test
-  void testCheckOperationNoAuthorizedCollections() throws IOException {
-    final ClusterDescription clusterDescription = mock(ClusterDescription.class);
-    final Document response = Document.parse(MoreResources.readResource("no_authorized_collections_response.json"));
-    final MongoDatabase mongoDatabase = mock(MongoDatabase.class);
-
-    when(clusterDescription.getType()).thenReturn(ClusterType.REPLICA_SET);
-    when(mongoDatabase.runCommand(any())).thenReturn(response);
-    when(mongoClient.getDatabase(any())).thenReturn(mongoDatabase);
-    when(mongoClient.getClusterDescription()).thenReturn(clusterDescription);
-
-    final AirbyteConnectionStatus airbyteConnectionStatus = source.check(airbyteSourceConfig);
-    assertNotNull(airbyteConnectionStatus);
-    assertEquals(AirbyteConnectionStatus.Status.FAILED, airbyteConnectionStatus.getStatus());
-    assertEquals("Target MongoDB database does not contain any authorized collections.", airbyteConnectionStatus.getMessage());
-  }
-
-  @Test
-  void testCheckOperationInvalidClusterType() throws IOException {
-    final ClusterDescription clusterDescription = mock(ClusterDescription.class);
-    final Document response = Document.parse(MoreResources.readResource("authorized_collections_response.json"));
-    final MongoDatabase mongoDatabase = mock(MongoDatabase.class);
-
-    when(clusterDescription.getType()).thenReturn(ClusterType.STANDALONE);
-    when(mongoDatabase.runCommand(any())).thenReturn(response);
-    when(mongoClient.getDatabase(any())).thenReturn(mongoDatabase);
-    when(mongoClient.getClusterDescription()).thenReturn(clusterDescription);
-
-    final AirbyteConnectionStatus airbyteConnectionStatus = source.check(airbyteSourceConfig);
-    assertNotNull(airbyteConnectionStatus);
-    assertEquals(AirbyteConnectionStatus.Status.FAILED, airbyteConnectionStatus.getStatus());
-    assertEquals("Target MongoDB instance is not a replica set cluster.", airbyteConnectionStatus.getMessage());
-  }
-
-  @Test
-  void testCheckOperationUnexpectedException() {
-    final String expectedMessage = "This is just a test failure.";
-    when(mongoClient.getDatabase(any())).thenThrow(new IllegalArgumentException(expectedMessage));
-
-    final AirbyteConnectionStatus airbyteConnectionStatus = source.check(airbyteSourceConfig);
-    assertNotNull(airbyteConnectionStatus);
-    assertEquals(AirbyteConnectionStatus.Status.FAILED, airbyteConnectionStatus.getStatus());
-    assertEquals(expectedMessage, airbyteConnectionStatus.getMessage());
-  }
-
-  @Test
-  void testDiscoverOperation() throws IOException {
-    final AggregateIterable<Document> aggregateIterable = mock(AggregateIterable.class);
-    final List<Map<String, Object>> schemaDiscoveryJsonResponses =
-        Jsons.deserialize(MoreResources.readResource("schema_discovery_response.json"), new TypeReference<>() {});
-    final List<Document> schemaDiscoveryResponses = schemaDiscoveryJsonResponses.stream().map(s -> new Document(s)).collect(Collectors.toList());
-    final Document authorizedCollectionsResponse = Document.parse(MoreResources.readResource("authorized_collections_response.json"));
-    final MongoCollection mongoCollection = mock(MongoCollection.class);
-    final MongoCursor<Document> cursor = mock(MongoCursor.class);
-    final MongoDatabase mongoDatabase = mock(MongoDatabase.class);
-
-    when(cursor.hasNext()).thenReturn(true, true, false);
-    when(cursor.next()).thenReturn(schemaDiscoveryResponses.get(0), schemaDiscoveryResponses.get(1));
-    when(aggregateIterable.cursor()).thenReturn(cursor);
-    when(mongoCollection.aggregate(any())).thenReturn(aggregateIterable);
-    when(mongoDatabase.getCollection(any())).thenReturn(mongoCollection);
-    when(mongoDatabase.runCommand(any())).thenReturn(authorizedCollectionsResponse);
-    when(mongoClient.getDatabase(any())).thenReturn(mongoDatabase);
-
-    final AirbyteCatalog airbyteCatalog = source.discover(airbyteSourceConfig);
-
-    assertNotNull(airbyteCatalog);
-    assertEquals(1, airbyteCatalog.getStreams().size());
-
-    final Optional<AirbyteStream> stream = airbyteCatalog.getStreams().stream().findFirst();
-    assertTrue(stream.isPresent());
-    assertEquals(DB_NAME, stream.get().getNamespace());
-    assertEquals("testCollection", stream.get().getName());
-    assertEquals(JsonSchemaType.STRING.getJsonSchemaTypeMap().get("type"),
-        stream.get().getJsonSchema().get("properties").get("_id").get("type").asText());
-    assertEquals(JsonSchemaType.STRING.getJsonSchemaTypeMap().get("type"),
-        stream.get().getJsonSchema().get("properties").get("name").get("type").asText());
-    assertEquals(JsonSchemaType.STRING.getJsonSchemaTypeMap().get("type"),
-        stream.get().getJsonSchema().get("properties").get("last_updated").get("type").asText());
-    assertEquals(JsonSchemaType.NUMBER.getJsonSchemaTypeMap().get("type"),
-        stream.get().getJsonSchema().get("properties").get("total").get("type").asText());
-    assertEquals(JsonSchemaType.NUMBER.getJsonSchemaTypeMap().get("type"),
-        stream.get().getJsonSchema().get("properties").get("price").get("type").asText());
-    assertEquals(JsonSchemaType.ARRAY.getJsonSchemaTypeMap().get("type"),
-        stream.get().getJsonSchema().get("properties").get("items").get("type").asText());
-    assertEquals(JsonSchemaType.OBJECT.getJsonSchemaTypeMap().get("type"),
-        stream.get().getJsonSchema().get("properties").get("owners").get("type").asText());
-    assertEquals(JsonSchemaType.STRING.getJsonSchemaTypeMap().get("type"),
-        stream.get().getJsonSchema().get("properties").get("other").get("type").asText());
-    assertEquals(JsonSchemaType.NUMBER.getJsonSchemaTypeMap().get("type"),
-        stream.get().getJsonSchema().get("properties").get(DebeziumEventUtils.CDC_LSN).get("type").asText());
-    assertEquals(JsonSchemaType.STRING.getJsonSchemaTypeMap().get("type"),
-        stream.get().getJsonSchema().get("properties").get(DebeziumEventUtils.CDC_DELETED_AT).get("type").asText());
-    assertEquals(JsonSchemaType.STRING.getJsonSchemaTypeMap().get("type"),
-        stream.get().getJsonSchema().get("properties").get(DebeziumEventUtils.CDC_UPDATED_AT).get("type").asText());
-    assertEquals(true, stream.get().getSourceDefinedCursor());
-    assertEquals(List.of(MongoCatalogHelper.DEFAULT_CURSOR_FIELD), stream.get().getDefaultCursorField());
-    assertEquals(List.of(List.of(MongoCatalogHelper.DEFAULT_CURSOR_FIELD)), stream.get().getSourceDefinedPrimaryKey());
-    assertEquals(MongoCatalogHelper.SUPPORTED_SYNC_MODES, stream.get().getSupportedSyncModes());
-  }
-
-  @Test
-  void testDiscoverOperationWithUnexpectedFailure() throws IOException {
-    final String expectedMessage = "This is just a test failure.";
-    when(mongoClient.getDatabase(any())).thenThrow(new IllegalArgumentException(expectedMessage));
-
-    assertThrows(IllegalArgumentException.class, () -> source.discover(airbyteSourceConfig));
-  }
-
-  @Test
-  void testFullRefresh() throws Exception {
-    // TODO implement
-  }
-
-  @Test
-  void testIncrementalRefresh() throws Exception {
-    // TODO implement
-  }
-
-  private static JsonNode createConfiguration(final Optional<String> username, final Optional<String> password) {
+  private static JsonNode createConfiguration(
+      final String connectionString,
+      final Optional<String> replicaSet,
+      final Optional<String> username,
+      final Optional<String> password) {
     final Map<String, Object> config = new HashMap<>();
     final Map<String, Object> baseConfig = Map.of(
         MongoConstants.DATABASE_CONFIGURATION_KEY, DB_NAME,
-        MongoConstants.CONNECTION_STRING_CONFIGURATION_KEY, "mongodb://localhost:27017/",
-        MongoConstants.AUTH_SOURCE_CONFIGURATION_KEY, "admin",
-        MongoConstants.REPLICA_SET_CONFIGURATION_KEY, "replica-set");
+        MongoConstants.CONNECTION_STRING_CONFIGURATION_KEY, connectionString,
+        MongoConstants.AUTH_SOURCE_CONFIGURATION_KEY, "admin");
 
     config.putAll(baseConfig);
+    replicaSet.ifPresent( r -> config.put(MongoConstants.REPLICA_SET_CONFIGURATION_KEY, r));
     username.ifPresent(u -> config.put(MongoConstants.USER_CONFIGURATION_KEY, u));
     password.ifPresent(p -> config.put(MongoConstants.PASSWORD_CONFIGURATION_KEY, p));
     return Jsons.deserialize(Jsons.serialize(config));
   }
-
 }
