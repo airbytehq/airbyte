73b14aaeec48748c99135128f9b856344ddf7629
Alexandre Girard <alexandre@airbyte.io>
2023-08-30 09:33:51 -0700

    hide more implementation details

--- airbyte-cdk/python/airbyte_cdk/sources/stream_reader/concurrent/concurrent_full_refresh_reader.py
+++ airbyte-cdk/python/airbyte_cdk/sources/stream_reader/concurrent/concurrent_full_refresh_reader.py
@@ -11,7 +11,6 @@ from airbyte_cdk.models import SyncMode
 from airbyte_cdk.sources.stream_reader.concurrent.partition_generator import PartitionGenerator
 from airbyte_cdk.sources.stream_reader.concurrent.partition_reader import PartitionReader
 from airbyte_cdk.sources.stream_reader.concurrent.queue_consumer import _SENTINEL, QueueConsumer
-from airbyte_cdk.sources.stream_reader.concurrent.record import Record
 from airbyte_cdk.sources.stream_reader.concurrent.stream_partition import StreamPartition
 from airbyte_cdk.sources.stream_reader.full_refresh_stream_reader import FullRefreshStreamReader
 from airbyte_cdk.sources.streams import Stream
@@ -24,17 +23,14 @@ class ConcurrentStreamReader(FullRefreshStreamReader):
     def __init__(
         self,
         partition_generator: PartitionGenerator,
-        queue_consumer: QueueConsumer,
-        queue: Queue[Optional[StreamPartition]],
+        partition_reader: PartitionReader,
         max_workers: int,
         slice_logger: SliceLogger,
     ):
         self._partitions_generator = partition_generator
-        self._queue_consumer = queue_consumer
-        self._queue = queue
+        self._partition_reader = partition_reader
         self._max_workers = max_workers
         self._slice_logger = slice_logger
-        self._partition_reader = PartitionReader("partition_reader", Queue())
 
     def read_stream(
         self, stream: Stream, cursor_field: Optional[List[str]], logger: logging.Logger, internal_config: InternalConfig = InternalConfig()
@@ -42,46 +38,23 @@ class ConcurrentStreamReader(FullRefreshStreamReader):
         logger.debug(f"Processing stream slices for {stream.name} (sync_mode: full_refresh)")
         total_records_counter = 0
         with concurrent.futures.ThreadPoolExecutor(max_workers=self._max_workers, thread_name_prefix="workerpool") as executor:
-            try:
-                # Submit partition generation tasks
-                partition_generation_future = executor.submit(
-                    PartitionGenerator.generate_partitions_for_stream,
-                    self._partitions_generator,
-                    stream,
-                    SyncMode.full_refresh,
-                    cursor_field,
-                )
-                # While partitions are still being generated
-                while not partition_generation_future.done() or not self._partition_reader.is_done() or self._queue.qsize() > 0:
-                    # While there is a partition to process
-                    while self._partition_reader.there_are_records_ready():
-                        record = self._partition_reader.get_next_record()
-                        yield record.stream_data
-                        if FullRefreshStreamReader.is_record(record.stream_data):
-                            total_records_counter += 1
-                            if internal_config and internal_config.is_limit_reached(total_records_counter):
-                                return
-                    while self._queue.qsize() > 0:
-                        partition = self._queue.get()
-                        print(f"processing partition {partition} for {stream.name}")
-                        if self._slice_logger.should_log_slice_message(logger):
-                            # FIXME: This is creating slice log messages for parity with the synchronous implementation
-                            # but these cannot be used by the connector builder to build slices because they can be unordered
-                            yield self._slice_logger.create_slice_log_message(partition.slice)
-                        self._partition_reader.process_partition_async(partition, executor)
-            except Exception as e:
-                self._terminate_consumers()
-                executor.shutdown(wait=True, cancel_futures=True)
-                raise e
-
-    def _get_num_dedicated_consumer_worker(self) -> int:
-        # FIXME figure this out and add a unit test
-        return int(max(self._max_workers / 2, 1))
-
-    def _terminate_consumers(self):
-        # FIXME: add a unit test
-        for _ in range(self._get_num_dedicated_consumer_worker()):
-            self._queue.put(_SENTINEL)
-
-    def _futures_are_running(self, queue_consumer_futures):
-        return not all(future.done() for future in queue_consumer_futures)
+            # Submit partition generation tasks
+            self._partitions_generator.generate_partitions_async(stream, SyncMode.full_refresh, cursor_field, executor)
+            # While partitions are still being generated
+            while not self._partitions_generator.is_done() or not self._partition_reader.is_done():
+                # While there is a partition to process
+                while self._partition_reader.there_are_records_ready():
+                    record = self._partition_reader.get_next_record()
+                    yield record.stream_data
+                    if FullRefreshStreamReader.is_record(record.stream_data):
+                        total_records_counter += 1
+                        if internal_config and internal_config.is_limit_reached(total_records_counter):
+                            return
+                while self._partitions_generator.there_are_partitions_ready():
+                    partition = self._partitions_generator.get_next_partition()
+                    print(f"processing partition {partition} for {stream.name}")
+                    if self._slice_logger.should_log_slice_message(logger):
+                        # FIXME: This is creating slice log messages for parity with the synchronous implementation
+                        # but these cannot be used by the connector builder to build slices because they can be unordered
+                        yield self._slice_logger.create_slice_log_message(partition.slice)
+                    self._partition_reader.process_partition_async(partition, executor)
--- airbyte-cdk/python/airbyte_cdk/sources/stream_reader/concurrent/partition_generator.py
+++ airbyte-cdk/python/airbyte_cdk/sources/stream_reader/concurrent/partition_generator.py
@@ -14,6 +14,7 @@ class PartitionGenerator:
     def __init__(self, queue: Queue[Optional[StreamPartition]], name: str):
         self._queue = queue
         self._name = name
+        self._futures = []
 
     def generate_partitions_for_stream(self, stream: Stream, sync_mode: SyncMode, cursor_field: Optional[List[str]]) -> None:
         print(f"generate_partitions_for_stream for {self._name} for stream {stream.name}")
@@ -21,3 +22,28 @@ class PartitionGenerator:
             print(f"putting partition and stream on queue for {partition}. stream: {stream.name}")
             stream_partition = StreamPartition(stream, partition, cursor_field)
             self._queue.put(stream_partition)
+
+    def generate_partitions_async(self, stream: Stream, sync_mode: SyncMode, cursor_field: Optional[List[str]], executor):
+        f = executor.submit(
+            PartitionGenerator.generate_partitions_for_stream,
+            self,
+            stream,
+            SyncMode.full_refresh,
+            cursor_field,
+        )
+        self._futures.append(f)
+
+    def get_next_partition(self) -> Optional[StreamPartition]:
+        if self._queue.qsize() > 0:
+            return self._queue.get()
+        else:
+            return None
+
+    def there_are_partitions_ready(self) -> bool:
+        return self._queue.qsize() > 0
+
+    def is_done(self) -> bool:
+        return self._queue.qsize() == 0 and not self._futures_are_running(self._futures)
+
+    def _futures_are_running(self, futures):
+        return not all(future.done() for future in futures)
