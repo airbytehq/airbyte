921dbb217155ec1d8d62847877544f658d42cd4c
Christo Grabowski <108154848+ChristoGrab@users.noreply.github.com>
2023-08-30 12:01:49 -0400

    fix formatting errors

--- airbyte-integrations/connectors/source-notion/unit_tests/test_source.py
+++ airbyte-integrations/connectors/source-notion/unit_tests/test_source.py
@@ -12,11 +12,13 @@ RESTRICTED_RESOURCE_ERROR_MESSAGE = "The provided API access token does not have
 GENERIC_ERROR_MESSAGE = "Conflict occured while saving. Please try again."
 NO_ERROR_MESSAGE = "An unexpected error occured while connecting to Notion. Please check your credentials and try again."
 
+
 def test_check_connection(mocker, requests_mock):
     source = SourceNotion()
     logger_mock, config_mock = MagicMock(), MagicMock()
     requests_mock.get("https://api.notion.com/v1/users", json={"results": [{"id": "aaa"}], "next_cursor": None})
     assert source.check_connection(logger_mock, config_mock) == (True, None)
+
 
 @pytest.mark.parametrize(
         "status_code,json_response,expected_message",
@@ -27,15 +29,15 @@ def test_check_connection(mocker, requests_mock):
             (400, {}, NO_ERROR_MESSAGE)
         ]
 )
-
 def test_check_connection_errors(mocker, requests_mock, status_code, json_response, expected_message):
     source = SourceNotion()
     logger_mock, config_mock = MagicMock(), MagicMock()
     requests_mock.get("https://api.notion.com/v1/users", status_code=status_code, json=json_response)
     result, message = source.check_connection(logger_mock, config_mock)
 
-    assert result == False
+    assert result is False
     assert message == expected_message
+
 
 def test_streams(mocker):
     source = SourceNotion()
