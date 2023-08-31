1a5d600aecf09f1a01039f645c56a4b2165b7fd1
edgao <edgao@users.noreply.github.com>
2023-08-31 23:43:55 +0000

    Automated Commit - Formatting Changes

--- airbyte-integrations/connectors/destination-snowflake/src/main/java/io/airbyte/integrations/destination/snowflake/typing_deduping/SnowflakeV2TableMigrator.java
+++ airbyte-integrations/connectors/destination-snowflake/src/main/java/io/airbyte/integrations/destination/snowflake/typing_deduping/SnowflakeV2TableMigrator.java
@@ -1,3 +1,7 @@
+/*
+ * Copyright (c) 2023 Airbyte, Inc., all rights reserved.
+ */
+
 package io.airbyte.integrations.destination.snowflake.typing_deduping;
 
 import static io.airbyte.integrations.base.JavaBaseConstants.DEFAULT_AIRBYTE_INTERNAL_NAMESPACE;
@@ -5,7 +9,6 @@ import static io.airbyte.integrations.destination.snowflake.SnowflakeInternalSta
 
 import io.airbyte.db.jdbc.JdbcDatabase;
 import io.airbyte.integrations.base.TypingAndDedupingFlag;
-import io.airbyte.integrations.base.destination.typing_deduping.ColumnId;
 import io.airbyte.integrations.base.destination.typing_deduping.StreamConfig;
 import io.airbyte.integrations.base.destination.typing_deduping.StreamId;
 import io.airbyte.integrations.base.destination.typing_deduping.V2TableMigrator;
@@ -14,13 +17,17 @@ import java.util.LinkedHashMap;
 import java.util.Optional;
 
 public class SnowflakeV2TableMigrator implements V2TableMigrator<SnowflakeTableDefinition> {
+
   private final JdbcDatabase database;
   private final String rawNamespace;
   private final String databaseName;
   private final SnowflakeSqlGenerator generator;
   private final SnowflakeDestinationHandler handler;
 
-  public SnowflakeV2TableMigrator(final JdbcDatabase database, final String databaseName, final SnowflakeSqlGenerator generator, final SnowflakeDestinationHandler handler) {
+  public SnowflakeV2TableMigrator(final JdbcDatabase database,
+                                  final String databaseName,
+                                  final SnowflakeSqlGenerator generator,
+                                  final SnowflakeDestinationHandler handler) {
     this.database = database;
     this.databaseName = databaseName;
     this.generator = generator;
@@ -41,8 +48,10 @@ public class SnowflakeV2TableMigrator implements V2TableMigrator<SnowflakeTableD
     }
   }
 
-  // These methods were copied from https://github.com/airbytehq/airbyte/blob/d5fdb1b982d464f54941bf9a830b9684fb47d249/airbyte-integrations/connectors/destination-snowflake/src/main/java/io/airbyte/integrations/destination/snowflake/typing_deduping/SnowflakeSqlGenerator.java
-  // which is the highest version of destination-snowflake that still uses quoted+lowercased identifiers
+  // These methods were copied from
+  // https://github.com/airbytehq/airbyte/blob/d5fdb1b982d464f54941bf9a830b9684fb47d249/airbyte-integrations/connectors/destination-snowflake/src/main/java/io/airbyte/integrations/destination/snowflake/typing_deduping/SnowflakeSqlGenerator.java
+  // which is the highest version of destination-snowflake that still uses quoted+lowercased
+  // identifiers
   private static StreamId buildStreamId_lowercase(final String namespace, final String name, final String rawNamespaceOverride) {
     // No escaping needed, as far as I can tell. We quote all our identifier names.
     return new StreamId(
@@ -60,22 +69,23 @@ public class SnowflakeV2TableMigrator implements V2TableMigrator<SnowflakeTableD
     return identifier.replace("\"", "\"\"");
   }
 
-  // And this was taken from https://github.com/airbytehq/airbyte/blob/master/airbyte-integrations/connectors/destination-snowflake/src/main/java/io/airbyte/integrations/destination/snowflake/typing_deduping/SnowflakeDestinationHandler.java
+  // And this was taken from
+  // https://github.com/airbytehq/airbyte/blob/master/airbyte-integrations/connectors/destination-snowflake/src/main/java/io/airbyte/integrations/destination/snowflake/typing_deduping/SnowflakeDestinationHandler.java
   public Optional<SnowflakeTableDefinition> findExistingTable_lowercase(final StreamId id) throws SQLException {
     // The obvious database.getMetaData().getColumns() solution doesn't work, because JDBC translates
     // VARIANT as VARCHAR
     final LinkedHashMap<String, String> columns = database.queryJsons(
-            """
-            SELECT column_name, data_type
-            FROM information_schema.columns
-            WHERE table_catalog = ?
-              AND table_schema = ?
-              AND table_name = ?
-            ORDER BY ordinal_position;
-            """,
-            databaseName.toUpperCase(),
-            id.finalNamespace(),
-            id.finalName()).stream()
+        """
+        SELECT column_name, data_type
+        FROM information_schema.columns
+        WHERE table_catalog = ?
+          AND table_schema = ?
+          AND table_name = ?
+        ORDER BY ordinal_position;
+        """,
+        databaseName.toUpperCase(),
+        id.finalNamespace(),
+        id.finalName()).stream()
         .collect(LinkedHashMap::new,
             (map, row) -> map.put(row.get("COLUMN_NAME").asText(), row.get("DATA_TYPE").asText()),
             LinkedHashMap::putAll);
