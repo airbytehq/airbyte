c0e374d317ee9e22e1e6a9cf216f81821edd051a
Alexandre Girard <alexandre@airbyte.io>
2023-08-31 08:37:06 -0700

    type annotations

--- airbyte-cdk/python/airbyte_cdk/sources/stream_reader/concurrent/partition_generator.py
+++ airbyte-cdk/python/airbyte_cdk/sources/stream_reader/concurrent/partition_generator.py
@@ -7,15 +7,17 @@ from typing import List, Optional
 
 from airbyte_cdk.models import SyncMode
 from airbyte_cdk.sources.stream_reader.concurrent.stream_partition import StreamPartition
-from airbyte_cdk.sources.streams import Stream
+from airbyte_cdk.sources.streams import FullRefreshStreamReader, Stream
 
 
 class PartitionGenerator:
     def __init__(self, queue: Optional[Queue[Optional[StreamPartition]]] = None):
         self._queue = queue if queue else Queue()
-        self._futures: List[Future] = []
+        self._futures: List[Future[None]] = []
 
-    def generate_partitions_for_stream(self, stream: Stream, sync_mode: SyncMode, cursor_field: Optional[List[str]], stream_reader) -> None:
+    def generate_partitions_for_stream(
+        self, stream: Stream, sync_mode: SyncMode, cursor_field: Optional[List[str]], stream_reader: FullRefreshStreamReader
+    ) -> None:
         print(f"generate_partitions_for_stream for stream {stream.name}")
         for partition in stream.generate_partitions(sync_mode=sync_mode, cursor_field=cursor_field, stream_reader=stream_reader):
             print(f"putting partition and stream on queue for {partition}. stream: {stream.name}")
@@ -23,7 +25,9 @@ class PartitionGenerator:
             self._queue.put(stream_partition)
         print(f"done. queue size: {self._queue.qsize()}")
 
-    def generate_partitions_async(self, stream: Stream, sync_mode: SyncMode, cursor_field: Optional[List[str]], executor, stream_reader):
+    def generate_partitions_async(
+        self, stream: Stream, sync_mode: SyncMode, cursor_field: Optional[List[str]], executor, stream_reader: FullRefreshStreamReader
+    ):
         f = executor.submit(
             PartitionGenerator.generate_partitions_for_stream,
             self,
@@ -46,8 +50,8 @@ class PartitionGenerator:
     def is_done(self) -> bool:
         return self._queue.qsize() == 0 and self._futures_are_done(self._futures)
 
-    def _futures_are_done(self, futures):
+    def _futures_are_done(self, futures) -> bool:
         return all(future.done() for future in futures)
 
-    def get_exceptions(self):
-        return [future.exception() for future in self._futures if future.exception() is not None]
+    def get_exceptions(self) -> List[BaseException]:
+        return [f for f in [future.exception() for future in self._futures] if f is not None]
