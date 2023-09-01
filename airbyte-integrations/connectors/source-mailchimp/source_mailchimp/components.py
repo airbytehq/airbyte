13bfb0fbe1cabc1649fe38ffaafafce03c7ebaec
Tope Folorunso <folorunsotemitope10@gmail.com>
2023-09-01 03:09:18 +0100

    implement custom auth and extractor

--- airbyte-integrations/connectors/source-mailchimp/source_mailchimp/components.py
+++ airbyte-integrations/connectors/source-mailchimp/source_mailchimp/components.py
@@ -3,11 +3,16 @@
 #
 
 from dataclasses import dataclass
-from typing import Iterable
+from typing import Any, Iterable, List, Mapping
 
 from airbyte_cdk.models import SyncMode
 from airbyte_cdk.sources.declarative.partition_routers.substream_partition_router import SubstreamPartitionRouter
+from airbyte_cdk.sources.declarative.extractors.dpath_extractor import DpathExtractor
 from airbyte_cdk.sources.declarative.types import StreamSlice
+from airbyte_cdk.sources.declarative.auth.declarative_authenticator import DeclarativeAuthenticator
+from airbyte_cdk.sources.declarative.auth.token import BearerAuthenticator, BasicHttpAuthenticator
+
+import requests
 
 
 
@@ -37,3 +42,31 @@ class CampaignIdPartitionRouter(SubstreamPartitionRouter):
             slice_ = {"campaign_id": campaign["id"]}
             yield slice_
 
+@dataclass
+class EmailActivityRecordExtractor(DpathExtractor):
+    
+    def extract_records(self, response: requests.Response) -> List[Mapping[str, Any]]:
+        extracted_records = super().extract_records(response)
+        # transform before save
+        # [{'campaign_id', 'list_id', 'list_is_active', 'email_id', 'email_address', 'activity[array[object]]', '_links'}] ->
+        # -> [[{'campaign_id', 'list_id', 'list_is_active', 'email_id', 'email_address', '**activity[i]', '_links'}, ...]]
+        for item in extracted_records:
+            for activity_item in item.pop("activity", []):
+                yield {**item, **activity_item}
+
+
+@dataclass
+class MailchimpAuthenticator(DeclarativeAuthenticator):
+    config: Mapping[str, Any]
+    basic_auth: BasicHttpAuthenticator
+    oauth: BearerAuthenticator
+
+    def __new__(cls, basic_auth, oauth, config, *args, **kwargs):
+        authorization = config.get("credentials", {})
+        auth_type = authorization.get("auth_type")
+        if auth_type == "apikey" or not authorization:
+            return basic_auth
+        elif auth_type == "oauth2.0":
+            return oauth
+        else:
+            raise Exception(f"Invalid auth type: {auth_type}")
