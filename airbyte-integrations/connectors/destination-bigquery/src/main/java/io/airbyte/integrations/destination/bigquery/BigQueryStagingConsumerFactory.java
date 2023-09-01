30ef40bc45023d6f03e60bd91c19022a9c75dead
jbfbell <jbfbell@users.noreply.github.com>
2023-09-01 03:41:53 +0000

    Automated Commit - Formatting Changes

--- airbyte-integrations/connectors/destination-bigquery/src/main/java/io/airbyte/integrations/destination/bigquery/BigQueryStagingConsumerFactory.java
+++ airbyte-integrations/connectors/destination-bigquery/src/main/java/io/airbyte/integrations/destination/bigquery/BigQueryStagingConsumerFactory.java
@@ -81,15 +81,15 @@ public class BigQueryStagingConsumerFactory {
 
   // TODO Commenting this out for now since it slows down syncs
   private CheckedConsumer<AirbyteStreamNameNamespacePair, Exception> incrementalTypingAndDedupingStreamConsumer(final TyperDeduper typerDeduper) {
-//    final TypeAndDedupeOperationValve valve = new TypeAndDedupeOperationValve();
+    // final TypeAndDedupeOperationValve valve = new TypeAndDedupeOperationValve();
     return (streamId) -> {
-//      if (!valve.containsKey(streamId)) {
-//        valve.addStream(streamId);
-//      }
-//      if (valve.readyToTypeAndDedupe(streamId)) {
-//        typerDeduper.typeAndDedupe(streamId.getNamespace(), streamId.getName());
-//        valve.updateTimeAndIncreaseInterval(streamId);
-//      }
+      // if (!valve.containsKey(streamId)) {
+      // valve.addStream(streamId);
+      // }
+      // if (valve.readyToTypeAndDedupe(streamId)) {
+      // typerDeduper.typeAndDedupe(streamId.getNamespace(), streamId.getName());
+      // valve.updateTimeAndIncreaseInterval(streamId);
+      // }
     };
   }
 
