50ddce00a33780a7ac4de665384e2c306b5df0fb
Jose Pefaur <jose.pefaur@gmail.com>
2023-08-31 23:42:44 -0500

    add tests

--- airbyte-integrations/connectors/source-mongodb-internal-poc/src/test/java/io/airbyte/integrations/source/mongodb/internal/cdc/MongoDbCdcPropertiesTest.java
+++ airbyte-integrations/connectors/source-mongodb-internal-poc/src/test/java/io/airbyte/integrations/source/mongodb/internal/cdc/MongoDbCdcPropertiesTest.java
@@ -4,29 +4,68 @@
 
 package io.airbyte.integrations.source.mongodb.internal.cdc;
 
+import static com.mongodb.internal.connection.tlschannel.util.Util.assertTrue;
+import static io.airbyte.integrations.source.mongodb.internal.MongoConstants.DATABASE_CONFIGURATION_KEY;
 import static io.airbyte.integrations.source.mongodb.internal.cdc.MongoDbCdcProperties.CAPTURE_MODE_KEY;
 import static io.airbyte.integrations.source.mongodb.internal.cdc.MongoDbCdcProperties.CAPTURE_MODE_VALUE;
 import static io.airbyte.integrations.source.mongodb.internal.cdc.MongoDbCdcProperties.CONNECTOR_CLASS_KEY;
 import static io.airbyte.integrations.source.mongodb.internal.cdc.MongoDbCdcProperties.CONNECTOR_CLASS_VALUE;
+import static io.airbyte.integrations.source.mongodb.internal.cdc.MongoDbCdcProperties.FIELD_EXCLUDE_LIST_KEY;
 import static io.airbyte.integrations.source.mongodb.internal.cdc.MongoDbCdcProperties.HEARTBEAT_FREQUENCY_MS;
 import static io.airbyte.integrations.source.mongodb.internal.cdc.MongoDbCdcProperties.HEARTBEAT_INTERVAL_KEY;
 import static io.airbyte.integrations.source.mongodb.internal.cdc.MongoDbCdcProperties.SNAPSHOT_MODE_KEY;
 import static io.airbyte.integrations.source.mongodb.internal.cdc.MongoDbCdcProperties.SNAPSHOT_MODE_VALUE;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import io.airbyte.commons.json.Jsons;
+import io.airbyte.integrations.source.mongodb.internal.cdc.MongoDbCdcProperties.CollectionAndField;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
 import java.util.Properties;
+import java.util.Set;
 import org.junit.jupiter.api.Test;
 
 class MongoDbCdcPropertiesTest {
 
+  private final String COLLECTION1 = "collection1";
+  private final String COLLECTION2 = "collection2";
+  private final String COLLECTION3 = "collection3";
+
+  private final String FIELD1 = "field1";
+  private final String FIELD2 = "field2";
+  private final String FIELD3 = "field3";
+
+  private final String DATABASE= "database";
+
+  private final String FULLY_QUALIFIED_FIELD1 = DATABASE + "." + COLLECTION1 + "." + FIELD1;
+  private final String FULLY_QUALIFIED_FIELD2 = DATABASE + "." + COLLECTION2 + "." + FIELD2;
+  private final String FULLY_QUALIFIED_FIELD3 = DATABASE + "." + COLLECTION3 + "." + FIELD3;
+  private final String FULLY_QUALIFIED_FIELD4 = DATABASE + "." + COLLECTION3 + "." + FIELD1;
+
   @Test
   void testDebeziumProperties() {
-    final Properties debeziumProperties = MongoDbCdcProperties.getDebeziumProperties();
-    assertEquals(4, debeziumProperties.size());
+    final JsonNode config = Jsons.jsonNode(Map.of(DATABASE_CONFIGURATION_KEY, DATABASE));
+    final Set<CollectionAndField>  fieldsToExclude = Set.of(
+        new CollectionAndField(COLLECTION1, FIELD1),
+        new CollectionAndField(COLLECTION2, FIELD2),
+        new CollectionAndField(COLLECTION3, FIELD3),
+        new CollectionAndField(COLLECTION3, FIELD1)
+    );
+
+    final Properties debeziumProperties = MongoDbCdcProperties.getDebeziumProperties(config, fieldsToExclude);
+    assertEquals(5, debeziumProperties.size());
     assertEquals(CONNECTOR_CLASS_VALUE, debeziumProperties.get(CONNECTOR_CLASS_KEY));
     assertEquals(SNAPSHOT_MODE_VALUE, debeziumProperties.get(SNAPSHOT_MODE_KEY));
     assertEquals(CAPTURE_MODE_VALUE, debeziumProperties.get(CAPTURE_MODE_KEY));
     assertEquals(HEARTBEAT_FREQUENCY_MS, debeziumProperties.get(HEARTBEAT_INTERVAL_KEY));
+    final String fieldExcludeList = (String) debeziumProperties.get(FIELD_EXCLUDE_LIST_KEY);
+    final List<String> actualFullyQualifiedExcludedFields = Arrays.asList(fieldExcludeList.split(","));
+    assertEquals(4, actualFullyQualifiedExcludedFields.size());
+    assertTrue(actualFullyQualifiedExcludedFields.contains(FULLY_QUALIFIED_FIELD1));
+    assertTrue(actualFullyQualifiedExcludedFields.contains(FULLY_QUALIFIED_FIELD2));
+    assertTrue(actualFullyQualifiedExcludedFields.contains(FULLY_QUALIFIED_FIELD3));
+    assertTrue(actualFullyQualifiedExcludedFields.contains(FULLY_QUALIFIED_FIELD4));
   }
-
 }
---
+++ airbyte-integrations/connectors/source-mongodb-internal-poc/src/test/java/io/airbyte/integrations/source/mongodb/internal/cdc/MongoDbDebeziumFieldsUtilTest.java
@@ -0,0 +1,62 @@
+package io.airbyte.integrations.source.mongodb.internal.cdc;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import io.airbyte.integrations.source.mongodb.internal.cdc.MongoDbCdcProperties.CollectionAndField;
+import io.airbyte.protocol.models.Field;
+import io.airbyte.protocol.models.JsonSchemaType;
+import io.airbyte.protocol.models.v0.AirbyteStream;
+import io.airbyte.protocol.models.v0.CatalogHelpers;
+import io.airbyte.protocol.models.v0.ConfiguredAirbyteCatalog;
+import io.airbyte.protocol.models.v0.ConfiguredAirbyteStream;
+import java.util.List;
+import java.util.Set;
+import org.junit.jupiter.api.Test;
+
+class MongoDbDebeziumFieldsUtilTest {
+
+  private static final String COLLECTION1 = "collection1";
+  private static final String COLLECTION2 = "collection2";
+  private static final String COLLECTION3 = "collection3";
+
+  private static final String FIELD1 = "field1";
+  private static final String FIELD2 = "field2";
+  private static final String FIELD3 = "field3";
+  private static final String FIELD4 = "field4";
+  private static final String FIELD5 = "field5";
+
+  @Test
+  void getFieldsNotIncludedInCatalogTest() {
+    //source
+    final AirbyteStream sourceAirbyteStream1 = CatalogHelpers.createAirbyteStream(COLLECTION1,
+        Field.of(FIELD1, JsonSchemaType.STRING),
+        Field.of(FIELD2, JsonSchemaType.STRING));
+    final AirbyteStream sourceAirbyteStream2 = CatalogHelpers.createAirbyteStream(COLLECTION2,
+        Field.of(FIELD2, JsonSchemaType.STRING),
+        Field.of(FIELD3, JsonSchemaType.STRING));
+    final AirbyteStream sourceAirbyteStream3 = CatalogHelpers.createAirbyteStream(COLLECTION3,
+        Field.of(FIELD4, JsonSchemaType.STRING),
+        Field.of(FIELD5, JsonSchemaType.STRING));
+
+    //configured
+    final AirbyteStream configuredAirbyteStream1 = CatalogHelpers.createAirbyteStream(COLLECTION2,
+        Field.of(FIELD2, JsonSchemaType.STRING));
+    final AirbyteStream configuredAirbyteStream2 = CatalogHelpers.createAirbyteStream(COLLECTION3,
+        Field.of(FIELD4, JsonSchemaType.STRING),
+        Field.of(FIELD5, JsonSchemaType.STRING));
+    final ConfiguredAirbyteCatalog configuredAirbyteCatalog = new ConfiguredAirbyteCatalog().withStreams(
+        List.of(new ConfiguredAirbyteStream().withStream(configuredAirbyteStream1), new ConfiguredAirbyteStream().withStream(configuredAirbyteStream2)));
+
+    final Set<CollectionAndField> fieldsNotIncludedInCatalog =
+        MongoDbDebeziumFieldsUtil.getFieldsNotIncludedInConfiguredStreams(
+            configuredAirbyteCatalog,
+            List.of(sourceAirbyteStream1, sourceAirbyteStream2, sourceAirbyteStream3));
+
+    final Set<CollectionAndField> expectedFieldsNotIncludedInCatalog = Set.of(
+        new CollectionAndField(COLLECTION1, FIELD1),
+        new CollectionAndField(COLLECTION1, FIELD2),
+        new CollectionAndField(COLLECTION2, FIELD3));
+
+    assertEquals(expectedFieldsNotIncludedInCatalog, fieldsNotIncludedInCatalog);
+  }
+}
