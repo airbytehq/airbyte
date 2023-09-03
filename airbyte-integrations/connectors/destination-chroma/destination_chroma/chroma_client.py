cc5834498a9224174e565ba2ecebee12027c30b1
Tope Folorunso <folorunsotemitope10@gmail.com>
2023-09-03 23:24:16 +0100

    add persistent and in-memory auth methods

--- airbyte-integrations/connectors/destination-chroma/destination_chroma/chroma_client.py
+++ airbyte-integrations/connectors/destination-chroma/destination_chroma/chroma_client.py
@@ -3,30 +3,47 @@
 #
 
 import json
+import uuid
 from typing import Any, Mapping, MutableMapping
 
 import chromadb
 from chromadb.config import Settings
 
-from .utils import convert_to_valid_collection_name
+from .utils import convert_to_valid_collection_name, parse_id_schema, parse_embedding_schema
 
 
 class ChromaClient:
     def __init__(self, config: Mapping[str, Any], schema: Mapping[str, str]) -> None:
-        self.username = config.get('username')
-        self.password = config.get('password')
-        self.host = config.get('host')
-        self.port = config.get('port')
-        self.client = self.get_client()
+        self.config = config
         self.schema = schema
+        self.id_map = parse_id_schema(config.get('id_schema'))
+        self.embedding_map = parse_embedding_schema(config.get('embeddings_schema'))
+        self.client = self.get_client()
 
     @staticmethod
     def get_client(self):
 
-        settings = Settings(chroma_client_auth_provider="chromadb.auth.basic.BasicAuthClientProvider",chroma_client_auth_credentials=f"{self.username}:{self.password}")
+        auth_method = self.config['credentials']['auth_method']
 
-        client = chromadb.HttpClient(settings=settings, host=self.host, port=self.port)
+        if auth_method == 'persistent_client':
+            path = self.config['credentials']['path']
+            client = chromadb.PersistentClient(path=path)
+            return client
 
+        elif auth_method == 'http_client':
+            credentials = self.config.get('credentials')
+
+            username = credentials.get('username')
+            password = credentials.get('password')
+            host = credentials.get('host')
+            port = credentials.get('port')
+
+            settings = Settings(chroma_client_auth_provider="chromadb.auth.basic.BasicAuthClientProvider", chroma_client_auth_credentials=f"{username}:{password}")
+
+            client = chromadb.HttpClient(settings=settings, host=host, port=port)
+            return client
+
+        client = chromadb.Client()
         return client
     
     def delete_collection_data(self, stream_name: str):
@@ -41,6 +58,39 @@ class ChromaClient:
         pass
 
     def write_data(self, stream_name: str, record: MutableMapping):
+        record_id = self.get_record_id(stream_name=stream_name, record=record)
+        embedding = self.get_embedding(stream_name=stream_name, record=record)
+        record = self.prepare_record(stream_name=stream_name, record=record)
+        collection_name = convert_to_valid_collection_name(stream_name)
+
+        collection = self.client.get_collection(name=collection_name)
+        collection.add(
+            embeddings=embedding,
+            documents=[record], # TODO get specific records to add
+            ids=[record_id],
+            metadata=None # TODO add support for metadata
+        )
+
+    def get_record_id(self, stream_name: str, record: MutableMapping):
+        if self.id_map.get(stream_name, "") in record:
+            id_field_name = self.id_map.get(stream_name, "")
+            record_id = record.get(id_field_name)
+            del record[id_field_name]
+        else:
+            if "id" in record:
+                record_id = record.get("id")
+                del record["id"]
+            # Weaviate will throw an error if you try to store a field with name _id
+            elif "_id" in record:
+                record_id = record.get("_id")
+                del record["_id"]
+            else:
+                record_id = uuid.uuid4()
+        record_id = str(record_id)
+        return record_id
+
+    def prepare_record(self, stream_name: str, record: MutableMapping):
+   
         for k, v in record.items():
             if self.schema[stream_name].get(k, "") == "jsonify":
                 record[k] = json.dumps(v)
@@ -59,10 +109,12 @@ class ChromaClient:
             if isinstance(record[prop], list) and len(record[prop]) > 0 and isinstance(record[prop][0], dict):
                 record[prop] = json.dumps(record[prop])
 
-        collection_name = convert_to_valid_collection_name(stream_name)
-        collection = self.client.get_collection(name=collection_name)
-        collection.add(
-            documents=[record],
-            ids=[record.primary_key],
-            metadata=None
-        )
+        return record
+
+    def get_embedding(self, stream_name: str, record: MutableMapping):
+        if stream_name in self.embedding_map:
+            embedding_column_name = self.embedding_map.get(stream_name)
+            embedding = record.get(embedding_column_name)
+            del record[embedding_column_name]
+            return [embedding]
+        return None
--- airbyte-integrations/connectors/destination-chroma/destination_chroma/utils.py
+++ airbyte-integrations/connectors/destination-chroma/destination_chroma/utils.py
@@ -10,13 +10,17 @@ from airbyte_cdk.models import ConfiguredAirbyteCatalog
 def convert_to_valid_collection_name(stream_name):
     if is_valid_collection_name(stream_name):
         return stream_name
+    
     # Remove characters that are not lowercase letters, digits, dots, dashes, or underscores
     valid_chars = re.sub(r'[^a-z0-9._-]', '', stream_name)
+
     # Ensure the resulting name is within length constraints
     truncated_name = valid_chars[:63]
+
     # If the resulting name is too short, add some characters to meet the minimum length
     while len(truncated_name) < 3:
         truncated_name += 'x'
+        
     # Ensure the resulting name starts and ends with a lowercase letter or digit
     if truncated_name[0].isdigit():
         truncated_name = 'a' + truncated_name[1:]
@@ -57,3 +61,25 @@ def get_schema_from_catalog(configured_catalog: ConfiguredAirbyteCatalog) -> Map
                 stream_schema[k] = "jsonify"
         schema[stream.stream.name] = stream_schema
     return schema
+
+def parse_id_schema(id_schema_config: str) -> Mapping[str, str]:
+    id_schema = {}
+    if not id_schema_config:
+        return id_schema
+
+    id_schema_list = id_schema_config.replace(" ", "").split(",")
+    for schema_id in id_schema_list:
+        stream_name, id_field_name = schema_id.split(".")
+        id_schema[stream_name] = id_field_name
+    return id_schema
+
+def parse_embedding_schema(embeddings_config: str) -> Mapping[str, str]:
+    embeddings = {}
+    if not embeddings_config:
+        return embeddings
+
+    embeddings_list = embeddings_config.replace(" ", "").split(",")
+    for embedding in embeddings_list:
+        stream_name, embedding_column_name = embedding.split(".")
+        embeddings[stream_name] = embedding_column_name
+    return embeddings
