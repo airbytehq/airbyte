5d5ab1ccd01f1e469cef3e9f0e031c66ffe3e7af
benmoriceau <benoit@airbyte.io>
2023-09-11 15:58:41 -0700

    Fix duplicated condition

--- airbyte-integrations/bases/base-java/src/main/java/io/airbyte/integrations/destination_async/state/GlobalAsyncStateManager.java
+++ airbyte-integrations/bases/base-java/src/main/java/io/airbyte/integrations/destination_async/state/GlobalAsyncStateManager.java
@@ -263,8 +263,12 @@ public class GlobalAsyncStateManager {
    */
   private void closeState(final PartialAirbyteMessage message, final long sizeInBytes) {
     final StreamDescriptor resolvedDescriptor = extractStream(message).orElse(SENTINEL_GLOBAL_DESC);
-    if (streamToStateIdQ.containsKey(resolvedDescriptor) && stateIdToState.containsKey(streamToStateIdQ.get(resolvedDescriptor)) &&
-            stateIdToState.get(streamToStateIdQ.get(resolvedDescriptor)).getLeft().getSerialized().equals(message.getSerialized())) {
+    log.error("Resolved descriptor: " + resolvedDescriptor.toString());
+
+    if (streamToStateIdQ.containsKey(resolvedDescriptor)
+            && streamToStateIdQ.get(resolvedDescriptor).size() > 0
+            && stateIdToState.containsKey(streamToStateIdQ.get(resolvedDescriptor).getLast())
+            && stateIdToState.get(streamToStateIdQ.get(resolvedDescriptor).getLast()).getLeft().getSerialized().equals(message.getSerialized())) {
       log.error("Received Duplicated state");
     } else {
       stateIdToState.put(getStateId(resolvedDescriptor), ImmutablePair.of(message, sizeInBytes));
