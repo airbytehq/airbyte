9420def4fb3374529506afd201700aa514888408
subodh <subodh1810@gmail.com>
2023-09-11 21:57:40 +0530

    mysql-source-cdc: enable compression logic for schema history

--- airbyte-integrations/bases/debezium/src/main/java/io/airbyte/integrations/debezium/internals/mysql/MysqlCdcStateConstants.java
+++ airbyte-integrations/bases/debezium/src/main/java/io/airbyte/integrations/debezium/internals/mysql/MysqlCdcStateConstants.java
@@ -9,6 +9,6 @@ public class MysqlCdcStateConstants {
   public static final String MYSQL_CDC_OFFSET = "mysql_cdc_offset";
   public static final String MYSQL_DB_HISTORY = "mysql_db_history";
   public static final String IS_COMPRESSED = "is_compressed";
-  public static final boolean COMPRESSION_ENABLED = false;
+  public static final boolean COMPRESSION_ENABLED = true;
 
 }
