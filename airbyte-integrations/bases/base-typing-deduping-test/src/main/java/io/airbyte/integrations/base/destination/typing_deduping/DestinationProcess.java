e1aa831cf6f45ce5a046b9387c72a47038821001
Edward Gao <edward.gao@airbyte.io>
2023-09-19 15:12:05 -0700

    meh

--- airbyte-integrations/bases/base-typing-deduping-test/src/main/java/io/airbyte/integrations/base/destination/typing_deduping/DestinationProcess.java
+++ airbyte-integrations/bases/base-typing-deduping-test/src/main/java/io/airbyte/integrations/base/destination/typing_deduping/DestinationProcess.java
@@ -2,7 +2,6 @@ package io.airbyte.integrations.base.destination.typing_deduping;
 
 import static io.airbyte.integrations.base.destination.typing_deduping.BaseTypingDedupingTest.convertProtocolObject;
 
-import io.airbyte.commons.lang.Exceptions;
 import io.airbyte.protocol.models.v0.AirbyteMessage;
 import io.airbyte.workers.internal.AirbyteDestination;
 
@@ -10,12 +9,16 @@ public class DestinationProcess implements AutoCloseable {
 
   private final AirbyteDestination destination;
 
-  public DestinationProcess(AirbyteDestination destination) {
+  public DestinationProcess(final AirbyteDestination destination) {
     this.destination = destination;
   }
 
-  public void accept(AirbyteMessage message) {
-    Exceptions.toRuntime(() -> destination.accept(convertProtocolObject(message, io.airbyte.protocol.models.AirbyteMessage.class)));
+  public void accept(final AirbyteMessage message) {
+    try {
+      destination.accept(convertProtocolObject(message, io.airbyte.protocol.models.AirbyteMessage.class));
+    } catch (final Exception e) {
+      throw new RuntimeException(e);
+    }
   }
 
   @Override
