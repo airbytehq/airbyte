223da7b69e24f0142372f4a35e54c89311706945
Maxime Carbonneau-Leclerc <maxi297@users.noreply.github.com>
2023-09-21 07:51:42 -0400

    🐛 Source Facebook Marketing: assume start_date can be none (#30650)

--- airbyte-integrations/connectors/source-facebook-marketing/source_facebook_marketing/streams/streams.py
+++ airbyte-integrations/connectors/source-facebook-marketing/source_facebook_marketing/streams/streams.py
@@ -134,12 +134,22 @@ class Activities(FBMarketingIncrementalStream):
     def _state_filter(self, stream_state: Mapping[str, Any]) -> Mapping[str, Any]:
         """Additional filters associated with state if any set"""
         state_value = stream_state.get(self.cursor_field)
-        since = self._start_date if not state_value else pendulum.parse(state_value)
+        if stream_state:
+            since = pendulum.parse(state_value)
+        elif self._start_date:
+            since = self._start_date
+        else:
+            # if start_date is not specified then do not use date filters
+            return {}
 
         potentially_new_records_in_the_past = self._include_deleted and not stream_state.get("include_deleted", False)
         if potentially_new_records_in_the_past:
             self.logger.info(f"Ignoring bookmark for {self.name} because of enabled `include_deleted` option")
-            since = self._start_date
+            if self._start_date:
+                since = self._start_date
+            else:
+                # if start_date is not specified then do not use date filters
+                return {}
 
         return {"since": since.int_timestamp}
 
