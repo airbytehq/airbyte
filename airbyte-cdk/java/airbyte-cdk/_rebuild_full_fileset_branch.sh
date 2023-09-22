08d2ca2a6ea06bec1513e4546e1e47a9f49a786e
Aaron Steers <aj@airbyte.io>
2023-09-22 14:55:18 -0700

    don't automatically merge from master in script

--- airbyte-cdk/java/airbyte-cdk/_rebuild_full_fileset_branch.sh
+++ airbyte-cdk/java/airbyte-cdk/_rebuild_full_fileset_branch.sh
@@ -3,12 +3,12 @@
 # This script is used to rebuild the java-cdk-010-d2 branch from scratch.
 
 # Check with the use to confirm the action
-read -p "This script will delete and recreate the java-cdk-010-d2 branch. Are you sure? (y/n) " -n 1 -r
+read -p "This script will delete and recreate the java-cdk-010-d2 branch. You should merge in the latest from from master before running this. Are you sure? (y/n) " -n 1 -r
 
 # Switch to the base branch
 git checkout java-cdk-010-d
 # Make sure the base branch is up-to-date
-git merge origin/master
+# git merge origin/master
 # Delete and recreate the old branch locally
 git branch -D java-cdk-010-d2
 git checkout -b java-cdk-010-d2
