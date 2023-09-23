74408095226ca91122b81d28877560ff4090216b
Denys Davydov <davydov.den18@gmail.com>
2023-09-23 08:36:36 +0300

    debug in ci

--- airbyte-integrations/connectors/source-file/integration_tests/conftest.py
+++ airbyte-integrations/connectors/source-file/integration_tests/conftest.py
@@ -161,9 +161,12 @@ def ssh_service(move_sample_files_to_tmp, docker_client):
     # import paramiko
     print(container.attrs["NetworkSettings"])
     print(container.attrs["NetworkSettings"]["Networks"])
-    while not is_ssh_ready("localhost", 2222):
+    count = 0
+    while not is_ssh_ready("localhost", 2222) and count < 10:
         print('sleep')
         time.sleep(1)
+        count += 1
+
     # time.sleep(5)
     # import socket
     # s = socket.socket()
