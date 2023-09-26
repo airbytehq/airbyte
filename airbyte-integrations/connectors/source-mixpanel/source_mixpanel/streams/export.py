3dcd3a721678d0742cdbcb88591482de453a4259
Anatolii Yatsuk <tolikyatsuk@gmail.com>
2023-09-26 21:14:34 +0300

    Update tests

--- airbyte-integrations/connectors/source-mixpanel/source_mixpanel/streams/export.py
+++ airbyte-integrations/connectors/source-mixpanel/source_mixpanel/streams/export.py
@@ -75,7 +75,7 @@ class Export(DateSlicesMixin, IncrementalMixpanelStream):
      3 queries per second and 60 queries per hour.
     """
 
-    primary_key: Iterable[str] = ["distinct_id", "event_name", "event_time"]
+    primary_key: Iterable[str] = ["distinct_id", "event", "time"]
     cursor_field: str = "time"
 
     transformer = TypeTransformer(TransformConfig.DefaultSchemaNormalization)
