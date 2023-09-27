b02cb17e91c3d7414bcd18c4ad944b3e66ab9cce
Alexandre Girard <alexandre@airbyte.io>
2023-09-27 16:11:53 -0700

    fix

--- airbyte-cdk/python/airbyte_cdk/sources/streams/concurrent/partitions/partition_generator.py
+++ airbyte-cdk/python/airbyte_cdk/sources/streams/concurrent/partitions/partition_generator.py
@@ -12,12 +12,8 @@ class PartitionGenerator(ABC):
     @abstractmethod
     def generate(self, sync_mode: SyncMode) -> Iterable[Partition]:
         """
-                Generates partitions for a given sync mode.
-                :param sync_mode: SyncMode
-        <<<<<<< HEAD
-                :return: An iterable of partitions.
-        =======
-                :return: An iterable of partitions
-        >>>>>>> alex/abstract_stream
+        Generates partitions for a given sync mode.
+        :param sync_mode: SyncMode
+        :return: An iterable of partitions
         """
         pass
