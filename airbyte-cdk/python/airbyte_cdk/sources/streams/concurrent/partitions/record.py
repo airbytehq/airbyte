e813b34adaa88b703a2a3835b8ebe3cdff325a30
Alexandre Girard <alexandre@airbyte.io>
2023-09-27 16:03:20 -0700

    rename

--- airbyte-cdk/python/airbyte_cdk/sources/streams/concurrent/partitions/record.py
+++ airbyte-cdk/python/airbyte_cdk/sources/streams/concurrent/partitions/record.py
@@ -10,10 +10,10 @@ class Record:
     Represents a record read from a stream.
     """
 
-    def __init__(self, stream_data: Mapping[str, Any]):
-        self.stream_data = stream_data
+    def __init__(self, data: Mapping[str, Any]):
+        self.data = data
 
     def __eq__(self, other: Any) -> bool:
         if not isinstance(other, Record):
             return False
-        return self.stream_data == other.stream_data
+        return self.data == other.data
