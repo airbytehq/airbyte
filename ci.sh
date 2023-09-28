1acf7f3ce5e7b446196ae47fb73d1b664d615194
Aaron Steers <aj@airbyte.io>
2023-09-28 14:31:58 -0700

    updated to work with docker-in-docker

--- ci.sh
+++ ci.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
+set -e
 
 # Get the parent folder of this script
 export AIRBYTE_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-
 # Mount the repo a Python docker container with the script 
-docker build -t=airbyte-ci:dev ${AIRBYTE_ROOT}/airbyte-ci
-docker run -it -v ${AIRBYTE_ROOT}:/airbyte --env-file=.env --env=GCP_GSM_CREDENTIALS airbyte-ci:dev $@
+docker build -t=airbyte-ci:dev ${AIRBYTE_ROOT}/airbyte-ci --platform=linux/arm64
+
+# Run the airbyte-ci CLI
+docker run -it -v ${AIRBYTE_ROOT}:/airbyte -v /var/run/docker.sock:/var/run/docker.sock --env-file=.env --env=GCP_GSM_CREDENTIALS airbyte-ci:dev $@
