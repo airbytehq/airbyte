39f54e3cf8a613d25da54852ecaf689892f0a382
Rohit Chatterjee <rohit@rootbridgeservices.in>
2023-09-30 08:30:05 +0530

    check_connection also needs to pass form_fields_to_exclude to Application()

--- airbyte-integrations/connectors/source-commcare/source_commcare/source.py
+++ airbyte-integrations/connectors/source-commcare/source_commcare/source.py
@@ -14,6 +14,7 @@ from airbyte_cdk.sources.streams import IncrementalMixin, Stream
 from airbyte_cdk.sources.streams.http import HttpStream
 from airbyte_cdk.sources.streams.http.requests_native_auth import TokenAuthenticator
 
+
 # Basic full refresh stream
 class CommcareStream(HttpStream, ABC):
     def __init__(self, project_space, form_fields_to_exclude, **kwargs):
@@ -31,7 +32,7 @@ class CommcareStream(HttpStream, ABC):
     forms = set()
     last_form_date = None
     schemas = {}
-  
+
     @property
     def dateformat(self):
         return "%Y-%m-%dT%H:%M:%S.%f"
@@ -40,15 +41,15 @@ class CommcareStream(HttpStream, ABC):
         new_dict = {}
         for key, value in form.items():
             if key in self.form_fields_to_exclude:
-             continue
+                continue
             if any(key.startswith(prefix) for prefix in self.form_fields_to_exclude):
-             continue
+                continue
             if isinstance(value, dict):
-             new_dict[key] = self.scrubUnwantedFields(value)
+                new_dict[key] = self.scrubUnwantedFields(value)
             else:
                 new_dict[key] = value
         return new_dict
-       
+
     def next_page_token(self, response: requests.Response) -> Optional[Mapping[str, Any]]:
         try:
             # Server returns status 500 when there are no more rows.
@@ -62,7 +63,6 @@ class CommcareStream(HttpStream, ABC):
     def request_params(
         self, stream_state: Mapping[str, Any], stream_slice: Mapping[str, any] = None, next_page_token: Mapping[str, Any] = None
     ) -> MutableMapping[str, Any]:
-
         params = {"format": "json"}
         return params
 
@@ -85,7 +85,6 @@ class Application(CommcareStream):
     def request_params(
         self, stream_state: Mapping[str, Any], stream_slice: Mapping[str, any] = None, next_page_token: Mapping[str, Any] = None
     ) -> MutableMapping[str, Any]:
-
         params = {"format": "json", "extras": "true"}
         return params
 
@@ -129,7 +128,6 @@ class IncrementalStream(CommcareStream, IncrementalMixin):
     def request_params(
         self, stream_state: Mapping[str, Any], stream_slice: Mapping[str, any] = None, next_page_token: Mapping[str, Any] = None
     ) -> MutableMapping[str, Any]:
-
         params = {"format": "json"}
         if next_page_token:
             params.update(next_page_token)
@@ -173,7 +171,6 @@ class Case(IncrementalStream):
     def request_params(
         self, stream_state: Mapping[str, Any], stream_slice: Mapping[str, any] = None, next_page_token: Mapping[str, Any] = None
     ) -> MutableMapping[str, Any]:
-
         # start date is what we saved for forms
         # if self.cursor_field in self.state else (CommcareStream.last_form_date or self.initial_date)
         ix = self.state[self.cursor_field]
@@ -186,9 +183,9 @@ class Case(IncrementalStream):
         for record in super().read_records(*args, **kwargs):
             date_string = record[self.cursor_field]
             if "Z" in date_string:
-                 date_format = "%Y-%m-%dT%H:%M:%S.%fZ"  
+                date_format = "%Y-%m-%dT%H:%M:%S.%fZ"
             else:
-                 date_format = "%Y-%m-%dT%H:%M:%S.%f"
+                date_format = "%Y-%m-%dT%H:%M:%S.%f"
             found = False
             for f in record["xform_ids"]:
                 if f in CommcareStream.forms:
@@ -244,7 +241,6 @@ class Form(IncrementalStream):
     def request_params(
         self, stream_state: Mapping[str, Any], stream_slice: Mapping[str, any] = None, next_page_token: Mapping[str, Any] = None
     ) -> MutableMapping[str, Any]:
-
         # if self.cursor_field in self.state else self.initial_date
         ix = self.state[self.cursor_field]
         params = {
@@ -263,9 +259,9 @@ class Form(IncrementalStream):
         for record in super().read_records(*args, **kwargs):
             date_string = record[self.cursor_field]
             if "Z" in date_string:
-                 date_format = "%Y-%m-%dT%H:%M:%S.%fZ"  
+                date_format = "%Y-%m-%dT%H:%M:%S.%fZ"
             else:
-                 date_format = "%Y-%m-%dT%H:%M:%S.%f"
+                date_format = "%Y-%m-%dT%H:%M:%S.%f"
             self._cursor_value = datetime.strptime(date_string, date_format)
             CommcareStream.forms.add(record["id"])
             newform = self.scrubUnwantedFields(record)
@@ -280,18 +276,23 @@ class Form(IncrementalStream):
 
 # Source
 class SourceCommcare(AbstractSource):
-    
     def check_connection(self, logger, config) -> Tuple[bool, any]:
-        
         try:
             auth = TokenAuthenticator(config["api_key"], auth_method="ApiKey")
             args = {
                 "authenticator": auth,
             }
-            stream = Application(**{**args, "app_id": config["app_id"], "project_space": config["project_space"]}).check_availability(logger=logger)
+            stream = Application(
+                **{
+                    **args,
+                    "app_id": config["app_id"],
+                    "form_fields_to_exclude": config["form_fields_to_exclude"],
+                    "project_space": config["project_space"],
+                }
+            ).check_availability(logger=logger)
             return True, None
         except Exception as error:
-             return False, " Invalid apikey, project_space or app_id : "+str(error)
+            return False, " Invalid apikey, project_space or app_id : " + str(error)
 
     def base_schema(self):
         return {
@@ -305,16 +306,27 @@ class SourceCommcare(AbstractSource):
         args = {
             "authenticator": auth,
         }
-        appdata = Application(**{**args, "app_id": config["app_id"], "form_fields_to_exclude": config["form_fields_to_exclude"], "project_space": config["project_space"]}).read_records(
-            sync_mode=SyncMode.full_refresh
-        )
+        appdata = Application(
+            **{
+                **args,
+                "app_id": config["app_id"],
+                "form_fields_to_exclude": config["form_fields_to_exclude"],
+                "project_space": config["project_space"],
+            }
+        ).read_records(sync_mode=SyncMode.full_refresh)
 
         # Generate streams for forms, one per xmlns and one stream for cases.
         streams = self.generate_streams(args, config, appdata)
         return streams
 
     def generate_streams(self, args, config, appdata):
-        form_args = {"app_id": config["app_id"], "start_date": config["start_date"], "form_fields_to_exclude": config["form_fields_to_exclude"], "project_space": config["project_space"], **args}
+        form_args = {
+            "app_id": config["app_id"],
+            "start_date": config["start_date"],
+            "form_fields_to_exclude": config["form_fields_to_exclude"],
+            "project_space": config["project_space"],
+            **args,
+        }
         streams = []
         name2xmlns = {}
 
