fb4ac786c50cd4573e2bf6c2d2c9f6e48ff018c5
shoaibejaz <shoaibejazabc@gmail.com>
2023-10-02 21:47:16 +0500

    latets-changes

--- airbyte-webapp/src/pages/DestinationPage/pages/AllDestinationsPage/AllDestinationsPage.tsx
+++ airbyte-webapp/src/pages/DestinationPage/pages/AllDestinationsPage/AllDestinationsPage.tsx
@@ -1,23 +1,15 @@
 import { faPlus } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import React, { useCallback, useState } from "react";
 import { FormattedMessage } from "react-intl";
 import styled from "styled-components";
 
 import { Button, MainPageWithScroll } from "components";
 import HeadTitle from "components/HeadTitle";
-import { PageSize } from "components/PageSize";
 import PageTitle from "components/PageTitle";
-import { Pagination } from "components/Pagination";
-import { Separator } from "components/Separator";
 
-import { FilterConnectionRequestBody } from "core/request/DaspireClient";
 import { useTrackPage, PageTrackingCodes } from "hooks/services/Analytics";
-import { useConnectionFilterOptions, useFilteredConnectionList } from "hooks/services/useConnectionHook";
 import { useDestinationList } from "hooks/services/useDestinationHook";
-import { usePageConfig } from "hooks/services/usePageConfig";
 import useRouter from "hooks/useRouter";
-import { useCurrentWorkspace } from "services/workspaces/WorkspacesService";
 
 import DestinationsTable from "./components/DestinationsTable";
 import { RoutePaths } from "../../../routePaths";
@@ -40,65 +32,15 @@ const BtnText = styled.div`
   font-size: 16px;
   color: #ffffff;
 `;
-const Footer = styled.div`
-  width: 100%;
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  justify-content: center;
-`;
 
 const AllDestinationsPage: React.FC = () => {
-  const { push, query } = useRouter();
+  const { push } = useRouter();
   const { destinations } = useDestinationList();
-  const [pageConfig, updatePageSize] = usePageConfig();
-  const [currentPageSize, setCurrentPageSize] = useState<number>(pageConfig.connection.pageSize);
 
   useTrackPage(PageTrackingCodes.DESTINATION_LIST);
 
   const onCreateDestination = () => push(`${RoutePaths.SelectDestination}`);
-  const workspace = useCurrentWorkspace();
-  const { statusOptions, sourceOptions, destinationOptions } = useConnectionFilterOptions();
 
-  const initialFiltersState = {
-    workspaceId: workspace.workspaceId,
-    pageSize: currentPageSize,
-    pageCurrent: query.pageCurrent ? JSON.parse(query.pageCurrent) : 1,
-    status: statusOptions[0].value,
-    sourceDefinitionId: sourceOptions[0].value,
-    destinationDefinitionId: destinationOptions[0].value,
-  };
-
-  const [filters, setFilters] = useState<FilterConnectionRequestBody>(initialFiltersState);
-
-  const { connections, total, pageSize } = useFilteredConnectionList(filters);
-
-  const onSelectFilter = useCallback(
-    (
-      filterType: "pageCurrent" | "status" | "sourceDefinitionId" | "destinationDefinitionId" | "pageSize",
-      filterValue: number | string
-    ) => {
-      if (
-        filterType === "status" ||
-        filterType === "sourceDefinitionId" ||
-        filterType === "destinationDefinitionId" ||
-        filterType === "pageSize"
-      ) {
-        setFilters({ ...filters, [filterType]: filterValue, pageCurrent: 1 });
-      } else if (filterType === "pageCurrent") {
-        setFilters({ ...filters, [filterType]: filterValue as number });
-      }
-    },
-    [filters]
-  );
-  const onChangePageSize = useCallback(
-    (size: number) => {
-      setCurrentPageSize(size);
-      updatePageSize("connection", size);
-      onSelectFilter("pageSize", size);
-    },
-    [onSelectFilter]
-  );
   if (destinations.length === 0) {
     onCreateDestination();
     return null;
@@ -124,19 +66,7 @@ const AllDestinationsPage: React.FC = () => {
         />
       }
     >
-      <Separator height="10px" />
-      <PageSize currentPageSize={currentPageSize} totalPage={total / pageSize} onChange={onChangePageSize} />
-      <Separator height="10px" />
-      <DestinationsTable destinations={destinations} connections={connections} />
-      <Separator height="24px" />
-      <Footer>
-        <Pagination
-          pages={total / pageSize}
-          value={filters.pageCurrent}
-          onChange={(value: number) => onSelectFilter("pageCurrent", value)}
-        />
-      </Footer>
-      <Separator height="24px" />
+      <DestinationsTable destinations={destinations} />
     </MainPageWithScroll>
   );
 };
