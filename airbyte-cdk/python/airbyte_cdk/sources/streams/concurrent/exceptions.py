519e9fbebbd3f328671444d4e2f67f47b150d8c7
girarda <girarda@users.noreply.github.com>
2023-10-03 15:16:40 +0000

    Automated Commit - Formatting Changes

--- airbyte-cdk/python/airbyte_cdk/sources/streams/concurrent/exceptions.py
+++ airbyte-cdk/python/airbyte_cdk/sources/streams/concurrent/exceptions.py
@@ -1,6 +1,8 @@
 #
 # Copyright (c) 2023 Airbyte, Inc., all rights reserved.
 #
+
+
 class ExceptionWithDisplayMessage(Exception):
     def __init__(self, display_message: str, **kwargs):
         super().__init__(**kwargs)
