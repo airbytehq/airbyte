114decf8cfcc896843e8d297ae1dabc2297f4b48
Akash Kulkarni <akash@airbyte.io>
2023-10-03 14:02:25 -0700

    Create PostgresSourceRunner
    -Adds PostgresSourceRunner as a bridge to determine to run strict-encrypt/non-strict-encrypt

---
+++ airbyte-integrations/connectors/source-postgres/src/main/java/io/airbyte/integrations/source/postgres/PostgresSourceRunner.java
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) 2023 Airbyte, Inc., all rights reserved.
+ */
+
+package io.airbyte.integrations.source.postgres;
+
+import io.airbyte.cdk.integrations.base.adaptive.AdaptiveSourceRunner;
+
+public class PostgresSourceRunner {
+
+  public static void main(final String[] args) throws Exception {
+    AdaptiveSourceRunner.baseOnEnv()
+        .withOssSource(PostgresSource::sshWrappedSource)
+        .withCloudSource(PostgresSourceStrictEncrypt::new)
+        .run(args);
+  }
+
+}
