558b10e33a754c5a90e1b52757b2974a8623e87d
Roman Yermilov <roman.yermilov@globallogic.com>
2023-10-06 21:00:21 +0400

    Source Facebook Marketing: fix permission error

--- airbyte-integrations/connectors/source-facebook-marketing/unit_tests/test_errors.py
+++ airbyte-integrations/connectors/source-facebook-marketing/unit_tests/test_errors.py
@@ -103,6 +103,8 @@ CONFIG_ERRORS = [
         #      remove all permissions
         #      re-generate access token
         # Re-authenticate (for cloud) or refresh access token (for oss) and check if all required permissions are granted
+        # Example: One of possible reasons why this error happen is an attempt to access `owner` field:
+        #   GET /act_<account-id>?fields=<field-1>,owner,...<field-n>
     ),
     (
         "error_403_requires_permission",
