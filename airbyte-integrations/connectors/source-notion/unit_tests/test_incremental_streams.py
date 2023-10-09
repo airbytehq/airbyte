cfff846f67249de9a4a92ef16897691409659735
ChristoGrab <ChristoGrab@users.noreply.github.com>
2023-10-09 14:43:19 +0000

    Automated Commit - Formatting Changes

--- airbyte-integrations/connectors/source-notion/unit_tests/test_incremental_streams.py
+++ airbyte-integrations/connectors/source-notion/unit_tests/test_incremental_streams.py
@@ -223,7 +223,7 @@ def test_invalid_start_cursor(parent, requests_mock, caplog):
 
 # Tests for Comments stream
 def test_comments_path(comments):
-    assert comments.path() == 'comments'
+    assert comments.path() == "comments"
 
 
 def test_comments_request_params(comments):
@@ -231,16 +231,10 @@ def test_comments_request_params(comments):
     Test that the request_params function returns the correct parameters for the Comments endpoint
     """
     params = comments.request_params(
-        next_page_token=None, 
-        stream_slice={
-            'block_id': 'block1', 
-            'page_last_edited_time': '2021-01-01T00:00:00.000Z'
-        })
-    
-    assert params == {
-        'block_id': 'block1', 
-        'page_size': comments.page_size
-        }
+        next_page_token=None, stream_slice={"block_id": "block1", "page_last_edited_time": "2021-01-01T00:00:00.000Z"}
+    )
+
+    assert params == {"block_id": "block1", "page_size": comments.page_size}
 
 
 def test_comments_stream_slices(comments, requests_mock):
@@ -248,25 +242,22 @@ def test_comments_stream_slices(comments, requests_mock):
     Test that the stream_slices function returns the parent page ids as "block_id" and the last edited time as "page_last_edited_time"
     """
 
-    inputs = {
-        'sync_mode': SyncMode.incremental, 
-        'cursor_field': comments.cursor_field, 
-        'stream_state': {}
-    }
+    inputs = {"sync_mode": SyncMode.incremental, "cursor_field": comments.cursor_field, "stream_state": {}}
 
     requests_mock.post(
-        "https://api.notion.com/v1/search", 
+        "https://api.notion.com/v1/search",
         json={
-            "results": 
-            [
-                {"name": "page_1", "id": "id_1", "last_edited_time": "2021-01-01T00:00:00.000Z"}, 
-                {"name": "page_2", "id": "id_2", "last_edited_time": "2021-20-01T00:00:00.000Z"}
-            ], 
-            "next_cursor": None})
-    
+            "results": [
+                {"name": "page_1", "id": "id_1", "last_edited_time": "2021-01-01T00:00:00.000Z"},
+                {"name": "page_2", "id": "id_2", "last_edited_time": "2021-20-01T00:00:00.000Z"},
+            ],
+            "next_cursor": None,
+        },
+    )
+
     expected_stream_slice = [
-        {"block_id": "id_1", "page_last_edited_time": "2021-01-01T00:00:00.000Z"}, 
-        {"block_id": "id_2", "page_last_edited_time": "2021-20-01T00:00:00.000Z"}
+        {"block_id": "id_1", "page_last_edited_time": "2021-01-01T00:00:00.000Z"},
+        {"block_id": "id_2", "page_last_edited_time": "2021-20-01T00:00:00.000Z"},
     ]
 
     actual_stream_slices_list = list(comments.stream_slices(**inputs))
@@ -281,37 +272,62 @@ def test_comments_stream_slices(comments, requests_mock):
             {"block_id": "block_id_1", "page_last_edited_time": "2023-10-10T00:00:00.000Z"},
             {"page_last_edited_time": "2021-10-10T00:00:00.000Z"},
             [
-                {"id": "comment_id_1", "rich_text": [{"type": "text", "text": {"content": "I am the Alpha comment"}}], "last_edited_time": "2021-01-01T00:00:00.000Z"},
-                {"id": "comment_id_2", "rich_text": [{"type": "text", "text": {"content": "I am the Omega comment"}}], "last_edited_time": "2022-12-31T00:00:00.000Z"}
+                {
+                    "id": "comment_id_1",
+                    "rich_text": [{"type": "text", "text": {"content": "I am the Alpha comment"}}],
+                    "last_edited_time": "2021-01-01T00:00:00.000Z",
+                },
+                {
+                    "id": "comment_id_2",
+                    "rich_text": [{"type": "text", "text": {"content": "I am the Omega comment"}}],
+                    "last_edited_time": "2022-12-31T00:00:00.000Z",
+                },
             ],
             [
-                {"id": "comment_id_1", "rich_text": [{"type": "text", "text": {"content": "I am the Alpha comment"}}], "last_edited_time": "2021-01-01T00:00:00.000Z", "page_last_edited_time": "2023-10-10T00:00:00.000Z"},
-                {"id": "comment_id_2", "rich_text": [{"type": "text", "text": {"content": "I am the Omega comment"}}], "last_edited_time": "2022-12-31T00:00:00.000Z", "page_last_edited_time": "2023-10-10T00:00:00.000Z"}
-            ]
+                {
+                    "id": "comment_id_1",
+                    "rich_text": [{"type": "text", "text": {"content": "I am the Alpha comment"}}],
+                    "last_edited_time": "2021-01-01T00:00:00.000Z",
+                    "page_last_edited_time": "2023-10-10T00:00:00.000Z",
+                },
+                {
+                    "id": "comment_id_2",
+                    "rich_text": [{"type": "text", "text": {"content": "I am the Omega comment"}}],
+                    "last_edited_time": "2022-12-31T00:00:00.000Z",
+                    "page_last_edited_time": "2023-10-10T00:00:00.000Z",
+                },
+            ],
         ),
         # Test that comments with page_last_edited_time < stream_state are not replicated, regardless of each record's LMD
         (
             {"block_id": "block_id_2", "page_last_edited_time": "2021-01-01T00:00:00.000Z"},
             {"page_last_edited_time": "2022-20-20T00:00:00.000Z"},
             [
-                {"id": "comment_id_1", "rich_text": [{"type": "text", "text": {"content": "I will not be replicated"}}], "last_edited_time": "2021-10-30T00:00:00.000Z"},
-                {"id": "comment_id_2", "rich_text": [{"type": "text", "text": {"content": "I will also not be replicated"}}], "last_edited_time": "2023-01-01T00:00:00.000Z"}
+                {
+                    "id": "comment_id_1",
+                    "rich_text": [{"type": "text", "text": {"content": "I will not be replicated"}}],
+                    "last_edited_time": "2021-10-30T00:00:00.000Z",
+                },
+                {
+                    "id": "comment_id_2",
+                    "rich_text": [{"type": "text", "text": {"content": "I will also not be replicated"}}],
+                    "last_edited_time": "2023-01-01T00:00:00.000Z",
+                },
             ],
-            []
+            [],
         ),
-    ]
+    ],
 )
 def test_comments_read_records(comments, requests_mock, stream_slice, stream_state, mock_data, expected_records):
     inputs = {
-        "sync_mode": SyncMode.incremental, 
-        "cursor_field": comments.cursor_field, 
-        "stream_state": stream_state, 
-        "stream_slice": stream_slice
+        "sync_mode": SyncMode.incremental,
+        "cursor_field": comments.cursor_field,
+        "stream_state": stream_state,
+        "stream_slice": stream_slice,
     }
-    
+
     requests_mock.get(
-        f"https://api.notion.com/v1/comments?block_id={stream_slice['block_id']}", 
-        json={"results": mock_data, "next_cursor": None}
+        f"https://api.notion.com/v1/comments?block_id={stream_slice['block_id']}", json={"results": mock_data, "next_cursor": None}
     )
 
     response = list(comments.read_records(**inputs))
