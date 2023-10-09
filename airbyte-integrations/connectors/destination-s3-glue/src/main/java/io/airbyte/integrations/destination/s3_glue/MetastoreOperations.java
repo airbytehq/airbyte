96901828fe2913ae412c5805b581103667f29693
Mike Saffitz <mike@magnify.io>
2023-10-08 21:33:59 -0700

    Add Parquet support to S3 Glue Destination
    Adds Parquet support along with date format handling and handling for edge cases such as null arrays.

--- airbyte-integrations/connectors/destination-s3-glue/src/main/java/io/airbyte/integrations/destination/s3_glue/MetastoreOperations.java
+++ airbyte-integrations/connectors/destination-s3-glue/src/main/java/io/airbyte/integrations/destination/s3_glue/MetastoreOperations.java
@@ -5,12 +5,12 @@
 package io.airbyte.integrations.destination.s3_glue;
 
 import com.fasterxml.jackson.databind.JsonNode;
+import io.airbyte.cdk.integrations.destination.s3.S3DestinationConfig;
 
 // TODO (itaseskii) allow config based implementation of different metastores i.e Hive, Nessie, etc.
 public interface MetastoreOperations extends AutoCloseable {
 
-  // TODO (itaseskii) extend metadata with data format (json, avro, parquet)
-  void upsertTable(String databaseName, String tableName, String location, JsonNode jsonSchema, String serializationLibrary);
+  void upsertTable(String databaseName, String tableName, String location, JsonNode jsonSchema, String serializationLibrary, S3DestinationConfig s3DestinationConfig);
 
   void deleteTable(String databaseName, String tableName);
 
