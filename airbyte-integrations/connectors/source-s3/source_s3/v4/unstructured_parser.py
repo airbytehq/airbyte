8060e67a98cb1197c3ad9db04f1b2c83eb566810
flash1293 <flash1293@users.noreply.github.com>
2023-10-10 11:36:41 +0000

    Automated Commit - Formatting Changes

--- airbyte-integrations/connectors/source-s3/source_s3/v4/unstructured_parser.py
+++ airbyte-integrations/connectors/source-s3/source_s3/v4/unstructured_parser.py
@@ -1,8 +1,6 @@
-from io import IOBase
-from source_s3.v4.config import S3FileBasedStreamConfig
-from airbyte_cdk.sources.file_based.file_types.file_type_parser import FileTypeParser
 import json
 import logging
+from io import IOBase
 from typing import Any, Dict, Iterable, List, Mapping, Optional, Union
 
 from airbyte_cdk.sources.file_based.config.file_based_stream_config import FileBasedStreamConfig
@@ -11,8 +9,10 @@ from airbyte_cdk.sources.file_based.file_based_stream_reader import AbstractFile
 from airbyte_cdk.sources.file_based.file_types.file_type_parser import FileTypeParser
 from airbyte_cdk.sources.file_based.remote_file import RemoteFile
 from airbyte_cdk.sources.file_based.schema_helpers import PYTHON_TYPE_MAPPING, SchemaType, merge_schemas
+from source_s3.v4.config import S3FileBasedStreamConfig
+from unstructured.documents.elements import Element, Formula, ListItem, Title
 from unstructured.partition.auto import partition
-from unstructured.documents.elements import Element, Title, ListItem, Formula
+
 
 class UnstructuredParser(FileTypeParser):
 
@@ -43,14 +43,17 @@ class UnstructuredParser(FileTypeParser):
     ) -> Iterable[Dict[str, Any]]:
         with stream_reader.open_file(file, self.file_read_mode, None, logger) as fp:
             markdown = self.read_file(fp)
-            chunks = [markdown[i:i+self.MAX_SIZE_PER_CHUNK] for i in range(0, len(markdown), self.MAX_SIZE_PER_CHUNK)]
-            yield from [{
-                "content": chunk,
-                "chunk_number": i,
-                "no_of_chunks": len(chunks),
-                "id": f"{file.uri}_{i}",
-            } for i, chunk in enumerate(chunks)]
-    
+            chunks = [markdown[i : i + self.MAX_SIZE_PER_CHUNK] for i in range(0, len(markdown), self.MAX_SIZE_PER_CHUNK)]
+            yield from [
+                {
+                    "content": chunk,
+                    "chunk_number": i,
+                    "no_of_chunks": len(chunks),
+                    "id": f"{file.uri}_{i}",
+                }
+                for i, chunk in enumerate(chunks)
+            ]
+
     def read_file(self, file: IOBase) -> str:
         # set name to none, otherwise unstructured will try to get the modified date from the local file system
         file_name = file.name
@@ -61,7 +64,7 @@ class UnstructuredParser(FileTypeParser):
 
     def render_markdown(self, elements: List[Element]) -> str:
         return "\n\n".join([self.convert_to_markdown(el) for el in elements])
-    
+
     def convert_to_markdown(self, el: Element) -> str:
         if type(el) == Title:
             return f"# {el.text}"
