5b806a84bf78f3f8a7c903f6c1a86f21f12038d7
Augustin <augustin@airbyte.io>
2023-10-11 16:06:58 +0200

    Revert "[airbyte-ci] implement pre/post build hooks (#30526)"
    This reverts commit 950ff920a19bb347aeea8d2afd89d2975c743c24.

--- airbyte-ci/connectors/pipelines/tests/test_builds/test_python_connectors.py
+++ airbyte-ci/connectors/pipelines/tests/test_builds/test_python_connectors.py
@@ -2,11 +2,9 @@
 # Copyright (c) 2023 Airbyte, Inc., all rights reserved.
 #
 
-from pathlib import Path
-
 import pytest
 from pipelines.bases import StepStatus
-from pipelines.builds import build_customization, python_connectors
+from pipelines.builds import python_connectors
 from pipelines.contexts import ConnectorContext
 
 pytestmark = [
@@ -30,41 +28,17 @@ class TestBuildConnectorImage:
         return test_context
 
     @pytest.fixture
-    def connector_with_base_image_no_build_customization(self, all_connectors):
+    def connector_with_base_image(self, all_connectors):
         for connector in all_connectors:
             if connector.metadata and connector.metadata.get("connectorBuildOptions", {}).get("baseImage"):
-                if not (connector.code_directory / "build_customization.py").exists():
-                    return connector
+                return connector
         pytest.skip("No connector with a connectorBuildOptions.baseImage metadata found")
 
     @pytest.fixture
-    def connector_with_base_image_with_build_customization(self, connector_with_base_image_no_build_customization):
-        dummy_build_customization = (Path(__file__).parent / "dummy_build_customization.py").read_text()
-        (connector_with_base_image_no_build_customization.code_directory / "build_customization.py").write_text(dummy_build_customization)
-        yield connector_with_base_image_no_build_customization
-        (connector_with_base_image_no_build_customization.code_directory / "build_customization.py").unlink()
-
-    @pytest.fixture
-    def test_context_with_real_connector_using_base_image(self, connector_with_base_image_no_build_customization, dagger_client):
+    def test_context_with_real_connector_using_base_image(self, connector_with_base_image, dagger_client):
         context = ConnectorContext(
             pipeline_name="test build",
-            connector=connector_with_base_image_no_build_customization,
-            git_branch="test",
-            git_revision="test",
-            report_output_prefix="test",
-            is_local=True,
-            use_remote_secrets=True,
-        )
-        context.dagger_client = dagger_client
-        return context
-
-    @pytest.fixture
-    def test_context_with_real_connector_using_base_image_with_build_customization(
-        self, connector_with_base_image_with_build_customization, dagger_client
-    ):
-        context = ConnectorContext(
-            pipeline_name="test build",
-            connector=connector_with_base_image_with_build_customization,
+            connector=connector_with_base_image,
             git_branch="test",
             git_revision="test",
             report_output_prefix="test",
@@ -113,11 +87,9 @@ class TestBuildConnectorImage:
         step_result = await step._run()
         step_result.status is StepStatus.SUCCESS
         built_container = step_result.output_artifact[current_platform]
-        assert await built_container.env_variable("AIRBYTE_ENTRYPOINT") == " ".join(
-            build_customization.get_entrypoint(step.context.connector)
-        )
+        assert await built_container.env_variable("AIRBYTE_ENTRYPOINT") == " ".join(step.DEFAULT_ENTRYPOINT)
         assert await built_container.workdir() == step.PATH_TO_INTEGRATION_CODE
-        assert await built_container.entrypoint() == build_customization.get_entrypoint(step.context.connector)
+        assert await built_container.entrypoint() == step.DEFAULT_ENTRYPOINT
         assert (
             await built_container.label("io.airbyte.version")
             == test_context_with_real_connector_using_base_image.connector.metadata["dockerImageTag"]
@@ -126,18 +98,6 @@ class TestBuildConnectorImage:
             await built_container.label("io.airbyte.name")
             == test_context_with_real_connector_using_base_image.connector.metadata["dockerRepository"]
         )
-
-    async def test_building_from_base_image_with_customization_for_real(
-        self, test_context_with_real_connector_using_base_image_with_build_customization, current_platform
-    ):
-        step = python_connectors.BuildConnectorImages(
-            test_context_with_real_connector_using_base_image_with_build_customization, current_platform
-        )
-        step_result = await step._run()
-        step_result.status is StepStatus.SUCCESS
-        built_container = step_result.output_artifact[current_platform]
-        assert await built_container.env_variable("MY_PRE_BUILD_ENV_VAR") == "my_pre_build_env_var_value"
-        assert await built_container.env_variable("MY_POST_BUILD_ENV_VAR") == "my_post_build_env_var_value"
 
     async def test__run_using_base_dockerfile_with_mocks(self, mocker, test_context_with_connector_without_base_image, current_platform):
         container_built_from_dockerfile = mocker.AsyncMock()
