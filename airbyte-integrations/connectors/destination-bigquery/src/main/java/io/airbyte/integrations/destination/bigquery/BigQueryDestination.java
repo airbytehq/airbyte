aa431a741ae38c6427fdf30179c3886d856e474b
benmoriceau <benoit@airbyte.io>
2023-10-10 17:27:47 -0700

    Restore sleep

--- airbyte-integrations/connectors/destination-bigquery/src/main/java/io/airbyte/integrations/destination/bigquery/BigQueryDestination.java
+++ airbyte-integrations/connectors/destination-bigquery/src/main/java/io/airbyte/integrations/destination/bigquery/BigQueryDestination.java
@@ -393,6 +393,7 @@ public class BigQueryDestination extends BaseConnector implements Destination {
         },
         (hasFailed) -> {
           try {
+            Thread.sleep(30 * 1000);
             typerDeduper.typeAndDedupe();
             typerDeduper.commitFinalTables();
             typerDeduper.cleanup();
