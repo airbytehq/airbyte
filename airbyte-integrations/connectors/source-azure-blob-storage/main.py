688b6350a8527f27ae43537aa602bafff90ae6d9
Denys Davydov <davydov.den18@gmail.com>
2023-10-12 23:02:47 +0300

    update cat

--- airbyte-integrations/connectors/source-azure-blob-storage/main.py
+++ airbyte-integrations/connectors/source-azure-blob-storage/main.py
@@ -2,14 +2,33 @@
 # Copyright (c) 2023 Airbyte, Inc., all rights reserved.
 #
 
-
 import sys
+import traceback
+from datetime import datetime
+
+from source_azure_blob_storage import Config, SourceAzureBlobStorage, SourceAzureBlobStorageStreamReader
 
 from airbyte_cdk.entrypoint import AirbyteEntrypoint, launch
-from source_azure_blob_storage import Config, SourceAzureBlobStorage, SourceAzureBlobStorageStreamReader
+from airbyte_cdk.models import AirbyteErrorTraceMessage, AirbyteMessage, AirbyteTraceMessage, TraceType, Type
+
 
 if __name__ == "__main__":
     args = sys.argv[1:]
     catalog_path = AirbyteEntrypoint.extract_catalog(args)
     source = SourceAzureBlobStorage(SourceAzureBlobStorageStreamReader(), Config, catalog_path)
-    launch(source, args)
+    try:
+        launch(source, args)
+    except KeyError:
+        print(
+            AirbyteMessage(
+                type=Type.TRACE,
+                trace=AirbyteTraceMessage(
+                    type=TraceType.ERROR,
+                    emitted_at=int(datetime.now().timestamp() * 1000),
+                    error=AirbyteErrorTraceMessage(
+                        message="Error starting the sync. This could be due to an invalid configuration or catalog. Please contact Support for assistance.",
+                        stack_trace=traceback.format_exc(),
+                    ),
+                ),
+            ).json()
+        )
