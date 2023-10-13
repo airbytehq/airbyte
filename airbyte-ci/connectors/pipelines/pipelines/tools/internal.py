038aa604a2c3d886f1e3624058ac333f09f3d0b5
Ben Church <ben@airbyte.io>
2023-10-12 18:16:07 -0700

    remove metadata test commands

--- airbyte-ci/connectors/pipelines/pipelines/tools/internal.py
+++ airbyte-ci/connectors/pipelines/pipelines/tools/internal.py
@@ -8,4 +8,6 @@ from enum import Enum
 class INTERNAL_TOOL_PATHS(str, Enum):
     CI_CREDENTIALS = "airbyte-ci/connectors/ci_credentials"
     CONNECTOR_OPS = "airbyte-ci/connectors/connector_ops"
+    METADATA_ROOT = "airbyte-ci/connectors/metadata_service"
     METADATA_SERVICE = "airbyte-ci/connectors/metadata_service/lib"
+    METADATA_ORCHESTRATOR = "airbyte-ci/connectors/metadata_service/orchestrator"
