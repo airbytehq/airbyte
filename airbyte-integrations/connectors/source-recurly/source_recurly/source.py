8cd6e98c6c3369b87d87e0500a81fa5a2ba9b304
Aries Thio <aries.thio@hotmail.com>
2023-10-13 17:28:40 +0200

    Migrate to Low-code CDK

--- airbyte-integrations/connectors/source-recurly/source_recurly/source.py
+++ airbyte-integrations/connectors/source-recurly/source_recurly/source.py
@@ -12,6 +12,6 @@ WARNING: Do not modify this file.
 
 
 # Declarative Source
-class SourceRecurly(YamlDeclarativeSource):
+class SourceFreshdesk(YamlDeclarativeSource):
     def __init__(self):
         super().__init__(**{"path_to_yaml": "manifest.yaml"})
