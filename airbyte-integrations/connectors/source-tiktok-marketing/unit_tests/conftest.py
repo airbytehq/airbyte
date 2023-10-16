528d2c9cd028d0cb877149d8c49aa24b677347d0
Denys Davydov <davydov.den18@gmail.com>
2023-10-16 14:29:49 +0300

    fix env path

--- airbyte-integrations/connectors/source-tiktok-marketing/unit_tests/conftest.py
+++ airbyte-integrations/connectors/source-tiktok-marketing/unit_tests/conftest.py
@@ -1,4 +1,9 @@
+import os
+
 import pytest
+from airbyte_cdk.utils.constants import ENV_REQUEST_CACHE_PATH
+
+os.environ[ENV_REQUEST_CACHE_PATH] = ENV_REQUEST_CACHE_PATH
 
 
 @pytest.fixture(autouse=True)
