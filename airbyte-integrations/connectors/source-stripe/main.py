9c038a3743190f60782d1a210c7d4c606a868170
Alexandre Girard <alexandre@airbyte.io>
2023-10-17 17:47:13 -0700

    reset

--- airbyte-integrations/connectors/source-stripe/main.py
+++ airbyte-integrations/connectors/source-stripe/main.py
@@ -5,12 +5,9 @@
 
 import sys
 
-from airbyte_cdk.entrypoint import AirbyteEntrypoint, launch
+from airbyte_cdk.entrypoint import launch
 from source_stripe import SourceStripe
 
 if __name__ == "__main__":
-    args = sys.argv[1:]
-    state = AirbyteEntrypoint.extract_state(args)
-    use_concurrent_cdk = state is None
-    source = SourceStripe(use_concurrent_cdk=use_concurrent_cdk)
-    launch(source, args)
+    source = SourceStripe()
+    launch(source, sys.argv[1:])
