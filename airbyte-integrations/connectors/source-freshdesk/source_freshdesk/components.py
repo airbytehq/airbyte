f236d759d3d9c739107a73879416f4f6116cbcc3
Mal Hancock <mallory@archangelic.space>
2023-10-19 10:48:56 -0700

    remove unit tests, formatting, update expected records

--- airbyte-integrations/connectors/source-freshdesk/source_freshdesk/components.py
+++ airbyte-integrations/connectors/source-freshdesk/source_freshdesk/components.py
@@ -2,22 +2,21 @@
 # Copyright (c) 2023 Airbyte, Inc., all rights reserved.
 #
 
+import re
 from dataclasses import dataclass
 from typing import Any, List, Mapping, MutableMapping, Optional, Tuple
-
-import re
-import requests
 from urllib import parse
 
+import requests
 from airbyte_cdk.sources.declarative.requesters.http_requester import HttpRequester
 from airbyte_cdk.sources.declarative.requesters.paginators.strategies.cursor_pagination_strategy import CursorPaginationStrategy
-from airbyte_cdk.sources.declarative.types import Record, StreamState, StreamSlice
+from airbyte_cdk.sources.declarative.types import Record, StreamSlice, StreamState
 
 
 @dataclass
 class FreshDeskRequester(HttpRequester):
     NEXT_PAGE_TOKEN_FIELD_NAME = "next_page_token"
-    
+
     def __post_init__(self, parameters: Mapping[str, Any]):
         super(FreshDeskRequester, self).__post_init__(parameters)
 
@@ -38,18 +37,18 @@ class FreshDeskRequester(HttpRequester):
         page = next_page_token and next_page_token[self.NEXT_PAGE_TOKEN_FIELD_NAME]
         if self.name == "tickets":
             page, last_updated = page if page else (None, None)
-            if (last_updated is not None):
+            if last_updated is not None:
                 params["updated_since"] = last_updated
                 params.pop("page")
-            
+
         return params
-    
+
     # We are using an LRU cache in should_retry() method which requires all incoming arguments (including self) to be hashable.
     # Dataclasses by default are not hashable, so we need to define __hash__(). Alternatively, we can set @dataclass(frozen=True),
     # but this has a cascading effect where all dataclass fields must also be set to frozen.
     def __hash__(self):
         return hash(tuple(self.__dict__))
-    
+
 
 class TicketsPaginationStrategy(CursorPaginationStrategy):
     """
@@ -59,16 +58,16 @@ class TicketsPaginationStrategy(CursorPaginationStrategy):
     def __post_init__(self, parameters: Mapping[str, Any]):
         super().__post_init__(parameters=parameters)
         self.ticket_paginate_limit = 300
-    
+
     def next_page_token(self, response: requests.Response, last_records: List[Record]) -> Optional[Tuple[Optional[int], Optional[int]]]:
         """
         This block extends Incremental stream to overcome '300 page' server error.
         Since the Ticket endpoint has a 300 page pagination limit, after 300 pages, update the parameters with
         query using 'updated_since' = last_record, if there is more data remaining.
-        """ 
+        """
         next_token = super().next_page_token(response, last_records)
         last_record_updated = None
-        
+
         if next_token:
             link_regex = re.compile(r'<(.*?)>;\s*rel="next"')
             link_header = response.headers.get("Link")
