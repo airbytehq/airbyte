8bd6f63bc979fcd567f96180d2bb8048d84af9ee
Martin Hwasser <hwaxxer@users.noreply.github.com>
2023-10-19 15:37:47 +0200

    ✨ vector_based_cdk: Add option to rename field names (#31524)
    Co-authored-by: Joe Reuter <joe@airbyte.io>
    Co-authored-by: flash1293 <flash1293@users.noreply.github.com>

--- airbyte-cdk/python/airbyte_cdk/destinations/vector_db_based/document_processor.py
+++ airbyte-cdk/python/airbyte_cdk/destinations/vector_db_based/document_processor.py
@@ -12,7 +12,7 @@ from airbyte_cdk.destinations.vector_db_based.config import ProcessingConfigMode
 from airbyte_cdk.models import AirbyteRecordMessage, AirbyteStream, ConfiguredAirbyteCatalog, ConfiguredAirbyteStream, DestinationSyncMode
 from airbyte_cdk.utils.traced_exception import AirbyteTracedException, FailureType
 from langchain.document_loaders.base import Document
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.text_splitter import Language, RecursiveCharacterTextSplitter
 from langchain.utils import stringify_dict
 
 METADATA_STREAM_FIELD = "_ab_stream"
@@ -60,7 +60,9 @@ class DocumentProcessor:
                     return f"Invalid separator: {s}. Separator needs to be a valid JSON string using double quotes."
         return None
 
-    def _get_text_splitter(self, chunk_size: int, chunk_overlap: int, splitter_config: Optional[TextSplitterConfigModel]):
+    def _get_text_splitter(
+        self, chunk_size: int, chunk_overlap: int, splitter_config: Optional[TextSplitterConfigModel]
+    ) -> RecursiveCharacterTextSplitter:
         if splitter_config is None:
             splitter_config = SeparatorSplitterConfigModel(mode="separator")
         if splitter_config.mode == "separator":
@@ -82,7 +84,7 @@ class DocumentProcessor:
             return RecursiveCharacterTextSplitter.from_tiktoken_encoder(
                 chunk_size=chunk_size,
                 chunk_overlap=chunk_overlap,
-                separators=RecursiveCharacterTextSplitter.get_separators_for_language(splitter_config.language),
+                separators=RecursiveCharacterTextSplitter.get_separators_for_language(Language(splitter_config.language)),
             )
 
     def __init__(self, config: ProcessingConfigModel, catalog: ConfiguredAirbyteCatalog):
@@ -91,6 +93,7 @@ class DocumentProcessor:
         self.splitter = self._get_text_splitter(config.chunk_size, config.chunk_overlap, config.text_splitter)
         self.text_fields = config.text_fields
         self.metadata_fields = config.metadata_fields
+        self.field_name_mappings = config.field_name_mappings
         self.logger = logging.getLogger("airbyte.document_processor")
 
     def _stream_identifier(self, stream: Union[AirbyteStream, AirbyteRecordMessage]) -> str:
@@ -137,7 +140,7 @@ class DocumentProcessor:
                     relevant_fields[field] = values if len(values) > 1 else values[0]
         else:
             relevant_fields = record.data
-        return relevant_fields
+        return self._remap_field_names(relevant_fields)
 
     def _extract_metadata(self, record: AirbyteRecordMessage) -> Dict[str, Any]:
         metadata = self._extract_relevant_fields(record, self.metadata_fields)
@@ -159,5 +162,16 @@ class DocumentProcessor:
         return "_".join(primary_key)
 
     def _split_document(self, doc: Document) -> List[Document]:
-        chunks = self.splitter.split_documents([doc])
+        chunks: List[Document] = self.splitter.split_documents([doc])
         return chunks
+
+    def _remap_field_names(self, fields: Dict[str, Any]) -> Dict[str, Any]:
+        if not self.field_name_mappings:
+            return fields
+
+        new_fields = fields.copy()
+        for mapping in self.field_name_mappings:
+            if mapping.from_field in new_fields:
+                new_fields[mapping.to_field] = new_fields.pop(mapping.from_field)
+
+        return new_fields
