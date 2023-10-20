aedc46015bacb8ba53d6ee07e6704cb7429b3d97
Denys Davydov <davydov.den18@gmail.com>
2023-10-20 11:17:45 +0300

    :sparkles: Source TikTok Marketing: add new streams (#31610)
    Co-authored-by: davydov-d <davydov-d@users.noreply.github.com>

--- airbyte-integrations/connectors/source-tiktok-marketing/unit_tests/unit_test.py
+++ airbyte-integrations/connectors/source-tiktok-marketing/unit_tests/unit_test.py
@@ -134,8 +134,8 @@ def test_random_items(prepared_prod_args):
 @pytest.mark.parametrize(
     "config, stream_len",
     [
-        (PROD_CONFIG_FILE, 30),
-        (SANDBOX_CONFIG_FILE, 22),
+        (PROD_CONFIG_FILE, 35),
+        (SANDBOX_CONFIG_FILE, 27),
     ],
 )
 def test_source_streams(config, stream_len):
