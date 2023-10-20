5dce44fe44b67e9b24a3a204c0a99c32279c1e35
flash1293 <flash1293@users.noreply.github.com>
2023-10-20 12:55:46 +0000

    Automated Commit - Formatting Changes

--- airbyte-integrations/connectors/destination-weaviate/unit_tests/indexer_test.py
+++ airbyte-integrations/connectors/destination-weaviate/unit_tests/indexer_test.py
@@ -2,8 +2,8 @@
 # Copyright (c) 2023 Airbyte, Inc., all rights reserved.
 #
 
-from collections import defaultdict
 import unittest
+from collections import defaultdict
 from unittest.mock import ANY, Mock, call, patch
 
 from airbyte_cdk.destinations.vector_db_based.document_processor import Chunk
