d32e9a86ff8ac40de13e60465118c5b553f76b69
Alexandre Girard <alexandre@airbyte.io>
2023-10-24 11:27:39 -0700

    comment

--- airbyte-integrations/connectors/source-stripe/main.py
+++ airbyte-integrations/connectors/source-stripe/main.py
@@ -10,6 +10,7 @@ from source_stripe import SourceStripe
 
 if __name__ == "__main__":
     args = sys.argv[1:]
+    # Use the presence of a state file as a proxy for whether we're running in full refresh
     state = AirbyteEntrypoint.extract_state(args)
     use_concurrent_cdk = state is None
     source = SourceStripe(use_concurrent_cdk=use_concurrent_cdk)
