0a61de1d46903eb90b56bf43c6eb131bc69ca193
Artem Inzhyyants <artem.inzhyyants@gmail.com>
2023-10-24 12:04:51 +0200

    Source Bing Ads: update unit tests

--- airbyte-integrations/connectors/source-bing-ads/unit_tests/test_source.py
+++ airbyte-integrations/connectors/source-bing-ads/unit_tests/test_source.py
@@ -8,7 +8,8 @@ from unittest.mock import patch
 import pytest
 import source_bing_ads
 from airbyte_cdk.models import SyncMode
-from source_bing_ads.streams import AccountPerformanceReportMonthly, Accounts, AdGroups, Ads, Campaigns, SourceBingAds
+from source_bing_ads.source import SourceBingAds
+from source_bing_ads.streams import AccountPerformanceReportMonthly, Accounts, AdGroups, Ads, Campaigns
 
 
 @pytest.fixture(name="config")
