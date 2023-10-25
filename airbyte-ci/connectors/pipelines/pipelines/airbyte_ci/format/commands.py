dce7dc1b9df425fa8f43b7b26ac3bf0cef9cba72
erohmensing <erohmensing@gmail.com>
2023-10-25 15:51:53 -0500

    allow invoking without subcommand to call all commands

--- airbyte-ci/connectors/pipelines/pipelines/airbyte_ci/format/commands.py
+++ airbyte-ci/connectors/pipelines/pipelines/airbyte_ci/format/commands.py
@@ -14,13 +14,26 @@ from pipelines.cli.lazy_group import LazyGroup
     cls=LazyGroup,
     help="Commands related to formatting.",
     lazy_subcommands={
-        "python": "pipelines.airbyte_ci.format.python.commands.python",
         "java": "pipelines.airbyte_ci.format.java.commands.java",
         "js": "pipelines.airbyte_ci.format.js.commands.js",
         "license": "pipelines.airbyte_ci.format.license.commands.license",
+        "python": "pipelines.airbyte_ci.format.python.commands.python",
     },
+    invoke_without_command=True,
 )
 @click.option("--fix/--check", type=bool, default=None, help="Whether to automatically fix any formatting issues detected.  [required]")
 @click.pass_context
-def format(ctx: click.Context, fix: bool):
+async def format(ctx: click.Context, fix: bool):
+    from pipelines.airbyte_ci.format.java.commands import java
+    from pipelines.airbyte_ci.format.js.commands import js
+    from pipelines.airbyte_ci.format.license.commands import license
+    from pipelines.airbyte_ci.format.python.commands import python
+
     ctx.obj["fix_formatting"] = fix
+
+    if ctx.invoked_subcommand is None:
+        # TODO: ctx.forward should forward the fix commands to the subcommands
+        await ctx.invoke(license)
+        await ctx.invoke(java)
+        await ctx.invoke(js)
+        await ctx.invoke(python)
