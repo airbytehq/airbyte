cbede4ecf14f30a2d017fb5429146829aadd6287
marcosmarxm <marcosmarxm@gmail.com>
2023-10-25 19:21:16 -0300

    fix tests

--- airbyte-integrations/connectors/source-qualaroo/source_qualaroo/components.py
+++ airbyte-integrations/connectors/source-qualaroo/source_qualaroo/components.py
@@ -2,10 +2,11 @@
 # Copyright (c) 2023 Airbyte, Inc., all rights reserved.
 #
 
-import requests
-from typing import Any, List, Mapping
 from base64 import b64encode
 from dataclasses import dataclass
+from typing import Any, List, Mapping
+
+import requests
 from airbyte_cdk.sources.declarative.auth.token import BasicHttpAuthenticator
 from airbyte_cdk.sources.declarative.extractors.record_extractor import RecordExtractor
 
@@ -23,12 +24,11 @@ class CustomAuthenticator(BasicHttpAuthenticator):
 
 
 class CustomExtractor(RecordExtractor):
-
     def extract_records(self, response: requests.Response, **kwargs) -> List[Mapping[str, Any]]:
 
-        extracted=[]
-        for rec in response.json():
-            if "answered_questions" in rec:
-                rec["answered_questions"] = list(rec["answered_questions"].values())
-            extracted.append(rec)
+        extracted = []
+        for record in response.json():
+            if "answered_questions" in record:
+                record["answered_questions"] = list(record["answered_questions"].values())
+            extracted.append(record)
         return extracted
--- airbyte-integrations/connectors/source-qualaroo/unit_tests/test_components.py
+++ airbyte-integrations/connectors/source-qualaroo/unit_tests/test_components.py
@@ -2,9 +2,10 @@
 # Copyright (c) 2023 Airbyte, Inc., all rights reserved.
 #
 
-from source_qualaroo.components import CustomAuthenticator, CustomExtractor
-import requests
 import json
+
+import requests
+from source_qualaroo.components import CustomAuthenticator, CustomExtractor
 
 
 def test_token_generation():
@@ -14,19 +15,19 @@ def test_token_generation():
     token = authenticator.token
     expected_token = "Basic ZXhhbXBsZUBnbWFpbC5jb206YXBpX2tleQ=="
     assert expected_token == token
-    
+
+
 def test_extract_records_with_answered_questions():
 
-        response_data = [
-            {"id": 1, "answered_questions": {"q1": "A1", "q2": "A2"}},
-            {"id": 2, "answered_questions": {"q3": "A3"}},
-        ]
-        response = requests.Response()
-        response._content = json.dumps(response_data).encode('utf-8')
-        extracted_records = CustomExtractor().extract_records(response)
-
-        assert (len(extracted_records), 2)
-        assert (extracted_records[0]["id"], 1)
-        assert (extracted_records[0]["answered_questions"], ["A1", "A2"])
-        assert (extracted_records[1]["id"], 2)
-        assert (extracted_records[1]["answered_questions"], ["A3"])
+    response_data = [
+        {"id": 1, "answered_questions": {"q1": "A1", "q2": "A2"}},
+        {"id": 2, "answered_questions": {"q3": "A3"}},
+    ]
+    response = requests.Response()
+    response._content = json.dumps(response_data).encode("utf-8")
+    extracted_records = CustomExtractor().extract_records(response)
+    expected_records = [
+        {"id": 1, "answered_questions": ["A1", "A2"]},
+        {"id": 2, "answered_questions": ["A3"]}
+    ]
+    assert expected_records == extracted_records
