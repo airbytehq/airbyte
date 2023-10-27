8888a1bcdbca819322862c4a6e54c47cdaec13a0
Brian Leonard <brian.leonard@airbyte.io>
2023-10-26 17:07:53 -0700

    Copying things over

--- airbyte-integrations/connectors/destination-snowflake-bulk/src/main/java/io/airbyte/integrations/destination/snowflake_bulk/SnowflakeBulkDestination.java
+++ airbyte-integrations/connectors/destination-snowflake-bulk/src/main/java/io/airbyte/integrations/destination/snowflake_bulk/SnowflakeBulkDestination.java
@@ -5,32 +5,88 @@
 package io.airbyte.integrations.destination.snowflake_bulk;
 
 import com.fasterxml.jackson.databind.JsonNode;
-import io.airbyte.cdk.integrations.BaseConnector;
+import io.airbyte.cdk.db.factory.DataSourceFactory;
+import io.airbyte.cdk.db.jdbc.JdbcDatabase;
 import io.airbyte.cdk.integrations.base.AirbyteMessageConsumer;
 import io.airbyte.cdk.integrations.base.Destination;
 import io.airbyte.cdk.integrations.base.IntegrationRunner;
+import io.airbyte.cdk.integrations.base.SerializedAirbyteMessageConsumer;
+import io.airbyte.cdk.integrations.destination.NamingConventionTransformer;
+import io.airbyte.cdk.integrations.destination.jdbc.AbstractJdbcDestination;
+import io.airbyte.cdk.integrations.destination.jdbc.SqlOperations;
+import io.airbyte.commons.json.Jsons;
 import io.airbyte.protocol.models.v0.AirbyteConnectionStatus;
-import io.airbyte.protocol.models.v0.AirbyteConnectionStatus.Status;
 import io.airbyte.protocol.models.v0.AirbyteMessage;
 import io.airbyte.protocol.models.v0.ConfiguredAirbyteCatalog;
+import java.util.Collections;
+import java.util.Map;
 import java.util.function.Consumer;
+import javax.sql.DataSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class SnowflakeBulkDestination extends BaseConnector implements Destination {
+public class SnowflakeBulkDestination extends AbstractJdbcDestination implements Destination {
   private static final Logger LOGGER = LoggerFactory.getLogger(SnowflakeBulkDestination.class);
 
-  @Override
-  public AirbyteConnectionStatus check(final JsonNode config) {
-    return new AirbyteConnectionStatus().withStatus(Status.SUCCEEDED);
+
+  public SnowflakeBulkDestination() {
+    super("", new SnowflakeSQLNameTransformer(), new SnowflakeSqlOperations());
   }
 
   @Override
+  protected Map<String, String> getDefaultConnectionProperties(final JsonNode jsonNode) {
+    return Collections.emptyMap();
+  }
+
+  @Override
+  public JsonNode toJdbcConfig(final JsonNode config) {
+    return Jsons.emptyObject();
+  }
+
+  @Override
+  protected DataSource getDataSource(final JsonNode config) {
+    return SnowflakeDatabase.createDataSource(config, airbyteEnvironment);
+  }
+
+  @Override
+  public SerializedAirbyteMessageConsumer getSerializedMessageConsumer(final JsonNode config,
+      final ConfiguredAirbyteCatalog catalog, final Consumer<AirbyteMessage> outputRecordCollector)
+      throws Exception {
+    return new ShimToSerializedAirbyteMessageConsumer(getConsumer(config, catalog, outputRecordCollector));
+  }
+
   public AirbyteMessageConsumer getConsumer(final JsonNode config,
       final ConfiguredAirbyteCatalog catalog,
       final Consumer<AirbyteMessage> outputRecordCollector) {
     return new BulkConsumer(config, catalog, outputRecordCollector);
   }
+
+  @Override
+  public AirbyteConnectionStatus check(final JsonNode config) {
+    final NamingConventionTransformer nameTransformer = getNamingResolver();
+    final SqlOperations snowflakeInternalStagingSqlOperations = new SnowflakeSqlOperations();
+    final DataSource dataSource = getDataSource(config);
+    try {
+      final JdbcDatabase database = getDatabase(dataSource);
+      final String outputSchema = nameTransformer.getIdentifier(config.get("schema").asText());
+      attemptTableOperations(outputSchema, database, nameTransformer,
+          snowflakeInternalStagingSqlOperations, true);
+      attemptStageOperations(outputSchema, database, nameTransformer, snowflakeInternalStagingSqlOperations);
+      return new AirbyteConnectionStatus().withStatus(AirbyteConnectionStatus.Status.SUCCEEDED);
+    } catch (final Exception e) {
+      LOGGER.error("Exception while checking connection: ", e);
+      return new AirbyteConnectionStatus()
+          .withStatus(AirbyteConnectionStatus.Status.FAILED)
+          .withMessage("Could not connect with provided configuration. \n" + e.getMessage());
+    } finally {
+      try {
+        DataSourceFactory.close(dataSource);
+      } catch (final Exception e) {
+        LOGGER.warn("Unable to close data source.", e);
+      }
+    }
+  }
+
 
   public static void main(final String[] args) throws Exception {
     final Destination destination = new SnowflakeBulkDestination();
@@ -38,5 +94,4 @@ public class SnowflakeBulkDestination extends BaseConnector implements Destinati
     new IntegrationRunner(destination).run(args);
     LOGGER.info("completed destination: {}", SnowflakeBulkDestination.class);
   }
-
 }
