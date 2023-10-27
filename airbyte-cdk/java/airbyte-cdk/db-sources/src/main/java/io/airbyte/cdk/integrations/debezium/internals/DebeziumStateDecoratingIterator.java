9bc1560fd8af9f7231a0e4db605eab5ce93583ba
Rodi Reich Zilberman <867491+rodireich@users.noreply.github.com>
2023-10-26 22:05:39 -0700

    sanity

--- airbyte-cdk/java/airbyte-cdk/db-sources/src/main/java/io/airbyte/cdk/integrations/debezium/internals/DebeziumStateDecoratingIterator.java
+++ airbyte-cdk/java/airbyte-cdk/db-sources/src/main/java/io/airbyte/cdk/integrations/debezium/internals/DebeziumStateDecoratingIterator.java
@@ -19,7 +19,6 @@ import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
 import org.apache.kafka.connect.errors.ConnectException;
-import org.bson.BsonInvalidOperationException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
