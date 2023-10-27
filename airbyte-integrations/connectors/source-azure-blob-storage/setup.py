1cc2edc25735f3d41bfd28fb44ec066de9b2f9da
Joe Reuter <joe@airbyte.io>
2023-10-27 10:28:48 +0200

    update file cdk

--- airbyte-integrations/connectors/source-azure-blob-storage/setup.py
+++ airbyte-integrations/connectors/source-azure-blob-storage/setup.py
@@ -5,7 +5,20 @@
 
 from setuptools import find_packages, setup
 
-MAIN_REQUIREMENTS = ["airbyte-cdk>=0.51.17", "smart_open[azure]", "pytz", "fastavro==1.4.11", "pyarrow"]
+MAIN_REQUIREMENTS = [
+    "airbyte-cdk>=0.51.17",
+    "smart_open[azure]",
+    "pytz",
+    "fastavro==1.4.11",
+    "pyarrow",
+    "unstructured==0.10.19",
+    "pdf2image==1.16.3",
+    "pdfminer.six==20221105",
+    "unstructured[docx]==0.10.19",
+    "unstructured.pytesseract>=0.3.12",
+    "pytesseract==0.3.10",
+    "markdown",
+]
 
 TEST_REQUIREMENTS = ["requests-mock~=1.9.3", "pytest-mock~=3.6.1", "pytest~=6.2"]
 
