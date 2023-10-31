c21302ebc484c43037a433b244a9a04faaf406a3
erohmensing <erohmensing@gmail.com>
2023-10-31 13:25:16 -0500

    fix rest of commands to use pipelinecontext

--- airbyte-ci/connectors/pipelines/pipelines/airbyte_ci/format/js/commands.py
+++ airbyte-ci/connectors/pipelines/pipelines/airbyte_ci/format/js/commands.py
@@ -4,23 +4,25 @@ import sys
 import asyncclick as click
 import dagger
 from pipelines.airbyte_ci.format.consts import DEFAULT_FORMAT_IGNORE_LIST
+from pipelines.cli.click_decorators import LazyPassDecorator, click_ignore_unused_kwargs
 from pipelines.helpers.utils import sh_dash_c
+from pipelines.models.contexts.click_pipeline_context import ClickPipelineContext
+
+pass_pipeline_context: LazyPassDecorator = LazyPassDecorator(ClickPipelineContext)
 
 
 @click.command()
-@click.pass_context
-async def js(ctx: click.Context):
+@pass_pipeline_context
+@click_ignore_unused_kwargs
+async def js(ctx: ClickPipelineContext):
     """Format yaml and json code via prettier."""
-    fix = ctx.obj.get("fix_formatting")
-    if fix is None:
-        raise click.UsageError("You must specify either --fix or --check")
 
-    success = await format_js(fix)
+    success = await format_js(ctx)
     if not success:
         click.Abort()
 
 
-async def format_js(fix: bool) -> bool:
+async def format_js(ctx: ClickPipelineContext) -> bool:
     """Checks whether the repository is formatted correctly.
     Args:
         fix (bool): Whether to automatically fix any formatting issues detected.
@@ -29,47 +31,44 @@ async def format_js(fix: bool) -> bool:
     """
     logger = logging.getLogger(f"format")
 
+    fix = ctx.params.get("fix_formatting")
     if fix:
         prettier_command = ["prettier", "--write", "."]
     else:
         prettier_command = ["prettier", "--check", "."]
 
-    async with dagger.Connection(dagger.Config(log_output=sys.stderr)) as dagger_client:
-        try:
-            format_container = await (
-                dagger_client.container()
-                .from_("node:18.18.0-slim")
-                .with_exec(
-                    sh_dash_c(
-                        [
-                            "apt-get update",
-                            "apt-get install -y bash",
-                        ]
-                    )
+    dagger_client = await ctx.get_dagger_client(pipeline_name="Format Yaml and Json")
+
+    try:
+        format_container = await (
+            dagger_client.container()
+            .from_("node:18.18.0-slim")
+            .with_exec(
+                sh_dash_c(
+                    [
+                        "apt-get update",
+                        "apt-get install -y bash",
+                    ]
                 )
-                .with_mounted_directory(
-                    "/src",
-                    dagger_client.host().directory(
-                        ".",
-                        include=["**/*.yaml", "**/*.yml", "**.*/json", "package.json", "package-lock.json"],
-                        exclude=DEFAULT_FORMAT_IGNORE_LIST,
-                    ),
-                )
-                .with_workdir(f"/src")
-                .with_exec(["npm", "install", "-g", "npm@10.1.0"])
-                .with_exec(["npm", "install", "-g", "prettier@2.8.1"])
-                .with_exec(prettier_command)
             )
+            .with_mounted_directory(
+                "/src",
+                dagger_client.host().directory(
+                    ".",
+                    include=["**/*.yaml", "**/*.yml", "**.*/json", "package.json", "package-lock.json"],
+                    exclude=DEFAULT_FORMAT_IGNORE_LIST,
+                ),
+            )
+            .with_workdir(f"/src")
+            .with_exec(["npm", "install", "-g", "npm@10.1.0"])
+            .with_exec(["npm", "install", "-g", "prettier@2.8.1"])
+            .with_exec(prettier_command)
+        )
 
-            await format_container
-            if fix:
-                await format_container.directory("/src").export(".")
-            return True
-        except Exception as e:
-            logger.error(f"Failed to format code: {e}")
-            return False
-
-        except dagger.ExecError as e:
-            logger.error("Format failed")
-            logger.error(e.stderr)
-            sys.exit(1)
+        await format_container
+        if fix:
+            await format_container.directory("/src").export(".")
+        return True
+    except Exception as e:
+        logger.error(f"Failed to format code: {e}")
+        return False
--- airbyte-ci/connectors/pipelines/pipelines/airbyte_ci/format/license/commands.py
+++ airbyte-ci/connectors/pipelines/pipelines/airbyte_ci/format/license/commands.py
@@ -6,53 +6,56 @@ import sys
 import asyncclick as click
 import dagger
 from pipelines.airbyte_ci.format.consts import DEFAULT_FORMAT_IGNORE_LIST
+from pipelines.cli.click_decorators import LazyPassDecorator, click_ignore_unused_kwargs
 from pipelines.helpers.utils import sh_dash_c
+from pipelines.models.contexts.click_pipeline_context import ClickPipelineContext
+
+pass_pipeline_context: LazyPassDecorator = LazyPassDecorator(ClickPipelineContext)
 
 
 @click.command()
-@click.pass_context
-async def license(ctx: click.Context):
+@pass_pipeline_context
+@click_ignore_unused_kwargs
+async def license(ctx: ClickPipelineContext):
     """Add license to python and java code via addlicense."""
-    fix = ctx.obj.get("fix_formatting")
-    if fix is None:
-        raise click.UsageError("You must specify either --fix or --check")
-
-    success = await format_license(fix)
+    success = await format_license(ctx)
     if not success:
         click.Abort()
 
 
-async def format_license(fix: bool = False) -> bool:
+async def format_license(ctx: ClickPipelineContext) -> bool:
     license_text = "LICENSE_SHORT"
     logger = logging.getLogger(f"format")
 
+    fix = ctx.params.get("fix_formatting")
     if fix:
         addlicense_command = ["addlicense", "-c", "Airbyte, Inc.", "-l", "apache", "-v", "-f", license_text, "."]
     else:
         addlicense_command = ["addlicense", "-c", "Airbyte, Inc.", "-l", "apache", "-v", "-f", license_text, "-check", "."]
 
-    async with dagger.Connection(dagger.Config(log_output=sys.stderr)) as dagger_client:
-        try:
-            license_container = await (
-                dagger_client.container()
-                .from_("golang:1.17")
-                .with_exec(sh_dash_c(["apt-get update", "apt-get install -y bash tree", "go get -u github.com/google/addlicense"]))
-                .with_mounted_directory(
-                    "/src",
-                    dagger_client.host().directory(
-                        ".",
-                        include=["**/*.java", "**/*.py", "LICENSE_SHORT"],
-                        exclude=DEFAULT_FORMAT_IGNORE_LIST,
-                    ),
-                )
-                .with_workdir(f"/src")
-                .with_exec(addlicense_command)
+    dagger_client = await ctx.get_dagger_client(pipeline_name="Format License")
+    try:
+        license_container = await (
+            dagger_client.container()
+            .from_("golang:1.17")
+            .with_exec(sh_dash_c(["apt-get update", "apt-get install -y bash tree", "go get -u github.com/google/addlicense"]))
+            .with_mounted_directory(
+                "/src",
+                dagger_client.host().directory(
+                    ".",
+                    include=["**/*.java", "**/*.py", "LICENSE_SHORT"],
+                    exclude=DEFAULT_FORMAT_IGNORE_LIST,
+                ),
             )
+            .with_workdir(f"/src")
+            .with_exec(addlicense_command)
+        )
 
-            await license_container
-            if fix:
-                await license_container.directory("/src").export(".")
-            return True
-        except Exception as e:
-            logger.error(f"Failed to apply license: {e}")
-            return False
+        await license_container
+        if fix:
+            await license_container.directory("/src").export(".")
+        return True
+
+    except Exception as e:
+        logger.error(f"Failed to apply license: {e}")
+        return False
