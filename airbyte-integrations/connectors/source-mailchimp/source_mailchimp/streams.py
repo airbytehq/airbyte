c5876bdb35a78310f25e8c7680ba6d241d6a9d1a
Christo <christo.grab@gmail.com>
2023-10-31 20:50:36 -0400

    implement new base class for list child streams

--- airbyte-integrations/connectors/source-mailchimp/source_mailchimp/streams.py
+++ airbyte-integrations/connectors/source-mailchimp/source_mailchimp/streams.py
@@ -123,6 +123,51 @@ class IncrementalMailChimpStream(MailChimpStream, ABC):
         default_params = {"sort_field": self.sort_field, "sort_dir": "ASC", **stream_slice}
         params.update(default_params)
         return params
+    
+
+class MailchimpListChildStream(IncrementalMailChimpStream):
+    """
+    Base class for Mailchimp streams that are children of the Lists stream.
+    """
+
+    def stream_slices(self, stream_state: Mapping[str, Any] = None, **kwargs) -> Iterable[Optional[Mapping[str, Any]]]:
+        stream_state = stream_state or {}
+        parent = Lists(authenticator=self.authenticator).read_records(sync_mode=SyncMode.full_refresh)
+        for slice in parent:
+            yield {"list_id": slice["id"]}
+
+    def path(self, stream_slice: Mapping[str, Any] = None, **kwargs) -> str:
+        list_id = stream_slice.get("list_id")
+        return f"lists/{list_id}/{self.data_field}"
+
+    def request_params(self, stream_state=None, stream_slice=None, **kwargs):
+        params = super().request_params(stream_state=stream_state, stream_slice=stream_slice, **kwargs)
+
+        # Exclude the _links field, as it is not user-relevant data
+        params["exclude_fields"] = f"{self.data_field}._links"
+
+        # Get the current state value for this list, if it exists
+        # Then, use the value in state to filter the request
+        current_slice = stream_slice.get("list_id")
+        filter_date = stream_state.get(current_slice)
+        if filter_date:
+            params[self.filter_field] = filter_date.get(self.cursor_field)
+        return params
+
+    def get_updated_state(self, current_stream_state: MutableMapping[str, Any], latest_record: Mapping[str, Any]) -> Mapping[str, Any]:
+        current_stream_state = current_stream_state or {}
+        list_id = latest_record.get("list_id")
+        latest_cursor_value = latest_record.get(self.cursor_field)
+
+        # Get the current state value for this list, if it exists
+        list_state = current_stream_state.get(list_id, {})
+        current_cursor_value = list_state.get(self.cursor_field, latest_cursor_value)
+
+        # Update the cursor value and set it in state
+        updated_cursor_value = max(current_cursor_value, latest_cursor_value)
+        current_stream_state[list_id] = {self.cursor_field: updated_cursor_value}
+
+        return current_stream_state
 
 
 class Lists(IncrementalMailChimpStream):
@@ -215,24 +260,13 @@ class EmailActivity(IncrementalMailChimpStream):
                 yield {**item, **activity_item}
 
 
-class ListMembers(IncrementalMailChimpStream):
+class ListMembers(MailchimpListChildStream):
     """
-    Get 
+    Get information about members in a specific Mailchimp list.
+    Docs link: https://mailchimp.com/developer/marketing/api/list-members/list-members-info/
     """
     cursor_field = "last_changed"
     data_field = "members"
-
-    def stream_slices(self, stream_state: Mapping[str, Any] = None, **kwargs) -> Iterable[Optional[Mapping[str, Any]]]:
-        stream_state = stream_state or {}
-        parent = Lists(authenticator=self.authenticator).read_records(sync_mode=SyncMode.full_refresh)
-        for slice in parent:
-            yield {"list_id": slice["id"]}
-
-    def path(self, stream_slice: Mapping[str, Any] = None, **kwargs) -> str:
-        list_id = stream_slice.get("list_id")
-        return f"lists/{list_id}/members"
-    
-    def request_params
 
 
 class Reports(IncrementalMailChimpStream):
@@ -243,7 +277,7 @@ class Reports(IncrementalMailChimpStream):
         return "reports"
 
 
-class Segments(IncrementalMailChimpStream):
+class Segments(MailchimpListChildStream):
     """
     Get information about all available segments for a specific list.
     Docs link: https://mailchimp.com/developer/marketing/api/list-segments/list-segments/
@@ -251,50 +285,6 @@ class Segments(IncrementalMailChimpStream):
 
     cursor_field = "updated_at"
     data_field = "segments"
-
-    def stream_slices(
-        self, *, sync_mode: SyncMode, cursor_field: List[str] = None, stream_state: Mapping[str, Any] = None
-    ) -> Iterable[Optional[Mapping[str, Any]]]:
-
-        stream_state = stream_state or {}
-
-        parent = Lists(authenticator=self.authenticator).read_records(sync_mode=SyncMode.full_refresh)
-        for slice in parent:
-            yield {"list_id": slice["id"]}
-
-    def path(self, stream_slice: Mapping[str, Any] = None, **kwargs) -> str:
-        list_id = stream_slice.get("list_id")
-        return f"lists/{list_id}/segments"
-
-    def request_params(self, stream_state=None, stream_slice=None, **kwargs):
-
-        params = super().request_params(stream_state=stream_state, stream_slice=stream_slice, **kwargs)
-        # Exclude the _links field, as it is not user-relevant data
-        params["exclude_fields"] = f"{self.data_field}._links"
-
-        # Get the current state value for this list, if it exists
-        # Then, use the value in state to filter the request
-        current_slice = stream_slice.get("list_id")
-        filter_date = stream_state.get(current_slice)
-
-        if filter_date:
-            params[self.filter_field] = filter_date.get(self.cursor_field)
-
-        return params
-
-    def get_updated_state(self, current_stream_state: MutableMapping[str, Any], latest_record: Mapping[str, Any]) -> Mapping[str, Any]:
-        current_stream_state = current_stream_state or {}
-        list_id = latest_record.get("list_id")
-        latest_cursor_value = latest_record.get(self.cursor_field)
-
-        # Get the current state value for this list, if it exists
-        list_state = current_stream_state.get(list_id, {})
-        current_cursor_value = list_state.get(self.cursor_field, latest_cursor_value)
-
-        # Update the cursor value and set it in state
-        updated_cursor_value = max(current_cursor_value, latest_cursor_value)
-        current_stream_state[list_id] = {self.cursor_field: updated_cursor_value}
-        return current_stream_state
 
 
 class Unsubscribes(IncrementalMailChimpStream):
