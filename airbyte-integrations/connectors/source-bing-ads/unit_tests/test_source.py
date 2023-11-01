d2cd8b4c8f2166bffeea0b63d1a7a3b8d774a80c
Artem Inzhyyants <artem.inzhyyants@gmail.com>
2023-11-01 15:36:40 +0100

    Source Bing Ads: update unit tests

--- airbyte-integrations/connectors/source-bing-ads/unit_tests/test_source.py
+++ airbyte-integrations/connectors/source-bing-ads/unit_tests/test_source.py
@@ -32,7 +32,7 @@ def logger_mock_fixture():
 @patch.object(source_bing_ads.source, "Client")
 def test_streams_config_based(mocked_client, config):
     streams = SourceBingAds().streams(config)
-    assert len(streams) == 52
+    assert len(streams) == 56
 
 
 @patch.object(source_bing_ads.source, "Client")
