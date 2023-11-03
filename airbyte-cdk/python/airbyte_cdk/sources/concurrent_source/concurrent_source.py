d5cc747680a0037c82c2b5667d19d6d327d2110a
Alexandre Girard <alexandre@airbyte.io>
2023-11-02 18:30:13 -0700

    derp

--- airbyte-cdk/python/airbyte_cdk/sources/concurrent_source/concurrent_source.py
+++ airbyte-cdk/python/airbyte_cdk/sources/concurrent_source/concurrent_source.py
@@ -21,6 +21,7 @@ from airbyte_cdk.models import (
 )
 from airbyte_cdk.models import Type as MessageType
 from airbyte_cdk.sources import AbstractSource
+from airbyte_cdk.sources.concurrent_source.stream_complete_sentinel import StreamCompleteSentinel
 from airbyte_cdk.sources.concurrent_source.stream_reader import StreamReader
 from airbyte_cdk.sources.connector_state_manager import ConnectorStateManager
 from airbyte_cdk.sources.message import InMemoryMessageRepository
@@ -29,6 +30,7 @@ from airbyte_cdk.sources.streams.concurrent.adapters import StreamFacade
 from airbyte_cdk.sources.utils.record_helper import stream_data_to_airbyte_message
 from airbyte_cdk.sources.utils.schema_helpers import split_config
 from airbyte_cdk.utils.event_timing import create_timer
+from airbyte_cdk.utils.stream_status_utils import as_airbyte_message as stream_status_as_airbyte_message
 
 
 class ConcurrentSource(AbstractSource, ABC):
@@ -61,6 +63,7 @@ class ConcurrentSource(AbstractSource, ABC):
         # TODO assert all streams exist in the connector
         # get the streams once in case the connector needs to make any queries to generate them
         stream_instances: Mapping[str, AbstractStream] = {s.name: s for s in self._streams_as_abstract_streams(config)}
+        # FIXME need to do something with state messages
         state_manager = ConnectorStateManager(stream_instance_map=stream_instances, state=state)
         self._stream_to_instance_map = stream_instances
 
@@ -91,7 +94,7 @@ class ConcurrentSource(AbstractSource, ABC):
 
             stop = False
             total_records_counter = 0
-            stream_done_counter = 0
+            streams_in_progress = {stream for stream in stream_instances_to_read_from}
             while airbyte_message_or_record_or_exception := queue.get(block=True, timeout=self._timeout_seconds):
                 if isinstance(airbyte_message_or_record_or_exception, Exception):
                     # An exception was raised while processing the stream
@@ -99,14 +102,16 @@ class ConcurrentSource(AbstractSource, ABC):
                     # print(f"received exception {airbyte_message_or_record_or_exception}")
                     # print(f"{stream_done_counter} out of {len(stream_instances_to_read_from)} left.")
                     self._stream_read_threadpool.shutdown(wait=False, cancel_futures=True)
+                    for stream in streams_in_progress:
+                        yield stream_status_as_airbyte_message(stream.name, stream.as_airbyte_stream().namespace, AirbyteStreamStatus.INCOMPLETE)
                     raise airbyte_message_or_record_or_exception
 
-                elif airbyte_message_or_record_or_exception == SENTINEL:
+                elif isinstance(airbyte_message_or_record_or_exception, StreamCompleteSentinel):
                     # Update the map of stream -> done
-                    stream_done_counter += 1
                     # print(f"done with a stream. {stream_done_counter} out of {len(stream_instances_to_read_from)}")
-                    if stream_done_counter == len(stream_instances_to_read_from):
-                        stop = True
+                    stream = airbyte_message_or_record_or_exception.stream
+                    streams_in_progress.remove(stream)
+                    yield stream_status_as_airbyte_message(stream.name, stream.as_airbyte_stream().namespace, AirbyteStreamStatus.COMPLETE)
                 else:
                     # Do not pass a transformer or a schema
                     # AbstractStreams are expected to return data as they are expected.
@@ -118,7 +123,7 @@ class ConcurrentSource(AbstractSource, ABC):
                     yield message
                     if message.type == MessageType.RECORD:
                         total_records_counter += 1
-                if stop:
+                if not streams_in_progress:
                     # If all streams are done -> break
                     break
 
--- airbyte-cdk/python/airbyte_cdk/sources/concurrent_source/stream_reader.py
+++ airbyte-cdk/python/airbyte_cdk/sources/concurrent_source/stream_reader.py
@@ -2,6 +2,7 @@
 # Copyright (c) 2023 Airbyte, Inc., all rights reserved.
 #
 from airbyte_cdk.models import AirbyteStreamStatus
+from airbyte_cdk.sources.concurrent_source.stream_complete_sentinel import StreamCompleteSentinel
 from airbyte_cdk.sources.message import MessageRepository
 from airbyte_cdk.sources.streams.concurrent.abstract_stream import AbstractStream
 from airbyte_cdk.utils.stream_status_utils import as_airbyte_message as stream_status_as_airbyte_message
@@ -24,21 +25,13 @@ class StreamReader:
             )
             record_counter = 0
             for record in stream.read():
-                # print(f"adding record to queue {record}")
                 if record_counter == 0:
                     self._message_repository.emit_message(
                         stream_status_as_airbyte_message(airbyte_stream.name, airbyte_stream.namespace, AirbyteStreamStatus.RUNNING)
                     )
                 record_counter += 1
                 self._queue.put(record)
-            self._message_repository.emit_message(
-                stream_status_as_airbyte_message(airbyte_stream.name, airbyte_stream.namespace, AirbyteStreamStatus.COMPLETE)
-            )
-            self._queue.put(self._sentinel)
+            self._queue.put(StreamCompleteSentinel(stream))
             self._logger.info(f"Read {record_counter} records from {stream.name} stream")
         except Exception as e:
-            # print(f"exception: {e}")
-            self._message_repository.emit_message(
-                stream_status_as_airbyte_message(stream.name, stream.as_airbyte_stream().namespace, AirbyteStreamStatus.INCOMPLETE)
-            )
             self._queue.put(e)
