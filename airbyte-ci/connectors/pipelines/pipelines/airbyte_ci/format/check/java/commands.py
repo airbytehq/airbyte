d79f05692fe1f84f7056e759eeaeccf70a9cf07d
erohmensing <erohmensing@gmail.com>
2023-11-05 13:44:29 -0600

    java: feedback on includes

--- airbyte-ci/connectors/pipelines/pipelines/airbyte_ci/format/check/java/commands.py
+++ airbyte-ci/connectors/pipelines/pipelines/airbyte_ci/format/check/java/commands.py
@@ -8,6 +8,7 @@ import asyncclick as click
 import dagger
 from pipelines.airbyte_ci.format.consts import DEFAULT_FORMAT_IGNORE_LIST
 from pipelines.cli.click_decorators import click_ignore_unused_kwargs
+from pipelines.consts import AMAZONCORRETTO_IMAGE
 from pipelines.helpers.utils import sh_dash_c
 from pipelines.models.contexts.click_pipeline_context import ClickPipelineContext, pass_pipeline_context
 
@@ -30,7 +31,7 @@ async def check_java(ctx: ClickPipelineContext) -> bool:
     try:
         format_container = await (
             dagger_client.container()
-            .from_("openjdk:17.0.1-jdk-slim")
+            .from_(AMAZONCORRETTO_IMAGE)
             .with_exec(
                 sh_dash_c(
                     [
@@ -48,9 +49,8 @@ async def check_java(ctx: ClickPipelineContext) -> bool:
                         "**/*.sql",
                         "**/*.gradle",
                         "gradlew",
-                        "gradlew.bat",
                         "gradle",
-                        "**/deps.toml",
+                        "deps.toml",
                         "**/gradle.properties",
                         "**/version.properties",
                         "tools/gradle/codestyle/java-google-style.xml",
