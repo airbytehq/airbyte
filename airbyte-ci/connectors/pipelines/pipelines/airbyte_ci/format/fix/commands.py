7b126dd7555d752b4a559a1ae9e889ed8cb8e7cd
erohmensing <erohmensing@gmail.com>
2023-11-05 17:01:55 -0600

    merge fix

--- airbyte-ci/connectors/pipelines/pipelines/airbyte_ci/format/fix/commands.py
+++ airbyte-ci/connectors/pipelines/pipelines/airbyte_ci/format/fix/commands.py
@@ -4,20 +4,14 @@ from typing import Optional
 
 import asyncclick as click
 import dagger
+from pipelines.airbyte_ci.format.fix.utils import format
 from pipelines.cli.click_decorators import click_ignore_unused_kwargs, click_merge_args_into_context_obj
 from pipelines.cli.lazy_group import LazyGroup
 from pipelines.models.contexts.click_pipeline_context import ClickPipelineContext, pass_pipeline_context
 
 
 @click.group(
-    cls=LazyGroup,
     help="Run code format checks and fix any failures.",
-    lazy_subcommands={
-        "java": "pipelines.airbyte_ci.format.fix.java.commands.java",
-        "js": "pipelines.airbyte_ci.format.fix.js.commands.js",
-        "license": "pipelines.airbyte_ci.format.fix.license.commands.license",
-        "python": "pipelines.airbyte_ci.format.fix.python.commands.python",
-    },
     invoke_without_command=True,
     chain=True,
 )
@@ -31,7 +25,81 @@ async def fix(ctx: click.Context, pipeline_ctx: ClickPipelineContext):
 
     if ctx.invoked_subcommand is None:
         print("Running all formatters...")
-        await ctx.invoke(fix.get_command(ctx, "java"))
-        await ctx.invoke(fix.get_command(ctx, "js"))
-        await ctx.invoke(fix.get_command(ctx, "license"))
-        await ctx.invoke(fix.get_command(ctx, "python"))
+        await ctx.invoke(java)
+        await ctx.invoke(js)
+        await ctx.invoke(license)
+        await ctx.invoke(python)
+
+
+@fix.command()
+@pass_pipeline_context
+@click_ignore_unused_kwargs
+async def java(ctx: ClickPipelineContext):
+    """Format java, groovy, and sql code via spotless."""
+    await format(
+        ctx,
+        base_image="openjdk:17.0.1-jdk-slim",
+        include=[
+            "**/*.java",
+            "**/*.sql",
+            "**/*.gradle",
+            "gradlew",
+            "gradlew.bat",
+            "gradle",
+            "**/deps.toml",
+            "**/gradle.properties",
+            "**/version.properties",
+            "tools/gradle/codestyle/java-google-style.xml",
+            "tools/gradle/codestyle/sql-dbeaver.properties",
+        ],
+        install_commands=[],
+        format_commands=["./gradlew spotlessApply --scan"],
+    )
+
+
+@fix.command()
+@pass_pipeline_context
+@click_ignore_unused_kwargs
+async def js(ctx: ClickPipelineContext):
+    await format(
+        ctx,
+        base_image="node:18.18.0-slim",
+        include=["**/*.yaml", "**/*.yml", "**.*/json", "package.json", "package-lock.json"],
+        install_commands=["npm install -g npm@10.1.0", "npm install -g prettier@2.8.1"],
+        format_commands=["prettier --write ."],
+    )
+
+
+@fix.command()
+@pass_pipeline_context
+@click_ignore_unused_kwargs
+async def license(ctx: ClickPipelineContext):
+    """Add license to python and java code via addlicense."""
+    license_file = "LICENSE_SHORT"
+
+    await format(
+        ctx,
+        base_image="golang:1.17",
+        include=["**/*.java", "**/*.py", license_file],
+        install_commands=["go get -u github.com/google/addlicense"],
+        format_commands=[f"addlicense -c 'Airbyte, Inc.' -l apache -v -f {license_file} ."],
+    )
+
+
+@fix.command()
+@pass_pipeline_context
+@click_ignore_unused_kwargs
+async def python(ctx: ClickPipelineContext):
+    """Format python code via black and isort."""
+    await format(
+        ctx,
+        base_image="python:3.10.13-slim",
+        env_vars={"PIPX_BIN_DIR": "/usr/local/bin"},
+        include=["**/*.py", "pyproject.toml", "poetry.lock"],
+        install_commands=["pip install pipx", "pipx ensurepath", "pipx install poetry"],
+        format_commands=[
+            "poetry install",
+            "poetry run isort --settings-file pyproject.toml .",
+            "poetry run black --config pyproject.toml .",
+        ],
+    )
