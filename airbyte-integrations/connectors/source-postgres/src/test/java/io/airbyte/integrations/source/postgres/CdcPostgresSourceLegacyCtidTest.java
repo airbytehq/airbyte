c0626252fe7a476e42c93c1d8f8250803b784dbb
Marius Posta <marius@airbyte.io>
2023-11-08 16:42:27 -0500

    s/12-bullseye/13-bullseye/ in an integration test

--- airbyte-integrations/connectors/source-postgres/src/test/java/io/airbyte/integrations/source/postgres/CdcPostgresSourceLegacyCtidTest.java
+++ airbyte-integrations/connectors/source-postgres/src/test/java/io/airbyte/integrations/source/postgres/CdcPostgresSourceLegacyCtidTest.java
@@ -7,7 +7,7 @@ package io.airbyte.integrations.source.postgres;
 public class CdcPostgresSourceLegacyCtidTest extends CdcPostgresSourceTest {
 
   protected static String getServerImageName() {
-    return "debezium/postgres:12-bullseye";
+    return "debezium/postgres:13-bullseye";
   }
 
 }
