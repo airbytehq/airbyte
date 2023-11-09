e113ff66c5030b4d7e3bbebd1ee54ff101192802
Joe Reuter <joe@airbyte.io>
2023-11-09 16:12:11 +0100

    CDK: Make consts required in Pydantic generated json schemas (#32251)

--- airbyte-cdk/python/airbyte_cdk/sources/file_based/config/unstructured_format.py
+++ airbyte-cdk/python/airbyte_cdk/sources/file_based/config/unstructured_format.py
@@ -2,13 +2,15 @@
 # Copyright (c) 2023 Airbyte, Inc., all rights reserved.
 #
 
+from airbyte_cdk.utils.oneof_option_config import OneOfOptionConfig
 from pydantic import BaseModel, Field
 
 
 class UnstructuredFormat(BaseModel):
-    class Config:
+    class Config(OneOfOptionConfig):
         title = "Document File Type Format (Experimental)"
-        schema_extra = {"description": "Extract text from document formats (.pdf, .docx, .md, .pptx) and emit as one record per file."}
+        description = "Extract text from document formats (.pdf, .docx, .md, .pptx) and emit as one record per file."
+        discriminator = "filetype"
 
     filetype: str = Field(
         "unstructured",
