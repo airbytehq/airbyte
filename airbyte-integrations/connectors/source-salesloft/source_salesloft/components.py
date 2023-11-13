12c3408539a396514469c3756d102b1a58d27c33
marcosmarxm <marcosmarxm@gmail.com>
2023-11-13 17:49:23 -0300

    fix files

--- airbyte-integrations/connectors/source-salesloft/source_salesloft/components.py
+++ airbyte-integrations/connectors/source-salesloft/source_salesloft/components.py
@@ -1,12 +1,9 @@
 from dataclasses import dataclass
-from typing import Any, Iterable, Mapping
+from typing import Any, Mapping
 
-from airbyte_cdk.models import SyncMode
 from airbyte_cdk.sources.declarative.auth.declarative_authenticator import DeclarativeAuthenticator
 from airbyte_cdk.sources.declarative.auth.oauth import DeclarativeSingleUseRefreshTokenOauth2Authenticator
 from airbyte_cdk.sources.declarative.auth.token import BearerAuthenticator
-from airbyte_cdk.sources.declarative.partition_routers.substream_partition_router import SubstreamPartitionRouter
-from airbyte_cdk.sources.declarative.types import StreamSlice
 
 
 @dataclass
@@ -16,7 +13,6 @@ class SalesloftAuthenticator(DeclarativeAuthenticator):
     oauth2: DeclarativeSingleUseRefreshTokenOauth2Authenticator
 
     def __new__(cls, token_auth, oauth2, config, *args, **kwargs):
-        print(config["credentials"])
         if config["credentials"]["auth_type"] == "api_key":
             return token_auth
         return oauth2
