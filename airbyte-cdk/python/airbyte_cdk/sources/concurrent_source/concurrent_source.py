55203ec8a255b6e6e9fb9c598444b025559614f5
Alexandre Girard <alexandre@airbyte.io>
2023-11-13 13:39:11 -0800

    yield from []

--- airbyte-cdk/python/airbyte_cdk/sources/concurrent_source/concurrent_source.py
+++ airbyte-cdk/python/airbyte_cdk/sources/concurrent_source/concurrent_source.py
@@ -72,6 +72,7 @@ class ConcurrentSource(AbstractSource, ABC):
 
         # Return early if there are no streams to read from
         if not stream_instances_to_read_from:
+            yield from []
             return
 
         queue: Queue[QueueItem] = Queue()
