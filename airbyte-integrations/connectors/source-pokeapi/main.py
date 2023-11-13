969d23f3ec5c9b45d6c66ed86a1e7c14bf050ce7
alafanechere <alafanechere@users.noreply.github.com>
2023-11-13 09:08:46 +0000

    Automated Commit - Formatting Changes

--- airbyte-integrations/connectors/source-pokeapi/main.py
+++ airbyte-integrations/connectors/source-pokeapi/main.py
@@ -2,12 +2,11 @@
 # Copyright (c) 2023 Airbyte, Inc., all rights reserved.
 #
 
-from airbyte_cdk.entrypoint import launch
-
 import sys
 
+from airbyte_cdk.entrypoint import launch
 from source_pokeapi import SourcePokeapi
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     source = SourcePokeapi()
     launch(source, sys.argv[1:])
