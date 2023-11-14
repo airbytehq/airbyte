d5d02779f51168694ab60d0606e8437af8242043
Edward Gao <edward.gao@airbyte.io>
2023-11-14 12:59:19 -0800

    stuff

--- airbyte-cdk/java/airbyte-cdk/db-destinations/src/main/java/io/airbyte/cdk/integrations/destination/jdbc/typing_deduping/JdbcV1V2Migrator.java
+++ airbyte-cdk/java/airbyte-cdk/db-destinations/src/main/java/io/airbyte/cdk/integrations/destination/jdbc/typing_deduping/JdbcV1V2Migrator.java
@@ -4,20 +4,22 @@ import io.airbyte.cdk.db.jdbc.JdbcDatabase;
 import io.airbyte.cdk.integrations.destination.NamingConventionTransformer;
 import io.airbyte.cdk.integrations.destination.jdbc.TableDefinition;
 import io.airbyte.integrations.base.destination.typing_deduping.BaseDestinationV1V2Migrator;
-import io.airbyte.integrations.base.destination.typing_deduping.CollectionUtils;
 import io.airbyte.integrations.base.destination.typing_deduping.NamespacedTableName;
 import io.airbyte.integrations.base.destination.typing_deduping.StreamConfig;
 import java.util.Collection;
 import java.util.Optional;
 import lombok.SneakyThrows;
 
+/**
+ * Largely based on {@link io.airbyte.integrations.destination.snowflake.typing_deduping.SnowflakeV1V2Migrator}.
+ */
 public class JdbcV1V2Migrator extends BaseDestinationV1V2Migrator<TableDefinition> {
 
   private final NamingConventionTransformer namingConventionTransformer;
   private final JdbcDatabase database;
   private final String databaseName;
 
-  public JdbcV1V2Migrator(NamingConventionTransformer namingConventionTransformer, JdbcDatabase database, String databaseName) {
+  public JdbcV1V2Migrator(final NamingConventionTransformer namingConventionTransformer, final JdbcDatabase database, final String databaseName) {
     this.namingConventionTransformer = namingConventionTransformer;
     this.database = database;
     this.databaseName = databaseName;
@@ -41,7 +43,7 @@ public class JdbcV1V2Migrator extends BaseDestinationV1V2Migrator<TableDefinitio
 
   @Override
   protected boolean schemaMatchesExpectation(final TableDefinition existingTable, final Collection<String> columns) {
-    return CollectionUtils.containsAllIgnoreCase(existingTable.columns().keySet(), columns);
+    return existingTable.columns().keySet().containsAll(columns);
   }
 
   @SneakyThrows
@@ -52,7 +54,6 @@ public class JdbcV1V2Migrator extends BaseDestinationV1V2Migrator<TableDefinitio
 
   @Override
   protected NamespacedTableName convertToV1RawName(final StreamConfig streamConfig) {
-    // The implicit upper-casing happens for this in the SqlGenerator
     @SuppressWarnings("deprecation") final String tableName = this.namingConventionTransformer.getRawTableName(streamConfig.id().originalName());
     return new NamespacedTableName(
         this.namingConventionTransformer.getIdentifier(streamConfig.id().originalNamespace()),
