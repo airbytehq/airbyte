d6cb5c9f892203ce290f9f57a7e5de12fdb00511
Edward Gao <edward.gao@airbyte.io>
2023-11-14 14:21:17 -0800

    destination redshift v2: implement migrator (#32524)
    Co-authored-by: edgao <edgao@users.noreply.github.com>

---
+++ airbyte-cdk/java/airbyte-cdk/db-destinations/src/main/java/io/airbyte/cdk/integrations/destination/jdbc/typing_deduping/JdbcV1V2Migrator.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright (c) 2023 Airbyte, Inc., all rights reserved.
+ */
+
+package io.airbyte.cdk.integrations.destination.jdbc.typing_deduping;
+
+import io.airbyte.cdk.db.jdbc.JdbcDatabase;
+import io.airbyte.cdk.integrations.destination.NamingConventionTransformer;
+import io.airbyte.cdk.integrations.destination.jdbc.TableDefinition;
+import io.airbyte.integrations.base.destination.typing_deduping.BaseDestinationV1V2Migrator;
+import io.airbyte.integrations.base.destination.typing_deduping.NamespacedTableName;
+import io.airbyte.integrations.base.destination.typing_deduping.StreamConfig;
+import java.util.Collection;
+import java.util.Optional;
+import lombok.SneakyThrows;
+
+/**
+ * Largely based on
+ * {@link io.airbyte.integrations.destination.snowflake.typing_deduping.SnowflakeV1V2Migrator}.
+ */
+public class JdbcV1V2Migrator extends BaseDestinationV1V2Migrator<TableDefinition> {
+
+  private final NamingConventionTransformer namingConventionTransformer;
+  private final JdbcDatabase database;
+  private final String databaseName;
+
+  public JdbcV1V2Migrator(final NamingConventionTransformer namingConventionTransformer, final JdbcDatabase database, final String databaseName) {
+    this.namingConventionTransformer = namingConventionTransformer;
+    this.database = database;
+    this.databaseName = databaseName;
+  }
+
+  @SneakyThrows
+  @Override
+  protected boolean doesAirbyteInternalNamespaceExist(final StreamConfig streamConfig) throws Exception {
+    return !database
+        .queryJsons(
+            """
+            SELECT SCHEMA_NAME
+            FROM information_schema.schemata
+            WHERE schema_name = ?
+            AND catalog_name = ?;
+            """,
+            streamConfig.id().rawNamespace(),
+            databaseName)
+        .isEmpty();
+  }
+
+  @Override
+  protected boolean schemaMatchesExpectation(final TableDefinition existingTable, final Collection<String> columns) {
+    return existingTable.columns().keySet().containsAll(columns);
+  }
+
+  @SneakyThrows
+  @Override
+  protected Optional<TableDefinition> getTableIfExists(final String namespace, final String tableName) throws Exception {
+    return JdbcDestinationHandler.findExistingTable(database, databaseName, namespace, tableName);
+  }
+
+  @Override
+  protected NamespacedTableName convertToV1RawName(final StreamConfig streamConfig) {
+    @SuppressWarnings("deprecation")
+    final String tableName = this.namingConventionTransformer.getRawTableName(streamConfig.id().originalName());
+    return new NamespacedTableName(
+        this.namingConventionTransformer.getIdentifier(streamConfig.id().originalNamespace()),
+        tableName);
+  }
+
+}
