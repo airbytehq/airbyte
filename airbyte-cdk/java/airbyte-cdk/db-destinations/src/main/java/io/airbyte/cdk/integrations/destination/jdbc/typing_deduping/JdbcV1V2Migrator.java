d65cb97edeb3e96d4bad88b08030087e08b0247f
Gireesh Sreepathi <gisripa@gmail.com>
2023-11-15 10:21:34 -0800

    checkpoint
    Signed-off-by: Gireesh Sreepathi <gisripa@gmail.com>

--- airbyte-cdk/java/airbyte-cdk/db-destinations/src/main/java/io/airbyte/cdk/integrations/destination/jdbc/typing_deduping/JdbcV1V2Migrator.java
+++ airbyte-cdk/java/airbyte-cdk/db-destinations/src/main/java/io/airbyte/cdk/integrations/destination/jdbc/typing_deduping/JdbcV1V2Migrator.java
@@ -10,6 +10,7 @@ import io.airbyte.cdk.integrations.destination.jdbc.TableDefinition;
 import io.airbyte.integrations.base.destination.typing_deduping.BaseDestinationV1V2Migrator;
 import io.airbyte.integrations.base.destination.typing_deduping.NamespacedTableName;
 import io.airbyte.integrations.base.destination.typing_deduping.StreamConfig;
+import java.sql.ResultSet;
 import java.util.Collection;
 import java.util.Optional;
 import lombok.SneakyThrows;
@@ -33,17 +34,15 @@ public class JdbcV1V2Migrator extends BaseDestinationV1V2Migrator<TableDefinitio
   @SneakyThrows
   @Override
   protected boolean doesAirbyteInternalNamespaceExist(final StreamConfig streamConfig) throws Exception {
-    return !database
-        .queryJsons(
-            """
-            SELECT SCHEMA_NAME
-            FROM information_schema.schemata
-            WHERE schema_name = ?
-            AND catalog_name = ?;
-            """,
-            streamConfig.id().rawNamespace(),
-            databaseName)
-        .isEmpty();
+    String retrievedSchema = "";
+    try(ResultSet columns = database.getMetaData().getSchemas(databaseName, streamConfig.id().rawNamespace())) {
+      while(columns.next()) {
+        retrievedSchema = columns.getString("TABLE_SCHEM");
+        // Catalog can be null, so don't do anything with it.
+        String catalog = columns.getString("TABLE_CATALOG");
+      }
+    }
+    return !retrievedSchema.isEmpty();
   }
 
   @Override
