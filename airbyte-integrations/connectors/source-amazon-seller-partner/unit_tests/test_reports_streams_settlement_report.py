1fa68c66fc0ac00c5ee7b346bfc1ec40b7d1ea9b
Artem Inzhyyants <artem.inzhyyants@gmail.com>
2023-11-15 18:10:36 +0100

    Source Amazon Seller Partner: remove advanced_stream_options

--- airbyte-integrations/connectors/source-amazon-seller-partner/unit_tests/test_reports_streams_settlement_report.py
+++ airbyte-integrations/connectors/source-amazon-seller-partner/unit_tests/test_reports_streams_settlement_report.py
@@ -94,7 +94,6 @@ def settlement_reports_stream():
             authenticator=None,
             period_in_days=0,
             report_options=None,
-            advanced_stream_options=None,
             max_wait_seconds=500,
         )
         return stream
