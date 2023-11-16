e247a0c3e36d3e76e8cf4805838f5cbf368e741c
Ben Church <ben@airbyte.io>
2023-11-16 11:08:02 -0800

    Add clean command

--- Makefile
+++ Makefile
@@ -9,12 +9,13 @@ tools.airbyte-ci.install: ## Install airbyte-ci
 tools.airbyte-ci-dev.install: ## Install the development version of airbyte-ci
 	@./tools/bin/make/airbyte_ci_dev_install.sh
 
-
 tools.airbyte-ci.check: # Check if airbyte-ci is installed correctly
 	@./tools/bin/make/airbyte_ci_check.sh
 
+tools.airbyte-ci.clean: ## Clean airbyte-ci installations
+	@./tools/bin/make/airbyte_ci_clean.sh
 
 tools.install: tools.airbyte-ci.install tools.airbyte-ci.check
 
-.PHONY: tools.install tools.airbyte-ci.install tools.airbyte-ci-dev.install
+.PHONY: tools.install tools.airbyte-ci.install tools.airbyte-ci-dev.install tools.airbyte-ci.check tools.airbyte-ci.clean
 
--- tools/bin/make/airbyte_ci_dev_install.sh
+++ tools/bin/make/airbyte_ci_dev_install.sh
@@ -1,5 +1,24 @@
 #!/bin/bash
 
+# Check if pyenv is installed, and install it if not
+if ! which pyenv >/dev/null; then
+    echo "pyenv not found, installing pyenv..."
+    brew install pyenv
+    echo "pyenv installed."
+else
+    echo "pyenv is already installed."
+fi
+
+# Check if pipx is installed, and install it if not
+if ! which pipx >/dev/null; then
+    echo "pipx not found, installing pipx..."
+    python -m pip install --user pipx
+    python -m pipx ensurepath
+    echo "pipx installed."
+else
+    echo "pipx is already installed."
+fi
+
 # Install airbyte-ci development version
 pipx install --editable --force --python=python3.10 airbyte-ci/connectors/pipelines/
 echo "Development version of airbyte-ci installed."
