9a2d9c66e4fae486934cb03263bf5941a2ebaac4
Edward Gao <edward.gao@airbyte.io>
2023-11-15 16:34:27 -0800

    fix test

--- airbyte-cdk/java/airbyte-cdk/typing-deduping/src/testFixtures/java/io/airbyte/integrations/base/destination/typing_deduping/BaseSqlGeneratorIntegrationTest.java
+++ airbyte-cdk/java/airbyte-cdk/typing-deduping/src/testFixtures/java/io/airbyte/integrations/base/destination/typing_deduping/BaseSqlGeneratorIntegrationTest.java
@@ -1195,7 +1195,10 @@ public abstract class BaseSqlGeneratorIntegrationTest<DialectTableDefinition> {
           () -> assertEquals(v1id, v2RecordMap.get(v1id).get("_airbyte_raw_id").asText()),
           () -> assertEquals(v1Record.get("_airbyte_emitted_at").asText(), v2RecordMap.get(v1id).get("_airbyte_extracted_at").asText()),
           () -> assertNull(v2RecordMap.get(v1id).get("_airbyte_loaded_at")));
-      final JsonNode originalData = v1Record.get("_airbyte_data");
+      JsonNode originalData = v1Record.get("_airbyte_data");
+      if (originalData.isTextual()) {
+        originalData = Jsons.deserializeExact(originalData.asText());
+      }
       JsonNode migratedData = v2RecordMap.get(v1id).get("_airbyte_data");
       if (migratedData.isTextual()) {
         migratedData = Jsons.deserializeExact(migratedData.asText());
