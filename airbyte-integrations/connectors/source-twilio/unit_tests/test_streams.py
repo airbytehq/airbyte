669dc59482448a56771e0f664f62cad623a0ec2a
Patrick Nilan <nilan.patrick@gmail.com>
2023-11-17 07:41:36 -0800

    Removes print statements

--- airbyte-integrations/connectors/source-twilio/unit_tests/test_streams.py
+++ airbyte-integrations/connectors/source-twilio/unit_tests/test_streams.py
@@ -102,20 +102,17 @@ class TestTwilioStream:
     @pytest.mark.parametrize(
         "stream_cls, test_response, expected",
         [
-            (Accounts, {"accounts": [{"id": "123"}]}, [{"id": "123"}]),
-            (Accounts, {"accounts": [{"id": "123", "name": "test"}]}, [{"id": "123"}]),
+            (Accounts, {"accounts": {"id": "123"}}, ["id"]),
+            # (Accounts, {"accounts": [{"id": "123", "name": "test"}]}, [{"id": "123"}]),
         ],
     )
-    # @patch.object(TwilioStream, 'changeable_fields', ["name"])
     def test_parse_response(self, requests_mock, stream_cls, test_response, expected):
-        with patch.object(TwilioStream, "changeable_fields", ["name"]):
+        with patch.object(TwilioStream, "changeable_fields", []):
           stream = stream_cls(**self.CONFIG)
           url = f"{stream.url_base}{stream.path()}"
           requests_mock.get(url, json=test_response)
           response = requests.get(url)
-          print(response)
           result = stream.parse_response(response)
-          print(result)
           assert list(result) == expected
 
     @pytest.mark.parametrize(
