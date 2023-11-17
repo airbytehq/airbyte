e6bff5fabd0f823e8f4dbc9e9204a9f087ba267a
Edward Gao <edward.gao@airbyte.io>
2023-11-17 09:59:44 -0800

    add test, use super methods

--- airbyte-integrations/connectors/source-redshift/src/main/java/io/airbyte/integrations/source/redshift/RedshiftSourceOperations.java
+++ airbyte-integrations/connectors/source-redshift/src/main/java/io/airbyte/integrations/source/redshift/RedshiftSourceOperations.java
@@ -29,34 +29,15 @@ public class RedshiftSourceOperations extends JdbcSourceOperations {
 
   @Override
   public void copyToJsonField(final ResultSet resultSet, final int colIndex, final ObjectNode json) throws SQLException {
-    final int columnTypeInt = resultSet.getMetaData().getColumnType(colIndex);
-    final String columnName = resultSet.getMetaData().getColumnName(colIndex);
-    final JDBCType columnType = safeGetJdbcType(columnTypeInt);
 
     if ("timestamptz".equalsIgnoreCase(resultSet.getMetaData().getColumnTypeName(colIndex))) {
       // Massive hack. Sometimes the JDBCType is TIMESTAMP (i.e. without timezone)
       // even though it _should_ be TIMESTAMP_WITH_TIMEZONE.
       // Check for this case explicitly.
+      final String columnName = resultSet.getMetaData().getColumnName(colIndex);
       putTimestampWithTimezone(json, columnName, resultSet, colIndex);
     } else {
-      // https://www.cis.upenn.edu/~bcpierce/courses/629/jdkdocs/guide/jdbc/getstart/mapping.doc.html
-      switch (columnType) {
-        case BIT, BOOLEAN -> putBoolean(json, columnName, resultSet, colIndex);
-        case TINYINT, SMALLINT -> putShortInt(json, columnName, resultSet, colIndex);
-        case INTEGER -> putInteger(json, columnName, resultSet, colIndex);
-        case BIGINT -> putBigInt(json, columnName, resultSet, colIndex);
-        case FLOAT, DOUBLE -> putDouble(json, columnName, resultSet, colIndex);
-        case REAL -> putFloat(json, columnName, resultSet, colIndex);
-        case NUMERIC, DECIMAL -> putBigDecimal(json, columnName, resultSet, colIndex);
-        case CHAR, VARCHAR, LONGVARCHAR -> putString(json, columnName, resultSet, colIndex);
-        case DATE -> putDate(json, columnName, resultSet, colIndex);
-        case TIME -> putTime(json, columnName, resultSet, colIndex);
-        case TIMESTAMP -> putTimestamp(json, columnName, resultSet, colIndex);
-        case TIMESTAMP_WITH_TIMEZONE -> putTimestampWithTimezone(json, columnName, resultSet, colIndex);
-        case BLOB, BINARY, VARBINARY, LONGVARBINARY -> putBinary(json, columnName, resultSet, colIndex);
-        case ARRAY -> putArray(json, columnName, resultSet, colIndex);
-        default -> putDefault(json, columnName, resultSet, colIndex);
-      }
+      super.copyToJsonField(resultSet, colIndex, json);
     }
   }
 
@@ -85,9 +66,7 @@ public class RedshiftSourceOperations extends JdbcSourceOperations {
   protected void putTimestampWithTimezone(final ObjectNode node, final String columnName, final ResultSet resultSet, final int index)
       throws SQLException {
     try {
-      final OffsetDateTime timestamptz = getObject(resultSet, index, OffsetDateTime.class);
-      final LocalDate localDate = timestamptz.toLocalDate();
-      node.put(columnName, resolveEra(localDate, timestamptz.format(TIMESTAMPTZ_FORMATTER)));
+      super.putTimestampWithTimezone(node, columnName, resultSet, index);
     } catch (final Exception e) {
       final Instant instant = resultSet.getTimestamp(index).toInstant();
       node.put(columnName, instant.toString());
