034efe7732611dd8e19d3244ff7ed0c21516c43e
Artem Inzhyyants <artem.inzhyyants@gmail.com>
2023-11-17 17:33:54 +0100

    Source Amazon Seller Partner: add test for report options

--- airbyte-integrations/connectors/source-amazon-seller-partner/unit_tests/test_source.py
+++ airbyte-integrations/connectors/source-amazon-seller-partner/unit_tests/test_source.py
@@ -3,17 +3,11 @@
 #
 
 import pytest
-from airbyte_cdk.sources.streams import Stream
 from source_amazon_seller_partner import SourceAmazonSellerPartner
 
 
 @pytest.fixture
-def connector_source():
-    return SourceAmazonSellerPartner()
-
-
-@pytest.fixture
-def connector_config():
+def connector_config_with_report_options():
     return {
         "replication_start_date": "2017-01-25T00:00:00Z",
         "replication_end_date": "2017-02-25T00:00:00Z",
@@ -23,9 +17,33 @@ def connector_config():
         "lwa_client_secret": "abc123",
         "aws_environment": "SANDBOX",
         "region": "US",
+        "report_options_list": [
+            {
+                "stream_name": "GET_FBA_FULFILLMENT_CUSTOMER_RETURNS_DATA",
+                "options_list": [
+                    {
+                        "option_name": "some_name_1",
+                        "option_value": "some_value_1"
+                    },
+
+                    {
+                        "option_name": "some_name_2",
+                        "option_value": "some_value_2"
+                    }
+                ]
+            }
+        ],
     }
 
 
-def test_streams(connector_source, connector_config):
-    for stream in connector_source.streams(connector_config):
-        assert isinstance(stream, Stream)
+NOT_SUPPORTED_STREAMS_REPORT_OPTIONS = ["VendorDirectFulfillmentShipping", "Orders", "OrderItems", "ListFinancialEventGroups", "ListFinancialEvents"]
+
+
+def test_config_report_options(connector_config_with_report_options):
+    streams = SourceAmazonSellerPartner().streams(connector_config_with_report_options)
+    fba_fulfilment_stream = [x for x in streams if x.name == "GET_FBA_FULFILLMENT_CUSTOMER_RETURNS_DATA"][0]
+    assert fba_fulfilment_stream.report_options() == {"option_name": "some_name_2", "option_value": "some_value_2"}
+    all_other_streams = (x for x in streams if
+                         x.name not in ["GET_FBA_FULFILLMENT_CUSTOMER_RETURNS_DATA", *NOT_SUPPORTED_STREAMS_REPORT_OPTIONS])
+    for streams in all_other_streams:
+        assert not streams.report_options()
