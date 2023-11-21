952bf9a7ab6a7df5dbe47bb7f03367c0ca3197c9
Aaron Steers <aj@airbyte.io>
2023-11-21 14:56:15 -0800

    add manifest metadata and tests

--- airbyte-integrations/connectors/source-dbt-duckdb/source_dbt_duckdb/utils/dbt.py
+++ airbyte-integrations/connectors/source-dbt-duckdb/source_dbt_duckdb/utils/dbt.py
@@ -1,28 +1,49 @@
 import os
-import subprocess
 from airbyte_cdk.logger import AirbyteLogger
 from pathlib import Path
 
 from dbt.cli.main import dbtRunner, dbtRunnerResult
-
-# initialize
-dbt = dbtRunner()
+from dbt.contracts.graph.manifest import Manifest
 
 
-def run_dbt(
-    *args,
+def get_dbt_manifest(
+    *,
     project_dir: str,
     logger: AirbyteLogger,
     profiles_dir: str = None,
     env_vars: dict[str, str] = None,
+) -> Manifest:
+    # use 'parse' command to load a Manifest
+    dbt_runner_result: dbtRunnerResult = invoke_dbt(
+        "parse",
+        project_dir=project_dir,
+        logger=logger,
+        profiles_dir=profiles_dir,
+        env_vars=env_vars,
+    )
+    return dbt_runner_result.result
+
+
+def invoke_dbt(
+    command: str = "run",
+    *,
+    project_dir: str,
+    logger: AirbyteLogger,
+    profiles_dir: str = None,
+    env_vars: dict[str, str] = None,
+    manifest: Manifest = None,
     **kwargs,
-) -> None:
+) -> dbtRunnerResult:
     profiles_dir = profiles_dir or project_dir
 
     profiles_dir_path = Path(profiles_dir).absolute()
     project_dir_path = Path(project_dir).absolute()
 
-    arg_list = list(args) + [f"--project-dir={project_dir_path}" ,f"--profiles-dir={profiles_dir_path}"] + [f"--{key}={value}" for key, value in kwargs.items() if value is not None]
+    arg_list = [
+        command,
+        f"--project-dir={project_dir_path}",
+        f"--profiles-dir={profiles_dir_path}",
+    ] + [f"--{key}={value}" for key, value in kwargs.items() if value is not None]
     logger.info(f"Running dbt with arguments: {arg_list}")
 
     # Set NO_COLOR to prevent dbt from printing ANSI color codes
@@ -33,11 +54,16 @@ def run_dbt(
     }
 
     os.environ.update(env_vars)
-    results_obj: dbtRunnerResult = dbt.invoke(arg_list)
 
-    if not results_obj.success:
-        logger.info(f'An error occurred. Log: {results_obj.exception}')
-        raise results_obj.exception
+    dbt = dbtRunner(manifest=manifest)
+    dbt_runner_result: dbtRunnerResult = dbt.invoke(arg_list)
 
-    for r in results_obj.result:
-        logger.info(f"dbt result for '{r.node.name}': {r.status}")
+    if not dbt_runner_result.success:
+        logger.info(f"An error occurred. Log: {dbt_runner_result.exception}")
+        raise dbt_runner_result.exception
+
+    if command in ["run"]:
+        for r in dbt_runner_result.result:
+            logger.info(f"dbt result for '{r.node.name}': {r.status}")
+
+    return dbt_runner_result
--- airbyte-integrations/connectors/source-dbt-duckdb/unit_tests/unit_test.py
+++ airbyte-integrations/connectors/source-dbt-duckdb/unit_tests/unit_test.py
@@ -2,34 +2,40 @@
 # Copyright (c) 2023 Airbyte, Inc., all rights reserved.
 #
 
+import json
 import pytest
 from pathlib import Path
-
-from airbyte_cdk.entrypoint import launch
-from source_dbt_duckdb import SourceDbtDuckDB
+from typing import Any
 
 from airbyte_cdk import models
-from typing import Any
-from pathlib import Path
-import json
+from airbyte_cdk.entrypoint import launch
+
+from source_dbt_duckdb import SourceDbtDuckDB
+
 
 SAMPLE_CATALOG_PATH = Path(__file__).parent.parent / "sample-catalog.json"
-SAMPLE_CONFIGURED_CATALOG_PATH = Path(__file__).parent.parent / "sample-configured-catalog.json"
+SAMPLE_CONFIGURED_CATALOG_PATH = (
+    Path(__file__).parent.parent / "sample-configured-catalog.json"
+)
 SAMPLE_CONFIG_PATH = Path(__file__).parent.parent / "sample-config.json"
+
 
 @pytest.fixture
 def sample_catalog() -> models.AirbyteCatalog:
     catalog = json.loads(SAMPLE_CATALOG_PATH.read_text())
     return models.AirbyteCatalog.parse_obj(catalog)
 
+
 @pytest.fixture
 def sample_configured_catalog() -> models.ConfiguredAirbyteCatalog:
     catalog = json.loads(SAMPLE_CONFIGURED_CATALOG_PATH.read_text())
     return models.ConfiguredAirbyteCatalog.parse_obj(catalog)
 
+
 @pytest.fixture
 def sample_config() -> dict[str, Any]:
     return json.loads(SAMPLE_CONFIG_PATH.read_text())
+
 
 def test_test_sync(sample_configured_catalog, sample_config):
     """Test that we can run a sync."""
@@ -39,5 +45,5 @@ def test_test_sync(sample_configured_catalog, sample_config):
             "read",
             f"--catalog={SAMPLE_CONFIGURED_CATALOG_PATH}",
             f"--config={SAMPLE_CONFIG_PATH}",
-        ]
+        ],
     )
