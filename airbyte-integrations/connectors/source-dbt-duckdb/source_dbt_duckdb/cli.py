200bb5531df07084258fa6906e1b745cd720faa9
Aaron Steers <aj@airbyte.io>
2023-11-20 18:48:19 -0800

    move main.py into package, rename to cli.py

--- airbyte-integrations/connectors/source-dbt-duckdb/source_dbt_duckdb/cli.py
+++ airbyte-integrations/connectors/source-dbt-duckdb/source_dbt_duckdb/cli.py
@@ -2,12 +2,14 @@
 # Copyright (c) 2023 Airbyte, Inc., all rights reserved.
 #
 
-
 import sys
 
 from airbyte_cdk.entrypoint import launch
 from source_dbt_duckdb import SourceDbtDuckdb
 
-if __name__ == "__main__":
+def main():
     source = SourceDbtDuckdb()
     launch(source, sys.argv[1:])
+
+if __name__ == "__main__":
+    main()
