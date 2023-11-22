0f65e96c590f57bb9a4c77e99965f8b43be4ac11
flash1293 <flash1293@users.noreply.github.com>
2023-11-22 11:04:12 +0000

    Automated Commit - Formatting Changes

--- tools/bin/bump_cdk.py
+++ tools/bin/bump_cdk.py
@@ -1,3 +1,5 @@
+# Copyright (c) 2023 Airbyte, Inc., all rights reserved.
+
 # This script takes an arg with a connector selector, then bumps the CDK version of all connectors that match that selector
 # Example: python tools/bin/bump_cdk.py --selector=source-s3 <- this targets the s3 source connector
 # Example: python tools/bin/bump_cdk.py --selector=connectorSubtype:vectorstore <- this targets all connectors with subtype vectorstore
@@ -11,11 +13,11 @@ import re
 import subprocess
 import sys
 
-parser = argparse.ArgumentParser(description='Bump CDK version for connectors')
-parser.add_argument('--selector', required=True, help='Connector selector')
-parser.add_argument('--pr', required=True, help='PR Number')
-parser.add_argument('--version', required=True, help='New CDK version number')
-parser.add_argument('--changelog', required=True, help='Changelog entry')
+parser = argparse.ArgumentParser(description="Bump CDK version for connectors")
+parser.add_argument("--selector", required=True, help="Connector selector")
+parser.add_argument("--pr", required=True, help="PR Number")
+parser.add_argument("--version", required=True, help="New CDK version number")
+parser.add_argument("--changelog", required=True, help="Changelog entry")
 args = parser.parse_args()
 
 # store the selector in a variable
@@ -28,32 +30,32 @@ selector = args.selector
 # Example: connectorSubtype:vectorstore,source-s3,connectorSubtype:vectorstore -> {connectorSubtype: vectorstore, name: source-s3, connectorSubtype: vectorstore}
 
 # Split the selector into an array by comma
-selectorArray = selector.split(',')
+selectorArray = selector.split(",")
 # Create a new array to store the key value pairs
 selectorArrayKeyValue = []
 # Loop through the selector array
 for selector in selectorArray:
     # If the selector has a colon, split on that
-    if ':' in selector:
-        selectorKeyValue = selector.split(':')
+    if ":" in selector:
+        selectorKeyValue = selector.split(":")
         selectorArrayKeyValue.append({selectorKeyValue[0]: selectorKeyValue[1]})
     # Otherwise, the key is name and the value is the selector
     else:
-        selectorArrayKeyValue.append({'connector': selector})
+        selectorArrayKeyValue.append({"connector": selector})
 
 # Get all folders in airbyte-integrations/connectors
-connectorFolders = os.listdir('./airbyte-integrations/connectors')
+connectorFolders = os.listdir("./airbyte-integrations/connectors")
 
 connectors_to_bump = []
 
 # Loop through the connector folders and read the metadata.yaml file as object
 for connectorFolder in connectorFolders:
     # Get the manifest file path
-    manifestFilePath = f'./airbyte-integrations/connectors/{connectorFolder}/metadata.yaml'
+    manifestFilePath = f"./airbyte-integrations/connectors/{connectorFolder}/metadata.yaml"
     # Check if the manifest file exists
     if os.path.exists(manifestFilePath):
         # Read the manifest file as object
-        manifestFile = open(manifestFilePath, 'r')
+        manifestFile = open(manifestFilePath, "r")
         manifestFileObject = manifestFile.read()
         manifestFile.close()
         # Loop through the selector array key value pairs
@@ -61,12 +63,12 @@ for connectorFolder in connectorFolders:
             # Loop through the key value pairs
             for key, value in selector.items():
                 # If the key is connector, check if the connector name matches the value
-                if key == 'connector':
+                if key == "connector":
                     if connectorFolder == value:
                         connectors_to_bump.append(connectorFolder)
                 # If the key is connectorSubtype, check if the connectorSubtype matches the value
                 # TODO use actual yaml parsing for this
-                elif re.search(f'{key}: {value}', manifestFileObject):
+                elif re.search(f"{key}: {value}", manifestFileObject):
                     connectors_to_bump.append(connectorFolder)
 
 print("Found connectors to bump:")
@@ -87,15 +89,17 @@ print(f"New CDK version: {current_cdk_version}")
 # Loop through the connectors to bump
 for connector in connectors_to_bump:
     # Get the setup file path
-    setupFilePath = f'./airbyte-integrations/connectors/{connector}/setup.py'
+    setupFilePath = f"./airbyte-integrations/connectors/{connector}/setup.py"
     # Check if the setup file exists
     if os.path.exists(setupFilePath):
         # Read the setup file as object
-        setupFile = open(setupFilePath, 'r')
+        setupFile = open(setupFilePath, "r")
         setupFileObject = setupFile.read()
         setupFile.close()
         # Find the airbyte-cdk dependency
-        airbyte_cdk_dependency = re.search(r'airbyte-cdk(?P<extra>\[[a-zA-Z0-9-]*\])?(?P<version>[<>=!~]+[0-9]*\.[0-9]*\.[0-9]*)?', setupFileObject)
+        airbyte_cdk_dependency = re.search(
+            r"airbyte-cdk(?P<extra>\[[a-zA-Z0-9-]*\])?(?P<version>[<>=!~]+[0-9]*\.[0-9]*\.[0-9]*)?", setupFileObject
+        )
         # If there is no airbyte-cdk dependency, add the version
         if airbyte_cdk_dependency is not None:
 
@@ -105,11 +109,13 @@ for connector in connectors_to_bump:
         else:
             print(f"No airbyte-cdk dependency found, skipping {connector}")
         # Write the setup file
-        setupFile = open(setupFilePath, 'w')
+        setupFile = open(setupFilePath, "w")
         setupFile.write(setupFileObject)
         setupFile.close()
 
         # Bump version via airbyte-ci = airbyte-ci connectors --name={connector} bump_version patch {pr number from args} "Update CDK version: {changelog from args}"
-        subprocess.run(f"airbyte-ci connectors --name={connector} bump_version patch {args.pr} \"Update CDK version: {args.changelog}\"", shell=True)
+        subprocess.run(
+            f'airbyte-ci connectors --name={connector} bump_version patch {args.pr} "Update CDK version: {args.changelog}"', shell=True
+        )
     else:
         print(f"No setup.py found, skipping {connector}")
