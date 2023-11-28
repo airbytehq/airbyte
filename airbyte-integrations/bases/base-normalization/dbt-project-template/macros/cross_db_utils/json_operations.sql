5df9a4bd5f632d18932b0f673b816d1a21d146f2
Florian Schirmer <jolt@tuxbox.org>
2023-11-28 20:28:34 +0100

    Fix MariaDB compatibility

--- airbyte-integrations/bases/base-normalization/dbt-project-template/macros/cross_db_utils/json_operations.sql
+++ airbyte-integrations/bases/base-normalization/dbt-project-template/macros/cross_db_utils/json_operations.sql
@@ -213,7 +213,7 @@
 {%- endmacro %}
 
 {% macro mysql__json_extract_scalar(json_column, json_path_list, normalized_json_path) -%}
-    json_value({{ json_column }}, {{ format_json_path(normalized_json_path) }} RETURNING CHAR)
+    json_unquote(json_extract({{ json_column }}, {{ format_json_path(normalized_json_path) }}))
 {%- endmacro %}
 
 {% macro redshift__json_extract_scalar(json_column, json_path_list, normalized_json_path) -%}
