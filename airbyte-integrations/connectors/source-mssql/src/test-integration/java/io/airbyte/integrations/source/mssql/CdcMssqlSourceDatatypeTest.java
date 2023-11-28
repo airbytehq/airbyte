7399531ff33bd9c2231fed8088fee91144d91ec3
Stephane Geneix <stephane@airbyte.io>
2023-11-28 10:33:34 -0800

    introduce ImageModifier to MS SQL source

--- airbyte-integrations/connectors/source-mssql/src/test-integration/java/io/airbyte/integrations/source/mssql/CdcMssqlSourceDatatypeTest.java
+++ airbyte-integrations/connectors/source-mssql/src/test-integration/java/io/airbyte/integrations/source/mssql/CdcMssqlSourceDatatypeTest.java
@@ -8,6 +8,7 @@ import com.fasterxml.jackson.databind.JsonNode;
 import io.airbyte.cdk.db.Database;
 import io.airbyte.cdk.integrations.standardtest.source.TestDestinationEnv;
 import io.airbyte.integrations.source.mssql.MsSQLTestDatabase.BaseImage;
+import io.airbyte.integrations.source.mssql.MsSQLTestDatabase.ContainerModifier;
 
 public class CdcMssqlSourceDatatypeTest extends AbstractMssqlSourceDatatypeTest {
 
@@ -21,7 +22,7 @@ public class CdcMssqlSourceDatatypeTest extends AbstractMssqlSourceDatatypeTest 
 
   @Override
   protected Database setupDatabase() {
-    testdb = MsSQLTestDatabase.in(BaseImage.MSSQL_2022_LATEST, "withAgent")
+    testdb = MsSQLTestDatabase.in(BaseImage.MSSQL_2022_LATEST, ContainerModifier.AGENT)
         .withSnapshotIsolation()
         .withCdc();
     return testdb.getDatabase();
