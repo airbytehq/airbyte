2bde54a4392bb206dfe2a14e76fdbf2b5d40e568
Ben Church <ben@airbyte.io>
2023-11-29 11:36:58 -0800

    Revert "airbyte-ci: enable architecture selection on connector build (#32816)"
    This reverts commit 965ed210dc454d040d1197064b71a75e3e9f3433.

--- airbyte-ci/connectors/pipelines/pipelines/consts.py
+++ airbyte-ci/connectors/pipelines/pipelines/consts.py
@@ -2,6 +2,7 @@
 # Copyright (c) 2023 Airbyte, Inc., all rights reserved.
 #
 
+import os
 import platform
 from enum import Enum
 
@@ -19,7 +20,7 @@ CONNECTOR_TESTING_REQUIREMENTS = [
     "pytest-custom_exit_code",
 ]
 
-BUILD_PLATFORMS = (Platform("linux/amd64"), Platform("linux/arm64"))
+BUILD_PLATFORMS = [Platform("linux/amd64"), Platform("linux/arm64")]
 
 PLATFORM_MACHINE_TO_DAGGER_PLATFORM = {
     "x86_64": Platform("linux/amd64"),
@@ -27,8 +28,7 @@ PLATFORM_MACHINE_TO_DAGGER_PLATFORM = {
     "aarch64": Platform("linux/amd64"),
     "amd64": Platform("linux/amd64"),
 }
-LOCAL_MACHINE_TYPE = platform.machine()
-LOCAL_BUILD_PLATFORM = PLATFORM_MACHINE_TO_DAGGER_PLATFORM[LOCAL_MACHINE_TYPE]
+LOCAL_BUILD_PLATFORM = PLATFORM_MACHINE_TO_DAGGER_PLATFORM[platform.machine()]
 AMAZONCORRETTO_IMAGE = "amazoncorretto:17.0.8-al2023"
 NODE_IMAGE = "node:18.18.0-slim"
 GO_IMAGE = "golang:1.17"
