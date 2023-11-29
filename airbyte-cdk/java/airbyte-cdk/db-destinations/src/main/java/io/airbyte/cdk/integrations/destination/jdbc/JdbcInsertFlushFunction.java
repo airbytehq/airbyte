00676163602f2e0aec0edb6b53540d1975775344
Edward Gao <edward.gao@airbyte.io>
2023-11-29 10:18:12 -0800

    actually lets use 5mb

--- airbyte-cdk/java/airbyte-cdk/db-destinations/src/main/java/io/airbyte/cdk/integrations/destination/jdbc/JdbcInsertFlushFunction.java
+++ airbyte-cdk/java/airbyte-cdk/db-destinations/src/main/java/io/airbyte/cdk/integrations/destination/jdbc/JdbcInsertFlushFunction.java
@@ -28,15 +28,15 @@ public class JdbcInsertFlushFunction implements DestinationFlushFunction {
 
   @Override
   public long getOptimalBatchSizeBytes() {
-    // use 2MB batches.
+    // use 5MB batches.
     // historical context:
     // old JDBC connectors actually split their batches down into 10K record partitions,
     // so they use GlobalDataSizeConstants.DEFAULT_MAX_BATCH_SIZE_BYTES batches (i.e. 25MiB).
     // we want to stop doing that partitioning and just rely on these batches to be right-sized,
-    // so shrinking down the batches. 2MB is a conservative estimate; we'll need to tune it.
+    // so shrinking down the batches. 5MB is a conservative estimate; we'll need to tune it.
     // destination-redshift is the first destination to use this behavior, which will be done
     // as part of the DV2 work.
-    return 2 * 1024 * 1024;
+    return 5 * 1024 * 1024;
   }
 
 }
