b42a93ead9819e0b85683c6018bde051851ee9cb
Edward Gao <edward.gao@airbyte.io>
2023-11-29 13:34:37 -0800

    revert to 25MB batch

--- airbyte-cdk/java/airbyte-cdk/db-destinations/src/main/java/io/airbyte/cdk/integrations/destination/jdbc/JdbcInsertFlushFunction.java
+++ airbyte-cdk/java/airbyte-cdk/db-destinations/src/main/java/io/airbyte/cdk/integrations/destination/jdbc/JdbcInsertFlushFunction.java
@@ -5,6 +5,7 @@
 package io.airbyte.cdk.integrations.destination.jdbc;
 
 import io.airbyte.cdk.integrations.destination.buffered_stream_consumer.RecordWriter;
+import io.airbyte.cdk.integrations.destination.jdbc.constants.GlobalDataSizeConstants;
 import io.airbyte.cdk.integrations.destination_async.DestinationFlushFunction;
 import io.airbyte.cdk.integrations.destination_async.partial_messages.PartialAirbyteMessage;
 import io.airbyte.protocol.models.v0.AirbyteStreamNameNamespacePair;
@@ -28,15 +29,9 @@ public class JdbcInsertFlushFunction implements DestinationFlushFunction {
 
   @Override
   public long getOptimalBatchSizeBytes() {
-    // use 5MB batches.
-    // historical context:
-    // old JDBC connectors actually split their batches down into 10K record partitions,
-    // so they use GlobalDataSizeConstants.DEFAULT_MAX_BATCH_SIZE_BYTES batches (i.e. 25MiB).
-    // we want to stop doing that partitioning and just rely on these batches to be right-sized,
-    // so shrinking down the batches. 5MB is a conservative estimate; we'll need to tune it.
-    // destination-redshift is the first destination to use this behavior, which will be done
-    // as part of the DV2 work.
-    return 5 * 1024 * 1024;
+    // TODO tune this value - currently SqlOperationUtils partitions 10K records per insert statement,
+    // but we'd like to stop doing that and instead control sql insert statement size via batch size.
+    return GlobalDataSizeConstants.DEFAULT_MAX_BATCH_SIZE_BYTES;
   }
 
 }
