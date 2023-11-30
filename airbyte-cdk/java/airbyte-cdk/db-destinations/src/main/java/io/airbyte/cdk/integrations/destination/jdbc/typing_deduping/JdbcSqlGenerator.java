0e0a59dd8d49291d672e222b6e9231e22527bf01
Gireesh Sreepathi <gisripa@gmail.com>
2023-11-30 11:16:07 -0800

    Cleaning dead code and obsolote todo
    Signed-off-by: Gireesh Sreepathi <gisripa@gmail.com>

--- airbyte-cdk/java/airbyte-cdk/db-destinations/src/main/java/io/airbyte/cdk/integrations/destination/jdbc/typing_deduping/JdbcSqlGenerator.java
+++ airbyte-cdk/java/airbyte-cdk/db-destinations/src/main/java/io/airbyte/cdk/integrations/destination/jdbc/typing_deduping/JdbcSqlGenerator.java
@@ -57,65 +57,14 @@ public abstract class JdbcSqlGenerator implements SqlGenerator<TableDefinition> 
         namingTransformer.getIdentifier(nameWithSuffix));
   }
 
-  protected String columnsAndTypes(final Statement statement, final StreamConfig stream, final SQLType structType) throws SQLException {
-    final List<String> typeColumns = new ArrayList<>();
-    for (final Entry<ColumnId, AirbyteType> entry : stream.columns().entrySet()) {
-      typeColumns.add(
-          String.join(" ", statement.enquoteIdentifier(entry.getKey().name(), false), toDialectType(entry.getValue(), structType).getName()));
-    }
-    return String.join(",\n", typeColumns);
-  }
-
   protected String vendorId() {
     return "JDBC";
   }
 
   protected SQLType widestType() {
     // Redshift and Mysql maps their SUPER and JSON to LONGVARCHAR code in Jdbc database metadata calls.
+    // Is the default useful in any shape and form with longvarchar ?
     return JDBCType.LONGNVARCHAR;
-  }
-
-  protected SQLType preferredStructType(final LinkedHashMap<String, TypeInfoRecordSet> supportedTypes) {
-    // TODO: Make this abstract, let vendor specific handle this logic.
-    return preferredType(supportedTypes, List.of("VARIANT", "SUPER", "JSONB", "JSON"), widestType());
-  }
-
-  protected SQLType preferredType(final LinkedHashMap<String, TypeInfoRecordSet> supportedTypes,
-                                  final List<String> orderedPreference,
-                                  final SQLType fallback) {
-    final var bestType = orderedPreference.stream()
-        .filter(supportedTypes::containsKey)
-        .map(preferredType -> new CustomSqlType(preferredType, vendorId(),
-            supportedTypes.get(preferredType).dataType()))
-        .findFirst();
-    return bestType.isPresent() ? bestType.get() : fallback;
-  }
-
-  protected SQLType toDialectType(final AirbyteType type, final SQLType structType) {
-    if (type instanceof final AirbyteProtocolType airbyteProtocolType) {
-      return toDialectType(airbyteProtocolType, structType);
-    }
-    return switch (type.getTypeName()) {
-      case Struct.TYPE, UnsupportedOneOf.TYPE -> structType;
-      case Array.TYPE -> JDBCType.ARRAY;
-      case Union.TYPE -> toDialectType(((Union) type).chooseType(), structType);
-      default -> throw new IllegalArgumentException("Unsupported AirbyteType: " + type);
-    };
-  }
-
-  protected SQLType toDialectType(final AirbyteProtocolType airbyteProtocolType, final SQLType structType) {
-    return switch (airbyteProtocolType) {
-      case STRING -> JDBCType.VARCHAR;
-      case NUMBER -> JDBCType.NUMERIC;
-      case INTEGER -> JDBCType.BIGINT;
-      case BOOLEAN -> JDBCType.BOOLEAN;
-      case TIMESTAMP_WITH_TIMEZONE -> JDBCType.TIMESTAMP_WITH_TIMEZONE;
-      case TIMESTAMP_WITHOUT_TIMEZONE -> JDBCType.TIMESTAMP;
-      case TIME_WITH_TIMEZONE -> JDBCType.TIME_WITH_TIMEZONE;
-      case TIME_WITHOUT_TIMEZONE -> JDBCType.TIME;
-      case DATE -> JDBCType.DATE;
-      case UNKNOWN -> widestType();
-    };
   }
 
   protected DataType<?> toDialectType(final AirbyteType type) {
