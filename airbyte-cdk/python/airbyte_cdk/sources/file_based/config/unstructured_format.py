271d10f5ba521a8298097943aad389fd2a792dc2
Joe Reuter <joe@airbyte.io>
2023-12-01 11:18:40 +0100

    move strategy to top level

--- airbyte-cdk/python/airbyte_cdk/sources/file_based/config/unstructured_format.py
+++ airbyte-cdk/python/airbyte_cdk/sources/file_based/config/unstructured_format.py
@@ -82,7 +82,7 @@ class UnstructuredFormat(BaseModel):
         default="auto",
         title="Parsing Strategy",
         enum=["auto", "fast", "ocr_only", "hi_res"],
-        description="The strategy used to parse documents. `fast` extracts text directly from the document which doesn't work for all files. `ocr_only` is more reliable, but slower. `hi_res` is the most reliable, but requires an API key and a hosted instance of unstructured and can't be used with local mode. See the [unstructured.io documentation](https://unstructured-io.github.io/unstructured/core/partition.html#partition-pdf) for more details.",
+        description="The strategy used to parse documents. `fast` extracts text directly from the document which doesn't work for all files. `ocr_only` is more reliable, but slower. `hi_res` is the most reliable, but requires an API key and a hosted instance of unstructured and can't be used with local mode. See the unstructured.io documentation for more details: https://unstructured-io.github.io/unstructured/core/partition.html#partition-pdf",
     )
 
     processing: Union[LocalProcessingConfigModel, APIProcessingConfigModel,] = Field(
