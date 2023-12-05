d29bf51479aa9f91705f64908cdfb4ea6ea2ecf1
midavadim <midavadim@yahoo.com>
2023-12-05 06:44:03 +0200

    :tada: Source Xero handle 403 error (#32837)

--- airbyte-integrations/connectors/source-xero/unit_tests/test_source.py
+++ airbyte-integrations/connectors/source-xero/unit_tests/test_source.py
@@ -15,6 +15,18 @@ def test_check_connection(mock_auth, mock_stream, mock_response, config):
     assert source.check_connection(logger_mock, config_mock) == (True, None)
 
 
+def test_check_connection_failed(mock_auth, mock_stream, mock_response, config, requests_mock):
+    mock_stream("Organisation", response={"Organisations": [{"OrganisationID": "tenant_id"}]})
+    mock_auth({"access_token": "TOKEN", "expires_in": 123})
+
+    requests_mock.get(url="https://api.xero.com/api.xro/2.0/Organisation", status_code=403, content= b'{"status": 403, "code": "restricted_resource"}')
+
+    source = SourceXero()
+    check_succeeded, error = source.check_connection(MagicMock(), config)
+    assert check_succeeded is False
+    assert 'For oauth2 authentication try to re-authenticate and allow all requested scopes' in error
+
+
 def test_streams(config):
     source = SourceXero()
     streams = source.streams(config)
