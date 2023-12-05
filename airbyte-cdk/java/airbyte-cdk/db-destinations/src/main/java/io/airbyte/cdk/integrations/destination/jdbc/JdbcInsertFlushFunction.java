320eaf0e287461561ebfcbbffd17c80878b33ebc
edgao <edgao@users.noreply.github.com>
2023-12-05 18:40:45 +0000

    Automated Commit - Formatting Changes

--- airbyte-cdk/java/airbyte-cdk/db-destinations/src/main/java/io/airbyte/cdk/integrations/destination/jdbc/JdbcInsertFlushFunction.java
+++ airbyte-cdk/java/airbyte-cdk/db-destinations/src/main/java/io/airbyte/cdk/integrations/destination/jdbc/JdbcInsertFlushFunction.java
@@ -5,7 +5,6 @@
 package io.airbyte.cdk.integrations.destination.jdbc;
 
 import io.airbyte.cdk.integrations.destination.buffered_stream_consumer.RecordWriter;
-import io.airbyte.cdk.integrations.destination.jdbc.constants.GlobalDataSizeConstants;
 import io.airbyte.cdk.integrations.destination_async.DestinationFlushFunction;
 import io.airbyte.cdk.integrations.destination_async.partial_messages.PartialAirbyteMessage;
 import io.airbyte.protocol.models.v0.AirbyteStreamNameNamespacePair;
