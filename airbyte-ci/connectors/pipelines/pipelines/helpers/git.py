6913ff9e8503068854a13072727c86f1ac8b1206
alafanechere <augustin.lafanechere@gmail.com>
2023-12-11 12:51:21 +0100

    chore: format

--- airbyte-ci/connectors/pipelines/pipelines/helpers/git.py
+++ airbyte-ci/connectors/pipelines/pipelines/helpers/git.py
@@ -96,7 +96,7 @@ def find_all_git_ignored_items() -> List[str]:
     main_logger.info("Running git status --short --ignored to find all git ignored files or directories")
     # Use re.findall to extract all matches in a single call
     # Ignored files/dirs are prefixed with "!!" in the git status --short output
-    ignored = re.findall(r'^!! (.+?)(/)?$', status_output, re.MULTILINE)    
+    ignored = re.findall(r"^!! (.+?)(/)?$", status_output, re.MULTILINE)
     # Process the matches to remove trailing slashes
     ignored = [item[0] if item[:-1] != "/" else item[0][:-1] for item in ignored]
     return ignored
