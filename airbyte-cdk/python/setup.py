f6097bae8570eb3d804894aab1d5e428832c04ac
Flutra Osmani <129116758+flutra-osmani@users.noreply.github.com>
2023-12-12 13:42:38 -0800

    update the cdk to use the latest version of the protocol (#33340)

--- airbyte-cdk/python/setup.py
+++ airbyte-cdk/python/setup.py
@@ -65,7 +65,7 @@ setup(
     packages=find_packages(exclude=("unit_tests",)),
     package_data={"airbyte_cdk": ["py.typed", "sources/declarative/declarative_component_schema.yaml"]},
     install_requires=[
-        "airbyte-protocol-models==0.4.2",
+        "airbyte-protocol-models==0.5.1",
         "backoff",
         "dpath~=2.0.1",
         "isodate~=0.6.1",
