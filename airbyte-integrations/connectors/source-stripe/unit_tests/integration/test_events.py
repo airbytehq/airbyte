5ebea15a2abebbc8e54c363efcdb7532bf9afd97
maxi297 <maxime@airbyte.io>
2023-12-13 11:42:28 -0500

    Adding a way to consistently build HttpRequests

--- airbyte-integrations/connectors/source-stripe/unit_tests/integration/test_events.py
+++ airbyte-integrations/connectors/source-stripe/unit_tests/integration/test_events.py
@@ -2,14 +2,13 @@
 
 import json
 from datetime import datetime, timedelta, timezone
-from typing import Any, Dict, Optional, Tuple
+from typing import Any, Dict, Optional
 from unittest import TestCase
 
 import freezegun
 from airbyte_cdk.test.catalog_builder import CatalogBuilder
 from airbyte_cdk.test.entrypoint_wrapper import EntrypointOutput, read
-from airbyte_cdk.test.mock_http import HttpMocker, HttpRequest, HttpResponse
-from airbyte_cdk.test.mock_http.request import ANY_QUERY_PARAMS
+from airbyte_cdk.test.mock_http import HttpMocker, HttpResponse
 from airbyte_cdk.test.mock_http.response_builder import (
     FieldPath,
     HttpResponseBuilder,
@@ -22,6 +21,7 @@ from airbyte_cdk.test.state_builder import StateBuilder
 from airbyte_protocol.models import AirbyteStreamStatus, ConfiguredAirbyteCatalog, FailureType, SyncMode
 from integration.config import ConfigBuilder
 from integration.pagination import StripePaginationStrategy
+from integration.request_builder import StripeRequestBuilder
 from source_stripe import SourceStripe
 
 _STREAM_NAME = "events"
@@ -29,7 +29,6 @@ _NOW = datetime.now(timezone.utc)
 _A_START_DATE = _NOW - timedelta(days=60)
 _ACCOUNT_ID = "account_id"
 _CLIENT_SECRET = "client_secret"
-_AUTHENTICATION_HEADERS = {"Stripe-Account": _ACCOUNT_ID, "Authorization": f"Bearer {_CLIENT_SECRET}"}
 _NO_STATE = {}
 _HTTP_RESPONSE_STATUS_500 = HttpResponse(
     json.dumps(
@@ -37,9 +36,13 @@ _HTTP_RESPONSE_STATUS_500 = HttpResponse(
     ),
     500
 )
-_AVOIDING_INCLUSIVE_BOUNDARIES = 1
-_SECOND_REQUEST = 1
-_THIRD_REQUEST = 2
+_AVOIDING_INCLUSIVE_BOUNDARIES = timedelta(seconds=1)
+_SECOND_REQUEST = timedelta(seconds=1)
+_THIRD_REQUEST = timedelta(seconds=2)
+
+
+def _a_request() -> StripeRequestBuilder:
+    return StripeRequestBuilder("events", _ACCOUNT_ID, _CLIENT_SECRET)
 
 
 def _config() -> ConfigBuilder:
@@ -50,8 +53,8 @@ def _catalog(sync_mode: SyncMode) -> ConfiguredAirbyteCatalog:
     return CatalogBuilder().with_stream(_STREAM_NAME, sync_mode).build()
 
 
-def _source(catalog: ConfiguredAirbyteCatalog) -> SourceStripe:
-    return SourceStripe(catalog)
+def _source(catalog: ConfiguredAirbyteCatalog, config: Dict[str, Any]) -> SourceStripe:
+    return SourceStripe(catalog, config)
 
 
 def _a_record() -> RecordBuilder:
@@ -68,13 +71,14 @@ def _a_response() -> HttpResponseBuilder:
 
 
 def _read(
-    config: ConfigBuilder,
+    config_builder: ConfigBuilder,
     sync_mode: SyncMode,
     state: Optional[Dict[str, Any]] = None,
     expecting_exception: bool = False
 ) -> EntrypointOutput:
     catalog = _catalog(sync_mode)
-    return read(_source(catalog), config.build(), catalog, state, expecting_exception)
+    config = config_builder.build()
+    return read(_source(catalog, config), config, catalog, state, expecting_exception)
 
 
 @freezegun.freeze_time(_NOW.isoformat())
@@ -83,12 +87,8 @@ class FullRefreshTest(TestCase):
     @HttpMocker()
     def test_given_one_page_when_read_then_return_record(self, http_mocker: HttpMocker) -> None:
         http_mocker.get(
-            HttpRequest(
-                url="https://api.stripe.com/v1/events",
-                query_params={"created[gte]": str(int(_A_START_DATE.timestamp())), "created[lte]": str(int(_NOW.timestamp())), "limit": 100},
-                headers=_AUTHENTICATION_HEADERS
-            ),
-            _a_response().with_record(_a_record()).with_record(_a_record()).build()
+            _a_request().with_created_gte(_A_START_DATE).with_created_lte(_NOW).with_limit(100).build(),
+            _a_response().with_record(_a_record()).with_record(_a_record()).build(),
         )
         output = self._read(_config().with_start_date(_A_START_DATE))
         assert len(output.records) == 2
@@ -96,25 +96,12 @@ class FullRefreshTest(TestCase):
     @HttpMocker()
     def test_given_many_pages_when_read_then_return_records(self, http_mocker: HttpMocker) -> None:
         http_mocker.get(
-            HttpRequest(
-                url="https://api.stripe.com/v1/events",
-                query_params={"created[gte]": str(int(_A_START_DATE.timestamp())), "created[lte]": str(int(_NOW.timestamp())), "limit": 100},
-                headers=_AUTHENTICATION_HEADERS,
-            ),
-            _a_response().with_pagination().with_record(_a_record().with_id("last_record_id_from_first_page")).build()
+            _a_request().with_created_gte(_A_START_DATE).with_created_lte(_NOW).with_limit(100).build(),
+            _a_response().with_pagination().with_record(_a_record().with_id("last_record_id_from_first_page")).build(),
         )
         http_mocker.get(
-            HttpRequest(
-                url="https://api.stripe.com/v1/events",
-                query_params={
-                    "starting_after": "last_record_id_from_first_page",
-                    "created[gte]": str(int(_A_START_DATE.timestamp())),
-                    "created[lte]": str(int(_NOW.timestamp())),
-                    "limit": 100,
-                },
-                headers=_AUTHENTICATION_HEADERS,
-            ),
-            _a_response().with_record(_a_record()).with_record(_a_record()).build()
+            _a_request().with_starting_after("last_record_id_from_first_page").with_created_gte(_A_START_DATE).with_created_lte(_NOW).with_limit(100).build(),
+            _a_response().with_record(_a_record()).with_record(_a_record()).build(),
         )
         output = self._read(_config().with_start_date(_A_START_DATE))
         assert len(output.records) == 3
@@ -125,40 +112,16 @@ class FullRefreshTest(TestCase):
         slice_range = timedelta(days=30)
         slice_datetime = start_date + slice_range
         http_mocker.get(  # this first request has both gte and lte before 30 days even though we know there should not be records returned
-            HttpRequest(
-                url="https://api.stripe.com/v1/events",
-                query_params={
-                    "created[gte]": str(int(start_date.timestamp())),
-                    "created[lte]": str(int(slice_datetime.timestamp())),
-                    "limit": 100
-                },
-                headers=_AUTHENTICATION_HEADERS,
-            ),
-            _a_response().build()
+            _a_request().with_created_gte(start_date).with_created_lte(slice_datetime).with_limit(100).build(),
+            _a_response().build(),
         )
         http_mocker.get(
-            HttpRequest(
-                url="https://api.stripe.com/v1/events",
-                query_params={
-                    "created[gte]": str(int(slice_datetime.timestamp()) + _SECOND_REQUEST),
-                    "created[lte]": str(int((slice_datetime + slice_range).timestamp() + _SECOND_REQUEST)),
-                    "limit": 100
-                },
-                headers=_AUTHENTICATION_HEADERS,
-            ),
-            _a_response().build()
+            _a_request().with_created_gte(slice_datetime + _SECOND_REQUEST).with_created_lte(slice_datetime + slice_range + _SECOND_REQUEST).with_limit(100).build(),
+            _a_response().build(),
         )
         http_mocker.get(
-            HttpRequest(
-                url="https://api.stripe.com/v1/events",
-                query_params={
-                    "created[gte]": str(int((slice_datetime + slice_range).timestamp() + _THIRD_REQUEST)),
-                    "created[lte]": str(int(_NOW.timestamp())),
-                    "limit": 100
-                },
-                headers=_AUTHENTICATION_HEADERS,
-            ),
-            _a_response().build()
+            _a_request().with_created_gte(slice_datetime + slice_range + _THIRD_REQUEST).with_created_lte(_NOW).with_limit(100).build(),
+            _a_response().build(),
         )
 
         self._read(_config().with_start_date(start_date).with_slice_range_in_days(slice_range.days))
@@ -170,12 +133,8 @@ class FullRefreshTest(TestCase):
         start_date = _NOW - timedelta(days=30)
         lookback_window = timedelta(days=10)
         http_mocker.get(
-            HttpRequest(
-                url="https://api.stripe.com/v1/events",
-                query_params={"created[gte]": str(int((start_date - lookback_window).timestamp())), "created[lte]": str(int(_NOW.timestamp())), "limit": 100},
-                headers=_AUTHENTICATION_HEADERS,
-            ),
-            _a_response().build()
+            _a_request().with_created_gte(start_date - lookback_window).with_created_lte(_NOW).with_limit(100).build(),
+            _a_response().build(),
         )
 
         self._read(_config().with_start_date(start_date).with_lookback_window_in_days(lookback_window.days))
@@ -188,28 +147,12 @@ class FullRefreshTest(TestCase):
         slice_range = timedelta(days=20)
         slice_datetime = start_date + slice_range
         http_mocker.get(
-            HttpRequest(
-                url="https://api.stripe.com/v1/events",
-                query_params={
-                    "created[gte]": str(int(start_date.timestamp())),
-                    "created[lte]": str(int(slice_datetime.timestamp())),
-                    "limit": 100
-                },
-                headers=_AUTHENTICATION_HEADERS,
-            ),
-            _a_response().build()
+            _a_request().with_created_gte(start_date).with_created_lte(slice_datetime).with_limit(100).build(),
+            _a_response().build(),
         )
         http_mocker.get(
-            HttpRequest(
-                url="https://api.stripe.com/v1/events",
-                query_params={
-                    "created[gte]": str(int(slice_datetime.timestamp()) + _SECOND_REQUEST),
-                    "created[lte]": str(int(_NOW.timestamp())),
-                    "limit": 100
-                },
-                headers=_AUTHENTICATION_HEADERS,
-            ),
-            _a_response().build()
+            _a_request().with_created_gte(slice_datetime + _SECOND_REQUEST).with_created_lte(_NOW).with_limit(100).build(),
+            _a_response().build(),
         )
 
         self._read(_config().with_start_date(start_date).with_slice_range_in_days(slice_range.days))
@@ -217,11 +160,7 @@ class FullRefreshTest(TestCase):
     @HttpMocker()
     def test_given_http_status_400_when_read_then_stream_is_ignored(self, http_mocker: HttpMocker) -> None:
         http_mocker.get(
-            HttpRequest(
-                url="https://api.stripe.com/v1/events",
-                query_params=ANY_QUERY_PARAMS,
-                headers=_AUTHENTICATION_HEADERS
-            ),
+            _a_request().with_any_query_params().build(),
             HttpResponse(
                 json.dumps({
                     "error": {
@@ -241,11 +180,7 @@ class FullRefreshTest(TestCase):
     @HttpMocker()
     def test_given_http_status_401_when_read_then_stream_is_incomplete(self, http_mocker: HttpMocker) -> None:
         http_mocker.get(
-            HttpRequest(
-                url="https://api.stripe.com/v1/events",
-                query_params=ANY_QUERY_PARAMS,
-                headers=_AUTHENTICATION_HEADERS
-            ),
+            _a_request().with_any_query_params().build(),
             HttpResponse(
                 json.dumps({
                     "error": {
@@ -254,7 +189,7 @@ class FullRefreshTest(TestCase):
                     }
                 }),
                 401
-            )
+            ),
         )
         output = self._read(_config().with_start_date(_A_START_DATE), expecting_exception=True)
         assert output.errors[-1].trace.error.failure_type == FailureType.system_error
@@ -262,11 +197,7 @@ class FullRefreshTest(TestCase):
     @HttpMocker()
     def test_given_rate_limited_when_read_then_retry_and_return_records(self, http_mocker: HttpMocker) -> None:
         http_mocker.get(
-            HttpRequest(
-                url="https://api.stripe.com/v1/events",
-                query_params=ANY_QUERY_PARAMS,
-                headers=_AUTHENTICATION_HEADERS
-            ),
+            _a_request().with_any_query_params().build(),
             [
                 HttpResponse(
                     json.dumps({
@@ -280,7 +211,7 @@ class FullRefreshTest(TestCase):
                     429
                 ),
                 _a_response().with_record(_a_record()).build(),
-            ]
+            ],
         )
         output = self._read(_config().with_start_date(_A_START_DATE))
         assert len(output.records) == 1
@@ -288,12 +219,8 @@ class FullRefreshTest(TestCase):
     @HttpMocker()
     def test_given_http_status_500_once_before_200_when_read_then_retry_and_return_records(self, http_mocker: HttpMocker) -> None:
         http_mocker.get(
-            HttpRequest(
-                url="https://api.stripe.com/v1/events",
-                query_params=ANY_QUERY_PARAMS,
-                headers=_AUTHENTICATION_HEADERS
-            ),
-            [_HTTP_RESPONSE_STATUS_500, _a_response().with_record(_a_record()).build()]
+            _a_request().with_any_query_params().build(),
+            [_HTTP_RESPONSE_STATUS_500, _a_response().with_record(_a_record()).build()],
         )
         output = self._read(_config().with_start_date(_A_START_DATE))
         assert len(output.records) == 1
@@ -301,22 +228,18 @@ class FullRefreshTest(TestCase):
     @HttpMocker()
     def test_given_http_status_500_on_availability_when_read_then_stream_is_incomplete(self, http_mocker: HttpMocker) -> None:
         http_mocker.get(
-            HttpRequest(
-                url="https://api.stripe.com/v1/events",
-                query_params=ANY_QUERY_PARAMS,
-                headers=_AUTHENTICATION_HEADERS
-            ),
-            [_HTTP_RESPONSE_STATUS_500]
+            _a_request().with_any_query_params().build(),
+            [_HTTP_RESPONSE_STATUS_500],
         )
         output = self._read(_config().with_start_date(_A_START_DATE), expecting_exception=True)
         assert output.get_stream_statuses(_STREAM_NAME) == [AirbyteStreamStatus.INCOMPLETE]
 
     @HttpMocker()
     def test_when_read_then_validate_availability_for_full_refresh_and_incremental(self, http_mocker: HttpMocker) -> None:
-        request = HttpRequest(url="https://api.stripe.com/v1/events", query_params=ANY_QUERY_PARAMS, headers=_AUTHENTICATION_HEADERS)
+        request = _a_request().with_any_query_params().build()
         http_mocker.get(
             request,
-            _a_response().build()
+            _a_response().build(),
         )
         self._read(_config().with_start_date(_A_START_DATE))
         http_mocker.assert_number_of_calls(request, 3)  # one call for full_refresh availability, one call for incremental availability and one call for the actual read
@@ -332,40 +255,28 @@ class IncrementalTest(TestCase):
     def test_given_no_initial_state_when_read_then_return_state_based_on_cursor_field(self, http_mocker: HttpMocker) -> None:
         cursor_value = int(_A_START_DATE.timestamp()) + 1
         http_mocker.get(
-            HttpRequest(
-                url="https://api.stripe.com/v1/events",
-                query_params={"created[gte]": str(int(_A_START_DATE.timestamp())), "created[lte]": str(int(_NOW.timestamp())), "limit": 100},
-                headers=_AUTHENTICATION_HEADERS
-            ),
-            _a_response().with_record(_a_record().with_cursor(cursor_value)).build()
+            _a_request().with_created_gte(_A_START_DATE).with_created_lte(_NOW).with_limit(100).build(),
+            _a_response().with_record(_a_record().with_cursor(cursor_value)).build(),
         )
         output = self._read(_config().with_start_date(_A_START_DATE), {})
         assert output.most_recent_state == {"events": {"created": cursor_value}}
 
     @HttpMocker()
     def test_given_state_when_read_then_use_state_for_query_params(self, http_mocker: HttpMocker) -> None:
-        state_value = int(_A_START_DATE.timestamp()) + 1
-        availability_check_requests = HttpRequest(
-            url="https://api.stripe.com/v1/events",
-            query_params=ANY_QUERY_PARAMS,
-            headers=_AUTHENTICATION_HEADERS
-        )
+        state_value = _A_START_DATE + timedelta(seconds=1)
+        availability_check_requests = _a_request().with_any_query_params().build()
         http_mocker.get(
             availability_check_requests,
-            _a_response().with_record(_a_record()).build()
+            _a_response().with_record(_a_record()).build(),
         )
         http_mocker.get(
-            HttpRequest(
-                url="https://api.stripe.com/v1/events",
-                query_params={"created[gte]": str(state_value + _AVOIDING_INCLUSIVE_BOUNDARIES), "created[lte]": str(int(_NOW.timestamp())), "limit": 100},
-                headers=_AUTHENTICATION_HEADERS
-            ),
-            _a_response().with_record(_a_record()).build()
+            _a_request().with_created_gte(state_value + _AVOIDING_INCLUSIVE_BOUNDARIES).with_created_lte(_NOW).with_limit(100).build(),
+            _a_response().with_record(_a_record()).build(),
         )
 
         self._read(
             _config().with_start_date(_A_START_DATE),
-            StateBuilder().with_stream_state("events", {"created": state_value}).build()
+            StateBuilder().with_stream_state("events", {"created": int(state_value.timestamp())}).build()
         )
 
         # request matched http_mocker
@@ -375,12 +286,8 @@ class IncrementalTest(TestCase):
         cursor_value = int(_A_START_DATE.timestamp()) + 1
         more_recent_than_record_cursor = int(_NOW.timestamp()) - 1
         http_mocker.get(
-            HttpRequest(
-                url="https://api.stripe.com/v1/events",
-                query_params={"created[gte]": str(int(_A_START_DATE.timestamp())), "created[lte]": str(int(_NOW.timestamp())), "limit": 100},
-                headers=_AUTHENTICATION_HEADERS
-            ),
-            _a_response().with_record(_a_record().with_cursor(cursor_value)).build()
+            _a_request().with_created_gte(_A_START_DATE).with_created_lte(_NOW).with_limit(100).build(),
+            _a_response().with_record(_a_record().with_cursor(cursor_value)).build(),
         )
         output = self._read(
             _config().with_start_date(_A_START_DATE),
