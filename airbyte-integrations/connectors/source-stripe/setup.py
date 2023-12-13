9bc9c4d018fd5189ca53385a51f18547791e0d11
maxi297 <maxime@airbyte.io>
2023-12-13 10:36:34 -0500

    Ensure thread safe http mocking

--- airbyte-integrations/connectors/source-stripe/setup.py
+++ airbyte-integrations/connectors/source-stripe/setup.py
@@ -8,7 +8,7 @@ from setuptools import find_packages, setup
 # FIXME update to the version released following https://github.com/airbytehq/airbyte/pull/33305
 MAIN_REQUIREMENTS = ["airbyte-cdk==0.55.5", "stripe==2.56.0", "pendulum==2.1.2"]
 
-TEST_REQUIREMENTS = ["pytest-mock~=3.6.1", "pytest~=6.1", "requests-mock", "requests_mock~=1.8", "freezegun==1.2.2"]
+TEST_REQUIREMENTS = ["pytest-mock~=3.6.1", "pytest~=6.1", "requests-mock~=1.11.0", "requests_mock~=1.8", "freezegun==1.2.2"]
 
 setup(
     name="source_stripe",
