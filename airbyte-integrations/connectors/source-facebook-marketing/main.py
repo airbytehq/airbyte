1c84befe74f7159a37392e10644fce8c3f5c8253
Joe Reuter <joe@airbyte.io>
2023-12-13 12:50:34 +0100

    prepare for airbyte-lib

--- airbyte-integrations/connectors/source-facebook-marketing/main.py
+++ airbyte-integrations/connectors/source-facebook-marketing/main.py
@@ -2,12 +2,7 @@
 # Copyright (c) 2023 Airbyte, Inc., all rights reserved.
 #
 
-
-import sys
-
-from airbyte_cdk.entrypoint import launch
-from source_facebook_marketing import SourceFacebookMarketing
+from source_facebook_marketing.run import run
 
 if __name__ == "__main__":
-    source = SourceFacebookMarketing()
-    launch(source, sys.argv[1:])
+    run()
