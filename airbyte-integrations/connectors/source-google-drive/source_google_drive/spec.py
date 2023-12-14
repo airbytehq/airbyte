4ce8dbbbe16a3d8e5336a935cb650bfbe1095dc2
Joe Reuter <joe@airbyte.io>
2023-12-14 11:17:54 +0100

    fix specs

--- airbyte-integrations/connectors/source-google-drive/source_google_drive/spec.py
+++ airbyte-integrations/connectors/source-google-drive/source_google_drive/spec.py
@@ -78,4 +78,8 @@ class SourceGoogleDriveSpec(AbstractFileBasedSpec, BaseModel):
         dpath.util.delete(schema, "properties/streams/items/properties/legacy_prefix")
         dpath.util.delete(schema, "properties/streams/items/properties/format/oneOf/*/properties/inference_type")
 
+        # Hide API processing option until https://github.com/airbytehq/airbyte-platform-internal/issues/10354 is fixed
+        processing_options = dpath.util.get(schema, "properties/streams/items/properties/format/oneOf/4/properties/processing/oneOf")
+        dpath.util.set(schema, "properties/streams/items/properties/format/oneOf/4/properties/processing/oneOf", processing_options[:1])
+
         return schema
