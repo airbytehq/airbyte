40ccf4d598ebecba6b86bf32c9a4792e46113ed0
Joe Reuter <joe@airbyte.io>
2023-12-15 17:33:01 +0100

    make validate CLI entrypoint work

--- airbyte-lib/airbyte_lib/validate.py
+++ airbyte-lib/airbyte_lib/validate.py
@@ -4,6 +4,7 @@ import os
 from pathlib import Path
 import subprocess
 import sys
+import tempfile
 from typing import List
 import yaml
 import airbyte_lib as ab
@@ -31,22 +32,43 @@ def _run_subprocess_and_raise_on_failure(args: List[str]):
     if result.returncode != 0:
         raise Exception(f"{args} exited with code {result.returncode}")
 
+def tests(connector_name, sample_config):
+    print("Creating source and validating spec and version...")
+    source = ab.get_connector(connector_name, config=json.load(open(sample_config)))
+
+    print("Running check...")
+    source.check()
+
+    print("Fetching streams...")
+    first_stream = source.get_available_streams()[0]
+
+    source.set_streams([first_stream])
+
+    print("Performing read...")
+    source.peek(first_stream, 1)
+
 def run():
     """
     This is a CLI entrypoint for the `airbyte-lib-validate-source` command.
     It's called like this: airbyte-lib-validate-source —connector-dir . -—sample-config secrets/config.json
+
+    It performs a basic smoke test to make sure the connector in question is airbyte-lib compliant:
+    * Can be installed into a venv
+    * Can be called via cli entrypoint
+    * Answers according to the Airbyte protocol when called with spec, check, discover and read
     """
 
     # parse args
     args = _parse_args()
     connector_dir = args.connector_dir
-    connector_version = args.connector_version
     sample_config = args.sample_config
 
     # read metadata.yaml
     metadata_path = Path(connector_dir) / "metadata.yaml"
-    metadata = yaml.safe_load(metadata_path)["data"]
-    connector_name = metadata["name"]
+    with open(metadata_path, "r") as stream:
+        metadata = yaml.safe_load(stream)["data"]
+
+    connector_name = metadata["dockerRepository"].replace("airbyte/", "")
 
     # create a venv and install the connector
     venv_name = f".venv-{connector_name}"
@@ -62,39 +84,14 @@ def run():
     registry = {
         "sources": [
             {
-                "sourceDefinitionId": "9f32dab3-77cb-45a1-9d33-347aa5fbe363",
-                "name": "Test Source",
-                "dockerRepository": f"airbyte/source-{connector_name}",
+                "dockerRepository": f"airbyte/{connector_name}",
                 "dockerImageTag": "0.0.0",
-                "documentationUrl": "https://docs.airbyte.com/integrations/sources/test",
-                "icon": "test.svg",
-                "iconUrl": "https://connectors.airbyte.com/files/metadata/airbyte/source-test/latest/icon.svg",
-                "sourceType": "api",
-                "spec": {},
-                "tombstone": False,
-                "public": True,
-                "custom": False,
-                "releaseStage": "alpha",
-                "supportLevel": "community",
-                "ab_internal": {"sl": 100, "ql": 200},
-                "tags": ["language:python"],
-                "githubIssueLabel": "source-test",
-                "license": "MIT",
             }
         ]
     }
 
-    registry_path = Path("registry.json")
-    registry_path.write_text(json.dumps(registry))
-
-    os.environ["AIRBYTE_LOCAL_REGISTRY"] = str(registry_path)
-
-    source = ab.get_connector(connector_name, config=json.load(open(sample_config)))
-
-    source.check()
-
-    first_stream = source.get_available_streams()[0]
-
-    source.set_streams([first_stream])
-
-    source.peek(first_stream, 1)
+    with tempfile.NamedTemporaryFile(mode="w+t", delete=True) as temp_file:   
+        temp_file.write(json.dumps(registry))
+        temp_file.seek(0)
+        os.environ["AIRBYTE_LOCAL_REGISTRY"] = str(temp_file.name)
+        tests(connector_name, sample_config)
