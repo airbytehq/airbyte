55e1551097e580e29e3a8c49adee81717deaceb9
maxi297 <maxime@airbyte.io>
2023-12-15 09:45:28 -0500

    Adding missing request builder file

---
+++ airbyte-integrations/connectors/source-stripe/unit_tests/integration/request_builder.py
@@ -0,0 +1,95 @@
+from datetime import datetime
+from typing import List, Optional
+
+from airbyte_cdk.test.mock_http.request import ANY_QUERY_PARAMS
+from airbyte_cdk.test.mock_http import HttpRequest
+
+
+class StripeRequestBuilder:
+
+    @classmethod
+    def application_fees_endpoint(cls, account_id: str, client_secret: str) -> "StripeRequestBuilder":
+        return cls("application_fees", account_id, client_secret)
+
+    @classmethod
+    def events_endpoint(cls, account_id: str, client_secret: str) -> "StripeRequestBuilder":
+        return cls("events", account_id, client_secret)
+
+    @classmethod
+    def issuing_authorizations_endpoint(cls, account_id: str, client_secret: str) -> "StripeRequestBuilder":
+        return cls("issuing/authorizations", account_id, client_secret)
+
+    @classmethod
+    def issuing_cards_endpoint(cls, account_id: str, client_secret: str) -> "StripeRequestBuilder":
+        return cls("issuing/cards", account_id, client_secret)
+
+    @classmethod
+    def issuing_transactions_endpoint(cls, account_id: str, client_secret: str) -> "StripeRequestBuilder":
+        return cls("issuing/transactions", account_id, client_secret)
+
+    @classmethod
+    def reviews_endpoint(cls, account_id: str, client_secret: str) -> "StripeRequestBuilder":
+        return cls("reviews", account_id, client_secret)
+
+    @classmethod
+    def _for_endpoint(cls, endpoint: str, account_id: str, client_secret: str) -> "StripeRequestBuilder":
+        return cls(endpoint, account_id, client_secret)
+
+    def __init__(self, resource: str, account_id: str, client_secret: str) -> None:
+        self._resource = resource
+        self._account_id = account_id
+        self._client_secret = client_secret
+        self._any_query_params = False
+        self._created_gte: Optional[datetime] = None
+        self._created_lte: Optional[datetime] = None
+        self._limit: Optional[int] = None
+        self._starting_after_id: Optional[str] = None
+        self._types: List[str] = []
+
+    def with_created_gte(self, created_gte: datetime) -> "StripeRequestBuilder":
+        self._created_gte = created_gte
+        return self
+
+    def with_created_lte(self, created_lte: datetime) -> "StripeRequestBuilder":
+        self._created_lte = created_lte
+        return self
+
+    def with_limit(self, limit: int) -> "StripeRequestBuilder":
+        self._limit = limit
+        return self
+
+    def with_starting_after(self, starting_after_id: str) -> "StripeRequestBuilder":
+        self._starting_after_id = starting_after_id
+        return self
+
+    def with_any_query_params(self) -> "StripeRequestBuilder":
+        self._any_query_params = True
+        return self
+
+    def with_types(self, types: List[str]) -> "StripeRequestBuilder":
+        self._types = types
+        return self
+
+    def build(self) -> HttpRequest:
+        query_params = {}
+        if self._created_gte:
+            query_params["created[gte]"] = str(int(self._created_gte.timestamp()))
+        if self._created_lte:
+            query_params["created[lte]"] = str(int(self._created_lte.timestamp()))
+        if self._limit:
+            query_params["limit"] = str(self._limit)
+        if self._starting_after_id:
+            query_params["starting_after"] = self._starting_after_id
+        if self._types:
+            query_params["types[]"] = self._types
+
+        if self._any_query_params:
+            if query_params:
+                raise ValueError(f"Both `any_query_params` and {list(query_params.keys())} were configured. Provide only one of none but not both.")
+            query_params = ANY_QUERY_PARAMS
+
+        return HttpRequest(
+            url=f"https://api.stripe.com/v1/{self._resource}",
+            query_params=query_params,
+            headers={"Stripe-Account": self._account_id, "Authorization": f"Bearer {self._client_secret}"},
+        )
