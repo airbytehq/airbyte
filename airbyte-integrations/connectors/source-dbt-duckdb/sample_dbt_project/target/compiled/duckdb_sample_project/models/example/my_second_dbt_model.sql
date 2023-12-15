146c365b610dd4a0c57d580b0499dcdc31b177df
Aaron Steers <aj@airbyte.io>
2023-12-14 23:39:35 -0800

    delete files from missing .gitignore rules

--- airbyte-integrations/connectors/source-dbt-duckdb/sample_dbt_project/target/compiled/duckdb_sample_project/models/example/my_second_dbt_model.sql
+++
@@ -1,5 +0,0 @@
--- Use the `ref` function to select from other models
-
-select *
-from "dev"."main"."my_first_dbt_model"
-where id = 1
--- airbyte-integrations/connectors/source-dbt-duckdb/sample_dbt_project/target/run/duckdb_sample_project/models/example/my_second_dbt_model.sql
+++
@@ -1,9 +0,0 @@
-
-  
-  create view "dev"."main"."my_second_dbt_model__dbt_tmp" as (
-    -- Use the `ref` function to select from other models
-
-select *
-from "dev"."main"."my_first_dbt_model"
-where id = 1
-  );
