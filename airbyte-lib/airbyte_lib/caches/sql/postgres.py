e5d76c9c2f9fde357d820a0253b75ccc830cafe9
Aaron Steers <aj@airbyte.io>
2023-12-18 14:34:15 -0800

    initial commit - airbyte-lib caches implementation
    improve sql handling, add duckdb and pgsql
    
    poetry init
    
    cleanup imports
    
    add pandas option
    
    delete stale file
    
    set parquet config class
    
    Merge 'origin/flash1293/airbyte-lib' into aj/airbyt-lib-caches
    
    update deps
    
    move all non-public methods to be protected members
    
    consolidate public interface in code file
    
    delete files from missing .gitignore rules
    
    decouple file writers from caches
    
    mypy fixes, refactor filewriters,
    
    cleanup finalize_batches implementation
    
    cleanup sql cache implementation

---
+++ airbyte-lib/airbyte_lib/caches/sql/postgres.py
@@ -0,0 +1,42 @@
+"""A Postgres implementation of the cache."""
+
+from __future__ import annotations
+
+from overrides import overrides
+
+from airbyte_lib.caches.bases import SQLCache, SQLCacheConfigBase
+from airbyte_lib.file_writers import ParquetWriter, ParquetWriterConfig
+
+
+class PostgresCacheConfig(SQLCacheConfigBase, ParquetWriterConfig):
+    """Configuration for the Postgres cache.
+
+    Also inherits config from the ParquetWriter, which is responsible for writing files to disk.
+    """
+
+    type: str = "postgres"
+    host: str
+    port: int
+    username: str
+    password: str
+    database: str
+
+    # Already defined in base class:
+    # schema: str
+
+    @overrides
+    def get_sql_alchemy_url(self) -> str:
+        """Return the SQLAlchemy URL to use."""
+        return f"postgresql://{self.username}:{self.password}@{self.host}:{self.port}/{self.database}?schema={self.schema}"
+
+
+class PostgresSQLCache(SQLCache):
+    """A Postgres implementation of the cache.
+
+    Parquet is used for local file storage before bulk loading.
+    Unlike the Snowflake implementation, we can't use the COPY command to load data
+    so we insert as values instead.
+    """
+
+    config_class = PostgresCacheConfig
+    file_writer_class = ParquetWriter
