0917482d1a2619d578971d4b3b70418ff140bbf8
Gireesh Sreepathi <gisripa@gmail.com>
2023-12-18 10:51:49 -0800

    revert source changes
    Signed-off-by: Gireesh Sreepathi <gisripa@gmail.com>

--- airbyte-integrations/connectors/source-postgres/src/testFixtures/java/io/airbyte/integrations/source/postgres/PostgresTestDatabase.java
+++ airbyte-integrations/connectors/source-postgres/src/testFixtures/java/io/airbyte/integrations/source/postgres/PostgresTestDatabase.java
@@ -19,11 +19,10 @@ import org.testcontainers.containers.PostgreSQLContainer;
 public class PostgresTestDatabase extends
     TestDatabase<PostgreSQLContainer<?>, PostgresTestDatabase, PostgresTestDatabase.PostgresConfigBuilder> {
 
-  public enum BaseImage {
+  public static enum BaseImage {
 
     POSTGRES_16("postgres:16-bullseye"),
     POSTGRES_12("postgres:12-bullseye"),
-    POSTGRES_13("postgres:13-alpine"),
     POSTGRES_9("postgres:9-alpine"),
     POSTGRES_SSL_DEV("marcosmarxm/postgres-ssl:dev");
 
