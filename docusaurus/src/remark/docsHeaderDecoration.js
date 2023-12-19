f28fb8649cb1f19dbaa8c9c1f06f33e3be3738f6
Evan Tahler <evan@airbyte.io>
2023-12-19 14:26:26 -0800

    Docs Fixes (#33656)

--- docusaurus/src/remark/docsHeaderDecoration.js
+++ docusaurus/src/remark/docsHeaderDecoration.js
@@ -13,10 +13,11 @@ const fetchCatalog = async () => {
 
 const catalog = fetchCatalog();
 
-const toAttributes = (props) => Object.entries(props).map(([key, value]) => ({
-      type: "mdxJsxAttribute",
-      name: key,
-      value: value
+const toAttributes = (props) =>
+  Object.entries(props).map(([key, value]) => ({
+    type: "mdxJsxAttribute",
+    name: key,
+    value: value,
   }));
 
 const plugin = () => {
@@ -50,14 +51,16 @@ const plugin = () => {
         node.children = [];
         node.type = "mdxJsxFlowElement";
         node.name = "HeaderDecoration";
-        node.attributes =  toAttributes({
+        node.attributes = toAttributes({
           isOss: registryEntry.is_oss,
           isCloud: registryEntry.is_cloud,
           supportLevel: registryEntry.supportLevel_oss,
           dockerImageTag: registryEntry.dockerImageTag_oss,
           iconUrl: registryEntry.iconUrl_oss,
+          github_url: registryEntry.github_url,
+          issue_url: registryEntry.issue_url,
           originalTitle,
-          originalId
+          originalId,
         });
       }
     });
