dd4250588470b06c5b3fb092015700f405833e2d
Edward Gao <edward.gao@airbyte.io>
2023-12-21 13:28:52 -0800

    readability

--- airbyte-integrations/connectors/destination-mysql/src/main/java/io/airbyte/integrations/destination/mysql/typing_deduping/MysqlDestinationHandler.java
+++ airbyte-integrations/connectors/destination-mysql/src/main/java/io/airbyte/integrations/destination/mysql/typing_deduping/MysqlDestinationHandler.java
@@ -1,11 +1,16 @@
 package io.airbyte.integrations.destination.mysql.typing_deduping;
 
+import static org.jooq.impl.DSL.case_;
+import static org.jooq.impl.DSL.exists;
+import static org.jooq.impl.DSL.name;
+import static org.jooq.impl.DSL.selectOne;
+import static org.jooq.impl.DSL.using;
+
 import io.airbyte.cdk.db.jdbc.JdbcDatabase;
 import io.airbyte.cdk.integrations.destination.jdbc.typing_deduping.JdbcDestinationHandler;
 import io.airbyte.integrations.base.destination.typing_deduping.StreamId;
 import org.jooq.SQLDialect;
 import org.jooq.conf.ParamType;
-import org.jooq.impl.DSL;
 
 public class MysqlDestinationHandler extends JdbcDestinationHandler {
   public MysqlDestinationHandler(final String databaseName, final JdbcDatabase jdbcDatabase) {
@@ -17,8 +22,11 @@ public class MysqlDestinationHandler extends JdbcDestinationHandler {
     // mysql's information_schema.table.table_rows is an approximation.
     // select exists should be reasonably efficient and also gives us the information we need.
     final int rowCount = jdbcDatabase.queryInt(
-        DSL.using(SQLDialect.MYSQL).select(
-            DSL.case_().when(DSL.exists(DSL.selectOne().from(DSL.name(id.finalNamespace(), id.finalName()))), 1).else_(0)
+        using(SQLDialect.MYSQL).select(
+            case_().when(
+                exists(selectOne().from(name(id.finalNamespace(), id.finalName()))),
+                1
+            ).else_(0)
         ).getSQL(ParamType.INLINED));
     return rowCount == 0;
   }
