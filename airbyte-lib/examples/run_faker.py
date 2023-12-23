14b0408411c3cdf593b442c92f1098e189c5199d
Aaron Steers <aj@airbyte.io>
2023-12-23 09:55:17 -0800

    add faker example script (not yet working)

---
+++ airbyte-lib/examples/run_faker.py
@@ -0,0 +1,30 @@
+# Copyright (c) 2023 Airbyte, Inc., all rights reserved.
+
+from itertools import islice
+
+import airbyte_lib as ab
+
+source = ab.get_connector(
+    "source-faker",
+    config={
+        "count": 10000,
+        "seed": 0,
+        "parallelism": 1,
+        "always_updated": False
+    },
+    install_if_missing=True,
+)
+cache = ab.new_local_cache(source_catalog=source.configured_catalog)
+
+source.check()
+
+# TODO: Pur the real stream names here:
+streams = ["stream1", "stream2", "stream3"]
+# source.set_streams(["launches", "rockets", "capsules"])
+
+result = source.read_all(cache)
+
+print(islice(source.get_stream_records(streams[0]), 10))
+
+for name, records in result.cache.streams.items():
+    print(f"Stream {name}: {len(records)} records")
