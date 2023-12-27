ac5917d48eac55e89544a242caf3559ed14d95e7
Rodi Reich Zilberman <867491+rodireich@users.noreply.github.com>
2023-12-26 17:04:28 -0800

    Initial

--- airbyte-integrations/connectors/source-mssql/src/main/java/io/airbyte/integrations/source/mssql/MssqlQueryUtils.java
+++ airbyte-integrations/connectors/source-mssql/src/main/java/io/airbyte/integrations/source/mssql/MssqlQueryUtils.java
@@ -1,11 +1,16 @@
+/*
+ * Copyright (c) 2023 Airbyte, Inc., all rights reserved.
+ */
 package io.airbyte.integrations.source.mssql;
 
 import static io.airbyte.cdk.integrations.source.relationaldb.RelationalDbQueryUtils.getFullyQualifiedTableNameWithQuoting;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.microsoft.sqlserver.jdbc.SQLServerException;
 import io.airbyte.cdk.db.jdbc.JdbcDatabase;
 import io.airbyte.cdk.db.jdbc.JdbcUtils;
+import io.airbyte.commons.json.Jsons;
 import io.airbyte.protocol.models.v0.ConfiguredAirbyteCatalog;
 import io.airbyte.protocol.models.v0.ConfiguredAirbyteStream;
 import java.sql.SQLException;
@@ -19,29 +24,29 @@ import org.slf4j.LoggerFactory;
 public class MssqlQueryUtils {
   private static final Logger LOGGER = LoggerFactory.getLogger(MssqlQueryUtils.class);
   public static final String INDEX_QUERY = "EXEC sp_helpindex N'%s'";
+  public record Index(
+      @JsonProperty("index_name") String name,
+      @JsonProperty("index_description") String description,
+      @JsonProperty("index_keys") String keys
+  ) { };
 
   public static void getIndexInfoForStreams(final JdbcDatabase database, final ConfiguredAirbyteCatalog catalog, final String quoteString) {
     for (final ConfiguredAirbyteStream stream : catalog.getStreams()) {
       final String streamName = stream.getStream().getName();
       final String schemaName = stream.getStream().getNamespace();
       final String fullTableName = getFullyQualifiedTableNameWithQuoting(schemaName, streamName, quoteString);
-      LOGGER.info("Discovering indexes for schema \"{}\", table \"{}\"", schemaName, streamName);
+      LOGGER.info("Discovering indexes for table {}", fullTableName);
       try {
-
         final String query = INDEX_QUERY.formatted(fullTableName);
         LOGGER.debug("Index lookup query: {}", query);
         final List<JsonNode> jsonNodes = database.bufferedResultSetQuery(conn -> conn.prepareStatement(query).executeQuery(),
             resultSet -> JdbcUtils.getDefaultSourceOperations().rowToJson(resultSet));
-        if (jsonNodes != null && !jsonNodes.isEmpty()) {
-          for (final JsonNode node : jsonNodes) {
-            LOGGER.info("Index: name: {}. description: {}. keys {}.",
-                node.get("index_name"),
-                node.get("index_description"),
-                node.get("index_keys"));
-          }
+        if (jsonNodes != null) {
+          jsonNodes.stream().map(node -> Jsons.convertValue(node, Index.class))
+              .forEach(i -> LOGGER.info(String.valueOf(i)));
         }
-      } catch (final SQLException ex) {
-        LOGGER.info("No index found for {}", fullTableName);
+      } catch (final Exception ex) {
+        LOGGER.info("Failed to get index for {}", fullTableName);
       }
     }
 
