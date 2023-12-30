6ebde97987eca32aa650eab440321ee077c1bf5e
Rodi Reich Zilberman <867491+rodireich@users.noreply.github.com>
2023-12-29 21:09:40 -0800

    Insert change to allow submit

--- airbyte-integrations/connectors/source-mssql/src/main/java/io/airbyte/integrations/source/mssql/initialsync/MssqlInitialReadUtil.java
+++ airbyte-integrations/connectors/source-mssql/src/main/java/io/airbyte/integrations/source/mssql/initialsync/MssqlInitialReadUtil.java
@@ -182,8 +182,8 @@ public class MssqlInitialReadUtil {
   }
 
   public static InitialLoadStreams cdcStreamsForInitialOrderedCoumnLoad(final CdcStateManager stateManager,
-                                                                        final ConfiguredAirbyteCatalog fullCatalog,
-                                                                        final boolean savedOffsetStillPresentOnServer) {
+      final ConfiguredAirbyteCatalog fullCatalog,
+      final boolean savedOffsetStillPresentOnServer) {
     if (!savedOffsetStillPresentOnServer) {
       return new InitialLoadStreams(
           fullCatalog.getStreams()
@@ -230,10 +230,10 @@ public class MssqlInitialReadUtil {
   }
 
   public static Map<io.airbyte.protocol.models.AirbyteStreamNameNamespacePair, OrderedColumnInfo> initPairToOrderedColumnInfoMap(
-                                                                                                                                 final JdbcDatabase database,
-                                                                                                                                 final InitialLoadStreams initialLoadStreams,
-                                                                                                                                 final Map<String, TableInfo<CommonField<JDBCType>>> tableNameToTable,
-                                                                                                                                 final String quoteString) {
+      final JdbcDatabase database,
+      final InitialLoadStreams initialLoadStreams,
+      final Map<String, TableInfo<CommonField<JDBCType>>> tableNameToTable,
+      final String quoteString) {
     final Map<io.airbyte.protocol.models.AirbyteStreamNameNamespacePair, OrderedColumnInfo> pairToOcInfoMap = new HashMap<>();
     // For every stream that is in initial ordered column sync, we want to maintain information about
     // the current ordered column info associated with the stream
@@ -271,7 +271,7 @@ public class MssqlInitialReadUtil {
   }
 
   public static List<ConfiguredAirbyteStream> identifyStreamsToSnapshot(final ConfiguredAirbyteCatalog catalog,
-                                                                        final Set<AirbyteStreamNameNamespacePair> alreadySyncedStreams) {
+      final Set<AirbyteStreamNameNamespacePair> alreadySyncedStreams) {
     final Set<AirbyteStreamNameNamespacePair> allStreams = AirbyteStreamNameNamespacePair.fromConfiguredCatalog(catalog);
     final Set<AirbyteStreamNameNamespacePair> newlyAddedStreams = new HashSet<>(Sets.difference(allStreams, alreadySyncedStreams));
     return catalog.getStreams().stream()
