ac9edcfdb1c896a512e61d565df88db89a7516d8
Roman Yermilov <roman.yermilov@globallogic.com>
2024-01-02 22:50:16 +0100

    add timeout for custom insights streams

--- airbyte-integrations/connectors/source-facebook-marketing/source_facebook_marketing/source.py
+++ airbyte-integrations/connectors/source-facebook-marketing/source_facebook_marketing/source.py
@@ -285,6 +285,7 @@ class SourceFacebookMarketing(AbstractSource):
                 start_date=insight.start_date or config.start_date or pendulum.now().add(years=-2),
                 end_date=insight.end_date or config.end_date,
                 insights_lookback_window=insight.insights_lookback_window or config.insights_lookback_window,
+                insights_job_timeout=insight.insights_job_timeout or config.insights_job_timeout,
                 level=insight.level,
             )
             streams.append(stream)
