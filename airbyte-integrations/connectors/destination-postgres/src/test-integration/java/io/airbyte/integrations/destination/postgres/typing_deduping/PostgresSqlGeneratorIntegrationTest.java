e0a3928f17d00bd8cc338bb9d13bb2c01161ef93
Edward Gao <edward.gao@airbyte.io>
2024-01-03 15:22:37 -0800

    fix raw table fetch

--- airbyte-integrations/connectors/destination-postgres/src/test-integration/java/io/airbyte/integrations/destination/postgres/typing_deduping/PostgresSqlGeneratorIntegrationTest.java
+++ airbyte-integrations/connectors/destination-postgres/src/test-integration/java/io/airbyte/integrations/destination/postgres/typing_deduping/PostgresSqlGeneratorIntegrationTest.java
@@ -3,16 +3,22 @@ package io.airbyte.integrations.destination.postgres.typing_deduping;
 import static io.airbyte.integrations.destination.postgres.typing_deduping.PostgresSqlGenerator.JSONB_TYPE;
 
 import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import io.airbyte.cdk.db.jdbc.DefaultJdbcDatabase;
 import io.airbyte.cdk.db.jdbc.JdbcDatabase;
+import io.airbyte.cdk.db.jdbc.JdbcSourceOperations;
 import io.airbyte.cdk.db.jdbc.JdbcUtils;
 import io.airbyte.cdk.integrations.destination.jdbc.TableDefinition;
 import io.airbyte.cdk.integrations.destination.jdbc.typing_deduping.JdbcDestinationHandler;
 import io.airbyte.cdk.integrations.destination.jdbc.typing_deduping.JdbcSqlGenerator;
 import io.airbyte.cdk.integrations.standardtest.destination.typing_deduping.JdbcSqlGeneratorIntegrationTest;
+import io.airbyte.commons.json.Jsons;
 import io.airbyte.integrations.base.destination.typing_deduping.DestinationHandler;
 import io.airbyte.integrations.destination.postgres.PostgresDestination;
 import io.airbyte.integrations.destination.postgres.PostgresSQLNameTransformer;
 import io.airbyte.integrations.destination.postgres.PostgresTestDatabase;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import javax.sql.DataSource;
 import org.jooq.DataType;
 import org.jooq.Field;
@@ -28,6 +34,29 @@ public class PostgresSqlGeneratorIntegrationTest extends JdbcSqlGeneratorIntegra
   private static String databaseName;
   private static JdbcDatabase database;
 
+  /**
+   * See {@link io.airbyte.integrations.destination.redshift.typing_deduping.RedshiftSqlGeneratorIntegrationTest.RedshiftSourceOperations}.
+   * Copied here to avoid weird dependencies.
+   */
+  public static class PostgresSourceOperations extends JdbcSourceOperations {
+
+    @Override
+    public void copyToJsonField(final ResultSet resultSet, final int colIndex, final ObjectNode json) throws SQLException {
+      final String columnName = resultSet.getMetaData().getColumnName(colIndex);
+      final String columnTypeName = resultSet.getMetaData().getColumnTypeName(colIndex).toLowerCase();
+
+      switch (columnTypeName) {
+        // JSONB has no equivalent in JDBCType
+        case "jsonb" -> json.set(columnName, Jsons.deserializeExact(resultSet.getString(colIndex)));
+        // For some reason, the driver maps these to their timezoneless equivalents (TIME and TIMESTAMP)
+        case "timetz" -> putTimeWithTimezone(json, columnName, resultSet, colIndex);
+        case "timestamptz" -> putTimestampWithTimezone(json, columnName, resultSet, colIndex);
+        default -> super.copyToJsonField(resultSet, colIndex, json);
+      }
+    }
+
+  }
+
   @BeforeAll
   public static void setupPostgres() {
     testContainer = PostgresTestDatabase.in(PostgresTestDatabase.BaseImage.POSTGRES_13);
@@ -42,7 +71,7 @@ public class PostgresSqlGeneratorIntegrationTest extends JdbcSqlGeneratorIntegra
     databaseName = config.get(JdbcUtils.DATABASE_KEY).asText();
     final PostgresDestination postgresDestination = new PostgresDestination();
     final DataSource dataSource = postgresDestination.getDataSource(config);
-    database = postgresDestination.getDatabase(dataSource);
+    database = new DefaultJdbcDatabase(dataSource, new PostgresSourceOperations());
   }
 
   @AfterAll
