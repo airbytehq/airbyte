3854d2c876f736e8d73129900f8271b92c1eb3a7
Maxime Carbonneau-Leclerc <maxi297@users.noreply.github.com>
2024-01-04 10:30:04 -0500

    Issue 32871/test application fees (#33352)

--- airbyte-integrations/connectors/source-stripe/unit_tests/integration/test_events.py
+++ airbyte-integrations/connectors/source-stripe/unit_tests/integration/test_events.py
@@ -22,6 +22,7 @@ from airbyte_protocol.models import ConfiguredAirbyteCatalog, FailureType, SyncM
 from integration.config import ConfigBuilder
 from integration.pagination import StripePaginationStrategy
 from integration.request_builder import StripeRequestBuilder
+from integration.response_builder import a_response_with_status
 from source_stripe import SourceStripe
 
 _STREAM_NAME = "events"
@@ -30,23 +31,17 @@ _A_START_DATE = _NOW - timedelta(days=60)
 _ACCOUNT_ID = "account_id"
 _CLIENT_SECRET = "client_secret"
 _NO_STATE = {}
-_HTTP_RESPONSE_STATUS_500 = HttpResponse(
-    json.dumps(
-        {"unknown": "maxi297: I could not reproduce the issue hence this response will not look like the actual 500 status response"}
-    ),
-    500
-)
 _AVOIDING_INCLUSIVE_BOUNDARIES = timedelta(seconds=1)
 _SECOND_REQUEST = timedelta(seconds=1)
 _THIRD_REQUEST = timedelta(seconds=2)
 
 
 def _a_request() -> StripeRequestBuilder:
-    return StripeRequestBuilder("events", _ACCOUNT_ID, _CLIENT_SECRET)
+    return StripeRequestBuilder.events_endpoint(_ACCOUNT_ID, _CLIENT_SECRET)
 
 
 def _config() -> ConfigBuilder:
-    return ConfigBuilder().with_account_id(_ACCOUNT_ID).with_client_secret(_CLIENT_SECRET)
+    return ConfigBuilder().with_start_date(_NOW - timedelta(days=73)).with_account_id(_ACCOUNT_ID).with_client_secret(_CLIENT_SECRET)
 
 
 def _catalog(sync_mode: SyncMode) -> ConfiguredAirbyteCatalog:
@@ -85,7 +80,7 @@ def _read(
 class FullRefreshTest(TestCase):
 
     @HttpMocker()
-    def test_given_one_page_when_read_then_return_record(self, http_mocker: HttpMocker) -> None:
+    def test_given_one_page_when_read_then_return_records(self, http_mocker: HttpMocker) -> None:
         http_mocker.get(
             _a_request().with_created_gte(_A_START_DATE).with_created_lte(_NOW).with_limit(100).build(),
             _a_response().with_record(_a_record()).with_record(_a_record()).build(),
@@ -166,35 +161,16 @@ class FullRefreshTest(TestCase):
     def test_given_http_status_400_when_read_then_stream_is_ignored(self, http_mocker: HttpMocker) -> None:
         http_mocker.get(
             _a_request().with_any_query_params().build(),
-            HttpResponse(
-                json.dumps({
-                    "error": {
-                        "code": "parameter_invalid_integer",
-                        "doc_url": "https://stripe.com/docs/error-codes/parameter-invalid-integer",
-                        "message": "Invalid integer: not_an_integer",
-                        "param": "limit",
-                        "request_log_url": "https://dashboard.stripe.com/test/logs/req_yVDTOo9Jrd1suG?t=1701959063",
-                        "type": "invalid_request_error"
-                    }
-                }),
-                400)
+            a_response_with_status(400),
         )
-        output = self._read(_config().with_start_date(_A_START_DATE))
+        output = self._read(_config())
         assert len(output.get_stream_statuses(_STREAM_NAME)) == 0
 
     @HttpMocker()
     def test_given_http_status_401_when_read_then_stream_is_incomplete(self, http_mocker: HttpMocker) -> None:
         http_mocker.get(
             _a_request().with_any_query_params().build(),
-            HttpResponse(
-                json.dumps({
-                    "error": {
-                        "message": "Invalid API Key provided: sk_test_*****************************************************mFeM",
-                        "type": "invalid_request_error"
-                    }
-                }),
-                401
-            ),
+            a_response_with_status(401),
         )
         output = self._read(_config().with_start_date(_A_START_DATE), expecting_exception=True)
         assert output.errors[-1].trace.error.failure_type == FailureType.system_error
@@ -204,17 +180,7 @@ class FullRefreshTest(TestCase):
         http_mocker.get(
             _a_request().with_any_query_params().build(),
             [
-                HttpResponse(
-                    json.dumps({
-                        "error": {
-                            "message": "Request rate limit exceeded. Learn more about rate limits here https://stripe.com/docs/rate-limits.",
-                            "type": "invalid_request_error",
-                            "code": "rate_limit",
-                            "doc_url": "https://stripe.com/docs/error-codes/rate-limit"
-                        }
-                    }),
-                    429
-                ),
+                a_response_with_status(429),
                 _a_response().with_record(_a_record()).build(),
             ],
         )
@@ -225,18 +191,18 @@ class FullRefreshTest(TestCase):
     def test_given_http_status_500_once_before_200_when_read_then_retry_and_return_records(self, http_mocker: HttpMocker) -> None:
         http_mocker.get(
             _a_request().with_any_query_params().build(),
-            [_HTTP_RESPONSE_STATUS_500, _a_response().with_record(_a_record()).build()],
+            [a_response_with_status(500), _a_response().with_record(_a_record()).build()],
         )
-        output = self._read(_config().with_start_date(_A_START_DATE))
+        output = self._read(_config())
         assert len(output.records) == 1
 
     @HttpMocker()
     def test_given_http_status_500_on_availability_when_read_then_raise_system_error(self, http_mocker: HttpMocker) -> None:
         http_mocker.get(
             _a_request().with_any_query_params().build(),
-            [_HTTP_RESPONSE_STATUS_500],
+            a_response_with_status(500),
         )
-        output = self._read(_config().with_start_date(_A_START_DATE), expecting_exception=True)
+        output = self._read(_config(), expecting_exception=True)
         assert output.errors[-1].trace.error.failure_type == FailureType.system_error
 
     @HttpMocker()
@@ -263,7 +229,7 @@ class IncrementalTest(TestCase):
             _a_request().with_created_gte(_A_START_DATE).with_created_lte(_NOW).with_limit(100).build(),
             _a_response().with_record(_a_record().with_cursor(cursor_value)).build(),
         )
-        output = self._read(_config().with_start_date(_A_START_DATE), {})
+        output = self._read(_config().with_start_date(_A_START_DATE), _NO_STATE)
         assert output.most_recent_state == {"events": {"created": cursor_value}}
 
     @HttpMocker()
@@ -294,10 +260,12 @@ class IncrementalTest(TestCase):
             _a_request().with_created_gte(_A_START_DATE).with_created_lte(_NOW).with_limit(100).build(),
             _a_response().with_record(_a_record().with_cursor(cursor_value)).build(),
         )
+
         output = self._read(
             _config().with_start_date(_A_START_DATE),
             StateBuilder().with_stream_state("events", {"created": more_recent_than_record_cursor}).build()
         )
+
         assert output.most_recent_state == {"events": {"created": more_recent_than_record_cursor}}
 
     def _read(self, config: ConfigBuilder, state: Optional[Dict[str, Any]], expecting_exception: bool = False) -> EntrypointOutput:
