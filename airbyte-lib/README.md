97f7711523967af28e2883a949c06e8deebad845
Joe Reuter <joe@airbyte.io>
2024-01-05 18:11:47 +0100

    add generated docs

--- airbyte-lib/README.md
+++ airbyte-lib/README.md
@@ -8,3 +8,9 @@ airbyte-lib is a library that allows to run Airbyte syncs embedded into any Pyth
 * Run `poetry install`
 * For examples, check out the `examples` folder. They can be run via `poetry run python examples/<example file>`
 * Unit tests and type checks can be run via `poetry run pytest`
+
+## Documentation
+
+Regular documentation lives in the `/docs` folder. Based on the doc strings of public methods, we generate API documentation using [pdoc](https://pdoc.dev). To generate the documentation, run `poetry run generate-docs`. The documentation will be generated in the `docs/generate` folder. This needs to be done manually when changing the public interface of the library.
+
+A unit test validates the documentation is up to date. 
--- airbyte-lib/docs.py
+++ airbyte-lib/docs.py
@@ -1,7 +1,10 @@
+# Copyright (c) 2023 Airbyte, Inc., all rights reserved.
+
 import os
 import pathlib
-import pdoc
 import shutil
+
+import pdoc
 
 
 def run():
@@ -14,14 +17,14 @@ def run():
     public_modules = ["airbyte_lib"]
 
     # recursively delete the docs/generated folder if it exists
-    if os.path.exists('docs/generated'):
-        shutil.rmtree('docs/generated') 
+    if os.path.exists("docs/generated"):
+        shutil.rmtree("docs/generated")
 
     # determine all folders in airbyte_lib that don't start with an underscore and add them to public_modules
-    for d in os.listdir('airbyte_lib'):
-        dir_path = pathlib.Path(f'airbyte_lib/{d}')
-        if dir_path.is_dir() and not d.startswith('_'):
+    for d in os.listdir("airbyte_lib"):
+        dir_path = pathlib.Path(f"airbyte_lib/{d}")
+        if dir_path.is_dir() and not d.startswith("_"):
             public_modules.append(dir_path)
 
-    pdoc.render.configure(template_directory='docs', show_source=False, search=False)
-    pdoc.pdoc(*public_modules, output_directory=pathlib.Path('docs/generated'))
+    pdoc.render.configure(template_directory="docs", show_source=False, search=False)
+    pdoc.pdoc(*public_modules, output_directory=pathlib.Path("docs/generated"))
