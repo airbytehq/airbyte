24dfebc89dde22731ee80c11da8d9ecd3f0926e9
Joe Reuter <joe@airbyte.io>
2024-01-10 14:59:34 +0100

    Source Stripe: Convert to airbyte-lib (#33940)

--- airbyte-integrations/connectors/source-stripe/setup.py
+++ airbyte-integrations/connectors/source-stripe/setup.py
@@ -20,4 +20,9 @@ setup(
     extras_require={
         "tests": TEST_REQUIREMENTS,
     },
+    entry_points={
+        "console_scripts": [
+            "source-stripe=source_stripe.run:run",
+        ],
+    },
 )
