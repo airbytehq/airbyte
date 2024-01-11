e5c9427e935775cd633b5e5fa02a62d95518066d
Maxime Carbonneau-Leclerc <3360483+maxi297@users.noreply.github.com>
2024-01-11 10:50:54 -0500

    🐛 Source Stripe: adding integration tests (#33306)
    Co-authored-by: maxi297 <maxi297@users.noreply.github.com>
    Co-authored-by: Catherine Noll <clnoll@users.noreply.github.com>
    Co-authored-by: Brian Lai <51336873+brianjlai@users.noreply.github.com>

--- airbyte-integrations/connectors/source-stripe/setup.py
+++ airbyte-integrations/connectors/source-stripe/setup.py
@@ -5,9 +5,10 @@
 
 from setuptools import find_packages, setup
 
-MAIN_REQUIREMENTS = ["airbyte-cdk==0.55.2", "stripe==2.56.0", "pendulum==2.1.2"]
+MAIN_REQUIREMENTS = ["airbyte-cdk==0.57.6", "stripe==2.56.0", "pendulum==2.1.2"]
 
-TEST_REQUIREMENTS = ["pytest-mock~=3.6.1", "pytest~=6.1", "requests-mock", "requests_mock~=1.8", "freezegun==1.2.2"]
+# we set `requests-mock~=1.11.0` to ensure concurrency is supported
+TEST_REQUIREMENTS = ["pytest-mock~=3.6.1", "pytest~=6.1", "requests-mock~=1.11.0", "requests_mock~=1.8", "freezegun==1.2.2"]
 
 setup(
     name="source_stripe",
