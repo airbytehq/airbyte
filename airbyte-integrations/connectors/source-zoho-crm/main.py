38904d68684acdce9985f869bde04cc364ad7151
Joe Reuter <joe@airbyte.io>
2024-01-15 10:52:04 +0100

    convert

--- airbyte-integrations/connectors/source-zoho-crm/main.py
+++ airbyte-integrations/connectors/source-zoho-crm/main.py
@@ -2,12 +2,7 @@
 # Copyright (c) 2023 Airbyte, Inc., all rights reserved.
 #
 
-
-import sys
-
-from airbyte_cdk.entrypoint import launch
-from source_zoho_crm import SourceZohoCrm
+from source_zoho_crm.run import run
 
 if __name__ == "__main__":
-    source = SourceZohoCrm()
-    launch(source, sys.argv[1:])
+    run()
