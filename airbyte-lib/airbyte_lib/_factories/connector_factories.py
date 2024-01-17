6eb84599c51bef865ab0a325e0bbc2d4e1bd35a7
Aaron ("AJ") Steers <aj@airbyte.io>
2024-01-16 16:46:51 -0800

    AirbyteLib: Python lint cleanup (#34223)

--- airbyte-lib/airbyte_lib/_factories/connector_factories.py
+++ airbyte-lib/airbyte_lib/_factories/connector_factories.py
@@ -1,5 +1,5 @@
 # Copyright (c) 2023 Airbyte, Inc., all rights reserved.
-
+from __future__ import annotations
 
 from typing import Any
 
@@ -13,17 +13,26 @@ def get_connector(
     version: str | None = None,
     pip_url: str | None = None,
     config: dict[str, Any] | None = None,
+    *,
     use_local_install: bool = False,
     install_if_missing: bool = True,
 ) -> Source:
-    """
-    Get a connector by name and version.
-    :param name: connector name
-    :param version: connector version - if not provided, the currently installed version will be used. If no version is installed, the latest available version will be used. The version can also be set to "latest" to force the use of the latest available version.
-    :param pip_url: connector pip URL - if not provided, the pip url will be inferred from the connector name.
-    :param config: connector config - if not provided, you need to set it later via the set_config method.
-    :param use_local_install: whether to use a virtual environment to run the connector. If True, the connector is expected to be available on the path (e.g. installed via pip). If False, the connector will be installed automatically in a virtual environment.
-    :param install_if_missing: whether to install the connector if it is not available locally. This parameter is ignored if use_local_install is True.
+    """Get a connector by name and version.
+
+    Args:
+        name: connector name
+        version: connector version - if not provided, the currently installed version will be used.
+            If no version is installed, the latest available version will be used. The version can
+            also be set to "latest" to force the use of the latest available version.
+        pip_url: connector pip URL - if not provided, the pip url will be inferred from the
+            connector name.
+        config: connector config - if not provided, you need to set it later via the set_config
+            method.
+        use_local_install: whether to use a virtual environment to run the connector. If True, the
+            connector is expected to be available on the path (e.g. installed via pip). If False,
+            the connector will be installed automatically in a virtual environment.
+        install_if_missing: whether to install the connector if it is not available locally. This
+            parameter is ignored if use_local_install is True.
     """
     metadata = get_connector_metadata(name)
     if use_local_install:
