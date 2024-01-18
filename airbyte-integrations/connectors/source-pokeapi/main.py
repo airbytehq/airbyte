638b30c6dc78be5304d0d651168bf517d85c4245
Joe Reuter <joe@airbyte.io>
2024-01-18 12:49:59 +0100

    revert

--- airbyte-integrations/connectors/source-pokeapi/main.py
+++ airbyte-integrations/connectors/source-pokeapi/main.py
@@ -7,7 +7,6 @@ import sys
 from airbyte_cdk.entrypoint import launch
 from source_pokeapi import SourcePokeapi
 
-
 if __name__ == "__main__":
     source = SourcePokeapi()
     launch(source, sys.argv[1:])
