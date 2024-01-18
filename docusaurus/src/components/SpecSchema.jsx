dbab92870683ada7147924f3a1cdec0c0b0c81ac
Joe Reuter <joe@airbyte.io>
2024-01-18 15:45:59 +0100

    nora style

--- docusaurus/src/components/SpecSchema.jsx
+++ docusaurus/src/components/SpecSchema.jsx
@@ -19,39 +19,130 @@ export const SpecSchema = ({
 function JSONSchemaViewer(props) {
   return <div>
     <Heading as="h4">Config fields reference</Heading>
-    <JSONSchemaObject schema={props.schema} />
+    <div class={styles.grid}>
+      <div class={className(styles.headerItem, styles.tableHeader)}>
+        Field
+      </div>
+      <div class={className(styles.headerItem, styles.tableHeader)}>
+        Type
+      </div>
+      <div class={className(styles.headerItem, styles.tableHeader)}>
+        Title
+      </div>
+      <JSONSchemaObject schema={props.schema} />
+    </div>
   </div>
+}
+
+function getOrderedProperties(schema) {
+  const requiredProperties = new Set(schema.required || []);
+  return Object.entries(schema.properties).sort(([a], [b]) => {
+    // Sort required properties first, then respect the order of the schema
+    if (requiredProperties.has(a) && !requiredProperties.has(b)) {
+      return -1;
+    }
+    else if (requiredProperties.has(b) && !requiredProperties.has(a)) {
+      return 1;
+    }
+    else {
+      return 0;
+    }
+  });
 }
 
 function JSONSchemaObject(props) {
   const requiredProperties = new Set(props.schema.required || []);
-  return <div>
-    {Object.entries(props.schema.properties).sort(([a], [b]) => {
-      // Sort required properties first, then respect the order of the schema
-      if (requiredProperties.has(a) && !requiredProperties.has(b)) {
-        return -1;
-      } else if (requiredProperties.has(b) && !requiredProperties.has(a)) {
-        return 1;
-      } else {
-        return 0;
-      }
-    }).map(([key, schema]) => {
-      return <JSONSchemaProperty key={key} propertyKey={key} schema={schema} required={requiredProperties.has(key)} />
+  return <>
+    {getOrderedProperties(props.schema).map(([key, schema]) => {
+      return <JSONSchemaProperty key={key} propertyKey={key} schema={schema} required={requiredProperties.has(key)} depth={props.depth} />
     })}
-  </div>
+  </>
 }
 
 function JSONSchemaOneOf(props) {
-  return <div>
-    <Heading as="h5">One of:</Heading>
-    <ul className={styles.oneOfList}>
+  return <>
+    <ul className={styles.contents}>
       {props.schema.oneOf.map((schema, i) => {
-        return <li key={i}>
-          <JSONSchemaProperty schema={schema} />
+        return <li key={i} className={styles.contents}>
+          <JSONSchemaProperty schema={schema} depth={props.depth} />
         </li>
       })}
     </ul>
-  </div>
+  </>
+}
+
+function isOneOf(schema) {
+  return schema.type === "object" && schema.oneOf;
+}
+
+function showCollapsible(schema) {
+  return schema.type === "array" || (schema.type === "object" && schema.properties) || showDescription(schema)
+}
+
+function showDescription(schema) {
+  return schema.const || typeof schema.default !== "undefined" || schema.pattern || schema.examples || schema.description || isOneOf(schema);
+}
+
+function getIndentStyle(depth) {
+  return {
+    paddingLeft: `${depth * 13}px`
+  };
+}
+
+function JSONSchemaProperty({ propertyKey, schema, required, depth = 0 }) {
+  const newDepth = depth + 1;
+  const propertyName = <>
+    <div>{propertyKey || schema.title}</div>
+    {required && <div className={styles.tag}>required</div>}
+  </>;
+  const typeAndTitle = <>
+    <div className={styles.headerItem}>
+      {schema.type}
+    </div>
+    <div className={styles.headerItem}>
+      {schema.title && <div>{schema.title}</div>}
+    </div>
+  </>;
+  if (showCollapsible(schema)) {
+    return <Disclosure initiallyOpen={false}>
+      {({ open }) => (
+        <>
+          <Disclosure.Button className={className(styles.headerItem, styles.clickable, styles.propertyName)} style={getIndentStyle(newDepth)}>
+            <div className={className({ [styles.open]: open })}>›</div>
+            {propertyName}
+          </Disclosure.Button>
+          {typeAndTitle}
+          <Disclosure.Panel className={styles.contents}>
+            {showDescription(schema) && <Description schema={schema} style={getIndentStyle(newDepth + 1)} />}
+            {schema.type === "object" && schema.oneOf && <JSONSchemaOneOf schema={schema} depth={newDepth} />}
+            {schema.type === "object" && schema.properties && <JSONSchemaObject schema={schema} depth={newDepth} />}
+            {schema.type === "array" && <JSONSchemaProperty propertyKey="items[x]" schema={schema.items} depth={newDepth} />}
+          </Disclosure.Panel>
+        </>)}
+    </Disclosure>
+  } else {
+    return <>
+      <div className={className(styles.headerItem, styles.propertyName)} style={getIndentStyle(newDepth)}>
+        {propertyName}
+      </div>
+      {typeAndTitle}
+    </>
+  }
+}
+
+function Description({ schema, style }) {
+  return <div className={styles.descriptionItem} style={style}>
+  {(typeof schema.default !== "undefined" && !schema.const) && <div>Default: <pre>{JSON.stringify(schema.default, null, 2)}</pre></div>}
+  {schema.const && <div>Constant value: <pre>{JSON.stringify(schema.const, null, 2)}</pre></div>}
+  {schema.pattern && <div>Pattern{schema.pattern_descriptor && <> ({schema.pattern_descriptor})</>}: <pre>{schema.pattern}</pre></div>}
+  {(schema.examples && schema.examples.length > 1) && <div>Examples: <ul>
+    {schema.examples.map((example, i) => <li key={i}><pre>{JSON.stringify(example)}</pre></li>)}
+  </ul></div>}
+  {(schema.examples && schema.examples.length === 1) && <div>Example: <pre>{JSON.stringify(schema.examples[0])}</pre></div>}
+  {schema.description && <div><TextWithHTML text={schema.description} /></div>}
+  {isOneOf(schema) &&
+    <Heading as="h5" className={styles.oneOfHeader}>One of:</Heading>}
+</div>
 }
 
 const allowedAttributes = {
@@ -76,45 +167,3 @@ const TextWithHTML = ({ text, className }) => {
 
   return <span className={className} dangerouslySetInnerHTML={{ __html: sanitizedHtmlText }} />;
 };
-
-function JSONSchemaProperty({ propertyKey, schema, required }) {
-  const isPrimitive = schema.type !== "object" && schema.type !== "array" && !schema.description && !schema.pattern && !schema.examples;
-  if (isPrimitive) {
-    return <div className={styles.block}>
-      <div className={styles.header}>
-        {propertyKey && <div className={styles.propertyName}>{propertyKey}</div>}
-        {schema.title && <div>{schema.title}</div>}
-        {required && <div className={styles.tag}>required</div>}
-        <div className={styles.tag}>type: {schema.type}</div>
-        {schema.const && <div className={styles.tag}>constant value: {JSON.stringify(schema.const)}</div>}
-      </div></div>
-  } else {
-    return <Disclosure initiallyOpen={false}>
-      {({ open }) => (
-        <div className={styles.block}>
-          <Disclosure.Button className={className(styles.header, styles.clickable)}>
-            <div className={className({ [styles.open]: open })}>›</div>
-            {propertyKey && <div className={styles.propertyName}>{propertyKey}</div>}
-            {schema.title && <div>{schema.title}</div>}
-            {required && <div className={styles.tag}>required</div>}
-            {schema.const && <div className={styles.tag}>constant value: {JSON.stringify(schema.const)}</div>}
-          </Disclosure.Button>
-          <Disclosure.Panel>
-            <div className={styles.propertyDocumentation}>
-              <div>Type: {schema.type}</div>
-              {(typeof schema.default !== "undefined" && !schema.const) && <div>Default: <pre>{JSON.stringify(schema.default, null, 2)}</pre></div>}
-              {schema.pattern && <div>Pattern{schema.pattern_descriptor && <> ({schema.pattern_descriptor})</>}: <pre>{schema.pattern}</pre></div>}
-              {(schema.examples && schema.examples.length > 1) && <div>Examples: <ul>
-                {schema.examples.map((example, i) => <li key={i}><pre>{JSON.stringify(example)}</pre></li>)}
-              </ul></div>}
-              {(schema.examples && schema.examples.length === 1) && <div>Example: <pre>{JSON.stringify(schema.examples[0])}</pre></div>}
-              {schema.description && <div><TextWithHTML text={schema.description} /></div>}
-              {schema.type === "object" && schema.oneOf && <JSONSchemaOneOf schema={schema} />}
-              {schema.type === "object" && schema.properties && <JSONSchemaObject schema={schema} />}
-              {schema.type === "array" && <JSONSchemaProperty propertyKey="items[x]" schema={schema.items} />}
-            </div>
-          </Disclosure.Panel>
-        </div>)}
-    </Disclosure>
-  }
-}
