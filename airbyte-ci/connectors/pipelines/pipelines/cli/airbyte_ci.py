21fca1c6ad66b562ff8b35e2dd1b6f508fb942f6
Joe Reuter <joe@airbyte.io>
2024-01-24 16:27:49 +0100

    airbyte-ci: Add pypi publishing logic (#34111)

--- airbyte-ci/connectors/pipelines/pipelines/cli/airbyte_ci.py
+++ airbyte-ci/connectors/pipelines/pipelines/cli/airbyte_ci.py
@@ -130,6 +130,7 @@ def is_current_process_wrapped_by_dagger_run() -> bool:
     help="Airbyte CI top-level command group.",
     lazy_subcommands={
         "connectors": "pipelines.airbyte_ci.connectors.commands.connectors",
+        "poetry": "pipelines.airbyte_ci.poetry.commands.poetry",
         "format": "pipelines.airbyte_ci.format.commands.format_code",
         "metadata": "pipelines.airbyte_ci.metadata.commands.metadata",
         "test": "pipelines.airbyte_ci.test.commands.test",
