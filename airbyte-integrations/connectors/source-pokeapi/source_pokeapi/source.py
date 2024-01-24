f99aaef3e49e911e8667d514c49cf507a51d8c87
Joe Reuter <joe@airbyte.io>
2024-01-24 16:11:50 +0100

    revert test changes

--- airbyte-integrations/connectors/source-pokeapi/source_pokeapi/source.py
+++ airbyte-integrations/connectors/source-pokeapi/source_pokeapi/source.py
@@ -15,5 +15,4 @@ WARNING: Do not modify this file.
 # Declarative Source
 class SourcePokeapi(YamlDeclarativeSource):
     def __init__(self):
-
         super().__init__(**{"path_to_yaml": "manifest.yaml"})
