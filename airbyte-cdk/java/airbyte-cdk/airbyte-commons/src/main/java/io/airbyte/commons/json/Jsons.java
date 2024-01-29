493c4363eb68e1993319edfe18018eb9af9b03d5
Gireesh Sreepathi <gisripa@gmail.com>
2024-01-29 15:26:54 -0800

    nit
    Co-authored-by: Edward Gao <edward.gao@airbyte.io>

--- airbyte-cdk/java/airbyte-cdk/airbyte-commons/src/main/java/io/airbyte/commons/json/Jsons.java
+++ airbyte-cdk/java/airbyte-cdk/airbyte-commons/src/main/java/io/airbyte/commons/json/Jsons.java
@@ -143,6 +143,7 @@ public class Jsons {
 
   /**
    * This method does not generate deserialization warn log on why serialization failed.
+   * See also {@link #tryDeserialize(String)}.
    *
    * @param jsonString
    * @return
