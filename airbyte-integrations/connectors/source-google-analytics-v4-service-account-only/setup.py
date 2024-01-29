b37efe9cda2aa3f347a4ff581737a10948308036
Artem Inzhyyants <36314070+artem1205@users.noreply.github.com>
2024-01-29 17:31:30 +0100

    🎉 New Source: Google Analytics v4 Service Account Only (#34323)

--- airbyte-integrations/connectors/source-google-analytics-v4-service-account-only/setup.py
+++ airbyte-integrations/connectors/source-google-analytics-v4-service-account-only/setup.py
@@ -2,10 +2,17 @@
 # Copyright (c) 2023 Airbyte, Inc., all rights reserved.
 #
 
+from pathlib import Path
 
 from setuptools import find_packages, setup
 
-MAIN_REQUIREMENTS = ["airbyte-cdk", "PyJWT", "cryptography", "requests"]
+
+def local_dependency(name: str) -> str:
+    """Returns a path to a local package."""
+    return f"{name} @ file://{Path.cwd().parent / name}"
+
+
+MAIN_REQUIREMENTS = ["airbyte-cdk", "PyJWT", "cryptography", "requests", local_dependency("source-google-analytics-v4")]
 
 TEST_REQUIREMENTS = [
     "pytest~=6.1",
@@ -15,13 +22,13 @@ TEST_REQUIREMENTS = [
 ]
 
 setup(
-    name="source_google_analytics_v4",
+    name="source_google_analytics_v4_service_account_only",
     description="Source implementation for Google Analytics V4.",
     author="Airbyte",
     author_email="contact@airbyte.io",
     packages=find_packages(),
     install_requires=MAIN_REQUIREMENTS,
-    package_data={"": ["*.json", "defaults/*.json", "schemas/*.json", "schemas/shared/*.json"]},
+    package_data={"": ["*.json", "schemas/*.json", "schemas/shared/*.json"]},
     extras_require={
         "tests": TEST_REQUIREMENTS,
     },
