1e12d903486b53f815431c72b165ae78fe0f233b
Catherine Noll <noll.catherine@gmail.com>
2024-01-30 10:46:10 -0500

    Pin file-based sources to airbyte-cdk version 0.59.2 until they can be made compatible with https://github.com/airbytehq/airbyte/pull/34411

--- airbyte-integrations/connectors/source-gcs/setup.py
+++ airbyte-integrations/connectors/source-gcs/setup.py
@@ -6,7 +6,7 @@
 from setuptools import find_packages, setup
 
 MAIN_REQUIREMENTS = [
-    "airbyte-cdk[file-based]>=0.55.5",
+    "airbyte-cdk[file-based]==0.59.2",  # pinned until compatible with https://github.com/airbytehq/airbyte/pull/34411
     "google-cloud-storage==2.12.0",
     "smart-open[s3]==5.1.0",
     "pandas==1.5.3",
