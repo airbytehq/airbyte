4cfaebeacfeb3139466dab986fa717dece322e94
Joe Reuter <joe@airbyte.io>
2024-01-30 12:11:41 +0100

    airbyte-lib: Refactor follow-up (#34649)

--- airbyte-integrations/connectors/source-hubspot/setup.py
+++ airbyte-integrations/connectors/source-hubspot/setup.py
@@ -28,7 +28,19 @@ setup(
     author_email="contact@airbyte.io",
     packages=find_packages(),
     install_requires=MAIN_REQUIREMENTS,
-    package_data={"": ["*.json", "*.yaml", "schemas/*.json"]},
+    package_data={
+        "": [
+            # Include yaml files in the package (if any)
+            "*.yml",
+            "*.yaml",
+            # Include all json files in the package, up to 4 levels deep
+            "*.json",
+            "*/*.json",
+            "*/*/*.json",
+            "*/*/*/*.json",
+            "*/*/*/*/*.json",
+        ]
+    },
     extras_require={
         "tests": TEST_REQUIREMENTS,
     },
