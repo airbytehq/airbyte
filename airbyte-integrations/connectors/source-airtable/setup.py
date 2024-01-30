cc1f1a28590ca3efaf6bea3135d7aaa6a225d38b
Joe Reuter <joe@airbyte.io>
2024-01-30 12:11:41 +0100

    airbyte-lib: Refactor follow-up (#34649)

--- airbyte-integrations/connectors/source-airtable/setup.py
+++ airbyte-integrations/connectors/source-airtable/setup.py
@@ -27,7 +27,19 @@ setup(
     author_email="anhtuan.nguyen@me.com",
     packages=find_packages(),
     install_requires=MAIN_REQUIREMENTS,
-    package_data={"": ["*.json", "schemas/*.json", "schemas/shared/*.json"]},
+    package_data={
+        "": [
+            # Include yaml files in the package (if any)
+            "*.yml",
+            "*.yaml",
+            # Include all json files in the package, up to 4 levels deep
+            "*.json",
+            "*/*.json",
+            "*/*/*.json",
+            "*/*/*/*.json",
+            "*/*/*/*/*.json",
+        ]
+    },
     extras_require={
         "tests": TEST_REQUIREMENTS,
     },
--- airbyte-integrations/connectors/source-python-http-tutorial/setup.py
+++ airbyte-integrations/connectors/source-python-http-tutorial/setup.py
@@ -16,5 +16,17 @@ setup(
     author_email="contact@airbyte.io",
     packages=find_packages(),
     install_requires=["airbyte-cdk", "pytest"],
-    package_data={"": ["*.json"]},
+    package_data={
+        "": [
+            # Include yaml files in the package (if any)
+            "*.yml",
+            "*.yaml",
+            # Include all json files in the package, up to 4 levels deep
+            "*.json",
+            "*/*.json",
+            "*/*/*.json",
+            "*/*/*/*.json",
+            "*/*/*/*/*.json",
+        ]
+    },
 )
