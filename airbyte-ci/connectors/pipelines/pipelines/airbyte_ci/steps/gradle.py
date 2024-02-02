8f29b9680831e5c3400fc452c000579d3c5b0ef9
alafanechere <augustin.lafanechere@gmail.com>
2024-02-02 16:20:03 +0100

    [DNM] checking runner docker config

--- airbyte-ci/connectors/pipelines/pipelines/airbyte_ci/steps/gradle.py
+++ airbyte-ci/connectors/pipelines/pipelines/airbyte_ci/steps/gradle.py
@@ -183,7 +183,7 @@ class GradleTask(Step, ABC):
             # If this GradleTask subclass needs docker, then install it and bind it to the existing global docker host container.
             gradle_container = pipelines.dagger.actions.system.docker.with_bound_docker_host(self.context, gradle_container)
             # This installation should be cheap, as the package has already been downloaded, and its dependencies are already installed.
-            gradle_container = gradle_container.with_exec(["yum", "install", "-y", "docker"])
+            gradle_container = gradle_container.with_exec(["yum", "install", "-y", "docker"]).with_exec(["docker", "info"])
 
         # Run the gradle task that we actually care about.
         connector_task = f":airbyte-integrations:connectors:{self.context.connector.technical_name}:{self.gradle_task_name}"
