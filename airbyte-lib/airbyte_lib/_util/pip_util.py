a2d658afb5366300e9917e522fb5fa800889f436
Aaron Steers <aj@airbyte.io>
2024-02-04 15:26:33 -0800

    add pip git helpers

---
+++ airbyte-lib/airbyte_lib/_util/pip_util.py
@@ -0,0 +1,43 @@
+# Copyright (c) 2023 Airbyte, Inc., all rights reserved.
+"""Internal utility functions for dealing with pip."""
+
+from __future__ import annotations
+
+
+def github_pip_url(
+    owner: str = "airbytehq",
+    repo: str = "airbyte",
+    *,
+    branch_or_ref: str | None = None,
+    package_name: str | None = None,
+    subdirectory: str | None = None,
+) -> str:
+    """Return the pip URL for a GitHub repository."""
+    if not branch_or_ref:
+        branch_or_ref = "master" if repo == "airbyte" else "main"
+
+    result = f"git+{owner}/{repo}.git"
+    if branch_or_ref:
+        result += f"@{branch_or_ref}"
+    if package_name:
+        result += f"#egg={package_name}"
+    if subdirectory:
+        result += f"#subdirectory={subdirectory}"
+
+    return result
+
+
+def connector_pip_url(
+    connector_name: str,
+    *,
+    branch: str | None = None,
+) -> str:
+    """Return a pip URL for a connector in the main `airbytehq/airbyte` git repo."""
+    if not connector_name.startswith("source-") and not connector_name.startswith("destination-"):
+        connector_name = "source-" + connector_name
+
+    return github_pip_url(
+        branch=branch,
+        package_name=f"source-{connector_name}",
+        subdirectory=f"airbyte-integrations/connectors/{connector_name}",
+    )
