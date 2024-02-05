c4046dd32d2595863261e15e30adec2134e403ad
Aaron Steers <aj@airbyte.io>
2024-02-04 16:13:49 -0800

    fix bug

--- airbyte-lib/airbyte_lib/_util/pip_util.py
+++ airbyte-lib/airbyte_lib/_util/pip_util.py
@@ -8,27 +8,30 @@ def github_pip_url(
     owner: str = "airbytehq",
     repo: str = "airbyte",
     *,
+    package_name: str,
     branch_or_ref: str | None = None,
-    package_name: str | None = None,
     subdirectory: str | None = None,
 ) -> str:
     """Return the pip URL for a GitHub repository.
 
     Results will look like:
+    - `git+airbytehq/airbyte.git#egg=airbyte-lib&subdirectory=airbyte-lib'
     - `git+airbytehq/airbyte.git@master#egg=airbyte-lib&subdirectory=airbyte-lib'
     - `git+airbytehq/airbyte.git@my-branch#egg=source-github
        &subdirectory=airbyte-integrations/connectors/source-github'
     """
-    if not branch_or_ref:
-        branch_or_ref = "master" if repo == "airbyte" else "main"
-
     result = f"git+https://github.com/{owner}/{repo}.git"
+
     if branch_or_ref:
         result += f"@{branch_or_ref}"
+
+    next_delimiter = "#"
     if package_name:
-        result += f"#egg={package_name}"
+        result += f"{next_delimiter}egg={package_name}"
+        next_delimiter = "&"
+
     if subdirectory:
-        result += f"#subdirectory={subdirectory}"
+        result += f"{next_delimiter}subdirectory={subdirectory}"
 
     return result
 
--- airbyte-lib/tests/unit_tests/test_pip_helpers.py
+++ airbyte-lib/tests/unit_tests/test_pip_helpers.py
@@ -4,11 +4,11 @@ import pytest
 from airbyte_lib._util import github_pip_url
 
 @pytest.mark.parametrize('owner, repo, branch_or_ref, package_name, subdirectory, expected', [
-    ('airbytehq', 'airbyte', None, None, None, 'git+https://github.com/airbytehq/airbyte.git@master'),
+    ('airbytehq', 'airbyte', None, None, None, 'git+https://github.com/airbytehq/airbyte.git'),
+    ('airbytehq', 'airbyte', 'master', None, None, 'git+https://github.com/airbytehq/airbyte.git@master'),
     ('airbytehq', 'airbyte', 'my-branch', None, None, 'git+https://github.com/airbytehq/airbyte.git@my-branch'),
     ('airbytehq', 'airbyte', 'my-branch', 'airbyte-lib', None, 'git+https://github.com/airbytehq/airbyte.git@my-branch#egg=airbyte-lib'),
-    ('airbytehq', 'airbyte', 'my-branch', 'airbyte-lib', 'airbyte-lib', 'git+https://github.com/airbytehq/airbyte.git@my-branch#egg=airbyte-lib#subdirectory=airbyte-lib'),
-    ('airbytehq', 'other-repo', None, None, None, 'git+https://github.com/airbytehq/other-repo.git@main'),
+    ('airbytehq', 'airbyte', 'my-branch', 'airbyte-lib', 'airbyte-lib', 'git+https://github.com/airbytehq/airbyte.git@my-branch#egg=airbyte-lib&subdirectory=airbyte-lib'),
 ])
 def test_github_pip_url(owner, repo, branch_or_ref, package_name, subdirectory, expected):
     result = github_pip_url(owner, repo, branch_or_ref=branch_or_ref, package_name=package_name, subdirectory=subdirectory)
