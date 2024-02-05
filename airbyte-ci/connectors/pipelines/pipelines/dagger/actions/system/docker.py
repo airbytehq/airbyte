3cc0f21ece5a08809473691124f80e31a6801dc1
Marius Posta <marius@airbyte.io>
2024-02-05 09:45:10 -0500

    airbyte-ci: dockerd tweaks

--- airbyte-ci/connectors/pipelines/pipelines/dagger/actions/system/docker.py
+++ airbyte-ci/connectors/pipelines/pipelines/dagger/actions/system/docker.py
@@ -144,7 +144,15 @@ def with_global_dockerd_service(
         # Docker login happens late because there's a cache buster in the docker login command.
         dockerd_container = docker_login(dockerd_container, docker_hub_username_secret, docker_hub_password_secret)
     return dockerd_container.with_exec(
-        ["dockerd", "--log-level=error", f"--host=tcp://0.0.0.0:{DOCKER_HOST_PORT}", "--tls=false"], insecure_root_capabilities=True
+        [
+            "dockerd",
+            "--data-root=/var/lib/docker_debug",
+            "--log-level=debug",
+            "--debug",
+            f"--host=tcp://0.0.0.0:{DOCKER_HOST_PORT}",
+            "--tls=false",
+        ],
+        insecure_root_capabilities=True,
     ).as_service()
 
 
