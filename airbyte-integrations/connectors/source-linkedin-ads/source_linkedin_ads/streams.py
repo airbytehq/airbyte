61aa222540d96a953b304ebda4f106a33acf2608
FVidalCarneiro <francisco.carneiro@agicap.com>
2024-02-06 14:13:18 +0100

    feat: Complementing next page request loop stopping criteria

--- airbyte-integrations/connectors/source-linkedin-ads/source_linkedin_ads/streams.py
+++ airbyte-integrations/connectors/source-linkedin-ads/source_linkedin_ads/streams.py
@@ -70,7 +70,7 @@ class LinkedinAdsStream(HttpStream, ABC):
         https://docs.microsoft.com/en-us/linkedin/shared/api-guide/concepts/pagination?context=linkedin/marketing/context
         """
         parsed_response = response.json()
-        if len(parsed_response.get("elements")) == 0:
+        if len(parsed_response.get("elements")) < self.records_limit and (parsed_response.get("paging")["start"] + self.records_limit > parsed_response.get("paging")["total"]):
             return None
         return {"start": parsed_response.get("paging").get("start") + self.records_limit}
 
