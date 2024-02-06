1dd5d9438ae87f2d05dfbc96251e76da2eb9dbf3
Artem Inzhyyants <artem.inzhyyants@gmail.com>
2024-02-06 23:46:16 +0100

    Revert "Airbyte CDK: add headers"
    This reverts commit ee904f67

--- airbyte-cdk/python/airbyte_cdk/test/mock_http/response.py
+++ airbyte-cdk/python/airbyte_cdk/test/mock_http/response.py
@@ -3,10 +3,9 @@ from typing import Any, MutableMapping
 
 
 class HttpResponse:
-    def __init__(self, body: str, status_code: int = 200, headers: MutableMapping[str, Any] = {}):
+    def __init__(self, body: str, status_code: int = 200):
         self._body = body
         self._status_code = status_code
-        self._headers = headers
 
     @property
     def body(self) -> str:
@@ -15,7 +14,3 @@ class HttpResponse:
     @property
     def status_code(self) -> int:
         return self._status_code
-
-    @property
-    def headers(self) -> dict:
-        return self._headers
