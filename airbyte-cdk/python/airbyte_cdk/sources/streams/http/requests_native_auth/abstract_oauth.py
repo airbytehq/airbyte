e8eacc23d0c38fc83685770386c8e525b39e22cc
Alexandre Girard <alexandre@airbyte.io>
2024-02-07 09:19:40 -0800

    Update abstract_oauth.py

--- airbyte-cdk/python/airbyte_cdk/sources/streams/http/requests_native_auth/abstract_oauth.py
+++ airbyte-cdk/python/airbyte_cdk/sources/streams/http/requests_native_auth/abstract_oauth.py
@@ -119,6 +119,7 @@ class AbstractOauth2Authenticator(AuthBase):
             if response.ok:
                 response_json = response.json()
                 # Add the access token to the list of secrets so it is replaced before logging the response
+                # An argument could be made to remove the prevous access key from the list of secrets, but unmasking values seems like a security incident waiting to happen...
                 access_key = response_json[self.get_access_token_name()]
                 add_to_secrets(access_key)
                 self._log_response(response)
