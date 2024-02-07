f83853f78fcd636abb708477eb9786e27dffeed0
Xiaohan Song <xiaohan@airbyte.io>
2024-02-06 20:32:34 -0800

    get container jdbc url

--- airbyte-cdk/java/airbyte-cdk/db-sources/src/testFixtures/java/io/airbyte/cdk/testutils/TestDatabase.java
+++ airbyte-cdk/java/airbyte-cdk/db-sources/src/testFixtures/java/io/airbyte/cdk/testutils/TestDatabase.java
@@ -193,7 +193,8 @@ abstract public class TestDatabase<C extends JdbcDatabaseContainer<?>, T extends
         LOGGER.debug("execution success\nstdout:\n{}\nstderr:\n{}", exec.getStdout(), exec.getStderr());
       } else {
         LOGGER.error("execution failure, code {}\nstdout:\n{}\nstderr:\n{}", exec.getExitCode(), exec.getStdout(), exec.getStderr());
-        throw new RuntimeException("error executing bootstrap command: " + cmd + "; output: " + exec.getStdout() + "; error: " + exec.getStderr());
+        throw new RuntimeException("error executing bootstrap command: " + cmd + ";\n output: " + exec.getStdout() + ";\n error: " + exec.getStderr()
+            + "other info: container.getjdbcurl: " + this.getContainer().getJdbcUrl());
       }
     } catch (IOException e) {
       throw new UncheckedIOException(e);
