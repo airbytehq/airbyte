bcb061c2d35d81c0e7f698a9b762c8497736a855
Xiaohan Song <xiaohan@airbyte.io>
2024-02-07 14:44:44 -0800

    create init sql

--- airbyte-integrations/connectors/source-mysql/src/testFixtures/java/io/airbyte/integrations/source/mysql/MySQLContainerFactory.java
+++ airbyte-integrations/connectors/source-mysql/src/testFixtures/java/io/airbyte/integrations/source/mysql/MySQLContainerFactory.java
@@ -14,20 +14,16 @@ import org.testcontainers.containers.Network;
 import org.testcontainers.containers.output.Slf4jLogConsumer;
 import org.testcontainers.utility.DockerImageName;
 
-public class MySQLContainerFactory implements ContainerFactory<MySQLContainer<?>> {
+public class MySQLContainerFactory extends ContainerFactory<MySQLContainer<?>> {
   final static Logger logger = LoggerFactory.getLogger(MySQLContainerFactory.class);
 
   @Override
-  public MySQLContainer<?> createNewContainer(DockerImageName imageName) {
+  protected MySQLContainer<?> createNewContainer(DockerImageName imageName) {
     var container = new MySQLContainer<>(imageName.asCompatibleSubstituteFor("mysql"))
         .withLogConsumer(new Slf4jLogConsumer(logger))
-        .withAccessToHost(true);
+        .withAccessToHost(true)
+        .withInitScript("init.sql");
     return container;
-  }
-
-  @Override
-  public Class<?> getContainerClass() {
-    return MySQLContainer.class;
   }
 
   /**
