e24a139a4436cdf25a1cca1a8887b9b2d690673b
Aaron Steers <aj@airbyte.io>
2024-02-07 15:31:33 -0800

    expanded docstr

--- airbyte-lib/airbyte_lib/registry.py
+++ airbyte-lib/airbyte_lib/registry.py
@@ -108,7 +108,10 @@ def get_connector_metadata(name: str) -> ConnectorMetadata:
 
 
 def get_available_connectors() -> list[str]:
-    """Return a list of all available connectors."""
+    """Return a list of all available connectors.
+
+    Connectors will be returned in alphabetical order, with the standard prefix "source-".
+    """
     return sorted(
         conn.name for conn in _get_registry_cache().values() if conn.pypi_package_name is not None
     )
