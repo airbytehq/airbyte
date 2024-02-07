12e7aa727e2de2716ec6fd7594687be1b6ec9b8a
Aaron Steers <aj@airbyte.io>
2024-02-07 13:58:46 -0800

    add get_available_connectors()

--- airbyte-lib/airbyte_lib/registry.py
+++ airbyte-lib/airbyte_lib/registry.py
@@ -105,3 +105,10 @@ def get_connector_metadata(name: str) -> ConnectorMetadata:
             },
         )
     return cache[name]
+
+
+def get_available_connectors() -> list[str]:
+    """Return a list of all available connectors."""
+    return sorted(
+        conn.name for conn in _get_registry_cache().values() if conn.pypi_package_name is not None
+    )
