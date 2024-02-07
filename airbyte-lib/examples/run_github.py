13df6e6eddf2c9bfd04331b3c491a62b1e5cba40
Aaron Steers <aj@airbyte.io>
2024-02-07 10:32:24 -0800

    update examples

--- airbyte-lib/examples/run_github.py
+++ airbyte-lib/examples/run_github.py
@@ -2,7 +2,7 @@
 """A simple test of AirbyteLib, using the Faker source connector.
 
 Usage (from airbyte-lib root directory):
-> poetry run python ./examples/run_faker.py
+> poetry run python ./examples/run_github.py
 
 No setup is needed, but you may need to delete the .venv-source-faker folder
 if your installation gets interrupted or corrupted.
@@ -12,15 +12,19 @@ from __future__ import annotations
 import airbyte_lib as ab
 
 
+# Create a token here: https://github.com/settings/tokens
 GITHUB_TOKEN = ab.get_secret("GITHUB_PERSONAL_ACCESS_TOKEN")
 
 
 source = ab.get_source("source-github")
 source.set_config(
-    {"repositories": ["airbytehq/airbyte"], "credentials": {"personal_access_token": GITHUB_TOKEN}}
+    {
+        "repositories": ["airbytehq/quickstarts"],
+        "credentials": {"personal_access_token": GITHUB_TOKEN},
+    }
 )
 source.check()
-source.set_streams(["products", "users", "purchases"])
+source.set_streams(["issues", "pull_requests", "commits"])
 
 result = source.read()
 
