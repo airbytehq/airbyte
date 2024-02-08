d0347755085d7dd188f375e616c694ab63401067
Christo Grabowski <108154848+ChristoGrab@users.noreply.github.com>
2024-02-08 13:45:28 -0500

    Source Linnworks: CDK update (#34717)

--- airbyte-integrations/connectors/source-linnworks/setup.py
+++ airbyte-integrations/connectors/source-linnworks/setup.py
@@ -5,9 +5,7 @@
 
 from setuptools import find_packages, setup
 
-MAIN_REQUIREMENTS = [
-    "airbyte-cdk",
-]
+MAIN_REQUIREMENTS = ["airbyte-cdk", "vcrpy"]
 
 TEST_REQUIREMENTS = [
     "pytest~=6.1",
