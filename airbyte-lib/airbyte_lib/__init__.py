1710a5dc020f5970d70c9e1e0dbe50bef6e1a5b1
Aaron ("AJ") Steers <aj@airbyte.io>
2024-02-07 16:04:45 -0800

    AirbyteLib: Add 'get_available_connectors()' option (#34982)

--- airbyte-lib/airbyte_lib/__init__.py
+++ airbyte-lib/airbyte_lib/__init__.py
@@ -5,6 +5,7 @@ from airbyte_lib._factories.cache_factories import get_default_cache, new_local_
 from airbyte_lib._factories.connector_factories import get_source
 from airbyte_lib.caches import DuckDBCache, DuckDBCacheConfig
 from airbyte_lib.datasets import CachedDataset
+from airbyte_lib.registry import get_available_connectors
 from airbyte_lib.results import ReadResult
 from airbyte_lib.secrets import SecretSource, get_secret
 from airbyte_lib.source import Source
@@ -14,6 +15,7 @@ __all__ = [
     "CachedDataset",
     "DuckDBCache",
     "DuckDBCacheConfig",
+    "get_available_connectors",
     "get_source",
     "get_default_cache",
     "get_secret",
