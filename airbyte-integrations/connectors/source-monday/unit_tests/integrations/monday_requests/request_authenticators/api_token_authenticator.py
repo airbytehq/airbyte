b983e8fb576153dab7e84081fd7bff62d05b273c
Roman Yermilov [GL] <86300758+roman-yermilov-gl@users.noreply.github.com>
2024-02-09 14:20:44 +0100

    Source Monday: add integration tests (#35016)

---
+++ airbyte-integrations/connectors/source-monday/unit_tests/integrations/monday_requests/request_authenticators/api_token_authenticator.py
@@ -0,0 +1,15 @@
+# Copyright (c) 2023 Airbyte, Inc., all rights reserved.
+
+import base64
+
+from .authenticator import Authenticator
+
+
+class ApiTokenAuthenticator(Authenticator):
+    def __init__(self, api_token: str) -> None:
+        super().__init__()
+        self._api_token = api_token
+
+    @property
+    def client_access_token(self) -> str:
+        return f"Bearer {self._api_token}"
