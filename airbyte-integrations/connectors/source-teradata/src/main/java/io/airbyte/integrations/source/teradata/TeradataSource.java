1839327b8eb83eb16a5dc9a175448154e5e7899b
Baptiste Bergmann <baptiste.bergmann@maxa.ai>
2024-02-13 09:31:51 -0500

    feat(teradata): add ssh tunnel

--- airbyte-integrations/connectors/source-teradata/src/main/java/io/airbyte/integrations/source/teradata/TeradataSource.java
+++ airbyte-integrations/connectors/source-teradata/src/main/java/io/airbyte/integrations/source/teradata/TeradataSource.java
@@ -4,32 +4,61 @@
 
 package io.airbyte.integrations.source.teradata;
 
+import static io.airbyte.cdk.db.jdbc.JdbcConstants.INTERNAL_COLUMN_NAME;
+import static io.airbyte.cdk.db.jdbc.JdbcConstants.INTERNAL_COLUMN_SIZE;
+import static io.airbyte.cdk.db.jdbc.JdbcConstants.INTERNAL_COLUMN_TYPE;
+import static io.airbyte.cdk.db.jdbc.JdbcConstants.INTERNAL_COLUMN_TYPE_NAME;
+import static io.airbyte.cdk.db.jdbc.JdbcConstants.INTERNAL_DECIMAL_DIGITS;
+import static io.airbyte.cdk.db.jdbc.JdbcConstants.INTERNAL_IS_NULLABLE;
+import static io.airbyte.cdk.db.jdbc.JdbcConstants.INTERNAL_SCHEMA_NAME;
+import static io.airbyte.cdk.db.jdbc.JdbcConstants.INTERNAL_TABLE_NAME;
+import static io.airbyte.cdk.db.jdbc.JdbcConstants.JDBC_COLUMN_COLUMN_NAME;
+import static io.airbyte.cdk.db.jdbc.JdbcConstants.JDBC_COLUMN_DATABASE_NAME;
+import static io.airbyte.cdk.db.jdbc.JdbcConstants.JDBC_COLUMN_DATA_TYPE;
+import static io.airbyte.cdk.db.jdbc.JdbcConstants.JDBC_COLUMN_SCHEMA_NAME;
+import static io.airbyte.cdk.db.jdbc.JdbcConstants.JDBC_COLUMN_SIZE;
+import static io.airbyte.cdk.db.jdbc.JdbcConstants.JDBC_COLUMN_TABLE_NAME;
+import static io.airbyte.cdk.db.jdbc.JdbcConstants.JDBC_COLUMN_TYPE_NAME;
+import static io.airbyte.cdk.db.jdbc.JdbcConstants.JDBC_DECIMAL_DIGITS;
+import static io.airbyte.cdk.db.jdbc.JdbcConstants.JDBC_IS_NULLABLE;
+import static io.airbyte.cdk.db.jdbc.JdbcConstants.KEY_SEQ;
+
 import com.fasterxml.jackson.databind.JsonNode;
 import com.google.common.collect.ImmutableMap;
 import io.airbyte.cdk.db.factory.DataSourceFactory;
+import io.airbyte.cdk.db.factory.DatabaseDriver;
 import io.airbyte.cdk.db.jdbc.JdbcDatabase;
 import io.airbyte.cdk.db.jdbc.JdbcUtils;
 import io.airbyte.cdk.db.jdbc.StreamingJdbcDatabase;
 import io.airbyte.cdk.db.jdbc.streaming.AdaptiveStreamingQueryConfig;
+import io.airbyte.cdk.db.SqlDatabase;
 import io.airbyte.cdk.integrations.base.IntegrationRunner;
 import io.airbyte.cdk.integrations.base.Source;
+import io.airbyte.cdk.integrations.base.ssh.SshWrappedSource;
 import io.airbyte.cdk.integrations.source.jdbc.AbstractJdbcSource;
+import io.airbyte.cdk.integrations.source.jdbc.dto.JdbcPrivilegeDto;
 import io.airbyte.cdk.integrations.source.jdbc.JdbcDataSourceUtils;
 import io.airbyte.cdk.integrations.source.relationaldb.TableInfo;
 import io.airbyte.commons.json.Jsons;
 import io.airbyte.commons.map.MoreMaps;
 import io.airbyte.protocol.models.CommonField;
+import io.airbyte.protocol.models.JsonSchemaType;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.UncheckedIOException;
 import java.nio.charset.StandardCharsets;
 import java.sql.JDBCType;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.stream.Collectors;
 import javax.sql.DataSource;
+import org.apache.commons.lang3.tuple.ImmutablePair;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -39,8 +68,9 @@ public class TeradataSource extends AbstractJdbcSource<JDBCType> implements Sour
 
   private static final int INTERMEDIATE_STATE_EMISSION_FREQUENCY = 10_000;
 
-  static final String DRIVER_CLASS = "com.teradata.jdbc.TeraDriver";
+  public static final String DRIVER_CLASS = DatabaseDriver.TERADATA.getDriverClassName();
 
+  public static final String PARAM_DBS_PORT = "dbs_port";
   public static final String PARAM_MODE = "mode";
   public static final String PARAM_SSL = "ssl";
   public static final String PARAM_SSL_MODE = "ssl_mode";
@@ -50,12 +80,16 @@ public class TeradataSource extends AbstractJdbcSource<JDBCType> implements Sour
 
   private static final String CA_CERTIFICATE = "ca.pem";
 
+  public static Source sshWrappedSource(TeradataSource source) {
+    return new SshWrappedSource(source, JdbcUtils.HOST_LIST_KEY, JdbcUtils.PORT_LIST_KEY);
+  }
+
   public TeradataSource() {
     super(DRIVER_CLASS, AdaptiveStreamingQueryConfig::new, new TeradataSourceOperations());
   }
 
   public static void main(final String[] args) throws Exception {
-    final Source source = new TeradataSource();
+    final Source source = TeradataSource.sshWrappedSource(new TeradataSource());
     LOGGER.info("starting source: {}", TeradataSource.class);
     new IntegrationRunner(source).run(args);
     LOGGER.info("completed source: {}", TeradataSource.class);
@@ -64,16 +98,17 @@ public class TeradataSource extends AbstractJdbcSource<JDBCType> implements Sour
   @Override
   public JsonNode toDatabaseConfig(final JsonNode config) {
     final String schema = config.get(JdbcUtils.DATABASE_KEY).asText();
-
-    final String host = config.has(JdbcUtils.PORT_KEY) ? config.get(JdbcUtils.HOST_KEY).asText() + ":" + config.get(JdbcUtils.PORT_KEY).asInt()
-        : config.get(JdbcUtils.HOST_KEY).asText();
-
-    final String jdbcUrl = String.format("jdbc:teradata://%s/", host);
+    final String host = config.get(JdbcUtils.HOST_KEY).asText();
+    final String jdbcUrl = String.format(DatabaseDriver.TERADATA.getUrlFormatString(), host);
 
     final ImmutableMap.Builder<Object, Object> configBuilder = ImmutableMap.builder()
         .put(JdbcUtils.USERNAME_KEY, config.get(JdbcUtils.USERNAME_KEY).asText())
         .put(JdbcUtils.JDBC_URL_KEY, jdbcUrl)
         .put(JdbcUtils.SCHEMA_KEY, schema);
+
+    if (config.has(JdbcUtils.PORT_KEY)) {
+      configBuilder.put(JdbcUtils.PORT_KEY, config.get(JdbcUtils.PORT_KEY).asText());
+    }
 
     if (config.has(JdbcUtils.PASSWORD_KEY)) {
       configBuilder.put(JdbcUtils.PASSWORD_KEY, config.get(JdbcUtils.PASSWORD_KEY).asText());
@@ -99,18 +134,110 @@ public class TeradataSource extends AbstractJdbcSource<JDBCType> implements Sour
 
   @Override
   public List<TableInfo<CommonField<JDBCType>>> discoverInternal(JdbcDatabase database) throws Exception {
-    return discoverInternal(database,
-        database.getSourceConfig().has(JdbcUtils.DATABASE_KEY) ? database.getSourceConfig().get(JdbcUtils.DATABASE_KEY).asText() : null);
+    return discoverInternal(database, database.getSourceConfig().has(JdbcUtils.DATABASE_KEY) ? database.getSourceConfig().get(JdbcUtils.DATABASE_KEY).asText() : null);
+  }
+
+
+  @Override
+  protected List<TableInfo<CommonField<JDBCType>>> discoverInternal(final JdbcDatabase database, final String schema) throws Exception {
+    final Set<String> internalSchemas = new HashSet<>(getExcludedInternalNameSpaces());
+    LOGGER.info("Internal schemas to exclude: {}", internalSchemas);
+    final Set<JdbcPrivilegeDto> tablesWithSelectGrantPrivilege = getPrivilegesTableForCurrentUser(database, schema);
+    List<JsonNode> columnsInfo;
+    
+    try {
+      columnsInfo = database.bufferedResultSetQuery(
+        connection -> connection.getMetaData().getColumns(getCatalog(database), schema, null, null),
+        this::getColumnMetadata);
+    } catch (final Exception getColumnsError) {
+      LOGGER.info("EXCEPTION: unable to run getColumns on all tables in schema: '{}'", schema);
+      var query = "SELECT DataBaseName, TableName FROM DBC.TablesV WHERE DatabaseName = '" + getCatalog(database).toUpperCase() + "' ORDER BY TableName;";
+      columnsInfo = new ArrayList<JsonNode>();
+      var tableRows = database.bufferedResultSetQuery(
+        conn -> conn.createStatement().executeQuery(query),
+        resultSet -> JdbcUtils.getDefaultSourceOperations().rowToJson(resultSet));
+      for (final JsonNode tableRow : tableRows) {
+        var DataBaseName = tableRow.get("DataBaseName").asText();
+        var TableName = tableRow.get("TableName").asText();
+        try {
+          columnsInfo.addAll(database.bufferedResultSetQuery(
+            connection -> connection.getMetaData().getColumns(getCatalog(database), DataBaseName, TableName, null),
+            this::getColumnMetadata));
+        } catch (final Exception e) {
+          LOGGER.info("FAILING ON schema: '{}' and table: '{}'", DataBaseName, TableName);
+        }
+      }
+    }
+
+    return columnsInfo.stream()
+        .filter(excludeNotAccessibleTables(internalSchemas, tablesWithSelectGrantPrivilege))
+        // group by schema and table name to handle the case where a table with the same name exists in
+        // multiple schemas.
+        .collect(Collectors.groupingBy(t -> ImmutablePair.of(t.get(INTERNAL_SCHEMA_NAME).asText(), t.get(INTERNAL_TABLE_NAME).asText())))
+        .values()
+        .stream()
+        .map(fields -> TableInfo.<CommonField<JDBCType>>builder()
+            .nameSpace(fields.get(0).get(INTERNAL_SCHEMA_NAME).asText())
+            .name(fields.get(0).get(INTERNAL_TABLE_NAME).asText())
+            .fields(fields.stream()
+                // read the column metadata Json object, and determine its type
+                .map(f -> {
+                  final JDBCType datatype = sourceOperations.getDatabaseFieldType(f);
+                  final JsonSchemaType jsonType = getAirbyteType(datatype);
+                  LOGGER.debug("Table {} column {} (type {}[{}], nullable {}) -> {}",
+                      fields.get(0).get(INTERNAL_TABLE_NAME).asText(),
+                      f.get(INTERNAL_COLUMN_NAME).asText(),
+                      f.get(INTERNAL_COLUMN_TYPE_NAME).asText(),
+                      f.get(INTERNAL_COLUMN_SIZE).asInt(),
+                      f.get(INTERNAL_IS_NULLABLE).asBoolean(),
+                      jsonType);
+                  return new CommonField<JDBCType>(f.get(INTERNAL_COLUMN_NAME).asText(), datatype) {};
+                })
+                .collect(Collectors.toList()))
+            .cursorFields(extractCursorFields(fields))
+            .build())
+        .collect(Collectors.toList());
+  }
+
+  private JsonNode getColumnMetadata(final ResultSet resultSet) throws SQLException {
+    final var fieldMap = ImmutableMap.<String, Object>builder()
+        // we always want a namespace, if we cannot get a schema, use db name.
+        .put(INTERNAL_SCHEMA_NAME,
+            resultSet.getObject(JDBC_COLUMN_SCHEMA_NAME) != null ? resultSet.getString(JDBC_COLUMN_SCHEMA_NAME)
+                : resultSet.getObject(JDBC_COLUMN_DATABASE_NAME))
+        .put(INTERNAL_TABLE_NAME, resultSet.getString(JDBC_COLUMN_TABLE_NAME))
+        .put(INTERNAL_COLUMN_NAME, resultSet.getString(JDBC_COLUMN_COLUMN_NAME))
+        .put(INTERNAL_COLUMN_TYPE, resultSet.getString(JDBC_COLUMN_DATA_TYPE))
+        .put(INTERNAL_COLUMN_TYPE_NAME, resultSet.getString(JDBC_COLUMN_TYPE_NAME))
+        .put(INTERNAL_COLUMN_SIZE, resultSet.getInt(JDBC_COLUMN_SIZE))
+        .put(INTERNAL_IS_NULLABLE, resultSet.getString(JDBC_IS_NULLABLE));
+    if (resultSet.getString(JDBC_DECIMAL_DIGITS) != null) {
+      fieldMap.put(INTERNAL_DECIMAL_DIGITS, resultSet.getString(JDBC_DECIMAL_DIGITS));
+    }
+    return Jsons.jsonNode(fieldMap.build());
+  }
+  private String getCatalog(final SqlDatabase database) {
+    var return_data = (database.getSourceConfig().has(JdbcUtils.DATABASE_KEY) ? database.getSourceConfig().get(JdbcUtils.DATABASE_KEY).asText() : null);
+    return return_data;
+  }
+
+  private List<String> extractCursorFields(final List<JsonNode> fields) {
+    return fields.stream()
+        .filter(field -> isCursorType(sourceOperations.getDatabaseFieldType(field)))
+        .map(field -> field.get(INTERNAL_COLUMN_NAME).asText())
+        .collect(Collectors.toList());
   }
 
   @Override
   public JdbcDatabase createDatabase(JsonNode sourceConfig) throws SQLException {
     final Map<String, String> customProperties = JdbcUtils.parseJdbcParameters(sourceConfig, JdbcUtils.JDBC_URL_PARAMS_KEY);
     final Map<String, String> sslConnectionProperties = getSslConnectionProperties(sourceConfig);
+    final Map<String, String> portProperty = getPortProperty(sourceConfig);
     JdbcDataSourceUtils.assertCustomParametersDontOverwriteDefaultParameters(customProperties, sslConnectionProperties);
 
     final JsonNode jdbcConfig = toDatabaseConfig(sourceConfig);
-    final Map<String, String> connectionProperties = MoreMaps.merge(customProperties, sslConnectionProperties);
+    final Map<String, String> connectionProperties = MoreMaps.merge(customProperties, sslConnectionProperties, portProperty);
+
     // Create the data source
     final DataSource dataSource = DataSourceFactory.create(
         jdbcConfig.has(JdbcUtils.USERNAME_KEY) ? jdbcConfig.get(JdbcUtils.USERNAME_KEY).asText() : null,
@@ -131,6 +258,17 @@ public class TeradataSource extends AbstractJdbcSource<JDBCType> implements Sour
     database.setSourceConfig(sourceConfig);
     database.setDatabaseConfig(jdbcConfig);
     return database;
+  }
+
+  private Map<String, String> getPortProperty(JsonNode config) {
+    final Map<String, String> additionalParameters = new HashMap<>();
+
+    if (config.has(JdbcUtils.PORT_KEY)) {
+      LOGGER.debug("Using custom port");
+      additionalParameters.put(TeradataSource.PARAM_DBS_PORT, config.get(JdbcUtils.PORT_KEY).asText());
+    }
+
+    return additionalParameters;
   }
 
   private Map<String, String> getSslConnectionProperties(JsonNode config) {
