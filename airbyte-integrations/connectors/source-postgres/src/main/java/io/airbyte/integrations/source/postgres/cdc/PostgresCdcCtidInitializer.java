2930c8dfc3997a511b4905a418cb5a95f8a3453d
Akash Kulkarni <akash@airbyte.io>
2024-02-15 10:52:42 -0800

    only throw exception

--- airbyte-integrations/connectors/source-postgres/src/main/java/io/airbyte/integrations/source/postgres/cdc/PostgresCdcCtidInitializer.java
+++ airbyte-integrations/connectors/source-postgres/src/main/java/io/airbyte/integrations/source/postgres/cdc/PostgresCdcCtidInitializer.java
@@ -112,8 +112,8 @@ public class PostgresCdcCtidInitializer {
 
       if (!savedOffsetAfterReplicationSlotLSN) {
         AirbyteTraceMessageUtility.emitAnalyticsTrace(cdcCursorInvalidMessage());
-        AirbyteTraceMessageUtility.emitConfigErrorTrace(new ConfigErrorException("Invalid cdc cursor. Check docs"), "Invalid CDC cursor.");
-        throw new ConfigErrorException("Throwing config error - invalid cdc cursor.");
+        //AirbyteTraceMessageUtility.emitConfigErrorTrace(new ConfigErrorException("Invalid cdc cursor. Check docs"), "Invalid CDC cursor.");
+        throw new ConfigErrorException("Throwing config error - invalid cdc cursor. Check docs");
         //LOGGER.warn("Saved offset is before Replication slot's confirmed_flush_lsn, Airbyte will trigger sync from scratch");
       } else if (!isDebugMode(sourceConfig) && PostgresUtils.shouldFlushAfterSync(sourceConfig)) {
         // We do not want to acknowledge the WAL logs in debug mode.
