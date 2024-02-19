b08c4b7fa67adf3ff30bfea6e29021540d288ed3
Artem Inzhyyants <artem.inzhyyants@gmail.com>
2024-02-19 21:21:50 +0100

    Source Mailchimp: add unit tests

--- airbyte-integrations/connectors/source-mailchimp/unit_tests/integration/test_automations.py
+++ airbyte-integrations/connectors/source-mailchimp/unit_tests/integration/test_automations.py
@@ -91,53 +91,32 @@ class AutomationsTest(TestCase):
 
         assert len(actual_messages.records) == 1003
 
-    # TODO: add tests
-    # def test_given_state_more_recent_than_some_records_when_read_incrementally_then_filter_records(self):
-    #     """Ensure incremental sync.
-    #     Stream `Events` is semi-incremental, so all requests will be performed and only new records will be extracted"""
-    #
-    #     self.r_mock.get(
-    #         HttpRequest(
-    #             url=f"https://api.github.com/repos/{_CONFIG.get('repositories')[0]}/events",
-    #             query_params={"per_page": 100},
-    #         ),
-    #         HttpResponse(json.dumps(find_template("events", __file__)), 200),
-    #     )
-    #
-    #     source = SourceMailchimp()
-    #     actual_messages = read(
-    #         source,
-    #         config=_CONFIG,
-    #         catalog=_create_catalog(sync_mode=SyncMode.incremental),
-    #         state=StateBuilder()
-    #         .with_stream_state("events", {"airbytehq/integration-test": {"created_at": "2022-06-09T10:00:00Z"}})
-    #         .build(),
-    #     )
-    #     assert len(actual_messages.records) == 1
-    #
-    # def test_when_read_incrementally_then_emit_state_message(self):
-    #     """Ensure incremental sync emits correct stream state message"""
-    #
-    #     self.r_mock.get(
-    #         HttpRequest(
-    #             url=f"https://api.github.com/repos/{_CONFIG.get('repositories')[0]}/events",
-    #             query_params={"per_page": 100},
-    #         ),
-    #         HttpResponse(json.dumps(find_template("events", __file__)), 200),
-    #     )
-    #
-    #     source = SourceMailchimp()
-    #     actual_messages = read(
-    #         source,
-    #         config=_CONFIG,
-    #         catalog=_create_catalog(sync_mode=SyncMode.incremental),
-    #         state=StateBuilder()
-    #         .with_stream_state("events", {"airbytehq/integration-test": {"created_at": "2020-06-09T10:00:00Z"}})
-    #         .build(),
-    #     )
-    #     assert actual_messages.state_messages[0].state.data == {
-    #         "events": {"airbytehq/integration-test": {"created_at": "2022-06-09T12:47:28Z"}}
-    #     }
+    def test_when_read_incrementally_then_emit_state_message(self):
+        """Ensure incremental sync emits correct stream state message"""
+
+        self.r_mock.get(
+            HttpRequest(
+                url="https://us10.api.mailchimp.com/3.0/automations",
+                query_params={"sort_field": "create_time",
+                              "sort_dir": "ASC",
+                              "exclude_fields": "automations._links",
+                              "page_size": 1000,
+                              "since_create_time": "2023-01-01T00:00:00.001000Z",
+                              "before_create_time": "2023-01-31T23:59:59.001000Z"},
+            ),
+            HttpResponse(json.dumps(find_template("automations", __file__)), 200),
+        )
+
+        source = SourceMailchimp()
+        actual_messages = read(
+            source,
+            config=_CONFIG,
+            catalog=_create_catalog(sync_mode=SyncMode.incremental),
+            state=StateBuilder()
+            .with_stream_state("automations", {"create_time": "2220-11-23T05:42:11+00:00"})
+            .build(),
+        )
+        assert actual_messages.state_messages[0].state.data == {'automations': {'create_time': '2220-11-23T05:42:11+00:00'}}
     #
     # def test_read_handles_expected_error_correctly_and_exits_with_complete_status(self):
     #     """Ensure read() method does not raise an Exception and log message with error is in output"""
