7b11e0f31d06883e31e3e732344652db0897ba01
darynaishchenko <darina.ishchenko17@gmail.com>
2024-02-21 13:20:20 +0200

    updated TestSuiteReportStream with skipp for base test

--- airbyte-integrations/connectors/source-bing-ads/unit_tests/integrations/test_report_stream.py
+++ airbyte-integrations/connectors/source-bing-ads/unit_tests/integrations/test_report_stream.py
@@ -42,50 +42,61 @@ class TestSuiteReportStream(TestReportStream):
     second_read_state: dict
     transform_field: str = "AccountId"
 
+    def setUp(self):
+        if not self.stream_name:
+            self.skipTest("Skipping TestSuiteReportStream")
+
     @HttpMocker()
     def test_return_records_from_given_csv_file(self, http_mocker: HttpMocker):
-        if self.stream_name:
-            self.auth_client(http_mocker)
-            output = self.read_stream(
-                self.stream_name,
-                SyncMode.full_refresh, self._config, [],
-                self.report_file)
-            assert len(output.records) == self.records_number
+        self.auth_client(http_mocker)
+        output = self.read_stream(
+            self.stream_name,
+            SyncMode.full_refresh,
+            self._config,
+            [],
+            self.report_file
+        )
+        assert len(output.records) == self.records_number
 
     @HttpMocker()
     def test_transform_records_from_given_csv_file(self, http_mocker: HttpMocker):
-        if self.stream_name:
-            self.auth_client(http_mocker)
-            output = self.read_stream(
-                self.stream_name,
-                SyncMode.full_refresh, self._config, [],
-                self.report_file)
+        self.auth_client(http_mocker)
+        output = self.read_stream(
+            self.stream_name,
+            SyncMode.full_refresh,
+            self._config,
+            [],
+            self.report_file
+        )
 
-            assert len(output.records) == self.records_number
-            for record in output.records:
-                assert self.transform_field in record.record.data.keys()
+        assert len(output.records) == self.records_number
+        for record in output.records:
+            assert self.transform_field in record.record.data.keys()
 
     @HttpMocker()
     def test_incremental_read_returns_records(self, http_mocker: HttpMocker):
-        if self.stream_name:
-            self.auth_client(http_mocker)
-            output = self.read_stream(
-                self.stream_name,
-                SyncMode.incremental, self._config, [],
-                self.report_file)
-            assert len(output.records) == self.records_number
-            assert output.most_recent_state == self.first_read_state
+        self.auth_client(http_mocker)
+        output = self.read_stream(
+            self.stream_name,
+            SyncMode.incremental,
+            self._config,
+            [],
+            self.report_file
+        )
+        assert len(output.records) == self.records_number
+        assert output.most_recent_state == self.first_read_state
 
     @HttpMocker()
     def test_incremental_read_with_state_returns_records(self, http_mocker: HttpMocker):
-        if self.stream_name:
-            state = self._state(self.state_file)
-            self.auth_client(http_mocker)
-            output = self.read_stream(self.stream_name,
-                                      SyncMode.incremental, self._config, [],
-                                      self.incremental_report_file,
-                                      state)
-            assert len(output.records) == self.records_number
-            assert output.most_recent_state == self.second_read_state
-
-
+        state = self._state(self.state_file)
+        self.auth_client(http_mocker)
+        output = self.read_stream(
+            self.stream_name,
+            SyncMode.incremental,
+            self._config,
+            [],
+            self.incremental_report_file,
+            state
+        )
+        assert len(output.records) == self.records_number
+        assert output.most_recent_state == self.second_read_state
