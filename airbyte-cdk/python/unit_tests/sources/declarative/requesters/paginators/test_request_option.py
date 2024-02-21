f56893da90b9b8963a959e40a3a91c466362b28b
Artem Inzhyyants <artem.inzhyyants@gmail.com>
2024-02-21 18:16:38 +0100

    Airbyte CDK: add unit test
    Signed-off-by: Artem Inzhyyants <artem.inzhyyants@gmail.com>

--- airbyte-cdk/python/unit_tests/sources/declarative/requesters/paginators/test_request_option.py
+++ airbyte-cdk/python/unit_tests/sources/declarative/requesters/paginators/test_request_option.py
@@ -7,15 +7,19 @@ from airbyte_cdk.sources.declarative.requesters.request_option import RequestOpt
 
 
 @pytest.mark.parametrize(
-    "test_name, option_type, field_name",
+    "test_name, option_type, field_name, expected_field_name",
     [
-        ("test_limit_param_with_field_name", RequestOptionType.request_parameter, "field"),
-        ("test_limit_header_with_field_name", RequestOptionType.header, "field"),
-        ("test_limit_data_with_field_name", RequestOptionType.body_data, "field"),
-        ("test_limit_json_with_field_name", RequestOptionType.body_json, "field"),
+        ("test_limit_param_with_field_name", RequestOptionType.request_parameter, "field", "field"),
+        ("test_limit_header_with_field_name", RequestOptionType.header, "field", "field"),
+        ("test_limit_data_with_field_name", RequestOptionType.body_data, "field", "field"),
+        ("test_limit_json_with_field_name", RequestOptionType.body_json, "field", "field"),
+        ("test_limit_json_with_field_name", RequestOptionType.request_parameter, "since_{{ parameters['cursor_field'] }}", "since_updated_at"),
+        ("test_limit_header_with_field_name", RequestOptionType.header, "since_{{ parameters['cursor_field'] }}", "since_updated_at"),
+        ("test_limit_data_with_field_name", RequestOptionType.body_data, "since_{{ parameters['cursor_field'] }}", "since_updated_at"),
+        ("test_limit_json_with_field_name", RequestOptionType.body_json, "since_{{ parameters['cursor_field'] }}", "since_updated_at"),
     ],
 )
-def test_request_option(test_name, option_type, field_name):
-    request_option = RequestOption(inject_into=option_type, field_name=field_name, parameters={})
-    assert request_option.field_name.eval({}) == field_name
+def test_request_option(test_name, option_type, field_name: str, expected_field_name: str):
+    request_option = RequestOption(inject_into=option_type, field_name=field_name, parameters={"cursor_field": "updated_at"})
+    assert request_option.field_name.eval({}) == expected_field_name
     assert request_option.inject_into == option_type
