440f68134d1760a1cd0cf7e2ff7cf3b54a027fba
Daniel Tiesling <tiesling@gmail.com>
2024-02-21 10:46:13 -0800

    Moves TSV test to better location.

--- airbyte-integrations/connectors/source-s3/unit_tests/v4/test_format.py
+++
@@ -1,7 +0,0 @@
-# Copyright (c) 2023 Airbyte, Inc., all rights reserved.
-
-from airbyte_cdk.sources.file_based.config.csv_format import CsvFormat
-
-
-def test_csv_format_tab_delimited():
-    assert CsvFormat(delimiter=r"\t").delimiter == '\\t'
