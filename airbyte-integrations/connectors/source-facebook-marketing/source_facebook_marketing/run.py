8d6154f6700ec68d2a1d1f9b4d749799720dc2dc
Anatolii Yatsuk <35109939+tolik0@users.noreply.github.com>
2024-02-22 17:01:56 +0200

    :bug: Source Facebook Marketing: Add missing config migration (#35539)

--- airbyte-integrations/connectors/source-facebook-marketing/source_facebook_marketing/run.py
+++ airbyte-integrations/connectors/source-facebook-marketing/source_facebook_marketing/run.py
@@ -7,11 +7,12 @@ import sys
 
 from airbyte_cdk.entrypoint import launch
 
-from .config_migrations import MigrateAccountIdToArray
+from .config_migrations import MigrateAccountIdToArray, MigrateIncludeDeletedToStatusFilters
 from .source import SourceFacebookMarketing
 
 
 def run():
     source = SourceFacebookMarketing()
     MigrateAccountIdToArray.migrate(sys.argv[1:], source)
+    MigrateIncludeDeletedToStatusFilters.migrate(sys.argv[1:], source)
     launch(source, sys.argv[1:])
