6cfb2e855236516a4b0e5e7459ce71e0bef49db4
Oleksandr Bazarnov <oleksandr.bazarnov@globallogic.com>
2024-02-23 15:45:27 +0200

    added metafields and other streams, added RETRY for 200 and broken response VS Content-Length

--- airbyte-integrations/connectors/source-recharge/source_recharge/source.py
+++ airbyte-integrations/connectors/source-recharge/source_recharge/source.py
@@ -2,7 +2,11 @@
 # Copyright (c) 2023 Airbyte, Inc., all rights reserved.
 #
 
+
+from typing import Any, List, Mapping
+from airbyte_cdk.sources.streams import Stream
 from airbyte_cdk.sources.declarative.yaml_declarative_source import YamlDeclarativeSource
+from source_recharge.streams import Orders, RechargeTokenAuthenticator
 
 """
 This file provides the necessary constructs to interpret a provided declarative YAML configuration file into
@@ -13,5 +17,11 @@ WARNING: Do not modify this file.
 
 # Declarative Source
 class SourceRecharge(YamlDeclarativeSource):
-    def __init__(self):
+    def __init__(self) -> None:
         super().__init__(**{"path_to_yaml": "manifest.yaml"})
+
+    def streams(self, config: Mapping[str, Any]) -> List[Stream]:
+        auth = RechargeTokenAuthenticator(token=config["access_token"])
+        streams = super().streams(config=config)
+        streams.append(Orders(config, authenticator=auth))
+        return streams
