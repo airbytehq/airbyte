84ec2d471b92995b8a9fd8745386eb9d8fae5e88
Catherine Noll <noll.catherine@gmail.com>
2024-02-26 11:41:41 -0800

    Use FileBackend to write records & state

--- airbyte-ci/connectors/regression-testing/regression_testing/backends/file_backend.py
+++ airbyte-ci/connectors/regression-testing/regression_testing/backends/file_backend.py
@@ -1,16 +1,30 @@
+from typing import Any, Generator
+
+from airbyte_protocol.models import AirbyteMessage
+from airbyte_protocol.models import Type as AirbyteMessageType
+
 from .base_backend import BaseBackend
 
 
 class FileBackend(BaseBackend):
+    RELATIVE_RECORDS_PATH = "records.json"
+    RELATIVE_STATES_PATH = "states.json"
 
     def __init__(self, output_directory: str):
-        self.output_directory = output_directory
+        self._output_directory = output_directory
 
-    def write(self):
+    async def write(self, messages: Generator[AirbyteMessage, Any, None]):
+        with open(f"{self._output_directory}/{self.RELATIVE_RECORDS_PATH}", "w") as records_file:
+            with open(f"{self._output_directory}/{self.RELATIVE_STATES_PATH}", "w") as states_file:
+                for message in messages:
+                    if message.type == AirbyteMessageType.RECORD:
+                        # TODO: sort & filter out things like timestamp
+                        records_file.write(f"{message.record.json()}\n")
+                    elif message.type == AirbyteMessageType.STATE:
+                        states_file.write(f"{message.state.json()}\n")
+
+    async def read(self):
         raise NotImplementedError
 
-    def read(self):
-        raise NotImplementedError
-
-    def compare(self):
+    async def compare(self):
         raise NotImplementedError
