37e3d144fd90ce4ba694509a9a5a5b1cce3cb407
Catherine Noll <noll.catherine@gmail.com>
2024-02-26 14:11:12 -0800

    Organize files for all commands

--- airbyte-ci/connectors/regression-testing/src/regression_testing/backends/file_backend.py
+++ airbyte-ci/connectors/regression-testing/src/regression_testing/backends/file_backend.py
@@ -9,41 +9,77 @@ from .base_backend import BaseBackend
 
 
 class FileBackend(BaseBackend):
-    RELATIVE_RECORDS_PATH = "records.json"
-    RELATIVE_STATES_PATH = "states.json"
+    RELATIVE_CATALOGS_PATH = "catalog.jsonl"
+    RELATIVE_CONNECTION_STATUS_PATH = "connection_status.jsonl"
+    RELATIVE_RECORDS_PATH = "records.jsonl"
+    RELATIVE_SPECS_PATH = "spec.jsonl"
+    RELATIVE_STATES_PATH = "states.jsonl"
 
     def __init__(self, output_directory: str):
         self._output_directory = output_directory
 
     async def write(self, messages: Generator[AirbyteMessage, Any, None]):
-        streams_to_records, streams_to_states = self._get_per_stream_records_and_states(messages)
-        for stream in streams_to_states | streams_to_records:
+        messages = list(messages)
+        messages_by_type = self._get_messages_by_type(messages)
+
+        for stream in messages_by_type["records"] | messages_by_type["states"]:
             os.makedirs(f"{self._output_directory}/{stream}", exist_ok=True)
 
-        for stream, records in streams_to_records.items():
+        if messages_by_type["catalog"]:
+            with open(f"{self._output_directory}/{self.RELATIVE_CATALOGS_PATH}", "w") as catalogs_file:
+                for catalog in messages_by_type["catalog"]:
+                    catalogs_file.write(f"{catalog.json()}\n")
+
+        if messages_by_type["connection_status"]:
+            with open(f"{self._output_directory}/{self.RELATIVE_CONNECTION_STATUS_PATH}", "w") as statuses_file:
+                for status in messages_by_type["connection_status"]:
+                    statuses_file.write(f"{status.json()}\n")
+
+        for stream, records in messages_by_type["records"].items():
             with open(f"{self._output_directory}/{stream}/{self.RELATIVE_RECORDS_PATH}", "w") as records_file:
                 # TODO: sort & filter out things like timestamp
                 for record in records:
                     records_file.write(f"{record.json()}\n")
 
-        for stream, states in streams_to_states.items():
+        if messages_by_type["spec"]:
+            with open(f"{self._output_directory}/{self.RELATIVE_SPECS_PATH}", "w") as specs_file:
+                for spec in messages_by_type["spec"]:
+                    specs_file.write(f"{spec.json()}\n")
+
+        for stream, states in messages_by_type["states"].items():
             with open(f"{self._output_directory}/{stream}/{self.RELATIVE_STATES_PATH}", "w") as states_file:
                 for state in states:
                     states_file.write(f"{state.json()}\n")
 
-    def _get_per_stream_records_and_states(self, messages: Generator[AirbyteMessage, Any, None]) -> Tuple[Dict, Dict]:
-        streams_to_records = defaultdict(list)
-        streams_to_states = defaultdict(list)
+    def _get_messages_by_type(self, messages: Generator[AirbyteMessage, Any, None]) -> Dict[str, Dict]:
+        messages_by_type = {
+            "catalog": [],
+            "connection_status": [],
+            "records": defaultdict(list),
+            "spec": [],
+            "states": defaultdict(list),
+        }
+
         for message in messages:
-            if message.type == AirbyteMessageType.RECORD:
-                streams_to_records[message.record.stream].append(message.record)
+            if message.type == AirbyteMessageType.CATALOG:
+                messages_by_type["catalog"].append(message.catalog)
+
+            elif message.type == AirbyteMessageType.CONNECTION_STATUS:
+                messages_by_type["connection_status"].append(message.connectionStatus)
+
+            elif message.type == AirbyteMessageType.RECORD:
+                messages_by_type["records"][message.record.stream].append(message.record)
+
+            elif message.type == AirbyteMessageType.SPEC:
+                messages_by_type["spec"].append(message.spec)
+
             elif message.type == AirbyteMessageType.STATE:
                 stream_name = message.state.stream.stream_descriptor.name
                 stream_namespace = message.state.stream.stream_descriptor.namespace
                 key = f"{stream_name}_{stream_namespace}" if stream_namespace else stream_name
-                streams_to_states[key].append(message.state)
+                messages_by_type["records"][key].append(message.state)
 
-        return streams_to_records, streams_to_states
+        return messages_by_type
 
     async def read(self):
         raise NotImplementedError
