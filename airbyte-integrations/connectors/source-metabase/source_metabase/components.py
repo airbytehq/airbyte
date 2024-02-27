69e9a546386126ea68971ede0daaa39189557f24
Patrick Nilan <nilan.patrick@gmail.com>
2024-02-27 13:32:42 -0800

    Add custom component to remove dashcards from dashboards stream

---
+++ airbyte-integrations/connectors/source-metabase/source_metabase/components.py
@@ -0,0 +1,47 @@
+#
+# Copyright (c) 2023 Airbyte, Inc., all rights reserved.
+#
+
+from dataclasses import InitVar, dataclass
+from typing import Any, Iterable, Mapping, Optional, Union
+
+from airbyte_cdk.sources.declarative.transformations.transformation import RecordTransformation
+from airbyte_cdk.sources.declarative.types import Config, Record, StreamSlice, StreamState
+
+@dataclass
+class CustomFieldTransformation(RecordTransformation):
+    """
+    Add custom field based on condition. Jinja interpolation does not support list comprehension.
+    https://github.com/airbytehq/airbyte/issues/23134
+    """
+
+    def transform(
+        self,
+        record: Record,
+        config: Optional[Config] = None,
+        stream_state: Optional[StreamState] = None,
+        stream_slice: Optional[StreamSlice] = None,
+    ) -> Record:
+        """
+        Method to detect custom fields that start with 'cf_' from chargbee models.
+        Args:
+            record:
+            {
+                ...
+                'cf_custom_fields': 'some_value',
+                ...
+            }
+
+        Returns:
+            record:
+            {
+                ...
+                'custom_fields': [{
+                    'name': 'cf_custom_fields',
+                    'value': some_value'
+                }],
+                ...
+            }
+        """
+        record.pop("dashcards")
+        return record
