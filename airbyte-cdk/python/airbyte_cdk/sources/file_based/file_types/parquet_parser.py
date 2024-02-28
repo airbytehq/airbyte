b4e3fb2166e78f42bee65b8e017577fd597c8cd6
erohmensing <erohmensing@gmail.com>
2024-02-27 17:20:20 -0800

    remove extra handling for none binary values

--- airbyte-cdk/python/airbyte_cdk/sources/file_based/file_types/parquet_parser.py
+++ airbyte-cdk/python/airbyte_cdk/sources/file_based/file_types/parquet_parser.py
@@ -122,10 +122,8 @@ class ParquetParser(FileTypeParser):
 
         # Decode binary strings to utf-8
         if ParquetParser._is_binary(parquet_value.type):
-            py_value = parquet_value.as_py()
-            if py_value is None:
-                return py_value
-            return py_value.decode("utf-8")
+            return parquet_value.as_py().decode("utf-8")
+
         if pa.types.is_decimal(parquet_value.type):
             if parquet_format.decimal_as_float:
                 return parquet_value.as_py()
