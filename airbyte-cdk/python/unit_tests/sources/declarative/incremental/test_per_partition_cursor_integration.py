65a8dc9dbb27e3646fb59c5dad9cc792d2579993
Alexandre Girard <alexandre@airbyte.io>
2024-02-28 16:26:23 -0800

    first pass at unit test

--- airbyte-cdk/python/unit_tests/sources/declarative/incremental/test_per_partition_cursor_integration.py
+++ airbyte-cdk/python/unit_tests/sources/declarative/incremental/test_per_partition_cursor_integration.py
@@ -9,6 +9,8 @@ from airbyte_cdk.sources.declarative.incremental.per_partition_cursor import Per
 from airbyte_cdk.sources.declarative.manifest_declarative_source import ManifestDeclarativeSource
 from airbyte_cdk.sources.declarative.retrievers.simple_retriever import SimpleRetriever
 from airbyte_cdk.sources.declarative.types import Record
+from airbyte_cdk.logger import init_logger
+from airbyte_cdk.models import AirbyteMessage, ConfiguredAirbyteCatalog, Type
 
 CURSOR_FIELD = "cursor_field"
 SYNC_MODE = SyncMode.incremental
@@ -18,12 +20,28 @@ class ManifestBuilder:
     def __init__(self):
         self._incremental_sync = None
         self._partition_router = None
+        self._substream_partition_router = None
 
     def with_list_partition_router(self, cursor_field, partitions):
         self._partition_router = {
             "type": "ListPartitionRouter",
             "cursor_field": cursor_field,
             "values": partitions,
+        }
+        return self
+
+    def with_substream_partition_router(self):
+        self._substream_partition_router = {
+            "type": "SubstreamPartitionRouter",
+            "parent_stream_configs": [
+                {
+                    "type": "ParentStreamConfig",
+                    "stream": "#/definitions/Rates",
+                    "parent_key": "id",
+                    "partition_field": "parent_id",
+
+                }
+            ]
         }
         return self
 
@@ -44,8 +62,27 @@ class ManifestBuilder:
             "version": "0.34.2",
             "type": "DeclarativeSource",
             "check": {"type": "CheckStream", "stream_names": ["Rates"]},
-            "streams": [
-                {
+            "definitions": {
+                "AnotherStream": {
+                    "type": "DeclarativeStream",
+                    "name": "AnotherStream",
+                    "primary_key": [],
+                    "schema_loader": {
+                        "type": "InlineSchemaLoader",
+                        "schema": {"$schema": "http://json-schema.org/schema#", "properties": {"id": {"type": "string"}}, "type": "object"},
+                    },
+                    "retriever": {
+                        "type": "SimpleRetriever",
+                        "requester": {
+                            "type": "HttpRequester",
+                            "url_base": "https://api.apilayer.com",
+                            "path": "/exchangerates_data/latest",
+                            "http_method": "GET",
+                        },
+                        "record_selector": {"type": "RecordSelector", "extractor": {"type": "DpathExtractor", "field_path": []}},
+                    },
+                },
+                "Rates": {
                     "type": "DeclarativeStream",
                     "name": "Rates",
                     "primary_key": [],
@@ -63,7 +100,11 @@ class ManifestBuilder:
                         },
                         "record_selector": {"type": "RecordSelector", "extractor": {"type": "DpathExtractor", "field_path": []}},
                     },
-                }
+                },
+            },
+            "streams": [
+                {"$ref": "#/definitions/Rates"},
+                {"$ref": "#/definitions/AnotherStream"}
             ],
             "spec": {
                 "connection_specification": {
@@ -78,9 +119,12 @@ class ManifestBuilder:
             },
         }
         if self._incremental_sync:
-            manifest["streams"][0]["incremental_sync"] = self._incremental_sync
+            manifest["definitions"]["Rates"]["incremental_sync"] = self._incremental_sync
+            manifest["definitions"]["AnotherStream"]["incremental_sync"] = self._incremental_sync
         if self._partition_router:
-            manifest["streams"][0]["retriever"]["partition_router"] = self._partition_router
+            manifest["definitions"]["Rates"]["retriever"]["partition_router"] = self._partition_router
+        if self._substream_partition_router:
+            manifest["definitions"]["AnotherStream"]["retriever"]["partition_router"] = self._substream_partition_router
         return manifest
 
 
@@ -137,9 +181,11 @@ def test_given_record_for_partition_when_read_then_update_state():
     stream_instance = source.streams({})[0]
     list(stream_instance.stream_slices(sync_mode=SYNC_MODE))
 
-    stream_slice = PerPartitionStreamSlice(partition={"partition_field": "1"}, cursor_slice={"start_time": "2022-01-01", "end_time": "2022-01-31"})
+    stream_slice = PerPartitionStreamSlice(partition={"partition_field": "1"},
+                                           cursor_slice={"start_time": "2022-01-01", "end_time": "2022-01-31"})
     with patch.object(
-        SimpleRetriever, "_read_pages", side_effect=[[Record({"a record key": "a record value", CURSOR_FIELD: "2022-01-15"}, stream_slice)]]
+            SimpleRetriever, "_read_pages",
+            side_effect=[[Record({"a record key": "a record value", CURSOR_FIELD: "2022-01-15"}, stream_slice)]]
     ):
         list(
             stream_instance.read_records(
@@ -158,3 +204,102 @@ def test_given_record_for_partition_when_read_then_update_state():
             }
         ]
     }
+
+
+def test_substream_without_input_state():
+    source = ManifestDeclarativeSource(
+        source_config=ManifestBuilder()
+        .with_substream_partition_router()
+        .with_incremental_sync(
+            start_datetime="2022-01-01",
+            end_datetime="2022-02-28",
+            datetime_format="%Y-%m-%d",
+            cursor_field=CURSOR_FIELD,
+            step="P1M",
+            cursor_granularity="P1D",
+        )
+        .build()
+    )
+
+    stream_instance = source.streams({})[1]
+
+    stream_slice = PerPartitionStreamSlice(partition={"parent_id": "1"},
+                                           cursor_slice={"start_time": "2022-01-01", "end_time": "2022-01-31"})
+
+    with patch.object(
+            SimpleRetriever, "_read_pages", side_effect=[[Record({"id": "1", CURSOR_FIELD: "2022-01-15"}, stream_slice)],
+                                                         Record({"id": "2", CURSOR_FIELD: "2022-01-15"}, stream_slice)]
+    ):
+        slices = list(stream_instance.stream_slices(sync_mode=SYNC_MODE))
+        assert list(slices) == [
+            PerPartitionStreamSlice(partition={"parent_id": "1", "parent_slice": {}}, cursor_slice={}),
+        ]
+
+
+def test_substream_with_legacy_input_state():
+    source = ManifestDeclarativeSource(
+        source_config=ManifestBuilder()
+        .with_substream_partition_router()
+        .with_incremental_sync(
+            start_datetime="2022-01-01",
+            end_datetime="2022-02-28",
+            datetime_format="%Y-%m-%d",
+            cursor_field=CURSOR_FIELD,
+            step="P1M",
+            cursor_granularity="P1D",
+        )
+        .build()
+    )
+
+    stream_instance = source.streams({})[1]
+
+    input_state = {
+        "states": [
+            {
+                "partition": {"item_id": "an_item_id",
+                              "parent_slice": {"end_time": "1629640663", "start_time": "1626962264"},
+                              },
+                "cursor": {
+                    "updated_at": "1709058818"
+                }
+            }
+        ]
+    }
+    stream_instance.state = input_state
+
+    stream_slice = PerPartitionStreamSlice(partition={"parent_id": "1"},
+                                           cursor_slice={"start_time": "2022-01-01", "end_time": "2022-01-31"})
+
+    logger = init_logger("airbyte")
+    configured_catalog = ConfiguredAirbyteCatalog(
+        streams=[
+            {
+                "stream": {"name": "AnotherStream", "json_schema": {}, "supported_sync_modes": ["incremental"]},
+                "sync_mode": "incremental",
+                "destination_sync_mode": "overwrite",
+            },
+        ]
+    )
+
+    with patch.object(
+            SimpleRetriever, "_read_pages", side_effect=[
+                [Record({"id": "1", CURSOR_FIELD: "2022-01-15"}, stream_slice)],
+                [Record({"parent_id": "1"}, stream_slice)],
+                [Record({"id": "2", CURSOR_FIELD: "2022-01-15"}, stream_slice)],
+                [Record({"parent_id": "2", CURSOR_FIELD: "2022-01-15"}, stream_slice)]
+            ]
+    ):
+        messages = list(source.read(logger, {}, configured_catalog, input_state))
+
+        output_state_message = [message for message in messages if message.type == Type.STATE][0]
+
+        expected_state = {"states": [
+            {
+                "cursor": {
+                    "cursor_field": "2022-01-31"
+                },
+                "partition": {"parent_id": "1", "parent_slice": {}}
+            }
+        ]}
+
+        assert output_state_message.state.data["AnotherStream"] == expected_state
