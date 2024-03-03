66c774566779728ceddba1d862584343e3fa7d67
Alex Birdsall <ambirdsall@gmail.com>
2024-03-03 14:49:39 -0800

    Delete python connector, support files, & scripts

--- airbyte-integrations/connectors/source-zendesk-talk/source_zendesk_talk/run.py
+++ airbyte-integrations/connectors/source-zendesk-talk/source_zendesk_talk/run.py
@@ -7,14 +7,8 @@ import sys
 
 from airbyte_cdk.entrypoint import launch
 from source_zendesk_talk import SourceZendeskTalk
-from source_zendesk_talk.source import SourceZendeskTalkTwo
 
 
 def run():
     source = SourceZendeskTalk()
-    launch(source, sys.argv[1:])
-
-
-def walk():
-    source = SourceZendeskTalkTwo()
     launch(source, sys.argv[1:])
