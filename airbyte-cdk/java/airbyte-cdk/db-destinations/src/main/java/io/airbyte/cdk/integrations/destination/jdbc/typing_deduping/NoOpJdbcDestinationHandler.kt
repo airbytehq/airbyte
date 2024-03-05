dbcc23f7e777a9bae52330ffa2f35359f802dcd4
Joe Bell <jbfbell@gmail.com>
2024-03-05 14:03:09 -0800

    formatting

--- airbyte-cdk/java/airbyte-cdk/db-destinations/src/main/java/io/airbyte/cdk/integrations/destination/jdbc/typing_deduping/NoOpJdbcDestinationHandler.kt
+++ airbyte-cdk/java/airbyte-cdk/db-destinations/src/main/java/io/airbyte/cdk/integrations/destination/jdbc/typing_deduping/NoOpJdbcDestinationHandler.kt
@@ -1,3 +1,7 @@
+/*
+ * Copyright (c) 2024 Airbyte, Inc., all rights reserved.
+ */
+
 package io.airbyte.cdk.integrations.destination.jdbc.typing_deduping
 
 import io.airbyte.cdk.db.jdbc.JdbcDatabase
@@ -7,14 +11,15 @@ import io.airbyte.integrations.base.destination.typing_deduping.DestinationIniti
 import io.airbyte.integrations.base.destination.typing_deduping.Sql
 import io.airbyte.integrations.base.destination.typing_deduping.StreamConfig
 
-class NoOpJdbcDestinationHandler(databaseName: String,
-                                 jdbcDatabase: JdbcDatabase
-): JdbcDestinationHandler(databaseName, jdbcDatabase) {
+class NoOpJdbcDestinationHandler(databaseName: String, jdbcDatabase: JdbcDatabase) :
+    JdbcDestinationHandler(databaseName, jdbcDatabase) {
     override fun execute(sql: Sql?) {
         throw NotImplementedError("This JDBC Destination Handler does not support typing deduping")
     }
 
-    override fun gatherInitialState(streamConfigs: MutableList<StreamConfig>?): MutableList<DestinationInitialState> {
+    override fun gatherInitialState(
+        streamConfigs: MutableList<StreamConfig>?
+    ): MutableList<DestinationInitialState> {
         throw NotImplementedError("This JDBC Destination Handler does not support typing deduping")
     }
 
