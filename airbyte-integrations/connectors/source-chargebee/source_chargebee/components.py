d241e107b0a65789f5f0173949b44e813c24226e
Patrick Nilan <nilan.patrick@gmail.com>
2024-03-05 08:17:13 -0800

    Remove erroneous import statements

--- airbyte-integrations/connectors/source-chargebee/source_chargebee/components.py
+++ airbyte-integrations/connectors/source-chargebee/source_chargebee/components.py
@@ -2,20 +2,14 @@
 # Copyright (c) 2023 Airbyte, Inc., all rights reserved.
 #
 
-import os
 from dataclasses import InitVar, dataclass
 from typing import Any, Iterable, Mapping, Optional, Union
 
-import requests
 from airbyte_cdk.sources.declarative.incremental.cursor import Cursor
 from airbyte_cdk.sources.declarative.interpolation.interpolated_string import InterpolatedString
-from airbyte_cdk.sources.declarative.requesters.error_handlers.backoff_strategies import WaitTimeFromHeaderBackoffStrategy
 from airbyte_cdk.sources.declarative.requesters.request_option import RequestOptionType
 from airbyte_cdk.sources.declarative.transformations.transformation import RecordTransformation
 from airbyte_cdk.sources.declarative.types import Config, Record, StreamSlice, StreamState
-
-IS_TESTING = os.environ.get("DEPLOYMENT_MODE") == "testing"
-
 
 @dataclass
 class CustomFieldTransformation(RecordTransformation):
