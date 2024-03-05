a0c32dd9e6df98ba8fdec6da2394022d883891cf
Alexandre Girard <alexandre@airbyte.io>
2024-03-05 13:50:11 -0800

    never build the java base image

--- tools/integrations/manage.sh
+++ tools/integrations/manage.sh
@@ -235,9 +235,6 @@ cmd_publish() {
     do
       echo "building base images for $arch"
       docker buildx build -t airbyte/integration-base:dev --platform $arch --load airbyte-integrations/bases/base
-      if [ "$path" != "airbyte-cdk/python" ]; then
-        docker buildx build -t airbyte/integration-base-java:dev --platform $arch --load airbyte-integrations/bases/base-java
-      fi
 
       # For a short while (https://github.com/airbytehq/airbyte/pull/25034), destinations rely on the normalization image to build
       # Thanks to gradle, destinstaions which need normalization will already have built base-normalization's "build" artifacts
