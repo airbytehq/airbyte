4a6172087ca09f32e3b342fda868257ce8437a5f
Oleksandr Bazarnov <oleksandr.bazarnov@globallogic.com>
2024-03-07 12:37:31 +0200

    simplified the next_page_poken logic for custom components

--- airbyte-integrations/connectors/source-zendesk-chat/source_zendesk_chat/components/id_offset_pagination.py
+++ airbyte-integrations/connectors/source-zendesk-chat/source_zendesk_chat/components/id_offset_pagination.py
@@ -30,13 +30,32 @@ class ZendeskChatIdOffsetIncrementPaginationStrategy(OffsetIncrement):
             self._id_field = InterpolatedString.create(self.id_field, parameters=parameters).eval(self.config)
         super().__post_init__(parameters=parameters, **kwargs)
 
+    def should_stop_pagination(self, decoded_response: Mapping[str, Any], last_records: List[Mapping[str, Any]]) -> bool:
+        """
+        Stop paginating when there are fewer records than the page size or the current page has no records
+        """
+        last_records_len = len(last_records)
+        no_records = last_records_len == 0
+        current_page_len = self._page_size.eval(self.config, response=decoded_response)
+        return (self._page_size and last_records_len < current_page_len) or no_records
+
+    def get_next_page_token_offset(self, last_records: List[Mapping[str, Any]]) -> int:
+        """
+        The `IDs` are returned in `ASC` order, we add `+1` to the ID integer value to avoid the record duplicates,
+        Described in: https://developer.zendesk.com/api-reference/live-chat/chat-api/agents/#pagination
+
+        Arguments:
+            last_records: List[Records] -- decoded from the RESPONSE.
+
+        Returns:
+            The offset value as the `next_page_token`
+        """
+        self._offset = last_records[-1][self._id_field]
+        return self._offset + 1
+
     def next_page_token(self, response: requests.Response, last_records: List[Mapping[str, Any]]) -> Optional[Any]:
         decoded_response = self.decoder.decode(response)
-        # Stop paginating when there are fewer records than the page size or the current page has no records
-        if (self._page_size and len(last_records) < self._page_size.eval(self.config, response=decoded_response)) or len(last_records) == 0:
+        if self.should_stop_pagination(decoded_response, last_records):
             return None
         else:
-            # the `IDs` are returned in `ASC` order, we add `+1` to the ID integer value to avoid the record duplicates,
-            # as described in: https://developer.zendesk.com/api-reference/live-chat/chat-api/agents/#pagination
-            self._offset = last_records[-1][self._id_field]
-            return self._offset + 1
+            return self.get_next_page_token_offset(last_records)
--- airbyte-integrations/connectors/source-zendesk-chat/source_zendesk_chat/components/time_offset_pagination.py
+++ airbyte-integrations/connectors/source-zendesk-chat/source_zendesk_chat/components/time_offset_pagination.py
@@ -30,13 +30,32 @@ class ZendeskChatTimeOffsetIncrementPaginationStrategy(OffsetIncrement):
             self._time_field_name = InterpolatedString.create(self.time_field_name, parameters=parameters).eval(self.config)
         super().__post_init__(parameters=parameters, **kwargs)
 
+    def should_stop_pagination(self, decoded_response: Mapping[str, Any], last_records: List[Mapping[str, Any]]) -> bool:
+        """
+        Stop paginating when there are fewer records than the page size or the current page has no records
+        """
+        last_records_len = len(last_records)
+        no_records = last_records_len == 0
+        current_page_len = self._page_size.eval(self.config, response=decoded_response)
+        return (self._page_size and last_records_len < current_page_len) or no_records
+
+    def get_next_page_token_offset(self, decoded_response: Mapping[str, Any]) -> int:
+        """
+        The `records` are returned in `ASC` order.
+        Described in: https://developer.zendesk.com/api-reference/live-chat/chat-api/incremental_export/#incremental-agent-timeline-export
+
+        Arguments:
+            decoded_response: Mapping[str, Any] -- The object with RECORDS decoded from the RESPONSE.
+
+        Returns:
+            The offset value as the `next_page_token`
+        """
+        self._offset = decoded_response[self._time_field_name]
+        return self._offset
+
     def next_page_token(self, response: requests.Response, last_records: List[Mapping[str, Any]]) -> Optional[Any]:
         decoded_response = self.decoder.decode(response)
-        # Stop paginating when there are fewer records than the page size or the current page has no records
-        if (self._page_size and len(last_records) < self._page_size.eval(self.config, response=decoded_response)) or len(last_records) == 0:
+        if self.should_stop_pagination(decoded_response, last_records):
             return None
         else:
-            # the `records` are returned in `ASC` order,
-            # as described in: https://developer.zendesk.com/api-reference/live-chat/chat-api/incremental_export/#incremental-agent-timeline-export
-            self._offset = decoded_response[self._time_field_name]
-            return self._offset
+            return self.get_next_page_token_offset(decoded_response)
