29bab8a62c8694fbc7931b250120d88caad89742
erohmensing <erohmensing@gmail.com>
2024-03-08 13:10:12 -0600

    add tests and update util logic

--- airbyte-cdk/python/airbyte_cdk/utils/message_utils.py
+++ airbyte-cdk/python/airbyte_cdk/utils/message_utils.py
@@ -1,13 +1,13 @@
 # Copyright (c) 2024 Airbyte, Inc., all rights reserved.
 
 from airbyte_cdk.sources.connector_state_manager import HashableStreamDescriptor
-from airbyte_protocol.models import AirbyteMessage
+from airbyte_protocol.models import AirbyteMessage, Type
 
 
 def get_stream_descriptor(message: AirbyteMessage) -> HashableStreamDescriptor:
-    if message.record:
+    if message.type == Type.RECORD:
         return HashableStreamDescriptor(name=message.record.stream, namespace=message.record.namespace)
-    elif message.state:
+    elif message.type == Type.STATE:
         return HashableStreamDescriptor(
             name=message.state.stream.stream_descriptor.name, namespace=message.state.stream.stream_descriptor.namespace
         )
