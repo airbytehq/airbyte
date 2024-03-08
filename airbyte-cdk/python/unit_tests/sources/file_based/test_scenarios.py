2b761a93d43917cabef0939419c6e9d6d185e59d
erohmensing <erohmensing@gmail.com>
2024-03-08 00:05:18 -0600

    clean up verify record counts

--- airbyte-cdk/python/unit_tests/sources/file_based/test_scenarios.py
+++ airbyte-cdk/python/unit_tests/sources/file_based/test_scenarios.py
@@ -71,7 +71,7 @@ def assert_exception(expected_exception: type[BaseException], output: Entrypoint
 
 
 def _verify_read_output(output: EntrypointOutput, scenario: TestScenario[AbstractSource]) -> None:
-    records, log_messages = output.records_and_state_messages, output.logs
+    records_and_state_messages, log_messages = output.records_and_state_messages, output.logs
     logs = [message.log for message in log_messages if message.log.level.value in scenario.log_levels]
     if scenario.expected_records is None:
         return
@@ -85,7 +85,7 @@ def _verify_read_output(output: EntrypointOutput, scenario: TestScenario[Abstrac
         ),
     )
     sorted_records = sorted(
-        filter(lambda r: r.record, records),
+        filter(lambda r: r.record, records_and_state_messages),
         key=lambda record: ",".join(
             f"{k}={v}" for k, v in sorted(record.record.data.items(), key=lambda items: (items[0], items[1])) if k != "emitted_at"
         ),
@@ -104,8 +104,9 @@ def _verify_read_output(output: EntrypointOutput, scenario: TestScenario[Abstrac
             assert actual.record.stream == expected["stream"]
 
     expected_states = list(filter(lambda e: "data" not in e, expected_records))
-    states = list(filter(lambda r: r.state, records))
+    states = list(filter(lambda r: r.state, records_and_state_messages))
     assert len(states) > 0, "No state messages emitted. Successful syncs should emit at least one stream state."
+    _verify_state_record_counts(sorted_records, states)
 
     if hasattr(scenario.source, "cursor_cls") and issubclass(scenario.source.cursor_cls, AbstractConcurrentFileBasedCursor):
         # Only check the last state emitted because we don't know the order the others will be in.
@@ -114,13 +115,6 @@ def _verify_read_output(output: EntrypointOutput, scenario: TestScenario[Abstrac
     else:
         for actual, expected in zip(states, expected_states):  # states should be emitted in sorted order
             assert actual.state.stream.stream_state.dict() == expected
-
-    # TODO: probably split this up per stream
-    for stream in set([record.record.stream for record in sorted_records]):
-        # Deal with namespaces here for the comparison
-        total_state_message_record_count_for_stream = sum([state_message.state.sourceStats.recordCount for state_message in states if state_message.state.stream.stream_descriptor.name == stream])
-        num_records_for_stream = len([record for record in sorted_records if record.record.stream == stream])
-        assert total_state_message_record_count_for_stream == num_records_for_stream
 
     if scenario.expected_logs:
         read_logs = scenario.expected_logs.get("read")
@@ -133,9 +127,31 @@ def _verify_read_output(output: EntrypointOutput, scenario: TestScenario[Abstrac
         _verify_analytics(analytics, scenario.expected_analytics)
 
 
+def _verify_state_record_counts(records: List[AirbyteMessage], states: List[AirbyteMessage]) -> None:
+    # TODO: Use HashableStreamDescriptor here
+    record_streams = {record.record.stream for record in records}
+    actual_record_counts = {stream: 0 for stream in record_streams}
+    for record in records:
+        actual_record_counts[record.record.stream] += 1
+
+    state_message_streams = {state.state.stream.stream_descriptor.name for state in states}
+    state_record_count_sums = {stream: 0 for stream in state_message_streams}
+    for state in states:
+        state_record_count_sums[state.state.stream.stream_descriptor.name] += state.state.sourceStats.recordCount
+
+    for stream, actual_count in actual_record_counts.items():
+        assert state_record_count_sums.get(stream) == actual_count
+
+    # We can have extra keys in state_record_count_sums if we processed a stream and reported 0 records
+    extra_keys = state_record_count_sums.keys() - actual_record_counts.keys()
+    for stream in extra_keys:
+        assert state_record_count_sums[stream] == 0
+
+
 def _verify_analytics(analytics: List[AirbyteMessage], expected_analytics: Optional[List[AirbyteAnalyticsTraceMessage]]) -> None:
     if expected_analytics:
-        assert len(analytics) == len(expected_analytics), \
+        assert len(analytics) == len(
+            expected_analytics), \
             f"Number of actual analytics messages ({len(analytics)}) did not match expected ({len(expected_analytics)})"
         for actual, expected in zip(analytics, expected_analytics):
             actual_type, actual_value = actual.trace.analytics.type, actual.trace.analytics.value
