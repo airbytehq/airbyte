ac775b4cd203df80c60f3c5648d57c9c4e631e82
jdpgrailsdev <jpearlin1@gmail.com>
2024-03-08 16:17:22 -0500

    feat(cdk): Convert async framework to Kotlin

---
+++ airbyte-cdk/java/airbyte-cdk/core/src/test/kotlin/io/airbyte/cdk/integrations/destnation/async/GlobalMemoryManagerTest.kt
@@ -0,0 +1,27 @@
+/*
+ * Copyright (c) 2024 Airbyte, Inc., all rights reserved.
+ */
+
+package io.airbyte.cdk.integrations.destnation.async
+
+import io.airbyte.cdk.integrations.destination_async.GlobalMemoryManager
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+
+class GlobalMemoryManagerTest {
+    private val BYTES_MB = (1024 * 1024).toLong()
+
+    @Test
+    internal fun test() {
+        val mgr = GlobalMemoryManager(15 * BYTES_MB)
+
+        Assertions.assertEquals(10 * BYTES_MB, mgr.requestMemory())
+        Assertions.assertEquals(5 * BYTES_MB, mgr.requestMemory())
+        Assertions.assertEquals(0, mgr.requestMemory())
+
+        mgr.free(10 * BYTES_MB)
+        Assertions.assertEquals(10 * BYTES_MB, mgr.requestMemory())
+        mgr.free(16 * BYTES_MB)
+        Assertions.assertEquals(10 * BYTES_MB, mgr.requestMemory())
+    }
+}
