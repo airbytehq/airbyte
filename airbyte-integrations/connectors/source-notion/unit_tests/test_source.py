0e3db7751a7eb6527e8d26127a3ce65e7ceaa0a8
Christo <christo.grab@gmail.com>
2024-03-10 20:02:09 -0400

    implement SelectiveAuthenticator

--- airbyte-integrations/connectors/source-notion/unit_tests/test_source.py
+++ airbyte-integrations/connectors/source-notion/unit_tests/test_source.py
@@ -2,14 +2,11 @@
 # Copyright (c) 2023 Airbyte, Inc., all rights reserved.
 #
 
-from unittest.mock import MagicMock
-
-import pytest
 from source_notion.source import SourceNotion
 
-def test_streams(mocker):
+def test_streams():
     source = SourceNotion()
-    config_mock = MagicMock()
+    config_mock = {"start_date": "2020-01-01T00:00:00.000Z", "credentials": {"auth_type": "token", "token": "banana"}}
     streams = source.streams(config_mock)
     expected_streams_number = 5
     assert len(streams) == expected_streams_number
