3a905b8787a9d7764ea13ac5b1712713efc29edc
stamplevskiyd <stamplevskiyd@gmail.com>
2024-03-11 14:43:41 +0300

    Timestamp fix

--- airbyte-integrations/connectors/source-cian/source_cian/source.py
+++ airbyte-integrations/connectors/source-cian/source_cian/source.py
@@ -18,10 +18,7 @@ class CianStream(HttpStream, ABC):
         super().__init__(authenticator)
 
     def request_params(
-            self,
-            stream_state: Mapping[str, Any],
-            stream_slice: Mapping[str, any] = None,
-            next_page_token: Mapping[str, Any] = None
+        self, stream_state: Mapping[str, Any], stream_slice: Mapping[str, any] = None, next_page_token: Mapping[str, Any] = None
     ) -> MutableMapping[str, Any]:
         return {}
 
@@ -35,7 +32,9 @@ class CianStream(HttpStream, ABC):
 class BuilderCalls(CianStream):
     primary_key = "id"
 
-    def __init__(self, authenticator: TokenAuthenticator, date_from: datetime.date, date_to: datetime.date, building_id: Optional[int] = None):
+    def __init__(
+        self, authenticator: TokenAuthenticator, date_from: datetime.date, date_to: datetime.date, building_id: Optional[int] = None
+    ):
         super().__init__(authenticator)
         self.date_from: datetime.date = date_from
         self.date_to: datetime.date = date_to
@@ -45,17 +44,15 @@ class BuilderCalls(CianStream):
         return "v1/get-builder-calls/"
 
     def stream_slices(
-            self, *, sync_mode: SyncMode, cursor_field: Optional[List[str]] = None, stream_state: Optional[Mapping[str, Any]] = None
+        self, *, sync_mode: SyncMode, cursor_field: Optional[List[str]] = None, stream_state: Optional[Mapping[str, Any]] = None
     ) -> Iterable[Optional[Mapping[str, Any]]]:
         """One slice for each request -> each day"""
-        for t_delta in range((self.date_to - self.date_from).days):
+        for t_delta in range((self.date_to - self.date_from).days + 1):
             dt: datetime = datetime(year=self.date_from.year, month=self.date_from.month, day=self.date_from.day) + timedelta(days=t_delta)
             yield {"load_date": dt.isoformat()}
 
     def request_params(self, stream_slice: Mapping[str, any] = None, *args, **kwargs) -> MutableMapping[str, Any]:
-        data: dict[str, any] = {
-            "onDate": stream_slice["load_date"]
-        }
+        data: dict[str, any] = {"onDate": stream_slice["load_date"]}
         if self.building_id is not None:
             data["newbuildingId"] = self.building_id
         return data
@@ -79,18 +76,14 @@ class SourceCian(AbstractSource):
         today_date: datetime.date = datetime.today()
         dt: datetime = datetime(year=today_date.year, month=today_date.month, day=today_date.day)
         try:
-            test_stream = BuilderCalls(
-                authenticator=self.get_auth(config),
-                date_from=today_date,
-                date_to=today_date
-            )
+            test_stream = BuilderCalls(authenticator=self.get_auth(config), date_from=today_date, date_to=today_date)
             next(test_stream.read_records(sync_mode=SyncMode.full_refresh, stream_slice={"load_date": dt.isoformat()}))
             return True, None
         except StopIteration:  # Just no data for today, but everything works just fine
             return True, None
         except Exception as ex:
             return False, ex
-    
+
     @staticmethod
     def transform_config_date_range(config: Mapping[str, Any]) -> Mapping[str, Any]:
         date_range: Mapping[str, Any] = config.get("date_range", {})
@@ -132,9 +125,11 @@ class SourceCian(AbstractSource):
     def streams(self, config: Mapping[str, Any]) -> List[Stream]:
         config = self.transform_config(config)
         auth = self.get_auth(config)
-        return [BuilderCalls(
-            authenticator=auth,
-            date_from=config["date_from_transformed"],
-            date_to=config["date_to_transformed"],
-            building_id=config["building_id"]
-        )]
+        return [
+            BuilderCalls(
+                authenticator=auth,
+                date_from=config["date_from_transformed"],
+                date_to=config["date_to_transformed"],
+                building_id=config["building_id"],
+            )
+        ]
