a55cb28df38290bb828d8a5208489c65a3d59788
Aaron Steers <aj@airbyte.io>
2024-03-12 21:46:33 -0700

    import library components

--- airbyte-ci/connectors/ci_credentials/ci_credentials/__init__.py
+++ airbyte-ci/connectors/ci_credentials/ci_credentials/__init__.py
@@ -1,6 +1,13 @@
 #
 # Copyright (c) 2023 Airbyte, Inc., all rights reserved.
 #
-from .secrets_manager import SecretsManager
+from .secrets_manager import SecretsManager, RemoteSecret
+from .library import get_secrets_manager, get_connector_secrets
 
-__all__ = ("SecretsManager",)
+
+__all__ = (
+    "get_connector_secrets",
+    "get_secrets_manager",
+    "RemoteSecret",
+    "SecretsManager",
+)
