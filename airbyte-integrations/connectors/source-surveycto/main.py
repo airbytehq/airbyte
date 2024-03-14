e2d5ff191e5ffc681042ba0d4a0938a2ca149cbf
wambui gitau <ywambui10@gmail.com>
2024-03-14 14:39:31 +0300

    QA is failing

--- airbyte-integrations/connectors/source-surveycto/main.py
+++ airbyte-integrations/connectors/source-surveycto/main.py
@@ -3,11 +3,7 @@
 #
 
 
-import sys
-
-from airbyte_cdk.entrypoint import launch
-from source_surveycto import SourceSurveycto
+from source_surveycto.run import run
 
 if __name__ == "__main__":
-    source = SourceSurveycto()
-    launch(source, sys.argv[1:])
+    run()
