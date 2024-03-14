850b015de4e6797c6f0131d239da20f399a321a5
wambui gitau <ywambui10@gmail.com>
2024-03-14 14:39:31 +0300

    QA is failing

--- airbyte-integrations/connectors/source-surveycto/setup.py
+++ airbyte-integrations/connectors/source-surveycto/setup.py
@@ -1,8 +1,6 @@
 #
 # Copyright (c) 2023 Airbyte, Inc., all rights reserved.
 #
-
-
 from setuptools import find_packages, setup
 
 MAIN_REQUIREMENTS = [
@@ -15,20 +13,33 @@ MAIN_REQUIREMENTS = [
     "botocore",
 ]
 
-TEST_REQUIREMENTS = [
-    "pytest~=6.1",
-    "pytest-mock~=3.6.1",
-    "connector-acceptance-test",
-]
+TEST_REQUIREMENTS = ["pytest~=6.1", "pytest-mock~=3.6.1"]
 
 setup(
+    entry_points={
+        "console_scripts": [
+            "source-surveycto=source_surveycto.run:run",
+        ],
+    },
     name="source_surveycto",
     description="Source implementation for Surveycto.",
     author="Airbyte",
     author_email="contact@airbyte.io",
     packages=find_packages(),
     install_requires=MAIN_REQUIREMENTS,
-    package_data={"": ["*.json", "*.yaml", "schemas/*.json", "schemas/shared/*.json"]},
+    package_data={
+        "": [
+            # Include yaml files in the package (if any)
+            "*.yml",
+            "*.yaml",
+            # Include all json files in the package, up to 4 levels deep
+            "*.json",
+            "*/*.json",
+            "*/*/*.json",
+            "*/*/*/*.json",
+            "*/*/*/*/*.json",
+        ]
+    },
     extras_require={
         "tests": TEST_REQUIREMENTS,
     },
