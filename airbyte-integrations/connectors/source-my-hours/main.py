6d770991bcf4d3b96c5788c163e208dc72bcbf7f
btkcodedev <btk.codedev@gmail.com>
2024-03-14 21:48:01 +0530

    Source My Hours: Migrate Python CDK to Low Code CDK

--- airbyte-integrations/connectors/source-my-hours/main.py
+++ airbyte-integrations/connectors/source-my-hours/main.py
@@ -2,7 +2,12 @@
 # Copyright (c) 2023 Airbyte, Inc., all rights reserved.
 #
 
-from source_my_hours.run import run
+
+import sys
+
+from airbyte_cdk.entrypoint import launch
+from source_my_hours import SourceMyHours
 
 if __name__ == "__main__":
-    run()
+    source = SourceMyHours()
+    launch(source, sys.argv[1:])
--- airbyte-integrations/connectors/source-my-hours/setup.py
+++ airbyte-integrations/connectors/source-my-hours/setup.py
@@ -6,41 +6,24 @@
 from setuptools import find_packages, setup
 
 MAIN_REQUIREMENTS = [
-    "airbyte-cdk",
+    "airbyte-cdk~=0.1",
 ]
 
 TEST_REQUIREMENTS = [
     "requests-mock~=1.9.3",
-    "pytest~=6.1",
+    "pytest~=6.2",
     "pytest-mock~=3.6.1",
-    "responses~=0.16.0",
+    "connector-acceptance-test",
 ]
 
 setup(
-    entry_points={
-        "console_scripts": [
-            "source-my-hours=source_my_hours.run:run",
-        ],
-    },
     name="source_my_hours",
     description="Source implementation for My Hours.",
     author="Wisse Jelgersma",
     author_email="wisse@vrowl.nl",
     packages=find_packages(),
     install_requires=MAIN_REQUIREMENTS,
-    package_data={
-        "": [
-            # Include yaml files in the package (if any)
-            "*.yml",
-            "*.yaml",
-            # Include all json files in the package, up to 4 levels deep
-            "*.json",
-            "*/*.json",
-            "*/*/*.json",
-            "*/*/*/*.json",
-            "*/*/*/*/*.json",
-        ]
-    },
+    package_data={"": ["*.json", "*.yaml", "schemas/*.json", "schemas/shared/*.json"]},
     extras_require={
         "tests": TEST_REQUIREMENTS,
     },
