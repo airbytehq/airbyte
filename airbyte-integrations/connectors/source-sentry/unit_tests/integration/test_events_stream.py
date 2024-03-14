c8c4ed508a61fad75c03d77df81bd13b557e5993
darynaishchenko <darina.ishchenko17@gmail.com>
2024-03-14 19:31:29 +0200

    moved response for integration tests to files

--- airbyte-integrations/connectors/source-sentry/unit_tests/integration/test_events_stream.py
+++ airbyte-integrations/connectors/source-sentry/unit_tests/integration/test_events_stream.py
@@ -6,6 +6,7 @@ from unittest import TestCase
 from airbyte_cdk.test.catalog_builder import CatalogBuilder
 from airbyte_cdk.test.entrypoint_wrapper import read
 from airbyte_cdk.test.mock_http import HttpMocker, HttpRequest, HttpResponse
+from airbyte_cdk.test.mock_http.response_builder import find_template
 from airbyte_cdk.test.state_builder import StateBuilder
 from airbyte_protocol.models import SyncMode
 from config_builder import ConfigBuilder
@@ -13,6 +14,9 @@ from source_sentry.source import SourceSentry
 
 
 class TestEvents(TestCase):
+    fr_read_file = "events_full_refresh"
+    inc_read_file = "events_incremental"
+
     def catalog(self, sync_mode: SyncMode = SyncMode.full_refresh):
         return CatalogBuilder().with_stream(name="events", sync_mode=sync_mode).build()
 
@@ -24,36 +28,12 @@ class TestEvents(TestCase):
 
     @HttpMocker()
     def test_read(self, http_mocker: HttpMocker):
-        response = [
-            {
-                "eventID": "9fac2ceed9344f2bbfdd1fdacb0ed9b1",
-                "tags": [
-                    {"key": "browser", "value": "Chrome 60.0"},
-                    {
-                        "key": "device", "value": "Other"},
-                    {"key": "environment", "value": "production"},
-                    {"value": "fatal", "key": "level"},
-                    {"key": "os", "value": "Mac OS X 10.12.6"},
-                    {"value": "CPython 2.7.16", "key": "runtime"},
-                    {"key": "release", "value": "17642328ead24b51867165985996d04b29310337"},
-                    {"key": "server_name", "value": "web1.example.com"}
-                ],
-                "dateCreated": "2022-09-02T15:01:28.946777Z",
-                "user": None,
-                "message": "",
-                "title": "This is an example Python exception",
-                "id": "dfb1a2d057194e76a4186cc8a5271553",
-                "platform": "python",
-                "event.type": "error",
-                "groupID": "1889724436"
-            }
-        ]
         http_mocker.get(
             HttpRequest(
                 url="https://sentry.io/api/0/projects/test%20organization/test%20project/events/",
                 query_params={"full": "true"}
             ),
-            HttpResponse(body=json.dumps(response), status_code=200)
+            HttpResponse(body=json.dumps(find_template(self.fr_read_file, __file__)), status_code=200)
 
         )
         output = read(SourceSentry(), self.config(), self.catalog())
@@ -61,58 +41,12 @@ class TestEvents(TestCase):
 
     @HttpMocker()
     def test_read_incremental(self, http_mocker: HttpMocker):
-        response = [
-            {
-                "eventID": "9fac2ceed9344f2bbfdd1fdacb0ed9b1",
-                "tags": [
-                    {"key": "browser", "value": "Chrome 60.0"},
-                    {
-                        "key": "device", "value": "Other"},
-                    {"key": "environment", "value": "production"},
-                    {"value": "fatal", "key": "level"},
-                    {"key": "os", "value": "Mac OS X 10.12.6"},
-                    {"value": "CPython 2.7.16", "key": "runtime"},
-                    {"key": "release", "value": "17642328ead24b51867165985996d04b29310337"},
-                    {"key": "server_name", "value": "web1.example.com"}
-                ],
-                "dateCreated": "2023-02-01T00:00:00.0Z",
-                "user": None,
-                "message": "",
-                "title": "This is an example Python exception",
-                "id": "dfb1a2d057194e76a4186cc8a5271553",
-                "platform": "python",
-                "event.type": "error",
-                "groupID": "1889724436"
-            },
-            {
-                "eventID": "9fac2ceed9344f2bbfdd1fdacb0ed9b1",
-                "tags": [
-                    {"key": "browser", "value": "Chrome 60.0"},
-                    {
-                        "key": "device", "value": "Other"},
-                    {"key": "environment", "value": "production"},
-                    {"value": "fatal", "key": "level"},
-                    {"key": "os", "value": "Mac OS X 10.12.6"},
-                    {"value": "CPython 2.7.16", "key": "runtime"},
-                    {"key": "release", "value": "17642328ead24b51867165985996d04b29310337"},
-                    {"key": "server_name", "value": "web1.example.com"}
-                ],
-                "dateCreated": "2024-01-02T15:01:28.946777Z",
-                "user": None,
-                "message": "",
-                "title": "This is an example Python exception",
-                "id": "dfb1a2d057194e76a4186cc8a5271553",
-                "platform": "python",
-                "event.type": "error",
-                "groupID": "1889724436"
-            }
-        ]
         http_mocker.get(
             HttpRequest(
                 url="https://sentry.io/api/0/projects/test%20organization/test%20project/events/",
                 query_params={"full": "true"}
             ),
-            HttpResponse(body=json.dumps(response), status_code=200)
+            HttpResponse(body=json.dumps(find_template(self.inc_read_file, __file__)), status_code=200)
 
         )
         output = read(SourceSentry(), self.config(), self.catalog(SyncMode.incremental), self.state())
--- airbyte-integrations/connectors/source-sentry/unit_tests/integration/test_issues_stream.py
+++ airbyte-integrations/connectors/source-sentry/unit_tests/integration/test_issues_stream.py
@@ -6,6 +6,7 @@ from unittest import TestCase
 from airbyte_cdk.test.catalog_builder import CatalogBuilder
 from airbyte_cdk.test.entrypoint_wrapper import read
 from airbyte_cdk.test.mock_http import HttpMocker, HttpRequest, HttpResponse
+from airbyte_cdk.test.mock_http.response_builder import find_template
 from airbyte_cdk.test.state_builder import StateBuilder
 from airbyte_protocol.models import SyncMode
 from config_builder import ConfigBuilder
@@ -13,6 +14,9 @@ from source_sentry.source import SourceSentry
 
 
 class TestEvents(TestCase):
+    fr_read_file = "issues_full_refresh"
+    inc_read_file = "issues_incremental"
+
     def catalog(self, sync_mode: SyncMode = SyncMode.full_refresh):
         return CatalogBuilder().with_stream(name="issues", sync_mode=sync_mode).build()
 
@@ -24,50 +28,12 @@ class TestEvents(TestCase):
 
     @HttpMocker()
     def test_read(self, http_mocker: HttpMocker):
-        response = [
-          {
-            "annotations": [],
-            "assignedTo": None,
-            "count": "1",
-            "culprit": "raven.scripts.runner in main",
-            "firstSeen": "2018-11-06T21:19:55Z",
-            "hasSeen": False,
-            "id": "1",
-            "isBookmarked": False,
-            "isPublic": False,
-            "isSubscribed": True,
-            "lastSeen": "2018-11-06T21:19:55Z",
-            "level": "error",
-            "logger": None,
-            "metadata": {
-              "title": "This is an example Python exception"
-            },
-            "numComments": 0,
-            "permalink": "https://sentry.io/the-interstellar-jurisdiction/pump-station/issues/1/",
-            "project": {
-              "id": "2",
-              "name": "Pump Station",
-              "slug": "pump-station"
-            },
-            "shareId": None,
-            "shortId": "PUMP-STATION-1",
-            "stats": {
-              "24h": [[1541455200, 473], [1541458800, 914], [1541462400, 991]]
-            },
-            "status": "unresolved",
-            "statusDetails": {},
-            "subscriptionDetails": None,
-            "title": "This is an example Python exception",
-            "type": "default",
-            "userCount": 0
-          }
-        ]
         http_mocker.get(
             HttpRequest(
                 url="https://sentry.io/api/0/projects/test%20organization/test%20project/issues/",
                 query_params={"query": "lastSeen:>1900-01-01T00:00:00.0Z"}
             ),
-            HttpResponse(body=json.dumps(response), status_code=200)
+            HttpResponse(body=json.dumps(find_template(self.fr_read_file, __file__)), status_code=200)
 
         )
         # https://sentry.io/api/1/projects/airbyte-09/airbyte-09/issues/?query=lastSeen%3A%3E2022-01-01T00%3A00%3A00.0Z
@@ -76,86 +42,12 @@ class TestEvents(TestCase):
 
     @HttpMocker()
     def test_read_incremental(self, http_mocker: HttpMocker):
-        response = [
-            {
-                "annotations": [],
-                "assignedTo": None,
-                "count": "1",
-                "culprit": "raven.scripts.runner in main",
-                "firstSeen": "2018-11-06T21:19:55Z",
-                "hasSeen": False,
-                "id": "1",
-                "isBookmarked": False,
-                "isPublic": False,
-                "isSubscribed": True,
-                "lastSeen": "2023-02-02T00:00:00.0Z",
-                "level": "error",
-                "logger": None,
-                "metadata": {
-                    "title": "This is an example Python exception"
-                },
-                "numComments": 0,
-                "permalink": "https://sentry.io/the-interstellar-jurisdiction/pump-station/issues/1/",
-                "project": {
-                    "id": "2",
-                    "name": "Pump Station",
-                    "slug": "pump-station"
-                },
-                "shareId": None,
-                "shortId": "PUMP-STATION-1",
-                "stats": {
-                    "24h": [[1541455200, 473], [1541458800, 914], [1541462400, 991]]
-                },
-                "status": "unresolved",
-                "statusDetails": {},
-                "subscriptionDetails": None,
-                "title": "This is an example Python exception",
-                "type": "default",
-                "userCount": 0
-            },
-            {
-                "annotations": [],
-                "assignedTo": None,
-                "count": "1",
-                "culprit": "raven.scripts.runner in main",
-                "firstSeen": "2018-11-06T21:19:55Z",
-                "hasSeen": False,
-                "id": "1",
-                "isBookmarked": False,
-                "isPublic": False,
-                "isSubscribed": True,
-                "lastSeen": "2023-01-02T00:00:00.0Z",
-                "level": "error",
-                "logger": None,
-                "metadata": {
-                    "title": "This is an example Python exception"
-                },
-                "numComments": 0,
-                "permalink": "https://sentry.io/the-interstellar-jurisdiction/pump-station/issues/1/",
-                "project": {
-                    "id": "2",
-                    "name": "Pump Station",
-                    "slug": "pump-station"
-                },
-                "shareId": None,
-                "shortId": "PUMP-STATION-1",
-                "stats": {
-                    "24h": [[1541455200, 473], [1541458800, 914], [1541462400, 991]]
-                },
-                "status": "unresolved",
-                "statusDetails": {},
-                "subscriptionDetails": None,
-                "title": "This is an example Python exception",
-                "type": "default",
-                "userCount": 0
-            }
-        ]
         http_mocker.get(
             HttpRequest(
                 url="https://sentry.io/api/0/projects/test%20organization/test%20project/issues/",
                 query_params={"query": "lastSeen:>2023-01-01T00:00:00.0Z"}
             ),
-            HttpResponse(body=json.dumps(response), status_code=200)
+            HttpResponse(body=json.dumps(find_template(self.inc_read_file, __file__)), status_code=200)
 
         )
         output = read(SourceSentry(), self.config(), self.catalog(SyncMode.incremental), self.state())
