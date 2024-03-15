db191f3cebcc0cdc78407b081ba10912d3936d5f
Stephane Geneix <stephane@airbyte.io>
2024-03-15 09:56:11 -0700

    convert core textFixtures to kotlin

---
+++ airbyte-cdk/java/airbyte-cdk/core/src/testFixtures/kotlin/io/airbyte/cdk/integrations/util/HostPortResolver.kt
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) 2023 Airbyte, Inc., all rights reserved.
+ */
+package io.airbyte.cdk.integrations.util
+
+import org.testcontainers.containers.GenericContainer
+import java.util.*
+
+object HostPortResolver {
+    @JvmStatic
+    fun resolveHost(container: GenericContainer<*>?): String? {
+        return getIpAddress(container)
+    }
+
+    @JvmStatic
+    fun resolvePort(container: GenericContainer<*>?): Int {
+        return container!!.exposedPorts.stream().findFirst().get()
+    }
+
+    fun resolveIpAddress(container: GenericContainer<*>?): String? {
+        return getIpAddress(container)
+    }
+
+    private fun getIpAddress(container: GenericContainer<*>?): String? {
+        return Objects.requireNonNull(container!!.containerInfo
+                .networkSettings
+                .networks
+                .entries.stream()
+                .findFirst()
+                .get().value.ipAddress)
+    }
+}
