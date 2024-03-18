ec70083f85bb88cf1827012faadacf2a5183638a
erohmensing <erohmensing@gmail.com>
2024-03-18 16:49:24 -0500

    pass in the changed files from ci

--- airbyte-cdk/python/bin/run-mypy-on-modified-files.sh
+++ airbyte-cdk/python/bin/run-mypy-on-modified-files.sh
@@ -5,9 +5,14 @@ set -e
 # Ensure script always runs from the project directory.
 cd "$(dirname "${0}")/.." || exit 1
 
-# TODO change this to include unit_tests as well once it's in a good state
-{
-  git diff --name-only --diff-filter=d --relative ':(exclude)unit_tests'
-  git diff --name-only --diff-filter=d --staged --relative ':(exclude)unit_tests'
-  git diff --name-only --diff-filter=d master... --relative ':(exclude)unit_tests'
-} | grep -E '\.py$' | sort | uniq | xargs mypy --config-file pyproject.toml --install-types --non-interactive
+if [ "$#" -gt 0 ]; then
+  # Usually CI: if files are provided as arguments, run mypy directly on those files.
+  mypy --config-file pyproject.toml -- "$@"
+else
+  # Usually local: if no files are provided, calculate the diff and run mypy on the result.
+  {
+    git diff --name-only --diff-filter=d --relative ':(exclude)unit_tests'
+    git diff --name-only --diff-filter=d --staged --relative ':(exclude)unit_tests'
+    git diff --name-only --diff-filter=d master... --relative ':(exclude)unit_tests'
+  } | grep -E '\.py$' | sort | uniq | xargs mypy --config-file pyproject.toml --install-types --non-interactive
+fi
