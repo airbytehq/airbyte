5edf6da9a6ea777d396e7ddaacaa2565e48fd543
Artem Inzhyyants <artem.inzhyyants@gmail.com>
2024-03-20 11:58:08 +0100

    Source SFTP BULK: update spec[skip ci]
    Signed-off-by: Artem Inzhyyants <artem.inzhyyants@gmail.com>

--- airbyte-integrations/connectors/source-sftp-bulk/source_sftp_bulk/spec.py
+++ airbyte-integrations/connectors/source-sftp-bulk/source_sftp_bulk/spec.py
@@ -8,15 +8,7 @@ from pydantic import Field
 
 class SourceSFTPBulkSpec(AbstractFileBasedSpec):
     class Config:
-        title = "Google Drive Source Spec"
-
-    folder_url: Optional[str] = Field(
-        description="URL for the folder you want to sync. Using individual streams and glob patterns, it's possible to only sync a subset of all files located in the folder.",
-        examples=["https://drive.google.com/drive/folders/1Xaz0vXXXX2enKnNYU5qSt9NS70gvMyYn"],
-        order=0,
-        pattern="^https://drive.google.com/.+",
-        pattern_descriptor="https://drive.google.com/drive/folders/MY-FOLDER-ID",
-    )
+        title = "SFTP Bulk Source Spec"
 
     username: str = Field(title="User Name", description="The server user", order=0)
 
@@ -24,6 +16,14 @@ class SourceSFTPBulkSpec(AbstractFileBasedSpec):
     private_key: Optional[str] = Field(title="Private key", description="The Private key", multiline=True, order=2)
     host: str = Field(title="Host Address", description="The server host address", examples=["www.host.com", "192.0.2.1"], order=3)
     port: int = Field(title="Host Address", description="The server port", default=22, examples=["22"], order=4)
+    folder_path: Optional[str] = Field(
+        title="Folder Path",
+        description="The directory to search files for sync",
+        examples=["/logs/2022"],
+        order=5,
+        default="",
+        pattern_descriptor="/folder_to_sync",
+    )
 
     @classmethod
     def documentation_url(cls) -> str:
