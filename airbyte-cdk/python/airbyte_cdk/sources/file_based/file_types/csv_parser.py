7d5ef055fd50d5ad6c85fe4bf269060e95b84ce3
Tobias Macey <tmacey@mit.edu>
2024-03-20 12:23:48 -0400

    Increase the maximum parseable field size for CSV files
    The Python CSV library defaults to a maximum allowed length of 128k for a given
    field. This can cause issues when loading files that contain fields exceeding that
    length. This updates the parser to register the maximum allowable field size based on
    the runtime system.

--- airbyte-cdk/python/airbyte_cdk/sources/file_based/file_types/csv_parser.py
+++ airbyte-cdk/python/airbyte_cdk/sources/file_based/file_types/csv_parser.py
@@ -132,20 +132,11 @@ class _CsvReader:
 class CsvParser(FileTypeParser):
     _MAX_BYTES_PER_FILE_FOR_SCHEMA_INFERENCE = 1_000_000
 
-    def __init__(self, csv_reader: Optional[_CsvReader] = None):
+    def __init__(self, csv_reader: Optional[_CsvReader] = None, csv_field_max_bytes: int = 2**31):
         # Increase the maximum length of data that can be parsed in a single CSV field. The default is 128k, which is typically sufficient
-        # but given the use of Airbyte in loading a large variety of data it is best to allow for the maximum field size possible to avoid
+        # but given the use of Airbyte in loading a large variety of data it is best to allow for a larger maximum field size to avoid
         # skipping data on load. https://stackoverflow.com/questions/15063936/csv-error-field-larger-than-field-limit-131072
-        max_int = sys.maxsize
-
-        while True:
-            # decrease the max_int value by factor 10 as long as the OverflowError occurs.
-            try:
-                csv.field_size_limit(max_int)
-                break
-            except OverflowError:
-                max_int = int(max_int/10)
-
+        csv.field_size_limit(csv_field_max_bytes)
         self._csv_reader = csv_reader if csv_reader else _CsvReader()
 
     def check_config(self, config: FileBasedStreamConfig) -> Tuple[bool, Optional[str]]:
