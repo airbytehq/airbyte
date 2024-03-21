17af3c33fcd4344f3ee2671f8e2b7294dacf6e11
aliaksandr <aliaksandr@voodoo.io>
2024-03-21 08:06:23 +0100

    Applovin metrics: retry on 500

--- airbyte-integrations/connectors/source-applovin/source_applovin/streams.py
+++ airbyte-integrations/connectors/source-applovin/source_applovin/streams.py
@@ -24,46 +24,44 @@ class ApplovinStream(HttpStream):
 
     def should_retry(self, response: requests.Response) -> bool:
         if response.status_code == 500:
-            logging.warning("Received error: " + str(response.status_code) + " " + response.text)
-            if "Execution time exceeded run time cap" in response.text:
-                return True
-            logging.warning("URL: " + str(response.url))
-            logging.warning("Status Code: " + str(response.status_code))
-            logging.warning("Reason: " + str(response.reason))
-            logging.warning("HTTP Version: " + str(response.raw.version))
+            print("Received error: " + str(response.status_code) + " " + response.text)
+            # logging.warning("URL: " + str(response.url))
+            # logging.warning("Status Code: " + str(response.status_code))
+            # logging.warning("Reason: " + str(response.reason))
+            # logging.warning("HTTP Version: " + str(response.raw.version))
+            #
+            # logging.warning("\n---- HEADERS ----")
+            # for key, value in response.headers.items():
+            #     logging.warning(f"{str(key)}: {str(value)}")
+            #
+            # logging.warning("\n---- COOKIES ----")
+            # for name, value in response.cookies.items():
+            #     logging.warning(f"{str(name)}: {str(value)}")
+            #
+            # logging.warning("\n---- CONTENT ----")
+            # logging.warning(response.text)
+            #
+            # logging.warning("\n---- REDIRECT HISTORY ----")
+            # for resp in response.history:
+            #     logging.warning(f"Redirected to {resp.url} with status code {resp.status_code}")
+            #
+            # logging.warning("\n---- REQUEST INFO ----")
+            # logging.warning("Request Method:" + str(response.request.method))
+            # logging.warning("Request URL:" + str(response.request.url))
+            # logging.warning("Request Headers:")
+            #
+            # for key, value in response.request.headers.items():
+            #     logging.warning(f"{str(key)}: {str(value)}")
+            #
+            # if response.request.body:
+            #     logging.warning("\nRequest Body:" + str(response.request.body))
+            #
+            # logging.warning("\n---- OTHER INFO ----")
+            # logging.warning("Elapsed Time:" + str(response.elapsed))
+            # logging.warning("Encoding:" + str(response.encoding))
+            # logging.warning("Content Length:" + str(len(response.content)))
 
-            logging.warning("\n---- HEADERS ----")
-            for key, value in response.headers.items():
-                logging.warning(f"{str(key)}: {str(value)}")
-
-            logging.warning("\n---- COOKIES ----")
-            for name, value in response.cookies.items():
-                logging.warning(f"{str(name)}: {str(value)}")
-
-            logging.warning("\n---- CONTENT ----")
-            logging.warning(response.text)
-
-            logging.warning("\n---- REDIRECT HISTORY ----")
-            for resp in response.history:
-                logging.warning(f"Redirected to {resp.url} with status code {resp.status_code}")
-
-            logging.warning("\n---- REQUEST INFO ----")
-            logging.warning("Request Method:" + str(response.request.method))
-            logging.warning("Request URL:" + str(response.request.url))
-            logging.warning("Request Headers:")
-
-            for key, value in response.request.headers.items():
-                logging.warning(f"{str(key)}: {str(value)}")
-
-            if response.request.body:
-                logging.warning("\nRequest Body:" + str(response.request.body))
-
-            logging.warning("\n---- OTHER INFO ----")
-            logging.warning("Elapsed Time:" + str(response.elapsed))
-            logging.warning("Encoding:" + str(response.encoding))
-            logging.warning("Content Length:" + str(len(response.content)))
-
-            return False
+            return True
         if response.status_code == 429 or 501 <= response.status_code < 600:
             return True
         else:
