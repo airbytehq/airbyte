dfd3eacdf57ca31345a4e5ae8b52691384c71b20
darynaishchenko <darina.ishchenko17@gmail.com>
2024-03-21 13:25:33 +0200

    moved join channels logic to custom retriever for channels stream

--- airbyte-integrations/connectors/source-slack/source_slack/components/join_channels.py
+++ airbyte-integrations/connectors/source-slack/source_slack/components/join_channels.py
@@ -1,13 +1,15 @@
 # Copyright (c) 2023 Airbyte, Inc., all rights reserved.
 
 import logging
-from dataclasses import dataclass
+from functools import partial
 from typing import Any, Iterable, List, Mapping, Optional
 
 import requests
 from airbyte_cdk.models import SyncMode
-from airbyte_cdk.sources.declarative.transformations import RecordTransformation
-from airbyte_cdk.sources.declarative.types import Config, Record, StreamSlice, StreamState
+from airbyte_cdk.sources.declarative.partition_routers import SinglePartitionRouter
+from airbyte_cdk.sources.declarative.retrievers import SimpleRetriever
+from airbyte_cdk.sources.declarative.types import Record, StreamSlice
+from airbyte_cdk.sources.streams.core import StreamData
 from airbyte_cdk.sources.streams.http import HttpStream
 from airbyte_cdk.sources.streams.http.auth import TokenAuthenticator
 
@@ -36,8 +38,12 @@ class JoinChannelsStream(HttpStream):
         Override to simply indicate that the specific channel was joined successfully.
         This method should not return any data, but should return an empty iterable.
         """
-        self.logger.info(f"Successfully joined channel: {stream_slice['channel_name']}")
-        yield response.json()["channel"]
+        is_ok = response.json()["ok"]
+        if is_ok:
+            self.logger.info(f"Successfully joined channel: {stream_slice['channel_name']}")
+        else:
+            self.logger.info(f"Unable to joined channel: {stream_slice['channel_name']}. Reason: {response.json()}")
+        return []
 
     def request_body_json(self, stream_slice: Mapping = None, **kwargs) -> Optional[Mapping]:
         return {"channel": stream_slice["channel"]}
@@ -49,12 +55,11 @@ class JoinChannelsStream(HttpStream):
         return None
 
 
-@dataclass
-class JoinChannels(RecordTransformation):
-    """
-    Make 'conversations.join' POST request for every found channel id
-    if we are not still a member of such channel
-    """
+class ChannelsRetriever(SimpleRetriever):
+    def __post_init__(self, parameters: Mapping[str, Any]):
+        super().__post_init__(parameters)
+        self.stream_slicer = SinglePartitionRouter(parameters={})
+        self.record_selector.transformations = []
 
     def should_join_to_channel(self, config: Mapping[str, Any], record: Record) -> bool:
         """
@@ -75,19 +80,43 @@ class JoinChannels(RecordTransformation):
         channel_filter = config["channel_filter"]
         return JoinChannelsStream(authenticator=authenticator, channel_filter=channel_filter)
 
-    def transform(
-        self,
-        record: Record,
-        config: Optional[Config] = None,
-        stream_state: Optional[StreamState] = None,
-        stream_slice: Optional[StreamSlice] = None,
-    ) -> Mapping[str, Any]:
-        if self.should_join_to_channel(config, record):
-            channel = list(
-                self.join_channels_stream(config).read_records(
-                    sync_mode=SyncMode.full_refresh,
-                    stream_slice=self.make_join_channel_slice(record),
-                )
+    def join_channel(self, config: Mapping[str, Any], record: Mapping[str, Any]):
+        list(
+            self.join_channels_stream(config).read_records(
+                sync_mode=SyncMode.full_refresh,
+                stream_slice=self.make_join_channel_slice(record),
             )
-            return channel[0]
-        return record
+        )
+
+    def read_records(
+        self,
+        records_schema: Mapping[str, Any],
+        stream_slice: Optional[StreamSlice] = None,
+    ) -> Iterable[StreamData]:
+        _slice = stream_slice or StreamSlice(partition={}, cursor_slice={})  # None-check
+
+        self._paginator.reset()
+
+        most_recent_record_from_slice = None
+        record_generator = partial(
+            self._parse_records,
+            stream_state=self.state or {},
+            stream_slice=_slice,
+            records_schema=records_schema,
+        )
+
+        for stream_data in self._read_pages(record_generator, self.state, _slice):
+            current_record = self._extract_record(stream_data, _slice)
+            if self.cursor and current_record:
+                self.cursor.observe(_slice, current_record)
+
+            most_recent_record_from_slice = self._get_most_recent_record(most_recent_record_from_slice, current_record, _slice)
+            # joining channel logic
+            if self.should_join_to_channel(self.config, stream_data):
+                self.join_channel(self.config, stream_data)
+
+            yield stream_data
+
+        if self.cursor:
+            self.cursor.close_slice(_slice, most_recent_record_from_slice)
+        return
--- airbyte-integrations/connectors/source-slack/unit_tests/test_components.py
+++ airbyte-integrations/connectors/source-slack/unit_tests/test_components.py
@@ -3,14 +3,16 @@
 from unittest.mock import MagicMock
 
 import pendulum
+import pytest
+from airbyte_cdk.sources.declarative.extractors import DpathExtractor, RecordSelector
 from airbyte_cdk.sources.declarative.partition_routers.substream_partition_router import ParentStreamConfig
-from airbyte_cdk.sources.declarative.requesters import RequestOption
+from airbyte_cdk.sources.declarative.requesters import HttpRequester, RequestOption
 from airbyte_cdk.sources.streams.http.auth import TokenAuthenticator
 from airbyte_protocol.models import SyncMode
 from freezegun import freeze_time
 from source_slack import SourceSlack
 from source_slack.components.channel_members_extractor import ChannelMembersExtractor
-from source_slack.components.join_channels import JoinChannels, JoinChannelsStream
+from source_slack.components.join_channels import ChannelsRetriever, JoinChannelsStream
 from source_slack.components.threads_partition_router import ThreadsPartitionRouter
 
 
@@ -29,10 +31,10 @@ def test_channel_members_extractor(token_config):
         "U061F7AUR",
         "W012A3CDE"
     ]}
-    records = ChannelMembersExtractor(config=token_config, parameters={}, field_path=['members']).extract_records(response=response_mock)
-    assert records == [{'member_id': 'U023BECGF'},
-                       {'member_id': 'U061F7AUR'},
-                       {'member_id': 'W012A3CDE'}]
+    records = ChannelMembersExtractor(config=token_config, parameters={}, field_path=["members"]).extract_records(response=response_mock)
+    assert records == [{"member_id": "U023BECGF"},
+                       {"member_id": "U061F7AUR"},
+                       {"member_id": "W012A3CDE"}]
 
 
 @freeze_time("2024-03-10T20:00:00Z", tz_offset=-2)
@@ -73,36 +75,66 @@ def test_threads_partition_router(token_config, requests_mock):
 
 
 def test_join_channels(token_config, requests_mock, joined_channel):
-    requests_mock.post(
+    mocked_request = requests_mock.post(
         url="https://slack.com/api/conversations.join",
-        json={"channel": joined_channel}
+        json={"ok": True, "channel": joined_channel}
     )
     token = token_config["credentials"]["api_token"]
     authenticator = TokenAuthenticator(token)
     channel_filter = token_config["channel_filter"]
     stream = JoinChannelsStream(authenticator=authenticator, channel_filter=channel_filter)
-    records = list(stream.read_records(sync_mode=SyncMode.full_refresh, stream_slice={"channel": "C061EG9SL", "channel_name": "general"}))
-    assert records[0] == joined_channel
+    records = stream.read_records(sync_mode=SyncMode.full_refresh, stream_slice={"channel": "C061EG9SL", "channel_name": "general"})
+    assert not list(records)
+    assert mocked_request.called
+
+
+def get_channels_retriever_instance(token_config):
+    return ChannelsRetriever(
+        config=token_config,
+        requester=HttpRequester(name="channels", path="conversations.list", url_base="https://slack.com/api/", config=token_config,
+                                parameters={}),
+        record_selector=RecordSelector(
+            extractor=DpathExtractor(field_path=["channels"], config=token_config, parameters={}),
+            config=token_config, parameters={},
+            schema_normalization=None),
+        parameters={}
+    )
 
 
 def test_join_channels_should_join_to_channel(token_config):
-    transformation = JoinChannels()
-    assert transformation.should_join_to_channel(token_config, {"is_member": False}) is True
-    assert transformation.should_join_to_channel(token_config, {"is_member": True}) is False
+    retriever = get_channels_retriever_instance(token_config)
+    assert retriever.should_join_to_channel(token_config, {"is_member": False}) is True
+    assert retriever.should_join_to_channel(token_config, {"is_member": True}) is False
 
 
 def test_join_channels_make_join_channel_slice(token_config):
-    transformation = JoinChannels()
-    assert transformation.make_join_channel_slice({"id": "C061EG9SL", "name": "general"}) == {"channel": "C061EG9SL",
-                                                                                              "channel_name": "general"}
+    retriever = get_channels_retriever_instance(token_config)
+    expected_slice = {"channel": "C061EG9SL", "channel_name": "general"}
+    assert retriever.make_join_channel_slice({"id": "C061EG9SL", "name": "general"}) == expected_slice
 
 
-def test_join_channel_transformation(requests_mock, token_config, joined_channel):
-    requests_mock.post(
+@pytest.mark.parametrize(
+    "join_response, log_message",
+    (
+        ({"ok": True, "channel": {"is_member": True, "id": "channel 2", "name": "test channel"}}, "Successfully joined channel: test channel"),
+        ({"ok": False, "error": "missing_scope", "needed": "channels:write"},
+         "Unable to joined channel: test channel. Reason: {'ok': False, 'error': " "'missing_scope', 'needed': 'channels:write'}"),
+    ),
+    ids=["successful_join_to_channel", "failed_join_to_channel"]
+)
+def test_join_channel_read(requests_mock, token_config, joined_channel, caplog, join_response, log_message):
+    mocked_request = requests_mock.post(
         url="https://slack.com/api/conversations.join",
-        json={"channel": joined_channel}
+        json=join_response
+    )
+    requests_mock.get(
+        url="https://slack.com/api/conversations.list",
+        json={"channels": [{"is_member": True, "id": "channel 1"}, {"is_member": False, "id": "channel 2", "name": "test channel"}]}
     )
 
-    transformation = JoinChannels()
-    assert transformation.transform(config=token_config, record={"is_member": True}) == {"is_member": True}
-    assert transformation.transform(config=token_config, record={"is_member": False}) == joined_channel
+    retriever = get_channels_retriever_instance(token_config)
+    assert len(list(retriever.read_records(records_schema={}))) == 2
+    assert mocked_request.called
+    assert mocked_request.last_request._request.body == b'{"channel": "channel 2"}'
+    assert log_message in caplog.text
+
