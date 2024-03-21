8266f22f7e35978cff51a05389a64214e3debdb6
Gireesh Sreepathi <gisripa@gmail.com>
2024-03-21 11:53:48 -0700

    Removing old data adapter class

--- airbyte-integrations/connectors/destination-postgres/src/main/java/io/airbyte/integrations/destination/postgres/PostgresSqlOperations.java
+++ airbyte-integrations/connectors/destination-postgres/src/main/java/io/airbyte/integrations/destination/postgres/PostgresSqlOperations.java
@@ -8,8 +8,8 @@ import static io.airbyte.cdk.integrations.base.JavaBaseConstants.*;
 
 import io.airbyte.cdk.db.jdbc.JdbcDatabase;
 import io.airbyte.cdk.integrations.base.TypingAndDedupingFlag;
+import io.airbyte.cdk.integrations.destination.async.partial_messages.PartialAirbyteMessage;
 import io.airbyte.cdk.integrations.destination.jdbc.JdbcSqlOperations;
-import io.airbyte.cdk.integrations.destination_async.partial_messages.PartialAirbyteMessage;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
@@ -25,9 +25,7 @@ import org.postgresql.core.BaseConnection;
 
 public class PostgresSqlOperations extends JdbcSqlOperations {
 
-  public PostgresSqlOperations() {
-    super(new PostgresDataAdapter());
-  }
+  public PostgresSqlOperations() {}
 
   @Override
   protected List<String> postCreateTableQueries(final String schemaName, final String tableName) {
