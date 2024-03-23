a4021dcf1fded489c8969f6a990b70b682d0e7d9
Stephane Geneix <stephane@airbyte.io>
2024-03-22 17:36:10 -0700

    fix compilation errors

--- airbyte-cdk/java/airbyte-cdk/datastore-mongo/src/test/kotlin/io/airbyte/cdk/db/mongodb/MongoUtilsTest.kt
+++ airbyte-cdk/java/airbyte-cdk/datastore-mongo/src/test/kotlin/io/airbyte/cdk/db/mongodb/MongoUtilsTest.kt
@@ -12,22 +12,23 @@ import org.junit.jupiter.api.Test
 internal class MongoUtilsTest {
     @Test
     fun testTransformToStringIfMarked() {
-        val columnNames = listOf("_id", "createdAt", "connectedWallets", "connectedAccounts_aibyte_transform")
+        val columnNames =
+            listOf("_id", "createdAt", "connectedWallets", "connectedAccounts_aibyte_transform")
         val fieldName = "connectedAccounts"
-        val node = Jsons.deserialize(
+        val node =
+            Jsons.deserialize(
                 "{\"_id\":\"12345678as\",\"createdAt\":\"2022-11-11 12:13:14\",\"connectedWallets\":\"wallet1\"," +
-                        "\"connectedAccounts\":" +
-                        "{\"google\":{\"provider\":\"google\",\"refreshToken\":\"test-rfrsh-google-token-1\",\"accessToken\":\"test-access-google-token-1\",\"expiresAt\":\"2020-09-01T21:07:00Z\",\"createdAt\":\"2020-09-01T20:07:01Z\"},"
-                        +
-                        "\"figma\":{\"provider\":\"figma\",\"refreshToken\":\"test-rfrsh-figma-token-1\",\"accessToken\":\"test-access-figma-token-1\",\"expiresAt\":\"2020-12-13T22:08:03Z\",\"createdAt\":\"2020-09-14T22:08:03Z\",\"figmaInfo\":{\"teamID\":\"501087711831561793\"}},"
-                        +
-                        "\"slack\":{\"provider\":\"slack\",\"accessToken\":\"test-access-slack-token-1\",\"createdAt\":\"2020-09-01T20:15:07Z\",\"slackInfo\":{\"userID\":\"UM5AD2YCE\",\"teamID\":\"T2VGY5GH5\"}}}}")
+                    "\"connectedAccounts\":" +
+                    "{\"google\":{\"provider\":\"google\",\"refreshToken\":\"test-rfrsh-google-token-1\",\"accessToken\":\"test-access-google-token-1\",\"expiresAt\":\"2020-09-01T21:07:00Z\",\"createdAt\":\"2020-09-01T20:07:01Z\"}," +
+                    "\"figma\":{\"provider\":\"figma\",\"refreshToken\":\"test-rfrsh-figma-token-1\",\"accessToken\":\"test-access-figma-token-1\",\"expiresAt\":\"2020-12-13T22:08:03Z\",\"createdAt\":\"2020-09-14T22:08:03Z\",\"figmaInfo\":{\"teamID\":\"501087711831561793\"}}," +
+                    "\"slack\":{\"provider\":\"slack\",\"accessToken\":\"test-access-slack-token-1\",\"createdAt\":\"2020-09-01T20:15:07Z\",\"slackInfo\":{\"userID\":\"UM5AD2YCE\",\"teamID\":\"T2VGY5GH5\"}}}}"
+            )
         Assertions.assertTrue(node[fieldName].isObject)
 
         transformToStringIfMarked((node as ObjectNode), columnNames, fieldName)
 
         Assertions.assertNull(node[fieldName])
-        Assertions.assertNotNull(node[fieldName + AIRBYTE_SUFFIX])
-        Assertions.assertTrue(node[fieldName + AIRBYTE_SUFFIX].isTextual)
+        Assertions.assertNotNull(node[fieldName + MongoUtils.AIRBYTE_SUFFIX])
+        Assertions.assertTrue(node[fieldName + MongoUtils.AIRBYTE_SUFFIX].isTextual)
     }
 }
