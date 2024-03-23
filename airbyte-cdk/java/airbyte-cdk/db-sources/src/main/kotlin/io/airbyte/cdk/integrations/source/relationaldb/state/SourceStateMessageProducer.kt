d0730f4d86a9ca28465e89ccf8f614e1986400c7
Stephane Geneix <stephane@airbyte.io>
2024-03-23 15:09:15 -0700

    convert CDK db-sources submodules to kotlin

--- airbyte-cdk/java/airbyte-cdk/db-sources/src/main/kotlin/io/airbyte/cdk/integrations/source/relationaldb/state/SourceStateMessageProducer.kt
+++ airbyte-cdk/java/airbyte-cdk/db-sources/src/main/kotlin/io/airbyte/cdk/integrations/source/relationaldb/state/SourceStateMessageProducer.kt
@@ -12,14 +12,18 @@ import io.airbyte.protocol.models.v0.ConfiguredAirbyteStream
  * generate state messages when needed. This interface defines how would those state messages be
  * generated, and how the incoming record messages will be processed.
  *
- * @param <T> </T>
- */
+ * @param <T>
+</T> */
 interface SourceStateMessageProducer<T> {
-    /** Returns a state message that should be emitted at checkpoint. */
+    /**
+     * Returns a state message that should be emitted at checkpoint.
+     */
     fun generateStateMessageAtCheckpoint(stream: ConfiguredAirbyteStream?): AirbyteStateMessage?
 
-    /** For the incoming record message, this method defines how the connector will consume it. */
-    fun processRecordMessage(stream: ConfiguredAirbyteStream?, message: T): AirbyteMessage
+    /**
+     * For the incoming record message, this method defines how the connector will consume it.
+     */
+    fun processRecordMessage(stream: ConfiguredAirbyteStream, message: T): AirbyteMessage
 
     /**
      * At the end of the iteration, this method will be called and it will generate the final state
@@ -27,12 +31,12 @@ interface SourceStateMessageProducer<T> {
      *
      * @return
      */
-    fun createFinalStateMessage(stream: ConfiguredAirbyteStream?): AirbyteStateMessage?
+    fun createFinalStateMessage(stream: ConfiguredAirbyteStream): AirbyteStateMessage?
 
     /**
-     * Determines if the iterator has reached checkpoint or not per connector's definition. By
-     * default iterator will check if the number of records processed is greater than the checkpoint
-     * interval or last state message has already passed syncCheckpointDuration.
+     * Determines if the iterator has reached checkpoint or not per connector's definition. By default
+     * iterator will check if the number of records processed is greater than the checkpoint interval or
+     * last state message has already passed syncCheckpointDuration.
      */
     fun shouldEmitStateMessage(stream: ConfiguredAirbyteStream?): Boolean
 }
