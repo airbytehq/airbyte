1d231cde5c016c24c5dee3d40b8b9d4448efa148
Stephane Geneix <stephane@airbyte.io>
2024-03-22 17:38:47 -0700

    convert java CDK postgres submodule to kotlin

---
+++ airbyte-cdk/java/airbyte-cdk/datastore-postgres/src/testFixtures/kotlin/io/airbyte/cdk/testutils/PostgreSQLContainerHelper.kt
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) 2023 Airbyte, Inc., all rights reserved.
+ */
+package io.airbyte.cdk.testutils
+
+import org.testcontainers.containers.PostgreSQLContainer
+import org.testcontainers.utility.MountableFile
+import java.io.IOException
+import java.util.*
+
+object PostgreSQLContainerHelper {
+    @JvmStatic
+    fun runSqlScript(file: MountableFile?, db: PostgreSQLContainer<*>) {
+        try {
+            val scriptPath = "/etc/" + UUID.randomUUID() + ".sql"
+            db.copyFileToContainer(file, scriptPath)
+            db.execInContainer(
+                    "psql", "-d", db.databaseName, "-U", db.username, "-a", "-f", scriptPath)
+        } catch (e: InterruptedException) {
+            throw RuntimeException(e)
+        } catch (e: IOException) {
+            throw RuntimeException(e)
+        }
+    }
+}
