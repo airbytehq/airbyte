5ece58fa7d9397b1f99c06cdd7b3371919651671
Alexandre Girard <alexandre@airbyte.io>
2024-03-25 15:33:49 -0700

    parametrize unit test

--- airbyte-cdk/python/unit_tests/sources/declarative/migrations/test_legacy_to_per_partition_migration.py
+++ airbyte-cdk/python/unit_tests/sources/declarative/migrations/test_legacy_to_per_partition_migration.py
@@ -1,7 +1,7 @@
 #
 # Copyright (c) 2023 Airbyte, Inc., all rights reserved.
 #
-
+import pytest
 from airbyte_cdk.sources.declarative.migrations.legacy_to_per_partition_state_migration import LegacyToPerPartitionStateMigration
 from airbyte_cdk.sources.declarative.models import (
     CustomRetriever,
@@ -42,165 +42,119 @@ def test_migrate_a_valid_legacy_state_to_per_partition():
     assert migrator.migrate(input_state) == expected_state
 
 
-def test_should_not_migrate_a_per_partition_state():
-    input_state = {
-        "states": [
+@pytest.mark.parametrize(
+    "input_state",  [
+        pytest.param({
+            "states": [
+                {
+                    "partition": {"id": "13506132"},
+                    "cursor": {"last_changed": "2022-12-27T08:34:39+00:00"}
+                },
+                {
+                    "partition": {"id": "14351124"},
+                    "cursor": {"last_changed": "2022-12-27T08:35:39+00:00"}
+                },
+            ]
+        }, id="test_should_not_migrate_a_per_partition_state"),
+        pytest.param({
+            "states": [
+                {
+                    "partition": {"id": "13506132"},
+                    "cursor": {"last_changed": "2022-12-27T08:34:39+00:00"}
+                },
+                {
+                    "partition": {"id": "14351124"},
+                },
+            ]
+        }, id="test_should_not_migrate_state_without_a_cursor_component"),
+        pytest.param({
+            "states": [
+                {
+                    "partition": {"id": "13506132"},
+                    "cursor": {"updated_at": "2022-12-27T08:34:39+00:00"}
+                },
+                {
+                    "partition": {"id": "14351124"},
+                    "cursor": {"updated_at": "2022-12-27T08:35:39+00:00"}
+                },
+            ]
+        }, id="test_should_not_migrate_a_per_partition_state_with_wrong_cursor_field"),
+        pytest.param({
+            "states": [
+                {
+                    "partition": {"id": "13506132"},
+                    "cursor": {"last_changed": "2022-12-27T08:34:39+00:00"}
+                },
+                {
+                    "partition": {"id": "14351124"},
+                    "cursor": {"last_changed": "2022-12-27T08:35:39+00:00", "updated_at": "2021-01-01"}
+                },
+            ]
+        }, id="test_should_not_migrate_a_per_partition_state_with_multiple_cursor_fields"),
+        pytest.param(
             {
-                "partition": {"id": "13506132"},
-                "cursor": {"last_changed": "2022-12-27T08:34:39+00:00"}
-            },
+                "states": [
+                    {
+                        "partition": {"id": "13506132"},
+                        "cursor": {"last_changed": "2022-12-27T08:34:39+00:00"}
+                    },
+                    {
+                        "cursor": {"last_changed": "2022-12-27T08:34:39+00:00"}
+                    },
+                ]
+            }, id="test_should_not_migrate_state_without_a_partition_component"
+        ),
+        pytest.param(
             {
-                "partition": {"id": "14351124"},
-                "cursor": {"last_changed": "2022-12-27T08:35:39+00:00"}
+                "states": [
+                    {
+                        "partition": {"id": "13506132", "another_id": "A"},
+                        "cursor": {"last_changed": "2022-12-27T08:34:39+00:00"}
+                    },
+                    {
+                        "partition": {"id": "13506134"},
+                        "cursor": {"last_changed": "2022-12-27T08:34:39+00:00"}
+                    },
+                ]
+            }, id="test_should_not_migrate_state_if_multiple_partition_keys"
+        ),
+        pytest.param(
+            {
+                "states": [
+                    {
+                        "partition": {"identifier": "13506132"},
+                        "cursor": {"last_changed": "2022-12-27T08:34:39+00:00"}
+                    },
+                    {
+                        "partition": {"id": "13506134"},
+                        "cursor": {"last_changed": "2022-12-27T08:34:39+00:00"}
+                    },
+                ]
+            }, id="test_should_not_migrate_state_if_invalid_partition_key"
+        ),
+        pytest.param(
+            {
+                "13506132": {
+                    "last_changed": "2022-12-27T08:34:39+00:00"
+                },
+                "14351124": {
+                    "last_changed": "2022-12-27T08:35:39+00:00",
+                    "another_key": "2022-12-27T08:35:39+00:00"
+                },
+            }, id="test_should_not_migrate_if_the_partitioned_state_has_more_than_one_key"
+        ),
+        pytest.param({
+            "13506132": {
+                "last_changed": "2022-12-27T08:34:39+00:00"
             },
-        ]
-    }
-
+            "14351124": {
+                "another_key": "2022-12-27T08:35:39+00:00"
+            },
+        }, id="test_should_not_migrate_if_the_partitioned_state_key_is_not_the_cursor_field"),
+    ]
+)
+def test_should_not_migrate(input_state):
     migrator = _migrator()
-
-    assert not migrator.should_migrate(input_state)
-
-
-def test_should_not_migrate_state_without_a_cursor_component():
-    input_state = {
-        "states": [
-            {
-                "partition": {"id": "13506132"},
-                "cursor": {"last_changed": "2022-12-27T08:34:39+00:00"}
-            },
-            {
-                "partition": {"id": "14351124"},
-            },
-        ]
-    }
-
-    migrator = _migrator()
-
-    assert not migrator.should_migrate(input_state)
-
-
-def test_should_not_migrate_a_per_partition_state_with_wrong_cursor_field():
-    input_state = {
-        "states": [
-            {
-                "partition": {"id": "13506132"},
-                "cursor": {"updated_at": "2022-12-27T08:34:39+00:00"}
-            },
-            {
-                "partition": {"id": "14351124"},
-                "cursor": {"updated_at": "2022-12-27T08:35:39+00:00"}
-            },
-        ]
-    }
-
-    migrator = _migrator()
-
-    assert not migrator.should_migrate(input_state)
-
-
-def test_should_not_migrate_a_per_partition_state_with_multiple_cursor_fields():
-    input_state = {
-        "states": [
-            {
-                "partition": {"id": "13506132"},
-                "cursor": {"last_changed": "2022-12-27T08:34:39+00:00"}
-            },
-            {
-                "partition": {"id": "14351124"},
-                "cursor": {"last_changed": "2022-12-27T08:35:39+00:00", "updated_at": "2021-01-01"}
-            },
-        ]
-    }
-
-    migrator = _migrator()
-
-    assert not migrator.should_migrate(input_state)
-
-
-def test_should_not_migrate_state_without_a_partition_component():
-    input_state = {
-        "states": [
-            {
-                "partition": {"id": "13506132"},
-                "cursor": {"last_changed": "2022-12-27T08:34:39+00:00"}
-            },
-            {
-                "cursor": {"last_changed": "2022-12-27T08:34:39+00:00"}
-            },
-        ]
-    }
-
-    migrator = _migrator()
-
-    assert not migrator.should_migrate(input_state)
-
-
-def test_should_not_migrate_state_if_multiple_partition_keys():
-    input_state = {
-        "states": [
-            {
-                "partition": {"id": "13506132", "another_id": "A"},
-                "cursor": {"last_changed": "2022-12-27T08:34:39+00:00"}
-            },
-            {
-                "partition": {"id": "13506134"},
-                "cursor": {"last_changed": "2022-12-27T08:34:39+00:00"}
-            },
-        ]
-    }
-
-    migrator = _migrator()
-
-    assert not migrator.should_migrate(input_state)
-
-
-def test_should_not_migrate_state_if_invalid_partition_key():
-    input_state = {
-        "states": [
-            {
-                "partition": {"identifier": "13506132"},
-                "cursor": {"last_changed": "2022-12-27T08:34:39+00:00"}
-            },
-            {
-                "partition": {"id": "13506134"},
-                "cursor": {"last_changed": "2022-12-27T08:34:39+00:00"}
-            },
-        ]
-    }
-
-    migrator = _migrator()
-
-    assert not migrator.should_migrate(input_state)
-
-
-def test_should_not_migrate_if_the_partitioned_state_has_more_than_one_key():
-    input_state = {
-        "13506132": {
-            "last_changed": "2022-12-27T08:34:39+00:00"
-        },
-        "14351124": {
-            "last_changed": "2022-12-27T08:35:39+00:00",
-            "another_key": "2022-12-27T08:35:39+00:00"
-        },
-    }
-
-    migrator = _migrator()
-
-    assert not migrator.should_migrate(input_state)
-
-
-def test_should_not_migrate_if_the_partitioned_state_key_is_not_the_cursor_field():
-    input_state = {
-        "13506132": {
-            "last_changed": "2022-12-27T08:34:39+00:00"
-        },
-        "14351124": {
-            "another_key": "2022-12-27T08:35:39+00:00"
-        },
-    }
-
-    migrator = _migrator()
-
     assert not migrator.should_migrate(input_state)
 
 
