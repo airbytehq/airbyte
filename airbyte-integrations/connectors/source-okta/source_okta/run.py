c57276800fdc0d323308db1ab4e99dccd61e3769
Alexandre Girard <alexandre@airbyte.io>
2024-03-26 16:21:43 -0700

    Add a config migration

--- airbyte-integrations/connectors/source-okta/source_okta/run.py
+++ airbyte-integrations/connectors/source-okta/source_okta/run.py
@@ -7,7 +7,9 @@ import sys
 
 from airbyte_cdk.entrypoint import launch
 from .source import SourceOkta
+from .config_migration import OktaConfigMigration
 
 def run():
     source = SourceOkta()
+    OktaConfigMigration.migrate(sys.argv[1:], source)
     launch(source, sys.argv[1:])
