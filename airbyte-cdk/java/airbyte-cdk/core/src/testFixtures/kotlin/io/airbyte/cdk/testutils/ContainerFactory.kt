1c974050e1400df097799187fda06c38d7267976
Stephane Geneix <147216312+stephane-airbyte@users.noreply.github.com>
2024-03-27 15:58:50 -0700

    fix ContainerFactory (#36574)

--- airbyte-cdk/java/airbyte-cdk/core/src/testFixtures/kotlin/io/airbyte/cdk/testutils/ContainerFactory.kt
+++ airbyte-cdk/java/airbyte-cdk/core/src/testFixtures/kotlin/io/airbyte/cdk/testutils/ContainerFactory.kt
@@ -30,9 +30,9 @@ import org.testcontainers.utility.DockerImageName
 abstract class ContainerFactory<C : GenericContainer<*>> {
     @JvmRecord
     private data class ContainerKey<C : GenericContainer<*>>(
-        val clazz: Class<out ContainerFactory<*>?>?,
-        val imageName: DockerImageName?,
-        val methods: MutableList<out NamedContainerModifier<C>>
+        val clazz: Class<out ContainerFactory<*>>,
+        val imageName: DockerImageName,
+        val methods: kotlin.collections.List<String>
     )
 
     private class ContainerOrException(
@@ -90,7 +90,6 @@ abstract class ContainerFactory<C : GenericContainer<*>> {
      *
      * @Deprecated use shared(String, NamedContainerModifier) instead
      */
-    @Deprecated("")
     fun shared(imageName: String, vararg methods: String): C {
         return shared(
             imageName,
@@ -110,7 +109,11 @@ abstract class ContainerFactory<C : GenericContainer<*>> {
         namedContainerModifiers: MutableList<out NamedContainerModifier<C>> = ArrayList()
     ): C {
         val containerKey =
-            ContainerKey<C>(javaClass, DockerImageName.parse(imageName), namedContainerModifiers)
+            ContainerKey<C>(
+                javaClass,
+                DockerImageName.parse(imageName),
+                namedContainerModifiers.map { it.name() }.toList()
+            )
         // We deliberately avoid creating the container itself eagerly during the evaluation of the
         // map
         // value.
@@ -119,7 +122,7 @@ abstract class ContainerFactory<C : GenericContainer<*>> {
         val containerOrError =
             SHARED_CONTAINERS!!.computeIfAbsent(containerKey) { key: ContainerKey<*>? ->
                 ContainerOrException {
-                    createAndStartContainer(key!!.imageName, (key as ContainerKey<C>)!!.methods)
+                    createAndStartContainer(key!!.imageName, namedContainerModifiers)
                 }
             }
         // Instead, the container creation (if applicable) is deferred to here.
@@ -131,7 +134,6 @@ abstract class ContainerFactory<C : GenericContainer<*>> {
      *
      * @Deprecated use exclusive(String, NamedContainerModifier) instead
      */
-    @Deprecated("")
     fun exclusive(imageName: String, vararg methods: String): C {
         return exclusive(
             imageName,
