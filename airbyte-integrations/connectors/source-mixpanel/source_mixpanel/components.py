f64abec2606941b9a60de0f94721c4e3681a91dd
Vadym Ratniuk <midavadim@yahoo.com>
2024-03-27 18:55:21 +0200

    cohorts and revenue streams custom components

--- airbyte-integrations/connectors/source-mixpanel/source_mixpanel/components.py
+++ airbyte-integrations/connectors/source-mixpanel/source_mixpanel/components.py
@@ -1,3 +1,4 @@
+from airbyte_cdk.sources.declarative.extractors import DpathExtractor
 from airbyte_cdk.sources.declarative.partition_routers import SubstreamPartitionRouter
 
 from dataclasses import InitVar, dataclass
@@ -8,6 +9,20 @@ from airbyte_cdk.sources.declarative.interpolation.interpolated_string import In
 from airbyte_cdk.sources.declarative.requesters.request_option import RequestOption, RequestOptionType
 from airbyte_cdk.sources.declarative.stream_slicers.stream_slicer import StreamSlicer
 from airbyte_cdk.sources.declarative.types import Config, Record, StreamSlice, StreamState
+from airbyte_cdk.sources.declarative.transformations import RecordTransformation, AddFields
+from dataclasses import InitVar, dataclass
+from typing import Any, List, Mapping, Union
+
+import dpath.util
+import requests
+from airbyte_cdk.sources.declarative.decoders.decoder import Decoder
+from airbyte_cdk.sources.declarative.decoders.json_decoder import JsonDecoder
+from airbyte_cdk.sources.declarative.extractors.record_extractor import RecordExtractor
+from airbyte_cdk.sources.declarative.interpolation.interpolated_string import InterpolatedString
+from airbyte_cdk.sources.declarative.types import Config
+
+
+
 
 class CohortMembersSubstreamPartitionRouter(SubstreamPartitionRouter):
 
@@ -20,3 +35,62 @@ class CohortMembersSubstreamPartitionRouter(SubstreamPartitionRouter):
         # https://developer.mixpanel.com/reference/engage-query
         cohort_id = stream_slice["id"]
         return {"filter_by_cohort": f"{{\"id\":{cohort_id}}}"}
+
+
+@dataclass
+class RevenueTransformation(RecordTransformation):
+    """
+    Make 'conversations.join' POST request for every found channel id
+    if we are not still a member of such channel
+    """
+
+    def transform(
+        self,
+        record: Record,
+        config: Optional[Config] = None,
+        stream_state: Optional[StreamState] = None,
+        stream_slice: Optional[StreamSlice] = None,
+    ) -> Record:
+        print("=====================================")
+        print("=====================================")
+        print(record)
+        # records = response.json().get(self.data_field, {})
+        # for date_entry in records:
+        #     if date_entry != "$overall":
+        #         yield {"date": date_entry, **records[date_entry]}
+
+
+class RevenueDpathExtractor(DpathExtractor):
+    def extract_records(self, response: requests.Response) -> List[Mapping[str, Any]]:
+        """
+        response.json() example:
+        {
+            'computed_at': '2021-07-03T12:43:48.889421+00:00',
+            'results': {
+                '$overall': {       <-- should be skipped
+                    'amount': 0.0,
+                    'count': 124,
+                    'paid_count': 0
+                },
+                '2021-06-01': {
+                    'amount': 0.0,
+                    'count': 124,
+                    'paid_count': 0
+                },
+                '2021-06-02': {
+                    'amount': 0.0,
+                    'count': 124,
+                    'paid_count': 0
+                },
+                ...
+            },
+            'session_id': '162...',
+            'status': 'ok'
+        }
+        """
+        new_records = []
+        for record in super().extract_records(response):
+            for date_entry in record:
+                if date_entry != "$overall":
+                    list.append(new_records, {"date": date_entry, **record[date_entry]})
+        return new_records
