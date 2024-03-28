58e87a06834bd0cb24ccf7bf4cf16123d1633da7
Michal Krawczyk <michal@corraldata.com>
2024-03-28 13:25:23 +0100

    Support appointments lookback/lookahead timeframes

--- airbyte-integrations/connectors/source-zenoti/source_zenoti/components.py
+++ airbyte-integrations/connectors/source-zenoti/source_zenoti/components.py
@@ -1,6 +1,7 @@
 import datetime
 
 from dataclasses import dataclass
+from typing import Any, Mapping
 from airbyte_cdk.sources.declarative.incremental.datetime_based_cursor import DatetimeBasedCursor
 
 @dataclass
@@ -10,3 +11,8 @@ class AppointmentsDatetimeBasedCursor(DatetimeBasedCursor):
     if not self._end_datetime:
         return now
     return self._end_datetime.get_datetime(self.config)
+
+  def _calculate_cursor_datetime_from_state(self, stream_state: Mapping[str, Any]) -> datetime.datetime:
+    if self._cursor_field.eval(self.config, stream_state=stream_state) in stream_state:
+      return datetime.datetime.now(tz=self._timezone)
+    return datetime.datetime.min.replace(tzinfo=datetime.timezone.utc)
