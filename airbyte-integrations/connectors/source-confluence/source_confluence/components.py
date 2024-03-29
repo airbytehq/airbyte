a3e10f248030202576aa85b813f7c260344482ad
nobowned <lancerbarnes@gmail.com>
2024-03-28 20:14:00 -0600

    Provide authenticator type

--- airbyte-integrations/connectors/source-confluence/source_confluence/components.py
+++ airbyte-integrations/connectors/source-confluence/source_confluence/components.py
@@ -10,6 +10,7 @@ from airbyte_cdk.sources.declarative.types import Config
 
 @dataclass
 class CustomAuthenticator(NoAuth):
+    authenticator: DeclarativeAuthenticator
     config: Config
     email: Union[InterpolatedString, str]
     api_token: Union[InterpolatedString, str]
@@ -17,13 +18,13 @@ class CustomAuthenticator(NoAuth):
     def __post_init__(self, parameters: Mapping[str, Any]):
         confluence_server: bool = self.config.get("confluence_server")
         if confluence_server:
-            self._authenticator = BearerAuthenticator(
+            self.authenticator = BearerAuthenticator(
                 InterpolatedStringTokenProvider(api_token=self.api_token or "", config=self.config, parameters=parameters),
                 config=self.config,
                 parameters=parameters,
             )
         else:
-            self._authenticator = BasicHttpAuthenticator(
+            self.authenticator = BasicHttpAuthenticator(
                 password=self.api_token, username=self.email, config=self.config, parameters=parameters
             )
 
@@ -33,4 +34,4 @@ class CustomAuthenticator(NoAuth):
 
     @property
     def token(self) -> str:
-        return self._authenticator.token()
+        return self.authenticator.token()
