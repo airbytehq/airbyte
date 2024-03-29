10252d6811003bcc2b11973327cccd221bd6fcb1
Artem Inzhyyants <artem.inzhyyants@gmail.com>
2024-03-29 10:33:48 +0100

    Source Azure Blob Storage: fix docker script
    Signed-off-by: Artem Inzhyyants <artem.inzhyyants@gmail.com>

--- airbyte-integrations/connectors/source-azure-blob-storage/integration_tests/csv_export/main.sh
+++ airbyte-integrations/connectors/source-azure-blob-storage/integration_tests/csv_export/main.sh
@@ -3,7 +3,12 @@
 cd "$(dirname "$0")"
 
 mkdir -p /tmp/csv
+cp -r $(pwd) /tmp/csv
 
-
-docker run --rm -v $(pwd)/secret_faker:/secrets -v $(pwd)/configured_catalog:/integration_tests airbyte/source-faker:latest read --config /secrets/secret_faker.json --catalog /integration_tests/configured_catalog.json\
-  | tee >(./purchases.sh) >(./products.sh) >(./users.sh)
+docker run --rm \
+ -v /tmp/csv/csv_export/secret_faker:/secrets \
+  -v /tmp/csv/csv_export/configured_catalog:/integration_tests \
+  airbyte/source-faker:latest read \
+  --config /secrets/secret_faker.json \
+  --catalog /integration_tests/configured_catalog.json \
+  | tee >(./purchases.sh) >(./products.sh) >(./users.sh) > dev/null
