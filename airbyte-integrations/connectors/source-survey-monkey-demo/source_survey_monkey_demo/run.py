10c086dc3c402329b43823335b1161d5869f50f4
Alexandre Girard <alexandre@airbyte.io>
2024-03-29 12:38:12 -0700

    Add concurrency

--- airbyte-integrations/connectors/source-survey-monkey-demo/source_survey_monkey_demo/run.py
+++ airbyte-integrations/connectors/source-survey-monkey-demo/source_survey_monkey_demo/run.py
@@ -4,12 +4,44 @@
 
 
 import sys
+import sys
+import traceback
+from datetime import datetime
+from typing import List
+
+from airbyte_cdk.entrypoint import AirbyteEntrypoint, launch
+from airbyte_cdk.models import AirbyteErrorTraceMessage, AirbyteMessage, AirbyteTraceMessage, TraceType, Type
 
 from airbyte_cdk.entrypoint import launch
 
 from .source import SourceSurveyMonkeyDemo
+def _get_source(args: List[str]):
+    config_path = AirbyteEntrypoint.extract_config(args)
+    state_path = AirbyteEntrypoint.extract_state(args)
+    try:
+        return SourceSurveyMonkeyDemo(
+            SourceSurveyMonkeyDemo.read_config(config_path) if config_path else None,
+            SourceSurveyMonkeyDemo.read_state(state_path) if state_path else None,
+        )
+    except Exception as error:
+        print(
+            AirbyteMessage(
+                type=Type.TRACE,
+                trace=AirbyteTraceMessage(
+                    type=TraceType.ERROR,
+                    emitted_at=int(datetime.now().timestamp() * 1000),
+                    error=AirbyteErrorTraceMessage(
+                        message=f"Error starting the sync. This could be due to an invalid configuration or catalog. Please contact Support for assistance. Error: {error}",
+                        stack_trace=traceback.format_exc(),
+                    ),
+                ),
+            ).json()
+        )
+        return None
+
 
 
 def run():
-    source = SourceSurveyMonkeyDemo()
-    launch(source, sys.argv[1:])
+    args = sys.argv[1:]
+    source = _get_source(args)
+    launch(source, args)
--- airbyte-integrations/connectors/source-survey-monkey-demo/source_survey_monkey_demo/source.py
+++ airbyte-integrations/connectors/source-survey-monkey-demo/source_survey_monkey_demo/source.py
@@ -13,10 +13,43 @@ from airbyte_cdk.sources.streams import Stream
 from airbyte_cdk.sources.streams.http import HttpStream
 from airbyte_cdk.sources.streams.http.auth import Oauth2Authenticator, TokenAuthenticator
 import datetime
+from airbyte_cdk.sources.concurrent_source.concurrent_source_adapter import ConcurrentSourceAdapter
+from airbyte_cdk.sources.concurrent_source.concurrent_source import ConcurrentSource
+import logging
 
+
+from typing import Any, Iterator, List, Mapping, MutableMapping, Optional, Tuple, Union
+
+import pendulum
+import requests
+from airbyte_cdk import AirbyteLogger
+from airbyte_cdk.logger import AirbyteLogFormatter
+from airbyte_cdk.models import AirbyteMessage, AirbyteStateMessage, ConfiguredAirbyteCatalog, ConfiguredAirbyteStream, Level, SyncMode
+from airbyte_cdk.sources.concurrent_source.concurrent_source import ConcurrentSource
+from airbyte_cdk.sources.concurrent_source.concurrent_source_adapter import ConcurrentSourceAdapter
+from airbyte_cdk.sources.connector_state_manager import ConnectorStateManager
+from airbyte_cdk.sources.message import InMemoryMessageRepository
+from airbyte_cdk.sources.source import TState
+from airbyte_cdk.sources.streams import Stream
+from airbyte_cdk.sources.streams.concurrent.adapters import StreamFacade
+from airbyte_cdk.sources.streams.concurrent.cursor import ConcurrentCursor, CursorField, FinalStateCursor
+from airbyte_cdk.sources.streams.http.auth import TokenAuthenticator
+from airbyte_cdk.sources.utils.schema_helpers import InternalConfig
+from airbyte_cdk.utils.traced_exception import AirbyteTracedException
+from airbyte_protocol.models import FailureType
+from dateutil.relativedelta import relativedelta
+from pendulum.parsing.exceptions import ParserError
+from requests import codes, exceptions  # type: ignore[import]
+from airbyte_cdk.sources.streams.concurrent.state_converters.datetime_stream_state_converter import IsoMillisConcurrentStreamStateConverter
+
+_DEFAULT_CONCURRENCY = 10
+_MAX_CONCURRENCY = 10
+_logger = logging.getLogger("airbyte")
 
 # Basic full refresh stream
 class SurveyMonkeyBaseStream(HttpStream, ABC):
+    state_converter = IsoMillisConcurrentStreamStateConverter()
+
     def __init__(self, name: str, path: str, primary_key: Union[str, List[str]], cursor_field: Optional[str], start_date: Optional[str], **kwargs: Any) -> None:
         self._name = name
         self._path = path
@@ -77,7 +110,7 @@ class SurveyMonkeyBaseStream(HttpStream, ABC):
         and returning an updated state object.
         """
         state_value = max(current_stream_state.get(self.cursor_field, ""), latest_record.get(self._cursor_field, ""))
-        return {self._cursor_field: state_value} 
+        return {self._cursor_field: datetime.datetime.strptime(state_value, "%Y-%m-%dT%H:%M:%SZ").timestamp()} 
 
     def stream_slices(self, stream_state: Mapping[str, Any] = None, **kwargs) -> Iterable[Optional[Mapping[str, any]]]:
         start_ts = datetime.datetime.strptime(self._start_date, "%Y-%m-%dT%H:%M:%SZ").timestamp()
@@ -86,7 +119,7 @@ class SurveyMonkeyBaseStream(HttpStream, ABC):
             yield from []
             return
         for start, end in self.chunk_dates(start_ts, now_ts):
-            yield {"created[gte]": start, "created[lte]": end}
+            yield {"start_date": datetime.datetime.fromtimestamp(start).strftime("%Y-%m-%dT%H:%M:%SZ"), "end_date": datetime.datetime.fromtimestamp(end).strftime("%Y-%m-%dT%H:%M:%SZ")}
 
     def chunk_dates(self, start_date_ts: int, end_date_ts: int) -> Iterable[Tuple[int, int]]:
         step = int(self._slice_range * 24 * 60 * 60)
@@ -98,11 +131,59 @@ class SurveyMonkeyBaseStream(HttpStream, ABC):
 
 
 # Source
-class SourceSurveyMonkeyDemo(AbstractSource):
+class SourceSurveyMonkeyDemo(ConcurrentSourceAdapter):
+    message_repository = InMemoryMessageRepository(Level(AirbyteLogFormatter.level_mapping[_logger.level]))
+
+    def __init__(self, config: Optional[Mapping[str, Any]], state: Optional[Mapping[str, Any]]):
+        if config:
+            concurrency_level = min(config.get("num_workers", _DEFAULT_CONCURRENCY), _MAX_CONCURRENCY)
+        else:
+            concurrency_level = _DEFAULT_CONCURRENCY
+        _logger.info(f"Using concurrent cdk with concurrency level {concurrency_level}")
+        concurrent_source = ConcurrentSource.create(
+            concurrency_level, concurrency_level // 2, _logger, self._slice_logger, self.message_repository
+        )
+        super().__init__(concurrent_source)
+        self._config = config
+        self._state = state
+
     def check_connection(self, logger, config) -> Tuple[bool, any]:
         return True, None
 
     def streams(self, config: Mapping[str, Any]) -> List[Stream]:
         auth = TokenAuthenticator(config["access_token"])
         start_date = config["start_date"]
-        return [SurveyMonkeyBaseStream(name="surveys", path="/v3/surveys", primary_key=None, authenticator=auth, cursor_field="date_modified", start_date=start_date)]
+
+        synchronous_streams = [
+            SurveyMonkeyBaseStream(name="surveys", path="/v3/surveys", primary_key=None, authenticator=auth, cursor_field="date_modified", start_date=start_date)
+        ]
+        state_manager = ConnectorStateManager(stream_instance_map={s.name: s for s in synchronous_streams}, state=self._state)
+
+        configured_streams = []
+
+        for stream in synchronous_streams:
+
+            if stream.cursor_field:
+                cursor_field = CursorField(stream.cursor_field)
+                legacy_state = state_manager.get_stream_state(stream.name, stream.namespace)
+                cursor = ConcurrentCursor(
+                    stream.name,
+                    stream.namespace,
+                    legacy_state,
+                    self.message_repository,
+                    state_manager,
+                    stream.state_converter,
+                    cursor_field,
+                    self._get_slice_boundary_fields(stream, state_manager),
+                    config["start_date"],
+                )
+            configured_streams.append (
+                StreamFacade.create_from_stream(stream,
+                                                self,
+                                                _logger,
+                                                legacy_state,
+                                                cursor)
+                )
+        return configured_streams
+    def _get_slice_boundary_fields(self, stream: Stream, state_manager: ConnectorStateManager) -> Optional[Tuple[str, str]]:
+        return ("start_date", "end_date")
