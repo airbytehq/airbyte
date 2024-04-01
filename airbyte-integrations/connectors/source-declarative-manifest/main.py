f82eae4f10165b8b06dd69e2ad2ac7ee0563a0f8
erohmensing <erohmensing@gmail.com>
2024-04-01 12:14:12 -0500

    move some stuff to run

--- airbyte-integrations/connectors/source-declarative-manifest/main.py
+++ airbyte-integrations/connectors/source-declarative-manifest/main.py
@@ -5,23 +5,12 @@
 import json
 import pkgutil
 import sys
-from typing import List
 
-from airbyte_cdk.connector import BaseConnector
+
 from airbyte_cdk.entrypoint import AirbyteEntrypoint, launch
 from airbyte_cdk.models import ConnectorSpecification, AirbyteMessage, Type
-from airbyte_cdk.sources.declarative.manifest_declarative_source import ManifestDeclarativeSource
 
-
-
-def create_manifest(args: List[str]):
-    config = BaseConnector.read_config(parsed_args.config)
-    if "__injected_declarative_manifest" not in config:
-        raise ValueError(
-            f"Invalid config: `__injected_declarative_manifest` should be provided at the root of the config but config only has keys {list(config.keys())}"
-        )
-    return ManifestDeclarativeSource(config.get("__injected_declarative_manifest"))
-
+from source_declarative_manifest.run import run
 
 if __name__ == "__main__":
     args = sys.argv[1:]
@@ -34,5 +23,4 @@ if __name__ == "__main__":
         message = AirbyteMessage(type=Type.SPEC, spec=spec)
         print(AirbyteEntrypoint.airbyte_message_to_string(message))
     else:
-        source = create_manifest(sys.argv[1:])
-        launch(source, sys.argv[1:])
+        run()
