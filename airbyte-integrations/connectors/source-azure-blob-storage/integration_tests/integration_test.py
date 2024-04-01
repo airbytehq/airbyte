5d681d3bd6529818996ab9d97d5316ec0c3f1da8
Artem Inzhyyants <artem.inzhyyants@gmail.com>
2024-04-01 20:01:31 +0200

    Source Azure Blob Storage: ref tests
    Signed-off-by: Artem Inzhyyants <artem.inzhyyants@gmail.com>

--- airbyte-integrations/connectors/source-azure-blob-storage/integration_tests/integration_test.py
+++ airbyte-integrations/connectors/source-azure-blob-storage/integration_tests/integration_test.py
@@ -2,39 +2,32 @@
 
 from typing import Any, Mapping
 
+import pytest
 from airbyte_cdk.sources.file_based.stream.cursor import DefaultFileBasedCursor
 from airbyte_cdk.test.entrypoint_wrapper import read
 from airbyte_protocol.models import ConfiguredAirbyteCatalog
 from source_azure_blob_storage import Config, SourceAzureBlobStorage, SourceAzureBlobStorageStreamReader
 
 
-def test_read_csv_files(configured_catalog: ConfiguredAirbyteCatalog, config_csv: Mapping[str, Any]):
+@pytest.mark.parametrize(
+    "config",
+    [
+        "config_csv",
+        "config_jsonl",
+    ],
+)
+def test_read_jsonl_files(configured_catalog: ConfiguredAirbyteCatalog, config: Mapping[str, Any], request):
     """Read about 300Mb of raw csv files
     Test 3 streams: 2 * 1_000_000 recs + 1 * 1000 recs
     """
+    config = request.getfixturevalue(config)
     source = SourceAzureBlobStorage(
         SourceAzureBlobStorageStreamReader(),
         spec_class=Config,
         catalog=configured_catalog,
-        config=config_csv,
+        config=config,
         state=None,
         cursor_cls=DefaultFileBasedCursor,
     )
-    output = read(source=source, config=config_csv, catalog=configured_catalog)
-    assert sum(x.state.sourceStats.recordCount for x in output.state_messages) == 2_001_000
-
-
-def test_read_jsonl_files(configured_catalog: ConfiguredAirbyteCatalog, config_jsonl: Mapping[str, Any]):
-    """Read about 300Mb of raw csv files
-    Test 3 streams: 2 * 1_000_000 recs + 1 * 1000 recs
-    """
-    source = SourceAzureBlobStorage(
-        SourceAzureBlobStorageStreamReader(),
-        spec_class=Config,
-        catalog=configured_catalog,
-        config=config_jsonl,
-        state=None,
-        cursor_cls=DefaultFileBasedCursor,
-    )
-    output = read(source=source, config=config_jsonl, catalog=configured_catalog)
-    assert sum(x.state.sourceStats.recordCount for x in output.state_messages) == 2_001_000
+    output = read(source=source, config=config, catalog=configured_catalog)
+    assert sum(x.state.sourceStats.recordCount for x in output.state_messages) == 300
