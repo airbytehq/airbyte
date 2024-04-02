4a4a6dcd800baed6f11cf7038495c27ed25ef302
btkcodedev <btk.codedev@gmail.com>
2024-04-02 23:55:00 +0530

    feat: Migrate config, Migrate to Inline Schema loader, Update manifest

--- airbyte-integrations/connectors/source-asana/source_asana/run.py
+++ airbyte-integrations/connectors/source-asana/source_asana/run.py
@@ -6,9 +6,11 @@
 import sys
 
 from airbyte_cdk.entrypoint import launch
+from .config_migration import AsanaConfigMigration
 from source_asana import SourceAsana
 
 
 def run():
     source = SourceAsana()
+    AsanaConfigMigration.migrate(sys.argv[1:], source)
     launch(source, sys.argv[1:])
