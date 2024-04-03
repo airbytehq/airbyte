fad0737d4e3d1f52defd6c3d154f3f2bd85398b9
Stephane Geneix <147216312+stephane-airbyte@users.noreply.github.com>
2024-04-03 15:28:41 -0700

    enable spotbugs for db-sources submodule (#36705)

--- airbyte-cdk/java/airbyte-cdk/db-sources/src/test/kotlin/io/airbyte/cdk/integrations/source/jdbc/DefaultJdbcSourceAcceptanceTest.kt
+++ airbyte-cdk/java/airbyte-cdk/db-sources/src/test/kotlin/io/airbyte/cdk/integrations/source/jdbc/DefaultJdbcSourceAcceptanceTest.kt
@@ -59,8 +59,8 @@ internal class DefaultJdbcSourceAcceptanceTest :
 
     fun getConfigWithConnectionProperties(
         psqlDb: PostgreSQLContainer<*>,
-        dbName: String?,
-        additionalParameters: String?
+        dbName: String,
+        additionalParameters: String
     ): JsonNode {
         return Jsons.jsonNode(
             ImmutableMap.builder<Any, Any?>()
@@ -68,7 +68,7 @@ internal class DefaultJdbcSourceAcceptanceTest :
                 .put(JdbcUtils.PORT_KEY, resolvePort(psqlDb))
                 .put(JdbcUtils.DATABASE_KEY, dbName)
                 .put(JdbcUtils.SCHEMAS_KEY, List.of(SCHEMA_NAME))
-                .put(JdbcUtils.USERNAME_KEY, psqlDb!!.username)
+                .put(JdbcUtils.USERNAME_KEY, psqlDb.username)
                 .put(JdbcUtils.PASSWORD_KEY, psqlDb.password)
                 .put(JdbcUtils.CONNECTION_PROPERTIES_KEY, additionalParameters)
                 .build()
