e628c0b0ead18492c688542f0de18d7db30bff49
Vadym Ratniuk <midavadim@yahoo.com>
2024-04-10 00:31:47 +0300

    add custom FunnelsSubstreamPartitionRouter to populate funnel_name attribute

--- airbyte-integrations/connectors/source-mixpanel/source_mixpanel/components.py
+++ airbyte-integrations/connectors/source-mixpanel/source_mixpanel/components.py
@@ -197,6 +197,54 @@ class FunnelsDpathExtractor(DpathExtractor):
         return new_records
 
 
+class FunnelsSubstreamPartitionRouter(SubstreamPartitionRouter):
+
+    def stream_slices(self) -> Iterable[StreamSlice]:
+        """
+        Add 'funnel_name' to the slice
+        """
+        if not self.parent_stream_configs:
+            yield from []
+        else:
+            for parent_stream_config in self.parent_stream_configs:
+                parent_stream = parent_stream_config.stream
+                parent_field = parent_stream_config.parent_key.eval(self.config)  # type: ignore # parent_key is always casted to an interpolated string
+                partition_field = parent_stream_config.partition_field.eval(self.config)  # type: ignore # partition_field is always casted to an interpolated string
+                for parent_stream_slice in parent_stream.stream_slices(
+                    sync_mode=SyncMode.full_refresh, cursor_field=None, stream_state=None
+                ):
+                    empty_parent_slice = True
+                    parent_partition = parent_stream_slice.partition if parent_stream_slice else {}
+
+                    for parent_record in parent_stream.read_records(
+                        sync_mode=SyncMode.full_refresh, cursor_field=None, stream_slice=parent_stream_slice, stream_state=None
+                    ):
+                        # Skip non-records (eg AirbyteLogMessage)
+                        if isinstance(parent_record, AirbyteMessage):
+                            if parent_record.type == Type.RECORD:
+                                parent_record = parent_record.record.data
+                            else:
+                                continue
+                        elif isinstance(parent_record, Record):
+                            parent_record = parent_record.data
+                        try:
+                            partition_value = dpath.util.get(parent_record, parent_field)
+                        except KeyError:
+                            pass
+                        else:
+                            empty_parent_slice = False
+                            yield StreamSlice(
+                                partition={
+                                    partition_field: partition_value,
+                                    "funnel_name":  parent_record.get('name'),
+                                    "parent_slice": parent_partition
+                                },
+                                cursor_slice={}
+                            )
+                    # If the parent slice contains no records,
+                    if empty_parent_slice:
+                        yield from []
+
 class EngageDefaultPaginator(DefaultPaginator):
     ...
 
