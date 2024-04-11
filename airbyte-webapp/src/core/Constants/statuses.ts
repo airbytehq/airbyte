a5fe4c9df2dd129a6a3877af5590d37de1369c4c
shoaibejaz <shoaibejazabc@gmail.com>
2024-04-11 15:20:26 +0500

    CU-86ep30xdp - Bug: Chinese language, update plan after unsubscribing, cannot choose original plan

--- airbyte-webapp/src/core/Constants/statuses.ts
+++ airbyte-webapp/src/core/Constants/statuses.ts
@@ -5,9 +5,18 @@ export const PAYMENT_STATUS = {
   Pause_Subscription: "Pause subscription",
   Renewal_Failed: "Renewal failed",
 };
-
+export const CHINESE_PAYMENT_STATUS = {
+  Free_Trial: "免费试用",
+  Subscription: "订阅",
+  Cancel_Subscription: "取消订阅",
+  Pause_Subscription: "暂停订阅",
+  Renewal_Failed: "续期失败",
+};
 export const DASPIRE_PAYMENT_STATUS = Object.values(PAYMENT_STATUS);
-
+export const DASPIRE_CHINESE_PAYMENT_STATUS = Object.values(CHINESE_PAYMENT_STATUS);
 export const getPaymentStatus = (status: number): string => {
   return DASPIRE_PAYMENT_STATUS[status - 1];
+};
+export const getChinesePaymentStatus = (status: number): string => {
+  return DASPIRE_CHINESE_PAYMENT_STATUS[status - 1];
 };
