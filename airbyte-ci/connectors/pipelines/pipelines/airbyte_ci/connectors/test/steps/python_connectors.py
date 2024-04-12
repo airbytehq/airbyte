e744d0ce54b8fb440f60ea49ec4219afb4867ec2
Alexandre Girard <alexandre@airbyte.io>
2024-04-12 09:36:10 -0700

    bump to 0.10.2

--- airbyte-ci/connectors/pipelines/pipelines/airbyte_ci/connectors/test/steps/python_connectors.py
+++ airbyte-ci/connectors/pipelines/pipelines/airbyte_ci/connectors/test/steps/python_connectors.py
@@ -22,7 +22,7 @@ from pipelines.dagger.actions.python.poetry import with_poetry
 from pipelines.helpers.execution.run_steps import STEP_TREE, StepToRun
 from pipelines.models.steps import STEP_PARAMS, Step, StepResult
 
-PYAIRBYTE_VERSION = "0.9.0"
+PYAIRBYTE_VERSION = "0.10.2"
 
 
 class PytestStep(Step, ABC):
