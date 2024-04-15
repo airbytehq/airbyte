0e374b32df4c8e10c482cab4a7e3d25239a8c682
Patrick Nilan <nilan.patrick@gmail.com>
2024-04-15 11:07:53 -0700

    Adds base64 secret key encoding, updates jwt.py returns, adds default values for header properties

--- airbyte-cdk/python/airbyte_cdk/sources/declarative/auth/jwt.py
+++ airbyte-cdk/python/airbyte_cdk/sources/declarative/auth/jwt.py
@@ -2,9 +2,10 @@
 # Copyright (c) 2023 Airbyte, Inc., all rights reserved.
 #
 
+import base64
 from dataclasses import InitVar, dataclass
 from datetime import datetime
-from typing import Any, Mapping, Optional, Union
+from typing import Any, Mapping, Union
 
 import jwt
 from airbyte_cdk.sources.declarative.auth.declarative_authenticator import DeclarativeAuthenticator
@@ -19,9 +20,10 @@ class JwtAuthenticator(DeclarativeAuthenticator):
     parameters: InitVar[Mapping[str, Any]]
     secret_key: Union[InterpolatedString, str]
     algorithm: Union[InterpolatedString, str]
-    token_duration: Union[InterpolatedString, str] = 1200
+    base64_encode_secret_key: bool = False
+    token_duration: int = None
     kid: Union[InterpolatedString, str] = None
-    typ: Union[InterpolatedString, str] = "JWT"
+    typ: Union[InterpolatedString, str] = None
     iss: Union[InterpolatedString, str] = None
     sub: Union[InterpolatedString, str] = None
     aud: Union[InterpolatedString, str] = None
@@ -31,43 +33,49 @@ class JwtAuthenticator(DeclarativeAuthenticator):
 
     def __post_init__(self, parameters: Mapping[str, Any]) -> None:
         super().__init__()
-        self._algorithm = InterpolatedString.create(self.algorithm, parameters=parameters)
         self._secret_key = InterpolatedString.create(self.secret_key, parameters=parameters)
+        self._algorithm = InterpolatedString.create(self.algorithm, parameters=parameters)
+        self._base64_encode_secret_key = self.base64_encode_secret_key
+        self._token_duration = self.token_duration
         self._kid = InterpolatedString.create(self.kid, parameters=parameters)
         self._typ = InterpolatedString.create(self.typ, parameters=parameters)
         self._iss = InterpolatedString.create(self.iss, parameters=parameters)
         self._sub = InterpolatedString.create(self.sub, parameters=parameters)
         self._aud = InterpolatedString.create(self.aud, parameters=parameters)
         self._cty = InterpolatedString.create(self.cty, parameters=parameters)
-        self._token_duration = self.token_duration
         self._additional_jwt_headers = InterpolatedMapping(self.additional_jwt_headers or {}, parameters=parameters)
         self._additional_jwt_payload = InterpolatedMapping(self.additional_jwt_payload or {}, parameters=parameters)
 
     def _get_jwt_headers(self) -> Mapping[str, Any]:
-        headers = {}
-        headers.update(self._additional_jwt_headers.eval(self.config))
+        headers = self._additional_jwt_headers.eval(self.config)
+        if any(prop in headers for prop in ["kid", "alg", "typ", "cty"]):
+            raise ValueError("'kid', 'alg', 'typ', 'cty' are reserved headers and should not be set")
+
         if self._kid:
-            headers["kid"] = f"{self._kid.eval(self.config)}"
+            headers["kid"] = self._kid.eval(self.config)
         if self._algorithm:
             headers["alg"] = self._get_algorithm()
         if self._typ:
-            headers["typ"] = f"{self._typ.eval(self.config)}"
+            headers["typ"] = self._typ.eval(self.config)
         if self._cty:
-            headers["cty"] = f"{self._cty.eval(self.config)}"
+            headers["cty"] = self._cty.eval(self.config)
         return headers
 
     def _get_jwt_payload(self) -> Mapping[str, Any]:
-        payload = {}
         now = int(datetime.now().timestamp())
         exp = now + self._token_duration
         nbf = now
-        payload.update(self._additional_jwt_payload.eval(self.config))
+
+        payload = self._additional_jwt_payload.eval(self.config)
+        if any(prop in payload for prop in ["iss", "sub", "aud", "iat", "exp", "nbf"]):
+            raise ValueError("'iss', 'sub', 'aud', 'iat', 'exp', 'nbf' are reserved properties and should not be set")
+
         if self._iss:
-            payload["iss"] = f"{self._iss.eval(self.config)}"
+            payload["iss"] = self._iss.eval(self.config)
         if self._sub:
-            payload["sub"] = f"{self._sub.eval(self.config)}"
+            payload["sub"] = self._sub.eval(self.config)
         if self._aud:
-            payload["aud"] = f"{self._aud.eval(self.config)}"
+            payload["aud"] = self._aud.eval(self.config)
         payload["iat"] = now
         payload["exp"] = exp
         payload["nbf"] = nbf
@@ -77,15 +85,18 @@ class JwtAuthenticator(DeclarativeAuthenticator):
         algorithm: str = self._algorithm.eval(self.config)
         if not algorithm:
             raise ValueError("Algorithm is required")
-        return f"{algorithm}"
+        return algorithm
 
     def _get_secret_key(self) -> str:
         secret_key: str = self._secret_key.eval(self.config)
         if not secret_key:
             raise ValueError("secret_key is required")
-        return f"{secret_key}"
+        if self._base64_encode_secret_key:
+            secret_key = base64.b64encode(secret_key.encode()).decode()
+        return secret_key
 
     def _get_signed_token(self) -> str:
+        # todo error handling
         return jwt.encode(
             payload=self._get_jwt_payload(),
             key=self._get_secret_key(),
