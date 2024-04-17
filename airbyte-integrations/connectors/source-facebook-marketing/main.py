1560c37d56f073c56b81f29ef9e71d6535cbf6bb
aliaksandr <aliaksandr@voodoo.io>
2024-04-17 14:37:05 +0200

    Facebook: small additional prints to benchmark time execution

--- airbyte-integrations/connectors/source-facebook-marketing/main.py
+++ airbyte-integrations/connectors/source-facebook-marketing/main.py
@@ -9,4 +9,20 @@ from source_facebook_marketing.run import run
 
 
 if __name__ == "__main__":
+
+    from datetime import datetime
+
+    now_start = datetime.now()
+
+    current_time = now_start.strftime("%H:%M:%S")
+    print("Current Time =", current_time)
+
     run()
+
+    now_end = datetime.now()
+
+    current_time = now_end.strftime("%H:%M:%S")
+    print("Current Time =", current_time)
+
+    runtime = now_end - now_start
+    print("Runtime ", runtime.total_seconds())
