bd2a77cfdf70cea42384257d3ece7002a2488f2c
FVidalCarneiro <francisco.carneiro@agicap.com>
2024-04-17 15:25:06 +0200

    feat: Goldcast source connector WIP

---
+++ airbyte-integrations/connectors/source-goldcast-api/source_goldcast_api/run.py
@@ -0,0 +1,13 @@
+#
+# Copyright (c) 2023 Airbyte, Inc., all rights reserved.
+#
+
+
+import sys
+
+from airbyte_cdk.entrypoint import launch
+from .source import SourceGoldcastApi
+
+def run():
+    source = SourceGoldcastApi()
+    launch(source, sys.argv[1:])
