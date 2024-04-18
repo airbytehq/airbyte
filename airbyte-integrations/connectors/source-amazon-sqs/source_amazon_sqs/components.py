4b2da449bf04530036ab475970330b586b3e4bfe
Tope Folorunso <folorunsotemitope10@gmail.com>
2024-04-18 20:09:21 +0100

    fix formatting errors

--- airbyte-integrations/connectors/source-amazon-sqs/source_amazon_sqs/components.py
+++ airbyte-integrations/connectors/source-amazon-sqs/source_amazon_sqs/components.py
@@ -2,13 +2,14 @@
 # Copyright (c) 2024 Airbyte, Inc., all rights reserved.
 #
 
-from dataclasses import dataclass
-from datetime import datetime, timezone
 import hashlib
 import hmac
+from dataclasses import dataclass
+from datetime import datetime, timezone
 from typing import Any, Mapping
-from airbyte_cdk.sources.declarative.types import Config
+
 from airbyte_cdk.sources.declarative.auth.declarative_authenticator import DeclarativeAuthenticator
+from airbyte_cdk.sources.declarative.types import Config
 
 
 @dataclass
@@ -18,38 +19,42 @@ class AmazonSQSAuthenticator(DeclarativeAuthenticator):
     def get_auth_header(self) -> Mapping[str, Any]:
         """The header to set on outgoing HTTP requests"""
 
-        date_time_stamp = datetime.now(timezone.utc).strftime('%Y%m%dT%H%M%SZ')
-        date_stamp      = datetime.now(timezone.utc).strftime('%Y%m%d')
+        date_time_stamp = datetime.now(timezone.utc).strftime("%Y%m%dT%H%M%SZ")
+        date_stamp = datetime.now(timezone.utc).strftime("%Y%m%d")
 
         access_key = self.config.get("access_key")
         secret_key = self.config.get("secret_key")
-        region     = self.config.get("region")
+        region = self.config.get("region")
 
-        service     = 'sqs'
+        service = "sqs"
         service_url = "sqs.amazonaws.com"
 
-        algorithm      = 'AWS4-HMAC-SHA256'
-        signed_headers = 'host;x-amz-date'
+        algorithm = "AWS4-HMAC-SHA256"
+        signed_headers = "host;x-amz-date"
 
-        credential_scope  = f"{date_stamp}/{region}/{service}/aws4_request"
+        credential_scope = f"{date_stamp}/{region}/{service}/aws4_request"
         canonical_headers = f"host:{service_url}\nx-amz-date:{date_time_stamp}\n"
-        payload_hash      = hashlib.sha256(('').encode('utf-8')).hexdigest()
+        payload_hash = hashlib.sha256(("").encode("utf-8")).hexdigest()
         canonical_request = f"GET\n/\n\n{canonical_headers}\n{signed_headers}\n{payload_hash}"
-        string_to_sign = f"{algorithm}\n{date_time_stamp}\n{credential_scope}\n'{hashlib.sha256(canonical_request.encode('utf-8')).hexdigest()}"
+        string_to_sign = (
+            f"{algorithm}\n{date_time_stamp}\n{credential_scope}\n'{hashlib.sha256(canonical_request.encode('utf-8')).hexdigest()}"
+        )
 
         signing_key = self.get_signature_key(secret_key, date_stamp, region, service)
-        signature = hmac.new(signing_key, (string_to_sign).encode('utf-8'), hashlib.sha256).hexdigest()
+        signature = hmac.new(signing_key, (string_to_sign).encode("utf-8"), hashlib.sha256).hexdigest()
 
-        authorization_header = f"{algorithm} Credential={access_key}/{credential_scope}, SignedHeaders={signed_headers}, Signature={signature}"
+        authorization_header = (
+            f"{algorithm} Credential={access_key}/{credential_scope}, SignedHeaders={signed_headers}, Signature={signature}"
+        )
 
-        return {'x-amz-date':date_time_stamp, 'Authorization':authorization_header}
+        return {"x-amz-date": date_time_stamp, "Authorization": authorization_header}
 
     def get_signature_key(self, key, date_stamp, region_name, service_name):
-        k_date = self.sign((f"AWS4{key}").encode('utf-8'), date_stamp)
+        k_date = self.sign((f"AWS4{key}").encode("utf-8"), date_stamp)
         k_region = self.sign(k_date, region_name)
         k_service = self.sign(k_region, service_name)
-        k_signing = self.sign(k_service, 'aws4_request')
-        return k_signing  
-    
+        k_signing = self.sign(k_service, "aws4_request")
+        return k_signing
+
     def sign(self, key, msg):
-        return hmac.new(key, msg.encode('utf-8'), hashlib.sha256).digest()
+        return hmac.new(key, msg.encode("utf-8"), hashlib.sha256).digest()
--- airbyte-integrations/connectors/source-amazon-sqs/source_amazon_sqs/run.py
+++ airbyte-integrations/connectors/source-amazon-sqs/source_amazon_sqs/run.py
@@ -6,6 +6,7 @@
 import sys
 
 from airbyte_cdk.entrypoint import launch
+
 from .source import SourceAmazonSqs
 
 
