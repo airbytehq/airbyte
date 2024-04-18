aeca9448d3431be71df7738a7ab4236ea57052c0
Patrick Nilan <nilan.patrick@gmail.com>
2024-04-18 12:35:54 -0700

    Updates JwtAlgorithm to include all supported algos

--- airbyte-cdk/python/airbyte_cdk/sources/declarative/auth/jwt.py
+++ airbyte-cdk/python/airbyte_cdk/sources/declarative/auth/jwt.py
@@ -19,18 +19,20 @@ class JwtAlgorithm(str):
     Enum for supported JWT algorithms
     """
 
-    HS256 = "HS256"
-    HS384 = "HS384"
-    HS512 = "HS512"
-    RS256 = "RS256"
-    RS384 = "RS384"
-    RS512 = "RS512"
-    ES256 = "ES256"
-    ES384 = "ES384"
-    ES512 = "ES512"
-    PS256 = "PS256"
-    PS384 = "PS384"
-    PS512 = "PS512"
+    HS256 = 'HS256'
+    HS384 = 'HS384'
+    HS512 = 'HS512'
+    ES256 = 'ES256'
+    ES256K = 'ES256K'
+    ES384 = 'ES384'
+    ES512 = 'ES512'
+    RS256 = 'RS256'
+    RS384 = 'RS384'
+    RS512 = 'RS512'
+    PS256 = 'PS256'
+    PS384 = 'PS384'
+    PS512 = 'PS512'
+    EdDSA = 'EdDSA'
 
 
 @dataclass
