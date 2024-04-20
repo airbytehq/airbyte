f493c7b577bb2204ef7d43e9972c9cac18784684
Gireesh Sreepathi <gisripa@gmail.com>
2024-04-19 17:09:35 -0700

    Rename File format related classes to be agnostic of S3 (#37442)

--- airbyte-cdk/java/airbyte-cdk/s3-destinations/src/main/kotlin/io/airbyte/cdk/integrations/destination/s3/jsonl/UploadJsonlFormatConfig.kt
+++ airbyte-cdk/java/airbyte-cdk/s3-destinations/src/main/kotlin/io/airbyte/cdk/integrations/destination/s3/jsonl/UploadJsonlFormatConfig.kt
@@ -1,27 +1,30 @@
 /*
  * Copyright (c) 2023 Airbyte, Inc., all rights reserved.
  */
-package io.airbyte.cdk.integrations.destination.s3.csv
+package io.airbyte.cdk.integrations.destination.s3.jsonl
 
 import com.fasterxml.jackson.databind.JsonNode
+import io.airbyte.cdk.integrations.destination.s3.FileUploadFormat
 import io.airbyte.cdk.integrations.destination.s3.S3DestinationConstants
-import io.airbyte.cdk.integrations.destination.s3.S3Format
-import io.airbyte.cdk.integrations.destination.s3.S3FormatConfig
+import io.airbyte.cdk.integrations.destination.s3.UploadFormatConfig
 import io.airbyte.cdk.integrations.destination.s3.util.CompressionType
 import io.airbyte.cdk.integrations.destination.s3.util.CompressionTypeHelper
 import io.airbyte.cdk.integrations.destination.s3.util.Flattening
 import io.airbyte.cdk.integrations.destination.s3.util.Flattening.Companion.fromValue
 import java.util.*
+import lombok.ToString
 
-class S3CsvFormatConfig(val flattening: Flattening, val compressionType: CompressionType) :
-    S3FormatConfig {
+@ToString
+class UploadJsonlFormatConfig(
+    val flatteningType: Flattening,
+    val compressionType: CompressionType
+) : UploadFormatConfig {
     constructor(
         formatConfig: JsonNode
     ) : this(
-        fromValue(
-            if (formatConfig.has("flattening")) formatConfig["flattening"].asText()
-            else Flattening.NO.value
-        ),
+        if (formatConfig.has(S3DestinationConstants.FLATTENING_ARG_NAME))
+            fromValue(formatConfig[S3DestinationConstants.FLATTENING_ARG_NAME].asText())
+        else Flattening.NO,
         if (formatConfig.has(S3DestinationConstants.COMPRESSION_ARG_NAME))
             CompressionTypeHelper.parseCompressionType(
                 formatConfig[S3DestinationConstants.COMPRESSION_ARG_NAME]
@@ -29,18 +32,9 @@ class S3CsvFormatConfig(val flattening: Flattening, val compressionType: Compres
         else S3DestinationConstants.DEFAULT_COMPRESSION_TYPE
     )
 
-    override val format: S3Format = S3Format.CSV
+    override val format: FileUploadFormat = FileUploadFormat.JSONL
 
-    override val fileExtension: String = CSV_SUFFIX + compressionType.fileExtension
-
-    override fun toString(): String {
-        return "S3CsvFormatConfig{" +
-            "flattening=" +
-            flattening +
-            ", compression=" +
-            compressionType!!.name +
-            '}'
-    }
+    override val fileExtension: String = JSONL_SUFFIX + compressionType.fileExtension
 
     override fun equals(o: Any?): Boolean {
         if (this === o) {
@@ -49,15 +43,15 @@ class S3CsvFormatConfig(val flattening: Flattening, val compressionType: Compres
         if (o == null || javaClass != o.javaClass) {
             return false
         }
-        val that = o as S3CsvFormatConfig
-        return flattening == that.flattening && compressionType == that.compressionType
+        val that = o as UploadJsonlFormatConfig
+        return flatteningType == that.flatteningType && compressionType == that.compressionType
     }
 
     override fun hashCode(): Int {
-        return Objects.hash(flattening, compressionType)
+        return Objects.hash(flatteningType, compressionType)
     }
 
     companion object {
-        const val CSV_SUFFIX: String = ".csv"
+        const val JSONL_SUFFIX: String = ".jsonl"
     }
 }
