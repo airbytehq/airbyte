970422f210010b877bf46eb937432237e155c22c
Anatolii Yatsuk <tolikyatsuk@gmail.com>
2024-04-22 15:42:15 +0300

    Fix format for unit tests

--- airbyte-integrations/connectors/source-zendesk-support/unit_tests/test_components.py
+++ airbyte-integrations/connectors/source-zendesk-support/unit_tests/test_components.py
@@ -1,17 +1,26 @@
-import pytest
 from unittest.mock import MagicMock
+
+import pytest
 import requests
 from airbyte_cdk.sources.declarative.requesters.request_option import RequestOptionType
-from source_zendesk_support.components import ZendeskSupportAuditLogsIncrementalSync, ZendeskSupportExtractorEvents, ZendeskSupportAttributeDefinitionsExtractor
+from source_zendesk_support.components import (
+    ZendeskSupportAttributeDefinitionsExtractor,
+    ZendeskSupportAuditLogsIncrementalSync,
+    ZendeskSupportExtractorEvents,
+)
 
 
 class TestZendeskSupportAuditLogsIncrementalSync:
     @pytest.mark.parametrize(
         "stream_state, stream_slice, next_page_token, expected_params",
         [
-            ({}, {"start_time": "2022-01-01T00:00:00Z", "end_time": "2022-01-02T00:00:00Z"}, {},
-             {"start_time_field": ["2022-01-01T00:00:00Z", "2022-01-02T00:00:00Z"]}),
-            ({}, {}, {}, {})
+            (
+                {},
+                {"start_time": "2022-01-01T00:00:00Z", "end_time": "2022-01-02T00:00:00Z"},
+                {},
+                {"start_time_field": ["2022-01-01T00:00:00Z", "2022-01-02T00:00:00Z"]},
+            ),
+            ({}, {}, {}, {}),
         ],
     )
     def test_get_request_params(self, mocker, stream_state, stream_slice, next_page_token, expected_params):
@@ -32,9 +41,9 @@ class TestZendeskSupportAuditLogsIncrementalSync:
 
         # Patch eval methods to return appropriate field keys
         sync._partition_field_start = mocker.MagicMock()
-        sync._partition_field_start.eval.return_value = 'start_time'
+        sync._partition_field_start.eval.return_value = "start_time"
         sync._partition_field_end = mocker.MagicMock()
-        sync._partition_field_end.eval.return_value = 'end_time'
+        sync._partition_field_end.eval.return_value = "end_time"
 
         # Get the request parameters
         params = sync.get_request_params(stream_state=stream_state, stream_slice=stream_slice, next_page_token=next_page_token)
@@ -48,36 +57,47 @@ class TestZendeskSupportAuditLogsIncrementalSync:
     [
         # Test case with no ticket_events in response
         ({"some_other_data": [{}]}, []),
-
         # Test case with empty ticket_events
         ({"ticket_events": []}, []),
-
         # Test case with ticket_events but no child_events
         ({"ticket_events": [{"via_reference_id": 123, "ticket_id": 456, "timestamp": "2022-01-01T00:00:00Z"}]}, []),
-
         # Test case with valid child_events and Comment event_type
         (
-                {"ticket_events": [
-                    {"via_reference_id": 123, "ticket_id": 456, "timestamp": "2022-01-01T00:00:00Z",
-                     "child_events": [
-                         {"event_type": "Comment", "via_reference_id": "unused", "via": {"some": "data"}}
-                     ]}
-                ]},
-                [{"event_type": "Comment", "via_reference_id": 123, "ticket_id": 456, "timestamp": "2022-01-01T00:00:00Z",
-                  "via": {"some": "data"}}]
+            {
+                "ticket_events": [
+                    {
+                        "via_reference_id": 123,
+                        "ticket_id": 456,
+                        "timestamp": "2022-01-01T00:00:00Z",
+                        "child_events": [{"event_type": "Comment", "via_reference_id": "unused", "via": {"some": "data"}}],
+                    }
+                ]
+            },
+            [
+                {
+                    "event_type": "Comment",
+                    "via_reference_id": 123,
+                    "ticket_id": 456,
+                    "timestamp": "2022-01-01T00:00:00Z",
+                    "via": {"some": "data"},
+                }
+            ],
         ),
-
         # Test case with an invalid 'via' property format
         (
-                {"ticket_events": [
-                    {"via_reference_id": 123, "ticket_id": 456, "timestamp": "2022-01-01T00:00:00Z",
-                     "child_events": [
-                         {"event_type": "Comment", "via_reference_id": "unused", "via": "incorrect_format"}
-                     ]}
-                ]},
-                [{"event_type": "Comment", "via_reference_id": 123, "ticket_id": 456, "timestamp": "2022-01-01T00:00:00Z", "via": None}]
+            {
+                "ticket_events": [
+                    {
+                        "via_reference_id": 123,
+                        "ticket_id": 456,
+                        "timestamp": "2022-01-01T00:00:00Z",
+                        "child_events": [{"event_type": "Comment", "via_reference_id": "unused", "via": "incorrect_format"}],
+                    }
+                ]
+            },
+            [{"event_type": "Comment", "via_reference_id": 123, "ticket_id": 456, "timestamp": "2022-01-01T00:00:00Z", "via": None}],
         ),
-    ]
+    ],
 )
 def test_extract_records(response_data, expected_events):
     # Create an instance of the extractor
@@ -100,29 +120,17 @@ def test_extract_records(response_data, expected_events):
         # Test case with both conditions_all and conditions_any properly filled
         (
             {"definitions": {"conditions_all": [{"id": 1}], "conditions_any": [{"id": 2}]}},
-            [{"id": 1, "condition": "all"}, {"id": 2, "condition": "any"}]
+            [{"id": 1, "condition": "all"}, {"id": 2, "condition": "any"}],
         ),
         # Test case where conditions_all is empty
-        (
-            {"definitions": {"conditions_any": [{"id": 2}], "conditions_all": []}},
-            [{"id": 2, "condition": "any"}]
-        ),
+        ({"definitions": {"conditions_any": [{"id": 2}], "conditions_all": []}}, [{"id": 2, "condition": "any"}]),
         # Test case where conditions_any is empty
-        (
-            {"definitions": {"conditions_all": [{"id": 1}], "conditions_any": []}},
-            [{"id": 1, "condition": "all"}]
-        ),
+        ({"definitions": {"conditions_all": [{"id": 1}], "conditions_any": []}}, [{"id": 1, "condition": "all"}]),
         # Test case where both conditions are empty
-        (
-            {"definitions": {"conditions_all": [], "conditions_any": []}},
-            []
-        ),
+        ({"definitions": {"conditions_all": [], "conditions_any": []}}, []),
         # Test case with malformed JSON (simulate JSONDecodeError)
-        (
-            None,  # This will be used to mock an exception in the response.json() call
-            []
-        ),
-    ]
+        (None, []),  # This will be used to mock an exception in the response.json() call
+    ],
 )
 def test_extract_records(response_data, expected_records):
     # Create an instance of the extractor
