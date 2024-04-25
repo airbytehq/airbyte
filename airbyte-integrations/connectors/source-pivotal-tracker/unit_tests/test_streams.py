e7a9aabf70aa544a8e037acc75bc42f1166c12e5
btkcodedev <btk.codedev@gmail.com>
2024-04-25 23:28:31 +0530

    fix: unit test

--- airbyte-integrations/connectors/source-pivotal-tracker/unit_tests/test_streams.py
+++ airbyte-integrations/connectors/source-pivotal-tracker/unit_tests/test_streams.py
@@ -2,22 +2,15 @@
 # Copyright (c) 2023 Airbyte, Inc., all rights reserved.
 #
 
-import json
-import os
 from typing import Any, Mapping
-
 from airbyte_cdk.models import SyncMode
 from airbyte_cdk.sources.streams import Stream
-from pytest import fixture
 from source_pivotal_tracker.source import SourcePivotalTracker
 
 CONFIG = {
   "api_token": "good"
 }
 stream_slice = {"project_id": 99}
-
-def load_file(fn):
-    return open(os.path.join("unit_tests", "responses", fn)).read()
 
 
 def get_stream_by_name(stream_name: str, config: Mapping[str, Any]) -> Stream:
@@ -28,107 +21,58 @@ def get_stream_by_name(stream_name: str, config: Mapping[str, Any]) -> Stream:
     return matches_by_name[0]
 
 
-projects_mock = json.loads(load_file("projects.json"))
-def test_projects(requests_mock):
+def get_projects_mock(requests_mock, projects_response):
     requests_mock.get(url="https://www.pivotaltracker.com/services/v5/projects", status_code=200)
-    stream = get_stream_by_name("projects", CONFIG)
-    expected_record = dict(projects_mock[0])
-    for json in projects_mock:
+    for json in projects_response:
         requests_mock.get("projects", json=json)
 
-    for stream_slice in stream.stream_slices(sync_mode=SyncMode.full_refresh):
-        for record in stream.read_records(sync_mode=SyncMode.full_refresh, stream_slice=stream_slice):
-            assert dict(record) == expected_record
 
+def set_test_stream(requests_mock, projects_response, stream_name, stream_response, endpoint_suffix):
+    base_url = "https://www.pivotaltracker.com/services/v5/projects/99/"
+    endpoint_url = base_url + endpoint_suffix
+    expected_record = dict(stream_response[0])
 
-project_memberships_mock = json.loads(load_file("project_memberships.json"))
-def test_project_memberships(requests_mock):
-    requests_mock.get(url="https://www.pivotaltracker.com/services/v5/projects", status_code=200)
-    requests_mock.get(url="https://www.pivotaltracker.com/services/v5/projects/99/memberships", status_code=200)
-    stream = get_stream_by_name("project_memberships", CONFIG)
-    expected_record = dict(project_memberships_mock[0])
-    for json in projects_mock:
-        requests_mock.get("project", json=json)
-    for json in project_memberships_mock:
-        requests_mock.get("project_memberships", json=json)
+    requests_mock.get(url=endpoint_url, status_code=200)
+    stream = get_stream_by_name(stream_name, CONFIG)
+    get_projects_mock(requests_mock, projects_response)
+
+    for json_data in stream_response:
+        requests_mock.get(endpoint_url, json=json_data)
 
     for stream_slice in stream.stream_slices(sync_mode=SyncMode.full_refresh):
         for record in stream.read_records(sync_mode=SyncMode.full_refresh, stream_slice=stream_slice):
             assert dict(record) == expected_record
 
 
-activity_mock = json.loads(load_file("activity.json"))
-def test_activity(requests_mock):
-    requests_mock.get(url="https://www.pivotaltracker.com/services/v5/projects", status_code=200)
-    requests_mock.get(url="https://www.pivotaltracker.com/services/v5/projects/99/activity", status_code=200)
-    stream = get_stream_by_name("activity", CONFIG)
-    expected_record = dict(activity_mock[0])
-    for json in projects_mock:
-        requests_mock.get("project", json=json)
-    for json in activity_mock:
-        requests_mock.get("activity", json=json)
-
+def test_projects(requests_mock, projects_response):
+    stream = get_stream_by_name("projects", CONFIG)
+    expected_record = dict(projects_response[0])
+    get_projects_mock(requests_mock, projects_response)
     for stream_slice in stream.stream_slices(sync_mode=SyncMode.full_refresh):
         for record in stream.read_records(sync_mode=SyncMode.full_refresh, stream_slice=stream_slice):
             assert dict(record) == expected_record
 
 
-labels_mock = json.loads(load_file("labels.json"))
-def test_labels(requests_mock):
-    requests_mock.get(url="https://www.pivotaltracker.com/services/v5/projects", status_code=200)
-    requests_mock.get(url="https://www.pivotaltracker.com/services/v5/projects/99/labels", status_code=200)
-    stream = get_stream_by_name("labels", CONFIG)
-    expected_record = dict(labels_mock[0])
-    for json in projects_mock:
-        requests_mock.get("project", json=json)
-    for json in labels_mock:
-        requests_mock.get("labels", json=json)
-    for stream_slice in stream.stream_slices(sync_mode=SyncMode.full_refresh):
-        for record in stream.read_records(sync_mode=SyncMode.full_refresh, stream_slice=stream_slice):
-            count += 1
-            assert dict(record) == expected_record
+def test_project_memberships(requests_mock, projects_response, project_memberships_response):
+    set_test_stream(requests_mock, projects_response, "project_memberships", project_memberships_response, "memberships")
 
 
-releases_mock = json.loads(load_file("releases.json"))
-def test_releases(requests_mock):
-    requests_mock.get(url="https://www.pivotaltracker.com/services/v5/projects", status_code=200)
-    requests_mock.get(url="https://www.pivotaltracker.com/services/v5/projects/99/releases", status_code=200)
-    stream = get_stream_by_name("releases", CONFIG)
-    expected_record = dict(releases_mock[0])
-    for json in projects_mock:
-        requests_mock.get("project", json=json)
-    for json in releases_mock:
-        requests_mock.get("releases", json=json)
-    for stream_slice in stream.stream_slices(sync_mode=SyncMode.full_refresh):
-        for record in stream.read_records(sync_mode=SyncMode.full_refresh, stream_slice=stream_slice):
-            assert dict(record) == expected_record
+def test_activity(requests_mock, projects_response, activity_response):
+    set_test_stream(requests_mock, projects_response, "activity", activity_response, "activity")
 
 
-epics_mock = json.loads(load_file("epics.json"))
-def test_epics(requests_mock):
-    requests_mock.get(url="https://www.pivotaltracker.com/services/v5/projects", status_code=200)
-    requests_mock.get(url="https://www.pivotaltracker.com/services/v5/projects/99/epics", status_code=200)
-    stream = get_stream_by_name("epics", CONFIG)
-    expected_record = dict(epics_mock[0])
-    for json in projects_mock:
-        requests_mock.get("project", json=json)
-    for json in epics_mock:
-        requests_mock.get("epics", json=json)
-    for stream_slice in stream.stream_slices(sync_mode=SyncMode.full_refresh):
-        for record in stream.read_records(sync_mode=SyncMode.full_refresh, stream_slice=stream_slice):
-            assert dict(record) == expected_record
-        
+def test_labels(requests_mock, projects_response, labels_response):
+    set_test_stream(requests_mock, projects_response, "labels", labels_response, "labels")
 
-stories_mock = json.loads(load_file("stories.json"))
-def test_stories(requests_mock):
-    requests_mock.get(url="https://www.pivotaltracker.com/services/v5/projects", status_code=200)
-    requests_mock.get(url="https://www.pivotaltracker.com/services/v5/projects/99/stories", status_code=200)
-    stream = get_stream_by_name("stories", CONFIG)
-    expected_record = dict(stories_mock[0])
-    for json in projects_mock:
-        requests_mock.get("project", json=json)
-    for json in stories_mock:
-        requests_mock.get("stories", json=json)
-    for stream_slice in stream.stream_slices(sync_mode=SyncMode.full_refresh):
-        for record in stream.read_records(sync_mode=SyncMode.full_refresh, stream_slice=stream_slice):
-            assert dict(record) == expected_record
+
+def test_releases(requests_mock, projects_response, releases_response):
+    set_test_stream(requests_mock, projects_response, "releases", releases_response, "releases")
+
+
+def test_epics(requests_mock, projects_response, epics_response):
+    set_test_stream(requests_mock, projects_response, "epics", epics_response, "epics")
+
+
+def test_stories(requests_mock, projects_response, stories_response):
+    set_test_stream(requests_mock, projects_response, "stories", stories_response, "stories")
+
