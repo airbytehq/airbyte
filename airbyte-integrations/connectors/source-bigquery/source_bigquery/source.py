db75c7eecd69f4e336be206272eb0e26d85a2531
Mariam <mariam@FRART0402M.local>
2024-04-25 13:02:55 +0200

    Implement check_connection for SourceBigquery

--- airbyte-integrations/connectors/source-bigquery/source_bigquery/source.py
+++ airbyte-integrations/connectors/source-bigquery/source_bigquery/source.py
@@ -8,6 +8,7 @@ from abc import ABC
 from typing import Any, Iterable, List, Mapping, MutableMapping, Optional, Tuple
 
 import requests
+from requests import codes, exceptions  # type: ignore[import]
 from airbyte_cdk.sources import AbstractSource
 from airbyte_cdk.sources.streams import Stream
 from airbyte_cdk.sources.streams.http import HttpStream, HttpSubStream
@@ -16,6 +17,7 @@ from airbyte_protocol.models import SyncMode
 from airbyte_cdk.models import AirbyteCatalog, AirbyteMessage, AirbyteStateMessage, ConfiguredAirbyteCatalog
 from airbyte_cdk.logger import AirbyteLogger
 
+from .auth import BigqueryAuth
 from .streams import BigqueryDatasets, BigqueryTables, BigqueryStream
 """
 TODO: Most comments in this class are instructive and should be deleted after the source is implemented.
@@ -52,7 +54,27 @@ class SourceBigquery(AbstractSource):
         :param logger:  logger object
         :return Tuple[bool, any]: (True, None) if the input config can be used to connect to the API successfully, (False, error) otherwise.
         """
-        return False, ""
+        try:
+            self._auth = BigqueryAuth(config)
+            # try reading first table from each dataset, to check the connectivity,
+            for dataset in BigqueryDatasets(project_id=config["project_id"], authenticator=self._auth).read_records(sync_mode=SyncMode.full_refresh):
+                dataset_id = dataset.get("datasetReference")["datasetId"]
+                next(BigqueryTables(dataset_id=dataset_id, project_id=config["project_id"], authenticator=self._auth).read_records(sync_mode=SyncMode.full_refresh))
+        except exceptions.HTTPError as error:
+            error_msg = f"An error occurred: {error.response.text}"
+            try:
+                error_data = error.response.json()[0]
+            except (KeyError, requests.exceptions.JSONDecodeError):
+                pass
+            else:
+                error_code = error_data.get("errorCode")
+                if error.response.status_code == codes.FORBIDDEN and error_code == "REQUEST_LIMIT_EXCEEDED":
+                    logger.warn(f"API Call limit is exceeded. Error message: '{error_data.get('message')}'")
+                    error_msg = "API Call limit is exceeded. Make sure that you have enough API allocation for your organization needs or retry later. For more information, see https://developer.salesforce.com/docs/atlas.en-us.salesforce_app_limits_cheatsheet.meta/salesforce_app_limits_cheatsheet/salesforce_app_limits_platform_api.htm"
+            return False, error_msg
+        except Exception as e:
+            return False, str(e)
+        return True, None
 
     def streams(self, config: Mapping[str, Any]) -> List[Stream]:
         """
