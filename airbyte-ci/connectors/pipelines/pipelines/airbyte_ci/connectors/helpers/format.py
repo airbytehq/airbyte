073bf318c087babd1b12a209029f0741f0ffb69a
Brian Leonard <brian.leonard@airbyte.io>
2024-04-26 15:50:22 -0700

    Use repo .prettierrc

--- airbyte-ci/connectors/pipelines/pipelines/airbyte_ci/connectors/helpers/format.py
+++ airbyte-ci/connectors/pipelines/pipelines/airbyte_ci/connectors/helpers/format.py
@@ -1,14 +1,21 @@
+import os
 from pathlib import Path
 import subprocess
 from typing import List
 
+from pipelines.airbyte_ci.connectors.context import ConnectorContext
 
-async def format_prettier(files: List[Path]):
+
+async def format_prettier(context: ConnectorContext, files: List[Path]):
     if len(files) == 0:
         return
 
+    repo_root_path = Path(os.path.abspath(os.path.join(context.connector.metadata_file_path, "../../../..")))
+    config_path = repo_root_path / ".prettierrc"
+    if not config_path.exists():
+        raise Exception(f"Prettier config file not found: {config_path}")
+
     to_format = [str(file) for file in files]
-    config_path = Path(__file__).parent / ".prettierrc"
 
     print(f"       Formatting files: npx prettier --write {' '.join(to_format)}")
     command = ["npx", "prettier", "--config", str(config_path), "--write"] + to_format
