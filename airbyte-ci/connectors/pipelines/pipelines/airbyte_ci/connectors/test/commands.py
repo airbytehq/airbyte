fec0e01010e1b04f5636a8b4526514ddeb27dadb
Catherine Noll <noll.catherine@gmail.com>
2024-04-29 22:00:30 -0400

    Don't skip regression tests

--- airbyte-ci/connectors/pipelines/pipelines/airbyte_ci/connectors/test/commands.py
+++ airbyte-ci/connectors/pipelines/pipelines/airbyte_ci/connectors/test/commands.py
@@ -90,7 +90,7 @@ async def test(
         raise click.UsageError("Cannot use both --only-step and --skip-step at the same time.")
     if not only_steps:
         skip_steps = list(skip_steps)
-        skip_steps += [CONNECTOR_TEST_STEP_ID.REGRESSION_TEST]
+        skip_steps += [CONNECTOR_TEST_STEP_ID.CONNECTOR_REGRESSION_TESTS]
     if ctx.obj["is_ci"]:
         fail_if_missing_docker_hub_creds(ctx)
 
