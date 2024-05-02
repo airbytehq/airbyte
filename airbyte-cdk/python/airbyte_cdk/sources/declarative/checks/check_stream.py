ff653c43a602fc899c5aaa330536ff8c223889b4
Natik Gadzhi <natik@respawn.io>
2024-05-01 20:45:59 -0700

    Apply suggestions from code review

--- airbyte-cdk/python/airbyte_cdk/sources/declarative/checks/check_stream.py
+++ airbyte-cdk/python/airbyte_cdk/sources/declarative/checks/check_stream.py
@@ -8,7 +8,7 @@ from dataclasses import InitVar, dataclass
 from typing import Any, List, Mapping, Tuple
 
 from airbyte_cdk.sources.declarative.checks.connection_checker import ConnectionChecker
-from airbyte_cdk.sources.declarative.declarative_source import DeclarativeSource
+from airbyte_cdk.sources.source import Source
 from airbyte_cdk.sources.streams.http.availability_strategy import HttpAvailabilityStrategy
 
 
@@ -27,7 +27,7 @@ class CheckStream(ConnectionChecker):
     def __post_init__(self, parameters: Mapping[str, Any]) -> None:
         self._parameters = parameters
 
-    def check_connection(self, source: DeclarativeSource, logger: logging.Logger, config: Mapping[str, Any]) -> Tuple[bool, Any]:
+    def check_connection(self, source: Source, logger: logging.Logger, config: Mapping[str, Any]) -> Tuple[bool, Any]:
         streams = source.streams(config)
         stream_name_to_stream = {s.name: s for s in streams}
         if len(streams) == 0:
