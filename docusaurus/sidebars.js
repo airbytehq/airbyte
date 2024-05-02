0d076ff65f5c1371797356f93c1c335bd274f373
Natalie Kwong <38087517+nataliekwong@users.noreply.github.com>
2024-05-02 10:50:25 +0000

    april24

--- docusaurus/sidebars.js
+++ docusaurus/sidebars.js
@@ -606,6 +606,7 @@ module.exports = {
         type: "generated-index",
       },
       items: [
+        "release_notes/april_2024",
         "release_notes/march_2024",
         "release_notes/february_2024",
         "release_notes/january_2024",
