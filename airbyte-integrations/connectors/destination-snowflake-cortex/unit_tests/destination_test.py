830ec1429f64a78ec6f0bc7c0b3955f84b63ae70
bindipankhudi <bindi@airbyte.io>
2024-05-06 17:39:51 -0700

    added spec unit test

--- airbyte-integrations/connectors/destination-snowflake-cortex/unit_tests/destination_test.py
+++ airbyte-integrations/connectors/destination-snowflake-cortex/unit_tests/destination_test.py
@@ -4,9 +4,34 @@
 
 from airbyte_cdk.models import ConnectorSpecification
 from destination_snowflake_cortex.destination import DestinationSnowflakeCortex
+import unittest
+from unittest.mock import MagicMock, Mock, patch
 
-#def test_spec(self):
-#    destination = DestinationSnowflakeCortex()
-#    result = destination.spec()
-#
-#    self.assertIsInstance(result, ConnectorSpecification)
+from airbyte_cdk import AirbyteLogger
+from airbyte_cdk.models import ConnectorSpecification, Status
+from destination_snowflake_cortex.config import ConfigModel
+from destination_snowflake_cortex.destination import DestinationSnowflakeCortex
+
+
+class TestDestinationSnowflakeCortex(unittest.TestCase):
+    def setUp(self):
+        self.config = {
+            "processing": {"text_fields": ["str_col"], "metadata_fields": [], "chunk_size": 1000},
+            "embedding": {"mode": "openai", "openai_key": "mykey"},
+            "indexing": {
+                "account": "MYACCOUNT",
+                "username": "MYUSERNAME",
+                "password": "xxxxxxx",
+                "database": "MYDATABASE",
+                "warehouse": "MYWAREHOUSE",
+                "role": "ACCOUNTADMIN"
+            },
+        }
+        self.config_model = ConfigModel.parse_obj(self.config)
+        self.logger = AirbyteLogger()
+
+    def test_spec(self):
+        destination = DestinationSnowflakeCortex()
+        result = destination.spec()
+
+        self.assertIsInstance(result, ConnectorSpecification)
