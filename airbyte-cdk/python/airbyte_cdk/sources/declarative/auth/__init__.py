bd4fe768e264c1dcfd8f46b3844a90b60da995b6
Natik Gadzhi <natik@respawn.io>
2024-05-08 14:12:52 +0400

    Revert "isort & black for **/__init__.py"
    This reverts commit d59ddc494a878666ba05f34ed4c906a3d043d22c.

--- airbyte-cdk/python/airbyte_cdk/sources/declarative/auth/__init__.py
+++ airbyte-cdk/python/airbyte_cdk/sources/declarative/auth/__init__.py
@@ -2,7 +2,10 @@
 # Copyright (c) 2023 Airbyte, Inc., all rights reserved.
 #
 
-from airbyte_cdk.sources.declarative.auth.jwt import JwtAuthenticator
 from airbyte_cdk.sources.declarative.auth.oauth import DeclarativeOauth2Authenticator
+from airbyte_cdk.sources.declarative.auth.jwt import JwtAuthenticator
 
-__all__ = ["DeclarativeOauth2Authenticator", "JwtAuthenticator"]
+__all__ = [
+    "DeclarativeOauth2Authenticator",
+    "JwtAuthenticator"
+]
--- airbyte-integrations/connectors/source-amazon-ads/source_amazon_ads/schemas/__init__.py
+++ airbyte-integrations/connectors/source-amazon-ads/source_amazon_ads/schemas/__init__.py
@@ -2,9 +2,18 @@
 # Copyright (c) 2023 Airbyte, Inc., all rights reserved.
 #
 from .attribution_report import AttributionReportModel
-from .common import CatalogModel, Keywords, MetricsReport, NegativeKeywords, Portfolio
+from .common import (
+    CatalogModel,
+    Keywords,
+    MetricsReport,
+    NegativeKeywords,
+    Portfolio
+)
 from .profile import Profile
-from .sponsored_brands import BrandsAdGroup, BrandsCampaign
+from .sponsored_brands import (
+    BrandsAdGroup,
+    BrandsCampaign,
+)
 from .sponsored_display import DisplayAdGroup, DisplayBudgetRules, DisplayCampaign, DisplayCreatives, DisplayProductAds, DisplayTargeting
 from .sponsored_products import (
     ProductAd,
@@ -15,7 +24,7 @@ from .sponsored_products import (
     ProductTargeting,
     SponsoredProductCampaignNegativeKeywordsModel,
     SponsoredProductKeywordsModel,
-    SponsoredProductNegativeKeywordsModel,
+    SponsoredProductNegativeKeywordsModel
 )
 
 __all__ = [
@@ -43,5 +52,5 @@ __all__ = [
     "AttributionReportModel",
     "SponsoredProductCampaignNegativeKeywordsModel",
     "SponsoredProductKeywordsModel",
-    "SponsoredProductNegativeKeywordsModel",
+    "SponsoredProductNegativeKeywordsModel"
 ]
