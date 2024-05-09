66898dcbdd2425ae2296f7eb38ca413480fc4b69
Erick Corona <erick.corona@airbyte.io>
2024-05-08 18:10:27 -0600

    Fix data graphql end date

--- airbyte-integrations/connectors/source-shopify/unit_tests/integration/test_bulk_stream.py
+++ airbyte-integrations/connectors/source-shopify/unit_tests/integration/test_bulk_stream.py
@@ -16,11 +16,13 @@ import pendulum as pdm
 
 
 _JOB_START_DATE = pdm.parse("2024-05-05T00:00:00+00:00")
-_JOB_END_DATE = _JOB_START_DATE.add(hours=3)
+_JOB_END_DATE = _JOB_START_DATE.add(hours=2, minutes=24)
+
 _URL_ACCESS_SCOPES = "https://airbyte-integration-test.myshopify.com/admin/oauth/access_scopes.json"
 _URL_SHOP = "https://airbyte-integration-test.myshopify.com/admin/api/2023-07/shop.json"
 _URL_GRAPHQL = "https://airbyte-integration-test.myshopify.com/admin/api/2023-07/graphql.json"
 _URL_RECORDS_FILE = "https://storage.googleapis.com/shopify-tiers-assets-prod-us-east1/bulk-operation-outputs/l6lersgk4i81iqc3n6iisywwtipb-final"
+
 _BULK_STREAM = "metafield_orders"
 
 
@@ -53,7 +55,7 @@ def _get_shop_request() -> HttpRequest:
 def _get_data_graphql_request() -> HttpRequest:
     return HttpRequest(
         url=_URL_GRAPHQL,
-        body='{"query": "mutation {\\n                bulkOperationRunQuery(\\n                    query: \\"\\"\\"\\n                     {\\n  orders(\\n    query: \\"updated_at:>=\'2024-05-05T00:00:00+00:00\' AND updated_at:<=\'2024-05-05T03:00:00+00:00\'\\"\\n    sortKey: UPDATED_AT\\n  ) {\\n    edges {\\n      node {\\n        __typename\\n        id\\n        metafields {\\n          edges {\\n            node {\\n              __typename\\n              id\\n              namespace\\n              value\\n              key\\n              description\\n              createdAt\\n              updatedAt\\n              type\\n            }\\n          }\\n        }\\n      }\\n    }\\n  }\\n}\\n                    \\"\\"\\"\\n                ) {\\n                    bulkOperation {\\n                        id\\n                        status\\n                        createdAt\\n                    }\\n                    userErrors {\\n                        field\\n                        message\\n                    }\\n                }\\n            }"}'
+        body='{"query": "mutation {\\n                bulkOperationRunQuery(\\n                    query: \\"\\"\\"\\n                     {\\n  orders(\\n    query: \\"updated_at:>=\'2024-05-05T00:00:00+00:00\' AND updated_at:<=\'2024-05-05T02:24:00+00:00\'\\"\\n    sortKey: UPDATED_AT\\n  ) {\\n    edges {\\n      node {\\n        __typename\\n        id\\n        metafields {\\n          edges {\\n            node {\\n              __typename\\n              id\\n              namespace\\n              value\\n              key\\n              description\\n              createdAt\\n              updatedAt\\n              type\\n            }\\n          }\\n        }\\n      }\\n    }\\n  }\\n}\\n                    \\"\\"\\"\\n                ) {\\n                    bulkOperation {\\n                        id\\n                        status\\n                        createdAt\\n                    }\\n                    userErrors {\\n                        field\\n                        message\\n                    }\\n                }\\n            }"}'
     )
 
 
@@ -117,7 +119,8 @@ def test_read_graphql_records_successfully():
         catalog = CatalogBuilder().with_stream(_BULK_STREAM, SyncMode.full_refresh).build()
         output = read(SourceShopify(), _get_config(_JOB_START_DATE.to_date_string()), catalog)
 
-        assert output.records
+        assert output.errors == []
+        assert len(output.records) == 2
 
 
 def _mock_failing_read_requests(http_mocker: HttpMocker):
@@ -132,11 +135,17 @@ def _mock_failing_read_requests(http_mocker: HttpMocker):
         HttpResponse(get_scopes_response())
     )
     inner_mocker = http_mocker.__getattribute__("_mocker")
+
+    def raise_connectio_error(request, context):
+        raise ConnectionError("ConnectionError")
+
+    # Mock the first GraphQL request to fail with a ConnectionError, and then succeed in the next call.
     inner_mocker.register_uri(
         "POST",
         _URL_GRAPHQL,
-        [{"status_code": 104}, {"text": get_data_graphql_response(), "status_code": 200}],
+        [{"text": raise_connectio_error}, {"text": get_data_graphql_response(), "status_code": 200}],
     )
+
     http_mocker.post(
         _get_status_graphql_request(),
         HttpResponse(get_status_graphql_response())
@@ -150,16 +159,12 @@ def _mock_failing_read_requests(http_mocker: HttpMocker):
 @freeze_time(_JOB_END_DATE)
 def test_check_for_errors_with_connection_error() -> None:
     with HttpMocker() as http_mocker:
-
         _mock_failing_read_requests(http_mocker)
 
         catalog = CatalogBuilder().with_stream(_BULK_STREAM, SyncMode.full_refresh).build()
         output = read(SourceShopify(), _get_config(_JOB_START_DATE.to_date_string()), catalog)
 
-        inner_mocker = http_mocker.__getattribute__("_mocker")
-        print(f"Call Count: {inner_mocker.call_count}")
-        print(f"IsCalled: {inner_mocker.called}")
-        print(f"Mocker history: {inner_mocker.request_history}")
-        print(output.errors)
-        # assert "ConnectionAbortedError" in output.errors.__str__()
-        assert output.records
+        assert "ConnectionError" in output.errors.__str__()
+
+        # TODO: We should be able to read records once the retry logic is implemented in HTTPClient.
+        # assert output.records == 2
