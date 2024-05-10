bae39f433f9bacca41dbcc17c15e81cab1e02a54
Ben Church <ben@airbyte.io>
2024-05-10 15:01:41 -0700

    update(registry): Bump to dagster-cloud 1.7.5 and use serverless

--- airbyte-ci/connectors/pipelines/pipelines/airbyte_ci/metadata/pipeline.py
+++ airbyte-ci/connectors/pipelines/pipelines/airbyte_ci/metadata/pipeline.py
@@ -125,7 +125,7 @@ class DeployOrchestrator(Step):
         # mount metadata_service/lib and metadata_service/orchestrator
         parent_dir = self.context.get_repo_dir("airbyte-ci/connectors/metadata_service")
         python_base = with_python_base(self.context, "3.9")
-        python_with_dependencies = with_pip_packages(python_base, ["dagster-cloud==1.5.14", "poetry2setup==1.1.0"])
+        python_with_dependencies = with_pip_packages(python_base, ["'dagster-cloud[serverless]'==1.7.5", "poetry2setup==1.1.0"])
         dagster_cloud_api_token_secret: dagger.Secret = get_secret_host_variable(
             self.context.dagger_client, "DAGSTER_CLOUD_METADATA_API_TOKEN"
         )
