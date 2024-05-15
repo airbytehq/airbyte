a2aa15bbf1990b034757a6cfdfaf065a47fa5be4
Dhroov Makwana <pabloescoder@gmail.com>
2024-05-15 11:09:21 +0530

    Use Poetry, use inline schema, remove parameter macro, make builder compatible

--- airbyte-integrations/connectors/source-freshcaller/source_freshcaller/run.py
+++ airbyte-integrations/connectors/source-freshcaller/source_freshcaller/run.py
@@ -1,12 +1,13 @@
 #
-# Copyright (c) 2023 Airbyte, Inc., all rights reserved.
+# Copyright (c) 2024 Airbyte, Inc., all rights reserved.
 #
 
 
 import sys
 
 from airbyte_cdk.entrypoint import launch
-from source_freshcaller import SourceFreshcaller
+
+from .source import SourceFreshcaller
 
 
 def run():
