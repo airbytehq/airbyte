16b8e23d92b5517c1518eae47e11dd2486d95f85
Anatolii Yatsuk <tolikyatsuk@gmail.com>
2024-05-15 15:03:43 +0300

    Fix other partition routers

--- airbyte-cdk/python/airbyte_cdk/sources/declarative/partition_routers/single_partition_router.py
+++ airbyte-cdk/python/airbyte_cdk/sources/declarative/partition_routers/single_partition_router.py
@@ -5,12 +5,12 @@
 from dataclasses import InitVar, dataclass
 from typing import Any, Iterable, Mapping, Optional
 
-from airbyte_cdk.sources.declarative.stream_slicers.stream_slicer import StreamSlicer
+from airbyte_cdk.sources.declarative.partition_routers.partition_router import PartitionRouter
 from airbyte_cdk.sources.types import StreamSlice, StreamState
 
 
 @dataclass
-class SinglePartitionRouter(StreamSlicer):
+class SinglePartitionRouter(PartitionRouter):
     """Partition router returning only a stream slice"""
 
     parameters: InitVar[Mapping[str, Any]]
