4c1a3214ab218b4d4de116a1c04211d0aacb5770
btkcodedev <btk.codedev@gmail.com>
2024-05-15 21:37:35 +0530

    make compatible with builder

--- airbyte-integrations/connectors/source-activecampaign/source_activecampaign/run.py
+++ airbyte-integrations/connectors/source-activecampaign/source_activecampaign/run.py
@@ -1,13 +1,12 @@
 #
-# Copyright (c) 2023 Airbyte, Inc., all rights reserved.
+# Copyright (c) 2024 Airbyte, Inc., all rights reserved.
 #
 
 
 import sys
 
 from airbyte_cdk.entrypoint import launch
-from source_activecampaign import SourceActivecampaign
-
+from .source import SourceActivecampaign
 
 def run():
     source = SourceActivecampaign()
