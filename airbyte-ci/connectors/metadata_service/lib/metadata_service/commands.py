cbe46539761f97a564222f95b6f16c8afd28042a
erohmensing <erohmensing@gmail.com>
2024-05-16 11:44:05 -0500

    update tests do reflect that we now log when we don't upload things

--- airbyte-ci/connectors/metadata_service/lib/metadata_service/commands.py
+++ airbyte-ci/connectors/metadata_service/lib/metadata_service/commands.py
@@ -58,10 +58,9 @@ def upload(metadata_file_path: pathlib.Path, docs_path: pathlib.Path, bucket_nam
         upload_info = upload_metadata_to_gcs(bucket_name, metadata_file_path, validator_opts)
         log_metadata_upload_info(upload_info)
     except (ValidationError, FileNotFoundError) as e:
-        click.secho(f"The metadata file could not be uploaded: {str(e)}", color="red")
+        click.secho(f"The metadata file could not be uploaded: {str(e)}", fg="red")
         exit(1)
     if upload_info.metadata_uploaded:
         exit(0)
     else:
-        click.secho(f"The metadata file {metadata_file_path} was not uploaded.", color="yellow")
         exit(5)
