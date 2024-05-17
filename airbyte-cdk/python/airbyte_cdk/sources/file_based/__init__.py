4eda1ba6c2c0511dc91226325646522952576318
Alexandre Girard <alexandre@airbyte.io>
2024-05-16 19:55:03 -0700

    fix imports

--- airbyte-cdk/python/airbyte_cdk/sources/file_based/__init__.py
+++ airbyte-cdk/python/airbyte_cdk/sources/file_based/__init__.py
@@ -1,12 +1,12 @@
-from .file_based.config.abstract_file_based_spec import AbstractFileBasedSpec
-from .file_based.config.csv_format import CsvFormat
-from .file_based.config.file_based_stream_config import FileBasedStreamConfig
-from .file_based.config.jsonl_format import JsonlFormat
-from .file_based.exceptions import CustomFileBasedException, ErrorListingFiles, FileBasedSourceError
-from .file_based.file_based_source import DEFAULT_CONCURRENCY, FileBasedSource
-from .file_based.file_based_stream_reader import AbstractFileBasedStreamReader, FileReadMode
-from .file_based.remote_file import RemoteFile
-from .file_based.stream.cursor import DefaultFileBasedCursor
+from .config.abstract_file_based_spec import AbstractFileBasedSpec
+from .config.csv_format import CsvFormat
+from .config.file_based_stream_config import FileBasedStreamConfig
+from .config.jsonl_format import JsonlFormat
+from .exceptions import CustomFileBasedException, ErrorListingFiles, FileBasedSourceError
+from .file_based_source import DEFAULT_CONCURRENCY, FileBasedSource
+from .file_based_stream_reader import AbstractFileBasedStreamReader, FileReadMode
+from .remote_file import RemoteFile
+from .stream.cursor import DefaultFileBasedCursor
 
 __all__ = [
     "AbstractFileBasedSpec",
