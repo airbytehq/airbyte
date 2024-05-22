5ee19600526a5e6cc288b7b475b64252e0e1ddcc
darynaishchenko <darina.ishchenko17@gmail.com>
2024-05-22 14:33:28 +0300

    updated custom components, default start date, check stream, discovered streams

--- airbyte-integrations/connectors/source-tiktok-marketing/source_tiktok_marketing/components/advertiser_ids_partition_router.py
+++ airbyte-integrations/connectors/source-tiktok-marketing/source_tiktok_marketing/components/advertiser_ids_partition_router.py
@@ -1,18 +1,49 @@
 # Copyright (c) 2024 Airbyte, Inc., all rights reserved.
 
 import json
-from typing import Iterable
+from typing import Any, Iterable, Mapping
 
-from airbyte_cdk.sources.declarative.partition_routers import SubstreamPartitionRouter
+import dpath.util
+from airbyte_cdk.sources.declarative.partition_routers.substream_partition_router import SubstreamPartitionRouter
 from airbyte_cdk.sources.declarative.types import StreamSlice
 
 
-class AdvertiserIdsPartitionRouter(SubstreamPartitionRouter):
+class MultipleAdvertiserIdsPartitionRouter(SubstreamPartitionRouter):
+    """
+    Custom AdvertiserIdsPartitionRouter and AdvertiserIdPartitionRouter partition routers are used to get advertiser_ids
+    as slices for streams where it uses as request param.
+
+    When user uses sandbox account it's impossible to get advertiser_ids via API.
+    In this case user need to provide advertiser_id in a config and connector need to use provided ids
+    and do not make requests to get this id.
+
+    When advertiser_id not provided components get slices as usual.
+    Main difference between AdvertiserIdsPartitionRouter and AdvertiserIdPartitionRouter is
+    that AdvertiserIdPartitionRouter returns multiple advertiser_ids in a one slice when id is not provided,
+    e.g. {"advertiser_ids": '["11111111", "22222222"]', "parent_slice": {}}.
+    And AdvertiserIdPartitionRouter returns single slice for every advertiser_id as usual.
+
+    path_in_config: List[List[str]]: path to value in the config in priority order.
+    partition_field: str: field to insert partition value.
+    """
+
+    def __post_init__(self, parameters: Mapping[str, Any]) -> None:
+        super().__post_init__(parameters)
+        self._path_to_partition_in_config = self._parameters["path_in_config"]
+        self._partition_field = self._parameters["partition_field"]
+
+    def get_partition_value_from_config(self) -> str:
+        for path in self._path_to_partition_in_config:
+            config_value = dpath.util.get(self.config, path, default=None)
+            if config_value:
+                return config_value
+
     def stream_slices(self) -> Iterable[StreamSlice]:
-        if self.config.get("credentials", {}).get("advertiser_id"):
-            slices = [self.config["credentials"].get("advertiser_id")]
+        partition_value_in_config = self.get_partition_value_from_config()
+        if partition_value_in_config:
+            slices = [partition_value_in_config]
         else:
-            slices = [_id.partition["advertiser_ids"] for _id in super().stream_slices()]
+            slices = [_id.partition[self._partition_field] for _id in super().stream_slices()]
 
         start, end, step = 0, len(slices), 100
 
@@ -20,11 +51,11 @@ class AdvertiserIdsPartitionRouter(SubstreamPartitionRouter):
             yield StreamSlice(partition={"advertiser_ids": json.dumps(slices[i : min(end, i + step)]), "parent_slice": {}}, cursor_slice={})
 
 
-class AdvertiserIdPartitionRouter(SubstreamPartitionRouter):
+class SingleAdvertiserIdPartitionRouter(MultipleAdvertiserIdsPartitionRouter):
     def stream_slices(self) -> Iterable[StreamSlice]:
-        if self.config.get("credentials", {}).get("advertiser_id"):
-            yield StreamSlice(
-                partition={"advertiser_id": self.config["credentials"].get("advertiser_id"), "parent_slice": {}}, cursor_slice={}
-            )
+        partition_value_in_config = self.get_partition_value_from_config()
+
+        if partition_value_in_config:
+            yield StreamSlice(partition={self._partition_field: partition_value_in_config, "parent_slice": {}}, cursor_slice={})
         else:
-            yield from super().stream_slices()
+            yield from super(MultipleAdvertiserIdsPartitionRouter, self).stream_slices()
--- airbyte-integrations/connectors/source-tiktok-marketing/source_tiktok_marketing/components/semi_incremental_record_filter.py
+++ airbyte-integrations/connectors/source-tiktok-marketing/source_tiktok_marketing/components/semi_incremental_record_filter.py
@@ -7,6 +7,23 @@ from airbyte_cdk.sources.declarative.types import StreamSlice, StreamState
 
 
 class PerPartitionRecordFilter(RecordFilter):
+
+    """
+    Prepares per partition stream state to be used in the Record Filter condition.
+    Gets current stream state cursor value for stream slice and passes it to condition.
+
+    From
+     {"states": [{"partition": {"advertiser_id": 1, "parent_slice": {}}, "cursor": {"start_time": "2023-12-31"}}]}
+    To
+     {"start_time": "2023-12-31"}.
+
+    partition_field:str: Used to get partition value from current slice.
+    """
+
+    def __post_init__(self, parameters: Mapping[str, Any]) -> None:
+        super().__post_init__(parameters)
+        self._partition_field = parameters["partition_field"]
+
     def filter_records(
         self,
         records: List[Mapping[str, Any]],
@@ -14,7 +31,12 @@ class PerPartitionRecordFilter(RecordFilter):
         stream_slice: Optional[StreamSlice] = None,
         next_page_token: Optional[Mapping[str, Any]] = None,
     ) -> List[Mapping[str, Any]]:
-        stream_states = [p["cursor"] for p in stream_state["states"] if p["partition"]["advertiser_id"] == stream_slice["advertiser_id"]]
+        stream_states = None
+        if stream_state:
+            stream_states = [
+                p["cursor"] for p in stream_state["states"] if p["partition"][self._partition_field] == stream_slice[self._partition_field]
+            ]
+
         if not stream_states:
             stream_state = {}
         else:
