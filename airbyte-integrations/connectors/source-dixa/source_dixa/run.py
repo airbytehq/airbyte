aa2dcb493e4625aaf01f1fe14680c2a2cfd3a24b
btkcodedev <btk.codedev@gmail.com>
2024-05-23 13:23:56 +0530

    make connector compatible with builder

--- airbyte-integrations/connectors/source-dixa/source_dixa/run.py
+++ airbyte-integrations/connectors/source-dixa/source_dixa/run.py
@@ -1,13 +1,12 @@
 #
-# Copyright (c) 2023 Airbyte, Inc., all rights reserved.
+# Copyright (c) 2024 Airbyte, Inc., all rights reserved.
 #
 
 
 import sys
 
 from airbyte_cdk.entrypoint import launch
-from source_dixa import SourceDixa
-
+from .source import SourceDixa
 
 def run():
     source = SourceDixa()
