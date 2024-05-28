ff270891aa710fcf72104561adf926157bfbf8be
btkcodedev <btk.codedev@gmail.com>
2024-05-28 09:29:20 +0530

    make compatible with builder

--- airbyte-integrations/connectors/source-serpstat/source_serpstat/run.py
+++ airbyte-integrations/connectors/source-serpstat/source_serpstat/run.py
@@ -1,13 +1,12 @@
 #
-# Copyright (c) 2023 Airbyte, Inc., all rights reserved.
+# Copyright (c) 2024 Airbyte, Inc., all rights reserved.
 #
 
 
 import sys
 
 from airbyte_cdk.entrypoint import launch
-from source_serpstat import SourceSerpstat
-
+from .source import SourceSerpstat
 
 def run():
     source = SourceSerpstat()
