0d28d8d9342f597df30e5669ff5182a8953eda9c
alafanechere <augustin.lafanechere@gmail.com>
2024-05-29 16:47:52 +0200

    fix type check error

--- airbyte-ci/connectors/connectors_qa/src/connectors_qa/checks/testing.py
+++ airbyte-ci/connectors/connectors_qa/src/connectors_qa/checks/testing.py
@@ -3,7 +3,7 @@
 
 from connector_ops.utils import Connector  # type: ignore
 from connectors_qa.models import Check, CheckCategory, CheckResult
-from pydash.collections import find # type: ignore
+from pydash.collections import find  # type: ignore
 
 
 class TestingCheck(Check):
