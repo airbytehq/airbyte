7a69e7d30aa15537e3b28af6af04d3056751ac14
Dhroov Makwana <pabloescoder@gmail.com>
2024-05-30 04:16:19 +0530

    Use inline spec & schema, remove parameters, use Poetry, make builder compatible

--- airbyte-integrations/connectors/source-breezometer/source_breezometer/run.py
+++ airbyte-integrations/connectors/source-breezometer/source_breezometer/run.py
@@ -1,12 +1,13 @@
 #
-# Copyright (c) 2023 Airbyte, Inc., all rights reserved.
+# Copyright (c) 2024 Airbyte, Inc., all rights reserved.
 #
 
 
 import sys
 
 from airbyte_cdk.entrypoint import launch
-from source_breezometer import SourceBreezometer
+
+from .source import SourceBreezometer
 
 
 def run():
