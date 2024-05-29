5c629d8434f343769ee9bb73e371bc66533ede91
Patrick Nilan <nilan.patrick@gmail.com>
2024-05-28 17:38:59 -0700

    Add method for determining optimal error resolution when provided multiple

--- airbyte-cdk/python/airbyte_cdk/sources/declarative/requesters/error_handlers/composite_error_handler.py
+++ airbyte-cdk/python/airbyte_cdk/sources/declarative/requesters/error_handlers/composite_error_handler.py
@@ -7,7 +7,7 @@ from typing import Any, List, Mapping, Optional, Union
 
 import requests
 from airbyte_cdk.sources.streams.http.error_handlers import ErrorHandler
-from airbyte_cdk.sources.streams.http.error_handlers.response_models import DEFAULT_ERROR_RESOLUTION, ErrorResolution
+from airbyte_cdk.sources.streams.http.error_handlers.response_models import DEFAULT_ERROR_RESOLUTION, ErrorResolution, ResponseAction
 
 
 @dataclass
@@ -50,8 +50,28 @@ class CompositeErrorHandler(ErrorHandler):
     def max_time(self) -> Union[int, None]:
         return max([error_handler.max_time or 0 for error_handler in self.error_handlers])
 
+    def _new_error_resolution_is_preferred(
+        self, new_error_resolution: Optional[ErrorResolution], current_error_resolution: Optional[ErrorResolution]
+    ) -> bool:
+        if new_error_resolution is None:
+            return False
+
+        if current_error_resolution is None:
+            return True
+
+        priority = {ResponseAction.FAIL: 0, ResponseAction.RETRY: 1, ResponseAction.IGNORE: 2, ResponseAction.SUCCESS: 3}
+
+        return priority[new_error_resolution.response_action] > priority[current_error_resolution.response_action]
+
     def interpret_response(self, response_or_exception: Optional[Union[requests.Response, Exception]]) -> ErrorResolution:
+        optimal_error_resolution = None
         for retrier in self.error_handlers:
             error_resolution = retrier.interpret_response(response_or_exception)
-            return error_resolution
+            if error_resolution is not None and self._new_error_resolution_is_preferred(
+                new_error_resolution=error_resolution, current_error_resolution=optimal_error_resolution
+            ):
+                optimal_error_resolution = error_resolution
+
+        if optimal_error_resolution is not None:
+            return optimal_error_resolution
         return DEFAULT_ERROR_RESOLUTION
