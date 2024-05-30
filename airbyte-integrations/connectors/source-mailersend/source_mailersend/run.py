e4156af57f6b35be4aefc829c9d6d44036f96be9
btkcodedev <btk.codedev@gmail.com>
2024-05-30 19:12:16 +0530

    make compatible with builder

--- airbyte-integrations/connectors/source-mailersend/source_mailersend/run.py
+++ airbyte-integrations/connectors/source-mailersend/source_mailersend/run.py
@@ -1,13 +1,12 @@
 #
-# Copyright (c) 2023 Airbyte, Inc., all rights reserved.
+# Copyright (c) 2024 Airbyte, Inc., all rights reserved.
 #
 
 
 import sys
 
 from airbyte_cdk.entrypoint import launch
-from source_mailersend import SourceMailersend
-
+from .source import SourceMailersend
 
 def run():
     source = SourceMailersend()
