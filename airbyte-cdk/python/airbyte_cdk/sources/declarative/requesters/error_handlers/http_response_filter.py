9740aa6c856c9ab1ea50ed0beef71d4b69f4a11f
Patrick Nilan <nilan.patrick@gmail.com>
2024-05-30 13:57:00 -0700

    Update test for http response filter for correct behavior

--- airbyte-cdk/python/airbyte_cdk/sources/declarative/requesters/error_handlers/http_response_filter.py
+++ airbyte-cdk/python/airbyte_cdk/sources/declarative/requesters/error_handlers/http_response_filter.py
@@ -65,10 +65,10 @@ class HttpResponseFilter:
 
         if filter_action is not None:
             default_error_message = default_mapped_error_resolution.error_message if default_mapped_error_resolution is not None else ""
+            error_message = ""
             if isinstance(response_or_exception, requests.Response):
                 error_message = self._create_error_message(response_or_exception)
-            else:
-                error_message = default_error_message
+            error_message = error_message if len(error_message) else default_error_message
             failure_type = default_mapped_error_resolution.failure_type if default_mapped_error_resolution else FailureType.system_error
 
             return ErrorResolution(
