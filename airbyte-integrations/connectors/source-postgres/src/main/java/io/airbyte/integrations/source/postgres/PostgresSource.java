dc53dc75f11892655f7c915c392c395c3528de9b
Yue Li <yue.li@airbyte.io>
2024-06-03 10:38:45 -0700

    code review

--- airbyte-integrations/connectors/source-postgres/src/main/java/io/airbyte/integrations/source/postgres/PostgresSource.java
+++ airbyte-integrations/connectors/source-postgres/src/main/java/io/airbyte/integrations/source/postgres/PostgresSource.java
@@ -473,8 +473,9 @@ public class PostgresSource extends AbstractJdbcSource<PostgresType> implements 
     if (isXmin(config)) {
       checkOperations.add(database -> {
         if (PostgresQueryUtils.getXminStatus(database).getNumWraparound() > 0) {
-          throw new ConfigErrorException("The database has experienced transaction ID wraparound. " +
-              "Please fix this issue before continue setting up.");
+          throw new ConfigErrorException("We detected XMIN transaction wraparound in the database, " +
+              "which makes this sync option inefficient and can lead to higher credit consumption. " +
+              "Please change the replication method to CDC or cursor based.");
         }
       });
     }
