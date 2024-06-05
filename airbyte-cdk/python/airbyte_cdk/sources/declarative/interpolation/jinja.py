7d72d9f06cbd4026494cfafa898695353145d7fa
Natik Gadzhi <natik@respawn.io>
2024-06-05 14:01:07 -0700

    [airbyte-cdk] Use `jinja.sandbox.SandboxEnvironment` to restrict `InterpolatedString` (#39130)

--- airbyte-cdk/python/airbyte_cdk/sources/declarative/interpolation/jinja.py
+++ airbyte-cdk/python/airbyte_cdk/sources/declarative/interpolation/jinja.py
@@ -11,7 +11,7 @@ from airbyte_cdk.sources.declarative.interpolation.macros import macros
 from airbyte_cdk.sources.types import Config
 from jinja2 import meta
 from jinja2.exceptions import UndefinedError
-from jinja2.sandbox import Environment
+from jinja2.sandbox import SandboxedEnvironment
 
 
 class JinjaInterpolation(Interpolation):
@@ -49,7 +49,7 @@ class JinjaInterpolation(Interpolation):
     RESTRICTED_BUILTIN_FUNCTIONS = ["range"]  # The range function can cause very expensive computations
 
     def __init__(self) -> None:
-        self._environment = Environment()
+        self._environment = SandboxedEnvironment()
         self._environment.filters.update(**filters)
         self._environment.globals.update(**macros)
 
