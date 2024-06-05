71f0fd7b83e76055f1ccb8e9c3e2297264e6b42a
colesnodgrass <colesnodgrass@users.noreply.github.com>
2024-06-05 19:39:53 +0000

    Bump Airbyte version from 0.62.0 to 0.62.1

--- run-ab-platform.sh
+++ run-ab-platform.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-VERSION=0.62.0
+VERSION=0.62.1
 # Run away from anything even a little scary
 set -o nounset # -u exit if a variable is not set
 set -o errexit # -f exit for any command failure"
