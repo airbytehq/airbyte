831e7cc0285532ca222eff9137b98c40e7f5d996
Artem Inzhyyants <artem.inzhyyants@gmail.com>
2024-06-07 18:49:23 +0200

    Airbyte CDK: add unit test
    Signed-off-by: Artem Inzhyyants <artem.inzhyyants@gmail.com>

--- airbyte-cdk/python/unit_tests/sources/declarative/extractors/test_record_filter.py
+++ airbyte-cdk/python/unit_tests/sources/declarative/extractors/test_record_filter.py
@@ -11,6 +11,30 @@ from airbyte_cdk.sources.declarative.interpolation import InterpolatedString
 from airbyte_cdk.sources.declarative.models import CustomRetriever, DeclarativeStream, ParentStreamConfig, SubstreamPartitionRouter
 from airbyte_cdk.sources.declarative.types import StreamSlice
 
+DATE_FORMAT = "%Y-%m-%d"
+RECORDS_TO_FILTER_DATE_FORMAT = [
+        {"id": 1, "created_at": "2020-01-03"},
+        {"id": 2, "created_at": "2021-01-03"},
+        {"id": 3, "created_at": "2021-01-04"},
+        {"id": 4, "created_at": "2021-02-01"},
+    ]
+
+DATE_TIME_WITH_TZ_FORMAT = "%Y-%m-%dT%H:%M:%S%z"
+RECORDS_TO_FILTER_DATE_TIME_WITH_TZ_FORMAT = [
+        {"id": 1, "created_at": "2020-01-03T00:00:00+00:00"},
+        {"id": 2, "created_at": "2021-01-03T00:00:00+00:00"},
+        {"id": 3, "created_at": "2021-01-04T00:00:00+00:00"},
+        {"id": 4, "created_at": "2021-02-01T00:00:00+00:00"},
+    ]
+
+DATE_TIME_WITHOUT_TZ_FORMAT = "%Y-%m-%dT%H:%M:%S"
+RECORDS_TO_FILTER_DATE_TIME_WITHOUT_TZ_FORMAT = [
+        {"id": 1, "created_at": "2020-01-03T00:00:00"},
+        {"id": 2, "created_at": "2021-01-03T00:00:00"},
+        {"id": 3, "created_at": "2021-01-04T00:00:00"},
+        {"id": 4, "created_at": "2021-02-01T00:00:00"},
+    ]
+
 
 @pytest.mark.parametrize(
     "filter_template, records, expected_records",
@@ -59,28 +83,61 @@ def test_record_filter(filter_template: str, records: List[Mapping], expected_re
 
 
 @pytest.mark.parametrize(
-    "stream_state, record_filter_expression, expected_record_ids",
+    "datetime_format, stream_state, record_filter_expression, end_datetime,  records_to_filter, expected_record_ids",
     [
-        ({}, None, [2, 3]),
-        ({"created_at": "2021-01-04"}, None, [3]),
-        ({}, "{{ record['id'] % 2 == 1 }}", [3]),
+        (DATE_FORMAT, {}, None, "2021-01-05", RECORDS_TO_FILTER_DATE_FORMAT, [2, 3]),
+        (DATE_FORMAT, {}, None, None, RECORDS_TO_FILTER_DATE_FORMAT, [2, 3, 4]),
+        (DATE_FORMAT, {"created_at": "2021-01-04"}, None, "2021-01-05", RECORDS_TO_FILTER_DATE_FORMAT, [3]),
+        (DATE_FORMAT, {"created_at": "2021-01-04"}, None, None, RECORDS_TO_FILTER_DATE_FORMAT, [3, 4]),
+        (DATE_FORMAT, {}, "{{ record['id'] % 2 == 1 }}", "2021-01-05", RECORDS_TO_FILTER_DATE_FORMAT, [3]),
+
+        (DATE_TIME_WITH_TZ_FORMAT, {}, None, "2021-01-05T00:00:00+00:00", RECORDS_TO_FILTER_DATE_TIME_WITH_TZ_FORMAT, [2, 3]),
+        (DATE_TIME_WITH_TZ_FORMAT, {}, None, None, RECORDS_TO_FILTER_DATE_TIME_WITH_TZ_FORMAT, [2, 3, 4]),
+        (DATE_TIME_WITH_TZ_FORMAT, {"created_at": "2021-01-04T00:00:00+00:00"}, None, "2021-01-05T00:00:00+00:00", RECORDS_TO_FILTER_DATE_TIME_WITH_TZ_FORMAT, [3]),
+        (DATE_TIME_WITH_TZ_FORMAT, {"created_at": "2021-01-04T00:00:00+00:00"}, None, None, RECORDS_TO_FILTER_DATE_TIME_WITH_TZ_FORMAT, [3, 4]),
+        (DATE_TIME_WITH_TZ_FORMAT, {}, "{{ record['id'] % 2 == 1 }}", "2021-01-05T00:00:00+00:00", RECORDS_TO_FILTER_DATE_TIME_WITH_TZ_FORMAT, [3]),
+
+
+        (DATE_TIME_WITHOUT_TZ_FORMAT, {}, None, "2021-01-05T00:00:00", RECORDS_TO_FILTER_DATE_TIME_WITHOUT_TZ_FORMAT, [2, 3]),
+        (DATE_TIME_WITHOUT_TZ_FORMAT, {}, None, None, RECORDS_TO_FILTER_DATE_TIME_WITHOUT_TZ_FORMAT, [2, 3, 4]),
+        (DATE_TIME_WITHOUT_TZ_FORMAT, {"created_at": "2021-01-04T00:00:00"}, None, "2021-01-05T00:00:00", RECORDS_TO_FILTER_DATE_TIME_WITHOUT_TZ_FORMAT, [3]),
+        (DATE_TIME_WITHOUT_TZ_FORMAT, {"created_at": "2021-01-04T00:00:00"}, None, None, RECORDS_TO_FILTER_DATE_TIME_WITHOUT_TZ_FORMAT, [3, 4]),
+        (DATE_TIME_WITHOUT_TZ_FORMAT, {}, "{{ record['id'] % 2 == 1 }}", "2021-01-05T00:00:00", RECORDS_TO_FILTER_DATE_TIME_WITHOUT_TZ_FORMAT, [3]),
     ],
-    ids=["no_stream_state_no_record_filter", "with_stream_state_no_record_filter", "no_stream_state_with_record_filter"]
-)
-def test_client_side_record_filter_decorator_no_parent_stream(stream_state: Optional[Mapping], record_filter_expression: str,
-                                                              expected_record_ids: List[int]):
-    records_to_filter = [
-        {"id": 1, "created_at": "2020-01-03"},
-        {"id": 2, "created_at": "2021-01-03"},
-        {"id": 3, "created_at": "2021-01-04"},
-        {"id": 4, "created_at": "2021-02-01"},
+    ids=[
+        "date_format_no_stream_state_no_record_filter",
+        "date_format_no_stream_state_no_end_date_no_record_filter",
+        "date_format_with_stream_state_no_record_filter",
+        "date_format_with_stream_state_no_end_date_no_record_filter",
+        "date_format_no_stream_state_with_record_filter",
+
+        "date_time_with_tz_format_no_stream_state_no_record_filter",
+        "date_time_with_tz_format_no_stream_state_no_end_date_no_record_filter",
+        "date_time_with_tz_format_with_stream_state_no_record_filter",
+        "date_time_with_tz_format_with_stream_state_no_end_date_no_record_filter",
+        "date_time_with_tz_format_no_stream_state_with_record_filter",
+
+        "date_time_without_tz_format_no_stream_state_no_record_filter",
+        "date_time_without_tz_format_no_stream_state_no_end_date_no_record_filter",
+        "date_time_without_tz_format_with_stream_state_no_record_filter",
+        "date_time_without_tz_format_with_stream_state_no_end_date_no_record_filter",
+        "date_time_without_tz_format_no_stream_state_with_record_filter",
     ]
+)
+def test_client_side_record_filter_decorator_no_parent_stream(
+        datetime_format: str,
+        stream_state: Optional[Mapping],
+        record_filter_expression: str,
+        end_datetime: Optional[str],
+        records_to_filter: List[Mapping],
+        expected_record_ids: List[int]
+):
     date_time_based_cursor = DatetimeBasedCursor(
-                        start_datetime=MinMaxDatetime(datetime="2021-01-01", parameters={}),
-                        end_datetime=MinMaxDatetime(datetime="2021-01-05", parameters={}),
+                        start_datetime=MinMaxDatetime(datetime="2021-01-01", datetime_format=DATE_FORMAT, parameters={}),
+                        end_datetime=MinMaxDatetime(datetime=end_datetime, parameters={}) if end_datetime else None,
                         step="P10Y",
                         cursor_field=InterpolatedString.create("created_at", parameters={}),
-                        datetime_format="%Y-%m-%d",
+                        datetime_format=datetime_format,
                         cursor_granularity="P1D",
                         config={},
                         parameters={},
@@ -110,18 +167,12 @@ def test_client_side_record_filter_decorator_no_parent_stream(stream_state: Opti
     ids=["no_stream_state_no_record_filter", "with_stream_state_no_record_filter"]
 )
 def test_client_side_record_filter_decorator_with_parent_stream(stream_state: Optional[Mapping], expected_record_ids: List[int]):
-    records_to_filter = [
-        {"id": 1, "created_at": "2020-01-03"},
-        {"id": 2, "created_at": "2021-01-03"},
-        {"id": 3, "created_at": "2021-01-04"},
-        {"id": 4, "created_at": "2021-02-01"},
-    ]
     date_time_based_cursor = DatetimeBasedCursor(
-        start_datetime=MinMaxDatetime(datetime="2021-01-01", datetime_format="%Y-%m-%d", parameters={}),
-        end_datetime=MinMaxDatetime(datetime="2021-01-05", datetime_format="%Y-%m-%d", parameters={}),
+        start_datetime=MinMaxDatetime(datetime="2021-01-01", datetime_format=DATE_FORMAT, parameters={}),
+        end_datetime=MinMaxDatetime(datetime="2021-01-05", datetime_format=DATE_FORMAT, parameters={}),
         step="P10Y",
         cursor_field=InterpolatedString.create("created_at", parameters={}),
-        datetime_format="%Y-%m-%d",
+        datetime_format=DATE_FORMAT,
         cursor_granularity="P1D",
         config={},
         parameters={},
@@ -156,7 +207,7 @@ def test_client_side_record_filter_decorator_with_parent_stream(stream_state: Op
         per_partition_cursor=per_partition_cursor
     )
     filtered_records = list(
-        record_filter_decorator.filter_records(records=records_to_filter, stream_state=stream_state,
+        record_filter_decorator.filter_records(records=RECORDS_TO_FILTER_DATE_FORMAT, stream_state=stream_state,
                                                stream_slice=StreamSlice(partition={"id": "some_parent_id", "parent_slice": {}}, cursor_slice={}),
                                                next_page_token=None)
     )
