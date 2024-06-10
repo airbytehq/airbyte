cf3f601dac455569bdaba5b69b6701caa022c66a
Serhii Lazebnyi <serhii.lazebnyi@globallogic.com>
2024-06-11 01:31:39 +0200

    Fix tests

--- airbyte-integrations/connectors/source-linkedin-ads/unit_tests/conftest.py
+++ airbyte-integrations/connectors/source-linkedin-ads/unit_tests/conftest.py
@@ -2,4 +2,20 @@
 
 import os
 
+from source_linkedin_ads.source import SourceLinkedinAds
+
 os.environ["REQUEST_CACHE_PATH"] = "REQUEST_CACHE_PATH"
+
+
+def find_stream(stream_name, config):
+    streams = SourceLinkedinAds().streams(config=config)
+
+    # cache should be disabled once this issue is fixed https://github.com/airbytehq/airbyte-internal-issues/issues/6513
+    for stream in streams:
+        stream.retriever.requester.use_cache = True
+
+    # find by name
+    for stream in streams:
+        if stream.name == stream_name:
+            return stream
+    raise ValueError(f"Stream {stream_name} not found")
--- airbyte-integrations/connectors/source-linkedin-ads/unit_tests/test_analytics_streams.py
+++ airbyte-integrations/connectors/source-linkedin-ads/unit_tests/test_analytics_streams.py
@@ -1,12 +1,16 @@
 #
 # Copyright (c) 2023 Airbyte, Inc., all rights reserved.
 #
+
+
 import json
 import os
 from typing import Any, Mapping
 
+from airbyte_cdk.models import SyncMode
+from airbyte_cdk.sources.declarative.types import StreamSlice
 from airbyte_cdk.sources.streams.http.requests_native_auth import TokenAuthenticator
-from source_linkedin_ads.analytics_streams import AdMemberCountryAnalytics, LinkedInAdsAnalyticsStream
+from conftest import find_stream
 
 # Test input arguments for the `make_analytics_slices`
 TEST_KEY_VALUE_MAP = {"camp_id": "id"}
@@ -28,22 +32,6 @@ TEST_CONFIG: dict = {
     },
 }
 
-# Test chunk size for each field set
-TEST_FIELDS_CHUNK_SIZE = 3
-# Test fields assuming they are really available for the fetch
-TEST_ANALYTICS_FIELDS = [
-    "field_1",
-    "base_field_1",
-    "field_2",
-    "base_field_2",
-    "field_3",
-    "field_4",
-    "field_5",
-    "field_6",
-    "field_7",
-    "field_8",
-]
-
 
 # HELPERS
 def load_json_file(file_name: str) -> Mapping[str, Any]:
@@ -52,16 +40,16 @@ def load_json_file(file_name: str) -> Mapping[str, Any]:
 
 
 def test_analytics_stream_slices(requests_mock):
+    stream = find_stream("ad_member_country_analytics", TEST_CONFIG)
     requests_mock.get("https://api.linkedin.com/rest/adAccounts", json={"elements": [{"id": 1}]})
     requests_mock.get("https://api.linkedin.com/rest/adAccounts/1/adCampaigns", json={"elements": [{"id": 123}]})
-    assert list(
-        AdMemberCountryAnalytics(config=TEST_CONFIG).stream_slices(
-            sync_mode=None,
-        )
-    ) == load_json_file("output_slices.json")
+
+    assert list(stream.retriever.stream_slicer.stream_slices()) == load_json_file("output_slices.json")
 
 
 def test_read_records(requests_mock):
+    stream = find_stream("ad_member_country_analytics", TEST_CONFIG)
+    requests_mock.get("https://api.linkedin.com/rest/adAccounts", json={"elements": [{"id": 1}]})
     requests_mock.get(
         "https://api.linkedin.com/rest/adAnalytics",
         [
@@ -70,40 +58,7 @@ def test_read_records(requests_mock):
             {"json": load_json_file("responses/ad_member_country_analytics/response_3.json")},
         ],
     )
+
     stream_slice = load_json_file("output_slices.json")[0]
-    records = list(AdMemberCountryAnalytics(config=TEST_CONFIG).read_records(stream_slice=stream_slice, sync_mode=None))
+    records = list(stream.read_records(sync_mode=SyncMode.incremental, stream_slice=StreamSlice(partition={}, cursor_slice=stream_slice), stream_state=None))
     assert len(records) == 2
-
-
-def test_chunk_analytics_fields():
-    """
-    We expect to truncate the field list into the chunks of equal size,
-    with "dateRange" field presented in each chunk.
-    """
-    expected_output = [
-        ["field_1", "base_field_1", "field_2", "dateRange", "pivotValues"],
-        ["base_field_2", "field_3", "field_4", "dateRange", "pivotValues"],
-        ["field_5", "field_6", "field_7", "dateRange", "pivotValues"],
-        ["field_8", "dateRange", "pivotValues"],
-    ]
-
-    assert list(LinkedInAdsAnalyticsStream.chunk_analytics_fields(TEST_ANALYTICS_FIELDS, TEST_FIELDS_CHUNK_SIZE)) == expected_output
-
-
-def test_get_date_slices():
-    """
-    By default, we use the `WINDOW_SIZE = 30`, as it set in the analytics module
-    This value could be changed by setting the corresponding argument in the method.
-    The `end_date` is not specified by default, but for this test it was specified to have the test static.
-    """
-
-    test_start_date = "2021-08-01"
-    test_end_date = "2021-10-01"
-
-    expected_output = [
-        {"dateRange": {"start.day": 1, "start.month": 8, "start.year": 2021, "end.day": 31, "end.month": 8, "end.year": 2021}},
-        {"dateRange": {"start.day": 31, "start.month": 8, "start.year": 2021, "end.day": 30, "end.month": 9, "end.year": 2021}},
-        {"dateRange": {"start.day": 30, "start.month": 9, "start.year": 2021, "end.day": 30, "end.month": 10, "end.year": 2021}},
-    ]
-
-    assert list(LinkedInAdsAnalyticsStream.get_date_slices(test_start_date, test_end_date)) == expected_output
