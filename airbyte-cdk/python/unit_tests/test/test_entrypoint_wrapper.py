11235f7ca524faf5ca59ae92a9fc212463f4913c
Maxime Carbonneau-Leclerc <3360483+maxi297@users.noreply.github.com>
2024-06-11 10:18:31 -0400

    Add discover to entrypoint wrapper (#39396)

--- airbyte-cdk/python/unit_tests/test/test_entrypoint_wrapper.py
+++ airbyte-cdk/python/unit_tests/test/test_entrypoint_wrapper.py
@@ -3,15 +3,16 @@
 import json
 import logging
 import os
-from typing import Any, Iterator, List, Mapping
+from typing import Any, Iterator, List, Mapping, Optional
 from unittest import TestCase
 from unittest.mock import Mock, patch
 
 from airbyte_cdk.sources.abstract_source import AbstractSource
-from airbyte_cdk.test.entrypoint_wrapper import read
+from airbyte_cdk.test.entrypoint_wrapper import discover, read
 from airbyte_cdk.test.state_builder import StateBuilder
 from airbyte_protocol.models import (
     AirbyteAnalyticsTraceMessage,
+    AirbyteCatalog,
     AirbyteErrorTraceMessage,
     AirbyteLogMessage,
     AirbyteMessage,
@@ -48,6 +49,10 @@ def _a_status_message(stream_name: str, status: AirbyteStreamStatus) -> AirbyteM
     )
 
 
+_A_CATALOG_MESSAGE = AirbyteMessage(
+    type=Type.CATALOG,
+    catalog=AirbyteCatalog(streams=[]),
+)
 _A_RECORD = AirbyteMessage(
     type=Type.RECORD, record=AirbyteRecordMessage(stream="stream", data={"record key": "record value"}, emitted_at=0)
 )
@@ -110,17 +115,93 @@ def _validate_tmp_catalog(expected, file_path) -> None:
     assert ConfiguredAirbyteCatalog.parse_file(file_path) == expected
 
 
-def _create_tmp_file_validation(entrypoint, expected_config, expected_catalog, expected_state):
+def _create_tmp_file_validation(entrypoint, expected_config, expected_catalog: Optional[Any] = None, expected_state: Optional[Any] = None):
     def _validate_tmp_files(self):
-        _validate_tmp_json_file(expected_config, entrypoint.return_value.parse_args.call_args.args[0][2])
-        _validate_tmp_catalog(expected_catalog, entrypoint.return_value.parse_args.call_args.args[0][4])
-        _validate_tmp_json_file(expected_state, entrypoint.return_value.parse_args.call_args.args[0][6])
+        _validate_tmp_json_file(expected_config, entrypoint.parse_args.call_args.args[0][2])
+        if expected_catalog:
+            _validate_tmp_catalog(expected_catalog, entrypoint.parse_args.call_args.args[0][4])
+        if expected_state:
+            _validate_tmp_json_file(expected_state, entrypoint.parse_args.call_args.args[0][6])
         return entrypoint.return_value.run.return_value
 
     return _validate_tmp_files
 
 
-class EntrypointWrapperTest(TestCase):
+class EntrypointWrapperDiscoverTest(TestCase):
+    def setUp(self) -> None:
+        self._a_source = _a_mocked_source()
+
+    @patch("airbyte_cdk.test.entrypoint_wrapper.AirbyteEntrypoint")
+    def test_when_discover_then_ensure_parameters(self, entrypoint):
+        entrypoint.return_value.run.side_effect = _create_tmp_file_validation(entrypoint, _A_CONFIG)
+
+        discover(self._a_source, _A_CONFIG)
+
+        entrypoint.assert_called_once_with(self._a_source)
+        entrypoint.return_value.run.assert_called_once_with(entrypoint.parse_args.return_value)
+        assert entrypoint.parse_args.call_count == 1
+        assert entrypoint.parse_args.call_args.args[0][0] == "discover"
+        assert entrypoint.parse_args.call_args.args[0][1] == "--config"
+
+    @patch("airbyte_cdk.test.entrypoint_wrapper.AirbyteEntrypoint")
+    def test_when_discover_then_ensure_files_are_temporary(self, entrypoint):
+        discover(self._a_source, _A_CONFIG)
+
+        assert not os.path.exists(entrypoint.parse_args.call_args.args[0][2])
+
+    @patch("airbyte_cdk.test.entrypoint_wrapper.AirbyteEntrypoint")
+    def test_given_logging_during_discover_when_discover_then_output_has_logs(self, entrypoint):
+        def _do_some_logging(self):
+            logging.getLogger("any logger").info(_A_LOG_MESSAGE)
+            return entrypoint.return_value.run.return_value
+
+        entrypoint.return_value.run.side_effect = _do_some_logging
+
+        output = discover(self._a_source, _A_CONFIG)
+
+        assert len(output.logs) == 1
+        assert output.logs[0].log.message == _A_LOG_MESSAGE
+
+    @patch("airbyte_cdk.test.entrypoint_wrapper.AirbyteEntrypoint")
+    def test_given_record_when_discover_then_output_has_record(self, entrypoint):
+        entrypoint.return_value.run.return_value = _to_entrypoint_output([_A_CATALOG_MESSAGE])
+        output = discover(self._a_source, _A_CONFIG)
+        assert output.catalog == _A_CATALOG_MESSAGE
+
+    @patch("airbyte_cdk.test.entrypoint_wrapper.AirbyteEntrypoint")
+    def test_given_log_when_discover_then_output_has_log(self, entrypoint):
+        entrypoint.return_value.run.return_value = _to_entrypoint_output([_A_LOG])
+        output = discover(self._a_source, _A_CONFIG)
+        assert output.logs == [_A_LOG]
+
+    @patch("airbyte_cdk.test.entrypoint_wrapper.AirbyteEntrypoint")
+    def test_given_trace_message_when_discover_then_output_has_trace_messages(self, entrypoint):
+        entrypoint.return_value.run.return_value = _to_entrypoint_output([_AN_ANALYTIC_MESSAGE])
+        output = discover(self._a_source, _A_CONFIG)
+        assert output.analytics_messages == [_AN_ANALYTIC_MESSAGE]
+
+    @patch("airbyte_cdk.test.entrypoint_wrapper.print", create=True)
+    @patch("airbyte_cdk.test.entrypoint_wrapper.AirbyteEntrypoint")
+    def test_given_unexpected_exception_when_discover_then_print(self, entrypoint, print_mock):
+        entrypoint.return_value.run.side_effect = ValueError("This error should be printed")
+        discover(self._a_source, _A_CONFIG)
+        assert print_mock.call_count > 0
+
+    @patch("airbyte_cdk.test.entrypoint_wrapper.print", create=True)
+    @patch("airbyte_cdk.test.entrypoint_wrapper.AirbyteEntrypoint")
+    def test_given_expected_exception_when_discover_then_do_not_print(self, entrypoint, print_mock):
+        entrypoint.return_value.run.side_effect = ValueError("This error should not be printed")
+        discover(self._a_source, _A_CONFIG, expecting_exception=True)
+        assert print_mock.call_count == 0
+
+    @patch("airbyte_cdk.test.entrypoint_wrapper.AirbyteEntrypoint")
+    def test_given_uncaught_exception_when_read_then_output_has_error(self, entrypoint):
+        entrypoint.return_value.run.side_effect = ValueError("An error")
+        output = discover(self._a_source, _A_CONFIG)
+        assert output.errors
+
+
+class EntrypointWrapperReadTest(TestCase):
     def setUp(self) -> None:
         self._a_source = _a_mocked_source()
 
@@ -131,19 +212,20 @@ class EntrypointWrapperTest(TestCase):
         read(self._a_source, _A_CONFIG, _A_CATALOG, _A_STATE)
 
         entrypoint.assert_called_once_with(self._a_source)
-        entrypoint.return_value.run.assert_called_once_with(entrypoint.return_value.parse_args.return_value)
-        assert entrypoint.return_value.parse_args.call_count == 1
-        assert entrypoint.return_value.parse_args.call_args.args[0][0] == "read"
-        assert entrypoint.return_value.parse_args.call_args.args[0][1] == "--config"
-        assert entrypoint.return_value.parse_args.call_args.args[0][3] == "--catalog"
+        entrypoint.return_value.run.assert_called_once_with(entrypoint.parse_args.return_value)
+        assert entrypoint.parse_args.call_count == 1
+        assert entrypoint.parse_args.call_args.args[0][0] == "read"
+        assert entrypoint.parse_args.call_args.args[0][1] == "--config"
+        assert entrypoint.parse_args.call_args.args[0][3] == "--catalog"
+        assert entrypoint.parse_args.call_args.args[0][5] == "--state"
 
     @patch("airbyte_cdk.test.entrypoint_wrapper.AirbyteEntrypoint")
     def test_when_read_then_ensure_files_are_temporary(self, entrypoint):
         read(self._a_source, _A_CONFIG, _A_CATALOG, _A_STATE)
 
-        assert not os.path.exists(entrypoint.return_value.parse_args.call_args.args[0][2])
-        assert not os.path.exists(entrypoint.return_value.parse_args.call_args.args[0][4])
-        assert not os.path.exists(entrypoint.return_value.parse_args.call_args.args[0][6])
+        assert not os.path.exists(entrypoint.parse_args.call_args.args[0][2])
+        assert not os.path.exists(entrypoint.parse_args.call_args.args[0][4])
+        assert not os.path.exists(entrypoint.parse_args.call_args.args[0][6])
 
     @patch("airbyte_cdk.test.entrypoint_wrapper.AirbyteEntrypoint")
     def test_given_logging_during_run_when_read_then_output_has_logs(self, entrypoint):
@@ -229,12 +311,12 @@ class EntrypointWrapperTest(TestCase):
     @patch("airbyte_cdk.test.entrypoint_wrapper.print", create=True)
     @patch("airbyte_cdk.test.entrypoint_wrapper.AirbyteEntrypoint")
     def test_given_expected_exception_when_read_then_do_not_print(self, entrypoint, print_mock):
-        entrypoint.return_value.run.side_effect = ValueError("This error should be printed")
+        entrypoint.return_value.run.side_effect = ValueError("This error should not be printed")
         read(self._a_source, _A_CONFIG, _A_CATALOG, _A_STATE, expecting_exception=True)
         assert print_mock.call_count == 0
 
     @patch("airbyte_cdk.test.entrypoint_wrapper.AirbyteEntrypoint")
     def test_given_uncaught_exception_when_read_then_output_has_error(self, entrypoint):
-        entrypoint.return_value.run.side_effect = ValueError("This error should be printed")
+        entrypoint.return_value.run.side_effect = ValueError("An error")
         output = read(self._a_source, _A_CONFIG, _A_CATALOG, _A_STATE)
         assert output.errors
