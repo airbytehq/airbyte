4b1b1f929fb0b6a8dea338281de58ebcabd20305
Ishankoradia <ikoradia@umich.edu>
2024-06-11 23:06:08 +0530

    updates

---
+++ airbyte-integrations/connectors/source-github-files/source_github_files/spec.py
@@ -0,0 +1,15 @@
+from airbyte_cdk.sources.file_based.file_based_source import AbstractFileBasedSpec
+from pydantic import BaseModel, Field
+
+
+class SourceGoogleDriveSpec(AbstractFileBasedSpec, BaseModel):
+    class Config:
+        title = "Google Drive Source Spec"
+
+    folder_url: str = Field(
+        description="URL for the folder you want to sync. Using individual streams and glob patterns, it's possible to only sync a subset of all files located in the folder.",
+        examples=["https://drive.google.com/drive/folders/1Xaz0vXXXX2enKnNYU5qSt9NS70gvMyYn"],
+        order=0,
+        pattern="^https://drive.google.com/.+",
+        pattern_descriptor="https://drive.google.com/drive/folders/MY-FOLDER-ID",
+    )
