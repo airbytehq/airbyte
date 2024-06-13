7e05dc064484b8e6f33ed85ab3a81c9785c939b4
marcosmarxm <marcosmarxm@gmail.com>
2024-06-13 08:53:28 -0300

    add poetry files

--- airbyte-integrations/connectors/source-amazon-sqs/main.py
+++ airbyte-integrations/connectors/source-amazon-sqs/main.py
@@ -1,8 +1,8 @@
-#
-# Copyright (c) 2023 Airbyte, Inc., all rights reserved.
-#
+import sys
 
-from source_amazon_sqs.run import run
+from airbyte_cdk.entrypoint import launch
+from source_amazon_sqs import SourceAmazonSqs
 
 if __name__ == "__main__":
-    run()
+    source = SourceAmazonSqs()
+    launch(source, sys.argv[1:])
