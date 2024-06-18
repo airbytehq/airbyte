18e82d949aa8a9b220f038d903c9872a680bd46b
Patrick Nilan <nilan.patrick@gmail.com>
2024-06-17 18:03:15 -0700

    [airbyte-cdk] - Integrate `HttpClient` into `HttpRequester` (#38906)

--- airbyte-cdk/python/airbyte_cdk/sources/declarative/requesters/error_handlers/composite_error_handler.py
+++ airbyte-cdk/python/airbyte_cdk/sources/declarative/requesters/error_handlers/composite_error_handler.py
@@ -44,11 +44,11 @@ class CompositeErrorHandler(ErrorHandler):
 
     @property
     def max_retries(self) -> Optional[int]:
-        return self.error_handlers[0].max_retries  # type: ignore # property not defined in ErrorHandler
+        return self.error_handlers[0].max_retries
 
     @property
-    def max_time(self) -> Union[int, None]:
-        return max([error_handler.max_time or 0 for error_handler in self.error_handlers])  # type: ignore # property not defined in ErrorHandler
+    def max_time(self) -> Optional[int]:
+        return max([error_handler.max_time or 0 for error_handler in self.error_handlers])
 
     def interpret_response(self, response_or_exception: Optional[Union[requests.Response, Exception]]) -> ErrorResolution:
         matched_error_resolution = None
