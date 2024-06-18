c0fadda8edf2df2fa4fd6013f72f4a0970935763
Serhii Lazebnyi <serhii.lazebnyi@globallogic.com>
2024-06-18 22:04:19 +0200

    Fix statsparsing

--- airbyte-integrations/connectors/source-snapchat-marketing/unit_tests/unit_test.py
+++ airbyte-integrations/connectors/source-snapchat-marketing/unit_tests/unit_test.py
@@ -5,7 +5,6 @@
 import json
 
 import pytest
-import source_snapchat_marketing
 from airbyte_cdk.models import SyncMode
 from airbyte_cdk.sources.declarative.types import StreamSlice
 from airbyte_cdk.sources.streams.http.exceptions import UserDefinedBackoffException
@@ -16,7 +15,7 @@ config_mock = {
     "client_id": "client_id",
     "client_secret": "client_secret",
     "refresh_token": "refresh_token",
-    "start_date": "2000-01-01",
+    "start_date": "2024-01-01",
     "end_date": "2024-02-10",
     "action_report_time": "impression",
     "swipe_up_attribution_window": "7_DAY",
@@ -28,20 +27,34 @@ response_organizations = {
         {
             "organization": {
                 "id": "organization_id_1",
-                "updated_at": "2020-12-15T22:35:17.819Z",
-                "created_at": "2020-12-15T11:13:03.910Z",
+                "updated_at": "2024-02-05T22:35:17.819Z",
+                "created_at": "2024-02-05T11:13:03.910Z",
             }
         }
     ]
 }
 
 
+def test_should_retry_403_error(requests_mock):
+    requests_mock.post("https://accounts.snapchat.com/login/oauth2/access_token", json={"access_token": "XXX", "expires_in": 3600})
+    requests_mock.register_uri(
+        "GET", "https://adsapi.snapchat.com/v1/me/organizations", [{"status_code": 403, "json": {"organizations": []}}]
+    )
+    stream = find_stream("organizations", config_mock)
+    with pytest.raises(UserDefinedBackoffException):
+        list(stream.read_records(sync_mode=SyncMode.full_refresh))
+
+
 def test_organizations(requests_mock):
     requests_mock.post("https://accounts.snapchat.com/login/oauth2/access_token", json={"access_token": "XXX", "expires_in": 3600})
     requests_mock.get("https://adsapi.snapchat.com/v1/me/organizations", json=response_organizations)
     stream = find_stream("organizations", config_mock)
-    records = stream.read_records(sync_mode=SyncMode.full_refresh, stream_slice=StreamSlice(partition={}, cursor_slice={"start_time": "2000-01-01", "end_time": "2024-01-01"}), stream_state=None)
-    assert json.dumps(next(records), sort_keys=True) == json.dumps({"id": "organization_id_1", "updated_at": "2020-12-15T22:35:17.819Z", "created_at": "2020-12-15T11:13:03.910Z"}, sort_keys=True)
+    records = stream.read_records(sync_mode=SyncMode.full_refresh, stream_slice=StreamSlice(partition={},
+                                                                                            cursor_slice={"start_time": "2024-01-01",
+                                                                                                          "end_time": "2024-01-01"}),
+                                  stream_state=None)
+    assert json.dumps(next(records).data, sort_keys=True) == json.dumps(
+        {"id": "organization_id_1", "updated_at": "2024-02-05T22:35:17.819Z", "created_at": "2024-02-05T11:13:03.910Z"}, sort_keys=True)
 
 
 response_adaccounts = {
@@ -49,15 +62,15 @@ response_adaccounts = {
         {
             "adaccount": {
                 "id": "adaccount_id_1",
-                "updated_at": "2020-12-15T22:35:17.819Z",
-                "created_at": "2020-12-15T11:13:03.910Z",
+                "updated_at": "2024-02-05T22:35:17.819Z",
+                "created_at": "2024-02-05T11:13:03.910Z",
             }
         },
         {
             "adaccount": {
                 "id": "adaccount_id_2",
-                "updated_at": "2020-12-15T22:35:17.819Z",
-                "created_at": "2020-12-15T11:13:03.910Z",
+                "updated_at": "2024-02-05T22:35:17.819Z",
+                "created_at": "2024-02-05T11:13:03.910Z",
             }
         },
     ]
@@ -78,16 +91,17 @@ def test_accounts(requests_mock):
 
     stream = find_stream("adaccounts", config_mock)
     records = run_stream(stream)
-    assert list(records) == [
+    list_records = [record.data for record in records]
+    assert list_records == [
         {
             "id": "adaccount_id_1",
-            "updated_at": "2020-12-15T22:35:17.819Z",
-            "created_at": "2020-12-15T11:13:03.910Z",
+            "updated_at": "2024-02-05T22:35:17.819Z",
+            "created_at": "2024-02-05T11:13:03.910Z",
         },
         {
             "id": "adaccount_id_2",
-            "updated_at": "2020-12-15T22:35:17.819Z",
-            "created_at": "2020-12-15T11:13:03.910Z",
+            "updated_at": "2024-02-05T22:35:17.819Z",
+            "created_at": "2024-02-05T11:13:03.910Z",
         },
     ]
 
@@ -97,15 +111,15 @@ response_ads = {
         {
             "ad": {
                 "id": "ad_id_1",
-                "updated_at": "2020-12-15T22:35:17.819Z",
-                "created_at": "2020-12-15T11:13:03.910Z",
+                "updated_at": "2024-02-05T22:35:17.819Z",
+                "created_at": "2024-02-05T11:13:03.910Z",
             }
         },
         {
             "ad": {
                 "id": "ad_id_2",
-                "updated_at": "2020-12-15T22:35:17.819Z",
-                "created_at": "2020-12-15T11:13:03.910Z",
+                "updated_at": "2024-02-05T22:35:17.819Z",
+                "created_at": "2024-02-05T11:13:03.910Z",
             }
         },
     ]
@@ -122,16 +136,17 @@ def test_ads(requests_mock):
 
     stream = find_stream("ads", config_mock)
     records = run_stream(stream)
-    assert list(records) == [
+    list_records = [record.data for record in records]
+    assert list_records == [
         {
             "id": "ad_id_1",
-            "updated_at": "2020-12-15T22:35:17.819Z",
-            "created_at": "2020-12-15T11:13:03.910Z",
+            "updated_at": "2024-02-05T22:35:17.819Z",
+            "created_at": "2024-02-05T11:13:03.910Z",
         },
         {
             "id": "ad_id_2",
-            "updated_at": "2020-12-15T22:35:17.819Z",
-            "created_at": "2020-12-15T11:13:03.910Z",
+            "updated_at": "2024-02-05T22:35:17.819Z",
+            "created_at": "2024-02-05T11:13:03.910Z",
         },
     ]
 
@@ -158,7 +173,6 @@ response_ads_stats_lifetime_1 = {
         }
     ],
 }
-
 
 response_ads_stats_lifetime_2 = {
     "request_status": "SUCCESS",
@@ -196,7 +210,8 @@ def test_ads_stats_lifetime(requests_mock):
 
     stream = find_stream("ads_stats_lifetime", config_mock)
     records = run_stream(stream)
-    assert list(records) == [
+    list_records = [record.data for record in records]
+    assert list_records == [
         {
             "conversion_data_processed_end_time": "2022-07-01T00:00:00.000Z",
             "end_time": "2022-07-01T07:00:00.000-07:00",
@@ -232,14 +247,14 @@ response_ads_stats_daily_1 = {
                 "id": "417d0269-80fb-496a-b5f3-ec0bac665144",
                 "type": "AD",
                 "granularity": "DAY",
-                "start_time": "2022-06-25T00:00:00.000-07:00",
-                "end_time": "2022-06-29T00:00:00.000-07:00",
+                "start_time": "2024-02-05T00:00:00.000-07:00",
+                "end_time": "2024-02-08T00:00:00.000-07:00",
                 "finalized_data_end_time": "2022-06-30T00:00:00.000-07:00",
                 "conversion_data_processed_end_time": "2022-06-30T00:00:00.000Z",
                 "timeseries": [
                     {
-                        "start_time": "2022-06-25T00:00:00.000-07:00",
-                        "end_time": "2022-06-26T00:00:00.000-07:00",
+                        "start_time": "2024-02-05T00:00:00.000-07:00",
+                        "end_time": "2024-02-06T00:00:00.000-07:00",
                         "stats": {
                             "impressions": 0,
                             "swipes": 0,
@@ -249,8 +264,8 @@ response_ads_stats_daily_1 = {
                         },
                     },
                     {
-                        "start_time": "2022-06-26T00:00:00.000-07:00",
-                        "end_time": "2022-06-27T00:00:00.000-07:00",
+                        "start_time": "2024-02-07T00:00:00.000-07:00",
+                        "end_time": "2024-02-08T00:00:00.000-07:00",
                         "stats": {
                             "impressions": 0,
                             "swipes": 0,
@@ -294,13 +309,3 @@ def test_source_check_connection(requests_mock):
 
     results = SourceSnapchatMarketing().check_connection(logger=None, config=source_config)
     assert results == (True, None)
-
-
-def test_should_retry_403_error(requests_mock):
-    requests_mock.register_uri(
-        "GET", "https://adsapi.snapchat.com/v1/me/organizations", [{"status_code": 403, "json": {"organizations": []}}]
-    )
-    requests_mock.post("https://accounts.snapchat.com/login/oauth2/access_token", json={"access_token": "XXX", "expires_in": 3600})
-    stream = find_stream("organizations", config_mock)
-    with pytest.raises(UserDefinedBackoffException) as e:
-        list(stream.read_records(sync_mode=SyncMode.full_refresh))
