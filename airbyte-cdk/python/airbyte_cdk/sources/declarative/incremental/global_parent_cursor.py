62e9567956c13b68259b3cf370135328e31ff6c8
Anatolii Yatsuk <tolikyatsuk@gmail.com>
2024-06-19 18:02:51 +0300

    Fix docs

--- airbyte-cdk/python/airbyte_cdk/sources/declarative/incremental/global_parent_cursor.py
+++ airbyte-cdk/python/airbyte_cdk/sources/declarative/incremental/global_parent_cursor.py
@@ -11,23 +11,11 @@ from airbyte_cdk.sources.types import Record, StreamSlice, StreamState
 
 class GlobalParentCursor(DeclarativeCursor):
     """
-    Given a stream has many partitions, it is important to provide a state per partition.
+    The GlobalParentCursor is designed to track the state of parent streams using a single global cursor.
+    This class is useful for streams that have many partitions, allowing the state to be managed globally
+    rather than per partition, which simplifies state management and reduces the size of state messages.
 
-    Record | Stream Slice | Last Record | DatetimeCursorBased cursor
-    -- | -- | -- | --
-    1 | {"start_time": "2021-01-01","end_time": "2021-01-31","owner_resource": "1"''} | cursor_field: “2021-01-15” | 2021-01-15
-    2 | {"start_time": "2021-02-01","end_time": "2021-02-28","owner_resource": "1"''} | cursor_field: “2021-02-15” | 2021-02-15
-    3 | {"start_time": "2021-01-01","end_time": "2021-01-31","owner_resource": "2"''} | cursor_field: “2021-01-03” | 2021-01-03
-    4 | {"start_time": "2021-02-01","end_time": "2021-02-28","owner_resource": "2"''} | cursor_field: “2021-02-14” | 2021-02-14
-
-    Given the following errors, this can lead to some loss or duplication of records:
-    When | Problem | Affected Record
-    -- | -- | --
-    Between record #1 and #2 | Loss | #3
-    Between record #2 and #3 | Loss | #3, #4
-    Between record #3 and #4 | Duplication | #1, #2
-
-    Therefore, we need to manage state per partition.
+    This cursor will be used when the `global_parent_cursor` parameter is set for incremental sync.
     """
 
     def __init__(self, stream_cursor: DeclarativeCursor, partition_router: PartitionRouter):
@@ -56,8 +44,8 @@ class GlobalParentCursor(DeclarativeCursor):
         """
         Set the initial state for the cursors.
 
-        This method initializes the state for each partition cursor using the provided stream state.
-        If a partition state is provided in the stream state, it will update the corresponding partition cursor with this state.
+        This method initializes the state for the global cursor using the provided stream state.
+        If a partition state is provided in the stream state, it will update the corresponding global cursor with this state.
 
         Additionally, it sets the parent state for partition routers that are based on parent streams. If a partition router
         does not have parent streams, this step will be skipped due to the default PartitionRouter implementation.
@@ -65,16 +53,9 @@ class GlobalParentCursor(DeclarativeCursor):
         Args:
             stream_state (StreamState): The state of the streams to be set. The format of the stream state should be:
                 {
-                    "states": [
-                        {
-                            "partition": {
-                                "partition_key": "value"
-                            },
-                            "cursor": {
-                                "last_updated": "2023-05-27T00:00:00Z"
-                            }
-                        }
-                    ],
+                    "state": {
+                        "last_updated": "2023-05-27T00:00:00Z"
+                    },
                     "parent_state": {
                         "parent_stream_name": {
                             "last_updated": "2023-05-27T00:00:00Z"
@@ -94,6 +75,17 @@ class GlobalParentCursor(DeclarativeCursor):
         self._stream_cursor.observe(StreamSlice(partition={}, cursor_slice=stream_slice.cursor_slice), record)
 
     def close_slice(self, stream_slice: StreamSlice, *args: Any) -> None:
+        """
+        Close the current stream slice.
+
+        This method is called when a stream slice is completed. For the global parent cursor, we close the child cursor
+        only after reading all slices. This ensures that we do not miss any child records from a later parent record
+        if the child cursor is earlier than a record from the first parent record.
+
+        Args:
+            stream_slice (StreamSlice): The stream slice to be closed.
+            *args (Any): Additional arguments.
+        """
         if stream_slice.last_slice:
             self._stream_cursor.close_slice(StreamSlice(partition={}, cursor_slice=stream_slice.cursor_slice), *args)
 
@@ -193,13 +185,6 @@ class GlobalParentCursor(DeclarativeCursor):
         return self._stream_cursor.should_be_synced(self._convert_record_to_cursor_record(record))
 
     def is_greater_than_or_equal(self, first: Record, second: Record) -> bool:
-        # if not first.associated_slice or not second.associated_slice:
-        #     raise ValueError(f"Both records should have an associated slice but got {first.associated_slice} and {second.associated_slice}")
-        # if first.associated_slice.partition != second.associated_slice.partition:
-        #     raise ValueError(
-        #         f"To compare records, partition should be the same but got {first.associated_slice.partition} and {second.associated_slice.partition}"
-        #     )
-
         return self._stream_cursor.is_greater_than_or_equal(
             self._convert_record_to_cursor_record(first), self._convert_record_to_cursor_record(second)
         )
