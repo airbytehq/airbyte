fe1b7e092714310e4f0dc5bad002921b16f6c55b
Audrey Maldonado <audrey.maldonado@gmail.com>
2024-06-20 14:04:49 -0400

    Audrey/rbac (#39501)
    Co-authored-by: Alexandre Cuoci <Hesperide@users.noreply.github.com>

--- docusaurus/sidebars.js
+++ docusaurus/sidebars.js
@@ -4,6 +4,7 @@ const {
   parseMarkdownContentTitle,
   parseFrontMatter,
 } = require("@docusaurus/utils");
+const { type } = require("os");
 
 const connectorsDocsRoot = "../docs/integrations";
 const sourcesDocs = `${connectorsDocsRoot}/sources`;
@@ -585,8 +586,18 @@ module.exports = {
             },
           ],
         },
-        "access-management/role-mapping"
-      ],
+        {
+          type: "category",
+          label: "Role-Based Access Control (RBAC)",
+          link: {
+            type: "doc",
+            id: "access-management/rbac",
+          },
+          items: [
+            {type: "doc", id: "access-management/role-mapping"},
+          ],
+        },
+      ]
     },
     {
       type: "category",
