a2df8f2af288d502f5efa90f7f21b7ab178cbc11
Dhroov Makwana <pabloescoder@gmail.com>
2024-06-21 10:26:05 +0530

    Fix components.py formatting

--- airbyte-integrations/connectors/source-adjust/source_adjust/components.py
+++ airbyte-integrations/connectors/source-adjust/source_adjust/components.py
@@ -6,7 +6,6 @@ from dataclasses import dataclass
 from typing import Any, Mapping
 
 from airbyte_cdk.sources.declarative.schema.json_file_schema_loader import JsonFileSchemaLoader
-
 import source_adjust.model
 
 
