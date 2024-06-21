9eb248f38d83bb12b49fee7617d6b6460aabc67b
Augustin <augustin@airbyte.io>
2024-06-21 11:24:16 +0200

    airbyte-ci: productionize up-to-date 🧹 + 🚀 (#39600)

--- airbyte-ci/connectors/pipelines/pipelines/airbyte_ci/connectors/migrate_to_poetry/pipeline.py
+++ airbyte-ci/connectors/pipelines/pipelines/airbyte_ci/connectors/migrate_to_poetry/pipeline.py
@@ -12,7 +12,6 @@ import requests
 import toml
 from connector_ops.utils import ConnectorLanguage  # type: ignore
 from jinja2 import Environment, PackageLoader, select_autoescape
-from pipelines.airbyte_ci.connectors.bump_version.pipeline import AddChangelogEntry, SetConnectorVersion, get_bumped_version
 from pipelines.airbyte_ci.connectors.consts import CONNECTOR_TEST_STEP_ID
 from pipelines.airbyte_ci.connectors.context import ConnectorContext, PipelineContext
 from pipelines.airbyte_ci.connectors.reports import ConnectorReport, Report
@@ -401,14 +400,9 @@ class UpdateReadMe(Step):
         )
 
 
-async def run_connector_migration_to_poetry_pipeline(
-    context: ConnectorContext, semaphore: "Semaphore", changelog: bool, bump: str | None
-) -> Report:
+async def run_connector_migration_to_poetry_pipeline(context: ConnectorContext, semaphore: "Semaphore") -> Report:
     restore_original_state = RestorePoetryState(context)
-    if bump:
-        new_version = get_bumped_version(context.connector.version, bump)
-    else:
-        new_version = None
+    new_version = None
     context.targeted_platforms = [LOCAL_BUILD_PLATFORM]
     steps_to_run: STEP_TREE = []
 
@@ -435,58 +429,6 @@ async def run_connector_migration_to_poetry_pipeline(
             )
         ]
     )
-
-    # steps_to_run.append(
-    #     [
-    #         StepToRun(
-    #             id=CONNECTOR_TEST_STEP_ID.BUILD,
-    #             step=BuildConnectorImages(context),
-    #             depends_on=[CONNECTOR_TEST_STEP_ID.MIGRATE_POETRY_DELETE_SETUP_PY],
-    #         )
-    #     ]
-    # )
-
-    # steps_to_run.append(
-    #     [
-    #         StepToRun(
-    #             id=CONNECTOR_TEST_STEP_ID.MIGRATE_POETRY_REGRESSION_TEST,
-    #             step=RegressionTest(context),
-    #             depends_on=[CONNECTOR_TEST_STEP_ID.BUILD],
-    #             args=lambda results: {
-    #                 "new_connector_container": results[CONNECTOR_TEST_STEP_ID.BUILD].output[LOCAL_BUILD_PLATFORM],
-    #                 "original_dependencies": results[CONNECTOR_TEST_STEP_ID.MIGRATE_POETRY_POETRY_INIT].output[0],
-    #                 "original_dev_dependencies": results[CONNECTOR_TEST_STEP_ID.MIGRATE_POETRY_POETRY_INIT].output[1],
-    #             },
-    #         )
-    #     ]
-    # )
-
-    if new_version:
-        steps_to_run.append(
-            [
-                StepToRun(
-                    id=CONNECTOR_TEST_STEP_ID.SET_CONNECTOR_VERSION,
-                    step=SetConnectorVersion(context, new_version),
-                    depends_on=[CONNECTOR_TEST_STEP_ID.MIGRATE_POETRY_DELETE_SETUP_PY],
-                )
-            ]
-        )
-
-    if new_version and changelog:
-        steps_to_run.append(
-            [
-                StepToRun(
-                    id=CONNECTOR_TEST_STEP_ID.ADD_CHANGELOG_ENTRY,
-                    step=AddChangelogEntry(
-                        context,
-                        new_version,
-                        "Manage dependencies with Poetry.",
-                        "0",
-                    ),
-                    depends_on=[CONNECTOR_TEST_STEP_ID.MIGRATE_POETRY_REGRESSION_TEST],
-                )
-            ]
-        )
 
     steps_to_run.append(
         [
