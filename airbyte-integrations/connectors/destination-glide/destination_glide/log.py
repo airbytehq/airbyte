98b96d2da52209dc218d07fdf6087df46fd67517
Scott Willeke <scott@willeke.com>
2024-06-21 23:16:58 -0400

    chore: cleanup logging

--- airbyte-integrations/connectors/destination-glide/destination_glide/log.py
+++ airbyte-integrations/connectors/destination-glide/destination_glide/log.py
@@ -1,21 +1,4 @@
 
 import logging
 
-# Create a logger
-
-logger = logging.getLogger("destination-glide")
-logger.setLevel(logging.DEBUG)
-
-# Create a file handler
-# TODO REMOVE?
-handler = logging.FileHandler('destination-glide.log')
-handler.setLevel(logging.INFO)
-# Create a logging format
-formatter = logging.Formatter(
-    '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-handler.setFormatter(formatter)
-# Add the handlers to the logger
-logger.addHandler(handler)
-
-def getLogger() -> logging.Logger: 
-  return logger
+LOG_LEVEL_DEFAULT = logging.INFO
