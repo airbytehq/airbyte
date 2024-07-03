b739ff6a2d126c2e67ce705ade5c24d10cfe1123
Catherine Noll <noll.catherine@gmail.com>
2024-07-03 14:30:26 +0100

    Exception for testing

--- airbyte-ci/connectors/live-tests/src/live_tests/validation_tests/test_read.py
+++ airbyte-ci/connectors/live-tests/src/live_tests/validation_tests/test_read.py
@@ -43,6 +43,7 @@ async def test_read(
     - Appropriate stream status messages are emitted for each stream
     - If a primary key exists for the stream, it is present in the records emitted
     """
+    raise ValueError("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
     has_records = False
     errors = []
     warnings = []
