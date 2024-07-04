3cb755e6ace5ce04510742e887a56b13473e0ff0
Aldo Gonzalez <aldo.gonzalez@airbyte.io>
2024-07-04 15:08:41 -0600

    Airbyte CDK:update _create_catalog to fully rely on ConfiguredAirbyteCatalog instead of CatalogBuilder

--- airbyte-cdk/python/unit_tests/sources/mock_server_tests/test_resumable_full_refresh.py
+++ airbyte-cdk/python/unit_tests/sources/mock_server_tests/test_resumable_full_refresh.py
@@ -51,14 +51,7 @@ class RequestBuilder:
         )
 
 
-def _create_catalog(names_and_sync_modes: List[tuple[str, SyncMode]]) -> ConfiguredAirbyteCatalog:
-    catalog_builder = CatalogBuilder()
-    for stream_name, sync_mode in names_and_sync_modes:
-        catalog_builder.with_stream(name=stream_name, sync_mode=sync_mode)
-    return catalog_builder.build()
-
-
-def _create_catalog_with_configured_schema(names_and_sync_modes: List[tuple[str, SyncMode, str: Any]]) -> ConfiguredAirbyteCatalog:
+def _create_catalog(names_and_sync_modes: List[tuple[str, SyncMode, str: Any]]) -> ConfiguredAirbyteCatalog:
     stream_builder = ConfiguredAirbyteStreamBuilder()
     streams = []
     for stream_name, sync_mode, json_schema in names_and_sync_modes:
@@ -146,7 +139,7 @@ class ResumableFullRefreshStreamTest(TestCase):
         )
 
         source = SourceFixture()
-        actual_messages = read(source, config=config, catalog=_create_catalog([("justice_songs", SyncMode.full_refresh)]))
+        actual_messages = read(source, config=config, catalog=_create_catalog([("justice_songs", SyncMode.full_refresh, {})]))
 
         assert emits_successful_sync_status_messages(actual_messages.get_stream_statuses("justice_songs"))
         assert len(actual_messages.records) == 5
@@ -164,42 +157,6 @@ class ResumableFullRefreshStreamTest(TestCase):
         assert actual_messages.state_messages[3].state.stream.stream_descriptor.name == "justice_songs"
         assert actual_messages.state_messages[3].state.stream.stream_state == AirbyteStateBlob()
         assert actual_messages.state_messages[3].state.sourceStats.recordCount == 0.0
-
-    @HttpMocker()
-    def test_resumable_full_refresh_sync_with_configured_schema(self, http_mocker):
-        config = {}
-
-        http_mocker.get(
-            _create_justice_songs_request().build(),
-            _create_response(pagination_has_more=False).with_record(record=_create_record("justice_songs")).build(),
-        )
-
-
-        source = SourceFixture()
-        json_schema: Dict[str:any] = {
-            "properties":
-                    {"id":
-                        {
-                            "description" : "id",
-                            "type" : "string"
-                        }
-            }
-        }
-        catalog = _create_catalog_with_configured_schema([("justice_songs", SyncMode.full_refresh, json_schema)])
-        assert catalog.streams[0].stream.json_schema == json_schema
-        actual_messages = read(source, config=config, catalog=catalog)
-
-        assert emits_successful_sync_status_messages(actual_messages.get_stream_statuses("justice_songs"))
-        assert len(actual_messages.records) == 1
-        assert len(actual_messages.state_messages) == 2
-        validate_message_order([Type.RECORD, Type.STATE, Type.STATE], actual_messages.records_and_state_messages)
-        assert actual_messages.state_messages[0].state.stream.stream_descriptor.name == "justice_songs"
-        assert actual_messages.state_messages[0].state.stream.stream_state == AirbyteStateBlob()
-        assert actual_messages.state_messages[0].state.sourceStats.recordCount == 1.0
-        assert actual_messages.state_messages[1].state.stream.stream_descriptor.name == "justice_songs"
-        assert actual_messages.state_messages[1].state.stream.stream_state == AirbyteStateBlob()
-        assert actual_messages.state_messages[1].state.sourceStats.recordCount == 0.0
-
 
     @HttpMocker()
     def test_resumable_full_refresh_second_attempt(self, http_mocker):
@@ -229,7 +186,7 @@ class ResumableFullRefreshStreamTest(TestCase):
         )
 
         source = SourceFixture()
-        actual_messages = read(source, config=config, catalog=_create_catalog([("justice_songs", SyncMode.full_refresh)]), state=state)
+        actual_messages = read(source, config=config, catalog=_create_catalog([("justice_songs", SyncMode.full_refresh, {})]), state=state)
 
         assert emits_successful_sync_status_messages(actual_messages.get_stream_statuses("justice_songs"))
         assert len(actual_messages.records) == 8
@@ -265,7 +222,7 @@ class ResumableFullRefreshStreamTest(TestCase):
         http_mocker.get(_create_justice_songs_request().with_page(2).build(), _create_response().with_status_code(status_code=400).build())
 
         source = SourceFixture()
-        actual_messages = read(source, config=config, catalog=_create_catalog([("justice_songs", SyncMode.full_refresh)]), expecting_exception=True)
+        actual_messages = read(source, config=config, catalog=_create_catalog([("justice_songs", SyncMode.full_refresh, {})]), expecting_exception=True)
 
         status_messages = actual_messages.get_stream_statuses("justice_songs")
         assert status_messages[-1] == AirbyteStreamStatus.INCOMPLETE
