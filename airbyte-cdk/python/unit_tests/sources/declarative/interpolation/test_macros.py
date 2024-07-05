fb1178557c601e0d24ac6581de5fbecb9e51bc59
cristina.mariscal <cristina.mariscal@cristina.mariscal--MacBook-Pro---DFJ27FJFXX>
2024-07-05 16:22:48 -0600

    fix test

--- airbyte-cdk/python/unit_tests/sources/declarative/interpolation/test_macros.py
+++ airbyte-cdk/python/unit_tests/sources/declarative/interpolation/test_macros.py
@@ -31,7 +31,7 @@ def test_macros_export(test_name, fn_name, found_in_macros):
     "test_name, input_value, format, expected_output",
     [
         ("test_datetime_string_to_date", "2022-01-01T01:01:01Z", "%Y-%m-%d", None, "2022-01-01"),
-        ("test_date_string_to_date", "2022-01-01", "%Y-%m-%d", "2022-01-01"),
+        ("test_date_string_to_date", "2022-01-01", "%Y-%m-%d", None, "2022-01-01"),
         ("test_datetime_string_to_date", "2022-01-01T00:00:00Z", "%Y-%m-%d", None, "2022-01-01"),
         ("test_datetime_with_tz_string_to_date", "2022-01-01T00:00:00Z", "%Y-%m-%d", None, "2022-01-01"),
         ("test_datetime_string_to_datetime", "2022-01-01T01:01:01Z", "%Y-%m-%dT%H:%M:%SZ", None, "2022-01-01T01:01:01Z"),
@@ -43,7 +43,7 @@ def test_macros_export(test_name, fn_name, found_in_macros):
 )
 def test_format_datetime(test_name, input_value, format, input_format, expected_output):
     format_datetime = macros["format_datetime"]
-    assert format_datetime(input_value, format) == expected_output
+    assert format_datetime(input_value, format, input_format) == expected_output
 
 
 @pytest.mark.parametrize(
