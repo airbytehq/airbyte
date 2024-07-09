af78ddafed2e27a7ac43c5c1d01804e0af84dc41
Anatolii Yatsuk <tolikyatsuk@gmail.com>
2024-07-09 16:49:43 +0300

    Add Slack changes

--- airbyte-cdk/python/airbyte_cdk/sources/declarative/parsers/model_to_component_factory.py
+++ airbyte-cdk/python/airbyte_cdk/sources/declarative/parsers/model_to_component_factory.py
@@ -630,7 +630,7 @@ class ModelToComponentFactory:
             and hasattr(model.incremental_sync, "is_client_side_incremental")
             and model.incremental_sync.is_client_side_incremental
         ):
-            if combined_slicers and not isinstance(combined_slicers, (DatetimeBasedCursor, PerPartitionCursor)):
+            if combined_slicers and not isinstance(combined_slicers, (DatetimeBasedCursor, GlobalParentCursor, PerPartitionCursor)):
                 raise ValueError("Unsupported Slicer is used. PerPartitionCursor should be used here instead")
             client_side_incremental_sync = {
                 "date_time_based_cursor": self._create_component_from_model(model=model.incremental_sync, config=config),
---
+++ airbyte-integrations/connectors/source-slack/source_slack/components/threads_state_migrations.py
@@ -0,0 +1,26 @@
+from airbyte_cdk.sources.declarative.migrations.state_migration import StateMigration
+from typing import Any, Mapping
+
+
+class ThreadsStateMigration(StateMigration):
+    """
+    Migrate threads state
+    """
+
+    def should_migrate(self, stream_state: Mapping[str, Any]) -> bool:
+        """
+        Check if the stream_state should be migrated
+        :param stream_state: The stream_state to potentially migrate
+        :return: true if the state is of the expected format and should be migrated. False otherwise.
+        """
+        return "state" not in stream_state and "float_ts" in stream_state
+
+    def migrate(self, stream_state: Mapping[str, Any]) -> Mapping[str, Any]:
+        """
+        Migrate the stream_state. Assumes should_migrate(stream_state) returned True.
+        :param stream_state: The stream_state to migrate
+        :return: The migrated stream_state
+        """
+        migrated_stream_state = {"state": {"ts": stream_state["float_ts"]},
+                                 "parent_state": {"channel_messages": {"state": stream_state}}}
+        return migrated_stream_state
