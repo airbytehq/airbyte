d383bf10af5d10aba66259d2eb84e62635030cbf
Serhii Lazebnyi <serhii.lazebnyi@globallogic.com>
2024-07-11 23:45:53 +0200

    Update approach with saing original value

--- airbyte-cdk/python/airbyte_cdk/sources/streams/utils/stream_helper.py
+++ airbyte-cdk/python/airbyte_cdk/sources/streams/utils/stream_helper.py
@@ -37,6 +37,7 @@ def get_first_record_for_slice(stream: Stream, stream_slice: Optional[Mapping[st
     """
     # Store the original value of exit_on_rate_limit
     exit_on_rate_limit_has_setter = hasattr(stream.exit_on_rate_limit, "setter")
+    original_exit_on_rate_limit = stream.exit_on_rate_limit
 
     try:
         # Ensure exit_on_rate_limit is safely set to True if possible
@@ -51,4 +52,4 @@ def get_first_record_for_slice(stream: Stream, stream_slice: Optional[Mapping[st
     finally:
         # Restore the original exit_on_rate_limit value
         if exit_on_rate_limit_has_setter:
-            stream.exit_on_rate_limit = False  # type: ignore[misc]
+            stream.exit_on_rate_limit = original_exit_on_rate_limit  # type: ignore[misc]
