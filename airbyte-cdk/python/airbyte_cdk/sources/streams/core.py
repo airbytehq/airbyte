496615b37a29ab96d73f7fef5c15c9497e54d38e
Aldo Gonzalez <aldo.gonzalez@airbyte.io>
2024-07-12 17:19:42 -0600

    Airbyte CDK: clean invalid fields from configured catalog

--- airbyte-cdk/python/airbyte_cdk/sources/streams/core.py
+++ airbyte-cdk/python/airbyte_cdk/sources/streams/core.py
@@ -544,4 +544,20 @@ class Stream(ABC):
 
     @configured_json_schema.setter
     def configured_json_schema(self, json_schema: Dict[str, Any]) -> None:
-        self._configured_json_schema = json_schema
+        self._configured_json_schema = self._filter_schema_invalid_properties(json_schema)
+
+    def _filter_schema_invalid_properties(self, json_schema: Dict[str, Any]):
+        """
+        Filters the properties in json_schema that are not present in the stream schema.
+        Configured Schemas can have very old fields, so we need to housekeeping ourselves.
+        """
+        if self.get_json_schema():
+            received_schema_properties = json_schema.get("properties")
+            stream_schema_properties = self.get_json_schema().get("properties")
+            valid_configured_schema_properties = {
+                stream_property: property_value
+                for stream_property, property_value in received_schema_properties.items()
+                if stream_property in stream_schema_properties
+            }
+            return {**json_schema, "properties": valid_configured_schema_properties}
+        return json_schema
