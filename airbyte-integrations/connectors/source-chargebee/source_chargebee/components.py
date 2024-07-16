bc953ce515fe0d8165b30c0b5e3c035b6e90ca5c
Patrick Nilan <nilan.patrick@gmail.com>
2024-07-16 16:03:54 -0700

    Update cursor

--- airbyte-integrations/connectors/source-chargebee/source_chargebee/components.py
+++ airbyte-integrations/connectors/source-chargebee/source_chargebee/components.py
@@ -5,7 +5,7 @@
 from dataclasses import InitVar, dataclass
 from typing import Any, Iterable, Mapping, Optional, Union
 
-from airbyte_cdk.sources.declarative.incremental.cursor import Cursor
+from airbyte_cdk.sources.declarative.incremental import CursorFactory as Cursor
 from airbyte_cdk.sources.declarative.interpolation.interpolated_string import InterpolatedString
 from airbyte_cdk.sources.declarative.requesters.request_option import RequestOptionType
 from airbyte_cdk.sources.declarative.transformations.transformation import RecordTransformation
