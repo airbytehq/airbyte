5b258d6d655f8e01b7b8f37efd72348b8f3afc59
Serhii Lazebnyi <serhii.lazebnyi@globallogic.com>
2024-07-16 11:39:19 +0200

    Remove handlers for utils

--- airbyte-integrations/connectors/source-mixpanel/source_mixpanel/utils.py
+++ airbyte-integrations/connectors/source-mixpanel/source_mixpanel/utils.py
@@ -71,65 +71,6 @@ def fix_date_time(record: Union[Mapping[str, Any], Dict[str, Any], List[Any]]) -
     return None
 
 
-class MixpanelStreamErrorHandler(HttpStatusErrorHandler):
-    def interpret_response(self, response_or_exception: Optional[Union[requests.Response, Exception]] = None) -> ErrorResolution:
-        if isinstance(response_or_exception, requests.Response):
-            if response_or_exception.status_code == 400 and "Unable to authenticate request" in response_or_exception.text:
-                message = (
-                    f"Your credentials might have expired. Please update your config with valid credentials."
-                    f" See more details: {response_or_exception.text}"
-                )
-                return ErrorResolution(
-                    response_action=ResponseAction.FAIL,
-                    failure_type=FailureType.config_error,
-                    error_message=message,
-                )
-            elif response_or_exception.status_code == 402:
-                message = f"Unable to perform a request. Payment Required: {response_or_exception.json()['error']}"
-                return ErrorResolution(
-                    response_action=ResponseAction.FAIL,
-                    failure_type=FailureType.transient_error,
-                    error_message=message,
-                )
-        return super().interpret_response(response_or_exception)
-
-
-class DateSlicesMixinErrorHandler(HttpStatusErrorHandler):
-    def __init__(self, stream: HttpStream, **kwargs):  # type: ignore # noqa
-        self.stream = stream
-        super().__init__(**kwargs)
-
-    def interpret_response(self, response_or_exception: Optional[Union[requests.Response, Exception]] = None) -> ErrorResolution:
-        if isinstance(response_or_exception, requests.Response):
-            if (
-                response_or_exception.status_code == requests.codes.bad_request
-                and "to_date cannot be later than today" in response_or_exception.text
-            ):
-                self.stream._timezone_mismatch = True
-                message = "Your project timezone must be misconfigured. Please set it to the one defined in your Mixpanel project settings. Stopping current stream sync."
-                return ErrorResolution(
-                    response_action=ResponseAction.IGNORE,
-                    failure_type=FailureType.config_error,
-                    error_message=message,
-                )
-        return super().interpret_response(response_or_exception)
-
-
-class ExportErrorHandler(MixpanelStreamErrorHandler, DateSlicesMixinErrorHandler):
-    def interpret_response(self, response_or_exception: Optional[Union[requests.Response, Exception]] = None) -> ErrorResolution:
-        if isinstance(response_or_exception, requests.Response):
-            try:
-                # trying to parse response to avoid ConnectionResetError and retry if it occurs
-                self.stream.iter_dicts(response_or_exception.iter_lines(decode_unicode=True))
-            except ConnectionResetError:
-                return ErrorResolution(
-                    response_action=ResponseAction.RETRY,
-                    failure_type=FailureType.transient_error,
-                    error_message=f"Response status code: {response_or_exception.status_code}. Retrying...",  # type: ignore[union-attr]
-                )
-        return super().interpret_response(response_or_exception)
-
-
 class MixpanelStreamBackoffStrategy(BackoffStrategy):
     def __init__(self, stream: HttpStream, **kwargs):  # type: ignore # noqa
         self.stream = stream
