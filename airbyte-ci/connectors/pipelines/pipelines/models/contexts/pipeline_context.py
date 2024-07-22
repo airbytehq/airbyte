6d08b3ced6573eba23684f799cdd76479268d25e
Catherine Noll <noll.catherine@gmail.com>
2024-07-22 13:22:59 -0400

    fixup! airbyte-ci: add status check for regression test runs

--- airbyte-ci/connectors/pipelines/pipelines/models/contexts/pipeline_context.py
+++ airbyte-ci/connectors/pipelines/pipelines/models/contexts/pipeline_context.py
@@ -219,7 +219,9 @@ class PipelineContext:
         return self.is_ci and bool(self.ci_report_bucket) and bool(self.ci_gcp_credentials)
 
     def _should_send_status_check(self) -> bool:
-        should_send = self.is_pr or any(self.pipeline_name.startswith(override) for override in MANUAL_PIPELINE_STATUS_CHECK_OVERRIDE_PREFIXES)
+        should_send = self.is_pr or any(
+            self.pipeline_name.startswith(override) for override in MANUAL_PIPELINE_STATUS_CHECK_OVERRIDE_PREFIXES
+        )
         self.logger.info(f"Should send status check: {should_send}")
         return should_send
 
