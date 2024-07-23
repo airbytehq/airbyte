06117b6f84aee86eddd2d9606a695f8272396ae4
Artem Inzhyyants <artem.inzhyyants@gmail.com>
2024-07-23 16:49:28 +0200

    Airbyte CDK: use time; migrate to match case
    Signed-off-by: Artem Inzhyyants <artem.inzhyyants@gmail.com>

--- airbyte-cdk/python/airbyte_cdk/sources/utils/record_helper.py
+++ airbyte-cdk/python/airbyte_cdk/sources/utils/record_helper.py
@@ -2,7 +2,7 @@
 # Copyright (c) 2023 Airbyte, Inc., all rights reserved.
 #
 
-import datetime
+import time
 from typing import Any, Mapping
 
 from airbyte_cdk.models import AirbyteLogMessage, AirbyteMessage, AirbyteRecordMessage, AirbyteTraceMessage
@@ -20,19 +20,20 @@ def stream_data_to_airbyte_message(
     if schema is None:
         schema = {}
 
-    if isinstance(data_or_message, Mapping):
-        data = dict(data_or_message)
-        now_millis = int(datetime.datetime.now().timestamp() * 1000)
-        # Transform object fields according to config. Most likely you will
-        # need it to normalize values against json schema. By default no action
-        # taken unless configured. See
-        # docs/connector-development/cdk-python/schemas.md for details.
-        transformer.transform(data, schema)  # type: ignore
-        message = AirbyteRecordMessage(stream=stream_name, data=data, emitted_at=now_millis)
-        return AirbyteMessage(type=MessageType.RECORD, record=message)
-    elif isinstance(data_or_message, AirbyteTraceMessage):
-        return AirbyteMessage(type=MessageType.TRACE, trace=data_or_message)
-    elif isinstance(data_or_message, AirbyteLogMessage):
-        return AirbyteMessage(type=MessageType.LOG, log=data_or_message)
-    else:
-        raise ValueError(f"Unexpected type for data_or_message: {type(data_or_message)}: {data_or_message}")
+    match data_or_message:
+        case Mapping():
+            data = dict(data_or_message)
+            now_millis = time.time_ns() // 1_000_000
+            # Transform object fields according to config. Most likely you will
+            # need it to normalize values against json schema. By default no action
+            # taken unless configured. See
+            # docs/connector-development/cdk-python/schemas.md for details.
+            transformer.transform(data, schema)  # type: ignore
+            message = AirbyteRecordMessage(stream=stream_name, data=data, emitted_at=now_millis)
+            return AirbyteMessage(type=MessageType.RECORD, record=message)
+        case AirbyteTraceMessage():
+            return AirbyteMessage(type=MessageType.TRACE, trace=data_or_message)
+        case  AirbyteLogMessage():
+            return AirbyteMessage(type=MessageType.LOG, log=data_or_message)
+        case _:
+            raise ValueError(f"Unexpected type for data_or_message: {type(data_or_message)}: {data_or_message}")
