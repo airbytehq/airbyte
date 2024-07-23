328631d2de7be0369789b725d3b5c6bb81c5d369
Serhii Lazebnyi <serhii.lazebnyi@globallogic.com>
2024-07-23 23:56:46 +0200

    Add new retriver

--- airbyte-integrations/connectors/source-linkedin-ads/source_linkedin_ads/components.py
+++ airbyte-integrations/connectors/source-linkedin-ads/source_linkedin_ads/components.py
@@ -7,7 +7,7 @@ import datetime
 from collections import defaultdict
 from copy import deepcopy
 from dataclasses import dataclass, field
-from typing import Any, Iterable, List, Mapping, MutableMapping, Optional, Union
+from typing import Any, Callable, Iterable, List, Mapping, MutableMapping, Optional, Union
 from urllib.parse import urlencode, urljoin
 
 import pendulum
@@ -272,3 +272,128 @@ class LinkedInAdsCustomRetriever(SimpleRetriever):
 
         if transformations:
             self.record_selector.transformations = transformations
+
+@dataclass
+class LinkedInAdsCustomRetriever(SimpleRetriever):
+    partition_router: Optional[Union[List[StreamSlicer], StreamSlicer]] = field(
+        default_factory=lambda: SinglePartitionRouter(parameters={})
+    )
+
+    def __post_init__(self, parameters: Mapping[str, Any]) -> None:
+        super().__post_init__(parameters)
+        self.cursor = self._initialize_cursor()
+
+    def _initialize_cursor(self):
+        partition_router = (
+            CartesianProductStreamSlicer(self.partition_router, parameters={})
+            if isinstance(self.partition_router, list)
+            else self.partition_router
+        )
+
+        return PerPartitionCursor(
+            cursor_factory=CursorFactory(
+                lambda: deepcopy(self.stream_slicer),
+            ),
+            partition_router=partition_router,
+        )
+
+    def stream_slices(self) -> Iterable[Optional[StreamSlice]]:
+        return self.cursor.stream_slices()
+
+    def read_records(
+        self,
+        records_schema: Mapping[str, Any],
+        stream_slice: Optional[StreamSlice] = None,
+    ) -> Iterable[StreamData]:
+        merged_records = defaultdict(dict)
+
+        self._apply_transformations()
+
+        for field_slice in stream_slice.cursor_slice.get("field_date_chunks", []):
+            updated_slice = StreamSlice(partition=stream_slice.partition, cursor_slice={**field_slice})
+            for record in super().read_records(records_schema, stream_slice=updated_slice):
+                merged_records[f"{record['end_date']}-{record['pivotValues']}"].update(record)
+
+        yield from merged_records.values()
+
+
+    # This logic is similar to _read_pages in the HttpStream class. When making changes here, consider making changes there as well.
+    def _read_pages(
+        self,
+        records_generator_fn: Callable[[Optional[requests.Response]], Iterable[StreamData]],
+        stream_state: Mapping[str, Any],
+        stream_slice: StreamSlice,
+    ) -> Iterable[StreamData]:
+        pagination_complete = False
+        next_page_token = None
+        while not pagination_complete:
+            response = self._fetch_next_page(stream_state, stream_slice, next_page_token)
+            breakpoint()
+            yield from records_generator_fn(response)
+
+            if not response:
+                pagination_complete = True
+            else:
+                next_page_token = self._next_page_token(response)
+                if not next_page_token:
+                    pagination_complete = True
+
+        # Always return an empty generator just in case no records were ever yielded
+        yield from []
+
+    def _apply_transformations(self):
+        transformations = [
+            AddFields(
+                fields=[
+                    AddedFieldDefinition(
+                        path=field["path"],
+                        value=InterpolatedString(string=field["value"], default=field["value"], parameters={}),
+                        value_type=str,
+                        parameters={},
+                    )
+                    for field in transformation.get("fields", [])
+                ],
+                parameters={},
+            )
+            for transformation in self.record_selector.transformations
+            if isinstance(transformation, dict)
+        ]
+
+        if transformations:
+            self.record_selector.transformations = transformations
+
+
+
+@dataclass
+class ChunkedHttpRequester(SafeEncodeHttpRequester):
+    def send_request(
+        self,
+        stream_state: Optional[StreamState] = None,
+        stream_slice: Optional[StreamSlice] = None,
+        next_page_token: Optional[Mapping[str, Any]] = None,
+        path: Optional[str] = None,
+        request_headers: Optional[Mapping[str, Any]] = None,
+        request_params: Optional[Mapping[str, Any]] = None,
+        request_body_data: Optional[Union[Mapping[str, Any], str]] = None,
+        request_body_json: Optional[Mapping[str, Any]] = None,
+        log_formatter: Optional[Callable[[requests.Response], Any]] = None,
+    ) -> Optional[requests.Response]:
+        breakpoint()
+        request, response = self._http_client.send_request(
+            http_method=self.get_method().value,
+            url=self._join_url(
+                self.get_url_base(),
+                path or self.get_path(stream_state=stream_state, stream_slice=stream_slice, next_page_token=next_page_token),
+            ),
+            request_kwargs={},
+            headers=self._request_headers(stream_state, stream_slice, next_page_token, request_headers),
+            params=self._request_params(stream_state, stream_slice, next_page_token, request_params),
+            json=self._request_body_json(stream_state, stream_slice, next_page_token, request_body_json),
+            data=self._request_body_data(stream_state, stream_slice, next_page_token, request_body_data),
+            dedupe_query_params=True,
+            log_formatter=log_formatter,
+            exit_on_rate_limit=self._exit_on_rate_limit,
+        )
+
+        return response
+
