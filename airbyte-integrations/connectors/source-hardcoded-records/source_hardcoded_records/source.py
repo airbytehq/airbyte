b41be5bbeb678775debf7d4d4e4ffa18da12fa41
Artem Inzhyyants <artem.inzhyyants@gmail.com>
2024-07-23 14:30:26 +0200

    Source Hardcoded Records: fmt
    Signed-off-by: Artem Inzhyyants <artem.inzhyyants@gmail.com>

--- airbyte-integrations/connectors/source-hardcoded-records/source_hardcoded_records/source.py
+++ airbyte-integrations/connectors/source-hardcoded-records/source_hardcoded_records/source.py
@@ -1,6 +1,5 @@
-#
-# Copyright (c) 2023 Airbyte, Inc., all rights reserved.
-#
+# Copyright (c) 2024 Airbyte, Inc., all rights reserved.
+
 
 import logging
 from typing import Any, List, Mapping, Tuple
