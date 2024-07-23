a96a68ad5178acd037a83311b6812e88677e35e6
Xiaohan Song <xiaohan@airbyte.io>
2024-07-23 15:31:01 -0700

    comment PR and release ci

--- airbyte-integrations/connectors/source-postgres/integration_tests/seed/hook.py
+++ airbyte-integrations/connectors/source-postgres/integration_tests/seed/hook.py
@@ -7,9 +7,10 @@ import string
 import sys
 from datetime import timedelta
 from pathlib import Path
+from typing import List, Tuple
 
 import psycopg2
-from psycopg2 import sql
+from psycopg2 import extensions, sql
 
 catalog_write_file = "/connector/integration_tests/temp/configured_catalog_copy.json"
 catalog_source_file = "/connector/integration_tests/configured_catalog_template.json"
@@ -21,13 +22,12 @@ abnormal_state_file = "/connector/integration_tests/abnormal_state_template.json
 secret_config_file = '/connector/secrets/config.json'
 secret_active_config_file = '/connector/integration_tests/config_active.json'
 
-def connect_to_db():
+def connect_to_db() -> extensions.connection:
     with open(secret_config_file) as f:
         secret = json.load(f)
 
     try:
-        # Define connection parameters
-        connection = psycopg2.connect(
+        conn: extensions.connection = psycopg2.connect(
             dbname=secret["database"],
             user=secret["username"],
             password=secret["password"],
@@ -35,14 +35,14 @@ def connect_to_db():
             port=secret["port"]
         )
         print("Connected to the database successfully")
-        return connection
+        return conn
     except Exception as error:
         print(f"Error connecting to the database: {error}")
-        return None
+        sys.exit(1)
 
-def insert_records(connection, schema_name, table_name, records):
+def insert_records(conn: extensions.connection, schema_name: str, table_name: str, records: List[Tuple[str, str]]) -> None:
     try:
-        cursor = connection.cursor()
+        cursor = conn.cursor()
         insert_query = sql.SQL("""
             INSERT INTO {}.{} (id, name)
             VALUES (%s, %s) ON CONFLICT DO NOTHING
@@ -51,33 +51,29 @@ def insert_records(connection, schema_name, table_name, records):
         for record in records:
             cursor.execute(insert_query, record)
 
-        connection.commit()
+        conn.commit()
         print("Records inserted successfully")
     except Exception as error:
         print(f"Error inserting records: {error}")
-        connection.rollback()
+        conn.rollback()
     finally:
         cursor.close()
 
-def create_schema(connection, schema_name):
+def create_schema(conn: extensions.connection, schema_name: str) -> None:
     try:
-        cursor = connection.cursor()
-        # Create schema
+        cursor = conn.cursor()
         create_schema_query = sql.SQL("CREATE SCHEMA IF NOT EXISTS {}").format(sql.Identifier(schema_name))
         cursor.execute(create_schema_query)
-        connection.commit()
+        conn.commit()
         print(f"Schema '{schema_name}' created successfully")
-
     except Exception as error:
         print(f"Error creating schema: {error}")
-        connection.rollback()
+        conn.rollback()
     finally:
         cursor.close()
 
-# We need to dynamically generate catalog and config files, by feeding them schema_name;
-# These files will be used in acceptance-test-config.yml as inputs of various test cases.
-def write_supporting_file(schema_name):
-    print(f"writting schema name to files: {schema_name}")
+def write_supporting_file(schema_name: str) -> None:
+    print(f"writing schema name to files: {schema_name}")
     Path("/connector/integration_tests/temp").mkdir(parents=False, exist_ok=True)
 
     with open(catalog_write_file, "w") as file:
@@ -89,17 +85,16 @@ def write_supporting_file(schema_name):
     with open(abnormal_state_write_file, "w") as file:
         with open(abnormal_state_file, 'r') as source_file:
             file.write(source_file.read() % (schema_name, schema_name))
-    # update configs:
-    with open(secret_config_file) as base_config:
-      secret = json.load(base_config)
-      secret["schemas"] = [schema_name]
-      with open(secret_active_config_file, 'w') as f:
-        json.dump(secret, f)
 
-def create_table(connection, schema_name, table_name):
+    with open(secret_config_file) as base_config:
+        secret = json.load(base_config)
+        secret["schemas"] = [schema_name]
+        with open(secret_active_config_file, 'w') as f:
+            json.dump(secret, f)
+
+def create_table(conn: extensions.connection, schema_name: str, table_name: str) -> None:
     try:
-        cursor = connection.cursor()
-        # Create table
+        cursor = conn.cursor()
         create_table_query = sql.SQL("""
             CREATE TABLE IF NOT EXISTS {}.{} (
                 id VARCHAR(100) PRIMARY KEY,
@@ -108,61 +103,50 @@ def create_table(connection, schema_name, table_name):
         """).format(sql.Identifier(schema_name), sql.Identifier(table_name))
 
         cursor.execute(create_table_query)
-        connection.commit()
+        conn.commit()
         print(f"Table '{schema_name}.{table_name}' created successfully")
-
     except Exception as error:
         print(f"Error creating table: {error}")
-        connection.rollback()
+        conn.rollback()
     finally:
         cursor.close()
 
-def generate_schema_date_with_suffix():
+def generate_schema_date_with_suffix() -> str:
     current_date = datetime.datetime.now().strftime("%Y%m%d")
     suffix = ''.join(random.choices(string.ascii_lowercase + string.digits, k=8))
     return f"{current_date}-{suffix}"
 
-def prepare():
+def prepare() -> None:
     schema_name = generate_schema_date_with_suffix()
     with open("./generated_schema.txt", "w") as f:
         f.write(schema_name)
 
-def setup():
+def setup() -> None:
     schema_name = load_schema_name_from_catalog()
     write_supporting_file(schema_name)
     table_name = "id_and_name_cat"
 
-    # Define the records to be inserted
     records = [
         ('1', 'one'),
         ('2', 'two'),
         ('3', 'three')
     ]
 
-    # Connect to the database
-    connection = connect_to_db()
+    conn = connect_to_db()
 
-    if connection:
-        # Create the schema
-        create_schema(connection, schema_name)
-        create_table(connection, schema_name, table_name)
+    if conn:
+        create_schema(conn, schema_name)
+        create_table(conn, schema_name, table_name)
+        insert_records(conn, schema_name, table_name, records)
+        conn.close()
 
-        # Insert the records
-        insert_records(connection, schema_name, table_name, records)
-
-        # Close the connection
-        connection.close()
-
-def load_schema_name_from_catalog():
+def load_schema_name_from_catalog() -> str:
     with open("./generated_schema.txt", "r") as f:
         return f.read()
 
-def delete_schemas_with_prefix(conn, date_prefix):
+def delete_schemas_with_prefix(conn: extensions.connection, date_prefix: str) -> None:
     try:
-        # Connect to the PostgreSQL database
         cursor = conn.cursor()
-
-        # Query to find all schemas that start with the specified date prefix
         query = sql.SQL("""
             SELECT schema_name
             FROM information_schema.schemata
@@ -172,7 +156,6 @@ def delete_schemas_with_prefix(conn, date_prefix):
         cursor.execute(query, (f"{date_prefix}%",))
         schemas = cursor.fetchall()
 
-        # Generate and execute DROP SCHEMA statements for each matching schema
         for schema in schemas:
             drop_query = sql.SQL("DROP SCHEMA IF EXISTS {} CASCADE;").format(sql.Identifier(schema[0]))
             cursor.execute(drop_query)
@@ -181,19 +164,18 @@ def delete_schemas_with_prefix(conn, date_prefix):
         conn.commit()
     except Exception as e:
         print(f"An error occurred: {e}")
+        sys.exit(1)
     finally:
-        if cursor:
-            cursor.close()
-        if conn:
-            conn.close()
+        cursor.close()
+        conn.close()
 
-def teardown():
-    connection = connect_to_db()
+def teardown() -> None:
+    conn = connect_to_db()
     today = datetime.datetime.now()
     yesterday = today - timedelta(days=1)
     formatted_yesterday = yesterday.strftime('%Y%m%d')
     print(f"formatted_yesterday: {formatted_yesterday}")
-    delete_schemas_with_prefix(connection, formatted_yesterday)
+    delete_schemas_with_prefix(conn, formatted_yesterday)
 
 if __name__ == "__main__":
     command = sys.argv[1]
@@ -203,3 +185,5 @@ if __name__ == "__main__":
         teardown()
     elif command == "prepare":
         prepare()
+    else:
+        ra
