d6231b3c2d9fc8db23437d73a08b542d9b5e6e79
btkcodedev <btk.codedev@gmail.com>
2024-07-24 10:20:45 +0530

    Migrate to low code

--- airbyte-integrations/connectors/source-xkcd/integration_tests/acceptance.py
+++ airbyte-integrations/connectors/source-xkcd/integration_tests/acceptance.py
@@ -1,6 +1,7 @@
 #
-# Copyright (c) 2023 Airbyte, Inc., all rights reserved.
+# Copyright (c) 2024 Airbyte, Inc., all rights reserved.
 #
+
 
 import pytest
 
@@ -10,4 +11,6 @@ pytest_plugins = ("connector_acceptance_test.plugin",)
 @pytest.fixture(scope="session", autouse=True)
 def connector_setup():
     """This fixture is a placeholder for external resources that acceptance test might require."""
+    # TODO: setup test dependencies if needed. otherwise remove the TODO comments
     yield
+    # TODO: clean up test dependencies
