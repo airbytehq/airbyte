0a91fb734c03e5a78ad27a3d1ea64a6a4b003f7a
Dhroov Makwana <pabloescoder@gmail.com>
2024-07-30 00:53:05 +0530

    Migrate to Poetry

--- airbyte-integrations/connectors/source-braze/unit_tests/test_datetime_incremental_sync.py
+++ airbyte-integrations/connectors/source-braze/unit_tests/test_datetime_incremental_sync.py
@@ -1,5 +1,5 @@
 #
-# Copyright (c) 2023 Airbyte, Inc., all rights reserved.
+# Copyright (c) 2024 Airbyte, Inc., all rights reserved.
 #
 
 from airbyte_cdk.sources.declarative.requesters import RequestOption
