9cf93a5a7d41f9ed4175152b2bd9acc0fdffbb76
darynaishchenko <darina.ishchenko17@gmail.com>
2024-08-01 13:24:17 +0300

    fix state for parent streams

--- airbyte-integrations/connectors/source-jira/source_jira/utils.py
+++ airbyte-integrations/connectors/source-jira/source_jira/utils.py
@@ -22,5 +22,5 @@ def read_full_refresh(stream_instance: Stream):
 
 
 def read_incremental(stream_instance: Stream, stream_state: MutableMapping[str, Any]):
-    stream_instance.state = stream_state
+    stream_instance.state = stream_state.copy()
     yield from stream_instance.read_only_records(stream_state)
