d4ec9e7cd10f15e68208d8caad25765ebc6b1d89
sajarindider <sajarindider@gmail.com>
2024-08-06 17:35:09 -0400

    chore: address review comments

--- airbyte-integrations/connectors/source-northpass-lms/main.py
+++ airbyte-integrations/connectors/source-northpass-lms/main.py
@@ -2,12 +2,7 @@
 # Copyright (c) 2024 Airbyte, Inc., all rights reserved.
 #
 
-
-import sys
-
-from airbyte_cdk.entrypoint import launch
-from source_northpass_lms import SourceNorthpassLms
+from source_northpass_lms.run import run
 
 if __name__ == "__main__":
-    source = SourceNorthpassLms()
-    launch(source, sys.argv[1:])
+    run()
