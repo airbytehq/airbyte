ae4cba2ffd7b1055486e57a5712e8ec24fe2a0ae
sajarindider <sajarindider@gmail.com>
2024-08-06 19:41:00 -0400

    format: run prettier and isort on files

--- airbyte-integrations/connectors/source-hibob/source_hibob/source.py
+++ airbyte-integrations/connectors/source-hibob/source_hibob/source.py
@@ -2,9 +2,7 @@
 # Copyright (c) 2024 Airbyte, Inc., all rights reserved.
 #
 
-from airbyte_cdk.sources.declarative.yaml_declarative_source import (
-    YamlDeclarativeSource,
-)
+from airbyte_cdk.sources.declarative.yaml_declarative_source import YamlDeclarativeSource
 
 """
 This file provides the necessary constructs to interpret a provided declarative YAML configuration file into
