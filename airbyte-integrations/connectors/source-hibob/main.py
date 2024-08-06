80c75a4092f64aeea65982ad342f82e48c3ffbf0
sajarindider <sajarindider@gmail.com>
2024-08-06 17:45:25 -0400

    chore: address review comments

--- airbyte-integrations/connectors/source-hibob/main.py
+++ airbyte-integrations/connectors/source-hibob/main.py
@@ -3,11 +3,7 @@
 #
 
 
-import sys
-
-from airbyte_cdk.entrypoint import launch
-from source_hibob import SourceHibob
+from source_hibob.run import run
 
 if __name__ == "__main__":
-    source = SourceHibob()
-    launch(source, sys.argv[1:])
+    run()
