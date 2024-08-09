b1ecee59eb4e36fce952ab7fedbaca5927688838
Erick Corona <erick.corona@airbyte.io>
2024-08-09 12:51:52 -0600

    Apply formatting

--- airbyte-cdk/python/airbyte_cdk/test/utils/http_mocking.py
+++ airbyte-cdk/python/airbyte_cdk/test/utils/http_mocking.py
@@ -1,11 +1,12 @@
 # Copyright (c) 2024 Airbyte, Inc., all rights reserved.
 
 import re
-from typing import Mapping, Any
+from typing import Any, Mapping
+
 from requests_mock import Mocker
 
 
-def register_mock_responses(mocker: Mocker, http_calls: list[Mapping[str, Mapping[str, Any]]]):
+def register_mock_responses(mocker: Mocker, http_calls: list[Mapping[str, Mapping[str, Any]]]) -> None:
     """Register a list of HTTP request-response pairs."""
     for call in http_calls:
         request, response = call["request"], call["response"]
