5b20bd1ab123d76fcb566ba22e8eba99bbe2462b
maxi297 <maxime@airbyte.io>
2024-08-12 14:27:21 -0400

    linting

--- airbyte-cdk/python/unit_tests/sources/streams/http/error_handlers/test_response_models.py
+++ airbyte-cdk/python/unit_tests/sources/streams/http/error_handlers/test_response_models.py
@@ -11,6 +11,7 @@ from airbyte_protocol.models import FailureType
 _A_SECRET = "a-secret"
 _A_URL = "https://a-url.com"
 
+
 class DefaultErrorResolutionTest(TestCase):
 
     def setUp(self) -> None:
@@ -25,7 +26,7 @@ class DefaultErrorResolutionTest(TestCase):
 
         assert error_resolution.failure_type == FailureType.system_error
         assert error_resolution.response_action == ResponseAction.RETRY
-        assert error_resolution.error_message ==  "Error handler did not receive a valid response or exception. This is unexpected please contact Airbyte Support"
+        assert error_resolution.error_message == "Error handler did not receive a valid response or exception. This is unexpected please contact Airbyte Support"
 
     def test_given_exception_when_create_fallback_error_resolution_then_return_error_resolution(self) -> None:
         exception = ValueError("This is an exception")
