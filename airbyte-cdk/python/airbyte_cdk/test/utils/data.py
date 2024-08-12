a158d095606dd391b4222bb03e2e670531a8578b
Erick Corona <erick.corona@airbyte.io>
2024-08-12 11:51:47 -0600

    Fix file path type annotation

--- airbyte-cdk/python/airbyte_cdk/test/utils/data.py
+++ airbyte-cdk/python/airbyte_cdk/test/utils/data.py
@@ -1,11 +1,9 @@
 # Copyright (c) 2024 Airbyte, Inc., all rights reserved.
 
-from pathlib import Path as FilePath
-
 from airbyte_cdk.test.mock_http.response_builder import get_unit_test_folder
 
 
-def read_resource_file_contents(resource: str, test_location: FilePath) -> str:
+def read_resource_file_contents(resource: str, test_location: str) -> str:
     """Read the contents of a test data file from the test resource folder."""
     file_path = str(get_unit_test_folder(test_location) / "resource" / "http" / "response" / f"{resource}")
     with open(file_path) as f:
