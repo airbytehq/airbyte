432f160a03fb92d51b40b497596650b8a0a8df93
Marius Posta <marius@airbyte.io>
2024-08-13 17:00:31 -0400

    remove unused import

--- airbyte-cdk/bulk/toolkits/extract-jdbc/src/main/kotlin/io/airbyte/cdk/read/StreamPartitionsCreatorUtils.kt
+++ airbyte-cdk/bulk/toolkits/extract-jdbc/src/main/kotlin/io/airbyte/cdk/read/StreamPartitionsCreatorUtils.kt
@@ -8,7 +8,6 @@ import io.airbyte.cdk.discover.Field
 import io.airbyte.cdk.util.Jsons
 import io.github.oshai.kotlinlogging.KotlinLogging
 import java.io.OutputStream
-import java.util.function.Function
 import kotlin.random.Random
 
 /** Utilities for [StreamPartitionsCreator] that don't rely directly on its input state. */
