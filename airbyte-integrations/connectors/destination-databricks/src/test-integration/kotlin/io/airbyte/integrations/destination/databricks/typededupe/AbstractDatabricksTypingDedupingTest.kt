34f58fab6d9679448777a0e87f11a806d60f5f1f
Edward Gao <edward.gao@airbyte.io>
2024-08-14 11:11:38 -0700

    Destination databricks: Remove user/pass auth (#40712)

--- airbyte-integrations/connectors/destination-databricks/src/test-integration/kotlin/io/airbyte/integrations/destination/databricks/typededupe/AbstractDatabricksTypingDedupingTest.kt
+++ airbyte-integrations/connectors/destination-databricks/src/test-integration/kotlin/io/airbyte/integrations/destination/databricks/typededupe/AbstractDatabricksTypingDedupingTest.kt
@@ -5,49 +5,66 @@
 package io.airbyte.integrations.destination.databricks.typededupe
 
 import com.fasterxml.jackson.databind.JsonNode
+import com.fasterxml.jackson.databind.node.ObjectNode
 import io.airbyte.cdk.db.jdbc.DefaultJdbcDatabase
 import io.airbyte.cdk.db.jdbc.JdbcDatabase
 import io.airbyte.cdk.db.jdbc.JdbcSourceOperations
 import io.airbyte.cdk.integrations.base.JavaBaseConstants
+import io.airbyte.commons.io.IOs
+import io.airbyte.commons.json.Jsons
 import io.airbyte.integrations.base.destination.typing_deduping.BaseTypingDedupingTest
 import io.airbyte.integrations.base.destination.typing_deduping.SqlGenerator
 import io.airbyte.integrations.base.destination.typing_deduping.StreamId
 import io.airbyte.integrations.destination.databricks.DatabricksConnectorClientsFactory
-import io.airbyte.integrations.destination.databricks.DatabricksIntegrationTestUtils
 import io.airbyte.integrations.destination.databricks.jdbc.DatabricksNamingTransformer
 import io.airbyte.integrations.destination.databricks.jdbc.DatabricksSqlGenerator
 import io.airbyte.integrations.destination.databricks.model.DatabricksConnectorConfig
+import java.nio.file.Path
 import java.sql.Connection
 import java.sql.ResultSet
-import java.util.concurrent.TimeUnit
-import org.junit.jupiter.api.BeforeAll
-import org.junit.jupiter.api.Timeout
-import org.mockito.Mockito
+import java.util.Locale
+import org.apache.commons.lang3.RandomStringUtils
 
-class DatabricksTypingDedupingTest : BaseTypingDedupingTest() {
+abstract class AbstractDatabricksTypingDedupingTest(
+    private val jdbcDatabase: JdbcDatabase,
+    private val jsonConfig: JsonNode,
+    private val connectorConfig: DatabricksConnectorConfig,
+) : BaseTypingDedupingTest() {
     override val imageName: String
         get() = "airbyte/destination-databricks:dev"
 
     companion object {
-        private var jdbcDatabase: JdbcDatabase = Mockito.mock()
-        private var connectorConfig: DatabricksConnectorConfig = Mockito.mock()
-        @JvmStatic
-        @BeforeAll
-        @Timeout(value = 10, unit = TimeUnit.MINUTES)
-        fun setupDatabase() {
-            connectorConfig = DatabricksIntegrationTestUtils.oauthConfig
-            jdbcDatabase =
+        fun setupDatabase(
+            connectorConfigPath: String
+        ): Triple<JdbcDatabase, JsonNode, DatabricksConnectorConfig> {
+            var jsonConfig = Jsons.deserialize(IOs.readFile(Path.of(connectorConfigPath)))
+
+            // Randomize the default namespace to avoid collisions between
+            // concurrent test runs.
+            // Technically, we should probably do this in `generateConfig`,
+            // because there could be concurrent test runs within a single class,
+            // but we currently only have a single test that uses the default
+            // namespace anyway.
+            val uniqueSuffix = RandomStringUtils.randomAlphabetic(10).lowercase(Locale.getDefault())
+            val defaultSchema = "typing_deduping_default_schema_$uniqueSuffix"
+            val connectorConfig =
+                DatabricksConnectorConfig.deserialize(jsonConfig).copy(schema = defaultSchema)
+            (jsonConfig as ObjectNode).put("schema", defaultSchema)
+
+            val jdbcDatabase =
                 DefaultJdbcDatabase(
                     DatabricksConnectorClientsFactory.createDataSource(connectorConfig)
                 )
             // This will trigger warehouse start
             jdbcDatabase.execute("SELECT 1")
+
+            return Triple(jdbcDatabase, jsonConfig, connectorConfig)
         }
     }
 
     override fun generateConfig(): JsonNode {
         // This method is called in BeforeEach so setup any other references needed per test
-        return DatabricksIntegrationTestUtils.oauthConfigJson.deepCopy()
+        return jsonConfig.deepCopy()
     }
 
     private fun rawTableIdentifier(
@@ -80,7 +97,7 @@ class DatabricksTypingDedupingTest : BaseTypingDedupingTest() {
                     .executeQuery(
                         """
                         SELECT *
-                        FROM `${connectorConfig.database}`.$tableIdentifier 
+                        FROM `${connectorConfig.database}`.$tableIdentifier
                         ORDER BY ${JavaBaseConstants.COLUMN_NAME_AB_EXTRACTED_AT} ASC
                     """.trimIndent(),
                     )
