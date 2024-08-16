b228cf8af5583768071b3ae9d34fa542e387e1cf
Aaron Steers <aj@airbyte.io>
2024-08-16 10:28:17 -0700

    try ddtrace config

--- airbyte-integrations/connectors/source-hardcoded-records/source_hardcoded_records/run.py
+++ airbyte-integrations/connectors/source-hardcoded-records/source_hardcoded_records/run.py
@@ -1,13 +1,27 @@
 # Copyright (c) 2024 Airbyte, Inc., all rights reserved.
-
-
 import sys
+
+import ddtrace
 
 from airbyte_cdk.entrypoint import launch
 from source_hardcoded_records import SourceHardcodedRecords
 
 
-def run():
+# Automatically patch all supported libraries to enable tracing
+ddtrace.patch_all()
+
+# Set DataDog configuration options directly in your Python code
+ddtrace.config.env = "dev"                          # Equivalent to DD_ENV
+ddtrace.config.service = "source-hardcoded-records" # Equivalent to DD_SERVICE
+ddtrace.config.version = "1.0.0"                    # Equivalent to DD_VERSION
+ddtrace.config.agent_hostname = "localhost"         # Equivalent to DD_AGENT_HOST
+
+
+def run() -> None:
+    # Turn on tracing
+    ddtrace.patch_all()
+
+    # Initialize and launch the source
     source = SourceHardcodedRecords()
     launch(source, sys.argv[1:])
 
