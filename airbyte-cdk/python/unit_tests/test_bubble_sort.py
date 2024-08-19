f12da0855bfdcb1a8d6c59b82b87951cb34b91be
Alexandre Girard <alexandre@airbyte.io>
2024-08-19 14:17:00 -0700

    commit them files

---
+++ airbyte-cdk/python/unit_tests/test_bubble_sort.py
@@ -0,0 +1,15 @@
+from airbyte_cdk.bubble_sort import sorter
+
+
+def test_sort():
+    input = [5, 4, 3, 2, 1, 0]
+    output = sorter(input)
+    assert output == [0, 1, 2, 3, 4, 5]
+
+    input = [5.0, 4.0, 3.0, 2.0, 1.0, 0.0]
+    output = sorter(input)
+    assert output == [0.0, 1.0, 2.0, 3.0, 4.0, 5.0]
+
+    input = list(reversed(range(100)))
+    output = sorter(input)
+    assert output == list(range(100))
