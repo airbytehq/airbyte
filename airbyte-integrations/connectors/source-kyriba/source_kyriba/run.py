3111f66da7cce867fd46fc76a677944f4f7ffdd0
btkcodedev <btk.codedev@gmail.com>
2024-08-23 22:22:44 +0530

    Revert "kyriba connector"
    This reverts commit 745600757bf7587d977c1efda0308ded7e56a659.

--- airbyte-integrations/connectors/source-kyriba/source_kyriba/run.py
+++ airbyte-integrations/connectors/source-kyriba/source_kyriba/run.py
@@ -1,12 +1,13 @@
 #
-# Copyright (c) 2024 Airbyte, Inc., all rights reserved.
+# Copyright (c) 2023 Airbyte, Inc., all rights reserved.
 #
 
 
 import sys
 
 from airbyte_cdk.entrypoint import launch
-from .source import SourceKyriba
+from source_kyriba import SourceKyriba
+
 
 def run():
     source = SourceKyriba()
