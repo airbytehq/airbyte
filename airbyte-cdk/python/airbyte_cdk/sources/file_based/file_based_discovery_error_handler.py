59c82fc703405d3dca8a93d31732744bd79be73e
Patrick Nilan <nilan.patrick@gmail.com>
2024-08-26 13:15:46 -0700

    updates file based discovery error handler to find error messages in AirbyteTracedMessage

--- airbyte-cdk/python/airbyte_cdk/sources/file_based/file_based_discovery_error_handler.py
+++ airbyte-cdk/python/airbyte_cdk/sources/file_based/file_based_discovery_error_handler.py
@@ -6,31 +6,22 @@ from typing import Optional
 from airbyte_cdk.utils.traced_exception import AirbyteTracedException
 from airbyte_cdk.sources.streams.core import Stream
 from airbyte_cdk.sources.utils.discover_error_handler import AbstractDiscoverErrorHandler
-from airbyte_cdk.sources.file_based.exceptions import SchemaInferenceError, InvalidSchemaError
+from airbyte_cdk.sources.file_based.exceptions import FileBasedSourceError
 
 
 class FileBasedDiscoverErrorHandler(AbstractDiscoverErrorHandler):
-
-    def __init__(self, errors_to_ignore: Optional[list[Exception]] = None) -> None:
-        self._errors_to_ignore = errors_to_ignore or [SchemaInferenceError, InvalidSchemaError]
 
     def handle_discover_error(self, logger: logging.Logger, exception: Exception, stream: Stream) -> None:
         """
         Default File Based source implementation of the discover error handler. Logs the error or raises the exception.
         """
-        if isinstance(exception, AirbyteTracedException):
-            exception_type = type(exception._exception)
-        else:
-            exception_type = type(exception)
+        exception_message = exception.message if isinstance(exception, AirbyteTracedException) else str(exception)
+        exceptions_to_log = [
+            FileBasedSourceError.SCHEMA_INFERENCE_ERROR,
+            FileBasedSourceError.INVALID_SCHEMA_ERROR,
+        ]
 
-        print("\n\n=-=-=-=-=-=-=\n\n")
-        print(exception._exception, exception_type, self._errors_to_ignore, exception_type in self._errors_to_ignore)
-        print("\n\n=-=-=-=-=-=-=\n\n")
-        if exception_type in self._errors_to_ignore:
-            print("\n\n=====================\n\n")
-            print("Error occurred while discovering stream {stream.name}: {exception}")
-            print("\n\n=====================\n\n")
+        if any(error.value in exception_message for error in exceptions_to_log):
             logger.error(f"Error occurred while discovering stream {stream.name}: {exception}", exc_info=True)
         else:
             raise exception
-        return
