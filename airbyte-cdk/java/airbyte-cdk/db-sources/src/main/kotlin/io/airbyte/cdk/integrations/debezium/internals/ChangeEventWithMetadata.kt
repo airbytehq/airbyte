9995f12614f0f51b89932da8bbca6de8e381a19a
Rodi Reich Zilberman <867491+rodireich@users.noreply.github.com>
2024-08-27 10:56:19 -0700

    test

--- airbyte-cdk/java/airbyte-cdk/db-sources/src/main/kotlin/io/airbyte/cdk/integrations/debezium/internals/ChangeEventWithMetadata.kt
+++ airbyte-cdk/java/airbyte-cdk/db-sources/src/main/kotlin/io/airbyte/cdk/integrations/debezium/internals/ChangeEventWithMetadata.kt
@@ -12,31 +12,15 @@ private val LOGGER = KotlinLogging.logger {}
 
 class ChangeEventWithMetadata(private val event: ChangeEvent<String?, String?>) {
     val eventKeyAsJson: JsonNode?
-        get() {
-            if (event.key() == null) {
-                LOGGER.warn { "Event key is null $event" }
-                return null
-            }
-            return Jsons.deserialize(event.key())
-        }
+        get() = event.key()?.let { Jsons.deserialize(it) }.also { it ?: LOGGER.warn { "Event key is null $event" } }
     val eventValueAsJson: JsonNode?
-        get() {
-            if (event.value() == null) {
-                LOGGER.warn { "Event value is null $event" }
-                return null
-            }
-            return Jsons.deserialize(event.value())
-        }
+        get() = event.value()?.let { Jsons.deserialize(it) }.also { it ?: LOGGER.warn { "Event value is null $event" } }
+
     val snapshotMetadata: SnapshotMetadata?
         get() {
             val metadataKey = eventValueAsJson?.get("source")?.get("snapshot")?.asText()
             return metadataKey?.let { return SnapshotMetadata.fromString(metadataKey) }
         }
-
-    fun event(): ChangeEvent<String?, String?> {
-        return event
-    }
-
 
     val isSnapshotEvent: Boolean
         get() = SnapshotMetadata.isSnapshotEventMetadata(snapshotMetadata)
