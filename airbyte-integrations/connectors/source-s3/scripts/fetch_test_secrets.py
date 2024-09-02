82568e092d3cd04079d6963176349f4d0bf0fe77
Aaron Steers <aj@airbyte.io>
2024-09-01 17:18:01 -0700

    tidy some more

--- airbyte-integrations/connectors/source-s3/scripts/fetch_test_secrets.py
+++ airbyte-integrations/connectors/source-s3/scripts/fetch_test_secrets.py
@@ -27,16 +27,18 @@ AIRBYTE_INTERNAL_GCP_PROJECT = "dataline-integration-testing"
 
 
 def main() -> None:
+    # Get a PyAirbyte secret manager instance for the GCP project.
     secret_mgr = GoogleGSMSecretManager(
         project=AIRBYTE_INTERNAL_GCP_PROJECT,
         credentials_json=ab.get_secret("GCP_GSM_CREDENTIALS"),
     )
 
+    # Fetch and write secrets to the `secrets/` directory, using the `filename` label
+    # to name the file.
     secret: SecretHandle
     for secret in secret_mgr.fetch_connector_secrets("source-s3"):
         if "filename" in secret.labels:
-            secret_file_path = Path(f'secrets/{secret.labels["filename"]}.json')
-            secret.write_to_file(secret_file_path)
+            secret.write_to_file(Path(f'secrets/{secret.labels["filename"]}.json'))
 
 
 if __name__ == "__main__":
