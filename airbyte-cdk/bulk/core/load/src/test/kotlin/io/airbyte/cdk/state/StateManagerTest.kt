ee9c9bb05ec0de4148eb86573df95961f4ff4618
Johnny Schmidt <john.schmidt@airbyte.io>
2024-09-03 10:34:16 -0700

    Bulk Load CDK: QueueWriter Unit Tests

--- airbyte-cdk/bulk/core/load/src/test/kotlin/io/airbyte/cdk/state/StateManagerTest.kt
+++ airbyte-cdk/bulk/core/load/src/test/kotlin/io/airbyte/cdk/state/StateManagerTest.kt
@@ -5,17 +5,13 @@
 package io.airbyte.cdk.state
 
 import com.google.common.collect.Range
-import com.google.common.collect.RangeSet
-import com.google.common.collect.TreeRangeSet
 import io.airbyte.cdk.command.DestinationCatalog
 import io.airbyte.cdk.command.DestinationStream
 import io.airbyte.cdk.command.MockCatalogFactory.Companion.stream1
 import io.airbyte.cdk.command.MockCatalogFactory.Companion.stream2
-import io.airbyte.cdk.message.Batch
-import io.airbyte.cdk.message.BatchEnvelope
 import io.airbyte.cdk.message.MessageConverter
+import io.airbyte.cdk.message.MockStreamsManager
 import io.micronaut.context.annotation.Prototype
-import io.micronaut.context.annotation.Requires
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest
 import jakarta.inject.Inject
 import jakarta.inject.Named
@@ -29,7 +25,7 @@ import org.junit.jupiter.params.provider.Arguments
 import org.junit.jupiter.params.provider.ArgumentsProvider
 import org.junit.jupiter.params.provider.ArgumentsSource
 
-@MicronautTest(environments = ["StateManagerTest"])
+@MicronautTest(environments = ["MockStreamsManager"])
 class StateManagerTest {
     @Inject lateinit var stateManager: TestStateManager
 
@@ -69,70 +65,6 @@ class StateManagerTest {
                     collectedStreamOutput.getOrPut(t.stream) { mutableListOf() }.add(t.payload)
                 is MockGlobalStateOut -> collectedGlobalOutput.add(t.payload)
             }
-        }
-    }
-
-    /**
-     * The only thing we really need is `areRecordsPersistedUntil`. (Technically we're emulating the
-     * @[StreamManager] behavior here, since the state manager doesn't actually know what ranges are
-     * closed, but less than that would make the test unrealistic.)
-     */
-    class MockStreamManager : StreamManager {
-        var persistedRanges: RangeSet<Long> = TreeRangeSet.create()
-
-        override fun countRecordIn(): Long {
-            throw NotImplementedError()
-        }
-
-        override fun countEndOfStream(): Long {
-            throw NotImplementedError()
-        }
-
-        override fun markCheckpoint(): Pair<Long, Long> {
-            throw NotImplementedError()
-        }
-
-        override fun <B : Batch> updateBatchState(batch: BatchEnvelope<B>) {
-            throw NotImplementedError()
-        }
-
-        override fun isBatchProcessingComplete(): Boolean {
-            throw NotImplementedError()
-        }
-
-        override fun areRecordsPersistedUntil(index: Long): Boolean {
-            return persistedRanges.encloses(Range.closedOpen(0, index))
-        }
-
-        override fun markClosed() {
-            throw NotImplementedError()
-        }
-
-        override fun streamIsClosed(): Boolean {
-            throw NotImplementedError()
-        }
-
-        override suspend fun awaitStreamClosed() {
-            throw NotImplementedError()
-        }
-    }
-
-    @Prototype
-    @Requires(env = ["StateManagerTest"])
-    class MockStreamsManager(@Named("mockCatalog") catalog: DestinationCatalog) : StreamsManager {
-        private val mockManagers = catalog.streams.associateWith { MockStreamManager() }
-
-        fun addPersistedRanges(stream: DestinationStream, ranges: List<Range<Long>>) {
-            mockManagers[stream]!!.persistedRanges.addAll(ranges)
-        }
-
-        override fun getManager(stream: DestinationStream): StreamManager {
-            return mockManagers[stream]
-                ?: throw IllegalArgumentException("Stream not found: $stream")
-        }
-
-        override suspend fun awaitAllStreamsClosed() {
-            throw NotImplementedError()
         }
     }
 
