c640ce1955c1a4aac72220f05181250f61b1dd54
btkcodedev <btk.codedev@gmail.com>
2024-09-04 11:53:04 +0530

    remove /c/chat

--- airbyte-integrations/connectors/source-zendesk-chat/unit_tests/components/test_bans_record_extractor.py
+++ airbyte-integrations/connectors/source-zendesk-chat/unit_tests/components/test_bans_record_extractor.py
@@ -12,7 +12,7 @@ def test_bans_stream_record_extractor(
     bans_stream_record, 
     bans_stream_record_extractor_expected_output,
 ) -> None:
-    test_url = f"https://{config['subdomain']}.zendesk.com/api/v2/chat/bans"
+    test_url = f"https://{config['subdomain']}.zendesk.com/api/v2/bans"
     requests_mock.get(test_url, json=bans_stream_record)
     test_response = requests.get(test_url)
     assert ZendeskChatBansRecordExtractor().extract_records(test_response) == bans_stream_record_extractor_expected_output
