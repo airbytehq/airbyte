443361363402ce840057f18275b38876840fad5f
darynaishchenko <darina.ishchenko17@gmail.com>
2024-09-13 16:15:58 +0300

    added unit tests

--- airbyte-integrations/connectors/source-gcs/unit_tests/test_stream_reader.py
+++ airbyte-integrations/connectors/source-gcs/unit_tests/test_stream_reader.py
@@ -5,14 +5,18 @@ from unittest.mock import Mock
 import pytest
 from airbyte_cdk.sources.file_based.exceptions import ErrorListingFiles
 from source_gcs import Config, SourceGCSStreamReader
+from source_gcs.config import ServiceAccountCredentials
 
 
 def test_get_matching_files_with_no_prefix(logger):
     reader = SourceGCSStreamReader()
     reader._config = Config(
-        service_account='{"type": "service_account"}',
         bucket="test_bucket",
         streams=[],
+        credentials=ServiceAccountCredentials(
+            service_account='{"type": "service_account"}',
+            auth_type="Service"
+        )
     )
     reader._gcs_client = Mock()
     globs = ["**/*.csv"]
