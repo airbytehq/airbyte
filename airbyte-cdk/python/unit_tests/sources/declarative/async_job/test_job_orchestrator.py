29385d69e78c44d07fa15d52d527fc69c4b1ef71
Artem Inzhyyants <artem.inzhyyants@gmail.com>
2024-09-13 16:31:35 +0200

    Airbyte CDK: update test [skip ci]
    Signed-off-by: Artem Inzhyyants <artem.inzhyyants@gmail.com>

--- airbyte-cdk/python/unit_tests/sources/declarative/async_job/test_job_orchestrator.py
+++ airbyte-cdk/python/unit_tests/sources/declarative/async_job/test_job_orchestrator.py
@@ -141,19 +141,16 @@ class AsyncJobOrchestratorTest(TestCase):
         assert self._job_repository.fetch_records.mock_calls == [call(first_job), call(second_job)]
 
     @mock.patch(sleep_mock_target)
-    def test_given_exception_when_start_job_and_raise_this_exception(self, mock_sleep: MagicMock) -> None:
-        self._job_repository.update_jobs_status.side_effect = _status_update_per_jobs({self._job_for_a_slice: [AsyncJobStatus.RUNNING]})
-        orchestrator = AsyncJobOrchestrator(self._job_repository, [_A_STREAM_SLICE], exceptions_to_break_on=[ValueError])
-        orchestrator._start_jobs = Mock(side_effect=[self._job_for_a_slice, ValueError("Something went wrong")])
-        first_job = _create_job()
-        second_job = _create_job()
-        partition = AsyncPartition([first_job, second_job], _A_STREAM_SLICE)
-        orchestrator._running_partitions = [partition]
+    def test_given_exception_when_start_job_and_raise_this_exception_and_abort_jobs(self, mock_sleep: MagicMock) -> None:
+        orchestrator = AsyncJobOrchestrator(self._job_repository, [_A_STREAM_SLICE, _ANOTHER_STREAM_SLICE],
+                                            exceptions_to_break_on=[ValueError])
+        self._job_repository.start.side_effect = [self._job_for_a_slice, ValueError("Something went wrong")]
 
         with pytest.raises(ValueError):
             # assert that orchestrator exits on expected error
             list(orchestrator.create_and_get_completed_partitions())
         assert len(orchestrator._running_partitions) == 0
+        self._job_repository.abort.assert_called_once_with(self._job_for_a_slice)
 
     @mock.patch(sleep_mock_target)
     def test_given_exception_when_start_job_and_skip_this_exception(self, mock_sleep: MagicMock) -> None:
