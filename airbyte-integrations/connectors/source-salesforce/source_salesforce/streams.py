238c3f246d56b31fcd90ca16cd7cc815ac5701f2
Artem Inzhyyants <artem.inzhyyants@gmail.com>
2024-09-16 16:31:12 +0200

    Source SalesForce: switch to rest if BulkNotSupportedException [skip ci]
    Signed-off-by: Artem Inzhyyants <artem.inzhyyants@gmail.com>

--- airbyte-integrations/connectors/source-salesforce/source_salesforce/streams.py
+++ airbyte-integrations/connectors/source-salesforce/source_salesforce/streams.py
@@ -407,6 +407,7 @@ class BulkDatetimeStreamSlicer(StreamSlicer):
 class BulkSalesforceStream(SalesforceStream):
     def __init__(self, **kwargs) -> None:
         self._stream_slicer_cursor = None
+        self._stream_to_use = None
         super().__init__(**kwargs)
 
     def next_page_token(self, response: requests.Response) -> Optional[Mapping[str, Any]]:
@@ -618,13 +619,22 @@ class BulkSalesforceStream(SalesforceStream):
         stream_state: Mapping[str, Any] = None,
         call_count: int = 0,
     ) -> Iterable[Mapping[str, Any]]:
-        yield from self._bulk_job_stream.read_records(sync_mode, cursor_field, stream_slice, stream_state)
+        yield from self._stream_to_use.read_records(sync_mode, cursor_field, stream_slice, stream_state)
 
     def stream_slices(
         self, *, sync_mode: SyncMode, cursor_field: Optional[List[str]] = None, stream_state: Optional[Mapping[str, Any]] = None
     ) -> Iterable[Optional[Mapping[str, Any]]]:
         self._instantiate_declarative_stream()
-        yield from self._bulk_job_stream.stream_slices(sync_mode=sync_mode, cursor_field=cursor_field, stream_state=stream_state)
+        self._stream_to_use = self._bulk_job_stream
+        standard_instance = self.get_standard_instance()
+        try:
+            yield from self._stream_to_use.stream_slices(sync_mode=sync_mode, cursor_field=cursor_field, stream_state=stream_state)
+        except BulkNotSupportedException:
+            self.logger.warning("switch to STANDARD(non-BULK) sync. Because the SalesForce BULK job has returned a failed status")
+            self._stream_to_use = standard_instance
+            yield from self._stream_to_use.read_records(
+                sync_mode=sync_mode, cursor_field=cursor_field, stream_state=stream_state
+            )
 
     def get_standard_instance(self) -> SalesforceStream:
         """Returns a instance of standard logic(non-BULK) with same settings"""
