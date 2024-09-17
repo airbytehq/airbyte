69a84b53d84a7b707555cbe0cd9258e6777b49f9
Octavia Squidington III <octavia-squidington-iii@users.noreply.github.com>
2024-09-17 09:29:30 +0000

    chore: auto-fix lint and format issues

--- airbyte-cdk/python/airbyte_cdk/sources/declarative/incremental/global_substream_cursor.py
+++ airbyte-cdk/python/airbyte_cdk/sources/declarative/incremental/global_substream_cursor.py
@@ -4,7 +4,7 @@
 
 import threading
 import time
-from typing import Any, Iterable, Mapping, Optional, Union, TypeVar
+from typing import Any, Iterable, Mapping, Optional, TypeVar, Union
 
 from airbyte_cdk.sources.declarative.incremental.datetime_based_cursor import DatetimeBasedCursor
 from airbyte_cdk.sources.declarative.incremental.declarative_cursor import DeclarativeCursor
