644861f1fdb7edd41a83ee11731db0edf0b4e66c
Aaron Steers <aj@airbyte.io>
2024-09-17 21:55:08 -0700

    tidy

--- airbyte-cdk/python/airbyte_cdk/sources/file_based/stream/abstract_file_based_stream.py
+++ airbyte-cdk/python/airbyte_cdk/sources/file_based/stream/abstract_file_based_stream.py
@@ -267,19 +267,6 @@ class AbstractFileBasedStream(Stream):
             airbyte_state_message = self._checkpoint_state(checkpoint, state_manager=state_manager)
             yield airbyte_state_message
 
-    def read_to_buffer(
-        self,
-        configured_stream: ConfiguredAirbyteStream,
-        logger: logging.Logger,
-        slice_logger: SliceLogger,
-        stream_state: MutableMapping[str, Any],
-        state_manager: ConnectorStateManager,
-        internal_config: InternalConfig,
-        stdout_buffer: TextIO = sys.stdout,
-    ) -> Iterable[AirbyteMessage]:
-        """Bulk process data into messages, and write directly to the IO buffer."""
-        raise NotImplementedError("Bulk processing to buffer is not yet implemented.")
-
     def read_records(
         self,
         sync_mode: SyncMode,
