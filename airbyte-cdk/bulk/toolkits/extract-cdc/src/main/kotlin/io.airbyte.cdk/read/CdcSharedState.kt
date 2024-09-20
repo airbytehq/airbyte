54b4db0f2f818349e88a538b9abfd1142d8047b9
Rodi Reich Zilberman <867491+rodireich@users.noreply.github.com>
2024-09-19 21:21:22 -0700

    cleanup

--- airbyte-cdk/bulk/toolkits/extract-cdc/src/main/kotlin/io.airbyte.cdk/read/CdcSharedState.kt
+++ airbyte-cdk/bulk/toolkits/extract-cdc/src/main/kotlin/io.airbyte.cdk/read/CdcSharedState.kt
@@ -8,8 +8,6 @@ import io.airbyte.cdk.cdc.CdcPartitionReader
 import io.airbyte.cdk.command.SourceConfiguration
 
 interface CdcSharedState {
-    val configuration: SourceConfiguration
-
     fun tryAcquireResourcesForCreator(): CdcPartitionCreator.AcquiredResources?
 
     fun tryAcquireResourcesForReader(): CdcPartitionReader.AcquiredResources?
--- airbyte-cdk/bulk/toolkits/extract-jdbc/src/main/kotlin/io/airbyte/cdk/read/cdc/StateUtils.kt
+++ airbyte-cdk/bulk/toolkits/extract-jdbc/src/main/kotlin/io/airbyte/cdk/read/cdc/StateUtils.kt
@@ -4,10 +4,35 @@
 
 package io.airbyte.cdk.read.cdc
 
+import io.airbyte.cdk.cdc.CdcPartitionReader
+import io.airbyte.cdk.command.SourceConfiguration
+import io.airbyte.cdk.read.CdcPartitionCreator
 import io.airbyte.cdk.read.CdcSharedState
-import io.airbyte.cdk.read.DefaultCdcSharedState
+import io.airbyte.cdk.read.JdbcPartitionReader
+import io.airbyte.cdk.read.JdbcPartitionsCreator
 import io.airbyte.cdk.read.JdbcSharedState
 
-public fun JdbcSharedState.toCdcSharedState(): CdcSharedState {
-    return DefaultCdcSharedState(this.configuration)
+fun JdbcSharedState.toCdcSharedState(): CdcSharedState
+    = object: CdcSharedState {
+        override fun tryAcquireResourcesForCreator(): CdcPartitionCreator.AcquiredResources?
+            = this@toCdcSharedState.tryAcquireResourcesForCreator()?.toCdcPartitionCreatorAcquiredResources()
+
+        override fun tryAcquireResourcesForReader(): CdcPartitionReader.AcquiredResources?
+            = this@toCdcSharedState.tryAcquireResourcesForReader()?.toCdcPartitionReaderAcquiredResources()
+    }
+
+fun JdbcPartitionsCreator.AcquiredResources.toCdcPartitionCreatorAcquiredResources(): CdcPartitionCreator.AcquiredResources {
+    return object : CdcPartitionCreator.AcquiredResources {
+        override fun close() {
+            this@toCdcPartitionCreatorAcquiredResources.close()
+        }
+    }
+}
+
+fun JdbcPartitionReader.AcquiredResources.toCdcPartitionReaderAcquiredResources(): CdcPartitionReader.AcquiredResources {
+    return object : CdcPartitionReader.AcquiredResources {
+        override fun close() {
+            this@toCdcPartitionReaderAcquiredResources.close()
+        }
+    }
 }
