1b73037dad8336307c96112745b1da6d4fb5c3af
Serhii Lazebnyi <serhii.lazebnyi@globallogic.com>
2024-09-21 05:12:31 +0200

    Update tests

--- airbyte-cdk/python/unit_tests/sources/streams/concurrent/scenarios/stream_facade_builder.py
+++ airbyte-cdk/python/unit_tests/sources/streams/concurrent/scenarios/stream_facade_builder.py
@@ -63,7 +63,7 @@ class StreamFacadeSource(ConcurrentSourceAdapter):
         state_converter = StreamFacadeConcurrentConnectorStateConverter()
 
         return [
-            self.convert_to_concurrent_stream(stream.logger, stream, self.initialize_cursor(
+            self.convert_to_concurrent_stream(stream.logger, stream, state_manager, self.initialize_cursor(
                 stream, state_manager, state_converter, self._cursor_boundaries, None, EpochValueConcurrentStreamStateConverter.get_end_provider())
             )
             for stream in self._streams
