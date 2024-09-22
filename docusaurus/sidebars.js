fbc63ee0616250109bc643127090f689d41e17f6
Ben Church <ben@airbyte.io>
2024-09-22 12:50:28 -0700

    chore: add docs for ai assist (#45845)

--- docusaurus/sidebars.js
+++ docusaurus/sidebars.js
@@ -222,6 +222,7 @@ const buildAConnector = {
         "connector-development/connector-builder-ui/overview",
         "connector-development/connector-builder-ui/connector-builder-compatibility",
         "connector-development/connector-builder-ui/tutorial",
+        "connector-development/connector-builder-ui/ai-assist",
         {
           type: "category",
           label: "Concepts",
