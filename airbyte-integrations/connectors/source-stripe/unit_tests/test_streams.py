c740057ae97c2eddaf08d796b2e605c89976d8f2
William Kaper <bill@hubifi.com>
2024-09-23 15:22:21 -0400

    Added incremental support to the invoice_line_items and subscription_items streams

--- airbyte-integrations/connectors/source-stripe/unit_tests/test_streams.py
+++ airbyte-integrations/connectors/source-stripe/unit_tests/test_streams.py
@@ -785,6 +785,7 @@ def test_subscription_items_extra_request_params(requests_mock, stream_by_name, 
                                 "created": 1699603175,
                                 "quantity": 1,
                                 "subscription": "sub_1OApco2eZvKYlo2CEDCzwLrE",
+                                'updated': 1699603884
                             }
                         ],
                         "has_more": True,
@@ -822,6 +823,7 @@ def test_subscription_items_extra_request_params(requests_mock, stream_by_name, 
             "created": 1699603175,
             "quantity": 1,
             "subscription": "sub_1OApco2eZvKYlo2CEDCzwLrE",
+            "updated": 1699603884,
         },
         {
             "id": "si_OynPdzMZykmCWm",
@@ -829,6 +831,7 @@ def test_subscription_items_extra_request_params(requests_mock, stream_by_name, 
             "created": 1699603884,
             "quantity": 2,
             "subscription": "sub_1OApco2eZvKYlo2CEDCzwLrE",
+            "updated": 1699603884,
         },
     ]
     assert len(requests_mock.request_history) == 2
