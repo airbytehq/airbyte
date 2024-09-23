60fc005fd139cf63eb5e2f5d0a5e900d28fe0c8f
Xiaohan Song <xiaohan@airbyte.io>
2024-09-23 16:16:36 -0700

    clean up and rewrite global discover logic

--- airbyte-integrations/connectors/source-mysql-v2/src/main/kotlin/io/airbyte/integrations/source/mysql/MysqlJdbcStreamFactory.kt
+++ airbyte-integrations/connectors/source-mysql-v2/src/main/kotlin/io/airbyte/integrations/source/mysql/MysqlJdbcStreamFactory.kt
@@ -7,12 +7,14 @@ package io.airbyte.integrations.source.mysql
 import com.fasterxml.jackson.databind.node.ObjectNode
 import io.airbyte.cdk.discover.AirbyteStreamFactory
 import io.airbyte.cdk.discover.CdcIntegerMetaFieldType
+import io.airbyte.cdk.discover.CdcOffsetDateTimeMetaFieldType
 import io.airbyte.cdk.discover.CdcStringMetaFieldType
 import io.airbyte.cdk.discover.DiscoveredStream
 import io.airbyte.cdk.discover.FieldType
 import io.airbyte.cdk.discover.JdbcAirbyteStreamFactory
 import io.airbyte.cdk.discover.MetaField
 import io.airbyte.protocol.models.v0.AirbyteStream
+import io.airbyte.protocol.models.v0.SyncMode
 import io.micronaut.context.annotation.Primary
 import javax.inject.Singleton
 
@@ -21,20 +23,30 @@ import javax.inject.Singleton
 class MysqlJdbcStreamFactory(val base: JdbcAirbyteStreamFactory) : AirbyteStreamFactory by base {
     @Override
     override fun createGlobal(discoveredStream: DiscoveredStream): AirbyteStream {
-        val mysqlGlobalStream = base.createGlobal(discoveredStream)
-        mysqlGlobalStream.apply {
+        return AirbyteStreamFactory.createAirbyteStream(discoveredStream).apply {
             (jsonSchema["properties"] as ObjectNode).apply {
                 for (metaField in MysqlCDCMetaFields.entries) {
                     set<ObjectNode>(metaField.id, metaField.type.airbyteType.asJsonSchema())
                 }
             }
+            if (base.hasValidPrimaryKey(discoveredStream)) {
+                supportedSyncModes = listOf(SyncMode.FULL_REFRESH, SyncMode.INCREMENTAL)
+                sourceDefinedPrimaryKey = discoveredStream.primaryKeyColumnIDs
+                isResumable = true
+                defaultCursorField = listOf(MysqlCDCMetaFields.CDC_CURSOR.id)
+            } else {
+                supportedSyncModes = listOf(SyncMode.FULL_REFRESH)
+                isResumable = false
+            }
         }
-        return mysqlGlobalStream
     }
 
     enum class MysqlCDCMetaFields(
         override val type: FieldType,
     ) : MetaField {
+        CDC_CURSOR(CdcIntegerMetaFieldType),
+        CDC_UPDATED_AT(CdcOffsetDateTimeMetaFieldType),
+        CDC_DELETED_AT(CdcOffsetDateTimeMetaFieldType),
         CDC_LOG_POS(CdcIntegerMetaFieldType),
         CDC_LOG_FILE(CdcStringMetaFieldType),
         ;
