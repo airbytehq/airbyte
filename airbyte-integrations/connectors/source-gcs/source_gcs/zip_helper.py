3bb0ae06ae29b688a901be84e049de75291eda99
darynaishchenko <darina.ishchenko17@gmail.com>
2024-09-24 12:16:06 +0300

    fix

--- airbyte-integrations/connectors/source-gcs/source_gcs/zip_helper.py
+++ airbyte-integrations/connectors/source-gcs/source_gcs/zip_helper.py
@@ -4,6 +4,7 @@ import logging
 import os
 import tempfile
 import zipfile
+from typing import Iterable
 
 from google.cloud.storage.blob import Blob
 from source_gcs.helpers import GCSRemoteFile
@@ -20,7 +21,7 @@ class ZipHelper:
         self._tmp_dir = tmp_dir
         self._zip_file = zip_file
 
-    def _chunk_download(self):
+    def _chunk_download(self) -> bytes:
         start = 0
         end = self.BUFFER_SIZE_DEFAULT
         object_bytes = b""
@@ -34,12 +35,12 @@ class ZipHelper:
 
         return object_bytes
 
-    def _extract_files_to_tmp_directory(self, object_bytes: io.IOBase):
+    def _extract_files_to_tmp_directory(self, object_bytes: bytes) -> None:
         with io.BytesIO(object_bytes) as bytes_io:
             with zipfile.ZipFile(bytes_io, "r") as zf:
                 zf.extractall(self._tmp_dir.name)
 
-    def get_gcs_remote_files(self):
+    def get_gcs_remote_files(self) -> Iterable[GCSRemoteFile]:
         self._extract_files_to_tmp_directory(self._chunk_download())
 
         for unzipped_file in os.listdir(self._tmp_dir.name):
