e1abcdf2c2fe85147b4b912dc8c7e2b14b3a2732
teetangh <teetangh@gmail.com>
2024-09-25 03:02:48 +0530

    chore: finally working source.py

--- airbyte-integrations/connectors/source-couchbase/source_couchbase/source.py
+++ airbyte-integrations/connectors/source-couchbase/source_couchbase/source.py
@@ -2,76 +2,32 @@
 # Copyright (c) 2024 Airbyte, Inc., all rights reserved.
 #
 
-
-from abc import ABC
+import logging
+import requests
+from datetime import timedelta
 from typing import Any, Iterable, List, Mapping, MutableMapping, Optional, Tuple
 
-import requests
 from airbyte_cdk.sources import AbstractSource
 from airbyte_cdk.sources.streams import Stream
-from airbyte_cdk.sources.streams.http import HttpStream
-from airbyte_cdk.sources.streams.http.requests_native_auth import TokenAuthenticator
 
-"""
-TODO: Most comments in this class are instructive and should be deleted after the source is implemented.
+from couchbase.auth import PasswordAuthenticator
+from couchbase.cluster import Cluster
+from couchbase.options import ClusterOptions, ClusterTimeoutOptions, QueryOptions
 
-This file provides a stubbed example of how to use the Airbyte CDK to develop both a source connector which supports full refresh or and an
-incremental syncs from an HTTP API.
-
-The various TODOs are both implementation hints and steps - fulfilling all the TODOs should be sufficient to implement one basic and one incremental
-stream from a source. This pattern is the same one used by Airbyte internally to implement connectors.
-
-The approach here is not authoritative, and devs are free to use their own judgement.
-
-There are additional required TODOs in the files within the integration_tests folder and the spec.yaml file.
-"""
-
-
-# Basic full refresh stream
-class CouchbaseStream(HttpStream, ABC):
+class CouchbaseStream(Stream):
     """
-    TODO remove this comment
-
-    This class represents a stream output by the connector.
-    This is an abstract base class meant to contain all the common functionality at the API level e.g: the API base URL, pagination strategy,
-    parsing responses etc..
-
-    Each stream should extend this class (or another abstract subclass of it) to specify behavior unique to that stream.
-
-    Typically for REST APIs each stream corresponds to a resource in the API. For example if the API
-    contains the endpoints
-        - GET v1/customers
-        - GET v1/employees
-
-    then you should have three classes:
-    `class CouchbaseStream(HttpStream, ABC)` which is the current class
-    `class Customers(CouchbaseStream)` contains behavior to pull data for customers using v1/customers
-    `class Employees(CouchbaseStream)` contains behavior to pull data for employees using v1/employees
-
-    If some streams implement incremental sync, it is typical to create another class
-    `class IncrementalCouchbaseStream((CouchbaseStream), ABC)` then have concrete stream implementations extend it. An example
-    is provided below.
-
-    See the reference docs for the full list of configurable options.
+    Base stream class for Couchbase. Handles connection and basic operations.
     """
+    primary_key = None
 
-    # TODO: Fill in the url base. Required.
-    url_base = "https://example-api.com/v1/"
-
+    def __init__(self, cluster: Cluster, bucket_name: str, scope_name: str = "_default"):
+        self.cluster = cluster
+        self.bucket = self.cluster.bucket(bucket_name)
+        self.scope = self.bucket.scope(scope_name)
+        
     def next_page_token(self, response: requests.Response) -> Optional[Mapping[str, Any]]:
         """
-        TODO: Override this method to define a pagination strategy. If you will not be using pagination, no action is required - just return None.
-
-        This method should return a Mapping (e.g: dict) containing whatever information required to make paginated requests. This dict is passed
-        to most other methods in this class to help you form headers, request bodies, query params, etc..
-
-        For example, if the API accepts a 'page' parameter to determine which page of the result to return, and a response from the API contains a
-        'page' number, then this method should probably return a dict {'page': response.json()['page'] + 1} to increment the page count by 1.
-        The request_params method should then read the input next_page_token and set the 'page' param to next_page_token['page'].
-
-        :param response: the most recent response from the API
-        :return If there is another page in the result, a mapping (e.g: dict) containing information needed to query the next page in the response.
-                If there are no more pages in the result, return None.
+        Couchbase REST API doesn't support pagination for most endpoints.
         """
         return None
 
@@ -79,128 +35,108 @@ class CouchbaseStream(HttpStream, ABC):
         self, stream_state: Mapping[str, Any], stream_slice: Mapping[str, any] = None, next_page_token: Mapping[str, Any] = None
     ) -> MutableMapping[str, Any]:
         """
-        TODO: Override this method to define any query parameters to be set. Remove this method if you don't need to define request params.
-        Usually contains common params e.g. pagination size etc.
+        No default query parameters.
         """
         return {}
 
     def parse_response(self, response: requests.Response, **kwargs) -> Iterable[Mapping]:
         """
-        TODO: Override this method to define how a response is parsed.
-        :return an iterable containing each record in the response
+        Parse the response and yield records.
         """
-        yield {}
+        yield response.json()
 
+    def read_records(self, sync_mode, cursor_field = None, stream_slice: Mapping[str, Any] = None, stream_state: Mapping[str, Any] = None) -> Iterable[Mapping[str, Any]]:
+        raise NotImplementedError("Subclasses should implement this method")
 
-class Customers(CouchbaseStream):
+class Buckets(CouchbaseStream):
     """
-    TODO: Change class name to match the table/data source this stream corresponds to.
+    Stream for Couchbase buckets.
     """
+    primary_key = "name"
 
-    # TODO: Fill in the primary key. Required. This is usually a unique field in the stream, like an ID or a timestamp.
-    primary_key = "customer_id"
+    def read_records(self, sync_mode, cursor_field = None, stream_slice: Mapping[str, Any] = None, stream_state: Mapping[str, Any] = None) -> Iterable[Mapping[str, Any]]:
+        query = "SELECT name, bucketType, numReplicas FROM system:buckets"
+        result = self.cluster.query(query)
+        for row in result:
+            yield row
 
-    def path(
-        self, stream_state: Mapping[str, Any] = None, stream_slice: Mapping[str, Any] = None, next_page_token: Mapping[str, Any] = None
-    ) -> str:
-        """
-        TODO: Override this method to define the path this stream corresponds to. E.g. if the url is https://example-api.com/v1/customers then this
-        should return "customers". Required.
-        """
-        return "customers"
-
-
-# Basic incremental stream
-class IncrementalCouchbaseStream(CouchbaseStream, ABC):
+class Documents(CouchbaseStream):
     """
-    TODO fill in details of this class to implement functionality related to incremental syncs for your connector.
-         if you do not need to implement incremental sync for any streams, remove this class.
+    Stream for Couchbase documents within a bucket and scope.
     """
+    primary_key = "id"
 
-    # TODO: Fill in to checkpoint stream reads after N records. This prevents re-reading of data if the stream fails for any reason.
-    state_checkpoint_interval = None
+    def __init__(self, cluster: Cluster, bucket_name: str, scope_name: str, collection_name: str):
+        super().__init__(cluster, bucket_name, scope_name)
+        self.collection = self.scope.collection(collection_name)
 
-    @property
-    def cursor_field(self) -> str:
-        """
-        TODO
-        Override to return the cursor field used by this stream e.g: an API entity might always use created_at as the cursor field. This is
-        usually id or date based. This field's presence tells the framework this in an incremental stream. Required for incremental.
+    def read_records(self, sync_mode, cursor_field = None, stream_slice: Mapping[str, Any] = None, stream_state: Mapping[str, Any] = None) -> Iterable[Mapping[str, Any]]:
+        query = f"SELECT META().id, * FROM `{self.bucket.name}`.`{self.scope.name}`.`{self.collection.name}`"
+        result = self.cluster.query(query)
+        for row in result:
+            yield row
 
-        :return str: The name of the cursor field.
-        """
-        return []
-
-    def get_updated_state(self, current_stream_state: MutableMapping[str, Any], latest_record: Mapping[str, Any]) -> Mapping[str, Any]:
-        """
-        Override to determine the latest state after reading the latest record. This typically compared the cursor_field from the latest record and
-        the current state and picks the 'most' recent cursor. This is how a stream's state is determined. Required for incremental.
-        """
-        return {}
-
-
-class Employees(IncrementalCouchbaseStream):
-    """
-    TODO: Change class name to match the table/data source this stream corresponds to.
-    """
-
-    # TODO: Fill in the cursor_field. Required.
-    cursor_field = "start_date"
-
-    # TODO: Fill in the primary key. Required. This is usually a unique field in the stream, like an ID or a timestamp.
-    primary_key = "employee_id"
-
-    def path(self, **kwargs) -> str:
-        """
-        TODO: Override this method to define the path this stream corresponds to. E.g. if the url is https://example-api.com/v1/employees then this should
-        return "single". Required.
-        """
-        return "employees"
-
-    def stream_slices(self, stream_state: Mapping[str, Any] = None, **kwargs) -> Iterable[Optional[Mapping[str, any]]]:
-        """
-        TODO: Optionally override this method to define this stream's slices. If slicing is not needed, delete this method.
-
-        Slices control when state is saved. Specifically, state is saved after a slice has been fully read.
-        This is useful if the API offers reads by groups or filters, and can be paired with the state object to make reads efficient. See the "concepts"
-        section of the docs for more information.
-
-        The function is called before reading any records in a stream. It returns an Iterable of dicts, each containing the
-        necessary data to craft a request for a slice. The stream state is usually referenced to determine what slices need to be created.
-        This means that data in a slice is usually closely related to a stream's cursor_field and stream_state.
-
-        An HTTP request is made for each returned slice. The same slice can be accessed in the path, request_params and request_header functions to help
-        craft that specific request.
-
-        For example, if https://example-api.com/v1/employees offers a date query params that returns data for that particular day, one way to implement
-        this would be to consult the stream state object for the last synced date, then return a slice containing each date from the last synced date
-        till now. The request_params function would then grab the date from the stream_slice and make it part of the request by injecting it into
-        the date query param.
-        """
-        raise NotImplementedError("Implement stream slices or delete this method!")
-
-
-# Source
 class SourceCouchbase(AbstractSource):
-    def check_connection(self, logger, config) -> Tuple[bool, any]:
-        """
-        TODO: Implement a connection check to validate that the user-provided config can be used to connect to the underlying API
 
-        See https://github.com/airbytehq/airbyte/blob/master/airbyte-integrations/connectors/source-stripe/source_stripe/source.py#L232
-        for an example.
+    logger: logging.Logger = logging.getLogger("airbyte")
 
-        :param config:  the user-input config object conforming to the connector's spec.yaml
-        :param logger:  logger object
-        :return Tuple[bool, any]: (True, None) if the input config can be used to connect to the API successfully, (False, error) otherwise.
+    def check_connection(self, logger: logging.Logger
+                         , config) -> Tuple[bool, any]:
         """
-        return True, None
+        Checks if the input configuration can be used to successfully connect to the Couchbase cluster.
+        """
+        try:
+            cluster = self._get_cluster(config)
+            cluster.ping()
+            logger.info("Successfully connected to Couchbase cluster")
+            return True, None
+        except Exception as e:
+            logger.error(f"Connection check failed: {str(e)}")
+            return False, f"Connection check failed: {str(e)}"
 
     def streams(self, config: Mapping[str, Any]) -> List[Stream]:
         """
-        TODO: Replace the streams below with your own streams.
-
-        :param config: A Mapping of the user input configuration as defined in the connector spec.
+        Returns a list of discovered streams.
         """
-        # TODO remove the authenticator if not required.
-        auth = TokenAuthenticator(token="api_key")  # Oauth2Authenticator is also available if you need oauth support
-        return [Customers(authenticator=auth), Employees(authenticator=auth)]
+        cluster = self._get_cluster(config)
+        streams = [Buckets(cluster, config['bucket_name'])]
+        
+        try:
+            bucket = cluster.bucket(config['bucket_name'])
+            for scope in bucket.collections().get_all_scopes():
+                for collection in scope.collections:
+                    streams.append(
+                        Documents(
+                            cluster,
+                            config['bucket_name'],
+                            scope.name,
+                            collection.name
+                        )
+                    )
+            self.logger.info(f"Discovered {len(streams)} streams")
+        except Exception as e:
+            self.logger.error(f"Error discovering streams: {str(e)}")
+        
+        return streams
+
+    @staticmethod
+    def _get_cluster(config: Mapping[str, Any]) -> Cluster:
+        auth = PasswordAuthenticator(config['username'], config['password'])
+        options = ClusterOptions(auth)
+        options.apply_profile('wan_development')
+        cluster = Cluster(f"{config['connection_string']}", options)
+        cluster.wait_until_ready(timedelta(seconds=5))
+        return cluster
+
+    @staticmethod
+    def validate_config(config: Mapping[str, Any]) -> Tuple[bool, Optional[str]]:
+        """
+        Validates the configuration.
+        """
+        required_fields = ["connection_string", "username", "password", "bucket_name"]
+        missing_fields = [field for field in required_fields if field not in config]
+        
+        if missing_fields:
+            return False, f"Missing required configuration fields: {', '.join(missing_fields)}"
+        
+        return True, None
