ab7331026e8f5748dc036c33fb4927620d32fe93
Xiaohan Song <xiaohan@airbyte.io>
2024-09-25 14:30:57 -0700

    [source-mysql-v2] Complete datatypes (#45465)

--- airbyte-integrations/connectors/source-mysql-v2/src/main/kotlin/io/airbyte/integrations/source/mysql/MysqlSourceOperations.kt
+++ airbyte-integrations/connectors/source-mysql-v2/src/main/kotlin/io/airbyte/integrations/source/mysql/MysqlSourceOperations.kt
@@ -1,30 +1,29 @@
 /* Copyright (c) 2024 Airbyte, Inc., all rights reserved. */
 package io.airbyte.integrations.source.mysql
 
+import com.mysql.cj.MysqlType
 import io.airbyte.cdk.discover.Field
 import io.airbyte.cdk.discover.FieldType
 import io.airbyte.cdk.discover.JdbcMetadataQuerier
 import io.airbyte.cdk.discover.SystemType
-import io.airbyte.cdk.discover.UserDefinedArray
-import io.airbyte.cdk.discover.UserDefinedType
-import io.airbyte.cdk.jdbc.ArrayFieldType
 import io.airbyte.cdk.jdbc.BigDecimalFieldType
 import io.airbyte.cdk.jdbc.BigIntegerFieldType
 import io.airbyte.cdk.jdbc.BinaryStreamFieldType
 import io.airbyte.cdk.jdbc.BooleanFieldType
-import io.airbyte.cdk.jdbc.ClobFieldType
+import io.airbyte.cdk.jdbc.ByteFieldType
 import io.airbyte.cdk.jdbc.DoubleFieldType
 import io.airbyte.cdk.jdbc.FloatFieldType
+import io.airbyte.cdk.jdbc.IntFieldType
 import io.airbyte.cdk.jdbc.JdbcFieldType
-import io.airbyte.cdk.jdbc.JsonStringFieldType
 import io.airbyte.cdk.jdbc.LocalDateFieldType
 import io.airbyte.cdk.jdbc.LocalDateTimeFieldType
+import io.airbyte.cdk.jdbc.LocalTimeFieldType
 import io.airbyte.cdk.jdbc.LongFieldType
 import io.airbyte.cdk.jdbc.LosslessJdbcFieldType
-import io.airbyte.cdk.jdbc.NClobFieldType
-import io.airbyte.cdk.jdbc.NStringFieldType
+import io.airbyte.cdk.jdbc.NullFieldType
 import io.airbyte.cdk.jdbc.OffsetDateTimeFieldType
 import io.airbyte.cdk.jdbc.PokemonFieldType
+import io.airbyte.cdk.jdbc.ShortFieldType
 import io.airbyte.cdk.jdbc.StringFieldType
 import io.airbyte.cdk.read.And
 import io.airbyte.cdk.read.Equal
@@ -63,81 +62,66 @@ import jakarta.inject.Singleton
 class MysqlSourceOperations : JdbcMetadataQuerier.FieldTypeMapper, SelectQueryGenerator {
     override fun toFieldType(c: JdbcMetadataQuerier.ColumnMetadata): FieldType =
         when (val type = c.type) {
-            is SystemType -> leafType(c.type.typeName, type.scale != 0)
-            is UserDefinedArray -> ArrayFieldType(recursiveArrayType(type))
-            is UserDefinedType -> PokemonFieldType
-        }
-
-    private fun recursiveArrayType(type: UserDefinedArray): JdbcFieldType<*> =
-        when (val elementType = type.elementType) {
-            is SystemType -> {
-                val leafType: JdbcFieldType<*> =
-                    leafType(elementType.typeName, elementType.scale != 0)
-                if (leafType == OffsetDateTimeFieldType) {
-                    // Mysql's JDBC driver has a bug which prevents object conversions in
-                    // ArrayDataResultSet instances. Fall back to strings instead.
-                    PokemonFieldType
-                } else {
-                    leafType
-                }
-            }
-            is UserDefinedArray -> ArrayFieldType(recursiveArrayType(elementType))
-            is UserDefinedType -> PokemonFieldType
-        }
-
-    private fun leafType(
-        typeName: String?,
-        notInteger: Boolean,
-    ): JdbcFieldType<*> =
-        // TODO: https://github.com/airbytehq/airbyte-internal-issues/issues/9670
-        when (typeName) {
-            "BINARY_FLOAT" -> FloatFieldType
-            "BINARY_DOUBLE" -> DoubleFieldType
-            "FLOAT",
-            "DOUBLE PRECISION",
-            "REAL", -> BigDecimalFieldType
-            "NUMBER",
-            "NUMERIC",
-            "DECIMAL",
-            "DEC", -> if (notInteger) BigDecimalFieldType else BigIntegerFieldType
-            "INTEGER",
-            "INT",
-            "SMALLINT", -> BigIntegerFieldType
-            "BOOLEAN",
-            "BOOL", -> BooleanFieldType
-            "CHAR",
-            "VARCHAR2",
-            "VARCHAR",
-            "CHARACTER",
-            "CHARACTER VARYING",
-            "CHAR VARYING", -> StringFieldType
-            "NCHAR",
-            "NVARCHAR2",
-            "NCHAR VARYING",
-            "NATIONAL CHARACTER VARYING",
-            "NATIONAL CHARACTER",
-            "NATIONAL CHAR VARYING",
-            "NATIONAL CHAR", -> NStringFieldType
-            "BLOB" -> BinaryStreamFieldType
-            "CLOB" -> ClobFieldType
-            "NCLOB" -> NClobFieldType
-            "BFILE" -> BinaryStreamFieldType
-            "DATE" -> LocalDateFieldType
-            "INTERVALDS",
-            "INTERVAL DAY TO SECOND",
-            "INTERVALYM",
-            "INTERVAL YEAR TO MONTH", -> StringFieldType
-            "JSON" -> JsonStringFieldType
-            "LONG",
-            "LONG RAW",
-            "RAW", -> BinaryStreamFieldType
-            "TIMESTAMP",
-            "TIMESTAMP WITH LOCAL TIME ZONE",
-            "TIMESTAMP WITH LOCAL TZ", -> LocalDateTimeFieldType
-            "TIMESTAMP WITH TIME ZONE",
-            "TIMESTAMP WITH TZ", -> OffsetDateTimeFieldType
+            is SystemType -> leafType(type)
             else -> PokemonFieldType
         }
+
+    private fun leafType(type: SystemType): JdbcFieldType<*> {
+        return when (MysqlType.getByName(type.typeName)) {
+            MysqlType.BIT -> {
+                if (type.precision!! > 1) {
+                    ByteFieldType
+                } else {
+                    BooleanFieldType
+                }
+            }
+            MysqlType.BOOLEAN -> BooleanFieldType
+            MysqlType.TINYINT,
+            MysqlType.TINYINT_UNSIGNED,
+            MysqlType.YEAR -> ShortFieldType
+            MysqlType.SMALLINT,
+            MysqlType.SMALLINT_UNSIGNED,
+            MysqlType.MEDIUMINT,
+            MysqlType.MEDIUMINT_UNSIGNED,
+            MysqlType.INT -> IntFieldType
+            MysqlType.INT_UNSIGNED,
+            MysqlType.BIGINT,
+            MysqlType.BIGINT_UNSIGNED -> BigIntegerFieldType
+            MysqlType.FLOAT,
+            MysqlType.FLOAT_UNSIGNED -> FloatFieldType
+            MysqlType.DOUBLE,
+            MysqlType.DOUBLE_UNSIGNED -> DoubleFieldType
+            MysqlType.DECIMAL,
+            MysqlType.DECIMAL_UNSIGNED -> {
+                if (type.scale == 0) BigIntegerFieldType else BigDecimalFieldType
+            }
+            MysqlType.DATE -> LocalDateFieldType
+            MysqlType.DATETIME -> LocalDateTimeFieldType
+            MysqlType.TIMESTAMP -> OffsetDateTimeFieldType
+            MysqlType.TIME -> LocalTimeFieldType
+            MysqlType.CHAR,
+            MysqlType.VARCHAR,
+            MysqlType.TINYTEXT,
+            MysqlType.TEXT,
+            MysqlType.MEDIUMTEXT,
+            MysqlType.LONGTEXT,
+            MysqlType.JSON,
+            MysqlType.ENUM,
+            MysqlType.SET -> StringFieldType
+            MysqlType.TINYBLOB,
+            MysqlType.BLOB,
+            MysqlType.MEDIUMBLOB,
+            MysqlType.LONGBLOB,
+            MysqlType.BINARY,
+            MysqlType.VARBINARY,
+            MysqlType.GEOMETRY -> BinaryStreamFieldType
+            MysqlType.NULL -> NullFieldType
+            else -> {
+                print("test debug: unrecognized type: ${type.typeName}")
+                PokemonFieldType
+            }
+        }
+    }
 
     override fun generate(ast: SelectQuerySpec): SelectQuery =
         SelectQuery(ast.sql(), ast.select.columns, ast.bindings())
