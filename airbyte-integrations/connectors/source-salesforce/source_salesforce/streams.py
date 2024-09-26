8386e774d826e4189ea7ad57ba635b69b688bcc4
maxi297 <maxime@airbyte.io>
2024-09-25 20:10:32 -0400

    Skip when rest stream is not available

--- airbyte-integrations/connectors/source-salesforce/source_salesforce/streams.py
+++ airbyte-integrations/connectors/source-salesforce/source_salesforce/streams.py
@@ -822,10 +822,15 @@ class BulkSalesforceStream(SalesforceStream):
         try:
             yield from self._bulk_job_stream.stream_slices(sync_mode=sync_mode, cursor_field=cursor_field, stream_state=stream_state)
         except BulkNotSupportedException:
-            self.logger.warning("switch to STANDARD(non-BULK) sync. Because the SalesForce BULK job has returned a failed status")
+            self.logger.warning("attempt to switch to STANDARD(non-BULK) sync. Because the SalesForce BULK job has returned a failed status")
             self._switch_from_bulk_to_rest = True
             self._rest_stream = self.get_standard_instance()
-            yield from self._rest_stream.stream_slices(sync_mode=sync_mode, cursor_field=cursor_field, stream_state=stream_state)
+            stream_is_available, error = SalesforceAvailabilityStrategy().check_availability(self._rest_stream, self.logger, None)
+            if not stream_is_available:
+                self.logger.warning(f"Skipped syncing stream '{self._rest_stream.name}' because it was unavailable. Error: {error}")
+                yield from []
+            else:
+                yield from self._rest_stream.stream_slices(sync_mode=sync_mode, cursor_field=cursor_field, stream_state=stream_state)
 
     def get_standard_instance(self) -> SalesforceStream:
         """Returns a instance of standard logic(non-BULK) with same settings"""
