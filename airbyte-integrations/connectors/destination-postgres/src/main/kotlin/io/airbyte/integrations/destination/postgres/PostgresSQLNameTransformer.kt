35ff246f165c78af946d4d1a30bb370989d39a7e
William Kaper <bill@hubifi.com>
2024-09-26 13:17:01 -0400

    Updated algo

--- airbyte-integrations/connectors/destination-postgres/src/main/kotlin/io/airbyte/integrations/destination/postgres/PostgresSQLNameTransformer.kt
+++ airbyte-integrations/connectors/destination-postgres/src/main/kotlin/io/airbyte/integrations/destination/postgres/PostgresSQLNameTransformer.kt
@@ -38,7 +38,15 @@ class PostgresSQLNameTransformer : StandardNameTransformer() {
      * truncation explicitly, so that we can detect e.g. name collisions.
      */
     private fun truncate(str: String): String {
-        // return str.substring(0, min(str.length.toDouble(), 63.0).toInt())
-        return if (str.length > 63.0) str.substring(0, min(str.length.toDouble(), 52.0).toInt()).plus(abs(str.hashCode()).toString(36)) else str
+        /*
+            V2 Now uses {rawTableName}_airbyte_tmp, but never checks the length of the full string.
+            Truncating the rawTableName to 63 leaves no room for this crazy long suffix name (why is it not _tmp??)
+            I've put this hack in for 2 purposes. First, anything longer than 60 gets truncated down and has a base36 hashcode
+            applied based on the untruncated string for reproducible uniquness. This still will cause `_airbyte_tmp` to get truncated
+            because nothing checks the length of that string, but that seems to be okay. I'd suggest shortening this to something like _tmp,
+            which will always fit with the algorithm below.
+        */
+
+        return if (str.length >= 60.0) str.substring(0, min(str.length.toDouble(), 52.0).toInt()).plus(abs(str.hashCode()).toString(36)) else str
     }
 }
