5a03b48a1067c1903c8b3e802cdee4b82e83ba50
pnilan <patrick.nilan@airbyte.io>
2024-10-02 10:03:02 -0700

    update pagination decoder to include logger

--- airbyte-cdk/python/airbyte_cdk/sources/declarative/decoders/pagination_decoder.py
+++ airbyte-cdk/python/airbyte_cdk/sources/declarative/decoders/pagination_decoder.py
@@ -4,22 +4,25 @@
 
 from typing import Any, Generator, Mapping
 import requests
+import logging
 
 from airbyte_cdk.sources.declarative.decoders import Decoder
 
 
-class PaginationDecoder:
+class PaginationDecoder(Decoder):
     """
     Decoder to wrap other decoders when instantiating a DefaultPaginator in order to bypass decoding if the response is streamed.
     """
-    def __init__(self, decoder: Decoder):
+    def __init__(self, logger: logging.Logger, decoder: Decoder):
         self._decoder = decoder
+        self._logger = logger
 
     def is_stream_response(self) -> bool:
         return self._decoder.is_stream_response
 
     def decode(self, response: requests.Response) -> Generator[Mapping[str, Any], None, None]:
-        if self.is_stream_response():
-            return {}
+        if self._decoder.is_stream_response():
+            self._logger.warning("Response is streamed and therefore will not be decoded for pagination.")
+            yield {}
         else:
             yield from self._decoder.decode(response)
