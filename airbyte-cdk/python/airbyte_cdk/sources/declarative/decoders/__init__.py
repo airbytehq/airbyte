2531640ecd29c8c3f05da114558cef66741241d6
pnilan <patrick.nilan@airbyte.io>
2024-10-02 10:56:06 -0700

    remove pagination decoder decorator from scope of this issue

--- airbyte-cdk/python/airbyte_cdk/sources/declarative/decoders/__init__.py
+++ airbyte-cdk/python/airbyte_cdk/sources/declarative/decoders/__init__.py
@@ -4,6 +4,5 @@
 
 from airbyte_cdk.sources.declarative.decoders.decoder import Decoder
 from airbyte_cdk.sources.declarative.decoders.json_decoder import JsonDecoder, JsonlDecoder, IterableDecoder
-from airbyte_cdk.sources.declarative.decoders.pagination_decoder_decorator import PaginationDecoderDecorator
 
-__all__ = ["Decoder", "JsonDecoder", "JsonlDecoder", "IterableDecoder", "PaginationDecoderDecorator"]
+__all__ = ["Decoder", "JsonDecoder", "JsonlDecoder", "IterableDecoder"]
