1b57c8e9639df3a79c43f9dc74cf7c9be1579a2a
Edward Gao <edward.gao@airbyte.io>
2024-10-03 08:53:37 -0700

    implement docker runner

--- airbyte-cdk/bulk/core/load/src/testFixtures/kotlin/io/airbyte/cdk/load/test/util/destination_process/DockerizedDestination.kt
+++ airbyte-cdk/bulk/core/load/src/testFixtures/kotlin/io/airbyte/cdk/load/test/util/destination_process/DockerizedDestination.kt
@@ -4,20 +4,110 @@
 
 package io.airbyte.cdk.load.test.util.destination_process
 
-import com.fasterxml.jackson.databind.JsonNode
+import com.google.common.collect.Lists
+import io.airbyte.cdk.command.ConfigurationSpecification
+import io.airbyte.cdk.util.Jsons
 import io.airbyte.protocol.models.v0.AirbyteMessage
 import io.airbyte.protocol.models.v0.ConfiguredAirbyteCatalog
+import io.github.oshai.kotlinlogging.KotlinLogging
 import java.io.ByteArrayOutputStream
 import java.io.InputStream
+import java.nio.file.Files
+import java.nio.file.Path
+import java.util.Locale
+import org.apache.commons.lang3.RandomStringUtils
+
+private val logger = KotlinLogging.logger {}
 
 // TODO define a factory for this class + @Require(env = CI_master_merge)
 class DockerizedDestination(
-    val command: String,
-    val config: JsonNode?,
-    val catalog: ConfiguredAirbyteCatalog?,
+    private val imageName: String,
+    private val command: String,
+    private val config: ConfigurationSpecification?,
+    private val catalog: ConfiguredAirbyteCatalog?,
+    private val testDeploymentMode: TestDeploymentMode,
 ) : DestinationProcess {
+    private lateinit var process: Process
+
     override fun run() {
-        TODO("launch a docker container")
+        // This is largely copied from the old cdk's DockerProcessFactory /
+        // AirbyteIntegrationLauncher / DestinationAcceptanceTest,
+        // but cleaned up, consolidated, and simplified.
+        // Those classes included a ton of logic that is only useful for
+        // the actual platform, and we don't need it here.
+        val testDir = Path.of("/tmp/airbyte_tests/")
+        Files.createDirectories(testDir)
+        val workspaceRoot = Files.createTempDirectory(testDir, "test")
+        // This directory gets mounted to the docker container,
+        // presumably so that we can extract some files out of it?
+        // It's unclear to me that we actually need to do this...
+        // Certainly nothing in the bulk CDK's test suites is reading back
+        // anything in this directory.
+        val localRoot = Files.createTempDirectory(testDir, "output")
+        // This directory will contain the actual inputs to the connector (config+catalog),
+        // and is also mounted as a volume.
+        val jobRoot = Files.createDirectories(workspaceRoot.resolve("job"))
+
+        val randomSuffix =
+            RandomStringUtils.insecure()
+                .nextAlphanumeric(5)
+                .lowercase(Locale.getDefault())
+        // Extract "destination-foo" from "gcr.io/airbyte/destination-foo:1.2.3"
+        // The old code had a ton of extra logic here, along with a max string
+        // length (docker container names must be <128 chars) - none of that
+        // seems necessary here.
+        // And platform doesn't even follow that convention anymore, now that
+        // we have monopods. (the pod has a name like replication-job-18386126-attempt-4,
+        // and the destination container is just called "destination")
+        val shortImageName =
+            imageName.substringAfterLast("/").substringBefore(":")
+        val containerName = "$shortImageName-$command-$randomSuffix"
+        logger.info { "Creating docker container $containerName" }
+
+        val cmd: MutableList<String> =
+            Lists.newArrayList(
+                "docker",
+                "run",
+                "--rm",
+                "--init",
+                "-i",
+                "-w",
+                "/data/job",
+                "--log-driver",
+                "none",
+                "--name",
+                containerName,
+                "--network",
+                "host",
+                "-v",
+                String.format("%s:%s", workspaceRoot, "/data"),
+                "-v",
+                String.format("%s:%s", localRoot, "/local"),
+                "-e",
+                "DEPLOYMENT_MODE=$testDeploymentMode",
+                // Yes, we hardcode the job ID to 0.
+                // Also yes, this is available in the configured catalog
+                // via the syncId property.
+                // Also also yes, we're relying on this env var >.>
+                "-e",
+                "WORKER_JOB_ID=0",
+                imageName,
+                command,
+            )
+
+        fun addInput(paramName: String, fileContents: Any) {
+            Files.write(
+                jobRoot.resolve("destination_$paramName.json"),
+                Jsons.writeValueAsBytes(fileContents)
+            )
+            cmd.add("--$paramName")
+            cmd.add("destination_$paramName.json")
+        }
+        config?.let { addInput("config", it) }
+        catalog?.let { addInput("catalog", it) }
+
+        logger.info { "Executing command: ${cmd.joinToString(" ")}" }
+        process = ProcessBuilder(cmd).start()
     }
 
     override fun sendMessage(message: AirbyteMessage) {
@@ -78,7 +168,7 @@ private class LazyInputStreamReader(private val input: InputStream) {
     }
 
     companion object {
-        interface MaybeLine
+        sealed interface MaybeLine
         enum class NoLine : MaybeLine {
             EOF,
             NOT_YET_AVAILABLE
