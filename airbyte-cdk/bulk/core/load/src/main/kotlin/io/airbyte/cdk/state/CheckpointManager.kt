67e9a8c4287468aabab996b9d0cede3d85562c4f
Johnny Schmidt <john.schmidt@airbyte.io>
2024-10-07 11:59:59 -0700

    Bulk Load CDK StreamIncomplete Prefactor 3: Checkpoints processed async (#46517)

--- airbyte-cdk/bulk/core/load/src/main/kotlin/io/airbyte/cdk/state/CheckpointManager.kt
+++ airbyte-cdk/bulk/core/load/src/main/kotlin/io/airbyte/cdk/state/CheckpointManager.kt
@@ -4,11 +4,12 @@
 
 package io.airbyte.cdk.state
 
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings
 import io.airbyte.cdk.command.DestinationCatalog
 import io.airbyte.cdk.command.DestinationStream
 import io.airbyte.cdk.file.TimeProvider
 import io.airbyte.cdk.message.CheckpointMessage
-import io.airbyte.cdk.message.MessageConverter
+import io.airbyte.cdk.util.use
 import io.airbyte.protocol.models.v0.AirbyteMessage
 import io.github.oshai.kotlinlogging.KotlinLogging
 import io.micronaut.context.annotation.Secondary
@@ -48,7 +49,7 @@ interface CheckpointManager<K, T> {
  * TODO: Ensure that checkpoint is flushed at the end, and require that all checkpoints be flushed
  * before the destination can succeed.
  */
-abstract class StreamsCheckpointManager<T, U> : CheckpointManager<DestinationStream.Descriptor, T> {
+abstract class StreamsCheckpointManager<T> : CheckpointManager<DestinationStream.Descriptor, T> {
 
     private val log = KotlinLogging.logger {}
     private val flushLock = Mutex()
@@ -56,8 +57,7 @@ abstract class StreamsCheckpointManager<T, U> : CheckpointManager<DestinationStr
 
     abstract val catalog: DestinationCatalog
     abstract val syncManager: SyncManager
-    abstract val outputFactory: MessageConverter<T, U>
-    abstract val outputConsumer: Consumer<U>
+    abstract val outputConsumer: suspend (T) -> Unit
     abstract val timeProvider: TimeProvider
 
     data class GlobalCheckpoint<T>(
@@ -200,8 +200,7 @@ abstract class StreamsCheckpointManager<T, U> : CheckpointManager<DestinationStr
     private suspend fun sendMessage(checkpointMessage: T) =
         withContext(Dispatchers.IO) {
             lastFlushTimeMs.set(timeProvider.currentTimeMillis())
-            val outMessage = outputFactory.from(checkpointMessage)
-            outputConsumer.accept(outMessage)
+            outputConsumer.invoke(checkpointMessage)
         }
 
     override suspend fun getLastSuccessfulFlushTimeMs(): Long =
@@ -235,11 +234,26 @@ abstract class StreamsCheckpointManager<T, U> : CheckpointManager<DestinationStr
 class DefaultCheckpointManager(
     override val catalog: DestinationCatalog,
     override val syncManager: SyncManager,
-    override val outputFactory: MessageConverter<CheckpointMessage, AirbyteMessage>,
-    override val outputConsumer: Consumer<AirbyteMessage>,
+    override val outputConsumer: suspend (Reserved<CheckpointMessage>) -> Unit,
     override val timeProvider: TimeProvider
-) : StreamsCheckpointManager<CheckpointMessage, AirbyteMessage>() {
+) : StreamsCheckpointManager<Reserved<CheckpointMessage>>() {
     init {
         lastFlushTimeMs.set(timeProvider.currentTimeMillis())
+    }
+}
+
+@SuppressFBWarnings(
+    "NP_NONNULL_PARAM_VIOLATION",
+    justification = "message is guaranteed to be non-null by Kotlin's type system"
+)
+@Singleton
+@Secondary
+class FreeingCheckpointConsumer(private val consumer: Consumer<AirbyteMessage>) :
+    suspend (Reserved<CheckpointMessage>) -> Unit {
+    override suspend fun invoke(message: Reserved<CheckpointMessage>) {
+        message.use {
+            val outMessage = it.value.asProtocolMessage()
+            consumer.accept(outMessage)
+        }
     }
 }
