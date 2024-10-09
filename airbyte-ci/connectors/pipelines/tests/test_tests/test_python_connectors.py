01d755bc3b2b9ca131ebbdf95a1aa26239096562
Aaron Steers <aj@airbyte.io>
2024-10-09 15:59:40 -0700

    update test name

--- airbyte-ci/connectors/pipelines/tests/test_tests/test_python_connectors.py
+++ airbyte-ci/connectors/pipelines/tests/test_tests/test_python_connectors.py
@@ -122,7 +122,7 @@ class TestPyAirbyteValidationTests:
     @pytest.fixture
     def context_for_valid_connector(self, compatible_connector, dagger_client, current_platform):
         context = ConnectorContext(
-            pipeline_name="CLI validation with PyAirbyte",
+            pipeline_name="CLI smoke test with PyAirbyte",
             connector=compatible_connector,
             git_branch="test",
             git_revision="test",
@@ -138,7 +138,7 @@ class TestPyAirbyteValidationTests:
     @pytest.fixture
     def context_for_invalid_connector(self, incompatible_connector, dagger_client, current_platform):
         context = ConnectorContext(
-            pipeline_name="CLI validation with PyAirbyte",
+            pipeline_name="CLI smoke test with PyAirbyte",
             connector=incompatible_connector,
             git_branch="test",
             git_revision="test",
