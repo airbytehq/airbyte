3939a67e7f6b23b368433ea05245a55278df1407
Natik Gadzhi <natik@respawn.io>
2024-10-08 20:29:47 -0700

    obfuscator comments

--- tools/bin/record_obfuscator.py
+++ tools/bin/record_obfuscator.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # Copyright (c) 2024 Airbyte, Inc., all rights reserved.
-
+from __future__ import annotations
 
 import hashlib
 import json
@@ -8,11 +8,26 @@ import sys
 from typing import Any
 
 
+#
+# record_obfuscator is a tiny script that:
+# 1. reads JSON lines from stdin
+# 2. obfuscates the data in AirbyteRecordMessage lines
+# 3. spits obfuscated lines back to stdout
+# All stdin lines that are not type: RECORD remain unchanged.
+#
+# It's used in live-tests (airbyte-ci/connectors/live-tests) to be able to dump raw data from a live connection
+# without leaking actual sensitive production data.
+#
+# The script is copied over into the live tests runnder dagger container.
+#
+
+
 def _generate_hash(value: Any) -> str:
     return hashlib.sha256(str(value).encode()).hexdigest()
 
 
 def obfuscate(value: Any) -> Any:
+    """Returns an obfuscated version of the input value while retaiining it's type and length information."""
     if isinstance(value, str):
         obfuscated_value = f"string_len-{len(value)}_" + _generate_hash(value)
     elif isinstance(value, int):
@@ -41,6 +56,8 @@ if __name__ == "__main__":
         except Exception as exc:
             # We don't expect invalid json so if we see it, it will go to stderr
             sys.stderr.write(f"{line}\n")
+
+        # try / except / else: Else block runs only if no exceptions were raised.
         else:
             if data.get("type") == "RECORD":
                 record_data = data["record"].get("data", {})
