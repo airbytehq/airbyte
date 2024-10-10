13b158ef95559ffc6c49e5d1f7d50c79596e2b44
letiescanciano <45267095+letiescanciano@users.noreply.github.com>
2024-10-10 07:51:39 +0200

    📚 docs: Allow visitors to request ERDs (#45694)

--- docusaurus/docusaurus.config.js
+++ docusaurus/docusaurus.config.js
@@ -1,6 +1,6 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
-
+import "dotenv/config.js";
 const yaml = require("js-yaml");
 const fs = require("node:fs");
 const path = require("node:path");
@@ -78,7 +78,9 @@ const config = {
       },
     }),
   ],
-
+  customFields: {
+    requestErdApiUrl: process.env.REQUEST_ERD_API_URL,
+  },
   clientModules: [
     require.resolve("./src/scripts/cloudStatus.js"),
     require.resolve("./src/scripts/download-abctl-buttons.js"),
