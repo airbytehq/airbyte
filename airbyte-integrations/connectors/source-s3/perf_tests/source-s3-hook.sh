c488f7ddb949afd2923b169fca0f663b67f5385f
Aaron Steers <aj@airbyte.io>
2024-10-14 14:47:14 -0700

    deletion: remove `run_perf_tests.py` (in favor of `pyab benchmark` CLI)

--- airbyte-integrations/connectors/source-s3/perf_tests/source-s3-hook.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# This script is used to run the source-s3 connector with the local editable
-# poetry environment. It is called by performance test script.
-
-SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-PYTHON_EXEC="$SCRIPT_DIR/../.venv/bin/python"
-
-"$PYTHON_EXEC" -m source_s3.run "$@"
