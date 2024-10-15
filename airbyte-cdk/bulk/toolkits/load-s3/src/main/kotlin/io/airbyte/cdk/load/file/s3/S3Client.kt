d8b1b0c60c03a5b095a992f6a7d05c84d8693b1c
Johnny Schmidt <john.schmidt@airbyte.io>
2024-10-15 10:23:10 -0700

    Bulk Load CDK: ObjectStoragePath Mixin, Path Factory, & S3V2 Usage (#46751)

--- airbyte-cdk/bulk/toolkits/load-s3/src/main/kotlin/io/airbyte/cdk/load/file/s3/S3Client.kt
+++ airbyte-cdk/bulk/toolkits/load-s3/src/main/kotlin/io/airbyte/cdk/load/file/s3/S3Client.kt
@@ -2,19 +2,24 @@
  * Copyright (c) 2024 Airbyte, Inc., all rights reserved.
  */
 
-package io.airbyte.cdk.load.command.s3
+package io.airbyte.cdk.load.file.s3
 
 import aws.sdk.kotlin.runtime.auth.credentials.StaticCredentialsProvider
 import aws.sdk.kotlin.services.s3.model.ListObjectsRequest
+import aws.sdk.kotlin.services.s3.model.PutObjectRequest
+import aws.smithy.kotlin.runtime.content.ByteStream
 import edu.umd.cs.findbugs.annotations.SuppressFBWarnings
 import io.airbyte.cdk.load.command.aws.AWSAccessKeyConfigurationProvider
-import io.airbyte.cdk.load.file.ObjectStorageClient
+import io.airbyte.cdk.load.command.s3.S3BucketConfiguration
+import io.airbyte.cdk.load.command.s3.S3BucketConfigurationProvider
+import io.airbyte.cdk.load.file.object_storage.ObjectStorageClient
 import io.airbyte.cdk.load.message.RemoteObject
 import io.micronaut.context.annotation.Factory
 import io.micronaut.context.annotation.Secondary
 import jakarta.inject.Singleton
+import kotlinx.coroutines.flow.flow
 
-class S3Object(
+data class S3Object(
     override val key: String,
 ) : RemoteObject()
 
@@ -23,15 +28,41 @@ class S3Client(
     private val client: aws.sdk.kotlin.services.s3.S3Client,
     private val bucketConfig: S3BucketConfiguration
 ) : ObjectStorageClient<S3Object> {
-    override suspend fun list(prefix: String): List<S3Object> {
-        val request = ListObjectsRequest {
+    override suspend fun list(prefix: String) = flow {
+        var request = ListObjectsRequest {
             bucket = bucketConfig.s3BucketName
             this.prefix = prefix
         }
-        return client.listObjects(request).contents?.mapNotNull {
-            it.key?.let { key -> S3Object(key) }
+        var lastKey: String? = null
+        while (true) {
+            val response = client.listObjects(request)
+            response.contents?.forEach { obj ->
+                lastKey = obj.key
+                emit(S3Object(obj.key!!))
+            } // null contents => empty list, not error
+            if (client.listObjects(request).isTruncated == false) {
+                break
+            }
+            request = request.copy { marker = lastKey }
         }
-            ?: emptyList()
+    }
+
+    override suspend fun put(key: String, bytes: ByteArray) {
+        val request = PutObjectRequest {
+            bucket = bucketConfig.s3BucketName
+            this.key = key
+            body = ByteStream.fromBytes(bytes)
+        }
+        client.putObject(request)
+    }
+
+    override suspend fun delete(key: String) {
+        val request =
+            aws.sdk.kotlin.services.s3.model.DeleteObjectRequest {
+                bucket = bucketConfig.s3BucketName
+                this.key = key
+            }
+        client.deleteObject(request)
     }
 }
 
--- airbyte-integrations/connectors/destination-s3-v2/src/main/kotlin/S3V2Checker.kt
+++ airbyte-integrations/connectors/destination-s3-v2/src/main/kotlin/S3V2Checker.kt
@@ -5,13 +5,36 @@
 package io.airbyte.integrations.destination.s3_v2
 
 import io.airbyte.cdk.load.check.DestinationChecker
-import io.airbyte.cdk.load.command.s3.S3Client
+import io.airbyte.cdk.load.file.object_storage.ObjectStoragePathFactory
+import io.airbyte.cdk.load.file.s3.S3Client
+import io.github.oshai.kotlinlogging.KotlinLogging
 import jakarta.inject.Singleton
+import kotlinx.coroutines.flow.toList
 import kotlinx.coroutines.runBlocking
 
 @Singleton
-class S3V2Checker(private val s3Client: S3Client) : DestinationChecker<S3V2Configuration> {
+class S3V2Checker(
+    private val s3Client: S3Client,
+    private val pathFactory: ObjectStoragePathFactory
+) : DestinationChecker<S3V2Configuration> {
+    private val log = KotlinLogging.logger {}
+
     override fun check(config: S3V2Configuration) {
-        runBlocking { s3Client.list("") }
+        runBlocking {
+            val path = pathFactory.getStagingDirectory(mockStream())
+            val key = path.resolve("_EMPTY").toString()
+            log.info { "Checking if destination can write to $path" }
+            try {
+                s3Client.put(key, byteArrayOf())
+                val results = s3Client.list(path.toString()).toList()
+                if (results.isEmpty() || results.find { it.key == key } == null) {
+                    throw IllegalStateException("Failed to write to S3 bucket")
+                }
+                log.info { "Successfully wrote: $results" }
+            } finally {
+                s3Client.delete(key)
+                s3Client.list(path.toString()).toList()
+            }
+        }
     }
 }
