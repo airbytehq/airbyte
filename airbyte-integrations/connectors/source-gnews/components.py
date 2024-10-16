142bb9a557028c1464b90303202bcdb0f7accdb0
Octavia Squidington III <octavia-squidington-iii@users.noreply.github.com>
2024-10-16 22:13:30 +0000

    chore: auto-fix lint and format issues

--- airbyte-integrations/connectors/source-gnews/components.py
+++ airbyte-integrations/connectors/source-gnews/components.py
@@ -20,7 +20,9 @@ class WaitUntilMidnightBackoffStrategy(BackoffStrategy):
     parameters: InitVar[Mapping[str, Any]]
     config: Config
 
-    def backoff_time(self, response_or_exception: Optional[Union[requests.Response, requests.RequestException]], attempt_count: int) -> Optional[float]:
+    def backoff_time(
+        self, response_or_exception: Optional[Union[requests.Response, requests.RequestException]], attempt_count: int
+    ) -> Optional[float]:
         now_utc = datetime.now(datetime.timezone.utc)
         midnight_utc = (now_utc + timedelta(days=1)).replace(hour=0, minute=0, second=0, microsecond=0)
         delta = midnight_utc - now_utc
