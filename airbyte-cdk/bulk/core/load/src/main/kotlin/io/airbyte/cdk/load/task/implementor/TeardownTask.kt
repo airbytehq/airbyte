ed3161a87812c86cb979bb1a3a4b149a392a71b3
Johnny Schmidt <john.schmidt@airbyte.io>
2024-10-17 10:20:13 -0700

    Bulk load CDK: Task launcher blocks and joins implementor scope (#46950)

--- airbyte-cdk/bulk/core/load/src/main/kotlin/io/airbyte/cdk/load/task/implementor/TeardownTask.kt
+++ airbyte-cdk/bulk/core/load/src/main/kotlin/io/airbyte/cdk/load/task/implementor/TeardownTask.kt
@@ -7,7 +7,7 @@ package io.airbyte.cdk.load.task.implementor
 import io.airbyte.cdk.load.state.CheckpointManager
 import io.airbyte.cdk.load.state.SyncManager
 import io.airbyte.cdk.load.task.DestinationTaskLauncher
-import io.airbyte.cdk.load.task.ShutdownScope
+import io.airbyte.cdk.load.task.ImplementorScope
 import io.airbyte.cdk.load.task.SyncLevel
 import io.airbyte.cdk.load.util.setOnce
 import io.airbyte.cdk.load.write.DestinationWriter
@@ -16,7 +16,7 @@ import io.micronaut.context.annotation.Secondary
 import jakarta.inject.Singleton
 import java.util.concurrent.atomic.AtomicBoolean
 
-interface TeardownTask : SyncLevel, ShutdownScope
+interface TeardownTask : SyncLevel, ImplementorScope
 
 /**
  * Wraps @[DestinationWriter.teardown] and stops the task launcher.
