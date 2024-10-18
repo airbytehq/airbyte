d1d6e2333adcdd7435a9baf89adcf936e5ec7bba
Christo Grabowski <108154848+ChristoGrab@users.noreply.github.com>
2024-10-18 19:52:15 -0400

    Source-gnews: Migrate to manifest-only (#46959)
    Co-authored-by: Octavia Squidington III <octavia-squidington-iii@users.noreply.github.com>

--- airbyte-integrations/connectors/source-gnews/components.py
+++ airbyte-integrations/connectors/source-gnews/components.py
@@ -4,7 +4,7 @@
 
 from dataclasses import InitVar, dataclass
 from datetime import datetime, timedelta
-from typing import Any, Mapping, Optional
+from typing import Any, Mapping, Optional, Union
 
 import requests
 from airbyte_cdk.sources.declarative.requesters.error_handlers import BackoffStrategy
@@ -20,8 +20,10 @@ class WaitUntilMidnightBackoffStrategy(BackoffStrategy):
     parameters: InitVar[Mapping[str, Any]]
     config: Config
 
-    def backoff(self, response: requests.Response, attempt_count: int) -> Optional[float]:
-        now_utc = datetime.utcnow()
+    def backoff_time(
+        self, response_or_exception: Optional[Union[requests.Response, requests.RequestException]], attempt_count: int
+    ) -> Optional[float]:
+        now_utc = datetime.now(datetime.timezone.utc)
         midnight_utc = (now_utc + timedelta(days=1)).replace(hour=0, minute=0, second=0, microsecond=0)
         delta = midnight_utc - now_utc
         return delta.total_seconds() if type(delta) is timedelta else delta.seconds
