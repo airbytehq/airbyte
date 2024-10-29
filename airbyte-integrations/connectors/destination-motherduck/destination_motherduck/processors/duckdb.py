ce05e57e7831484e4507194c27a271d84b24f3d9
Guen Prawiroatmodjo <guen@motherduck.com>
2024-10-28 21:31:24 -0700

    clean up code

--- airbyte-integrations/connectors/destination-motherduck/destination_motherduck/processors/duckdb.py
+++ airbyte-integrations/connectors/destination-motherduck/destination_motherduck/processors/duckdb.py
@@ -14,6 +14,7 @@ from airbyte_cdk.sql import exceptions as exc
 from airbyte_cdk.sql.constants import AB_EXTRACTED_AT_COLUMN
 from airbyte_cdk.sql.secrets import SecretString
 from airbyte_cdk.sql.shared.sql_processor import SqlConfig, SqlProcessorBase, SQLRuntimeError
+from airbyte_cdk.sql.shared.catalog_providers import CatalogProvider
 from duckdb_engine import DuckDBEngineWarning
 from overrides import overrides
 from pydantic import Field
@@ -262,6 +263,36 @@ class DuckDBSqlProcessor(SqlProcessorBase):
             self._execute_sql(sql)
             return new_table_name
         return table_name
+
+    def _ensure_table_exists(self, stream_name: str, table_name: str, sync_mode: DestinationSyncMode) -> None:
+        if sync_mode == DestinationSyncMode.overwrite:
+            # delete the tables
+            logger.info(f"Dropping tables for overwrite: {table_name}")
+
+            self._drop_temp_table(table_name, if_exists=True)
+
+        # Get the SQL column definitions
+        sql_columns = self._get_sql_column_definitions(stream_name)
+        column_definition_str = ",\n                ".join(
+            f"{self._quote_identifier(column_name)} {sql_type}" for column_name, sql_type in sql_columns.items()
+        )
+
+        # create the table if needed
+        primary_keys = self.catalog_provider.get_primary_keys(stream_name)
+        self._create_table_if_not_exists(
+            table_name=table_name,
+            column_definition_str=column_definition_str,
+            primary_keys=primary_keys,
+        )
+
+    def prepare_stream_table(self, stream_name: str, sync_mode: DestinationSyncMode) -> None:
+        """
+        Ensure schema and table exist, create any missing columns
+        """
+        self._ensure_schema_exists()
+        table_name = self.normalizer.normalize(stream_name)
+        self._ensure_table_exists(stream_name=stream_name, table_name=table_name, sync_mode=sync_mode)
+        self._ensure_compatible_table_schema(stream_name=stream_name, table_name=table_name)
 
     def write_stream_data_from_buffer(
         self,
