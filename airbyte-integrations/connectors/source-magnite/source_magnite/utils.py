3cd3190fb0fd534e55c2e2d1ef1c5fc358aec974
ekladiousm <mira.ekladious@taico.io>
2024-10-30 14:45:35 +0100

    add magnite utilities

---
+++ airbyte-integrations/connectors/source-magnite/source_magnite/utils.py
@@ -0,0 +1,27 @@
+
+
+import argparse
+import calendar
+import datetime
+import json
+import sys
+
+import jsonschema
+import pandas as pd
+
+DATE_FORMAT = "%Y-%m-%d"
+
+
+def string_to_date(d: str, f: str = DATE_FORMAT, old_format=None) -> datetime.date:
+    # To convert the old STATE date format "YYYY-MM-DD" to the new format "YYYYMMDD" we need this `old_format` additional param.
+    # As soon as all current cloud sync will be converted to the new format we can remove this double format support.
+    if old_format:
+        try:
+            return datetime.datetime.strptime(d, old_format).date()
+        except ValueError:
+            pass
+    return datetime.datetime.strptime(d, f).date()
+
+
+def date_to_string(d: datetime.date, f: str = DATE_FORMAT) -> str:
+    return d.strftime(f)
