a69fd74a0e3f35c8fa4d7bc585e87e540b6a2019
Artem Inzhyyants <artem.inzhyyants@gmail.com>
2024-10-31 18:02:35 +0100

    Amazon Ads: migrate spec to pydantic
    Signed-off-by: Artem Inzhyyants <artem.inzhyyants@gmail.com>

--- airbyte-integrations/connectors/source-amazon-ads/source_amazon_ads/source.py
+++ airbyte-integrations/connectors/source-amazon-ads/source_amazon_ads/source.py
@@ -7,10 +7,12 @@ import logging
 from typing import Any, List, Mapping, Optional, Tuple
 
 import pendulum
+from airbyte_cdk.models import AdvancedAuth, AuthFlowType, ConnectorSpecification, DestinationSyncMode, OAuthConfigSpecification
 from airbyte_cdk.sources import AbstractSource
 from airbyte_cdk.sources.streams import Stream
 from airbyte_cdk.sources.streams.http.requests_native_auth import Oauth2Authenticator
 
+from .spec import SourceAmazonAdsSpec
 from .streams import (
     AttributionReportPerformanceAdgroup,
     AttributionReportPerformanceCampaign,
@@ -157,3 +159,41 @@ class SourceAmazonAds(AbstractSource):
                 if profile["profileId"] in requested_profiles or profile["accountInfo"]["marketplaceStringId"] in requested_marketplace_ids
             ]
         return available_profiles
+
+    def spec(self, logger: logging.Logger) -> ConnectorSpecification:
+
+        return ConnectorSpecification(
+            documentationUrl="https://docs.airbyte.com/integrations/sources/amazon-ads",
+            connectionSpecification=SourceAmazonAdsSpec.schema(),
+            supportsDBT=False,
+            advanced_auth=AdvancedAuth(
+                auth_flow_type=AuthFlowType.oauth2_0,
+                predicate_key=["auth_type"],
+                predicate_value="oauth2.0",
+                oauth_config_specification=OAuthConfigSpecification(
+                    oauth_user_input_from_connector_config_specification={
+                        "type": "object",
+                        "additionalProperties": False,
+                        "properties": {"region": {"type": "string", "path_in_connector_config": ["region"]}},
+                    },
+                    complete_oauth_output_specification={
+                        "type": "object",
+                        "additionalProperties": True,
+                        "properties": {"refresh_token": {"type": "string", "path_in_connector_config": ["refresh_token"]}},
+                    },
+                    complete_oauth_server_input_specification={
+                        "type": "object",
+                        "additionalProperties": True,
+                        "properties": {"client_id": {"type": "string"}, "client_secret": {"type": "string"}},
+                    },
+                    complete_oauth_server_output_specification={
+                        "type": "object",
+                        "additionalProperties": True,
+                        "properties": {
+                            "client_id": {"type": "string", "path_in_connector_config": ["client_id"]},
+                            "client_secret": {"type": "string", "path_in_connector_config": ["client_secret"]},
+                        },
+                    },
+                ),
+            ),
+        )
