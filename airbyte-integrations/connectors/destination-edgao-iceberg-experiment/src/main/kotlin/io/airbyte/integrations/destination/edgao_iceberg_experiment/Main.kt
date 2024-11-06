053f7f96fa8031d13ffcd956683f091f77112d7e
Edward Gao <edward.gao@airbyte.io>
2024-11-06 09:32:32 -0800

    basic glue interaction

--- airbyte-integrations/connectors/destination-edgao-iceberg-experiment/src/main/kotlin/io/airbyte/integrations/destination/edgao_iceberg_experiment/Main.kt
+++ airbyte-integrations/connectors/destination-edgao-iceberg-experiment/src/main/kotlin/io/airbyte/integrations/destination/edgao_iceberg_experiment/Main.kt
@@ -2,15 +2,22 @@ package io.airbyte.integrations.destination.edgao_iceberg_experiment
 
 import aws.sdk.kotlin.runtime.auth.credentials.StaticCredentialsProvider
 import aws.sdk.kotlin.services.glue.GlueClient
+import aws.sdk.kotlin.services.glue.model.Column
+import aws.sdk.kotlin.services.glue.model.CreateDatabaseRequest
 import aws.sdk.kotlin.services.glue.model.CreateTableRequest
+import aws.sdk.kotlin.services.glue.model.UpdateTableRequest
 import io.airbyte.cdk.util.Jsons
+import io.github.oshai.kotlinlogging.KotlinLogging
 import java.io.File
+import java.time.LocalTime
 import kotlinx.coroutines.runBlocking
 
+private val logger = KotlinLogging.logger {}
+
 fun main(): Unit = runBlocking {
-    val s3Config = Jsons.readTree(File("/Users/edgao/code/airbyte/airbyte-integrations/connectors/destination-edgao-iceberg-experiment/secrets/s3.json"))
-    val accessKeyId = s3Config["access_key_id"].asText()
-    val secretAccessKey = s3Config["secret_access_key"].asText()
+    val glueConfig = Jsons.readTree(File("/Users/edgao/code/airbyte/airbyte-integrations/connectors/destination-edgao-iceberg-experiment/secrets/glue.json"))
+    val accessKeyId = glueConfig["storage_config"]["access_key_id"].asText()
+    val secretAccessKey = glueConfig["storage_config"]["secret_access_key"].asText()
 //    S3FileIOProperties.SECRET_ACCESS_KEY
 
 //    val catalog = GlueCatalog()
@@ -33,22 +40,59 @@ fun main(): Unit = runBlocking {
 //    catalog.createNamespace(Namespace.of("edgao_test"))
 
 
-
-
-
-
-
     val glueClient = GlueClient {
         credentialsProvider = StaticCredentialsProvider {
             this.accessKeyId = accessKeyId
             this.secretAccessKey = secretAccessKey
         }
+        region = "us-east-2"
     }
-    glueClient.createTable(CreateTableRequest.invoke {
-        catalogId = "edgao_test_catalog"
-        databaseName = "edgao_test_database"
+    val databaseName = "edgao_test_database"
+    val tableName = "edgao_test_table"
+    // TODO create if not exists, for now just do it manually
+    if (false) {
+        glueClient.createDatabase(
+            CreateDatabaseRequest.invoke {
+                databaseInput {
+                    name = databaseName
+                }
+            },
+        )
+        glueClient.createTable(
+            CreateTableRequest.invoke {
+                this.databaseName = databaseName
+                tableInput {
+                    name = tableName
+                }
+            },
+        )
+    }
+
+    println("${LocalTime.now()} starting update")
+    glueClient.updateTable(UpdateTableRequest.invoke {
+        this.databaseName = databaseName
         tableInput {
-            name = "edgao_test_table"
+            name = tableName
+            storageDescriptor {
+                location = "s3://ab-destination-iceberg/edgao_test/test2.jsonl"
+                columns = listOf(
+                    Column.invoke {
+                        name = "uuid"
+                        type = "string"
+                    },
+                    Column.invoke {
+                        name = "updated_at"
+                        type = "timestamp"
+                    },
+                    Column.invoke {
+                        name = "foo"
+                        type = "struct<bar: INT>"
+                    },
+                )
+            }
         }
     })
+    println("${LocalTime.now()} done with update")
+    glueClient.close()
+    println("${LocalTime.now()} closed client")
 }
