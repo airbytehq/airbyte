b58551d4ef5a0a5ac556e8965cb3e3d3fe42a829
Aldo Gonzalez <aldo.gonzalez@airbyte.io>
2024-11-05 18:57:48 -0600

    file-transfer(cdk): remove unused imports

--- airbyte-cdk/python/airbyte_cdk/sources/file_based/file_types/blob_transfer.py
+++ airbyte-cdk/python/airbyte_cdk/sources/file_based/file_types/blob_transfer.py
@@ -3,8 +3,7 @@
 #
 import logging
 import os
-from io import IOBase
-from typing import Any, Dict, Generator, Iterable, Optional, Tuple
+from typing import Any, Dict, Iterable
 
 from airbyte_cdk.sources.file_based.config.file_based_stream_config import FileBasedStreamConfig
 from airbyte_cdk.sources.file_based.file_based_stream_reader import AbstractFileBasedStreamReader, FileReadMode
