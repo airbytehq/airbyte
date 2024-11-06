3bb65141669b7f76c3013de34b5e6d384ec7e58b
Artem Inzhyyants <artem.inzhyyants@gmail.com>
2024-11-06 16:55:25 +0100

    Amazon Ads: fmt
    Signed-off-by: Artem Inzhyyants <artem.inzhyyants@gmail.com>

--- airbyte-integrations/connectors/source-amazon-ads/source_amazon_ads/source.py
+++ airbyte-integrations/connectors/source-amazon-ads/source_amazon_ads/source.py
@@ -134,7 +134,7 @@ class SourceAmazonAds(AbstractSource):
         ]
         portfolios_stream = Portfolios(**stream_args)
         return [
-            profiles_stream,
+            Profiles(**stream_args),
             portfolios_stream,
             *[stream_class(**stream_args) for stream_class in non_profile_stream_classes],
         ]
