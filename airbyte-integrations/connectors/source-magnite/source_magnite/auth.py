8457c21132dc2f53b6067d0d49a08d338979231d
ekladiousm <mira.ekladious@taico.io>
2024-11-07 17:29:06 +0100

    fix auth import

--- airbyte-integrations/connectors/source-magnite/source_magnite/auth.py
+++ airbyte-integrations/connectors/source-magnite/source_magnite/auth.py
@@ -7,7 +7,7 @@ from typing import Any, Mapping
 import logging
 
 # from airbyte_cdk.sources.streams.http.requests_native_auth import TokenAuthenticator
-from airbyte_cdk.sources.streams.http.auth import HttpAuthenticator
+from airbyte_cdk.sources.streams.http.auth.core import HttpAuthenticator
 
 # class MissingAccessTokenError(Exception):
 #     """
