1025083d501fc5cddf1bdbba368f40a4e4b2a7fa
Johnny Schmidt <john.schmidt@airbyte.io>
2024-11-08 15:15:32 -0800

    Bulk Load CDK: Mapper cleanup: Mappers are now stateless (#48421)

--- airbyte-cdk/bulk/toolkits/load-parquet/src/main/kotlin/io/airbyte/cdk/load/data/parquet/ParquetMapperPipelineFactory.kt
+++ airbyte-cdk/bulk/toolkits/load-parquet/src/main/kotlin/io/airbyte/cdk/load/data/parquet/ParquetMapperPipelineFactory.kt
@@ -10,8 +10,8 @@ import io.airbyte.cdk.load.data.AirbyteValueNoopMapper
 import io.airbyte.cdk.load.data.MapperPipeline
 import io.airbyte.cdk.load.data.MapperPipelineFactory
 import io.airbyte.cdk.load.data.MergeUnions
-import io.airbyte.cdk.load.data.SchemalessTypesToJson
-import io.airbyte.cdk.load.data.SchemalessValuesToJson
+import io.airbyte.cdk.load.data.SchemalessTypesToJsonString
+import io.airbyte.cdk.load.data.SchemalessValuesToJsonString
 import io.airbyte.cdk.load.data.TimeStringToInteger
 import io.airbyte.cdk.load.data.UnionTypeToDisjointRecord
 import io.airbyte.cdk.load.data.UnionValueToDisjointRecord
@@ -21,7 +21,7 @@ class ParquetMapperPipelineFactory : MapperPipelineFactory {
         MapperPipeline(
             stream.schema,
             listOf(
-                SchemalessTypesToJson() to SchemalessValuesToJson(),
+                SchemalessTypesToJsonString() to SchemalessValuesToJsonString(),
                 AirbyteSchemaNoopMapper() to TimeStringToInteger(),
                 MergeUnions() to AirbyteValueNoopMapper(),
                 UnionTypeToDisjointRecord() to UnionValueToDisjointRecord(),
