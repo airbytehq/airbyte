e08fb2553fd348096bfe6e8a268833c216b26299
Edward Gao <edward.gao@airbyte.io>
2024-11-13 14:00:10 -0800

    copy over legacy DATs + enable remaining tests

--- airbyte-integrations/connectors/destination-s3-v2/src/test-integration-legacy/kotlin/io/airbyte/integrations/destination/s3/S3V2DestinationTestUtils.kt
+++ airbyte-integrations/connectors/destination-s3-v2/src/test-integration-legacy/kotlin/io/airbyte/integrations/destination/s3/S3V2DestinationTestUtils.kt
@@ -1,7 +1,6 @@
 /*
  * Copyright (c) 2024 Airbyte, Inc., all rights reserved.
  */
-
 package io.airbyte.integrations.destination.s3
 
 import com.fasterxml.jackson.databind.JsonNode
@@ -10,9 +9,16 @@ import io.airbyte.commons.json.Jsons.deserialize
 import java.nio.file.Path
 
 object S3V2DestinationTestUtils {
+    private const val ACCESS_KEY_CONFIG_SECRET_PATH =
+        "secrets/s3_dest_min_required_permissions_creds.json"
     private const val ASSUME_ROLE_CONFIG_SECRET_PATH = "secrets/s3_dest_assume_role_config.json"
     private const val ASSUME_ROLE_INTERNAL_CREDENTIALS_SECRET_PATH =
         "secrets/s3_dest_iam_role_credentials_for_assume_role_auth.json"
+    private const val POLICY_MANAGER_CREDENTIALS_SECRET_PATH =
+        "secrets/s3_dest_policy_manager_credentials.json"
+
+    val baseConfigJsonFilePath: JsonNode
+        get() = deserialize(readFile(Path.of(ACCESS_KEY_CONFIG_SECRET_PATH)))
 
     val assumeRoleConfig: JsonNode
         get() = deserialize(readFile(Path.of(ASSUME_ROLE_CONFIG_SECRET_PATH)))
@@ -27,4 +33,7 @@ object S3V2DestinationTestUtils {
 
     val assumeRoleInternalCredentials: Map<String, String>
         get() = getCredentials(ASSUME_ROLE_INTERNAL_CREDENTIALS_SECRET_PATH)
+    
+    val policyManagerCredentials: Map<String, String>
+        get() = getCredentials(POLICY_MANAGER_CREDENTIALS_SECRET_PATH)
 }
