0061f855cf00efbe9a296b4977e42da1b7413d76
Ryan Br... <ryan.broughan@gmail.com>
2024-11-15 15:39:06 -0800

    chore: rbroughan/disk back pressure no deadlock prevention (#48505)

--- airbyte-cdk/bulk/core/load/src/main/kotlin/io/airbyte/cdk/load/config/SyncBeanFactory.kt
+++ airbyte-cdk/bulk/core/load/src/main/kotlin/io/airbyte/cdk/load/config/SyncBeanFactory.kt
@@ -5,19 +5,30 @@
 package io.airbyte.cdk.load.config
 
 import io.airbyte.cdk.load.command.DestinationConfiguration
-import io.airbyte.cdk.load.state.MemoryManager
+import io.airbyte.cdk.load.state.ReservationManager
 import io.micronaut.context.annotation.Factory
+import io.micronaut.context.annotation.Value
+import jakarta.inject.Named
 import jakarta.inject.Singleton
 
 /** Factory for instantiating beans necessary for the sync process. */
 @Factory
 class SyncBeanFactory {
     @Singleton
+    @Named("memoryManager")
     fun memoryManager(
         config: DestinationConfiguration,
-    ): MemoryManager {
+    ): ReservationManager {
         val memory = config.maxMessageQueueMemoryUsageRatio * Runtime.getRuntime().maxMemory()
 
-        return MemoryManager(memory.toLong())
+        return ReservationManager(memory.toLong())
+    }
+
+    @Singleton
+    @Named("diskManager")
+    fun diskManager(
+        @Value("\${airbyte.resources.disk.bytes}") availableBytes: Long,
+    ): ReservationManager {
+        return ReservationManager(availableBytes)
     }
 }
