faed1db18c7ac7f58c4575296f80a184cc3b306e
Edward Gao <edward.gao@airbyte.io>
2024-11-18 14:13:40 -0800

    add rough check test sketch

---
+++ airbyte-integrations/connectors/destination-iceberg-v2/src/test-integration/kotlin/io/airbyte/integrations/destination/iceberg_v2/IcebergV2TestUtil.kt
@@ -0,0 +1,10 @@
+package io.airbyte.integrations.destination.iceberg_v2
+
+import java.nio.file.Files
+import java.nio.file.Path
+
+object IcebergV2TestUtil {
+    // TODO this is just here as an example, we should remove it + add real configs
+    const val SOME_RANDOM_S3_CONFIG = "secrets/s3_dest_v2_minimal_required_config.json"
+    fun getConfig(configPath: String): String = Files.readString(Path.of(configPath))
+}
