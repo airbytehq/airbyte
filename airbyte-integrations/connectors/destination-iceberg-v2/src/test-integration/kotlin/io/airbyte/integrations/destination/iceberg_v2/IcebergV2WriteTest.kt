bb1e7b14a9b23f039c30f249a8ae2feb3fc40b8e
Edward Gao <edward.gao@airbyte.io>
2024-11-18 15:31:12 -0800

    rough write test

---
+++ airbyte-integrations/connectors/destination-iceberg-v2/src/test-integration/kotlin/io/airbyte/integrations/destination/iceberg_v2/IcebergV2WriteTest.kt
@@ -0,0 +1,30 @@
+package io.airbyte.integrations.destination.iceberg_v2
+
+import io.airbyte.cdk.load.test.util.FakeDataDumper
+import io.airbyte.cdk.load.test.util.NoopDestinationCleaner
+import io.airbyte.cdk.load.test.util.NoopExpectedRecordMapper
+import io.airbyte.cdk.load.write.BasicFunctionalityIntegrationTest
+import io.airbyte.cdk.load.write.StronglyTyped
+import org.junit.jupiter.api.Disabled
+import org.junit.jupiter.api.Test
+
+abstract class IcebergV2WriteTest(path: String) :
+    BasicFunctionalityIntegrationTest(
+        IcebergV2TestUtil.getConfig(path),
+        IcebergV2Specification::class.java,
+        FakeDataDumper,
+        NoopDestinationCleaner,
+        NoopExpectedRecordMapper,
+        // TODO let's validate these - I'm making some assumptions about how iceberg works
+        isStreamSchemaRetroactive = true,
+        supportsDedup = false,
+        stringifySchemalessObjects = true,
+        promoteUnionToObject = true,
+        preserveUndeclaredFields = false,
+        commitDataIncrementally = false,
+        allTypesBehavior = StronglyTyped(),
+    )
+
+// TODO replace this with a real test class for an actual config
+@Disabled("nowhere even close to functional")
+class FakeIcebergWriteTest : IcebergV2WriteTest(IcebergV2TestUtil.SOME_RANDOM_S3_CONFIG)
---
+++ airbyte-integrations/connectors/destination-iceberg-v2/src/test-integration/kotlin/io/airbyte/integrations/destination/iceberg_v2/IcebergV2Writer.kt
@@ -0,0 +1,25 @@
+package io.airbyte.integrations.destination.iceberg_v2
+
+import io.airbyte.cdk.load.command.DestinationStream
+import io.airbyte.cdk.load.message.Batch
+import io.airbyte.cdk.load.message.DestinationRecord
+import io.airbyte.cdk.load.message.SimpleBatch
+import io.airbyte.cdk.load.write.DestinationWriter
+import io.airbyte.cdk.load.write.StreamLoader
+import javax.inject.Singleton
+
+@Singleton
+class IcebergV2Writer : DestinationWriter {
+    override fun createStreamLoader(stream: DestinationStream): StreamLoader {
+        // TODO instantiate an actual IcebergStreamLoader
+        return object: StreamLoader {
+            override val stream = stream
+
+            override suspend fun processRecords(
+                records: Iterator<DestinationRecord>,
+                totalSizeBytes: Long
+            ) = SimpleBatch(state = Batch.State.COMPLETE)
+
+        }
+    }
+}
