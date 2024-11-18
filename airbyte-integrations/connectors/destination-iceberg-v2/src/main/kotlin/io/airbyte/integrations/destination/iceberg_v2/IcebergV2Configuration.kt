dd77ee045357729f5885efcf7af8b4475d01b5f9
Edward Gao <edward.gao@airbyte.io>
2024-11-18 14:13:40 -0800

    add rough check test sketch

---
+++ airbyte-integrations/connectors/destination-iceberg-v2/src/main/kotlin/io/airbyte/integrations/destination/iceberg_v2/IcebergV2Configuration.kt
@@ -0,0 +1,20 @@
+package io.airbyte.integrations.destination.iceberg_v2
+
+import io.airbyte.cdk.load.command.DestinationConfiguration
+import io.airbyte.cdk.load.command.DestinationConfigurationFactory
+import javax.inject.Singleton
+
+// TODO put real fields here
+data class IcebergV2Configuration(val something: String) : DestinationConfiguration()
+
+@Singleton
+class IcebergV2ConfigurationFactory :
+    DestinationConfigurationFactory<IcebergV2Specification, IcebergV2Configuration> {
+    override fun makeWithoutExceptionHandling(
+        pojo: IcebergV2Specification
+    ): IcebergV2Configuration {
+        // TODO convert from the jackson-friendly IcebergV2Specification
+        //   to the programmer-friendly IcebergV2Configuration
+        return IcebergV2Configuration("hello world")
+    }
+}
---
+++ airbyte-integrations/connectors/destination-iceberg-v2/src/test-integration/kotlin/io/airbyte/integrations/destination/iceberg_v2/IcebergV2CheckTest.kt
@@ -0,0 +1,13 @@
+package io.airbyte.integrations.destination.iceberg_v2
+
+import io.airbyte.cdk.load.check.CheckIntegrationTest
+import io.airbyte.cdk.load.check.CheckTestConfig
+import java.nio.file.Path
+
+class IcebergV2CheckTest: CheckIntegrationTest<IcebergV2Specification>(
+    successConfigFilenames = listOf(
+        CheckTestConfig(Path.of(IcebergV2TestUtil.SOME_RANDOM_S3_CONFIG))
+    ),
+    // TODO we maybe should add some configs that are expected to fail `check`
+    failConfigFilenamesAndFailureReasons = mapOf(),
+)
