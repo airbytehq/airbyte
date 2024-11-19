2bf9d1ca19ed583327057965f4749c7c72119a67
subodhchaturvedi <subodh1810@gmail.com>
2024-11-20 01:42:15 +0530

    fix test

--- airbyte-integrations/connectors/destination-iceberg-v2/src/test-integration/kotlin/io/airbyte/integrations/destination/iceberg.v2/IcebergV2TestUtil.kt
+++ airbyte-integrations/connectors/destination-iceberg-v2/src/test-integration/kotlin/io/airbyte/integrations/destination/iceberg.v2/IcebergV2TestUtil.kt
@@ -9,6 +9,10 @@ import java.nio.file.Path
 
 object IcebergV2TestUtil {
     // TODO this is just here as an example, we should remove it + add real configs
-    const val SOME_RANDOM_S3_CONFIG = "secrets/s3_dest_v2_minimal_required_config.json"
+    private val resource =
+        this::class.java.classLoader.getResource("iceberg_dest_v2_minimal_required_config.json")
+            ?: throw IllegalArgumentException("File not found in resources")
+    val PATH: Path = Path.of(resource.toURI())
+
     fun getConfig(configPath: String): String = Files.readString(Path.of(configPath))
 }
