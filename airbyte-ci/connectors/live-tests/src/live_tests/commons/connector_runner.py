7845b6b690c724199e3bdbc59752f1eb3981c2bf
Augustin <augustin@airbyte.io>
2024-11-22 15:35:04 +0100

    cat/live-test: give ownership of copied objects to the image user (#48616)

--- airbyte-ci/connectors/live-tests/src/live_tests/commons/connector_runner.py
+++ airbyte-ci/connectors/live-tests/src/live_tests/commons/connector_runner.py
@@ -118,6 +118,8 @@ class ConnectorRunner:
         self,
     ) -> ExecutionResult:
         container = self._connector_under_test_container
+        current_user = (await container.with_exec(["whoami"]).stdout()).strip()
+        container = container.with_user(current_user)
         container = container.with_exec(["mkdir", "-p", self.DATA_DIR])
         # Do not cache downstream dagger layers
         container = container.with_env_variable("CACHEBUSTER", str(uuid.uuid4()))
@@ -132,13 +134,14 @@ class ConnectorRunner:
         for env_var_name, env_var_value in self.environment_variables.items():
             container = container.with_env_variable(env_var_name, env_var_value)
         if self.config:
-            container = container.with_new_file(self.IN_CONTAINER_CONFIG_PATH, contents=json.dumps(dict(self.config)))
+            container = container.with_new_file(self.IN_CONTAINER_CONFIG_PATH, contents=json.dumps(dict(self.config)), owner=current_user)
         if self.state:
-            container = container.with_new_file(self.IN_CONTAINER_STATE_PATH, contents=json.dumps(self.state))
+            container = container.with_new_file(self.IN_CONTAINER_STATE_PATH, contents=json.dumps(self.state), owner=current_user)
         if self.configured_catalog:
             container = container.with_new_file(
                 self.IN_CONTAINER_CONFIGURED_CATALOG_PATH,
                 contents=self.configured_catalog.json(),
+                owner=current_user,
             )
         if self.http_proxy:
             container = await self.http_proxy.bind_container(container)
