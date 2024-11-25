721f95aec0e9a281654590088fbd64d8ae8b5b2f
benmoriceau <benoit@airbyte.io>
2024-11-25 15:22:10 -0800

    Remove the mock integration test

--- airbyte-cdk/bulk/core/load/src/integrationTest/kotlin/io/airbyte/cdk/load/mock_integration_test/MockBasicFunctionalityIntegrationTest.kt
+++ airbyte-cdk/bulk/core/load/src/integrationTest/kotlin/io/airbyte/cdk/load/mock_integration_test/MockBasicFunctionalityIntegrationTest.kt
@@ -9,6 +9,7 @@ import io.airbyte.cdk.load.test.util.NoopExpectedRecordMapper
 import io.airbyte.cdk.load.test.util.NoopNameMapper
 import io.airbyte.cdk.load.write.BasicFunctionalityIntegrationTest
 import io.airbyte.cdk.load.write.Untyped
+import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 
 class MockBasicFunctionalityIntegrationTest :
@@ -90,5 +91,10 @@ class MockBasicFunctionalityIntegrationTest :
     @Test
     override fun testBasicTypes() {
         super.testBasicTypes()
+    }
+
+    @Test
+    @Disabled
+    override fun testBasicWriteFile() {
     }
 }
