07721171a2153b6273366d838391be2b08ca3e35
Johnny Schmidt <john.schmidt@airbyte.io>
2024-11-27 10:30:24 -0800

    merge s3v2 into s3v1

--- airbyte-integrations/connectors/destination-s3/src/main/kotlin/S3V2Checker.kt
+++ airbyte-integrations/connectors/destination-s3/src/main/kotlin/S3V2Checker.kt
@@ -2,7 +2,7 @@
  * Copyright (c) 2024 Airbyte, Inc., all rights reserved.
  */
 
-package io.airbyte.integrations.destination.s3_v2
+package io.airbyte.integrations.destination.s3
 
 import io.airbyte.cdk.load.check.DestinationChecker
 import io.airbyte.cdk.load.file.TimeProvider
