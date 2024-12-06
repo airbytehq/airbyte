399d532e823130585a6c8e48ecc753a809bd67aa
benmoriceau <benoit@airbyte.io>
2024-12-06 11:13:02 -0800

    Good format

--- airbyte-cdk/java/airbyte-cdk/s3-destinations/src/main/kotlin/io/airbyte/cdk/integrations/destination/s3/csv/CsvSerializedBuffer.kt
+++ airbyte-cdk/java/airbyte-cdk/s3-destinations/src/main/kotlin/io/airbyte/cdk/integrations/destination/s3/csv/CsvSerializedBuffer.kt
@@ -36,7 +36,7 @@ class CsvSerializedBuffer(
     private var csvFormat: CSVFormat
 
     init {
-        csvFormat = CSVFormat.DEFAULT.withQuoteMode(QuoteMode.NONE)
+        csvFormat = CSVFormat.Builder.create().setQuoteMode(QuoteMode.NONE).build()
         // we always want to compress csv files
         withCompression(compression)
     }
