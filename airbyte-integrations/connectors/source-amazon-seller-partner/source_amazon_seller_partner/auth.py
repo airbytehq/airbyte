049800f34e3e462175fdfb81c1da10853a29e1d5
Keith Thompson <keithjoethompson@gmail.com>
2024-12-09 14:09:26 +0000

    add 5 minutes of buffer to amazon seller partner auth

--- airbyte-integrations/connectors/source-amazon-seller-partner/source_amazon_seller_partner/auth.py
+++ airbyte-integrations/connectors/source-amazon-seller-partner/source_amazon_seller_partner/auth.py
@@ -2,12 +2,14 @@
 # Copyright (c) 2023 Airbyte, Inc., all rights reserved.
 #
 
-
-from typing import Any, Mapping
-
+import logging
+import backoff
 import pendulum
+from typing import Any, Mapping, Tuple
 from airbyte_cdk.sources.streams.http.auth import Oauth2Authenticator
+from airbyte_cdk.sources.streams.http.exceptions import DefaultBackoffException
 
+logger = logging.getLogger("airbyte")
 
 class AWSAuthenticator(Oauth2Authenticator):
     def __init__(self, host: str, *args, **kwargs):
@@ -22,3 +24,19 @@ class AWSAuthenticator(Oauth2Authenticator):
             "x-amz-access-token": self.get_access_token(),
             "x-amz-date": pendulum.now("utc").strftime("%Y%m%dT%H%M%SZ"),
         }
+
+    @backoff.on_exception(
+        backoff.expo,
+        DefaultBackoffException,
+        on_backoff=lambda details: logger.info(
+            f"Caught retryable error after {details['tries']} tries. Waiting {details['wait']} seconds then retrying..."
+        ),
+        max_time=300,
+    )
+    def refresh_access_token(self) -> Tuple[str, int]:
+        """
+        returns a tuple of (access_token, token_lifespan_in_seconds). 
+        Add 5 minute buffer to access token refresh. 
+        """
+        access_token, expires_in = super().refresh_access_token()
+        return access_token, (expires_in - 300)
