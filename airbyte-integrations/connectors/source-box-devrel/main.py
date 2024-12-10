fccc8061e70aacff079ef0393a5d779a99e6f531
Barduino <barduinor@gmail.com>
2024-12-10 18:18:49 +0000

    initial project setup

--- airbyte-integrations/connectors/source-box-devrel/main.py
+++ airbyte-integrations/connectors/source-box-devrel/main.py
@@ -2,7 +2,7 @@
 # Copyright (c) 2023 Airbyte, Inc., all rights reserved.
 #
 
-from source_google_drive.run import run
+from source_box_devrel.run import run
 
 if __name__ == "__main__":
     run()
