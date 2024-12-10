6ddd18e96f7e7349f5e082a9d8e859ca3cfb13b1
ekladiousm <mira.ekladious@taico.io>
2024-12-10 11:17:59 +0100

    add hardcoded fields

--- airbyte-integrations/connectors/source-magnite/source_magnite/utils.py
+++ airbyte-integrations/connectors/source-magnite/source_magnite/utils.py
@@ -25,3 +25,39 @@ def string_to_date(d: str, f: str = DATE_FORMAT, old_format=None) -> datetime.da
 
 def date_to_string(d: datetime.date, f: str = DATE_FORMAT) -> str:
     return d.strftime(f)
+
+def get_dimensions(source):
+    dimensions_dict = {
+        "adstats-publisher": ["channel", "dealCode", "supplySeat", "brand", "publisher", "platform",
+                              "supplyType", "parentAdvertiser", "supplyDomain", "adSourceEndDate"],
+        #                       ["orderId", "year", "parentAdvertiser", "channel", "inventoryLabel",
+        #                       "supplySeat", "supply", "industryCode", "adSourceId","adSourceMinDuration",
+        #                       "industryId", "appBundleId", "season", "dealCode", "supplyDomain", "brand",
+        #                       "adSourceEndDate", "adSourceLabel", "supplyType", "publisher", "platform",
+        #                       "platformFamily", "buyerSeatName"],
+
+        "deals-publisher": ["nameInAppStore", "parentAdvertiser", "channel", "supplySeat", "holdingCompany",
+                            "supply", "dealDateCreated", "adSourceStatus", "appBundleId", "dealCode",
+                            "supplyDomain", "day", "brand", "buyerSeatCode", "adSource", "dealStatus",
+                            "dealType", "publisher", "mktStartDate", "supplySeatId", "currency", "dealOrigin",
+                            "advertiser", "buyerName", "buyerCode", "pacingType", "dealName",
+                            "calculatedMinDuration", "advertiserDomain", "mktEndDate"]
+    }
+    return dimensions_dict[source]
+
+def get_metrics(source):
+    metrics_dict = {
+        "adstats-publisher": ["sspNetRevenue", "adsrvFee", "ctr", "operatorCpm", "netCpm", "netProgCpm", "grossProgCpm",
+                              "netRevenue", "grossRevenue", "sspGrossRevenue"],
+                            # ["sspNetRevenue", "adsrvFee", "testImpressions", "ctr", "useRate", "avgCompletionRate",
+                            #   "impsRemaining", "tagUseRate", "discountGrossRevenue", "grossTagRevenue", "requests",
+                            #   "operatorCpm", "sspGrossRevenue", "grossProgCpmInclDemandFee", "fee", "fillRate",
+                            #   "completionRate", "impressionRate", "netCpm", "netProgCpm", "grossProgCpm",
+                            #   "tagFillRate", "netRevenue", "completions", "grossRevenue", "netTagRevenue", "tagFee"],
+
+        "deals-publisher": ["ctr", "netRevenue", "avgFloorPricesPlusDemandFee", "firstQuartiles", "firstQuartilesRate",
+                            "bidsLoss", "completions", "impressions", "operatorCost", "grossRevenue", "validBids",
+                            "netCpm", "clicks", "secondQuartilesRate", "revenueInclDemandFee", "eligibleBids", 
+                            "demandAcquisitionCost"]
+    }
+    return metrics_dict[source]
