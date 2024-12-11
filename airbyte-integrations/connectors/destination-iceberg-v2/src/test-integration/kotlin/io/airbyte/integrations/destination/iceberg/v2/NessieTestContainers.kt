481b805b8dd12b71527d7f62c4a68a77b018d200
Francis Genet <francis.genet@airbyte.io>
2024-12-11 10:57:39 -0800

    Try something else

--- airbyte-integrations/connectors/destination-iceberg-v2/src/test-integration/kotlin/io/airbyte/integrations/destination/iceberg/v2/NessieTestContainers.kt
+++ airbyte-integrations/connectors/destination-iceberg-v2/src/test-integration/kotlin/io/airbyte/integrations/destination/iceberg/v2/NessieTestContainers.kt
@@ -5,23 +5,22 @@
 package io.airbyte.integrations.destination.iceberg.v2
 
 import io.airbyte.cdk.load.util.setOnce
-import io.github.oshai.kotlinlogging.KotlinLogging
 import java.io.File
 import java.util.concurrent.atomic.AtomicBoolean
-import org.testcontainers.containers.DockerComposeContainer
-
-private val logger = KotlinLogging.logger {}
+import kotlin.test.DefaultAsserter.fail
+import org.projectnessie.minio.MinioContainer
+import org.testcontainers.containers.ComposeContainer
 
 /**
  * Shared test containers for all nessie tests, so that we don't launch redundant docker containers
  */
 object NessieTestContainers {
-    val testcontainers =
-        DockerComposeContainer(File("src/test-integration/resources/nessie/docker-compose.yml"))
-            .withExposedService("nessie", 19120)
-            .withExposedService("minio", 9000)
-            .withExposedService("keycloak", 8080)
+    val testcontainers: ComposeContainer =
+        ComposeContainer(File("src/test-integration/resources/nessie/docker-compose.yml"))
             .withLocalCompose(true)
+//            .withExposedService("nessie", 19120)
+//            .withExposedService("minio", 9000)
+//            .withExposedService("keycloak", 8080)
     private val startRunOnce = AtomicBoolean(false)
 
     /**
@@ -30,16 +29,16 @@ object NessieTestContainers {
      */
     fun start() {
         if (startRunOnce.setOnce()) {
-            testcontainers.start()
-            //            val minio = MinioContainer()
-            //                .withEnv("MINIO_ROOT_USER", "inioadmin")
-            //                .withEnv("MINIO_ROOT_PASSWORD", "inioadmin")
-            //                .withEnv("MINIO_ADDRESS", ":9000")
-            //                .withEnv("MINIO_CONSOLE_ADDRESS", ":9090")
-            //                .withExposedPorts(9000)
-            //            minio.start()
-            //            val minioPort = minio.getMappedPort(9000)
-            //            fail("Started minio. Port is $minioPort")
+//            testcontainers.start()
+            val minio = MinioContainer()
+                .withEnv("MINIO_ROOT_USER", "inioadmin")
+                .withEnv("MINIO_ROOT_PASSWORD", "inioadmin")
+                .withEnv("MINIO_ADDRESS", ":9000")
+                .withEnv("MINIO_CONSOLE_ADDRESS", ":9090")
+                .withExposedPorts(9000)
+            minio.start()
+            val minioPort = minio.getMappedPort(9000)
+            fail("Started minio. Port is $minioPort")
         } else {
             // afaict there's no method to wait for the containers to start
             // so just poll until these methods stop throwing exceptions
