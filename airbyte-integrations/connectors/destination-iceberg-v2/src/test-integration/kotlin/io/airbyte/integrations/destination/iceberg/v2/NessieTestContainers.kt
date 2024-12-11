9864f6a55a6d439d22960eff87d14f8d4ed66f82
Francis Genet <francis.genet@airbyte.io>
2024-12-11 11:27:23 -0800

    Back to that?

--- airbyte-integrations/connectors/destination-iceberg-v2/src/test-integration/kotlin/io/airbyte/integrations/destination/iceberg/v2/NessieTestContainers.kt
+++ airbyte-integrations/connectors/destination-iceberg-v2/src/test-integration/kotlin/io/airbyte/integrations/destination/iceberg/v2/NessieTestContainers.kt
@@ -5,20 +5,20 @@
 package io.airbyte.integrations.destination.iceberg.v2
 
 import io.airbyte.cdk.load.util.setOnce
-import java.io.File
 import java.util.concurrent.atomic.AtomicBoolean
-import org.testcontainers.containers.ComposeContainer
+import kotlin.test.DefaultAsserter.fail
+import org.projectnessie.minio.MinioContainer
 
 /**
  * Shared test containers for all nessie tests, so that we don't launch redundant docker containers
  */
 object NessieTestContainers {
-    val testcontainers: ComposeContainer =
-        ComposeContainer(File("src/test-integration/resources/nessie/docker-compose.yml"))
-            .withLocalCompose(true)
-    //            .withExposedService("nessie", 19120)
-    //            .withExposedService("minio", 9000)
-    //            .withExposedService("keycloak", 8080)
+    //    val testcontainers: ComposeContainer =
+    //        ComposeContainer(File("src/test-integration/resources/nessie/docker-compose.yml"))
+    //            .withLocalCompose(true)
+    //    //            .withExposedService("nessie", 19120)
+    //    //            .withExposedService("minio", 9000)
+    //    //            .withExposedService("keycloak", 8080)
     private val startRunOnce = AtomicBoolean(false)
 
     /**
@@ -27,30 +27,32 @@ object NessieTestContainers {
      */
     fun start() {
         if (startRunOnce.setOnce()) {
-            testcontainers.start()
-            //            val minio = MinioContainer()
-            //                .withEnv("MINIO_ROOT_USER", "inioadmin")
-            //                .withEnv("MINIO_ROOT_PASSWORD", "inioadmin")
-            //                .withEnv("MINIO_ADDRESS", ":9000")
-            //                .withEnv("MINIO_CONSOLE_ADDRESS", ":9090")
-            //                .withExposedPorts(9000)
-            //            minio.start()
-            //            val minioPort = minio.getMappedPort(9000)
-            //            fail("Started minio. Port is $minioPort")
-        } else {
-            // afaict there's no method to wait for the containers to start
-            // so just poll until these methods stop throwing exceptions
-            while (true) {
-                try {
-                    testcontainers.getServicePort("nessie", 19120)
-                    testcontainers.getServicePort("minio", 9000)
-                    testcontainers.getServicePort("keycloak", 8080)
-                } catch (e: IllegalStateException) {
-                    // do nothing
-                }
-                break
-            }
+            //            testcontainers.start()
+            val minio =
+                MinioContainer()
+                    .withEnv("MINIO_ROOT_USER", "inioadmin")
+                    .withEnv("MINIO_ROOT_PASSWORD", "inioadmin")
+                    .withEnv("MINIO_ADDRESS", ":9000")
+                    .withEnv("MINIO_CONSOLE_ADDRESS", ":9090")
+                    .withExposedPorts(9000)
+            minio.start()
+            val minioPort = minio.getMappedPort(9000)
+            fail("Started minio. Port is $minioPort")
         }
+        //        } else {
+        //            // afaict there's no method to wait for the containers to start
+        //            // so just poll until these methods stop throwing exceptions
+        //            while (true) {
+        //                try {
+        //                    testcontainers.getServicePort("nessie", 19120)
+        //                    testcontainers.getServicePort("minio", 9000)
+        //                    testcontainers.getServicePort("keycloak", 8080)
+        //                } catch (e: IllegalStateException) {
+        //                    // do nothing
+        //                }
+        //                break
+        //            }
+        //        }
     }
 
     // intentionally no stop method - testcontainers automatically stop when their parent java
