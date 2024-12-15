47cbc60dcb814fedd29932bc9e9894c1cd7135fd
Yarden Ofir <76483336+yardenoff@users.noreply.github.com>
2024-12-15 10:32:10 +0200

    pulse microsoft entra id source (#1)
    Co-authored-by: moriluz <mor@pulse-security.io>

---
+++ airbyte-integrations/connectors/source-pulse-microsoft-entra-id/main.py
@@ -0,0 +1,8 @@
+#
+# Copyright (c) 2024 Airbyte, Inc., all rights reserved.
+#
+
+from source_pulse_microsoft_entra_id.run import run
+
+if __name__ == "__main__":
+    run()
---
+++ airbyte-integrations/connectors/source-pulse-microsoft-entra-id/source_pulse_microsoft_entra_id/__init__.py
@@ -0,0 +1,8 @@
+#
+# Copyright (c) 2024 Airbyte, Inc., all rights reserved.
+#
+
+
+from .source import SourcePulseMicrosoftEntraId
+
+__all__ = ["SourcePulseMicrosoftEntraId"]
