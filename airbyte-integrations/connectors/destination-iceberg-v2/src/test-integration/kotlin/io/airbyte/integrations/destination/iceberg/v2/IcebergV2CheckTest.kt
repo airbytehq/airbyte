17447fcb39245c97e50df2db3dfa7b8910292677
Edward Gao <edward.gao@airbyte.io>
2024-12-16 12:57:58 -0800

    add to check test + reenable

--- airbyte-integrations/connectors/destination-iceberg-v2/src/test-integration/kotlin/io/airbyte/integrations/destination/iceberg/v2/IcebergV2CheckTest.kt
+++ airbyte-integrations/connectors/destination-iceberg-v2/src/test-integration/kotlin/io/airbyte/integrations/destination/iceberg/v2/IcebergV2CheckTest.kt
@@ -7,16 +7,13 @@ package io.airbyte.integrations.destination.iceberg.v2
 import io.airbyte.cdk.load.check.CheckIntegrationTest
 import io.airbyte.cdk.load.check.CheckTestConfig
 import io.airbyte.integrations.destination.iceberg.v2.IcebergV2TestUtil.GLUE_CONFIG_PATH
-import io.airbyte.integrations.destination.iceberg.v2.IcebergV2TestUtil.MINIMAL_CONFIG_PATH
-import org.junit.jupiter.api.Disabled
 
-@Disabled
 class IcebergV2CheckTest :
     CheckIntegrationTest<IcebergV2Specification>(
-        successConfigFilenames = listOf(
-            CheckTestConfig(MINIMAL_CONFIG_PATH),
-            CheckTestConfig(GLUE_CONFIG_PATH),
-        ),
+        successConfigFilenames =
+            listOf(
+                CheckTestConfig(GLUE_CONFIG_PATH),
+            ),
         // TODO we maybe should add some configs that are expected to fail `check`
         failConfigFilenamesAndFailureReasons = mapOf(),
     )
--- airbyte-integrations/connectors/destination-iceberg-v2/src/test-integration/kotlin/io/airbyte/integrations/destination/iceberg/v2/IcebergV2TestUtil.kt
+++ airbyte-integrations/connectors/destination-iceberg-v2/src/test-integration/kotlin/io/airbyte/integrations/destination/iceberg/v2/IcebergV2TestUtil.kt
@@ -7,14 +7,10 @@ package io.airbyte.integrations.destination.iceberg.v2
 import io.airbyte.cdk.command.ConfigurationSpecification
 import io.airbyte.cdk.command.ValidatedJsonUtils
 import io.airbyte.integrations.destination.iceberg.v2.io.IcebergUtil
-import java.net.URI
 import java.nio.file.Files
 import java.nio.file.Path
 
 object IcebergV2TestUtil {
-    // TODO this is just here as an example, we should remove it + add real configs
-    val MINIMAL_CONFIG_PATH: Path =
-        Path.of(getResourceUri("iceberg_dest_v2_minimal_required_config.json"))
     val GLUE_CONFIG_PATH: Path = Path.of("secrets/glue.json")
 
     fun parseConfig(path: Path) =
@@ -30,8 +26,4 @@ object IcebergV2TestUtil {
             val props = icebergUtil.toCatalogProperties(config)
             icebergUtil.createCatalog(DEFAULT_CATALOG_NAME, props)
         }
-
-    private fun getResourceUri(path: String): URI =
-        this::class.java.classLoader.getResource(path)?.toURI()
-            ?: throw IllegalArgumentException("File not found in resources")
 }
