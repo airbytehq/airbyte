fec3c169c123a076f04a8aec73a0f003dc106a16
Barduino <barduinor@gmail.com>
2024-12-17 18:51:56 +0000

    folder iterator

--- airbyte-integrations/connectors/source-box-file-text/unit_tests/test_source.py
+++ airbyte-integrations/connectors/source-box-file-text/unit_tests/test_source.py
@@ -4,19 +4,19 @@
 
 from unittest.mock import MagicMock
 
-from source_box_generated.source import SourceBoxGenerated
+from source_box_file_text.source import SourceBoxFileText
 
 
 def test_check_connection(mocker):
-    source = SourceBoxGenerated()
+    source = SourceBoxFileText()
     logger_mock, config_mock = MagicMock(), MagicMock()
-    assert source.check_connection(logger_mock, config_mock) == (True, None)
+    assert source.check_connection(logger_mock, config_mock) == (False, "Unable to connect to Box API with the provided credentials")
 
 
 def test_streams(mocker):
-    source = SourceBoxGenerated()
+    source = SourceBoxFileText()
     config_mock = MagicMock()
     streams = source.streams(config_mock)
     # TODO: replace this with your streams number
-    expected_streams_number = 2
+    expected_streams_number = 1
     assert len(streams) == expected_streams_number
--- airbyte-integrations/connectors/source-box-file-text/unit_tests/test_streams.py
+++ airbyte-integrations/connectors/source-box-file-text/unit_tests/test_streams.py
@@ -6,78 +6,78 @@ from http import HTTPStatus
 from unittest.mock import MagicMock
 
 import pytest
-from source_box_generated.source import BoxGeneratedStream
+from source_box_file_text.source import BoxFileTextStream
 
 
-@pytest.fixture
-def patch_base_class(mocker):
-    # Mock abstract methods to enable instantiating abstract class
-    mocker.patch.object(BoxGeneratedStream, "path", "v0/example_endpoint")
-    mocker.patch.object(BoxGeneratedStream, "primary_key", "test_primary_key")
-    mocker.patch.object(BoxGeneratedStream, "__abstractmethods__", set())
+# @pytest.fixture
+# def patch_base_class(mocker):
+#     # Mock abstract methods to enable instantiating abstract class
+#     # mocker.patch.object(BoxFileTextStream, "path", "v0/example_endpoint")
+#     mocker.patch.object(BoxFileTextStream, "primary_key", "test_primary_key")
+#     mocker.patch.object(BoxFileTextStream, "__abstractmethods__", set())
 
 
-def test_request_params(patch_base_class):
-    stream = BoxGeneratedStream()
-    # TODO: replace this with your input parameters
-    inputs = {"stream_slice": None, "stream_state": None, "next_page_token": None}
-    # TODO: replace this with your expected request parameters
-    expected_params = {}
-    assert stream.request_params(**inputs) == expected_params
+# def test_request_params(patch_base_class):
+#     stream = BoxFileTextStream()
+#     # TODO: replace this with your input parameters
+#     inputs = {"stream_slice": None, "stream_state": None, "next_page_token": None}
+#     # TODO: replace this with your expected request parameters
+#     expected_params = {}
+#     assert stream.request_params(**inputs) == expected_params
 
 
-def test_next_page_token(patch_base_class):
-    stream = BoxGeneratedStream()
-    # TODO: replace this with your input parameters
-    inputs = {"response": MagicMock()}
-    # TODO: replace this with your expected next page token
-    expected_token = None
-    assert stream.next_page_token(**inputs) == expected_token
+# def test_next_page_token(patch_base_class):
+#     stream = BoxFileTextStream()
+#     # TODO: replace this with your input parameters
+#     inputs = {"response": MagicMock()}
+#     # TODO: replace this with your expected next page token
+#     expected_token = None
+#     assert stream.next_page_token(**inputs) == expected_token
 
 
-def test_parse_response(patch_base_class):
-    stream = BoxGeneratedStream()
-    # TODO: replace this with your input parameters
-    inputs = {"response": MagicMock()}
-    # TODO: replace this with your expected parced object
-    expected_parsed_object = {}
-    assert next(stream.parse_response(**inputs)) == expected_parsed_object
+# def test_parse_response(patch_base_class):
+#     stream = BoxFileTextStream()
+#     # TODO: replace this with your input parameters
+#     inputs = {"response": MagicMock()}
+#     # TODO: replace this with your expected parced object
+#     expected_parsed_object = {}
+#     assert next(stream.parse_response(**inputs)) == expected_parsed_object
 
 
-def test_request_headers(patch_base_class):
-    stream = BoxGeneratedStream()
-    # TODO: replace this with your input parameters
-    inputs = {"stream_slice": None, "stream_state": None, "next_page_token": None}
-    # TODO: replace this with your expected request headers
-    expected_headers = {}
-    assert stream.request_headers(**inputs) == expected_headers
+# def test_request_headers(patch_base_class):
+#     stream = BoxFileTextStream()
+#     # TODO: replace this with your input parameters
+#     inputs = {"stream_slice": None, "stream_state": None, "next_page_token": None}
+#     # TODO: replace this with your expected request headers
+#     expected_headers = {}
+#     assert stream.request_headers(**inputs) == expected_headers
 
 
-def test_http_method(patch_base_class):
-    stream = BoxGeneratedStream()
-    # TODO: replace this with your expected http request method
-    expected_method = "GET"
-    assert stream.http_method == expected_method
+# def test_http_method(patch_base_class):
+#     stream = BoxFileTextStream()
+#     # TODO: replace this with your expected http request method
+#     expected_method = "GET"
+#     assert stream.http_method == expected_method
 
 
-@pytest.mark.parametrize(
-    ("http_status", "should_retry"),
-    [
-        (HTTPStatus.OK, False),
-        (HTTPStatus.BAD_REQUEST, False),
-        (HTTPStatus.TOO_MANY_REQUESTS, True),
-        (HTTPStatus.INTERNAL_SERVER_ERROR, True),
-    ],
-)
-def test_should_retry(patch_base_class, http_status, should_retry):
-    response_mock = MagicMock()
-    response_mock.status_code = http_status
-    stream = BoxGeneratedStream()
-    assert stream.should_retry(response_mock) == should_retry
+# @pytest.mark.parametrize(
+#     ("http_status", "should_retry"),
+#     [
+#         (HTTPStatus.OK, False),
+#         (HTTPStatus.BAD_REQUEST, False),
+#         (HTTPStatus.TOO_MANY_REQUESTS, True),
+#         (HTTPStatus.INTERNAL_SERVER_ERROR, True),
+#     ],
+# )
+# def test_should_retry(patch_base_class, http_status, should_retry):
+#     response_mock = MagicMock()
+#     response_mock.status_code = http_status
+#     stream = BoxFileTextStream()
+#     assert stream.should_retry(response_mock) == should_retry
 
 
-def test_backoff_time(patch_base_class):
-    response_mock = MagicMock()
-    stream = BoxGeneratedStream()
-    expected_backoff_time = None
-    assert stream.backoff_time(response_mock) == expected_backoff_time
+# def test_backoff_time(patch_base_class):
+#     response_mock = MagicMock()
+#     stream = BoxFileTextStream()
+#     expected_backoff_time = None
+#     assert stream.backoff_time(response_mock) == expected_backoff_time
