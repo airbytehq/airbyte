3af90912578376be5051d5633f68da5c69cee3ee
Edward Gao <edward.gao@airbyte.io>
2024-12-17 11:57:45 -0800

    use table cleaner

--- airbyte-integrations/connectors/destination-iceberg-v2/src/test-integration/kotlin/io/airbyte/integrations/destination/iceberg/v2/IcebergDestinationCleaner.kt
+++ airbyte-integrations/connectors/destination-iceberg-v2/src/test-integration/kotlin/io/airbyte/integrations/destination/iceberg/v2/IcebergDestinationCleaner.kt
@@ -7,6 +7,8 @@ package io.airbyte.integrations.destination.iceberg.v2
 import io.airbyte.cdk.load.test.util.DestinationCleaner
 import io.airbyte.cdk.load.test.util.IntegrationTest.Companion.isNamespaceOld
 import io.airbyte.cdk.load.test.util.IntegrationTest.Companion.randomizedNamespaceRegex
+import io.airbyte.integrations.destination.iceberg.v2.io.IcebergTableCleaner
+import io.airbyte.integrations.destination.iceberg.v2.io.IcebergUtil
 import org.apache.iceberg.catalog.Catalog
 import org.apache.iceberg.catalog.Namespace
 import org.apache.iceberg.catalog.SupportsNamespaces
@@ -18,9 +20,14 @@ class IcebergDestinationCleaner(private val catalog: Catalog) : DestinationClean
                 val namespace = it.level(0)
                 randomizedNamespaceRegex.matches(namespace) && isNamespaceOld(namespace)
             }
+
+        // we're passing explicit TableIdentifier to clearTable, so just use SimpleTableIdGenerator
+        val tableCleaner = IcebergTableCleaner(IcebergUtil(SimpleTableIdGenerator()))
+
         namespaces.forEach { namespace ->
-            catalog.listTables(namespace).forEach { table ->
-                catalog.dropTable(table, /* purge = */ true)
+            catalog.listTables(namespace).forEach { tableId ->
+                val table = catalog.loadTable(tableId)
+                tableCleaner.clearTable(catalog, tableId, table.io(), table.location())
             }
             catalog.dropNamespace(namespace)
         }
