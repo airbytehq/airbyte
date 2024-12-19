dac34e88b4028aeb59b78955eea576b445291263
Jonathan Pearlin <jpearlin1@gmail.com>
2024-12-19 15:59:49 -0500

    Test data source configuration

---
+++ airbyte-integrations/connectors/destination-mssql-v2/src/test/kotlin/io/airbyte/integrations/destination/mssql/v2/config/DataSourceFactoryTest.kt
@@ -0,0 +1,18 @@
+package io.airbyte.integrations.destination.mssql.v2.config
+
+import org.junit.jupiter.api.Test
+
+internal class DataSourceFactoryTest {
+
+    @Test
+    fun test() {
+        val factory = DataSourceFactory()
+        val dataSource = factory.dataSource()
+
+        dataSource.connection.use { connection ->
+            connection.createStatement().use { statement ->
+                statement.execute("SELECT * FROM Inventory")
+            }
+        }
+    }
+}
