3057cc0b150a6ad45da30cc6e2a734e303ea6d4d
Marius Posta <marius@airbyte.io>
2024-12-20 09:44:54 -0500

    source-mysql: clean up config (#49950)

--- airbyte-integrations/connectors/source-mysql/src/test/kotlin/io/airbyte/integrations/source/mysql/MySqlSourceConfigurationSpecificationTest.kt
+++ airbyte-integrations/connectors/source-mysql/src/test/kotlin/io/airbyte/integrations/source/mysql/MySqlSourceConfigurationSpecificationTest.kt
@@ -14,6 +14,7 @@ import org.junit.jupiter.api.Test
 
 @MicronautTest(environments = [Environment.TEST], rebuildContext = true)
 class MySqlSourceConfigurationSpecificationTest {
+
     @Inject
     lateinit var supplier: ConfigurationSpecificationSupplier<MySqlSourceConfigurationSpecification>
 
