fff4535588a78c81641366cec082dc63a42624aa
Edward Gao <edward.gao@airbyte.io>
2024-12-20 15:24:43 -0800

    fix avro+parquet

--- airbyte-integrations/connectors/destination-s3-v2/src/test-integration/kotlin/io/airbyte/integrations/destination/s3_v2/S3V2WriteTest.kt
+++ airbyte-integrations/connectors/destination-s3-v2/src/test-integration/kotlin/io/airbyte/integrations/destination/s3_v2/S3V2WriteTest.kt
@@ -6,6 +6,7 @@ package io.airbyte.integrations.destination.s3_v2
 
 import io.airbyte.cdk.load.test.util.ExpectedRecordMapper
 import io.airbyte.cdk.load.test.util.NoopDestinationCleaner
+import io.airbyte.cdk.load.test.util.NoopExpectedRecordMapper
 import io.airbyte.cdk.load.test.util.UncoercedExpectedRecordMapper
 import io.airbyte.cdk.load.write.AllTypesBehavior
 import io.airbyte.cdk.load.write.BasicFunctionalityIntegrationTest
@@ -147,7 +148,7 @@ class S3V2WriteTestCsvGzip :
 class S3V2WriteTestAvroUncompressed :
     S3V2WriteTest(
         S3V2TestUtils.AVRO_UNCOMPRESSED_CONFIG_PATH,
-        TODO(),
+        NoopExpectedRecordMapper,
         stringifySchemalessObjects = true,
         promoteUnionToObject = false,
         preserveUndeclaredFields = false,
@@ -159,7 +160,7 @@ class S3V2WriteTestAvroUncompressed :
 class S3V2WriteTestAvroBzip2 :
     S3V2WriteTest(
         S3V2TestUtils.AVRO_BZIP2_CONFIG_PATH,
-        TODO(),
+        NoopExpectedRecordMapper,
         stringifySchemalessObjects = true,
         promoteUnionToObject = false,
         preserveUndeclaredFields = false,
@@ -171,7 +172,7 @@ class S3V2WriteTestAvroBzip2 :
 class S3V2WriteTestParquetUncompressed :
     S3V2WriteTest(
         S3V2TestUtils.PARQUET_UNCOMPRESSED_CONFIG_PATH,
-        TODO(),
+        NoopExpectedRecordMapper,
         stringifySchemalessObjects = true,
         promoteUnionToObject = true,
         preserveUndeclaredFields = false,
@@ -183,7 +184,7 @@ class S3V2WriteTestParquetUncompressed :
 class S3V2WriteTestParquetSnappy :
     S3V2WriteTest(
         S3V2TestUtils.PARQUET_SNAPPY_CONFIG_PATH,
-        TODO(),
+        NoopExpectedRecordMapper,
         stringifySchemalessObjects = true,
         promoteUnionToObject = true,
         preserveUndeclaredFields = false,
@@ -195,7 +196,8 @@ class S3V2WriteTestParquetSnappy :
 class S3V2WriteTestEndpointURL :
     S3V2WriteTest(
         S3V2TestUtils.ENDPOINT_URL_CONFIG_PATH,
-        TODO(),
+        // this test is writing to CSV
+        UncoercedExpectedRecordMapper,
         stringifySchemalessObjects = false,
         promoteUnionToObject = false,
         preserveUndeclaredFields = false,
