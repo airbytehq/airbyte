069474639da37b4f78593825bf5e1170ea0d27b9
Johnny Schmidt <john.schmidt@airbyte.io>
2024-12-23 17:29:57 -0800

    Destination S3V2: Guard against name conflicts.

--- airbyte-cdk/bulk/toolkits/load-object-storage/src/main/kotlin/io/airbyte/cdk/load/write/object_storage/ObjectStorageStreamLoaderFactory.kt
+++ airbyte-cdk/bulk/toolkits/load-object-storage/src/main/kotlin/io/airbyte/cdk/load/write/object_storage/ObjectStorageStreamLoaderFactory.kt
@@ -94,7 +94,7 @@ class ObjectStorageStreamLoader<T : RemoteObject<*>, U : OutputStream>(
             fileSizeBytes = fileSizeBytes,
             stream,
             fileNumber
-        )
+        ) { name -> destinationStateManager.getState(stream).ensureUnique(name) }
     }
 
     override suspend fun processFile(file: DestinationFile): Batch {
