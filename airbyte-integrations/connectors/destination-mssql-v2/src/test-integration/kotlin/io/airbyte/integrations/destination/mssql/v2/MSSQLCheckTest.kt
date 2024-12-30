e0cdf1c74c5bbb6886e87f9a8a3ad5b26e6cbb7d
Jimmy Ma <gosusnp@users.noreply.github.com>
2024-12-30 10:28:02 -0800

    chore(destination-mssql-v2): add datasource url tests (#50426)
    Co-authored-by: Jonathan Pearlin <jpearlin1@gmail.com>

--- airbyte-integrations/connectors/destination-mssql-v2/src/test-integration/kotlin/io/airbyte/integrations/destination/mssql/v2/MSSQLCheckTest.kt
+++ airbyte-integrations/connectors/destination-mssql-v2/src/test-integration/kotlin/io/airbyte/integrations/destination/mssql/v2/MSSQLCheckTest.kt
@@ -1,13 +1,25 @@
+/*
+ * Copyright (c) 2024 Airbyte, Inc., all rights reserved.
+ */
+
 package io.airbyte.integrations.destination.mssql.v2
 
 import io.airbyte.cdk.load.check.CheckIntegrationTest
 import io.airbyte.cdk.load.check.CheckTestConfig
 import io.airbyte.integrations.destination.mssql.v2.config.MSSQLSpecification
+import java.util.regex.Pattern
 
-class MSSQLCheckTest : CheckIntegrationTest<MSSQLSpecification>(
-    successConfigFilenames = listOf(
-        CheckTestConfig(MSSQLTestConfigUtil.getConfigPath("check/valid.json")),
-    ),
-    failConfigFilenamesAndFailureReasons = emptyMap()
-) {
-}
+class MSSQLCheckTest :
+    CheckIntegrationTest<MSSQLSpecification>(
+        successConfigFilenames =
+            listOf(
+                CheckTestConfig(MSSQLTestConfigUtil.getConfigPath("check/valid.json")),
+                CheckTestConfig(MSSQLTestConfigUtil.getConfigPath("check/valid-ssl-trust.json")),
+            ),
+        failConfigFilenamesAndFailureReasons =
+            mapOf(
+                CheckTestConfig(
+                    MSSQLTestConfigUtil.getConfigPath("check/fail-internal-schema-invalid.json")
+                ) to Pattern.compile("\"iamnotthere\" either does not exist"),
+            )
+    ) {}
