f92097feba216e9649b37b6e53db942d3023a58f
Jimmy Ma <gosusnp@users.noreply.github.com>
2024-12-30 10:28:02 -0800

    chore(destination-mssql-v2): add datasource url tests (#50426)
    Co-authored-by: Jonathan Pearlin <jpearlin1@gmail.com>

--- airbyte-integrations/connectors/destination-mssql-v2/src/test-integration/kotlin/io/airbyte/integrations/destination/mssql/v2/MSSQLTestConfigUtil.kt
+++ airbyte-integrations/connectors/destination-mssql-v2/src/test-integration/kotlin/io/airbyte/integrations/destination/mssql/v2/MSSQLTestConfigUtil.kt
@@ -1,8 +1,15 @@
+/*
+ * Copyright (c) 2024 Airbyte, Inc., all rights reserved.
+ */
+
 package io.airbyte.integrations.destination.mssql.v2
 
 import java.nio.file.Path
 
 object MSSQLTestConfigUtil {
     fun getConfigPath(relativePath: String): Path =
-        Path.of(this::class.java.classLoader.getResource(relativePath)?.toURI() ?: throw IllegalArgumentException("Resource $relativePath could not be found"))
+        Path.of(
+            this::class.java.classLoader.getResource(relativePath)?.toURI()
+                ?: throw IllegalArgumentException("Resource $relativePath could not be found")
+        )
 }
