1395213f55974a8bc51f0285636701245a640b33
Nisarg Patel <nisarg@magnify.io>
2025-01-02 09:57:12 -0800

    totango-adapter. this commits includes a new custom source adapter for totango. functionalities supported: authentication with oauth flow ingestion of accounts ingestion of tasks. tasks is a sub-stream. which means it requires the account id for ingestion. Hence, it will always run once account ingestion is completed

--- airbyte-integrations/connectors/source-totango/source_totango/source.py
+++ airbyte-integrations/connectors/source-totango/source_totango/source.py
@@ -5,109 +5,264 @@
 
 from abc import ABC
 from typing import Any, Iterable, List, Mapping, MutableMapping, Optional, Tuple
-
+import time
+import json
+import logging
 import requests
+from airbyte_cdk.models import SyncMode
 from airbyte_cdk.sources import AbstractSource
 from airbyte_cdk.sources.streams import Stream
-from airbyte_cdk.sources.streams.http import HttpStream
-from airbyte_cdk.sources.streams.http.auth import TokenAuthenticator
-
-"""
-TODO: Most comments in this class are instructive and should be deleted after the source is implemented.
-
-This file provides a stubbed example of how to use the Airbyte CDK to develop both a source connector which supports full refresh or and an
-incremental syncs from an HTTP API.
-
-The various TODOs are both implementation hints and steps - fulfilling all the TODOs should be sufficient to implement one basic and one incremental
-stream from a source. This pattern is the same one used by Airbyte internally to implement connectors.
-
-The approach here is not authoritative, and devs are free to use their own judgement.
-
-There are additional required TODOs in the files within the integration_tests folder and the spec.yaml file.
-"""
+from airbyte_cdk.sources.streams.http import HttpStream, HttpSubStream
+from airbyte_cdk.sources.streams.http.requests_native_auth import Oauth2Authenticator
 
 
-# Basic full refresh stream
 class TotangoStream(HttpStream, ABC):
+    # Base URL for the Totango API
+    url_base = "https://api-gw-us.totango.com"
+
+class TotangoAuthenticator(Oauth2Authenticator):
     """
-    TODO remove this comment
-
-    This class represents a stream output by the connector.
-    This is an abstract base class meant to contain all the common functionality at the API level e.g: the API base URL, pagination strategy,
-    parsing responses etc..
-
-    Each stream should extend this class (or another abstract subclass of it) to specify behavior unique to that stream.
-
-    Typically for REST APIs each stream corresponds to a resource in the API. For example if the API
-    contains the endpoints
-        - GET v1/customers
-        - GET v1/employees
-
-    then you should have three classes:
-    `class TotangoStream(HttpStream, ABC)` which is the current class
-    `class Customers(TotangoStream)` contains behavior to pull data for customers using v1/customers
-    `class Employees(TotangoStream)` contains behavior to pull data for employees using v1/employees
-
-    If some streams implement incremental sync, it is typical to create another class
-    `class IncrementalTotangoStream((TotangoStream), ABC)` then have concrete stream implementations extend it. An example
-    is provided below.
-
-    See the reference docs for the full list of configurable options.
+    Custom Authenticator that handles token validation, refresh, and rotation.
     """
+    url_base = "https://api-gw-us.totango.com"
+    token_url = f"{url_base}/oauth/token"
 
-    # TODO: Fill in the url base. Required.
-    url_base = "https://example-api.com/v1/"
+    def __init__(self, config: Mapping[str, Any]):
+        if not config.get("client_id") or not config.get("client_secret"):
+            raise ValueError("Client ID or secret is missing in the configuration.")
+
+        super().__init__(
+            token_refresh_endpoint=self.token_url,
+            client_id=config["client_id"],
+            client_secret=config["client_secret"],
+            refresh_token=config["refresh_token"],
+        )
+        self.config = config
+        self.logger = logging.getLogger("TotangoAuthenticator")
+
+    def is_token_expired(self) -> bool:
+        """
+        Check if the access token is expired based on the current time and expiry time.
+
+        TODO: can check if the access token is getting expired within 3 hours, refresh the token
+        """
+        current_time =  int(time.time() * 1000)
+        access_token_expiry = self.config.get("access_token_expiry", 0)
+        expired = current_time >= access_token_expiry
+        self.logger.info(f"Checking if token is expired. Current time: {current_time}, Expiry: {access_token_expiry}, Expired: {expired}")
+        return expired
+
+    def refresh_access_token(self) -> str:
+        """
+        Refresh the access token and update the config with the new token and expiry times.
+        """
+        headers = {"Content-Type": "application/x-www-form-urlencoded"}
+        data = {
+            "client_id": self.config["client_id"],
+            "client_secret": self.config["client_secret"],
+            "refresh_token": self.config["refresh_token"],
+            "grant_type": "refresh_token",
+        }
+
+        response = requests.post(self.token_url, headers=headers, data=data)
+
+        if response.status_code == 200:
+            token_data = response.json()
+            self.config["access_token"] = token_data["access_token"]
+            self.config["access_token_expiry"] = token_data["access_token_expiration"]
+            self.config["refresh_token"] = token_data["refresh_token"]
+            self.config["refresh_token_expiry"] = token_data["refresh_token_expiration"]
+            self.logger.info("Access token and refresh token updated successfully.")
+            return self.config["access_token"]
+        else:
+            self.logger.error(f"Failed to refresh access token: {response.text}")
+            response.raise_for_status()
+
+    def get_access_token(self) -> str:
+        """
+        Get the current valid access token or refresh it if expired.
+        """
+        if self.is_token_expired():
+            self.logger.info("Access token expired. Refreshing token...")
+            return self.refresh_access_token()
+        self.logger.info("Using existing access token.")
+        return self.config["access_token"]
+    
+    def request_headers(self, **kwargs) -> Mapping[str, Any]:
+        """
+        Returns the headers required for making requests to the Totango API.
+        """
+        return {
+            "Content-Type": "application/json",
+            "Authorization": f"Bearer {self.get_access_token()}"
+        }
+
+class Account(TotangoStream):
+
+    # Primary key of the stream
+    primary_key = "account_id"
+    record_count = 1000
+
+    @property
+    def use_cache(self):
+        return True
+
+    @property
+    def cache_filename(self):
+        return "accounts.yml"
+
+    def __init__(self, authenticator: TotangoAuthenticator):
+        super().__init__(authenticator=authenticator)  # Pass authenticator to the parent class
+
+    @property
+    def http_method(self) -> str:
+        """
+        Returns the HTTP method to use for requests.
+        """
+        return "POST"
+
+    def path(self, stream_state=None, stream_slice=None, next_page_token=None) -> str:
+        """Returns the endpoint path for this stream."""
+        return "/api/v1/search/accounts"
+
+    def get_fields(self):
+        """
+        Returns the fields for the API response.
+        """
+        schema = self.get_json_schema()
+        fields = []
+
+        for field_name, field_details in schema["properties"].items():
+            field = {
+                "type": field_details["source_attribute"],
+            }
+            if "additionalProps" in field_details:
+                field.update(field_details["additionalProps"])
+                field["field_display_name"] = field_details["attribute"]
+                fields.append(field)
+                continue
+
+            field["attribute"] = field_details["attribute"]
+            fields.append(field)
+        return fields
+
+    def request_body_json(self, stream_state=None, stream_slice=None, next_page_token=None) ->  MutableMapping[str, Any]:
+        """Constructs the form-encoded body for the POST request."""
+        query = {
+            "terms": [],
+            "count": self.record_count,
+            "offset": next_page_token.get("offset", 0) if next_page_token else 0,
+            "fields": self.get_fields(),
+            "sort_by": "display_name",
+            "sort_order": "ASC",
+            "scope": "all",
+        }
+        data = {
+            "query": json.dumps(query)
+        }
+
+        return data
+
+    def request_headers(self, **kwargs) -> Mapping[str, Any]:
+        """Returns the headers required for making requests."""
+        headers = super().request_headers(**kwargs)  # Inherit default headers from HttpStream
+        headers["Content-Type"] = "application/json"  # Add custom header
+        return headers
 
     def next_page_token(self, response: requests.Response) -> Optional[Mapping[str, Any]]:
-        """
-        TODO: Override this method to define a pagination strategy. If you will not be using pagination, no action is required - just return None.
+        """Handles pagination based on the API response."""
+        total_hits = response.json().get("response", {}).get("stats", {}).get("total_hits", 0)
+        current_offset = response.json().get("response", {}).get("accounts", {}).get("offset", 0)
+        count = self.record_count
 
-        This method should return a Mapping (e.g: dict) containing whatever information required to make paginated requests. This dict is passed
-        to most other methods in this class to help you form headers, request bodies, query params, etc..
-
-        For example, if the API accepts a 'page' parameter to determine which page of the result to return, and a response from the API contains a
-        'page' number, then this method should probably return a dict {'page': response.json()['page'] + 1} to increment the page count by 1.
-        The request_params method should then read the input next_page_token and set the 'page' param to next_page_token['page'].
-
-        :param response: the most recent response from the API
-        :return If there is another page in the result, a mapping (e.g: dict) containing information needed to query the next page in the response.
-                If there are no more pages in the result, return None.
-        """
+        if current_offset + count < total_hits:
+            return {"offset": current_offset + count}
         return None
-
-    def request_params(
-        self, stream_state: Mapping[str, Any], stream_slice: Mapping[str, any] = None, next_page_token: Mapping[str, Any] = None
-    ) -> MutableMapping[str, Any]:
-        """
-        TODO: Override this method to define any query parameters to be set. Remove this method if you don't need to define request params.
-        Usually contains common params e.g. pagination size etc.
-        """
-        return {}
 
     def parse_response(self, response: requests.Response, **kwargs) -> Iterable[Mapping]:
         """
-        TODO: Override this method to define how a response is parsed.
-        :return an iterable containing each record in the response
+        Parses the API response to yield individual records.
         """
-        yield {}
+        accounts = response.json().get("response", {}).get("accounts", {}).get("hits", [])
+        fields = self.get_fields()
+        for account in accounts:
+            selected_fields = account.get("selected_fields", [])
+            yield_dict = {}
+            account_id = account.get('name')
+            yield_dict["account_id"] = account_id
 
+            yield_dict["account_name"] = account.get("display_name")
+            for field, value in zip(fields, selected_fields):
+                # had to skip account_name and account_id as they are already added to the yield_dict
+                if field.get("attribute") == "account_name" or field.get("attribute") == "account_id":
+                    continue
+                if "attribute" in field:
+                    yield_dict[field.get("attribute")] = value
+                else:
+                    yield_dict[field.get("field_display_name")] = value
 
-class Customers(TotangoStream):
-    """
-    TODO: Change class name to match the table/data source this stream corresponds to.
-    """
+            yield yield_dict
 
-    # TODO: Fill in the primary key. Required. This is usually a unique field in the stream, like an ID or a timestamp.
-    primary_key = "customer_id"
+class Tasks(HttpSubStream, TotangoStream):
+    primary_key = "id"
+    record_count = 1000
 
-    def path(
-        self, stream_state: Mapping[str, Any] = None, stream_slice: Mapping[str, Any] = None, next_page_token: Mapping[str, Any] = None
-    ) -> str:
+    def __init__(self, parent: Account, **kwargs):
+        super().__init__(parent=parent, **kwargs)
+
+    @property
+    def http_method(self) -> str:
+        """Returns the HTTP method to use for requests."""
+        return "GET"
+
+    @property
+    def cache_filename(self):
+        return "tasks.yml"
+    
+    @property
+    def use_cache(self):
+        return True
+
+    @property
+    def name(self):
+        return "tasks"
+
+    def path(self, stream_state=None, stream_slice=None, next_page_token=None) -> str:
+        """Returns the endpoint path for this stream."""
+        account_id = stream_slice["account_id"]
+        return f"/api/v2/events?account_id={account_id}"
+
+    def stream_slices(self, sync_mode: SyncMode, cursor_field: List[str] = None, stream_state: Mapping[str, Any] = None) -> Iterable[Optional[Mapping[str, Any]]]:
         """
-        TODO: Override this method to define the path this stream corresponds to. E.g. if the url is https://example-api.com/v1/customers then this
-        should return "customers". Required.
+        Returns the stream slices for this stream.
         """
-        return "customers"
+        parent_stream_slices = self.parent.stream_slices(
+            sync_mode=SyncMode.full_refresh,
+            cursor_field=cursor_field,
+            stream_state=stream_state
+        )
+        for stream_slice in parent_stream_slices:
+            parent_records = self.parent.read_records(
+                sync_mode=SyncMode.full_refresh,
+                cursor_field=cursor_field,
+                stream_slice=stream_slice,
+                stream_state=stream_state
+            )
+            for record in parent_records:
+                yield {"account_id": record.get("account_id")}
+    
+    def parse_response(self, response: requests.Response, **kwargs) -> Iterable[Mapping]:
+        """
+        Parses the API response to yield individual records.
+        """
+        response_json = response.json()
+
+        for task in response_json:
+            yield task
+    
+    def next_page_token(self, response: requests.Response) -> Optional[Mapping[str, Any]]:
+        """Handles pagination based on the API response."""
+        return None
 
 
 # Basic incremental stream
@@ -139,68 +294,39 @@ class IncrementalTotangoStream(TotangoStream, ABC):
         return {}
 
 
-class Employees(IncrementalTotangoStream):
-    """
-    TODO: Change class name to match the table/data source this stream corresponds to.
-    """
-
-    # TODO: Fill in the cursor_field. Required.
-    cursor_field = "start_date"
-
-    # TODO: Fill in the primary key. Required. This is usually a unique field in the stream, like an ID or a timestamp.
-    primary_key = "employee_id"
-
-    def path(self, **kwargs) -> str:
-        """
-        TODO: Override this method to define the path this stream corresponds to. E.g. if the url is https://example-api.com/v1/employees then this should
-        return "single". Required.
-        """
-        return "employees"
-
-    def stream_slices(self, stream_state: Mapping[str, Any] = None, **kwargs) -> Iterable[Optional[Mapping[str, any]]]:
-        """
-        TODO: Optionally override this method to define this stream's slices. If slicing is not needed, delete this method.
-
-        Slices control when state is saved. Specifically, state is saved after a slice has been fully read.
-        This is useful if the API offers reads by groups or filters, and can be paired with the state object to make reads efficient. See the "concepts"
-        section of the docs for more information.
-
-        The function is called before reading any records in a stream. It returns an Iterable of dicts, each containing the
-        necessary data to craft a request for a slice. The stream state is usually referenced to determine what slices need to be created.
-        This means that data in a slice is usually closely related to a stream's cursor_field and stream_state.
-
-        An HTTP request is made for each returned slice. The same slice can be accessed in the path, request_params and request_header functions to help
-        craft that specific request.
-
-        For example, if https://example-api.com/v1/employees offers a date query params that returns data for that particular day, one way to implement
-        this would be to consult the stream state object for the last synced date, then return a slice containing each date from the last synced date
-        till now. The request_params function would then grab the date from the stream_slice and make it part of the request by injecting it into
-        the date query param.
-        """
-        raise NotImplementedError("Implement stream slices or delete this method!")
-
-
-# Source
 class SourceTotango(AbstractSource):
+    logger = logging.getLogger("SourceTotango")
+    url_base = "https://api-gw-us.totango.com"
+
     def check_connection(self, logger, config) -> Tuple[bool, any]:
         """
-        TODO: Implement a connection check to validate that the user-provided config can be used to connect to the underlying API
-
-        See https://github.com/airbytehq/airbyte/blob/master/airbyte-integrations/connectors/source-stripe/source_stripe/source.py#L232
-        for an example.
-
-        :param config:  the user-input config object conforming to the connector's spec.yaml
-        :param logger:  logger object
-        :return Tuple[bool, any]: (True, None) if the input config can be used to connect to the API successfully, (False, error) otherwise.
+        Check connection and handle token rotation.
         """
-        return True, None
+        self.logger.info("Checking connection...")
+        authenticator = TotangoAuthenticator(config)
+
+        try:
+            access_token = authenticator.get_access_token()
+            self.logger.info(f"Access token used for connection check: {access_token}")
+
+            url = f"{self.url_base}/api/v1/users/whoami"
+            response = requests.get(url, headers=authenticator.request_headers())
+
+            self.logger.info(f"Connection check response: {response.text}")
+
+            if response.status_code == 200:
+                return True, None
+            else:
+                return False, f"Failed to connect to API: {response.text}"
+        except Exception as e:
+            self.logger.error(f"Error during connection check: {str(e)}")
+            return False, str(e)
 
     def streams(self, config: Mapping[str, Any]) -> List[Stream]:
         """
-        TODO: Replace the streams below with your own streams.
-
-        :param config: A Mapping of the user input configuration as defined in the connector spec.
+        Define streams for this source.
         """
-        # TODO remove the authenticator if not required.
-        auth = TokenAuthenticator(token="api_key")  # Oauth2Authenticator is also available if you need oauth support
-        return [Customers(authenticator=auth), Employees(authenticator=auth)]
+        authenticator = TotangoAuthenticator(config)
+
+        return [Account(authenticator=authenticator), Tasks(parent=Account(authenticator=authenticator), authenticator=authenticator)]
+
