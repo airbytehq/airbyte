069a0bc31f70d92017d9a52f39f06646b79f7e4c
Jonathan Pearlin <jonathan@airbyte.io>
2025-01-03 14:20:57 -0500

    feat: use mssql server test container in integration tests (#50869)

--- airbyte-integrations/connectors/destination-mssql-v2/src/test-integration/kotlin/io/airbyte/integrations/destination/mssql/v2/MSSQLCheckTest.kt
+++ airbyte-integrations/connectors/destination-mssql-v2/src/test-integration/kotlin/io/airbyte/integrations/destination/mssql/v2/MSSQLCheckTest.kt
@@ -7,8 +7,12 @@ package io.airbyte.integrations.destination.mssql.v2
 import io.airbyte.cdk.load.check.CheckIntegrationTest
 import io.airbyte.cdk.load.check.CheckTestConfig
 import io.airbyte.integrations.destination.mssql.v2.config.MSSQLSpecification
+import org.junit.jupiter.api.AfterAll
+import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.TestInstance
 
-class MSSQLCheckTest :
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+internal class MSSQLCheckTest :
     CheckIntegrationTest<MSSQLSpecification>(
         successConfigFilenames =
             listOf(
@@ -20,5 +24,17 @@ class MSSQLCheckTest :
                 CheckTestConfig(
                     MSSQLTestConfigUtil.getConfigPath("check/fail-internal-schema-invalid.json")
                 ) to "\"iamnotthere\" either does not exist".toPattern(),
-            )
-    ) {}
+            ),
+        configUpdater = MSSQLContainerHelper
+    ) {
+
+    @BeforeAll
+    fun beforeAll() {
+        MSSQLContainerHelper.start()
+    }
+
+    @AfterAll
+    fun afterAll() {
+        MSSQLContainerHelper.stop()
+    }
+}
