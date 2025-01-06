f75332784547341043f049a146c5412e8a8861f1
Artem Inzhyyants <artem.inzhyyants@gmail.com>
2025-01-06 15:42:54 +0100

    Amazon SP: add decoders & type transformers [skip ci]
    Signed-off-by: Artem Inzhyyants <artem.inzhyyants@gmail.com>

--- airbyte-integrations/connectors/source-amazon-seller-partner/source_amazon_seller_partner/components/decoder.py
+++ airbyte-integrations/connectors/source-amazon-seller-partner/source_amazon_seller_partner/components/decoder.py
@@ -8,8 +8,9 @@ from dataclasses import InitVar, dataclass
 from io import StringIO
 from typing import Any, Generator, Mapping, MutableMapping
 
-import orjson
 import requests
+import xmltodict
+
 from airbyte_cdk.sources.declarative.decoders.decoder import Decoder
 
 logger = logging.getLogger("airbyte")
@@ -33,3 +34,89 @@ class GzipCsvDecoder(Decoder):
             document = response.content.decode("iso-8859-1")
 
         yield from csv.DictReader(StringIO(document), delimiter="\t")
+
+
+@dataclass
+class GzipXmlDecoder(Decoder):
+    """
+    Decoder strategy that returns the json-encoded content of a response, if any.
+    """
+
+    parameters: InitVar[Mapping[str, Any]]
+
+    def is_stream_response(self) -> bool:
+        return False
+
+    def decode(self, response: requests.Response) -> Generator[MutableMapping[str, Any], None, None]:
+        try:
+            document = gzip.decompress(response.content).decode("iso-8859-1")
+        except gzip.BadGzipFile:
+            document = response.content.decode("iso-8859-1")
+
+        try:
+            parsed = xmltodict.parse(document, attr_prefix="", cdata_key="value", force_list={"Message"})
+        except Exception as e:
+            logger.warning(f"Unable to parse the report for the stream {self.name}, error: {str(e)}")
+            return []
+
+        reports = parsed.get("AmazonEnvelope", {}).get("Message", {})
+        for report in reports:
+            yield report.get("OrderReport", {})
+
+
+@dataclass
+class SellerFeedbackReportsGzipCsvDecoder(Decoder):
+    parameters: InitVar[Mapping[str, Any]]
+    NORMALIZED_FIELD_NAMES = ["date", "rating", "comments", "response", "order_id", "rater_email"]
+
+    def is_stream_response(self) -> bool:
+        return False
+
+    def decode(self, response: requests.Response) -> Generator[MutableMapping[str, Any], None, None]:
+        # csv header field names for this report differ per marketplace (are localized to marketplace language)
+        # but columns come in the same order, so we set fieldnames to our custom ones
+        # and raise error if original and custom header field count does not match
+        try:
+            document = gzip.decompress(response.content).decode("iso-8859-1")
+        except gzip.BadGzipFile:
+            document = response.content.decode("iso-8859-1")
+
+        reader = csv.DictReader(StringIO(document), delimiter="\t", fieldnames=self.NORMALIZED_FIELD_NAMES)
+        original_fieldnames = next(reader)
+        if len(original_fieldnames) != len(self.NORMALIZED_FIELD_NAMES):
+            raise ValueError("Original and normalized header field count does not match")
+
+        yield from reader
+
+
+@dataclass
+class GetXmlBrowseTreeDataDecoder(Decoder):
+    parameters: InitVar[Mapping[str, Any]]
+    NORMALIZED_FIELD_NAMES = ["date", "rating", "comments", "response", "order_id", "rater_email"]
+
+
+    def is_stream_response(self) -> bool:
+        return False
+
+    def decode(self, response: requests.Response) -> Generator[MutableMapping[str, Any], None, None]:
+        # csv header field names for this report differ per marketplace (are localized to marketplace language)
+        # but columns come in the same order, so we set fieldnames to our custom ones
+        # and raise error if original and custom header field count does not match
+        try:
+            document = gzip.decompress(response.content).decode("iso-8859-1")
+        except gzip.BadGzipFile:
+            document = response.content.decode("iso-8859-1")
+
+        try:
+            parsed = xmltodict.parse(
+                document,
+                dict_constructor=dict,
+                attr_prefix="",
+                cdata_key="text",
+                force_list={"attribute", "id", "refinementField"},
+            )
+        except Exception as e:
+            logger.warning(f"Unable to parse the report for the stream, error: {str(e)}")
+            parsed = {}
+
+        yield from parsed.get("Result", {}).get("Node", [])
