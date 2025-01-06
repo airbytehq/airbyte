1803ba89099228915be66da32adbf3b21fbe5fe8
Aldo Gonzalez <aldo.gonzalez@airbyte.io>
2025-01-06 13:52:56 -0800

    source-google-sheets: ruff-format

--- airbyte-integrations/connectors/source-google-sheets/source_google_sheets/components/retrievers.py
+++ airbyte-integrations/connectors/source-google-sheets/source_google_sheets/components/retrievers.py
@@ -13,6 +13,4 @@ from source_google_sheets.components import RangePartitionRouter
 class SheetsDataRetriever(SimpleRetriever):
     def __post_init__(self, parameters: Mapping[str, Any]) -> None:
         super().__post_init__(parameters)
-        self.stream_slicer: StreamSlicer = RangePartitionRouter(
-            parameters= {"batch_size": self.config.get("batch_size"), **parameters }
-        )
+        self.stream_slicer: StreamSlicer = RangePartitionRouter(parameters={"batch_size": self.config.get("batch_size"), **parameters})
