3f2b7bbef71ddeefac31a1cb625a066138b57241
Artem Inzhyyants <artem.inzhyyants@gmail.com>
2025-01-08 17:03:09 +0100

    Amazon SP: fmt init
    Signed-off-by: Artem Inzhyyants <artem.inzhyyants@gmail.com>

--- airbyte-integrations/connectors/source-amazon-seller-partner/source_amazon_seller_partner/components/__init__.py
+++ airbyte-integrations/connectors/source-amazon-seller-partner/source_amazon_seller_partner/components/__init__.py
@@ -1,18 +1,27 @@
+# Copyright (c) 2024 Airbyte, Inc., all rights reserved.
+
 from .auth import AmazonSPOauthAuthenticator
 from .backoff_strategy import AmazonSPWaitTimeFromHeaderBackoffStrategy
+from .decoder import GetXmlBrowseTreeDataDecoder, GzipCsvDecoder, GzipXmlDecoder, SellerFeedbackReportsGzipCsvDecoder
+from .type_transformers import (
+    FlatFileSettlementV2ReportsTypeTransformer,
+    LedgerDetailedViewReportsTypeTransformer,
+    MerchantListingsFypReportTypeTransformer,
+    MerchantReportsTypeTransformer,
+    SellerFeedbackReportsTypeTransformer,
+)
 
-from .decoder import GzipCsvDecoder, GzipXmlDecoder, SellerFeedbackReportsGzipCsvDecoder, GetXmlBrowseTreeDataDecoder
-
-from .type_transformers import LedgerDetailedViewReportsTypeTransformer, MerchantListingsFypReportTypeTransformer, FlatFileSettlementV2ReportsTypeTransformer, \
-    MerchantReportsTypeTransformer, SellerFeedbackReportsTypeTransformer
 
 __all__ = [
     "AmazonSPOauthAuthenticator",
     "AmazonSPWaitTimeFromHeaderBackoffStrategy",
-    "GzipCsvDecoder",  "GzipXmlDecoder", "SellerFeedbackReportsGzipCsvDecoder", "GetXmlBrowseTreeDataDecoder",
+    "GzipCsvDecoder",
+    "GzipXmlDecoder",
+    "SellerFeedbackReportsGzipCsvDecoder",
+    "GetXmlBrowseTreeDataDecoder",
     "LedgerDetailedViewReportsTypeTransformer",
     "MerchantListingsFypReportTypeTransformer",
     "FlatFileSettlementV2ReportsTypeTransformer",
     "MerchantReportsTypeTransformer",
-    "SellerFeedbackReportsTypeTransformer"
+    "SellerFeedbackReportsTypeTransformer",
 ]
