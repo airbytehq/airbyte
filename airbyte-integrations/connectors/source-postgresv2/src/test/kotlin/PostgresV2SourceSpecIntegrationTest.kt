7816d197fe6dbf97d5255fe64aab59caeec8173f
Yue Li <yue.li@airbyte.io>
2025-01-15 15:42:26 -0800

    clean

--- airbyte-integrations/connectors/source-postgresv2/src/test/kotlin/PostgresV2SourceSpecIntegrationTest.kt
+++ airbyte-integrations/connectors/source-postgresv2/src/test/kotlin/PostgresV2SourceSpecIntegrationTest.kt
@@ -1,6 +1,5 @@
-/* Copyright (c) 2024 Airbyte, Inc., all rights reserved. */
-package io.airbyte.integrations.source.postgresv2
 
+package io.airbyte.integrations.source.postgresv2
 import io.airbyte.cdk.command.SyncsTestFixture
 import org.junit.jupiter.api.Test
 
