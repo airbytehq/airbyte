4bc8e89edcf600eafab6be558c31549207103def
Yue Li <yue.li@airbyte.io>
2025-01-15 16:12:00 -0800

    adding configruation spec test for postgres v2

--- airbyte-integrations/connectors/source-postgresv2/src/main/kotlin/PostgresV2SourceConfiguration.kt
+++ airbyte-integrations/connectors/source-postgresv2/src/main/kotlin/PostgresV2SourceConfiguration.kt
@@ -18,6 +18,7 @@ data class PostgresV2SourceConfiguration(
     override val realPort: Int,
     override val jdbcProperties: Map<String, String>,
     override val jdbcUrlFmt: String = "",
+    val dbName: String = "",
     override val namespaces: Set<String> = setOf<String>(),
     override val global: Boolean = false,
     override val maxSnapshotReadDuration: Duration? = null,
@@ -55,13 +56,19 @@ class PostgresV2SourceConfigurationFactory :
     ): PostgresV2SourceConfiguration {
         val realHost = pojo.host
         val realPort = pojo.port
+        val dbName = pojo.database
+        val jdbcUrlFmt = "jdbc:postgresql://%s:%s"
         val jdbcProperties = mutableMapOf<String, String>()
+        val defaultSchema = "public"
         jdbcProperties["user"] = pojo.username
         pojo.password?.let { jdbcProperties["password"] = it }
         return PostgresV2SourceConfiguration(
             realHost = realHost,
             realPort = realPort,
+            dbName = dbName,
             jdbcProperties = jdbcProperties,
+            jdbcUrlFmt = jdbcUrlFmt,
+            namespaces = pojo.schemas?.toSet() ?: setOf(defaultSchema),
         )
     }
 }
---
+++ airbyte-integrations/connectors/source-postgresv2/src/test/kotlin/PostgresV2SourceConfigurationSpecificationTest.kt
@@ -0,0 +1,64 @@
+package io.airbyte.integrations.source.postgresv2
+
+import io.airbyte.cdk.ConfigErrorException
+import io.airbyte.cdk.command.ConfigurationSpecificationSupplier
+import io.airbyte.cdk.ssh.SshPasswordAuthTunnelMethod
+import io.airbyte.cdk.ssh.SshTunnelMethodConfiguration
+import io.micronaut.context.annotation.Property
+import io.micronaut.context.env.Environment
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest
+import jakarta.inject.Inject
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+
+@MicronautTest(environments = [Environment.TEST], rebuildContext = true)
+class PostgresV2SourceConfigurationSpecificationTest {
+    @Inject
+    lateinit var supplier:
+        ConfigurationSpecificationSupplier<PostgresV2SourceConfigurationSpecification>
+
+    @Test
+    @Property(name = "airbyte.connector.config.host", value = "localhost")
+    @Property(name = "airbyte.connector.config.port", value = "12345")
+    @Property(name = "airbyte.connector.config.username", value = "FOO")
+    @Property(name = "airbyte.connector.config.password", value = "BAR")
+    @Property(name = "airbyte.connector.config.database", value = "TESTDB")
+    @Property(name = "airbyte.connector.config.schemas", value = "FOO,SYSTEM")
+    fun testPropertyInjection() {
+        val pojo: PostgresV2SourceConfigurationSpecification = supplier.get()
+        Assertions.assertEquals("localhost", pojo.host)
+        Assertions.assertEquals(12345, pojo.port)
+        Assertions.assertEquals("FOO", pojo.username)
+        Assertions.assertEquals("BAR", pojo.password)
+        Assertions.assertEquals("TESTDB", pojo.database)
+        Assertions.assertEquals(listOf("FOO", "SYSTEM"), pojo.schemas)
+    }
+
+    @Test
+    @Property(name = "airbyte.connector.config.json", value = CONFIG_JSON)
+    fun testJson() {
+        val pojo: PostgresV2SourceConfigurationSpecification = supplier.get()
+        Assertions.assertEquals("localhost", pojo.host)
+        Assertions.assertEquals(12345, pojo.port)
+        Assertions.assertEquals("FOO", pojo.username)
+        Assertions.assertEquals("BAR", pojo.password)
+        Assertions.assertEquals(listOf("FOO", "SYSTEM"), pojo.schemas)
+        Assertions.assertEquals("TESTDB", pojo.database)
+    }
+}
+
+const val CONFIG_JSON =
+    """
+{
+  "host": "localhost",
+  "port": 12345,
+  "database": "TESTDB",
+  "username": "FOO",
+  "password": "BAR",
+  "schemas": [
+    "FOO",
+    "SYSTEM"
+  ]
+}
+"""
+
