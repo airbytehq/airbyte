123e643cb5833d2fc8ba2d970c4734d8bbdf753f
Jimmy Ma <jimmy@airbyte.io>
2025-01-16 11:58:06 -0800

    fix tests

--- airbyte-integrations/connectors/destination-mssql-v2/src/test/kotlin/io/airbyte/integrations/destination/mssql/v2/ResultSetToAirbyteValueTest.kt
+++ airbyte-integrations/connectors/destination-mssql-v2/src/test/kotlin/io/airbyte/integrations/destination/mssql/v2/ResultSetToAirbyteValueTest.kt
@@ -4,8 +4,10 @@
 
 package io.airbyte.integrations.destination.mssql.v2
 
-import io.airbyte.cdk.load.data.TimeValue
-import io.airbyte.cdk.load.data.TimestampValue
+import io.airbyte.cdk.load.data.TimeWithTimezoneValue
+import io.airbyte.cdk.load.data.TimeWithoutTimezoneValue
+import io.airbyte.cdk.load.data.TimestampWithTimezoneValue
+import io.airbyte.cdk.load.data.TimestampWithoutTimezoneValue
 import io.airbyte.integrations.destination.mssql.v2.convert.ResultSetToAirbyteValue.Companion.toTimeWithTimezone
 import io.airbyte.integrations.destination.mssql.v2.convert.ResultSetToAirbyteValue.Companion.toTimeWithoutTimezone
 import io.airbyte.integrations.destination.mssql.v2.convert.ResultSetToAirbyteValue.Companion.toTimestampWithTimezone
@@ -17,28 +19,28 @@ class ResultSetToAirbyteValueTest {
     @Test
     fun `test TimeWithTimezone read`() {
         val actual = "1970-01-01 12:34:56.0000000 +00:00".toTimeWithTimezone()
-        val expected = TimeValue("12:34:56Z")
+        val expected = TimeWithTimezoneValue("12:34:56Z")
         assertEquals(expected, actual)
     }
 
     @Test
     fun `test TimeWithoutTimezone read`() {
         val actual = "12:34:56.0000000".toTimeWithoutTimezone()
-        val expected = TimeValue("12:34:56")
+        val expected = TimeWithoutTimezoneValue("12:34:56")
         assertEquals(expected, actual)
     }
 
     @Test
     fun `test TimestampWithTimezone read`() {
         val actual = "2023-01-23 12:34:56.0000000 +00:00".toTimestampWithTimezone()
-        val expected = TimestampValue("2023-01-23T12:34:56Z")
+        val expected = TimestampWithTimezoneValue("2023-01-23T12:34:56Z")
         assertEquals(expected, actual)
     }
 
     @Test
     fun `test TimestampWithoutTimezone read`() {
         val actual = "2023-01-23 12:34:56.0".toTimestampWithoutTimezone()
-        val expected = TimestampValue("2023-01-23T12:34:56")
+        val expected = TimestampWithoutTimezoneValue("2023-01-23T12:34:56")
         assertEquals(expected, actual)
     }
 }
