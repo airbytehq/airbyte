97465ae52d7d3787adc438a22a5504faa018bb8b
maxi297 <maxime@airbyte.io>
2025-01-23 11:49:10 -0500

    fix relative paths in test

--- airbyte-integrations/connectors/source-amazon-seller-partner/unit_tests/test_migrations.py
+++ airbyte-integrations/connectors/source-amazon-seller-partner/unit_tests/test_migrations.py
@@ -4,7 +4,11 @@
 
 
 import json
+import os
+import tempfile
+from pathlib import Path
 from typing import Any, Mapping
+from unittest import TestCase
 
 import pytest
 from source_amazon_seller_partner.config_migrations import MigrateAccountType, MigrateReportOptions, MigrateStreamNameOption
@@ -14,6 +18,8 @@ from airbyte_cdk.models import OrchestratorType, Type
 from airbyte_cdk.sources import Source
 from airbyte_cdk.test.state_builder import StateBuilder
 
+
+MIGRATIONS_TEST_DIRECTORY = Path(__file__).parent / "test_migrations"
 
 CMD = "check"
 SOURCE: Source = SourceAmazonSellerPartner(
@@ -33,20 +39,20 @@ SOURCE: Source = SourceAmazonSellerPartner(
 )
 
 
-def load_config(config_path: str) -> Mapping[str, Any]:
-    with open(config_path, "r") as config:
+def load_config(config_path: Path) -> Mapping[str, Any]:
+    with config_path.open() as config:
         return json.load(config)
 
 
 class TestMigrateAccountType:
-    test_not_migrated_config_path = "unit_tests/test_migrations/account_type_migration/not_migrated_config.json"
-    test_migrated_config_path = "unit_tests/test_migrations/account_type_migration/migrated_config.json"
+    test_not_migrated_config_path = MIGRATIONS_TEST_DIRECTORY / "account_type_migration/not_migrated_config.json"
+    test_migrated_config_path = MIGRATIONS_TEST_DIRECTORY / "account_type_migration/migrated_config.json"
 
     def test_migrate_config(self, capsys):
         config = load_config(self.test_not_migrated_config_path)
         assert "account_type" not in config
         migration_instance = MigrateAccountType()
-        migration_instance.migrate([CMD, "--config", self.test_not_migrated_config_path], SOURCE)
+        migration_instance.migrate([CMD, "--config", self.test_not_migrated_config_path.as_posix()], SOURCE)
         control_msg = json.loads(capsys.readouterr().out)
         assert control_msg["type"] == Type.CONTROL.value
         assert control_msg["control"]["type"] == OrchestratorType.CONNECTOR_CONFIG.value
@@ -61,8 +67,8 @@ class TestMigrateAccountType:
 
 
 class TestMigrateReportOptions:
-    test_not_migrated_config_path = "unit_tests/test_migrations/report_options_migration/not_migrated_config.json"
-    test_migrated_config_path = "unit_tests/test_migrations/report_options_migration/migrated_config.json"
+    test_not_migrated_config_path = MIGRATIONS_TEST_DIRECTORY / "report_options_migration/not_migrated_config.json"
+    test_migrated_config_path = MIGRATIONS_TEST_DIRECTORY / "report_options_migration/migrated_config.json"
 
     @pytest.mark.parametrize(
         ("input_config", "expected_report_options_list"),
@@ -84,7 +90,7 @@ class TestMigrateReportOptions:
         config = load_config(self.test_not_migrated_config_path)
         assert "report_options_list" not in config
         migration_instance = MigrateReportOptions()
-        migration_instance.migrate([CMD, "--config", self.test_not_migrated_config_path], SOURCE)
+        migration_instance.migrate([CMD, "--config", self.test_not_migrated_config_path.as_posix()], SOURCE)
         control_msg = json.loads(capsys.readouterr().out)
         assert control_msg["type"] == Type.CONTROL.value
         assert control_msg["control"]["type"] == OrchestratorType.CONNECTOR_CONFIG.value
@@ -102,8 +108,8 @@ class TestMigrateReportOptions:
 
 
 class TestMigrateStreamNameOption:
-    test_not_migrated_config_path = "unit_tests/test_migrations/stream_name_option_migration/not_migrated_config.json"
-    test_migrated_config_path = "unit_tests/test_migrations/stream_name_option_migration/migrated_config.json"
+    test_not_migrated_config_path = MIGRATIONS_TEST_DIRECTORY / "stream_name_option_migration/not_migrated_config.json"
+    test_migrated_config_path = MIGRATIONS_TEST_DIRECTORY / "stream_name_option_migration/migrated_config.json"
 
     def test_migrate_config(self, capsys):
         config = load_config(self.test_not_migrated_config_path)
@@ -111,7 +117,7 @@ class TestMigrateStreamNameOption:
             assert "report_name" not in options_list
 
         migration_instance = MigrateStreamNameOption()
-        migration_instance.migrate([CMD, "--config", self.test_not_migrated_config_path], SOURCE)
+        migration_instance.migrate([CMD, "--config", self.test_not_migrated_config_path.as_posix()], SOURCE)
         control_msg = json.loads(capsys.readouterr().out)
         assert control_msg["type"] == Type.CONTROL.value
         assert control_msg["control"]["type"] == OrchestratorType.CONNECTOR_CONFIG.value
