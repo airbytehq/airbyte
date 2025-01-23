3b71d320d7e09dc7ad5f03aed535b8c032180953
darynaishchenko <darina.ishchenko17@gmail.com>
2025-01-23 20:39:25 +0200

    roll back to 0.8.3

--- airbyte-integrations/connectors/source-intercom/source_intercom/components.py
+++ airbyte-integrations/connectors/source-intercom/source_intercom/components.py
@@ -8,7 +8,6 @@ from time import sleep
 from typing import Any, Iterable, List, Mapping, Optional, Union
 
 import requests
-
 from airbyte_cdk.models import SyncMode
 from airbyte_cdk.sources.declarative.incremental import DeclarativeCursor
 from airbyte_cdk.sources.declarative.interpolation.interpolated_string import InterpolatedString
@@ -18,7 +17,6 @@ from airbyte_cdk.sources.declarative.requesters.request_option import RequestOpt
 from airbyte_cdk.sources.declarative.types import Config, Record, StreamSlice, StreamState
 from airbyte_cdk.sources.streams.core import Stream
 from airbyte_cdk.sources.streams.http.error_handlers.response_models import ErrorResolution
-
 
 RequestInput = Union[str, Mapping[str, str]]
 
@@ -201,6 +199,7 @@ class IncrementalSubstreamSlicerCursor(IncrementalSingleSliceCursor):
         cursor_field: Optional[str],
         stream_state: Mapping[str, Any],
     ) -> Iterable[Mapping[str, Any]]:
+
         self.parent_stream.state = stream_state
 
         parent_stream_slices_gen = self.parent_stream.stream_slices(
