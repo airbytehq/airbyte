b716603c8039bf67e143206d17e64d294d80cbe7
Johnny Schmidt <john.schmidt@airbyte.io>
2025-01-23 16:57:36 -0800

    spill file uses default tmp directory

--- airbyte-cdk/bulk/core/load/src/main/kotlin/io/airbyte/cdk/load/file/SpillFileProvider.kt
+++ airbyte-cdk/bulk/core/load/src/main/kotlin/io/airbyte/cdk/load/file/SpillFileProvider.kt
@@ -18,8 +18,8 @@ interface SpillFileProvider {
 @Secondary
 class DefaultSpillFileProvider(val config: DestinationConfiguration) : SpillFileProvider {
     override fun createTempFile(): Path {
-        val directory = config.tmpFileDirectory
-        Files.createDirectories(directory)
-        return Files.createTempFile(directory, "staged-raw-records", ".jsonl")
+        //val directory = config.tmpFileDirectory
+        //Files.createDirectories(directory)
+        return Files.createTempFile("staged-raw-records", ".jsonl")
     }
 }
