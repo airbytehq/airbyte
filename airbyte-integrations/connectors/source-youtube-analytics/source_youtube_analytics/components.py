5a76ca9091c2002dcb240198af6bd333a7060eaa
Tope Folorunso <folorunsotemitope10@gmail.com>
2025-01-24 18:19:29 +0100

    fix creation requester

--- airbyte-integrations/connectors/source-youtube-analytics/source_youtube_analytics/components.py
+++ airbyte-integrations/connectors/source-youtube-analytics/source_youtube_analytics/components.py
@@ -2,15 +2,41 @@
 # Copyright (c) 2023 Airbyte, Inc., all rights reserved.
 #
 
+from dataclasses import dataclass
+from typing import Any, Mapping, Optional
 
-import csv
-import io
-from typing import List
-
-import requests
 from airbyte_cdk.sources.declarative.requesters.http_requester import HttpRequester
-from airbyte_cdk.sources.declarative.types import Record
+from airbyte_cdk.sources.declarative.requesters.request_options.interpolated_request_options_provider import (
+    InterpolatedRequestOptionsProvider,
+    RequestInput,
+)
+from airbyte_cdk.sources.declarative.requesters.http_requester import HttpRequester
 
+@dataclass
+class CreationRequester(HttpRequester):
+
+    request_body_json: Optional[RequestInput] = None
+
+    def __post_init__(self, parameters: Mapping[str, Any]) -> None:
+        self.request_options_provider = InterpolatedRequestOptionsProvider(
+            request_body_json=self.request_body_json,
+            config=self.config,
+            parameters=parameters or {},
+        )
+        super().__post_init__(parameters)
+
+    def send_request(self, **kwargs):
+        request, jobs_response = self._http_client.send_request(
+                http_method="GET",
+                url=self._join_url(self.get_url_base(), "jobs"),
+                request_kwargs={"stream": self.stream_response},
+            )
+        jobs_list = jobs_response.json().get("jobs", [])
+        stream_name = self.name.split(" - ")[-1]
+        if stream_name in [job["reportTypeId"] for job in jobs_list]:
+            return jobs_response
+        else:
+            return super().send_request(**kwargs)
 
 class PollingRequester(HttpRequester):
 
@@ -19,7 +45,7 @@ class PollingRequester(HttpRequester):
             "status": "pending"
         }
         jobs_response = super().send_request(stream_state, stream_slice, next_page_token, path, request_headers, request_params, request_body_data, request_body_json, log_formatter)
-        jobs_list = jobs_response.get("jobs", [])
+        jobs_list = jobs_response.json().get("jobs", [])
         job_resource = [job for job in jobs_list if job["reportTypeId"] == self.name][0]
         job_id = job_resource["id"]
 
@@ -31,7 +57,7 @@ class PollingRequester(HttpRequester):
                 params={"startTimeAtOrAfter": self.start_time} if self.start_time else {},
             )
 
-            urls = [report["downloadUrl"] for report in reports_response["reports"]]
+            urls = [report["downloadUrl"] for report in reports_response.json()["reports"]]
             response["urls"] = urls
             response["status"] = "ready"
         except:
