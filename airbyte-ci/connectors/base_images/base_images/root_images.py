34726804bc5d7f5c9c0a97e8b22ebaae6d8c34af
alafanechere <augustin.lafanechere@gmail.com>
2025-01-27 18:27:05 +0100

    fix image ref

--- airbyte-ci/connectors/base_images/base_images/root_images.py
+++ airbyte-ci/connectors/base_images/base_images/root_images.py
@@ -30,5 +30,5 @@ AMAZON_CORRETTO_21_AL_2023 = PublishedImage(
     registry="docker.io",
     repository="amazoncorretto",
     tag="21-al2023",
-    sha="sha256:6436a0b9e8de5b89014722728803e4445a510e545b4b766c7b3cfd8db6dbad26",
+    sha="6436a0b9e8de5b89014722728803e4445a510e545b4b766c7b3cfd8db6dbad26",
 )
