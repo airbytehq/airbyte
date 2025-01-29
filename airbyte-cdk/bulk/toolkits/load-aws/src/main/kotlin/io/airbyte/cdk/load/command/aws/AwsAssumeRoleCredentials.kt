a093f03e52c3d992f47d3cccf9e02dfa5a9f3b10
Edward Gao <edward.gao@airbyte.io>
2025-01-29 13:20:21 -0800

    Bulk Load CDK: micronaut nonsense (#52627)

--- airbyte-cdk/bulk/toolkits/load-aws/src/main/kotlin/io/airbyte/cdk/load/command/aws/AwsAssumeRoleCredentials.kt
+++ airbyte-cdk/bulk/toolkits/load-aws/src/main/kotlin/io/airbyte/cdk/load/command/aws/AwsAssumeRoleCredentials.kt
@@ -6,6 +6,8 @@ package io.airbyte.cdk.load.command.aws
 
 import io.micronaut.context.annotation.Requires
 import io.micronaut.context.annotation.Value
+import io.micronaut.context.condition.Condition
+import io.micronaut.context.condition.ConditionContext
 import javax.inject.Singleton
 
 // For some reason, micronaut doesn't handle plain `@Requires(property = "...")` when you declare
@@ -14,9 +16,18 @@ import javax.inject.Singleton
 // but then tries to inject a null value to the field, which obviously throws an exception.
 // So instead, we declare the property as `${FOO_BAR:}` (i.e. default to empty string)
 // and set a property notEquals empty string requirement.
-@Requires(property = ACCESS_KEY_PROPERTY, notEquals = "")
-@Requires(property = SECRET_KEY_PROPERTY, notEquals = "")
-@Requires(property = EXTERNAL_ID_PROPERTY, notEquals = "")
+
+// You might expect that after dealing with that mess, we could just declare three @Requires flags,
+// like so:
+// @Requires(property = ACCESS_KEY_PROPERTY, notEquals = "")
+// @Requires(property = SECRET_KEY_PROPERTY, notEquals = "")
+// @Requires(property = EXTERNAL_ID_PROPERTY, notEquals = "")
+
+// However, micronaut sometimes decides to not load the bean, when there are three different
+// @Requires annotations.
+// So instead we define a custom condition, which does the _exact same thing_,
+// but lets us use only a single Requires annotation.
+@Requires(condition = AssumeRoleCredentialsCondition::class)
 @Singleton
 data class AwsAssumeRoleCredentials(
     @Value("\${$ACCESS_KEY_PROPERTY}") val accessKey: String,
@@ -29,3 +40,13 @@ data class AwsAssumeRoleCredentials(
 const val ACCESS_KEY_PROPERTY = "airbyte.destination.aws.assume-role.access-key"
 const val SECRET_KEY_PROPERTY = "airbyte.destination.aws.assume-role.secret-key"
 const val EXTERNAL_ID_PROPERTY = "airbyte.destination.aws.assume-role.external-id"
+
+private object AssumeRoleCredentialsCondition : Condition {
+    override fun matches(context: ConditionContext<*>): Boolean {
+        val accessKey = context.getProperty(ACCESS_KEY_PROPERTY, String::class.java).orElse("")
+        val secretKey = context.getProperty(SECRET_KEY_PROPERTY, String::class.java).orElse("")
+        val externalId = context.getProperty(EXTERNAL_ID_PROPERTY, String::class.java).orElse("")
+
+        return accessKey.isNotEmpty() && secretKey.isNotEmpty() && externalId.isNotEmpty()
+    }
+}
