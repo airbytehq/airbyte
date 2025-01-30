2ce1ffc02d160e4c352770072275f9c6e9fe8d5e
Johnny Schmidt <john.schmidt@airbyte.io>
2025-01-30 11:11:02 -0800

    Destination-S3: Missing nested IT cases (#52584)

--- airbyte-integrations/connectors/destination-s3/src/test-integration/kotlin/io/airbyte/integrations/destination/s3_v2/S3V2WriteTest.kt
+++ airbyte-integrations/connectors/destination-s3/src/test-integration/kotlin/io/airbyte/integrations/destination/s3_v2/S3V2WriteTest.kt
@@ -4,11 +4,17 @@
 
 package io.airbyte.integrations.destination.s3_v2
 
+import io.airbyte.cdk.load.command.Append
+import io.airbyte.cdk.load.command.DestinationStream
 import io.airbyte.cdk.load.command.aws.asMicronautProperties
+import io.airbyte.cdk.load.data.*
 import io.airbyte.cdk.load.data.avro.AvroExpectedRecordMapper
+import io.airbyte.cdk.load.message.InputRecord
 import io.airbyte.cdk.load.test.util.ExpectedRecordMapper
 import io.airbyte.cdk.load.test.util.NoopDestinationCleaner
+import io.airbyte.cdk.load.test.util.OutputRecord
 import io.airbyte.cdk.load.test.util.UncoercedExpectedRecordMapper
+import io.airbyte.cdk.load.test.util.destination_process.DestinationUncleanExitException
 import io.airbyte.cdk.load.write.AllTypesBehavior
 import io.airbyte.cdk.load.write.BasicFunctionalityIntegrationTest
 import io.airbyte.cdk.load.write.SchematizedNestedValueBehavior
@@ -16,9 +22,11 @@ import io.airbyte.cdk.load.write.StronglyTyped
 import io.airbyte.cdk.load.write.UnionBehavior
 import io.airbyte.cdk.load.write.Untyped
 import java.util.concurrent.TimeUnit
+import org.junit.jupiter.api.Assumptions
 import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.Timeout
+import org.junit.jupiter.api.assertThrows
 
 @Timeout(60, unit = TimeUnit.MINUTES)
 abstract class S3V2WriteTest(
@@ -34,6 +42,8 @@ abstract class S3V2WriteTest(
     allTypesBehavior: AllTypesBehavior,
     nullEqualsUnset: Boolean = false,
     nullUnknownTypes: Boolean = false,
+    envVars: Map<String, String> = emptyMap(),
+    private val mergesUnions: Boolean = false
 ) :
     BasicFunctionalityIntegrationTest(
         S3V2TestUtils.getConfig(path),
@@ -66,6 +76,233 @@ abstract class S3V2WriteTest(
     @Test
     override fun testBasicWriteFile() {
         super.testBasicWriteFile()
+    }
+
+    @Test
+    fun testMergeUnions() {
+        Assumptions.assumeTrue(mergesUnions)
+        // Avro and parquet merges unions, merging schemas. Validate the behavior by ensuring
+        // that fields not matching the schema are dropped.
+        val streamName = "stream"
+        val stream =
+            DestinationStream(
+                descriptor = DestinationStream.Descriptor(randomizedNamespace, streamName),
+                importType = Append,
+                generationId = 1L,
+                minimumGenerationId = 0L,
+                syncId = 101L,
+                schema =
+                    ObjectType(
+                        linkedMapOf(
+                            "id" to FieldType(IntegerType, nullable = true),
+                            "union_of_objects" to
+                                FieldType(
+                                    type =
+                                        UnionType.of(
+                                            ObjectType(
+                                                linkedMapOf(
+                                                    "field1" to FieldType(StringType, true),
+                                                    "field2" to FieldType(IntegerType, true),
+                                                    "field4" to FieldType(StringType, true)
+                                                )
+                                            ),
+                                            ObjectType(
+                                                linkedMapOf(
+                                                    "field1" to
+                                                        FieldType(
+                                                            StringType,
+                                                            true
+                                                        ), // merges to String
+                                                    "field3" to FieldType(NumberType, true),
+                                                    "field4" to
+                                                        FieldType(
+                                                            BooleanType,
+                                                            true
+                                                        ) // merges to String|Boolean
+                                                )
+                                            )
+                                        ),
+                                    nullable = true
+                                )
+                        )
+                    )
+            )
+        runSync(
+            configContents,
+            stream,
+            listOf(
+                    """{"id": 1, "union_of_objects": {"field1": "a", "field2": 1, "field3": 3.14, "field4": "boo", "field5": "extra"}}""",
+                    """{"id": 2, "union_of_objects": {"field1": "b", "field2": 2, "field3": 2.71, "field4": true, "field5": "extra"}}"""
+                )
+                .map { InputRecord(randomizedNamespace, streamName, it, 1L) }
+        )
+        val field4a: Any =
+            if (unionBehavior == UnionBehavior.PROMOTE_TO_OBJECT) {
+                mapOf("type" to "string", "string" to "boo")
+            } else {
+                "boo"
+            }
+        val field4b: Any =
+            if (unionBehavior == UnionBehavior.PROMOTE_TO_OBJECT) {
+                mapOf("type" to "boolean", "boolean" to true)
+            } else {
+                true
+            }
+        dumpAndDiffRecords(
+            config = parsedConfig,
+            canonicalExpectedRecords =
+                listOf(
+                        mapOf(
+                            "id" to 1,
+                            "union_of_objects" to
+                                mutableMapOf(
+                                    "field1" to "a",
+                                    "field2" to 1,
+                                    "field3" to 3.14,
+                                    "field4" to field4a
+                                )
+                        ),
+                        mapOf(
+                            "id" to 2,
+                            "union_of_objects" to
+                                mapOf(
+                                    "field1" to "b",
+                                    "field2" to 2,
+                                    "field3" to 2.71,
+                                    "field4" to field4b
+                                )
+                        )
+                    )
+                    .map {
+                        OutputRecord(
+                            extractedAt = 1L,
+                            generationId = 1L,
+                            data = it,
+                            airbyteMeta = OutputRecord.Meta(syncId = 101L)
+                        )
+                    },
+            stream,
+            primaryKey = listOf(listOf("id")),
+            cursor = listOf("id")
+        )
+    }
+
+    @Test
+    fun conflictingTypesInMappedUnions() {
+        Assumptions.assumeTrue(unionBehavior == UnionBehavior.PROMOTE_TO_OBJECT)
+        val stream =
+            DestinationStream(
+                descriptor = DestinationStream.Descriptor(randomizedNamespace, "stream"),
+                importType = Append,
+                generationId = 1L,
+                minimumGenerationId = 0L,
+                syncId = 101L,
+                schema =
+                    ObjectType(
+                        linkedMapOf(
+                            "id" to FieldType(IntegerType, nullable = true),
+                            "union_of_objects" to
+                                FieldType(
+                                    type =
+                                        UnionType.of(
+                                            ObjectType(
+                                                linkedMapOf(
+                                                    "field1" to FieldType(StringType, true),
+                                                )
+                                            ),
+                                            ObjectTypeWithoutSchema
+                                        ),
+                                    nullable = true
+                                )
+                        )
+                    )
+            )
+
+        assertThrows<DestinationUncleanExitException> {
+            runSync(
+                configContents,
+                stream,
+                listOf(
+                        """{"id": 1, "union_of_objects": {"field1": "a"}}""",
+                        """{"id": 2, "union_of_objects": {"undeclared": "field"}}"""
+                    )
+                    .map { InputRecord(randomizedNamespace, "stream", it, 1L) }
+            )
+        }
+    }
+
+    @Test
+    fun testMappableTypesNestedInUnions() {
+        // Avro and parquet both merge unions and map complex types to other types. Validate
+        // that the behavior still works as expected when nested within a union.
+        Assumptions.assumeTrue(mergesUnions)
+        val stream =
+            DestinationStream(
+                descriptor = DestinationStream.Descriptor(randomizedNamespace, "stream"),
+                importType = Append,
+                generationId = 1L,
+                minimumGenerationId = 0L,
+                syncId = 101L,
+                schema =
+                    ObjectType(
+                        linkedMapOf(
+                            "id" to FieldType(IntegerType, nullable = true),
+                            "union_of_objects" to
+                                FieldType(
+                                    type =
+                                        UnionType.of(
+                                            ObjectType(
+                                                linkedMapOf(
+                                                    "field1" to FieldType(StringType, true),
+                                                    "field2" to
+                                                        FieldType(
+                                                            ObjectType(
+                                                                linkedMapOf(
+                                                                    "nested_schemaless" to
+                                                                        FieldType(
+                                                                            ObjectTypeWithoutSchema,
+                                                                            true
+                                                                        ),
+                                                                    "nested_union" to
+                                                                        FieldType(
+                                                                            UnionType.of(
+                                                                                StringType,
+                                                                                BooleanType
+                                                                            ),
+                                                                            true
+                                                                        )
+                                                                )
+                                                            ),
+                                                            true
+                                                        )
+                                                )
+                                            ),
+                                            StringType
+                                        ),
+                                    nullable = true
+                                ),
+                        )
+                    )
+            )
+
+        val expectedRecords =
+            if (unionBehavior == UnionBehavior.PROMOTE_TO_OBJECT) {
+                listOf(
+                    """{"id": 1, "union_of_objects": {"field1": "a", "field2": {"nested_schemaless": "{\"field\": \"value\"}", "nested_union": {"type": "string", "string": "string"}}}}""",
+                    """{"id": 2, "union_of_objects": {"field1": "b", "field2": {"nested_schemaless": "{\"field\": \"value\"}", "nested_union": {"type": "boolean", "boolean": true}}}}"""
+                )
+            } else {
+                listOf(
+                    """{"id": 1, "union_of_objects": {"field1": "a", "field2": {"nested_schemaless": "{\"field\": \"value\"}", "nested_union": "string"}}}""",
+                    """{"id": 2, "union_of_objects": {"field1": "b", "field2": {"nested_schemaless": "{\"field\": \"value\"}", "nested_union": true}}}"""
+                )
+            }
+
+        runSync(
+            configContents,
+            stream,
+            expectedRecords.map { InputRecord(randomizedNamespace, "stream", it, 1L) }
+        )
     }
 }
 
@@ -160,17 +397,8 @@ class S3V2WriteTestAvroUncompressed :
         allTypesBehavior = StronglyTyped(integerCanBeLarge = false),
         nullEqualsUnset = true,
         nullUnknownTypes = true,
-    ) {
-    @Test
-    override fun testUnknownTypes() {
-        super.testUnknownTypes()
-    }
-
-    @Test
-    override fun testFunkyCharacters() {
-        super.testFunkyCharacters()
-    }
-}
+        mergesUnions = true
+    )
 
 class S3V2WriteTestAvroBzip2 :
     S3V2WriteTest(
@@ -184,6 +412,7 @@ class S3V2WriteTestAvroBzip2 :
         allTypesBehavior = StronglyTyped(integerCanBeLarge = false),
         nullEqualsUnset = true,
         nullUnknownTypes = true,
+        mergesUnions = true
     )
 
 class S3V2WriteTestParquetUncompressed :
@@ -198,6 +427,7 @@ class S3V2WriteTestParquetUncompressed :
         allTypesBehavior = StronglyTyped(integerCanBeLarge = false),
         nullEqualsUnset = true,
         nullUnknownTypes = true,
+        mergesUnions = true
     )
 
 class S3V2WriteTestParquetSnappy :
@@ -212,6 +442,7 @@ class S3V2WriteTestParquetSnappy :
         allTypesBehavior = StronglyTyped(integerCanBeLarge = false),
         nullEqualsUnset = true,
         nullUnknownTypes = true,
+        mergesUnions = true
     )
 
 class S3V2WriteTestEndpointURL :
