fb8b1d58e28da2467dc9ea0cc94b48e25b961002
Francis Genet <francis.genet@airbyte.io>
2025-01-30 13:28:34 -0800

    Disabling the S3DataLakePerformanceTest class for CI

--- airbyte-integrations/connectors/destination-s3-data-lake/src/test-integration/kotlin/io/airbyte/integrations/destination/s3_data_lake/S3DataLakePerformanceTest.kt
+++ airbyte-integrations/connectors/destination-s3-data-lake/src/test-integration/kotlin/io/airbyte/integrations/destination/s3_data_lake/S3DataLakePerformanceTest.kt
@@ -7,7 +7,9 @@ package io.airbyte.integrations.destination.s3_data_lake
 import io.airbyte.cdk.load.command.aws.asMicronautProperties
 import io.airbyte.cdk.load.write.BasicPerformanceTest
 import java.nio.file.Files
+import org.junit.jupiter.api.Disabled
 
+@Disabled("We don't want this to run in CI")
 class S3DataLakePerformanceTest :
     BasicPerformanceTest(
         configContents = Files.readString(S3DataLakeTestUtil.GLUE_CONFIG_PATH),
