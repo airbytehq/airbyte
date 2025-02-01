f29a9afb4ca1a9377904eb0225faddfe2ee7be8b
Aaron ("AJ") Steers <aj@airbyte.io>
2025-01-31 17:41:10 -0800

    revert-me: dummy change to trigger workflows

--- airbyte-integrations/connectors/source-outbrain-amplify/source_outbrain_amplify/run.py
+++ airbyte-integrations/connectors/source-outbrain-amplify/source_outbrain_amplify/run.py
@@ -9,6 +9,7 @@ from airbyte_cdk.entrypoint import launch
 from source_outbrain_amplify import SourceOutbrainAmplify
 
 
+
 def run():
     source = SourceOutbrainAmplify()
     launch(source, sys.argv[1:])
