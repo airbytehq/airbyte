8537ddfa97be0cc04a824cf1502e142ef3d1b02c
Wenqi Hu <wenqi.huu@gmail.com>
2025-02-03 22:09:50 -0800

    [source volcano] Decorate stream with cursor field for trigger based CDC stream

--- airbyte-cdk/bulk/core/extract/src/main/kotlin/io/airbyte/cdk/discover/AirbyteStreamFactory.kt
+++ airbyte-cdk/bulk/core/extract/src/main/kotlin/io/airbyte/cdk/discover/AirbyteStreamFactory.kt
@@ -1,17 +1,15 @@
 /* Copyright (c) 2024 Airbyte, Inc., all rights reserved. */
 package io.airbyte.cdk.discover
 
+import io.airbyte.cdk.command.SourceConfiguration
 import io.airbyte.protocol.models.Field as AirbyteField
 import io.airbyte.protocol.models.v0.AirbyteStream
 import io.airbyte.protocol.models.v0.CatalogHelpers
 
 /** Stateless object for building an [AirbyteStream] during DISCOVER. */
 interface AirbyteStreamFactory {
-    /** Connector-specific [AirbyteStream] creation logic for GLOBAL-state streams. */
-    fun createGlobal(discoveredStream: DiscoveredStream): AirbyteStream
-
-    /** Connector-specific [AirbyteStream] creation logic for STREAM-state streams. */
-    fun createNonGlobal(discoveredStream: DiscoveredStream): AirbyteStream
+    /** Connector-specific [AirbyteStream] creation logic. */
+    fun create(config: SourceConfiguration, discoveredStream: DiscoveredStream): AirbyteStream
 
     companion object {
 
