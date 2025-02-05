565b1cfe9fb1862b5897a67588e8b7041a09a432
Artem Inzhyyants <artem.inzhyyants@gmail.com>
2025-02-05 18:06:29 +0100

    Amazon Ads: ref source check method [skip ci]
    Signed-off-by: Artem Inzhyyants <artem.inzhyyants@gmail.com>

--- airbyte-integrations/connectors/source-amazon-ads/source_amazon_ads/streams/__init__.py
+++ airbyte-integrations/connectors/source-amazon-ads/source_amazon_ads/streams/__init__.py
@@ -1,14 +1,12 @@
 #
 # Copyright (c) 2023 Airbyte, Inc., all rights reserved.
 #
-from .profiles import Profiles
 from .report_streams import (
     SponsoredDisplayReportStream,
     SponsoredProductsReportStream,
 )
 
 __all__ = [
-    "Profiles",
     "SponsoredDisplayReportStream",
     "SponsoredProductsReportStream",
 ]
