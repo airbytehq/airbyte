85fab29ad800967dfd349cb2d9eb46083852446f
Francis Genet <francis.genet@airbyte.io>
2025-02-07 14:14:01 -0800

    [S3-data-lake] Add rest catalog integration tests (#53141)
    Co-authored-by: Octavia Squidington III <octavia-squidington-iii@users.noreply.github.com>
    Co-authored-by: Edward Gao <edward.gao@airbyte.io>

--- airbyte-integrations/connectors/destination-s3-data-lake/src/test-integration/kotlin/io/airbyte/integrations/destination/s3_data_lake/RestTestContainers.kt
+++ airbyte-integrations/connectors/destination-s3-data-lake/src/test-integration/kotlin/io/airbyte/integrations/destination/s3_data_lake/RestTestContainers.kt
@@ -10,31 +10,34 @@ import java.util.concurrent.atomic.AtomicBoolean
 import org.testcontainers.containers.ComposeContainer
 
 /**
- * Shared test containers for all nessie tests, so that we don't launch redundant docker containers
+ * Shared test containers for all Rest catalog tests, so that we don't launch redundant docker
+ * containers
  */
-object NessieTestContainers {
+object RestTestContainers {
+    val composeFile = File("src/test-integration/resources/rest/docker-compose.yml")
     val testcontainers: ComposeContainer =
-        ComposeContainer(File("src/test-integration/resources/nessie/docker-compose.yml"))
-            .withExposedService("nessie", 19120)
+        ComposeContainer(composeFile)
             .withExposedService("minio", 9000)
-            .withExposedService("keycloak", 8080)
-    private val startNessieContainerRunOnce = AtomicBoolean(false)
+            .withExposedService("rest", 8181)
+            // we don't directly interact with spark,
+            // but this container depends on minio+rest,
+            // so it's an easy proxy for everything being started.
+            .withExposedService("spark-iceberg", 8080)
+    private val startRestContainerRunOnce = AtomicBoolean(false)
 
     /**
      * Start the test containers, or if another thread already called this method, wait for them to
      * finish starting
      */
     fun start() {
-        if (startNessieContainerRunOnce.setOnce()) {
+        if (startRestContainerRunOnce.setOnce()) {
             testcontainers.start()
         } else {
             // afaict there's no method to wait for the containers to start
             // so just poll until these methods stop throwing exceptions
             while (true) {
                 try {
-                    testcontainers.getServicePort("nessie", 19120)
-                    testcontainers.getServicePort("minio", 9000)
-                    testcontainers.getServicePort("keycloak", 8080)
+                    testcontainers.getServicePort("spark-iceberg", 8080)
                 } catch (e: IllegalStateException) {
                     // do nothing
                 }
--- docs/integrations/destinations/s3-data-lake.md
+++ docs/integrations/destinations/s3-data-lake.md
@@ -40,6 +40,7 @@ written into the Iceberg file.
 
 | Version | Date       | Pull Request                                               | Subject                                                                      |
 |:--------|:-----------|:-----------------------------------------------------------|:-----------------------------------------------------------------------------|
+| 0.3.7   | 2025-02-07 | [\#53141](https://github.com/airbytehq/airbyte/pull/53141) | Adding integration tests around the Rest catalog                             |
 | 0.3.6   | 2025-02-06 | [\#53172](https://github.com/airbytehq/airbyte/pull/53172) | Internal refactor                                                            |
 | 0.3.5   | 2025-02-06 | [\#53164](https://github.com/airbytehq/airbyte/pull/53164) | Improve error message on null primary key in dedup mode                      |
 | 0.3.4   | 2025-02-05 | [\#53173](https://github.com/airbytehq/airbyte/pull/53173) | Tweak spec wording                                                           |
