58fad21b99262320b13a4ee79c3f14a34cb448e5
Edward Gao <edward.gao@airbyte.io>
2025-02-10 13:29:47 -0800

    Revert "chore: clean out unused "bases" and utils (#53234)"
    This reverts commit 4dec57a29f50de72c606a7b24e01155b17901753.

---
+++ airbyte-integrations/bases/base-normalization/integration_tests/normalization_test_output/bigquery/test_nested_streams/models/generated/airbyte_ctes/test_normalization/nested_stream_with_complex_columns_resulting_into_long_names_ab1.sql
@@ -0,0 +1,21 @@
+{{ config(
+    cluster_by = "_airbyte_emitted_at",
+    partition_by = {"field": "_airbyte_emitted_at", "data_type": "timestamp", "granularity": "day"},
+    unique_key = '_airbyte_ab_id',
+    schema = "_airbyte_test_normalization",
+    tags = [ "top-level-intermediate" ]
+) }}
+-- SQL model to parse JSON blob stored in a single column and extract into separated field columns as described by the JSON Schema
+-- depends_on: {{ source('test_normalization', '_airbyte_raw_nested_stream_with_complex_columns_resulting_into_long_names') }}
+select
+    {{ json_extract_scalar('_airbyte_data', ['id'], ['id']) }} as id,
+    {{ json_extract_scalar('_airbyte_data', ['date'], ['date']) }} as date,
+    {{ json_extract('table_alias', '_airbyte_data', ['partition'], ['partition']) }} as {{ adapter.quote('partition') }},
+    _airbyte_ab_id,
+    _airbyte_emitted_at,
+    {{ current_timestamp() }} as _airbyte_normalized_at
+from {{ source('test_normalization', '_airbyte_raw_nested_stream_with_complex_columns_resulting_into_long_names') }} as table_alias
+-- nested_stream_with_complex_columns_resulting_into_long_names
+where 1 = 1
+{{ incremental_clause('_airbyte_emitted_at', this) }}
+
---
+++ airbyte-integrations/bases/base-normalization/integration_tests/normalization_test_output/postgres/test_nested_streams/second_output/airbyte_incremental/test_normalization/nested_stream_with_c___long_names_partition.sql
@@ -0,0 +1,9 @@
+
+      
+
+    insert into "postgres".test_normalization."nested_stream_with_c___long_names_partition" ("_airbyte_nested_stre__nto_long_names_hashid", "double_array_data", "DATA", "_airbyte_ab_id", "_airbyte_emitted_at", "_airbyte_normalized_at", "_airbyte_partition_hashid")
+    (
+        select "_airbyte_nested_stre__nto_long_names_hashid", "double_array_data", "DATA", "_airbyte_ab_id", "_airbyte_emitted_at", "_airbyte_normalized_at", "_airbyte_partition_hashid"
+        from "nested_stream_with_c___long_names_partitio__dbt_tmp"
+    )
+  
