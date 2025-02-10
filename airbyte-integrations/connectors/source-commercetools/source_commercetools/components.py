1bb15b32fd532c0422510e13f0e289c251ac4e9d
marcosmarxm <marcosmarxm@gmail.com>
2025-02-10 13:56:23 -0300

    format files

--- airbyte-integrations/connectors/source-commercetools/source_commercetools/components.py
+++ airbyte-integrations/connectors/source-commercetools/source_commercetools/components.py
@@ -7,8 +7,10 @@ from dataclasses import dataclass
 
 import backoff
 import requests
+
 from airbyte_cdk.sources.declarative.auth import DeclarativeOauth2Authenticator
 from airbyte_cdk.sources.streams.http.exceptions import DefaultBackoffException
+
 
 logger = logging.getLogger("airbyte")
 
@@ -28,16 +30,12 @@ class CommerceToolsOauth2Authenticator(DeclarativeOauth2Authenticator):
         host = self.config["host"]
         url = f"https://auth.{region}.{host}.commercetools.com/oauth/token?grant_type=client_credentials&scope=manage_project:{project_key}"
         try:
-            response = requests.post(
-                url, auth=(self.config["client_id"], self.config["client_secret"])
-            )
+            response = requests.post(url, auth=(self.config["client_id"], self.config["client_secret"]))
             response.raise_for_status()
             return response.json()
         except requests.exceptions.RequestException as e:
             if e.response.status_code == 429 or e.response.status_code >= 500:
-                raise DefaultBackoffException(
-                    request=e.response.request, response=e.response
-                )
+                raise DefaultBackoffException(request=e.response.request, response=e.response)
             raise
         except Exception as e:
             raise Exception(f"Error while refreshing access token: {e}") from e
