636fed56705f003f48be097e152f59bdfcdc77e4
Aaron Steers <aj@airbyte.io>
2025-02-11 13:03:17 -0800

    misc updates

---
+++ airbyte-integrations/connectors/source-hardcoded-records/integration_tests/test_minicat.py
@@ -0,0 +1,14 @@
+"""Mini-CAT test for Google Sheets source.
+
+We import the `MiniCAT` class from the CDK. This gives us the ability to step-debug tests and
+iterate locally and quickly.
+"""
+
+from airbyte_cdk.test.declarative.test_suites import SourceTestSuiteBase
+
+
+class MyMiniCAT(SourceTestSuiteBase):
+    """Instantiates the Mini-CAT test suite for Google Sheets source.
+
+    This suite can be run with pytest. All config is optional.
+    """
