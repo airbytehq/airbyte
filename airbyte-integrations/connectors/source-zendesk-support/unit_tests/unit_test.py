7da5e47ab59fb80783f8a9e18057e44f68138839
Marcos Marx <marcosmarxm@users.noreply.github.com>
2025-02-12 15:01:46 -0300

    Update unit_test.py

--- airbyte-integrations/connectors/source-zendesk-support/unit_tests/unit_test.py
+++ airbyte-integrations/connectors/source-zendesk-support/unit_tests/unit_test.py
@@ -185,7 +185,7 @@ def test_check(response, start_date, check_passed):
 @pytest.mark.parametrize(
     "ticket_forms_response, status_code, expected_n_streams, expected_warnings, reason",
     [
-        ('{"ticket_forms": [{"id": 1, "updated_at": "2021-07-08T00:05:45Z"}]}', 200, 38, [], None),
+        ('{"ticket_forms": [{"id": 1, "updated_at": "2021-07-08T00:05:45Z"}]}', 200, 40, [], None),
         (
             '{"error": "Not sufficient permissions"}',
             403,
