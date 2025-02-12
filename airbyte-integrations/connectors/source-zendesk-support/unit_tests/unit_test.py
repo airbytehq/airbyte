f9a245f7a0fcd41d1c562d5c68eb33a840e04195
Bill McGuinness <117106863+wmcguinn-octave@users.noreply.github.com>
2025-02-12 12:33:16 -0700

    ✨ Source Zendesk Support: add new stream User Identities (#47939)
    Co-authored-by: Marcos Marx <marcosmarxm@users.noreply.github.com>

--- airbyte-integrations/connectors/source-zendesk-support/unit_tests/unit_test.py
+++ airbyte-integrations/connectors/source-zendesk-support/unit_tests/unit_test.py
@@ -185,11 +185,11 @@ def test_check(response, start_date, check_passed):
 @pytest.mark.parametrize(
     "ticket_forms_response, status_code, expected_n_streams, expected_warnings, reason",
     [
-        ('{"ticket_forms": [{"id": 1, "updated_at": "2021-07-08T00:05:45Z"}]}', 200, 39, [], None),
+        ('{"ticket_forms": [{"id": 1, "updated_at": "2021-07-08T00:05:45Z"}]}', 200, 40, [], None),
         (
             '{"error": "Not sufficient permissions"}',
             403,
-            36,
+            37,
             [
                 "An exception occurred while trying to access TicketForms stream: Forbidden. You don't have permission to access this resource.. Skipping this stream."
             ],
@@ -198,7 +198,7 @@ def test_check(response, start_date, check_passed):
         (
             "",
             404,
-            36,
+            37,
             [
                 "An exception occurred while trying to access TicketForms stream: Not found. The requested resource was not found on the server.. Skipping this stream."
             ],
