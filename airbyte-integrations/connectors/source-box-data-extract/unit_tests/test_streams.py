200d15368466c77a8b8724afa58e902437de8a21
Barduino <barduinor@gmail.com>
2025-02-13 14:54:29 +0000

    [UNIT TESTS] flaky tests adjustment

--- airbyte-integrations/connectors/source-box-data-extract/unit_tests/test_streams.py
+++ airbyte-integrations/connectors/source-box-data-extract/unit_tests/test_streams.py
@@ -2,18 +2,39 @@
 # Copyright (c) 2024 Airbyte, Inc., all rights reserved.
 #
 
-from http import HTTPStatus
-from unittest.mock import MagicMock
+# from http import HTTPStatus
+from typing import Any, Mapping
+# from unittest.mock import MagicMock
 
 import pytest
-from source_box_data_extract.source import BoxFileTextStream
+from source_box_data_extract.source import StreamTextRepresentationFolder
+from source_box_data_extract.box_api import get_box_ccg_client
 
 # @pytest.fixture
 # def patch_base_class(mocker):
 #     # Mock abstract methods to enable instantiating abstract class
 #     # mocker.patch.object(BoxFileTextStream, "path", "v0/example_endpoint")
-#     mocker.patch.object(BoxFileTextStream, "primary_key", "test_primary_key")
-#     mocker.patch.object(BoxFileTextStream, "__abstractmethods__", set())
+#     mocker.patch.object(StreamTextRepresentationFolder, "primary_key", "test_primary_key")
+#     mocker.patch.object(StreamTextRepresentationFolder, "__abstractmethods__", set())
+
+@pytest.fixture
+def sample_config()->Mapping[str, Any]:
+    return {
+        "client_id": "test_client_id",
+        "client_secret": "test_client_secret",
+        "box_subject_type": "user",
+        "box_subject_id": "test_box_subject_id",
+        "folder_id": "test_folder_id"
+    }
+
+def test_stream_text_representation_folder(sample_config):
+    client = get_box_ccg_client(sample_config)
+    stream = StreamTextRepresentationFolder(client,sample_config["folder_id"])
+    
+    assert stream.folder_id == sample_config["folder_id"]
+    assert stream.client == client
+    assert stream.primary_key == "id"
+
 
 
 # def test_request_params(patch_base_class):
@@ -35,7 +56,18 @@ from source_box_data_extract.source import BoxFileTextStream
 
 
 # def test_parse_response(patch_base_class):
-#     stream = BoxFileTextStream()
+#     config: Mapping[str, Any] = {}
+#     config["client_id"] = "test_client_id"
+#     config["client_secret"] = "test_client_secret"
+#     config["box_subject_type"] = "user"
+#     config["box_subject_id"] = "test_box_subject_id"
+
+#     config["folder_id"] = "test_folder_id"
+
+
+#     client = get_box_ccg_client(config)
+#     stream = StreamTextRepresentationFolder(client,config["folder_id"])
+
 #     # TODO: replace this with your input parameters
 #     inputs = {"response": MagicMock()}
 #     # TODO: replace this with your expected parced object
