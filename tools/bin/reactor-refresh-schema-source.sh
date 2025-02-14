996c81aff9537024503dd62c83b79da888f8cac4
Matthew Winslow <mattjw@soundcommerce.com>
2025-02-14 11:24:43 -0800

    ACC-7752
    - adding cart-discounts stream
    - adding helper script for refreshing catalog and schemas with some basic checks.

---
+++ tools/bin/reactor-refresh-schema-source.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+which schema_generator > /dev/null
+if [ $? != 0 ];then
+  echo "schema_generator is not installed.  See https://github.com/airbytehq/airbyte/tree/master/tools/schema_generator/ for installation instructions"
+fi
+root=$(git rev-parse --show-toplevel 2>> /dev/null)
+source=$1
+airbyte_config=$2
+source_root="${root}/airbyte-integrations/connectors/source-${source}"
+if [ ! --d "${source_root}" ];then
+  echo "Invalid source specified . Source folder not found at ${source_root}"
+  exit 1
+fi
+
+if [ -z "${airbyte_config}" ];then
+  echo "airbyte config must be provided"
+  exit 1
+fi
+if [ ! -f "${airbyte_config}" ];then
+  echo "${airbyte_config} is not a valid path"
+  exit 1
+fi
+pushd $PWD > /dev/null
+cd $source_root
+echo "refreshing catalog for source"
+python main.py discover --config $airbyte_config | schema_generator --configure-catalog
+
+
+echo "refreshing schemas for source. (note: This is fetching live data using the provided credentials. it may take a while."
+cd source_commercetools
+python ../main.py read --config $airbyte_config --catalog ../integration_tests/configured_catalog.json | schema_generator --infer-schemas
+echo "Finished refreshing schemas.  NOTE: Some schemas have been tweaked and standardized by hand.  You probably only want to check in the new schema for the no stream that was added."
+echo "$source_root/integration_tests/configured_catalog.json should be checked in along with any new schemas found at $source_root/source_${source}/schemas/"
+popd > /dev/null
