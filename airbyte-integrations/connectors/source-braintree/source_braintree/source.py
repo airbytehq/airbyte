36ba9ce63f0ae40b60e54ecf00c98e23ea641640
Matthew Winslow <mattjw@soundcommerce.com>
2025-02-18 13:36:44 -0800

    ACC-4009
    - updating to newer airbyte-cdk
    - removing schema loader elements because it just makes the manifest.yaml harder to manage.
    - removing usage of data classes defined for api responses because they just cause validation errors and most likely cause unrecognized fields to be omitted.
      - making braintree implementation compatible with python 3.11 by dropping older pydantic dependencies.

--- airbyte-integrations/connectors/source-braintree/source_braintree/source.py
+++ airbyte-integrations/connectors/source-braintree/source_braintree/source.py
@@ -3,32 +3,14 @@
 #
 
 from dataclasses import dataclass
-from typing import List, Union
+from typing import List, Iterable, Mapping, Any
 
 import requests
-from braintree.attribute_getter import AttributeGetter
-from braintree.customer import Customer as BCustomer
-from braintree.discount import Discount as BDiscount
-from braintree.dispute import Dispute as BDispute
-from braintree.merchant_account.merchant_account import MerchantAccount as BMerchantAccount
-from braintree.plan import Plan as BPlan
-from braintree.subscription import Subscription as BSubscription
-from braintree.transaction import Transaction as BTransaction
-from braintree.util.xml_util import XmlUtil
-
 from airbyte_cdk.sources.declarative.extractors.record_extractor import RecordExtractor
-from airbyte_cdk.sources.declarative.types import Record
-from airbyte_cdk.sources.declarative.yaml_declarative_source import YamlDeclarativeSource
-from source_braintree.schemas import Customer, Discount, Dispute, MerchantAccount, Plan, Subscription, Transaction
-
-
-"""
-This file provides the necessary constructs to interpret a provided declarative YAML configuration file into
-source connector.
-
-WARNING: Do not modify this file.
-"""
-
+from airbyte_cdk.sources.declarative.yaml_declarative_source import (
+    YamlDeclarativeSource,
+)
+from braintree.util.xml_util import XmlUtil
 
 @dataclass
 class BraintreeExtractor(RecordExtractor):
@@ -36,30 +18,37 @@ class BraintreeExtractor(RecordExtractor):
     Extractor Template for all BrainTree streams.
     """
 
+    def extract_records(self, response: requests.Response) -> Iterable[Mapping[str, Any]]:
+        raise NotImplementedError()
+
     @staticmethod
-    def _extract_as_array(results, attribute):
-        if attribute not in results:
+    def _extract_as_array(container: dict | list, array_name: str):
+        #  NOTE: So far what has been observed is that it is already a list when no
+        #  records are returned.
+        # examples: '<?xml version="1.0" encoding="UTF-8"?><plans type="array"/>'
+        #           '<?xml version="1.0" encoding="UTF-8"?><discounts type="array"/>'
+        if isinstance(container, list):
+            return container
+
+        record_list = container.get(array_name)
+        if record_list is None:
             return []
 
-        value = results[attribute]
-        if not isinstance(value, list):
-            value = [value]
-        return value
+        if not isinstance(record_list, list):
+            return [record_list]
 
-    def _get_json_from_resource(self, resource_obj: Union[AttributeGetter, List[AttributeGetter]]):
-        if isinstance(resource_obj, list):
-            return [obj if not isinstance(obj, AttributeGetter) else self._get_json_from_resource(obj) for obj in resource_obj]
-        obj_dict = resource_obj.__dict__
-        result = dict()
-        for attr in obj_dict:
-            if not attr.startswith("_"):
-                if callable(obj_dict[attr]):
-                    continue
-                result[attr] = (
-                    self._get_json_from_resource(obj_dict[attr]) if isinstance(obj_dict[attr], (AttributeGetter, list)) else obj_dict[attr]
-                )
-        return result
+        return record_list
 
+    def _extract_records(
+            self,
+            response: requests.Response,
+            collection_name: str,
+            array_name: str | None = None
+    ) -> List[Mapping[str, Any]]:
+        return self._extract_as_array(
+            XmlUtil.dict_from_xml(response.text).get(collection_name),
+            array_name or collection_name.removesuffix("s")
+        )
 
 @dataclass
 class MerchantAccountExtractor(BraintreeExtractor):
@@ -69,16 +58,10 @@ class MerchantAccountExtractor(BraintreeExtractor):
     """
 
     def extract_records(
-        self,
-        response: requests.Response,
-    ) -> List[Record]:
-        data = XmlUtil.dict_from_xml(response.text)["merchant_accounts"]
-        merchant_accounts = self._extract_as_array(data, "merchant_account")
-        return [
-            MerchantAccount(**self._get_json_from_resource(BMerchantAccount(None, merchant_account))).dict(exclude_unset=True)
-            for merchant_account in merchant_accounts
-        ]
-
+            self,
+            response: requests.Response,
+    ) -> List[Mapping[str, Any]]:
+        return self._extract_records(response, "merchant_accounts")
 
 @dataclass
 class CustomerExtractor(BraintreeExtractor):
@@ -88,12 +71,10 @@ class CustomerExtractor(BraintreeExtractor):
     """
 
     def extract_records(
-        self,
-        response: requests.Response,
-    ) -> List[Record]:
-        data = XmlUtil.dict_from_xml(response.text)["customers"]
-        customers = self._extract_as_array(data, "customer")
-        return [Customer(**self._get_json_from_resource(BCustomer(None, customer))).dict(exclude_unset=True) for customer in customers]
+            self,
+            response: requests.Response,
+    ) -> List[Mapping[str, Any]]:
+        return self._extract_records(response, "customers")
 
 
 @dataclass
@@ -104,12 +85,10 @@ class DiscountExtractor(BraintreeExtractor):
     """
 
     def extract_records(
-        self,
-        response: requests.Response,
-    ) -> List[Record]:
-        data = XmlUtil.dict_from_xml(response.text)
-        discounts = self._extract_as_array(data, "discounts")
-        return [Discount(**self._get_json_from_resource(BDiscount(None, discount))).dict(exclude_unset=True) for discount in discounts]
+            self,
+            response: requests.Response,
+    ) -> List[Mapping[str, Any]]:
+        return self._extract_records(response, "discounts")
 
 
 @dataclass
@@ -120,16 +99,10 @@ class TransactionExtractor(BraintreeExtractor):
     """
 
     def extract_records(
-        self,
-        response: requests.Response,
-    ) -> List[Record]:
-        data = XmlUtil.dict_from_xml(response.text)["credit_card_transactions"]
-        transactions = self._extract_as_array(data, "transaction")
-        return [
-            Transaction(**self._get_json_from_resource(BTransaction(None, transaction))).dict(exclude_unset=True)
-            for transaction in transactions
-        ]
-
+            self,
+            response: requests.Response,
+    ) -> List[Mapping[str, Any]]:
+        return self._extract_records(response, "credit_card_transactions", "transaction")
 
 @dataclass
 class SubscriptionExtractor(BraintreeExtractor):
@@ -139,15 +112,10 @@ class SubscriptionExtractor(BraintreeExtractor):
     """
 
     def extract_records(
-        self,
-        response: requests.Response,
-    ) -> List[Record]:
-        data = XmlUtil.dict_from_xml(response.text)["subscriptions"]
-        subscriptions = self._extract_as_array(data, "subscription")
-        return [
-            Subscription(**self._get_json_from_resource(BSubscription(None, subscription))).dict(exclude_unset=True)
-            for subscription in subscriptions
-        ]
+            self,
+            response: requests.Response,
+    ) -> List[Mapping[str, Any]]:
+        return self._extract_records(response, "subscriptions")
 
 
 @dataclass
@@ -158,12 +126,10 @@ class PlanExtractor(BraintreeExtractor):
     """
 
     def extract_records(
-        self,
-        response: requests.Response,
-    ) -> List[Record]:
-        data = XmlUtil.dict_from_xml(response.text)
-        plans = self._extract_as_array(data, "plans")
-        return [Plan(**self._get_json_from_resource(BPlan(None, plan))).dict(exclude_unset=True) for plan in plans]
+            self,
+            response: requests.Response,
+    ) -> List[Mapping[str, Any]]:
+        return self._extract_records(response, "plans")
 
 
 @dataclass
@@ -174,13 +140,10 @@ class DisputeExtractor(BraintreeExtractor):
     """
 
     def extract_records(
-        self,
-        response: requests.Response,
-    ) -> List[Record]:
-        data = XmlUtil.dict_from_xml(response.text)["disputes"]
-        disputes = self._extract_as_array(data, "dispute")
-        return [Dispute(**self._get_json_from_resource(BDispute(dispute))).dict(exclude_unset=True) for dispute in disputes]
-
+            self,
+            response: requests.Response,
+    ) -> List[Mapping[str, Any]]:
+        return self._extract_records(response, "disputes")
 
 # Declarative Source
 class SourceBraintree(YamlDeclarativeSource):
