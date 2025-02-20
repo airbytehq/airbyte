699669179d88a188ce8eb437da7a0ba50f8fbf74
btkcodedev <btk.codedev@gmail.com>
2025-02-21 02:38:12 +0530

    delete unused components

--- airbyte-integrations/connectors/source-railz/components.py
+++ airbyte-integrations/connectors/source-railz/components.py
@@ -5,18 +5,15 @@
 import datetime
 import time
 from dataclasses import InitVar, dataclass
-from typing import Any, Iterable, Mapping, Optional, Union
+from typing import Any, Mapping, Union
 
 import requests
 from isodate import Duration, parse_duration
 
-from airbyte_cdk.models import SyncMode
 from airbyte_cdk.sources.declarative.auth.declarative_authenticator import DeclarativeAuthenticator
 from airbyte_cdk.sources.declarative.auth.token import BasicHttpAuthenticator
 from airbyte_cdk.sources.declarative.interpolation.interpolated_string import InterpolatedString
-from airbyte_cdk.sources.declarative.stream_slicers import CartesianProductStreamSlicer
-from airbyte_cdk.sources.declarative.types import Config, Record, StreamSlice
-from airbyte_cdk.sources.streams.http.requests_native_auth.abstract_token import AbstractHeaderAuthenticator
+from airbyte_cdk.sources.declarative.types import Config
 
 
 @dataclass
@@ -83,53 +80,3 @@ class ShortLivedTokenAuthenticator(DeclarativeAuthenticator):
     def token(self) -> str:
         self.check_token()
         return f"Bearer {self._token}"
-
-
-@dataclass
-class NestedStateCartesianProductStreamSlicer(CartesianProductStreamSlicer):
-    """
-    [Low-Code Custom Component] NestedStateCartesianProductStreamSlicer
-    https://github.com/airbytehq/airbyte/issues/22873
-
-    Some streams require support of nested state:
-    {
-      "accounting_transactions": {
-        "Business1": {
-          "dynamicsBusinessCentral": {
-            "postedDate": "2022-12-28T00:00:00.000Z"
-          }
-        },
-        "Business2": {
-          "oracleNetsuite": {
-            "postedDate": "2022-12-28T00:00:00.000Z"
-          }
-        }
-      }
-    }
-    """
-
-    def __post_init__(self, parameters: Mapping[str, Any]):
-        self._cursor = {}
-        self._parameters = parameters
-
-    def get_stream_state(self) -> Mapping[str, Any]:
-        return self._cursor
-
-    def stream_slices(self, sync_mode: SyncMode, stream_state: Mapping[str, Any]) -> Iterable[Mapping[str, Any]]:
-        connections_slicer, datetime_slicer = self.stream_slicers
-        for connection_slice in connections_slicer.stream_slices(sync_mode, stream_state):
-            businessName = connection_slice["connection"]["businessName"]
-            serviceName = connection_slice["connection"]["serviceName"]
-            datetime_slicer._cursor = None
-            for datetime_slice in datetime_slicer.stream_slices(sync_mode, stream_state.get(businessName, {}).get(serviceName, {})):
-                yield connection_slice | datetime_slice
-
-    def update_cursor(self, stream_slice: StreamSlice, last_record: Optional[Record] = None):
-        datetime_slicer = self.stream_slicers[1]
-        datetime_slicer.update_cursor(stream_slice, last_record)
-        if last_record:
-            businessName = stream_slice["connection"]["businessName"]
-            serviceName = stream_slice["connection"]["serviceName"]
-            self._cursor.setdefault(businessName, {}).setdefault(serviceName, {}).update(datetime_slicer.get_stream_state())
-        else:
-            self._cursor = stream_slice
