176fb96f0e19574bcef7a0b2fc84f501b184f279
Artem Inzhyyants <artem.inzhyyants@gmail.com>
2025-02-20 17:33:15 +0100

    Zendesk Chat: remove tests
    Signed-off-by: Artem Inzhyyants <artem.inzhyyants@gmail.com>

--- airbyte-integrations/connectors/source-zendesk-chat/unit_tests/components/test_bans_record_extractor.py
+++ airbyte-integrations/connectors/source-zendesk-chat/unit_tests/components/test_bans_record_extractor.py
@@ -15,4 +15,4 @@ def test_bans_stream_record_extractor(
     test_url = f"https://{config['subdomain']}.zendesk.com/api/v2/chat/bans"
     requests_mock.get(test_url, json=bans_stream_record)
     test_response = requests.get(test_url)
-    assert ZendeskChatBansRecordExtractor().extract_records(test_response) == bans_stream_record_extractor_expected_output
+    assert list(ZendeskChatBansRecordExtractor().extract_records(test_response)) == bans_stream_record_extractor_expected_output
