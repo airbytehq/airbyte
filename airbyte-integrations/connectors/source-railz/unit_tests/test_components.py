5289f092171c5fbf0630581347166a11722fee7d
pnilan <patrick.nilan@airbyte.io>
2025-02-24 17:55:15 -0800

    fix unit tests

--- airbyte-integrations/connectors/source-railz/unit_tests/test_components.py
+++ airbyte-integrations/connectors/source-railz/unit_tests/test_components.py
@@ -1,22 +1,17 @@
 #
 # Copyright (c) 2023 Airbyte, Inc., all rights reserved.
 #
-from datetime import datetime, timezone
-from unittest.mock import MagicMock, patch
+from freezegun import freeze_time
 
 
-def test_get_tokens(components_module):
+def test_get_tokens(components_module, requests_mock):
     url = "https://auth.railz.ai/getAccess"
     responses = [
         {"access_token": "access_token1"},
         {"access_token": "access_token2"},
     ]
 
-    timestamps = [
-        datetime(2023, 1, 1, 12, 0, 0, tzinfo=timezone.utc).timestamp(),
-        datetime(2023, 1, 1, 12, 30, 0, tzinfo=timezone.utc).timestamp(),
-        datetime(2023, 1, 1, 13, 0, 0, tzinfo=timezone.utc).timestamp(),
-    ]
+    requests_mock.get(url, json=lambda request, context: responses.pop(0))
 
     ShortLivedTokenAuthenticator = components_module.ShortLivedTokenAuthenticator
     authenticator = ShortLivedTokenAuthenticator(
@@ -29,13 +24,9 @@ def test_get_tokens(components_module):
         parameters={},
     )
 
-    def mock_requests_get(*args, **kwargs):
-        mock_response = MagicMock()
-        mock_response.json.return_value = responses.pop(0)
-        return mock_response
-
-    # Mock requests.get and time.time
-    with patch("requests.get", side_effect=mock_requests_get), patch("time.time", side_effect=timestamps):
+    with freeze_time("2023-01-01 12:00:00"):
         assert authenticator.token == "Bearer access_token1"
+    with freeze_time("2023-01-01 12:30:00"):
         assert authenticator.token == "Bearer access_token1"
+    with freeze_time("2023-01-01 13:00:00"):
         assert authenticator.token == "Bearer access_token2"
