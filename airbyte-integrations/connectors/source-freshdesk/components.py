b6ce32b09b152183ceedbdce655d459091f515ca
Tope Folorunso <folorunsotemitope10@gmail.com>
2025-02-26 16:01:49 +0100

    migrate to manifest only

--- airbyte-integrations/connectors/source-freshdesk/components.py
+++ airbyte-integrations/connectors/source-freshdesk/components.py
@@ -3,8 +3,10 @@
 from dataclasses import dataclass
 from typing import Any, List, Mapping, MutableMapping, Optional
 
+import time
 import requests
 
+from airbyte_cdk.entrypoint import logger
 from airbyte_cdk.sources.declarative.incremental import DatetimeBasedCursor
 from airbyte_cdk.sources.declarative.requesters.http_requester import HttpRequester
 from airbyte_cdk.sources.declarative.requesters.paginators.strategies.page_increment import PageIncrement
@@ -14,7 +16,33 @@ from airbyte_cdk.sources.declarative.requesters.request_options.interpolated_req
     RequestInput,
 )
 from airbyte_cdk.sources.declarative.types import StreamSlice, StreamState
-from source_freshdesk.utils import CallCredit
+
+
+class CallCredit:
+    """Class to manage call credit balance"""
+
+    def __init__(self, balance: int, reload_period: int = 60):
+        self._max_balance = balance
+        self._balance_reload_period = reload_period
+        self._current_period_start = time.time()
+        self._credits_consumed = 0
+
+    def reset_period(self):
+        self._current_period_start = time.time()
+        self._credits_consumed = 0
+
+    def consume(self, credit: int):
+        # Reset time window if it has elapsed
+        if time.time() > self._current_period_start + self._balance_reload_period:
+            self.reset_period()
+
+        if self._credits_consumed + credit >= self._max_balance:
+            sleep_time = self._balance_reload_period - (time.time() - self._current_period_start)
+            logger.info(f"Reached call limit for this minute, wait for {sleep_time:.2f} seconds")
+            time.sleep(max(1.0, sleep_time))
+            self.reset_period()
+
+        self._credits_consumed += credit
 
 
 @dataclass
@@ -130,7 +158,7 @@ class FreshdeskTicketsPaginationStrategy(PageIncrement):
             return None
         elif self._page >= self.PAGE_LIMIT:
             # reset page count as cursor parameter will be updated in the stream slicer
-            self.reset()
+            self._page = self.start_from_page
             # get last_record from latest batch, pos. -1, because of ACS order of records
             last_record_updated_at = last_records[-1]["updated_at"]
             # updating slicer request parameters with last_record state
