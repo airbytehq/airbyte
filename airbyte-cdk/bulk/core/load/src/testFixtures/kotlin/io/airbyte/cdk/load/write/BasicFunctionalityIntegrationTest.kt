c6c182b6046bb9eab1d4cb65a7d563593a74894d
subodhchaturvedi <subodh1810@gmail.com>
2025-02-27 22:56:37 +0530

    undo changes

--- airbyte-cdk/bulk/core/load/src/testFixtures/kotlin/io/airbyte/cdk/load/write/BasicFunctionalityIntegrationTest.kt
+++ airbyte-cdk/bulk/core/load/src/testFixtures/kotlin/io/airbyte/cdk/load/write/BasicFunctionalityIntegrationTest.kt
@@ -1540,8 +1540,7 @@ abstract class BasicFunctionalityIntegrationTest(
                             "id1" to 1,
                             "id2" to 201,
                             "updated_at" to TimestampWithTimezoneValue("2000-01-01T00:02:00Z"),
-                            "name" to "Bob1",
-                            "_ab_cdc_deleted_at" to null
+                            "name" to "Bob1"
                         ),
                     airbyteMeta = OutputRecord.Meta(syncId = 42),
                 ),
