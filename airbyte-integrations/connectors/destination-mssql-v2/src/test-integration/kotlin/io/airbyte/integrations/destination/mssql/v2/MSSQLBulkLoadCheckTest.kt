285fd6fe449727baa861c975b23a5305a38ef59d
subodhchaturvedi <subodh1810@gmail.com>
2025-02-28 13:53:34 +0530

    format

--- airbyte-integrations/connectors/destination-mssql-v2/src/test-integration/kotlin/io/airbyte/integrations/destination/mssql/v2/MSSQLBulkLoadCheckTest.kt
+++ airbyte-integrations/connectors/destination-mssql-v2/src/test-integration/kotlin/io/airbyte/integrations/destination/mssql/v2/MSSQLBulkLoadCheckTest.kt
@@ -1,3 +1,7 @@
+/*
+ * Copyright (c) 2024 Airbyte, Inc., all rights reserved.
+ */
+
 package io.airbyte.integrations.destination.mssql.v2
 
 import io.airbyte.cdk.load.check.CheckIntegrationTest
@@ -5,14 +9,15 @@ import io.airbyte.cdk.load.check.CheckTestConfig
 import io.airbyte.cdk.load.test.util.FakeConfigurationUpdater
 import io.airbyte.integrations.destination.mssql.v2.config.MSSQLSpecification
 
-class MSSQLBulkLoadCheckTest : CheckIntegrationTest<MSSQLSpecification>(
-    successConfigFilenames =
-    listOf(
-        CheckTestConfig(
-            MSSQLTestConfigUtil.getConfigPath("secrets/bulk_upload_config.json"),
-            name = "Bulk Load Check Should work",
-        ),
-    ),
-    emptyMap(),
-    configUpdater = FakeConfigurationUpdater,
-)
+class MSSQLBulkLoadCheckTest :
+    CheckIntegrationTest<MSSQLSpecification>(
+        successConfigFilenames =
+            listOf(
+                CheckTestConfig(
+                    MSSQLTestConfigUtil.getConfigPath("secrets/bulk_upload_config.json"),
+                    name = "Bulk Load Check Should work",
+                ),
+            ),
+        emptyMap(),
+        configUpdater = FakeConfigurationUpdater,
+    )
