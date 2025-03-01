f750104143eb6066f3cb63efae06bafa5822e3fe
Octavia Squidington III <octavia-squidington-iii@users.noreply.github.com>
2025-03-01 15:14:13 +0000

    chore: auto-fix lint and format issues

--- airbyte-ci/connectors/erd/tests/test_dbml_assembler.py
+++ airbyte-ci/connectors/erd/tests/test_dbml_assembler.py
@@ -4,8 +4,8 @@ from unittest import TestCase
 from unittest.mock import Mock
 
 from airbyte_cdk.models import AirbyteCatalog, AirbyteStream, SyncMode
-from erd.dbml_assembler import DbmlAssembler, Source
 
+from erd.dbml_assembler import DbmlAssembler, Source
 from tests.builder import RelationshipBuilder
 
 _A_STREAM_NAME = "a_stream_name"
--- airbyte-ci/connectors/erd/tests/test_relationships.py
+++ airbyte-ci/connectors/erd/tests/test_relationships.py
@@ -3,7 +3,6 @@
 from unittest import TestCase
 
 from erd.relationships import Relationships, RelationshipsMerger
-
 from tests.builder import RelationshipBuilder
 
 _A_STREAM_NAME = "a_stream_name"
