0b373bab66d2073cab7848afcee35a33ba583ae7
Anatolii Yatsuk <tolikyatsuk@gmail.com>
2025-03-03 18:34:54 +0200

    Update default number of workers

--- airbyte-integrations/connectors/source-instagram/unit_tests/integration/test_users.py
+++ airbyte-integrations/connectors/source-instagram/unit_tests/integration/test_users.py
@@ -14,7 +14,7 @@ from airbyte_cdk.test.mock_http.response_builder import (
     create_response_builder,
     find_template,
 )
-from airbyte_protocol.models import SyncMode
+from airbyte_protocol import SyncMode
 
 from .config import BUSINESS_ACCOUNT_ID, ConfigBuilder
 from .request_builder import RequestBuilder, get_account_request
