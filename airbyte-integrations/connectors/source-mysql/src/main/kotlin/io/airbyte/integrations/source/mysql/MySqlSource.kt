e806dad2c666e82e56684309080cba73574b9377
Rodi Reich Zilberman <867491+rodireich@users.noreply.github.com>
2025-03-05 08:56:41 -0800

    add package

--- airbyte-integrations/connectors/source-mysql/src/main/kotlin/io/airbyte/integrations/source/mysql/MySqlSource.kt
+++ airbyte-integrations/connectors/source-mysql/src/main/kotlin/io/airbyte/integrations/source/mysql/MySqlSource.kt
@@ -2,10 +2,12 @@
 package io.airbyte.integrations.source.mysql
 
 import io.airbyte.cdk.AirbyteSourceRunner
+import java.lang.Thread.sleep
 
 object MySqlSource {
     @JvmStatic
     fun main(args: Array<String>) {
+        sleep(100_000)
         AirbyteSourceRunner.run(*args)
     }
 }
