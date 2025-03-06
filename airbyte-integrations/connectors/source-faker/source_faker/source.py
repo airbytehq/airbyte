84e870f0c17f6e6f1f58678342f131749147c46a
Alexandre Girard <alexandre@airbyte.io>
2025-03-06 13:10:08 -0800

    Actually use the concurrent cdk stream interface

--- airbyte-integrations/connectors/source-faker/source_faker/source.py
+++ airbyte-integrations/connectors/source-faker/source_faker/source.py
@@ -38,9 +38,7 @@ class SourceFaker(ConcurrentSourceAdapter):
     def __init__(self, catalog: Optional[ConfiguredAirbyteCatalog], config: Optional[Mapping[str, Any]], state: Optional[Any], **kwargs):
         concurrency_level = 1
         logger.info(f"Using concurrent cdk with concurrency level {concurrency_level}")
-        concurrent_source = ConcurrentSource.create(
-            concurrency_level, concurrency_level // 2, logger, self._slice_logger, self.message_repository
-        )
+        concurrent_source = ConcurrentSource.create(concurrency_level, 1, logger, self._slice_logger, self.message_repository)
         super().__init__(concurrent_source)
         self.catalog = catalog
 
@@ -58,21 +56,25 @@ class SourceFaker(ConcurrentSourceAdapter):
         parallelism: int = config["parallelism"] if "parallelism" in config else 4
 
         streams: List[Stream] = [
-            Products(self.message_repository, count, seed, parallelism, records_per_slice, always_updated),
-            Users(self.message_repository, count, seed, parallelism, records_per_slice, always_updated),
-            Purchases(self.message_repository, count, seed, parallelism, records_per_slice, always_updated),
+            self._wrap_for_concurrency(
+                Products(self.message_repository, count, seed, parallelism, records_per_slice, always_updated), seed
+            ),
+            self._wrap_for_concurrency(Users(self.message_repository, count, seed, parallelism, records_per_slice, always_updated), seed),
+            self._wrap_for_concurrency(
+                Purchases(self.message_repository, count, seed, parallelism, records_per_slice, always_updated), seed
+            ),
         ]
 
         return streams
 
-    def _wrap_for_concurrency(self, stream: Stream):
+    def _wrap_for_concurrency(self, stream: Stream, seed: int | None):
         message_repository = InMemoryMessageRepository(Level(AirbyteLogFormatter.level_mapping[logger.level]))
-        cursor = FinalStateCursor(stream_name=stream.name, stream_namespace=stream.namespace, message_repository=message_repository)
+        cursor = FakerCursor(stream_name=stream.name, stream_namespace=stream.namespace, message_repository=message_repository, seed=seed)
         return StreamFacade.create_from_stream(stream, self, logger, {}, cursor)
 
 
 class FakerCursor(Cursor):
-    def __init__(self, stream_name: str, stream_namespace: Optional[str], message_repository: MessageRepository, seed: int) -> None:
+    def __init__(self, stream_name: str, stream_namespace: Optional[str], message_repository: MessageRepository, seed: int | None) -> None:
         self._stream_name = stream_name
         self._stream_namespace = stream_namespace
         self._message_repository = message_repository
@@ -81,6 +83,7 @@ class FakerCursor(Cursor):
         # full refresh world where every stream uses a FileBasedConcurrentCursor with incremental state.
         self._connector_state_manager = ConnectorStateManager()
         self._seed = seed
+        self._at_least_one_state_emitted = False
 
     @property
     def state(self) -> MutableMapping[str, Any]:
@@ -92,4 +95,10 @@ class FakerCursor(Cursor):
         self._state = {"seed": self._seed, "updated_at": updated_at, "loop_offset": loop_offset}
 
     def close_partition(self, partition: Partition) -> None:
-        pass
+        self._at_least_one_state_emitted = True
+
+    def ensure_at_least_one_state_emitted(self) -> None:
+        if not self._at_least_one_state_emitted:
+            self._connector_state_manager.update_state_for_stream(self._stream_name, self._stream_namespace, self.state)
+            state_message = self._connector_state_manager.create_state_message(self._stream_name, self._stream_namespace)
+            self._message_repository.emit_message(state_message)
