436d4e88a63ee89d7beda8d89ff0195bf7816002
ekladiousm <mira.ekladious@taico.io>
2025-03-06 15:00:02 +0100

    added reporting base stream

--- airbyte-integrations/connectors/source-campaign-manager/source_campaign_manager/source.py
+++ airbyte-integrations/connectors/source-campaign-manager/source_campaign_manager/source.py
@@ -10,69 +10,22 @@ import requests
 from airbyte_cdk.sources import AbstractSource
 from airbyte_cdk.sources.streams import Stream
 from airbyte_cdk.sources.streams.http import HttpStream
-from airbyte_cdk.sources.streams.http.requests_native_auth import TokenAuthenticator
+from airbyte_cdk.sources.streams.http.auth import Oauth2Authenticator
+from airbyte_cdk.models import SyncMode
 
-"""
-TODO: Most comments in this class are instructive and should be deleted after the source is implemented.
-
-This file provides a stubbed example of how to use the Airbyte CDK to develop both a source connector which supports full refresh or and an
-incremental syncs from an HTTP API.
-
-The various TODOs are both implementation hints and steps - fulfilling all the TODOs should be sufficient to implement one basic and one incremental
-stream from a source. This pattern is the same one used by Airbyte internally to implement connectors.
-
-The approach here is not authoritative, and devs are free to use their own judgement.
-
-There are additional required TODOs in the files within the integration_tests folder and the spec.yaml file.
-"""
-
-
-# Basic full refresh stream
-class CampaignManagerStream(HttpStream, ABC):
+POLLING_IN_SECONDS = 60
+class DFAReportingStream(HttpStream):
     """
-    TODO remove this comment
-
-    This class represents a stream output by the connector.
-    This is an abstract base class meant to contain all the common functionality at the API level e.g: the API base URL, pagination strategy,
-    parsing responses etc..
-
-    Each stream should extend this class (or another abstract subclass of it) to specify behavior unique to that stream.
-
-    Typically for REST APIs each stream corresponds to a resource in the API. For example if the API
-    contains the endpoints
-        - GET v1/customers
-        - GET v1/employees
-
-    then you should have three classes:
-    `class CampaignManagerStream(HttpStream, ABC)` which is the current class
-    `class Customers(CampaignManagerStream)` contains behavior to pull data for customers using v1/customers
-    `class Employees(CampaignManagerStream)` contains behavior to pull data for employees using v1/employees
-
-    If some streams implement incremental sync, it is typical to create another class
-    `class IncrementalCampaignManagerStream((CampaignManagerStream), ABC)` then have concrete stream implementations extend it. An example
-    is provided below.
-
-    See the reference docs for the full list of configurable options.
+    Base stream for DFA Reporting API.
     """
+    url_base = "https://dfareporting.googleapis.com"
+    
+    def __init__(self, profile_id: str, authenticator: Oauth2Authenticator, **kwargs):
+        super().__init__(authenticator=authenticator)
+        self.profile_id = profile_id
 
-    # TODO: Fill in the url base. Required.
-    url_base = "https://example-api.com/v1/"
-
-    def next_page_token(self, response: requests.Response) -> Optional[Mapping[str, Any]]:
-        """
-        TODO: Override this method to define a pagination strategy. If you will not be using pagination, no action is required - just return None.
-
-        This method should return a Mapping (e.g: dict) containing whatever information required to make paginated requests. This dict is passed
-        to most other methods in this class to help you form headers, request bodies, query params, etc..
-
-        For example, if the API accepts a 'page' parameter to determine which page of the result to return, and a response from the API contains a
-        'page' number, then this method should probably return a dict {'page': response.json()['page'] + 1} to increment the page count by 1.
-        The request_params method should then read the input next_page_token and set the 'page' param to next_page_token['page'].
-
-        :param response: the most recent response from the API
-        :return If there is another page in the result, a mapping (e.g: dict) containing information needed to query the next page in the response.
-                If there are no more pages in the result, return None.
-        """
+    def next_page_token(self, response: requests.Response) -> Mapping[str, Any]:
+        # Implement pagination if needed
         return None
 
     def request_params(
