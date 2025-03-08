a0077c7e4e13271107a257f14d6a76b449f71031
Serhii Lazebnyi <serhii.lazebnyi@globallogic.com>
2025-03-08 01:50:41 +0100

    Update source and remove unnessesary methods

--- airbyte-integrations/connectors/source-stripe/unit_tests/integration/test_application_fees.py
+++ airbyte-integrations/connectors/source-stripe/unit_tests/integration/test_application_fees.py
@@ -26,7 +26,7 @@ from airbyte_cdk.test.mock_http.response_builder import (
 )
 from airbyte_cdk.test.state_builder import StateBuilder
 from integration.config import ConfigBuilder
-from integration.helpers import assert_stream_did_not_run
+from integration.helpers import assert_stream_incomplete
 from integration.pagination import StripePaginationStrategy
 from integration.request_builder import StripeRequestBuilder
 from integration.response_builder import a_response_with_status
@@ -166,13 +166,13 @@ class FullRefreshTest(TestCase):
         slice_datetime = start_date + slice_range
 
         http_mocker.get(
-            _application_fees_request().with_created_gte(start_date).with_created_lte(slice_datetime).with_limit(100).build(),
+            _application_fees_request().with_created_gte(slice_datetime).with_created_lte(_NOW).with_limit(100).build(),
             _application_fees_response().build(),
         )
         http_mocker.get(
             _application_fees_request()
-            .with_created_gte(slice_datetime + _AVOIDING_INCLUSIVE_BOUNDARIES)
-            .with_created_lte(_NOW)
+            .with_created_gte(start_date)
+            .with_created_lte(slice_datetime - _AVOIDING_INCLUSIVE_BOUNDARIES)
             .with_limit(100)
             .build(),
             _application_fees_response().build(),
@@ -180,16 +180,14 @@ class FullRefreshTest(TestCase):
 
         self._read(_config().with_start_date(start_date).with_slice_range_in_days(slice_range.days))
 
-        # request matched http_mocker
-
     @HttpMocker()
-    def test_given_http_status_400_when_read_then_stream_did_not_run(self, http_mocker: HttpMocker) -> None:
+    def test_given_http_status_400_when_read_then_stream_incomplete(self, http_mocker: HttpMocker) -> None:
         http_mocker.get(
             _application_fees_request().with_any_query_params().build(),
             a_response_with_status(400),
         )
         output = self._read(_config())
-        assert_stream_did_not_run(output, _STREAM_NAME, "Your account is not set up to use Issuing")
+        assert_stream_incomplete(output, _STREAM_NAME, "Your account is not set up to use Issuing")
 
     @HttpMocker()
     def test_given_http_status_401_when_read_then_config_error(self, http_mocker: HttpMocker) -> None:
@@ -250,17 +248,17 @@ class IncrementalTest(TestCase):
         output = self._read(_config().with_start_date(_A_START_DATE), _NO_STATE)
         most_recent_state = output.most_recent_state
         assert most_recent_state.stream_descriptor == StreamDescriptor(name=_STREAM_NAME)
-        assert most_recent_state.stream_state == AirbyteStateBlob(updated=cursor_value)
+        assert most_recent_state.stream_state.updated == str(cursor_value)
 
     @HttpMocker()
     def test_given_state_when_read_then_query_events_using_types_and_state_value_plus_1(self, http_mocker: HttpMocker) -> None:
         start_date = _NOW - timedelta(days=40)
         state_datetime = _NOW - timedelta(days=5)
-        cursor_value = int(state_datetime.timestamp()) + 1
+        cursor_value = int(state_datetime.timestamp())
 
         http_mocker.get(
             _events_request()
-            .with_created_gte(state_datetime + _AVOIDING_INCLUSIVE_BOUNDARIES)
+            .with_created_gte(state_datetime)
             .with_created_lte(_NOW)
             .with_limit(100)
             .with_types(_EVENT_TYPES)
@@ -277,14 +275,14 @@ class IncrementalTest(TestCase):
 
         most_recent_state = output.most_recent_state
         assert most_recent_state.stream_descriptor == StreamDescriptor(name=_STREAM_NAME)
-        assert most_recent_state.stream_state == AirbyteStateBlob(updated=cursor_value)
+        assert most_recent_state.stream_state.updated == str(cursor_value)
 
     @HttpMocker()
     def test_given_state_and_pagination_when_read_then_return_records(self, http_mocker: HttpMocker) -> None:
         state_datetime = _NOW - timedelta(days=5)
         http_mocker.get(
             _events_request()
-            .with_created_gte(state_datetime + _AVOIDING_INCLUSIVE_BOUNDARIES)
+            .with_created_gte(state_datetime)
             .with_created_lte(_NOW)
             .with_limit(100)
             .with_types(_EVENT_TYPES)
@@ -297,7 +295,7 @@ class IncrementalTest(TestCase):
         http_mocker.get(
             _events_request()
             .with_starting_after("last_record_id_from_first_page")
-            .with_created_gte(state_datetime + _AVOIDING_INCLUSIVE_BOUNDARIES)
+            .with_created_gte(state_datetime)
             .with_created_lte(_NOW)
             .with_limit(100)
             .with_types(_EVENT_TYPES)
@@ -316,12 +314,12 @@ class IncrementalTest(TestCase):
     def test_given_state_and_small_slice_range_when_read_then_perform_multiple_queries(self, http_mocker: HttpMocker) -> None:
         state_datetime = _NOW - timedelta(days=5)
         slice_range = timedelta(days=3)
-        slice_datetime = state_datetime + _AVOIDING_INCLUSIVE_BOUNDARIES + slice_range
+        slice_datetime = state_datetime + slice_range
 
         http_mocker.get(
             _events_request()
-            .with_created_gte(state_datetime + _AVOIDING_INCLUSIVE_BOUNDARIES)
-            .with_created_lte(slice_datetime)
+            .with_created_gte(state_datetime)
+            .with_created_lte(slice_datetime - _AVOIDING_INCLUSIVE_BOUNDARIES)
             .with_limit(100)
             .with_types(_EVENT_TYPES)
             .build(),
@@ -329,7 +327,7 @@ class IncrementalTest(TestCase):
         )
         http_mocker.get(
             _events_request()
-            .with_created_gte(slice_datetime + _AVOIDING_INCLUSIVE_BOUNDARIES)
+            .with_created_gte(slice_datetime)
             .with_created_lte(_NOW)
             .with_limit(100)
             .with_types(_EVENT_TYPES)
