90f7bbc2d860a23c385cac3b677b7cdeed460ed2
Rodi Reich Zilberman <867491+rodireich@users.noreply.github.com>
2025-03-11 15:11:27 -0700

    control disk space

--- airbyte-cdk/bulk/toolkits/extract-jdbc/src/main/kotlin/io/airbyte/cdk/read/DefaultJdbcSharedState.kt
+++ airbyte-cdk/bulk/toolkits/extract-jdbc/src/main/kotlin/io/airbyte/cdk/read/DefaultJdbcSharedState.kt
@@ -6,7 +6,13 @@ package io.airbyte.cdk.read
 
 import io.airbyte.cdk.command.JdbcSourceConfiguration
 import io.airbyte.cdk.jdbc.DefaultJdbcConstants
+import io.github.oshai.kotlinlogging.KotlinLogging
 import jakarta.inject.Singleton
+import java.io.File
+import java.nio.file.FileSystem
+import java.nio.file.Files
+import java.nio.file.Path
+import java.nio.file.Paths
 import java.time.Instant
 
 /** Default implementation of [JdbcSharedState]. */
@@ -55,7 +61,27 @@ class DefaultJdbcSharedState(
         return JdbcPartitionsCreator.AcquiredResources { acquiredThread.close() }
     }
 
+    private val log = KotlinLogging.logger {}
     override fun tryAcquireResourcesForReader(): JdbcPartitionReader.AcquiredResources? {
+
+        ////
+        /// disk space in file staging area
+        try {
+            val folder: Path = Paths.get("/staging/files")
+            val size = Files.walk(folder)
+                .filter { Files.isRegularFile(it) }
+                .mapToLong { it.toFile().length() }
+                .sum()
+//            log.info { "Total size of files in /staging/files: $size bytes" }
+            if (size > 4L shl 30) { // 4GB
+                log.info { "staging dir too full $size" }
+                return null
+            }
+        } catch (_: Exception) {
+            log.info { "Error calculating total size of files in /staging/files" }
+        }
+        /// if disk too full return null
+        ////
         val acquiredThread: ConcurrencyResource.AcquiredThread =
             concurrencyResource.tryAcquire() ?: return null
         return JdbcPartitionReader.AcquiredResources { acquiredThread.close() }
