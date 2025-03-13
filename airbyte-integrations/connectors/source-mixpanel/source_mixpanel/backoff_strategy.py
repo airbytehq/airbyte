9da31b0012aece015b702e49789c91a893ff46cf
darynaishchenko <darina.ishchenko17@gmail.com>
2025-03-13 13:46:59 +0200

    removed unused backoff strategy

--- airbyte-integrations/connectors/source-mixpanel/source_mixpanel/backoff_strategy.py
+++ airbyte-integrations/connectors/source-mixpanel/source_mixpanel/backoff_strategy.py
@@ -3,31 +3,10 @@
 #
 
 from datetime import timedelta
-from typing import Any, Optional, Union
 
-import requests
-
-from airbyte_cdk import BackoffStrategy
 from airbyte_cdk.sources.streams.call_rate import APIBudget, HttpRequestMatcher, MovingWindowCallRatePolicy, Rate
-from airbyte_cdk.sources.streams.http import HttpStream
 
 
 DEFAULT_API_BUDGET = APIBudget(
     policies=[MovingWindowCallRatePolicy(rates=[Rate(limit=60, interval=timedelta(hours=1.0))], matchers=[HttpRequestMatcher()])]
 )
-
-
-class MixpanelStreamBackoffStrategy(BackoffStrategy):
-    def __init__(self, stream: HttpStream, **kwargs):  # type: ignore # noqa
-        self.stream = stream
-        super().__init__(**kwargs)
-
-    def backoff_time(
-        self, response_or_exception: Optional[Union[requests.Response, requests.RequestException]], **kwargs: Any
-    ) -> Optional[float]:
-        if isinstance(response_or_exception, requests.Response):
-            retry_after = response_or_exception.headers.get("Retry-After")
-            if retry_after:
-                self._logger.debug(f"API responded with `Retry-After` header: {retry_after}")
-                return float(retry_after)
-        return None
