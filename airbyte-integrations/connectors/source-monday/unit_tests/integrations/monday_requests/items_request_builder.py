96d8d0bd6fa36f4588262fd98cb87ec05cc49b5b
David Gold <32782137+dbgold17@users.noreply.github.com>
2025-03-14 10:28:18 -0700

    small test clarity refactor

--- airbyte-integrations/connectors/source-monday/unit_tests/integrations/monday_requests/items_request_builder.py
+++ airbyte-integrations/connectors/source-monday/unit_tests/integrations/monday_requests/items_request_builder.py
@@ -15,13 +15,13 @@ class ItemsRequestBuilder(MondayBaseRequestBuilder):
     def query_params(self):
         params = super().query_params or {}
         if self._board_ids:
-            board_ids_str = ", ".join(list(map(str, self._board_ids)))
-            params["query"] = (
-                "query{boards(limit:1,ids:[%s]){items_page(limit:20){cursor,items{id,name,assets{created_at,file_extension,file_size,id,name,original_geometry,public_url,uploaded_by{id},url,url_thumbnail},board{id,name},column_values{id,text,type,value,... on MirrorValue{display_value},... on BoardRelationValue{display_value},... on DependencyValue{display_value}},created_at,creator_id,group{id},parent_item{id},state,subscribers{id},updated_at,updates{id}}}}}"
-                % board_ids_str
-            )
+            board_ids = ", ".join(list(map(str, self._board_ids)))
+            board_ids_str = f",ids:[{board_ids}]"
         else:
-            params["query"] = (
-                "query{boards(limit:1){items_page(limit:20){cursor,items{id,name,assets{created_at,file_extension,file_size,id,name,original_geometry,public_url,uploaded_by{id},url,url_thumbnail},board{id,name},column_values{id,text,type,value,... on MirrorValue{display_value},... on BoardRelationValue{display_value},... on DependencyValue{display_value}},created_at,creator_id,group{id},parent_item{id},state,subscribers{id},updated_at,updates{id}}}}}"
-            )
+            board_ids_str = ""
+
+        params["query"] = (
+            "query{boards(limit:1%s){items_page(limit:20){cursor,items{id,name,assets{created_at,file_extension,file_size,id,name,original_geometry,public_url,uploaded_by{id},url,url_thumbnail},board{id,name},column_values{id,text,type,value,... on MirrorValue{display_value},... on BoardRelationValue{display_value},... on DependencyValue{display_value}},created_at,creator_id,group{id},parent_item{id},state,subscribers{id},updated_at,updates{id}}}}}"
+            % board_ids_str
+        )
         return params
