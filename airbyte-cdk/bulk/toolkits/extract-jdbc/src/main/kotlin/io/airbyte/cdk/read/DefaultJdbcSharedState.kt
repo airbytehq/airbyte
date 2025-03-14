6672726dee04fc303b2eb6e3af6cd787ca84bba2
Rodi Reich Zilberman <867491+rodireich@users.noreply.github.com>
2025-03-14 12:05:23 -0700

    max 15gb

--- airbyte-cdk/bulk/toolkits/extract-jdbc/src/main/kotlin/io/airbyte/cdk/read/DefaultJdbcSharedState.kt
+++ airbyte-cdk/bulk/toolkits/extract-jdbc/src/main/kotlin/io/airbyte/cdk/read/DefaultJdbcSharedState.kt
@@ -73,7 +73,7 @@ class DefaultJdbcSharedState(
                 .mapToLong { it.toFile().length() }
                 .sum()
 //            log.info { "Total size of files in /staging/files: $size bytes" }
-            if (size > (3.5 * (1L shl 30)).toLong()) { // 3.5GB
+            if (size > 15L shl 30) { // 3GB
                 log.info { "staging dir too full $size" }
                 return null
             }
