59a25c4562ba82cb51b3661ef660ab6f876292b2
Tope Folorunso <folorunsotemitope10@gmail.com>
2025-03-16 17:15:23 +0100

    fix async retriever

--- airbyte-integrations/connectors/source-youtube-analytics/components.py
+++ airbyte-integrations/connectors/source-youtube-analytics/components.py
@@ -3,10 +3,11 @@
 #
 
 from dataclasses import dataclass
-from typing import Any, Mapping, Optional, Union
+from typing import Any, Iterable, Mapping, MutableMapping, Optional, Union
 
 import requests
 
+from airbyte_cdk.sources.declarative.extractors.record_extractor import RecordExtractor
 from airbyte_cdk.sources.declarative.requesters.error_handlers import DefaultErrorHandler
 from airbyte_cdk.sources.declarative.requesters.http_requester import HttpRequester
 from airbyte_cdk.sources.declarative.requesters.request_options.interpolated_request_options_provider import (
@@ -29,35 +30,58 @@ class CreationRequester(HttpRequester):
         super().__post_init__(parameters)
 
     def send_request(self, **kwargs):
+        jobs_response = self.get_jobs_response()
+        stream_name = self.name.split(" - ")[-1]
+        if stream_name not in [job["reportTypeId"] for job in jobs_response.json().get("jobs", [])]:
+            super().send_request(**kwargs)
+            jobs_response = self.get_jobs_response()
+        return jobs_response
+        
+    def get_jobs_response(self):
         request, jobs_response = self._http_client.send_request(
             http_method="GET",
             url=self._join_url(self.get_url_base(), "jobs"),
             request_kwargs={"stream": self.stream_response},
         )
-        jobs_list = jobs_response.json().get("jobs", [])
-        stream_name = self.name.split(" - ")[-1]
-        if stream_name in [job["reportTypeId"] for job in jobs_list]:
-            return jobs_response
-        else:
-            return super().send_request(**kwargs)
+        return jobs_response
 
 
 @dataclass
 class PollingRequester(HttpRequester):
     def send_request(self, **kwargs):
+        stream_name = self.name.split(" - ")[-1]
+        
         jobs_response = super().send_request(**kwargs)
         jobs_list = jobs_response.json().get("jobs", [])
-        job_resource = [job for job in jobs_list if job["reportTypeId"] == self.name][0]
+        job_resource = list(filter(lambda job: job["reportTypeId"] == stream_name, jobs_list))[0]
         job_id = job_resource["id"]
 
         request, reports_response = self._http_client.send_request(
             http_method=self.get_method().value,
             url=self._join_url(self.get_url_base(), f"jobs/{job_id}/reports"),
             request_kwargs={"stream": self.stream_response},
-            params={"startTimeAtOrAfter": self.start_time} if self.start_time else {},
+            # params={"startTimeAtOrAfter": self.start_time} if self.start_time else {},
         )
         return reports_response
 
+
+@dataclass
+class StatusExtractor(RecordExtractor):
+    def extract_records(self, response: requests.Response) -> Iterable[MutableMapping[Any, Any]]:
+        reports = response.json().get("reports", [])
+        if not reports:
+            yield "running"
+        else:
+            yield "completed"
+            
+
+@dataclass
+class DownloadTargetExtractor(RecordExtractor):
+    def extract_records(self, response: requests.Response) -> Iterable[MutableMapping[Any, Any]]:
+        reports = response.json().get("reports", [])
+        urls = list(map(lambda report: report.get("downloadUrl", ""), reports))
+        return urls
+            
 
 @dataclass
 class YoutubeAnalyticsErrorHandler(DefaultErrorHandler):
