fed842f87162abb2a6bbae06896a9e334624c375
Tope Folorunso <folorunsotemitope10@gmail.com>
2025-03-20 03:52:44 +0100

    update pr

--- airbyte-integrations/connectors/source-instagram/unit_tests/integration/test_media_insights.py
+++ airbyte-integrations/connectors/source-instagram/unit_tests/integration/test_media_insights.py
@@ -74,7 +74,7 @@ _MEDIA_IDS = {
     GENERAL_MEDIA: MEDIA_ID_GENERAL_MEDIA,
 }
 
-METRICS_GENERAL_MEDIA = ["impressions", "reach", "saved", "likes", "comments", "shares", "follows", "profile_visits"]
+METRICS_GENERAL_MEDIA = ["reach", "saved", "likes", "comments", "shares", "follows", "profile_visits"]
 
 _METRICS = {
     MEDIA_ID_REELS: [
@@ -86,12 +86,10 @@ _METRICS = {
         "reach",
         "saved",
         "shares",
-        "ig_reels_aggregated_all_plays_count",
-        "clips_replays_count",
     ],
-    MEDIA_ID_VIDEO_FEED: ["impressions", "reach", "saved"],
-    MEDIA_ID_VIDEO: ["impressions", "reach", "saved", "likes", "comments", "shares", "follows", "profile_visits"],
-    MEDIA_ID_CAROUSEL_ALBUM: ["impressions", "reach", "saved", "shares", "follows", "profile_visits"],
+    MEDIA_ID_VIDEO_FEED: ["reach", "saved"],
+    MEDIA_ID_VIDEO: ["reach", "saved", "likes", "comments", "shares", "follows", "profile_visits"],
+    MEDIA_ID_CAROUSEL_ALBUM: ["reach", "saved", "shares", "follows", "profile_visits"],
     MEDIA_ID_GENERAL_MEDIA: METRICS_GENERAL_MEDIA,
     # Reusing general media metrics for error scenarios
     MEDIA_ID_ERROR_POSTED_BEFORE_BUSINESS: METRICS_GENERAL_MEDIA,
