6baa701aed96c5a32d1fb87f99d7bd784db8ee86
Anatolii Yatsuk <tolikyatsuk@gmail.com>
2025-03-21 15:55:14 +0200

    Fix userErrors in bulkOperationRunQuery

--- airbyte-integrations/connectors/source-shopify/source_shopify/shopify_graphql/bulk/job.py
+++ airbyte-integrations/connectors/source-shopify/source_shopify/shopify_graphql/bulk/job.py
@@ -5,6 +5,7 @@
 from dataclasses import dataclass, field
 from datetime import datetime
 from time import sleep, time
+from enum import Enum
 from typing import Any, Final, Iterable, List, Mapping, Optional
 
 import pendulum as pdm
@@ -22,6 +23,14 @@ from .retry import bulk_retry_on_exception
 from .status import ShopifyBulkJobStatus
 from .tools import END_OF_FILE, BulkTools
 
+
+class BulkOperationUserErrorCode(Enum):
+    """
+    Possible error codes that can be returned by BulkOperationUserError.
+    https://shopify.dev/docs/api/admin-graphql/latest/enums/BulkOperationUserErrorCode
+    """
+    INVALID = "INVALID"
+    OPERATION_IN_PROGRESS = "OPERATION_IN_PROGRESS"
 
 @dataclass
 class ShopifyBulkManager:
@@ -417,18 +426,17 @@ class ShopifyBulkManager:
         Error example:
         [
             {
+                'code': 'OPERATION_IN_PROGRESS',
                 'field': None,
                 'message': 'A bulk query operation for this app and shop is already in progress: gid://shopify/BulkOperation/4039184154813.',
             }
         ]
         """
-
-        concurrent_job_pattern = "A bulk query operation for this app and shop is already in progress"
         # the errors are handled in `job_job_check_for_errors`
         if errors:
             for error in errors:
-                message = error.get("message", "") if isinstance(error, dict) else ""
-                if concurrent_job_pattern in message:
+                error_code = error.get("code", "") if isinstance(error, dict) else ""
+                if error_code == BulkOperationUserErrorCode.OPERATION_IN_PROGRESS.value:
                     return True
         return False
 
--- airbyte-integrations/connectors/source-shopify/source_shopify/shopify_graphql/bulk/query.py
+++ airbyte-integrations/connectors/source-shopify/source_shopify/shopify_graphql/bulk/query.py
@@ -68,6 +68,7 @@ class ShopifyBulkTemplates:
                         createdAt
                     }
                     userErrors {
+                        code
                         field
                         message
                     }
