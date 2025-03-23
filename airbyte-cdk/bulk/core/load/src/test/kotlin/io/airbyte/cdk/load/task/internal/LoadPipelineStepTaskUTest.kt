e8724fa1263da55a19d2271c8db03f7fec85dad8
Johnny Schmidt <john.schmidt@airbyte.io>
2025-03-22 17:04:27 -0700

    Load CDK: Insert Loader Interface

--- airbyte-cdk/bulk/core/load/src/test/kotlin/io/airbyte/cdk/load/task/internal/LoadPipelineStepTaskUTest.kt
+++ airbyte-cdk/bulk/core/load/src/test/kotlin/io/airbyte/cdk/load/task/internal/LoadPipelineStepTaskUTest.kt
@@ -423,7 +423,11 @@ class LoadPipelineStepTaskUTest {
                             return TestKey(output, inputKey.stream)
                         }
 
-                        override fun getPart(outputKey: TestKey, numParts: Int): Int {
+                        override fun getPart(
+                            outputKey: TestKey,
+                            inputPart: Int,
+                            numParts: Int
+                        ): Int {
                             if (outputKey.output) return 1
                             return 0
                         }
