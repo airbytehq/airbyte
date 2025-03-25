9ac8aad729977dedd782b23865c24a6cc40c6fab
Satish Chinthanippu <satish.chinthanippu@teradata.com>
2025-03-25 09:30:57 -0700

    format applied

--- airbyte-integrations/connectors/destination-teradata/src/test/kotlin/io/airbyte/integrations/destination/teradata/util/JSONStructTest.kt
+++ airbyte-integrations/connectors/destination-teradata/src/test/kotlin/io/airbyte/integrations/destination/teradata/util/JSONStructTest.kt
@@ -11,7 +11,6 @@ import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 
-
 /** Unit tests for the JSONStruct class. */
 class JSONStructTest {
 
