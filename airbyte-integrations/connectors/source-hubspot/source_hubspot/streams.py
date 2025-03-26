e25d754c63f982dc08a7c1bf39e1f410944eb5e9
Oleksandr Bazarnov <oleksandr.bazarnov@globallogic.com>
2025-03-26 15:12:30 +0200

    formatted

--- airbyte-integrations/connectors/source-hubspot/source_hubspot/streams.py
+++ airbyte-integrations/connectors/source-hubspot/source_hubspot/streams.py
@@ -1881,15 +1881,13 @@ class Forms(ClientSideIncrementalStream):
     ) -> MutableMapping[str, Any]:
         params = super().request_params(stream_state, stream_slice, next_page_token)
         params["formTypes"] = [
-            "hubspot", 
-            "captured", 
+            "hubspot",
+            "captured",
             "flow",
-            
             # Not supported by `v1` api version used by the FormSubmissions stream
             # TODO: consider migrating FormSubmissions stream to use `/v3` api version
             # then uncomment the last 2 form types.
-            
-            # "blog_comment", 
+            # "blog_comment",
             # "all",
         ]
         return params
