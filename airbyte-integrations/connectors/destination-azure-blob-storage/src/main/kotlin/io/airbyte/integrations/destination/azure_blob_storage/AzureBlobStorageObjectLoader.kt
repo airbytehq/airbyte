e7c4f4a72f35a9fdc52e51f3af4a524f3fd89ad0
Edward Gao <edward.gao@airbyte.io>
2025-03-26 11:22:31 -0700

    derp, add partitioner

--- airbyte-integrations/connectors/destination-azure-blob-storage/src/main/kotlin/io/airbyte/integrations/destination/azure_blob_storage/AzureBlobStorageObjectLoader.kt
+++ airbyte-integrations/connectors/destination-azure-blob-storage/src/main/kotlin/io/airbyte/integrations/destination/azure_blob_storage/AzureBlobStorageObjectLoader.kt
@@ -4,8 +4,11 @@
 
 package io.airbyte.integrations.destination.azure_blob_storage
 
+import io.airbyte.cdk.load.pipeline.RoundRobinInputPartitioner
 import io.airbyte.cdk.load.write.object_storage.ObjectLoader
 import javax.inject.Singleton
 
 // TODO we'll eventually want to override various performance values here, maybe
 @Singleton class AzureBlobStorageObjectLoader : ObjectLoader
+
+@Singleton class AzureRoundRobinInputPartitioner : RoundRobinInputPartitioner()
