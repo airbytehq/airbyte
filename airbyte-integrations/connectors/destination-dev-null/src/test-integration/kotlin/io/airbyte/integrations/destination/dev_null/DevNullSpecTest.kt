f51fb69a9851edb2e9eab42b8b9b6e8cd0886392
Wenqi Hu <wenqi.huu@gmail.com>
2025-03-26 14:14:18 -0700

    temp

--- airbyte-integrations/connectors/destination-dev-null/src/test-integration/kotlin/io/airbyte/integrations/destination/dev_null/DevNullSpecTest.kt
+++
@@ -1,9 +0,0 @@
-/*
- * Copyright (c) 2024 Airbyte, Inc., all rights reserved.
- */
-
-package io.airbyte.integrations.destination.dev_null
-
-import io.airbyte.cdk.load.spec.SpecTest
-
-class DevNullSpecTest : SpecTest()
