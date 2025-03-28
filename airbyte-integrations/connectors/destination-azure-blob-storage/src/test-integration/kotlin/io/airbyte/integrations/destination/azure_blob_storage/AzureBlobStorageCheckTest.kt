8a54e47b2f4ef3eb5f7d7203498fb1b0afb428b3
Edward Gao <edward.gao@airbyte.io>
2025-03-28 10:52:25 -0700

    check test wiring

--- airbyte-integrations/connectors/destination-azure-blob-storage/src/test-integration/kotlin/io/airbyte/integrations/destination/azure_blob_storage/AzureBlobStorageCheckTest.kt
+++ airbyte-integrations/connectors/destination-azure-blob-storage/src/test-integration/kotlin/io/airbyte/integrations/destination/azure_blob_storage/AzureBlobStorageCheckTest.kt
@@ -1,10 +1,20 @@
+/*
+ * Copyright (c) 2025 Airbyte, Inc., all rights reserved.
+ */
+
 package io.airbyte.integrations.destination.azure_blob_storage
 
 import io.airbyte.cdk.load.check.CheckIntegrationTest
 import io.airbyte.cdk.load.check.CheckTestConfig
+import io.airbyte.cdk.load.command.object_storage.CSVFormatSpecification
+import io.airbyte.cdk.load.command.object_storage.JsonFormatSpecification
 
 class AzureBlobStorageCheckTest :
     CheckIntegrationTest<AzureBlobStorageSpecification>(
-        successConfigFilenames = listOf(CheckTestConfig(AzureBlobStorageTestUtil.configPath)),
+        successConfigFilenames =
+            listOf(
+                CheckTestConfig(AzureBlobStorageTestUtil.getConfig(JsonFormatSpecification())),
+                CheckTestConfig(AzureBlobStorageTestUtil.getConfig(CSVFormatSpecification()))
+            ),
         failConfigFilenamesAndFailureReasons = emptyMap(),
     )
