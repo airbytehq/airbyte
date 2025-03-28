74ed88c8a510530386a8a31c0d506a96b8a1b949
Octavia Squidington III <octavia-squidington-iii@users.noreply.github.com>
2025-03-28 20:41:12 +0000

    chore: auto-fix lint and format issues

--- airbyte-integrations/connectors/source-google-search-console/source_google_search_console/source.py
+++ airbyte-integrations/connectors/source-google-search-console/source_google_search_console/source.py
@@ -185,14 +185,14 @@ class SourceGoogleSearchConsole(AbstractSource):
             SearchAnalyticsByPage(**stream_config),
             SearchAnalyticsAllFields(**stream_config),
             SearchAnalyticsKeywordPageReport(**stream_config),
-            SearchAnalyticsKeywordPageReportMinimalDimensions(**stream_config), 
+            SearchAnalyticsKeywordPageReportMinimalDimensions(**stream_config),
             SearchAnalyticsPageReport(**stream_config),
             SearchAnalyticsSiteReportBySite(**stream_config),
             SearchAnalyticsSiteReportByPage(**stream_config),
             SearchAnalyticsKeywordSiteReportByPage(**stream_config),
             SearchAnalyticsKeywordSiteReportByPageMinimalDimensions(**stream_config),
             SearchAnalyticsKeywordSiteReportBySite(**stream_config),
-            SearchAnalyticsKeywordSiteReportBySiteMinimalDimensions(**stream_config),  
+            SearchAnalyticsKeywordSiteReportBySiteMinimalDimensions(**stream_config),
         ]
 
         streams = streams + self.get_custom_reports(config=config, stream_config=stream_config)
--- airbyte-integrations/connectors/source-google-search-console/source_google_search_console/streams.py
+++ airbyte-integrations/connectors/source-google-search-console/source_google_search_console/streams.py
@@ -412,29 +412,35 @@ class SearchAnalyticsKeywordPageReport(SearchByKeyword):
     primary_key = ["site_url", "date", "country", "device", "query", "page", "search_type"]
     dimensions = ["date", "country", "device", "query", "page"]
 
+
 class SearchAnalyticsKeywordPageReportMinimalDimensions(SearchByKeyword):
     primary_key = ["site_url", "date", "country", "device", "query", "page", "search_type"]
     dimensions = ["date"]
+
 
 class SearchAnalyticsKeywordSiteReportByPage(SearchByKeyword):
     primary_key = ["site_url", "date", "country", "device", "query", "search_type"]
     dimensions = ["date", "country", "device", "query"]
     aggregation_type = QueryAggregationType.by_page
 
+
 class SearchAnalyticsKeywordSiteReportByPageMinimalDimensions(SearchByKeyword):
     primary_key = ["site_url", "date", "country", "device", "query", "search_type"]
     dimensions = ["date"]
     aggregation_type = QueryAggregationType.by_page
+
 
 class SearchAnalyticsKeywordSiteReportBySite(SearchByKeyword):
     primary_key = ["site_url", "date", "country", "device", "query", "search_type"]
     dimensions = ["date", "country", "device", "query"]
     aggregation_type = QueryAggregationType.by_property
 
+
 class SearchAnalyticsKeywordSiteReportBySiteMinimalDimensions(SearchByKeyword):
     primary_key = ["site_url", "date", "country", "device", "query", "search_type"]
     dimensions = ["date"]
     aggregation_type = QueryAggregationType.by_property
+
 
 class SearchAnalyticsSiteReportBySite(SearchAnalytics):
     primary_key = ["site_url", "date", "country", "device", "search_type"]
