b3454bd18010596bb17f017b10273fb1c5854979
Francis Genet <francis.genet@airbyte.io>
2025-03-31 10:30:16 -0700

    Adding the AzureBlobStorageChecker

--- airbyte-integrations/connectors/destination-azure-blob-storage/src/main/kotlin/io/airbyte/integrations/destination/azure_blob_storage/AzureBlobStorageChecker.kt
+++ airbyte-integrations/connectors/destination-azure-blob-storage/src/main/kotlin/io/airbyte/integrations/destination/azure_blob_storage/AzureBlobStorageChecker.kt
@@ -5,13 +5,58 @@
 package io.airbyte.integrations.destination.azure_blob_storage
 
 import io.airbyte.cdk.load.check.DestinationChecker
-import io.airbyte.cdk.load.command.DestinationConfiguration
+import io.airbyte.cdk.load.file.azureBlobStorage.AzureBlobStorageClientFactory
+import java.io.OutputStream
 import javax.inject.Singleton
+import kotlinx.coroutines.runBlocking
 
-// TODO use actual azure config object once it exists
 @Singleton
-class AzureBlobStorageChecker : DestinationChecker<DestinationConfiguration> {
-    override fun check(config: DestinationConfiguration) {
-        // TODO check the config
+class AzureBlobStorageChecker<T : OutputStream> :
+    DestinationChecker<AzureBlobStorageConfiguration<T>> {
+    override fun check(config: AzureBlobStorageConfiguration<T>) {
+        val azureBlobClient = AzureBlobStorageClientFactory(config).make()
+
+        // Prepare test data
+        val testData = "test".toByteArray()
+        val checkFilePath = "check_test_data"
+
+        runBlocking {
+            // 1) Upload blob
+            val checkBlob = azureBlobClient.put(checkFilePath, testData)
+
+            try {
+                // 2) Verify the blob exists by attempting to get metadata
+                val metadata =
+                    try {
+                        azureBlobClient.getMetadata(checkFilePath)
+                        true // If we reach this point, the blob exists
+                    } catch (e: Exception) {
+                        false // The blob doesn't exist
+                    }
+
+                check(metadata) { "Failed to verify blob existence after upload" }
+
+                // 3) Download and verify content
+                azureBlobClient.get(checkFilePath) { inputStream ->
+                    val downloadedContent = inputStream.readBytes().toString(Charsets.UTF_8)
+                    check(downloadedContent == "test") {
+                        "Downloaded content doesn't match uploaded content"
+                    }
+                }
+
+                // 4) List blobs to verify the blob appears in the container
+                var blobFound = false
+                azureBlobClient.list(checkFilePath.substringBefore("/")).collect { blob ->
+                    if (blob.key == checkFilePath) {
+                        blobFound = true
+                    }
+                }
+
+                check(blobFound) { "Uploaded blob not found in blob listing" }
+            } finally {
+                // 5) Clean up remote files
+                azureBlobClient.delete(checkBlob)
+            }
+        }
     }
 }
