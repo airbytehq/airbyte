ff5efc6a5f78f99b0736a7aea454d272208b1ef3
Edward Gao <edward.gao@airbyte.io>
2025-04-02 12:06:09 -0700

    Bulk load CDK: GCS toolkit spec stuff (#56956)

---
+++ airbyte-cdk/bulk/toolkits/load-gcs/src/main/kotlin/io/airbyte/cdk/load/command/gcs/GcsCommonSpecification.kt
@@ -0,0 +1,29 @@
+/*
+ * Copyright (c) 2024 Airbyte, Inc., all rights reserved.
+ */
+
+package io.airbyte.cdk.load.command.gcs
+
+import com.fasterxml.jackson.annotation.JsonProperty
+import com.fasterxml.jackson.annotation.JsonPropertyDescription
+import com.kjetland.jackson.jsonSchema.annotations.JsonSchemaInject
+import com.kjetland.jackson.jsonSchema.annotations.JsonSchemaTitle
+
+/** Represents basic GCS options. You may also want to extend [GcsRegionSpecification]. */
+interface GcsCommonSpecification {
+    @get:JsonSchemaTitle("GCS Bucket Name")
+    @get:JsonPropertyDescription(
+        """The name of the GCS bucket. Read more <a href="https://cloud.google.com/storage/docs/naming-buckets">here</a>."""
+    )
+    @get:JsonProperty("gcs_bucket_name")
+    @get:JsonSchemaInject(json = """{"examples":["airbyte_sync"]}""")
+    val gcsBucketName: String
+
+    @get:JsonSchemaTitle("GCS Bucket Path")
+    @get:JsonPropertyDescription("Directory under the GCS bucket where data will be written.")
+    @get:JsonProperty("gcs_bucket_path")
+    @get:JsonSchemaInject(json = """{"examples":["data_sync/test"]}""")
+    val path: String
+
+    @get:JsonSchemaTitle("Credential") val credential: GcsAuthSpecification
+}
