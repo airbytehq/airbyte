6a5dd5e1e40a8f7f3c8cdfce0ebca4f5317bfec8
Johnny Schmidt <john.schmidt@airbyte.io>
2025-04-03 12:37:43 -0700

    added microbatch details

--- airbyte-cdk/bulk/core/load/src/main/kotlin/io/airbyte/cdk/load/pipeline/DefaultPipelineFlushStrategy.kt
+++ airbyte-cdk/bulk/core/load/src/main/kotlin/io/airbyte/cdk/load/pipeline/DefaultPipelineFlushStrategy.kt
@@ -17,6 +17,12 @@ import jakarta.inject.Singleton
  * NOTE: If you override this, you must at least provide the microbatch behavior to keep tests from
  * breaking. TODO: make the pipeline inject the microbatching no matter what, so no dev has to think
  * about it.
+ *
+ * Microbatching means finishing work for every record. We do this in integration tests to guarantee
+ * that state gets flushed, so multi-sync tests that test recovery can wait for a state ack before
+ * killing the connector. (Otherwise they might hang forever.) We can probably get rid of this if we
+ * can better guarantee that all in-flight work is flushed during exception handling:
+ * https://github.com/airbytehq/airbyte-internal-issues/issues/12310
  */
 @Singleton
 class DefaultPipelineFlushStrategy(
