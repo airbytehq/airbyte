7703792a1b305dc83f1ff359e8b4e83ce2e6102f
Alexander Günther <alexander.guenther@mayflower.de>
2025-04-10 13:25:45 +0200

    removed testconnectors

--- airbyte-integrations/connectors/destination-testconnector/destination_testconnector/__init__.py
+++
@@ -1,8 +0,0 @@
-#
-# Copyright (c) 2025 Airbyte, Inc., all rights reserved.
-#
-
-
-from .destination import DestinationTestconnector
-
-__all__ = ["DestinationTestconnector"]
--- airbyte-integrations/connectors/destination-testconnector/integration_tests/integration_test.py
+++
@@ -1,8 +0,0 @@
-#
-# Copyright (c) 2025 Airbyte, Inc., all rights reserved.
-#
-
-
-def integration_test():
-    # TODO write integration tests
-    pass
