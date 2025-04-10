78e56f748a71f09d87f9c111e0890878890a9e74
Aaron Steers <aj@airbyte.io>
2025-04-10 12:32:54 -0700

    add `test_airbyte_standards.py'

---
+++ airbyte-integrations/connectors/source-hubspot/unit_tests/test_airbyte_standards.py
@@ -0,0 +1,47 @@
+"""Airbyte standard connector tests.
+
+The AST (Airbyte Standard Tests) suite is designed to ensure that connectors meet Airbyte
+protocol standards.
+"""
+
+from pathlib import Path
+
+from source_hubspot import SourceHubspot
+
+from airbyte_cdk.test.declarative.test_suites import (
+    ConnectorTestScenario,
+    SourceTestSuiteBase,
+    generate_tests,
+)
+
+
+CONNECTOR_ROOT = Path(__file__).parent.parent
+
+
+def get_file_path(file_name: str) -> Path:
+    """Get the path to a resource file."""
+    return CONNECTOR_ROOT / file_name
+
+
+def pytest_generate_tests(metafunc) -> None:
+    generate_tests(metafunc)
+
+
+class TestAirbyteStandardTests(SourceTestSuiteBase):
+    """Test suite for the Airbyte standard tests.
+
+    This class inherits from SourceTestSuiteBase and implements all of the tests in the suite.
+
+    As long as the class name starts with "Test", pytest will automatically discover and run the
+    tests in this class.
+    """
+
+    connector = SourceHubspot
+    working_dir = CONNECTOR_ROOT
+    acceptance_test_config_path = get_file_path("acceptance-test-config.yml")
+
+    @classmethod
+    def create_connector(cls, scenario: ConnectorTestScenario) -> SourceHubspot:
+        """Create a new instance of the connector."""
+
+        return SourceHubspot()
