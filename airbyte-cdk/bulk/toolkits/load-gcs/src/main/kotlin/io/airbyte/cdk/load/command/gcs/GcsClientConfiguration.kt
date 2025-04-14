66b38b21e501b26b7a77d93fac382e2077cb4920
Edward Gao <edward.gao@airbyte.io>
2025-04-14 12:05:22 -0700

    Bulk Load CDK: add load-gcs toolkit (#56953)
    Co-authored-by: Francis Genet <francis.genet@airbyte.io>

---
+++ airbyte-cdk/bulk/toolkits/load-gcs/src/main/kotlin/io/airbyte/cdk/load/command/gcs/GcsClientConfiguration.kt
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) 2025 Airbyte, Inc., all rights reserved.
+ */
+
+package io.airbyte.cdk.load.command.gcs
+
+data class GcsClientConfiguration(
+    val gcsBucketName: String,
+    val path: String,
+    val credential: GcsAuthConfiguration,
+    val region: GcsRegion?,
+) {
+    constructor(
+        commonSpecification: GcsCommonSpecification,
+        regionSpecification: GcsRegion,
+    ) : this(
+        commonSpecification.gcsBucketName,
+        commonSpecification.path,
+        commonSpecification.credential.toGcsAuthConfiguration(),
+        regionSpecification,
+    )
+}
+
+interface GcsClientConfigurationProvider {
+    val gcsClientConfiguration: GcsClientConfiguration
+}
