0715fceee6e217220c239f7216f4da67f445cfd9
Francis Genet <francis.genet@airbyte.io>
2025-04-14 15:09:26 -0700

    Better if we use CSV it looks like

--- airbyte-integrations/connectors/destination-bigquery/src/main/kotlin/io/airbyte/integrations/destination/bigquery/BigQueryBulkLoader.kt
+++ airbyte-integrations/connectors/destination-bigquery/src/main/kotlin/io/airbyte/integrations/destination/bigquery/BigQueryBulkLoader.kt
@@ -5,14 +5,16 @@
 package io.airbyte.integrations.destination.bigquery
 
 import com.google.cloud.bigquery.BigQuery
+import com.google.cloud.bigquery.FormatOptions
 import com.google.cloud.bigquery.JobInfo
-import com.google.cloud.bigquery.QueryJobConfiguration
+import com.google.cloud.bigquery.LoadJobConfiguration
 import io.airbyte.cdk.load.command.DestinationCatalog
 import io.airbyte.cdk.load.command.DestinationStream
 import io.airbyte.cdk.load.file.s3.S3Object
 import io.airbyte.cdk.load.message.StreamKey
 import io.airbyte.cdk.load.write.db.BulkLoader
 import io.airbyte.cdk.load.write.db.BulkLoaderFactory
+import io.airbyte.integrations.destination.bigquery.formatter.BigQueryRecordFormatter
 import io.airbyte.integrations.destination.bigquery.spec.BigqueryConfiguration
 import io.airbyte.integrations.destination.bigquery.write.TempUtils
 import jakarta.inject.Singleton
@@ -25,78 +27,30 @@ class BigQueryBulkLoader(
     override suspend fun load(remoteObject: S3Object) {
         val rawTableId = TempUtils.rawTableId(bigQueryConfiguration, stream.descriptor)
 
-        // Create a temporary external table pointing to the JSONL file
-        val createExternalTableSql =
-            """
-            CREATE OR REPLACE EXTERNAL TABLE `${rawTableId.dataset}.temp_external_table`
-            OPTIONS (
-              format = 'NEWLINE_DELIMITED_JSON',
-              uris = ['gs://${remoteObject.keyWithBucketName}']
-            )
-            """.trimIndent()
+        BigQueryRecordFormatter
 
-        // Execute the external table creation
-        val createTableJob =
-            bigQueryClient
-                .create(
-                    JobInfo.newBuilder(
-                            QueryJobConfiguration.newBuilder(createExternalTableSql)
-                                .setUseLegacySql(false)
-                                .build()
-                        )
-                        .build()
-                )
-                .waitFor()
+        // Use LoadJobConfiguration API approach instead of SQL
+        val gcsUri = "gs://${remoteObject.keyWithBucketName}"
 
-        if (createTableJob.status.error != null) {
+        // Configure the load job for CSV format
+        val configuration =
+            LoadJobConfiguration.builder(rawTableId, gcsUri)
+                .setFormatOptions(FormatOptions.csv())
+                .setSchema(BigQueryRecordFormatter.SCHEMA_V2)
+                .setWriteDisposition(JobInfo.WriteDisposition.WRITE_APPEND)
+                .setJobTimeoutMs(600000L) // 10 min timeout
+                .build()
+
+        // Create and execute the load job
+        val loadJob = bigQueryClient.create(JobInfo.of(configuration))
+
+        try {
+            BigQueryUtils.waitForJobFinish(loadJob)
+        } catch (e: Exception) {
             throw RuntimeException(
-                "Failed to create external table: ${createTableJob.status.error}"
+                "Failed to load CSV data from $gcsUri to table ${rawTableId.dataset}.${rawTableId.table}: ${e.message}",
+                e
             )
-        }
-
-        // Insert data from external table to destination table
-        val insertSql =
-            """
-            INSERT INTO `${rawTableId.dataset}.${rawTableId.table}`
-            SELECT * FROM `${rawTableId.dataset}.temp_external_table`
-            """.trimIndent()
-
-        val insertJob =
-            bigQueryClient
-                .create(
-                    JobInfo.newBuilder(
-                            QueryJobConfiguration.newBuilder(insertSql)
-                                .setUseLegacySql(false)
-                                .build()
-                        )
-                        .build()
-                )
-                .waitFor()
-
-        if (insertJob.status.error != null) {
-            throw RuntimeException("Failed to insert data: ${insertJob.status.error}")
-        }
-
-        // Drop the temporary table
-        val dropTableSql =
-            """
-            DROP TABLE `${rawTableId.dataset}.temp_external_table`
-            """.trimIndent()
-
-        val dropTableJob =
-            bigQueryClient
-                .create(
-                    JobInfo.newBuilder(
-                            QueryJobConfiguration.newBuilder(dropTableSql)
-                                .setUseLegacySql(false)
-                                .build()
-                        )
-                        .build()
-                )
-                .waitFor()
-
-        if (dropTableJob.status.error != null) {
-            throw RuntimeException("Failed to drop temporary table: ${dropTableJob.status.error}")
         }
     }
 
