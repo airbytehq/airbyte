7d718d00f6a25a5bcfcc417b44cf72f9513734ea
Edward Gao <edward.gao@airbyte.io>
2025-04-14 17:04:13 -0700

    rough direct-load sketch

---
+++ airbyte-cdk/bulk/toolkits/load-db/src/main/kotlin/io/airbyte/cdk/load/orchestration/direct_load_table/DirectLoadTableOperations.kt
@@ -0,0 +1,39 @@
+/*
+ * Copyright (c) 2025 Airbyte, Inc., all rights reserved.
+ */
+
+package io.airbyte.cdk.load.orchestration.direct_load_table
+
+import io.airbyte.cdk.load.command.DestinationStream
+import io.airbyte.cdk.load.orchestration.DestinationHandler
+import io.airbyte.cdk.load.orchestration.TableName
+
+class DirectLoadTableOperations(
+    private val sqlGenerator: DirectLoadSqlGenerator,
+    private val destinationHandler: DestinationHandler,
+) {
+    fun createTable(
+        stream: DestinationStream,
+        finalTableName: TableName,
+        suffix: String,
+        replace: Boolean
+    ) {
+        destinationHandler.execute(TODO())
+    }
+
+    fun alterTable(
+        stream: DestinationStream,
+        finalTableName: TableName,
+    ) {
+        // TODO we should figure out some reasonable abstraction for diffing existing+expected
+        //  table schema
+        TODO()
+    }
+
+    fun overwriteFinalTable(
+        finalTableName: TableName,
+        suffix: String,
+    ) {
+        TODO()
+    }
+}
