71a9ff31b2cda6cdab4b393e31664366ea5a8a23
tryangul <ryan.broughan@gmail.com>
2025-04-16 16:25:32 -0700

    Save output partitioner.

---
+++ airbyte-cdk/bulk/toolkits/load-object-storage/src/main/kotlin/io/airbyte/cdk/load/pipline/object_storage/file/FileCompletedOutputPartitioner.kt
@@ -0,0 +1,25 @@
+package io.airbyte.cdk.load.pipline.object_storage.file
+
+import io.airbyte.cdk.load.factory.object_storage.ObjectKey
+import io.airbyte.cdk.load.file.object_storage.RemoteObject
+import io.airbyte.cdk.load.message.StreamKey
+import io.airbyte.cdk.load.pipline.object_storage.ObjectLoaderCompletedUploadPartitioner
+import io.airbyte.cdk.load.pipline.object_storage.ObjectLoaderUploadCompleter
+import jakarta.inject.Named
+import jakarta.inject.Singleton
+
+@Singleton
+@Named("fileCompletedOutputPartitioner")
+class FileCompletedOutputPartitioner<T : RemoteObject<*>> :
+    ObjectLoaderCompletedUploadPartitioner<StreamKey, T> {
+    override fun getOutputKey(
+        inputKey: ObjectKey,
+        output: ObjectLoaderUploadCompleter.UploadResult<T>
+    ): StreamKey {
+        return StreamKey(inputKey.stream)
+    }
+
+    override fun getPart(outputKey: StreamKey, inputPart: Int, numParts: Int): Int {
+        return 0
+    }
+}
