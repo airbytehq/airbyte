e544aa00f998cf846d16b946ceed6299de3f7aed
Aaron Steers <aj@airbyte.io>
2025-04-16 10:05:31 -0700

    use more concise standard tests stub file

--- airbyte-integrations/connectors/source-hubspot/integration_tests/test_airbyte_standards.py
+++ airbyte-integrations/connectors/source-hubspot/integration_tests/test_airbyte_standards.py
@@ -7,21 +7,16 @@ protocol standards.
 """
 
 import pytest
-from source_hubspot import SourceHubspot
 
-from airbyte_cdk.test.declarative.test_suites import (
-    SourceTestSuiteBase,
-    generate_tests,
-)
+from airbyte_cdk.test import standard_tests
 
 
-def pytest_generate_tests(metafunc) -> None:
-    generate_tests(metafunc)
+pytest_plugins = ["airbyte_cdk.test.standard_tests.pytest_hooks"]
 
 
 @pytest.mark.slow
 @pytest.mark.requires_creds
-class TestAirbyteStandardTests(SourceTestSuiteBase):
+class TestAirbyteStandardTests(standard_tests.SourceTestSuiteBase):
     """Test suite for the Airbyte standard tests.
 
     This class inherits from SourceTestSuiteBase and implements all of the tests in the suite.
@@ -29,5 +24,3 @@ class TestAirbyteStandardTests(SourceTestSuiteBase):
     As long as the class name starts with "Test", pytest will automatically discover and run the
     tests in this class.
     """
-
-    connector = SourceHubspot
