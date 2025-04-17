0b888860908cd08bc611bded5b335bf76d399bb5
Davide Miranda <davide.miranda@olx.com>
2025-04-17 17:21:39 +0100

    Prevent raise of KeyError when response from https://www.googleapis.com/webmasters/v3/sites is empty

--- airbyte-integrations/connectors/source-google-search-console/source_google_search_console/source.py
+++ airbyte-integrations/connectors/source-google-search-console/source_google_search_console/source.py
@@ -151,7 +151,7 @@ class SourceGoogleSearchConsole(AbstractSource):
         if response.status_code != 200:
             raise UnidentifiedError(response.json())
 
-        remote_site_urls = {s["siteUrl"] for s in response.json()["siteEntry"]}
+        remote_site_urls = {s["siteUrl"] for s in response.json().get("siteEntry", [])}
         invalid_site_url = set(site_urls) - remote_site_urls
         if invalid_site_url:
             raise InvalidSiteURLValidationError(invalid_site_url)
