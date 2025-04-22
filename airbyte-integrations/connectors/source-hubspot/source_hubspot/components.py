08749da2f0b494ef93c070b38e6050e3ee0dc8ba
darynaishchenko <darina.ishchenko17@gmail.com>
2025-04-22 14:46:52 +0300

    add custom transformation

--- airbyte-integrations/connectors/source-hubspot/source_hubspot/components.py
+++ airbyte-integrations/connectors/source-hubspot/source_hubspot/components.py
@@ -2,8 +2,11 @@
 # Copyright (c) 2024 Airbyte, Inc., all rights reserved.
 #
 
+from dataclasses import dataclass
 from typing import Any, Dict, Optional
 
+from airbyte_cdk.sources.declarative.extractors.http_selector import HttpSelector
+from airbyte_cdk.sources.declarative.requesters.requester import Requester
 from airbyte_cdk.sources.declarative.transformations import RecordTransformation
 from airbyte_cdk.sources.types import Config, StreamSlice, StreamState
 
@@ -49,3 +52,31 @@ class NewtoLegacyFieldTransformation(RecordTransformation):
                     else:
                         if record_or_schema.get(transformed_field) is None:
                             record_or_schema[transformed_field] = value
+
+
+@dataclass
+class AddFieldsFromEndpointTransformation(RecordTransformation):
+    """
+    Makes request to provided endpoint and updates record with retrieved data.
+
+    requester: Requester
+    record_selector: HttpSelector
+    """
+
+    requester: Requester
+    record_selector: HttpSelector
+
+    def transform(
+        self,
+        record: Dict[str, Any],
+        config: Optional[Config] = None,
+        stream_state: Optional[StreamState] = None,
+        stream_slice: Optional[StreamSlice] = None,
+    ) -> None:
+        additional_data_response = self.requester.send_request(
+            stream_slice=StreamSlice(partition={"parent_id": record["id"]}, cursor_slice={})
+        )
+        additional_data = self.record_selector.select_records(response=additional_data_response, stream_state={}, records_schema={})
+
+        for data in additional_data:
+            record.update(data)
