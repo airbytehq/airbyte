bd0c68a187d679dc6b7b3b2e3b041859acd81309
parthiv11 <parthivmakwana11@gmail.com>
2025-04-23 08:25:26 +0000

    🎉 New Destination: Ragie

---
+++ airbyte-integrations/connectors/destination-ragie/main.py
@@ -0,0 +1,15 @@
+#
+# Copyright (c) 2025 Airbyte, Inc., all rights reserved.
+#
+
+
+import sys
+
+from destination_ragie import DestinationRagie
+
+if __name__ == "__main__":
+    # if len(sys.argv) > 1 and sys.argv[1] == "write":
+    #     sys.stdin = open("./secrets/input_messages_file.txt", "r")
+
+    DestinationRagie().run(sys.argv[1:])
+
