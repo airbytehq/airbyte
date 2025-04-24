7477f8562a1ab8e0beaa6bdd02c2fe40f12f905d
Edward Gao <edward.gao@airbyte.io>
2025-04-24 09:23:16 -0700

    simplify raw table name gen

--- airbyte-cdk/bulk/toolkits/load-db/src/main/kotlin/io/airbyte/cdk/load/orchestration/db/legacy_typing_deduping/TypingDedupingUtil.kt
+++ airbyte-cdk/bulk/toolkits/load-db/src/main/kotlin/io/airbyte/cdk/load/orchestration/db/legacy_typing_deduping/TypingDedupingUtil.kt
@@ -7,25 +7,18 @@ package io.airbyte.cdk.load.orchestration.db.legacy_typing_deduping
 import kotlin.math.max
 
 object TypingDedupingUtil {
-    // copied wholesale from old CDK's StreamId
     fun concatenateRawTableName(namespace: String, name: String): String {
         val plainConcat = namespace + name
-        // Pretend we always have at least one underscore, so that we never generate
-        // `_raw_stream_`
-        var longestUnderscoreRun = 1
-        var i = 0
-        while (i < plainConcat.length) {
-            // If we've found an underscore, count the number of consecutive underscores
-            var underscoreRun = 0
-            while (i < plainConcat.length && plainConcat[i] == '_') {
-                underscoreRun++
-                i++
-            }
-            longestUnderscoreRun =
-                max(longestUnderscoreRun.toDouble(), underscoreRun.toDouble()).toInt()
-            i++
-        }
+        val longestUnderscoreRun =
+            Regex("_+")
+                .findAll(plainConcat)
+                .map { it.value.length }
+                .maxOrNull()
+                // Pretend we always have at least one underscore, so that we never
+                // generate `_raw_stream_`
+                .let { max(it ?: 0, 1) }
 
-        return namespace + "_raw" + "_".repeat(longestUnderscoreRun + 1) + "stream_" + name
+        val underscores = "_".repeat(longestUnderscoreRun + 1)
+        return "${namespace}_raw${underscores}stream_$name"
     }
 }
