dd5643cb5a2d0afb9e060f1bf030d62296a6080a
Francis Genet <francis.genet@airbyte.io>
2025-04-24 11:47:51 -0700

    [BigQuery] Update the emittedAt value to be 1000 * its own value in BulkLoad (#58642)

---
+++ airbyte-integrations/connectors/destination-bigquery/src/main/kotlin/io/airbyte/integrations/destination/bigquery/write/bulk_loader/BigQueryObjectStorageFormattingWriter.kt
@@ -0,0 +1,46 @@
+/*
+ * Copyright (c) 2025 Airbyte, Inc., all rights reserved.
+ */
+
+package io.airbyte.integrations.destination.bigquery.write.bulk_loader
+
+import io.airbyte.cdk.load.command.DestinationStream
+import io.airbyte.cdk.load.command.object_storage.ObjectStorageFormatConfigurationProvider
+import io.airbyte.cdk.load.file.object_storage.CSVFormattingWriter
+import io.airbyte.cdk.load.file.object_storage.ObjectStorageFormattingWriter
+import io.airbyte.cdk.load.file.object_storage.ObjectStorageFormattingWriterFactory
+import io.airbyte.cdk.load.message.DestinationRecordRaw
+import jakarta.inject.Singleton
+import java.io.OutputStream
+
+class BigQueryObjectStorageFormattingWriter(
+    stream: DestinationStream,
+    outputStream: OutputStream,
+    rootLevelFlattening: Boolean = true,
+) :
+    ObjectStorageFormattingWriter by CSVFormattingWriter(
+        stream,
+        outputStream,
+        rootLevelFlattening
+    ) {
+
+    private val csvFormattingWriter = CSVFormattingWriter(stream, outputStream, rootLevelFlattening)
+
+    override fun accept(record: DestinationRecordRaw) {
+        record.dangerousMutateData { it.record.emittedAt *= 1000 }
+        csvFormattingWriter.accept(record)
+    }
+}
+
+@Singleton
+class BigQueryObjectStorageFormattingWriterFactory(
+    private val formatConfigProvider: ObjectStorageFormatConfigurationProvider,
+) : ObjectStorageFormattingWriterFactory {
+    override fun create(
+        stream: DestinationStream,
+        outputStream: OutputStream
+    ): ObjectStorageFormattingWriter {
+        val flatten = formatConfigProvider.objectStorageFormatConfiguration.rootLevelFlattening
+        return BigQueryObjectStorageFormattingWriter(stream, outputStream, flatten)
+    }
+}
