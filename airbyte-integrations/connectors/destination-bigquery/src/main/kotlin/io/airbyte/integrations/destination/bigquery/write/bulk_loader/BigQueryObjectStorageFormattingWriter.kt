a1ef53dcbc2c19765ff37e3374d5119c03bdc88f
Francis Genet <francis.genet@airbyte.io>
2025-04-24 09:42:18 -0700

    Using by

--- airbyte-integrations/connectors/destination-bigquery/src/main/kotlin/io/airbyte/integrations/destination/bigquery/write/bulk_loader/BigQueryObjectStorageFormattingWriter.kt
+++ airbyte-integrations/connectors/destination-bigquery/src/main/kotlin/io/airbyte/integrations/destination/bigquery/write/bulk_loader/BigQueryObjectStorageFormattingWriter.kt
@@ -6,10 +6,7 @@ package io.airbyte.integrations.destination.bigquery.write.bulk_loader
 
 import io.airbyte.cdk.load.command.DestinationStream
 import io.airbyte.cdk.load.command.object_storage.ObjectStorageFormatConfigurationProvider
-import io.airbyte.cdk.load.data.csv.toCsvRecord
-import io.airbyte.cdk.load.data.dataWithAirbyteMeta
-import io.airbyte.cdk.load.data.withAirbyteMeta
-import io.airbyte.cdk.load.file.csv.toCsvPrinterWithHeader
+import io.airbyte.cdk.load.file.object_storage.CSVFormattingWriter
 import io.airbyte.cdk.load.file.object_storage.ObjectStorageFormattingWriter
 import io.airbyte.cdk.load.file.object_storage.ObjectStorageFormattingWriterFactory
 import io.airbyte.cdk.load.message.DestinationRecordRaw
@@ -17,31 +14,21 @@ import jakarta.inject.Singleton
 import java.io.OutputStream
 
 class BigQueryObjectStorageFormattingWriter(
-    private val stream: DestinationStream,
+    stream: DestinationStream,
     outputStream: OutputStream,
-    private val rootLevelFlattening: Boolean
-) : ObjectStorageFormattingWriter {
+    rootLevelFlattening: Boolean = true,
+) :
+    ObjectStorageFormattingWriter by CSVFormattingWriter(
+        stream,
+        outputStream,
+        rootLevelFlattening
+    ) {
 
-    private val finalSchema = stream.schema.withAirbyteMeta(rootLevelFlattening)
-    private val printer = finalSchema.toCsvPrinterWithHeader(outputStream)
+    private val csvFormattingWriter = CSVFormattingWriter(stream, outputStream, rootLevelFlattening)
 
     override fun accept(record: DestinationRecordRaw) {
         record.rawData.record.emittedAt *= 1000
-
-        printer.printRecord(
-            record
-                .asDestinationRecordAirbyteValue()
-                .dataWithAirbyteMeta(stream, rootLevelFlattening)
-                .toCsvRecord(finalSchema)
-        )
-    }
-
-    override fun flush() {
-        printer.flush()
-    }
-
-    override fun close() {
-        printer.close()
+        csvFormattingWriter.accept(record)
     }
 }
 
