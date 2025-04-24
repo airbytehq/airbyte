80dfcb466e7217eaf342c6b70cb099a4ec13ae24
maxi297 <maxime@airbyte.io>
2025-04-24 18:54:50 -0400

    [TMP] add campaigns stream but needs to resolve schema for Settings and Languages

--- airbyte-integrations/connectors/source-bing-ads/source_bing_ads/source.py
+++ airbyte-integrations/connectors/source-bing-ads/source_bing_ads/source.py
@@ -162,7 +162,7 @@ class SourceBingAds(YamlDeclarativeSource):
             AppInstallAdLabels(client, config),
             Ads(client, config),
             Budget(client, config),
-            Campaigns(client, config),
+            # Campaigns(client, config),
             BudgetSummaryReport(client, config),
             Labels(client, config),
             KeywordLabels(client, config),
