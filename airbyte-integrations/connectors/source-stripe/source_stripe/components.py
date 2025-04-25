5558b23aad5ac539880d1b2931f3c4ba5e0630e2
Eric Van Benschoten <ericvanbenschoten@hubifi.com>
2025-04-25 14:52:06 -0400

    fix
    adding concurrency to stripe
    
    changing concurrency
    
    adding concurrency to stripe 2
    
    ili changes
    
    change type
    
    line items

--- airbyte-integrations/connectors/source-stripe/source_stripe/components.py
+++ airbyte-integrations/connectors/source-stripe/source_stripe/components.py
@@ -1,7 +1,7 @@
 from typing import Iterable, Mapping, Any, Optional, Set, Dict
 from datetime import timedelta, datetime
 from airbyte_cdk.sources.declarative.partition_routers import SubstreamPartitionRouter
-#from airbyte_cdk.sources.declarative.migrations.state_migration import StateMigration
+from airbyte_cdk.sources.declarative.migrations.state_migration import StateMigration
 from airbyte_cdk.sources.declarative.types import StreamSlice
 from collections import Counter
 
@@ -46,35 +46,36 @@ class CustomerIdPartitionRouter(SubstreamPartitionRouter):
 
         yield from unique_slices
 
-#class CustomerBalanceStateMigration(StateMigration):
-#    def should_migrate(self, stream_state: Mapping[str, Any]) -> bool:
-#        return "parent_state" in stream_state
-#
-#    def migrate(self, stream_state: Mapping[str, Any]) -> Mapping[str, Any]:
-#        current_state = stream_state or {}
-#        state_created = int(current_state.get("state", {}).get("created", 0))
-#        current_parent_state = current_state.get("parent_state", {})
-#
-#        two_days = int(timedelta(days=2).total_seconds())
-#        fourteen_day_floor = int(datetime.utcnow().timestamp()) - int(timedelta(days=14).total_seconds())
-#
-#        created_customer_balance_transactions = max(state_created - two_days, fourteen_day_floor, 0)
-#        updated_customers = max(0, current_parent_state.get("customers", {}).get("updated", 0) - two_days, fourteen_day_floor)
-#        updated_invoices = max(0, current_parent_state.get("invoices", {}).get("updated", 0) - two_days, fourteen_day_floor)
-#
-#        migrated_state = {
-#            "state": {
-#                "created": created_customer_balance_transactions
-#            },
-#            "parent_state": {
-#                "customers": {
-#                    "updated": updated_customers
-#                },
-#                "invoices": {
-#                    "updated": updated_invoices
-#                }
-#            },
-#            "use_global_cursor": True
-#        }
-#
-#        return migrated_state
+class CustomerBalanceStateMigration(StateMigration):
+    def should_migrate(self, stream_state: Mapping[str, Any]) -> bool:
+        return "parent_state" in stream_state
+
+    def migrate(self, stream_state: Mapping[str, Any]) -> Mapping[str, Any]:
+        current_state = stream_state or {}
+        state_created = int(current_state.get("state", {}).get("created", 0))
+        current_parent_state = current_state.get("parent_state", {})
+
+        two_days = int(timedelta(days=2).total_seconds())
+        fourteen_day_floor = int(datetime.utcnow().timestamp()) - int(timedelta(days=14).total_seconds())
+
+        created_customer_balance_transactions = max(state_created - two_days, fourteen_day_floor, 0)
+        updated_customers = max(0, current_parent_state.get("customers", {}).get("updated", 0) - two_days, fourteen_day_floor)
+        updated_invoices = max(0, current_parent_state.get("invoices", {}).get("updated", 0) - two_days, fourteen_day_floor)
+
+        migrated_state = {
+            "state": {
+                "created": created_customer_balance_transactions
+            },
+            "parent_state": {
+                "customers": {
+                    "updated": updated_customers
+                },
+                "invoices": {
+                    "updated": updated_invoices
+                }
+            },
+            "use_global_cursor": True
+        }
+
+        return migrated_state
+
