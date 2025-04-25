bcfae4aceb67fbae0a9e436aa1290102ee25cd60
brianjlai <brian.lai@airbyte.io>
2025-04-24 21:07:51 -0700

    clean up and consolidate helper methods into one place

--- airbyte-integrations/connectors/source-linkedin-ads/unit_tests/conftest.py
+++ airbyte-integrations/connectors/source-linkedin-ads/unit_tests/conftest.py
@@ -2,7 +2,6 @@
 # Copyright (c) 2024 Airbyte, Inc., all rights reserved.
 #
 
-
 import json
 import os
 import sys
@@ -19,21 +18,37 @@ os.environ["REQUEST_CACHE_PATH"] = "REQUEST_CACHE_PATH"
 pytest_plugins = ["airbyte_cdk.test.utils.manifest_only_fixtures"]
 
 
-# def _get_manifest_path() -> Path:
-#     source_declarative_manifest_path = Path("/airbyte/integration_code/source_declarative_manifest")
-#     if source_declarative_manifest_path.exists():
-#         return source_declarative_manifest_path
-#     return Path(__file__).parent.parent
-#
-#
-# _SOURCE_FOLDER_PATH = _get_manifest_path()
-# _YAML_FILE_PATH = _SOURCE_FOLDER_PATH / "manifest.yaml"
-#
-#
-# def get_source(config) -> YamlDeclarativeSource:
-#     catalog = CatalogBuilder().build()
-#     state = StateBuilder().build()
-#     return YamlDeclarativeSource(path_to_yaml=str(_YAML_FILE_PATH), catalog=catalog, config=config, state=state)
+def _get_manifest_path() -> Path:
+    source_declarative_manifest_path = Path("/airbyte/integration_code/source_declarative_manifest")
+    if source_declarative_manifest_path.exists():
+        return source_declarative_manifest_path
+    return Path(__file__).parent.parent
+
+
+_SOURCE_FOLDER_PATH = _get_manifest_path()
+_YAML_FILE_PATH = _SOURCE_FOLDER_PATH / "manifest.yaml"
+
+sys.path.append(str(_SOURCE_FOLDER_PATH))  # to allow loading custom components
+
+
+def get_source(config) -> YamlDeclarativeSource:
+    catalog = CatalogBuilder().build()
+    state = StateBuilder().build()
+    return YamlDeclarativeSource(path_to_yaml=str(_YAML_FILE_PATH), catalog=catalog, config=config, state=state)
+
+
+def find_stream(stream_name, config):
+    streams = get_source(config).streams(config=config)
+
+    # cache should be disabled once this issue is fixed https://github.com/airbytehq/airbyte-internal-issues/issues/6513
+    for stream in streams:
+        stream.retriever.requester.use_cache = True
+
+    # find by name
+    for stream in streams:
+        if stream.name == stream_name:
+            return stream
+    raise ValueError(f"Stream {stream_name} not found")
 
 
 def load_config(config_path: str) -> Mapping[str, Any]:
--- airbyte-integrations/connectors/source-linkedin-ads/unit_tests/test_streams.py
+++ airbyte-integrations/connectors/source-linkedin-ads/unit_tests/test_streams.py
@@ -2,17 +2,11 @@
 # Copyright (c) 2024 Airbyte, Inc., all rights reserved.
 #
 
-import sys
-from pathlib import Path
-
-from conftest import load_json_file
+from conftest import find_stream, load_json_file
 from freezegun import freeze_time
 
 from airbyte_cdk.models import SyncMode
-from airbyte_cdk.sources.declarative.yaml_declarative_source import YamlDeclarativeSource
 from airbyte_cdk.sources.streams.http.requests_native_auth import TokenAuthenticator
-from airbyte_cdk.test.catalog_builder import CatalogBuilder
-from airbyte_cdk.test.state_builder import StateBuilder
 
 
 # Test input arguments for the `make_analytics_slices`
@@ -33,39 +27,6 @@ TEST_CONFIG: dict = {
         "authenticator": TokenAuthenticator(token="123"),
     },
 }
-
-
-def get_source(config) -> YamlDeclarativeSource:
-    catalog = CatalogBuilder().build()
-    state = StateBuilder().build()
-    return YamlDeclarativeSource(path_to_yaml=str(_YAML_FILE_PATH), catalog=catalog, config=config, state=state)
-
-
-def _get_manifest_path() -> Path:
-    source_declarative_manifest_path = Path("/airbyte/integration_code/source_declarative_manifest")
-    if source_declarative_manifest_path.exists():
-        return source_declarative_manifest_path
-    return Path(__file__).parent.parent
-
-
-def find_stream(stream_name, config):
-    streams = get_source(config).streams(config=config)
-
-    # cache should be disabled once this issue is fixed https://github.com/airbytehq/airbyte-internal-issues/issues/6513
-    for stream in streams:
-        stream.retriever.requester.use_cache = True
-
-    # find by name
-    for stream in streams:
-        if stream.name == stream_name:
-            return stream
-    raise ValueError(f"Stream {stream_name} not found")
-
-
-_SOURCE_FOLDER_PATH = _get_manifest_path()
-_YAML_FILE_PATH = _SOURCE_FOLDER_PATH / "manifest.yaml"
-
-sys.path.append(str(_SOURCE_FOLDER_PATH))  # to allow loading custom components
 
 
 @freeze_time("2021-03-01")
