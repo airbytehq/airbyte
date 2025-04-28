b5f1f195b3e3f3b52fd7a19bcc17650c41ef7fdb
Christo Grabowski <108154848+ChristoGrab@users.noreply.github.com>
2025-04-28 16:03:03 -0400

    chore: add 429 error code to retry logic

--- airbyte-integrations/connectors/source-google-search-console/source_google_search_console/streams.py
+++ airbyte-integrations/connectors/source-google-search-console/source_google_search_console/streams.py
@@ -81,7 +81,7 @@ class GoogleSearchConsole(HttpStream, ABC):
                 self.logger.error(f"Stream `{self.name}`. {error.get('message')}. Trying with `aggregationType = auto` instead.")
                 self.aggregation_type = QueryAggregationType.auto
                 setattr(self, "raise_on_http_errors", False)
-        return response.status_code >= 500
+        return response.status_code == 429 or response.status_code >= 500
 
 
 class Sites(GoogleSearchConsole):
