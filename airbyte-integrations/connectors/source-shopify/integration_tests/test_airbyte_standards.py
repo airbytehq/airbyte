738fb08049b71b2f1a410f13b86d9d32a8466b06
Octavia Squidington III <octavia-squidington-iii@users.noreply.github.com>
2025-04-29 20:12:55 +0000

    chore: auto-fix lint and format issues

--- airbyte-integrations/connectors/source-shopify/integration_tests/test_airbyte_standards.py
+++ airbyte-integrations/connectors/source-shopify/integration_tests/test_airbyte_standards.py
@@ -1,8 +1,7 @@
-
 # Copyright (c) 2025 Airbyte, Inc., all rights reserved.
 """FAST Airbyte Standard Tests for the source_pokeapi_w_components source."""
 
-#from airbyte_cdk.test.standard_tests import SourceTestSuiteBase
+# from airbyte_cdk.test.standard_tests import SourceTestSuiteBase
 from pathlib import Path
 
 from airbyte_cdk.test.standard_tests import SourceTestSuiteBase
@@ -12,6 +11,7 @@ from airbyte_cdk.test.standard_tests.util import create_connector_test_suite
 pytest_plugins = [
     "airbyte_cdk.test.standard_tests.pytest_hooks",
 ]
+
 
 class TestSuite(SourceTestSuiteBase):
     """Test suite for Python sources.
