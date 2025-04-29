1e90a227cd88f7755f7a5a59c074dec346b03d6e
Aaron Steers <aj@airbyte.io>
2025-04-29 13:06:40 -0700

    chore(source-shopify): bump cdk, move to fast standard test suite

---
+++ airbyte-integrations/connectors/source-shopify/integration_tests/test_airbyte_standards.py
@@ -0,0 +1,23 @@
+
+# Copyright (c) 2025 Airbyte, Inc., all rights reserved.
+"""FAST Airbyte Standard Tests for the source_pokeapi_w_components source."""
+
+#from airbyte_cdk.test.standard_tests import SourceTestSuiteBase
+from pathlib import Path
+
+from airbyte_cdk.test.standard_tests import SourceTestSuiteBase
+from airbyte_cdk.test.standard_tests.util import create_connector_test_suite
+
+
+pytest_plugins = [
+    "airbyte_cdk.test.standard_tests.pytest_hooks",
+]
+
+class TestSuite(SourceTestSuiteBase):
+    """Test suite for the source_pokeapi_w_components source.
+
+    This class inherits from SourceTestSuiteBase and implements all of the tests in the suite.
+
+    As long as the class name starts with "Test", pytest will automatically discover and run the
+    tests in this class.
+    """
