9adc80820f6f6641d71c43fa0913a0299e533538
Aaron Steers <aj@airbyte.io>
2025-05-03 16:49:33 -0700

    multi-arch build

--- docker-images/test-base-image-build.sh
+++ docker-images/test-base-image-build.sh
@@ -8,14 +8,21 @@ set -euo pipefail
 CONNECTOR_TYPE=$1
 IMAGE_TO_BUILD="docker.io/airbyte/${CONNECTOR_TYPE}-connector-base"
 
-ARCH='arm64'
 TAG='dev'
+PRIMARY_ARCH='arm64'  # This will get aliased to 'dev' in the final image.
 CONTEXT_DIR="."  # This doesn't matter, since nothing is copied from the context dir.
 
 export DOCKER_BUILDKIT=1
 
-docker build \
+# build for both architectures without duplicating the docker build command
+ARCHES=(arm64 amd64)
+for ARCH in "${ARCHES[@]}"; do
+  echo "Building '${ARCH}' image: ${IMAGE_TO_BUILD}:${TAG}-${ARCH}"
+  docker build \
     --platform linux/${ARCH} \
     --file Dockerfile.${CONNECTOR_TYPE}-connector-base \
-    -t ${IMAGE_TO_BUILD}:${TAG} \
+    -t ${IMAGE_TO_BUILD}:${TAG}-${ARCH} \
     ${CONTEXT_DIR}
+done
+
+echo -e "Built base images:\n- ${IMAGE_TO_BUILD}:${TAG}-amd64\n- ${IMAGE_TO_BUILD}:${TAG}-arm64\n- ${IMAGE_TO_BUILD}:${TAG} ('dev-${PRIMARY_ARCH}' alias)"
--- docker-images/test-connector-image-build.sh
+++ docker-images/test-connector-image-build.sh
@@ -12,24 +12,30 @@ CONNECTOR_DIR="../airbyte-integrations/connectors/${CONNECTOR_NAME}"
 BASE_IMAGE_NAME="docker.io/airbyte/${CONNECTOR_TYPE}-connector-base"
 BASE_IMAGE_TAG='dev'
 
-ARCH='arm64'
+PRIMARY_ARCH='arm64'
+IMAGE_TO_BUILD="airbyte/${CONNECTOR_TYPE}-connector"
+TAG='dev'
 
 ./test-base-image-build.sh ${CONNECTOR_TYPE} ${BASE_IMAGE_TAG}
 
 export DOCKER_BUILDKIT=1
 
-docker build \
+ARCHES=(arm64 amd64)
+for ARCH in "${ARCHES[@]}"; do
+  echo "Building '${ARCH}' image: ${IMAGE_TO_BUILD}:${TAG}-${ARCH}"
+
+  docker build \
     --platform linux/${ARCH} \
     --label io.airbyte.version=3.11.15\
     --label io.airbyte.name=airbyte/${CONNECTOR_NAME} \
     --file Dockerfile.${CONNECTOR_TYPE}-connector \
-    --build-arg=BASE_IMAGE=${BASE_IMAGE_NAME}:${BASE_IMAGE_TAG} \
+    --build-arg=BASE_IMAGE=${BASE_IMAGE_NAME}:${BASE_IMAGE_TAG}-${ARCH} \
     --build-arg=CONNECTOR_NAME=${CONNECTOR_NAME} \
     --build-arg=EXTRA_BUILD_SCRIPT= \
-    -t airbyte/${CONNECTOR_NAME}:dev-${ARCH} \
+    -t ${IMAGE_TO_BUILD}:${TAG}-${ARCH} \
     ${CONNECTOR_DIR}
+done
 
-docker tag airbyte/${CONNECTOR_NAME}:dev-${ARCH} airbyte/${CONNECTOR_NAME}:dev
-echo -e "Built images:\n- airbyte/${CONNECTOR_NAME}:dev-${ARCH}\n- airbyte/${CONNECTOR_NAME}:dev ('dev-${ARCH}' alias)"
-
-echo -e "Test by running: \n  docker run --rm -it airbyte/${CONNECTOR_NAME}:dev-${ARCH} spec"
+docker tag ${IMAGE_TO_BUILD}:${TAG}-${PRIMARY_ARCH} airbyte/${CONNECTOR_NAME}:dev
+echo -e "Built images:\n- ${IMAGE_TO_BUILD}:${TAG}-${PRIMARY_ARCH}\n- ${IMAGE_TO_BUILD}:${TAG} ('${TAG}-${PRIMARY_ARCH}' alias)"
+echo -e "Test by running: \n  docker run --rm -it ${IMAGE_TO_BUILD}:${TAG}-${PRIMARY_ARCH} spec"
