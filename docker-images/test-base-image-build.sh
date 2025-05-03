cdf80614187948529b1beea77abc5a8d1a1d7a75
Aaron Steers <aj@airbyte.io>
2025-05-03 16:08:04 -0700

    add shell scripts

---
+++ docker-images/test-base-image-build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Test ability to build the base image for a connector type (python or java).
+# Usage: ./test-base-image-build.sh CONNECTOR_TYPE
+
+set -euo pipefail
+
+CONNECTOR_TYPE=$1
+IMAGE_TO_BUILD="docker.io/airbyte/${CONNECTOR_TYPE}-connector-base"
+
+ARCH='arm64'
+TAG='dev'
+CONTEXT_DIR="."  # This doesn't matter, since nothing is copied from the context dir.
+
+docker build \
+    --platform linux/${ARCH} \
+    --file Dockerfile.${CONNECTOR_TYPE}-connector-base \
+    -t ${IMAGE_TO_BUILD}:${TAG} \
+    ${CONTEXT_DIR}
---
+++ docker-images/test-connector-image-build.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Test ability to build a connector image.
+# Usage: ./test-connector-image-build.sh CONNECTOR_TYPE CONNECTOR_NAME
+
+set -euo pipefail
+
+CONNECTOR_TYPE=$1
+CONNECTOR_NAME=$2
+CONNECTOR_DIR="../airbyte-integrations/connectors/${CONNECTOR_NAME}"
+
+BASE_IMAGE_NAME="docker.io/airbyte/${CONNECTOR_TYPE}-connector-base"
+BASE_IMAGE_TAG='dev'
+
+ARCH='arm64'
+
+./test-base-image-build.sh ${CONNECTOR_TYPE} ${BASE_IMAGE_TAG}
+
+docker build \
+    --platform linux/${ARCH} \
+    --label io.airbyte.version=3.11.15\
+    --label io.airbyte.name=airbyte/${CONNECTOR_NAME} \
+    --file Dockerfile.${CONNECTOR_TYPE}-connector \
+    --build-arg=BASE_IMAGE=${BASE_IMAGE_NAME}:${BASE_IMAGE_TAG} \
+    --build-arg=CONNECTOR_NAME=${CONNECTOR_NAME} \
+    --build-arg=EXTRA_BUILD_SCRIPT= \
+    -t airbyte/${CONNECTOR_NAME}:dev-${ARCH} \
+    ${CONNECTOR_DIR}
+
+docker tag airbyte/${CONNECTOR_NAME}:dev-${ARCH} airbyte/${CONNECTOR_NAME}:dev
+echo -e "Built images:\n- airbyte/${CONNECTOR_NAME}:dev-${ARCH}\n- airbyte/${CONNECTOR_NAME}:dev"
