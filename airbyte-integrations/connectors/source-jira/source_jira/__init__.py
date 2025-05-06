d9fef77b51390ca7c86b2fa9998a7fdc58804b44
ChristoGrab <christo.grab@gmail.com>
2025-05-06 13:20:02 -0700

    chore: migrate to manifest-only

--- airbyte-integrations/connectors/source-jira/source_jira/__init__.py
+++
@@ -1,9 +0,0 @@
-from .source import SourceJira
-from .components import (
-    LabelsRecordExtractor,
-    SprintIssuesSubstreamPartitionRouter,
-    SubstreamOrSinglePartitionRouter,
-    RemoveEmptyFields,
-)
-
-__all__ = ["SourceJira", "LabelsRecordExtractor", "SprintIssuesSubstreamPartitionRouter", "SubstreamOrSinglePartitionRouter", "RemoveEmptyFields"]
--- airbyte-integrations/connectors/source-jira/source_jira/utils.py
+++
@@ -1,9 +0,0 @@
-#
-# Copyright (c) 2023 Airbyte, Inc., all rights reserved.
-#
-
-from airbyte_cdk.sources.streams import Stream
-
-
-def read_full_refresh(stream_instance: Stream):
-    yield from stream_instance.read_only_records()
