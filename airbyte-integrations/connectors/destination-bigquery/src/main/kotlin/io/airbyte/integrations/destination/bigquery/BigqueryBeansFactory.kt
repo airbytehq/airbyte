1e19d784010ac0f34509fea178902bf9220c1005
Edward Gao <edward.gao@airbyte.io>
2025-05-07 12:25:07 -0700

    move bq client into beans factory

--- airbyte-integrations/connectors/destination-bigquery/src/main/kotlin/io/airbyte/integrations/destination/bigquery/BigqueryBeansFactory.kt
+++ airbyte-integrations/connectors/destination-bigquery/src/main/kotlin/io/airbyte/integrations/destination/bigquery/BigqueryBeansFactory.kt
@@ -4,16 +4,25 @@
 
 package io.airbyte.integrations.destination.bigquery
 
+import com.google.auth.oauth2.GoogleCredentials
+import com.google.cloud.bigquery.BigQuery
+import com.google.cloud.bigquery.BigQueryOptions
 import io.airbyte.cdk.load.check.DestinationCheckerSync
 import io.airbyte.cdk.load.command.DestinationCatalog
 import io.airbyte.cdk.load.state.SyncManager
 import io.airbyte.cdk.load.task.DestinationTaskLauncher
 import io.airbyte.cdk.load.write.WriteOperation
 import io.airbyte.integrations.destination.bigquery.check.BigqueryCheckCleaner
+import io.airbyte.integrations.destination.bigquery.spec.BigqueryConfiguration
+import io.github.oshai.kotlinlogging.KotlinLogging
 import io.micronaut.context.annotation.Factory
 import jakarta.inject.Named
 import jakarta.inject.Singleton
+import java.io.ByteArrayInputStream
 import java.io.InputStream
+import java.nio.charset.StandardCharsets
+
+private val logger = KotlinLogging.logger {}
 
 @Factory
 class BigqueryBeansFactory {
@@ -30,4 +39,31 @@ class BigqueryBeansFactory {
             WriteOperation(taskLauncher, syncManager),
             BigqueryCheckCleaner(),
         )
+
+    @Singleton
+    fun getBigqueryClient(config: BigqueryConfiguration): BigQuery {
+        // Follows this order of resolution:
+        // https://cloud.google.com/java/docs/reference/google-auth-library/latest/com.google.auth.oauth2.GoogleCredentials#com_google_auth_oauth2_GoogleCredentials_getApplicationDefault
+        val credentials =
+            if (config.credentialsJson == null) {
+                logger.info {
+                    "No service account key json is provided. It is required if you are using Airbyte cloud."
+                }
+                logger.info { "Using the default service account credential from environment." }
+                GoogleCredentials.getApplicationDefault()
+            } else {
+                // The JSON credential can either be a raw JSON object, or a serialized JSON object.
+                GoogleCredentials.fromStream(
+                    ByteArrayInputStream(
+                        config.credentialsJson.toByteArray(StandardCharsets.UTF_8)
+                    ),
+                )
+            }
+        return BigQueryOptions.newBuilder()
+            .setProjectId(config.projectId)
+            .setCredentials(credentials)
+            .setHeaderProvider(BigQueryUtils.headerProvider)
+            .build()
+            .service
+    }
 }
