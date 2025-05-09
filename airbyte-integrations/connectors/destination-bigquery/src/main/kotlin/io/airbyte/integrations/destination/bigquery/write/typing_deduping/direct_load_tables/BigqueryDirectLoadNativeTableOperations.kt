d82f3ae4b1e226d2f0c172abd0bbe39e3581f430
Edward Gao <edward.gao@airbyte.io>
2025-05-09 12:49:37 -0700

    copy over implementations

--- airbyte-integrations/connectors/destination-bigquery/src/main/kotlin/io/airbyte/integrations/destination/bigquery/write/typing_deduping/direct_load_tables/BigqueryDirectLoadNativeTableOperations.kt
+++ airbyte-integrations/connectors/destination-bigquery/src/main/kotlin/io/airbyte/integrations/destination/bigquery/write/typing_deduping/direct_load_tables/BigqueryDirectLoadNativeTableOperations.kt
@@ -4,12 +4,16 @@
 
 package io.airbyte.integrations.destination.bigquery.write.typing_deduping.direct_load_tables
 
+import com.google.cloud.bigquery.BigQuery
+import com.google.cloud.bigquery.QueryJobConfiguration
 import io.airbyte.cdk.load.command.DestinationStream
+import io.airbyte.cdk.load.message.Meta
 import io.airbyte.cdk.load.orchestration.db.ColumnNameMapping
 import io.airbyte.cdk.load.orchestration.db.TableName
 import io.airbyte.cdk.load.orchestration.db.direct_load_table.DirectLoadTableNativeOperations
 
-class BigqueryDirectLoadNativeTableOperations : DirectLoadTableNativeOperations {
+class BigqueryDirectLoadNativeTableOperations(private val bigquery: BigQuery) :
+    DirectLoadTableNativeOperations {
     override fun ensureSchemaMatches(
         stream: DestinationStream,
         tableName: TableName,
@@ -19,6 +23,17 @@ class BigqueryDirectLoadNativeTableOperations : DirectLoadTableNativeOperations 
     }
 
     override fun getGenerationId(tableName: TableName): Long {
-        TODO("Not yet implemented")
+        val result =
+            bigquery.query(
+                QueryJobConfiguration.of(
+                    "SELECT _airbyte_generation_id FROM ${tableName.namespace}.${tableName.name}$ LIMIT 1"
+                ),
+            )
+        val value = result.iterateAll().first().get(Meta.COLUMN_NAME_AB_GENERATION_ID)
+        return if (value.isNull) {
+            0
+        } else {
+            value.longValue
+        }
     }
 }
