ec3d73ce6f8f72bd94cbcf477cccf18536625e03
David Gold <32782137+dbgold17@users.noreply.github.com>
2025-05-12 14:21:13 -0700

    WIP

--- airbyte-ci/connectors/connectors_qa/src/connectors_qa/checks/version.py
+++ airbyte-ci/connectors/connectors_qa/src/connectors_qa/checks/version.py
@@ -1,96 +1,65 @@
+# Copyright (c) 2025 Airbyte, Inc., all rights reserved.
 
+from typing import Any, Dict
 
 import requests
 import semver
 import yaml
-from typing import Optional, Dict, Any
-
 from connector_ops.utils import Connector  # type: ignore
 
 from connectors_qa.models import Check, CheckCategory, CheckResult
 
 
-METADATA_FILE_NAME = "metadata.yaml"
-GITHUB_URL_PREFIX_FOR_CONNECTORS = "https://raw.githubusercontent.com/airbytehq/airbyte/master/airbyte-integrations/connectors"
+class VersionIncrementCheck(Check):
+    """Check that the connector version was incremented if files were modified."""
 
+    category = CheckCategory.VERSION
+    name = "Connector Version Increment Check"
+    description = "Validates that the connector version was incremented if files were modified."
 
-class VersionCheck(Check):
-    """Base class for version-related checks."""
-    
-    @property
-    def category(self) -> CheckCategory:
-        return CheckCategory.METADATA
-    
-    @property
-    def name(self) -> str:
-        return "Version Check"
-    
-    @property
-    def description(self) -> str:
-        return "Validates the connector version."
-    
-    def _get_github_master_metadata_url(self, connector: Connector) -> str:
-        return f"{GITHUB_URL_PREFIX_FOR_CONNECTORS}/{connector.technical_name}/{METADATA_FILE_NAME}"
-    
-    def _get_master_metadata(self, connector: Connector) -> Optional[Dict[str, Any]]:
-        """Get the metadata from the master branch."""
-        response = requests.get(self._get_github_master_metadata_url(connector))
-        
+    _METADATA_FILE_NAME = "metadata.yaml"
+    # _BYPASS_CHECK_FOR = [
+    #     _METADATA_FILE_NAME,
+    #     "acceptance-test-config.yml",
+    #     "README.md",
+    #     "bootstrap.md",
+    #     ".dockerignore",
+    #     "unit_tests",
+    #     "integration_tests",
+    #     "src/test",
+    #     "src/test-integration",
+    #     "src/test-performance",
+    #     "build.gradle",
+    #     "erd",
+    #     "build_customization.py",
+    # ]
+
+    def _get_master_metadata(self, connector: Connector) -> Dict[str, Any] | None:
+        """Get the metadata from the master branch or None if unable to retrieve."""
+        github_url_prefix = "https://raw.githubusercontent.com/airbytehq/airbyte/master/airbyte-integrations/connectors"
+        master_metadata_url = f"{github_url_prefix}/{connector.technical_name}/{self._METADATA_FILE_NAME}"
+        response = requests.get(master_metadata_url)
+
+        # New connectors will not have a metadata file in master
         if not response.ok:
             return None
-        return yaml.safe_load(response.text)
-    
+        return yaml.safe_load(response.text).get("data", None)
+
+    def _parse_version_from_metadata(self, metadata: Dict[str, Any]) -> semver.Version:
+        return semver.Version.parse(str(metadata["dockerImageTag"]))
+
     def _get_master_connector_version(self, connector: Connector) -> semver.Version:
         """Get the version from the master branch."""
-        metadata = self._get_master_metadata(connector)
-        if not metadata:
+        master_metadata = self._get_master_metadata(connector)
+        if not master_metadata:
             return semver.Version.parse("0.0.0")
-        
-        return semver.Version.parse(str(metadata["data"]["dockerImageTag"]))
-    
+
+        return self._parse_version_from_metadata(master_metadata)
+
     def _get_current_connector_version(self, connector: Connector) -> semver.Version:
         """Get the current version."""
-        return semver.Version.parse(str(connector.metadata["dockerImageTag"]))
+        return self._parse_version_from_metadata(connector.metadata)
 
-
-class VersionIncrementCheck(VersionCheck):
-    """Check that the connector version was incremented if files were modified."""
-    
-    @property
-    def name(self) -> str:
-        return "Connector Version Increment Check"
-    
-    @property
-    def description(self) -> str:
-        return "Validates that the connector version was incremented if files were modified."
-    
-    BYPASS_CHECK_FOR = [
-        METADATA_FILE_NAME,
-        "acceptance-test-config.yml",
-        "README.md",
-        "bootstrap.md",
-        ".dockerignore",
-        "unit_tests",
-        "integration_tests",
-        "src/test",
-        "src/test-integration",
-        "src/test-performance",
-        "build.gradle",
-        "erd",
-        "build_customization.py",
-    ]
-    
-    def _should_run(self, connector: Connector) -> bool:
-        """Determine if the check should run based on modified files."""
-        if connector.metadata and connector.metadata.get("ab_internal", {}).get("requireVersionIncrementsInPullRequests") is False:
-            return False
-        
-        return True
-    
-    def _is_version_not_incremented(self, master_version: semver.Version, current_version: semver.Version) -> bool:
-        """Check if the version was not incremented."""
-        return master_version >= current_version
-    
     def _are_both_versions_release_candidates(self, master_version: semver.Version, current_version: semver.Version) -> bool:
         """Check if both versions are release candidates."""
         return bool(
@@ -99,7 +68,7 @@ class VersionIncrementCheck(VersionCheck):
             and "rc" in master_version.prerelease
             and "rc" in current_version.prerelease
         )
-    
+
     def _have_same_major_minor_patch(self, master_version: semver.Version, current_version: semver.Version) -> bool:
         """Check if both versions have the same major, minor, and patch versions."""
         return (
@@ -107,39 +76,41 @@ class VersionIncrementCheck(VersionCheck):
             and master_version.minor == current_version.minor
             and master_version.patch == current_version.patch
         )
-    
+
     def _run(self, connector: Connector) -> CheckResult:
         """Run the version increment check."""
-        if not self._should_run(connector):
-            return self.skip(
-                connector,
-                "No modified files required a version bump or connector opts out of version checks."
-            )
-        
+        if connector.metadata and connector.metadata.get("ab_internal", {}).get("requireVersionIncrementsInPullRequests") is False:
+            return self.skip(connector, "Connector opts out of version checks.")
+        # if self.context.ci_context == CIContext.MASTER: # TODO: fix this
+        #     return self.skip(
+        #         connector,
+        #         "Connector is in master branch."
+        #     )
+        # if connector.code_directory.modified_files.intersection(self._BYPASS_CHECK_FOR): # TODO: fix this
+        #     return False
+
         try:
             master_version = self._get_master_connector_version(connector)
             current_version = self._get_current_connector_version(connector)
-            
-            if self._is_version_not_incremented(master_version, current_version):
+
+            # Require a version increment
+            if current_version <= master_version:
                 return self.fail(
                     connector,
-                    f"The dockerImageTag in {METADATA_FILE_NAME} was not incremented. "
-                    f"Master version is {master_version}, current version is {current_version}"
+                    f"The dockerImageTag in {self._METADATA_FILE_NAME} was not incremented. "
+                    f"Master version is {master_version}, current version is {current_version}",
                 )
-            
+
             if self._are_both_versions_release_candidates(master_version, current_version):
                 if not self._have_same_major_minor_patch(master_version, current_version):
                     return self.fail(
                         connector,
                         f"Master and current version are release candidates but they have different major, minor or patch versions. "
                         f"Release candidates should only differ in the prerelease part. Master version is {master_version}, "
-                        f"current version is {current_version}"
+                        f"current version is {current_version}",
                     )
-            
-            return self.pass_(
-                connector,
-                f"Version was properly incremented from {master_version} to {current_version}."
-            )
+
+            return self.pass_(connector, f"Version was properly incremented from {master_version} to {current_version}.")
         except (requests.HTTPError, ValueError, TypeError) as e:
             return self.fail(connector, str(e))
 
---
+++ airbyte-ci/connectors/connectors_qa/tests/unit_tests/test_checks/test_version.py
@@ -0,0 +1,69 @@
+# Copyright (c) 2024 Airbyte, Inc., all rights reserved.
+
+import pytest
+import semver
+from connector_ops.utils import METADATA_FILE_NAME
+
+from connectors_qa.checks.version import VersionIncrementCheck
+
+
+class TestVersionIncrementCheck:
+    @pytest.fixture
+    def mock_connector(self, mocker, tmp_path):
+        connector = mocker.Mock(code_directory=str(tmp_path), technical_name="mock-connector")
+        return connector
+
+    def _get_version_increment_check(self, mocker, connector, master_version="1.0.0", current_version="1.0.1"):
+        mocker.patch(
+            "pipelines.airbyte_ci.connectors.test.steps.common.VersionIncrementCheck.master_connector_version",
+            new_callable=mocker.PropertyMock,
+            return_value=semver.Version.parse(master_version),
+        )
+        mocker.patch(
+            "pipelines.airbyte_ci.connectors.test.steps.common.VersionIncrementCheck.current_connector_version",
+            new_callable=mocker.PropertyMock,
+            return_value=semver.Version.parse(current_version),
+        )
+
+        return VersionIncrementCheck(connector)
+
+    def test_should_run(self, connector):
+        connector.modified_files = ["some_file"]
+        assert VersionIncrementCheck(connector).should_run
+
+    def test_should_not_run(self, context):
+        for bypassed_file in VersionIncrementCheck.BYPASS_CHECK_FOR:
+            context.modified_files = [bypassed_file]
+            assert not VersionIncrementCheck(context).should_run
+
+    def test_validate_success_no_rc_increment(self, mocker, context):
+        version_increment_check = self._get_version_increment_check(mocker, context, master_version="1.0.0", current_version="1.0.1")
+        result = version_increment_check.validate()
+        assert result.success
+
+    def test_validate_failure_no_increment(self, context, mocker):
+        version_increment_check = self._get_version_increment_check(mocker, context, master_version="1.0.0", current_version="1.0.0")
+        result = version_increment_check.validate()
+        assert not result.success
+        assert (
+            result.stderr
+            == f"The dockerImageTag in {METADATA_FILE_NAME} was not incremented. Master version is {version_increment_check.master_connector_version}, current version is {version_increment_check.current_connector_version}"
+        )
+
+    def test_validate_failure_rc_with_different_versions(self, context, mocker):
+        version_increment_check = self._get_version_increment_check(
+            mocker, context, master_version="1.0.0-rc.1", current_version="1.0.1-rc.1"
+        )
+        result = version_increment_check.validate()
+        assert not result.success
+        assert (
+            result.stderr
+            == f"Master and current version are release candidates but they have different major, minor or patch versions. Release candidates should only differ in the prerelease part. Master version is {version_increment_check.master_connector_version}, current version is {version_increment_check.current_connector_version}"
+        )
+
+    def test_validate_success_rc_increment(self, context, mocker):
+        version_increment_check = self._get_version_increment_check(
+            mocker, context, master_version="1.0.1-rc.1", current_version="1.0.1-rc.2"
+        )
+        result = version_increment_check.validate()
+        assert result.success
