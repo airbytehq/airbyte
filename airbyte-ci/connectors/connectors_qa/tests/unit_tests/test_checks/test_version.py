17a7f05d6ac82eaa257d57aff6c725afdec48c57
David Gold <32782137+dbgold17@users.noreply.github.com>
2025-05-12 15:08:38 -0700

    WIP unit testing

--- airbyte-ci/connectors/connectors_qa/tests/unit_tests/test_checks/test_version.py
+++ airbyte-ci/connectors/connectors_qa/tests/unit_tests/test_checks/test_version.py
@@ -5,6 +5,7 @@ import semver
 from connector_ops.utils import METADATA_FILE_NAME
 
 from connectors_qa.checks.version import CheckVersionIncrement
+from connectors_qa.models import CheckStatus
 
 
 class TestVersionIncrementCheck:
@@ -13,57 +14,42 @@ class TestVersionIncrementCheck:
         connector = mocker.Mock(code_directory=str(tmp_path), technical_name="mock-connector")
         return connector
 
-    def _get_version_increment_check(self, mocker, connector, master_version="1.0.0", current_version="1.0.1"):
+    def _get_version_increment_check(self, mocker, master_version="1.0.0", current_version="1.0.1"):
         mocker.patch(
-            "pipelines.airbyte_ci.connectors.test.steps.common.VersionIncrementCheck.master_connector_version",
-            new_callable=mocker.PropertyMock,
+            "connectors_qa.checks.version.CheckVersionIncrement._get_master_connector_version",
             return_value=semver.Version.parse(master_version),
         )
         mocker.patch(
-            "pipelines.airbyte_ci.connectors.test.steps.common.VersionIncrementCheck.current_connector_version",
-            new_callable=mocker.PropertyMock,
+            "connectors_qa.checks.version.CheckVersionIncrement._get_current_connector_version",
             return_value=semver.Version.parse(current_version),
         )
 
-        return CheckVersionIncrement(connector)
+        return CheckVersionIncrement()
 
-    def test_should_run(self, connector):
-        connector.modified_files = ["some_file"]
-        assert CheckVersionIncrement(connector).should_run
+    def test_validate_success(self, mocker, mock_connector):
+        version_increment_check = self._get_version_increment_check(mocker, master_version="1.0.0", current_version="1.0.1")
+        result = version_increment_check._run(mock_connector)
+        assert result.status == CheckStatus.PASSED
 
-    def test_should_not_run(self, context):
-        for bypassed_file in CheckVersionIncrement.BYPASS_CHECK_FOR:
-            context.modified_files = [bypassed_file]
-            assert not CheckVersionIncrement(context).should_run
-
-    def test_validate_success_no_rc_increment(self, mocker, context):
-        version_increment_check = self._get_version_increment_check(mocker, context, master_version="1.0.0", current_version="1.0.1")
-        result = version_increment_check.validate()
-        assert result.success
-
-    def test_validate_failure_no_increment(self, context, mocker):
-        version_increment_check = self._get_version_increment_check(mocker, context, master_version="1.0.0", current_version="1.0.0")
-        result = version_increment_check.validate()
-        assert not result.success
+    def test_validate_failure_no_increment(self, mock_connector, mocker):
+        version_increment_check = self._get_version_increment_check(mocker, master_version="1.0.0", current_version="1.0.0")
+        result = version_increment_check._run(mock_connector)
+        assert result.status == CheckStatus.FAILED
         assert (
-            result.stderr
-            == f"The dockerImageTag in {METADATA_FILE_NAME} was not incremented. Master version is {version_increment_check.master_connector_version}, current version is {version_increment_check.current_connector_version}"
+            result.message
+            == f"The dockerImageTag in {METADATA_FILE_NAME} was not incremented. Master version is 1.0.0, current version is 1.0.0"
         )
 
-    def test_validate_failure_rc_with_different_versions(self, context, mocker):
-        version_increment_check = self._get_version_increment_check(
-            mocker, context, master_version="1.0.0-rc.1", current_version="1.0.1-rc.1"
-        )
-        result = version_increment_check.validate()
-        assert not result.success
+    def test_validate_success_rc_increment(self, mock_connector, mocker):
+        version_increment_check = self._get_version_increment_check(mocker, master_version="1.0.1-rc.1", current_version="1.0.1-rc.2")
+        result = version_increment_check._run(mock_connector)
+        assert result.status == CheckStatus.PASSED
+
+    def test_validate_failure_rc_with_different_versions(self, mock_connector, mocker):
+        version_increment_check = self._get_version_increment_check(mocker, master_version="1.0.0-rc.1", current_version="1.0.1-rc.1")
+        result = version_increment_check._run(mock_connector)
+        assert result.status == CheckStatus.FAILED
         assert (
-            result.stderr
-            == f"Master and current version are release candidates but they have different major, minor or patch versions. Release candidates should only differ in the prerelease part. Master version is {version_increment_check.master_connector_version}, current version is {version_increment_check.current_connector_version}"
+            result.message
+            == f"Master and current version are release candidates but they have different major, minor or patch versions. Release candidates should only differ in the prerelease part. Master version is 1.0.0-rc.1, current version is 1.0.1-rc.1"
         )
-
-    def test_validate_success_rc_increment(self, context, mocker):
-        version_increment_check = self._get_version_increment_check(
-            mocker, context, master_version="1.0.1-rc.1", current_version="1.0.1-rc.2"
-        )
-        result = version_increment_check.validate()
-        assert result.success
