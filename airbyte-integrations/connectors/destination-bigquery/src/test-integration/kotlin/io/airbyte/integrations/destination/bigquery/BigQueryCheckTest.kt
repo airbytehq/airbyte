cc770bbda13acb9e08460bbb5d99138fa9b67039
Edward Gao <edward.gao@airbyte.io>
2025-05-13 14:45:36 -0700

    bigquery: disable broken tests (#60247)

--- airbyte-integrations/connectors/destination-bigquery/src/test-integration/kotlin/io/airbyte/integrations/destination/bigquery/BigQueryCheckTest.kt
+++ airbyte-integrations/connectors/destination-bigquery/src/test-integration/kotlin/io/airbyte/integrations/destination/bigquery/BigQueryCheckTest.kt
@@ -19,27 +19,36 @@ class BigQueryCheckTest :
         failConfigFilenamesAndFailureReasons =
             mapOf(
                 CheckTestConfig(
-                    BigQueryDestinationTestUtils.createConfig("secrets/credentials-badproject.json")
+                    BigQueryDestinationTestUtils.createConfig(
+                        "secrets/credentials-badproject.json"
+                    ),
+                    name = "bad project",
                 ) to
                     Pattern.compile(
                         "Access Denied: Project fake: User does not have bigquery.datasets.create permission in project fake"
                     ),
-                CheckTestConfig(
-                    BigQueryDestinationTestUtils.createConfig(
-                        "secrets/credentials-no-edit-public-schema-role.json"
-                    )
-                ) to Pattern.compile("Permission bigquery.tables.create denied"),
-                CheckTestConfig(
-                    BigQueryDestinationTestUtils.createConfig(
-                        "secrets/credentials-standard-no-dataset-creation.json"
-                    )
-                ) to Pattern.compile("Permission bigquery.tables.create denied"),
-                CheckTestConfig(
-                    BigQueryDestinationTestUtils.createConfig(
-                        "secrets/credentials-1s1t-gcs-bad-copy-permission.json"
-                    )
-                ) to Pattern.compile("Permission bigquery.tables.updateData denied on table"),
-            ),
+                // these tests somehow hang in CI.
+                // CheckTestConfig(
+                //     BigQueryDestinationTestUtils.createConfig(
+                //         "secrets/credentials-no-edit-public-schema-role.json"
+                //     ),
+                //     name = "no edit public schema role",
+                // ) to Pattern.compile("Permission bigquery.tables.create denied"),
+                // CheckTestConfig(
+                //     BigQueryDestinationTestUtils.createConfig(
+                //         "secrets/credentials-standard-no-dataset-creation.json"
+                //     ),
+                //     name = "no dataset creation",
+                // ) to Pattern.compile("Permission bigquery.tables.create denied"),
+                // somehow this test causes the docker container to emit a malformed log message
+                // (it's truncated).
+                // CheckTestConfig(
+                //     BigQueryDestinationTestUtils.createConfig(
+                //         "secrets/credentials-1s1t-gcs-bad-copy-permission.json"
+                //     ),
+                //     name = "gcs bad copy permission",
+                // ) to Pattern.compile("Permission bigquery.tables.updateData denied on table"),
+                ),
         additionalMicronautEnvs = additionalMicronautEnvs,
     ) {
     @Test
