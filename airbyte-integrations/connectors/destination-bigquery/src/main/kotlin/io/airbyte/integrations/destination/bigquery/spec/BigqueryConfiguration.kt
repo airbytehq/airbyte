2a184f2b3d0a571ff56b8008b48284162c4276c7
Edward Gao <edward.gao@airbyte.io>
2025-05-14 08:53:52 -0700

    bigquery: allow unlimited concurrency, remove spec option (#60258)

--- airbyte-integrations/connectors/destination-bigquery/src/main/kotlin/io/airbyte/integrations/destination/bigquery/spec/BigqueryConfiguration.kt
+++ airbyte-integrations/connectors/destination-bigquery/src/main/kotlin/io/airbyte/integrations/destination/bigquery/spec/BigqueryConfiguration.kt
@@ -19,8 +19,9 @@ data class BigqueryConfiguration(
     val transformationPriority: TransformationPriority,
     val rawTableDataset: String,
     val disableTypingDeduping: Boolean,
-    override val numOpenStreamWorkers: Int,
-) : DestinationConfiguration()
+) : DestinationConfiguration() {
+    override val numOpenStreamWorkers = 3
+}
 
 sealed interface LoadingMethodConfiguration
 
@@ -61,7 +62,6 @@ class BigqueryConfigurationFactory :
                     pojo.rawTableDataset!!
                 },
             disableTypingDeduping = pojo.disableTypingDeduping ?: false,
-            numOpenStreamWorkers = pojo.numOpenStreamWorkers ?: 10,
         )
     }
 }
