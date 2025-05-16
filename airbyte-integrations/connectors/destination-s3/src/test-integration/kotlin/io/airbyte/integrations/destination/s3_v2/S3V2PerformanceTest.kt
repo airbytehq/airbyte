bd6b5345e74fda577f0b81c5f926779683b25b91
tryangul <ryan.broughan@gmail.com>
2025-05-16 14:17:27 -0700

    Enable file perf test in CI so we catch files regressions.

--- airbyte-integrations/connectors/destination-s3/src/test-integration/kotlin/io/airbyte/integrations/destination/s3_v2/S3V2PerformanceTest.kt
+++ airbyte-integrations/connectors/destination-s3/src/test-integration/kotlin/io/airbyte/integrations/destination/s3_v2/S3V2PerformanceTest.kt
@@ -8,6 +8,40 @@ import io.airbyte.cdk.load.write.BasicPerformanceTest
 import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 
+// Just ensure file transfer gets tested in CI
+class FileTransferPerformanceTest :
+    BasicPerformanceTest(
+        configContents = S3V2TestUtils.getConfig(S3V2TestUtils.JSON_UNCOMPRESSED_CONFIG_PATH),
+        configSpecClass = S3V2Specification::class.java,
+        defaultRecordsToInsert = 1_000_000,
+        micronautProperties = S3V2TestUtils.PERFORMANCE_TEST_MICRONAUT_PROPERTIES,
+        numFilesForFileTransfer = 5,
+        fileSizeMbForFileTransfer = 1024,
+    ) {
+
+    @Test
+    override fun testFileTransferNew() {
+        super.testFileTransferNew()
+    }
+
+    @Disabled
+    override fun testInsertRecords() {}
+    @Disabled
+    override fun testRefreshingRecords() {}
+    @Disabled
+    override fun testInsertRecordsComplexTypes() {}
+    @Disabled
+    override fun testInsertRecordsWithDedup() {}
+    @Disabled
+    override fun testInsertRecordsWithManyColumns() {}
+    @Disabled
+    override fun testAppendRecordsWithDuplicates() {}
+    @Disabled
+    override fun testFileTransferOld() {}
+    @Disabled
+    override fun testManyStreamsInsertRecords() {}
+}
+
 @Disabled("We don't want this to run in CI")
 class S3V2JsonNoFrillsPerformanceTest :
     BasicPerformanceTest(
