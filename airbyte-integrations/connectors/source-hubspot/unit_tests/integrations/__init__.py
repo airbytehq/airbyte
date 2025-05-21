da5e610cf373f682c385962d60f0f433f436caf6
Daryna Ishchenko <80129833+darynaishchenko@users.noreply.github.com>
2025-05-21 12:48:32 +0300

    feat(source-hubspot) crm search: companies to low code (#60812)
    Co-authored-by: brianjlai <brian.j.lai@gmail.com>

--- airbyte-integrations/connectors/source-hubspot/unit_tests/integrations/__init__.py
+++ airbyte-integrations/connectors/source-hubspot/unit_tests/integrations/__init__.py
@@ -22,6 +22,7 @@ from .response_builder.streams import GenericResponseBuilder, HubspotStreamRespo
 
 OBJECTS_WITH_DYNAMIC_SCHEMA = [
     "calls",
+    "company",
     "deal",
     "emails",
     "form",
