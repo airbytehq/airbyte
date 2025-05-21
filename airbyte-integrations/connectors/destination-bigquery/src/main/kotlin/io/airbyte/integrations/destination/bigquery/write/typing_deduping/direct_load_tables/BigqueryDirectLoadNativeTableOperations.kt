9d3bebf5ba1b446c8bcdeae29ab654e2448c2a74
Edward Gao <edward.gao@airbyte.io>
2025-05-21 10:44:44 -0700

    destination bigquery: start direct-load implementation (#59757)

--- airbyte-integrations/connectors/destination-bigquery/src/main/kotlin/io/airbyte/integrations/destination/bigquery/write/typing_deduping/direct_load_tables/BigqueryDirectLoadNativeTableOperations.kt
+++ airbyte-integrations/connectors/destination-bigquery/src/main/kotlin/io/airbyte/integrations/destination/bigquery/write/typing_deduping/direct_load_tables/BigqueryDirectLoadNativeTableOperations.kt
@@ -2,7 +2,7 @@
  * Copyright (c) 2025 Airbyte, Inc., all rights reserved.
  */
 
-package io.airbyte.integrations.destination.bigquery.typing_deduping
+package io.airbyte.integrations.destination.bigquery.write.typing_deduping.direct_load_tables
 
 import com.google.cloud.bigquery.BigQuery
 import com.google.cloud.bigquery.Field
@@ -10,7 +10,6 @@ import com.google.cloud.bigquery.QueryJobConfiguration
 import com.google.cloud.bigquery.StandardSQLTypeName
 import com.google.cloud.bigquery.StandardTableDefinition
 import com.google.cloud.bigquery.TableDefinition
-import com.google.cloud.bigquery.TableId
 import com.google.cloud.bigquery.TimePartitioning
 import com.google.common.annotations.VisibleForTesting
 import io.airbyte.cdk.ConfigErrorException
@@ -22,132 +21,40 @@ import io.airbyte.cdk.load.command.SoftDelete
 import io.airbyte.cdk.load.command.Update
 import io.airbyte.cdk.load.message.Meta
 import io.airbyte.cdk.load.orchestration.db.ColumnNameMapping
-import io.airbyte.cdk.load.orchestration.db.DatabaseInitialStatusGatherer
 import io.airbyte.cdk.load.orchestration.db.TableName
-import io.airbyte.cdk.load.orchestration.db.TableNames
+import io.airbyte.cdk.load.orchestration.db.direct_load_table.DirectLoadTableNativeOperations
 import io.airbyte.cdk.load.orchestration.db.legacy_typing_deduping.AlterTableReport
-import io.airbyte.cdk.load.orchestration.db.legacy_typing_deduping.FinalTableInitialStatus
-import io.airbyte.cdk.load.orchestration.db.legacy_typing_deduping.RawTableInitialStatus
-import io.airbyte.cdk.load.orchestration.db.legacy_typing_deduping.TableCatalog
-import io.airbyte.cdk.load.orchestration.db.legacy_typing_deduping.TypingDedupingDatabaseInitialStatus
 import io.airbyte.cdk.util.CollectionUtils.containsAllIgnoreCase
 import io.airbyte.cdk.util.CollectionUtils.containsIgnoreCase
 import io.airbyte.cdk.util.CollectionUtils.matchingKey
 import io.github.oshai.kotlinlogging.KotlinLogging
-import java.math.BigInteger
 import java.util.stream.Collectors
 import java.util.stream.Stream
 
 private val logger = KotlinLogging.logger {}
 
-class BigqueryDatabaseInitialStatusGatherer(private val bq: BigQuery) :
-    DatabaseInitialStatusGatherer<TypingDedupingDatabaseInitialStatus> {
-    private fun findExistingTable(finalTableName: TableName): TableDefinition? {
-        val table = bq.getTable(finalTableName.namespace, finalTableName.name)
-        return table?.getDefinition()
+class BigqueryDirectLoadNativeTableOperations(private val bigquery: BigQuery) :
+    DirectLoadTableNativeOperations {
+    override fun ensureSchemaMatches(
+        stream: DestinationStream,
+        tableName: TableName,
+        columnNameMapping: ColumnNameMapping
+    ) {
+        TODO("Not yet implemented")
     }
 
-    private fun isFinalTableEmpty(finalTableName: TableName): Boolean {
-        return BigInteger.ZERO ==
-            bq.getTable(TableId.of(finalTableName.namespace, finalTableName.name)).numRows
-    }
-
-    private fun getInitialRawTableState(
-        rawTableName: TableName,
-        suffix: String
-    ): RawTableInitialStatus? {
-        bq.getTable(TableId.of(rawTableName.namespace, rawTableName.name + suffix))
-        // Table doesn't exist. There are no unprocessed records, and no timestamp.
-        ?: return null
-
-        val rawTableIdQuoted = """`${rawTableName.namespace}`.`${rawTableName.name}$suffix`"""
-        val unloadedRecordTimestamp =
-            bq.query(
-                    QueryJobConfiguration.of(
-                        """
-                            SELECT TIMESTAMP_SUB(MIN(_airbyte_extracted_at), INTERVAL 1 MICROSECOND)
-                            FROM $rawTableIdQuoted
-                            WHERE _airbyte_loaded_at IS NULL
-                            """.trimIndent()
-                    )
-                )
-                .iterateAll()
-                .iterator()
-                .next()
-                .first()
-        // If this value is null, then there are no records with null loaded_at.
-        // If it's not null, then we can return immediately - we've found some unprocessed records
-        // and their timestamp.
-        if (!unloadedRecordTimestamp.isNull) {
-            return RawTableInitialStatus(
-                hasUnprocessedRecords = true,
-                maxProcessedTimestamp = unloadedRecordTimestamp.timestampInstant,
+    override fun getGenerationId(tableName: TableName): Long {
+        val result =
+            bigquery.query(
+                QueryJobConfiguration.of(
+                    "SELECT _airbyte_generation_id FROM ${tableName.namespace}.${tableName.name} LIMIT 1"
+                ),
             )
-        }
-
-        val loadedRecordTimestamp =
-            bq.query(
-                    QueryJobConfiguration.of(
-                        """
-                    SELECT MAX(_airbyte_extracted_at)
-                    FROM $rawTableIdQuoted
-                    """.trimIndent()
-                    )
-                )
-                .iterateAll()
-                .iterator()
-                .next()
-                .first()
-        // We know (from the previous query) that all records have been processed by T+D already.
-        // So we just need to get the timestamp of the most recent record.
-        return if (loadedRecordTimestamp.isNull) {
-            // Null timestamp because the table is empty. T+D can process the entire raw table
-            // during this sync.
-            RawTableInitialStatus(hasUnprocessedRecords = false, maxProcessedTimestamp = null)
+        val value = result.iterateAll().first().get(Meta.COLUMN_NAME_AB_GENERATION_ID)
+        return if (value.isNull) {
+            0
         } else {
-            // The raw table already has some records. T+D can skip all records with timestamp <=
-            // this value.
-            RawTableInitialStatus(
-                hasUnprocessedRecords = false,
-                maxProcessedTimestamp = loadedRecordTimestamp.timestampInstant
-            )
-        }
-    }
-
-    override suspend fun gatherInitialStatus(
-        streams: TableCatalog,
-    ): Map<DestinationStream, TypingDedupingDatabaseInitialStatus> {
-        return streams.mapValues { (stream, names) ->
-            val (tableNames, columnNameMapping) = names
-            val finalTable = findExistingTable(tableNames.finalTableName!!)
-            val finalTableStatus =
-                finalTable?.let {
-                    FinalTableInitialStatus(
-                        isSchemaMismatch =
-                            !existingSchemaMatchesStreamConfig(
-                                stream,
-                                columnNameMapping,
-                                finalTable
-                            ),
-                        isEmpty = isFinalTableEmpty(tableNames.finalTableName!!),
-                        // for now, just use 0. this means we will always use a temp final table.
-                        // platform has a workaround for this, so it's OK.
-                        // TODO only fetch this on truncate syncs
-                        // TODO once we have destination state, use that instead of a query
-                        finalTableGenerationId = 0,
-                    )
-                }
-            val rawTableState = getInitialRawTableState(tableNames.rawTableName!!, "")
-            val tempRawTableState =
-                getInitialRawTableState(
-                    tableNames.rawTableName!!,
-                    TableNames.TMP_TABLE_SUFFIX,
-                )
-            TypingDedupingDatabaseInitialStatus(
-                finalTableStatus,
-                rawTableState,
-                tempRawTableState,
-            )
+            value.longValue
         }
     }
 
@@ -178,8 +85,9 @@ class BigqueryDatabaseInitialStatusGatherer(private val bq: BigQuery) :
         val pks = getPks(stream, columnNameMapping)
 
         val streamSchema: Map<String, StandardSQLTypeName> =
-            stream.schema.asColumns().entries.associate {
-                columnNameMapping[it.key]!! to BigQuerySqlGenerator.toDialectType(it.value.type)
+            (stream.schema as ObjectType).properties.entries.associate {
+                columnNameMapping[it.key]!! to
+                    BigqueryDirectLoadSqlGenerator.toDialectType(it.value.type)
             }
 
         val existingSchema =
@@ -238,6 +146,7 @@ class BigqueryDatabaseInitialStatusGatherer(private val bq: BigQuery) :
         )
     }
 
+    // TODO this stuff will be useful for ensureSchemaMatches, maybe
     companion object {
         @VisibleForTesting
         fun clusteringMatches(
@@ -248,7 +157,7 @@ class BigqueryDatabaseInitialStatusGatherer(private val bq: BigQuery) :
             return (existingTable.clustering != null &&
                 containsAllIgnoreCase(
                     HashSet<String>(existingTable.clustering!!.fields),
-                    BigQuerySqlGenerator.clusteringColumns(stream, columnNameMapping)
+                    BigqueryDirectLoadSqlGenerator.clusteringColumns(stream, columnNameMapping)
                 ))
         }
 
