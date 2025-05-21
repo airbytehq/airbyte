c66b42833f3560b24ddc68573c4da542cc6a2ab7
letiescanciano <leticia.escanciano@gmail.com>
2025-05-21 09:41:05 +0200

    move trigger to Navbar

--- docusaurus/src/components/MarkpromptChat/MarkpromptChat.jsx
+++ docusaurus/src/components/MarkpromptChat/MarkpromptChat.jsx
@@ -1,3 +1,4 @@
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 import "@markprompt/css";
 import { Markprompt } from "@markprompt/react";
 import React from "react";
@@ -5,10 +6,16 @@ import ChatTrigger from "./ChatTrigger";
 import "./ChatTrigger.module.css";
 
 export default function MarkpromptChat() {
+  const {
+    siteConfig: {
+      customFields: { markpromptProjectKey },
+    },
+  } = useDocusaurusContext();
   return (
     <Markprompt
-      projectKey="pk_c87ydSnE4o1tX9txQReh3vDvVnKDnbje" // public key, safe to commit
+      projectKey={markpromptProjectKey}
       defaultView="chat"
+      customElement={true}
       display="sheet"
       chat={{
         assistantId: "d1399022-d7e2-4404-bd16-8b3ad2b5465b",
