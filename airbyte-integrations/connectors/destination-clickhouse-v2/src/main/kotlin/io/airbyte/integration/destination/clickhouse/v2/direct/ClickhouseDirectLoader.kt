b415c8fb82c4a9dade3c91a47cfc98897ac89e55
benmoriceau <benoit@airbyte.io>
2025-05-27 10:24:39 -0700

    TMP

---
+++ airbyte-integrations/connectors/destination-clickhouse-v2/src/main/kotlin/io/airbyte/integration/destination/clickhouse/v2/direct/ClickhouseDirectLoader.kt
@@ -0,0 +1,34 @@
+package io.airbyte.integration.destination.clickhouse.v2.direct
+
+import com.clickhouse.client.api.Client
+import com.clickhouse.data.ClickHouseFormat
+import io.airbyte.cdk.load.message.DestinationRecordRaw
+import io.airbyte.cdk.load.write.DirectLoader
+import java.io.ByteArrayInputStream
+import java.io.ByteArrayOutputStream
+import java.io.InputStream
+
+class ClickhouseDirectLoader(private val clickhouseClient: Client) : DirectLoader {
+    private var buffer: ByteArrayOutputStream = ByteArrayOutputStream()
+
+    override fun accept(record: DestinationRecordRaw): DirectLoader.DirectLoadResult {
+        val byteArray = "${record.rawData.record.data}\n".toByteArray()
+
+        buffer.write(byteArray)
+        if (buffer!!.size() > 15 * 1024 * 1024) {
+            val inputStream: InputStream = ByteArrayInputStream(buffer.toByteArray())
+            clickhouseClient.insert("test", inputStream, ClickHouseFormat.JSONEachRow)
+        }
+
+        // rely on the CDK to tell us when to finish()
+        return DirectLoader.Incomplete
+    }
+
+    override fun finish() {
+        TODO("Not yet implemented")
+    }
+
+    override fun close() {
+        TODO("Not yet implemented")
+    }
+}
