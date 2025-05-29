df6ffdea317c89a2fe4955acd1c99e75ed8a04a3
Francis Genet <francis.genet@airbyte.io>
2025-05-29 16:04:41 -0700

    Format

--- airbyte-integrations/connectors/destination-bigquery/src/main/kotlin/io/airbyte/integrations/destination/bigquery/write/bulk_loader/BigQueryCsvValueProcessor.kt
+++ airbyte-integrations/connectors/destination-bigquery/src/main/kotlin/io/airbyte/integrations/destination/bigquery/write/bulk_loader/BigQueryCsvValueProcessor.kt
@@ -1,3 +1,7 @@
+/*
+ * Copyright (c) 2025 Airbyte, Inc., all rights reserved.
+ */
+
 package io.airbyte.integrations.destination.bigquery.write.bulk_loader
 
 import io.airbyte.cdk.load.data.AirbyteValue
