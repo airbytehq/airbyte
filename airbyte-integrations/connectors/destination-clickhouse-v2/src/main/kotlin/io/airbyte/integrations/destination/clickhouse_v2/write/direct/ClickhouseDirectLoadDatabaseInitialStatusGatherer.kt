9ae2922b1189b6faae8d21a599ad7be63d04697a
benmoriceau <benoit@airbyte.io>
2025-06-02 16:47:55 -0700

    Example for abstraction

--- airbyte-integrations/connectors/destination-clickhouse-v2/src/main/kotlin/io/airbyte/integrations/destination/clickhouse_v2/write/direct/ClickhouseDirectLoadDatabaseInitialStatusGatherer.kt
+++ airbyte-integrations/connectors/destination-clickhouse-v2/src/main/kotlin/io/airbyte/integrations/destination/clickhouse_v2/write/direct/ClickhouseDirectLoadDatabaseInitialStatusGatherer.kt
@@ -1,50 +1,28 @@
 package io.airbyte.integrations.destination.clickhouse_v2.write.direct
 
 import com.clickhouse.client.api.Client
+import com.clickhouse.client.api.data_formats.ClickHouseBinaryFormatReader
+import io.airbyte.cdk.load.client.AirbyteClient
 import io.airbyte.cdk.load.command.DestinationStream
+import io.airbyte.cdk.load.orchestration.db.BaseDatabaseInitialStatusGatherer
 import io.airbyte.cdk.load.orchestration.db.DatabaseInitialStatusGatherer
 import io.airbyte.cdk.load.orchestration.db.TableName
 import io.airbyte.cdk.load.orchestration.db.direct_load_table.DirectLoadInitialStatus
 import io.airbyte.cdk.load.orchestration.db.direct_load_table.DirectLoadTableStatus
 import io.airbyte.cdk.load.orchestration.db.legacy_typing_deduping.TableCatalog
+import io.airbyte.integrations.destination.clickhouse_v2.spec.ClickhouseConfiguration
+import io.github.oshai.kotlinlogging.KotlinLogging
+import jakarta.inject.Singleton
 import java.util.concurrent.ConcurrentHashMap
 import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.launch
 
 class ClickhouseDirectLoadDatabaseInitialStatusGatherer(
-    private val clickhouseClient: Client,
-    private val internalTableDataset: String = "airbyte_internal",
-) : DatabaseInitialStatusGatherer<DirectLoadInitialStatus> {
-    // TODO: See if there is a different way.
-    override suspend fun gatherInitialStatus(streams: TableCatalog): Map<DestinationStream, DirectLoadInitialStatus> {
-        val map = ConcurrentHashMap<DestinationStream, DirectLoadInitialStatus>(streams.size)
-        coroutineScope {
-            streams.forEach { (stream, tableNameInfo) ->
-                launch {
-                    val tableName = tableNameInfo.tableNames.finalTableName!!
-                    map[stream] =
-                        DirectLoadInitialStatus(
-                            realTable = getTableStatus(tableName),
-                            // TODO this feels sketchy. We maybe should compute the temp table name
-                            //   in DirectLoadTableWriter, then pass that down to the status
-                            //   gatherer (and wherever else we're using it)?
-                            tempTable =
-                            getTableStatus(
-                                tableName.asTempTable(internalNamespace = internalTableDataset)
-                            ),
-                        )
-                }
-            }
-        }
-        return map
-    }
-
-    // TODO: Implement this
-    private fun getTableStatus(tableName: TableName): DirectLoadTableStatus? {
-        // val table = clickhouseClient.query("SELECT count(1) FROM ${tableName.name}").get()
-        //     .inputStream.
-//        return DirectLoadTableStatus(isEmpty = true)
-        return null
-    }
-
-}
+    airbyteClient: AirbyteClient,
+    clickhouseConfiguration: ClickhouseConfiguration,
+    internalTableDataset: String = "airbyte_internal",
+) : BaseDatabaseInitialStatusGatherer<DirectLoadInitialStatus>(
+    airbyteClient,
+    clickhouseConfiguration.resolvedDatabase,
+    internalTableDataset,
+)
