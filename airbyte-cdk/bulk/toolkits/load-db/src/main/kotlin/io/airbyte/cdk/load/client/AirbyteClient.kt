644ed96b9682973a24caef78add290239ade861c
benmoriceau <benoit@airbyte.io>
2025-06-03 16:32:49 -0700

    Revert "Example for abstraction"
    This reverts commit 9ae2922b1189b6faae8d21a599ad7be63d04697a.

--- airbyte-cdk/bulk/toolkits/load-db/src/main/kotlin/io/airbyte/cdk/load/client/AirbyteClient.kt
+++
@@ -1,6 +0,0 @@
-package io.airbyte.cdk.load.client
-
-abstract class AirbyteClient {
-    // TODO: missing namespace
-    abstract fun getNumberOfRecordsInTable(database: String, table: String): Long
-}
