648cfe284919a1418cea54685fba86b2c40da661
Anatolii Yatsuk <tolikyatsuk@gmail.com>
2025-06-05 16:54:56 +0300

    Change airbyte-ci install mode to source

--- airbyte-ci/connectors/pipelines/pipelines/airbyte_ci/connectors/build_image/steps/build_customization.py
+++ airbyte-ci/connectors/pipelines/pipelines/airbyte_ci/connectors/build_image/steps/build_customization.py
@@ -58,7 +58,6 @@ def get_main_file_name(connector: Connector) -> str:
 
 def get_entrypoint(connector: Connector) -> List[str]:
     main_file_name = get_main_file_name(connector)
-    # adding a random comment to trigger the airbyte-ci build again
     return ["ddtrace-run", "python", f"/airbyte/integration_code/{main_file_name}"]
 
 
