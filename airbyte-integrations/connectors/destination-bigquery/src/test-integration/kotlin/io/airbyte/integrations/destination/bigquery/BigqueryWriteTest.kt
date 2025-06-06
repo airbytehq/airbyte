fef8fb3ee5cb774c6b11c6a6f24a36873a40192c
Johnny Schmidt <john.schmidt@airbyte.io>
2025-06-06 09:35:39 -0700

    [Load-CDK] Speed: Unbreak what speed broke (#61420)

--- airbyte-integrations/connectors/destination-bigquery/src/test-integration/kotlin/io/airbyte/integrations/destination/bigquery/BigqueryWriteTest.kt
+++ airbyte-integrations/connectors/destination-bigquery/src/test-integration/kotlin/io/airbyte/integrations/destination/bigquery/BigqueryWriteTest.kt
@@ -7,6 +7,7 @@ package io.airbyte.integrations.destination.bigquery
 import io.airbyte.cdk.load.command.Append
 import io.airbyte.cdk.load.command.Dedupe
 import io.airbyte.cdk.load.command.DestinationStream
+import io.airbyte.cdk.load.command.NamespaceMapper
 import io.airbyte.cdk.load.data.ObjectType
 import io.airbyte.cdk.load.message.InputRecord
 import io.airbyte.cdk.load.test.util.DestinationDataDumper
@@ -108,12 +109,14 @@ abstract class BigqueryTDWriteTest(configContents: String) :
     open fun testAppendCdkMigration() {
         val stream =
             DestinationStream(
-                DestinationStream.Descriptor(randomizedNamespace, "test_stream"),
+                unmappedNamespace = randomizedNamespace,
+                unmappedName = "test_stream",
                 Append,
                 ObjectType(linkedMapOf("id" to intType)),
                 generationId = 0,
                 minimumGenerationId = 0,
                 syncId = 42,
+                namespaceMapper = NamespaceMapper()
             )
         // Run a sync on the old CDK
         runSync(
@@ -191,12 +194,14 @@ abstract class BigqueryTDWriteTest(configContents: String) :
     open fun testDedupCdkMigration() {
         val stream =
             DestinationStream(
-                DestinationStream.Descriptor(randomizedNamespace, "test_stream"),
+                unmappedNamespace = randomizedNamespace,
+                unmappedName = "test_stream",
                 Dedupe(primaryKey = listOf(listOf("id")), cursor = emptyList()),
                 ObjectType(linkedMapOf("id" to intType)),
                 generationId = 0,
                 minimumGenerationId = 0,
                 syncId = 42,
+                namespaceMapper = NamespaceMapper(),
             )
         // Run a sync on the old CDK
         runSync(
