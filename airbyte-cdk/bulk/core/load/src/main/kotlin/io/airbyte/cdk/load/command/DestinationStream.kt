f8a317a1efeb8334a7bbffc8262defa33d0369ed
maxime.c <maxime@airbyte.io>
2025-06-09 09:44:52 -0400

    add Data Activation fields to stream

--- airbyte-cdk/bulk/core/load/src/main/kotlin/io/airbyte/cdk/load/command/DestinationStream.kt
+++ airbyte-cdk/bulk/core/load/src/main/kotlin/io/airbyte/cdk/load/command/DestinationStream.kt
@@ -59,6 +59,8 @@ data class DestinationStream(
     val isFileBased: Boolean = false,
     // whether we will move the file (in addition to the metadata)
     val includeFiles: Boolean = false,
+    val destinationObjectName: String? = null,
+    val matchingKey: List<String> = emptyList<String>(),
     private val namespaceMapper: NamespaceMapper
 ) {
     val descriptor = namespaceMapper.map(namespace = unmappedNamespace, name = unmappedName)
@@ -184,9 +186,23 @@ class DestinationStreamFactory(
             schema = jsonSchemaToAirbyteType.convert(stream.stream.jsonSchema),
             isFileBased = stream.stream.isFileBased ?: false,
             includeFiles = stream.includeFiles ?: false,
+            destinationObjectName = stream.destinationObjectName,
+            matchingKey = stream.destinationObjectName?.let { fromCompositeNestedKeyToCompositeKey(stream.primaryKey) }.orEmpty()
         )
     }
 }
+
+private fun fromCompositeNestedKeyToCompositeKey(compositeNestedKey: List<List<String>>) : List<String> {
+    if (compositeNestedKey.any { it.size > 1 }) {
+        throw IllegalArgumentException("Nested keys are not supported for matching keys. Key was $compositeNestedKey")
+    }
+    if (compositeNestedKey.any { it.isEmpty() }) {
+        throw IllegalArgumentException("Parts of the composite key need to have at least one element. Key was $compositeNestedKey")
+    }
+
+    return compositeNestedKey.map { it[0] }.toList()
+}
+
 
 sealed interface ImportType
 
