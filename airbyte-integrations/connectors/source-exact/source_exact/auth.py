b55e4df8880447bc3d0e909cdc256a6a2d977d94
Jeroen de Vries <jeroen@maildevries.eu>
2025-06-09 15:40:20 +0200

    keuze voor Oauth2authenticator aangezien refresh token 30 dagen geldig id

--- airbyte-integrations/connectors/source-exact/source_exact/auth.py
+++ airbyte-integrations/connectors/source-exact/source_exact/auth.py
@@ -1,7 +1,7 @@
-from airbyte_cdk import SingleUseRefreshTokenOauth2Authenticator
+from airbyte_cdk.sources.streams.http.requests_native_auth import Oauth2Authenticator
 
 
-class ExactOauth2Authenticator(SingleUseRefreshTokenOauth2Authenticator):
+class ExactOauth2Authenticator(Oauth2Authenticator):
     pass
 
 
