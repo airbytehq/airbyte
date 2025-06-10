5aaaca4a1fe5ab6431d6a83a6509926b9cd3736c
Edward Gao <edward.gao@airbyte.io>
2025-06-10 12:50:03 -0700

    Bulk load CDK: Behave correctly in truncate refresh when receiving INCOMPLETE stream status (#61382)

--- airbyte-cdk/bulk/core/load/src/main/kotlin/io/airbyte/cdk/load/state/PipelineEventBookkeepingRouter.kt
+++ airbyte-cdk/bulk/core/load/src/main/kotlin/io/airbyte/cdk/load/state/PipelineEventBookkeepingRouter.kt
@@ -12,10 +12,8 @@ import io.airbyte.cdk.load.message.CheckpointMessage
 import io.airbyte.cdk.load.message.CheckpointMessageWrapped
 import io.airbyte.cdk.load.message.DestinationFile
 import io.airbyte.cdk.load.message.DestinationFileStreamComplete
-import io.airbyte.cdk.load.message.DestinationFileStreamIncomplete
 import io.airbyte.cdk.load.message.DestinationRecord
 import io.airbyte.cdk.load.message.DestinationRecordStreamComplete
-import io.airbyte.cdk.load.message.DestinationRecordStreamIncomplete
 import io.airbyte.cdk.load.message.DestinationStreamAffinedMessage
 import io.airbyte.cdk.load.message.FileTransferQueueEndOfStream
 import io.airbyte.cdk.load.message.FileTransferQueueMessage
@@ -115,13 +113,6 @@ class PipelineEventBookkeepingRouter(
                 }
                 PipelineEndOfStream(stream.descriptor)
             }
-            is DestinationRecordStreamIncomplete -> {
-                log.info { "Read INCOMPLETE for stream ${stream.descriptor}" }
-                if (!markEndOfStreamAtEndOfSync) {
-                    manager.markEndOfStream(false)
-                }
-                PipelineEndOfStream(stream.descriptor)
-            }
 
             // DEPRECATED: Legacy file transfer
             is DestinationFile -> {
@@ -137,10 +128,6 @@ class PipelineEventBookkeepingRouter(
                 fileTransferQueue.publish(FileTransferQueueEndOfStream(stream))
                 PipelineHeartbeat()
             }
-            is DestinationFileStreamIncomplete ->
-                throw IllegalStateException(
-                    "File stream ${stream.descriptor} failed upstream, cannot continue."
-                )
         }
     }
 
