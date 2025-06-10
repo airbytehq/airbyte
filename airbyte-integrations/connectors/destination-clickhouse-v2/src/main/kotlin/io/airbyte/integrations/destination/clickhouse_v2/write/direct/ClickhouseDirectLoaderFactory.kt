b5f6c8f5c9cfb250674aa713490123bc17c1c64b
tryangul <ryan.broughan@gmail.com>
2025-06-10 16:18:26 -0700

    Generate uuids.

--- airbyte-integrations/connectors/destination-clickhouse-v2/src/main/kotlin/io/airbyte/integrations/destination/clickhouse_v2/write/direct/ClickhouseDirectLoaderFactory.kt
+++ airbyte-integrations/connectors/destination-clickhouse-v2/src/main/kotlin/io/airbyte/integrations/destination/clickhouse_v2/write/direct/ClickhouseDirectLoaderFactory.kt
@@ -7,15 +7,23 @@ package io.airbyte.integrations.destination.clickhouse_v2.write.direct
 import com.clickhouse.client.api.Client
 import io.airbyte.cdk.load.command.DestinationStream
 import io.airbyte.cdk.load.write.DirectLoaderFactory
+import io.airbyte.integrations.destination.clickhouse_v2.config.UUIDGenerator
 import jakarta.inject.Singleton
 
 @Singleton
-class ClickhouseDirectLoaderFactory(private val clickhouseClient: Client) :
+class ClickhouseDirectLoaderFactory(
+    private val clickhouseClient: Client,
+    private val uuidGenerator: UUIDGenerator,
+) :
     DirectLoaderFactory<ClickhouseDirectLoader> {
     override val maxNumOpenLoaders = 2
 
     override fun create(
         streamDescriptor: DestinationStream.Descriptor,
         part: Int
-    ): ClickhouseDirectLoader = ClickhouseDirectLoader(streamDescriptor, clickhouseClient)
+    ): ClickhouseDirectLoader = ClickhouseDirectLoader(
+        streamDescriptor,
+        clickhouseClient,
+        uuidGenerator,
+    )
 }
