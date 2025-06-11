de39eb9f5f34f713da1aec173f4192e1efe6d5d7
subodhchaturvedi <subodh1810@gmail.com>
2025-06-12 01:39:05 +0530

    fix test

--- airbyte-cdk/bulk/toolkits/load-object-storage/src/test/kotlin/io/airbyte/cdk/load/write/object_storage/ObjectStorageFormattingWriterTest.kt
+++ airbyte-cdk/bulk/toolkits/load-object-storage/src/test/kotlin/io/airbyte/cdk/load/write/object_storage/ObjectStorageFormattingWriterTest.kt
@@ -110,7 +110,6 @@ class JsonFormattingWriterTest {
         assertEquals(
             """
             {"_airbyte_raw_id":"0197604b-ca2e-7e7c-9126-dacc18b68e8e","_airbyte_extracted_at":1234,"_airbyte_meta":{"sync_id":123,"changes":[]},"_airbyte_generation_id":42,"_airbyte_data":{"foo":"bar"}}
-            
             """.trimIndent(),
             os.toByteArray().decodeToString(),
         )
@@ -124,7 +123,6 @@ class JsonFormattingWriterTest {
         assertEquals(
             """
             {"_airbyte_raw_id":"0197604b-ca2e-7e7c-9126-dacc18b68e8e","_airbyte_extracted_at":1234,"_airbyte_meta":{"sync_id":123,"changes":[]},"_airbyte_generation_id":42,"foo":"bar"}
-            
             """.trimIndent(),
             os.toByteArray().decodeToString(),
         )
