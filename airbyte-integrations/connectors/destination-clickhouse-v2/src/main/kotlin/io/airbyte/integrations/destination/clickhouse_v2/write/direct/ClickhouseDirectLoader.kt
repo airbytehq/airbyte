0c40f9e983a4cffc8e762fd3fab680a78cfd4755
tryangul <ryan.broughan@gmail.com>
2025-06-12 15:36:47 -0700

    Save work.

--- airbyte-integrations/connectors/destination-clickhouse-v2/src/main/kotlin/io/airbyte/integrations/destination/clickhouse_v2/write/direct/ClickhouseDirectLoader.kt
+++ airbyte-integrations/connectors/destination-clickhouse-v2/src/main/kotlin/io/airbyte/integrations/destination/clickhouse_v2/write/direct/ClickhouseDirectLoader.kt
@@ -6,7 +6,6 @@ package io.airbyte.integrations.destination.clickhouse_v2.write.direct
 
 import com.clickhouse.client.api.Client
 import com.clickhouse.client.api.data_formats.RowBinaryFormatWriter
-import com.clickhouse.client.api.metadata.TableSchema
 import com.clickhouse.data.ClickHouseFormat
 import edu.umd.cs.findbugs.annotations.SuppressFBWarnings
 import io.airbyte.cdk.load.command.DestinationStream
@@ -24,13 +23,12 @@ import io.airbyte.cdk.load.data.TimeWithoutTimezoneValue
 import io.airbyte.cdk.load.data.TimestampWithTimezoneValue
 import io.airbyte.cdk.load.data.TimestampWithoutTimezoneValue
 import io.airbyte.cdk.load.message.DestinationRecordRaw
-import io.airbyte.cdk.load.message.Meta
+import io.airbyte.cdk.load.util.serializeToString
 import io.airbyte.cdk.load.write.DirectLoader
 import io.github.oshai.kotlinlogging.KotlinLogging
 import java.io.ByteArrayInputStream
 import java.io.ByteArrayOutputStream
 import java.io.InputStream
-import java.time.Instant
 import kotlinx.coroutines.future.await
 
 private val log = KotlinLogging.logger {}
@@ -43,44 +41,13 @@ class ClickhouseDirectLoader(
     private val descriptor: DestinationStream.Descriptor,
     private val clickhouseClient: Client,
 ) : DirectLoader {
-    private val buffer: ClickHouseBinaryBuffer = ClickHouseBinaryBuffer()
-    private val schema: TableSchema = clickhouseClient.getTableSchema(descriptor.name, descriptor.namespace ?: "default")
-    private val writer: RowBinaryFormatWriter = RowBinaryFormatWriter(buffer, schema, ClickHouseFormat.RowBinary)
-
-    private fun writeAirbyteValue(key: String, abValue: AirbyteValue) {
-            when (abValue) {
-                is NullValue -> writer.setValue(key, null)
-                is ObjectValue -> writer.setValue(key, abValue.values.toString())
-                is ArrayValue -> writer.setValue(key, abValue.values.toString())
-                is BooleanValue -> writer.setValue(key, abValue.value)
-                is IntegerValue -> writer.setValue(key, abValue.value)
-                is NumberValue -> writer.setValue(key, abValue.value)
-                is StringValue -> writer.setValue(key, abValue.value)
-                is DateValue -> writer.setValue(key, abValue.value)
-                is TimeWithTimezoneValue -> writer.setValue(key, abValue.value)
-                is TimeWithoutTimezoneValue -> writer.setValue(key, abValue.value)
-                is TimestampWithTimezoneValue -> writer.setValue(key, abValue.value)
-                is TimestampWithoutTimezoneValue -> writer.setValue(key, abValue.value)
-            }
-    }
-
-
-    private fun accRow(record: DestinationRecordRaw) {
-        // coerces and munges record to schema, adding any changes to the meta
-        val enriched = record.asEnrichedDestinationRecordAirbyteValue()
-        val cols = enriched.allTypedFields
-        cols.filter { it.key != Meta.COLUMN_NAME_AB_EXTRACTED_AT }.forEach {
-            when (val abValue = it.value.abValue) {
-
-            }
-        val extractedAt = Instant.ofEpochMilli(cols[Meta.COLUMN_NAME_AB_EXTRACTED_AT].abValue)
-        writer.setValue(Meta.COLUMN_NAME_AB_EXTRACTED_AT, )
-
-        writer.commitRow()
-    }
+    private val buffer = InputOutputBuffer()
+    private val schema = clickhouseClient.getTableSchema(descriptor.name, descriptor.namespace ?: "default")
+    private val writer = RowBinaryFormatWriter(buffer, schema, ClickHouseFormat.RowBinary)
 
     override suspend fun accept(record: DestinationRecordRaw): DirectLoader.DirectLoadResult {
-        accRow(record)
+        // accumulate the record
+        accumulateRecord(record)
 
         // determine whether we're complete
         if (writer.rowCount >= Constants.BATCH_SIZE_RECORDS) {
@@ -90,6 +57,44 @@ class ClickhouseDirectLoader(
         }
 
         return DirectLoader.Incomplete
+    }
+
+    // only calls this on force complete
+    override suspend fun finish() {
+        flush()
+    }
+
+    // this is always called on complete
+    override fun close() {}
+
+    private fun writeAirbyteValue(key: String, abValue: AirbyteValue) {
+        when (abValue) {
+            is NullValue -> writer.setValue(key, null)
+            is ObjectValue -> writer.setValue(key, abValue.values.serializeToString())
+            is ArrayValue -> writer.setValue(key, abValue.values.serializeToString())
+            is BooleanValue -> writer.setValue(key, abValue.value)
+            is IntegerValue -> writer.setValue(key, abValue.value)
+            is NumberValue -> writer.setValue(key, abValue.value)
+            is StringValue -> writer.setValue(key, abValue.value)
+            is DateValue -> writer.setValue(key, abValue.value)
+            is TimeWithTimezoneValue -> writer.setValue(key, abValue.value)
+            is TimeWithoutTimezoneValue -> writer.setValue(key, abValue.value)
+            is TimestampWithTimezoneValue -> writer.setValue(key, abValue.value)
+            is TimestampWithoutTimezoneValue -> writer.setValue(key, abValue.value)
+        }
+    }
+
+    private fun accumulateRecord(record: DestinationRecordRaw) {
+        // coerces and munges record to schema, adding any changes to the meta
+        val enriched = record.asEnrichedDestinationRecordAirbyteValue(
+            extractedAtAsTimestampWithTimezone = true
+        )
+        val cols = enriched.allTypedFields
+        cols.forEach {
+            writeAirbyteValue(it.key, it.value.abValue)
+        }
+
+        writer.commitRow()
     }
 
     private suspend fun flush() {
@@ -107,14 +112,6 @@ class ClickhouseDirectLoader(
         log.info { "Finished insert of ${insertResult.writtenRows} rows into ${descriptor.name}" }
     }
 
-    // only calls this on force complete
-    override suspend fun finish() {
-        flush()
-    }
-
-    // this is always called on complete
-    override fun close() {}
-
     object Constants {
         const val BATCH_SIZE_RECORDS = 500000
     }
@@ -122,7 +119,7 @@ class ClickhouseDirectLoader(
     /**
      * Naive wrapper class to having to copy the buffer contents around.
      */
-    private class ClickHouseBinaryBuffer : ByteArrayOutputStream() {
+    private class InputOutputBuffer : ByteArrayOutputStream() {
         /**
          * Get an input stream based on the contents of this output stream.
          * Do not use the output stream after calling this method.
