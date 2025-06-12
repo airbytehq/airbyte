31f620273c1faf62813aab10ba98cb1080b91d2e
tryangul <ryan.broughan@gmail.com>
2025-06-12 11:09:11 -0700

    Hack in row-binary.

--- airbyte-integrations/connectors/destination-clickhouse-v2/src/main/kotlin/io/airbyte/integrations/destination/clickhouse_v2/write/direct/ClickhouseDirectLoaderFactory.kt
+++ airbyte-integrations/connectors/destination-clickhouse-v2/src/main/kotlin/io/airbyte/integrations/destination/clickhouse_v2/write/direct/ClickhouseDirectLoaderFactory.kt
@@ -7,13 +7,11 @@ package io.airbyte.integrations.destination.clickhouse_v2.write.direct
 import com.clickhouse.client.api.Client
 import io.airbyte.cdk.load.command.DestinationStream
 import io.airbyte.cdk.load.write.DirectLoaderFactory
-import io.airbyte.integrations.destination.clickhouse_v2.config.UUIDGenerator
 import jakarta.inject.Singleton
 
 @Singleton
 class ClickhouseDirectLoaderFactory(
     private val clickhouseClient: Client,
-    private val uuidGenerator: UUIDGenerator,
 ) :
     DirectLoaderFactory<ClickhouseDirectLoader> {
     override val maxNumOpenLoaders = 2
@@ -24,6 +22,5 @@ class ClickhouseDirectLoaderFactory(
     ): ClickhouseDirectLoader = ClickhouseDirectLoader(
         streamDescriptor,
         clickhouseClient,
-        uuidGenerator,
     )
 }
