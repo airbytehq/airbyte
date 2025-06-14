08478e542d606f4b4118ee415ff7febdc09187f3
tryangul <ryan.broughan@gmail.com>
2025-06-13 20:52:17 -0700

    Wire up table name mapping. Exclude internal fields from mapping lookup.

--- airbyte-integrations/connectors/destination-clickhouse-v2/src/main/kotlin/io/airbyte/integrations/destination/clickhouse_v2/write/direct/ClickhouseDirectLoaderFactory.kt
+++ airbyte-integrations/connectors/destination-clickhouse-v2/src/main/kotlin/io/airbyte/integrations/destination/clickhouse_v2/write/direct/ClickhouseDirectLoaderFactory.kt
@@ -6,26 +6,24 @@ package io.airbyte.integrations.destination.clickhouse_v2.write.direct
 
 import com.clickhouse.client.api.Client
 import io.airbyte.cdk.load.command.DestinationStream
+import io.airbyte.cdk.load.orchestration.db.legacy_typing_deduping.TableCatalogByDescriptor
 import io.airbyte.cdk.load.write.DirectLoaderFactory
-import io.airbyte.integrations.destination.clickhouse_v2.config.ClickhouseFinalTableNameGenerator
 import io.airbyte.integrations.destination.clickhouse_v2.write.RecordMunger
 import jakarta.inject.Singleton
 
 @Singleton
 class ClickhouseDirectLoaderFactory(
     private val clickhouseClient: Client,
-    private val tableNameMapper: ClickhouseFinalTableNameGenerator,
+    private val tableInfo: TableCatalogByDescriptor,
     private val munger: RecordMunger,
-) :
-    DirectLoaderFactory<ClickhouseDirectLoader> {
+) : DirectLoaderFactory<ClickhouseDirectLoader> {
     override val maxNumOpenLoaders = 2
 
     override fun create(
         streamDescriptor: DestinationStream.Descriptor,
         part: Int
     ): ClickhouseDirectLoader {
-        // Initialize the buffer
-        val tableName = tableNameMapper.getTableName(streamDescriptor)
+        val tableName = tableInfo[streamDescriptor]!!.tableNames.finalTableName!!
         val buffer = ClickhouseBinaryRowInsertBuffer(tableName, clickhouseClient)
 
         return ClickhouseDirectLoader(
