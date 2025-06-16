8c78f9d12cf2a7870efb852ed31763a6269398c5
benmoriceau <benoit@airbyte.io>
2025-06-16 14:37:52 -0700

    Use consts

--- airbyte-integrations/connectors/destination-clickhouse-v2/src/test/kotlin/io/airbyte/integrations/destination/clickhouse_v2/client/ClickhouseAirbyteClientTest.kt
+++ airbyte-integrations/connectors/destination-clickhouse-v2/src/test/kotlin/io/airbyte/integrations/destination/clickhouse_v2/client/ClickhouseAirbyteClientTest.kt
@@ -31,35 +31,26 @@ class ClickhouseAirbyteClientTest {
     private val clickhouseAirbyteClient =
         ClickhouseAirbyteClient(client, clickhouseSqlGenerator, clickhouseFinalTableNameGenerator)
 
-    // Constants
-    private val COL1 = "col1"
-    private val COL2 = "col2"
-    private val COL3 = "col3"
-    private val COL4 = "col4"
-    private val COL5 = "col5"
-    private val STRING_TYPE = "String"
-    private val dummySentence = "SELECT 1"
-
     @Test
     fun testExecute() = runTest {
         val expectedResponse = mockk<CommandResponse>(relaxed = true)
         val completableFuture = CompletableFuture.completedFuture(expectedResponse)
-        coEvery { client.execute(dummySentence) } returns completableFuture
+        coEvery { client.execute(DUMMY_SENTENCE) } returns completableFuture
 
-        clickhouseAirbyteClient.execute(dummySentence)
+        clickhouseAirbyteClient.execute(DUMMY_SENTENCE)
 
-        coVerify { client.execute(dummySentence) }
+        coVerify { client.execute(DUMMY_SENTENCE) }
     }
 
     @Test
     fun testQuery() = runTest {
         val expectedResponse = mockk<QueryResponse>(relaxed = true)
         val completableFuture = CompletableFuture.completedFuture(expectedResponse)
-        coEvery { client.query(dummySentence) } returns completableFuture
+        coEvery { client.query(DUMMY_SENTENCE) } returns completableFuture
 
-        clickhouseAirbyteClient.query(dummySentence)
+        clickhouseAirbyteClient.query(DUMMY_SENTENCE)
 
-        coVerify { client.query(dummySentence) }
+        coVerify { client.query(DUMMY_SENTENCE) }
     }
 
     private fun getMockColumn(
@@ -81,7 +72,7 @@ class ClickhouseAirbyteClientTest {
                 getMockColumn(columnName = COL1, columnType = ClickHouseDataType.String),
                 getMockColumn(columnName = COL2, columnType = ClickHouseDataType.Int32)
             )
-        val catalogColumns = mapOf(COL1 to STRING_TYPE, COL2 to "Int32")
+        val catalogColumns = mapOf(COL1 to STRING_TYPE, COL2 to INT_TYPE)
         val expected =
             AlterationSummary(added = emptyMap(), modified = emptyMap(), deleted = emptySet())
         val actual = clickhouseAirbyteClient.getChangedColumns(tableColumns, catalogColumns)
@@ -92,10 +83,10 @@ class ClickhouseAirbyteClientTest {
     fun `test added columns`() {
         val tableColumns = listOf(getMockColumn(COL1, ClickHouseDataType.String))
         val catalogColumns =
-            mapOf(COL1 to STRING_TYPE, COL2 to "Int32", COL3 to "Float64") // Added col2 and col3
+            mapOf(COL1 to STRING_TYPE, COL2 to INT_TYPE, COL3 to FLOAT_TYPE) // Added col2 and col3
         val expected =
             AlterationSummary(
-                added = mapOf(COL2 to "Int32", COL3 to "Float64"),
+                added = mapOf(COL2 to INT_TYPE, COL3 to FLOAT_TYPE),
                 modified = emptyMap(), // No modified columns
                 deleted = emptySet()
             )
@@ -133,10 +124,10 @@ class ClickhouseAirbyteClientTest {
 
         val tableColumns2 =
             listOf(getMockColumn(columnName = COL1, columnType = ClickHouseDataType.String))
-        val catalogColumns2 = mapOf(COL2 to STRING_TYPE, COL3 to "Int32")
+        val catalogColumns2 = mapOf(COL2 to STRING_TYPE, COL3 to INT_TYPE)
         val expected2 =
             AlterationSummary(
-                added = mapOf(COL2 to STRING_TYPE, COL3 to "Int32"),
+                added = mapOf(COL2 to STRING_TYPE, COL3 to INT_TYPE),
                 modified = emptyMap(), // No modified columns
                 deleted = setOf(COL1)
             )
@@ -152,10 +143,10 @@ class ClickhouseAirbyteClientTest {
                 getMockColumn(columnName = COL3, columnType = ClickHouseDataType.Int32)
             )
         val catalogColumns =
-            mapOf(COL1 to STRING_TYPE, COL2 to STRING_TYPE, COL3 to STRING_TYPE, COL4 to "Float64")
+            mapOf(COL1 to STRING_TYPE, COL2 to STRING_TYPE, COL3 to STRING_TYPE, COL4 to FLOAT_TYPE)
         val expected =
             AlterationSummary( // Added col2 and col4, modified col3
-                added = mapOf(COL2 to STRING_TYPE, COL4 to "Float64"),
+                added = mapOf(COL2 to STRING_TYPE, COL4 to FLOAT_TYPE),
                 modified = mapOf(COL3 to STRING_TYPE),
                 deleted = emptySet()
             )
@@ -182,7 +173,7 @@ class ClickhouseAirbyteClientTest {
                 getMockColumn(columnName = COL1, columnType = ClickHouseDataType.String),
                 getMockColumn(columnName = COL3, columnType = ClickHouseDataType.Int32)
             )
-        val catalogColumns3 = mapOf(COL1 to STRING_TYPE, COL2 to STRING_TYPE, COL3 to "Int32")
+        val catalogColumns3 = mapOf(COL1 to STRING_TYPE, COL2 to STRING_TYPE, COL3 to INT_TYPE)
         val expected3 =
             AlterationSummary( // Added col2
                 added = mapOf(COL2 to STRING_TYPE),
@@ -202,7 +193,7 @@ class ClickhouseAirbyteClientTest {
             mapOf(
                 COL1 to STRING_TYPE,
                 COL2 to STRING_TYPE,
-                COL3 to "Int32",
+                COL3 to INT_TYPE,
                 COL5 to "DateTime64(3)"
             )
         val expected4 =
@@ -213,5 +204,18 @@ class ClickhouseAirbyteClientTest {
             )
         val actual4 = clickhouseAirbyteClient.getChangedColumns(tableColumns4, catalogColumns4)
         Assertions.assertEquals(expected4, actual4)
+    }
+
+    companion object {
+        // Constants
+        private const val COL1 = "col1"
+        private const val COL2 = "col2"
+        private const val COL3 = "col3"
+        private const val COL4 = "col4"
+        private const val COL5 = "col5"
+        private const val STRING_TYPE = "String"
+        private const val INT_TYPE = "Int32"
+        private const val FLOAT_TYPE = "Float64"
+        private const val DUMMY_SENTENCE = "SELECT 1"
     }
 }
