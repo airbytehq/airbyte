fba2733bf0b71db24fff70f0d7fe2123858d888b
Francis Genet <francis.genet@airbyte.io>
2025-06-17 07:57:37 -0700

    [BigQuery] 2.12.3 release. (#61648)
    Co-authored-by: Edward Gao <edward.gao@airbyte.io>

---
+++ airbyte-integrations/connectors/destination-bigquery/src/test-integration/kotlin/io/airbyte/integrations/destination/bigquery/operation/BigQueryStorageOperationTest.kt
@@ -0,0 +1,213 @@
+/*
+ * Copyright (c) 2024 Airbyte, Inc., all rights reserved.
+ */
+
+package io.airbyte.integrations.destination.bigquery.operation
+
+import com.fasterxml.jackson.databind.JsonNode
+import com.google.cloud.bigquery.BigQuery
+import com.google.cloud.bigquery.BigQueryException
+import com.google.cloud.bigquery.DatasetId
+import com.google.cloud.bigquery.DatasetInfo
+import com.google.cloud.bigquery.QueryJobConfiguration
+import com.google.cloud.bigquery.TableResult
+import io.airbyte.cdk.integrations.base.JavaBaseConstants
+import io.airbyte.cdk.integrations.destination.async.model.PartialAirbyteMessage
+import io.airbyte.cdk.integrations.destination.async.model.PartialAirbyteRecordMessage
+import io.airbyte.commons.json.Jsons
+import io.airbyte.commons.string.Strings
+import io.airbyte.integrations.base.destination.operation.AbstractStreamOperation.Companion.TMP_TABLE_SUFFIX
+import io.airbyte.integrations.base.destination.typing_deduping.ImportType
+import io.airbyte.integrations.base.destination.typing_deduping.StreamConfig
+import io.airbyte.integrations.base.destination.typing_deduping.StreamId
+import io.airbyte.integrations.destination.bigquery.BigQueryConsts
+import io.airbyte.integrations.destination.bigquery.BigQueryDestination
+import io.airbyte.integrations.destination.bigquery.formatter.BigQueryRecordFormatter
+import io.airbyte.integrations.destination.bigquery.typing_deduping.BigQueryDestinationHandler
+import io.airbyte.integrations.destination.bigquery.typing_deduping.BigQuerySqlGenerator
+import io.airbyte.integrations.destination.bigquery.typing_deduping.BigQuerySqlGeneratorIntegrationTest
+import io.airbyte.protocol.models.v0.AirbyteMessage.Type
+import io.airbyte.protocol.models.v0.AirbyteRecordMessageMeta
+import java.nio.file.Files
+import java.nio.file.Path
+import java.util.Optional
+import java.util.stream.Stream
+import kotlin.test.assertEquals
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import org.junit.jupiter.api.parallel.Execution
+import org.junit.jupiter.api.parallel.ExecutionMode
+
+/**
+ * Extremely barebones integration test for the direct inserts storage op. We should eventually:
+ * * Make something similar for the GCS storage op
+ * * Genericize this and put it in the CDK
+ * * Add assertions for all the columns, not just airbyte_data
+ * * Actually test all the methods on StorageOperation
+ */
+@Execution(ExecutionMode.CONCURRENT)
+class BigQueryDirectLoadingStorageOperationTest {
+    private val randomString = Strings.addRandomSuffix("", "", 10)
+    private val streamId =
+        StreamId(
+            finalNamespace = "final_namespace_$randomString",
+            finalName = "final_name_$randomString",
+            rawNamespace = "raw_namespace_$randomString",
+            rawName = "raw_name_$randomString",
+            originalNamespace = "original_namespace_$randomString",
+            originalName = "original_name_$randomString",
+        )
+    private val streamConfig =
+        StreamConfig(
+            streamId,
+            ImportType.APPEND,
+            emptyList(),
+            Optional.empty(),
+            LinkedHashMap(),
+            GENERATION_ID,
+            0,
+            SYNC_ID,
+        )
+
+    @BeforeEach
+    fun setup() {
+        bq.create(DatasetInfo.of(streamId.rawNamespace))
+    }
+
+    @AfterEach
+    fun teardown() {
+        bq.delete(
+            DatasetId.of(streamId.rawNamespace),
+            BigQuery.DatasetDeleteOption.deleteContents()
+        )
+    }
+
+    @Test
+    fun testTransferStage() {
+        storageOperation.prepareStage(streamId, "")
+        storageOperation.prepareStage(streamId, TMP_TABLE_SUFFIX)
+        // Table is currently empty, so expect null generation.
+        assertEquals(null, storageOperation.getStageGeneration(streamId, TMP_TABLE_SUFFIX))
+
+        // Write one record to the real raw table
+        storageOperation.writeToStage(
+            streamConfig,
+            "",
+            Stream.of(record(1)),
+        )
+        assertEquals(
+            listOf("""{"record_number": 1}"""),
+            // We write the raw data as a string column, not a JSON column, so use asText().
+            dumpRawRecords("").map { it["_airbyte_data"].asText() },
+        )
+
+        // And write one record to the temp final table
+        storageOperation.writeToStage(
+            streamConfig,
+            TMP_TABLE_SUFFIX,
+            Stream.of(record(2)),
+        )
+        assertEquals(
+            listOf("""{"record_number": 2}"""),
+            dumpRawRecords(TMP_TABLE_SUFFIX).map { it["_airbyte_data"].asText() },
+        )
+        assertEquals(GENERATION_ID, storageOperation.getStageGeneration(streamId, TMP_TABLE_SUFFIX))
+
+        // If we transfer the records, we should end up with 2 records in the real raw table.
+        storageOperation.transferFromTempStage(streamId, TMP_TABLE_SUFFIX)
+        assertEquals(
+            listOf(
+                """{"record_number": 1}""",
+                """{"record_number": 2}""",
+            ),
+            dumpRawRecords("")
+                .sortedBy {
+                    Jsons.deserialize(it["_airbyte_data"].asText())["record_number"].asLong()
+                }
+                .map { it["_airbyte_data"].asText() },
+        )
+
+        // After transferring the records to the real table, the temp table should no longer exist.
+        assertEquals(404, assertThrows<BigQueryException> { dumpRawRecords(TMP_TABLE_SUFFIX) }.code)
+    }
+
+    @Test
+    fun testOverwriteStage() {
+        // If we then create another temp raw table and _overwrite_ the real raw table,
+        // we should end up with a single raw record.
+        storageOperation.prepareStage(streamId, "")
+        storageOperation.prepareStage(streamId, TMP_TABLE_SUFFIX)
+        storageOperation.writeToStage(
+            streamConfig,
+            "",
+            Stream.of(record(3)),
+        )
+        storageOperation.writeToStage(
+            streamConfig,
+            TMP_TABLE_SUFFIX,
+            Stream.of(record(4)),
+        )
+
+        storageOperation.overwriteStage(streamId, TMP_TABLE_SUFFIX)
+
+        assertEquals(
+            listOf("""{"record_number": 4}"""),
+            dumpRawRecords("").map { it["_airbyte_data"].asText() },
+        )
+        assertEquals(404, assertThrows<BigQueryException> { dumpRawRecords(TMP_TABLE_SUFFIX) }.code)
+    }
+
+    private fun dumpRawRecords(suffix: String): List<JsonNode> {
+        val result: TableResult =
+            bq.query(
+                QueryJobConfiguration.of(
+                    "SELECT * FROM " + streamId.rawTableId(BigQuerySqlGenerator.QUOTE, suffix)
+                ),
+            )
+        return BigQuerySqlGeneratorIntegrationTest.toJsonRecords(result)
+    }
+
+    private fun record(recordNumber: Int): PartialAirbyteMessage {
+        val serializedData = """{"record_number": $recordNumber}"""
+        return PartialAirbyteMessage()
+            .withType(Type.RECORD)
+            .withSerialized(serializedData)
+            .withRecord(
+                PartialAirbyteRecordMessage()
+                    .withNamespace(streamId.originalNamespace)
+                    .withStream(streamId.originalName)
+                    .withEmittedAt(10_000)
+                    .withMeta(
+                        AirbyteRecordMessageMeta()
+                            .withChanges(emptyList())
+                            .withAdditionalProperty(
+                                JavaBaseConstants.AIRBYTE_META_SYNC_ID_KEY,
+                                SYNC_ID,
+                            ),
+                    )
+                    .withData(Jsons.deserialize(serializedData)),
+            )
+    }
+
+    companion object {
+        private val config =
+            Jsons.deserialize(Files.readString(Path.of("secrets/credentials-gcs-staging.json")))
+        private val bq = BigQueryDestination.getBigQuery(config)
+        private val projectId = config.get(BigQueryConsts.CONFIG_PROJECT_ID).asText()
+        private val datasetLocation = config.get(BigQueryConsts.CONFIG_DATASET_LOCATION).asText()
+        private val storageOperation =
+            BigQueryDirectLoadingStorageOperation(
+                bq,
+                15,
+                BigQueryRecordFormatter(),
+                BigQuerySqlGenerator(projectId, datasetLocation),
+                BigQueryDestinationHandler(bq, datasetLocation),
+                datasetLocation,
+            )
+
+        private const val SYNC_ID = 12L
+        private const val GENERATION_ID = 42L
+    }
+}
--- airbyte-integrations/connectors/destination-bigquery/src/test/kotlin/io/airbyte/integrations/destination/bigquery/typing_deduping/BigqueryDestinationHandlerTest.kt
+++ airbyte-integrations/connectors/destination-bigquery/src/test/kotlin/io/airbyte/integrations/destination/bigquery/typing_deduping/BigqueryDestinationHandlerTest.kt
@@ -4,195 +4,122 @@
 package io.airbyte.integrations.destination.bigquery.typing_deduping
 
 import com.google.cloud.bigquery.Clustering
-import com.google.cloud.bigquery.Field
-import com.google.cloud.bigquery.FieldList
 import com.google.cloud.bigquery.StandardSQLTypeName
 import com.google.cloud.bigquery.StandardTableDefinition
 import com.google.cloud.bigquery.TimePartitioning
-import io.airbyte.cdk.load.command.Append
-import io.airbyte.cdk.load.command.Dedupe
-import io.airbyte.cdk.load.command.DestinationStream
-import io.airbyte.cdk.load.command.NamespaceMapper
-import io.airbyte.cdk.load.data.ArrayType
-import io.airbyte.cdk.load.data.BooleanType
-import io.airbyte.cdk.load.data.FieldType
-import io.airbyte.cdk.load.data.IntegerType
-import io.airbyte.cdk.load.data.NumberType
-import io.airbyte.cdk.load.data.ObjectType
-import io.airbyte.cdk.load.data.ObjectTypeWithoutSchema
-import io.airbyte.cdk.load.data.UnionType
-import io.airbyte.cdk.load.orchestration.db.ColumnNameMapping
+import com.google.common.collect.ImmutableList
+import io.airbyte.integrations.base.destination.typing_deduping.AirbyteProtocolType
+import io.airbyte.integrations.base.destination.typing_deduping.AirbyteType
+import io.airbyte.integrations.base.destination.typing_deduping.Array
+import io.airbyte.integrations.base.destination.typing_deduping.ColumnId
+import io.airbyte.integrations.base.destination.typing_deduping.ImportType
+import io.airbyte.integrations.base.destination.typing_deduping.StreamConfig
+import io.airbyte.integrations.base.destination.typing_deduping.Struct
+import io.airbyte.integrations.base.destination.typing_deduping.Union
+import io.airbyte.integrations.base.destination.typing_deduping.UnsupportedOneOf
+import io.airbyte.integrations.destination.bigquery.typing_deduping.BigQueryDestinationHandler.Companion.clusteringMatches
+import io.airbyte.integrations.destination.bigquery.typing_deduping.BigQueryDestinationHandler.Companion.partitioningMatches
+import io.airbyte.integrations.destination.bigquery.typing_deduping.BigQueryDestinationHandler.Companion.schemaContainAllFinalTableV2AirbyteColumns
 import io.airbyte.integrations.destination.bigquery.typing_deduping.BigQuerySqlGenerator.Companion.toDialectType
-import io.airbyte.integrations.destination.bigquery.typing_deduping.BigqueryDatabaseInitialStatusGatherer.Companion.clusteringMatches
-import io.airbyte.integrations.destination.bigquery.typing_deduping.BigqueryDatabaseInitialStatusGatherer.Companion.partitioningMatches
+import java.util.Optional
+import java.util.stream.Collectors
+import java.util.stream.Stream
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
 import org.mockito.Mockito
-import org.mockito.Mockito.RETURNS_DEEP_STUBS
 
 class BigqueryDestinationHandlerTest {
     @Test
     fun testToDialectType() {
-        val s = ObjectType(linkedMapOf())
-        val a = ArrayType(FieldType(BooleanType, nullable = true))
+        val s = Struct(LinkedHashMap())
+        val a = Array(AirbyteProtocolType.BOOLEAN)
 
-        Assertions.assertEquals(StandardSQLTypeName.INT64, toDialectType(IntegerType))
+        Assertions.assertEquals(
+            StandardSQLTypeName.INT64,
+            toDialectType((AirbyteProtocolType.INTEGER as AirbyteType))
+        )
         Assertions.assertEquals(StandardSQLTypeName.JSON, toDialectType(s))
         Assertions.assertEquals(StandardSQLTypeName.JSON, toDialectType(a))
         Assertions.assertEquals(
             StandardSQLTypeName.JSON,
-            toDialectType(UnionType(emptySet(), isLegacyUnion = false))
+            toDialectType(UnsupportedOneOf(ArrayList()))
         )
 
-        var u = UnionType(setOf(s), isLegacyUnion = true)
+        var u = Union(ImmutableList.of(s))
         Assertions.assertEquals(StandardSQLTypeName.JSON, toDialectType(u))
-        u = UnionType(setOf(a), isLegacyUnion = true)
+        u = Union(ImmutableList.of(a))
         Assertions.assertEquals(StandardSQLTypeName.JSON, toDialectType(u))
-        u = UnionType(setOf(BooleanType, NumberType), isLegacyUnion = true)
+        u = Union(ImmutableList.of(AirbyteProtocolType.BOOLEAN, AirbyteProtocolType.NUMBER))
         Assertions.assertEquals(StandardSQLTypeName.NUMERIC, toDialectType(u))
-    }
-
-    @Test
-    fun testColumnsMatch() {
-        val stream =
-            DestinationStream(
-                "foo",
-                "bar",
-                Append,
-                ObjectType(linkedMapOf("a1" to FieldType(IntegerType, nullable = true))),
-                generationId = 0,
-                minimumGenerationId = 0,
-                syncId = 0,
-                namespaceMapper = NamespaceMapper()
-            )
-        val columnNameMapping = ColumnNameMapping(mapOf("a1" to "a2"))
-        val existingTable = Mockito.mock(StandardTableDefinition::class.java, RETURNS_DEEP_STUBS)
-        Mockito.`when`(existingTable.schema!!.fields)
-            .thenReturn(FieldList.of(Field.of("a2", StandardSQLTypeName.INT64)))
-        val alterTableReport =
-            BigqueryDatabaseInitialStatusGatherer(Mockito.mock())
-                .buildAlterTableReport(stream, columnNameMapping, existingTable)
-        Assertions.assertAll(
-            { Assertions.assertEquals(emptySet<String>(), alterTableReport.columnsToAdd) },
-            { Assertions.assertEquals(emptySet<String>(), alterTableReport.columnsToRemove) },
-            { Assertions.assertEquals(emptySet<String>(), alterTableReport.columnsToChangeType) },
-        )
-    }
-
-    @Test
-    fun testColumnsNotMatch() {
-        val stream =
-            DestinationStream(
-                "foo",
-                "bar",
-                Append,
-                ObjectType(
-                    linkedMapOf(
-                        "a1" to FieldType(IntegerType, nullable = true),
-                        "c1" to FieldType(IntegerType, nullable = true),
-                    )
-                ),
-                generationId = 0,
-                minimumGenerationId = 0,
-                syncId = 0,
-                namespaceMapper = NamespaceMapper()
-            )
-        val columnNameMapping = ColumnNameMapping(mapOf("a1" to "a2", "c1" to "c2"))
-        val existingTable = Mockito.mock(StandardTableDefinition::class.java, RETURNS_DEEP_STUBS)
-        Mockito.`when`(existingTable.schema!!.fields)
-            .thenReturn(
-                FieldList.of(
-                    listOf(
-                        Field.of("a2", StandardSQLTypeName.STRING),
-                        Field.of("b2", StandardSQLTypeName.INT64)
-                    )
-                )
-            )
-        val alterTableReport =
-            BigqueryDatabaseInitialStatusGatherer(Mockito.mock())
-                .buildAlterTableReport(stream, columnNameMapping, existingTable)
-        Assertions.assertAll(
-            { Assertions.assertEquals(setOf("c2"), alterTableReport.columnsToAdd) },
-            { Assertions.assertEquals(setOf("b2"), alterTableReport.columnsToRemove) },
-            { Assertions.assertEquals(setOf("a2"), alterTableReport.columnsToChangeType) },
-        )
     }
 
     @Test
     fun testClusteringMatches() {
         var stream =
-            DestinationStream(
-                "foo",
-                "bar",
-                Dedupe(
-                    listOf(listOf("bar")),
-                    emptyList(),
-                ),
-                ObjectTypeWithoutSchema,
-                generationId = 0,
-                minimumGenerationId = 0,
-                syncId = 0,
-                namespaceMapper = NamespaceMapper()
+            StreamConfig(
+                Mockito.mock(),
+                ImportType.DEDUPE,
+                listOf(ColumnId("foo", "bar", "fizz")),
+                Optional.empty(),
+                LinkedHashMap(),
+                0,
+                0,
+                0
             )
-        var columnNameMapping = ColumnNameMapping(mapOf("bar" to "foo"))
 
         // Clustering is null
         val existingTable = Mockito.mock(StandardTableDefinition::class.java)
         Mockito.`when`(existingTable.clustering).thenReturn(null)
-        Assertions.assertFalse(clusteringMatches(stream, columnNameMapping, existingTable))
+        Assertions.assertFalse(clusteringMatches(stream, existingTable))
 
         // Clustering does not contain all fields
         Mockito.`when`(existingTable.clustering)
             .thenReturn(Clustering.newBuilder().setFields(listOf("_airbyte_extracted_at")).build())
-        Assertions.assertFalse(clusteringMatches(stream, columnNameMapping, existingTable))
+        Assertions.assertFalse(clusteringMatches(stream, existingTable))
 
         // Clustering matches
         stream =
-            DestinationStream(
-                "foo",
-                "bar",
-                Append,
-                ObjectTypeWithoutSchema,
-                generationId = 0,
-                minimumGenerationId = 0,
-                syncId = 0,
-                namespaceMapper = NamespaceMapper()
+            StreamConfig(
+                Mockito.mock(),
+                ImportType.APPEND,
+                emptyList(),
+                Optional.empty(),
+                LinkedHashMap(),
+                0,
+                0,
+                0
             )
-        Assertions.assertTrue(clusteringMatches(stream, columnNameMapping, existingTable))
+        Assertions.assertTrue(clusteringMatches(stream, existingTable))
 
         // Clustering only the first 3 PK columns (See
         // https://github.com/airbytehq/oncall/issues/2565)
+        val expectedStreamColumnNames = listOf("a", "b", "c")
         Mockito.`when`(existingTable.clustering)
             .thenReturn(
                 Clustering.newBuilder()
-                    .setFields(listOf("a2", "b2", "c2", "_airbyte_extracted_at"))
+                    .setFields(
+                        Stream.concat(
+                                expectedStreamColumnNames.stream(),
+                                Stream.of("_airbyte_extracted_at")
+                            )
+                            .collect(Collectors.toList())
+                    )
                     .build()
             )
         stream =
-            DestinationStream(
-                "foo",
-                "bar",
-                Dedupe(
-                    listOf(listOf("a1"), listOf("b1"), listOf("c1"), listOf("d1"), listOf("e1")),
-                    emptyList()
-                ),
-                ObjectTypeWithoutSchema,
-                generationId = 0,
-                minimumGenerationId = 0,
-                syncId = 0,
-                namespaceMapper = NamespaceMapper()
+            StreamConfig(
+                Mockito.mock(),
+                ImportType.DEDUPE,
+                Stream.concat(expectedStreamColumnNames.stream(), Stream.of("d", "e"))
+                    .map { name: String -> ColumnId(name, "foo", "bar") }
+                    .collect(Collectors.toList()),
+                Optional.empty(),
+                LinkedHashMap(),
+                0,
+                0,
+                0
             )
-        columnNameMapping =
-            ColumnNameMapping(
-                mapOf(
-                    "a1" to "a2",
-                    "b1" to "b2",
-                    "c1" to "c2",
-                    "d1" to "d2",
-                    "e1" to "e2",
-                )
-            )
-        Assertions.assertTrue(clusteringMatches(stream, columnNameMapping, existingTable))
+        Assertions.assertTrue(clusteringMatches(stream, existingTable))
     }
 
     @Test
@@ -224,5 +151,43 @@ class BigqueryDestinationHandlerTest {
                     .build()
             )
         Assertions.assertTrue(partitioningMatches(existingTable))
+    }
+
+    @Test
+    fun testSchemaContainAllFinalTableV2AirbyteColumns() {
+        Assertions.assertTrue(
+            schemaContainAllFinalTableV2AirbyteColumns(
+                setOf(
+                    "_airbyte_meta",
+                    "_airbyte_generation_id",
+                    "_airbyte_extracted_at",
+                    "_airbyte_raw_id"
+                )
+            )
+        )
+        Assertions.assertFalse(
+            schemaContainAllFinalTableV2AirbyteColumns(
+                setOf("_airbyte_extracted_at", "_airbyte_raw_id")
+            )
+        )
+        Assertions.assertFalse(
+            schemaContainAllFinalTableV2AirbyteColumns(setOf("_airbyte_meta", "_airbyte_raw_id"))
+        )
+        Assertions.assertFalse(
+            schemaContainAllFinalTableV2AirbyteColumns(
+                setOf("_airbyte_meta", "_airbyte_extracted_at")
+            )
+        )
+        Assertions.assertFalse(schemaContainAllFinalTableV2AirbyteColumns(setOf()))
+        Assertions.assertTrue(
+            schemaContainAllFinalTableV2AirbyteColumns(
+                setOf(
+                    "_AIRBYTE_META",
+                    "_AIRBYTE_GENERATION_ID",
+                    "_AIRBYTE_EXTRACTED_AT",
+                    "_AIRBYTE_RAW_ID"
+                )
+            )
+        )
     }
 }
