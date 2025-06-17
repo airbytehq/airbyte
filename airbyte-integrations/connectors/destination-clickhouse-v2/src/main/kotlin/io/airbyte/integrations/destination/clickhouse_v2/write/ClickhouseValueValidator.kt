797ffc6e0c3fdbc7724ecacdbd117e042f3cec05
tryangul <ryan.broughan@gmail.com>
2025-06-17 15:38:25 -0700

    Try suppress annotation.

--- airbyte-integrations/connectors/destination-clickhouse-v2/src/main/kotlin/io/airbyte/integrations/destination/clickhouse_v2/write/ClickhouseValueValidator.kt
+++ airbyte-integrations/connectors/destination-clickhouse-v2/src/main/kotlin/io/airbyte/integrations/destination/clickhouse_v2/write/ClickhouseValueValidator.kt
@@ -1,37 +1,13 @@
 package io.airbyte.integrations.destination.clickhouse_v2.write
 
-import com.clickhouse.client.api.Client
-import com.clickhouse.client.api.data_formats.RowBinaryFormatWriter
-import com.clickhouse.data.ClickHouseChecker
-import com.clickhouse.data.ClickHouseFormat
 import com.clickhouse.data.format.BinaryStreamUtils.DECIMAL64_MAX
 import com.clickhouse.data.format.BinaryStreamUtils.DECIMAL64_MIN
-import com.google.common.annotations.VisibleForTesting
-import edu.umd.cs.findbugs.annotations.SuppressFBWarnings
-import io.airbyte.cdk.load.data.AirbyteValue
-import io.airbyte.cdk.load.data.ArrayValue
-import io.airbyte.cdk.load.data.BooleanValue
-import io.airbyte.cdk.load.data.DateValue
-import io.airbyte.cdk.load.data.IntegerValue
-import io.airbyte.cdk.load.data.NullValue
-import io.airbyte.cdk.load.data.NumberValue
-import io.airbyte.cdk.load.data.ObjectValue
-import io.airbyte.cdk.load.data.StringValue
-import io.airbyte.cdk.load.data.TimeWithTimezoneValue
-import io.airbyte.cdk.load.data.TimeWithoutTimezoneValue
-import io.airbyte.cdk.load.data.TimestampWithTimezoneValue
-import io.airbyte.cdk.load.data.TimestampWithoutTimezoneValue
-import io.airbyte.cdk.load.orchestration.db.TableName
-import io.airbyte.cdk.load.util.serializeToString
-import io.github.oshai.kotlinlogging.KotlinLogging
-import java.io.ByteArrayInputStream
-import java.io.ByteArrayOutputStream
-import kotlinx.coroutines.future.await
-
 import io.airbyte.cdk.load.data.EnrichedAirbyteValue
+import io.airbyte.cdk.load.data.NumberValue
 import io.airbyte.protocol.models.v0.AirbyteRecordMessageMetaChange
 import jakarta.inject.Singleton
 
+//@Suppress("deprecated")
 @Singleton
 class ClickhouseValueValidator {
     fun validateAndCoerce(value: EnrichedAirbyteValue): EnrichedAirbyteValue {
