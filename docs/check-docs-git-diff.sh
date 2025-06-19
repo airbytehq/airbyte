736a866414313a557b64bb793526f71f177c8297
Aaron Steers <aj@airbyte.io>
2025-06-18 18:35:10 -0700

    add --latest-commit-only flag

--- docs/check-docs-git-diff.sh
+++ docs/check-docs-git-diff.sh
@@ -7,6 +7,23 @@ set -eu
 # This script will be called by Vercel here:
 # - https://vercel.com/airbyte-growth/airbyte-docs/settings/git#ignored-build-step
 
+# parse flags
+LATEST_COMMIT_ONLY=false
+while [[ $# -gt 0 ]]; do
+  case "$1" in
+    --latest-commit-only)
+      LATEST_COMMIT_ONLY=true
+      echo "⚙️ Running in '--latest-commit-only' mode."
+      echo "Only the most recent commit will be checked for changes."
+      ;;
+    *)
+      echo "Unknown argument: $1" >&2;
+      exit 1
+      ;;
+  esac
+  shift
+done
+
 latest_commit_message=$(git log -n 1 --oneline)
 echo "⚙️ Checking for a skip directive in the commit message: ('$latest_commit_message')"
 if echo "$latest_commit_message" | grep -Eq "\[(up-to-date|auto-publish)\]"; then
@@ -17,8 +34,10 @@ fi
 REMOTE=origin
 DEFAULT_BRANCH=master
 COMPARE_TO_REF="${REMOTE}/${DEFAULT_BRANCH}"
-# If we only wanted to compare against the previous commit:
-# COMPARE_TO_REF="HEAD^"
+if [ "$LATEST_COMMIT_ONLY" == 'true' ]; then
+    # We only want to compare the latest commit against the previous one:
+    COMPARE_TO_REF="HEAD^"
+fi
 
 echo "⚙️ Checking for git changes within the docs paths..."
 if git diff "${COMPARE_TO_REF}"...HEAD --quiet ./docusaurus ./docs .markdownlint.jsonc; then
