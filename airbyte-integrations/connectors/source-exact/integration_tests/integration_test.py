4f920582164a6646db286a6a9fb5535191e4785a
Jeroen de Vries <jeroen@maildevries.eu>
2025-06-23 16:28:11 +0200

    new schema and updated a stream slices method from the base connecto

--- airbyte-integrations/connectors/source-exact/integration_tests/integration_test.py
+++ airbyte-integrations/connectors/source-exact/integration_tests/integration_test.py
@@ -2,14 +2,15 @@
 # Copyright (c) 2025 Airbyte, Inc., all rights reserved.
 #
 
-from airbyte_cdk.models import AirbyteCatalog
+from airbyte_cdk.models import AirbyteCatalog, SyncMode, ConfiguredAirbyteCatalog
 from airbyte_cdk.test.entrypoint_wrapper import read
-from source_exact.streams import CRMAccountClassifications
-
-from source_exact import SourceExact
+from airbyte_protocol_dataclasses.models import ConfiguredAirbyteStream, DestinationSyncMode
 from pytest import fixture
 from pathlib import Path
 import json
+
+from source_exact.streams import CRMAccountClassifications,CRMAccountClassificationNames
+from source_exact import SourceExact
 
 HERE = Path(__file__).parent
 
@@ -19,17 +20,21 @@ def config():
     with open(HERE.parent / "secrets/config.json", "r") as file:
         return json.loads(file.read())
 
+
 @fixture
 def configured_catalog(config):
-    ab_stream =[]
-    crmac_stream = CRMAccountClassifications(config).as_airbyte_stream()
-    ab_stream.append(crmac_stream)
-    return AirbyteCatalog(streams=ab_stream)
+    crmac_stream = CRMAccountClassificationNames(config).as_airbyte_stream()
+    configured_stream = ConfiguredAirbyteStream(
+        stream=crmac_stream,
+        sync_mode=SyncMode.full_refresh,
+        destination_sync_mode=DestinationSyncMode.overwrite
+    )
+
+    return ConfiguredAirbyteCatalog(streams=[configured_stream])
 
 
-
-def test_read_crn_account_classifications(config, configured_catalog):
+def test_read_crm_account_classification_names(config, configured_catalog):
     source = SourceExact()
-    output = source.read()
+    output = read(source, config, configured_catalog)
 
-    # assert len(output) > 1
+    assert len(output.records) > 0
