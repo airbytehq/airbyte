9ba408b78ec4de38fcee0c962716cae91ef48dba
benmoriceau <benoit@airbyte.io>
2025-06-24 10:02:04 -0700

    Fix the client connection

--- airbyte-integrations/connectors/destination-clickhouse-v2/src/main/kotlin/io/airbyte/integrations/destination/clickhouse_v2/config/ClickhouseBeanFactory.kt
+++ airbyte-integrations/connectors/destination-clickhouse-v2/src/main/kotlin/io/airbyte/integrations/destination/clickhouse_v2/config/ClickhouseBeanFactory.kt
@@ -23,20 +23,33 @@ class ClickhouseBeanFactory {
 
     @Singleton
     fun clickhouseClient(config: ClickhouseConfiguration): Client {
-        // We don't set the default database here because the client expects that database
-        // to already exist during instantiation. If we instantiate the client with a default
-        // database that does not exist, it will hard fail.
 
-        // In order to solve this chicken-and-egg problem, we avoid setting the default db on the
-        // client.
-        // Instead, we resolve the default database in the ClickhouseConfiguration, which is used
-        // for table creation when the stream descriptor does not specificy a namespace directly.
-        return Client.Builder()
+        val clientWithDb = Client.Builder()
             .addEndpoint(config.endpoint)
             .setUsername(config.username)
             .setPassword(config.password)
+            .setDefaultDatabase(config.resolvedDatabase)
             .compressClientRequest(true)
             .build()
+
+        return if (clientWithDb.ping()) {
+            clientWithDb
+        } else {
+            // We don't set the default database here because the client expects that database
+            // to already exist during instantiation. If we instantiate the client with a default
+            // database that does not exist, it will hard fail.
+
+            // In order to solve this chicken-and-egg problem, we avoid setting the default db on the
+            // client.
+            // Instead, we resolve the default database in the ClickhouseConfiguration, which is used
+            // for table creation when the stream descriptor does not specificy a namespace directly.
+            Client.Builder()
+                .addEndpoint(config.endpoint)
+                .setUsername(config.username)
+                .setPassword(config.password)
+                .compressClientRequest(true)
+                .build()
+        }
     }
 
     @Singleton
