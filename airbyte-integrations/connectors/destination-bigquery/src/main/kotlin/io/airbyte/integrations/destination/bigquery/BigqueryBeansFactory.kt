d06a64a30e7c1f62cee608e5af16d64fd950b3d3
Francis Genet <francis.genet@airbyte.io>
2025-06-24 11:44:21 -0700

    [Bigquery] More retries is better retries (#62041)

--- airbyte-integrations/connectors/destination-bigquery/src/main/kotlin/io/airbyte/integrations/destination/bigquery/BigqueryBeansFactory.kt
+++ airbyte-integrations/connectors/destination-bigquery/src/main/kotlin/io/airbyte/integrations/destination/bigquery/BigqueryBeansFactory.kt
@@ -4,6 +4,7 @@
 
 package io.airbyte.integrations.destination.bigquery
 
+import com.google.api.gax.retrying.RetrySettings
 import com.google.auth.oauth2.GoogleCredentials
 import com.google.cloud.bigquery.BigQuery
 import com.google.cloud.bigquery.BigQueryOptions
@@ -34,6 +35,7 @@ import jakarta.inject.Singleton
 import java.io.ByteArrayInputStream
 import java.io.InputStream
 import java.nio.charset.StandardCharsets
+import org.threeten.bp.Duration
 
 private val logger = KotlinLogging.logger {}
 
@@ -108,6 +110,18 @@ class BigqueryBeansFactory {
             .setProjectId(config.projectId)
             .setCredentials(credentials)
             .setHeaderProvider(BigQueryUtils.headerProvider)
+            .setRetrySettings(
+                RetrySettings.newBuilder()
+                    .setInitialRetryDelayDuration(java.time.Duration.ofMillis(1000L))
+                    .setMaxRetryDelayDuration(java.time.Duration.ofMillis(32_000L))
+                    .setTotalTimeoutDuration(java.time.Duration.ofMillis(60_000L))
+                    .setInitialRpcTimeoutDuration(java.time.Duration.ofMillis(50_000L))
+                    .setRpcTimeoutMultiplier(1.0)
+                    .setMaxRpcTimeoutDuration(java.time.Duration.ofMillis(50_000L))
+                    .setMaxAttempts(15)
+                    .setRetryDelayMultiplier(1.5)
+                    .build()
+            )
             .build()
             .service
     }
