db988f87dd5eb10907fbe83da691d9db80a841bc
Jeroen de Vries <jeroen@maildevries.eu>
2025-06-25 15:41:45 +0200

    we dont set prod on airbyte instance therfore change is dev

--- airbyte-integrations/connectors/source-exact/source_exact/utils_func.py
+++ airbyte-integrations/connectors/source-exact/source_exact/utils_func.py
@@ -6,11 +6,12 @@ import os
 
 
 def is_prod() -> bool:
-    """Returns True if environment variable is prod, False otherwise."""
+    """Returns True if environment variable prod is set, False otherwise."""
     prod = os.environ.get("PROD", "False").lower() in ("true", "1", "t")
     return prod
 
 
 def is_dev() -> bool:
-    """Return True if no environment variable is set, False otherwise."""
-    return not is_prod()
+    """Return True if environment variable dev is set, False otherwise."""
+    dev = os.environ.get("DEV", "False").lower() in ("true", "1", "t")
+    return dev
