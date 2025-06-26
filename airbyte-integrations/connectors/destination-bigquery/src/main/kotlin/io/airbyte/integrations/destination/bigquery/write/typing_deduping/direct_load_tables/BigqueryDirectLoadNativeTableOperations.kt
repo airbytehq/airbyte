85010284e78ebb583675598938a499ae006914fe
Edward Gao <edward.gao@airbyte.io>
2025-06-26 15:31:41 -0700

    include exception message in trace message

--- airbyte-integrations/connectors/destination-bigquery/src/main/kotlin/io/airbyte/integrations/destination/bigquery/write/typing_deduping/direct_load_tables/BigqueryDirectLoadNativeTableOperations.kt
+++ airbyte-integrations/connectors/destination-bigquery/src/main/kotlin/io/airbyte/integrations/destination/bigquery/write/typing_deduping/direct_load_tables/BigqueryDirectLoadNativeTableOperations.kt
@@ -343,7 +343,7 @@ class BigqueryDirectLoadNativeTableOperations(
                 )
             } catch (e: Exception) {
                 val message =
-                    "Error while updating schema for table ${tableName.toPrettyString()} (attempting to change column $realColumnName from $originalType to $newType). You should manually update the schema for this table."
+                    "Error while updating schema for table ${tableName.toPrettyString()} (attempting to change column $realColumnName from $originalType to $newType). You should manually update the schema for this table. Details: ${e.message}"
                 logger.warn(e) { message }
                 // no rollback logic. On the next sync, we'll see the temp columns in columnsToDrop.
                 throw ConfigErrorException(message, e)
