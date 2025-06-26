0d66c270c7d156e709736d8f2587bb7770044454
Maxime Carbonneau-Leclerc <3360483+maxi297@users.noreply.github.com>
2025-06-26 17:08:42 -0400

    fix: nullability when assembling to JsonNode (#61731)
    Co-authored-by: Octavia Squidington III <octavia-squidington-iii@users.noreply.github.com>

--- airbyte-cdk/bulk/core/load/src/main/kotlin/io/airbyte/cdk/load/data/json/AirbyteTypeToJsonSchema.kt
+++ airbyte-cdk/bulk/core/load/src/main/kotlin/io/airbyte/cdk/load/data/json/AirbyteTypeToJsonSchema.kt
@@ -10,27 +10,35 @@ import com.fasterxml.jackson.databind.node.ObjectNode
 import io.airbyte.cdk.load.data.*
 
 class AirbyteTypeToJsonSchema {
-    private fun ofType(typeName: String): ObjectNode {
-        return JsonNodeFactory.instance.objectNode().put("type", typeName)
+    private fun ofType(typeName: String, nullable: Boolean): ObjectNode {
+        val objectNode = JsonNodeFactory.instance.objectNode()
+        if (nullable) {
+            objectNode.putArray("type").add("null").add(typeName)
+        } else {
+            objectNode.put("type", typeName)
+        }
+        return objectNode
     }
 
     fun convert(airbyteType: AirbyteType): JsonNode {
+        return convertInner(airbyteType, false)
+    }
+
+    private fun convertInner(airbyteType: AirbyteType, nullable: Boolean = false): JsonNode {
         return when (airbyteType) {
-            is StringType -> ofType("string")
-            is BooleanType -> ofType("boolean")
-            is IntegerType -> ofType("integer")
-            is NumberType -> ofType("number")
+            is StringType -> ofType("string", nullable)
+            is BooleanType -> ofType("boolean", nullable)
+            is IntegerType -> ofType("integer", nullable)
+            is NumberType -> ofType("number", nullable)
             is ArrayType ->
-                JsonNodeFactory.instance
-                    .objectNode()
-                    .put("type", "array")
-                    .set("items", convert(airbyteType.items.type))
-            is ArrayTypeWithoutSchema -> ofType("array")
+                ofType("array", nullable)
+                    .set("items", convertInner(airbyteType.items.type, airbyteType.items.nullable))
+            is ArrayTypeWithoutSchema -> ofType("array", nullable)
             is ObjectType -> {
-                val objNode = ofType("object")
+                val objNode = ofType("object", nullable)
                 val properties = objNode.putObject("properties")
                 airbyteType.properties.forEach { (name, field) ->
-                    properties.replace(name, convert(field.type))
+                    properties.replace(name, convertInner(field.type, field.nullable))
                 }
                 if (!airbyteType.required.isEmpty()) {
                     val required = objNode.putArray("required")
@@ -39,9 +47,9 @@ class AirbyteTypeToJsonSchema {
                 objNode.put("additionalProperties", airbyteType.additionalProperties)
                 objNode
             }
-            is ObjectTypeWithoutSchema -> ofType("object")
+            is ObjectTypeWithoutSchema -> ofType("object", nullable)
             is ObjectTypeWithEmptySchema -> {
-                val objectNode = ofType("object")
+                val objectNode = ofType("object", nullable)
                 objectNode.putObject("properties")
                 objectNode
             }
@@ -51,21 +59,21 @@ class AirbyteTypeToJsonSchema {
                 airbyteType.options.forEach { unionOptions.add(convert(it)) }
                 unionNode
             }
-            is DateType -> ofType("string").put("format", "date")
+            is DateType -> ofType("string", nullable).put("format", "date")
             is TimeTypeWithTimezone -> {
-                val timeNode = ofType("string").put("format", "time")
+                val timeNode = ofType("string", nullable).put("format", "time")
                 timeNode.put("airbyte_type", "time_with_timezone")
             }
             is TimeTypeWithoutTimezone -> {
-                val timeNode = ofType("string").put("format", "time")
+                val timeNode = ofType("string", nullable).put("format", "time")
                 timeNode.put("airbyte_type", "time_without_timezone")
             }
             is TimestampTypeWithTimezone -> {
-                val timestampNode = ofType("string").put("format", "date-time")
+                val timestampNode = ofType("string", nullable).put("format", "date-time")
                 timestampNode.put("airbyte_type", "timestamp_with_timezone")
             }
             is TimestampTypeWithoutTimezone -> {
-                val timestampNode = ofType("string").put("format", "date-time")
+                val timestampNode = ofType("string", nullable).put("format", "date-time")
                 timestampNode.put("airbyte_type", "timestamp_without_timezone")
             }
             is UnknownType -> airbyteType.schema
