5a778dc8a35f2e33c4f7f2f80d7627f7b970229a
Aldo Gonzalez <aldo.gonzalez@airbyte.io>
2025-06-27 04:10:23 -0600

    fix import in test

--- airbyte-integrations/connectors/source-slack/unit_tests/test_components.py
+++ airbyte-integrations/connectors/source-slack/unit_tests/test_components.py
@@ -8,10 +8,10 @@ from source_slack import SourceSlack
 from source_slack.components.channel_members_extractor import ChannelMembersExtractor
 from source_slack.components.join_channels import ChannelsRetriever, JoinChannelsStream
 
+from airbyte_cdk.models import SyncMode
 from airbyte_cdk.sources.declarative.extractors import DpathExtractor, RecordSelector
 from airbyte_cdk.sources.declarative.requesters import HttpRequester
 from airbyte_cdk.sources.streams.http.requests_native_auth import TokenAuthenticator
-from airbyte_protocol.models import SyncMode
 
 
 def get_stream_by_name(stream_name, config):
