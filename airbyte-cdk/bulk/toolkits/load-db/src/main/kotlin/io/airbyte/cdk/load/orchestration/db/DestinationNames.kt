97ddf224e413a559da9d4272759d7307631e0511
benmoriceau <benoit@airbyte.io>
2025-06-30 12:41:05 -0700

    More cleanup

--- airbyte-cdk/bulk/toolkits/load-db/src/main/kotlin/io/airbyte/cdk/load/orchestration/db/DestinationNames.kt
+++ airbyte-cdk/bulk/toolkits/load-db/src/main/kotlin/io/airbyte/cdk/load/orchestration/db/DestinationNames.kt
@@ -101,9 +101,7 @@ open class DefaultTempTableNameGenerator(
  * Some destinations don't support moving table between namespace. This will generate a temp table
  * in the same namespace as the origin table.
  */
-class DefaultTempTableNameGeneratorPreserveNamespace(
-    internalNamespace: String,
-) : DefaultTempTableNameGenerator(internalNamespace) {
+class DefaultTempTableNameGeneratorPreserveNamespace() : DefaultTempTableNameGenerator("unused") {
     override fun generate(originalName: TableName): TableName {
         val resolvedTableName = super.generate(originalName)
 
