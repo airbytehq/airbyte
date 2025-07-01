01f50b0fe4c3eeab9821e5a9be76f6d1461f369e
Brian Lai <51336873+brianjlai@users.noreply.github.com>
2025-07-01 11:36:31 -0700

    feat(source-amazon-ads): Convert Amazon Ads to manifest-only format (#61707)

--- airbyte-integrations/connectors/source-amazon-ads/unit_tests/integrations/ad_requests/sponsored_brands_report_v3_request_builder.py
+++ airbyte-integrations/connectors/source-amazon-ads/unit_tests/integrations/ad_requests/sponsored_brands_report_v3_request_builder.py
@@ -1,10 +1,10 @@
-# Copyright (c) 2023 Airbyte, Inc., all rights reserved.
+# Copyright (c) 2025 Airbyte, Inc., all rights reserved.
 
 import json
 from collections import OrderedDict
 from typing import Any, Dict, List, Optional
 
-import pendulum
+from airbyte_cdk.utils.datetime_helpers import AirbyteDateTime
 
 from .base_request_builder import AmazonAdsBaseRequestBuilder
 
@@ -89,8 +89,8 @@ class SponsoredBrandsV3ReportRequestBuilder(AmazonAdsBaseRequestBuilder):
 
         return json.dumps(body)
 
-    def with_report_date(self, report_date: pendulum.date) -> "SponsoredBrandsV3ReportRequestBuilder":
-        self._report_date = report_date.format("YYYY-MM-DD")
+    def with_report_date(self, report_date: AirbyteDateTime) -> "SponsoredBrandsV3ReportRequestBuilder":
+        self._report_date = report_date.strftime("%Y-%m-%d")
         return self
 
     def with_report_type(self, report_type: str) -> "SponsoredBrandsV3ReportRequestBuilder":
--- airbyte-integrations/connectors/source-amazon-ads/unit_tests/integrations/utils.py
+++ airbyte-integrations/connectors/source-amazon-ads/unit_tests/integrations/utils.py
@@ -1,21 +1,20 @@
-# Copyright (c) 2023 Airbyte, Inc., all rights reserved.
+# Copyright (c) 2025 Airbyte, Inc., all rights reserved.
 
 import operator
 from typing import Any, Dict, List, Optional
-
-from source_amazon_ads import SourceAmazonAds
 
 from airbyte_cdk.models import AirbyteMessage, SyncMode
 from airbyte_cdk.models import Level as LogLevel
 from airbyte_cdk.test.catalog_builder import CatalogBuilder
 from airbyte_cdk.test.entrypoint_wrapper import EntrypointOutput, read
+from unit_tests.conftest import get_source
 
 
 def read_stream(
     stream_name: str, sync_mode: SyncMode, config: Dict[str, Any], state: Optional[Dict[str, Any]] = None, expecting_exception: bool = False
 ) -> EntrypointOutput:
     catalog = CatalogBuilder().with_stream(stream_name, sync_mode).build()
-    return read(SourceAmazonAds(catalog=catalog, config=config, state=state), config, catalog, state, expecting_exception)
+    return read(get_source(config=config, state=state), config, catalog, state, expecting_exception)
 
 
 def get_log_messages_by_log_level(logs: List[AirbyteMessage], log_level: LogLevel) -> List[str]:
