826e1e75271138da4ca10c489300c20a08248a70
darynaishchenko <darina.ishchenko17@gmail.com>
2025-07-03 16:07:26 +0300

    updated conftest

--- airbyte-integrations/connectors/source-bing-ads/unit_tests/conftest.py
+++ airbyte-integrations/connectors/source-bing-ads/unit_tests/conftest.py
@@ -1,12 +1,28 @@
 #
 # Copyright (c) 2023 Airbyte, Inc., all rights reserved.
 #
+import sys
 from unittest.mock import patch
 
 import pytest
-from source_bing_ads import SourceBingAds
 
 from airbyte_cdk.test.state_builder import StateBuilder
+from airbyte_cdk.sources.declarative.yaml_declarative_source import YamlDeclarativeSource
+
+from airbyte_cdk.test.catalog_builder import CatalogBuilder
+from pathlib import Path
+
+
+def _get_manifest_path() -> Path:
+    source_declarative_manifest_path = Path("/airbyte/integration_code/source_declarative_manifest")
+    if source_declarative_manifest_path.exists():
+        return source_declarative_manifest_path
+    return Path(__file__).parent.parent
+
+
+_SOURCE_FOLDER_PATH = _get_manifest_path()
+_YAML_FILE_PATH = _SOURCE_FOLDER_PATH / "manifest.yaml"
+sys.path.append(str(_SOURCE_FOLDER_PATH))  # to allow loading custom components
 
 
 @pytest.fixture(autouse=True)
@@ -129,9 +145,14 @@ def mock_account_query_fixture(requests_mock):
     )
 
 
+def get_source(config, state=None) -> YamlDeclarativeSource:
+    catalog = CatalogBuilder().build()
+    state = StateBuilder().build() if not state else state
+    return YamlDeclarativeSource(path_to_yaml=str(_YAML_FILE_PATH), catalog=catalog, config=config, state=state)
+
 def find_stream(stream_name, config, state=None):
     state = StateBuilder().build() if not state else state
-    streams = SourceBingAds(catalog=None, config=config, state=state).streams(config=config)
+    streams = get_source(config, state).streams(config=config)
     for stream in streams:
         if stream.name == stream_name:
             return stream
