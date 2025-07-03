14823babdd1d6b78597c20443dffb89810985811
tryangul <ryan.broughan@gmail.com>
2025-07-03 13:52:39 -0700

    Munger calls stringify when appropriate.

--- airbyte-integrations/connectors/destination-clickhouse-v2/src/test/kotlin/io/airbyte/integrations/destination/clickhouse_v2/write/transform/RecordMungerTest.kt
+++ airbyte-integrations/connectors/destination-clickhouse-v2/src/test/kotlin/io/airbyte/integrations/destination/clickhouse_v2/write/transform/RecordMungerTest.kt
@@ -5,11 +5,16 @@
 package io.airbyte.integrations.destination.clickhouse_v2.write.transform
 
 import io.airbyte.cdk.load.data.AirbyteValue
+import io.airbyte.cdk.load.data.BooleanType
 import io.airbyte.cdk.load.data.BooleanValue
 import io.airbyte.cdk.load.data.EnrichedAirbyteValue
+import io.airbyte.cdk.load.data.FieldType
+import io.airbyte.cdk.load.data.IntegerType
 import io.airbyte.cdk.load.data.IntegerValue
+import io.airbyte.cdk.load.data.ObjectValue
 import io.airbyte.cdk.load.data.StringType
 import io.airbyte.cdk.load.data.StringValue
+import io.airbyte.cdk.load.data.UnionType
 import io.airbyte.cdk.load.message.DestinationRecordRaw
 import io.airbyte.cdk.load.message.EnrichedDestinationRecordAirbyteValue
 import io.airbyte.cdk.load.orchestration.db.legacy_typing_deduping.TableCatalog
@@ -46,14 +51,19 @@ class RecordMungerTest {
 
         every { validator.validate(any()) } answers { firstArg() }
 
+        val stringfiedValue = Fixtures.mockCoercedValue(StringValue("stringified"))
+        every { validator.stringify(any()) } answers { stringfiedValue }
+
         // mock coercion output
-        val userFields =
+        val nonUnionUserFields =
             linkedMapOf(
                 "user_field_1" to Fixtures.mockCoercedValue(StringValue("test1")),
                 "user_field_2" to Fixtures.mockCoercedValue(StringValue("test2")),
                 "user_field_3" to Fixtures.mockCoercedValue(IntegerValue(777)),
                 "user_field_4" to Fixtures.mockCoercedValue(BooleanValue(false)),
             )
+        val unionUserField = "user_field_5" to Fixtures.mockCoercedValue(ObjectValue(linkedMapOf()))
+        val userFields = (nonUnionUserFields + unionUserField) as LinkedHashMap<String, EnrichedAirbyteValue>
         val internalFields =
             mapOf(
                 "internal_field_1" to Fixtures.mockCoercedValue(StringValue("internal1")),
@@ -69,6 +79,14 @@ class RecordMungerTest {
         val input =
             mockk<DestinationRecordRaw>(relaxed = true) {
                 every { asEnrichedDestinationRecordAirbyteValue(any()) } answers { coerced }
+                every { schemaFields } returns linkedMapOf(
+                    "user_field_1" to FieldType(StringType, false),
+                    "user_field_2" to FieldType(StringType, false),
+                    "user_field_3" to FieldType(IntegerType, false),
+                    "user_field_4" to FieldType(BooleanType, false),
+                    // this field is a union type so it should be stringified
+                    "user_field_5" to FieldType(UnionType(setOf(), false), false),
+                )
             }
 
         val output = munger.transformForDest(input)
@@ -78,7 +96,10 @@ class RecordMungerTest {
         verify {
             input.asEnrichedDestinationRecordAirbyteValue(extractedAtAsTimestampWithTimezone = true)
         }
-        coerced.declaredFields.forEach { verify { validator.validate(it.value) } }
+        // we validate each non-union field directly
+        nonUnionUserFields.forEach { verify { validator.validate(it.value) } }
+        // the stringified field is also validated
+        verify { validator.validate(stringfiedValue) }
 
         // munged keys map to unwrapped / coerced values
         val expected =
@@ -88,6 +109,8 @@ class RecordMungerTest {
                 "user_field_2_munged" to StringValue("test2"),
                 "user_field_3_munged" to IntegerValue(777),
                 "user_field_4_munged" to BooleanValue(false),
+                // union is stringified
+                "user_field_5_munged" to stringfiedValue.abValue,
                 // internal cols are not munged for now
                 "internal_field_1" to StringValue("internal1"),
                 "internal_field_2" to IntegerValue(0),
