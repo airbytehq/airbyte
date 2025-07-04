be0f11cd2d2ae773fa2e8d1b4b27c2a6753bcb37
Aaron ("AJ") Steers <aj@airbyte.io>
2025-07-03 22:46:12 -0700

    Apply suggestion from @aaronsteers

--- poe-tasks/detect-python-cdk.py
+++ poe-tasks/detect-python-cdk.py
@@ -196,7 +196,7 @@ def main():
         sys.exit(0 if success else 1)
 
     else:
-        print(json.dumps(cdk_info, indent=2))
+        print(json.dumps(cdk_info))
 
 
 if __name__ == "__main__":
