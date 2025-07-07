1ff3468b71279f70d36b3cb889094f83c7731b8b
Ryan Br... <ryan.broughan@gmail.com>
2025-07-07 12:56:36 -0700

    Rbroughan/cleanup stringify unions (#62509)
    Co-authored-by: Octavia Squidington III <octavia-squidington-iii@users.noreply.github.com>

--- airbyte-integrations/connectors/destination-clickhouse-v2/src/test/kotlin/io/airbyte/integrations/destination/clickhouse_v2/write/transform/ClickhouseCoercerTest.kt
+++ airbyte-integrations/connectors/destination-clickhouse-v2/src/test/kotlin/io/airbyte/integrations/destination/clickhouse_v2/write/transform/ClickhouseCoercerTest.kt
@@ -5,22 +5,31 @@
 package io.airbyte.integrations.destination.clickhouse_v2.write.transform
 
 import io.airbyte.cdk.load.data.AirbyteValue
+import io.airbyte.cdk.load.data.ArrayValue
+import io.airbyte.cdk.load.data.BooleanValue
 import io.airbyte.cdk.load.data.DateValue
 import io.airbyte.cdk.load.data.EnrichedAirbyteValue
 import io.airbyte.cdk.load.data.IntegerValue
 import io.airbyte.cdk.load.data.NullValue
 import io.airbyte.cdk.load.data.NumberValue
+import io.airbyte.cdk.load.data.ObjectValue
 import io.airbyte.cdk.load.data.StringType
+import io.airbyte.cdk.load.data.StringValue
+import io.airbyte.cdk.load.data.TimeWithTimezoneValue
+import io.airbyte.cdk.load.data.TimeWithoutTimezoneValue
 import io.airbyte.cdk.load.data.TimestampWithTimezoneValue
 import io.airbyte.cdk.load.data.TimestampWithoutTimezoneValue
-import io.airbyte.integrations.destination.clickhouse_v2.write.transform.ClickhouseCoercingValidator.Constants.DATE32_MAX
-import io.airbyte.integrations.destination.clickhouse_v2.write.transform.ClickhouseCoercingValidator.Constants.DATE32_MIN
-import io.airbyte.integrations.destination.clickhouse_v2.write.transform.ClickhouseCoercingValidator.Constants.DECIMAL128_MAX
-import io.airbyte.integrations.destination.clickhouse_v2.write.transform.ClickhouseCoercingValidator.Constants.DECIMAL128_MIN
-import io.airbyte.integrations.destination.clickhouse_v2.write.transform.ClickhouseCoercingValidator.Constants.INT64_MAX
-import io.airbyte.integrations.destination.clickhouse_v2.write.transform.ClickhouseCoercingValidator.Constants.INT64_MIN
-import io.airbyte.integrations.destination.clickhouse_v2.write.transform.ClickhouseCoercingValidatorTest.Fixtures.toAirbyteIntegerValue
-import io.airbyte.integrations.destination.clickhouse_v2.write.transform.ClickhouseCoercingValidatorTest.Fixtures.toAirbyteNumberValue
+import io.airbyte.integrations.destination.clickhouse_v2.write.transform.ClickhouseCoercer.Constants.DATE32_MAX
+import io.airbyte.integrations.destination.clickhouse_v2.write.transform.ClickhouseCoercer.Constants.DATE32_MIN
+import io.airbyte.integrations.destination.clickhouse_v2.write.transform.ClickhouseCoercer.Constants.DECIMAL128_MAX
+import io.airbyte.integrations.destination.clickhouse_v2.write.transform.ClickhouseCoercer.Constants.DECIMAL128_MIN
+import io.airbyte.integrations.destination.clickhouse_v2.write.transform.ClickhouseCoercer.Constants.INT64_MAX
+import io.airbyte.integrations.destination.clickhouse_v2.write.transform.ClickhouseCoercer.Constants.INT64_MIN
+import io.airbyte.integrations.destination.clickhouse_v2.write.transform.ClickhouseCoercerTest.Fixtures.toAirbyteDateValue
+import io.airbyte.integrations.destination.clickhouse_v2.write.transform.ClickhouseCoercerTest.Fixtures.toAirbyteIntegerValue
+import io.airbyte.integrations.destination.clickhouse_v2.write.transform.ClickhouseCoercerTest.Fixtures.toAirbyteNumberValue
+import io.airbyte.integrations.destination.clickhouse_v2.write.transform.ClickhouseCoercerTest.Fixtures.toAirbyteTimestampWithTimezoneValue
+import io.airbyte.integrations.destination.clickhouse_v2.write.transform.ClickhouseCoercerTest.Fixtures.toAirbyteTimestampWithoutTimezoneValue
 import io.airbyte.protocol.models.v0.AirbyteRecordMessageMetaChange
 import io.mockk.impl.annotations.InjectMockKs
 import io.mockk.junit5.MockKExtension
@@ -29,7 +38,9 @@ import java.math.BigInteger
 import java.time.LocalDate
 import java.time.LocalDateTime
 import java.time.LocalTime
+import java.time.OffsetTime
 import java.time.ZoneOffset
+import kotlin.test.Test
 import kotlin.test.assertEquals
 import org.junit.jupiter.api.extension.ExtendWith
 import org.junit.jupiter.params.ParameterizedTest
@@ -37,16 +48,16 @@ import org.junit.jupiter.params.provider.Arguments
 import org.junit.jupiter.params.provider.MethodSource
 
 @ExtendWith(MockKExtension::class)
-class ClickhouseCoercingValidatorTest {
+class ClickhouseCoercerTest {
 
-    @InjectMockKs private lateinit var validator: ClickhouseCoercingValidator
+    @InjectMockKs private lateinit var coercer: ClickhouseCoercer
 
     @ParameterizedTest
     @MethodSource("validFloats")
     fun `validate and coerces big decimals - valid values are left unchanged`(value: String) {
         val input = Fixtures.mockCoercedValue(value.toAirbyteNumberValue())
 
-        val result = validator.validateAndCoerce(input)
+        val result = coercer.validate(input)
 
         assertEquals(input, result)
         assertEquals(input.abValue, result.abValue)
@@ -58,7 +69,7 @@ class ClickhouseCoercingValidatorTest {
     fun `validate and coerces big decimals - invalid values are nulled`(value: String) {
         val input = Fixtures.mockCoercedValue(value.toAirbyteNumberValue())
 
-        val result = validator.validateAndCoerce(input)
+        val result = coercer.validate(input)
 
         assertEquals(NullValue, result.abValue)
         assertEquals(
@@ -72,7 +83,7 @@ class ClickhouseCoercingValidatorTest {
     fun `validate and coerces big integers - valid values are left unchanged`(value: String) {
         val input = Fixtures.mockCoercedValue(value.toAirbyteIntegerValue())
 
-        val result = validator.validateAndCoerce(input)
+        val result = coercer.validate(input)
 
         assertEquals(input, result)
         assertEquals(input.abValue, result.abValue)
@@ -84,7 +95,7 @@ class ClickhouseCoercingValidatorTest {
     fun `validate and coerces big integers - invalid values are nulled`(value: String) {
         val input = Fixtures.mockCoercedValue(value.toAirbyteIntegerValue())
 
-        val result = validator.validateAndCoerce(input)
+        val result = coercer.validate(input)
 
         assertEquals(NullValue, result.abValue)
         assertEquals(
@@ -96,10 +107,10 @@ class ClickhouseCoercingValidatorTest {
     @ParameterizedTest
     @MethodSource("validDates")
     fun `validate and coerces dates - valid values are left unchanged`(value: Long) {
-        val date = LocalDate.ofEpochDay(value)
-        val input = Fixtures.mockCoercedValue(DateValue(date))
+        val date = value.toAirbyteDateValue()
+        val input = Fixtures.mockCoercedValue(date)
 
-        val result = validator.validateAndCoerce(input)
+        val result = coercer.validate(input)
 
         assertEquals(input, result)
         assertEquals(input.abValue, result.abValue)
@@ -109,10 +120,10 @@ class ClickhouseCoercingValidatorTest {
     @ParameterizedTest
     @MethodSource("invalidDates")
     fun `validate and coerces dates - invalid values are nulled`(value: Long) {
-        val date = LocalDate.ofEpochDay(value)
-        val input = Fixtures.mockCoercedValue(DateValue(date))
+        val date = value.toAirbyteDateValue()
+        val input = Fixtures.mockCoercedValue(date)
 
-        val result = validator.validateAndCoerce(input)
+        val result = coercer.validate(input)
 
         assertEquals(NullValue, result.abValue)
         assertEquals(
@@ -126,11 +137,10 @@ class ClickhouseCoercingValidatorTest {
     fun `validate and coerces timestamps with timezones - valid values are left unchanged`(
         value: Long
     ) {
-        val timestamp =
-            LocalDateTime.of(LocalDate.ofEpochDay(value), LocalTime.MAX).atOffset(ZoneOffset.UTC)
-        val input = Fixtures.mockCoercedValue(TimestampWithTimezoneValue(timestamp))
+        val timestamp = value.toAirbyteTimestampWithTimezoneValue()
+        val input = Fixtures.mockCoercedValue(timestamp)
 
-        val result = validator.validateAndCoerce(input)
+        val result = coercer.validate(input)
 
         assertEquals(input, result)
         assertEquals(input.abValue, result.abValue)
@@ -140,11 +150,10 @@ class ClickhouseCoercingValidatorTest {
     @ParameterizedTest
     @MethodSource("invalidDates")
     fun `validate and coerces timestamps with timezones - invalid values are nulled`(value: Long) {
-        val timestamp =
-            LocalDateTime.of(LocalDate.ofEpochDay(value), LocalTime.MAX).atOffset(ZoneOffset.UTC)
-        val input = Fixtures.mockCoercedValue(TimestampWithTimezoneValue(timestamp))
+        val timestamp = value.toAirbyteTimestampWithTimezoneValue()
+        val input = Fixtures.mockCoercedValue(timestamp)
 
-        val result = validator.validateAndCoerce(input)
+        val result = coercer.validate(input)
 
         assertEquals(NullValue, result.abValue)
         assertEquals(
@@ -158,10 +167,10 @@ class ClickhouseCoercingValidatorTest {
     fun `validate and coerces timestamps without timezones - valid values are left unchanged`(
         value: Long
     ) {
-        val timestamp = LocalDateTime.of(LocalDate.ofEpochDay(value), LocalTime.MAX)
-        val input = Fixtures.mockCoercedValue(TimestampWithoutTimezoneValue(timestamp))
+        val timestamp = value.toAirbyteTimestampWithoutTimezoneValue()
+        val input = Fixtures.mockCoercedValue(timestamp)
 
-        val result = validator.validateAndCoerce(input)
+        val result = coercer.validate(input)
 
         assertEquals(input, result)
         assertEquals(input.abValue, result.abValue)
@@ -173,16 +182,35 @@ class ClickhouseCoercingValidatorTest {
     fun `validate and coerces timestamps without timezones - invalid values are nulled`(
         value: Long
     ) {
-        val timestamp = LocalDateTime.of(LocalDate.ofEpochDay(value), LocalTime.MAX)
-        val input = Fixtures.mockCoercedValue(TimestampWithoutTimezoneValue(timestamp))
+        val timestamp = value.toAirbyteTimestampWithoutTimezoneValue()
+        val input = Fixtures.mockCoercedValue(timestamp)
 
-        val result = validator.validateAndCoerce(input)
+        val result = coercer.validate(input)
 
         assertEquals(NullValue, result.abValue)
         assertEquals(
             AirbyteRecordMessageMetaChange.Reason.DESTINATION_FIELD_SIZE_LIMITATION,
             result.changes[0].reason
         )
+    }
+
+    @ParameterizedTest
+    @MethodSource("nonNullValues")
+    fun `toJsonString turns non-null values into a string`(value: AirbyteValue) {
+        val input = Fixtures.mockCoercedValue(value)
+
+        val result = coercer.toJsonStringValue(input)
+
+        assert(result.abValue is StringValue)
+    }
+
+    @Test
+    fun `toJsonString passes through null values`() {
+        val input = Fixtures.mockCoercedValue(NullValue)
+
+        val result = coercer.toJsonStringValue(input)
+
+        assertEquals(NullValue, result.abValue)
     }
 
     companion object {
@@ -259,12 +287,45 @@ class ClickhouseCoercingValidatorTest {
                 Arguments.of(255670),
                 Arguments.of(-255670),
             )
+
+        @JvmStatic
+        fun nonNullValues() =
+            listOf(
+                Arguments.of(NumberValue(BigDecimal("89214.7834"))),
+                Arguments.of(IntegerValue(BigInteger("72317631278"))),
+                Arguments.of(StringValue("this should just stay the same")),
+                Arguments.of(
+                    ObjectValue(
+                        linkedMapOf("cat" to StringValue("dog"), "bird" to StringValue("dog"))
+                    )
+                ),
+                Arguments.of(ArrayValue(listOf(StringValue("cat"), StringValue("dog")))),
+                Arguments.of(BooleanValue(true)),
+                Arguments.of(BooleanValue(false)),
+                Arguments.of(1241124L.toAirbyteDateValue()),
+                Arguments.of(9924124L.toAirbyteTimestampWithTimezoneValue()),
+                Arguments.of(16524124L.toAirbyteTimestampWithoutTimezoneValue()),
+                Arguments.of(TimeWithoutTimezoneValue(LocalTime.now())),
+                Arguments.of(TimeWithTimezoneValue(OffsetTime.now())),
+            )
     }
 
     object Fixtures {
         fun String.toAirbyteNumberValue() = NumberValue(BigDecimal(this))
 
         fun String.toAirbyteIntegerValue() = IntegerValue(BigInteger(this))
+
+        fun Long.toAirbyteDateValue() = DateValue(LocalDate.ofEpochDay(this))
+
+        fun Long.toAirbyteTimestampWithTimezoneValue() =
+            TimestampWithTimezoneValue(
+                LocalDateTime.of(LocalDate.ofEpochDay(this), LocalTime.MAX).atOffset(ZoneOffset.UTC)
+            )
+
+        fun Long.toAirbyteTimestampWithoutTimezoneValue() =
+            TimestampWithoutTimezoneValue(
+                LocalDateTime.of(LocalDate.ofEpochDay(this), LocalTime.MAX)
+            )
 
         fun mockCoercedValue(value: AirbyteValue) =
             EnrichedAirbyteValue(
