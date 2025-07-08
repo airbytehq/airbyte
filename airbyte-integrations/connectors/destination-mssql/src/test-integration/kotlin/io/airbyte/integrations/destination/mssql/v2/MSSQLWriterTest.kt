da56fa8b65ee608bb52b7ce79face4c4b61b40b9
Edward Gao <edward.gao@airbyte.io>
2025-07-08 13:47:43 -0700

    Destination bigquery: more schema evolution fixes (#62448)

--- airbyte-integrations/connectors/destination-mssql/src/test-integration/kotlin/io/airbyte/integrations/destination/mssql/v2/MSSQLWriterTest.kt
+++ airbyte-integrations/connectors/destination-mssql/src/test-integration/kotlin/io/airbyte/integrations/destination/mssql/v2/MSSQLWriterTest.kt
@@ -91,6 +91,12 @@ abstract class MSSQLWriterTest(
     override fun testFunkyCharacters() {
         super.testFunkyCharacters()
     }
+
+    @Test
+    @Disabled("https://github.com/airbytehq/airbyte-internal-issues/issues/13572")
+    override fun testBizarrePkSchemaEvolution() {
+        super.testBizarrePkSchemaEvolution()
+    }
 }
 
 class MSSQLDataDumper(private val configProvider: (MSSQLSpecification) -> MSSQLConfiguration) :
