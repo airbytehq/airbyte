4129adfd94440697dd92cfa11785444f0acafa5b
tryangul <ryan.broughan@gmail.com>
2025-07-09 12:35:00 -0700

    Only set json properties on client when JSON enabled. Remove unnecessary ping code.

--- airbyte-integrations/connectors/destination-clickhouse-v2/src/main/kotlin/io/airbyte/integrations/destination/clickhouse_v2/config/ClickhouseBeanFactory.kt
+++ airbyte-integrations/connectors/destination-clickhouse-v2/src/main/kotlin/io/airbyte/integrations/destination/clickhouse_v2/config/ClickhouseBeanFactory.kt
@@ -19,48 +19,24 @@ import jakarta.inject.Singleton
 class ClickhouseBeanFactory {
     @Singleton
     fun clickhouseClient(config: ClickhouseConfiguration): Client {
-
-        val clientWithDb =
-            Client.Builder()
+        val builder = Client.Builder()
                 .addEndpoint(config.endpoint)
                 .setUsername(config.username)
                 .setPassword(config.password)
-                .setDefaultDatabase(config.resolvedDatabase)
                 .compressClientRequest(true)
+                .setClientName("airbyte-v2")
+
+        if (config.enableJson) {
+            builder
                 // allow experimental JSON type
                 .serverSetting("allow_experimental_json_type", "1")
                 // allow JSON transcoding as a string. We need this to be able to provide a string
                 // as a JSON input.
                 .serverSetting(ServerSettings.INPUT_FORMAT_BINARY_READ_JSON_AS_STRING, "1")
                 .serverSetting(ServerSettings.OUTPUT_FORMAT_BINARY_WRITE_JSON_AS_STRING, "1")
-                .setClientName("airbyte-v2")
-                .build()
-
-        return if (clientWithDb.ping()) {
-            clientWithDb
-        } else {
-            // We don't set the default database here because the client expects that database
-            // to already exist during instantiation. If we instantiate the client with a default
-            // database that does not exist, it will hard fail.
-
-            // In order to solve this chicken-and-egg problem, we avoid setting the default db on
-            // the client.
-            // Instead, we resolve the default database in the ClickhouseConfiguration, which is
-            // used for table creation when the stream descriptor does not specificy a namespace
-            // directly.
-            Client.Builder()
-                .addEndpoint(config.endpoint)
-                .setUsername(config.username)
-                .setPassword(config.password)
-                .compressClientRequest(true)
-                // allow experimental JSON type
-                .serverSetting("allow_experimental_json_type", "1")
-                // allow JSON transcoding as a string
-                .serverSetting(ServerSettings.INPUT_FORMAT_BINARY_READ_JSON_AS_STRING, "1")
-                .serverSetting(ServerSettings.OUTPUT_FORMAT_BINARY_WRITE_JSON_AS_STRING, "1")
-                .setClientName("airbyte-v2")
-                .build()
         }
+
+        return builder.build()
     }
 
     @Singleton
