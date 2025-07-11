4be98133d480888a54c36a0ff05ead6b69b47366
David Gold <32782137+dbgold17@users.noreply.github.com>
2025-07-11 12:39:59 -0700

    chore: Bump Python to 3.11.13 in python connector base image (#62909)

--- airbyte-ci/connectors/base_images/base_images/root_images.py
+++ airbyte-ci/connectors/base_images/base_images/root_images.py
@@ -33,6 +33,13 @@ PYTHON_3_11_11 = PublishedImage(
     sha="6ed5bff4d7d377e2a27d9285553b8c21cfccc4f00881de1b24c9bc8d90016e82",
 )
 
+PYTHON_3_11_13 = PublishedImage(
+    registry="docker.io",
+    repository="python",
+    tag="3.11.13-slim-bookworm",
+    sha="139020233cc412efe4c8135b0efe1c7569dc8b28ddd88bddb109b764f8977e30",
+)
+
 AMAZON_CORRETTO_21_AL_2023 = PublishedImage(
     registry="docker.io",
     repository="amazoncorretto",
