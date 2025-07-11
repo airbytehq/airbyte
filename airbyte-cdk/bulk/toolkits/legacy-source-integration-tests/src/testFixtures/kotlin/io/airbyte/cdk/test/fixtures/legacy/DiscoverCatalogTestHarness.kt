c9fea5c089a1e20fdd03801ca5ae7780ec97105f
matt.bayley <matt.bayley@airbyte.io>
2025-07-10 22:43:04 -0700

    remove legacy test scaffolding

--- airbyte-cdk/bulk/toolkits/legacy-source-integration-tests/src/testFixtures/kotlin/io/airbyte/cdk/test/fixtures/legacy/DiscoverCatalogTestHarness.kt
+++
@@ -1,7 +0,0 @@
-/*
- * Copyright (c) 2023 Airbyte, Inc., all rights reserved.
- */
-package io.airbyte.cdk.test.fixtures.legacy
-
-interface DiscoverCatalogTestHarness :
-    TestHarness<StandardDiscoverCatalogInput, ConnectorJobOutput>
