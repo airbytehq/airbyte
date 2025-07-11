a4d47460200e1b8d94c2e82e1f3498347ccc23a0
Matt Bayley <31358253+mwbayley@users.noreply.github.com>
2025-07-11 16:51:52 -0700

    Move source test framework to cdk (#62929)

--- airbyte-cdk/bulk/toolkits/source-tests/src/testFixtures/kotlin/io/airbyte/cdk/test/fixtures/cleanup/TestAssetResourceNamer.kt
+++ airbyte-cdk/bulk/toolkits/source-tests/src/testFixtures/kotlin/io/airbyte/cdk/test/fixtures/cleanup/TestAssetResourceNamer.kt
@@ -2,7 +2,7 @@
  * Copyright (c) 2025 Airbyte, Inc., all rights reserved.
  */
 
-package io.airbyte.integrations.sourceTesting
+package io.airbyte.cdk.test.fixtures.cleanup
 
 import java.time.Clock
 import kotlin.random.Random
