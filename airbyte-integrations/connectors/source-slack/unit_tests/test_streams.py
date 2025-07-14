65929056cd6eb12758a58572257e32885f97f38b
darynaishchenko <darina.ishchenko17@gmail.com>
2025-07-14 15:40:09 +0300

    fixed unit tests

--- airbyte-integrations/connectors/source-slack/unit_tests/test_streams.py
+++ airbyte-integrations/connectors/source-slack/unit_tests/test_streams.py
@@ -8,10 +8,12 @@ import pendulum
 import pytest
 from requests import Response
 from source_slack import SourceSlack
-from source_slack.streams import Channels, JoinChannelsStream, Threads
 
+from airbyte_cdk.models import ConfiguredAirbyteCatalogSerializer, SyncMode
 from airbyte_cdk.sources.streams.http.error_handlers import ResponseAction
 from airbyte_cdk.sources.streams.http.requests_native_auth import TokenAuthenticator
+from airbyte_cdk.test.entrypoint_wrapper import read
+from airbyte_cdk.test.state_builder import StateBuilder
 
 
 @pytest.fixture
@@ -20,7 +22,7 @@ def authenticator(token_config):
 
 
 def get_stream_by_name(stream_name, config):
-    streams = SourceSlack().streams(config=config)
+    streams = SourceSlack(catalog={}, config=config, state={}).streams(config=config)
     for stream in streams:
         if stream.name == stream_name:
             return stream
@@ -36,24 +38,33 @@ def get_stream_by_name(stream_name, config):
             [{"ts": 1577866844}, {"ts": 1577877406}],
             {},
             [
-                # two messages per each channel
-                {"channel": "airbyte-for-beginners", "ts": 1577866844},
-                {"channel": "airbyte-for-beginners", "ts": 1577877406},
-                {"channel": "good-reads", "ts": 1577866844},
-                {"channel": "good-reads", "ts": 1577877406},
+                {
+                    "float_ts": 1577866844,
+                    "parent_slice": {"channel": "airbyte-for-beginners", "parent_slice": {}},
+                    "start_time": "1626984000",
+                    "end_time": "1627070400",
+                },
+                {
+                    "float_ts": 1577877406,
+                    "parent_slice": {"channel": "airbyte-for-beginners", "parent_slice": {}},
+                    "start_time": "1626984000",
+                    "end_time": "1627070400",
+                },
+                {
+                    "float_ts": 1577866844,
+                    "parent_slice": {"channel": "good-reads", "parent_slice": {}},
+                    "start_time": "1626984000",
+                    "end_time": "1627070400",
+                },
+                {
+                    "float_ts": 1577877406,
+                    "parent_slice": {"channel": "good-reads", "parent_slice": {}},
+                    "start_time": "1626984000",
+                    "end_time": "1627070400",
+                },
             ],
         ),
-        ("2020-01-02T00:00:00Z", "2020-01-01T00:00:00Z", [], {}, [{}]),
-        (
-            "2020-01-01T00:00:00Z",
-            "2020-01-02T00:00:00Z",
-            [{"ts": 1577866844}, {"ts": 1577877406}],
-            {"float_ts": 2577866844},
-            [
-                # no slice when state greater than ts
-                {},
-            ],
-        ),
+        ("2020-01-02T00:00:00Z", "2020-01-01T00:00:00Z", [], {}, []),
     ),
 )
 def test_threads_stream_slices(requests_mock, authenticator, token_config, start_date, end_date, messages, stream_state, expected_result):
@@ -70,87 +81,174 @@ def test_threads_stream_slices(requests_mock, authenticator, token_config, start
         [{"json": {"messages": messages}}, {"json": {"messages": []}}],
     )
 
-    start_date = pendulum.parse(start_date)
-    end_date = end_date and pendulum.parse(end_date)
+    stream = get_stream_by_name("threads", token_config)
+    slices = list(stream.stream_slices(sync_mode=SyncMode.incremental, stream_state=stream_state))
 
-    stream = Threads(
-        authenticator=authenticator,
-        default_start_date=start_date,
-        end_date=end_date,
-        lookback_window=pendulum.Duration(days=token_config["lookback_window"]),
-    )
-    slices = list(stream.stream_slices(stream_state=stream_state))
-    assert slices == expected_result
+    assert len(slices) == len(expected_result)
+    for s in slices:
+        assert s in expected_result
 
 
 @pytest.mark.parametrize(
     "current_state, latest_record, expected_state",
     (
-        ({}, {"float_ts": 1507866844}, {"float_ts": 1626984000.0}),
-        ({}, {"float_ts": 1726984000}, {"float_ts": 1726984000.0}),
-        ({"float_ts": 1588866844}, {"float_ts": 1577866844}, {"float_ts": 1588866844}),
-        ({"float_ts": 1577800844}, {"float_ts": 1577866844}, {"float_ts": 1577866844}),
+        ({}, {"ts": 1507866844}, {"float_ts": "1626984000"}),
+        ({}, {"ts": 1726984000}, {"float_ts": "1726984000"}),
+        ({"float_ts": 1588866844}, {"ts": 1626984010}, {"float_ts": "1626984010"}),
+        ({"float_ts": 1577800844}, {"ts": 1626984010}, {"float_ts": "1626984010"}),
     ),
 )
-def test_get_updated_state(authenticator, token_config, current_state, latest_record, expected_state):
-    stream = Threads(
-        authenticator=authenticator,
-        default_start_date=pendulum.parse(token_config["start_date"]),
-        lookback_window=token_config["lookback_window"],
+def test_get_updated_state(requests_mock, authenticator, token_config, current_state, latest_record, expected_state):
+    requests_mock.register_uri(
+        "GET",
+        "https://slack.com/api/conversations.history?limit=1000&channel=airbyte-for-beginners",
+        [{"json": {"messages": [{"ts": 1507866847}]}}, {"json": {"messages": []}}],
     )
-    assert stream._get_updated_state(current_stream_state=current_state, latest_record=latest_record) == expected_state
+    requests_mock.register_uri(
+        "GET",
+        "https://slack.com/api/conversations.history?limit=1000&channel=good-reads",
+        [{"json": {"messages": [{"ts": 1507866847}]}}, {"json": {"messages": []}}],
+    )
+    requests_mock.register_uri(
+        "GET",
+        "https://slack.com/api/conversations.replies?channel=good-reads&limit=1000&ts=1507866847",
+        [{"json": {"messages": [latest_record]}}, {"json": {"messages": []}}],
+    )
+    requests_mock.register_uri(
+        "GET",
+        "https://slack.com/api/conversations.replies?channel=airbyte-for-beginners&limit=1000&ts=1507866847",
+        [{"json": {"messages": [latest_record]}}, {"json": {"messages": []}}],
+    )
+    catalog = ConfiguredAirbyteCatalogSerializer.load(
+        {
+            "streams": [
+                {
+                    "stream": {"name": "threads", "json_schema": {}, "supported_sync_modes": ["full_refresh", "incremental"]},
+                    "sync_mode": "incremental",
+                    "destination_sync_mode": "append",
+                }
+            ]
+        }
+    )
+    state = StateBuilder().with_stream_state("threads", current_state).build()
+    source_slack = SourceSlack(catalog=catalog, config=token_config, state=state)
+    output = read(source_slack, config=token_config, catalog=catalog, state=state)
+    assert output.records
+    assert output.most_recent_state.stream_state.state == expected_state
 
 
 def test_threads_request_params(authenticator, token_config):
-    stream = Threads(
-        authenticator=authenticator,
-        default_start_date=pendulum.parse(token_config["start_date"]),
-        lookback_window=token_config["lookback_window"],
-    )
-    threads_slice = {"channel": "airbyte-for-beginners", "ts": 1577866844}
-    expected = {"channel": "airbyte-for-beginners", "limit": 1000, "ts": 1577866844}
-    assert stream.request_params(stream_slice=threads_slice, stream_state={}) == expected
-
-
-def test_threads_parse_response(mocker, authenticator, token_config):
-    stream = Threads(
-        authenticator=authenticator,
-        default_start_date=pendulum.parse(token_config["start_date"]),
-        lookback_window=token_config["lookback_window"],
-    )
-    resp = {
-        "messages": [
-            {
-                "type": "message",
-                "user": "U061F7AUR",
-                "text": "island",
-                "thread_ts": "1482960137.003543",
-                "reply_count": 3,
-                "subscribed": True,
-                "last_read": "1484678597.521003",
-                "unread_count": 0,
-                "ts": "1482960137.003543",
-            }
-        ]
+    stream = get_stream_by_name("threads", token_config)
+    threads_slice = {"parent_slice": {"channel": "airbyte-for-beginners"}}
+    assert stream.retriever.requester.get_request_params(stream_slice=threads_slice, next_page_token={}) == {
+        "channel": "airbyte-for-beginners"
     }
-    resp_mock = mocker.Mock()
-    resp_mock.json.return_value = resp
-    threads_slice = {"channel": "airbyte-for-beginners", "ts": 1577866844}
-    actual_response = list(stream.parse_response(response=resp_mock, stream_slice=threads_slice))
-    assert len(actual_response) == 1
-    assert actual_response[0]["float_ts"] == 1482960137.003543
-    assert actual_response[0]["channel_id"] == "airbyte-for-beginners"
 
 
-@pytest.mark.parametrize("headers, expected_result", (({}, 5), ({"Retry-After": 15}, 15)))
-def test_backoff(token_config, authenticator, headers, expected_result):
-    stream = Threads(
-        authenticator=authenticator,
-        default_start_date=pendulum.parse(token_config["start_date"]),
-        lookback_window=token_config["lookback_window"],
+def test_threads_parse_response(requests_mock, authenticator, token_config):
+    requests_mock.register_uri(
+        "GET",
+        "https://slack.com/api/conversations.history?limit=1000&channel=airbyte-for-beginners",
+        [{"json": {"messages": [{"ts": 1507866847}]}}, {"json": {"messages": []}}],
     )
-    mocked_response = MagicMock(spec=Response, headers=headers)
-    assert stream.get_backoff_strategy().backoff_time(mocked_response) == expected_result
+    requests_mock.register_uri(
+        "GET",
+        "https://slack.com/api/conversations.history?limit=1000&channel=good-reads",
+        [{"json": {"messages": [{"ts": 1507866847}]}}, {"json": {"messages": []}}],
+    )
+
+    requests_mock.register_uri(
+        "GET",
+        "https://slack.com/api/conversations.replies?channel=airbyte-for-beginners&limit=1000&ts=1507866847",
+        [
+            {
+                "json": {
+                    "messages": [
+                        {
+                            "type": "message",
+                            "user": "U061F7AUR",
+                            "text": "island",
+                            "thread_ts": "1482960137.003543",
+                            "reply_count": 3,
+                            "subscribed": True,
+                            "last_read": "1484678597.521003",
+                            "unread_count": 0,
+                            "ts": "1482960137.003543",
+                        }
+                    ]
+                }
+            },
+            {"json": {"messages": []}},
+        ],
+    )
+    requests_mock.register_uri(
+        "GET",
+        "https://slack.com/api/conversations.replies?channel=good-reads&limit=1000&ts=1507866847",
+        [
+            {"json": {}},
+        ],
+    )
+
+    catalog = ConfiguredAirbyteCatalogSerializer.load(
+        {
+            "streams": [
+                {
+                    "stream": {"name": "threads", "json_schema": {}, "supported_sync_modes": ["full_refresh", "incremental"]},
+                    "sync_mode": "incremental",
+                    "destination_sync_mode": "append",
+                }
+            ]
+        }
+    )
+    state = StateBuilder().with_stream_state("threads", {}).build()
+    source_slack = SourceSlack(catalog=catalog, config=token_config, state=state)
+    output = read(source_slack, config=token_config, catalog=catalog, state=state)
+    actual_response = output.records
+    assert len(actual_response) == 1
+    assert actual_response[0].record.data["float_ts"] == 1482960137.003543
+    assert actual_response[0].record.data["channel_id"] == "airbyte-for-beginners"
+
+
+@pytest.mark.parametrize("headers, expected_result", (({"Retry-After": 15}, 15),))
+def test_backoff(requests_mock, token_config, authenticator, headers, expected_result):
+    requests_mock.get(
+        url="https://slack.com/api/conversations.replies?channel=airbyte-for-beginners&limit=1000&ts=1507866847",
+        status_code=429,
+        headers=headers,
+        json={"message": "rate limited"},
+    )
+    requests_mock.get(
+        url="https://slack.com/api/conversations.replies?channel=good-reads&limit=1000&ts=1507866847",
+        status_code=429,
+        headers=headers,
+        json={"message": "rate limited"},
+    )
+    requests_mock.register_uri(
+        "GET",
+        "https://slack.com/api/conversations.history?limit=1000&channel=airbyte-for-beginners",
+        [{"json": {"messages": [{"ts": 1507866847}]}}, {"json": {"messages": []}}],
+    )
+    requests_mock.register_uri(
+        "GET",
+        "https://slack.com/api/conversations.history?limit=1000&channel=good-reads",
+        [{"json": {"messages": [{"ts": 1507866847}]}}, {"json": {"messages": []}}],
+    )
+
+    catalog = ConfiguredAirbyteCatalogSerializer.load(
+        {
+            "streams": [
+                {
+                    "stream": {"name": "threads", "json_schema": {}, "supported_sync_modes": ["full_refresh", "incremental"]},
+                    "sync_mode": "incremental",
+                    "destination_sync_mode": "append",
+                }
+            ]
+        }
+    )
+    state = StateBuilder().with_stream_state("threads", {}).build()
+    source_slack = SourceSlack(catalog=catalog, config=token_config, state=state)
+    output = read(source_slack, config=token_config, catalog=catalog, state=state)
+    assert output.records == expected_result
 
 
 def test_channels_stream_with_autojoin(authenticator) -> None:
