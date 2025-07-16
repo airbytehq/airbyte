3ea23109a9d1298269165dd758b225425dbf8f48
Jeroen de Vries <jeroen@maildevries.eu>
2025-07-16 15:36:52 +0200

    explicit stream tests to verify endpoint

--- airbyte-integrations/connectors/source-exact/integration_tests/integration_test.py
+++ airbyte-integrations/connectors/source-exact/integration_tests/integration_test.py
@@ -8,7 +8,10 @@ from pathlib import Path
 from airbyte_protocol_dataclasses.models import ConfiguredAirbyteStream, DestinationSyncMode
 from pytest import fixture
 from source_exact import SourceExact
-from source_exact.streams import SyncProjectProjects
+from source_exact.streams import (
+    SyncProjectProjects, PayrollActiveEmployments, HRMDepartments, SyncPayrollEmployments, ProjectInvoiceTerms,
+    SyncSalesInvoiceSalesInvoices
+)
 
 from airbyte_cdk.models import ConfiguredAirbyteCatalog, SyncMode
 from airbyte_cdk.test.entrypoint_wrapper import EntrypointOutput, read
@@ -38,3 +41,48 @@ def test_read_sync_project_projects(config, configured_catalog):
     output: EntrypointOutput = read(source, config, configured_catalog)
 
     assert len(output.records) > 0
+
+def test_payroll_active_employments(config):
+    stream = PayrollActiveEmployments(config).as_airbyte_stream()
+    stream_configured = ConfiguredAirbyteStream(stream=stream, sync_mode=SyncMode.full_refresh, destination_sync_mode=DestinationSyncMode.overwrite)
+    configured_catalog = ConfiguredAirbyteCatalog(streams=[stream_configured])
+    source = SourceExact()
+    output: EntrypointOutput = read(source, config, configured_catalog)
+
+    assert len(output.records) >= 0
+
+def test_departments(config):
+    stream = HRMDepartments(config).as_airbyte_stream()
+    stream_configured = ConfiguredAirbyteStream(stream=stream, sync_mode=SyncMode.full_refresh, destination_sync_mode=DestinationSyncMode.overwrite)
+    configured_catalog = ConfiguredAirbyteCatalog(streams=[stream_configured])
+    source = SourceExact()
+    output: EntrypointOutput = read(source, config, configured_catalog)
+
+    assert len(output.records) >= 0
+
+def test_payroll_employments(config):
+    stream = SyncPayrollEmployments(config).as_airbyte_stream()
+    stream_configured = ConfiguredAirbyteStream(stream=stream,sync_mode=SyncMode.full_refresh, destination_sync_mode=DestinationSyncMode.overwrite)
+    configured_catalog = ConfiguredAirbyteCatalog(streams=[stream_configured])
+    source = SourceExact()
+    output: EntrypointOutput = read(source, config, configured_catalog)
+
+    assert len(output.records) >= 0
+
+def test_invoice_terms(config):
+    stream = ProjectInvoiceTerms(config).as_airbyte_stream()
+    stream_configured = ConfiguredAirbyteStream(stream=stream, sync_mode=SyncMode.full_refresh, destination_sync_mode=DestinationSyncMode.overwrite)
+    configured_catalog = ConfiguredAirbyteCatalog(streams=[stream_configured])
+    source = SourceExact()
+    output: EntrypointOutput = read(source, config, configured_catalog)
+
+    assert len(output.records) >= 0
+
+def test_sales_invoice(config):
+    stream = SyncSalesInvoiceSalesInvoices(config).as_airbyte_stream()
+    stream_configured = ConfiguredAirbyteStream(stream=stream, sync_mode=SyncMode.full_refresh, destination_sync_mode=DestinationSyncMode.overwrite)
+    configured_catalog = ConfiguredAirbyteCatalog(streams=[stream_configured])
+    source = SourceExact()
+    output: EntrypointOutput = read(source, config, configured_catalog)
+
+    assert len(output.records) >= 0
