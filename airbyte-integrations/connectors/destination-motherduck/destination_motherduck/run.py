a68c3874a986b8791106e87f9cf181d730eb887f
Aaron Steers <aj@airbyte.io>
2025-07-21 17:25:47 -0700

    make injection conditional

--- airbyte-integrations/connectors/destination-motherduck/destination_motherduck/run.py
+++ airbyte-integrations/connectors/destination-motherduck/destination_motherduck/run.py
@@ -3,12 +3,15 @@
 import os
 import sys
 
+from pathlib import Path
+
 from destination_motherduck import DestinationMotherDuck
 
 
 def run() -> None:
     # Resolve unset "HOME" variable and lack of access to '/nonexistent' path
-    os.environ["HOME"] = "/airbyte"
+    if "HOME" not in os.environ and Path("/airbyte").exists():
+        os.environ["HOME"] = "/airbyte"
 
     DestinationMotherDuck().run(sys.argv[1:])
 
