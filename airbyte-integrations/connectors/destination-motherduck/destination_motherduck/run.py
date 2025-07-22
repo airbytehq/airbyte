90d9ee1d1c3ecec1802f22a49ecc0323470ee39a
Aaron Steers <aj@airbyte.io>
2025-07-21 17:11:44 -0700

    fix: declare PATH explicitly

--- airbyte-integrations/connectors/destination-motherduck/destination_motherduck/run.py
+++ airbyte-integrations/connectors/destination-motherduck/destination_motherduck/run.py
@@ -1,11 +1,15 @@
 # Copyright (c) 2024 Airbyte, Inc., all rights reserved.
 
+import os
 import sys
 
 from destination_motherduck import DestinationMotherDuck
 
 
 def run() -> None:
+    # Resolve unset "HOME" variable and lack of access to '/nonexistent' path
+    os.environ["HOME"] = "/airbyte"
+
     DestinationMotherDuck().run(sys.argv[1:])
 
 
