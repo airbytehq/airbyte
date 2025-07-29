d1f15a0da853a5c9a53f54e50dfd6553d80de2e7
benmoriceau <benoit@airbyte.io>
2025-07-29 12:14:12 -0700

    Tmp

--- airbyte-cdk/bulk/core/load/src/main/kotlin/io/airbyte/cdk/load/lifecycle/DestinationLifecycle.kt
+++ airbyte-cdk/bulk/core/load/src/main/kotlin/io/airbyte/cdk/load/lifecycle/DestinationLifecycle.kt
@@ -26,7 +26,7 @@ class DestinationLifecycle(
         // Initialize the destination to make sure that it is ready for the data ingestion
         initializeDestination()
 
-        // Create prepare individual streams for the data ingestion. E.g create table and propagate the schema updates
+        // Create prepare individual streams for the data ingestion. E.g create tables and propagate the schema updates
         initializeIndividualStream()
     }
 
