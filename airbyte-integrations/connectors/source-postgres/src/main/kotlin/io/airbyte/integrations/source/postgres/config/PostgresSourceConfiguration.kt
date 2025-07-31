f5dcdbf53f3168cc5338de5991bc749f356aa67a
matt.bayley <matt.bayley@airbyte.io>
2025-07-31 11:19:32 -0700

    license headers

--- airbyte-integrations/connectors/source-postgres/src/main/kotlin/io/airbyte/integrations/source/postgres/config/PostgresSourceConfiguration.kt
+++ airbyte-integrations/connectors/source-postgres/src/main/kotlin/io/airbyte/integrations/source/postgres/config/PostgresSourceConfiguration.kt
@@ -1,3 +1,7 @@
+/*
+ * Copyright (c) 2025 Airbyte, Inc., all rights reserved.
+ */
+
 package io.airbyte.integrations.source.postgres.config
 
 import io.airbyte.cdk.command.CdcSourceConfiguration
--- airbyte-integrations/connectors/source-postgres/src/main/kotlin/io/airbyte/integrations/source/postgres/config/PostgresSourceConfigurationSpecification.kt
+++ airbyte-integrations/connectors/source-postgres/src/main/kotlin/io/airbyte/integrations/source/postgres/config/PostgresSourceConfigurationSpecification.kt
@@ -1,3 +1,7 @@
+/*
+ * Copyright (c) 2025 Airbyte, Inc., all rights reserved.
+ */
+
 package io.airbyte.integrations.source.postgres.config
 
 import com.fasterxml.jackson.annotation.JsonAnyGetter
