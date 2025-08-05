94ad36e465119c862261bb94eab14e5f35c8cb0e
Subodh Kant Chaturvedi <subodh1810@gmail.com>
2025-08-05 16:33:00 +0530

    chore: get azure blob storage on the latest cdk (#64487)
    Co-authored-by: Maxime Carbonneau-Leclerc <3360483+maxi297@users.noreply.github.com>

--- airbyte-integrations/connectors/destination-azure-blob-storage/src/test-integration/kotlin/io/airbyte/integrations/destination/azure_blob_storage/AzureBlobStorageWriteTest.kt
+++ airbyte-integrations/connectors/destination-azure-blob-storage/src/test-integration/kotlin/io/airbyte/integrations/destination/azure_blob_storage/AzureBlobStorageWriteTest.kt
@@ -7,18 +7,21 @@ package io.airbyte.integrations.destination.azure_blob_storage
 import io.airbyte.cdk.load.command.object_storage.CSVFormatSpecification
 import io.airbyte.cdk.load.command.object_storage.FlatteningSpecificationProvider.Flattening
 import io.airbyte.cdk.load.command.object_storage.JsonFormatSpecification
+import io.airbyte.cdk.load.config.DataChannelFormat
+import io.airbyte.cdk.load.config.DataChannelMedium
 import io.airbyte.cdk.load.test.util.NoopDestinationCleaner
 import io.airbyte.cdk.load.test.util.UncoercedExpectedRecordMapper
 import io.airbyte.cdk.load.write.BasicFunctionalityIntegrationTest
 import io.airbyte.cdk.load.write.SchematizedNestedValueBehavior
 import io.airbyte.cdk.load.write.UnionBehavior
+import io.airbyte.cdk.load.write.UnknownTypesBehavior
 import io.airbyte.cdk.load.write.Untyped
-import org.junit.jupiter.api.Test
 
 abstract class AzureBlobStorageWriteTest(
     configContents: String,
-    preserveUndeclaredFields: Boolean = true,
     nullEqualsUnset: Boolean,
+    dataChannelMedium: DataChannelMedium = DataChannelMedium.STDIO,
+    dataChannelFormat: DataChannelFormat = DataChannelFormat.JSONL
 ) :
     BasicFunctionalityIntegrationTest(
         configContents = configContents,
@@ -32,11 +35,14 @@ abstract class AzureBlobStorageWriteTest(
         schematizedObjectBehavior = SchematizedNestedValueBehavior.PASS_THROUGH,
         schematizedArrayBehavior = SchematizedNestedValueBehavior.PASS_THROUGH,
         unionBehavior = UnionBehavior.PASS_THROUGH,
-        preserveUndeclaredFields = preserveUndeclaredFields,
         supportFileTransfer = true,
         commitDataIncrementally = true,
         allTypesBehavior = Untyped,
         nullEqualsUnset = nullEqualsUnset,
+        unknownTypesBehavior = UnknownTypesBehavior.PASS_THROUGH,
+        mismatchedTypesUnrepresentable = false,
+        dataChannelMedium = dataChannelMedium,
+        dataChannelFormat = dataChannelFormat,
     )
 
 class AzureBlobStorageCsvNoFlatteningWriteTest :
@@ -45,24 +51,13 @@ class AzureBlobStorageCsvNoFlatteningWriteTest :
             CSVFormatSpecification(flattening = Flattening.NO_FLATTENING)
         ),
         nullEqualsUnset = true,
-    ) {
-    @Test
-    override fun testBasicWriteFile() {
-        super.testBasicWriteFile()
-    }
-
-    @Test
-    override fun testBasicWrite() {
-        super.testBasicWrite()
-    }
-}
+    )
 
 class AzureBlobStorageCsvWithFlatteningWriteTest :
     AzureBlobStorageWriteTest(
         AzureBlobStorageTestUtil.getAccountKeyConfig(
             CSVFormatSpecification(flattening = Flattening.ROOT_LEVEL_FLATTENING)
         ),
-        preserveUndeclaredFields = false,
         nullEqualsUnset = true,
     )
 
@@ -87,6 +82,5 @@ class AzureBlobStorageSasCsvWithFlatteningWriteTest :
         AzureBlobStorageTestUtil.getSasConfig(
             CSVFormatSpecification(flattening = Flattening.ROOT_LEVEL_FLATTENING)
         ),
-        preserveUndeclaredFields = false,
         nullEqualsUnset = true,
     )
