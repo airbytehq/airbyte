e708affbc2adcf03f1eed4ab8e63c45efe52e108
Benoit Moriceau <benoit@airbyte.io>
2025-08-05 10:03:45 -0700

    Cdk data flow spike (#64163)
    Co-authored-by: tryangul <ryan.broughan@gmail.com>
    Co-authored-by: benoit.m <benoit.m@benoit.m--MacBook-Pro---GMG2HJ9HJQ>

---
+++ airbyte-cdk/bulk/core/load/src/main/kotlin/io/airbyte/cdk/load/dataflow/DataFlowStageIO.kt
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2025 Airbyte, Inc., all rights reserved.
+ */
+
+package io.airbyte.cdk.load.dataflow
+
+import io.airbyte.cdk.load.dataflow.aggregate.Aggregate
+import io.airbyte.cdk.load.dataflow.state.StateHistogram
+import io.airbyte.cdk.load.dataflow.transform.RecordDTO
+import io.airbyte.cdk.load.message.DestinationRecordRaw
+
+data class DataFlowStageIO(
+    var raw: DestinationRecordRaw? = null,
+    var munged: RecordDTO? = null,
+    var aggregate: Aggregate? = null,
+    var stateHist: StateHistogram? = null,
+)
