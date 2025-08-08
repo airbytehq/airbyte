4c1ec8b2b80b42265dd2c28261074cf36e4c87ad
Ryan Br... <ryan.broughan@gmail.com>
2025-08-07 19:48:00 -0700

    Rbroughan/dataflow state (#64514)

--- airbyte-cdk/bulk/core/load/src/main/kotlin/io/airbyte/cdk/load/dataflow/aggregate/Aggregate.kt
+++ airbyte-cdk/bulk/core/load/src/main/kotlin/io/airbyte/cdk/load/dataflow/aggregate/Aggregate.kt
@@ -4,16 +4,13 @@
 
 package io.airbyte.cdk.load.dataflow.aggregate
 
-import io.airbyte.cdk.load.dataflow.state.StateHistogram
 import io.airbyte.cdk.load.dataflow.transform.RecordDTO
 
 interface Aggregate {
 
     fun accept(record: RecordDTO)
 
-    suspend fun flush() // Maybe we want some sort of generalizable result
-
-    fun getStateHistogram(): StateHistogram
+    suspend fun flush()
 }
 
 interface AggregateFactory {
