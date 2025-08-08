b8866785aa553c81d145a6dc4eceae4019a38e76
Patrick Nilan <nilan.patrick@gmail.com>
2025-08-08 09:19:32 -0700

    feat(metadata_service): creates `publish_stale_metadata_report` operation (#64502)

--- airbyte-ci/connectors/metadata_service/lib/tests/conftest.py
+++ airbyte-ci/connectors/metadata_service/lib/tests/conftest.py
@@ -1,7 +1,25 @@
 #
-# Copyright (c) 2023 Airbyte, Inc., all rights reserved.
+# Copyright (c) 2025 Airbyte, Inc., all rights reserved.
 #
+
+import pytest
 
 pytest_plugins = [
     "tests.fixtures",
 ]
+
+
+def pytest_addoption(parser):
+    parser.addoption("--skipslow", action="store_true", default=False, help="skip slow tests")
+
+
+def pytest_configure(config):
+    config.addinivalue_line("markers", "slow: mark test as slow to run")
+
+
+def pytest_collection_modifyitems(config, items):
+    if config.getoption("--skipslow"):
+        skip_slow = pytest.mark.skip(reason="--skipslow option has been provided and this test is marked as slow")
+        for item in items:
+            if "slow" in item.keywords:
+                item.add_marker(skip_slow)
