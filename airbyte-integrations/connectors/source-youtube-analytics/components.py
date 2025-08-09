09cb4e7e91bb495fee0df35e0f7eddbd45d57301
Tope Folorunso <folorunsotemitope10@gmail.com>
2025-08-09 22:04:27 +0100

    add comments for custom components

--- airbyte-integrations/connectors/source-youtube-analytics/components.py
+++ airbyte-integrations/connectors/source-youtube-analytics/components.py
@@ -3,22 +3,27 @@
 #
 
 from dataclasses import dataclass
-from typing import Any, Iterable, Mapping, MutableMapping, Optional, Union
+from typing import Any, Iterable, Mapping, MutableMapping, Optional
 
 import requests
 
 from airbyte_cdk.sources.declarative.extractors.record_extractor import RecordExtractor
-from airbyte_cdk.sources.declarative.requesters.error_handlers import DefaultErrorHandler
 from airbyte_cdk.sources.declarative.requesters.http_requester import HttpRequester
 from airbyte_cdk.sources.declarative.requesters.request_options.interpolated_request_options_provider import (
     InterpolatedRequestOptionsProvider,
     RequestInput,
 )
-from airbyte_cdk.sources.streams.http.error_handlers import ErrorResolution, ResponseAction
 
 
 @dataclass
 class CreationRequester(HttpRequester):
+
+    # CreationRequester checks if the job exists first before attempting to create the job
+    # This is because If the job exists, creating the job returns an error
+    # CreationRequester achieves this by first pulling a list of all jobs. It then checks if the job is in the list.
+    # If the job is not in the list, CreationRequester goes ahead to create the job as configured.
+    # Essentially, CreationRequester sends two requests in the worst case.
+
     request_body_json: Optional[RequestInput] = None
 
     def __post_init__(self, parameters: Mapping[str, Any]) -> None:
@@ -48,6 +53,12 @@ class CreationRequester(HttpRequester):
 
 @dataclass
 class PollingRequester(HttpRequester):
+
+    # PollingRequester gets the job id first before pulling the job's reports
+    # It achieves this by first pulling a list of all jobs, extracting the job and getting its id
+    # PollingRequester then uses the id to pull the job's reports
+    # Essentially, PollingRequester sends two requests in cases.
+
     def send_request(self, **kwargs):
         stream_name = self.name.split(" - ")[-1]
 
@@ -60,13 +71,18 @@ class PollingRequester(HttpRequester):
             http_method=self.get_method().value,
             url=self._join_url(self.get_url_base(), f"jobs/{job_id}/reports"),
             request_kwargs={"stream": self.stream_response},
-            params={"createdAfter": self.start_time} if self.start_time else {},
+            # params={"createdAfter": self.start_time} if self.start_time else {},
         )
         return reports_response
 
 
 @dataclass
 class StatusExtractor(RecordExtractor):
+
+    # The API doesn't explicitly state the status of the job's reports creation.
+    # Hence, StatusExtractor improvises by returning "running" if the reports are not available i.e the reports list is empty
+    # It then returns "completed" if the reports list contains items.
+
     def extract_records(self, response: requests.Response) -> Iterable[MutableMapping[Any, Any]]:
         reports = response.json().get("reports", [])
         if not reports:
@@ -77,7 +93,11 @@ class StatusExtractor(RecordExtractor):
 
 @dataclass
 class DownloadTargetExtractor(RecordExtractor):
+
+    # There are usually more than one report url for a job returned as items in a json
+    # DownloadTargetExtractor extracts these urls as a list
+
     def extract_records(self, response: requests.Response) -> Iterable[MutableMapping[Any, Any]]:
-        reports = response.json().get("reports", [])
+        reports = response.json().get("reports", {})
         urls = list(map(lambda report: report.get("downloadUrl", ""), reports))
         return urls
