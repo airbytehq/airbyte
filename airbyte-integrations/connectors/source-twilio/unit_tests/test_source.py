d1ab52fe2b8c96793709f48ac479ddb8c1085500
Anatolii Yatsuk <tolikyatsuk@gmail.com>
2025-08-11 12:16:35 +0300

    Update CDK to v6

--- airbyte-integrations/connectors/source-twilio/unit_tests/test_source.py
+++ airbyte-integrations/connectors/source-twilio/unit_tests/test_source.py
@@ -37,6 +37,7 @@ from source_twilio.streams import (
     Users,
     VerifyServices,
 )
+from airbyte_cdk.sources.streams.http.exceptions import DefaultBackoffException
 
 
 @pytest.fixture
@@ -64,9 +65,9 @@ TEST_INSTANCE = SourceTwilio()
             "Unable to connect to Twilio API with the provided credentials - TimeoutError('Socket timed out')",
         ),
         (
-            requests.exceptions.HTTPError("401 Client Error: Unauthorized for url: https://api.twilio.com/"),
+            DefaultBackoffException(None, None, "Unexpected exception in error handler: 401 Client Error: Unauthorized for url: https://api.twilio.com/"),
             "Unable to connect to Twilio API with the provided credentials - "
-            "HTTPError('401 Client Error: Unauthorized for url: https://api.twilio.com/')",
+            "DefaultBackoffException('Unexpected exception in error handler: Unexpected exception in error handler: 401 Client Error: Unauthorized for url: https://api.twilio.com/')",
         ),
     ),
 )
