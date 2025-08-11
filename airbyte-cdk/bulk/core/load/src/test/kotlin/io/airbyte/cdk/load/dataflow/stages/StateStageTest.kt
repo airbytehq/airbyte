9c418bd464dcf335aa16001c0add628cecc7f3a1
Benoit Moriceau <benoit@airbyte.io>
2025-08-11 15:40:14 -0700

    Add tests to the stage steps

---
+++ airbyte-cdk/bulk/core/load/src/test/kotlin/io/airbyte/cdk/load/dataflow/stages/StateStageTest.kt
@@ -0,0 +1,49 @@
+/*
+ * Copyright (c) 2025 Airbyte, Inc., all rights reserved.
+ */
+
+package io.airbyte.cdk.load.dataflow.stages
+
+import io.airbyte.cdk.load.dataflow.DataFlowStageIO
+import io.airbyte.cdk.load.dataflow.state.PartitionHistogram
+import io.airbyte.cdk.load.dataflow.state.StateWatermarkStore
+import io.mockk.justRun
+import io.mockk.mockk
+import io.mockk.verify
+import kotlinx.coroutines.test.runTest
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import kotlin.test.assertFailsWith
+import kotlin.test.assertSame
+
+class StateStageTest {
+    private lateinit var stateStore: StateWatermarkStore
+    private lateinit var stateStage: StateStage
+
+    @BeforeEach
+    fun setup() {
+        stateStore = mockk<StateWatermarkStore>(relaxed = true)
+        stateStage = StateStage(stateStore)
+    }
+
+    @Test
+    fun `test apply happy path`() = runTest {
+        // Arrange
+        val histogram = mockk<PartitionHistogram>()
+        val input = DataFlowStageIO(partitionHistogram = histogram)
+
+        // Act
+        val result = stateStage.apply(input)
+
+        // Assert
+        verify(exactly = 1) { stateStore.acceptAggregateCounts(histogram) }
+        assertSame(input, result, "The output should be the same as the input object")
+    }
+
+    @Test
+    fun `test apply with null partition histogram throws exception`() = runTest {
+        val input = DataFlowStageIO(partitionHistogram = null)
+        assertFailsWith<NullPointerException> { stateStage.apply(input) }
+        verify(exactly = 0) { stateStore.acceptAggregateCounts(any()) }
+    }
+}
