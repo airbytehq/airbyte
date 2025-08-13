0a83be59bd00397b1c312eee115dddb786de89a0
Rodi Reich Zilberman <867491+rodireich@users.noreply.github.com>
2025-08-13 16:32:13 -0700

    config

--- airbyte-integrations/connectors/source-postgres/src/main/kotlin/io/airbyte/integrations/source/postgres/config/PostgresSourceConfiguration.kt
+++ airbyte-integrations/connectors/source-postgres/src/main/kotlin/io/airbyte/integrations/source/postgres/config/PostgresSourceConfiguration.kt
@@ -4,14 +4,25 @@
 
 package io.airbyte.integrations.source.postgres.config
 
+import io.airbyte.cdk.ConfigErrorException
 import io.airbyte.cdk.command.CdcSourceConfiguration
+import io.airbyte.cdk.command.ConfigurationSpecificationSupplier
+import io.airbyte.cdk.command.FeatureFlag
 import io.airbyte.cdk.command.JdbcSourceConfiguration
 import io.airbyte.cdk.command.SourceConfiguration
 import io.airbyte.cdk.command.SourceConfigurationFactory
+import io.airbyte.cdk.output.DataChannelMedium.STDIO
+import io.airbyte.cdk.output.sockets.DATA_CHANNEL_PROPERTY_PREFIX
 import io.airbyte.cdk.ssh.SshConnectionOptions
+import io.airbyte.cdk.ssh.SshNoTunnelMethod
 import io.airbyte.cdk.ssh.SshTunnelMethodConfiguration
 import io.github.oshai.kotlinlogging.KotlinLogging
+import io.micronaut.context.annotation.Factory
+import io.micronaut.context.annotation.Value
+import jakarta.inject.Inject
 import jakarta.inject.Singleton
+import java.net.URLDecoder
+import java.nio.charset.StandardCharsets
 import java.time.Duration
 import java.util.*
 
@@ -21,7 +32,7 @@ private val log = KotlinLogging.logger {}
 data class PostgresSourceConfiguration(
     override val realHost: String,
     override val realPort: Int,
-    override val sshTunnel: SshTunnelMethodConfiguration,
+    override val sshTunnel: SshTunnelMethodConfiguration?,
     override val sshConnectionOptions: SshConnectionOptions,
     override val jdbcUrlFmt: String,
     // TODO: Initialize parameters with prepareThreshold=0 to mitigate pgbouncer errors
@@ -29,17 +40,27 @@ data class PostgresSourceConfiguration(
     // mapOf(PREPARE_THRESHOLD, "0", TCP_KEEP_ALIVE, "true")
     override val jdbcProperties: Map<String, String>,
     override val namespaces: Set<String>,
-    val incremental: IncrementalConfiguration,
+    val incrementalConfiguration: IncrementalConfiguration,
     override val maxConcurrency: Int,
     override val resourceAcquisitionHeartbeat: Duration = Duration.ofMillis(100L),
     override val checkpointTargetInterval: Duration,
     override val checkPrivileges: Boolean,
     override val debeziumHeartbeatInterval: Duration = Duration.ofSeconds(10),
 ) : JdbcSourceConfiguration, CdcSourceConfiguration {
-    val cdc: CdcIncrementalConfiguration? = incremental as? CdcIncrementalConfiguration
+    val cdc: CdcIncrementalConfiguration? = incrementalConfiguration as? CdcIncrementalConfiguration
 
     override val global: Boolean = cdc != null
     override val maxSnapshotReadDuration: Duration? = cdc?.initialLoadTimeout
+
+    /** Required to inject [PostgresSourceConfiguration] directly. */
+    @Factory
+    private class MicronautFactory {
+        @Singleton
+        fun postgresSourceConfig(
+            factory: SourceConfigurationFactory<PostgresSourceConfigurationSpecification, PostgresSourceConfiguration>,
+            supplier: ConfigurationSpecificationSupplier<PostgresSourceConfigurationSpecification>
+        ) = factory.make(supplier.get())
+    }
 }
 
 sealed interface IncrementalConfiguration
@@ -58,12 +79,114 @@ enum class InvalidCdcCursorPositionBehavior {
 }
 
 @Singleton
-class PostgresSourceConfigurationFactory :
-    SourceConfigurationFactory<
+class PostgresSourceConfigurationFactory
+@Inject
+constructor(
+    val featureFlags: Set<FeatureFlag>,
+    @Value("\${${DATA_CHANNEL_PROPERTY_PREFIX}.medium}") val dataChannelMedium: String = STDIO.name,
+    @Value("\${${DATA_CHANNEL_PROPERTY_PREFIX}.socket-paths}")
+    val socketPaths: List<String> = emptyList(),
+) :SourceConfigurationFactory<
         PostgresSourceConfigurationSpecification, PostgresSourceConfiguration> {
     override fun makeWithoutExceptionHandling(
         pojo: PostgresSourceConfigurationSpecification,
     ): PostgresSourceConfiguration {
-        throw NotImplementedError()
+        val realHost: String = pojo.host
+        val realPort: Int = pojo.port
+        val jdbcProperties = mutableMapOf<String, String>()
+        jdbcProperties["user"] = pojo.username
+        pojo.password?.let { jdbcProperties["password"] = it }
+
+        // Parse URL parameters.
+        val pattern = "^([^=]+)=(.*)$".toRegex()
+        for (pair in (pojo.jdbcUrlParams ?: "").trim().split("&".toRegex())) {
+            if (pair.isBlank()) {
+                continue
+            }
+            val result: MatchResult? = pattern.matchEntire(pair)
+            if (result == null) {
+                log.warn { "ignoring invalid JDBC URL param '$pair'" }
+            } else {
+                val key: String = result.groupValues[1].trim()
+                val urlEncodedValue: String = result.groupValues[2].trim()
+                jdbcProperties[key] = URLDecoder.decode(urlEncodedValue, StandardCharsets.UTF_8)
+            }
+        }
+
+        // Configure SSH tunneling.
+        val sshTunnel: SshTunnelMethodConfiguration? = pojo.getTunnelMethodValue()
+        val sshOpts: SshConnectionOptions =
+            SshConnectionOptions.fromAdditionalProperties(pojo.getAdditionalProperties())
+
+
+        /*// Configure SSL encryption.
+        if (
+            pojo.getEncryptionValue() is EncryptionPreferred &&
+            sshTunnel is SshNoTunnelMethod &&
+            featureFlags.contains(FeatureFlag.AIRBYTE_CLOUD_DEPLOYMENT)
+        ) {
+            throw ConfigErrorException(
+                "Connection from Airbyte Cloud requires SSL encryption or an SSH tunnel."
+            )
+        }
+        val sslJdbcProperties: Map<String, String> = fromEncryptionSpec(pojo.getEncryptionValue()!!)
+        jdbcProperties.putAll(sslJdbcProperties)
+        io.airbyte.integrations.source.mysql.log.info { "SSL mode: ${sslJdbcProperties["sslMode"]}" }*/
+
+        // Configure cursor.
+        val incremental: IncrementalConfiguration = fromIncrementalSpec(pojo.getIncrementalConfigurationSpecificationValue())
+
+        // Build JDBC URL.
+        val address = "%s:%d"
+        val jdbcUrlFmt = "jdbc:mysql://${address}"
+        jdbcProperties["useCursorFetch"] = "true"
+        jdbcProperties["sessionVariables"] = "autocommit=0"
+
+        // Internal configuration settings.
+        val checkpointTargetInterval: Duration =
+            Duration.ofSeconds(pojo.checkpointTargetIntervalSeconds?.toLong() ?: 0)
+        if (!checkpointTargetInterval.isPositive) {
+            throw ConfigErrorException("Checkpoint Target Interval should be positive")
+        }
+
+        val concurrency: Int = pojo.concurrency ?: 1
+
+        return PostgresSourceConfiguration(
+            realHost = realHost,
+            realPort = realPort,
+            sshTunnel = sshTunnel,
+            sshConnectionOptions = sshOpts,
+            jdbcUrlFmt = jdbcUrlFmt,
+            jdbcProperties = jdbcProperties,
+            namespaces = pojo.schemas?.toSet() ?: emptySet(),
+            incrementalConfiguration = incremental,
+            maxConcurrency = concurrency,
+            checkpointTargetInterval = checkpointTargetInterval,
+            checkPrivileges = pojo.checkPrivileges ?: true,
+        )
     }
+
+    private fun fromIncrementalSpec(
+        incrementalSpec: IncrementalConfigurationSpecification
+    ): IncrementalConfiguration =
+        when (incrementalSpec) {
+            UserDefinedCursorConfigurationSpecification -> UserDefinedCursorIncrementalConfiguration
+            is CdcCursorConfigurationSpecification -> {
+                val initialLoadTimeout: Duration =
+                    Duration.ofHours(incrementalSpec.initialLoadTimeoutHours!!.toLong())
+                val invalidCdcCursorPositionBehavior: InvalidCdcCursorPositionBehavior =
+                    if (incrementalSpec.invalidCdcCursorPositionBehavior == "Fail sync") {
+                        InvalidCdcCursorPositionBehavior.FAIL_SYNC
+                    } else {
+                        InvalidCdcCursorPositionBehavior.RESET_SYNC
+                    }
+                val shutdownTimeout: Duration =
+                    Duration.ofSeconds(incrementalSpec.debeziumShutdownTimeoutSeconds!!.toLong())
+                CdcIncrementalConfiguration(
+                    initialLoadTimeout,
+                    invalidCdcCursorPositionBehavior,
+                    shutdownTimeout,
+                )
+            }
+        }
 }
