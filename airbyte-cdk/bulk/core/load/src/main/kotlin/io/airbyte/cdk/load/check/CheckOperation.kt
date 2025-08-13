3a212f1dd06e5dfa567a8f9636c68d5c4318e79c
Benoit Moriceau <benoit@airbyte.io>
2025-08-13 09:30:01 -0700

    Test

--- airbyte-cdk/bulk/core/load/src/main/kotlin/io/airbyte/cdk/load/check/CheckOperation.kt
+++ airbyte-cdk/bulk/core/load/src/main/kotlin/io/airbyte/cdk/load/check/CheckOperation.kt
@@ -22,6 +22,7 @@ private val logger = KotlinLogging.logger {}
 @Singleton
 @Requires(property = Operation.PROPERTY, value = "check")
 @Requires(env = ["destination"])
+// test
 class CheckOperation<T : ConfigurationSpecification, C : DestinationConfiguration>(
     val configJsonObjectSupplier: ConfigurationSpecificationSupplier<T>,
     val configFactory: DestinationConfigurationFactory<T, C>,
