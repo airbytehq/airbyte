3f8a610c53fc24bd17c31f27e12e971f23588629
Benoit Moriceau <benoit@airbyte.io>
2025-08-13 09:34:38 -0700

    All good

--- airbyte-cdk/bulk/core/load/src/test/kotlin/io/airbyte/cdk/load/file/ProtobufDataChannelReaderTest.kt
+++ airbyte-cdk/bulk/core/load/src/test/kotlin/io/airbyte/cdk/load/file/ProtobufDataChannelReaderTest.kt
@@ -17,7 +17,6 @@ import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 
-// test
 class ProtobufDataChannelReaderTest {
     val factory: DestinationMessageFactory = mockk(relaxed = true)
 
