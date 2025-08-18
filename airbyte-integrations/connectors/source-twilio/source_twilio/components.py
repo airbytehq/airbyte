1718241eb08ca7af41db2d64bed5fbc2b40157b8
Anatolii Yatsuk <tolikyatsuk@gmail.com>
2025-08-18 20:04:06 +0300

    Migrate incremental streams

---
+++ airbyte-integrations/connectors/source-twilio/source_twilio/components.py
@@ -0,0 +1,75 @@
+#
+# Copyright (c) 2025 Airbyte, Inc., all rights reserved.
+#
+
+from typing import Any, Dict, Iterable, List, Mapping, MutableMapping, Optional, Union
+from airbyte_cdk.sources.declarative.migrations.state_migration import StateMigration
+from airbyte_cdk.sources.utils.transform import TransformConfig, TypeTransformer
+from airbyte_cdk.sources.types import Config
+from airbyte_cdk.utils.datetime_helpers import ab_datetime_parse, ab_datetime_format
+from datetime import datetime, timezone
+
+from test_substream_partition_router import parent_slices
+
+
+class TwilioDateTimeTypeTransformer(TypeTransformer):
+    """
+    Twilio API returns datetime in two formats:
+    - RFC2822, like "Fri, 11 Dec 2020 04:28:40 +0000";
+    - ISO8601, like "2020-12-11T04:29:09Z".
+    We only transform RFC2822 values (detected by the presence of ", ").
+    """
+
+    def __init__(self, *args, **kwargs):
+        # apply this transformer during schema normalization phase(s)
+        config = TransformConfig.DefaultSchemaNormalization | TransformConfig.CustomSchemaNormalization
+        super().__init__(config)
+        # register our custom transform
+        self.registerCustomTransform(self.get_transform_function())
+
+    @staticmethod
+    def get_transform_function():
+        def custom_transform_function(original_value: Any, field_schema: Mapping[str, Any]) -> Any:
+            if original_value and field_schema.get("format") == "date-time":
+                try:
+                    dt = datetime.strptime(original_value, "%a, %d %b %Y %H:%M:%S %z").astimezone(timezone.utc)
+                    return ab_datetime_format(dt, "%Y-%m-%dT%H:%M:%SZ")
+                except ValueError:
+                    pass
+            return original_value
+
+        return custom_transform_function
+
+class TwilioStateMigration(StateMigration):
+    def migrate(self, stream_state: Mapping[str, Any]) -> Mapping[str, Any]:
+        for state in stream_state.get("states", []):
+            state["partition"]["parent_slice"] = {}
+        return stream_state
+
+    def should_migrate(self, stream_state: Mapping[str, Any]) -> bool:
+        if stream_state and any("parent_slice" not in state["partition"] for state in stream_state.get("states", [])):
+            return True
+        return False
+
+class TwilioUsageRecordsStateMigration(StateMigration):
+    def migrate(self, stream_state: Mapping[str, Any]) -> Mapping[str, Any]:
+        for state in stream_state.get("states", []):
+            state["partition"] = {"account_sid": state["partition"]["account_sid"], "parent_slice": {}}
+        return stream_state
+
+    def should_migrate(self, stream_state: Mapping[str, Any]) -> bool:
+        if stream_state and any("parent_slice" not in state["partition"] for state in stream_state.get("states", [])):
+            return True
+        return False
+
+class TwilioMessageMediaStateMigration(StateMigration):
+    def migrate(self, stream_state: Mapping[str, Any]) -> Mapping[str, Any]:
+        for state in stream_state.get("states", []):
+            state["partition"] = {"subresource_uri": state["partition"]["subresource_uri"], "parent_slice":
+                {"subresource_uri": state["partition"]["subresource_uri"].split("Messages")[0] + "Messages.json", "parent_slice": {}}}
+        return stream_state
+
+    def should_migrate(self, stream_state: Mapping[str, Any]) -> bool:
+        if stream_state and any("parent_slice" not in state["partition"] for state in stream_state.get("states", [])):
+            return True
+        return False
