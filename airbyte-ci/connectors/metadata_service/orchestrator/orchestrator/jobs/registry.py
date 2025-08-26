0a0f2b4840c69597e6718d4d2583dbd8d21e976e
Edward Gao <edward.gao@airbyte.io>
2025-08-26 14:58:11 -0700

    undo orchestrator changes

--- airbyte-ci/connectors/metadata_service/orchestrator/orchestrator/jobs/registry.py
+++ airbyte-ci/connectors/metadata_service/orchestrator/orchestrator/jobs/registry.py
@@ -15,6 +15,9 @@ generate_oss_registry = define_asset_job(name="generate_oss_registry", selection
 cloud_registry_inclusive = AssetSelection.keys("persisted_cloud_registry", "specs_secrets_mask_yaml").upstream()
 generate_cloud_registry = define_asset_job(name="generate_cloud_registry", selection=cloud_registry_inclusive)
 
+registry_reports_inclusive = AssetSelection.keys("connector_registry_report").upstream()
+generate_registry_reports = define_asset_job(name="generate_registry_reports", selection=registry_reports_inclusive)
+
 registry_entry_inclusive = AssetSelection.keys("registry_entry").upstream()
 generate_registry_entry = define_asset_job(
     name="generate_registry_entry",
--- airbyte-ci/connectors/metadata_service/orchestrator/tests/test_registry.py
+++ airbyte-ci/connectors/metadata_service/orchestrator/tests/test_registry.py
@@ -20,6 +20,14 @@ from orchestrator.assets.registry_entry import (
     metadata_to_registry_entry,
     safe_parse_metadata_definition,
 )
+from orchestrator.assets.registry_report import (
+    all_destinations_dataframe,
+    all_sources_dataframe,
+    cloud_destinations_dataframe,
+    cloud_sources_dataframe,
+    oss_destinations_dataframe,
+    oss_sources_dataframe,
+)
 from orchestrator.models.metadata import LatestMetadataEntry, MetadataDefinition
 from orchestrator.utils.blob_helpers import yaml_blob_to_dict
 from pydantic import ValidationError
@@ -152,6 +160,47 @@ def test_get_connector_type_from_registry_entry(registry_entry, expected_type, e
         with pytest.raises(Exception) as e_info:
             get_connector_type_from_registry_entry(registry_entry)
         assert str(e_info.value) == "Could not determine connector type from registry entry"
+
+
+def test_merged_registry_dataframes(oss_registry_dict, cloud_registry_dict):
+    oss_registry_model = ConnectorRegistryV0.parse_obj(oss_registry_dict)
+    cloud_registry_model = ConnectorRegistryV0.parse_obj(cloud_registry_dict)
+
+    github_connector_folders = []
+    num_oss_destinations = len(oss_registry_dict["destinations"])
+    num_cloud_destinations = len(cloud_registry_dict["destinations"])
+
+    num_cloud_sources = len(cloud_registry_dict["sources"])
+    num_oss_sources = len(oss_registry_dict["sources"])
+
+    cloud_destinations_df = cloud_destinations_dataframe(cloud_registry_model).value
+    assert len(cloud_destinations_df) == num_cloud_destinations
+
+    cloud_sources_df = cloud_sources_dataframe(cloud_registry_model).value
+    assert len(cloud_sources_df) == num_cloud_sources
+
+    oss_destinations_df = oss_destinations_dataframe(oss_registry_model).value
+    assert len(oss_destinations_df) == num_oss_destinations
+
+    oss_sources_df = oss_sources_dataframe(oss_registry_model).value
+    assert len(oss_sources_df) == num_oss_sources
+
+    all_sources_df = all_sources_dataframe(cloud_sources_df, oss_sources_df, github_connector_folders)
+    all_destinations_df = all_destinations_dataframe(cloud_destinations_df, oss_destinations_df, github_connector_folders)
+
+    # assert that all_sources_df has a entry for each sourceDefinitionId in the cloud registry and oss registry
+    oss_source_definition_ids = set([source["sourceDefinitionId"] for source in oss_registry_dict["sources"]])
+    cloud_source_definition_ids = set([source["sourceDefinitionId"] for source in cloud_registry_dict["sources"]])
+    all_source_definition_ids = set(all_sources_df["definitionId"])
+
+    assert all_source_definition_ids == oss_source_definition_ids.union(cloud_source_definition_ids)
+
+    # assert that all_destinations_df has a entry for each sourceDefinitionId in the cloud registry and oss registry
+    oss_destination_definition_ids = set([destination["destinationDefinitionId"] for destination in oss_registry_dict["destinations"]])
+    cloud_destination_definition_ids = set([destination["destinationDefinitionId"] for destination in cloud_registry_dict["destinations"]])
+    all_destination_definition_ids = set(all_destinations_df["definitionId"])
+
+    assert all_destination_definition_ids == oss_destination_definition_ids.union(cloud_destination_definition_ids)
 
 
 @pytest.mark.parametrize(
