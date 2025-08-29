e387650c1407ec996e1836af55867556f7b7f3e1
tryangul <ryan.broughan@gmail.com>
2025-08-29 12:13:52 -0700

    Changelog and formatting.

--- airbyte-cdk/bulk/core/load/src/main/kotlin/io/airbyte/cdk/load/dataflow/DestinationLifecycle.kt
+++ airbyte-cdk/bulk/core/load/src/main/kotlin/io/airbyte/cdk/load/dataflow/DestinationLifecycle.kt
@@ -62,21 +62,22 @@ class DestinationLifecycle(
             )
 
         return runBlocking {
-            val result = destinationCatalog.streams
-                .map {
-                    async(initDispatcher) {
-                        log.info {
-                            "Starting stream loader for stream ${it.mappedDescriptor.namespace}:${it.mappedDescriptor.name}"
+            val result =
+                destinationCatalog.streams
+                    .map {
+                        async(initDispatcher) {
+                            log.info {
+                                "Starting stream loader for stream ${it.mappedDescriptor.namespace}:${it.mappedDescriptor.name}"
+                            }
+                            val streamLoader = destinationInitializer.createStreamLoader(it)
+                            streamLoader.start()
+                            log.info {
+                                "Stream loader for stream ${it.mappedDescriptor.namespace}:${it.mappedDescriptor.name} started"
+                            }
+                            streamLoader
                         }
-                        val streamLoader = destinationInitializer.createStreamLoader(it)
-                        streamLoader.start()
-                        log.info {
-                            "Stream loader for stream ${it.mappedDescriptor.namespace}:${it.mappedDescriptor.name} started"
-                        }
-                        streamLoader
                     }
-                }
-                .awaitAll()
+                    .awaitAll()
 
             return@runBlocking result
         }
