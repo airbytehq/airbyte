e7893bff0d749b4c3daccede1b2ed62f0a4d137f
Jose Pefaur <jose.pefaur@gmail.com>
2025-09-03 09:59:14 -0500

    fix yq filter

--- poe-tasks/upload-connector-metadata.sh
+++ poe-tasks/upload-connector-metadata.sh
@@ -34,7 +34,7 @@ syft_docker_image="anchore/syft:v1.6.0"
 sbom_extension="spdx.json"
 
 meta="${CONNECTORS_DIR}/${connector}/metadata.yaml"
-is_enterprise=$(yq -r '.data.ab_internal.isEnterprise' // false "$meta")
+is_enterprise=$(yq -r '.data.ab_internal.isEnterprise // false' "$meta")
 doc="$(connector_docs_path $connector $is_enterprise)"
 
 docker_repository=$(yq -r '.data.dockerRepository' "$meta")
