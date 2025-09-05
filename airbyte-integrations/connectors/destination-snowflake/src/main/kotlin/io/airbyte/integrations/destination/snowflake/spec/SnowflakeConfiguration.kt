018248be7df0dc9547ce4650a5752b8083df0936
Francis Genet <francis.genet@airbyte.io>
2025-09-05 10:18:30 -0700

    [Snowflake] Adding the configuration (#65951)

--- airbyte-integrations/connectors/destination-snowflake/src/main/kotlin/io/airbyte/integrations/destination/snowflake/spec/SnowflakeConfiguration.kt
+++ airbyte-integrations/connectors/destination-snowflake/src/main/kotlin/io/airbyte/integrations/destination/snowflake/spec/SnowflakeConfiguration.kt
@@ -6,6 +6,7 @@ package io.airbyte.integrations.destination.snowflake.spec
 
 import io.airbyte.cdk.load.command.DestinationConfiguration
 import io.airbyte.cdk.load.command.DestinationConfigurationFactory
+import io.airbyte.cdk.load.write.db.DbConstants
 import jakarta.inject.Singleton
 
 data class SnowflakeConfiguration(
@@ -14,20 +15,26 @@ data class SnowflakeConfiguration(
     val warehouse: String,
     val database: String,
     val schema: String,
-    val username: String
+    val username: String,
+    val authType: AuthTypeConfiguration,
+    val cdcDeletionMode: CdcDeletionMode,
+    val legacyRawTablesOnly: Boolean?,
+    val internalTableDataset: String,
+    val jdbcUrlParams: String?,
+    val retentionPeriodDays: Int,
+    val useMergeForUpsert: Boolean
 ) : DestinationConfiguration()
 
-// sealed interface CredentialConfiguration
-//
-// @JsonSchemaTitle("Key Pair Authentication Configuration")
-// @JsonSchemaDescription("Key pair specific configuration details for connecting to Snowflake")
-// data class PrivateKeyCredentialConfiguration(
-//    @JsonSchemaTitle("Private Key")
-//    @JsonPropertyDescription("RSA Private key to use for Snowflake connection. See the <a
-// href=\"https://docs.airbyte.com/integrations/destinations/snowflake\">docs</a> for more
-// information on how to obtain this key.")
-//    val privateKey: String
-// ): CredentialConfiguration
+sealed interface AuthTypeConfiguration
+
+data class KeyPairAuthConfiguration(
+    val privateKey: String,
+    val privateKeyPassword: String?,
+) : AuthTypeConfiguration
+
+data class UsernamePasswordAuthConfiguration(
+    val password: String,
+) : AuthTypeConfiguration
 
 @Singleton
 class SnowflakeConfigurationFactory :
@@ -35,13 +42,42 @@ class SnowflakeConfigurationFactory :
     override fun makeWithoutExceptionHandling(
         pojo: SnowflakeSpecification
     ): SnowflakeConfiguration {
+        val authTypeConfig =
+            when (pojo.authType) {
+                is KeyPairAuthSpecification -> {
+                    val keyPairAuthSpec = pojo.authType as KeyPairAuthSpecification
+                    KeyPairAuthConfiguration(
+                        keyPairAuthSpec.privateKey,
+                        keyPairAuthSpec.privateKeyPassword
+                    )
+                }
+                is UsernamePasswordAuthSpecification,
+                null -> {
+                    val usernamePasswordAuthSpec =
+                        pojo.authType as UsernamePasswordAuthSpecification
+                    UsernamePasswordAuthConfiguration(usernamePasswordAuthSpec.password)
+                }
+            }
+
         return SnowflakeConfiguration(
             host = pojo.host,
             role = pojo.role,
             warehouse = pojo.warehouse,
             database = pojo.database,
             schema = pojo.schema,
-            username = pojo.username
+            username = pojo.username,
+            authType = authTypeConfig,
+            cdcDeletionMode = pojo.cdcDeletionMode ?: CdcDeletionMode.HARD_DELETE,
+            legacyRawTablesOnly = pojo.legacyRawTablesOnly ?: false,
+            internalTableDataset =
+                if (pojo.internalTableDataset.isNullOrBlank()) {
+                    DbConstants.DEFAULT_RAW_TABLE_NAMESPACE
+                } else {
+                    pojo.internalTableDataset!!
+                },
+            jdbcUrlParams = pojo.jdbcUrlParams,
+            retentionPeriodDays = pojo.retentionPeriodDays ?: 1,
+            useMergeForUpsert = pojo.useMergeForUpsert ?: false,
         )
     }
 }
