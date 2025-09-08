6ecf7c42480a0f51b35f051d6acd58f0a49bef7c
Francis Genet <francis.genet@airbyte.io>
2025-09-08 09:30:16 -0700

    Update the bean factory

--- airbyte-integrations/connectors/destination-snowflake/src/main/kotlin/io/airbyte/integrations/destination/snowflake/SnowflakeBeansFactory.kt
+++ airbyte-integrations/connectors/destination-snowflake/src/main/kotlin/io/airbyte/integrations/destination/snowflake/SnowflakeBeansFactory.kt
@@ -4,8 +4,11 @@
 
 package io.airbyte.integrations.destination.snowflake
 
+import io.airbyte.cdk.command.ConfigurationSpecificationSupplier
 import io.airbyte.cdk.load.command.DestinationConfiguration
 import io.airbyte.integrations.destination.snowflake.spec.SnowflakeConfiguration
+import io.airbyte.integrations.destination.snowflake.spec.SnowflakeConfigurationFactory
+import io.airbyte.integrations.destination.snowflake.spec.SnowflakeSpecification
 import io.micronaut.context.annotation.Factory
 import jakarta.inject.Singleton
 import java.sql.Connection
@@ -13,10 +16,13 @@ import javax.sql.DataSource
 
 @Factory
 class SnowflakeBeansFactory {
-    @Singleton fun getConfig(config: DestinationConfiguration) = config as SnowflakeConfiguration
-
     @Singleton
-    fun getSnowflakeClient(snowflakeClient: AirbyteSnowflakeClient): Connection {
-        return snowflakeClient.getConnection()
+    fun snowflakeConfiguration(
+        configFactory: SnowflakeConfigurationFactory,
+        specFactory: ConfigurationSpecificationSupplier<SnowflakeSpecification>,
+    ): SnowflakeConfiguration {
+        val spec = specFactory.get()
+
+        return configFactory.makeWithoutExceptionHandling(spec)
     }
 }
