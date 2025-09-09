aeb498e86267d96189d8d49c7fac185865869234
Francis Genet <francis.genet@airbyte.io>
2025-09-09 09:29:49 -0700

    Introducing the SnowflakeDirectLoadDatabaseInitialStatusGatherer

---
+++ airbyte-integrations/connectors/destination-snowflake/src/main/kotlin/io/airbyte/integrations/destination/snowflake/config/SnowflakeDirectLoadDatabaseInitialStatusGatherer.kt
@@ -0,0 +1,16 @@
+package io.airbyte.integrations.destination.snowflake.config
+
+import io.airbyte.cdk.load.client.AirbyteClient
+import io.airbyte.cdk.load.orchestration.db.BaseDirectLoadInitialStatusGatherer
+import io.airbyte.cdk.load.orchestration.db.TempTableNameGenerator
+import jakarta.inject.Singleton
+
+@Singleton
+class SnowflakeDirectLoadDatabaseInitialStatusGatherer(
+    airbyteClient: AirbyteClient,
+    tempTableNameGenerator: TempTableNameGenerator,
+) :
+    BaseDirectLoadInitialStatusGatherer(
+        airbyteClient,
+        tempTableNameGenerator,
+    )
