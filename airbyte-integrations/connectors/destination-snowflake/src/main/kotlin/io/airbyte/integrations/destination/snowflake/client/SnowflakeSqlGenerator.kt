446b0e399e23c9cbf9571f352fd3111f143e59b9
Francis Genet <francis.genet@airbyte.io>
2025-09-09 07:13:20 -0700

    [Snowflake] Add basic SQLGenerator + update client (#66005)

--- airbyte-integrations/connectors/destination-snowflake/src/main/kotlin/io/airbyte/integrations/destination/snowflake/client/SnowflakeSqlGenerator.kt
+++
@@ -1,9 +0,0 @@
-/*
- * Copyright (c) 2025 Airbyte, Inc., all rights reserved.
- */
-
-package io.airbyte.integrations.destination.snowflake.client
-
-import jakarta.inject.Singleton
-
-@Singleton class SnowflakeSqlGenerator {}
