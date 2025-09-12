8acfa8fa0d55a7b2e6f67dea3cea5d405ddcc35f
Jonathan Pearlin <jonathan@airbyte.io>
2025-09-12 09:20:21 -0400

    Remove unused constant

--- airbyte-integrations/connectors/destination-snowflake/src/test-integration/kotlin/io/airbyte/integrations/destination/snowflake/SnowflakeTestUtils.kt
+++ airbyte-integrations/connectors/destination-snowflake/src/test-integration/kotlin/io/airbyte/integrations/destination/snowflake/SnowflakeTestUtils.kt
@@ -8,7 +8,6 @@ import java.nio.file.Files
 import java.nio.file.Path
 
 object SnowflakeTestUtils {
-    const val KEYPAIR_AUTH_CONFIG_PATH = "secrets/snowflake_keypair_auth.json"
     const val USERNAME_AUTH_CONFIG_PATH = "secrets/snowflake_username_auth.json"
 
     fun getConfig(configPath: String): String = Files.readString(Path.of(configPath))
