d91efb8e8fbe15138d678785df1211533d3db808
brianjlai <brian.j.lai@gmail.com>
2025-09-15 18:14:22 -0700

    better delete

--- airbyte-integrations/connectors/destination-hubspot/src/main/kotlin/HubSpotDiscoverer.kt
+++ airbyte-integrations/connectors/destination-hubspot/src/main/kotlin/HubSpotDiscoverer.kt
@@ -1,6 +1,7 @@
 /*
  * Copyright (c) 2024 Airbyte, Inc., all rights reserved.
  */
+
 package io.airbyte.integrations.destination.hubspot
 
 import io.airbyte.cdk.load.command.DestinationDiscoverCatalog
