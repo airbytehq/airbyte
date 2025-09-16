0bdc47a8f570686123b7fd237322ba80a78e52c4
Jonathan Pearlin <jonathan@airbyte.io>
2025-09-16 16:19:06 -0400

    feat: convert values to snowflake values

--- airbyte-integrations/connectors/destination-snowflake/src/test/kotlin/io/airbyte/integrations/destination/snowflake/write/transform/SnowflakeValueCoercerTest.kt
+++ airbyte-integrations/connectors/destination-snowflake/src/test/kotlin/io/airbyte/integrations/destination/snowflake/write/transform/SnowflakeValueCoercerTest.kt
@@ -4,25 +4,181 @@
 
 package io.airbyte.integrations.destination.snowflake.write.transform
 
+import io.airbyte.cdk.load.data.AirbyteValue
+import io.airbyte.cdk.load.data.ArrayType
+import io.airbyte.cdk.load.data.ArrayValue
 import io.airbyte.cdk.load.data.EnrichedAirbyteValue
+import io.airbyte.cdk.load.data.FieldType
+import io.airbyte.cdk.load.data.IntegerType
+import io.airbyte.cdk.load.data.IntegerValue
+import io.airbyte.cdk.load.data.NullValue
+import io.airbyte.cdk.load.data.NumberType
+import io.airbyte.cdk.load.data.NumberValue
+import io.airbyte.cdk.load.data.ObjectType
+import io.airbyte.cdk.load.data.ObjectValue
+import io.airbyte.cdk.load.data.StringType
+import io.airbyte.cdk.load.data.StringValue
+import io.airbyte.protocol.models.v0.AirbyteRecordMessageMetaChange
+import io.airbyte.protocol.models.v0.AirbyteRecordMessageMetaChange.Change
 import io.mockk.mockk
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
 
 internal class SnowflakeValueCoercerTest {
+
+    private lateinit var coercer: SnowflakeValueCoercer
+
+    @BeforeEach
+    fun setUp() {
+        coercer = SnowflakeValueCoercer()
+    }
 
     @Test
     fun testMap() {
         val airbyteValue = mockk<EnrichedAirbyteValue>()
-        val coercer = SnowflakeValueCoercer()
         val result = coercer.map(airbyteValue)
         assertEquals(airbyteValue, result)
     }
 
     @Test
-    fun testValidate() {
-        val airbyteValue = mockk<EnrichedAirbyteValue>()
-        val coercer = SnowflakeValueCoercer()
+    fun testValidateValidArray() {
+        val arrayValue = ArrayValue(listOf(IntegerValue(1), IntegerValue(2), IntegerValue(3)))
+        val airbyteValue =
+            EnrichedAirbyteValue(
+                abValue = arrayValue,
+                type = ArrayType(FieldType(IntegerType, false)),
+                name = "name",
+                changes = mutableListOf(),
+                airbyteMetaField = null,
+            )
+        val result = coercer.validate(airbyteValue)
+        assertEquals(airbyteValue, result)
+    }
+
+    @Test
+    fun testValidateValidObject() {
+        val values =
+            LinkedHashMap<String, AirbyteValue>().apply {
+                put("foo", IntegerValue(1))
+                put("bar", IntegerValue(2))
+            }
+        val objectValue = ObjectValue(values)
+        val properties =
+            LinkedHashMap<String, FieldType>().apply {
+                put("foo", FieldType(IntegerType, false))
+                put("bar", FieldType(IntegerType, false))
+            }
+        val airbyteValue =
+            EnrichedAirbyteValue(
+                abValue = objectValue,
+                type =
+                    ObjectType(
+                        properties = properties,
+                        additionalProperties = false,
+                        required = emptyList()
+                    ),
+                name = "name",
+                changes = mutableListOf(),
+                airbyteMetaField = null,
+            )
+
+        val result = coercer.validate(airbyteValue)
+        assertEquals(airbyteValue, result)
+    }
+
+    @Test
+    fun testValidInteger() {
+        val integerValue = IntegerValue(10000.toBigInteger())
+        val airbyteValue =
+            EnrichedAirbyteValue(
+                abValue = integerValue,
+                type = IntegerType,
+                name = "name",
+                changes = mutableListOf(),
+                airbyteMetaField = null,
+            )
+
+        val result = coercer.validate(airbyteValue)
+        assertEquals(airbyteValue, result)
+    }
+
+    @Test
+    fun testInvalidInteger() {
+        val integerValue =
+            IntegerValue("1${"0".repeat(INTEGER_PRECISION_LIMIT + 1)}".toBigInteger())
+        val airbyteValue =
+            EnrichedAirbyteValue(
+                abValue = integerValue,
+                type = IntegerType,
+                name = "name",
+                changes = mutableListOf(),
+                airbyteMetaField = null,
+            )
+
+        val result = coercer.validate(airbyteValue)
+        assertEquals(NullValue, result.abValue)
+        assertEquals(1, result.changes.size)
+        assertEquals(Change.NULLED, result.changes.first().change)
+        assertEquals(
+            AirbyteRecordMessageMetaChange.Reason.DESTINATION_FIELD_SIZE_LIMITATION,
+            result.changes.first().reason
+        )
+    }
+
+    @Test
+    fun testValidNumber() {
+        val numberValue = NumberValue(10000.123.toBigDecimal())
+        val airbyteValue =
+            EnrichedAirbyteValue(
+                abValue = numberValue,
+                type = NumberType,
+                name = "name",
+                changes = mutableListOf(),
+                airbyteMetaField = null,
+            )
+
+        val result = coercer.validate(airbyteValue)
+        assertEquals(airbyteValue, result)
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = ["-3.4028235E38", "${Float.MAX_VALUE}"])
+    fun testInvalidNumber(value: Float) {
+        val numberValue = NumberValue(value.toBigDecimal())
+        val airbyteValue =
+            EnrichedAirbyteValue(
+                abValue = numberValue,
+                type = NumberType,
+                name = "name",
+                changes = mutableListOf(),
+                airbyteMetaField = null,
+            )
+
+        val result = coercer.validate(airbyteValue)
+        assertEquals(NullValue, result.abValue)
+        assertEquals(1, result.changes.size)
+        assertEquals(Change.NULLED, result.changes.first().change)
+        assertEquals(
+            AirbyteRecordMessageMetaChange.Reason.DESTINATION_FIELD_SIZE_LIMITATION,
+            result.changes.first().reason
+        )
+    }
+
+    @Test
+    fun testValidString() {
+        val stringValue = StringValue("a valid string")
+        val airbyteValue =
+            EnrichedAirbyteValue(
+                abValue = stringValue,
+                type = StringType,
+                name = "name",
+                changes = mutableListOf(),
+                airbyteMetaField = null,
+            )
+
         val result = coercer.validate(airbyteValue)
         assertEquals(airbyteValue, result)
     }
