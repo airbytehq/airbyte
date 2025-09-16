0a837baff956623800dbd0039828f6faae8c22e5
Octavia Squidington III <octavia-squidington-iii@users.noreply.github.com>
2025-09-16 21:47:10 +0000

    chore: auto-fix lint and format issues

--- airbyte-integrations/connectors/source-datagen/src/main/kotlin/io/airbyte/integrations/source/datagen/flavor/increment/IncrementDataGenerator.kt
+++ airbyte-integrations/connectors/source-datagen/src/main/kotlin/io/airbyte/integrations/source/datagen/flavor/increment/IncrementDataGenerator.kt
@@ -1,3 +1,7 @@
+/*
+ * Copyright (c) 2025 Airbyte, Inc., all rights reserved.
+ */
+
 package io.airbyte.integrations.source.datagen.flavor.increment
 
 import io.airbyte.cdk.data.LongCodec
@@ -6,17 +10,18 @@ import io.airbyte.cdk.output.sockets.NativeRecordPayload
 import io.airbyte.integrations.source.datagen.flavor.DataGenerator
 import io.airbyte.integrations.source.datagen.flavor.increment.IncrementFlavor.fields
 
-class IncrementDataGenerator: DataGenerator {
+class IncrementDataGenerator : DataGenerator {
 
     override fun generateData(currentID: Long, modulo: Int, offset: Int): NativeRecordPayload {
         val incrementedID = (currentID * modulo + offset)
         val idField = fields["increment"]?.find { it.id == "id" }!!
 
         val recordData: NativeRecordPayload = mutableMapOf()
-        recordData[idField.id] = FieldValueEncoder(
-            incrementedID,
-            (idField.type as IntegerFieldType).jsonEncoder as LongCodec
-        )
+        recordData[idField.id] =
+            FieldValueEncoder(
+                incrementedID,
+                (idField.type as IntegerFieldType).jsonEncoder as LongCodec
+            )
 
         return recordData
     }
