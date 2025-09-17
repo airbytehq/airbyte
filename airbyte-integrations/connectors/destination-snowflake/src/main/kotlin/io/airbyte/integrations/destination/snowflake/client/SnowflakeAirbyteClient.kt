144c3b18320a92c56e8bc5ccc2799da9093b24d1
Benoit Moriceau <benoit@airbyte.io>
2025-09-17 09:13:35 -0700

    Some fixes

--- airbyte-integrations/connectors/destination-snowflake/src/main/kotlin/io/airbyte/integrations/destination/snowflake/client/SnowflakeAirbyteClient.kt
+++ airbyte-integrations/connectors/destination-snowflake/src/main/kotlin/io/airbyte/integrations/destination/snowflake/client/SnowflakeAirbyteClient.kt
@@ -133,7 +133,7 @@ class SnowflakeAirbyteClient(
                         val mappedName = columnNameMapping.get(name) ?: name
                         ColumnDefinition(
                             mappedName,
-                            snowflakeColumnUtils.toDialectType(fieldType.type),
+                            snowflakeColumnUtils.toDialectType(fieldType.type).takeWhile { char -> char != '(' },
                             fieldType.nullable
                         )
                     }
