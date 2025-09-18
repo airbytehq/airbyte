763d766ad5448f445ecaecc1099af521b1e4edd6
Francis Genet <francis.genet@airbyte.io>
2025-09-18 14:42:34 -0700

    Fix the testGetMappedColumnName from SnowflakeColumnNameMapperTest

--- airbyte-integrations/connectors/destination-snowflake/src/test/kotlin/io/airbyte/integrations/destination/snowflake/write/transform/SnowflakeColumnNameMapperTest.kt
+++ airbyte-integrations/connectors/destination-snowflake/src/test/kotlin/io/airbyte/integrations/destination/snowflake/write/transform/SnowflakeColumnNameMapperTest.kt
@@ -5,6 +5,8 @@
 package io.airbyte.integrations.destination.snowflake.write.transform
 
 import io.airbyte.cdk.load.command.DestinationStream
+import io.airbyte.cdk.load.orchestration.db.legacy_typing_deduping.TableCatalog
+import io.mockk.every
 import io.mockk.mockk
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
@@ -14,9 +16,15 @@ internal class SnowflakeColumnNameMapperTest {
     @Test
     fun testGetMappedColumnName() {
         val columnName = "test-column-name"
+        val expectedMappedName = "TEST_COLUMN_NAME"
         val stream = mockk<DestinationStream>()
-        val mapper = SnowflakeColumnNameMapper()
+        val tableCatalog = mockk<TableCatalog>()
+
+        // Configure the mock to return the expected mapped column name
+        every { tableCatalog.getMappedColumnName(stream, columnName) } returns expectedMappedName
+
+        val mapper = SnowflakeColumnNameMapper(tableCatalog)
         val result = mapper.getMappedColumnName(stream = stream, columnName = columnName)
-        assertEquals(columnName, result)
+        assertEquals(expectedMappedName, result)
     }
 }
