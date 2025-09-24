c64bfdbc7696cf86ec361bbcbd4e1d63b3b14765
Jonathan Pearlin <jonathan@airbyte.io>
2025-09-24 16:15:36 -0400

    Clean stages for raw tests

--- airbyte-integrations/connectors/destination-snowflake/src/test-integration/kotlin/io/airbyte/integrations/destination/snowflake/write/SnowflakeDataCleaner.kt
+++ airbyte-integrations/connectors/destination-snowflake/src/test-integration/kotlin/io/airbyte/integrations/destination/snowflake/write/SnowflakeDataCleaner.kt
@@ -10,6 +10,7 @@ import io.airbyte.integrations.destination.snowflake.cdk.SnowflakeMigratingConfi
 import io.airbyte.integrations.destination.snowflake.db.toSnowflakeCompatibleName
 import io.airbyte.integrations.destination.snowflake.spec.SnowflakeConfiguration
 import io.airbyte.integrations.destination.snowflake.spec.SnowflakeConfigurationFactory
+import io.airbyte.integrations.destination.snowflake.sql.STAGE_NAME_PREFIX
 import java.nio.file.Files
 import java.sql.Connection
 import java.time.Instant
@@ -61,12 +62,12 @@ object SnowflakeDataCleaner : DestinationCleaner {
 
     private fun cleanRawData(connection: Connection, config: SnowflakeConfiguration) {
         val statement = connection.createStatement()
-        val sql =
+        val tableSql =
             """SHOW TABLES
                     IN SCHEMA "${config.internalTableSchema?.toSnowflakeCompatibleName()}" 
                     STARTS WITH 'test'
                 """.trimIndent()
-        val tables = statement.executeQuery(sql)
+        val tables = statement.executeQuery(tableSql)
         while (tables.next()) {
             val databaseName = tables.getString("database_name")
             val schemaName = tables.getString("schema_name")
@@ -75,6 +76,24 @@ object SnowflakeDataCleaner : DestinationCleaner {
             // Clear all raw test tables in the database older than 24 hours
             if (createdOn.isBefore(Instant.now().minus(RETENTION_PERIOD))) {
                 val fullyQualifiedTable = "\"$databaseName\".\"$schemaName\".\"$tableName\""
+                statement.execute("DROP TABLE IF EXISTS $fullyQualifiedTable CASCADE")
+            }
+        }
+
+        val stagesSql =
+            """SHOW STAGES
+                    IN SCHEMA "${config.internalTableSchema?.toSnowflakeCompatibleName()}" 
+                    STARTS WITH '${STAGE_NAME_PREFIX}test'
+                """.trimIndent()
+        val stages = statement.executeQuery(stagesSql)
+        while (stages.next()) {
+            val databaseName = tables.getString("database_name")
+            val schemaName = tables.getString("schema_name")
+            val stageName = tables.getString("name")
+            val createdOn = tables.getTimestamp("created_on").toInstant()
+            // Clear all raw staging tables in the database older than 24 hours
+            if (createdOn.isBefore(Instant.now().minus(RETENTION_PERIOD))) {
+                val fullyQualifiedTable = "\"$databaseName\".\"$schemaName\".\"$stageName\""
                 statement.execute("DROP TABLE IF EXISTS $fullyQualifiedTable CASCADE")
             }
         }
