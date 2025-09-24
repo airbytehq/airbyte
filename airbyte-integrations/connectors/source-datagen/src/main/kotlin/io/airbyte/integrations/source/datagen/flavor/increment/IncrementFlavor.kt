5c6655b54343a2c67ca4616c51565d2da0cd7dbd
Sophie Cui <sophie.cui@airbyte.io>
2025-09-24 16:03:31 -0700

    mysql stuff cleanup

--- airbyte-integrations/connectors/source-datagen/src/main/kotlin/io/airbyte/integrations/source/datagen/flavor/increment/IncrementFlavor.kt
+++ airbyte-integrations/connectors/source-datagen/src/main/kotlin/io/airbyte/integrations/source/datagen/flavor/increment/IncrementFlavor.kt
@@ -5,18 +5,10 @@
 package io.airbyte.integrations.source.datagen.flavor.increment
 
 import io.airbyte.cdk.data.AirbyteSchemaType
-import io.airbyte.cdk.data.AnyEncoder
-import io.airbyte.cdk.data.ArrayAirbyteSchemaType
-import io.airbyte.cdk.data.ArrayEncoder
 import io.airbyte.cdk.data.BooleanCodec
 import io.airbyte.cdk.data.JsonEncoder
 import io.airbyte.cdk.data.LeafAirbyteSchemaType
-import io.airbyte.cdk.data.LocalDateCodec
-import io.airbyte.cdk.data.LocalDateTimeCodec
-import io.airbyte.cdk.data.LocalTimeCodec
 import io.airbyte.cdk.data.LongCodec
-import io.airbyte.cdk.data.OffsetDateTimeCodec
-import io.airbyte.cdk.data.OffsetTimeCodec
 import io.airbyte.cdk.data.TextCodec
 import io.airbyte.cdk.discover.Field
 import io.airbyte.cdk.discover.FieldType
@@ -32,10 +24,6 @@ data object IncrementFlavor : Flavor {
             Field("boolean", BooleanFieldType),
             Field("string", StringFieldType),))
     override val primaryKey = listOf("id")
-
-//    val customSchema = fields.mapValues { (_, fields) ->
-//        fields.filterNot { it.id == "boolean" } // user deselected "boolean"
-//    }
 
     override val dataGenerator = IncrementDataGenerator(fields["increment"] ?: listOf(Field("id", IntegerFieldType)))
 }
@@ -55,63 +43,77 @@ data object StringFieldType : FieldType {
     override val jsonEncoder: JsonEncoder<*> = TextCodec
 }
 
-data object DateFieldType : FieldType {
-    override val airbyteSchemaType: AirbyteSchemaType = LeafAirbyteSchemaType.DATE
-    override val jsonEncoder: JsonEncoder<*> = LocalDateCodec
-}
-
-data object TimeWithTimeZoneFieldType : FieldType {
-    override val airbyteSchemaType: AirbyteSchemaType = LeafAirbyteSchemaType.TIME_WITH_TIMEZONE
-    override val jsonEncoder: JsonEncoder<*> = OffsetTimeCodec
-}
-
-data object TimeWithoutTimeZoneFieldType : FieldType {
-    override val airbyteSchemaType: AirbyteSchemaType = LeafAirbyteSchemaType.TIME_WITHOUT_TIMEZONE
-    override val jsonEncoder: JsonEncoder<*> = LocalTimeCodec
-}
-
-data object TimestampWithTimeZoneFieldType : FieldType {
-    override val airbyteSchemaType: AirbyteSchemaType = LeafAirbyteSchemaType.TIMESTAMP_WITH_TIMEZONE
-    override val jsonEncoder: JsonEncoder<*> = OffsetDateTimeCodec
-}
-
-data object TimestampWithoutTimeZoneFieldType : FieldType {
-    override val airbyteSchemaType: AirbyteSchemaType = LeafAirbyteSchemaType.TIMESTAMP_WITHOUT_TIMEZONE
-    override val jsonEncoder: JsonEncoder<*> = LocalDateTimeCodec
-}
-
-data class ArrayFieldType(val elementFieldType: FieldType) : FieldType {
-    override val airbyteSchemaType: AirbyteSchemaType =
-        ArrayAirbyteSchemaType(elementFieldType.airbyteSchemaType)
-    override val jsonEncoder: JsonEncoder<*> = ArrayEncoder(elementFieldType.jsonEncoder)
-}
-
-data object ArrayWithoutSchemaFieldType : FieldType {
-    override val airbyteSchemaType: AirbyteSchemaType = ArrayAirbyteSchemaType(Any)
-    override val jsonEncoder: JsonEncoder<*> = ArrayEncoder(AnyEncoder)
-}
-
-data object UnionFieldType : FieldType {
-    override val airbyteSchemaType: AirbyteSchemaType = LeafAirbyteSchemaType.TIMESTAMP_WITHOUT_TIMEZONE
-    override val jsonEncoder: JsonEncoder<*> =
-}
-
-data object ObjectFieldType : FieldType {
-    override val airbyteSchemaType: AirbyteSchemaType = LeafAirbyteSchemaType.TIMESTAMP_WITHOUT_TIMEZONE
-    override val jsonEncoder: JsonEncoder<*> = LocalDateTimeCodec
-}
-
-data object ObjectWithEmptySchemaFieldType : FieldType {
-    override val airbyteSchemaType: AirbyteSchemaType = LeafAirbyteSchemaType.TIMESTAMP_WITHOUT_TIMEZONE
-    override val jsonEncoder: JsonEncoder<*> = LocalDateTimeCodec
-}
-
-data object ObjectWithoutEmptySchemaFieldType : FieldType {
-    override val airbyteSchemaType: AirbyteSchemaType = LeafAirbyteSchemaType.TIMESTAMP_WITHOUT_TIMEZONE
-    override val jsonEncoder: JsonEncoder<*> = LocalDateTimeCodec
-}
-
-data object UnknownFieldType : FieldType {
-    override val airbyteSchemaType: AirbyteSchemaType = LeafAirbyteSchemaType.TIMESTAMP_WITHOUT_TIMEZONE
-    override val jsonEncoder: JsonEncoder<*> = LocalDateTimeCodec
-}
+//data object DateFieldType : FieldType {
+//    override val airbyteSchemaType: AirbyteSchemaType = LeafAirbyteSchemaType.DATE
+//    override val jsonEncoder: JsonEncoder<*> = LocalDateCodec
+//}
+//
+//data object TimeWithTimeZoneFieldType : FieldType {
+//    override val airbyteSchemaType: AirbyteSchemaType = LeafAirbyteSchemaType.TIME_WITH_TIMEZONE
+//    override val jsonEncoder: JsonEncoder<*> = OffsetTimeCodec
+//}
+//
+//data object TimeWithoutTimeZoneFieldType : FieldType {
+//    override val airbyteSchemaType: AirbyteSchemaType = LeafAirbyteSchemaType.TIME_WITHOUT_TIMEZONE
+//    override val jsonEncoder: JsonEncoder<*> = LocalTimeCodec
+//}
+//
+//data object TimestampWithTimeZoneFieldType : FieldType {
+//    override val airbyteSchemaType: AirbyteSchemaType = LeafAirbyteSchemaType.TIMESTAMP_WITH_TIMEZONE
+//    override val jsonEncoder: JsonEncoder<*> = OffsetDateTimeCodec
+//}
+//
+//data object TimestampWithoutTimeZoneFieldType : FieldType {
+//    override val airbyteSchemaType: AirbyteSchemaType = LeafAirbyteSchemaType.TIMESTAMP_WITHOUT_TIMEZONE
+//    override val jsonEncoder: JsonEncoder<*> = LocalDateTimeCodec
+//}
+//
+//data class ArrayFieldType(val elementFieldType: FieldType) : FieldType {
+//    override val airbyteSchemaType: AirbyteSchemaType =
+//        ArrayAirbyteSchemaType(elementFieldType.airbyteSchemaType)
+//    override val jsonEncoder: JsonEncoder<*> = ArrayEncoder(elementFieldType.jsonEncoder)
+//}
+//
+//data object ArrayWithoutSchemaFieldType : FieldType {
+//    override val airbyteSchemaType: AirbyteSchemaType = ArrayAirbyteSchemaType()
+//    override val jsonEncoder: JsonEncoder<*> = ArrayEncoder(AnyEncoder)
+//}
+//
+//// TODO: left off here
+//data class UnionFieldType(val options: Set<FieldType>,
+//                           val isLegacyUnion: Boolean,) : FieldType {
+//    override val airbyteSchemaType: AirbyteSchemaType = TODO()
+//    override val jsonEncoder: JsonEncoder<*> = TODO()
+//}
+//
+//data class ObjectFieldType(
+//    val properties: LinkedHashMap<String, FieldType>,
+//    val additionalProperties: Boolean,
+//    val required: List<String>
+//) : FieldType {
+//    val schemaMap: LinkedHashMap<String, AirbyteSchemaType> =
+//        LinkedHashMap(properties.mapValues { (_, fieldType) ->
+//            fieldType.airbyteSchemaType
+//        })
+//    override val airbyteSchemaType: AirbyteSchemaType = ObjectAirbyteSchemaType(schemaMap, additionalProperties, required)
+//    override val jsonEncoder: JsonEncoder<*> = TODO()
+//}
+//
+//data class ObjectWithEmptySchemaFieldType(
+//    val propertiesEmpty: LinkedHashMap<String, AirbyteSchemaType> = LinkedHashMap(),
+//    val additionalProperties: Boolean,
+//    val required: List<String>
+//) : FieldType {
+//    override val airbyteSchemaType: AirbyteSchemaType = ObjectAirbyteSchemaType(propertiesEmpty, additionalProperties, required)
+//    override val jsonEncoder: JsonEncoder<*> = TODO()
+//}
+//
+//data object ObjectWithoutSchemaFieldType : FieldType {
+//    override val airbyteSchemaType: AirbyteSchemaType = ObjectAirbyteSchemaType()
+//    override val jsonEncoder: JsonEncoder<*> = TODO()
+//}
+//
+//data class UnknownFieldType(val schema: JsonNode): FieldType {
+//    override val airbyteSchemaType: AirbyteSchemaType = UnknownAirbyteSchemaType(schema)
+//    override val jsonEncoder: JsonEncoder<*> = AnyEncoder
+//}
