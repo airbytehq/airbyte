0c161ce0da23ae1e1cf57ffd2e62ab3d8d22ca88
Daryna Ishchenko <80129833+darynaishchenko@users.noreply.github.com>
2025-09-25 19:00:36 +0300

    feat(source-notion): unwrap to manifest only (#66532)

--- airbyte-integrations/connectors/source-notion/components.py
+++ airbyte-integrations/connectors/source-notion/components.py
@@ -1,19 +1,12 @@
 # Copyright (c) 2024 Airbyte, Inc., all rights reserved.
-import copy
 import logging
 from dataclasses import dataclass
-from datetime import timedelta
-from functools import partial
 from typing import Any, Iterable, List, Mapping, MutableMapping, Optional
 
-from airbyte_cdk.sources.declarative.extractors.record_filter import RecordFilter
-from airbyte_cdk.sources.declarative.incremental import ConcurrentCursorFactory, ConcurrentPerPartitionCursor
-from airbyte_cdk.sources.declarative.partition_routers import SubstreamPartitionRouter
-from airbyte_cdk.sources.declarative.retrievers.simple_retriever import FULL_REFRESH_SYNC_COMPLETE_KEY, SimpleRetriever
+from airbyte_cdk.sources.declarative.retrievers.simple_retriever import SimpleRetriever
 from airbyte_cdk.sources.declarative.transformations import RecordTransformation
-from airbyte_cdk.sources.declarative.types import StreamSlice, StreamState
+from airbyte_cdk.sources.declarative.types import StreamSlice
 from airbyte_cdk.sources.streams.core import StreamData
-from airbyte_cdk.utils.datetime_helpers import ab_datetime_format, ab_datetime_now
 
 
 # maximum block hierarchy recursive request depth
@@ -58,45 +51,6 @@ class NotionPropertiesTransformation(RecordTransformation):
         transformed_properties = [{"name": name, "value": value} for name, value in properties.items()]
         record["properties"] = transformed_properties
         return record
-
-
-@dataclass
-class NotionDataFeedFilter(RecordFilter):
-    """
-    Custom filter to implement functioning incremental sync for Data Feed endpoints.
-    The Data Feed incremental logic doesn't seem to play nice with Notion's cursor-based pagination,
-    and if the current state is not far enough in the future, at least one page will be queried,
-    causing any records in that page to be read despite not passing the state threshold. Setting the
-    page_size to a lower value can help mitigate this issue, but it's not a perfect solution, and the more
-    granular the page size, the greater the traffic. By using this filter, we can ensure the value of state is respected,
-    while still using the max page_size in requests.
-    """
-
-    def filter_records(
-        self, records: List[Mapping[str, Any]], stream_state: StreamState, stream_slice: Optional[StreamSlice] = None, **kwargs
-    ) -> List[Mapping[str, Any]]:
-        """
-        Filters a list of records, returning only those with a cursor_value greater than the current value in state.
-        """
-        current_state = stream_state.get("last_edited_time", {})
-        default_start_date = ab_datetime_format(ab_datetime_now() - timedelta(days=730), "%Y-%m-%dT%H:%M:%S.%fZ")
-        cursor_value = self._get_filter_date(self.config.get("start_date", default_start_date), current_state)
-        if cursor_value:
-            return [record for record in records if record["last_edited_time"] >= cursor_value]
-        return records
-
-    def _get_filter_date(self, start_date: str, state_value: list) -> str:
-        """
-        Calculates the filter date to pass in the request parameters by comparing the start_date with the value of state obtained from the stream_slice.
-        If only the start_date exists, use it by default.
-        """
-
-        start_date_timestamp = start_date or None
-        state_value_timestamp = state_value or None
-
-        if state_value_timestamp:
-            return max(filter(None, [start_date_timestamp, state_value_timestamp]), default=start_date_timestamp)
-        return start_date_timestamp
 
 
 @dataclass
--- airbyte-integrations/connectors/source-notion/unit_tests/test_components.py
+++ airbyte-integrations/connectors/source-notion/unit_tests/test_components.py
@@ -4,15 +4,14 @@
 from unittest.mock import MagicMock
 
 import pytest
-from source_notion import SourceNotion
-from source_notion.components import *
 
 from airbyte_cdk.models import ConfiguredAirbyteCatalogSerializer
 from airbyte_cdk.test.entrypoint_wrapper import read
 from airbyte_cdk.test.state_builder import StateBuilder
+from unit_tests.conftest import get_source
 
 
-def test_users_stream_transformation():
+def test_users_stream_transformation(components_module):
     input_record = {
         "object": "user",
         "id": "123",
@@ -55,10 +54,10 @@ def test_users_stream_transformation():
             "workspace_name": "test",
         },
     }
-    assert NotionUserTransformation().transform(input_record) == output_record
+    assert components_module.NotionUserTransformation().transform(input_record) == output_record
 
 
-def test_notion_properties_transformation():
+def test_notion_properties_transformation(components_module):
     input_record = {
         "id": "123",
         "properties": {
@@ -88,7 +87,7 @@ def test_notion_properties_transformation():
             },
         ],
     }
-    assert NotionPropertiesTransformation().transform(input_record) == output_record
+    assert components_module.NotionPropertiesTransformation().transform(input_record) == output_record
 
 
 state_test_records = [
@@ -96,42 +95,6 @@ state_test_records = [
     {"id": "2", "last_edited_time": "2022-01-03T00:00:00.000Z"},
     {"id": "3", "last_edited_time": "2022-01-04T00:00:00.000Z"},
 ]
-
-
-@pytest.fixture
-def data_feed_config():
-    return NotionDataFeedFilter(parameters={}, config={"start_date": "2021-01-01T00:00:00.000Z"})
-
-
-@pytest.mark.parametrize(
-    "state_value, expected_return",
-    [
-        ("2021-02-01T00:00:00.000Z", "2021-02-01T00:00:00.000Z"),
-        ("2020-01-01T00:00:00.000Z", "2021-01-01T00:00:00.000Z"),
-        ({}, "2021-01-01T00:00:00.000Z"),
-    ],
-    ids=["State value is greater than start_date", "State value is less than start_date", "Empty state, default to start_date"],
-)
-def test_data_feed_get_filter_date(data_feed_config, state_value, expected_return):
-    start_date = data_feed_config.config["start_date"]
-
-    result = data_feed_config._get_filter_date(start_date, state_value)
-    assert result == expected_return, f"Expected {expected_return}, but got {result}."
-
-
-@pytest.mark.parametrize(
-    "stream_state,stream_slice,expected_records",
-    [
-        ({"last_edited_time": "2022-01-01T00:00:00.000Z"}, {"id": "some_id"}, state_test_records),
-        ({"last_edited_time": "2022-01-03T00:00:00.000Z"}, {"id": "some_id"}, [state_test_records[-2], state_test_records[-1]]),
-        ({"last_edited_time": "2022-01-05T00:00:00.000Z"}, {"id": "some_id"}, []),
-        ({}, {"id": "some_id"}, state_test_records),
-    ],
-    ids=["No records filtered", "Some records filtered", "All records filtered", "Empty state: no records filtered"],
-)
-def test_data_feed_filter_records(data_feed_config, stream_state, stream_slice, expected_records):
-    filtered_records = data_feed_config.filter_records(state_test_records, stream_state, stream_slice)
-    assert filtered_records == expected_records, "Filtered records do not match the expected records."
 
 
 def test_blocks_retriever(requests_mock):
@@ -333,6 +296,6 @@ def test_blocks_retriever(requests_mock):
         }
     )
     state = StateBuilder().with_stream_state(blocks_stream_name, {}).build()
-    source = SourceNotion(catalog=catalog, config=config_mock, state=state)
+    source = get_source(config_mock, state)
     output = read(source, config=config_mock, catalog=catalog, state=state)
     assert len(output.records) == 3
