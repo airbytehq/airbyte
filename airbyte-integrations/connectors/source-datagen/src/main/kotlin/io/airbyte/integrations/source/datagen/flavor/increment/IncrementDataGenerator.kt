5fa2370a8fcfab0446be2e05da01fb5ee2425170
sophiecuiy <sophie.cui@airbyte.io>
2025-09-29 10:39:14 -0700

    data gen source (#66331)
    Co-authored-by: Octavia Squidington III <octavia-squidington-iii@users.noreply.github.com>

---
+++ airbyte-integrations/connectors/source-datagen/src/main/kotlin/io/airbyte/integrations/source/datagen/flavor/increment/IncrementDataGenerator.kt
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) 2025 Airbyte, Inc., all rights reserved.
+ */
+
+package io.airbyte.integrations.source.datagen.flavor.increment
+
+import io.airbyte.cdk.data.LongCodec
+import io.airbyte.cdk.output.sockets.FieldValueEncoder
+import io.airbyte.cdk.output.sockets.NativeRecordPayload
+import io.airbyte.integrations.source.datagen.flavor.DataGenerator
+
+class IncrementDataGenerator() : DataGenerator {
+
+    override fun generateData(currentID: Long, modulo: Int, offset: Int): NativeRecordPayload {
+        val incrementedID = (currentID * modulo + offset)
+        val recordData: NativeRecordPayload = mutableMapOf()
+
+        recordData["id"] =
+            FieldValueEncoder(incrementedID, IntegerFieldType.jsonEncoder as LongCodec)
+
+        return recordData
+    }
+}
