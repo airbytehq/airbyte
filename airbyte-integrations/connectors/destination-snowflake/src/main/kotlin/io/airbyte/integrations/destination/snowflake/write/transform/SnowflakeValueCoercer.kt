518e80891270f849e254317511d6a763851f67d2
tryangul <ryan.broughan@gmail.com>
2025-10-06 11:12:39 -0700

    Cleanup and comments.

--- airbyte-integrations/connectors/destination-snowflake/src/main/kotlin/io/airbyte/integrations/destination/snowflake/write/transform/SnowflakeValueCoercer.kt
+++ airbyte-integrations/connectors/destination-snowflake/src/main/kotlin/io/airbyte/integrations/destination/snowflake/write/transform/SnowflakeValueCoercer.kt
@@ -23,29 +23,30 @@ import java.math.BigInteger
 
 /*
  * Limits defined for datatypes in Snowflake.
- * See https://docs.snowflake.com/en/sql-reference/data-types-numeric and
- * https://docs.snowflake.com/en/sql-reference/data-types-semistructured for more
- * information
+ * See https://docs.snowflake.com/en/sql-reference-data-types for more information
  */
-internal const val INTEGER_PRECISION_LIMIT = 38
-internal val INT_MAX = BigInteger("99999999999999999999999999999999999999") // 38 9s 
+
+// https://docs.snowflake.com/en/sql-reference/data-types-numeric#number
+internal val INT_MAX = BigInteger("99999999999999999999999999999999999999") // 38 9s
 internal val INT_MIN = BigInteger("-99999999999999999999999999999999999999") // 38 9s
 internal val INT_RANGE = INT_MIN..INT_MAX
-internal const val FLOAT_MAX = 9.007199E15f
-internal const val FLOAT_MIN = -9.007199E15f
-internal val DECIMAL_MAX = BigDecimal(FLOAT_MAX.toString())
-internal val DECIMAL_MIN = BigDecimal(FLOAT_MIN.toString())
-internal val DECIMAL_RANGE = DECIMAL_MIN..DECIMAL_MAX
+
+// https://docs.snowflake.com/en/sql-reference/data-types-numeric#label-data-type-float
+internal val FLOAT_MAX = BigDecimal("9007199254740991")
+internal val FLOAT_MIN = BigDecimal("-9007199254740991")
+internal val FLOAT_RANGE = FLOAT_MIN..FLOAT_MAX
+
+// https://docs.snowflake.com/en/sql-reference/data-types-text#varchar
 internal const val VARCHAR_AND_VARIANT_LIMIT_BYTES = 134217728 // 128 MB
-// UTF-8 has max 4 bytes per char, so we pre-calculate a safe length limit
-internal const val MAX_UTF_8_STRING_LENGTH_UNDER_LIMIT = VARCHAR_AND_VARIANT_LIMIT_BYTES / 4
+// UTF-8 has max 4 bytes per char, so anything under this length is safe
+internal const val MAX_UTF_8_STRING_LENGTH_UNDER_LIMIT = 33554432 // (134217728 / 4)
 
 fun isValid(value: AirbyteValue): Boolean {
     return when (value) {
         is ArrayValue,
         is ObjectValue -> isStringValid(value.toCsvValue().toString())
         is IntegerValue -> value.value in INT_RANGE
-        is NumberValue -> value.value in DECIMAL_RANGE
+        is NumberValue -> value.value in FLOAT_RANGE
         is StringValue -> isStringValid(value.value)
         else -> true
     }
