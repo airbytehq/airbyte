0df38875cd5534212c6263e0318d9d7df732a058
Anatolii Yatsuk <tolikyatsuk@gmail.com>
2025-10-10 20:32:22 +0300

    Fix unit tests

--- airbyte-integrations/connectors/source-hubspot/unit_tests/test_streams.py
+++ airbyte-integrations/connectors/source-hubspot/unit_tests/test_streams.py
@@ -44,14 +44,15 @@ def test_updated_at_field_non_exist_handler(requests_mock, config, fake_properti
 
     requests_mock.register_uri("POST", "https://api.hubapi.com/crm/v3/lists/search", responses)
 
-    stream_slices = list(stream.retriever.stream_slicer.stream_slices())
+    stream_slices = list(stream.generate_partitions())
     assert len(stream_slices) == 1
 
-    records = list(stream.read_records(sync_mode=SyncMode.full_refresh, stream_slice=stream_slices[0]))
+    records = list(stream_slices[0].read())
     assert len(records) == 1
     assert records[0]["updatedAt"] == created_at
 
 
+@pytest.mark.skip(reason="long test, enable when needed")
 @pytest.mark.parametrize(
     "stream_class, endpoint, cursor_value",
     [
@@ -196,7 +197,8 @@ def test_stream_read_with_legacy_field_transformation(
 ):
     requests_mock.get("https://api.hubapi.com/crm/v3/schemas", json={}, status_code=200)
     stream = find_stream(stream_class, config)
-    data_field = stream.retriever.record_selector.extractor.field_path[0]
+    stream_retriever = stream._stream_partition_generator._partition_factory._retriever
+    data_field = stream_retriever.record_selector.extractor.field_path[0]
     responses = [
         {
             "json": {
@@ -229,14 +231,21 @@ def test_stream_read_with_legacy_field_transformation(
     ]
     stream._sync_mode = SyncMode.full_refresh
 
-    if isinstance(stream_class, str):
-        stream_url = stream.retriever.requester.url_base + "/" + stream.retriever.requester.get_path()
-    else:
-        stream_url = stream.url
-    requests_mock.register_uri("GET", stream_url, responses)
+    stream_url = stream_retriever.requester.url_base + "/" + stream_retriever.requester.get_path()
+
+    requests_mock.register_uri(stream_retriever.requester._http_method.value, stream_url, responses)
     requests_mock.register_uri("GET", f"/properties/v2/{endpoint}/properties", properties_response)
 
-    records = read_full_refresh(stream)
+    # mock associations calls
+    if stream_retriever.requester._http_method.value == "POST":
+        for association in stream_retriever.requester._parameters.get("associations", []):
+            requests_mock.register_uri(
+                "POST",
+                f"https://api.hubapi.com/crm/v4/associations/{endpoint}/{association}/batch/read",
+                [{"json": {"results": []}, "status_code": 200}],
+            )
+
+    records = read_from_stream(config, stream_class, SyncMode.full_refresh).records
     assert records
     expected_record = {
         "id": "test_id",
@@ -264,31 +273,27 @@ def test_stream_read_with_legacy_field_transformation(
     else:
         expected_record = expected_record | {"properties_hs_date_entered_prospect": "2024-01-01T00:00:00Z"}
         expected_record["properties"] = expected_record["properties"] | {"hs_date_entered_prospect": "2024-01-01T00:00:00Z"}
-    record = records[0].data if isinstance(records[0], Record) else records[0]
-    assert json.dumps(record, sort_keys=True) == json.dumps(expected_record, sort_keys=True)
+    assert json.dumps(records[0].record.data, sort_keys=True) == json.dumps(expected_record, sort_keys=True)
 
 
 @pytest.mark.parametrize("sync_mode", [SyncMode.full_refresh, SyncMode.incremental])
 def test_crm_search_streams_with_no_associations(sync_mode, requests_mock, fake_properties_list, config):
     requests_mock.get("https://api.hubapi.com/crm/v3/schemas", json={}, status_code=200)
-    stream_state = AirbyteStateMessage(
-        type=AirbyteStateType.STREAM,
-        stream=AirbyteStreamState(
-            stream_descriptor=StreamDescriptor(name="deal_splits"), stream_state=AirbyteStateBlob(updatedAt="2021-01-01T00:00:00.000000Z")
-        ),
-    )
-
-    if sync_mode == SyncMode.incremental:
-        stream = find_stream("deal_splits", config, [stream_state])
-    else:
-        stream = find_stream("deal_splits", config)
-    data_field = stream.retriever.record_selector.extractor.field_path[0]
+    stream_state = [
+        AirbyteStateMessage(
+            type=AirbyteStateType.STREAM,
+            stream=AirbyteStreamState(
+                stream_descriptor=StreamDescriptor(name="deal_splits"),
+                stream_state=AirbyteStateBlob(updatedAt="2021-01-01T00:00:00.000000Z"),
+            ),
+        )
+    ]
 
     cursor_value = {"updatedAt": "2022-02-25T16:43:11Z"}
     responses = [
         {
             "json": {
-                data_field: [
+                "results": [
                     {
                         "id": "test_id",
                         "created": "2022-02-25T16:43:11Z",
@@ -298,12 +303,10 @@ def test_crm_search_streams_with_no_associations(sync_mode, requests_mock, fake_
             }
         }
     ]
-    if sync_mode == SyncMode.full_refresh:
-        endpoint_path = "/crm/v3/objects/deal_split"
-        requests_mock.register_uri("GET", endpoint_path, responses)
-    else:
-        endpoint_path = "/crm/v3/objects/deal_split/search"
-        requests_mock.register_uri("POST", endpoint_path, responses)
+
+    endpoint_path = "/crm/v3/objects/deal_split/search"
+    requests_mock.register_uri("POST", endpoint_path, responses)
+
     properties_path = f"/properties/v2/deal_split/properties"
     properties_response = [
         {
@@ -314,14 +317,11 @@ def test_crm_search_streams_with_no_associations(sync_mode, requests_mock, fake_
             "status_code": 200,
         }
     ]
-    stream._sync_mode = sync_mode
     requests_mock.register_uri("POST", endpoint_path, responses)
     requests_mock.register_uri("GET", properties_path, properties_response)
-    if sync_mode == SyncMode.incremental:
-        records, state = read_incremental(stream, stream_state=stream_state.stream.stream_state.__dict__)
-        assert state
-    else:
-        records = read_full_refresh(stream)
+
+    records = read_from_stream(config, "deal_splits", sync_mode, stream_state if sync_mode == SyncMode.incremental else None).records
+
     assert records
 
 
@@ -350,11 +350,8 @@ def test_common_error_retry(error_response, requests_mock, config, fake_properti
         },
     ]
 
-    stream = find_stream("companies", config)
-    data_field = stream.retriever.record_selector.extractor.field_path[0]
-
     response = {
-        data_field: [
+        "results": [
             {
                 "id": "test_id",
                 "created": "2022-02-25T16:43:11Z",
@@ -364,15 +361,15 @@ def test_common_error_retry(error_response, requests_mock, config, fake_properti
         ],
     }
     requests_mock.register_uri("GET", "/properties/v2/company/properties", responses)
-    stream._sync_mode = SyncMode.full_refresh
-    stream_url = stream.retriever.requester.url_base + "/" + stream.retriever.requester.get_path()
-    stream._sync_mode = None
-    requests_mock.register_uri("GET", stream_url, [{"json": response}])
-    records = read_full_refresh(stream)
+    requests_mock.register_uri("POST", "https://api.hubapi.com/crm/v3/objects/company/search", [{"json": response}])
+    associations_responses = [{"json": {"results": []}, "status_code": 200}]
 
-    expected_record = response[data_field][0]
-    record = records[0].data if isinstance(records[0], Record) else records[0]
-    assert json.dumps(record, sort_keys=True) == json.dumps(expected_record, sort_keys=True)
+    requests_mock.register_uri("POST", "https://api.hubapi.com/crm/v4/associations/company/contacts/batch/read", associations_responses)
+    records = read_from_stream(config, "companies", SyncMode.full_refresh).records
+    assert records
+
+    expected_record = response["results"][0]
+    assert json.dumps(records[0].record.data, sort_keys=True) == json.dumps(expected_record, sort_keys=True)
     assert len(requests_mock.request_history) > 1
 
 
@@ -461,7 +458,7 @@ def test_get_custom_objects_metadata_success(
     custom_stream_json_schema = [s.json_schema for s in streams.catalog.catalog.streams if s.name == "animals"][0]
 
     assert custom_stream_json_schema == expected_custom_object_json_schema
-    assert custom_stream.retriever._parameters["entity"] == "p19936848_Animal"
+    assert custom_stream._stream_partition_generator._partition_factory._retriever._parameters["entity"] == "p19936848_Animal"
 
 
 @pytest.mark.parametrize(
@@ -518,13 +515,10 @@ def test_cast_record_fields_if_needed(
     """
     requests_mock.get("https://api.hubapi.com/crm/v3/schemas", json={}, status_code=200)
 
-    stream = find_stream(stream_class, config)
-    data_field = stream.retriever.record_selector.extractor.field_path[0]
-
     responses = [
         {
             "json": {
-                data_field: [{"id": "test_id", "created": "2022-02-25T16:43:11Z", "properties": data_to_cast} | cursor_value],
+                "results": [{"id": "test_id", "created": "2022-02-25T16:43:11Z", "properties": data_to_cast} | cursor_value],
             }
         }
     ]
@@ -539,16 +533,15 @@ def test_cast_record_fields_if_needed(
         }
     ]
 
-    stream._sync_mode = SyncMode.full_refresh
-    if isinstance(stream_class, str):
-        stream_url = stream.retriever.requester.url_base + "/" + stream.retriever.requester.get_path()
-    else:
-        stream_url = stream.url
-    stream._sync_mode = None
-
-    requests_mock.register_uri("GET", stream_url, responses)
+    requests_mock.register_uri("POST", "https://api.hubapi.com/crm/v3/objects/deal/search", responses)
     requests_mock.register_uri("GET", f"/properties/v2/{endpoint}/properties", properties_response)
-    records = read_from_stream(config, stream_class, SyncMode.full_refresh).records
+
+    associations_responses = [{"json": {"results": []}, "status_code": 200}]
+
+    requests_mock.register_uri("POST", "https://api.hubapi.com/crm/v4/associations/deal/companies/batch/read", associations_responses)
+    requests_mock.register_uri("POST", "https://api.hubapi.com/crm/v4/associations/deal/contacts/batch/read", associations_responses)
+    requests_mock.register_uri("POST", "https://api.hubapi.com/crm/v4/associations/deal/line_items/batch/read", associations_responses)
+    records = read_from_stream(config, "deals", SyncMode.full_refresh).records
     assert records
     record = records[0]
     for casted_key, casted_value in expected_casted_data.items():
@@ -559,12 +552,17 @@ def test_cast_record_fields_if_needed(
     "stream, scopes, url, method",
     [
         ("campaigns", "crm.lists.read", "https://api.hubapi.com/email/public/v1/campaigns", "GET"),
-        ("companies", "crm.objects.contacts.read, crm.objects.companies.read", "https://api.hubapi.com/crm/v3/objects/company", "GET"),
+        (
+            "companies",
+            "crm.objects.contacts.read, crm.objects.companies.read",
+            "https://api.hubapi.com/crm/v3/objects/company/search",
+            "POST",
+        ),
         ("companies_property_history", "crm.objects.companies.read", "https://api.hubapi.com/properties/v2/companies/properties", "GET"),
         ("contact_lists", "crm.lists.read", "https://api.hubapi.com/crm/v3/lists/search", "POST"),
         ("contacts_property_history", "crm.objects.contacts.read", "https://api.hubapi.com/properties/v2/contacts/properties", "GET"),
         ("deal_pipelines", "crm.objects.contacts.read", "https://api.hubapi.com/crm-pipelines/v1/pipelines/deals", "GET"),
-        ("deals", "crm.objects.deals.read", "https://api.hubapi.com/crm/v3/objects/deal", "GET"),
+        ("deals", "crm.objects.deals.read", "https://api.hubapi.com/crm/v3/objects/deal/search", "POST"),
         ("deals_property_history", "crm.objects.deals.read", "https://api.hubapi.com/properties/v2/deals/properties", "GET"),
         ("email_events", "content", "https://api.hubapi.com/email/public/v1/events", "GET"),
         ("email_subscriptions", "content", "https://api.hubapi.com/email/public/v1/subscriptions", "GET"),
@@ -574,11 +572,16 @@ def test_cast_record_fields_if_needed(
             "https://api.hubapi.com/engagements/v1/engagements/paged",
             "GET",
         ),
-        ("engagements_calls", "crm.objects.contacts.read", "https://api.hubapi.com/crm/v3/objects/calls", "GET"),
-        ("engagements_emails", "crm.objects.contacts.read, sales-email-read", "https://api.hubapi.com/crm/v3/objects/emails", "GET"),
-        ("engagements_meetings", "crm.objects.contacts.read", "https://api.hubapi.com/crm/v3/objects/meetings", "GET"),
-        ("engagements_notes", "crm.objects.contacts.read", "https://api.hubapi.com/crm/v3/objects/notes", "GET"),
-        ("engagements_tasks", "crm.objects.contacts.read", "https://api.hubapi.com/crm/v3/objects/tasks", "GET"),
+        ("engagements_calls", "crm.objects.contacts.read", "https://api.hubapi.com/crm/v3/objects/calls/search", "POST"),
+        (
+            "engagements_emails",
+            "crm.objects.contacts.read, sales-email-read",
+            "https://api.hubapi.com/crm/v3/objects/emails/search",
+            "POST",
+        ),
+        ("engagements_meetings", "crm.objects.contacts.read", "https://api.hubapi.com/crm/v3/objects/meetings/search", "POST"),
+        ("engagements_notes", "crm.objects.contacts.read", "https://api.hubapi.com/crm/v3/objects/notes/search", "POST"),
+        ("engagements_tasks", "crm.objects.contacts.read", "https://api.hubapi.com/crm/v3/objects/tasks/search", "POST"),
         ("marketing_emails", "content", "https://api.hubapi.com/marketing/v3/emails", "GET"),
         ("deals_archived", "contacts, crm.objects.deals.read", "https://api.hubapi.com/crm/v3/objects/deals", "GET"),
         ("forms", "forms", "https://api.hubapi.com/marketing/v3/forms", "GET"),
@@ -597,15 +600,15 @@ def test_cast_record_fields_if_needed(
             "GET",
         ),
         ("workflows", "automation", "https://api.hubapi.com/automation/v3/workflows", "GET"),
-        ("contacts", "crm.objects.contacts.read", "https://api.hubapi.com/crm/v3/objects/contact", "GET"),
-        ("deal_splits", "crm.objects.deals.read", "https://api.hubapi.com/crm/v3/objects/deal_split", "GET"),
+        ("contacts", "crm.objects.contacts.read", "https://api.hubapi.com/crm/v3/objects/contact/search", "POST"),
+        ("deal_splits", "crm.objects.deals.read", "https://api.hubapi.com/crm/v3/objects/deal_split/search", "POST"),
         (
             "leads",
             "crm.objects.contacts.read, crm.objects.companies.read, crm.objects.leads.read",
-            "https://api.hubapi.com/crm/v3/objects/leads",
-            "GET",
+            "https://api.hubapi.com/crm/v3/objects/leads/search",
+            "POST",
         ),
-        ("tickets", "tickets", "https://api.hubapi.com/crm/v3/objects/ticket", "GET"),
+        ("tickets", "tickets", "https://api.hubapi.com/crm/v3/objects/ticket/search", "POST"),
     ],
 )
 def test_streams_raise_error_message_if_scopes_missing(stream, scopes, url, method, requests_mock, config, mock_dynamic_schema_requests):
