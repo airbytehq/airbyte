d88dbbc737f2e9e8d0e6d3ff5abde2fdce85ed59
Benoit Moriceau <benoit@airbyte.io>
2025-10-14 12:44:29 -0700

    Both

---
+++ airbyte-integrations/connectors/destination-postgres/src/main/kotlin/io/airbyte/integrations/destination/postgres/client/PostgresAirbyteClient.kt
@@ -0,0 +1,136 @@
+/*
+* Copyright (c) 2025 Airbyte, Inc., all rights reserved.
+*/
+
+package io.airbyte.integrations.destination.postgres.client
+
+import io.airbyte.cdk.load.client.AirbyteClient
+import io.airbyte.cdk.load.command.DestinationStream
+import io.airbyte.cdk.load.message.Meta.Companion.COLUMN_NAME_AB_GENERATION_ID
+import io.airbyte.cdk.load.orchestration.db.ColumnNameMapping
+import io.airbyte.cdk.load.orchestration.db.TableName
+import io.airbyte.cdk.load.orchestration.db.direct_load_table.DirectLoadTableNativeOperations
+import io.airbyte.cdk.load.orchestration.db.direct_load_table.DirectLoadTableSqlOperations
+import io.airbyte.integrations.destination.postgres.sql.PostgresDirectLoadSqlGenerator
+import io.github.oshai.kotlinlogging.KotlinLogging
+import jakarta.inject.Singleton
+import javax.sql.DataSource
+
+private val log = KotlinLogging.logger {}
+
+internal const val COUNT_TOTAL_ALIAS = "total"
+
+@Singleton
+class PostgresAirbyteClient(
+    private val dataSource: DataSource,
+    private val sqlGenerator: PostgresDirectLoadSqlGenerator,
+) : AirbyteClient, DirectLoadTableSqlOperations, DirectLoadTableNativeOperations {
+
+    override suspend fun countTable(tableName: TableName): Long? =
+        try {
+            dataSource.connection.use { connection ->
+                val resultSet =
+                    connection.createStatement().executeQuery(sqlGenerator.countTable(tableName))
+
+                if (resultSet.next()) {
+                    resultSet.getLong(COUNT_TOTAL_ALIAS)
+                } else {
+                    0L
+                }
+            }
+        } catch (e: Exception) {
+            log.debug(e) {
+                "Table ${tableName.namespace}.${tableName.name} does not exist. Returning a null count to signal a missing table."
+            }
+            null
+        }
+
+    override suspend fun createNamespace(namespace: String) {
+        execute(sqlGenerator.createNamespace(namespace))
+    }
+
+    override suspend fun createTable(
+        stream: DestinationStream,
+        tableName: TableName,
+        columnNameMapping: ColumnNameMapping,
+        replace: Boolean
+    ) {
+        execute(sqlGenerator.createTable(stream, tableName, columnNameMapping, replace))
+    }
+
+    override suspend fun overwriteTable(
+        sourceTableName: TableName,
+        targetTableName: TableName
+    ) {
+        execute(sqlGenerator.overwriteTable(sourceTableName, targetTableName))
+    }
+
+    override suspend fun copyTable(
+        columnNameMapping: ColumnNameMapping,
+        sourceTableName: TableName,
+        targetTableName: TableName
+    ) {
+        execute(sqlGenerator.copyTable(columnNameMapping, sourceTableName, targetTableName))
+    }
+
+    override suspend fun upsertTable(
+        stream: DestinationStream,
+        columnNameMapping: ColumnNameMapping,
+        sourceTableName: TableName,
+        targetTableName: TableName
+    ) {
+        execute(sqlGenerator.upsertTable(stream, columnNameMapping, sourceTableName, targetTableName))
+    }
+
+    override suspend fun dropTable(tableName: TableName) {
+        execute(sqlGenerator.dropTable(tableName))
+    }
+
+    override suspend fun ensureSchemaMatches(
+        stream: DestinationStream,
+        tableName: TableName,
+        columnNameMapping: ColumnNameMapping
+    ) = TODO("PostgresAirbyteClient.ensureSchemaMatches not yet implemented")
+
+    override suspend fun getGenerationId(tableName: TableName): Long =
+        try {
+            val sql = sqlGenerator.getGenerationId(tableName)
+            dataSource.connection.use { connection ->
+                val resultSet = connection.createStatement().executeQuery(sql)
+                if (resultSet.next()) {
+                    resultSet.getLong(COLUMN_NAME_AB_GENERATION_ID)
+                } else {
+                    log.warn { "No generation ID found for table $tableName, returning 0" }
+                    0L
+                }
+            }
+        } catch (e: Exception) {
+            log.error(e) { "Failed to retrieve the generation ID for table $tableName" }
+            0L
+        }
+
+    fun describeTable(tableName: TableName): List<String> =
+        dataSource.connection.use { connection ->
+            val resultSet =
+                connection.createStatement().executeQuery(sqlGenerator.showColumns(tableName))
+            val columns = mutableListOf<String>()
+            while (resultSet.next()) {
+                columns.add(resultSet.getString("column_name"))
+            }
+            return columns
+        }
+
+    fun copyFromCsv(tableName: TableName, filePath: String) {
+        dataSource.connection.use { connection ->
+            val copyManager = connection.unwrap(org.postgresql.core.BaseConnection::class.java)
+                .getCopyAPI()
+            val sql = sqlGenerator.copyFromCsv(tableName, filePath)
+            java.io.FileInputStream(filePath).use { fileInputStream ->
+                copyManager.copyIn(sql, fileInputStream)
+            }
+        }
+    }
+
+    internal fun execute(query: String) =
+        dataSource.connection.use { connection -> connection.createStatement().executeQuery(query) }
+}
---
+++ airbyte-integrations/connectors/destination-postgres/src/main/kotlin/io/airbyte/integrations/destination/postgres/spec/PostgresSpecification.kt
@@ -0,0 +1,229 @@
+/*
+ * Copyright (c) 2025 Airbyte, Inc., all rights reserved.
+ */
+
+package io.airbyte.integrations.destination.postgres.spec
+
+import com.fasterxml.jackson.annotation.JsonGetter
+import com.fasterxml.jackson.annotation.JsonIgnore
+import com.fasterxml.jackson.annotation.JsonProperty
+import com.fasterxml.jackson.annotation.JsonPropertyDescription
+import com.fasterxml.jackson.annotation.JsonSetter
+import com.fasterxml.jackson.annotation.JsonSubTypes
+import com.fasterxml.jackson.annotation.JsonTypeInfo
+import com.fasterxml.jackson.annotation.JsonValue
+import com.kjetland.jackson.jsonSchema.annotations.JsonSchemaDescription
+import com.kjetland.jackson.jsonSchema.annotations.JsonSchemaInject
+import com.kjetland.jackson.jsonSchema.annotations.JsonSchemaTitle
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings
+import io.airbyte.cdk.command.ConfigurationSpecification
+import io.airbyte.cdk.load.spec.DestinationSpecificationExtension
+import io.airbyte.cdk.ssh.MicronautPropertiesFriendlySshTunnelMethodConfigurationSpecification
+import io.airbyte.cdk.ssh.SshTunnelMethodConfiguration
+import io.airbyte.protocol.models.v0.DestinationSyncMode
+import io.micronaut.context.annotation.ConfigurationBuilder
+import jakarta.inject.Singleton
+
+@Singleton
+@JsonSchemaTitle("Postgres Destination Specification")
+@SuppressFBWarnings("NP_NONNULL_RETURN_VIOLATION")
+class PostgresSpecification : ConfigurationSpecification() {
+    @get:JsonSchemaTitle("Host")
+    @get:JsonPropertyDescription("Hostname of the database.")
+    @get:JsonProperty("host")
+    @get:JsonSchemaInject(json = """{"group": "connection", "order": 0}""")
+    val host: String = ""
+
+    @get:JsonSchemaTitle("Port")
+    @get:JsonPropertyDescription("Port of the database.")
+    @get:JsonProperty("port")
+    @get:JsonSchemaInject(json = """{"group": "connection", "order": 1, "minimum": 0, "maximum": 65536, "examples": ["5432"]}""")
+    val port: Int = 5432
+
+    @get:JsonSchemaTitle("Database Name")
+    @get:JsonPropertyDescription("Name of the database.")
+    @get:JsonProperty("database")
+    @get:JsonSchemaInject(json = """{"group": "connection", "order": 2}""")
+    val database: String = ""
+
+    @get:JsonSchemaTitle("Default Schema")
+    @get:JsonPropertyDescription(
+        "The default schema tables are written. If not specified otherwise, the \"public\" schema will be used."
+    )
+    @get:JsonProperty("schema")
+    @get:JsonSchemaInject(json = """{"group": "connection", "order": 3, "examples": ["public"], "default": "public"}""")
+    val schema: String = "public"
+
+    @get:JsonSchemaTitle("Username")
+    @get:JsonPropertyDescription("Username to access the database.")
+    @get:JsonProperty("username")
+    @get:JsonSchemaInject(json = """{"group": "connection", "order": 4}""")
+    val username: String = ""
+
+    @get:JsonSchemaTitle("Password")
+    @get:JsonPropertyDescription("Password associated with the username.")
+    @get:JsonProperty("password")
+    @get:JsonSchemaInject(json = """{"group": "connection", "order": 5, "airbyte_secret": true}""")
+    val password: String? = null
+
+    @get:JsonSchemaTitle("SSL Modes")
+    @get:JsonPropertyDescription(
+        """SSL connection modes.
+  <b>disable</b> - Disables encryption of communication between Airbyte and destination database.
+  <b>allow</b> - Enables encryption only when required by the destination database.
+  <b>prefer</b> - Allows unencrypted connections only if the destination database does not support encryption.
+  <b>require</b> - Always require encryption. If the destination database server does not support encryption, connection will fail.
+  <b>verify-ca</b> - Always require encryption and verifies that the destination database server has a valid SSL certificate.
+  <b>verify-full</b> - This is the most secure mode. Always require encryption and verifies the identity of the destination database server.
+ See more information - <a href="https://jdbc.postgresql.org/documentation/head/ssl-client.html"> in the docs</a>."""
+    )
+    @get:JsonProperty("ssl_mode")
+    @get:JsonSchemaInject(json = """{"group": "connection", "order": 6}""")
+    val sslMode: SslMode? = null
+
+    @get:JsonSchemaTitle("JDBC URL Parameters (Advanced)")
+    @get:JsonPropertyDescription(
+        """Additional properties to pass to the JDBC URL string when connecting to the database formatted as 'key=value' pairs separated by the symbol '&'. (Eg. key1=value1&key2=value2&key3=value3). For more information read about <a href="https://jdbc.postgresql.org/documentation/head/connect.html">JDBC URL parameters</a>."""
+    )
+    @get:JsonProperty("jdbc_url_params")
+    @get:JsonSchemaInject(json = """{"group": "advanced", "order": 7}""")
+    val jdbcUrlParams: String? = null
+
+    @get:JsonSchemaTitle("CDC deletion mode")
+    @get:JsonPropertyDescription(
+        """Whether to execute CDC deletions as hard deletes (i.e. propagate source deletions to the destination), or soft deletes (i.e. leave a tombstone record in the destination). Defaults to hard deletes.""",
+    )
+    @get:JsonProperty("cdc_deletion_mode", defaultValue = "Hard delete")
+    @get:JsonSchemaInject(
+        json = """{"group": "sync_behavior", "order": 8, "always_show": true}""",
+    )
+    val cdcDeletionMode: CdcDeletionMode? = null
+
+    @get:JsonSchemaTitle(
+        """Legacy raw tables""",
+    )
+    @get:JsonPropertyDescription(
+        """Write the legacy "raw tables" format, to enable backwards compatibility with older versions of this connector.""",
+    )
+    @get:JsonProperty("disable_type_dedupe")
+    @get:JsonSchemaInject(json = """{"group": "advanced", "order": 9}""")
+    @Suppress("RedundantNullableReturnType")
+    val legacyRawTablesOnly: Boolean? = false
+
+    @get:JsonSchemaTitle("Airbyte Internal Schema Name")
+    @get:JsonPropertyDescription(
+        """Airbyte will use this schema for various internal tables. In legacy raw tables mode, the raw tables will be stored in this schema. Defaults to "airbyte_internal".""",
+    )
+    @get:JsonProperty("raw_data_schema")
+    @get:JsonSchemaInject(json = """{"group": "advanced", "order": 10}""")
+    val internalTableSchema: String? = null
+
+    @JsonIgnore
+    @ConfigurationBuilder(configurationPrefix = "tunnel_method")
+    val tunnelMethod = MicronautPropertiesFriendlySshTunnelMethodConfigurationSpecification()
+
+    @JsonIgnore var tunnelMethodJson: SshTunnelMethodConfiguration? = null
+
+    @JsonSetter("tunnel_method")
+    fun setTunnelMethodValue(value: SshTunnelMethodConfiguration?) {
+        tunnelMethodJson = value
+    }
+
+    @JsonGetter("tunnel_method")
+    @JsonSchemaTitle("SSH Tunnel Method")
+    @JsonPropertyDescription(
+        "Whether to initiate an SSH tunnel before connecting to the database, and if so, which kind of authentication to use.",
+    )
+    @JsonSchemaInject(json = """{"group": "connection", "order": 11}""")
+    fun getTunnelMethodValue(): SshTunnelMethodConfiguration? =
+        tunnelMethodJson ?: tunnelMethod.asSshTunnelMethod()
+}
+
+@JsonTypeInfo(use = JsonTypeInfo.Id.NAME, property = "mode")
+@JsonSubTypes(
+    JsonSubTypes.Type(value = SslModeDisable::class, name = SslModeDisable.MODE),
+    JsonSubTypes.Type(value = SslModeAllow::class, name = SslModeAllow.MODE),
+    JsonSubTypes.Type(value = SslModePrefer::class, name = SslModePrefer.MODE),
+    JsonSubTypes.Type(value = SslModeRequire::class, name = SslModeRequire.MODE),
+    JsonSubTypes.Type(value = SslModeVerifyCa::class, name = SslModeVerifyCa.MODE),
+    JsonSubTypes.Type(value = SslModeVerifyFull::class, name = SslModeVerifyFull.MODE),
+)
+sealed interface SslMode {
+    @get:JsonProperty("mode") val mode: String
+}
+
+@JsonSchemaTitle("disable")
+@JsonSchemaDescription("Disable SSL.")
+class SslModeDisable : SslMode {
+    companion object {
+        const val MODE = "disable"
+    }
+    override val mode: String = MODE
+}
+
+@JsonSchemaTitle("allow")
+@JsonSchemaDescription("Allow SSL mode.")
+class SslModeAllow : SslMode {
+    companion object {
+        const val MODE = "allow"
+    }
+    override val mode: String = MODE
+}
+
+@JsonSchemaTitle("prefer")
+@JsonSchemaDescription("Prefer SSL mode.")
+class SslModePrefer : SslMode {
+    companion object {
+        const val MODE = "prefer"
+    }
+    override val mode: String = MODE
+}
+
+@JsonSchemaTitle("require")
+@JsonSchemaDescription("Require SSL mode.")
+class SslModeRequire : SslMode {
+    companion object {
+        const val MODE = "require"
+    }
+    override val mode: String = MODE
+}
+
+@JsonSchemaTitle("verify-ca")
+@JsonSchemaDescription("Verify-ca SSL mode.")
+class SslModeVerifyCa : SslMode {
+    companion object {
+        const val MODE = "verify-ca"
+    }
+    override val mode: String = MODE
+}
+
+@JsonSchemaTitle("verify-full")
+@JsonSchemaDescription("Verify-full SSL mode.")
+class SslModeVerifyFull : SslMode {
+    companion object {
+        const val MODE = "verify-full"
+    }
+    override val mode: String = MODE
+}
+
+enum class CdcDeletionMode(@Suppress("unused") @get:JsonValue val cdcDeletionMode: String) {
+    HARD_DELETE("Hard delete"),
+    SOFT_DELETE("Soft delete"),
+}
+
+@Singleton
+class PostgresSpecificationExtension : DestinationSpecificationExtension {
+    override val supportedSyncModes =
+        listOf(
+            DestinationSyncMode.OVERWRITE,
+            DestinationSyncMode.APPEND,
+            DestinationSyncMode.APPEND_DEDUP,
+        )
+    override val supportsIncremental = true
+    override val groups =
+        listOf(
+            DestinationSpecificationExtension.Group("connection", "Connection"),
+            DestinationSpecificationExtension.Group("sync_behavior", "Sync Behavior"),
+            DestinationSpecificationExtension.Group("advanced", "Advanced"),
+        )
+}
