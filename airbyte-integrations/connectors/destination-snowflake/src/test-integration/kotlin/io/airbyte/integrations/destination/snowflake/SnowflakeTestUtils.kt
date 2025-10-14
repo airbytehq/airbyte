7294ab0815819929d9dcc61fe13ae8bced2864c5
Edward Gao <edward.gao@airbyte.io>
2025-10-14 08:04:02 -0700

    Destination Snowflake: fix config migrator edge case; add tests for QUOTED_IDENTIFIERS_IGNORE_CASE (#67721)
    closes https://github.com/airbytehq/airbyte-internal-issues/issues/14843
    
    Review each commit separately, they're pretty self-contained.

--- airbyte-integrations/connectors/destination-snowflake/src/test-integration/kotlin/io/airbyte/integrations/destination/snowflake/SnowflakeTestUtils.kt
+++ airbyte-integrations/connectors/destination-snowflake/src/test-integration/kotlin/io/airbyte/integrations/destination/snowflake/SnowflakeTestUtils.kt
@@ -9,6 +9,17 @@ import java.nio.file.Path
 
 object SnowflakeTestUtils {
     const val CONFIG_WITH_AUTH_STAGING = "secrets/config_with_auth_staging.json"
+
+    /**
+     * This config uses user `INTEGRATION_TEST_USER_DESTINATION_QUOTED_IDENTIFIERS_IGNORE_CASE`,
+     * which has QUOTED_IDENTIFIERS_IGNORE_CASE=true.
+     *
+     * This parameter causes Snowflake to upcase all identifiers, regardless of quoting. We've seen
+     * this parameter cause problems in the past (e.g. a simple `select count(*) as "total"` will
+     * return a ResultSet with `TOTAL` as the column name rather than `total`), so run all our tests
+     * against this config.
+     */
+    const val CONFIG_WITH_AUTH_STAGING_IGNORE_CASING = "secrets/1s1t_case_insensitive.json"
     const val CONFIG_WITH_AUTH_STAGING_AND_RAW_OVERRIDE =
         "secrets/config_with_auth_staging_and_raw_override.json"
 
