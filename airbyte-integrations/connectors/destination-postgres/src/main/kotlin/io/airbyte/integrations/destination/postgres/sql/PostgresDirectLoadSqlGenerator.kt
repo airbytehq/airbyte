c2a53d8acf7fc3160bd166677bb168c52bace36d
Jose Pefaur <jose.pefaur@gmail.com>
2025-10-16 09:51:59 -0500

    feat: add basic operations to postgres destination sql generator (#68122)
    ## What
    https://github.com/airbytehq/airbyte-internal-issues/issues/14722
    
    I'm adding the following operations to the postgres sql generator:
    -  Create table
    -  Create namespace
    -  Generation ID
    -  Copy table
    -  Overwrite table
    -  Drop table
    -  Count table

--- airbyte-integrations/connectors/destination-postgres/src/main/kotlin/io/airbyte/integrations/destination/postgres/sql/PostgresDirectLoadSqlGenerator.kt
+++ airbyte-integrations/connectors/destination-postgres/src/main/kotlin/io/airbyte/integrations/destination/postgres/sql/PostgresDirectLoadSqlGenerator.kt
@@ -5,33 +5,141 @@
 package io.airbyte.integrations.destination.postgres.sql
 
 import io.airbyte.cdk.load.command.DestinationStream
+import io.airbyte.cdk.load.data.AirbyteType
+import io.airbyte.cdk.load.data.ArrayType
+import io.airbyte.cdk.load.data.ArrayTypeWithoutSchema
+import io.airbyte.cdk.load.data.BooleanType
+import io.airbyte.cdk.load.data.DateType
+import io.airbyte.cdk.load.data.IntegerType
+import io.airbyte.cdk.load.data.NumberType
+import io.airbyte.cdk.load.data.ObjectType
+import io.airbyte.cdk.load.data.ObjectTypeWithEmptySchema
+import io.airbyte.cdk.load.data.ObjectTypeWithoutSchema
+import io.airbyte.cdk.load.data.StringType
+import io.airbyte.cdk.load.data.TimeTypeWithTimezone
+import io.airbyte.cdk.load.data.TimeTypeWithoutTimezone
+import io.airbyte.cdk.load.data.TimestampTypeWithTimezone
+import io.airbyte.cdk.load.data.TimestampTypeWithoutTimezone
+import io.airbyte.cdk.load.data.UnionType
+import io.airbyte.cdk.load.data.UnknownType
+import io.airbyte.cdk.load.message.Meta.Companion.COLUMN_NAME_AB_EXTRACTED_AT
+import io.airbyte.cdk.load.message.Meta.Companion.COLUMN_NAME_AB_GENERATION_ID
+import io.airbyte.cdk.load.message.Meta.Companion.COLUMN_NAME_AB_META
+import io.airbyte.cdk.load.message.Meta.Companion.COLUMN_NAME_AB_RAW_ID
 import io.airbyte.cdk.load.orchestration.db.ColumnNameMapping
 import io.airbyte.cdk.load.orchestration.db.TableName
+import io.github.oshai.kotlinlogging.KotlinLogging
 import jakarta.inject.Singleton
+
+internal const val COUNT_TOTAL_ALIAS = "total"
+
+private val log = KotlinLogging.logger {}
 
 @Singleton
 class PostgresDirectLoadSqlGenerator {
+    companion object {
+        internal val DEFAULT_COLUMNS =
+            listOf(
+                ColumnAndType(
+                    columnName = COLUMN_NAME_AB_RAW_ID,
+                    columnTypeName = PostgresDataType.VARCHAR.typeName,
+                    nullable = false
 
-    @Suppress("UNUSED_PARAMETER")
+                ),
+                ColumnAndType(
+                    columnName = COLUMN_NAME_AB_EXTRACTED_AT,
+                    columnTypeName = PostgresDataType.TIMESTAMP_WITH_TIMEZONE.typeName,
+                    nullable = false
+                ),
+                ColumnAndType(
+                    columnName = COLUMN_NAME_AB_META,
+                    columnTypeName = PostgresDataType.JSONB.typeName,
+                    nullable = false
+                ),
+                ColumnAndType(
+                    columnName = COLUMN_NAME_AB_GENERATION_ID,
+                    columnTypeName = PostgresDataType.BIGINT.typeName,
+                    nullable = false
+                ),
+            )
+
+        /**
+         * This extension is here to avoid writing `.also { log.info { it }}` for every returned string
+         * we want to log
+         */
+        fun String.andLog(): String {
+            log.info { this.trim() }
+            return this
+        }
+    }
+
     fun createTable(
         stream: DestinationStream,
         tableName: TableName,
         columnNameMapping: ColumnNameMapping,
         replace: Boolean
-    ): String = TODO("PostgresDirectLoadSqlGenerator.createTable not yet implemented")
+    ): String {
+        val columnDeclarations = columnsAndTypes(stream, columnNameMapping)
+        val dropTableIfExistsStatement = if (replace) "DROP TABLE IF EXISTS ${getFullyQualifiedName(tableName)};" else ""
+        return """
+            BEGIN TRANSACTION;
+            $dropTableIfExistsStatement
+            CREATE TABLE ${getFullyQualifiedName(tableName)} (
+                $columnDeclarations
+            );
+            COMMIT;
+            """
+            .trimIndent()
+            .andLog()
+    }
 
-    @Suppress("UNUSED_PARAMETER")
+    private fun columnsAndTypes(
+        stream: DestinationStream,
+        columnNameMapping: ColumnNameMapping
+    ): String {
+        val targetColumns = stream.schema
+            .asColumns()
+            .map { (columnName, columnType) ->
+                val targetColumnName = columnNameMapping[columnName] ?: columnName
+                val typeName = columnType.type.toDialectType()
+                "\"$targetColumnName\" $typeName"
+            }
+
+        return (DEFAULT_COLUMNS + targetColumns).joinToString(",\n")
+    }
+
     fun overwriteTable(
         sourceTableName: TableName,
         targetTableName: TableName
-    ): String = TODO("PostgresDirectLoadSqlGenerator.overwriteTable not yet implemented")
+    ): String {
+        return """
+            BEGIN TRANSACTION;
+            DROP TABLE IF EXISTS ${getFullyQualifiedName(targetTableName)};
+            ALTER TABLE ${getFullyQualifiedName(sourceTableName)} RENAME TO ${getFullyQualifiedName(targetTableName)};
+            COMMIT;
+            """
+            .trimIndent()
+            .andLog()
+    }
 
-    @Suppress("UNUSED_PARAMETER")
     fun copyTable(
         columnNameMapping: ColumnNameMapping,
         sourceTableName: TableName,
         targetTableName: TableName
-    ): String = TODO("PostgresDirectLoadSqlGenerator.copyTable not yet implemented")
+    ): String {
+        val columnNames = getTargetColumnNames(columnNameMapping).joinToString(",")
+        return """
+            INSERT INTO ${getFullyQualifiedName(targetTableName)} ($columnNames)
+            SELECT $columnNames
+            FROM ${getFullyQualifiedName(sourceTableName)};
+            """
+            .trimIndent()
+            .andLog()
+
+    }
+
+    private fun getTargetColumnNames(columnNameMapping: ColumnNameMapping): List<String> =
+        DEFAULT_COLUMNS.map { "\"${it.columnName}\"" } + columnNameMapping.map { (_, targetName) -> "\"${targetName}\"" }
 
     @Suppress("UNUSED_PARAMETER")
     fun upsertTable(
@@ -41,21 +149,19 @@ class PostgresDirectLoadSqlGenerator {
         targetTableName: TableName
     ): String = TODO("PostgresDirectLoadSqlGenerator.upsertTable not yet implemented")
 
-    @Suppress("UNUSED_PARAMETER")
     fun dropTable(tableName: TableName): String =
-        TODO("PostgresDirectLoadSqlGenerator.dropTable not yet implemented")
+        "DROP TABLE IF EXISTS ${getFullyQualifiedName(tableName)};".andLog()
 
-    @Suppress("UNUSED_PARAMETER")
-    fun countTable(tableName: TableName): String =
-        TODO("PostgresDirectLoadSqlGenerator.countTable not yet implemented")
+    fun countTable(tableName: TableName): String {
+        return "SELECT COUNT(*) AS \"$COUNT_TOTAL_ALIAS\" FROM ${getFullyQualifiedName(tableName)};".andLog()
+    }
 
-    @Suppress("UNUSED_PARAMETER")
-    fun createNamespace(namespace: String): String =
-        TODO("PostgresDirectLoadSqlGenerator.createNamespace not yet implemented")
+    fun createNamespace(namespace: String): String {
+        return "CREATE SCHEMA IF NOT EXISTS \"$namespace\";".andLog()
+    }
 
-    @Suppress("UNUSED_PARAMETER")
     fun getGenerationId(tableName: TableName): String =
-        TODO("PostgresDirectLoadSqlGenerator.getGenerationId not yet implemented")
+        "SELECT \"${COLUMN_NAME_AB_GENERATION_ID}\" FROM ${getFullyQualifiedName(tableName)} LIMIT 1;".andLog()
 
     fun showColumns(tableName: TableName): String =
         """
@@ -72,4 +178,40 @@ class PostgresDirectLoadSqlGenerator {
         FROM '$filePath'
         WITH (FORMAT csv)
         """.trimIndent()
+
+    private fun getFullyQualifiedName(tableName: TableName): String =
+        "${getNamespace(tableName)}.${getName(tableName)}"
+
+    private fun getNamespace(tableName: TableName): String =
+        "\"${tableName.namespace}\""
+
+    private fun getName(tableName: TableName): String =
+        "\"${tableName.name}\""
+
+    fun AirbyteType.toDialectType(): String =
+        when (this) {
+            BooleanType -> PostgresDataType.BOOLEAN.typeName
+            DateType -> PostgresDataType.DATE.typeName
+            IntegerType -> PostgresDataType.BIGINT.typeName
+            NumberType -> PostgresDataType.DECIMAL.typeName
+            StringType -> PostgresDataType.VARCHAR.typeName
+            TimeTypeWithTimezone -> PostgresDataType.TIME_WITH_TIMEZONE.typeName
+            TimeTypeWithoutTimezone -> PostgresDataType.TIME.typeName
+            TimestampTypeWithTimezone -> PostgresDataType.TIMESTAMP_WITH_TIMEZONE.typeName
+            TimestampTypeWithoutTimezone -> PostgresDataType.TIMESTAMP.typeName
+            is ArrayType,
+            ArrayTypeWithoutSchema,
+            is ObjectType,
+            ObjectTypeWithEmptySchema,
+            ObjectTypeWithoutSchema,
+            is UnknownType -> PostgresDataType.JSONB.typeName
+            is UnionType -> this.chooseType().toDialectType()
+        }
+}
+
+data class ColumnAndType(val columnName: String, val columnTypeName: String, val nullable: Boolean = false) {
+    override fun toString(): String {
+        val isNullableSuffix = if (nullable) "" else "NOT NULL"
+        return "\"$columnName\" $columnTypeName $isNullableSuffix".trim()
+    }
 }
---
+++ airbyte-integrations/connectors/destination-postgres/src/test/kotlin/io/airbyte/integrations/destination/postgres/sql/PostgresDirectLoadSqlGeneratorTest.kt
@@ -0,0 +1,265 @@
+/*
+ * Copyright (c) 2025 Airbyte, Inc., all rights reserved.
+ */
+
+package io.airbyte.integrations.destination.postgres.sql
+
+import com.fasterxml.jackson.databind.JsonNode
+import io.airbyte.cdk.load.command.DestinationStream
+import io.airbyte.cdk.load.data.ArrayType
+import io.airbyte.cdk.load.data.ArrayTypeWithoutSchema
+import io.airbyte.cdk.load.data.BooleanType
+import io.airbyte.cdk.load.data.DateType
+import io.airbyte.cdk.load.data.FieldType
+import io.airbyte.cdk.load.data.IntegerType
+import io.airbyte.cdk.load.data.NumberType
+import io.airbyte.cdk.load.data.ObjectType
+import io.airbyte.cdk.load.data.ObjectTypeWithEmptySchema
+import io.airbyte.cdk.load.data.ObjectTypeWithoutSchema
+import io.airbyte.cdk.load.data.StringType
+import io.airbyte.cdk.load.data.TimeTypeWithTimezone
+import io.airbyte.cdk.load.data.TimeTypeWithoutTimezone
+import io.airbyte.cdk.load.data.TimestampTypeWithTimezone
+import io.airbyte.cdk.load.data.TimestampTypeWithoutTimezone
+import io.airbyte.cdk.load.data.UnionType
+import io.airbyte.cdk.load.data.UnknownType
+import io.airbyte.cdk.load.orchestration.db.ColumnNameMapping
+import io.airbyte.cdk.load.orchestration.db.TableName
+import io.mockk.every
+import io.mockk.mockk
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+
+internal class PostgresDirectLoadSqlGeneratorTest {
+
+    private lateinit var postgresDirectLoadSqlGenerator: PostgresDirectLoadSqlGenerator
+
+    @BeforeEach
+    fun setUp() {
+        postgresDirectLoadSqlGenerator = PostgresDirectLoadSqlGenerator()
+    }
+
+    @Test
+    fun testCreateTable() {
+        val stream = mockk<DestinationStream> {
+            every { schema } returns ObjectType(
+                properties = linkedMapOf(
+                    "sourceId" to FieldType(StringType, nullable = false),
+                    "sourceName" to FieldType(StringType, nullable = false)
+                )
+            )
+        }
+        val columnNameMapping = ColumnNameMapping(
+            mapOf(
+                "sourceId" to "targetId"
+            )
+        )
+        val tableName = TableName(namespace = "namespace", name = "name")
+
+        val sql = postgresDirectLoadSqlGenerator.createTable(
+            stream = stream,
+            tableName = tableName,
+            columnNameMapping = columnNameMapping,
+            replace = true
+        )
+
+        val expected = """
+            BEGIN TRANSACTION;
+            DROP TABLE IF EXISTS "namespace"."name";
+            CREATE TABLE "namespace"."name" (
+            "_airbyte_raw_id" varchar NOT NULL,
+            "_airbyte_extracted_at" timestamp with time zone NOT NULL,
+            "_airbyte_meta" jsonb NOT NULL,
+            "_airbyte_generation_id" bigint NOT NULL,
+            "targetId" varchar,
+            "sourceName" varchar
+            );
+            COMMIT;
+            """.trimIndent()
+
+        assertEqualsIgnoreWhitespace(expected, sql)
+    }
+
+    private fun assertEqualsIgnoreWhitespace(expected: String, actual: String) {
+        assertEquals(
+            dropWhitespace(expected),
+            dropWhitespace(actual))
+    }
+
+    private fun dropWhitespace(text: String) = text
+        .lines()
+        .map { it.trim()}
+        .filter { it.isNotEmpty() }
+        .joinToString("\n") { it.trim() }
+
+    @Test
+    fun testCreateTableNoReplace() {
+        val stream = mockk<DestinationStream> {
+            every { schema } returns ObjectType(
+                properties = linkedMapOf(
+                    "sourceId" to FieldType(StringType, nullable = false),
+                )
+            )
+        }
+        val columnNameMapping = ColumnNameMapping(
+            mapOf(
+                "sourceId" to "targetId",
+            )
+        )
+        val tableName = TableName(namespace = "namespace", name = "name")
+
+        val sql = postgresDirectLoadSqlGenerator.createTable(
+            stream = stream,
+            tableName = tableName,
+            columnNameMapping = columnNameMapping,
+            replace = false
+        )
+
+        val expected = """
+            BEGIN TRANSACTION;
+            CREATE TABLE "namespace"."name" (
+            "_airbyte_raw_id" varchar NOT NULL,
+            "_airbyte_extracted_at" timestamp with time zone NOT NULL,
+            "_airbyte_meta" jsonb NOT NULL,
+            "_airbyte_generation_id" bigint NOT NULL,
+            "targetId" varchar
+            );
+            COMMIT;
+            """.trimIndent()
+
+        assertEqualsIgnoreWhitespace(expected, sql)
+    }
+
+    @Test
+    fun testOverwriteTable() {
+        val sourceTableName = TableName(namespace = "sourceNamespace", name = "source")
+        val targetTableName = TableName(namespace = "targetNamespace", name = "target")
+        val sql = postgresDirectLoadSqlGenerator.overwriteTable(sourceTableName, targetTableName)
+
+        val expected = """
+            BEGIN TRANSACTION;
+            DROP TABLE IF EXISTS "targetNamespace"."target";
+            ALTER TABLE "sourceNamespace"."source" RENAME TO "targetNamespace"."target";
+            COMMIT;
+        """.trimIndent()
+
+        assertEquals(expected, sql)
+    }
+
+    @Test
+    fun testGenerateCopyTable() {
+        val columnNameMapping = ColumnNameMapping(
+            mapOf(
+                "sourceId" to "targetId"
+            )
+        )
+        val sourceTableName = TableName(namespace = "namespace", name = "source")
+        val destinationTableName = TableName(namespace = "namespace", name = "target")
+
+        val sql = postgresDirectLoadSqlGenerator.copyTable(
+            columnNameMapping = columnNameMapping,
+            sourceTableName = sourceTableName,
+            targetTableName = destinationTableName
+        )
+
+        val expected = """
+            INSERT INTO "namespace"."target" ("_airbyte_raw_id","_airbyte_extracted_at","_airbyte_meta","_airbyte_generation_id","targetId")
+            SELECT "_airbyte_raw_id","_airbyte_extracted_at","_airbyte_meta","_airbyte_generation_id","targetId"
+            FROM "namespace"."source";
+        """.trimIndent()
+
+        assertEquals(expected, sql)
+    }
+
+    @Test
+    fun testDropTable() {
+        val tableName = TableName(namespace = "namespace", name = "name")
+        val sql = postgresDirectLoadSqlGenerator.dropTable(tableName)
+        assertEquals("DROP TABLE IF EXISTS \"namespace\".\"name\";", sql)
+    }
+
+    @Test
+    fun testCountTable() {
+        val tableName = TableName(namespace = "namespace", name = "name")
+        val sql = postgresDirectLoadSqlGenerator.countTable(tableName)
+        assertEquals(
+            "SELECT COUNT(*) AS \"total\" FROM \"namespace\".\"name\";",
+            sql
+        )
+    }
+
+    @Test
+    fun testCreateNamespace() {
+        val namespace = "namespace"
+        val sql = postgresDirectLoadSqlGenerator.createNamespace(namespace)
+        assertEquals(
+            "CREATE SCHEMA IF NOT EXISTS \"namespace\";",
+            sql
+        )
+    }
+
+    @Test
+    fun testGetGenerationId() {
+        val tableName = TableName(namespace = "namespace", name = "name")
+        val sql = postgresDirectLoadSqlGenerator.getGenerationId(tableName = tableName)
+        assertEquals(
+            "SELECT \"_airbyte_generation_id\" FROM \"namespace\".\"name\" LIMIT 1;",
+            sql
+        )
+    }
+
+    @Test
+    fun testToDialectTypeMapping() {
+        with(postgresDirectLoadSqlGenerator) {
+            assertEquals("boolean", BooleanType.toDialectType())
+            assertEquals("date", DateType.toDialectType())
+            assertEquals("bigint", IntegerType.toDialectType())
+            assertEquals("decimal", NumberType.toDialectType())
+            assertEquals("varchar", StringType.toDialectType())
+            assertEquals("time with time zone", TimeTypeWithTimezone.toDialectType())
+            assertEquals("time", TimeTypeWithoutTimezone.toDialectType())
+            assertEquals("timestamp with time zone", TimestampTypeWithTimezone.toDialectType())
+            assertEquals("timestamp", TimestampTypeWithoutTimezone.toDialectType())
+
+            assertEquals("jsonb", ArrayType(items = FieldType(StringType, false)).toDialectType())
+            assertEquals("jsonb", ArrayTypeWithoutSchema.toDialectType())
+            assertEquals("jsonb", ObjectType(linkedMapOf()).toDialectType())
+            assertEquals("jsonb", ObjectTypeWithEmptySchema.toDialectType())
+            assertEquals("jsonb", ObjectTypeWithoutSchema.toDialectType())
+            assertEquals("jsonb", UnknownType(mockk<JsonNode>()).toDialectType())
+        }
+    }
+
+    @Test
+    fun testToDialectTypeMappingUnions() {
+        with(postgresDirectLoadSqlGenerator) {
+            val unionWithStruct = UnionType(
+                options = setOf(
+                    StringType,
+                    ObjectType(linkedMapOf("field" to FieldType(StringType, nullable = false)))
+                ),
+                isLegacyUnion = true
+            )
+            assertEquals("jsonb", unionWithStruct.toDialectType())
+
+            val unionWithBasicTypes = UnionType(
+                options = setOf(StringType, IntegerType),
+                isLegacyUnion = true
+            )
+            assertEquals("varchar", unionWithBasicTypes.toDialectType())
+        }
+    }
+
+    @Test
+    fun testColumnAndTypeToString() {
+        val notNullColumn = ColumnAndType("column", "varchar", nullable = false)
+        assertEquals("\"column\" varchar NOT NULL", notNullColumn.toString())
+    }
+
+    @Test
+    fun testNullableColumnAndTypeToString() {
+        val nullableColumn = ColumnAndType("column", "varchar", nullable = true)
+        assertEquals("\"column\" varchar", nullableColumn.toString())
+    }
+}
