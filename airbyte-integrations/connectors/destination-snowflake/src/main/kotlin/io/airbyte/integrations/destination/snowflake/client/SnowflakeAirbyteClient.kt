2e7b1e74ba789d73cea3b1e2d48c8a013923646b
Edward Gao <edward.gao@airbyte.io>
2025-10-20 11:44:25 -0700

    connectors code

--- airbyte-integrations/connectors/destination-snowflake/src/main/kotlin/io/airbyte/integrations/destination/snowflake/client/SnowflakeAirbyteClient.kt
+++ airbyte-integrations/connectors/destination-snowflake/src/main/kotlin/io/airbyte/integrations/destination/snowflake/client/SnowflakeAirbyteClient.kt
@@ -8,14 +8,9 @@ import edu.umd.cs.findbugs.annotations.SuppressFBWarnings
 import io.airbyte.cdk.load.command.DestinationStream
 import io.airbyte.cdk.load.component.TableOperationsClient
 import io.airbyte.cdk.load.component.TableSchemaEvolutionClient
-import io.airbyte.cdk.load.data.AirbyteValue
-import io.airbyte.cdk.load.dataflow.state.PartitionKey
-import io.airbyte.cdk.load.dataflow.transform.RecordDTO
 import io.airbyte.cdk.load.table.ColumnNameMapping
 import io.airbyte.cdk.load.table.TableName
-import io.airbyte.cdk.load.util.Jsons
 import io.airbyte.cdk.load.util.deserializeToNode
-import io.airbyte.integrations.destination.snowflake.dataflow.SnowflakeAggregate
 import io.airbyte.integrations.destination.snowflake.db.ColumnDefinition
 import io.airbyte.integrations.destination.snowflake.db.escapeJsonIdentifier
 import io.airbyte.integrations.destination.snowflake.db.toSnowflakeCompatibleName
@@ -24,14 +19,11 @@ import io.airbyte.integrations.destination.snowflake.sql.COUNT_TOTAL_ALIAS
 import io.airbyte.integrations.destination.snowflake.sql.SnowflakeColumnUtils
 import io.airbyte.integrations.destination.snowflake.sql.SnowflakeDirectLoadSqlGenerator
 import io.airbyte.integrations.destination.snowflake.sql.andLog
-import io.airbyte.integrations.destination.snowflake.write.load.SnowflakeInsertBuffer
 import io.github.oshai.kotlinlogging.KotlinLogging
 import jakarta.inject.Singleton
 import java.sql.ResultSet
-import java.time.format.DateTimeFormatter
 import javax.sql.DataSource
 import net.snowflake.client.jdbc.SnowflakeSQLException
-import net.snowflake.client.jdbc.SnowflakeTimestampWithTimezone
 
 internal const val DESCRIBE_TABLE_COLUMN_NAME_FIELD = "column_name"
 internal const val DESCRIBE_TABLE_COLUMN_TYPE_FIELD = "data_type"
@@ -116,10 +108,6 @@ class SnowflakeAirbyteClient(
             // Create the schema only if it doesn't exist
             execute(sqlGenerator.createNamespace(namespace))
         }
-    }
-
-    override suspend fun dropNamespace(namespace: String) {
-        execute(sqlGenerator.dropNamespace(namespace))
     }
 
     override suspend fun createTable(
@@ -311,84 +299,6 @@ class SnowflakeAirbyteClient(
             0L
         }
 
-    override suspend fun insertRecords(table: TableName, records: List<Map<String, AirbyteValue>>) {
-        val a =
-            SnowflakeAggregate(
-                SnowflakeInsertBuffer(
-                    table,
-                    describeTable(table),
-                    this,
-                    snowflakeConfiguration,
-                    snowflakeColumnUtils,
-                )
-            )
-        records.forEach { a.accept(RecordDTO(it, PartitionKey(""), 0, 0)) }
-        a.flush()
-    }
-
-    override suspend fun readTable(table: TableName): List<Map<String, Any>> {
-        dataSource.connection.use { connection ->
-            connection.createStatement().use { statement ->
-                statement
-                    .executeQuery("""SELECT * FROM "${table.namespace}"."${table.name}";""")
-                    .use { resultSet ->
-                        val metaData = resultSet.metaData
-                        val columnCount = metaData.columnCount
-                        val result = mutableListOf<Map<String, Any>>()
-
-                        while (resultSet.next()) {
-                            val row = mutableMapOf<String, Any>()
-                            for (i in 1..columnCount) {
-                                val columnName = metaData.getColumnName(i)
-                                val columnType = metaData.getColumnTypeName(i)
-                                when (columnType) {
-                                    "TIMESTAMPTZ" -> {
-                                        val value =
-                                            resultSet.getTimestamp(i)
-                                                as SnowflakeTimestampWithTimezone?
-                                        if (value != null) {
-                                            val formattedTimestamp =
-                                                DateTimeFormatter.ISO_DATE_TIME.format(
-                                                    value.toZonedDateTime().toOffsetDateTime()
-                                                )
-                                            row[columnName] = formattedTimestamp
-                                        }
-                                    }
-                                    "VARIANT",
-                                    "OBJECT",
-                                    "ARRAY" -> {
-                                        val stringValue: String? = resultSet.getString(i)
-                                        if (stringValue != null) {
-                                            // Automatically convert values to their native type.
-                                            // (map, list, etc.)
-                                            val parsedValue =
-                                                Jsons.readValue(stringValue, Any::class.java)
-                                            // but handle some annoying edge cases
-                                            val actualValue =
-                                                when (parsedValue) {
-                                                    is Integer -> parsedValue.toLong()
-                                                    else -> parsedValue
-                                                }
-                                            row[columnName] = actualValue
-                                        }
-                                    }
-                                    else -> {
-                                        val value = resultSet.getObject(i)
-                                        if (value != null) {
-                                            row[columnName] = value
-                                        }
-                                    }
-                                }
-                            }
-                            result.add(row)
-                        }
-
-                        return result
-                    }
-            }
-        }
-    }
-
     fun createSnowflakeStage(tableName: TableName) {
         execute(sqlGenerator.createSnowflakeStage(tableName))
     }
@@ -426,8 +336,10 @@ class SnowflakeAirbyteClient(
             }
         }
 
-    internal fun execute(query: String) =
-        dataSource.connection.use { connection ->
-            connection.createStatement().use { it.executeQuery(query) }
-        }
+    internal fun execute(query: String) = dataSource.execute(query)
 }
+
+fun DataSource.execute(query: String): ResultSet =
+    this.connection.use { connection ->
+        connection.createStatement().use { it.executeQuery(query) }
+    }
