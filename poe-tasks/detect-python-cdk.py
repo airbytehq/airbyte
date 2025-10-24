6096462697ce69d96b32be664c7f73aa2f3fd831
David Gold <32782137+dbgold17@users.noreply.github.com>
2025-10-24 11:11:17 -0700

    make minor and patch numbers optional in regex

--- poe-tasks/detect-python-cdk.py
+++ poe-tasks/detect-python-cdk.py
@@ -100,7 +100,7 @@ def is_prerelease_version(version_str) -> bool:
     if not version_str:
         return True
 
-    version_pattern = r"^[~^>=<]*\d+\.\d+\.\d+([a-zA-Z0-9\-\.]*)?$"
+    version_pattern = r"^[~^>=<]*\d+(?:\.\d+)?(?:\.\d+)?([a-zA-Z0-9\-\.]*)?$"
     is_prod_version = bool(re.match(version_pattern, version_str.strip()))
     return not is_prod_version
 
