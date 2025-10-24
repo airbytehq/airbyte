e2597eb19c506f1295d268f9a7e240d3062623a2
maxime.c <maxime@airbyte.io>
2025-10-24 11:27:45 -0400

    add mock server tests

---
+++ airbyte-integrations/connectors/source-zendesk-chat/unit_tests/pagination_strategy.py
@@ -0,0 +1,13 @@
+from typing import Any, Dict, List
+
+from airbyte_cdk.test.mock_http.response_builder import PaginationStrategy
+
+
+class ZendeskChatPaginationStrategy(PaginationStrategy):
+    def __init__(self, entry_field: str, next_page_url: str):
+        self._entry_field = entry_field
+        self._next_page_url = next_page_url
+
+    def update(self, response: Dict[str, Any]) -> None:
+        response["count"] = 1000
+        response["next_page"] = self._next_page_url
