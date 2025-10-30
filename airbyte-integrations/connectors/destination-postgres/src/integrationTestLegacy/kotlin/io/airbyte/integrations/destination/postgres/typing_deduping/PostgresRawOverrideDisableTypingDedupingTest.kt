c5f84beaab8b6fad250d09c82de77c35a7c4470a
Jose Pefaur <jose.pefaur@gmail.com>
2025-10-30 10:23:25 -0500

    disable some tests

--- airbyte-integrations/connectors/destination-postgres/src/integrationTestLegacy/kotlin/io/airbyte/integrations/destination/postgres/typing_deduping/PostgresRawOverrideDisableTypingDedupingTest.kt
+++ airbyte-integrations/connectors/destination-postgres/src/integrationTestLegacy/kotlin/io/airbyte/integrations/destination/postgres/typing_deduping/PostgresRawOverrideDisableTypingDedupingTest.kt
@@ -3,9 +3,18 @@
  */
 package io.airbyte.integrations.destination.postgres.typing_deduping
 
+import com.fasterxml.jackson.databind.JsonNode
 import com.fasterxml.jackson.databind.node.ObjectNode
+import io.airbyte.protocol.models.v0.AirbyteStream
+import io.airbyte.protocol.models.v0.ConfiguredAirbyteStream
+import io.airbyte.protocol.models.v0.DestinationSyncMode
+import io.airbyte.protocol.models.v0.SyncMode
+import kotlin.collections.forEach
+import kotlin.longArrayOf
 import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
 
 class PostgresRawOverrideDisableTypingDedupingTest : PostgresTypingDedupingTest() {
     override fun getBaseConfig(): ObjectNode {
@@ -24,4 +33,8 @@ class PostgresRawOverrideDisableTypingDedupingTest : PostgresTypingDedupingTest(
     @Disabled @Test override fun identicalNameSimultaneousSync() {}
 
     @Disabled @Test override fun testVarcharLimitOver64K() {}
+
+    @Disabled @Test override fun interruptedTruncateWithPriorData() {}
+
+//    @Disabled @ParameterizedTest @ValueSource(longs = [0L, 42L]) override fun testIncrementalSyncDropOneColumn(inputGenerationId: Long) {}
 }
