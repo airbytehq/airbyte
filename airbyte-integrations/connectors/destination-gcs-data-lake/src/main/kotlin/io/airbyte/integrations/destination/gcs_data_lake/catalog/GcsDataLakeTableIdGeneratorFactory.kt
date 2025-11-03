c679328c77e5ba36756eeb7f59c9fe66a0f72727
Francis Genet <francis.genet@airbyte.io>
2025-11-03 13:47:04 -0800

    Save

--- airbyte-integrations/connectors/destination-gcs-data-lake/src/main/kotlin/io/airbyte/integrations/destination/gcs_data_lake/catalog/GcsDataLakeTableIdGeneratorFactory.kt
+++ airbyte-integrations/connectors/destination-gcs-data-lake/src/main/kotlin/io/airbyte/integrations/destination/gcs_data_lake/catalog/GcsDataLakeTableIdGeneratorFactory.kt
@@ -4,16 +4,42 @@
 
 package io.airbyte.integrations.destination.gcs_data_lake.catalog
 
-import io.airbyte.cdk.load.toolkits.iceberg.parquet.SimpleTableIdGenerator
+import io.airbyte.cdk.load.command.DestinationStream
+import io.airbyte.cdk.load.data.Transformations
 import io.airbyte.cdk.load.toolkits.iceberg.parquet.TableIdGenerator
+import io.airbyte.cdk.load.toolkits.iceberg.parquet.tableIdOf
 import io.airbyte.integrations.destination.gcs_data_lake.spec.GcsDataLakeConfiguration
 import io.micronaut.context.annotation.Factory
 import javax.inject.Singleton
+import org.apache.iceberg.catalog.TableIdentifier
+
+/**
+ * BigLake table ID generator that sanitizes names.
+ * BigLake doesn't support:
+ * - Special characters (converts to alphanumeric+underscore)
+ * - Table names starting with numbers (prefixes with underscore)
+ */
+class BigLakeTableIdGenerator(private val databaseName: String) : TableIdGenerator {
+    override fun toTableIdentifier(stream: DestinationStream.Descriptor): TableIdentifier {
+        val namespace = sanitizeName(stream.namespace ?: databaseName)
+        val name = sanitizeName(stream.name)
+        return tableIdOf(namespace, name)
+    }
+
+    private fun sanitizeName(name: String): String {
+        var sanitized = Transformations.toAlphanumericAndUnderscore(name)
+        // BigLake/BigQuery doesn't allow table names starting with numbers
+        // Prefix with underscore if it starts with a digit
+        if (sanitized.isNotEmpty() && sanitized[0].isDigit()) {
+            sanitized = "_$sanitized"
+        }
+        return sanitized
+    }
+}
 
 @Factory
-class GcsDataLakeTableIdGeneratorFactory(
-    private val gcsDataLakeConfiguration: GcsDataLakeConfiguration
-) {
+class GcsDataLakeTableIdGeneratorFactory(private val gcsDataLakeConfiguration: GcsDataLakeConfiguration) {
     @Singleton
-    fun create(): TableIdGenerator = SimpleTableIdGenerator(gcsDataLakeConfiguration.databaseName)
+    fun create(): TableIdGenerator =
+        BigLakeTableIdGenerator(gcsDataLakeConfiguration.databaseName)
 }
