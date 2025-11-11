9fa9c0bd760b0852ed57886f42f95526c674aa6d
Francis Genet <francis.genet@airbyte.io>
2025-11-11 14:20:31 -0800

    format

--- airbyte-integrations/connectors/destination-gcs-data-lake/src/main/kotlin/io/airbyte/integrations/destination/gcs_data_lake/GcsDataLakeBeanFactory.kt
+++ airbyte-integrations/connectors/destination-gcs-data-lake/src/main/kotlin/io/airbyte/integrations/destination/gcs_data_lake/GcsDataLakeBeanFactory.kt
@@ -1,20 +1,23 @@
+/*
+ * Copyright (c) 2025 Airbyte, Inc., all rights reserved.
+ */
+
 package io.airbyte.integrations.destination.gcs_data_lake
 
 import io.airbyte.cdk.load.dataflow.config.AggregatePublishingConfig
 import io.micronaut.context.annotation.Factory
 import jakarta.inject.Singleton
 
-
 @Factory
 class SnowflakeBeanFactory {
     @Singleton
     fun aggregatePublishingConfig(): AggregatePublishingConfig {
-            // NOT speed mode
-            return AggregatePublishingConfig(
-                maxRecordsPerAgg = 100_000L,
-                maxEstBytesPerAgg = 50_000_000L,
-                maxEstBytesAllAggregates = 250_000_000L,
-                maxBufferedAggregates = 5,
-            )
-        }
+        // NOT speed mode
+        return AggregatePublishingConfig(
+            maxRecordsPerAgg = 100_000L,
+            maxEstBytesPerAgg = 50_000_000L,
+            maxEstBytesAllAggregates = 250_000_000L,
+            maxBufferedAggregates = 5,
+        )
+    }
 }
