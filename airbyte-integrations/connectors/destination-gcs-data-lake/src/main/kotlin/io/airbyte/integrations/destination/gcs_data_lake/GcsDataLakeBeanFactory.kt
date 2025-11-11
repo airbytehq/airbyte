6eb3a8d429d0022d0a9b364074a881b95f75dda9
Francis Genet <francis.genet@airbyte.io>
2025-11-11 14:15:03 -0800

    Adding that factory

---
+++ airbyte-integrations/connectors/destination-gcs-data-lake/src/main/kotlin/io/airbyte/integrations/destination/gcs_data_lake/GcsDataLakeBeanFactory.kt
@@ -0,0 +1,20 @@
+package io.airbyte.integrations.destination.gcs_data_lake
+
+import io.airbyte.cdk.load.dataflow.config.AggregatePublishingConfig
+import io.micronaut.context.annotation.Factory
+import jakarta.inject.Singleton
+
+
+@Factory
+class SnowflakeBeanFactory {
+    @Singleton
+    fun aggregatePublishingConfig(): AggregatePublishingConfig {
+            // NOT speed mode
+            return AggregatePublishingConfig(
+                maxRecordsPerAgg = 100_000L,
+                maxEstBytesPerAgg = 50_000_000L,
+                maxEstBytesAllAggregates = 250_000_000L,
+                maxBufferedAggregates = 5,
+            )
+        }
+}
