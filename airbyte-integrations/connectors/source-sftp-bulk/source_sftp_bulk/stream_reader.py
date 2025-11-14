8ce75aeff47009b756e423226c3a732e2f8cbf05
darynaishchenko <darina.ishchenko17@gmail.com>
2025-11-14 16:29:20 +0200

    remove upload method, use uploadable remote file instead in stream reader

--- airbyte-integrations/connectors/source-sftp-bulk/source_sftp_bulk/stream_reader.py
+++ airbyte-integrations/connectors/source-sftp-bulk/source_sftp_bulk/stream_reader.py
@@ -4,26 +4,82 @@
 import datetime
 import logging
 import stat
-import time
 from io import IOBase
-from typing import Iterable, List, Optional, Tuple
+from typing import Any, Iterable, List, Optional
 
 import psutil
-from typing_extensions import override
 
-from airbyte_cdk import FailureType
-from airbyte_cdk.models import AirbyteRecordMessageFileReference
-from airbyte_cdk.sources.file_based.exceptions import FileSizeLimitError
 from airbyte_cdk.sources.file_based.file_based_stream_reader import AbstractFileBasedStreamReader, FileReadMode
-from airbyte_cdk.sources.file_based.file_record_data import FileRecordData
-from airbyte_cdk.sources.file_based.remote_file import RemoteFile
+from airbyte_cdk.sources.file_based.remote_file import UploadableRemoteFile
 from source_sftp_bulk.client import SFTPClient
 from source_sftp_bulk.spec import SourceSFTPBulkSpec
 
 
-class SourceSFTPBulkStreamReader(AbstractFileBasedStreamReader):
-    FILE_SIZE_LIMIT = 1_500_000_000
+class SFTPBulkUploadableRemoteFile(UploadableRemoteFile):
+    sftp_client: Any
+    logger: Any
 
+    def __init__(self, sftp_client: SFTPClient, logger: logging.Logger, **kwargs):
+        super().__init__(**kwargs)
+        self.sftp_client = sftp_client
+        self.logger = logger
+
+    @property
+    def size(self) -> int:
+        file_size = self.sftp_client.sftp_connection.stat(self.uri).st_size
+        return file_size
+
+    def _create_progress_handler(self, local_file_path: str):
+        previous_bytes_copied = 0
+
+        def progress_handler(bytes_copied, total_bytes):
+            nonlocal previous_bytes_copied
+            if bytes_copied - previous_bytes_copied >= 100 * 1024 * 1024:
+                self.logger.info(
+                    f"{bytes_copied / (1024 * 1024):,.2f} MB ({bytes_copied / (1024 * 1024 * 1024):.2f} GB) "
+                    f"of {total_bytes / (1024 * 1024):,.2f} MB ({total_bytes / (1024 * 1024 * 1024):.2f} GB) "
+                    f"written to {local_file_path}"
+                )
+                previous_bytes_copied = bytes_copied
+
+                # Get available disk space
+                disk_usage = psutil.disk_usage("/")
+                available_disk_space = disk_usage.free
+
+                # Get available memory
+                memory_info = psutil.virtual_memory()
+                available_memory = memory_info.available
+                self.logger.info(
+                    f"Available disk space: {available_disk_space / (1024 * 1024):,.2f} MB ({available_disk_space / (1024 * 1024 * 1024):.2f} GB), "
+                    f"available memory: {available_memory / (1024 * 1024):,.2f} MB ({available_memory / (1024 * 1024 * 1024):.2f} GB)."
+                )
+
+        return progress_handler
+
+    def download_to_local_directory(self, local_file_path: str) -> None:
+        # Get available disk space
+        disk_usage = psutil.disk_usage("/")
+        available_disk_space = disk_usage.free
+
+        # Get available memory
+        memory_info = psutil.virtual_memory()
+        available_memory = memory_info.available
+
+        # Log file size, available disk space, and memory
+        file_size = self.size
+        self.logger.info(
+            f"Starting to download the file {self.uri} with size: {file_size / (1024 * 1024):,.2f} MB ({file_size / (1024 * 1024 * 1024):.2f} GB) "
+            f"to '{local_file_path}' "
+            f"with size: {file_size / (1024 * 1024):,.2f} MB ({file_size / (1024 * 1024 * 1024):.2f} GB), "
+            f"available disk space: {available_disk_space / (1024 * 1024):,.2f} MB ({available_disk_space / (1024 * 1024 * 1024):.2f} GB),"
+            f"available memory: {available_memory / (1024 * 1024):,.2f} MB ({available_memory / (1024 * 1024 * 1024):.2f} GB)."
+        )
+        progress_handler = self._create_progress_handler(local_file_path)
+        # Copy a remote file in remote path from the SFTP server to the local host as local path.
+        self.sftp_client.sftp_connection.get(self.uri, local_file_path, callback=progress_handler)
+
+
+class SourceSFTPBulkStreamReader(AbstractFileBasedStreamReader):
     def __init__(self):
         super().__init__()
         self._sftp_client = None
@@ -67,7 +123,7 @@ class SourceSFTPBulkStreamReader(AbstractFileBasedStreamReader):
         globs: List[str],
         prefix: Optional[str],
         logger: logging.Logger,
-    ) -> Iterable[RemoteFile]:
+    ) -> Iterable[SFTPBulkUploadableRemoteFile]:
         directories = [self._config.folder_path or "/"]
 
         # Iterate through directories and subdirectories
@@ -77,113 +133,18 @@ class SourceSFTPBulkStreamReader(AbstractFileBasedStreamReader):
                 if item.st_mode and stat.S_ISDIR(item.st_mode):
                     directories.append(f"{current_dir}/{item.filename}")
                 else:
+                    file = SFTPBulkUploadableRemoteFile(
+                        sftp_client=self.sftp_client,
+                        logger=logger,
+                        uri=f"{current_dir}/{item.filename}",
+                        last_modified=datetime.datetime.fromtimestamp(item.st_mtime),
+                        updated_at=datetime.datetime.fromtimestamp(item.st_mtime).strftime("%Y-%m-%dT%H:%M:%S.%fZ"),
+                    )
                     yield from self.filter_files_by_globs_and_start_date(
-                        [RemoteFile(uri=f"{current_dir}/{item.filename}", last_modified=datetime.datetime.fromtimestamp(item.st_mtime))],
+                        [file],
                         globs,
                     )
 
-    def open_file(self, file: RemoteFile, mode: FileReadMode, encoding: Optional[str], logger: logging.Logger) -> IOBase:
+    def open_file(self, file: SFTPBulkUploadableRemoteFile, mode: FileReadMode, encoding: Optional[str], logger: logging.Logger) -> IOBase:
         remote_file = self.sftp_client.sftp_connection.open(file.uri, mode=mode.value)
         return remote_file
-
-    @staticmethod
-    def create_progress_handler(local_file_path: str, logger: logging.Logger):
-        previous_bytes_copied = 0
-
-        def progress_handler(bytes_copied, total_bytes):
-            nonlocal previous_bytes_copied
-            if bytes_copied - previous_bytes_copied >= 100 * 1024 * 1024:
-                logger.info(
-                    f"{bytes_copied / (1024 * 1024):,.2f} MB ({bytes_copied / (1024 * 1024 * 1024):.2f} GB) "
-                    f"of {total_bytes / (1024 * 1024):,.2f} MB ({total_bytes / (1024 * 1024 * 1024):.2f} GB) "
-                    f"written to {local_file_path}"
-                )
-                previous_bytes_copied = bytes_copied
-
-                # Get available disk space
-                disk_usage = psutil.disk_usage("/")
-                available_disk_space = disk_usage.free
-
-                # Get available memory
-                memory_info = psutil.virtual_memory()
-                available_memory = memory_info.available
-                logger.info(
-                    f"Available disk space: {available_disk_space / (1024 * 1024):,.2f} MB ({available_disk_space / (1024 * 1024 * 1024):.2f} GB), "
-                    f"available memory: {available_memory / (1024 * 1024):,.2f} MB ({available_memory / (1024 * 1024 * 1024):.2f} GB)."
-                )
-
-        return progress_handler
-
-    @override
-    def upload(
-        self, file: RemoteFile, local_directory: str, logger: logging.Logger
-    ) -> Tuple[FileRecordData, AirbyteRecordMessageFileReference]:
-        """
-        Downloads a file from SFTP server to a specified local directory.
-
-        Args:
-            file (RemoteFile): The remote file object containing URI and metadata.
-            local_directory (str): The local directory path where the file will be downloaded.
-            logger (logging.Logger): Logger for logging information and errors.
-
-        Returns:
-            Tuple[FileRecordData, AirbyteRecordMessageFileReference]: Contains file record data and file reference for Airbyte protocol.
-
-        Raises:
-            FileSizeLimitError: If the file size exceeds the predefined limit (1 GB).
-        """
-        file_size = self.file_size(file)
-        # I'm putting this check here so we can remove the safety wheels per connector when ready.
-        if file_size > self.FILE_SIZE_LIMIT:
-            message = "File size exceeds the 1 GB limit."
-            raise FileSizeLimitError(message=message, internal_message=message, failure_type=FailureType.config_error)
-
-        file_paths = self._get_file_transfer_paths(file.uri, local_directory)
-        local_file_path = file_paths[self.LOCAL_FILE_PATH]
-        file_relative_path = file_paths[self.FILE_RELATIVE_PATH]
-        file_name = file_paths[self.FILE_NAME]
-
-        # Get available disk space
-        disk_usage = psutil.disk_usage("/")
-        available_disk_space = disk_usage.free
-
-        # Get available memory
-        memory_info = psutil.virtual_memory()
-        available_memory = memory_info.available
-
-        # Log file size, available disk space, and memory
-        logger.info(
-            f"Starting to download the file {file.uri} with size: {file_size / (1024 * 1024):,.2f} MB ({file_size / (1024 * 1024 * 1024):.2f} GB) "
-            f"to '{local_file_path}' "
-            f"with size: {file_size / (1024 * 1024):,.2f} MB ({file_size / (1024 * 1024 * 1024):.2f} GB), "
-            f"available disk space: {available_disk_space / (1024 * 1024):,.2f} MB ({available_disk_space / (1024 * 1024 * 1024):.2f} GB),"
-            f"available memory: {available_memory / (1024 * 1024):,.2f} MB ({available_memory / (1024 * 1024 * 1024):.2f} GB)."
-        )
-        progress_handler = self.create_progress_handler(local_file_path, logger)
-        start_download_time = time.time()
-        # Copy a remote file in remote path from the SFTP server to the local host as local path.
-        self.sftp_client.sftp_connection.get(file.uri, local_file_path, callback=progress_handler)
-
-        download_duration = time.time() - start_download_time
-        logger.info(f"Time taken to download the file {file.uri}: {download_duration:,.2f} seconds.")
-        logger.info(f"File {file_relative_path} successfully written to {local_directory}.")
-
-        file_record_data = FileRecordData(
-            folder=file_paths[self.FILE_FOLDER],
-            file_name=file_name,
-            bytes=file_size,
-            updated_at=file.last_modified.strftime("%Y-%m-%dT%H:%M:%S.%fZ"),
-            source_uri=f"sftp://{self.config.username}@{self.config.host}:{self.config.port}{file.uri}",
-        )
-
-        file_reference = AirbyteRecordMessageFileReference(
-            staging_file_url=local_file_path,
-            source_file_relative_path=file_relative_path,
-            file_size_bytes=file_size,
-        )
-
-        return file_record_data, file_reference
-
-    def file_size(self, file: RemoteFile):
-        file_size = self.sftp_client.sftp_connection.stat(file.uri).st_size
-        return file_size
