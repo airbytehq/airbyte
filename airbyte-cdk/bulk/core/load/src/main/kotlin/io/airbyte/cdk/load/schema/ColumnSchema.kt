0fe4af945da82fc611a20ad328bf0e723c90f8e0
tryangul <ryan.broughan@gmail.com>
2025-11-14 15:43:32 -0800

    FieldType.

--- airbyte-cdk/bulk/core/load/src/main/kotlin/io/airbyte/cdk/load/schema/ColumnSchema.kt
+++ airbyte-cdk/bulk/core/load/src/main/kotlin/io/airbyte/cdk/load/schema/ColumnSchema.kt
@@ -1,10 +1,10 @@
 package io.airbyte.cdk.load.schema
 
 import io.airbyte.cdk.load.component.ColumnType
-import io.airbyte.cdk.load.data.AirbyteType
+import io.airbyte.cdk.load.data.FieldType
 
 data class ColumnSchema(
-    val rawSchema: Map<String, AirbyteType>,
+    val rawSchema: Map<String, FieldType>,
     // raw schema name to final resolved name
     val rawToFinalColumnNames: Map<String, String>,
     // resolved name to resolved type
