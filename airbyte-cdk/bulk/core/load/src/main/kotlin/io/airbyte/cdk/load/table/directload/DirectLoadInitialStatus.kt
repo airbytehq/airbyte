8e437b321da6c8245b56108ebe2e28ae22fefa0d
Ryan Br... <ryan.broughan@gmail.com>
2025-12-03 13:29:15 -0800

    Rbroughan/dest stream table schema final (#70279)

--- airbyte-cdk/bulk/core/load/src/main/kotlin/io/airbyte/cdk/load/table/directload/DirectLoadInitialStatus.kt
+++ airbyte-cdk/bulk/core/load/src/main/kotlin/io/airbyte/cdk/load/table/directload/DirectLoadInitialStatus.kt
@@ -2,9 +2,9 @@
  * Copyright (c) 2025 Airbyte, Inc., all rights reserved.
  */
 
-package io.airbyte.cdk.load.orchestration.db.direct_load_table
+package io.airbyte.cdk.load.table.directload
 
-import io.airbyte.cdk.load.orchestration.db.DatabaseInitialStatus
+import io.airbyte.cdk.load.table.DatabaseInitialStatus
 
 data class DirectLoadInitialStatus(
     val realTable: DirectLoadTableStatus?,
