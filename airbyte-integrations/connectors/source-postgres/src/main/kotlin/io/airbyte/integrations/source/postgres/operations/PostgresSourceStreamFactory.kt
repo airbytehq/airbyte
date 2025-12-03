fd5bfb5cc285ec200fa6a7e27d7798b1b31e9115
Matt Bayley <31358253+mwbayley@users.noreply.github.com>
2025-12-03 15:54:12 -0800

    source-postgres: introduce cdc on bulk cdk (#70278)

--- airbyte-integrations/connectors/source-postgres/src/main/kotlin/io/airbyte/integrations/source/postgres/operations/PostgresSourceStreamFactory.kt
+++ airbyte-integrations/connectors/source-postgres/src/main/kotlin/io/airbyte/integrations/source/postgres/operations/PostgresSourceStreamFactory.kt
@@ -1,14 +1,24 @@
 /* Copyright (c) 2025 Airbyte, Inc., all rights reserved. */
 package io.airbyte.integrations.source.postgres.operations
 
+import com.fasterxml.jackson.databind.JsonNode
+import com.fasterxml.jackson.databind.node.NullNode
 import com.fasterxml.jackson.databind.node.ObjectNode
 import io.airbyte.cdk.command.OpaqueStateValue
+import io.airbyte.cdk.data.JsonEncoder
+import io.airbyte.cdk.discover.CdcOffsetDateTimeMetaFieldType
+import io.airbyte.cdk.discover.CdcStringMetaFieldType
+import io.airbyte.cdk.discover.CommonMetaField
 import io.airbyte.cdk.discover.DataOrMetaField
 import io.airbyte.cdk.discover.JdbcAirbyteStreamFactory
 import io.airbyte.cdk.discover.MetaField
+import io.airbyte.cdk.output.sockets.FieldValueEncoder
 import io.airbyte.cdk.output.sockets.NativeRecordPayload
 import io.airbyte.cdk.read.Stream
-import io.airbyte.integrations.source.postgres.PostgresSourceCdcMetaFields
+import io.airbyte.cdk.read.cdc.DebeziumOffset
+import io.airbyte.integrations.source.postgres.cdc.PostgresSourceCdcMetaFields
+import io.airbyte.integrations.source.postgres.cdc.PostgresSourceCdcPosition
+import io.airbyte.integrations.source.postgres.cdc.PostgresSourceDebeziumOperations
 import io.micronaut.context.annotation.Primary
 import jakarta.inject.Singleton
 import java.time.OffsetDateTime
@@ -17,9 +27,14 @@ import java.time.OffsetDateTime
 @Primary
 class PostgresSourceStreamFactory : JdbcAirbyteStreamFactory {
 
-    override val globalCursor: DataOrMetaField = PostgresSourceCdcMetaFields.CDC_CURSOR
+    override val globalCursor: DataOrMetaField = PostgresSourceCdcMetaFields.CDC_LSN
 
-    override val globalMetaFields: Set<MetaField> = emptySet() // TEMP
+    override val globalMetaFields: Set<MetaField> =
+        setOf(
+            PostgresSourceCdcMetaFields.CDC_LSN,
+            CommonMetaField.CDC_UPDATED_AT,
+            CommonMetaField.CDC_DELETED_AT,
+        )
 
     override fun decorateRecordData(
         timestamp: OffsetDateTime,
@@ -27,15 +42,56 @@ class PostgresSourceStreamFactory : JdbcAirbyteStreamFactory {
         stream: Stream,
         recordData: ObjectNode
     ) {
-        TODO()
+        recordData.set<JsonNode>(
+            CommonMetaField.CDC_UPDATED_AT.id,
+            CdcOffsetDateTimeMetaFieldType.jsonEncoder.encode(timestamp),
+        )
+        recordData.set<JsonNode>(PostgresSourceCdcMetaFields.CDC_LSN.id, NullNode.getInstance())
+        if (globalStateValue == null) {
+            return
+        }
+        val offset: DebeziumOffset =
+            PostgresSourceDebeziumOperations.deserializeStateUnvalidated(globalStateValue)
+        val position: PostgresSourceCdcPosition = PostgresSourceDebeziumOperations.position(offset)
+        val lsn = position.lsn?.asString()
+        if (lsn != null) {
+            recordData.set<JsonNode>(
+                PostgresSourceCdcMetaFields.CDC_LSN.id,
+                // TODO: Duplicates CDC_LSN.type.
+                //  Unable to use the reference due to * star projection.
+                //  Note: the same is true in the MySQL connector.
+                CdcStringMetaFieldType.jsonEncoder.encode(lsn),
+            )
+        }
     }
 
+    @Suppress("UNCHECKED_CAST")
     override fun decorateRecordData(
         timestamp: OffsetDateTime,
         globalStateValue: OpaqueStateValue?,
         stream: Stream,
         recordData: NativeRecordPayload
     ) {
-        TODO()
+        recordData[CommonMetaField.CDC_UPDATED_AT.id] =
+            FieldValueEncoder(
+                timestamp,
+                CommonMetaField.CDC_UPDATED_AT.type.jsonEncoder as JsonEncoder<Any>
+            )
+        recordData[PostgresSourceCdcMetaFields.CDC_LSN.id] =
+            FieldValueEncoder(
+                0.toDouble(),
+                PostgresSourceCdcMetaFields.CDC_LSN.type.jsonEncoder as JsonEncoder<Any>
+            )
+        if (globalStateValue == null) {
+            return
+        }
+        val offset: DebeziumOffset =
+            PostgresSourceDebeziumOperations.deserializeStateUnvalidated(globalStateValue)
+        val position: PostgresSourceCdcPosition = PostgresSourceDebeziumOperations.position(offset)
+        recordData[PostgresSourceCdcMetaFields.CDC_LSN.id] =
+            FieldValueEncoder(
+                position.lsn,
+                PostgresSourceCdcMetaFields.CDC_LSN.type.jsonEncoder as JsonEncoder<Any>
+            )
     }
 }
