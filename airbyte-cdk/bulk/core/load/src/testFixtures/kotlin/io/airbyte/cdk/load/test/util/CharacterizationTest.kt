28062d05bdd9a42b7abe7de51df32d4ef66e9ed8
Edward Gao <edward.gao@airbyte.io>
2025-12-17 15:54:00 -0800

    the dumbest possible thing

---
+++ airbyte-cdk/bulk/core/load/src/testFixtures/kotlin/io/airbyte/cdk/load/test/util/CharacterizationTest.kt
@@ -0,0 +1,30 @@
+/*
+ * Copyright (c) 2025 Airbyte, Inc., all rights reserved.
+ */
+
+package io.airbyte.cdk.load.test.util
+
+import java.nio.file.Files
+import java.nio.file.Path
+import org.junit.jupiter.api.Assertions
+
+object CharacterizationTest {
+    val testResourcesPath: Path = Path.of("src/test-integration/resources")
+    fun doAssert(
+        goldenFileName: String,
+        actualContents: String,
+    ) {
+        val goldenFilePath = testResourcesPath.resolve(goldenFileName)
+        if (!Files.exists(goldenFilePath)) {
+            Files.createDirectories(testResourcesPath)
+            Files.createFile(goldenFilePath)
+        }
+        val goldenFileContents = Files.readString(goldenFilePath)
+        Files.write(goldenFilePath, actualContents.toByteArray())
+        Assertions.assertEquals(
+            goldenFileContents,
+            actualContents,
+            "File contents did not match previous version. Inspect the `git diff` and commit the change if this is intended.",
+        )
+    }
+}
