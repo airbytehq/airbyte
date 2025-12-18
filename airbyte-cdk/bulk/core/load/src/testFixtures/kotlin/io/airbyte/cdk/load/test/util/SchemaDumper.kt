5874e2eeb60680c94483d870a830c27de6afaf60
Edward Gao <edward.gao@airbyte.io>
2025-12-17 16:11:06 -0800

    more comment

--- airbyte-cdk/bulk/core/load/src/testFixtures/kotlin/io/airbyte/cdk/load/test/util/SchemaDumper.kt
+++ airbyte-cdk/bulk/core/load/src/testFixtures/kotlin/io/airbyte/cdk/load/test/util/SchemaDumper.kt
@@ -9,5 +9,15 @@ package io.airbyte.cdk.load.test.util
  * [io.airbyte.cdk.load.component.TableSchemaEvolutionClient] instead.
  */
 interface SchemaDumper {
+    /**
+     * for destinations that already have a TableSchemaEvolutionClient, this is maybe just
+     * ```
+     * return Jsons.writerWithDefaultPrettyPrinter()
+     *   .writeValueAsString(client.discoverSchema(<something>))
+     * ```
+     * (converting namespace+name to TableName is doable but slightly nontrivial)
+     *
+     * and then destinations can append other stuff if they want (tablengine, etc.)
+     */
     fun discoverSchema(namespace: String?, name: String): String
 }
