b4fb45b9a6586bc057f6b928e306cf3745f22fac
Edward Gao <edward.gao@airbyte.io>
2025-12-19 13:35:04 -0800

    Bulk load CDK: data coercion test calls ValueCoercer.map (#71001)

--- airbyte-cdk/bulk/core/load/src/testFixtures/kotlin/io/airbyte/cdk/load/component/TableOperationsTestHarness.kt
+++ airbyte-cdk/bulk/core/load/src/testFixtures/kotlin/io/airbyte/cdk/load/component/TableOperationsTestHarness.kt
@@ -145,7 +145,8 @@ class TableOperationsTestHarness(
                 "test",
                 airbyteMetaField = null,
             )
-        val validatedValue = coercer.validate(inputValueAsEnrichedAirbyteValue)
+        val mappedValue = coercer.map(inputValueAsEnrichedAirbyteValue)
+        val validatedValue = coercer.validate(mappedValue)
         val valueToInsert: AirbyteValue
         val changeReason: Reason?
         when (validatedValue) {
@@ -158,7 +159,7 @@ class TableOperationsTestHarness(
                 changeReason = validatedValue.reason
             }
             ValidationResult.Valid -> {
-                valueToInsert = inputValue
+                valueToInsert = mappedValue.abValue
                 changeReason = null
             }
         }
