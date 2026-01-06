37fab670bb256ef47a87a7453769059c0b538db9
matt.bayley <matt.bayley@airbyte.io>
2026-01-05 16:24:16 -0800

    spaces in username

--- airbyte-ci/connectors/metadata_service/lib/metadata_service/docker_hub.py
+++ airbyte-ci/connectors/metadata_service/lib/metadata_service/docker_hub.py
@@ -17,7 +17,7 @@ def get_docker_hub_auth_token() -> str:
         raise ValueError("Please set the DOCKER_HUB_USERNAME and DOCKER_HUB_PASSWORD environment variables.")
 
     # TODO: remove
-    print(f"DOCKER_HUB_USERNAME = '{docker_username}'")
+    print(f"DOCKER_HUB_USERNAME = '{" ".join(docker_username)}'")
 
     auth_url = "https://hub.docker.com/v2/users/login/"
     auth_data = {"username": docker_username, "password": docker_password}
