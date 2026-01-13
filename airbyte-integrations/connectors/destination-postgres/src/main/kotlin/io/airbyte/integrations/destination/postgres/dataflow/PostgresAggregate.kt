92fc209cce4327ee420980f0369e8d84ee340be5
Benoit Moriceau <benoit@airbyte.io>
2026-01-13 14:27:02 -0800

    tmp

--- airbyte-integrations/connectors/destination-postgres/src/main/kotlin/io/airbyte/integrations/destination/postgres/dataflow/PostgresAggregate.kt
+++ airbyte-integrations/connectors/destination-postgres/src/main/kotlin/io/airbyte/integrations/destination/postgres/dataflow/PostgresAggregate.kt
@@ -7,11 +7,15 @@ package io.airbyte.integrations.destination.postgres.dataflow
 import io.airbyte.cdk.load.dataflow.aggregate.Aggregate
 import io.airbyte.cdk.load.dataflow.transform.RecordDTO
 import io.airbyte.integrations.destination.postgres.write.load.PostgresInsertBuffer
+import io.github.oshai.kotlinlogging.KotlinLogging
+
+private val log = KotlinLogging.logger {}
 
 class PostgresAggregate(
     private val buffer: PostgresInsertBuffer,
 ) : Aggregate {
     override fun accept(record: RecordDTO) {
+        log.info { "received a record" }
         buffer.accumulate(record.fields)
     }
 
