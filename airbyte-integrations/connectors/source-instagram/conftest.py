a8c5d1356ab7be55d2cdec9047af3c166c7461ea
Octavia Squidington III <octavia-squidington-iii@users.noreply.github.com>
2026-02-09 22:59:47 +0000

    chore: auto-fix lint and format issues

--- airbyte-integrations/connectors/source-instagram/conftest.py
+++ airbyte-integrations/connectors/source-instagram/conftest.py
@@ -1,10 +1,11 @@
+# Copyright (c) 2026 Airbyte, Inc., all rights reserved.
+
 import pytest
 
 
 def pytest_collection_modifyitems(config, items):
     skip_basic_read = pytest.mark.skip(
-        reason="No Instagram Business Account linked to test Facebook Page "
-        "(see: https://github.com/airbytehq/oncall/issues/11217)"
+        reason="No Instagram Business Account linked to test Facebook Page " "(see: https://github.com/airbytehq/oncall/issues/11217)"
     )
     for item in items:
         if "test_airbyte_standards" in str(item.fspath) and item.name.startswith("test_basic_read"):
