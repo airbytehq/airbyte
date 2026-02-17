753719009c12aed661b06c2715a63d3c144d37f5
Rodi Reich Zilberman <867491+rodireich@users.noreply.github.com>
2026-02-17 15:03:10 -0800

    Hstore provides its own value type for protobuf encoding

--- airbyte-integrations/connectors/source-postgres/src/main/kotlin/io/airbyte/integrations/source/postgres/operations/types/HstoreFieldType.kt
+++ airbyte-integrations/connectors/source-postgres/src/main/kotlin/io/airbyte/integrations/source/postgres/operations/types/HstoreFieldType.kt
@@ -8,10 +8,10 @@ import com.fasterxml.jackson.core.type.TypeReference
 import com.fasterxml.jackson.databind.JsonNode
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.databind.node.TextNode
-import io.airbyte.cdk.data.JsonCodec
 import io.airbyte.cdk.data.LeafAirbyteSchemaType
 import io.airbyte.cdk.jdbc.JdbcAccessor
 import io.airbyte.cdk.jdbc.SymmetricJdbcFieldType
+import io.airbyte.cdk.output.sockets.ProtobufAwareCustomConnectorJsonCodec
 import java.sql.PreparedStatement
 import java.sql.ResultSet
 
@@ -45,7 +45,7 @@ object HstoreAccessor : JdbcAccessor<Map<String, String?>> {
     }
 }
 
-object HstoreCodec : JsonCodec<Map<String, String?>> {
+object HstoreCodec : ProtobufAwareCustomConnectorJsonCodec<Map<String, String?>> {
     override fun encode(decoded: Map<String, String?>): JsonNode {
         return TextNode(nullValuePreservingObjectMapper.writeValueAsString(decoded))
     }
@@ -55,6 +55,10 @@ object HstoreCodec : JsonCodec<Map<String, String?>> {
             encoded.asText(),
             object : TypeReference<Map<String, String?>>() {}
         )
+    }
+
+    override fun valueForProtobufEncoding(v: Map<String, String?>): Any? {
+        return encode(v).asText()
     }
 }
 
