aee521e8c0224733370cd49dc44ba2e0421c9197
Rodi Reich Zilberman <867491+rodireich@users.noreply.github.com>
2026-03-24 16:37:33 -0700

    sanity

--- airbyte-integrations/connectors/source-clickhouse/src/main/java/io/airbyte/integrations/source/clickhouse/ClickHouseSource.java
+++ airbyte-integrations/connectors/source-clickhouse/src/main/java/io/airbyte/integrations/source/clickhouse/ClickHouseSource.java
@@ -90,7 +90,7 @@ public class ClickHouseSource extends AbstractJdbcSource<JDBCType> implements So
   }
 
   public ClickHouseSource(final FeatureFlags featureFlags) {
-    super(DRIVER_CLASS, NoOpStreamingQueryConfig::new, new ClickHouseSourceOperations());
+    super(DRIVER_CLASS, NoOpStreamingQueryConfig::new, JdbcUtils.getDefaultSourceOperations());
     this.featureFlags = featureFlags;
   }
 
