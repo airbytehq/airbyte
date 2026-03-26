32ff513429325642db90b709b0b29d6cbceab528
Octavia Squidington III <octavia-squidington-iii@users.noreply.github.com>
2026-03-26 21:40:21 +0000

    chore: auto-fix lint and format issues

--- airbyte-integrations/connectors/source-postgres/src/test/kotlin/io/airbyte/integrations/source/postgres/PostgresSourceFieldTypeMapperTest.kt
+++ airbyte-integrations/connectors/source-postgres/src/test/kotlin/io/airbyte/integrations/source/postgres/PostgresSourceFieldTypeMapperTest.kt
@@ -13,11 +13,11 @@ import io.airbyte.cdk.test.fixtures.connector.JdbcTestDbExecutor
 import io.airbyte.cdk.test.fixtures.tests.AnsiSql
 import io.airbyte.cdk.test.fixtures.tests.ExtendedSql
 import io.airbyte.cdk.test.fixtures.tests.FieldTypeMapperTest
+import io.airbyte.integrations.source.postgres.PostgresTestDatabase.BaseImage
 import io.airbyte.integrations.source.postgres.config.EncryptionDisable
 import io.airbyte.integrations.source.postgres.config.PostgresSourceConfigurationFactory
 import io.airbyte.integrations.source.postgres.config.PostgresSourceConfigurationSpecification
 import io.airbyte.integrations.source.postgres.config.StandardReplicationMethodConfigurationSpecification
-import io.airbyte.integrations.source.postgres.PostgresTestDatabase.BaseImage
 import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Timeout
 import org.testcontainers.containers.PostgreSQLContainer
