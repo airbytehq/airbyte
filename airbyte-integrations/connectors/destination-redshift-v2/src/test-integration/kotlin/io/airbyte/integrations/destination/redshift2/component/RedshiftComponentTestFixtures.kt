b675ae33e7a618ddfe19c54f5491ac2eba17a743
Sunil Kuruba <sunil.kuruba@airbyte.io>
2026-04-28 14:34:12 -0700

    feat(destination-redshift-v2): add SQL generation layer (#76427)

---
+++ airbyte-integrations/connectors/destination-redshift-v2/src/test-integration/kotlin/io/airbyte/integrations/destination/redshift2/component/RedshiftComponentTestFixtures.kt
@@ -0,0 +1,43 @@
+/*
+ * Copyright (c) 2026 Airbyte, Inc., all rights reserved.
+ */
+
+package io.airbyte.integrations.destination.redshift2.component
+
+import io.airbyte.cdk.load.component.ColumnType
+import io.airbyte.cdk.load.component.TableSchema
+import io.airbyte.integrations.destination.redshift2.sql.RedshiftDataType
+
+/**
+ * Test fixtures for Redshift component tests.
+ *
+ * Redshift uses lowercase identifiers (via [toRedshiftCompatibleName]), so all column name mappings
+ * are identity mappings -- the CDK default mappings work without transformation.
+ */
+object RedshiftComponentTestFixtures {
+
+    /**
+     * Expected table schema mapping all Airbyte types to their Redshift equivalents. Column names
+     * match the CDK's [TableOperationsFixtures.ALL_TYPES_SCHEMA] field names after applying
+     * [toRedshiftCompatibleName] (which is identity for these lowercase names).
+     */
+    val allTypesTableSchema =
+        TableSchema(
+            mapOf(
+                "string" to ColumnType(RedshiftDataType.VARCHAR.typeName, true),
+                "boolean" to ColumnType(RedshiftDataType.BOOLEAN.typeName, true),
+                "integer" to ColumnType(RedshiftDataType.BIGINT.typeName, true),
+                "number" to ColumnType(RedshiftDataType.NUMERIC.typeName, true),
+                "date" to ColumnType(RedshiftDataType.DATE.typeName, true),
+                "timestamp_tz" to ColumnType(RedshiftDataType.TIMESTAMPTZ.typeName, true),
+                "timestamp_ntz" to ColumnType(RedshiftDataType.TIMESTAMP.typeName, true),
+                "time_tz" to ColumnType(RedshiftDataType.TIMETZ.typeName, true),
+                "time_ntz" to ColumnType(RedshiftDataType.TIME.typeName, true),
+                "array" to ColumnType(RedshiftDataType.SUPER.typeName, true),
+                "object" to ColumnType(RedshiftDataType.SUPER.typeName, true),
+                "union" to ColumnType(RedshiftDataType.SUPER.typeName, true),
+                "legacy_union" to ColumnType(RedshiftDataType.SUPER.typeName, true),
+                "unknown" to ColumnType(RedshiftDataType.SUPER.typeName, true),
+            )
+        )
+}
