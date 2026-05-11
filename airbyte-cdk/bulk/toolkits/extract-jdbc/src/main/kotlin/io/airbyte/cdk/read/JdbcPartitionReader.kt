eaebe62c72a8fdc575ef9e86e1188681d6b0e6ad
Rodi Reich Zilberman <867491+rodireich@users.noreply.github.com>
2026-05-11 15:59:42 -0700

    test

--- airbyte-cdk/bulk/toolkits/extract-jdbc/src/main/kotlin/io/airbyte/cdk/read/JdbcPartitionReader.kt
+++ airbyte-cdk/bulk/toolkits/extract-jdbc/src/main/kotlin/io/airbyte/cdk/read/JdbcPartitionReader.kt
@@ -12,6 +12,8 @@ import io.airbyte.cdk.output.OutputMessageRouter
 import io.airbyte.cdk.output.sockets.NativeRecordPayload
 import io.airbyte.protocol.models.v0.AirbyteStateMessage
 import io.airbyte.protocol.models.v0.AirbyteStreamStatusTraceMessage
+import io.github.oshai.kotlinlogging.KotlinLogging
+import java.net.SocketTimeoutException
 import java.time.Duration
 import java.time.Instant
 import java.util.concurrent.atomic.AtomicBoolean
@@ -119,6 +121,11 @@ class JdbcNonResumablePartitionReader<P : JdbcPartition<*>>(
     val runComplete = AtomicBoolean(false)
     val numRecords = AtomicLong()
 
+    private val log = KotlinLogging.logger {}
+    companion object {
+        val flf = AtomicBoolean(false)
+    }
+
     override suspend fun run() {
         outputPendingMessages()
         /* Don't start read if we've gone over max duration.
@@ -126,6 +133,11 @@ class JdbcNonResumablePartitionReader<P : JdbcPartition<*>>(
         existing exiting with an exception skips checkpoint(), so any work we
         did before time has elapsed will be wasted. */
         checkMaxReadTimeElapsed()
+
+        if (/*stream.name in listOf( "snowflake_1tb", "vv100", "caseSen") &&*/ flf.getAndSet(true).not()) {
+            log.info { "*** throwing an exception" }
+            throw SocketTimeoutException()
+        }
 
         selectQuerier
             .executeQuery(
