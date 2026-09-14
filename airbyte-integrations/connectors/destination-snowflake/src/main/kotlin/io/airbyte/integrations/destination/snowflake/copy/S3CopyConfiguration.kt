d8a260ffdd25cb7b9783e410bb25dbc825248675
Johnny Schmidt <john.schmidt@airbyte.io>
2026-09-14 13:42:54 -0700

    chore(destination-snowflake): add required Fusion source license headers

--- airbyte-integrations/connectors/destination-snowflake/src/main/kotlin/io/airbyte/integrations/destination/snowflake/copy/S3CopyConfiguration.kt
+++ airbyte-integrations/connectors/destination-snowflake/src/main/kotlin/io/airbyte/integrations/destination/snowflake/copy/S3CopyConfiguration.kt
@@ -1,3 +1,7 @@
+/*
+ * Copyright (c) 2026 Airbyte, Inc., all rights reserved.
+ */
+
 package io.airbyte.integrations.destination.snowflake.copy
 
 import io.airbyte.integrations.destination.snowflake.spec.SnowflakeSpecification
